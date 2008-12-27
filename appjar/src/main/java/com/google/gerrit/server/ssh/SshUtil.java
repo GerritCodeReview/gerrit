ea674ad42259286913b42b341ed640345944fa43
Shawn O. Pearce <sop@google.com>
2008-12-26 18:47:40 -0800

    Automatically convert RFC 4716 style SSH keys to OpenSSH key format
    This way users can paste whatever style key they have on hand, and
    the server writes the key into the database using a uniform format.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- appjar/src/main/java/com/google/gerrit/server/ssh/SshUtil.java
+++ appjar/src/main/java/com/google/gerrit/server/ssh/SshUtil.java
@@ -21,12 +21,18 @@ import com.google.gwtorm.client.OrmException;
 import com.google.gwtorm.client.SchemaFactory;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.sshd.common.KeyPairProvider;
 import org.apache.sshd.common.util.Buffer;
 import org.spearce.jgit.lib.Constants;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PublicKey;
+import java.security.interfaces.DSAPublicKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -57,6 +63,65 @@ public class SshUtil {
       return new Buffer(bin).getPublicKey();
     } catch (RuntimeException re) {
       throw new InvalidKeySpecException("Cannot parse key", re);
+    }
+  }
+
+  /**
+   * Convert an RFC 4716 style key to an OpenSSH style key.
+   * 
+   * @param keyStr the key string to convert.
+   * @return <code>keyStr</code> if conversion failed; otherwise the converted
+   *         key, in OpenSSH key format.
+   */
+  public static String toOpenSshPublicKey(final String keyStr) {
+    try {
+      final StringBuilder strBuf = new StringBuilder();
+      final BufferedReader br = new BufferedReader(new StringReader(keyStr));
+      String line = br.readLine(); // BEGIN SSH2 line...
+      if (!line.equals("---- BEGIN SSH2 PUBLIC KEY ----")) {
+        return keyStr;
+      }
+
+      while ((line = br.readLine()) != null) {
+        if (line.indexOf(':') == -1) {
+          strBuf.append(line);
+          break;
+        }
+      }
+
+      while ((line = br.readLine()) != null) {
+        if (line.startsWith("---- ")) {
+          break;
+        }
+        strBuf.append(line);
+      }
+
+      final PublicKey key =
+          new Buffer(Base64.decodeBase64(Constants.encodeASCII(strBuf
+              .toString()))).getPublicKey();
+      if (key instanceof RSAPublicKey) {
+        strBuf.insert(0, KeyPairProvider.SSH_RSA + " ");
+
+      } else if (key instanceof DSAPublicKey) {
+        strBuf.insert(0, KeyPairProvider.SSH_DSS + " ");
+
+      } else {
+        return keyStr;
+      }
+
+      strBuf.append(' ');
+      strBuf.append("converted-key");
+      return strBuf.toString();
+    } catch (IOException e) {
+      return keyStr;
+    } catch (RuntimeException re) {
+      return keyStr;
+    } catch (NoSuchAlgorithmException e) {
+      return keyStr;
+    } catch (InvalidKeySpecException e) {
+      return keyStr;
+    } catch (NoSuchProviderException e) {
+      return keyStr;
     }
   }
 

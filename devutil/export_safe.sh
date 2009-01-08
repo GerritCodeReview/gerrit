be68741424bb654bb249cb124bf3b32328817c17
Shawn O. Pearce <sop@google.com>
2009-01-08 09:00:38 -0800

    Don't drop gerrit1.delta_content as it doesn't exist anymore
    Gerrit1 no longer creates this table when it makes a backup
    database, as it no longer populates it.  Future dumps made
    though this script won't contain the entity.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- devutil/export_safe.sh
+++ devutil/export_safe.sh
@@ -14,7 +14,6 @@ dropdb $tmp
 createdb -E UTF-8 $tmp &&
 pg_dump -O -Fc $src >$N &&
 pg_restore -d $tmp $N &&
-psql -c 'DROP TABLE gerrit1.delta_content' $tmp &&
 psql -c 'UPDATE gerrit1.accounts SET
   mailing_address = NULL
  ,mailing_address_country = NULL

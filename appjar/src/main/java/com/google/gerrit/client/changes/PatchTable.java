613aeac219f2493cd9f11a1fd3b58c5877d99968
Shawn O. Pearce <sop@google.com>
2009-01-22 14:35:41 -0800

    Rewrite PatchTable to generate itself by parsing HTML
    In most cases it can be more efficient to build dense table HTML
    in a string and let the browser parse it.  This should help on a
    really bad case, like a patch set with 4,600+ files.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- appjar/src/main/java/com/google/gerrit/client/changes/PatchTable.java
+++ appjar/src/main/java/com/google/gerrit/client/changes/PatchTable.java
@@ -16,45 +16,131 @@ package com.google.gerrit.client.changes;
 
 import com.google.gerrit.client.Link;
 import com.google.gerrit.client.reviewdb.Patch;
+import com.google.gerrit.client.ui.DomUtil;
 import com.google.gerrit.client.ui.FancyFlexTable;
-import com.google.gerrit.client.ui.PatchLink;
-import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.SourcesTableEvents;
 import com.google.gwt.user.client.ui.TableListener;
-import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
-import com.google.gwt.user.client.ui.HTMLTable.CellFormatter;
 
 import java.util.List;
 
 public class PatchTable extends FancyFlexTable<Patch> {
-  private static final int C_TYPE = 1;
-  private static final int C_NAME = 2;
-  private static final int C_COMMENTS = 3;
-  private static final int C_DIFF = 4;
-  private static final int N_DIFF = 2;
-
   public PatchTable() {
-    table.setText(0, C_TYPE, "");
-    table.setText(0, C_NAME, Util.C.patchTableColumnName());
-    table.setText(0, C_COMMENTS, Util.C.patchTableColumnComments());
-    table.setText(0, C_DIFF, Util.C.patchTableColumnDiff());
-
-    final FlexCellFormatter fmt = table.getFlexCellFormatter();
-    fmt.addStyleName(0, C_TYPE, S_ICON_HEADER);
-    fmt.addStyleName(0, C_NAME, S_DATA_HEADER);
-    fmt.addStyleName(0, C_COMMENTS, S_DATA_HEADER);
-    fmt.addStyleName(0, C_DIFF, S_DATA_HEADER);
-    fmt.setColSpan(0, C_DIFF, N_DIFF);
-
     table.addTableListener(new TableListener() {
       public void onCellClicked(SourcesTableEvents sender, int row, int cell) {
-        movePointerTo(row);
+        if (row > 0) {
+          movePointerTo(row);
+        }
       }
     });
+  }
+
+  public void display(final List<Patch> list) {
+    final StringBuilder nc = new StringBuilder();
+    appendHeader(nc);
+    for (final Patch p : list) {
+      appendRow(nc, p);
+    }
+    appendRow(nc, null);
+    resetHtml(nc.toString());
+
+    int row = 1;
+    for (final Patch p : list) {
+      setRowItem(row++, p);
+    }
+  }
+
+  private void appendHeader(final StringBuilder nc) {
+    nc.append("<tr>");
+    nc.append("<td class=\"" + S_ICON_HEADER + " LeftMostCell\">&nbsp;</td>");
+    nc.append("<td class=\"" + S_ICON_HEADER + "\">&nbsp;</td>");
+
+    nc.append("<td class=\"" + S_DATA_HEADER + "\">");
+    nc.append(Util.C.patchTableColumnName());
+    nc.append("</td>");
+
+    nc.append("<td class=\"" + S_DATA_HEADER + "\">");
+    nc.append(Util.C.patchTableColumnComments());
+    nc.append("</td>");
+
+    nc.append("<td class=\"" + S_DATA_HEADER + "\" colspan=\"2\">");
+    nc.append(Util.C.patchTableColumnDiff());
+    nc.append("</td>");
+
+    nc.append("</tr>");
+  }
+
+  private void appendRow(final StringBuilder nc, final Patch p) {
+    nc.append("<tr>");
+    nc.append("<td class=\"" + S_ICON_CELL + " LeftMostCell\">&nbsp;</td>");
+
+    nc.append("<td class=\"ChangeTypeCell\">");
+    nc.append(p.getChangeType().getCode());
+    nc.append("</td>");
+
+    nc.append("<td class=\"" + S_DATA_CELL + " FilePathCell\">");
+    nc.append("<a href=\"#");
+    if (p.getPatchType() == Patch.PatchType.UNIFIED) {
+      nc.append(Link.toPatchSideBySide(p.getKey()));
+    } else {
+      nc.append(Link.toPatchUnified(p.getKey()));
+    }
+    nc.append("\">");
+    nc.append(DomUtil.escape(p.getFileName()));
+    nc.append("</a>");
+    if (p.getSourceFileName() != null) {
+      final String secondLine;
+      if (p.getChangeType() == Patch.ChangeType.RENAMED) {
+        secondLine = Util.M.renamedFrom(p.getSourceFileName());
+
+      } else if (p.getChangeType() == Patch.ChangeType.COPIED) {
+        secondLine = Util.M.copiedFrom(p.getSourceFileName());
+
+      } else {
+        secondLine = Util.M.otherFrom(p.getSourceFileName());
+      }
+      nc.append("<br>");
+      nc.append("<span class=\"SourceFilePath\">");
+      nc.append(DomUtil.escape(secondLine));
+      nc.append("</span>");
+    }
+    nc.append("</td>");
+
+    nc.append("<td class=\"" + S_DATA_CELL + " CommentCell\">");
+    if (p.getCommentCount() > 0) {
+      nc.append(Util.M.patchTableComments(p.getCommentCount()));
+    }
+    if (p.getDraftCount() > 0) {
+      if (p.getCommentCount() > 0) {
+        nc.append(", ");
+      }
+      nc.append("<span class=\"Drafts\">");
+      nc.append(Util.M.patchTableDrafts(p.getDraftCount()));
+      nc.append("</span>");
+    }
+    nc.append("</td>");
+
+    nc.append("<td class=\"" + S_DATA_CELL + " DiffLinkCell\">");
+    if (p.getPatchType() == Patch.PatchType.UNIFIED) {
+      nc.append("<a href=\"#");
+      nc.append(Link.toPatchSideBySide(p.getKey()));
+      nc.append("\">");
+      nc.append(Util.C.patchTableDiffSideBySide());
+      nc.append("</a>");
+    } else {
+      nc.append("&nbsp;");
+    }
+    nc.append("</td>");
+
+    nc.append("<td class=\"" + S_DATA_CELL + " DiffLinkCell\">");
+    nc.append("<a href=\"#");
+    nc.append(Link.toPatchUnified(p.getKey()));
+    nc.append("\">");
+    nc.append(Util.C.patchTableDiffUnified());
+    nc.append("</a>");
+    nc.append("</td>");
+
+    nc.append("</tr>");
   }
 
   @Override
@@ -65,123 +151,5 @@ public class PatchTable extends FancyFlexTable<Patch> {
   @Override
   protected void onOpenItem(final Patch item) {
     History.newItem(Link.toPatchSideBySide(item.getKey()));
-  }
-
-  @Override
-  protected void applyDataRowStyle(final int row) {
-    super.applyDataRowStyle(row);
-    final CellFormatter fmt = table.getCellFormatter();
-    fmt.addStyleName(row, C_TYPE, "ChangeTypeCell");
-
-    fmt.addStyleName(row, C_NAME, S_DATA_CELL);
-    fmt.addStyleName(row, C_NAME, "FilePathCell");
-
-    fmt.addStyleName(row, C_COMMENTS, S_DATA_CELL);
-    fmt.addStyleName(row, C_COMMENTS, "CommentCell");
-
-    fmt.addStyleName(row, C_DIFF + 0, S_DATA_CELL);
-    fmt.addStyleName(row, C_DIFF + 0, "DiffLinkCell");
-
-    fmt.addStyleName(row, C_DIFF + 1, S_DATA_CELL);
-    fmt.addStyleName(row, C_DIFF + 1, "DiffLinkCell");
-  }
-
-  public void display(final List<Patch> list) {
-    final int sz = list != null ? list.size() : 0;
-    int dataRows = table.getRowCount() - 1;
-    while (sz < dataRows) {
-      table.removeRow(dataRows);
-      dataRows--;
-    }
-
-    for (int i = 0; i < sz; i++) {
-      if (dataRows <= i) {
-        table.insertRow(++dataRows);
-        applyDataRowStyle(i + 1);
-      }
-      populate(i + 1, list.get(i));
-    }
-  }
-
-  private void populate(final int row, final Patch patch) {
-    table.setWidget(row, C_ARROW, null);
-    table.setText(row, C_TYPE, "" + patch.getChangeType().getCode());
-
-    Widget nameLink;
-    if (patch.getPatchType() == Patch.PatchType.UNIFIED) {
-      nameLink = new SideBySide(patch.getKey().get(), patch.getKey());
-    } else {
-      nameLink = new Unified(patch.getKey().get(), patch.getKey());
-    }
-    if (patch.getSourceFileName() != null) {
-      final String secondLine;
-      if (patch.getChangeType() == Patch.ChangeType.RENAMED) {
-        secondLine = Util.M.renamedFrom(patch.getSourceFileName());
-
-      } else if (patch.getChangeType() == Patch.ChangeType.COPIED) {
-        secondLine = Util.M.copiedFrom(patch.getSourceFileName());
-
-      } else {
-        secondLine = Util.M.otherFrom(patch.getSourceFileName());
-      }
-
-      final InlineLabel secondLineLabel = new InlineLabel(secondLine);
-      secondLineLabel.setStyleName("SourceFilePath");
-
-      final FlowPanel fp = new FlowPanel();
-      fp.add(nameLink);
-      fp.add(secondLineLabel);
-      nameLink = fp;
-    }
-    table.setWidget(row, C_NAME, nameLink);
-
-    final StringBuilder commentStr = new StringBuilder();
-    if (patch.getCommentCount() > 0) {
-      commentStr.append(Util.M.patchTableComments(patch.getCommentCount()));
-    }
-    if (patch.getDraftCount() > 0) {
-      if (commentStr.length() > 0) {
-        commentStr.append(", ");
-      }
-      commentStr.append("<span class=\"Drafts\">");
-      commentStr.append(Util.M.patchTableDrafts(patch.getDraftCount()));
-      commentStr.append("</span>");
-    }
-    table.setHTML(row, C_COMMENTS, commentStr.toString());
-
-    if (patch.getPatchType() == Patch.PatchType.UNIFIED) {
-      table.setWidget(row, C_DIFF + 0, new SideBySide(Util.C
-          .patchTableDiffSideBySide(), patch.getKey()));
-    } else {
-      table.clearCell(row, C_DIFF + 0);
-    }
-    table.setWidget(row, C_DIFF + 1, new Unified(
-        Util.C.patchTableDiffUnified(), patch.getKey()));
-
-    setRowItem(row, patch);
-  }
-
-  private final class SideBySide extends PatchLink.SideBySide {
-    private SideBySide(final String text, final Patch.Key p) {
-      super(text, p);
-    }
-
-    @Override
-    protected void onClick(final Event event) {
-      movePointerTo(key);
-      super.onClick(event);
-    }
-  }
-
-  private final class Unified extends PatchLink.Unified {
-    private Unified(final String text, final Patch.Key p) {
-      super(text, p);
-    }
-
-    @Override
-    protected void onClick(final Event event) {
-      movePointerTo(key);
-      super.onClick(event);
-    }
   }
 }

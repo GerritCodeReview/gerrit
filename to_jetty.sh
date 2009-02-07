ba1f56d15eb3f4f10abe4f08f2466121ebf1c1f8
Shawn O. Pearce <sop@google.com>
2009-02-06 16:32:56 -0800

    Fix to_jetty so it doesn't unpack c3p0 from our WAR
    We don't have c3p0 in the WAR anymore as its an "optional" feature
    that the user should setup in their container.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- to_jetty.sh
+++ to_jetty.sh
@@ -24,14 +24,10 @@ if [ -f "$ctx" ]
 then
 	touch "$ctx"
 else
-	for f in postgresql-8.3-603.jdbc3.jar c3p0-0.9.1.2.jar
-	do
-		java -jar $war --cat lib/$f >"$jetty/lib/plus/$f"
-	done
-
 	rm -f "$jetty/contexts/test.xml" &&
 	java -jar $war --cat extra/jetty_gerrit.xml >"$ctx" &&
 
-	echo >&2 &&
+	echo >&2
+	echo >&2 "You need to copy JDBC drivers to $jetty/lib/plus"
 	echo >&2 "You need to edit and configure $ctx"
 fi

d90a77098d675c752c2d3b6ede082d860869d43a
Shawn O. Pearce <sop@google.com>
2009-02-25 21:40:54 -0800

    Allow clicking on a copyable text to switch label to input box
    Another great idea stolen from github.com.  If the user single-clicks
    on a label that is marked as copyable we convert the label to a text
    box and select everything, so the user only needs to use Ctrl-C to
    copy the text onto their clipboard.  A keyboard binding converts the
    field back to a label after the copy completes, and a focus listener
    also does the same if focus is lost on the input box.
    
    This is a nice workaround for browsers which do not have a flash
    plugin installed or enabled.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- src/main/java/com/google/gerrit/client/ui/CopyableText.java
+++ src/main/java/com/google/gerrit/client/ui/CopyableText.java
@@ -16,33 +16,51 @@ package com.google.gerrit.client.ui;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusListener;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.KeyboardListenerAdapter;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
-public class CopyableText extends Widget {
+public class CopyableText extends Composite implements ClickListener {
   private static final int SWF_WIDTH = 110;
   private static final int SWF_HEIGHT = 14;
   private static final String swfUrl =
       GWT.getModuleBaseURL() + "clippy1.cache.swf";
+
+  private final FlowPanel content;
+  private Label textLabel;
+  private TextBox textBox;
 
   public CopyableText(final String str) {
     this(str, true);
   }
 
   public CopyableText(final String str, final boolean showLabel) {
-    setElement(DOM.createDiv());
-    setStyleName("gerrit-CopyableText");
+    content = new FlowPanel();
+    content.setStyleName("gerrit-CopyableText");
+    initWidget(content);
 
     final String flashVars = "text=" + URL.encodeComponent(str);
     final StringBuilder html = new StringBuilder();
 
     if (showLabel) {
-      html.append("<span class=\"gerrit-CopyableText-Label\">");
-      html.append(DomUtil.escape(str));
-      html.append("</span>");
+      textLabel = new InlineLabel(str);
+      textLabel.setStyleName("gerrit-CopyableText-Label");
+      textLabel.addClickListener(this);
+      content.add(textLabel);
     }
 
-    html.append("<span class=\"gerrit-CopyableText-SWF\">");
+    final Element span = DOM.createSpan();
+    setStyleName(span, "gerrit-CopyableText-SWF");
     html.append("<object");
     html.append(" classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\"");
     html.append(" width=\"" + SWF_WIDTH + "\"");
@@ -60,9 +78,64 @@ public class CopyableText extends Widget {
     html.append("/>");
 
     html.append("</object>");
-    html.append("</span>");
 
-    DOM.setInnerHTML(getElement(), html.toString());
+    DOM.setInnerHTML(span, html.toString());
+    DOM.appendChild(content.getElement(), span);
+  }
+
+  public void onClick(final Widget source) {
+    if (textLabel == source) {
+      showTextBox();
+    }
+  }
+
+  private void showTextBox() {
+    if (textBox == null) {
+      textBox = new TextBox();
+      textBox.setText(textLabel.getText());
+      textBox.setVisibleLength(textBox.getText().length());
+      textBox.addKeyboardListener(new KeyboardListenerAdapter() {
+        @Override
+        public void onKeyPress(final Widget sender, final char kc, final int mod) {
+          if ((mod & MODIFIER_CTRL) == MODIFIER_CTRL
+              || (mod & MODIFIER_META) == MODIFIER_META) {
+            switch (kc) {
+              case 'c':
+              case 'x':
+                DeferredCommand.addCommand(new Command() {
+                  public void execute() {
+                    hideTextBox();
+                  }
+                });
+                break;
+            }
+          }
+        }
+      });
+      textBox.addFocusListener(new FocusListener() {
+        public void onFocus(Widget arg0) {
+        }
+
+        public void onLostFocus(Widget arg0) {
+          hideTextBox();
+        }
+      });
+      content.insert(textBox, 1);
+    }
+
+    textLabel.setVisible(false);
+    textBox.setVisible(true);
+    textBox.selectAll();
+    textBox.setFocus(true);
+  }
+
+  private void hideTextBox() {
+    if (textBox != null) {
+      textBox.setText(textLabel.getText());
+      textBox.removeFromParent();
+      textBox = null;
+    }
+    textLabel.setVisible(true);
   }
 
   private static void param(StringBuilder html, String name, String value) {

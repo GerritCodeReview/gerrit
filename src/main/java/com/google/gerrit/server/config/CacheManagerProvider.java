8696b5c0730152323eb1afdef5900d9e19e2013a
Shawn O. Pearce <sop@google.com>
2009-08-03 09:11:32 -0700

    Cleanup CacheManagerProvider's construction of the configuration
    This should make it easier (and more clear) to setup the cache.
    
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- src/main/java/com/google/gerrit/server/config/CacheManagerProvider.java
+++ src/main/java/com/google/gerrit/server/config/CacheManagerProvider.java
@@ -1,8 +1,8 @@
 package com.google.gerrit.server.config;
 
-import com.google.gerrit.client.reviewdb.LoginType;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.ProvisionException;
 
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.config.CacheConfiguration;
@@ -20,123 +20,132 @@ public class CacheManagerProvider implements Provider<CacheManager> {
   private static final Logger log =
       LoggerFactory.getLogger(CacheManagerProvider.class);
 
-  private final Config cfg;
+  private final Config config;
   private final File sitePath;
   private final AuthConfig authConfig;
 
   @Inject
   CacheManagerProvider(@GerritServerConfig final Config cfg,
       @SitePath final File sitePath, final AuthConfig authConfig) {
-    this.cfg = cfg;
+    this.config = cfg;
     this.sitePath = sitePath;
     this.authConfig = authConfig;
   }
 
   @Override
   public CacheManager get() {
-    return new CacheManager(toConfiguration(cfg, sitePath, authConfig));
+    return new CacheManager(new Factory().toConfiguration());
   }
 
-  private Configuration toConfiguration(final Config cfg, final File sitePath,
-      final AuthConfig authConfig) {
-    final Configuration mgrCfg = new Configuration();
-    configureDiskStore(cfg, sitePath, mgrCfg);
-    configureDefaultCache(cfg, mgrCfg);
+  private class Factory {
+    private static final int MB = 1024 * 1024;
+    private static final int ONE_DAY = 24 * 60;
+    private static final int D_MAXAGE = 3 * 30 * ONE_DAY;
+    private final Configuration mgr = new Configuration();
 
-    if (authConfig.getLoginType() == LoginType.OPENID) {
-      final CacheConfiguration c;
-      c = configureNamedCache(cfg, mgrCfg, "openid", false, 5);
-      c.setTimeToLiveSeconds(c.getTimeToIdleSeconds());
-      mgrCfg.addCache(c);
-    }
+    Configuration toConfiguration() {
+      configureDiskStore();
+      configureDefaultCache();
 
-    mgrCfg.addCache(configureNamedCache(cfg, mgrCfg, "diff", true, 0));
-    for (String n : new String[] {"sshkeys"}) {
-      mgrCfg.addCache(configureNamedCache(cfg, mgrCfg, n, false, 0));
-    }
-    return mgrCfg;
-  }
-
-  private void configureDiskStore(final Config cfg, final File sitePath,
-      final Configuration mgrCfg) {
-    String path = cfg.getString("cache", null, "directory");
-    if (path == null || path.length() == 0) {
-      path = "disk_cache";
-    }
-
-    final File loc = new File(sitePath, path);
-    if (loc.exists() || loc.mkdirs()) {
-      if (loc.canWrite()) {
-        final DiskStoreConfiguration c = new DiskStoreConfiguration();
-        c.setPath(loc.getAbsolutePath());
-        mgrCfg.addDiskStore(c);
-        log.info("Enabling disk cache " + loc.getAbsolutePath());
-      } else {
-        log.warn("Can't write to disk cache: " + loc.getAbsolutePath());
+      switch (authConfig.getLoginType()) {
+        case OPENID:
+          mgr.addCache(ttl(named("openid"), 5));
+          break;
       }
-    } else {
-      log.warn("Can't create disk cache: " + loc.getAbsolutePath());
-    }
-  }
 
-  private void configureDefaultCache(final Config i, final Configuration mgrCfg) {
-    final CacheConfiguration c = new CacheConfiguration();
+      mgr.addCache(disk(named("diff")));
+      mgr.addCache(named("sshkeys"));
 
-    c.setMaxElementsInMemory(i.getInt("cache", "memorylimit", 1024));
-    c.setMemoryStoreEvictionPolicyFromObject(MemoryStoreEvictionPolicy.LFU);
-
-    c.setTimeToLiveSeconds(0);
-    final int oneday = 24 * 60;
-    c.setTimeToIdleSeconds(i.getInt("cache", "maxage", 3 * 30 * oneday) * 60);
-    c.setEternal(c.getTimeToIdleSeconds() == 0);
-
-    if (mgrCfg.getDiskStoreConfiguration() != null) {
-      c.setMaxElementsOnDisk(i.getInt("cache", "disklimit", 16384));
-      c.setOverflowToDisk(false);
-      c.setDiskPersistent(false);
-
-      int diskbuffer = i.getInt("cache", "diskbuffer", 5 * 1024 * 1024);
-      diskbuffer /= 1024 * 1024;
-      c.setDiskSpoolBufferSizeMB(Math.max(1, diskbuffer));
-      c.setDiskExpiryThreadIntervalSeconds(60 * 60);
+      return mgr;
     }
 
-    mgrCfg.setDefaultCacheConfiguration(c);
-  }
+    private void configureDiskStore() {
+      String path = config.getString("cache", null, "directory");
+      if (path == null || path.length() == 0) {
+        path = "disk_cache";
+      }
 
-  private CacheConfiguration configureNamedCache(final Config i,
-      final Configuration mgrCfg, final String name, final boolean disk,
-      final int defaultAge) {
-    final CacheConfiguration def = mgrCfg.getDefaultCacheConfiguration();
-    final CacheConfiguration cfg;
-    try {
-      cfg = def.clone();
-    } catch (CloneNotSupportedException e) {
-      throw new RuntimeException("Cannot configure cache " + name, e);
-    }
-    cfg.setName(name);
-
-    cfg.setMaxElementsInMemory(i.getInt("cache", name, "memorylimit", def
-        .getMaxElementsInMemory()));
-
-    cfg.setTimeToIdleSeconds(i.getInt("cache", name, "maxage", defaultAge > 0
-        ? defaultAge : (int) (def.getTimeToIdleSeconds() / 60)) * 60);
-    cfg.setEternal(cfg.getTimeToIdleSeconds() == 0);
-
-    if (disk && mgrCfg.getDiskStoreConfiguration() != null) {
-      cfg.setMaxElementsOnDisk(i.getInt("cache", name, "disklimit", def
-          .getMaxElementsOnDisk()));
-
-      final int m = 1024 * 1024;
-      final int diskbuffer =
-          i.getInt("cache", name, "diskbuffer", def.getDiskSpoolBufferSizeMB()
-              * m)
-              / m;
-      cfg.setDiskSpoolBufferSizeMB(Math.max(1, diskbuffer));
-      cfg.setOverflowToDisk(true);
-      cfg.setDiskPersistent(true);
+      final File loc = new File(sitePath, path);
+      if (loc.exists() || loc.mkdirs()) {
+        if (loc.canWrite()) {
+          final DiskStoreConfiguration c = new DiskStoreConfiguration();
+          c.setPath(loc.getAbsolutePath());
+          mgr.addDiskStore(c);
+          log.info("Enabling disk cache " + loc.getAbsolutePath());
+        } else {
+          log.warn("Can't write to disk cache: " + loc.getAbsolutePath());
+        }
+      } else {
+        log.warn("Can't create disk cache: " + loc.getAbsolutePath());
+      }
     }
 
-    return cfg;
+    private void configureDefaultCache() {
+      final CacheConfiguration c = new CacheConfiguration();
+
+      c.setMaxElementsInMemory(config.getInt("cache", "memorylimit", 1024));
+      c.setMemoryStoreEvictionPolicyFromObject(MemoryStoreEvictionPolicy.LFU);
+
+      c.setTimeToIdleSeconds(config.getInt("cache", "maxage", D_MAXAGE) * 60);
+      c.setTimeToLiveSeconds(c.getTimeToIdleSeconds());
+      c.setEternal(c.getTimeToIdleSeconds() == 0);
+
+      if (mgr.getDiskStoreConfiguration() != null) {
+        c.setMaxElementsOnDisk(config.getInt("cache", "disklimit", 16384));
+        c.setOverflowToDisk(false);
+        c.setDiskPersistent(false);
+
+        final int diskbuffer = config.getInt("cache", "diskbuffer", 5 * MB);
+        c.setDiskSpoolBufferSizeMB(Math.max(1, diskbuffer / MB));
+        c.setDiskExpiryThreadIntervalSeconds(60 * 60);
+      }
+
+      mgr.setDefaultCacheConfiguration(c);
+    }
+
+    private CacheConfiguration named(final String name) {
+      final CacheConfiguration c = newCache(name);
+
+      int e = c.getMaxElementsInMemory();
+      c.setMaxElementsInMemory(config.getInt("cache", name, "memorylimit", e));
+
+      ttl(c, (int) c.getTimeToIdleSeconds() / 60);
+
+      return c;
+    }
+
+    private CacheConfiguration newCache(final String name) {
+      try {
+        final CacheConfiguration c;
+        c = mgr.getDefaultCacheConfiguration().clone();
+        c.setName(name);
+        return c;
+      } catch (CloneNotSupportedException e) {
+        throw new ProvisionException("Cannot configure cache " + name, e);
+      }
+    }
+
+    private CacheConfiguration ttl(final CacheConfiguration c, final int age) {
+      final String name = c.getName();
+      c.setTimeToIdleSeconds(config.getInt("cache", name, "maxage", age) * 60);
+      c.setTimeToLiveSeconds(c.getTimeToIdleSeconds());
+      c.setEternal(c.getTimeToIdleSeconds() == 0);
+      return c;
+    }
+
+    private CacheConfiguration disk(final CacheConfiguration c) {
+      final String name = c.getName();
+      if (mgr.getDiskStoreConfiguration() != null) {
+        int e = c.getMaxElementsOnDisk();
+        c.setMaxElementsOnDisk(config.getInt("cache", name, "disklimit", e));
+
+        int buffer = c.getDiskSpoolBufferSizeMB() * MB;
+        buffer = config.getInt("cache", name, "diskbuffer", buffer) / MB;
+        c.setDiskSpoolBufferSizeMB(Math.max(1, buffer));
+        c.setOverflowToDisk(true);
+        c.setDiskPersistent(true);
+      }
+      return c;
+    }
   }
 }

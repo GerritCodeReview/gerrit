ba39339066458f5c5be6ff3c38250dcef4cd5d59
Shawn O. Pearce <sop@google.com>
2009-08-26 10:13:37 -0700

    Fix bad change_key creation in upgrade016_017_mysql
    In MySQL || is the logical OR operator, unlike every other SQL
    database out there, so instead we need to rely upon the CONCAT
    function to perform a string concatenation.
    
    Bug: GERRIT-269
    Change-Id: Ib9b35b6928d3c4113052d6e6c7ff2c910f669b98
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- src/main/webapp/WEB-INF/sql/upgrade016_017_mysql.sql
+++ src/main/webapp/WEB-INF/sql/upgrade016_017_mysql.sql
@@ -7,10 +7,10 @@ ALTER TABLE system_config DROP xsrf_private_key;
 ALTER TABLE system_config CHANGE COLUMN account_private_key register_email_private_key VARCHAR(36) NOT NULL;
 ALTER TABLE changes ADD change_key VARCHAR(60);
 
-UPDATE changes SET change_key = 'I' || (SELECT p.revision
+UPDATE changes SET change_key = CONCAT('I', (SELECT p.revision
   FROM patch_sets p
   WHERE p.change_id = changes.change_id
-  AND p.patch_set_id = 1);
+  AND p.patch_set_id = 1));
 
 CREATE INDEX changes_key
 ON changes (change_key);

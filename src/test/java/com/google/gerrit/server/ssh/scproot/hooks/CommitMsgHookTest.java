8e713b527908f91c25f7965d2c36ae100fe6cfe9
Shawn O. Pearce <sop@google.com>
2009-09-05 12:39:34 -0700

    Add test for commit-msg with commit -v
    Just verify that we insert the Change-Id above the diff --git part,
    and not somehow below it.
    
    Change-Id: I986aac6980cbaaadf222993fa7029a59edfe370f
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- src/test/java/com/google/gerrit/server/ssh/scproot/hooks/CommitMsgHookTest.java
+++ src/test/java/com/google/gerrit/server/ssh/scproot/hooks/CommitMsgHookTest.java
@@ -284,6 +284,23 @@ public class CommitMsgHookTest extends HookTestCase {
             SOB1));
   }
 
+  public void testCommitDashV() throws Exception {
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c\n" + //
+        SOB1 + //
+        SOB2, //
+        call("a\n" + //
+            "\n" + //
+            SOB1 + //
+            SOB2 + //
+            "\n" + //
+            "# on branch master\n" + //
+            "diff --git a/src b/src\n" + //
+            "new file mode 100644\n" + //
+            "index 0000000..c78b7f0\n"));
+  }
+
   private void hookDoesNotModify(final String in) throws Exception {
     assertEquals(in, call(in));
   }

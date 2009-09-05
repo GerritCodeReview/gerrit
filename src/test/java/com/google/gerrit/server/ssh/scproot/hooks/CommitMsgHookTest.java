47598c936348eb65fe3776631ad5ab56abe3d5f2
Shawn O. Pearce <sop@google.com>
2009-09-05 10:59:41 -0700

    Add tests for Change-Id generating commit-msg hook
    This hook is really finicky, as it has a number of cases where it
    must perform correctly.  Define a test suite which enumerates most
    of the cases we know it needs to handle right.
    
    Change-Id: Ic9bd03587ff1e359e824830a9b7c524f2077b630
    Signed-off-by: Shawn O. Pearce <sop@google.com>

---
+++ src/test/java/com/google/gerrit/server/ssh/scproot/hooks/CommitMsgHookTest.java
@@ -0,0 +1,265 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.ssh.scproot.hooks;
+
+import org.spearce.jgit.dircache.DirCache;
+import org.spearce.jgit.dircache.DirCacheBuilder;
+import org.spearce.jgit.dircache.DirCacheEntry;
+import org.spearce.jgit.lib.Commit;
+import org.spearce.jgit.lib.Constants;
+import org.spearce.jgit.lib.FileMode;
+import org.spearce.jgit.lib.ObjectId;
+import org.spearce.jgit.lib.ObjectWriter;
+import org.spearce.jgit.lib.RefUpdate;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CommitMsgHookTest extends HookTestCase {
+  private final String SOB1 = "Signed-off-by: J Author <ja@example.com>\n";
+  private final String SOB2 = "Signed-off-by: J Committer <jc@example.com>\n";
+
+  public void testEmptyMessages() throws Exception {
+    // Empty input must yield empty output so commit will abort.
+    // Note we must consider different commit templates formats.
+    //
+    hookDoesNotModify("");
+    hookDoesNotModify(" ");
+    hookDoesNotModify("\n");
+    hookDoesNotModify("\n\n");
+    hookDoesNotModify("  \n  ");
+
+    hookDoesNotModify("#");
+    hookDoesNotModify("#\n");
+    hookDoesNotModify("# on branch master\n# Untracked files:\n");
+    hookDoesNotModify("\n# on branch master\n# Untracked files:\n");
+    hookDoesNotModify("\n\n# on branch master\n# Untracked files:\n");
+
+    hookDoesNotModify("\n# on branch master\ndiff --git a/src b/src\n"
+        + "new file mode 100644\nindex 0000000..c78b7f0\n");
+  }
+
+  public void testChangeIdAlreadySet() throws Exception {
+    // If a Change-Id is already present in the footer, the hook must
+    // not modify the message but instead must leave the identity alone.
+    //
+    hookDoesNotModify("a\n" + //
+        "\n" + //
+        "Change-Id: Iaeac9b4149291060228ef0154db2985a31111335\n");
+    hookDoesNotModify("fix: this thing\n" + //
+        "\n" + //
+        "Change-Id: I388bdaf52ed05b55e62a22d0a20d2c1ae0d33e7e\n");
+    hookDoesNotModify("fix-a-widget: this thing\n" + //
+        "\n" + //
+        "Change-Id: Id3bc5359d768a6400450283e12bdfb6cd135ea4b\n");
+    hookDoesNotModify("FIX: this thing\n" + //
+        "\n" + //
+        "Change-Id: I1b55098b5a2cce0b3f3da783dda50d5f79f873fa\n");
+    hookDoesNotModify("Fix-A-Widget: this thing\n" + //
+        "\n" + //
+        "Change-Id: I4f4e2e1e8568ddc1509baecb8c1270a1fb4b6da7\n");
+  }
+
+  public void testTimeAltersId() throws Exception {
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c\n",//
+        call("a\n"));
+
+    tick();
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I3251906b99dda598a58a6346d8126237ee1ea800\n",//
+        call("a\n"));
+
+    tick();
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I69adf9208d828f41a3d7e41afbca63aff37c0c5c\n",//
+        call("a\n"));
+  }
+
+  public void testFirstParentAltersId() throws Exception {
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c\n",//
+        call("a\n"));
+
+    setHEAD();
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I51e86482bde7f92028541aaf724d3a3f996e7ea2\n",//
+        call("a\n"));
+  }
+
+  public void testDirCacheAltersId() throws Exception {
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c\n",//
+        call("a\n"));
+
+    final DirCacheBuilder builder = DirCache.lock(repository).builder();
+    builder.add(file("A"));
+    assertTrue(builder.commit());
+
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: If56597ea9759f23b070677ea6f064c60c38da631\n",//
+        call("a\n"));
+  }
+
+  public void testSingleLineMessages() throws Exception {
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c\n",//
+        call("a\n"));
+
+    assertEquals("fix: this thing\n" + //
+        "\n" + //
+        "Change-Id: I0f13d0e6c739ca3ae399a05a93792e80feb97f37\n",//
+        call("fix: this thing\n"));
+    assertEquals("fix-a-widget: this thing\n" + //
+        "\n" + //
+        "Change-Id: I1a1a0c751e4273d532e4046a501a612b9b8a775e\n",//
+        call("fix-a-widget: this thing\n"));
+
+    assertEquals("FIX: this thing\n" + //
+        "\n" + //
+        "Change-Id: If816d944c57d3893b60cf10c65931fead1290d97\n",//
+        call("FIX: this thing\n"));
+    assertEquals("Fix-A-Widget: this thing\n" + //
+        "\n" + //
+        "Change-Id: I3e18d00cbda2ba1f73aeb63ed8c7d57d7fd16c76\n",//
+        call("Fix-A-Widget: this thing\n"));
+  }
+
+  public void testMultiLineMessagesWithoutFooter() throws Exception {
+    assertEquals("a\n" + //
+        "\n" + //
+        "b\n" + //
+        "\n" + //
+        "Change-Id: Id0b4f42d3d6fc1569595c9b97cb665e738486f5d\n",//
+        call("a\n" + "\n" + "b\n"));
+
+    assertEquals("a\n" + //
+        "\n" + //
+        "b\nc\nd\ne\n" + //
+        "\n" + //
+        "Change-Id: I7d237b20058a0f46cc3f5fabc4a0476877289d75\n",//
+        call("a\n" + "\n" + "b\nc\nd\ne\n"));
+
+    assertEquals("a\n" + //
+        "\n" + //
+        "b\nc\nd\ne\n" + //
+        "\n" + //
+        "f\ng\nh\n" + //
+        "\n" + //
+        "Change-Id: I382e662f47bf164d6878b7fe61637873ab7fa4e8\n",//
+        call("a\n" + "\n" + "b\nc\nd\ne\n" + "\n" + "f\ng\nh\n"));
+  }
+
+  public void testSingleLineMessagesWithSignedOffBy() throws Exception {
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c\n" + //
+        SOB1,//
+        call("a\n" + "\n" + SOB1));
+
+    assertEquals("a\n" + //
+        "\n" + //
+        "Change-Id: I7fc3876fee63c766a2063df97fbe04a2dddd8d7c\n" + //
+        SOB1 + //
+        SOB2,//
+        call("a\n" + "\n" + SOB1 + SOB2));
+  }
+
+  public void testMultiLineMessagesWithSignedOffBy() throws Exception {
+    assertEquals("a\n" + //
+        "\n" + //
+        "b\nc\nd\ne\n" + //
+        "\n" + //
+        "f\ng\nh\n" + //
+        "\n" + //
+        "Change-Id: I382e662f47bf164d6878b7fe61637873ab7fa4e8\n" + //
+        SOB1,//
+        call("a\n" + "\n" + "b\nc\nd\ne\n" + "\n" + "f\ng\nh\n" + "\n" + SOB1));
+
+    assertEquals("a\n" + //
+        "\n" + //
+        "b\nc\nd\ne\n" + //
+        "\n" + //
+        "f\ng\nh\n" + //
+        "\n" + //
+        "Change-Id: I382e662f47bf164d6878b7fe61637873ab7fa4e8\n" + //
+        SOB1 + //
+        SOB2,//
+        call("a\n" + //
+            "\n" + //
+            "b\nc\nd\ne\n" + //
+            "\n" + //
+            "f\ng\nh\n" + //
+            "\n" + //
+            SOB1 + //
+            SOB2));
+  }
+
+  private void hookDoesNotModify(final String in) throws Exception {
+    assertEquals(in, call(in));
+  }
+
+  private String call(final String body) throws Exception {
+    final File tmp = write(body);
+    try {
+      final File hook = getHook("commit-msg");
+      assertEquals(0, runHook(repository, hook, tmp.getAbsolutePath()));
+      return read(tmp);
+    } finally {
+      tmp.delete();
+    }
+  }
+
+  private DirCacheEntry file(final String name) throws IOException {
+    final DirCacheEntry e = new DirCacheEntry(name);
+    e.setFileMode(FileMode.REGULAR_FILE);
+    e.setObjectId(writer().writeBlob(Constants.encode(name)));
+    return e;
+  }
+
+  private void setHEAD() throws Exception {
+    final ObjectWriter ow = writer();
+    final Commit commit = new Commit(repository);
+    commit.setTreeId(DirCache.newInCore().writeTree(ow));
+    commit.setAuthor(author);
+    commit.setCommitter(committer);
+    commit.setMessage("test\n");
+    final ObjectId commitId = ow.writeCommit(commit);
+
+    final RefUpdate ref = repository.updateRef(Constants.HEAD);
+    ref.setNewObjectId(commitId);
+    switch (ref.forceUpdate()) {
+      case NEW:
+      case FAST_FORWARD:
+      case FORCED:
+      case NO_CHANGE:
+        break;
+      default:
+        fail(Constants.HEAD + " did not change: " + ref.getResult());
+    }
+  }
+
+  private ObjectWriter writer() {
+    return new ObjectWriter(repository);
+  }
+}
---
+++ src/test/java/com/google/gerrit/testutil/LocalDiskRepositoryTestCase.java
@@ -0,0 +1,264 @@
+// Copyright (C) 2009, The Android Open Source Project
+// Copyright (C) 2008, Robin Rosenberg <robin.rosenberg@dewire.com>
+// Copyright (C) 2007, Shawn O. Pearce <spearce@spearce.org>
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// - Neither the name of the Git Development Community nor the names of its
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Taken almost literally from JGit's test suite, as it does almost everything
+// we need to create a local git repository for testing.
+
+package com.google.gerrit.testutil;
+
+import junit.framework.TestCase;
+
+import org.spearce.jgit.lib.FileBasedConfig;
+import org.spearce.jgit.lib.PersonIdent;
+import org.spearce.jgit.lib.Repository;
+import org.spearce.jgit.lib.RepositoryCache;
+import org.spearce.jgit.util.NB;
+import org.spearce.jgit.util.SystemReader;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public abstract class LocalDiskRepositoryTestCase extends TestCase {
+  private static Thread shutdownHook;
+  private static int testCount;
+
+  protected PersonIdent author;
+  protected PersonIdent committer;
+
+  private final File trash = new File(new File("target"), "trash");
+  private final List<Repository> toClose = new ArrayList<Repository>();
+  private MockSystemReader mockSystemReader;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    if (shutdownHook == null) {
+      shutdownHook = new Thread() {
+        @Override
+        public void run() {
+          System.gc();
+          recursiveDelete("SHUTDOWN", trash, false, false);
+        }
+      };
+      Runtime.getRuntime().addShutdownHook(shutdownHook);
+    }
+
+    recursiveDelete(testName(), trash, true, false);
+
+    mockSystemReader = new MockSystemReader();
+    mockSystemReader.userGitConfig =
+        new FileBasedConfig(new File(trash, "usergitconfig"));
+    SystemReader.setInstance(mockSystemReader);
+
+    final long now = mockSystemReader.getCurrentTime();
+    final int tz = mockSystemReader.getTimezone(now);
+    author = new PersonIdent("J. Author", "ja@example.com");
+    author = new PersonIdent(author, now, tz);
+
+    committer = new PersonIdent("J. Committer", "jc@example.com");
+    committer = new PersonIdent(committer, now, tz);
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    RepositoryCache.clear();
+    for (Repository r : toClose)
+      r.close();
+    toClose.clear();
+
+    recursiveDelete(testName(), trash, false, true);
+    super.tearDown();
+  }
+
+  /** Increment the {@link #author} and {@link #committer} times. */
+  protected void tick() {
+    final long delta = TimeUnit.MILLISECONDS.convert(5, TimeUnit.MINUTES);
+    final long now = author.getWhen().getTime() + delta;
+    final int tz = mockSystemReader.getTimezone(now);
+
+    author = new PersonIdent(author, now, tz);
+    committer = new PersonIdent(committer, now, tz);
+  }
+
+  /** Recursively delete a directory, failing the test if the delete fails. */
+  protected void recursiveDelete(final File dir) {
+    recursiveDelete(testName(), dir, false, true);
+  }
+
+  private static boolean recursiveDelete(final String testName, final File dir,
+      boolean silent, boolean failOnError) {
+    assert !(silent && failOnError);
+    if (!dir.exists()) {
+      return silent;
+    }
+    final File[] ls = dir.listFiles();
+    if (ls != null) {
+      for (int k = 0; k < ls.length; k++) {
+        final File e = ls[k];
+        if (e.isDirectory()) {
+          silent = recursiveDelete(testName, e, silent, failOnError);
+        } else {
+          if (!e.delete()) {
+            if (!silent) {
+              reportDeleteFailure(testName, failOnError, e);
+            }
+            silent = !failOnError;
+          }
+        }
+      }
+    }
+    if (!dir.delete()) {
+      if (!silent) {
+        reportDeleteFailure(testName, failOnError, dir);
+      }
+      silent = !failOnError;
+    }
+    return silent;
+  }
+
+  private static void reportDeleteFailure(final String testName,
+      final boolean failOnError, final File e) {
+    final String severity;
+    if (failOnError)
+      severity = "ERROR";
+    else
+      severity = "WARNING";
+
+    final String msg = severity + ": Failed to delete " + e + " in " + testName;
+    if (failOnError)
+      fail(msg);
+    else
+      System.err.println(msg);
+  }
+
+  /** Creates a new empty repository. */
+  protected Repository createRepository(boolean bare) throws IOException {
+    final String uniqueId = System.currentTimeMillis() + "_" + (testCount++);
+    final String gitdirName = "test" + uniqueId + (bare ? "" : "/") + ".git";
+    final File gitdir = new File(trash, gitdirName).getCanonicalFile();
+    final Repository db = new Repository(gitdir);
+
+    assertFalse(gitdir.exists());
+    db.create();
+    toClose.add(db);
+    return db;
+  }
+
+  /** Run a hook script in the repository, returning the exit status. */
+  protected int runHook(final Repository db, final File hook,
+      final String... args) throws IOException, InterruptedException {
+    final String[] argv = new String[1 + args.length];
+    argv[0] = hook.getAbsolutePath();
+    System.arraycopy(args, 0, argv, 1, args.length);
+
+    final Map<String, String> env = cloneEnv();
+    env.put("GIT_DIR", db.getDirectory().getAbsolutePath());
+    putPersonIdent(env, "AUTHOR", author);
+    putPersonIdent(env, "COMMITTER", committer);
+
+    final File cwd = db.getWorkDir();
+    final Process p = Runtime.getRuntime().exec(argv, toEnvArray(env), cwd);
+    p.getOutputStream().close();
+    p.getErrorStream().close();
+    p.getInputStream().close();
+    return p.waitFor();
+  }
+
+  private static void putPersonIdent(final Map<String, String> env,
+      final String type, final PersonIdent who) {
+    final String ident = who.toExternalString();
+    final String date = ident.substring(ident.indexOf("> ") + 2);
+    env.put("GIT_" + type + "_NAME", who.getName());
+    env.put("GIT_" + type + "_EMAIL", who.getEmailAddress());
+    env.put("GIT_" + type + "_DATE", date);
+  }
+
+  /** Create a string to a UTF-8 temporary file and return the path. */
+  protected File write(final String body) throws IOException {
+    final File f = File.createTempFile("temp", "txt", trash);
+    try {
+      write(f, body);
+      return f;
+    } catch (Error e) {
+      f.delete();
+      throw e;
+    } catch (RuntimeException e) {
+      f.delete();
+      throw e;
+    } catch (IOException e) {
+      f.delete();
+      throw e;
+    }
+  }
+
+  /** Write a string as a UTF-8 file. */
+  protected void write(final File f, final String body) throws IOException {
+    final Writer w = new OutputStreamWriter(new FileOutputStream(f), "UTF-8");
+    try {
+      w.write(body);
+    } finally {
+      w.close();
+    }
+  }
+
+  /** Fully read a UTF-8 file and return as a string. */
+  protected String read(final File f) throws IOException {
+    final byte[] body = NB.readFully(f);
+    return new String(body, 0, body.length, "UTF-8");
+  }
+
+  private static String[] toEnvArray(final Map<String, String> env) {
+    final String[] envp = new String[env.size()];
+    int i = 0;
+    for (Map.Entry<String, String> e : env.entrySet()) {
+      envp[i++] = e.getKey() + "=" + e.getValue();
+    }
+    return envp;
+  }
+
+  private static HashMap<String, String> cloneEnv() {
+    return new HashMap<String, String>(System.getenv());
+  }
+
+  private String testName() {
+    return getClass().getName() + "." + getName();
+  }
+}

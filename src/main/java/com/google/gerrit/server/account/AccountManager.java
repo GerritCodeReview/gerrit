GerritCodeReview/gerrit

commit 818fa7568021f41863df0dbcb6db68d8692404b0

Abstract out account creation and simplify sign-on for users
We now do all account management in the AccountManager class, so we
don't have code duplication between HTTP and OpenID authentication.
This also cleans up GerritCall, moving the account creation logic
out of a per-request state object.  It just doesn't belong there.

In HTTP authentication mode we also correctly store the state of
the "#change,42" style anchors during web authentication by using
a JavaScript based redirect from an unauthenticated space into the
authenticated space, do the authentication, then redirect back to
the token.  This allows users who have not yet logged into the app
to still click on anchor links received by email, and visit the URL.

Signed-off-by: Shawn O. Pearce <sop@google.com>

src/main/java/com/google/gerrit/server/account/AccountManager.java
@@ -1,0 +1,265 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import com.google.gerrit.client.openid.OpenIdUtil;
+import com.google.gerrit.client.reviewdb.Account;
+import com.google.gerrit.client.reviewdb.AccountExternalId;
+import com.google.gerrit.client.reviewdb.ReviewDb;
+import com.google.gerrit.server.config.AuthConfig;
+import com.google.gwtorm.client.OrmException;
+import com.google.gwtorm.client.SchemaFactory;
+import com.google.gwtorm.client.Transaction;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import java.util.Collections;
+import java.util.List;
+
+/** Tracks authentication related details for user accounts. */
+@Singleton
+public class AccountManager {
+  private final SchemaFactory<ReviewDb> schema;
+  private final AccountCache byIdCache;
+  private final AccountByEmailCache byEmailCache;
+  private final EmailExpander emailExpander;
+  private final AuthConfig authConfig;
+
+  @Inject
+  AccountManager(final SchemaFactory<ReviewDb> schema,
+      final AccountCache byIdCache, final AccountByEmailCache byEmailCache,
+      final EmailExpander emailExpander, final AuthConfig authConfig) {
+    this.schema = schema;
+    this.byIdCache = byIdCache;
+    this.byEmailCache = byEmailCache;
+    this.emailExpander = emailExpander;
+    this.authConfig = authConfig;
+  }
+
+  /**
+   * True if user identified by this external identity string has an account.
+   */
+  public boolean exists(final String externalId) throws AccountException {
+    try {
+      final ReviewDb db = schema.open();
+      try {
+        final List<AccountExternalId> matches =
+            db.accountExternalIds().byExternal(externalId).toList();
+        return !matches.isEmpty();
+      } finally {
+        db.close();
+      }
+    } catch (OrmException e) {
+      throw new AccountException("Cannot lookup account " + externalId, e);
+    }
+  }
+
+  /**
+   * Authenticate the user, potentially creating a new account if they are new.
+   *
+   * @param who identity of the user, with any details we received about them.
+   * @return the result of authenticating the user.
+   * @throws AccountException the account does not exist, and cannot be created,
+   *         or exists, but cannot be located.
+   */
+  public AuthResult authenticate(final AuthRequest who) throws AccountException {
+    try {
+      final ReviewDb db = schema.open();
+      try {
+        final List<AccountExternalId> matches =
+            db.accountExternalIds().byExternal(who.getExternalId()).toList();
+        switch (matches.size()) {
+          case 0:
+            // New account, automatically create and return.
+            //
+            return create(db, who);
+
+          case 1: {
+            // Account exists, return the identity to the caller.
+            //
+            final AccountExternalId id = matches.get(0);
+            update(db, who, id);
+            return new AuthResult(id.getAccountId(), false);
+          }
+
+          default: {
+            final StringBuilder r = new StringBuilder();
+            r.append("Multiple accounts match \"");
+            r.append(who.getExternalId());
+            r.append("\":");
+            for (AccountExternalId e : matches) {
+              r.append(' ');
+              r.append(e.getAccountId());
+            }
+            throw new AccountException(r.toString());
+          }
+        }
+
+      } finally {
+        db.close();
+      }
+    } catch (OrmException e) {
+      throw new AccountException("Authentication error", e);
+    }
+  }
+
+  private void update(final ReviewDb db, final AuthRequest who,
+      final AccountExternalId extId) throws OrmException, AccountException {
+    final Transaction txn = db.beginTransaction();
+    final Account account = db.accounts().get(extId.getAccountId());
+    if (account == null) {
+      throw new AccountException("Account has been deleted");
+    }
+
+    // If the email address was modified by the authentication provider,
+    // update our records to match the changed email.
+    //
+    final String newEmail = who.getEmailAddress();
+    final String oldEmail = extId.getEmailAddress();
+    if (newEmail != null && !newEmail.equals(oldEmail)) {
+      if (oldEmail != null && oldEmail.equals(account.getPreferredEmail())) {
+        account.setPreferredEmail(newEmail);
+        db.accounts().update(Collections.singleton(account), txn);
+      }
+
+      extId.setEmailAddress(newEmail);
+    }
+
+    extId.setLastUsedOn();
+    db.accountExternalIds().update(Collections.singleton(extId), txn);
+    txn.commit();
+
+    if (newEmail != null && !newEmail.equals(oldEmail)) {
+      byEmailCache.evict(oldEmail);
+      byEmailCache.evict(newEmail);
+    }
+  }
+
+  private AuthResult create(final ReviewDb db, final AuthRequest who)
+      throws OrmException, AccountException {
+    if (authConfig.isAllowGoogleAccountUpgrade()
+        && who.getExternalId().startsWith(OpenIdUtil.URL_GOOGLE + "?")
+        && who.getEmailAddress() != null) {
+      final List<AccountExternalId> legacyAppEngine =
+          db.accountExternalIds().byExternal(
+              AccountExternalId.LEGACY_GAE + who.getEmailAddress()).toList();
+
+      if (legacyAppEngine.size() == 1) {
+        // Exactly one user was imported from Gerrit 1.x with this email
+        // address. Upgrade their account by deleting the legacy import
+        // identity and creating a new identity matching the token we have.
+        //
+        final AccountExternalId oldId = legacyAppEngine.get(0);
+        final AccountExternalId newId = createId(oldId.getAccountId(), who);
+        newId.setEmailAddress(who.getEmailAddress());
+        newId.setLastUsedOn();
+        final Transaction txn = db.beginTransaction();
+        db.accountExternalIds().delete(Collections.singleton(oldId), txn);
+        db.accountExternalIds().insert(Collections.singleton(newId), txn);
+        txn.commit();
+        return new AuthResult(newId.getAccountId(), false);
+
+      } else if (legacyAppEngine.size() > 1) {
+        throw new AccountException("Multiple Gerrit 1.x accounts found");
+      }
+    }
+
+    final Account.Id newId = new Account.Id(db.nextAccountId());
+    final Account account = new Account(newId);
+    final AccountExternalId extId = createId(newId, who);
+
+    if (who.getLocalUser() != null && who.getEmailAddress() == null) {
+      // A SCHEMA_GERRIT account was authenticated by an external SSO
+      // solution. The external identity string actually contains a
+      // name that we can uniquely refer to the user by, so set
+      // account information based upon that name.
+      //
+      final String user = who.getLocalUser();
+      if (emailExpander.canExpand(user)) {
+        extId.setEmailAddress(emailExpander.expand(user));
+      }
+      account.setSshUserName(user);
+    } else if (who.getEmailAddress() != null) {
+      extId.setEmailAddress(who.getEmailAddress());
+    }
+
+    extId.setLastUsedOn();
+    account.setFullName(who.getDisplayName());
+    account.setPreferredEmail(extId.getEmailAddress());
+
+    final Transaction txn = db.beginTransaction();
+    db.accounts().insert(Collections.singleton(account), txn);
+    db.accountExternalIds().insert(Collections.singleton(extId), txn);
+    txn.commit();
+
+    byEmailCache.evict(account.getPreferredEmail());
+    return new AuthResult(newId, true);
+  }
+
+  private static AccountExternalId createId(final Account.Id newId,
+      final AuthRequest who) {
+    final String ext = who.getExternalId();
+    return new AccountExternalId(new AccountExternalId.Key(newId, ext));
+  }
+
+  /**
+   * Link another authentication identity to an existing account.
+   *
+   * @param to account to link the identity onto.
+   * @param who the additional identity.
+   * @throws AccountException the identity belongs to a different account, or it
+   *         cannot be linked at this time.
+   */
+  public void link(final Account.Id to, final AuthRequest who)
+      throws AccountException {
+    try {
+      final ReviewDb db = schema.open();
+      try {
+        final List<AccountExternalId> matches =
+            db.accountExternalIds().byExternal(who.getExternalId()).toList();
+        switch (matches.size()) {
+          case 0: {
+            final AccountExternalId extId = createId(to, who);
+            extId.setEmailAddress(who.getEmailAddress());
+            extId.setLastUsedOn();
+            db.accountExternalIds().insert(Collections.singleton(extId));
+            if (who.getEmailAddress() != null) {
+              byEmailCache.evict(who.getEmailAddress());
+              byIdCache.evict(to);
+            }
+            break;
+          }
+
+          case 1: {
+            final AccountExternalId extId = matches.get(0);
+            if (!extId.getAccountId().equals(to)) {
+              throw new AccountException("Identity already used");
+            }
+            update(db, who, extId);
+            break;
+          }
+
+          default:
+            throw new AccountException("Identity already used");
+        }
+
+      } finally {
+        db.close();
+      }
+    } catch (OrmException e) {
+      throw new AccountException("Cannot link identity", e);
+    }
+  }
+}

src/main/java/com/google/gerrit/server/account/AccountException.java
@@ -1,0 +1,26 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+/** An account processing error thrown by {@link AccountManager}. */
+public class AccountException extends Exception {
+  public AccountException(final String message) {
+    super(message);
+  }
+
+  public AccountException(final String message, final Throwable why) {
+    super(message, why);
+  }
+}

src/main/java/com/google/gerrit/server/account/AuthRequest.java
@@ -1,0 +1,82 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import static com.google.gerrit.client.reviewdb.AccountExternalId.SCHEME_GERRIT;
+import static com.google.gerrit.client.reviewdb.AccountExternalId.SCHEME_MAILTO;
+
+/**
+ * Information for {@link AccountManager#authenticate(AuthRequest)}.
+ * <p>
+ * Callers should populate this object with as much information as possible
+ * about the user account. For example, OpenID authentication might return
+ * registration information including a display name for the user, and an email
+ * address for them. These fields however are optional, as not all OpenID
+ * providers return them, and not all non-OpenID systems can use them.
+ */
+public class AuthRequest {
+  /** Create a request for a local username, such as from LDAP. */
+  public static AuthRequest forUser(final String username) {
+    return new AuthRequest(SCHEME_GERRIT + username);
+  }
+
+  /**
+   * Create a request for an email address registration.
+   * <p>
+   * This type of request should be used only to attach a new email address to
+   * an existing user account.
+   */
+  public static AuthRequest forEmail(final String email) {
+    final AuthRequest r;
+    r = new AuthRequest(SCHEME_MAILTO + email);
+    r.setEmailAddress(email);
+    return r;
+  }
+
+  private final String externalId;
+  private String displayName;
+  private String emailAddress;
+
+  public AuthRequest(final String externalId) {
+    this.externalId = externalId;
+  }
+
+  public String getExternalId() {
+    return externalId;
+  }
+
+  public String getLocalUser() {
+    if (getExternalId().startsWith(SCHEME_GERRIT)) {
+      return getExternalId().substring(SCHEME_GERRIT.length());
+    }
+    return null;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(final String name) {
+    displayName = name != null && name.length() > 0 ? name : null;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public void setEmailAddress(final String email) {
+    emailAddress = email != null && email.length() > 0 ? email : null;
+  }
+}

src/main/java/com/google/gerrit/server/account/AuthResult.java
@@ -1,0 +1,43 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import com.google.gerrit.client.reviewdb.Account;
+
+/** Result from {@link AccountManager#authenticate(AuthRequest)}. */
+public class AuthResult {
+  private final Account.Id accountId;
+  private final boolean isNew;
+
+  AuthResult(final Account.Id accountId, final boolean isNew) {
+    this.accountId = accountId;
+    this.isNew = isNew;
+  }
+
+  /** Identity of the user account that was authenticated into. */
+  public Account.Id getAccountId() {
+    return accountId;
+  }
+
+  /**
+   * True if this account was recently created for the user.
+   * <p>
+   * New users should be redirected to the registration screen, so they can
+   * configure their new user account.
+   */
+  public boolean isNew() {
+    return isNew;
+  }
+}

src/main/java/com/google/gerrit/server/config/CanonicalWebUrlProvider.java
@@ -67,22 +67,12 @@ public class CanonicalWebUrlProvider implements Provider<String> {
         }
       }
 
-      // Assume this servlet is in the context with a simple name like "login"
-      // and we were accessed without any path info. Clipping the last part of
-      // the name from the URL should generate the web application's root path.
-      //
-      String uri = req.getRequestURL().toString();
-      final int s = uri.lastIndexOf('/');
-      if (s >= 0) {
-        uri = uri.substring(0, s + 1);
+      final StringBuffer url = req.getRequestURL();
+      url.setLength(url.length() - req.getServletPath().length());
+      if (url.charAt(url.length() - 1) != '/') {
+        url.append('/');
       }
-      final String sfx = "/gerrit/rpc/";
-      if (uri.endsWith(sfx)) {
-        // Nope, it was one of our RPC servlets. Drop the rpc too.
-        //
-        uri = uri.substring(0, uri.length() - (sfx.length() - 1));
-      }
-      return uri;
+      return url.toString();
     }
 
     // We have no way of guessing our HTTP url.

commit dbc53b701924844ea0f4f1ba49d3d41b01da1083

Fix OpenID delegate authentication
OpenID delegate support was accidentally broken back when we switched
to openid4java.  Issue 38 claims it was broken in commit 818fa7568021
(v2.0.19~85) when we did a refactoring on how accounts are handled,
but I can't seem to fault that commit with the actual problem of
not honoring the claimed identity supplied by the user.

We now store both the claimed identity and the delegate identity into
the database.  This makes it easier to link back to the same account
if the user enters through one or the other.  It also permits the
us to only grant extended access to a user account if both their
claimed and delegate provider are trusted by the site administrator.

If an account has only the delegate identity (e.g. because it
was made before this fix was put into production use) we add the
claimed identity as an additional identity during the next login.
If an account has the claimed identity but not the delegate, we
add the delegate on the next login under the assumption that the
user has updated their delegation rule stored at the claimed address.

Bug: issue 38
Change-Id: Ie1e1265c94e5cafc75e8d71c9a1cbfa4df4337b7
Signed-off-by: Shawn O. Pearce <sop@google.com>

src/main/java/com/google/gerrit/server/account/AccountManager.java
@@ -50,14 +50,15 @@ public class AccountManager {
   }
 
   /**
-   * True if user identified by this external identity string has an account.
+   * @return user identified by this external identity string, or null.
    */
-  public boolean exists(final String externalId) throws AccountException {
+  public Account.Id lookup(final String externalId) throws AccountException {
     try {
       final ReviewDb db = schema.open();
       try {
-        return db.accountExternalIds().get(
-            new AccountExternalId.Key(externalId)) != null;
+        final AccountExternalId ext =
+            db.accountExternalIds().get(new AccountExternalId.Key(externalId));
+        return ext != null ? ext.getAccountId() : null;
       } finally {
         db.close();
       }

src/main/java/com/google/gerrit/server/openid/OpenIdServiceImpl.java
@@ -20,6 +20,7 @@ import com.google.gerrit.client.SignInDialog.Mode;
 import com.google.gerrit.client.auth.openid.DiscoveryResult;
 import com.google.gerrit.client.auth.openid.OpenIdService;
 import com.google.gerrit.client.auth.openid.OpenIdUtil;
+import com.google.gerrit.client.reviewdb.Account;
 import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.UrlEncoded;
 import com.google.gerrit.server.account.AccountException;
@@ -73,6 +74,7 @@ class OpenIdServiceImpl implements OpenIdService {
   private static final String P_MODE = "gerrit.mode";
   private static final String P_TOKEN = "gerrit.token";
   private static final String P_REMEMBER = "gerrit.remember";
+  private static final String P_CLAIMED = "gerrit.claimed";
   private static final int LASTID_AGE = 365 * 24 * 60 * 60; // seconds
 
   private static final String OPENID_MODE = "openid.mode";
@@ -170,7 +172,7 @@ class OpenIdServiceImpl implements OpenIdService {
     // We might already have this account on file. Look for it.
     //
     try {
-      return !accountManager.exists(aReq.getIdentity());
+      return accountManager.lookup(aReq.getIdentity()) == null;
     } catch (AccountException e) {
       log.warn("Cannot determine if user account exists", e);
       return true;
@@ -189,11 +191,14 @@ class OpenIdServiceImpl implements OpenIdService {
     //
     final SignInDialog.Mode mode = signInMode(req);
     final String openidIdentifier = req.getParameter("openid.identity");
+    final String claimedIdentifier = req.getParameter(P_CLAIMED);
     final String returnToken = req.getParameter(P_TOKEN);
     final boolean remember = "1".equals(req.getParameter(P_REMEMBER));
+    final String rediscoverIdentifier =
+        claimedIdentifier != null ? claimedIdentifier : openidIdentifier;
     final State state;
 
-    state = init(openidIdentifier, mode, remember, returnToken);
+    state = init(rediscoverIdentifier, mode, remember, returnToken);
     if (state == null) {
       // Re-discovery must have failed, we can't run a login.
       //
@@ -214,8 +219,7 @@ class OpenIdServiceImpl implements OpenIdService {
     final VerificationResult result =
         manager.verify(state.retTo.toString(), new ParameterList(req
             .getParameterMap()), state.discovered);
-    final Identifier user = result.getVerifiedId();
-    if (user == null /* authentication failure */) {
+    if (result.getVerifiedId() == null /* authentication failure */) {
       if ("Nonce verification failed.".equals(result.getStatusMsg())) {
         // We might be suffering from clock skew on this system.
         //
@@ -258,7 +262,7 @@ class OpenIdServiceImpl implements OpenIdService {
     }
 
     final com.google.gerrit.server.account.AuthRequest areq =
-        new com.google.gerrit.server.account.AuthRequest(user.getIdentifier());
+        new com.google.gerrit.server.account.AuthRequest(openidIdentifier);
 
     if (sregRsp != null) {
       areq.setDisplayName(sregRsp.getAttributeValue("fullname"));
@@ -281,6 +285,51 @@ class OpenIdServiceImpl implements OpenIdService {
       areq.setEmailAddress(fetchRsp.getAttributeValue("Email"));
     }
 
+    if (claimedIdentifier != null) {
+      // The user used a claimed identity which has delegated to the verified
+      // identity we have in our AuthRequest above. We still should have a
+      // link between the two, so set one up if not present.
+      //
+      Account.Id claimedId = accountManager.lookup(claimedIdentifier);
+      Account.Id actualId = accountManager.lookup(areq.getExternalId());
+
+      if (claimedId != null && actualId != null) {
+        if (claimedId.equals(actualId)) {
+          // Both link to the same account, that's what we expected.
+        } else {
+          // This is (for now) a fatal error. There are two records
+          // for what might be the same user.
+          //
+          log.error("OpenID accounts disagree over user identity:\n"
+              + "  Claimed ID: " + claimedId + " is " + claimedIdentifier
+              + "\n" + "  Delgate ID: " + actualId + " is "
+              + areq.getExternalId());
+          cancelWithError(req, rsp, "Contact site administrator");
+          return;
+        }
+
+      } else if (claimedId == null && actualId != null) {
+        // Older account, the actual was already created but the claimed
+        // was missing due to a bug in Gerrit. Link the claimed.
+        //
+        final com.google.gerrit.server.account.AuthRequest linkReq =
+            new com.google.gerrit.server.account.AuthRequest(claimedIdentifier);
+        linkReq.setDisplayName(areq.getDisplayName());
+        linkReq.setEmailAddress(areq.getEmailAddress());
+        accountManager.link(actualId, linkReq);
+
+      } else if (claimedId != null && actualId == null) {
+        // Claimed account already exists, but it smells like the user has
+        // changed their delegate to point to a different provider. Link
+        // the new provider.
+        //
+        accountManager.link(claimedId, areq);
+
+      } else {
+        // Both are null, we are going to create a new account below.
+      }
+    }
+
     try {
       switch (mode) {
         case REGISTER:
@@ -291,13 +340,21 @@ class OpenIdServiceImpl implements OpenIdService {
           final Cookie lastId = new Cookie(OpenIdUtil.LASTID_COOKIE, "");
           lastId.setPath(req.getContextPath() + "/");
           if (remember) {
-            lastId.setValue(user.getIdentifier());
+            lastId.setValue(rediscoverIdentifier);
             lastId.setMaxAge(LASTID_AGE);
           } else {
             lastId.setMaxAge(0);
           }
           rsp.addCookie(lastId);
           webSession.get().login(arsp.getAccountId(), remember);
+          if (arsp.isNew() && claimedIdentifier != null) {
+            final com.google.gerrit.server.account.AuthRequest linkReq =
+                new com.google.gerrit.server.account.AuthRequest(
+                    claimedIdentifier);
+            linkReq.setDisplayName(areq.getDisplayName());
+            linkReq.setEmailAddress(areq.getEmailAddress());
+            accountManager.link(arsp.getAccountId(), linkReq);
+          }
           callback(arsp.isNew(), req, rsp);
           break;
 
@@ -391,6 +448,9 @@ class OpenIdServiceImpl implements OpenIdService {
     }
     if (remember) {
       retTo.put(P_REMEMBER, "1");
+    }
+    if (discovered.hasClaimedIdentifier()) {
+      retTo.put(P_CLAIMED, discovered.getClaimedIdentifier().getIdentifier());
     }
     return new State(discovered, retTo, contextUrl);
   }
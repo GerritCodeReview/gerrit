238b981fbf3e582ab44f9cab0b924e4e75453c5e
Nico Sallembien <nsallembien@google.com>
2009-12-16 15:48:07 -0800

    Push the encryption in a different thread.
    This will allow us to spread the encryption and jgit processes onto
    different CPUs. This might prove helpful in scaling Gerrit to more
    users.

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/Upload.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/Upload.java
@@ -17,12 +17,47 @@ package com.google.gerrit.sshd.commands;
 import org.eclipse.jgit.transport.UploadPack;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 
 /** Publishes Git repositories over SSH using the Git upload-pack protocol. */
 final class Upload extends AbstractGitCommand {
+
+  /**
+   * Copies the data from JGit into MINA's output stream. This is done
+   * in a separate thread in an attempt to offload the MINA encrytion code
+   * and the JGit code on separate CPUs.
+   */
+  private final class CopyRunnable implements CommandRunnable {
+     private final InputStream input;
+     private final OutputStream output;
+
+     CopyRunnable(InputStream input, OutputStream output) {
+       this.input = input;
+       this.output = output;
+     }
+
+     @Override
+     public void run() throws Exception {
+       byte[] buff = new byte[32768];
+       int read = input.read(buff);
+       while (read > 0) {
+         output.write(buff, 0, read);
+         output.flush();
+         read = input.read(buff);
+       }
+     }
+  }
+
   @Override
   protected void runImpl() throws IOException {
     final UploadPack up = new UploadPack(repo);
-    up.upload(in, out, err);
+    PipedOutputStream source = new PipedOutputStream();
+    PipedInputStream sink = new PipedInputStream(source, 128 * 1024 * 1024);
+    CopyRunnable copy = new CopyRunnable(sink, out);
+    startThread(copy);
+    up.upload(in, source, err);
   }
 }

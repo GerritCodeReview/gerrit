23e0987350e921e9e8fb55e178a11e13769311e6
Shawn O. Pearce <sop@google.com>
2009-12-24 16:25:59 -0800

    Remove unnecessary /login/* URLs when auth.type = LDAP
    Under auth.type LDAP we are doing username/password authentication
    via an RPC.  There is no need for the browser to redirect through
    our /login/ subdirectory in order to perform the login process.
    
    Change-Id: I13a61229ccd9c43d62309d357c2fdb8df4b95c10
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/auth/ldap/LdapAuthModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/auth/ldap/LdapAuthModule.java
@@ -22,7 +22,6 @@ import com.google.inject.servlet.ServletModule;
 public class LdapAuthModule extends ServletModule {
   @Override
   protected void configureServlets() {
-    serve("/login/*").with(LoginRedirectServlet.class);
     install(new RpcServletModule(UiRpcModule.PREFIX) {
       @Override
       protected void configureServlets() {

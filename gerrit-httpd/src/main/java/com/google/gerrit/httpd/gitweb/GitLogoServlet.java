b2a249cabd7225c6d9631eef91d884036477c7cb
Shawn O. Pearce <sop@google.com>
2010-04-24 16:03:11 -0700

    Correct gitweb servlet copyright notices
    These files didn't originate from the Jetty server, we wrote
    them here from scratch.  I simply made a mistake and copied
    the file header template from the CGI servlet rather than a
    different Gerrit source file, and accidentally dragged in the
    notice message from Mort Bay Consulting.
    
    Change-Id: I8dc926d9dd7b43e36dfd6574503820ec449e3f8a
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/gitweb/GitLogoServlet.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/gitweb/GitLogoServlet.java
@@ -12,21 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// CGI environment and execution management portions are:
-//
-// ========================================================================
-// Copyright (c) 2006-2009 Mort Bay Consulting Pty. Ltd.
-// ------------------------------------------------------------------------
-// All rights reserved. This program and the accompanying materials
-// are made available under the terms of the Eclipse Public License v1.0
-// and Apache License v2.0 which accompanies this distribution.
-// The Eclipse Public License is available at
-// http://www.eclipse.org/legal/epl-v10.html
-// The Apache License v2.0 is available at
-// http://www.opensource.org/licenses/apache2.0.php
-// You may elect to redistribute this code under either of these licenses.
-// ========================================================================
-
 package com.google.gerrit.httpd.gitweb;
 
 import com.google.gerrit.httpd.GitWebConfig;

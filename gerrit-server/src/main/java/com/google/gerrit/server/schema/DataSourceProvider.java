d848828a4e1ef02b10c2589aba859160f006b2a5
Shawn O. Pearce <sop@google.com>
2010-06-08 18:17:57 -0700

    Refactor DataSourceProvider so DBCP is optional
    Even though we bundle Apache Commons DBCP into our WAR file, we
    may still need to avoid loading its classes when we are trying to
    run in a NoSQL environment.
    
    If a NoSQL database type was selected in gerrit.config, try to avoid
    the JVM needing to reference the DBCP classes by going through an
    additional layer of indirection.
    
    Change-Id: If04a31a3b356b72feaa56606dbb937a4d321c63e
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/DataSourceProvider.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/DataSourceProvider.java
@@ -50,14 +50,19 @@ public final class DataSourceProvider implements
   private static final Logger log =
       LoggerFactory.getLogger(DataSourceProvider.class);
 
-  private final SchemaFactory<ReviewDb> ds;
-  private BasicDataSource basicDataSource;
+  private final MyProvider ds;
 
   @Inject
   DataSourceProvider(final SitePaths site,
       @GerritServerConfig final Config cfg, Context ctx) {
     try {
-      ds = open(site, cfg, ctx);
+      Type type = getEnum(cfg, "database", null, "type", Type.values(), null);
+      if (type == DataSourceProvider.Type.NOSQL_HEAP_FILE) {
+        ds = new NoSqlHeapFileProvider(site, cfg, ctx);
+
+      } else {
+        ds = new JdbcProvider(site, cfg, ctx);
+      }
     } catch (OrmException e) {
       throw new ProvisionException("Cannot create ReviewDb", e);
     }
@@ -65,33 +70,18 @@ public final class DataSourceProvider implements
 
   @Override
   public SchemaFactory<ReviewDb> get() {
-    return ds;
+    return ds.get();
   }
 
   @Override
   public void start() {
+    ds.start();
   }
 
   @SuppressWarnings("unchecked")
   @Override
   public synchronized void stop() {
-    if (basicDataSource != null) {
-      try {
-        basicDataSource.close();
-      } catch (SQLException e) {
-        // Ignore the close failure.
-      } finally {
-        basicDataSource = null;
-      }
-    }
-
-    if (ds instanceof FileDatabase) {
-      try {
-        ((FileDatabase) ds).close();
-      } catch (OrmException err) {
-        log.warn("Cannot safely close database", err);
-      }
-    }
+    ds.stop();
   }
 
   public static enum Context {
@@ -104,151 +94,6 @@ public final class DataSourceProvider implements
 
     // NoSQL types we also support
     NOSQL_HEAP_FILE;
-  }
-
-  private SchemaFactory<ReviewDb> open(final SitePaths site, final Config cfg,
-      final Context context) throws OrmException {
-    Type type = getEnum(cfg, "database", null, "type", Type.values(), null);
-    String driver = optional(cfg, "driver");
-    String url = optional(cfg, "url");
-    String username = optional(cfg, "username");
-    String password = optional(cfg, "password");
-
-    if (url == null || url.isEmpty()) {
-      if (type == null) {
-        type = Type.H2;
-      }
-
-      switch (type) {
-        case H2: {
-          String database = optional(cfg, "database");
-          if (database == null || database.isEmpty()) {
-            database = "db/ReviewDB";
-          }
-          File db = site.resolve(database);
-          try {
-            db = db.getCanonicalFile();
-          } catch (IOException e) {
-            db = db.getAbsoluteFile();
-          }
-          url = "jdbc:h2:" + db.toURI().toString();
-          break;
-        }
-
-        case POSTGRESQL: {
-          final StringBuilder b = new StringBuilder();
-          b.append("jdbc:postgresql://");
-          b.append(hostname(optional(cfg, "hostname")));
-          b.append(port(optional(cfg, "port")));
-          b.append("/");
-          b.append(required(cfg, "database"));
-          url = b.toString();
-          break;
-        }
-
-        case MYSQL: {
-          final StringBuilder b = new StringBuilder();
-          b.append("jdbc:mysql://");
-          b.append(hostname(optional(cfg, "hostname")));
-          b.append(port(optional(cfg, "port")));
-          b.append("/");
-          b.append(required(cfg, "database"));
-          url = b.toString();
-          break;
-        }
-
-        case JDBC:
-          driver = required(cfg, "driver");
-          url = required(cfg, "url");
-          break;
-
-        case NOSQL_HEAP_FILE: {
-          String database = optional(cfg, "database");
-          if (database == null || database.isEmpty()) {
-            database = "db/ReviewDB";
-          }
-          File db = site.resolve(database);
-          try {
-            db = db.getCanonicalFile();
-          } catch (IOException e) {
-            db = db.getAbsoluteFile();
-          }
-          return new FileDatabase<ReviewDb>(db, ReviewDb.class);
-        }
-
-        default:
-          throw new IllegalArgumentException(type + " not supported");
-      }
-    }
-
-    if (driver == null || driver.isEmpty()) {
-      if (url.startsWith("jdbc:h2:")) {
-        driver = "org.h2.Driver";
-
-      } else if (url.startsWith("jdbc:postgresql:")) {
-        driver = "org.postgresql.Driver";
-
-      } else if (url.startsWith("jdbc:mysql:")) {
-        driver = "com.mysql.jdbc.Driver";
-
-      } else {
-        throw new IllegalArgumentException("database.driver must be set");
-      }
-    }
-
-    boolean usePool;
-    if (url.startsWith("jdbc:mysql:")) {
-      // MySQL has given us trouble with the connection pool,
-      // sometimes the backend disconnects and the pool winds
-      // up with a stale connection. Fortunately opening up
-      // a new MySQL connection is usually very fast.
-      //
-      usePool = false;
-    } else {
-      usePool = true;
-    }
-    usePool = cfg.getBoolean("database", "connectionpool", usePool);
-    if (context == Context.SINGLE_USER) {
-      usePool = false;
-    }
-
-    if (usePool) {
-      final BasicDataSource ds = new BasicDataSource();
-      ds.setDriverClassName(driver);
-      ds.setUrl(url);
-      if (username != null && !username.isEmpty()) {
-        ds.setUsername(username);
-      }
-      if (password != null && !password.isEmpty()) {
-        ds.setPassword(password);
-      }
-      ds.setMaxActive(cfg.getInt("database", "poollimit", 8));
-      ds.setMinIdle(cfg.getInt("database", "poolminidle", 4));
-      ds.setMaxIdle(cfg.getInt("database", "poolmaxidle", 4));
-      ds.setMaxWait(ConfigUtil.getTimeUnit(cfg, "database", null,
-          "poolmaxwait", MILLISECONDS.convert(30, SECONDS), MILLISECONDS));
-      ds.setInitialSize(ds.getMinIdle());
-      basicDataSource = ds;
-      return new Database<ReviewDb>(ds, ReviewDb.class);
-
-    } else {
-      // Don't use the connection pool.
-      //
-      try {
-        final Properties p = new Properties();
-        p.setProperty("driver", driver);
-        p.setProperty("url", url);
-        if (username != null) {
-          p.setProperty("user", username);
-        }
-        if (password != null) {
-          p.setProperty("password", password);
-        }
-        return new Database<ReviewDb>(new SimpleDataSource(p), ReviewDb.class);
-      } catch (SQLException se) {
-        throw new ProvisionException("Database unavailable", se);
-      }
-    }
   }
 
   private static String hostname(String hostname) {
@@ -278,5 +123,209 @@ public final class DataSourceProvider implements
       throw new IllegalArgumentException("No database." + name + " configured");
     }
     return v;
+  }
+
+  private abstract static class MyProvider implements
+      Provider<SchemaFactory<ReviewDb>>, LifecycleListener {
+  }
+
+  private static class NoSqlHeapFileProvider extends MyProvider {
+    private FileDatabase<ReviewDb> db;
+
+    NoSqlHeapFileProvider(final SitePaths site, final Config cfg,
+        final Context context) throws OrmException {
+      String database = optional(cfg, "database");
+      if (database == null || database.isEmpty()) {
+        database = "db/ReviewDB";
+      }
+      File path = site.resolve(database);
+      try {
+        path = path.getCanonicalFile();
+      } catch (IOException e) {
+        path = path.getAbsoluteFile();
+      }
+      db = new FileDatabase<ReviewDb>(path, ReviewDb.class);
+    }
+
+    @Override
+    public SchemaFactory<ReviewDb> get() {
+      return db;
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+      if (db != null) {
+        try {
+          db.close();
+        } catch (OrmException err) {
+          log.warn("Cannot safely close database", err);
+        } finally {
+          db = null;
+        }
+      }
+    }
+  }
+
+  private static class JdbcProvider extends MyProvider {
+    private Database<ReviewDb> db;
+    private BasicDataSource dataSource;
+
+    JdbcProvider(final SitePaths site, final Config cfg, final Context context)
+        throws OrmException {
+      Type type = getEnum(cfg, "database", null, "type", Type.values(), null);
+      String driver = optional(cfg, "driver");
+      String url = optional(cfg, "url");
+      String username = optional(cfg, "username");
+      String password = optional(cfg, "password");
+
+      if (url == null || url.isEmpty()) {
+        if (type == null) {
+          type = Type.H2;
+        }
+
+        switch (type) {
+          case H2: {
+            String database = optional(cfg, "database");
+            if (database == null || database.isEmpty()) {
+              database = "db/ReviewDB";
+            }
+            File db = site.resolve(database);
+            try {
+              db = db.getCanonicalFile();
+            } catch (IOException e) {
+              db = db.getAbsoluteFile();
+            }
+            url = "jdbc:h2:" + db.toURI().toString();
+            break;
+          }
+
+          case POSTGRESQL: {
+            final StringBuilder b = new StringBuilder();
+            b.append("jdbc:postgresql://");
+            b.append(hostname(optional(cfg, "hostname")));
+            b.append(port(optional(cfg, "port")));
+            b.append("/");
+            b.append(required(cfg, "database"));
+            url = b.toString();
+            break;
+          }
+
+          case MYSQL: {
+            final StringBuilder b = new StringBuilder();
+            b.append("jdbc:mysql://");
+            b.append(hostname(optional(cfg, "hostname")));
+            b.append(port(optional(cfg, "port")));
+            b.append("/");
+            b.append(required(cfg, "database"));
+            url = b.toString();
+            break;
+          }
+
+          case JDBC:
+            driver = required(cfg, "driver");
+            url = required(cfg, "url");
+            break;
+
+          default:
+            throw new IllegalArgumentException(type + " not supported");
+        }
+      }
+
+      if (driver == null || driver.isEmpty()) {
+        if (url.startsWith("jdbc:h2:")) {
+          driver = "org.h2.Driver";
+
+        } else if (url.startsWith("jdbc:postgresql:")) {
+          driver = "org.postgresql.Driver";
+
+        } else if (url.startsWith("jdbc:mysql:")) {
+          driver = "com.mysql.jdbc.Driver";
+
+        } else {
+          throw new IllegalArgumentException("database.driver must be set");
+        }
+      }
+
+      boolean usePool;
+      if (url.startsWith("jdbc:mysql:")) {
+        // MySQL has given us trouble with the connection pool,
+        // sometimes the backend disconnects and the pool winds
+        // up with a stale connection. Fortunately opening up
+        // a new MySQL connection is usually very fast.
+        //
+        usePool = false;
+      } else {
+        usePool = true;
+      }
+      usePool = cfg.getBoolean("database", "connectionpool", usePool);
+      if (context == Context.SINGLE_USER) {
+        usePool = false;
+      }
+
+      if (usePool) {
+        final BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(driver);
+        ds.setUrl(url);
+        if (username != null && !username.isEmpty()) {
+          ds.setUsername(username);
+        }
+        if (password != null && !password.isEmpty()) {
+          ds.setPassword(password);
+        }
+        ds.setMaxActive(cfg.getInt("database", "poollimit", 8));
+        ds.setMinIdle(cfg.getInt("database", "poolminidle", 4));
+        ds.setMaxIdle(cfg.getInt("database", "poolmaxidle", 4));
+        ds.setMaxWait(ConfigUtil.getTimeUnit(cfg, "database", null,
+            "poolmaxwait", MILLISECONDS.convert(30, SECONDS), MILLISECONDS));
+        ds.setInitialSize(ds.getMinIdle());
+        dataSource = ds;
+        db = new Database<ReviewDb>(ds, ReviewDb.class);
+
+      } else {
+        // Don't use the connection pool.
+        //
+        try {
+          final Properties p = new Properties();
+          p.setProperty("driver", driver);
+          p.setProperty("url", url);
+          if (username != null) {
+            p.setProperty("user", username);
+          }
+          if (password != null) {
+            p.setProperty("password", password);
+          }
+          db = new Database<ReviewDb>(new SimpleDataSource(p), ReviewDb.class);
+        } catch (SQLException se) {
+          throw new ProvisionException("Database unavailable", se);
+        }
+      }
+    }
+
+    @Override
+    public SchemaFactory<ReviewDb> get() {
+      return db;
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+      if (dataSource != null) {
+        try {
+          dataSource.close();
+        } catch (SQLException e) {
+          // Ignore the close failure.
+        } finally {
+          dataSource = null;
+          db = null;
+        }
+      }
+    }
   }
 }

d5c3530d89e4ed279f6a517a767771ac03f32c3a
Shawn O. Pearce <sop@google.com>
2010-06-16 20:06:06 -0700

    gerrit.sh: Request at least 1024 file descriptors
    We may run out of transient file descriptors if packedGitOpenFiles is
    too low, such as the default setting of 128.  Assume we can get at
    least 1024 file descriptors and request that many by default.
    
    Bug: issue 460
    Change-Id: I45e62a8af9f3763eacf977431c299193f014bd1e
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- gerrit-pgm/src/main/resources/com/google/gerrit/pgm/gerrit.sh
+++ gerrit-pgm/src/main/resources/com/google/gerrit/pgm/gerrit.sh
@@ -295,6 +295,7 @@ fi
 GERRIT_FDS=`get_config --int core.packedGitOpenFiles`
 test -z "$GERRIT_FDS" && GERRIT_FDS=128
 GERRIT_FDS=`expr $GERRIT_FDS + $GERRIT_FDS`
+test $GERRIT_FDS -lt 1024 && GERRIT_FDS=1024
 
 GERRIT_USER=`get_config --get container.user`
 

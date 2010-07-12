60df79d8a630f8dd73c653d6545630203e3afa9e
Sasa Zivkov <sasa.zivkov@sap.com>
2010-07-12 08:50:59 +0200

    Open per-file diff browser window/tab for each file.
    Some Gerrit users find it convenient to open every file
    in a separate per-file diff browser window or tab. This
    should reduce a possibility to forget to review some
    file(s).
    
    Bug: issue 395
    Signed-off-by: Sasa Zivkov <sasa.zivkov@sap.com>
    Change-Id: Ie4138df0080fc92b7a451426f5dc1176283a6f55

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeConstants.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeConstants.java
@@ -112,4 +112,7 @@ public interface ChangeConstants extends Constants {
   String reviewed();
   String submitFailed();
   String buttonClose();
+
+  String buttonDiffAllSideBySide();
+  String buttonDiffAllUnified();
 }

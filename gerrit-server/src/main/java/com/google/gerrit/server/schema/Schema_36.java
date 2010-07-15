c9a699f99ad0ab8fcfd153cd3e34f948c6753d36
Shawn O. Pearce <sop@google.com>
2010-07-15 14:17:02 -0700

    Fix schema 36 on MySQL
    Change-Id: I802d7973ceb52aadd5383bae1fdae1c9f9dceacf
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_36.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_36.java
@@ -16,6 +16,7 @@ package com.google.gerrit.server.schema;
 
 import com.google.gerrit.reviewdb.ReviewDb;
 import com.google.gwtorm.jdbc.JdbcSchema;
+import com.google.gwtorm.schema.sql.DialectMySQL;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
@@ -32,9 +33,15 @@ public class Schema_36 extends SchemaVersion {
   protected void migrateData(ReviewDb db, UpdateUI ui) throws SQLException {
     Statement stmt = ((JdbcSchema) db).getConnection().createStatement();
     try {
-      stmt.execute("DROP INDEX account_project_watches_ntNew");
-      stmt.execute("DROP INDEX account_project_watches_ntCmt");
-      stmt.execute("DROP INDEX account_project_watches_ntSub");
+      if (((JdbcSchema) db).getDialect() instanceof DialectMySQL) {
+        stmt.execute("DROP INDEX account_project_watches_ntNew ON account_project_watches");
+        stmt.execute("DROP INDEX account_project_watches_ntCmt ON account_project_watches");
+        stmt.execute("DROP INDEX account_project_watches_ntSub ON account_project_watches");
+      } else {
+        stmt.execute("DROP INDEX account_project_watches_ntNew");
+        stmt.execute("DROP INDEX account_project_watches_ntCmt");
+        stmt.execute("DROP INDEX account_project_watches_ntSub");
+      }
       stmt.execute("CREATE INDEX account_project_watches_byProject"
           + " ON account_project_watches (project_name)");
     } finally {

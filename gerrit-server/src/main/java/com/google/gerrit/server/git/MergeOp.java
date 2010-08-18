5cced865689934207cc2dc5171e6815273635ac9
Goran Lungberg <goeran.lungberg@sonyericsson.com>
2010-08-18 09:51:42 +0200

    Support for git submodules and using .gitmodules as manifest
    A new subscriptions table will be populated based on the content of the
    .gitmodules file when merged. The gitlinks of all subscribers of a module
    branch will be updated by Gerrrit when the branch is updated.
    The update is recursive supporting submodules of submodules.
    
    Change-Id: Ic22f273fa0032a4d402c0ca8f494179e40de3b2b

--- gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
@@ -23,6 +23,7 @@ import com.google.gerrit.common.data.ApprovalTypes;
 import com.google.gerrit.reviewdb.Account;
 import com.google.gerrit.reviewdb.ApprovalCategory;
 import com.google.gerrit.reviewdb.Branch;
+import com.google.gerrit.reviewdb.Branch.NameKey;
 import com.google.gerrit.reviewdb.Change;
 import com.google.gerrit.reviewdb.ChangeMessage;
 import com.google.gerrit.reviewdb.PatchSet;
@@ -30,6 +31,7 @@ import com.google.gerrit.reviewdb.PatchSetApproval;
 import com.google.gerrit.reviewdb.Project;
 import com.google.gerrit.reviewdb.RevId;
 import com.google.gerrit.reviewdb.ReviewDb;
+import com.google.gerrit.reviewdb.Subscription;
 import com.google.gerrit.server.ChangeUtil;
 import com.google.gerrit.server.GerritPersonIdent;
 import com.google.gerrit.server.IdentifiedUser;
@@ -52,12 +54,20 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.assistedinject.Assisted;
 
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheBuilder;
+import org.eclipse.jgit.dircache.DirCacheEditor;
+import org.eclipse.jgit.dircache.DirCacheEditor.PathEdit;
+import org.eclipse.jgit.dircache.DirCacheEntry;
+import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.BlobBasedConfig;
 import org.eclipse.jgit.lib.Commit;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.PersonIdent;
@@ -73,6 +83,7 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevFlag;
 import org.eclipse.jgit.revwalk.RevSort;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -90,6 +101,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TimeZone;
+import java.util.zip.DataFormatException;
 
 import javax.annotation.Nullable;
 
@@ -153,6 +165,7 @@ public class MergeOp {
   private CodeReviewCommit mergeTip;
   private Set<RevCommit> alreadyAccepted;
   private RefUpdate branchUpdate;
+  private HashSet<NameKey> updatedSubscribers;
 
   private final ChangeHookRunner hooks;
   private final AccountCache accountCache;
@@ -234,6 +247,8 @@ public class MergeOp {
     }
     updateBranch();
     updateChangeStatus();
+    updateSubscriptions();
+    updateSubscribers(destBranch, mergeTip.getId().toObjectId(), null);
   }
 
   private void openRepository() throws MergeException {
@@ -947,6 +962,293 @@ public class MergeOp {
     }
   }
 
+  private String getSrvName(String srvUrl) {
+    String srvName = null;
+    final int begin = srvUrl.indexOf("//") + 2;
+    if (begin >= 2) {
+      int end = srvUrl.length();
+      int e = srvUrl.indexOf("/", begin);
+      if (e > 0) end = e;
+      e = srvUrl.indexOf(".", begin);
+      if (e > 0 && e < end) end = e;
+      e = srvUrl.indexOf(":", begin);
+      if (e > 0 && e < end) end = e;
+      if (end >= begin) srvName = srvUrl.substring(begin, end);
+    }
+    return srvName;
+  }
+
+  private void updateSubscriptions() {
+    // Note: The merge is already performed so we shall not throw an exception
+    // on failure.
+    final String gitmodulesFile = ".gitmodules";
+    try {
+      final TreeWalk tw =
+          TreeWalk.forPath(db, gitmodulesFile, mergeTip.getTree());
+      if (tw != null
+          && (FileMode.REGULAR_FILE.equals(tw.getRawMode(0)) || FileMode.EXECUTABLE_FILE
+              .equals(tw.getRawMode(0)))) {
+
+        BlobBasedConfig bbc =
+            new BlobBasedConfig(null, mergeTip.asCommit(rw), gitmodulesFile);
+
+        final String thisServer = getSrvName(urlProvider.get());
+
+        if (thisServer == null) {
+
+          if (urlProvider.get() == null) {
+            throw new DataFormatException(
+                "You must define gerrit.canonicalWebUrl inside gerrit.config.");
+          } else {
+            throw new DataFormatException("At parse of " + gitmodulesFile
+                + ": problem retrieving name of this server.");
+          }
+
+        }
+
+        List<Subscription> oldSubscriptions =
+            schema.subscriptions().getSubscription(destBranch).toList();
+        List<Subscription> newSubscriptions = new ArrayList<Subscription>();
+
+        for (String id : bbc.getSubsections("submodule")) {
+          final String url = bbc.getString("submodule", id, "url");
+          String path = bbc.getString("submodule", id, "path");
+          String revision = bbc.getString("submodule", id, "revision");
+          if (url != null && url.length() > 0 && path != null
+              && path.length() > 0 && revision != null && revision.length() > 0) {
+            boolean pathIsRelative = url.startsWith("/");
+            String server = null;
+            if (!pathIsRelative) server = getSrvName(url);
+            if ((pathIsRelative)
+                || (server != null && server.equalsIgnoreCase(thisServer))) {
+              if (revision.equals(".")) {
+                revision = destBranch.get();
+              } else if (!revision.startsWith(Constants.R_REFS)) {
+                revision = Constants.R_HEADS + revision;
+              }
+              if (!path.contains("/")) {
+                if (destProject.getName().contains("/")) {
+                  String prefix =
+                      destProject.getName().substring(0,
+                          destProject.getName().indexOf("/") + 1);
+                  path = prefix + path;
+                }
+              }
+              newSubscriptions.add(new Subscription(
+                  new Branch.NameKey(
+                      new Project.NameKey(destProject.getName()), destBranch
+                          .get()), new Branch.NameKey(
+                      new Project.NameKey(path), revision)));
+            }
+          }
+        }
+
+        List<Subscription> alreadySubscribeds = new ArrayList<Subscription>();
+        for (Subscription s : newSubscriptions) {
+          if (oldSubscriptions.contains(s)) {
+            alreadySubscribeds.add(s);
+          }
+        }
+        newSubscriptions.removeAll(alreadySubscribeds);
+        oldSubscriptions.removeAll(alreadySubscribeds);
+        if (!newSubscriptions.isEmpty()) {
+          if (!oldSubscriptions.isEmpty()) {
+            schema.subscriptions().delete(oldSubscriptions);
+          }
+          schema.subscriptions().insert(newSubscriptions);
+        }
+      }
+    }
+
+    catch (OrmException e) {
+      log.error("Database problem at update of subscriptions table from "
+          + gitmodulesFile + " file.", e);
+    } catch (ConfigInvalidException e) {
+      log.error("Problem at update of subscriptions table: " + gitmodulesFile
+          + " config file is invalid.", e);
+    } catch (IOException e) {
+      log.error("Problem at update of subscriptions table from "
+          + gitmodulesFile + ".", e);
+    } catch (DataFormatException e) {
+      log.error(e.getMessage(), e);
+    }
+  }
+
+  private void updateSubscribers(final Branch.NameKey updatedBranch,
+      final ObjectId mergedCommit, final String msg) {
+    // Note: The merge is already performed so we shouldn't throw an Exception
+    // on failure
+
+    try {
+      final List<Subscription> subscribers =
+          schema.subscriptions().getSubscribers(updatedBranch).toList();
+
+      if (!subscribers.isEmpty()) {
+        final StringBuilder msgbuf = new StringBuilder();
+        if (msg == null) {
+          // safeguard against circular subscriptions
+          updatedSubscribers = new HashSet<Branch.NameKey>();
+
+          updatedSubscribers.add(updatedBranch);
+
+          for (final Change chg : submitted) {
+            final CodeReviewCommit c = commits.get(chg.getId());
+
+            if (c != null
+                && (c.statusCode == CommitMergeStatus.CLEAN_MERGE || c.statusCode == CommitMergeStatus.CLEAN_PICK)) {
+              msgbuf.append("\n");
+              msgbuf.append(c.getFullMessage());
+            }
+          }
+        } else {
+          msgbuf.append(msg);
+        }
+
+        if (msgbuf.length() > 0) {
+          // update subscribers of this module
+          for (final Subscription s : subscribers) {
+            if (updatedSubscribers.contains(s.getSubscriber())) {
+              log.error("Possible circular subscription involving "
+                  + s.toString());
+            } else {
+              Map<Branch.NameKey, ObjectId> modules =
+                  new HashMap<Branch.NameKey, ObjectId>(1);
+              modules.put(updatedBranch, mergedCommit);
+              updateGitlinks(s.getSubscriber(), modules, msgbuf.toString());
+              updatedSubscribers.add(s.getSubscriber());
+            }
+          }
+        }
+      }
+    } catch (OrmException e) {
+      log.error("Cannot read subscription records", e);
+    }
+  }
+
+  private void updateGitlinks(final Branch.NameKey subscriber,
+      final Map<Branch.NameKey, ObjectId> modules, final String msg) {
+    // Note: should we support default values for author/committer,
+    // configured in gerrit.config maybe?
+
+    PersonIdent author = null;
+    PersonIdent committer = null;
+    PatchSetApproval submitter = null;
+    final StringBuilder msgbuf = new StringBuilder();
+    msgbuf.append("Updated " + subscriber.getParentKey().get());
+
+    for (final Map.Entry<Branch.NameKey, ObjectId> me : modules.entrySet()) {
+      final CodeReviewCommit c = new CodeReviewCommit(me.getValue());
+      msgbuf.append("\nProject: ");
+      msgbuf.append(me.getKey().getParentKey().get());
+      msgbuf.append("  " + me.getValue().getName());
+      msgbuf.append("\n");
+      if (modules.size() == 1 && msg != null) {
+        msgbuf.append(msg);
+      } else {
+        msgbuf.append(c.getShortMessage());
+      }
+      msgbuf.append("\n");
+
+      PatchSetApproval s = getSubmitter(c.patchsetId);
+      if (submitter == null
+          || (s != null && s.getGranted().compareTo(submitter.getGranted()) > 0)) {
+        submitter = s;
+      }
+
+      if (submitter != null) {
+        IdentifiedUser who =
+            identifiedUserFactory.create(submitter.getAccountId());
+        Set<String> emails = new HashSet<String>();
+        emails.add(c.getAuthorIdent().getEmailAddress());
+
+        final Timestamp dt = submitter.getGranted();
+        final TimeZone tz = myIdent.getTimeZone();
+        if (emails.size() == 1
+            && who.getEmailAddresses().contains(emails.iterator().next())) {
+          author = new PersonIdent(c.getAuthorIdent(), dt, tz);
+        } else {
+          author = who.newCommitterIdent(dt, tz);
+        }
+      } else {
+        author = myIdent;
+      }
+    }
+
+    try {
+      final Repository pdb =
+          repoManager.openRepository(subscriber.getParentKey().get());
+
+      if (pdb.getRef(subscriber.get()) == null) {
+        throw new IOException(
+            "The branch was probably deleted from the subscriber repository");
+      }
+
+      final ObjectId currentCommitId = pdb.getRef(subscriber.get()).getObjectId();
+
+      DirCache dc = readTree(pdb, pdb.getRef(subscriber.get()));
+      DirCacheEditor ed = dc.editor();
+      for (final Map.Entry<Branch.NameKey, ObjectId> me : modules.entrySet()) {
+        ed.add(new PathEdit(me.getKey().get()) {
+          public void apply(DirCacheEntry ent) {
+            ent.setFileMode(FileMode.GITLINK);
+            ent.setObjectId(me.getValue().copy());
+          }
+        });
+      }
+      ed.finish();
+
+      ObjectInserter oi = pdb.newObjectInserter();
+      ObjectId tree = dc.writeTree(oi);
+
+      final Commit commit = new Commit(pdb);
+      commit.setTreeId(tree);
+      commit.setParentIds(new ObjectId[] {currentCommitId});
+      commit.setAuthor(author);
+      commit.setCommitter(myIdent);
+      commit.setMessage(msgbuf.toString());
+      commit.commit();
+
+      final RefUpdate rfu = pdb.updateRef(subscriber.get());
+      rfu.setForceUpdate(false);
+      rfu.setNewObjectId(commit.getCommitId());
+      rfu.setExpectedOldObjectId(currentCommitId);
+      rfu.setRefLogMessage("Submit to " + subscriber.getParentKey().get(), true);
+
+      switch (rfu.update()) {
+        case NEW:
+        case FAST_FORWARD:
+          replication.scheduleUpdate(subscriber.getParentKey(), rfu.getName());
+          // TODO since this is performed "in the background" no mail will be
+          // sent
+          // to inform users about the updated branch
+          break;
+
+        default:
+          throw new IOException(rfu.getResult().name());
+      }
+
+      // update subscribers of the subscriber
+      updateSubscribers(subscriber, commit.getCommitId(), msgbuf.toString());
+    } catch (RepositoryNotFoundException e) {
+      log.error("Cannot update gitlinks for " + subscriber.get(), e);
+    } catch (IOException e) {
+      log.error("Cannot update gitlinks for " + subscriber.get(), e);
+    }
+  }
+
+  private DirCache readTree(Repository pdb, Ref branch)
+      throws MissingObjectException, IncorrectObjectTypeException, IOException {
+    RevWalk rw = new RevWalk(pdb);
+
+    DirCache dc = DirCache.newInCore();
+    DirCacheBuilder b = dc.builder();
+    b.addTree(new byte[0], // no prefix path
+        DirCacheEntry.STAGE_0, // standard stage
+        pdb.newObjectReader(), rw.parseTree(branch.getObjectId()));
+    b.finish();
+    return dc;
+  }
+
   private void dependencyError(final CodeReviewCommit commit) {
     final Change c = commit.change;
     if (commit.missing == null) {
@@ -1253,6 +1555,7 @@ public class MergeOp {
           cm.setFrom(submitter.getAccountId());
         }
       }
+
       cm.setPatchSet(schema.patchSets().get(c.currentPatchSetId()));
       cm.setChangeMessage(msg);
       cm.send();

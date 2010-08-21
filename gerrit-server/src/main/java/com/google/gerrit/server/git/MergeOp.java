39f4b62298b29f941a542b1e262761a0684cebee
Shawn O. Pearce <sop@google.com>
2010-08-21 13:00:51 -0700

    Fix NPE during merge failure on new branch
    The following caused an NPE in the submit queue:
    
      1. Create a new project
      2. Push change 1 to the new project
      3. Push change 2 which depends on change 1 to the new project
      4. (verify/approve) submit change 2
    
    What happened was we processed change 2 without having change 1
    in the submit queue.  This lead us to give a "missing dependency"
    error for change 2, which is reasonable.  However the branch we are
    submitting to does not yet exist, and nothing was submitted into it.
    We still tried to create the branch, but with a null ObjectId.
    
    Avoid branch creation if we have nothing to store in it yet.
    
    Bug: issue 652
    Change-Id: Ia4112bbb03526d8a75e0eba577c7f5a672bf7298
    Signed-off-by: Shawn O. Pearce <sop@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
@@ -834,7 +834,7 @@ public class MergeOp {
   }
 
   private void updateBranch() throws MergeException {
-    if (branchTip == null || branchTip != mergeTip) {
+    if (mergeTip != null && (branchTip == null || branchTip != mergeTip)) {
       branchUpdate.setForceUpdate(false);
       branchUpdate.setNewObjectId(mergeTip);
       branchUpdate.setRefLogMessage("merged", true);

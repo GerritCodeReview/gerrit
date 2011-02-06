834eabd20dbdefe7ae9033f9a123cd32cd5a4cdf
Josh Turmel <jturmel@gmail.com>
2011-02-06 02:01:23 -0600

    Add reviewer/verifier name out beside check/plus/minus
    Change-Id: Iae347521b7645082a1d65759198d0347502ab55d

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
@@ -314,6 +314,7 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
 
       fmt.removeStyleName(row, col, Gerrit.RESOURCES.css().negscore());
       fmt.removeStyleName(row, col, Gerrit.RESOURCES.css().posscore());
+      fmt.addStyleName(row, col, Gerrit.RESOURCES.css().singleLine());
 
       if (ca == null || ca.getValue() == 0) {
         table.clearCell(row, col);
@@ -321,25 +322,29 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
       } else {
         haveReview = true;
 
+        final ApprovalCategoryValue acv = type.getValue(ca);
+        final AccountInfo ai = aic.get(ca.getAccountId());
+
         if (type.isMaxNegative(ca)) {
-          table.setWidget(row, col, new Image(Gerrit.RESOURCES.redNot()));
+          table.setHTML(row, col, new Image(Gerrit.RESOURCES.redNot())
+              .getElement().getString()
+              + FormatUtil.name(ai));
 
         } else if (type.isMaxPositive(ca)) {
-          table.setWidget(row, col, new Image(Gerrit.RESOURCES.greenCheck()));
+          table.setHTML(row, col, new Image(Gerrit.RESOURCES.greenCheck())
+              .getElement().getString()
+              + FormatUtil.name(ai));
 
         } else {
           String vstr = String.valueOf(ca.getValue());
           if (ca.getValue() > 0) {
-            vstr = "+" + vstr;
+            vstr = "+" + vstr + " " + FormatUtil.nameEmail(ai);
             fmt.addStyleName(row, col, Gerrit.RESOURCES.css().posscore());
           } else {
             fmt.addStyleName(row, col, Gerrit.RESOURCES.css().negscore());
           }
-          table.setText(row, col, vstr);
+          table.setHTML(row, col, vstr);
         }
-
-        final ApprovalCategoryValue acv = type.getValue(ca);
-        final AccountInfo ai = aic.get(ca.getAccountId());
 
         // Some web browsers ignore the embedded newline; some like it;
         // so we include a space before the newline to accommodate both.

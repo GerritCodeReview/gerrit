213971ab11ec331bede46ee36bc0bc517613b72b
monica.dionisio <monica.dionisio@sonyericsson.com>
2011-04-08 13:04:16 -0300

    Suggestion Oracle to labels
    LabelSuggestOracle created. It provides suggestions of labels.
    In the suggestion list there should not be the labels tied to
    specified change.
    
    One should update list of labels tied to specific change
    whenever a label is untied. When a label is untied one should
    call LabelSuggestOracle.updateLabelsToExclude.
    
    LabelSuggestOracle use integrated in LabelsEditor by this commit.
    
    Change-Id: Iba1ba161969b16e7d8e2f191e78b41aca4b1d332

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/ui/LabelsEditor.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/ui/LabelsEditor.java
@@ -24,6 +24,8 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
@@ -33,9 +35,12 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PushButton;
+import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
 import com.google.gwtjsonrpc.client.VoidResult;
 
+import java.util.HashSet;
 import java.util.List;
 
 /** UI Component to edit change labels */
@@ -43,10 +48,13 @@ public class LabelsEditor extends Composite {
   private final HorizontalPanel addPanel;
   private final Button addLabel;
   private HintTextBox nameTxtBox;
+  private SuggestBox nameTxt;
   private FlowPanel arbitraryLabelsPanel;
   private List<ChangeLabel.LabelKey> arbitraryLabels;
   private VerticalPanel labelsPanel;
   private final Change.Id changeId;
+  private boolean submitOnSelection;
+  private LabelSuggestOracle labelSuggestOracle;
 
   public LabelsEditor(final Change.Id changeId) {
     this.changeId = changeId;
@@ -82,17 +90,38 @@ public class LabelsEditor extends Composite {
 
   public void initAddLabel() {
     nameTxtBox = new HintTextBox();
+    labelSuggestOracle =
+        new LabelSuggestOracle(changeId, new HashSet<ChangeLabel.LabelKey>(
+            arbitraryLabels));
+
+    nameTxt =
+        new SuggestBox(new RPCSuggestOracle(labelSuggestOracle), nameTxtBox);
+
     nameTxtBox.setVisibleLength(40);
     nameTxtBox.addKeyPressHandler(new KeyPressHandler() {
       @Override
       public void onKeyPress(KeyPressEvent event) {
+        submitOnSelection = false;
+
         if (event.getCharCode() == KeyCodes.KEY_ENTER) {
-          doAddNew();
+          if (nameTxt.isSuggestionListShowing()) {
+            submitOnSelection = true;
+          } else {
+            doAddNew();
+          }
         }
       }
     });
 
-    addPanel.add(nameTxtBox);
+    nameTxt.addSelectionHandler(new SelectionHandler<Suggestion>() {
+      @Override
+      public void onSelection(SelectionEvent<Suggestion> event) {
+        if (submitOnSelection) {
+          submitOnSelection = false;
+          doAddNew();
+        }
+      }
+    });
 
     final TextBoxBaseListener validator = new TextBoxBaseListener(nameTxtBox) {
       @Override
@@ -108,6 +137,8 @@ public class LabelsEditor extends Composite {
 
     nameTxtBox.addKeyPressHandler(validator);
     nameTxtBox.addMouseUpHandler(validator);
+
+    addPanel.add(nameTxt);
 
     addPanel.add(addLabel);
   }
@@ -178,6 +209,8 @@ public class LabelsEditor extends Composite {
               @Override
               public void onSuccess(VoidResult result) {
                 arbitraryLabels.remove(labelKey);
+                labelSuggestOracle
+                    .updateLabelsToExclude(new ChangeLabel.LabelKey(labelKey));
                 g.removeFromParent();
               }
             });

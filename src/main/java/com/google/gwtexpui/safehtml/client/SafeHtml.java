742a56e31dcab7bb3314c47107ac00e7e47e6464
Shawn O. Pearce <sop@google.com>
2011-06-20 08:33:28 -0700

    Support ~ in URLs in linkfy()
    Change-Id: Ie2cbd43903f4f6e453adc52f5d4e543847788658

--- src/main/java/com/google/gwtexpui/safehtml/client/SafeHtml.java
+++ src/main/java/com/google/gwtexpui/safehtml/client/SafeHtml.java
@@ -111,7 +111,7 @@ public abstract class SafeHtml {
   /** Convert bare http:// and https:// URLs into &lt;a href&gt; tags. */
   public SafeHtml linkify() {
     final String part = "(?:" +
-		"[a-zA-Z0-9$_.+!*',%;:@=?#/-]" +
+		"[a-zA-Z0-9$_.+!*',%;:@=?#/-~]" +
 		"|&(?!lt;|gt;)" +
 		")";
     return replaceAll(

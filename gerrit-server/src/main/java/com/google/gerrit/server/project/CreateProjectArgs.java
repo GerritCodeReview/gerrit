75fcbd25b39540c46a7a935e31eb57868eb34ae8
monica.dionisio <monica.dionisio@sonyericsson.com>
2011-07-12 09:57:07 -0300

    Create project refactoring
    This refactoring is a preparation to feature
    'Create project through web interface' which
    includes a new UI to create projects.
    
    It splits out the create project code
    adding new classes that will be used both
    by ssh command and the new UI.
    
    Updated some naming pattern to keep it more
    consistent.
    
    Improved the exception handling.
    
    Change-Id: I789c9a09f905d90680cc74d9b6aa7ae8f4d808b7

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/CreateProjectArgs.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2011 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
+package com.google.gerrit.server.project;
+
+import com.google.gerrit.reviewdb.AccountGroup;
+import com.google.gerrit.reviewdb.Project;
+import com.google.gerrit.reviewdb.Project.NameKey;
+import com.google.gerrit.reviewdb.Project.SubmitType;
+
+import java.util.List;
+
+public class CreateProjectArgs {
+
+  private Project.NameKey projectName;
+  public List<AccountGroup.UUID> ownerIds;
+  public ProjectControl newParent;
+  public String projectDescription;
+  public SubmitType submitType;
+  public boolean contributorAgreements;
+  public boolean signedOffBy;
+  public boolean permissionsOnly;
+  public String branch;
+  public boolean contentMerge;
+  public boolean changeIdRequired;
+  public boolean createEmptyCommit;
+
+  public CreateProjectArgs() {
+  }
+
+  public Project.NameKey getProject() {
+    return projectName;
+  }
+
+  public String getProjectName() {
+    return projectName != null ? projectName.get() : null;
+  }
+
+  public void setProjectName(String n) {
+    projectName = n != null ? new NameKey(n) : null;
+  }
+
+  public void setProjectName(NameKey n) {
+    projectName = n;
+  }
+}

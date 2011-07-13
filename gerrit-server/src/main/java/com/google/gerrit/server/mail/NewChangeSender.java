1cd650b662b4f26a48b9c41ba29e598e7cb2b5ba
Gal Hammer <ghammer@redhat.com>
2011-07-13 14:50:45 +0300

    Add unified diff to newchange mail template.
    This change add a new macro to the NewChange mail template. The
    $email.UnifiedDiff is expanded to a unified diff of the patch.
    
    Change-Id: I9c72cc7129adb9aae4f8168e7bba9215aa6ced3c

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/NewChangeSender.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/NewChangeSender.java
@@ -16,10 +16,21 @@ package com.google.gerrit.server.mail;
 
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.patch.PatchList;
 import com.google.gerrit.server.ssh.SshInfo;
 
 import com.jcraft.jsch.HostKey;
 
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.util.RawParseUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -31,6 +42,9 @@ public abstract class NewChangeSender extends ChangeEmail {
   private final SshInfo sshInfo;
   private final Set<Account.Id> reviewers = new HashSet<Account.Id>();
   private final Set<Account.Id> extraCC = new HashSet<Account.Id>();
+
+  private static final Logger log =
+      LoggerFactory.getLogger(NewChangeSender.class);
 
   protected NewChangeSender(EmailArguments ea, String anonymousCowardName,
       SshInfo sshInfo, Change c) {
@@ -84,5 +98,34 @@ public abstract class NewChangeSender extends ChangeEmail {
       return getGerritHost() + host.substring(1);
     }
     return host;
+  }
+
+  /** Show patch set as unified difference.  */
+  public String getUnifiedDiff() {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    Repository repo = openRepository();
+    if (repo != null) {
+      DiffFormatter df = new DiffFormatter(out);
+      df.setRepository(repo);
+      df.setDetectRenames(true);
+      try {
+        PatchList patchList = getPatchList();
+        df.format(patchList.getOldId(), patchList.getNewId());
+      } catch (IOException e) {
+        log.error("Cannot format patch", e);
+      } finally {
+        df.release();
+      }
+    }
+    return RawParseUtils.decode(out.toByteArray());
+  }
+
+  private Repository openRepository() {
+    try {
+      return args.server.openRepository(change.getProject());
+    } catch (RepositoryNotFoundException e) {
+      log.error("Cannot open repository", e);
+      return null;
+    }
   }
 }

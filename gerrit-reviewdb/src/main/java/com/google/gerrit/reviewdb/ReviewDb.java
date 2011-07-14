80798a44351bfbc036f2d90cbd215958689577c6
Rafael Rabelo Silva <rafael.rabelosilva@sonyericsson.com>
2011-07-14 15:39:01 -0300

    New SchemaVersion
    New schema version creates ChangeLabel table and populates
    ApprovalCategory and ApprovalCategoryValues tables with
    TieArbitraryLabel permission.
    
    Change-Id: Ie1a0af0339a5b1270e5fe5864edf6f4c93f23d98

--- gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/ReviewDb.java
+++ gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/ReviewDb.java
@@ -113,6 +113,9 @@ public interface ReviewDb extends Schema {
   @Relation
   TrackingIdAccess trackingIds();
 
+  @Relation
+  ChangeLabelAccess changeLabels();
+
   /** Create the next unique id for an {@link Account}. */
   @Sequence(startWith = 1000000)
   int nextAccountId() throws OrmException;

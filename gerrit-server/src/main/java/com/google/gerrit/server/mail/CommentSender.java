ed6b8f64e7bdae2be2e71d9d0c43af8163db906b
Gal Hammer <ghammer@redhat.com>
2011-08-15 11:44:26 +0300

    Add lines count parameter to the inlineComments function.
    This change add the ability to send an email which span muliple lines around
    a comment. Thereby allowing the reader to have a better understanding of the
    comment's context.
    
    Change-Id: Ic87cdbccfbd3e27e63fdeeafef1dcc16c41ae8f3

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/CommentSender.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/CommentSender.java
@@ -77,6 +77,10 @@ public class CommentSender extends ReplyToChangeSender {
   }
 
   public String getInlineComments() {
+    return getInlineComments(1);
+  }
+
+  public String getInlineComments(int lines) {
     StringBuilder  cmts = new StringBuilder();
 
     final Repository repo = getRepository();
@@ -113,19 +117,29 @@ public class CommentSender extends ReplyToChangeSender {
           }
         }
 
-        cmts.append("Line " + lineNbr);
         if (currentFileData != null) {
+          int maxLines;
           try {
-            final String lineStr = currentFileData.getLine(side, lineNbr);
-            cmts.append(": ");
-            cmts.append(lineStr);
-          } catch (Throwable cce) {
-            // Don't quote the line if we can't safely convert it.
+            maxLines = currentFileData.getLineCount(side);
+          } catch (Throwable e) {
+            maxLines = lineNbr;
+          }
+
+          final int startLine = Math.max(1, lineNbr - lines + 1);
+          final int stopLine = Math.min(maxLines, lineNbr + lines);
+
+          for (int line = startLine; line <= lineNbr; ++line) {
+            appendFileLine(cmts, currentFileData, side, line);
+          }
+
+          cmts.append(c.getMessage().trim());
+          cmts.append("\n");
+
+          for (int line = lineNbr + 1; line < stopLine; ++line) {
+            appendFileLine(cmts, currentFileData, side, line);
           }
         }
-        cmts.append("\n");
 
-        cmts.append(c.getMessage().trim());
         cmts.append("\n\n");
       }
     } finally {
@@ -134,6 +148,18 @@ public class CommentSender extends ReplyToChangeSender {
       }
     }
     return cmts.toString();
+  }
+
+  private void appendFileLine(StringBuilder cmts, PatchFile fileData, short side, int line) {
+    cmts.append("Line " + line);
+    try {
+      final String lineStr = fileData.getLine(side, line);
+      cmts.append(": ");
+      cmts.append(lineStr);
+    } catch (Throwable e) {
+      // Don't quote the line if we can't safely convert it.
+    }
+    cmts.append("\n");
   }
 
   private Repository getRepository() {

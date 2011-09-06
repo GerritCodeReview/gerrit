a7e928daaf7ba4176b5044797f90b21f420ab2f7
Edwin Kempin <edwin.kempin@sap.com>
2011-09-06 16:43:30 +0200

    Verify the case of the project name before opening git repository
    When a project is not in the project cache, Gerrit checks for the
    corresponding git repository in the file system and adds the project
    to the cache if the git repository is found. On Windows the paths are
    case-insensitive, which means that the lookup for the git repository
    in the file system is even successfull if the name of the project was
    given in an incorrect case.
    
    E.g. lets assume the project 'test/myProject' exists, then asking the
    project cache for 'test/myProject', 'TEST/myProject', 'test/MyProject'
    would all succeed. Even worse for each variant of the case a new
    instance of the project would be added to the cache. These instances
    of the same project in the project cache might even become
    inconsistent with each other.
    
    This problem is relevant for all SSH commands where the user can
    specify a project name, e.g. for 'set-project-parent'. If for the
    '--parent' parameter the project is specified in an incorrect case,
    this incorrect case is then even persisted in the 'project.config'
    file of the child projects.
    
    To avoid these problems, with this change the case of the project name
    is now verified and the project is only added to the cache if the
    project name was specified in the correct case. If the project name
    was specified in an incorrect case the lookup would fail with an
    RepositoryNotFoundException.
    
    Change-Id: Icf826d16e05a8c19537e86fe302b158bcb407bba
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/RepositoryCaseMismatchException.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2011 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.gerrit.reviewdb.Project;
+import com.google.gerrit.reviewdb.Project.NameKey;
+
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+
+/**
+ * This exception is thrown if a project cannot be created because a project
+ * with the same name in a different case already exists. This can only happen
+ * if the OS has a case insensitive file system (e.g. Windows), because in this
+ * case the name for the git repository in the file system is already occupied
+ * by the existing project.
+ */
+public class RepositoryCaseMismatchException extends
+    RepositoryNotFoundException {
+
+  private static final long serialVersionUID = 1L;
+
+  private final NameKey nameOfExistingProject;
+
+  /**
+   * @param projectName name of the project that cannot be created
+   * @param nameOfExistingProject name of the project that already exists and
+   *        occupies the name for the git repository in the file system
+   */
+  public RepositoryCaseMismatchException(final Project.NameKey projectName,
+      final Project.NameKey nameOfExistingProject) {
+    super("Name occupied in other case: " + projectName.get() + "; project "
+        + nameOfExistingProject.get() + " exists");
+    this.nameOfExistingProject = nameOfExistingProject;
+  }
+
+  public NameKey getNameOfExistingProject() {
+    return nameOfExistingProject;
+  }
+}

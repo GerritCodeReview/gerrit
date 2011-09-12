924cfd3b8b96bda4611fc10f678e5d13ddbbc356
Edwin Kempin <edwin.kempin@sap.com>
2011-09-12 09:43:53 +0200

    Add SSH command to list group members
    Change-Id: I7f189100a7a0994a79bedd92ce1897bb69257b34
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritRequestModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritRequestModule.java
@@ -16,6 +16,7 @@ package com.google.gerrit.server.config;
 
 import static com.google.inject.Scopes.SINGLETON;
 
+import com.google.gerrit.common.AccountFormatter;
 import com.google.gerrit.reviewdb.ReviewDb;
 import com.google.gerrit.server.AnonymousUser;
 import com.google.gerrit.server.IdentifiedUser;
@@ -95,5 +96,6 @@ public class GerritRequestModule extends FactoryModule {
     factory(VisibleGroups.Factory.class);
     factory(GroupDetailFactory.Factory.class);
     factory(GroupMembers.Factory.class);
+    factory(AccountFormatter.Factory.class);
   }
 }

2a2d9c5f9caa2f0dfef2e657de9fde9e0a12484c
Martin Fick <mfick@codeaurora.org>
2011-11-23 10:30:29 -0700

    Add user pref to enable manual reviewing
    Add a checkbox to the preferences header on the diff
    screen which allows a user to specify whether they
    want manual-reviewing enabled or disabled.  Previously,
    every file was auto marked reviewed when a user first
    displayed it.  The new manual mode prevents this auto
    marking and only marks a file reviewed when the user
    explicitly clicks on the reviewed button.
    
    Using the manual mode allows users to better decide
    when they feel they have reviewed a file.  This mode
    is particularly useful for larger reviews when a
    user may need to jump around and look at many other
    files while commenting on a specific one.  This
    prevents their jumping around from file to file from
    marking the files as reviewed.  The intention is to
    allow the reviewed status to provide more meaning
    than simply "I have seen the file" (which in many
    cases might even be a stretch).
    
    Change-Id: Iaedbcd717ef8a587a78c56e3d1b5f3fcc86a6d21

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/patches/PatchScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/patches/PatchScreen.java
@@ -189,6 +189,12 @@ public abstract class PatchScreen extends Screen implements
   }
 
   private void update(AccountDiffPreference dp) {
+    // Did the user just turn on auto-review?
+    if (!reviewed.getValue() && !dp.isManualReview()) {
+      reviewed.setValue(true);
+      setReviewedByCurrentUser(true);
+    }
+
     if (lastScript != null && canReuse(dp, lastScript)) {
       lastScript.setDiffPrefs(dp);
       RpcStatus.INSTANCE.onRpcStart(null);
@@ -450,10 +456,19 @@ public abstract class PatchScreen extends Screen implements
       bottomNav.display(patchIndex, getPatchScreenType(), fileList);
     }
 
-    // Mark this file reviewed as soon we display the diff screen
-    if (Gerrit.isSignedIn() && isFirst) {
-      reviewed.setValue(true);
-      setReviewedByCurrentUser(true /* reviewed */);
+    if (Gerrit.isSignedIn()) {
+      boolean revd = false;
+      if (isFirst && !prefs.get().isManualReview()) {
+        revd = true;
+        setReviewedByCurrentUser(revd);
+      } else {
+        for (Patch p : patchSetDetail.getPatches()) {
+          if (p.getKey().equals(patchKey)) {
+            revd = p.isReviewedByCurrentUser();
+          }
+        }
+      }
+      reviewed.setValue(revd);
     }
 
     intralineFailure = isFirst && script.hasIntralineFailure();

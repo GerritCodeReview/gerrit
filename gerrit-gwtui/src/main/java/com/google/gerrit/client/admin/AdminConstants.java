ecf4b2a13f228d96c48bfbe925a9261827a69898
Bruce Zu <bruce.zu@sonyericsson.com>
2011-12-14 13:27:43 +0800

    Create project through web interface
    It adds a new panel in the Admin->Projects Screen.It
    enables the users that are allowed to create projects
    via command-line to create them also via web interface.
    
    Bug: issue 203
    Change-Id: I082ab755276ab9d08e17603bc0f69c23566ac405

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/admin/AdminConstants.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/admin/AdminConstants.java
@@ -31,10 +31,13 @@ public interface AdminConstants extends Constants {
   String buttonSaveDescription();
   String buttonRenameGroup();
   String buttonCreateGroup();
+  String buttonCreateProject();
   String buttonChangeGroupOwner();
   String buttonChangeGroupType();
   String buttonSelectGroup();
   String buttonSaveChanges();
+  String checkBoxEmptyCommit();
+  String checkBoxPermissionsOnly();
   String useContentMerge();
   String useContributorAgreements();
   String useSignedOffBy();
@@ -56,6 +59,9 @@ public interface AdminConstants extends Constants {
   String noMembersInfo();
   String headingExternalGroup();
   String headingCreateGroup();
+  String headingCreateProject();
+  String headingParentProjectName();
+  String columnProjectName();
   String headingAgreements();
 
   String projectSubmitType_FAST_FORWARD_ONLY();
@@ -89,6 +95,7 @@ public interface AdminConstants extends Constants {
   String groupTabGeneral();
   String groupTabMembers();
   String projectListTitle();
+  String createProjectTitle();
   String projectAdminTabGeneral();
   String projectAdminTabBranches();
   String projectAdminTabAccess();
--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/project/ProjectAdminServiceImpl.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/project/ProjectAdminServiceImpl.java
@@ -19,9 +19,11 @@ import com.google.gerrit.common.data.ListBranchesResult;
 import com.google.gerrit.common.data.ProjectAccess;
 import com.google.gerrit.common.data.ProjectAdminService;
 import com.google.gerrit.common.data.ProjectDetail;
+import com.google.gerrit.common.data.ProjectList;
 import com.google.gerrit.reviewdb.Branch;
 import com.google.gerrit.reviewdb.Project;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwtjsonrpc.client.VoidResult;
 import com.google.inject.Inject;
 
 import org.eclipse.jgit.lib.ObjectId;
@@ -37,6 +39,7 @@ class ProjectAdminServiceImpl implements ProjectAdminService {
   private final ListBranches.Factory listBranchesFactory;
   private final VisibleProjects.Factory visibleProjectsFactory;
   private final ProjectAccessFactory.Factory projectAccessFactory;
+  private final CreateProjectHandler.Factory createProjectHandlerFactory;
   private final ProjectDetailFactory.Factory projectDetailFactory;
 
   @Inject
@@ -47,7 +50,8 @@ class ProjectAdminServiceImpl implements ProjectAdminService {
       final ListBranches.Factory listBranchesFactory,
       final VisibleProjects.Factory visibleProjectsFactory,
       final ProjectAccessFactory.Factory projectAccessFactory,
-      final ProjectDetailFactory.Factory projectDetailFactory) {
+      final ProjectDetailFactory.Factory projectDetailFactory,
+      final CreateProjectHandler.Factory createNewProjectFactory) {
     this.addBranchFactory = addBranchFactory;
     this.changeProjectAccessFactory = changeProjectAccessFactory;
     this.changeProjectSettingsFactory = changeProjectSettingsFactory;
@@ -56,10 +60,11 @@ class ProjectAdminServiceImpl implements ProjectAdminService {
     this.visibleProjectsFactory = visibleProjectsFactory;
     this.projectAccessFactory = projectAccessFactory;
     this.projectDetailFactory = projectDetailFactory;
+    this.createProjectHandlerFactory = createNewProjectFactory;
   }
 
   @Override
-  public void visibleProjects(final AsyncCallback<List<Project>> callback) {
+  public void visibleProjects(final AsyncCallback<ProjectList> callback) {
     visibleProjectsFactory.create().to(callback);
   }
 
@@ -108,5 +113,13 @@ class ProjectAdminServiceImpl implements ProjectAdminService {
       final AsyncCallback<ListBranchesResult> callback) {
     addBranchFactory.create(projectName, branchName, startingRevision).to(
         callback);
+  }
+
+  @Override
+  public void createNewProject(String projectName, String parentName,
+      boolean emptyCommit, boolean permissionsOnly,
+      AsyncCallback<VoidResult> callback) {
+    createProjectHandlerFactory.create(projectName, parentName, emptyCommit,
+        permissionsOnly).to(callback);
   }
 }

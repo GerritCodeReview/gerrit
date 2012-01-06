2c42a5280af36800049560829bd2d9c0703d9f21
Chris Harris <chris.harris@kitware.com>
2012-01-06 10:22:07 -0500

    Add the ability to run an ref update hook
    Allow the user to provide an update (similar to gits update hook) hook
    run before a push is accepted by gerrit. This allows the exclusion of
    certain commits or any other update checks.
    
    The hook takes the following form:
    
    ref-update --project <project name> --refname <refname> --uploader <uploader>
    --oldrev <sha1> --newrev <sha1>
    
    If the script exits with non zero return code the push will be
    rejected. The output of the script will be returned to the user
    as the reason for the rejection. This hook is called synchronously
    so shouldn't block or wait. A timeout on the hook is set to 30
    seconds to avoid "runaway" hooks using up server threads. This value
    can be configured using "syncHookTimeout" in the "hooks" stanza it
    is an integer value in seconds.
    
    Change-Id: Ibed5dc5c18e59db465511520f76fac93acc561e0

--- gerrit-server/src/main/java/com/google/gerrit/common/ChangeHookRunner.java
+++ gerrit-server/src/main/java/com/google/gerrit/common/ChangeHookRunner.java
@@ -16,6 +16,8 @@ package com.google.gerrit.common;
 
 import com.google.gerrit.common.data.ApprovalType;
 import com.google.gerrit.common.data.ApprovalTypes;
+import com.google.gerrit.lifecycle.LifecycleListener;
+import com.google.gerrit.lifecycle.LifecycleModule;
 import com.google.gerrit.reviewdb.Account;
 import com.google.gerrit.reviewdb.ApprovalCategory;
 import com.google.gerrit.reviewdb.ApprovalCategoryValue;
@@ -42,6 +44,7 @@ import com.google.gerrit.server.events.PatchSetCreatedEvent;
 import com.google.gerrit.server.events.RefUpdatedEvent;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.WorkQueue;
+import com.google.gerrit.server.git.WorkQueue.Executor;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.server.project.ProjectControl;
 import com.google.gerrit.server.project.ProjectState;
@@ -61,24 +64,35 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /** Spawns local executables when a hook action occurs. */
 @Singleton
-public class ChangeHookRunner implements ChangeHooks {
+public class ChangeHookRunner implements ChangeHooks, LifecycleListener {
     /** A logger for this class. */
     private static final Logger log = LoggerFactory.getLogger(ChangeHookRunner.class);
 
-    public static class Module extends AbstractModule {
+    public static class Module extends LifecycleModule {
       @Override
       protected void configure() {
         bind(ChangeHookRunner.class);
         bind(ChangeHooks.class).to(ChangeHookRunner.class);
+        listener().to(ChangeHookRunner.class);
       }
     }
 
@@ -117,6 +131,9 @@ public class ChangeHookRunner implements ChangeHooks {
     /** Filename of the cla signed hook. */
     private final File claSignedHook;
 
+    /** Filename of the update hook. */
+    private final File refUpdateHook;
+
     private final String anonymousCowardName;
 
     /** Repository Manager. */
@@ -132,6 +149,12 @@ public class ChangeHookRunner implements ChangeHooks {
     private final ApprovalTypes approvalTypes;
 
     private final EventFactory eventFactory;
+
+    /** Thread pool used to monitor sync hooks */
+    private final ExecutorService syncHookThreadPool = Executors.newCachedThreadPool();
+
+    /** Timeout value for synchronous hooks */
+    private final int syncHookTimeout;
 
     /**
      * Create a new ChangeHookRunner.
@@ -167,6 +190,8 @@ public class ChangeHookRunner implements ChangeHooks {
         changeRestoredHook = sitePath.resolve(new File(hooksPath, getValue(config, "hooks", "changeRestoredHook", "change-restored")).getPath());
         refUpdatedHook = sitePath.resolve(new File(hooksPath, getValue(config, "hooks", "refUpdatedHook", "ref-updated")).getPath());
         claSignedHook = sitePath.resolve(new File(hooksPath, getValue(config, "hooks", "claSignedHook", "cla-signed")).getPath());
+        refUpdateHook = sitePath.resolve(new File(hooksPath, getValue(config, "hooks", "refUpdateHook", "ref-update")).getPath());
+        syncHookTimeout = config.getInt("hooks", "syncHookTimeout", 30);
     }
 
     public void addChangeListener(ChangeListener listener, IdentifiedUser user) {
@@ -223,6 +248,38 @@ public class ChangeHookRunner implements ChangeHooks {
         }
     }
 
+    /**
+     * Fire the update hook
+     *
+     */
+    public HookResult doRefUpdateHook(final Project project, final String refname,
+        final Account uploader, final ObjectId oldId, final ObjectId newId) {
+
+      final List<String> args = new ArrayList<String>();
+      addArg(args, "--project", project.getName());
+      addArg(args, "--refname", refname);
+      addArg(args, "--uploader", getDisplayName(uploader));
+      addArg(args, "--oldrev", oldId.getName());
+      addArg(args, "--newrev", newId.getName());
+
+      HookResult hookResult = null;
+
+      try {
+        hookResult = runSyncHook(project.getNameKey(), refUpdateHook, args);
+      } catch (TimeoutException e) {
+        hookResult = new HookResult(-1, "Synchronous hook timed out");
+      }
+
+      return hookResult;
+    }
+
+    /**
+     * Fire the Patchset Created Hook.
+     *
+     * @param change The change itself.
+     * @param patchSet The Patchset that was created.
+     * @throws OrmException
+     */
     public void doPatchsetCreatedHook(final Change change, final PatchSet patchSet,
           final ReviewDb db) throws OrmException {
         final PatchSetCreatedEvent event = new PatchSetCreatedEvent();
@@ -455,31 +512,110 @@ public class ChangeHookRunner implements ChangeHooks {
   private synchronized void runHook(Project.NameKey project, File hook,
       List<String> args) {
     if (project != null && hook.exists()) {
-      hookQueue.execute(new HookTask(project, hook, args));
+      hookQueue.execute(new AsyncHookTask(project, hook, args));
     }
   }
 
   private synchronized void runHook(File hook, List<String> args) {
     if (hook.exists()) {
-      hookQueue.execute(new HookTask(null, hook, args));
+      hookQueue.execute(new AsyncHookTask(null, hook, args));
     }
   }
 
-  private final class HookTask implements Runnable {
+  private HookResult runSyncHook(Project.NameKey project,
+      File hook, List<String> args) throws TimeoutException {
+    HookResult result = null;
+    if (hook.exists()) {
+
+      SyncHookTask syncHook = new SyncHookTask(project, hook, args);
+      FutureTask<HookResult> task = new FutureTask<HookResult>(syncHook);
+
+      syncHookThreadPool.execute(task);
+
+      try {
+        result = task.get(syncHookTimeout, TimeUnit.SECONDS);
+      } catch (TimeoutException e) {
+        task.cancel(true);
+        log.error("Synchronous hook timed out "  + hook.getAbsolutePath());
+        result = new HookResult(syncHook.getOutput(), "Synchronous hook timed out");
+      } catch (Exception e) {
+        task.cancel(true);
+        log.error("Error running hook " + hook.getAbsolutePath(), e);
+        result = new HookResult(syncHook.getOutput(), "Error running hook: " + e);
+      }
+    }
+
+    return result;
+  }
+
+  /** Container class used to hold the return code and output of script hook execution */
+  public final class HookResult {
+    private int exitValue = -1;
+    private String output;
+    private String executionError;
+
+    private HookResult(int exitValue, String output) {
+      this.exitValue = exitValue;
+      this.output = output;
+    }
+
+    private HookResult(String output, String executionError) {
+      this.output = output;
+      this.executionError = executionError;
+    }
+
+    public int getExitValue() {
+      return exitValue;
+    }
+
+    public void setExitValue(int exitValue) {
+      this.exitValue = exitValue;
+    }
+
+    public String getOutput() {
+      return output;
+    }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+
+      if(output != null && output.length() != 0) {
+        sb.append(output);
+
+        if(executionError != null) {
+          sb.append(" - ");
+        }
+      }
+
+      if(executionError != null ) {
+        sb.append(executionError);
+      }
+
+      return sb.toString();
+    }
+  }
+
+  private class HookTask {
     private final Project.NameKey project;
     private final File hook;
     private final List<String> args;
+    private StringWriter output;
 
-    private HookTask(Project.NameKey project, File hook, List<String> args) {
+    protected HookTask(Project.NameKey project, File hook, List<String> args) {
       this.project = project;
       this.hook = hook;
       this.args = args;
     }
 
-    @Override
-    public void run() {
+    public String getOutput() {
+      return output != null ? output.toString() : null;
+    }
+
+    protected HookResult runHook() {
       Repository repo = null;
+      HookResult result = null;
       try {
+
         final List<String> argv = new ArrayList<String>(1 + args.size());
         argv.add(hook.getAbsolutePath());
         argv.addAll(args);
@@ -487,9 +623,8 @@ public class ChangeHookRunner implements ChangeHooks {
         final ProcessBuilder pb = new ProcessBuilder(argv);
         pb.redirectErrorStream(true);
 
-        if (project != null) {
+        if(project != null)
           repo = openRepository(project);
-        }
 
         if (repo != null) {
           pb.directory(repo.getDirectory());
@@ -500,21 +635,20 @@ public class ChangeHookRunner implements ChangeHooks {
 
         Process ps = pb.start();
         ps.getOutputStream().close();
-
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(ps.getInputStream()));
+        InputStream is = ps.getInputStream();
+        String output = null;
         try {
-          String line;
-          while ((line = br.readLine()) != null) {
-            log.info("hook[" + hook.getName() + "] output: " + line);
-          }
+          output = readOutput(is);
         } finally {
           try {
-            br.close();
+            is.close();
           } catch (IOException closeErr) {
           }
           ps.waitFor();
+          result = new HookResult(ps.exitValue(), output);
         }
+      } catch (InterruptedException iex) {
+        // InterruptedExeception - timeout
       } catch (Throwable err) {
         log.error("Error running hook " + hook.getAbsolutePath(), err);
       } finally {
@@ -522,11 +656,86 @@ public class ChangeHookRunner implements ChangeHooks {
           repo.close();
         }
       }
+
+      log.info("hook[" + getName() + "] exitValue:" + result.getExitValue());
+
+      BufferedReader br =
+          new BufferedReader(new StringReader(result.getOutput()));
+      try {
+        String line;
+        while ((line = br.readLine()) != null) {
+          log.info("hook[" + getName() + "] output: " + line);
+        }
+      }
+      catch(IOException  iox) {
+        log.error("Error writing hook output", iox);
+      }
+
+      return result;
+    }
+
+    private String readOutput(InputStream is) throws IOException {
+      output = new StringWriter();
+      InputStreamReader input = new InputStreamReader(is);
+      char[] buffer = new char[4096];
+      int n = 0;
+      while ((n = input.read(buffer)) != -1) {
+        output.write(buffer, 0, n);
+      }
+
+      return output.toString();
+    }
+
+    protected String getName() {
+      return hook.getName();
     }
 
     @Override
     public String toString() {
       return "hook " + hook.getName();
     }
+  }
+
+  /** Callable type used to run synchronous hooks */
+  private final class SyncHookTask extends HookTask implements Callable<HookResult> {
+
+    private SyncHookTask( Project.NameKey project, File hook, List<String> args) {
+      super(project, hook, args);
+    }
+
+    @Override
+    public HookResult call() throws Exception {
+      return super.runHook();
+    }
+  }
+
+  /** Runable type used to run async hooks */
+  private final class AsyncHookTask extends HookTask implements Runnable {
+
+    private AsyncHookTask(Project.NameKey project, File hook, List<String> args) {
+      super(project, hook, args);
+    }
+
+    @Override
+    public void run() {
+      super.runHook();
+    }
+  }
+
+  @Override
+  public void start() {
+  }
+
+  @Override
+  public void stop() {
+    syncHookThreadPool.shutdown();
+    boolean isTerminated;
+    do {
+      try {
+        isTerminated = syncHookThreadPool.awaitTermination(10, TimeUnit.SECONDS);
+      } catch (InterruptedException ie) {
+        isTerminated = false;
+      }
+    } while (!isTerminated);
   }
 }

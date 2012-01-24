0e164b439e84e9b8449e88fb8fd65a63d448c740
Conley Owens <cco3@android.com>
2012-01-24 10:59:10 -0800

    Refactor DeleteDraft into gerrit-server
    This change moves logic that was duplicated over the ssh command and the
    rpc handler into a DeleteDraft class in gerrit-server.
    
    Change-Id: I308f5371697776cd210199910fc787fc1220fbf1

--- gerrit-common/src/main/java/com/google/gerrit/common/data/ReviewResult.java
+++ gerrit-common/src/main/java/com/google/gerrit/common/data/ReviewResult.java
@@ -67,7 +67,10 @@ public class ReviewResult {
       PUBLISH_NOT_PERMITTED,
 
       /** Review operation not permitted by rule. */
-      RULE_ERROR
+      RULE_ERROR,
+
+      /** Error writing change to git repository */
+      GIT_ERROR,
     }
 
     protected Type type;

870ed1000c31c7c575baf496a2cbedac32cebd2f
Edwin Kempin <edwin.kempin@sap.com>
2012-02-09 15:47:52 +0100

    Add SSH command to run Git garbage collection
    Add a new SSH command that allows to run the Git garbage collection
    for specific or all Gerrit projects.
    
    The default parameters for the Git garbage collection can be defined
    in the user global Git configuration file of the system user that
    runs the Gerrit server.
    
    It is possible to specify repository specific parameters for the
    garbage collection in the Git configuration files on repository
    level.
    
    All GC runs are logged in a newly added GC log file. This log file
    also contains statistics of the repositories that were garbage
    collected.
    
    Change-Id: Ie8730e3db785d5e05d5b739cfb1ef87ba515e870
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -68,6 +68,7 @@ import com.google.gerrit.server.events.EventFactory;
 import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
 import com.google.gerrit.server.git.ChangeCache;
 import com.google.gerrit.server.git.ChangeMergeQueue;
+import com.google.gerrit.server.git.GarbageCollection;
 import com.google.gerrit.server.git.GitModule;
 import com.google.gerrit.server.git.MergeQueue;
 import com.google.gerrit.server.git.NotesBranchUtil;
@@ -187,6 +188,7 @@ public class GerritGlobalModule extends FactoryModule {
     factory(RebasedPatchSetSender.Factory.class);
     factory(ReplacePatchSetSender.Factory.class);
     factory(PerformCreateProject.Factory.class);
+    factory(GarbageCollection.Factory.class);
     bind(PermissionCollection.Factory.class);
     bind(AccountVisibility.class)
         .toProvider(AccountVisibilityProvider.class)

b077f1cb5f93bccc4b979f348ffaa9423eac446b
Conley Owens <cco3@android.com>
2012-02-29 11:38:23 -0800

    Refactor out approval checking from ReviewCommand
    PublishComments already does some approval verification.  This change
    places the burden of this verification there instead of on
    ReviewCommand.
    
    Change-Id: Iaf4fb7b6c073b3fbaed64a34263f71d8d2d57507

--- gerrit-server/src/main/java/com/google/gerrit/server/patch/PublishComments.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/patch/PublishComments.java
@@ -151,7 +151,8 @@ public class PublishComments implements Callable<VoidResult> {
     db.patchComments().update(drafts);
   }
 
-  private void publishApprovals(ChangeControl ctl) throws OrmException {
+  private void publishApprovals(ChangeControl ctl)
+      throws InvalidChangeOperationException, OrmException {
     ChangeUtil.updated(change);
 
     final Set<ApprovalCategory.Id> dirty = new HashSet<ApprovalCategory.Id>();
@@ -188,11 +189,10 @@ public class PublishComments implements Callable<VoidResult> {
       if (!ApprovalCategory.SUBMIT.equals(a.getCategoryId())) {
         functionState.normalize(types.byId(a.getCategoryId()), a);
       }
-      if (o != a.getValue()) {
-        // Value changed, ensure we update the database.
-        //
-        a.setGranted();
-        dirty.add(a.getCategoryId());
+      if (want.get() != a.getValue()) {
+        throw new InvalidChangeOperationException(
+            types.byId(a.getCategoryId()).getCategory().getLabelName()
+            + "=" + o + " not permitted");
       }
       if (!ins.contains(a)) {
         upd.add(a);

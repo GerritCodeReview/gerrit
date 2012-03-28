dc543e53e8c29dfb9f748cb3b7acdeaf5003c8c8
Edwin Kempin <edwin.kempin@sap.com>
2012-03-28 16:06:49 +0200

    Create index for submodule subscriptions on site upgrade
    Commit d15704079c2f820995ee70e7403dfa7ebcb3d791 introduced a new
    database index for accessing submodule subscriptions, but the
    corresponding database schema Schema_61 did not create the index. As
    result the new index was only created if a new site was
    initialized but not when an existing site was upgraded. This change
    adds a new database schema that takes care to create the missing
    index.
    
    Change-Id: Ib4fa1cf184c91935445ce41f66634cd8a5221a45
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
@@ -32,7 +32,7 @@ import java.util.List;
 /** A version of the database schema. */
 public abstract class SchemaVersion {
   /** The current schema version. */
-  public static final Class<Schema_63> C = Schema_63.class;
+  public static final Class<Schema_64> C = Schema_64.class;
 
   public static class Module extends AbstractModule {
     @Override

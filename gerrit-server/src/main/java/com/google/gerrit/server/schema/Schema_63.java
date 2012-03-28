5a51393792219af51b2285cfdcdb2c84cf5faaa5
Edwin Kempin <edwin.kempin@sap.com>
2012-03-28 16:06:49 +0200

    Create index for submodule subscriptions on site upgrade
    Commit d15704079c2f820995ee70e7403dfa7ebcb3d791 introduced a new
    database index for accessing submodule subscriptions, but the
    corresponding database schema Schema_61 did not create the index. As
    result the new index was only created if a new site was
    initialized but not when an existing site was upgraded. This change
    adds a new database schema that takes care to create the missing
    index.
    
    Change-Id: Ib4fa1cf184c91935445ce41f66634cd8a5221a45
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_63.java
@@ -0,0 +1,42 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.schema;
+
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gwtorm.jdbc.JdbcSchema;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Schema_63 extends SchemaVersion {
+  @Inject
+  Schema_63(Provider<Schema_62> prior) {
+    super(prior);
+  }
+
+  @Override
+  protected void migrateData(final ReviewDb db, final UpdateUI ui)
+      throws SQLException {
+    final Statement stmt = ((JdbcSchema) db).getConnection().createStatement();
+    try {
+      stmt.execute("CREATE INDEX submodule_subscription_access_bySubscription"
+          + " ON submodule_subscriptions (submodule_project_name, submodule_branch_name)");
+    } finally {
+      stmt.close();
+    }
+  }
+}

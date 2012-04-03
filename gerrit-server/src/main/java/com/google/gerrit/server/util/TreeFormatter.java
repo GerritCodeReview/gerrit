ccb8e6740c39543b352317488db28c0b68ad4455
Shawn O. Pearce <sop@google.com>
2012-04-03 16:14:32 -0700

    Refactor ls-projects code to gerrit-server
    Move the implementation to gerrit-server, making it available to
    both the SSH and HTTP environments. Later we will define a new
    REST-ful API over HTTP that supports the same sort of query as
    `ssh gerrit ls-projects`.
    
    Change-Id: Ic8052b4dd3c70809feb4b43e5b2bae6b72b10503

--- gerrit-server/src/main/java/com/google/gerrit/server/util/TreeFormatter.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/util/TreeFormatter.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.sshd.commands;
+package com.google.gerrit.server.util;
 
 import java.io.PrintWriter;
 import java.util.SortedSet;
---
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ListProjectsCommand.java
@@ -0,0 +1,72 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd.commands;
+
+import com.google.gerrit.server.project.ListProjects.FilterType;
+import com.google.gerrit.sshd.BaseCommand;
+import com.google.inject.Inject;
+
+import org.apache.sshd.server.Environment;
+import org.kohsuke.args4j.Option;
+
+import java.util.List;
+
+final class ListProjectsCommand extends BaseCommand {
+  @Inject
+  private com.google.gerrit.server.project.ListProjects impl;
+
+  @Option(name = "--show-branch", aliases = {"-b"}, multiValued = true,
+      usage = "displays the sha of each project in the specified branch")
+  private List<String> showBranch;
+
+  @Option(name = "--tree", aliases = {"-t"}, usage = "displays project inheritance in a tree-like format\n" +
+      "this option does not work together with the show-branch option")
+  void setTree(boolean show) {
+    impl.setShowTree(show);
+  }
+
+  @Option(name = "--type", usage = "type of project")
+  void setType(FilterType type) {
+    impl.setType(type);
+  }
+
+  @Option(name = "--description", aliases = {"-d"}, usage = "include description of project in list")
+  void setDescription(boolean show) {
+    impl.setShowDescription(show);
+  }
+
+  @Option(name = "--all", usage = "display all projects that are accessible by the calling user")
+  void setAll(boolean show) {
+    impl.setShowAll(show);
+  }
+
+  @Override
+  public void start(final Environment env) {
+    startThread(new CommandRunnable() {
+      @Override
+      public void run() throws Exception {
+        parseCommandLine();
+        if (impl.isShowTree() && (showBranch != null)) {
+          throw new UnloggedFailure(1, "fatal: --tree and --show-branch options are not compatible.");
+        }
+        if (impl.isShowTree() && impl.isShowDescription()) {
+          throw new UnloggedFailure(1, "fatal: --tree and --description options are not compatible.");
+        }
+        impl.setShowBranch(showBranch);
+        impl.display(out);
+      }
+    });
+  }
+}

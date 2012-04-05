db3a4e22833eb17319d1ebcc644c3ff95f00331c
Shawn O. Pearce <sop@google.com>
2012-04-05 11:18:50 -0700

    Use REST API for listing projects
    Instead of using the JSON-RPC interface, use the new standard REST
    interface for loading the list of projects. This shares the backend
    implementation with the SSH `gerrit ls-projects` command, and will
    be the long-term supported interface to talk to Gerrit.
    
    Change-Id: If538bbc87410a36ed030721ef6ccda23409d287e

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/account/MyWatchedProjectsScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/account/MyWatchedProjectsScreen.java
@@ -15,6 +15,7 @@
 package com.google.gerrit.client.account;
 
 import com.google.gerrit.client.Gerrit;
+import com.google.gerrit.client.projects.ProjectList;
 import com.google.gerrit.client.rpc.GerritCallback;
 import com.google.gerrit.client.rpc.ScreenLoadCallback;
 import com.google.gerrit.client.ui.HintTextBox;
@@ -22,7 +23,6 @@ import com.google.gerrit.client.ui.ProjectNameSuggestOracle;
 import com.google.gerrit.client.ui.ProjectsTable;
 import com.google.gerrit.common.PageLinks;
 import com.google.gerrit.common.data.AccountProjectWatchInfo;
-import com.google.gerrit.common.data.ProjectList;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
@@ -361,11 +361,10 @@ public class MyWatchedProjectsScreen extends SettingsScreen implements
   }
 
   protected void populateProjects() {
-    Util.PROJECT_SVC.visibleProjects(
-        new GerritCallback<ProjectList>() {
+    ProjectList.all(new GerritCallback<ProjectList>() {
       @Override
       public void onSuccess(final ProjectList result) {
-        projectsTab.display(result.getProjects());
+        projectsTab.display(result);
         if (firstPopupLoad) { // Display was delayed until table was loaded
           firstPopupLoad = false;
           displayPopup();
---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/projects/ProjectInfo.java
@@ -0,0 +1,30 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.projects;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class ProjectInfo extends JavaScriptObject {
+  protected ProjectInfo() {
+  }
+
+  public final Project.NameKey getNameKey() {
+    return new Project.NameKey(getName());
+  }
+
+  public final native String getName() /*-{ return this.name; }-*/;
+  public final native String getDescription() /*-{ return this.description; }-*/;
+}

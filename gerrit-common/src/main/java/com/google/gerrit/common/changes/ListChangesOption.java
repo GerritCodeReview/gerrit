bfb2b6c15a7c5e34ba95d6dde8d6105107433696
Shawn O. Pearce <sop@google.com>
2012-04-07 19:48:25 -0700

    /changes/: Return more data
    Allow more data to be provided in the JSON results from /changes/,
    including all revisions of the change (aka patch sets), commit
    message data, files, and URLs to fetch the revisions with.
    
    Revisions are indexed by the commit SHA-1, as a commit can only
    really appear once per change. The current_revision field points
    to the revision structure that is the most recent for this change.
    
    Within a revision structure the fetch map provides a set of URLs and
    the corresponding reference that should point to the commit. This
    can be used by to fetch the change over the Git protocols. All
    available URL permutations are made by the server rather than the
    client, giving the server more flexibility in the long term to move
    changes around. This should one day support moving closed changes
    out to archive repositories with different reference names.
    
    $ curl -n --digest 'http://127.0.0.1:8080/a/changes/?n=1&format=JSON&o=CURRENT_REVISION&o=CURRENT_COMMIT&o=CURRENT_FILES'
    )]}'
    [
      {
        "project": "tools/repo",
        "branch": "master",
        "id": "I37f758e238c2dbc891a0d876547cb7acc29fc7f7",
        "subject": "test 40",
        "status": "NEW",
        "created": "2012-04-05 22:27:16.820000000",
        "updated": "2012-04-07 01:52:19.759000000",
        "starred": true,
        "_sortkey": "001c36f000000028",
        "_number": 40,
        "owner": {
          "name": "Shawn Pearce"
        },
        "current_revision": "e6b0d98216411f2a0f11fd4a51c0ff4fe1e41b17",
        "revisions": {
          "e6b0d98216411f2a0f11fd4a51c0ff4fe1e41b17": {
            "_number": 1,
            "fetch": {
              "git://localhost/tools/repo": "refs/changes/40/40/1",
              "https://foo/tools/repo": "refs/changes/40/40/1",
              "http://127.0.0.1:8080/tools/repo": "refs/changes/40/40/1",
              "ssh://sop@localhost:29418/tools/repo": "refs/changes/40/40/1",
              "http://sop@127.0.0.1:8080/tools/repo": "refs/changes/40/40/1"
            },
            "commit": {
              "parents": {
                "45bb53f39f4db32afb3458519c93135b30d71690": {
                  "subject": "test 39"
                }
              },
              "author": {
                "name": "Shawn O. Pearce",
                "email": "sop@google.com",
                "date": "2010-06-04 21:46:25.000000000",
                "tz": -420
              },
              "committer": {
                "name": "Shawn O. Pearce",
                "email": "sop@google.com",
                "date": "2010-06-04 21:46:25.000000000",
                "tz": -420
              },
              "message": "test 40\n\nChange-Id: I37f758e238c2dbc891a0d876547cb7acc29fc7f7\n"
            },
            "files": [
              {
                "path": "two",
                "lines_inserted": 1,
                "lines_deleted": 1
              }
            ]
          }
        },
        "_more_changes": true
      }
    ]
    
    Change-Id: I9589cc46b3dfcc43e799fdd40ee8d6c292c4f17b

---
+++ gerrit-common/src/main/java/com/google/gerrit/common/changes/ListChangesOption.java
@@ -0,0 +1,65 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.common.changes;
+
+import java.util.EnumSet;
+
+/** Output options available when using {@code /changes/} RPCs. */
+public enum ListChangesOption {
+  LABELS(0),
+
+  /** Return information on the current patch set of the change. */
+  CURRENT_REVISION(1),
+  ALL_REVISIONS(2),
+
+  /** If revisions are included, parse the commit object. */
+  CURRENT_COMMIT(3),
+  ALL_COMMITS(4),
+
+  /** If a patch set is included, include the files of the patch set. */
+  CURRENT_FILES(4),
+  ALL_FILES(5);
+
+  private final int value;
+
+  private ListChangesOption(int v) {
+    this.value = v;
+  }
+
+  public static EnumSet<ListChangesOption> fromBits(int v) {
+    EnumSet<ListChangesOption> r = EnumSet.noneOf(ListChangesOption.class);
+    for (ListChangesOption o : ListChangesOption.values()) {
+      if ((v & (1 << o.value)) != 0) {
+        r.add(o);
+        v &= ~(1 << o.value);
+      }
+      if (v == 0) {
+        return r;
+      }
+    }
+    if (v != 0) {
+      throw new IllegalArgumentException("unknown " + Integer.toHexString(v));
+    }
+    return r;
+  }
+
+  public static int toBits(EnumSet<ListChangesOption> set) {
+    int r = 0;
+    for (ListChangesOption o : set) {
+      r |= 1 << o.value;
+    }
+    return r;
+  }
+}

fb1c167e458df5a6a90f2088b82671d94749299b
Shawn O. Pearce <sop@google.com>
2012-04-07 19:48:25 -0700

    /changes/: Return more data
    Allow more data to be provided in the JSON results from /changes/,
    including all revisions of the change (aka patch sets), commit
    message data, files, and URLs to fetch the revisions with.
    
    Revisions are indexed by the commit SHA-1, as a commit can only
    really appear once per change. The current_revision field points
    to the revision structure that is the most recent for this change.
    
    Within a revision structure the fetch map provides a set of URLs and
    the corresponding reference that should point to the commit. This
    can be used by to fetch the change over the Git protocols. All
    available URL permutations are made by the server rather than the
    client, giving the server more flexibility in the long term to move
    changes around. This should one day support moving closed changes
    out to archive repositories with different reference names.
    
    $ curl -n --digest 'http://127.0.0.1:8080/a/changes/?n=1&format=JSON&o=CURRENT_REVISION&o=CURRENT_COMMIT&o=CURRENT_FILES'
    )]}'
    [
      {
        "project": "tools/repo",
        "branch": "master",
        "id": "I37f758e238c2dbc891a0d876547cb7acc29fc7f7",
        "subject": "test 40",
        "status": "NEW",
        "created": "2012-04-05 22:27:16.820000000",
        "updated": "2012-04-07 01:52:19.759000000",
        "starred": true,
        "_sortkey": "001c36f000000028",
        "_number": 40,
        "owner": {
          "name": "Shawn Pearce"
        },
        "current_revision": "e6b0d98216411f2a0f11fd4a51c0ff4fe1e41b17",
        "revisions": {
          "e6b0d98216411f2a0f11fd4a51c0ff4fe1e41b17": {
            "_number": 1,
            "fetch": {
              "git://localhost/tools/repo": "refs/changes/40/40/1",
              "https://foo/tools/repo": "refs/changes/40/40/1",
              "http://127.0.0.1:8080/tools/repo": "refs/changes/40/40/1",
              "ssh://sop@localhost:29418/tools/repo": "refs/changes/40/40/1",
              "http://sop@127.0.0.1:8080/tools/repo": "refs/changes/40/40/1"
            },
            "commit": {
              "parents": {
                "45bb53f39f4db32afb3458519c93135b30d71690": {
                  "subject": "test 39"
                }
              },
              "author": {
                "name": "Shawn O. Pearce",
                "email": "sop@google.com",
                "date": "2010-06-04 21:46:25.000000000",
                "tz": -420
              },
              "committer": {
                "name": "Shawn O. Pearce",
                "email": "sop@google.com",
                "date": "2010-06-04 21:46:25.000000000",
                "tz": -420
              },
              "message": "test 40\n\nChange-Id: I37f758e238c2dbc891a0d876547cb7acc29fc7f7\n"
            },
            "files": [
              {
                "path": "two",
                "lines_inserted": 1,
                "lines_deleted": 1
              }
            ]
          }
        },
        "_more_changes": true
      }
    ]
    
    Change-Id: I9589cc46b3dfcc43e799fdd40ee8d6c292c4f17b

--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/ListChanges.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/ListChanges.java
@@ -14,43 +14,103 @@
 
 package com.google.gerrit.server.query.change;
 
+import static com.google.gerrit.common.changes.ListChangesOption.ALL_COMMITS;
+import static com.google.gerrit.common.changes.ListChangesOption.ALL_FILES;
+import static com.google.gerrit.common.changes.ListChangesOption.ALL_REVISIONS;
+import static com.google.gerrit.common.changes.ListChangesOption.CURRENT_COMMIT;
+import static com.google.gerrit.common.changes.ListChangesOption.CURRENT_FILES;
+import static com.google.gerrit.common.changes.ListChangesOption.CURRENT_REVISION;
+import static com.google.gerrit.common.changes.ListChangesOption.LABELS;
+
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.gerrit.common.changes.ListChangesOption;
 import com.google.gerrit.common.data.SubmitRecord;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.Patch;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.PatchSetInfo;
+import com.google.gerrit.reviewdb.client.PatchSetInfo.ParentInfo;
+import com.google.gerrit.reviewdb.client.UserIdentity;
 import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.AnonymousUser;
 import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.OutputFormat;
 import com.google.gerrit.server.account.AccountCache;
 import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.config.CanonicalWebUrl;
+import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.events.AccountAttribute;
+import com.google.gerrit.server.patch.PatchList;
+import com.google.gerrit.server.patch.PatchListCache;
+import com.google.gerrit.server.patch.PatchListEntry;
+import com.google.gerrit.server.patch.PatchSetInfoFactory;
+import com.google.gerrit.server.patch.PatchSetInfoNotAvailableException;
 import com.google.gerrit.server.project.ChangeControl;
 import com.google.gerrit.server.project.NoSuchChangeException;
 import com.google.gerrit.server.query.QueryParseException;
+import com.google.gerrit.server.ssh.SshInfo;
 import com.google.gson.reflect.TypeToken;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.Singleton;
 
+import com.jcraft.jsch.HostKey;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.transport.URIish;
 import org.kohsuke.args4j.Option;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.net.URISyntaxException;
 import java.sql.Timestamp;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 
 public class ListChanges {
+  @Singleton
+  static class Urls {
+    final String git;
+    final String http;
+
+    @Inject
+    Urls(@GerritServerConfig Config cfg) {
+      this.git = ensureSlash(cfg.getString("gerrit", null, "canonicalGitUrl"));
+      this.http = ensureSlash(cfg.getString("gerrit", null, "gitHttpUrl"));
+    }
+
+    private static String ensureSlash(String in) {
+      if (in != null && !in.endsWith("/")) {
+        return in + "/";
+      }
+      return in;
+    }
+  }
+
   private final QueryProcessor imp;
   private final Provider<ReviewDb> db;
   private final AccountCache accountCache;
   private final CurrentUser user;
+  private final AnonymousUser anonymous;
   private final ChangeControl.Factory changeControlFactory;
+  private final PatchSetInfoFactory patchSetInfoFactory;
+  private final PatchListCache patchListCache;
+  private final SshInfo sshInfo;
+  private final Provider<String> urlProvider;
+  private final Urls urls;
   private boolean reverse;
   private String defaultField;
+  private Map<Account.Id, AccountAttribute> accounts = Maps.newHashMap();
+  private EnumSet<ListChangesOption> options =
+      EnumSet.noneOf(ListChangesOption.class);
 
   @Option(name = "--format", metaVar = "FMT", usage = "Output display format")
   private OutputFormat format = OutputFormat.TEXT;
@@ -61,6 +121,16 @@ public class ListChanges {
   @Option(name = "--limit", aliases = {"-n"}, metaVar = "CNT", usage = "Maximum number of results to return")
   void setLimit(int limit) {
     imp.setLimit(limit);
+  }
+
+  @Option(name = "-o", multiValued = true, usage = "Output options per change")
+  void addOption(ListChangesOption o) {
+    options.add(o);
+  }
+
+  @Option(name = "-O", usage = "Output option flags, in hex")
+  void setOptionFlagsHex(String hex) {
+    options.addAll(ListChangesOption.fromBits(Integer.parseInt(hex, 16)));
   }
 
   @Option(name = "-P", metaVar = "SORTKEY", usage = "Previous changes before SORTKEY")
@@ -81,13 +151,28 @@ public class ListChanges {
   }
 
   @Inject
-  ListChanges(QueryProcessor qp, Provider<ReviewDb> db, AccountCache ac,
-      CurrentUser u, ChangeControl.Factory cf) {
+  ListChanges(QueryProcessor qp,
+      Provider<ReviewDb> db,
+      AccountCache ac,
+      CurrentUser u,
+      AnonymousUser au,
+      ChangeControl.Factory cf,
+      PatchSetInfoFactory psi,
+      PatchListCache plc,
+      SshInfo sshInfo,
+      @CanonicalWebUrl Provider<String> curl,
+      Urls urls) {
     this.imp = qp;
     this.db = db;
     this.accountCache = ac;
     this.user = u;
+    this.anonymous = au;
     this.changeControlFactory = cf;
+    this.patchSetInfoFactory = psi;
+    this.patchListCache = plc;
+    this.sshInfo = sshInfo;
+    this.urlProvider = curl;
+    this.urls = urls;
   }
 
   public OutputFormat getFormat() {
@@ -181,27 +266,43 @@ public class ListChanges {
     out.subject = in.getSubject();
     out.status = in.getStatus();
     out.owner = asAccountAttribute(in.getOwner());
+    out.created = in.getCreatedOn();
     out.updated = in.getLastUpdatedOn();
     out._number = in.getId().get();
     out._sortkey = in.getSortKey();
     out.starred = user.getStarredChanges().contains(in.getId()) ? true : null;
-    out.labels = true ? labelsFor(cd) : null;
+    out.labels = options.contains(LABELS) ? labelsFor(cd) : null;
+
+    if (options.contains(ALL_REVISIONS) || options.contains(CURRENT_REVISION)) {
+      out.revisions = revisions(cd);
+      for (String commit : out.revisions.keySet()) {
+        if (out.revisions.get(commit).isCurrent) {
+          out.current_revision = commit;
+          break;
+        }
+      }
+    }
+
     return out;
   }
 
   private AccountAttribute asAccountAttribute(Account.Id user) {
     if (user == null) {
       return null;
+    } else if (accounts.containsKey(user)) {
+      return accounts.get(user);
     }
 
     AccountState state = accountCache.get(user);
     String name = state.getAccount().getFullName();
     if (Strings.isNullOrEmpty(name)) {
+      accounts.put(user, null);
       return null;
     }
 
     AccountAttribute a = new AccountAttribute();
     a.name = name;
+    accounts.put(user, a);
     return a;
   }
 
@@ -238,6 +339,152 @@ public class ListChanges {
     return labels;
   }
 
+  private Map<String, RevisionInfo> revisions(ChangeData cd) throws OrmException {
+    ChangeControl ctl = cd.changeControl();
+    if (ctl == null || ctl.getCurrentUser() != user) {
+      try {
+        ctl = changeControlFactory.controlFor(cd.change(db));
+      } catch (NoSuchChangeException e) {
+        return Collections.emptyMap();
+      }
+    }
+
+    Collection<PatchSet> src;
+    if (options.contains(ALL_REVISIONS)) {
+      src = cd.patches(db);
+    } else {
+      src = Collections.singletonList(cd.currentPatchSet(db));
+    }
+    Map<String, RevisionInfo> res = Maps.newLinkedHashMap();
+    for (PatchSet in : src) {
+      if (ctl.isPatchVisible(in, db.get())) {
+        res.put(in.getRevision().get(), toRevisionInfo(cd, in));
+      }
+    }
+    return res;
+  }
+
+  private RevisionInfo toRevisionInfo(ChangeData cd, PatchSet in)
+      throws OrmException {
+    RevisionInfo out = new RevisionInfo();
+    out.isCurrent = in.getId().equals(cd.change(db).currentPatchSetId());
+    out._number = in.getId().get();
+    out.draft = in.isDraft() ? true : null;
+    out.fetch = makeFetchMap(cd, in);
+
+    if (options.contains(ALL_COMMITS)
+        || (out.isCurrent && options.contains(CURRENT_COMMIT))) {
+      try {
+        PatchSetInfo info = patchSetInfoFactory.get(db.get(), in.getId());
+        out.commit = new CommitInfo();
+        out.commit.parents = Maps.newLinkedHashMap();
+        out.commit.author = toGitPerson(info.getAuthor());
+        out.commit.committer = toGitPerson(info.getCommitter());
+        out.commit.message = info.getMessage();
+
+        for (ParentInfo p1 : info.getParents()) {
+          CommitInfo p2 = new CommitInfo();
+          p2.subject = p1.shortMessage;
+          out.commit.parents.put(p1.id.get(), p2);
+        }
+      } catch (PatchSetInfoNotAvailableException e) {
+      }
+    }
+
+    if (options.contains(ALL_FILES)
+        || (out.isCurrent && options.contains(CURRENT_FILES))) {
+      PatchList list = patchListCache.get(cd.change(db), in);
+      if (list != null) {
+        out.files = Lists.newArrayListWithCapacity(list.getPatches().size());
+        for (PatchListEntry e : list.getPatches()) {
+          if (Patch.COMMIT_MSG.equals(e.getNewName())) {
+            continue;
+          }
+          FileInfo o = new FileInfo();
+          o.status = e.getChangeType() != Patch.ChangeType.MODIFIED
+              ? e.getChangeType().getCode()
+              : null;
+          if (e.getOldName() == null && e.getNewName() != null) {
+            o.path = e.getNewName();
+          } else {
+            o.oldPath = e.getOldName();
+            o.newPath = e.getNewName();
+          }
+          if (e.getPatchType() == Patch.PatchType.BINARY) {
+            o.binary = true;
+          } else {
+            o.linesInserted = e.getInsertions();
+            o.linesDeleted = e.getDeletions();
+          }
+          out.files.add(o);
+        }
+      }
+    }
+    return out;
+  }
+
+  private Map<String, String> makeFetchMap(ChangeData cd, PatchSet in) throws OrmException {
+    Map<String, String> r = Maps.newLinkedHashMap();
+    boolean anon = false;
+    ChangeControl ctl = cd.changeControl();
+    if (ctl == null || ctl.getCurrentUser() != user) {
+      try {
+        ctl = changeControlFactory.controlFor(cd.change(db));
+      } catch (NoSuchChangeException e) {
+        ctl = null;
+      }
+    }
+    if (ctl != null && ctl.forUser(anonymous).isPatchVisible(in, db.get())) {
+      anon = true;
+    }
+
+    if (anon && urls.git != null) {
+      r.put(urls.git + cd.change(db).getProject().get(), in.getRefName());
+    }
+    if (anon && urls.http != null) {
+      r.put(urls.http + cd.change(db).getProject().get(), in.getRefName());
+    }
+
+    String http = urlProvider.get();
+    if (anon && !Strings.isNullOrEmpty(http)) {
+      r.put(http + cd.change(db).getProject().get(), in.getRefName());
+    }
+
+    if (user instanceof IdentifiedUser) {
+      String username = ((IdentifiedUser)user).getUserName();
+      if (!Strings.isNullOrEmpty(username)) {
+        if (!sshInfo.getHostKeys().isEmpty()) {
+          HostKey host = sshInfo.getHostKeys().get(0);
+          r.put(String.format(
+              "ssh://%s@%s/%s",
+              username, host.getHost(), cd.change(db).getProject().get()),
+              in.getRefName());
+        }
+
+        if (!Strings.isNullOrEmpty(http)) {
+          try {
+            r.put(new URIish(http + cd.change(db).getProject().get())
+                  .setUser(username)
+                  .toPrivateString(),
+                in.getRefName());
+          } catch (URISyntaxException e) {
+          }
+        }
+      }
+    }
+
+    return r;
+  }
+
+  private static GitPerson toGitPerson(UserIdentity committer) {
+    GitPerson p = new GitPerson();
+    p.name = committer.getName();
+    p.email = committer.getEmail();
+    p.date = committer.getDate();
+    p.tz = committer.getTimeZone();
+    return p;
+  }
+
   static class ChangeInfo {
     String project;
     String branch;
@@ -245,14 +492,53 @@ public class ListChanges {
     String id;
     String subject;
     Change.Status status;
-    AccountAttribute owner;
+    Timestamp created;
     Timestamp updated;
     Boolean starred;
-    Map<String, LabelInfo> labels;
 
     String _sortkey;
     int _number;
+
+    AccountAttribute owner;
+    Map<String, LabelInfo> labels;
+    String current_revision;
+    Map<String, RevisionInfo> revisions;
+
     Boolean _moreChanges;
+  }
+
+  static class RevisionInfo {
+    private transient boolean isCurrent;
+    Boolean draft;
+    int _number;
+    Map<String, String> fetch;
+    CommitInfo commit;
+    List<FileInfo> files;
+  }
+
+  static class GitPerson {
+    String name;
+    String email;
+    Timestamp date;
+    int tz;
+  }
+
+  static class CommitInfo {
+    Map<String, CommitInfo> parents;
+    GitPerson author;
+    GitPerson committer;
+    String subject;
+    String message;
+  }
+
+  static class FileInfo {
+    Character status;
+    Boolean binary;
+    String path;
+    String oldPath;
+    String newPath;
+    Integer linesInserted;
+    Integer linesDeleted;
   }
 
   static class LabelInfo {

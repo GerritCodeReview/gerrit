090c44c6c32b16a2b52ec074baff0f7b2c9f4420
Shawn O. Pearce <sop@google.com>
2012-04-13 12:47:03 -0700

    REST API /changes/: implement server
    Implement a new JSON based REST API to query for changes that are
    visible to the user, and replace the current query RPCs with this
    new REST based implementation that is built on QueryProcessor.
    
    The eventual goal of this new endpoint is to provide all of the change
    information needed to show search results, a user's dashboard, or the
    change detail page describing a change. Patch level information to
    view a file's actual lines of code will probably be deferred to a new
    endpoint that has not yet been written.
    
    The query string is taken from the "q" query parameter, for example,
    this URL shows "tools/repo status:open", matching any open change
    whose project name contains the string "tools/repo":
    
    $ curl 'http://localhost:8080/changes/?q=tools/repo&n=2&format=JSON'
    )]}'
    [
      {
        "project": "tools/repo",
        "id": "I37f758e238c2dbc891a0d876547cb7acc29fc7f7",
      },
      {
        "project": "tools/repo",
        "id": "I340b5a3aab999850acf43856a46671f02ef9ae22",
      }
    ]
    
    More complex queries can be passed in the query parameter:
    
      /changes/?q=status:open+is:watched&n=2&format=JSON
    
    Change output is sorted by the last update time, most recently
    updated to oldest update.
    
    If the n query parameter is supplied and additional changes exist
    that match the query beyond the end, the last change object has a
    "_more_changes: true" JSON field set. Callers can resume a query
    with the N query parameter, supplying the last change's _sortkey
    field as the value. When going in the reverse direction with the
    P query parameter puts "_more_changes: true" in the first change
    object if there are results *before* the first change returned.
    
    Change-Id: Ib9378b1b138ad923f7b6699ccfad797e5c8606ae

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/change/ListChangesServlet.java
@@ -0,0 +1,83 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd.rpc.change;
+
+import com.google.gerrit.httpd.RestApiServlet;
+import com.google.gerrit.server.OutputFormat;
+import com.google.gerrit.server.query.QueryParseException;
+import com.google.gerrit.server.query.change.ListChanges;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Singleton
+public class ListChangesServlet extends RestApiServlet {
+  private static final long serialVersionUID = 1L;
+  private static final Logger log = LoggerFactory.getLogger(ListChangesServlet.class);
+  private final ParameterParser paramParser;
+  private final Provider<ListChanges> factory;
+
+  @Inject
+  ListChangesServlet(ParameterParser paramParser, Provider<ListChanges> ls) {
+    this.paramParser = paramParser;
+    this.factory = ls;
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse res)
+      throws IOException {
+    ListChanges impl = factory.get();
+    if (acceptsJson(req)) {
+      impl.setFormat(OutputFormat.JSON_COMPACT);
+    }
+    if (paramParser.parse(impl, req, res)) {
+      ByteArrayOutputStream buf = new ByteArrayOutputStream();
+      if (impl.getFormat().isJson()) {
+        buf.write(JSON_MAGIC);
+      }
+
+      Writer out = new BufferedWriter(new OutputStreamWriter(buf, "UTF-8"));
+      try {
+        impl.query(out);
+      } catch (QueryParseException e) {
+        res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        sendText(req, res, e.getMessage());
+        return;
+      } catch (OrmException e) {
+        log.error("Error querying /changes/", e);
+        res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        return;
+      }
+      out.flush();
+
+      res.setContentType(impl.getFormat().isJson() ? JSON_TYPE : "text/plain");
+      res.setCharacterEncoding("UTF-8");
+      send(req, res, buf.toByteArray());
+    }
+  }
+}

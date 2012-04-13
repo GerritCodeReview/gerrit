cccc31529b5f19e4d72e9a7085813f914f590724
Shawn O. Pearce <sop@google.com>
2012-04-13 12:47:03 -0700

    REST API /changes/: implement server
    Implement a new JSON based REST API to query for changes that are
    visible to the user, and replace the current query RPCs with this
    new REST based implementation that is built on QueryProcessor.
    
    The eventual goal of this new endpoint is to provide all of the change
    information needed to show search results, a user's dashboard, or the
    change detail page describing a change. Patch level information to
    view a file's actual lines of code will probably be deferred to a new
    endpoint that has not yet been written.
    
    The query string is taken from the "q" query parameter, for example,
    this URL shows "tools/repo status:open", matching any open change
    whose project name contains the string "tools/repo":
    
    $ curl 'http://localhost:8080/changes/?q=tools/repo&n=2&format=JSON'
    )]}'
    [
      {
        "project": "tools/repo",
        "id": "I37f758e238c2dbc891a0d876547cb7acc29fc7f7",
      },
      {
        "project": "tools/repo",
        "id": "I340b5a3aab999850acf43856a46671f02ef9ae22",
      }
    ]
    
    More complex queries can be passed in the query parameter:
    
      /changes/?q=status:open+is:watched&n=2&format=JSON
    
    Change output is sorted by the last update time, most recently
    updated to oldest update.
    
    If the n query parameter is supplied and additional changes exist
    that match the query beyond the end, the last change object has a
    "_more_changes: true" JSON field set. Callers can resume a query
    with the N query parameter, supplying the last change's _sortkey
    field as the value. When going in the reverse direction with the
    P query parameter puts "_more_changes: true" in the first change
    object if there are results *before* the first change returned.
    
    Change-Id: Ib9378b1b138ad923f7b6699ccfad797e5c8606ae

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/ListChanges.java
@@ -0,0 +1,296 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.change;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.gerrit.common.data.ApprovalType;
+import com.google.gerrit.common.data.ApprovalTypes;
+import com.google.gerrit.common.data.SubmitRecord;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.PatchSetApproval;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.OutputFormat;
+import com.google.gerrit.server.account.AccountCache;
+import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.events.AccountAttribute;
+import com.google.gerrit.server.project.ChangeControl;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.gerrit.server.query.QueryParseException;
+import com.google.gson.reflect.TypeToken;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import org.kohsuke.args4j.Option;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+
+public class ListChanges {
+  private final QueryProcessor imp;
+  private final Provider<ReviewDb> db;
+  private final AccountCache accountCache;
+  private final ApprovalTypes approvalTypes;
+  private final CurrentUser user;
+  private final ChangeControl.Factory changeControlFactory;
+  private boolean reverse;
+  private Map<Account.Id, AccountAttribute> accounts;
+
+  @Option(name = "--format", metaVar = "FMT", usage = "Output display format")
+  private OutputFormat format = OutputFormat.TEXT;
+
+  @Option(name = "--query", aliases = {"-q"}, metaVar = "QUERY", usage = "Query string")
+  private String query = "status:open";
+
+  @Option(name = "--limit", aliases = {"-n"}, metaVar = "CNT", usage = "Maximum number of results to return")
+  void setLimit(int limit) {
+    imp.setLimit(limit);
+  }
+
+  @Option(name = "-P", metaVar = "SORTKEY", usage = "Previous changes before SORTKEY")
+  void setSortKeyAfter(String key) {
+    // Querying for the prior page of changes requires sortkey_after predicate.
+    // Changes are shown most recent->least recent. The previous page of
+    // results contains changes that were updated after the given key.
+    imp.setSortkeyAfter(key);
+    reverse = true;
+  }
+
+  @Option(name = "-N", metaVar = "SORTKEY", usage = "Next changes after SORTKEY")
+  void setSortKeyBefore(String key) {
+    // Querying for the next page of changes requires sortkey_before predicate.
+    // Changes are shown most recent->least recent. The next page contains
+    // changes that were updated before the given key.
+    imp.setSortkeyBefore(key);
+  }
+
+  @Inject
+  ListChanges(QueryProcessor qp,
+      Provider<ReviewDb> db,
+      AccountCache ac,
+      ApprovalTypes at,
+      CurrentUser u,
+      ChangeControl.Factory cf) {
+    this.imp = qp;
+    this.db = db;
+    this.accountCache = ac;
+    this.approvalTypes = at;
+    this.user = u;
+    this.changeControlFactory = cf;
+
+    accounts = Maps.newHashMap();
+  }
+
+  public OutputFormat getFormat() {
+    return format;
+  }
+
+  public ListChanges setFormat(OutputFormat fmt) {
+    this.format = fmt;
+    return this;
+  }
+
+  public void query(Writer out)
+      throws OrmException, QueryParseException, IOException {
+    if (imp.isDisabled()) {
+      throw new QueryParseException("query disabled");
+    }
+
+    List<ChangeData> changes = imp.queryChanges(query);
+    boolean moreChanges = imp.getLimit() > 0 && changes.size() > imp.getLimit();
+    if (moreChanges) {
+      if (reverse) {
+        changes = changes.subList(1, changes.size());
+      } else {
+        changes = changes.subList(0, imp.getLimit());
+      }
+    }
+
+    List<ChangeInfo> info = Lists.newArrayListWithCapacity(changes.size());
+    for (ChangeData cd : changes) {
+      info.add(toChangeInfo(cd));
+    }
+    if (moreChanges && !info.isEmpty()) {
+      if (reverse) {
+        info.get(0)._moreChanges = true;
+      } else {
+        info.get(info.size() - 1)._moreChanges = true;
+      }
+    }
+
+    if (format.isJson()) {
+      format.newGson().toJson(
+          info,
+          new TypeToken<List<ChangeInfo>>() {}.getType(),
+          out);
+      out.write('\n');
+    } else {
+      for (ChangeInfo c : info) {
+        String id = new Change.Key(c.id).abbreviate();
+        String subject = c.subject;
+        if (subject.length() + id.length() > 80) {
+          subject = subject.substring(0, 80 - id.length());
+        }
+        out.write(id);
+        out.write(' ');
+        out.write(subject.replace('\n', ' '));
+        out.write('\n');
+      }
+    }
+  }
+
+  private ChangeInfo toChangeInfo(ChangeData cd) throws OrmException {
+    ChangeInfo out = new ChangeInfo();
+    Change in = cd.change(db);
+    out.project = in.getProject().get();
+    out.branch = in.getDest().getShortName();
+    out.topic = in.getTopic();
+    out.id = in.getKey().get();
+    out.subject = in.getSubject();
+    out.status = in.getStatus();
+    out.owner = asAccountAttribute(in.getOwner());
+    out.created = in.getCreatedOn();
+    out.updated = in.getLastUpdatedOn();
+    out._number = in.getId().get();
+    out._sortkey = in.getSortKey();
+    out.starred = user.getStarredChanges().contains(in.getId()) ? true : null;
+    out.labels = labelsFor(cd);
+    return out;
+  }
+
+  private AccountAttribute asAccountAttribute(Account.Id user) {
+    if (user == null) {
+      return null;
+    } else if (accounts.containsKey(user)) {
+      return accounts.get(user);
+    }
+
+    AccountState state = accountCache.get(user);
+    String name = state.getAccount().getFullName();
+    if (Strings.isNullOrEmpty(name)) {
+      accounts.put(user, null);
+      return null;
+    }
+
+    AccountAttribute a = new AccountAttribute();
+    a.name = name;
+    accounts.put(user, a);
+    return a;
+  }
+
+  private Map<String, LabelInfo> labelsFor(ChangeData cd) throws OrmException {
+    Change in = cd.change(db);
+    ChangeControl ctl = cd.changeControl();
+    if (ctl == null || ctl.getCurrentUser() != user) {
+      try {
+        ctl = changeControlFactory.controlFor(in);
+      } catch (NoSuchChangeException e) {
+        return null;
+      }
+    }
+
+    PatchSet.Id ps = in.currentPatchSetId();
+    Map<String, LabelInfo> labels = Maps.newLinkedHashMap();
+    for (SubmitRecord rec : ctl.canSubmit(db.get(), ps)) {
+      if (rec.labels == null) {
+        continue;
+      }
+      for (SubmitRecord.Label r : rec.labels) {
+        LabelInfo p = labels.get(r.label);
+        if (p == null || p._status.compareTo(r.status) < 0) {
+          LabelInfo n = new LabelInfo();
+          n._status = r.status;
+          switch (r.status) {
+            case OK:
+              n.approved = asAccountAttribute(r.appliedBy);
+              break;
+            case REJECT:
+              n.rejected = asAccountAttribute(r.appliedBy);
+              break;
+          }
+          labels.put(r.label, n);
+        }
+      }
+    }
+
+    List<PatchSetApproval> approvals = null;
+    for (Map.Entry<String, LabelInfo> e : labels.entrySet()) {
+      if (e.getValue().approved != null || e.getValue().rejected != null) {
+        continue;
+      }
+
+      ApprovalType type = approvalTypes.byLabel(e.getKey());
+      if (type == null || type.getMin() == null || type.getMax() == null) {
+        // Unknown or misconfigured type can't have intermediate scores.
+        continue;
+      }
+
+      short min = type.getMin().getValue();
+      short max = type.getMax().getValue();
+      if (-1 <= min && max <= 1) {
+        // Types with a range of -1..+1 can't have intermediate scores.
+        continue;
+      }
+
+      if (approvals == null) {
+        approvals = db.get().patchSetApprovals().byPatchSet(ps).toList();
+      }
+      for (PatchSetApproval psa : approvals) {
+        short val = psa.getValue();
+        if (val != 0 && min < val && val < max
+            && psa.getCategoryId().equals(type.getCategory().getId())) {
+          e.getValue().tagged = asAccountAttribute(psa.getAccountId());
+          e.getValue().value = val != 1 ? val : null;
+        }
+      }
+    }
+    return labels;
+  }
+
+  static class ChangeInfo {
+    String project;
+    String branch;
+    String topic;
+    String id;
+    String subject;
+    Change.Status status;
+    Timestamp created;
+    Timestamp updated;
+    Boolean starred;
+
+    String _sortkey;
+    int _number;
+
+    AccountAttribute owner;
+    Map<String, LabelInfo> labels;
+    Boolean _moreChanges;
+  }
+
+  static class LabelInfo {
+    transient SubmitRecord.Label.Status _status;
+    AccountAttribute approved;
+    AccountAttribute rejected;
+    AccountAttribute tagged;
+    Short value;
+  }
+}

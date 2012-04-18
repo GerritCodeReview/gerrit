8dbb281d03157df03226ff6199cff3548ad4eb4e
Colby Ranger <cranger@google.com>
2012-04-18 16:16:30 -0700

    Migrate contributor agreements to All-Projects.
    Added a new schema to move the ContributorAgreement, AccountAgreement,
    and AccountGroupAgreement information into the All-Projects
    project.config.
    
    Updated VersionedMetaData to expose a way to do batch commits to the
    meta data.
    
    Change-Id: Ifb3fdd8c0d6a0c988941d6949f6501b8f8856412

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
@@ -32,7 +32,7 @@ import java.util.List;
 /** A version of the database schema. */
 public abstract class SchemaVersion {
   /** The current schema version. */
-  public static final Class<Schema_64> C = Schema_64.class;
+  public static final Class<Schema_65> C = Schema_65.class;
 
   public static class Module extends AbstractModule {
     @Override

e18c5ddf2a808a2d25133c589f216d088cef57e3
Dave Borowitz <dborowitz@google.com>
2012-04-26 16:23:35 -0700

    Add abstract methods to the ChangeEvent hierarchy
    There is some common information in the ChangeEvent hierarchy, such as
    the type string, the change, and the project, that is not expressed in
    the old POJO hierarchy. Add a few convenience methods that can be used
    if the user has just a ChangeObject, but which do not affect the JSON
    serialization.
    
    Change-Id: I22aadf76835528a63743bd758b5d6868dad212ed

--- gerrit-server/src/main/java/com/google/gerrit/server/events/PatchSetCreatedEvent.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/events/PatchSetCreatedEvent.java
@@ -14,9 +14,34 @@
 
 package com.google.gerrit.server.events;
 
+import static org.eclipse.jgit.lib.Constants.R_HEADS;
+
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.Project;
+
 public class PatchSetCreatedEvent extends ChangeEvent {
-    public final String type = "patchset-created";
-    public ChangeAttribute change;
-    public PatchSetAttribute patchSet;
-    public AccountAttribute uploader;
+  public final String type = "patchset-created";
+  public ChangeAttribute change;
+  public PatchSetAttribute patchSet;
+  public AccountAttribute uploader;
+
+  @Override
+  public String getType() {
+    return type;
+  }
+
+  @Override
+  public Project.NameKey getProjectNameKey() {
+    return new Project.NameKey(change.project);
+  }
+
+  @Override
+  public Change.Key getChangeKey() {
+    return new Change.Key(change.id);
+  }
+
+  @Override
+  public String getRefName() {
+    return R_HEADS + change.branch;
+  }
 }

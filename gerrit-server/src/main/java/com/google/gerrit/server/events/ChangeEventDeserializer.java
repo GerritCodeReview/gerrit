d54d498b1b3429f5fecdecbef702c9a0ca1512fb
Dave Borowitz <dborowitz@google.com>
2012-04-28 15:26:47 -0700

    Add a custom JSON deserializer for ChangeEvents
    Change-Id: I11f6a7f2234f914a33ab83f3a44adce71dfc7e59

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/events/ChangeEventDeserializer.java
@@ -0,0 +1,59 @@
+// Copyright (C) 2010 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.events;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+/**
+ * JSON deserializer for {@link ChangeEvent}s.
+ * <p>
+ * Deserialized objects are of an appropriate subclass based on the value of the
+ * top-level "type" element.
+ */
+public class ChangeEventDeserializer implements JsonDeserializer<ChangeEvent> {
+  @Override
+  public ChangeEvent deserialize(JsonElement json, Type typeOfT,
+      JsonDeserializationContext context) throws JsonParseException {
+    if (!json.isJsonObject()) {
+      throw new JsonParseException("Not an object");
+    }
+    JsonElement typeJson = json.getAsJsonObject().get("type");
+    if (typeJson == null || !typeJson.isJsonPrimitive()
+        || !typeJson.getAsJsonPrimitive().isString()) {
+      throw new JsonParseException("Type is not a string: " + typeJson);
+    }
+    String type = typeJson.getAsJsonPrimitive().getAsString();
+    if (type.equals(ChangeAbandonedEvent.type)) {
+      return context.deserialize(json, ChangeAbandonedEvent.class);
+    } else if (type.equals(ChangeMergedEvent.type)) {
+      return context.deserialize(json, ChangeMergedEvent.class);
+    } else if (type.equals(ChangeRestoreEvent.type)) {
+      return context.deserialize(json, ChangeRestoreEvent.class);
+    } else if (type.equals(CommentAddedEvent.type)) {
+      return context.deserialize(json, CommentAddedEvent.class);
+    } else if (type.equals(PatchSetCreatedEvent.type)) {
+      return context.deserialize(json, PatchSetCreatedEvent.class);
+    } else if (type.equals(RefUpdatedEvent.type)) {
+      return context.deserialize(json, RefUpdatedEvent.class);
+    } else {
+      throw new JsonParseException("unknown event type: " + type);
+    }
+  }
+}

2852ba1d7ef0e958b341924826ccef7a15b0aa43
Luca Milanesio <luca.milanesio@gmail.com>
2012-05-05 14:28:23 -0700

    Extension for allowing pluggable custom commit validation.
    CommitValidationListener is informed on new commits coming
    through ReceiveCommits with the ability to reject
    and send messages back to the Git client output console.
    
    Plugins can contribute with additional commit validation
    by offering an implementation to CommitValidatorListener
    and returning a CommitValidationResult.
    
    Change-Id: Ie14400617cfb2d9ab4591522be1b3b492d3846f2
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/validators/CommitValidatorListener.java
@@ -0,0 +1,41 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git.validators;
+
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.events.CommitReceivedEvent;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.transport.ReceiveCommand;
+
+/**
+ * Listener to Git extra validation step.
+ *
+ * Invoked by Gerrit when a new commit is received, has passed basic Gerrit
+ * validation and can be then subject to extra validation checks.
+ *
+ */
+@ExtensionPoint
+public interface CommitValidatorListener {
+  /**
+   * Commit validation.
+   *
+   * @param received commit event details
+   * @return validation result
+   */
+  public CommitValidationResult onCommitReceived(CommitReceivedEvent receiveEvent);
+}

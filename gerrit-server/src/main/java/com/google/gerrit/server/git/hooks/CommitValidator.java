094392b95ce8b7ef40c791edf218849c6393f6a3
Luca Milanesio <luca.milanesio@gmail.com>
2012-05-05 14:28:23 -0700

    Hook for performing extra validation of commits.
    Commit validators are configured in gerrit.config
    in a new section named "gitHooks".
    
    Still not implemented the dynamic loading of the
    validator classes, waiting for the formal definition
    of Gerrit plug-ins.
    
    Change-Id: I4c0f9420d35298b27d54123a6904e13f8ceb2c3f
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/hooks/CommitValidator.java
@@ -0,0 +1,30 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git.hooks;
+
+import com.google.gerrit.server.project.RefControl;
+
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.transport.ReceiveCommand;
+
+import java.io.IOException;
+
+public interface CommitValidator {
+
+  public boolean isValid(final RefControl ctl, final ReceiveCommand cmd,
+      final RevCommit c) throws MissingObjectException, IOException;
+
+}

cc4036f8ae9af8d4beaca6976c5f2920821eac35
Luca Milanesio <luca.milanesio@gmail.com>
2012-05-07 15:54:09 -0700

    PluginLoader to scan and parse all plugins.
    Manifests inspected and module class is retrieved
    and loaded.
    Each plugin jar is loaded using a separate 
    UrlClassLoader using Gerrit classLoader as basis.
    
    Change-Id: Ic60bacd528100f502e2b7c5514bbb02ec39ab939
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/common/PluginLoader.java
@@ -0,0 +1,121 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.common;
+
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+public class PluginLoader {
+
+  private File pluginsDir;
+
+  private HashMap<String, Plugin> pluginCache;
+
+  private Logger log = LoggerFactory.getLogger(PluginLoader.class);
+
+  @Inject
+  public PluginLoader(SitePaths sitePaths) {
+    pluginsDir = sitePaths.plugins_dir;
+  }
+
+  private synchronized void initialize() {
+    if (pluginCache != null) {
+      return;
+    }
+
+    pluginCache = new HashMap<String, Plugin>();
+    loadPlugIns();
+  }
+
+  public Plugin get(String plugInName) {
+    initialize();
+    return pluginCache.get(plugInName);
+  }
+
+  public Collection<Plugin> getPlugins() {
+    initialize();
+    return pluginCache.values();
+  }
+
+  private void loadPlugIns() {
+
+    Collection<File> plugInJars = Collections.emptyList();
+    try {
+      plugInJars = getPluginFiles();
+    } catch (IOException e) {
+      log.error("Cannot scan Gerrit plugins directory looking for jar files", e);
+      return;
+    }
+
+    for (File jarFile : plugInJars) {
+
+      Plugin plugIn;
+      try {
+        plugIn = loadPlugin(jarFile);
+        pluginCache.put(plugIn.name, plugIn);
+      } catch (IOException e) {
+        log.warn("Cannot access PlugIn jar " + jarFile, e);
+      } catch (ClassNotFoundException e) {
+        log.warn("Cannot load PlugIn class module from " + jarFile, e);
+      }
+    }
+  }
+
+  private Plugin loadPlugin(File jarFile) throws IOException,
+      ClassNotFoundException {
+
+    Manifest jarManifest = new JarFile(jarFile).getManifest();
+    ClassLoader jarClassLoader = URLClassLoader.newInstance(getPluginURLs(jarFile), PluginLoader.class.getClassLoader());
+
+    Attributes attrs = jarManifest.getMainAttributes();
+    String pluginName = attrs.getValue("Gerrit-Plugin");
+    String moduleName = attrs.getValue("Gerrit-SshModule");
+
+    Class<?> moduleClass = jarClassLoader.loadClass(moduleName);
+
+    return new Plugin(pluginName, moduleClass);
+  }
+
+  private URL[] getPluginURLs(File jarFile) throws MalformedURLException {
+    return new URL[] {jarFile.toURI().toURL()};
+  }
+
+
+  @SuppressWarnings("unchecked")
+  private Collection<File> getPluginFiles() throws IOException {
+    if (pluginsDir == null || !pluginsDir.exists())
+      return Collections.emptyList();
+
+    return FileUtils.listFiles(pluginsDir, new String[] {"jar"}, false);
+  }
+
+}

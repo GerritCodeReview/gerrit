f0e9ac0f163100706bb9a9bc3cf702122af4632d
Luca Milanesio <luca.milanesio@gmail.com>
2012-05-09 10:37:36 -0700

    Additional plug-in load status tracking.
    Allows to understand from "gerrit plugin ls" command which
    plug-in has been loaded successfully and which one has failed.
    (otherwise you looking at the plugin listing you may have the
    impression that all plugins are available whilst this is not
    true for all of them).
    
    Change-Id: Id836f4959ca66bd57c7161bd821c23d41b6c44f9
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/plugins/Plugin.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/Plugin.java
@@ -52,6 +52,13 @@ public class Plugin {
   private Injector httpInjector;
   private LifecycleManager manager;
 
+  public static enum Status {
+    LOADED, STARTED, STOPPED, FAILED
+  }
+
+  private Status status = Status.LOADED;
+  private Exception startupException;
+
   public Plugin(String name,
       File jar,
       Manifest manifest,
@@ -101,6 +108,7 @@ public class Plugin {
   }
 
   public void start(PluginGuiceEnvironment env) throws Exception {
+    startupException = null;
     Injector root = newRootInjector(env);
     manager = new LifecycleManager();
 
@@ -126,6 +134,11 @@ public class Plugin {
     }
 
     manager.start();
+
+    if (startupException == null)
+      setStatus(Status.STARTED);
+    else
+      setStatus(Status.FAILED);
   }
 
   private Injector newRootInjector(PluginGuiceEnvironment env) {
@@ -148,6 +161,7 @@ public class Plugin {
       sysInjector = null;
       sshInjector = null;
       httpInjector = null;
+      setStatus(Status.STOPPED);
     }
   }
 
@@ -181,5 +195,17 @@ public class Plugin {
   @Override
   public String toString() {
     return "Plugin [" + name + "]";
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status newStatus) {
+    this.status = newStatus;
+  }
+
+  public void setStartupException(Exception e) {
+    this.startupException = e;
   }
 }

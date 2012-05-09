b9e3be1ef69223f3968d589978a861d1ebe63643
Shawn O. Pearce <sop@google.com>
2012-05-08 21:33:35 -0700

    Automatically load/unload/reload plugins
    The PluginScanner thread runs every 1 minute by default and loads any
    newly created plugins, unloads any deleted plugins, and reloads any
    plugins that have been modified.
    
    It is generally unsafe to delete or modify a JAR that the running
    server is using. The standard URLClassLoader might need to dynamically
    open the JAR to access a newly referenced class file or resource, and
    it often uses a cached table of contents from the JAR's ZIP format.
    If someone replaces the JAR on disk, the running URLClassLoader may
    try to incorrectly read from it. Discourage this with a warning, but
    still permit reloading the plugin whenever it is modified.
    
    Change-Id: I7afb61925235f94c015024d00e0d5830d56d6f2d

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginScannerThread.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.plugins;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+class PluginScannerThread extends Thread {
+  private final CountDownLatch done = new CountDownLatch(1);
+  private final PluginLoader loader;
+  private final long checkFrequencyMillis;
+
+  PluginScannerThread(PluginLoader loader, long checkFrequencyMillis) {
+    this.loader = loader;
+    this.checkFrequencyMillis = checkFrequencyMillis;
+    setDaemon(true);
+    setName("PluginScanner");
+  }
+
+  @Override
+  public void run() {
+    for (;;) {
+      try {
+        if (done.await(checkFrequencyMillis, TimeUnit.MILLISECONDS)) {
+          return;
+        }
+      } catch (InterruptedException e) {
+      }
+      loader.rescan();
+    }
+  }
+
+  void end() {
+    done.countDown();
+    try {
+      join();
+    } catch (InterruptedException e) {
+    }
+  }
+}

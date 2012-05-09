928123f05eefbee48c28846fe73eb78fc996e187
Colby Ranger <cranger@google.com>
2012-05-09 09:27:07 -0700

    Added the RequestContext for the scope.
    The RequestContext attempts to make data that is normally RequestScoped
    available in a non-RequestScoped context. Request scoping of the
    RequestContext is facilitated by the ThreadLocalRequestContext, using
    a ThreadLocal to setup the context, similar to the normal way
    request scopes work. However, if there is no RequestContext set, the
    a default FallbackRequestContext is used, instead of throwing an
    OutOfScope exception.
    
    The motivation for this change was prompted by the new GroupBackend
    system needing to know the current user in a Singleton context, without
    passing the current user through the method.
    
    Change-Id: Ifd7745bec0d4b22dfc98b9a0d18cfd7138bc3375

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/util/FallbackRequestContext.java
@@ -0,0 +1,40 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.util;
+
+import com.google.gerrit.server.AnonymousUser;
+import com.google.gerrit.server.CurrentUser;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * The default RequestContext to use when not in a request scope e.g.
+ * ThreadLocalRequestContext is not set.
+ */
+@Singleton
+public class FallbackRequestContext implements RequestContext {
+
+  private final AnonymousUser user;
+
+  @Inject
+  FallbackRequestContext(AnonymousUser user) {
+    this.user = user;
+  }
+
+  @Override
+  public CurrentUser getCurrentUser() {
+    return user;
+  }
+}

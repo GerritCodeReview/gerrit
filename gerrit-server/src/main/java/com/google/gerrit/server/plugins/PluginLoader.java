41f8af35cc7099b78aca0d021b832eec381e94ae
Shawn O. Pearce <sop@google.com>
2012-05-10 17:44:24 -0700

    Provide a data directory to plugins on demand
    If a plugin requests a data directory with a @PluginData File
    dependency, a data directory will be created automatically by the
    server in $site_path/data/$plugin_name and passed to the plugin.
    
    Plugins can use this to store any data they want.
    
    Change-Id: I98de44a033889740cafc8a3d72c582c0e99b7ffd

--- gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginLoader.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginLoader.java
@@ -59,6 +59,7 @@ public class PluginLoader implements LifecycleListener {
   static final Logger log = LoggerFactory.getLogger(PluginLoader.class);
 
   private final File pluginsDir;
+  private final File dataDir;
   private final File tmpDir;
   private final PluginGuiceEnvironment env;
   private final Map<String, Plugin> running;
@@ -72,6 +73,7 @@ public class PluginLoader implements LifecycleListener {
       PluginGuiceEnvironment pe,
       @GerritServerConfig Config cfg) {
     pluginsDir = sitePaths.plugins_dir;
+    dataDir = sitePaths.data_dir;
     tmpDir = sitePaths.tmp_dir;
     env = pe;
     running = Maps.newHashMap();
@@ -345,7 +347,7 @@ public class PluginLoader implements LifecycleListener {
       return new Plugin(name,
           srcJar, snapshot,
           jarFile, manifest,
-          pluginLoader,
+          new File(dataDir, name), pluginLoader,
           sysModule, sshModule, httpModule);
     } finally {
       if (!keep) {

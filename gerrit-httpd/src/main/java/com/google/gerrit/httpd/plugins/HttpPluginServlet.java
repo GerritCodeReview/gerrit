d8e6b5f0a49af28797dc5ce658485393007ff56a
Shawn O. Pearce <sop@google.com>
2012-05-12 11:20:18 -0700

    Markdown formatting improvements
    Extract the first H1 level string from the documentation and make that
    the HTML page title. Embed a small block of CSS that makes the style
    of the page look more like the rest of the Gerrit documentation pages.
    Wrap the entire pegdown output in a proper HTML tag.
    
    The pegdown.css is cached when read from a JAR, but loads on demand
    when read from a file:// URL. This enables a fast edit-view cycle of
    the style sheet when running the server from within Eclipse or another
    IDE that loads the resources from the source tree.
    
    If the Documentation/ URL ends with / redirect to look for index.html
    instead. This provides some simple support for directory indexes by
    allowing directory references to jump to the index.html document,
    which might be index.md in the JAR.
    
    If the plugin doesn't answer to / on its own, redirect web requests
    for /plugins/NAME to /plugins/NAME/Documentation/index.html allowing
    the user to at least see the documentation.
    
    Only attempt to convert resources that end with ".md" from Markdown to
    HTML, rather than everything.
    
    If there is no index.html or index.md in Documentation/, render one on
    the fly by creating a listing of documentation pages, extracting their
    titles and rendering it using the MarkdownFormatter.  This keeps the
    style consistent with the rest of the documentation in a plugin.
    
    When rendering Documentation files from Markdown to HTML, replace the
    macro @PLUGIN@ with the current name of the plugin. This allows
    authors to write documentation with reasonably accurate examples that
    adjusts automatically based on the installation.
    
    Also support @URL@, @SSH_HOST@, and @SSH_PORT@ replacements in
    Markdown documentation for common ways to reference this server.
    
    Change-Id: I988038021d4a047bd008561aeedc3ccfd5a1b294

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/plugins/HttpPluginServlet.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/plugins/HttpPluginServlet.java
@@ -18,24 +18,31 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.gerrit.extensions.registration.RegistrationHandle;
-import com.google.gerrit.server.documentation.MarkdownFormatter;
 import com.google.gerrit.server.MimeUtilFileTypeRegistry;
+import com.google.gerrit.server.config.CanonicalWebUrl;
+import com.google.gerrit.server.documentation.MarkdownFormatter;
 import com.google.gerrit.server.plugins.Plugin;
 import com.google.gerrit.server.plugins.ReloadPluginListener;
 import com.google.gerrit.server.plugins.StartPluginListener;
+import com.google.gerrit.server.ssh.SshInfo;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.google.inject.servlet.GuiceFilter;
 
-import eu.medsea.mimeutil.MimeType;
-
 import org.eclipse.jgit.util.IO;
+import org.eclipse.jgit.util.RawParseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 import java.util.jar.Attributes;
@@ -55,19 +62,43 @@ import javax.servlet.http.HttpServletResponse;
 @Singleton
 class HttpPluginServlet extends HttpServlet
     implements StartPluginListener, ReloadPluginListener {
+  private static final int SMALL_RESOURCE = 128 * 1024;
   private static final long serialVersionUID = 1L;
   private static final Logger log
       = LoggerFactory.getLogger(HttpPluginServlet.class);
 
   private final MimeUtilFileTypeRegistry mimeUtil;
+  private final Provider<String> webUrl;
+  private final String sshHost;
+  private final int sshPort;
+
   private List<Plugin> pending = Lists.newArrayList();
   private String base;
   private final ConcurrentMap<String, PluginHolder> plugins
       = Maps.newConcurrentMap();
 
   @Inject
-  HttpPluginServlet(MimeUtilFileTypeRegistry mimeUtil) {
+  HttpPluginServlet(MimeUtilFileTypeRegistry mimeUtil,
+      @CanonicalWebUrl Provider<String> webUrl,
+      SshInfo sshInfo) {
     this.mimeUtil = mimeUtil;
+    this.webUrl = webUrl;
+
+    String sshHost = "review.example.com";
+    int sshPort = 29418;
+    if (!sshInfo.getHostKeys().isEmpty()) {
+      String host = sshInfo.getHostKeys().get(0).getHost();
+      int c = host.lastIndexOf(':');
+      if (0 <= c) {
+        sshHost = host.substring(0, c);
+        sshPort = Integer.parseInt(host.substring(c+1));
+      } else {
+        sshHost = host;
+        sshPort = 22;
+      }
+    }
+    this.sshHost = sshHost;
+    this.sshPort = sshPort;
   }
 
   @Override
@@ -168,59 +199,242 @@ class HttpPluginServlet extends HttpServlet
   private void onDefault(PluginHolder holder,
       HttpServletRequest req,
       HttpServletResponse res) throws IOException {
+    if (!"GET".equals(req.getMethod()) && !"HEAD".equals(req.getMethod())) {
+      noCache(res);
+      res.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+      return;
+    }
+
     String uri = req.getRequestURI();
     String ctx = req.getContextPath();
     String file = uri.substring(ctx.length() + 1);
-    if (file.startsWith("Documentation/") || file.startsWith("static/")) {
+
+    if ("".equals(file)) {
+      res.sendRedirect(uri + "Documentation/index.html");
+    } else if (file.startsWith("static/")) {
       JarFile jar = holder.plugin.getJarFile();
       JarEntry entry = jar.getJarEntry(file);
-      if (file.startsWith("Documentation/") && !isValidEntry(entry)) {
-        entry = getRealFileEntry(jar, file);
-        if (isValidEntry(entry)) {
-          sendResource(jar, entry, res, holder.plugin.getName(), true);
-          return;
+      if (exists(entry)) {
+        sendResource(jar, entry, res);
+      } else {
+        noCache(res);
+        res.sendError(HttpServletResponse.SC_NOT_FOUND);
+      }
+    } else if (file.equals("Documentation")) {
+      res.sendRedirect(uri + "/index.html");
+    } else if (file.startsWith("Documentation/") && file.endsWith("/")) {
+      res.sendRedirect(uri + "index.html");
+    } else if (file.startsWith("Documentation/")) {
+      JarFile jar = holder.plugin.getJarFile();
+      JarEntry entry = jar.getJarEntry(file);
+      if (!exists(entry)) {
+        entry = findSource(jar, file);
+      }
+      if (!exists(entry) && file.endsWith("/index.html")) {
+        String pfx = file.substring(0, file.length() - "index.html".length());
+        sendAutoIndex(jar, pfx, holder.plugin.getName(), res);
+      } else if (exists(entry) && entry.getName().endsWith(".md")) {
+        sendMarkdownAsHtml(jar, entry, holder.plugin.getName(), res);
+      } else if (exists(entry)) {
+        sendResource(jar, entry, res);
+      } else {
+        noCache(res);
+        res.sendError(HttpServletResponse.SC_NOT_FOUND);
+      }
+    } else {
+      noCache(res);
+      res.sendError(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  private void sendAutoIndex(JarFile jar,
+      String prefix, String pluginName,
+      HttpServletResponse res) throws IOException {
+    List<JarEntry> cmds = Lists.newArrayList();
+    List<JarEntry> docs = Lists.newArrayList();
+    Enumeration<JarEntry> entries = jar.entries();
+    while (entries.hasMoreElements()) {
+      JarEntry entry = entries.nextElement();
+      String name = entry.getName();
+      long size = entry.getSize();
+      if (name.startsWith(prefix)
+          && (name.endsWith(".md")
+              || name.endsWith(".html"))
+          && 0 < size && size <= SMALL_RESOURCE) {
+        if (name.substring(prefix.length()).startsWith("cmd-")) {
+          cmds.add(entry);
+        } else {
+          docs.add(entry);
         }
       }
-      if (isValidEntry(entry)) {
-        sendResource(jar, entry, res, holder.plugin.getName());
-        return;
+    }
+    Collections.sort(cmds, new Comparator<JarEntry>() {
+      @Override
+      public int compare(JarEntry a, JarEntry b) {
+        return a.getName().compareTo(b.getName());
       }
+    });
+    Collections.sort(docs, new Comparator<JarEntry>() {
+      @Override
+      public int compare(JarEntry a, JarEntry b) {
+        return a.getName().compareTo(b.getName());
+      }
+    });
+
+    StringBuilder md = new StringBuilder();
+    md.append(String.format("# Plugin %s #\n", pluginName));
+    md.append("\n");
+    appendPluginInfoTable(md, jar.getManifest().getMainAttributes());
+
+    if (!docs.isEmpty()) {
+      md.append("## Documentation ##\n");
+      for(JarEntry entry : docs) {
+        String rsrc = entry.getName().substring(prefix.length());
+        String title = rsrc;
+        if (rsrc.endsWith(".html")) {
+          title = rsrc.substring(0, rsrc.length() - 5);
+        } else if (rsrc.endsWith(".md")) {
+          title = extractTitleFromMarkdown(jar, entry);
+          if (Strings.isNullOrEmpty(title)) {
+            title = rsrc.substring(0, rsrc.length() - 3);
+          }
+          rsrc = rsrc.substring(0, rsrc.length() - 3) + ".html";
+        }
+        md.append(String.format("* [%s](%s)\n", title, rsrc));
+      }
+      md.append("\n");
     }
 
-    noCache(res);
-    res.sendError(HttpServletResponse.SC_NOT_FOUND);
+    if (!cmds.isEmpty()) {
+      md.append("## Commands ##\n");
+      for(JarEntry entry : cmds) {
+        String rsrc = entry.getName().substring(prefix.length());
+        String title = rsrc;
+        if (rsrc.endsWith(".html")) {
+          title = rsrc.substring(4, rsrc.length() - 5);
+        } else if (rsrc.endsWith(".md")) {
+          title = extractTitleFromMarkdown(jar, entry);
+          if (Strings.isNullOrEmpty(title)) {
+            title = rsrc.substring(4, rsrc.length() - 3);
+          }
+          rsrc = rsrc.substring(0, rsrc.length() - 3) + ".html";
+        }
+        md.append(String.format("* [%s](%s)\n", title, rsrc));
+      }
+      md.append("\n");
+    }
+
+    sendMarkdownAsHtml(md.toString(), pluginName, res);
   }
 
-  private JarEntry getRealFileEntry(JarFile jar, String file) {
-    // TODO: Replace with a loop iterating over possible formatters
-    return jar.getJarEntry(file.replaceAll("\\.html$", ".md"));
+  private void sendMarkdownAsHtml(String md, String pluginName,
+      HttpServletResponse res) throws UnsupportedEncodingException, IOException {
+    md = md.replaceAll("@PLUGIN@", pluginName);
+    md = md.replaceAll("@SSH_HOST@", sshHost);
+    md = md.replaceAll("@SSH_PORT@", "" + sshPort);
+    String url = webUrl.get();
+    if (Strings.isNullOrEmpty(url)) {
+      url = "http://review.example.com/";
+    }
+    md = md.replaceAll("@URL@", url);
+
+    byte[] html = new MarkdownFormatter()
+      .markdownToDocHtml(md, "UTF-8");
+    res.setContentType("text/html");
+    res.setCharacterEncoding("UTF-8");
+    res.getOutputStream().write(html);
+    res.setContentLength(html.length);
   }
 
-  private boolean isValidEntry(JarEntry entry) {
+  private static void appendPluginInfoTable(StringBuilder html, Attributes main) {
+    if (main != null) {
+      String t = main.getValue(Attributes.Name.IMPLEMENTATION_TITLE);
+      String n = main.getValue(Attributes.Name.IMPLEMENTATION_VENDOR);
+      String v = main.getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+      String u = main.getValue(Attributes.Name.IMPLEMENTATION_URL);
+      String a = main.getValue("Gerrit-ApiVersion");
+
+      html.append("<table class=\"plugin_info\">");
+      if (!Strings.isNullOrEmpty(t)) {
+        html.append("<tr><th>Name</th><td>")
+            .append(t)
+            .append("</td></tr>\n");
+      }
+      if (!Strings.isNullOrEmpty(n)) {
+        html.append("<tr><th>Vendor</th><td>")
+            .append(n)
+            .append("</td></tr>\n");
+      }
+      if (!Strings.isNullOrEmpty(v)) {
+        html.append("<tr><th>Version</th><td>")
+            .append(v)
+            .append("</td></tr>\n");
+      }
+      if (!Strings.isNullOrEmpty(u)) {
+        html.append("<tr><th>URL</th><td>")
+            .append(String.format("<a href=\"%s\">%s</a>", u, u))
+            .append("</td></tr>\n");
+      }
+      if (!Strings.isNullOrEmpty(a)) {
+        html.append("<tr><th>API Version</th><td>")
+            .append(a)
+            .append("</td></tr>\n");
+      }
+      html.append("</table>\n");
+    }
+  }
+
+  private static String extractTitleFromMarkdown(JarFile jar, JarEntry entry)
+        throws IOException {
+    String charEnc = null;
+    Attributes atts = entry.getAttributes();
+    if (atts != null) {
+      charEnc = Strings.emptyToNull(atts.getValue("Character-Encoding"));
+    }
+    if (charEnc == null) {
+      charEnc = "UTF-8";
+    }
+    return new MarkdownFormatter().extractTitleFromMarkdown(
+          readWholeEntry(jar, entry),
+          charEnc);
+  }
+
+  private static JarEntry findSource(JarFile jar, String file) {
+    if (file.endsWith(".html")) {
+      int d = file.lastIndexOf('.');
+      return jar.getJarEntry(file.substring(0, d) + ".md");
+    }
+    return null;
+  }
+
+  private static boolean exists(JarEntry entry) {
     return entry != null && entry.getSize() > 0;
   }
 
-  private void sendResource(JarFile jar, JarEntry entry,
-      HttpServletResponse res, String pluginName) throws IOException {
-    sendResource(jar, entry, res, pluginName, false);
+  private void sendMarkdownAsHtml(JarFile jar, JarEntry entry,
+      String pluginName, HttpServletResponse res) throws IOException {
+    byte[] rawmd = readWholeEntry(jar, entry);
+    String encoding = null;
+    Attributes atts = entry.getAttributes();
+    if (atts != null) {
+      encoding = Strings.emptyToNull(atts.getValue("Character-Encoding"));
+    }
+
+    String txtmd = RawParseUtils.decode(
+        Charset.forName(encoding != null ? encoding : "UTF-8"),
+        rawmd);
+    long time = entry.getTime();
+    if (0 < time) {
+      res.setDateHeader("Last-Modified", time);
+    }
+    sendMarkdownAsHtml(txtmd, pluginName, res);
   }
 
-  private void sendResource(JarFile jar, JarEntry entry,
-      HttpServletResponse res, String pluginName, boolean format)
+  private void sendResource(JarFile jar, JarEntry entry, HttpServletResponse res)
       throws IOException {
-    String entryName = entry.getName();
     byte[] data = null;
-    if (entry.getSize() <= 128 * 1024) {
-      data = new byte[(int) entry.getSize()];
-      InputStream in = jar.getInputStream(entry);
-      try {
-        IO.readFully(in, data, 0, data.length);
-      } finally {
-        in.close();
-      }
-    } else if (format == true) {
-      log.warn(String.format("Plugin '%s' file '%s' too large to format",
-          pluginName, entryName));
+    if (entry.getSize() <= SMALL_RESOURCE) {
+      data = readWholeEntry(jar, entry);
     }
 
     String contentType = null;
@@ -230,28 +444,15 @@ class HttpPluginServlet extends HttpServlet
       contentType = Strings.emptyToNull(atts.getValue("Content-Type"));
       charEnc = Strings.emptyToNull(atts.getValue("Character-Encoding"));
     }
-
     if (contentType == null) {
-      MimeType type = mimeUtil.getMimeType(entryName, data);
-      contentType = type.toString();
-    }
-
-    if (format && data != null) {
-      if (charEnc == null) {
-        charEnc = "UTF-8";
-      }
-      MarkdownFormatter fmter = new MarkdownFormatter();
-      data = fmter.getHtmlFromMarkdown(data, charEnc);
-      res.setHeader("Content-Length", Long.toString(data.length));
-      contentType = "text/html";
-    } else {
-      res.setHeader("Content-Length", Long.toString(entry.getSize()));
+      contentType = mimeUtil.getMimeType(entry.getName(), data).toString();
     }
 
     long time = entry.getTime();
     if (0 < time) {
       res.setDateHeader("Last-Modified", time);
     }
+    res.setHeader("Content-Length", Long.toString(entry.getSize()));
     res.setContentType(contentType);
     if (charEnc != null) {
       res.setCharacterEncoding(charEnc);
@@ -275,6 +476,18 @@ class HttpPluginServlet extends HttpServlet
         in.close();
       }
     }
+  }
+
+  private static byte[] readWholeEntry(JarFile jar, JarEntry entry)
+      throws IOException {
+    byte[] data = new byte[(int) entry.getSize()];
+    InputStream in = jar.getInputStream(entry);
+    try {
+      IO.readFully(in, data, 0, data.length);
+    } finally {
+      in.close();
+    }
+    return data;
   }
 
   private static String extractName(HttpServletRequest req) {
--- gerrit-server/src/main/java/com/google/gerrit/server/documentation/MarkdownFormatter.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/documentation/MarkdownFormatter.java
@@ -17,20 +17,129 @@ package com.google.gerrit.server.documentation;
 import static org.pegdown.Extensions.ALL;
 import static org.pegdown.Extensions.HARDWRAPS;
 
-import org.eclipse.jgit.util.RawParseUtils;
-import org.pegdown.PegDownProcessor;
+import com.google.common.base.Strings;
 
+import org.eclipse.jgit.util.RawParseUtils;
+import org.eclipse.jgit.util.TemporaryBuffer;
+import org.pegdown.LinkRenderer;
+import org.pegdown.PegDownProcessor;
+import org.pegdown.ToHtmlSerializer;
+import org.pegdown.ast.HeaderNode;
+import org.pegdown.ast.Node;
+import org.pegdown.ast.RootNode;
+import org.pegdown.ast.TextNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MarkdownFormatter {
-  public byte[] getHtmlFromMarkdown(byte[] data, String charEnc)
-      throws UnsupportedEncodingException {
-    return new PegDownProcessor(ALL & ~(HARDWRAPS))
-        .markdownToHtml(RawParseUtils.decode(
-            Charset.forName(charEnc),
-            data))
-        .getBytes(charEnc);
+  private static final Logger log =
+      LoggerFactory.getLogger(MarkdownFormatter.class);
+
+  private static final String css;
+
+  static {
+    AtomicBoolean file = new AtomicBoolean();
+    String src;
+    try {
+      src = readPegdownCss(file);
+    } catch (IOException err) {
+      log.warn("Cannot load pegdown.css", err);
+      src = "";
+    }
+    css = file.get() ? null : src;
   }
-  // TODO: Add a cache
+
+  private static String readCSS() {
+    if (css != null) {
+      return css;
+    }
+    try {
+      return readPegdownCss(new AtomicBoolean());
+    } catch (IOException err) {
+      log.warn("Cannot load pegdown.css", err);
+      return "";
+    }
+  }
+
+  public byte[] markdownToDocHtml(String md, String charEnc)
+      throws UnsupportedEncodingException {
+    RootNode root = parseMarkdown(md);
+    String title = findTitle(root);
+
+    StringBuilder html = new StringBuilder();
+    html.append("<html>");
+    html.append("<head>");
+    if (!Strings.isNullOrEmpty(title)) {
+      html.append("<title>").append(title).append("</title>");
+    }
+    html.append("<style type=\"text/css\">\n")
+        .append(readCSS())
+        .append("\n</style>");
+    html.append("</head>");
+    html.append("<body>\n");
+    html.append(new ToHtmlSerializer(new LinkRenderer()).toHtml(root));
+    html.append("\n</body></html>");
+    return html.toString().getBytes(charEnc);
+  }
+
+  public String extractTitleFromMarkdown(byte[] data, String charEnc) {
+    String md = RawParseUtils.decode(Charset.forName(charEnc), data);
+    return findTitle(parseMarkdown(md));
+  }
+
+  private String findTitle(Node root) {
+    if (root instanceof HeaderNode) {
+      HeaderNode h = (HeaderNode) root;
+      if (h.getLevel() == 1
+          && h.getChildren() != null
+          && !h.getChildren().isEmpty()) {
+        StringBuilder b = new StringBuilder();
+        for (Node n : root.getChildren()) {
+          if (n instanceof TextNode) {
+            b.append(((TextNode) n).getText());
+          }
+        }
+        return b.toString();
+      }
+    }
+
+    for (Node n : root.getChildren()) {
+      String title = findTitle(n);
+      if (title != null) {
+        return title;
+      }
+    }
+    return null;
+  }
+
+  private RootNode parseMarkdown(String md) {
+    return new PegDownProcessor(ALL & ~(HARDWRAPS))
+        .parseMarkdown(md.toCharArray());
+  }
+
+  private static String readPegdownCss(AtomicBoolean file)
+      throws IOException {
+    String name = "pegdown.css";
+    URL url = MarkdownFormatter.class.getResource(name);
+    if (url == null) {
+      throw new FileNotFoundException("Resource " + name);
+    }
+    file.set("file".equals(url.getProtocol()));
+    InputStream in = url.openStream();
+    try {
+      TemporaryBuffer.Heap tmp = new TemporaryBuffer.Heap(128 * 1024);
+      tmp.copy(in);
+      return new String(tmp.toByteArray(), "UTF-8");
+    } finally {
+      in.close();
+    }
+  }
 }

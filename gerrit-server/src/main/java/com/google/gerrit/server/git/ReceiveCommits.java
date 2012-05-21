9e9a10201a081022e3d93189e87e1d710e23ec4f
Gustaf Lundh <gustaf.lundh@sonymobile.com>
2012-05-21 11:18:42 +0200

    Caching of changes
    During Ref Advertisments (via VisibleRefFilter), all changes
    needs to be fetched from the database to allow gerrit to
    figure out which change refs are visible and should be
    advertised to the user.
    
    This behaviour can generate massive amounts of traffic
    against the database.
    
    This feature introduces a ChangeCache which will store changes
    by using the project name as a key. This should help avoid
    unneccessary change look-ups and cut away some of the database
    traffic.
    
    Change-Id: I6e4120986a98dcfb3d9a5cb12aa91374c1be4194

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -247,6 +247,7 @@ public class ReceiveCommits {
   private final PersonIdent gerritIdent;
   private final TrackingFooters trackingFooters;
   private final TagCache tagCache;
+  private final ChangeCache changeCache;
   private final WorkQueue workQueue;
   private final ListeningExecutorService changeUpdateExector;
   private final RequestScopePropagator requestScopePropagator;
@@ -296,6 +297,7 @@ public class ReceiveCommits {
       final ProjectCache projectCache,
       final GitRepositoryManager repoManager,
       final TagCache tagCache,
+      final ChangeCache changeCache,
       @CanonicalWebUrl @Nullable final String canonicalWebUrl,
       @GerritPersonIdent final PersonIdent gerritIdent,
       final TrackingFooters trackingFooters,
@@ -323,6 +325,7 @@ public class ReceiveCommits {
     this.gerritIdent = gerritIdent;
     this.trackingFooters = trackingFooters;
     this.tagCache = tagCache;
+    this.changeCache = changeCache;
     this.workQueue = workQueue;
     this.changeUpdateExector = changeUpdateExector;
     this.requestScopePropagator = requestScopePropagator;
@@ -344,7 +347,7 @@ public class ReceiveCommits {
 
     if (!projectControl.allRefsAreVisible()) {
       rp.setCheckReferencedObjectsAreReachable(true);
-      rp.setAdvertiseRefsHook(new VisibleRefFilter(tagCache, repo, projectControl, db, false));
+      rp.setAdvertiseRefsHook(new VisibleRefFilter(tagCache, changeCache, repo, projectControl, db, false));
     }
     List<AdvertiseRefsHook> advHooks = new ArrayList<AdvertiseRefsHook>(2);
     advHooks.add(rp.getAdvertiseRefsHook());

cdab83e8cd1ae8febecfc527fcba6276a4313982
Bruce Zu <bruce.zu@sonymobile.com>
2012-06-18 16:01:33 +0800

    Fix: wrong list of 'Parent Suggestion' in create-project UI.
    In 'Create Project' UI, 'Parent Suggestion' table
    shows wrong content. It is caused by [1] where we
    use 'ProjectMap.permissions()' style to replace the
    deprecated 'Util.PROJECT_SVC.suggestParentCandidates()'
    style, however their logic is different. This bug is
    fixed by providing an equivalent new method.
    
    [1]https://gerrit-review.googlesource.com/#/c/34477
    
    Change-Id: I1e229c8a0b5d66187c4e89dec2ce1ae72ae0b46c
    Signed-off-by: Bruce Zu <bruce.zu@sonymobile.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/projects/ProjectMap.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/projects/ProjectMap.java
@@ -37,6 +37,14 @@ public class ProjectMap extends NativeMap<ProjectInfo> {
         .send(NativeMap.copyKeysIntoChildren(callback));
   }
 
+  public static void parentCandidates(AsyncCallback<ProjectMap> callback) {
+    new RestApi("/projects/")
+        .addParameterRaw("type", "PARENT_CANDIDATES")
+        .addParameterTrue("all")
+        .addParameterTrue("d") // description
+        .send(NativeMap.copyKeysIntoChildren(callback));
+  }
+
   public static void suggest(String prefix, int limit, AsyncCallback<ProjectMap> cb) {
     new RestApi("/projects/" + URL.encode(prefix).replaceAll("[?]", "%3F"))
         .addParameterRaw("type", "ALL")

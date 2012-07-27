b31e8e66ee24be3efb79b77752b940cfc1fa1e3d
Shawn O. Pearce <sop@google.com>
2012-07-27 16:38:55 -0700

    Perform change update on multiple threads
    When multiple changes need to be created or updated for a single push
    operation they are now inserted into the database by parallel threads,
    up to the maximum allowed thread count. The current thread is used
    when the thread pool is already fully in use, falling back to the
    prior behavior where each concurrent push operation can do its own
    concurrent database update. The thread pool exists to reduce latency
    so long as there are sufficient threads available.
    
    This helps push times on databases that are high latency, such as
    database servers that are running on a different machine from the
    Gerrit server itself, e.g. gerrit.googlesource.com.
    
    The new thread pool is disabled by default, limiting the overhead to
    servers that have good latency with their database, such as using
    in-process H2 database, or a MySQL or PostgreSQL on the same host.
    
    Change-Id: I7d7368cee99a47e3f2ad1e753cc3f7e1c82d37b0

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -21,11 +21,15 @@ import static org.eclipse.jgit.transport.ReceiveCommand.Result.REJECTED_MISSING_
 import static org.eclipse.jgit.transport.ReceiveCommand.Result.REJECTED_NONFASTFORWARD;
 import static org.eclipse.jgit.transport.ReceiveCommand.Result.REJECTED_OTHER_REASON;
 
+import com.google.common.base.Function;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.gerrit.common.ChangeHooks;
 import com.google.gerrit.common.PageLinks;
 import com.google.gerrit.common.data.Capable;
@@ -65,6 +69,7 @@ import com.google.gerrit.server.util.RequestScopePropagator;
 import com.google.gwtorm.server.AtomicUpdate;
 import com.google.gwtorm.server.OrmException;
 import com.google.gwtorm.server.ResultSet;
+import com.google.gwtorm.server.SchemaFactory;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
@@ -104,6 +109,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -206,11 +212,23 @@ public class ReceiveCommits {
     }
   }
 
+  private static final Function<Exception, OrmException> ORM_EXCEPTION =
+      new Function<Exception, OrmException>() {
+        @Override
+        public OrmException apply(Exception input) {
+          if (input instanceof OrmException) {
+            return (OrmException) input;
+          }
+          return new OrmException("Error updating database", input);
+        }
+      };
+
   private final Set<Account.Id> reviewerId = new HashSet<Account.Id>();
   private final Set<Account.Id> ccId = new HashSet<Account.Id>();
 
   private final IdentifiedUser currentUser;
   private final ReviewDb db;
+  private final SchemaFactory<ReviewDb> schemaFactory;
   private final AccountResolver accountResolver;
   private final CreateChangeSender.Factory createChangeSenderFactory;
   private final MergedSender.Factory mergedSenderFactory;
@@ -226,6 +244,7 @@ public class ReceiveCommits {
   private final TrackingFooters trackingFooters;
   private final TagCache tagCache;
   private final WorkQueue workQueue;
+  private final ListeningExecutorService changeUpdateExector;
   private final RequestScopePropagator requestScopePropagator;
 
   private final ProjectControl projectControl;
@@ -261,6 +280,7 @@ public class ReceiveCommits {
 
   @Inject
   ReceiveCommits(final ReviewDb db,
+      final SchemaFactory<ReviewDb> schemaFactory,
       final AccountResolver accountResolver,
       final CreateChangeSender.Factory createChangeSenderFactory,
       final MergedSender.Factory mergedSenderFactory,
@@ -276,6 +296,7 @@ public class ReceiveCommits {
       @GerritPersonIdent final PersonIdent gerritIdent,
       final TrackingFooters trackingFooters,
       final WorkQueue workQueue,
+      @ChangeUpdateExecutor ListeningExecutorService changeUpdateExector,
       final RequestScopePropagator requestScopePropagator,
 
       @Assisted final ProjectControl projectControl,
@@ -283,6 +304,7 @@ public class ReceiveCommits {
       final SubmoduleOp.Factory subOpFactory) throws IOException {
     this.currentUser = (IdentifiedUser) projectControl.getCurrentUser();
     this.db = db;
+    this.schemaFactory = schemaFactory;
     this.accountResolver = accountResolver;
     this.createChangeSenderFactory = createChangeSenderFactory;
     this.mergedSenderFactory = mergedSenderFactory;
@@ -298,6 +320,7 @@ public class ReceiveCommits {
     this.trackingFooters = trackingFooters;
     this.tagCache = tagCache;
     this.workQueue = workQueue;
+    this.changeUpdateExector = changeUpdateExector;
     this.requestScopePropagator = requestScopePropagator;
 
     this.projectControl = projectControl;
@@ -584,7 +607,7 @@ public class ReceiveCommits {
         }
       } else if (replace.cmd.getResult() == OK) {
         try {
-          if (replace.insertPatchSet() != null) {
+          if (replace.insertPatchSet().checkedGet() != null) {
             replace.inputCommand.setResult(OK);
           }
         } catch (IOException err) {
@@ -626,14 +649,19 @@ public class ReceiveCommits {
     }
 
     try {
+      List<CheckedFuture<?, OrmException>> futures = Lists.newArrayList();
       for (ReplaceRequest replace : replaceByChange.values()) {
         if (replace.inputCommand == newChange) {
-          replace.insertPatchSet();
+          futures.add(replace.insertPatchSet());
         }
       }
 
       for (CreateRequest create : newChanges) {
-        create.insertChange();
+        futures.add(create.insertChange());
+      }
+
+      for (CheckedFuture<?, OrmException> f : futures) {
+        f.checkedGet();
       }
       newChange.setResult(OK);
     } catch (OrmException err) {
@@ -1216,10 +1244,35 @@ public class ReceiveCommits {
       cmd = new ReceiveCommand(ObjectId.zeroId(), c, ps.getRefName());
     }
 
-    void insertChange() throws IOException, OrmException {
+    CheckedFuture<Void, OrmException> insertChange() throws IOException {
       rp.getRevWalk().parseBody(commit);
       warnMalformedMessage(commit);
 
+      final Thread caller = Thread.currentThread();
+      ListenableFuture<Void> future =
+          changeUpdateExector.submit(new Callable<Void>() {
+        @Override
+        public Void call() throws OrmException {
+          if (caller == Thread.currentThread()) {
+            insertChange(db);
+          } else {
+            ReviewDb db = schemaFactory.open();
+            try {
+              insertChange(db);
+            } finally {
+              db.close();
+            }
+          }
+          synchronized (newProgress) {
+            newProgress.update(1);
+          }
+          return null;
+        }
+      });
+      return Futures.makeChecked(future, ORM_EXCEPTION);
+    }
+
+    private void insertChange(ReviewDb db) throws OrmException {
       final Account.Id me = currentUser.getAccountId();
       final Set<Account.Id> reviewers = new HashSet<Account.Id>(reviewerId);
       final Set<Account.Id> cc = new HashSet<Account.Id>(ccId);
@@ -1245,7 +1298,8 @@ public class ReceiveCommits {
         db.patchSets().insert(Collections.singleton(ps));
         db.changes().insert(Collections.singleton(change));
         ChangeUtil.updateTrackingIds(db, change, trackingFooters, footerLines);
-        approvalsUtil.addReviewers(change, ps, info, reviewers);
+        approvalsUtil.addReviewers(db, change, ps, info,
+            reviewers, Collections.<Account.Id> emptySet());
         db.commit();
       } finally {
         db.rollback();
@@ -1253,7 +1307,6 @@ public class ReceiveCommits {
 
       replication.fire(project.getNameKey(), ps.getRefName());
       hooks.doPatchsetCreatedHook(change, ps, db);
-      newProgress.update(1);
       workQueue.getDefaultQueue()
           .submit(requestScopePropagator.wrap(new Runnable() {
         @Override
@@ -1498,10 +1551,38 @@ public class ReceiveCommits {
       return true;
     }
 
-    PatchSet.Id insertPatchSet() throws IOException, OrmException {
+    CheckedFuture<PatchSet.Id, OrmException> insertPatchSet()
+        throws IOException {
       rp.getRevWalk().parseBody(newCommit);
       warnMalformedMessage(newCommit);
 
+      final Thread caller = Thread.currentThread();
+      ListenableFuture<PatchSet.Id> future =
+          changeUpdateExector.submit(new Callable<PatchSet.Id>() {
+        @Override
+        public PatchSet.Id call() throws OrmException {
+          try {
+            if (caller == Thread.currentThread()) {
+              return insertPatchSet(db);
+            } else {
+              ReviewDb db = schemaFactory.open();
+              try {
+                return insertPatchSet(db);
+              } finally {
+                db.close();
+              }
+            }
+          } finally {
+            synchronized (newProgress) {
+              replaceProgress.update(1);
+            }
+          }
+        }
+      });
+      return Futures.makeChecked(future, ORM_EXCEPTION);
+    }
+
+    PatchSet.Id insertPatchSet(ReviewDb db) throws OrmException {
       final Account.Id me = currentUser.getAccountId();
       final Set<Account.Id> reviewers = new HashSet<Account.Id>(reviewerId);
       final Set<Account.Id> cc = new HashSet<Account.Id>(ccId);
@@ -1551,7 +1632,8 @@ public class ReceiveCommits {
           mergedIntoRef = mergedInto != null ? mergedInto.getName() : null;
         }
 
-        List<PatchSetApproval> patchSetApprovals = approvalsUtil.copyVetosToLatestPatchSet(change);
+        List<PatchSetApproval> patchSetApprovals =
+            approvalsUtil.copyVetosToLatestPatchSet(db, change);
 
         final Set<Account.Id> haveApprovals = new HashSet<Account.Id>();
         oldReviewers.clear();
@@ -1566,7 +1648,8 @@ public class ReceiveCommits {
           }
         }
 
-        approvalsUtil.addReviewers(change, newPatchSet, info, reviewers, haveApprovals);
+        approvalsUtil.addReviewers(db, change, newPatchSet, info,
+            reviewers, haveApprovals);
 
         msg =
             new ChangeMessage(new ChangeMessage.Key(change.getId(), ChangeUtil
@@ -1627,7 +1710,6 @@ public class ReceiveCommits {
 
       replication.fire(project.getNameKey(), newPatchSet.getRefName());
       hooks.doPatchsetCreatedHook(change, newPatchSet, db);
-      replaceProgress.update(1);
       if (mergedIntoRef != null) {
         hooks.doChangeMergedHook(
             change, currentUser.getAccount(), newPatchSet, db);
@@ -2019,7 +2101,9 @@ public class ReceiveCommits {
       }
 
       for (final ReplaceRequest req : toClose) {
-        final PatchSet.Id psi = req.validate(true) ? req.insertPatchSet() : null;
+        final PatchSet.Id psi = req.validate(true)
+            ? req.insertPatchSet().checkedGet()
+            : null;
         if (psi != null) {
           closeChange(req.inputCommand, psi, req.newCommit);
           closeProgress.update(1);

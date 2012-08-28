0467e2a2e3c3311f8b94e06bbdf7f3c74ee70570
Edwin Kempin <edwin.kempin@sap.com>
2012-08-28 14:07:37 +0200

    Refactor MergeOp, implement each submit strategy in an own class
    The MergeOp class is difficult to maintain and to extend. It is a huge
    class that implements four different submit strategies. The code for
    the different submit strategies is only partially the same and there
    are special coding blocks which just apply to one of the submit
    strategies. The class has many member variables to hold the current
    state of the merge operation. They are read and updated in many methods
    which makes it difficult to understand the pre and post conditions for
    a method. Also some of the methods do more than their names suggest.
    
    This refactoring introduces an abstraction for a submit strategy. There
    is a new abstract base class that each submit strategy has to extend.
    By having polymorphism for the submit type the coding for the different
    submit strategies gets seperated. This makes it much easier to add a
    new submit strategy because only a clearly defined interface has to be
    implemented.
    
    The motivation for this refactoring is to support different submit
    types for changes pushed to the same destination branch. The idea is to
    control the submit type for a change from Prolog. The current MergeOp
    algorithm assumes that the submit type is the same for all changes to
    be merged. To support different submit types for changes going to the
    same destination branch MergeOp should scan all changes and determine
    their submit type, then cluster changes with the same submit type and
    loop through the submit strategies to execute those clustered groups in
    turn. This will be implemented by a follow-up change, but this is why
    the submit strategy was designed in such a way that it can be executed
    several times, each time for a different set of submitted changes.
    
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    Change-Id: I502a8a963e0c0efd97b781be9d2eb95832e346d9

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/FastForwardOnly.java
@@ -0,0 +1,64 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import static com.google.gerrit.server.git.MergeUtil.getFirstFastForward;
+import static com.google.gerrit.server.git.MergeUtil.markCleanMerges;
+import static com.google.gerrit.server.git.MergeUtil.reduceToMinimalMerge;
+
+import com.google.gerrit.reviewdb.client.PatchSetApproval;
+import com.google.gerrit.reviewdb.client.Branch.NameKey;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.IdentifiedUser.GenericFactory;
+
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevFlag;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+import java.util.List;
+import java.util.Set;
+
+public class FastForwardOnly extends SubmitStrategy {
+
+  FastForwardOnly(final GenericFactory identifiedUserFactory,
+      final PersonIdent myIdent, final ReviewDb db, final Repository repo,
+      final RevWalk rw, final ObjectInserter inserter,
+      final RevFlag canMergeFlag, final Set<RevCommit> alreadyAccepted,
+      final NameKey destBranch, final boolean useContentMerge) {
+    super(identifiedUserFactory, myIdent, db, repo, rw, inserter, canMergeFlag,
+        alreadyAccepted, destBranch, useContentMerge);
+  }
+
+  @Override
+  protected CodeReviewCommit _run(final CodeReviewCommit mergeTip,
+      final List<CodeReviewCommit> toMerge) throws MergeException {
+    reduceToMinimalMerge(mergeSorter, toMerge);
+    final CodeReviewCommit newMergeTip = getFirstFastForward(mergeTip, rw, toMerge);
+
+    while (!toMerge.isEmpty()) {
+      final CodeReviewCommit n = toMerge.remove(0);
+      n.statusCode = CommitMergeStatus.NOT_FAST_FORWARD;
+    }
+
+    final PatchSetApproval submitApproval =
+        markCleanMerges(db, rw, canMergeFlag, newMergeTip, alreadyAccepted);
+    setRefLogIdent(submitApproval);
+
+    return newMergeTip;
+  }
+}

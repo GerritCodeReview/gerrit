db0c37afa6e332ecf20a33be3d7e481cab765e3e
Edwin Kempin <edwin.kempin@sap.com>
2012-08-28 14:07:37 +0200

    Refactor MergeOp, implement each submit strategy in an own class
    The MergeOp class is difficult to maintain and to extend. It is a huge
    class that implements four different submit strategies. The code for
    the different submit strategies is only partially the same and there
    are special coding blocks which just apply to one of the submit
    strategies. The class has many member variables to hold the current
    state of the merge operation. They are read and updated in many methods
    which makes it difficult to understand the pre and post conditions for
    a method. Also some of the methods do more than their names suggest.
    
    This refactoring introduces an abstraction for a submit strategy. There
    is a new abstract base class that each submit strategy has to extend.
    By having polymorphism for the submit type the coding for the different
    submit strategies gets seperated. This makes it much easier to add a
    new submit strategy because only a clearly defined interface has to be
    implemented.
    
    The motivation for this refactoring is to support different submit
    types for changes pushed to the same destination branch. The idea is to
    control the submit type for a change from Prolog. The current MergeOp
    algorithm assumes that the submit type is the same for all changes to
    be merged. To support different submit types for changes going to the
    same destination branch MergeOp should scan all changes and determine
    their submit type, then cluster changes with the same submit type and
    loop through the submit strategies to execute those clustered groups in
    turn. This will be implemented by a follow-up change, but this is why
    the submit strategy was designed in such a way that it can be executed
    several times, each time for a different set of submitted changes.
    
    Change-Id: I5afe9fdc3e4c5a796cc2be715194effed9e34ddf
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/SubmitStrategy.java
@@ -0,0 +1,147 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.gerrit.reviewdb.client.Branch;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSetApproval;
+import com.google.gerrit.reviewdb.client.Project.SubmitType;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.IdentifiedUser;
+
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevFlag;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Base class that submit strategies must extend. A submit strategy for a
+ * certain {@link SubmitType} defines how the submitted commits should be
+ * merged.
+ */
+public abstract class SubmitStrategy {
+  protected final IdentifiedUser.GenericFactory identifiedUserFactory;
+  protected final PersonIdent myIdent;
+  protected final ReviewDb db;
+
+  protected final Repository repo;
+  protected final RevWalk rw;
+  protected final ObjectInserter inserter;
+  protected final RevFlag canMergeFlag;
+  protected final Set<RevCommit> alreadyAccepted;
+  protected final Branch.NameKey destBranch;
+  protected final boolean useContentMerge;
+  protected final MergeSorter mergeSorter;
+
+  private PersonIdent refLogIdent;
+
+  SubmitStrategy(final IdentifiedUser.GenericFactory identifiedUserFactory,
+      final PersonIdent myIdent, final ReviewDb db, final Repository repo,
+      final RevWalk rw, final ObjectInserter inserter,
+      final RevFlag canMergeFlag, final Set<RevCommit> alreadyAccepted,
+      final Branch.NameKey destBranch, final boolean useContentMerge) {
+    this.identifiedUserFactory = identifiedUserFactory;
+    this.myIdent = myIdent;
+    this.db = db;
+
+    this.repo = repo;
+    this.rw = rw;
+    this.inserter = inserter;
+    this.canMergeFlag = canMergeFlag;
+    this.alreadyAccepted = alreadyAccepted;
+    this.destBranch = destBranch;
+    this.useContentMerge = useContentMerge;
+    this.mergeSorter = new MergeSorter(rw, alreadyAccepted, canMergeFlag);
+  }
+
+  /**
+   * Runs this submit strategy. If possible the provided commits will be merged
+   * with this submit strategy.
+   *
+   * @param mergeTip the mergeTip
+   * @param toMerge the list of submitted commits that should be merged using
+   *        this submit strategy
+   * @return the new mergeTip
+   * @throws MergeException
+   */
+  public final CodeReviewCommit run(final CodeReviewCommit mergeTip,
+      final List<CodeReviewCommit> toMerge) throws MergeException {
+    refLogIdent = null;
+    return _run(mergeTip, toMerge);
+  }
+
+  /**
+   * Runs this submit strategy. If possible the provided commits will be merged
+   * with this submit strategy.
+   *
+   * @param mergeTip the mergeTip
+   * @param toMerge the list of submitted commits that should be merged using
+   *        this submit strategy
+   * @return the new mergeTip
+   * @throws MergeException
+   */
+  protected abstract CodeReviewCommit _run(CodeReviewCommit mergeTip,
+      List<CodeReviewCommit> toMerge) throws MergeException;
+
+  /**
+   * Returns the PersonIdent that should be used for the ref log entries when
+   * updating the destination branch. The ref log identity may be set after the
+   * {@link #run(CodeReviewCommit, List)} method finished.
+   *
+   * Do only call this method after the {@link #run(CodeReviewCommit, List)}
+   * method has been invoked.
+   *
+   * @return the ref log identity, may be <code>null</code>
+   */
+  public final PersonIdent getRefLogIdent() {
+    return refLogIdent;
+  }
+
+  /**
+   * Returns all commits that have been newly created for the changes that are
+   * getting merged.
+   *
+   * By default this method is returning an empty map, but subclasses may
+   * overwrite this method to provide newly created commits.
+   *
+   * Do only call this method after the {@link #run(CodeReviewCommit, List)}
+   * method has been invoked.
+   *
+   * @return new commits created for changes that are getting merged
+   */
+  public Map<Change.Id, CodeReviewCommit> getNewCommits() {
+    return Collections.emptyMap();
+  }
+
+  /**
+   * Sets the ref log identity if it wasn't set yet.
+   *
+   * @param submitApproval the approval that submitted the patch set
+   */
+  protected final void setRefLogIdent(final PatchSetApproval submitApproval) {
+    if (refLogIdent == null && submitApproval != null) {
+      refLogIdent =
+          identifiedUserFactory.create(submitApproval.getAccountId())
+              .newRefLogIdent();
+    }
+  }
+}

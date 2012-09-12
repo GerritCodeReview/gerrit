04e89633b42b966790b432d387882443e5540e36
Edwin Kempin <edwin.kempin@sap.com>
2012-09-12 09:17:16 +0200

    Refactor code for rebasing changes to make it reusable
    A new submit strategy 'Rebase If Necessary' should be introduced that
    automatically rebases the changes on submit if needed. Refactor the
    existing code for rebasing changes from the WebUI to be reusable from
    this new submit strategy.
    
    Change-Id: I0ac803deea68f9e1294b3fa5bf814277e899f075
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/changedetail/RebaseChangeHandler.java
@@ -0,0 +1,67 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd.rpc.changedetail;
+
+import com.google.gerrit.common.data.ChangeDetail;
+import com.google.gerrit.common.errors.NoSuchEntityException;
+import com.google.gerrit.httpd.rpc.Handler;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.changedetail.RebaseChange;
+import com.google.gerrit.server.mail.EmailException;
+import com.google.gerrit.server.patch.PatchSetInfoNotAvailableException;
+import com.google.gerrit.server.project.InvalidChangeOperationException;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+
+import java.io.IOException;
+
+class RebaseChangeHandler extends Handler<ChangeDetail> {
+  interface Factory {
+    RebaseChangeHandler create(PatchSet.Id patchSetId);
+  }
+
+  private final RebaseChange rebaseChange;
+  private final IdentifiedUser currentUser;
+  private final ChangeDetailFactory.Factory changeDetailFactory;
+
+  private final PatchSet.Id patchSetId;
+
+  @Inject
+  RebaseChangeHandler(final RebaseChange.Factory rebaseChangeFactory,
+      final IdentifiedUser currentUser,
+      final ChangeDetailFactory.Factory changeDetailFactory,
+      @Assisted final PatchSet.Id patchSetId) {
+    this.rebaseChange = rebaseChangeFactory.create();
+    this.currentUser = currentUser;
+    this.changeDetailFactory = changeDetailFactory;
+
+    this.patchSetId = patchSetId;
+  }
+
+  @Override
+  public ChangeDetail call() throws NoSuchChangeException, OrmException,
+      EmailException, NoSuchEntityException, PatchSetInfoNotAvailableException,
+      MissingObjectException, IncorrectObjectTypeException, IOException,
+      InvalidChangeOperationException {
+    rebaseChange.rebase(patchSetId, currentUser.getAccountId());
+    return changeDetailFactory.create(patchSetId.getParentKey()).call();
+  }
+}

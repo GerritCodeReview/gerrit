92ea5a9964d49dd59c11d62a512f28ac050a21f3
Edwin Kempin <edwin.kempin@sap.com>
2012-09-26 08:11:43 +0200

    Remove red background for outdated dependency after rebase
    In the Dependencies table on the ChangeScreen an outdated dependency is
    highlighted by a red background. If the user clicks on the rebase
    button and the rebase is successful the outdated dependency gets fixed,
    but in the Dependencies table the dependency is still highlighted by
    the red background because the corresponding style is not removed. This
    was broken by af4138b9657ca978138ca56b89380e94e7e75ab1.
    
    Change-Id: I7ae672a6adea721bac91c7b6584cca6d0b8f3a74
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
@@ -207,7 +207,9 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     if (c.getStatus() != null && c.getStatus() != Change.Status.NEW) {
       s += " (" + c.getStatus().name() + ")";
     }
+
     if (changeRowFormatter != null) {
+      removeChangeStyle(row, changeRowFormatter);
       final String rowStyle = changeRowFormatter.getRowStyle(c);
       if (rowStyle != null) {
         table.getRowFormatter().addStyleName(row, rowStyle);
@@ -224,6 +226,19 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     table.setText(row, C_LAST_UPDATE, shortFormat(c.getLastUpdatedOn()));
 
     setRowItem(row, c);
+  }
+
+  private void removeChangeStyle(int row,
+      final ChangeRowFormatter changeRowFormatter) {
+    final ChangeInfo oldChange = getRowItem(row);
+    if (oldChange == null) {
+      return;
+    }
+
+    final String oldRowStyle = changeRowFormatter.getRowStyle(oldChange);
+    if (oldRowStyle != null) {
+      table.getRowFormatter().removeStyleName(row, oldRowStyle);
+    }
   }
 
   private AccountLink link(final Account.Id id) {

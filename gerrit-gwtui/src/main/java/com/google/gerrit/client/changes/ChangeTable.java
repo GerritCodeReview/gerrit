GerritCodeReview/gerrit

commit af4138b9657ca978138ca56b89380e94e7e75ab1

Allow users of ChangeTable to control styling of change rows
Currently it is hard-coded in the ChangeTable that outdated and
abandoned changes should be highlighted. With this change users of the
ChangeTable can now control from outside which changes should be
highlighted and how the highlighting should be done.

The ChangeTable which shows the dependencies on the ChangeScreen is
using the new ability so that outdated and abandoned changes are now
only highlighted in the 'Depends On' section, but not anymore in the
'Needed By' section.

Change-Id: If3140a3ad0a47d4c6813fc3aef06adb1c959f49b
Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
@@ -191,7 +191,8 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     }
   }
 
-  private void populateChangeRow(final int row, final ChangeInfo c) {
+  private void populateChangeRow(final int row, final ChangeInfo c,
+      final ChangeRowFormatter changeRowFormatter) {
     ChangeCache cache = ChangeCache.get(c.getId());
     cache.getChangeInfoCache().set(c);
 
@@ -206,13 +207,12 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     if (c.getStatus() != null && c.getStatus() != Change.Status.NEW) {
       s += " (" + c.getStatus().name() + ")";
     }
-    if (! c.isLatest()) {
-      s += " [OUTDATED]";
-      table.getRowFormatter().addStyleName(row, Gerrit.RESOURCES.css().outdated());
-    } else if (Change.Status.ABANDONED.equals(c.getStatus())) {
-      table.getRowFormatter().addStyleName(row, Gerrit.RESOURCES.css().outdated());
-    } else {
-      table.getRowFormatter().removeStyleName(row, Gerrit.RESOURCES.css().outdated());
+    if (changeRowFormatter != null) {
+      final String rowStyle = changeRowFormatter.getRowStyle(c);
+      if (rowStyle != null) {
+        table.getRowFormatter().addStyleName(row, rowStyle);
+      }
+      s = changeRowFormatter.getDisplayText(c, s);
     }
 
     table.setWidget(row, C_SUBJECT, new TableChangeLink(s, c));
@@ -222,6 +222,7 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     table.setWidget(row, C_BRANCH, new BranchLink(c.getProject().getKey(), c
         .getStatus(), c.getBranch(), c.getTopic()));
     table.setText(row, C_LAST_UPDATE, shortFormat(c.getLastUpdatedOn()));
+
     setRowItem(row, c);
   }
 
@@ -418,6 +419,8 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     int dataBegin;
     int rows;
 
+    private ChangeRowFormatter changeRowFormatter;
+
     public Section() {
       this(null, ApprovalViewType.NONE, null);
     }
@@ -438,6 +441,10 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
       if (titleRow >= 0) {
         parent.table.setText(titleRow, 0, titleText);
       }
+    }
+
+    public void setChangeRowFormatter(final ChangeRowFormatter changeRowFormatter) {
+      this.changeRowFormatter = changeRowFormatter;
     }
 
     public void display(final List<ChangeInfo> changeList) {
@@ -469,7 +476,7 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
 
         for (int i = 0; i < sz; i++) {
           ChangeInfo c = changeList.get(i);
-          parent.populateChangeRow(dataBegin + i, c);
+          parent.populateChangeRow(dataBegin + i, c, changeRowFormatter);
           cids.add(c.getId());
         }
 
@@ -487,5 +494,26 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
         }
       }
     }
+  }
+
+  public static interface ChangeRowFormatter {
+    /**
+     * Returns the name of the CSS style that should be applied to the change
+     * row.
+     *
+     * @param c the change for which the styling should be returned
+     * @return the name of the CSS style that should be applied to the change
+     *         row
+     */
+    String getRowStyle(ChangeInfo c);
+
+    /**
+     * Returns the text that should be displayed for the change.
+     *
+     * @param c the change for which the display text should be returned
+     * @param displayText the current display text
+     * @return the new display text
+     */
+    String getDisplayText(ChangeInfo c, String displayText);
   }
 }

gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeScreen.java
@@ -183,6 +183,23 @@ public class ChangeScreen extends Screen
       }
     };
     dependsOn = new ChangeTable.Section(Util.C.changeScreenDependsOn());
+    dependsOn.setChangeRowFormatter(new ChangeTable.ChangeRowFormatter() {
+      @Override
+      public String getRowStyle(ChangeInfo c) {
+        if (! c.isLatest() || Change.Status.ABANDONED.equals(c.getStatus())) {
+          return Gerrit.RESOURCES.css().outdated();
+        }
+        return null;
+      }
+
+      @Override
+      public String getDisplayText(final ChangeInfo c, final String displayText) {
+        if (! c.isLatest()) {
+          return displayText + " [OUTDATED]";
+        }
+        return displayText;
+      }
+    });
     neededBy = new ChangeTable.Section(Util.C.changeScreenNeededBy());
     dependencies.addSection(dependsOn);
     dependencies.addSection(neededBy);

commit 6b9cc403fd43afc97532f2f007336e6ee0e56981

Remove red background for outdated dependency after rebase
In the Dependencies table on the ChangeScreen an outdated dependency is
highlighted by a red background. If the user clicks on the rebase
button and the rebase is successful the outdated dependency gets fixed,
but in the Dependencies table the dependency is still highlighted by
the red background because the corresponding style is not removed. This
was broken by af4138b9657ca978138ca56b89380e94e7e75ab1.

Change-Id: I7ae672a6adea721bac91c7b6584cca6d0b8f3a74
Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
@@ -54,6 +54,7 @@ import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.UIObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -73,6 +74,7 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
   private AccountInfoCache accountCache = AccountInfoCache.empty();
   private final List<ApprovalType> approvalTypes;
   private final int columns;
+  private final Map<Integer, String> appliedRowStyles;
 
   public ChangeTable() {
     this(false);
@@ -85,6 +87,7 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     } else {
       columns = BASE_COLUMNS;
     }
+    appliedRowStyles = new HashMap<Integer, String>();
 
     keysNavigation.add(new PrevKeyCommand(0, 'k', Util.C.changeTablePrev()));
     keysNavigation.add(new NextKeyCommand(0, 'j', Util.C.changeTableNext()));
@@ -207,10 +210,16 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     if (c.getStatus() != null && c.getStatus() != Change.Status.NEW) {
       s += " (" + c.getStatus().name() + ")";
     }
+
+    final String oldRowStyle = appliedRowStyles.remove(row);
+    if (oldRowStyle != null) {
+      table.getRowFormatter().removeStyleName(row, oldRowStyle);
+    }
     if (changeRowFormatter != null) {
       final String rowStyle = changeRowFormatter.getRowStyle(c);
       if (rowStyle != null) {
         table.getRowFormatter().addStyleName(row, rowStyle);
+        appliedRowStyles.put(row, rowStyle);
       }
       s = changeRowFormatter.getDisplayText(c, s);
     }
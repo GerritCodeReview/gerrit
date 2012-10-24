7ddebdd6068b77610e4b9eadcb512f47325a7afc
Bruce Zu <bruce.zu@sonymobile.com>
2012-10-24 21:26:49 +0800

    Click icon to add file comment in Unified view.
    In the table header of Unified diff view
    add file level comment (comment on the whole file)
    by single click on the icon in the line-number
    column header.
    
    Change-Id: Ifa2b1140ee4748dedf2c2eb76623d4fea13d16b2

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/patches/AbstractPatchContentTable.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/patches/AbstractPatchContentTable.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2008 The Android Open Source Project
+//Copyright (C) 2008 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -444,6 +444,9 @@ public abstract class AbstractPatchContentTable extends NavigationTable<Object>
     }
   }
 
+  protected void updateCursor(final PatchLineComment newComment) {
+  }
+
   abstract void insertFileCommentRow(final int row);
 
   private CommentEditorPanel findOrCreateCommentEditor(final int suggestRow,
@@ -547,6 +550,7 @@ public abstract class AbstractPatchContentTable extends NavigationTable<Object>
       }
     }
 
+    updateCursor(newComment);
     return ed;
   }
 

d4c617cc30fe858f74a857438bc2bb35c90d3e2c
David Pursehouse <david.pursehouse@sonymobile.com>
2012-10-29 22:53:47 +0900

    gerrit-pgm: Remove some unused imports
    Change-Id: Iab9c0fcc5bcf7892b048858e286e4ff34fbba1fd

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitPluginStepsLoader.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitPluginStepsLoader.java
@@ -22,9 +22,6 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.FileFilter;
 import java.net.URL;

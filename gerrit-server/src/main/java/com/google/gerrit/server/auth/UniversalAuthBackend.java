ad5b2330ac1c07d1839f9210a5700e774c9a40b2
Luca Milanesio <luca.milanesio@gmail.com>
2012-11-12 10:51:32 -0800

    Added pluggable API for authentication backends.
    An AuthBackend authenticates an incoming request and provides the
    authenticated user. An incoming request is represented as an
    AuthRequest. Each protocol is responsible for parsing the request and
    creating the AuthRequest e.g Digest HTTP, PubKey SSH, etc.
    
    The core server defines two AuthBackends in this change:
      Internal: gerrit DB password based authentication
      Ldap: delegates the password verification to the LDAP server
    
    The allowed AuthBackends are bound in a DynamicSet which the
    UniversalAuthBackend handles by attempting to authenticate the request
    with every backend. It is expected that at most one AuthBackend will
    successfully authenticate a user. It is considered an error if more than
    one user is authenticated. If this is an issue for a service, consider
    separating users by username, so a backend can quickly determine if it
    should consider the request.
    
    After a user is authenticated, an AuthUser object is returned with a
    globally unique UUID. More work needs to be done to join the concept of
    the AuthUser UUID and the Account ID.
    
    The AuthBackend is marked as an ExtensionPoint so it may be implemented
    as a plugin.
    
    Change-Id: I645262519f15806517c7ff4058ed6c5aabf8e754

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/auth/UniversalAuthBackend.java
@@ -0,0 +1,73 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.auth;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.collect.Lists;
+import com.google.gerrit.extensions.registration.DynamicSet;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Universal implementation of the AuthBackend that works with the injected
+ * set of AuthBackends.
+ */
+@Singleton
+public final class UniversalAuthBackend implements AuthBackend {
+  private final DynamicSet<AuthBackend> authBackends;
+
+  @Inject
+  UniversalAuthBackend(DynamicSet<AuthBackend> authBackends) {
+    this.authBackends = authBackends;
+  }
+
+  @Override
+  public AuthUser authenticate(final AuthRequest request) throws AuthException {
+    List<AuthUser> authUsers = Lists.newArrayList();
+    List<AuthException> authExs = Lists.newArrayList();
+    for (AuthBackend backend : authBackends) {
+      try {
+        authUsers.add(checkNotNull(backend.authenticate(request)));
+      } catch (MissingCredentialsException ex) {
+        // Not handled by this backend.
+      } catch (AuthException ex) {
+        authExs.add(ex);
+      }
+    }
+
+    // Handle the valid responses
+    if (authUsers.size() == 1) {
+      return authUsers.get(0);
+    } else if (authUsers.isEmpty() && authExs.size() == 1) {
+      throw authExs.get(0);
+    } else if (authExs.isEmpty() && authUsers.isEmpty()) {
+      throw new MissingCredentialsException();
+    }
+
+    String msg = String.format("Multiple AuthBackends attempted to handle request:"
+        + " authUsers=%s authExs=%s", authUsers, authExs);
+    throw new AuthException(msg);
+  }
+
+  @Override
+  public String getDomain() {
+    throw new UnsupportedOperationException(
+        "UniversalAuthBackend doesn't support domain.");
+  }
+}

17d47815530a81fccd928f059593ceca07705656
Luca Milanesio <luca.milanesio@gmail.com>
2012-11-12 10:51:32 -0800

    Added new AuthRequest for allowing plugin-based authentication.
    AuthRequest allows the new auth plugin infrastructure
    to get a clean hierarchy of authentication request
    across all different protocols.
    
    i.e.
    gerrit-httpd will define its own subclass
    (HttpAuthRequest)
    gerrit-sshd will define its won subclass
    (SshAuthRequest)
    
    Change-Id: Ic60bc1bcd5c5c37363a0e4d9de1b32ecff36ac8a

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/auth/AuthRequest.java
@@ -0,0 +1,39 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.auth;
+
+import javax.annotation.Nullable;
+
+/**
+ * Defines an abstract request for user authentication to Gerrit.
+ */
+public abstract class AuthRequest {
+
+  /**
+   * Returns the username to be authenticated.
+   *
+   * @return username for authentication or null for anonymous access.
+   */
+  @Nullable
+  public abstract String getUsername();
+
+  /**
+   * Returns the user's credentials
+   *
+   * @return user's credentials or null
+   */
+  @Nullable
+  public abstract String getPassword();
+}

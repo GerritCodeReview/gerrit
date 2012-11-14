b78ce1cc4891f5dee37f3b346e92b765b67bd9ee
Chad Horohoe <chorohoe@wikimedia.org>
2012-11-14 09:38:50 -0800

    Allow group includes to be by UUID instead of group ID
    Per discussion on-list, the behavior change in 2.5 to only
    allow groups by ID is considered a regression for groups
    that want to include external groups (like LDAP).
    
    In order to permit smooth migration, we want to allow both
    ID and UUIDs to co-exist for the time being.
    
    Change-Id: I0dbdb15b9c62f2dbce64acbc34c515c7b8229c04

--- gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/server/ReviewDb.java
+++ gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/server/ReviewDb.java
@@ -112,6 +112,12 @@ public interface ReviewDb extends Schema {
   @Relation(id = 28)
   SubmoduleSubscriptionAccess submoduleSubscriptions();
 
+  @Relation(id = 29)
+  AccountGroupIncludeByUuidAccess accountGroupIncludesByUuid();
+
+  @Relation(id = 30)
+  AccountGroupIncludeByUuidAuditAccess accountGroupIncludesByUuidAudit();
+
   /** Create the next unique id for an {@link Account}. */
   @Sequence(startWith = 1000000)
   int nextAccountId() throws OrmException;

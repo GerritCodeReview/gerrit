a7d9c9b7ac203bb0dc89583889633a8f8d266cc1
Shawn O. Pearce <sop@google.com>
2012-11-16 15:00:21 -0800

    Manage default dashboard by PUT and DELETE
    PUT with an id to /default will set the local-default property and
    commit this change to refs/meta/config.
    
    DELETE on /default will remove the local-default property.
    
    Change-Id: If804762df9fa11dcb3a7ceebc5867ace70afdb35

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/DeleteDashboard.java
@@ -0,0 +1,56 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.project;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.MethodNotAllowedException;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.project.DeleteDashboard.Input;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+class DeleteDashboard implements RestModifyView<DashboardResource, Input> {
+  static class Input {
+    String commitMessage;
+  }
+
+  private final Provider<SetDefaultDashboard> defaultSetter;
+
+  @Inject
+  DeleteDashboard(Provider<SetDefaultDashboard> defaultSetter) {
+    this.defaultSetter = defaultSetter;
+  }
+
+  @Override
+  public Class<Input> inputType() {
+    return Input.class;
+  }
+
+  @Override
+  public Object apply(DashboardResource resource, Input input)
+      throws AuthException, BadRequestException, ResourceConflictException,
+      Exception {
+    if (resource.isProjectDefault()) {
+      SetDashboard.Input in = new SetDashboard.Input();
+      in.commitMessage = input != null ? input.commitMessage : null;
+      return defaultSetter.get().apply(resource, in);
+    }
+
+    // TODO: Implement delete of dashboards by API.
+    throw new MethodNotAllowedException();
+  }
+}

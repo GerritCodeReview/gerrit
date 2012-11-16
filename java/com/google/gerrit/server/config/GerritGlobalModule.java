3e6b1a70bd2e2541dbb018b1f2bc03b277de1d25
Dariusz Luksza <dariusz@luksza.org>
2012-11-16 21:07:53 +0100

    Add RealmBackend extension point
    Adds RealmBackend extension point with two default implementations:
     * InternalRealmBackend - to retrieve data from internal DB
     * UniversalRealmBackend - to search all bound backends for user data
    
    RealmBackend's base on given AuthUser should return user additional data
    like:
     * display name
     * email address
    
    Change-Id: Iacf980e45832719c21e9cb3f2eb9512257aeeb2a
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -94,7 +94,10 @@ import com.google.gerrit.server.account.VersionedAuthorizedKeys;
 import com.google.gerrit.server.account.externalids.ExternalIdModule;
 import com.google.gerrit.server.audit.AuditModule;
 import com.google.gerrit.server.auth.AuthBackend;
+import com.google.gerrit.server.auth.InternalRealmBackend;
+import com.google.gerrit.server.auth.RealmBackend;
 import com.google.gerrit.server.auth.UniversalAuthBackend;
+import com.google.gerrit.server.auth.UniversalRealmBackend;
 import com.google.gerrit.server.auth.oauth.OAuthTokenCache;
 import com.google.gerrit.server.avatar.AvatarProvider;
 import com.google.gerrit.server.cache.CacheRemovalListener;
@@ -274,6 +277,10 @@ public class GerritGlobalModule extends FactoryModule {
 
     bind(AuthBackend.class).to(UniversalAuthBackend.class).in(SINGLETON);
     DynamicSet.setOf(binder(), AuthBackend.class);
+
+    bind(RealmBackend.class).to(UniversalRealmBackend.class).in(SINGLETON);
+    DynamicSet.setOf(binder(), RealmBackend.class);
+    DynamicSet.bind(binder(), RealmBackend.class).to(InternalRealmBackend.class);
 
     bind(GroupControl.Factory.class).in(SINGLETON);
     bind(GroupControl.GenericFactory.class).in(SINGLETON);

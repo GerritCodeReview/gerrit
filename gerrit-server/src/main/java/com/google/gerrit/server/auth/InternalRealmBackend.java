974c6d1ccc0a4fa8e8fb1687fae671dde728bc2e
Dariusz Luksza <dariusz@luksza.org>
2012-11-16 21:07:53 +0100

    Add RealmBackend extension point
    Adds RealmBackend extension point with two default implementations:
     * InternalRealmBackend - to retrieve data from internal DB
     * UniversalRealmBackend - to search all bound backends for user data
    
    RealmBackend's base on given AuthUser should return user additional data
    like:
     * display name
     * email address
    
    Change-Id: Iacf980e45832719c21e9cb3f2eb9512257aeeb2a
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/auth/InternalRealmBackend.java
@@ -0,0 +1,61 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.auth;
+
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountExternalId;
+import com.google.gerrit.server.account.AccountCache;
+import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.auth.AuthUser.UUID;
+import com.google.gerrit.server.auth.UserData.Builder;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import javax.annotation.Nullable;
+
+/**
+ * Search user data in internal DB
+ */
+@Singleton
+public class InternalRealmBackend implements RealmBackend {
+  private AccountCache accountCache;
+
+  @Inject
+  InternalRealmBackend(AccountCache accountCache) {
+    this.accountCache = accountCache;
+  }
+
+  @Override
+  public boolean handles(UUID uuid) {
+    return uuid.get().startsWith(AccountExternalId.SCHEME_GERRIT);
+  }
+
+  @Override
+  @Nullable
+  public UserData getUserData(AuthUser user) {
+    AccountState accountState = accountCache.getByUsername(user.getUsername());
+    if (accountState != null && accountState.getAccount() != null) {
+      Account account = accountState.getAccount();
+      Builder builder = new UserData.Builder(user.getUsername());
+      builder.setDisplayName(account.getFullName());
+      builder.setEmailAddress(account.getPreferredEmail());
+      builder.setExternalId(user.getUUID().get());
+
+      return builder.build();
+    }
+    return null;
+  }
+
+}
--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -51,7 +51,10 @@ import com.google.gerrit.server.account.Realm;
 import com.google.gerrit.server.account.UniversalGroupBackend;
 import com.google.gerrit.server.auth.AuthBackend;
 import com.google.gerrit.server.auth.InternalAuthBackend;
+import com.google.gerrit.server.auth.InternalRealmBackend;
+import com.google.gerrit.server.auth.RealmBackend;
 import com.google.gerrit.server.auth.UniversalAuthBackend;
+import com.google.gerrit.server.auth.UniversalRealmBackend;
 import com.google.gerrit.server.auth.ldap.LdapGroupBackend;
 import com.google.gerrit.server.auth.ldap.LdapModule;
 import com.google.gerrit.server.auth.ldap.LdapRealmModule;
@@ -163,6 +166,11 @@ public class GerritGlobalModule extends FactoryModule {
     bind(AuthBackend.class).to(UniversalAuthBackend.class).in(SINGLETON);
     DynamicSet.setOf(binder(), AuthBackend.class);
     DynamicSet.bind(binder(), AuthBackend.class).to(InternalAuthBackend.class);
+
+    bind(RealmBackend.class).to(UniversalRealmBackend.class).in(SINGLETON);
+    DynamicSet.setOf(binder(), RealmBackend.class);
+    DynamicSet.bind(binder(), RealmBackend.class).to(InternalRealmBackend.class);
+
 
     bind(GroupControl.Factory.class).in(SINGLETON);
     factory(IncludingGroupMembership.Factory.class);

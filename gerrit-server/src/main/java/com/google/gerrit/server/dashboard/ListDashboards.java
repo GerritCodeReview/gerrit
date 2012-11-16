f45a0ba195f8026b2650abf59cbd6b77060ade0d
Martin Fick <mogulguy10@gmail.com>
2012-11-15 19:46:54 -0700

    Add a section name to project dashboards
    The section name is currently equal to the dashboard ref
    which includes refs/meta/dashboards.  Since this is ugly
    to display to users, strip it off the section name.
    
    Change-Id: Iee135aaecb07c65867a56ebe1107fdb373aec315

--- gerrit-server/src/main/java/com/google/gerrit/server/dashboard/ListDashboards.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/dashboard/ListDashboards.java
@@ -346,6 +346,7 @@ public class ListDashboards {
       throws IOException, ConfigInvalidException {
     DashboardInfo info = new DashboardInfo();
     info.dashboardName = path;
+    info.section = refName.substring(REFS_DASHBOARDS.length());
     info.refName = refName;
     info.projectName = project.getName();
     info.id = createId(info.refName, info.dashboardName);
@@ -386,6 +387,7 @@ public class ListDashboards {
     final String kind = "gerritcodereview#dashboard";
     String id;
     String dashboardName;
+    String section;
     String refName;
     String projectName;
     String description;

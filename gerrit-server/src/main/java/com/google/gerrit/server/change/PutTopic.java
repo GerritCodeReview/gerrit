2a0dc00dfd9e2bd9f3b36f67bffeda1b39a17306
David Pursehouse <david.pursehouse@sonymobile.com>
2012-11-21 15:50:26 +0900

    Fix: Topic name cannot be force-edited on closed change
    There is a permission setting to allow a change's topic to be
    edited when the change is closed, but this does not work any more
    because the refactoring of topic name editing to use the REST API
    in change Ia5edbb232bc288acdad8b145956ad275d170629a adds explicit
    checks for the change being open before allowing the edit.
    
    Remove the checks to make force-editing work again.
    
    Change-Id: Ib83451c9c7f373ca64a395b804ed337a7026aacd

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PutTopic.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PutTopic.java
@@ -64,8 +64,6 @@ class PutTopic implements RestModifyView<ChangeResource, Input> {
     Change change = req.getChange();
     if (!control.canEditTopicName()) {
       throw new AuthException("changing topic not permitted");
-    } else if (!change.getStatus().isOpen()) {
-      throw new ResourceConflictException("change is " + status(change));
     }
 
     ReviewDb db = dbProvider.get();
@@ -98,11 +96,8 @@ class PutTopic implements RestModifyView<ChangeResource, Input> {
         new AtomicUpdate<Change>() {
           @Override
           public Change update(Change change) {
-            if (change.getStatus().isOpen()) {
-              change.setTopic(Strings.emptyToNull(newTopicName));
-              return change;
-            }
-            return null;
+            change.setTopic(Strings.emptyToNull(newTopicName));
+            return change;
           }
         });
       if (updatedChange == null) {

ec6b5a337939da006851fe15ba0376b288fd6082
Shawn O. Pearce <sop@google.com>
2012-11-22 21:46:05 -0800

    Accept simple form encoded data for REST APIs
    Simple cases like /review or /abandon can now accept standard form
    values for basic properties, making it simple for tools to directly
    post data:
    
      curl -n --digest \
      --data 'message=Does not compile.' \
      --data labels.Verified=-1 \
      http://localhost:8080/a/changes/3/revisions/1/review
    
    Form field names are JSON field names in the top level object.  If dot
    appears in the name the part to the left is taken as the JSON field
    name and the part to the right as the key for a Map. This nicely fits
    with the labels structure used by /review, but doesn't support the
    much more complex inline comment case. Clients that need to use more
    complex fields must use JSON formatting for the request body.
    
    Change-Id: Ica3db5db43c2fbc84f4cf7f36677e0cbbd116f26

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/restapi/ParameterParser.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/restapi/ParameterParser.java
@@ -18,12 +18,20 @@ import static com.google.gerrit.httpd.restapi.RestApiServlet.replyError;
 import static com.google.gerrit.httpd.restapi.RestApiServlet.replyText;
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
+import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.server.util.Url;
 import com.google.gerrit.util.cli.CmdLineParser;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.google.inject.Inject;
 
 import org.kohsuke.args4j.CmdLineException;
@@ -31,6 +39,8 @@ import org.kohsuke.args4j.CmdLineException;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -90,5 +100,115 @@ class ParameterParser {
         }
       }
     }
+  }
+
+  private static Set<String> query(HttpServletRequest req) {
+    Set<String> params = Sets.newHashSet();
+    if (!Strings.isNullOrEmpty(req.getQueryString())) {
+      for (String kvPair : Splitter.on('&').split(req.getQueryString())) {
+        params.add(Iterables.getFirst(
+            Splitter.on('=').limit(2).split(kvPair),
+            null));
+      }
+    }
+    return params;
+  }
+
+  /**
+   * Convert a standard URL encoded form input into a parsed JSON tree.
+   * <p>
+   * Given an input such as:
+   *
+   * <pre>
+   * message=Does+not+compile.&labels.Verified=-1
+   * </pre>
+   *
+   * which is easily created using the curl command line tool:
+   *
+   * <pre>
+   * curl --data 'message=Does not compile.' --data labels.Verified=-1
+   * </pre>
+   *
+   * converts to a JSON object structure that is normally expected:
+   *
+   * <pre>
+   * {
+   *   "message": "Does not compile.",
+   *   "labels": {
+   *     "Verified": "-1"
+   *   }
+   * }
+   * </pre>
+   *
+   * This input can then be further processed into the Java input type expected
+   * by a view using Gson. Here we rely on Gson to perform implicit conversion
+   * of a string {@code "-1"} to a number type when the Java input type expects
+   * a number.
+   * <p>
+   * Conversion assumes any field name that does not contain {@code "."} will be
+   * a property of the top level input object. Any field with a dot will use the
+   * first segment as the top level property name naming an object, and the rest
+   * of the field name as a property in the nested object.
+   *
+   * @param req request to parse form input from and create JSON tree.
+   * @return the converted JSON object tree.
+   * @throws BadRequestException the request cannot be cast, as there are
+   *         conflicting definitions for a nested object.
+   */
+  static JsonObject formToJson(HttpServletRequest req)
+      throws BadRequestException {
+    @SuppressWarnings("unchecked")
+    Map<String, String[]> map = req.getParameterMap();
+    return formToJson(map, query(req));
+  }
+
+  @VisibleForTesting
+  static JsonObject formToJson(Map<String, String[]> map, Set<String> query)
+      throws BadRequestException {
+    JsonObject inputObject = new JsonObject();
+    for (Map.Entry<String, String[]> ent : map.entrySet()) {
+      String key = ent.getKey();
+      String[] values = ent.getValue();
+
+      if (query.contains(key) || values.length == 0) {
+        // Disallow processing query parameters as input body fields.
+        // Implementations of views should avoid duplicate naming.
+        continue;
+      }
+
+      JsonObject obj = inputObject;
+      int dot = key.indexOf('.');
+      if (0 <= dot) {
+        String property = key.substring(0, dot);
+        JsonElement e = inputObject.get(property);
+        if (e == null) {
+          obj = new JsonObject();
+          inputObject.add(property, obj);
+        } else if (e.isJsonObject()) {
+          obj = e.getAsJsonObject();
+        } else {
+          throw new BadRequestException(String.format(
+              "key %s conflicts with %s",
+              key, property));
+        }
+        key = key.substring(dot + 1);
+      }
+
+      if (obj.get(key) != null) {
+        // This error should never happen. If all form values are handled
+        // together in a single pass properties are set only once. Setting
+        // again indicates something has gone very wrong.
+        throw new BadRequestException("invalid form input, use JSON instead");
+      } else if (values.length == 1) {
+        obj.addProperty(key, values[0]);
+      } else {
+        JsonArray list = new JsonArray();
+        for (String v : values) {
+          list.add(new JsonPrimitive(v));
+        }
+        obj.add(key, list);
+      }
+    }
+    return inputObject;
   }
 }

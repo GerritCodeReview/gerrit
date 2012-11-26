4d1ba8987e9ca3ae23ec0f5d5ac8b411d78594aa
Shawn O. Pearce <sop@google.com>
2012-11-26 13:48:14 -0800

    Fix missing import of Gerrit in ProjectDashboardScreen
    Change-Id: I025aaf8143284bff3078ebe9cfa0a3c71754af14

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ProjectDashboardScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ProjectDashboardScreen.java
@@ -14,6 +14,7 @@
 
 package com.google.gerrit.client.changes;
 
+import com.google.gerrit.client.Gerrit;
 import com.google.gerrit.client.admin.ProjectScreen;
 import com.google.gerrit.client.ui.InlineHyperlink;
 import com.google.gerrit.common.PageLinks;

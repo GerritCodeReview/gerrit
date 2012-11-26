48534e95807639694d01feb8679d90c64b739daa
David Pursehouse <david.pursehouse@sonymobile.com>
2012-11-26 21:07:15 +0900

    [RFC] Refactor the commit validation API
    Commit validation plugins should now raise an exception on
    validation faliure, and on success return a list of messages
    which may be empty.
    
    Change-Id: Idada034feaeef1524a607fd8b31d108c1b14ab8d

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -24,7 +24,6 @@ import static org.eclipse.jgit.transport.ReceiveCommand.Result.REJECTED_OTHER_RE
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
-import com.google.common.base.Strings;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.ListMultimap;
@@ -62,9 +61,9 @@ import com.google.gerrit.server.config.TrackingFooters;
 import com.google.gerrit.server.events.CommitReceivedEvent;
 import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
 import com.google.gerrit.server.git.MultiProgressMonitor.Task;
+import com.google.gerrit.server.git.validators.CommitValidationException;
 import com.google.gerrit.server.git.validators.CommitValidationListener;
 import com.google.gerrit.server.git.validators.CommitValidationMessage;
-import com.google.gerrit.server.git.validators.CommitValidationResult;
 import com.google.gerrit.server.mail.CreateChangeSender;
 import com.google.gerrit.server.mail.MergedSender;
 import com.google.gerrit.server.mail.ReplacePatchSetSender;
@@ -2051,16 +2050,16 @@ public class ReceiveCommits {
       }
     }
 
+    // Execute commit validation plugins
     for (CommitValidationListener validator : commitValidators) {
-      CommitValidationResult validationResult =
-          validator.onCommitReceived(new CommitReceivedEvent(cmd, project, ctl
-              .getRefName(), c, currentUser));
-      final String message = validationResult.getValidationReason();
-      if (!validationResult.isValidated()) {
-        reject(cmd, message);
+      try {
+        List<CommitValidationMessage> results =
+            validator.onCommitReceived(new CommitReceivedEvent(cmd, project, ctl
+                .getRefName(), c, currentUser));
+        messages.addAll(results);
+      } catch (CommitValidationException error) {
+        reject(cmd, error.getMessage());
         return false;
-      } else if (!Strings.isNullOrEmpty(message)) {
-        addMessage(String.format("(W) %s", message));
       }
     }
 

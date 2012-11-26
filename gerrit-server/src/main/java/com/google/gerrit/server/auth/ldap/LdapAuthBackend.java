137239608594928992e9a11cf12336b0c2600855
Dariusz Luksza <dariusz@luksza.org>
2012-11-26 12:00:32 +0100

    Include domain name in AuthUser.UUID
    This will simplify things when we would deal with obtaining additional
    user information from backed.
    
    Change-Id: I958556e1e426bd36729b53047035433b9cbb51b6
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-server/src/main/java/com/google/gerrit/server/auth/ldap/LdapAuthBackend.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/auth/ldap/LdapAuthBackend.java
@@ -91,7 +91,7 @@ public class LdapAuthBackend implements AuthBackend {
           //
           helper.authenticate(m.getDN(), req.getPassword());
         }
-        return new AuthUser(new AuthUser.UUID(username), username);
+        return new AuthUser(new AuthUser.UUID(getDomain(), username), username);
       } finally {
         try {
           ctx.close();

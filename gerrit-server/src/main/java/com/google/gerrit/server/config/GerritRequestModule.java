5b25923aa38bbb29e21015c1ce9472f87b38abb7
Gustaf Lundh <gustaf.lundh@sonymobile.com>
2012-11-29 03:52:53 -0800

    Refactored out commit validation code from ReceiveCommits.
    Refactored out commit validation logic from ReceiveCommits to
    a new CommitValidators class. Other parts of Gerrit that allow
    the user to create new Changes (CherryPick, Revert, Rebase)
    should be able to reuse selected parts of this validation code.
    
    Each separate entity of validation logic will now reside its own
    class, implementing the CommitValidationListener interface. While
    this approach will generate some more code, the maintainability
    should be far more sustainable, especially when we start to reuse
    the code in other places in Gerrit.
    
    In the future, some generalization of the error messages will
    probably be needed so they fit a broader scope and not only when
    the user is pushing the change using Git.
    
    Change-Id: I99c807742ae06571c023249f7d965b633359e7ac

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritRequestModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritRequestModule.java
@@ -34,6 +34,7 @@ import com.google.gerrit.server.git.MergeOp;
 import com.google.gerrit.server.git.MetaDataUpdate;
 import com.google.gerrit.server.git.NotesBranchUtil;
 import com.google.gerrit.server.git.SubmoduleOp;
+import com.google.gerrit.server.git.validators.CommitValidators;
 import com.google.gerrit.server.mail.AddReviewerSender;
 import com.google.gerrit.server.mail.CommitMessageEditedSender;
 import com.google.gerrit.server.mail.CreateChangeSender;
@@ -93,5 +94,6 @@ public class GerritRequestModule extends FactoryModule {
     factory(CreateProject.Factory.class);
     factory(SuggestParentCandidates.Factory.class);
     factory(BanCommit.Factory.class);
+    factory(CommitValidators.Factory.class);
   }
 }

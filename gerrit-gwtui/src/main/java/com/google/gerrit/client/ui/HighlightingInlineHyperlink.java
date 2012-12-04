e4006e7de1ee7d83e8fe889a54c458024eedabb4
Edwin Kempin <edwin.kempin@sap.com>
2012-12-04 16:39:40 +0100

    Highlight the filter string in project names
    When filtering projects on the ProjectListScreen highlight the filter
    string by bold printing in the project names so that users can easily
    spot the match.
    
    Change-Id: I7a83192bfcf0d5ab6157585f196aac801587e3bd
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/ui/HighlightingInlineHyperlink.java
@@ -0,0 +1,61 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.ui;
+
+import com.google.gwtexpui.safehtml.client.SafeHtmlBuilder;
+
+public class HighlightingInlineHyperlink extends InlineHyperlink {
+
+  private String toHighlight;
+
+  public HighlightingInlineHyperlink(final String text, final String token,
+      final String toHighlight) {
+    super(text, token);
+    this.toHighlight = toHighlight;
+    highlight(text, toHighlight);
+  }
+
+
+  @Override
+  public void setText(String text) {
+    super.setText(text);
+    highlight(text, toHighlight);
+  }
+
+  private void highlight(final String text, final String toHighlight) {
+    if (toHighlight == null || "".equals(toHighlight)) {
+      return;
+    }
+
+    final SafeHtmlBuilder b = new SafeHtmlBuilder();
+    int pos = 0;
+    int endPos = 0;
+    while ((pos = text.toLowerCase().indexOf(
+        toHighlight.toLowerCase(), pos)) > -1) {
+      if (pos > endPos) {
+        b.append(text.substring(endPos, pos));
+      }
+      endPos = pos + toHighlight.length();
+      b.openElement("b");
+      b.append(text.substring(pos, endPos));
+      b.closeElement("b");
+      pos = endPos;
+    }
+    if (endPos < text.length()) {
+      b.append(text.substring(endPos));
+    }
+    setHTML(b.toSafeHtml().asString());
+  }
+}

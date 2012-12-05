de1911bfac65bd10fa91ac9c710ceab0523416a8
Bruce Zu <bruce.zu@sonymobile.com>
2012-12-05 16:40:24 +0800

    Fix: Wrong project name end with more than one '.git'
    Gerrit drop the trailing ".git" suffix, which we never keep
    in our database, but clients might mistakenly provide anyway.
    
    'p'-> 'p'; 'p.git' -> 'p'; 'p/.git' -> 'p'.
    
    thus users are free to provide 'p', 'p.git' or 'p/.git' with
    same result. But project name ending with more than one '.git'
    like 'p.git.git' or 'p/.git.git' should be rejected, becasue
    Gerrit will convert it into 'p.git'( or 'p/.git'), then Jgit
    will think of it as project 'p' if it exists, see [2], then
    Gerrit will create dirty data in RepositoryCache and
    ProjectCache by adding one new item whose key is the converted
    'p.git'( or 'p/.git'), NOT 'p', accordingly dirty data DB is
    created too in DB, e.g. issue 1699.
    
    This commit fixes it by
    1> Extract the project checking logic out into a public function
    in [1] and making it as an early verification before preparing
    ProjectControl object.
    2> Forbid clients providing project name ending with more than
    one '.git'.
    
    This commit also shifts the error message a bit to make it easy
    to understand.
    
    [1] com.google.gerrit.server.git.LocalDiskRepositoryManager.
    [2] resolve() of org.eclipse.jgit.lib.RespositoryCache$FileKey;
        getProjectName() of
        com.google.gerrit.server.git.LocalDiskRepositoryManager.
    
    Bug: Issue 1699
    Change-Id: I1a884b1e9c95fb940af5d9e8d3d5a074e6388da5

--- gerrit-server/src/main/java/com/google/gerrit/server/args4j/ProjectControlHandler.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/args4j/ProjectControlHandler.java
@@ -20,6 +20,7 @@ import com.google.gerrit.server.project.ProjectControl;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
+import org.eclipse.jgit.lib.Constants;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.OptionDef;
@@ -42,21 +43,9 @@ public class ProjectControlHandler extends OptionHandler<ProjectControl> {
   @Override
   public final int parseArguments(final Parameters params)
       throws CmdLineException {
-    final String token = params.getParameter(0);
-    String projectName = token;
-
+    String projectName = params.getParameter(0);
     while (projectName.endsWith("/")) {
       projectName = projectName.substring(0, projectName.length() - 1);
-    }
-
-    if (projectName.endsWith(".git")) {
-      // Be nice and drop the trailing ".git" suffix, which we never keep
-      // in our database, but clients might mistakenly provide anyway.
-      //
-      projectName = projectName.substring(0, projectName.length() - 4);
-      while (projectName.endsWith("/")) {
-        projectName = projectName.substring(0, projectName.length() - 1);
-      }
     }
 
     while (projectName.startsWith("/")) {
@@ -67,13 +56,27 @@ public class ProjectControlHandler extends OptionHandler<ProjectControl> {
       //
       projectName = projectName.substring(1);
     }
+    String projectNameWithoutSuffix = projectName;
+    if (projectName.endsWith(Constants.DOT_GIT_EXT)) {
+      // Be nice and drop the trailing ".git" suffix, which we never keep
+      // in our database, but clients might mistakenly provide anyway.
+      // 'p'-> 'p'; 'p.git' -> 'p'; 'p/.git' -> 'p'.
+      //
+      //
+      projectNameWithoutSuffix =
+          projectName.substring(0, projectName.length() - 4);
+      while (projectNameWithoutSuffix.endsWith("/")) {
+        projectNameWithoutSuffix =
+            projectName.substring(0, projectName.length() - 1);
+      }
+    }
 
     final ProjectControl control;
     try {
-      Project.NameKey nameKey = new Project.NameKey(projectName);
+      Project.NameKey nameKey = new Project.NameKey(projectNameWithoutSuffix);
       control = projectControlFactory.validateFor(nameKey, ProjectControl.OWNER | ProjectControl.VISIBLE);
     } catch (NoSuchProjectException e) {
-      throw new CmdLineException(owner, "'" + token + "': not a Gerrit project");
+      throw new CmdLineException(owner, "'" + projectName + "': is not a Gerrit project");
     }
 
     setter.addValue(control);

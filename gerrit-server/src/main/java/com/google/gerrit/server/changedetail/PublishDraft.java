013229c9a4ef44073b70b2f7bef6b73956816719
Edwin Kempin <edwin.kempin@sap.com>
2013-01-04 14:06:53 +0100

    Notify project watchers if draft change is published
    Watchers that want to be notified on New Changes don't get notified
    when a draft change is uploaded because the draft change is not visible
    to them (the draft change is only visible to the owner). This is
    correct but the watchers should get notified when the draft change gets
    published because now the change appears as a new change. The watchers
    want to be notified about all new changes and don't care whether a
    change was first staged as draft or not.
    
    Change-Id: I6c3f863f9f88bf54bd2f005351e1473bf133b423
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/changedetail/PublishDraft.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/changedetail/PublishDraft.java
@@ -19,8 +19,13 @@ import com.google.gerrit.common.ChangeHooks;
 import com.google.gerrit.common.data.ReviewResult;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.PatchSetInfo;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.ChangeUtil;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.mail.CreateChangeSender;
+import com.google.gerrit.server.patch.PatchSetInfoFactory;
+import com.google.gerrit.server.patch.PatchSetInfoNotAvailableException;
 import com.google.gerrit.server.project.ChangeControl;
 import com.google.gerrit.server.project.NoSuchChangeException;
 import com.google.gwtorm.server.AtomicUpdate;
@@ -28,9 +33,14 @@ import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.Callable;
 
 public class PublishDraft implements Callable<ReviewResult> {
+  private static final Logger log =
+      LoggerFactory.getLogger(PublishDraft.class);
 
   public interface Factory {
     PublishDraft create(PatchSet.Id patchSetId);
@@ -39,22 +49,29 @@ public class PublishDraft implements Callable<ReviewResult> {
   private final ChangeControl.Factory changeControlFactory;
   private final ReviewDb db;
   private final ChangeHooks hooks;
+  private final PatchSetInfoFactory patchSetInfoFactory;
+  private final CreateChangeSender.Factory createChangeSenderFactory;
 
   private final PatchSet.Id patchSetId;
 
   @Inject
-  PublishDraft(ChangeControl.Factory changeControlFactory,
-      ReviewDb db, @Assisted final PatchSet.Id patchSetId,
-      final ChangeHooks hooks) {
+  PublishDraft(final ChangeControl.Factory changeControlFactory,
+      final ReviewDb db, final ChangeHooks hooks,
+      final PatchSetInfoFactory patchSetInfoFactory,
+      final CreateChangeSender.Factory createChangeSenderFactory,
+      @Assisted final PatchSet.Id patchSetId) {
     this.changeControlFactory = changeControlFactory;
     this.db = db;
     this.hooks = hooks;
+    this.patchSetInfoFactory = patchSetInfoFactory;
+    this.createChangeSenderFactory = createChangeSenderFactory;
 
     this.patchSetId = patchSetId;
   }
 
   @Override
-  public ReviewResult call() throws NoSuchChangeException, OrmException {
+  public ReviewResult call() throws NoSuchChangeException, OrmException,
+      PatchSetInfoNotAvailableException {
     final ReviewResult result = new ReviewResult();
 
     final Change.Id changeId = patchSetId.getParentKey();
@@ -74,7 +91,7 @@ public class PublishDraft implements Callable<ReviewResult> {
       result.addError(new ReviewResult.Error(
           ReviewResult.Error.Type.PUBLISH_NOT_PERMITTED));
     } else {
-      final PatchSet updatedPatch = db.patchSets().atomicUpdate(patchSetId,
+      final PatchSet updatedPatchSet = db.patchSets().atomicUpdate(patchSetId,
           new AtomicUpdate<PatchSet>() {
         @Override
         public PatchSet update(PatchSet patchset) {
@@ -95,11 +112,30 @@ public class PublishDraft implements Callable<ReviewResult> {
         }
       });
 
-      if (!updatedPatch.isDraft() || updatedChange.getStatus() == Change.Status.NEW) {
-        hooks.doDraftPublishedHook(updatedChange, updatedPatch, db);
+      if (!updatedPatchSet.isDraft() || updatedChange.getStatus() == Change.Status.NEW) {
+        hooks.doDraftPublishedHook(updatedChange, updatedPatchSet, db);
+
+        if (control.getChange().getStatus() == Change.Status.DRAFT) {
+          notifyWatchers((IdentifiedUser) control.getCurrentUser(),
+              updatedChange, updatedPatchSet);
+        }
       }
     }
 
     return result;
+  }
+
+  private void notifyWatchers(final IdentifiedUser currentUser,
+      final Change updatedChange, final PatchSet updatedPatchSet)
+      throws PatchSetInfoNotAvailableException {
+    final PatchSetInfo info = patchSetInfoFactory.get(updatedChange, updatedPatchSet);
+    try {
+      CreateChangeSender cm = createChangeSenderFactory.create(updatedChange);
+      cm.setFrom(currentUser.getAccountId());
+      cm.setPatchSet(updatedPatchSet, info);
+      cm.send();
+    } catch (Exception e) {
+      log.error("Cannot send email for new change " + updatedChange.getId(), e);
+    }
   }
 }

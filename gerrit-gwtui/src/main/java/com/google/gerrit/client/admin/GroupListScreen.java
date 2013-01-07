b76a0bb510b02653aaafa69b5bf2eb7a05d42767
Edwin Kempin <edwin.kempin@sap.com>
2013-01-07 15:40:05 +0100

    Support filtering of groups in GroupListScreen
    Being able to filter the group list makes it easier and faster to find
    a certain group. Filter matches are highlighted by bold printing.
    
    Change-Id: I19992747dbd080cf0887d037c862d809c11256de
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/admin/GroupListScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/admin/GroupListScreen.java
@@ -14,32 +14,83 @@
 
 package com.google.gerrit.client.admin;
 
+import com.google.gerrit.client.Gerrit;
 import com.google.gerrit.client.groups.GroupMap;
 import com.google.gerrit.client.rpc.ScreenLoadCallback;
 import com.google.gerrit.client.ui.AccountScreen;
 import com.google.gerrit.common.PageLinks;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwtexpui.globalkey.client.NpTextBox;
 
 public class GroupListScreen extends AccountScreen {
   private GroupTable groups;
+  private NpTextBox filterTxt;
+  private String subname;
 
   @Override
   protected void onLoad() {
     super.onLoad();
-    GroupMap.all(new ScreenLoadCallback<GroupMap>(this) {
+    refresh();
+  }
+
+  private void refresh() {
+    final String mySubname = subname;
+    GroupMap.match(subname, new ScreenLoadCallback<GroupMap>(this) {
       @Override
       protected void preDisplay(final GroupMap result) {
-        groups.display(result);
-        groups.finishDisplay();
-      }});
+        if ((mySubname == null && subname == null)
+            || (mySubname != null && mySubname.equals(subname))) {
+          display(result);
+        }
+        // Else ignore the result, the user has already changed subname and
+        // the result is not relevant anymore. If multiple RPC's are fired
+        // the results may come back out-of-order and a non-relevant result
+        // could overwrite the correct result if not ignored.
+      }
+    });
+  }
+
+  private void display(final GroupMap result) {
+    groups.display(result, subname);
+    groups.finishDisplay();
   }
 
   @Override
   protected void onInitUI() {
     super.onInitUI();
     setPageTitle(Util.C.groupListTitle());
+    initPageHeader();
 
     groups = new GroupTable(true /* hyperlink to admin */, PageLinks.ADMIN_GROUPS);
     add(groups);
+  }
+
+  private void initPageHeader() {
+    final HorizontalPanel hp = new HorizontalPanel();
+    hp.setStyleName(Gerrit.RESOURCES.css().projectFilterPanel());
+    final Label filterLabel = new Label(Util.C.projectFilter());
+    filterLabel.setStyleName(Gerrit.RESOURCES.css().projectFilterLabel());
+    hp.add(filterLabel);
+    filterTxt = new NpTextBox();
+    filterTxt.setValue(subname);
+    filterTxt.addKeyUpHandler(new KeyUpHandler() {
+      @Override
+      public void onKeyUp(KeyUpEvent event) {
+        subname = filterTxt.getValue();
+        refresh();
+      }
+    });
+    hp.add(filterTxt);
+    add(hp);
+  }
+
+  @Override
+  public void onShowView() {
+    super.onShowView();
+    filterTxt.setFocus(true);
   }
 
   @Override

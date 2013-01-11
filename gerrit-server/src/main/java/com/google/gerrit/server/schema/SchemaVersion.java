23e03017f91faed29e19c7c96dd34a11aa1558ca
Shawn Pearce <sop@google.com>
2013-01-10 16:26:04 -0800

    Fix schema version to be 75
    Change-Id: I8862eb1d1c5182beed75d3e4ef7370a205bf18c7

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
@@ -32,7 +32,7 @@ import java.util.List;
 /** A version of the database schema. */
 public abstract class SchemaVersion {
   /** The current schema version. */
-  public static final Class<Schema_74> C = Schema_74.class;
+  public static final Class<Schema_75> C = Schema_75.class;
 
   public static class Module extends AbstractModule {
     @Override

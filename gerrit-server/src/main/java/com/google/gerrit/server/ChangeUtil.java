72d43af3702efda27718e47b1ceb450dbb206077
Bruce Zu <bruce.zu@sonymobile.com>
2013-01-16 22:54:39 +0800

    Fix: some new patchset is failed to check Change-Id.
    Currently new patchset with refs like
    
      'refs/changes/23456/new'
      'refs/changes/23456'
    
    can not be matched with CommitValidators.NEW_PATCHSET
    and the commit message validation part in
    ChangeIdValidator.onCommitReceived() is skipped.
    
    This patch fix it by:
    --using ReceiveCommits.NEW_PATCHSET
    --shifting the 'refName' format of new patchset created
    by 'revert' or 'edit commit message' to make these
    'refName' matched and keep their format align with that
    of pushed new patchset's 'refName'.
    
    Change-Id: Idbc7764db9edcc067445e7246c03eb1c3e686955

--- gerrit-server/src/main/java/com/google/gerrit/server/ChangeUtil.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/ChangeUtil.java
@@ -41,6 +41,7 @@ import com.google.gerrit.server.project.InvalidChangeOperationException;
 import com.google.gerrit.server.project.NoSuchChangeException;
 import com.google.gerrit.server.project.RefControl;
 import com.google.gerrit.server.util.IdGenerator;
+import com.google.gerrit.server.util.MagicBranch;
 import com.google.gwtorm.server.AtomicUpdate;
 import com.google.gwtorm.server.OrmConcurrencyException;
 import com.google.gwtorm.server.OrmException;
@@ -256,10 +257,16 @@ public class ChangeUtil {
       ps.setRevision(new RevId(revertCommit.name()));
 
       CommitReceivedEvent commitReceivedEvent =
-          new CommitReceivedEvent(new ReceiveCommand(ObjectId.zeroId(),
-              revertCommit.getId(), ps.getRefName()), refControl
-              .getProjectControl().getProject(), refControl.getRefName(),
-              revertCommit, user);
+          new CommitReceivedEvent(//
+              new ReceiveCommand(//
+                  ObjectId.zeroId(),//
+                  revertCommit.getId(), //
+                  MagicBranch.NEW_PUBLISH_CHANGE
+                      + refControl.getProjectControl().getProject().getName()),//
+              refControl.getProjectControl().getProject(),//
+              refControl.getRefName(),//
+              revertCommit,//
+              user);
 
       try {
         commitValidators.validateForRevertCommits(commitReceivedEvent);
@@ -375,12 +382,16 @@ public class ChangeUtil {
 
       final PatchSetInfo info =
           patchSetInfoFactory.get(newCommit, newPatchSet.getId());
-
-      CommitReceivedEvent commitReceivedEvent =
-          new CommitReceivedEvent(new ReceiveCommand(ObjectId.zeroId(),
-              newCommit.getId(), newPatchSet.getRefName()), refControl
-              .getProjectControl().getProject(), refControl.getRefName(),
-              newCommit, user);
+      final String refName = newPatchSet.getRefName();
+      CommitReceivedEvent commitReceivedEvent = new CommitReceivedEvent(//
+          new ReceiveCommand(//
+              ObjectId.zeroId(),//
+              newCommit.getId(), //
+              refName.substring(0, refName.lastIndexOf("/") + 1) + "new"), //
+          refControl.getProjectControl().getProject(), //
+          refControl.getRefName(),//
+          newCommit, //
+          user);
 
       try {
         commitValidators.validateForReceiveCommits(commitReceivedEvent);

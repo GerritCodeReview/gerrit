28b36956f6887dedfdafdc6eb09c3eefecb092d8
Shawn Pearce <sop@google.com>
2013-01-16 20:09:46 -0800

    Allow administrators to see other user capabilities
    Expand /accounts/{id}/capabilities to permit an administrator
    to inspect another user's effective capabilities by identifying
    the user with anything AccountResolver understands.
    
    This is a stepping stone to making more data available about
    other accounts.
    
    Change-Id: I81c786825f6531844b36f6178e5ed7324ccebd3a

--- gerrit-server/src/main/java/com/google/gerrit/server/account/Capabilities.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/Capabilities.java
@@ -16,22 +16,27 @@ package com.google.gerrit.server.account;
 
 import com.google.gerrit.common.data.GlobalCapability;
 import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.ChildCollection;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.account.AccountResource.Capability;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 class Capabilities implements
     ChildCollection<AccountResource, AccountResource.Capability> {
+  private final Provider<CurrentUser> self;
   private final DynamicMap<RestView<AccountResource.Capability>> views;
   private final Provider<GetCapabilities> get;
 
   @Inject
   Capabilities(
+      Provider<CurrentUser> self,
       DynamicMap<RestView<AccountResource.Capability>> views,
       Provider<GetCapabilities> get) {
+    this.self = self;
     this.views = views;
     this.get = get;
   }
@@ -43,7 +48,12 @@ class Capabilities implements
 
   @Override
   public Capability parse(AccountResource parent, String id)
-      throws ResourceNotFoundException, Exception {
+      throws ResourceNotFoundException, AuthException {
+    if (self.get() != parent.getUser()
+        && !self.get().getCapabilities().canAdministrateServer()) {
+      throw new AuthException("restricted to administrator");
+    }
+
     CapabilityControl cap = parent.getUser().getCapabilities();
     if (cap.canPerform(id)
         || (cap.canAdministrateServer() && GlobalCapability.isCapability(id))) {

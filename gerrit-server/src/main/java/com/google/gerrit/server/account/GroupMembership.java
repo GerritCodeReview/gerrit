55117d37db9543e089ccde5b51bc1658d0158d18
Shawn Pearce <sop@google.com>
2013-01-18 11:08:27 -0800

    Add intersection to GroupMembership
    Support slower group backends by allowing a lookup of a lot of
    groups at once to use intersection(). This method signature allows
    the backend to filter the input query set and return the subset of
    groups that the user is a member of.
    
    Using a batch style interface can decrease latency when resolving
    a large number of external groups identified through the internal
    group system's nested includes feature.
    
    Change-Id: I6926a3373589f439c8d694d36cdbe5fa8396a040

--- gerrit-server/src/main/java/com/google/gerrit/server/account/GroupMembership.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/GroupMembership.java
@@ -40,6 +40,20 @@ public interface GroupMembership {
   boolean containsAnyOf(Iterable<AccountGroup.UUID> groupIds);
 
   /**
+   * Returns a set containing an input member of {@code contains(id)} is true.
+   * <p>
+   * This is batch form of contains that returns specific group information.
+   * Implementors may implement the method as:
+   *
+   * <pre>
+   * Set&lt;AccountGroup.UUID&gt; r = Sets.newHashSet();
+   * for (AccountGroup.UUID id : groupIds)
+   *   if (contains(id)) r.add(id);
+   * </pre>
+   */
+  Set<AccountGroup.UUID> intersection(Iterable<AccountGroup.UUID> groupIds);
+
+  /**
    * Returns the set of groups that can be determined by the implementation.
    * This may not return all groups the {@link #contains(AccountGroup.UUID)}
    * would return {@code true} for, but will at least contain all top level

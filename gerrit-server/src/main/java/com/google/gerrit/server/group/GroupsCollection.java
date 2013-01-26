938ce41c915c049a7f339d2d2112a0034b92be55
Shawn Pearce <sop@google.com>
2013-01-26 10:33:54 -0800

    Accept group names in /group/{name}/... URLs
    If the id supplied in the REST API is not a UUID but does
    uniquely name a group known to the server, accept it. This
    matches behavior with /changes/ and /accounts/ where the
    server will also accept email addresses or other unique
    identifiers that are commonly used.
    
    Change-Id: I51ba5da5f5668659adb91fb275fdc3724d34208e

--- gerrit-server/src/main/java/com/google/gerrit/server/group/GroupsCollection.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/group/GroupsCollection.java
@@ -14,6 +14,8 @@
 
 package com.google.gerrit.server.group;
 
+import com.google.common.collect.Iterables;
+import com.google.gerrit.common.data.GroupReference;
 import com.google.gerrit.common.errors.NoSuchGroupException;
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.restapi.AuthException;
@@ -25,26 +27,32 @@ import com.google.gerrit.reviewdb.client.AccountGroup;
 import com.google.gerrit.server.AnonymousUser;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.GroupBackend;
 import com.google.gerrit.server.account.GroupControl;
 import com.google.gerrit.server.util.Url;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+
+import java.util.Collection;
 
 public class GroupsCollection implements
     RestCollection<TopLevelResource, GroupResource> {
   private final DynamicMap<RestView<GroupResource>> views;
   private final Provider<ListGroups> list;
   private final GroupControl.Factory groupControlFactory;
+  private final GroupBackend groupBackend;
   private final Provider<CurrentUser> self;
 
   @Inject
   GroupsCollection(final DynamicMap<RestView<GroupResource>> views,
       final Provider<ListGroups> list,
       final GroupControl.Factory groupControlFactory,
+      final GroupBackend groupBackend,
       final Provider<CurrentUser> self) {
     this.views = views;
     this.list = list;
     this.groupControlFactory = groupControlFactory;
+    this.groupBackend = groupBackend;
     this.self = self;
   }
 
@@ -70,33 +78,46 @@ public class GroupsCollection implements
     } else if(!(user instanceof IdentifiedUser)) {
       throw new ResourceNotFoundException(id);
     }
-    return parse(id, groupControlFactory);
+    return parse(id);
   }
 
-  public static GroupResource parse(String id, GroupControl.Factory factory)
-      throws ResourceNotFoundException {
-    String uuid = Url.decode(id);
-    GroupControl ctl;
+  GroupResource parse(String urlId) throws ResourceNotFoundException {
+    String id = Url.decode(urlId);
     try {
-      ctl = factory.controlFor(new AccountGroup.UUID(uuid));
+      AccountGroup.UUID uuid = new AccountGroup.UUID(id);
+      return check(urlId, groupControlFactory.controlFor(uuid));
     } catch (NoSuchGroupException noSuchGroup) {
-      if (uuid.matches("^[1-9][0-9]*$")) {
-        // Might be a legacy AccountGroup.Id.
-        try {
-          ctl = factory.controlFor(AccountGroup.Id.parse(uuid));
-        } catch (IllegalArgumentException invalidId) {
-          throw new ResourceNotFoundException(id);
-        } catch (NoSuchGroupException e) {
-          throw new ResourceNotFoundException(id);
-        }
-      } else {
-        throw new ResourceNotFoundException(id);
+    }
+
+    // Might be a legacy AccountGroup.Id.
+    if (id.matches("^[1-9][0-9]*$")) {
+      try {
+        AccountGroup.Id legacyId = AccountGroup.Id.parse(id);
+        return check(urlId, groupControlFactory.controlFor(legacyId));
+      } catch (IllegalArgumentException invalidId) {
+      } catch (NoSuchGroupException e) {
       }
     }
+
+    // Might be a group name. Try to be nice and accept unique names.
+    Collection<GroupReference> suggestions = groupBackend.suggest(id);
+    if (suggestions.size() == 1) {
+      try {
+        GroupReference ref = Iterables.getOnlyElement(suggestions);
+        return check(urlId, groupControlFactory.controlFor(ref.getUUID()));
+      } catch (NoSuchGroupException e) {
+      }
+    }
+
+    throw new ResourceNotFoundException(urlId);
+  }
+
+  private GroupResource check(String urlId, GroupControl ctl)
+      throws ResourceNotFoundException {
     if (ctl.isVisible()) {
       return new GroupResource(ctl);
     }
-    throw new ResourceNotFoundException(id);
+    throw new ResourceNotFoundException(urlId);
   }
 
   @Override

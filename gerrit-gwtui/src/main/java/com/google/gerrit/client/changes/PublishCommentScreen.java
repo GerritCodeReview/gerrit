57048321231d386fcee93eebe086f6f096db57a3
Dave Borowitz <dborowitz@google.com>
2013-01-30 11:48:39 -0800

    Add a class for grouping together related callbacks
    The goal is to split up some of the more complicated screens, like the
    publish comments screen, into multiple RPCs. This is obviously a bit
    more server load in the short term, but makes it much easier to convert
    complicated screens piecemeal.
    
    Because the final callback for the screen is a ScreenLoadCallback, which
    needs to "own" the screen and handle displaying on success for failure,
    we need to be able to specify the order in which onSuccess methods are
    called.
    
    Change-Id: Ie459fd1f91bbe10cd45a0f67a76a59e9f8890aa7

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/PublishCommentScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/PublishCommentScreen.java
@@ -18,6 +18,7 @@ import com.google.gerrit.client.Gerrit;
 import com.google.gerrit.client.patches.AbstractPatchContentTable;
 import com.google.gerrit.client.patches.CommentEditorContainer;
 import com.google.gerrit.client.patches.CommentEditorPanel;
+import com.google.gerrit.client.rpc.CallbackGroup;
 import com.google.gerrit.client.rpc.GerritCallback;
 import com.google.gerrit.client.rpc.RestApi;
 import com.google.gerrit.client.rpc.ScreenLoadCallback;
@@ -145,7 +146,8 @@ public class PublishCommentScreen extends AccountScreen implements
   @Override
   protected void onLoad() {
     super.onLoad();
-    Util.DETAIL_SVC.patchSetPublishDetail(patchSetId,
+
+    com.google.gwtjsonrpc.common.AsyncCallback<PatchSetPublishDetail> load =
         new ScreenLoadCallback<PatchSetPublishDetail>(this) {
           @Override
           protected void preDisplay(final PatchSetPublishDetail result) {
@@ -157,7 +159,10 @@ public class PublishCommentScreen extends AccountScreen implements
           protected void postDisplay() {
             message.setFocus(true);
           }
-        });
+        };
+
+    CallbackGroup cbs = new CallbackGroup();
+    Util.DETAIL_SVC.patchSetPublishDetail(patchSetId, cbs.add(load));
   }
 
   @Override

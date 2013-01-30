946fa7cbd559043c5d5188146575ac67bea9058f
Edwin Kempin <edwin.kempin@sap.com>
2013-01-30 09:53:52 +0100

    Support rename of groups via REST
    A group can now be renamed by PUT on '/groups/<group>/name'.
    
    Change-Id: I86d9bda2d8c74dfd6857b82e052a620f3770d2a6
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/group/PutName.java
@@ -0,0 +1,71 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.group;
+
+import com.google.common.base.Strings;
+import com.google.gerrit.common.errors.InvalidNameException;
+import com.google.gerrit.common.errors.NameAlreadyUsedException;
+import com.google.gerrit.common.errors.NoSuchGroupException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.DefaultInput;
+import com.google.gerrit.extensions.restapi.MethodNotAllowedException;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.account.PerformRenameGroup;
+import com.google.gerrit.server.auth.AuthException;
+import com.google.gerrit.server.group.PutName.Input;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+
+public class PutName implements RestModifyView<GroupResource, Input> {
+  static class Input {
+    @DefaultInput
+    String newName;
+  }
+
+  private final PerformRenameGroup.Factory performRenameGroupFactory;
+
+  @Inject
+  PutName(PerformRenameGroup.Factory performRenameGroupFactory) {
+    this.performRenameGroupFactory = performRenameGroupFactory;
+  }
+
+  @Override
+  public String apply(GroupResource resource, Input input)
+      throws MethodNotAllowedException, AuthException, BadRequestException,
+      ResourceNotFoundException, ResourceConflictException, OrmException {
+    if (resource.toAccountGroup() == null) {
+      throw new MethodNotAllowedException();
+    } else if (!resource.getControl().isOwner()) {
+      throw new AuthException("Not group owner");
+    }
+
+    if (input == null || Strings.emptyToNull(input.newName) == null) {
+      throw new BadRequestException("new name is missing");
+    }
+
+    try {
+      return performRenameGroupFactory.create().renameGroup(
+          resource.toAccountGroup().getId(), input.newName).group.getName();
+    } catch (NameAlreadyUsedException e) {
+      throw new ResourceConflictException(e.getMessage());
+    } catch (NoSuchGroupException e) {
+      throw new ResourceNotFoundException();
+    } catch (InvalidNameException e) {
+      throw new BadRequestException(e.getMessage());
+    }
+  }
+}

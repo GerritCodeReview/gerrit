af0f91df65a360222b87361b5d2cc59075c821d4
Dave Borowitz <dborowitz@google.com>
2013-01-30 11:48:39 -0800

    Add a class for grouping together related callbacks
    The goal is to split up some of the more complicated screens, like the
    publish comments screen, into multiple RPCs. This is obviously a bit
    more server load in the short term, but makes it much easier to convert
    complicated screens piecemeal.
    
    Because the final callback for the screen is a ScreenLoadCallback, which
    needs to "own" the screen and handle displaying on success for failure,
    we need to be able to specify the order in which onSuccess methods are
    called.
    
    Change-Id: Ie459fd1f91bbe10cd45a0f67a76a59e9f8890aa7

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/rpc/CallbackGroup.java
@@ -0,0 +1,136 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.rpc;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class for grouping together callbacks and calling them in order.
+ * <p>
+ * Callbacks are added to the group with {@link #add(AsyncCallback)}, which
+ * returns a wrapped callback suitable for passing to an asynchronous RPC call.
+ * The enclosing group buffers returned results and ensures that
+ * {@code onSuccess} is called exactly once for each callback in the group, in
+ * the same order that callbacks were added. This allows callers to, for
+ * example, use a {@link ScreenLoadCallback} as the last callback in the list
+ * and only display the screen once all callbacks have succeeded.
+ * <p>
+ * In the event of a failure, the <em>first</em> caught exception is sent to
+ * <em>all</em> callbacks' {@code onFailure} methods, in order; subsequent
+ * successes or failures are all ignored. Note that this means
+ * {@code onFailure} may be called with an exception unrelated to the callback
+ * processing it.
+ * <p>
+ * In the event of an unchecked exception during {@code onSuccess}, all
+ * {@code onSuccess} methods are still called, and the <em>first</em> caught
+ * exception will be rethrown from the <em>last</em> {@code onSuccess} method.
+ */
+public class CallbackGroup {
+  private final List<Object> callbacks;
+  private final Map<Object, Object> results;
+
+  public CallbackGroup() {
+    callbacks = new ArrayList<Object>();
+    results = new HashMap<Object, Object>();
+  }
+
+  public <T> AsyncCallback<T> add(final AsyncCallback<T> cb) {
+    callbacks.add(cb);
+    return new AsyncCallback<T>() {
+      @Override
+      public void onSuccess(T result) {
+        results.put(cb, result);
+        CallbackGroup.this.onSuccess();
+      }
+
+      @Override
+      public void onFailure(Throwable caught) {
+        CallbackGroup.this.onFailure(caught);
+      }
+    };
+  }
+
+  public <T> com.google.gwtjsonrpc.common.AsyncCallback<T> add(
+      final com.google.gwtjsonrpc.common.AsyncCallback<T> cb) {
+    callbacks.add(cb);
+    return new com.google.gwtjsonrpc.common.AsyncCallback<T>() {
+      @Override
+      public void onSuccess(T result) {
+        results.put(cb, result);
+        CallbackGroup.this.onSuccess();
+      }
+
+      @Override
+      public void onFailure(Throwable caught) {
+        CallbackGroup.this.onFailure(caught);
+      }
+    };
+  }
+
+  private void onSuccess() {
+    if (results.size() < callbacks.size()) {
+      return;
+    }
+    Throwable caught = null;
+    for (Object o : callbacks) {
+      try {
+        Object result = results.get(o);
+        if (o instanceof AsyncCallback) {
+          @SuppressWarnings("unchecked")
+          AsyncCallback<Object> cb = (AsyncCallback<Object>) o;
+          cb.onSuccess(result);
+        } else {
+          @SuppressWarnings("unchecked")
+          com.google.gwtjsonrpc.common.AsyncCallback<Object> cb =
+              (com.google.gwtjsonrpc.common.AsyncCallback<Object>) o;
+          cb.onSuccess(result);
+        }
+      } catch (Throwable t) {
+        if (caught != null) {
+          caught = t;
+        }
+      }
+    }
+    if (caught != null) {
+      if (caught instanceof RuntimeException) {
+        throw (RuntimeException) caught;
+      } else if (caught instanceof Error) {
+        throw (Error) caught;
+      } else {
+        throw new RuntimeException(caught);
+      }
+    }
+  }
+
+  private void onFailure(Throwable caught) {
+    for (Object o : callbacks) {
+      if (o instanceof AsyncCallback) {
+        @SuppressWarnings("unchecked")
+        AsyncCallback<Object> cb = (AsyncCallback<Object>) o;
+        cb.onFailure(caught);
+      } else {
+        @SuppressWarnings("unchecked")
+        com.google.gwtjsonrpc.common.AsyncCallback<Object> cb =
+            (com.google.gwtjsonrpc.common.AsyncCallback<Object>) o;
+        cb.onFailure(caught);
+      }
+    }
+  }
+}

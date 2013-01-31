b1e265de81b5ea1a0426f127ffdf8b2ead23c55c
Dave Borowitz <dborowitz@google.com>
2013-01-30 16:18:59 -0800

    Add a /changes option to include detailed label info
    Change-Id: I97e97fa0cb27cc3dbdc8496d5115dc353dafc9ae

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
@@ -20,19 +20,25 @@ import static com.google.gerrit.common.changes.ListChangesOption.ALL_REVISIONS;
 import static com.google.gerrit.common.changes.ListChangesOption.CURRENT_COMMIT;
 import static com.google.gerrit.common.changes.ListChangesOption.CURRENT_FILES;
 import static com.google.gerrit.common.changes.ListChangesOption.CURRENT_REVISION;
+import static com.google.gerrit.common.changes.ListChangesOption.DETAILED_LABELS;
 import static com.google.gerrit.common.changes.ListChangesOption.LABELS;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.gerrit.common.changes.ListChangesOption;
 import com.google.gerrit.common.data.ApprovalType;
 import com.google.gerrit.common.data.ApprovalTypes;
+import com.google.gerrit.common.data.Permission;
+import com.google.gerrit.common.data.PermissionRange;
 import com.google.gerrit.common.data.SubmitRecord;
 import com.google.gerrit.extensions.restapi.Url;
 import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.ApprovalCategoryValue;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.ChangeMessage;
 import com.google.gerrit.reviewdb.client.Patch;
@@ -220,7 +226,9 @@ public class ChangeJson {
     out._sortkey = in.getSortKey();
     out.starred = user.getStarredChanges().contains(in.getId()) ? true : null;
     out.reviewed = in.getStatus().isOpen() && isChangeReviewed(cd) ? true : null;
-    out.labels = options.contains(LABELS) ? labelsFor(cd) : null;
+    if (options.contains(LABELS) || options.contains(DETAILED_LABELS)) {
+      out.labels = labelsFor(cd, options.contains(DETAILED_LABELS));
+    }
     out.finish();
 
     if (options.contains(ALL_REVISIONS) || options.contains(CURRENT_REVISION)) {
@@ -272,7 +280,8 @@ public class ChangeJson {
     return ctrl;
   }
 
-  private Map<String, LabelInfo> labelsFor(ChangeData cd) throws OrmException {
+  private Map<String, LabelInfo> labelsFor(ChangeData cd, boolean detailed)
+      throws OrmException {
     ChangeControl ctl = control(cd);
     if (ctl == null) {
       return Collections.emptyMap();
@@ -303,7 +312,25 @@ public class ChangeJson {
             default:
               break;
           }
+
           n.optional = n._status == SubmitRecord.Label.Status.MAY ? true : null;
+          if (detailed) {
+            PermissionRange range = ctl.getRange(Permission.forLabel(r.label));
+            ApprovalType at = approvalTypes.byLabel(r.label);
+            n.category = at.getCategory().getName();
+            n.id = at.getCategory().getId().get();
+
+            n.values = Maps.newLinkedHashMap();
+            for (ApprovalCategoryValue acv : at.getValues()) {
+              if (range.contains(acv.getValue())) {
+                n.values.put(acv.formatValue(), acv.getName());
+              }
+            }
+            if (n.values.size() == 1 &&
+                " 0".equals(Iterables.getOnlyElement(n.values.keySet()))) {
+              n.values = ImmutableMap.of();
+            }
+          }
           labels.put(r.label, n);
         }
       }
@@ -601,6 +628,11 @@ public class ChangeJson {
 
   static class LabelInfo {
     transient SubmitRecord.Label.Status _status;
+
+    String category;
+    String id;
+
+    Map<String, String> values;
     AccountAttribute approved;
     AccountAttribute rejected;
 

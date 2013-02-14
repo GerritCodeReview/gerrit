a6e39d202ac59cfe21e31568ab003093c611d5ff
Dave Borowitz <dborowitz@google.com>
2013-02-14 12:58:28 -0800

    Sort approvals in REST endpoints and approval table
    Document REST endpoints to return approvals within a given list by
    timestamp, although the timestamp is not exposed.
    
    On the client side, sort the owner first, followed by users with
    nonzero votes.
    
    Change-Id: I68cd21e4a62b76bd7de2bf1dd1e18e8675dd34db

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ReviewerJson.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ReviewerJson.java
@@ -28,7 +28,6 @@ import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.reviewdb.client.PatchSetApproval;
 import com.google.gerrit.reviewdb.server.ReviewDb;
-import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.project.ChangeControl;
 import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.workflow.CategoryFunction;
@@ -43,19 +42,16 @@ import java.util.Map;
 
 public class ReviewerJson {
   private final Provider<ReviewDb> db;
-  private final IdentifiedUser.GenericFactory userFactory;
   private final ApprovalTypes approvalTypes;
   private final FunctionState.Factory functionState;
   private final AccountInfo.Loader.Factory accountLoaderFactory;
 
   @Inject
   ReviewerJson(Provider<ReviewDb> db,
-      IdentifiedUser.GenericFactory userFactory,
       ApprovalTypes approvalTypes,
       FunctionState.Factory functionState,
       AccountInfo.Loader.Factory accountLoaderFactory) {
     this.db = db;
-    this.userFactory = userFactory;
     this.approvalTypes = approvalTypes;
     this.functionState = functionState;
     this.accountLoaderFactory = accountLoaderFactory;
@@ -83,8 +79,8 @@ public class ReviewerJson {
     PatchSet.Id psId = ctl.getChange().currentPatchSetId();
 
     if (approvals == null) {
-      approvals = db.get().patchSetApprovals()
-          .byPatchSetUser(psId, out._id).toList();
+      approvals = ChangeData.sortApprovals(db.get().patchSetApprovals()
+          .byPatchSetUser(psId, out._id));
     }
 
     FunctionState fs = functionState.create(ctl, psId, approvals);

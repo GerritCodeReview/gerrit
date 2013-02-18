9e5d32c79e7a6abd9a4cac44b0a7ee4c520374f5
Dave Borowitz <dborowitz@google.com>
2013-02-18 15:15:53 -0800

    Drop LabelType.getPosition()
    Instead sort labels implicitly based on the order passed into
    LabelTypes.
    
    Change-Id: Ib57664acc543539a7c5c1b9279970f4ba7d04bb7

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
@@ -317,7 +317,7 @@ public class ChangeJson {
 
     LabelTypes labelTypes = ctl.getLabelTypes();
     Map<String, LabelInfo> labels =
-        Maps.newTreeMap(LabelOrdering.create(labelTypes));
+        Maps.newTreeMap(labelTypes.nameComparator());
     initLabels(cd, labels, standard);
 
     Collection<PatchSetApproval> approvals = null;

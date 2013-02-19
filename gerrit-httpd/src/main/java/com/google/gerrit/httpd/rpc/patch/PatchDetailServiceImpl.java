a4e79cc040e5fe7c1e2c81ce4584434aaa95d107
Dave Borowitz <dborowitz@google.com>
2013-02-19 09:13:39 -0800

    Remove unused methods from PatchDetailService
    Change-Id: I2e24f5ff0a4a359468b1d23e92a48b44e6ead47e

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/patch/PatchDetailServiceImpl.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/rpc/patch/PatchDetailServiceImpl.java
@@ -14,8 +14,6 @@
 
 package com.google.gerrit.httpd.rpc.patch;
 
-import com.google.gerrit.common.data.ApprovalSummary;
-import com.google.gerrit.common.data.ApprovalSummarySet;
 import com.google.gerrit.common.data.ChangeDetail;
 import com.google.gerrit.common.data.PatchDetailService;
 import com.google.gerrit.common.data.PatchScript;
@@ -23,23 +21,17 @@ import com.google.gerrit.common.data.ReviewResult;
 import com.google.gerrit.common.errors.NoSuchEntityException;
 import com.google.gerrit.httpd.rpc.BaseServiceImplementation;
 import com.google.gerrit.httpd.rpc.changedetail.ChangeDetailFactory;
-import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.AccountDiffPreference;
-import com.google.gerrit.reviewdb.client.ApprovalCategory;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.Patch;
 import com.google.gerrit.reviewdb.client.PatchLineComment;
 import com.google.gerrit.reviewdb.client.PatchSet;
-import com.google.gerrit.reviewdb.client.PatchSetApproval;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.CurrentUser;
-import com.google.gerrit.server.account.AccountInfoCacheFactory;
 import com.google.gerrit.server.changedetail.DeleteDraftPatchSet;
 import com.google.gerrit.server.patch.PatchSetInfoNotAvailableException;
-import com.google.gerrit.server.project.ChangeControl;
 import com.google.gerrit.server.project.NoSuchChangeException;
 import com.google.gerrit.server.project.NoSuchProjectException;
-import com.google.gerrit.server.workflow.FunctionState;
 import com.google.gwtjsonrpc.common.AsyncCallback;
 import com.google.gwtjsonrpc.common.VoidResult;
 import com.google.gwtorm.server.OrmException;
@@ -50,16 +42,10 @@ import org.eclipse.jgit.errors.RepositoryNotFoundException;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 class PatchDetailServiceImpl extends BaseServiceImplementation implements
     PatchDetailService {
-  private final AccountInfoCacheFactory.Factory accountInfoCacheFactory;
-  private final ChangeControl.Factory changeControlFactory;
   private final DeleteDraftPatchSet.Factory deleteDraftPatchSetFactory;
-  private final FunctionState.Factory functionStateFactory;
   private final PatchScriptFactory.Factory patchScriptFactoryFactory;
   private final SaveDraft.Factory saveDraftFactory;
   private final ChangeDetailFactory.Factory changeDetailFactory;
@@ -67,19 +53,13 @@ class PatchDetailServiceImpl extends BaseServiceImplementation implements
   @Inject
   PatchDetailServiceImpl(final Provider<ReviewDb> schema,
       final Provider<CurrentUser> currentUser,
-      final AccountInfoCacheFactory.Factory accountInfoCacheFactory,
-      final ChangeControl.Factory changeControlFactory,
       final DeleteDraftPatchSet.Factory deleteDraftPatchSetFactory,
-      final FunctionState.Factory functionStateFactory,
       final PatchScriptFactory.Factory patchScriptFactoryFactory,
       final SaveDraft.Factory saveDraftFactory,
       final ChangeDetailFactory.Factory changeDetailFactory) {
     super(schema, currentUser);
 
-    this.accountInfoCacheFactory = accountInfoCacheFactory;
-    this.changeControlFactory = changeControlFactory;
     this.deleteDraftPatchSetFactory = deleteDraftPatchSetFactory;
-    this.functionStateFactory = functionStateFactory;
     this.patchScriptFactoryFactory = patchScriptFactoryFactory;
     this.saveDraftFactory = saveDraftFactory;
     this.changeDetailFactory = changeDetailFactory;
@@ -156,112 +136,6 @@ class PatchDetailServiceImpl extends BaseServiceImplementation implements
         } catch (IOException e) {
           throw new Failure(e);
         }
-      }
-    });
-  }
-
-  public void userApprovals(final Set<Change.Id> cids, final Account.Id aid,
-      final AsyncCallback<ApprovalSummarySet> callback) {
-    run(callback, new Action<ApprovalSummarySet>() {
-      public ApprovalSummarySet run(ReviewDb db) throws OrmException {
-        final Map<Change.Id, ApprovalSummary> approvals =
-            new HashMap<Change.Id, ApprovalSummary>();
-        final AccountInfoCacheFactory aicFactory =
-            accountInfoCacheFactory.create();
-
-        aicFactory.want(aid);
-        for (final Change.Id id : cids) {
-          try {
-            final ChangeControl cc = changeControlFactory.validateFor(id);
-            final Change change = cc.getChange();
-            final PatchSet.Id ps_id = change.currentPatchSetId();
-            final Map<ApprovalCategory.Id, PatchSetApproval> psas =
-                new HashMap<ApprovalCategory.Id, PatchSetApproval>();
-            final FunctionState fs =
-                functionStateFactory.create(cc, ps_id, psas.values());
-
-            for (final PatchSetApproval ca : db.patchSetApprovals()
-                .byPatchSetUser(ps_id, aid)) {
-              final ApprovalCategory.Id category = ca.getCategoryId();
-              if (ApprovalCategory.SUBMIT.equals(category)) {
-                continue;
-              }
-              if (change.getStatus().isOpen()) {
-                fs.normalize(cc.getLabelTypes().byId(category.get()), ca);
-              }
-              if (ca.getValue() == 0) {
-                continue;
-              }
-              psas.put(category, ca);
-            }
-
-            approvals.put(id, new ApprovalSummary(psas.values()));
-          } catch (NoSuchChangeException nsce) {
-            /*
-             * The user has no access to see this change, so we simply do not
-             * provide any details about it.
-             */
-          }
-        }
-        return new ApprovalSummarySet(aicFactory.create(), approvals);
-      }
-    });
-  }
-
-  public void strongestApprovals(final Set<Change.Id> cids,
-      final AsyncCallback<ApprovalSummarySet> callback) {
-    run(callback, new Action<ApprovalSummarySet>() {
-      public ApprovalSummarySet run(ReviewDb db) throws OrmException {
-        final Map<Change.Id, ApprovalSummary> approvals =
-            new HashMap<Change.Id, ApprovalSummary>();
-        final AccountInfoCacheFactory aicFactory =
-            accountInfoCacheFactory.create();
-
-        for (final Change.Id id : cids) {
-          try {
-            final ChangeControl cc = changeControlFactory.validateFor(id);
-            final Change change = cc.getChange();
-            final PatchSet.Id ps_id = change.currentPatchSetId();
-            final Map<ApprovalCategory.Id, PatchSetApproval> psas =
-                new HashMap<ApprovalCategory.Id, PatchSetApproval>();
-            final FunctionState fs =
-                functionStateFactory.create(cc, ps_id, psas.values());
-
-            for (PatchSetApproval ca : db.patchSetApprovals().byPatchSet(ps_id)) {
-              final ApprovalCategory.Id category = ca.getCategoryId();
-              if (ApprovalCategory.SUBMIT.equals(category)) {
-                continue;
-              }
-              if (change.getStatus().isOpen()) {
-                fs.normalize(cc.getLabelTypes().byId(category.get()), ca);
-              }
-              if (ca.getValue() == 0) {
-                continue;
-              }
-              boolean keep = true;
-              if (psas.containsKey(category)) {
-                final short oldValue = psas.get(category).getValue();
-                final short newValue = ca.getValue();
-                keep =
-                    (Math.abs(oldValue) < Math.abs(newValue))
-                        || ((Math.abs(oldValue) == Math.abs(newValue) && (newValue < oldValue)));
-              }
-              if (keep) {
-                aicFactory.want(ca.getAccountId());
-                psas.put(category, ca);
-              }
-            }
-
-            approvals.put(id, new ApprovalSummary(psas.values()));
-          } catch (NoSuchChangeException nsce) {
-            /*
-             * The user has no access to see this change, so we simply do not
-             * provide any details about it.
-             */
-          }
-        }
-
-        return new ApprovalSummarySet(aicFactory.create(), approvals);
       }
     });
   }

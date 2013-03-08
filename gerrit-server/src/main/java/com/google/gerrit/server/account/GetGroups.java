96e039be0aa81878b3bbc4065c599b0783ac17e3
Edwin Kempin <edwin.kempin@sap.com>
2013-03-08 16:37:14 +0100

    Ignore non-visible included groups when listing members recursively
    When the members of a group are listed via REST with the 'recursive'
    option ignore those included groups which are not visible to the
    calling user. This is better than failing completely as we do
    currently and is consistent with how included external groups are
    handled (they are also ignored).
    
    Change-Id: I02b2213573b2647582bddcdcab374c672308bc89
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/account/GetGroups.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/GetGroups.java
@@ -16,7 +16,6 @@ package com.google.gerrit.server.account;
 
 import com.google.common.collect.Lists;
 import com.google.gerrit.common.errors.NoSuchGroupException;
-import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.AccountGroup;
@@ -39,8 +38,7 @@ public class GetGroups implements RestReadView<AccountResource> {
   }
 
   @Override
-  public List<GroupInfo> apply(AccountResource resource)
-      throws ResourceNotFoundException, OrmException {
+  public List<GroupInfo> apply(AccountResource resource) throws OrmException {
     IdentifiedUser user = resource.getUser();
     Account.Id userId = user.getAccountId();
     List<GroupInfo> groups = Lists.newArrayList();

4c74aeb9ac194acf3db4ff88880fb7d4a1a027cf
Gustaf Lundh <gustaf.lundh@sonymobile.com>
2013-03-11 14:36:33 +0100

    Remove ReceiveCommits.insertAncestors()
    Reuse the already existing code in ChangeUtil.
    
    Change-Id: I427552485f7b8bf7a2012d5e0fc1a5dd5008936c

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -1460,7 +1460,7 @@ public class ReceiveCommits {
 
       db.changes().beginTransaction(change.getId());
       try {
-        insertAncestors(db, ps.getId(), commit);
+        ChangeUtil.insertAncestors(db, ps.getId(), commit);
         db.patchSets().insert(Collections.singleton(ps));
         db.changes().insert(Collections.singleton(change));
         ChangeUtil.updateTrackingIds(db, change, trackingFooters, footerLines);
@@ -1761,7 +1761,7 @@ public class ReceiveCommits {
           return null;
         }
 
-        insertAncestors(db, newPatchSet.getId(), newCommit);
+        ChangeUtil.insertAncestors(db, newPatchSet.getId(), newCommit);
         db.patchSets().insert(Collections.singleton(newPatchSet));
 
         if (checkMergedInto) {
@@ -2177,20 +2177,6 @@ public class ReceiveCommits {
         return "send-email merged";
       }
     }));
-  }
-
-  private void insertAncestors(ReviewDb db, PatchSet.Id id, RevCommit src)
-      throws OrmException {
-    final int cnt = src.getParentCount();
-    List<PatchSetAncestor> toInsert = new ArrayList<PatchSetAncestor>(cnt);
-    for (int p = 0; p < cnt; p++) {
-      PatchSetAncestor a;
-
-      a = new PatchSetAncestor(new PatchSetAncestor.Id(id, p + 1));
-      a.setAncestorRevision(toRevId(src.getParent(p)));
-      toInsert.add(a);
-    }
-    db.patchSetAncestors().insert(toInsert);
   }
 
   private static RevId toRevId(final RevCommit src) {

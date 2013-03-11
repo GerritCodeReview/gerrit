ed55f93e37947b4a5ba86d4b6f923b7cae4a2cd3
Clint Morgan <clint.a.m@gmail.com>
2013-03-11 15:50:25 -0700

    Git over http does not honor auth.httpHeader
    If auth type is HTTP, then pull the username from the appropriate header
    
    Bug: Issue 1473
    Task-URL: https://code.google.com/p/gerrit/issues/detail?id=1473
    Change-Id: Id61bfbf73d08fd89fd3787c6051d4d096bae0cfa

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/ContainerAuthFilter.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/ContainerAuthFilter.java
@@ -16,10 +16,17 @@ package com.google.gerrit.httpd;
 
 import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountExternalId;
+import com.google.gerrit.reviewdb.client.AuthType;
 import com.google.gerrit.server.AccessPath;
 import com.google.gerrit.server.account.AccountCache;
+import com.google.gerrit.server.account.AccountException;
+import com.google.gerrit.server.account.AccountManager;
 import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.config.AuthConfig;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -57,13 +64,24 @@ class ContainerAuthFilter implements Filter {
   private final Provider<WebSession> session;
   private final AccountCache accountCache;
   private final Config config;
+  private final AccountManager accountManager;
+  private final String loginHttpHeader;
 
   @Inject
   ContainerAuthFilter(Provider<WebSession> session, AccountCache accountCache,
-      @GerritServerConfig Config config) {
+      @GerritServerConfig Config config, AuthConfig authConfig, AccountManager accountManager) {
     this.session = session;
     this.accountCache = accountCache;
     this.config = config;
+    this.accountManager = accountManager;
+
+    if (authConfig.getAuthType().equals(AuthType.HTTP)
+        && authConfig.getLoginHttpHeader() != null) {
+      loginHttpHeader = authConfig.getLoginHttpHeader();
+    } else {
+      loginHttpHeader = null;
+    }
+
   }
 
   @Override
@@ -87,7 +105,8 @@ class ContainerAuthFilter implements Filter {
 
   private boolean verify(HttpServletRequest req, HttpServletResponse rsp)
       throws IOException {
-    String username = req.getRemoteUser();
+    String username = extractUsername(req, rsp);
+
     if (username == null) {
       rsp.sendError(SC_FORBIDDEN);
       return false;
@@ -105,5 +124,24 @@ class ContainerAuthFilter implements Filter {
     ws.setAccessPathOk(AccessPath.GIT, true);
     ws.setAccessPathOk(AccessPath.REST_API, true);
     return true;
+  }
+
+  private String extractUsername(HttpServletRequest req, HttpServletResponse rsp)
+      throws IOException {
+    if (loginHttpHeader != null) {
+      String externalUsername = req.getHeader(loginHttpHeader);
+      try {
+        Account.Id accountId =
+            accountManager.lookup(new AccountExternalId.Key(
+                AccountExternalId.SCHEME_GERRIT, externalUsername).get());
+        AccountState account = accountCache.get(accountId);
+        return account.getUserName();
+      } catch (AccountException e) {
+        rsp.sendError(SC_INTERNAL_SERVER_ERROR);
+        throw new RuntimeException(e);
+      }
+    } else {
+      return req.getRemoteUser();
+    }
   }
 }

ee030f96a72b465e115a6532f4761f26b2705d56
Edwin Kempin <edwin.kempin@sap.com>
2013-03-12 16:04:37 +0100

    Use SECONDS as default time unit for cache.web_sessions.maxAge
    DefaultCacheFactory already uses SECONDS as default time unit for
    cache.*.maxAge.
    
    Update the described default time unit for cache.*.maxAge in the
    documentation.
    
    Change-Id: Ibc1e4e8e8085292c313dd301de022687f0a453a4
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/WebSessionManager.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/WebSessionManager.java
@@ -24,6 +24,7 @@ import static com.google.gerrit.server.ioutil.BasicSerialization.writeString;
 import static com.google.gerrit.server.ioutil.BasicSerialization.writeVarInt32;
 import static java.util.concurrent.TimeUnit.HOURS;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import com.google.common.cache.Cache;
 import com.google.gerrit.reviewdb.client.Account;
@@ -65,9 +66,9 @@ class WebSessionManager {
     prng = new SecureRandom();
     self = cache;
 
-    sessionMaxAgeMillis = ConfigUtil.getTimeUnit(cfg,
+    sessionMaxAgeMillis = SECONDS.toMillis(ConfigUtil.getTimeUnit(cfg,
         "cache", CACHE_NAME, "maxAge",
-        MAX_AGE_MINUTES, MILLISECONDS);
+        MAX_AGE_MINUTES, SECONDS));
     if (sessionMaxAgeMillis < TimeUnit.MINUTES.toMillis(5)) {
       log.warn(String.format(
           "cache.%s.maxAge is set to %d milliseconds;" +

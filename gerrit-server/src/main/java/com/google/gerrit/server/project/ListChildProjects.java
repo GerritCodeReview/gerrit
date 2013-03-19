bc1edbba9303030fd46ea7948a8821eaeddd85c8
Edwin Kempin <edwin.kempin@sap.com>
2013-03-19 11:10:57 +0100

    Support to list child project recursively via REST
    Change-Id: I046e4d546ca1f0a971382816de4ea76aef035f3f
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/project/ListChildProjects.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/ListChildProjects.java
@@ -15,30 +15,49 @@
 package com.google.gerrit.server.project;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.config.AllProjectsName;
 import com.google.gerrit.server.project.ProjectJson.ProjectInfo;
 import com.google.inject.Inject;
 
+import org.kohsuke.args4j.Option;
+
 import java.util.List;
+import java.util.Map;
 
 public class ListChildProjects implements RestReadView<ProjectResource> {
+
+  @Option(name = "--recursive", usage = "to list child projects recursively")
+  private boolean recursive;
 
   private final ProjectCache projectCache;
   private final AllProjectsName allProjects;
   private final ProjectJson json;
+  private final ProjectNode.Factory projectNodeFactory;
 
   @Inject
   ListChildProjects(ProjectCache projectCache, AllProjectsName allProjects,
-      ProjectJson json) {
+      ProjectJson json, ProjectNode.Factory projectNodeFactory) {
     this.projectCache = projectCache;
     this.allProjects = allProjects;
     this.json = json;
+    this.projectNodeFactory = projectNodeFactory;
   }
 
   @Override
   public List<ProjectInfo> apply(ProjectResource rsrc) {
+    if (recursive) {
+      return getChildProjectsRecursively(rsrc.getNameKey(),
+          rsrc.getControl().getCurrentUser());
+    } else {
+      return getDirectChildProjects(rsrc.getNameKey());
+    }
+  }
+
+  private List<ProjectInfo> getDirectChildProjects(Project.NameKey parent) {
     List<ProjectInfo> childProjects = Lists.newArrayList();
     for (Project.NameKey projectName : projectCache.all()) {
       ProjectState e = projectCache.get(projectName);
@@ -46,10 +65,42 @@ public class ListChildProjects implements RestReadView<ProjectResource> {
         // If we can't get it from the cache, pretend it's not present.
         continue;
       }
-      if (rsrc.getNameKey().equals(e.getProject().getParent(allProjects))) {
+      if (parent.equals(e.getProject().getParent(allProjects))) {
         childProjects.add(json.format(e.getProject()));
       }
     }
     return childProjects;
+  }
+
+  private List<ProjectInfo> getChildProjectsRecursively(Project.NameKey parent,
+      CurrentUser user) {
+    Map<Project.NameKey, ProjectNode> projects = Maps.newHashMap();
+    for (Project.NameKey name : projectCache.all()) {
+      ProjectState p = projectCache.get(name);
+      if (p == null) {
+        // If we can't get it from the cache, pretend it's not present.
+        continue;
+      }
+      projects.put(name, projectNodeFactory.create(p.getProject(),
+          p.controlFor(user).isVisible()));
+    }
+    for (ProjectNode key : projects.values()) {
+      ProjectNode node = projects.get(key.getParentName());
+      if (node != null) {
+        node.addChild(key);
+      }
+    }
+    return getChildProjectsRecursively(projects.get(parent));
+  }
+
+  private List<ProjectInfo> getChildProjectsRecursively(ProjectNode p) {
+    List<ProjectInfo> allChildren = Lists.newArrayList();
+    for (ProjectNode c : p.getChildren()) {
+      if (c.isVisible()) {
+        allChildren.add(json.format(c.getProject()));
+        allChildren.addAll(getChildProjectsRecursively(c));
+      }
+    }
+    return allChildren;
   }
 }

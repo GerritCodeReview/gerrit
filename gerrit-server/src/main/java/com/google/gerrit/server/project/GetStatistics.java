dfb0cf843dd1b44130366846f4ee6b814720c2d1
Edwin Kempin <edwin.kempin@sap.com>
2013-03-20 13:20:26 +0100

    Add support to retrieve repository statistics for a project via REST
    Statistics for a repository can now be retrieved by GET on
    '/projects/*/statistics.git'.
    
    To get the statistics for a repository the global capability
    'Run Garbage Collection' is required. Normal users should not be
    interested in the repository statistics which is why we keep the access
    restricted for now.
    
    Change-Id: If5a2def2f840e1b30206a19a923d02e813db4049
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/GetStatistics.java
@@ -0,0 +1,63 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.project;
+
+import com.google.gerrit.common.data.GlobalCapability;
+import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.inject.Inject;
+
+import org.eclipse.jgit.api.GarbageCollectCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.lib.Repository;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@RequiresCapability(GlobalCapability.RUN_GC)
+public class GetStatistics implements RestReadView<ProjectResource> {
+
+  private final GitRepositoryManager repoManager;
+
+  @Inject
+  GetStatistics(GitRepositoryManager repoManager) {
+    this.repoManager = repoManager;
+  }
+
+  @Override
+  public RepositoryStatistics apply(ProjectResource rsrc)
+      throws ResourceNotFoundException, ResourceConflictException {
+    try {
+      Repository repo = repoManager.openRepository(rsrc.getNameKey());
+      try {
+        GarbageCollectCommand gc = Git.wrap(repo).gc();
+        return new RepositoryStatistics(gc.getStatistics());
+      } catch (GitAPIException e) {
+        throw new ResourceConflictException(e.getMessage());
+      } catch (JGitInternalException e) {
+        throw new ResourceConflictException(e.getMessage());
+      } finally {
+        repo.close();
+      }
+    } catch (IOException e) {
+      throw new ResourceNotFoundException(rsrc.getName());
+    }
+  }
+}

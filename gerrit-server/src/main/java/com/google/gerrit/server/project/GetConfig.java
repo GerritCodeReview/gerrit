a82466db9ce673221ea100164a02e4b5f6156e48
Dave Borowitz <dborowitz@google.com>
2013-04-04 16:52:27 -0700

    Add a REST API endpoint for getting project configuration
    To start, only expose the inheritable booleans stored in the project
    config, and only to users who can see refs/meta/config.
    
    Change-Id: I299a5317fb641e84b899f8240764c4902a34c72a

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/GetConfig.java
@@ -0,0 +1,71 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.project;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.OptionUtil;
+import com.google.gerrit.server.git.GitRepositoryManager;
+
+import org.kohsuke.args4j.Option;
+
+import java.util.Set;
+
+class GetConfig implements RestReadView<ProjectResource> {
+  @Option(name = "-q", metaVar = "OPT", multiValued = true,
+      usage = "Config option to inspect")
+  void addQuery(String name) {
+    if (query == null) {
+      query = Sets.newHashSet();
+    }
+    Iterables.addAll(query, OptionUtil.splitOptionValue(name));
+  }
+  private Set<String> query;
+
+  static class ConfigInfo {
+    Boolean useContributorAgreements;
+    Boolean useContentMerge;
+    Boolean useSignedOffBy;
+    Boolean requireChangeID;
+  }
+
+  @Override
+  public Object apply(ProjectResource resource) {
+    ConfigInfo result = new ConfigInfo();
+    RefControl refConfig = resource.getControl()
+        .controlForRef(GitRepositoryManager.REF_CONFIG);
+    if (refConfig.isVisible()) {
+      ProjectState project = resource.getControl().getProjectState();
+      if (want("usecontributoragreements")) {
+        result.useContributorAgreements = project.isUseContributorAgreements();
+      }
+      if (want("usecontentmerge")) {
+        result.useContentMerge = project.isUseContentMerge();
+      }
+      if (want("usesignedoffby")) {
+        result.useSignedOffBy = project.isUseSignedOffBy();
+      }
+      if (want("requirechangeid")) {
+        result.requireChangeID = project.isRequireChangeID();
+      }
+    }
+    return result;
+  }
+
+  private boolean want(String name) {
+    return query == null || query.contains(name);
+  }
+}

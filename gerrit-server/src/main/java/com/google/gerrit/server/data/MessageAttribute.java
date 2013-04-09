c4d0a135fed19779d14f8921e6dd3630f663bfce
David Pursehouse <david.pursehouse@sonymobile.com>
2013-04-09 12:22:31 +0900

    Move event attribute classes to a separate package
    The Attribute classes are moved from:
    
      com.google.gerrit.server.events
    
    to the newly created package:
    
      com.google.gerrit.server.data
    
    The `QueryStats` class is renamed to `QueryStatsAttribute` to be
    consistent with the other classes in the package.
    
    This is only a code reorganisation.  No functionality is changed.
    
    Change-Id: I5c125e8d45d8f8e6943021cd90ddba1c364a103e

--- gerrit-server/src/main/java/com/google/gerrit/server/data/MessageAttribute.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/data/MessageAttribute.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2010 The Android Open Source Project
+// Copyright (C) 2011 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,11 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.events;
+package com.google.gerrit.server.data;
 
-public class QueryStats {
-  public final String type = "stats";
-  public int rowCount;
-  public long runTimeMilliseconds;
-  public String resumeSortKey;
+public class MessageAttribute {
+    public Long timestamp;
+    public AccountAttribute reviewer;
+    public String message;
 }
--- gerrit-server/src/main/java/com/google/gerrit/server/data/SubmitRecordAttribute.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/data/SubmitRecordAttribute.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2010 The Android Open Source Project
+// Copyright (C) 2012 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,22 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.events;
+package com.google.gerrit.server.data;
 
 import java.util.List;
 
-public class PatchSetAttribute {
-  public String number;
-  public String revision;
-  public List<String> parents;
-  public String ref;
-  public AccountAttribute uploader;
-  public Long createdOn;
-  public AccountAttribute author;
-
-  public List<ApprovalAttribute> approvals;
-  public List<PatchSetCommentAttribute> comments;
-  public List<PatchAttribute> files;
-  public int sizeInsertions;
-  public int sizeDeletions;
+public class SubmitRecordAttribute {
+    public String status;
+    public List<SubmitLabelAttribute> labels;
 }

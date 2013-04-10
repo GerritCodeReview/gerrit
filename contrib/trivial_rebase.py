0ee021b543390d88699b414cb12bf692de145cd0
Orgad Shaneh <orgads@gmail.com>
2013-04-10 06:03:21 +0300

    TrivialRebase: Skip unsupported categories
    No reason to fail on such cases
    
    Change-Id: I58cf2c119c377a8e525f2dd307d437c56b6dadbd

--- contrib/trivial_rebase.py
+++ contrib/trivial_rebase.py
@@ -215,7 +215,7 @@ class TrivialRebase:
         continue
       else:
         print "Unsupported category: %s" % approval
-        return
+        continue
 
       score = approval["value"]
       gerrit_approve_cmd = ['gerrit', 'approve', '--project', self.project,

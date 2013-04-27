2a2e28b4fafb82251e05da06d471ed0b09fc78cc
Shawn Pearce <sop@google.com>
2013-04-26 17:52:28 -0700

    RFC Build with Buck
    Implement a new build system using Buck[1], Facebook's
    open source clone of Google's internal build system.
    
      Pros:
      - Fast
      - Concise build language
      - Extensible with simple blocks of Python
    
      Cons:
      - No Windows support
      - Bootstrap of buck requires Ant
    
    Getting started:
    
      git clone https://github.com/facebook/buck.git
      cd buck
      ant
    
      Mac OS X:
      alias buck='PATH=/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands:$PATH ANDROID_HOME=/tmp $HOME/git/buck/bin'
    
      Linux:
      alias buck='ANDROID_HOME=/tmp $HOME/git/buck/bin'
    
      # partial list of targets that are (mostly) ported
      buck build //gerrit-pgm:gerrit //gerrit-common:server //gerrit-common:client
    
    [1] http://facebook.github.io/buck/
    
    Change-Id: I40853b108bd8e153cefa0896a5280a9a5ff81655

---
+++ tools/download_jar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+OUT=$1
+URL=$2
+SHA1=$3
+
+curl -sfo "$OUT" "$URL" || exit
+if [ -n "$SHA1" ]
+then
+  have=$(openssl sha1 -sha1 -hex <"$OUT")
+  if ! [ "$SHA1" = "$have" ]
+  then
+    echo >&2 "fatal: bad content from $URL"
+    echo >&2 "       expected $SHA1"
+    echo >&2 "       received $have"
+  fi
+fi

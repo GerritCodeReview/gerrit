f52e3f4bb54f83cac78a6e05c60f9f430b30beac
Shawn Pearce <sop@google.com>
2013-04-26 17:52:28 -0700

    Build with Buck
    Implement a new build system using Buck[1], Facebook's
    open source clone of Google's internal build system.
    
      Pros:
      - Concise build language
      - Test and build output is concise
      - Test failures and stack traces show on terminal
      - Reliable incrementals; clean is unnecessary
      - Extensible with simple blocks of Python
      - Fast
          buck: clean: 0.452s, full 1m21.083s [*], no-op:  7.145s,
          mvn:  clean: 4.596s, full 2m53.776s,     no-op: 59.108s,
    
          [*] full build includes downloading all dependencies,
              time can vary due to remote server performance.
    
      Cons:
      - No Windows support
      - No native Maven Central support (added by macros)
      - No native GWT, Prolog, or WAR support (added by macros)
      - Bootstrap of buck requires Ant
    
    Getting started:
    
      git clone https://github.com/facebook/buck.git
      cd buck
      ant
    
      Mac OS X:
      PATH="`pwd`/bin/buck:/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands:$PATH"
    
      Linux:
      PATH="`pwd`/bin/buck:$PATH"
    
    Building:
    
      buck build :gerrit :api
      java -jar buck-out/gen/gerrit.war
      ls -lh buck-out/gen/{extension,plugin}-api.jar
    
    Testing:
    
      $ time buck test --all -e slow  # skip slow tests
      0m19.320s
    
      $ time buck test --all          # includes acceptance tests
      5m17.517s
    
    Documentation:
    
      time make: 57.0s ; time make -j7 9.9s
      time buck: 10.4s
    
    Full Release:
    
      buck test --all && buck build :release
      java -jar buck-out/gen/release.war
    
    Eclipse Project:
    
      buck build :eclipse
      Import existing project from `pwd`
      Expand 'gerrit'
      Right click 'buck-out' > Properties
      Under Attributes check 'Derived'
    
    [1] http://facebook.github.io/buck/
    
    Change-Id: I40853b108bd8e153cefa0896a5280a9a5ff81655

---
+++ lib/eclipse.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# TODO(sop): Remove hack after Buck supports Eclipse
+
+from os import path
+import re
+from subprocess import Popen, PIPE
+from xml.dom import minidom
+
+MAIN = [
+  '//gerrit-acceptance-tests:acceptance_tests',
+  '//gerrit-gwtui:ui_module',
+  '//gerrit-httpd:httpd_tests',
+  '//gerrit-launcher:launcher',
+  '//gerrit-pgm:pgm',
+  '//gerrit-server:server__compile',
+  '//gerrit-war:init',
+]
+JAVA = 'org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.6'
+
+src = set()
+lib = set()
+p = Popen(['buck', 'audit', 'classpath'] + MAIN, stdout = PIPE)
+for line in p.stdout:
+  line = line.strip()
+  m = re.search(r'/(gerrit-[^/]+)/lib__[^/]+__output/', line)
+  if m:
+    src.add(m.group(1))
+  else:
+    lib.add(line)
+r = p.wait()
+if r != 0:
+  exit(r)
+
+if not path.exists('.project'):
+  fd = open('.project', 'w')
+  print >>fd, """\
+<?xml version="1.0" encoding="UTF-8"?>
+<projectDescription>
+  <name>gerrit</name>
+  <buildSpec>
+    <buildCommand>
+      <name>org.eclipse.jdt.core.javabuilder</name>
+    </buildCommand>
+  </buildSpec>
+  <natures>
+    <nature>org.eclipse.jdt.core.javanature</nature>
+  </natures>
+</projectDescription>\
+"""
+  fd.close()
+
+doc = minidom.getDOMImplementation().createDocument(None, 'classpath', None)
+def classpathentry(kind, path, src = None):
+  e = doc.createElement('classpathentry')
+  e.setAttribute('kind', kind)
+  e.setAttribute('path', path)
+  if src:
+    e.setAttribute('sourcepath', src)
+  doc.documentElement.appendChild(e)
+
+for s in sorted(src):
+  for n in [
+      'src/main/java',
+      'src/main/resources',
+      'src/test/java',
+      'src/test/resources']:
+    p = path.join(s, n)
+    if path.exists(p):
+      classpathentry('src', p)
+for j in sorted(lib):
+  s = None
+  if j.endswith('.jar'):
+    s = j[0:-4] + '-src.jar'
+    if not path.exists(s):
+      s = None
+  classpathentry('lib', j, s)
+classpathentry('con', JAVA)
+classpathentry('output', 'buck-out/classes')
+
+fd = open('.classpath', 'w')
+doc.writexml(fd, addindent = '  ', newl = '\n', encoding='UTF-8')
+fd.close()

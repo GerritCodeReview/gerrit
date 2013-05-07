0e35a50fba33f189ac0b95827c8a5730918f88f6
Edwin Kempin <edwin.kempin@sap.com>
2013-05-07 16:15:55 +0200

    Support to retrieve the content of a file from a patch set via REST
    By GET on
    /changes/<change-id>/revisions/<revision-id>/files/<patch-id>/content
    it is now possible to retrieve the content of a certain file from a
    patch set. The file content is returned as base64 encoded string so
    that also binary files can be handled.
    
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>
    Change-Id: I429ce448c22b7b3b6ffd5503463676a798ea162a

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/GetContent.java
@@ -0,0 +1,75 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.common.io.BaseEncoding;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.inject.Inject;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+import java.io.CharArrayWriter;
+import java.io.IOException;
+
+public class GetContent implements RestReadView<FileResource> {
+  private final GitRepositoryManager repoManager;
+
+  @Inject
+  GetContent(GitRepositoryManager repoManager) {
+    this.repoManager = repoManager;
+  }
+
+  @Override
+  public String apply(FileResource rsrc) throws ResourceNotFoundException,
+      IOException {
+    Project.NameKey project = rsrc.getRevision().getControl().getProject().getNameKey();
+    Repository repo = repoManager.openRepository(project);
+    try {
+      RevWalk rw = new RevWalk(repo);
+      try {
+        RevCommit commit =
+            rw.parseCommit(ObjectId.fromString(rsrc.getRevision().getPatchSet()
+                .getRevision().get()));
+        TreeWalk tw =
+            TreeWalk.forPath(rw.getObjectReader(), rsrc.getPatchKey().get(),
+                commit.getTree().getId());
+        if (tw == null) {
+          throw new ResourceNotFoundException();
+        }
+
+        try {
+          ObjectLoader loader = repo.open(tw.getObjectId(0));
+          CharArrayWriter w = new CharArrayWriter(4 * (((int)loader.getSize() - 1)/3 + 1));
+          loader.copyTo(BaseEncoding.base64().encodingStream(w));
+          return w.toString();
+        } finally {
+          tw.release();
+        }
+      } finally {
+        rw.release();
+      }
+    } finally {
+      repo.close();
+    }
+  }
+}

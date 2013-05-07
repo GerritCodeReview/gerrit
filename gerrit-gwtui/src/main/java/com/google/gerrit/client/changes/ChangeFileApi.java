7c48661c21c33ae5da44b59d9e6435eac3753fe4
Dave Borowitz <dborowitz@google.com>
2013-05-07 13:43:00 +0100

    Add an edit button next to each file
    Change-Id: I6c4fd212e46a98c4e6bc0acb88ad1e5f960bb804

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeFileApi.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.changes;
+
+import com.google.gerrit.client.VoidResult;
+import com.google.gerrit.client.rpc.NativeString;
+import com.google.gerrit.client.rpc.RestApi;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * A collection of static methods which work on the Gerrit REST API for specific
+ * files in a change.
+ */
+public class ChangeFileApi {
+  /** Get the contents of a File in a PatchSet. */
+  public static void getContent(PatchSet.Id id, String filename, 
+      AsyncCallback<NativeString> content) {  // Needs NewPatchSet to get draft
+    ChangeApi.change(id.getParentKey().get()).view("revisions").id(id.get())
+      .view("files").id(filename).view("content").get(content);
+  }
+}
--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/PatchTable.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/PatchTable.java
@@ -17,10 +17,15 @@ package com.google.gerrit.client.changes;
 import com.google.gerrit.client.Dispatcher;
 import com.google.gerrit.client.Gerrit;
 import com.google.gerrit.client.patches.PatchScreen;
+import com.google.gerrit.client.VoidResult;
+import com.google.gerrit.client.rpc.GerritCallback;
+import com.google.gerrit.client.rpc.NativeString;
+import com.google.gerrit.client.ui.CommentedActionDialog;
 import com.google.gerrit.client.ui.InlineHyperlink;
 import com.google.gerrit.client.ui.ListenableAccountDiffPreference;
 import com.google.gerrit.client.ui.NavigationTable;
 import com.google.gerrit.client.ui.PatchLink;
+import com.google.gerrit.common.data.ChangeDetail;
 import com.google.gerrit.common.data.PatchSetDetail;
 import com.google.gerrit.reviewdb.client.Patch;
 import com.google.gerrit.reviewdb.client.Patch.ChangeType;
@@ -38,6 +43,7 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.HTMLTable.Cell;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
@@ -393,12 +399,24 @@ public class PatchTable extends Composite {
     }
 
     void initializeRow(int row) {
-      Patch patch = PatchTable.this.patchList.get(row - 1);
+      final Patch patch = PatchTable.this.patchList.get(row - 1);
       setRowItem(row, patch);
 
-      Widget nameCol;
-      nameCol = new PatchLink.SideBySide(getDisplayFileName(patch), base,
-          patch.getKey(), row - 1, detail, PatchTable.this);
+      final Image edit = new Image(Gerrit.RESOURCES.edit());
+      edit.addStyleName(Gerrit.RESOURCES.css().link());
+//        edit.setTitle(Util.C.changeInfoBlockTopicAlterTopicToolTip());
+      edit.setTitle("Edit Draft File");
+      edit.addClickHandler(new  ClickHandler() {
+        @Override
+        public void onClick(final ClickEvent event) {
+          new EditFileDialog(patch).center();
+        }
+      });
+
+      final FlowPanel nameCol = new FlowPanel();
+      nameCol.add(edit);
+      nameCol.add(new PatchLink.SideBySide(getDisplayFileName(patch), base,
+          patch.getKey(), row - 1, detail, PatchTable.this));
 
       if (patch.getSourceFileName() != null) {
         final String text;
@@ -411,11 +429,9 @@ public class PatchTable extends Composite {
         }
         final Label line = new Label(text);
         line.setStyleName(Gerrit.RESOURCES.css().sourceFilePath());
-        final FlowPanel cell = new FlowPanel();
-        cell.add(nameCol);
-        cell.add(line);
-        nameCol = cell;
+        nameCol.add(line);
       }
+
       table.setWidget(row, C_PATH, nameCol);
 
       int C_UNIFIED = C_SIDEBYSIDE + 1;
@@ -888,5 +904,54 @@ public class PatchTable extends Composite {
       }
     }
     return true;
+  }
+
+  private class EditFileDialog extends CommentedActionDialog<ChangeDetail> {
+    Patch patch;
+
+    EditFileDialog(Patch patch) {
+      super("Edit Draft File", getDisplayFileName(patch),
+          new ChangeDetailCache.IgnoreErrorCallback());
+      this.patch = patch;
+      sendButton.setText("Publish"); // Save instead eventually
+    }
+
+    @Override
+    protected void onLoad() {
+      super.onLoad();
+      ChangeFileApi.getContent(patch.getKey().getParentKey(), patch.getFileName(),
+          new GerritCallback<NativeString>() {
+            @Override
+            public void onSuccess(NativeString result) {
+              display(result.asString());
+            }
+          });
+    }
+
+    public void display(String content) {
+      message.setText(content);
+      sendButton.setEnabled(true);
+    }
+
+    @Override
+    public void onSend() {
+      enableButtons(false);
+      ChangeFileApi.putContent(patch.getKey().getParentKey(), patch.getFileName(),
+          getMessageText(), new GerritCallback<VoidResult>() {
+            @Override
+            public void onSuccess(VoidResult result) {
+              hide();
+          // For demo, call publish REST-API here
+          // Gerrit.display(PageLinks.toChange(patch.getParentKey().getParentKey()));
+          //   It might be good to force the patchset disclosure "open" here?
+            }
+
+            @Override
+            public void onFailure(final Throwable caught) {
+              enableButtons(true);
+              super.onFailure(caught);
+            }
+          });
+    }
   }
 }

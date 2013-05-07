7f510ffea587a551e4d2cb552f316727c8a6f316
Dave Borowitz <dborowitz@google.com>
2013-05-07 13:44:03 +0100

    Support PUT /changes/X/revisions/Y.edit/files/PATH/content
    Change-Id: I7c1f69686d7252ca4088174e10618b2997ced564

--- gerrit-server/src/main/java/com/google/gerrit/server/change/RevisionResource.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/RevisionResource.java
@@ -69,6 +69,12 @@ public class RevisionResource implements RestResource {
     }
   }
 
+  void checkEdit() throws ResourceNotFoundException {
+    if (!revId.isEdit()) {
+      throw new ResourceNotFoundException();
+    }
+  }
+
   RevisionId getId() {
     return revId;
   }

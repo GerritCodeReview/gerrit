53b93dc4f8a1baf9d57efe4733e3cf61eaf9f847
Dariusz Luksza <dariusz@luksza.org>
2013-05-07 17:21:23 +0200

    Add top menu extension point
    This extension point allows plugin implementers contribute entry's to
    top menu of Gerit Web UI.
    
    The usage is really simple, just implement TopMenuExtension and add
    @Listen annotation (also one should remove Gerrit-Module definition from
    pom.xml).
    
    Currently we only allow contribute top level menu entry's with given
    name and list of menu items with name and URL.
    
    Example plugin can be found at:
    https://github.com/dluksza/gerrit-top-menu-extension-example
    
    Change-Id: Ie161e0a61fdf041340df1aa0d6945fd479f34842
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/WebModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/WebModule.java
@@ -19,6 +19,7 @@ import static com.google.inject.Scopes.SINGLETON;
 
 import com.google.gerrit.common.data.GerritConfig;
 import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.webui.TopMenuExtension;
 import com.google.gerrit.extensions.webui.WebUiPlugin;
 import com.google.gerrit.httpd.auth.become.BecomeAnyAccountModule;
 import com.google.gerrit.httpd.auth.container.HttpAuthModule;
@@ -129,6 +130,7 @@ public class WebModule extends FactoryModule {
     bind(GerritConfigProvider.class);
     bind(GerritConfig.class).toProvider(GerritConfigProvider.class);
     DynamicSet.setOf(binder(), WebUiPlugin.class);
+    DynamicSet.setOf(binder(), TopMenuExtension.class);
 
     factory(ClearPassword.Factory.class);
     install(new AsyncReceiveCommits.Module());

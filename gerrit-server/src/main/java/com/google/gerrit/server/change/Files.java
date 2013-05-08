4d92fa60a91f8f6e584ad4c7f9756b43ec8a3e11
Colby Ranger <cranger@google.com>
2013-05-07 19:55:34 -0700

    Move FileInfo to FileInfoJson and implement Files.list().
    Change-Id: I893dd097f2372f9a0e2a788a973fa30b53b3e0e4

--- gerrit-server/src/main/java/com/google/gerrit/server/change/Files.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Files.java
@@ -16,19 +16,32 @@ package com.google.gerrit.server.change;
 
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ChildCollection;
 import com.google.gerrit.extensions.restapi.IdString;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import org.kohsuke.args4j.Option;
 
 class Files implements ChildCollection<RevisionResource, FileResource> {
   private final DynamicMap<RestView<FileResource>> views;
+  private final FileInfoJson fileInfoJson;
+  private final Provider<Revisions> revisions;
 
   @Inject
-  Files(DynamicMap<RestView<FileResource>> views) {
+  Files(DynamicMap<RestView<FileResource>> views,
+      FileInfoJson fileInfoJson,
+      Provider<Revisions> revisions) {
     this.views = views;
+    this.fileInfoJson = fileInfoJson;
+    this.revisions = revisions;
   }
 
   @Override
@@ -38,12 +51,30 @@ class Files implements ChildCollection<RevisionResource, FileResource> {
 
   @Override
   public RestView<RevisionResource> list() throws AuthException {
-    throw new UnsupportedOperationException();
+    return new List();
   }
 
   @Override
   public FileResource parse(RevisionResource rev, IdString id)
       throws ResourceNotFoundException, OrmException, AuthException {
     return new FileResource(rev, id.get());
+  }
+
+  private final class List implements RestReadView<RevisionResource> {
+    @Option(name = "--base", metaVar = "revision-id")
+    String base;
+
+    @Override
+    public Object apply(RevisionResource resource) throws AuthException,
+        BadRequestException, ResourceConflictException, Exception {
+      PatchSet basePatchSet = null;
+      if (base != null) {
+        RevisionResource baseResource = revisions.get().parse(
+            resource.getChangeResource(), IdString.fromUrl(base));
+        basePatchSet = baseResource.getPatchSet();
+      }
+      return fileInfoJson.toFileInfoMap(
+          resource.getChange(), resource.getPatchSet(), basePatchSet);
+    }
   }
 }

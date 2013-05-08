14f1dfadcfd82884c7e71a6a1db1ab5aecc5cb59
Dave Borowitz <dborowitz@google.com>
2013-05-08 23:38:40 +0100

    Add a method for fetching all revision edits for a patch set
    Change-Id: I256891a37a9dd81b80383887fa786a6490273cc7

--- gerrit-server/src/main/java/com/google/gerrit/server/git/RevisionEdit.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/RevisionEdit.java
@@ -14,6 +14,8 @@
 
 package com.google.gerrit.server.git;
 
+import com.google.common.collect.Maps;
+import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.server.IdentifiedUser;
 
@@ -23,10 +25,43 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 
 /** A single user's edits on top of an existing patch set. */
 public class RevisionEdit {
   private static final String REFS_EDITS = "refs/edits/";
+
+  public static Map<PatchSet.Id, RevisionEdit> forChange(Repository repo,
+      Change.Id changeId, IdentifiedUser user) throws IOException {
+    Set<String> names = repo.getRefDatabase().getRefs(
+        refPrefix(user, changeId).toString()).keySet();
+    Map<PatchSet.Id, RevisionEdit> result =
+        Maps.newHashMapWithExpectedSize(names.size());
+    for (String name : names) {
+      PatchSet.Id psid = new PatchSet.Id(changeId, Integer.valueOf(name));
+      result.put(psid, new RevisionEdit(user, psid));
+    }
+    return Collections.unmodifiableMap(result);
+  }
+
+  private static StringBuilder refPrefix(IdentifiedUser user,
+      Change.Id changeId) {
+    StringBuilder r = new StringBuilder();
+    r.append(REFS_EDITS);
+    appendSharded(r, user.getAccountId().get());
+    appendSharded(r, changeId.get());
+    return r.append('/');
+  }
+
+  private static void appendSharded(StringBuilder sb, int n) {
+    int m = n % 100;
+    if (m < 10) {
+      sb.append('0');
+    }
+    sb.append(m).append('/').append(n).append('/');
+  }
 
   private final IdentifiedUser user;
   private final PatchSet.Id psid;
@@ -37,19 +72,7 @@ public class RevisionEdit {
   }
 
   public String toRefName() {
-    StringBuilder r = new StringBuilder();
-    r.append(REFS_EDITS);
-    appendSharded(r, user.getAccountId().get());
-    appendSharded(r, psid.getParentKey().get());
-    return r.append(psid.get()).toString();
-  }
-
-  private static void appendSharded(StringBuilder sb, int n) {
-    int m = n % 100;
-    if (m < 10) {
-      sb.append('0');
-    }
-    sb.append(m).append('/').append(n).append('/');
+    return refPrefix(user, psid.getParentKey()).append(psid.get()).toString();
   }
 
   public RevCommit get(Repository repo, RevWalk rw) throws IOException {

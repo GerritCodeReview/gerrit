c57bff48d14059cbb50595f5f7604a52f208e8ff
Shawn Pearce <sop@google.com>
2013-05-07 22:29:02 -0700

    Build with Buck
    Implement a new build system using Buck[1], Facebook's
    open source clone of Google's internal build system.
    
      Pros:
      - Concise build language
      - Test and build output is concise
      - Test failures and stack traces show on terminal
      - Reliable incrementals; clean is unnecessary
      - Extensible with simple blocks of Python
      - Fast
          buck: clean: 0.452s, full 1m21.083s [*], no-op:  7.145s,
          mvn:  clean: 4.596s, full 2m53.776s,     no-op: 59.108s,
    
          [*] full build includes downloading all dependencies,
              time can vary due to remote server performance.
    
      Cons:
      - No Windows support
      - No native Maven Central support (added by macros)
      - No native GWT, Prolog, or WAR support (added by macros)
      - Bootstrap of buck requires Ant
    
    Getting started:
    
      git clone https://github.com/facebook/buck.git
      cd buck
      ant
    
      Mac OS X:
      PATH="`pwd`/bin/buck:/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands:$PATH"
    
      Linux:
      PATH="`pwd`/bin/buck:$PATH"
    
    Building:
    
      buck build :gerrit
      java -jar buck-out/gen/gerrit.war
    
    Testing:
    
      $ time buck test --all -e slow  # skip slow tests
      0m19.320s
    
      $ time buck test --all          # includes acceptance tests
      5m17.517s
    
    Documentation:
    
      time make: 57.0s ; time make -j7 9.9s
      time buck: 10.4s
    
    Full Release:
    
      buck test --all && buck build :release :api
      java -jar buck-out/gen/release.war
      ls -lh buck-out/gen/{extension,plugin}-api.jar
    
    Eclipse Project:
    
      buck build :eclipse
      Import existing project from `pwd`
      Expand 'gerrit'
      Right click 'buck-out' > Properties
      Under Attributes check 'Derived'
    
    [1] http://facebook.github.io/buck/
    
    Change-Id: I40853b108bd8e153cefa0896a5280a9a5ff81655

---
+++ Documentation/licenses.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# TODO(sop): Be more detailed: version, link to Maven Central
+
+from collections import defaultdict, deque
+import re
+from shutil import copyfileobj
+from subprocess import Popen, PIPE
+from sys import stdout
+
+MAIN = ['//gerrit-pgm:pgm', '//gerrit-gwtui:ui_module']
+
+def parse_graph():
+  graph = defaultdict(list)
+  p = Popen(
+    ['buck', 'audit', 'classpath', '--dot'] + MAIN,
+    stdout = PIPE)
+  for line in p.stdout:
+    m = re.search(r'"(//.*?)" -> "(//.*?)";', line)
+    if not m:
+      continue
+    target, dep = m.group(1), m.group(2)
+    if not target.endswith('__compile'):
+      graph[target].append(dep)
+  r = p.wait()
+  if r != 0:
+    exit(r)
+  return graph
+
+graph = parse_graph()
+licenses = defaultdict(set)
+
+queue = deque(MAIN)
+while queue:
+  target = queue.popleft()
+  for dep in graph[target]:
+    if not dep.startswith('//lib:LICENSE-'):
+      continue
+    licenses[dep].add(target)
+  queue.extend(graph[target])
+used = sorted(licenses.iterkeys())
+
+print """\
+Gerrit Code Review - Licenses
+=============================
+
+Gerrit open source software licensed under the <<Apache2.0,Apache
+License 2.0>>.  Executable distributions also include other software
+components that are provided under additional licenses.
+
+[[cryptography]]
+Cryptography Notice
+-------------------
+
+This distribution includes cryptographic software.  The country
+in which you currently reside may have restrictions on the import,
+possession, use, and/or re-export to another country, of encryption
+software.  BEFORE using any encryption software, please check
+your country's laws, regulations and policies concerning the
+import, possession, or use, and re-export of encryption software,
+to see if this is permitted.  See the
+link:http://www.wassenaar.org/[Wassenaar Arrangement]
+for more information.
+
+The U.S. Government Department of Commerce, Bureau of Industry
+and Security (BIS), has classified this software as Export
+Commodity Control Number (ECCN) 5D002.C.1, which includes
+information security software using or performing cryptographic
+functions with asymmetric algorithms.  The form and manner of
+this distribution makes it eligible for export under the License
+Exception ENC Technology Software Unrestricted (TSU) exception
+(see the BIS Export Administration Regulations, Section 740.13)
+for both object code and source code.
+
+Gerrit includes an SSH daemon (Apache SSHD), to support authenticated
+uploads of changes directly from `git push` command line clients.
+
+Gerrit includes an SSH client (JSch), to support authenticated
+replication of changes to remote systems, such as for automatic
+updates of mirror servers, or realtime backups.
+
+For either feature to function, Gerrit requires the
+link:http://java.sun.com/javase/technologies/security/[Java Cryptography extensions]
+and/or the
+link:http://www.bouncycastle.org/java.html[Bouncy Castle Crypto API]
+to be installed by the end-user.
+
+Licenses
+--------
+"""
+
+for n in used:
+  libs = sorted(licenses[n])
+  name = n[len('//lib:LICENSE-'):]
+  print
+  print '[[%s]]' % name
+  print name
+  print '~' * len(name)
+  print
+  for d in libs:
+    if d.startswith('//lib:') or d.startswith('//lib/'):
+      p = d[len('//lib:'):]
+    else:
+      p = d[d.index(':')+1:].lower()
+    print '* ' + p
+  print
+  print '----'
+  with open(n[2:].replace(':', '/')) as fd:
+    copyfileobj(fd, stdout)
+  print '----'
+
+print """
+GERRIT
+------
+Part of link:index.html[Gerrit Code Review]
+"""
---
+++ lib/eclipse_project.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# TODO(sop): Remove hack after Buck supports Eclipse
+
+from os import path
+
+ROOT = path.dirname(path.dirname(path.abspath(__file__)))
+
+with open(path.join(ROOT, '.project'), 'w') as fd:
+  print >>fd, """\
+<?xml version="1.0" encoding="UTF-8"?>
+<projectDescription>
+  <name>gerrit</name>
+  <buildSpec>
+    <buildCommand>
+      <name>org.eclipse.jdt.core.javabuilder</name>
+    </buildCommand>
+  </buildSpec>
+  <natures>
+    <nature>org.eclipse.jdt.core.javanature</nature>
+  </natures>
+</projectDescription>\
+"""

a1060c9c988d1cb9224f5d7183725770a821357d
Shawn Pearce <sop@google.com>
2013-05-08 14:14:24 -0700

    Build with Buck
    Implement a new build system using Buck[1], Facebook's
    open source clone of Google's internal build system.
    
      Pros:
      - Concise build language
      - Test and build output is concise
      - Test failures and stack traces show on terminal
      - Reliable incrementals; clean is unnecessary
      - Extensible with simple blocks of Python
      - Fast
          buck: clean: 0.452s, full 1m21.083s [*], no-op:  7.145s,
          mvn:  clean: 4.596s, full 2m53.776s,     no-op: 59.108s,
    
          [*] full build includes downloading all dependencies,
              time can vary due to remote server performance.
    
      Cons:
      - No Windows support
      - No native Maven Central support (added by macros)
      - No native GWT, Prolog, or WAR support (added by macros)
      - Bootstrap of buck requires Ant
    
    Getting started:
    
      git clone https://gerrit.googlesource.com/buck
      cd buck
      ant
    
      Mac OS X:
        PATH="`pwd`/bin/buck:/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands:$PATH"
    
      Linux:
        PATH="`pwd`/bin/buck:$PATH"
    
    Importing into Eclipse:
    
      $ time buck build :eclipse
      0m48.949s
    
      Import existing project from `pwd`
      Import 'gerrit' (do not import other Maven based projects)
      Expand 'gerrit'
      Right click 'buck-out' > Properties
      Under Attributes check 'Derived'
    
      If the code doesn't currently compile but an updated classpath
      is needed, refresh the configs and obtain missing JARs:
    
      $ buck build :eclipse_project :download
    
    Running JUnit tests:
    
      $ time buck test --all -e slow  # skip slow tests
      0m19.320s
    
      $ time buck test --all          # includes acceptance tests
      5m17.517s
    
    Building WAR:
    
      $ buck build :gerrit
      $ java -jar buck-out/gen/gerrit.war
    
    Building release:
    
      $ buck test --all && buck build :api :release
      $ java -jar buck-out/gen/release.war
      $ ls -lh buck-out/gen/{extension,plugin}-api.jar
    
    Downloading dependencies:
    
      Dependencies are normally downloaded automatically, but Buck can
      invoke inspect its graph and download missing dependencies so future
      compiles can run without the network:
    
      $ buck build :download
    
    [1] http://facebook.github.io/buck/
    
    Change-Id: I40853b108bd8e153cefa0896a5280a9a5ff81655

---
+++ tools/download_jar.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from hashlib import sha1
+from optparse import OptionParser
+from os import link, makedirs, path
+import shutil
+from subprocess import check_call
+from sys import stderr
+from zipfile import ZipFile, BadZipfile, LargeZipFile
+
+def hashfile(p):
+  d = sha1()
+  with open(p, 'rb') as f:
+    while True:
+      b = f.read(8192)
+      if not b:
+        break
+      d.update(b)
+  return d.hexdigest()
+
+def safe_mkdirs(d):
+  if path.isdir(d):
+    return
+  try:
+    makedirs(d)
+  except OSError as err:
+    if not path.isdir(d):
+      raise err
+
+opts = OptionParser()
+opts.add_option('-o', help='local output file')
+opts.add_option('-u', help='URL to download')
+opts.add_option('-v', help='expected content SHA-1')
+opts.add_option('-x', action='append', help='file to delete from ZIP')
+opts.add_option('--exclude_java_sources', action='store_true')
+args, _ = opts.parse_args()
+
+root_dir = args.o
+while root_dir:
+  root_dir, n = path.split(root_dir)
+  if n == 'buck-out':
+    break
+
+cache_ent = path.join(
+    root_dir,
+    'buck-cache',
+    '%s-%s' % (path.basename(args.o), sha1(args.u).hexdigest()))
+
+if not path.exists(cache_ent):
+  try:
+    safe_mkdirs(path.dirname(cache_ent))
+    check_call(['curl', '-sfo', cache_ent, args.u])
+  except OSError as err:
+    print >>stderr, "error using curl: %s" % str(err)
+    exit(1)
+
+if args.v:
+  have = hashfile(cache_ent)
+  if args.v != have:
+    o = cache_ent[len(root_dir) + 1:]
+    print >>stderr, (
+      '%s:\n' +
+      'expected %s\n' +
+      'received %s\n' +
+      '         %s\n') % (args.u, args.v, have, o)
+    exit(1)
+
+exclude = []
+if args.x:
+  exclude += args.x
+if args.exclude_java_sources:
+  try:
+    zf = ZipFile(cache_ent, 'r')
+    try:
+      for n in zf.namelist():
+        if n.endswith('.java'):
+          exclude.append(n)
+    finally:
+      zf.close()
+  except (BadZipfile, LargeZipFile) as err:
+    print >>stderr, "error opening %s: %s"  % (cache_ent, str(err))
+    exit(1)
+
+safe_mkdirs(path.dirname(args.o))
+if exclude:
+  shutil.copyfile(cache_ent, args.o)
+  check_call(['zip', '-d', args.o] + exclude)
+else:
+  try:
+    link(cache_ent, args.o)
+  except OSError as err:
+    symlink(cache_ent, args.o)

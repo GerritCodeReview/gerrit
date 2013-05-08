971bc607ac804adc9c4495248ca7158100e86e7b
Shawn Pearce <sop@google.com>
2013-05-07 22:54:32 -0700

    Implement plugin WrappedContext using reflection based proxy
    Gerrit sources do not compile directly against servlet 3.0 because
    WrappedContext implements the ServletContext interface.  In the 3.0
    API a number of new methods and types were added which are not
    available in 2.5.  Unfortunately WrappedContext needs to compile for
    both APIs in order to support the "odd" linking case at Google.
    
    Implement the ServletContext using an indirect reflection based proxy,
    with supported methods delgated to a Handler implementation.  This
    allows the source code to compile against either servlet API version,
    with newer methods throwing NoSuchMethodError.
    
    Change-Id: Id41e37d4cacdd0cb1c90828fc4c2436b7e1284a7

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/plugins/WrappedContext.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/plugins/WrappedContext.java
@@ -22,7 +22,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.net.URL;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -32,147 +34,205 @@ import java.util.concurrent.ConcurrentMap;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 
-class WrappedContext implements ServletContext {
+class WrappedContext {
   private static final Logger log = LoggerFactory.getLogger("plugin");
-  private final Plugin plugin;
-  private final String contextPath;
-  private final ConcurrentMap<String, Object> attributes;
 
-  WrappedContext(Plugin plugin, String contextPath) {
-    this.plugin = plugin;
-    this.contextPath = contextPath;
-    this.attributes = Maps.newConcurrentMap();
+  static ServletContext create(Plugin plugin, String contextPath) {
+    return (ServletContext) Proxy.newProxyInstance(
+        WrappedContext.class.getClassLoader(),
+        new Class[] {ServletContext.class, API.class},
+        new Handler(plugin, contextPath));
   }
 
-  @Override
-  public String getContextPath() {
-    return contextPath;
+  private WrappedContext() {
   }
 
-  @Override
-  public String getInitParameter(String name) {
-    return null;
+  private static class Handler implements InvocationHandler, API {
+    private final Plugin plugin;
+    private final String contextPath;
+    private final ConcurrentMap<String, Object> attributes;
+
+    Handler(Plugin plugin, String contextPath) {
+      this.plugin = plugin;
+      this.contextPath = contextPath;
+      this.attributes = Maps.newConcurrentMap();
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args)
+        throws Throwable {
+      Method handler;
+      try {
+        handler = API.class.getDeclaredMethod(
+            method.getName(),
+            method.getParameterTypes());
+      } catch (NoSuchMethodException e) {
+        throw new NoSuchMethodError();
+      }
+      return handler.invoke(this, args);
+    }
+
+    @Override
+    public String getContextPath() {
+      return contextPath;
+    }
+
+    @Override
+    public String getInitParameter(String name) {
+      return null;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Enumeration getInitParameterNames() {
+      return Collections.enumeration(Collections.emptyList());
+    }
+
+    @Override
+    public ServletContext getContext(String name) {
+      return null;
+    }
+
+    @Override
+    public RequestDispatcher getNamedDispatcher(String name) {
+      return null;
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String name) {
+      return null;
+    }
+
+    @Override
+    public URL getResource(String name) {
+      return null;
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String name) {
+      return null;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Set getResourcePaths(String name) {
+      return null;
+    }
+
+    @Override
+    public Servlet getServlet(String name) {
+      return null;
+    }
+
+    @Override
+    public String getRealPath(String name) {
+      return null;
+    }
+
+    @Override
+    public String getServletContextName() {
+      return plugin.getName();
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Enumeration getServletNames() {
+      return Collections.enumeration(Collections.emptyList());
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Enumeration getServlets() {
+      return Collections.enumeration(Collections.emptyList());
+    }
+
+    @Override
+    public void log(Exception reason, String msg) {
+      log(msg, reason);
+    }
+
+    @Override
+    public void log(String msg) {
+      log(msg, null);
+    }
+
+    @Override
+    public void log(String msg, Throwable reason) {
+      log.warn(String.format("[plugin %s] %s", plugin.getName(), msg), reason);
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+      return attributes.get(name);
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+      return Collections.enumeration(attributes.keySet());
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+      attributes.put(name, value);
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+      attributes.remove(name);
+    }
+
+    @Override
+    public String getMimeType(String file) {
+      return null;
+    }
+
+    @Override
+    public int getMajorVersion() {
+      return 2;
+    }
+
+    @Override
+    public int getMinorVersion() {
+      return 5;
+    }
+
+    @Override
+    public String getServerInfo() {
+      String v = Version.getVersion();
+      return "Gerrit Code Review/" + (v != null ? v : "dev");
+    }
   }
 
-  @SuppressWarnings("rawtypes")
-  @Override
-  public Enumeration getInitParameterNames() {
-    return Collections.enumeration(Collections.emptyList());
-  }
-
-  @Override
-  public ServletContext getContext(String name) {
-    return null;
-  }
-
-  @Override
-  public RequestDispatcher getNamedDispatcher(String name) {
-    return null;
-  }
-
-  @Override
-  public RequestDispatcher getRequestDispatcher(String name) {
-    return null;
-  }
-
-  @Override
-  public URL getResource(String name) throws MalformedURLException {
-    return null;
-  }
-
-  @Override
-  public InputStream getResourceAsStream(String name) {
-    return null;
-  }
-
-  @SuppressWarnings("rawtypes")
-  @Override
-  public Set getResourcePaths(String name) {
-    return null;
-  }
-
-  @Override
-  public Servlet getServlet(String name) throws ServletException {
-    return null;
-  }
-
-  @Override
-  public String getRealPath(String name) {
-    return null;
-  }
-
-  @Override
-  public String getServletContextName() {
-    return plugin.getName();
-  }
-
-  @SuppressWarnings("rawtypes")
-  @Override
-  public Enumeration getServletNames() {
-    return Collections.enumeration(Collections.emptyList());
-  }
-
-  @SuppressWarnings("rawtypes")
-  @Override
-  public Enumeration getServlets() {
-    return Collections.enumeration(Collections.emptyList());
-  }
-
-  @Override
-  public void log(Exception reason, String msg) {
-    log(msg, reason);
-  }
-
-  @Override
-  public void log(String msg) {
-    log(msg, null);
-  }
-
-  @Override
-  public void log(String msg, Throwable reason) {
-    log.warn(String.format("[plugin %s] %s", plugin.getName(), msg), reason);
-  }
-
-  @Override
-  public Object getAttribute(String name) {
-    return attributes.get(name);
-  }
-
-  @Override
-  public Enumeration<String> getAttributeNames() {
-    return Collections.enumeration(attributes.keySet());
-  }
-
-  @Override
-  public void setAttribute(String name, Object value) {
-    attributes.put(name, value);
-  }
-
-  @Override
-  public void removeAttribute(String name) {
-    attributes.remove(name);
-  }
-
-  @Override
-  public String getMimeType(String file) {
-    return null;
-  }
-
-  @Override
-  public int getMajorVersion() {
-    return 2;
-  }
-
-  @Override
-  public int getMinorVersion() {
-    return 5;
-  }
-
-  @Override
-  public String getServerInfo() {
-    String v = Version.getVersion();
-    return "Gerrit Code Review/" + (v != null ? v : "dev");
+  static interface API {
+    String getContextPath();
+    String getInitParameter(String name);
+    @SuppressWarnings("rawtypes")
+    Enumeration getInitParameterNames();
+    ServletContext getContext(String name);
+    RequestDispatcher getNamedDispatcher(String name);
+    RequestDispatcher getRequestDispatcher(String name);
+    URL getResource(String name);
+    InputStream getResourceAsStream(String name);
+    @SuppressWarnings("rawtypes")
+    Set getResourcePaths(String name);
+    Servlet getServlet(String name);
+    String getRealPath(String name);
+    String getServletContextName();
+    @SuppressWarnings("rawtypes")
+    Enumeration getServletNames();
+    @SuppressWarnings("rawtypes")
+    Enumeration getServlets();
+    void log(Exception reason, String msg);
+    void log(String msg);
+    void log(String msg, Throwable reason);
+    Object getAttribute(String name);
+    Enumeration<String> getAttributeNames();
+    void setAttribute(String name, Object value);
+    void removeAttribute(String name);
+    String getMimeType(String file);
+    int getMajorVersion();
+    int getMinorVersion();
+    String getServerInfo();
   }
 }

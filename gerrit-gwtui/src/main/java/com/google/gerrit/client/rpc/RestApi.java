6757fbc896e9df693dbfb27295de76a801494dfa
Dave Borowitz <dborowitz@google.com>
2013-05-08 20:28:35 +0100

    Allow RestApi calls to receive text from handlers
    Wrap the result in a NativeString, and naively cast it to whatever
    the callback is asking for (which is not particularly less safe than
    what we do for JSON).
    
    Change-Id: I1807ce33544964533f8d2a88c45576c18f4394ec

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/rpc/RestApi.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/rpc/RestApi.java
@@ -124,26 +124,29 @@ public class RestApi {
         }
 
       } else if (200 <= status && status < 300) {
-        if (!isJsonBody(res)) {
+        T data;
+        if (isTextBody(res)) {
+          data = NativeString.wrap(res.getText()).cast();
+          res.getText();
+        } else if (isJsonBody(res)) {
+          try {
+            // javac generics bug
+            data = RestApi.<T>cast(parseJson(res));
+          } catch (JSONException e) {
+            if (!background) {
+              RpcStatus.INSTANCE.onRpcComplete();
+            }
+            cb.onFailure(new StatusCodeException(SC_BAD_RESPONSE,
+                "Invalid JSON: " + e.getMessage()));
+            return;
+          }
+        } else {
           if (!background) {
             RpcStatus.INSTANCE.onRpcComplete();
           }
           cb.onFailure(new StatusCodeException(SC_BAD_RESPONSE, "Expected "
-              + JSON_TYPE + "; received Content-Type: "
+              + JSON_TYPE + " or " + TEXT_TYPE + "; received Content-Type: "
               + res.getHeader("Content-Type")));
-          return;
-        }
-
-        T data;
-        try {
-          // javac generics bug
-          data = RestApi.<T>cast(parseJson(res));
-        } catch (JSONException e) {
-          if (!background) {
-            RpcStatus.INSTANCE.onRpcComplete();
-          }
-          cb.onFailure(new StatusCodeException(SC_BAD_RESPONSE,
-              "Invalid JSON: " + e.getMessage()));
           return;
         }
 

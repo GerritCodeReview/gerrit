53510879acb29d5708556e502bb217ede1ff58d8
Dariusz Luksza <dariusz@luksza.org>
2013-05-09 21:09:59 +0200

    Improve error message when JS plugin fails to load.
    Previous implementation was showing misleading error message when plugin
    was removed by hand from server. Now message is more informative.
    
    Change-Id: Id18fd995033f9c2f8dae22f054308d17fa05e04d
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/GerritMessages.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/GerritMessages.java
@@ -33,4 +33,5 @@ public interface GerritMessages extends Messages {
   String branchCreationConflict(String branchName, String existingBranchName);
 
   String pluginFailed(String scriptPath);
+  String cannotDownloadlPlugin(String scriptPath);
 }

853922cc09a040a711527a628d57ad3617037c21
Sasa Zivkov <sasa.zivkov@sap.com>
2013-05-10 15:12:13 +0200

    Optionally, initialize site from WebAppInitializer.
    This supports automatic site initialization on Gerrit server startup
    when Gerrit runs in a servlet container. Both creation of a new site
    and upgrade of an existing site are supported.
    
    This feature may be useful for such setups where Gerrit admins don't
    have direct access to the database and the file system of the server
    where Gerrit should be deployed and, therefore, cannot perform the
    init from their local machine prior to deploying Gerrit on such a
    server. It may also make deployment and testing in a local servlet
    container faster to setup as the init step could be skipped.
    
    The site initialization will be performed only if the 'gerrit.init'
    system property exists (the value of the property is not used only the
    existence of the property matters).
    
    If the 'gerrit.site_path' system property is defined then the init is
    run for that site. The database connectivity, in that case, is defined
    in the etc/gerrit.config.
    
    If 'gerrit.site_path' is not defined then Gerrit will try to find an
    existing site by looking into the system_config table in the database
    defined via the 'jdbc/ReviewDb' JNDI property. If system_config table
    exists then the site_path from that table is used for initialization.
    Database connectivity is defined by the jdbc/ReviewDb JNDI property.
    
    Finally, if neither 'gerrit.site_path' property nor the system_config
    table exist, the 'gerrit.init_path' system property, if defined, will
    be used to determine the site path. Database connectivity, also for this
    case,is defined by the jdbc/ReviewDb JNDI property.
    
    Example 1:
      Prepare Tomcat so that a site is initialized at a given path using
      the H2 database (if the site doesn't exist yet) or using whatever
      database is defined in the etc/gerrit.config of that site:
    
      $ export CATALINA_OPTS='-Dgerrit.init -Dgerrit.site_path=/path/to/site'
      $ catalina.sh start
    
    Example 2:
      Prepare Tomcat so that an existing site with the path defined in the
      system_config table is initialized (upgraded) on Gerrit startup. The
      assumption is that the jdbc/ReviewDb JNDI property is defined in
      Tomcat:
    
      $ export CATALINA_OPTS='-Dgerrit.init'
      $ catalina.sh start
    
    Example 3:
      Assuming the database schema doesn't exist in the database defined
      via the jdbc/ReviewDb JNDI property initialize a new site using that
      database and a given path:
    
      $ export CATALINA_OPTS='-Dgerrit.init -Dgerrit.init_path=/path/to/site'
      $ catalina.sh start
    
    Change-Id: Ic3e8c993087d2fbb38e14479a539dc62495ad908

---
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/BaseInit.java
@@ -0,0 +1,274 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm;
+
+import static com.google.gerrit.server.schema.DataSourceProvider.Context.SINGLE_USER;
+import static com.google.inject.Stage.PRODUCTION;
+
+import com.google.gerrit.pgm.init.InitFlags;
+import com.google.gerrit.pgm.init.InitModule;
+import com.google.gerrit.pgm.init.SitePathInitializer;
+import com.google.gerrit.pgm.util.ConsoleUI;
+import com.google.gerrit.pgm.util.Die;
+import com.google.gerrit.pgm.util.SiteProgram;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.config.SitePath;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.schema.SchemaUpdater;
+import com.google.gerrit.server.schema.UpdateUI;
+import com.google.gwtorm.jdbc.JdbcExecutor;
+import com.google.gwtorm.jdbc.JdbcSchema;
+import com.google.gwtorm.server.OrmException;
+import com.google.gwtorm.server.SchemaFactory;
+import com.google.gwtorm.server.StatementExecutor;
+import com.google.inject.AbstractModule;
+import com.google.inject.CreationException;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.spi.Message;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+/** Initialize a new Gerrit installation. */
+public class BaseInit extends SiteProgram {
+
+  public BaseInit() {
+  }
+
+  public BaseInit(File sitePath) {
+    this(sitePath, null);
+  }
+
+  public BaseInit(File sitePath, final Provider<DataSource> dsProvider) {
+    super(sitePath, dsProvider);
+  }
+
+  @Override
+  public int run() throws Exception {
+    beforeInit();
+
+    final SiteInit init = createSiteInit();
+    init.flags.autoStart = getAutoStart();
+
+    final SiteRun run;
+    try {
+      init.initializer.run();
+      init.flags.deleteOnFailure = false;
+
+      run = createSiteRun(init);
+      run.upgradeSchema();
+    } catch (Exception failure) {
+      if (init.flags.deleteOnFailure) {
+        recursiveDelete(getSitePath());
+      }
+      throw failure;
+    } catch (Error failure) {
+      if (init.flags.deleteOnFailure) {
+        recursiveDelete(getSitePath());
+      }
+      throw failure;
+    }
+
+    System.err.println("Initialized " + getSitePath().getCanonicalPath());
+    afterInit(run);
+    return 0;
+  }
+
+  protected void beforeInit() {
+  }
+
+  protected void afterInit(SiteRun run) throws Exception {
+  }
+
+  protected boolean getAutoStart() {
+    return false;
+  }
+
+  static class SiteInit {
+    final SitePaths site;
+    final InitFlags flags;
+    final ConsoleUI ui;
+    final SitePathInitializer initializer;
+
+    @Inject
+    SiteInit(final SitePaths site, final InitFlags flags, final ConsoleUI ui,
+        final SitePathInitializer initializer) {
+      this.site = site;
+      this.flags = flags;
+      this.ui = ui;
+      this.initializer = initializer;
+    }
+  }
+
+  private SiteInit createSiteInit() {
+    final ConsoleUI ui = getConsoleUI();
+    final File sitePath = getSitePath();
+    final List<Module> m = new ArrayList<Module>();
+
+    m.add(new InitModule(dsProvider == null));
+    m.add(new AbstractModule() {
+      @Override
+      protected void configure() {
+        bind(ConsoleUI.class).toInstance(ui);
+        bind(File.class).annotatedWith(SitePath.class).toInstance(sitePath);
+      }
+    });
+
+    try {
+      return Guice.createInjector(PRODUCTION, m).getInstance(SiteInit.class);
+    } catch (CreationException ce) {
+      final Message first = ce.getErrorMessages().iterator().next();
+      Throwable why = first.getCause();
+
+      if (why instanceof Die) {
+        throw (Die) why;
+      }
+
+      final StringBuilder buf = new StringBuilder(ce.getMessage());
+      while (why != null) {
+        buf.append("\n");
+        buf.append(why.getMessage());
+        why = why.getCause();
+        if (why != null) {
+          buf.append("\n  caused by ");
+        }
+      }
+      throw die(buf.toString(), new RuntimeException("InitInjector failed", ce));
+    }
+  }
+
+  protected ConsoleUI getConsoleUI() {
+    return ConsoleUI.getInstance(false);
+  }
+
+  static class SiteRun {
+    final ConsoleUI ui;
+    final SitePaths site;
+    final InitFlags flags;
+    final SchemaUpdater schemaUpdater;
+    final SchemaFactory<ReviewDb> schema;
+    final GitRepositoryManager repositoryManager;
+
+    @Inject
+    SiteRun(final ConsoleUI ui, final SitePaths site, final InitFlags flags,
+        final SchemaUpdater schemaUpdater,
+        final SchemaFactory<ReviewDb> schema,
+        final GitRepositoryManager repositoryManager) {
+      this.ui = ui;
+      this.site = site;
+      this.flags = flags;
+      this.schemaUpdater = schemaUpdater;
+      this.schema = schema;
+      this.repositoryManager = repositoryManager;
+    }
+
+    void upgradeSchema() throws OrmException {
+      final List<String> pruneList = new ArrayList<String>();
+      schemaUpdater.update(new UpdateUI() {
+        @Override
+        public void message(String msg) {
+          System.err.println(msg);
+          System.err.flush();
+        }
+
+        @Override
+        public boolean yesno(boolean def, String msg) {
+          return ui.yesno(def, msg);
+        }
+
+        @Override
+        public boolean isBatch() {
+          return ui.isBatch();
+        }
+
+        @Override
+        public void pruneSchema(StatementExecutor e, List<String> prune) {
+          for (String p : prune) {
+            if (!pruneList.contains(p)) {
+              pruneList.add(p);
+            }
+          }
+        }
+      });
+
+      if (!pruneList.isEmpty()) {
+        StringBuilder msg = new StringBuilder();
+        msg.append("Execute the following SQL to drop unused objects:\n");
+        msg.append("\n");
+        for (String sql : pruneList) {
+          msg.append("  ");
+          msg.append(sql);
+          msg.append(";\n");
+        }
+
+        if (ui.isBatch()) {
+          System.err.print(msg);
+          System.err.flush();
+
+        } else if (ui.yesno(true, "%s\nExecute now", msg)) {
+          final JdbcSchema db = (JdbcSchema) schema.open();
+          try {
+            final JdbcExecutor e = new JdbcExecutor(db);
+            try {
+              for (String sql : pruneList) {
+                e.execute(sql);
+              }
+            } finally {
+              e.close();
+            }
+          } finally {
+            db.close();
+          }
+        }
+      }
+    }
+  }
+
+  private SiteRun createSiteRun(final SiteInit init) {
+    return createSysInjector(init).getInstance(SiteRun.class);
+  }
+
+  private Injector createSysInjector(final SiteInit init) {
+    final List<Module> modules = new ArrayList<Module>();
+    modules.add(new AbstractModule() {
+      @Override
+      protected void configure() {
+        bind(ConsoleUI.class).toInstance(init.ui);
+        bind(InitFlags.class).toInstance(init.flags);
+      }
+    });
+    return createDbInjector(SINGLE_USER).createChildInjector(modules);
+  }
+
+  private static void recursiveDelete(File path) {
+    File[] entries = path.listFiles();
+    if (entries != null) {
+      for (File e : entries) {
+        recursiveDelete(e);
+      }
+    }
+    if (!path.delete() && path.exists()) {
+      System.err.println("warn: Cannot remove " + path);
+    }
+  }
+}
--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/Init.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/Init.java
@@ -14,305 +14,126 @@
 
 package com.google.gerrit.pgm;
 
-import static com.google.gerrit.server.schema.DataSourceProvider.Context.SINGLE_USER;
-import static com.google.inject.Stage.PRODUCTION;
-
+import com.google.common.collect.Lists;
 import com.google.gerrit.common.PageLinks;
 import com.google.gerrit.pgm.init.Browser;
-import com.google.gerrit.pgm.init.InitFlags;
-import com.google.gerrit.pgm.init.InitModule;
-import com.google.gerrit.pgm.init.SitePathInitializer;
 import com.google.gerrit.pgm.util.ConsoleUI;
-import com.google.gerrit.pgm.util.Die;
 import com.google.gerrit.pgm.util.ErrorLogFile;
 import com.google.gerrit.pgm.util.IoUtil;
-import com.google.gerrit.pgm.util.SiteProgram;
-import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.config.GerritServerConfigModule;
 import com.google.gerrit.server.config.SitePath;
-import com.google.gerrit.server.config.SitePaths;
-import com.google.gerrit.server.git.GitRepositoryManager;
-import com.google.gerrit.server.schema.SchemaUpdater;
-import com.google.gerrit.server.schema.UpdateUI;
 import com.google.gerrit.server.util.HostPlatform;
-import com.google.gwtorm.jdbc.JdbcExecutor;
-import com.google.gwtorm.jdbc.JdbcSchema;
-import com.google.gwtorm.server.OrmException;
-import com.google.gwtorm.server.SchemaFactory;
-import com.google.gwtorm.server.StatementExecutor;
 import com.google.inject.AbstractModule;
-import com.google.inject.CreationException;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.google.inject.spi.Message;
+import com.google.inject.Provider;
 
 import org.kohsuke.args4j.Option;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 /** Initialize a new Gerrit installation. */
-public class Init extends SiteProgram {
+public class Init extends BaseInit {
   @Option(name = "--batch", usage = "Batch mode; skip interactive prompting")
   private boolean batchMode;
 
   @Option(name = "--no-auto-start", usage = "Don't automatically start daemon after init")
   private boolean noAutoStart;
 
+  @Inject
+  Browser browser;
+
   public Init() {
   }
 
   public Init(File sitePath) {
-    super(sitePath);
+    this(sitePath, null);
+  }
+
+  public Init(File sitePath, final Provider<DataSource> dsProvider) {
+    super(sitePath, dsProvider);
     batchMode = true;
     noAutoStart = true;
   }
 
   @Override
-  public int run() throws Exception {
+  protected void beforeInit() {
     ErrorLogFile.errorOnlyConsole();
-
-    final SiteInit init = createSiteInit();
-    init.flags.autoStart = !noAutoStart && init.site.isNew;
-
-    final SiteRun run;
-    try {
-      init.initializer.run();
-      init.flags.deleteOnFailure = false;
-
-      run = createSiteRun(init);
-      run.upgradeSchema();
-    } catch (Exception failure) {
-      if (init.flags.deleteOnFailure) {
-        recursiveDelete(getSitePath());
-      }
-      throw failure;
-    } catch (Error failure) {
-      if (init.flags.deleteOnFailure) {
-        recursiveDelete(getSitePath());
-      }
-      throw failure;
-    }
-
-    System.err.println("Initialized " + getSitePath().getCanonicalPath());
-    run.start();
-    return 0;
   }
 
-  static class SiteInit {
-    final SitePaths site;
-    final InitFlags flags;
-    final ConsoleUI ui;
-    final SitePathInitializer initializer;
-
-    @Inject
-    SiteInit(final SitePaths site, final InitFlags flags, final ConsoleUI ui,
-        final SitePathInitializer initializer) {
-      this.site = site;
-      this.flags = flags;
-      this.ui = ui;
-      this.initializer = initializer;
-    }
-  }
-
-  private SiteInit createSiteInit() {
-    final ConsoleUI ui = ConsoleUI.getInstance(batchMode);
-    final File sitePath = getSitePath();
-    final List<Module> m = new ArrayList<Module>();
-
-    m.add(new InitModule());
-    m.add(new AbstractModule() {
-      @Override
-      protected void configure() {
-        bind(ConsoleUI.class).toInstance(ui);
-        bind(File.class).annotatedWith(SitePath.class).toInstance(sitePath);
-      }
-    });
-
-    try {
-      return Guice.createInjector(PRODUCTION, m).getInstance(SiteInit.class);
-    } catch (CreationException ce) {
-      final Message first = ce.getErrorMessages().iterator().next();
-      Throwable why = first.getCause();
-
-      if (why instanceof Die) {
-        throw (Die) why;
-      }
-
-      final StringBuilder buf = new StringBuilder(ce.getMessage());
-      while (why != null) {
-        buf.append("\n");
-        buf.append(why.getMessage());
-        why = why.getCause();
-        if (why != null) {
-          buf.append("\n  caused by ");
-        }
-      }
-      throw die(buf.toString(), new RuntimeException("InitInjector failed", ce));
-    }
-  }
-
-  static class SiteRun {
-    final ConsoleUI ui;
-    final SitePaths site;
-    final InitFlags flags;
-    final SchemaUpdater schemaUpdater;
-    final SchemaFactory<ReviewDb> schema;
-    final GitRepositoryManager repositoryManager;
-    final Browser browser;
-
-    @Inject
-    SiteRun(final ConsoleUI ui, final SitePaths site, final InitFlags flags,
-        final SchemaUpdater schemaUpdater,
-        final SchemaFactory<ReviewDb> schema,
-        final GitRepositoryManager repositoryManager,
-        final Browser browser) {
-      this.ui = ui;
-      this.site = site;
-      this.flags = flags;
-      this.schemaUpdater = schemaUpdater;
-      this.schema = schema;
-      this.repositoryManager = repositoryManager;
-      this.browser = browser;
-    }
-
-    void upgradeSchema() throws OrmException {
-      final List<String> pruneList = new ArrayList<String>();
-      schemaUpdater.update(new UpdateUI() {
-        @Override
-        public void message(String msg) {
-          System.err.println(msg);
-          System.err.flush();
-        }
-
-        @Override
-        public boolean yesno(boolean def, String msg) {
-          return ui.yesno(def, msg);
-        }
-
-        @Override
-        public boolean isBatch() {
-          return ui.isBatch();
-        }
-
-        @Override
-        public void pruneSchema(StatementExecutor e, List<String> prune) {
-          for (String p : prune) {
-            if (!pruneList.contains(p)) {
-              pruneList.add(p);
-            }
-          }
-        }
-      });
-
-      if (!pruneList.isEmpty()) {
-        StringBuilder msg = new StringBuilder();
-        msg.append("Execute the following SQL to drop unused objects:\n");
-        msg.append("\n");
-        for (String sql : pruneList) {
-          msg.append("  ");
-          msg.append(sql);
-          msg.append(";\n");
-        }
-
-        if (ui.isBatch()) {
-          System.err.print(msg);
-          System.err.flush();
-
-        } else if (ui.yesno(true, "%s\nExecute now", msg)) {
-          final JdbcSchema db = (JdbcSchema) schema.open();
-          try {
-            final JdbcExecutor e = new JdbcExecutor(db);
-            try {
-              for (String sql : pruneList) {
-                e.execute(sql);
-              }
-            } finally {
-              e.close();
-            }
-          } finally {
-            db.close();
-          }
-        }
-      }
-    }
-
-    void start() throws Exception {
-      if (flags.autoStart) {
-        if (HostPlatform.isWin32()) {
-          System.err.println("Automatic startup not supported on Win32.");
-
-        } else {
-          startDaemon();
-          if (!ui.isBatch()) {
-            browser.open(PageLinks.ADMIN_PROJECTS);
-          }
-        }
-      }
-    }
-
-    void startDaemon() {
-      final String[] argv = {site.gerrit_sh.getAbsolutePath(), "start"};
-      final Process proc;
-      try {
-        System.err.println("Executing " + argv[0] + " " + argv[1]);
-        proc = Runtime.getRuntime().exec(argv);
-      } catch (IOException e) {
-        System.err.println("error: cannot start Gerrit: " + e.getMessage());
-        return;
-      }
-
-      try {
-        proc.getOutputStream().close();
-      } catch (IOException e) {
-      }
-
-      IoUtil.copyWithThread(proc.getInputStream(), System.err);
-      IoUtil.copyWithThread(proc.getErrorStream(), System.err);
-
-      for (;;) {
-        try {
-          final int rc = proc.waitFor();
-          if (rc != 0) {
-            System.err.println("error: cannot start Gerrit: exit status " + rc);
-          }
-          break;
-        } catch (InterruptedException e) {
-          // retry
-        }
-      }
-    }
-
-  }
-
-  private SiteRun createSiteRun(final SiteInit init) {
-    return createSysInjector(init).getInstance(SiteRun.class);
-  }
-
-  private Injector createSysInjector(final SiteInit init) {
-    final List<Module> modules = new ArrayList<Module>();
+  @Override
+  protected void afterInit(SiteRun run) throws Exception {
+    List<Module> modules = Lists.newArrayList();
     modules.add(new AbstractModule() {
       @Override
       protected void configure() {
-        bind(ConsoleUI.class).toInstance(init.ui);
-        bind(InitFlags.class).toInstance(init.flags);
+        bind(File.class).annotatedWith(SitePath.class).toInstance(getSitePath());
+        bind(Browser.class);
       }
     });
-    return createDbInjector(SINGLE_USER).createChildInjector(modules);
+    modules.add(new GerritServerConfigModule());
+    Guice.createInjector(modules).injectMembers(this);
+    start(run);
   }
 
-  private static void recursiveDelete(File path) {
-    File[] entries = path.listFiles();
-    if (entries != null) {
-      for (File e : entries) {
-        recursiveDelete(e);
+  @Override
+  protected ConsoleUI getConsoleUI() {
+    return ConsoleUI.getInstance(batchMode);
+  }
+
+  @Override
+  protected boolean getAutoStart() {
+    return !noAutoStart;
+  }
+
+  void start(SiteRun run) throws Exception {
+    if (run.flags.autoStart) {
+      if (HostPlatform.isWin32()) {
+        System.err.println("Automatic startup not supported on Win32.");
+
+      } else {
+        startDaemon(run);
+        if (!run.ui.isBatch()) {
+          browser.open(PageLinks.ADMIN_PROJECTS);
+        }
       }
     }
-    if (!path.delete() && path.exists()) {
-      System.err.println("warn: Cannot remove " + path);
+  }
+
+  void startDaemon(SiteRun run) {
+    final String[] argv = {run.site.gerrit_sh.getAbsolutePath(), "start"};
+    final Process proc;
+    try {
+      System.err.println("Executing " + argv[0] + " " + argv[1]);
+      proc = Runtime.getRuntime().exec(argv);
+    } catch (IOException e) {
+      System.err.println("error: cannot start Gerrit: " + e.getMessage());
+      return;
+    }
+
+    try {
+      proc.getOutputStream().close();
+    } catch (IOException e) {
+    }
+
+    IoUtil.copyWithThread(proc.getInputStream(), System.err);
+    IoUtil.copyWithThread(proc.getErrorStream(), System.err);
+
+    for (;;) {
+      try {
+        final int rc = proc.waitFor();
+        if (rc != 0) {
+          System.err.println("error: cannot start Gerrit: exit status " + rc);
+        }
+        break;
+      } catch (InterruptedException e) {
+        // retry
+      }
     }
   }
 }

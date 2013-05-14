990f75d3fac4186789497930e32e4c094aecf657
David Ostrovsky <david@ostrovsky.org>
2013-05-14 10:34:40 +0200

    Suppress alert windows if UiCommand returns empty message
    Change-Id: I007def156135741d1380660e74f3b838120beb44

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/PatchSetComplexDisclosurePanel.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/PatchSetComplexDisclosurePanel.java
@@ -567,7 +567,7 @@ class PatchSetComplexDisclosurePanel extends ComplexDisclosurePanel
                 @Override
                 public void onSuccess(NativeString msg) {
                   b.setEnabled(true);
-                  if (msg != null) {
+                  if (!msg.toString().isEmpty()) {
                     Window.alert(msg.asString());
                   }
                 }

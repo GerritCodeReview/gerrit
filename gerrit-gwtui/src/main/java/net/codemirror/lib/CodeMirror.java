36a32e31f3a806a496666fdedf424bd025ab362f
Michael Zhou <moz@google.com>
2013-05-23 11:47:17 -0700

    Starting to implement side-by-side diff.
    Including the mark-selection addon from CodeMirror.
    Adding more API wrappers to CodeMirror to support line formatting.
    Adding line coloring and padding for inserts and deletes to CodeMirrorDemo.
    
    Change-Id: I0a96506166de8e9dc32241b21d99cce9a3890fad

--- gerrit-gwtui/src/main/java/net/codemirror/lib/CodeMirror.java
+++ gerrit-gwtui/src/main/java/net/codemirror/lib/CodeMirror.java
@@ -28,7 +28,8 @@ public class CodeMirror extends JavaScriptObject {
     Loader.initLibrary(cb);
   }
 
-  public static native CodeMirror create(Element parent, Configuration cfg) /*-{
+  public static native CodeMirror create(Element parent,
+      Configuration cfg) /*-{
     return $wnd.CodeMirror(parent, cfg);
   }-*/;
 
@@ -37,10 +38,38 @@ public class CodeMirror extends JavaScriptObject {
   public final native void setWidth(int w) /*-{ this.setSize(w, null); }-*/;
   public final native void setWidth(String w) /*-{ this.setSize(w, null); }-*/;
   public final native void setHeight(int h) /*-{ this.setSize(null, h); }-*/;
-  public final native void setHeight(String h) /*-{ this.setSize(null, h); }-*/;
+  public final native void setHeight(String h)/*-{
+    this.setSize(null, h);
+  }-*/;
 
   public final native void refresh() /*-{ this.refresh(); }-*/;
-  public final native Element getWrapperElement() /*-{ return this.getWrapperElement(); }-*/;
+  public final native Element getWrapperElement() /*-{
+    return this.getWrapperElement();
+  }-*/;
+
+  public final native void markText(LineCharacter from, LineCharacter to,
+      Configuration options) /*-{
+    this.markText(from, to, options);
+  }-*/;
+
+  public enum LineClassWhere {
+    TEXT, BACKGROUND, WRAP;
+  }
+
+  public final void addLineClass(int line, LineClassWhere where,
+      String className) {
+    addLineClassNative(line, where.name().toLowerCase(), className);
+  }
+
+  public final native void addLineClassNative(int line, String where,
+      String lineClass) /*-{
+    this.addLineClass(line, where, lineClass);
+  }-*/;
+
+  public final native void addLineWidget(int line, Element node,
+      Configuration options) /*-{
+    this.addLineWidget(line, node, options);
+  }-*/;
 
   protected CodeMirror() {
   }
--- gerrit-gwtui/src/main/java/net/codemirror/lib/Configuration.java
+++ gerrit-gwtui/src/main/java/net/codemirror/lib/Configuration.java
@@ -36,6 +36,9 @@ public class Configuration extends JavaScriptObject {
   public final native Configuration set(String name, boolean val)
   /*-{ this[name] = val; return this; }-*/;
 
+  public final native Configuration setInfinity(String name)
+  /*-{ this[name] = Infinity; return this; }-*/;
+
   protected Configuration() {
   }
 }

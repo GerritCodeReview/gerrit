5c89c9565faab2b0c9b2b40eac51b787bc2a961b
David Ostrovsky <david@ostrovsky.org>
2013-05-23 02:08:09 +0200

    BUCK: deploy plugin api to local and remote maven repos
    Make sources for {extension,plugin}-api targets too and install/deploy it.
    
    BUCK is currently lacking the native support for source jar generation, i. e.
    parameter 'gensrc=True' for java_library() or something like this. Generate
    sources manually for now and replace it once BUCK supports it.
    
    Note: only Gerrit's own sources are included in plugin-api-VERSION-src.jar.
    
    `buck build api_deploy` uploads the artifacts to the remote maven repository.
    The type of the repo is induced from the Gerrit version name, i. e.
    * 2.8-SNAPSHOT: snapshot repo
    * 2.8: release repo
    
    Change-Id: I063834bb39311a4d92f5fdbb183f70b1fb8418b6

---
+++ tools/api_util.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+from optparse import OptionParser
+from os.path import exists
+from sys import stderr
+from gerrit.util import check_output
+
+opts = OptionParser()
+opts.add_option('-a', help='action (valid actions are: install,deploy)')
+opts.add_option('-v', help='gerrit version')
+opts.add_option('-g', help='gen directory')
+
+args, ctx = opts.parse_args()
+action = args.a
+if action not in ['deploy', 'install']:
+  print("unknown action : %s" % action, file=stderr)
+  exit(1)
+
+gendir = args.g
+if not exists(gendir):
+  print("gen directory doesn't exist: %s" % gendir, file=stderr)
+  exit(1)
+
+version = args.v
+if not version:
+  print('version is empty')
+  exit(1)
+
+REPO_TYPE = 'snapshot' if version.endswith("SNAPSHOT") else 'release'
+URL = 's3://gerrit-api@commondatastorage.googleapis.com/%s' % REPO_TYPE
+
+cmd = {
+  'deploy': ['mvn',
+             'deploy:deploy-file',
+             '-DrepositoryId=gerrit-api-repository',
+             '-Durl=%s' % URL],
+  'install': ['mvn',
+              'install:install-file'],
+  'plugin': ['-DartifactId=gerrit-plugin-api'],
+  'extension': ['-DartifactId=gerrit-extension-api'],
+  'common': ['-DgroupId=com.google.gerrit',
+             '-Dversion=%s' % version],
+  'jar': ['-Dpackaging=jar'],
+  'src': ['-Dpackaging=java-source'],
+  }
+
+try:
+  check_output(cmd[action] +
+               cmd['plugin'] +
+               cmd['common'] +
+               cmd['jar'] +
+               ['-Dfile=%s/plugin-api-%s.jar' % (gendir, version)])
+  check_output(cmd[action] +
+               cmd['plugin'] +
+               cmd['common'] +
+               cmd['src'] +
+               ['-Dfile=%s/plugin-api-%s-src.jar' % (gendir, version)])
+  check_output(cmd[action] +
+               cmd['extension'] +
+               cmd['common'] +
+               cmd['jar'] +
+               ['-Dfile=%s/extension-api-%s.jar' % (gendir, version)])
+  check_output(cmd[action] +
+               cmd['extension'] +
+               cmd['common'] +
+               cmd['src'] +
+               ['-Dfile=%s/extension-api-%s-src.jar' % (gendir, version)])
+except Exception as e:
+  print('%s command failed: %s' % (action, e), file=stderr)
+  exit(1)

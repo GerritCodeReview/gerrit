e43697176cfa7cb71884f5d95989e8e170234db7
David Ostrovsky <david@ostrovsky.org>
2013-05-25 19:20:34 +0200

    Use custom style to mark patch sets with edits
    Change-Id: Icee1565d5fe76274aaf67d91d7e844229d31dcbf

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/PatchSetComplexDisclosurePanel.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/PatchSetComplexDisclosurePanel.java
@@ -132,9 +132,8 @@ class PatchSetComplexDisclosurePanel extends ComplexDisclosurePanel
       addOpenHandler(this);
     }
 
-    // TODO(davido): Gerrit.RESOURCES.css().patchSetWithDraft() removed on HEAD
     if (ps.isEdit()) {
-      addStyleName(Gerrit.RESOURCES.css().patchSetWithDraft());
+      addStyleName(Gerrit.RESOURCES.css().patchSetWithEdit());
     }
   }
 

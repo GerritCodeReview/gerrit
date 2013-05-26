0bc378686ccf5a8de7ab26720bf0d1e2443ec02f
David Ostrovsky <david@ostrovsky.org>
2013-05-27 00:48:14 +0200

    Add REST API Endpoint to retrieve file's content type
    All CodeMirror is needed to do it's magic is a content type.
    Provide a new REST API Endpoint to retrieve it.
    
    Note: we may want to combine the both REST API Endpoints into one:
    * content => base64 encoded file content
    * content_type => text/x-java-source
    
    If we not combine the requests, then we should consider to cache the content
    type, to prevent it from be loaded multiple times for the same file.
    
    Change-Id: I1b3b1869dfb09966b14058f117ed0b7226a887ad

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/GetContentType.java
@@ -0,0 +1,112 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.common.base.Strings;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.FileTypeRegistry;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.RevisionEdit;
+import com.google.gerrit.server.patch.Text;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import eu.medsea.mimeutil.MimeType;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+import java.io.IOException;
+
+public class GetContentType implements RestReadView<FileResource> {
+  private final GitRepositoryManager repoManager;
+  private final Provider<CurrentUser> user;
+  private final FileTypeRegistry registry;
+
+  @Inject
+  GetContentType(GitRepositoryManager repoManager, Provider<CurrentUser> user,
+      final FileTypeRegistry ftr) {
+    this.repoManager = repoManager;
+    this.user = user;
+    this.registry = ftr;
+  }
+
+  @Override
+  public String apply(FileResource rsrc)
+      throws ResourceNotFoundException, IOException, AuthException {
+    rsrc.getRevision().checkPublished();
+    Project.NameKey project =
+        rsrc.getRevision().getControl().getProject().getNameKey();
+    Repository repo = repoManager.openRepository(project);
+    try {
+      RevWalk rw = new RevWalk(repo);
+      try {
+        RevCommit commit = getCommit(rsrc, repo, rw);
+        TreeWalk tw =
+            TreeWalk.forPath(rw.getObjectReader(), rsrc.getPatchKey().get(),
+                commit.getTree().getId());
+        if (tw == null) {
+          throw new ResourceNotFoundException();
+        }
+        try {
+          byte[] srcContent = Text.asByteArray(repo.open(tw.getObjectId(0)));
+          MimeType mimeType = registry.getMimeType(rsrc.getPatchKey().getFileName(), srcContent);
+          // It is never null (if unknown, then application/octet-stream)
+          return mimeType.toString();
+        } finally {
+          tw.release();
+        }
+      } finally {
+        rw.release();
+      }
+    } finally {
+      repo.close();
+    }
+  }
+
+  private RevCommit getCommit(FileResource rsrc, Repository repo, RevWalk rw)
+      throws IOException, AuthException {
+    if (rsrc.getRevision().getId().isEdit()) {
+      RevCommit c = new RevisionEdit(checkIdentifiedUser(),
+          rsrc.getRevision().getId()).get(repo, rw);
+      if (c != null) {
+        return c;
+      }
+    }
+    return getPublishedCommit(rsrc, rw);
+  }
+
+  private IdentifiedUser checkIdentifiedUser() throws AuthException {
+    CurrentUser u = user.get();
+    if (!(u instanceof IdentifiedUser)) {
+      throw new AuthException("edits only available to authenticated users");
+    }
+    return (IdentifiedUser) u;
+  }
+
+  private RevCommit getPublishedCommit(FileResource rsrc, RevWalk rw)
+      throws IOException {
+    return rw.parseCommit(ObjectId.fromString(rsrc.getRevision().getPatchSet()
+        .getRevision().get()));
+  }
+}

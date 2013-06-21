60b20d026f91272e6d4e347cada46c1001688cc7
Michael Zhou <moz@google.com>
2013-06-21 15:50:55 -0700

    Refactor CommentBox to start supporting both published comments and drafts.
    Change-Id: Ibab8b198499d9f7920ef914b3c2ed88e5daa60d4

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/CommentBoxResources.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.diff;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+/**
+ * Resources used by diff.
+ */
+interface CommentBoxResources extends ClientBundle {
+  @Source("CommentBoxUi.css")
+  Style style();
+
+  interface Style extends CssResource {
+    String open();
+    String close();
+    String commentBox();
+    String table();
+    String summary();
+    String summaryText();
+    String date();
+    String contentPanel();
+    String message();
+  }
+}

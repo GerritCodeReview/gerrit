3540d284ae1cbe11f6165d55a126d849fecdc9c7
Martin Fick <mogulguy10@gmail.com>
2013-06-28 15:29:05 -0600

    Add ability to prepend/append stuff to query_tester inputs
    Add support for prepend and append files to the query_tester
    so that it is easy to add things such as "status:open" to
    a full suite of queries.  This is helpful to compare pre
    lucene results to lucene results since the default behavior
    of some queries has changed.
    
    Change-Id: I2f3b1dfc5de06d93ed3facf8c098ef758e66c463

--- tools/util/query_tester.sh
+++ tools/util/query_tester.sh
@@ -14,6 +14,11 @@ usage() {
   The query outputs for each suite will be stored in files named
   after their suites and input file names under the output_dir.
 
+  Special files:
+
+  append_all    Append this text to all queries in the test suite
+  prepend_all   Prepend this text to all queries in the test suite
+
 EOF
     exit
 }
@@ -26,7 +31,8 @@ query()  { # host query
 }
 
 run_suite() { # host idir odir suite operator
-    local host=$1 idir=$2 odir=$3 suite=$4 operator=$5 qfile files
+    local host=$1 idir=$2 odir=$3 suite=$4 operator=$5
+    local qfile files pre='' post='' fdir=$idir
 
     mkdir -p "$odir/$suite"
 
@@ -35,17 +41,23 @@ run_suite() { # host idir odir suite operator
     if [ -z "$suite" ] ; then
         files=($(cd "$idir" ; echo *))
     else
+        fdir=$idir/$suite
         files=($(cd "$idir" ; echo $suite/*))
     fi
 
+    [ -f "$fdir/prepend_all" ] && pre=$(< "$fdir/prepend_all")
+    [ -f "$fdir/append_all" ] && post=$(< "$fdir/append_all")
+
     for qfile in "${files[@]}" ; do
+        [ "$(basename "$qfile")" = "prepend_all" ] && continue
+        [ "$(basename "$qfile")" = "append_all" ] && continue
         if [ -d "$idir/$qfile" ] ; then
             run_suite "$host" "$idir" "$odir" "$qfile" "$operator"
         else
             if [ -n "$operator" ] ; then
-                query "$host" "$operator:{$(< "$idir/$qfile")}" > "$odir/$qfile"
+                query "$host" "$pre $operator:{$(< "$idir/$qfile")} $post" > "$odir/$qfile"
             else
-                query "$host" "$(< "$idir/$qfile")" > "$odir/$qfile"
+                query "$host" "$pre $(< "$idir/$qfile") $post" > "$odir/$qfile"
             fi
         fi
     done

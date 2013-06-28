40e596800e7b949e41164ad4450449a0512cb2dd
David Pursehouse <david.pursehouse@sonymobile.com>
2013-06-28 10:56:14 +0900

    Remove unused import of PrintWriter in Reindex
    Change-Id: If50ad22a238f7c98b01109c7683adffc5feb0a7a

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/Reindex.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/Reindex.java
@@ -82,7 +82,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;

2ecc85265a48500c357bd9d95a4bf450750e7f4c
Edwin Kempin <edwin.kempin@sap.com>
2013-07-04 09:47:13 +0200

    For the 'conflicts' operator return only changes that actually conflict
    So far the 'conflicts:<change>' query operator returns all changes that
    touch at least one file that was also modified by the specified change.
    This means all changes that potentially conflict with the specifed
    change are returned.
    
    Now we check the mergeability for the changes and only return those
    changes that actually conflict. This means now the 'Automatically
    resolve conflicts' project setting is taken into account and if the
    content merge succeeds a change is not returned.
    
    Change-Id: I1d6a3c431ed679f786e589bbed3aed52fe439895
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/ConflictsPredicate.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/ConflictsPredicate.java
@@ -15,30 +15,68 @@
 package com.google.gerrit.server.query.change;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.gerrit.common.data.SubmitTypeRecord;
 import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.Project.SubmitType;
 import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.git.CodeReviewCommit;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.MergeException;
+import com.google.gerrit.server.git.SubmitStrategy;
+import com.google.gerrit.server.git.SubmitStrategyFactory;
 import com.google.gerrit.server.patch.PatchListCache;
+import com.google.gerrit.server.project.ChangeControl;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.gerrit.server.project.NoSuchProjectException;
+import com.google.gerrit.server.query.OperatorPredicate;
 import com.google.gerrit.server.query.OrPredicate;
 import com.google.gerrit.server.query.Predicate;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Provider;
 
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevFlag;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class ConflictsPredicate extends OrPredicate<ChangeData> {
   private final String value;
 
-  ConflictsPredicate(Provider<ReviewDb> db, PatchListCache plc, String value,
-      List<Change> changes) throws OrmException {
-    super(predicates(db, plc, changes));
+  ConflictsPredicate(Provider<ReviewDb> db, PatchListCache plc,
+      SubmitStrategyFactory submitStrategyFactory,
+      ChangeControl.GenericFactory changeControlFactory,
+      IdentifiedUser.GenericFactory identifiedUserFactory,
+      GitRepositoryManager repoManager, String value, List<Change> changes)
+      throws OrmException {
+    super(predicates(db, plc, submitStrategyFactory, changeControlFactory,
+        identifiedUserFactory, repoManager, value, changes));
     this.value = value;
   }
 
-  private static List<Predicate<ChangeData>> predicates(Provider<ReviewDb> db,
-      PatchListCache plc, List<Change> changes) throws OrmException {
+  private static List<Predicate<ChangeData>> predicates(
+      final Provider<ReviewDb> db, final PatchListCache plc,
+      final SubmitStrategyFactory submitStrategyFactory,
+      final ChangeControl.GenericFactory changeControlFactory,
+      final IdentifiedUser.GenericFactory identifiedUserFactory,
+      final GitRepositoryManager repoManager, final String value,
+      List<Change> changes)
+      throws OrmException {
+    final Cache cache = new Cache();
     List<Predicate<ChangeData>> changePredicates =
         Lists.newArrayListWithCapacity(changes.size());
-    for (Change c : changes) {
+    for (final Change c : changes) {
       List<String> files = new ChangeData(c).currentFilePaths(db, plc);
       List<Predicate<ChangeData>> filePredicates =
           Lists.newArrayListWithCapacity(files.size());
@@ -47,11 +85,109 @@ class ConflictsPredicate extends OrPredicate<ChangeData> {
       }
 
       List<Predicate<ChangeData>> predicatesForOneChange =
-          Lists.newArrayListWithCapacity(2);
+          Lists.newArrayListWithCapacity(3);
       predicatesForOneChange.add(
           not(new LegacyChangeIdPredicate(db, c.getId())));
       predicatesForOneChange.add(or(filePredicates));
+      predicatesForOneChange.add(new OperatorPredicate<ChangeData>(
+          ChangeQueryBuilder.FIELD_CONFLICTS, value) {
 
+        @Override
+        public boolean match(ChangeData object) throws OrmException {
+          Change otherChange = object.change(db);
+          if (otherChange == null) {
+            return false;
+          }
+          if (!otherChange.getDest().equals(c.getDest())) {
+            return false;
+          }
+          SubmitType submitType = getSubmitType(otherChange, object);
+          if (submitType == null) {
+            return false;
+          }
+          try {
+            Repository repo =
+                repoManager.openRepository(otherChange.getProject());
+            try {
+              RevWalk rw = new RevWalk(repo) {
+                @Override
+                protected RevCommit createCommit(final AnyObjectId id) {
+                  return new CodeReviewCommit(id);
+                }
+              };
+              try {
+                RevFlag canMergeFlag = rw.newFlag("CAN_MERGE");
+                CodeReviewCommit commit =
+                    (CodeReviewCommit) rw.parseCommit(cache.getTestAgainst(db, c));
+                SubmitStrategy strategy =
+                    submitStrategyFactory.create(submitType,
+                        db.get(), repo, rw, null, canMergeFlag,
+                        getAlreadyAccepted(repo, rw, commit),
+                        otherChange.getDest());
+                CodeReviewCommit otherCommit =
+                    (CodeReviewCommit) rw.parseCommit(ObjectId.fromString(
+                        object.currentPatchSet(db).getRevision().get()));
+                otherCommit.add(canMergeFlag);
+                return !strategy.dryRun(commit, otherCommit);
+              } catch (MergeException e) {
+                throw new IllegalStateException(e);
+              } catch (NoSuchProjectException e) {
+                throw new IllegalStateException(e);
+              } finally {
+                rw.release();
+              }
+            } finally {
+              repo.close();
+            }
+          } catch (IOException e) {
+            throw new IllegalStateException(e);
+          }
+        }
+
+        @Override
+        public int getCost() {
+          return 5;
+        }
+
+        private SubmitType getSubmitType(Change change, ChangeData cd) throws OrmException {
+          try {
+            final SubmitTypeRecord r =
+                changeControlFactory.controlFor(change,
+                    identifiedUserFactory.create(change.getOwner()))
+                    .getSubmitTypeRecord(db.get(), cd.currentPatchSet(db), cd);
+            if (r.status != SubmitTypeRecord.Status.OK) {
+              return null;
+            }
+            return r.type;
+          } catch (NoSuchChangeException e) {
+            return null;
+          }
+        }
+
+        private Set<RevCommit> getAlreadyAccepted(Repository repo, RevWalk rw,
+            CodeReviewCommit tip) throws MergeException {
+          final Set<RevCommit> alreadyAccepted = new HashSet<RevCommit>();
+
+          if (tip != null) {
+            alreadyAccepted.add(tip);
+          }
+
+          try {
+            for (ObjectId id : cache.getAlreadyAccepted(repo)) {
+              try {
+                alreadyAccepted.add(rw.parseCommit(id));
+              } catch (IncorrectObjectTypeException iote) {
+                // Not a commit? Skip over it.
+              }
+            }
+          } catch (IOException e) {
+            throw new MergeException(
+                "Failed to determine already accepted commits.", e);
+          }
+
+          return alreadyAccepted;
+        }
+      });
       changePredicates.add(and(predicatesForOneChange));
     }
     return changePredicates;
@@ -60,5 +196,32 @@ class ConflictsPredicate extends OrPredicate<ChangeData> {
   @Override
   public String toString() {
     return ChangeQueryBuilder.FIELD_CONFLICTS + ":" + value;
+  }
+
+  private static class Cache {
+    private ObjectId testAgainst;
+    private Set<ObjectId> alreadyAccepted;
+
+    ObjectId getTestAgainst(Provider<ReviewDb> db, Change c)
+        throws OrmException {
+      if (testAgainst == null) {
+        testAgainst = ObjectId.fromString(
+          new ChangeData(c).currentPatchSet(db).getRevision().get());
+      }
+      return testAgainst;
+    }
+
+    public Set<ObjectId> getAlreadyAccepted(Repository repo) {
+      if (alreadyAccepted == null) {
+        alreadyAccepted = Sets.newHashSet();
+        for (final Ref r : repo.getAllRefs().values()) {
+          if (r.getName().startsWith(Constants.R_HEADS)
+              || r.getName().startsWith(Constants.R_TAGS)) {
+            alreadyAccepted.add(r.getObjectId());
+          }
+        }
+      }
+      return alreadyAccepted;
+    }
   }
 }

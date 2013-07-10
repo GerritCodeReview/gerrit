0dd0c3273bf25a92d37ddbabc7e858698d490eea
David Pursehouse <david.pursehouse@sonymobile.com>
2013-07-10 11:37:06 +0900

    Refactor plugin doc index page generation to remove duplicate code
    The code to generate the documentation and command lists on plugins'
    documentation page is duplicated in 2 blocks.
    
    Refactor it into a method that is reusable to generate both lists.
    
    Change-Id: Ibd86e73fc813d80c7267efc6d9a04261800bc11a

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/plugins/HttpPluginServlet.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/plugins/HttpPluginServlet.java
@@ -300,6 +300,31 @@ class HttpPluginServlet extends HttpServlet
     }
   }
 
+  private void appendEntriesSection(JarFile jar, List<JarEntry> entries,
+      String sectionTitle, StringBuilder md, String prefix,
+      int nameOffset) throws IOException {
+    if (!entries.isEmpty()) {
+      md.append("## " + sectionTitle +  " ##\n");
+      for(JarEntry entry : entries) {
+        String rsrc = entry.getName().substring(prefix.length());
+        String entryTitle;
+        if (rsrc.endsWith(".html")) {
+          entryTitle = rsrc.substring(nameOffset, rsrc.length() - 5).replace('-', ' ');
+        } else if (rsrc.endsWith(".md")) {
+          entryTitle = extractTitleFromMarkdown(jar, entry);
+          if (Strings.isNullOrEmpty(entryTitle)) {
+            entryTitle = rsrc.substring(nameOffset, rsrc.length() - 3).replace('-', ' ');
+          }
+          rsrc = rsrc.substring(0, rsrc.length() - 3) + ".html";
+        } else {
+          entryTitle = rsrc.substring(nameOffset).replace('-', ' ');
+        }
+        md.append(String.format("* [%s](%s)\n", entryTitle, rsrc));
+      }
+      md.append("\n");
+    }
+  }
+
   private void sendAutoIndex(JarFile jar,
       String prefix, String pluginName,
       ResourceKey cacheKey, HttpServletResponse res) throws IOException {
@@ -339,47 +364,8 @@ class HttpPluginServlet extends HttpServlet
     md.append("\n");
     appendPluginInfoTable(md, jar.getManifest().getMainAttributes());
 
-    if (!docs.isEmpty()) {
-      md.append("## Documentation ##\n");
-      for(JarEntry entry : docs) {
-        String rsrc = entry.getName().substring(prefix.length());
-        String title;
-        if (rsrc.endsWith(".html")) {
-          title = rsrc.substring(0, rsrc.length() - 5).replace('-', ' ');
-        } else if (rsrc.endsWith(".md")) {
-          title = extractTitleFromMarkdown(jar, entry);
-          if (Strings.isNullOrEmpty(title)) {
-            title = rsrc.substring(0, rsrc.length() - 3).replace('-', ' ');
-          }
-          rsrc = rsrc.substring(0, rsrc.length() - 3) + ".html";
-        } else {
-          title = rsrc.replace('-', ' ');
-        }
-        md.append(String.format("* [%s](%s)\n", title, rsrc));
-      }
-      md.append("\n");
-    }
-
-    if (!cmds.isEmpty()) {
-      md.append("## Commands ##\n");
-      for(JarEntry entry : cmds) {
-        String rsrc = entry.getName().substring(prefix.length());
-        String title;
-        if (rsrc.endsWith(".html")) {
-          title = rsrc.substring(4, rsrc.length() - 5).replace('-', ' ');
-        } else if (rsrc.endsWith(".md")) {
-          title = extractTitleFromMarkdown(jar, entry);
-          if (Strings.isNullOrEmpty(title)) {
-            title = rsrc.substring(4, rsrc.length() - 3).replace('-', ' ');
-          }
-          rsrc = rsrc.substring(0, rsrc.length() - 3) + ".html";
-        } else {
-          title = rsrc.substring(4).replace('-', ' ');
-        }
-        md.append(String.format("* [%s](%s)\n", title, rsrc));
-      }
-      md.append("\n");
-    }
+    appendEntriesSection(jar, docs, "Documentation", md, prefix, 0);
+    appendEntriesSection(jar, cmds, "Commands", md, prefix, "cmd-".length());
 
     sendMarkdownAsHtml(md.toString(), pluginName, cacheKey, res);
   }

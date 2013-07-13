c4b3ae08dba02976af049e3ad19a4aff7b38a880
Shawn Pearce <sop@google.com>
2013-07-13 14:45:20 -0700

    Redesign change screen on new REST API
    The new screen exists at #/c2/<id> as it is missing a large number of
    features.  The proposal is to include the screen as-is and iterate in
    tree, similar to the CodeMirror work.  This allows power users to
    start trying the screen out by editing the URL.
    
    Missing features:
    - Diff files in two patch sets
    - Edit button on topic
    - Hyperlink to gitweb UI
    - Download links by URL and action (cherry-pick, checkout, etc.)
    - Dependencies and dependents
    
    Change-Id: Ie957fb85c873d044c947000f0f0207a66f87c784

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ActionButton.java
@@ -0,0 +1,100 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.change;
+
+import com.google.gerrit.client.ConfirmationCallback;
+import com.google.gerrit.client.ConfirmationDialog;
+import com.google.gerrit.client.ErrorDialog;
+import com.google.gerrit.client.changes.ChangeApi;
+import com.google.gerrit.client.changes.ChangeInfo.ActionInfo;
+import com.google.gerrit.client.rpc.NativeString;
+import com.google.gerrit.client.rpc.RestApi;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwtexpui.safehtml.client.SafeHtmlBuilder;
+
+class ActionButton extends Button implements ClickHandler {
+  private final Change.Id changeId;
+  private final String revision;
+  private final ActionInfo action;
+
+  ActionButton(Change.Id changeId, String revision, ActionInfo action) {
+    super(new SafeHtmlBuilder()
+      .openDiv()
+      .append(action.label())
+      .closeDiv());
+    setStyleName("");
+    setTitle(action.title());
+    setEnabled(action.enabled());
+    addClickHandler(this);
+
+    this.changeId = changeId;
+    this.revision = revision;
+    this.action = action;
+  }
+
+  @Override
+  public void onClick(ClickEvent event) {
+    if (action.confirmation_message() != null
+        && !action.confirmation_message().isEmpty()) {
+      new ConfirmationDialog(
+        action.title(),
+        new SafeHtmlBuilder().append(action.confirmation_message()),
+        new ConfirmationCallback() {
+          @Override
+          public void onOk() {
+            send();
+          }
+        }).center();
+    } else {
+      send();
+    }
+  }
+
+  private void send() {
+    setEnabled(false);
+
+    AsyncCallback<NativeString> cb = new AsyncCallback<NativeString>() {
+      @Override
+      public void onFailure(Throwable caught) {
+        setEnabled(true);
+        new ErrorDialog(caught).center();
+      }
+
+      @Override
+      public void onSuccess(NativeString msg) {
+        setEnabled(true);
+        if (msg != null && !msg.asString().isEmpty()) {
+          // TODO Support better UI on UiCommand results.
+          Window.alert(msg.asString());
+        }
+      }
+    };
+
+    RestApi api = ChangeApi.revision(changeId.get(), revision).view(action.id());
+    if ("PUT".equalsIgnoreCase(action.method())) {
+      api.put(JavaScriptObject.createObject(), cb);
+    } else if ("DELETE".equalsIgnoreCase(action.method())) {
+      api.delete(cb);
+    } else {
+      api.post(JavaScriptObject.createObject(), cb);
+    }
+  }
+}

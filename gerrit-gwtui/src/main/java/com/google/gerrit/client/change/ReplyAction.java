fc521e3685898359bbafbf5448aad7a9eb2ee1e5
Shawn Pearce <sop@google.com>
2013-07-18 11:38:07 -0700

    Fix missing copyright statement in ChangeScreen2 code
    This new file was refactored from elsewhere and I missed adding the
    copyright header. Shame on me. Its still only my code so I hereby
    assert its Apache License 2.0. :-)
    
    Change-Id: Ife4918fb32d62369e1c74c8260652be764428f01

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ReplyAction.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ReplyAction.java
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 package com.google.gerrit.client.change;
 
 import com.google.gerrit.client.changes.ChangeInfo;

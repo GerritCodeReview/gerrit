faacb29b3fd019dcb438a482c14ce43105569379
Dariusz Luksza <dariusz@luksza.org>
2013-07-18 14:31:17 +0200

    Add SecureStore extension point
    With this extension point plugins can contribute encryption of sensitive
    parts of gerrit.config like eg. database password.
    
    To provide such extension plugin author must implement
    com.google.gerrit.common.secure.SecureStore interface and mark it with
    @Export annotation with given name.
    
    Then during startup process Gerrit will look for gerrit.secureStore
    property in gerrit.config and based on its value will select proper
    implementation. When gerrit.secureStore is not defined then the default
    implementation will be used. If no implementation with given name will
    be found Gerrit will not starts up. Default SecureStore implementation
    will just store plain text values in secure.config file.
    
    To reduce overhead of jar scanning and prevent name collisions,
    gerrit.secureStore property apart from SecureStore implementation name,
    also contains name of plugin that contains this class file. Proper
    syntax of this parameter is $pluginName/$secureStoreImplName
    
    Also init step was enhanced to support installing Gerrit in secure way.
    To include SecureStore plugin during init step, the
    --with-secure-store-plugin <PLUGIN_JAR_ABSOLUTE_PATH> parameter must be
    provided. This will also install that plugin.
    
    Example implementation can be found in Cookbook-plugin:
    
    https://gerrit-review.googlesource.com/53940
    
    Change-Id: I02fb1437179d004c675e6dd822bab51eae60a178
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitHttpd.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitHttpd.java
@@ -20,8 +20,10 @@ import static com.google.gerrit.pgm.init.InitUtil.domainOf;
 import static com.google.gerrit.pgm.init.InitUtil.isAnyAddress;
 import static com.google.gerrit.pgm.init.InitUtil.toURI;
 
+import com.google.gerrit.extensions.registration.DynamicItem;
 import com.google.gerrit.pgm.util.ConsoleUI;
 import com.google.gerrit.server.config.SitePaths;
+import com.google.gerrit.server.securestore.SecureStore;
 import com.google.gwtjsonrpc.server.SignedToken;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -36,18 +38,19 @@ import java.net.URISyntaxException;
 class InitHttpd implements InitStep {
   private final ConsoleUI ui;
   private final SitePaths site;
-  private final InitFlags flags;
   private final Section httpd;
   private final Section gerrit;
+  private final DynamicItem<SecureStore> secureStoreItem;
 
   @Inject
-  InitHttpd(final ConsoleUI ui, final SitePaths site, final InitFlags flags,
-      final Section.Factory sections) {
+  InitHttpd(final ConsoleUI ui, final SitePaths site,
+      final Section.Factory sections,
+      final DynamicItem<SecureStore> secureStoreItem) {
     this.ui = ui;
     this.site = site;
-    this.flags = flags;
     this.httpd = sections.get("httpd", null);
     this.gerrit = sections.get("gerrit", null);
+    this.secureStoreItem = secureStoreItem;
   }
 
   public void run() throws IOException, InterruptedException {
@@ -150,10 +153,11 @@ class InitHttpd implements InitStep {
       return;
     }
 
-    String ssl_pass = flags.sec.getString("http", null, "sslKeyPassword");
+    SecureStore secureStore = secureStoreItem.get();
+    String ssl_pass = secureStore.get("http", null, "sslKeyPassword");
     if (ssl_pass == null || ssl_pass.isEmpty()) {
       ssl_pass = SignedToken.generateRandomKey();
-      flags.sec.setString("httpd", null, "sslKeyPassword", ssl_pass);
+      secureStore.set("httpd", null, "sslKeyPassword", ssl_pass);
     }
 
     hostname = ui.readString(hostname, "Certificate server name");

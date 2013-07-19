4e19ee6a0ced62fc5bdfe85301cb3e9aef438bcd
Michael Zhou <moz@google.com>
2013-07-19 10:49:33 -0700

    SideBySide2: Add file comment functionality
    Added functionality to display, add, edit and hide file comments.
    Chose to add a separate table on top of the two CMs, because messing
    around with CM linewidgets and adjusting the padding is not fun,
    particularly when it happens on the somewhat quirky "first line".
    For simplicity, FileCommentPanel is table-based, and the padding will
    look like what's in the old view.
    
    Added PatchSelectBox2. Currently it only holds the icon to add file
    comments and the anchor to hide file comments. The ability to hide
    file comments is necessary, because without a system scrollbar the
    vertical space is limited. The file comments auto-hide the same way
    Gerrit header does.
    
    Fixed glitch in line padding code. Padding for CommentBox still
    needs to be calculated by PaddingManager, The previous change was too
    simplistic and introduced misalignment.
    
    Experiment with new scrolling hack. The previous approach of syncing
    scrollTop doesn't work when CM constantly refuses to take the heights
    of line widgets into account. Added an ad-hoc check to see if the
    lines are actually aligned and do additional scrolling if necessary.
    
    Change-Id: I066ed2bde342eabd37759a13ed43ddba0f12d436

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/DraftBox.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/DraftBox.java
@@ -20,6 +20,7 @@ import com.google.gerrit.client.changes.CommentInfo;
 import com.google.gerrit.client.changes.CommentInput;
 import com.google.gerrit.client.rpc.GerritCallback;
 import com.google.gerrit.client.ui.CommentLinkProcessor;
+import com.google.gerrit.common.changes.Side;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
@@ -214,21 +215,23 @@ class DraftBox extends CommentBox {
     if (replyToBox != null) {
       replyToBox.unregisterReplyBox();
     }
-    parent.removeDraft(this, comment.side(), comment.line() - 1);
-
+    Side side = comment.side();
     removeFromParent();
+    if (!getCommentInfo().has_line()) {
+      parent.removeFileCommentBox(this, side);
+      return;
+    }
+    PaddingManager manager = getPaddingManager();
+    manager.remove(this);
+    parent.removeDraft(this, side, comment.line() - 1);
+    cm.focus();
+    getSelfWidgetWrapper().getWidget().clear();
     Scheduler.get().scheduleDeferred(new ScheduledCommand() {
       @Override
       public void execute() {
         resizePaddingWidget();
       }
     });
-    getSelfWidget().clear();
-
-    PaddingManager manager = getPaddingManager();
-    manager.remove(this);
-    manager.resizePaddingWidget();
-    cm.focus();
   }
 
   @UiHandler("message")
--- gerrit-gwtui/src/main/java/net/codemirror/lib/CodeMirror.java
+++ gerrit-gwtui/src/main/java/net/codemirror/lib/CodeMirror.java
@@ -144,12 +144,16 @@ public class CodeMirror extends JavaScriptObject {
     return this.getScrollInfo();
   }-*/;
 
-  public final native boolean isScrollSetByOther() /*-{
-    return this.state.scrollSetByOther == true;
+  public final native Viewport getViewport() /*-{
+    return this.getViewport();
   }-*/;
 
-  public final native void setScrollSetByOther(boolean setByOther) /*-{
-    this.state.scrollSetByOther = setByOther;
+  public final native double getScrollSetAt() /*-{
+    return this.state.scrollSetAt || 0;
+  }-*/;
+
+  public final native void setScrollSetAt(double when) /*-{
+    this.state.scrollSetAt = when;
   }-*/;
 
   public final native void on(String event, Runnable thunk) /*-{
@@ -244,6 +248,14 @@ public class CodeMirror extends JavaScriptObject {
   }-*/;
 
   protected CodeMirror() {
+  }
+
+  public static class Viewport extends JavaScriptObject {
+    public final native int getFrom() /*-{ return this.from; }-*/;
+    public final native int getTo() /*-{ return this.to; }-*/;
+
+    protected Viewport() {
+    }
   }
 
   public static class LineHandle extends JavaScriptObject {

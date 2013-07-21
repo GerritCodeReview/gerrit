3eb610db6e462089f70570707992c3eb4aab0005
Shawn Pearce <sop@google.com>
2013-07-20 17:17:17 -0700

    SideBySide2: Sign in the user when creating a comment
    If the user is not currently signed in bind 'c' to sign-in
    first and return back to the current SideBySide2 view. This
    is a useful shortcut to remind the user they need to login
    before they can comment on code.
    
    Change-Id: I46fee10a6c3d78cca879675c73e907a5150d54fa

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/SideBySide2.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/SideBySide2.java
@@ -262,17 +262,14 @@ public class SideBySide2 extends Screen {
     cm.on("scroll", doScroll(cm));
     cm.on("renderLine", resizeEmptyLine(getSideFromCm(cm)));
     // TODO: Prevent right click from updating the cursor.
-    KeyMap km = KeyMap.create()
+    cm.addKeyMap(KeyMap.create()
         .on("'j'", moveCursorDown(cm, 1))
         .on("'k'", moveCursorDown(cm, -1))
         .on("'u'", upToChange())
         .on("'r'", toggleReviewed())
         .on("'o'", toggleOpenBox(cm))
-        .on("Enter", toggleOpenBox(cm));
-    if (Gerrit.isSignedIn()) {
-      km.on("'c'", insertNewDraft(cm));
-    }
-    cm.addKeyMap(km);
+        .on("Enter", toggleOpenBox(cm))
+        .on("'c'", insertNewDraft(cm)));
 
     /**
      * TODO: Work on a better way for customizing keybindings and remove
@@ -815,6 +812,14 @@ public class SideBySide2 extends Screen {
   }
 
   private Runnable insertNewDraft(final CodeMirror cm) {
+    if (!Gerrit.isSignedIn()) {
+      return new Runnable() {
+        @Override
+        public void run() {
+          Gerrit.doSignIn(getToken());
+        }
+     };
+    }
     return new Runnable() {
       public void run() {
         LineHandle handle = cm.getActiveLine();

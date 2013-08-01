56780e4fa2fffac46e43fc1ca81bd1576efa0b36
Bruce Zu <bruce.zu@sonymobile.com>
2013-08-01 20:06:43 +0800

    Refactor RefControlTest
    Refactor RefControlTest to extract reusable code for the
    following up commit Id12dbec23a7.
    
    Change-Id: I4b9ea23b83de648910a07a162ae1c30820d0136b

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/project/Util.java
@@ -0,0 +1,231 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.project;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Lists;
+import com.google.gerrit.common.data.GroupReference;
+import com.google.gerrit.common.data.PermissionRule;
+import com.google.gerrit.reviewdb.client.AccountGroup;
+import com.google.gerrit.reviewdb.client.AccountProjectWatch;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.rules.PrologEnvironment;
+import com.google.gerrit.rules.RulesCache;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.CapabilityControl;
+import com.google.gerrit.server.account.GroupMembership;
+import com.google.gerrit.server.account.ListGroupMembership;
+import com.google.gerrit.server.config.AllProjectsName;
+import com.google.gerrit.server.config.FactoryModule;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.ProjectConfig;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import org.eclipse.jgit.lib.Config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Util {
+  public static AccountGroup.UUID ANONYMOUS = AccountGroup.ANONYMOUS_USERS;
+  public static AccountGroup.UUID REGISTERED = AccountGroup.REGISTERED_USERS;
+  public static AccountGroup.UUID ADMIN = new AccountGroup.UUID("test.admin");
+  public static AccountGroup.UUID DEVS = new AccountGroup.UUID("test.devs");
+
+  static public PermissionRule newRule(ProjectConfig project,
+      AccountGroup.UUID groupUUID) {
+    GroupReference group = new GroupReference(groupUUID, groupUUID.get());
+    group = project.resolve(group);
+
+    return new PermissionRule(group);
+  }
+
+  static public PermissionRule grantLabel(ProjectConfig project,
+      String permissionName, int min, int max, AccountGroup.UUID group,
+      String ref) {
+    PermissionRule rule = newRule(project, group);
+    rule.setMin(min);
+    rule.setMax(max);
+    return grant(project, permissionName, rule, ref);
+  }
+
+  static public PermissionRule grant(ProjectConfig project,
+      String permissionName, AccountGroup.UUID group, String ref) {
+    return grant(project, permissionName, newRule(project, group), ref);
+  }
+
+  static PermissionRule grant(ProjectConfig project, String permissionName,
+      PermissionRule rule, String ref) {
+    project.getAccessSection(ref, true) //
+        .getPermission(permissionName, true) //
+        .add(rule);
+    return rule;
+  }
+
+  private final Map<Project.NameKey, ProjectState> all;
+  private final ProjectCache projectCache;
+  private final CapabilityControl.Factory capabilityControlFactory;
+  private final PermissionCollection.Factory sectionSorter;
+
+  private final AllProjectsName allProjectsName = new AllProjectsName("parent");
+  private final ProjectConfig parent = new ProjectConfig(allProjectsName);
+
+  public Util() {
+    all = new HashMap<Project.NameKey, ProjectState>();
+    parent.createInMemory();
+    add(parent);
+
+    projectCache = new ProjectCache() {
+      @Override
+      public ProjectState getAllProjects() {
+        return get(allProjectsName);
+      }
+
+      @Override
+      public ProjectState get(Project.NameKey projectName) {
+        return all.get(projectName);
+      }
+
+      @Override
+      public void evict(Project p) {
+      }
+
+      @Override
+      public void remove(Project p) {
+      }
+
+      @Override
+      public Iterable<Project.NameKey> all() {
+        return Collections.emptySet();
+      }
+
+      @Override
+      public Iterable<Project.NameKey> byName(String prefix) {
+        return Collections.emptySet();
+      }
+
+      @Override
+      public void onCreateProject(Project.NameKey newProjectName) {
+      }
+
+      @Override
+      public Set<AccountGroup.UUID> guessRelevantGroupUUIDs() {
+        return Collections.emptySet();
+      }
+    };
+
+    Injector injector = Guice.createInjector(new FactoryModule() {
+      @Override
+      protected void configure() {
+        bind(Config.class).annotatedWith(GerritServerConfig.class).toInstance(
+            new Config());
+
+        factory(CapabilityControl.Factory.class);
+        bind(ProjectCache.class).toInstance(projectCache);
+      }
+    });
+
+    Cache<SectionSortCache.EntryKey, SectionSortCache.EntryVal> c =
+        CacheBuilder.newBuilder().build();
+    sectionSorter = new PermissionCollection.Factory(new SectionSortCache(c));
+    capabilityControlFactory =
+        injector.getInstance(CapabilityControl.Factory.class);
+  }
+
+  public ProjectConfig getParentConfig() {
+    return this.parent;
+  }
+
+  public void add(ProjectConfig pc) {
+    PrologEnvironment.Factory envFactory = null;
+    GitRepositoryManager mgr = null;
+    ProjectControl.AssistedFactory projectControlFactory = null;
+    RulesCache rulesCache = null;
+
+    all.put(pc.getProject().getNameKey(),
+        new ProjectState(projectCache, allProjectsName, projectControlFactory,
+            envFactory, mgr, rulesCache, pc));
+  }
+
+  public ProjectControl user(ProjectConfig local, AccountGroup.UUID... memberOf) {
+    return user(local, null, memberOf);
+  }
+
+  public ProjectControl user(ProjectConfig local, String name,
+      AccountGroup.UUID... memberOf) {
+    String canonicalWebUrl = "http://localhost";
+
+    return new ProjectControl(Collections.<AccountGroup.UUID> emptySet(),
+        Collections.<AccountGroup.UUID> emptySet(), projectCache,
+        sectionSorter, canonicalWebUrl, new MockUser(name, memberOf),
+        newProjectState(local));
+  }
+
+  private ProjectState newProjectState(ProjectConfig local) {
+    PrologEnvironment.Factory envFactory = null;
+    GitRepositoryManager mgr = null;
+    ProjectControl.AssistedFactory projectControlFactory = null;
+    RulesCache rulesCache = null;
+
+    all.put(local.getProject().getNameKey(), new ProjectState(projectCache,
+        allProjectsName, projectControlFactory, envFactory, mgr, rulesCache,
+        local));
+
+    return all.get(local.getProject().getNameKey());
+  }
+
+  private class MockUser extends CurrentUser {
+    private final String username;
+    private final GroupMembership groups;
+
+    MockUser(String name, AccountGroup.UUID[] groupId) {
+      // super(RefControlTest.this.util.getCapabilityControFactory());
+      super(capabilityControlFactory);
+      username = name;
+      ArrayList<AccountGroup.UUID> groupIds = Lists.newArrayList(groupId);
+      groupIds.add(REGISTERED);
+      groupIds.add(ANONYMOUS);
+      groups = new ListGroupMembership(groupIds);
+    }
+
+    @Override
+    public GroupMembership getEffectiveGroups() {
+      return groups;
+    }
+
+    @Override
+    public String getUserName() {
+      return username;
+    }
+
+    @Override
+    public Set<Change.Id> getStarredChanges() {
+      return Collections.emptySet();
+    }
+
+    @Override
+    public Collection<AccountProjectWatch> getNotificationFilters() {
+      return Collections.emptySet();
+    }
+  }
+}

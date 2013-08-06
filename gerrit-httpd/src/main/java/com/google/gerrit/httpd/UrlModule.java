c360a68617632a10916e717cefd704c7e1b79d9f
Juan Hernandez <juanantonio.hernandez@gmail.com>
2013-08-06 16:30:50 +0200

    Configurable external robots.txt file
    Currently the robots.txt file used by the application is inside the .war
    file and thus difficult to modify for users. This patch adds a new
    optional configuration parameter that allows the user to specify an
    external robots file, so that it is easy to modify and easy to preserve
    during upgrades of the application. If no configuration change is made
    the original robots file is used, only if the following is added to the
    gerrit.config file will the external file be used:
    
      [httpd]
        robotsFile = etc/myrobots.txt
    
    If the file indicated by this parameter is relative then it will be
    resolved as sub directory of the site directory, if it is absulute it
    will be used as is.
    
    If the file doesn't exist or can't be read a message will be written to
    the log and the default file will be used.
    
    Change-Id: Iad02dbd97633e9c45dbce15d1f227f3931255e0a
    Signed-off-by: Juan Hernandez <juanantonio.hernandez@gmail.com>

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/UrlModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/UrlModule.java
@@ -21,6 +21,7 @@ import com.google.gerrit.common.PageLinks;
 import com.google.gerrit.httpd.raw.CatServlet;
 import com.google.gerrit.httpd.raw.HostPageServlet;
 import com.google.gerrit.httpd.raw.LegacyGerritServlet;
+import com.google.gerrit.httpd.raw.RobotsServlet;
 import com.google.gerrit.httpd.raw.SshInfoServlet;
 import com.google.gerrit.httpd.raw.StaticServlet;
 import com.google.gerrit.httpd.raw.ToolServlet;
@@ -112,6 +113,8 @@ class UrlModule extends ServletModule {
     if (cfg.deprecatedQuery) {
       serve("/query").with(DeprecatedChangeQueryServlet.class);
     }
+
+    serve("/robots.txt").with(RobotsServlet.class);
   }
 
   private Key<HttpServlet> notFound() {

GerritCodeReview/gerrit

commit e51c428fe5ef3960f4990e1c0471d067783d4718

Display command description in ssh gerrit --help
Show one line description for each ssh command. Make COMMAND
argument optional, so that --help argument is not needed.

This approach is working for core gerrit commands, aliases, nested
commands and plugin's own ssh commands.

To provide a description ssh command must be annotated with
CommandMetaData annotation, i. e.:

@CommandMetaData(name="print", descr="Print greeting in different languages")
public final class PrintHelloWorldCommand extends SshCommand { ...

Syntactic sugar for command registration is provided to reflect the fact,
that both name and description are included in CommandMetaData annotation.

To register command and alias in plugin:
protected void configureCommands() {
  command(PrintHelloWorldCommand.class);
  alias("say-hello", PrintHelloWorldCommand.class);
[...]

With the outcome:
$ ssh gerrit helloworld
Available commands of helloworld are:

   print       Print greeting in different languages
   say-hello   Print greeting in different languages

Change-Id: I2e5440378023ecc5425092d8131f121da2f20a30

gerrit-sshd/src/main/java/com/google/gerrit/sshd/DispatchCommandProvider.java
@@ -39,7 +39,7 @@ public class DispatchCommandProvider implements Provider<DispatchCommand> {
   private DispatchCommand.Factory factory;
 
   private final CommandName parent;
-  private volatile ConcurrentMap<String, Provider<Command>> map;
+  private volatile ConcurrentMap<String, CommandProvider> map;
 
   public DispatchCommandProvider(final CommandName cn) {
     this.parent = cn;
@@ -52,8 +52,8 @@ public class DispatchCommandProvider implements Provider<DispatchCommand> {
 
   public RegistrationHandle register(final CommandName name,
       final Provider<Command> cmd) {
-    final ConcurrentMap<String, Provider<Command>> m = getMap();
-    if (m.putIfAbsent(name.value(), cmd) != null) {
+    final ConcurrentMap<String, CommandProvider> m = getMap();
+    if (m.putIfAbsent(name.value(), new CommandProvider(cmd, null)) != null) {
       throw new IllegalArgumentException(name.value() + " exists");
     }
     return new RegistrationHandle() {
@@ -66,8 +66,8 @@ public class DispatchCommandProvider implements Provider<DispatchCommand> {
 
   public RegistrationHandle replace(final CommandName name,
       final Provider<Command> cmd) {
-    final ConcurrentMap<String, Provider<Command>> m = getMap();
-    m.put(name.value(), cmd);
+    final ConcurrentMap<String, CommandProvider> m = getMap();
+    m.put(name.value(), new CommandProvider(cmd, null));
     return new RegistrationHandle() {
       @Override
       public void remove() {
@@ -76,7 +76,7 @@ public class DispatchCommandProvider implements Provider<DispatchCommand> {
     };
   }
 
-  ConcurrentMap<String, Provider<Command>> getMap() {
+  ConcurrentMap<String, CommandProvider> getMap() {
     if (map == null) {
       synchronized (this) {
         if (map == null) {
@@ -88,14 +88,21 @@ public class DispatchCommandProvider implements Provider<DispatchCommand> {
   }
 
   @SuppressWarnings("unchecked")
-  private ConcurrentMap<String, Provider<Command>> createMap() {
-    ConcurrentMap<String, Provider<Command>> m = Maps.newConcurrentMap();
+  private ConcurrentMap<String, CommandProvider> createMap() {
+    ConcurrentMap<String, CommandProvider> m = Maps.newConcurrentMap();
     for (final Binding<?> b : allCommands()) {
       final Annotation annotation = b.getKey().getAnnotation();
       if (annotation instanceof CommandName) {
+        String descr = null;
+        if (annotation instanceof Commands.NestedCommandNameImpl) {
+          Commands.NestedCommandNameImpl impl =
+              ((Commands.NestedCommandNameImpl) annotation);
+          descr = impl.descr();
+        }
         final CommandName n = (CommandName) annotation;
         if (!Commands.CMD_ROOT.equals(n) && Commands.isChild(parent, n)) {
-          m.put(n.value(), (Provider<Command>) b.getProvider());
+          m.put(n.value(),
+              new CommandProvider((Provider<Command>) b.getProvider(), descr));
         }
       }
     }

gerrit-sshd/src/main/java/com/google/gerrit/sshd/AliasCommand.java
@@ -60,26 +60,26 @@ public class AliasCommand extends BaseCommand {
   }
 
   private void begin(Environment env) throws UnloggedFailure, IOException {
-    Map<String, Provider<Command>> map = root.getMap();
+    Map<String, CommandProvider> map = root.getMap();
     for (String name : chain(command)) {
-      Provider<? extends Command> p = map.get(name);
+      CommandProvider p = map.get(name);
       if (p == null) {
         throw new UnloggedFailure(1, getName() + ": not found");
       }
 
-      Command cmd = p.get();
+      Command cmd = p.getProvider().get();
       if (!(cmd instanceof DispatchCommand)) {
         throw new UnloggedFailure(1, getName() + ": not found");
       }
       map = ((DispatchCommand) cmd).getMap();
     }
 
-    Provider<? extends Command> p = map.get(command.value());
+    CommandProvider p = map.get(command.value());
     if (p == null) {
       throw new UnloggedFailure(1, getName() + ": not found");
     }
 
-    Command cmd = p.get();
+    Command cmd = p.getProvider().get();
     checkRequiresCapability(cmd);
     if (cmd instanceof BaseCommand) {
       BaseCommand bc = (BaseCommand)cmd;

gerrit-sshd/src/main/java/com/google/gerrit/sshd/CommandMetaData.java
@@ -1,0 +1,31 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation tagged on a concrete Command to describe what it is doing
+ */
+@Target( {ElementType.TYPE})
+@Retention(RUNTIME)
+public @interface CommandMetaData {
+  String name();
+  String descr() default "";
+}

gerrit-sshd/src/main/java/com/google/gerrit/sshd/CommandModule.java
@@ -48,7 +48,7 @@ public abstract class CommandModule extends AbstractModule {
   /**
    * Configure a command to be invoked by name.
    *
-   *@param parent context of the parent command, that this command is a
+   * @param parent context of the parent command, that this command is a
    *        subcommand of.
    * @param name the name of the command the client will provide in order to
    *        call the command.
@@ -58,6 +58,42 @@ public abstract class CommandModule extends AbstractModule {
   protected LinkedBindingBuilder<Command> command(final CommandName parent,
       final String name) {
     return bind(Commands.key(parent, name));
+  }
+
+  /**
+   * Configure a command to be invoked by name. The command is bound to the passed class.
+   *
+   * @param parent context of the parent command, that this command is a
+   *        subcommand of.
+   * @param clazz class of the command with {@link CommandMetaData} annotation
+   *        to retrieve the name and the description from
+   */
+  protected void command(final CommandName parent,
+      final Class<? extends BaseCommand> clazz) {
+    CommandMetaData meta = (CommandMetaData)clazz.getAnnotation(CommandMetaData.class);
+    if (meta == null) {
+      throw new IllegalStateException("no CommandMetaData annotation found");
+    }
+    bind(Commands.key(parent, meta.name(), meta.descr())).to(clazz);
+  }
+
+  /**
+   * Alias one command to another. The alias is bound to the passed class.
+   *
+   * @param parent context of the parent command, that this command is a
+   *        subcommand of.
+   * @param name the name of the command the client will provide in order to
+   *        call the command.
+   * @param clazz class of the command with {@link CommandMetaData} annotation
+   *        to retrieve the description from
+   */
+  protected void alias(final CommandName parent, final String name,
+      final Class<? extends BaseCommand> clazz) {
+    CommandMetaData meta = (CommandMetaData)clazz.getAnnotation(CommandMetaData.class);
+    if (meta == null) {
+      throw new IllegalStateException("no CommandMetaData annotation found");
+    }
+    bind(Commands.key(parent, name, meta.descr())).to(clazz);
   }
 
   /**

gerrit-sshd/src/main/java/com/google/gerrit/sshd/CommandProvider.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2011 The Android Open Source Project
+// Copyright (C) 2013 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,22 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.sshd.commands;
+package com.google.gerrit.sshd;
 
-import com.google.gerrit.common.Version;
-import com.google.gerrit.sshd.CommandMetaData;
-import com.google.gerrit.sshd.SshCommand;
+import com.google.inject.Provider;
 
-@CommandMetaData(name = "version", descr = "Display gerrit version")
-final class VersionCommand extends SshCommand {
+import org.apache.sshd.server.Command;
 
-  @Override
-  protected void run() throws Failure {
-    String v = Version.getVersion();
-    if (v == null) {
-      throw new Failure(1, "fatal: version unavailable");
-    }
+final class CommandProvider {
 
-    stdout.println("gerrit version " + v);
+  private final Provider<Command> provider;
+  private final String description;
+
+  CommandProvider(final Provider<Command> p, final String d) {
+    this.provider = p;
+    this.description = d;
+  }
+
+  public Provider<Command> getProvider() {
+    return provider;
+  }
+
+  public String getDescription() {
+    return description;
   }
 }

gerrit-sshd/src/main/java/com/google/gerrit/sshd/Commands.java
@@ -16,6 +16,7 @@ package com.google.gerrit.sshd;
 
 import com.google.inject.Key;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.sshd.server.Command;
 
 import java.lang.annotation.Annotation;
@@ -39,6 +40,11 @@ public class Commands {
   public static Key<Command> key(final CommandName parent,
       final String name) {
     return Key.get(Command.class, named(parent, name));
+  }
+
+  public static Key<Command> key(final CommandName parent,
+      final String name, final String descr) {
+    return Key.get(Command.class, named(parent, name, descr));
   }
 
   /** Create a CommandName annotation for the supplied name. */
@@ -78,6 +84,12 @@ public class Commands {
     return new NestedCommandNameImpl(parent, name);
   }
 
+  /** Create a CommandName annotation for the supplied name and description. */
+  public static CommandName named(final CommandName parent, final String name,
+      final String descr) {
+    return new NestedCommandNameImpl(parent, name, descr);
+  }
+
   /** Return the name of this command, possibly including any parents. */
   public static String nameOf(final CommandName name) {
     if (name instanceof NestedCommandNameImpl) {
@@ -104,18 +116,31 @@ public class Commands {
     return null;
   }
 
-  private static final class NestedCommandNameImpl implements CommandName {
+  static final class NestedCommandNameImpl implements CommandName {
     private final CommandName parent;
     private final String name;
+    private final String descr;
 
     NestedCommandNameImpl(final CommandName parent, final String name) {
       this.parent = parent;
       this.name = name;
+      this.descr = StringUtils.EMPTY;
+    }
+
+    NestedCommandNameImpl(final CommandName parent, final String name,
+        final String descr) {
+      this.parent = parent;
+      this.name = name;
+      this.descr = descr;
     }
 
     @Override
     public String value() {
       return name;
+    }
+
+    public String descr() {
+      return descr;
     }
 
     @Override

gerrit-sshd/src/main/java/com/google/gerrit/sshd/DispatchCommand.java
@@ -14,6 +14,7 @@
 
 package com.google.gerrit.sshd;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Atomics;
 import com.google.gerrit.extensions.annotations.RequiresCapability;
@@ -29,6 +30,7 @@ import org.apache.sshd.server.Environment;
 import org.kohsuke.args4j.Argument;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,14 +41,14 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 final class DispatchCommand extends BaseCommand {
   interface Factory {
-    DispatchCommand create(Map<String, Provider<Command>> map);
+    DispatchCommand create(Map<String, CommandProvider> map);
   }
 
   private final Provider<CurrentUser> currentUser;
-  private final Map<String, Provider<Command>> commands;
+  private final Map<String, CommandProvider> commands;
   private final AtomicReference<Command> atomicCmd;
 
-  @Argument(index = 0, required = true, metaVar = "COMMAND", handler = SubcommandHandler.class)
+  @Argument(index = 0, required = false, metaVar = "COMMAND", handler = SubcommandHandler.class)
   private String commandName;
 
   @Argument(index = 1, multiValued = true, metaVar = "ARG")
@@ -54,13 +56,13 @@ final class DispatchCommand extends BaseCommand {
 
   @Inject
   DispatchCommand(final Provider<CurrentUser> cu,
-      @Assisted final Map<String, Provider<Command>> all) {
+      @Assisted final Map<String, CommandProvider> all) {
     currentUser = cu;
     commands = all;
     atomicCmd = Atomics.newReference();
   }
 
-  Map<String, Provider<Command>> getMap() {
+  Map<String, CommandProvider> getMap() {
     return commands;
   }
 
@@ -68,8 +70,13 @@ final class DispatchCommand extends BaseCommand {
   public void start(final Environment env) throws IOException {
     try {
       parseCommandLine();
+      if (Strings.isNullOrEmpty(commandName)) {
+        StringWriter msg = new StringWriter();
+        msg.write(usage());
+        throw new UnloggedFailure(1, msg.toString());
+      }
 
-      final Provider<Command> p = commands.get(commandName);
+      final CommandProvider p = commands.get(commandName);
       if (p == null) {
         String msg =
             (getName().isEmpty() ? "Gerrit Code Review" : getName()) + ": "
@@ -77,7 +84,7 @@ final class DispatchCommand extends BaseCommand {
         throw new UnloggedFailure(1, msg);
       }
 
-      final Command cmd = p.get();
+      final Command cmd = p.getProvider().get();
       checkRequiresCapability(cmd);
       if (cmd instanceof BaseCommand) {
         final BaseCommand bc = (BaseCommand) cmd;
@@ -138,9 +145,17 @@ final class DispatchCommand extends BaseCommand {
     }
     usage.append(" are:\n");
     usage.append("\n");
+
+    int maxLength = -1;
+    for (String name : commands.keySet()) {
+      maxLength = Math.max(maxLength, name.length());
+    }
+    String format = "%-" + maxLength + "s   %s";
     for (String name : Sets.newTreeSet(commands.keySet())) {
+      final CommandProvider p = commands.get(name);
       usage.append("   ");
-      usage.append(name);
+      usage.append(String.format(format, name,
+          Strings.nullToEmpty(p.getDescription())));
       usage.append("\n");
     }
     usage.append("\n");

gerrit-sshd/src/main/java/com/google/gerrit/sshd/PluginCommandModule.java
@@ -16,21 +16,17 @@ package com.google.gerrit.sshd;
 
 import com.google.common.base.Preconditions;
 import com.google.gerrit.extensions.annotations.PluginName;
-import com.google.gerrit.sshd.CommandName;
-import com.google.gerrit.sshd.Commands;
-import com.google.gerrit.sshd.DispatchCommandProvider;
-import com.google.inject.AbstractModule;
 import com.google.inject.binder.LinkedBindingBuilder;
 
 import org.apache.sshd.server.Command;
 
 import javax.inject.Inject;
 
-public abstract class PluginCommandModule extends AbstractModule {
+public abstract class PluginCommandModule extends CommandModule {
   private CommandName command;
 
   @Inject
-  void setPluginName(@PluginName String name) {
+  void setPluginName(@PluginName String name, final String descr) {
     this.command = Commands.named(name);
   }
 
@@ -46,4 +42,13 @@ public abstract class PluginCommandModule extends AbstractModule {
   protected LinkedBindingBuilder<Command> command(String subCmd) {
     return bind(Commands.key(command, subCmd));
   }
+
+  protected void command(Class<? extends BaseCommand> clazz) {
+    command(command, clazz);
+  }
+
+  protected void alias(final String name, Class<? extends BaseCommand> clazz) {
+    alias(command, name, clazz);
+  }
+
 }

commit 47e0f773b3219873dfaa0797005ae2b0f20bce88

Fix reenabling of plugins
e51c428fe5ef3960f4990e1c0471d067783d4718 broke the unregistering of plugin
owned SSH command by pssing wrong instance to the ConcurrentMap.remove()
function. Currently plugins can not be reenabled.

Change-Id: I2b2dfe6ffe89a426a03d945d9695bc35e7fe9f1a

gerrit-sshd/src/main/java/com/google/gerrit/sshd/DispatchCommandProvider.java
@@ -59,7 +59,10 @@ public class DispatchCommandProvider implements Provider<DispatchCommand> {
     return new RegistrationHandle() {
       @Override
       public void remove() {
-        m.remove(name.value(), cmd);
+        if (!m.remove(name.value(), new CommandProvider(cmd, null))) {
+          throw new IllegalStateException(String.format(
+              "can not unregister command: %s", name.value()));
+        }
       }
     };
   }
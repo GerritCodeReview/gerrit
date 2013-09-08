f46fe96e7ccd4830ffdd3d98286f4ba439a46906
David Ostrovsky <david@ostrovsky.org>
2013-09-08 07:11:16 +0200

    [RFC] Change Screen2: Support removing reviewers with their votes
    Given that the reviewers are listed on three different places on CS2, it is not
    obviously where to put that functionality.
    
    This change implements the idea, that was proposed on dev ML on CS2 thread,
    extending adding reviewers dropdown box with the ApprovalTable, borrowed
    from the old change screen. That way we not only can remove reviewers with their
    votes, but also see, which reviewers are able to vote on which label: it was
    the only missing requirement, that wasn't implemented on CS2 in the first
    place.
    
    To update the reviewer specific fields after reviewers removal:
    
    * the reviewers (with votes), and
    * labels
    
    we need to pass the corresponding fields from ChangeScreen2 to Reviewers
    instance.
    
    Bug: Issue 2067
    Change-Id: I654906c94b859f1b915c417890e19b4e1728ed4d

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Reviewers.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Reviewers.java
@@ -14,6 +14,8 @@
 
 package com.google.gerrit.client.change;
 
+import static com.google.gerrit.common.data.LabelValue.formatValue;
+
 import com.google.gerrit.client.ConfirmationCallback;
 import com.google.gerrit.client.ConfirmationDialog;
 import com.google.gerrit.client.Gerrit;
@@ -27,11 +29,17 @@ import com.google.gerrit.client.changes.ChangeInfo.LabelInfo;
 import com.google.gerrit.client.changes.Util;
 import com.google.gerrit.client.rpc.GerritCallback;
 import com.google.gerrit.client.rpc.Natives;
+import com.google.gerrit.client.ui.AccountLinkPanel;
 import com.google.gerrit.client.ui.HintTextBox;
+import com.google.gerrit.common.data.ApprovalDetail;
+import com.google.gerrit.common.data.SubmitRecord;
+import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
@@ -43,7 +51,11 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.rpc.StatusCodeException;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.HTMLTable.CellFormatter;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.SuggestBox.DefaultSuggestionDisplay;
 import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
@@ -51,8 +63,15 @@ import com.google.gwt.user.client.ui.UIObject;
 import com.google.gwtexpui.safehtml.client.SafeHtml;
 import com.google.gwtexpui.safehtml.client.SafeHtmlBuilder;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /** Add reviewers. */
 class Reviewers extends Composite {
@@ -65,19 +84,29 @@ class Reviewers extends Composite {
   @UiField Element error;
   @UiField(provided = true)
   SuggestBox suggestBox;
+  @UiField Grid table;
+
+  // References to UiFields from ChangeScreen2
+  Labels labels;
+  Element realReviewers;
 
   private RestReviewerSuggestOracle reviewerSuggestOracle;
   private HintTextBox nameTxtBox;
   private Change.Id changeId;
   private ChangeScreen2.Style style;
+  private ChangeInfo info;
+  private String revision;
   private boolean submitOnSelection;
+  private Map<Integer, Integer> rows;
 
   Reviewers() {
+    rows = new HashMap<Integer, Integer>();
     reviewerSuggestOracle = new RestReviewerSuggestOracle();
     nameTxtBox = new HintTextBox();
     suggestBox = new SuggestBox(reviewerSuggestOracle, nameTxtBox);
     initWidget(uiBinder.createAndBindUi(this));
 
+    table.addStyleName(Gerrit.RESOURCES.css().infoTable());
     nameTxtBox.setVisibleLength(55);
     nameTxtBox.setHintText(Util.C.approvalTableAddReviewerHint());
     nameTxtBox.addKeyDownHandler(new KeyDownHandler() {
@@ -108,8 +137,13 @@ class Reviewers extends Composite {
     });
   }
 
-  void set(Change.Id changeId) {
-    this.changeId = changeId;
+  void set(ChangeInfo info, String revision,
+      Labels labels, Element realReviewers) {
+    this.changeId = info.legacy_id();
+    this.info = info;
+    this.revision = revision;
+    this.labels = labels;
+    this.realReviewers = realReviewers;
     reviewerSuggestOracle.setChange(changeId);
   }
 
@@ -132,6 +166,193 @@ class Reviewers extends Composite {
     UIObject.setVisible(error, false);
     openForm.setVisible(false);
     suggestBox.setFocus(true);
+    renderRemoveReviewers();
+  }
+
+  private void renderRemoveReviewers() {
+    Map<Integer, ApprovalDetail> byUser =
+        new LinkedHashMap<Integer, ApprovalDetail>();
+    Map<Integer, AccountInfo> accounts =
+        new LinkedHashMap<Integer, AccountInfo>();
+    initLabels(accounts, byUser);
+    table.clear();
+    if (byUser.isEmpty()) {
+      table.setVisible(false);
+    } else {
+      table.resize(1, 3);
+      List<String> labels = new ArrayList<String>(info.labels());
+      Collections.sort(labels);
+      displayHeader(labels);
+      table.resizeRows(1 + byUser.size());
+      int i = 1;
+      for (ApprovalDetail ad : ApprovalDetail.sort(
+          byUser.values(), info.owner()._account_id())) {
+        displayRow(i++, ad, labels, accounts.get(ad.getAccount().get()));
+      }
+      table.setVisible(true);
+    }
+  }
+
+  private List<String> initLabels(
+      Map<Integer, AccountInfo> accounts,
+      Map<Integer, ApprovalDetail> byUser) {
+    Set<Integer> removableReviewers = removableReviewers();
+    List<String> missing = new ArrayList<String>();
+    for (String name : info.labels()) {
+      LabelInfo label = info.label(name);
+
+      String min = null;
+      String max = null;
+      for (String v : label.values()) {
+        if (min == null) {
+          min = v;
+        }
+        if (v.startsWith("+")) {
+          max = v;
+        }
+      }
+
+      if (label.status() == SubmitRecord.Label.Status.NEED) {
+        missing.add(name);
+      }
+
+      if (label.all() != null) {
+        for (ApprovalInfo ai : Natives.asList(label.all())) {
+          if (!accounts.containsKey(ai._account_id())) {
+            accounts.put(ai._account_id(), ai);
+          }
+          int id = ai._account_id();
+          ApprovalDetail ad = byUser.get(id);
+          if (ad == null) {
+            ad = new ApprovalDetail(new Account.Id(id));
+            ad.setCanRemove(removableReviewers.contains(id));
+            byUser.put(id, ad);
+          }
+          if (ai.has_value()) {
+            ad.votable(name);
+            ad.value(name, ai.value());
+            String fv = formatValue(ai.value());
+            if (fv.equals(max)) {
+              ad.approved(name);
+            } else if (ai.value() < 0 && fv.equals(min)) {
+              ad.rejected(name);
+            }
+          }
+        }
+      }
+    }
+    return missing;
+  }
+
+  private Set<Integer> removableReviewers() {
+    Set<Integer> result =
+        new HashSet<Integer>(info.removable_reviewers().length());
+    for (int i = 0; i < info.removable_reviewers().length(); i++) {
+      result.add(info.removable_reviewers().get(i)._account_id());
+    }
+    return result;
+  }
+
+  /**
+   * Sets the header row
+   *
+   * @param labels The list of labels to display in the header. This list does
+   *    not get resorted, so be sure that the list's elements are in the same
+   *    order as the list of labels passed to the {@code displayRow} method.
+   */
+  private void displayHeader(Collection<String> labels) {
+    table.resizeColumns(2 + labels.size());
+
+    final CellFormatter fmt = table.getCellFormatter();
+    int col = 0;
+
+    table.setText(0, col, Util.C.approvalTableReviewer());
+    fmt.setStyleName(0, col, Gerrit.RESOURCES.css().header());
+    col++;
+
+    table.clearCell(0, col);
+    fmt.setStyleName(0, col, Gerrit.RESOURCES.css().header());
+    col++;
+
+    for (String name : labels) {
+      table.setText(0, col, name);
+      fmt.setStyleName(0, col, Gerrit.RESOURCES.css().header());
+      col++;
+    }
+    fmt.addStyleName(0, col - 1, Gerrit.RESOURCES.css().rightmost());
+  }
+
+  /**
+   * Sets the reviewer data for a row.
+   *
+   * @param row The number of the row on which to set the reviewer.
+   * @param ad The details for this reviewer's approval.
+   * @param labels The list of labels to show. This list does not get resorted,
+   *    so be sure that the list's elements are in the same order as the list
+   *    of labels passed to the {@code displayHeader} method.
+   * @param account The account information for the approval.
+   */
+  private void displayRow(int row, final ApprovalDetail ad,
+      List<String> labels, AccountInfo account) {
+    final CellFormatter fmt = table.getCellFormatter();
+    int col = 0;
+
+    table.setWidget(row, col++, new AccountLinkPanel(account));
+    rows.put(account._account_id(), row);
+
+    if (ad.canRemove()) {
+      final PushButton remove = new PushButton( //
+          new Image(Util.R.removeReviewerNormal()), //
+          new Image(Util.R.removeReviewerPressed()));
+      remove.setTitle(Util.M.removeReviewer(account.name()));
+      remove.setStyleName(Gerrit.RESOURCES.css().removeReviewer());
+      remove.addStyleName(Gerrit.RESOURCES.css().link());
+      remove.addClickHandler(new ClickHandler() {
+        @Override
+        public void onClick(ClickEvent event) {
+          doRemove(ad, remove);
+        }
+      });
+      table.setWidget(row, col, remove);
+    } else {
+      table.clearCell(row, col);
+    }
+    fmt.setStyleName(row, col++, Gerrit.RESOURCES.css().removeReviewerCell());
+
+    for (String labelName : labels) {
+      fmt.setStyleName(row, col, Gerrit.RESOURCES.css().approvalscore());
+      if (!ad.canVote(labelName)) {
+        fmt.addStyleName(row, col, Gerrit.RESOURCES.css().notVotable());
+        fmt.getElement(row, col).setTitle(Gerrit.C.userCannotVoteToolTip());
+      }
+
+      if (ad.isRejected(labelName)) {
+        table.setWidget(row, col, new Image(Gerrit.RESOURCES.redNot()));
+
+      } else if (ad.isApproved(labelName)) {
+        table.setWidget(row, col, new Image(Gerrit.RESOURCES.greenCheck()));
+
+      } else {
+        int v = ad.getValue(labelName);
+        if (v == 0) {
+          table.clearCell(row, col);
+          col++;
+          continue;
+        }
+        String vstr = String.valueOf(ad.getValue(labelName));
+        if (v > 0) {
+          vstr = "+" + vstr;
+          fmt.addStyleName(row, col, Gerrit.RESOURCES.css().posscore());
+        } else {
+          fmt.addStyleName(row, col, Gerrit.RESOURCES.css().negscore());
+        }
+        table.setText(row, col, vstr);
+      }
+
+      col++;
+    }
+
+    fmt.addStyleName(row, col - 1, Gerrit.RESOURCES.css().rightmost());
   }
 
   @UiHandler("add")
@@ -201,7 +422,9 @@ class Reviewers extends Composite {
         new GerritCallback<ChangeInfo>() {
           @Override
           public void onSuccess(ChangeInfo result) {
+            Reviewers.this.info = result;
             display(result);
+            renderRemoveReviewers();
           }
         });
   }
@@ -219,7 +442,37 @@ class Reviewers extends Composite {
     for (Integer i : r.keySet()) {
       cc.remove(i);
     }
+    r.remove(info.owner()._account_id());
+    realReviewers.setInnerSafeHtml(Labels.formatUserList(style, r.values()));
+
     cc.remove(info.owner()._account_id());
     setReviewers(Labels.formatUserList(style, cc.values()));
+
+    labels.clear();
+    boolean current = info.status().isOpen()
+        && revision.equals(info.current_revision());
+
+    // Do we need to take care for statusText too?
+    /*
+    boolean canSubmit =
+    */
+    labels.set(info, current);
+  }
+
+  private void doRemove(ApprovalDetail ad, final PushButton remove) {
+    remove.setEnabled(false);
+    ChangeApi.reviewer(info.legacy_id().get(), ad.getAccount().get())
+      .delete(new GerritCallback<JavaScriptObject>() {
+          @Override
+          public void onSuccess(JavaScriptObject result) {
+            updateReviewerList();
+          }
+
+          @Override
+          public void onFailure(final Throwable caught) {
+            remove.setEnabled(true);
+            super.onFailure(caught);
+          }
+        });
   }
 }

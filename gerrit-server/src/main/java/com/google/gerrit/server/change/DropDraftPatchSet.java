ea21239cb4551cc95d895fc5dfec04379509ff80
David Ostrovsky <david@ostrovsky.org>
2013-09-10 23:10:23 +0200

    Add REST endpoints for draft changes
    Change-Id: Ia30917267cffdeb4013e1415bed5e2a17ffdef6d

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/DropDraftPatchSet.java
@@ -0,0 +1,136 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.extensions.webui.UiAction;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.PatchSetInfo;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.ChangeUtil;
+import com.google.gerrit.server.change.DropDraftPatchSet.Input;
+import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.patch.PatchSetInfoFactory;
+import com.google.gerrit.server.patch.PatchSetInfoNotAvailableException;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.gwtorm.server.AtomicUpdate;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import java.io.IOException;
+
+public class DropDraftPatchSet implements RestModifyView<RevisionResource, Input>,
+    UiAction<RevisionResource> {
+  public static class Input {
+  }
+
+  private final Provider<ReviewDb> dbProvider;
+  private final GitRepositoryManager gitManager;
+  private final GitReferenceUpdated gitRefUpdated;
+  private final PatchSetInfoFactory patchSetInfoFactory;
+
+  @Inject
+  public DropDraftPatchSet(Provider<ReviewDb> dbProvider,
+      GitRepositoryManager gitManager,
+      GitReferenceUpdated gitRefUpdated,
+      PatchSetInfoFactory patchSetInfoFactory) {
+    this.dbProvider = dbProvider;
+    this.gitManager = gitManager;
+    this.gitRefUpdated = gitRefUpdated;
+    this.patchSetInfoFactory = patchSetInfoFactory;
+  }
+
+  @Override
+  public Object apply(RevisionResource rsrc, Input input)
+      throws NoSuchChangeException, OrmException,
+      IOException, ResourceConflictException {
+    final PatchSet patchSet = rsrc.getPatchSet();
+    final PatchSet.Id patchSetId = patchSet.getId();
+    final Change change = rsrc.getChange();
+
+    // Check to support SSH review command
+    if (!patchSet.isDraft()) {
+      throw new ResourceConflictException("patch set is not a draft.");
+    }
+
+    if (!rsrc.getControl().canDeleteDraft(dbProvider.get())) {
+      throw new ResourceConflictException(
+          "Not permitted to delete this draft patch set");
+    }
+
+    ChangeUtil.deleteOnlyDraftPatchSet(patchSet,
+          change, gitManager, gitRefUpdated, dbProvider.get());
+    // if the last ps was deleted, delete the change
+    if (dbProvider.get().patchSets().byChange(
+            change.getId()).toList().size() == 0) {
+        ChangeUtil.deleteDraftChange(patchSetId,
+            gitManager, gitRefUpdated, dbProvider.get());
+    } else {
+      if (change.currentPatchSetId().equals(patchSetId)) {
+        try {
+          updateCurrentPatchSet(dbProvider.get(), change,
+              previousPatchSetInfo(patchSetId));
+        } catch (PatchSetInfoNotAvailableException e) {
+          throw new NoSuchChangeException(change.getId());
+        }
+      }
+    }
+
+    return Response.none();
+  }
+
+  @Override
+  public UiAction.Description getDescription(RevisionResource resource) {
+    PatchSet.Id current = resource.getChange().currentPatchSetId();
+    try {
+      int psCount = dbProvider.get().patchSets()
+          .byChange(resource.getChange().getId()).toList().size();
+      return new UiAction.Description()
+        .setTitle(String.format("Delete Draft Revision %d",
+            resource.getPatchSet().getPatchSetId()))
+        .setVisible(resource.getPatchSet().isDraft()
+            && resource.getPatchSet().getId().equals(current)
+            && resource.getControl().canDeleteDraft(dbProvider.get())
+            && psCount > 1);
+    } catch (OrmException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  private PatchSetInfo previousPatchSetInfo(final PatchSet.Id patchSetId)
+      throws PatchSetInfoNotAvailableException {
+    PatchSet.Id id =
+        new PatchSet.Id(patchSetId.getParentKey(), patchSetId.get() - 1);
+    return patchSetInfoFactory.get(dbProvider.get(), id);
+  }
+
+  private static void updateCurrentPatchSet(final ReviewDb db,
+      final Change change, final PatchSetInfo psInfo)
+      throws OrmException {
+    db.changes().atomicUpdate(change.getId(), new AtomicUpdate<Change>() {
+      @Override
+      public Change update(Change c) {
+        c.setCurrentPatchSet(psInfo);
+        ChangeUtil.updated(c);
+        return c;
+      }
+    });
+  }
+}

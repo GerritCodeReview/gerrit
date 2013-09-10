970f2d77cbd6a04a5b508531f298ef78087b24c1
David Ostrovsky <david@ostrovsky.org>
2013-09-10 23:10:23 +0200

    Add REST endpoints for draft changes
    Change-Id: Ia30917267cffdeb4013e1415bed5e2a17ffdef6d

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/DeleteDraftChange.java
@@ -0,0 +1,73 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.extensions.webui.UiAction;
+import com.google.gerrit.reviewdb.client.Change.Status;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.ChangeUtil;
+import com.google.gerrit.server.change.DeleteDraftChange.Input;
+import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.patch.PatchSetInfoFactory;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import java.io.IOException;
+
+public class DeleteDraftChange implements RestModifyView<ChangeResource, Input>,
+    UiAction<ChangeResource> {
+  public static class Input {
+  }
+
+  private final Provider<ReviewDb> dbProvider;
+  private final GitRepositoryManager gitManager;
+  private final GitReferenceUpdated gitRefUpdated;
+
+  @Inject
+  public DeleteDraftChange(Provider<ReviewDb> dbProvider,
+      GitRepositoryManager gitManager,
+      GitReferenceUpdated gitRefUpdated,
+      PatchSetInfoFactory patchSetInfoFactory) {
+    this.dbProvider = dbProvider;
+    this.gitManager = gitManager;
+    this.gitRefUpdated = gitRefUpdated;
+  }
+
+  @Override
+  public Object apply(ChangeResource rsrc, Input input)
+      throws NoSuchChangeException, OrmException, IOException {
+    ChangeUtil.deleteDraftChange(rsrc.getChange().getId(),
+        gitManager, gitRefUpdated, dbProvider.get());
+    return Response.none();
+  }
+
+  @Override
+  public UiAction.Description getDescription(ChangeResource resource) {
+    try {
+      return new UiAction.Description()
+        .setTitle(String.format("Delete Draft Change %d",
+            resource.getChange().getChangeId()))
+        .setVisible(resource.getChange().getStatus() == Status.DRAFT
+            && resource.getControl().canDeleteDraft(dbProvider.get()));
+    } catch (OrmException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+}
--- gerrit-server/src/main/java/com/google/gerrit/server/change/Module.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Module.java
@@ -49,7 +49,9 @@ public class Module extends RestApiModule {
     get(CHANGE_KIND, "topic").to(GetTopic.class);
     put(CHANGE_KIND, "topic").to(PutTopic.class);
     delete(CHANGE_KIND, "topic").to(PutTopic.class);
+    delete(CHANGE_KIND, "draft").to(DeleteDraftChange.class);
     post(CHANGE_KIND, "abandon").to(Abandon.class);
+    post(CHANGE_KIND, "publish").to(Publish.CurrentRevision.class);
     post(CHANGE_KIND, "restore").to(Restore.class);
     post(CHANGE_KIND, "revert").to(Revert.class);
     post(CHANGE_KIND, "submit").to(Submit.CurrentRevision.class);
@@ -64,7 +66,9 @@ public class Module extends RestApiModule {
     child(CHANGE_KIND, "revisions").to(Revisions.class);
     post(REVISION_KIND, "cherrypick").to(CherryPick.class);
     get(REVISION_KIND, "commit").to(GetCommit.class);
+    delete(REVISION_KIND, "draft").to(DeleteDraftPatchSet.class);
     get(REVISION_KIND, "mergeable").to(Mergeable.class);
+    post(REVISION_KIND, "publish").to(Publish.class);
     get(REVISION_KIND, "related").to(GetRelated.class);
     get(REVISION_KIND, "review").to(GetReview.class);
     post(REVISION_KIND, "review").to(PostReview.class);

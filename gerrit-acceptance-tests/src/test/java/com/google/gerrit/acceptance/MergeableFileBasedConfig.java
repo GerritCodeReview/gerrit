feb5e7e98a070b49ff542aa2d0b13952f5a50398
David Ostrovsky <david@ostrovsky.org>
2013-09-15 19:22:09 +0200

    Acceptance tests framework: Allow tests to change Gerrit configuration
    Tests may want to contribute to the Gerrit configuration. Each method in
    the test class should be able to add or override one or more parameters
    of the Gerrit server configuration.
    
      public class UseGerritConfigAnnotationTest extends AbstractDaemonTest {
    
        @Inject
        @GerritServerConfig
        Config serverConfig;
    
        @Test
        @GerritConfig(name="x.y", value="z")
        public void testOne() {
          assertEquals("z", serverConfig.getString("x", null, "y"));
        }
    
        @Test
        @GerritConfigs({
            @GerritConfig(name="x.y", value="z"),
            @GerritConfig(name="a.b", value="c"),
        })
        public void testMultiple() {
          assertEquals("z", serverConfig.getString("x", null, "y"));
          assertEquals("c", serverConfig.getString("a", null, "b"));
        }
      }
    
    Change-Id: I506d9f86280f1b067d2083cb33726009ed72c218

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/MergeableFileBasedConfig.java
@@ -0,0 +1,60 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance;
+
+import com.google.common.collect.Lists;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
+
+import java.io.File;
+
+/**
+ * A file based Config that can merge another Config instance.
+ */
+public class MergeableFileBasedConfig extends FileBasedConfig {
+  public MergeableFileBasedConfig(File cfgLocation, FS fs) {
+    super(cfgLocation, fs);
+  }
+
+  /**
+   * Merge another Config into this Config.
+   *
+   * In case a configuration parameter exists both in this instance and in the
+   * merged instance then the value in this instance will simply replaced by
+   * the value from the merged instance.
+   *
+   * @param s Config to merge into this instance
+   */
+  public void merge(Config s) {
+    if (s == null) {
+      return;
+    }
+    for (String section : s.getSections()) {
+      for (String subsection : s.getSubsections(section)) {
+        for (String name : s.getNames(section, subsection)) {
+          setStringList(section, subsection, name, Lists.newArrayList(s
+              .getStringList(section, subsection, name)));
+        }
+      }
+
+      for (String name : s.getNames(section)) {
+        setStringList(section, null, name,
+            Lists.newArrayList(s.getStringList(section, null, name)));
+      }
+    }
+  }
+}

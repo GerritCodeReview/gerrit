07fb3e6b785ad62c961546b5925e16303768cda8
Doug Kelly <doug.kelly@garmin.com>
2013-09-17 10:22:22 -0500

    Update prettify to March 4, 2013 release
    Updated version of prettify: in addition to adding new syntax
    highlighters, it appears to run much better in IE versions >8.
    This would manifest as an unhandled JavaScript exception.
    
    Additionally, formatting appeared to not work well in IE standards
    mode and Safari because the @media selector was getting mangled
    by GWT.  By disabling the @media selector, the output was
    consistent.  GWT claims to have fixed this with 2.5.1, but did
    not seem to fix the issue in this case.
    
    Change-Id: Idcc095dac1007c6794e9f67fa12a92b01695d5cf

--- gerrit-prettify/src/main/java/com/google/gerrit/prettify/client/Resources.java
+++ gerrit-prettify/src/main/java/com/google/gerrit/prettify/client/Resources.java
@@ -33,18 +33,27 @@ interface Resources extends ClientBundle {
   TextResource core();
 
   @Source("lang-apollo.js") TextResource lang_apollo();
+  @Source("lang-basic.js") TextResource lang_basic();
   @Source("lang-clj.js") TextResource lang_clj();
   @Source("lang-css.js") TextResource lang_css();
   @Source("lang-dart.js") TextResource lang_dart();
+  @Source("lang-erlang.js") TextResource lang_erlang();
   @Source("lang-go.js") TextResource lang_go();
   @Source("lang-hs.js") TextResource lang_hs();
   @Source("lang-lisp.js") TextResource lang_lisp();
+  @Source("lang-llvm.js") TextResource lang_llvm();
   @Source("lang-lua.js") TextResource lang_lua();
+  @Source("lang-matlab.js") TextResource lang_matlab();
   @Source("lang-ml.js") TextResource lang_ml();
+  @Source("lang-mumps.js") TextResource lang_mumps();
   @Source("lang-n.js") TextResource lang_n();
+  @Source("lang-pascal.js") TextResource lang_pascal();
   @Source("lang-proto.js") TextResource lang_proto();
+  @Source("lang-r.js") TextResource lang_r();
+  @Source("lang-rd.js") TextResource lang_rd();
   @Source("lang-scala.js") TextResource lang_scala();
   @Source("lang-sql.js") TextResource lang_sql();
+  @Source("lang-tcl.js") TextResource lang_tcl();
   @Source("lang-tex.js") TextResource lang_tex();
   @Source("lang-vb.js") TextResource lang_vb();
   @Source("lang-vhdl.js") TextResource lang_vhdl();

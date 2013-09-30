a3284e28954f69da0bf04c430080dd283c9a53e5
David Ostrovsky <david@ostrovsky.org>
2013-09-30 21:36:09 +0200

    ChangeScreen2: mark patch sets with draft comments in revision list
    Bug: Issue 2072
    Change-Id: I10294b78cd3d832b5b64b146286f9368e12fa87b

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeInfo.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeInfo.java
@@ -203,6 +203,7 @@ public class ChangeInfo extends JavaScriptObject {
     public final native int _number() /*-{ return this._number; }-*/;
     public final native String name() /*-{ return this.name; }-*/;
     public final native boolean draft() /*-{ return this.draft || false; }-*/;
+    public final native boolean has_draft_comments() /*-{ return this.has_draft_comments || false; }-*/;
     public final native CommitInfo commit() /*-{ return this.commit; }-*/;
     public final native void set_commit(CommitInfo c) /*-{ this.commit = c; }-*/;
 

b98e6bbe2cd340daf755433a65703a101d3c26a3
Shawn Pearce <sop@google.com>
2013-10-07 17:16:44 -0700

    Remove unused import in AsciiDoctor
    Change-Id: I74edd35d7563e8feba5f962e9ab18318b141156b

--- lib/asciidoctor/java/AsciiDoctor.java
+++ lib/asciidoctor/java/AsciiDoctor.java
@@ -15,7 +15,6 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;

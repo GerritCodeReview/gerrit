3235d925547444a4cc72e7a1b1d50a6ad5fed91e
David Pursehouse <david.pursehouse@sonymobile.com>
2013-10-09 10:23:56 +0900

    Remove unused import in DocIndexer
    Change-Id: I767af51950957e16e7c7d9aac573c2a03cba1641

--- lib/asciidoctor/java/DocIndexer.java
+++ lib/asciidoctor/java/DocIndexer.java
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;

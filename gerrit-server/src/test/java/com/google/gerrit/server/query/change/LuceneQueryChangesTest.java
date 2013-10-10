fb5744b065a5749eb6fa4b7dd5fe91ac7c2d6e97
Dave Borowitz <dborowitz@google.com>
2013-10-10 11:33:01 -0700

    Extract an abstract test case for querying changes
    Add implementations for SQL and Lucene backends.
    
    Convert to JUnit 4 so we can use @Ignore on the test, as Buck's test
    runner does not skip abstract classes.
    
    Change-Id: I1a9b6ed53e27cab81ea88ff4e8fece8e999596e0

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/query/change/LuceneQueryChangesTest.java
@@ -0,0 +1,31 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.package com.google.gerrit.server.git;
+
+package com.google.gerrit.server.query.change;
+
+import com.google.gerrit.testutil.InMemoryModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import org.eclipse.jgit.lib.Config;
+
+public class LuceneQueryChangesTest extends AbstractQueryChangesTest {
+  protected Injector createInjector() {
+    Config cfg = InMemoryModule.newDefaultConfig();
+    cfg.setString("index", null, "type", "lucene");
+    cfg.setBoolean("index", "lucene", "testInmemory", true);
+    cfg.setInt("index", "lucene", "testVersion", 4);
+    return Guice.createInjector(new InMemoryModule(cfg));
+  }
+}

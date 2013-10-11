46f116a7fd9d08fb55fe6bf698c7005f1d9beb29
Edwin Kempin <edwin.kempin@sap.com>
2013-10-11 09:28:25 +0200

    ChangeScreen2: Display Related Changes as tab in a tabpanel
    This enables us to display further related changes in own tabs (e.g.
    changes that conflict with the current change).
    
    Change-Id: Iec9a7a8d74ad3bd8ab011017491d6dc6325c20e5
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/RelatedChanges.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/RelatedChanges.java
@@ -39,15 +39,16 @@ import com.google.gwt.user.client.EventListener;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.TabBar.Tab;
+import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.UIObject;
 import com.google.gwt.user.client.ui.impl.HyperlinkImpl;
 import com.google.gwtexpui.progress.client.ProgressBar;
 import com.google.gwtexpui.safehtml.client.SafeHtmlBuilder;
 
 class RelatedChanges extends Composite {
-  interface Binder extends UiBinder<HTMLPanel, RelatedChanges> {}
+  interface Binder extends UiBinder<TabPanel, RelatedChanges> {}
   private static final Binder uiBinder = GWT.create(Binder.class);
 
   private static final String OPEN;
@@ -95,8 +96,8 @@ class RelatedChanges extends Composite {
   private MyTable table;
   private boolean register;
 
+  @UiField TabPanel tabPanel;
   @UiField Style style;
-  @UiField Element header;
   @UiField Element none;
   @UiField ScrollPanel scroll;
   @UiField ProgressBar progress;
@@ -104,6 +105,11 @@ class RelatedChanges extends Composite {
 
   RelatedChanges() {
     initWidget(uiBinder.createAndBindUi(this));
+
+    tabPanel.selectTab(0);
+    Tab relatedChangesTab = tabPanel.getTabBar().getTab(0);
+    relatedChangesTab.setWordWrap(false);
+    ((Composite)relatedChangesTab).setTitle(Resources.C.relatedChangesTooltip());
   }
 
   void set(ChangeInfo info, final String revision) {
@@ -133,8 +139,7 @@ class RelatedChanges extends Composite {
   }
 
   void setMaxHeight(int height) {
-    int h = height - header.getOffsetHeight();
-    scroll.setHeight(h + "px");
+    scroll.setHeight(height + "px");
   }
 
   void registerKeys() {
@@ -153,7 +158,6 @@ class RelatedChanges extends Composite {
       }
     } else {
       progress.setVisible(false);
-      UIObject.setVisible(header, false);
       UIObject.setVisible(none, true);
     }
   }

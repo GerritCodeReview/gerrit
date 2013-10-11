1ccb3e17ce5d35d152f2e418266bf15a41559535
David Pursehouse <david.pursehouse@sonymobile.com>
2013-10-11 18:00:26 +0900

    Fix: unmappable character for encoding ASCII
    Having a unicode character in the source code seems to break
    diffy [1].
    
    Replace it with Character.toString(...).
    
    [1] https://gerrit-review.googlesource.com/#/c/50350/
    
    Change-Id: I1efeb334601e978d3a0a57be88702edbc19759ad

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/Gerrit.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/Gerrit.java
@@ -888,7 +888,9 @@ public class Gerrit implements EntryPoint {
     userSummaryPanel.setStyleName(RESOURCES.css().menuBarUserNamePanel());
     userSummaryPanel.add(l);
     userSummaryPanel.add(avatar);
-    userSummaryPanel.add(new InlineLabel(" ▾"));
+    // "BLACK DOWN-POINTING SMALL TRIANGLE"
+    String arrow = Character.toString((char)0x25be);
+    userSummaryPanel.add(new InlineLabel(" " + arrow));
     userPopup.addAutoHidePartner(userSummaryPanel.getElement());
     FocusPanel fp = new FocusPanel(userSummaryPanel);
     fp.setStyleName(RESOURCES.css().menuBarUserNameFocusPanel());

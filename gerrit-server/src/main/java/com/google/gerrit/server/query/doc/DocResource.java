7ec86c78af02a167eb0821c52881d92ae4c13e7f
Yuxuan 'fishy' Wang <fishywang@google.com>
2013-10-14 12:39:30 -0700

    REST endpoint for searching Gerrit documentation.
    The documentation of this endpoint is available in
    rest-api-docsearch[.txt|.html].
    
    The UI of showing the search result and doing the search will be in a
    separate change.
    
    Change-Id: Ifa4f5a7d576ada7f88a4fa1b765a38cba6d7e964

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/doc/DocResource.java
@@ -0,0 +1,53 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.doc;
+
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import com.google.gerrit.extensions.restapi.RestResource;
+import com.google.gerrit.extensions.restapi.RestResource.HasETag;
+import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.server.query.doc.QueryDocs.DocResult;
+import com.google.inject.Inject;
+import com.google.inject.TypeLiteral;
+
+import java.nio.charset.Charset;
+
+public class DocResource implements RestResource, HasETag {
+  public static final TypeLiteral<RestView<DocResource>> DOC_KIND =
+      new TypeLiteral<RestView<DocResource>>() {};
+
+  private final DocResult doc;
+
+  @Inject
+  public DocResource(DocResult doc) {
+    this.doc = doc;
+  }
+
+  protected DocResource(DocResource copy) {
+    this.doc = copy.doc;
+  }
+
+  public DocResult getDoc() {
+    return doc;
+  }
+
+  @Override
+  public String getETag() {
+    Hasher h = Hashing.md5().newHasher().putString(
+        doc.content, Charset.forName("UTF-8"));
+    return h.hash().toString();
+  }
+}

637abe7ee37c0c2e2f13cc75e5d243acd976cc28
David Ostrovsky <david@ostrovsky.org>
2013-10-23 21:10:39 +0200

    Bump SSHD version to 0.9.0.201311081
    With new Mina project release two backends are supported Mina &
    NIO2. Expose new NIO2 backend through new configuration option
    while preserving the Mina backend to be default.
    
    Remove the fake to turn off closing idle connections by using the
    highest timeout allowed by Apache SSD (~24 days).  That special
    treatment is not needed any more.
    
    Restrict `sshd.tcpKeepAlive` configuration option to be used only
    for Mina backend.
    
    Remove `sshd.reuseAddress` (it is per default true) as there is no
    obvious way how to set it.
    
    SSHD 0.9.0 incorrectly sends "Authenticated with..." message:
    
      davido@wizball:>ssh gerrit cookbook say-hello Dude
      Authenticated with partial success.
      Hello Dude!
    
    That bug is tracked under [1].  With that patch applied the problem
    with "Authenticated with partial success." is solved. Gerrit's
    Maven repository contains 0.9.0.201311081 which is 0.9.0 with [2]
    cherry-picked on top.
    
    [1] https://issues.apache.org/jira/browse/SSHD-254
    [2] https://github.com/apache/mina-sshd/pull/2
    
    Bug: issue 2003
    Change-Id: Ic352d7e6a9e6f8ab4122b2667ead24ac3d73f68a

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ShowCaches.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ShowCaches.java
@@ -30,8 +30,9 @@ import com.google.gerrit.sshd.SshDaemon;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
-import org.apache.mina.core.service.IoAcceptor;
-import org.apache.mina.core.session.IoSession;
+import org.apache.sshd.common.io.IoAcceptor;
+import org.apache.sshd.common.io.IoSession;
+import org.apache.sshd.common.io.mina.MinaSession;
 import org.apache.sshd.server.Environment;
 import org.eclipse.jgit.internal.storage.file.WindowCacheStatAccessor;
 import org.kohsuke.args4j.Option;
@@ -275,8 +276,12 @@ final class ShowCaches extends CacheCommand {
     long now = System.currentTimeMillis();
     Collection<IoSession> list = acceptor.getManagedSessions().values();
     long oldest = now;
+
     for (IoSession s : list) {
-      oldest = Math.min(oldest, s.getCreationTime());
+      if (s instanceof MinaSession) {
+        MinaSession minaSession = (MinaSession)s;
+        oldest = Math.min(oldest, minaSession.getSession().getCreationTime());
+      }
     }
 
     stdout.format(

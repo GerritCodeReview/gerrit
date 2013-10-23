c38bb74aeb57b2863924aea2f634c9de54e753ec
David Ostrovsky <david@ostrovsky.org>
2013-10-23 21:10:39 +0200

    Bump SSHD version to 0.9.0 and Mina to 2.0.7
    Remove the fake to turn off closing idle connections by using the highest
    timeout allowed by Apache SSD (~24 days).  That special treatment is not
    needed any more.
    
    Remove `sshd.tcpKeepAlive` configuration option, as there is no obvious way
    to set it.
    
    Remove `sshd.reuseAddress` (it is per default true) and there is no obvious
    way to set it.
    
    Some complication were to register close session listener, as `getCloseFuture`
    getter was removed from IoSession.  Derive GerritServerSession from
    ServerSession and expose the protected member `closeFuture` to the world.
    
    TODO: find a way to mimmic removed methods getCreationTime() & getLastIoTime()
    from IoSession (or alternatievely remove/replace the code that need it).
    
    UPSTREAM: SSHD incorrectly sends "Authenticated with..." message:
    
      davido@wizball:>ssh gerrit cookbook say-hello Dude
      Authenticated with partial success.
      Hello Dude!
    
    That bug seems to be tracked under [1].
    
    [1] https://github.com/apache/mina-sshd/pull/2
    
    Change-Id: Ic352d7e6a9e6f8ab4122b2667ead24ac3d73f68a

---
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/GerritServerSession.java
@@ -0,0 +1,33 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd;
+
+import org.apache.sshd.common.future.CloseFuture;
+import org.apache.sshd.common.io.IoSession;
+import org.apache.sshd.server.ServerFactoryManager;
+import org.apache.sshd.server.session.ServerSession;
+
+/* Expose protected closeFuture to register close session listeners */
+public class GerritServerSession extends ServerSession {
+
+  public GerritServerSession(ServerFactoryManager server,
+      IoSession ioSession) throws Exception {
+    super(server, ioSession);
+  }
+
+  public CloseFuture getCloseFuture() {
+    return closeFuture;
+  }
+}

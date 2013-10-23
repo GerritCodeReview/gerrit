e2bd2e1411741db55bb8e5281d08ab60e2fa823f
David Pursehouse <david.pursehouse@sonymobile.com>
2013-10-23 14:16:21 +0900

    Update jgit to 3.1.0.201310021548-r
    Change-Id: Ib1526cd97353933dee1b76de33239cbcb31107b0

--- tools/download_file.py
+++ tools/download_file.py
@@ -25,6 +25,7 @@ from zipfile import ZipFile, BadZipfile, LargeZipFile
 
 REPO_ROOTS = {
   'GERRIT': 'http://gerrit-maven.storage.googleapis.com',
+  'ECLIPSE': 'https://repo.eclipse.org/content/groups/releases',
   'MAVEN_CENTRAL': 'http://repo1.maven.org/maven2',
   'MAVEN_LOCAL': 'file://' + path.expanduser('~/.m2/repository'),
 }

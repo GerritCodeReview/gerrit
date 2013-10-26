010b32c6d39ab0cedb852552dc03156cfe0de3ad
David Ostrovsky <david@ostrovsky.org>
2013-10-26 09:55:15 +0200

    Buck: extend the tool chain to support unsigning JARs
    After switching to Eclipse Maven repository for pulling JGit lib, we
    have the problem that according to Eclipse release train the JARs have
    to be signed. That collids with our jgit patch for diff deserialization.
    
    To rectify that, add `unsign` parameter to maven_jar() function.
    
    Change-Id: Ib7bfa5d16f980a64b887d61a4b4ec325e6ffb0a1

--- tools/download_file.py
+++ tools/download_file.py
@@ -109,6 +109,7 @@ opts.add_option('-u', help='URL to download')
 opts.add_option('-v', help='expected content SHA-1')
 opts.add_option('-x', action='append', help='file to delete from ZIP')
 opts.add_option('--exclude_java_sources', action='store_true')
+opts.add_option('--unsign', action='store_true')
 args, _ = opts.parse_args()
 
 root_dir = args.o
@@ -162,6 +163,21 @@ if args.exclude_java_sources:
     try:
       for n in zf.namelist():
         if n.endswith('.java'):
+          exclude.append(n)
+    finally:
+      zf.close()
+  except (BadZipfile, LargeZipFile) as err:
+    print("error opening %s: %s"  % (cache_ent, err), file=stderr)
+    exit(1)
+
+if args.unsign:
+  try:
+    zf = ZipFile(cache_ent, 'r')
+    try:
+      for n in zf.namelist():
+        if n.endswith('.RSA') \
+              or n.endswith('.SF') \
+              or n.endswith('.LIST'):
           exclude.append(n)
     finally:
       zf.close()

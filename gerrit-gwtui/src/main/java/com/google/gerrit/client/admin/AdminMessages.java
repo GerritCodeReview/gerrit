2dcd3b3d5ffac4316e11e31b992f7e68fca61fbb
Edwin Kempin <edwin.kempin@sap.com>
2013-11-15 16:00:00 +0100

    Support to edit project plugin configuration parameters in UI
    Plugins can define project configuration parameters that project
    owners are allowed to edit through the ProjectInfoScreen. This makes
    it much easier for project owners to change the project specific plugin
    configuration.
    
    With this change only string parameters without inheritance are
    supported. Support for inheritance and other parameter types is added
    by follow-up changes.
    
    Change-Id: Iab3bc2ea2eb6dd6d0508f0049f43f7b9b21f58c8
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/admin/AdminMessages.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/admin/AdminMessages.java
@@ -28,4 +28,5 @@ public interface AdminMessages extends Messages {
 
   String effectiveMaxObjectSizeLimit(String effectiveMaxObjectSizeLimit);
   String globalMaxObjectSizeLimit(String globalMaxObjectSizeLimit);
+  String pluginProjectOptionsTitle(String pluginName);
 }

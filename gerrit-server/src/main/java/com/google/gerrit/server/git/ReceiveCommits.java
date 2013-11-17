aed9baf960a441bf3c701c59bfbc7ca051701c1a
Edwin Kempin <edwin.kempin@sap.com>
2013-11-17 19:04:51 +0100

    Validate plugin list parameter on direct push
    Change-Id: Iff09f51480588b3b65aab4478b9e2f21ebaafb3e
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -108,6 +108,7 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.assistedinject.Assisted;
 
+import org.eclipse.jgit.api.errors.InvalidConfigurationException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.BatchRefUpdate;
@@ -870,8 +871,14 @@ public class ReceiveCommits {
                     case LONG:
                       configEntry.onUpdate(project, Long.parseLong(value));
                       break;
-                    case STRING:
                     case LIST:
+                      if (!configEntry.getSupportedValues().contains(value)) {
+                        throw new InvalidConfigurationException("The value '"
+                            + value + "' is invalid for parameter '"
+                            + e.getExportName() + "' of plugin '"
+                            + e.getPluginName() + "'");
+                      }
+                    case STRING:
                     default:
                       configEntry.onUpdate(project, value);
                   }

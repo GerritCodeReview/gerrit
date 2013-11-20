4badfef9cbd2e9c5c0afa48618a970c2257549ba
Shawn Pearce <sop@google.com>
2013-11-20 15:41:14 -0800

    SideBySide2: Disable blinking cursor
    Some users are bothered by the cursor blinking in the review
    pane. Instead of a blinking box that overlays a character use a
    single character underline that does not blink. This is more "old
    school vi" and can be less distruptive when reading code.
    
    Change-Id: I3791bd472da2975288acfb1195959ec7e5f207bc
    (cherry picked from commit e4bf42fa4a41ccc6d81a2a944e4756016ab51d56)

--- gerrit-gwtui/src/main/java/net/codemirror/lib/Configuration.java
+++ gerrit-gwtui/src/main/java/net/codemirror/lib/Configuration.java
@@ -33,6 +33,9 @@ public class Configuration extends JavaScriptObject {
   public final native Configuration set(String name, int val)
   /*-{ this[name] = val; return this; }-*/;
 
+  public final native Configuration set(String name, double val)
+  /*-{ this[name] = val; return this; }-*/;
+
   public final native Configuration set(String name, boolean val)
   /*-{ this[name] = val; return this; }-*/;
 

cfd5d254b88f64a1a9a6434aed82d7da78443127
Edwin Kempin <edwin.kempin@sap.com>
2013-11-22 16:53:09 +0100

    Add 'is:mergeable' operator to find changes without merge conflict
    With this new search operator contributors can easily find changes
    that need to be rebased.
    
    Reviewers might find this new search operator useful to filter out
    changes that are not mergeable.
    
    This change introduces a new index schema version and reindexing the
    changes is required.
    
    Change-Id: Iba8447f643d44bc13721302e54d2059d79fee37f
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/IsMergeablePredicate.java
@@ -0,0 +1,42 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.change;
+
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.index.ChangeField;
+import com.google.gerrit.server.index.IndexPredicate;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Provider;
+
+class IsMergeablePredicate extends IndexPredicate<ChangeData> {
+  private final Provider<ReviewDb> dbProvider;
+
+  IsMergeablePredicate(Provider<ReviewDb> dbProvider) {
+    super(ChangeField.MERGEABLE, "1");
+    this.dbProvider = dbProvider;
+  }
+
+  @Override
+  public boolean match(ChangeData object) throws OrmException {
+    Change c = object.change(dbProvider);
+    return c != null && c.isMergeable();
+  }
+
+  @Override
+  public int getCost() {
+    return 1;
+  }
+}

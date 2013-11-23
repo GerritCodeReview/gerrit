6c35f07dfdd888ddd55d30f1c33b92097d15b870
Shawn Pearce <sop@google.com>
2013-11-22 20:16:44 -0800

    SideBySide2: Batch adjustments of padding widgets
    Right after the view opens a number of deferred tasks fire, one for
    each padding region and comment widget to rescale the padding size.
    Every time these update a CM3 controlled property the CM3 instance
    has to reflow its DOM, which is relatively expensive in the browser.
    
    Batch the deferred work inside of the SideBySide2 instance as a list
    of Runnables.  If any tasks are scheduled, schedule only one deferred
    task to run all batched items inside of an operation() wrapper. This
    allows CM3 to batch up the updates and reflow its DOM only once per
    side, rather than once per padding widget.
    
    In one example file (some version of ChangeScreen2.java) with a
    handful of diff chunks and test comments this batching process saves
    40.9 ms during the initial display of the file.
    
    The exact amount of time saved depends on the complexity of the file,
    the delta chunks and the number of comments, but this was a fairly
    typical trivial example.  Real world files may see an even larger
    savings.
    
    Change-Id: Ib84c67d2f999fb7df5731ca3389a79b53da45a01

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/SideBySide2.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/SideBySide2.java
@@ -132,6 +132,7 @@ public class SideBySide2 extends Screen {
   private KeyCommandSet keysAction;
   private KeyCommandSet keysComment;
   private List<HandlerRegistration> handlers;
+  private List<Runnable> deferred;
 
   public SideBySide2(
       PatchSet.Id base,
@@ -603,6 +604,7 @@ public class SideBySide2 extends Screen {
   }
 
   CommentBox addCommentBox(CommentInfo info, CommentBox box) {
+    box.setParent(this);
     diffTable.add(box);
     DisplaySide side = box.getSide();
     CodeMirror cm = getCmFromSide(side);
@@ -641,7 +643,6 @@ public class SideBySide2 extends Screen {
     LineWidget boxWidget = cm.addLineWidget(line, box.getElement(), config);
     box.setPaddingManager(manager);
     box.setSelfWidgetWrapper(new PaddingWidgetWrapper(boxWidget, box.getElement()));
-    box.setParent(this);
     if (otherChunk == null) {
       box.setDiffChunkInfo(myChunk);
     }
@@ -944,9 +945,9 @@ public class SideBySide2 extends Screen {
          * key (or j/k) is held down. Performance on Chrome is fine
          * without the deferral.
          */
-        Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+        defer(new Runnable() {
           @Override
-          public void execute() {
+          public void run() {
             LineHandle handle = cm.getLineHandleVisualStart(
                 cm.getCursor("end").getLine());
             if (cm.hasActiveLine() && cm.getActiveLine().equals(handle)) {
@@ -1181,6 +1182,33 @@ public class SideBySide2 extends Screen {
     return (index + diffChunks.size()) % diffChunks.size();
   }
 
+  void defer(Runnable thunk) {
+    if (deferred == null) {
+      final ArrayList<Runnable> list = new ArrayList<Runnable>();
+      deferred = list;
+      Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+        @Override
+        public void execute() {
+          deferred = null;
+          cmA.operation(new Runnable() {
+            @Override
+            public void run() {
+              cmB.operation(new Runnable() {
+                @Override
+                public void run() {
+                  for (Runnable thunk : list) {
+                    thunk.run();
+                  }
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+    deferred.add(thunk);
+  }
+
   void resizePaddingOnOtherSide(DisplaySide mySide, int line) {
     CodeMirror cm = getCmFromSide(mySide);
     LineHandle handle = cm.getLineHandle(line);
@@ -1236,9 +1264,9 @@ public class SideBySide2 extends Screen {
           lineActiveBoxMap.get(handle).resizePaddingWidget();
         }
         if (linePaddingOnOtherSideMap.containsKey(handle)) {
-          Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+          defer(new Runnable() {
             @Override
-            public void execute() {
+            public void run() {
               resizePaddingOnOtherSide(side, instance.getLineNumber(handle));
             }
           });

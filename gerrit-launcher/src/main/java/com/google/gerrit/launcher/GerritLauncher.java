61a41973e21a0f707893bb33433fe0402d3292f2
Gustaf Lundh <gustaf.lundh@sonymobile.com>
2013-11-26 10:05:25 +0100

    Add 'reindex' to the pgm help output.
    Change-Id: I237c738dbbc6baf3e73c2de408e04bf2ea982e79

--- gerrit-launcher/src/main/java/com/google/gerrit/launcher/GerritLauncher.java
+++ gerrit-launcher/src/main/java/com/google/gerrit/launcher/GerritLauncher.java
@@ -67,6 +67,7 @@ public final class GerritLauncher {
       System.err.println();
       System.err.println("The most commonly used commands are:");
       System.err.println("  init           Initialize a Gerrit installation");
+      System.err.println("  reindex        Rebuild the secondary index");
       System.err.println("  daemon         Run the Gerrit network daemons");
       System.err.println("  gsql           Run the interactive query console");
       System.err.println("  version        Display the build version number");

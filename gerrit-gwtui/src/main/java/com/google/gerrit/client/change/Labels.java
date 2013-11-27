c1f28eb699406b1b60816e6b82b20946d85f833f
David Ostrovsky <david@ostrovsky.org>
2013-11-27 08:23:43 +0100

    ChangeScreen2: Allow to see if a reviewer can't vote on a label
    Old change screen had grey styled cells in approval table for labels a
    reviewer hadn't right to vote on. The new change screen uses chips to
    present the reviewers name and therefore this valuable information is lost.
    
    Add it back in form of tooltip with the list of non votable labels
    for each reviewer in the form:
    
      "Non votable: Foo, Bar, Baz"
    
    Change-Id: I0f101e43c625bfe1f181b46574c6677d930a5c19

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Labels.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Labels.java
@@ -25,6 +25,7 @@ import com.google.gerrit.client.changes.Util;
 import com.google.gerrit.client.rpc.GerritCallback;
 import com.google.gerrit.client.rpc.Natives;
 import com.google.gerrit.common.PageLinks;
+import com.google.gerrit.common.data.ApprovalDetail;
 import com.google.gerrit.common.data.LabelValue;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gwt.core.client.JavaScriptObject;
@@ -176,7 +177,7 @@ class Labels extends Grid {
       }
       html.append(val).append(" ");
       html.append(formatUserList(style, m.get(v),
-          Collections.<Integer> emptySet()));
+          Collections.<Integer> emptySet(), null));
       html.closeSpan();
     }
     return html.toBlockWidget();
@@ -222,7 +223,8 @@ class Labels extends Grid {
 
   static SafeHtml formatUserList(ChangeScreen2.Style style,
       Collection<? extends AccountInfo> in,
-      Set<Integer> removable) {
+      Set<Integer> removable,
+      Map<Integer, ApprovalDetail> votable) {
     List<AccountInfo> users = new ArrayList<AccountInfo>(in);
     Collections.sort(users, new Comparator<AccountInfo>() {
       @Override
@@ -261,9 +263,24 @@ class Labels extends Grid {
         name = Integer.toString(ai._account_id());
       }
 
+      String nonVotableCategories = "";
+      if (votable != null) {
+        Set<String> nonVotable = votable.get(ai._account_id()).nonVotable();
+        if (!nonVotable.isEmpty()) {
+          StringBuilder sb = new StringBuilder("Non votable: ");
+          for (Iterator<String> it = nonVotable.iterator(); it.hasNext();) {
+            sb.append(it.next());
+            if (it.hasNext()) {
+              sb.append(", ");
+            }
+          }
+          nonVotableCategories = sb.toString();
+        }
+      }
       html.openSpan()
           .setAttribute("role", "listitem")
           .setAttribute(DATA_ID, ai._account_id())
+          .setAttribute("title", nonVotableCategories)
           .setStyleName(style.label_user());
       if (img != null) {
         html.openElement("img")

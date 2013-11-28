54ecc8136fa9689c8c4b0987c31283fd3c1ba1af
David Pursehouse <david.pursehouse@sonymobile.com>
2013-11-28 15:56:51 +0900

    Add --branch option on the review ssh command
    The review command allows the patch set to be specified by the commit's
    sha1 revision.  It is possible that the revision does not identify a
    unique change if the same sha1 has been pushed to more than one project
    or to more than one branch of the same project.  This causes the review
    to fail with an error.
    
    The results can be narrowed down to a specific project by using the
    --project option, but it will still fail if the sha1 is on more than
    one branch in the same project.
    
    Add a new --branch option that allows to narrow down the results by
    branch.
    
    Bug: Issue 1752
    Change-Id: I555dc9de1b092e7562f5d99c4c60df589935a47f

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ReviewCommand.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ReviewCommand.java
@@ -91,6 +91,9 @@ public class ReviewCommand extends SshCommand {
   @Option(name = "--project", aliases = "-p", usage = "project containing the specified patch set(s)")
   private ProjectControl projectControl;
 
+  @Option(name = "--branch", aliases = "-b", usage = "branch containing the specified patch set(s)")
+  private String branch;
+
   @Option(name = "--message", aliases = "-m", usage = "cover message to publish on change(s)", metaVar = "MESSAGE")
   private String changeComment;
 
@@ -354,7 +357,7 @@ public class ReviewCommand extends SshCommand {
       final Set<PatchSet> matches = new HashSet<PatchSet>();
       for (final PatchSet ps : patches) {
         final Change change = db.changes().get(ps.getId().getParentKey());
-        if (inProject(change)) {
+        if (inProject(change) && inBranch(change)) {
           matches.add(ps);
         }
       }
@@ -382,11 +385,15 @@ public class ReviewCommand extends SshCommand {
       if (patchSet == null) {
         throw error("\"" + patchIdentity + "\" no such patch set");
       }
-      if (projectControl != null) {
+      if (projectControl != null || branch != null) {
         final Change change = db.changes().get(patchSetId.getParentKey());
-        if (!inProject(change)) {
+        if (projectControl != null && !inProject(change)) {
           throw error("change " + change.getId() + " not in project "
               + projectControl.getProject().getName());
+        }
+        if (branch != null && !inBranch(change)) {
+          throw error("change " + change.getId() + " not in branch "
+              + change.getDest().toString());
         }
       }
       return patchSet;
@@ -401,6 +408,13 @@ public class ReviewCommand extends SshCommand {
       return true;
     }
     return projectControl.getProject().getNameKey().equals(change.getProject());
+  }
+
+  private boolean inBranch(final Change change) {
+    if (branch == null) {
+      return true;
+    }
+    return change.getDest().toString().equals(branch);
   }
 
   @Override

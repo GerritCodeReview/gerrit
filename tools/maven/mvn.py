34d5c9f4c1f4f81bf74ba71fc0972967714c3762
David Ostrovsky <david@ostrovsky.org>
2013-11-30 16:24:36 +0100

    Buck: Adjust to unflattened directory structure for python libraries
    Since this Buck's commit 59759043f66de17140b423a7f11a2d8827b4cbbf the whole
    unflattened subtree is used.
    
    Change-Id: I4ec930a452d4867add3f5dd46430d3184c2a7277

--- tools/maven/mvn.py
+++ tools/maven/mvn.py
@@ -16,8 +16,10 @@
 from __future__ import print_function
 from optparse import OptionParser
 from os import path
+import sys
 from sys import stderr
-from util import check_output
+
+from tools.util import check_output
 
 opts = OptionParser()
 opts.add_option('--repository', help='maven repository id')

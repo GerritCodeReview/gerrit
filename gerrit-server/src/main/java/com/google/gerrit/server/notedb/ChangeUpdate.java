a0daabd028ac811c28a07b33501195c14cc9bf97
Dave Borowitz <dborowitz@google.com>
2013-12-05 12:22:40 -0800

    RFC: Store PatchSetApprovals in a git-notes-based database
    Add a new ref namespace refs/changes/*/*/meta for storing the state of
    a Gerrit change in Git. Information about the change is mostly stored
    in commit messages, the entire history of which are read at load time
    to populate information about the change. Eventually, information tied
    to a particular patch set will be stored in a git note referring to
    the patch set's commit.
    
    This data format only allows fast access to data on a per-change
    basis. Lookups by other keys (e.g. votes by user) will depend on the
    secondary index.
    
    The commit message format is simple:
    
      Update patch set 1
    
      <optional cover message>
    
      Gerrit-Patch-Set: 1
      Gerrit-Vote: code-review=+1
    
    The subject line is machine-generated and need not be descriptive. The
    cover message is user-provided (not implemented in this change). At
    least the Gerrit-Patch-Set and Gerrit-Account footer lines are
    required for the time being.
    
    The author of the commit is the end-user performing the update, using
    their full name and email address. The committer is the same user, but
    includes the Gerrit account ID in the email address for
    disambiguation. (Eventually, we may decide that the (name, email)
    combination is enough to uniquely identify a user, but for now we
    still require the account ID.)
    
    On the code side, reading and writing changes from this ref is
    implemented with two VersionedMetaData classes:
    
    ChangeNotes implements the read operation. When a ChangeNotes is
    loaded, it traverses the entire history of the ref and populates the
    object with all relevant information. Traversing the entire history
    should be a fast operation on most changes, which typically have
    roughly tens of operations.
    
    ChangeUpdate implements the write operation. Unlike the traditional
    database, where operations are organized around read-modify-write
    operations on various entities spread across tables, a ChangeUpdate
    encapsulates a set of updates that can be applied atomically. Because
    each commit has a single author, timestamp, and patch set, these are
    logically restricted to operations performed by a single user on a
    single patch set. This corresponds better to the typical usage of the
    database, where a single end-user is performing an operation on a
    single change via a REST endpoint.
    
    ChangeNotes and ChangeUpdate are separate VersionedMetaData subclasses
    for a few reasons, mostly technical. First, it should not be necessary
    in all cases to slurp all change data via ChangeNotes before preparing
    a ChangeUpdate. Second, due to subtleties like LabelType sorting, it
    would be tricky to make ChangeNotes mutable in such a way that the
    result of reading it after one or more mutations would be the same as
    if that change were read freshly from the git data. Keeping
    ChangeNotes immutable and ChangeUpdate separate makes it less tempting
    to assume this behavior.
    
    This change implements only reading/writing for
    ChangeNotes/ChangeUpdate; they are not yet used in any read/write code
    paths in the server.
    
    Change-Id: Id28978a92f3b197c8b5be6e2db0c1e3923f2211e

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeUpdate.java
@@ -0,0 +1,200 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.notedb;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.gerrit.server.notedb.ChangeNoteUtil.FOOTER_LABEL;
+import static com.google.gerrit.server.notedb.ChangeNoteUtil.FOOTER_PATCH_SET;
+import static com.google.gerrit.server.notedb.ChangeNoteUtil.GERRIT_PLACEHOLDER_HOST;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Maps;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.common.data.LabelType;
+import com.google.gerrit.common.data.LabelTypes;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.MetaDataUpdate;
+import com.google.gerrit.server.git.VersionedMetaData;
+import com.google.gerrit.server.project.NoSuchProjectException;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.util.LabelVote;
+import com.google.gerrit.server.util.TimeUtil;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.CommitBuilder;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.FooterKey;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Map;
+
+/**
+ * A single delta to apply atomically to a change.
+ * <p>
+ * This delta becomes a single commit on the notes branch, so there are
+ * limitations on the set of modifications that can be handled in a single
+ * update. In particular, there is a single author and timestamp for each
+ * update.
+ */
+public class ChangeUpdate extends VersionedMetaData {
+  @Singleton
+  public static class Factory {
+    private final GitRepositoryManager repoManager;
+    private final MetaDataUpdate.User updateFactory;
+    private final ProjectCache projectCache;
+    private final Provider<IdentifiedUser> user;
+
+    @Inject
+    Factory(
+        GitRepositoryManager repoManager,
+        MetaDataUpdate.User updateFactory,
+        ProjectCache projectCache,
+        Provider<IdentifiedUser> user) {
+      this.repoManager = repoManager;
+      this.updateFactory = updateFactory;
+      this.projectCache = projectCache;
+      this.user = user;
+    }
+
+    public ChangeUpdate load(Change change)
+        throws NoSuchProjectException, ConfigInvalidException, IOException {
+      return load(change, TimeUtil.nowTs());
+    }
+
+    public ChangeUpdate load(Change change, Timestamp when)
+        throws NoSuchProjectException, ConfigInvalidException, IOException {
+      Repository repo;
+      try {
+        repo = repoManager.openRepository(change.getProject());
+      } catch (RepositoryNotFoundException e) {
+        throw new NoSuchProjectException(change.getProject(), e);
+      }
+      try {
+        return new ChangeUpdate(
+            updateFactory, repo,
+            projectCache.get(change.getProject()).getLabelTypes(),
+            change, user.get().getAccountId(), when);
+      } finally {
+        repo.close();
+      }
+    }
+  }
+
+  private final MetaDataUpdate.User updateFactory;
+  private final LabelTypes labelTypes;
+  private final Change change;
+  private final Account.Id accountId;
+  private final Timestamp when;
+  private final Map<String, Short> approvals;
+  private String subject;
+
+  @VisibleForTesting
+  ChangeUpdate(Repository repo, LabelTypes labelTypes, Change change,
+      Account.Id accountId, @Nullable Timestamp when)
+      throws ConfigInvalidException, IOException {
+    this(null, repo, labelTypes, change, accountId, when);
+  }
+
+  private ChangeUpdate(@Nullable MetaDataUpdate.User updateFactory,
+      Repository repo, LabelTypes labelTypes, Change change,
+      Account.Id accountId, @Nullable Timestamp when)
+      throws ConfigInvalidException, IOException {
+    this.updateFactory = updateFactory;
+    this.labelTypes = labelTypes;
+    this.change = change;
+    this.accountId = accountId;
+    this.when = when;
+    this.approvals = Maps.newTreeMap(labelTypes.nameComparator());
+    load(repo);
+  }
+
+  public void putApproval(String label, short value) {
+    approvals.put(label.toLowerCase(), value);
+  }
+
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  public RevCommit commit() throws IOException {
+    return commit(checkNotNull(updateFactory, "MetaDataUpdate.Factory")
+        .create(change.getProject()));
+  }
+
+  @Override
+  public RevCommit commit(MetaDataUpdate md) throws IOException {
+    md.setAllowEmpty(true);
+    CommitBuilder cb = md.getCommitBuilder();
+    cb.setCommitter(new PersonIdent(
+        cb.getAuthor().getName(),
+        accountId.get() + "@" + GERRIT_PLACEHOLDER_HOST,
+        when != null ? when : cb.getCommitter().getWhen(),
+        cb.getCommitter().getTimeZone()));
+    if (when != null) {
+      md.getCommitBuilder().setAuthor(new PersonIdent(
+          md.getCommitBuilder().getAuthor(), when));
+    }
+    return super.commit(md);
+  }
+
+  @Override
+  protected String getRefName() {
+    return ChangeNoteUtil.changeRefName(change.getId());
+  }
+
+  @Override
+  protected void onSave(CommitBuilder commit) {
+    if (approvals.isEmpty()) {
+      return;
+    }
+    int psId = change.currentPatchSetId().get();
+    StringBuilder msg = new StringBuilder();
+    if (subject != null) {
+      msg.append(subject);
+    } else {
+      msg.append("Update patch set ").append(psId);
+    }
+    msg.append("\n\n");
+    addFooter(msg, FOOTER_PATCH_SET, psId);
+    for (Map.Entry<String, Short> e : approvals.entrySet()) {
+      LabelType lt = labelTypes.byLabel(e.getKey());
+      if (lt != null) {
+        addFooter(msg, FOOTER_LABEL,
+            new LabelVote(lt.getName(), e.getValue()).formatWithEquals());
+      }
+    }
+    commit.setMessage(msg.toString());
+  }
+
+  private static void addFooter(StringBuilder sb, FooterKey footer,
+      Object value) {
+    sb.append(footer.getName()).append(": ").append(value).append('\n');
+  }
+
+  @Override
+  protected void onLoad() throws IOException, ConfigInvalidException {
+    // Do nothing; just reads current revision.
+  }
+}

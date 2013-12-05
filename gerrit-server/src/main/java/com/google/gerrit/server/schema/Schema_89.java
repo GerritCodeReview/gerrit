342781e1818a4c2c7c5c19548ae3128cfab3c89c
Shawn Pearce <sop@google.com>
2013-12-04 23:25:59 -0800

    Schema_89: Drop indexes from patch_set_approvals
    88->89 removes columns from patch_set_approvals that have
    indexes on them. Drop the indexes first during migrateData.
    
    Change-Id: I14e41f309cc8d580a91ed277db623a8b0fcecbb5

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_89.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_89.java
@@ -14,12 +14,41 @@
 
 package com.google.gerrit.server.schema;
 
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gwtorm.jdbc.JdbcSchema;
+import com.google.gwtorm.schema.sql.DialectMySQL;
+import com.google.gwtorm.schema.sql.SqlDialect;
+import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Schema_89 extends SchemaVersion {
   @Inject
   Schema_89(Provider<Schema_88> prior) {
     super(prior);
+  }
+
+  @Override
+  protected void migrateData(ReviewDb db, UpdateUI ui) throws OrmException,
+      SQLException {
+    SqlDialect dialect = ((JdbcSchema) db).getDialect();
+    Statement stmt = ((JdbcSchema) db).getConnection().createStatement();
+    try {
+      for (String name : ImmutableList.of(
+          "patch_set_approvals_openByUser",
+          "patch_set_approvals_closedByU")) {
+        if (dialect instanceof DialectMySQL) {
+          stmt.executeUpdate("DROP INDEX " + name + " ON patch_set_approvals");
+        } else {
+          stmt.executeUpdate("DROP INDEX " + name);
+        }
+      }
+    } finally {
+      stmt.close();
+    }
   }
 }

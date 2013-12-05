ec508deb8145b3ef58f5d3c60d09afc7423ecf61
Dave Borowitz <dborowitz@google.com>
2013-12-05 12:22:40 -0800

    Store PatchSetApprovals in a git-notes-based database
    Add a new ref namespace refs/changes/*/*/meta for storing the state of
    a Gerrit change in Git. Information about the change is mostly stored
    in commit messages, the entire history of which are read at load time
    to populate information about the change. Eventually, information tied
    to a particular patch set will be stored in a git note referring to
    the patch set's commit.
    
    This data format only allows fast access to data on a per-change
    basis. Lookups by other keys (e.g. votes by user) will depend on the
    secondary index.
    
    The commit message format is simple:
    
      Update patch set 1
    
      <optional cover message>
    
      Gerrit-Patch-Set: 1
      Gerrit-Vote: code-review=+1
    
    The subject line is machine-generated and need not be descriptive. The
    cover message is user-provided (not implemented in this change). At
    least the Gerrit-Patch-Set and Gerrit-Account footer lines are
    required for the time being.
    
    The author of the commit is the end-user performing the update, using
    their full name and email address. The committer is the same user, but
    includes the Gerrit account ID in the email address for
    disambiguation. (Eventually, we may decide that the (name, email)
    combination is enough to uniquely identify a user, but for now we
    still require the account ID.)
    
    On the code side, reading and writing changes from this ref is
    implemented with two VersionedMetaData classes:
    
    ChangeNotes implements the read operation. When a ChangeNotes is
    loaded, it traverses the entire history of the ref and populates the
    object with all relevant information. Traversing the entire history
    should be a fast operation on most changes, which typically have
    roughly tens of operations.
    
    ChangeUpdate implements the write operation. Unlike the traditional
    database, where operations are organized around read-modify-write
    operations on various entities spread across tables, a ChangeUpdate
    encapsulates a set of updates that can be applied atomically. Because
    each commit has a single author, timestamp, and patch set, these are
    logically restricted to operations performed by a single user on a
    single patch set. This corresponds better to the typical usage of the
    database, where a single end-user is performing an operation on a
    single change via a REST endpoint.
    
    ChangeNotes and ChangeUpdate are separate VersionedMetaData subclasses
    for a few reasons, mostly technical. First, it should not be necessary
    in all cases to slurp all change data via ChangeNotes before preparing
    a ChangeUpdate. Second, due to subtleties like LabelType sorting, it
    would be tricky to make ChangeNotes mutable in such a way that the
    result of reading it after one or more mutations would be the same as
    if that change were read freshly from the git data. Keeping
    ChangeNotes immutable and ChangeUpdate separate makes it less tempting
    to assume this behavior.
    
    This change implements only reading/writing for
    ChangeNotes/ChangeUpdate; they are not yet used in any read/write code
    paths in the server.
    
    Change-Id: Id28978a92f3b197c8b5be6e2db0c1e3923f2211e

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNoteUtil.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.notedb;
+
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.RefNames;
+
+import org.eclipse.jgit.revwalk.FooterKey;
+
+public class ChangeNoteUtil {
+  static final String GERRIT_PLACEHOLDER_HOST = "gerrit";
+
+  static final FooterKey FOOTER_LABEL = new FooterKey("Label");
+  static final FooterKey FOOTER_PATCH_SET = new FooterKey("Patch-Set");
+
+  public static String changeRefName(Change.Id id) {
+    StringBuilder r = new StringBuilder();
+    r.append(RefNames.REFS_CHANGES);
+    int n = id.get();
+    int m = n % 100;
+    if (m < 10) {
+      r.append('0');
+    }
+    r.append(m);
+    r.append('/');
+    r.append(n);
+    r.append("/meta");
+    return r.toString();
+  }
+
+  private ChangeNoteUtil() {
+  }
+}

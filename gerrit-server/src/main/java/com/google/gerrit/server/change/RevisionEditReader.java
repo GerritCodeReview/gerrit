43f7fa96595e8c424030fab37f07631ca3a5f444
David Ostrovsky <david@ostrovsky.org>
2013-12-11 01:30:37 +0100

    InlineEdit: Lay out the groundwork for the inline edit
    This change lays out the groundwork for the inline edit feature.
    Revision edits are created on top of patch sets and are virtual patch
    sets that don't exist in the database.
    
    They are stored on the refs:
    
    refs/edits/UU/UUUU/CC/CCCC/P
    
    for edits by user UUUU on top of patch set P of CCCC, where change numbers
    and user account numbers are sharded in the same way as for refs/changes.
    
    This change creates handlers to read, modify, publish and delete revision
    edits.
    
    This change is based on previous work of Dave Borowitz and Martin Fick.
    
    Bug: issue 2338
    Change-Id: I048d32b9423486347d38a3865d346df6549b8773

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/RevisionEditReader.java
@@ -0,0 +1,81 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.RevisionEdit;
+import com.google.gerrit.server.project.InvalidChangeOperationException;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RevisionEditReader {
+
+  private final GitRepositoryManager gitManager;
+  private final Provider<CurrentUser> currentUser;
+
+  @Inject
+  public RevisionEditReader(GitRepositoryManager gitManager,
+      Provider<CurrentUser> currentUser) {
+    this.gitManager = gitManager;
+    this.currentUser = currentUser;
+  }
+
+  public Map<PatchSet.Id, PatchSet> read(Change change)
+      throws AuthException, InvalidChangeOperationException,
+      NoSuchChangeException, IOException {
+    if (!currentUser.get().isIdentifiedUser()) {
+      throw new AuthException("edits only available to authenticated users");
+    }
+
+    final Repository repo;
+    try {
+      repo = gitManager.openRepository(change.getProject());
+    } catch (RepositoryNotFoundException e) {
+      throw new NoSuchChangeException(change.getId(), e);
+    }
+
+    try {
+      IdentifiedUser me = (IdentifiedUser)currentUser.get();
+      Map<String, Ref> names =
+          repo.getRefDatabase().getRefs(
+              RevisionEdit.getChangeEditsRef(me, change.getId()).toString());
+      Map<PatchSet.Id, PatchSet> result = new HashMap<>(names.size());
+      for (Map.Entry<String, Ref> entry : names.entrySet()) {
+        PatchSet.Id psid = new PatchSet.Id(change.getId(),
+            Integer.valueOf(entry.getKey()));
+        RevisionEdit edit = new RevisionEdit(me, psid, entry.getValue());
+        result.put(psid, edit.getPatchSet(repo));
+      }
+      return Collections.unmodifiableMap(result);
+    } finally {
+      repo.close();
+    }
+  }
+}

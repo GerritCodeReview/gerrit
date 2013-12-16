79c05d102a99fb2d0df1c7cd4dbf258900643417
David Ostrovsky <david@ostrovsky.org>
2013-12-17 00:01:02 +0100

    InlineEdit: Reuse edit message box to edit file content
    Change-Id: Ie29042c97a3f53ac8e7496509b80f29c54b2c793

--- gerrit-common/src/main/java/com/google/gerrit/common/PageLinks.java
+++ gerrit-common/src/main/java/com/google/gerrit/common/PageLinks.java
@@ -67,7 +67,7 @@ public class PageLinks {
   }
 
   public static String toChange(final PatchSet.Id ps) {
-    return "/c/" + ps.getParentKey() + "/" + ps.get();
+    return "/c/" + ps.getParentKey() + "/" + ps.getId();
   }
 
   public static String toProject(final Project.NameKey p) {

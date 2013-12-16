8978b2f1900e5ed32e144d5cfda6c393cd00fa59
David Ostrovsky <david@ostrovsky.org>
2013-12-17 00:01:02 +0100

    InlineEdit: Reuse edit message box to edit file content
    Change-Id: Ie29042c97a3f53ac8e7496509b80f29c54b2c793

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PutContent.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PutContent.java
@@ -48,7 +48,6 @@ public class PutContent implements RestModifyView<FileResource, Input> {
       throws AuthException, ResourceNotFoundException,
       ResourceConflictException, OrmException, IOException {
     RevisionResource rev = rsrc.getRevision();
-    rev.checkEdit();
     Change change = rev.getChange();
     PatchSet ps = rev.getPatchSet();
     String file = rsrc.getPath();

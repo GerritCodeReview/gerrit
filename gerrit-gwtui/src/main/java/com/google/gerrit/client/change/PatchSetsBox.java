0d5760e14f741ad6b8afff56cf2413bad9e96ca8
David Ostrovsky <david@ostrovsky.org>
2013-12-18 00:55:48 +0100

    InlineEdit: Implement SBS2 integration
    Change-Id: I18c185f0209c9d9c8243a44d16030ed9d9c4b85a

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/PatchSetsBox.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/PatchSetsBox.java
@@ -207,7 +207,7 @@ class PatchSetsBox extends Composite {
         .closeSpan()
         .append(' ');
     }
-    sb.append(r._number());
+    sb.append(r.id());
     sb.closeTd();
 
     sb.openTd()

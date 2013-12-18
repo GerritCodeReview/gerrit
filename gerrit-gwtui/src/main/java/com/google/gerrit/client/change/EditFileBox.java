276f64de761df728521c7242c0ac8586e975e141
David Ostrovsky <david@ostrovsky.org>
2013-12-19 00:10:39 +0100

    InlineEdit: Support adding new files to the change
    This change adds support for adding new files to the change: new file to
    the repository and existing files in the repository, but missing in the
    change.
    
    New button "Add..." appears now in edit mode on the header of the file
    table. In the edit file popup dialog, text edit control is added to enter
    the file name to add.
    
    After entering the file name, new "Load from Git" button is clicked to
    retrieve the content of the file to add. After content is set, the content
    of the new added file can be amended as usually.
    
    This change makes it possible to create the change content in browser:
    
      git commit -m "Git in browser" --allow-empty
      git push review
    
    Now empty commit is created in Gerrit and with Edit Mode/Add... feature
    the content can be populated on the fly in browser.
    
    Change-Id: Id3bf7d08533f20fee13e514933c55a990c722ffd

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/EditFileBox.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/EditFileBox.java
@@ -17,30 +17,78 @@ package com.google.gerrit.client.change;
 import com.google.gerrit.client.Gerrit;
 import com.google.gerrit.client.VoidResult;
 import com.google.gerrit.client.changes.ChangeFileApi;
+import com.google.gerrit.client.rpc.GerritCallback;
+import com.google.gerrit.client.ui.TextBoxChangeListener;
 import com.google.gerrit.common.PageLinks;
 import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwtexpui.globalkey.client.NpTextArea;
+import com.google.gwtexpui.globalkey.client.NpTextBox;
 
-class EditFileBox extends EditMessageBox {
+class EditFileBox extends Composite {
+  interface Binder extends UiBinder<HTMLPanel, EditFileBox> {}
+  private static final Binder uiBinder = GWT.create(Binder.class);
 
-  private PatchSet.Id id;
-  private String file;
+  final private PatchSet.Id id;
+  final private String fileName;
+  final private String fileContent;
+
+  @UiField NpTextBox file;
+  @UiField NpTextArea content;
+  @UiField Button save;
+  @UiField Button reload;
+  @UiField Button cancel;
 
   EditFileBox(
       PatchSet.Id id,
-      String content,
-      String file) {
-    super(null, null, content);
+      String fileC,
+      String fileName) {
     this.id = id;
-    this.file = file;
+    this.fileName = fileName;
+    this.fileContent = fileC;
+    initWidget(uiBinder.createAndBindUi(this));
+    new TextBoxChangeListener(file) {
+      public void onTextChanged(String newText) {
+        reload.setEnabled(!file.getText().trim().isEmpty());
+      }
+    };
+    new TextBoxChangeListener(content) {
+      public void onTextChanged(String newText) {
+        save.setEnabled(!file.getText().trim().isEmpty()
+            && !newText.trim().equals(fileContent));
+      }
+    };
   }
 
   @Override
+  protected void onLoad() {
+    file.setText(fileName);
+    file.setEnabled(fileName.isEmpty());
+    reload.setEnabled(!fileName.isEmpty());
+    content.setText(fileContent);
+    save.setEnabled(false);
+    Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+      @Override
+      public void execute() {
+        content.setFocus(true);
+      }});
+  }
+
   @UiHandler("save")
   void onSave(ClickEvent e) {
-    ChangeFileApi.putContent(id, file, message.getText().trim(),
+    ChangeFileApi.putContent(id, file.getText(), content.getText().trim(),
         new AsyncCallback<VoidResult>() {
           @Override
           public void onSuccess(VoidResult result) {
@@ -52,5 +100,30 @@ class EditFileBox extends EditMessageBox {
           public void onFailure(Throwable caught) {
           }
         });
+  }
+
+  @UiHandler("reload")
+  void onRefresh(ClickEvent e) {
+    ChangeFileApi.getContent(id, file.getText().trim(),
+        new GerritCallback<String>() {
+          @Override
+          public void onSuccess(String result) {
+            content.setText(result);
+          }
+        });
+  }
+
+  @UiHandler("cancel")
+  void onCancel(ClickEvent e) {
+    hide();
+  }
+
+  protected void hide() {
+    for (Widget w = getParent(); w != null; w = w.getParent()) {
+      if (w instanceof PopupPanel) {
+        ((PopupPanel) w).hide();
+        break;
+      }
+    }
   }
 }

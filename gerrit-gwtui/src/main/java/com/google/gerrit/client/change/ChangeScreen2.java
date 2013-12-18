976db5a9817330d72bbe7469653da8d9fd4d0c0a
David Ostrovsky <david@ostrovsky.org>
2013-12-19 00:10:39 +0100

    InlineEdit: Support adding new files to the change
    This change adds support for adding new files to the change: new file to
    the repository and existing files in the repository, but missing in the
    change.
    
    New button "Add..." appears now in edit mode on the header of the file
    table. In the edit file popup dialog, text edit control is added to enter
    the file name to add.
    
    After entering the file name, new "Load from Git" button is clicked to
    retrieve the content of the file to add. After content is set, the content
    of the new added file can be amended as usually.
    
    This change makes it possible to create the change content in browser:
    
      git commit -m "Git in browser" --allow-empty
      git push review
    
    Now empty commit is created in Gerrit and with Edit Mode/Add... feature
    the content can be populated on the fly in browser.
    
    Change-Id: Id3bf7d08533f20fee13e514933c55a990c722ffd

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ChangeScreen2.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ChangeScreen2.java
@@ -174,6 +174,7 @@ public class ChangeScreen2 extends Screen {
   @UiField Button openAll;
   @UiField Button editMode;
   @UiField Button reviewMode;
+  @UiField Button addFile;
   @UiField Button expandAll;
   @UiField Button collapseAll;
   @UiField Button editMessage;
@@ -184,6 +185,7 @@ public class ChangeScreen2 extends Screen {
   private IncludedInAction includedInAction;
   private PatchSetsAction patchSetsAction;
   private DownloadAction downloadAction;
+  private EditFileAction editFileAction;
 
   public ChangeScreen2(Change.Id changeId, String base, String revision, boolean openReplyBox) {
     this.changeId = changeId;
@@ -409,6 +411,13 @@ public class ChangeScreen2 extends Screen {
                 null)));
   }
 
+  private void initEditFileAction(ChangeInfo info, String revision2) {
+    RevisionInfo rev = info.revision(revision);
+    editFileAction = new EditFileAction(
+        new PatchSet.Id(changeId, rev._number(), rev.edit()),
+        "", "", style, editMessage, reply);
+  }
+
   private void initEditMode(ChangeInfo info) {
     if (Gerrit.isSignedIn() && info.status() == Status.NEW) {
       editMode.setVisible(fileTableMode == FileTable.Mode.REVIEW);
@@ -549,6 +558,7 @@ public class ChangeScreen2 extends Screen {
     fileTableMode = FileTable.Mode.EDIT;
     refreshFileTable();
     editMode.setVisible(false);
+    addFile.setVisible(true);
     reviewMode.setVisible(true);
   }
 
@@ -557,7 +567,13 @@ public class ChangeScreen2 extends Screen {
     fileTableMode = FileTable.Mode.REVIEW;
     refreshFileTable();
     editMode.setVisible(true);
+    addFile.setVisible(false);
     reviewMode.setVisible(false);
+  }
+
+  @UiHandler("addFile")
+  void onAddFile(ClickEvent e) {
+    editFileAction.onEdit();
   }
 
   private void refreshFileTable() {
@@ -864,6 +880,7 @@ public class ChangeScreen2 extends Screen {
     initProjectLinks(info);
     initBranchLink(info);
     initEditMode(info);
+    initEditFileAction(info, revision);
     actions.display(info, revision);
 
     star.setValue(info.starred());

cbf35fb9670a670923ee35cfd2c07f45f4d2b9c3
Dave Borowitz <dborowitz@google.com>
2013-12-18 13:12:44 -0800

    Move more functionality from elsewhere into ApprovalsUtil
    The commonality of these methods is they will have a different
    implementation when approvals are read from an eventual notedb, so
    it makes sense to keep them located together.
    
    Change-Id: I5de63a4210f29863274111964480fb1f5720ca60

--- gerrit-server/src/main/java/com/google/gerrit/server/git/SubmitStrategy.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/SubmitStrategy.java
@@ -19,6 +19,7 @@ import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSetApproval;
 import com.google.gerrit.reviewdb.client.Project.SubmitType;
 import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.ApprovalsUtil;
 import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.index.ChangeIndexer;
 
@@ -55,6 +56,7 @@ public abstract class SubmitStrategy {
     protected final RevFlag canMergeFlag;
     protected final Set<RevCommit> alreadyAccepted;
     protected final Branch.NameKey destBranch;
+    protected final ApprovalsUtil approvalsUtil;
     protected final MergeUtil mergeUtil;
     protected final ChangeIndexer indexer;
     protected final MergeSorter mergeSorter;
@@ -63,8 +65,8 @@ public abstract class SubmitStrategy {
         final PersonIdent myIdent, final ReviewDb db, final Repository repo,
         final RevWalk rw, final ObjectInserter inserter,
         final RevFlag canMergeFlag, final Set<RevCommit> alreadyAccepted,
-        final Branch.NameKey destBranch, final MergeUtil mergeUtil,
-        final ChangeIndexer indexer) {
+        final Branch.NameKey destBranch, final ApprovalsUtil approvalsUtil,
+        final MergeUtil mergeUtil, final ChangeIndexer indexer) {
       this.identifiedUserFactory = identifiedUserFactory;
       this.myIdent = myIdent;
       this.db = db;
@@ -75,6 +77,7 @@ public abstract class SubmitStrategy {
       this.canMergeFlag = canMergeFlag;
       this.alreadyAccepted = alreadyAccepted;
       this.destBranch = destBranch;
+      this.approvalsUtil = approvalsUtil;
       this.mergeUtil = mergeUtil;
       this.indexer = indexer;
       this.mergeSorter = new MergeSorter(rw, alreadyAccepted, canMergeFlag);

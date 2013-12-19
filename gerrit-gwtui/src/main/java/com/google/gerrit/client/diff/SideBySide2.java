08ff593446bc658868f61749bcdc2667ba9a1004
Shawn Pearce <sop@google.com>
2013-12-19 08:29:23 -0800

    SideBySide2: Clarify diffChunkNav direction
    Instead of abusing a boolean true as previous and false as next,
    define an enum with symbols PREV and NEXT, making it more clear
    which direction navigation will go in the file.
    
    Change-Id: I61f2279aa1239ce88727235d88e125fc0cec1ee9

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/SideBySide2.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/SideBySide2.java
@@ -353,7 +353,7 @@ public class SideBySide2 extends Screen {
         .on("Enter", toggleOpenBox(cm))
         .on("'c'", insertNewDraft(cm))
         .on("N", maybeNextVimSearch(cm))
-        .on("P", diffChunkNav(cm, true))
+        .on("P", diffChunkNav(cm, Direction.PREV))
         .on("Shift-O", openClosePublished(cm))
         .on("Shift-Left", moveCursorToSide(cm, DisplaySide.A))
         .on("Shift-Right", moveCursorToSide(cm, DisplaySide.B))
@@ -1467,13 +1467,13 @@ public class SideBySide2 extends Screen {
         if (cm.hasVimSearchHighlight()) {
           CodeMirror.handleVimKey(cm, "n");
         } else {
-          diffChunkNav(cm, false).run();
+          diffChunkNav(cm, Direction.NEXT).run();
         }
       }
     };
   }
 
-  private Runnable diffChunkNav(final CodeMirror cm, final boolean prev) {
+  private Runnable diffChunkNav(final CodeMirror cm, final Direction dir) {
     return new Runnable() {
       @Override
       public void run() {
@@ -1483,9 +1483,9 @@ public class SideBySide2 extends Screen {
                 new DiffChunkInfo(getSideFromCm(cm), line, 0, false),
                 getDiffChunkComparator());
         if (res < 0) {
-          res = -res - (prev ? 1 : 2);
+          res = -res - (dir == Direction.PREV ? 1 : 2);
         }
-        res = res + (prev ? -1 : 1);
+        res = res + (dir == Direction.PREV ? -1 : 1);
         if (res < 0 || diffChunks.size() <= res) {
           return;
         }
@@ -1493,7 +1493,7 @@ public class SideBySide2 extends Screen {
         DiffChunkInfo lookUp = diffChunks.get(res);
         // If edit, skip the deletion chunk and set focus on the insertion one.
         if (lookUp.isEdit() && lookUp.getSide() == DisplaySide.A) {
-          res = res + (prev ? -1 : 1);
+          res = res + (dir == Direction.PREV ? -1 : 1);
           if (res < 0 || diffChunks.size() <= res) {
             return;
           }

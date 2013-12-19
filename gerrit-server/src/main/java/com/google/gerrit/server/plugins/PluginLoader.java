d83a68cccb4afebd6a621771201f38a758add98a
Dariusz Luksza <dariusz@luksza.org>
2013-12-19 15:18:32 +0100

    Remove support for standalone JavaScript plugins
    Since Gerrit now can autoregister `static/init.js` and serve content of
    `static/` directory within `$site_path/plugins/$folder_name/` we don't
    need to support standalone JavaScript plugins.
    
    Same effect (and more) be achived now with previously mentioned
    autoregistration.
    
    Change-Id: I8e145e3f094e53a8ad9f6c08707915247f5f7633
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginLoader.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginLoader.java
@@ -154,8 +154,7 @@ public class PluginLoader implements LifecycleListener {
     checkRemoteInstall();
 
     String fileName = originalName;
-    if (!(fileName.endsWith(".jar") || fileName.endsWith(".js") || fileName
-        .endsWith(".zip"))) {
+    if (!(fileName.endsWith(".jar") || fileName.endsWith(".zip"))) {
       fileName += ".jar";
     }
     File tmp = asTemp(in, ".next_" + fileName + "_", ".tmp", pluginsDir);
@@ -527,10 +526,8 @@ public class PluginLoader implements LifecycleListener {
         in.close();
       }
       return loadJarPlugin(name, srcPlugin, snapshot, tmp);
-    } else if (isJsPlugin(pluginName)) {
-      return loadJsPlugin(name, srcPlugin, snapshot);
     } else if (isContainerPlugin(srcPlugin)) {
-      return loadJsPlugin(name, srcPlugin, snapshot);
+      return loadJsContainerPlugin(name, srcPlugin, snapshot);
     } else {
       throw new InvalidPluginException(String.format(
           "Unsupported plugin type: %s", srcPlugin.getName()));
@@ -594,8 +591,10 @@ public class PluginLoader implements LifecycleListener {
     }
   }
 
-  private Plugin loadJsPlugin(String name, File srcJar, FileSnapshot snapshot) {
-    return new JsPlugin(name, srcJar, pluginUserFactory.create(name), snapshot);
+  private Plugin loadJsContainerPlugin(String name, File srcJar,
+      FileSnapshot snapshot) {
+    return new JsContainerPlugin(name, srcJar, pluginUserFactory.create(name),
+        snapshot);
   }
 
   private static ClassLoader parentFor(Plugin.ApiType type)
@@ -710,7 +709,7 @@ public class PluginLoader implements LifecycleListener {
       public boolean accept(File pathname) {
         String n = pathname.getName();
         boolean isContainerPlugin = isContainerPlugin(pathname);
-        return (isJarPlugin(n) || isJsPlugin(n) || isContainerPlugin)
+        return (isJarPlugin(n) || isContainerPlugin)
             && !n.startsWith(".last_")
             && !n.startsWith(".next_")
             && (pathname.isFile() || isContainerPlugin);
@@ -746,9 +745,6 @@ public class PluginLoader implements LifecycleListener {
         jarFile.close();
       }
     }
-    if (isJsPlugin(fileName)) {
-      return fileName.substring(0, fileName.length() - 3);
-    }
     return null;
   }
 
@@ -764,10 +760,6 @@ public class PluginLoader implements LifecycleListener {
 
   private static boolean isJarPlugin(String name) {
     return isPlugin(name, "jar") || isPlugin(name, "zip");
-  }
-
-  private static boolean isJsPlugin(String name) {
-    return isPlugin(name, "js");
   }
 
   private static boolean isContainerPlugin(File file) {

498b19f419e97c16f6e641a6d01c2d1593fa1004
Dave Borowitz <dborowitz@google.com>
2014-01-08 11:51:31 -0800

    DiffTable: remove unused import
    Change-Id: Ia67b812bee54e25b4af46aca0b1fb0a113d7ba35

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/DiffTable.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/DiffTable.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.client.diff;
 
-import com.google.gerrit.client.Gerrit;
 import com.google.gerrit.client.changes.ChangeInfo.RevisionInfo;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gwt.core.client.GWT;

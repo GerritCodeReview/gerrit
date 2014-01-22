1ed399650de6267c4c9bafcb2434bdafb39ff396
Shawn Pearce <sop@google.com>
2014-01-21 16:33:53 -0800

    Require additional review for lib/* changes

---
+++ rules.pl
@@ -0,0 +1,12 @@
+submit_rule(submit(CR, V, L)) :-
+  gerrit:commit_delta('^lib/'),
+  !,
+  base(CR, V),
+  gerrit:max_with_block(-1, 1, 'lib', L).
+
+submit_rule(submit(CR, V)) :-
+  base(CR, V).
+
+base(CR, V) :-
+  gerrit:max_with_block(-2, 2, 'Code-Review', CR),
+  gerrit:max_with_block(-1, 1, 'Verified', V).

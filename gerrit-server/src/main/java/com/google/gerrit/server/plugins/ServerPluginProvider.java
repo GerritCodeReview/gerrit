7d5f5c7ae29ba19918987faa6bb7fbb99ccdbb5a
Luca Milanesio <luca.milanesio@gmail.com>
2014-01-29 23:53:00 +0000

    Introduce support for externally loaded plugins
    Allows developers to use an external means for loading
    plugins into Gerrit. By declaring a ServerPluginProvider
    with a @Listen annotation it is possible to support another
    non-jar plugin file format to be loaded into Gerrit.
    
    One example use of externally managed plugins are
    the ones that support Groovy and Scala as scripting
    language for loading a Gerrit plugin.
    
    From now on it will be possible to extend Gerrit
    for multiple languages and formats without change or
    impact on JsPlugins and existing plugin loading functionalities.
    
    As additional value, from now the ability to extend Gerrit
    with other external files format (e.g. Directories,
    other files types and scripting languages) would not impact anymore
    the Plugin enums, associated switch/case or the complexity of
    Gerrit PluginLoader.
    
    Change-Id: I6a64a336f30a18409079e2d4df48d577cd462d6d

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/ServerPluginProvider.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.server.plugins;
+
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+import com.google.gerrit.server.PluginUser;
+
+import org.eclipse.jgit.internal.storage.file.FileSnapshot;
+
+import java.io.File;
+
+/**
+ * Provider of Server plugin from an external file
+ *
+ * Allows to load a plugin from an external file or
+ * directory by declaring the ability to handle it
+ */
+@ExtensionPoint
+public interface ServerPluginProvider {
+  /**
+   * Declares the availability to manage an external file or directory
+   *
+   * @param srcFile the external file or directory
+   * @return true if file or directory can be loaded into a Server Plugin
+   */
+  boolean handles(File srcFile);
+
+  /**
+   * Returns the plugin name of an external file or directory
+   *
+   * @param srcFile external file or directory
+   * @return plugin name
+   */
+  String getPluginName(File srcFile);
+
+  /**
+   * Loads an external file or directory into a Server plugin.
+   *
+   * @param name plugin name
+   * @param srcFile external file or directory
+   * @param pluginUser Gerrit user for interacting with plugins
+   * @param snapshot snapshot of the external file
+   * @return the Server Plugin
+   */
+  ServerPlugin get(String name, File srcFile, PluginUser pluginUser,
+      FileSnapshot snapshot);
+}

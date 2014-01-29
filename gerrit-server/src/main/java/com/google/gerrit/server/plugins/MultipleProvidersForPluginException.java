d705996673ea58f52541750bd86ab0f01710154b
Luca Milanesio <luca.milanesio@gmail.com>
2014-01-29 23:53:00 +0000

    Introduce support for externally loaded plugins
    Allows developers to use an external means for loading
    plugins into Gerrit. By declaring a ServerPluginProvider
    with a @Listen annotation it is possible to support another
    non-jar plugin file format to be loaded into Gerrit.
    
    One example use of externally managed plugins are
    the ones that support Groovy and Scala as scripting
    language for loading a Gerrit plugin.
    
    From now on it will be possible to extend Gerrit
    for multiple languages and formats without change or
    impact on JsPlugins and existing plugin loading functionalities.
    
    As additional value, from now the ability to extend Gerrit
    with other external files format (e.g. Directories,
    other files types and scripting languages) would not impact anymore
    the Plugin enums, associated switch/case or the complexity of
    Gerrit PluginLoader.
    
    Change-Id: I6a64a336f30a18409079e2d4df48d577cd462d6d

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/MultipleProvidersForPluginException.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.plugins;
+
+import java.io.File;
+
+class MultipleProvidersForPluginException extends
+    IllegalArgumentException {
+  private static final long serialVersionUID = 8593375014154751135L;
+
+  MultipleProvidersForPluginException(File pluginSrcFile, Iterable<ServerPluginProvider> providersHandlers) {
+    super(pluginSrcFile.getAbsolutePath()
+        + " is claimed to be handled by more than one plugin provider: "
+        + providersHandlers);
+  }
+}

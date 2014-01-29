44dac02b85d5552a9d099d3d3595fba721987d80
Luca Milanesio <luca.milanesio@gmail.com>
2014-01-29 23:53:00 +0000

    Decouple plugins from their "jar" external form
    Until now all the server-side plugins have been associated
    to a single jar file in the /plugin directory.
    
    As first step to allow different forms of plugins
    (e.g. script files, directories or anything else that
    can provide classes and resources) we need to de-couple
    the underlying Jar file from the server side plugin.
    
    We introduce the concept of "plugin-scanner" as the interface
    to scan the external form to discover:
    - plugin classes
    - plugin resources
    - plugin meta-data (i.e. Manifest)
    
    Change-Id: I769595a030545a5f272f453c3cf435b74719e1e7

--- gerrit-server/src/main/java/com/google/gerrit/server/plugins/JarScanner.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/JarScanner.java
@@ -38,6 +38,7 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
@@ -48,8 +49,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
-public class JarScanner {
+public class JarScanner implements PluginContentScanner {
   private static final int SKIP_ALL = ClassReader.SKIP_CODE
       | ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES;
   private static final Function<ClassData, ExtensionMetaData> CLASS_DATA_TO_EXTENSION_META_DATA =
@@ -61,27 +63,19 @@ public class JarScanner {
         }
       };
 
-  public static class ExtensionMetaData {
-    private final String className;
-    private final String annotationValue;
+  private final JarFile jarFile;
 
-    private ExtensionMetaData(String className, String annotationValue) {
-      this.className = className;
-      this.annotationValue = annotationValue;
-    }
-
-    public String getAnnotationValue() {
-      return annotationValue;
-    }
-
-    public String getClassName() {
-      return className;
+  public JarScanner(File srcFile) throws InvalidPluginException {
+    try {
+      this.jarFile = new JarFile(srcFile);
+    } catch (IOException e) {
+      throw new InvalidPluginException("Cannot scan plugin file " + srcFile, e);
     }
   }
 
-  public static Map<Class<? extends Annotation>, Iterable<ExtensionMetaData>> scan(
-      JarFile jarFile, String pluginName,
-      Iterable<Class<? extends Annotation>> annotations)
+  @Override
+  public Map<Class<? extends Annotation>, Iterable<ExtensionMetaData>> scan(
+      String pluginName, Iterable<Class<? extends Annotation>> annotations)
       throws InvalidPluginException {
     Set<String> descriptors = Sets.newHashSet();
     Multimap<String, JarScanner.ClassData> rawMap = ArrayListMultimap.create();
@@ -261,5 +255,42 @@ public class JarScanner {
     @Override
     public void visitEnd() {
     }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <T> Optional<T> getResource(String resourcePath,
+      Class<? extends T> resourceClass) {
+    if (isJarResource(resourceClass)) {
+      return (Optional<T>) Optional.fromNullable(jarFile
+          .getJarEntry(resourcePath));
+    } else {
+      return Optional.absent();
+    }
+  }
+
+  private <T> boolean isJarResource(Class<? extends T> resourceClass) {
+    return resourceClass.equals(JarEntry.class);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <T> Enumeration<T> resources(Class<? extends T> resourceClass) {
+    if (isJarResource(resourceClass)) {
+      return (Enumeration<T>) jarFile.entries();
+    } else {
+      return Collections.emptyEnumeration();
+    }
+  }
+
+  @Override
+  public Optional<InputStream> getResourceInputStream(String resourcePath)
+      throws IOException {
+    return Optional.fromNullable(jarFile.getInputStream(jarFile.getEntry(resourcePath)));
+  }
+
+  @Override
+  public Manifest getManifest() throws IOException {
+    return jarFile.getManifest();
   }
 }

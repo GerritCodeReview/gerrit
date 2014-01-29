4635d03702c497f4c3daa3a68481051cb16ce91f
Luca Milanesio <luca.milanesio@gmail.com>
2014-01-29 23:53:00 +0000

    Introduce support for externally loaded plugins
    Allows developers to use an external means for loading
    plugins into Gerrit. By declaring a ServerPluginProvider
    with a @Listen annotation it is possible to support another
    non-jar plugin file format to be loaded into Gerrit.
    
    One example use of externally managed plugins are
    the ones that support Groovy and Scala as scripting
    language for loading a Gerrit plugin.
    
    From now on it will be possible to extend Gerrit
    for multiple languages and formats without change or
    impact on JsPlugins and existing plugin loading functionalities.
    
    As additional value, from now the ability to extend Gerrit
    with other external files format (e.g. Directories,
    other files types and scripting languages) would not impact anymore
    the Plugin enums, associated switch/case or the complexity of
    Gerrit PluginLoader.
    
    Change-Id: I6a64a336f30a18409079e2d4df48d577cd462d6d

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/ServerPluginProvider.java
@@ -0,0 +1,107 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.plugins;
+
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+import com.google.gerrit.server.PluginUser;
+
+import org.eclipse.jgit.internal.storage.file.FileSnapshot;
+
+import java.io.File;
+
+/**
+ * Provider of one Server plugin from one external file
+ *
+ * Allows to load one plugin from one external file or
+ * one directory by declaring the ability to handle it.
+ *
+ * In order to load multiple files into a single plugin,
+ * group them into a directory tree and then load the directory
+ * root as a single plugin.
+ */
+@ExtensionPoint
+public interface ServerPluginProvider {
+
+  /**
+   * Descriptor of the Plugin that ServerPluginProvider has to load.
+   */
+  public class PluginDescription {
+    public final PluginUser user;
+    public final String canonicalUrl;
+    public final File dataDir;
+
+    /**
+     * Creates a new PluginDescription for ServerPluginProvider.
+     *
+     * @param pluginUser Gerrit user for interacting with plugins
+     * @param pluginCanonicalWebUrl plugin root Web URL
+     * @param pluginDataDir directory for plugin data
+     */
+    public PluginDescription(PluginUser user, String canonicalUrl, File dataDir) {
+      this.user = user;
+      this.canonicalUrl = canonicalUrl;
+      this.dataDir = dataDir;
+    }
+  }
+
+  /**
+   * Declares the availability to manage an external file or directory
+   *
+   * @param srcFile the external file or directory
+   * @return true if file or directory can be loaded into a Server Plugin
+   */
+  boolean handles(File srcFile);
+
+  /**
+   * Returns the plugin name of an external file or directory
+   *
+   * Should be called only if {@link #handles(File) handles(srcFile)}
+   * returns true and thus srcFile is a supported plugin format.
+   * An IllegalArgumentException is thrown otherwise as srcFile
+   * is not a valid file format for extracting its plugin name.
+   *
+   * @param srcFile external file or directory
+   * @return plugin name
+   */
+  String getPluginName(File srcFile);
+
+  /**
+   * Loads an external file or directory into a Server plugin.
+   *
+   * Should be called only if {@link #handles(File) handles(srcFile)}
+   * returns true and thus srcFile is a supported plugin format.
+   * An IllegalArgumentException is thrown otherwise as srcFile
+   * is not a valid file format for extracting its plugin name.
+   *
+   * @param srcFile external file or directory
+   * @param snapshot snapshot of the external file
+   * @param pluginDescription descriptor of the ServerPlugin to load
+   * @throws InvalidPluginException if plugin is supposed to be handled
+   *         but cannot be loaded for any other reason
+   */
+  ServerPlugin get(File srcFile, FileSnapshot snapshot,
+      PluginDescription pluginDescriptor) throws InvalidPluginException;
+
+  /**
+   * Returns the plugin name of this provider.
+   *
+   * Allows to identify which plugin provided the current ServerPluginProvider
+   * by returning the plugin name. Helpful for troubleshooting plugin loading
+   * problems.
+   *
+   * @return plugin name of this provider
+   */
+  String getProviderPluginName();
+}

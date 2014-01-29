281b4d87f45bcc68be55f5324b1cf3218a053a92
Luca Milanesio <luca.milanesio@gmail.com>
2014-01-29 23:53:00 +0000

    Refactor JsPlugin to support generic scripting plugins.
    Extract the generic concept of a "scripting" plugin from
    existing implementation of a JsPlugin.
    
    This allows future subclassing of ScriptingPlugin for
    start supporting other scripting languages and make then
    pluggable into Gerrit just by registering their factories
    from PluginModule.
    
    This refactoring improves the extensibility of Gerrit
    code-base for multiple scripting without having to impact
    existing plugin loading functionalities and without having
    to touch multiple times the Plugin enums and associated
    switch/case.
    
    Change-Id: Ifca14d8974d3bdb313cfce10d39e688b8ddf7626

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/ScriptingPluginFactory.java
@@ -0,0 +1,66 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.server.plugins;
+
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.PluginUser;
+import com.google.gerrit.server.plugins.ScriptingPlugin.Factory;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jgit.internal.storage.file.FileSnapshot;
+
+import java.io.File;
+
+@Singleton
+public class ScriptingPluginFactory implements ScriptingPlugin.Factory {
+
+  private final DynamicSet<ScriptingPlugin.Factory> scriptingFactories;
+
+  @Inject
+  public ScriptingPluginFactory(DynamicSet<ScriptingPlugin.Factory> sf) {
+    this.scriptingFactories = sf;
+  }
+
+  @Override
+  public ScriptingPlugin get(String name, File srcFile, PluginUser pluginUser,
+      FileSnapshot snapshot) {
+
+    String srcFilename = srcFile.getName();
+    String scrFileExtension = StringUtils.substringAfterLast(srcFilename, ".");
+    ScriptingPlugin.Factory scriptingFactory = getFactory(scrFileExtension);
+    if (scriptingFactory == null) {
+      throw new IllegalArgumentException("Script file "
+          + srcFile.getAbsolutePath()
+          + " is not a known scripting language for a Gerrit plugin");
+    }
+
+    return scriptingFactory.get(name, srcFile, pluginUser, snapshot);
+  }
+
+  private Factory getFactory(String srcFileExtension) {
+    for (ScriptingPlugin.Factory scriptingFactory : scriptingFactories) {
+      if (scriptingFactory.isMyScriptExtension(srcFileExtension)) {
+        return scriptingFactory;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public boolean isMyScriptExtension(String scriptExtension) {
+    return getFactory(scriptExtension) != null;
+  }
+}

09ebf8e9f1d1d7e63d2020961376f523cb80000c
Luca Milanesio <luca.milanesio@gmail.com>
2014-01-29 23:53:00 +0000

    Introduce support for externally loaded plugins
    Allows developers to use an external means for loading
    plugins into Gerrit. By declaring a ServerPluginProvider
    with a @Listen annotation it is possible to support another
    non-jar plugin file format to be loaded into Gerrit.
    
    One example use of externally managed plugins are
    the ones that support Groovy and Scala as scripting
    language for loading a Gerrit plugin.
    
    From now on it will be possible to extend Gerrit
    for multiple languages and formats without change or
    impact on JsPlugins and existing plugin loading functionalities.
    
    As additional value, from now the ability to extend Gerrit
    with other external files format (e.g. Directories,
    other files types and scripting languages) would not impact anymore
    the Plugin enums, associated switch/case or the complexity of
    Gerrit PluginLoader.
    
    Change-Id: I6a64a336f30a18409079e2d4df48d577cd462d6d

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/UniversalServerPluginProvider.java
@@ -0,0 +1,65 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.server.plugins;
+
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.PluginUser;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.eclipse.jgit.internal.storage.file.FileSnapshot;
+
+import java.io.File;
+
+@Singleton
+class UniversalServerPluginProvider implements ServerPluginProvider {
+
+  private final DynamicSet<ServerPluginProvider> serverPluginProviders;
+
+  @Inject
+  public UniversalServerPluginProvider(DynamicSet<ServerPluginProvider> sf) {
+    this.serverPluginProviders = sf;
+  }
+
+  @Override
+  public ServerPlugin get(String name, File srcFile, PluginUser pluginUser,
+      FileSnapshot snapshot) {
+    return providerOf(srcFile).get(name, srcFile, pluginUser, snapshot);
+  }
+
+  @Override
+  public String getPluginName(File srcFile) {
+    return providerOf(srcFile).getPluginName(srcFile);
+  }
+
+  private ServerPluginProvider providerOf(File srcFile) {
+    for (ServerPluginProvider provider : serverPluginProviders) {
+      if (provider.handles(srcFile)) {
+        return provider;
+      }
+    }
+    throw new IllegalArgumentException(srcFile.getAbsolutePath()
+        + " is not a supported Gerrit plugin format");
+  }
+
+  @Override
+  public boolean handles(File srcFile) {
+    for (ServerPluginProvider scriptingFactory : serverPluginProviders) {
+      if (scriptingFactory.handles(srcFile)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}

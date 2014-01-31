12f31e684f40381cc5a4679c6853902103a1704a
David Ostrovsky <david@ostrovsky.org>
2014-01-31 23:16:34 +0100

    Add support for JS API in Gerrit core
    JS API significantly simplifies interactive data gathering for UiActions.
    This change extends the support for JS API for Gerrit core. To add a
    JS callback for an action the following must be implemented:
    
    1. Java script file must be put under src/main/resource directory
    and registered in CoreScripts:
    
      @Source("projects/put-change.js")
      @DoNotEmbed
      DataResource put_change();
    
    2. Java script file must be added to BUCK file:
    
    CORE_SCRIPTS = [
      'src/main/resources/com/google/gerrit/client/projects/put-change.js'
    ]
    
    3. Java script must have the following structure:
    
      Gerrit.installCore(function() {
        function onPutChange(c) {
            var f = c.textfield();
            var t = c.textarea();
            var b = c.button('Create it', {onclick: function() {
              c.call(
                {msg: t.value, ref: f.value},
                function(r) {
                  c.hide();
                  Gerrit.go('/c/' + r);
                });
            }});
            c.popup(c.div(
              c.msg('Refs name'),
              c.br(),
              f,
              c.br(),
              c.msg('Commit message'),
              c.br(),
              t,
              c.br(),
              b));
            t.focus();
        }
        Gerrit.onActionCore('project', 'put-change', onPutChange);
      });
    
    Change-Id: I01b85e4c8d97ef4dd5d6043f1f5e41db5a1e094e

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/CoreScriptsLoader.java
@@ -0,0 +1,49 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client;
+
+import com.google.gwt.core.client.ScriptInjector;
+import com.google.gwt.resources.client.DataResource;
+import com.google.gwt.safehtml.shared.SafeUri;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoreScriptsLoader {
+
+  /** Map of names such as "put-change" to URI for code download. */
+  private static final Map<String, SafeUri> scriptUris;
+
+  static {
+    DataResource[] all = {
+       // Add here your scripts:
+       // CoreScripts.I.put_here_core_js_api_script();
+    };
+
+    scriptUris = new HashMap<>();
+
+    for (DataResource m : all) {
+      scriptUris.put(m.getName(), m.getSafeUri());
+    }
+  }
+
+  public static void load() {
+    for (SafeUri url : scriptUris.values()) {
+      ScriptInjector.fromUrl(url.asString())
+        .setWindow(ScriptInjector.TOP_WINDOW)
+        .inject();
+    }
+  }
+}

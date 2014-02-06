25e44801bb9669b5149dba7d5bd97319ca8484e9
David Pursehouse <david.pursehouse@sonymobile.com>
2014-02-06 12:30:19 +0900

    Add init step to configure the Solr index URL
    If the index URL is not configured, running the `reindex`
    program fails with:
    
      java.lang.IllegalStateException: index.url must be supplied
    
    Add a step in the initialisation to allow the user to provide
    the URL.  Set the default value to "localhost:8983" same as in
    the Solr tutorial [1].
    
    [1] http://lucene.apache.org/solr/4_6_1/tutorial.html
    
    Change-Id: Iae52ade99dd5e17a0688f090cd3f3aea3b676682

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitIndex.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitIndex.java
@@ -79,6 +79,9 @@ class InitIndex implements InitStep {
     ui.header("Index");
 
     IndexType type = index.select("Type", "type", IndexType.LUCENE);
+    if (type == IndexType.SOLR) {
+      index.string("Solr Index URL", "url", "localhost:8983");
+    }
     if (site.isNew && type == IndexType.LUCENE) {
       createLuceneIndex();
     } else {

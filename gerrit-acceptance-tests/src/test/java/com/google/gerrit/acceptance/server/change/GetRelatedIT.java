f064eb9f5e3755f25121212c1458b86380444e2d
Mani Chandel <mani.chandel@tcs.com>
2014-02-10 01:10:20 -0800

    Show red '*' for related changes merged in different branch(es)
    This is meant to indicate that the change is merged into a different
    branch(es) than the branch of current change under review.
    Also mouse over on * shows the branch(es) (which are different) in
    which the change is merged.
    
    Feature: Issue 1107
    Change-Id: I6f395fe023e25514c26509fb2831bc8f7b4fe1ef

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/server/change/GetRelatedIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/server/change/GetRelatedIT.java
@@ -15,15 +15,19 @@
 package com.google.gerrit.acceptance.server.change;
 
 import static com.google.gerrit.acceptance.GitUtil.add;
+import static com.google.gerrit.acceptance.GitUtil.checkout;
 import static com.google.gerrit.acceptance.GitUtil.createCommit;
 import static com.google.gerrit.acceptance.GitUtil.pushHead;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.GitUtil.Commit;
 import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.extensions.api.projects.BranchInput;
+import com.google.gerrit.extensions.common.ChangeInfo;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.server.change.GetRelated.ChangeAndCommit;
@@ -40,8 +44,8 @@ import java.util.List;
 public class GetRelatedIT extends AbstractDaemonTest {
 
   @Test
-  public void getRelatedNoResult() throws GitAPIException,
-      IOException, Exception {
+  public void getRelatedNoResult() throws GitAPIException, IOException,
+      Exception {
     PushOneCommit push = pushFactory.create(db, admin.getIdent());
     PatchSet.Id ps = push.to(git, "refs/for/master").getPatchSetId();
     List<ChangeAndCommit> related = getRelated(ps);
@@ -49,8 +53,7 @@ public class GetRelatedIT extends AbstractDaemonTest {
   }
 
   @Test
-  public void getRelatedLinear() throws GitAPIException,
-      IOException, Exception {
+  public void getRelatedLinear() throws GitAPIException, IOException, Exception {
     add(git, "a.txt", "1");
     Commit c1 = createCommit(git, admin.getIdent(), "subject: 1");
     add(git, "b.txt", "2");
@@ -60,14 +63,16 @@ public class GetRelatedIT extends AbstractDaemonTest {
     for (Commit c : ImmutableList.of(c2, c1)) {
       List<ChangeAndCommit> related = getRelated(getPatchSetId(c));
       assertEquals(2, related.size());
-      assertEquals("related to " + c.getChangeId(), c2.getChangeId(), related.get(0).changeId);
-      assertEquals("related to " + c.getChangeId(), c1.getChangeId(), related.get(1).changeId);
+      assertEquals("related to " + c.getChangeId(), c2.getChangeId(),
+          related.get(0).changeId);
+      assertEquals("related to " + c.getChangeId(), c1.getChangeId(),
+          related.get(1).changeId);
     }
   }
 
   @Test
-  public void getRelatedReorder() throws GitAPIException,
-      IOException, Exception {
+  public void getRelatedReorder() throws GitAPIException, IOException,
+      Exception {
     // Create two commits and push.
     add(git, "a.txt", "1");
     Commit c1 = createCommit(git, admin.getIdent(), "subject: 1");
@@ -87,21 +92,25 @@ public class GetRelatedIT extends AbstractDaemonTest {
     for (PatchSet.Id ps : ImmutableList.of(c2ps2, c1ps2)) {
       List<ChangeAndCommit> related = getRelated(ps);
       assertEquals(2, related.size());
-      assertEquals("related to " + ps, c1.getChangeId(), related.get(0).changeId);
-      assertEquals("related to " + ps, c2.getChangeId(), related.get(1).changeId);
+      assertEquals("related to " + ps, c1.getChangeId(),
+          related.get(0).changeId);
+      assertEquals("related to " + ps, c2.getChangeId(),
+          related.get(1).changeId);
     }
 
     for (PatchSet.Id ps : ImmutableList.of(c2ps1, c1ps1)) {
       List<ChangeAndCommit> related = getRelated(ps);
       assertEquals(2, related.size());
-      assertEquals("related to " + ps, c2.getChangeId(), related.get(0).changeId);
-      assertEquals("related to " + ps, c1.getChangeId(), related.get(1).changeId);
+      assertEquals("related to " + ps, c2.getChangeId(),
+          related.get(0).changeId);
+      assertEquals("related to " + ps, c1.getChangeId(),
+          related.get(1).changeId);
     }
   }
 
   @Test
-  public void getRelatedReorderAndExtend() throws GitAPIException,
-      IOException, Exception {
+  public void getRelatedReorderAndExtend() throws GitAPIException, IOException,
+      Exception {
     // Create two commits and push.
     add(git, "a.txt", "1");
     Commit c1 = createCommit(git, admin.getIdent(), "subject: 1");
@@ -125,29 +134,67 @@ public class GetRelatedIT extends AbstractDaemonTest {
     for (PatchSet.Id ps : ImmutableList.of(c3ps1, c2ps2, c1ps2)) {
       List<ChangeAndCommit> related = getRelated(ps);
       assertEquals(3, related.size());
-      assertEquals("related to " + ps, c3.getChangeId(), related.get(0).changeId);
-      assertEquals("related to " + ps, c1.getChangeId(), related.get(1).changeId);
-      assertEquals("related to " + ps, c2.getChangeId(), related.get(2).changeId);
+      assertEquals("related to " + ps, c3.getChangeId(),
+          related.get(0).changeId);
+      assertEquals("related to " + ps, c1.getChangeId(),
+          related.get(1).changeId);
+      assertEquals("related to " + ps, c2.getChangeId(),
+          related.get(2).changeId);
     }
 
     for (PatchSet.Id ps : ImmutableList.of(c2ps1, c1ps1)) {
       List<ChangeAndCommit> related = getRelated(ps);
       assertEquals(3, related.size());
-      assertEquals("related to " + ps, c3.getChangeId(), related.get(0).changeId);
-      assertEquals("related to " + ps, c2.getChangeId(), related.get(1).changeId);
-      assertEquals("related to " + ps, c1.getChangeId(), related.get(2).changeId);
+      assertEquals("related to " + ps, c3.getChangeId(),
+          related.get(0).changeId);
+      assertEquals("related to " + ps, c2.getChangeId(),
+          related.get(1).changeId);
+      assertEquals("related to " + ps, c1.getChangeId(),
+          related.get(2).changeId);
     }
   }
 
+  @Test
+  public void getRelatedBranches_postive() throws GitAPIException, IOException,
+      Exception {
+    gApi.projects().name(project.get()).branch("foo").create(new BranchInput());
+    add(git, "a.txt", "1");
+    Commit c1 = createCommit(git, admin.getIdent(), "subject: 1");
+    pushHead(git, "refs/heads/master", false);
+    checkout(git, c1.getCommit().name());
+    add(git, "b.txt", "2");
+    Commit c2 = createCommit(git, admin.getIdent(), "subject: 2");
+    pushHead(git, "refs/for/foo", false);
+    List<ChangeAndCommit> related = getRelated(getPatchSetId(c2));
+    assertEquals(1, related.get(1).branches.size());
+  }
+
+
+  @Test
+  public void getRelatedBranches_negative() throws GitAPIException,
+      IOException, Exception {
+    gApi.projects().name(project.get()).branch("foo").create(new BranchInput());
+    add(git, "a.txt", "1");
+    createCommit(git, admin.getIdent(), "subject: 1");
+    add(git, "b.txt", "2");
+    Commit c2 = createCommit(git, admin.getIdent(), "subject: 2");
+    pushHead(git, "refs/for/foo", false);
+    List<ChangeAndCommit> related = getRelated(getPatchSetId(c2));
+    assertNull(related.get(1).branches);
+  }
+
   private List<ChangeAndCommit> getRelated(PatchSet.Id ps) throws IOException {
-    String url = String.format("/changes/%d/revisions/%d/related",
-        ps.getParentKey().get(), ps.get());
+    String url =
+        String.format("/changes/%d/revisions/%d/related", ps.getParentKey()
+            .get(), ps.get());
     return newGson().fromJson(adminSession.get(url).getReader(),
         RelatedInfo.class).changes;
   }
 
   private PatchSet.Id getPatchSetId(Commit c) throws OrmException {
     return Iterables.getOnlyElement(
-        db.changes().byKey(new Change.Key(c.getChangeId()))).currentPatchSetId();
+        db.changes().byKey(new Change.Key(c.getChangeId())))
+        .currentPatchSetId();
   }
+
 }

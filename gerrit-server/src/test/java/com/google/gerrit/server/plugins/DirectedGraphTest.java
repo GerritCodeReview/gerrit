b06481300fb9d1021468741f904b1eac2b9275b8
David Ostrovsky <david@ostrovsky.org>
2014-02-10 08:57:08 +0100

    Add directed graph data structure and topological sort algorithm
    This generic directed graph data structure and topological sort algorithm
    is going to be used to resolve plugins dependencies:
    
    "Plugin foo depends on plugin bar"
    
    and load them in the topological order (first plugin bar and then plugin
    foo).
    
    The original source of two Java files is [1].  There is no license on
    the web site, but it is explicitly said:
    
    "If you're interested in using any of this code in your applications,
    feel free to do so!"
    
    [1] http://keithschwarz.com/interesting/
    
    Change-Id: I495412a9f09596c732a1ce3b017159ba744805f7

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/plugins/DirectedGraphTest.java
@@ -0,0 +1,70 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.plugins;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class DirectedGraphTest {
+
+  static class Node {
+    public String name;
+    Node(String n) {
+      name = n;
+    }
+    @Override
+    public String toString() {
+      return name;
+    }
+  }
+
+  @Test
+  public void sort() {
+    DirectedGraph<Node> g = new DirectedGraph<>();
+    Node seven = addNode(g, "7");
+    Node five = addNode(g, "5");
+    Node three = addNode(g, "3");
+    Node eleven = addNode(g, "11");
+    Node eight = addNode(g, "8");
+    Node two = addNode(g, "2");
+    Node nine = addNode(g, "9");
+    Node ten = addNode(g, "10");
+
+    g.addEdge(five, three);
+    g.addEdge(three, seven);
+    g.addEdge(seven, eleven);
+    g.addEdge(seven, eight);
+    g.addEdge(five, eleven);
+    g.addEdge(three, eight);
+    g.addEdge(three, ten);
+    g.addEdge(eleven, two);
+    g.addEdge(two, eight);
+    g.addEdge(eleven, nine);
+    g.addEdge(eleven, ten);
+    g.addEdge(eight, nine);
+    g.addEdge(eight, ten);
+    g.addEdge(nine, ten);
+
+    assertArrayEquals(new Node[] {five, three, seven, eleven, two, eight, nine,
+        ten}, TopologicalSort.sort(g).toArray());
+  }
+
+  private static Node addNode(DirectedGraph<Node> g, String n) {
+    Node node = new Node(n);
+    g.addNode(node);
+    return node;
+  }
+}

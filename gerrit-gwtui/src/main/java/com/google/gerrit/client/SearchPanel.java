b05f85ed5fb5a00084a3694e64d618a19d897c7c
Dave Borowitz <dborowitz@google.com>
2014-02-24 09:35:06 -0800

    Add timezones to search queries on the client
    The REST API is defined to use UTC if no timezone is specified. Using
    this in searches from the web UI would be a poor client experience,
    since elsewhere the client uses the local timezone for formatting.
    Automatically append the client timezone (and 00:00:00 if necessary)
    to any search queries involving the before/after operators.
    
    Don't just modify the query sent to the server; also modify the actual
    contents of the search box and the URL bar, so that the query is
    copy/pasteable to a command-line interface.
    
    Change-Id: I5ebf1c5c250b1fabc0ac7b05eacedd8d1d4fc415

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/SearchPanel.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/SearchPanel.java
@@ -24,6 +24,7 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -31,6 +32,9 @@ import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
 import com.google.gwtexpui.globalkey.client.GlobalKey;
 import com.google.gwtexpui.globalkey.client.KeyCommand;
+import com.google.gwtjsonrpc.common.JavaSqlTimestampHelper;
+
+import java.util.Date;
 
 class SearchPanel extends Composite {
   private final HintTextBox searchBox;
@@ -102,7 +106,7 @@ class SearchPanel extends Composite {
   }
 
   private void doSearch() {
-    final String query = searchBox.getText().trim();
+    String query = searchBox.getText().trim();
     if ("".equals(query)) {
       return;
     }
@@ -112,8 +116,91 @@ class SearchPanel extends Composite {
     if (query.matches("^[1-9][0-9]*$")) {
       Gerrit.display(PageLinks.toChange(Change.Id.parse(query)));
     } else {
+      query = replaceTimestamps(query);
+      searchBox.setText(query);
       Gerrit.display(PageLinks.toChangeQuery(query), QueryScreen.forQuery(query));
     }
+  }
+
+  private static String replaceTimestamps(String query) {
+    // Add explicit timezone for any before/after queries and update the search
+    // box, since REST API inputs/outputs are UTC unless otherwise specified.
+    StringBuilder sb = new StringBuilder(query.length() + 30);
+    boolean quote = false;
+    for (int i = 0; i < query.length(); i++) {
+      switch (query.charAt(i)) {
+        case 'b':
+          i += replaceTimestamp(query, i, quote, sb, "before:");
+          break;
+        case 'a':
+          i += replaceTimestamp(query, i, quote, sb, "after:");
+          break;
+        case '"':
+          quote = !quote;
+          // Fallthrough.
+        default:
+          sb.append(query.charAt(i));
+          break;
+      }
+    }
+    return sb.toString();
+  }
+
+  private static final DateTimeFormat TZ_FORMAT = DateTimeFormat.getFormat("Z");
+
+  private static int replaceTimestamp(String query, int start, boolean quote,
+      StringBuilder out, String pred) {
+    int l = pred.length();
+    if (quote || !query.substring(start, start + l).equals(pred)) {
+      out.append(query.charAt(start));
+      return 0;
+    }
+    int skip = l;
+
+    StringBuilder sb = new StringBuilder(30);
+    quote = false;
+    boolean needQuote = false;
+    LOOP: for (int i = start + l; i < query.length(); i++) {
+      char c = query.charAt(i);
+      switch (c) {
+        case '"':
+          if (quote) {
+            // Grammar doesn't allow escaped/nested quotes.
+            break LOOP;
+          }
+          quote = true;
+          break;
+        case ' ':
+          if (quote) {
+            sb.append(c);
+            needQuote = true;
+          } else {
+            break LOOP;
+          }
+          break;
+        default:
+          sb.append(c);
+          break;
+      }
+      skip++;
+    }
+    String ts = sb.toString();
+    if (!JavaSqlTimestampHelper.hasTime(ts)) {
+      sb.append(" 00:00:00");
+      ts = sb.toString();
+      needQuote = true;
+    }
+    if (!JavaSqlTimestampHelper.hasTimeZone(ts)) {
+      sb.append(' ').append(TZ_FORMAT.format(new Date()));
+      needQuote = true;
+    }
+    out.append(pred);
+    if (needQuote) {
+      out.append('"').append(sb).append('"');
+    } else {
+      out.append(sb);
+    }
+    return skip;
   }
 
   private static class MySuggestionDisplay extends SuggestBox.DefaultSuggestionDisplay {

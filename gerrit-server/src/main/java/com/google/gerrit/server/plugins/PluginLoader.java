91bc08ec8c7c8500c68c3bcbf6ac97eff96407be
David Ostrovsky <david@ostrovsky.org>
2014-03-07 01:09:29 +0100

    Move non JAR plugins to the end of the topological sort order
    For now the scripting plugins cannot provide dependency information,
    that they depend on foo-provider plugin.  Put them in the end of
    the reversed topological sort order. This will ensure that they
    are loaded after the actual provider plugin is loaded.
    
    Change-Id: Ie6fefbe8935133bb523f00e2de9f5b86b59ebbe1

--- gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginLoader.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginLoader.java
@@ -514,7 +514,7 @@ public class PluginLoader implements LifecycleListener {
     return 0 < ext ? name.substring(0, ext) : name;
   }
 
-  private static String getExtension(File file) {
+  static String getExtension(File file) {
     return getExtension(file.getName());
   }
 
@@ -786,7 +786,7 @@ public class PluginLoader implements LifecycleListener {
   }
 
   private static boolean isScriptingPlugin(String name) {
-    String ext = StringUtils.substringAfterLast(name,".");
+    String ext = StringUtils.substringAfterLast(name, ".");
     return isPlugin(name, ext);
   }
 

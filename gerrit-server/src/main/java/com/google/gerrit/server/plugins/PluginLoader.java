459e1980f92b9c591edb77d0c7958237f1abb8f3
David Ostrovsky <david@ostrovsky.org>
2014-03-07 01:09:29 +0100

    Make dependency resolver scripting plugins aware
    Change-Id: Ie6fefbe8935133bb523f00e2de9f5b86b59ebbe1

--- gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginLoader.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/PluginLoader.java
@@ -510,7 +510,7 @@ public class PluginLoader implements LifecycleListener {
     return 0 < ext ? name.substring(0, ext) : name;
   }
 
-  private static String getExtension(File file) {
+  static String getExtension(File file) {
     return getExtension(file.getName());
   }
 
@@ -781,7 +781,7 @@ public class PluginLoader implements LifecycleListener {
   }
 
   private static boolean isScriptingPlugin(String name) {
-    String ext = StringUtils.substringAfterLast(name,".");
+    String ext = StringUtils.substringAfterLast(name, ".");
     return isPlugin(name, ext);
   }
 

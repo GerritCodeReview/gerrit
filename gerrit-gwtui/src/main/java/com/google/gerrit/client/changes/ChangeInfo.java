89fe874e3390511dfcfdb354846e9a79eec039e2
Mani Chandel <mani.chandel@tcs.com>
2014-03-17 23:35:32 -0700

    Add REST API extension otherBranchCommit
    Added REST API extension 'otherBranchCommit' which will return true 
    if while submitting a commit other commit(s)(which are already 
    merged in a different branch) are also being merged in the branch
    of current change. 
    
    Here is an example:
    Branch A = 66c1bbaa
    Branch B = 66c1bbaa
    
    then Branch A gets a new commit:
    66c1bbaa -> ab8db2c7     (Gerrit change Id #123 to branch A)
    
    then Branch B needs a new commit and it should look like this:
    66c1bbaa -> be64dd43
    
    but instead, Branch B looks like this
    66c1bbaa -> ab8db2c7 -> be64dd43 (Gerrit change Id #456 to branch B)
    
    because the developer made a mistake and submitted a Gerrit 
    change (starting from branchA pushed a commit in branchB) 
    that included commit ab8db2c7
    So for commit be64dd43 otherBranchCommit is true.
    
    Feature: Issue 1107
    Change-Id: I1747bc0c56283a10a64728c4b48ac937bdef1335

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeInfo.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeInfo.java
@@ -252,6 +252,7 @@ public class ChangeInfo extends JavaScriptObject {
     public final native GitPerson committer() /*-{ return this.committer; }-*/;
     public final native String subject() /*-{ return this.subject; }-*/;
     public final native String message() /*-{ return this.message; }-*/;
+    public final native boolean otherBranchCommit() /*-{ return this.other_branch_commit; }-*/;
 
     protected CommitInfo() {
     }

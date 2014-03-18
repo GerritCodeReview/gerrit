528149330fa2bb906e328ae03a8a83963bacffa1
Phil Lello <philipl@catalyst-eu.net>
2014-03-18 20:51:03 +0000

    Cross-project dependencies
    This is a patch to allow manual addition of dependencies in other
    projects, by adding a Depends-On: footer.
    
    e.g. Depends-On: <projectname>~<branch>~<Change-Id>
    
    Note that this is a work-in-progress requiring further testing,
    but is hopefully complete.
    
    Additional considerations:
    - Need to check what happens if a depended-on project is deleted
    - It might be nice to allow a numeric change number
    
    Feature: Issue 377
    Change-Id: Idc47ce55677c87fbd528a1d1c5225ba70fc3abc8

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeScreen.java
@@ -42,7 +42,6 @@ import com.google.gerrit.common.data.ChangeDetail;
 import com.google.gerrit.common.data.ChangeInfo;
 import com.google.gerrit.common.data.SubmitTypeRecord;
 import com.google.gerrit.extensions.common.ListChangesOption;
-import com.google.gerrit.extensions.common.SubmitType;
 import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.CommentVisibilityStrategy;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.Change.Status;
@@ -51,6 +50,7 @@ import com.google.gerrit.reviewdb.client.Patch;
 import com.google.gerrit.reviewdb.client.Patch.ChangeType;
 import com.google.gerrit.reviewdb.client.Patch.PatchType;
 import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.event.dom.client.ChangeEvent;
@@ -300,7 +300,7 @@ public class ChangeScreen extends Screen
       CallbackGroup cbs1 = new CallbackGroup();
       final CallbackGroup cbs2 = new CallbackGroup();
       final PatchSet.Id psId = event.getValue().getCurrentPatchSet().getId();
-      final Map<String, Patch> patches = new HashMap<>();
+      final Map<String, Patch> patches = new HashMap<String, Patch>();
       String revId =
           event.getValue().getCurrentPatchSetDetail().getInfo().getRevId();
 
@@ -311,7 +311,7 @@ public class ChangeScreen extends Screen
             @Override
             public void onSuccess(NativeString result) {
               event.getValue().setSubmitTypeRecord(SubmitTypeRecord.OK(
-                  SubmitType.valueOf(result.asString())));
+                  Project.SubmitType.valueOf(result.asString())));
             }
             public void onFailure(Throwable caught) {}
           }));
@@ -329,8 +329,24 @@ public class ChangeScreen extends Screen
               }
 
               private void dependsOn(RelatedChanges.RelatedInfo info) {
+                String thisProject = event.getValue().getChange().getProject().get();
+                String thisBranch = event.getValue().getChange().getDest().get();
                 ChangeAndCommit self = null;
-                Map<String, ChangeAndCommit> m = new HashMap<>();
+                Map<String, ChangeAndCommit> m = new HashMap<String, ChangeAndCommit>();
+                List<ChangeInfo> d = new ArrayList<ChangeInfo>();
+
+                // First check for dependencies on external projects
+                for (int i = 0; i < info.changes().length(); i++) {
+                  ChangeAndCommit c = info.changes().get(i);
+                  if (!c.projectName().equals(thisProject)
+                   || !c.branch().equals(thisBranch)) {
+                    ChangeInfo ci = new ChangeInfo();
+                    load(c, ci);
+                    d.add(ci);
+                  }
+                }
+
+                // Now check for same-project dependencies
                 for (int i = 0; i < info.changes().length(); i++) {
                   ChangeAndCommit c = info.changes().get(i);
                   if (changeId.equals(c.legacy_id())) {
@@ -342,7 +358,6 @@ public class ChangeScreen extends Screen
                 }
                 if (self != null && self.commit() != null
                     && self.commit().parents() != null) {
-                  List<ChangeInfo> d = new ArrayList<>();
                   for (CommitInfo p : Natives.asList(self.commit().parents())) {
                     ChangeAndCommit pc = m.get(p.commit());
                     if (pc != null && pc.has_change_number()) {
@@ -356,12 +371,12 @@ public class ChangeScreen extends Screen
               }
 
               private void neededBy(RelatedChanges.RelatedInfo info) {
-                Set<String> mine = new HashSet<>();
+                Set<String> mine = new HashSet<String>();
                 for (PatchSet ps : event.getValue().getPatchSets()) {
                   mine.add(ps.getRevision().get());
                 }
 
-                List<ChangeInfo> n = new ArrayList<>();
+                List<ChangeInfo> n = new ArrayList<ChangeInfo>();
                 for (int i = 0; i < info.changes().length(); i++) {
                   ChangeAndCommit c = info.changes().get(i);
                   if (c.has_change_number()
@@ -418,7 +433,7 @@ public class ChangeScreen extends Screen
               }
               public void onFailure(Throwable caught) {}
             }));
-        final Set<PatchSet.Id> withDrafts = new HashSet<>();
+        final Set<PatchSet.Id> withDrafts = new HashSet<PatchSet.Id>();
         event.getValue().setPatchSetsWithDraftComments(withDrafts);
         for (PatchSet ps : event.getValue().getPatchSets()) {
           if (!ps.getId().equals(psId)) {
@@ -478,7 +493,7 @@ public class ChangeScreen extends Screen
             public void onSuccess(NativeMap<FileInfo> result) {
               JsArray<FileInfo> fileInfos = result.values();
               FileInfo.sortFileInfoByPath(fileInfos);
-              List<Patch> list = new ArrayList<>(fileInfos.length());
+              List<Patch> list = new ArrayList<Patch>(fileInfos.length());
               for (FileInfo f : Natives.asList(fileInfos)) {
                 Patch p = patches.get(f.path());
                 if (p == null) {

6b9159abe776732468274727e29a523b752625e6
Shawn Pearce <sop@google.com>
2014-03-27 17:37:29 -0700

    Remove message box when editing topic of change
    I do not recall why I added this message box below the topic name
    field in ChangeScreen2.  I suspect the original idea was to permit
    more notes to be added, for example why the topic is being assigned.
    
    At the recent Gerrit hackathon it sounded like nobody ever uses this
    box.  Remove it to simplify the UI.
    
    Also remove message from the PutTopic REST API.  We suspect nobody
    will notice the missing field.
    
    Change-Id: I8d32287ee3b08d9409dd7d58688cd9effdfd380f

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Topic.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Topic.java
@@ -36,7 +36,6 @@ import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.UIObject;
-import com.google.gwtexpui.globalkey.client.NpTextArea;
 import com.google.gwtexpui.globalkey.client.NpTextBox;
 
 /** Displays (and edits) the change topic string. */
@@ -53,7 +52,6 @@ class Topic extends Composite {
 
   @UiField Element form;
   @UiField NpTextBox input;
-  @UiField NpTextArea message;
   @UiField Button save;
   @UiField Button cancel;
 
@@ -116,22 +114,11 @@ class Topic extends Composite {
     }
   }
 
-  @UiHandler("message")
-  void onKeyDownMessage(KeyDownEvent e) {
-    if (e.getNativeKeyCode() == KeyCodes.KEY_ESCAPE) {
-      onCancel(null);
-    } else if (e.getNativeKeyCode() == KeyCodes.KEY_ENTER
-        && e.isControlKeyDown()) {
-      onSave(null);
-    }
-  }
-
   @UiHandler("save")
   void onSave(ClickEvent e) {
     ChangeApi.topic(
         psId.getParentKey().get(),
         input.getValue().trim(),
-        message.getValue().trim(),
         new GerritCallback<String>() {
           @Override
           public void onSuccess(String result) {

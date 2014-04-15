049c35574d154a6d5a21b9e33de263ce9af12a2d
Edwin Kempin <edwin.kempin@sap.com>
2014-04-15 11:26:23 +0200

    Allow plugins to refresh Gerrit's menu bar
    Change-Id: Id908ebe23a07f2ae45e131a9b8a98c24f4a9cf74
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/api/ActionContext.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/api/ActionContext.java
@@ -37,6 +37,7 @@ public class ActionContext extends JavaScriptObject {
     Gerrit.ActionContext.prototype = {
       go: Gerrit.go,
       refresh: Gerrit.refresh,
+      refreshMenuBar: Gerrit.refreshMenuBar,
       showError: Gerrit.showError,
 
       br: function(){return doc.createElement('br')},

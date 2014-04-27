917e9df7b30af6969d92e5c8632a062ebd16441e
David Ostrovsky <david@ostrovsky.org>
2014-04-27 12:54:20 +0200

    Add REST endpoint to create empty change
    This change is based on change-fabricator plugin [1] and is a
    prerequisite for inline edit feature to support 100% Gerrit workflow
    in browser.
    
    [1] https://github.com/davido/gerrit-change-fabricator
    
    Bug: Issue 2338
    Change-Id: Idf290c14d5a478479fe1cb916ee227aa11435fcd

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/project/CreateChangeIT.java
@@ -0,0 +1,59 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.rest.project;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.extensions.api.projects.CreateChangeInput;
+
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class CreateChangeIT extends AbstractDaemonTest {
+
+  @Test
+  public void createEmptyChange_MissingDestination() throws IOException {
+    RestResponse r = adminSession.put("/projects/" + project.get()
+        + "/change");
+    assertEquals(HttpStatus.SC_BAD_REQUEST, r.getStatusCode());
+    r.getEntityContent().contains("destination must be non-empty");
+  }
+
+  @Test
+  public void createEmptyChange_MissingMessage() throws IOException {
+    CreateChangeInput in = new CreateChangeInput();
+    in.destination = "master";
+    RestResponse r = adminSession.put("/projects/" + project.get()
+        + "/change", in);
+    assertEquals(HttpStatus.SC_BAD_REQUEST, r.getStatusCode());
+    r.getEntityContent().contains("commit message must be non-empty");
+  }
+
+  @Test
+  public void createEmptyChange() throws IOException {
+    CreateChangeInput in = new CreateChangeInput();
+    in.destination = "master";
+    in.message = "Empty change";
+    RestResponse r = adminSession.put("/projects/" + project.get()
+        + "/change", in);
+    assertEquals(HttpStatus.SC_CREATED, r.getStatusCode());
+    Integer number = newGson().fromJson(r.getReader(), Integer.class);
+    assertEquals(1, number.intValue());
+  }
+}

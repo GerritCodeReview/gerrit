df818045561fb60619277ec00829ce191a7d1733
Urs Wolfer <uwolfer@fwo.ch>
2014-04-27 14:28:45 +0200

    Add default empty implementations for extensions-api interfaces
    This way it's easier to add new api without breaking existing impls.
    Default implementation throws a NotImplementedException.
    
    Change-Id: I19e495745baa8d628cab861a3ac95acaf7de3836

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/projects/BranchApi.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/projects/BranchApi.java
@@ -14,8 +14,20 @@
 
 package com.google.gerrit.extensions.api.projects;
 
+import com.google.gerrit.extensions.restapi.NotImplementedException;
 import com.google.gerrit.extensions.restapi.RestApiException;
 
 public interface BranchApi {
   BranchApi create(BranchInput in) throws RestApiException;
+
+  /**
+   * A default implementation which allows source compatibility
+   * when adding new methods to the interface.
+   **/
+  public class NotImplemented implements BranchApi {
+    @Override
+    public BranchApi create(BranchInput in) throws RestApiException {
+      throw new NotImplementedException();
+    }
+  }
 }

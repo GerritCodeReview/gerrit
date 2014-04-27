1a209a64df39a74cf647289cdfa7576396ddb146
Luca Milanesio <luca.milanesio@gmail.com>
2014-04-28 00:45:34 +0100

    Jar plugin loading refactor as generic ServerPlugin
    Now that we have a fully pluggable ServerPlugin mechanism
    with registered providers, there is no reason why the jar
    plugins cannot be loaded using the same interface.
    
    Removing all the custom code managing the jar plugins 
    and creating a new pre-defined JarPluginProvider.
    
    NOTE: A limitation still exists on init-steps as they
    can only be provided by jar plugins. This is due to 
    the fact that Gerrit init runs without having a fully
    configured plugin loader and full set of Guice injectors.
    
    Change-Id: I793570735376be3a40f3204af6e82820191caac6

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitPlugins.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitPlugins.java
@@ -17,7 +17,7 @@ package com.google.gerrit.pgm.init;
 import com.google.common.collect.Lists;
 import com.google.gerrit.pgm.util.ConsoleUI;
 import com.google.gerrit.server.config.SitePaths;
-import com.google.gerrit.server.plugins.PluginLoader;
+import com.google.gerrit.server.plugins.JarPluginProvider;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -63,7 +63,7 @@ public class InitPlugins implements InitStep {
     pluginsDistribution.foreach(new PluginsDistribution.Processor() {
       @Override
       public void process(String pluginName, InputStream in) throws IOException {
-        File tmpPlugin = PluginLoader.storeInTemp(pluginName, in, site);
+        File tmpPlugin = JarPluginProvider.storeInTemp(pluginName, in, site);
         String pluginVersion = getVersion(tmpPlugin);
         if (deleteTempPluginFile) {
           tmpPlugin.delete();

0ab549a9a2968ece036e8cdebfd0222272fa1159
Luca Milanesio <luca.milanesio@gmail.com>
2014-05-08 09:17:43 +0100

    Extend top-menus REST API to get the entire menu bar
    Allows the top-menus REST API to be more flexible and
    serve the entire Gerrit menu bar items. This allows other
    Gerrit plugins to keep consistency with the Gerrit menu bar
    and allowing the user to directly deep link to all main
    functionalities without having to rely only on the "back" browser
    button.
    
    WIP: Next change on the same topic will remove the current
    Gerrit logic in GWT to leverage the new enhanced top-menus REST API.
    
    Two extra parameters introduced to:
    - get the full menu bar whilst keeping existing functionality
      on the extra menu items
    - set the project focus of the top menu bar
    
    See /Documentation/rest-api-config.txt for an example
    of how the extended top-menus API works.
    
    Change-Id: I065fbb10e4a16f88ea570da0baaeef913b5af281

--- gerrit-server/src/main/java/com/google/gerrit/server/account/GetPreferences.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/GetPreferences.java
@@ -180,5 +180,9 @@ public class GetPreferences implements RestReadView<AccountResource> {
       String val = cfg.getString(MY, subsection, key);
       return !Strings.isNullOrEmpty(val) ? val : defaultValue;
     }
+
+    public List<TopMenu.MenuItem> getMyMenu() {
+      return my;
+    }
   }
 }

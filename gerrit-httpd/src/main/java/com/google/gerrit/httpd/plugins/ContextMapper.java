0295fc08b1ee0f9ef68765685d69f3b7610f39cb
Adrian Görler <adrian.goerler@sap.com>
2014-05-12 16:10:52 +0200

    Fix the request wrapper for http requests served from plugins
    A request wrapper is used to provide servlets contributed by plugins
    with a context that is rooted in the plugins path segment of the request
    URI. For example a plugin myPlugin that is invoked under
    '/a/plugins/myPlugin' should be provided with the context path
    '/a/plugins/myPlugin'. The request '/a/plugins/myPlugin/myResource'
    should give the following info for the wrapped context:
    
    contextPath: '/a/plugins/myPlugin'
    servletPath: ''
    pathInfo:    '/myResource'
    requestURI:  '/a/plugins/myPlugin/myResource'
    
    The request wrapper failed to comply with the servlet specification
    (servlet specification 3.1, §3.5). For example the invariant
    
    requestURI = contextPath + servletPath + pathInfo
    
    was violated.
    
    The request mapping mechanism is now extracted to a ContextMapper class.
    
    Change-Id: Ie8d9a9ac151162efb080a6e676a121a53bc11852

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/plugins/ContextMapper.java
@@ -0,0 +1,74 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.package com.google.gerrit.httpd.plugins;
+
+package com.google.gerrit.httpd.plugins;
+
+import com.google.common.base.Strings;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class ContextMapper {
+  private static final String PLUGINS_PREFIX = "/plugins/";
+  static final String AUTHORIZED_PREFIX = "/a" + PLUGINS_PREFIX;
+  private final String base;
+  private final String authorizedBase;
+
+  public ContextMapper(String contextPath) {
+    base = Strings.nullToEmpty(contextPath) + PLUGINS_PREFIX;
+    authorizedBase = Strings.nullToEmpty(contextPath) + AUTHORIZED_PREFIX;
+  }
+
+  static boolean isAuthorizedCall(HttpServletRequest req) {
+    return !Strings.isNullOrEmpty(req.getServletPath())
+        && req.getServletPath().startsWith(AUTHORIZED_PREFIX);
+  }
+
+  HttpServletRequest create(HttpServletRequest req, String name) {
+    String contextPath = (isAuthorizedCall(req) ? authorizedBase : base) + name;
+
+    return new WrappedRequest(req, contextPath);
+  }
+
+  public String getFullPath(String name) {
+    return base + name;
+  }
+
+  private class WrappedRequest extends HttpServletRequestWrapper {
+    private final String contextPath;
+    private final String pathInfo;
+
+    private WrappedRequest(HttpServletRequest req, String contextPath) {
+      super(req);
+      this.contextPath = contextPath;
+      this.pathInfo = getRequestURI().substring(contextPath.length());
+    }
+
+    @Override
+    public String getServletPath() {
+      return "";
+    }
+
+    @Override
+    public String getContextPath() {
+      return contextPath;
+    }
+
+    @Override
+    public String getPathInfo() {
+      return pathInfo;
+    }
+  }
+
+}

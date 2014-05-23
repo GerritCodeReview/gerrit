cb63c804c3c53337c8f7fd79b14bd206868a0670
Edwin Kempin <edwin.kempin@sap.com>
2014-05-23 10:24:41 +0200

    Add REST endpoint to get a cache
    Change-Id: I09bfa7c052f2b8411b218c77683cda45a0afe844
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/CachesCollection.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/CachesCollection.java
@@ -14,11 +14,15 @@
 
 package com.google.gerrit.server.config;
 
+import com.google.common.cache.Cache;
 import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.ChildCollection;
 import com.google.gerrit.extensions.restapi.IdString;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.server.AnonymousUser;
+import com.google.gerrit.server.CurrentUser;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
@@ -27,12 +31,17 @@ public class CachesCollection implements
 
   private final DynamicMap<RestView<CacheResource>> views;
   private final Provider<ListCaches> list;
+  private final Provider<CurrentUser> self;
+  private final DynamicMap<Cache<?, ?>> cacheMap;
 
   @Inject
   CachesCollection(DynamicMap<RestView<CacheResource>> views,
-      Provider<ListCaches> list) {
+      Provider<ListCaches> list, Provider<CurrentUser> self,
+      DynamicMap<Cache<?, ?>> cacheMap) {
     this.views = views;
     this.list = list;
+    this.self = self;
+    this.cacheMap = cacheMap;
   }
 
   @Override
@@ -42,8 +51,30 @@ public class CachesCollection implements
 
   @Override
   public CacheResource parse(ConfigResource parent, IdString id)
-      throws ResourceNotFoundException {
-    throw new ResourceNotFoundException(id);
+      throws AuthException, ResourceNotFoundException {
+    final CurrentUser user = self.get();
+    if (user instanceof AnonymousUser) {
+      throw new AuthException("Authentication required");
+    } else if(!(user.isIdentifiedUser())) {
+      throw new ResourceNotFoundException();
+    }
+    if (!user.getCapabilities().canViewCaches()) {
+      throw new AuthException("not allowed to view caches");
+    }
+
+    String cacheName = id.get();
+    String pluginName = "gerrit";
+    int i = cacheName.indexOf('-');
+    if (i != -1) {
+      pluginName = cacheName.substring(0, i);
+      cacheName = cacheName.length() > i + 1 ? cacheName.substring(i + 1) : "";
+    }
+
+    Provider<Cache<?, ?>> cacheProvider = cacheMap.byPlugin(pluginName).get(cacheName);
+    if (cacheProvider == null) {
+      throw new ResourceNotFoundException(id);
+    }
+    return new CacheResource(pluginName, cacheName, cacheProvider);
   }
 
   @Override

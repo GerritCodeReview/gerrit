c4d526db65f5b144ea06a5df4e0567bfbe652573
David Ostrovsky <david@ostrovsky.org>
2014-05-27 17:56:22 +0200

    ChangeJson: Don't use ChangesCollection
    This change removes circular dependency:
    
      ChangeJson depends on ChangesCollection
      ChangesCollection depends on CreateChange
      CreateChange depends on ChangeJson
    
    by not using ChangesCollection in ChangeJson class.
    This is needed to remove the needs to use factory to inject
    CreateChange in ChangesCollection. This is needed to move
    ChangesCollection and ChangeJson to singleton scope. This is
    done in the follow-up change.
    
    Change-Id: I2fc4ac3d7f00e91e934fd496c164555ab751babc

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
@@ -140,7 +140,6 @@ public class ChangeJson {
   private final ProjectControl.GenericFactory projectControlFactory;
   private final ChangeData.Factory changeDataFactory;
   private final PatchSetInfoFactory patchSetInfoFactory;
-  private final ChangesCollection changes;
   private final FileInfoJson fileInfoJson;
   private final AccountInfo.Loader.Factory accountLoaderFactory;
   private final DynamicMap<DownloadScheme> downloadSchemes;
@@ -163,7 +162,6 @@ public class ChangeJson {
       ProjectControl.GenericFactory pcf,
       ChangeData.Factory cdf,
       PatchSetInfoFactory psi,
-      ChangesCollection changes,
       FileInfoJson fileInfoJson,
       AccountInfo.Loader.Factory ailf,
       DynamicMap<DownloadScheme> downloadSchemes,
@@ -179,7 +177,6 @@ public class ChangeJson {
     this.projectControlFactory = pcf;
     this.changeDataFactory = cdf;
     this.patchSetInfoFactory = psi;
-    this.changes = changes;
     this.fileInfoJson = fileInfoJson;
     this.accountLoaderFactory = ailf;
     this.downloadSchemes = downloadSchemes;
@@ -356,7 +353,7 @@ public class ChangeJson {
       out.actions = Maps.newTreeMap();
       for (UiAction.Description d : UiActions.from(
           changeViews,
-          changes.parse(ctl),
+          new ChangeResource(ctl),
           userProvider)) {
         out.actions.put(d.getId(), new ActionInfo(d));
       }
@@ -857,7 +854,7 @@ public class ChangeJson {
       out.actions = Maps.newTreeMap();
       for (UiAction.Description d : UiActions.from(
           revisions,
-          new RevisionResource(changes.parse(ctl), in),
+          new RevisionResource(new ChangeResource(ctl), in),
           userProvider)) {
         out.actions.put(d.getId(), new ActionInfo(d));
       }

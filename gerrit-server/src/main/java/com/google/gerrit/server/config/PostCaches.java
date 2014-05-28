d6243c5b57ed7990c64824a6fd00e8d7d34df926
Edwin Kempin <edwin.kempin@sap.com>
2014-05-28 14:46:44 +0200

    Support flushing a set of caches at once via REST
    Change-Id: I2e51e3138eea15b305c3fc5029e91aa6b2f0b4cb
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/PostCaches.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/PostCaches.java
@@ -23,26 +23,36 @@ import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
 import com.google.gerrit.server.config.PostCaches.Input;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiresCapability(GlobalCapability.FLUSH_CACHES)
 @Singleton
 public class PostCaches implements RestModifyView<ConfigResource, Input> {
   public static class Input {
     public Operation operation;
+    public List<String> caches;
 
     public Input() {
     }
 
     public Input(Operation op) {
+      this(op, null);
+    }
+
+    public Input(Operation op, List<String> c) {
       operation = op;
+      caches = c;
     }
   }
 
   public static enum Operation {
-    FLUSH_ALL;
+    FLUSH_ALL, FLUSH;
   }
 
   private final DynamicMap<Cache<?, ?>> cacheMap;
@@ -57,25 +67,68 @@ public class PostCaches implements RestModifyView<ConfigResource, Input> {
 
   @Override
   public Object apply(ConfigResource rsrc, Input input) throws AuthException,
-      ResourceNotFoundException, BadRequestException {
+      ResourceNotFoundException, BadRequestException,
+      UnprocessableEntityException {
     if (input == null || input.operation == null) {
       throw new BadRequestException("operation must be specified");
     }
 
     switch (input.operation) {
       case FLUSH_ALL:
-        for (DynamicMap.Entry<Cache<?, ?>> e : cacheMap) {
-          CacheResource cacheResource =
-              new CacheResource(e.getPluginName(), e.getExportName(),
-                  e.getProvider());
-          if (FlushCache.WEB_SESSIONS.equals(cacheResource.getName())) {
-            continue;
-          }
-          flushCache.apply(cacheResource, null);
+        if (input.caches != null) {
+          throw new BadRequestException(
+              "specifying caches is not allowed for operation 'FLUSH_ALL'");
         }
+        flushAll();
+        return Response.ok("");
+      case FLUSH:
+        if (input.caches == null || input.caches.isEmpty()) {
+          throw new BadRequestException(
+              "caches must be specified for operation 'FLUSH'");
+        }
+        flush(input.caches);
         return Response.ok("");
       default:
         throw new BadRequestException("unsupported operation: " + input.operation);
+    }
+  }
+
+  private void flushAll() throws AuthException {
+    for (DynamicMap.Entry<Cache<?, ?>> e : cacheMap) {
+      CacheResource cacheResource =
+          new CacheResource(e.getPluginName(), e.getExportName(),
+              e.getProvider());
+      if (FlushCache.WEB_SESSIONS.equals(cacheResource.getName())) {
+        continue;
+      }
+      flushCache.apply(cacheResource, null);
+    }
+  }
+
+  private void flush(List<String> cacheNames)
+      throws UnprocessableEntityException, AuthException {
+    List<CacheResource> cacheResources = new ArrayList<>(cacheNames.size());
+
+    for (String n : cacheNames) {
+      String pluginName = "gerrit";
+      String cacheName = n;
+      int i = cacheName.lastIndexOf('-');
+      if (i != -1) {
+        pluginName = cacheName.substring(0, i);
+        cacheName = cacheName.length() > i + 1 ? cacheName.substring(i + 1) : "";
+      }
+
+      Cache<?, ?> cache = cacheMap.get(pluginName, cacheName);
+      if (cache != null) {
+        cacheResources.add(new CacheResource(pluginName, cacheName, cache));
+      } else {
+        throw new UnprocessableEntityException(String.format(
+            "cache %s not found", n));
+      }
+    }
+
+    for (CacheResource rsrc : cacheResources) {
+      flushCache.apply(rsrc, null);
     }
   }
 }

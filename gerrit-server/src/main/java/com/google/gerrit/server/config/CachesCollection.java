79c76795de428bee39ea8a08e5dc32fdee5ee46a
Edwin Kempin <edwin.kempin@sap.com>
2014-05-28 14:17:22 +0200

    Support flushing of all caches via REST
    Change-Id: I82807e71e7fceb7ba3bba23d51263b72998f251d
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/CachesCollection.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/CachesCollection.java
@@ -18,10 +18,12 @@ import com.google.common.cache.Cache;
 import com.google.gerrit.common.data.GlobalCapability;
 import com.google.gerrit.extensions.annotations.RequiresCapability;
 import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.AcceptsPost;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.ChildCollection;
 import com.google.gerrit.extensions.restapi.IdString;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestView;
 import com.google.gerrit.server.AnonymousUser;
 import com.google.gerrit.server.CurrentUser;
@@ -32,21 +34,23 @@ import com.google.inject.Singleton;
 @RequiresCapability(GlobalCapability.VIEW_CACHES)
 @Singleton
 public class CachesCollection implements
-    ChildCollection<ConfigResource, CacheResource> {
+    ChildCollection<ConfigResource, CacheResource>, AcceptsPost<ConfigResource> {
 
   private final DynamicMap<RestView<CacheResource>> views;
   private final Provider<ListCaches> list;
   private final Provider<CurrentUser> self;
   private final DynamicMap<Cache<?, ?>> cacheMap;
+  private final Provider<PostCaches> postCaches;
 
   @Inject
   CachesCollection(DynamicMap<RestView<CacheResource>> views,
       Provider<ListCaches> list, Provider<CurrentUser> self,
-      DynamicMap<Cache<?, ?>> cacheMap) {
+      DynamicMap<Cache<?, ?>> cacheMap, Provider<PostCaches> postCaches) {
     this.views = views;
     this.list = list;
     this.self = self;
     this.cacheMap = cacheMap;
+    this.postCaches = postCaches;
   }
 
   @Override
@@ -84,5 +88,11 @@ public class CachesCollection implements
   @Override
   public DynamicMap<RestView<CacheResource>> views() {
     return views;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public PostCaches post(ConfigResource parent) throws RestApiException {
+    return postCaches.get();
   }
 }

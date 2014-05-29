72579238e5df9421810b347fdc6c1a197d95142e
David Ostrovsky <david@ostrovsky.org>
2014-05-29 11:37:52 +0200

    Plugin API: Add configs API
    Change-Id: I4bf2fa023712e5b34fbbdce9f99899dfe231360d

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/configs/Caches.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/configs/Caches.java
@@ -12,17 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.config;
+package com.google.gerrit.extensions.api.configs;
 
 import com.google.gerrit.extensions.common.CacheInfo;
-import com.google.gerrit.extensions.restapi.RestReadView;
-import com.google.inject.Singleton;
 
-@Singleton
-public class GetCache implements RestReadView<CacheResource> {
+import java.util.Collection;
 
-  @Override
-  public CacheInfo apply(CacheResource rsrc) {
-    return ListCaches.newCacheInfo(rsrc.getName(), rsrc.getCache());
-  }
+public interface Caches {
+  Collection<CacheInfo> list();
 }

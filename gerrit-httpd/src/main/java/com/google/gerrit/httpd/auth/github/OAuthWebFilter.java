4259d5c462c5d2bdb02b5bb9e303decf1fadede0
Luca Milanesio <luca.milanesio@gmail.com>
2014-05-29 02:15:49 +0100

    [RFC] GitHub OAuth login for Gerrit
    Reduced version copied from GitHub plugin to allow
    Gerrit to use GitHub OAuth 2.0 as authentication
    provider.
    
    Documentation still WIP but needed a feedback on 
    the direction and if the feature is welcome by
    Gerrit developers and community :-)
    
    Change-Id: I86fb8fab3350ee62ea4c1c3fb633e596f894f883

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/auth/github/OAuthWebFilter.java
@@ -0,0 +1,97 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.httpd.auth.github;
+
+
+import com.google.gerrit.httpd.auth.github.OAuthProtocol.AccessToken;
+import com.google.gerrit.server.CurrentUser;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Singleton
+public class OAuthWebFilter implements Filter {
+  private static final org.slf4j.Logger log = LoggerFactory
+      .getLogger(OAuthWebFilter.class);
+
+  private final GitHubOAuthConfig config;
+  private final Provider<GitHubLogin> loginProvider;
+  private final Provider<CurrentUser> currentUserProvider;
+  private final Provider<OAuthProtocol> oauthProvider;
+
+  @Inject
+  public OAuthWebFilter(GitHubOAuthConfig config, Provider<OAuthProtocol> oauthProtocol,
+      Provider<GitHubLogin> loginProvider,
+      Provider<CurrentUser> currentUserProvider) {
+    this.config = config;
+    this.oauthProvider = oauthProtocol;
+    this.loginProvider = loginProvider;
+    this.currentUserProvider = currentUserProvider;
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+      FilterChain chain) throws IOException, ServletException {
+    if (currentUserProvider.get().isIdentifiedUser()) {
+      chain.doFilter(request, response);
+      return;
+    }
+
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+    String authCode = request.getParameter("oauth");
+    log.debug("OAuthWebFilter(" + httpRequest.getRequestURL() + ") oauth="
+        + authCode);
+
+    GitHubLogin ghLogin = loginProvider.get();
+    if (authCode != null) {
+      ghLogin.login(new AccessToken(authCode));
+    }
+
+    if (oauthProvider.get().isOAuthRequest(httpRequest) && !ghLogin.isLoggedIn()) {
+      ghLogin.login(httpRequest, httpResponse);
+    } else if (config.autoLogin && !ghLogin.isLoggedIn()) {
+      httpResponse.sendRedirect("/login");
+    } else {
+      if (ghLogin.isLoggedIn()) {
+        httpRequest =
+            new AuthenticatedHttpRequest(httpRequest, config.httpHeader,
+                ghLogin.getMyself().getLogin());
+      }
+      chain.doFilter(httpRequest, response);
+    }
+  }
+
+  @Override
+  public void destroy() {
+    log.info("Init");
+  }
+}

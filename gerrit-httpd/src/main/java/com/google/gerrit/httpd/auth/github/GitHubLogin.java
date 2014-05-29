9969e6659b1b3eab6814e72e25a113875fdf344b
Luca Milanesio <luca.milanesio@gmail.com>
2014-05-29 02:15:49 +0100

    GitHub OAuth login for Gerrit
    Change-Id: I86fb8fab3350ee62ea4c1c3fb633e596f894f883

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/auth/github/GitHubLogin.java
@@ -0,0 +1,86 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd.auth.github;
+
+import com.google.inject.servlet.SessionScoped;
+
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SessionScoped
+class GitHubLogin {
+  private static final Logger log = LoggerFactory.getLogger(GitHubLogin.class);
+
+  private String token;
+  private String user;
+  private String state = OAuthProtocol.generateRandomState();
+
+  boolean isLoggedIn() {
+    return token != null && user != null;
+  }
+
+  boolean login(HttpServletRequest request, HttpServletResponse response,
+      OAuthProtocol oauth) throws IOException {
+    if (isLoggedIn()) {
+      return true;
+    }
+
+    log.debug("Login " + this);
+
+    if (OAuthProtocol.isOAuthFinal(request)) {
+      String redirectUrl = oauth.getTargetUrl(request, state);
+      if (redirectUrl == null) {
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        return false;
+      }
+
+      log.debug("Login-Retrieve-User " + this);
+      token = oauth.loginPhase2(request);
+      user = oauth.retrieveUser(token);
+      if (isLoggedIn()) {
+        log.debug("Login-SUCCESS " + this);
+        response.sendRedirect(redirectUrl);
+        return true;
+      } else {
+        response.sendError(HttpStatus.SC_UNAUTHORIZED);
+        return false;
+      }
+    } else {
+      log.debug("Login-PHASE1 " + this);
+      oauth.loginPhase1(request, response, state);
+      return false;
+    }
+  }
+
+  void logout() {
+    token = null;
+    user = null;
+  }
+
+  String getUsername() {
+    return isLoggedIn() ? user : null;
+  }
+
+  @Override
+  public String toString() {
+    return "GitHubLogin [token=" + token + ", user=" + user + "]";
+  }
+}

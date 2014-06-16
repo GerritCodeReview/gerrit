2566acdd80329a1f7d40317e34a23b10459b0f6d
Bruce Zu <bruce.zu@sonymobile.com>
2014-06-16 14:18:46 +0800

    Refactor: extract out Helper.findGroup()
    Extract Helper.findGroup() out from LdapGroupBackend.suggestLdap()
    to make it can be used later, also make it easy to maintain LdapSchema
    and LdapQuery.
    
    Change-Id: Id554ad46d99638b7847478dafeb9e574e46cb4ba

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/GroupException.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+/** An group processing error. */
+public class GroupException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public GroupException(final String message) {
+    super(message);
+  }
+
+  public GroupException(final String message, final Throwable why) {
+    super(message, why);
+  }
+}

040108ba319b2c83c4d181134bf5f50cf85774dc
David Ostrovsky <david@ostrovsky.org>
2014-06-19 23:43:25 +0200

    Revision API: Prepare to expose comments and drafts
    This is a refactoring change to expose comments and drafts to plugin API.
    
    - remove own DraftInput class from server package and reuse existing one
    from the extensions package
    - remove Side enum and reuse the existing enum from extensions package
    - replace CommentRange with Range in CommentInfo class
    
    This is needed to simplify mapping from server side CommentInfo class
    to one from extensions package.  It cannot be used directly because
    of AccountInfo class and needs to be mapped in plugin API layer.
    
    Change-Id: If6c4cbce715f89f84459a6cd97073d1de31b0312

---
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/changes/DraftInput.java
@@ -0,0 +1,6 @@
+package com.google.gerrit.extensions.api.changes;
+
+import com.google.gerrit.extensions.common.Comment;
+
+public class DraftInput extends Comment {
+}
---
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/common/Side.java
@@ -0,0 +1,5 @@
+package com.google.gerrit.extensions.common;
+
+public enum Side {
+  PARENT, REVISION
+}

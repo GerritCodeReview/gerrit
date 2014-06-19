b760f976661f555a0b65b9b7fc6dc7d7029f91c4
David Ostrovsky <david@ostrovsky.org>
2014-06-19 23:43:25 +0200

    Revision API: Prepare to expose comments and drafts
    This is a refactoring change to expose comments and drafts to plugin API.
    
    - remove own DraftInput class from server package and reuse existing one
    from the extensions package
    - remove Side enum and reuse the existing enum from extensions package
    - replace CommentRange with Range in CommentInfo class
    
    This is needed to simplify mapping from server side CommentInfo class
    to one from extensions package.  It cannot be used directly because
    of AccountInfo class and needs to be mapped in plugin API layer.
    
    Change-Id: If6c4cbce715f89f84459a6cd97073d1de31b0312

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PutDraft.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PutDraft.java
@@ -17,18 +17,16 @@ package com.google.gerrit.server.change;
 import static com.google.gerrit.server.PatchLineCommentsUtil.setCommentRevId;
 
 import com.google.gerrit.common.TimeUtil;
-import com.google.gerrit.common.changes.Side;
+import com.google.gerrit.extensions.api.changes.DraftInput;
+import com.google.gerrit.extensions.common.Side;
 import com.google.gerrit.extensions.restapi.BadRequestException;
-import com.google.gerrit.extensions.restapi.DefaultInput;
 import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.extensions.restapi.Url;
-import com.google.gerrit.reviewdb.client.CommentRange;
 import com.google.gerrit.reviewdb.client.Patch;
 import com.google.gerrit.reviewdb.client.PatchLineComment;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.PatchLineCommentsUtil;
-import com.google.gerrit.server.change.PutDraft.Input;
 import com.google.gerrit.server.notedb.ChangeUpdate;
 import com.google.gerrit.server.patch.PatchListCache;
 import com.google.gwtorm.server.OrmException;
@@ -37,23 +35,10 @@ import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 import java.util.Collections;
 
 @Singleton
-class PutDraft implements RestModifyView<DraftResource, Input> {
-  static class Input {
-    String id;
-    String path;
-    Side side;
-    Integer line;
-    String inReplyTo;
-    Timestamp updated; // Accepted but ignored.
-    CommentRange range;
-
-    @DefaultInput
-    String message;
-  }
+class PutDraft implements RestModifyView<DraftResource, DraftInput> {
 
   private final Provider<ReviewDb> db;
   private final DeleteDraft delete;
@@ -75,7 +60,7 @@ class PutDraft implements RestModifyView<DraftResource, Input> {
   }
 
   @Override
-  public Response<CommentInfo> apply(DraftResource rsrc, Input in) throws
+  public Response<CommentInfo> apply(DraftResource rsrc, DraftInput in) throws
       BadRequestException, OrmException, IOException {
     PatchLineComment c = rsrc.getComment();
     ChangeUpdate update = updateFactory.create(rsrc.getControl());
@@ -85,7 +70,7 @@ class PutDraft implements RestModifyView<DraftResource, Input> {
       throw new BadRequestException("id must match URL");
     } else if (in.line != null && in.line < 0) {
       throw new BadRequestException("line must be >= 0");
-    } else if (in.line != null && in.range != null && in.line != in.range.getEndLine()) {
+    } else if (in.line != null && in.range != null && in.line != in.range.endLine) {
       throw new BadRequestException("range endLine must be on the same line as the comment");
     }
 
@@ -117,7 +102,7 @@ class PutDraft implements RestModifyView<DraftResource, Input> {
     return Response.ok(new CommentInfo(c, null));
   }
 
-  private PatchLineComment update(PatchLineComment e, Input in) {
+  private PatchLineComment update(PatchLineComment e, DraftInput in) {
     if (in.side != null) {
       e.setSide(in.side == Side.PARENT ? (short) 0 : (short) 1);
     }
@@ -126,8 +111,8 @@ class PutDraft implements RestModifyView<DraftResource, Input> {
     }
     e.setMessage(in.message.trim());
     if (in.range != null || in.line != null) {
-      e.setRange(in.range);
-      e.setLine(in.range != null ? in.range.getEndLine() : in.line);
+      e.fromRange(in.range);
+      e.setLine(in.range != null ? in.range.endLine : in.line);
     }
     e.setWrittenOn(TimeUtil.nowTs());
     return e;

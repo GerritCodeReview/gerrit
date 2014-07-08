75dc4d5d26d9f5b9114cdd9ad4a9ae1142483083
Michael Zhou <moz@google.com>
2014-07-08 17:13:12 +0530

    Add a dropdown to control email notifications on user preference page
    Add a dropdown 'Email Notifications' in the user preferences to allow
    users to control their email notifications.
    
    On selecting 'Disabled' user will not receive any email notifications
    from Gerrit.
    
    Remove the checkbox 'CC Me On Comments I write' and add it to the new
    dropdown. Functionality remains the same as the original checkbox.
    
    Options in the dropdown are:
    
    1. Enabled
    2. CC On Own Comments
    3. Disabled
    
    Feature: Issue 989
    Change-Id: If40d0d34f39415d5c64ccaa5803fc6321bddd322

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/OutgoingEmail.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/OutgoingEmail.java
@@ -19,6 +19,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import com.google.common.collect.Sets;
 import com.google.gerrit.common.errors.EmailException;
 import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.EmailStrategy;
 import com.google.gerrit.reviewdb.client.UserIdentity;
 import com.google.gerrit.server.account.AccountState;
 import com.google.gerrit.server.mail.EmailHeader.AddressList;
@@ -95,28 +96,25 @@ public abstract class OutgoingEmail {
     if (shouldSendMessage()) {
       if (fromId != null) {
         final Account fromUser = args.accountCache.get(fromId).getAccount();
+        EmailStrategy strategy =
+            fromUser.getGeneralPreferences().getEmailStrategy();
 
-        if (fromUser.getGeneralPreferences().isCopySelfOnEmails()) {
+        if (strategy == EmailStrategy.CC_ON_OWN_COMMENTS) {
           // If we are impersonating a user, make sure they receive a CC of
           // this message so they can always review and audit what we sent
           // on their behalf to others.
           //
           add(RecipientType.CC, fromId);
 
-        } else if (rcptTo.remove(fromId)) {
-          // If they don't want a copy, but we queued one up anyway,
-          // drop them from the recipient lists.
-          //
-          final String fromEmail = fromUser.getPreferredEmail();
-          for (Iterator<Address> i = smtpRcptTo.iterator(); i.hasNext();) {
-            if (i.next().email.equals(fromEmail)) {
-              i.remove();
-            }
-          }
-          for (EmailHeader hdr : headers.values()) {
-            if (hdr instanceof AddressList) {
-              ((AddressList) hdr).remove(fromEmail);
-            }
+        }
+
+        // Check the preferences of all recipients. If any user has disabled
+        // his email notifications then drop him from recipients list
+        for (Account.Id id : rcptTo) {
+          Account thisUser = args.accountCache.get(id).getAccount();
+          if (thisUser.getGeneralPreferences().getEmailStrategy()
+                  == EmailStrategy.DISABLED) {
+            removeUser(thisUser);
           }
 
           if (smtpRcptTo.isEmpty()) {
@@ -140,6 +138,20 @@ public abstract class OutgoingEmail {
       }
 
       args.emailSender.send(va.smtpFromAddress, va.smtpRcptTo, va.headers, va.body);
+    }
+  }
+
+  public void removeUser(Account user) {
+    String fromEmail = user.getPreferredEmail();
+    for (Iterator<Address> j = smtpRcptTo.iterator(); j.hasNext();) {
+      if (j.next().email.equals(fromEmail)) {
+        j.remove();
+      }
+    }
+    for (EmailHeader hdr : headers.values()) {
+      if (hdr instanceof AddressList) {
+        ((AddressList) hdr).remove(fromEmail);
+      }
     }
   }
 

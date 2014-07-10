9d2634293077ccd30f438c9104b63dd7dbec952e
Yacob Yonas <yyonas@google.com>
2014-07-10 16:22:37 -0700

    Refactor ChangeUpdate: Make superclass AbstractChangeUpdate
    Similar to the change that refactors ChangeNotes out, I created a
    superclass, AbstractChangeUpdate, for use by both ChangeUpdate and the
    new Update class I will be writing for the purpose of draft
    comments. I extracted all common variables and methods that I thought
    generically should be part of an "update" of this kind.
    
    However, I left some out (see commit()) because they depended on
    functions that I thought aren't generic enough to be part of a generic
    update (see storeCommentsInNotes()).
    
    Finally, there was one method in ChangeUpdate that I refactored out to
    CommentsInNotesUtil because I knew that it will be needed there. I
    changed that method a bit because the old implementation depended on
    the fact that we were going to just be adding comments to the list of
    preexisting comments. However, with drafts, we will be adding and
    deleting, so I changed that method to force the caller to have already
    made all modifications to the list, so that the method just writes the
    list it is given to a note and writes that note to the NoteMap.
    
    Change-Id: I0229e895e34c4ddd728b461ac64e3b3be936032d

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/AbstractChangeUpdate.java
@@ -0,0 +1,149 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.notedb;
+
+import static com.google.gerrit.server.notedb.ChangeNoteUtil.GERRIT_PLACEHOLDER_HOST;
+
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.MetaDataUpdate;
+import com.google.gerrit.server.git.VersionedMetaData;
+import com.google.gerrit.server.project.ChangeControl;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.CommitBuilder;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+import java.io.IOException;
+import java.util.Date;
+
+/** A single delta related to a specific patch-set of a change. */
+public abstract class AbstractChangeUpdate extends VersionedMetaData {
+  protected final NotesMigration migration;
+  protected final GitRepositoryManager repoManager;
+  protected final MetaDataUpdate.User updateFactory;
+  protected final ChangeControl ctl;
+  protected final PersonIdent serverIdent;
+  protected final Date when;
+  protected PatchSet.Id psId;
+
+  AbstractChangeUpdate(NotesMigration migration,
+      GitRepositoryManager repoManager,
+      MetaDataUpdate.User updateFactory, ChangeControl ctl,
+      PersonIdent serverIdent, Date when) {
+    this.migration = migration;
+    this.repoManager = repoManager;
+    this.updateFactory = updateFactory;
+    this.ctl = ctl;
+    this.serverIdent = serverIdent;
+    this.when = when;
+  }
+
+  public Change getChange() {
+    return ctl.getChange();
+  }
+
+  public Date getWhen() {
+    return when;
+  }
+
+  public IdentifiedUser getUser() {
+    return (IdentifiedUser) ctl.getCurrentUser();
+  }
+
+  private void load() throws IOException {
+    if (migration.write() && getRevision() == null) {
+      Repository repo = repoManager.openRepository(getProjectName());
+      try {
+        load(repo);
+      } catch (ConfigInvalidException e) {
+        throw new IOException(e);
+      } finally {
+        repo.close();
+      }
+    }
+  }
+
+  @Override
+  public BatchMetaDataUpdate openUpdate(MetaDataUpdate update) throws IOException {
+    throw new UnsupportedOperationException("use openUpdate()");
+  }
+
+  public BatchMetaDataUpdate openUpdate() throws IOException {
+    if (migration.write()) {
+      load();
+      MetaDataUpdate md =
+          updateFactory.create(getProjectName(), getUser());
+      md.setAllowEmpty(true);
+      return super.openUpdate(md);
+    }
+    return new BatchMetaDataUpdate() {
+      @Override
+      public void write(CommitBuilder commit) {
+        // Do nothing.
+      }
+
+      @Override
+      public void write(VersionedMetaData config, CommitBuilder commit) {
+        // Do nothing.
+      }
+
+      @Override
+      public RevCommit createRef(String refName) {
+        return null;
+      }
+
+      @Override
+      public RevCommit commit() {
+        return null;
+      }
+
+      @Override
+      public RevCommit commitAt(ObjectId revision) {
+        return null;
+      }
+
+      @Override
+      public void close() {
+        // Do nothing.
+      }
+    };
+  }
+
+  @Override
+  protected void onLoad() throws IOException, ConfigInvalidException {
+    //Do nothing; just reads the current revision.
+  }
+
+  protected PersonIdent newIdent(Account author, Date when) {
+    return new PersonIdent(
+        author.getFullName(),
+        author.getId().get() + "@" + GERRIT_PLACEHOLDER_HOST,
+        when, serverIdent.getTimeZone());
+  }
+
+  /**
+   * @return the NameKey for the project where the update will be stored,
+   *    which is not necessarily the same as the change's project.
+   */
+  abstract protected Project.NameKey getProjectName();
+}

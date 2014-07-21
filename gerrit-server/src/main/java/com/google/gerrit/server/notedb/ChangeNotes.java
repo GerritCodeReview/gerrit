5bd17eb42694720bfb3f5e1c7825028cf576b472
Yacob Yonas <yyonas@google.com>
2014-07-21 10:13:33 -0700

    Made generic methods for modifying comments in updates
    There are two classes responsible for writing updates for comments: 1)
    ChangeUpdate for published comments, and 2) ChangeDraftUpdate for
    draft comments. Now since the workflow for updating both of those
    classes includes modifying just a ChangeUpdate, I decided to make a
    method for updating, inserting, upserting, and deleting comments that
    wraps the actual function calls to update the ChangeUpdate and
    the ChangeDraftUpdate. I decided to have all four of those methods to
    replicate the logic for updating the ReviewDb.  This means that
    callers can just call changeUpdate.*Comment() and not have to worry
    about modifying both the ChangeUpdate and the ChangeDraftUpdate.
    
    The insert method for published and draft comments enforces that we do
    not add a comment with the same UUID as another on the same change and
    clarified function names for the two unique operations (insert and
    upsert) for comments. Finally, I updated tests to use these new
    functions.
    
    In the ReviewDb, it is necessary to ensure that no two comments have
    the same UUID. However, with the notedb, we don't have to assert that
    UUIDs are globally unique, only unique from the UUIDs of comments on
    the same change. This is sufficient because with the notedb design, we
    look comments up by change (using ChangeNotes), not amongst all
    comments on all changes. Therefore, comment UUIDs in the notedb only
    have to be unique within a change.
    
    The update method enforces that a matching comment already existed,
    similar to how the ReviewDb would enforce the same thing if you tried
    to update a comment in there.
    
    Finally, within a ChangeDraftUpdate, I separated the delete comment
    method into two separate methods: one to delete a comment only if the
    comment already existed and one to delete a comment and throw an
    exception if it didn't already exist. This is useful because in some
    cases (like upserting a published comment when you don't know if it
    already existed as a draft before), you don't care if it already
    existed as opposed to in other cases where you expect that the comment
    already existed.
    
    Change-Id: I637c5c6a0979d2622981b255ca5ed3fbd1044033

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
@@ -19,6 +19,7 @@ import static com.google.gerrit.server.notedb.ChangeNoteUtil.FOOTER_PATCH_SET;
 import static com.google.gerrit.server.notedb.ChangeNoteUtil.FOOTER_STATUS;
 import static com.google.gerrit.server.notedb.ChangeNoteUtil.FOOTER_SUBMITTED_WITH;
 import static com.google.gerrit.server.notedb.ChangeNoteUtil.GERRIT_PLACEHOLDER_HOST;
+import static com.google.gerrit.server.notedb.CommentsInNotesUtil.getCommentPsId;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Enums;
@@ -574,6 +575,26 @@ public class ChangeNotes extends AbstractChangeNotes<ChangeNotes> {
       draftCommentNotes.load();
     }
     return draftCommentNotes;
+  }
+
+  public boolean containsComment(PatchLineComment c) throws OrmException {
+    if (containsCommentPublished(c)) {
+      return true;
+    }
+    DraftCommentNotes draftNotes = loadDraftComments(c.getAuthor());
+    return draftNotes.containsComment(c);
+  }
+
+  public boolean containsCommentPublished(PatchLineComment c) {
+    PatchSet.Id psId = getCommentPsId(c);
+    List<PatchLineComment> list = (c.getSide() == (short) 0) ?
+        getBaseComments().get(psId) : getPatchSetComments().get(psId);
+    for (PatchLineComment l : list) {
+      if (c.getKey().equals(l.getKey())) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /** @return the NoteMap */

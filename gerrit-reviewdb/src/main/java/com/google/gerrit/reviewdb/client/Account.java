37040ce56bee8413e089f075b424af0d684a2f49
Yacob Yonas <yyonas@google.com>
2014-07-23 10:05:59 -0700

    Add method to parse an AccountId out of a RefName
    Change-Id: Ie71c6889cf7690e3fd2ac1b2bbe00fd74d381e7d

--- gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/client/Account.java
+++ gerrit-reviewdb/src/main/java/com/google/gerrit/reviewdb/client/Account.java
@@ -104,6 +104,20 @@ public final class Account {
       r.fromString(str);
       return r;
     }
+
+    public static Id fromRef(String name) {
+      final String[] parts = name.split("/");
+      final int n = parts.length;
+      if (n < 2) {
+        throw new IllegalArgumentException("Not an Account.Id: " + name);
+      }
+      final int shard = Integer.parseInt(parts[0]);
+      final int id = Integer.parseInt(parts[1]);
+      if (id % 100 != shard) {
+        throw new IllegalArgumentException("Not an Account.Id: " + name);
+      }
+      return new Account.Id(id);
+    }
   }
 
   @Column(id = 1)

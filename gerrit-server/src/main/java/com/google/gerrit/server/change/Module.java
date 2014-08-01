bb231dd51bbac05c80d93e438f037c96a1e17f2c
David Ostrovsky <david@ostrovsky.org>
2014-08-01 02:24:31 +0200

    InlineEdit: Add REST endpoint to delete content from a change edit
    Allow to delete file from change edit:
    
      DELETE /changes/X/edits/0/files/PATH
    
    Change-Id: I62ea1f3df02a2872ed51456f44fa961dfe518b37

--- gerrit-server/src/main/java/com/google/gerrit/server/change/Module.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Module.java
@@ -106,6 +106,7 @@ public class Module extends RestApiModule {
     child(CHANGE_KIND, "edits").to(ChangeEdits.class);
     child(CHANGE_EDIT_KIND, "files").to(EditFiles.class);
     put(EDIT_FILE_KIND, "content").to(PutContent.class);
+    delete(EDIT_FILE_KIND).to(DeleteContent.class);
 
     install(new FactoryModule() {
       @Override

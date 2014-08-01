7ba9d852bd9f1452df477592a28e66d4ae081bca
Olga Grinberg <olga.grinberg@ericsson.com>
2014-08-01 08:08:39 -0400

    Implement pagination in branch list screen.
    Branch list screen should use page size set in preferences
    and have navigation links to the next and previous pages
    and have a filter field.
    
    Change-Id: I90c5241a82a65addbc12334b068621deaae531b1

--- gerrit-server/src/main/java/com/google/gerrit/server/project/BranchesCollection.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/BranchesCollection.java
@@ -16,6 +16,7 @@ package com.google.gerrit.server.project;
 
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.restapi.AcceptsCreate;
+import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ChildCollection;
 import com.google.gerrit.extensions.restapi.IdString;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
@@ -52,7 +53,7 @@ public class BranchesCollection implements
 
   @Override
   public BranchResource parse(ProjectResource parent, IdString id)
-      throws ResourceNotFoundException, IOException {
+      throws ResourceNotFoundException, IOException, BadRequestException {
     String branchName = id.get();
     if (!branchName.startsWith(Constants.R_REFS)
         && !branchName.equals(Constants.HEAD)) {
--- gerrit-server/src/main/java/com/google/gerrit/server/project/ListBranches.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/ListBranches.java
@@ -15,11 +15,14 @@
 package com.google.gerrit.server.project;
 
 import com.google.common.base.Strings;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.gerrit.extensions.common.ActionInfo;
 import com.google.gerrit.extensions.common.WebLinkInfo;
 import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.extensions.restapi.RestView;
@@ -33,16 +36,21 @@ import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.google.inject.util.Providers;
 
+import dk.brics.automaton.RegExp;
+import dk.brics.automaton.RunAutomaton;
+
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefDatabase;
 import org.eclipse.jgit.lib.Repository;
+import org.kohsuke.args4j.Option;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -52,6 +60,38 @@ public class ListBranches implements RestReadView<ProjectResource> {
   private final GitRepositoryManager repoManager;
   private final DynamicMap<RestView<BranchResource>> branchViews;
   private final Provider<WebLinks> webLinks;
+
+  @Option(name = "--limit", aliases = {"-n"}, metaVar = "CNT",
+      usage = "maximum number of branches to list")
+  private int limit;
+
+  public void setLimit(int limit) {
+    this.limit = limit;
+  }
+
+  @Option(name = "--start", aliases = {"-s"}, metaVar = "CNT",
+      usage = "number of branches to skip")
+  private int start;
+
+  public void setStart(int start) {
+    this.start = start;
+  }
+
+  @Option(name = "--match", aliases = {"-m"}, metaVar = "MATCH",
+      usage = "match branches substring")
+  private String matchSubstring;
+
+  public void setMatchSubstring(String matchSubstring) {
+    this.matchSubstring = matchSubstring;
+  }
+
+  @Option(name = "--regex", aliases = {"-r"}, metaVar = "REGEX",
+      usage = "match branches regex")
+  private String matchRegex;
+
+  public void setMatchRegex(String matchRegex) {
+    this.matchRegex = matchRegex;
+  }
 
   @Inject
   public ListBranches(GitRepositoryManager repoManager,
@@ -64,7 +104,7 @@ public class ListBranches implements RestReadView<ProjectResource> {
 
   @Override
   public List<BranchInfo> apply(ProjectResource rsrc)
-      throws ResourceNotFoundException, IOException {
+      throws ResourceNotFoundException, IOException, BadRequestException {
     List<BranchInfo> branches = Lists.newArrayList();
 
     BranchInfo headBranch = null;
@@ -150,6 +190,57 @@ public class ListBranches implements RestReadView<ProjectResource> {
     }
     if (headBranch != null) {
       branches.add(0, headBranch);
+    }
+
+    List<BranchInfo> filteredBranches;
+    if ((matchSubstring != null && !matchSubstring.isEmpty())
+        || (matchRegex != null && !matchRegex.isEmpty())) {
+      filteredBranches = filterBranches(branches);
+    } else {
+      filteredBranches = branches;
+    }
+
+    if (filteredBranches.isEmpty()) {
+      return filteredBranches;
+    } else {
+      return filteredBranches.subList(start, (start + limit) <
+          filteredBranches.size() ? (start + limit) : filteredBranches.size());
+    }
+  }
+
+  private List<BranchInfo> filterBranches(List<BranchInfo> branches)
+      throws BadRequestException {
+    if (matchSubstring != null) {
+      return ((List<BranchInfo>) Lists.newArrayList(Iterables.filter(branches,
+          new Predicate<BranchInfo>() {
+            public boolean apply(BranchInfo in) {
+              return in.ref.toLowerCase(Locale.US).contains(
+                  matchSubstring.toLowerCase(Locale.US));
+            }
+          })));
+    } else if (matchRegex != null) {
+      if (matchRegex.startsWith("^")) {
+        matchRegex = matchRegex.substring(1);
+        if (matchRegex.endsWith("$") && !matchRegex.endsWith("\\$")) {
+          matchRegex = matchRegex.substring(0, matchRegex.length() - 1);
+        }
+      }
+      if (matchRegex.equals(".*")) {
+        return branches.subList(start, (start + limit) < branches.size()
+            ? (start + limit) : branches.size() - 1);
+      }
+      try {
+        final RunAutomaton a =
+            new RunAutomaton(new RegExp(matchRegex).toAutomaton());
+        return ((List<BranchInfo>) Lists.newArrayList(Iterables.filter(
+            branches, new Predicate<BranchInfo>() {
+              public boolean apply(BranchInfo in) {
+                return a.run(in.ref);
+              }
+            })));
+      } catch (IllegalArgumentException e) {
+        throw new BadRequestException(e.getMessage());
+      }
     }
     return branches;
   }

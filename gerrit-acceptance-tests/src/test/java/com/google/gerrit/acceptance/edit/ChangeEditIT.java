64b4aecde132f62e744c0a178dbb593e415e90a0
David Ostrovsky <david@ostrovsky.org>
2014-08-01 09:23:28 +0200

    InlineEdit: Allow to list files in change edit
    Add --list and --base option to GET edit REST endpoint to list files
    in change edit:
    
      GET /changes/<id>/edit?list
    
    Change-Id: Idfb6278c67750c09b41653dda050b560d05695f8

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/edit/ChangeEditIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/edit/ChangeEditIT.java
@@ -28,12 +28,14 @@ import static org.junit.Assert.fail;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.AcceptanceTestRequestScope;
 import com.google.gerrit.acceptance.PushOneCommit;
 import com.google.gerrit.acceptance.RestResponse;
 import com.google.gerrit.acceptance.RestSession;
 import com.google.gerrit.extensions.common.EditInfo;
+import com.google.gerrit.extensions.common.FileInfo;
 import com.google.gerrit.extensions.restapi.BinaryResult;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.reviewdb.client.Change;
@@ -47,6 +49,7 @@ import com.google.gerrit.server.edit.ChangeEdit;
 import com.google.gerrit.server.edit.ChangeEditModifier;
 import com.google.gerrit.server.edit.ChangeEditUtil;
 import com.google.gerrit.server.project.InvalidChangeOperationException;
+import com.google.gson.reflect.TypeToken;
 import com.google.gwtorm.server.SchemaFactory;
 import com.google.inject.Inject;
 import com.google.inject.util.Providers;
@@ -66,6 +69,8 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class ChangeEditIT extends AbstractDaemonTest {
@@ -270,7 +275,7 @@ public class ChangeEditIT extends AbstractDaemonTest {
             FILE_NAME,
             CONTENT_NEW));
     edit = editUtil.byChange(change);
-    EditInfo info = toEditInfo();
+    EditInfo info = toEditInfo(false);
     assertEquals(edit.get().getRevision().get(), info.commit.commit);
     assertEquals(1, info.commit.parents.size());
 
@@ -279,6 +284,26 @@ public class ChangeEditIT extends AbstractDaemonTest {
 
     r = session.get(urlEdit());
     assertEquals(SC_NO_CONTENT, r.getStatusCode());
+  }
+
+  @Test
+  public void retrieveFilesInEdit() throws Exception {
+    assertEquals(RefUpdate.Result.NEW,
+        modifier.createEdit(
+            change,
+            ps));
+    Optional<ChangeEdit> edit = editUtil.byChange(change);
+    assertEquals(RefUpdate.Result.FORCED,
+        modifier.modifyFile(
+            edit.get(),
+            FILE_NAME,
+            CONTENT_NEW));
+
+    EditInfo info = toEditInfo(true);
+    assertEquals(2, info.files.size());
+    List<String> l = Lists.newArrayList(info.files.keySet());
+    assertEquals("/COMMIT_MSG", l.get(0));
+    assertEquals("foo", l.get(1));
   }
 
   @Test
@@ -642,14 +667,19 @@ public class ChangeEditIT extends AbstractDaemonTest {
         + FILE_NAME;
   }
 
+  private String urlGetFiles() {
+    return urlEdit()
+        + "?list";
+  }
+
   private String urlPublish() {
     return "/changes/"
         + change.getChangeId()
         + "/publish_edit";
   }
 
-  private EditInfo toEditInfo() throws IOException {
-    RestResponse r = session.get(urlEdit());
+  private EditInfo toEditInfo(boolean files) throws IOException {
+    RestResponse r = session.get(files ? urlGetFiles() : urlEdit());
     assertEquals(SC_OK, r.getStatusCode());
     return newGson().fromJson(r.getReader(), EditInfo.class);
   }

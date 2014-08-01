a348bc7f03e10639d9423e6ee9560fbdf4558279
David Ostrovsky <david@ostrovsky.org>
2014-08-01 09:56:34 +0200

    InlineEdit: Add REST endpoint to publish change edit
    Allow to publish change edit:
    
      POST /changes/X/edits/0/publish
    
    Change-Id: Ifd3203539b00ec2a7b60734d501b6ae0790e73f9

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/edit/ChangeEditIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/edit/ChangeEditIT.java
@@ -137,6 +137,25 @@ public class ChangeEditIT extends AbstractDaemonTest {
   }
 
   @Test
+  public void publishEditRest() throws Exception {
+    RestResponse r = session.post(urlCreateEdit());
+    assertEquals(HttpStatus.SC_NO_CONTENT, r.getStatusCode());
+    Optional<ChangeEdit> edit = editUtil.byChange(change);
+    assertTrue(edit.isPresent());
+    assertEquals(RefUpdate.Result.FORCED,
+        modifier.modifyFile(
+            edit.get(),
+            FILE_NAME,
+            Constants.encode(CONTENT_NEW)));
+    edit = editUtil.byChange(change);
+    assertTrue(edit.isPresent());
+    session.post(urlPublishEdit());
+    assertEquals(HttpStatus.SC_NO_CONTENT, r.getStatusCode());
+    edit = editUtil.byChange(change);
+    assertFalse(edit.isPresent());
+  }
+
+  @Test
   public void updateExistingFile() throws Exception {
     assertEquals(RefUpdate.Result.NEW,
         modifier.createEdit(
@@ -489,6 +508,14 @@ public class ChangeEditIT extends AbstractDaemonTest {
             + 0
             + "/files/"
             + FILE_NAME;
+  }
+
+  private String urlPublishEdit() {
+    return "/changes/"
+        + change.getChangeId()
+        + "/edits/"
+        + 0
+        + "/publish/";
   }
 
   private String urlPut(Change c) {

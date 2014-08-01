8fc14de188131c246a3bd17e1710d52bd740693e
David Ostrovsky <david@ostrovsky.org>
2014-08-01 18:46:31 +0200

    InlineEdit: Factor out get content method for file resource
    Currently there are two callers of get content class for file resources.
    Extract this method in FileContentUtil class to reuse it also for inline
    edit feature.
    
    Change-Id: Ie43846d20170fd12c8c87e26349ffa85dc8296ff

--- gerrit-server/src/main/java/com/google/gerrit/server/change/GetContent.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/GetContent.java
@@ -17,65 +17,26 @@ package com.google.gerrit.server.change;
 import com.google.gerrit.extensions.restapi.BinaryResult;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.RestReadView;
-import com.google.gerrit.reviewdb.client.Project;
-import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import org.eclipse.jgit.lib.ObjectLoader;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.treewalk.TreeWalk;
-
 import java.io.IOException;
-import java.io.OutputStream;
 
 @Singleton
 public class GetContent implements RestReadView<FileResource> {
-  private final GitRepositoryManager repoManager;
+  private final FileContentUtil fileContentUtil;
 
   @Inject
-  GetContent(GitRepositoryManager repoManager) {
-    this.repoManager = repoManager;
+  GetContent(FileContentUtil fileContentUtil) {
+    this.fileContentUtil = fileContentUtil;
   }
 
   @Override
   public BinaryResult apply(FileResource rsrc)
       throws ResourceNotFoundException, IOException {
-    return apply(rsrc.getRevision().getControl().getProject().getNameKey(),
+    return fileContentUtil.getContent(
+        rsrc.getRevision().getControl().getProject().getNameKey(),
         rsrc.getRevision().getPatchSet().getRevision().get(),
         rsrc.getPatchKey().get());
-  }
-
-  public BinaryResult apply(Project.NameKey project, String revstr, String path)
-      throws ResourceNotFoundException, IOException {
-    Repository repo = repoManager.openRepository(project);
-    try {
-      RevWalk rw = new RevWalk(repo);
-      try {
-        RevCommit commit =
-            rw.parseCommit(repo.resolve(revstr));
-        TreeWalk tw =
-            TreeWalk.forPath(rw.getObjectReader(), path,
-                commit.getTree().getId());
-        if (tw == null) {
-          throw new ResourceNotFoundException();
-        }
-        final ObjectLoader object = repo.open(tw.getObjectId(0));
-        @SuppressWarnings("resource")
-        BinaryResult result = new BinaryResult() {
-          @Override
-          public void writeTo(OutputStream os) throws IOException {
-            object.copyTo(os);
-          }
-        };
-        return result.setContentLength(object.getSize()).base64();
-      } finally {
-        rw.release();
-      }
-    } finally {
-      repo.close();
-    }
   }
 }

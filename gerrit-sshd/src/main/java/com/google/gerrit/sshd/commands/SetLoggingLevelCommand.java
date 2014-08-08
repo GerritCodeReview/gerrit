31e475ec8b00e50f0434a827e9c39dd82321d1fc
Simon Lei <simon.lei@ericsson.com>
2014-08-08 11:12:38 -0400

    Add SSH to configure logging level at runtime
    Can configure the logging level of loggers with SSH command. This allows
    the admin to change the logging level without restarting the server.
    
    Add additional command to print the logging level of loggers.
    
    Change-Id: I3bd3076bf617607430b86d23ca53d48203afcd76

---
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/SetLoggingLevelCommand.java
@@ -0,0 +1,109 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd.commands;
+
+import static com.google.gerrit.sshd.CommandMetaData.Mode.MASTER_OR_SLAVE;
+
+import com.google.gerrit.common.data.GlobalCapability;
+import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gerrit.sshd.CommandMetaData;
+import com.google.gerrit.sshd.SshCommand;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
+
+import java.util.Enumeration;
+
+@RequiresCapability(GlobalCapability.ADMINISTRATE_SERVER)
+@CommandMetaData(name = "set-logging-level", description = "Change the level of loggers",
+  runsAt = MASTER_OR_SLAVE)
+public class SetLoggingLevelCommand extends SshCommand {
+  private static final String LOG_CONFIGURATION = "log4j.properties";
+  private static final String RESET = "reset";
+  private static final String LEVEL_ALL = "all";
+  private static final String LEVEL_TRACE = "trace";
+  private static final String LEVEL_DEBUG = "debug";
+  private static final String LEVEL_INFO = "info";
+  private static final String LEVEL_WARN = "warn";
+  private static final String LEVEL_ERROR = "error";
+  private static final String LEVEL_FATAL = "fatal";
+  private static final String LEVEL_OFF = "off";
+
+  @Argument(index = 0, required = true, metaVar = "LEVEL", usage = "logging level to set to")
+  private String level;
+
+  @Option(name = "--logger", aliases = "-l", metaVar = "LOGGER", usage = "loggers to change")
+  private String regex;
+
+  @Override
+  protected void run() throws UnloggedFailure, Failure, Exception {
+    if (regex == null) {
+      regex = "^*.*$";
+    }
+    switch (level.toLowerCase()) {
+      case RESET:
+        reset();
+        break;
+      case LEVEL_ALL:
+        setLoggingLevel(Level.ALL);
+        break;
+      case LEVEL_TRACE:
+        setLoggingLevel(Level.TRACE);
+        break;
+      case LEVEL_DEBUG:
+        setLoggingLevel(Level.DEBUG);
+        break;
+      case LEVEL_INFO:
+        setLoggingLevel(Level.INFO);
+        break;
+      case LEVEL_WARN:
+        setLoggingLevel(Level.WARN);
+        break;
+      case LEVEL_ERROR:
+        setLoggingLevel(Level.ERROR);
+        break;
+      case LEVEL_FATAL:
+        setLoggingLevel(Level.FATAL);
+        break;
+      case LEVEL_OFF:
+        setLoggingLevel(Level.OFF);
+        break;
+      default:
+        stdout.println("Invalid argument");
+        break;
+    }
+  }
+
+  private void reset() {
+    LogManager.resetConfiguration();
+    PropertyConfigurator.configure(getClass().getClassLoader().getResource(
+        LOG_CONFIGURATION));
+  }
+
+  @SuppressWarnings("unchecked")
+  private void setLoggingLevel(Level level) {
+    for (Enumeration<Logger> logger = LogManager.getCurrentLoggers();
+        logger.hasMoreElements();) {
+      Logger log = logger.nextElement();
+      if (log.getName().matches(regex)) {
+        log.setLevel(level);
+      }
+    }
+  }
+}

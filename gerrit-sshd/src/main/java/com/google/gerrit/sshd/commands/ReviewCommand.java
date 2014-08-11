140de84fededa5b182b718efad692d3dc00d0ef5
Simon Lei <simon.lei@ericsson.com>
2014-08-11 15:25:02 -0400

    Set default for review SSH command to notify=ALL
    When the --notify option wasn't given, it defaulted to notify=NONE.
    Make it default to notify=ALL.
    
    Bug: issue 2801
    Change-Id: I799d3036f93286328311f3339a0e384cc56542c3

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ReviewCommand.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/ReviewCommand.java
@@ -242,6 +242,9 @@ public class ReviewCommand extends SshCommand {
     if (changeComment == null) {
       changeComment = "";
     }
+    if (notify == null) {
+      notify = NotifyHandling.ALL;
+    }
 
     ReviewInput review = new ReviewInput();
     review.message = Strings.emptyToNull(changeComment);

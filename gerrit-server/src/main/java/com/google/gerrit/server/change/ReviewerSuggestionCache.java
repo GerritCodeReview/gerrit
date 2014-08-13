749d25f012f4c5ad52e71484dcb7611b2c821830
Sven Selberg <sven.selberg@sonymobile.com>
2014-08-13 11:20:11 +0200

    Full text search in memory for review suggestions
    This change also makes it possible to configure maximum
    displayed reviewers.
    
    On some Gerrit instances the full name is formatted like:
    <given name> <surname>
    and email like:
    <given name>.<surname>@...
    This would make it impossible to get reviewer suggestions from
    surnames.
    Since gwtorm doesn't support sql LIKE there is no straight forward
    way of filtering on substring in the DB. Hence this in memory approach.
    
    For performance reasons this implementation differs from the default
    implementation in that it does not look at the email_address in
    account_external_ids but only at the preferred_email of accounts.
    The default implementation does only look for 10 matches and afterwards
    filters out the acconts that are not allowed to view the change.
    
    Configuration:
    suggest.maxsuggestedreviewers
    Maximum number of suggested reviewers (default 10).
    
    suggest.fulltextsearch
    Enable full text search (default "false").
    
    suggest.fulltextsearchmaxmatches
    Maximum number of matches to be checked for accessability when using
    full text search (default 100).
    
    Change-Id: Ia4c3a15263783bc144e66a05854c3915392095b5

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ReviewerSuggestionCache.java
@@ -0,0 +1,65 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gwtorm.server.SchemaFactory;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Singleton
+public class ReviewerSuggestionCache {
+  private static final Logger log = LoggerFactory
+      .getLogger(ReviewerSuggestionCache.class);
+  private final LoadingCache<Boolean, List<Account>> cache;
+
+  @Inject
+  ReviewerSuggestionCache(final Provider<ReviewDb> dbProvider) {
+    this.cache =
+        CacheBuilder.newBuilder().maximumSize(1)
+            .expireAfterWrite(30, TimeUnit.SECONDS)
+            .build(new CacheLoader<Boolean, List<Account>>() {
+              @Override
+              public List<Account> load(Boolean key) throws Exception {
+                return ImmutableList.copyOf(dbProvider.get().accounts().all());
+              }
+            });
+  }
+
+  List<Account> get() {
+    try {
+      return cache.get(true);
+    } catch (ExecutionException e) {
+      log.warn("Cannot fetch reviewers from cache", e);
+      return Collections.emptyList();
+    }
+  }
+}

6437cd4335f9b41ae6e39d2e1e82adb493d1ef43
Adrian Görler <adrian.goerler@sap.com>
2014-08-22 17:09:18 +0200

    Allow to listen to garbage collector events
    A new event type is introduced that allows to listen to garbage
    collector events. This enables to trigger actions that depend on
    changes in the file size footprint of a project such as quota
    enforcement or usage data monitoring.
    
    Change-Id: I1eb913a618df5fe972f3f516af504c13871e5fa5

---
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/events/GarbageCollectorListener.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.extensions.events;
+
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+
+import java.util.Properties;
+
+/**
+ * Notified whenever the garbage collector has run successfully on a project.
+ */
+@ExtensionPoint
+public interface GarbageCollectorListener {
+  public interface Event {
+    /**
+     * The name of the Gerrit project that has been garbage collected
+     * @return
+     */
+    String getProjectName();
+
+    /**
+     * Properties describing the result of the garbage collection performed by
+     * JGit
+     *
+     * @see org.eclipse.jgit.api.GarbageCollectCommand#call()
+     */
+    Properties getStatistics();
+  }
+
+  void onGarbageCollected(Event event);
+}

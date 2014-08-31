5bdc2d9fd9b7e75b593d5a12c470f0f52e20d055
Orgad Shaneh <orgads@gmail.com>
2014-08-31 17:48:29 +0300

    Mark direct child commits in Related Changes
    Bug: issue 2372
    Change-Id: I112712edd9494c75ec0d2bca1e23701fe2e3540a

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/RelatedChanges.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/RelatedChanges.java
@@ -56,6 +56,7 @@ public class RelatedChanges extends TabPanel {
     String gitweb();
     String indirect();
     String notCurrent();
+    String directChild();
     String pointer();
     String row();
     String subject();

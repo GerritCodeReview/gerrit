85f97710d3580ac3db79e7148e06558191bddb6b
David Pursehouse <david.pursehouse@sonymobile.com>
2014-09-04 22:23:53 +0900

    Don't require secondary index when running daemon in slave mode
    Add a dummy index implementation and install it when the daemon is
    started in slave mode.
    
    Make the reindex program exit with an error if run on a server that
    is configured to run in slave mode.
    
    Disable the query ssh command when in slave mode.
    
    Change-Id: Ia323d7dfc3b8f333857ba310072e41943878c11b

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/Daemon.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/Daemon.java
@@ -55,6 +55,7 @@ import com.google.gerrit.server.config.MasterNodeStartup;
 import com.google.gerrit.server.contact.HttpContactStoreConnection;
 import com.google.gerrit.server.git.ReceiveCommitsExecutorModule;
 import com.google.gerrit.server.git.WorkQueue;
+import com.google.gerrit.server.index.DummyIndexModule;
 import com.google.gerrit.server.index.IndexModule;
 import com.google.gerrit.server.index.IndexModule.IndexType;
 import com.google.gerrit.server.mail.SignedTokenEmailTokenVerifier;
@@ -357,6 +358,9 @@ public class Daemon extends SiteProgram {
   }
 
   private AbstractModule createIndexModule() {
+    if (slave) {
+      return new DummyIndexModule();
+    }
     IndexType indexType = IndexModule.getIndexType(cfgInjector);
     switch (indexType) {
       case LUCENE:

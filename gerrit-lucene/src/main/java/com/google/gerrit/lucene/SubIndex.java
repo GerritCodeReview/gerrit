7d28fe8e9eb88b423fdab23eafecd255b9ea933e
Dave Borowitz <dborowitz@google.com>
2014-09-05 15:30:51 -0700

    Lucene: Fix deprecated IndexWriter.close() call
    Change-Id: Id5296a32e6d3d6ef62e02b0492875528d06b5041

--- gerrit-lucene/src/main/java/com/google/gerrit/lucene/SubIndex.java
+++ gerrit-lucene/src/main/java/com/google/gerrit/lucene/SubIndex.java
@@ -159,7 +159,7 @@ class SubIndex {
     try {
       writer.getIndexWriter().commit();
       try {
-        writer.getIndexWriter().close(true);
+        writer.getIndexWriter().close();
       } catch (AlreadyClosedException e) {
         // Ignore.
       }

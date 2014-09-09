6aa6ed92cd445f6fcfda285b064e5a58b80730eb
Dariusz Luksza <dariusz@luksza.org>
2014-09-09 10:04:50 +0200

    Enable SecureStore configuration during init step
    Add new flag for init step:
    
     * --secure-store-lib
    
    That enables configuration of SecureStore during init step. It will
    automatically discover implementation of SecureStore interface inside
    jar file. In case of having more or less then one implementation
    exception will be thrown.
    
    During init step SecureStore jar file will be added to Gerrit classpath.
    Then after init this file will be copied to $gerrit_site/lib directory.
    Also discovered value of 'secureStoreImpl' will be saved in
    gerrit.config
    file.
    
    Without this modification schema updates will fail because of wrong
    password when custom SecureStore is used.
    
    Change-Id: Iae22bbdace0d9c7e7db0690c4bf522176fc3308e
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

---
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/util/SecureStoreClassNameProvider.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm.util;
+
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
+
+import java.io.IOException;
+
+public class SecureStoreClassNameProvider implements Provider<String> {
+  private final String secureStoreClass;
+
+  @Inject
+  SecureStoreClassNameProvider(SitePaths sitePath) {
+    FileBasedConfig cfg = new FileBasedConfig(sitePath.gerrit_config, FS.DETECTED);
+    try {
+      cfg.load();
+    } catch (IOException | ConfigInvalidException e) {
+      throw new RuntimeException("Cannot read gerrit.config file", e);
+    }
+    secureStoreClass = cfg.getString("gerrit", null, "secureStoreClass");
+  }
+
+  @Override
+  public String get() {
+    return secureStoreClass;
+  }
+}

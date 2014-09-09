27527d44013da816a53f9806cfb5e13e10d987ea
Dariusz Luksza <dariusz@luksza.org>
2014-09-09 10:04:50 +0200

    Enable SecureStore configuration during init
    Add new flag for init step:
    
     * --secure-store-lib
    
    That enables configuration of SecureStore during init step. It will
    automatically discover implementation of SecureStore interface inside
    jar file. If there is not exactly one implementation the init will fail.
    
    During init the SecureStore jar file will be added to Gerrit classpath.
    Then after init this file will be copied to $gerrit_site/lib directory.
    The discovered value of 'secureStoreImpl' will be saved in the
    gerrit.config file.
    
    Without this modification schema updates will fail because of wrong
    password when custom SecureStore is used.
    
    Change-Id: Iae22bbdace0d9c7e7db0690c4bf522176fc3308e
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

---
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitStepSecureStoreProvider.java
@@ -0,0 +1,34 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm.init;
+
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.gerrit.server.securestore.SecureStoreProvider;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+
+class InitStepSecureStoreProvider extends SecureStoreProvider {
+
+  @Inject
+  InitStepSecureStoreProvider(Injector injector, SitePaths sitePaths,
+      @Nullable SecureStoreInitData secureStoreInitData) {
+    super(injector, sitePaths);
+
+    if (secureStoreInitData != null) {
+      this.secureStoreClassName = secureStoreInitData.className;
+    }
+  }
+}

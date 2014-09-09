0965216908aabe74f8167bc5616264e4e3cfe184
Sven Selberg <sven.selberg@sonymobile.com>
2014-09-09 00:45:00 -0700

    UI and client parts for hashtag manipulation
    Add UI components to view/add/remove hashtags.
    
    The new UI panel will only be visible if NoteDB is enable.
    
    Change-Id: Icb2c24bfc7f5d63d76f80ddcfb6b72fef02e370c

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeInfo.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeInfo.java
@@ -107,6 +107,7 @@ public class ChangeInfo extends JavaScriptObject {
   public final native void set_edit(EditInfo edit) /*-{ this.edit = edit; }-*/;
   public final native EditInfo edit() /*-{ return this.edit; }-*/;
   public final native boolean has_edit() /*-{ return this.hasOwnProperty('edit') }-*/;
+  public final native JsArrayString hashtags() /*-{ return this.hashtags; }-*/;
 
   public final native boolean has_permitted_labels()
   /*-{ return this.hasOwnProperty('permitted_labels') }-*/;

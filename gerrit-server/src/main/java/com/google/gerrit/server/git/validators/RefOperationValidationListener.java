41138ecf5870fb717ab1dd4144495735d29a597e
Jacek Centkowski <geminica.programs@gmail.com>
2014-09-10 07:42:46 -0400

    Introduce ref operation validation
    Create extension point that is responsible for ref oprations validation.
    
    Change-Id: Ia6386ce6480ae63b75ec3b2836267c2f8d68d426
    Signed-off-by: Jacek Centkowski <geminica.programs@gmail.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/validators/RefOperationValidationListener.java
@@ -0,0 +1,30 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.server.git.validators;
+
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+import com.google.gerrit.server.events.RefOperationReceivedEvent;
+import com.google.gerrit.server.validators.ValidationException;
+
+import java.util.List;
+
+/**
+ * Listener to provide validation on operation that is going to be performed on
+ * given ref
+ */
+@ExtensionPoint
+public interface RefOperationValidationListener {
+  List<ValidationMessage> onRefOperation(RefOperationReceivedEvent refEvent)
+      throws ValidationException;
+}

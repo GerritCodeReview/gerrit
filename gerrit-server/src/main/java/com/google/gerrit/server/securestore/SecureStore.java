864d5d24656b05ea0ac548300e32ebecb0989744
Dariusz Luksza <dariusz@luksza.org>
2014-09-10 11:14:19 +0200

    Add documentation for SecureStore
    Provides javadoc and documentation for SecureStore.
    
    Change-Id: I24a5565dc7d38b506a77459aa13fccd606f0e408
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-server/src/main/java/com/google/gerrit/server/securestore/SecureStore.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/securestore/SecureStore.java
@@ -18,12 +18,38 @@ import com.google.common.collect.Lists;
 
 import java.util.List;
 
+/**
+ * Abstract class for providing new SecureStore implementation for Gerrit.
+ *
+ * SecureStore is responsible for storing sensitive data like passwords in a
+ * secure manner.
+ *
+ * It is implementator's responsibility to encrypt and store values.
+ *
+ * To deploy new SecureStore one needs to provide a jar file with explicitly one
+ * class that extends {@code SecureStore} and put it in Gerrit server. Then run:
+ *
+ * `java -jar gerrit.war SwitchSecureStore -d $gerrit_site --new-secure-store-lib
+ *  $path_to_new_secure_store.jar`
+ *
+ * on stopped Gerrit instance.
+ */
 public abstract class SecureStore {
+  /**
+   * Describes {@link SecureStore} entry
+   */
   public static class EntryKey {
     public final String name;
     public final String section;
     public final String subsection;
 
+    /**
+     * Creates EntryKey.
+     *
+     * @param section
+     * @param subsection
+     * @param name
+     */
     public EntryKey(String section, String subsection, String name) {
       this.name = name;
       this.section = section;
@@ -31,6 +57,15 @@ public abstract class SecureStore {
     }
   }
 
+  /**
+   * Extract decrypted value of stored property from SecureStore or {@code null}
+   * when property was not found.
+   *
+   * @param section
+   * @param subsection
+   * @param name
+   * @return decrypted String value or {@code null} if not found
+   */
   public final String get(String section, String subsection, String name) {
     String[] values = getList(section, subsection, name);
     if (values != null && values.length > 0) {
@@ -39,15 +74,55 @@ public abstract class SecureStore {
     return null;
   }
 
+  /**
+   * Extract list of values from SecureStore and decrypt every value in that
+   * list or {@code null} when property was not found.
+   *
+   * @param section
+   * @param subsection
+   * @param name
+   * @return decrypted list of string values or {@code null}
+   */
   public abstract String[] getList(String section, String subsection, String name);
 
+  /**
+   * Store single value in SecureStore.
+   *
+   * This method is responsible for encrypting value and storing it.
+   *
+   * @param section
+   * @param subsection
+   * @param name
+   * @param value plain text value
+   */
   public final void set(String section, String subsection, String name, String value) {
     setList(section, subsection, name, Lists.newArrayList(value));
   }
 
+  /**
+   * Store list of values in SecureStore.
+   *
+   * This method is responsible for encrypting all values in the list and storing them.
+   *
+   * @param section
+   * @param subsection
+   * @param name
+   * @param values list of plain text values
+   */
   public abstract void setList(String section, String subsection, String name, List<String> values);
 
+  /**
+   * Remove value for given {@code section}, {@code subsection} and {@code name}
+   * from SecureStore.
+   *
+   * @param section
+   * @param subsection
+   * @param name
+   */
   public abstract void unset(String section, String subsection, String name);
 
+  /**
+   * @return list of stored entries.
+   */
   public abstract Iterable<EntryKey> list();
 }

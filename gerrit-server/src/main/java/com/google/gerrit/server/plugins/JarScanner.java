06a5bc3d92fe7aa2d0a594e48764e4590eb2931f
Dariusz Luksza <dariusz@luksza.org>
2014-09-10 10:04:02 +0200

    Make SecureStore an abstract class
    This modification will make switch-secure-store site program
    implementation easier.
    
    In switch-secure-store we need to migrate all values stored in old
    SecureStore to new one. Because we don't store (and also can't reliably
    figure out) value type the only possible way is to first try read value
    as String, then when it is null try to get it as List. But this
    assumption will fail with jgit Config implementation which is used in
    DefaultSecureStore. JGit Config during getString() method call will call
    getRawStringList() and return its first element. Therefore migration
    will be incorrect for all stored List values.
    
    The solution here is to always use getList() and setList() in
    switch-secure-store and repeat jgit behavior in SecureStore. Making the
    get() method to return getList()[0] and set() to call setList() with
    single element.
    
    To provide default implementations of get() and set() method in
    SecureStore we need to make it an abstract class. Also those methods are
    marked as final to prevent implementors from overriding them.
    
    Also changes JarScanner.findImplementationsOf to findChildOf(). This
    method was introduced for SecureStore (and it was only used by it), so
    now when SecureStore becomes abstract class its implementation (and
    name) also should be updated.
    
    Change-Id: Id13bc6ec170c31b43b5a1cd696ba746006e1f0b2
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-server/src/main/java/com/google/gerrit/server/plugins/JarScanner.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/JarScanner.java
@@ -18,6 +18,7 @@ import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.collect.Iterables.transform;
 
 import com.google.common.base.Function;
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.base.Predicates;
 import com.google.common.base.Strings;
@@ -43,7 +44,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -138,10 +138,10 @@ public class JarScanner implements PluginContentScanner {
     return result.build();
   }
 
-  public List<String> findImplementationsOf(final Class<?> requestedInterface)
+  public List<String> findChildOf(final Class<?> superClass)
       throws InvalidPluginException {
     List<String> result = Lists.newArrayList();
-    String name = requestedInterface.getName().replace('.', '/');
+    String name = superClass.getName().replace('.', '/');
 
     Enumeration<JarEntry> e = jarFile.entries();
     while (e.hasMoreElements()) {
@@ -162,8 +162,7 @@ public class JarScanner implements PluginContentScanner {
         continue;
       }
 
-      if (def.isConcrete() && def.interfaces != null
-          && Iterables.contains(Arrays.asList(def.interfaces), name)) {
+      if (def.isConcrete() && Objects.equal(def.superName, name)) {
         result.add(def.className);
       }
     }
@@ -199,6 +198,7 @@ public class JarScanner implements PluginContentScanner {
   public static class ClassData extends ClassVisitor {
     int access;
     String className;
+    String superName;
     String annotationName;
     String annotationValue;
     String[] interfaces;
@@ -219,7 +219,7 @@ public class JarScanner implements PluginContentScanner {
         String superName, String[] interfaces) {
       this.className = Type.getObjectType(name).getClassName();
       this.access = access;
-      this.interfaces = interfaces;
+      this.superName = superName;
     }
 
     @Override

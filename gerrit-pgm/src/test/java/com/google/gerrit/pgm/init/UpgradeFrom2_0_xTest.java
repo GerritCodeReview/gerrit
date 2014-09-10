8a28014b5fcf9eaf9d125c4a89a7bebdae62c2dc
Dariusz Luksza <dariusz@luksza.org>
2014-09-10 10:04:02 +0200

    Make SecureStore an abstract class
    This modification will make switch-secure-store site program
    implementation easier.
    
    In switch-secure-store we need to migrate all values stored in old
    SecureStore to new one. Because we don't store (and also can't reliably
    figure out) value type the only possible way is to first try read value
    as String, then when it is null try to get it as List. But this
    assumption will fail with jgit Config implementation which is used in
    DefaultSecureStore. JGit Config during getString() method call will call
    getRawStringList() and return its first element. Therefore migration
    will be incorrect for all stored List values.
    
    The solution here is to always use getList() and setList() in
    switch-secure-store and repeat jgit behavior in SecureStore. Making the
    get() method to return getList()[0] and set() to call setList() with
    single element.
    
    To provide default implementations of get() and set() method in
    SecureStore we need to make it an abstract class. Also those methods are
    marked as final to prevent implementors from overriding them.
    
    Also changes JarScanner.findImplementationsOf to findChildOf(). This
    method was introduced for SecureStore (and it was only used by it), so
    now when SecureStore becomes abstract class its implementation (and
    name) also should be updated.
    
    Change-Id: Id13bc6ec170c31b43b5a1cd696ba746006e1f0b2
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-pgm/src/test/java/com/google/gerrit/pgm/init/UpgradeFrom2_0_xTest.java
+++ gerrit-pgm/src/test/java/com/google/gerrit/pgm/init/UpgradeFrom2_0_xTest.java
@@ -116,22 +116,12 @@ public class UpgradeFrom2_0_xTest extends InitTestCase {
     u.run();
   }
 
-  private static class InMemorySecureStore implements SecureStore {
+  private static class InMemorySecureStore extends SecureStore {
     private final Config cfg = new Config();
-
-    @Override
-    public String get(String section, String subsection, String name) {
-      return cfg.getString(section, subsection, name);
-    }
 
     @Override
     public String[] getList(String section, String subsection, String name) {
       return cfg.getStringList(section, subsection, name);
-    }
-
-    @Override
-    public void set(String section, String subsection, String name, String value) {
-      cfg.setString(section, subsection, name, value);
     }
 
     @Override

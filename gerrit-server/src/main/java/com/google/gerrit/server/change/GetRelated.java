366a92d952ba89bddd591e8abfa04c1c6bf51367
Dave Borowitz <dborowitz@google.com>
2014-09-11 15:52:16 +0200

    Handle edits in GetRelated
    GetRelated collects all patch sets of all open changes, but was not
    including the edit itself.
    
    Change-Id: Iff6389a11ea1befd998d6b21616661f14e2665e2

--- gerrit-server/src/main/java/com/google/gerrit/server/change/GetRelated.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/GetRelated.java
@@ -91,7 +91,7 @@ public class GetRelated implements RestReadView<RevisionResource> {
   private List<ChangeAndCommit> walk(RevisionResource rsrc, RevWalk rw, Ref ref)
       throws OrmException, IOException {
     Map<Change.Id, Change> changes = allOpenChanges(rsrc);
-    Map<PatchSet.Id, PatchSet> patchSets = allPatchSets(changes.keySet());
+    Map<PatchSet.Id, PatchSet> patchSets = allPatchSets(rsrc, changes.keySet());
 
     Map<String, PatchSet> commits = Maps.newHashMap();
     for (PatchSet p : patchSets.values()) {
@@ -114,6 +114,7 @@ public class GetRelated implements RestReadView<RevisionResource> {
     Set<Change.Id> added = Sets.newHashSet();
     List<ChangeAndCommit> parents = Lists.newArrayList();
     for (RevCommit c; (c = rw.next()) != null;) {
+      // TODO(dborowitz): Remove this check when edit parent is changed to base.
       PatchSet p = commits.get(c.name());
       Change g = null;
       if (p != null) {
@@ -143,8 +144,8 @@ public class GetRelated implements RestReadView<RevisionResource> {
         db.changes().byBranchOpenAll(rsrc.getChange().getDest()));
   }
 
-  private Map<PatchSet.Id, PatchSet> allPatchSets(Collection<Change.Id> ids)
-      throws OrmException {
+  private Map<PatchSet.Id, PatchSet> allPatchSets(RevisionResource rsrc,
+      Collection<Change.Id> ids) throws OrmException {
     int n = ids.size();
     ReviewDb db = dbProvider.get();
     List<ResultSet<PatchSet>> t = Lists.newArrayListWithCapacity(n);
@@ -157,6 +158,10 @@ public class GetRelated implements RestReadView<RevisionResource> {
       for (PatchSet p : rs) {
         r.put(p.getId(), p);
       }
+    }
+
+    if (rsrc.getEdit().isPresent()) {
+      r.put(rsrc.getPatchSet().getId(), rsrc.getPatchSet());
     }
     return r;
   }

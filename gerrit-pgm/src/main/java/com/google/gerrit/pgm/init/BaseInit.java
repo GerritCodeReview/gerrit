c70306d91b0843b5db83eff8512751ed00c9fbdc
Dariusz Luksza <dariusz@luksza.org>
2014-09-11 12:25:15 +0200

    Fail init when SecureStore was already setup
    When SecureStore was already set up (gerrit.config already has
    gerrit.secureStoreClass) and --secure-store-lib flag to init should
    result in failure.
    
    To switch used SecureStore SwitchSecureStore program must be used.
    
    Change-Id: I887ace2f632232d52a2262792ef8d6ec6b10f9e9
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/BaseInit.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/BaseInit.java
@@ -15,6 +15,7 @@
 package com.google.gerrit.pgm.init;
 
 import static com.google.gerrit.server.schema.DataSourceProvider.Context.SINGLE_USER;
+import static com.google.gerrit.server.securestore.SecureStoreProvider.getSecureStoreClassFromGerritConfig;
 import static com.google.inject.Stage.PRODUCTION;
 
 import com.google.common.base.MoreObjects;
@@ -209,7 +210,8 @@ public class BaseInit extends SiteProgram {
     final File sitePath = getSitePath();
     final List<Module> m = new ArrayList<>();
 
-    final SecureStoreInitData secureStoreInitData = discoverSecureStoreClass();
+    final SecureStoreInitData secureStoreInitData =
+        discoverSecureStoreClass(sitePath);
     m.add(new InitModule(standalone, initDb));
     m.add(new AbstractModule() {
       @Override
@@ -259,14 +261,26 @@ public class BaseInit extends SiteProgram {
     return ConsoleUI.getInstance(false);
   }
 
-  private SecureStoreInitData discoverSecureStoreClass() {
+  private SecureStoreInitData discoverSecureStoreClass(File sitePath) {
     String secureStore = getSecureStoreLib();
     if (Strings.isNullOrEmpty(secureStore)) {
       return null;
     }
 
     try {
-      File secureStoreLib = new File(secureStore);
+      String currentSecureStore =
+          getSecureStoreClassFromGerritConfig(new SitePaths(sitePath));
+      if (!Strings.isNullOrEmpty(currentSecureStore)) {
+        throw new RuntimeException(
+            String.format(
+                "SecureStore is already set to %s. To change it use SwitchSecureStore program.",
+                currentSecureStore));
+      }
+    } catch (FileNotFoundException e) {
+      // ignore, site is initialized from scratch
+    }
+    try {
+      File secureStoreLib = new File(getSecureStoreLib());
       if (!secureStoreLib.exists()) {
         throw new InvalidSecureStoreException(String.format(
             "File %s doesn't exist", secureStore));

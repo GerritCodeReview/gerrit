deafb3e7101d64593301ca950702d41ce6b8930b
Gustaf Lundh <gustaf.lundh@sonymobile.com>
2014-09-11 17:56:37 +0200

    Make hashtags clickable
    Change-Id: I802c8a0e0bcdb0b3dce69b6ea4e68b9a36660e8a

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Hashtags.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Hashtags.java
@@ -158,7 +158,8 @@ public class Hashtags extends Composite {
     Iterator<String> itr = Natives.asList(hashtags).iterator();
     while (itr.hasNext()) {
       String hashtagName = itr.next();
-      html.openSpan()
+      html.openAnchor()
+          .setAttribute("href", "#" + PageLinks.toChangeQuery("hashtag:" + hashtagName))
           .setAttribute("role", "listitem")
           .setAttribute(DATA_ID, hashtagName)
           .setStyleName(style.hashtagName())

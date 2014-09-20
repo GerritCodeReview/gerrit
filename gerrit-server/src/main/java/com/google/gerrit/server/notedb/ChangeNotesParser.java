6432c9d0f2e95d6452bb5090ac5438289987f6d7
Bruce Zu <bruce.zu@sonymobile.com>
2014-09-20 19:07:38 +0800

    Create hashtags based on 'Hashtags' commit message footer of new patch
    Hashtags are detected based on the "Hashtags" commit message
    footer of new patch-set.
    
    The new patch-set can be of new change created by
     - revert;
     - cherry-pick a commit to be a new change;
     - create a new change by git-push;
    In this case hashtags are added automatically
    
    or be created to upate an existing change by update a commit locally,
    e.g. git-ammend, then git-push, or be created in the UI:
     - edit the commit-message;
     - cherry-pick a commit to another existing change;
     - rebase a commit;
     - publish change edit;
    In this case if hashtags exist already, they will be updated automatically.
    
    The "Hashtags" footer can be one or more lines.
    
    Change-Id: Ic683cf95e88e08c9e1d2a02e78fdcb8caf9794d4

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotesParser.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotesParser.java
@@ -27,7 +27,6 @@ import com.google.common.base.Splitter;
 import com.google.common.base.Supplier;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -171,16 +170,16 @@ class ChangeNotesParser implements AutoCloseable {
 
   private void parseHashtags(RevCommit commit) throws ConfigInvalidException {
     // Commits are parsed in reverse order and only the last set of hashtags should be used.
-    if (hashtags != null) {
+    if (hashtags != null && !hashtags.isEmpty()) {
       return;
     }
     List<String> hashtagsLines = commit.getFooterLines(FOOTER_HASHTAGS);
     if (hashtagsLines.isEmpty()) {
-      return;
+      hashtags = Collections.emptySet();
     } else if (hashtagsLines.size() > 1) {
       throw expectedOneFooter(FOOTER_HASHTAGS, hashtagsLines);
     } else if (hashtagsLines.get(0).isEmpty()) {
-      hashtags = ImmutableSet.of();
+      hashtags = Collections.emptySet();
     } else {
       hashtags = Sets.newHashSet(Splitter.on(',').split(hashtagsLines.get(0)));
     }

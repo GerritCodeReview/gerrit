6ec6ad08f0f15c31a9daa859ff986c21ffcb5383
David Ostrovsky <david@ostrovsky.org>
2014-09-21 12:46:01 +0200

    Change screen: Make change owner votes removable
    When change owner votes on change and disappears (yeah, that happens
    very often: people suddenly disappear) it isn't possible any more to
    remove her vote.
    
    Old change screen had this feature as well.
    
    Bug: issue 2909
    Change-Id: I7cdbbc5faa2fa6eb68fd42c91ad2181e98bdc2fd

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Reviewers.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Reviewers.java
@@ -229,7 +229,6 @@ public class Reviewers extends Composite {
     for (Integer i : r.keySet()) {
       cc.remove(i);
     }
-    r.remove(info.owner()._account_id());
     cc.remove(info.owner()._account_id());
 
     Set<Integer> removable = new HashSet<>();

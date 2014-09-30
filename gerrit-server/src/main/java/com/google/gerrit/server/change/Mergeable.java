787ea77e1b4d0788d569c8bcb1852952f6609eb7
Dave Borowitz <dborowitz@google.com>
2014-09-30 16:56:45 -0700

    Remove mergeable bit from Change
    Upgrade the schema to remove the field, but do not actually migrate
    data. This will be done later during a special case in the online
    reindexing step.
    
    The reason for this suboptimal upgrade process is that it would be
    nontrivial to copy the data in a schema upgrade, since the key for the
    cache requires running the entire submit rule evaluator to get the
    submit type. The dbInjector used to create the chain of SchemaVersions
    has far too few bindings for this. Getting these at schema upgrade
    time would require a completely different approach to injecting schema
    upgrade steps.
    
    Change-Id: I6fd6de3d06ed92ee4d2dbea43f3e9974e9491542

--- gerrit-server/src/main/java/com/google/gerrit/server/change/Mergeable.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Mergeable.java
@@ -22,7 +22,6 @@ import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSet;
-import com.google.gerrit.reviewdb.client.RevId;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.git.BranchOrderSection;
 import com.google.gerrit.server.git.GitRepositoryManager;
@@ -30,7 +29,6 @@ import com.google.gerrit.server.index.ChangeIndexer;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.server.project.SubmitRuleEvaluator;
 import com.google.gerrit.server.query.change.ChangeData;
-import com.google.gwtorm.server.AtomicUpdate;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -119,7 +117,6 @@ public class Mergeable implements RestReadView<RevisionResource> {
       throw new OrmException("Submit type rule failed: " + rec);
     }
     result.submitType = rec.type;
-    result.mergeable = change.isMergeable();
 
     Repository git = gitManager.openRepository(change.getProject());
     try {
@@ -181,22 +178,6 @@ public class Mergeable implements RestReadView<RevisionResource> {
       Repository git, Boolean old) throws OrmException, IOException {
     final boolean mergeable =
         cache.load(commit, into, type, change.getDest(), git);
-    db.get().changes().atomicUpdate(
-        change.getId(),
-        new AtomicUpdate<Change>() {
-          @Override
-          public Change update(Change c) {
-            if (c.getStatus().isOpen()
-                && ps.getId().equals(c.currentPatchSetId())) {
-              c.setMergeable(mergeable);
-              c.setLastSha1MergeTested(new RevId(
-                  !into.equals(ObjectId.zeroId()) ? into.name() : ""));
-              return c;
-            } else {
-              return null;
-            }
-          }
-        });
     if (reindex && !Objects.equals(mergeable, old)) {
       indexer.index(db.get(), change);
     }

c0bafd2f223211f60365b3afc8f1f1303c47be5a
Dave Borowitz <dborowitz@google.com>
2014-09-30 13:55:25 -0700

    Write change mergeability info to a persistent cache
    The mergeability bit and the last SHA-1 it was checked against are not
    really properties of the change, but rather properties of the state of
    Gerrit's mergeability checker. Moreover, mergeability checking happens
    sufficiently often (at least on every branch update) that it would
    clutter information in the notedb.
    
    As a first step towards eliminating these fields from Change, start
    writing them to a persistent cache when the value is recomputed. Cache
    only the most recent value for each change.
    
    Unlike the field in Change, don't bother overwriting the field when
    the change is merged. Determining what is the actual branch tip at
    which the merge was "tested" is nontrivial, and anyway the mergeable
    field is left unset by ChangeJson for merged changes.
    
    Change-Id: I0c1c4b73725a6de3fc90b21f3e65714ad2208a6e

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/MergeabilityCache.java
@@ -0,0 +1,158 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import static com.google.gerrit.server.ioutil.BasicSerialization.readVarInt32;
+import static com.google.gerrit.server.ioutil.BasicSerialization.writeVarInt32;
+import static org.eclipse.jgit.lib.ObjectIdSerialization.readNotNull;
+import static org.eclipse.jgit.lib.ObjectIdSerialization.writeNotNull;
+
+import com.google.common.base.Optional;
+import com.google.common.cache.Cache;
+import com.google.common.cache.Weigher;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.cache.CacheModule;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Singleton
+public class MergeabilityCache {
+  private static final String CACHE_NAME = "mergeability";
+
+  public static Module module() {
+    return new CacheModule() {
+      @Override
+      protected void configure() {
+        persist(CACHE_NAME, EntryKey.class, EntryVal.class)
+            .maximumWeight(1 << 20)
+            .weigher(MergeabilityWeigher.class);
+        bind(MergeabilityCache.class);
+      }
+    };
+  }
+
+  private final Cache<EntryKey, EntryVal> cache;
+
+  @Inject
+  MergeabilityCache(@Named(CACHE_NAME) Cache<EntryKey, EntryVal> cache) {
+    this.cache = cache;
+  }
+
+  public Optional<Boolean> get(Change c, @Nullable Ref ref) {
+    return getImpl(c, ref != null ? ref.getObjectId() : ObjectId.zeroId());
+  }
+
+  public Optional<Boolean> get(Change c) {
+    return getImpl(c, null);
+  }
+
+  private Optional<Boolean> getImpl(Change c, ObjectId tip) {
+    if (c.getStatus() == Change.Status.MERGED) {
+      return Optional.of(true);
+    }
+    EntryVal v = cache.getIfPresent(new EntryKey(c));
+    // For null tip, return the mergeable bit without checking. Check against
+    // a missing ref by passing zeroId.
+    if (v != null && (tip == null || v.tip.equals(tip))) {
+      return Optional.of(v.mergeable);
+    } else {
+      return Optional.absent();
+    }
+  }
+
+  public void save(Change change, Ref branchTip, boolean mergeable) {
+    cache.put(new EntryKey(change), new EntryVal(branchTip, mergeable));
+  }
+
+  private static class MergeabilityWeigher
+      implements Weigher<EntryKey, EntryVal> {
+    @Override
+    public int weigh(EntryKey k, EntryVal v) {
+      return 16 + 8 // Size of EntryKey, 64-bit JVM.
+          + 16 + (16 + 20) + 4 + 1; // Size of EntryVal, 64-bit JVM.
+    }
+  }
+
+  private static class EntryKey implements Serializable {
+    private static final long serialVersionUID = 17L;
+
+    private transient int changeId;
+    private transient int psId;
+
+    private EntryKey(Change c) {
+      this.changeId = c.getId().get();
+      this.psId = c.currentPatchSetId().get();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (o instanceof EntryKey) {
+        EntryKey k = (EntryKey) o;
+        return changeId == k.changeId
+            && psId == k.psId;
+      }
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(changeId, psId);
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+      writeVarInt32(out, changeId);
+      writeVarInt32(out, psId);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException {
+      changeId = readVarInt32(in);
+      psId = readVarInt32(in);
+    }
+  }
+
+  private static class EntryVal implements Serializable {
+    private static final long serialVersionUID = 17L;
+
+    private transient ObjectId tip;
+    private transient boolean mergeable;
+
+    private EntryVal(Ref ref, boolean mergeable) {
+      this.tip = ref != null ? ref.getObjectId() : ObjectId.zeroId();
+      this.mergeable = mergeable;
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+      writeNotNull(out, tip);
+      out.writeBoolean(mergeable);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException {
+      tip = readNotNull(in);
+      mergeable = in.readBoolean();
+    }
+  }
+}
--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -71,6 +71,7 @@ import com.google.gerrit.server.auth.UniversalAuthBackend;
 import com.google.gerrit.server.avatar.AvatarProvider;
 import com.google.gerrit.server.cache.CacheRemovalListener;
 import com.google.gerrit.server.change.ChangeKindCacheImpl;
+import com.google.gerrit.server.change.MergeabilityCache;
 import com.google.gerrit.server.change.MergeabilityChecker;
 import com.google.gerrit.server.events.EventFactory;
 import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
@@ -166,6 +167,7 @@ public class GerritGlobalModule extends FactoryModule {
     install(ConflictsCacheImpl.module());
     install(GroupCacheImpl.module());
     install(GroupIncludeCacheImpl.module());
+    install(MergeabilityCache.module());
     install(PatchListCacheImpl.module());
     install(ProjectCacheImpl.module());
     install(SectionSortCache.module());

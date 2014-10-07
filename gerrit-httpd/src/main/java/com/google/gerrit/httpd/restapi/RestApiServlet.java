GerritCodeReview/gerrit

commit 3bde74c27978e2b82063d41cef2613258929f7f1

Support AcceptsPost on non top level REST collections
Change-Id: I67a7c62beb3125cda968cc84c92cdd6f8a46a6f4
Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

gerrit-httpd/src/main/java/com/google/gerrit/httpd/restapi/RestApiServlet.java
@@ -820,6 +820,14 @@ public class RestApiServlet extends HttpServlet {
     RestView<RestResource> core = views.get("gerrit", name);
     if (core != null) {
       return new ViewData(null, core);
+    } else {
+      name = "GET." + p.get(0);
+      core = views.get("gerrit", name);
+      if (core instanceof AcceptsPost && "POST".equals(method)) {
+        @SuppressWarnings("unchecked")
+        AcceptsPost<RestResource> ap = (AcceptsPost<RestResource>) core;
+        return new ViewData(null, ap.post(rsrc));
+      }
     }
 
     Map<String, RestView<RestResource>> r = Maps.newTreeMap();

commit c0a1745291ac5878b5a465974aa087cd3c4c392b

Fix using HTTP methods other than GET from plugins
Commit 3bde74c2797 introduced a bug in the REST API. Methods other
than GET could not be used anymore  from plugins. For example it was not
possible anymore to delete projects with the delete-plugin using DELETE.

Bug: issue 2949
Change-Id: If90247ec4e0dad03b02d0a9e21202455fece90a7
Signed-off-by: Stefan Lay <stefan.lay@sap.com>

gerrit-httpd/src/main/java/com/google/gerrit/httpd/restapi/RestApiServlet.java
@@ -834,8 +834,7 @@ public class RestApiServlet extends HttpServlet {
     if (core != null) {
       return new ViewData(null, core);
     } else {
-      name = "GET." + p.get(0);
-      core = views.get("gerrit", name);
+      core = views.get("gerrit", "GET." + p.get(0));
       if (core instanceof AcceptsPost && "POST".equals(method)) {
         @SuppressWarnings("unchecked")
         AcceptsPost<RestResource> ap = (AcceptsPost<RestResource>) core;
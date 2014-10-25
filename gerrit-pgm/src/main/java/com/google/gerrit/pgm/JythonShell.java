d045cfc0babef892f829465e49e6ac2373016922
David Ostrovsky <david@ostrovsky.org>
2014-10-25 15:22:33 +0200

    gerrit-pgm: Align Buck rules with package boundaries
    Extract http package in its own rule and include it it gerrit-war. That
    means that the new artifact is going to be packaged in WEB-INF/lib
    directory leaving only root pgm package artifact in pgm-lib.
    
    Remove shell package and move one class in root pgm package. A resource
    file that belongs to this functionality as well, was already in root
    resource package, so it wasn't consistent anyway.
    
    This change also extracts DEPS dependency definition with artifacts that
    are shared in different rules to reuse them and not to repeat the code.
    
    Change-Id: Ie8cbab9c1333ef34dd240619e214674f8b583d2c

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/JythonShell.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/JythonShell.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.pgm.shell;
+package com.google.gerrit.pgm;
 
 import com.google.gerrit.launcher.GerritLauncher;
 

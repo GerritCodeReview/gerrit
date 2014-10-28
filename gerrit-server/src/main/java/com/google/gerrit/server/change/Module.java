0773ea387b1342f80d8c511c82a37cd1a127dfd1
David Ostrovsky <david@ostrovsky.org>
2014-10-28 23:44:27 +0100

    InlineEdit: Add REST endpoint to retrieve commit message
    Change-Id: I9017df268614e8035513af173a7067a8aa10fcec

--- gerrit-server/src/main/java/com/google/gerrit/server/change/Module.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Module.java
@@ -108,6 +108,7 @@ public class Module extends RestApiModule {
     child(CHANGE_KIND, "edit:publish").to(PublishChangeEdit.class);
     child(CHANGE_KIND, "edit:rebase").to(RebaseChangeEdit.class);
     put(CHANGE_KIND, "edit:message").to(ChangeEdits.EditMessage.class);
+    get(CHANGE_KIND, "edit:message").to(ChangeEdits.GetMessage.class);
     put(CHANGE_EDIT_KIND, "/").to(ChangeEdits.Put.class);
     delete(CHANGE_EDIT_KIND).to(ChangeEdits.DeleteContent.class);
     get(CHANGE_EDIT_KIND, "/").to(ChangeEdits.Get.class);

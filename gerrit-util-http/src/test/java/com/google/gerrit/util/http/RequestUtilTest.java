bba1998bbbef1966869a844cabf22ad3edd637ef
David Pletcher <dpletcher@google.com>
2014-10-28 21:57:32 -0700

    Refactor RequestUtil into a separate package
    Move RequestUtil into its own package. This facilitates
    reuse of request attribute accessor methods by external
    libraries.
    
    Change-Id: Id002eb4f72827d65087a999adbd2ba9a58795fe2

--- gerrit-util-http/src/test/java/com/google/gerrit/util/http/RequestUtilTest.java
+++ gerrit-util-http/src/test/java/com/google/gerrit/util/http/RequestUtilTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.httpd;
+package com.google.gerrit.util.http;
 
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;

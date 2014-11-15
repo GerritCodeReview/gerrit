d02cbfeefc50f83bbb0ad4b70dddd492efd8481b
David Ostrovsky <david@ostrovsky.org>
2014-11-15 23:18:48 +0100

    InlineEdit: Allow to save/load user preferences in Codemirror
    Change-Id: Ice334364cdb952eaa93118362c438dcc291db2af

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/Gerrit.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/Gerrit.java
@@ -46,6 +46,7 @@ import com.google.gerrit.common.data.GitwebConfig;
 import com.google.gerrit.common.data.HostPageData;
 import com.google.gerrit.common.data.SystemInfoService;
 import com.google.gerrit.extensions.client.GerritTopMenu;
+import com.google.gerrit.extensions.client.EditPreferencesInfo;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.AccountDiffPreference;
 import com.google.gerrit.reviewdb.client.AccountGeneralPreferences;
@@ -110,6 +111,7 @@ public class Gerrit implements EntryPoint {
   private static Account myAccount;
   private static String defaultScreenToken;
   private static AccountDiffPreference myAccountDiffPref;
+  private static EditPreferencesInfo editPrefs;
   private static String xGerritAuth;
   private static boolean isNoteDbEnabled;
 
@@ -320,6 +322,14 @@ public class Gerrit implements EntryPoint {
 
   public static void setAccountDiffPreference(AccountDiffPreference accountDiffPref) {
     myAccountDiffPref = accountDiffPref;
+  }
+
+  public static EditPreferencesInfo getEditPreferences() {
+    return editPrefs;
+  }
+
+  public static void setEditPreferences(EditPreferencesInfo p) {
+    editPrefs = p;
   }
 
   /** @return true if the user is currently authenticated */

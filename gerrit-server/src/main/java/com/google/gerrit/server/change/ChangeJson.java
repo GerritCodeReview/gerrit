f5d1c987fe22fbfb434ee04b54439d8984b2c153
Sven Selberg <sven.selberg@sonymobile.com>
2014-11-21 16:28:10 +0100

    Add webLink support for commit parents in changescreen
    This change moves webLinks from RevisionInfo to CommitInfo
    
    Change-Id: I2f79b451f88070ae674aefe2dfe4a9df3f5a8ef1

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
@@ -807,7 +807,7 @@ public class ChangeJson {
 
     if (has(ALL_COMMITS) || (out.isCurrent && has(CURRENT_COMMIT))) {
       try {
-        out.commit = toCommit(in);
+        out.commit = toCommit(in, cd.change().getProject().get());
       } catch (PatchSetInfoNotAvailableException e) {
         log.warn("Cannot load PatchSetInfo " + in.getId(), e);
       }
@@ -845,14 +845,12 @@ public class ChangeJson {
     }
 
     if (has(WEB_LINKS)) {
-      FluentIterable<WebLinkInfo> links =
-          webLinks.getPatchSetLinks(project, in.getRevision().get());
-      out.webLinks = links.isEmpty() ? null : links.toList();
+
     }
     return out;
   }
 
-  CommitInfo toCommit(PatchSet in)
+  CommitInfo toCommit(PatchSet in, String project)
       throws PatchSetInfoNotAvailableException {
     PatchSetInfo info = patchSetInfoFactory.get(db.get(), in.getId());
     CommitInfo commit = new CommitInfo();
@@ -861,10 +859,16 @@ public class ChangeJson {
     commit.committer = toGitPerson(info.getCommitter());
     commit.subject = info.getSubject();
     commit.message = info.getMessage();
+    FluentIterable<WebLinkInfo> links =
+        webLinks.getPatchSetLinks(project, in.getRevision().get());
+    commit.webLinks = links.isEmpty() ? null : links.toList();
     for (ParentInfo parent : info.getParents()) {
       CommitInfo i = new CommitInfo();
       i.commit = parent.id.get();
       i.subject = parent.shortMessage;
+      FluentIterable<WebLinkInfo> parentLinks =
+          webLinks.getPatchSetLinks(project, parent.id.get());
+      i.webLinks = parentLinks.isEmpty() ? null : parentLinks.toList();
       commit.parents.add(i);
     }
     return commit;

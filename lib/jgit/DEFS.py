021cdf01c24791d3c2f0a35e000ef7a879c485c1
Shawn Pearce <sop@google.com>
2014-11-26 16:01:37 -0800

    Link to JGit source as git submodule
    Using a submodule to bring in the source files vastly simplifies
    updating JGit. Gerrit developers can bump the SHA-1 of the submodule
    and not deal with the Maven rebuild process for JGit.
    
    Building JGit from source for Gerrit Code Review's needs is not any
    more complex than importing the Maven style JARs. Dependency data
    is identical and pointing at source files by globs is trivial.
    
    The increased build time for Gerrit is trivial. Buck is more than
    fast enough to handle the additional source files:
    
                    no-op rebuild
                   --------------
      maven jars :  2.782s
      from source:  2.961s
    
    Change-Id: I5ea9049164062e694c46dbd10ee4b47f8b9aef96

---
+++ lib/jgit/DEFS.py
@@ -0,0 +1,10 @@
+DIRS = {
+  'jgit': 'org.eclipse.jgit',
+  'jgit-servlet': 'org.eclipse.jgit.http.server',
+  'jgit-archive': 'org.eclipse.jgit.archive',
+  'junit': 'org.eclipse.jgit.junit',
+}
+
+EXCLUDE = {
+  'jgit-archive': ['org/eclipse/jgit/archive/FormatActivator.java'],
+}

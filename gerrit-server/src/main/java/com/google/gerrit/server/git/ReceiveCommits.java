bf65097271e5990350330c8d09596ad92d01c3cc
Stefan Beller <sbeller@google.com>
2014-12-04 14:04:39 -0800

    Add checking for Footer Depends-On:
    Change-Id: I5844a743bd48f4538a375efb3c16e6a1c4ae26b0

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -180,6 +180,7 @@ public class ReceiveCommits {
       "^" + REFS_CHANGES + "(?:[0-9][0-9]/)?([1-9][0-9]*)(?:/new)?$");
 
   private static final FooterKey CHANGE_ID = new FooterKey("Change-Id");
+  private static final FooterKey DEPENDS_ON = new FooterKey("Depends-On");
 
   private static final String COMMAND_REJECTION_MESSAGE_FOOTER =
       "Please read the documentation and contact an administrator\n"
@@ -1535,6 +1536,12 @@ public class ReceiveCommits {
           // Reject this invalid line from EGit.
           reject(magicBranch.cmd, "invalid Change-Id");
           return Collections.emptyList();
+        }
+
+        final List<String> dependencies = c.getFooterLines(DEPENDS_ON);
+        if (dependencies.size() > 1) {
+          // this makes life very easy for selecting all other dependencies
+          reject(magicBranch.cmd, "just one Depends-on for now. sorry.");
         }
 
         changeKey = new Change.Key(idStr);

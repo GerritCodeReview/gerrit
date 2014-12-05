1724e550dc56d047657f5c09e28686e4ce2368a8
Dave Borowitz <dborowitz@google.com>
2014-12-05 11:54:56 -0800

    Revert "Set Verified function in rules.pl"
    Somehow made everything submittable with no labels.
    
    This reverts commit d5a114b1b75a2551d395e5eb92ef3b5955105571.

--- rules.pl
+++ rules.pl
@@ -9,4 +9,4 @@ submit_rule(submit(CR, V)) :-
 
 base(CR, V) :-
   gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-  gerrit:max_no_block(-1, 1, 'Verified', V).
+  gerrit:max_with_block(-1, 1, 'Verified', V).

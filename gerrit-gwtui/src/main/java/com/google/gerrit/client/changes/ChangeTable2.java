eafc3347a9f44f5ee2ecdfd0ab9734c4daa80331
Shawn Pearce <sop@google.com>
2014-12-18 22:45:05 +0000

    Revert "Don't sort labels alphabetically in UI"
    This reverts commit d5bbe0841fb3b6ec2ca7dd7f28bb9bccb857c9c1.
    
    Labels are returned to the browser inside of a map/object
    whose ordering is undefined once parsed into a JavaScript
    object for use by the UI.
    
    By removing client side sorting we are at the mercy of
    the browser's iteration of map/object keys, which can be
    different across versions or implementations.
    
    A correct solution to rely on project defined ordering
    will require exporting the project defined ordering in
    the label data, or as part of the project config read
    for the change. But just dropping client side sorting is
    not a solution.
    
    Change-Id: Ib7c9ba241a8d1d97c5106f22059f71ca43f55a2d

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable2.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable2.java
@@ -183,6 +183,7 @@ public class ChangeTable2 extends NavigationTable<ChangeInfo> {
         }
       }
     }
+    Collections.sort(labelNames);
 
     int baseColumns = useNewFeatures ? BASE_COLUMNS : BASE_COLUMNS - 1;
     if (baseColumns + labelNames.size() < columns) {

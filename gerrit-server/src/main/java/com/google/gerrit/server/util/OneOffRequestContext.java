226cd2c33e2638e436a2db717669c147ded4ce0d
Dave Borowitz <dborowitz@google.com>
2014-12-23 12:25:37 -0800

    Replace most ChangeAccess queries with searches
    Use the secondary index rather than depending on SQL indexes, which
    will not be available in a notedb world. For the most part this change
    is pretty straightforward, as the interface for InternalChangeQuery is
    almost the same as for ChangeAccess. It also provides a bit more
    functionality, such as limiting the results.
    
    By default, all of these queries should bypass visibility checks,
    since things like the submit queue necessarily need to see everything.
    Even if it is properly running as InternalUser and that is hard-coded
    to have visibility, it is simpler to just bypass the checks entirely.
    
    The biggest complication comes from converting callers that previously
    opened a new ReviewDb in a try/finally block and called a ChangeAccess
    method directly from that. In the InternalChangeQuery model,
    Provider<ReviewDb> needs to be injected, so we need to set the
    ThreadLocalRequestContext with the manually-opened DB. To simplify
    this code, add an AutoCloseable RequestContext implementation.
    
    To save future schema churn, this change does not include a schema
    change to drop the indexes.
    
    Change-Id: I99de8a2cf2aba01971059b89df33b1676cd8546e

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/util/OneOffRequestContext.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.util;
+
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.InternalUser;
+import com.google.gwtorm.server.OrmException;
+import com.google.gwtorm.server.SchemaFactory;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * Helper to create one-off request contexts.
+ * <p>
+ * Each call to {@link #open()} opens a new {@link ReviewDb}, so this class
+ * should only be used in a bounded try/finally block.
+ * <p>
+ * The user in the request context is {@link InternalUser}.
+ */
+@Singleton
+public class OneOffRequestContext {
+  private final InternalUser.Factory userFactory;
+  private final SchemaFactory<ReviewDb> schemaFactory;
+  private final ThreadLocalRequestContext requestContext;
+
+  @Inject
+  OneOffRequestContext(InternalUser.Factory userFactory,
+      SchemaFactory<ReviewDb> schemaFactory,
+      ThreadLocalRequestContext requestContext) {
+    this.userFactory = userFactory;
+    this.schemaFactory = schemaFactory;
+    this.requestContext = requestContext;
+  }
+
+  public ManualRequestContext open() throws OrmException {
+    return new ManualRequestContext(userFactory.create(),
+        schemaFactory, requestContext);
+  }
+}

232b6b7f7c1c6a898caed89eeda84daeadb90924
Edwin Kempin <edwin.kempin@sap.com>
2015-01-07 14:25:16 +0100

    Move 'Publish' and 'Delete Change/Revision' buttons into header
    If a change/revision is a draft the natural next step is to publish
    (or delete) it, hence these buttons should be displayed in a more
    prominent place.
    
    Change-Id: I4aa756152548ef78cd9dc9a67ecc397a3af12ff7
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Actions.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/Actions.java
@@ -44,9 +44,6 @@ class Actions extends Composite {
   private static final Binder uiBinder = GWT.create(Binder.class);
 
   @UiField Button cherrypick;
-  @UiField Button deleteChange;
-  @UiField Button deleteRevision;
-  @UiField Button publish;
   @UiField Button rebase;
   @UiField Button revert;
   @UiField Button submit;
@@ -100,7 +97,6 @@ class Actions extends Composite {
     actions.copyKeysIntoChildren("id");
 
     if (hasUser) {
-      a2b(actions, "/", deleteChange);
       a2b(actions, "abandon", abandon);
       a2b(actions, "restore", restore);
       a2b(actions, "revert", revert);
@@ -129,9 +125,7 @@ class Actions extends Composite {
             .append(action.label())
             .closeDiv());
       }
-      a2b(actions, "/", deleteRevision);
       a2b(actions, "cherrypick", cherrypick);
-      a2b(actions, "publish", publish);
       a2b(actions, "rebase", rebase);
       for (String id : filterNonCore(actions)) {
         add(new ActionButton(info, revInfo, actions.get(id)));
@@ -174,21 +168,6 @@ class Actions extends Composite {
       abandonAction = new AbandonAction(abandon, changeId);
     }
     abandonAction.show();
-  }
-
-  @UiHandler("publish")
-  void onPublish(@SuppressWarnings("unused") ClickEvent e) {
-    DraftActions.publish(changeId, revision);
-  }
-
-  @UiHandler("deleteRevision")
-  void onDeleteRevision(@SuppressWarnings("unused") ClickEvent e) {
-    DraftActions.delete(changeId, revision);
-  }
-
-  @UiHandler("deleteChange")
-  void onDeleteChange(@SuppressWarnings("unused") ClickEvent e) {
-    DraftActions.delete(changeId);
   }
 
   @UiHandler("restore")

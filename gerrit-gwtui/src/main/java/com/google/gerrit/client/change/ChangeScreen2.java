c1f4370acc0fe509b571a016d4c7a88d94cc4da2
Edwin Kempin <edwin.kempin@sap.com>
2015-01-07 14:25:16 +0100

    Move 'Publish' and 'Delete Change/Revision' buttons into header
    If a change/revision is a draft the natural next step is to publish
    (or delete) it, hence these buttons should be displayed in a more
    prominent place.
    
    Change-Id: I4aa756152548ef78cd9dc9a67ecc397a3af12ff7
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ChangeScreen2.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ChangeScreen2.java
@@ -182,6 +182,9 @@ public class ChangeScreen2 extends Screen {
   @UiField Button publishEdit;
   @UiField Button rebaseEdit;
   @UiField Button deleteEdit;
+  @UiField Button publish;
+  @UiField Button deleteChange;
+  @UiField Button deleteRevision;
   @UiField Button openAll;
   @UiField Button editMode;
   @UiField Button reviewMode;
@@ -384,6 +387,19 @@ public class ChangeScreen2 extends Screen {
     }
   }
 
+  private void initChangeAction(ChangeInfo info) {
+    if (info.status() == Status.DRAFT) {
+      NativeMap<ActionInfo> actions = info.has_actions()
+          ? info.actions()
+          : NativeMap.<ActionInfo> create();
+      actions.copyKeysIntoChildren("id");
+      if (actions.containsKey("/")) {
+        deleteChange.setVisible(true);
+        deleteChange.setTitle(actions.get("/").title());
+      }
+    }
+  }
+
   private void initRevisionsAction(ChangeInfo info, String revision) {
     int currentPatchSet;
     if (info.current_revision() != null
@@ -409,6 +425,23 @@ public class ChangeScreen2 extends Screen {
     patchSetsAction = new PatchSetsAction(
         info.legacy_id(), revision,
         style, headerLine, patchSets);
+
+    RevisionInfo revInfo = info.revision(revision);
+    if (revInfo.draft()) {
+      NativeMap<ActionInfo> actions = revInfo.has_actions()
+          ? revInfo.actions()
+          : NativeMap.<ActionInfo> create();
+      actions.copyKeysIntoChildren("id");
+
+      if (actions.containsKey("publish")) {
+        publish.setVisible(true);
+        publish.setTitle(actions.get("publish").title());
+      }
+      if (actions.containsKey("/")) {
+        deleteRevision.setVisible(true);
+        deleteRevision.setTitle(actions.get("/").title());
+      }
+    }
   }
 
   private void initDownloadAction(ChangeInfo info, String revision) {
@@ -528,6 +561,21 @@ public class ChangeScreen2 extends Screen {
   @UiHandler("deleteEdit")
   void onDeleteEdit(@SuppressWarnings("unused") ClickEvent e) {
     EditActions.deleteEdit(changeId);
+  }
+
+  @UiHandler("publish")
+  void onPublish(@SuppressWarnings("unused") ClickEvent e) {
+    DraftActions.publish(changeId, revision);
+  }
+
+  @UiHandler("deleteRevision")
+  void onDeleteRevision(@SuppressWarnings("unused") ClickEvent e) {
+    DraftActions.delete(changeId, revision);
+  }
+
+  @UiHandler("deleteChange")
+  void onDeleteChange(@SuppressWarnings("unused") ClickEvent e) {
+    DraftActions.delete(changeId);
   }
 
   @Override
@@ -1020,6 +1068,7 @@ public class ChangeScreen2 extends Screen {
     renderDiffBaseListBox(info);
     initReplyButton(info, revision);
     initIncludedInAction(info);
+    initChangeAction(info);
     initRevisionsAction(info, revision);
     initDownloadAction(info, revision);
     initProjectLinks(info);

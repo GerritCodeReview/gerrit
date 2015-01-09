80ee2656843774f6773a4cacf6de5463a580d24d
David Ostrovsky <david@ostrovsky.org>
2015-01-09 08:43:55 +0100

    InlineEdit: Fix mangling non-ASCII characters
    Bug: issue 3093
    Change-Id: I549037bfb82aff15bbffc8b4a01e8955ae0ce318

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/rpc/RestApi.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/rpc/RestApi.java
@@ -423,8 +423,80 @@ public class RestApi {
   private static native String str(JavaScriptObject jso)
   /*-{ return JSON.stringify(jso) }-*/;
 
+  /*
+   * Shamelessly stolen from MDN:
+   * https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+   * Comment style was adjusted.
+   */
   private static native String decodeBase64(String a)
-  /*-{ return $wnd.atob(a) }-*/;
+  /*-{
+
+    function b64ToUint6 (nChr) {
+      return nChr > 64 && nChr < 91 ?
+          nChr - 65
+        : nChr > 96 && nChr < 123 ?
+          nChr - 71
+        : nChr > 47 && nChr < 58 ?
+          nChr + 4
+        : nChr === 43 ?
+          62
+        : nChr === 47 ?
+          63
+        :
+          0;
+    }
+
+    function base64DecToArr (sBase64, nBlocksSize) {
+
+      var
+        sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""), nInLen = sB64Enc.length,
+        nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2, taBytes = new Uint8Array(nOutLen);
+
+      for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+        nMod4 = nInIdx & 3;
+        nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+        if (nMod4 === 3 || nInLen - nInIdx === 1) {
+          for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+            taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+          }
+          nUint24 = 0;
+
+        }
+      }
+
+      return taBytes;
+    }
+
+    // UTF-8 array to DOMString and vice versa
+
+    function UTF8ArrToStr (aBytes) {
+
+      var sView = "";
+
+      for (var nPart, nLen = aBytes.length, nIdx = 0; nIdx < nLen; nIdx++) {
+        nPart = aBytes[nIdx];
+        sView += String.fromCharCode(
+          nPart > 251 && nPart < 254 && nIdx + 5 < nLen ? // six bytes
+            // (nPart - 252 << 30) may be not so safe in ECMAScript! So...:
+            (nPart - 252) * 1073741824 + (aBytes[++nIdx] - 128 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+          : nPart > 247 && nPart < 252 && nIdx + 4 < nLen ? // five bytes
+            (nPart - 248 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+          : nPart > 239 && nPart < 248 && nIdx + 3 < nLen ? // four bytes
+            (nPart - 240 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+          : nPart > 223 && nPart < 240 && nIdx + 2 < nLen ? // three bytes
+            (nPart - 224 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+          : nPart > 191 && nPart < 224 && nIdx + 1 < nLen ? // two bytes
+            (nPart - 192 << 6) + aBytes[++nIdx] - 128
+          : // nPart < 127 ? // one byte
+            nPart
+        );
+      }
+
+      return sView;
+    }
+
+    return UTF8ArrToStr(base64DecToArr(a));
+  }-*/;
 
   private <T extends JavaScriptObject> void sendRaw(Method method, String body,
       HttpCallback<T> cb) {

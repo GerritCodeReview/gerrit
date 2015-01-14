09110a5dffcef3548d4427824a0aac77670435b0
David Ostrovsky <david@ostrovsky.org>
2015-01-14 10:58:42 +0100

    Refactor LabelId out of PatchSetApprovals
    Change-Id: Ibb162a6a9249204c24c8751ea01fb816586449dd

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotesParser.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotesParser.java
@@ -40,10 +40,10 @@ import com.google.gerrit.common.data.SubmitRecord;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.ChangeMessage;
+import com.google.gerrit.reviewdb.client.LabelId;
 import com.google.gerrit.reviewdb.client.PatchLineComment;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.reviewdb.client.PatchSetApproval;
-import com.google.gerrit.reviewdb.client.PatchSetApproval.LabelId;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.util.LabelVote;
 

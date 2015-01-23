7b39b6f7ebfdb1266660959d5ba9a893acd61c7a
Olga Grinberg <olga.grinberg@ericsson.com>
2015-01-23 14:40:27 -0500

    Enable project list in gitweb
    A message "Forbidden" was displayed when clicking on any part of the
    breadcrumb trail displayed at the top of Gitweb pages. That happened for
    projects with parent folders because browsing parent folders is not
    supported by Gerrit.
    
    Now when the user clicks on the parent folder, redirect to Gerrit projects
    list screen with the parent folder path as the filter.
    
    Change-Id: I3eb94a195df06f2525b3abc3bd355ee1821821c7

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/gitweb/GitWebServlet.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/gitweb/GitWebServlet.java
@@ -29,6 +29,7 @@
 
 package com.google.gerrit.httpd.gitweb;
 
+import com.google.gerrit.common.PageLinks;
 import com.google.gerrit.common.data.GerritConfig;
 import com.google.gerrit.extensions.restapi.Url;
 import com.google.gerrit.httpd.GitWebConfig;
@@ -79,6 +80,8 @@ class GitWebServlet extends HttpServlet {
   private static final Logger log =
       LoggerFactory.getLogger(GitWebServlet.class);
 
+  private static final String PROJECT_LIST_ACTION = "project_list";
+
   private final Set<String> deniedActions;
   private final int bufferSize = 8192;
   private final File gitwebCgi;
@@ -119,7 +122,6 @@ class GitWebServlet extends HttpServlet {
 
     deniedActions.add("forks");
     deniedActions.add("opml");
-    deniedActions.add("project_list");
     deniedActions.add("project_index");
 
     _env = new EnvList();
@@ -360,6 +362,12 @@ class GitWebServlet extends HttpServlet {
     final Map<String, String> params = getParameters(req);
     if (deniedActions.contains(params.get("a"))) {
       rsp.sendError(HttpServletResponse.SC_FORBIDDEN);
+      return;
+    }
+
+    if (params.get("a").equals(PROJECT_LIST_ACTION)) {
+      rsp.sendRedirect(req.getContextPath() + "/#" + PageLinks.ADMIN_PROJECTS +
+          "?filter=" + params.get("pf"));
       return;
     }
 

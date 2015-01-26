5e9bd0717c6b35ad6de9af68d93f60c10b5d67d2
David Pursehouse <david.pursehouse@sonymobile.com>
2015-01-26 11:25:00 +0900

    WiP: change-merged event is not fired when noteDb is enabled
    Add a 'noteDb enabled' config in the submit acceptance tests, which
    exposes the fact that the change-merged event is not fired when the
    Note DB is enabled.
    
    Bug: Issue 3127
    Change-Id: I2527d3b5495a43aa0cbf121c3b6408a044603ea5

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/AbstractSubmit.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/AbstractSubmit.java
@@ -50,6 +50,7 @@ import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.events.ChangeMergedEvent;
 import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gerrit.server.notedb.NotesMigration;
 import com.google.gerrit.testutil.ConfigSuite;
 import com.google.gson.reflect.TypeToken;
 import com.google.gwtorm.server.OrmException;
@@ -78,6 +79,11 @@ public abstract class AbstractSubmit extends AbstractDaemonTest {
   @ConfigSuite.Config
   public static Config submitWholeTopicEnabled() {
     return submitWholeTopicEnabledConfig();
+  }
+
+  @ConfigSuite.Config
+  public static Config noteDbEnabled() {
+    return NotesMigration.allEnabledConfig();
   }
 
   private Map<String, String> mergeResults;
@@ -229,8 +235,10 @@ public abstract class AbstractSubmit extends AbstractDaemonTest {
       BranchInfo branch =
           newGson().fromJson(b.getReader(),
               new TypeToken<BranchInfo>() {}.getType());
-      assertThat(branch.revision).isEqualTo(
-          mergeResults.get(Integer.toString(change._number)));
+      assertThat(mergeResults).isNotEmpty();
+      String newRev = mergeResults.get(Integer.toString(change._number));
+      assertThat(newRev).isNotNull();
+      assertThat(branch.revision).isEqualTo(newRev);
     }
     b.consume();
   }

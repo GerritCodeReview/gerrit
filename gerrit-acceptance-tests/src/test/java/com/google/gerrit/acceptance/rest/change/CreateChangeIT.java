eb1be8f4f5b290357f01dee6cd19f90b918d4527
David Pursehouse <david.pursehouse@sonymobile.com>
2015-01-30 19:35:12 +0900

    Fix: REST API allows to create draft change when drafts are disabled
    It was possible to create a draft change via the REST API even when
    the `change.allowDrafts` setting was false in the configuration.
    
    Add a check to prevent this.
    
    Extend the acceptance test case to include a configuration with the
    `change.allowDrafts` set to false, and add a new test case to verify
    that draft changes cannot be created.
    
    Change-Id: I79cff3cd05361e2e81ba2ab51381c9a8c0f04530

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/CreateChangeIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/CreateChangeIT.java
@@ -15,17 +15,24 @@
 package com.google.gerrit.acceptance.rest.change;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.TruthJUnit.assume;
 
 import com.google.common.collect.Iterables;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.RestResponse;
 import com.google.gerrit.extensions.client.ChangeStatus;
 import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.testutil.ConfigSuite;
 
 import org.apache.http.HttpStatus;
+import org.eclipse.jgit.lib.Config;
 import org.junit.Test;
 
 public class CreateChangeIT extends AbstractDaemonTest {
+  @ConfigSuite.Config
+  public static Config allowDraftsDisabled() {
+    return allowDraftsDisabledConfig();
+  }
 
   @Test
   public void createEmptyChange_MissingBranch() throws Exception {
@@ -61,7 +68,17 @@ public class CreateChangeIT extends AbstractDaemonTest {
 
   @Test
   public void createDraftChange() throws Exception {
+    assume().that(isAllowDrafts()).isTrue();
     assertChange(newChangeInfo(ChangeStatus.DRAFT));
+  }
+
+  @Test
+  public void createDraftChangeNotAllowed() throws Exception {
+    assume().that(isAllowDrafts()).isFalse();
+    ChangeInfo ci = newChangeInfo(ChangeStatus.DRAFT);
+    RestResponse r = adminSession.post("/changes/", ci);
+    assertThat(r.getStatusCode()).isEqualTo(HttpStatus.SC_METHOD_NOT_ALLOWED);
+    assertThat(r.getEntityContent()).contains("drafts are disabled");
   }
 
   private ChangeInfo newChangeInfo(ChangeStatus status) {

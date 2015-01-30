1c7f86b2a88cacc7bc519dd99740ee6735b4d9c0
Stefan Beller <sbeller@google.com>
2015-01-30 10:12:52 -0800

    WIP/Add changes/<id>/actions rest api call
    If you have enabled the new option `submitWholeTopic`, we eventually want
    to query all changes in the topic if they can be submitted (CRV +2,
    verified and the user has permission to submit) to determine if the
    submit button is enabled in a ChangeScreen. This query may take some
    time, so we don't want to have it in the same query for the whole
    change screen, which should render fast.
    
    This commit introduces two new rest api calls which allows to query only
    for the available actions for a given change id or revision id:
    
      changes/<id>/actions
      changes/<id>/revisions/<id>/actions
    
    By introducing a new ActionJson class we can eventually move all
    actions related json processing out of ChangeJson which currently
    covers also actions such as in the change/<id>/details rest api call.
    
    Change-Id: I58c67944e756f0b130a9c1d89d83f7635bf474a7

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ActionJson.java
@@ -0,0 +1,97 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Maps;
+import com.google.gerrit.extensions.common.ActionInfo;
+import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.extensions.webui.UiAction;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.extensions.webui.UiActions;
+import com.google.gerrit.server.project.ChangeControl;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import java.util.Map;
+
+public class ActionJson {
+  private final Provider<CurrentUser> userProvider;
+  private final Provider<ReviewDb> db;
+  private final Revisions revisions;
+  private final DynamicMap<RestView<ChangeResource>> changeViews;
+  private final ChangeData.Factory changeDataFactory;
+
+  @Inject
+  ActionJson(
+      Provider<CurrentUser> user,
+      Provider<ReviewDb> db,
+      Revisions revisions,
+      DynamicMap<RestView<ChangeResource>> changeViews,
+      ChangeData.Factory cdf) {
+    this.userProvider = user;
+    this.db = db;
+    this.revisions = revisions;
+    this.changeViews = changeViews;
+    this.changeDataFactory = cdf;
+  }
+
+  public ChangeInfo format(ChangeResource rsrc) throws OrmException {
+    ChangeData cd = changeDataFactory.create(db.get(), rsrc.getControl());
+    Optional<PatchSet.Id> ps = Optional.<PatchSet.Id> absent();
+    return format(cd, ps);
+  }
+
+  public ChangeInfo format(RevisionResource rsrc) throws OrmException {
+    ChangeData cd = changeDataFactory.create(db.get(), rsrc.getControl());
+    Optional<PatchSet.Id> ps = Optional.of(rsrc.getPatchSet().getId());
+    return format(cd, ps);
+  }
+
+  public ChangeInfo format(ChangeData cd, Optional<PatchSet.Id> ps) throws OrmException {
+    ChangeInfo out = new ChangeInfo();
+    out.actions = toActionInfoMap(cd, ps);
+    return out;
+  }
+
+  Map<String, ActionInfo> toActionInfoMap(ChangeData cd, Optional<PatchSet.Id> in)
+      throws OrmException {
+    Map<String, ActionInfo> out = Maps.newLinkedHashMap();
+    if (userProvider.get().isIdentifiedUser()) {
+      ChangeControl ctl = cd.changeControl().forUser(userProvider.get());
+      if (in.isPresent() && in.get().equals(cd.currentPatchSet().getId())) {
+        for (UiAction.Description d : UiActions.from(
+            revisions,
+            new RevisionResource(new ChangeResource(ctl), cd.currentPatchSet()),
+            userProvider)) {
+          out.put(d.getId(), new ActionInfo(d));
+        }
+      }
+      for (UiAction.Description d : UiActions.from(
+          changeViews,
+          new ChangeResource(ctl),
+          userProvider)) {
+        out.put(d.getId(), new ActionInfo(d));
+      }
+    }
+    return out;
+  }
+}
---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/GetActions.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+
+public class GetActions implements RestReadView<ChangeResource> {
+  private final ActionJson delegate;
+
+  @Inject
+  GetActions(ActionJson delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public Response<ChangeInfo> apply(ChangeResource rsrc) throws OrmException {
+    return Response.withMustRevalidate(delegate.format(rsrc));
+  }
+}

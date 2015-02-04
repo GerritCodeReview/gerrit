464cbb5efe199d2a15104c338713dc2b99804214
David Ostrovsky <david@ostrovsky.org>
2015-02-04 23:49:46 +0100

    InlineEdit: Fix diff against revision feature for change edits
    Bug: Issue 3142
    Change-Id: I567a51ed5dca7774ef886b4c9953b14cafa84c46

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ChangeScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ChangeScreen.java
@@ -724,7 +724,7 @@ public class ChangeScreen extends Screen {
   @UiHandler("diffBase")
   void onChangeRevision(@SuppressWarnings("unused") ChangeEvent e) {
     int idx = diffBase.getSelectedIndex();
-    if (0 <= idx) {
+    if (0 <= idx && fileTableMode == FileTable.Mode.REVIEW) {
       String n = diffBase.getValue(idx);
       loadConfigInfo(changeInfo, !n.isEmpty() ? n : null);
     }
@@ -770,16 +770,12 @@ public class ChangeScreen extends Screen {
     final RevisionInfo b = resolveRevisionOrPatchSetId(info, base, null);
 
     CallbackGroup group = new CallbackGroup();
+    Timestamp lastReply = myLastReply(info);
     if (rev.is_edit()) {
-      NativeMap<JsArray<CommentInfo>> emptyComment = NativeMap.create();
-      files.set(
-          b != null ? new PatchSet.Id(changeId, b._number()) : null,
-          new PatchSet.Id(changeId, rev._number()),
-          style, reply, fileTableMode, edit != null);
-      files.setValue(info.edit().files(), myLastReply(info), emptyComment,
-          emptyComment);
+      NativeMap<JsArray<CommentInfo>> empty = NativeMap.create();
+      loadFileList(b, rev, lastReply, group, null, null, empty);
     } else {
-      loadDiff(b, rev, myLastReply(info), group);
+      loadDiff(b, rev, lastReply, group);
     }
     loadCommit(rev, group);
 
@@ -819,23 +815,7 @@ public class ChangeScreen extends Screen {
       final Timestamp myLastReply, CallbackGroup group) {
     final List<NativeMap<JsArray<CommentInfo>>> comments = loadComments(rev, group);
     final List<NativeMap<JsArray<CommentInfo>>> drafts = loadDrafts(rev, group);
-    DiffApi.list(changeId.get(),
-      base != null ? base.name() : null,
-      rev.name(),
-      group.add(new AsyncCallback<NativeMap<FileInfo>>() {
-        @Override
-        public void onSuccess(NativeMap<FileInfo> m) {
-          files.set(
-              base != null ? new PatchSet.Id(changeId, base._number()) : null,
-              new PatchSet.Id(changeId, rev._number()),
-              style, reply, fileTableMode, edit != null);
-          files.setValue(m, myLastReply, comments.get(0), drafts.get(0));
-        }
-
-        @Override
-        public void onFailure(Throwable caught) {
-        }
-      }));
+    loadFileList(base, rev, myLastReply, group, comments, drafts, null);
 
     if (Gerrit.isSignedIn() && fileTableMode == FileTable.Mode.REVIEW) {
       ChangeApi.revision(changeId.get(), rev.name())
@@ -852,6 +832,32 @@ public class ChangeScreen extends Screen {
             }
           }));
     }
+  }
+
+  private void loadFileList(final RevisionInfo base, final RevisionInfo rev,
+      final Timestamp myLastReply, CallbackGroup group,
+      final List<NativeMap<JsArray<CommentInfo>>> comments,
+      final List<NativeMap<JsArray<CommentInfo>>> drafts,
+      final NativeMap<JsArray<CommentInfo>> empty) {
+    DiffApi.list(changeId.get(),
+      base != null ? base.name() : null,
+      rev.name(),
+      group.add(new AsyncCallback<NativeMap<FileInfo>>() {
+        @Override
+        public void onSuccess(NativeMap<FileInfo> m) {
+          files.set(
+              base != null ? new PatchSet.Id(changeId, base._number()) : null,
+              new PatchSet.Id(changeId, rev._number()),
+              style, reply, fileTableMode, edit != null);
+          files.setValue(m, myLastReply,
+              comments == null ? empty : comments.get(0),
+              drafts == null ? empty : drafts.get(0));
+        }
+
+        @Override
+        public void onFailure(Throwable caught) {
+        }
+      }));
   }
 
   private List<NativeMap<JsArray<CommentInfo>>> loadComments(

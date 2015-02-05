b03f00b0cb53d6af3b8b67ebc98b76d9336df013
Stefan Beller <sbeller@google.com>
2015-02-04 17:12:37 -0800

    RestApiServlet: Leave OutputStream open when flushing pending padding
    See discussion on I4dc2d1cd9f52740490fda7c37e98b115fa59ec3a
    
    Change-Id: I84bd3c8031580f805d5d4ef5d70f09b89e170450

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/restapi/RestApiServlet.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/restapi/RestApiServlet.java
@@ -105,6 +105,7 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.EOFException;
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -766,11 +767,16 @@ public class RestApiServlet extends HttpServlet {
     } else {
       b64 = new BinaryResult() {
         @Override
-        public void writeTo(OutputStream out) throws IOException {
-          try (OutputStreamWriter w = new OutputStreamWriter(out, ISO_8859_1);
-              OutputStream e = BaseEncoding.base64().encodingStream(w)) {
+        public void writeTo(final OutputStream out) throws IOException {
+          try (OutputStreamWriter w = new OutputStreamWriter(
+              new FilterOutputStream(out) {
+                @Override
+                public void close() {
+                  // do not close `out`, but only `w` and `e`
+                }
+              }, ISO_8859_1);
+            OutputStream e = BaseEncoding.base64().encodingStream(w)) {
             src.writeTo(e);
-            e.flush();
           }
         }
       };

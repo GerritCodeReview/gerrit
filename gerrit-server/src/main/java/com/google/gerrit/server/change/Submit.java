0398d3ea001b1aead92cb00b7e5a180c781b8ad7
Stefan Beller <sbeller@google.com>
2015-02-06 11:56:11 -0800

    Submit whole topic: check if all changes are submittable
    We were checking if only the current change was submittable, but we
    really need to check all changes
    
    Change-Id: Ief424e7bd32f5d552b3adc18fc3c7c2ff7c46df2

--- gerrit-server/src/main/java/com/google/gerrit/server/change/Submit.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Submit.java
@@ -395,15 +395,20 @@ public class Submit implements RestModifyView<RevisionResource, SubmitInput>,
       IOException {
     Preconditions.checkNotNull(topic);
 
-    List<SubmitRecord> submitRecords = checkSubmitRule(cd,
-        rsrc.getPatchSet(), force);
+    List<ChangeData> changesByTopic = queryProvider.get().byTopicOpen(topic);
+    String problems = areChangesSubmittable(changesByTopic, caller, force);
+    if (problems != null) {
+      throw new OrmRuntimeException(problems);
+    }
 
     Change change = rsrc.getChange();
     ChangeUpdate update = updateFactory.create(rsrc.getControl(), timestamp);
+
+    List<SubmitRecord> submitRecords = checkSubmitRule(cd,
+        rsrc.getPatchSet(), force);
     update.submit(submitRecords);
 
     db.changes().beginTransaction(change.getId());
-    List<ChangeData> changesByTopic = queryProvider.get().byTopicOpen(topic);
     try {
       BatchMetaDataUpdate batch = approve(rsrc, update, caller, timestamp);
       // Write update commit after all normalized label commits.

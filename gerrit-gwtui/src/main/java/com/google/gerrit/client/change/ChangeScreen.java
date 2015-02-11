53a93caa437aa98856ea0421cf353142804c96e0
Stefan Beller <sbeller@google.com>
2015-02-11 10:40:08 -0800

    ChangeScreen: explicitly load revision info
    The new explicit loading of the revision actions is performed by the new
    REST API call /changes/<id>/revisions/<id>/actions, which also checks other
    changes for submittability if `submitWholeTopic` is enabled. This may slow
    down revision loading, hence move it out in an extra async call.
    
    Change-Id: I0be8bcef18aed34d912ab3048436bd62cbc47ead

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ChangeScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/ChangeScreen.java
@@ -238,7 +238,10 @@ public class ChangeScreen extends Screen {
           @Override
           public void onSuccess(ChangeInfo info) {
             info.init();
+            // Revision loading may be slower than the rest, so do it
+            // asynchronous to have the rest fast.
             loadConfigInfo(info, base);
+            loadRevisionInfo();
           }
         }));
   }
@@ -246,12 +249,24 @@ public class ChangeScreen extends Screen {
   void loadChangeInfo(boolean fg, AsyncCallback<ChangeInfo> cb) {
     RestApi call = ChangeApi.detail(changeId.get());
     ChangeList.addOptions(call, EnumSet.of(
-      ListChangesOption.CURRENT_ACTIONS,
+      ListChangesOption.CHANGE_ACTIONS,
       ListChangesOption.ALL_REVISIONS));
     if (!fg) {
       call.background();
     }
     call.get(cb);
+  }
+
+  void loadRevisionInfo() {
+    RestApi call = ChangeApi.actions(changeId.get(), revision);
+    call.background();
+    call.get(new GerritCallback<NativeMap<ActionInfo>>() {
+      @Override
+      public void onSuccess(NativeMap<ActionInfo> actionMap) {
+        actionMap.copyKeysIntoChildren("id");
+        renderRevisionInfo(changeInfo, actionMap);
+      }
+    });
   }
 
   @Override

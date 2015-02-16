b6876302221e0b6136e9eb696455950a8e0ac0bf
David Pursehouse <david.pursehouse@sonymobile.com>
2015-02-16 15:37:10 +0900

    RFC/WiP: Web UI acceptance tests with Selenium Webdriver
    Initial attempt at automated testing of the Gerrit web UI using
    Selenium Webdriver.
    
    Tests will run in different browsers:
    
    - Selenium's built-in "HTML Unit Driver"
    - Firefox
    - Chrome
    
    Firefox and Chrome must be installed on the system where the tests are
    run, otherwise those tests will just fail. Furthermore, for the Chrome
    tests, Selenium's chromedriver [1] must be located in the user's home
    folder. This could be improved to only test with those browsers if they
    are present.
    
    [1] http://chromedriver.storage.googleapis.com/index.html
    
    Change-Id: I7d9e4d6a143676d7509278099fe7e87b170607d1

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/webui/ChromeIT.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.webui;
+
+import static org.junit.Assert.fail;
+
+import com.google.gerrit.acceptance.UseGui;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+@UseGui
+public class ChromeIT extends AbstractWebUiTest {
+  private File getUserHome() {
+    String userHome = System.getProperty("user.home");
+    if (userHome == null || "".equals(userHome)) {
+      userHome = System.getenv("HOME");
+      if (userHome == null || "".equals(userHome)) {
+        fail("Could not determine location of user home folder");
+      }
+    }
+    return new File(userHome);
+  }
+
+  @Override
+  public WebDriver getDriver() {
+    File home = getUserHome();
+    System.setProperty("webdriver.chrome.driver",
+        home.getAbsolutePath() + "/chromedriver");
+    return new ChromeDriver();
+  }
+}

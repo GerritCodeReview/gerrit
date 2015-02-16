310d75df17868eb2df88eb49634b36f24e5326e4
Janice Agustin <janice.agustin@ericsson.com>
2015-02-16 11:03:35 -0500

    WIP: Elasticsearch with custom mapping and queries
    Implement Elasticsearch query builders instead of using Lucene queries.
    Integrate custom mapping for open and closed changes. Index strings as
    analyzed and not_analyzed so that we can search by exact value and regex.
    
    Follow up for David Pursehouse's change adding support for Elasticsearch
    I5e4fc08ce34d33c090c9e0bf320de1b17309f774
    
    This fixes the following test cases:
        - byBranchAndRef
        - byTopic (search for empty fields; fix by Olga Grinberg)
        - byLabel
        - byFileRegex
        - byPathExact
        - byPathRegex
    
    Change-Id: I57528d21bf3b28f5f8889ac6549fef98fd959723

--- gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticsearchQueryChangesTest.java
+++ gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticsearchQueryChangesTest.java
@@ -15,7 +15,6 @@
 package com.google.gerrit.elasticsearch;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.TruthJUnit.assume;
 import static org.junit.Assert.fail;
 
 import com.google.gerrit.server.index.IndexModule.IndexType;
@@ -32,52 +31,11 @@ import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
 import java.io.File;
 
 public class ElasticsearchQueryChangesTest extends AbstractQueryChangesTest {
   private static Node node;
-
-  private void disabled() {
-    assume().that(false).isTrue();
-  }
-
-  @Test
-  @Override
-  public void byBranchAndRef() throws Exception {
-    disabled();
-  }
-
-  @Test
-  @Override
-  public void byTopic() throws Exception {
-    disabled();
-  }
-
-  @Test
-  @Override
-  public void byLabel() throws Exception {
-    disabled();
-  }
-
-  @Test
-  @Override
-  public void byFileRegex() throws Exception {
-    disabled();
-  }
-
-  @Test
-  @Override
-  public void byPathExact() throws Exception {
-    disabled();
-  }
-
-  @Test
-  @Override
-  public void byPathRegex() throws Exception {
-    disabled();
-  }
 
   @Before
   public void startElasticsearchNode() throws Exception {

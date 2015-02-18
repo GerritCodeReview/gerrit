f195c213b4c41078d83b16da2046cbbd0b211032
Shawn Pearce <sop@google.com>
2015-02-17 16:53:22 -0800

    IdentifiedUser: import AccountGroup not bare UUID inner class
    Change-Id: I0c0d1a0d7d0ee4130d5e4abff3bb88cc8d6e85b0

--- gerrit-server/src/main/java/com/google/gerrit/server/IdentifiedUser.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/IdentifiedUser.java
@@ -20,7 +20,7 @@ import com.google.gerrit.common.Nullable;
 import com.google.gerrit.common.data.AccountInfo;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.AccountDiffPreference;
-import com.google.gerrit.reviewdb.client.AccountGroup.UUID;
+import com.google.gerrit.reviewdb.client.AccountGroup;
 import com.google.gerrit.reviewdb.client.AccountProjectWatch;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.StarredChange;
@@ -326,7 +326,7 @@ public class IdentifiedUser extends CurrentUser {
   }
 
   @Override
-  public boolean memberOfAny(Iterable<UUID> ids) {
+  public boolean memberOfAny(Iterable<AccountGroup.UUID> ids) {
     return groupBackend.memberOfAny(this, ids);
   }
 

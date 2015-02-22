6ff4a8872e469507edf2953429e16cbc35acae7f
David Ostrovsky <david@ostrovsky.org>
2015-02-22 01:14:02 +0100

    Expose extension point for generic OAuth providers
    Further development of OAuth authentication scheme support suggested in
    I86fb8fab3 is to restrict the core to expose only the OAuth extension
    point and use Gerrit plugin concept for OAuth provider implementations.
    
    This change makes use of scribe library, that doesn't have any further
    dependency [1].
    
    [1] https://github.com/fernandezpablo85/scribe-java
    
    Bug: issue: 2677
    Bug: issue: 2715
    
    Contributed-by: Luca Milanesio <luca.milanesio@gmail.com>
    Change-Id: I7da0a6b3f2a99b6188bd14cf2818f673a3ddd680

---
+++ gerrit-oauth/src/main/java/com/google/gerrit/httpd/auth/oauth/OAuthSession.java
@@ -0,0 +1,129 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd.auth.oauth;
+
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static org.scribe.model.OAuthConstants.CODE;
+
+import com.google.common.base.Strings;
+import com.google.inject.servlet.SessionScoped;
+
+import org.apache.commons.codec.binary.Base64;
+import org.scribe.model.Token;
+import org.scribe.model.Verifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SessionScoped
+class OAuthSession {
+  private static final Logger log = LoggerFactory.getLogger(OAuthSession.class);
+  private static final SecureRandom randomState = newRandomGenerator();
+  private String state = generateRandomState();
+  private Token token;
+  private String user;
+  private String redirectUrl;
+
+  boolean isLoggedIn() {
+    return token != null && user != null;
+  }
+
+  boolean isOAuthFinal(HttpServletRequest request) {
+    return Strings.emptyToNull(request.getParameter(CODE)) != null;
+  }
+
+  boolean login(HttpServletRequest request, HttpServletResponse response,
+      OAuthServiceProvider oauth) throws IOException {
+    if (isLoggedIn()) {
+      return true;
+    }
+
+    log.debug("Login " + this);
+
+    if (isOAuthFinal(request)) {
+      if (!checkState(request)) {
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        return false;
+      }
+
+      log.debug("Login-Retrieve-User " + this);
+      token = oauth.getService().getAccessToken(null,
+          new Verifier(request.getParameter(CODE)));
+      user = oauth.getUsername(token);
+
+      if (isLoggedIn()) {
+        log.debug("Login-SUCCESS " + this);
+        response.sendRedirect(redirectUrl);
+        return true;
+      } else {
+        response.sendError(SC_UNAUTHORIZED);
+        return false;
+      }
+    } else {
+      log.debug("Login-PHASE1 " + this);
+      String authorizationUrl = oauth.getService().getAuthorizationUrl(null);
+      redirectUrl = request.getRequestURI();
+      String string = authorizationUrl + "&state=" + state;
+      response.sendRedirect(string);
+      return false;
+    }
+  }
+
+  void logout() {
+    token = null;
+    user = null;
+    redirectUrl = null;
+  }
+
+  String getUsername() {
+    return isLoggedIn() ? user : null;
+  }
+
+  private boolean checkState(ServletRequest request) {
+    String s = Strings.nullToEmpty(request.getParameter("state"));
+    if (!s.equals(state)) {
+      log.error("Illegal request state '" + s + "' on OAuthProtocol " + this);
+      return false;
+    }
+    return true;
+  }
+
+  private static SecureRandom newRandomGenerator() {
+    try {
+      return SecureRandom.getInstance("SHA1PRNG");
+    } catch (NoSuchAlgorithmException e) {
+      throw new IllegalArgumentException(
+          "No SecureRandom available for GitHub authentication", e);
+    }
+  }
+
+  private static String generateRandomState() {
+    byte[] state = new byte[32];
+    randomState.nextBytes(state);
+    return Base64.encodeBase64URLSafeString(state);
+  }
+
+  @Override
+  public String toString() {
+    return "GitHubLogin [token=" + token + ", user=" + user + "]";
+  }
+}
---
+++ gerrit-oauth/src/main/java/com/google/gerrit/httpd/auth/oauth/OAuthWebFilter.java
@@ -0,0 +1,124 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd.auth.oauth;
+
+import com.google.common.base.Strings;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.config.AuthConfig;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Singleton
+class OAuthWebFilter implements Filter {
+  private static final Logger log = LoggerFactory
+      .getLogger(OAuthWebFilter.class);
+  public static final String GERRIT_LOGIN = "/login";
+
+  private final Provider<CurrentUser> currentUserProvider;
+  private final Provider<OAuthSession> oauthSessionProvider;
+  private final DynamicSet<OAuthServiceProvider> oauthServiceProviders;
+  private final boolean autoLogin;
+  private OAuthServiceProvider oauthServiceProvider;
+
+  @Inject
+  OAuthWebFilter(Provider<CurrentUser> currentUserProvider,
+      DynamicSet<OAuthServiceProvider> oauthServiceProviders,
+      Provider<OAuthSession> oauthSessionProvider,
+      AuthConfig authConfig) {
+    this.currentUserProvider = currentUserProvider;
+    this.oauthServiceProviders = oauthServiceProviders;
+    this.oauthSessionProvider = oauthSessionProvider;
+    this.autoLogin = Strings.isNullOrEmpty(authConfig.getLoginUrl());
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    oauthServiceProvider = get(oauthServiceProviders);
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+      FilterChain chain) throws IOException, ServletException {
+    HttpSession httpSession = ((HttpServletRequest) request).getSession(false);
+    if (currentUserProvider.get().isIdentifiedUser()) {
+      if (httpSession != null) {
+        httpSession.invalidate();
+      }
+      chain.doFilter(request, response);
+      return;
+    }
+
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+    log.debug("OAuthWebFilter({})", httpRequest.getRequestURL());
+
+    OAuthSession oauthSession = oauthSessionProvider.get();
+    if ((isGerritLogin(httpRequest)
+        || oauthSession.isOAuthFinal(httpRequest))
+        && !oauthSession.isLoggedIn()) {
+      oauthSession.login(httpRequest, httpResponse, oauthServiceProvider);
+    } else if (autoLogin && !oauthSession.isLoggedIn()) {
+      httpResponse.sendRedirect("/login");
+    } else {
+      chain.doFilter(httpRequest, response);
+    }
+  }
+
+  @Override
+  public void destroy() {
+  }
+
+  private OAuthServiceProvider get(
+      DynamicSet<OAuthServiceProvider> oauthServiceProviders) {
+    List<OAuthServiceProvider> providers = new ArrayList<>(1);
+    for (OAuthServiceProvider p : oauthServiceProviders) {
+      providers.add(p);
+    }
+    switch (providers.size()) {
+      case 0:
+        throw new IllegalStateException(
+            "OAuth service provider wasn't installed");
+      case 1:
+        return providers.get(0);
+      default:
+        // TODO(davido): support multiple OAuth service providers
+        throw new IllegalStateException(
+            "Multiple OAuth providers not supported");
+    }
+  }
+
+  private static boolean isGerritLogin(HttpServletRequest request) {
+    return request.getRequestURI().indexOf(GERRIT_LOGIN) >= 0;
+  }
+}

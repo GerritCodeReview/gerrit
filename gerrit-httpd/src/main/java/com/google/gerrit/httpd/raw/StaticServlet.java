e5f93ed53d7eab4499c350cfe9fa379a317c6d95
Dave Borowitz <dborowitz@google.com>
2015-02-24 14:26:21 -0800

    SitePaths: Convert static_dir and themes_dir to Path
    Change-Id: I5099a23c8384a4ad19d2fcd6e3f2503c295ac4c1

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/StaticServlet.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/StaticServlet.java
@@ -17,8 +17,11 @@ package com.google.gerrit.httpd.raw;
 import static com.google.common.net.HttpHeaders.CONTENT_ENCODING;
 import static com.google.common.net.HttpHeaders.ETAG;
 import static com.google.common.net.HttpHeaders.IF_NONE_MATCH;
+import static com.google.gerrit.common.FileUtil.lastModified;
+
 import static java.util.concurrent.TimeUnit.DAYS;
 import static java.util.concurrent.TimeUnit.MINUTES;
+
 import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_MODIFIED;
@@ -30,7 +33,7 @@ import com.google.common.cache.LoadingCache;
 import com.google.common.cache.Weigher;
 import com.google.common.collect.Maps;
 import com.google.common.hash.Hashing;
-import com.google.common.io.ByteStreams;
+import com.google.gerrit.common.FileUtil;
 import com.google.gerrit.common.Nullable;
 import com.google.gerrit.httpd.HtmlDomUtil;
 import com.google.gerrit.server.config.GerritServerConfig;
@@ -44,11 +47,11 @@ import org.eclipse.jgit.lib.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -89,21 +92,19 @@ public class StaticServlet extends HttpServlet {
     return type != null ? type : "application/octet-stream";
   }
 
-  private final File staticBase;
-  private final String staticBasePath;
+  private final Path staticBase;
   private final boolean refresh;
   private final LoadingCache<String, Resource> cache;
 
   @Inject
   StaticServlet(@GerritServerConfig Config cfg, SitePaths site) {
-    File f;
+    Path p;
     try {
-      f = site.static_dir.getCanonicalFile();
+      p = site.static_dir.toRealPath().normalize();
     } catch (IOException e) {
-      f = site.static_dir.getAbsoluteFile();
+      p = site.static_dir.toAbsolutePath().normalize();
     }
-    staticBase = f;
-    staticBasePath = staticBase.getPath() + File.separator;
+    staticBase = p;
     refresh = cfg.getBoolean("site", "refreshHeaderFooter", true);
     cache = CacheBuilder.newBuilder()
         .maximumWeight(1 << 20)
@@ -131,7 +132,8 @@ public class StaticServlet extends HttpServlet {
     }
   }
 
-  private Resource getResource(HttpServletRequest req) throws ExecutionException {
+  private Resource getResource(HttpServletRequest req)
+      throws ExecutionException {
     String name = CharMatcher.is('/').trimFrom(req.getPathInfo());
     if (isUnreasonableName(name)) {
       return Resource.NOT_FOUND;
@@ -209,29 +211,22 @@ public class StaticServlet extends HttpServlet {
   }
 
   private Resource loadResource(String name) throws IOException {
-    File p = new File(staticBase, name);
+    Path p = staticBase.resolve(name);
     try {
-      p = p.getCanonicalFile();
+      p = p.toRealPath().normalize();
     } catch (IOException e) {
       return Resource.NOT_FOUND;
     }
-    if (!p.getPath().startsWith(staticBasePath)) {
+    if (!p.startsWith(staticBase)) {
       return Resource.NOT_FOUND;
     }
 
-    long ts = p.lastModified();
-    FileInputStream in;
-    try {
-      in = new FileInputStream(p);
-    } catch (FileNotFoundException e) {
-      return Resource.NOT_FOUND;
-    }
-
+    long ts = FileUtil.lastModified(p);
     byte[] raw;
     try {
-      raw = ByteStreams.toByteArray(in);
-    } finally {
-      in.close();
+      raw = Files.readAllBytes(p);
+    } catch (NoSuchFileException e) {
+      return Resource.NOT_FOUND;
     }
     return new Resource(p, ts, contentType(name), raw);
   }
@@ -239,13 +234,13 @@ public class StaticServlet extends HttpServlet {
   static class Resource {
     static final Resource NOT_FOUND = new Resource(null, -1, "", new byte[] {});
 
-    final File src;
+    final Path src;
     final long lastModified;
     final String contentType;
     final String etag;
     final byte[] raw;
 
-    Resource(File src, long lastModified, String contentType, byte[] raw) {
+    Resource(Path src, long lastModified, String contentType, byte[] raw) {
       this.src = src;
       this.lastModified = lastModified;
       this.contentType = contentType;
@@ -254,7 +249,7 @@ public class StaticServlet extends HttpServlet {
     }
 
     boolean isStale() {
-      return lastModified != src.lastModified();
+      return lastModified != lastModified(src);
     }
   }
 }

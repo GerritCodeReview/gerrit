10480aa43ffa43c8dea6ac9ebf3c35d8904ffb24
David Pursehouse <david.pursehouse@sonymobile.com>
2015-02-27 15:04:36 +0900

    Allow custom class to extract tracking ID values from commit messages
    Tracking IDs are only indexed from commit messages if they are specified
    in the commit message footer in the Key:Value format.
    
    For projects using a different convention for referencing issue trackers,
    the IDs cannot be indexed.
    
    Provide a new class that a custom class can extend, to extract tracking
    ID values from a commit message in a customized way.
    
    If a class named `com.google.gerrit.CustomTrackingIdExtractor` is found
    in the site's `lib` folder, this will be used.
    
    TODO: documentation
    
    Change-Id: I13afec737bc29e57be029f94cde0c8b498c071ee

--- gerrit-server/src/main/java/com/google/gerrit/server/index/ChangeField.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/index/ChangeField.java
@@ -296,12 +296,13 @@ public class ChangeField {
         public Iterable<String> get(ChangeData input, FillArgs args)
             throws OrmException {
           try {
+            HashSet<String> values = new HashSet<>();
             List<FooterLine> footers = input.commitFooters();
-            if (footers == null) {
-              return null;
+            if (footers != null) {
+              values.addAll(args.trackingFooters.extract(footers).values());
             }
-            return Sets.newHashSet(
-                args.trackingFooters.extract(footers).values());
+            values.addAll(args.trackingValueExtractor.getValues(input.commitMessage()));
+            return values.isEmpty() ? null : values;
           } catch (IOException e) {
             throw new OrmException(e);
           }

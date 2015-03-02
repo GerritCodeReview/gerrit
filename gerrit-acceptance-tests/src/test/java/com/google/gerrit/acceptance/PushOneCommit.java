99f9f99efd333dbbd2336e548c08684090184fce
Hugo Arès <hugo.ares@ericsson.com>
2015-03-02 12:07:21 -0500

    Fix force push
    Since change I3dbf66f17, the force push was no longer working. The intent
    was to set allowNonFastForwards to true for changeEdit but it was always
    setting it to false when not a changeEdit.
    
    Change-Id: Ic4ead13ef5f2fabf2ea2e487c0de0f9ecb1ef52f

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/PushOneCommit.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/PushOneCommit.java
@@ -113,6 +113,7 @@ public class PushOneCommit {
   private final String content;
   private String changeId;
   private Tag tag;
+  private boolean force;
 
   @AssistedInject
   PushOneCommit(ChangeNotes.Factory notesFactory,
@@ -189,11 +190,15 @@ public class PushOneCommit {
       }
       tagCommand.call();
     }
-    return new Result(ref, pushHead(git, ref, tag != null), c, subject);
+    return new Result(ref, pushHead(git, ref, tag != null, force), c, subject);
   }
 
   public void setTag(final Tag tag) {
     this.tag = tag;
+  }
+
+  public void setForce(boolean force) {
+    this.force = force;
   }
 
   public class Result {

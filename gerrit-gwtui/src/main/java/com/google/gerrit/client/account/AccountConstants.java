6dce0e47a270bd03dcbc45b9f8d681e91f6047d0
Yang Zhenhui <zhenhui.yang@sonymobile.com>
2015-03-03 15:29:26 -0800

    Send email to subscribers for direct pushes on branch
    Add a "Direct Pushes" configuration on watched projects and for project
    level notification. This enables receipt of a notification when ever a
    new commit is pushed directly into the repository bypassing review, for
    example:
    
      git push remote HEAD:refs/heads/master
    
    Now, the user can receive email when this occurs. Previously, users had
    to set up ancillary notification systems such as Teamforge notification
    plugin, or similar.
    
    Signed-off-by: Yang Zhenhui <zhenhui.yang@sonymobile.com>
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>
    Change-Id: I75bad73e3f71cd01a3bef6f2c66a65d0fe1b8c38
    Bug: issue 118

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/account/AccountConstants.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/account/AccountConstants.java
@@ -116,6 +116,7 @@ public interface AccountConstants extends Constants {
   String watchedProjectColumnAllComments();
   String watchedProjectColumnSubmittedChanges();
   String watchedProjectColumnAbandonedChanges();
+  String watchedProjectColumnDirectPushes();
 
   String contactFieldFullName();
   String contactFieldEmail();
--- gerrit-gwtui/src/main/java/com/google/gerrit/client/account/MyWatchesTable.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/account/MyWatchesTable.java
@@ -49,17 +49,19 @@ public class MyWatchesTable extends FancyFlexTable<AccountProjectWatchInfo> {
     fmt.setRowSpan(0, 2, 2);
     fmt.getElement(0, 3).setPropertyString("align", "center");
 
-    fmt.setColSpan(0, 3, 5);
+    fmt.setColSpan(0, 3, 6);
     table.setText(1, 0, Util.C.watchedProjectColumnNewChanges());
     table.setText(1, 1, Util.C.watchedProjectColumnNewPatchSets());
     table.setText(1, 2, Util.C.watchedProjectColumnAllComments());
     table.setText(1, 3, Util.C.watchedProjectColumnSubmittedChanges());
     table.setText(1, 4, Util.C.watchedProjectColumnAbandonedChanges());
+    table.setText(1, 5, Util.C.watchedProjectColumnDirectPushes());
     fmt.addStyleName(1, 0, Gerrit.RESOURCES.css().dataHeader());
     fmt.addStyleName(1, 1, Gerrit.RESOURCES.css().dataHeader());
     fmt.addStyleName(1, 2, Gerrit.RESOURCES.css().dataHeader());
     fmt.addStyleName(1, 3, Gerrit.RESOURCES.css().dataHeader());
     fmt.addStyleName(1, 4, Gerrit.RESOURCES.css().dataHeader());
+    fmt.addStyleName(1, 5, Gerrit.RESOURCES.css().dataHeader());
   }
 
   public void deleteChecked() {
@@ -141,6 +143,7 @@ public class MyWatchesTable extends FancyFlexTable<AccountProjectWatchInfo> {
     addNotifyButton(AccountProjectWatch.NotifyType.ALL_COMMENTS, info, row, 5);
     addNotifyButton(AccountProjectWatch.NotifyType.SUBMITTED_CHANGES, info, row, 6);
     addNotifyButton(AccountProjectWatch.NotifyType.ABANDONED_CHANGES, info, row, 7);
+    addNotifyButton(AccountProjectWatch.NotifyType.DIRECT_PUSHES, info, row, 8);
 
     final FlexCellFormatter fmt = table.getFlexCellFormatter();
     fmt.addStyleName(row, 1, Gerrit.RESOURCES.css().iconCell());
@@ -150,6 +153,7 @@ public class MyWatchesTable extends FancyFlexTable<AccountProjectWatchInfo> {
     fmt.addStyleName(row, 5, Gerrit.RESOURCES.css().dataCell());
     fmt.addStyleName(row, 6, Gerrit.RESOURCES.css().dataCell());
     fmt.addStyleName(row, 7, Gerrit.RESOURCES.css().dataCell());
+    fmt.addStyleName(row, 8, Gerrit.RESOURCES.css().dataCell());
 
     setRowItem(row, info);
   }

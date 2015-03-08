1fb3550a082294fd3ba09f93b59f78b15efedcda
Urs Wolfer <uwolfer@fwo.ch>
2015-03-08 16:22:04 +0100

    Extensions API: Add config API
    Initial skeleton includes server API with method
    to get server version.
    
    Structure and naming follows existing REST API.
    
    Change-Id: I98dbc21889cd6d6ea7ce8dd224b6f013b06c516d

--- gerrit-server/src/main/java/com/google/gerrit/server/api/GerritApiImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/api/GerritApiImpl.java
@@ -17,6 +17,7 @@ package com.google.gerrit.server.api;
 import com.google.gerrit.extensions.api.GerritApi;
 import com.google.gerrit.extensions.api.accounts.Accounts;
 import com.google.gerrit.extensions.api.changes.Changes;
+import com.google.gerrit.extensions.api.config.Config;
 import com.google.gerrit.extensions.api.groups.Groups;
 import com.google.gerrit.extensions.api.projects.Projects;
 import com.google.inject.Inject;
@@ -26,16 +27,19 @@ import com.google.inject.Singleton;
 class GerritApiImpl implements GerritApi {
   private final Accounts accounts;
   private final Changes changes;
+  private final Config config;
   private final Groups groups;
   private final Projects projects;
 
   @Inject
   GerritApiImpl(Accounts accounts,
       Changes changes,
+      Config config,
       Groups groups,
       Projects projects) {
     this.accounts = accounts;
     this.changes = changes;
+    this.config = config;
     this.groups = groups;
     this.projects = projects;
   }
@@ -48,6 +52,11 @@ class GerritApiImpl implements GerritApi {
   @Override
   public Changes changes() {
     return changes;
+  }
+
+  @Override
+  public Config config() {
+    return config;
   }
 
   @Override
---
+++ gerrit-server/src/main/java/com/google/gerrit/server/api/config/ConfigImpl.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.api.config;
+
+import com.google.gerrit.extensions.api.config.Config;
+import com.google.gerrit.extensions.api.config.Server;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class ConfigImpl implements Config {
+  private final ServerImpl serverApi;
+
+  @Inject
+  ConfigImpl(ServerImpl serverApi) {
+    this.serverApi = serverApi;
+  }
+
+  @Override
+  public Server server() {
+    return serverApi;
+  }
+}

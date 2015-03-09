6ca071c6e0fb17f6b70b6ef782e68eb6056c04a6
Stefan Beller <sbeller@google.com>
2015-03-09 12:10:57 -0700

    WIP/SubmoduleOp: Fix merging multiple changes to superprojects
    Under certain conditions the SubmoduleOp did not behave correctly. Instead
    of merging the changes from a submodule to a superproject, there was a
    null pointer exception instead when parsing the commit messages of the
    changes.
    
    Bug: issue 3197
    Change-Id: I168cb2feeaba072987a9376352d289f6acccc08a

--- gerrit-server/src/main/java/com/google/gerrit/server/git/SubmoduleOp.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/SubmoduleOp.java
@@ -129,7 +129,7 @@ public class SubmoduleOp {
 
       updateSubmoduleSubscriptions();
       updateSuperProjects(destBranch, rw, mergeTip.getId().toObjectId(), null);
-    } catch (OrmException e) {
+    } catch (OrmException | IOException e) {
       throw new SubmoduleException("Cannot open database", e);
     } finally {
       if (schema != null) {
@@ -205,7 +205,8 @@ public class SubmoduleOp {
   }
 
   private void updateSuperProjects(final Branch.NameKey updatedBranch, RevWalk myRw,
-      final ObjectId mergedCommit, final String msg) throws SubmoduleException {
+      final ObjectId mergedCommit, final String msg) throws SubmoduleException,
+      IOException {
     try {
       final List<SubmoduleSubscription> subscribers =
           schema.submoduleSubscriptions().bySubmodule(updatedBranch).toList();
@@ -231,6 +232,7 @@ public class SubmoduleOp {
                 && (c.getStatusCode() == CommitMergeStatus.CLEAN_MERGE
                     || c.getStatusCode() == CommitMergeStatus.CLEAN_PICK
                     || c.getStatusCode() == CommitMergeStatus.CLEAN_REBASE)) {
+              myRw.parseBody(c);
               sb.append("\n")
                 .append(c.getFullMessage());
             }

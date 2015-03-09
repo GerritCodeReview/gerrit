dce679137e4c6ba352abc6d155edf2086b4779c0
Hugo Arès <hugo.ares@ericsson.com>
2015-03-09 15:20:28 -0400

    Work around MyersDiff infinite loop in PatchListLoader
    This infinite loop is happening for some files when the PatchListLoader
    is computing the differences between 2 commits. It first showed up[1] in
    the mergeability checks done in background and was easy to work around
    by killing the thread using Javamelody and abandoning the faulty commits.
    
    The issue showed up again, when upgrading from 2.9.x to 2.10: the online
    reindexer getting stuck because of that infinite loop and this time, no
    easy work around.
    
    Use a similar approach that was done in intraline diff to work around
    the MyersDiff infinite loop. Instead of returning a timeout error
    message when the infinite loop is detected, fallback to a diff algorithm
    that does not use MyersDiff. Returning a timeout error was not an option
    because the failing operation is not always triggered by a user.
    
    From the user perspective, the only difference when the infinite loop
    is detected is that the files in the commit will not be compared
    in-depth, which will result in bigger edit regions.
    
    [1]https://groups.google.com/d/msg/repo-discuss/ZtiCilM3wFA/LijfZ4YkLHsJ
    
    Change-Id: Ib00de070dd8df1722d4ade0a83c0ffa8eaa37f8e

--- gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchListLoader.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchListLoader.java
@@ -16,11 +16,13 @@
 package com.google.gerrit.server.patch;
 
 import com.google.common.base.Function;
+import com.google.common.base.Throwables;
 import com.google.common.cache.CacheLoader;
 import com.google.common.collect.FluentIterable;
 import com.google.gerrit.reviewdb.client.AccountDiffPreference.Whitespace;
 import com.google.gerrit.reviewdb.client.Patch;
 import com.google.gerrit.reviewdb.client.RefNames;
+import com.google.gerrit.server.config.ConfigUtil;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.MergeUtil;
@@ -69,6 +71,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class PatchListLoader extends CacheLoader<PatchListKey, PatchList> {
   static final Logger log = LoggerFactory.getLogger(PatchListLoader.class);
@@ -76,13 +84,23 @@ public class PatchListLoader extends CacheLoader<PatchListKey, PatchList> {
   private final GitRepositoryManager repoManager;
   private final PatchListCache patchListCache;
   private final ThreeWayMergeStrategy mergeStrategy;
+  private final ExecutorService diffExecutor;
+  private final long timeoutMillis;
+
 
   @Inject
-  PatchListLoader(GitRepositoryManager mgr, PatchListCache plc,
-      @GerritServerConfig Config cfg) {
+  PatchListLoader(GitRepositoryManager mgr,
+      PatchListCache plc,
+      @GerritServerConfig Config cfg,
+      @DiffExecutor ExecutorService de) {
     repoManager = mgr;
     patchListCache = plc;
     mergeStrategy = MergeUtil.getMergeStrategy(cfg);
+    diffExecutor = de;
+    timeoutMillis =
+        ConfigUtil.getTimeUnit(cfg, "cache", PatchListCacheImpl.FILE_NAME,
+            "timeout", TimeUnit.MILLISECONDS.convert(5, TimeUnit.SECONDS),
+            TimeUnit.MILLISECONDS);
   }
 
   @Override
@@ -164,7 +182,7 @@ public class PatchListLoader extends CacheLoader<PatchListKey, PatchList> {
         DiffEntry diffEntry = diffEntries.get(i);
         if (paths == null || paths.contains(diffEntry.getNewPath())
             || paths.contains(diffEntry.getOldPath())) {
-          FileHeader fh = df.toFileHeader(diffEntry);
+          FileHeader fh = toFileHeader(key, df, diffEntry);
           entries.add(newEntry(aTree, fh));
         }
       }
@@ -173,6 +191,44 @@ public class PatchListLoader extends CacheLoader<PatchListKey, PatchList> {
     } finally {
       reader.release();
     }
+  }
+
+  private FileHeader toFileHeader(PatchListKey key,
+      final DiffFormatter diffFormatter, final DiffEntry diffEntry)
+      throws IOException {
+
+    Future<FileHeader> result = diffExecutor.submit(new Callable<FileHeader>() {
+      @Override
+      public FileHeader call() throws IOException {
+        return diffFormatter.toFileHeader(diffEntry);
+      }
+    });
+
+    try {
+      return result.get(timeoutMillis, TimeUnit.MILLISECONDS);
+    } catch (InterruptedException | TimeoutException e) {
+      log.warn(timeoutMillis + " ms timeout reached for Diff loader"
+                      + " in project " + key.projectKey.get()
+                      + " on commit " + key.getNewId()
+                      + " on path " + diffEntry.getNewPath()
+                      + " comparing " + diffEntry.getOldId()
+                      + ".." + diffEntry.getNewId());
+      result.cancel(true);
+      return toFileHeaderWithoutMyersDiff(diffFormatter, diffEntry);
+    } catch (ExecutionException e) {
+      // If there was an error computing the result, carry it
+      // up to the caller so the cache knows this key is invalid.
+      Throwables.propagateIfInstanceOf(e.getCause(), IOException.class);
+      throw new IOException(e.getMessage(), e.getCause());
+    }
+  }
+
+  private FileHeader toFileHeaderWithoutMyersDiff(DiffFormatter diffFormatter,
+      DiffEntry diffEntry) throws IOException {
+    HistogramDiff histogramDiff = new HistogramDiff();
+    histogramDiff.setFallbackAlgorithm(null);
+    diffFormatter.setDiffAlgorithm(histogramDiff);
+    return diffFormatter.toFileHeader(diffEntry);
   }
 
   private PatchListEntry newCommitMessage(final RawTextComparator cmp,

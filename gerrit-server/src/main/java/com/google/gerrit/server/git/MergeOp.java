4c3722c5f50b8b19f750f33d35b564fcc328e428
Stefan Beller <sbeller@google.com>
2015-03-19 13:00:08 -0700

    WIP/ChangeMergeQueue: Rewrite such that it works on set of changes
    ChangeMergeQueue used to work on branches rather than sets of changes.
    This change is a first step to merge sets of changes (e.g. grouped by a
    topic and `changes.submitWholeTopic` enabled) in an atomic fashion.
    This change doesn't aim to implement these changes, but only as a step
    towards it.
    
    MergeOp which performs the actual merging, has been renamed to
    MergeOpBranch and keeps its functionality and behavior as is. A new class
    MergeOp is introduced which will map the set of changes to the set of
    branches. Eventually one of the classes should go away again, when the
    merging itself is performed on a change set level instead of pulling
    in as many changes to a triggered branch as possible.
    
    TODO: Either rename MergeOp/MergeOpMapper, or adapt names.
    
    Change-Id: I1ffe09a505e25f15ce1521bcfb222e51e62c2a14

--- gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
@@ -93,6 +93,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -115,12 +116,12 @@ import java.util.Set;
  * marking it as conflicting, even if an earlier commit along that same line can
  * be merged cleanly.
  */
-public class MergeOp {
+public class MergeOpBranch {
   public interface Factory {
-    MergeOp create(Branch.NameKey branch);
+    MergeOpBranch create(Branch.NameKey branch);
   }
 
-  private static final Logger log = LoggerFactory.getLogger(MergeOp.class);
+  private static final Logger log = LoggerFactory.getLogger(MergeOpBranch.class);
 
   /** Amount of time to wait between submit and checking for missing deps. */
   private static final long DEPENDENCY_DELAY =
@@ -176,7 +177,7 @@ public class MergeOp {
   private PersonIdent refLogIdent;
 
   @Inject
-  MergeOp(AccountCache accountCache,
+  MergeOpBranch(AccountCache accountCache,
       ApprovalsUtil approvalsUtil,
       ChangeControl.GenericFactory changeControlFactory,
       ChangeData.Factory changeDataFactory,
@@ -278,7 +279,7 @@ public class MergeOp {
           }
           SubmitStrategy strategy = createStrategy(submitType);
           MergeTip mergeTip = preMerge(strategy, toMerge.get(submitType));
-          RefUpdate update = updateBranch(strategy, branchUpdate);
+          RefUpdate update = updateBranch(strategy, branchUpdate, toSubmit.get(submitType));
           reopen = true;
 
           updateChangeStatus(toSubmit.get(submitType), mergeTip);
@@ -658,7 +659,7 @@ public class MergeOp {
   }
 
   private RefUpdate updateBranch(SubmitStrategy strategy,
-      RefUpdate branchUpdate) throws MergeException {
+      RefUpdate branchUpdate, List<Change> changes) throws MergeException {
     CodeReviewCommit currentTip =
         mergeTip != null ? mergeTip.getCurrentTip() : null;
     if (Objects.equals(branchTip, currentTip)) {
@@ -715,8 +716,8 @@ public class MergeOp {
         case LOCK_FAILURE:
           String msg;
           if (strategy.retryOnLockFailure()) {
-            mergeQueue.recheckAfter(destBranch, LOCK_FAILURE_RETRY_DELAY,
-                MILLISECONDS);
+            mergeQueue.recheckAfter(new HashSet<>(changes),
+                LOCK_FAILURE_RETRY_DELAY, MILLISECONDS);
             msg = "will retry";
           } else {
             msg = "will not retry";
@@ -859,7 +860,8 @@ public class MergeOp {
       // If we waited a short while we might still be able to get
       // this change submitted. Reschedule an attempt in a bit.
       //
-      mergeQueue.recheckAfter(destBranch, recheckIn, MILLISECONDS);
+      mergeQueue.recheckAfter(new HashSet<>(Arrays.asList(c)), recheckIn,
+          MILLISECONDS);
       capable = Capable.OK;
     } else if (submitStillPossible) {
       // It would be possible to submit the change if the missing
---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOpMapper.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.gerrit.reviewdb.client.Branch;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.assistedinject.Assisted;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is a 'temporary' class for mapping sets of changes to the
+ * set of branches required for merging.
+ */
+public class MergeOp {
+  public interface Factory {
+    MergeOpBranch create(Set<Change> changes);
+  }
+
+  private final Set<Change> changes;
+  private final Provider<MergeOpBranch.Factory> bgFactory;
+
+  @Inject
+  MergeOp(
+      Provider<MergeOpBranch.Factory> bgFactory,
+      @Assisted Set<Change> changes) {
+    this.bgFactory = bgFactory;
+    this.changes = changes;
+  }
+
+  public void merge()
+      throws MergeException, NoSuchChangeException, IOException {
+    HashSet<Branch.NameKey> set = new HashSet<>();
+    for (Change c : changes) {
+      set.add(c.getDest());
+    }
+    for (Branch.NameKey branch : set) {
+      bgFactory.get().create(branch).merge();
+    }
+  }
+}

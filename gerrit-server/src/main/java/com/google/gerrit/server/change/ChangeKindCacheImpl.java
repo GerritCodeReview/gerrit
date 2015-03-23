44475bd7bf7ceb8453b3d8814d70865823746789
Marco Miller <marco.miller@ericsson.com>
2015-03-23 13:13:30 -0400

    Add copyAllScoresOnNonTrivialRebase project config (optional) label
    Add copyAllScoresOnNonTrivialRebase label to project config, which is of
    course optional and disabled by default. If enabled, will copy all
    review scores in case of non-trivial (conflicting) rebase. This is
    similar to the existing Rework case, adding further handling for the
    non-trivial, kind of Rework, rebase case. The act of changing the parent
    for the commit differentiates non-trivial from rework, once we're clear
    on the case not being a trivial rebase.
    
    Add new copyAllScoresOnNonTrivialRebase config label to Documentation.
    The latter is based on the current doc for copyAllScoresOnTrivialRebase.
    -Only difference being that non-trivial targets a differing code delta.
    (There is also a mention about enabling only if "explicitly wanted".)
    
    Add this new NON_TRIVIAL_REBASE case to Documentation for change kind.
    Clarify the sibling REWORK case (in documentation) accordingly.
    
    Add 2 tests in LabelTypeIT to cover this new NON_TRIVIAL_REBASE handling
    in ChangeKindCacheImpl. All other LabelTypeIT tests still passing and
    debugged to confirm that they still hit their respective (case-specific)
    breakpoints in ChangeKindCacheImpl. Also confirmed is
    editCommitMessageCopiesLabelScores in ChangeEditIT. Unit tests passing
    before this change still pass.
    
    To further clarify this addition in its context, here is a recap/summary
    of all involved copy-score cases, in terms of commit aspects (triggers)
    for them. Below, all cases assume a differing SHA-1 hash:
    
          |no-chg|no-code-chg|trivial-rebase|non-trivial-rebase|other-rework
    ------+------+-----------+--------------+------------------+------------
     delta| same |   same    |     same     |       diff       | diff||same
       msg| same |   diff    |     same     |       same       | same||diff
    parent| same |   same    |     diff     |       diff       |    same
    ------+------+-----------+--------------+------------------+------------
    
    Above,
    - chg == change;
    - delta == code delta;
    - msg == commit message;
    - same == does not change;
    - diff == differs, changes (!same).
    
    Change-Id: Ibd787ecf8fe4d8135e53cfda1b996ecf39f1f98d

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeKindCacheImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeKindCacheImpl.java
@@ -222,7 +222,11 @@ public class ChangeKindCacheImpl implements ChangeKindCache {
             && merger.getResultTreeId().equals(next.getTree())) {
           return ChangeKind.TRIVIAL_REBASE;
         } else {
-          return ChangeKind.REWORK;
+          if (prior.getParent(0).equals(next.getParent(0))) {
+            return ChangeKind.REWORK;
+          } else {
+            return ChangeKind.NON_TRIVIAL_REBASE;
+          }
         }
       } finally {
         key.repo = null;

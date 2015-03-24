f000798848e92e905d1b1aad95082aad53803080
Dave Borowitz <dborowitz@google.com>
2015-03-23 17:21:53 -0700

    Implement list extension API
    Convert ListBranchesIT to use this API. Tighten up the BranchAssert
    library so tests are less verbose.
    
    Change-Id: I7ff9460cb6d2d559b9bdf6a94191602e389981e3

--- gerrit-server/src/main/java/com/google/gerrit/server/project/RefControl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/RefControl.java
@@ -364,18 +364,13 @@ public class RefControl {
     }
 
     switch (getCurrentUser().getAccessPath()) {
-      case REST_API:
-      case JSON_RPC:
-      case SSH_COMMAND:
-        return getCurrentUser().getCapabilities().canAdministrateServer()
-            || (isOwner() && !isForceBlocked(Permission.PUSH))
-            || canPushWithForce();
-
       case GIT:
         return canPushWithForce();
 
       default:
-        return false;
+        return getCurrentUser().getCapabilities().canAdministrateServer()
+            || (isOwner() && !isForceBlocked(Permission.PUSH))
+            || canPushWithForce();
     }
   }
 

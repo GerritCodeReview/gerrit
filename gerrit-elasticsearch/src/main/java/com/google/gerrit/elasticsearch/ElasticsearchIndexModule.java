81b4cef815d665db0065ffffa4c69062bae04ae0
David Pursehouse <david.pursehouse@sonymobile.com>
2015-04-07 16:37:47 +0900

    Updates for Elasticsearch to work with Lucene 5
    Update to a snapshot version of Elasticsearch 2.0 which must be built
    from source and installed to the local Maven repository.
    
    Add several other new dependencies required by Elasticsearch.
    
    Provide an indexConfig instead of creating a default.
    
    Joda-time: query dates using the proper date format. Avoid the known
    bug, where the maximum date value generated by long.MAX_VALUE exceeds
    the accepted range.
    
    Rename the legacy id from "_id" to id. Elasticsearch reserves "_id" and
    it may no longer be configured.
    
    Convert elasticsearch result to change data. This fixes the broken
    "reviewedby" test case. Currently missing implementation for setting
    change proto, patch sets and approvals.
    
    Change-Id: Ie58f978f25fd6179b0e115f89135d5786de3843e

--- gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticsearchIndexModule.java
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticsearchIndexModule.java
@@ -43,7 +43,6 @@ public class ElasticsearchIndexModule extends LifecycleModule {
 
   @Override
   protected void configure() {
-    bind(IndexConfig.class).toInstance(IndexConfig.createDefault());
     install(new IndexModule(threads));
     bind(ChangeIndex.class).to(ElasticsearchChangeIndex.class);
     listener().to(ElasticsearchChangeIndex.class);
@@ -58,5 +57,11 @@ public class ElasticsearchIndexModule extends LifecycleModule {
       FillArgs fillArgs) {
     return new ElasticsearchChangeIndex(cfg, db, changeDataFactory, fillArgs,
         indexes, ChangeSchemas.getLatest());
+  }
+
+  @Provides
+  @Singleton
+  IndexConfig getIndexConfig(@GerritServerConfig Config cfg) {
+    return IndexConfig.fromConfig(cfg);
   }
 }

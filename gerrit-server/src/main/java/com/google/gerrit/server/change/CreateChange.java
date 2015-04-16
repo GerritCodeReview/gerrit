98d81db343b6c5e9f3cee2b8f3ac8664679b30e8
Dave Borowitz <dborowitz@google.com>
2015-04-16 11:47:32 -0700

    CreateChange: Remove unused import
    Change-Id: Ic2c1dbae7cb9f3c37cee71468e3412ee0f917117

--- gerrit-server/src/main/java/com/google/gerrit/server/change/CreateChange.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/CreateChange.java
@@ -56,7 +56,6 @@ import com.google.inject.Singleton;
 
 import org.eclipse.jgit.lib.CommitBuilder;
 import org.eclipse.jgit.lib.Config;
-import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.PersonIdent;

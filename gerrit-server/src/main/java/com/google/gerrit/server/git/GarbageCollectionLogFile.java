e1a551c4c610bc41ecb627ce8c3c3e4d38477dd6
Hugo Arès <hugo.ares@ericsson.com>
2015-04-24 08:48:44 -0400

    Remove logs folder creation from GarbageCollectionLogFile
    This is not needed, logs folder will be created by log4j if it does not
    exist.
    
    Change-Id: Ied7cb07735c289e6f97d8f780ae81e9b9dd78b29

--- gerrit-server/src/main/java/com/google/gerrit/server/git/GarbageCollectionLogFile.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/GarbageCollectionLogFile.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.server.git;
 
-import com.google.gerrit.common.Die;
 import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.gerrit.server.config.SitePaths;
 import com.google.gerrit.server.util.SystemLog;
@@ -30,12 +29,8 @@ public class GarbageCollectionLogFile implements LifecycleListener {
 
   @Inject
   public GarbageCollectionLogFile(SitePaths sitePaths) {
-    File logdir = sitePaths.logs_dir;
-    if (!logdir.exists() && !logdir.mkdirs()) {
-      throw new Die("Cannot create log directory: " + logdir);
-    }
     if (SystemLog.shouldConfigure()) {
-      initLogSystem(logdir);
+      initLogSystem(sitePaths.logs_dir);
     }
   }
 

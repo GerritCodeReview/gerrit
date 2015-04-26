df98f4b74c244f65140334a2dc2347d2d4235376
David Ostrovsky <david@ostrovsky.org>
2015-04-26 18:52:45 +0200

    Bump JGit version to 3.7.1.201504261725-r
    This version fixed JGit regression, causing severe (>10x)
    performance degradation on huge repositories (>2GB) on git
    push and CPU consumption explosion during replication: [1].
    
    [1] https://bugs.eclipse.org/bugs/show_bug.cgi?id=465509
    Change-Id: I6b1fa985fa3738801d3fa27d690a1c02c1afc1db

--- tools/util.py
+++ tools/util.py
@@ -20,6 +20,7 @@ REPO_ROOTS = {
   'GERRIT_API': 'https://gerrit-api.commondatastorage.googleapis.com/release',
   'MAVEN_CENTRAL': 'http://repo1.maven.org/maven2',
   'MAVEN_LOCAL': 'file://' + path.expanduser('~/.m2/repository'),
+  'MAVEN_STAGING_JGIT_1026': 'https://oss.sonatype.org/content/repositories/orgeclipsejgit-1026',
 }
 
 

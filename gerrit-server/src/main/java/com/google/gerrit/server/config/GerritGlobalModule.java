c63516f114abd9170364eaf8a30bae6bc5ac0e3e
Stefan Beller <sbeller@google.com>
2015-04-29 14:36:52 -0700

    ChangeMergeQueue: Rewrite such that it works on set of changes
    ChangeMergeQueue used to work on branches rather than sets of changes.
    This change is a first step to merge sets of changes (e.g. grouped by a
    topic and `changes.submitWholeTopic` enabled) in an atomic fashion.
    This change doesn't aim to implement these changes, but only as a step
    towards it.
    
    MergeOp keeps its functionality and behavior as is. A new class
    MergeOpMapper is introduced which will map the set of changes to
    the set of branches. Additionally the MergeOpMapper is also
    responsible for the threading done right now, which was part of
    the ChangeMergeQueue before.
    
    Change-Id: I1ffe09a505e25f15ce1521bcfb222e51e62c2a14

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -72,6 +72,7 @@ import com.google.gerrit.server.events.EventFactory;
 import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
 import com.google.gerrit.server.git.ChangeMergeQueue;
 import com.google.gerrit.server.git.GitModule;
+import com.google.gerrit.server.git.MergeOpMapper;
 import com.google.gerrit.server.git.MergeQueue;
 import com.google.gerrit.server.git.MergeUtil;
 import com.google.gerrit.server.git.NotesBranchUtil;
@@ -219,6 +220,7 @@ public class GerritGlobalModule extends FactoryModule {
     bind(ApprovalsUtil.class);
     bind(ChangeMergeQueue.class).in(SINGLETON);
     bind(MergeQueue.class).to(ChangeMergeQueue.class).in(SINGLETON);
+    factory(MergeOpMapper.Factory.class);
 
     bind(RuntimeInstance.class)
         .toProvider(VelocityRuntimeProvider.class)

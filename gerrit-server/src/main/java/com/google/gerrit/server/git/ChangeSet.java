ba0784810594ebd4e9cb47262c369d76da729d51
Stefan Beller <sbeller@google.com>
2015-04-29 14:36:52 -0700

    ChangeMergeQueue: Rewrite such that it works on set of changes
    ChangeMergeQueue used to work on branches rather than sets of changes.
    This change is a first step to merge sets of changes (e.g. grouped by a
    topic and `changes.submitWholeTopic` enabled) in an atomic fashion.
    This change doesn't aim to implement these changes, but only as a step
    towards it.
    
    MergeOp keeps its functionality and behavior as is. A new class
    MergeOpMapper is introduced which will map the set of changes to
    the set of branches. Additionally the MergeOpMapper is also
    responsible for the threading done right now, which was part of
    the ChangeMergeQueue before. ChangeMergeQueue however gets a
    new responsibiltiy, it needs to make sure all changes to be
    merged, which requires locking the set of branches the set of
    changes is involved in. For convenience the set of changes are
    adressed via the new ChangeSet class.
    
    Change-Id: I1ffe09a505e25f15ce1521bcfb222e51e62c2a14

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ChangeSet.java
@@ -0,0 +1,41 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.reviewdb.client.Branch;
+import com.google.gerrit.reviewdb.client.Change;
+
+@AutoValue
+abstract class ChangeSet {
+  public static ChangeSet create(Iterable<Change> changes) {
+    ImmutableSet.Builder<Branch.NameKey> bb = ImmutableSet.builder();
+    ImmutableSet.Builder<Change.Id> ib = ImmutableSet.builder();
+    for (Change c : changes) {
+      bb.add(c.getDest());
+      ib.add(c.getId());
+    }
+    return new AutoValue_ChangeSet(bb.build(), ib.build());
+  }
+
+  abstract ImmutableSet<Branch.NameKey> branches();
+  abstract ImmutableSet<Change.Id> ids();
+
+  @Override
+  public int hashCode() {
+    return ids().hashCode();
+  }
+}

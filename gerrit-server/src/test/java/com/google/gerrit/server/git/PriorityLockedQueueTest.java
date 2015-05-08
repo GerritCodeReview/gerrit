55e075947f88fdfc10b68be46cc1fd7a56feb10b
Stefan Beller <sbeller@google.com>
2015-05-07 17:13:54 -0700

    priolockedQueueTests
    Change-Id: I2d3b294488d1f49e0ea74db27245ba31ba2c120b

--- gerrit-server/src/test/java/com/google/gerrit/server/git/PriorityLockedQueueTest.java
+++ gerrit-server/src/test/java/com/google/gerrit/server/git/PriorityLockedQueueTest.java
@@ -14,106 +14,162 @@
 
 package com.google.gerrit.server.git;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.Sets;
+
 import org.jmock.lib.concurrent.DeterministicScheduler;
 import org.junit.Test;
 
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-import autovalue.shaded.com.google.common.common.collect.Sets;
+public class PriorityLockedQueueTest {
 
-
-class PriorityLockedQueueTest {
-
-  private final Set<String> processedEntries;
-  DeterministicScheduler scheduler;
+  Set<String> processedEntries;
+  ScheduledExecutorService scheduler;
   TestPriorityLockedQueue testPriorityLockedQueue;
 
-  PriorityLockedQueueTest() {
-    DeterministicScheduler scheduler = new DeterministicScheduler();
+  // This test is a lot about timing, except for testing the processOneTask test
+  // which would lock up
+  boolean noBlocking;
+
+  public PriorityLockedQueueTest() {
+    //scheduler = new DeterministicScheduler();
+    //scheduler = new ScheduledExecutorService();
+    scheduler = Executors.newSingleThreadScheduledExecutor();
+
+    testPriorityLockedQueue = new TestPriorityLockedQueue(scheduler);
     processedEntries = Sets.newHashSet();
-    new PriorityLockedQueue<String> {
-
-    }
+    noBlocking = false;
   }
 
   @Test
-  public void testOneProcess() {
+  public void testOneProcess() throws Exception {
+    // We need to start the test in its own thread and in this thread we will
+    // just control the time, so we don't lock up ourselves.
+    noBlocking = true;
+    assertThat(testPriorityLockedQueue.processItem(new ResourceTask<String>() {
+      @Override
+      public Set<String> resources() {
+        return Sets.newHashSet("A");
+      }
+    })).isEqualTo(true);
+    noBlocking = false;
 
+    assertThat(processedEntries).contains("A");
   }
 
   @Test
-  public void testSchedule() {
-       context.checking(new Expectations() {{
-      door.unlock();
-  }});
+  public void testSchedule() throws Exception {
+    testPriorityLockedQueue.schedule(new ResourceTask<String>() {
+      @Override
+      public Set<String> resources() {
+        return Sets.newHashSet("B");
+      }
+    }, 0);
+    //scheduler.tick(2, TimeUnit.SECONDS);
+    //scheduler.wait(2000);
+    tick(2000);
+    assertThat(processedEntries).contains("B");
+  }
+
+  synchronized void tick(long ms) throws InterruptedException {
+    this.wait(ms);
   }
 
   @Test
-  public void testParallel() {
+  public void testParallel() throws Exception {
+    testPriorityLockedQueue.schedule(new ResourceTask<String>() {
+      @Override
+      public Set<String> resources() {
+        return Sets.newHashSet("C");
+      }
+    }, 0);
+    testPriorityLockedQueue.schedule(new ResourceTask<String>() {
+      @Override
+      public Set<String> resources() {
+        return Sets.newHashSet("D");
+      }
+    }, 10);
 
+    tick(2000);
+    assertThat(processedEntries).contains("C");
+    assertThat(processedEntries).contains("D");
   }
 
   @Test
-  public void testSameResource() {
+  public void testSameResource() throws Exception {
+    testPriorityLockedQueue.schedule(new ResourceTask<String>() {
+      @Override
+      public Set<String> resources() {
+        return Sets.newHashSet("E");
+      }
+    }, 0);
+    testPriorityLockedQueue.schedule(new ResourceTask<String>() {
+      @Override
+      public Set<String> resources() {
+        return Sets.newHashSet("E");
+      }
+    }, 500); // milliseconds!
 
+    // pass just as much time we'll be sure F has finished, but not enough,
+    // such that sequential scheduling would have worked
+    tick(1600);
+    assertThat(processedEntries).contains("E");
   }
 
   @Test
-  public void TestParallelWithManyResources() {
-
+  public void TestParallelWithManyResources() throws Exception {
+    testPriorityLockedQueue.schedule(new ResourceTask<String>() {
+      @Override
+      public Set<String> resources() {
+        return Sets.newHashSet("G", "H", "I");
+      }
+    }, 0);
+    testPriorityLockedQueue.schedule(new ResourceTask<String>() {
+      @Override
+      public Set<String> resources() {
+        return Sets.newHashSet("J", "K", "L");
+      }
+    }, 500); // milliseconds!
+    tick(1600);
+    assertThat(processedEntries).contains("G");
+    assertThat(processedEntries).contains("H");
+    assertThat(processedEntries).contains("I");
+    assertThat(processedEntries).contains("J");
+    assertThat(processedEntries).contains("K");
+    assertThat(processedEntries).contains("L");
   }
 
   @Test
-  public void testLoadedScheduleManyJobsOneResource() {
+  public void testLoadedScheduleManyJobsOneResource() throws Exception {
 
   }
 
-/*
-  Door door = context.mock(Door.class);
-  Alarm alarm = context.mock(Alarm.class);
-
-
-
-  TimeLock lock = new TimeLock(door, alarm);
-
-  @Test
-  public void locksAndUnlocksDoorOnRegularScheduleAndSoundsAlarmBeforeUnlockingDoor() {
-
-    // Configure the behaviour of the lock
-    lock.setOpenFor(1000);
-    lock.setClosedFor(2000);
-    lock.setSoundsAlarmBeforeClosingFor(100);
-
-    context.checking(new Expectations() {{
-        door.unlock();
-    }});
-    lock.activate();
-
-    context.checking(new Expectations() {{
-        alarm.start();
-    }});
-    scheduler.tick(900);
-
-    context.checking(new Expectations() {{
-        alarm.stop();
-        door.lock();
-    }});
-    scheduler.tick(100);
-
-    context.checking(new Expectations() {{
-        door.unlock();
-    }});
-    scheduler.tick(2000);
-  }*/
 
   class TestPriorityLockedQueue extends PriorityLockedQueue<String> {
+    TestPriorityLockedQueue(ScheduledExecutorService sched) {
+      super(sched);
+    }
     @Override
-    void process(ResourceTask<String> task) {
-      new Thread(group, target)
-      for (String s : task.resources()) {
-        Thread.sleep(100); // assume
-        processedEntries.add(s);
+    void process(final ResourceTask<String> task) {
+      if (noBlocking) {
+        processedEntries.addAll(task.resources());
+        processed(task);
+      } else {
+        // Assume any task given to us takes 1 second to process
+        scheduler.schedule(new Runnable() {
+          @Override
+          public void run() {
+            processedEntries.addAll(task.resources());
+            processed(task);
+          }
+        }, 100, TimeUnit.MILLISECONDS);
       }
     }
   }
 }
+

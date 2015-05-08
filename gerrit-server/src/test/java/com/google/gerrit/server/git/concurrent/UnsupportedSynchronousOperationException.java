6e3725714416fc708f043d17133a98ef66dfd685
Stefan Beller <sbeller@google.com>
2015-05-08 14:13:05 -0700

    Add a DeterministicScheduler class to the tests
    This adds a new class DeterministicScheduler and its internals to the
    tests of gerrit-server and is intended to be used for testing the rewrite
    of the MergeQueue.
    
    It is not written by me but originates from jmock.org
    As this copy includes additional features[1], which were unimplemented
    in the original, I added it here so we can use it in our tests until
    a new release jmock library includes said changes.
    
    [1] https://github.com/jmock-developers/jmock-library/pull/75
    
    Change-Id: Ib56286ed6af2a8a950613f4786356e3ec3b72876

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/git/concurrent/UnsupportedSynchronousOperationException.java
@@ -0,0 +1,14 @@
+package com.google.gerrit.server.git.concurrent;
+
+/**
+ * Thrown to report that a {@link DeterministicScheduler} has been asked to perform
+ * a blocking wait, which is not supported.
+ *
+ * @author nat
+ *
+ */
+public class UnsupportedSynchronousOperationException extends UnsupportedOperationException {
+    public UnsupportedSynchronousOperationException(String message) {
+        super(message);
+    }
+}

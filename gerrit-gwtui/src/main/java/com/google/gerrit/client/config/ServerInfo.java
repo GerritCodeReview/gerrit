6a518e0f3fcddc76ee9e5f3337bc118763954ccf
Edwin Kempin <edwin.kempin@sap.com>
2015-05-08 15:31:56 +0200

    Expose more config parameters via REST
    The /config/server/info REST endpoint now provides almost all
    configuration parameters which are needed by the Gerrit Client.
    
    GerritConfig still contains 3 values. 2 cannot easily be exposed via
    REST due to missing Guice bindings in the REST API layer (sshdAddress,
    documentationAvailable), 1 is not used on client-side (gitDaemonUrl).
    
    Futher cleanups will be done in follow-up changes.
    
    Change-Id: I660db7aa63253cb6a448079228301461dd1aa06f
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/config/ServerInfo.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/config/ServerInfo.java
@@ -18,10 +18,13 @@ import com.google.gwt.core.client.JavaScriptObject;
 
 public class ServerInfo extends JavaScriptObject {
   public final native AuthInfo auth() /*-{ return this.auth; }-*/;
+  public final native ChangeConfigInfo change() /*-{ return this.change; }-*/;
   public final native ContactStoreInfo contactStore() /*-{ return this.contact_store; }-*/;
   public final native DownloadInfo download() /*-{ return this.download; }-*/;
   public final native GerritInfo gerrit() /*-{ return this.gerrit; }-*/;
   public final native GitWebInfo gitWeb() /*-{ return this.git_web; }-*/;
+  public final native SuggestInfo suggest() /*-{ return this.suggest; }-*/;
+  public final native UserConfigInfo user() /*-{ return this.user; }-*/;
 
   public final boolean hasContactStore() {
     return contactStore() != null;
@@ -30,10 +33,35 @@ public class ServerInfo extends JavaScriptObject {
   protected ServerInfo() {
   }
 
+  public static class ChangeConfigInfo extends JavaScriptObject {
+    public final native boolean allowDrafts() /*-{ return this.allow_drafts || false; }-*/;
+    public final native int largeChange() /*-{ return this.large_change || 0; }-*/;
+    public final native String replyLabel() /*-{ return this.reply_label; }-*/;
+    public final native String replyTooltip() /*-{ return this.reply_tooltip; }-*/;
+    public final native int updateDelay() /*-{ return this.update_delay || 0; }-*/;
+
+    protected ChangeConfigInfo() {
+    }
+  }
+
   public static class ContactStoreInfo extends JavaScriptObject {
     public final native String url() /*-{ return this.url; }-*/;
 
     protected ContactStoreInfo() {
+    }
+  }
+
+  public static class SuggestInfo extends JavaScriptObject {
+    public final native int from() /*-{ return this.from || 0; }-*/;
+
+    protected SuggestInfo() {
+    }
+  }
+
+  public static class UserConfigInfo extends JavaScriptObject {
+    public final native String anonymousCowardName() /*-{ return this.anonymous_coward_name; }-*/;
+
+    protected UserConfigInfo() {
     }
   }
 }

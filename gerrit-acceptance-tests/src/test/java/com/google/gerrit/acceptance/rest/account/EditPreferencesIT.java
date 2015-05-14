4359f76ea310b6996a54347dd12309ca9ef62fab
David Ostrovsky <david@ostrovsky.org>
2015-05-15 00:28:40 +0200

    InlineEdit: Allow to activate auto-close brackets Codemirror addon
    Codemirror closebrackets.js addons defines an option `autoCloseBrackets`
    that will auto-close brackets and quotes when typed. By default, it will
    auto-close ()[]{}''"".
    
    Change-Id: I844d49c7d60d8fc78808df566e4c4f9aff1b012e

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/account/EditPreferencesIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/account/EditPreferencesIT.java
@@ -44,6 +44,7 @@ public class EditPreferencesIT extends AbstractDaemonTest {
     assertThat(out.syntaxHighlighting).isTrue();
     assertThat(out.hideLineNumbers).isNull();
     assertThat(out.matchBrackets).isNull();
+    assertThat(out.autoCloseBrackets).isNull();
     assertThat(out.theme).isEqualTo(Theme.DEFAULT);
     assertThat(out.keyMapType).isEqualTo(KeyMapType.DEFAULT);
 
@@ -57,6 +58,7 @@ public class EditPreferencesIT extends AbstractDaemonTest {
     out.syntaxHighlighting = false;
     out.hideLineNumbers = true;
     out.matchBrackets = true;
+    out.autoCloseBrackets = true;
     out.theme = Theme.TWILIGHT;
     out.keyMapType = KeyMapType.EMACS;
 
@@ -86,6 +88,7 @@ public class EditPreferencesIT extends AbstractDaemonTest {
     assertThat(out.syntaxHighlighting).isNull();
     assertThat(out.hideLineNumbers).isEqualTo(in.hideLineNumbers);
     assertThat(out.matchBrackets).isEqualTo(in.matchBrackets);
+    assertThat(out.autoCloseBrackets).isEqualTo(in.autoCloseBrackets);
     assertThat(out.theme).isEqualTo(in.theme);
     assertThat(out.keyMapType).isEqualTo(in.keyMapType);
   }

b2e6af46428a10a47023537274f6c9c19d990757
Edwin Kempin <edwin.kempin@sap.com>
2015-05-22 15:22:41 +0200

    Add extension point for the 'Included In' drop-down panel
    The new extension point allows plugins to add additional rows in the
    'Included In' drop-down panel on the change screen.
    
    E.g. if there is a fixed set of production servers a plugin can show
    on which servers this change was deployed. To provide this information
    the plugin could e.g. ask each production server which tag it runs and
    then include the name of this server if the change is included in this
    tag. Being able to immediately see on which production servers a
    change is running is very useful for users to know if a bug-fix has
    reached a certain server or which servers are effected by a bad
    change.
    
    Change-Id: I93ba27fe84e6830f485b5117afe9c951e8dc885a
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/config/ExternalIncludedIn.java
@@ -0,0 +1,41 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.extensions.config;
+
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+
+import java.util.Collection;
+import java.util.List;
+
+@ExtensionPoint
+public interface ExternalIncludedIn {
+
+  /**
+   * Returns a list of systems that include the given commit.
+   *
+   * The tags and branches in which the commit is included are provided so that
+   * a RevWalk can be avoided when a system runs a certain tag or branch.
+   *
+   * @param project the name of the project
+   * @param commit the ID of the commit for which it should be checked if it is
+   *        included
+   * @param tags the tags that include the commit
+   * @param branches the branches that include the commit
+   * @return a list of systems that contain the given commit, e.g. names of
+   *         servers on which this commit is deployed
+   */
+  List<String> getIncludedIn(String project, String commit,
+      Collection<String> tags, Collection<String> branches);
+}

25ad5ab9dd3b93f458beabd00e0642f324998506
Stefan Beller <sbeller@google.com>
2015-05-26 15:47:05 -0700

    ChangeMergeQueue: Rewrite such that it works on ChangeSets
    ChangeMergeQueue used to work on branches rather than sets of changes.
    This change is a first step to merge sets of changes (e.g. grouped by a
    topic and `changes.submitWholeTopic` enabled) in an atomic fashion.
    This change doesn't aim to implement these changes, but only as a step
    towards it.
    
    MergeOp keeps its functionality and behavior as is. A new class
    ChangeSetMergeOp is introduced which will map the set of changes to
    the set of branches. Additionally the ChangeSetMergeOp is also
    responsible for the threading done right now, which was part of
    the ChangeMergeQueue before. ChangeMergeQueue however gets a
    new responsibiltiy, it needs to make sure all changes to be
    merged, which requires locking the set of branches the set of
    changes is involved in. For convenience the set of changes are
    adressed via the new ChangeSet class.
    
    So here is an ASCII art diagram:
    
                      MultiLock   -- Can lock many resources atomically
                          ^          Tested as a selfcontained unit
                          |
     MergeQueue   +-> PriorityQueue  -- Schedules the resource locks over
     (interface)  |                     time, tested as a selfcontained unit
         ^        |
         |        |
     ChangeMergeQueue <-- delegates all its responsibilities by using
                  |       a PriorityQueue<ResourceTask, ChangeSet>
                  |
                  v
          ChangeSetMergeOp   <-- bundles the changes to a set,
                  |              loads off the work to a different
                  |              thread pool
                  v
               MergeOp  <-- the actual merging work,
                            it's nearly untouched.
    
    Change-Id: I1ffe09a505e25f15ce1521bcfb222e51e62c2a14

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/git/PriorityLockedQueueTest.java
@@ -0,0 +1,256 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.AtomicLongMap;
+import com.google.gerrit.common.MultiLock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class PriorityLockedQueueTest {
+
+  AtomicLongMap<String> processedEntries;
+  ScheduledExecutorService scheduler;
+  PriorityLockedQueueUnderTest testPriorityLockedQueue;
+
+  public PriorityLockedQueueTest() {
+
+  }
+
+  @Before
+  public void setup() {
+    scheduler = Executors.newSingleThreadScheduledExecutor();
+    testPriorityLockedQueue = new PriorityLockedQueueUnderTest(scheduler);
+    processedEntries = AtomicLongMap.create();
+  }
+
+  @Test
+  public void testOneProcessLockingDuringProcessing() throws Exception {
+    final TestingTask task1 = new TestingTask(Sets.newHashSet("A"), false, 0, 1);
+    new Thread() {
+      @Override
+      public void run() {
+        task1.awaitBefore();
+        // lock must be there while processing B
+        MultiLock<String> lock = testPriorityLockedQueue.locks();
+        assertThat(lock.lock(Sets.newHashSet("A"))).isFalse();
+        task1.countDownAfter();
+      }
+    }.start();
+
+    boolean result = testPriorityLockedQueue.process(task1);
+    assertThat(result).isTrue();
+    assertThat(processedEntries.get("A")).isEqualTo(1);
+    assertThat(testPriorityLockedQueue.locks.lock(Sets.newHashSet("A"))).isTrue();
+  }
+
+  @Test
+  public void testOneProcessLockingDuringScheduling() throws Exception {
+    final TestingTask task1 = new TestingTask(Sets.newHashSet("B"), false, 0, 1);
+
+    new Thread() {
+      @Override
+      public void run() {
+        task1.awaitBefore();
+        // lock must be there while processing B
+        MultiLock<String> locks = testPriorityLockedQueue.locks();
+        assertThat(locks.lock(Sets.newHashSet("B"))).isFalse();
+        task1.countDownAfter();
+      }
+    }.start();
+
+    testPriorityLockedQueue.latchAttempt = new CountDownLatch(1);
+    testPriorityLockedQueue.schedule(task1, 0);
+
+    // lock must be free before processing B
+    MultiLock<String> locks = testPriorityLockedQueue.locks();
+    assertThat(locks.lock(Sets.newHashSet("B"))).isTrue();
+    locks.unlock(Sets.newHashSet("B"));
+
+    testPriorityLockedQueue.latchAttempt.countDown();
+
+    task1.awaitAfter();
+
+    // lock must be free after processing B
+    assertThat(locks.lock(Sets.newHashSet("B"))).isTrue();
+    locks.unlock(Sets.newHashSet("B"));
+  }
+
+  @Test
+  public void testParallelScheduling() throws Exception {
+    TestingTask task1 = new TestingTask(Sets.newHashSet("C"), false, 0, 1);
+    TestingTask task2 = new TestingTask(Sets.newHashSet("D"), false, 0, 1);
+
+    testPriorityLockedQueue.schedule(task1, 1);
+    testPriorityLockedQueue.schedule(task2, 1);
+
+    task1.awaitBefore();
+    task2.awaitBefore();
+    // both are processing now at the same time
+    task1.countDownAfter();
+    task2.countDownAfter();
+    task1.awaitAfter();
+    task2.awaitAfter();
+    assertThat(processedEntries.get("C")).isEqualTo(1);
+    assertThat(processedEntries.get("D")).isEqualTo(1);
+  }
+
+
+  @Test
+  public void testResourceConflict() throws Exception {
+    TestingTask task1 = new TestingTask(Sets.newHashSet("E", "F"), false, 0, 1);
+    TestingTask task2 = new TestingTask(Sets.newHashSet("E", "G"), false, 0, 0);
+
+    testPriorityLockedQueue.schedule(task1, 1);
+    task1.awaitBefore();
+    assertThat(testPriorityLockedQueue.process(task2)).isFalse();
+    task1.countDownAfter();
+    task1.awaitAfter();
+    assertThat(testPriorityLockedQueue.process(task2)).isTrue();
+
+    assertThat(processedEntries.get("E")).isEqualTo(2);
+    assertThat(processedEntries.get("F")).isEqualTo(1);
+    assertThat(processedEntries.get("G")).isEqualTo(1);
+  }
+
+  class TestingTask implements PriorityLockedQueue.Task<String> {
+    final Set<String> resources;
+
+    CountDownLatch before;
+    CountDownLatch after;
+
+    boolean fail;
+
+    TestingTask(Set<String> resources, boolean shouldFailOnFirstTry,
+        int in, int out) {
+      this.resources = resources;
+      this.fail = shouldFailOnFirstTry;
+      this.before = new CountDownLatch(in + 1);
+      this.after = new CountDownLatch(out + 1);
+    }
+
+    boolean shouldFail() {
+      boolean ret = fail;
+      fail = false;
+      return ret;
+    }
+
+    void awaitBefore() {
+      try {
+        before.await();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
+    }
+
+    void countDownBefore() {
+      before.countDown();
+    }
+
+    void awaitAfter() {
+      try {
+        after.await();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
+    }
+
+    void countDownAfter() {
+      after.countDown();
+    }
+
+    @Override
+    public Set<String> resources() {
+      return resources;
+    }
+  }
+
+  /**
+   * For testing we just subclass the PriorityLockedQueue and a task
+   * always takes 100 milliseconds to process.
+   */
+  class PriorityLockedQueueUnderTest extends
+      PriorityLockedQueue<String, TestingTask> {
+
+    CountDownLatch latchAttempt = new CountDownLatch(0);
+
+    PriorityLockedQueueUnderTest(ScheduledExecutorService sched) {
+      super(sched, 250);
+    }
+
+    MultiLock<String> locks() {
+      return locks;
+    }
+
+    void processTestingTask(final TestingTask task) {
+      synchronized (processedEntries) {
+        for (String s : task.resources()) {
+          processedEntries.addAndGet(s, 1);
+        }
+      }
+    }
+
+    @Override
+    protected boolean tryRunTask(final TestingTask task, boolean async,
+        long timeout) {
+      try {
+        latchAttempt.await();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
+      return super.tryRunTask(task, async, timeout);
+    }
+
+    @Override
+    void processTask(final TestingTask task) {
+      task.countDownBefore();
+      task.awaitBefore();
+      processTestingTask(task);
+      task.countDownAfter();
+      task.awaitAfter();
+    }
+
+    @Override
+    void processAsyncTask(final TestingTask task) {
+      new Thread(new Runnable(){
+        @Override
+        public void run() {
+          task.countDownBefore();
+          task.awaitBefore();
+          if (!task.shouldFail()) {
+            processTestingTask(task);
+            processingAsyncTaskDone(task);
+          }
+          task.countDownAfter();
+          task.awaitAfter();
+          processingAsyncTaskDone(task);
+        }
+      }).start();
+    }
+  }
+}
+

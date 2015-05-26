df634ac28f02f7de9d7bc4b4c04b4d57b00b28d0
Stefan Beller <sbeller@google.com>
2015-05-26 15:47:05 -0700

    ChangeMergeQueue: Rewrite such that it works on ChangeSets
    ChangeMergeQueue used to work on branches rather than sets of changes.
    This change is a first step to merge sets of changes (e.g. grouped by a
    topic and `changes.submitWholeTopic` enabled) in an atomic fashion.
    This change doesn't aim to implement these changes, but only as a step
    towards it.
    
    MergeOp keeps its functionality and behavior as is. A new class
    ChangeSetMergeOp is introduced which will map the set of changes to
    the set of branches. Additionally the ChangeSetMergeOp is also
    responsible for the threading done right now, which was part of
    the ChangeMergeQueue before. ChangeMergeQueue however gets a
    new responsibiltiy, it needs to make sure all changes to be
    merged, which requires locking the set of branches the set of
    changes is involved in. For convenience the set of changes are
    adressed via the new ChangeSet class.
    
    So here is an ASCII art diagram:
    
                      MultiLock   -- Can lock many resources atomically
                          ^          Tested as a selfcontained unit
                          |
     MergeQueue   +-> PriorityQueue  -- Schedules the resource locks over
     (interface)  |                     time, tested as a selfcontained unit
         ^        |
         |        |
     ChangeMergeQueue <-- delegates all its responsibilities by using
                  |       a PriorityQueue<ResourceTask, ChangeSet>
                  |
                  v
          ChangeSetMergeOp   <-- bundles the changes to a set,
                  |              loads off the work to a different
                  |              thread pool
                  v
               MergeOp  <-- the actual merging work,
                            it's nearly untouched.
    
    Change-Id: I1ffe09a505e25f15ce1521bcfb222e51e62c2a14

--- gerrit-server/src/main/java/com/google/gerrit/server/change/Submit.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Submit.java
@@ -53,6 +53,7 @@ import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.ProjectUtil;
 import com.google.gerrit.server.account.AccountsCollection;
 import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.git.ChangeSet;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.LabelNormalizer;
 import com.google.gerrit.server.git.MergeQueue;
@@ -83,8 +84,10 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Singleton
 public class Submit implements RestModifyView<RevisionResource, SubmitInput>,
@@ -210,19 +213,13 @@ public class Submit implements RestModifyView<RevisionResource, SubmitInput>,
           rsrc.getPatchSet().getRevision().get()));
     }
 
-    List<Change> submittedChanges = submit(rsrc, caller, false);
+    ChangeSet submittedChanges = ChangeSet.create(submit(rsrc, caller, false));
 
     if (input.waitForMerge) {
-      for (Change c : submittedChanges) {
-        // TODO(sbeller): We should make schedule return a Future, then we
-        // could do these all in parallel and still block until they're done.
-        mergeQueue.merge(c.getDest());
-      }
+      mergeQueue.merge(submittedChanges);
       change = dbProvider.get().changes().get(change.getId());
     } else {
-      for (Change c : submittedChanges) {
-        mergeQueue.schedule(c.getDest());
-      }
+      mergeQueue.schedule(submittedChanges);
     }
 
     if (change == null) {
---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/PriorityLockedQueue.java
@@ -0,0 +1,246 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.common.collect.Sets;
+import com.google.gerrit.common.MultiLock;
+
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This class schedules tasks requiring resources such that no tasks requiring
+ * the same resource are scheduled at the same time.
+ * <p>
+ * There are two ways of scheduling a task. The first way {@code process} is
+ * synchronous and either proceeds or fails to process the task. The second way
+ * {@code schedule} is asynchronous and will process the task eventually when
+ * resource constraints are met.
+ *
+ * @param <R> The resource type to be required for the scheduled tasks.
+ * @param <T> The task type to be scheduled.
+ */
+public abstract class PriorityLockedQueue<R, T extends PriorityLockedQueue.Task<R>> {
+  public interface Task<R> {
+    /** @return the resources required to be locked to perform this task. */
+    Set<R> resources();
+  }
+
+  Set<T> currentTasks;
+
+  // Contains locks on all resources we are currently holding
+  protected MultiLock<R> locks;
+
+  // all future tasks
+  private PriorityQueue<TimedResourceTask> queue;
+
+  private ScheduledExecutorService exec;
+
+  // keeps track of the next wake up time
+  private ScheduledFuture<?> nextWakeUp;
+
+  private long timeout;
+
+  PriorityLockedQueue(ScheduledExecutorService sched, long timeout) {
+    this.locks = new MultiLock<>();
+    this.queue = new PriorityQueue<>(1);
+    this.exec = sched;
+    this.timeout = timeout;
+    this.currentTasks = Sets.newHashSet();
+  }
+
+  /**
+   * Checks if a task can be processed right now and if it can, it will do so.
+   * If it cannot be processed right now, no further action will be taken and
+   * false is returned.
+   *
+   * @param task the task be be run
+   * @return if the given item will be processed right now.
+   */
+  public boolean process(T task) {
+    boolean ret = tryRunTask(task, false, 0);
+    // During processing some other scheduled task may have become ready
+    // but blocked so check if we can run them now
+    checkQueue();
+
+    return ret;
+  }
+
+  /**
+   * This will schedule a task to be processed in the future.
+   * <p>
+   * The {@code minimumDelay} which is measured in milliseconds determines the
+   * minimum time it waits until a scheduling is attempted. If the resource
+   * constraints cannot be met at the time of the scheduling attempt, the attempt
+   * will be further delaying using an exponential back off scheme, doubling the
+   * {@code minimumDelay} with each failed attempt.
+   *
+   * @param rtask the task be be run
+   * @param minimumDelay the minimum delay in milliseconds when this task should be
+   *        run.
+   */
+  public synchronized void schedule(T rtask, final long minimumDelay) {
+    queue.add(new TimedResourceTask(rtask, minimumDelay));
+    scheduleQueueCheck(minimumDelay);
+  }
+
+  /**
+   * Tries processing a task.
+   * <p>
+   * @param timeout the timeout in milliseconds for the resources to be reserved
+   *        if run asynchronously.
+   * @returns {@code true} if the task was processed when running synchronously
+   *          or if it was started when running asynchronously.
+   *          {@code false} otherwise.
+   */
+  private boolean tryRunTask(final T task, boolean async, long timeout) {
+    if (locks.lock(task.resources())) {
+      currentTasks.add(task);
+      try {
+        if (!async) {
+          processTask(task);
+        } else {
+          processAsyncTask(task);
+        }
+      } finally {
+        if (!async) {
+          currentTasks.remove(task);
+          locks.unlock(task.resources());
+        } else {
+          exec.schedule(new Runnable() {
+            @Override
+            public void run() {
+              processingAsyncTaskDone(task);
+            }
+          }, timeout, TimeUnit.MILLISECONDS);
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Schedules a check for a queue after {@code delay} milliseconds.
+   * <p>
+   * if there is already a check scheduled the earlier check is kept, while the
+   * later check is dropped.
+   *
+   * @param delay The minimum amount of time in milliseconds after which the
+   *              next wake up call will check the queue.
+   */
+  private synchronized void scheduleQueueCheck(final long delay) {
+    Runnable wakeUpAction = new Runnable() {
+      @Override
+      public void run() {
+        nextWakeUp = null;
+        for (TimedResourceTask t : queue) {
+          t.decreaseDelay(delay);
+        }
+        checkQueue();
+      }
+    };
+    if (nextWakeUp == null) {
+      nextWakeUp = exec.schedule(wakeUpAction, delay, TimeUnit.MILLISECONDS);
+    } else if (delay < nextWakeUp.getDelay(TimeUnit.MILLISECONDS)) {
+      nextWakeUp.cancel(false);
+      nextWakeUp = exec.schedule(wakeUpAction, delay, TimeUnit.MILLISECONDS);
+    }
+  }
+
+  private synchronized void checkQueue() {
+    TimedResourceTask rtask = queue.peek();
+    while (rtask != null && rtask.delay() <= 0) {
+      queue.remove();
+      if (!tryRunTask(rtask.task, true, timeout)) {
+        rtask.calculateNewDelay();
+        scheduleQueueCheck(rtask.delay());
+      }
+      rtask = queue.peek();
+    }
+
+    TimedResourceTask nextTask = queue.peek();
+    if (nextTask != null) {
+      scheduleQueueCheck(nextTask.delay());
+    }
+  }
+
+  /**
+   * The actual work will be done inside this function.
+   *
+   * @param task the task to be performed
+   */
+  abstract void processTask(T task);
+
+  /**
+   * The actual work will be done inside this function. It is assumed the work
+   * will be split out to another thread, and this may return early fast.
+   *
+   * If the task is done and its resources don't require a lock anymore,
+   * a call to {@code processingAsyncTaskDone} should be made. If this call
+   * is not made, it will be called internally after the timeout as specified in
+   *
+   * @param task the task to be performed
+   */
+  abstract void processAsyncTask(T task);
+
+  public synchronized void processingAsyncTaskDone(T task) {
+    if (currentTasks.contains(task)) {
+      currentTasks.remove(task);
+      locks.unlock(task.resources());
+      // TODO(sbeller): As this method is called twice (once as a timeout and
+      // once supposedly by the finished worker. Maybe there is an efficient
+      // way to cancel the later call from the first call.
+    }
+  }
+
+  private class TimedResourceTask implements PriorityLockedQueue.Task<R>, Comparable<TimedResourceTask> {
+    private T task;
+    private long delay;
+    private long backoff;
+
+    public TimedResourceTask(T task, long delay) {
+      this.task = task;
+      this.delay = delay;
+      this.backoff = delay;
+    }
+
+    @Override
+    public Set<R> resources() {
+      return task.resources();
+    }
+
+    void calculateNewDelay() {
+      delay = 2 * backoff;
+      backoff = delay;
+    }
+
+    void decreaseDelay(long decreaseBy) {
+      delay -= decreaseBy;
+    }
+
+    long delay() {
+      return delay;
+    }
+
+    @Override
+    public int compareTo(TimedResourceTask o) {
+      return Long.compare(delay(), o.delay());
+    }
+  }
+}

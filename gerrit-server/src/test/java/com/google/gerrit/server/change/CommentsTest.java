1497e888d14182c66ba23831a26683bae49495bb
Edwin Kempin <edwin.kempin@sap.com>
2015-05-27 13:39:45 +0200

    Fix outdated draft comment icons in 'Patch Sets' drop-down panel
    When the 'Patch Sets' drop-down panel is opened on the change screen
    it gets the change detail with the DRAFT_COMMENTS option. This allows
    to decorate the patch sets that have draft comments with a draft
    comment icon. The change detail is cached in the browser so that it is
    only reloaded when the change is modified on the server.
    
    If a user adds/removes draft comments on any patch set, and there was
    no other modification of the change, then the change detail is not
    reloaded and as result the draft comment icons shown in the 'Patch
    Sets' drop-down panel may be outdated. The draft comment icon may be
    shown for patch sets that have no draft comments, and it may be
    missing for patch sets that have draft comments. This turns these
    icons rather useless.
    
    To fix this the change detail should be reloaded if there was any
    change to the draft comment status of a patch set. At the moment the
    reload doesn't happen because the ETag of the change doesn't inlcude
    the draft comment status of the patch sets.
    
    With this change the ETag computation of a change includes the draft
    comment status of the patch sets, so that draft comment icons in the
    'Patch Sets' drop-down panel are always shown correctly.
    
    In order to include the draft comment status into the ETag computation
    the ChangeResource must have access to ChangeData. This means
    ChangeData is already loaded when the change is parsed. On the other
    hand ChangeJson can now use the ChangeData from ChangeResource and
    doesn't need to do the loading anymore.
    
    Change-Id: Id78bbb869cc6a7c310a6019d6adc8e79819c92f4
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-server/src/test/java/com/google/gerrit/server/change/CommentsTest.java
+++ gerrit-server/src/test/java/com/google/gerrit/server/change/CommentsTest.java
@@ -318,9 +318,9 @@ public class CommentsTest  {
     update.commit();
 
     ChangeControl ctl = stubChangeControl(change1);
-    revRes1 = new RevisionResource(new ChangeResource(ctl, null), ps1);
-    revRes2 = new RevisionResource(new ChangeResource(ctl, null), ps2);
-    revRes3 = new RevisionResource(new ChangeResource(stubChangeControl(change2), null), ps3);
+    revRes1 = new RevisionResource(new ChangeResource(ctl, null, null), ps1);
+    revRes2 = new RevisionResource(new ChangeResource(ctl, null, null), ps2);
+    revRes3 = new RevisionResource(new ChangeResource(stubChangeControl(change2), null, null), ps3);
   }
 
   private ChangeControl stubChangeControl(Change c) throws OrmException {

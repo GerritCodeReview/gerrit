4512829c4122f7cc5a4f52da044163580be2b277
Dave Borowitz <dborowitz@google.com>
2015-06-04 12:15:45 -0700

    GetRelated: Special case zero related changes earlier
    If the only result that comes back from the search is the change we're
    looking at, we aren't going to render it in the UI, so don't even
    bother opening the repo to populate the commit info, much less sort
    it.
    
    Change-Id: Iad7b06faae43fa615025a673594c25418dfac6b9

--- gerrit-server/src/main/java/com/google/gerrit/server/change/GetRelated.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/GetRelated.java
@@ -99,6 +99,12 @@ public class GetRelated implements RestReadView<RevisionResource> {
         .byProjectGroups(
             rsrc.getChange().getProject(),
             getAllGroups(rsrc.getChange().getId()));
+    if (cds.isEmpty()) {
+      return Collections.emptyList();
+    } if (cds.size() == 1
+        && cds.get(0).getId().equals(rsrc.getChange().getId())) {
+      return Collections.emptyList();
+    }
     List<ChangeAndCommit> result = new ArrayList<>(cds.size());
 
     PatchSet.Id editBaseId = rsrc.getEdit().isPresent()

2b7659ba9d65c8568266ce387768bc252d8ca8f0
Khai Do <zaro0508@gmail.com>
2015-06-04 14:04:27 -0700

    Append approvals information to every review stream event
    Stream events only provides label information on review events
    that contain a vote transition.  When a user replies with only
    a message the approvals information is not added to the stream event.
    This change will make Gerrit append approvals info to every review
    event including the ones that only contains a reply message.
    More info can be found in the related RFC discussion[1].
    
    [1] https://groups.google.com/d/msg/repo-discuss/soqmGjRpl-4/IIfAF4jbCQAJ
    
    Bug: Issue 3220
    Change-Id: I79791f8deee08273038c99daa31fa72b1e3940e0

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/server/project/CustomLabelIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/server/project/CustomLabelIT.java
@@ -19,25 +19,43 @@ import static com.google.gerrit.server.group.SystemGroupBackend.ANONYMOUS_USERS;
 import static com.google.gerrit.server.project.Util.category;
 import static com.google.gerrit.server.project.Util.value;
 
+import static org.junit.Assert.fail;
+
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.common.UserScopedEventListener;
 import com.google.gerrit.common.data.LabelType;
 import com.google.gerrit.common.data.Permission;
 import com.google.gerrit.extensions.api.changes.AddReviewerInput;
 import com.google.gerrit.extensions.api.changes.ReviewInput;
 import com.google.gerrit.extensions.common.ChangeInfo;
 import com.google.gerrit.extensions.common.LabelInfo;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.registration.RegistrationHandle;
 import com.google.gerrit.reviewdb.client.AccountGroup;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.data.ApprovalAttribute;
+import com.google.gerrit.server.events.CommentAddedEvent;
+import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.git.ProjectConfig;
 import com.google.gerrit.server.group.SystemGroupBackend;
 import com.google.gerrit.server.project.Util;
+import com.google.inject.Inject;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 @NoHttpd
 public class CustomLabelIT extends AbstractDaemonTest {
+
+  @Inject
+  private IdentifiedUser.GenericFactory factory;
+
+  @Inject
+  private DynamicSet<UserScopedEventListener> source;
 
   private final LabelType label = category("CustomLabel",
       value(1, "Positive"),
@@ -47,6 +65,9 @@ public class CustomLabelIT extends AbstractDaemonTest {
   private final LabelType P = category("CustomLabel2",
       value(1, "Positive"),
       value(0, "No score"));
+
+  private CommentAddedEvent lastCommentAddedEvent;
+  private RegistrationHandle eventListenerRegistration;
 
   @Before
   public void setUp() throws Exception {
@@ -58,6 +79,26 @@ public class CustomLabelIT extends AbstractDaemonTest {
     Util.allow(cfg, Permission.forLabel(P.getName()), 0, 1, anonymousUsers,
         "refs/heads/*");
     saveProjectConfig(project, cfg);
+
+    eventListenerRegistration = source.add(new UserScopedEventListener() {
+      @Override
+      public void onEvent(Event event) {
+        if (event instanceof CommentAddedEvent) {
+          lastCommentAddedEvent = (CommentAddedEvent) event;
+        }
+      }
+
+      @Override
+      public CurrentUser getUser() {
+        return factory.create(user.id);
+      }
+    });
+  }
+
+  @After
+  public void cleanup() {
+    eventListenerRegistration.remove();
+    db.close();
   }
 
   @Test
@@ -124,13 +165,56 @@ public class CustomLabelIT extends AbstractDaemonTest {
         .id(r.getChangeId())
         .addReviewer(in);
 
-    revision(r).review(new ReviewInput().label(P.getName(), 0));
+    ReviewInput input = new ReviewInput().label(P.getName(), 0);
+    input.message = "foo";
+
+    revision(r).review(input);
     ChangeInfo c = get(r.getChangeId());
     LabelInfo q = c.labels.get(P.getName());
     assertThat(q.all).hasSize(2);
     assertThat(q.disliked).isNull();
     assertThat(q.rejected).isNull();
     assertThat(q.blocking).isNull();
+    assertThat(lastCommentAddedEvent.comment).isEqualTo(
+        "Patch Set 1:\n\n" + input.message);
+  }
+
+  @Test
+  public void customLabel_MultipleVotes() throws Exception {
+    saveLabelConfig();
+    PushOneCommit.Result r = createChange();
+    ReviewInput vote = new ReviewInput().label(label.getName(), -1);
+    vote.message = label.getName();
+    revision(r).review(vote);
+
+    ChangeInfo c = get(r.getChangeId());
+    LabelInfo q = c.labels.get(label.getName());
+    assertThat(q.all).hasSize(1);
+    assertThat(lastCommentAddedEvent.comment).isEqualTo(
+        String.format("Patch Set 1: %s-1\n\n%s",
+            label.getName(), label.getName()));
+
+    vote = new ReviewInput().label(P.getName(), 1);
+    vote.message = P.getName();
+    revision(r).review(vote);
+
+    c = get(r.getChangeId());
+    q = c.labels.get(label.getName());
+    assertThat(q.all).hasSize(1);
+    assertThat(lastCommentAddedEvent.comment).isEqualTo(
+        String.format("Patch Set 1: %s+1\n\n%s",
+            P.getName(), P.getName()));
+
+    assertThat(lastCommentAddedEvent.approvals.get()).hasLength(2);
+    for (ApprovalAttribute approval : lastCommentAddedEvent.approvals.get()) {
+      if (approval.type.equals(label.getName())) {
+        assertThat(approval.value).isEqualTo("-1");
+      } else if (approval.type.equals(P.getName())) {
+        assertThat(approval.value).isEqualTo("1");
+      } else {
+        fail("Unexpected label: " + approval.type);
+      }
+    }
   }
 
   @Test

0dc10f6f67a1fa7379dec6f05a45e7a9d8540f87
David Ostrovsky <david@ostrovsky.org>
2015-06-15 07:59:30 +0200

    Reapply: Revert "Use changeRefsById to track existing revisions"
    This Revert was lost when merging stable-2.11 into master.
    
    This change reverts the commit that broke the %base option [1] which
    allows to push a commit, which is already merged into a branch, for
    review to another branch of the same project (same SHA-1).
    
    Since 2.11 it is also new and shiny option on the project config page
    "Create a new change for every commit not in the target branch" which
    makes use of the %base parameter internally. This option is broken as
    well, obviously.
    
    New unit test is added that verifies this functionality and is failing
    without this change, so that such regressions can be avoided in future.
    
    This reverts commit 2ffd2cb83f22726fa7274bea54ae065f64064837.
    
    [1] https://gerrit-review.googlesource.com/Documentation/user-upload.html#base
    
    Bug: Issue 3426
    Change-Id: I9f66fc0977679be6153b58188c3c29716e97eb98

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -1503,8 +1503,8 @@ public class ReceiveCommits {
   private void selectNewAndReplacedChangesFromMagicBranch() {
     newChanges = Lists.newArrayList();
 
-    SetMultimap<ObjectId, Ref> existing = changeRefsById();
-    GroupCollector groupCollector = new GroupCollector(refsById, db);
+    SetMultimap<ObjectId, Ref> existing = HashMultimap.create();
+    GroupCollector groupCollector = new GroupCollector(changeRefsById(), db);
 
     rp.getRevWalk().reset();
     rp.getRevWalk().sort(RevSort.TOPO);
@@ -1524,6 +1524,7 @@ public class ReceiveCommits {
       } else {
         markHeadsAsUninteresting(
             rp.getRevWalk(),
+            existing,
             magicBranch.ctl != null ? magicBranch.ctl.getRefName() : null);
       }
 
@@ -1536,7 +1537,6 @@ public class ReceiveCommits {
         if (c == null) {
           break;
         }
-        groupCollector.visit(c);
         Collection<Ref> existingRefs = existing.get(c);
         if (!existingRefs.isEmpty()) { // Commit is already tracked.
           // Corner cases where an existing commit might need a new group:
@@ -1681,15 +1681,23 @@ public class ReceiveCommits {
     }
   }
 
-  private void markHeadsAsUninteresting(RevWalk rw, @Nullable String forRef) {
+  private void markHeadsAsUninteresting(
+      final RevWalk walk,
+      SetMultimap<ObjectId, Ref> existing,
+      @Nullable String forRef) {
     for (Ref ref : allRefs.values()) {
-      if ((ref.getName().startsWith(R_HEADS) || ref.getName().equals(forRef))
-          && ref.getObjectId() != null) {
+      if (ref.getObjectId() == null) {
+        continue;
+      } else if (ref.getName().startsWith(REFS_CHANGES)) {
+        existing.put(ref.getObjectId(), ref);
+      } else if (ref.getName().startsWith(R_HEADS)
+          || (forRef != null && forRef.equals(ref.getName()))) {
         try {
-          rw.markUninteresting(rw.parseCommit(ref.getObjectId()));
+          walk.markUninteresting(walk.parseCommit(ref.getObjectId()));
         } catch (IOException e) {
           log.warn(String.format("Invalid ref %s in %s",
               ref.getName(), project.getName()), e);
+          continue;
         }
       }
     }
@@ -2567,11 +2575,11 @@ public class ReceiveCommits {
       if (!(parsedObject instanceof RevCommit)) {
         return;
       }
+      SetMultimap<ObjectId, Ref> existing = HashMultimap.create();
       walk.markStart((RevCommit)parsedObject);
-      markHeadsAsUninteresting(walk, cmd.getRefName());
-      Set<ObjectId> existing = changeRefsById().keySet();
+      markHeadsAsUninteresting(walk, existing, cmd.getRefName());
       for (RevCommit c; (c = walk.next()) != null;) {
-        if (existing.contains(c)) {
+        if (existing.keySet().contains(c)) {
           continue;
         } else if (!validCommit(ctl, cmd, c)) {
           break;

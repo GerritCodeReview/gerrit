94e7263bc44a10adef4dfcbfd47acea21b937166
David Ostrovsky <david@ostrovsky.org>
2015-06-17 00:25:59 +0200

    Buck: Allow to trigger Maven deployment even when nothing changed
    Buck extensively uses caching and storing metadata in buck-out
    directory, so that it's not possible per design to re-trigger the
    execution of custom rule, without weeping out the whole buck-out
    directory. See also the discussion on this issue: [1].
    
    The correct solution to this problem is not to deploy to Maven repo
    as a side effect of (cached) custom python_binary() rule, but use
    built-in Buck rule. Such a rule doesn't exist yet.
    
    As workaround for now add standalone Python script, that re-uses Buck
    api_{deploy|install} targets, resolves $(location <target>) macros and
    executes the deployment manually by calling mvn.py utility directly:
    
      $ tools/maven/api.py {deploy,install}
    
    [1] https://github.com/facebook/buck/issues/342
    
    Change-Id: I7fb86ad6967a1fa1e7ac842ba5e0e8cf0103b773

---
+++ tools/maven/api.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# Copyright (C) 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+from argparse import ArgumentParser
+from os import environ, path, remove
+from subprocess import check_output
+from sys import stderr
+import json
+import tempfile
+
+LOCATION = {
+  '//gerrit-extension-api:extension-api':'gerrit-extension-api/extension-api.jar',
+  '//gerrit-plugin-gwtui:gwtui-api':'gerrit-plugin-gwtui/gwtui-api.jar',
+  '//gerrit-plugin-api:plugin-api':'gerrit-plugin-api/plugin-api.jar',
+  '//gerrit-extension-api:extension-api-src':'gerrit-extension-api/lib__extension-api-src__output/extension-api-src.jar',
+  '//gerrit-plugin-gwtui:gwtui-api-src':'gerrit-plugin-gwtui/gwtui-api-src.jar',
+  '//gerrit-plugin-api:plugin-api-src':'gerrit-plugin-api/plugin-api-src.jar',
+  '//gerrit-extension-api:extension-api-javadoc':'gerrit-extension-api/extension-api-javadoc.jar',
+  '//gerrit-plugin-gwtui:gwtui-api-javadoc':'gerrit-plugin-gwtui/gwtui-api-javadoc.jar',
+  '//gerrit-plugin-api:plugin-api-javadoc':'gerrit-plugin-api/plugin-api-javadoc.jar',
+}
+
+parser = ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true')
+
+subparsers = parser.add_subparsers(help='action', dest='action')
+subparsers.add_parser('deploy', help='Deploy to Maven (remote)')
+subparsers.add_parser('install', help='Install to Maven (local)')
+
+args = parser.parse_args()
+
+root = path.abspath(__file__)
+while not path.exists(path.join(root, '.buckconfig')):
+  root = path.dirname(root)
+
+check_output(['buck', 'build', 'api'])
+
+target = check_output(['buck', 'targets', '--json', 'api_%s' % args.action])
+json = json.loads(target)
+
+s = json[0][u'cmd']
+
+fd, tempfile = tempfile.mkstemp()
+s = s.replace('$(exe //tools/maven:mvn)', path.join(root, 'tools/maven/mvn.py'))
+s = s.replace('-o $OUT', '-o %s' % tempfile)
+
+while '$(location' in s:
+  start = s.index('$(location')
+  end = s.index(')', start)
+  target = s[start+11:end]
+  s = s.replace(s[start:end+1], 'buck-out/gen/%s' % LOCATION[target])
+
+try:
+  if args.verbose or environ.get('VERBOSE'):
+    print(s, file=stderr)
+  check_output(s.split())
+finally:
+  remove(tempfile)

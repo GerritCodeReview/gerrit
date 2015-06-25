8e82c76df4c02c8618a0ffb841a191872fa00c91
Hugo Arès <hugo.ares@ericsson.com>
2015-06-25 15:10:19 -0400

    Print proper name for project indexer tasks in show-queue command
    Change-Id: I282a8fc7eafa741f44be6ccbbcb078ff46e2187d

--- gerrit-server/src/main/java/com/google/gerrit/server/index/SiteIndexer.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/index/SiteIndexer.java
@@ -269,6 +269,12 @@ public class SiteIndexer {
         }
         return null;
       }
+
+      @Override
+      public String toString() {
+        return "Index all changes of project " + project.get();
+      }
+
     };
   }
 

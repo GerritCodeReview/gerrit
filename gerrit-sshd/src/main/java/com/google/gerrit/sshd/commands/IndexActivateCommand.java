e5ef1e3e016c4b5f41a712fb3799c844f8239d98
Hugo Arès <hugo.ares@ericsson.com>
2015-06-30 10:36:08 -0400

    Add ssh command to activate the latest index
    In case the online indexer fails, this allows the admins to activate
    the latest index.
    
    Change-Id: I59ebf2bce02a599b87d0f7771fc806ff64a3ba64

---
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/IndexActivateCommand.java
@@ -0,0 +1,49 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd.commands;
+
+import static com.google.gerrit.sshd.CommandMetaData.Mode.MASTER;
+
+import com.google.gerrit.common.data.GlobalCapability;
+import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gerrit.lucene.LuceneVersionManager;
+import com.google.gerrit.lucene.ReindexerAlreadyRunningException;
+import com.google.gerrit.sshd.CommandMetaData;
+import com.google.gerrit.sshd.SshCommand;
+import com.google.inject.Inject;
+
+@RequiresCapability(GlobalCapability.ADMINISTRATE_SERVER)
+@CommandMetaData(name = "activate",
+  description = "Activate the latest index version available",
+  runsAt = MASTER)
+public class IndexActivateCommand extends SshCommand {
+
+  @Inject
+  private LuceneVersionManager luceneVersionManager;
+
+  @Override
+  protected void run() throws UnloggedFailure {
+    try {
+      if (luceneVersionManager.activateLatestIndex()) {
+        stdout.println("Activated latest index version");
+      } else {
+        stdout.println("Not activating index, already using latest version");
+      }
+    } catch (ReindexerAlreadyRunningException e) {
+      throw new UnloggedFailure("Failed to activate latest index: "
+          + e.getMessage());
+    }
+  }
+}

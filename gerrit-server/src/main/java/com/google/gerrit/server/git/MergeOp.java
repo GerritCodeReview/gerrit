12bf503aa2e7fb175be6509af307b9769ad5d4fa
Stefan Beller <sbeller@google.com>
2015-07-07 17:08:49 -0700

    Remove the state SUBMITTED altogether
    There is a new schema to move all submitted changes to new.
    
    Change-Id: I9f3b015a59fc2446a98a03866330b0d4e22bbf3d

--- gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/MergeOp.java
@@ -393,8 +393,7 @@ public class MergeOp {
       throws ResourceConflictException, OrmException {
     for (Change.Id id : cs.ids()) {
       ChangeData cd = changeDataFactory.create(db, id);
-      if (cd.change().getStatus() != Change.Status.NEW
-          && cd.change().getStatus() != Change.Status.SUBMITTED){
+      if (cd.change().getStatus() != Change.Status.NEW){
         throw new OrmException("Change " + cd.change().getChangeId()
             + " is in state " + cd.change().getStatus());
       } else {
@@ -424,8 +423,6 @@ public class MergeOp {
           break;
         case MERGED:
           // we're racing here, but having it already merged is fine.
-        case SUBMITTED:
-          // ok
       }
     }
   }
@@ -692,9 +689,8 @@ public class MergeOp {
         throw new MergeException("Failed to validate changes", e);
       }
       Change.Id changeId = cd.getId();
-      if (chg.getStatus() != Change.Status.SUBMITTED
-          && chg.getStatus() != Change.Status.NEW) {
-        logDebug("Change {} is not new or submitted: {}", changeId, chg.getStatus());
+      if (chg.getStatus() != Change.Status.NEW) {
+        logDebug("Change {} is not new: {}", changeId, chg.getStatus());
         continue;
       }
       if (chg.currentPatchSetId() == null) {

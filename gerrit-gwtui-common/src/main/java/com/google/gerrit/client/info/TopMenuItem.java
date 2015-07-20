7fbc955187404328cbf0ce45a8f9bcd9dadc9a61
Edwin Kempin <edwin.kempin@sap.com>
2015-07-20 13:27:40 +0200

    Make account preferences info class accessible by plugins
    Move the Preferences class to gerrit-gwtui-common so that it can be
    used by plugins. Also rename the class to AccountPreferencesInfo.
    
    As dependencies of this class also TopMenu, TopMenuItem and
    TopMenuList are moved.
    
    Change-Id: I6a7589097da0e1e6f589cf20518ea8fe2ef722bc
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- gerrit-gwtui-common/src/main/java/com/google/gerrit/client/info/TopMenuItem.java
+++ gerrit-gwtui-common/src/main/java/com/google/gerrit/client/info/TopMenuItem.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.client.extensions;
+package com.google.gerrit.client.info;
 
 import com.google.gwt.core.client.JavaScriptObject;
 

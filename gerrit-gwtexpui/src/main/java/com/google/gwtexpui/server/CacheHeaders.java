9f1c458caa619dc68c56dd0d145db289b130de0f
Shawn Pearce <sop@google.com>
2015-08-14 17:06:31 -0700

    Fix expires date in CacheHeaders
    Jan 1 1990 was a Monday, not a Friday.
    
    Change-Id: I16ae708130d4d21e68cf8fad216068000f703a38

--- gerrit-gwtexpui/src/main/java/com/google/gwtexpui/server/CacheHeaders.java
+++ gerrit-gwtexpui/src/main/java/com/google/gwtexpui/server/CacheHeaders.java
@@ -35,7 +35,7 @@ public class CacheHeaders {
     String cc = "no-cache, no-store, max-age=0, must-revalidate";
     res.setHeader("Cache-Control", cc);
     res.setHeader("Pragma", "no-cache");
-    res.setHeader("Expires", "Fri, 01 Jan 1990 00:00:00 GMT");
+    res.setHeader("Expires", "Mon, 01 Jan 1990 00:00:00 GMT");
     res.setDateHeader("Date", System.currentTimeMillis());
   }
 

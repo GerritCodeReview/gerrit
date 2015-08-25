2746b8645c078626428fceae011b0f9a101f3ff0
Doug Kelly <doug.kelly@garmin.com>
2015-08-25 11:02:04 -0500

    Add notification when adding keys
    When adding SSH keys, add a notification to the user to inform them
    as an additional protection for their account should any credentials
    be compromised.
    
    Change-Id: Ia448af8da33dc0be3ba1acbb354ff3628630fe09

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/AddSshKeySender.java
@@ -0,0 +1,84 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.mail;
+
+import com.google.gerrit.common.errors.EmailException;
+import com.google.gerrit.reviewdb.client.AccountSshKey;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+
+public class AddSshKeySender extends OutgoingEmail {
+  public interface Factory {
+    public AddSshKeySender create(IdentifiedUser user, AccountSshKey sshKey);
+  }
+
+  private final IdentifiedUser callingUser;
+  private final IdentifiedUser user;
+  private final AccountSshKey sshKey;
+
+  @Inject
+  public AddSshKeySender(EmailArguments ea,
+      IdentifiedUser callingUser,
+      @Assisted final IdentifiedUser user,
+      @Assisted final AccountSshKey sshKey) {
+    super(ea, "addsshkey");
+    this.callingUser = callingUser;
+    this.user = user;
+    this.sshKey = sshKey;
+  }
+
+  @Override
+  protected void init() throws EmailException {
+    super.init();
+    setHeader("Subject", "[Gerrit Code Review] SSH Key Added");
+    add(RecipientType.TO, new Address(user.getAccount().getPreferredEmail()));
+  }
+
+  @Override
+  protected boolean shouldSendMessage() {
+    // Don't send an email if an admin is adding a key to a user.
+    return !(!user.equals(callingUser) &&
+        callingUser.getCapabilities().canAdministrateServer());
+  }
+
+  @Override
+  protected void format() throws EmailException {
+    appendText(velocifyFile("AddSshKey.vm"));
+  }
+
+  public String getUserNameEmail() {
+    String name = user.getAccount().getFullName();
+    String email = user.getAccount().getPreferredEmail();
+
+    if (name != null && email != null) {
+      return name + " <" + email + ">";
+    } else if (email != null) {
+      return email;
+    } else if (name != null) {
+      return name;
+    } else {
+      String username = user.getUserName();
+      if (username != null) {
+        return username;
+      }
+    }
+    return null;
+  }
+
+  public String getSshKey() {
+    return sshKey.getSshPublicKey();
+  }
+}

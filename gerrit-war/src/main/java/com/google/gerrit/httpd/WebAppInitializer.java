d197282d2061f11db7d297ecb16c8158bfc1a887
Dave Borowitz <dborowitz@google.com>
2015-08-26 09:51:27 -0400

    Move most GPG-related code to a gerrit-gpg module
    Bouncy Castle is still an optional dependency for Gerrit, so we want
    to avoid accidentally attempting to load Bouncy Castle classes when
    they might not be available. Rather than try to guard every
    org.bouncycastle.* reference with a hasPGP() check, reduce the surface
    area of calls that actually require Bouncy Castle. Move almost all
    code that calls Bouncy Castle into a new module, gerrit-gpg.
    
    Callers need only interact with this module by installing the
    GpgModule, which is careful to protect all Bouncy Castle class loading
    with the appropriate havePGP() check. Moreover, this module doesn't
    need to be installed in the gerrit-server package at all, so we can
    break the compile-time dependency between gerrit-server and Bouncy
    Castle, so accidentally introducing a dependency on Bouncy Castle
    results in a compile error.
    
    The REST API and extension APIs dealing with GPG keys only refer to
    the GpgKeyInfo POJO, and don't need to actually refer to Bouncy Castle
    classes. Add a shim interface, GpgApiAdapter, that is used by
    AccountApiImpl to process GPG keys. GpgModule binds this interface to
    either the Bouncy Castle enabled implementation, or a not-implemented
    implementation.
    
    Since there are various places in the server code where we want to
    inspect whether signed push is enabled at the server level, but we
    don't want to have to call into gerrit-gpg code to do this, bind a
    boolean with @EnableSignedPush from GpgModule.
    
    Change-Id: Idbab00a52d86216cae73d02876d56be54aef6581

--- gerrit-war/src/main/java/com/google/gerrit/httpd/WebAppInitializer.java
+++ gerrit-war/src/main/java/com/google/gerrit/httpd/WebAppInitializer.java
@@ -19,6 +19,7 @@ import static com.google.inject.Stage.PRODUCTION;
 
 import com.google.common.base.Splitter;
 import com.google.gerrit.common.ChangeHookRunner;
+import com.google.gerrit.gpg.GpgModule;
 import com.google.gerrit.httpd.auth.oauth.OAuthModule;
 import com.google.gerrit.httpd.auth.openid.OpenIdModule;
 import com.google.gerrit.httpd.plugins.HttpPluginModule;
@@ -107,6 +108,7 @@ public class WebAppInitializer extends GuiceServletContextListener
   private Path sitePath;
   private Injector dbInjector;
   private Injector cfgInjector;
+  private Config config;
   private Injector sysInjector;
   private Injector webInjector;
   private Injector sshInjector;
@@ -163,6 +165,8 @@ public class WebAppInitializer extends GuiceServletContextListener
       }
 
       cfgInjector = createCfgInjector();
+      config = cfgInjector.getInstance(
+          Key.get(Config.class, GerritServerConfig.class));
       sysInjector = createSysInjector();
       if (!sshdOff()) {
         sshInjector = createSshInjector();
@@ -202,8 +206,7 @@ public class WebAppInitializer extends GuiceServletContextListener
   }
 
   private boolean sshdOff() {
-    Config cfg = cfgInjector.getInstance(Key.get(Config.class, GerritServerConfig.class));
-    return new SshAddressesModule().getListenAddresses(cfg).isEmpty();
+    return new SshAddressesModule().getListenAddresses(config).isEmpty();
   }
 
   private Injector createDbInjector() {
@@ -294,6 +297,7 @@ public class WebAppInitializer extends GuiceServletContextListener
     modules.add(new SignedTokenEmailTokenVerifier.Module());
     modules.add(new PluginRestApiModule());
     modules.add(new RestCacheAdminModule());
+    modules.add(new GpgModule(config));
     AbstractModule changeIndexModule;
     switch (IndexModule.getIndexType(cfgInjector)) {
       case LUCENE:

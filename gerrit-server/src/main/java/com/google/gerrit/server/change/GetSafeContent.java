0c56b519ae5d3752479b4b709a7cb4afaade0b80
David Pletcher <dpletcher@google.com>
2015-09-01 17:45:55 -0700

    Implement Safe Content REST API method
    The Safe Content REST method has similar behavior to the legacy
    CatServlet servlet. It retrieves the requested content directly,
    for safe file types, or as a ZIP file containing an entry with
    an unpredictable filename based on the resource filename.
    
    There are minor differences. RestApiServlet always specifies
    attachment, with or without a filename, for the Content-Disposition,
    whereas CatServlet would not declare direct downloads as attachments.
    Given that the attachment disposition is obligatory, this REST API
    method always sets a meaningful attachment filename, for ZIP files
    and direct file alike.
    
    RestApiServlet specifies and parses resources differently than
    CatServlet. The GetSafeContent view includes an optional parameter
    for naming the side, which becomes a suffix decorating the filenames.
    CatServlet will use this parameter to designate whether the file
    comes from a child or parent in a diff.
    
    A forthcoming patch will modify CatServlet to consolidate its
    implementation, taking advantage of this new code.
    
    Change-Id: I2ecdbd0bd6706066026000de813244f909d48cba

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/GetSafeContent.java
@@ -0,0 +1,53 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.gerrit.extensions.restapi.BinaryResult;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.kohsuke.args4j.Option;
+
+import java.io.IOException;
+
+@Singleton
+public class GetSafeContent implements RestReadView<FileResource> {
+  private final FileContentUtil fileContentUtil;
+
+  @Option(name = "--side")
+  private String side;
+
+  @Inject
+  GetSafeContent(FileContentUtil fileContentUtil) {
+    this.fileContentUtil = fileContentUtil;
+  }
+
+  @Override
+  public BinaryResult apply(FileResource rsrc)
+      throws ResourceNotFoundException, IOException, NoSuchChangeException,
+      OrmException {
+    String path = rsrc.getPatchKey().get();
+    return fileContentUtil.getSafeContent(
+        rsrc.getRevision().getControl().getProjectControl().getProjectState(),
+        ObjectId.fromString(rsrc.getRevision().getPatchSet().getRevision().get()),
+        path,
+        side);
+  }
+}

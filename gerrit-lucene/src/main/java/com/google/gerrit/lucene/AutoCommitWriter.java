7dc7b0049e027191f43aa4eea98ce77ce67a913e
Gustaf Lundh <gustaf.lundh@axis.com>
2015-09-08 15:43:10 +0200

    Expose Lucene Index Writers for plug-ins
    By allowing other packages to reach the Lucene Index Writer,
    Gerrit plug-ins can now be written to allow runtime
    reconfiguration of various Lucene performance related
    parameters.
    
    All IndexWriters are also now AutoCommitWriters. This will
    allow a plug-in to temporarily turn on auto-committing in
    situations where it makes sense enforcing all changes to
    be written to disk ASAP.
    
    In our case, this allows us building a hot swap mechanism for
    two Gerrit servers to be used within a continuous deployment
    setup, without needing to sacrifice the daily performance of
    the production systems (which a permanent index.name.commitWithin
    = 0 configuration would have done).
    
    Change-Id: Iacba1f5acd4b59c77e6b1619d1438a69fe0c77d0

--- gerrit-lucene/src/main/java/com/google/gerrit/lucene/AutoCommitWriter.java
+++ gerrit-lucene/src/main/java/com/google/gerrit/lucene/AutoCommitWriter.java
@@ -25,13 +25,22 @@ import org.apache.lucene.store.Directory;
 import java.io.IOException;
 
 /** Writer that optionally flushes/commits after every write. */
-class AutoCommitWriter extends IndexWriter {
+public class AutoCommitWriter extends IndexWriter {
   private boolean autoCommit;
+
+  AutoCommitWriter(Directory dir, IndexWriterConfig config)
+      throws IOException {
+    this(dir, config, false);
+  }
 
   AutoCommitWriter(Directory dir, IndexWriterConfig config, boolean autoCommit)
       throws IOException {
     super(dir, config);
-    this.autoCommit = autoCommit;
+    setAutoCommit(autoCommit);
+  }
+
+  public void setAutoCommit(boolean enable) {
+    this.autoCommit = enable;
   }
 
   @Override
@@ -99,7 +108,7 @@ class AutoCommitWriter extends IndexWriter {
     }
   }
 
-  private void autoFlush() throws IOException {
+  public void autoFlush() throws IOException {
     if (autoCommit) {
       manualFlush();
     }

9823c0aa9236b4110b194e94beedeccd6bcf9f00
Dave Borowitz <dborowitz@google.com>
2015-09-14 12:23:38 -0400

    Store push certificate in patch set during ReceiveCommits
    Push certificates for new/updated changes contain commands for
    refs/for/*, even though that ref is not created. This means if we
    store such certificates in PushCertificateStore, we can't use the
    history of push certs on that ref to reconstruct anything like a
    reflog. It would be confusing to see in refs/meta/push-certs a push
    cert mentioning refs/for/* either on that ref (which doesn't exist) or
    refs/changes/* (which is a completely unrelated refname).
    
    Pushing to refs/for/* and having the data become a patch set is a
    detail of the Gerrit model, so it makes sense to store the cert in the
    same place as the patch set SHA-1 itself, namely the PatchSets table.
    
    Change-Id: I51924054742fbd31b1abcce1ccdcf9bce76642ac

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -2075,6 +2075,8 @@ public class ReceiveCommits {
       newPatchSet.setUploader(currentUser.getAccountId());
       newPatchSet.setRevision(toRevId(newCommit));
       newPatchSet.setGroups(groups);
+      newPatchSet.setPushCertificate(
+          rp.getPushCertificate().toTextWithSignature());
       if (magicBranch != null && magicBranch.draft) {
         newPatchSet.setDraft(true);
       }

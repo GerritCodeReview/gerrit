538b33e862b462b3c1f7dd2397c389d02bbc6383
Dave Borowitz <dborowitz@google.com>
2015-09-15 09:58:17 -0400

    Allow disabling GPG key editing while leaving signed push enabled
    Administrators may choose to populate GPG keys by hand, or to
    temporarily restrict adding new keys, without disabling signed push
    verification or verification of push certs stored in PatchSets.
    Separate the UI for editing GPG keys from the UI for verifying signed
    push.
    
    Change-Id: I2c921cf92a3452d44ee8cb02efd77ab7f2bd02dd

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/Dispatcher.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/Dispatcher.java
@@ -539,7 +539,8 @@ public class Dispatcher {
           return new MySshKeysScreen();
         }
 
-        if (matchExact(SETTINGS_GPGKEYS, token)) {
+        if (matchExact(SETTINGS_GPGKEYS, token)
+            && Gerrit.info().gerrit().editGpgKeys()) {
           return new MyGpgKeysScreen();
         }
 

0683e635f7da69ffde802778990e2f1bbb2ccf80
David Ostrovsky <david@ostrovsky.org>
2015-09-19 13:41:07 +0200

    Replace ACCOUNT_DIFF_PREFERENCES table with Git backend (part2)
    This is the second part in the series of migration of user preferences
    table to Git backend. The split is necessary to support zero-downtime
    migration. The background article about details how zero-downtime
    schema migration is implemented is here: [1].
    
    Database migration is implemented, migrating the content of the diff
    account preferences table into Git backend, All-Users repository.
    
    Migrated flag is not used any more and can be removed in future schema
    migrations.
    
    [1] https://groups.google.com/d/topic/repo-discuss/iR-T8fTRC7E/discussion
    
    Change-Id: I2245d0a3948fd3d65151b3774f7778842e95234a

--- gerrit-server/src/main/java/com/google/gerrit/server/account/GetDiffPreferences.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/GetDiffPreferences.java
@@ -15,22 +15,14 @@
 package com.google.gerrit.server.account;
 
 import static com.google.gerrit.server.config.ConfigUtil.loadSection;
-import static com.google.gerrit.server.config.ConfigUtil.storeSection;
 
 import com.google.gerrit.extensions.client.DiffPreferencesInfo;
-import com.google.gerrit.extensions.client.Theme;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.RestReadView;
-import com.google.gerrit.reviewdb.client.Account;
-import com.google.gerrit.reviewdb.client.AccountDiffPreference;
-import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.config.AllUsersName;
 import com.google.gerrit.server.git.GitRepositoryManager;
-import com.google.gerrit.server.git.MetaDataUpdate;
 import com.google.gerrit.server.git.UserConfigSections;
-import com.google.gerrit.server.patch.PatchListKey;
-import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
@@ -43,27 +35,21 @@ import java.io.IOException;
 @Singleton
 public class GetDiffPreferences implements RestReadView<AccountResource> {
   private final Provider<CurrentUser> self;
-  private final Provider<ReviewDb> db;
-  private final MetaDataUpdate.User metaDataUpdateFactory;
   private final Provider<AllUsersName> allUsersName;
   private final GitRepositoryManager gitMgr;
 
   @Inject
   GetDiffPreferences(Provider<CurrentUser> self,
-      Provider<ReviewDb> db,
-      MetaDataUpdate.User metaDataUpdateFactory,
       Provider<AllUsersName> allUsersName,
       GitRepositoryManager gitMgr) {
     this.self = self;
-    this.db = db;
-    this.metaDataUpdateFactory = metaDataUpdateFactory;
     this.allUsersName = allUsersName;
     this.gitMgr = gitMgr;
   }
 
   @Override
   public DiffPreferencesInfo apply(AccountResource rsrc)
-      throws AuthException, OrmException, ConfigInvalidException, IOException {
+      throws AuthException, ConfigInvalidException, IOException {
     if (self.get() != rsrc.getUser()
         && !self.get().getCapabilities().canAdministrateServer()) {
       throw new AuthException("restricted to administrator");
@@ -76,46 +62,9 @@ public class GetDiffPreferences implements RestReadView<AccountResource> {
       DiffPreferencesInfo prefs = new DiffPreferencesInfo();
       loadSection(p.getConfig(), UserConfigSections.DIFF, null, prefs,
           DiffPreferencesInfo.defaults());
-      if (prefs.migrated == null) {
-        Account.Id id = rsrc.getUser().getAccountId();
-        AccountDiffPreference a = db.get().accountDiffPreferences().get(id);
-        initFromDb(prefs, a);
-        prefs.migrated = true;
-        MetaDataUpdate md = metaDataUpdateFactory.create(allUsersName.get());
-        storeSection(p.getConfig(), UserConfigSections.DIFF, null, prefs,
-            DiffPreferencesInfo.defaults());
-        p.commit(md);
-      }
       // Reset the migrated flag to prevent it from being sent over the wire.
       prefs.migrated = null;
       return prefs;
-    }
-  }
-
-  private static void initFromDb(DiffPreferencesInfo prefs,
-      AccountDiffPreference a) {
-    if (a != null) {
-      prefs.context = (int)a.getContext();
-      prefs.expandAllComments = a.isExpandAllComments();
-      prefs.hideLineNumbers = a.isHideLineNumbers();
-      prefs.hideTopMenu = a.isHideTopMenu();
-      prefs.ignoreWhitespace = PatchListKey.WHITESPACE_TYPES.inverse().get(
-          a.getIgnoreWhitespace().getCode());
-      prefs.intralineDifference = a.isIntralineDifference();
-      prefs.lineLength = a.getLineLength();
-      prefs.manualReview = a.isManualReview();
-      prefs.renderEntireFile = a.isRenderEntireFile();
-      prefs.retainHeader = a.isRetainHeader();
-      prefs.showLineEndings = a.isShowLineEndings();
-      prefs.showTabs = a.isShowTabs();
-      prefs.showWhitespaceErrors = a.isShowWhitespaceErrors();
-      prefs.skipDeleted = a.isSkipDeleted();
-      prefs.skipUncommented = a.isSkipUncommented();
-      prefs.syntaxHighlighting = a.isSyntaxHighlighting();
-      prefs.tabSize = a.getTabSize();
-      prefs.theme = Theme.valueOf(a.getTheme().name());
-      prefs.hideEmptyPane = a.isHideEmptyPane();
-      prefs.autoHideDiffTableHeader = a.isAutoHideDiffTableHeader();
     }
   }
 }

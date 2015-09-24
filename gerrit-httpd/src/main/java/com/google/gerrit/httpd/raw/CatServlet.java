b3c1fcfee8ae2f11ccdd513917015c3001bc3507
David Pursehouse <david.pursehouse@sonymobile.com>
2015-09-24 10:29:39 +0900

    CatServlet: Remove unused imports
    Change-Id: I5433c7b959f4da3100cac87abd3c8fb54c317a0d

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/CatServlet.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/CatServlet.java
@@ -20,24 +20,16 @@ import com.google.gerrit.extensions.restapi.Url;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.Patch;
 import com.google.gerrit.reviewdb.client.PatchSet;
-import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.edit.ChangeEdit;
 import com.google.gerrit.server.edit.ChangeEditUtil;
-import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.project.ChangeControl;
 import com.google.gerrit.server.project.NoSuchChangeException;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
-
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectReader;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
 
 import java.io.IOException;
 

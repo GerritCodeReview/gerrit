df3ce66cf9392dc1a4078f319ca517c977331b55
David Ostrovsky <david@ostrovsky.org>
2015-09-26 14:17:50 +0200

    Add support for Apache Derby database
    In the current gwtorm implementation, column renames for columns
    with a check constraint are not supported by Apache Derby.
    To rename a column with a check constraint the column constraint
    must be dropped manualy. After that the column can be renamed by
    the gwtorm implementation. After the renaming the constraint can
    be added again.
    
    Test plan:
    
    * Replace H2 with Apache Derby, add derby to Buck tests rules as
      new dependency and run the tests with `buck test`
    * Set up new site with Apache Derby database and perform the tests
    
    Bug: Issue 3441
    Change-Id: I46e719ed54ea633c127c7bcd381b882e3a9b37ba

---
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/DerbyInitializer.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm.init;
+
+import static com.google.gerrit.pgm.init.api.InitUtil.die;
+
+import com.google.gerrit.common.FileUtil;
+import com.google.gerrit.pgm.init.api.Section;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+
+import java.nio.file.Path;
+
+class DerbyInitializer implements DatabaseConfigInitializer {
+
+  private final SitePaths site;
+
+  @Inject
+  DerbyInitializer(final SitePaths site) {
+    this.site = site;
+  }
+
+  @Override
+  public void initConfig(Section databaseSection) {
+    String path = databaseSection.get("database");
+    Path db;
+    if (path == null) {
+      db = site.resolve("db").resolve("ReviewDB");
+      databaseSection.set("database", db.toString());
+    } else {
+      db = site.resolve(path);
+    }
+    if (db == null) {
+      throw die("database.database must be supplied for Derby");
+    }
+    db = db.getParent();
+    FileUtil.mkdirsOrDie(db, "cannot create database.database");
+  }
+}

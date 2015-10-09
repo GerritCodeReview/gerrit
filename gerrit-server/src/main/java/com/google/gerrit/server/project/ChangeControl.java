d79ac58829865ad1f715e257384f902c03b7320d
David Pursehouse <david.pursehouse@sonymobile.com>
2015-10-09 13:10:45 +0000

    ChangeControl: remove unused import
    Change-Id: I3a5f2cfce7d20e911112064a215fbf2d89c164df

--- gerrit-server/src/main/java/com/google/gerrit/server/project/ChangeControl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/ChangeControl.java
@@ -28,7 +28,6 @@ import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.IdentifiedUser;
-import com.google.gerrit.server.InternalUser;
 import com.google.gerrit.server.notedb.ChangeNotes;
 import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gwtorm.server.OrmException;

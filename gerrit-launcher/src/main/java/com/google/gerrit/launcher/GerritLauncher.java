873735cd66e3d55e33fdb9a27063e67b7786776e
David Ostrovsky <david@ostrovsky.org>
2015-10-12 20:00:59 +0200

    GerritLauncher: Add rebuild-notedb to commonly used commands
    Wit this change invoking gerrit.war without arguments lists
    RebuildNotedb command:
    
      $ java -jar buck-out/gen/gerrit/gerrit.war
      Gerrit Code Review
      usage: java -jar gerrit.war command [ARG ...]
    
      The most commonly used commands are:
        init             Initialize a Gerrit installation
        rebuild-notedb   Rebuild the review notes database
        reindex          Rebuild the secondary index
        [...]
    
    Change-Id: I1f6cfbce3855f4171ad92709f05bce55d18cd6b6

--- gerrit-launcher/src/main/java/com/google/gerrit/launcher/GerritLauncher.java
+++ gerrit-launcher/src/main/java/com/google/gerrit/launcher/GerritLauncher.java
@@ -66,14 +66,15 @@ public final class GerritLauncher {
       System.err.println("usage: java -jar " + jar + " command [ARG ...]");
       System.err.println();
       System.err.println("The most commonly used commands are:");
-      System.err.println("  init           Initialize a Gerrit installation");
-      System.err.println("  reindex        Rebuild the secondary index");
-      System.err.println("  daemon         Run the Gerrit network daemons");
-      System.err.println("  gsql           Run the interactive query console");
-      System.err.println("  version        Display the build version number");
+      System.err.println("  init            Initialize a Gerrit installation");
+      System.err.println("  rebuild-notedb  Rebuild the review notes database");
+      System.err.println("  reindex         Rebuild the secondary index");
+      System.err.println("  daemon          Run the Gerrit network daemons");
+      System.err.println("  gsql            Run the interactive query console");
+      System.err.println("  version         Display the build version number");
       System.err.println();
-      System.err.println("  ls             List files available for cat");
-      System.err.println("  cat FILE       Display a file from the archive");
+      System.err.println("  ls              List files available for cat");
+      System.err.println("  cat FILE        Display a file from the archive");
       System.err.println();
       return 1;
     }

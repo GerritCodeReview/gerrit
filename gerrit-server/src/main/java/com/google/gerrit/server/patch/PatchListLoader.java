4d876170d89e0deff386e023ec9cf6eebb113ea3
Stefan Beller <sbeller@google.com>
2015-10-13 13:50:52 -0700

    show submodule difference properly in side by side view
    Previous versions of JGit incorrectly include the submodule 'Subproject
    commit' diff lines as part of the diff header instead of as part of a
    diff hunk. Newer versions of JGit fix this. However, the way that
    submodule differences are displayed depends on this old broken behavior.
    
    The extra header lines are shown in place of the normal side by side
    view. In addition, gerrit doesn't know how to show gitlink files. Fix
    this by removing the locations hard coded to check for GITLINK files.
    Modify the PatchFile output to be able to show the C git style
    'Subproject commit <sha1sum>' format. Add the same format conditioned on
    OBJ_COMMIT into PatchScriptBuilder.
    
    The end result will be a proper side-by-side view of Submodules. It does
    not currently include any extra information. A future enhancement shall
    use a Repository object to perform lookups on the actual submodule
    repository to determine the extend of what commits were added or
    removed.
    
    Change-Id: Icf62d74c27ed81bf2c07b5ce0e18a527705f1631
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchListLoader.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/patch/PatchListLoader.java
@@ -315,11 +315,6 @@ public class PatchListLoader implements Callable<PatchList> {
     final FileMode oldMode = fileHeader.getOldMode();
     final FileMode newMode = fileHeader.getNewMode();
 
-    if (oldMode == FileMode.GITLINK || newMode == FileMode.GITLINK) {
-      return new PatchListEntry(fileHeader, Collections.<Edit> emptyList(),
-          size, sizeDelta);
-    }
-
     if (aTree == null // want combined diff
         || fileHeader.getPatchType() != PatchType.UNIFIED
         || fileHeader.getHunks().isEmpty()) {

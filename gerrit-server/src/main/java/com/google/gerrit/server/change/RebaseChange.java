8398b1cfca1542aa410ace12aa9d8a7812d9924a
Dave Borowitz <dborowitz@google.com>
2015-10-14 15:12:43 -0400

    Pass RevWalk to PatchSetInfoFactory
    Previously #get(RevCommit, PatchSet.Id) would NPE if the body of the
    commit was not parsed; callers needed to remember to either parse the
    body or ensure that the RevWalk that created the commit had
    setRetainBody(true). In all cases, there is already a handy RevWalk;
    pass this into get and do the parseBody there, such that it's
    impossible for the caller to forget. (In the BatchUpdate.Op cases, we
    need to shuffle call order around a bit so the PatchSetInfo gets
    populated during updateRepo.)
    
    Change-Id: I2e42ed1cc5c4a50958b6367051065e6bbad5cc54

--- gerrit-server/src/main/java/com/google/gerrit/server/change/RebaseChange.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/RebaseChange.java
@@ -271,7 +271,9 @@ public class RebaseChange {
 
     RevCommit rebasedCommit;
     ObjectId oldId = ObjectId.fromString(originalPatchSet.getRevision().get());
-    ObjectId newId = rebaseCommit(git, inserter, rw.parseCommit(oldId),
+    RevCommit original = rw.parseCommit(oldId);
+    rw.parseBody(original);
+    ObjectId newId = rebaseCommit(git, inserter, original,
         baseCommit, mergeUtil, committerIdent);
 
     rebasedCommit = rw.parseCommit(newId);

4a8cf21e0990a50e16fe34c0ec33636e9f9c61e2
David Ostrovsky <david@ostrovsky.org>
2015-10-16 22:48:58 +0000

    Buck: Trigger pack_war invocation when one of transitive deps changed
    Buck version was upgraded in Ib3f22e70b7. This version contains
    behavioral change for genrule()'s rules. Since: [1] the caching
    behavior was switched to input-based key. So that the command in
    genrule() is executed only when one of the input keys has changed.
    This massive change in behavior broke Gerrit build toolchain in
    very subtle way: when gerrit.war was built once, it wasn't rebuilt
    any more unless caches weren't invalidated. To understand why this
    is the case, some background understanding needed how gerrit.war
    is packaged in our build toolchain. Gathering of gerrit.war is done
    by genrule() that executes python script that retrieves the needed
    libraries by issuing buck audit classpath. However, since: [1] this
    python script is not invoked any more, when some libraries in
    transitive dependency chain have changed, because only very few
    libraries specified as the input to this python rule and the rest of
    the dependencies is retrieved in dynamic way in the script itself.
    So when some source code was changed, and corresponding library, say
    gerrit-server was changed as well, the first order dependency to the
    python script, like gerrit-pgm wasn't changed, and thus its rule key
    remained stable, so that pack_war.py wasn't invoked any more, leaving
    gerrit.war stale.
    
    The reproducer for this problem can be found here: [2]. The upstream
    issue is here: [3].
    
    The fix is straight forward: instead of passing only the first order
    dependencies to the war packager script (that cannot reliably be done
    after changed caching behavior of genrule() in: [1]) and issue buck
    audit classpath in python script, pass the whole classpath from Buck
    build file to war packager. For one this will fix the caching issue,
    for another we wouldn't have to retrieve the classpath twice: in Buck
    build itself and in war packager script.
    
    [1] https://github.com/facebook/buck/commit/143646f4ccd8737f73a2667a244c4c38dc18cb89
    [2] https://github.com/davido/buck_genrule_changed_caching_behaviour_143646f
    [3] https://github.com/facebook/buck/issues/470
    
    Reported-By: Doug Kelly <dougk.ff7@gmail.com>
    Change-Id: I09d95176db62ea5846a4791b9a21b1c3a577f548

--- tools/pack_war.py
+++ tools/pack_war.py
@@ -16,7 +16,7 @@
 from __future__ import print_function
 from optparse import OptionParser
 from os import chdir, makedirs, path, symlink
-from subprocess import check_call, check_output
+from subprocess import check_call
 import sys
 
 opts = OptionParser()
@@ -30,16 +30,17 @@ war = args.tmp
 root = war[:war.index('buck-out')]
 jars = set()
 
+def prune(libs):
+  t = []
+  for j in libs:
+    for e in j.split(':'):
+      t.append(e[e.find('buck-out'):])
+  return t
 
 def link_jars(libs, directory):
   makedirs(directory)
   while not path.isfile('.buckconfig'):
     chdir('..')
-  try:
-    cp = check_output(['buck', 'audit', 'classpath'] + libs)
-  except Exception as e:
-    print('call to buck audit failed: %s' % e, file=sys.stderr)
-    exit(1)
   for j in cp.strip().splitlines():
     if j not in jars:
       jars.add(j)
@@ -49,9 +50,9 @@ def link_jars(libs, directory):
       symlink(path.join(root, j), path.join(directory, n))
 
 if args.lib:
-  link_jars(args.lib, path.join(war, 'WEB-INF', 'lib'))
+  link_jars(prune(args.lib), path.join(war, 'WEB-INF', 'lib'))
 if args.pgmlib:
-  link_jars(args.pgmlib, path.join(war, 'WEB-INF', 'pgm-lib'))
+  link_jars(prune(args.pgmlib), path.join(war, 'WEB-INF', 'pgm-lib'))
 try:
   for s in ctx:
     check_call(['unzip', '-q', '-d', war, s])

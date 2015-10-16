72872662eb4c0bae63da67d3cf3dde883e25fdac
David Pursehouse <david.pursehouse@sonymobile.com>
2015-10-16 23:50:59 +0900

    Elimiate unnecessary Charset constants
    Change-Id: Ibb217a1cc08ae8f86a8c0bd9814f53cf3b0299e0

--- gerrit-openid/src/main/java/com/google/gerrit/httpd/auth/openid/XrdsServlet.java
+++ gerrit-openid/src/main/java/com/google/gerrit/httpd/auth/openid/XrdsServlet.java
@@ -22,7 +22,6 @@ import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -32,7 +31,6 @@ import javax.servlet.http.HttpServletResponse;
 @Singleton
 class XrdsServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
-  private static final Charset ENC = UTF_8;
   static final String LOCATION = "OpenID.XRDS";
 
   private final Provider<String> url;
@@ -46,7 +44,8 @@ class XrdsServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse rsp)
       throws IOException {
     final StringBuilder r = new StringBuilder();
-    r.append("<?xml version=\"1.0\" encoding=\"").append(ENC.name()).append("\"?>");
+    r.append("<?xml version=\"1.0\" encoding=\"")
+     .append(UTF_8.name()).append("\"?>");
     r.append("<xrds:XRDS");
     r.append(" xmlns:xrds=\"xri://$xrds\"");
     r.append(" xmlns:openid=\"http://openid.net/xmlns/1.0\"");
@@ -61,10 +60,10 @@ class XrdsServlet extends HttpServlet {
     r.append("</xrds:XRDS>");
     r.append("\n");
 
-    final byte[] raw = r.toString().getBytes(ENC);
+    final byte[] raw = r.toString().getBytes(UTF_8);
     rsp.setContentLength(raw.length);
     rsp.setContentType("application/xrds+xml");
-    rsp.setCharacterEncoding(ENC.name());
+    rsp.setCharacterEncoding(UTF_8.name());
 
     try (ServletOutputStream out = rsp.getOutputStream()) {
       out.write(raw);

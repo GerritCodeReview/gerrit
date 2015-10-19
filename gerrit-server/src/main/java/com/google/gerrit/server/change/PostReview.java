054c44c94def4650ae0d99c4722936a366bcb60e
Bill Wendling <morbo@google.com>
2015-10-19 15:40:57 -0700

    Add option to omit duplicate comments.
    Change-Id: If2de87cbf61619ded55bbd6af57fc57b9026bd23

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PostReview.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PostReview.java
@@ -47,6 +47,7 @@ import com.google.gerrit.reviewdb.client.Patch;
 import com.google.gerrit.reviewdb.client.PatchLineComment;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.reviewdb.client.PatchSetApproval;
+import com.google.gerrit.reviewdb.server.PatchLineCommentAccess;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.ApprovalsUtil;
 import com.google.gerrit.server.ChangeMessagesUtil;
@@ -64,6 +65,7 @@ import com.google.gerrit.server.project.ChangeControl;
 import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.util.LabelVote;
 import com.google.gwtorm.server.OrmException;
+import com.google.gwtorm.server.ResultSet;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
@@ -377,6 +379,9 @@ public class PostReview implements RestModifyView<RevisionResource, ReviewInput>
       for (Map.Entry<String, List<CommentInput>> ent : map.entrySet()) {
         String path = ent.getKey();
         for (CommentInput c : ent.getValue()) {
+          if (in.omitDuplicateComments && isDuplicateComment(ctx, c)) {
+            continue;
+          }
           String parent = Url.decode(c.inReplyTo);
           PatchLineComment e = drafts.remove(Url.decode(c.id));
           if (e == null) {
@@ -428,6 +433,21 @@ public class PostReview implements RestModifyView<RevisionResource, ReviewInput>
       plcUtil.upsertComments(ctx.getDb(), ctx.getChangeUpdate(), ups);
       comments.addAll(ups);
       return !del.isEmpty() || !ups.isEmpty();
+    }
+
+    private boolean isDuplicateComment(ChangeContext ctx, CommentInput c)
+        throws OrmException {
+      PatchLineCommentAccess patchLineCommentAccess = db.get().patchComments();
+      List<PatchLineComment> patchLineComments =
+          patchLineCommentAccess.byChange(ctx.getChange().getId()).toList();
+
+      for (PatchLineComment comment : patchLineComments) {
+        if (comment.getStatus() == PatchLineComment.Status.PUBLISHED
+            && c.id.equals(comment.getKey().get())) {
+          return true;
+        }
+      }
+      return false;
     }
 
     private Map<String, PatchLineComment> changeDrafts(ChangeContext ctx)

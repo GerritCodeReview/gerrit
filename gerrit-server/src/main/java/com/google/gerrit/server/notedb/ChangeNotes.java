aa0a3f93a0d3f3c6e5795acf8aa1b0f1b1657bcb
Edwin Kempin <ekempin@google.com>
2015-10-21 18:16:14 +0200

    Store topic of a change in Notedb
    This adds a 'Topic' footer to  the commit messages of the
    refs/changes/XX/YYYY/meta notes branch when the topic changes.
    
    Similar as for hashtags we cannot rebuild the history for topic
    changes since the database only stores the current topic.
    
    Change-Id: I5ca2c1ed6326ba03dfbee21f1a7d2328b73ad322
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
@@ -18,6 +18,7 @@ import static com.google.gerrit.server.notedb.ChangeNoteUtil.GERRIT_PLACEHOLDER_
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Function;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableSet;
@@ -264,6 +265,7 @@ public class ChangeNotes extends AbstractChangeNotes<ChangeNotes> {
       allChangeMessages = parser.buildAllMessages();
       comments = ImmutableListMultimap.copyOf(parser.comments);
       noteMap = parser.commentNoteMap;
+      change.setTopic(Strings.emptyToNull(parser.topic));
 
       if (parser.hashtags != null) {
         hashtags = ImmutableSet.copyOf(parser.hashtags);

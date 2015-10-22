09322bcb8c2e2120741c315f8ad5e9c4eb9ae69d
Simon Hwang <simon.hwang@ericsson.com>
2015-10-22 17:52:28 -0400

    Use image instead of Unicode Character for Copy Button
    Due to the incompatibility of fonts and encoding in some environments,
    some browsers could not render the Copy Button correctly.
    
    This change uses instead a clipboard image to replace the unstable
    Unicode Character for the Copy Button.
    
    The image is open sourced and is discovered at:
    http://uxrepo.com/icon/clipboard-by-ionicons
    Also, license for the use of the image is included.
    
    Change-Id: Ie7d55e8cde40df445d32e6b8fcab91cf8a0c05b7

--- gerrit-gwtexpui/src/main/java/com/google/gwtexpui/clippy/client/CopyableLabel.java
+++ gerrit-gwtexpui/src/main/java/com/google/gwtexpui/clippy/client/CopyableLabel.java
@@ -12,6 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// The above license applies to this file, whereas the below license applies to
+// ClippyResourcs (line 147) as used herein.
+
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 Drifty (http://drifty.com/)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package com.google.gwtexpui.clippy.client;
 
 import com.google.gwt.core.client.Scheduler;
@@ -119,7 +144,8 @@ public class CopyableLabel extends Composite implements HasText {
     }
 
     if (UserAgent.hasJavaScriptClipboard()) {
-      copier = new Button("&#x1f4cb;"); // CLIPBOARD
+      String img = ClippyResources.I.clipboard().getSafeUri().asString();
+      copier = new Button("<img src=\"" + img + "\" width=\"14\" height=\"14\"/>");
       copier.setStyleName(ClippyResources.I.css().copier());
       Tooltip.addStyle(copier);
       Tooltip.setLabel(copier, CopyableLabelText.I.tooltip());

9b49cbcfac29be91eb4861036d2fae8a88f6b0e3
David Ostrovsky <david@ostrovsky.org>
2015-10-31 16:09:42 +0100

    Replace AccountGeneralPreferences entity with git backend (phase 1)
    Replace the usage of AccountGeneralPreferences with a different class
    in extension API package.  Given that this is embedded entity in the
    account table, only the columns are removed from the database, while
    the accounts table itself is stil preserved.
    
    To support live migration, the upgrade is done in two steps:
    
    * phase 1 (this change):
      o Always write to both git and db
      o Introduce new configuration option to indicate whether to read from
        git or db, initially set to read from db
      o First binary update: some servers are reading/writing just the db;
        some servers are additionally writing to git
      o After first update: all servers are reading from the db, writing to
        both
      o Batch copy data from db to git (only related to non open source
        Gerrit version)
      o Update all servers to read from git. During the update, some will
        still be reading from the db; that's ok, because everybody is
        writing to git
      o OutgoingEmail class is retrieving copy_self_on_email preference
        always form the database (by getting AccountGlobalPreferences from
        the account cache). This is OK, because all servers are writing to
        the database
    * phase 2 (next change):
      o Bump database version, migrate the data from db to git, delete the
        entity (and the flag) from the code and update the servers.
    
    Change-Id: I218d92465665afa0caa01e88ff1fa87f773383eb

--- gerrit-gwtui-common/src/main/java/com/google/gerrit/client/info/AccountPreferencesInfo.java
+++ gerrit-gwtui-common/src/main/java/com/google/gerrit/client/info/AccountPreferencesInfo.java
@@ -17,12 +17,12 @@ package com.google.gerrit.client.info;
 import com.google.gerrit.client.rpc.NativeMap;
 import com.google.gerrit.client.rpc.NativeString;
 import com.google.gerrit.client.rpc.Natives;
-import com.google.gerrit.reviewdb.client.AccountGeneralPreferences;
-import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DateFormat;
-import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DiffView;
-import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.DownloadCommand;
-import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.ReviewCategoryStrategy;
-import com.google.gerrit.reviewdb.client.AccountGeneralPreferences.TimeFormat;
+import com.google.gerrit.extensions.client.AccountGeneralPreferencesInfo;
+import com.google.gerrit.extensions.client.AccountGeneralPreferencesInfo.DateFormat;
+import com.google.gerrit.extensions.client.AccountGeneralPreferencesInfo.DiffView;
+import com.google.gerrit.extensions.client.AccountGeneralPreferencesInfo.DownloadCommand;
+import com.google.gerrit.extensions.client.AccountGeneralPreferencesInfo.ReviewCategoryStrategy;
+import com.google.gerrit.extensions.client.AccountGeneralPreferencesInfo.TimeFormat;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
 
@@ -36,32 +36,32 @@ public class AccountPreferencesInfo extends JavaScriptObject {
   }
 
   public static AccountPreferencesInfo createDefault() {
-    AccountGeneralPreferences defaultPrefs =
-        AccountGeneralPreferences.createDefault();
+    AccountGeneralPreferencesInfo d =
+        AccountGeneralPreferencesInfo.defaults();
     AccountPreferencesInfo p = createObject().cast();
-    p.changesPerPage(defaultPrefs.getMaximumPageSize());
-    p.showSiteHeader(defaultPrefs.isShowSiteHeader());
-    p.useFlashClipboard(defaultPrefs.isUseFlashClipboard());
-    p.downloadScheme(defaultPrefs.getDownloadUrl());
-    p.downloadCommand(defaultPrefs.getDownloadCommand());
-    p.copySelfOnEmail(defaultPrefs.isCopySelfOnEmails());
-    p.dateFormat(defaultPrefs.getDateFormat());
-    p.timeFormat(defaultPrefs.getTimeFormat());
-    p.relativeDateInChangeTable(defaultPrefs.isRelativeDateInChangeTable());
-    p.sizeBarInChangeTable(defaultPrefs.isSizeBarInChangeTable());
-    p.legacycidInChangeTable(defaultPrefs.isLegacycidInChangeTable());
-    p.muteCommonPathPrefixes(defaultPrefs.isMuteCommonPathPrefixes());
-    p.reviewCategoryStrategy(defaultPrefs.getReviewCategoryStrategy());
-    p.diffView(defaultPrefs.getDiffView());
+    p.changesPerPage(d.changesPerPage);
+    p.showSiteHeader(d.showSiteHeader);
+    p.useFlashClipboard(d.useFlashClipboard);
+    p.downloadScheme(d.downloadScheme);
+    p.downloadCommand(d.downloadCommand);
+    p.copySelfOnEmail(d.copySelfOnEmail);
+    p.dateFormat(d.getDateFormat());
+    p.timeFormat(d.getTimeFormat());
+    p.relativeDateInChangeTable(d.relativeDateInChangeTable);
+    p.sizeBarInChangeTable(d.sizeBarInChangeTable);
+    p.legacycidInChangeTable(d.legacycidInChangeTable);
+    p.muteCommonPathPrefixes(d.muteCommonPathPrefixes);
+    p.reviewCategoryStrategy(d.getReviewCategoryStrategy());
+    p.diffView(d.getDiffView());
     return p;
   }
 
-  public final short changesPerPage() {
-    short changesPerPage =
-        get("changes_per_page", AccountGeneralPreferences.DEFAULT_PAGESIZE);
+  public final int changesPerPage() {
+    int changesPerPage =
+        get("changes_per_page", AccountGeneralPreferencesInfo.DEFAULT_PAGESIZE);
     return 0 < changesPerPage
         ? changesPerPage
-        : AccountGeneralPreferences.DEFAULT_PAGESIZE;
+        : AccountGeneralPreferencesInfo.DEFAULT_PAGESIZE;
   }
   private final native short get(String n, int d)
   /*-{ return this.hasOwnProperty(n) ? this[n] : d }-*/;
@@ -128,7 +128,7 @@ public class AccountPreferencesInfo extends JavaScriptObject {
   public final native JsArray<TopMenuItem> my()
   /*-{ return this.my; }-*/;
 
-  public final native void changesPerPage(short n)
+  public final native void changesPerPage(int n)
   /*-{ this.changes_per_page = n }-*/;
 
   public final native void showSiteHeader(boolean s)

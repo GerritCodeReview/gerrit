212d7562e31bf2fbf4003991d16565f9f0eec62b
Dave Borowitz <dborowitz@google.com>
2015-11-03 13:12:41 -0500

    Serve GWT UI from ResourceServlet
    We already have a somewhat-featureful static content servlet for
    serving data from /static; use it for the GWT UI as well. Java's zip
    filesystem support makes the war case easy; we don't have to do the
    extract-to-a-directory hack that makes it work with Jetty. The
    developer case is also pretty easy, though we have to move the
    filter to recompile the GWT UI into the httpd package.
    
    One other wrinkle is that the GWT build process puts bogus timestamps
    on the GWT compiler output, so we need to pretend the timestamps on
    all the files are the startup time of the server. This means clients
    will have to re-download large identical JS assets after a server
    restart even if the assets didn't change. Gerrit has mostly pretty
    good uptime so this is not a huge deal.
    
    Change-Id: I0a7ade3cadf3a4a4e1726b56b87b0cbe4c6e0c93

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/DeveloperGwtUiServlet.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd.raw;
+
+import com.google.common.cache.Cache;
+import com.google.gerrit.common.TimeUtil;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+
+class DeveloperGwtUiServlet extends ResourceServlet {
+  private static final long serialVersionUID = 1L;
+
+  private final Path ui;
+  private final FileTime now;
+
+  DeveloperGwtUiServlet(Cache<Path, Resource> cache, Path unpackedWar)
+      throws IOException {
+    super(cache, false);
+    now = FileTime.fromMillis(TimeUtil.nowMs());
+    ui = unpackedWar.resolve("gerrit_ui");
+    Files.createDirectory(ui);
+    ui.toFile().deleteOnExit();
+  }
+
+  @Override
+  protected Path getResourcePath(String pathInfo) {
+    return ui.resolve(pathInfo);
+  }
+
+  @Override
+  protected FileTime getLastModifiedTime(Path p) {
+    // Return construction time of this instance (assuming it is a singleton),
+    // since the GWT outputs from the build process all have mtimes of 1980/1/1.
+    return now;
+  }
+}

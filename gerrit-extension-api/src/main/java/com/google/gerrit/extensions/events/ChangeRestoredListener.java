58bcb07b6f016a4f7734041af2db5c839e1fa71a
Martin Fick <mogulguy10@gmail.com>
2015-11-09 16:33:19 -0700

    Define missing Change API events
    Core gerrit has defined a place for events which are to be exposed
    as an API, and newer events have often been defined there.  Gerrit also
    has older events that it fires for most important things which were
    designed for the stream events.  Define newer events in the API
    to mirror the older events that were only defined for stream events.
    Use the new API objects (ChangeInfo...) when appropriate in these newer
    events.
    
    If we want to move the hooks and stream events to plugins, we want them
    to listen to modern API events. This change helps define these modern
    events.
    
    Change-Id: Ic1fe0743d048707a9a28f467b249dab70836f79c

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/events/ChangeRestoredListener.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/events/ChangeRestoredListener.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 The Android Open Source Project
+// Copyright (C) 2015 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,15 +15,15 @@
 package com.google.gerrit.extensions.events;
 
 import com.google.gerrit.extensions.annotations.ExtensionPoint;
+import com.google.gerrit.extensions.common.AccountInfo;
 
-
-/** Notified whenever a project is created on the master. */
+/** Notified whenever a Change is restored. */
 @ExtensionPoint
-public interface NewProjectCreatedListener {
-  interface Event {
-    String getProjectName();
-    String getHeadName();
+public interface ChangeRestoredListener {
+  interface Event extends RevisionEvent {
+    AccountInfo getRestorer();
+    String getReason();
   }
 
-  void onNewProjectCreated(Event event);
+  void onChangeRestored(Event event);
 }

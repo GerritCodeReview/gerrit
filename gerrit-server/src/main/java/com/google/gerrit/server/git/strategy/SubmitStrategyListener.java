37abbd10614e2e0e0e52a1b15ac68bbbe82338fc
Martin Fick <mogulguy10@gmail.com>
2015-11-10 14:53:43 -0700

    Make hooks/events listen to the new API Events.
    Create a ChangeHookApiListener to listen to the new API Events and
    forward these events to the old legacy ChangeHookRunner.  This decouples
    the hooks and stream events from the internal events.  This also
    decouples the ChangeHooks interface (and implementations) from all
    internal classes except for the new ChangeHookApiListener.  This
    decoupling should now make it very easy to move all of these classes to
    plugins.
    
    Change-Id: I9153dca45727f1c7ea10a8d003e1acf2171b39ac

--- gerrit-server/src/main/java/com/google/gerrit/server/git/strategy/SubmitStrategyListener.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/strategy/SubmitStrategyListener.java
@@ -18,7 +18,6 @@ import com.google.common.base.CharMatcher;
 import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.extensions.api.changes.SubmitInput;
 import com.google.gerrit.extensions.restapi.ResourceConflictException;
-import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.server.change.Submit.TestSubmitInput;
 import com.google.gerrit.server.git.BatchUpdate;
@@ -70,12 +69,11 @@ public class SubmitStrategyListener extends BatchUpdate.Listener {
       if (args.mergeTip.getCurrentTip().equals(args.mergeTip.getInitialTip())) {
         continue;
       }
-      Account account =
-          args.accountCache.get(args.caller.getAccountId()).getAccount();
-      args.hooks.doRefUpdatedHook(args.destBranch,
+      args.refUpated.fire(args.project.getProject().getNameKey(),
+          args.destBranch.get(),
           args.mergeTip.getInitialTip(),
           args.mergeTip.getCurrentTip(),
-          account);
+          args.accountCache.get(args.caller.getAccountId()).getAccount());
     }
   }
 

3af33e7045923158ac1d46bc572c610e7b3eaa22
Shawn Pearce <sop@google.com>
2015-11-10 15:22:11 -0800

    Export metrics through REST API
    Make metrics and metadata available to users with viewCaches
    capability using the REST API:
    
    $ curl http://127.0.0.1:8080/config/server/metrics
    )]}'
    {
      "change/query/query_latency": {
        "description": "Successful change query latency, accumulated over the life of the process",
        "unit": "milliseconds",
        "cumulative": true,
        "count": 4,
        "rate_1m": 0.05423880917494861,
        "rate_5m": 0.01279022895425833,
        "rate_15m": 0.004383182198520226,
        "p50": 326000.0,
        "p75": 1.2924E7,
        "p95": 2.5574E7,
        "p98": 2.5574E7,
        "p99": 2.5574E7,
        "p99_9": 2.5574E7,
        "min": 316000,
        "max": 25574000,
        "std_dev": 1.0360843540142475E7
      },
    
    This could be used to build a monitoring system entirely external from
    Gerrit that scrapes the REST API.
    
    Change-Id: I24760c386b40cb2af8dcfb36a5b9b0b41162ca25

---
+++ gerrit-server/src/main/java/com/google/gerrit/metrics/dropwizard/GetMetric.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.metrics.dropwizard;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.CurrentUser;
+import com.google.inject.Inject;
+
+import org.kohsuke.args4j.Option;
+
+class GetMetric implements RestReadView<MetricResource> {
+  private final CurrentUser user;
+  private final DropWizardMetricMaker metrics;
+
+  @Option(name = "--data-only", usage = "return only values")
+  boolean dataOnly;
+
+  @Inject
+  GetMetric(CurrentUser user, DropWizardMetricMaker metrics) {
+    this.user = user;
+    this.metrics = metrics;
+  }
+
+  @Override
+  public MetricJson apply(MetricResource resource) throws AuthException {
+    if (!user.getCapabilities().canViewCaches()) {
+      throw new AuthException("restricted to viewCaches");
+    }
+    return new MetricJson(
+        resource.getMetric(),
+        metrics.getAnnotations(resource.getName()),
+        dataOnly);
+  }
+}

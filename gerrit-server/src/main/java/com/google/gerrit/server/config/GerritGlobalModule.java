cc9d5cefcfd7c6a2d55d8ffd3a0edfea1cf8d86f
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2015-11-11 14:30:46 -0500

    Create change-indexed extension point
    Add a new extension point so that plugins can be notified when a change
    has been indexed or removed from the secondary index.
    
    This allows the new Sync-index plugin to keep the secondary index of two
    Gerrit instances in sync. Listening to existing change/patch events
    cause the plugin to miss other cases where index updating is done, e.g.,
    deleted drafts or changes reindexed to update their mergeability status.
    
    Change-Id: I87de17de26e907d77b2fb5175b5c90c5dd0381e9

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -79,6 +79,7 @@ import com.google.gerrit.server.change.ChangeKindCacheImpl;
 import com.google.gerrit.server.change.MergeabilityCacheImpl;
 import com.google.gerrit.server.events.EventFactory;
 import com.google.gerrit.server.events.EventsMetrics;
+import com.google.gerrit.server.extensions.events.ChangeIndexedListener;
 import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
 import com.google.gerrit.server.git.BatchUpdate;
 import com.google.gerrit.server.git.EmailMerge;
@@ -272,6 +273,7 @@ public class GerritGlobalModule extends FactoryModule {
     DynamicSet.setOf(binder(), ReceivePackInitializer.class);
     DynamicSet.setOf(binder(), PostReceiveHook.class);
     DynamicSet.setOf(binder(), PreUploadHook.class);
+    DynamicSet.setOf(binder(), ChangeIndexedListener.class);
     DynamicSet.setOf(binder(), NewProjectCreatedListener.class);
     DynamicSet.setOf(binder(), ProjectDeletedListener.class);
     DynamicSet.setOf(binder(), GarbageCollectorListener.class);

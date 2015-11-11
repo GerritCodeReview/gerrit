b096c305d00027085a112176d3905d931bcaf07d
David Pursehouse <david.pursehouse@sonymobile.com>
2015-11-11 15:34:12 -0800

    RestApiServlet: Metrics for REST API responses
    Create a helper class, RequestMetrics, which gets a MetricMaker injected
    and uses it to create metrics for REST API responses.
    
    Add a RequestMetricsFilter which filters all requests and sends metrics
    for successful and error responses.
    
    Change-Id: Ib8da47b7f80c6f7ccb4206157e7f5139c099cfdd

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/Daemon.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/Daemon.java
@@ -28,6 +28,7 @@ import com.google.gerrit.httpd.GitOverHttpModule;
 import com.google.gerrit.httpd.H2CacheBasedWebSession;
 import com.google.gerrit.httpd.HttpCanonicalWebUrlProvider;
 import com.google.gerrit.httpd.RequestContextFilter;
+import com.google.gerrit.httpd.RequestMetricsFilter;
 import com.google.gerrit.httpd.RequireSslFilter;
 import com.google.gerrit.httpd.WebModule;
 import com.google.gerrit.httpd.WebSshGlueModule;
@@ -445,6 +446,7 @@ public class Daemon extends SiteProgram {
     }
     modules.add(RequestContextFilter.module());
     modules.add(AllRequestFilter.module());
+    modules.add(RequestMetricsFilter.module());
     modules.add(H2CacheBasedWebSession.module());
     modules.add(sysInjector.getInstance(GitOverHttpModule.class));
     modules.add(sysInjector.getInstance(WebModule.class));

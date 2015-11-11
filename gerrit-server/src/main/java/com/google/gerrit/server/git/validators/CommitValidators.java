73cb5b93206c88a59b36bc40d338fa6a0565f7b5
David Ostrovsky <david@ostrovsky.org>
2015-11-11 09:01:02 -0800

    ChangeHookValidator: Fix old revision retrieval for root commit
    Bug: Issue 3666
    Change-Id: Ib8ce3d69dc3dfce3ea9aec9315315f266c589eef

--- gerrit-server/src/main/java/com/google/gerrit/server/git/validators/CommitValidators.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/validators/CommitValidators.java
@@ -551,8 +551,10 @@ public class CommitValidators {
         IdentifiedUser user = (IdentifiedUser) refControl.getCurrentUser();
 
         String refname = receiveEvent.refName;
-        ObjectId old = receiveEvent.commit.getParent(0);
-
+        ObjectId old = ObjectId.zeroId();
+        if (receiveEvent.commit.getParentCount() > 0) {
+          old = receiveEvent.commit.getParent(0);
+        }
         if (receiveEvent.command.getRefName().startsWith(REFS_CHANGES)) {
           /*
            * If the ref-update hook tries to distinguish behavior between pushes to

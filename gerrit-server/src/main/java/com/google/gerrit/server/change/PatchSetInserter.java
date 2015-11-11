1f7d71122f54c775b9d1beb0ca377faaa3277d69
Saša Živkov <sasa.zivkov@sap.com>
2015-11-11 14:50:46 -0800

    New event listeners and new event types
    This is a first step towards moving ChangeHooks into a plugin.  The
    listeners[1] are now invoked from all places where corresponding
    hooks are invoked.  Invocation of hooks is not removed in this change.
    This will happen when we make sure that they are invoked from a new
    event listener.
    
    [1] All listeners under:
    gerrit-extension-api/src/main/java/com/google/gerrit/extensions/events/
    
    Change-Id: I6e5356edb2c326d2d7a0133b84509ad6071140d0

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PatchSetInserter.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PatchSetInserter.java
@@ -33,6 +33,7 @@ import com.google.gerrit.server.ChangeUtil;
 import com.google.gerrit.server.PatchSetUtil;
 import com.google.gerrit.server.ReviewerSet;
 import com.google.gerrit.server.events.CommitReceivedEvent;
+import com.google.gerrit.server.extensions.events.RevisionCreated;
 import com.google.gerrit.server.git.BanCommit;
 import com.google.gerrit.server.git.BatchUpdate;
 import com.google.gerrit.server.git.BatchUpdate.ChangeContext;
@@ -77,6 +78,7 @@ public class PatchSetInserter extends BatchUpdate.Op {
   private final ReviewDb db;
   private final CommitValidators.Factory commitValidatorsFactory;
   private final ReplacePatchSetSender.Factory replacePatchSetFactory;
+  private final RevisionCreated revisionCreated;
   private final ApprovalsUtil approvalsUtil;
   private final ApprovalCopier approvalCopier;
   private final ChangeMessagesUtil cmUtil;
@@ -116,6 +118,7 @@ public class PatchSetInserter extends BatchUpdate.Op {
       CommitValidators.Factory commitValidatorsFactory,
       ReplacePatchSetSender.Factory replacePatchSetFactory,
       PatchSetUtil psUtil,
+      RevisionCreated revisionCreated,
       @Assisted RefControl refControl,
       @Assisted PatchSet.Id psId,
       @Assisted RevCommit commit) {
@@ -128,6 +131,7 @@ public class PatchSetInserter extends BatchUpdate.Op {
     this.commitValidatorsFactory = commitValidatorsFactory;
     this.replacePatchSetFactory = replacePatchSetFactory;
     this.psUtil = psUtil;
+    this.revisionCreated = revisionCreated;
 
     this.refControl = refControl;
     this.psId = psId;
@@ -272,6 +276,7 @@ public class PatchSetInserter extends BatchUpdate.Op {
     }
 
     if (runHooks) {
+      revisionCreated.fire(change, patchSet, ctx.getUser().getAccountId());
       hooks.doPatchsetCreatedHook(change, patchSet, ctx.getDb());
     }
   }

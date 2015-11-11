49fe9d9303ae340d5b06b82c0c848d9dc7923881
David Pursehouse <david.pursehouse@sonymobile.com>
2015-11-11 15:34:12 -0800

    RestApiServlet: Metrics for REST API responses
    Create a helper class, RequestMetrics, which gets a MetricMaker injected
    and uses it to create metrics for REST API responses.
    
    Add a RequestMetricsFilter which filters all requests and sends metrics
    for successful and error responses.
    
    Change-Id: Ib8da47b7f80c6f7ccb4206157e7f5139c099cfdd

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/RequestMetrics.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd;
+
+import com.google.gerrit.metrics.Counter1;
+import com.google.gerrit.metrics.Description;
+import com.google.gerrit.metrics.Field;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class RequestMetrics {
+  public final Counter1<Integer> errors;
+  public final Counter1<Integer> successes;
+
+  @Inject
+  public RequestMetrics(MetricMaker metricMaker) {
+    errors = metricMaker.newCounter(
+        "rest/responses/errors",
+        new Description("Rate of REST API error responses")
+          .setRate()
+          .setUnit("errors"),
+        Field.ofInteger(
+            "status", "HTTP status code"));
+    successes = metricMaker.newCounter(
+        "rest/responses/successes",
+        new Description("Rate of REST API success responses")
+          .setRate()
+          .setUnit("successes"),
+        Field.ofInteger(
+            "status", "HTTP status code"));
+  }
+}

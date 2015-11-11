4c020965aa8a738d02ea2dcb127610196f8f570e
Edwin Kempin <ekempin@google.com>
2015-11-11 12:22:40 -0800

    Factor out an AccountInfoComparator class to avoid code duplication
    Add a new class that implements a comparator for AccountInfo that can
    be used in multiples places.
    
    Change-Id: If6fd06585e804ec1339f70d4928b87d9ac8cfa98
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/api/accounts/AccountInfoComparator.java
@@ -0,0 +1,32 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.api.accounts;
+
+import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.Ordering;
+import com.google.gerrit.extensions.common.AccountInfo;
+
+import java.util.Comparator;
+
+public class AccountInfoComparator implements Comparator<AccountInfo> {
+  @Override
+  public int compare(AccountInfo a, AccountInfo b) {
+    return ComparisonChain.start()
+        .compare(a.name, b.name, Ordering.natural().nullsFirst())
+        .compare(a.email, b.email, Ordering.natural().nullsFirst())
+        .compare(a._accountId, b._accountId, Ordering.natural().nullsFirst())
+        .result();
+  }
+}

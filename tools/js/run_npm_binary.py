10e031a6743a25466eb9f4897e37544701847766
Dave Borowitz <dborowitz@google.com>
2015-11-12 15:44:08 -0500

    Build bower_components with buck
    Add support for downloading npm binaries including dependencies and
    running them in buck genrules. In npm land, transitive dependencies
    are generally included in the package distribution, and there are a
    *lot* of them. Since we aren't redistributing these binaries and
    they're only part of the build process, we don't have to worry too
    much about licensing, only that they don't have anything totally
    crazy.
    
    We assume packages have a certain format and we can detect the binary
    to run from the genrule output filename. Actually running the binary
    is tricky as well, since we have to extract it first. But it might be
    large, so we don't want to extract it on every invocation; and naive
    extraction to a common location (in buck-out) is racy. So we need a
    custom extractor scheme using atomic rename to make this work.
    
    Download bower as an npm package and use it to download bower
    packages. Bower packages can come from a variety of sources, usually
    git repositories, so we can't simply use download_file. There is
    additional logic in bower to read bower.json and strip out unneeded
    files, so I didn't want to get into reimplementing that. The tricky
    thing about bower is convincing it to avoid transitive dependencies so
    we can let Buck handle parallelism and caching. To do this, we need to
    read the package information from the upstream bower repository, and
    explicitly ignore all listed dependencies when downloading.
    
    We combine the flattened list of bower packages in a single
    bower_components rule. It would be nice to have deps of each
    bower_component so we didn't need to flatten these, but Buck genrules
    don't have deps so this is a nonstarter. Considering we only expect to
    have a single bower_components for the whole project, hopefully this
    is not too onerous.
    
    This change just gets us the bower_components directory. We still have
    some work to do to use this from Gerrit. Plus even more work to
    replace the gulpfile and actually package this stuff together into a
    compiled JS app for the war distribution.
    
    Change-Id: Id277d2d812ffcc3bce87ff00b5894bacdffc038e

---
+++ tools/js/run_npm_binary.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# Copyright (C) 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import atexit
+import hashlib
+import os
+import shutil
+import sys
+import tarfile
+import tempfile
+
+
+def hash_file(p):
+  d = hashlib.sha1()
+  with open(p, 'rb') as f:
+    while True:
+      b = f.read(8192)
+      if not b:
+        break
+      d.update(b)
+  return d.hexdigest()
+
+
+def extract(path, outdir, bin):
+  if os.path.exists(os.path.join(outdir, bin)):
+    return # Another process finished extracting, ignore.
+
+  # Use a temp directory adjacent to outdir so shutil.move can use the same
+  # device atomically.
+  tmpdir = tempfile.mkdtemp(dir=os.path.dirname(outdir))
+  def cleanup():
+    try:
+      shutil.rmtree(tmpdir)
+    except OSError:
+      pass # Too late now
+  atexit.register(cleanup)
+
+  def extract_one(mem):
+    dest = os.path.join(outdir, mem.name)
+    tar.extract(mem, path=tmpdir)
+    try:
+      os.makedirs(os.path.dirname(dest))
+    except OSError:
+      pass # Either exists, or will fail on the next line.
+    shutil.move(os.path.join(tmpdir, mem.name), dest)
+
+  with tarfile.open(path, 'r:gz') as tar:
+    for mem in tar.getmembers():
+      if mem.name != bin:
+        extract_one(mem)
+    # Extract bin last so other processes only short circuit when extraction is
+    # finished.
+    extract_one(tar.getmember(bin))
+
+
+def main(args):
+  path = args[0]
+  suffix = '.npm_binary.tgz'
+  tgz = os.path.basename(path)
+  parts = tgz[:-len(suffix)].split('@')
+
+  if not tgz.endswith(suffix) or len(parts) != 2:
+    print('usage: %s <path/to/npm_binary>' % sys.argv[0], file=sys.stderr)
+    return 1
+
+  name, version = parts
+  outdir = '%s-%s' % (path[:-len(suffix)], hash_file(path))
+  rel_bin = os.path.join('package', 'bin', name)
+  bin = os.path.join(outdir, rel_bin)
+  if not os.path.isfile(bin):
+    extract(path, outdir, rel_bin)
+
+  os.execv(bin, args[1:])
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))

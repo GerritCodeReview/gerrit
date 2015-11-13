3e84b0b283d71b68188fd99ad9e9e98a755ef332
David Pursehouse <david.pursehouse@sonymobile.com>
2015-11-13 00:24:47 -0800

    Organize imports
    Change-Id: I2ad2a5597542461cc221800bdeab7f8bd09fd8c1

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/restapi/RestApiMetrics.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/restapi/RestApiMetrics.java
@@ -19,11 +19,11 @@ import com.google.gerrit.httpd.restapi.RestApiServlet.ViewData;
 import com.google.gerrit.metrics.Counter1;
 import com.google.gerrit.metrics.Counter2;
 import com.google.gerrit.metrics.Description;
+import com.google.gerrit.metrics.Description.Units;
 import com.google.gerrit.metrics.Field;
 import com.google.gerrit.metrics.Histogram1;
 import com.google.gerrit.metrics.MetricMaker;
 import com.google.gerrit.metrics.Timer1;
-import com.google.gerrit.metrics.Description.Units;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 

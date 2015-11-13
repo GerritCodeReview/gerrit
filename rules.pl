a10e4489b2b5439e91553d5ec747fc5bd4ae4626
Nasser Grainawi <nasser@grainawi.org>
2015-11-13 01:05:19 +0000

    Update prolog rules to require Verified-Notedb label
    Change-Id: I069989bda5d5c45b13681d4e2b1c61bfbabbd20d

--- rules.pl
+++ rules.pl
@@ -1,15 +1,21 @@
-submit_rule(submit(CR, V, L)) :-
+submit_rule(submit(CR, V, L, N)) :-
   needs_library_compliance,
   !,
-  base(CR, V),
+  base(CR, V, N),
   gerrit:max_with_block(-1, 1, 'Library-Compliance', L).
 
-submit_rule(submit(CR, V)) :-
-  base(CR, V).
+submit_rule(submit(CR, V, N)) :-
+  base(CR, V, N).
 
-base(CR, V) :-
+base(CR, V, N) :-
   gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-  gerrit:max_with_block(-1, 1, 'Verified', V).
+  max_no_block(1, 'Verified', V),
+  max_no_block(1, 'Verified-Notedb', N).
 
 needs_library_compliance :- gerrit:commit_delta('^lib/'), !.
 needs_library_compliance :- gerrit:commit_delta('^[.]buckversion$'), !.
+
+max_no_block(Max, Label, label(Label, S)) :-
+  number(Max), atom(Label),
+  !,
+  gerrit:max_no_block(Label, Max, S).

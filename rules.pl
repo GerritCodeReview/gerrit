1749662a4987beb95e7ff08ab786dcab7dcad61f
Nasser Grainawi <nasser@grainawi.org>
2015-11-13 01:37:39 +0000

    Fix Verified label to use max_no_block
    This is what the label has in project.config, but the prolog rule overrides that.
    
    Change-Id: I6148ec56eb812e448d756ec91fe9c6a2e8bb39dc

--- rules.pl
+++ rules.pl
@@ -9,7 +9,7 @@ submit_rule(submit(CR, V)) :-
 
 base(CR, V) :-
   gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-  gerrit:max_with_block(-1, 1, 'Verified', V).
+  gerrit:max_no_block('Verified', 1, V).
 
 needs_library_compliance :- gerrit:commit_delta('^lib/'), !.
 needs_library_compliance :- gerrit:commit_delta('^[.]buckversion$'), !.

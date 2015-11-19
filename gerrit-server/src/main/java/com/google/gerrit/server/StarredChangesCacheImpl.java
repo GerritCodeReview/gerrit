3df9cb13105d090d1bf479decbf95a0fc844ee89
Edwin Kempin <ekempin@google.com>
2015-11-19 14:03:41 -0800

    Notedb: Add cache for starred changes to make lookup by change faster
    Within notedb each change that is starred by a user is tracked by a
    refs/starred-changes/XX/YYYY/ZZZZ ref in the All-Users meta repository
    where XX/YYYY is the sharded account ID and ZZZZ is the change ID.
    With this storage format finding all changes that are starred by a
    user is cheap because we just need to list all refs that have
    refs/starred-changes/XX/YYYY/ as prefix and looking up refs with a
    prefix that end on '/' is optimized in jgit. However to find all
    accounts that have starred a change we must scan the complete
    refs/starred-changes/ namespace and check which of the refs ends with
    the change ID. This results in bad performance when there are many
    starred changes.
    
    To serve all use cases we must be able to lookup starred changes both
    by account and by change. Lookup by account is needed for querying
    changes (to filter changes if the 'is:starred' operator is used and to
    populate the 'starred' field for the changes in the result set).
    Lookup by change is needed to send email notifications for users that
    have starred a change and to remove all stars on a change when a draft
    change gets deleted.
    
    With the ref format we can only optimize for one case either lookup by
    account or lookup by change. Hence we add a new cache that supports
    lookup by both account and change. With the cache the
    refs/starred-changes/ refs are only iterated once and once the cache
    is built up Gerrit can keep it updated. Updates to the cache are
    synchronized. Only when star information is changed outside of Gerrit
    the cache must be rebuilt, this can be done by flushing the cache.
    
    There is an interface for the new cache and the cache implementation
    is bound via a DynamicItem. This allows plugins to exchange the cache
    implementation. E.g. one could implement an H2-based cache when the
    number of starred changes grows too big to be kept in memory, or one
    could implement an MySQL-based cache with replication if Gerrit should
    run in a multi master setup. The default cache is only loaded if no
    plugin provides a StarredChangesChange.
    
    A new schema version is needed for Google since a new method has been
    added to the StarredChangeAccess interface and Google has a custom
    implementation of this interface that needs to be adapted.
    
    Change-Id: I80237577b006e8f3525c98b153c80039528aee3a
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/StarredChangesCacheImpl.java
@@ -0,0 +1,236 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server;
+
+import static com.google.inject.Scopes.SINGLETON;
+
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.RefNames;
+import com.google.gerrit.reviewdb.client.StarredChange;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.cache.CacheModule;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.notedb.NotesMigration;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Names;
+
+import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+
+@Singleton
+public class StarredChangesCacheImpl implements StarredChangesCache {
+  private static final Logger log =
+      LoggerFactory.getLogger(StarredChangesCacheImpl.class);
+
+  private static final String CACHE_NAME = "starred_changes";
+
+  public static Module module() {
+    return new CacheModule() {
+      @Override
+      protected void configure() {
+        bind(StarredChangesChacheProvider.class).in(SINGLETON);
+      }
+    };
+  }
+
+  public static class StarredChangesChacheProvider
+      implements Provider<StarredChangesCache> {
+
+    private final DynamicItem<StarredChangesCache> starredChangesCache;
+    private final Injector sysInjector;
+
+    private StarredChangesCache defaultStarredChangesCache;
+
+    @Inject
+    StarredChangesChacheProvider(
+        DynamicItem<StarredChangesCache> starredChangesCache,
+        Injector sysInjector) {
+      this.starredChangesCache = starredChangesCache;
+      this.sysInjector = sysInjector;
+    }
+
+    @Override
+    public StarredChangesCache get() {
+      StarredChangesCache cache = starredChangesCache.get();
+      if (cache != null) {
+        // starred-changes cache provided by plugin
+        return cache;
+      }
+
+      if (defaultStarredChangesCache == null) {
+        // create default starred-changes cache
+        defaultStarredChangesCache = new StarredChangesCacheImpl(
+            sysInjector.createChildInjector(new CacheModule() {
+              @Override
+              protected void configure() {
+                persist(CACHE_NAME, CacheKey.class,
+                    new TypeLiteral<HashMultimapPair<Account.Id, Change.Id>>() {})
+                        .maximumWeight(1).loader(Loader.class);
+              }
+            }).getInstance(Key.get(
+                new TypeLiteral<LoadingCache<CacheKey,
+                    HashMultimapPair<Account.Id, Change.Id>>>() {},
+                Names.named(CACHE_NAME))));
+      }
+      return defaultStarredChangesCache;
+    }
+  }
+
+  private final LoadingCache<CacheKey,
+      HashMultimapPair<Account.Id, Change.Id>> cache;
+
+  private StarredChangesCacheImpl(
+      LoadingCache<CacheKey, HashMultimapPair<Account.Id, Change.Id>> cache) {
+    this.cache = cache;
+  }
+
+  @Override
+  public boolean isStarred(Account.Id accountId, Change.Id changeId) {
+    try {
+      return all().contains(accountId, changeId);
+    } catch (ExecutionException e) {
+      log.warn(String.format("Cannot lookup star for change %d by account %d",
+          changeId.get(), accountId.get()), e);
+      return false;
+    }
+  }
+
+  @Override
+  public Iterable<Change.Id> byAccount(Account.Id accountId) {
+    try {
+      return all().getByFirstKey(accountId);
+    } catch (ExecutionException e) {
+      log.warn(String.format("Cannot lookup stars by account %d",
+          accountId.get()), e);
+      return Collections.emptySet();
+    }
+  }
+
+  @Override
+  public Iterable<Account.Id> byChange(Change.Id changeId) {
+    try {
+      return all().getBySecondKey(changeId);
+    } catch (ExecutionException e) {
+      log.warn(String.format("Cannot lookup stars for change %d",
+          changeId.get()), e);
+      return Collections.emptySet();
+    }
+  }
+
+  @Override
+  public void star(Account.Id accountId, Change.Id changeId) {
+    try {
+      all().put(accountId, changeId);
+    } catch (ExecutionException e) {
+      log.warn(String.format("Failed to star change %d by account %d",
+          changeId.get(), accountId.get()), e);
+    }
+  }
+
+  @Override
+  public void unstar(final Account.Id accountId, final Change.Id changeId) {
+    try {
+      all().remove(accountId, changeId);
+    } catch (ExecutionException e) {
+      log.warn(String.format("Failed to unstar change %d by account %d",
+          changeId.get(), accountId.get()), e);
+    }
+  }
+
+  @Override
+  public Iterable<Account.Id> unstarAll(final Change.Id changeId) {
+    try {
+      return all().removeAllBySecondKey(changeId);
+    } catch (ExecutionException e) {
+      log.warn(String.format("Failed to unstar change %d by all accounts",
+          changeId.get()), e);
+      return Collections.emptySet();
+    }
+  }
+
+  private HashMultimapPair<Account.Id, Change.Id> all()
+      throws ExecutionException {
+    return cache.get(CacheKey.ALL);
+  }
+
+  private static class CacheKey implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    static final CacheKey ALL = new CacheKey();
+
+    private CacheKey() {
+    }
+  }
+
+  private static class Loader extends
+      CacheLoader<CacheKey, HashMultimapPair<Account.Id, Change.Id>> {
+    private final NotesMigration migration;
+    private final Provider<ReviewDb> dbProvider;
+    private final GitRepositoryManager repoManager;
+    private final AllUsersName allUsers;
+
+    @Inject
+    Loader(
+        NotesMigration migration,
+        Provider<ReviewDb> dbProvider,
+        GitRepositoryManager repoManager,
+        AllUsersName allUsers) {
+      this.migration = migration;
+      this.dbProvider = dbProvider;
+      this.repoManager = repoManager;
+      this.allUsers = allUsers;
+    }
+
+    @Override
+    public HashMultimapPair<Account.Id, Change.Id> load(CacheKey key)
+        throws Exception {
+      HashMultimapPair<Account.Id, Change.Id> t = HashMultimapPair.create();
+      if (!migration.readChanges()) {
+        for (StarredChange starredChange :
+            dbProvider.get().starredChanges().all()) {
+          t.put(
+              starredChange.getAccountId(),
+              starredChange.getChangeId());
+        }
+      } else {
+        try (Repository repo = repoManager.openMetadataRepository(allUsers)) {
+          for (String refPart : repo.getRefDatabase()
+              .getRefs(RefNames.REFS_STARRED_CHANGES).keySet()) {
+            t.put(
+                Account.Id.fromRefPart(refPart),
+                Change.Id.fromRefPart(refPart));
+          }
+        }
+      }
+      return t;
+    }
+  }
+}

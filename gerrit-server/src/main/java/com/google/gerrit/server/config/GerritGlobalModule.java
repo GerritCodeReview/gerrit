bccd6f9582559df30155723107eafefccc1d3d64
Edwin Kempin <ekempin@google.com>
2015-11-19 14:03:41 -0800

    Notedb: Add cache for starred changes to make lookup by change faster
    Within notedb each change that is starred by a user is tracked by a
    refs/starred-changes/XX/YYYY/ZZZZ ref in the All-Users meta repository
    where XX/YYYY is the sharded account ID and ZZZZ is the change ID.
    With this storage format finding all changes that are starred by a
    user is cheap because we just need to list all refs that have
    refs/starred-changes/XX/YYYY/ as prefix and looking up refs with a
    prefix that end on '/' is optimized in jgit. However to find all
    accounts that have starred a change we must scan the complete
    refs/starred-changes/ namespace and check which of the refs ends with
    the change ID. This results in bad performance when there are many
    starred changes.
    
    To serve all use cases we must be able to lookup starred changes both
    by account and by change. Lookup by account is needed for querying
    changes (to filter changes if the 'is:starred' operator is used and to
    populate the 'starred' field for the changes in the result set).
    Lookup by change is needed to send email notifications for users that
    have starred a change and to remove all stars on a change when a draft
    change gets deleted.
    
    With the ref format we can only optimize for one case either lookup by
    account or lookup by change. Hence we add an in-memory cache that
    supports lookup by both account and change. With the cache the
    refs/starred-changes/ refs are only iterated once on server startup.
    Gerrit then keep the cache updated while it is running.
    
    There is an interface for the new cache and the cache implementation
    is bound via a DynamicItem. This allows plugins to exchange the cache
    implementation. E.g. one could implement an H2-based cache when the
    number of starred changes grows too big to be kept in memory, or one
    could implement an MySQL-based cache with replication if Gerrit should
    run in a multi master setup. The default cache is only loaded if no
    plugin provides a StarredChangesChange.
    
    A new schema version is needed for Google since a new method has been
    added to the StarredChangeAccess interface and Google has a custom
    implementation of this interface that needs to be adapted.
    
    Change-Id: I80237577b006e8f3525c98b153c80039528aee3a
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -51,6 +51,7 @@ import com.google.gerrit.server.ApprovalsUtil;
 import com.google.gerrit.server.CmdLineParserModule;
 import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.PluginUser;
+import com.google.gerrit.server.StarredChangesCache;
 import com.google.gerrit.server.account.AccountByEmailCacheImpl;
 import com.google.gerrit.server.account.AccountCacheImpl;
 import com.google.gerrit.server.account.AccountControl;
@@ -298,6 +299,7 @@ public class GerritGlobalModule extends FactoryModule {
     DynamicSet.setOf(binder(), ProjectWebLink.class);
     DynamicSet.setOf(binder(), BranchWebLink.class);
     DynamicMap.mapOf(binder(), OAuthLoginProvider.class);
+    DynamicItem.itemOf(binder(), StarredChangesCache.class);
 
     factory(UploadValidators.Factory.class);
     DynamicSet.setOf(binder(), UploadValidationListener.class);

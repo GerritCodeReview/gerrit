fcd8d1fd6f49eddf64c0cd5f6f6ae4cebf93e05e
Andrew Bonventre <andybons@chromium.org>
2015-11-25 17:04:33 -0500

    Scaffold for user's logged-in dashboard
    This change introduces a dependency of the routing behavior on
    whether the user is logged in. Since the WebComponentsReady event
    can fire before OR after the account info is retrieved, a Promise
    is used to ensure deterministic ordering via `accountReady`.
    
    There is also some minor cleanup of which element is being used
    as the global `app` object.
    
    Change-Id: I9768f2eabd5cdb7c62ead16ae97df5c1c321eaf6

--- polygerrit-ui/app/scripts/app.js
+++ polygerrit-ui/app/scripts/app.js
@@ -12,61 +12,75 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-(function(document) {
-  'use strict';
+'use strict';
 
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // Middleware
-    page(function(ctx, next) {
-      document.body.scrollTop = 0;
-      next();
-    });
+// Polymer makes `app` intrinsically defined on the window by virtue of the
+// custom element having the id "app", but it is made explicit here.
+var app = document.querySelector('#app');
 
-    // Routes.
-    page('/', function() {
-      page.redirect('/q/status:open');
-    });
-
-    function queryHandler(data) {
-      app.route = 'gr-change-list';
-      app.params = data.params;
-    }
-
-    page('/q/:query,:offset', queryHandler);
-    page('/q/:query', queryHandler);
-
-    page(/^\/(\d+)\/?/, function(ctx) {
-      page.redirect('/c/' + ctx.params[0]);
-    });
-
-    page('/c/:changeNum', function(data) {
-      app.route = 'gr-change-view';
-      app.params = data.params;
-    });
-
-    page(/^\/c\/(\d+)\/((\d+)(\.\.(\d+))?)\/(.+)/, function(ctx) {
-      app.route = 'gr-diff-view';
-      var params = {
-        changeNum: ctx.params[0],
-        basePatchNum: ctx.params[2],
-        patchNum: ctx.params[4],
-        path: ctx.params[5]
-      };
-      // Don't allow diffing the same patch number against itself because WHY?
-      if (params.basePatchNum == params.patchNum) {
-        page.redirect('/c/' + params.changeNum + '/' + params.patchNum + '/' +
-            params.path);
-        return;
-      }
-      if (!params.patchNum) {
-        params.patchNum = params.basePatchNum;
-        delete(params.basePatchNum);
-      }
-      app.params = params;
-    });
-
-    page.start();
+window.addEventListener('WebComponentsReady', function() {
+  // Middleware
+  page(function(ctx, next) {
+    document.body.scrollTop = 0;
+    next();
   });
 
-})(document);
+  // Routes.
+  page('/', function() {
+    if (app.loggedIn) {
+      page.redirect('/dashboard/self');
+    } else {
+      page.redirect('/q/status:open');
+    }
+  });
+
+  page('/dashboard/*', function(data) {
+    app.route = 'gr-dashboard-view';
+    app.params = data.params;
+  });
+
+  function queryHandler(data) {
+    app.route = 'gr-change-list-view';
+    app.params = data.params;
+  }
+
+  page('/q/:query,:offset', queryHandler);
+  page('/q/:query', queryHandler);
+
+  page(/^\/(\d+)\/?/, function(ctx) {
+    page.redirect('/c/' + ctx.params[0]);
+  });
+
+  page('/c/:changeNum', function(data) {
+    app.route = 'gr-change-view';
+    app.params = data.params;
+  });
+
+  page(/^\/c\/(\d+)\/((\d+)(\.\.(\d+))?)\/(.+)/, function(ctx) {
+    app.route = 'gr-diff-view';
+    var params = {
+      changeNum: ctx.params[0],
+      basePatchNum: ctx.params[2],
+      patchNum: ctx.params[4],
+      path: ctx.params[5]
+    };
+    // Don't allow diffing the same patch number against itself because WHY?
+    if (params.basePatchNum == params.patchNum) {
+      page.redirect('/c/' + params.changeNum + '/' + params.patchNum + '/' +
+          params.path);
+      return;
+    }
+    if (!params.patchNum) {
+      params.patchNum = params.basePatchNum;
+      delete(params.basePatchNum);
+    }
+    app.params = params;
+  });
+
+  // Since routing depends on account information being retrieved in order to
+  // check whether the user is logged in, it can't be started until the
+  // accountReady promise is fulfilled.
+  app.accountReady.then(function() {
+    page.start();
+  });
+});

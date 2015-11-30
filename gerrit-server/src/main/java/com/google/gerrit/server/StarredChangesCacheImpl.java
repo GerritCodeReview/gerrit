967027a406553ab973298b7bd1ec09581dbee0bc
Edwin Kempin <ekempin@google.com>
2015-11-30 14:58:11 +0100

    Support labeled stars in notedb and starred changes cache
    At the moment Gerrit supports only one kind of star on changes, either
    a change is starred by a user or it is not. For some use cases users
    want to put different stars on changes. E.g. issue 1144 requests
    colored stars (yellow, red, blue) and issue 2576 requests an ignore
    star. To support this in the future we need to be able to store
    labeled stars. This change extends the star storage format in notedb
    so that labels can be applied to changes that are starred by a user.
    In notedb we have a refs/starred-changes/XX/YYYY/ZZZZ ref in the
    All-User meta repository to track that a change ZZZZ was starred by a
    user XX/YYYY (sharded account ID). So far this ref points to an empty
    tree object, since the existence of the reference was enough to know
    that the starred was changed. Now the ref will point to an object blob
    that contains the list of labels that the user applied to this change.
    The StarredChangesCache is extended to store the labels with the
    starred changes and it provides methods to access the stars with
    labels.
    
    This change is only the first step to support labeled stars. It
    doesn't implement any way to get/set/delete/query/display labeled
    stars. This will be implemented by follow-up changes.
    
    Labeled stars will only be supported with notedb, but not with the
    current database backend.
    
    Labeled stars behave like private hashtags. A user can apply any label
    to a change, but these labels are only visible to that user.
    
    Bug: issue 1144
    Bug: issue 2576
    Change-Id: Ie829e61054617015f642cec6c8f2305765c814b4
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/StarredChangesCacheImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/StarredChangesCacheImpl.java
@@ -16,6 +16,10 @@ package com.google.gerrit.server;
 
 import static com.google.inject.Scopes.SINGLETON;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Maps;
 import com.google.gerrit.extensions.registration.DynamicItem;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
@@ -39,6 +43,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 @Singleton
 public class StarredChangesCacheImpl implements StarredChangesCache {
@@ -97,7 +105,7 @@ public class StarredChangesCacheImpl implements StarredChangesCache {
   private final GitRepositoryManager repoManager;
   private final AllUsersName allUsers;
 
-  private final HashMultimapPair<Account.Id, Change.Id> cache;
+  private final HashBasedBiTable<Account.Id, Change.Id, SortedSet<String>> cache;
 
   @Inject
   StarredChangesCacheImpl(
@@ -110,21 +118,21 @@ public class StarredChangesCacheImpl implements StarredChangesCache {
     this.repoManager = repoManager;
     this.allUsers = allUsers;
 
-    this.cache = HashMultimapPair.create();
+    this.cache = HashBasedBiTable.create();
     try {
       load();
     } catch (OrmException | IOException e) {
       log.error("Failed to load starred changes cache", e);
     }
   }
-
   private void load() throws OrmException, IOException {
     if (!migration.readChanges()) {
       for (StarredChange starredChange :
           dbProvider.get().starredChanges().all()) {
         cache.put(
             starredChange.getAccountId(),
-            starredChange.getChangeId());
+            starredChange.getChangeId(),
+            StarredChangesUtil.DEFAULT_LABELS);
       }
     } else {
       try (Repository repo = repoManager.openMetadataRepository(allUsers)) {
@@ -132,35 +140,80 @@ public class StarredChangesCacheImpl implements StarredChangesCache {
             .getRefs(RefNames.REFS_STARRED_CHANGES).keySet()) {
           cache.put(
               Account.Id.fromRefPart(refPart),
-              Change.Id.fromRefPart(refPart));
+              Change.Id.fromRefPart(refPart),
+              StarredChangesUtil.readLabels(repo,
+                  RefNames.REFS_STARRED_CHANGES + refPart));
         }
       }
     }
   }
 
   @Override
-  public boolean isStarred(Account.Id accountId, Change.Id changeId) {
-    return cache.contains(accountId, changeId);
+  public boolean isStarred(Account.Id accountId, Change.Id changeId,
+      String label) {
+    return getLabels(accountId, changeId).contains(label);
   }
 
   @Override
-  public Iterable<Change.Id> byAccount(Account.Id accountId) {
-    return cache.getByFirstKey(accountId);
+  public ImmutableSortedSet<String> getLabels(Account.Id accountId,
+      Change.Id changeId) {
+    SortedSet<String> labels = cache.get(accountId, changeId);
+    if (labels != null) {
+      return ImmutableSortedSet.copyOf(labels);
+    } else {
+      return ImmutableSortedSet.of();
+    }
   }
 
   @Override
-  public Iterable<Account.Id> byChange(Change.Id changeId) {
-    return cache.getBySecondKey(changeId);
+  public Iterable<Change.Id> byAccount(Account.Id accountId,
+      final String label) {
+    return Maps.filterEntries(cache.getMapByFirstKey(accountId),
+        new Predicate<Map.Entry<Change.Id, SortedSet<String>>>() {
+          @Override
+          public boolean apply(Map.Entry<Change.Id, SortedSet<String>> e) {
+            return e.getValue().contains(label);
+          }
+        }).keySet();
   }
 
   @Override
-  public void star(Account.Id accountId, Change.Id changeId) {
-    cache.put(accountId, changeId);
+  public ImmutableMultimap<Change.Id, String> byAccount(Account.Id accountId) {
+    ImmutableMultimap.Builder<Change.Id, String> b =
+        ImmutableMultimap.builder();
+    for (Map.Entry<Change.Id, SortedSet<String>> entry : cache
+        .getMapByFirstKey(accountId).entrySet()) {
+      b.putAll(entry.getKey(), entry.getValue());
+    }
+    return b.build();
   }
 
   @Override
-  public void unstar(Account.Id accountId, Change.Id changeId) {
-    cache.remove(accountId, changeId);
+  public Iterable<Account.Id> byChange(Change.Id changeId, final String label) {
+    return Maps.filterEntries(cache.getMapBySecondKey(changeId),
+        new Predicate<Map.Entry<Account.Id, SortedSet<String>>>() {
+          @Override
+          public boolean apply(Map.Entry<Account.Id, SortedSet<String>> e) {
+            return e.getValue().contains(label);
+          }
+        }).keySet();
+  }
+
+  @Override
+  public ImmutableMultimap<Account.Id, String> byChange(Change.Id changeId) {
+    ImmutableMultimap.Builder<Account.Id, String> b =
+        ImmutableMultimap.builder();
+    for (Map.Entry<Account.Id, SortedSet<String>> entry : cache
+        .getMapBySecondKey(changeId).entrySet()) {
+      b.putAll(entry.getKey(), entry.getValue());
+    }
+    return b.build();
+  }
+
+  @Override
+  public void star(Account.Id accountId, Change.Id changeId,
+      Set<String> labels) {
+    cache.put(accountId, changeId, new TreeSet<>(labels));
   }
 
   @Override

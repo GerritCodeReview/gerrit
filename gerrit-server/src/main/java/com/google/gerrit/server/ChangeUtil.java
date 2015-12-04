edd021a435ca792a326c0c373dbfc379bfdb8f7b
Dave Borowitz <dborowitz@google.com>
2015-12-04 11:49:18 -0500

    Convert DeleteDraftChange to BatchUpdate
    Change-Id: Id4395f92b0c70e86970d479276e9c9e0cc8c7d8f

--- gerrit-server/src/main/java/com/google/gerrit/server/ChangeUtil.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/ChangeUtil.java
@@ -35,7 +35,6 @@ import com.google.gerrit.server.git.BatchUpdate;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.UpdateException;
 import com.google.gerrit.server.git.validators.CommitValidators;
-import com.google.gerrit.server.index.ChangeIndexer;
 import com.google.gerrit.server.mail.RevertedSender;
 import com.google.gerrit.server.notedb.ChangeUpdate;
 import com.google.gerrit.server.project.ChangeControl;
@@ -185,7 +184,6 @@ public class ChangeUtil {
   private final ChangeInserter.Factory changeInserterFactory;
   private final GitRepositoryManager gitManager;
   private final GitReferenceUpdated gitRefUpdated;
-  private final ChangeIndexer indexer;
   private final BatchUpdate.Factory updateFactory;
   private final ChangeMessagesUtil changeMessagesUtil;
   private final ChangeUpdate.Factory changeUpdateFactory;
@@ -200,7 +198,6 @@ public class ChangeUtil {
       ChangeInserter.Factory changeInserterFactory,
       GitRepositoryManager gitManager,
       GitReferenceUpdated gitRefUpdated,
-      ChangeIndexer indexer,
       BatchUpdate.Factory updateFactory,
       ChangeMessagesUtil changeMessagesUtil,
       ChangeUpdate.Factory changeUpdateFactory,
@@ -213,7 +210,6 @@ public class ChangeUtil {
     this.changeInserterFactory = changeInserterFactory;
     this.gitManager = gitManager;
     this.gitRefUpdated = gitRefUpdated;
-    this.indexer = indexer;
     this.updateFactory = updateFactory;
     this.changeMessagesUtil = changeMessagesUtil;
     this.changeUpdateFactory = changeUpdateFactory;
@@ -341,19 +337,6 @@ public class ChangeUtil {
       return commit.getFullMessage();
     } catch (RepositoryNotFoundException e) {
       throw new NoSuchChangeException(changeId, e);
-    }
-  }
-
-  // TODO(dborowitz): Delete after migrating DeleteDraftChange to BatchUpdate.
-  public void deleteDraftChangeInNewTransaction(Change change)
-      throws NoSuchChangeException, OrmException, IOException {
-    db.get().changes().beginTransaction(change.getId());
-    try {
-      deleteDraftChange(change);
-      db.get().commit();
-      indexer.delete(change.getId());
-    } finally {
-      db.get().rollback();
     }
   }
 

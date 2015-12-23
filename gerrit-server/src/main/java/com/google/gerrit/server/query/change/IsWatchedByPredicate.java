d8b88e476bdd124dc033e6a6043f36910833389d
Luca Milanesio <luca.milanesio@gmail.com>
2015-12-23 17:19:33 +0000

    Leverage ChangeCache when searching by number
    Use the in-memory cache when we need to parse a Gerrit change number
    (PAT_LEGACY_ID) instead of fetching the change directly from DB.
    
    Change-Id: If13d32b02bae89001be2bfa22d7da342a982c5d9

--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/IsWatchedByPredicate.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/IsWatchedByPredicate.java
@@ -17,6 +17,7 @@ package com.google.gerrit.server.query.change;
 import com.google.common.collect.ImmutableList;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.account.WatchConfig.ProjectWatchKey;
+import com.google.gerrit.server.git.ChangeCache;
 import com.google.gerrit.server.query.AndPredicate;
 import com.google.gerrit.server.query.Predicate;
 import com.google.gerrit.server.query.QueryBuilder;
@@ -36,16 +37,18 @@ class IsWatchedByPredicate extends AndPredicate<ChangeData> {
 
   private final CurrentUser user;
 
-  IsWatchedByPredicate(ChangeQueryBuilder.Arguments args, boolean checkIsVisible)
+  IsWatchedByPredicate(
+      ChangeQueryBuilder.Arguments args, ChangeCache changeCache, boolean checkIsVisible)
       throws QueryParseException {
-    super(filters(args, checkIsVisible));
+    super(filters(changeCache, args, checkIsVisible));
     this.user = args.getUser();
   }
 
   private static List<Predicate<ChangeData>> filters(
-      ChangeQueryBuilder.Arguments args, boolean checkIsVisible) throws QueryParseException {
+      ChangeCache changeCache, ChangeQueryBuilder.Arguments args, boolean checkIsVisible)
+      throws QueryParseException {
     List<Predicate<ChangeData>> r = new ArrayList<>();
-    ChangeQueryBuilder builder = new ChangeQueryBuilder(args);
+    ChangeQueryBuilder builder = new ChangeQueryBuilder(args, changeCache);
     for (ProjectWatchKey w : getWatches(args)) {
       Predicate<ChangeData> f = null;
       if (w.filter() != null) {

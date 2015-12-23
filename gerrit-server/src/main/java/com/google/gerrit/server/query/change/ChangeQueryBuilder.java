56cacb07c5b76a498842a5ab8f0f6d1197faa6c8
Luca Milanesio <luca.milanesio@gmail.com>
2015-12-23 17:19:33 +0000

    Leverage ChangeCache when searching by number
    Use the in-memory cache when we need to parse a Gerrit change number
    (PAT_LEGACY_ID) instead of fetching the change directly from DB.
    
    Change-Id: If13d32b02bae89001be2bfa22d7da342a982c5d9

--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/ChangeQueryBuilder.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/ChangeQueryBuilder.java
@@ -21,6 +21,7 @@ import static java.util.stream.Collectors.toSet;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Enums;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
@@ -52,6 +53,7 @@ import com.google.gerrit.server.config.AllProjectsName;
 import com.google.gerrit.server.config.AllUsersName;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.config.TrackingFooters;
+import com.google.gerrit.server.git.ChangeCache;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.strategy.SubmitDryRun;
 import com.google.gerrit.server.group.ListMembers;
@@ -416,19 +418,24 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData> {
   }
 
   private final Arguments args;
+  private final ChangeCache changeCache;
 
   @Inject
-  ChangeQueryBuilder(Arguments args) {
+  ChangeQueryBuilder(Arguments args, ChangeCache changeCache) {
     super(mydef);
     this.args = args;
+    this.changeCache = changeCache;
     setupDynamicOperators();
   }
 
   @VisibleForTesting
   protected ChangeQueryBuilder(
-      Definition<ChangeData, ? extends QueryBuilder<ChangeData>> def, Arguments args) {
+      Definition<ChangeData, ? extends QueryBuilder<ChangeData>> def,
+      Arguments args,
+      ChangeCache changeCache) {
     super(def);
     this.args = args;
+    this.changeCache = changeCache;
   }
 
   private void setupDynamicOperators() {
@@ -443,7 +450,7 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData> {
   }
 
   public ChangeQueryBuilder asUser(CurrentUser user) {
-    return new ChangeQueryBuilder(builderDef, args.asUser(user));
+    return new ChangeQueryBuilder(builderDef, args.asUser(user), changeCache);
   }
 
   @Operator
@@ -547,7 +554,7 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData> {
     }
 
     if ("watched".equalsIgnoreCase(value)) {
-      return new IsWatchedByPredicate(args, false);
+      return new IsWatchedByPredicate(args, changeCache, false);
     }
 
     if ("visible".equalsIgnoreCase(value)) {
@@ -821,7 +828,7 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData> {
       // changes visible to that user. The exception is if one of the users is
       // the caller of this method, in which case visibility is already being
       // checked at the top level.
-      p.add(new IsWatchedByPredicate(args.asUser(id), !id.equals(callerId)));
+      p.add(new IsWatchedByPredicate(args.asUser(id), changeCache, !id.equals(callerId)));
     }
     return Predicate.or(p);
   }
@@ -1173,7 +1180,7 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData> {
 
   private List<Change> parseChange(String value) throws OrmException, QueryParseException {
     if (PAT_LEGACY_ID.matcher(value).matches()) {
-      return asChanges(args.queryProvider.get().byLegacyChangeId(Change.Id.parse(value)));
+      return ImmutableList.of(changeCache.get(Change.Id.parse(value)));
     } else if (PAT_CHANGE_ID.matcher(value).matches()) {
       List<Change> changes = asChanges(args.queryProvider.get().byKeyPrefix(parseChangeId(value)));
       if (changes.isEmpty()) {

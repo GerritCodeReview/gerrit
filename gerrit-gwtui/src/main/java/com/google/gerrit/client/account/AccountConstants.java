f698212ed01497919503bb8a0875f158b7b12718
Michael Ochmann <michael.ochmann@sap.com>
2016-01-07 17:02:27 +0100

    Settings screen for OAuth tokens
    The new screen is visible if authentication type OAUTH is enabled
    for Git over HTTP communication (auth.gitBasicAuth = true).
    It shows the value of the access token that is assigned to the
    current web session, the expiration date of the token, and the
    corresponding entries for the .netrc and .gitcookies files.
    
    Change-Id: I4fad66f53ba8cc872de8f8a2120239c83624e9d9
    Signed-off-by: Michael Ochmann <michael.ochmann@sap.com>

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/account/AccountConstants.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/account/AccountConstants.java
@@ -57,6 +57,7 @@ public interface AccountConstants extends Constants {
   String tabGpgKeys();
   String tabHttpAccess();
   String tabMyGroups();
+  String tabOAuthToken();
   String tabPreferences();
   String tabSshKeys();
   String tabWatchedProjects();
@@ -80,6 +81,12 @@ public interface AccountConstants extends Constants {
   String linkReloadContact();
   String invalidUserName();
   String invalidUserEmail();
+
+  String labelOAuthToken();
+  String labelOAuthExpires();
+  String labelOAuthNetRCEntry();
+  String labelOAuthGitCookie();
+  String labelOAuthExpired();
 
   String sshKeyInvalid();
   String sshKeyAlgorithm();

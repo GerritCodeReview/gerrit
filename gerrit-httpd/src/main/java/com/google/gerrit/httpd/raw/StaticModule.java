53e2c5bae0dd834ac9c87098945ee8dbc1390021
David Ostrovsky <david@ostrovsky.org>
2016-01-09 14:46:57 +0100

    Polygerrit: Decouple bower components rebuild from dev workflow
    According to the polygerrit "development story", any changes related
    to polymer components (aka bower_components) involves buck invocations:
    
      buck run //tools/js:bower2buck -- -o /tmp/newbuck
    
    That why it doesn't really make sense to try to optimize for this use
    case and to refresh polymer components in running container. This only
    makes polygerrit development unnecessary slow. We are getting the main
    use case to refresh the polygerrit sources in running container for
    granted due to mounting the PolyGerritUiServlet to the source path.
    
    Remove bower components rebuild filter and store zip file system in
    bower component own servlet instead of leaking it to GerritLauncher
    instance. Eclipse project generation depends now also on polymer
    components.
    
    Test Plan:
    
    * Run:
    
      buck build polygerrit && \
          java -jar buck-out/gen/polygerrit/polygerrit.war daemon \
          --polygerrit-dev -d ../gerrit_testsite
    
    * Change polygerrit sources, refresh the browser window and observe
    that the changes are reflected and that there is no delay for
    unnecessary `buck build //polygerrit-ui:polygerrit_components`
    invocations, every time browser window is refreshed.
    
    Change-Id: If86bbbe14b9ddf0fa5001b2bbb9a39629e9b383f

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/StaticModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/StaticModule.java
@@ -214,15 +214,6 @@ public class StaticModule extends ServletModule {
     public void configureServlets() {
       Path buckOut = getPaths().buckOut;
       if (buckOut != null) {
-        RebuildBowerComponentsFilter rebuildFilter =
-            new RebuildBowerComponentsFilter(buckOut);
-        for (String p : POLYGERRIT_INDEX_PATHS) {
-          // Rebuilding bower_components once per load on the index request,
-          // is sufficient, since it will finish building before attempting to
-          // access any bower_components resources. Plus it saves contention and
-          // extraneous buck builds.
-          filter(p).through(rebuildFilter);
-        }
         serve("/bower_components/*").with(BowerComponentsServlet.class);
       } else {
         // In the war case, bower_components are either inlined by vulcanize, or
@@ -258,7 +249,12 @@ public class StaticModule extends ServletModule {
     @Singleton
     BowerComponentsServlet getBowerComponentsServlet(
         @Named(CACHE) Cache<Path, Resource> cache) {
-      return new BowerComponentsServlet(cache, getPaths().buckOut);
+      try {
+        return new BowerComponentsServlet(cache, getPaths().buckOut);
+      } catch (IOException e) {
+        throw new ProvisionException(
+            "Error initializing bower component servlet", e);
+      }
     }
 
     private boolean isDev() {

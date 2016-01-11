0f01ee4863fdabe81ef52481363e0242c83574e8
Rafal Klys <rafalklys@wp.pl>
2016-01-12 00:08:29 +0100

    New daemon start/stop methods for use with Procrun
    Added additional entry points GerritLauncher.daemonStart and
    GerritLauncher.daemonStop for use when hosting Gerrit in some other
    process. The intention of this is to make it an easy fit to the [Apache
    Commons Daemon Procrun][1]. This adds an easy way to host Gerrit with
    embedded Jetty as Windows Service.
    
    Also moved a bit of logic in Daemon from anonymous thread into the stop
    method.
    
    [1]: http://commons.apache.org/proper/commons-daemon/procrun.html
    
    Change-Id: Ie1fd3b7fceb56e159dc7f02fbcf481b0fcdb08e2

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/util/RuntimeShutdown.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/util/RuntimeShutdown.java
@@ -39,6 +39,10 @@ public class RuntimeShutdown {
     cb.waitForShutdown();
   }
 
+  public static void manualShutdown() {
+    cb.manualShutdown();
+  }
+
   private RuntimeShutdown() {
   }
 
@@ -95,6 +99,11 @@ public class RuntimeShutdown {
         shutdownComplete = true;
         notifyAll();
       }
+    }
+
+    void manualShutdown() {
+      Runtime.getRuntime().removeShutdownHook(this);
+      run();
     }
 
     void waitForShutdown() {

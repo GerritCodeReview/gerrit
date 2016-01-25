063d0ab7f8a659ca4c22c37cf38c57c000598b8c
Edwin Kempin <ekempin@google.com>
2016-01-25 16:27:01 +0100

    PutTopic: Use user from context for change message and hook
    Change-Id: I1c85f7806d41eff3e6c8450a7304f17e3351c221
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PutTopic.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PutTopic.java
@@ -28,7 +28,6 @@ import com.google.gerrit.reviewdb.client.ChangeMessage;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.ChangeMessagesUtil;
 import com.google.gerrit.server.ChangeUtil;
-import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.change.PutTopic.Input;
 import com.google.gerrit.server.git.BatchUpdate;
 import com.google.gerrit.server.git.BatchUpdate.ChangeContext;
@@ -73,7 +72,7 @@ public class PutTopic implements RestModifyView<ChangeResource, Input>,
       throw new AuthException("changing topic not permitted");
     }
 
-    Op op = new Op(ctl, input != null ? input : new Input());
+    Op op = new Op(input != null ? input : new Input());
     try (BatchUpdate u = batchUpdateFactory.create(dbProvider.get(),
         req.getChange().getProject(), ctl.getUser(), TimeUtil.nowTs())) {
       u.addOp(req.getId(), op);
@@ -86,15 +85,13 @@ public class PutTopic implements RestModifyView<ChangeResource, Input>,
 
   private class Op extends BatchUpdate.Op {
     private final Input input;
-    private final IdentifiedUser caller;
 
     private Change change;
     private String oldTopicName;
     private String newTopicName;
 
-    public Op(ChangeControl ctl, Input input) {
+    public Op(Input input) {
       this.input = input;
-      this.caller = ctl.getUser().asIdentifiedUser();
     }
 
     @Override
@@ -123,7 +120,7 @@ public class PutTopic implements RestModifyView<ChangeResource, Input>,
           new ChangeMessage.Key(
               change.getId(),
               ChangeUtil.messageUUID(ctx.getDb())),
-          caller.getAccountId(), ctx.getWhen(),
+          ctx.getUser().getAccountId(), ctx.getWhen(),
           change.currentPatchSetId());
       cmsg.setMessage(summary);
       cmUtil.addChangeMessage(ctx.getDb(), update, cmsg);
@@ -133,8 +130,11 @@ public class PutTopic implements RestModifyView<ChangeResource, Input>,
     @Override
     public void postUpdate(Context ctx) throws OrmException {
       if (change != null) {
-        hooks.doTopicChangedHook(change, caller.getAccount(),
-            oldTopicName, ctx.getDb());
+        hooks.doTopicChangedHook(
+            change,
+            ctx.getUser().asIdentifiedUser().getAccount(),
+            oldTopicName,
+            ctx.getDb());
       }
     }
   }

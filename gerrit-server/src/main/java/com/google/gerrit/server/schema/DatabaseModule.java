74c26d8a2b074ba4d459194805050fd2f519d6de
Dave Borowitz <dborowitz@google.com>
2016-01-28 08:39:36 -0500

    WIP: Disable reading from changes tables under notedb
    Wrap ReviewDb with an implementation that disables all read access
    methods on all Change-related tables. (Currently Changes itself is an
    exception because those calls have not all been migrated yet.)
    
    This exposes quite a few errors in notedb tests, so should not be
    submitted until those are cleaned up.
    
    Change-Id: I60f585f5ef12c660aa9298c3676c1e5c95c945f4

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/DatabaseModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/DatabaseModule.java
@@ -18,17 +18,22 @@ import static com.google.inject.Scopes.SINGLETON;
 
 import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.notedb.NotesMigration;
 import com.google.gwtorm.jdbc.Database;
 import com.google.gwtorm.server.SchemaFactory;
+import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
 
 /** Loads the database with standard dependencies. */
 public class DatabaseModule extends FactoryModule {
   @Override
   protected void configure() {
-    bind(new TypeLiteral<SchemaFactory<ReviewDb>>() {}).to(
-        new TypeLiteral<Database<ReviewDb>>() {}).in(SINGLETON);
-    bind(new TypeLiteral<Database<ReviewDb>>() {}).toProvider(
-        ReviewDbDatabaseProvider.class).in(SINGLETON);
+    bind(NotesMigration.class);
+    TypeLiteral<SchemaFactory<ReviewDb>> schemaFactory =
+        new TypeLiteral<SchemaFactory<ReviewDb>>() {};
+    bind(schemaFactory).to(NotesMigrationSchemaFactory.class);
+    bind(Key.get(schemaFactory, ReviewDbFactory.class))
+        .to(new TypeLiteral<Database<ReviewDb>>() {})
+        .in(SINGLETON);
   }
 }

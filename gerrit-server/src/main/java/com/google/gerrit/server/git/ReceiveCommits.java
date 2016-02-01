ebda07970d0b6104956a8be39be134e8f4101740
Edwin Kempin <ekempin@google.com>
2016-02-01 18:17:15 +0100

    Use ChangeNotes in more places to load the change
    Changes should be loaded via ChangeNotes.Factory and not directly from
    the database.
    
    Change-Id: I36224208428092ea4cd86843f72f9f72e8c08535
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -1492,7 +1492,8 @@ public class ReceiveCommits {
 
     final Change changeEnt;
     try {
-      changeEnt = db.changes().get(changeId);
+      changeEnt =
+          notesFactory.create(db, project.getNameKey(), changeId).getChange();
     } catch (OrmException e) {
       log.error("Cannot lookup existing change " + changeId, e);
       reject(cmd, "database error");
@@ -1843,7 +1844,8 @@ public class ReceiveCommits {
           changeCtl.getUser().asIdentifiedUser(), false);
     }
     addMessage("");
-    Change c = db.changes().get(rsrc.getChange().getId());
+    Change c = notesFactory
+        .create(db, project.getNameKey(), rsrc.getChange().getId()).getChange();
     switch (c.getStatus()) {
       case MERGED:
         addMessage("Change " + c.getChangeId() + " merged.");
@@ -2726,7 +2728,8 @@ public class ReceiveCommits {
     String refName = cmd.getRefName();
     Change.Id cid = psi.getParentKey();
 
-    Change change = db.changes().get(cid);
+    Change change =
+        notesFactory.create(db, project.getNameKey(), cid).getChange();
     ChangeControl ctl = projectControl.controlFor(change);
     PatchSet ps = psUtil.get(db, ctl.getNotes(), psi);
     if (change == null || ps == null) {

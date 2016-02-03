28b7a0f0cad5959a34e3c34f12abdc516ece1d27
Edwin Kempin <ekempin@google.com>
2016-02-03 12:43:53 +0100

    Allow ChangeControl.Factory#create(RefControl, Change) to throw OrmException
    This method will need to throw OrmException when the
    ChangeNotes.Factory will be responsible for loading changes from the
    database, which may fail with an OrmException.
    
    Change-Id: I25c69e634eca63a89761032e87ecc78faff525b0
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/project/ProjectControl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/ProjectControl.java
@@ -44,6 +44,7 @@ import com.google.gerrit.server.git.TagCache;
 import com.google.gerrit.server.git.VisibleRefFilter;
 import com.google.gerrit.server.group.SystemGroupBackend;
 import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.assistedinject.Assisted;
@@ -193,7 +194,7 @@ public class ProjectControl {
     return r;
   }
 
-  public ChangeControl controlFor(final Change change) {
+  public ChangeControl controlFor(Change change) throws OrmException {
     return changeControlFactory.create(controlForRef(change.getDest()), change);
   }
 

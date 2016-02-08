471406995fa8d179981d2a5f3798fcc3b423b7a3
Han-Wen Nienhuys <hanwen@google.com>
2016-02-08 15:17:30 +0100

    Document return value of BatchUpdate.Op#updateChange.
    Change-Id: Iafbd690dd7202f522b38f7335fee68fe46c93171

--- gerrit-server/src/main/java/com/google/gerrit/server/git/BatchUpdate.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/BatchUpdate.java
@@ -229,6 +229,10 @@ public class BatchUpdate implements AutoCloseable {
     public void updateRepo(RepoContext ctx) throws Exception {
     }
 
+    /**
+     * Override this method to modify a change. The return value indicates if
+     * anything was changed that might require a write to the metadata storage.
+     */
     @SuppressWarnings("unused")
     public boolean updateChange(ChangeContext ctx) throws Exception {
       return false;

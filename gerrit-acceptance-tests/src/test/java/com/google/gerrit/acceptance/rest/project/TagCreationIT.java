f2fb2bb4aac2dc8c71ed3c3aaea0a6c1a67ede7b
Saša Živkov <sasa.zivkov@sap.com>
2016-02-08 15:41:19 +0100

    WIP: Clarify permission for creation and update of tags
    There are several differences in how "Create Reference" and "Push"
    permissions are handled in 2.10 and 2.11+. Since the change in behavior
    is not documented we need an agreement on what is the correct behavior
    and what is a regression.
    
    Please post your comments on the tests wherever you think the test
    reveals a bug in Gerrit or the test itself is wrong. Once we agree
    on the tests I will start fixing stable-2.12 (if necessary).
    
    Change-Id: I351a84da278feb9d3aaeb82c92484f3cd9c59353

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/project/TagCreationIT.java
@@ -0,0 +1,88 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.rest.project;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.acceptance.GitUtil.createAnnotatedTag;
+import static com.google.gerrit.acceptance.GitUtil.pushHead;
+import static com.google.gerrit.acceptance.GitUtil.pushTag;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.NoHttpd;
+import com.google.gerrit.common.data.Permission;
+
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.RemoteRefUpdate;
+import org.eclipse.jgit.transport.RemoteRefUpdate.Status;
+import org.junit.Test;
+
+@NoHttpd
+public class TagCreationIT extends AbstractDaemonTest {
+
+  @Test
+  public void lightweightTagOnNewCommit_noPushPermission_rejected()
+      throws Exception {
+    grant(Permission.CREATE, project, "refs/tags/*");
+
+    commit(admin.getIdent(), "subject");
+    PushResult r = pushHead(testRepo, "refs/tags/v1");
+    RemoteRefUpdate refUpdate = r.getRemoteUpdate("refs/tags/v1");
+    assertThat(refUpdate.getStatus()).isEqualTo(Status.REJECTED_OTHER_REASON);
+  }
+
+  @Test
+  public void annotatedTagOnNewCommit_noPushPermission_rejected()
+      throws Exception {
+    grant(Permission.PUSH_TAG, project, "refs/tags/*");
+
+    commit(admin.getIdent(), "subject");
+    createAnnotatedTag(testRepo, "a1", admin.getIdent());
+    PushResult r = pushTag(testRepo, "a1");
+    RemoteRefUpdate refUpdate = r.getRemoteUpdate("refs/tags/a1");
+    System.out.println(refUpdate.getMessage());
+    assertThat(refUpdate.getStatus()).isEqualTo(Status.REJECTED_OTHER_REASON);
+  }
+
+  @Test
+  public void lightweightTagOnNewCommit_withPushPermission_ok() throws Exception {
+    grant(Permission.CREATE, project, "refs/tags/*");
+    grant(Permission.PUSH, project, "refs/tags/*");
+
+    commit(admin.getIdent(), "subject");
+    PushResult r = pushHead(testRepo, "refs/tags/v1");
+    RemoteRefUpdate refUpdate = r.getRemoteUpdate("refs/tags/v1");
+    assertThat(refUpdate.getStatus()).isEqualTo(Status.OK);
+  }
+
+  @Test
+  public void annotatedTagOnNewCommit_withPushPermission_ok() throws Exception {
+    grant(Permission.PUSH_TAG, project, "refs/tags/*");
+    grant(Permission.PUSH, project, "refs/tags/*");
+
+    commit(admin.getIdent(), "subject");
+    createAnnotatedTag(testRepo, "a1", admin.getIdent());
+    PushResult r = pushTag(testRepo, "a1");
+    RemoteRefUpdate refUpdate = r.getRemoteUpdate("refs/tags/a1");
+    assertThat(refUpdate.getStatus()).isEqualTo(Status.OK);
+  }
+
+  private void commit(PersonIdent ident, String subject) throws Exception {
+    commitBuilder()
+        .ident(ident)
+        .message(subject)
+        .create();
+  }
+ }

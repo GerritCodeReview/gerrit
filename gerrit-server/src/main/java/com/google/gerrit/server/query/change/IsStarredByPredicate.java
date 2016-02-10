f9313df66d368a00c710d5d84d79b599dd481e92
Dave Borowitz <dborowitz@google.com>
2016-02-10 17:22:40 -0500

    IsStarredByPredicate: Don't implement ChangeDataSource
    Since this is now a collection of index predicates, it will always get
    folded into other index predicates by the rewriter. The
    ChangeDataSource methods were dead code.
    
    (It's still possible that the number of starred changes will overflow
    the query term limit. Fixing that would require a different
    implementation of ChangeDataSource, given that we don't want to read
    from the Changes table to find project names, so this code is nothing
    we want to keep around anyway.)
    
    Change-Id: Icd2e1a97e607200703bee08d1696aa7d48837e67

--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/IsStarredByPredicate.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/IsStarredByPredicate.java
@@ -17,19 +17,15 @@ package com.google.gerrit.server.query.change;
 import com.google.common.collect.Lists;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.server.CurrentUser;
-import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.query.OrPredicate;
 import com.google.gerrit.server.query.Predicate;
 import com.google.gerrit.server.query.QueryParseException;
 import com.google.gerrit.server.query.change.ChangeQueryBuilder.Arguments;
-import com.google.gwtorm.server.OrmException;
-import com.google.gwtorm.server.ResultSet;
 
 import java.util.List;
 import java.util.Set;
 
-class IsStarredByPredicate extends OrPredicate<ChangeData> implements
-    ChangeDataSource {
+class IsStarredByPredicate extends OrPredicate<ChangeData> {
   private static String describe(CurrentUser user) {
     if (user.isIdentifiedUser()) {
       return user.getAccountId().toString();
@@ -45,38 +41,16 @@ class IsStarredByPredicate extends OrPredicate<ChangeData> implements
     return r;
   }
 
-  private final Arguments args;
   private final CurrentUser user;
 
   IsStarredByPredicate(Arguments args) throws QueryParseException {
-    this(args, args.getIdentifiedUser());
-  }
-
-  private IsStarredByPredicate(Arguments args, IdentifiedUser user) {
-    super(predicates(user.getStarredChanges()));
-    this.args = args;
-    this.user = user;
+    super(predicates(args.getIdentifiedUser().getStarredChanges()));
+    this.user = args.getIdentifiedUser();
   }
 
   @Override
   public boolean match(final ChangeData object) {
     return user.getStarredChanges().contains(object.getId());
-  }
-
-  @Override
-  public ResultSet<ChangeData> read() throws OrmException {
-    return ChangeDataResultSet.change(args.changeDataFactory, args.db,
-        args.db.get().changes().get(user.getStarredChanges()));
-  }
-
-  @Override
-  public boolean hasChange() {
-    return true;
-  }
-
-  @Override
-  public int getCardinality() {
-    return 10;
   }
 
   @Override

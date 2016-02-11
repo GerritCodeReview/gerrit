b60cd15d17b2246c1d00bf694953605d44f99106
Sammy Gillespie <sammygillespie@gmail.com>
2016-02-11 14:39:43 +0000

    Add ability to set cookie domain
    Change-Id: Ib74c95dd298f19df689c9843ee3144f387505865

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/CacheBasedWebSession.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/CacheBasedWebSession.java
@@ -214,6 +214,12 @@ public abstract class CacheBasedWebSession implements WebSession {
     }
 
     outCookie = new Cookie(ACCOUNT_COOKIE, token);
+
+    String domain = authConfig.getCookieDomain();
+    if (domain != null && !domain.isEmpty()) {
+      outCookie.setDomain(domain);
+    }
+
     outCookie.setSecure(isSecure(request));
     outCookie.setPath(path);
     outCookie.setMaxAge(ageSeconds);

2461265e4867f36b8127890393136c586d9018b2
Michael Ochmann <michael.ochmann@sap.com>
2016-02-12 17:26:18 +0100

    Additional AccountExternalId when creating account
    The REST API for creating accounts does only support the creation
    of accounts for users stored in Gerrit's internal user store.
    The current implementation of CreateAccount only registers an
    AccountExternalId of type SCHEME_USERNAME, while for example for
    OAuth authentication SCHEME_EXTERNAL is required.
    
    This patch adds a new extension point AccountExternalIdCreator
    that can be implemented to inject additional AccountExternalId
    entries for an account during account creation.
    
    Change-Id: I53df8ae04a023fd32587136981347bdf176bb4ee
    Signed-off-by: Michael Ochmann <michael.ochmann@sap.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/api/accounts/AccountExternalIdCreator.java
@@ -0,0 +1,37 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.api.accounts;
+
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountExternalId;
+
+import java.util.List;
+
+public interface AccountExternalIdCreator {
+
+  /**
+   * Returns additional external identifiers to assign to a given
+   * user when creating an account.
+   *
+   * @param id the identifier of the account.
+   * @param username the name of the user.
+   * @param email an optional email address to assign to the external
+   * identifiers, or {@code null}.
+   *
+   * @return a list of external identifiers, or an empty list.
+   */
+  public List<AccountExternalId> create(Account.Id id, String username,
+      String email);
+}

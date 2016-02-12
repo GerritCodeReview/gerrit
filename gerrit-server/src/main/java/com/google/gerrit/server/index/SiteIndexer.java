ec96988eb3a1747fc1648ac58bc07fa350b139f7
Edwin Kempin <ekempin@google.com>
2016-02-12 11:30:21 +0100

    Move methods for scanning change refs into ChangeNotes.Factory
    All remaining code that loads changes in batches from the database
    should go into ChangeNotes.Factory.
    
    Change-Id: Ib1d8c09f78e3f6d898378e91b501fa4d1ec154d5
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/index/SiteIndexer.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/index/SiteIndexer.java
@@ -37,7 +37,6 @@ import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.MergeUtil;
 import com.google.gerrit.server.git.MultiProgressMonitor;
 import com.google.gerrit.server.git.MultiProgressMonitor.Task;
-import com.google.gerrit.server.git.ScanningChangeCacheImpl;
 import com.google.gerrit.server.notedb.ChangeNotes;
 import com.google.gerrit.server.notedb.NotesMigration;
 import com.google.gerrit.server.patch.PatchListLoader;
@@ -257,8 +256,9 @@ public class SiteIndexer {
         try (Repository repo = repoManager.openRepository(project);
             ReviewDb db = schemaFactory.open()) {
           Map<String, Ref> refs = repo.getRefDatabase().getRefs(ALL);
-          for (Change c : ScanningChangeCacheImpl.scan(notesMigration,
+          for (ChangeNotes cn : ChangeNotes.Factory.scan(notesMigration,
               notesFactory, repo, db, project)) {
+            Change c = cn.getChange();
             Ref r = refs.get(c.currentPatchSetId().toRefName());
             if (r != null) {
               byId.put(r.getObjectId(), changeDataFactory.create(db, c));

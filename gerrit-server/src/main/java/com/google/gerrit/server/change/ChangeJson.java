398f7a5d0a8bc4709e416b7e3363bb3498842496
Edwin Kempin <ekempin@google.com>
2016-02-16 09:33:10 +0100

    Add factory for ChangeResource
    This will allow us to inject classes that we need for the ETag
    computation.
    
    Change-Id: I0411d53c085eb271b2704fc83f6236bf2a26cc5f
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
@@ -161,6 +161,7 @@ public class ChangeJson {
   private final ActionJson actionJson;
   private final GpgApiAdapter gpgApi;
   private final ChangeNotes.Factory notesFactory;
+  private final ChangeResource.Factory changeResourceFactory;
 
   private AccountLoader accountLoader;
   private Map<Change.Id, List<SubmitRecord>> submitRecords;
@@ -187,6 +188,7 @@ public class ChangeJson {
       ActionJson actionJson,
       GpgApiAdapter gpgApi,
       ChangeNotes.Factory notesFactory,
+      ChangeResource.Factory changeResourceFactory,
       @Assisted Set<ListChangesOption> options) {
     this.db = db;
     this.labelNormalizer = ln;
@@ -207,6 +209,7 @@ public class ChangeJson {
     this.actionJson = actionJson;
     this.gpgApi = gpgApi;
     this.notesFactory = notesFactory;
+    this.changeResourceFactory = changeResourceFactory;
     this.options = options.isEmpty()
         ? EnumSet.noneOf(ListChangesOption.class)
         : EnumSet.copyOf(options);
@@ -956,7 +959,7 @@ public class ChangeJson {
         && userProvider.get().isIdentifiedUser()) {
 
       actionJson.addRevisionActions(out,
-          new RevisionResource(new ChangeResource(ctl), in));
+          new RevisionResource(changeResourceFactory.create(ctl), in));
     }
 
     if (has(PUSH_CERTIFICATES)) {

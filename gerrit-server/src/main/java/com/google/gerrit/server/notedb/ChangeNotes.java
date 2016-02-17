5e27e74bc3446fba060b73adf7db04b500b43442
Edwin Kempin <ekempin@google.com>
2016-02-17 14:45:32 +0100

    ReceiveCommits: Read changes for replace via ChangeNotes.Factory
    This ensures that the changes are read from notedb if it is enabled.
    
    The change notes are read asynchronously, as were the changes before.
    
    Change-Id: Id9b9aa8927635f17c85158769cfe7ecec1f724d4
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
@@ -34,6 +34,11 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Ordering;
 import com.google.common.primitives.Ints;
+import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.gerrit.common.data.SubmitRecord;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Branch;
@@ -78,6 +83,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
 
 /** View of a single {@link Change} based on the log of its notes branch. */
 public class ChangeNotes extends AbstractChangeNotes<ChangeNotes> {
@@ -225,6 +231,36 @@ public class ChangeNotes extends AbstractChangeNotes<ChangeNotes> {
           + " createFromChangeWhenNotedbDisabled when notedb is enabled");
       return new ChangeNotes(repoManager, migration, allUsersProvider,
           change.getProject(), change).load();
+    }
+
+    public CheckedFuture<ChangeNotes, OrmException> createAsync(
+        final ListeningExecutorService executorService, final ReviewDb db,
+        final Project.NameKey project, final Change.Id changeId) {
+      return Futures.makeChecked(
+          Futures.transformAsync(db.changes().getAsync(changeId),
+              new AsyncFunction<Change, ChangeNotes>() {
+                @Override
+                public ListenableFuture<ChangeNotes> apply(
+                    final Change change) {
+                  return executorService.submit(new Callable<ChangeNotes>() {
+                    @Override
+                    public ChangeNotes call() throws Exception {
+                      checkState(project.equals(change.getProject()),
+                          "unexpected project for change " + changeId.get());
+                      return new ChangeNotes(repoManager, migration,
+                          allUsersProvider, project, change).load();
+                    }
+                  });
+                }
+              }), new Function<Exception, OrmException>() {
+                @Override
+                public OrmException apply(Exception e) {
+                  if (e instanceof OrmException) {
+                    return (OrmException) e;
+                  }
+                  return new OrmException(e);
+                }
+              });
     }
 
     public List<ChangeNotes> create(ReviewDb db,

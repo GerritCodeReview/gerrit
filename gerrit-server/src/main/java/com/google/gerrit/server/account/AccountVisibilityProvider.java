a0ad52dd7a63ab6ea9b5951df73b4b2e8a177f6c
David Pursehouse <david.pursehouse@sonymobile.com>
2016-02-18 16:56:27 +0900

    AccountVisibilityProvider: Remove support for legacy suggest.accounts
    The suggest.accounts setting has been considered legacy since before
    version 2.4.
    
    AccountVisibilityProvider was added in change I438e90fbe which went into
    version 2.4 almost 4 years ago, and the legacy handling was also in the
    previous implementation.
    
    It's time to remove it.
    
    Change-Id: I57161c14ce4edfbd6477f25e072539ec1d1bac1f

--- gerrit-server/src/main/java/com/google/gerrit/server/account/AccountVisibilityProvider.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/AccountVisibilityProvider.java
@@ -19,13 +19,8 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 import org.eclipse.jgit.lib.Config;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AccountVisibilityProvider implements Provider<AccountVisibility> {
-  private static final Logger log =
-      LoggerFactory.getLogger(AccountVisibilityProvider.class);
-
   private final AccountVisibility accountVisibility;
 
   @Inject
@@ -33,19 +28,6 @@ public class AccountVisibilityProvider implements Provider<AccountVisibility> {
     AccountVisibility av;
     if (cfg.getString("accounts", null, "visibility") != null) {
       av = cfg.getEnum("accounts", null, "visibility", AccountVisibility.ALL);
-    } else if (cfg.getString("suggest", null, "accounts") != null) {
-      try {
-        av = cfg.getEnum("suggest", null, "accounts", AccountVisibility.ALL);
-        log.warn(String.format(
-            "Using legacy value %s for suggest.accounts;"
-            + " use accounts.visibility=%s instead",
-            av, av));
-      } catch (IllegalArgumentException err) {
-        // If suggest.accounts is a valid boolean, it's a new-style config, and
-        // we should use the default here. Invalid values are caught in
-        // SuggestServiceImpl so we don't worry about them here.
-        av = AccountVisibility.ALL;
-      }
     } else {
       av = AccountVisibility.ALL;
     }

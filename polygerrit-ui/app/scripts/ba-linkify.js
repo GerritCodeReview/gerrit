1168a39417f11dc87d0bce4f77c10c03627f6944
Urs Wolfer <uwolfer@fwo.ch>
2016-02-18 21:17:01 +0100

    Add config files for 'JSHint' and 'JSCS'
    - handle (mark) some rare cases which do not comply
    - fix some minor issues
    
    Change-Id: I20067526ea21b30ff96177a182787a8b1d8aaaec

--- polygerrit-ui/app/scripts/ba-linkify.js
+++ polygerrit-ui/app/scripts/ba-linkify.js
@@ -45,6 +45,9 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
+// jshint ignore:start
+// jscs:disable
+
 window.linkify = (function(){
   var
   SCHEME = "[a-z\\d.-]+://",

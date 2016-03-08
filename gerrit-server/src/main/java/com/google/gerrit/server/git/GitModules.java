8ded46f99c6df1140cb5d5d0ca239fb7f012fc78
Stefan Beller <sbeller@google.com>
2016-03-08 12:31:36 -0800

    ACLs for superproject subscriptions
    This allows submodules of the superproject subscription feature to specify
    fine grained, who is allowed to subscribe to it. See
    Documentation/user-submodules.txt for the changed
    handling of subscriptions.
    
    The current tests were kept closely as-is, just enabling the subscription
    feature. New tests have been written to test for the denial of superproject
    subscriptions.
    
    As of this change the superproject subscription table which was an
    approximate cache for the subscriptions is dropped, and the superproject
    subscription is performed as
     * parse the submodule ACL for potential superprojects
     * check the .gitmodules file in all potential superprojects for a real
       subscription
     * perform the superproject update if the supscription is valid
    
    The cache worked semi-reliable (e.g. 2015-04-28 Submodule Subscriptions:
    Remove subscriptions by deleting .gitmodules,
    I1eaf452d5499397644e8eea3707a1352af89126d), so drop the cache and trade in
    correctness over performance.
    
    Bug: Issue 3311
    Change-Id: Id74dc5a34a50b336a22005c96b79f3c4688a36ec
    Signed-off-by: Stefan Beller <sbeller@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/GitModules.java
@@ -0,0 +1,131 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.reviewdb.client.Branch;
+import com.google.gerrit.reviewdb.client.SubmoduleSubscription;
+import com.google.gerrit.server.config.CanonicalWebUrl;
+import com.google.gerrit.server.util.SubmoduleSectionParser;
+import com.google.inject.Provider;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.BlobBasedConfig;
+import org.eclipse.jgit.lib.FileMode;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
+public class GitModules {
+  private static final Logger log = LoggerFactory.getLogger(GitModules.class);
+
+  public interface Factory {
+    GitModules create(Branch.NameKey project, String submissionId);
+  }
+
+  private static final String GIT_MODULES = ".gitmodules";
+
+  private final Provider<String> urlProvider;
+  private final GitRepositoryManager repoManager;
+  private final SubmoduleSectionParser.Factory subSecParserFactory;
+  private final Branch.NameKey branch;
+  private final String submissionId;
+
+  Set<SubmoduleSubscription> subscriptions;
+
+  @AssistedInject
+  GitModules(
+      @CanonicalWebUrl @Nullable Provider<String> urlProvider,
+      GitRepositoryManager repoManager,
+      SubmoduleSectionParser.Factory subSecParserFactory,
+      @Assisted Branch.NameKey branch,
+      @Assisted String submissionId) {
+    this.repoManager = repoManager;
+    this.urlProvider = urlProvider;
+    this.subSecParserFactory = subSecParserFactory;
+    this.branch = branch;
+    this.submissionId = submissionId;
+  }
+
+  void load() throws SubmoduleException {
+    logDebug("Loading .gitmodules of " + branch + " for project " + branch.getParentKey());
+    try (Repository repo = repoManager.openRepository(branch.getParentKey());
+        RevWalk rw = new RevWalk(repo)) {
+
+      ObjectId id = repo.resolve(branch.get());
+      if (id == null) {
+        throw new IOException("Cannot open branch " + branch.get());
+      }
+      RevCommit commit = rw.parseCommit(id);
+
+      TreeWalk tw = TreeWalk.forPath(repo, GIT_MODULES, commit.getTree());
+      if (tw != null
+          && (FileMode.REGULAR_FILE.equals(tw.getRawMode(0)) ||
+              FileMode.EXECUTABLE_FILE.equals(tw.getRawMode(0)))) {
+        BlobBasedConfig bbc =
+            new BlobBasedConfig(null, repo, commit, GIT_MODULES);
+
+        String thisServer = new URI(urlProvider.get()).getHost();
+        subscriptions = subSecParserFactory.create(bbc, thisServer,
+            branch).parseAllSections();
+      }
+    } catch (ConfigInvalidException | IOException e) {
+      logAndThrowSubmoduleException(
+          "Could not read .gitmodule file of super project: " +
+              branch.getParentKey(), e);
+    } catch (URISyntaxException e) {
+      logAndThrowSubmoduleException("Incorrect Gerrit canonical web url " +
+          "provided in gerrit.config file.", e);
+    }
+  }
+
+  public Collection<SubmoduleSubscription> subscribedTo(Branch.NameKey src) {
+    logDebug("Checking for a subscription of " + src);
+    Collection<SubmoduleSubscription> ret = new ArrayList<>();
+    for (SubmoduleSubscription s : subscriptions) {
+      if (s.getSubmodule().equals(src)) {
+        logDebug("Found " + s);
+        ret.add(s);
+      }
+    }
+    return ret;
+  }
+
+  private static void logAndThrowSubmoduleException(final String errorMsg,
+      final Exception e) throws SubmoduleException {
+    log.error(errorMsg, e);
+    throw new SubmoduleException(errorMsg, e);
+  }
+
+  private void logDebug(String msg, Object... args) {
+    if (log.isDebugEnabled()) {
+      log.debug("[" + submissionId + "]" + msg, args);
+    }
+  }
+}

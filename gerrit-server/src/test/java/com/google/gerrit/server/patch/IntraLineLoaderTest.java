ce814c00b3d4a8191772ec34ebb338786cd6650e
Martin Waitz <tali@admingilde.org>
2016-03-16 17:26:46 +0100

    Add unittest for IntraLineLoader
    In order to reproduce issue #3423, add a unittest for the server-side
    intra-line diff calculation.
    
    Change-Id: Icbee1fb4cd14ee8981b0f6a4ec18f65b3bd141fa

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/patch/IntraLineLoaderTest.java
@@ -0,0 +1,134 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.patch;
+
+import org.eclipse.jgit.diff.Edit;
+import org.eclipse.jgit.diff.EditList;
+import org.eclipse.jgit.diff.ReplaceEdit;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.junit.Ignore;
+
+import java.util.List;
+
+public class IntraLineLoaderTest {
+
+  @Test
+  public void rewriteAtStartOfLineIsRecognized() throws Exception {
+    String a = "abc1\n";
+    String b = "def1\n";
+    assertThat(intraline(a, b)).isEqualTo(wordEdit(0, 3, 0, 3));
+  }
+
+  @Test
+  public void rewriteAtEndOfLineIsRecognized() throws Exception {
+    String a = "abc1\n";
+    String b = "abc2\n";
+    assertThat(intraline(a, b)).isEqualTo(wordEdit(3, 4, 3, 4));
+  }
+
+  @Test
+  public void completeRewriteIncludesNewline() throws Exception {
+    String a = "abc1\n";
+    String b = "def2\n";
+    assertThat(intraline(a, b)).isEqualTo(wordEdit(0, 5, 0, 5));
+  }
+
+  @Test
+  public void duplicateTextIsRecognizedAtEnd() throws Exception {
+    String a = "abc\n";
+    String b = "abcabc\n";
+    assertThat(intraline(a, b)).isEqualTo(wordEdit(3, 3, 3, 6));
+  }
+
+  @Test(expected = AssertionError.class)
+  public void additionalIndentationIsRecognizedAtStart() throws Exception {
+    //         |0 2  |5 7
+    String a = " abc\n  def\n";
+    String b = "  abc\n   def\n";
+    List<Edit> expected = new EditList();
+    expected.add(new Edit(0, 0, 0, 1));
+    expected.add(new Edit(5, 5, 6, 7));
+    assertThat(intraline(a,b)).isEqualTo(expected);
+  }
+
+  @Test(expected = AssertionError.class)
+  public void lessIndentationIsRecognizedAtStart() throws Exception {
+    //         |0 2  |5 7
+    String a = "  abc\n   def\n";
+    String b = " abc\n  def\n";
+    List<Edit> expected = new EditList();
+    expected.add(new Edit(0, 1, 0, 0));
+    expected.add(new Edit(6, 7, 5, 5));
+    assertThat(intraline(a,b)).isEqualTo(expected);
+  }
+
+  @Test
+  public void insertedWhitespaceIsRecognized() throws Exception {
+    String a = " int *foobar\n";
+    String b = " int * foobar\n";
+    assertThat(intraline(a, b)).isEqualTo(wordEdit(6, 6, 6, 7));
+  }
+
+  @Test(expected = AssertionError.class) // issue #3423
+  public void insertedWhitespaceIsRecognizedInIdenticalLines() throws Exception {
+    //         |0    5   10  |  5   20    5   30
+    String a = " int *foobar\n int *foobar\n";
+    String b = " int * foobar\n int * foobar\n";
+    List<Edit> expected = new EditList();
+    expected.add(new Edit(6, 6, 6, 7));
+    expected.add(new Edit(19, 19, 20, 21));
+    assertThat(intraline(a,b)).isEqualTo(expected);
+  }
+
+  // helper functions for the tests
+
+  static private int countLines(String s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '\n') { count++; }
+    }
+    return count;
+  }
+
+  static private List<Edit> intraline(String a, String b) throws Exception {
+    return intraline(a, b, new Edit(0, countLines(a), 0, countLines(b)));
+  }
+
+  static private List<Edit> intraline(String a, String b, Edit lines) throws Exception {
+    Text aText = new Text(a.getBytes(UTF_8));
+    Text bText = new Text(b.getBytes(UTF_8));
+    IntraLineDiff diff = IntraLineLoader.compute(aText, bText, EditList.singleton(lines));
+    assertThat(diff.getStatus()).isEqualTo(IntraLineDiff.Status.EDIT_LIST);
+    List<Edit> actualEdits = diff.getEdits();
+    assertThat(actualEdits).hasSize(1);
+    Edit actualEdit = actualEdits.get(0);
+    assertThat(actualEdit.getBeginA()).isEqualTo(lines.getBeginA());
+    assertThat(actualEdit.getEndA()).isEqualTo(lines.getEndA());
+    assertThat(actualEdit.getBeginB()).isEqualTo(lines.getBeginB());
+    assertThat(actualEdit.getEndB()).isEqualTo(lines.getEndB());
+    assertThat(actualEdit).isInstanceOf(ReplaceEdit.class);
+    return ((ReplaceEdit) actualEdit).getInternalEdits();
+  }
+
+  static private List<Edit> wordEdit(int as, int ae, int bs, int be) {
+    return EditList.singleton(new Edit(as, ae, bs, be));
+  }
+
+}

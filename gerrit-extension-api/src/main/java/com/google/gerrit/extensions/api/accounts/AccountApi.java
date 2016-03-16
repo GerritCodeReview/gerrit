aef83932e95215eab6881da2ea161df5b0e39ed6
Patrick Hobusch <phobusch@elegosoft.com>
2016-03-16 16:57:30 +0100

    AccountApi: Add method to get GeneralPreferencesInfo
    Add method AccountApi.getPreferences() to align the Java API with the Rest API.
    It is needed for the new Android client.
    
    Change-Id: Ie472f9d740ab0b2c878400250e687e4452f7d880

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/accounts/AccountApi.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/accounts/AccountApi.java
@@ -14,6 +14,7 @@
 
 package com.google.gerrit.extensions.api.accounts;
 
+import com.google.gerrit.extensions.client.GeneralPreferencesInfo;
 import com.google.gerrit.extensions.common.AccountInfo;
 import com.google.gerrit.extensions.common.GpgKeyInfo;
 import com.google.gerrit.extensions.restapi.NotImplementedException;
@@ -26,6 +27,8 @@ public interface AccountApi {
   AccountInfo get() throws RestApiException;
 
   String getAvatarUrl(int size) throws RestApiException;
+  GeneralPreferencesInfo getPreferences() throws RestApiException;
+
   void starChange(String id) throws RestApiException;
   void unstarChange(String id) throws RestApiException;
   void addEmail(EmailInput input) throws RestApiException;
@@ -47,6 +50,11 @@ public interface AccountApi {
 
     @Override
     public String getAvatarUrl(int size) throws RestApiException {
+      throw new NotImplementedException();
+    }
+
+    @Override
+    public GeneralPreferencesInfo getPreferences() throws RestApiException {
       throw new NotImplementedException();
     }
 

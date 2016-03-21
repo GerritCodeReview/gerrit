aa9207a786267afa33676d038df07c6bcf59c1d9
Michael Zhou <zhoumotongxue008@gmail.com>
2016-03-21 13:40:59 -0400

    EditScreen: Show the original file in a CodeMirror merge view
    This adds a merge view to the inline editing screen so that
    users can diff against the latest patchset while editing.
    
    This new functionality makes use of CodeMirror's merge view addon, which
    requires Google's diff-match-patch library to be loaded. The library
    implements an algorithm based on the Myers diff in JavaScript on the
    client side, so there is no extra usage of Gerrit's DiffApi.
    
    The minified diff-match-patch library is bundled with CodeMirror's merge
    view addon during the build process.
    
    By default, the merge view is enabled. This can be changed via
    EditPreferencesBox. Also added a checkbox to the EditScreen header to
    toggle the view.
    
    Screenshot: http://i.imgur.com/VJbyEKQ.png
    
    Change-Id: I5eb4af9b67bbfcb0b149965b3c818c1f6118e6de

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/client/EditPreferencesInfo.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/client/EditPreferencesInfo.java
@@ -27,6 +27,7 @@ public class EditPreferencesInfo {
   public Boolean hideLineNumbers;
   public Boolean matchBrackets;
   public Boolean autoCloseBrackets;
+  public Boolean showOriginal;
   public Theme theme;
   public KeyMapType keyMapType;
 
@@ -43,6 +44,7 @@ public class EditPreferencesInfo {
     i.hideLineNumbers = false;
     i.matchBrackets = true;
     i.autoCloseBrackets = false;
+    i.showOriginal = true;
     i.theme = Theme.DEFAULT;
     i.keyMapType = KeyMapType.DEFAULT;
     return i;

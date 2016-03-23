c11eae02a7e74e5d022109fab98e9c44b605da60
Dave Borowitz <dborowitz@google.com>
2016-03-23 13:35:03 -0400

    Factor out an arguments class for ChangeNotes
    We do a semi-manual injection thing with ChangeNotes.Factory that
    makes adding new arguments painful. Limit the pain by encapsulating
    into an injectable Args class.
    
    This has more side benefits, like making DraftCommentNotes able to use
    straight assisted injection, and simplifying TestChanges.
    
    Change-Id: I747aefb8d6e16ad6488b2a686bd23304a0fe24f6

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/NoteDbModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/NoteDbModule.java
@@ -21,6 +21,7 @@ public class NoteDbModule extends FactoryModule {
   public void configure() {
     factory(ChangeUpdate.Factory.class);
     factory(ChangeDraftUpdate.Factory.class);
+    factory(DraftCommentNotes.Factory.class);
     factory(NoteDbUpdateManager.Factory.class);
   }
 }

c167261e48f1e7bc7c4743faf7e6b6bd4c93c878
Shawn Pearce <sop@google.com>
2016-03-24 20:06:31 -0700

    Fix non-Javadoc comment formatting inside method
    Change-Id: I57aae0c75c7d21b2baa29a945c7e1aa3edf7527a

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeInserter.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeInserter.java
@@ -392,11 +392,11 @@ public class ChangeInserter extends BatchUpdate.InsertChangeOp {
       }
     }
 
-    /** For labels that are not set in this operation, show the "current" value
+    /* For labels that are not set in this operation, show the "current" value
      * of 0, and no oldValue as the value was not modified by this operation.
      * For labels that are set in this operation, the value was modified, so
      * show a transition from an oldValue of 0 to the new value.
-     **/
+     */
     if (runHooks) {
       ReviewDb db = ctx.getDb();
       hooks.doPatchsetCreatedHook(change, patchSet, db);

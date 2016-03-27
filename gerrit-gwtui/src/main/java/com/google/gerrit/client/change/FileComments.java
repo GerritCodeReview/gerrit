d897fee539c94c7eb4a302611697eded63a6f974
Michael Zhou <zhoumotongxue008@gmail.com>
2016-03-27 13:18:58 -0400

    Fix side-by-side / unified navigation links
    Now that toSideBySide() explicitly redirects to SideBySide, use
    toPatch() instead when appropriate.
    
    Change-Id: Id8839c2b66f5ea39f7998ea8230e8ca0c5613d8f

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/FileComments.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/FileComments.java
@@ -49,6 +49,6 @@ class FileComments extends Composite {
   }
 
   private static String url(PatchSet.Id ps, CommentInfo info) {
-    return Dispatcher.toSideBySide(null, ps, info.path());
+    return Dispatcher.toPatch(null, ps, info.path());
   }
 }

4ea63d57d6e155c1b994f301bc0b3544a9f9553e
Björn Pedersen <iceelch@googlemail.com>
2016-04-07 18:16:57 +0200

    InlineEdit: Fix submodule editing
    Use the content as id for submodules instead of using the blob id.
    
    Note: The hash is only checked for syntactic validity (40 chars length
    and hexadecimal).
    No check is performed if the hash exists in the submodule repo (this is
    not possible for the general case anyway, as the gerrit server may not
    have access to the submodule repo, or one wants to set a not-yet pushed
    hash).
    
    Move blob creation/insertion into apply, where we can get the filemode
    to decide wether we need to create a blob or if we update just a
    gitlink.
    
    Errors are remembered and re-thrown.
    
    Bug: Issue 3603
    Change-Id: I1e04cecff9598e0bf8e1443713dce84ee3482f7e

--- gerrit-server/src/main/java/com/google/gerrit/server/edit/ChangeEditModifier.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/edit/ChangeEditModifier.java
@@ -341,7 +341,7 @@ public class ChangeEditModifier {
           reader,
           file,
           newFile,
-          toBlob(inserter, content));
+          content);
       if (ObjectId.equals(newTree, prevEdit.getTree())) {
         throw new InvalidChangeOperationException("no changes were made");
       }
@@ -403,9 +403,9 @@ public class ChangeEditModifier {
   }
 
   private static ObjectId writeNewTree(TreeOperation op, RevWalk rw,
-      ObjectInserter ins, RevCommit prevEdit, ObjectReader reader,
+      final ObjectInserter ins, RevCommit prevEdit, ObjectReader reader,
       String fileName, @Nullable String newFile,
-      @Nullable final ObjectId content) throws IOException {
+      @Nullable final RawInput content) throws IOException {
     DirCache newTree = readTree(reader, prevEdit);
     DirCacheEditor dce = newTree.editor();
     switch (op) {
@@ -425,15 +425,34 @@ public class ChangeEditModifier {
 
       case CHANGE_ENTRY:
         checkNotNull(content, "new content required");
+        final class ErrorHolder {
+          public IOException exception;
+        }
+
+        final ErrorHolder errorHolder = new ErrorHolder();
         dce.add(new PathEdit(fileName) {
           @Override
           public void apply(DirCacheEntry ent) {
-            if (ent.getRawMode() == 0) {
-              ent.setFileMode(FileMode.REGULAR_FILE);
+            try {
+              if (ent.getFileMode() == FileMode.GITLINK ) {
+                ent.setLength(0);
+                ent.setLastModified(0);
+                ent.setObjectId(ObjectId.fromString(
+                    ByteStreams.toByteArray(content.getInputStream()), 0));
+              } else {
+                if (ent.getRawMode() == 0) {
+                  ent.setFileMode(FileMode.REGULAR_FILE);
+                }
+                ent.setObjectId(toBlob(ins, content));
+              }
+            } catch (IOException e) {
+              errorHolder.exception = e;
             }
-            ent.setObjectId(content);
           }
         });
+        if (errorHolder.exception != null) {
+          throw errorHolder.exception;
+        }
         break;
 
       case RESTORE_ENTRY:

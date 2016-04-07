001504f1b7dda2f6c0945ad3266a61885b807804
Dave Borowitz <dborowitz@google.com>
2016-04-07 09:59:36 -0400

    Organize imports
    Change-Id: I900bf2892e0855e8e1d066000fb01d56c179a102

--- gerrit-server/src/main/java/com/google/gerrit/server/git/strategy/RebaseIfNecessary.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/strategy/RebaseIfNecessary.java
@@ -34,7 +34,6 @@ import com.google.gwtorm.server.OrmException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 public class RebaseIfNecessary extends SubmitStrategy {

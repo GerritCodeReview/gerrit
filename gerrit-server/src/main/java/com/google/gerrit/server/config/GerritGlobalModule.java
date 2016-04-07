6c122e34ad04bd185c406f1c31c71f8a8e1ad6ab
Edwin Kempin <ekempin@google.com>
2016-04-07 14:00:17 +0200

    Store SSH keys in git
    The public SSH keys of a user are now stored in an authorized_keys
    file in the All-Users repository in the refs/users/CD/ABCD branch of
    the user.
    
    Storing SSH keys in an authorized_keys file is the standard way for
    SSH to store public keys. Each key is stored on a separate line.
    
    The order of the keys in the file determines the sequence numbers of
    the keys.
    
    Invalid keys are marked with the prefix '# INVALID'.
    
    To keep the sequence numbers intact when a key is deleted, a
    '# DELETED' line is inserted at the position where the key was
    deleted.
    
    Other comment lines are ignored on read, and are not written back when
    the file is modified.
    
    Supporting a 2-step live migration for a multi-master Gerrit
    installation is not needed since the googlesource.com instances do not
    use SSH and there are no other multi-master installations.
    
    On creation of an SSH key, RFC 4716 style keys need to be converted to
    OpenSSH style keys. Also before adding a key it should be checked that
    the key is parseable. Both of this requires classes from SSH libs that
    are only available in the SSH layer. This is why the SSH key creation
    must be done there. So far this was done in SshKeyCacheImpl, but since
    SshKeyCacheImpl needs VersionedAuthorizedKeys to load keys and to mark
    keys as invalid, VersionedAuthorizedKeys cannot not depend on
    SshKeyCacheImpl as this would be a cyclic dependency. Instead split
    out the SSH key creation from SshKeyCacheImpl into SshKeyCreatorImpl.
    This way SshKeyCacheImpl depends on VersionedAuthorizedKeys and
    VersionedAuthorizedKeys depends on SshKeyCreatorImpl, and there is no
    dependency circle.
    
    Change-Id: I8fcc3c0f27e034fc2c8e8ae3612068099075467d
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -70,6 +70,7 @@ import com.google.gerrit.server.account.GroupControl;
 import com.google.gerrit.server.account.GroupDetailFactory;
 import com.google.gerrit.server.account.GroupIncludeCacheImpl;
 import com.google.gerrit.server.account.GroupMembers;
+import com.google.gerrit.server.account.VersionedAuthorizedKeys;
 import com.google.gerrit.server.api.accounts.AccountExternalIdCreator;
 import com.google.gerrit.server.auth.AuthBackend;
 import com.google.gerrit.server.auth.UniversalAuthBackend;
@@ -330,6 +331,7 @@ public class GerritGlobalModule extends FactoryModule {
     factory(SubmoduleSectionParser.Factory.class);
     factory(ReplaceOp.Factory.class);
     factory(GitModules.Factory.class);
+    factory(VersionedAuthorizedKeys.Factory.class);
 
     bind(AccountManager.class);
     factory(ChangeUserName.Factory.class);
---
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_124.java
@@ -0,0 +1,119 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.schema;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountSshKey;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.GerritPersonIdent;
+import com.google.gerrit.server.account.VersionedAuthorizedKeys;
+import com.google.gerrit.server.account.VersionedAuthorizedKeys.SimpleSshKeyCreator;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.MetaDataUpdate;
+import com.google.gwtorm.jdbc.JdbcSchema;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.BatchRefUpdate;
+import org.eclipse.jgit.lib.NullProgressMonitor;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.Map;
+
+public class Schema_124 extends SchemaVersion {
+  private final GitRepositoryManager repoManager;
+  private final AllUsersName allUsersName;
+  private final PersonIdent serverUser;
+
+  @Inject
+  Schema_124(Provider<Schema_123> prior,
+      GitRepositoryManager repoManager,
+      AllUsersName allUsersName,
+      @GerritPersonIdent PersonIdent serverUser) {
+    super(prior);
+    this.repoManager = repoManager;
+    this.allUsersName = allUsersName;
+    this.serverUser = serverUser;
+  }
+
+  @Override
+  protected void migrateData(ReviewDb db, UpdateUI ui)
+      throws OrmException, SQLException {
+    Multimap<Account.Id, AccountSshKey> imports = ArrayListMultimap.create();
+    try (Statement stmt = ((JdbcSchema) db).getConnection().createStatement();
+      ResultSet rs = stmt.executeQuery(
+          "SELECT "
+          + "account_id, "
+          + "seq, "
+          + "ssh_public_key, "
+          + "valid "
+          + "FROM account_ssh_keys")) {
+      while (rs.next()) {
+        Account.Id accountId = new Account.Id(rs.getInt(1));
+        int seq = rs.getInt(2);
+        String sshPublicKey = rs.getString(3);
+        AccountSshKey key = new AccountSshKey(
+            new AccountSshKey.Id(accountId, seq), sshPublicKey);
+        boolean valid = rs.getBoolean(4);
+        if (!valid) {
+          key.setInvalid();
+        }
+        imports.put(accountId, key);
+      }
+    }
+
+    if (imports.isEmpty()) {
+      return;
+    }
+
+    try (Repository git = repoManager.openRepository(allUsersName);
+        RevWalk rw = new RevWalk(git)) {
+      BatchRefUpdate bru = git.getRefDatabase().newBatchUpdate();
+
+      for (Map.Entry<Account.Id, Collection<AccountSshKey>> e : imports.asMap()
+          .entrySet()) {
+        try (MetaDataUpdate md = new MetaDataUpdate(
+                 GitReferenceUpdated.DISABLED, allUsersName, git, bru);
+             VersionedAuthorizedKeys authorizedKeys =
+                 new VersionedAuthorizedKeys(
+                     new SimpleSshKeyCreator(), e.getKey())) {
+          md.getCommitBuilder().setAuthor(serverUser);
+          md.getCommitBuilder().setCommitter(serverUser);
+
+          authorizedKeys.load(md);
+          authorizedKeys.setKeys(e.getValue());
+          authorizedKeys.commit(md);
+        }
+      }
+
+      bru.execute(rw, NullProgressMonitor.INSTANCE);
+    } catch (ConfigInvalidException | IOException ex) {
+      throw new OrmException(ex);
+    }
+  }
+}

537773f2f56fce70fca7170bea18f0ad8922bfed
David Pursehouse <david.pursehouse@sonymobile.com>
2016-04-07 10:19:31 +0000

    RebaseIfNecessary: Remove unused import
    Change-Id: I54c34ebe4f3f9daf9495052173137773b3795e0e

--- gerrit-server/src/main/java/com/google/gerrit/server/git/strategy/RebaseIfNecessary.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/strategy/RebaseIfNecessary.java
@@ -39,7 +39,6 @@ import org.eclipse.jgit.lib.PersonIdent;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;

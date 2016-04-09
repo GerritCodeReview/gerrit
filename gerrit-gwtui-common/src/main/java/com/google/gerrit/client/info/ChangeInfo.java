b92dab31d8d843b82224037566b70c1f315c90d0
Michael Zhou <zhoumotongxue008@gmail.com>
2016-04-09 05:55:10 -0400

    Add the refName for the base PatchSet to EditInfo
    This will be used by the EditScreen to show the diff against the
    original file.
    
    Change-Id: I70523f1007d51d69cdb7e8bf3b4e07da69bd410b

--- gerrit-gwtui-common/src/main/java/com/google/gerrit/client/info/ChangeInfo.java
+++ gerrit-gwtui-common/src/main/java/com/google/gerrit/client/info/ChangeInfo.java
@@ -312,6 +312,7 @@ public class ChangeInfo extends JavaScriptObject {
   public static class EditInfo extends JavaScriptObject {
     public final native String name() /*-{ return this.name; }-*/;
     public final native String setName(String n) /*-{ this.name = n; }-*/;
+    public final native String base() /*-{ return this.base; }-*/;
     public final native String baseRevision() /*-{ return this.base_revision; }-*/;
     public final native CommitInfo commit() /*-{ return this.commit; }-*/;
 

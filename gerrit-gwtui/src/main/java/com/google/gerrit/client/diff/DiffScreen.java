9ee5cf15dcc7a04c359acfe5de38dd4b054f551c
Michael Zhou <zhoumotongxue008@gmail.com>
2016-04-11 06:00:41 -0400

    DiffScreen: Respect the "Skip Deleted" diff preference
    The effect of this preference wasn't migrated from the old diff screen
    to the current one, and the prefernce hasn't been available in the diff
    preferences box, but the preference itself was migrated from ReviewDb to
    NoteDb (the Git backend).
    
    This change updates the diff screen header to respect this preference.
    Now that the side-by-side and the unified diff screens share the same
    code base, this feature is made available in both of them.
    
    Note that currently, the navigation links and keyboard shortcuts are not
    updated upon toggling the option in PreferencesBox: they are only
    updated when the preference is saved and the diff screen is reloaded.
    
    To support updating without refreshing, we will need to replace the
    KeyCommand in GlobalKey, which is of low priority and will be
    implemented in a future change.
    
    Bug: Issue 3445
    Change-Id: I3e98f58a24a419dc7579525c89986cff4d0a4ee9

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/DiffScreen.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/diff/DiffScreen.java
@@ -138,7 +138,8 @@ abstract class DiffScreen extends Screen {
     prefs = DiffPreferences.create(Gerrit.getDiffPreferences());
     handlers = new ArrayList<>(6);
     keysNavigation = new KeyCommandSet(Gerrit.C.sectionNavigation());
-    header = new Header(keysNavigation, base, revision, path, diffScreenType);
+    header = new Header(
+        keysNavigation, base, revision, path, diffScreenType, prefs);
   }
 
   @Override

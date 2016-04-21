44fc9f1121ad21ec7351c6ddf2d5b9f4377eaf92
Patrick Hiesel <hiesel@google.com>
2016-04-21 11:44:29 +0200

    Added /projects/name/access as REST endpoint. Implemented GET.
    We currently use RPC to view and edit access for projects. In an effort
    to migrate to polygerrit this has to be ported to REST.
    
    Change-Id: I5cc621ba28dc14bea15ae8e7a78a5ad3d0846c43

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/api/projects/AccessApiImpl.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.api.projects;
+
+import com.google.gerrit.extensions.api.projects.AccessApi;
+import com.google.gerrit.extensions.api.access.ProjectAccessInfo;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.server.account.GroupControl;
+import com.google.gerrit.server.project.GetAccess;
+import com.google.gerrit.server.project.ProjectJson;
+import com.google.gerrit.server.project.ProjectResource;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+
+import java.io.IOException;
+
+public class AccessApiImpl implements AccessApi {
+  interface Factory {
+    AccessApiImpl create(ProjectResource project, ProjectJson projectJson);
+  }
+
+  private final ProjectResource project;
+  private final ProjectJson projectJson;
+  private final GetAccess getAccess;
+
+  @Inject
+  AccessApiImpl(@Assisted ProjectResource project,
+    ProjectJson projectJson,
+    GetAccess getAccess) {
+    this.project = project;
+    this.projectJson = projectJson;
+    this.getAccess = getAccess;
+  }
+
+  @Override
+  public ProjectAccessInfo get() throws RestApiException {
+    return getAccess.apply(project);
+  }
+}

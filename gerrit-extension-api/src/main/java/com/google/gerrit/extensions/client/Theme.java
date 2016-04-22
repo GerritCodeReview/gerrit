ce5e4abce5a8deb5bc5b77b9aac72cbd9eab49a0
Michael Zhou <zhoumotongxue008@gmail.com>
2016-04-22 03:50:04 -0400

    Add all the CodeMirror themes as of 5.14.2
    This change makes all the CodeMirror themes as of the 5.14.2 release
    available in DiffScreen and EditScreen.
    
    Updated Documentation/rest-api-accounts.txt to reflect the addition.
    
    Change-Id: Ic37e2ac5cfae91ed3cf61d7dc9d3345668715dae

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/client/Theme.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/client/Theme.java
@@ -17,25 +17,103 @@ package com.google.gerrit.extensions.client;
 public enum Theme {
   // Light themes
   DEFAULT,
+  DAY_3024,
+  BASE16_LIGHT,
   ECLIPSE,
   ELEGANT,
+  MDN_LIKE,
   NEAT,
+  NEO,
+  PARAISO_LIGHT,
+  SOLARIZED_LIGHT,
+  TTCN,
+  XQ_LIGHT,
+  YETI,
 
   // Dark themes
+  NIGHT_3024,
+  ABCDEF,
+  AMBIANCE,
+  BASE16_DARK,
+  BESPIN,
+  BLACKBOARD,
+  COBALT,
+  COLORFORTH,
+  DRACULA,
+  ERLANG_DARK,
+  HOPSCOTCH,
+  ICECODER,
+  ISOTOPE,
+  LESSER_DARK,
+  LIQUIBYTE,
+  MATERIAL,
+  MBO,
   MIDNIGHT,
+  MONOKAI,
   NIGHT,
-  TWILIGHT;
+  PARAISO_DARK,
+  PASTEL_ON_DARK,
+  RAILSCASTS,
+  RUBYBLUE,
+  SETI,
+  SOLARIZED_DARK,
+  THE_MATRIX,
+  TOMORROW_NIGHT_BRIGHT,
+  TOMORROW_NIGHT_EIGHTIES,
+  TWILIGHT,
+  VIBRANT_INK,
+  XQ_DARK,
+  ZENBURN;
 
   public boolean isDark() {
     switch (this) {
+      case NIGHT_3024:
+      case ABCDEF:
+      case AMBIANCE:
+      case BASE16_DARK:
+      case BESPIN:
+      case BLACKBOARD:
+      case COBALT:
+      case COLORFORTH:
+      case DRACULA:
+      case ERLANG_DARK:
+      case HOPSCOTCH:
+      case ICECODER:
+      case ISOTOPE:
+      case LESSER_DARK:
+      case LIQUIBYTE:
+      case MATERIAL:
+      case MBO:
       case MIDNIGHT:
+      case MONOKAI:
       case NIGHT:
+      case PARAISO_DARK:
+      case PASTEL_ON_DARK:
+      case RAILSCASTS:
+      case RUBYBLUE:
+      case SETI:
+      case SOLARIZED_DARK:
+      case THE_MATRIX:
+      case TOMORROW_NIGHT_BRIGHT:
+      case TOMORROW_NIGHT_EIGHTIES:
       case TWILIGHT:
+      case VIBRANT_INK:
+      case XQ_DARK:
+      case ZENBURN:
         return true;
       case DEFAULT:
+      case DAY_3024:
+      case BASE16_LIGHT:
       case ECLIPSE:
       case ELEGANT:
+      case MDN_LIKE:
       case NEAT:
+      case NEO:
+      case PARAISO_LIGHT:
+      case SOLARIZED_LIGHT:
+      case TTCN:
+      case XQ_LIGHT:
+      case YETI:
       default:
         return false;
     }

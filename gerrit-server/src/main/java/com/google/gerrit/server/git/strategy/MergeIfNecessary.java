1e5657cfdb5afdd974301d0073e6877fee52f663
Zhen Chen <czhen@google.com>
2016-04-26 13:44:37 -0700

    Fix index out of bounds exception when nothing able to merge
    The IndexOutOfBoundsException could happen when all merge commits are
    filtered out during the MergeSorter.sort() process due to missing
    dependency.
    
    Change-Id: Ibfaaccf70cd31f84ac5df5336eedc6de5cbd11fd

--- gerrit-server/src/main/java/com/google/gerrit/server/git/strategy/MergeIfNecessary.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/strategy/MergeIfNecessary.java
@@ -34,6 +34,10 @@ public class MergeIfNecessary extends SubmitStrategy {
     List<SubmitStrategyOp> ops = new ArrayList<>(sorted.size());
     CodeReviewCommit firstFastForward;
     if (args.mergeTip.getInitialTip() == null) {
+      if (sorted.isEmpty()) {
+        throw new IntegrationException(
+            "nothing to merge, probably caused by missing dependencies");
+      }
       firstFastForward = sorted.remove(0);
     } else {
       firstFastForward = args.mergeUtil.getFirstFastForward(

fbbfa677e8175a558b4de4703d2c93e3453c9595
Edwin Kempin <ekempin@google.com>
2016-04-26 13:25:07 +0200

    Migrate project watches to git (part 1)
    In git the project watch configuration of a user is stored in a
    'watch.config' file in the refs/users/<sharded-id> user branch in the
    All-Users repository.
    
    The 'watch.config' file is a git config file that has one 'project'
    section for each project watch. This means if we ever want to watch
    other entities we can add further sections in this file (e.g. 'group'
    sections if we want to support watching of group modifications).
    
    The name of the watched project is used as subsection name and the
    events for which email notifications should be received are
    represented as 'notify' values in the subsection:
    
      [project "foo"]
        notify = ALL_COMMENTS
        notify = NEW_PATCHSETS
    
    If a project is watched with a filter, the filter is included into the
    subsection name. This prevents conflicting watch configurations for
    the same project and filter. For the same reason the primary key for
    storing watched projects in the database also consists of project and
    filter. When a filter is specified the subsection name is formatted
    like this: "<project-name>%filter=<filter-query>". '%' is chosen as a
    separator because this character is not valid in project names. Using
    "%filter=<filter-query>" makes the format extensible and follows the
    example of how parameters are specified in the target ref on push
    (e.g. "push HEAD:refs/for/master%topic=MyTopic"). A section with a
    filter would look like this:
    
      [project "foo%filter=branch:master"]
        notify = ALL_COMMENTS
        notify = NEW_PATCHSETS
    
    We also want to support watch configurations that notify on no event.
    They are e.g. useful to overwrite a watch configuration on the
    All-Projects project, but also when creating new watches in the the
    UI, the UI first creates an empty watch configuration. Only after the
    empty watch configuration is created a user can select the events that
    should send notifications. Each selection then updates the watch
    configuration. Since empty sections are omitted in git config files,
    'notify' is set to 'NONE' in this case:
    
      [project "foo%filter=branch:master"]
        notify = NONE
    
    If other notify values are specified in addition to 'NONE' they are
    ignored.
    
    To support a live migration on a multi-master Gerrit installation, the
    upgrade is done in 2 steps:
    
    - part 1 (this change):
      * always write to both backends (database and git)
      * a configuration option (user.readProjectWatchesFromGit) decides if
        the project watches are read from database or git (default:
        database)
      * upgraded instances write to both backends, old instances only
        read/write from/to the database
      * after upgrading all instances (all still read from the database)
        run a batch to copy all project watches from the database to git
      * update all instances to read from git
      * make sure all instances use the new schema version of the account
        index
    - part 2 (next change):
      * bump the database schema version
      * migrate the project watches from database to git (for single
        instance Gerrit servers)
      * delete the database table
      * delete the user.readProjectWatchesFromGit config option
    
    This change also slightly changes the behaviour of the
    DeleteWatchedProjects REST endpoint. It no longer fails if a user
    tries to delete a non-existing project watch. This was done because it
    simplifies the implementation and it should have almost no impact on
    users of this REST endpoint.
    
    The PostWatchedProjects REST endpoint now fails with Bad Request if
    the input contains two watch configurations for the same project and
    filter. Earlier this failed with an OrmDuplicateKeyException and
    resulted in Internal Server Error.
    
    This change requires that the account cache is manually flushed.
    
    Change-Id: I4f8d4e8a762aeb4b46e27bb3bf9e58f91611145d
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/test/java/com/google/gerrit/testutil/FakeAccountCache.java
+++ gerrit-server/src/test/java/com/google/gerrit/testutil/FakeAccountCache.java
@@ -19,10 +19,12 @@ import com.google.gerrit.common.TimeUtil;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.AccountExternalId;
 import com.google.gerrit.reviewdb.client.AccountGroup;
-import com.google.gerrit.reviewdb.client.AccountProjectWatch;
+import com.google.gerrit.reviewdb.client.AccountProjectWatch.NotifyType;
 import com.google.gerrit.server.account.AccountCache;
 import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.account.WatchConfig.ProjectWatchKey;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -76,6 +78,6 @@ public class FakeAccountCache implements AccountCache {
   private static AccountState newState(Account account) {
     return new AccountState(account, ImmutableSet.<AccountGroup.UUID> of(),
         ImmutableSet.<AccountExternalId> of(),
-        ImmutableSet.<AccountProjectWatch> of());
+        new HashMap<ProjectWatchKey, Collection<NotifyType>>());
   }
 }

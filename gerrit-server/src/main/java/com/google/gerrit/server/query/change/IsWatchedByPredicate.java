7073d4bbd3bb02b692f0e6226bcbb9d519d8ee9e
Edwin Kempin <ekempin@google.com>
2016-04-26 13:25:07 +0200

    Migrate project watches to git (part 1)
    In git the project watch configuration of a user is stored in a
    'watch.config' file in the refs/users/<sharded-id> user branch in the
    All-Users repository.
    
    The 'watch.config' file is a git config file that has one 'project'
    section for all project watches of a project. This means if we ever
    want to watch other entities we can add further sections in this file
    (e.g. 'group' sections if we want to support watching of group
    modifications).
    
    The project name is used as subsection name and the filters with the
    notify types that decide for which events email notifications should
    be sent are represented as 'notify' values in the subsection. A
    'notify' value is formatted as
    "<filter> [<comma-separated-list-of-notify-types>]":
    
      [project "foo"]
        notify = * [ALL_COMMENTS]
        notify = branch:master [ALL_COMMENTS, NEW_PATCHSETS]
        notify = branch:master owner:self [SUBMITTED_CHANGES]
    
    For a change event a notification will be send if any 'notify' value
    of the corresponding project has both, a filter that matches the
    change and a notify type that matches the event.
    
    If two notify values in the same subsection have the same filter they
    are merged on the next save, taking the union of the notify types.
    
    For watch configurations that notify on no event the list of notify
    types is empty:
    
      [project "foo"]
        notify = branch:master []
    
    Unknown notify types are ignored and removed on save.
    
    To support a live migration on a multi-master Gerrit installation, the
    upgrade is done in 2 steps:
    
    - part 1 (this change):
      * always write to both backends (database and git)
      * a configuration option (user.readProjectWatchesFromGit) decides if
        the project watches are read from database or git (default:
        database)
      * upgraded instances write to both backends, old instances only
        read/write from/to the database
      * after upgrading all instances (all still read from the database)
        run a batch to copy all project watches from the database to git
      * update all instances to read from git
      * make sure all instances use the new schema version of the account
        index
    - part 2 (next change):
      * bump the database schema version
      * migrate the project watches from database to git (for single
        instance Gerrit servers)
      * delete the database table
      * delete the user.readProjectWatchesFromGit config option
    
    This change requires that the account cache is manually evicted.
    
    Change-Id: I4f8d4e8a762aeb4b46e27bb3bf9e58f91611145d
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/IsWatchedByPredicate.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/IsWatchedByPredicate.java
@@ -15,8 +15,8 @@
 package com.google.gerrit.server.query.change;
 
 import com.google.common.collect.ImmutableList;
-import com.google.gerrit.reviewdb.client.AccountProjectWatch;
 import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.WatchConfig.ProjectWatchKey;
 import com.google.gerrit.server.query.AndPredicate;
 import com.google.gerrit.server.query.Predicate;
 import com.google.gerrit.server.query.QueryBuilder;
@@ -48,11 +48,11 @@ class IsWatchedByPredicate extends AndPredicate<ChangeData> {
       boolean checkIsVisible) throws QueryParseException {
     List<Predicate<ChangeData>> r = new ArrayList<>();
     ChangeQueryBuilder builder = new ChangeQueryBuilder(args);
-    for (AccountProjectWatch w : getWatches(args)) {
+    for (ProjectWatchKey w : getWatches(args)) {
       Predicate<ChangeData> f = null;
-      if (w.getFilter() != null) {
+      if (w.filter() != null) {
         try {
-          f = builder.parse(w.getFilter());
+          f = builder.parse(w.filter());
           if (QueryBuilder.find(f, IsWatchedByPredicate.class) != null) {
             // If the query is going to infinite loop, assume it
             // will never match and return null. Yes this test
@@ -66,10 +66,10 @@ class IsWatchedByPredicate extends AndPredicate<ChangeData> {
       }
 
       Predicate<ChangeData> p;
-      if (w.getProjectNameKey().equals(args.allProjectsName)) {
+      if (w.project().equals(args.allProjectsName)) {
         p = null;
       } else {
-        p = builder.project(w.getProjectNameKey().get());
+        p = builder.project(w.project().get());
       }
 
       if (p != null && f != null) {
@@ -91,14 +91,14 @@ class IsWatchedByPredicate extends AndPredicate<ChangeData> {
     }
   }
 
-  private static Collection<AccountProjectWatch> getWatches(
+  private static Collection<ProjectWatchKey> getWatches(
       ChangeQueryBuilder.Arguments args) throws QueryParseException {
     CurrentUser user = args.getUser();
     if (user.isIdentifiedUser()) {
       return args.accountCache.get(args.getUser().getAccountId())
-          .getProjectWatches();
+          .getProjectWatches().keySet();
     }
-    return Collections.<AccountProjectWatch> emptySet();
+    return Collections.<ProjectWatchKey> emptySet();
   }
 
   private static List<Predicate<ChangeData>> none() {

8b8a69962cfa9d640a97705135d7a7631a77fd43
Edwin Kempin <ekempin@google.com>
2016-04-26 13:25:07 +0200

    Migrate project watches to git (part 1)
    In git the project watch configuration of a user is stored in a
    'watch.config' file in the refs/users/<sharded-id> user branch in the
    All-Users repository.
    
    The 'watch.config' file is a git config file that has one 'project'
    section for all project watches of a project. This means if we ever
    want to watch other entities we can add further sections in this file
    (e.g. 'group' sections if we want to support watching of group
    modifications).
    
    The project name is used as subsection name and the filters with the
    notify types that decide for which events email notifications should
    be sent are represented as 'notify' values in the subsection. A
    'notify' value is formatted as
    "<filter> [<comma-separated-list-of-notify-types>]":
    
      [project "foo"]
        notify = * [ALL_COMMENTS]
        notify = branch:master [ALL_COMMENTS, NEW_PATCHSETS]
        notify = branch:master owner:self [SUBMITTED_CHANGES]
    
    For a change event a notification will be send if any 'notify' value
    of the corresponding project has both, a filter that matches the
    change and a notify type that matches the event.
    
    If two notify values in the same subsection have the same filter they
    are merged on the next save, taking the union of the notify types.
    
    For watch configurations that notify on no event the list of notify
    types is empty:
    
      [project "foo"]
        notify = branch:master []
    
    Unknown notify types are ignored and removed on save.
    
    To support a live migration on a multi-master Gerrit installation, the
    upgrade is done in 2 steps:
    
    - part 1 (this change):
      * always write to both backends (database and git)
      * a configuration option (user.readProjectWatchesFromGit) decides if
        the project watches are read from database or git (default:
        database)
      * upgraded instances write to both backends, old instances only
        read/write from/to the database
      * after upgrading all instances (all still read from the database)
        run a batch to copy all project watches from the database to git
      * update all instances to read from git
      * make sure all instances use the new schema version of the account
        index
    - part 2 (next change):
      * bump the database schema version
      * migrate the project watches from database to git (for single
        instance Gerrit servers)
      * delete the database table
      * delete the user.readProjectWatchesFromGit config option
    
    This change requires that the account cache is manually evicted.
    
    Change-Id: I4f8d4e8a762aeb4b46e27bb3bf9e58f91611145d
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/account/PostWatchedProjects.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/PostWatchedProjects.java
@@ -22,19 +22,26 @@ import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.AccountProjectWatch;
+import com.google.gerrit.reviewdb.client.AccountProjectWatch.NotifyType;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.WatchConfig.ProjectWatchKey;
 import com.google.gerrit.server.project.ProjectsCollection;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
+import org.eclipse.jgit.errors.ConfigInvalidException;
+
 import java.io.IOException;
+import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Singleton
@@ -45,39 +52,41 @@ public class PostWatchedProjects
   private final GetWatchedProjects getWatchedProjects;
   private final ProjectsCollection projectsCollection;
   private final AccountCache accountCache;
+  private final WatchConfig.Accessor watchConfig;
 
   @Inject
   public PostWatchedProjects(Provider<ReviewDb> dbProvider,
       Provider<IdentifiedUser> self,
       GetWatchedProjects getWatchedProjects,
       ProjectsCollection projectsCollection,
-      AccountCache accountCache) {
+      AccountCache accountCache,
+      WatchConfig.Accessor watchConfig) {
     this.dbProvider = dbProvider;
     this.self = self;
     this.getWatchedProjects = getWatchedProjects;
     this.projectsCollection = projectsCollection;
     this.accountCache = accountCache;
+    this.watchConfig = watchConfig;
   }
 
   @Override
   public List<ProjectWatchInfo> apply(AccountResource rsrc,
-      List<ProjectWatchInfo> input)
-      throws OrmException, RestApiException, IOException {
+      List<ProjectWatchInfo> input) throws OrmException, RestApiException,
+          IOException, ConfigInvalidException {
     if (self.get() != rsrc.getUser()
-      && !self.get().getCapabilities().canAdministrateServer()) {
+        && !self.get().getCapabilities().canAdministrateServer()) {
       throw new AuthException("not allowed to edit project watches");
     }
     Account.Id accountId = rsrc.getUser().getAccountId();
-    List<AccountProjectWatch> accountProjectWatchList =
-        getAccountProjectWatchList(input, accountId);
-    dbProvider.get().accountProjectWatches().upsert(accountProjectWatchList);
+    updateInDb(accountId, input);
+    updateInGit(accountId, input);
     accountCache.evict(accountId);
     return getWatchedProjects.apply(rsrc);
   }
 
-  private List<AccountProjectWatch> getAccountProjectWatchList(
-      List<ProjectWatchInfo> input, Account.Id accountId)
-      throws UnprocessableEntityException, BadRequestException, IOException {
+  private void updateInDb(Account.Id accountId, List<ProjectWatchInfo> input)
+      throws BadRequestException, UnprocessableEntityException, IOException,
+      OrmException {
     Set<AccountProjectWatch.Key> keys = new HashSet<>();
     List<AccountProjectWatch> watchedProjects = new LinkedList<>();
     for (ProjectWatchInfo a : input) {
@@ -87,15 +96,11 @@ public class PostWatchedProjects
 
       Project.NameKey projectKey =
           projectsCollection.parse(a.project).getNameKey();
-
       AccountProjectWatch.Key key =
           new AccountProjectWatch.Key(accountId, projectKey, a.filter);
       if (!keys.add(key)) {
-        throw new BadRequestException(
-            "duplicate entry for project " + key.getProjectName().get()
-                + (!AccountProjectWatch.FILTER_ALL.equals(key.getFilter().get())
-                    ? " and filter " + key.getFilter().get()
-                    : ""));
+        throw new BadRequestException("duplicate entry for project "
+            + format(key.getProjectName().get(), key.getFilter().get()));
       }
       AccountProjectWatch apw = new AccountProjectWatch(key);
       apw.setNotify(AccountProjectWatch.NotifyType.ABANDONED_CHANGES,
@@ -110,10 +115,61 @@ public class PostWatchedProjects
           toBoolean(a.notifySubmittedChanges));
       watchedProjects.add(apw);
     }
-    return watchedProjects;
+    dbProvider.get().accountProjectWatches().upsert(watchedProjects);
+  }
+
+  private void updateInGit(Account.Id accountId, List<ProjectWatchInfo> input)
+      throws BadRequestException, UnprocessableEntityException, IOException,
+      ConfigInvalidException {
+    watchConfig.upsertProjectWatches(accountId, asMap(input));
+  }
+
+  private Map<ProjectWatchKey, Set<NotifyType>> asMap(
+      List<ProjectWatchInfo> input) throws BadRequestException,
+          UnprocessableEntityException, IOException {
+    Map<ProjectWatchKey, Set<NotifyType>> m = new HashMap<>();
+    for (ProjectWatchInfo info : input) {
+      if (info.project == null) {
+        throw new BadRequestException("project name must be specified");
+      }
+
+      ProjectWatchKey key = ProjectWatchKey.create(
+          projectsCollection.parse(info.project).getNameKey(), info.filter);
+      if (m.containsKey(key)) {
+        throw new BadRequestException(
+            "duplicate entry for project " + format(info.project, info.filter));
+      }
+
+      Set<NotifyType> notifyValues = EnumSet.noneOf(NotifyType.class);
+      if (toBoolean(info.notifyAbandonedChanges)) {
+        notifyValues.add(NotifyType.ABANDONED_CHANGES);
+      }
+      if (toBoolean(info.notifyAllComments)) {
+        notifyValues.add(NotifyType.ALL_COMMENTS);
+      }
+      if (toBoolean(info.notifyNewChanges)) {
+        notifyValues.add(NotifyType.NEW_CHANGES);
+      }
+      if (toBoolean(info.notifyNewPatchSets)) {
+        notifyValues.add(NotifyType.NEW_PATCHSETS);
+      }
+      if (toBoolean(info.notifySubmittedChanges)) {
+        notifyValues.add(NotifyType.SUBMITTED_CHANGES);
+      }
+
+      m.put(key, notifyValues);
+    }
+    return m;
   }
 
   private boolean toBoolean(Boolean b) {
     return b == null ? false : b;
+  }
+
+  private static String format(String project, String filter) {
+    return project
+        + (filter != null && !AccountProjectWatch.FILTER_ALL.equals(filter)
+            ? " and filter " + filter
+            : "");
   }
 }

7e520d2e26af050ef3cee789d46308f17a6f5ea7
Patrick Hiesel <hiesel@google.com>
2016-04-28 10:51:47 +0200

    Added DELETE/POST endpoint for watched projects
    Added DELETE and POST endpoints for removing,
    adding and updating watched projects. This is an
    effort towards migrating to PolyGerrit UI.
    
    Change-Id: I6203af9ed0fd6448fc1395512bc3ffdca93c5c18

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/account/WatchedProjectsIT.java
@@ -0,0 +1,141 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.acceptance.rest.account;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.Lists;
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.extensions.api.accounts.DeleteWatchedProjectsInput;
+import com.google.gerrit.extensions.client.ProjectWatchInfo;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class WatchedProjectsIT extends AbstractDaemonTest {
+
+  private static final String NEW_PROJECT_NAME = "newProjectAccess";
+
+  @Test
+  public void setAndGetWatchedProjects() throws Exception {
+    String projectName1 = createProject(NEW_PROJECT_NAME).get();
+    String projectName2 = createProject(NEW_PROJECT_NAME + "2").get();
+
+    List<ProjectWatchInfo> projectsToWatch = new ArrayList<>(2);
+
+    ProjectWatchInfo wpi = new ProjectWatchInfo();
+    wpi.project = projectName1;
+    wpi.notifyAbandonedChanges = true;
+    wpi.notifyNewChanges = true;
+    wpi.notifyAllComments = true;
+    projectsToWatch.add(wpi);
+
+    wpi = new ProjectWatchInfo();
+    wpi.project = projectName2;
+    wpi.filter = "branch:master";
+    wpi.notifySubmittedChanges = true;
+    wpi.notifyNewPatchSets = true;
+    projectsToWatch.add(wpi);
+
+    List<ProjectWatchInfo> persistedWatchedProjects =
+        gApi.accounts().self().setWatchedProjects(projectsToWatch);
+    assertThat(persistedWatchedProjects)
+        .containsExactlyElementsIn(projectsToWatch).inOrder();
+  }
+
+  @Test
+  public void setAndDeleteWatchedProjects() throws Exception {
+    String projectName1 = createProject(NEW_PROJECT_NAME).get();
+    String projectName2 = createProject(NEW_PROJECT_NAME + "2").get();
+
+    List<ProjectWatchInfo> projectsToWatch = new LinkedList<>();
+
+    ProjectWatchInfo wpi = new ProjectWatchInfo();
+    wpi.project = projectName1;
+    wpi.notifyAbandonedChanges = true;
+    wpi.notifyNewChanges = true;
+    wpi.notifyAllComments = true;
+    projectsToWatch.add(wpi);
+
+    wpi = new ProjectWatchInfo();
+    wpi.project = projectName2;
+    wpi.filter = "branch:master";
+    wpi.notifySubmittedChanges = true;
+    wpi.notifyNewPatchSets = true;
+    projectsToWatch.add(wpi);
+
+    // Persist watched projects
+    gApi.accounts().self().setWatchedProjects(projectsToWatch);
+
+    DeleteWatchedProjectsInput d = new DeleteWatchedProjectsInput();
+    d.watchedProjects = Lists.newArrayList(projectName2);
+    gApi.accounts().self().deleteWatchedProjects(d);
+    projectsToWatch.remove(wpi);
+
+    List<ProjectWatchInfo> persistedWatchedProjects =
+        gApi.accounts().self().getWatchedProjects();
+
+    assertThat(persistedWatchedProjects).doesNotContain(wpi);
+    assertThat(persistedWatchedProjects).containsAllIn(projectsToWatch);
+  }
+
+  @Test
+  public void watchNonExistingProject() throws Exception {
+    String projectName = NEW_PROJECT_NAME + "3";
+
+    List<ProjectWatchInfo> projectsToWatch = new ArrayList<>(2);
+
+    ProjectWatchInfo wpi = new ProjectWatchInfo();
+    wpi.project = projectName;
+    wpi.notifyAbandonedChanges = true;
+    wpi.notifyNewChanges = true;
+    wpi.notifyAllComments = true;
+    projectsToWatch.add(wpi);
+
+    exception.expect(UnprocessableEntityException.class);
+    gApi.accounts().self().setWatchedProjects(projectsToWatch);
+  }
+
+  @Test
+  public void deleteNonExistingProject() throws Exception {
+    String projectName = createProject(NEW_PROJECT_NAME + "4").get();
+
+    // Let another user watch a project
+    setApiUser(admin);
+    List<ProjectWatchInfo> projectsToWatch = new LinkedList<>();
+
+    ProjectWatchInfo wpi = new ProjectWatchInfo();
+    wpi.project = projectName;
+    wpi.notifyAbandonedChanges = true;
+    wpi.notifyNewChanges = true;
+    wpi.notifyAllComments = true;
+    projectsToWatch.add(wpi);
+
+    gApi.accounts().self().setWatchedProjects(projectsToWatch);
+
+    // Try to delete a watched project using a different user
+    DeleteWatchedProjectsInput d = new DeleteWatchedProjectsInput();
+    d.watchedProjects = Lists.newArrayList(projectName);
+    gApi.accounts().self().deleteWatchedProjects(d);
+
+    setApiUser(user);
+    exception.expect(UnprocessableEntityException.class);
+    gApi.accounts().self().deleteWatchedProjects(d);
+  }
+}

22dc6dd84aa433509deee5a1baeb55cf36e8af85
Zhen Chen <czhen@google.com>
2016-05-02 15:14:43 -0700

    Add merge feature into /changes REST endpoint
    Allow user to create a merge in Gerrit via POST /changes REST API.
    By adding a "merge" attribute which could be a SHA1, a branch name or a
    tag name, etc in the post JSON, gerrit will create a merge commit
    instead of an empty commit. If there are conflicts, the request will be
    rejected.
    
    Add dry run end point into GET
    /projects/{project}/branches/{branch}/mergeable with query parameter
    source-ref (required) and merge-strategy (optional) and return a
    MergeableInfo entity.
    
    Change-Id: I8f45f324704b3ff3eb20cb57c6e3bd75f2bf60ef

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/common/MergeInput.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/common/MergeInput.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2014 The Android Open Source Project
+// Copyright (C) 2016 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,16 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+//
 
 package com.google.gerrit.extensions.common;
 
-import com.google.gerrit.extensions.client.SubmitType;
-
-import java.util.List;
-
-public class MergeableInfo {
-  public SubmitType submitType;
+/**
+ * Created by czhen on 5/10/16.
+ */
+public class MergeInput {
+  public String sourceRef;
   public String mergeStrategy;
-  public boolean mergeable;
-  public List<String> mergeableInto;
 }

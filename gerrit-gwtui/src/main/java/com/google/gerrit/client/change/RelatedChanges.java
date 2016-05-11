32a90705b589a1201418ebea018cde947db2a0f4
Orgad Shaneh <orgads@gmail.com>
2016-05-11 09:01:21 +0300

    RelatedChanges: Show "Submitted Together" also for changes with topic
    The reason for hiding the Submitted Together tab is that when
    SubmitWholeTopic option is enabled, the same changes would appear twice
    - in Submitted Together and in Same Topic.
    
    But when some changes have topic, and the Submit Whole Topic option is
    disabled, there's no reason to hide the Submitted Together tab.
    
    Change-Id: Ibbafbdbea921444278efc602801d28b756145f25

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/RelatedChanges.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/RelatedChanges.java
@@ -215,6 +215,11 @@ public class RelatedChanges extends TabPanel {
         EnumSet.of(ListChangesOption.CURRENT_REVISION, ListChangesOption.CURRENT_COMMIT),
         new TabChangeListCallback(Tab.CHERRY_PICKS, info.project(), revision));
 
+    // TODO(sbeller): show only on latest revision
+    ChangeApi.change(info.legacyId().get()).view("submitted_together")
+        .get(new TabChangeListCallback(Tab.SUBMITTED_TOGETHER,
+            info.project(), revision));
+
     if (!Gerrit.info().change().isSubmitWholeTopicEnabled()
         && info.topic() != null && !"".equals(info.topic())) {
       StringBuilder topicQuery = new StringBuilder();
@@ -226,11 +231,6 @@ public class RelatedChanges extends TabPanel {
                      ListChangesOption.DETAILED_LABELS,
                      ListChangesOption.LABELS),
           new TabChangeListCallback(Tab.SAME_TOPIC, info.project(), revision));
-    } else {
-      // TODO(sbeller): show only on latest revision
-      ChangeApi.change(info.legacyId().get()).view("submitted_together")
-          .get(new TabChangeListCallback(Tab.SUBMITTED_TOGETHER,
-              info.project(), revision));
     }
   }
 

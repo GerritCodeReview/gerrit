e133e3a375990c5960740766e54a76eee9531de1
Luca Milanesio <luca.milanesio@gmail.com>
2016-05-12 11:33:30 +0100

    Allow to listen to Web login/logout events
    When using Gerrit with external authentication systems (OAuth or other)
    it would be necessary to enforce additional requirements (e.g. 2-factor)
    or introduce some plug-in specific post-login screen for finalising
    the user's on boarding.
    
    Similarly when the user logs out, we may need to invalidate its token
    from an external SSO system or to perform some other plugin-specific
    operations or even simply request a feedback.
    
    With the introduction of this new extension point WebLoginListener
    it is possible to filter the HTTP response and override the status
    code to redirect or perform additional adjustments to comply with 
    the company or the plugin's requirements.
    
    It is possible to experiment this new extension with a simple
    Groovy scripting plugin (see below).
    
    ```
    import com.google.gerrit.extensions.annotations.Listen
    import com.google.gerrit.extensions.events.WebLoginListener
    import javax.servlet.http.*
    import com.google.inject.*
    
    @Singleton
    @Listen
    public class MyPostLogin implements WebLoginListener {
       
       public void afterLogin(HttpServletRequest req, HttpServletResponse resp) { 
          println "After login go to my 2-phase auth site" 
          resp.sendRedirect("https://twophase.mycompany.com/auth")
       }
    
       public void afterLogout(HttpServletRequest req, HttpServletResponse resp) { 
          println "After logout go to my SSO logout" 
          resp.sendRedirect("https://ssologout.mycompany.com")
       }
    }
    ```
    
    Change-Id: I76e8ec040072e317061234665a0d865927da55b9

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/UniversalWebLoginFilter.java
@@ -0,0 +1,90 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd;
+
+import com.google.gerrit.extensions.events.WebLoginListener;
+import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.inject.servlet.ServletModule;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UniversalWebLoginFilter implements Filter {
+  private final DynamicItem<WebSession> session;
+  private final DynamicSet<WebLoginListener> webLoginListeners;
+
+  public static ServletModule module() {
+    return new ServletModule() {
+      @Override
+      protected void configureServlets() {
+        filter("/login*", "/logout*").through(UniversalWebLoginFilter.class);
+        bind(UniversalWebLoginFilter.class).in(Singleton.class);
+
+        DynamicSet.setOf(binder(), WebLoginListener.class);
+      }
+    };
+  }
+
+  @Inject
+  public UniversalWebLoginFilter(DynamicItem<WebSession> session,
+      DynamicSet<WebLoginListener> webLoginListeners) {
+    this.session = session;
+    this.webLoginListeners = webLoginListeners;
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+      FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponseRecorder wrappedResponse =
+        new HttpServletResponseRecorder((HttpServletResponse) response);
+
+    boolean loggedInBefore = session.get().isSignedIn();
+    chain.doFilter(request, wrappedResponse);
+    boolean loggedInAfter = session.get().isSignedIn();
+
+    if (!loggedInBefore && loggedInAfter) {
+      for (WebLoginListener loginListener : webLoginListeners) {
+        loginListener.onLogin(httpRequest, wrappedResponse);
+      }
+    } else if (loggedInBefore && !loggedInAfter) {
+      for (WebLoginListener loginListener : webLoginListeners) {
+        loginListener.onLogout(httpRequest, wrappedResponse);
+      }
+    }
+
+    wrappedResponse.play();
+  }
+
+  @Override
+  public void destroy() {
+  }
+
+}

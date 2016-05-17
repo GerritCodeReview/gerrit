5569171e8110ed3396dc6bf671509d77d37ffe72
Alexandre Philbert <alexandre.philbert@ericsson.com>
2016-05-17 15:08:50 -0400

    Auto-abandon draft changes in ChangeCleanup
    Allows the ChangeCleanup to abandon draft changes by only firing the
    ChangeAbandoned event. The purpose is to clean up old changes without
    triggering listeners to events that would be fired by publishing the
    draft.
    
    Change-Id: Ic0ef1cefc8212eb6f8149ae5c0e55b84398ba9f2

--- gerrit-server/src/main/java/com/google/gerrit/server/change/AbandonUtil.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/AbandonUtil.java
@@ -15,6 +15,8 @@
 package com.google.gerrit.server.change;
 
 import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.server.InternalUser;
 import com.google.gerrit.server.config.ChangeCleanupConfig;
 import com.google.gerrit.server.project.ChangeControl;
@@ -22,6 +24,7 @@ import com.google.gerrit.server.query.QueryParseException;
 import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.query.change.ChangeQueryBuilder;
 import com.google.gerrit.server.query.change.QueryProcessor;
+import com.google.gwtorm.server.AtomicUpdate;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -29,6 +32,7 @@ import com.google.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -56,38 +60,89 @@ public class AbandonUtil {
     this.abandon = abandon;
   }
 
-  public void abandonInactiveOpenChanges() {
+  public void abandonInactiveChanges() {
     if (cfg.getAbandonAfter() <= 0) {
       return;
     }
 
-    try {
-      String query = "status:new age:"
-          + TimeUnit.MILLISECONDS.toMinutes(cfg.getAbandonAfter())
-          + "m";
-      if (!cfg.getAbandonIfMergeable()) {
-        query += " -is:mergeable";
-      }
-      List<ChangeData> changesToAbandon = queryProcessor.enforceVisibility(false)
-          .queryChanges(queryBuilder.parse(query)).changes();
-      int count = 0;
-      for (ChangeData cd : changesToAbandon) {
-        try {
-          abandon.abandon(changeControl(cd), cfg.getAbandonMessage());
-          count++;
-        } catch (ResourceConflictException e) {
-          // Change was already merged or abandoned.
-        } catch (Throwable e) {
-          log.error(String.format(
-              "Failed to auto-abandon inactive open change %d.",
-                  cd.getId().get()), e);
-        }
-      }
-      log.info(String.format("Auto-Abandoned %d of %d changes.",
-          count, changesToAbandon.size()));
-    } catch (QueryParseException | OrmException e) {
-      log.error("Failed to query inactive open changes for auto-abandoning.", e);
+    long abandonAfterMinutes =
+        TimeUnit.MILLISECONDS.toMinutes(cfg.getAbandonAfter());
+    String openQuery = "status:new age:" + abandonAfterMinutes + "m";
+    String draftQuery = "status:draft age:" + abandonAfterMinutes + "m";
+
+
+    if (!cfg.getAbandonIfMergeable()) {
+      openQuery += " -is:mergeable";
+      draftQuery += " -is:mergeable";
     }
+
+    List<ChangeData> openChangesToAbandon = new ArrayList<>();
+    List<ChangeData> draftsToAbandon = new ArrayList<>();
+    try {
+      openChangesToAbandon = queryProcessor.enforceVisibility(false)
+          .queryChanges(queryBuilder.parse(openQuery)).changes();
+
+      if (cfg.getAbandonDrafts()) {
+        draftsToAbandon = queryProcessor.enforceVisibility(false)
+            .queryChanges(queryBuilder.parse(draftQuery)).changes();
+      }
+    } catch (QueryParseException | OrmException e) {
+      log.error("Failed to query inactive changes for auto-abandoning.", e);
+      return;
+    }
+
+    int openAbandonedCount = 0;
+    for (ChangeData cd : openChangesToAbandon) {
+      if (abandon(cd)) {
+        openAbandonedCount++;
+      }
+    }
+    log.info(String.format("Auto-Abandoned %d of %d open changes.",
+        openAbandonedCount, openChangesToAbandon.size()));
+
+    int draftsAbandonedCount = 0;
+    for (ChangeData cd : draftsToAbandon) {
+      try {
+        cd.db().patchSets().atomicUpdate(cd.change().currentPatchSetId(),
+            new AtomicUpdate<PatchSet>() {
+              @Override
+              public PatchSet update(PatchSet patchset) {
+                patchset.setDraft(false);
+                return patchset;
+              }
+            });
+        cd.db().changes().atomicUpdate(cd.change().getId(),
+            new AtomicUpdate<Change>() {
+              @Override
+              public Change update(Change change) {
+                change.setStatus(Change.Status.NEW);
+                return change;
+              }
+            });
+      } catch (OrmException e) {
+        log.error("Failed to undraft change when preparing to abandon.", e);
+        continue;
+      }
+      if (abandon(cd)) {
+        draftsAbandonedCount++;
+      }
+    }
+    log.info(String.format("Auto-Abandoned %d of %d draft changes.",
+        draftsAbandonedCount, draftsToAbandon.size()));
+  }
+
+  private boolean abandon(ChangeData cd) {
+    try {
+      abandon.abandon(changeControl(cd), cfg.getAbandonMessage());
+    } catch (ResourceConflictException e) {
+      // Change was already merged or abandoned.
+      return false;
+    } catch (Throwable e) {
+      log.error(String.format("Failed to auto-abandon inactive change %d.",
+          cd.getId().get()), e);
+      return false;
+    }
+    return true;
   }
 
   private ChangeControl changeControl(ChangeData cd) throws OrmException {

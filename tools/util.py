31c8d6159bd2d7381554b58a5bd449a0f6238c81
Yuxuan 'fishy' Wang <fishywang@google.com>
2016-05-19 14:09:06 -0700

    Use https for maven downloads.
    repo1.maven.org supports https now, so switch to https to avoid DNS
    attacks and other attacks while downloading maven packages.
    
    Also gerrit-maven.storage.googleapis.com certainly supports https as
    well, so switched that to https, too.
    
    central.maven.org doesn't have a valid certificate, so I didn't change
    that link in docs.
    
    Change-Id: Ib619b7e6c3eb0ca89c728631b0c62814fbea6809

--- tools/util.py
+++ tools/util.py
@@ -16,9 +16,9 @@ import os
 from os import path
 
 REPO_ROOTS = {
-  'GERRIT': 'http://gerrit-maven.storage.googleapis.com',
+  'GERRIT': 'https://gerrit-maven.storage.googleapis.com',
   'GERRIT_API': 'https://gerrit-api.commondatastorage.googleapis.com/release',
-  'MAVEN_CENTRAL': 'http://repo1.maven.org/maven2',
+  'MAVEN_CENTRAL': 'https://repo1.maven.org/maven2',
   'MAVEN_LOCAL': 'file://' + path.expanduser('~/.m2/repository'),
   'MAVEN_SNAPSHOT': 'https://oss.sonatype.org/content/repositories/snapshots',
 }

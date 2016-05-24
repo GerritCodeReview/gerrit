4af9176bf6638199dd2635309a7569346306ea45
David Pursehouse <dpursehouse@collab.net>
2016-05-24 12:05:35 +0900

    RFC: Add endpoint to get file content type
    Change I3be4aeb009 replaced the "Get file type" REST API endpoint with
    the /content endpoint. According to documentation [1], the content type
    can be retrieved by making a HEAD request to the endpoint.
    
    The REST API framework does not actually support HEAD requests.
    
    Add a new GET endpoint to get the content type.
    
    [1] https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#get-content
    
    Bug: Issue 4136
    Change-Id: If5eaaba2fec94499f332970ce6b8c6db2ac9c571

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/GetContentType.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.gerrit.extensions.restapi.BinaryResult;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.reviewdb.client.Patch;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.eclipse.jgit.lib.ObjectId;
+
+import java.io.IOException;
+
+@Singleton
+public class GetContentType implements RestReadView<FileResource> {
+  private final FileContentUtil fileContentUtil;
+
+  @Inject
+  GetContentType(FileContentUtil fileContentUtil) {
+    this.fileContentUtil = fileContentUtil;
+  }
+
+  @Override
+  public BinaryResult apply(FileResource rsrc)
+      throws ResourceNotFoundException, IOException, NoSuchChangeException,
+      OrmException {
+    String path = rsrc.getPatchKey().get();
+    String contentType;
+    if (Patch.COMMIT_MSG.equals(path)) {
+      contentType = FileContentUtil.TEXT_X_GERRIT_COMMIT_MESSAGE;
+    } else {
+      contentType = fileContentUtil.getContentType(
+          rsrc.getRevision().getControl().getProjectControl().getProjectState(),
+          ObjectId.fromString(rsrc.getRevision().getPatchSet().getRevision().get()),
+          path);
+    }
+    return BinaryResult.create("")
+        .setContentType(contentType)
+        .base64();
+  }
+}

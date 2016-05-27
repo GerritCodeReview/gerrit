7541a52d075df010916e90f89d65ef0edd90571f
Wyatt Allen <wyatta@google.com>
2016-05-27 11:17:13 -0700

    Allows topic editing in the change view
    Adds an editable text label to the gr-change-metadata for modifying the
    topic of a change.  Introduces the gr-editable-label as a UI element for
    editable text labels.  Editing the topic is only allowed if the user is
    logged in and has the topic-edit permission.
    
    This is a redo of change 78110, which had been rebased wrong and had to
    be reverted. https://gerrit-review.googlesource.com/c/78110/1
    
    Bug: Issue 4102
    Change-Id: Ia7bb7b803a9fdd15a5f9f0f8eecf77bdd3ee4ba7

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -761,5 +761,10 @@
           return {baseImage: baseImage, revisionImage: revisionImage};
         }.bind(this));
     },
+
+    setChangeTopic: function(changeNum, topic) {
+      return this.send('PUT', '/changes/' + encodeURIComponent(changeNum) +
+          '/topic', {topic: topic});
+    },
   });
 })();

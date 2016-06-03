4306013bc7cfeac1af055ddaba6fe3e6c74fc0f4
Edwin Kempin <ekempin@google.com>
2016-06-03 10:05:47 +0200

    Revert "ChangeNotes: Better message when change not in NoteDb"
    This reverts commit b4b1664cf0c70b7c164fef496d7ad07a274f3b00.
    
    With the reverted commit NoSuchChangeException was not thrown by
    ChangeNotes for non-existing change IDs. This broke createChecked that
    relies on a ChangeNotes#getChange() to return null if a change does
    not exist, since with the reverted commit it was throwing an exception
    instead.
    
    Change-Id: I34a7cb76d15788320314aa7afc9f5bcc809b11c3
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
@@ -165,8 +165,6 @@ public class ChangeNotes extends AbstractChangeNotes<ChangeNotes> {
     public ChangeNotes create(ReviewDb db, Project.NameKey project,
         Change.Id changeId) throws OrmException {
       Change change = unwrap(db).changes().get(changeId);
-      checkNotNull(change,
-          "change %s not found in ReviewDb", changeId);
       checkArgument(change.getProject().equals(project),
           "passed project %s when creating ChangeNotes for %s, but actual"
           + " project is %s",
@@ -198,8 +196,6 @@ public class ChangeNotes extends AbstractChangeNotes<ChangeNotes> {
       checkState(!args.migration.readChanges(), "do not call"
           + " createFromIdOnlyWhenNoteDbDisabled when NoteDb is enabled");
       Change change = unwrap(db).changes().get(changeId);
-      checkNotNull(change,
-          "change %s not found in ReviewDb", changeId);
       return new ChangeNotes(args, change.getProject(), change).load();
     }
 

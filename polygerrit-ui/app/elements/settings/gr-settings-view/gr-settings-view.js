a2d7eae1b7aeaff8f27900986de3495e9dbd98e8
Wyatt Allen <wyatta@google.com>
2016-06-03 09:35:07 -0700

    Adds user settings/preferences page
    Adds a home for viewing and editing user preferences at a new client
    route: /settings/. This is first in a series of changes to bring
    preferences editing to PolyGerrit. At this point the settings page is
    read-only and mostly un-styled.
    
    Introduces the gr-settings-view element which is added to gr-app in the
    same manner as the other main PolyGerrit views. Introduces the /settings
    route which will display the new view, or redirect to login if the user
    is not authenticated. Adds a link to user settings in the
    gr-account-dropdown.
    
    Bug: Issue 3911
    Change-Id: Ib164502d586518876737c6d333c8940f2898dad4

---
+++ polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-view.js
@@ -0,0 +1,57 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-settings-view',
+
+    properties: {
+      account: {
+        type: Object,
+        value: function() { return {}; },
+      },
+      prefs: {
+        type: Object,
+        value: function() { return {}; },
+      },
+
+      _loading: {
+        type: Boolean,
+        value: true,
+      },
+    },
+
+    attached: function() {
+      var promises = [];
+
+      promises.push(this.$.restAPI.getAccount().then(function(account) {
+        this.account = account;
+      }.bind(this)));
+
+      promises.push(this.$.restAPI.getPreferences().then(function(prefs) {
+        this.prefs = prefs;
+      }.bind(this)));
+
+      Promise.all(promises).then(function() {
+        this._loading = false;
+      }.bind(this));
+    },
+
+    _computeRegistered: function(registered) {
+      if (!registered) { return ''; }
+      return util.parseDate(registered).toGMTString();
+    },
+  });
+})();

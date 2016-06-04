GerritCodeReview/gerrit

commit 5fc7df7a859bc1eeebfa217c24fe6f864a274a0b

submoduleOp: Move checking for existence to SubmoduleOp
Before loading the .gitmodules file of the super project, make sure the
project/branch exists.

Change-Id: Ic5d2710f77f96c393e2284fbf0b5d9deeb43f9a3
Signed-off-by: Stefan Beller <sbeller@google.com>

gerrit-server/src/main/java/com/google/gerrit/server/git/SubmoduleOp.java
@@ -149,6 +149,19 @@ public class SubmoduleOp {
       Collection<Branch.NameKey> branches =
           getDestinationBranches(branch, s, orm);
       for (Branch.NameKey targetBranch : branches) {
+        Project.NameKey p = targetBranch.getParentKey();
+        try {
+          orm.openRepo(p, false);
+          OpenRepo or = orm.getRepo(project);
+          ObjectId id = or.repo.resolve(branch.get());
+          if (id == null) {
+            logDebug("The branch " + branch + " doesn't exist.");
+            continue;
+          }
+        } catch (NoSuchProjectException e) {
+          logDebug("The project " + p + " doesn't exist");
+          continue;
+        }
         GitModules m = gitmodulesFactory.create(targetBranch, updateId, orm);
         for (SubmoduleSubscription ss : m.subscribedTo(branch)) {
           logDebug("Checking SubmoduleSubscription " + ss);

commit bdb41d85397c64b8b1f96e03c49f130f80f1365d

Fix "submoduleOp: Move checking for existence to SubmoduleOp"
Commit 5fc7df7a859bc1eeebfa217c24fe6f864a274a0b broke
24 tests. This fixes it by having the correct project/branch checked.

Change-Id: Ia6ffd80ee7b8ad6690b6abd861663f16fd659200

gerrit-server/src/main/java/com/google/gerrit/server/git/SubmoduleOp.java
@@ -152,10 +152,10 @@ public class SubmoduleOp {
         Project.NameKey p = targetBranch.getParentKey();
         try {
           orm.openRepo(p, false);
-          OpenRepo or = orm.getRepo(project);
-          ObjectId id = or.repo.resolve(branch.get());
+          OpenRepo or = orm.getRepo(p);
+          ObjectId id = or.repo.resolve(targetBranch.get());
           if (id == null) {
-            logDebug("The branch " + branch + " doesn't exist.");
+            logDebug("The branch " + targetBranch + " doesn't exist.");
             continue;
           }
         } catch (NoSuchProjectException e) {
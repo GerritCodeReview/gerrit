a500d270fcbb983b00f07fd2d8051ff7491ff306
Zhen Chen <czhen@google.com>
2016-06-07 14:17:58 -0700

    Refactor ChangeSet#changesByProject() to return ChangeData
    ChangeSet#changesByProject() used to return Change.Id then the method
    which calls this method retrieve Change.Data by the Change.Id. Also to
    be consistent with changesByBranch() method.
    
    Change-Id: I446c52626998bac9f4e33885e2864ccec2798c22

--- gerrit-server/src/main/java/com/google/gerrit/server/git/MergeSuperSet.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/MergeSuperSet.java
@@ -108,12 +108,11 @@ public class MergeSuperSet {
       IncorrectObjectTypeException, IOException, OrmException {
     List<ChangeData> ret = new ArrayList<>();
 
-    Multimap<Project.NameKey, Change.Id> pc = changes.changesByProject();
+    Multimap<Project.NameKey, ChangeData> pc = changes.changesByProject();
     for (Project.NameKey project : pc.keySet()) {
       try (Repository repo = repoManager.openRepository(project);
            RevWalk rw = CodeReviewCommit.newRevWalk(repo)) {
-        for (Change.Id cId : pc.get(project)) {
-          ChangeData cd = changeDataFactory.create(db, project, cId);
+        for (ChangeData cd : pc.get(project)) {
           cd.changeControl(user);
 
           SubmitTypeRecord str = cd.submitTypeRecord();

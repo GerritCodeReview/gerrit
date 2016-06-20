409d02879509e887ea7690fa9f6564437d7382b9
Wyatt Allen <wyatta@google.com>
2016-06-20 15:50:37 -0700

    Adds SSH Keys to the PolyGerrit settings screen
    Introduces the gr-ssh-editor element which is added to gr-settings-view.
    The new element lists the SSH public keys associated with the user's
    account, allows the user to view the public key, allows the user to
    remove keys from their account and allows the user to add new keys.
    
    Bug: Issue 3911
    Change-Id: I1b5b610836c15341482dcc917a43f8a76b77daeb

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -581,7 +581,7 @@
       return this.send(method, url);
     },
 
-    send: function(method, url, opt_body, opt_errFn, opt_ctx) {
+    send: function(method, url, opt_body, opt_errFn, opt_ctx, opt_conentType) {
       var headers = new Headers({
         'X-Gerrit-Auth': this._getCookie('XSRF_TOKEN'),
       });
@@ -591,7 +591,7 @@
         credentials: 'same-origin',
       };
       if (opt_body) {
-        headers.append('Content-Type', 'application/json');
+        headers.append('Content-Type', opt_conentType || 'application/json');
         if (typeof opt_body !== 'string') {
           opt_body = JSON.stringify(opt_body);
         }
@@ -848,6 +848,26 @@
     generateAccountHttpPassword: function() {
       return this.send('PUT', '/accounts/self/password.http', {generate: true})
           .then(this.getResponseObject);
+    },
+
+    getAccountSshKeys: function() {
+      return this._fetchSharedCacheURL('/accounts/self/sshkeys');
+    },
+
+    addAccountSshKey: function(key) {
+      return this.send('POST', '/accounts/self/sshkeys', key, null, null,
+          'plain/text')
+          .then(function(response) {
+            if (response.status >= 200 && response.status < 300) {
+              return this.getResponseObject(response);
+            } else {
+              return { valid: false };
+            }
+          }.bind(this));
+    },
+
+    deleteAccountSshKey: function(id) {
+      return this.send('DELETE', '/accounts/self/sshkeys/' + id);
     },
   });
 })();

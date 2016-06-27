e039318601ec72b5bb82fa1ebe75bc0e3f30371f
Edwin Kempin <ekempin@google.com>
2016-06-27 10:28:33 +0200

    Add predicate to find accounts by email
    Change-Id: I70e114e155b147351f0933fb1b4ca7cddfb8a766
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/account/EmailPredicate.java
@@ -0,0 +1,41 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.account;
+
+import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.index.IndexPredicate;
+import com.google.gerrit.server.index.account.AccountField;
+import com.google.gwtorm.server.OrmException;
+
+public class EmailPredicate extends IndexPredicate<AccountState> {
+  EmailPredicate(String email) {
+    super(AccountField.EMAIL, AccountQueryBuilder.FIELD_EMAIL, email);
+  }
+
+  @Override
+  public boolean match(AccountState accountState) throws OrmException {
+    for (String email : accountState.getEmails()) {
+      if (email.equals(getValue()) || email.startsWith(getValue())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public int getCost() {
+    return 1;
+  }
+}

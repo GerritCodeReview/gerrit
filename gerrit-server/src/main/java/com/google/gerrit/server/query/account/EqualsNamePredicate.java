34e3bf8ec1d942852468383631b63065f06f0e4d
Edwin Kempin <ekempin@google.com>
2016-06-27 12:55:11 +0200

    Add predicate to find accounts by name
    Change-Id: I37a9c0facfef018ceaf5ad821d97f8fef7d18d38
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/account/EqualsNamePredicate.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.account;
+
+import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.index.IndexPredicate;
+import com.google.gerrit.server.index.account.AccountField;
+
+public class EqualsNamePredicate extends IndexPredicate<AccountState> {
+  EqualsNamePredicate(String value) {
+    super(AccountField.NAME_PART, AccountQueryBuilder.FIELD_NAME, value);
+  }
+
+  @Override
+  public boolean match(AccountState accountState) {
+    return accountState.getNameParts().contains(getValue().toLowerCase());
+  }
+
+  @Override
+  public int getCost() {
+    return 1;
+  }
+}

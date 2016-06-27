16810999f64c6a13367c7215858d3dee68b0953b
Wyatt Allen <wyatta@google.com>
2016-06-27 09:36:36 -0700

    Prevent redefinition of class libraries
    A number of classes used in PolyGerrit are defined in vanilla JS files
    that are included multiple times by various elements.  For example,
    gr-diff-line.js is included by the gr-diff-builder and by gr-diff
    elements. Adds #ifndef-style guards to each of these libraries to
    prevent redefinition and avoid different elements potentially referring
    to different versions of the same class.
    
    Change-Id: I45e3ba425a59989b328475b1fe58fd9f350c8ae0

--- polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-unified.js
+++ polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-unified.js
@@ -14,6 +14,9 @@
 (function(window, GrDiffBuilder) {
   'use strict';
 
+  // Prevent redefinition.
+  if (window.GrDiffBuilderUnified) { return; }
+
   function GrDiffBuilderUnified(diff, comments, prefs, outputEl) {
     GrDiffBuilder.call(this, diff, comments, prefs, outputEl);
   }

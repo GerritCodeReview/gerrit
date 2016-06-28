e5526886cc3be872bc34f8d124e0fa922fb619f2
Dave Borowitz <dborowitz@google.com>
2016-06-28 10:20:12 -0400

    Remove ChangeNotes#createAsync
    I1ff49315 was a mistake: it sacrifices correctness to reduce
    contention on rebuilding. In this case we can't avoid the contention,
    at least without a lot of extra work to implement multi-change
    rebuilding. The actual proximate reason for that change was that
    rebuilding changes would fail due to not being able to open the
    ReviewDb in the background thread, which is not in request scope.
    
    createAsync was added before we had a cache of parsed ChangeNotes;
    with the cache, performance of iterating over notes shouldn't be
    *that* bad. In the ReviewDb case, lookup of changes in ReviewDb is not
    really a win compared to the batch Access#get(Iterable) method.
    
    All of this adds up to just deleting the createAsync method and using
    the same batch #create(ReviewDb, Collection) method used by
    ChangeJson. This is correct and requires minimal implementation
    complication. There is still the potential for contention if multiple
    changes in the result set are out of date, but in that case
    serializing the rebuilds is actually probably better than doing them
    in parallel.
    
    Change-Id: I28dab2fda8c0743c7ccb92aa06e0f9d6fbbefa60

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotes.java
@@ -34,11 +34,6 @@ import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 import com.google.common.collect.Ordering;
-import com.google.common.util.concurrent.AsyncFunction;
-import com.google.common.util.concurrent.CheckedFuture;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.gerrit.common.Nullable;
 import com.google.gerrit.common.data.SubmitRecord;
 import com.google.gerrit.metrics.Timer1;
@@ -80,7 +75,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Callable;
 
 /** View of a single {@link Change} based on the log of its notes branch. */
 public class ChangeNotes extends AbstractChangeNotes<ChangeNotes> {
@@ -215,41 +209,6 @@ public class ChangeNotes extends AbstractChangeNotes<ChangeNotes> {
       checkState(!args.migration.readChanges(), "do not call"
           + " createFromChangeWhenNoteDbDisabled when NoteDb is enabled");
       return new ChangeNotes(args, change).load();
-    }
-
-    public CheckedFuture<ChangeNotes, OrmException> createAsync(
-        final ListeningExecutorService executorService, final ReviewDb db,
-        final Project.NameKey project, final Change.Id changeId) {
-      return Futures.makeChecked(
-          Futures.transformAsync(unwrap(db).changes().getAsync(changeId),
-              new AsyncFunction<Change, ChangeNotes>() {
-                @Override
-                public ListenableFuture<ChangeNotes> apply(
-                    final Change change) {
-                  return executorService.submit(new Callable<ChangeNotes>() {
-                    @Override
-                    public ChangeNotes call() throws Exception {
-                      checkArgument(change.getProject().equals(project),
-                          "passed project %s when creating ChangeNotes for %s,"
-                              + " but actual project is %s",
-                          project, changeId, change.getProject());
-                      // Disable auto-rebuilding. This may be called async for a
-                      // large number of changes in one project, increasing
-                      // write contention. Plus, we haven't propagated the
-                      // request scope for the rebuilder to open the db.
-                      return new ChangeNotes(args, change, false, null).load();
-                    }
-                  });
-                }
-              }), new Function<Exception, OrmException>() {
-                @Override
-                public OrmException apply(Exception e) {
-                  if (e instanceof OrmException) {
-                    return (OrmException) e;
-                  }
-                  return new OrmException(e);
-                }
-              });
     }
 
     public List<ChangeNotes> create(ReviewDb db,

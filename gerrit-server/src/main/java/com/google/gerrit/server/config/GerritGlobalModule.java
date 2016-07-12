fb27779b3f191f92b9bc5b5785accd5591bfb8ae
David Pursehouse <dpursehouse@collab.net>
2016-07-12 15:27:28 +0900

    Fire a specific event when a vote is deleted from a change
    Currently, removing a vote causes a "comment added" event to be
    fired.
    
    Replace it with a specific event. The event contains the approvals
    before and after the removal, the removed vote(s), and the notify
    setting.
    
    Note that while the current implementation of DeleteVote only allows
    to delete a single vote, the event allows for multiple removed votes
    in case this is implemented in DeleteVote in future.
    
    Change-Id: I8d62dc61c13d231a47d109f544c472d974031f0c

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -50,6 +50,7 @@ import com.google.gerrit.extensions.events.ReviewerDeletedListener;
 import com.google.gerrit.extensions.events.RevisionCreatedListener;
 import com.google.gerrit.extensions.events.TopicEditedListener;
 import com.google.gerrit.extensions.events.UsageDataPublishedListener;
+import com.google.gerrit.extensions.events.VoteDeletedListener;
 import com.google.gerrit.extensions.registration.DynamicItem;
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.registration.DynamicSet;
@@ -306,6 +307,7 @@ public class GerritGlobalModule extends FactoryModule {
     DynamicSet.setOf(binder(), ChangeRevertedListener.class);
     DynamicSet.setOf(binder(), ReviewerAddedListener.class);
     DynamicSet.setOf(binder(), ReviewerDeletedListener.class);
+    DynamicSet.setOf(binder(), VoteDeletedListener.class);
     DynamicSet.setOf(binder(), RevisionCreatedListener.class);
     DynamicSet.setOf(binder(), TopicEditedListener.class);
     DynamicSet.setOf(binder(), AgreementSignupListener.class);

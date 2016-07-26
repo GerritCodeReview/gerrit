cebc6938debdd12f3a5709e899791ee1b5e54f55
Zhen Chen <czhen@google.com>
2016-07-26 16:54:59 -0700

    Add more information to MergeableInfo to indicate a merged commit
    Add two fields contentMerged and commitMerged into MergeableInfo.
    contentMerged is true if the content of the commit is merged,
    commitMerged is true if the commit itself is merged.
    
    Also add more tests for CheckMergeability class.
    
    Change-Id: Ia756644411f217b8d8bae06f44f14a24597544bd

--- gerrit-server/src/main/java/com/google/gerrit/server/project/CheckMergeability.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/CheckMergeability.java
@@ -14,6 +14,7 @@
 
 package com.google.gerrit.server.project;
 
+import com.google.gerrit.extensions.client.SubmitType;
 import com.google.gerrit.extensions.common.MergeableInfo;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
@@ -45,6 +46,7 @@ public class CheckMergeability implements RestReadView<BranchResource> {
 
   private String source;
   private String strategy;
+  private SubmitType submitType;
   private final Provider<ReviewDb> db;
 
   @Option(name = "--source", metaVar = "COMMIT",
@@ -71,13 +73,22 @@ public class CheckMergeability implements RestReadView<BranchResource> {
       Provider<ReviewDb> db) {
     this.gitManager = gitManager;
     this.strategy = MergeUtil.getMergeStrategy(cfg).getName();
+    this.submitType = cfg.getEnum("project", null, "submitType",
+        SubmitType.MERGE_IF_NECESSARY);
     this.db = db;
   }
 
   @Override
   public MergeableInfo apply(BranchResource resource)
       throws IOException, BadRequestException, ResourceNotFoundException {
+    if (!(submitType.equals(SubmitType.MERGE_ALWAYS) ||
+          submitType.equals(SubmitType.MERGE_IF_NECESSARY))) {
+      throw new BadRequestException(
+          "Submit type: " + submitType + " is not supported");
+    }
+
     MergeableInfo result = new MergeableInfo();
+    result.submitType = submitType;
     result.strategy = strategy;
     try (Repository git = gitManager.openRepository(resource.getNameKey());
          RevWalk rw = new RevWalk(git);
@@ -97,10 +108,25 @@ public class CheckMergeability implements RestReadView<BranchResource> {
             "do not have read permission for: " + source);
       }
 
-      result.mergeable = m.merge(targetCommit, sourceCommit);
-      if (m instanceof ResolveMerger) {
-        result.conflicts = ((ResolveMerger) m).getUnmergedPaths();
+      if (rw.isMergedInto(sourceCommit, targetCommit)) {
+        result.mergeable = true;
+        result.commitMerged = true;
+        result.contentMerged = true;
+        return result;
       }
+
+      if (m.merge(false, targetCommit, sourceCommit)) {
+        result.mergeable = true;
+        result.commitMerged = false;
+        result.contentMerged = m.getResultTreeId().equals(targetCommit.getTree());
+      } else {
+        result.mergeable = false;
+        if (m instanceof ResolveMerger) {
+          result.conflicts = ((ResolveMerger) m).getUnmergedPaths();
+        }
+      }
+    } catch (IllegalArgumentException e) {
+      throw new BadRequestException(e.getMessage());
     }
     return result;
   }

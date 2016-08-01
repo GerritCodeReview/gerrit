0d256e0140ae5bba45a5151fd15b10356f9d14b1
Logan Hanks <logan@google.com>
2016-08-01 13:23:38 -0700

    Separate CCs from reviewers when NoteDb is enabled
    Change-Id: Id5111220906ad728707087f6fb2ddb085c34a9b8

--- polygerrit-ui/app/elements/change/gr-account-entry/gr-account-entry.js
+++ polygerrit-ui/app/elements/change/gr-account-entry/gr-account-entry.js
@@ -35,9 +35,7 @@
 
       filter: {
         type: Function,
-        value: function() {
-          return this.notOwnerOrReviewer.bind(this);
-        },
+        value: function() { return function() { return true; }; },
       },
 
       query: {
@@ -46,16 +44,7 @@
           return this._getReviewerSuggestions.bind(this);
         },
       },
-
-      _reviewers: {
-        type: Array,
-        value: function() { return []; },
-      },
     },
-
-    observers: [
-      '_reviewersChanged(change.reviewers.*, change.owner)',
-    ],
 
     get focusStart() {
       return this.$.input.focusStart;
@@ -71,30 +60,6 @@
 
     _handleInputCommit: function(e) {
       this.fire('add', {value: e.detail.value});
-    },
-
-    _reviewersChanged: function(changeRecord, owner) {
-      var reviewerSet = {};
-      reviewerSet[owner._account_id] = true;
-      var addReviewers = function(reviewers) {
-        if (!reviewers) {
-          return;
-        }
-        reviewers.forEach(function(reviewer) {
-          reviewerSet[reviewer._account_id] = true;
-        });
-      };
-
-      var reviewers = changeRecord.base;
-      addReviewers(reviewers.CC);
-      addReviewers(reviewers.REVIEWER);
-      this._reviewers = reviewerSet;
-    },
-
-    notOwnerOrReviewer: function(reviewer) {
-      var account = reviewer.account;
-      if (!account) { return true; }
-      return !this._reviewers[reviewer.account._account_id];
     },
 
     _makeSuggestion: function(reviewer) {

214fe5f74d365ac8ebb9f80d170b5376990b614f
Stephen Li <stephenli@google.com>
2016-08-08 18:38:04 -0700

    Add data-branch and data-project to attributes for related changes.
    This is so that javascript plugins can more easily identify and
    modify the DOM elements associated with RelatedChanges.
    
    Change-Id: Ic4da416cbc359f29b202a966f85cd933e9fda7b3

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/change/RelatedChangesTab.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/change/RelatedChangesTab.java
@@ -284,6 +284,8 @@ class RelatedChangesTab implements IsWidget {
       } else {
         sb.openSpan().setStyleName(RelatedChanges.R.css().subject());
       }
+      sb.setAttribute("data-branch", info.branch());
+      sb.setAttribute("data-project", info.project());
       String url = url();
       if (url != null) {
         sb.openAnchor().setAttribute("href", url);

dfa6714e51ab83c2535b2c64ae48dbeeb0d17924
Yuxuan 'fishy' Wang <fishywang@google.com>
2016-08-10 20:15:46 -0700

    EventUtil needs ALL_REVISIONS and ALL_COMMITS
    Without them the event fired lacks a lot of useful information.
    
    I think we might actually want all of the ListChangesOption, but there's
    no evidence yet to support that, so let's use ALL_REVISIONS and
    ALL_COMMITS first.
    
    Change-Id: I04ddc340d72587b83bd260ae856b6a1e796aa2fb

--- gerrit-server/src/main/java/com/google/gerrit/server/extensions/events/EventUtil.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/extensions/events/EventUtil.java
@@ -55,7 +55,7 @@ public class EventUtil {
     this.changeDataFactory = changeDataFactory;
     this.db = db;
     this.changeJson = changeJsonFactory.create(
-        EnumSet.of(ListChangesOption.CURRENT_COMMIT));
+        EnumSet.of(ListChangesOption.ALL_REVISIONS, ListChangesOption.ALL_COMMITS));
     this.accountCache = accountCache;
   }
 

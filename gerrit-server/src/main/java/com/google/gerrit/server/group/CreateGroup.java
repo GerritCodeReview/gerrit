2fee81cdde355425d5e0e7fae586a58b58694190
Yuxuan 'fishy' Wang <fishywang@google.com>
2016-08-12 15:27:00 -0700

    Allow change GroupJson options in CreateGroup
    So the caller could to control what to be included in the return value.
    
    Change-Id: I6974d505ff4a2191b2f20efb96f7ba823ef5604b

--- gerrit-server/src/main/java/com/google/gerrit/server/group/CreateGroup.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/group/CreateGroup.java
@@ -21,6 +21,7 @@ import com.google.gerrit.common.data.GroupDescription;
 import com.google.gerrit.common.data.GroupDescriptions;
 import com.google.gerrit.extensions.annotations.RequiresCapability;
 import com.google.gerrit.extensions.api.groups.GroupInput;
+import com.google.gerrit.extensions.client.ListGroupsOption;
 import com.google.gerrit.extensions.common.GroupInfo;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.restapi.AuthException;
@@ -53,6 +54,7 @@ import org.eclipse.jgit.lib.PersonIdent;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -96,6 +98,16 @@ public class CreateGroup implements RestModifyView<TopLevelResource, GroupInput>
     this.addMembers = addMembers;
     this.defaultVisibleToAll = cfg.getBoolean("groups", "newGroupsVisibleToAll", false);
     this.name = name;
+  }
+
+  public CreateGroup addOption(ListGroupsOption o) {
+    json.addOption(o);
+    return this;
+  }
+
+  public CreateGroup addOptions(Collection<ListGroupsOption> o) {
+    json.addOptions(o);
+    return this;
   }
 
   @Override

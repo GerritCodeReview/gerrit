d373a096734709c453f14aa1d1280b64539dd8e8
Dave Borowitz <dborowitz@google.com>
2016-08-12 17:24:44 -0400

    ChangeJson: Ignore PUSH_CERTIFICATES if GPG API is disabled
    GpgApiAdapter throws an exception if the GPG API is disabled. The
    client respects whether the server advertises signed push is enabled
    when deciding whether to send the PUSH_CERTIFICATES option, but we
    can't assume all clients will be so well behaved.
    
    Recently in I04ddc340 we started passing this option when generating
    events, causing breakage when processing a patch set with a push
    certificate but the GPG API is disabled.
    
    Change-Id: I53981d521f67906def7e8d3018f87816d5e43ab9

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/api/change/ChangeIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/api/change/ChangeIT.java
@@ -34,6 +34,8 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.AcceptanceTestRequestScope;
+import com.google.gerrit.acceptance.GerritConfig;
+import com.google.gerrit.acceptance.GerritConfigs;
 import com.google.gerrit.acceptance.GitUtil;
 import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.PushOneCommit;
@@ -1342,6 +1344,10 @@ public class ChangeIT extends AbstractDaemonTest {
   }
 
   @Test
+  @GerritConfigs({
+    @GerritConfig(name = "gerrit.editGpgKeys", value = "true"),
+    @GerritConfig(name = "receive.enableSignedPush", value = "true"),
+  })
   public void pushCertificates() throws Exception {
     PushOneCommit.Result r1 = createChange();
     PushOneCommit.Result r2 = amendChange(r1.getChangeId());

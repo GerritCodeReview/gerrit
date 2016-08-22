d33b638f9099426ed9c6083bfef768d40b690e21
David Pursehouse <dpursehouse@collab.net>
2016-08-22 21:08:53 +0900

    Move AuthType from gerrit-reviewdb to extension API
    Change-Id: I94d93e7776accd08d9d307781952b899ba13cbad

--- gerrit-server/src/main/java/com/google/gerrit/server/account/DefaultRealm.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/DefaultRealm.java
@@ -16,8 +16,8 @@ package com.google.gerrit.server.account;
 
 import com.google.common.base.Strings;
 import com.google.gerrit.extensions.client.AccountFieldName;
+import com.google.gerrit.extensions.client.AuthType;
 import com.google.gerrit.reviewdb.client.Account;
-import com.google.gerrit.reviewdb.client.AuthType;
 import com.google.gerrit.server.config.AuthConfig;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;

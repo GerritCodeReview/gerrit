70da6b8957bde17c992352d4a7acb658b6b6aa23
Andrew Bonventre <andybons@chromium.org>
2016-08-23 16:01:21 -0400

    Always show actions for the latest patch set
    + Since the patch set dropdown has moved to the file list and
      actions aren’t shown for older patch sets in the GWT UI, show
      actions for the latest patch set. This goes in line with
      previous changes to have the most recent patch set info be in
      the overview.
    
    Change-Id: I389260aed0abaead70115ee3cb085794d714af6e

--- polygerrit-ui/app/elements/change/gr-confirm-revert-dialog/gr-confirm-revert-dialog.js
+++ polygerrit-ui/app/elements/change/gr-confirm-revert-dialog/gr-confirm-revert-dialog.js
@@ -30,14 +30,12 @@
      */
 
     properties: {
-      branch: String,
       message: String,
-      commitInfo: Object,
     },
 
-    populateRevertMessage: function() {
+    populateRevertMessage: function(message) {
       // Figure out what the revert title should be.
-      var originalTitle = this.commitInfo.message.split('\n')[0];
+      var originalTitle = message.split('\n')[0];
       var revertTitle = 'Revert of ' + originalTitle;
       if (originalTitle.startsWith('Revert of ')) {
         revertTitle = 'Reland of ' +
@@ -47,7 +45,7 @@
                       originalTitle.substring('Reland of '.length);
       }
       // Add '> ' in front of the original commit text.
-      var originalCommitText = this.commitInfo.message.replace(/^/gm, '> ');
+      var originalCommitText = message.replace(/^/gm, '> ');
 
       this.message = revertTitle + '\n\n' +
                      'Reason for revert: <INSERT REASONING HERE>\n\n' +

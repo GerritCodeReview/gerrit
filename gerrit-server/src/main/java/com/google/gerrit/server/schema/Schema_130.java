0b16a51f133a7b10fd27f8b44b709a39f73351ab
Edwin Kempin <ekempin@google.com>
2016-09-05 13:26:20 +0200

    Remove force option from 'Push Annotated Tag' permission
    The force option on 'Push Annotated Tag' is not needed since force
    pushing of annotated tags can be controlled by assigning force 'Push'
    on 'refs/tags/*'.
    
    At the moment the force option on 'Push Annotated Tag' has no effect.
    
    Since the force option on 'Push Annotated Tag' is neither needed nor
    used we should simply remove it.
    
    Also having a force option on 'Push Annotated Tag' is inconsistent
    with not having a force option on 'Push Signed Tag'.
    
    This change also adds a schema migration that removes the force option
    from any rule of existing 'Push Annotated Tag' permissions.
    
    Change-Id: I4a3611c4409e7d38a8b5dfcf4f452cf8fa477670
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_130.java
@@ -0,0 +1,92 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.schema;
+
+import com.google.gerrit.common.data.AccessSection;
+import com.google.gerrit.common.data.Permission;
+import com.google.gerrit.common.data.PermissionRule;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.GerritPersonIdent;
+import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.MetaDataUpdate;
+import com.google.gerrit.server.git.ProjectConfig;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+
+import java.io.IOException;
+
+public class Schema_130 extends SchemaVersion {
+  private static final String COMMIT_MSG =
+      "Remove force option from 'Push Annotated Tag' permission\n"
+      + "\n"
+      + "The force option on 'Push Annotated Tag' had no effect and is no longer\n"
+      + "supported.";
+
+  private final GitRepositoryManager repoManager;
+  private final PersonIdent serverUser;
+
+  @Inject
+  Schema_130(Provider<Schema_129> prior,
+      GitRepositoryManager repoManager,
+      @GerritPersonIdent PersonIdent serverUser) {
+    super(prior);
+    this.repoManager = repoManager;
+    this.serverUser = serverUser;
+  }
+
+  @Override
+  protected void migrateData(ReviewDb db, UpdateUI ui) throws OrmException {
+    for (Project.NameKey projectName : repoManager.list()) {
+      try (Repository git = repoManager.openRepository(projectName);
+          MetaDataUpdate md = new MetaDataUpdate(GitReferenceUpdated.DISABLED,
+              projectName, git)) {
+        ProjectConfig config = ProjectConfig.read(md);
+
+        boolean update = false;
+        for (AccessSection accessSection : config.getAccessSections()) {
+          Permission pushTagPermission =
+              accessSection.getPermission(Permission.PUSH_TAG);
+          if (pushTagPermission == null) {
+            continue;
+          }
+          for (PermissionRule rule : pushTagPermission.getRules()) {
+            if (rule.getForce()) {
+              rule.setForce(false);
+              update = true;
+            }
+          }
+        }
+
+        if (!update) {
+          continue;
+        }
+        md.getCommitBuilder().setAuthor(serverUser);
+        md.getCommitBuilder().setCommitter(serverUser);
+        md.setMessage(COMMIT_MSG);
+        config.commit(md);
+      } catch (ConfigInvalidException | IOException ex) {
+        throw new OrmException(ex);
+      }
+    }
+  }
+}

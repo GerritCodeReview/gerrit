af3f9345eeecbaed5675dadc8584d25a7451e9d7
Becky Siegel <beckysiegel@google.com>
2016-09-09 15:46:08 -0700

    Submit diff preferences with enter key
    Previously, the diff preferences form did not listen to the enter key
    to submit an update. This change adds support for the enter key to
    save changes.
    
    Bug: Issue 4140
    Change-Id: If15812bb4404ca4061597755eeaf68d4cae23b3f

--- polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.js
+++ polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.js
@@ -255,8 +255,7 @@
           break;
         case 188:  // ','
           e.preventDefault();
-          this.$.prefsOverlay.open();
-          break;
+          this._openPrefs();
       }
     },
 
@@ -266,6 +265,15 @@
 
       page.show(this._computeNavLinkURL(path, fileList, direction));
     },
+
+    _openPrefs: function() {
+      this.$.prefsOverlay.open().then(function() {
+        var diffPreferences = this.$.diffPreferences;
+        var focusStops = diffPreferences.getFocusStops();
+        this.$.prefsOverlay.setFocusStops(focusStops);
+        this.$.diffPreferences.resetFocus();
+      }.bind(this));
+  },
 
     /**
      * @param {?string} path The path of the current file being shown.
@@ -451,7 +459,7 @@
 
     _handlePrefsTap: function(e) {
       e.preventDefault();
-      this.$.prefsOverlay.open();
+      this._openPrefs();
     },
 
     _handlePrefsSave: function(e) {

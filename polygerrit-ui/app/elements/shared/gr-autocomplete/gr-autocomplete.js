73e70cab7430fceccce92e87979ad56aeb017136
Logan Hanks <logan@google.com>
2016-09-14 10:16:12 -0700

    Close autocomplete suggestions on tap
    When autocompletion "commits" a value, it should always clear the list
    of suggestions and hide it from view. This change moves that logic from
    the individual event handlers into _commit, so this now occurs on tap.
    Also refocuses on the input after a suggestion is tapped so the user can
    continue typing.
    
    Bug: Issue 4473
    Change-Id: If0343b38bcc61a49fcca2fb5eabea09a7b36fdd6

--- polygerrit-ui/app/elements/shared/gr-autocomplete/gr-autocomplete.js
+++ polygerrit-ui/app/elements/shared/gr-autocomplete/gr-autocomplete.js
@@ -200,13 +200,11 @@
           if (this._suggestions.length > 0) {
             e.preventDefault();
             this._commit(this.tabCompleteWithoutCommit);
-            this._suggestions = [];
           }
           break;
         case 13: // Enter
           e.preventDefault();
           this._commit();
-          this._suggestions = [];
           break;
       }
     },
@@ -245,6 +243,7 @@
     _handleSuggestionTap: function(e) {
       this.$.cursor.setCursor(e.target);
       this._commit();
+      this.focus();
     },
 
     /**
@@ -275,6 +274,7 @@
         }
       }
 
+      this._suggestions = [];
       if (!silent) {
         this.fire('commit', {value: value});
       }

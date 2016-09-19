a20d0ba1b1591ac568d1ed4fd11d0a61e09bf399
Kasper Nilsson <kaspern@google.com>
2016-09-19 14:11:30 -0700

    Add 'Show 25 more' button to gr-file-list
    gr-file-list has an issue handling very large (500+) amounts of files
    due to the dynamic rendering of many DOM elements. This change caps the
    number of files shown initially at 75, and allows the user to view more
    in chunks of 25.
    
    Bug: Issue 4575
    Change-Id: I84c7fc352da80609d424217250830f19a599c940

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
@@ -53,6 +53,10 @@
       _userPrefs: Object,
       _localPrefs: Object,
       _showInlineDiffs: Boolean,
+      _numFilesShown: {
+        type: Number,
+        value: 75,
+      },
     },
 
     behaviors: [
@@ -421,6 +425,10 @@
       return !expanded;
     },
 
+    _computeFilesShown: function(numFilesShown, files) {
+      return files.slice(0, numFilesShown);
+    },
+
     _filesChanged: function() {
       this.async(function() {
         var diffElements = Polymer.dom(this.root).querySelectorAll('gr-diff');
@@ -429,6 +437,18 @@
         this.$.cursor.splice.apply(this.$.cursor,
             ['diffs', 0, this.$.cursor.diffs.length].concat(diffElements));
       }.bind(this), 1);
+    },
+
+    _incrementNumFilesShown: function() {
+      this._numFilesShown += 25;
+    },
+
+    _computeIncrementHidden: function(numFilesShown, files) {
+      return numFilesShown >= files.length;
+    },
+
+    _computeIncrementText: function(numFilesShown, files) {
+      return 'Show ' + Math.min(25, files.length - numFilesShown) + ' more';
     },
   });
 })();

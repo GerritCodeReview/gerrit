37c95f31260aa0aa9ad81250b7f86ef72a2083bd
Gustaf Lundh <gustaflh@axis.com>
2016-09-20 10:47:50 +0200

    User pref: "Highlight Changes Assigned To Me In Change Table"
    Defaults to true.
    
    Change-Id: I724fc70b67cb4de20d31777e2ec878dd515b953f

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeTable.java
@@ -253,8 +253,9 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
       if (c.assignee() != null) {
         table.setWidget(row, C_ASSIGNEE,
             AccountLinkPanel.forAssignee(c.assignee()));
-        if (Objects.equals(c.assignee().getId(),
-            Gerrit.getUserAccount().getId())) {
+        if (Gerrit.getUserPreferences().highlightAssigneeInChangeTable()
+            && Objects.equals(c.assignee().getId(),
+                Gerrit.getUserAccount().getId())) {
           table.getRowFormatter().addStyleName(row,
               Gerrit.RESOURCES.css().cASSIGNEDTOME());
         }
@@ -264,7 +265,8 @@ public class ChangeTable extends NavigationTable<ChangeInfo> {
     }
 
     table.setWidget(row, C_PROJECT, new ProjectLink(c.projectNameKey()));
-    table.setWidget(row, C_BRANCH, new BranchLink(c.projectNameKey(), c
+    table.setWidget(row, C_BRANCH,
+        new BranchLink(c.projectNameKey(), c
         .status(), c.branch(), c.topic()));
     if (Gerrit.getUserPreferences().relativeDateInChangeTable()) {
       table.setText(row, C_LAST_UPDATE, relativeFormat(c.updated()));

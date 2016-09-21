bed1f41ac90c28d985b11d29ef67d9523235167e
Sven Selberg <sven.selberg@axis.com>
2016-09-21 14:37:45 +0200

    ChangeNotes: add past assignees
    To enable finding all users that have been assigned to a change.
    
    Change-Id: Idc2929b198ecb5800a4d32e06d096f586b6460ad

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotesParser.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotesParser.java
@@ -138,6 +138,7 @@ class ChangeNotesParser {
   private Change.Status status;
   private String topic;
   private Optional<Account.Id> assignee;
+  private List<Account.Id> pastAssignees;
   private Set<String> hashtags;
   private Timestamp createdOn;
   private Timestamp lastUpdatedOn;
@@ -186,6 +187,7 @@ class ChangeNotesParser {
       parseNotes();
       allPastReviewers.addAll(reviewers.rowKeySet());
       pruneReviewers();
+
       updatePatchSetStates();
       checkMandatoryFooters();
     }
@@ -213,6 +215,7 @@ class ChangeNotesParser {
         status,
 
         assignee != null ? assignee.orNull() : null,
+        Sets.newLinkedHashSet(Lists.reverse(pastAssignees)),
         hashtags,
         patchSets,
         buildApprovals(),
@@ -480,18 +483,25 @@ class ChangeNotesParser {
 
   private void parseAssignee(ChangeNotesCommit commit)
       throws ConfigInvalidException {
-    if (assignee != null) {
-      return;
+    if (pastAssignees == null) {
+      pastAssignees = Lists.newArrayList();
     }
     String assigneeValue = parseOneFooter(commit, FOOTER_ASSIGNEE);
-    if (assigneeValue == null){
-      //footer not found
-    } else if (assigneeValue.equals("")) {
-      // empty footer found, assignee deleted
-      assignee = Optional.absent();
-    } else {
-      PersonIdent ident = RawParseUtils.parsePersonIdent(assigneeValue);
-      assignee = Optional.fromNullable(noteUtil.parseIdent(ident, id));
+    if (assigneeValue != null) {
+      Optional<Account.Id> parsedAssignee;
+      if (assigneeValue.equals("")) {
+        // Empty footer found, assignee deleted
+        parsedAssignee = Optional.absent();
+      } else {
+        PersonIdent ident = RawParseUtils.parsePersonIdent(assigneeValue);
+        parsedAssignee = Optional.fromNullable(noteUtil.parseIdent(ident, id));
+      }
+      if (assignee == null) {
+        assignee = parsedAssignee;
+      }
+      if (parsedAssignee.isPresent()) {
+        pastAssignees.add(parsedAssignee.get());
+      }
     }
   }
 

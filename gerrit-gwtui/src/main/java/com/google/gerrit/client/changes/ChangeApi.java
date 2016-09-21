787c238a0ddd760856d2a3b780bd3a30c0663c41
Gustaf Lundh <gustaflh@axis.com>
2016-09-21 23:14:14 +0200

    Set assignee for ChangeApi
    Allows easy access to RESTApi from GWT-code.
    
    Change-Id: Ibade3447a65ae337507e0bb0907fb32303a35a3d

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeApi.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/changes/ChangeApi.java
@@ -15,6 +15,7 @@
 package com.google.gerrit.client.changes;
 
 import com.google.gerrit.client.Gerrit;
+import com.google.gerrit.client.info.AccountInfo;
 import com.google.gerrit.client.info.ChangeInfo;
 import com.google.gerrit.client.info.ChangeInfo.CommitInfo;
 import com.google.gerrit.client.info.ChangeInfo.EditInfo;
@@ -112,6 +113,12 @@ public class ChangeApi {
     return call(id, revision, "actions");
   }
 
+  public static void assignee(int id, String selectedUser, AsyncCallback<AccountInfo> cb) {
+    AssigneeInput input = AssigneeInput.create();
+    input.setAssignee(selectedUser);
+    change(id).view("assignee").put(selectedUser, cb);
+  }
+
   public static RestApi comments(int id) {
     return call(id, "comments");
   }
@@ -157,10 +164,11 @@ public class ChangeApi {
     return change(id).view("reviewers");
   }
 
-  public static RestApi suggestReviewers(int id, String q, int n) {
+  public static RestApi suggestReviewers(int id, String q, int n, boolean e) {
     return change(id).view("suggest_reviewers")
         .addParameter("q", q)
-        .addParameter("n", n);
+        .addParameter("n", n)
+        .addParameter("e", e);
   }
 
   public static RestApi vote(int id, int reviewer, String vote) {
@@ -242,6 +250,17 @@ public class ChangeApi {
     RebaseInput rebaseInput = RebaseInput.create();
     rebaseInput.setBase(base);
     call(id, commit, "rebase").post(rebaseInput, cb);
+  }
+
+  private static class AssigneeInput extends JavaScriptObject {
+    final native void setAssignee(String a) /*-{ if(a)this.assignee=a; }-*/;
+
+    static AssigneeInput create() {
+      return (AssigneeInput) createObject();
+    }
+
+    protected AssigneeInput() {
+    }
   }
 
   private static class Input extends JavaScriptObject {

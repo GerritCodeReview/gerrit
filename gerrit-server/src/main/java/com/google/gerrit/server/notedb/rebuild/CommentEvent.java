e24f8b44f964ffcaeda4d3f94f2e8ba9e6a08612
Edwin Kempin <ekempin@google.com>
2016-09-22 11:16:17 +0200

    ChangeRebuilderImpl: Have separate event for draft comments
    We are about to replace PatchLineComment with a new Comment class that
    doesn't know about the comment status. As result published and draft
    comments will be handled separately and it's easier if we have
    separate events for them.
    
    Change-Id: I513520c94ec253aa14c6218231e6226fbfeb178a
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/rebuild/CommentEvent.java
@@ -0,0 +1,56 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.notedb.rebuild;
+
+import static com.google.gerrit.server.PatchLineCommentsUtil.setCommentRevId;
+
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchLineComment;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.server.PatchLineCommentsUtil;
+import com.google.gerrit.server.notedb.ChangeUpdate;
+import com.google.gerrit.server.patch.PatchListCache;
+import com.google.gwtorm.server.OrmException;
+
+class CommentEvent extends Event {
+  public final PatchLineComment c;
+  private final Change change;
+  private final PatchSet ps;
+  private final PatchListCache cache;
+
+  CommentEvent(PatchLineComment c, Change change, PatchSet ps,
+      PatchListCache cache) {
+    super(PatchLineCommentsUtil.getCommentPsId(c), c.getAuthor(),
+        c.getWrittenOn(), change.getCreatedOn(), c.getTag());
+    this.c = c;
+    this.change = change;
+    this.ps = ps;
+    this.cache = cache;
+  }
+
+  @Override
+  boolean uniquePerUpdate() {
+    return false;
+  }
+
+  @Override
+  void apply(ChangeUpdate update) throws OrmException {
+    checkUpdate(update);
+    if (c.getRevId() == null) {
+      setCommentRevId(c, cache, change, ps);
+    }
+    update.putComment(c);
+  }
+}

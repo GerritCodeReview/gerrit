4d718dfcaa065d0e696cb5cc25d5711e679952ce
Saša Živkov <sasa.zivkov@sap.com>
2016-09-22 10:37:34 +0200

    Add new persistent cache "diff_file_path_list" to cache file paths only
    The new cache caches list of paths of changed files. Main intention of
    the new cache is to speed up change reindexing. By caching list of
    changed files we avoid computing diff for that purpose.
    
    NOTE: The existing "diff" cache implicitly caches list of changed files
    but its entries are quite large to keep it populated for all changes.
    
    Change-Id: I45f90deeba9fe70f32e4b0a5657bfb211ecc2eeb

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/patch/FilePathListLoader.java
@@ -0,0 +1,76 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.patch;
+
+import com.google.gerrit.reviewdb.client.Patch;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class FilePathListLoader implements Callable<FilePathList> {
+  static final Logger log = LoggerFactory.getLogger(FilePathListLoader.class);
+
+  public interface Factory {
+    FilePathListLoader create(PatchListKey key, Project.NameKey project);
+  }
+
+  private final PatchListCache patchListCache;
+  private final PatchListKey key;
+  private final Project.NameKey project;
+
+  @AssistedInject
+  FilePathListLoader(PatchListCache plc,
+      @Assisted PatchListKey k,
+      @Assisted Project.NameKey p) {
+    patchListCache = plc;
+    key = k;
+    project = p;
+  }
+
+  @Override
+  public FilePathList call() throws Exception {
+    PatchList patchList = patchListCache.get(key, project);
+    List<String> r = new ArrayList<>(patchList.getPatches().size());
+    for (PatchListEntry e : patchList.getPatches()) {
+      if (Patch.isMagic(e.getNewName())) {
+        continue;
+      }
+      switch (e.getChangeType()) {
+        case ADDED:
+        case MODIFIED:
+        case DELETED:
+        case COPIED:
+        case REWRITE:
+          r.add(e.getNewName());
+          break;
+
+        case RENAMED:
+          r.add(e.getOldName());
+          r.add(e.getNewName());
+          break;
+      }
+    }
+    Collections.sort(r);
+    return new FilePathList(r.toArray(new String[r.size()]));
+  }
+}

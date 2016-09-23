51dbb66835dea7a69bf7c3d4536f8c6f953f1bfc
Becky Siegel <beckysiegel@google.com>
2016-09-23 14:32:11 -0700

    Pressing "reply" from a comment clears out previous response
    Previously, if you pressed reply from a comment, closed the dialogue,
    and re-pressed "reply", the quoted text from the original comment was
    displayed twice. This resets the textarea to just have quoted text
    and resets anything else.
    
    Bug: Issue 4620
    Change-Id: I5730698e0b1d5328735cd6f49cfd136c55b847e4

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -259,8 +259,7 @@
       var msg = e.detail.message.message;
       var quoteStr = msg.split('\n').map(
           function(line) { return '> ' + line; }).join('\n') + '\n\n';
-      this.$.replyDialog.draft += quoteStr;
-      this._openReplyDialog();
+      this._openReplyDialog({quote: quoteStr});
     },
 
     _handleReplyOverlayOpen: function(e) {
@@ -285,7 +284,7 @@
       if (e.detail.value && e.detail.value.ccsOnly) {
         target = this.$.replyDialog.FocusTarget.CCS;
       }
-      this._openReplyDialog(target);
+      this._openReplyDialog({opt_section: target});
     },
 
     _handleScroll: function() {
@@ -577,10 +576,11 @@
       });
     },
 
-    _openReplyDialog: function(opt_section) {
+    _openReplyDialog: function(args) {
       this.$.replyOverlay.open().then(function() {
         this.$.replyOverlay.setFocusStops(this.$.replyDialog.getFocusStops());
-        this.$.replyDialog.open(opt_section);
+        this.$.replyDialog.draft = args ? args.quote : '';
+        this.$.replyDialog.open(args ? args.opt_section : null);
       }.bind(this));
     },
 

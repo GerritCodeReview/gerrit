8d7b7377232b61ee3dcb7faaa6e4f0e77e101258
Dave Borowitz <dborowitz@google.com>
2016-09-23 13:20:01 +0200

    Store insertions/deletions in DiffSummary cache
    The goal is to use this cache for everything that is needed to
    reindex a change, to avoid computing the full PatchList for each
    change. Having a completely standalone cache for insertions/deletions
    is overkill, but since we already have this cache, it's not too much
    work to store a few more bytes in it.
    
    This change is an alternative to I5a0bab3e.
    
    Change-Id: I52ef20916ddf03f3e8692f1f443d09b89a98b2f1

--- gerrit-server/src/main/java/com/google/gerrit/server/patch/DiffSummaryLoader.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/patch/DiffSummaryLoader.java
@@ -75,6 +75,9 @@ public class DiffSummaryLoader implements Callable<DiffSummary> {
       }
     }
     Collections.sort(r);
-    return new DiffSummary(r.toArray(new String[r.size()]));
+    return new DiffSummary(
+        r.toArray(new String[r.size()]),
+        patchList.getInsertions(),
+        patchList.getDeletions());
   }
 }
--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/ChangeData.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/ChangeData.java
@@ -57,7 +57,6 @@ import com.google.gerrit.server.git.MergeUtil;
 import com.google.gerrit.server.notedb.ChangeNotes;
 import com.google.gerrit.server.notedb.NotesMigration;
 import com.google.gerrit.server.patch.DiffSummary;
-import com.google.gerrit.server.patch.PatchList;
 import com.google.gerrit.server.patch.PatchListCache;
 import com.google.gerrit.server.patch.PatchListNotAvailableException;
 import com.google.gerrit.server.project.ChangeControl;
@@ -335,7 +334,6 @@ public class ChangeData {
   private ListMultimap<PatchSet.Id, PatchSetApproval> allApprovals;
   private List<PatchSetApproval> currentApprovals;
   private Map<Integer, List<String>> files;
-  private Map<Integer, Optional<PatchList>> patchLists;
   private Map<Integer, Optional<DiffSummary>> diffSummaries;
   private Collection<Comment> publishedComments;
   private CurrentUser visibleTo;
@@ -606,26 +604,6 @@ public class ChangeData {
     return r;
   }
 
-  private Optional<PatchList> getPatchList(Change c, PatchSet ps) {
-    Integer psId = ps.getId().get();
-    if (patchLists == null) {
-      patchLists = new HashMap<>();
-    }
-    Optional<PatchList> r = patchLists.get(psId);
-    if (r == null) {
-      if (!lazyLoad) {
-        return Optional.empty();
-      }
-      try {
-        r = Optional.of(patchListCache.get(c, ps));
-      } catch (PatchListNotAvailableException e) {
-        r = Optional.empty();
-      }
-      patchLists.put(psId, r);
-    }
-    return r;
-  }
-
   private Optional<DiffSummary> getDiffSummary(Change c, PatchSet ps) {
     Integer psId = ps.getId().get();
     if (diffSummaries == null) {
@@ -655,8 +633,11 @@ public class ChangeData {
     if (ps == null) {
       return Optional.empty();
     }
-    return getPatchList(c, ps).map(
-        p -> new ChangedLines(p.getInsertions(), p.getDeletions()));
+    Optional<DiffSummary> ds = getDiffSummary(c, ps);
+    if (ds.isPresent()) {
+      return Optional.of(ds.get().getChangedLines());
+    }
+    return Optional.empty();
   }
 
   public Optional<ChangedLines> changedLines() throws OrmException {
@@ -1283,7 +1264,7 @@ public class ChangeData {
     public final int insertions;
     public final int deletions;
 
-    ChangedLines(int insertions, int deletions) {
+    public ChangedLines(int insertions, int deletions) {
       this.insertions = insertions;
       this.deletions = deletions;
     }

8ed980d0ff0a61ad88ac36d0f420ce285e584e84
Stefan Beller <sbeller@google.com>
2016-09-27 10:16:51 -0700

    PreviewSubmit: always allow tgz format
    Currently the setting archive.download is used in two different cases:
    * It is used to specify formats for downloading a patch set archive
    * It is used to specify the formats in preview_submit
    
    However these two use cases serve different purposes:
    The format for downloading a patch set archive can be used to
    adapt to the resource environment:
    * When bandwidth is cheap, tar is a good choice.
    * When CPU is available, tbz2 and txz are good candidates.
    * tgz does an ok job w.r.t. any resource.
    * the admin may deem downloading patches as an archive is
      not a good idea and disable it completely by allowing no formats.
    
    The format for preview_submit is different, though, because it is
    used as a wrapper around already compressed files (git bundles),
    so it makes no sense to spent a lot of CPU to compress it further.
    Also at least one format needs to be allowed, otherwise this call
    cannot be used. Allow tgz as a fall back as tgz is doing well w.r.t.
    all resources.
    
    Change-Id: I7177a6135d507f481bb3fa52c46883951030af11
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PreviewSubmit.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PreviewSubmit.java
@@ -75,6 +75,13 @@ public class PreviewSubmit implements RestReadView<RevisionResource> {
       throw new BadRequestException("format is not specified");
     }
     ArchiveFormat f = allowedFormats.extensions.get("." + format);
+    if (allowedFormats.getAllowed().isEmpty() && format.equals("tar")) {
+      // When no formats are allowed, this is because the server admin wants
+      // to disallow downloading changes as archives. As a side effect this
+      // would also disallow the preview_submit call. Allow tar as a
+      // fall back.
+      f = ArchiveFormat.TGZ;
+    }
     if (f == null) {
       throw new BadRequestException("unknown archive format");
     }

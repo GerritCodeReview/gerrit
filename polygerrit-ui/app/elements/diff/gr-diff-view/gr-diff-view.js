6c06e78cf648a7cb5a8441a279918cc52b243c24
Becky Siegel <beckysiegel@google.com>
2016-10-11 13:23:53 -0700

    Fix horizontal scrolling in file dropdown, comments, and gr diff view
    Previously, long file names would display outside of the window size,
    causing a horizontal scroll on mobile browsers.
    
    With this change:
    * In the file list on the change view, the rightControls are adjusted so
    that they wrap on a new line on smaller/mobile screens.
    * In the comment list, word-wrapping is used for long file paths
    displayed above each comment.
    * In the diff view, instead of displaying full paths in the dropdown,
    the path is truncated to include an ellipsis, a slash and the file name
    so that it can be seen both in the dropdown and also the native browser
    select.
    
    Bug: Issue 4722
    Change-Id: Icd4644a45db71bc6666c21d62c864d91e9874654

--- polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.js
+++ polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.js
@@ -438,7 +438,22 @@
     },
 
     _computeFileDisplayName: function(path) {
-      return path == COMMIT_MESSAGE_PATH ? 'Commit message' : path;
+      return path === COMMIT_MESSAGE_PATH ? 'Commit message' : path;
+    },
+
+    _computeTruncatedFileDisplayName: function(path) {
+      return path === COMMIT_MESSAGE_PATH ?
+          'Commit message' : this._shortenPath(path);
+    },
+
+    _shortenPath: function(path) {
+      var pathPieces = path.split('/');
+
+      if (pathPieces.length < 2) {
+        return path;
+      }
+      // Character is an ellipsis.
+      return '\u2026/' + pathPieces.pop();
     },
 
     _computeFileSelected: function(path, currentPath) {

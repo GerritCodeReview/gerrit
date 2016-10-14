fccfb30eeca041903d964be583269f2caff6be4a
Becky Siegel <beckysiegel@google.com>
2016-10-14 10:59:45 -0700

    Don't display tooltips on touch devices
    Previously on iOS devices, tooltips in reply dialog did not always close
    when clicking outside of the button. The focusOut event was not
    triggered and thus _handleHideTooltip was not called. On other devices,
    the tooltip remained open when clicking on it.
    
    This change detects when a device triggers a touch event and sets a
    boolean variable that it is a touch device. If touch, we do not open and
    close tooltips. This was necessary because mouseenter and mouseleave
    events are still triggered on touch devices with a touch.
    
    Also, mouseover and mouseout events were replaced with mousenter and
    mouseleave events because it seemed better practice since they don't
    bubble to child elements & this is what is done in paper-tooltip.
    
    Bug: Issue 4649
    Change-Id: Ia5605617b00cd9aa8b6d9ef3a4c453faf4b1a4b2

--- polygerrit-ui/app/behaviors/gr-tooltip-behavior/gr-tooltip-behavior.js
+++ polygerrit-ui/app/behaviors/gr-tooltip-behavior/gr-tooltip-behavior.js
@@ -22,6 +22,7 @@
     properties: {
       hasTooltip: Boolean,
 
+      _touch: Boolean,
       _tooltip: Element,
       _titleText: String,
     },
@@ -29,10 +30,11 @@
     attached: function() {
       if (!this.hasTooltip) { return; }
 
-      this.addEventListener('mouseover', this._handleShowTooltip.bind(this));
-      this.addEventListener('mouseout', this._handleHideTooltip.bind(this));
-      this.addEventListener('focusin', this._handleShowTooltip.bind(this));
-      this.addEventListener('focusout', this._handleHideTooltip.bind(this));
+      this._touch = ('ontouchstart' in document.documentElement);
+      this.addEventListener('mouseenter', this._handleShowTooltip.bind(this));
+      this.addEventListener('mouseleave', this._handleHideTooltip.bind(this));
+      this.addEventListener('tap', this._handleHideTooltip.bind(this));
+
       this.listen(window, 'scroll', '_handleWindowScroll');
     },
 
@@ -41,40 +43,45 @@
     },
 
     _handleShowTooltip: function(e) {
-      if (!this.hasAttribute('title') ||
-          this.getAttribute('title') === '' ||
-          this._tooltip) {
-        return;
+      if (!this._touch) {
+        if (!this.hasAttribute('title') ||
+            this.getAttribute('title') === '' ||
+            this._tooltip) {
+          return;
+        }
+
+        // Store the title attribute text then set it to an empty string to
+        // prevent it from showing natively.
+        this._titleText = this.getAttribute('title');
+        this.setAttribute('title', '');
+
+        var tooltip = document.createElement('gr-tooltip');
+        tooltip.text = this._titleText;
+
+        // Set visibility to hidden before appending to the DOM so that
+        // calculations can be made based on the element’s size.
+        tooltip.style.visibility = 'hidden';
+        Polymer.dom(document.body).appendChild(tooltip);
+        this._positionTooltip(tooltip);
+        tooltip.style.visibility = null;
+
+        this._tooltip = tooltip;
       }
-
-      // Store the title attribute text then set it to an empty string to
-      // prevent it from showing natively.
-      this._titleText = this.getAttribute('title');
-      this.setAttribute('title', '');
-
-      var tooltip = document.createElement('gr-tooltip');
-      tooltip.text = this._titleText;
-
-      // Set visibility to hidden before appending to the DOM so that
-      // calculations can be made based on the element’s size.
-      tooltip.style.visibility = 'hidden';
-      Polymer.dom(document.body).appendChild(tooltip);
-      this._positionTooltip(tooltip);
-      tooltip.style.visibility = null;
-
-      this._tooltip = tooltip;
     },
 
     _handleHideTooltip: function(e) {
-      if (!this.hasAttribute('title') ||
-          this._titleText == null ||
-          this === document.activeElement) { return; }
+      if (!this._touch) {
+        if (!this.hasAttribute('title') ||
+            this._titleText == null) {
+          return;
+        }
 
-      this.setAttribute('title', this._titleText);
-      if (this._tooltip && this._tooltip.parentNode) {
-        this._tooltip.parentNode.removeChild(this._tooltip);
+        this.setAttribute('title', this._titleText);
+        if (this._tooltip && this._tooltip.parentNode) {
+          this._tooltip.parentNode.removeChild(this._tooltip);
+        }
+        this._tooltip = null;
       }
-      this._tooltip = null;
     },
 
     _handleWindowScroll: function(e) {

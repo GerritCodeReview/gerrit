8491084bc57fe384360ea17ac0e8bec00bad14c1
Patrick Hiesel <hiesel@google.com>
2016-10-21 16:43:13 +0200

    Implement receiver class structure and bindings
    This commit implements the basic class structure for receiving emails,
    all required config parameters, all necessary bindings depending on
    which protocol the administrator has configured and updates the
    documentation accordingly.
    
    It also adds test-only dependencies to Greenmail and javax.mail. These
    will be used to create integration tests.
    
    It's the first change in a topic of changes to implement email ingestion
    for Gerrit.
    
    Change-Id: I0edec7ca2655fcd70284bb75ca8eb94ce2491d7a

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/receive/MailReceiver.java
@@ -0,0 +1,126 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.mail.receive;
+
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.lifecycle.LifecycleModule;
+import com.google.gerrit.server.mail.EmailSettings;
+import com.google.inject.Inject;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/** MailReceiver implements base functionality for receving emails. */
+public abstract class MailReceiver implements LifecycleListener {
+  protected EmailSettings mailSettings;
+  private Timer timer;
+  private Set<String> pendingDeletion;
+
+  public static class Module extends LifecycleModule {
+    private final EmailSettings mailSettings;
+
+    @Inject
+    Module(EmailSettings mailSettings) {
+      this.mailSettings = mailSettings;
+    }
+
+    @Override
+    protected void configure() {
+      if (mailSettings.protocol == Protocol.NONE) {
+        return;
+      }
+      listener().to(MailReceiver.class);
+      switch (mailSettings.protocol) {
+        case IMAP:
+          bind(MailReceiver.class).to(ImapMailReceiver.class);
+          break;
+        case POP3:
+          bind(MailReceiver.class).to(Pop3MailReceiver.class);
+          break;
+        case NONE:
+        default:
+      }
+    }
+  }
+
+  @Inject
+  public MailReceiver(EmailSettings mailSettings) {
+    this.mailSettings = mailSettings;
+    pendingDeletion = new HashSet<>();
+  }
+
+  @Override
+  public void start() {
+    if (timer == null) {
+      timer = new Timer();
+    } else {
+      timer.cancel();
+    }
+    timer.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        MailReceiver.this.handleEmails();
+      }
+    }, 0l, mailSettings.fetchInterval * 1000);
+  }
+
+  @Override
+  public void stop() {
+    if (timer != null) {
+      timer.cancel();
+    }
+  }
+
+  /**
+   * deleteEmailEventually will enqueue an email for deletion and delete it the
+   * next time we connect to the email server. This does not guarantee deletion
+   * as the Gerrit instance might fail before we connect to the email server.
+   * @param messageId
+   */
+  public void deleteEmailEventually(String messageId) {
+    synchronized (pendingDeletion) {
+      pendingDeletion.add(messageId);
+    }
+  }
+
+  /**
+   * handleEmails will open a connection to the mail server, remove emails
+   * where deletion is pending, read new email and close the connection.
+   */
+  protected abstract void handleEmails();
+
+  /**
+   * Check if deletion for an email is pending.
+   * @param id Identifier of the email
+   * @return true of deletion is pending for this email, false otherwise.
+   */
+  protected boolean isDeletionPending(String id) {
+    synchronized (pendingDeletion) {
+      return pendingDeletion.contains(id);
+    }
+  }
+
+  /**
+   * Mark an email as deleted successfully.
+   * @param id Identifier of the email
+   */
+  protected void markEmailAsDeleted(String id) {
+    synchronized (pendingDeletion) {
+      pendingDeletion.remove(id);
+    }
+  }
+}

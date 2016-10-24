cc03c7bacceaf875ea3fc9b8839f42273d1bfd4a
Patrick Hiesel <hiesel@google.com>
2016-10-24 14:36:48 +0200

    Add POP3 implementation for receiving emails
    This change adds code to retrieve and delete emails using POP3 as well
    as a lightweight implementation to receive the Message-ID from a raw
    email.
    
    It uses Greenmail for integration testing and tests if Gerrit can delete
    emails which implies that it can also read emails.
    
    Change-Id: Ifa20a5d77428759d85b24b2fe2c161e72f81c991

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/receive/RawMailParser.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/receive/RawMailParser.java
@@ -14,13 +14,52 @@
 
 package com.google.gerrit.server.mail.receive;
 
+import org.joda.time.DateTime;
+
 /**
  * RawMailParser parses raw email content received through POP3 or IMAP into
  * an internal {@link MailMessage}.
+ *
+ * @see <a href="https://apache.googlesource.com/commons-email/+/EMAIL_1_4/src/main/java/org/apache/commons/mail/util/MimeMessageParser.java#175">
+ *   Apache Mail Parser for a reference implementation.</a>
  */
 public class RawMailParser {
+  private enum MessagePart {
+    HEADER, BODY
+  }
+
   public static MailMessage parse(String raw) throws MailParsingException {
-    // TODO(hiesel) Implement.
-    return null;
+    // TODO(hiesel) Provide comprehensive and fail-safe header parser.
+    MailMessage.Builder b = MailMessage.builder();
+    MessagePart messagePart = MessagePart.HEADER;
+    String[] lines = raw.split("\r\n");
+    for (String line : lines) {
+      if (messagePart == MessagePart.HEADER && line.equals("")) {
+        messagePart = MessagePart.BODY;
+      }
+      if (messagePart == MessagePart.HEADER) {
+        String key = line.split(": ")[0];
+        String value = line.split(": ")[1];
+        if (key.equals("Message-ID")) {
+          b.id(value);
+        }
+      }
+    }
+    // TODO(hiesel) Provide complete implementation.
+    b.textContent("")
+        .htmlContent("")
+        .subject("")
+        .dateReceived(new DateTime())
+        .from("");
+    return b.build();
+  }
+
+  public static MailMessage parse(int[] chars) throws MailParsingException {
+    // TODO(hiesel) Parse header first and check encoding. Handle body encoding properly
+    StringBuilder b = new StringBuilder(chars.length);
+    for (int c : chars) {
+      b.append((char) c);
+    }
+    return parse(b.toString());
   }
 }

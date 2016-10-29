0612feb28a1e2c90e714928f190361c7d5254f4a
David Ostrovsky <david@ostrovsky.org>
2016-10-29 11:57:46 +0200

    Bazel: Stamp git version for core plugins
    Extend workspace status script to scan plugins directory and generate
    git version for each of them in stable-status.txt file. This can be
    used in plugin build rule to include this as plugin version info.
    
    Given that there is no way to dynamically merge manifest file content
    in Bazel[1] in java_binary rule, we have two options: either manipulate
    the plugin artifact file by appending the generated plugin version in
    post java_binary step in the META-INF/MANIFEST.MF file, or add VERSION
    resource file in the plugin and route reading plugin version to this
    file.
    
    [1] https://github.com/bazelbuild/bazel/issues/2009
    
    Change-Id: Ie900617e918246ebb54080517161021f3030fdab

--- tools/workspace-status.sh
+++ tools/workspace-status.sh
@@ -10,6 +10,19 @@
 # If the script exits with non-zero code, it's considered as a failure
 # and the output will be discarded.
 
-git_rev=$(git describe --always --match "v[0-9].*" --dirty)
-
-echo "STABLE_BUILD_GERRIT_LABEL ${git_rev}"
+for p in . $(ls plugins); do
+  if test "$p" = .; then
+    dir=.
+    nameUpper=GERRIT
+  else
+    nameUpper=$(tr '[:lower:]' '[:upper:]' <<<"$p")
+    dir="plugins/$p"
+  fi
+  test -d "$dir" || continue
+  label=$(git -C "$dir" describe --always --match "v[0-9].*" --dirty)
+  if test -n "$label"; then
+    echo "STABLE_BUILD_${nameUpper}_LABEL $label"
+  else
+    echo "git describe failed for directory \`$dir'" >&2
+  fi
+done

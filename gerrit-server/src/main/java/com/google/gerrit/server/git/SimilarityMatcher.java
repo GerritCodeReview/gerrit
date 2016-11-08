7cead7f225a934853e251ff94206a80529167602
Masaya Suzuki <masayasuzuki@google.com>
2016-11-07 20:54:13 -0800

    Match changes based on similarity of commits
    When creating a new PatchSet, Gerrit uses the Change-Id line to find the
    corresponding change. When importing pull-requests to Gerrit, usually
    there's no Change-Id line in the commit message.
    
    This change adds a push option to find the corresponding change based on
    similarity. This option should be used with a topic option; if it's not
    specified, the push is rejected. When similarity-match option is used,
    Gerrit matches the pushed commits with the changes in the same topic. If
    there's a similar change, add the pushed commit as a new PatchSet.
    Otherwise, create a new change. Non-matched existing changes in the
    topic are abandoned.
    
    For example, suppose we have three changes in the topic "mytopic":
    
      c1 <- c2 <- c3
    
      change #1000: patchset=c1, message=First commit, topic=mytopic
      change #1001: patchset=c2, message=Second commit, topic=mytopic
      change #1002: patchset=c3, message=Third commit, topic=mytopic
    
    and then we squash c2 and c3 into one (c2') and add c4:
    
      c1 <- c2' <- c4
    
    When c4 is pushed with topic=mytopic,similarity-match option, we'll have
    following changes:
    
      change #1000: patchset=c1, message=First commit, topic=mytopic
      change #1001: patchset=c2',c2, message=Second commit, topic=mytopic
      change #1002: abandoned
      change #1003: patchset=c4, message=Fourth commit, topic=mytopic
    
    Change #1000 won't change. The commit c2' is classified as similar to
    c2, and added as a new patchset of change #1001. c4 doesn't have a
    corresponding change, and a new change #1003 is created. Change #1002
    doesn't have a corresponding commit in the pushed commits, so it's
    abandoned.
    
    The similarity of the commits are calculated based on the commit
    messages and the diff text. Calculate the similarities of each string
    like the delta compression. Based on the calculated similarities, use a
    classifier built with support vector machine to decide if the commits
    are similar or not.
    
    Change-Id: I1befbbf816720d834fec1da7f22500be259b5c4f

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/SimilarityMatcher.java
@@ -0,0 +1,109 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.server.git.SimilarityDecisionFunction.Features;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class SimilarityMatcher {
+  @Inject
+  SimilarityMatcher() {}
+
+  Result matchChanges(
+      List<RevCommit> commits, List<ChangeData> changes, Repository repo)
+      throws IOException, OrmException {
+    if (changes.isEmpty()) {
+      return new Result(
+          ImmutableList.copyOf(commits),
+          ImmutableMap.<RevCommit, ChangeData>of(),
+          ImmutableList.<ChangeData>of());
+    }
+
+    try (RevWalk walk = new RevWalk(repo)) {
+      Map<ChangeData, Features> changeFeatures = new HashMap<>();
+      for (ChangeData change : changes) {
+        PatchSet ps = change.currentPatchSet();
+        if (ps == null) {
+          // Ignore the changes with no PatchSet.
+          continue;
+        }
+        changeFeatures.put(
+            change,
+            new Features(
+                walk, walk.parseCommit(
+                    ObjectId.fromString(ps.getRevision().get())), repo));
+      }
+
+      ImmutableList.Builder<RevCommit> newCommits = ImmutableList.builder();
+      ImmutableMap.Builder<RevCommit, ChangeData> updateChanges =
+          ImmutableMap.builder();
+      for (RevCommit commit : commits) {
+        Features commitFeatures = new Features(walk, commit, repo);
+
+        ChangeData similarChange = null;
+        for (Map.Entry<ChangeData, Features> entry :
+            changeFeatures.entrySet()) {
+          if (SimilarityDecisionFunction.INSTANCE.isSimilar(
+              commitFeatures, entry.getValue())) {
+            similarChange = entry.getKey();
+            break;
+          }
+        }
+
+        if (similarChange != null) {
+          updateChanges.put(commit, similarChange);
+          changeFeatures.remove(similarChange);
+        } else {
+          newCommits.add(commit);
+        }
+      }
+
+      // Abandon the leftovers.
+      return new Result(
+          newCommits.build(), updateChanges.build(),
+          ImmutableList.copyOf(changeFeatures.keySet()));
+    }
+  }
+
+  static class Result {
+    final List<RevCommit> newCommits;
+    final Map<RevCommit, ChangeData> updateChanges;
+    final List<ChangeData> abandonChanges;
+
+    Result(
+        List<RevCommit> newCommits,
+        Map<RevCommit, ChangeData> updateChanges,
+        List<ChangeData> abandonChanges) {
+      this.newCommits = newCommits;
+      this.updateChanges = updateChanges;
+      this.abandonChanges = abandonChanges;
+    }
+  }
+}

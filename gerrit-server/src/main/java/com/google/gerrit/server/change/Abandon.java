f114bef5d80f7cd80d51485d7767b654b7603a67
Masaya Suzuki <masayasuzuki@google.com>
2016-11-07 20:54:13 -0800

    Match changes based on similarity of commits
    When creating a new PatchSet, Gerrit uses the Change-Id line to find the
    corresponding change. When importing pull-requests to Gerrit, usually
    there's no Change-Id line in the commit message.
    
    This change adds a push option to find the corresponding change based on
    similarity. When this option is used, Gerrit matches the pushed commits
    with the changes in the same topic. If there's a similar change, add the
    pushed commit as a new PatchSet. Otherwise, create a new change.
    Non-matched existing changes in the topic are abandoned.
    
    The similarity of the commits are calculated based on the commit
    messages and the diff text. Calculate the similarities of each string
    like the delta compression. Based on the calculated similarities, use a
    classifier built with support vector machine to decide if the commits
    are similar or not.
    
    Change-Id: I1befbbf816720d834fec1da7f22500be259b5c4f

--- gerrit-server/src/main/java/com/google/gerrit/server/change/Abandon.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/Abandon.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.server.change;
 
-import com.google.common.base.Strings;
 import com.google.gerrit.common.TimeUtil;
 import com.google.gerrit.extensions.api.changes.AbandonInput;
 import com.google.gerrit.extensions.api.changes.NotifyHandling;
@@ -26,21 +25,12 @@ import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.extensions.webui.UiAction;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
-import com.google.gerrit.reviewdb.client.ChangeMessage;
-import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.reviewdb.server.ReviewDb;
-import com.google.gerrit.server.ChangeMessagesUtil;
 import com.google.gerrit.server.CurrentUser;
-import com.google.gerrit.server.PatchSetUtil;
-import com.google.gerrit.server.extensions.events.ChangeAbandoned;
+import com.google.gerrit.server.git.AbandonOp;
 import com.google.gerrit.server.git.BatchUpdate;
-import com.google.gerrit.server.git.BatchUpdate.ChangeContext;
-import com.google.gerrit.server.git.BatchUpdate.Context;
 import com.google.gerrit.server.git.UpdateException;
-import com.google.gerrit.server.mail.send.AbandonedSender;
-import com.google.gerrit.server.mail.send.ReplyToChangeSender;
-import com.google.gerrit.server.notedb.ChangeUpdate;
 import com.google.gerrit.server.project.ChangeControl;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
@@ -57,29 +47,21 @@ public class Abandon implements RestModifyView<ChangeResource, AbandonInput>,
     UiAction<ChangeResource> {
   private static final Logger log = LoggerFactory.getLogger(Abandon.class);
 
-  private final AbandonedSender.Factory abandonedSenderFactory;
   private final Provider<ReviewDb> dbProvider;
   private final ChangeJson.Factory json;
-  private final ChangeMessagesUtil cmUtil;
-  private final PatchSetUtil psUtil;
   private final BatchUpdate.Factory batchUpdateFactory;
-  private final ChangeAbandoned changeAbandoned;
+  private final AbandonOp.Factory abandonOpFactory;
 
   @Inject
-  Abandon(AbandonedSender.Factory abandonedSenderFactory,
+  Abandon(
       Provider<ReviewDb> dbProvider,
       ChangeJson.Factory json,
-      ChangeMessagesUtil cmUtil,
-      PatchSetUtil psUtil,
       BatchUpdate.Factory batchUpdateFactory,
-      ChangeAbandoned changeAbandoned) {
-    this.abandonedSenderFactory = abandonedSenderFactory;
+      AbandonOp.Factory abandonOpFactory) {
     this.dbProvider = dbProvider;
     this.json = json;
-    this.cmUtil = cmUtil;
-    this.psUtil = psUtil;
     this.batchUpdateFactory = batchUpdateFactory;
-    this.changeAbandoned = changeAbandoned;
+    this.abandonOpFactory = abandonOpFactory;
   }
 
   @Override
@@ -105,7 +87,11 @@ public class Abandon implements RestModifyView<ChangeResource, AbandonInput>,
 
   public Change abandon(ChangeControl control, String msgTxt,
       NotifyHandling notifyHandling) throws RestApiException, UpdateException {
-    Op op = new Op(msgTxt, notifyHandling);
+    CurrentUser user = control.getUser();
+    Account account = user.isIdentifiedUser()
+        ? user.asIdentifiedUser().getAccount()
+        : null;
+    AbandonOp op = abandonOpFactory.create(account, msgTxt, notifyHandling);
     try (BatchUpdate u =
         batchUpdateFactory.create(
             dbProvider.get(),
@@ -114,7 +100,7 @@ public class Abandon implements RestModifyView<ChangeResource, AbandonInput>,
             TimeUtil.nowTs())) {
       u.addOp(control.getId(), op).execute();
     }
-    return op.change;
+    return op.getChange();
   }
 
   /**
@@ -131,6 +117,9 @@ public class Abandon implements RestModifyView<ChangeResource, AbandonInput>,
     if (controls.isEmpty()) {
       return;
     }
+    Account account = user.isIdentifiedUser()
+        ? user.asIdentifiedUser().getAccount()
+        : null;
     try (BatchUpdate u = batchUpdateFactory.create(
         dbProvider.get(), project, user, TimeUtil.nowTs())) {
       for (ChangeControl control : controls) {
@@ -141,7 +130,7 @@ public class Abandon implements RestModifyView<ChangeResource, AbandonInput>,
                   control.getProject().getNameKey().get(),
                   project.get()));
         }
-        u.addOp(control.getId(), new Op(msgTxt, notifyHandling));
+        u.addOp(control.getId(), abandonOpFactory.create(account, msgTxt, notifyHandling));
       }
       u.execute();
     }
@@ -159,75 +148,6 @@ public class Abandon implements RestModifyView<ChangeResource, AbandonInput>,
     batchAbandon(project, user, controls, "", NotifyHandling.ALL);
   }
 
-  private class Op extends BatchUpdate.Op {
-    private final String msgTxt;
-    private final NotifyHandling notifyHandling;
-
-    private Change change;
-    private PatchSet patchSet;
-    private ChangeMessage message;
-
-    private Op(String msgTxt, NotifyHandling notifyHandling) {
-      this.msgTxt = msgTxt;
-      this.notifyHandling = notifyHandling;
-    }
-
-    @Override
-    public boolean updateChange(ChangeContext ctx) throws OrmException,
-        ResourceConflictException {
-      change = ctx.getChange();
-      PatchSet.Id psId = change.currentPatchSetId();
-      ChangeUpdate update = ctx.getUpdate(psId);
-      if (!change.getStatus().isOpen()) {
-        throw new ResourceConflictException("change is " + status(change));
-      } else if (change.getStatus() == Change.Status.DRAFT) {
-        throw new ResourceConflictException(
-            "draft changes cannot be abandoned");
-      }
-      patchSet = psUtil.get(ctx.getDb(), ctx.getNotes(), psId);
-      change.setStatus(Change.Status.ABANDONED);
-      change.setLastUpdatedOn(ctx.getWhen());
-
-      update.setStatus(change.getStatus());
-      message = newMessage(ctx);
-      cmUtil.addChangeMessage(ctx.getDb(), update, message);
-      return true;
-    }
-
-    private ChangeMessage newMessage(ChangeContext ctx) throws OrmException {
-      StringBuilder msg = new StringBuilder();
-      msg.append("Abandoned");
-      if (!Strings.nullToEmpty(msgTxt).trim().isEmpty()) {
-        msg.append("\n\n");
-        msg.append(msgTxt.trim());
-      }
-
-      return ChangeMessagesUtil.newMessage(
-          ctx, msg.toString(), ChangeMessagesUtil.TAG_ABANDON);
-    }
-
-    @Override
-    public void postUpdate(Context ctx) throws OrmException {
-      Account account = ctx.getUser().isIdentifiedUser()
-          ? ctx.getUser().asIdentifiedUser().getAccount()
-          : null;
-      try {
-        ReplyToChangeSender cm =
-            abandonedSenderFactory.create(ctx.getProject(), change.getId());
-        if (account != null) {
-          cm.setFrom(account.getId());
-        }
-        cm.setChangeMessage(message.getMessage(), ctx.getWhen());
-        cm.setNotify(notifyHandling);
-        cm.send();
-      } catch (Exception e) {
-        log.error("Cannot email update for change " + change.getId(), e);
-      }
-      changeAbandoned.fire(change, patchSet, account, msgTxt, ctx.getWhen(),
-          notifyHandling);
-    }
-  }
-
   @Override
   public UiAction.Description getDescription(ChangeResource resource) {
     boolean canAbandon = false;
@@ -242,9 +162,5 @@ public class Abandon implements RestModifyView<ChangeResource, AbandonInput>,
       .setVisible(resource.getChange().getStatus().isOpen()
           && resource.getChange().getStatus() != Change.Status.DRAFT
           && canAbandon);
-  }
-
-  private static String status(Change change) {
-    return change != null ? change.getStatus().name().toLowerCase() : "deleted";
   }
 }

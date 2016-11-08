8c2c1105538e709d2da7dac14bf1a50895e5ae9f
Masaya Suzuki <masayasuzuki@google.com>
2016-11-07 20:54:13 -0800

    Match changes based on similarity of commits
    When creating a new PatchSet, Gerrit uses the Change-Id line to find the
    corresponding change. When importing pull-requests to Gerrit, usually
    there's no Change-Id line in the commit message.
    
    This change adds a push option to find the corresponding change based on
    similarity. When this option is used, Gerrit matches the pushed commits
    with the changes in the same topic. If there's a similar change, add the
    pushed commit as a new PatchSet. Otherwise, create a new change.
    Non-matched existing changes in the topic are abandoned.
    
    The similarity of the commits are calculated based on the commit
    messages and the diff text. Calculate the similarities of each string
    like the delta compression. Based on the calculated similarities, use a
    classifier built with support vector machine to decide if the commits
    are similar or not.
    
    Change-Id: I1befbbf816720d834fec1da7f22500be259b5c4f

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/git/SimilarityDecisionFunctionTest.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class SimilarityDecisionFunctionTest {
+  @Test
+  public void test() {
+    assertTrue(SimilarityDecisionFunction.INSTANCE.isSimilar(1.0, 1.0));
+    assertTrue(SimilarityDecisionFunction.INSTANCE.isSimilar(0.7, 0.7));
+    assertTrue(SimilarityDecisionFunction.INSTANCE.isSimilar(0.2, 0.7));
+    assertFalse(SimilarityDecisionFunction.INSTANCE.isSimilar(0.0, 0.0));
+    assertFalse(SimilarityDecisionFunction.INSTANCE.isSimilar(0.2, 0.2));
+    assertFalse(SimilarityDecisionFunction.INSTANCE.isSimilar(0.7, 0.2));
+  }
+}

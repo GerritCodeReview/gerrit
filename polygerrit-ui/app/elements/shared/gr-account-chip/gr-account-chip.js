4150787d31b0bdb40c596f1988289e0cd1b7ebdd
Kasper Nilsson <kaspern@google.com>
2016-11-11 12:44:14 -0800

    Make account list navigable by keyboard
    Makes account chips focusable via tabindex attribute, and responds to
    keypresses while they're focused.
    
    Feature: Issue 4703
    Change-Id: Id8f73e73b91afa0582f06cb981fd117d021accff

--- polygerrit-ui/app/elements/shared/gr-account-chip/gr-account-chip.js
+++ polygerrit-ui/app/elements/shared/gr-account-chip/gr-account-chip.js
@@ -18,6 +18,19 @@
   Polymer({
     is: 'gr-account-chip',
 
+    /**
+     * Fired to indicate a key was pressed while this chip was focused.
+     *
+     * @event account-chip-keydown
+     */
+
+    /**
+     * Fired to indicate this chip should be removed, i.e. when the x button is
+     * clicked or when the remove function is called.
+     *
+     * @event remove
+     */
+
     properties: {
       account: Object,
       removable: {
@@ -41,7 +54,8 @@
     },
 
     _getBackgroundClass: function(transparent) {
-      return transparent ? 'transparentBackground' : '';
+      if (transparent) { return 'transparentBackground'; }
+      return '';
     },
 
     _handleRemoveTap: function(e) {

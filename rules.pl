b0df4bb3cf1dccea8a129a415b536e8dd44f4ba0
David Ostrovsky <david.ostrovsky@gmail.com>
2016-11-13 23:19:49 +0000

    Split Verify label to account for Buck and Bazel verifications
    Change-Id: I0cb17c700cd8b20d04710b14ffff637381edd674

--- rules.pl
+++ rules.pl
@@ -3,24 +3,26 @@ submit_rule(submit(CR)) :-
   !,
   gerrit:max_with_block(-2, 2, 'Code-Review', CR).
 
-submit_rule(submit(CR, V, L)) :-
+submit_rule(submit(CR, VB, VZ, L)) :-
   needs_library_compliance,
   !,
-  base(CR, V),
+  base(CR, VB, VZ),
   gerrit:max_with_block(-1, 1, 'Library-Compliance', L).
 
-submit_rule(submit(CR, V)) :-
+submit_rule(submit(CR, VB, VZ)) :-
   is_no_polygerrit_change,
   !,
-  base(CR, V).
+  base(CR, VB, VZ).
 
-submit_rule(submit(P, V)) :-
+submit_rule(submit(P, VB, VZ)) :-
   gerrit:max_with_block(-2, 2, 'PolyGerrit-Review', P),
-  gerrit:max_with_block(-1, 1, 'Verified', V).
+  gerrit:max_with_block(-1, 1, 'Verified-Buck', VB),
+  gerrit:max_with_block(-1, 1, 'Verified-Bazel', VZ).
 
-base(CR, V) :-
+base(CR, VB, VZ) :-
   gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-  gerrit:max_with_block(-1, 1, 'Verified', V).
+  gerrit:max_with_block(-1, 1, 'Verified-Buck', VB),
+  gerrit:max_with_block(-1, 1, 'Verified-Bazel', VZ).
 
 needs_library_compliance :- gerrit:commit_delta('^lib/'), !.
 needs_library_compliance :- gerrit:commit_delta('^[.]buckversion$'), !.

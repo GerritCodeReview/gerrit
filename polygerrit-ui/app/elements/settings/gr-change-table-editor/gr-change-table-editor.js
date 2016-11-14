febf26b8f9e90aa48836ec682c1497c810edd34a
Becky Siegel <beckysiegel@google.com>
2016-11-14 08:37:06 -0800

    Add setting to Show/Hide change table columns in settings
    This change adds a widget to adjust change table columns in the user's
    settings. If no change table columns are adjusted, the server returns an
    empty array and the client determines the default columns, stored in the
    gr-change-table-behavior.
    
    Columns can get removed/re-ordered by the user but they are limited to
    those specified in gr-change-table-behavior. Column preferences persist
    across all gr-change-list items (dashboards, searches, etc).
    
    Feature: Issue 4753
    Change-Id: I47d5b9f53a95c0c010b04c4495094f188d85e67e

---
+++ polygerrit-ui/app/elements/settings/gr-change-table-editor/gr-change-table-editor.js
@@ -0,0 +1,51 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-change-table-editor',
+
+    properties: {
+      changeTableItems: Array,
+      changeTableNotDisplayed: Array,
+    },
+
+    behaviors: [
+      Gerrit.ChangeTableBehavior,
+    ],
+
+    _handleDeleteButton: function(e) {
+      var index = e.target.dataIndex;
+      this.splice('changeTableItems', index, 1);
+
+      // Cse the change table behavior to make sure ordering of unused
+      // columns ends up correct. If the removed item is appended to the
+      // end, when it is saved, the unused column order may shift
+      // around.
+      this.set('changeTableNotDisplayed',
+          this.getComplementColumns(this.changeTableItems));
+
+    },
+
+    _handleAddButton: function(e) {
+      var index = e.target.dataIndex;
+      var newColumn = this.changeTableNotDisplayed[index];
+      this.splice('changeTableNotDisplayed', index, 1);
+
+      this.splice('changeTableItems', this.getComplementColumns(
+          this.changeTableNotDisplayed).indexOf(newColumn), 0, newColumn);
+    },
+  });
+})();

7b03f1c01b17475437a4c82cd6e57dc8b7cfddbe
Luca Milanesio <luca.milanesio@gmail.com>
2016-11-15 10:22:38 -0800

    Support for loading Guice modules from /lib
    Gerrit already has "pluggable" features that need to be installed
    at the early stages of the bootstrap phase and cannot be "unplugged"
    as currently plugins do.
    
    We collectively decided to call this new entities "LibModule" and
    configure them in the gerrit.config as follows:
    
      [gerrit]
        installModule = com.googlesource.gerrit.libmodule.MyModule
        installModule = com.example.abc.OurSpecialSauceModule
    
    Change-Id: Id8bf1a2e88b14c8e9125f20cb10a041d47799100

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/LibModuleLoader.java
@@ -0,0 +1,67 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server;
+
+import static java.util.stream.Collectors.toList;
+
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+
+import org.eclipse.jgit.lib.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LibModuleLoader {
+  private static final Logger log = LoggerFactory
+      .getLogger(LibModuleLoader.class);
+
+  private final Config config;
+  private final Injector configInjector;
+
+  public static List<com.google.inject.Module> loadModules(
+      Injector configInjector) {
+    return configInjector.getInstance(LibModuleLoader.class).loadModules();
+  }
+
+  @Inject
+  LibModuleLoader(@GerritServerConfig Config config, Injector configInjector) {
+    this.config = config;
+    this.configInjector = configInjector;
+  }
+
+  private List<com.google.inject.Module> loadModules() {
+    return Arrays.stream(config.getStringList("gerrit", null, "installModule"))
+        .map(this::loadModule).collect(toList());
+  }
+
+  @SuppressWarnings("unchecked")
+  private com.google.inject.Module loadModule(String className) {
+    com.google.inject.Module libModule = null;
+    try {
+      Class<com.google.inject.Module> moduleClass =
+          (Class<com.google.inject.Module>) Class.forName(className);
+      libModule = configInjector.getInstance(moduleClass);
+      log.info("Installed {}", className);
+      return libModule;
+    } catch (ClassNotFoundException e) {
+      log.error("Unable to load LibModule " + className, e);
+      throw new RuntimeException(e);
+    }
+  }
+}

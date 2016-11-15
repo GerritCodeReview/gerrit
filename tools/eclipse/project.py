a21a00aa9942363b946914512bd9a3bf0fdc8de5
Zhen Chen <czhen@google.com>
2016-11-15 11:37:59 -0800

    Add missing import for project.py
    import os.makedirs
    
    Change-Id: Ibc279c8b7f2bb5198f9fa3fd1dfa587ee75e88b0

--- tools/eclipse/project.py
+++ tools/eclipse/project.py
@@ -18,6 +18,7 @@
 from __future__ import print_function
 from optparse import OptionParser
 from os import path
+from os import makedirs
 from subprocess import Popen, PIPE, CalledProcessError, check_call
 from xml.dom import minidom
 import re

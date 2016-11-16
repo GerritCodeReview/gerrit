131b96adc904bc03edc3af569f7cf38bfb40d3f4
David Ostrovsky <david@ostrovsky.org>
2016-11-16 08:17:38 -0800

    Fix build
    I9014b009 broke the build.
    
    TEST PLAN:
    
      $ bazel test //...
    
    Change-Id: I23326eb01de4884913dafe5b7fdcbe5a4694384e

--- gerrit-server/src/test/java/com/google/gerrit/server/index/change/FakeQueryBuilder.java
+++ gerrit-server/src/test/java/com/google/gerrit/server/index/change/FakeQueryBuilder.java
@@ -29,8 +29,8 @@ public class FakeQueryBuilder extends ChangeQueryBuilder {
           FakeQueryBuilder.class),
         new ChangeQueryBuilder.Arguments(null, null, null, null, null, null,
           null, null, null, null, null, null, null, null, null, null, null,
-          null, null, null, indexes, null, null, null, null, null, null, null,
-          null));
+          null, null, null, null, indexes, null, null, null, null, null, null,
+          null, null));
   }
 
   @Operator

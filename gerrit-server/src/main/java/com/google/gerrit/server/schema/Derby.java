d88ab0048e8677ba9bd55eea7e10e802306494db
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2016-11-21 16:00:26 -0500

    Fix DB connection pool verification
    So far, given no validation query was specified, a broken connection
    could be returned from the connection pool and this caused a 500 error
    when trying, for example, to read a change from the database.
    
    Specify a default validation query that works for several of the DBMS
    supported by Gerrit (H2, PostgreSQL, MySQL), and specific validation
    queries for other supported databases (Apache Derby, DB2, Oracle).
    Validation queries were verified for open source databases (H2, MySQL,
    PostgreSQL and Derby).
    
    Change-Id: Ia215288d70b11893b7d1403611e109a5a951f090

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/Derby.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/Derby.java
@@ -41,4 +41,9 @@ class Derby extends BaseDataSourceType {
     }
     return "jdbc:derby:" + site.resolve(database).toString() + ";create=true";
   }
+
+  @Override
+  public String getValidationQuery() {
+    return "values 1";
+  }
 }

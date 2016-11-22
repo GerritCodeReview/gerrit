bcdac1f71720ffccfcce43b0989a97dc3e12b37b
Edwin Kempin <ekempin@google.com>
2016-11-22 16:50:10 +0100

    Add REST endpoint to reindex a single account
    This may become handy to fix single accounts that are stale in the
    index.
    
    Change-Id: If4c1787a26a564c19d1c4c1896afa89120ef4813
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/Index.java
@@ -0,0 +1,56 @@
+//Copyright (C) 2016 The Android Open Source Project
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.Index.Input;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import java.io.IOException;
+
+@Singleton
+public class Index implements RestModifyView<AccountResource, Input> {
+  public static class Input {
+  }
+
+  private final AccountCache accountCache;
+  private final Provider<CurrentUser> self;
+
+  @Inject
+  Index(AccountCache accountCache,
+      Provider<CurrentUser> self) {
+    this.accountCache = accountCache;
+    this.self = self;
+  }
+
+  @Override
+  public Response<?> apply(AccountResource rsrc, Input input)
+      throws IOException, AuthException, OrmException {
+    if (self.get() != rsrc.getUser()
+        && !self.get().getCapabilities().canModifyAccount()) {
+      throw new AuthException("not allowed index account");
+    }
+
+    // evicting the account from the cache, reindexes the account
+    accountCache.evict(rsrc.getUser().getAccountId());
+    return Response.none();
+  }
+}

2a4152d99d660c209d4b2cc2c0728a26652e5518
Stefan Beller <sbeller@google.com>
2016-11-29 14:54:13 -0800

    PreviewSubmit: fix generation of compressed tar entries
    For the PreviewSubmit call plain tar as well as zip works fine, but
    formats like tbz2, tgz and txz are broken.
    
    This bug comes from not knowing the size of the bundles before writing
    them, such that it was assumed to 0. Plain tar as well as zip worked
    just fine as the tools ignored the wrong size.
    
    Fix the bug by creating the bundle in memory and then correctly use
    JGits ArchiveCommand for different formats.
    
    To demonstrate the bug is fixed, add a test that just lists files
    in the return of a call of preview submit call with tgz format
    specificed.
    
    By this change a bundle cannot be streamed any more, but needs to
    be kept in memory. As bundles may become large, introduce a new
    config option to set an upper bound for a bundle that is build up
    in memory.
    
    A slightly unrelated change is the wrapping of the
    ArchiveOutputStream in a try-with-resource block as it implements
    AutoCloseable. (Before it was not closed explicitely but just
    finish()ed.)
    
    Change-Id: I587c0e81ed79be94d7abf2a19ad2086c005933d9
    Signed-off-by: Stefan Beller <sbeller@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/LimitedByteArrayOutputStream.java
@@ -0,0 +1,72 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+class LimitedByteArrayOutputStream extends OutputStream {
+
+  private final int maxSize;
+  private final ByteArrayOutputStream buffer;
+
+  /**
+   * Constructs a LimitedByteArrayOutputStream, which stores output
+   * in memory up to a certain specified size. When the output exceeds
+   * the specified size a LimitExceededException is thrown.
+   *
+   * @param max the maximum size in bytes which may be stored.
+   * @param initial the initial size. It must be smaller than the max size.
+   */
+  public LimitedByteArrayOutputStream(int max, int initial) {
+    checkArgument(initial <= max);
+    maxSize = max;
+    buffer = new ByteArrayOutputStream(initial);
+  }
+
+  private void checkOversize(int additionalSize) throws IOException {
+    if (buffer.size() + additionalSize > maxSize) {
+      throw new LimitExceededException();
+    }
+  }
+
+  @Override
+  public void write(int b) throws IOException{
+    checkOversize(1);
+    buffer.write(b);
+  }
+
+  @Override
+  public void write(byte[] b, int off, int len) throws IOException{
+    checkOversize(len);
+    buffer.write(b, off, len);
+  }
+
+  /**
+   * Creates a newly allocated byte array. Its size is the current
+   * size of this output stream and the valid contents of the buffer
+   * have been copied into it.
+   */
+  public byte toByteArray()[] {
+      return buffer.toByteArray();
+  }
+
+  class LimitExceededException extends IOException {
+    private static final long serialVersionUID = 1L;
+  }
+}

2dba4a888a406cadbdb4a54d7596bf26d336c813
Stefan Beller <sbeller@google.com>
2016-11-29 14:54:13 -0800

    PreviewSubmit: fix generation of compressed tar entries
    For the PreviewSubmit call plain tar as well as zip works fine, but
    formats like tbz2, tgz and txz are broken.
    
    This bug comes from not knowing the size of the bundles before writing
    them, such that it was assumed to 0. Plain tar as well as zip worked
    just fine as the tools ignored the wrong size.
    
    Fix the bug by creating the bundle in memory and then correctly use
    JGits ArchiveCommand for different formats.
    
    To demonstrate the bug is fixed, add a test that just lists files
    in the return of a call of preview submit call with tgz format
    specificed.
    
    Change-Id: I587c0e81ed79be94d7abf2a19ad2086c005933d9
    Signed-off-by: Stefan Beller <sbeller@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/LimitedByteArrayOutputStream.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import java.io.ByteArrayOutputStream;
+
+public class LimitedByteArrayOutputStream extends ByteArrayOutputStream {
+
+  private final int maxSize;
+
+  public LimitedByteArrayOutputStream(int max, int initial) {
+    super(initial);
+    maxSize = max;
+  }
+
+  @Override
+  public synchronized void write(int b) {
+    if (size() + 1 > maxSize) {
+      throw new IndexOutOfBoundsException();
+    }
+    super.write(b);
+  }
+
+  @Override
+  public synchronized void write(byte[] b, int off, int len) {
+    if (size() + len > maxSize) {
+      throw new IndexOutOfBoundsException();
+    }
+    super.write(b, off, len);
+  }
+}

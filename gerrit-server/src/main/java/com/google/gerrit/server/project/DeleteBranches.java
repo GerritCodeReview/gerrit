39806f60fef51fdc9633fb1f71280f61e1896afd
David Pursehouse <dpursehouse@collab.net>
2016-12-02 22:06:50 +0900

    Move implementation from DeleteBranch and DeleteBranches to DeleteRef
    Introduce a new class, DeleteRef, that implements deletion of either a
    single ref or multiple refs.
    
    The intention is to reuse this to implement new REST API endpoints for
    tag deletion.
    
    Change-Id: I928007346310bf2b0a94e678c3eb0882b48ca244

--- gerrit-server/src/main/java/com/google/gerrit/server/project/DeleteBranches.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/DeleteBranches.java
@@ -14,61 +14,25 @@
 
 package com.google.gerrit.server.project;
 
-import static java.lang.String.format;
-
 import com.google.gerrit.extensions.api.projects.DeleteBranchesInput;
 import com.google.gerrit.extensions.restapi.BadRequestException;
-import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestModifyView;
-import com.google.gerrit.reviewdb.client.Branch;
-import com.google.gerrit.server.IdentifiedUser;
-import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
-import com.google.gerrit.server.git.GitRepositoryManager;
-import com.google.gerrit.server.query.change.InternalChangeQuery;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
-import com.google.inject.Provider;
 import com.google.inject.Singleton;
-
-import org.eclipse.jgit.lib.BatchRefUpdate;
-import org.eclipse.jgit.lib.NullProgressMonitor;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.RefUpdate;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.transport.ReceiveCommand;
-import org.eclipse.jgit.transport.ReceiveCommand.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @Singleton
 public class DeleteBranches
     implements RestModifyView<ProjectResource, DeleteBranchesInput> {
-  private static final Logger log = LoggerFactory.getLogger(DeleteBranches.class);
-
-  private final Provider<IdentifiedUser> identifiedUser;
-  private final GitRepositoryManager repoManager;
-  private final Provider<InternalChangeQuery> queryProvider;
-  private final GitReferenceUpdated referenceUpdated;
-  private final RefValidationHelper refDeletionValidator;
+  private final DeleteRef.Factory deleteRefFactory;
 
   @Inject
-  DeleteBranches(Provider<IdentifiedUser> identifiedUser,
-      GitRepositoryManager repoManager,
-      Provider<InternalChangeQuery> queryProvider,
-      GitReferenceUpdated referenceUpdated,
-      RefValidationHelper.Factory refHelperFactory) {
-    this.identifiedUser = identifiedUser;
-    this.repoManager = repoManager;
-    this.queryProvider = queryProvider;
-    this.referenceUpdated = referenceUpdated;
-    this.refDeletionValidator =
-        refHelperFactory.create(ReceiveCommand.Type.DELETE);
+  DeleteBranches(DeleteRef.Factory deleteRefFactory) {
+    this.deleteRefFactory = deleteRefFactory;
   }
 
   @Override
@@ -79,87 +43,7 @@ public class DeleteBranches
       throw new BadRequestException("branches must be specified");
     }
 
-    try (Repository r = repoManager.openRepository(project.getNameKey())) {
-      BatchRefUpdate batchUpdate = r.getRefDatabase().newBatchUpdate();
-      for (String branch : input.branches) {
-        batchUpdate.addCommand(createDeleteCommand(project, r, branch));
-      }
-      try (RevWalk rw = new RevWalk(r)) {
-        batchUpdate.execute(rw, NullProgressMonitor.INSTANCE);
-      }
-      StringBuilder errorMessages = new StringBuilder();
-      for (ReceiveCommand command : batchUpdate.getCommands()) {
-        if (command.getResult() == Result.OK) {
-          postDeletion(project, command);
-        } else {
-          appendAndLogErrorMessage(errorMessages, command);
-        }
-      }
-      if (errorMessages.length() > 0) {
-        throw new ResourceConflictException(errorMessages.toString());
-      }
-    }
+    deleteRefFactory.create(project).refs(input.branches).delete();
     return Response.none();
-  }
-
-  private ReceiveCommand createDeleteCommand(ProjectResource project,
-      Repository r, String branch)
-          throws OrmException, IOException, ResourceConflictException {
-    Ref ref = r.getRefDatabase().getRef(branch);
-    ReceiveCommand command;
-    if (ref == null) {
-      command = new ReceiveCommand(ObjectId.zeroId(), ObjectId.zeroId(), branch);
-      command.setResult(Result.REJECTED_OTHER_REASON,
-          "it doesn't exist or you do not have permission to delete it");
-      return command;
-    }
-    command =
-        new ReceiveCommand(ref.getObjectId(), ObjectId.zeroId(), ref.getName());
-    Branch.NameKey branchKey =
-        new Branch.NameKey(project.getNameKey(), ref.getName());
-    if (!project.getControl().controlForRef(branchKey).canDelete()) {
-      command.setResult(Result.REJECTED_OTHER_REASON,
-          "it doesn't exist or you do not have permission to delete it");
-    }
-    if (!queryProvider.get().setLimit(1).byBranchOpen(branchKey).isEmpty()) {
-      command.setResult(Result.REJECTED_OTHER_REASON, "it has open changes");
-    }
-    RefUpdate u = r.updateRef(branch);
-    u.setForceUpdate(true);
-    refDeletionValidator.validateRefOperation(
-        project.getName(), identifiedUser.get(), u);
-    return command;
-  }
-
-  private void appendAndLogErrorMessage(StringBuilder errorMessages,
-      ReceiveCommand cmd) {
-    String msg = null;
-    switch (cmd.getResult()) {
-      case REJECTED_CURRENT_BRANCH:
-        msg = format("Cannot delete %s: it is the current branch",
-            cmd.getRefName());
-        break;
-      case REJECTED_OTHER_REASON:
-        msg = format("Cannot delete %s: %s", cmd.getRefName(), cmd.getMessage());
-        break;
-      case LOCK_FAILURE:
-      case NOT_ATTEMPTED:
-      case OK:
-      case REJECTED_MISSING_OBJECT:
-      case REJECTED_NOCREATE:
-      case REJECTED_NODELETE:
-      case REJECTED_NONFASTFORWARD:
-      default:
-        msg = format("Cannot delete %s: %s", cmd.getRefName(), cmd.getResult());
-        break;
-    }
-    log.error(msg);
-    errorMessages.append(msg);
-    errorMessages.append("\n");
-  }
-
-  private void postDeletion(ProjectResource project, ReceiveCommand cmd) {
-    referenceUpdated.fire(project.getNameKey(), cmd,
-        identifiedUser.get().getAccount());
   }
 }

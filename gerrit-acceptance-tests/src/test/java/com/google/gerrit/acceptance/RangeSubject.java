bcd275eb7f3ca31b6e2e81cc4c69392a0ffcfe83
Alice Kober-Sotzek <aliceks@google.com>
2016-12-05 16:22:07 +0100

    Add support for suggested fixes of robot comments
    With this change, suggestions for fixes can be provided with a robot
    comment. At the moment, those suggestions are implicitly limited to the
    file to which the robot comment was attached.
    
    Change-Id: I69cf3fc4bdc66d69a5831b1e6c78937b8e945a72

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/RangeSubject.java
@@ -0,0 +1,62 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance;
+
+import static com.google.common.truth.Truth.assertAbout;
+
+import com.google.common.truth.FailureStrategy;
+import com.google.common.truth.IntegerSubject;
+import com.google.common.truth.Subject;
+import com.google.common.truth.SubjectFactory;
+import com.google.common.truth.Truth;
+import com.google.gerrit.extensions.client.Comment;
+
+public class RangeSubject extends Subject<RangeSubject, Comment.Range> {
+
+  private static final SubjectFactory<RangeSubject, Comment.Range>
+      RANGE_SUBJECT_FACTORY =
+      new SubjectFactory<RangeSubject, Comment.Range>() {
+        @Override
+        public RangeSubject getSubject(FailureStrategy failureStrategy,
+            Comment.Range range) {
+          return new RangeSubject(failureStrategy, range);
+        }
+      };
+
+  public static RangeSubject assertThat(Comment.Range range) {
+    return assertAbout(RANGE_SUBJECT_FACTORY)
+        .that(range);
+  }
+
+  private RangeSubject(FailureStrategy failureStrategy, Comment.Range range) {
+    super(failureStrategy, range);
+  }
+
+  public IntegerSubject startLine() {
+    return Truth.assertThat(actual().startLine).named("startLine");
+  }
+
+  public IntegerSubject startCharacter() {
+    return Truth.assertThat(actual().startCharacter).named("startCharacter");
+  }
+
+  public IntegerSubject endLine() {
+    return Truth.assertThat(actual().endLine).named("endLine");
+  }
+
+  public IntegerSubject endCharacter() {
+    return Truth.assertThat(actual().endCharacter).named("endCharacter");
+  }
+}

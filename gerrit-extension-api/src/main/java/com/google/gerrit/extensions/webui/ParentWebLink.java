e38855a8c4592c34630c05ec77b4db23ece5c96b
David Pursehouse <dpursehouse@collab.net>
2016-12-09 11:12:27 +0900

    Add support for parent revisions weblinks provided by plugins
    Support for parent weblinks was removed in change I8508dcaf3. This is a
    regression for some users who rely on that information being present.
    
    On the other hand, not all users want to have these links going to the
    same external service as the patch set revision links, so we can't just
    add it back as it was.
    
    Instead, add a new plugin web link interface for the parent revisions.
    
    If a plugin provides an implementation of ParentWebLink, the links will
    be displayed beside the parent revisions on the change screen.
    
    For the built-in gitweb support, provide the parent links using the same
    template as the patch set links, so they will always appear when the
    gitweb config defines the 'revision' setting.
    
    Bug: Issue 4908
    Change-Id: Icdf0f997f939128717ee4edc2c147a26eeaebad6
    (cherry picked from commit 58b8d76204ec59fa2550f450921a34bac2f0cea6)

---
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/webui/ParentWebLink.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.extensions.webui;
+
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+import com.google.gerrit.extensions.common.WebLinkInfo;
+
+@ExtensionPoint
+public interface ParentWebLink extends WebLink {
+
+  /**
+   * {@link com.google.gerrit.extensions.common.WebLinkInfo}
+   * describing a link from a parent revision to an external service.
+   *
+   * <p>In order for the web link to be visible
+   * {@link com.google.gerrit.extensions.common.WebLinkInfo#url}
+   * and {@link com.google.gerrit.extensions.common.WebLinkInfo#name}
+   * must be set.<p>
+   *
+   * @param projectName Name of the project
+   * @param commit Commit sha1 of the parent revision
+   * @return WebLinkInfo that links to parent commit in external service,
+   * null if there should be no link.
+   */
+  WebLinkInfo getParentWebLink(String projectName, String commit);
+}

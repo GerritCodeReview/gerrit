61e253ca6ab1ae817c77ee8c08ed11d145a1f51a
Kasper Nilsson <kaspern@google.com>
2016-12-15 17:23:07 -0800

    Fix copy/paste in comments
    Previous comment logic relied on the assumption that any element in
    a comment with textContent would be childless, which is false. Now, the
    method is to fetch all comments within the selection and recursively
    extract their text content.
    
    Bug: Issue 5144
    Change-Id: I22eac0b9b147fe0f3360d87529661efadb15940a

--- polygerrit-ui/app/elements/diff/gr-diff-selection/gr-diff-selection.js
+++ polygerrit-ui/app/elements/diff/gr-diff-selection/gr-diff-selection.js
@@ -213,7 +213,7 @@
      * @return {string} The selected comment text.
      */
     _getCommentLines: function(sel, side) {
-      var range = sel.getRangeAt(0);
+      var range = GrRangeNormalizer.normalize(sel.getRangeAt(0));
       var content = [];
       // Query the diffElement for comments.
       var messages = this.diffBuilder.diffElement.querySelectorAll(
@@ -232,18 +232,45 @@
             }
           }
 
-          if (!el.children.length) {
-            content.push(el.textContent);
+          if (el.id === 'output' &&
+              !this._elementDescendedFromClass(el, 'collapsed')) {
+            content.push(this._getTextContentForRange(el, sel, range));
           }
         }
       }
 
-      if (range.endOffset) {
-        content[content.length - 1] =
-            content[content.length - 1].substring(0, range.endOffset);
-      }
-      content[0] = content[0].substring(range.startOffset);
       return content.join('\n');
+    },
+
+    /**
+     * Given a DOM node, a selection, and a selection range, recursively get all
+     * of the text content within that selection.
+     * Using a domNode that isn't in the selection returns an empty string.
+     *
+     * @param {Element} domNode The root DOM node.
+     * @param {Selection} sel The selection.
+     * @param {Range} range The normalized selection range.
+     * @return {string} The text within the selection.
+     */
+    _getTextContentForRange: function(domNode, sel, range) {
+      if (!sel.containsNode(domNode, true)) { return ''; }
+
+      var text = '';
+      if (domNode instanceof Text) {
+        text = domNode.textContent;
+        if (domNode === range.endContainer) {
+          text = text.substring(0, range.endOffset);
+        }
+        if (domNode === range.startContainer) {
+          text = text.substring(range.startOffset);
+        }
+      } else {
+        for (var i = 0; i < domNode.childNodes.length; i++) {
+          text += this._getTextContentForRange(domNode.childNodes[i],
+              sel, range);
+        }
+      }
+      return text;
     },
   });
 })();

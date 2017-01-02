06716e4c969a5595c7a541fde2dede27a3d2833a
Edwin Kempin <ekempin@google.com>
2017-01-02 13:13:09 +0100

    Add GroupQueryProcessor
    Change-Id: I051a2e06cc6d5f86c0e6472e96d9c4e242746835
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/index/IndexModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/index/IndexModule.java
@@ -38,6 +38,7 @@ import com.google.gerrit.server.index.change.ChangeIndexer;
 import com.google.gerrit.server.index.change.ChangeSchemaDefinitions;
 import com.google.gerrit.server.index.group.GroupIndexCollection;
 import com.google.gerrit.server.index.group.GroupIndexDefinition;
+import com.google.gerrit.server.index.group.GroupIndexRewriter;
 import com.google.gerrit.server.index.group.GroupSchemaDefinitions;
 import com.google.inject.Injector;
 import com.google.inject.Key;
@@ -103,6 +104,7 @@ public class IndexModule extends LifecycleModule {
     listener().to(ChangeIndexCollection.class);
     factory(ChangeIndexer.Factory.class);
 
+    bind(GroupIndexRewriter.class);
     bind(GroupIndexCollection.class);
     listener().to(GroupIndexCollection.class);
   }

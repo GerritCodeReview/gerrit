673c0b7ee9fb7c019ca36e4b2dbd2f38a524fdca
Jacek Centkowski <geminica.programs@gmail.com>
2017-01-02 18:00:39 +0100

    Add support for plugin global aliases
    The following patch introduces global SSH aliases (they neither require
    gerrit nor pluginName root command) to the plugins. It is performed
    automatically by calling (see corresponding change in cookbook plugin)
      globalAlias("say-hi", PrintHelloWorldCommand.class);
    
    cammand call:
    ssh -p 29418 admin@localhost say-hi
    Hello world!
    
    This is necessary to implement LFS protocol auth over SSH. One needs to
    alias plugin command directly to gerrit address (described in [1]):
    ssh [{user}@]{server} git-lfs-authenticate {path} {operation}. This is
    different approach than [2] and doesn't require manual gerrit.config
    changes (which would be vital in certain cases).
    
    [1] https://groups.google.com/forum/#!topic/repo-discuss/_pngw9G6JNs
    [2] https://gerrit-review.googlesource.com/#/c/93373/
    Change-Id: I3a40211ccb869f81dd2e1a8adca86661c72ba2f2
    Signed-off-by: Jacek Centkowski <geminica.programs@gmail.com>

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/SshPluginStarterCallback.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/SshPluginStarterCallback.java
@@ -14,17 +14,25 @@
 
 package com.google.gerrit.sshd;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
 import com.google.gerrit.server.plugins.Plugin;
 import com.google.gerrit.server.plugins.ReloadPluginListener;
 import com.google.gerrit.server.plugins.StartPluginListener;
+import com.google.inject.Binding;
 import com.google.inject.Inject;
 import com.google.inject.Key;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+import com.google.inject.spi.ProviderInstanceBinding;
 
 import org.apache.sshd.server.Command;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
 
 @Singleton
 class SshPluginStarterCallback
@@ -45,6 +53,9 @@ class SshPluginStarterCallback
     Provider<Command> cmd = load(plugin);
     if (cmd != null) {
       plugin.add(root.register(Commands.named(plugin.getName()), cmd));
+      for (Map.Entry<CommandName, Provider<Command>> alias : aliases(plugin)) {
+        plugin.add(root.register(alias.getKey(), alias.getValue()));
+      }
     }
   }
 
@@ -53,7 +64,29 @@ class SshPluginStarterCallback
     Provider<Command> cmd = load(newPlugin);
     if (cmd != null) {
       newPlugin.add(root.replace(Commands.named(newPlugin.getName()), cmd));
+      for (Map.Entry<CommandName, Provider<Command>> alias : aliases(
+          newPlugin)) {
+        newPlugin.add(root.replace(alias.getKey(), alias.getValue()));
+      }
     }
+  }
+
+  private List<Map.Entry<CommandName, Provider<Command>>> aliases(
+      Plugin plugin) {
+    ImmutableList.Builder<Map.Entry<CommandName, Provider<Command>>> builder =
+        ImmutableList.builder();
+    List<Binding<Command>> aliases = plugin.getSshInjector()
+        .findBindingsByType(new TypeLiteral<Command>() {});
+    for (Binding<Command> alias : aliases) {
+      if (alias instanceof ProviderInstanceBinding
+          && ((ProviderInstanceBinding<?>) alias)
+              .getUserSuppliedProvider() instanceof AliasCommandProvider
+          && alias.getKey().getAnnotation() instanceof CommandName) {
+        builder.add(Maps.immutableEntry(
+            (CommandName) alias.getKey().getAnnotation(), alias.getProvider()));
+      }
+    }
+    return builder.build();
   }
 
   private Provider<Command> load(Plugin plugin) {

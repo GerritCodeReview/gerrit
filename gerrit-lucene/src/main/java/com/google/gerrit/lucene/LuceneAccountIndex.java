3d63baf9ff47cb3372811d4dcdb33ab56674a476
Edwin Kempin <ekempin@google.com>
2017-01-04 08:45:54 +0100

    LuceneAccountIndex: Fix comment
    Change-Id: Id902d6410d8a433d993e1211a1771d1da28baf8b
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-lucene/src/main/java/com/google/gerrit/lucene/LuceneAccountIndex.java
+++ gerrit-lucene/src/main/java/com/google/gerrit/lucene/LuceneAccountIndex.java
@@ -200,7 +200,7 @@ public class LuceneAccountIndex
     Account.Id id =
         new Account.Id(doc.getField(ID.getName()).numericValue().intValue());
     // Use the AccountCache rather than depending on any stored fields in the
-    // document (of which there shouldn't be any. The most expensive part to
+    // document (of which there shouldn't be any). The most expensive part to
     // compute anyway is the effective group IDs, and we don't have a good way
     // to reindex when those change.
     return accountCache.get(id);

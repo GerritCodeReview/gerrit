855f1518f79bc525fa79e8737313b5e78f292e3a
Luca Milanesio <luca.milanesio@gmail.com>
2017-01-05 17:03:20 +0000

    Check for successful termination of tasks through delete REST API
    The DeleteTask command needs to check the success of failure
    of the task termination: failing to do so and ignoring the results
    (as it was doing before) may lead to potential false positive in tests.
    
    Change-Id: Ia2081c30e7b518849efdce0c462192e34fb07d7b

--- gerrit-server/src/main/java/com/google/gerrit/server/config/DeleteTask.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/DeleteTask.java
@@ -16,11 +16,13 @@ package com.google.gerrit.server.config;
 
 import static com.google.gerrit.common.data.GlobalCapability.KILL_TASK;
 import static com.google.gerrit.common.data.GlobalCapability.MAINTAIN_SERVER;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 
 import com.google.gerrit.extensions.annotations.RequiresAnyCapability;
 import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.server.config.DeleteTask.Input;
+import com.google.gerrit.server.git.WorkQueue.Task;
 import com.google.inject.Singleton;
 
 @Singleton
@@ -31,7 +33,9 @@ public class DeleteTask implements RestModifyView<TaskResource, Input> {
 
   @Override
   public Response<?> apply(TaskResource rsrc, Input input) {
-    rsrc.getTask().cancel(true);
-    return Response.none();
+    Task<?> task = rsrc.getTask();
+    boolean taskDeleted = task.cancel(true);
+    return taskDeleted ? Response.none() : Response.withStatusCode(
+        SC_INTERNAL_SERVER_ERROR, "Unable to kill task " + task);
   }
 }

e60dffb1e86d9b9c63c814395e4769222a645d5c
Edwin Kempin <ekempin@google.com>
2017-01-05 09:36:37 +0100

    Index groups that are created during init
    During the initialization of a new site some default groups are
    automatically created ('Administrators' and 'Non-Interactive Users').
    These new groups must be added to the index so that they become
    queryable. For this init must have the group index available so that
    it can write the new groups to it. Reading from the index during init
    is not needed and not supported.
    
    Alternatively we could require users to run the Reindex program after
    the initial site initialization, but likely many people would forget
    about it and then wonder why the default groups are not available from
    the group index.
    
    Change-Id: I274b142a6efde224bba68f8b5c459736d7a1f985
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/index/IndexModuleOnInit.java
@@ -0,0 +1,85 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm.init.index;
+
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.server.account.GroupCache;
+import com.google.gerrit.server.index.IndexDefinition;
+import com.google.gerrit.server.index.SchemaDefinitions;
+import com.google.gerrit.server.index.SingleVersionModule;
+import com.google.gerrit.server.index.SingleVersionModule.SingleVersionListener;
+import com.google.gerrit.server.index.group.AllGroupsIndexer;
+import com.google.gerrit.server.index.group.GroupIndexCollection;
+import com.google.gerrit.server.index.group.GroupIndexDefinition;
+import com.google.gerrit.server.index.group.GroupSchemaDefinitions;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.ProvisionException;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Names;
+import com.google.inject.util.Providers;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class IndexModuleOnInit extends AbstractModule {
+  static final String INDEX_MANAGER = "IndexModuleOnInit/IndexManager";
+
+  @Override
+  protected void configure() {
+    // The GroupIndex implementations (LuceneGroupIndex and ElasticGroupIndex)
+    // need GroupCache only for reading from the index. On init we only want to
+    // write to the index, hence we don't need the group cache.
+    bind(GroupCache.class).toProvider(Providers.of(null));
+
+    // GroupIndexDefinition wants to have AllGroupsIndexer but it is only used
+    // by the Reindex program and the OnlineReindexer which are both not used
+    // during init, hence we don't need AllGroupsIndexer.
+    bind(AllGroupsIndexer.class).toProvider(Providers.of(null));
+
+    bind(GroupIndexCollection.class);
+
+    bind(new TypeLiteral<Map<String, Integer>>() {})
+        .annotatedWith(Names.named(SingleVersionModule.SINGLE_VERSIONS))
+        .toInstance(ImmutableMap.<String, Integer> of());
+    bind(LifecycleListener.class).annotatedWith(Names.named(INDEX_MANAGER))
+        .to(SingleVersionListener.class);
+  }
+
+  @Provides
+  Collection<IndexDefinition<?, ?, ?>> getIndexDefinitions(
+      GroupIndexDefinition groups) {
+    Collection<IndexDefinition<?, ?, ?>> result =
+        ImmutableList.<IndexDefinition<?, ?, ?>> of(
+            groups);
+    Set<String> expected =
+        FluentIterable.of(GroupSchemaDefinitions.INSTANCE)
+        .transform(SchemaDefinitions::getName)
+        .toSet();
+    Set<String> actual = FluentIterable.from(result)
+        .transform(IndexDefinition::getName)
+        .toSet();
+    if (!expected.equals(actual)) {
+      throw new ProvisionException(
+          "need index definitions for all schemas: "
+          + expected + " != " + actual);
+    }
+    return result;
+  }
+}

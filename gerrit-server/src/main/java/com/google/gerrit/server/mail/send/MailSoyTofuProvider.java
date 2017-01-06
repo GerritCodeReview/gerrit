04bc20aa5af39c63d516e2ece1dcc861cbefd808
David Ostrovsky <david@ostrovsky.org>
2017-01-06 17:15:02 +0100

    MailSoyTofuProvider: Use soy ast cache and parse 5x faster
    Parsing current 34 soy templates takes ca. 300 ms on my laptop. With
    activated soy ast cache, parsing starting from the second time is
    only 60 ms, 5 x faster.
    
    This subtantially improves unit test perfomance (one test example):
    Before:
    
    $ buck test //gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change:rest_change_submit
    [-] TESTING...FINISHED 2m 14.9s (290 PASS/26 SKIP/0 FAIL)
    
    After:
    
    $ buck test //gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change:rest_change_submit
    [-] TESTING...FINISHED 1m 33.6s (290 PASS/26 SKIP/0 FAIL)
    
    All tests:
    
    Before:
    $ buck test
    [-] TESTING...FINISHED 8m 13.0s (2509 PASS/99 SKIP/0 FAIL)
    
    After:
    $ buck test
    [-] TESTING...FINISHED 6m 33.6s (2509 PASS/99 SKIP/0 FAIL)
    
    This also wouldn't hurt in production, as the parsing takes place on
    each and every send mail invocation to support template adjustments in
    live server without reboot
    
    Change-Id: Icce1fbd741c1ed56f5b2108848b399b2c6c88867

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/send/MailSoyTofuProvider.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/send/MailSoyTofuProvider.java
@@ -22,6 +22,7 @@ import com.google.inject.Provider;
 import com.google.inject.ProvisionException;
 import com.google.inject.Singleton;
 import com.google.template.soy.SoyFileSet;
+import com.google.template.soy.shared.SoyAstCache;
 import com.google.template.soy.tofu.SoyTofu;
 
 import java.io.IOException;
@@ -71,15 +72,18 @@ public class MailSoyTofuProvider implements Provider<SoyTofu> {
   };
 
   private final SitePaths site;
+  private final SoyAstCache cache;
 
   @Inject
   MailSoyTofuProvider(SitePaths site) {
     this.site = site;
+    cache = new SoyAstCache();
   }
 
   @Override
   public SoyTofu get() throws ProvisionException {
     SoyFileSet.Builder builder = SoyFileSet.builder();
+    builder.setSoyAstCache(cache);
     for (String name : TEMPLATES) {
       addTemplate(builder, name);
     }

90daf6cb7d1abfc9e63ec307f0a82dec48df929f
Alice Kober-Sotzek <aliceks@google.com>
2017-01-10 13:19:18 +0100

    Refactor and extract tree modification code
    In order to separate concerns and allow reuse of code, the code which
    modifies a Git tree is extracted to separate classes.
    
    As the resulting code should be as independent and flexible as possible
    and not require too much knowledge of the internals (e.g. whether a tree
    is directly visible or requires a flush of the ObjectInserter), creating
    RevWalk, TreeWalk, ObjectReader, and ObjectInserter instances is done
    directly where it is necessary instead of passing those objects over
    multiple methods. In addition, properly closing them at the right time
    can easily be ensured. The only exception to this approach is the
    Repository instance which is passed along in order to not lose too much
    performance due to reopening the repository again and again.
    
    This refactoring will facilitate the introduction of the 'Apply fix'
    feature of robot comments.
    
    Change-Id: Ib54bc4c30eaf5690a9c7bc0a5563877afe82021c

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/edit/tree/RestoreFileModification.java
@@ -0,0 +1,67 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.edit.tree;
+
+import org.eclipse.jgit.dircache.DirCacheEditor;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A {@code TreeModification} which restores a file. The file is added again if
+ * it was present before the specified commit or deleted if it was absent.
+ */
+public class RestoreFileModification implements TreeModification {
+
+  private final RevCommit currentCommit;
+  private final String filePath;
+
+  public RestoreFileModification(RevCommit currentCommit, String filePath) {
+    this.currentCommit = currentCommit;
+    this.filePath = filePath;
+  }
+
+  @Override
+  public List<DirCacheEditor.PathEdit> getPathEdits(Repository repository)
+      throws IOException {
+    if (currentCommit.getParentCount() == 0) {
+      DirCacheEditor.DeletePath deletePath =
+          new DirCacheEditor.DeletePath(filePath);
+      return Collections.singletonList(deletePath);
+    }
+
+    RevCommit base = currentCommit.getParent(0);
+    try (RevWalk revWalk = new RevWalk(repository)) {
+      revWalk.parseHeaders(base);
+      try (TreeWalk treeWalk = TreeWalk.forPath(revWalk.getObjectReader(),
+          filePath, base.getTree())) {
+        if (treeWalk == null) {
+          DirCacheEditor.DeletePath deletePath =
+              new DirCacheEditor.DeletePath(filePath);
+          return Collections.singletonList(deletePath);
+        }
+
+        AddPath addPath = new AddPath(filePath, treeWalk.getFileMode(0),
+            treeWalk.getObjectId(0));
+        return Collections.singletonList(addPath);
+      }
+    }
+  }
+}

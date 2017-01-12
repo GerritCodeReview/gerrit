50f650fbc863df7c44cf5d73f1befd2d66754494
Becky Siegel <beckysiegel@google.com>
2017-01-11 18:07:29 -0800

    Conditionally add change-id under commit message
    Previously, there was an issue where if Change Ids are not required in
    the commit message, There is no way to know what a change Id for a given
    change is. Similarly, there are cases where an incorrect Change Id is
    incorrect in the Commit message for one reason or another.
    
    This change addresses the problems by doing the following:
    
    - Find any Change Ids present in the commit message (via regex match)
    - Take the last match, compare it to the change's change id
      - If they are the same, don't display Change Id separately
      - If they are different display Change Id and show it with a warning/
        red color
      - If there were no matches, display Change Id in a nonobtrusive grey
        color
    
    Feature: Issue 5159
    Change-Id: I0638267fa9cf142c19113bd048719493f603c849

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -16,7 +16,7 @@
 
   // Maximum length for patch set descriptions.
   var PATCH_DESC_MAX_LENGTH = 500;
-
+  var CHANGE_ID_REGEX_PATTERN = /(I[0-9a-f]{8,40})/g;
   Polymer({
     is: 'gr-change-view',
 
@@ -64,6 +64,14 @@
       _commitInfo: Object,
       _files: Object,
       _changeNum: String,
+      _changeIdClass: {
+        type: String,
+        computed: '_computeChangeIdClass(_changeIdCommitMessageError)',
+      },
+      _titleAttributeWarning: {
+        type: String,
+        computed: '_computeTitleAttributeWarning(_changeIdCommitMessageError)',
+      },
       _diffDrafts: {
         type: Object,
         value: function() { return {}; },
@@ -80,6 +88,11 @@
       _latestCommitMessage: {
         type: String,
         value: '',
+      },
+      _changeIdCommitMessageError: {
+        type: String,
+        computed:
+          '_computeChangeIdCommitMessageError(_latestCommitMessage, _change)',
       },
       _patchRange: {
         type: Object,
@@ -543,6 +556,38 @@
         statusString = this.changeStatusString(change);
       }
       return statusString ? ' (' + statusString + ')' : '';
+    },
+
+    _computeChangeIdClass: function(displayChangeId) {
+      return displayChangeId === 'mismatch' ? 'warning' : '';
+    },
+
+    _computeTitleAttributeWarning: function(displayChangeId) {
+      switch (displayChangeId) {
+        case 'mismatch':
+            return 'Change-Id mismatch';
+        case 'missing':
+            return 'No Change-Id in commit message';
+        default:
+            return '';
+      }
+    },
+
+    _computeChangeIdCommitMessageError: function(commitMessage, change) {
+      commitMessage = commitMessage || '';
+      var changeIds = commitMessage.match(CHANGE_ID_REGEX_PATTERN);
+
+      if (changeIds) {
+        // A change-id is detected in the commit message.
+        if (changeIds[changeIds.length - 1] === change.change_id) {
+          // The change-id found matches the real change-id.
+          return;
+        }
+        // The change-id found does not match the change-id.
+        return 'mismatch';
+      }
+      // There is no change-id in the commit message.
+      return 'missing';
     },
 
     _computeLatestPatchNum: function(allPatchSets) {

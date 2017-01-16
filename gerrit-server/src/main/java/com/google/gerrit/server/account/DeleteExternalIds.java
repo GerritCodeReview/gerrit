9eb917427268997890c02e0ab59d28330b9efd78
Changcheng Xiao <xchangcheng@google.com>
2017-01-16 11:38:03 +0100

    Account REST API: Add a new endpoint to delete external IDs
    The REST endpoint is added as:
    'POST /accounts/{account-id}/external.ids:delete'
    The target external ids must be provided in the request body.
    
    Change-Id: Ieef322d6bddc1f3659536081b3f3514474b7162a

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/DeleteExternalIds.java
@@ -0,0 +1,117 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import static com.google.gerrit.reviewdb.client.AccountExternalId.SCHEME_USERNAME;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountExternalId;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DeleteExternalIds implements
+    RestModifyView<AccountResource, List<String>> {
+  private final AccountByEmailCache accountByEmailCache;
+  private final AccountCache accountCache;
+  private final ExternalIdCache externalIdCache;
+  private final Provider<CurrentUser> self;
+  private final Provider<ReviewDb> dbProvider;
+
+  @Inject
+  DeleteExternalIds(
+      AccountByEmailCache accountByEmailCache,
+      AccountCache accountCache,
+      ExternalIdCache externalIdCache,
+      Provider<CurrentUser> self,
+      Provider<ReviewDb> dbProvider) {
+    this.accountByEmailCache = accountByEmailCache;
+    this.accountCache = accountCache;
+    this.externalIdCache = externalIdCache;
+    this.self = self;
+    this.dbProvider = dbProvider;
+  }
+
+  @Override
+  public Response<?> apply(AccountResource resource, List<String> externalIds)
+      throws RestApiException, IOException, OrmException {
+    if (self.get() != resource.getUser()) {
+      throw new AuthException("not allowed to delete external IDs");
+    }
+
+    if (externalIds == null || externalIds.size() == 0) {
+      throw new BadRequestException("external IDs are required");
+    }
+
+    Account.Id accountId = resource.getUser().getAccountId();
+    Map<AccountExternalId.Key, AccountExternalId> externalIdMap =
+        getAccountExternalIds(resource.getUser());
+
+    List<AccountExternalId> toDelete = new ArrayList<>();
+    for (String externalIdStr : externalIds) {
+      AccountExternalId id = externalIdMap.get(
+          new AccountExternalId.Key(externalIdStr));
+      if (id != null && id.canDelete()) {
+        toDelete.add(id);
+      }
+    }
+
+    if (!toDelete.isEmpty()) {
+      dbProvider.get().accountExternalIds().delete(toDelete);
+      externalIdCache.onRemove(toDelete);
+      accountCache.evict(accountId);
+      for (AccountExternalId e : toDelete) {
+        accountByEmailCache.evict(e.getEmailAddress());
+      }
+    }
+
+    return Response.none();
+  }
+
+  private Map<AccountExternalId.Key, AccountExternalId> getAccountExternalIds(
+      IdentifiedUser user) throws OrmException {
+    Map<AccountExternalId.Key, AccountExternalId> idMap = new HashMap<>();
+    Collection<AccountExternalId> idList =
+        externalIdCache.byAccount(user.getAccountId());
+
+    for (AccountExternalId id : idList) {
+      if (id.isScheme(SCHEME_USERNAME)) {
+        id.setCanDelete(false);
+      } else {
+        AccountExternalId.Key last = user.getLastLoginExternalIdKey();
+        id.setCanDelete(
+            (last == null) || (!last.get().equals(id.getExternalId())));
+      }
+
+      idMap.put(id.getKey(), id);
+    }
+    return idMap;
+  }
+}

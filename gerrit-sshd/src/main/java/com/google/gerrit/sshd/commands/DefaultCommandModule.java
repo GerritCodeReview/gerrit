31235233d0b140e01129b767b56b78eb315a0290
Jacek Centkowski <gemincia.programs@gmail.com>
2017-01-16 11:11:30 +0100

    Install "git-lfs-authenticate" SSH command when lfs.plugin is configured
    SSH command registration was extended so that command gets registered
    only in case when plugin is configured in gerrit.config.
    
    Change-Id: Iffaf00775bdf1242e1fbe1dea15e5ce0bf912079
    Signed-off-by: Jacek Centkowski <geminica.programs@gmail.com>

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/DefaultCommandModule.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/DefaultCommandModule.java
@@ -27,10 +27,13 @@ import com.google.gerrit.sshd.plugin.LfsPluginAuthCommand;
 /** Register the commands a Gerrit server supports. */
 public class DefaultCommandModule extends CommandModule {
   private final DownloadConfig downloadConfig;
+  private final LfsPluginAuthCommand.Module lfsPluginAuthModule;
 
-  public DefaultCommandModule(boolean slave, DownloadConfig downloadCfg) {
+  public DefaultCommandModule(boolean slave, DownloadConfig downloadCfg,
+      LfsPluginAuthCommand.Module module) {
     slaveMode = slave;
     downloadConfig = downloadCfg;
+    lfsPluginAuthModule = module;
   }
 
   @Override
@@ -124,7 +127,7 @@ public class DefaultCommandModule extends CommandModule {
     alias(logging, "ls", ListLoggingLevelCommand.class);
     alias(logging, "set", SetLoggingLevelCommand.class);
 
-    install(new LfsPluginAuthCommand.Module());
+    install(lfsPluginAuthModule);
   }
 
   private boolean sshEnabled() {

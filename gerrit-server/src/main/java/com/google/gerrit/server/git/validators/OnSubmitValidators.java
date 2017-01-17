7edb5ae3ab8d0b5405e2c70ffb99f8f9520baf12
Andrii Shyshkalov <tandrii@google.com>
2017-01-17 03:46:32 -0800

    New extension to validate branch updates by submit strategies.
    Adds new extension point to validate a branch tip update when a change is being
    submitted by submit strategy. As submit strategies may generate new commits
    (e.g. Cherry Pick), this listener allows validation of resulting new commit
    before branch is updated, potentially aborting the submission.
    
    Before, only MergeValidationListener.onPreMerge was available to
    validate changes being submitted. However, onPreMerge was called
    before submit strategies were run, thus commits generated by submit
    strategies were previously impossible to validate.
    
    Now, OnSubmitValidationListener.preBranchUpdate can be used
    to abort submission before any refs are updated, even if there are
    several changes submitted together.
    
    Change-Id: I2ab75c7c38eb8ed0c010507b49f40e1b5cebad3b

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/validators/OnSubmitValidators.java
@@ -0,0 +1,44 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.server.git.validators;
+
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.git.IntegrationException;
+import com.google.gerrit.server.git.validators.OnSubmitValidationListener.Arguments;
+import com.google.gerrit.server.validators.ValidationException;
+import com.google.inject.Inject;
+
+public class OnSubmitValidators {
+  public interface Factory {
+    OnSubmitValidators create();
+  }
+
+  private final DynamicSet<OnSubmitValidationListener> listeners;
+
+  @Inject
+  OnSubmitValidators(DynamicSet<OnSubmitValidationListener> listeners) {
+    this.listeners = listeners;
+  }
+
+  public void validateBranchUpdate(Arguments args)
+      throws IntegrationException {
+    try {
+      for (OnSubmitValidationListener listener : listeners) {
+        listener.preBranchUpdate(args);
+      }
+    } catch (ValidationException e) {
+      throw new IntegrationException(e.getMessage());
+    }
+  }
+}

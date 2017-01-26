77fc2ce840a291eb32f3b1304b021b3af63e0653
Becky Siegel <beckysiegel@google.com>
2017-01-26 14:16:10 -0800

    Move reply buttons to comment thread
    Move all buttons that generate a reply of some sort (done, ack, reply,
    quote) to the comment thread instead of the comment [1].
    
    When there is a draft for a particular comment thread, all reply buttons
    are hidden [2].  For example, if you click reply, you cannot click done
    on the same thread, unless you remove the draft.
    
    Each thread can have up to 1 draft. It's also worth noting that if a
    thread has a draft, and the user clicks on the line or 'c' at the same
    range, the existing draft will switch to 'editing' form.
    
    [1] With the exception of "please fix" for robot comments.
    [2] In this case, The please fix button will be disabled when other
    reply buttons are hidden.
    
    Feature: Issue 5410
    Change-Id: Id847ee0cba0d0ce4e5b6476f58141866d41ffdad

--- polygerrit-ui/app/elements/diff/gr-diff-comment-thread/gr-diff-comment-thread.js
+++ polygerrit-ui/app/elements/diff/gr-diff-comment-thread/gr-diff-comment-thread.js
@@ -46,6 +46,7 @@
       },
 
       _showActions: Boolean,
+      _lastComment: Object,
       _orderedComments: Array,
       _unresolved: {
         type: Boolean,
@@ -76,14 +77,14 @@
       this._setInitialExpandedState();
     },
 
-    addOrEditDraft: function(opt_lineNum) {
+    addOrEditDraft: function(opt_lineNum, opt_range) {
       var lastComment = this.comments[this.comments.length - 1];
       if (lastComment && lastComment.__draft) {
         var commentEl = this._commentElWithDraftID(
             lastComment.id || lastComment.__draftID);
         commentEl.editing = true;
       } else {
-        this.addDraft(opt_lineNum);
+        this.addDraft(opt_lineNum, opt_range);
       }
     },
 
@@ -100,7 +101,14 @@
 
     _commentsChanged: function(changeRecord) {
       this._orderedComments = this._sortedComments(this.comments);
+      this._lastComment =
+          this._orderedComments[this._orderedComments.length - 1];
+
       this._unresolved = this._getLastComment().unresolved;
+    },
+
+    _hideActions: function(_showActions, _lastComment) {
+      return !_showActions || !!_lastComment.__draft;
     },
 
     _getLastComment: function() {
@@ -186,9 +194,9 @@
     },
 
     _handleCommentReply: function(e) {
-      var comment = e.detail.comment;
+      var comment = this._lastComment;
       var quoteStr;
-      if (e.detail.quote) {
+      if (e.target.classList.contains('quote')) {
         var msg = comment.message;
         quoteStr = '> ' + msg.replace(NEWLINE_PATTERN, '\n> ') + '\n\n';
       }
@@ -196,12 +204,12 @@
     },
 
     _handleCommentAck: function(e) {
-      var comment = e.detail.comment;
+      var comment = this._lastComment;
       this._createReplyComment(comment, 'Ack', false, comment.unresolved);
     },
 
     _handleCommentDone: function(e) {
-      var comment = e.detail.comment;
+      var comment = this._lastComment;
       this._createReplyComment(comment, 'Done', false, false);
     },
 

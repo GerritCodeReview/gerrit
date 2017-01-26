610bb6ca3231e038acb2ee11595079ac917f38d0
Paladox none <thomasmulhall410@yahoo.com>
2017-01-26 21:01:15 +0000

    Add support for mariadb as a db in gerrit
    This fixes support for allowing users to set character encoding and
    collation such as utf8mb4 and utf8mb4_unicode_ci which currently with
    mysql's you have to set the db to do it. But in an enterprise world you
    can not do that or it is hard to do that if you share other db's on the
    same host that use different character encoding and collation.
    
    Please see our downstream report https://phabricator.wikimedia.org/T145885
    
    We need full unicode to support emoji's as utf8 is not full unicode.
    
    + i found this https://mathiasbynens.be/notes/mysql-utf8mb4 which
    says that this could be leaking data and be a security problem with utf8.
    
    So i have no idea what security problems it holds for gerrit.
    
    This plugin is also updated more regulary then mysql so more bug fixes
    + features.
    
    See https://github.com/MariaDB/mariadb-connector-j/
    
    https://downloads.mariadb.com/Connectors/java/connector-java-1.5.7/sha1sums.txt
    
    Change-Id: I72496f93eb15e24093c8b9d9cda0b225342faa13

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/MariaDB.java
@@ -0,0 +1,56 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.schema;
+
+import static com.google.gerrit.server.schema.JdbcUtil.hostname;
+import static com.google.gerrit.server.schema.JdbcUtil.port;
+
+import com.google.gerrit.server.config.ConfigSection;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.inject.Inject;
+
+import org.eclipse.jgit.lib.Config;
+
+class MariaDB extends BaseDataSourceType {
+
+  private Config cfg;
+
+  @Inject
+  MariaDB(@GerritServerConfig final Config cfg) {
+    super("org.mariadb.jdbc.Driver");
+    this.cfg = cfg;
+  }
+
+  @Override
+  public String getUrl() {
+    final StringBuilder b = new StringBuilder();
+    final ConfigSection dbs = new ConfigSection(cfg, "database");
+    b.append("jdbc:mariadb://");
+    b.append(hostname(dbs.optional("hostname")));
+    b.append(port(dbs.optional("port")));
+    b.append("/");
+    b.append(dbs.required("database"));
+    return b.toString();
+  }
+
+  @Override
+  public boolean usePool() {
+    // MariaDB has given us trouble with the connection pool,
+    // sometimes the backend disconnects and the pool winds
+    // up with a stale connection. Fortunately opening up
+    // a new MariaDB connection is usually very fast.
+    return false;
+  }
+}

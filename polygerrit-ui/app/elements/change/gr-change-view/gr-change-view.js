a7e4f0df05a548b0e448e3bb0a7391d1175fd159
Kasper Nilsson <kaspern@google.com>
2017-01-26 13:08:25 -0800

    Fix 'Merged as sadface'
    The commit field of CommitInfo is not always populated, but is relied
    upon by <gr-commit-info> for computing the shortened weblink.
    
    Bug: Issue 5337
    Change-Id: I94cfdb32702ba6140c4db14c7ee335a7dc1b073e

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -569,7 +569,10 @@
 
     _computeMergedCommitInfo: function(current_revision, revisions) {
       var rev = revisions[current_revision];
-      if (!rev) { return {}; }
+      if (!rev || !rev.commit) { return {}; }
+      // CommitInfo.commit is optional... Set commit in all cases to avoid error
+      // in <gr-commit-info>. @see Issue 5337
+      if (!rev.commit.commit) { rev.commit.commit = current_revision; }
       return rev.commit;
     },
 

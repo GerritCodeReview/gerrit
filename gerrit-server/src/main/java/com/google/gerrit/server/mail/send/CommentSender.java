666b8dd7db2aa5965fccdef052d3fc746b3b0dde
Wyatt Allen <wyatta@google.com>
2017-01-25 16:43:16 -0800

    Do not log exception details when catching file index exceptions
    CommentSender catches exceptions of various kinds when reading file
    contents to include in an email. Besides IOExceptions and file existence
    exceptions, it catches index out of bounds exceptions in cases when
    following an invalid patch number or an invalid line number. With this
    change, index exception details are not logged.
    
    Change-Id: I90893553fb6ad9159f52b729e99b354edbf0cc5c

--- gerrit-server/src/main/java/com/google/gerrit/server/mail/send/CommentSender.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/mail/send/CommentSender.java
@@ -629,7 +629,7 @@ public class CommentSender extends ReplyToChangeSender {
       // Default to the empty string if the given line number does not appear
       // in the file.
       log.warn(String.format("Failed to get line number of file on side %d",
-          side), err);
+          side));
       return "";
     } catch (NoSuchEntityException err) {
       // Default to the empty string if the side cannot be found.

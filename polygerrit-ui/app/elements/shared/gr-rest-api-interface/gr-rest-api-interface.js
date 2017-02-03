cf3a4578db7c3b907dffdecc0ce2c56599a60c8b
Becky Siegel <beckysiegel@google.com>
2017-02-03 15:56:05 -0800

    Fix range issue with comment replies
    - Adds range when parent exists when comment POSTed
    - Finds range via filtering comments for backfill
    
    Change-Id: Ied19cc4b33749ba81fad6d1a5030abab419b1e6f

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -728,6 +728,23 @@
           this._getDiffCommentsFetchURL(changeNum, endpoint, opt_patchNum);
       promises.push(this.fetchJSON(url).then(function(response) {
         comments = response[opt_path] || [];
+
+        // TODO(kaspern): Implement this on in the backend so this can be
+        // removed.
+
+        // Sort comments by date so that parent ranges can be propagated in a 
+        // single pass.
+        comments = comments.sort(function(a, b) {
+          return a.updated > b.updated;
+        });
+        comments.forEach(function(comment){
+          if (comment.in_reply_to && !comment.range) {
+            comment.range = comments.filter(function(c) {
+              return c.id === comment.in_reply_to;
+            })[0].range;
+          }
+        });
+
         if (opt_basePatchNum == PARENT_PATCH_NUM) {
           baseComments = comments.filter(onlyParent);
           baseComments.forEach(setPath);

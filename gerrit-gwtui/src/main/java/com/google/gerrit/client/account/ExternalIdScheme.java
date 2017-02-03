7d7672e6cea4e147787f8e006167cbfc0392def8
Changcheng Xiao <xchangcheng@google.com>
2017-02-03 08:52:26 +0100

    Adapt MyIdentitiesScreen from RPC to REST API
    This change adapts the MyIdentitiesScreen of GWT UI from RPC to REST
    API. The old RPC related code will be remove latter.
    
    Change-Id: I7238a411b7c91058c4f8ca529599dd9b8e128fd1

---
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/account/ExternalIdScheme.java
@@ -0,0 +1,83 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.client.account;
+
+import com.google.gerrit.client.auth.openid.OpenIdUtil;
+import com.google.gerrit.common.auth.openid.OpenIdUrls;
+import com.google.gerrit.extensions.client.AuthType;
+
+public final class ExternalIdScheme {
+  /**
+   * Scheme used for {@link AuthType#LDAP},
+   * {@link AuthType#CLIENT_SSL_CERT_LDAP},
+   * {@link AuthType#HTTP_LDAP}, and {@link AuthType#LDAP_BIND} usernames.
+   * <p>The name {@code gerrit:} was a very poor choice.
+   */
+  public static final String SCHEME_GERRIT = "gerrit:";
+
+  /** Scheme used to represent only an email address. */
+  public static final String SCHEME_MAILTO = "mailto:";
+
+  /**
+   * Scheme for the username used to authenticate an account,
+   * e.g. over SSH.
+   */
+  public static final String SCHEME_USERNAME = "username:";
+
+  public static boolean isUsername(final String identity) {
+    return isScheme(identity, SCHEME_USERNAME);
+  }
+
+  public static String describe(final String identity) {
+    if (isScheme(identity, SCHEME_GERRIT)) {
+      // A local user identity should just be itself.
+      return getSchemeRest(identity);
+    } else if (isScheme(identity, SCHEME_USERNAME)) {
+      // A local user identity should just be itself.
+      return getSchemeRest(identity);
+    } else if (isScheme(identity, SCHEME_MAILTO)) {
+      // Describe a mailto address as just its email address, which
+      // is already shown in the email address field.
+      return "";
+    } else if (isScheme(identity,
+        "https://www.google.com/accounts/o8/id")) {
+      return OpenIdUtil.C.nameGoogle();
+    } else if (isScheme(identity,
+        OpenIdUrls.URL_LAUNCHPAD)) {
+      return OpenIdUtil.C.nameLaunchpad();
+    } else if (isScheme(identity,
+        OpenIdUrls.URL_YAHOO)) {
+      return OpenIdUtil.C.nameYahoo();
+    }
+
+    return identity;
+  }
+
+  private static boolean isScheme(String id, final String scheme) {
+    return (id != null) && id.startsWith(scheme);
+  }
+
+  private static String getSchemeRest(String externalId) {
+    String scheme = getScheme(externalId);
+    return (scheme != null)
+        ? externalId.substring(scheme.length() + 1)
+        : null;
+  }
+
+  private static String getScheme(String externalId) {
+    int colonIdx = externalId.indexOf(':');
+    return (colonIdx > 0) ? externalId.substring(0, colonIdx) : null;
+  }
+}

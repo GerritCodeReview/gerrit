cb2d12770d5807f15e5528000a04c68344226831
Viktar Donich <viktard@google.com>
2017-02-06 10:47:53 -0800

    Handle triple click selection
    Double-clicking selects the word underneath it on OS X and Linux text
    editors. Triple-clicking selects the whole line.
    
    Selected line can be copied or commented on.
    
    Feature: Issue 5375
    Change-Id: I79fff57e7dbfde18ab741a3a67b61869fb52cecf

--- polygerrit-ui/app/elements/diff/gr-diff-highlight/gr-diff-highlight.js
+++ polygerrit-ui/app/elements/diff/gr-diff-highlight/gr-diff-highlight.js
@@ -94,13 +94,52 @@
       }
     },
 
-    _normalizeRange: function(range) {
-      range = GrRangeNormalizer.normalize(range);
-      return {
-        start: this._normalizeSelectionSide(range.startContainer,
-            range.startOffset),
-        end: this._normalizeSelectionSide(range.endContainer, range.endOffset),
-      };
+    _normalizeRange: function(domRange) {
+      var range = GrRangeNormalizer.normalize(domRange);
+      return this._fixTripleClickSelection({
+        start: this._normalizeSelectionSide(
+            range.startContainer, range.startOffset),
+        end: this._normalizeSelectionSide(
+            range.endContainer, range.endOffset),
+      }, domRange);
+    },
+
+    /**
+     * Adjust triple click selection for the whole line.
+     * domRange.endContainer may be one of the following:
+     * 1) 0 offset at right column's line number cell, or
+     * 2) 0 offset at left column's line number at the next line.
+     * Case 1 means left column was triple clicked.
+     * Case 2 means right column or unified view triple clicked.
+     * @param {!Object} range Normalized range, ie column/line numbers
+     * @param {!Range} domRange DOM Range object
+     * @return {!Object} fixed normalized range
+     */
+    _fixTripleClickSelection: function(range, domRange) {
+      var start = range.start;
+      var end = range.end;
+      var endsAtOtherSideLineNum =
+          domRange.endOffset === 0 &&
+          domRange.endContainer.nodeName === 'TD' &&
+          (domRange.endContainer.classList.contains('left') ||
+              domRange.endContainer.classList.contains('right'));
+      var endsOnOtherSideStart = endsAtOtherSideLineNum ||
+          end &&
+          end.column === 0 &&
+          end.line === start.line &&
+          end.side != start.side;
+      if (endsOnOtherSideStart || endsAtOtherSideLineNum) {
+        // Selection ends at the beginning of the next line.
+        // Move the selection to the end of the previous line.
+        range.end = {
+          node: start.node,
+          column: this._getLength(
+              domRange.cloneContents().querySelector('.contentText')),
+          side: start.side,
+          line: start.line,
+        };
+      }
+      return range;
     },
 
     /**

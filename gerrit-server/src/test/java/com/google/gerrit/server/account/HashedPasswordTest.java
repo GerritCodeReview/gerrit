45be4a1daa80161dd48b2f3bad2b08ac0ea46a69
Han-Wen Nienhuys <hanwen@google.com>
2017-02-07 16:31:02 +0100

    gerrit-server: use hashed passwords for HTTP.
    Removes the GET endpoint for the HTTP password.
    
    With the move to NoteDB, the per-account data (including the HTTP
    password) will be stored in a user-branch in the All-Users repo, where
    it is subject to Gerrit ACLs.  Since these are notoriously hard to
    setup correctly, we want to avoid storing the password in plaintext.
    
    With this change, we support hashed passwords, and a schema upgrade
    populates the new field using existing passwords.
    
    The old password checking/setting code (which has a timing
    side-channel) will be removed once this code has been rolled out
    without problems.
    
    Digest auth requires the server to know the plaintext password, so it
    is removed.
    
    Tested migration manually:
    
      * ran schema upgrade
      * verified that schema upgrade inserts hashed passwords with gsql.
      * verified that the password still works with the new code.
    
    Tested passwords manually:
      * verified that correct passwords get accepted when using curl --user.
      * verified that wrong passwords get rejected when using curl --user.
    
    Change-Id: I26f5bcd7848040107e3721eeabf75baeb79c1724

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/account/HashedPasswordTest.java
@@ -0,0 +1,69 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
+
+import com.google.common.base.Strings;
+import org.apache.commons.codec.DecoderException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class HashedPasswordTest {
+
+  @Test
+  public void encodeOneLine() throws Exception {
+    String password = "secret";
+    HashedPassword hashed = HashedPassword.fromPassword(password);
+    assertThat(hashed.encode()).doesNotContain("\n");
+    assertThat(hashed.encode()).doesNotContain("\r");
+  }
+
+  @Test
+  public void encodeDecode() throws Exception {
+    String password = "secret";
+    HashedPassword hashed = HashedPassword.fromPassword(password);
+    HashedPassword roundtrip = HashedPassword.decode(hashed.encode());
+    assertThat(hashed.encode()).isEqualTo(roundtrip.encode());
+    assertThat(roundtrip.checkPassword(password)).isTrue();
+    assertThat(roundtrip.checkPassword("not the password")).isFalse();
+  }
+
+  @Test(expected = DecoderException.class)
+  public void invalidDecode() throws Exception {
+    HashedPassword.decode("invalid");
+  }
+
+  @Test
+  public void lengthLimit() throws Exception {
+    String password = Strings.repeat("1", 72);
+
+    // make sure it fits in varchar(255).
+    assertThat(HashedPassword.fromPassword(password).encode().length()).isLessThan(255);
+  }
+
+  @Test
+  public void basicFunctionality() throws Exception {
+    String password = "secret";
+    HashedPassword hashed = HashedPassword.fromPassword(password);
+
+    assertThat(hashed.checkPassword("false")).isFalse();
+    assertThat(hashed.checkPassword(password)).isTrue();
+  }
+}

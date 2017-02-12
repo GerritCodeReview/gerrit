69dc2061fa0019108c3a9c5e1ac8a4d2581f2632
Paladox none <thomasmulhall410@yahoo.com>
2017-02-12 18:15:48 +0000

    Allow users given specific permission to delete their own changes
    This opens up deleting your own changes if the admin adds the user
    to the specific permision called Delete Own Changes. Admins can still
    delete changes without needing to be added to the permision.
    
    Change-Id: I0de15167c12bc004efb40f80df6e600d7a4d3e76

--- gerrit-common/src/main/java/com/google/gerrit/common/data/Permission.java
+++ gerrit-common/src/main/java/com/google/gerrit/common/data/Permission.java
@@ -28,6 +28,7 @@ public class Permission implements Comparable<Permission> {
   public static final String CREATE_TAG = "createTag";
   public static final String CREATE_SIGNED_TAG = "createSignedTag";
   public static final String DELETE_DRAFTS = "deleteDrafts";
+  public static final String DELETE_OWN_CHANGES = "deleteOwnChanges";
   public static final String EDIT_HASHTAGS = "editHashtags";
   public static final String EDIT_ASSIGNEE = "editAssignee";
   public static final String EDIT_TOPIC_NAME = "editTopicName";
@@ -77,6 +78,7 @@ public class Permission implements Comparable<Permission> {
     NAMES_LC.add(EDIT_HASHTAGS.toLowerCase());
     NAMES_LC.add(EDIT_ASSIGNEE.toLowerCase());
     NAMES_LC.add(DELETE_DRAFTS.toLowerCase());
+    NAMES_LC.add(DELETE_OWN_CHANGES.toLowerCase());
     NAMES_LC.add(PUBLISH_DRAFTS.toLowerCase());
 
     LABEL_INDEX = NAMES_LC.indexOf(Permission.LABEL);

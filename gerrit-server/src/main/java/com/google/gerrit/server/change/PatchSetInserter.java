6110c70267d9b225df8e19d23658e1b78a2e31e9
David Ostrovsky <david@ostrovsky.org>
2017-02-13 21:16:58 +0100

    Discontinue draft workflow
    Support for pushing refs/drafts/* and refs/publish/* magic branch is
    removed.
    
    Migration is implemented to either replace draft changes with private
    (default) or work-in-progress changes.
    
    We bump the change index version to V42 to trigger online reindexing.
    That's needed because we don't support reindexing during init step, and
    we need to reindex all changes, because DRAFT is removed from the
    Change.Status enum and index still contains the old references to the
    changes that were migrated to private or work-in-progress changes.
    
    PolyGerrit UI is not addressed in this change and will be done in a
    follow-up change.
    
    User banches are not updated in this change to clean-up My-Menu entry
    in user preferences. This should be done in follow-up change.
    
    refs/meta/config ist not updated in this change to clean-up draft
    related permissions. This should be done in follow-up change.
    
    Change-Id: Icfcb34efe1ff0ea1d39e94ed500db776f5770d8f

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PatchSetInserter.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PatchSetInserter.java
@@ -90,7 +90,6 @@ public class PatchSetInserter implements BatchUpdateOp {
   private String description;
   private boolean validate = true;
   private boolean checkAddPatchSetPermission = true;
-  private boolean draft;
   private List<String> groups = Collections.emptyList();
   private boolean fireRevisionCreated = true;
   private NotifyHandling notify = NotifyHandling.ALL;
@@ -153,11 +152,6 @@ public class PatchSetInserter implements BatchUpdateOp {
 
   public PatchSetInserter setCheckAddPatchSetPermission(boolean checkAddPatchSetPermission) {
     this.checkAddPatchSetPermission = checkAddPatchSetPermission;
-    return this;
-  }
-
-  public PatchSetInserter setDraft(boolean draft) {
-    this.draft = draft;
     return this;
   }
 
@@ -241,7 +235,6 @@ public class PatchSetInserter implements BatchUpdateOp {
             ctx.getUpdate(psId),
             psId,
             commitId,
-            draft,
             newGroups,
             null,
             description);
@@ -263,7 +256,7 @@ public class PatchSetInserter implements BatchUpdateOp {
 
     patchSetInfo =
         patchSetInfoFactory.get(ctx.getRevWalk(), ctx.getRevWalk().parseCommit(commitId), psId);
-    if (change.getStatus() != Change.Status.DRAFT && !allowClosed) {
+    if (!allowClosed) {
       change.setStatus(Change.Status.NEW);
     }
     change.setCurrentPatchSet(patchSetInfo);

c8c08c833f1d1d96e2fb4cfc5cf99573099be457
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2017-02-13 07:40:33 -0500

    Allow to index all changes of a project
    So far, indexing a series of changes had to be done by listing them one
    after the other which can be cumbersome when the number of changes to
    re-index is high.
    
    Add the possibility of indexing all the changes belonging to a project.
    
    Further possibilities of grouping include indexing changes by time
    interval, i.e., changes created/modified between two intervals of time.
    
    Change-Id: Ib47f6d65ca49c85713d30806d7579f9d2d9cf823

---
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/IndexProjectCommand.java
@@ -0,0 +1,64 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd.commands;
+
+import static com.google.gerrit.common.data.GlobalCapability.MAINTAIN_SERVER;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kohsuke.args4j.Argument;
+
+import com.google.gerrit.extensions.annotations.RequiresAnyCapability;
+import com.google.gerrit.server.project.Index;
+import com.google.gerrit.server.project.ProjectControl;
+import com.google.gerrit.server.project.ProjectResource;
+import com.google.gerrit.sshd.CommandMetaData;
+import com.google.gerrit.sshd.SshCommand;
+import com.google.inject.Inject;
+
+@RequiresAnyCapability({MAINTAIN_SERVER})
+@CommandMetaData(name = "project", description = "Index changes of a project")
+final class IndexProjectCommand extends SshCommand {
+
+  @Inject
+  private Index index;
+
+  @Argument(
+      index = 0,
+      required = true,
+      multiValued = true,
+      metaVar = "PROJECT",
+      usage = "projects for which the changes should be indexed"
+  )
+  private List<ProjectControl> projects = new ArrayList<>();
+
+  @Override
+  protected void run() throws UnloggedFailure, Failure, Exception {
+    if (projects.isEmpty()) {
+      throw die("needs at least one project as command arguments");
+    }
+    projects.stream().forEach(this::index);
+  }
+
+  private void index(ProjectControl projectControl) {
+    try {
+      index.apply(new ProjectResource(projectControl), null);
+    } catch (Exception e) {
+      writeError("error",
+          String.format("Unable to index %s: %s", projectControl.getProject().getName(), e.getMessage()));
+    }
+  }
+}

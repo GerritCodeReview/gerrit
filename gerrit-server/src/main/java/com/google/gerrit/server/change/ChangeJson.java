f4843136573819b1a54f967c59fa8d2f9c2a21a6
David Ostrovsky <david@ostrovsky.org>
2017-02-13 21:16:58 +0100

    WIP: Discontinue draft workflow
    TODOs:
    
    * zero-downtime migration
    
    PolyGerrit UI is not addressed in this change and will be done in a
    follow-up change.
    
    Change-Id: Icfcb34efe1ff0ea1d39e94ed500db776f5770d8f

--- gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/ChangeJson.java
@@ -554,7 +554,7 @@ public class ChangeJson {
       src = null;
     }
     if (needMessages) {
-      out.messages = messages(ctl, cd, src);
+      out.messages = messages(ctl, cd);
     }
     finish(out);
 
@@ -1017,8 +1017,8 @@ public class ChangeJson {
     return result;
   }
 
-  private Collection<ChangeMessageInfo> messages(
-      ChangeControl ctl, ChangeData cd, Map<PatchSet.Id, PatchSet> map) throws OrmException {
+  private Collection<ChangeMessageInfo> messages(ChangeControl ctl, ChangeData cd)
+      throws OrmException {
     List<ChangeMessage> messages = cmUtil.byChange(db.get(), cd.notes());
     if (messages.isEmpty()) {
       return Collections.emptyList();
@@ -1027,8 +1027,7 @@ public class ChangeJson {
     List<ChangeMessageInfo> result = Lists.newArrayListWithCapacity(messages.size());
     for (ChangeMessage message : messages) {
       PatchSet.Id patchNum = message.getPatchSetId();
-      PatchSet ps = patchNum != null ? map.get(patchNum) : null;
-      if (patchNum == null || ctl.isPatchVisible(ps, db.get())) {
+      if (patchNum == null || ctl.isPatchVisible(db.get())) {
         ChangeMessageInfo cmi = new ChangeMessageInfo();
         cmi.id = message.getKey().get();
         cmi.author = accountLoader.get(message.getAuthor());
@@ -1147,7 +1146,7 @@ public class ChangeJson {
         RevWalk rw = newRevWalk(repo)) {
       for (PatchSet in : map.values()) {
         if ((has(ALL_REVISIONS) || in.getId().equals(ctl.getChange().currentPatchSetId()))
-            && ctl.isPatchVisible(in, db.get())) {
+            && ctl.isPatchVisible(db.get())) {
           res.put(in.getRevision().get(), toRevisionInfo(ctl, cd, in, repo, rw, false, changeInfo));
         }
       }
@@ -1210,7 +1209,6 @@ public class ChangeJson {
     out.ref = in.getRefName();
     out.created = in.getCreatedOn();
     out.uploader = accountLoader.get(in.getUploader());
-    out.draft = in.isDraft() ? true : null;
     out.fetch = makeFetchMap(ctl, in);
     out.kind = changeKindCache.getChangeKind(repo, rw, cd, in);
     out.description = in.getDescription();
@@ -1247,9 +1245,7 @@ public class ChangeJson {
       out.files.remove(Patch.MERGE_LIST);
     }
 
-    if ((out.isCurrent || (out.draft != null && out.draft))
-        && has(CURRENT_ACTIONS)
-        && userProvider.get().isIdentifiedUser()) {
+    if ((out.isCurrent) && has(CURRENT_ACTIONS) && userProvider.get().isIdentifiedUser()) {
 
       actionJson.addRevisionActions(
           changeInfo, out, new RevisionResource(changeResourceFactory.create(ctl), in));
@@ -1312,7 +1308,7 @@ public class ChangeJson {
         continue;
       }
 
-      if (!scheme.isAuthSupported() && !ctl.forUser(anonymous).isPatchVisible(in, db.get())) {
+      if (!scheme.isAuthSupported() && !ctl.forUser(anonymous).isPatchVisible(db.get())) {
         continue;
       }
 

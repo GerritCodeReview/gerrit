c08ce16fccffed5c95e22ebd9bfb869769c19dec
David Ostrovsky <david@ostrovsky.org>
2017-02-13 21:16:58 +0100

    Discontinue draft workflow
    PolyGerrit UI is not addressed in this change and will be done in a
    follow-up change.
    
    Change-Id: Icfcb34efe1ff0ea1d39e94ed500db776f5770d8f

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotesParser.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/ChangeNotesParser.java
@@ -1028,13 +1028,6 @@ class ChangeNotesParser {
         case DELETED:
           patchSets.remove(e.getKey());
           break;
-
-        case DRAFT:
-          PatchSet ps = patchSets.get(e.getKey());
-          if (ps != null) {
-            ps.setDraft(true);
-          }
-          break;
       }
     }
 

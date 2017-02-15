f5d3f6d258650302c566773785d5a3c2a6f87646
Edwin Kempin <ekempin@google.com>
2017-02-15 11:10:59 +0100

    Migrate external IDs to NoteDb (part 2)
    This is the second part of migrating external IDs from ReviewDb to
    NoteDb.
    
    This change:
    * bumps the database schema version
    * migrates the external IDs from ReviewDb to NoteDb (for single
      instance Gerrit servers)
    * read external IDs from NoteDb
    * deletes the database table
    
    The ExternalIds class implements reading of external IDs from the
    refs/meta/external-ids Git notes branch in the All-Users repository.
    It supports to read single external IDs by external ID key (parse one
    Git note) and reading of all external IDs (parse all Git notes).
    Reading all external IDs of an account is not supported. Since the
    external IDs are keyed by external ID key ('<scheme>:<id>') and the
    account ID is only contained in the Git note content, finding all
    external IDs of one account would require parsing all Git notes, which
    is too expensive if it is done frequently. Instead there is a new
    ExternalIdCache which makes external IDs accessible by account. This
    cache is populated once by reading all external IDs from NoteDb and is
    then kept up to date by informing it whenever an external ID is added,
    updated or deleted. The external ID cache uses the revision of the
    refs/meta/external-ids branch as key, so that all external IDs are
    reloaded when the refs/meta/external-ids branch is changed behind
    Gerrit's back. This makes it easy to use this cache in a multimaster
    setup, since an update of the refs/meta/external-ids branch which is
    done due to replication between nodes causes a reload of the external
    IDs in the receiving node.
    
    Pushing to the refs/meta/external-ids branch is still prevented by a
    commit validator. Since all external IDs are now in NoteDb we could
    allow pushing to refs/meta/external-ids. However we would still like to
    do validation of the branch content and reject invalid content (e.g.
    invalid Git config files, usage of non-existing account IDs etc.) and
    such a validator is not implemented yet (but can be implemented in a
    follow-up change).
    
    Change-Id: Ia1dae9306b7ee07388b6c5e1f3dc4a1a5eea4b08
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -86,6 +86,7 @@ import com.google.gerrit.server.account.CapabilityCollection;
 import com.google.gerrit.server.account.CapabilityControl;
 import com.google.gerrit.server.account.ChangeUserName;
 import com.google.gerrit.server.account.EmailExpander;
+import com.google.gerrit.server.account.ExternalIdCacheImpl;
 import com.google.gerrit.server.account.GroupCacheImpl;
 import com.google.gerrit.server.account.GroupControl;
 import com.google.gerrit.server.account.GroupDetailFactory;
@@ -214,6 +215,7 @@ public class GerritGlobalModule extends FactoryModule {
     install(AccountCacheImpl.module());
     install(ChangeKindCacheImpl.module());
     install(ConflictsCacheImpl.module());
+    install(ExternalIdCacheImpl.module());
     install(GroupCacheImpl.module());
     install(GroupIncludeCacheImpl.module());
     install(MergeabilityCacheImpl.module());

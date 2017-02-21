65633a58588f8f3194dfb544dd6d5a2ffe738827
Edwin Kempin <ekempin@google.com>
2017-02-21 11:56:08 +0100

    Implement Private Changes
    - Add isPrivate field to change in ReviewDb
    - Check visibility for private changes
    - Add permission that allows users to see all private changes
    - Add Is-Private Footer to NoteDb
    - Add field for private changes to index and QueryBuilder
    - Add REST endpoints to GET/SET change private status
    - VisibleRefsFilter filters private changes
    
    Change-Id: Ib2b26ea19c0286cff9c05754b0875f61e5e9fceb
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Signed-off-by: Patrick Hiesel <hiesel@google.com>
    Signed-off-by: Changcheng Xiao <xchangcheng@google.com>

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PutPrivate.java
@@ -0,0 +1,72 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import com.google.gerrit.common.TimeUtil;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.git.BatchUpdate;
+import com.google.gerrit.server.git.UpdateException;
+import com.google.gerrit.server.project.ChangeControl;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+@Singleton
+public class PutPrivate implements RestModifyView<ChangeResource, PutPrivate.Input> {
+  public static class Input {}
+
+  private final Provider<ReviewDb> dbProvider;
+  private final BatchUpdate.Factory batchUpdateFactory;
+  private final Provider<CurrentUser> self;
+
+  @Inject
+  PutPrivate(Provider<ReviewDb> dbProvider,
+      BatchUpdate.Factory batchUpdateFactory,
+      Provider<CurrentUser> self) {
+    this.dbProvider = dbProvider;
+    this.batchUpdateFactory = batchUpdateFactory;
+    this.self = self;
+  }
+
+  @Override
+  public Response<String> apply(ChangeResource rsrc, Input input)
+      throws RestApiException, UpdateException {
+    if (self.get() != rsrc.getUser()) {
+      throw new AuthException("not allowed to set private status");
+    }
+
+    if (rsrc.getChange().isPrivate()) {
+      return Response.ok("");
+    }
+
+    ChangeControl control = rsrc.getControl();
+    SetPrivateOp op = new SetPrivateOp(true);
+    try (BatchUpdate u =
+        batchUpdateFactory.create(
+            dbProvider.get(),
+            control.getProject().getNameKey(),
+            control.getUser(),
+            TimeUtil.nowTs())) {
+      u.addOp(control.getId(), op).execute();
+    }
+
+    return Response.created("");
+  }
+}

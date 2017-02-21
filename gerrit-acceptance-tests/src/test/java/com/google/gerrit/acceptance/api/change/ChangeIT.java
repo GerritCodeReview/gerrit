81da9a7a470ff6cc063effa4cec4d921d7286903
Edwin Kempin <ekempin@google.com>
2017-02-21 11:56:08 +0100

    Implement Private Changes
    Private changes are only visible to owner, reviewers and users with the
    configured permission. This lets users stage changes without
    advertising their change, and conduct sensitive reviews (eg. security)
    among a small group.
    
    - Add Private field to change in ReviewDb
    - Check visibility for private changes
    - Add permission that allows users to see all private changes
    - Add Private Footer to NoteDb
    - Add field for private changes to index and QueryBuilder
    - Add REST endpoints to GET/PUT/DELETE change private status
    - VisibleRefsFilter filters private changes
    - GWT UI: Mark/Unmark change as private and show private label
    - GWT UI: Show 'status (Private)' in ChangeTable.
    - Support to control privacy of a change on push
    - Add tests for reviewer visibility
    - Add tests for query by private
    - Add tests for advertised references
    
      To push a private change or to turn a change private on push the
      'private' option can be specified:
    
        git push host HEAD:refs/for/master%private
    
      Removing the privacy flag should not happen accidentally, but should be
      a very explicit action. This is why omitting the 'private' option when
      pushing updates to a private change doesn't remove the privacy flag on
      the change. To remove the privacy flag from a change on push the
      'remove-private' option can be used:
    
        git push host HEAD:refs/for/master%remove-private
    
    Change-Id: Ib2b26ea19c0286cff9c05754b0875f61e5e9fceb
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Signed-off-by: Patrick Hiesel <hiesel@google.com>
    Signed-off-by: Changcheng Xiao <xchangcheng@google.com>
    Signed-off-by: Alice Kober-Sotzek <aliceks@google.com>

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/api/change/ChangeIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/api/change/ChangeIT.java
@@ -177,6 +177,60 @@ public class ChangeIT extends AbstractDaemonTest {
   }
 
   @Test
+  public void setPrivateByOwner() throws Exception {
+    TestRepository<InMemoryRepository> userRepo = cloneProject(project, user);
+    PushOneCommit.Result result =
+        pushFactory.create(db, user.getIdent(), userRepo).to("refs/for/master");
+
+    setApiUser(user);
+    String changeId = result.getChangeId();
+    assertThat(gApi.changes().id(changeId).isPrivate()).isFalse();
+    gApi.changes().id(changeId).setPrivate(true);
+    assertThat(gApi.changes().id(changeId).isPrivate()).isTrue();
+    gApi.changes().id(changeId).setPrivate(false);
+    assertThat(gApi.changes().id(changeId).isPrivate()).isFalse();
+  }
+
+  @Test
+  public void setPrivateByOtherUser() throws Exception {
+    TestRepository<InMemoryRepository> userRepo = cloneProject(project, user);
+    PushOneCommit.Result result =
+        pushFactory.create(db, user.getIdent(), userRepo).to("refs/for/master");
+
+    assertThat(gApi.changes().id(result.getChangeId()).isPrivate()).isFalse();
+    exception.expect(AuthException.class);
+    exception.expectMessage("not allowed to set private flag");
+    gApi.changes().id(result.getChangeId()).setPrivate(true);
+  }
+
+  @Test
+  public void accessPrivate() throws Exception {
+    TestRepository<InMemoryRepository> userRepo = cloneProject(project, user);
+    PushOneCommit.Result result =
+        pushFactory.create(db, user.getIdent(), userRepo).to("refs/for/master");
+
+    setApiUser(user);
+    gApi.changes().id(result.getChangeId()).setPrivate(true);
+    // Owner can always access its private changes.
+    assertThat(gApi.changes().id(result.getChangeId()).isPrivate()).isTrue();
+
+    // Add admin as a reviewer.
+    gApi.changes().id(result.getChangeId()).addReviewer(admin.getId().toString());
+
+    // This change should be visible for admin as a reviewer.
+    setApiUser(admin);
+    assertThat(gApi.changes().id(result.getChangeId()).isPrivate()).isTrue();
+
+    // Remove admin from reviewers.
+    gApi.changes().id(result.getChangeId()).reviewer(admin.getId().toString()).remove();
+
+    // This change should not be visible for admin anymore.
+    exception.expect(ResourceNotFoundException.class);
+    exception.expectMessage("Not found: " + result.getChangeId());
+    gApi.changes().id(result.getChangeId()).get();
+  }
+
+  @Test
   public void getAmbiguous() throws Exception {
     PushOneCommit.Result r1 = createChange();
     String changeId = r1.getChangeId();

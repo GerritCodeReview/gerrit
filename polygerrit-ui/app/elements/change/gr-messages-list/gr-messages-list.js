06141be187120deeb2c609b02352705c2bc3a598
Kasper Nilsson <kaspern@google.com>
2017-02-23 14:34:28 -0800

    Preserve expanded state for hidden messages
    Conditionally adds 'expanded' prop to the message object passed to
    gr-message that takes precedence over the default expand/collapse
    logic (only when defined). Gr-message also now watches this prop to
    determine expanded/collapsed status, as opposed to toggling the prop on
    the DOM element.
    
    Bug: Issue 5626
    Change-Id: I0cad5ce62a80409fd5709c12008dd4da603ab61a

--- polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list.js
+++ polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list.js
@@ -45,6 +45,7 @@
       _expanded: {
         type: Boolean,
         value: false,
+        observer: '_expandedChanged',
       },
       _hideAutomated: {
         type: Boolean,
@@ -71,7 +72,7 @@
       var el = this.$$('[data-message-id="' + messageID + '"]');
       if (!el) { return; }
 
-      el.expanded = true;
+      el.set('message.expanded', true);
       var top = el.offsetTop;
       for (var offsetParent = el.offsetParent;
            offsetParent;
@@ -121,6 +122,15 @@
       return result;
     },
 
+    _expandedChanged: function(exp) {
+      for (var i = 0; i < this._processedMessages.length; i++) {
+        this._processedMessages[i].expanded = exp;
+        if (i < this._visibleMessages.length) {
+          this.set(['_visibleMessages', i, 'expanded'], exp);
+        }
+      }
+    },
+
     _highlightEl: function(el) {
       var highlightedEls =
           Polymer.dom(this.root).querySelectorAll('.highlighted');
@@ -135,20 +145,9 @@
       el.classList.add('highlighted');
     },
 
-    /**
-    * @param {boolean} expand
-    */
-    handleExpandCollapse: function(expand) {
-      this._expanded = expand;
-      var messageEls = Polymer.dom(this.root).querySelectorAll('gr-message');
-      for (var i = 0; i < messageEls.length; i++) {
-        messageEls[i].expanded = expand;
-      }
-    },
-
     _handleExpandCollapseTap: function(e) {
       e.preventDefault();
-      this.handleExpandCollapse(!this._expanded);
+      this._expanded = !this._expanded;
     },
 
     _handleAutomatedMessageToggleTap: function(e) {

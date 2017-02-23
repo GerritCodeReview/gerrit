daad6082938c45778c6690d6d798523782a57d72
Han-Wen Nienhuys <hanwen@google.com>
2017-02-23 17:01:08 +0100

    Clarify getAccountId behavior for logged-out users.
    Change-Id: I6aa392411956305bbc14d40edaab29b9d9185989

--- gerrit-server/src/main/java/com/google/gerrit/server/CurrentUser.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/CurrentUser.java
@@ -117,7 +117,10 @@ public abstract class CurrentUser {
         getClass().getSimpleName() + " is not an IdentifiedUser");
   }
 
-  /** Return account ID if {@link #isIdentifiedUser} is true. */
+  /** Return account ID if {@link #isIdentifiedUser} is true.
+   *
+   * @throws UnsupportedOperationException if the user is not logged in.
+   * */
   public Account.Id getAccountId() {
     throw new UnsupportedOperationException(
         getClass().getSimpleName() + " is not an IdentifiedUser");

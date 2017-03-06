cd2fb842013600e379cbeecf8e6bfa0ae4e2a9e9
Kasper Nilsson <kaspern@google.com>
2017-03-06 11:04:51 -0800

    Enable message hyperlinking for hidden messages
    Previously, when given a message ID in the URL, the messages-list would
    check the already-rendered messages list for that ID and exit when it
    failed to find it.
    
    The addition of show/hide for messages broke the assumption that the
    already-rendered messages list directly corresponds to the total
    messages list.
    
    With this change, scrollToMessage now searches for the message ID in
    _processedMessages and loads back to that message if it is not already
    shown, then continues with the prior scrollTo and highlight logic.
    
    Bug: Issue 5676
    Change-Id: If8b2db1074c3ba128090e80bf89f81c5393c557a

--- polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list.js
+++ polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list.js
@@ -69,7 +69,24 @@
 
     scrollToMessage: function(messageID) {
       var el = this.$$('[data-message-id="' + messageID + '"]');
-      if (!el) { return; }
+      // If the message is hidden, expand the hidden messages back to that
+      // point.
+      if (!el) {
+        for (var index = 0; index < this._processedMessages.length; index++) {
+          if (this._processedMessages[index].id === messageID) {
+            break;
+          }
+        }
+        if (index === this._processedMessages.length) { return; }
+
+        var newMessages = this._processedMessages.slice(index,
+            -this._visibleMessages.length);
+        // Add newMessages to the beginning of _visibleMessages.
+        this.splice.apply(this, ['_visibleMessages', 0, 0].concat(newMessages));
+        // Allow the dom-repeat to stamp.
+        Polymer.dom.flush();
+        el = this.$$('[data-message-id="' + messageID + '"]');
+      }
 
       el.expanded = true;
       var top = el.offsetTop;

7feb9ff2227db4f4c2f31e6b1fbe533d05bc746e
David Ostrovsky <david@ostrovsky.org>
2017-03-09 23:56:56 +0100

    Add API for work in progress workflow
    Change-Id: Idb0faa778977038cea336da38acdfb4089679618

--- gerrit-server/src/main/java/com/google/gerrit/server/api/changes/ChangeApiImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/api/changes/ChangeApiImpl.java
@@ -71,8 +71,11 @@ import com.google.gerrit.server.change.Restore;
 import com.google.gerrit.server.change.Revert;
 import com.google.gerrit.server.change.Reviewers;
 import com.google.gerrit.server.change.Revisions;
+import com.google.gerrit.server.change.SetReadyForReview;
+import com.google.gerrit.server.change.SetWorkInProgress;
 import com.google.gerrit.server.change.SubmittedTogether;
 import com.google.gerrit.server.change.SuggestChangeReviewers;
+import com.google.gerrit.server.change.WorkInProgressOp;
 import com.google.gerrit.server.permissions.PermissionBackendException;
 import com.google.gerrit.server.project.InvalidChangeOperationException;
 import com.google.gerrit.server.update.UpdateException;
@@ -125,6 +128,8 @@ class ChangeApiImpl implements ChangeApi {
   private final Move move;
   private final PutPrivate putPrivate;
   private final DeletePrivate deletePrivate;
+  private final SetWorkInProgress setWip;
+  private final SetReadyForReview setReady;
 
   @Inject
   ChangeApiImpl(
@@ -161,6 +166,8 @@ class ChangeApiImpl implements ChangeApi {
       Move move,
       PutPrivate putPrivate,
       DeletePrivate deletePrivate,
+      SetWorkInProgress setWip,
+      SetReadyForReview setReady,
       @Assisted ChangeResource change) {
     this.changeApi = changeApi;
     this.revert = revert;
@@ -195,6 +202,8 @@ class ChangeApiImpl implements ChangeApi {
     this.move = move;
     this.putPrivate = putPrivate;
     this.deletePrivate = deletePrivate;
+    this.setWip = setWip;
+    this.setReady = setReady;
     this.change = change;
   }
 
@@ -286,6 +295,34 @@ class ChangeApiImpl implements ChangeApi {
     } catch (UpdateException e) {
       throw new RestApiException("Cannot change private status", e);
     }
+  }
+
+  @Override
+  public void setWorkInProgress(String message) throws RestApiException {
+    try {
+      setWip.apply(change, new WorkInProgressOp.Input(message));
+    } catch (UpdateException e) {
+      throw new RestApiException("Cannot set work in progress state", e);
+    }
+  }
+
+  @Override
+  public void setReadyForReview(String message) throws RestApiException {
+    try {
+      setReady.apply(change, new WorkInProgressOp.Input(message));
+    } catch (UpdateException e) {
+      throw new RestApiException("Cannot set ready for review state", e);
+    }
+  }
+
+  @Override
+  public void setWorkInProgress() throws RestApiException {
+    setWorkInProgress("");
+  }
+
+  @Override
+  public void setReadyForReview() throws RestApiException {
+    setReadyForReview("");
   }
 
   @Override

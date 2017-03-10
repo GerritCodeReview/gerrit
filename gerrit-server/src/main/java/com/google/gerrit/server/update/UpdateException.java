ea11b28e66cddba8a2228d7c5361119ceb15d859
Dave Borowitz <dborowitz@google.com>
2017-03-10 12:51:50 -0500

    Move BatchUpdate to a new package
    This class is a whole framework for executing updates in a mixed
    ReviewDb/NoteDb environment. Move it to some place more specific and
    applicable than "git", along with some auxiliary classes.
    
    Change-Id: Ibcea72b8900d259e51d3311b3db8bc70218ca148

--- gerrit-server/src/main/java/com/google/gerrit/server/update/UpdateException.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/update/UpdateException.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.git;
+package com.google.gerrit.server.update;
 
 /** Exception type thrown by {@link BatchUpdate} steps. */
 public class UpdateException extends Exception {

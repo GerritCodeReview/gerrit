0dfd0c363bd8ca87502052ff6dc51a58d1376500
Edwin Kempin <ekempin@google.com>
2017-03-10 14:35:58 +0100

    AccountAccess: Remove unused methods
    The database indexes that were only needed to support these methods are
    deleted.
    
    Change-Id: Iefc47c2dbe404453636d347c4fc2f3bc9a5c1cd4
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/SchemaVersion.java
@@ -35,7 +35,7 @@ import java.util.concurrent.TimeUnit;
 /** A version of the database schema. */
 public abstract class SchemaVersion {
   /** The current schema version. */
-  public static final Class<Schema_146> C = Schema_146.class;
+  public static final Class<Schema_147> C = Schema_147.class;
 
   public static int getBinaryVersion() {
     return guessVersion(C);

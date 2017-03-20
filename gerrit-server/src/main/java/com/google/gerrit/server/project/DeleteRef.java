afc7be5acfcc806261423c011d92278674844c71
David Pursehouse <dpursehouse@collab.net>
2017-03-20 09:07:44 +0900

    Be more consistent about object ids used in ref operation validation
    In CreateBranch, a RefUpdate is created with expectedOldObjectId set
    but not oldObjectId. In DeleteRef, none of expectedOldObjectId,
    oldObjectId, and newObjectId is set.
    
    As a result, when RefOperationValidators uses oldObjectId and
    newObjectId to create a ReceiveCommand, they can be null, resulting in
    NullPointerException when a listener dereferences them.
    
    So use expectedOldObjectId instead of oldObjectId in
    RefOperationValidators. Before a ref update has been performed, only
    the former is meant to be set.
    
    In DeleteRef, set the expectedOldObjectId and the newObjectId (which
    is zero). This way, in RefOperationValidators we know both values
    will always be set.
    
    Bug: Issue 5817
    Helped-By: Jonathan Nieder <jrn@google.com>
    Change-Id: If1f3a6179fa789077731a16e4b731227a73be7f2

--- gerrit-server/src/main/java/com/google/gerrit/server/project/DeleteRef.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/DeleteRef.java
@@ -115,6 +115,8 @@ public class DeleteRef {
     }
     RefUpdate.Result result;
     RefUpdate u = r.updateRef(ref);
+    u.setExpectedOldObjectId(r.exactRef(ref).getObjectId());
+    u.setNewObjectId(ObjectId.zeroId());
     u.setForceUpdate(true);
     refDeletionValidator.validateRefOperation(ref, identifiedUser.get(), u);
     int remainingLockFailureCalls = MAX_LOCK_FAILURE_CALLS;
@@ -222,6 +224,8 @@ public class DeleteRef {
 
     RefUpdate u = r.updateRef(refName);
     u.setForceUpdate(true);
+    u.setExpectedOldObjectId(r.exactRef(refName).getObjectId());
+    u.setNewObjectId(ObjectId.zeroId());
     refDeletionValidator.validateRefOperation(project.getName(), identifiedUser.get(), u);
     return command;
   }

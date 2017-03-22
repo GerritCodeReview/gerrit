16c898b553fc59a1fe209aa79bbca74b9ba98de6
Edwin Kempin <ekempin@google.com>
2017-03-22 14:48:18 +0100

    Migrate external IDs to NoteDb (part 3)
    This is the third part of migrating external IDs from ReviewDb to
    NoteDb.
    
    This change:
    * changes the code to always read external IDs from NoteDb (the
      user.readExternalIdsFromGit configuration parameter is removed)
    * bumps the database schema version
    * deletes the database table for external IDs
    
    Pushing to the refs/meta/external-ids branch is still prevented by a
    commit validator. Since all external IDs are now in NoteDb only we
    could allow pushing to refs/meta/external-ids. However we would still
    like to do validation of the branch content and reject invalid content
    (e.g. invalid Git config files, usage of non-existing account IDs
    etc.) and such a validator is not implemented yet (but can be
    implemented in a follow-up change).
    
    Change-Id: Id9e5574a1d8d82f4f48fbb0b6dadc0e27d138a28
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/account/ExternalIdIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/account/ExternalIdIT.java
@@ -201,7 +201,7 @@ public class ExternalIdIT extends AbstractDaemonTest {
             () -> {
               if (!doneBgUpdate.getAndSet(true)) {
                 try {
-                  extIdsUpdate.create().insert(db, ExternalId.create(barId, admin.id));
+                  extIdsUpdate.create().insert(ExternalId.create(barId, admin.id));
                 } catch (IOException | ConfigInvalidException | OrmException e) {
                   // Ignore, the successful insertion of the external ID is asserted later
                 }
@@ -209,11 +209,11 @@ public class ExternalIdIT extends AbstractDaemonTest {
             },
             retryer);
     assertThat(doneBgUpdate.get()).isFalse();
-    update.insert(db, ExternalId.create(fooId, admin.id));
+    update.insert(ExternalId.create(fooId, admin.id));
     assertThat(doneBgUpdate.get()).isTrue();
 
-    assertThat(externalIds.get(db, fooId)).isNotNull();
-    assertThat(externalIds.get(db, barId)).isNotNull();
+    assertThat(externalIds.get(fooId)).isNotNull();
+    assertThat(externalIds.get(barId)).isNotNull();
   }
 
   @Test
@@ -236,7 +236,7 @@ public class ExternalIdIT extends AbstractDaemonTest {
               try {
                 extIdsUpdate
                     .create()
-                    .insert(db, ExternalId.create(extIdsKeys[bgCounter.getAndAdd(1)], admin.id));
+                    .insert(ExternalId.create(extIdsKeys[bgCounter.getAndAdd(1)], admin.id));
               } catch (IOException | ConfigInvalidException | OrmException e) {
                 // Ignore, the successful insertion of the external ID is asserted later
               }
@@ -247,14 +247,14 @@ public class ExternalIdIT extends AbstractDaemonTest {
                 .build());
     assertThat(bgCounter.get()).isEqualTo(0);
     try {
-      update.insert(db, ExternalId.create(ExternalId.Key.create("abc", "abc"), admin.id));
+      update.insert(ExternalId.create(ExternalId.Key.create("abc", "abc"), admin.id));
       fail("expected LockFailureException");
     } catch (LockFailureException e) {
       // Ignore, expected
     }
     assertThat(bgCounter.get()).isEqualTo(extIdsKeys.length);
     for (ExternalId.Key extIdKey : extIdsKeys) {
-      assertThat(externalIds.get(db, extIdKey)).isNotNull();
+      assertThat(externalIds.get(extIdKey)).isNotNull();
     }
   }
 }

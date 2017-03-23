9209c61246a6fdbf8770be2822b418ccd400bd4a
Kasper Nilsson <kaspern@google.com>
2017-03-23 10:53:21 -0700

    Add handling for arbitrary CCs to the account-list
    If allowAnyInput is enabled for the account-list, receive the string
    value from the autocomplete, validate it as an email address, and add it
    to the accounts array so it shows up in the account-list. If the string
    is not a valid email address, reset the text in the autocomplete and
    toast the user.
    
    Also adds a function setText to gr-account-entry.
    
    Feature: Issue 5832
    Change-Id: Ia3cbc3142fb4b54fd37e5c559fddb9f425fdf948

--- polygerrit-ui/app/elements/change/gr-account-list/gr-account-list.js
+++ polygerrit-ui/app/elements/change/gr-account-list/gr-account-list.js
@@ -14,6 +14,8 @@
 (function() {
   'use strict';
 
+  var VALID_EMAIL_ALERT = 'Please input a valid email.';
+
   Polymer({
     is: 'gr-account-list',
 
@@ -99,6 +101,17 @@
         var group = Object.assign({}, reviewer.group,
             {_pendingAdd: true, _group: true});
         this.push('accounts', group);
+      } else if (this.allowAnyInput) {
+        if (reviewer.indexOf('@') === -1) {
+          // Repopulate the input with what the user tried to enter and have
+          // a toast tell them why they can't enter it.
+          this.$.entry.setText(reviewer);
+          this.dispatchEvent(new CustomEvent('show-alert',
+            {detail: {message: VALID_EMAIL_ALERT}, bubbles: true}));
+        } else {
+          var account = {email: reviewer, _pendingAdd: true};
+          this.push('accounts', account);
+        }
       }
       this.pendingConfirmation = null;
     },

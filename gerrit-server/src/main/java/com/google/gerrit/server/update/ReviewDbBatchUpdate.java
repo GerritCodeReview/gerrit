2af3c1e64c71e1caa90617c1dfc518693522578d
Dave Borowitz <dborowitz@google.com>
2017-03-24 09:42:18 -0700

    Only allow BatchUpdateListener with REPO_BEFORE_DB
    The listener interface is currently only used by MergeOp, which is
    REPO_BEFORE_DB. Reasoning about the order in which listeners are called
    is going to be more difficult if we fuse the repo and change meta ref
    update phases; sidestep the problem by just removing support for
    listeners in the odd DB_BEFORE_REPO case.
    
    Change-Id: Ia2a1c1f827247602eab1f8527f3cd3118f740f23

--- gerrit-server/src/main/java/com/google/gerrit/server/update/ReviewDbBatchUpdate.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/update/ReviewDbBatchUpdate.java
@@ -269,7 +269,7 @@ class ReviewDbBatchUpdate extends BatchUpdate {
     }
     setRequestIds(updates, requestId);
     try {
-      Order order = getOrder(updates);
+      Order order = getOrder(updates, listener);
       boolean updateChangesInParallel = getUpdateChangesInParallel(updates);
       switch (order) {
         case REPO_BEFORE_DB:
@@ -290,15 +290,12 @@ class ReviewDbBatchUpdate extends BatchUpdate {
           for (ReviewDbBatchUpdate u : updates) {
             u.reindexChanges(u.executeChangeOps(updateChangesInParallel, dryrun));
           }
-          listener.afterUpdateChanges();
           for (ReviewDbBatchUpdate u : updates) {
             u.executeUpdateRepo();
           }
-          listener.afterUpdateRepos();
           for (ReviewDbBatchUpdate u : updates) {
             u.executeRefUpdates(dryrun);
           }
-          listener.afterUpdateRefs();
           break;
         default:
           throw new IllegalStateException("invalid execution order: " + order);

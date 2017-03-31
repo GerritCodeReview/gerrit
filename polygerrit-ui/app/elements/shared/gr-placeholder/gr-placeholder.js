994a886660278d25599f934a912e5bcb58ccf484
Paladox none <thomasmulhall410@yahoo.com>
2017-03-31 14:37:00 +0000

    Do not merge: PolyGerrit
    Change-Id: Ib3bb694969e903fd76a1dad13cfb642bde086142

--- polygerrit-ui/app/elements/shared/gr-placeholder/gr-placeholder.js
+++ polygerrit-ui/app/elements/shared/gr-placeholder/gr-placeholder.js
@@ -21,5 +21,13 @@
       path: String,
       title: String,
     },
+
+    getBaseUrl: function(url, path) {
+      if (url != '' || url != '/') {
+        return url + '/?polygerrit=0#' + path;
+      } else {
+        return '/?polygerrit=0#' + path;
+      }
+    },
   });
 })();

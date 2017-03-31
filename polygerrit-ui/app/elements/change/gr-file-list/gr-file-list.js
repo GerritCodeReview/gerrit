bbe2654e613a00f292400973d64b10598fd63b60
Paladox none <thomasmulhall410@yahoo.com>
2017-03-31 14:37:00 +0000

    Fix PolyGerrit URLs to support prefixed URL
    This updates most of polygerrit links to use the implementation added in
    I2b2d704fe33c90ea2f2a2183fc79897642a48175 .
    
    Change-Id: Ib3bb694969e903fd76a1dad13cfb642bde086142

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
@@ -111,6 +111,7 @@
     },
 
     behaviors: [
+      Gerrit.BaseUrlBehavior,
       Gerrit.KeyboardShortcutBehavior,
       Gerrit.PatchSetBehavior,
       Gerrit.URLEncodingBehavior,
@@ -604,7 +605,7 @@
     },
 
     _computeDiffURL: function(changeNum, patchRange, path) {
-      return this.encodeURL('/c/' + changeNum + '/' +
+      return this.encodeURL(this.getBaseUrl() + '/c/' + changeNum + '/' +
           this._patchRangeStr(patchRange) + '/' + path, true);
     },
 

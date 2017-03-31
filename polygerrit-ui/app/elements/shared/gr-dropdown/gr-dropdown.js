132bfcbf4ca844658cdd0c9cea6c9b289981771c
Paladox none <thomasmulhall410@yahoo.com>
2017-03-31 14:37:00 +0000

    Fix PolyGerrit URLs to support prefixed URL
    This updates most of polygerrit links to use the implementation added in
    I2b2d704fe33c90ea2f2a2183fc79897642a48175 .
    
    Change-Id: Ib3bb694969e903fd76a1dad13cfb642bde086142

--- polygerrit-ui/app/elements/shared/gr-dropdown/gr-dropdown.js
+++ polygerrit-ui/app/elements/shared/gr-dropdown/gr-dropdown.js
@@ -56,6 +56,10 @@
       _hasAvatars: String,
     },
 
+    behaviors: [
+      Gerrit.BaseUrlBehavior,
+    ],
+
     attached: function() {
       this.$.restAPI.getConfig().then(function(cfg) {
         this._hasAvatars = !!(cfg && cfg.plugin && cfg.plugin.has_avatars);
@@ -75,7 +79,7 @@
     },
 
     _computeURLHelper: function(host, path) {
-      return '//' + host + path;
+      return '//' + host + this.getBaseUrl() + path;
     },
 
     _computeRelativeURL: function(path) {

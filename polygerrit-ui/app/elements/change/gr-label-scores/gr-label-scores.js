a499e3d9382fef139294472f4853d0d8f7e76931
Becky Siegel <beckysiegel@google.com>
2017-04-06 16:57:13 -0700

    Align labels in reply dialog
    This change adds a component to handle the layout of labels so that
    values are lined up.
    
    The process for doing this is as follows:
    - Compute all of the label values and sort them.
    - For each label, determine a number of placeholder elements needed so
      that the first label is in the position determined by the sorted list
      of labels
    
        ex: sorted values = [-2, -1, 0, 1, 2]
        particular lable values = [0, 1, 2]
        requires 2 placeholders
    
      Placeholders are used rather than padding so that items wrapping to
      the next line works as expected. An array of empty items is computed.
    
      There is also a placeholder after the values used for centering
      content on small screens.
    
    - In each of the iron selectors, There are two dom-repeats. First, the
      computed blank items, and then the actual label values.
    
    Note: there is a new media query value of max-width: 25em that is not
    currently used on the app. This is intentional, as using a 50em
    breakpoint does not make sense here. The goal is to keep all labels
    on the same line without increasing vertical space when unnecessary.
    
    Bug: Issue 5757
    Change-Id: I99564cff7a3e49252dd695f189f2654e97c86fbe

---
+++ polygerrit-ui/app/elements/change/gr-label-scores/gr-label-scores.js
@@ -0,0 +1,136 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-label-scores',
+    properties: {
+      _labels: {
+        type: Array,
+        computed: '_computeLabels(change.labels.*, account)',
+      },
+      permittedLabels: {
+        type: Object,
+        observer: '_computeColumns',
+      },
+      change: Object,
+      _labelValues: Object,
+    },
+
+    getLabelValues: function() {
+      var labels = {};
+      for (var label in this.permittedLabels) {
+        if (!this.permittedLabels.hasOwnProperty(label)) { continue; }
+
+        var selectorEl = this.$$('iron-selector[data-label="' +
+            label + '"]');
+
+        // The user may have not voted on this label.
+        if (!selectorEl || !selectorEl.selectedItem) { continue; }
+
+        var selectedVal = selectorEl.selectedItem.getAttribute('data-value');
+        selectedVal = parseInt(selectedVal, 10);
+
+        // Only send the selection if the user changed it.
+        var prevVal = this._getVoteForAccount(this.change.labels, label,
+            this.account);
+        if (prevVal !== null) {
+          prevVal = parseInt(prevVal, 10);
+        }
+        if (selectedVal !== prevVal) {
+          labels[label] = selectedVal;
+        }
+      }
+      return labels;
+    },
+
+    _getVoteForAccount: function(labels, labelName, account) {
+      var votes = labels[labelName];
+      if (votes.all && votes.all.length > 0) {
+        for (var i = 0; i < votes.all.length; i++) {
+          if (votes.all[i]._account_id == account._account_id) {
+            return votes.all[i].value;
+          }
+        }
+      }
+      return null;
+    },
+
+    _computeLabels: function(labelRecord) {
+      var labelsObj = labelRecord.base;
+      if (!labelsObj) { return []; }
+      return Object.keys(labelsObj).sort().map(function(key) {
+        return {
+          name: key,
+          value: this._getVoteForAccount(labelsObj, key, this.account),
+        };
+      }.bind(this));
+    },
+
+    _computeColumns: function(permittedLabels) {
+      var labels = Object.keys(permittedLabels);
+      var values = {};
+
+      labels.forEach(function(label) {
+        permittedLabels[label].forEach(function(value) {
+          values[parseInt(value, 10)] = true;
+        });
+      });
+
+      var orderedValues = Object.keys(values).sort(function(a, b) {
+        return a - b;
+      });
+
+      for (var i = 0; i < orderedValues.length; i++) {
+        values[orderedValues[i]] = i;
+      }
+      this._labelValues = values;
+    },
+
+    _computeIndexOfLabelValue: function(labels, permittedLabels, label) {
+      if (!labels[label.name]) { return null; }
+      var labelValue = label.value;
+      var len = permittedLabels[label.name] != null ?
+          permittedLabels[label.name].length : 0;
+      var numberBlanks = this._computeBlankItems(permittedLabels, label.name)
+          .length;
+      for (var i = 0; i < len; i++) {
+        var val = parseInt(permittedLabels[label.name][i], 10);
+        if (val == labelValue) {
+          return i + numberBlanks;
+        }
+      }
+      return null;
+    },
+
+    _computePermittedLabelValues: function(permittedLabels, label) {
+      return permittedLabels[label];
+    },
+
+    _computeBlankItems: function(permittedLabels, label) {
+      var startPosition = this._labelValues[parseInt(
+          permittedLabels[label][0])];
+      return new Array(startPosition);
+    },
+
+    _computeAnyPermittedLabelValues: function(permittedLabels, label) {
+      return permittedLabels.hasOwnProperty(label);
+    },
+
+     _computeLabelValueTitle: function(labels, label, value) {
+      return labels[label] && labels[label].values[value];
+    },
+  });
+})();
--- polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.js
+++ polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.js
@@ -84,10 +84,6 @@
         type: Object,
         observer: '_reviewerPendingConfirmationUpdated',
       },
-      _labels: {
-        type: Array,
-        computed: '_computeLabels(change.labels.*, _account)',
-      },
       _owner: Object,
       _pendingConfirmationDetails: Object,
       _includeComments: {
@@ -260,32 +256,13 @@
     },
 
     send: function(includeComments) {
+      var labels = this.$.labelScores.getLabelValues();
+
       var obj = {
         drafts: includeComments ? 'PUBLISH_ALL_REVISIONS' : 'KEEP',
-        labels: {},
+        labels: labels,
       };
 
-      for (var label in this.permittedLabels) {
-        if (!this.permittedLabels.hasOwnProperty(label)) { continue; }
-
-        var selectorEl = this.$$('iron-selector[data-label="' + label + '"]');
-
-        // The user may have not voted on this label.
-        if (!selectorEl || !selectorEl.selectedItem) { continue; }
-
-        var selectedVal = selectorEl.selectedItem.getAttribute('data-value');
-        selectedVal = parseInt(selectedVal, 10);
-
-        // Only send the selection if the user changed it.
-        var prevVal = this._getVoteForAccount(this.change.labels, label,
-            this._account);
-        if (prevVal !== null) {
-          prevVal = parseInt(prevVal, 10);
-        }
-        if (selectedVal !== prevVal) {
-          obj.labels[label] = selectedVal;
-        }
-      }
       if (this.draft != null) {
         obj.message = this.draft;
       }
@@ -409,55 +386,6 @@
       if (total == 0) { return ''; }
       if (total == 1) { return '1 Draft'; }
       if (total > 1) { return total + ' Drafts'; }
-    },
-
-    _computeLabelValueTitle: function(labels, label, value) {
-      return labels[label] && labels[label].values[value];
-    },
-
-    _computeLabels: function(labelRecord) {
-      var labelsObj = labelRecord.base;
-      if (!labelsObj) { return []; }
-      return Object.keys(labelsObj).sort().map(function(key) {
-        return {
-          name: key,
-          value: this._getVoteForAccount(labelsObj, key, this._account),
-        };
-      }.bind(this));
-    },
-
-    _getVoteForAccount: function(labels, labelName, account) {
-      var votes = labels[labelName];
-      if (votes.all && votes.all.length > 0) {
-        for (var i = 0; i < votes.all.length; i++) {
-          if (votes.all[i]._account_id == account._account_id) {
-            return votes.all[i].value;
-          }
-        }
-      }
-      return null;
-    },
-
-    _computeIndexOfLabelValue: function(labels, permittedLabels, label) {
-      if (!labels[label.name]) { return null; }
-      var labelValue = label.value;
-      var len = permittedLabels[label.name] != null ?
-          permittedLabels[label.name].length : 0;
-      for (var i = 0; i < len; i++) {
-        var val = parseInt(permittedLabels[label.name][i], 10);
-        if (val == labelValue) {
-          return i;
-        }
-      }
-      return null;
-    },
-
-    _computePermittedLabelValues: function(permittedLabels, label) {
-      return permittedLabels[label];
-    },
-
-    _computeAnyPermittedLabelValues: function(permittedLabels, label) {
-      return permittedLabels.hasOwnProperty(label);
     },
 
     _changeUpdated: function(changeRecord, owner, serverConfig) {

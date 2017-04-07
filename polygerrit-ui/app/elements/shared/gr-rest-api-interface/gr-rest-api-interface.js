48c5aab34b3f00c0291aad5b306ece540c1c121a
Paladox none <thomasmulhall410@yahoo.com>
2017-04-07 12:40:21 +0000

    Show a list of projects on /admin/projects page
    When going to /admin/projects/test for example it will take you back
    to the old ui to view the project configs.
    
    Bug: Issue 5966
    Change-Id: I7d6fb8f17a235602bba0c535cfe4b43030f515a3

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -549,6 +549,10 @@
       });
     },
 
+    getProjects: function() {
+      return this.fetchJSON('/projects/');
+    },
+
     getSuggestedGroups: function(inputVal, opt_n, opt_errFn, opt_ctx) {
       var params = {s: inputVal};
       if (opt_n) { params.n = opt_n; }

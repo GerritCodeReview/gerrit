d00428d7253f8d20493705f81f7ff4b127041d14
Paladox none <thomasmulhall410@yahoo.com>
2017-04-07 12:40:21 +0000

    Show a list of projects on /admin/projects page
    When going to /admin/projects/test for example it will take you back
    to the old ui to view the project configs.
    
    Bug: Issue 5966
    Change-Id: I7d6fb8f17a235602bba0c535cfe4b43030f515a3

---
+++ polygerrit-ui/app/elements/admin/gr-admin-project-list/gr-admin-project-list.js
@@ -0,0 +1,71 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-admin-project-list',
+
+    properties: {
+      _projects: Array,
+    },
+
+    behaviors: [
+      Gerrit.BaseUrlBehavior,
+    ],
+
+    attached: function() {
+      this._loadData();
+    },
+
+    _loadData: function() {
+      return this.$.restAPI.getProjects().then(function(projects) {
+        if (!projects) {
+          this._projects = [];
+          return;
+        }
+        this._projects = Object.keys(projects)
+            .map(function(key) {
+              var project = projects[key];
+              project.name = key;
+              return project;
+            });
+      }.bind(this));
+    },
+
+    _getUrl: function(item) {
+      return this.getBaseUrl() + '/admin/projects/' + item;
+    },
+
+    _computeWeblinkUrl: function(project) {
+      if (!project.web_links) {
+        return '';
+      }
+      for (var index = 0; index < project.web_links.length; index++) {
+        var test = project.web_links[index];
+      }
+      return test.url;
+    },
+
+    _computeWeblinkName: function(project) {
+      if (!project.web_links) {
+        return '';
+      }
+      for (var index = 0; index < project.web_links.length; index++) {
+        var test = project.web_links[index];
+      }
+      return '(' + test.name + ')';
+    },
+  });
+})();

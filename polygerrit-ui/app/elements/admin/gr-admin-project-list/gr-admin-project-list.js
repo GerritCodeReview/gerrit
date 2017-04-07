5c2aea7eba670b87bc93c505fdd3a464aa83c4b5
Paladox none <thomasmulhall410@yahoo.com>
2017-04-07 12:40:21 +0000

    PolyGerrit: Show a list of projects on /admin/projects page
    When going to /admin/projects/test for example it will take you back
    to the old ui to view the project configs.
    
    Bug: Issue 5966
    Change-Id: I7d6fb8f17a235602bba0c535cfe4b43030f515a3

---
+++ polygerrit-ui/app/elements/admin/gr-admin-project-list/gr-admin-project-list.js
@@ -0,0 +1,137 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-admin-project-list',
+
+    properties: {
+      /**
+       * URL params passed from the router.
+       */
+      params: {
+        type: Object,
+        observer: '_paramsChanged',
+      },
+
+      /**
+       * Offset of currently visible query results.
+       */
+      _offset: Number,
+
+      _projects: Array,
+
+      _projectsPerPage: {
+        type: Number,
+        value: 25,
+      },
+
+      _loading: {
+        type: Boolean,
+        value: true,
+      },
+    },
+
+    behaviors: [
+      Gerrit.BaseUrlBehavior,
+    ],
+
+    listeners: {
+      'next-page': '_handleNextPage',
+      'previous-page': '_handlePreviousPage',
+    },
+
+    _paramsChanged(value) {
+      this._loading = true;
+
+      if (value && value.offset) {
+        this._offset = value.offset;
+      } else {
+        this._offset = 0;
+      }
+
+      return this.$.restAPI.getProjects(this._projectsPerPage, this._offset)
+          .then(projects => {
+            if (!projects) {
+              this._projects = [];
+              return;
+            }
+            this._projects = Object.keys(projects)
+             .map(key => {
+               const project = projects[key];
+               project.name = key;
+               return project;
+             });
+            this._loading = false;
+          });
+    },
+
+    _readOnly(item) {
+      return item.state === 'READ_ONLY' ? 'Y' : 'N';
+    },
+
+    _getUrl(item) {
+      return this.getBaseUrl() + '/admin/projects/' + item;
+    },
+
+    _isProjectWebLink(link) {
+      return link.name === 'gitiles' || link.name === 'gitweb';
+    },
+
+    _computeWeblink(project) {
+      if (!project.web_links) {
+        return '';
+      }
+      const webLinks = project.web_links.filter(
+          l => !this._isProjectWebLink(l));
+      return webLinks.length ? webLinks : null;
+    },
+
+    _computeNavLink(offset, direction, projectsPerPage) {
+      // Offset could be a string when passed from the router.
+      offset = +(offset || 0);
+      const newOffset = Math.max(0, offset + (projectsPerPage * direction));
+      let href = this.getBaseUrl() + '/admin/projects';
+      if (newOffset > 0) {
+        href += ',' + newOffset;
+      }
+      return href;
+    },
+
+    _hidePrevArrow(offset) {
+      return offset === 0;
+    },
+
+    _hideNextArrow(loading, projects, projectsPerPage) {
+      let lastPage = false;
+      if (projects.length < projectsPerPage - 1) {
+        lastPage = true;
+      }
+      return loading || lastPage || !projects || !projects.length;
+    },
+
+    _handleNextPage() {
+      if (this.$.nextArrow.hidden) { return; }
+      page.show(this._computeNavLink(
+          this._offset, 1, this._projectsPerPage));
+    },
+
+    _handlePreviousPage() {
+      if (this.$.prevArrow.hidden) { return; }
+      page.show(this._computeNavLink(
+          this._offset, -1, this._projectsPerPage));
+    },
+  });
+})();

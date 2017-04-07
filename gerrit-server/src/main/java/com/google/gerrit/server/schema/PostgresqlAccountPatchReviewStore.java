2261204d7a3f77f8d8702c0fc3afb996cd51099b
Hongkai Liu <hongkai.liu@ericsson.com>
2017-04-07 16:54:25 -0400

    Support Jdbc implementation of AccountPatchReviewStore
    Provide in gerrit core support for alternative database backends
    for AccountPatchReview store.
    
    H2 was used for storing AccountPatchReview. This can become
    problematic in large installation since embedded H2 cannot execute
    concurrent requests even to the same database. The options related to
    multi-thread in H2 are:
    - MULTI_THREADED: a long running query blocks the ones in other
    threads [1].
    - MVCC: This is not production-ready yet, since it is not fully
    tested [2].
    
    AccountPatchReviewStore is an extension and a plugin could
    implement a jdbc store but this would result in duplicated code
    with core H2 implementation. Since core H2 implementation was
    almost already compliant to jdbc standard, it's easy to make
    jdbc url configurable in gerrit config and make the core support
    other database than H2.
    
    One could extend the H2 implementation in plugin, however the
    changes on the supper class could lead to breaking plugin
    implementation.
    
    [1] http://www.h2database.com/html/features.html#multiple_connections
    [2] http://www.h2database.com/html/advanced.html#mvcc
    
    Change-Id: Ie61ac63a72cab71bd907886a794541cb3dea291e

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/PostgresqlAccountPatchReviewStore.java
@@ -0,0 +1,54 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.schema;
+
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.gwtorm.server.OrmDuplicateKeyException;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import org.eclipse.jgit.lib.Config;
+
+import java.sql.SQLException;
+
+@Singleton
+public class PostgresqlAccountPatchReviewStore extends JdbcAccountPatchReviewStore {
+
+  @Inject
+  PostgresqlAccountPatchReviewStore(@GerritServerConfig Config cfg,
+      SitePaths sitePaths) {
+    super(cfg, sitePaths);
+  }
+
+  @Override
+  public OrmException convertError(String op, SQLException err) {
+    switch (getSQLStateInt(err)) {
+      case 23505: // DUPLICATE_KEY_1
+        return new OrmDuplicateKeyException("ACCOUNT_PATCH_REVIEWS", err);
+
+      case 23514: // CHECK CONSTRAINT VIOLATION
+      case 23503: // FOREIGN KEY CONSTRAINT VIOLATION
+      case 23502: // NOT NULL CONSTRAINT VIOLATION
+      case 23001: // RESTRICT VIOLATION
+      default:
+        if (err.getCause() == null && err.getNextException() != null) {
+          err.initCause(err.getNextException());
+        }
+        return new OrmException(op + " failure on ACCOUNT_PATCH_REVIEWS", err);
+    }
+  }
+}

cb7a5f2e72980a04e2236014daf9517482150c3c
Hongkai Liu <hongkai.liu@ericsson.com>
2017-04-07 16:54:25 -0400

    Support Jdbc implementation of AccountPatchReviewStore
    Provide in gerrit core support for alternative database backends
    for AccountPatchReview store.
    
    H2 was used for storing AccountPatchReview. This can become
    problematic in large installation since embedded H2 cannot execute
    concurrent requests even to the same database. The options related to
    multi-thread in H2 are:
    - MULTI_THREADED: a long running query blocks the ones in other
    threads [1].
    - MVCC: This is not production-ready yet, since it is not fully
    tested [2].
    
    AccountPatchReviewStore is an extension and a plugin could
    implement a jdbc store but this would result in duplicated code
    with core H2 implementation. Since core H2 implementation was
    almost already compliant to jdbc standard, it's easy to make
    jdbc url configurable in gerrit config and make the core support
    other database than H2.
    
    One could extend the H2 implementation in plugin, however the
    changes on the supper class could lead to breaking plugin
    implementation.
    
    [1] http://www.h2database.com/html/features.html#multiple_connections
    [2] http://www.h2database.com/html/advanced.html#mvcc
    
    Change-Id: Ie61ac63a72cab71bd907886a794541cb3dea291e

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/Daemon.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/Daemon.java
@@ -74,6 +74,7 @@ import com.google.gerrit.server.plugins.PluginGuiceEnvironment;
 import com.google.gerrit.server.plugins.PluginRestApiModule;
 import com.google.gerrit.server.schema.DataSourceProvider;
 import com.google.gerrit.server.schema.H2AccountPatchReviewStore;
+import com.google.gerrit.server.schema.JdbcAccountPatchReviewStore;
 import com.google.gerrit.server.schema.SchemaVersionCheck;
 import com.google.gerrit.server.securestore.DefaultSecureStore;
 import com.google.gerrit.server.securestore.SecureStore;
@@ -346,7 +347,7 @@ public class Daemon extends SiteProgram {
     modules.add(new EventBroker.Module());
     modules.add(test
         ? new H2AccountPatchReviewStore.InMemoryModule()
-        : new H2AccountPatchReviewStore.Module());
+        : new JdbcAccountPatchReviewStore.Module(config));
     modules.add(new ReceiveCommitsExecutorModule());
     modules.add(new DiffExecutorModule());
     modules.add(new MimeUtil2Module());

618e59ccb41e726a958cd537542902934522db84
Paladox none <thomasmulhall410@yahoo.com>
2017-04-07 12:40:21 +0000

    Show a list of projects on /admin/projects page
    When going to /admin/projects/test for example it will take you back
    to the old ui to view the project configs.
    
    Bug: Issue 5966
    Change-Id: I7d6fb8f17a235602bba0c535cfe4b43030f515a3

---
+++ polygerrit-ui/app/elements/admin/gr-admin-projects-view/gr-admin-project-list.js
@@ -0,0 +1,46 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-admin-project-list',
+
+    properties: {
+      _project: Array,
+    },
+
+    behaviors: [
+      Gerrit.BaseUrlBehavior,
+    ],
+
+    attached: function() {
+      this.loadData();
+    },
+
+    loadData: function() {
+      return this.$.restAPI.getProjects().then(function(projects) {
+        if (!projects) {
+          return [];
+        }
+        var keys = Object.keys(projects);
+        this._project = keys.map(function(key) { return projects[key]; });
+      }.bind(this));
+    },
+
+    url: function(item) {
+      return this.getBaseUrl() + '/admin/projects/' + item;
+    },
+  });
+})();

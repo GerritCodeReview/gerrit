389d0a73f64c56cc27f88fa19870571c3b7f0429
Edwin Kempin <ekempin@google.com>
2017-04-13 09:23:24 +0200

    Replace AccountAccess#firstNById(int) by looking at user refs
    Remove AccountAccess#firstNById(int) and find the first n accounts by
    looking at the user refs in All-Users instead. Since change I81491a253
    it is ensured that we have a user branch for each account.
    
    This is a preparation step for migrating the accounts from ReviewDb to
    NoteDb.
    
    Change-Id: I0d8a74d6d2de253b4311b8e91517568c1a09f2a8
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/account/Accounts.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/Accounts.java
@@ -14,12 +14,21 @@
 
 package com.google.gerrit.server.account;
 
+import static java.util.stream.Collectors.toList;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.RefNames;
 import com.google.gerrit.server.config.AllUsersName;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 
@@ -36,19 +45,87 @@ public class Accounts {
   }
 
   /**
+   * Returns the first n account IDs.
+   *
+   * @param n the number of account IDs that should be returned
+   * @return first n account IDs
+   */
+  public List<Account.Id> firstNIds(int n) throws IOException {
+    List<Account.Id> accountIds = new ArrayList<>();
+    readUserRefsSortedByAccountId(
+        r -> {
+          accountIds.add(r.accountId());
+          return accountIds.size() < n;
+        });
+    return accountIds;
+  }
+
+  /**
    * Checks if any account exists.
    *
    * @return {@code true} if at least one account exists, otherwise {@code false}
    */
   public boolean hasAnyAccount() throws IOException {
+    AtomicBoolean hasAnyAccount = new AtomicBoolean(false);
+    readUserRefs(
+        r -> {
+          hasAnyAccount.set(true);
+          return false;
+        });
+    return hasAnyAccount.get();
+  }
+
+  private void readUserRefsSortedByAccountId(UserRefConsumer consumer) throws IOException {
+    readUserRefs(true, consumer);
+  }
+
+  private void readUserRefs(UserRefConsumer consumer) throws IOException {
+    readUserRefs(false, consumer);
+  }
+
+  private void readUserRefs(boolean sortByAccountId, UserRefConsumer consumer) throws IOException {
     try (Repository repo = repoManager.openRepository(allUsersName)) {
-      for (Ref ref : repo.getRefDatabase().getRefs(RefNames.REFS_USERS).values()) {
-        if (RefNames.REFS_USERS_DEFAULT.equals(ref.getName())) {
-          continue;
-        }
-        return true;
+      Collection<Ref> userRefs = repo.getRefDatabase().getRefs(RefNames.REFS_USERS).values();
+      Stream<UserRef> userRefStream =
+          userRefs
+              .stream()
+              .filter(r -> !RefNames.REFS_USERS_DEFAULT.equals(r.getName()))
+              .map(UserRef::create);
+      if (sortByAccountId) {
+        userRefStream = userRefStream.sorted((r1, r2) -> r1.compareTo(r2));
       }
-      return false;
+      for (UserRef userRef : userRefStream.collect(toList())) {
+        if (!consumer.accept(userRef)) {
+          return;
+        }
+      }
+    }
+  }
+
+  @FunctionalInterface
+  private static interface UserRefConsumer {
+    /**
+     * Consumes a user ref.
+     *
+     * @param userRef user ref
+     * @return whether further user refs should be read
+     */
+    boolean accept(UserRef userRef) throws IOException;
+  }
+
+  @AutoValue
+  abstract static class UserRef implements Comparable<UserRef> {
+    static UserRef create(Ref ref) {
+      return new AutoValue_Accounts_UserRef(ref, Account.Id.fromRef(ref.getName()));
+    }
+
+    abstract Ref ref();
+
+    abstract Account.Id accountId();
+
+    @Override
+    public int compareTo(UserRef other) {
+      return accountId().get() - other.accountId().get();
     }
   }
 }

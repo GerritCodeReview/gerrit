7e218ddcaf71e7722848c79522e8ab9be818b7b2
Becky Siegel <beckysiegel@google.com>
2017-04-13 21:14:37 -0700

    Fix comment navigation in file list
    Previously, the check for modifiers came before the check for shift,
    so jumping to the next comment thread did not work properly.
    
    This change checks for modifiers, so long as it is not the shift key,
    and also checks for shift in the event detail and in the
    keyboardEvent, as is done in the check for meta keypress in general.
    
    Bug: Issue 6010
    Change-Id: Iadfda322400d59450b62c879d55ed5a6e3f233df

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
@@ -527,11 +527,13 @@
 
     _handleNKey: function(e) {
       if (this.shouldSuppressKeyboardShortcut(e) ||
-          this.modifierPressed(e)) { return; }
+          this.modifierPressed(e) && !this.isModifierPressed(e, 'shiftKey')) {
+        return;
+      }
       if (!this._showInlineDiffs) { return; }
 
       e.preventDefault();
-      if (e.shiftKey) {
+      if (this.isModifierPressed(e, 'shiftKey')) {
         this.$.diffCursor.moveToNextCommentThread();
       } else {
         this.$.diffCursor.moveToNextChunk();
@@ -540,11 +542,13 @@
 
     _handlePKey: function(e) {
       if (this.shouldSuppressKeyboardShortcut(e) ||
-          this.modifierPressed(e)) { return; }
+          this.modifierPressed(e) && !this.isModifierPressed(e, 'shiftKey')) {
+        return;
+      }
       if (!this._showInlineDiffs) { return; }
 
       e.preventDefault();
-      if (e.shiftKey) {
+      if (this.isModifierPressed(e, 'shiftKey')) {
         this.$.diffCursor.moveToPreviousCommentThread();
       } else {
         this.$.diffCursor.moveToPreviousChunk();

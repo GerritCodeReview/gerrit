3a8c34c42f7dcade529f81656e786b2dad7b3c9a
Wyatt Allen <wyatta@google.com>
2017-04-18 22:36:43 +0000

    Revert "NoteDbUpdateManager: Remove unused flush method"
    This reverts commit 4004a6860988debb455e4a75f810d9b5ec559963.
    
    Reason for revert: There is an internal caller for flush.
    
    Change-Id: I193dcc12f9e89178489a0b19c111dded005cf5eb

--- gerrit-server/src/main/java/com/google/gerrit/server/notedb/NoteDbUpdateManager.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/notedb/NoteDbUpdateManager.java
@@ -455,6 +455,15 @@ public class NoteDbUpdateManager implements AutoCloseable {
     return draftIds;
   }
 
+  public void flush() throws IOException {
+    if (changeRepo != null) {
+      changeRepo.flush();
+    }
+    if (allUsersRepo != null) {
+      allUsersRepo.flush();
+    }
+  }
+
   @Nullable
   public BatchRefUpdate execute() throws OrmException, IOException {
     return execute(false);

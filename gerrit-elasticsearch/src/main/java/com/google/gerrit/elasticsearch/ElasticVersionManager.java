74bb6d61843a2e2911514e3ae79050caeaa5072a
Dariusz Luksza <dluksza@collab.net>
2017-04-24 16:34:52 +0200

    ES: Implement online reindex for ElasticSearch
    Implement online reindexing for ElasticSearch based on the code for
    Lucene online reindex.
    
    Testing scenario:
     1. Start fresh Gerrit site with this patch
     2. Create account
     3. Verify data in ElasticSearch:
       curl http://localhost:9200/gerritaccounts_0004/
       curl http://localhost:9200/gerritaccounts_0004/_search
     4. Stop Gerrit
     5. Cherry pick change I77e1643cd1a7fbef9f4d2fa214823759188e9592
     6. Start Gerrit
     6. Wait for log message:
         Starting online reindex from schema version 4 to 5
     7. Verify state in ElasticSearch:
       curl http://localhost:9200/gerritaccounts_0005/
       curl http://localhost:9200/gerritaccounts_0005/_search
    
    Entry for user account created in step 2 should have "elastic_online"
    property with value "reindex work".
    
    Change-Id: I9efcf5735e65b4f2dc2a97914d398f81656fc12a

--- gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticVersionManager.java
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticVersionManager.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2013 The Android Open Source Project
+// Copyright (C) 2017 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,21 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.lucene;
+package com.google.gerrit.elasticsearch;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.primitives.Ints;
 import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.config.SitePaths;
-import com.google.gerrit.server.index.GerritIndexStatus;
 import com.google.gerrit.server.index.Index;
 import com.google.gerrit.server.index.IndexCollection;
 import com.google.gerrit.server.index.IndexDefinition;
 import com.google.gerrit.server.index.IndexDefinition.IndexFactory;
+import com.google.gerrit.server.index.IndexUtils;
 import com.google.gerrit.server.index.OnlineReindexer;
 import com.google.gerrit.server.index.ReindexerAlreadyRunningException;
 import com.google.gerrit.server.index.Schema;
@@ -34,58 +35,53 @@ import com.google.inject.Inject;
 import com.google.inject.ProvisionException;
 import com.google.inject.Singleton;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-public class LuceneVersionManager implements LifecycleListener {
-  private static final Logger log = LoggerFactory.getLogger(LuceneVersionManager.class);
+public class ElasticVersionManager implements LifecycleListener {
+  private static final Logger log = LoggerFactory.getLogger(ElasticVersionManager.class);
 
   private static class Version<V> {
     private final Schema<V> schema;
     private final int version;
-    private final boolean exists;
     private final boolean ready;
 
-    private Version(Schema<V> schema, int version, boolean exists, boolean ready) {
+    private Version(Schema<V> schema, int version, boolean ready) {
       checkArgument(schema == null || schema.getVersion() == version);
       this.schema = schema;
       this.version = version;
-      this.exists = exists;
       this.ready = ready;
     }
   }
 
-  static Path getDir(SitePaths sitePaths, String prefix, Schema<?> schema) {
-    return sitePaths.index_dir.resolve(String.format("%s%04d", prefix, schema.getVersion()));
-  }
-
-  private final SitePaths sitePaths;
   private final Map<String, IndexDefinition<?, ?, ?>> defs;
   private final Map<String, OnlineReindexer<?, ?, ?>> reindexers;
+  private final ElasticIndexVersionDiscovery versionDiscovery;
+  private final SitePaths sitePaths;
   private final boolean onlineUpgrade;
   private final String runReindexMsg;
+  private final String prefix;
 
   @Inject
-  LuceneVersionManager(
+  ElasticVersionManager(
       @GerritServerConfig Config cfg,
       SitePaths sitePaths,
-      Collection<IndexDefinition<?, ?, ?>> defs) {
+      Collection<IndexDefinition<?, ?, ?>> defs,
+      ElasticIndexVersionDiscovery versionDiscovery) {
     this.sitePaths = sitePaths;
+    this.versionDiscovery = versionDiscovery;
     this.defs = Maps.newHashMapWithExpectedSize(defs.size());
     for (IndexDefinition<?, ?, ?> def : defs) {
       this.defs.put(def.getName(), def);
     }
 
+    prefix = MoreObjects.firstNonNull(cfg.getString("index", null, "prefix"), "gerrit");
     reindexers = Maps.newHashMapWithExpectedSize(defs.size());
     onlineUpgrade = cfg.getBoolean("index", null, "onlineUpgrade", true);
     runReindexMsg =
@@ -96,28 +92,20 @@ public class LuceneVersionManager implements LifecycleListener {
 
   @Override
   public void start() {
-    GerritIndexStatus cfg;
     try {
-      cfg = new GerritIndexStatus(sitePaths);
-    } catch (ConfigInvalidException | IOException e) {
-      throw fail(e);
-    }
-
-    if (!Files.exists(sitePaths.index_dir)) {
-      throw new ProvisionException(runReindexMsg);
-    } else if (!Files.exists(sitePaths.index_dir)) {
-      log.warn("Not a directory: %s", sitePaths.index_dir.toAbsolutePath());
-      throw new ProvisionException(runReindexMsg);
-    }
-
-    for (IndexDefinition<?, ?, ?> def : defs.values()) {
-      initIndex(def, cfg);
+      for (IndexDefinition<?, ?, ?> def : defs.values()) {
+        initIndex(def);
+      }
+    } catch (IOException e) {
+      ProvisionException ex = new ProvisionException("Error scanning indexes");
+      ex.initCause(e);
+      throw ex;
     }
   }
 
-  private <K, V, I extends Index<K, V>> void initIndex(
-      IndexDefinition<K, V, I> def, GerritIndexStatus cfg) {
-    TreeMap<Integer, Version<V>> versions = scanVersions(def, cfg);
+  private <K, V, I extends Index<K, V>> void initIndex(IndexDefinition<K, V, I> def)
+      throws IOException {
+    TreeMap<Integer, Version<V>> versions = scanVersions(def);
     // Search from the most recent ready version.
     // Write to the most recent ready version and the most recent version.
     Version<V> search = null;
@@ -155,12 +143,12 @@ public class LuceneVersionManager implements LifecycleListener {
       }
     }
 
-    markNotReady(cfg, def.getName(), versions.values(), write);
+    markNotReady(def.getName(), versions.values(), write);
 
-    int latest = write.get(0).version;
-    OnlineReindexer<K, V, I> reindexer = new OnlineReindexer<>(def, latest);
     synchronized (this) {
       if (!reindexers.containsKey(def.getName())) {
+        int latest = write.get(0).version;
+        OnlineReindexer<K, V, I> reindexer = new OnlineReindexer<>(def, latest);
         reindexers.put(def.getName(), reindexer);
         if (onlineUpgrade && latest != search.version) {
           reindexer.start();
@@ -172,6 +160,7 @@ public class LuceneVersionManager implements LifecycleListener {
   /**
    * Start the online reindexer if the current index is not already the latest.
    *
+   * @param name index name
    * @param force start re-index
    * @return true if started, otherwise false.
    * @throws ReindexerAlreadyRunningException
@@ -180,7 +169,7 @@ public class LuceneVersionManager implements LifecycleListener {
       throws ReindexerAlreadyRunningException {
     OnlineReindexer<?, ?, ?> reindexer = reindexers.get(name);
     validateReindexerNotRunning(reindexer);
-    if (force || !isCurrentIndexVersionLatest(name, reindexer)) {
+    if (force || !isLatestIndexVersion(name, reindexer)) {
       reindexer.start();
       return true;
     }
@@ -190,21 +179,22 @@ public class LuceneVersionManager implements LifecycleListener {
   /**
    * Activate the latest index if the current index is not already the latest.
    *
-   * @return true if index was activate, otherwise false.
+   * @param name index name
+   * @return true if index was activated, otherwise false.
    * @throws ReindexerAlreadyRunningException
    */
   public synchronized boolean activateLatestIndex(String name)
       throws ReindexerAlreadyRunningException {
     OnlineReindexer<?, ?, ?> reindexer = reindexers.get(name);
     validateReindexerNotRunning(reindexer);
-    if (!isCurrentIndexVersionLatest(name, reindexer)) {
+    if (!isLatestIndexVersion(name, reindexer)) {
       reindexer.activateIndex();
       return true;
     }
     return false;
   }
 
-  private boolean isCurrentIndexVersionLatest(String name, OnlineReindexer<?, ?, ?> reindexer) {
+  private boolean isLatestIndexVersion(String name, OnlineReindexer<?, ?, ?> reindexer) {
     int readVersion = defs.get(name).getIndexCollection().getSearchIndex().getSchema().getVersion();
     return reindexer == null || reindexer.getVersion() == readVersion;
   }
@@ -217,67 +207,41 @@ public class LuceneVersionManager implements LifecycleListener {
   }
 
   private <K, V, I extends Index<K, V>> TreeMap<Integer, Version<V>> scanVersions(
-      IndexDefinition<K, V, I> def, GerritIndexStatus cfg) {
+      IndexDefinition<K, V, I> def) throws IOException {
     TreeMap<Integer, Version<V>> versions = new TreeMap<>();
     for (Schema<V> schema : def.getSchemas().values()) {
-      // This part is Lucene-specific.
-      Path p = getDir(sitePaths, def.getName(), schema);
-      boolean isDir = Files.isDirectory(p);
-      if (Files.exists(p) && !isDir) {
-        log.warn("Not a directory: %s", p.toAbsolutePath());
-      }
       int v = schema.getVersion();
-      versions.put(v, new Version<>(schema, v, isDir, cfg.getReady(def.getName(), v)));
+      versions.put(
+          v,
+          new Version<>(
+              schema, v, IndexUtils.getReady(sitePaths, def.getName(), schema.getVersion())));
     }
 
-    String prefix = def.getName() + "_";
-    try (DirectoryStream<Path> paths = Files.newDirectoryStream(sitePaths.index_dir)) {
-      for (Path p : paths) {
-        String n = p.getFileName().toString();
-        if (!n.startsWith(prefix)) {
-          continue;
-        }
-        String versionStr = n.substring(prefix.length());
-        Integer v = Ints.tryParse(versionStr);
-        if (v == null || versionStr.length() != 4) {
-          log.warn("Unrecognized version in index directory: {}", p.toAbsolutePath());
+    try {
+      for (String version : versionDiscovery.discover(prefix, def.getName())) {
+        Integer v = Ints.tryParse(version);
+        if (v == null || version.length() != 4) {
+          log.warn("Unrecognized version in index {}: {}", def.getName(), version);
           continue;
         }
         if (!versions.containsKey(v)) {
-          versions.put(v, new Version<V>(null, v, true, cfg.getReady(def.getName(), v)));
+          versions.put(
+              v, new Version<V>(null, v, IndexUtils.getReady(sitePaths, def.getName(), v)));
         }
       }
     } catch (IOException e) {
-      log.error("Error scanning index directory: " + sitePaths.index_dir, e);
+      log.error("Error scanning index: " + def.getName(), e);
     }
     return versions;
   }
 
   private <V> void markNotReady(
-      GerritIndexStatus cfg,
-      String name,
-      Iterable<Version<V>> versions,
-      Collection<Version<V>> inUse) {
-    boolean dirty = false;
+      String name, Iterable<Version<V>> versions, Collection<Version<V>> inUse) throws IOException {
     for (Version<V> v : versions) {
-      if (!inUse.contains(v) && v.exists) {
-        cfg.setReady(name, v.version, false);
-        dirty = true;
+      if (!inUse.contains(v)) {
+        IndexUtils.getReady(sitePaths, name, v.version);
       }
     }
-    if (dirty) {
-      try {
-        cfg.save();
-      } catch (IOException e) {
-        throw fail(e);
-      }
-    }
-  }
-
-  private ProvisionException fail(Throwable t) {
-    ProvisionException e = new ProvisionException("Error scanning indexes");
-    e.initCause(t);
-    throw e;
   }
 
   @Override

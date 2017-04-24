24482deaeb24f9ca7ec3fa6ec076a7225ca87a2e
Shawn Pearce <sop@google.com>
2017-04-24 15:46:50 +0200

    Hide ChangeControl.isReviewer, isAssignee
    The single-arg isReviewer method is not called by Gerrit.  The two-arg
    method is only used by ChangeControl, and can be private.
    
    isAssignee is only used by ChangeControl.
    
    Change-Id: Ic708851073e3c0bede7f160d9d8cb4e55f2770d2

--- gerrit-server/src/main/java/com/google/gerrit/server/project/ChangeControl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/ChangeControl.java
@@ -369,7 +369,7 @@ public class ChangeControl {
   }
 
   /** Is this user assigned to this change? */
-  public boolean isAssignee() {
+  private boolean isAssignee() {
     Account.Id currentAssignee = notes.getChange().getAssignee();
     if (currentAssignee != null && getUser().isIdentifiedUser()) {
       Account.Id id = getUser().getAccountId();
@@ -379,12 +379,7 @@ public class ChangeControl {
   }
 
   /** Is this user a reviewer for the change? */
-  public boolean isReviewer(ReviewDb db) throws OrmException {
-    return isReviewer(db, null);
-  }
-
-  /** Is this user a reviewer for the change? */
-  public boolean isReviewer(ReviewDb db, @Nullable ChangeData cd) throws OrmException {
+  private boolean isReviewer(ReviewDb db, @Nullable ChangeData cd) throws OrmException {
     if (getUser().isIdentifiedUser()) {
       Collection<Account.Id> results = changeData(db, cd).reviewers().all();
       return results.contains(getUser().getAccountId());

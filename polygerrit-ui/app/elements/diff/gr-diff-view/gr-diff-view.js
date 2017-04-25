eb160e2eb7af72cee427aecfc44f5a817b9f780f
David Ostrovsky <david@ostrovsky.org>
2017-04-25 23:48:05 +0200

    Render change edits on change and diff screens
    This change aims making change view and diff view change edit aware.
    Mutation operations on edit itself, like editing files in editor, or
    different actions on the edit, like rebase, publish or delete edit
    are beyond the scope of this change.
    
    To render change edit it must be fetched with change edit specific REST
    endpoint and merge into change.revisions object. Once complications, is
    that there is no patch set for change edit, and the patch set number is
    0. However, the user shouldn't really know that change edit
    representation is virtual patch set number 0, so that all places, we
    usually show 'edit' in the UI. This complicates the implementations,
    because the comparison in many parts of the UI is done based on the
    selected patch set (edit in change edit case), but the
    change.revisions[edit_commit]._number === 0.
    
    Alternative implementation considerations:
    
    It could be easier to handle edits on the Polygerrit UI, if
    
      GET /changes/<id>/detail endpoint
    
    would optionally include the change edit in the resulting
    change.revsions map. Another simplification would be to consistently
    show '0' as edit in the patch set selection drop down on different
    views in the UI.
    
    TODOs:
    
    * Overwrite change.current_revision with change edit commit in some
      use cases
    * Mark the change edit as Change Edit in the header of the change view
    * Write tests
    
    Test Plan:
    
    * upload change edit based on the latest patch set of the change
    
    Change edit rendering on the change view:
    
    * 'edit' is included in the change view, in patch set drop down
    * inline diff correctly shows change edit changes only when compared
    against the patch set the change edit is based on
    * download command overlay correctly renders change edit specific
      download commands for configured schemes on the gerrit site
    
    Change edit rendering on the diff view:
    
    * 'edit' is included in the diff view, in patch set drop down
    * change edit changes only are correctly shown, when compared against
      the patch set the change edit is based on
    
    Bug: Issue 4437
    Change-Id: Iafd4b80af53624027c347f0f443b4cdfde292e29

--- polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.js
+++ polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.js
@@ -109,6 +109,7 @@
     behaviors: [
       Gerrit.BaseUrlBehavior,
       Gerrit.KeyboardShortcutBehavior,
+      Gerrit.PatchSetBehavior,
       Gerrit.RESTClientBehavior,
       Gerrit.URLEncodingBehavior,
     ],
@@ -458,10 +459,23 @@
 
       promises.push(this._getChangeDetail(this._changeNum));
 
-      Promise.all(promises).then(function() {
+      if (this._patchRange.patchNum === 'edit' ||
+          this._patchRange.basePatchNum === 'edit') {
+        promises.push(this.$.restAPI.getChangeEdit(this._changeNum));
+      }
+
+      Promise.all(promises).then(r => {
+        const [, , , edit] = r;
+        if (edit) {
+          this.set('_change.revisions.' + edit.commit.commit, {
+            _number: 0,
+            basePatchSetNumber: edit.base_patch_set_number,
+            commit: edit.commit,
+          });
+        }
         this._loading = false;
         this.$.diff.reload();
-      }.bind(this));
+      });
 
       this._loadCommentMap().then(function(commentMap) {
         this._commentMap = commentMap;
@@ -512,12 +526,9 @@
       return patchStr;
     },
 
-    _computeAvailablePatches: function(revisions) {
-      var patchNums = [];
-      for (var rev in revisions) {
-        patchNums.push(revisions[rev]._number);
-      }
-      return patchNums.sort(function(a, b) { return a - b; });
+    _computeAvailablePatches(revs) {
+      return this.sortRevisions(Object.values(revs)).map(e =>
+          e._number === 0 ? 'edit' : e._number);
     },
 
     _getChangePath: function(changeNum, patchRange, revisions) {

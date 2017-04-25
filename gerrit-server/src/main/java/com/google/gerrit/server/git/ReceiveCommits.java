acb8ba85740a1efd8e5482f8d98a6c65dae3061e
Alan Tokaev <tokaev@gmail.com>
2017-04-25 11:01:29 +0200

    Disallow submitting new draft changes
    Change-Id: I8663f0580b90d7ce10a597b42abecf8734d3f9b2

--- gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/git/ReceiveCommits.java
@@ -52,7 +52,6 @@ import com.google.gerrit.common.TimeUtil;
 import com.google.gerrit.common.data.Capable;
 import com.google.gerrit.common.data.LabelType;
 import com.google.gerrit.common.data.LabelTypes;
-import com.google.gerrit.common.data.Permission;
 import com.google.gerrit.common.data.PermissionRule;
 import com.google.gerrit.extensions.api.changes.HashtagsInput;
 import com.google.gerrit.extensions.api.changes.NotifyHandling;
@@ -1564,17 +1563,9 @@ public class ReceiveCommits {
     }
 
     if (magicBranch.draft) {
-      if (!receiveConfig.allowDrafts) {
-        errors.put(Error.CODE_REVIEW, ref);
-        reject(cmd, "draft workflow is disabled");
-        return;
-      } else if (projectControl
-          .controlForRef(MagicBranch.NEW_DRAFT_CHANGE + ref)
-          .isBlocked(Permission.PUSH)) {
-        errors.put(Error.CODE_REVIEW, ref);
-        reject(cmd, "cannot upload drafts");
-        return;
-      }
+      errors.put(Error.CODE_REVIEW, ref);
+      reject(cmd, "Creation of draft refs is not allowed.");
+      return;
     }
 
     if (!magicBranch.ctl.canUpload()) {

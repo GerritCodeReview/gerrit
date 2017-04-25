fe2d292bbae3866ea652db8d58e1ca030ad54c0d
Matthias Sohn <matthias.sohn@sap.com>
2017-04-25 17:51:08 +0200

    [RFC] Implement rate limits per user for fetch requests
    This code will be moved into a new plugin.
    
    Guava's RateLimiter implementation class SmoothRateLimiter.SmoothBursty
    allows to collect permits during idle times which can be used to send
    bursts of requests exceeding the average rate until the stored permits
    are consumed. If the rate per second is 0.2 and you wait 20 seconds you
    can acquire 4 permits which in average matches the configured rate limit
    of 0.2 requests/second.
    
    Unfortunately the standard implementation doesn't allow any bursts if
    the configured permit rate is smaller than 1 per second since the
    maximum time which can be used to collect stored permits is hard-coded
    to 1 second.
    
    Build jobs fetching updates from Gerrit are typically triggered by
    events which can arrive in bursts. Hence the standard RateLimiter seems
    not to be the right choice for fetch requests we might want to limit to
    a rate of less than 1 request per second per user.
    
    The used constructor can't be accessed through a public method yet hence
    use reflection to instantiate it. There is a bug [1] open requesting to
    expose this parameter in the public guava API.
    
    Add a test demonstrating the difference between standard RateLimiter and
    an instance using a value of maxBurstSeconds larger than 1 second. This
    demo should be removed before submitting this change.
    
    [1] https://github.com/google/guava/issues/1974
    
    Change-Id: I0c2d66b14a88d10505e89e19c903510e7a50426a

---
+++ gerrit-server/src/test/java/com/google/gerrit/server/git/validators/RateLimiterDemoTest.java
@@ -0,0 +1,79 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git.validators;
+
+import org.junit.Test;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import junit.framework.TestCase;
+
+/**
+ * TODO remove this class before submitting this change
+ *
+ * <p>This is no test but a demo of the difference between default RateLimiter and one with a higher
+ * maxBurstSeconds which can only be used by accessing the non-public constructor of the hidden
+ * implementation class using reflection.
+ *
+ * <p>RateLimiter allows to store unused permits earned by not consuming permits for a while. These
+ * stored permits can be used to send bursts of requests exceeding the average rate until these
+ * stored permits are consumed. If the permitted rate is smaller than 1.0 the default implementation
+ * doesn't allow for any bursts since it hard-codes the maximum time which can be used to collect
+ * stored permits to 1 seconds.
+ */
+public class RateLimiterDemoTest extends TestCase {
+  private RateLimiter limiter;
+  private static double permitsPerSecond = 0.5;
+
+  @Test
+  public void testBurstyRateLimiter() throws Exception {
+    int maxBurstSeconds = 10;
+    limiter =
+        RateLimitUploadListener.createSmoothBurstyRateLimiter(permitsPerSecond, maxBurstSeconds);
+    explore(permitsPerSecond, maxBurstSeconds);
+  }
+
+  @Test
+  public void testRateLimiter() throws Exception {
+    limiter = RateLimiter.create(permitsPerSecond);
+    explore(permitsPerSecond, 1);
+  }
+
+  private void explore(double permitsPerSecond, int maxBurstSeconds) throws InterruptedException {
+    int permitInterval = (int) Math.round(1 / permitsPerSecond);
+    System.out.println("limiter: " + limiter.getClass().getName());
+    System.out.println(
+        "permitsPerSecond="
+            + permitsPerSecond
+            + ", maxBurstSeconds="
+            + maxBurstSeconds
+            + ", maxBurstSize="
+            + permitsPerSecond * maxBurstSeconds);
+    int w = permitInterval;
+    do {
+      measure(w);
+      w = w * 2;
+    } while (w < 8 * maxBurstSeconds);
+  }
+
+  private void measure(int waitSeconds) throws InterruptedException {
+    Thread.sleep(waitSeconds * 1000);
+    int i = 0;
+    while (limiter.tryAcquire()) {
+      i++;
+    }
+    System.out.println("wait=" + waitSeconds + "s, permits=" + i);
+  }
+}

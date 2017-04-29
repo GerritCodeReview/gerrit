f25a1ffc905e9ee995fb32eb9c8beafd32cd88cc
Shawn Pearce <sop@google.com>
2017-04-29 11:10:44 -0700

    Replace canUpload with CREATE_CHANGE
    Check RefPermission.CREATE_CHANGE before creating a change, instead of
    relying on the legacy RefControl.canUpload(). This delegates creation
    decisions through the PermissionBackend.
    
    Change-Id: Ic76e96ca6079a179ae65fa2de436fb6df929b6d9

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/CreateChangeIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/CreateChangeIT.java
@@ -46,6 +46,7 @@ import com.google.gerrit.extensions.common.RevisionInfo;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.MethodNotAllowedException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
 import com.google.gerrit.reviewdb.client.Branch;
@@ -195,7 +196,7 @@ public class CreateChangeIT extends AbstractDaemonTest {
 
     ChangeInput in = newChangeInput(ChangeStatus.NEW);
     in.branch = "invisible-branch";
-    assertCreateFails(in, AuthException.class, "cannot upload review");
+    assertCreateFails(in, ResourceNotFoundException.class, "");
   }
 
   @Test

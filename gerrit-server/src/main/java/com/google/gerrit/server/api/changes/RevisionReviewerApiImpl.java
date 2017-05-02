e7d7265ae25c6f7316dfd53a8d4e5584d74ff71c
Dave Borowitz <dborowitz@google.com>
2017-05-02 12:44:45 -0400

    Convert reviewer modify views to retrying helper
    Change-Id: I7c73673386e590c110d79837b77cfc9419699389

--- gerrit-server/src/main/java/com/google/gerrit/server/api/changes/RevisionReviewerApiImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/api/changes/RevisionReviewerApiImpl.java
@@ -14,6 +14,7 @@
 
 package com.google.gerrit.server.api.changes;
 
+import com.google.common.base.Throwables;
 import com.google.gerrit.extensions.api.changes.DeleteVoteInput;
 import com.google.gerrit.extensions.api.changes.RevisionReviewerApi;
 import com.google.gerrit.extensions.restapi.RestApiException;
@@ -21,7 +22,6 @@ import com.google.gerrit.server.change.DeleteVote;
 import com.google.gerrit.server.change.ReviewerResource;
 import com.google.gerrit.server.change.VoteResource;
 import com.google.gerrit.server.change.Votes;
-import com.google.gerrit.server.update.UpdateException;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
@@ -57,8 +57,8 @@ public class RevisionReviewerApiImpl implements RevisionReviewerApi {
   public void deleteVote(String label) throws RestApiException {
     try {
       deleteVote.apply(new VoteResource(reviewer, label), null);
-    } catch (UpdateException e) {
-      throw new RestApiException("Cannot delete vote", e);
+    } catch (Exception e) {
+      throwRestApiException("Cannot delete vote", e);
     }
   }
 
@@ -66,8 +66,14 @@ public class RevisionReviewerApiImpl implements RevisionReviewerApi {
   public void deleteVote(DeleteVoteInput input) throws RestApiException {
     try {
       deleteVote.apply(new VoteResource(reviewer, input.label), input);
-    } catch (UpdateException e) {
-      throw new RestApiException("Cannot delete vote", e);
+    } catch (Exception e) {
+      throwRestApiException("Cannot delete vote", e);
     }
+  }
+
+  private static void throwRestApiException(String msg, Exception e) throws RestApiException {
+    Throwables.throwIfInstanceOf(e, RestApiException.class);
+    Throwables.throwIfUnchecked(e);
+    throw new RestApiException(msg, e);
   }
 }

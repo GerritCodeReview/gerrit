8d3344528993acc5b839109e26a30722e7a3761b
Kasper Nilsson <kaspern@google.com>
2017-05-03 13:48:49 -0700

    Add comment mapping function
    As a component of implementing gr-patch-set-dialog, a new modal that
    presents patch set metadata to the user, a function was added that maps
    the comment data structure to an object that is much more convenient to
    work with in the UI. This function is a part of this new component, but
    can be reviewed orthogonally.
    
    Feature: Issue 5987
    Change-Id: I686199595f6fb8bb335e2b4c4d944fe42579dcd8

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -359,15 +359,6 @@
       this.$.downloadOverlay.close();
     },
 
-    _handlePatchsetTap: function(e) {
-      e.preventDefault();
-      this.$.patchsetOverlay.open();
-    },
-
-    _handlePatchsetDialogClose: function(e) {
-      this.$.patchsetOverlay.close();
-    },
-
     _handleMessageReply: function(e) {
       var msg = e.detail.message.message;
       var quoteStr = msg.split('\n').map(

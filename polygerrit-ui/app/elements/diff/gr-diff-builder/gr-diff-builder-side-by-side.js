5734f3e06f5a552d974e6eb1c7afc45586586050
Alice Kober-Sotzek <aliceks@google.com>
2017-05-05 17:48:31 +0200

    Highlight hunks which are due to rebase on PolyGerrit
    Change-Id: Ifd31c8b2d31d71ff19060da56d81b64954f4fdb7

--- polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-side-by-side.js
+++ polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-side-by-side.js
@@ -29,6 +29,9 @@
     if (this._isTotal(group)) {
       sectionEl.classList.add('total');
     }
+    if (group.dueToRebase) {
+      sectionEl.classList.add('due-to-rebase');
+    }
     var pairs = group.getSideBySidePairs();
     for (var i = 0; i < pairs.length; i++) {
       sectionEl.appendChild(this._createRow(sectionEl, pairs[i].left,

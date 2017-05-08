1f9a24258e1825f0cd2be667bc4ce5e25a411511
Paladox none <thomasmulhall410@yahoo.com>
2017-05-08 14:38:13 +0000

    InitSshd: Renegenerate rsa or dsa keys if they doint exist
    This will only be done if the file ssh_key_host does not exist.
    
    Since you could delete the rsa file by mistake then you will be without
    an rsa key.
    
    Change-Id: I91ce022c736dc30bcd044c6edfaa257b1250727f

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitSshd.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/init/InitSshd.java
@@ -80,7 +80,8 @@ class InitSshd implements InitStep {
   }
 
   private void generateSshHostKeys() throws InterruptedException, IOException {
-    if (!exists(site.ssh_key) && !exists(site.ssh_rsa) && !exists(site.ssh_dsa)
+    if ((!exists(site.ssh_key) && !exists(site.ssh_rsa))
+        || (!exists(site.ssh_key) && !exists(site.ssh_dsa))
         || (!exists(site.ssh_key) && !exists(site.ssh_ed25519))
         || (!exists(site.ssh_key) && !exists(site.ssh_ecdsa))) {
       System.err.print("Generating SSH host key ...");

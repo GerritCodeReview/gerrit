6fdbc8d61de83570b35bed1da41a91642fcd7a1d
Edwin Kempin <ekempin@google.com>
2017-05-09 10:47:22 +0200

    Allow to specify last index version for query tests via env var
    The Lucene query tests run twice, once against the lastest index schema
    version and once against the previous schema version. Now it can be
    configured via env vars against which previous schema version the tests
    are executed.
    
    The following env vars are supported:
    
    - LAST_ACCOUNTS_INDEX_VERSION
    - LAST_CHANGES_INDEX_VERSION
    - LAST_GROUPS_INDEX_VERSION
    
    This way the last index version against which the tests should be
    executed can be specified when running the tests:
    
    bazel test //... --test_env=LAST_CHANGES_INDEX_VERSION=40
    
    This is useful for sites that want to run Gerrit against an older index
    version, such as Gerrit hosts on *.googlesource.com. This allows them to
    verify that everything works with their current index version.
    
    If no last index version is specified by an env var the second last
    index version is used by default.
    
    Change-Id: Ib6d5bd3ae77e3fa0e57a472c58d43e5acc99422c
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/test/java/com/google/gerrit/server/query/account/LuceneQueryAccountsTest.java
+++ gerrit-server/src/test/java/com/google/gerrit/server/query/account/LuceneQueryAccountsTest.java
@@ -14,31 +14,27 @@
 
 package com.google.gerrit.server.query.account;
 
-import com.google.common.collect.Iterables;
 import com.google.gerrit.server.account.AccountState;
-import com.google.gerrit.server.index.Schema;
-import com.google.gerrit.server.index.SchemaUtil;
 import com.google.gerrit.server.index.account.AccountSchemaDefinitions;
+import com.google.gerrit.server.query.LastIndexVersion;
 import com.google.gerrit.testutil.ConfigSuite;
 import com.google.gerrit.testutil.InMemoryModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import java.util.SortedMap;
 import org.eclipse.jgit.lib.Config;
 
 public class LuceneQueryAccountsTest extends AbstractQueryAccountsTest {
   @ConfigSuite.Config
-  public static Config againstPreviousIndexVersion() {
+  public static Config againstLastIndexVersion() {
     Config cfg = defaultConfig();
-    SortedMap<Integer, Schema<AccountState>> schemas =
-        SchemaUtil.schemasFromClass(AccountSchemaDefinitions.class, AccountState.class);
-    if (schemas.size() > 1) {
-      int prevVersion = Iterables.get(schemas.keySet(), schemas.size() - 2);
+    Integer lastVersion =
+        LastIndexVersion.get(AccountSchemaDefinitions.INSTANCE, AccountState.class);
+    if (lastVersion != null) {
       cfg.setInt(
           "index",
           "lucene",
           AccountSchemaDefinitions.INSTANCE.getName() + "TestVersion",
-          prevVersion);
+          lastVersion);
     }
     return cfg;
   }

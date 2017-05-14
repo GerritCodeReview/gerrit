8c8ab83eaa875e2187b83b59e3af6126eb5d0f2c
Paladox none <thomasmulhall410@yahoo.com>
2017-05-14 15:01:38 +0000

    PolyGerrit: Implement /admin/projects/<project> page
    This will only show read only settings. Dosen't implement setting these
    settings yet.
    
    Change-Id: I8c5f63848fcf6423a1d39e363495a2285e43e83c

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -566,6 +566,16 @@
       );
     },
 
+    getProject(project) {
+      return this._fetchSharedCacheURL(
+        `/projects/${project}`);
+    },
+
+    getProjectConfig: function(project) {
+      return this._fetchSharedCacheURL(
+        `/projects/${project}/config`);
+    },
+
     getSuggestedGroups(inputVal, opt_n, opt_errFn, opt_ctx) {
       const params = {s: inputVal};
       if (opt_n) { params.n = opt_n; }

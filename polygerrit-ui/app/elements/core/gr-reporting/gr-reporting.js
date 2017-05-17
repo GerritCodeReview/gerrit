fdea7a6bfc10774adb7c4cf4d17832162245eed9
Viktar Donich <viktard@google.com>
2017-05-17 09:25:06 -0700

    Expose reply dialog and label score value to plugins.
    Provides sample plugin that sets Code-Review+1 on entering LGTM into
    reply dialog.
    
    Adds a getLabelValue(labelName) to plugin.changeReply().
    
    Adds support for plugin dom hooks in general, and for reply dialog text
    area as a first instance.
    
    Adds plugin.changeReply().addReplyTextChangedCallback() which uses dom
    plugin hook.
    
    Feature: Issue 6280
    Change-Id: I2b8d52c0d8000ea5d217268f6e6d7ef4137b2213

--- polygerrit-ui/app/elements/core/gr-reporting/gr-reporting.js
+++ polygerrit-ui/app/elements/core/gr-reporting/gr-reporting.js
@@ -65,7 +65,7 @@
     const context = opt_context || window;
     context.onerror = onError.bind(null, context.onerror);
   };
-  catchErrors();
+  // catchErrors();
 
   const GrReporting = Polymer({
     is: 'gr-reporting',

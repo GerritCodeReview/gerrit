b67a14a723f68d006e1ff118e8bd6ce4983374a4
Becky Siegel <beckysiegel@google.com>
2017-05-16 17:23:07 -0700

    Add gr-autogrow-textarea element with emoji support
    gr-autogrow-textarea is a replacement for iron-autogrow-textarea that
    includes emoji support. This uses gr-autocomplete-dropdown as a
    sub-element.
    
    The dropdown is triggered when ":" is added to the textarea followed by
    one more character (that is not a space or a new line) shows the top 10
    matches. As the user types, the matches update.
    
    Note, that the matches show any substring match, and this can be
    improved in further iterations. Once there are no matches, or the escape
    key is pressed, the dropdown is closed.
    
    It is up to the parent element to make sure the dropdown gets closed
    correctly/when it is closed.
    
    Future work:
    - Add more emoji
    - Determine how to deal with multiple matching keywords
    - Determine better ways to show top choices.
    - Possibly deal with resized windows while the emoji dropdown is open
    - Refactor to work properly with shadow dom - there are some issues
      with keeping focus on the dropdown.
    
    Change-Id: I11901676ed58fe9d3d0b36a43dd58002275f75db

---
+++ polygerrit-ui/app/elements/shared/gr-autogrow-textarea/gr-autogrow-textarea.js
@@ -0,0 +1,298 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  const MAX_ITEMS_DROPDOWN = 10;
+
+  Polymer({
+    is: 'gr-autogrow-textarea',
+
+    properties: {
+      autocomplete: Boolean,
+      disabled: Boolean,
+      rows: Number,
+      maxRows: Number,
+      placeholder: String,
+      fixedPositionDropdown: Boolean,
+      moveToRoot: Boolean,
+      text: {
+        type: String,
+        notify: true,
+      },
+      backgroundColor: {
+        type: String,
+        value: '#fff',
+      },
+      hideBorder: {
+        type: Boolean,
+        value: false,
+      },
+      monospace: {
+        type: Boolean,
+        value: false,
+      },
+      _allSuggestions: {
+        type: Object,
+        value: [
+          {value: '💯', match: '100'},
+          {value: '💔', match: 'broken heart'},
+          {value: '🍺', match: 'beer'},
+          {value: '✔', match: 'check'},
+          {value: '😎', match: 'cool'},
+          {value: '😕', match: 'confused'},
+          {value: '😭', match: 'crying'},
+          {value: '🔥', match: 'fire'},
+          {value: '👊', match: 'fistbump'},
+          {value: '🐨', match: 'koala'},
+          {value: '😄', match: 'laugh'},
+          {value: '🤓', match: 'glasses'},
+          {value: '😆', match: 'grin'},
+          {value: '😐', match: 'neutral'},
+          {value: '👌', match: 'ok'},
+          {value: '🎉', match: 'party'},
+          {value: '💩', match: 'poop'},
+          {value: '🙏', match: 'pray'},
+          {value: '😞', match: 'sad'},
+          {value: '😮', match: 'shock'},
+          {value: '😊', match: 'smile'},
+          {value: '😢', match: 'tear'},
+          {value: '😂', match: 'tears'},
+          {value: '😋', match: 'tongue'},
+          {value: '👍', match: 'thumbs up'},
+          {value: '👎', match: 'thumbs down'},
+          {value: '😒', match: 'unamused'},
+          {value: '😉', match: 'wink'},
+          {value: '🍷', match: 'wine'},
+          {value: '😜', match: 'winking tongue'},
+        ],
+
+      },
+      _colonIndex: Number,
+      _currentSearchString: {
+        type: String,
+        value: '',
+        observer: '_determineSuggestions',
+      },
+      _hideAutocomplete: {
+        type: Boolean,
+        value: true,
+      },
+      _index: Number,
+      _suggestions: {
+        type: Array,
+      },
+    },
+
+    behaviors: [
+      Gerrit.KeyboardShortcutBehavior,
+    ],
+
+    keyBindings: {
+      esc: '_handleEscKey',
+      tab: '_handleEnterByKey',
+      enter: '_handleEnterByKey',
+      up: '_handleUpKey',
+      down: '_handleDownKey',
+    },
+
+    ready() {
+      this._resetEmojiDropdown();
+      if (this.monospace) {
+        this.$.textarea.classList.add('monospace');
+        this.$.hiddenText.classList.add('monospace');
+      }
+      if (this.hideBorder) {
+        this.$.textarea.classList.add('no-border');
+      }
+      if (this.backgroundColor) {
+        this.customStyle['--background-color'] = this.backgroundColor;
+        this.updateStyles();
+      }
+      this.listen(this.$.emojiSuggestions, 'dropdown-closed', '_resetAndFocus');
+      this.listen(this.$.emojiSuggestions, 'item-selected',
+          '_handleEmojiSelect');
+    },
+
+    detached() {
+      this.closeDropdown();
+      this.listen(this.$.emojiSuggestions, 'dropdown-closed', '_resetAndFocus');
+      this.listen(this.$.emojiSuggestions, 'item-selected',
+          '_handleEmojiSelect');
+    },
+
+    closeDropdown() {
+      if (!this.$.emojiSuggestions.hidden) {
+        this._closeEmojiDropdown();
+      }
+    },
+
+    _handleEscKey(e) {
+      if (!this._hideAutocomplete) {
+        e.preventDefault();
+        e.stopPropagation();
+        this._resetAndFocus();
+      }
+    },
+
+    _resetAndFocus() {
+      this._resetEmojiDropdown();
+      this.$.textarea.textarea.focus();
+    },
+
+    _handleUpKey(e) {
+      if (!this._hideAutocomplete) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.$.emojiSuggestions.cursorUp();
+      }
+    },
+
+    _handleDownKey(e) {
+      if (!this._hideAutocomplete) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.$.emojiSuggestions.cursorDown();
+      }
+    },
+
+    _handleEnterByKey(e) {
+      if (!this._hideAutocomplete) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.text = this._getText(this.$.emojiSuggestions.getCursorTarget()
+            .dataset.value);
+        this._resetEmojiDropdown();
+      }
+    },
+
+    _handleEmojiSelect(e) {
+      this.text = this._getText(e.detail.selected.dataset.value);
+      this._resetEmojiDropdown();
+    },
+
+    _getText(value) {
+      return this.text.substr(0, this._colonIndex) +
+          value + this.text.substr(this.$.textarea.selectionStart) + ' ';
+    },
+
+    _getPositionOfCursor() {
+      this.$.hiddenText.textContent = this.$.textarea.value.substr(0,
+          this.$.textarea.selectionStart);
+
+      const caratSpan = document.createElement('span');
+      this.$.hiddenText.appendChild(caratSpan);
+      return caratSpan.getBoundingClientRect();
+    },
+
+    _getFontSize() {
+      const fontSizePx = getComputedStyle(this).fontSize || '12px';
+      return parseInt(fontSizePx.substr(0, fontSizePx.length - 2),
+          10);
+    },
+
+    /**
+     * This positions the dropdown to be just below the cursor position. It is
+     * calculated by having a hidden element with the same width and styling of
+     * the tetarea and the text up until the point of interest. Then a span
+     * element is added to the end so that there is a specific element to get
+     * the position of.  Line height is determined (or falls back to 12px) as
+     * extra height to add.
+     */
+    _updateSelectorPosition() {
+      // a little bit of extra height when placing the selector.
+      const EXTRA_HEIGHT = 7;
+
+      // These are broken out into separate functions for testability.
+      const caratPosition = this._getPositionOfCursor();
+      const fontSize = this._getFontSize();
+
+      const top = caratPosition.top + fontSize + EXTRA_HEIGHT + 'px';
+      const left = caratPosition.left + 'px';
+      this.$.emojiSuggestions.setPosition(top, left);
+    },
+
+    /**
+     * _handleKeydown used for key handling in the this.$.textarea AND all child
+     * autocomplete options.
+     */
+    _onValueChanged(e) {
+      // If cursor is not in textarea (just opened with colon as last char),
+      // Don't do anything.
+      if (!e.currentTarget.focused) { return; }
+      const newChar = e.detail.value[this.$.textarea.selectionStart - 1];
+
+      if (newChar == ':') {
+        this._colonIndex = this.$.textarea.selectionStart - 1;
+      } else if (this._colonIndex !== null) {
+        this._currentSearchString =
+            e.detail.value.substr(this._colonIndex + 1,
+                this.$.textarea.selectionStart);
+        if (this.$.textarea.selectionStart !==
+            this._currentSearchString.length + this._colonIndex + 1 ||
+            this._currentSearchString == ' ' ||
+            this._currentSearchString == '\n' ||
+            !e.detail.value[this._colonIndex] === ':' ||
+            !this._suggestions.length) {
+          this._resetEmojiDropdown();
+        } else if (this.$.emojiSuggestions.hidden) {
+          this._hideAutocomplete = false;
+          this._openEmojiDropdown();
+          this._updateSelectorPosition();
+        }
+        this.$.textarea.textarea.focus();
+      }
+    },
+
+    _closeEmojiDropdown() {
+      this.$.emojiSuggestions.close();
+      this.$.emojiSuggestions.hidden = true;
+    },
+
+    _openEmojiDropdown() {
+      this.$.emojiSuggestions.open();
+      this.$.emojiSuggestions.hidden = false;
+    },
+
+    _formatSuggestions(matchedSuggestions) {
+      const suggestions = [];
+      for (const suggestion of matchedSuggestions) {
+        suggestion.dataValue = suggestion.value;
+        suggestion.text = suggestion.value + ' ' + suggestion.match;
+        suggestions.push(suggestion);
+      }
+      this._suggestions = suggestions;
+    },
+
+    _determineSuggestions(emojiText) {
+      if (!emojiText.length) {
+        this._formatSuggestions(this._allSuggestions);
+      }
+      const matches = this._allSuggestions.filter(suggestion => {
+        return suggestion.match.includes(emojiText);
+      }).splice(0, MAX_ITEMS_DROPDOWN);
+      this._formatSuggestions(matches);
+    },
+
+    _resetEmojiDropdown() {
+      // hide and reset the autocomplete dropdown.
+      Polymer.dom.flush();
+      this._currentSearchString = '';
+      this._hideAutocomplete = true;
+      this.closeDropdown();
+      this._colonIndex = null;
+      this.$.textarea.textarea.focus();
+    },
+  });
+})();

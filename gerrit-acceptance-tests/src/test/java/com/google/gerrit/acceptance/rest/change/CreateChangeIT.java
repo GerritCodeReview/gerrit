507f9018f92836ba514d5451c3902bbca2cf608d
Logan Hanks <logan@google.com>
2017-05-18 10:19:58 -0700

    Add abstract base class for testing notifications
    This will be used for integration tests that make assertions about who
    receives notification emails in response to particular actions.
    
    Change-Id: Iea5b67a9988707a089c02a8b099138d84fbe07e0

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/CreateChangeIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/CreateChangeIT.java
@@ -113,7 +113,7 @@ public class CreateChangeIT extends AbstractDaemonTest {
   @Test
   public void notificationsOnChangeCreation() throws Exception {
     setApiUser(user);
-    watch(project.get(), null);
+    watch(project.get());
 
     // check that watcher is notified
     setApiUser(admin);

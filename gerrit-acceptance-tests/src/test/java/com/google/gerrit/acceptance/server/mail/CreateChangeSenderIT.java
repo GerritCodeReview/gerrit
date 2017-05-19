47ae4c8e820577873ab797e52d98eda9a55d4d2d
Logan Hanks <logan@google.com>
2017-05-19 11:00:00 -0700

    Add integration test for usages of CreateChangeSender
    Change-Id: Ib47887862b6924d78aabf130c3e9fff9bda9ad6c

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/server/mail/CreateChangeSenderIT.java
@@ -0,0 +1,43 @@
+package com.google.gerrit.acceptance.server.mail;
+
+import com.google.gerrit.acceptance.AbstractNotificationTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.google.gerrit.server.account.WatchConfig.NotifyType.NEW_CHANGES;
+import static com.google.gerrit.server.account.WatchConfig.NotifyType.NEW_PATCHSETS;
+
+public class CreateChangeSenderIT extends AbstractNotificationTest {
+  @Test
+  public void createReviewableChange() throws Exception {
+    StagedPreChange spc = stagePreChange("refs/for/master", NEW_CHANGES, NEW_PATCHSETS);
+    assertThat(sender).sent("newchange", spc)
+        .notTo(spc.owner)
+        .to(spc.watchingProjectOwner)
+        .bcc(NEW_CHANGES, NEW_PATCHSETS);
+  }
+
+  @Test
+  public void createWipChange() throws Exception {
+    StagedPreChange spc = stagePreChange("refs/for/master%wip", NEW_CHANGES, NEW_PATCHSETS);
+    assertThat(sender).sent("newchange", spc)
+        .notTo(spc.owner)
+        .to(spc.watchingProjectOwner)
+        .bcc(NEW_CHANGES, NEW_PATCHSETS);
+  }
+
+  @Test
+  public void createReviewableChangeWithNotifyOwnerReviewers() throws Exception {
+    stagePreChange("refs/for/master%notify=OWNER_REVIEWERS", NEW_CHANGES, NEW_PATCHSETS);
+    assertThat(sender).notSent();
+  }
+
+  @Test
+  public void createReviewableChangeWithReviewersAndCcs() throws Exception {
+    StagedPreChange spc = stagePreChange("refs/for/master%r=reviewer,cc=ccer", NEW_CHANGES, NEW_PATCHSETS);
+    assertThat(sender).sent("newchange", spc)
+        .notTo(spc.owner)
+        .to(spc.watchingProjectOwner)
+        .bcc(NEW_CHANGES, NEW_PATCHSETS);
+  }
+}

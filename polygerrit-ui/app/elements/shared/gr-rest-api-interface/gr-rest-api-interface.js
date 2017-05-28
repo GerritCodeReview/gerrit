25bd8c9cfe397ed196502f74bb727c4f4795b04e
Paladox none <thomasmulhall410@yahoo.com>
2017-05-28 14:48:33 +0000

    PolyGerrit: Implement /admin/plugins page
    Bug: Issue 6324
    Change-Id: I1d594f4bbe05a6c315661463394e0ce2f57c86d2

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -515,6 +515,10 @@
       );
     },
 
+    getPlugins() {
+      return this._fetchSharedCacheURL('/plugins/?all');
+    },
+
     getSuggestedGroups(inputVal, opt_n, opt_errFn, opt_ctx) {
       const params = {s: inputVal};
       if (opt_n) { params.n = opt_n; }

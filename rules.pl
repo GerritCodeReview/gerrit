2232e7944716187f10f44d2424cbac60c4aa496c
Luca Milanesio <luca.milanesio@gmail.com>
2017-05-29 21:38:46 +0100

    Add Code-Style label for lint/formatting problems
    When validating the code with eslint,polylint or
    google-java-format, whenever the style doesn't match the guidelines
    we want to have a specific label to block the change from being
    merged.
    
    Similarly, we want to check for code-style and formatting before
    merging changes from now on.
    
    Change-Id: Ie82439f8fb82ac280aa7dd1a5b37eecdc004f808

--- rules.pl
+++ rules.pl
@@ -3,23 +3,25 @@ submit_rule(submit(CR)) :-
   !,
   gerrit:max_with_block(-2, 2, 'Code-Review', CR).
 
-submit_rule(submit(CR, V, L)) :-
+submit_rule(submit(CR, CS, V, L)) :-
   needs_library_compliance,
   !,
-  base(CR, V),
+  base(CR, CS, V),
   gerrit:max_with_block(-1, 1, 'Library-Compliance', L).
 
-submit_rule(submit(CR, V)) :-
+submit_rule(submit(CR, CS, V)) :-
   is_no_polygerrit_change,
   !,
-  base(CR, V).
+  base(CR, CS, V).
 
-submit_rule(submit(P, V)) :-
+submit_rule(submit(P, CS, V)) :-
   gerrit:max_with_block(-2, 2, 'PolyGerrit-Review', P),
+  gerrit:max_with_block(-1, 1, 'Code-Style', CS),
   gerrit:max_with_block(-1, 1, 'Verified', V).
 
-base(CR, V) :-
+base(CR, CS, V) :-
   gerrit:max_with_block(-2, 2, 'Code-Review', CR),
+  gerrit:max_with_block(-1, 1, 'Code-Style', CS),
   gerrit:max_with_block(-1, 1, 'Verified', V).
 
 needs_library_compliance :- gerrit:commit_delta('^lib/'), !.

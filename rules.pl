509d48a92a6d71ab93bb3754d22086cf842778ad
Luca Milanesio <luca.milanesio@gmail.com>
2017-05-29 21:38:46 +0100

    Add Code-Style label for lint/formatting problems
    When validating the code with eslint,polylint or
    google-java-format, whenever the style doesn't match the guidelines
    we want to have a specific label to block the change from being
    merged.
    
    Similarly, we want to check for code-style and formatting before
    merging changes from now on.
    
    Change-Id: Ie82439f8fb82ac280aa7dd1a5b37eecdc004f808

--- rules.pl
+++ rules.pl
@@ -20,7 +20,8 @@ submit_rule(submit(P, V)) :-
 
 base(CR, V) :-
   gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-  gerrit:max_with_block(-1, 1, 'Verified', V).
+  gerrit:max_with_block(-1, 1, 'Verified', V),
+  gerrit:max_with_block(-1, 1, 'Code-Style', CS).
 
 needs_library_compliance :- gerrit:commit_delta('^lib/'), !.
 needs_library_compliance :- gerrit:commit_delta('^[.]buckversion$'), !.

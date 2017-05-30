79a75767c94fd8ab28f0807e040b1729ef8454dd
Becky Siegel <beckysiegel@google.com>
2017-05-30 15:09:20 -0700

    Break out gr-label-score-row
    In order to display the selected label text (in an upcoming change),
    it made sense to break out label score rows into its own element, so
    that the selected text label can be a property of the element. Doing so
    without separating this gets extremely ugly. With array mutation and
    difficulty observing changes.
    
    Bug: Issue 6306
    Change-Id: I4a583083363237982901da8871354c308d0206d8

---
+++ polygerrit-ui/app/elements/change/gr-label-score-row/gr-label-score-row.js
@@ -0,0 +1,90 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-label-score-row',
+    properties: {
+      label: Object,
+      change: Object,
+      name: {
+        type: String,
+        reflectToAttribute: true,
+      },
+      permittedLabels: Object,
+      labelValues: Object,
+      _selectedValueText: String,
+    },
+
+    get selectedItem() {
+      return this.$$('iron-selector .iron-selected');
+    },
+
+    get selectedValue() {
+      if (!this.selectedItem) { return; }
+      return this.selectedItem.getAttribute('data-value');
+    },
+
+    setSelectedValue(value) {
+      // The selector may not be present if it’s not at the latest patch set.
+      if (!this._ironSelector) { return; }
+      const item = this.$$('gr-button[data-value="' + value + '"]');
+      if (!item) { return; }
+      this._ironSelector.selectIndex(this._ironSelector.indexOf(item));
+    },
+
+    get _ironSelector() {
+      return this.$$('iron-selector');
+    },
+
+    _computeBlankItems(permittedLabels, label, side) {
+      if (!permittedLabels || !permittedLabels[label]) { return []; }
+      const startPosition = this.labelValues[parseInt(
+          permittedLabels[label][0])];
+      if (side === 'start') {
+        return new Array(startPosition);
+      }
+      const endPosition = this.labelValues[parseInt(
+          permittedLabels[label][permittedLabels[label].length - 1])];
+      return new Array(Object.keys(this.labelValues).length - endPosition - 1);
+    },
+
+    _computeIndexOfLabelValue(labels, permittedLabels, label) {
+      if (!labels[label.name]) { return null; }
+      const labelValue = label.value;
+      const len = permittedLabels[label.name] != null ?
+          permittedLabels[label.name].length : 0;
+      for (let i = 0; i < len; i++) {
+        const val = parseInt(permittedLabels[label.name][i], 10);
+        if (val === labelValue) {
+          return i;
+        }
+      }
+      return null;
+    },
+
+    _computeAnyPermittedLabelValues(permittedLabels, label) {
+      return permittedLabels.hasOwnProperty(label);
+    },
+
+    _computePermittedLabelValues(permittedLabels, label) {
+      return permittedLabels[label];
+    },
+
+    _computeLabelValueTitle(labels, label, value) {
+      return labels[label] && labels[label].values[value];
+    },
+  });
+})();

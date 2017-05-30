e2eeb4cd9aeef5a1070e54d2fc73a68f78c84b53
Viktar Donich <viktard@google.com>
2017-05-30 13:03:50 -0700

    Highlight messages with positive and negative scores
    http://imgur.com/a/J3LyD
    
    Change-Id: I5f10bcbe71718082b583d183d36d76a18b82d7e2

--- polygerrit-ui/app/elements/change/gr-message/gr-message.js
+++ polygerrit-ui/app/elements/change/gr-message/gr-message.js
@@ -161,10 +161,40 @@
       return event.type === 'REVIEWER_UPDATE';
     },
 
-    _computeClass(expanded, showAvatar) {
+    _isMessagePositive(message) {
+      if (!message.message) { return null; }
+      const line = message.message.split('\n', 1)[0];
+      const patchSetPrefix = /^Patch Set \d+: /;
+      if (!line.match(patchSetPrefix)) { return null;}
+      const scoresRaw = line.split(patchSetPrefix)[1];
+      if (!scoresRaw) { return null; }
+      const scores = scoresRaw.split(' ');
+      if (!scores.length) { return null; }
+      const {min, max} = scores
+            .map(s => s.match(/(.+)([+-\\ ]\d+)$/))
+            .filter(ms => ms && ms.length === 3)
+            .map(([, label, score]) => score)
+            .map(s => parseInt(s, 10))
+            .reduce(({min, max}, s) =>
+                    ({min: (s < min ? s : min), max: (s > max ? s : max)}),
+                    {min: 0, max: 0});
+      if (max - min === 0) {
+        return null;
+      } else {
+        return (max + min) > 0;
+      }
+    },
+
+    _computeClass(expanded, showAvatar, message) {
       const classes = [];
       classes.push(expanded ? 'expanded' : 'collapsed');
       classes.push(showAvatar ? 'showAvatar' : 'hideAvatar');
+      const maybePositive = this._isMessagePositive(message);
+      if (maybePositive === true) {
+        classes.push('positiveVote');
+      } else if (maybePositive === false) {
+        classes.push('negativeVote');
+      }
       return classes.join(' ');
     },
 

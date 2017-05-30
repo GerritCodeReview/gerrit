6396d6ddc6ee79cb4452b77f5280f335d8c88ca7
Edwin Kempin <ekempin@google.com>
2017-05-30 10:41:28 +0200

    Add basic consistency checker for accounts
    So far it only checks that preferred emails are backed by
    corresponding external IDs. E.g. change I2b3c5c9d assumes that each
    preferred email exists as external ID.
    
    Change-Id: I824caa34545b1ba147fb71ede503104ba1d5481c
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/config/CheckConsistency.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/config/CheckConsistency.java
@@ -16,13 +16,16 @@ package com.google.gerrit.server.config;
 
 import com.google.gerrit.extensions.api.config.ConsistencyCheckInfo;
 import com.google.gerrit.extensions.api.config.ConsistencyCheckInfo.CheckAccountExternalIdsResultInfo;
+import com.google.gerrit.extensions.api.config.ConsistencyCheckInfo.CheckAccountsResultInfo;
 import com.google.gerrit.extensions.api.config.ConsistencyCheckInput;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountsConsistencyChecker;
 import com.google.gerrit.server.account.externalids.ExternalIdsConsistencyChecker;
+import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
@@ -31,19 +34,22 @@ import java.io.IOException;
 @Singleton
 public class CheckConsistency implements RestModifyView<ConfigResource, ConsistencyCheckInput> {
   private final Provider<IdentifiedUser> userProvider;
+  private final AccountsConsistencyChecker accountsConsistencyChecker;
   private final ExternalIdsConsistencyChecker externalIdsConsistencyChecker;
 
   @Inject
   CheckConsistency(
       Provider<IdentifiedUser> currentUser,
+      AccountsConsistencyChecker accountsConsistencyChecker,
       ExternalIdsConsistencyChecker externalIdsConsistencyChecker) {
     this.userProvider = currentUser;
+    this.accountsConsistencyChecker = accountsConsistencyChecker;
     this.externalIdsConsistencyChecker = externalIdsConsistencyChecker;
   }
 
   @Override
   public ConsistencyCheckInfo apply(ConfigResource resource, ConsistencyCheckInput input)
-      throws RestApiException, IOException {
+      throws RestApiException, IOException, OrmException {
     IdentifiedUser user = userProvider.get();
     if (!user.isIdentifiedUser()) {
       throw new AuthException("Authentication required");
@@ -52,11 +58,15 @@ public class CheckConsistency implements RestModifyView<ConfigResource, Consiste
       throw new AuthException("not allowed to run consistency checks");
     }
 
-    if (input == null || input.checkAccountExternalIds == null) {
+    if (input == null || (input.checkAccounts == null && input.checkAccountExternalIds == null)) {
       throw new BadRequestException("input required");
     }
 
     ConsistencyCheckInfo consistencyCheckInfo = new ConsistencyCheckInfo();
+    if (input.checkAccounts != null) {
+      consistencyCheckInfo.checkAccountsResult =
+          new CheckAccountsResultInfo(accountsConsistencyChecker.check());
+    }
     if (input.checkAccountExternalIds != null) {
       consistencyCheckInfo.checkAccountExternalIdsResult =
           new CheckAccountExternalIdsResultInfo(externalIdsConsistencyChecker.check());

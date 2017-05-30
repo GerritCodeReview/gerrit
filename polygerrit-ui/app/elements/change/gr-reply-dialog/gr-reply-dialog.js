5412ffbd308c47551d190e43bd836dcd353407eb
Becky Siegel <beckysiegel@google.com>
2017-05-30 15:09:20 -0700

    Break out gr-label-score-row
    In order to display the selected label text (in an upcoming change),
    it made sense to break out label score rows into its own element, so
    that the selected text label can be a property of the element. Doing so
    without separating this gets extremely ugly. With array mutation and
    difficulty observing changes.
    
    Bug: Issue 6306
    Change-Id: I4a583083363237982901da8871354c308d0206d8

--- polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.js
+++ polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.js
@@ -198,12 +198,9 @@
 
     setLabelValue(label, value) {
       const selectorEl =
-          this.$.labelScores.$$('iron-selector[data-label="' + label + '"]');
-      // The selector may not be present if it’s not at the latest patch set.
-      if (!selectorEl) { return; }
-      const item = selectorEl.$$('gr-button[data-value="' + value + '"]');
-      if (!item) { return; }
-      selectorEl.selectIndex(selectorEl.indexOf(item));
+          this.$.labelScores.$$('gr-label-score-row[name="' + label + '"]');
+
+      selectorEl.setSelectedValue(value);
     },
 
     _handleEscKey(e) {

7dc038651eb0aa18d8d43cfcecf237fba84641a2
Logan Hanks <logan@google.com>
2017-05-30 13:49:04 -0700

    Downgrade notify for WIP reviewer deletion
    Previously, notify on deleting a reviewer defaulted to ALL. This
    continues to be the case for non-WIP changes.
    
    For WIP changes, we don't want to notify anyone unless this action
    results in one or more approvals being removed from the change. If any
    approvals are removed, the deleted reviewer and maybe the owner are the
    only recipients (which may seem strange but that's existing behavior
    when notify=OWNER for this particular notification). If no approvals are
    removed, then no notification is sent.
    
    Change-Id: I00821cddb2558fd35c17ae76c47567b8189be0db

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/changes/DeleteReviewerInput.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/changes/DeleteReviewerInput.java
@@ -19,7 +19,7 @@ import java.util.Map;
 /** Input passed to {@code DELETE /changes/[id]/reviewers/[id]}. */
 public class DeleteReviewerInput {
   /** Who to send email notifications to after the reviewer is deleted. */
-  public NotifyHandling notify = NotifyHandling.ALL;
+  public NotifyHandling notify = null;
 
   public Map<RecipientType, NotifyInfo> notifyDetails;
 }

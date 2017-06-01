049c5d28380be09bc4c52135382d94d1f34f22a5
Dave Borowitz <dborowitz@google.com>
2017-06-01 17:37:36 -0400

    NotesBranchUtil: Don't retry automatically
    The retry mechanism in NotesBranchUtil is inflexible, and only retries a
    fixed number of times on lock failure, with a fixed wait in between, up
    to 250ms total. Now that we have RetryHelper available globally, make
    use of that instead. This allows us to eliminate a lot of code from
    NotesBranchUtil, which is a good thing, since this extra code also has
    some bugs. For example, it doesn't handle all possible
    RefUpdate.Results.
    
    This is a philosophical difference in how to approach retries, which
    requires fixing downstream code, including in the reviewnotes plugin.
    RetryHelper is designed to be run at the highest level, completely
    reopening the repository, to ensure we get the latest ref state and
    start with a fresh inserter.
    
    Since we now expect downstream plugins to use RetryHelper, export
    guava-retrying from the plugin API.
    
    Change-Id: I8a93e20f811a7c67c5cfd9cdf630afac7327c64f

--- gerrit-server/src/main/java/com/google/gerrit/server/project/BanCommit.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/BanCommit.java
@@ -17,17 +17,17 @@ package com.google.gerrit.server.project;
 import com.google.common.collect.Lists;
 import com.google.gerrit.common.errors.PermissionDeniedException;
 import com.google.gerrit.extensions.restapi.AuthException;
-import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
 import com.google.gerrit.server.git.BanCommitResult;
 import com.google.gerrit.server.project.BanCommit.Input;
+import com.google.gerrit.server.update.RetryHelper;
+import com.google.gerrit.server.update.UpdateException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
 import org.eclipse.jgit.lib.ObjectId;
 
 @Singleton
@@ -47,39 +47,42 @@ public class BanCommit implements RestModifyView<ProjectResource, Input> {
     }
   }
 
+  private final RetryHelper retryHelper;
   private final com.google.gerrit.server.git.BanCommit banCommit;
 
   @Inject
-  BanCommit(com.google.gerrit.server.git.BanCommit banCommit) {
+  BanCommit(RetryHelper retryHelper, com.google.gerrit.server.git.BanCommit banCommit) {
+    this.retryHelper = retryHelper;
     this.banCommit = banCommit;
   }
 
   @Override
   public BanResultInfo apply(ProjectResource rsrc, Input input)
-      throws UnprocessableEntityException, AuthException, ResourceConflictException, IOException {
-    BanResultInfo r = new BanResultInfo();
-    if (input != null && input.commits != null && !input.commits.isEmpty()) {
-      List<ObjectId> commitsToBan = new ArrayList<>(input.commits.size());
-      for (String c : input.commits) {
-        try {
-          commitsToBan.add(ObjectId.fromString(c));
-        } catch (IllegalArgumentException e) {
-          throw new UnprocessableEntityException(e.getMessage());
-        }
-      }
+      throws RestApiException, UpdateException {
+    return retryHelper.execute(
+        updateFactory -> {
+          BanResultInfo r = new BanResultInfo();
+          if (input != null && input.commits != null && !input.commits.isEmpty()) {
+            List<ObjectId> commitsToBan = new ArrayList<>(input.commits.size());
+            for (String c : input.commits) {
+              try {
+                commitsToBan.add(ObjectId.fromString(c));
+              } catch (IllegalArgumentException e) {
+                throw new UnprocessableEntityException(e.getMessage());
+              }
+            }
 
-      try {
-        BanCommitResult result = banCommit.ban(rsrc.getControl(), commitsToBan, input.reason);
-        r.newlyBanned = transformCommits(result.getNewlyBannedCommits());
-        r.alreadyBanned = transformCommits(result.getAlreadyBannedCommits());
-        r.ignored = transformCommits(result.getIgnoredObjectIds());
-      } catch (PermissionDeniedException e) {
-        throw new AuthException(e.getMessage());
-      } catch (ConcurrentRefUpdateException e) {
-        throw new ResourceConflictException(e.getMessage(), e);
-      }
-    }
-    return r;
+            try {
+              BanCommitResult result = banCommit.ban(rsrc.getControl(), commitsToBan, input.reason);
+              r.newlyBanned = transformCommits(result.getNewlyBannedCommits());
+              r.alreadyBanned = transformCommits(result.getAlreadyBannedCommits());
+              r.ignored = transformCommits(result.getIgnoredObjectIds());
+            } catch (PermissionDeniedException e) {
+              throw new AuthException(e.getMessage());
+            }
+          }
+          return r;
+        });
   }
 
   private static List<String> transformCommits(List<ObjectId> commits) {

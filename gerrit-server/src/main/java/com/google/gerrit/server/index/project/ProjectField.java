2d662b4de3395825c8a21e065b0da1ad01e214a4
Xin Sun <xisun@google.com>
2017-06-01 14:02:36 -0700

    Add schema and index definitions for project index
    Change-Id: I5326c770c02d8bb3d7e283124207caa9e8d28581

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/index/project/ProjectField.java
@@ -0,0 +1,29 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.index.project;
+
+import static com.google.gerrit.server.index.FieldDef.exact;
+
+import com.google.gerrit.server.index.FieldDef;
+import com.google.gerrit.server.project.ProjectState;
+
+// Code Review: what are the other fields that Gerrit wants to have in the index?
+/** Index schema for projects. */
+public class ProjectField {
+  /** Gerrit Project Name. */
+  public static final FieldDef<ProjectState, String> NAME =
+      exact("name").build(p -> p.getProject().getName());
+}
+

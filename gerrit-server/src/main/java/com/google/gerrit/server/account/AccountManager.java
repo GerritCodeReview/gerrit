68cb49aa85d0577428b959cc1cdd38f92d805d4d
Edwin Kempin <ekempin@google.com>
2017-06-01 11:29:29 +0200

    Remove AccountByEmailCache
    This cache is not needed. We can just get external IDs by email from the
    ExternalIdCache and extract the account IDs from the external IDs. This
    is exactly what AccountByEmailCacheImpl.Loader was doing.
    
    Looking up accounts by email via the ExternalIdCache means that the SHA1
    of the refs/meta/external-ids branch is read on each lookup (to verify
    that the cache is up to date). This would be a disadvantage if we would
    need to lookup many accounts by email in a loop, but we don't. If we
    ever need this we may add methods to lookup multiple accounts by emails
    at once to the Accounts and ExternalIdCache classes, so that the SHA1 of
    the refs/meta/external-ids branch is read only once.
    
    When emails are changed the ExternalIdCache is automatically evicted
    since it detects when the refs/meta/external-ids branch was updated,
    hence manual cache eviction is no longer needed.
    
    Change-Id: I3a4279f5abda7ff3f03268258bb1755ce528f0d4
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/account/AccountManager.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/AccountManager.java
@@ -65,7 +65,6 @@ public class AccountManager {
   private final Accounts accounts;
   private final AccountsUpdate.Server accountsUpdateFactory;
   private final AccountCache byIdCache;
-  private final AccountByEmailCache byEmailCache;
   private final Realm realm;
   private final IdentifiedUser.GenericFactory userFactory;
   private final ChangeUserName.Factory changeUserNameFactory;
@@ -85,7 +84,6 @@ public class AccountManager {
       Accounts accounts,
       AccountsUpdate.Server accountsUpdateFactory,
       AccountCache byIdCache,
-      AccountByEmailCache byEmailCache,
       Realm accountMapper,
       IdentifiedUser.GenericFactory userFactory,
       ChangeUserName.Factory changeUserNameFactory,
@@ -100,7 +98,6 @@ public class AccountManager {
     this.accounts = accounts;
     this.accountsUpdateFactory = accountsUpdateFactory;
     this.byIdCache = byIdCache;
-    this.byEmailCache = byEmailCache;
     this.realm = accountMapper;
     this.userFactory = userFactory;
     this.changeUserNameFactory = changeUserNameFactory;
@@ -201,11 +198,6 @@ public class AccountManager {
         throw new OrmException("Account " + user.getAccountId() + " has been deleted");
       }
     }
-
-    if (newEmail != null && !newEmail.equals(oldEmail)) {
-      byEmailCache.evict(oldEmail);
-      byEmailCache.evict(newEmail);
-    }
   }
 
   private static boolean eq(String a, String b) {
@@ -303,7 +295,6 @@ public class AccountManager {
       }
     }
 
-    byEmailCache.evict(account.getPreferredEmail());
     realm.onCreateAccount(who, account);
     return new AuthResult(newId, extId.key(), true);
   }
@@ -386,7 +377,6 @@ public class AccountManager {
                       a.setPreferredEmail(who.getEmailAddress());
                     }
                   });
-          byEmailCache.evict(who.getEmailAddress());
         }
       }
 
@@ -484,7 +474,6 @@ public class AccountManager {
                     }
                   }
                 });
-        extIds.stream().forEach(e -> byEmailCache.evict(e.email()));
       }
     }
   }

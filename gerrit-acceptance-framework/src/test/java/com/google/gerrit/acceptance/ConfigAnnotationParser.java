1490efea788f7e528b4518b3e874d82aa7849e10
David Pursehouse <dpursehouse@collab.net>
2017-06-02 16:05:35 +0900

    Introduce GerritPluginConfig{s} annotations
    GerritPluginConfig and GerritPluginConfigs can be used to annotate
    test methods in plugin tests, to provide test configuration values
    in $site/etc/plugin-name.config.
    
    The annotations work in the same way as existing GerritConfig{s}, with
    addition of "pluginName" parameter.
    
    A limitation of these annotations is that they must be used together
    with @UseLocalDisk, so that the plugin configuration files may be
    written in the site's etc folder.
    
    Change-Id: I11a66eac87bec5f849858d1a4f38d3f01976ff47

--- gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/ConfigAnnotationParser.java
+++ gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/ConfigAnnotationParser.java
@@ -17,8 +17,11 @@ package com.google.gerrit.acceptance;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import org.eclipse.jgit.lib.Config;
 
 class ConfigAnnotationParser {
@@ -40,6 +43,67 @@ class ConfigAnnotationParser {
     Config cfg = new Config(base);
     parseAnnotation(cfg, annotation);
     return cfg;
+  }
+
+  static class GerritPluginConfigToGerritConfig implements GerritConfig {
+    private final GerritPluginConfig delegate;
+
+    GerritPluginConfigToGerritConfig(GerritPluginConfig delegate) {
+      this.delegate = delegate;
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+      return delegate.annotationType();
+    }
+
+    @Override
+    public String name() {
+      return delegate.name();
+    }
+
+    @Override
+    public String value() {
+      return delegate.value();
+    }
+
+    @Override
+    public String[] values() {
+      return delegate.values();
+    }
+  }
+
+  static Map<String, Config> parse(GerritPluginConfig annotation) {
+    if (annotation == null) {
+      return null;
+    }
+    Map<String, Config> result = new HashMap<>();
+    Config cfg = new Config();
+    parseAnnotation(cfg, new GerritPluginConfigToGerritConfig(annotation));
+    result.put(annotation.pluginName(), cfg);
+    return result;
+  }
+
+  static Map<String, Config> parse(GerritPluginConfigs annotation) {
+    if (annotation == null || annotation.value().length < 1) {
+      return null;
+    }
+
+    HashMap<String, Config> result = new HashMap<>();
+
+    for (GerritPluginConfig c : annotation.value()) {
+      String pluginName = c.pluginName();
+      Config config;
+      if (result.containsKey(pluginName)) {
+        config = result.get(pluginName);
+      } else {
+        config = new Config();
+        result.put(pluginName, config);
+      }
+      parseAnnotation(config, new GerritPluginConfigToGerritConfig(c));
+    }
+
+    return result;
   }
 
   private static void parseAnnotation(Config cfg, GerritConfig c) {

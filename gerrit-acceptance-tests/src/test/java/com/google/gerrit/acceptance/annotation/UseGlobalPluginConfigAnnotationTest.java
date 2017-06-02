5fec42c105734c07a04c948e354d2765c74abce5
David Pursehouse <dpursehouse@collab.net>
2017-06-02 16:05:35 +0900

    Introduce GlobalPluginConfig{s} annotations
    GlobalPluginConfig and GlobalPluginConfigs can be used to annotate
    test methods in plugin tests, to provide test configuration values
    in $site/etc/plugin-name.config.
    
    The annotations work in the same way as existing GerritConfig{s}, with
    addition of "pluginName" parameter.
    
    A limitation of these annotations is that they must be used together
    with @UseLocalDisk, so that the plugin configuration files may be
    written in the site's etc folder.
    
    Change-Id: I11a66eac87bec5f849858d1a4f38d3f01976ff47

---
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/annotation/UseGlobalPluginConfigAnnotationTest.java
@@ -0,0 +1,91 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.annotation;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.GlobalPluginConfig;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import org.junit.Test;
+
+public class UseGlobalPluginConfigAnnotationTest extends AbstractDaemonTest {
+  @Test
+  @UseLocalDisk
+  @GlobalPluginConfig(pluginName = "test", name = "section.name", value = "value")
+  public void testOne() {
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getString("section", null, "name")).isEqualTo("value");
+  }
+
+  @Test
+  @UseLocalDisk
+  @GlobalPluginConfig(pluginName = "test", name = "section.subsection.name", value = "value")
+  public void testOneWithSubsection() {
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getString("section", "subsection", "name")).isEqualTo("value");
+  }
+
+  @Test
+  @UseLocalDisk
+  @GlobalPluginConfig(pluginName = "test", name = "section.name", value = "value")
+  @GlobalPluginConfig(pluginName = "test", name = "section1.name", value = "value1")
+  @GlobalPluginConfig(pluginName = "test", name = "section.subsection.name", value = "value")
+  @GlobalPluginConfig(pluginName = "test", name = "section.subsection1.name", value = "value1")
+  public void testMultiple() {
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getString("section", null, "name")).isEqualTo("value");
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getString("section1", null, "name")).isEqualTo("value1");
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getString("section", "subsection", "name")).isEqualTo("value");
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getString("section", "subsection1", "name")).isEqualTo("value1");
+  }
+
+  @Test
+  @UseLocalDisk
+  @GlobalPluginConfig(
+    pluginName = "test",
+    name = "section.name",
+    values = {"value-1", "value-2"}
+  )
+  public void testList() {
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getStringList("section", null, "name"))
+        .asList()
+        .containsExactly("value-1", "value-2");
+  }
+
+  @Test
+  @UseLocalDisk
+  @GlobalPluginConfig(
+    pluginName = "test",
+    name = "section.subsection.name",
+    values = {"value-1", "value-2"}
+  )
+  public void testListWithSubsection() {
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getStringList("section", "subsection", "name"))
+        .asList()
+        .containsExactly("value-1", "value-2");
+  }
+
+  @Test
+  @UseLocalDisk
+  @GlobalPluginConfig(
+    pluginName = "test",
+    name = "section.name",
+    value = "value-1",
+    values = {"value-2", "value-3"}
+  )
+  public void valueHasPrecedenceOverValues() {
+    assertThat(pluginConfig.getGlobalPluginConfig("test").getStringList("section", null, "name"))
+        .asList()
+        .containsExactly("value-1");
+  }
+}

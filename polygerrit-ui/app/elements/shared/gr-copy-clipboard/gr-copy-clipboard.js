47729806f9a334761829126ae386be2c30591f3d
Becky Siegel <beckysiegel@google.com>
2017-06-07 11:12:21 -0700

    Add gr-copy-clipboard element
    This will be consumed by the download dialog.
    
    Change-Id: Iaec13b73f4a36f9805e2b2f22bb36fdcc7c7b1f2

---
+++ polygerrit-ui/app/elements/shared/gr-copy-clipboard/gr-copy-clipboard.js
@@ -0,0 +1,42 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-copy-clipboard',
+
+    properties: {
+      command: String,
+      title: String,
+    },
+
+    focusOnCopy() {
+      this.$.button.focus();
+    },
+
+    _handleInputTap(e) {
+      e.preventDefault();
+      Polymer.dom(e).rootTarget.select();
+    },
+
+    _copyToClipboard(e) {
+      this.$.input.select();
+      document.execCommand('copy');
+      getSelection().removeAllRanges();
+      e.target.textContent = 'done';
+      setTimeout(() => { e.target.textContent = 'copy'; }, 1000);
+    },
+  });
+})();

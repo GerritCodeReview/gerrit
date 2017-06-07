8562864736d470739f26477e337e709da82ce0d7
Edwin Kempin <ekempin@google.com>
2017-06-07 17:15:41 +0200

    Remove AccountAccess.byPreferredEmail
    BecomeAnyAccountLoginServlet was the last user of
    AccountAccess.byPreferredEmail. Instead of going to ReviewDb to lookup
    accounts by preferred email, it's now doing a lookup from the account
    index.
    
    This is a preparation for migrating the accounts to NoteDb, because then
    we can no longer lookup accounts by prefered email from ReviewDb.
    
    Change-Id: I371f5791455435faf47764c684377304ebf02ff0
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-reviewdb/src/main/resources/com/google/gerrit/reviewdb/server/index_maxdb.sql
+++ gerrit-reviewdb/src/main/resources/com/google/gerrit/reviewdb/server/index_maxdb.sql
@@ -6,14 +6,6 @@ delimiter #
 --
 
 -- *********************************************************************
--- AccountAccess
---    covers:             byPreferredEmail
-CREATE INDEX accounts_byPreferredEmail
-ON accounts (preferred_email)
-#
-
-
--- *********************************************************************
 -- AccountGroupMemberAccess
 --    @PrimaryKey covers: byAccount
 CREATE INDEX account_group_members_byGroup

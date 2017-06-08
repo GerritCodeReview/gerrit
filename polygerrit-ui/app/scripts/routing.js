2df4692ae98562f2083c8de194d57507e8887f6b
Wyatt Allen <wyatta@google.com>
2017-06-08 09:20:46 -0700

    route
    Change-Id: Idc18cbd87b8e4e05d24ae6a5feb0a0a43f47fd7f

---
+++ polygerrit-ui/app/scripts/routing.js
@@ -0,0 +1,92 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function(window) {
+  'use strict';
+
+  window.Gerrit = window.Gerrit || {};
+  if (window.Gerrit.hasOwnProperty('Routing')) { return; }
+
+  window.Gerrit.Routing = {
+
+    View: {
+      SEARCH: 'search',
+    },
+
+    /** @type {Function} */
+    _navigate: null,
+
+    /** @type {Function} */
+    _generateUrl: null,
+
+    /** @type {Boolean} */
+    _isInitialized: false,
+
+    /**
+     * Setup router implementation.
+     * @param {Function} handleNavigate
+     * @param {Function} generateUrl
+     */
+    setup(navigate, generateUrl) {
+      this._navigate = navigate;
+      this._generateUrl = generateUrl;
+      this._isInitialized = true;
+    },
+
+    destroy() {
+      this._navigate = null;
+      this._generateUrl = null;
+      this._isInitialized = false;
+    },
+
+    /**
+     * Generate a URL for the given route parameters.
+     * @param {Object} params
+     * @return {String}
+     */
+    getUrlFor(params) {
+      this._checkInit();
+      return this._generateUrl(params);
+    },
+
+    getUrlForProject(project) {
+      return this.getUrlFor({
+        view: Gerrit.Routing.View.SEARCH,
+        project,
+      });
+    },
+
+    getUrlForBranch(branch, project, status) {
+      return this.getUrlFor({
+        view: Gerrit.Routing.View.SEARCH,
+        branch,
+        project,
+        statuses: [status],
+      });
+    },
+
+    getUrlForTopic(topic) {
+      return this.getUrlFor({
+        view: Gerrit.Routing.View.SEARCH,
+        topic,
+        statuses: ['open', 'merged'],
+      });
+    },
+
+    _checkInit() {
+      if (!this._isInitialized) {
+        throw new Error('Use of uninitialized routing');
+      }
+    },
+  };
+})(window);

d58bc4cb49e39f73b5f793141fae869e95eb83e6
Han-Wen Nienhuys <hanwen@google.com>
2017-06-12 14:54:59 +0200

    Avoid referencing gerrit specific Executor.
    WorkQueue.Executor is a thin layer on top of
    ScheduledThreadPoolExecutor. Since it is a class, rather than an
    implementation, it is impossible to inject a wrapper around an
    existing implementation.
    
    This change is one step towards enabling this.
    
    Change-Id: Id6b1d0fd10b5f8549cbc76a4867b3afb750aca02

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/StreamCommandExecutor.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/StreamCommandExecutor.java
@@ -16,11 +16,11 @@ package com.google.gerrit.sshd;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import com.google.gerrit.server.git.WorkQueue.Executor;
 import com.google.inject.BindingAnnotation;
 import java.lang.annotation.Retention;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-/** Marker on {@link Executor} used by delayed event streaming. */
+/** Marker on {@link ScheduledThreadPoolExecutor} used by delayed event streaming. */
 @Retention(RUNTIME)
 @BindingAnnotation
 public @interface StreamCommandExecutor {}

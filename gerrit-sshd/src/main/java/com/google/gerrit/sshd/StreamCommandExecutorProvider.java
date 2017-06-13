ccb126e818ac777f101dcef469f97e652c90292e
Han-Wen Nienhuys <hanwen@google.com>
2017-06-13 17:48:36 +0200

    Add threadPriority argument to createQueue.
    This stops ScheduledThreadPoolExecutor API usage from leaking beyond WorkQueue.
    
    Change-Id: I9c7b141272a6d547c4f2c39c28a5ea60ada944d7

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/StreamCommandExecutorProvider.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/StreamCommandExecutorProvider.java
@@ -18,7 +18,6 @@ import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.git.WorkQueue;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import java.util.concurrent.ThreadFactory;
 import org.eclipse.jgit.lib.Config;
 
 class StreamCommandExecutorProvider implements Provider<WorkQueue.Executor> {
@@ -34,20 +33,6 @@ class StreamCommandExecutorProvider implements Provider<WorkQueue.Executor> {
 
   @Override
   public WorkQueue.Executor get() {
-    final WorkQueue.Executor executor;
-
-    executor = queues.createQueue(poolSize, "SSH-Stream-Worker");
-
-    final ThreadFactory parent = executor.getThreadFactory();
-    executor.setThreadFactory(
-        new ThreadFactory() {
-          @Override
-          public Thread newThread(Runnable task) {
-            final Thread t = parent.newThread(task);
-            t.setPriority(Thread.MIN_PRIORITY);
-            return t;
-          }
-        });
-    return executor;
+    return queues.createQueue(poolSize, "SSH-Stream-Worker", Thread.MIN_PRIORITY);
   }
 }

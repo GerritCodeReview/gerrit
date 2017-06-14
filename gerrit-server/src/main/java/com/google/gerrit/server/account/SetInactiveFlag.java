581d1acca29946d527eb670781cf143b14ae9791
Owen Li <owen.a.li@ericsson.com>
2017-06-14 10:04:00 -0400

    Synchronize Gerrit DB account inactive flag with LDAP auth
    Implement the capability to automatically synchronize an account's
    active/inactive flag in the internal Gerrit DB with the authentication
    back-end.
    
    This change is intended to remove the manual steps involved with
    activating/deactivating Gerrit accounts when their status changes in the
    authentication back-end. Upon interactive login, an account's inactive
    flag should be updated accordingly, and the login attempt should
    succeed/fail accordingly. To maintain backwards compatibility, this
    feature is by default disabled, and can be enabled within gerrit.config
    for supported authentication back-ends. Currently, it is implemented only for
    LDAP.
    
    Change-Id: I9dc124473ec6c83c369a9eee278bc07fa7cf3d4c

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/SetInactiveFlag.java
@@ -0,0 +1,90 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+
+@Singleton
+public class SetInactiveFlag {
+
+  private final Provider<ReviewDb> dbProvider;
+  private final AccountsUpdate.Server accountsUpdate;
+
+  @Inject
+  SetInactiveFlag(Provider<ReviewDb> dbProvider, AccountsUpdate.Server accountsUpdate) {
+    this.dbProvider = dbProvider;
+    this.accountsUpdate = accountsUpdate;
+  }
+
+  public Response<?> deactivate(IdentifiedUser user)
+      throws RestApiException, OrmException, IOException, ConfigInvalidException {
+    AtomicBoolean alreadyInactive = new AtomicBoolean(false);
+    Account account =
+        accountsUpdate
+            .create()
+            .update(
+                dbProvider.get(),
+                user.getAccountId(),
+                a -> {
+                  if (!a.isActive()) {
+                    alreadyInactive.set(true);
+                  } else {
+                    a.setActive(false);
+                  }
+                });
+    if (account == null) {
+      throw new ResourceNotFoundException("account not found");
+    }
+    if (alreadyInactive.get()) {
+      throw new ResourceConflictException("account not active");
+    }
+    return Response.none();
+  }
+
+  public Response<String> activate(IdentifiedUser user)
+      throws ResourceNotFoundException, OrmException, IOException, ConfigInvalidException {
+    AtomicBoolean alreadyActive = new AtomicBoolean(false);
+    Account account =
+        accountsUpdate
+            .create()
+            .update(
+                dbProvider.get(),
+                user.getAccountId(),
+                a -> {
+                  if (a.isActive()) {
+                    alreadyActive.set(true);
+                  } else {
+                    a.setActive(true);
+                  }
+                });
+    if (account == null) {
+      throw new ResourceNotFoundException("account not found");
+    }
+    return alreadyActive.get() ? Response.ok("") : Response.created("");
+  }
+}

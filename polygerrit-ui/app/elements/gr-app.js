431d329036904d30adab9afd5f673d5dbbb53c1e
Wyatt Allen <wyatta@google.com>
2017-06-15 12:51:19 -0700

    Show version info in the footer when provided
    When the index template renders with version information, pass the
    version info into the GR-APP element so that it can be displayed in the
    footer alongside the Gerrit version.
    
    With this change, the version info is passed as a property rather than a
    global.
    
    Change-Id: Id550753a4bbb1b8d644f895fd703a9522138109f

--- polygerrit-ui/app/elements/gr-app.js
+++ polygerrit-ui/app/elements/gr-app.js
@@ -37,6 +37,9 @@
         value() { return document.body; },
       },
 
+      /** Additional version information that may be provided. */
+      versionInfo: String,
+
       _account: {
         type: Object,
         observer: '_accountChanged',
@@ -95,6 +98,7 @@
       });
       this.$.restAPI.getVersion().then(version => {
         this._version = version;
+        if (this.versionInfo) { this._version += ` - ${this.versionInfo}`; }
       });
 
       this.$.reporting.appStarted();

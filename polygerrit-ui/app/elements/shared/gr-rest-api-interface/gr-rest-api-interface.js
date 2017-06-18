2d4f6eae0a7799241573772ad0771867137cf0da
Paladox none <thomasmulhall410@yahoo.com>
2017-06-18 20:21:37 +0000

    PolyGerrit: Implement /admin/groups/<group>
    Change-Id: I60c50a6c4b25597323781f481159d50a85eebbc5

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -148,6 +148,21 @@
           opt_ctx);
     },
 
+    checkGroupForIfYourTheOwner(group) {
+      return this._fetchSharedCacheURL(
+          '/groups/?owned&q=' + group);
+    },
+
+    getGroupConfig(group) {
+      return this._fetchSharedCacheURL(
+          '/groups/' + group + '/detail');
+    },
+
+    saveGroupConfig(api, group, config, opt_errFn, opt_ctx) {
+      return this.send('PUT', `/groups/${group}/${api}`, config, opt_errFn,
+          opt_ctx);
+    },
+
     getVersion() {
       return this._fetchSharedCacheURL('/config/server/version');
     },

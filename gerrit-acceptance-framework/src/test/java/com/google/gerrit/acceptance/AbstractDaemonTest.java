d1c5a93f76f76fd289e30dccad0069eb311b1605
Dave Borowitz <dborowitz@google.com>
2017-06-19 14:17:49 -0400

    GerritServer: Split site init and daemon startup
    The goal is support tests of offline programs, which by definition must
    be run while the server is stopped. Rename start to initAndStart, and
    split its components into start/init. This allows reusing a single Path
    for the site, assuming the tests are configured to use local disk.
    
    Also make GerritServer AutoCloseable, supporting usage via
    try-with-finally in test methods.
    
    Starting and stopping servers, particularly non-in-memory servers, is
    known to be very slow. This functionality is only intended to be used
    for relatively simple sanity checks of tests; this implies that the body
    of various SitePrograms should be relatively simple, and most
    functionality should be testable with a smaller test.
    
    Change-Id: I5c9de26d3d3849906cdb3f55ea532cc8f1fd7fa2

--- gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/AbstractDaemonTest.java
+++ gerrit-acceptance-framework/src/test/java/com/google/gerrit/acceptance/AbstractDaemonTest.java
@@ -276,7 +276,7 @@ public abstract class AbstractDaemonTest {
   public static void stopCommonServer() throws Exception {
     if (commonServer != null) {
       try {
-        commonServer.stop();
+        commonServer.close();
       } finally {
         commonServer = null;
       }
@@ -319,11 +319,11 @@ public abstract class AbstractDaemonTest {
     baseConfig.setInt("receive", null, "changeUpdateThreads", 4);
     if (classDesc.equals(methodDesc) && !classDesc.sandboxed() && !methodDesc.sandboxed()) {
       if (commonServer == null) {
-        commonServer = GerritServer.start(classDesc, baseConfig);
+        commonServer = GerritServer.initAndStart(classDesc, baseConfig);
       }
       server = commonServer;
     } else {
-      server = GerritServer.start(methodDesc, baseConfig);
+      server = GerritServer.initAndStart(methodDesc, baseConfig);
     }
 
     server.getTestInjector().injectMembers(this);
@@ -503,7 +503,7 @@ public abstract class AbstractDaemonTest {
       userSshSession.close();
     }
     if (server != commonServer) {
-      server.stop();
+      server.close();
       server = null;
     }
     notesMigration.resetFromEnv();

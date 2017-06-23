1889aaf99f996e817a82e160105897683597f3e0
Dave Borowitz <dborowitz@google.com>
2017-06-23 13:54:32 -0400

    Expand test for Reindex program
    Change-Id: I4caa8047e80150bfc76c76a6b5ed2a96f9176692
    WIP: Fails due to some database not getting closed.

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/pgm/ReindexIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/pgm/ReindexIT.java
@@ -14,14 +14,53 @@
 
 package com.google.gerrit.acceptance.pgm;
 
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
+import static org.junit.Assert.fail;
+
+import com.google.gerrit.acceptance.GerritServer;
 import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.StandaloneSiteTest;
+import com.google.gerrit.extensions.api.GerritApi;
+import com.google.gerrit.extensions.common.ChangeInput;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.util.ManualRequestContext;
+import java.nio.file.Files;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 @NoHttpd
 public class ReindexIT extends StandaloneSiteTest {
   @Test
-  public void reindexEmptySite() throws Exception {
+  public void reindexFromScratch() throws Exception {
+    Project.NameKey project = new Project.NameKey("project");
+    String changeId;
+    try (GerritServer server = startServer();
+        ManualRequestContext ctx = openContext(server)) {
+      GerritApi gApi = server.getTestInjector().getInstance(GerritApi.class);
+      gApi.projects().create("project");
+
+      ChangeInput in = new ChangeInput(project.get(), "master", "Test change");
+      in.newBranch = true;
+      changeId = gApi.changes().create(in).info().changeId;
+    }
+
+    FileUtils.deleteDirectory(sitePaths.index_dir.toFile());
+    Files.createDirectory(sitePaths.index_dir);
+
+    try (GerritServer server = startServer()) {
+      fail("expected server startup to fail");
+    } catch (GerritServer.StartupException e) {
+      // Expected.
+    }
+
     runGerrit("reindex", "-d", sitePaths.site_path.toString(), "--show-stack-trace");
+
+    try (GerritServer server = startServer();
+        ManualRequestContext ctx = openContext(server)) {
+      GerritApi gApi = server.getTestInjector().getInstance(GerritApi.class);
+      assertThat(gApi.changes().query("message:Test").get().stream().map(c -> c.changeId))
+          .containsExactly(changeId);
+    }
   }
 }

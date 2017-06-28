4f8fcc76bf93b5b01676c6b2ce1430380ba92622
Dave Borowitz <dborowitz@google.com>
2017-06-28 12:29:06 -0400

    Move ReindexIT.UpgradeController to its own file
    Change-Id: Id76b1baad9399037253d61f5b821c4b5444a1a7f

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/pgm/ReindexIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/pgm/ReindexIT.java
@@ -14,38 +14,27 @@
 
 package com.google.gerrit.acceptance.pgm;
 
-import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assert_;
 import static com.google.common.truth.Truth8.assertThat;
 
-import com.google.auto.value.AutoValue;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.io.MoreFiles;
 import com.google.common.io.RecursiveDeleteOption;
 import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.StandaloneSiteTest;
+import com.google.gerrit.acceptance.pgm.IndexUpgradeController.UpgradeAttempt;
 import com.google.gerrit.extensions.api.GerritApi;
 import com.google.gerrit.extensions.common.ChangeInput;
-import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.index.GerritIndexStatus;
-import com.google.gerrit.server.index.OnlineUpgradeListener;
 import com.google.gerrit.server.index.change.ChangeIndexCollection;
 import com.google.gerrit.server.index.change.ChangeSchemaDefinitions;
 import com.google.gerrit.server.query.change.InternalChangeQuery;
-import com.google.inject.AbstractModule;
-import com.google.inject.Module;
 import com.google.inject.Provider;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import org.eclipse.jgit.storage.file.FileBasedConfig;
 import org.eclipse.jgit.util.FS;
 import org.junit.Test;
@@ -91,7 +80,7 @@ public class ReindexIT extends StandaloneSiteTest {
     setUpChange();
     setOnlineUpgradeConfig(true);
 
-    UpgradeController u = new UpgradeController(1);
+    IndexUpgradeController u = new IndexUpgradeController(1);
     try (ServerContext ctx = startServer(u.module())) {
       assertSearchVersion(ctx, prevVersion);
       assertWriteVersions(ctx, prevVersion, currVersion);
@@ -174,112 +163,5 @@ public class ReindexIT extends StandaloneSiteTest {
             allVersions.stream().collect(toImmutableMap(v -> v, v -> status.getReady(CHANGES, v))))
         .named("ready state for index versions")
         .isEqualTo(allVersions.stream().collect(toImmutableMap(v -> v, v -> v == expectedReady)));
-  }
-
-  @AutoValue
-  abstract static class UpgradeAttempt {
-    static UpgradeAttempt create(String name, int oldVersion, int newVersion) {
-      return new AutoValue_ReindexIT_UpgradeAttempt(name, oldVersion, newVersion);
-    }
-
-    abstract String name();
-
-    abstract int oldVersion();
-
-    abstract int newVersion();
-  }
-
-  private static class UpgradeController implements OnlineUpgradeListener {
-    private final int numExpected;
-    private final CountDownLatch readyToStart;
-    private final CountDownLatch started;
-    private final CountDownLatch finished;
-
-    private final List<UpgradeAttempt> startedAttempts;
-    private final List<UpgradeAttempt> succeededAttempts;
-    private final List<UpgradeAttempt> failedAttempts;
-
-    UpgradeController(int numExpected) {
-      this.numExpected = numExpected;
-      readyToStart = new CountDownLatch(1);
-      started = new CountDownLatch(numExpected);
-      finished = new CountDownLatch(numExpected);
-      startedAttempts = new ArrayList<>();
-      succeededAttempts = new ArrayList<>();
-      failedAttempts = new ArrayList<>();
-    }
-
-    Module module() {
-      return new AbstractModule() {
-        @Override
-        public void configure() {
-          DynamicSet.bind(binder(), OnlineUpgradeListener.class).toInstance(UpgradeController.this);
-        }
-      };
-    }
-
-    @Override
-    public synchronized void onStart(String name, int oldVersion, int newVersion) {
-      UpgradeAttempt a = UpgradeAttempt.create(name, oldVersion, newVersion);
-      try {
-        readyToStart.await();
-      } catch (InterruptedException e) {
-        throw new AssertionError("interrupted waiting to start " + a, e);
-      }
-      checkState(
-          started.getCount() > 0, "already started %s upgrades, can't start %s", numExpected, a);
-      startedAttempts.add(a);
-      started.countDown();
-    }
-
-    @Override
-    public synchronized void onSuccess(String name, int oldVersion, int newVersion) {
-      finish(UpgradeAttempt.create(name, oldVersion, newVersion), succeededAttempts);
-    }
-
-    @Override
-    public synchronized void onFailure(String name, int oldVersion, int newVersion) {
-      finish(UpgradeAttempt.create(name, oldVersion, newVersion), failedAttempts);
-    }
-
-    private synchronized void finish(UpgradeAttempt a, List<UpgradeAttempt> out) {
-      checkState(readyToStart.getCount() == 0, "shouldn't be finishing upgrade before starting");
-      checkState(
-          finished.getCount() > 0, "already finished %s upgrades, can't finish %s", numExpected, a);
-      out.add(a);
-      finished.countDown();
-    }
-
-    void runUpgrades() throws Exception {
-      readyToStart.countDown();
-
-      // Wait with a timeout. Startup should happen quickly, but bugs preventing upgrading from
-      // starting might not be that uncommon, so we don't want to have to wait forever to discover
-      // them.
-      int timeoutSec = 60;
-      if (!started.await(timeoutSec, TimeUnit.SECONDS)) {
-        assert_()
-            .fail(
-                "%s/%s online upgrades started after %ss",
-                numExpected - started.getCount(), numExpected, timeoutSec);
-      }
-
-      // Wait with no timeout. Reindexing might be slow, and given that upgrading started
-      // successfully, it's unlikely there is a bug preventing it from tripping the finished latch
-      // eventually, even if it takes longer than we might guess.
-      finished.await();
-    }
-
-    synchronized ImmutableList<UpgradeAttempt> getStartedAttempts() {
-      return ImmutableList.copyOf(startedAttempts);
-    }
-
-    synchronized ImmutableList<UpgradeAttempt> getSucceededAttempts() {
-      return ImmutableList.copyOf(succeededAttempts);
-    }
-
-    synchronized ImmutableList<UpgradeAttempt> getFailedAttempts() {
-      return ImmutableList.copyOf(failedAttempts);
-    }
   }
 }

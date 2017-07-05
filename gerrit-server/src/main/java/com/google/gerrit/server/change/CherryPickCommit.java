54bc983c19f3f52275603cd426c407ed0bb01a25
Aaron Gable <agable@chromium.org>
2017-07-05 14:44:36 -0700

    Let cherrypick API optionally preserve reviewers/ccs
    I don't plan to change the default, or the action performed
    by the cherry-pick button in the UI, but adding this option
    to the API will allow API customers (like Chromium's "Reland"
    button) to carry forward some metadata from the original
    change to the newly-created one.
    
    Change-Id: I1d149d2758b825135460564dbf8038f4d7d52f0f

--- gerrit-server/src/main/java/com/google/gerrit/server/change/CherryPickCommit.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/CherryPickCommit.java
@@ -94,8 +94,6 @@ public class CherryPickCommit
               updateFactory,
               null,
               null,
-              null,
-              null,
               projectName,
               commit,
               input,

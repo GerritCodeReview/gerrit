12bfc690d08ce92e0156ecfc4ac58587d879cd67
Paladox none <thomasmulhall410@yahoo.com>
2017-07-11 23:05:25 +0000

    PolyGerrit: Add support for suggesting branches in cherry-pick dialog
    Mimics gwtui and makes it easier if you type a letter in, it should
    give you the results.
    
    Change-Id: I46a1192a565351389895599f1b1589bbb9e3d338

--- polygerrit-ui/app/elements/change/gr-confirm-cherrypick-dialog/gr-confirm-cherrypick-dialog.js
+++ polygerrit-ui/app/elements/change/gr-confirm-cherrypick-dialog/gr-confirm-cherrypick-dialog.js
@@ -35,6 +35,13 @@
       commitMessage: String,
       commitNum: String,
       message: String,
+      project: String,
+      _query: {
+        type: Function,
+        value() {
+          return this._getProjectBranchesSuggestions.bind(this);
+        },
+      },
     },
 
     observers: [
@@ -58,6 +65,25 @@
     _handleCancelTap(e) {
       e.preventDefault();
       this.fire('cancel', null, {bubbles: false});
+    },
+
+    _getProjectBranchesSuggestions(input) {
+      return this.$.restAPI.getProjectBranches(input, this.project, 15)
+          .then(response => {
+            console.log(response);
+            const branches = [];
+            for (const key in response) {
+              if (!response.hasOwnProperty(key)) { continue; }
+              branches.push({
+                name: key.ref,
+                value: response[key.ref],
+              });
+            }
+            console.log(branches.name);
+            console.log(branches.value);
+            console.log(branches);
+            return branches;
+          });
     },
   });
 })();

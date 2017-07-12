4855fc3d70c936f3631b2dac8b8843bc8960478e
Xin Sun <xisun@google.com>
2017-07-12 15:43:20 -0700

    Add ProjectQueryBuilder and define key predicate for project index
    Change-Id: I5ab054c3dade3dc965db040be537d21cdfa728a9

---
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/project/ProjectQueryBuilder.java
@@ -0,0 +1,41 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.project;
+
+import com.google.gerrit.reviewdb.client.AccountGroup;
+import com.google.gerrit.server.project.ProjectState;
+import com.google.gerrit.server.query.Predicate;
+import com.google.gerrit.server.query.QueryBuilder;
+import com.google.inject.Inject;
+
+/**
+ * Parses a query string meant to be applied to project objects.
+ */
+public class ProjectQueryBuilder extends QueryBuilder<ProjectState> {
+  public static final String FIELD_NAME = "name";
+
+  private static final QueryBuilder.Definition<ProjectState, ProjectQueryBuilder> mydef =
+      new QueryBuilder.Definition<>(ProjectQueryBuilder.class);
+
+  @Inject
+  ProjectQueryBuilder() {
+    super(mydef);
+  }
+
+  @Operator
+  public Predicate<ProjectState> name(String name) {
+    return ProjectPredicates.name(new Project.Namekey(name));
+  }
+}

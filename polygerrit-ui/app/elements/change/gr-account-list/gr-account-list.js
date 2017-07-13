a419f53a289020b2a23611eec869eab18cde8348
Becky Siegel <beckysiegel@google.com>
2017-07-13 11:43:59 -0700

    Update iron-input to 2.0
    This version is compatible with Polymer 1 and Polymer 2, but required
    for Polymer 2.
    
    Elements that were formerly
    <input is="iron-input>
    
    are now
    <iron-input>
      <input>
    </iron-input>
    
    There are a few scenarios in which inputs were not using two way data
    binding, which is the reason for using iron-input, and those have been
    modified back to a native input.
    
    With the updated iron-input to access the native input, there is an
    'inputElement' getter function, which is used heavily in this update.
    
    Also of note, in many tests, it is required to wrap Polymer.Base.async,
    which is necessary because the mutation observer is async:
    
    https://github.com/PolymerElements/iron-input/blob/master/test/iron-input.html
    
    Change-Id: I75f7fa1bb0c00837f631f6e1043e15a3270b9bce

--- polygerrit-ui/app/elements/change/gr-account-list/gr-account-list.js
+++ polygerrit-ui/app/elements/change/gr-account-list/gr-account-list.js
@@ -194,7 +194,7 @@
     },
 
     _handleInputKeydown(e) {
-      const input = e.detail.input;
+      const input = e.detail.input.inputElement;
       if (input.selectionStart !== input.selectionEnd ||
           input.selectionStart !== 0) {
         return;

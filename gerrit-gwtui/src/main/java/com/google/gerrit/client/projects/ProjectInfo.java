cee4baeb18bbea63c0cc0886ecdc21dddab02017
Paladox none <thomasmulhall410@yahoo.com>
2017-07-16 13:49:03 +0000

    Support for pagination including filters on plugins screen
    Change-Id: Ia08879e187e1c7e06eda1cd2002ecf3a170338be

--- gerrit-gwtui/src/main/java/com/google/gerrit/client/projects/ProjectInfo.java
+++ gerrit-gwtui/src/main/java/com/google/gerrit/client/projects/ProjectInfo.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 The Android Open Source Project
+// Copyright (C) 2017 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,21 +14,16 @@
 
 package com.google.gerrit.client.projects;
 
+import com.google.gerrit.client.general.Suggest;
 import com.google.gerrit.client.info.WebLinkInfo;
 import com.google.gerrit.extensions.client.ProjectState;
 import com.google.gerrit.reviewdb.client.Project;
-import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
-import com.google.gwt.user.client.ui.SuggestOracle;
 
-public class ProjectInfo extends JavaScriptObject implements SuggestOracle.Suggestion {
+public class ProjectInfo extends Suggest {
   public final Project.NameKey name_key() {
     return new Project.NameKey(name());
   }
-
-  public final native String name() /*-{ return this.name; }-*/;
-
-  public final native String description() /*-{ return this.description; }-*/;
 
   public final native JsArray<WebLinkInfo> webLinks() /*-{ return this.web_links; }-*/;
 
@@ -37,19 +32,6 @@ public class ProjectInfo extends JavaScriptObject implements SuggestOracle.Sugge
   }
 
   private native String getStringState() /*-{ return this.state; }-*/;
-
-  @Override
-  public final String getDisplayString() {
-    if (description() != null) {
-      return name() + " (" + description() + ")";
-    }
-    return name();
-  }
-
-  @Override
-  public final String getReplacementString() {
-    return name();
-  }
 
   protected ProjectInfo() {}
 }

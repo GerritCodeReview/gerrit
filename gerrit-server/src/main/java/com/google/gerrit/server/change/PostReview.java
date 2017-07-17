0ce6816f54c36692d5395111c7ae761b55a785bb
Changcheng Xiao <xchangcheng@google.com>
2017-07-17 15:00:34 +0200

    PostReview: Remove support for 'strictLabels' option
    Currently we support strict/non-strict label in PostReview,
    which is controlled by the 'strictLabels' option.
    
    For strict labels, we require all labels to be within the
    user's granted range. Attempting to vote a label value not
    granted will fail the entire operation. For non-strict labels,
    we squashes the proposed value to the nearest granted value
    and the operation will be executed.
    
    This change removes the support for non-strict labels.
    
    Change-Id: Icb9dfafea567e1e9c469811e830e3519b865700f

--- gerrit-server/src/main/java/com/google/gerrit/server/change/PostReview.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/change/PostReview.java
@@ -213,13 +213,20 @@ public class PostReview
     if (revision.getEdit().isPresent()) {
       throw new ResourceConflictException("cannot post review on edit");
     }
+
+    // Remove after the backend lives for a time.
+    if (!input.strictLabels) {
+      throw new BadRequestException(
+          "strictLabels option is deprecated, all labels must be within the permitted ranges.");
+    }
+
     if (input.onBehalfOf != null) {
       revision = onBehalfOf(revision, input);
     } else if (input.drafts == null) {
       input.drafts = DraftHandling.DELETE;
     }
     if (input.labels != null) {
-      checkLabels(revision, input.strictLabels, input.labels);
+      checkLabels(revision, input.labels);
     }
     if (input.comments != null) {
       cleanUpComments(input.comments);
@@ -430,12 +437,9 @@ public class PostReview
     while (itr.hasNext()) {
       Map.Entry<String, Short> ent = itr.next();
       LabelType type = labelTypes.byLabel(ent.getKey());
-      if (type == null && in.strictLabels) {
+      if (type == null) {
         throw new BadRequestException(
             String.format("label \"%s\" is not a configured label", ent.getKey()));
-      } else if (type == null) {
-        itr.remove();
-        continue;
       }
 
       if (!caller.isInternalUser()) {
@@ -466,7 +470,7 @@ public class PostReview
     return new RevisionResource(changes.parse(ctl), rev.getPatchSet());
   }
 
-  private void checkLabels(RevisionResource rsrc, boolean strict, Map<String, Short> labels)
+  private void checkLabels(RevisionResource rsrc, Map<String, Short> labels)
       throws BadRequestException, AuthException, PermissionBackendException {
     LabelTypes types = rsrc.getControl().getLabelTypes();
     PermissionBackend.ForChange perm = rsrc.permissions();
@@ -475,12 +479,8 @@ public class PostReview
       Map.Entry<String, Short> ent = itr.next();
       LabelType lt = types.byLabel(ent.getKey());
       if (lt == null) {
-        if (strict) {
-          throw new BadRequestException(
-              String.format("label \"%s\" is not a configured label", ent.getKey()));
-        }
-        itr.remove();
-        continue;
+        throw new BadRequestException(
+            String.format("label \"%s\" is not a configured label", ent.getKey()));
       }
 
       if (ent.getValue() == null || ent.getValue() == 0) {
@@ -490,23 +490,16 @@ public class PostReview
       }
 
       if (lt.getValue(ent.getValue()) == null) {
-        if (strict) {
-          throw new BadRequestException(
-              String.format("label \"%s\": %d is not a valid value", ent.getKey(), ent.getValue()));
-        }
-        itr.remove();
-        continue;
+        throw new BadRequestException(
+            String.format("label \"%s\": %d is not a valid value", ent.getKey(), ent.getValue()));
       }
 
       short val = ent.getValue();
       try {
         perm.check(new LabelPermission.WithValue(lt, val));
       } catch (AuthException e) {
-        if (strict) {
-          throw new AuthException(
-              String.format("Applying label \"%s\": %d is restricted", lt.getName(), val));
-        }
-        ent.setValue(perm.squashThenCheck(lt, val));
+        throw new AuthException(
+            String.format("Applying label \"%s\": %d is restricted", lt.getName(), val));
       }
     }
   }

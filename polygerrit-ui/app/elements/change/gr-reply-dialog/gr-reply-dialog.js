3e0917e10170f03b19a450cff4ee77e85dc9d5c3
Logan Hanks <logan@google.com>
2017-07-19 08:32:25 -0700

    Refactor and test sending in gr-reply-dialog
    The asynchronous logic in the send method has gotten complex and messy,
    and there may be some bugs hidden there. This change reorganizes the
    logic a bit to maintain some important invariants, and adds tests to be
    sure.
    
    The two most important invariants are:
    
    1) disabled is true until all promises are resolved
    2) draft/_includeComments are only reset if the review was posted
    
    Bug: Issue 6770
    Change-Id: I25dd7f64b32be778720747c474a56373cf538e6e

--- polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.js
+++ polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.js
@@ -343,7 +343,7 @@
       if (this.knownLatestState === 'not-latest') {
         this.fire('show-alert',
             {message: 'Cannot reply to non-latest patch.'});
-        return;
+        return Promise.resolve({});
       }
 
       const labels = this.$.labelScores.getLabelValues();
@@ -391,38 +391,60 @@
 
       const errFn = this._handle400Error.bind(this);
       return this._saveReview(obj, errFn).then(response => {
-        if (!response || !response.ok) {
-          return response;
+        if (!response) {
+          // Null or undefined response indicates that an error handler
+          // took responsibility, so just return.
+          return {};
         }
-        return this.$.restAPI.getResponseObject(response);
+        if (!response.ok) {
+          this.fire('server-error', {response});
+          return {};
+        }
+
+        // TODO(logan): Remove once the required API changes are live and stable
+        // on googlesource.com.
+        return this._maybeSetReady(startReview, response).catch(err => {
+          // We catch error here because we still want to treat this as a
+          // successful review.
+          console.error('error setting ready:', err);
+        }).then(() => {
+          this.draft = '';
+          this._includeComments = true;
+          this.fire('send', null, {bubbles: false});
+          return accountAdditions;
+        });
       }).then(result => {
-        // TODO(logan): Remove this once the required API changes are live and
-        // stable on googlesource.com.
-        if (startReview && !result.ready) {
-          // If we don't see ready in the response, then we're talking to a
-          // backend that doesn't support moving out of WIP at the same time as
-          // posting a review. Fall back to sending a second API call to start
-          // review and block until that returns.
-          return this.$.restAPI.startReview(this.change._number, null,
-              response => {
-                // If we see a 409 response code, then that means the server
-                // *does* support moving from WIP->ready when posting a review.
-                // In that case we should just ignore this error.
-                if (response.status === 409) {
-                  return;
-                }
-                this.fire('server-error', {response});
-              });
-        }
-      }).then(() => {
         this.disabled = false;
-        this.draft = '';
-        this._includeComments = true;
-        this.fire('send', null, {bubbles: false});
-        return accountAdditions;
+        return result;
       }).catch(err => {
         this.disabled = false;
         throw err;
+      });
+    },
+
+    /**
+     * Returns a promise resolving to true if review was successfully posted,
+     * false otherwise.
+     *
+     * TODO(logan): Remove this once the required API changes are live and
+     * stable on googlesource.com.
+     */
+    _maybeSetReady(startReview, response) {
+      return this.$.restAPI.getResponseObject(response).then(result => {
+        if (!startReview || result.ready) {
+          return Promise.resolve();
+        }
+        // We don't have confirmation that review was started, so attempt to
+        // start review explicitly.
+        return this.$.restAPI.startReview(
+            this.change._number, null, response => {
+              // If we see a 409 response code, then that means the server
+              // *does* support moving from WIP->ready when posting a
+              // review. Only alert user for non-409 failures.
+              if (response.status !== 409) {
+                this.fire('server-error', {response});
+              }
+            });
       });
     },
 

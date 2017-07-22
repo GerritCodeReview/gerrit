2bf2082fee33fa8f71fb83a41566a49dd075a4ac
Paladox none <thomasmulhall410@yahoo.com>
2017-07-22 15:36:52 +0000

    PolyGerrit: Fix caching the app by introducing a versioned string
    This adds a version string so that in the gerrit config you up the number
    so that in prod when they upgrade to gerrit 2.15 or 3.x they wont have to
    tell there users to clean there cache.
    
    Change-Id: I39c436d560519ec008b690c46e6c4730e169687d

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/StaticModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/raw/StaticModule.java
@@ -255,7 +255,8 @@ public class StaticModule extends ServletModule {
         @CanonicalWebUrl @Nullable String canonicalUrl, @GerritServerConfig Config cfg)
         throws URISyntaxException {
       String cdnPath = cfg.getString("gerrit", null, "cdnPath");
-      return new IndexServlet(canonicalUrl, cdnPath);
+      String staticVersion = cfg.getString("gerrit", null, "polyGerritStaticVersion");
+      return new IndexServlet(canonicalUrl, cdnPath, staticVersion);
     }
 
     @Provides

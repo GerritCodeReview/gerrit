4a49842d39e8571fc4552e70de245ff61b6676fc
Paladox none <thomasmulhall410@yahoo.com>
2017-07-23 13:30:38 +0000

    PolyGerrit: Create a new shared create dialog for projects, groups, branches and tags
    Change-Id: I56aee8cc6f4fb48ae9650b7ecb8891bf952a330f

--- polygerrit-ui/app/elements/admin/gr-create-pointer-dialog/gr-create-pointer-dialog.js
+++ polygerrit-ui/app/elements/admin/gr-create-pointer-dialog/gr-create-pointer-dialog.js
@@ -31,8 +31,18 @@
         value: false,
       },
       itemDetail: String,
-      _itemName: String,
-      _itemRevision: String,
+      _itemConfig: {
+        type: Object,
+        value: () => { return {}; },
+      },
+      _showCreateProject: Boolean,
+      _showCreateItem: Boolean,
+      _query: {
+        type: Function,
+        value() {
+          return this._getItemSuggestions.bind(this);
+        },
+      },
     },
 
     behaviors: [
@@ -41,40 +51,90 @@
     ],
 
     observers: [
-      '_updateItemName(_itemName)',
+      '_updateItemName(_itemConfig.name)',
+      '_paramsChanged(itemDetail)',
     ],
 
     _updateItemName(name) {
       this.hasNewItemName = !!name;
     },
 
-    _computeItemUrl(project) {
-      if (this.itemDetail === DETAIL_TYPES.branches) {
+    _paramsChanged(params) {
+      this.set('_showCreateProject', params === 'projects');
+      this.set('_showCreateItem', params === 'branches' || params === 'tags');
+    },
+
+    _computeItemUrl(item, name) {
+      if (item === 'branches') {
         return this.getBaseUrl() + '/admin/projects/' +
-            this.encodeURL(this.projectName, true) + ',branches';
-      } else if (this.itemDetail === DETAIL_TYPES.tags) {
+            this.encodeURL(name, true) + ',branches';
+      } else if (item === 'tags') {
         return this.getBaseUrl() + '/admin/projects/' +
-            this.encodeURL(this.projectName, true) + ',tags';
+            this.encodeURL(name, true) + ',tags';
+      } else if (item === 'projects') {
+        return this.getBaseUrl() + '/admin/projects/' +
+            this.encodeURL(name, true);
+      } else if (item === 'groups') {
+        return this.getBaseUrl() + '/admin/groups/' +
+            this.encodeURL(name, true);
       }
     },
 
     handleCreateItem() {
-      const USE_HEAD = this._itemRevision ? this._itemRevision : 'HEAD';
+      const USE_HEAD = this._itemConfig.revision ?
+            this._itemConfig.revision : 'HEAD';
       if (this.itemDetail === DETAIL_TYPES.branches) {
         return this.$.restAPI.createProjectBranch(this.projectName,
             this._itemName, {revision: USE_HEAD})
             .then(itemRegistered => {
               if (itemRegistered.status === 201) {
-                page.show(this._computeItemUrl(this.itemDetail));
+                page.show(
+                    this._computeItemUrl(this.itemDetail, this.projectName));
               }
             });
       } else if (this.itemDetail === DETAIL_TYPES.tag) {
         return this.$.restAPI.createProjectTag(this.projectName,
-            this._itemName, {revision: USE_HEAD})
+            this._itemConfig.name, {revision: USE_HEAD})
             .then(itemRegistered => {
               if (itemRegistered.status === 201) {
-                page.show(this._computeItemUrl(this.itemDetail));
+                page.show(
+                    this._computeItemUrl(this.itemDetail, this.projectName));
               }
+            });
+      } else if (this.itemDetail === 'projects') {
+        return this.$.restAPI.createProject(this._itemConfig)
+            .then(projectRegistered => {
+              if (projectRegistered.status === 201) {
+                page.show(this._computeItemUrl(
+                    this.itemDetail, this._itemConfig.name));
+              }
+            });
+      } else if (this.itemDetail === 'groups') {
+        return this.$.restAPI.createGroup({name: this._itemConfig.name})
+            .then(groupRegistered => {
+              if (groupRegistered.status !== 201) { return; }
+              return this.$.restAPI.getGroupConfig(this._itemConfig.name)
+                  .then(group => {
+                    page.show(
+                        this._computeItemUrl(this.itemDetail, group.group_id));
+                  });
+            });
+      }
+    },
+
+    _getItemSuggestions(input) {
+      if (this.itemDetail === 'projects') {
+        return this.$.restAPI.getSuggestedProjects(input)
+            .then(response => {
+              const projects = [];
+              for (const key in response) {
+                if (!response.hasOwnProperty(key)) { continue; }
+                projects.push({
+                  name: key,
+                  value: response[key],
+                });
+              }
+              return projects;
             });
       }
     },

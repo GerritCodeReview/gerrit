5fbf7bb6cb2475b79e4126f5a3e3bbfd36f624c5
David Pursehouse <dpursehouse@collab.net>
2017-07-25 13:30:46 +0100

    ListPlugins#display: Return Map<String, PluginInfo> instead of JsonElement
    Returning a Map<String, PluginInfo> makes it more consistent with
    how the other REST endpoints are implemented, and makes it easier
    to use in API calls.
    
    Also annotate the PrintWriter argument as @Nullable.
    
    Change-Id: I63cdffcf9970f6670bedf4479fd02a65afed6478

--- gerrit-server/src/main/java/com/google/gerrit/server/plugins/ListPlugins.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/plugins/ListPlugins.java
@@ -16,6 +16,7 @@ package com.google.gerrit.server.plugins;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
+import com.google.gerrit.common.Nullable;
 import com.google.gerrit.common.data.GlobalCapability;
 import com.google.gerrit.extensions.annotations.RequiresCapability;
 import com.google.gerrit.extensions.common.PluginInfo;
@@ -23,7 +24,6 @@ import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.extensions.restapi.TopLevelResource;
 import com.google.gerrit.extensions.restapi.Url;
 import com.google.gerrit.server.OutputFormat;
-import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import com.google.inject.Inject;
 import java.io.PrintWriter;
@@ -74,7 +74,7 @@ public class ListPlugins implements RestReadView<TopLevelResource> {
     return display(null);
   }
 
-  public JsonElement display(PrintWriter stdout) {
+  public Map<String, PluginInfo> display(@Nullable PrintWriter stdout) {
     Map<String, PluginInfo> output = new TreeMap<>();
     List<Plugin> plugins = Lists.newArrayList(pluginLoader.getPlugins(all));
     Collections.sort(
@@ -107,9 +107,7 @@ public class ListPlugins implements RestReadView<TopLevelResource> {
     }
 
     if (stdout == null) {
-      return OutputFormat.JSON
-          .newGson()
-          .toJsonTree(output, new TypeToken<Map<String, Object>>() {}.getType());
+      return output;
     } else if (format.isJson()) {
       format
           .newGson()

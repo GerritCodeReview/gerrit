3baccea9951b38cc387832a40e66fcdf254d9b3b
Paladox none <thomasmulhall410@yahoo.com>
2017-07-27 16:36:58 +0000

    PolyGerrit: Implement support for New Agreement screen
    Bug: Issue 6866
    Bug: Issue 6783
    Change-Id: I3e69d317255f7318e225ff54cf192d9812fe0a70

--- polygerrit-ui/app/elements/settings/gr-cla-view/gr-cla-view.js
+++ polygerrit-ui/app/elements/settings/gr-cla-view/gr-cla-view.js
@@ -14,11 +14,135 @@
 (function() {
   'use strict';
 
+  const TOAST_DEBOUNCE_INTERVAL = 200;
+
   Polymer({
     is: 'gr-cla-view',
 
     properties: {
-      path: String,
+      _groups: Object,
+      /** @type {?} */
+      _serverConfig: Object,
+      _agreementsText: String,
+      _agreementName: String,
+      _showAgreements: {
+        type: Boolean,
+        value: false,
+      },
+      _agreementsUrl: String,
+    },
+
+    behaviors: [
+      Gerrit.BaseUrlBehavior,
+    ],
+
+    attached() {
+      this.loadData();
+    },
+
+    loadData() {
+      const promises = [];
+      promises.push(this.$.restAPI.getConfig(true).then(config => {
+        this._serverConfig = config;
+      }));
+
+      promises.push(this.$.restAPI.getAccountGroups(true).then(groups => {
+        this._groups = groups.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }));
+
+      return Promise.all(promises);
+    },
+
+    _getAgreementsUrl(configUrl) {
+      let url;
+      if (!configUrl) { return ''; }
+      if (configUrl.startsWith('http:') || configUrl.startsWith('https:')) {
+        url = configUrl;
+      } else {
+        url = this.getBaseUrl() + '/' + configUrl;
+      }
+
+      return url;
+    },
+
+    _handleShowAgreement(e) {
+      this._agreementName = e.target.getAttribute('data-name');
+      const url = e.target.getAttribute('data-url');
+      this._agreementsUrl = this._getAgreementsUrl(url);
+      this._showAgreements = true;
+    },
+
+    _handleSaveAgreements(e) {
+      const name = this._agreementName;
+      return this.$.restAPI.saveAccountAgreement({name}).then(res => {
+        let message;
+        if (res.status === 200) {
+          message = 'Agreement has been successfully submited.';
+        } else {
+          message = 'Agreement failed to be submitted, please try again';
+        }
+        this.debounce('agreements-toast', () => {
+          document.body.dispatchEvent(
+              new CustomEvent(
+                'show-alert', {detail: {message}, bubbles: true}));
+        }, TOAST_DEBOUNCE_INTERVAL);
+        this.loadData();
+        this._agreementsText = '';
+        this._showAgreements = false;
+      });
+    },
+
+    _computeShowAgreementsClass(agreements) {
+      if (agreements) {
+        return 'show';
+      }
+
+      return '';
+    },
+
+    _disableAggreements(item, groups) {
+      for (const value of groups) {
+        if (item && item.auto_verify_group &&
+            item.auto_verify_group.name === value.name) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+
+    _hideAggreements(item, groups) {
+      if (this._disableAggreements(item, groups)) {
+        return '';
+      } else {
+        return 'agreementsSubmitted';
+      }
+    },
+
+    _disableAgreementsText(text) {
+      if (text.toLowerCase() === 'i agree') {
+        return false;
+      }
+
+      return true;
+    },
+
+    // what this does is it loops through to find auto_verify_group
+    // if specified, it means that you signed the agreement.
+    _computeHideAgreementClass(name, config) {
+      for (const key in config) {
+        if (!config.hasOwnProperty(key)) { return; }
+        const groupObject = config[key];
+        for (const prop in groupObject) {
+          if (!groupObject.hasOwnProperty(prop)) { return; }
+          if (name === config[key].name &&
+              !config[key].auto_verify_group) {
+            return 'hideAgreementsTextBox';
+          }
+        }
+      }
     },
   });
 })();

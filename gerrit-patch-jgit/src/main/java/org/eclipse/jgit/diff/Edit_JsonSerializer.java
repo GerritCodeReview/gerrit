0ce91d40e258dfefe7b982fbdf4509d0b84a3906
David Ostrovsky <david@ostrovsky.org>
2017-07-29 06:49:38 +0200

    Remove GWT UI
    Known issues:
    
    * some plugins depend on GWT extension point and must be adapted to
      target PG UI
    * Documentation wasn't adapted in this change
    
    Change-Id: I767118ba4a70f288b8f56686ab90aefa059912bb

--- gerrit-patch-jgit/src/main/java/org/eclipse/jgit/diff/Edit_JsonSerializer.java
+++ gerrit-patch-jgit/src/main/java/org/eclipse/jgit/diff/Edit_JsonSerializer.java
@@ -14,35 +14,13 @@
 
 package org.eclipse.jgit.diff;
 
-import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwtjsonrpc.client.impl.JsonSerializer;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Edit_JsonSerializer extends JsonSerializer<Edit> {
-  public static final Edit_JsonSerializer INSTANCE = new Edit_JsonSerializer();
 
   @Override
   public Edit fromJson(Object jso) {
-    if (jso == null) {
-      return null;
-    }
-
-    final JavaScriptObject o = (JavaScriptObject) jso;
-    final int cnt = length(o);
-    if (4 == cnt) {
-      return new Edit(get(o, 0), get(o, 1), get(o, 2), get(o, 3));
-    }
-
-    List<Edit> l = new ArrayList<>((cnt / 4) - 1);
-    for (int i = 4; i < cnt; ) {
-      int as = get(o, i++);
-      int ae = get(o, i++);
-      int bs = get(o, i++);
-      int be = get(o, i++);
-      l.add(new Edit(as, ae, bs, be));
-    }
-    return new ReplaceEdit(get(o, 0), get(o, 1), get(o, 2), get(o, 3), l);
+    return null;
   }
 
   @Override
@@ -67,8 +45,4 @@ public class Edit_JsonSerializer extends JsonSerializer<Edit> {
     sb.append(',');
     sb.append(o.getEndB());
   }
-
-  private static native int length(JavaScriptObject jso) /*-{ return jso.length; }-*/;
-
-  private static native int get(JavaScriptObject jso, int idx) /*-{ return jso[idx]; }-*/;
 }

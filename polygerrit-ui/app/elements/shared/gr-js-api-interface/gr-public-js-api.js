4f13e125cf23d0fca4da8fff99d24953e981133d
Paladox none <thomasmulhall410@yahoo.com>
2017-07-29 14:43:11 +0000

    PolyGerrit: Make plugins CANONICAL_PATH aware
    Bug: Issue 6890
    Change-Id: I71e221ca9442f36fb635ca210d1aa66afb083d75

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-public-js-api.js
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-public-js-api.js
@@ -54,6 +54,9 @@
   // http://www.gwtproject.org/doc/latest/DevGuideCodingBasicsJSNI.html
   window.$wnd = window;
 
+  const base = window.Gerrit.BaseUrlBehavior.getBaseUrl();
+  const baseUrl = base + '/';
+
   function Plugin(opt_url) {
     this._generatedHookNames = [];
     this._hooks = [];
@@ -65,7 +68,7 @@
     }
 
     this._url = new URL(opt_url);
-    if (!this._url.pathname.startsWith('/plugins')) {
+    if (!this._url.pathname.startsWith(baseUrl + 'plugins')) {
       console.warn('Plugin not being loaded from /plugins base path:',
           this._url.href, '— Unable to determine name.');
       return;

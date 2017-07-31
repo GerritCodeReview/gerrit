44dc0846220763d5b511379eda5314f8f417342f
Paladox none <thomasmulhall410@yahoo.com>
2017-07-31 21:45:05 +0000

    PolyGerrit: Show uploader on the change screen
    This also adds a check to not show Uploader: if the uploader is the owner
    of the change.
    
    Change-Id: I3c3ec89590e5927cd50cbe35c325aaada98a2a74

--- polygerrit-ui/app/elements/change/gr-change-metadata/gr-change-metadata.js
+++ polygerrit-ui/app/elements/change/gr-change-metadata/gr-change-metadata.js
@@ -332,5 +332,37 @@
     _computeIsWip(change) {
       return !!change.work_in_progress;
     },
+
+    _computeShowUploaderHide(change) {
+      if (!change.current_revision ||
+          !change.revisions[change.current_revision]) {
+        return 'hideDisplay';
+      }
+
+      const rev = change.revisions[change.current_revision];
+
+      if (!rev || !rev.uploader ||
+         change.owner._account_id === rev.uploader._account_id) {
+        return 'hideDisplay';
+      }
+
+      return '';
+    },
+
+    _computeShowUploader(change) {
+      if (!change.current_revision ||
+          !change.revisions[change.current_revision]) {
+        return '';
+      }
+
+      const rev = change.revisions[change.current_revision];
+
+      if (!rev || !rev.uploader ||
+         change.owner._account_id === rev.uploader._account_id) {
+        return '';
+      }
+
+      return rev.uploader;
+    },
   });
 })();

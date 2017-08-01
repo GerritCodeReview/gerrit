04b3fb787004fe7ae501470f46067be284ab498f
Paladox none <thomasmulhall410@yahoo.com>
2017-08-01 20:23:19 +0000

    PolyGerrit: Add project command to gr-project
    * Supports creating a change from the ui (will have to use gwtui to add
    files though).
    
    * Adds a run gc button.
    
    Change-Id: Ie1922a6910ad7e027a962d39aaab24f64d72cc48

--- polygerrit-ui/app/elements/admin/gr-project/gr-project.js
+++ polygerrit-ui/app/elements/admin/gr-project/gr-project.js
@@ -248,5 +248,26 @@
       }
       return commands;
     },
+
+    _handleRunningGC() {
+      return this.$.restAPI.runProjectGC(this.project).then(response => {
+            if (response.status === 200) {
+              return window.alert('Garbage collection completed successfully.');
+            }
+          });
+    },
+
+    _createNewChange() {
+      this.$.createChangeOverlay.open();
+    },
+
+    _handleCreateChange() {
+      this.$.createNewChangeModal.handleCreateChange();
+      this._handleCloseCreateChange();
+    },
+
+    _handleCloseCreateChange() {
+      this.$.createChangeOverlay.close();
+    },
   });
 })();

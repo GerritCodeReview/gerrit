1f2eea5b8c4c8b5bf8426009cb3ae5fdb0331709
Paladox none <thomasmulhall410@yahoo.com>
2017-08-03 20:15:03 +0000

    PolyGerrit: Add support for project command
    * This adds a new page called Project Command accessed by doing
    <url>/projects/<project>,commands
    
    * This supports creating a change through the new dialog (though note
    you carn't edit any files, will have to switch to gwtui to do that)
    
    * Supports the Run GC button allowing you to run gc on projects.
    
    Change-Id: Ibd3507c0fdb84303c8a978e3ea87f251582b13e8

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -206,6 +206,12 @@
           opt_errFn, opt_ctx);
     },
 
+    runProjectGC(project, opt_errFn, opt_ctx) {
+      const encodeName = encodeURIComponent(project);
+      return this.send('POST', `/projects/${encodeName}/gc`, '',
+          opt_errFn, opt_ctx);
+    },
+
     createProject(config, opt_errFn, opt_ctx) {
       if (!config.name) { return ''; }
       const encodeName = encodeURIComponent(config.name);
@@ -897,6 +903,13 @@
       const url = this.getChangeActionURL(changeNum, patchNum, '/review');
       return this.awaitPendingDiffDrafts()
           .then(() => this.send('POST', url, review, opt_errFn, opt_ctx));
+    },
+
+    createChange(createChangeParams) {
+      return this.send('POST', '/changes/', createChangeParams)
+          .then(response =>
+            this.getResponseObject(response)
+          );
     },
 
     getFileInChangeEdit(changeNum, path) {

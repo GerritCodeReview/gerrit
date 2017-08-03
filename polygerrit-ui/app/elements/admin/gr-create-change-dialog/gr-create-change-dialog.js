f4cf43b69aa9bcb0c2cc8db0fab0c0138efac6f1
Paladox none <thomasmulhall410@yahoo.com>
2017-08-03 20:15:03 +0000

    PolyGerrit: Add support for project command
    * This adds a new page called Project Command accessed by navigating to
    <url>/projects/<project>,commands
    
    * This supports creating a change through the new dialog (though note
    you can't edit any files, will have to switch to gwtui to do that)
    
    * Supports creating the change as a private and wip or one or the other.
    
    * Supports the Run GC button allowing you to run gc on projects.
    
    Feature: Issue 5232
    Change-Id: Ibd3507c0fdb84303c8a978e3ea87f251582b13e8

---
+++ polygerrit-ui/app/elements/admin/gr-create-change-dialog/gr-create-change-dialog.js
@@ -0,0 +1,101 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  const SUGGESTIONS_LIMIT = 15;
+  const REF_PREFIX = 'refs/heads/';
+
+  Polymer({
+    is: 'gr-create-change-dialog',
+
+    properties: {
+      projectName: String,
+      project: String,
+      branch: String,
+      subject: String,
+      topic: String,
+      opt_params: {
+        type: Object,
+        value: () => { return {}; },
+      },
+      _query: {
+        type: Function,
+        value() {
+          return this._getProjectBranchesSuggestions.bind(this);
+        },
+      },
+      checkedPrivate: {
+        type: Boolean,
+        value: true,
+      },
+      _serverConfig: Object,
+    },
+
+    behaviors: [
+      Gerrit.BaseUrlBehavior,
+      Gerrit.URLEncodingBehavior,
+    ],
+
+    attached() {
+      this.$.restAPI.getConfig().then(config => {
+        this._serverConfig = config;
+      });
+    },
+
+    handleCreateChange() {
+      if (this.$.privateChangeCheckBox.checked) {
+        this.opt_params.is_private = true;
+      }
+      if (this.$.wipChangeCheckBox.checked) {
+        this.opt_params.work_in_progress = true;
+      }
+      this.project = this.projectName;
+      if (!this.subject) {
+        this.subject = 'Insert the description of the change.';
+      }
+      return this.$.restAPI.createChange(this.project, this.branch,
+          this.subject, this.topic, this.opt_params)
+          .then(changeCreated => {
+            if (!changeCreated) {
+              return '';
+            }
+            Gerrit.Nav.navigateToChange(changeCreated);
+          });
+    },
+
+    _getProjectBranchesSuggestions(input) {
+      if (input.startsWith(REF_PREFIX)) {
+        input = input.substring(REF_PREFIX.length);
+      }
+      return this.$.restAPI.getProjectBranches(
+          input, this.projectName, SUGGESTIONS_LIMIT).then(response => {
+            const branches = [];
+            let branch;
+            for (const key in response) {
+              if (!response.hasOwnProperty(key)) { continue; }
+              if (response[key].ref.startsWith('refs/heads/')) {
+                branch = response[key].ref.substring('refs/heads/'.length);
+              } else {
+                branch = response[key].ref;
+              }
+              branches.push({
+                name: branch,
+              });
+            }
+            return branches;
+          });
+    },
+  });
+})();

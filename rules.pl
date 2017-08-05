168fe0bea17fb2caa57171c30ff8839927567f17
David Ostrovsky <david.ostrovsky@gmail.com>
2017-08-05 15:26:36 +0000

    RFC: Merge PR and CR labels
    Merge the PR and CR labels and grant PR maintainers CR+2.
    
    Change-Id: I36c6132da9a2b6fa41853ee2922814952fc758b7

--- rules.pl
+++ rules.pl
@@ -31,14 +31,7 @@ submit_rule(submit(CR, CS, V, L)) :-
   gerrit:max_with_block(-1, 1, 'Library-Compliance', L).
 
 submit_rule(submit(CR, CS, V)) :-
-  is_no_polygerrit_change,
-  !,
   base(CR, CS, V).
-
-submit_rule(submit(P, CS, V)) :-
-  gerrit:max_with_block(-2, 2, 'PolyGerrit-Review', P),
-  gerrit:max_with_block(-1, 1, 'Code-Style', CS),
-  gerrit:max_with_block(-1, 1, 'Verified', V).
 
 base(CR, CS, V) :-
   gerrit:max_with_block(-2, 2, 'Code-Review', CR),
@@ -48,6 +41,3 @@ base(CR, CS, V) :-
 needs_library_compliance :- gerrit:commit_delta('^lib/'), !.
 needs_library_compliance :- gerrit:commit_delta('^[.]buckversion$'), !.
 needs_library_compliance :- gerrit:commit_delta('^WORKSPACE$'), !.
-
-is_no_polygerrit_change :-
-  gerrit:commit_delta('^(?!polygerrit-ui/|gerrit-server/src/main/resources/com/google/gerrit/server/mail/).*$').

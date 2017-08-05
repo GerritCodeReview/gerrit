9e871f5ec345bb7212d4e64756d230eb36c012e7
Becky Siegel <beckysiegel@google.com>
2017-08-04 17:50:49 -0700

    build
    Change-Id: I84e5188883ff758b0f26fd9114463c27309b1297

---
+++ polygerrit-ui/app/template_test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -ex
+
+npm_bin=$(which npm)
+if [ -z "$npm_bin" ]; then
+    echo "NPM must be on the path."
+    exit 1
+fi
+
+node_bin=$(which node)
+if [ -z "$node_bin" ]; then
+    echo "node must be on the path."
+    exit 1
+fi
+
+fried_twinkie_config=$(npm list -g | grep -c fried-twinkie)
+typescript_config=$(npm list -g | grep -c typescript)
+if [ -z "$npm_bin" ] || [ "$fried_twinkie_config" -eq "0" ]; then
+    echo "You must install fried twinkie and its dependencies from NPM."
+    echo "> npm install -g fried-twinkie"
+    echo "For more information, view the README:"
+    echo "https://gerrit.googlesource.com/gerrit/+/master/polygerrit-ui/#Style-guide"
+    exit 1
+fi
+
+python $TEST_SRCDIR/gerrit/polygerrit-ui/app/template_test/convert_to_goog_module.py
+
+# ${node_bin} $TEST_SRCDIR/gerrit/polygerrit-ui/app/template_test_files/template_test.js
+
+# python $TEST_SRCDIR/gerrit/polygerrit-ui/app/template_test_files/remove_goog_modules.py

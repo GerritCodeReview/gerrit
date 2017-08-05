fe684fcf783c286b1b97a8456a90dc9f263f7a5a
Becky Siegel <beckysiegel@google.com>
2017-08-04 17:37:07 -0700

    Add scripts to be used for temp file generation and type checking test
    Change-Id: I773b53fc1bdf6ab94aa81c92c75a18035b2837c6

---
+++ polygerrit-ui/app/template_test_srcs/template_test.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.readdir( path.join(__dirname, '../../temp/behaviors/'), (err, data) => {
+  const behaviors = data;
+  const externs = [];
+
+  for (const behavior of behaviors) {
+    externs.push({
+      path: path.join(__dirname, `../../temp/behaviors/${behavior}`),
+      src: fs.readFileSync(
+          path.join(__dirname, `../../temp/behaviors/${behavior}`), 'utf-8'),
+    });
+  }
+
+  const mappings = JSON.parse(fs.readFileSync(
+      path.join(__dirname, `../../temp/map.json`), 'utf-8'));
+
+
+  externs.push({
+    path: 'custom-externs.js',
+    src: '/** @externs */ var page; var Gerrit; var GrRangeNormalizer; var GrAnnotation; var GrEtagDecorator; var GrGapiAuth; var GrGerritAuth; var GrReviewerUpdatesParser; var GrDiffBuilderUnified; var GrDiffBuilderSideBySide; var GrDiffLine; var util',
+  });
+
+  for (key of Object.keys(mappings)) {
+    if (mappings[key].html && mappings[key].js) {
+      require('fried-twinkie').checkTemplate(
+          path.join(__dirname, '../../' + mappings[key].html),
+          path.join(__dirname, '../../' + mappings[key].js),
+          'polygerrit.' + mappings[key].package,
+          externs
+      );
+    }
+  }
+});
+

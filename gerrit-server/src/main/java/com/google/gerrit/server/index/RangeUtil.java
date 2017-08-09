5a062527e614baa7b36b1077e8a7ff58c13bf027
Dave Borowitz <dborowitz@google.com>
2017-08-09 10:16:59 -0400

    Move RangeUtil to index package
    This particular utility is only used by the index subsystem.
    
    Change-Id: Idb6d3af603529c5fb04cb09a921960d795e2c978

--- gerrit-server/src/main/java/com/google/gerrit/server/index/RangeUtil.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/index/RangeUtil.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.util;
+package com.google.gerrit.server.index;
 
 import com.google.common.primitives.Ints;
 import com.google.gerrit.common.Nullable;

abd49b304c905d1acb4ebb3b7c0cbff978310aa3
Dave Borowitz <dborowitz@google.com>
2017-08-09 15:56:16 -0400

    Don't depend on GerritBaseTests from small index tests
    In preparation for moving these classes to a new package, from which we
    don't want a dependency into //gerrit-server:testutil.
    
    Change-Id: I9e4535cf66f2626a5023580bf3ad265ff0b2c937

--- gerrit-server/src/test/java/com/google/gerrit/server/query/PredicateTest.java
+++ gerrit-server/src/test/java/com/google/gerrit/server/query/PredicateTest.java
@@ -14,11 +14,16 @@
 
 package com.google.gerrit.server.query;
 
-import com.google.gerrit.testutil.GerritBaseTests;
 import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TestName;
 
 @Ignore
-public abstract class PredicateTest extends GerritBaseTests {
+public abstract class PredicateTest {
+  @Rule public ExpectedException exception = ExpectedException.none();
+  @Rule public final TestName testName = new TestName();
+
   protected static final class TestPredicate extends OperatorPredicate<String> {
     protected TestPredicate(String name, String value) {
       super(name, value);

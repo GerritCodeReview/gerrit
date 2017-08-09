2d33944aa7d01107511a36f596af28b181f4c93f
Dave Borowitz <dborowitz@google.com>
2017-08-09 15:20:07 -0400

    Ensure InternalQuery/QueryProcessor are one-time-use
    These classes are non-threadsafe and have mutable fields for a
    builder-like pattern; they should not be reused. Nonetheless, this was
    happening in a few places, which seemed to not be causing problems, but
    they may have just been hard to observe.
    
    Ensure they are consistently used only once with a checkState in
    QueryProcessor#query, and document this limitation.
    
    Change-Id: I1c7d392b1094dcb562894108b652ee7750d98da4

--- gerrit-server/src/main/java/com/google/gerrit/server/query/group/GroupQueryProcessor.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/group/GroupQueryProcessor.java
@@ -32,6 +32,12 @@ import com.google.gerrit.server.query.QueryProcessor;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
+/**
+ * Query processor for the group index.
+ *
+ * <p>Instances are one-time-use. Other singleton classes should inject a Provider rather than
+ * holding on to a single instance.
+ */
 public class GroupQueryProcessor extends QueryProcessor<AccountGroup> {
   private final GroupControl.GenericFactory groupControlFactory;
 

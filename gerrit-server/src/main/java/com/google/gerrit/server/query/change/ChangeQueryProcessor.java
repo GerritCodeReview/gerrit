a91edbf67f515f16919ee2dcee19585d907f4b93
Dave Borowitz <dborowitz@google.com>
2017-08-09 12:36:09 -0400

    QueryProcessor: Don't inject Metrics
    This allows us to continue avoiding a Guice dependency when we move this
    class to gerrit-index. Manual injection in this case is easy, given a
    MetricMaker, and it's unlikely that Metrics will grow too many more
    dependencies in the future.
    
    Change-Id: I951df6ea90191688fab525751680b99b28ec9d77

--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/ChangeQueryProcessor.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/ChangeQueryProcessor.java
@@ -23,6 +23,7 @@ import com.google.gerrit.index.IndexConfig;
 import com.google.gerrit.index.QueryOptions;
 import com.google.gerrit.index.query.IndexPredicate;
 import com.google.gerrit.index.query.Predicate;
+import com.google.gerrit.metrics.MetricMaker;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.account.AccountLimits;
@@ -76,7 +77,7 @@ public class ChangeQueryProcessor extends QueryProcessor<ChangeData>
   ChangeQueryProcessor(
       Provider<CurrentUser> userProvider,
       AccountLimits.Factory limitsFactory,
-      Metrics metrics,
+      MetricMaker metricMaker,
       IndexConfig indexConfig,
       ChangeIndexCollection indexes,
       ChangeIndexRewriter rewriter,
@@ -86,7 +87,7 @@ public class ChangeQueryProcessor extends QueryProcessor<ChangeData>
       DynamicMap<ChangeAttributeFactory> attributeFactories,
       PermissionBackend permissionBackend) {
     super(
-        metrics,
+        metricMaker,
         ChangeSchemaDefinitions.INSTANCE,
         indexConfig,
         indexes,

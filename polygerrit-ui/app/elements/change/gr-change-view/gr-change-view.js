49afd29abf0f126667f3d7115165f97b76640fc9
Paladox none <thomasmulhall410@yahoo.com>
2017-08-10 10:26:50 +0000

    PolyGerrit: Add support to get additional info about the commit
    This will allow users to get additional info about the parent
    commit and Commit and also author and commiter.
    
    This will be in a form of a dialog for now so that at least we can get
    the additional info.
    
    Bug: Issue 5433
    Change-Id: I4d9d099a73afdabc1f22862fd4f2a51fd095a72b

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -410,6 +410,14 @@
       this.$.downloadOverlay.close();
     },
 
+    _handleMoreInfoTap() {
+      this.$.moreInfoOverlay.open();
+    },
+
+    _handleMoreInfoDialogClose(e) {
+      this.$.moreInfoOverlay.close();
+    },
+
     _handleMessageReply(e) {
       const msg = e.detail.message.message;
       const quoteStr = msg.split('\n').map(

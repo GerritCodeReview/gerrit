cb5d2b64f68394a24aa16c266ad7afed8b95a6fa
Edwin Kempin <ekempin@google.com>
2017-08-18 09:24:40 +0200

    AccountIndex: Add field for exact preferred email
    All callers that query accounts be preferred email are interested in
    exact matches. With the new field we can do exact queries instead of
    doing a prefix query and post filter the results.
    
    Change-Id: I1dcdfb1be7090211eda97ad4709fa49a2a8fecdd
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/index/account/AccountSchemaDefinitions.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/index/account/AccountSchemaDefinitions.java
@@ -36,8 +36,11 @@ public class AccountSchemaDefinitions extends SchemaDefinitions<AccountState> {
 
   @Deprecated static final Schema<AccountState> V5 = schema(V4, AccountField.PREFERRED_EMAIL);
 
+  @Deprecated
   static final Schema<AccountState> V6 =
       schema(V5, AccountField.REF_STATE, AccountField.EXTERNAL_ID_STATE);
+
+  static final Schema<AccountState> V7 = schema(V6, AccountField.PREFERRED_EMAIL_EXACT);
 
   public static final String NAME = "accounts";
   public static final AccountSchemaDefinitions INSTANCE = new AccountSchemaDefinitions();

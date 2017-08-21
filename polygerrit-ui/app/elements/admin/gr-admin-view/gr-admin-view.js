a4144989a27c6e0cf31f168fb9e9a4871ceb2241
Paladox none <thomasmulhall410@yahoo.com>
2017-08-21 18:00:20 +0000

    PolyGerrit: Fix adding and deleting members in groups
    In GWTUI the search member bar and adding members was always shown.
    
    Lets do the same as there is no rest api to detect weather a user
    can add, remove or delete members.
    
    Change-Id: I1275ad5071b0f0ddde2090a2faa26c31595df6aa

--- polygerrit-ui/app/elements/admin/gr-admin-view/gr-admin-view.js
+++ polygerrit-ui/app/elements/admin/gr-admin-view/gr-admin-view.js
@@ -60,6 +60,10 @@
         type: Boolean,
         value: false,
       },
+      _isAdmin: {
+        type: Boolean,
+        value: false,
+      },
       _showGroup: Boolean,
       _showGroupAuditLog: Boolean,
       _showGroupList: Boolean,
@@ -155,7 +159,7 @@
               },
             ],
           };
-          if (this._groupOwner) {
+          if (this._isAdmin || this._groupOwner) {
             linkCopy.subsection.children.push(
                 {
                   name: 'Audit Log',
@@ -245,12 +249,14 @@
       this.$.restAPI.getGroupConfig(groupId).then(group => {
         this._groupName = group.name;
         this.reload();
+        this.$.restAPI.getIsAdmin().then(isAdmin => {
+          this._isAdmin = isAdmin ? true : false;
+          this.reload();
+        });
         this.$.restAPI.getIsGroupOwner(group.name).then(
             isOwner => {
-              if (isOwner) {
-                this._groupOwner = true;
-                this.reload();
-              }
+              this._groupOwner = isOwner ? true : false;
+              this.reload();
             });
       });
     },

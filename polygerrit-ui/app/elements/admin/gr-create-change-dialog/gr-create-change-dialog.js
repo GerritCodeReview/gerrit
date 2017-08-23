4b23ddc1275207bc77c51a1e463a414430848f62
Paladox none <thomasmulhall410@yahoo.com>
2017-08-23 09:23:35 +0000

    PolyGerrit: Update create dialog to use per project private config instead of global
    Since change d089b4adda8dcb970c3ad675bd681b1e980a3359 ,
    it has added support for per project private config instead of doing
    it globally.
    
    Change-Id: I72767ae5be0344d5275da1c41253f3284978c10a

--- polygerrit-ui/app/elements/admin/gr-create-change-dialog/gr-create-change-dialog.js
+++ polygerrit-ui/app/elements/admin/gr-create-change-dialog/gr-create-change-dialog.js
@@ -24,7 +24,7 @@
       projectName: String,
       branch: String,
       /** @type {?} */
-      _serverConfig: Object,
+      _projectConfig: Object,
       subject: String,
       topic: String,
       _query: {
@@ -50,8 +50,8 @@
     ],
 
     attached() {
-      this.$.restAPI.getConfig().then(config => {
-        this._serverConfig = config;
+      this.$.restAPI.getProjectConfig(this.projectName).then(config => {
+        this._projectConfig = config;
       });
     },
 

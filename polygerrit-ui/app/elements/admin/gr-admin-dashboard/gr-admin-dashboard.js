5b52c214d2d38f82d5619985e7e0f7f602dee0be
Paladox none <thomasmulhall410@yahoo.com>
2017-09-05 12:59:46 +0000

    PolyGerrit: Implement support for project dashboards
    Accessed via /admin/projects/<project>,dashboards
    
    Change-Id: If095e5a85d0bf84d73e7a0afb0a6ec168a287cde

---
+++ polygerrit-ui/app/elements/admin/gr-admin-dashboard/gr-admin-dashboard.js
@@ -0,0 +1,73 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-admin-dashboard',
+
+    /**
+     * Fired when the title of the page should change.
+     *
+     * @event title-change
+     */
+
+    properties: {
+      /** @type {{ selectedChangeIndex: number }} */
+      viewState: Object,
+      params: {
+        type: Object,
+      },
+
+      _results: Array,
+
+      /**
+       * For showing a "loading..." string during ajax requests.
+       */
+      _loading: {
+        type: Boolean,
+        value: true,
+      },
+      project: String,
+    },
+
+    behaviors: [
+      Gerrit.RESTClientBehavior,
+    ],
+
+    attached() {
+      this.fire('title-change', {title: 'Dashboards'});
+    },
+
+    observers: [
+      '_paramsChanged(params.project)',
+    ],
+
+    /**
+     * Allows a refresh if menu item is selected again.
+     */
+    _paramsChanged(project) {
+      if (!project) { return; }
+      this._loading = true;
+      this.$.restAPI.getDashboard(project)
+          .then(results => {
+            this._results = results;
+            this._loading = false;
+          }).catch(err => {
+            this._loading = false;
+            console.warn(err.message);
+          });
+    },
+  });
+})();

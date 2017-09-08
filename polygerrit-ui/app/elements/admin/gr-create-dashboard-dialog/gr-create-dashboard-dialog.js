c4e7a080aa96d31b547b9a46af8fb42952fef855
Paladox none <thomasmulhall410@yahoo.com>
2017-09-08 12:31:35 +0000

    PolyGerrit: Add support for adding and deleting dashboards
    This is a new feature for polygerrit and not found in gwtui.
    
    Change-Id: Ie681f949dd7291ae1bb30e01b1d607efd82b229a

---
+++ polygerrit-ui/app/elements/admin/gr-create-dashboard-dialog/gr-create-dashboard-dialog.js
@@ -0,0 +1,53 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-create-dashboard-dialog',
+
+    properties: {
+      projectName: String,
+      /** @type {?} */
+      _serverConfig: Object,
+      commitMessage: String,
+      query: String,
+    },
+
+    behaviors: [
+      Gerrit.BaseUrlBehavior,
+      Gerrit.URLEncodingBehavior,
+    ],
+
+    attached() {
+      this.$.restAPI.getConfig().then(config => {
+        this._serverConfig = config;
+      });
+    },
+
+    handleCreateDashboard() {
+      const IS_QUERY = this.query ? this.query : '';
+      const IS_COMMIT_MESSAGE = this.commitMessage ? this.commitMessage : '';
+      return this.$.restAPI.createDashboard(this.projectName, IS_QUERY,
+          IS_COMMIT_MESSAGE)
+          .then(dashboardCreated => {
+            if (!dashboardCreated) {
+              return;
+            }
+            this.dispatchEvent(new CustomEvent('show-alert',
+                {detail: {message: ''}, bubbles: true}));
+          });
+    },
+  });
+})();

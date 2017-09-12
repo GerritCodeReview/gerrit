59a650ca9196773ff17a1060ddb7490c3f1b69c6
Kasper Nilsson <kaspern@google.com>
2017-09-12 14:15:44 -0700

    Introduce gr-editor-view
    Adds a new element subdirectory, /edit/, and the gr-editor-view. This
    new view supports editing a file with a textarea, as well as editing the
    file path.
    
    Mechanism for loading a file into this view will come in a later change.
    
    Bug: Issue 4437
    Change-Id: I8ab480ce2aa7a6df90930b02c8961f395571f5a8

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -1237,9 +1237,18 @@
           .then(response => this.getResponseObject(response));
     },
 
-    getFileInChangeEdit(changeNum, path) {
+    /**
+     * Gets a file in a change edit.
+     * @param {number|string} changeNum
+     * @param {string} path
+     * @param {boolean=} opt_base If specified, file contents come from change edit's
+     *     base patchset.
+     */
+    getFileInChangeEdit(changeNum, path, opt_base) {
       const e = '/edit/' + encodeURIComponent(path);
-      return this.getChangeURLAndSend(changeNum, 'GET', null, e);
+      let payload = null;
+      if (opt_base) { payload = {base: true}; }
+      return this.getChangeURLAndSend(changeNum, 'GET', null, e, payload);
     },
 
     rebaseChangeEdit(changeNum) {
@@ -1267,7 +1276,8 @@
 
     saveChangeEdit(changeNum, path, contents) {
       const e = '/edit/' + encodeURIComponent(path);
-      return this.getChangeURLAndSend(changeNum, 'PUT', null, e, contents);
+      return this.getChangeURLAndSend(changeNum, 'PUT', null, e, contents, null,
+          null, 'text/plain');
     },
 
     // Deprecated, prefer to use putChangeCommitMessage instead.

8851abfcd7f5a9291ae0648b1ad1c1848017dbe6
David Ostrovsky <david@ostrovsky.org>
2017-09-18 09:14:56 +0200

    Move gpg test utility sources to testing package
    Change-Id: I8cc8eb247b635a49f62f18a75de12bdf67a1cdb5

--- java/com/google/gerrit/gpg/testing/TestKey.java
+++ java/com/google/gerrit/gpg/testing/TestKey.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.gpg.testutil;
+package com.google.gerrit.gpg.testing;
 
 import static com.google.gerrit.gpg.PublicKeyStore.keyIdToString;
 

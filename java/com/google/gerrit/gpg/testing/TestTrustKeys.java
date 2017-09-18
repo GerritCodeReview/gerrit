8e89a1d36bb60232949422308c2e6c89838f67fc
David Ostrovsky <david@ostrovsky.org>
2017-09-18 09:14:56 +0200

    Move gpg test utility sources to testing package
    Change-Id: I8cc8eb247b635a49f62f18a75de12bdf67a1cdb5

--- java/com/google/gerrit/gpg/testing/TestTrustKeys.java
+++ java/com/google/gerrit/gpg/testing/TestTrustKeys.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.gpg.testutil;
+package com.google.gerrit.gpg.testing;
 
 /**
  * Test keys specific to web-of-trust checks.

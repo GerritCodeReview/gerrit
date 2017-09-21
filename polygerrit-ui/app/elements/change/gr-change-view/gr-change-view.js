bc637c42748d57e7980fbb1aea9c951963237298
Paladox none <thomasmulhall410@yahoo.com>
2017-09-21 17:27:04 +0000

    PolyGerrit: Add support for "Included In"
    Bug: Issue 6112
    Change-Id: I033e3f248a404813b5bc46eeef6bf25b7153bc1a

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -512,6 +512,14 @@
       this.$.fileList.openDiffPrefs();
     },
 
+    _handleOpenIncludedInDialog() {
+      this.$.includedInOverlay.open();
+    },
+
+    _handleIncludedInDialogClose(e) {
+      this.$.includedInOverlay.close();
+    },
+
     _handleOpenDownloadDialog() {
       this.$.downloadOverlay.open().then(() => {
         this.$.downloadOverlay

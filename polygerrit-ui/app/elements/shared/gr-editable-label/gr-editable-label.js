af0826513adbc19d81b7e70058b5096397c18e87
Becky Siegel <beckysiegel@google.com>
2017-09-25 14:45:37 -0700

    Update gr-editable-label to new design
    Uses a gr-dropdown and paper-input.
    
    Change-Id: I34bdfecc5fbe53432aa4175cdc6151d8f85b2ac7

--- polygerrit-ui/app/elements/shared/gr-editable-label/gr-editable-label.js
+++ polygerrit-ui/app/elements/shared/gr-editable-label/gr-editable-label.js
@@ -14,6 +14,9 @@
 (function() {
   'use strict';
 
+  const AWAIT_MAX_ITERS = 10;
+  const AWAIT_STEP = 5;
+
   Polymer({
     is: 'gr-editable-label',
 
@@ -24,6 +27,7 @@
      */
 
     properties: {
+      labelText: String,
       editing: {
         type: Boolean,
         value: false,
@@ -69,21 +73,51 @@
       return value;
     },
 
-    _open() {
+    _showDropdown() {
       if (this.readOnly || this.editing) { return; }
+      this._open().then(() => {
+        this.$.input.$.input.focus();
+        if (!this.$.input.value) { return; }
+        this.$.input.$.input.setSelectionRange(0, this.$.input.value.length);
+      });
+    },
 
+    _open(...args) {
+      this.$.dropdown.open();
       this._inputText = this.value;
       this.editing = true;
 
-      this.async(() => {
-        this.$.input.focus();
-        this.$.input.setSelectionRange(0, this.$.input.value.length);
+      return new Promise(resolve => {
+        Polymer.IronOverlayBehaviorImpl.open.apply(this.$.dropdown, args);
+        this._awaitOpen(resolve);
       });
+    },
+
+    /**
+     * NOTE: (wyatta) Slightly hacky way to listen to the overlay actually
+     * opening. Eventually replace with a direct way to listen to the overlay.
+     */
+    _awaitOpen(fn) {
+      let iters = 0;
+      const step = () => {
+        this.async(() => {
+          if (this.style.display !== 'none') {
+            fn.call(this);
+          } else if (iters++ < AWAIT_MAX_ITERS) {
+            step.call(this);
+          }
+        }, AWAIT_STEP);
+      };
+      step.call(this);
+    },
+
+    _id() {
+      return this.getAttribute('id') || 'global';
     },
 
     _save() {
       if (!this.editing) { return; }
-
+      this.$.dropdown.close();
       this.value = this._inputText;
       this.editing = false;
       this.fire('changed', this.value);
@@ -91,7 +125,7 @@
 
     _cancel() {
       if (!this.editing) { return; }
-
+      this.$.dropdown.close();
       this.editing = false;
       this._inputText = this.value;
     },
@@ -104,7 +138,7 @@
     _handleEnter(e) {
       e = this.getKeyboardEvent(e);
       const target = Polymer.dom(e).rootTarget;
-      if (target === this.$.input) {
+      if (target === this.$.input.$.input) {
         e.preventDefault();
         this._save();
       }
@@ -118,7 +152,7 @@
     _handleEsc(e) {
       e = this.getKeyboardEvent(e);
       const target = Polymer.dom(e).rootTarget;
-      if (target === this.$.input) {
+      if (target === this.$.input.$.input) {
         e.preventDefault();
         this._cancel();
       }

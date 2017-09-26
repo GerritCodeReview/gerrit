47a89fc9561b3e350d94aecb953949e9b7173917
Paladox none <thomasmulhall410@yahoo.com>
2017-09-26 15:20:17 +0000

    Remove ABANDON change permission check for Moving changes
    Checking if the user had ABANDON permission, was a waste of time
    as moving a change moved the change over to the different branch.
    
    The change did not abandon the change, it instead moved it over.
    
    Change-Id: Ib6b4a4672253612b71446088602a0172de53fcd7

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/MoveChangeIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/rest/change/MoveChangeIT.java
@@ -168,22 +168,6 @@ public class MoveChangeIT extends AbstractDaemonTest {
   }
 
   @Test
-  public void moveChangeFromBranchWithoutAbandonPerms() throws Exception {
-    // Move change for which user does not have abandon permissions
-    PushOneCommit.Result r = createChange();
-    Branch.NameKey newBranch = new Branch.NameKey(r.getChange().change().getProject(), "moveTest");
-    createBranch(newBranch);
-    block(
-        r.getChange().change().getDest().get(),
-        Permission.ABANDON,
-        systemGroupBackend.getGroup(REGISTERED_USERS).getUUID());
-    setApiUser(user);
-    exception.expect(AuthException.class);
-    exception.expectMessage("move not permitted");
-    move(r.getChangeId(), newBranch.get());
-  }
-
-  @Test
   public void moveChangeToBranchThatContainsCurrentCommit() throws Exception {
     // Move change to a branch for which current PS revision is reachable from
     // tip

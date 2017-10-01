3ab50af04d46e749ccd816a276f19a63fbbf29ef
Kasper Nilsson <kaspern@google.com>
2017-10-01 08:50:36 +0100

    Introduce gr-edit-file-controls
    Component is a simple `edit` button and a `more` menu for taking
    per-file edit related actions, like reverting, renaming, and deleting
    files.
    
    Clicking any button will fire an event that will be caught and handled
    by a higher-level component that has yet to be implemented.
    
    Bug: Issue 4437
    Change-Id: I1ded6d261562039b094ac2bafa60d7aa9603444b

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
@@ -926,5 +926,10 @@
     _computeReviewedText(isReviewed) {
       return isReviewed ? 'MARK UNREVIEWED' : 'MARK REVIEWED';
     },
+
+    _handleEditTap(e) {
+      const url = Gerrit.Nav.getEditUrlForDiff(this.change, e.detail.path);
+      Gerrit.Nav.navigateToRelativeUrl(url);
+    },
   });
 })();

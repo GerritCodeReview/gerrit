c24c68c512a5d6cf92a4483fc3679482a5beee00
Ravi Mistry <rmistry@google.com>
2017-10-10 09:47:26 -0400

    Experiment with API for code coverage PG plugin 3
    coverage.js here is similar to the JS plugins skiastatus.js and commitqueue.js
    
    Bug: Issue 7339
    Change-Id: Ie51845e0b3564953aba5d7d41986cedce0337073

---
+++ polygerrit-ui/app/elements/plugins/gr-layer-api/gr-layer-api.js
@@ -0,0 +1,44 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function(window) {
+  'use strict';
+
+  // Prevent redefinition.
+  if (window.GrLayerApi) { return; }
+
+  function GrLayerApi(plugin) {
+    this.plugin = plugin;
+  }
+
+  GrLayerApi.prototype.addLayer = function(callback) {
+    this.plugin.hook('layer-helper').onAttached(element => {
+      if (!element.content) {return;}
+
+      var pluginLayerFunc = callback(element);
+
+      var annotateFunc = function() {
+        return {
+          annotate(el, line) {
+            pluginLayerFunc(el, line);
+          },
+        };
+      };
+
+      element.content.addLayer(annotateFunc);
+    });
+    return this;
+  };
+
+  window.GrLayerApi = GrLayerApi;
+})(window);

43126245111e2050f00ec7d6631f237edc0eccbf
Ravi Mistry <rmistry@google.com>
2017-10-10 09:47:26 -0400

    Experiment with API for code coverage PG plugin 3
    This uses a JS plugin like commitqueue.js and calls the plugin(s) directly
    from gr-diff-builder.html.
    
    Bug: Issue 7339
    Change-Id: Ie51845e0b3564953aba5d7d41986cedce0337073

---
+++ polygerrit-ui/app/elements/coverage.js
@@ -0,0 +1,64 @@
+(function() {
+  'use strict';
+
+  function CoveragePlugin(gerritPlugin) {
+    this.gerritPlugin_ = gerritPlugin;
+
+    var coverageData = {}
+    this._annotationApi = this.gerritPlugin_.annotationApi()
+      .addNotifier(notifyFunc => {
+        new Promise(resolve => setTimeout(resolve, 3000)).then(
+          () => {
+            populateWithDummyData(coverageData);
+            Object.keys(coverageData).forEach(function(file) {
+              notifyFunc(file, 0, coverageData[file].totalLines, 'right');
+            });
+          });
+      }).addLayer(annotationActionsContext => {
+        if (Object.keys(coverageData).length === 0) {
+          // Coverage data is not ready yet.
+          return;
+        }
+        const path = annotationActionsContext.path;
+        const line = annotationActionsContext.line;
+        const el = annotationActionsContext.el;
+        // Highlight lines missing coverage with this background color.
+        const cssClass = Gerrit.css('background-color: #EF9B9B');
+        if (coverageData[path] &&
+            coverageData[path].changeNum === parseInt(annotationActionsContext.changeNum) &&
+            coverageData[path].patchNum === parseInt(annotationActionsContext.patchNum)) {
+          var linesMissingCoverage = coverageData[path].linesMissingCoverage
+          if (linesMissingCoverage.includes(line.afterNumber)) {
+            annotationActionsContext.annotateRangeFunc(0, line.text.length, el, cssClass);
+          }
+        }
+      });
+  }
+
+  function populateWithDummyData(coverageData) {
+    coverageData['NewFile'] = {
+        'linesMissingCoverage': [1,2,3],
+        'totalLines': 5,
+        'changeNum': 94,
+        'patchNum': 2,
+    }
+    coverageData['/COMMIT_MSG'] = {
+        'linesMissingCoverage': [3, 4, 7, 14],
+        'totalLines': 14,
+        'changeNum': 94,
+        'patchNum': 2,
+    }
+  }
+
+  // Gerrit may not be defined (say, in a test environment).
+  if (window.Gerrit) {
+    Gerrit.install(function(self) {
+      // coverage.js is only supported in PolyGerrit.
+      if (!window.Polymer) { return; }
+
+      const coveragePlugin = new CoveragePlugin(self);
+    }, '0.1');
+  }
+
+  window.__CoveragePlugin = window.__CoveragePlugin || CoveragePlugin;
+})(window);

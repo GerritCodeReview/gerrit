42e6034ac1b4bbe0e8aed7dd24c2530afccd8529
Paladox none <thomasmulhall410@yahoo.com>
2017-10-11 18:28:37 +0000

    PolyGerrit: Fix gr-watched-projects-editor
    Trying to add your self to a project to watch fails
    
    There's no error in the log, but clicking the add button fails.
    
    Bug: Issue 7402
    Change-Id: I0b9c41037be8c7ebcc548007c4ae36c68b6977cb

--- polygerrit-ui/app/elements/settings/gr-watched-projects-editor/gr-watched-projects-editor.js
+++ polygerrit-ui/app/elements/settings/gr-watched-projects-editor/gr-watched-projects-editor.js
@@ -99,7 +99,8 @@
     },
 
     _handleRemoveProject(e) {
-      const index = parseInt(e.target.getAttribute('data-index'), 10);
+      const el = e.currentTarget;
+      const index = parseInt(el.getAttribute('data-index'), 10);
       const project = this._projects[index];
       this.splice('_projects', index, 1);
       this.push('_projectsToRemove', project);
@@ -154,15 +155,17 @@
     },
 
     _handleCheckboxChange(e) {
-      const index = parseInt(e.target.getAttribute('data-index'), 10);
-      const key = e.target.getAttribute('data-key');
-      const checked = e.target.checked;
+      const el = e.currentTarget;
+      const index = parseInt(el.getAttribute('data-index'), 10);
+      const key = el.getAttribute('data-key');
+      const checked = el.checked;
       this.set(['_projects', index, key], !!checked);
       this.hasUnsavedChanges = true;
     },
 
     _handleNotifCellTap(e) {
-      const checkbox = Polymer.dom(e.target).querySelector('input');
+      const el = e.currentTarget;
+      const checkbox = Polymer.dom(el).querySelector('input');
       if (checkbox) { checkbox.click(); }
     },
   });

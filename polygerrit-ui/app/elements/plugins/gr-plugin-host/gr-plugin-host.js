e9e984b2eaf03ac3d637e12680e808d5f6ca49ee
Becky Siegel <beckysiegel@google.com>
2017-10-13 15:36:28 -0700

    Revert "Revert "Use only Gerrit.install as a plugin loaded signal""
    This reverts commit 96d037968f1674163da80ab44e8c95c31f4e10f3.
    
    It also contains the underyling fix for loading plugins correctly. The
    importHref function expected functions for onload and onerror arguments
    and passing null instead was causing the unexplained behavior.
    
    Change-Id: Ic2a36d9bfcf85e5769c26ab49caf15e572d9463e

--- polygerrit-ui/app/elements/plugins/gr-plugin-host/gr-plugin-host.js
+++ polygerrit-ui/app/elements/plugins/gr-plugin-host/gr-plugin-host.js
@@ -47,12 +47,11 @@
      * States that it expects no more than 3 parameters, but that's not true.
      * @todo (beckysiegel) check Polymer annotations and submit change.
      */
-
     _importHtmlPlugins(plugins) {
       for (const url of plugins) {
+        // onload needs to be a function. When null or undefined were passed
         this.importHref(
-            this._urlFor(url), Gerrit._pluginInstalled, Gerrit._pluginInstalled,
-            true);
+            this._urlFor(url), () => {}, Gerrit._pluginInstalled, true);
       }
     },
 

0244fbb97e43a506a8a7fb72148d282e70f5cada
Paladox none <thomasmulhall410@yahoo.com>
2017-10-13 11:38:00 +0000

    PolyGerrit: Add missing submit type Rebase Always
    Bug: Issue 7409
    Change-Id: I098797c460980454a0a10cf74e3cef6005d39867

--- polygerrit-ui/app/elements/change/gr-change-metadata/gr-change-metadata.js
+++ polygerrit-ui/app/elements/change/gr-change-metadata/gr-change-metadata.js
@@ -21,6 +21,7 @@
     MERGE_IF_NECESSARY: 'Merge if Necessary',
     REBASE_IF_NECESSARY: 'Rebase if Necessary',
     MERGE_ALWAYS: 'Always Merge',
+    REBASE_ALWAYS: 'Always Rebase',
     CHERRY_PICK: 'Cherry Pick',
   };
 

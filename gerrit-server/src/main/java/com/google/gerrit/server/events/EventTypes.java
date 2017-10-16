9c72471ba33bdd9daf5be7fd69f094913ffa7f6a
Hugo Arès <hugo.ares@ericsson.com>
2017-10-16 08:41:04 -0400

    Add missing registration for VoteDeletedEvent
    Without this, code trying to deserialize this event type will fail with:
    
    com.google.gson.JsonParseException: Unknown event type: vote-deleted
    
    Change-Id: I0a9db2e0ec117c4b47e689d0e7c596b38a4f1f4a

--- gerrit-server/src/main/java/com/google/gerrit/server/events/EventTypes.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/events/EventTypes.java
@@ -37,6 +37,7 @@ public class EventTypes {
     register(ReviewerAddedEvent.TYPE, ReviewerAddedEvent.class);
     register(ReviewerDeletedEvent.TYPE, ReviewerDeletedEvent.class);
     register(TopicChangedEvent.TYPE, TopicChangedEvent.class);
+    register(VoteDeletedEvent.TYPE, VoteDeletedEvent.class);
   }
 
   /**

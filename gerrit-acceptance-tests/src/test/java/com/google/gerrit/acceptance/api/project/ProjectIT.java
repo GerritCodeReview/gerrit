b90511523f0a92a40738708ff1fd6a4198da36e1
David Pursehouse <dpursehouse@collab.net>
2017-10-17 14:21:29 +0900

    ProjectApi: Add methods to get/set project's HEAD
    Change-Id: Iec3e6e6bf3988634ad461cad108082d787288d7c

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/api/project/ProjectIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/api/project/ProjectIT.java
@@ -36,6 +36,7 @@ import com.google.gerrit.extensions.client.SubmitType;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.reviewdb.client.RefNames;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -379,6 +380,41 @@ public class ProjectIT extends AbstractDaemonTest {
     exception.expect(ResourceConflictException.class);
     exception.expectMessage("100 foo");
     setMaxObjectSize("100 foo");
+  }
+
+  @Test
+  public void setHead() throws Exception {
+    assertThat(gApi.projects().name(project.get()).head()).isEqualTo("refs/heads/master");
+    gApi.projects().name(project.get()).branch("test1").create(new BranchInput());
+    gApi.projects().name(project.get()).branch("test2").create(new BranchInput());
+    for (String head : new String[] {"test1", "refs/heads/test2"}) {
+      gApi.projects().name(project.get()).head(head);
+      assertThat(gApi.projects().name(project.get()).head()).isEqualTo(RefNames.fullName(head));
+    }
+  }
+
+  @Test
+  public void setHeadToNonexistentBranch() throws Exception {
+    exception.expect(UnprocessableEntityException.class);
+    gApi.projects().name(project.get()).head("does-not-exist");
+  }
+
+  @Test
+  public void setHeadToSameBranch() throws Exception {
+    gApi.projects().name(project.get()).branch("test").create(new BranchInput());
+    for (String head : new String[] {"test", "refs/heads/test"}) {
+      gApi.projects().name(project.get()).head(head);
+      assertThat(gApi.projects().name(project.get()).head()).isEqualTo(RefNames.fullName(head));
+    }
+  }
+
+  @Test
+  public void setHeadNotAllowed() throws Exception {
+    gApi.projects().name(project.get()).branch("test").create(new BranchInput());
+    setApiUser(user);
+    exception.expect(AuthException.class);
+    exception.expectMessage("restricted to project owner");
+    gApi.projects().name(project.get()).head("test");
   }
 
   private ConfigInput createTestConfigInput() {
--- gerrit-server/src/main/java/com/google/gerrit/server/api/projects/ProjectApiImpl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/api/projects/ProjectApiImpl.java
@@ -59,6 +59,7 @@ import com.google.gerrit.server.project.DeleteTags;
 import com.google.gerrit.server.project.GetAccess;
 import com.google.gerrit.server.project.GetConfig;
 import com.google.gerrit.server.project.GetDescription;
+import com.google.gerrit.server.project.GetHead;
 import com.google.gerrit.server.project.ListBranches;
 import com.google.gerrit.server.project.ListChildProjects;
 import com.google.gerrit.server.project.ListDashboards;
@@ -69,6 +70,7 @@ import com.google.gerrit.server.project.ProjectsCollection;
 import com.google.gerrit.server.project.PutConfig;
 import com.google.gerrit.server.project.PutDescription;
 import com.google.gerrit.server.project.SetAccess;
+import com.google.gerrit.server.project.SetHead;
 import com.google.inject.Provider;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
@@ -110,6 +112,8 @@ public class ProjectApiImpl implements ProjectApi {
   private final DashboardApiImpl.Factory dashboardApi;
   private final CheckAccess checkAccess;
   private final Provider<ListDashboards> listDashboards;
+  private final GetHead getHead;
+  private final SetHead setHead;
 
   @AssistedInject
   ProjectApiImpl(
@@ -139,6 +143,8 @@ public class ProjectApiImpl implements ProjectApi {
       DashboardApiImpl.Factory dashboardApi,
       CheckAccess checkAccess,
       Provider<ListDashboards> listDashboards,
+      GetHead getHead,
+      SetHead setHead,
       @Assisted ProjectResource project) {
     this(
         user,
@@ -168,6 +174,8 @@ public class ProjectApiImpl implements ProjectApi {
         dashboardApi,
         checkAccess,
         listDashboards,
+        getHead,
+        setHead,
         null);
   }
 
@@ -199,6 +207,8 @@ public class ProjectApiImpl implements ProjectApi {
       DashboardApiImpl.Factory dashboardApi,
       CheckAccess checkAccess,
       Provider<ListDashboards> listDashboards,
+      GetHead getHead,
+      SetHead setHead,
       @Assisted String name) {
     this(
         user,
@@ -228,6 +238,8 @@ public class ProjectApiImpl implements ProjectApi {
         dashboardApi,
         checkAccess,
         listDashboards,
+        getHead,
+        setHead,
         name);
   }
 
@@ -259,6 +271,8 @@ public class ProjectApiImpl implements ProjectApi {
       DashboardApiImpl.Factory dashboardApi,
       CheckAccess checkAccess,
       Provider<ListDashboards> listDashboards,
+      GetHead getHead,
+      SetHead setHead,
       String name) {
     this.user = user;
     this.permissionBackend = permissionBackend;
@@ -287,6 +301,8 @@ public class ProjectApiImpl implements ProjectApi {
     this.dashboardApi = dashboardApi;
     this.checkAccess = checkAccess;
     this.listDashboards = listDashboards;
+    this.getHead = getHead;
+    this.setHead = setHead;
     this.name = name;
   }
 
@@ -522,6 +538,26 @@ public class ProjectApiImpl implements ProjectApi {
         }
       }
     };
+  }
+
+  @Override
+  public String head() throws RestApiException {
+    try {
+      return getHead.apply(checkExists());
+    } catch (Exception e) {
+      throw asRestApiException("Cannot get HEAD", e);
+    }
+  }
+
+  @Override
+  public void head(String head) throws RestApiException {
+    SetHead.Input input = new SetHead.Input();
+    input.ref = head;
+    try {
+      setHead.apply(checkExists(), input);
+    } catch (Exception e) {
+      throw asRestApiException("Cannot set HEAD", e);
+    }
   }
 
   private ProjectResource checkExists() throws ResourceNotFoundException {

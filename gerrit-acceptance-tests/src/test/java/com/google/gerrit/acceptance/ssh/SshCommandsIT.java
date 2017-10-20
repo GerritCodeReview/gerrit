0e0785178cdfa4908d07f915e6ba2e0359a93cc9
Edwin Kempin <ekempin@google.com>
2017-10-20 11:01:07 +0200

    Enable testing of Gerrit in slave mode
    Recently we had a few breakages of running Gerrit in slave mode (e.g.
    due to trying to access secondary indexes which don't exist on slaves).
    These breakages were only noticed after user reports since we don't have
    any integration tests for Gerrit slaves at all.
    
    This change enables integration tests against Gerrit slaves. It is easy
    to start the Gerrit daemon in slave mode but then the server is in
    read-only mode and we can't create any test data using the Gerrit API.
    To be able to test against a slave server which is populated with test
    data we first start the daemon as master and then after the test data
    has been created we restart the daemon in slave mode.
    
    If the server is restarted in slave mode we must preserve the content
    of the test site, the repositories and the database content.
    
    Preserving the content of the test site is not a problem since the test
    site folders are only deleted at the very end after executing all tests.
    
    The git repositories and the database may exist only in memory and in
    this case we must make sure that the same instances are used when the
    daemon is restarted in slave mode.
    
    As in-memory database we use H2 [1]. The contents of an in-memory H2
    database are gone as soon as the last connection to the database is
    closed. This is why we must ensure to keep a database connection open
    when then server in master mode is shutdown and we intend to restart it
    as slave.
    
    When the server is restarted we must make sure to reopen SSH connections
    and reinject members into the test class.
    
    Tests that want to run against a Gerrit slave can use the normal Gerrit
    API to setup the test data and then use the 'restartAsGerritSlave'
    method from AbstractDaemonTest to restart the daemon in slave mode. For
    these tests it is required to use the @Sandboxed annotation so that they
    get an own server instance. Restarting the common server instance as
    slave would break other tests using this server.
    
    As an example the SshCommandsIT test is extended to check the list of
    available commands in master and slave mode. Also it executes all
    available commands in both modes.
    
    [1] http://www.h2database.com/html/features.html#in_memory_databases
    
    Change-Id: I2856e04ffa9fe4aac63747d4fc463581f7a1a6d6
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/ssh/SshCommandsIT.java
+++ gerrit-acceptance-tests/src/test/java/com/google/gerrit/acceptance/ssh/SshCommandsIT.java
@@ -22,9 +22,14 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.NoHttpd;
+import com.google.gerrit.acceptance.Sandboxed;
 import com.google.gerrit.acceptance.UseSsh;
 import com.google.gerrit.common.data.GlobalCapability;
 import com.google.gerrit.sshd.Commands;
+import com.jcraft.jsch.JSchException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.junit.Test;
@@ -36,44 +41,59 @@ import org.slf4j.LoggerFactory;
 public class SshCommandsIT extends AbstractDaemonTest {
   private static final Logger log = LoggerFactory.getLogger(SshCommandsIT.class);
 
-  //TODO: It would be better to dynamically generate this list
-  private static final Map<String, List<String>> COMMANDS =
+  // TODO: It would be better to dynamically generate these lists
+  private static final List<String> COMMON_ROOT_COMMANDS =
+      ImmutableList.of(
+          "apropos",
+          "close-connection",
+          "flush-caches",
+          "gc",
+          "logging",
+          "ls-groups",
+          "ls-members",
+          "ls-projects",
+          "ls-user-refs",
+          "plugin",
+          "show-caches",
+          "show-connections",
+          "show-queue",
+          "version");
+
+  private static final List<String> MASTER_ONLY_ROOT_COMMANDS =
+      ImmutableList.of(
+          "ban-commit",
+          "create-account",
+          "create-branch",
+          "create-group",
+          "create-project",
+          "gsql",
+          "index",
+          "query",
+          "receive-pack",
+          "rename-group",
+          "review",
+          "set-account",
+          "set-head",
+          "set-members",
+          "set-project",
+          "set-project-parent",
+          "set-reviewers",
+          "stream-events",
+          "test-submit");
+
+  private static final Map<String, List<String>> MASTER_COMMANDS =
       ImmutableMap.of(
           Commands.ROOT,
-          ImmutableList.of(
-              "apropos",
-              "ban-commit",
-              "close-connection",
-              "create-account",
-              "create-branch",
-              "create-group",
-              "create-project",
-              "flush-caches",
-              "gc",
-              "gsql",
-              "index",
-              "logging",
-              "ls-groups",
-              "ls-members",
-              "ls-projects",
-              "ls-user-refs",
-              "plugin",
-              "query",
-              "receive-pack",
-              "rename-group",
-              "review",
-              "set-account",
-              "set-head",
-              "set-members",
-              "set-project",
-              "set-project-parent",
-              "set-reviewers",
-              "show-caches",
-              "show-connections",
-              "show-queue",
-              "stream-events",
-              "test-submit",
-              "version"),
+          ImmutableList.copyOf(
+              new ArrayList<String>() {
+                private static final long serialVersionUID = 1L;
+
+                {
+                  addAll(COMMON_ROOT_COMMANDS);
+                  addAll(MASTER_ONLY_ROOT_COMMANDS);
+                  Collections.sort(this);
+                }
+              }),
           "index",
           ImmutableList.of("activate", "changes", "project", "start"),
           "plugin",
@@ -81,13 +101,29 @@ public class SshCommandsIT extends AbstractDaemonTest {
           "test-submit",
           ImmutableList.of("rule", "type"));
 
+  private static final Map<String, List<String>> SLAVE_COMMANDS =
+      ImmutableMap.of(
+          Commands.ROOT,
+          COMMON_ROOT_COMMANDS,
+          "plugin",
+          ImmutableList.of("add", "enable", "install", "ls", "reload", "remove", "rm"));
+
   @Test
+  @Sandboxed
   public void sshCommandCanBeExecuted() throws Exception {
     // Access Database capability is required to run the "gerrit gsql" command
     allowGlobalCapabilities(REGISTERED_USERS, GlobalCapability.ACCESS_DATABASE);
 
-    for (String root : COMMANDS.keySet()) {
-      for (String command : COMMANDS.get(root)) {
+    testCommandExecution(MASTER_COMMANDS);
+
+    restartAsSlave();
+    testCommandExecution(SLAVE_COMMANDS);
+  }
+
+  private void testCommandExecution(Map<String, List<String>> commands)
+      throws JSchException, IOException {
+    for (String root : commands.keySet()) {
+      for (String command : commands.get(root)) {
         // We can't assert that adminSshSession.hasError() is false, because using the --help
         // option causes the usage info to be written to stderr. Instead, we assert on the
         // content of the stderr, which will always start with "gerrit command" when the --help
@@ -108,5 +144,51 @@ public class SshCommandsIT extends AbstractDaemonTest {
     adminSshSession.exec("gerrit non-existing-command --help");
     assertThat(adminSshSession.getError())
         .startsWith("fatal: gerrit: non-existing-command: not found");
+  }
+
+  @Test
+  @Sandboxed
+  public void listCommands() throws Exception {
+    adminSshSession.exec("gerrit --help");
+    List<String> commands = parseCommandsFromGerritHelpText(adminSshSession.getError());
+    assertThat(commands).containsExactlyElementsIn(MASTER_COMMANDS.get(Commands.ROOT)).inOrder();
+
+    restartAsSlave();
+    adminSshSession.exec("gerrit --help");
+    commands = parseCommandsFromGerritHelpText(adminSshSession.getError());
+    assertThat(commands).containsExactlyElementsIn(SLAVE_COMMANDS.get(Commands.ROOT)).inOrder();
+  }
+
+  private List<String> parseCommandsFromGerritHelpText(String helpText) {
+    List<String> commands = new ArrayList<>();
+
+    String[] lines = helpText.split("\\n");
+
+    // Skip all lines including the line starting with "Available commands"
+    int row = 0;
+    do {
+      row++;
+    } while (row < lines.length && !lines[row - 1].startsWith("Available commands"));
+
+    // Skip all empty lines
+    while (lines[row].trim().isEmpty()) {
+      row++;
+    }
+
+    // Parse commands from all lines that are indented (start with a space)
+    while (row < lines.length && lines[row].startsWith(" ")) {
+      String line = lines[row].trim();
+      // Abort on empty line
+      if (line.isEmpty()) {
+        break;
+      }
+
+      // Cut off command description if there is one
+      int endOfCommand = line.indexOf(' ');
+      commands.add(endOfCommand > 0 ? line.substring(0, line.indexOf(' ')) : line);
+      row++;
+    }
+
+    return commands;
   }
 }

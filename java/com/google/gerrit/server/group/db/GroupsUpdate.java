a8259c27e674148c1350562641d249bb36c4585a
Alice Kober-Sotzek <aliceks@google.com>
2017-10-24 13:55:13 +0200

    Create groups in SchemaCreator also in NoteDb
    Contrary to the implementation in GroupsUpdate, the created groups in
    SchemaCreator don't add any initial members or subgroups. In
    addition, caches don't need to be invalidated and name clashes need
    not be considered. Hence, those aspects are stripped from the
    implementation in SchemaCreator. Other aspects like the creation of the
    MetaDataUpdate have to be done manually because the classes which would
    be typically used for them aren't available in SchemaCreator.
    
    With groups in NoteDb, we need to be careful about the order of the
    setup steps. To be able to store groups in NoteDb, we need the
    All-Users repository. That repository is also created during setup
    and uses the newly created admin group. Fortunately, the circular
    dependency can be resolved by using a group reference of a
    not-yet-existent group.
    
    Don't submit or cherry-pick this change ever alone. It is just one
    piece in a larger series of changes which all need to be applied at
    once.
    
    This change adds some TODOs which must be addressed before we can
    submit this topic.
    
    Change-Id: Ic1b37f22fa45d38ff658ee6d813833102b8095db

--- java/com/google/gerrit/server/group/db/GroupsUpdate.java
+++ java/com/google/gerrit/server/group/db/GroupsUpdate.java
@@ -212,6 +212,13 @@ public class GroupsUpdate {
     updateGroupInReviewDb(db, group, groupUpdate);
   }
 
+  public static AccountGroup createAccountGroup(
+      InternalGroupCreation groupCreation, InternalGroupUpdate groupUpdate) {
+    AccountGroup group = createAccountGroup(groupCreation);
+    applyUpdate(group, groupUpdate);
+    return group;
+  }
+
   private static AccountGroup createAccountGroup(InternalGroupCreation groupCreation) {
     return new AccountGroup(
         groupCreation.getNameKey(),

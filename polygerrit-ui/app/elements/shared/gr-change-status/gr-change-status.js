e670f2c693548123e4b166f25856943d75c8e3de
Becky Siegel <beckysiegel@google.com>
2017-10-24 17:23:42 -0700

    Add gr-change-status element
    This will be used by the change view to display chips with with the
    change status.
    
    Statuses can have tooltips if desired. A few preliminary ones have been
    added with this change (WIP, Private).
    
    Change-Id: I5c0a6c9d5abf4f8d9fa1a1bd3d224c808bb247a5

---
+++ polygerrit-ui/app/elements/shared/gr-change-status/gr-change-status.js
@@ -0,0 +1,79 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+
+  const CHANGE_STATES = {
+    merged: 'Merged',
+    abandoned: 'Abandoned',
+    merge_conflict: 'Merge Conflict',
+    wip: 'WIP',
+    private: 'Private',
+  };
+
+  Polymer({
+    is: 'gr-change-status',
+
+    properties: {
+      status: {
+        type: String,
+        observer: '_updateChipDetails',
+      },
+      title: {
+        type: String,
+        reflectToAttribute: true,
+      },
+      hasTooltip: {
+        type: Boolean,
+        reflectToAttribute: true,
+        computed: '_determineHasTooltip(title)',
+      },
+    },
+
+    behaviors: [Gerrit.TooltipBehavior],
+
+    _determineHasTooltip(title) {
+      return !!title;
+    },
+
+    _computeStatusString(status) {
+      if (status === CHANGE_STATES.wip) {
+        return 'Work in Progress';
+      }
+      return status;
+    },
+
+    _updateChipDetails(status) {
+      switch (status) {
+        case CHANGE_STATES.merged:
+          this.updateStyles({'--chip-background-color': '#4ab64a'});
+          break;
+        case CHANGE_STATES.abandoned:
+          this.updateStyles({'--chip-background-color': '#aaa'});
+          break;
+        case CHANGE_STATES.merge_conflict:
+          this.updateStyles({'--chip-background-color': '#f67070'});
+          break;
+        case CHANGE_STATES.wip:
+          this.updateStyles({'--chip-background-color': '#fcb015'});
+          this.title = 'Change is public but does not notify reviewers';
+          break;
+        case CHANGE_STATES.private:
+          this.updateStyles({'--chip-background-color': '#ff7bec'});
+          this.title = 'Change is only viewable by you';
+          break;
+      }
+    },
+  });
+})();

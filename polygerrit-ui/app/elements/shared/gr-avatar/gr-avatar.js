9f82d9424253b839ba67565a0e5f70c9c9157d8e
Paladox none <thomasmulhall410@yahoo.com>
2017-10-27 17:37:12 +0000

    PolyGerrit: Fix tag screen using avatar for tagger
    Change-Id: I5290951ef6423bc1abef77842ae0f09850fc90a9

--- polygerrit-ui/app/elements/shared/gr-avatar/gr-avatar.js
+++ polygerrit-ui/app/elements/shared/gr-avatar/gr-avatar.js
@@ -60,6 +60,14 @@
       }
     },
 
+    getAccounts(account) {
+      return
+        account._account_id ||
+        account.email ||
+        account.username ||
+        account.name;
+    },
+
     _buildAvatarURL(account) {
       if (!account) { return ''; }
       const avatars = account.avatars || [];
@@ -69,7 +77,7 @@
         }
       }
       return this.getBaseUrl() + '/accounts/' +
-        account._account_id + '/avatar?s=' + this.imageSize;
+        this.getAccounts(account) + '/avatar?s=' + this.imageSize;
     },
   });
 })();

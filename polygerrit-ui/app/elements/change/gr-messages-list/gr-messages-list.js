f3bab15b1aa4c35d6f35187a6e4d0da90afed4df
Becky Siegel <beckysiegel@google.com>
2017-11-02 16:07:36 -0700

    Get robot comments in gr-messages-list
    Bug: Issue 5902
    Change-Id: I37fee68a86c7c6a7960a3e74ee2b4c41a888504e

--- polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list.js
+++ polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list.js
@@ -35,7 +35,7 @@
         type: Array,
         value() { return []; },
       },
-      comments: Object,
+      changeComments: Object,
       projectName: String,
       showReplyButtons: {
         type: Boolean,
@@ -207,11 +207,14 @@
      * Computes message author's file comments for change's message.
      * Method uses this.messages to find next message and relies on messages
      * to be sorted by date field descending.
-     * @param {!Object} comments Hash of arrays of comments, filename as key.
+     * @param {!Object} changeComments changeComment object, which includes
+     *     a method to get all published comments (including robot comments),
+     *     which returns a Hash of arrays of comments, filename as key.
      * @param {!Object} message
      * @return {!Object} Hash of arrays of comments, filename as key.
      */
-    _computeCommentsForMessage(comments, message) {
+    _computeCommentsForMessage(changeComments, message) {
+      const comments = changeComments.getAllPublishedComments();
       if (message._index === undefined || !comments || !this.messages) {
         return [];
       }

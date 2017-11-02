11abbf48ee5b5c0240ed6379f61b26e0882603bf
Edwin Kempin <ekempin@google.com>
2017-11-02 13:32:39 +0100

    CleanUp after query tests
    Each test invocation creates a new lifecycle listeners but they are
    never stopped.
    
    Also each test invocation creates a new ReviewDb which is never
    closed.
    
    This contributes to the OutOfMemoryError that happens when
    LuceneQueryChangesTest is executed on Mac (see issue 7611). However
    this clean up alone doesn't fix it yet.
    
    Bug: Issue 7611
    Change-Id: Ib00376ac58c4c952529df034d84d5d9f9089e17c
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- gerrit-server/src/test/java/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
+++ gerrit-server/src/test/java/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
@@ -172,6 +172,12 @@ public abstract class AbstractQueryChangesTest extends GerritServerTests {
     setUpDatabase();
   }
 
+  @After
+  public void cleanUp() {
+    lifecycle.stop();
+    db.close();
+  }
+
   protected void setUpDatabase() throws Exception {
     db = schemaFactory.open();
     schemaCreator.create(db);

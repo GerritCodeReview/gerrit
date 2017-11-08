0ed45d06c99040c3794e696b2e2d4deefff38bfe
Han-Wen Nienhuys <hanwen@google.com>
2017-11-08 13:33:33 +0100

    Move group sequences to NoteDB
    This mirrors commit 5be9c31 ("Move account sequence to NoteDb"), with
    the following differences:
    
    * The group IDs go into refs/sequences/groups
    
    * Since the group sequence needs ReviewDb, but must also be available
      in SchemaCreator (which creates ReviewDb), GroupSequence is not
      instantiated through injection.
    
    * Since group creation is rare, there is no tuning for batch size or
      tracking of latency.
    
    This is the first step in the procedure to move the group IDs to
    notedb. The next steps are:
    
    * Ensure that this code is stable and rolled out
    
    * Run the schema upgrade
    
    * Flip the readSequenceFromNoteDb flag
    
    * Ensure that code is stable
    
    * Delete readSequenceFromNoteDb flag, and associated dead code.
    
    TODO Write tests for new code?
    
    Change-Id: I3c8f8c0e86de93184b3ea5e801d0f2b25121a10d

---
+++ java/com/google/gerrit/server/GroupSequence.java
@@ -0,0 +1,53 @@
+package com.google.gerrit.server;
+
+import static com.google.gerrit.server.Sequences.NAME_GROUPS;
+
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.notedb.RepoSequence;
+import com.google.gwtorm.server.OrmException;
+import org.eclipse.jgit.lib.Config;
+
+/** Like Sequences for groups, but without injection to facilitate usage in SchemaCreator. */
+public class GroupSequence {
+  private final boolean readGroupSeqFromNoteDb;
+  private final ReviewDb db;
+  private final RepoSequence groupSeq;
+
+  public GroupSequence(
+      ReviewDb db,
+      boolean readFromNoteDb,
+      AllUsersName allUsers,
+      GitRepositoryManager repoManager,
+      GitReferenceUpdated gitRefUpdated) {
+    this.db = db;
+    readGroupSeqFromNoteDb = readFromNoteDb;
+    RepoSequence.Seed groupSeed = () -> nextGroupId(db);
+
+    int groupBatchSize = 1;
+    groupSeq =
+        new RepoSequence(
+            repoManager, gitRefUpdated, allUsers, NAME_GROUPS, groupSeed, groupBatchSize);
+  }
+
+  public static boolean readSetting(Config cfg) {
+    return cfg.getBoolean("noteDb", "groups", "readSequenceFromNoteDb", false);
+  }
+
+  public int nextGroupId() throws OrmException {
+    if (readGroupSeqFromNoteDb) {
+      // NOSUBMIT: should we add latency stats here?
+      return groupSeq.next();
+    }
+    int groupId = nextGroupId(db);
+    groupSeq.increaseTo(groupId + 1); // NoteDb stores next available account ID.
+    return groupId;
+  }
+
+  @SuppressWarnings("deprecation")
+  static int nextGroupId(ReviewDb db) throws OrmException {
+    return db.nextAccountGroupId();
+  }
+}

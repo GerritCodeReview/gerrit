c04ed8a8e2a7c74da73b5dfa2c728dc6f261e33b
Dave Borowitz <dborowitz@google.com>
2017-11-08 14:28:58 -0500

    Implement group audit log in NoteDb
    In the GetAuditLog endpoint, AccountGroupMemberAudit logs are
    read and processed first followed by AccountGroupByIdAud logs.
    In the end, logs are ordered from new -> old.
    
    Currently, we only return logs created by real users and logs
    created by server are skipped. The creator of a log is decided
    by the Author of the commit. We may revisit this behavior after
    groups are fully migrated to NoteDb.
    
    Change-Id: I08e335931cefa0e99a82917e1902794980cc3d07

--- java/com/google/gerrit/server/group/db/GroupConfig.java
+++ java/com/google/gerrit/server/group/db/GroupConfig.java
@@ -44,12 +44,20 @@ import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.FooterKey;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevSort;
+import org.eclipse.jgit.revwalk.RevWalk;
 
 // TODO(aliceks): Add Javadoc descriptions to this file.
 public class GroupConfig extends VersionedMetaData {
   public static final String GROUP_CONFIG_FILE = "group.config";
+
+  static final FooterKey FOOTER_ADD_MEMBER = new FooterKey("Add");
+  static final FooterKey FOOTER_REMOVE_MEMBER = new FooterKey("Remove");
+  static final FooterKey FOOTER_ADD_GROUP = new FooterKey("Add-group");
+  static final FooterKey FOOTER_REMOVE_GROUP = new FooterKey("Remove-group");
+
   private static final String MEMBERS_FILE = "members";
   private static final String SUBGROUPS_FILE = "subgroups";
   private static final Pattern LINE_SEPARATOR_PATTERN = Pattern.compile("\\R");
@@ -75,6 +83,13 @@ public class GroupConfig extends VersionedMetaData {
     GroupConfig groupConfig = new GroupConfig(groupCreation.getGroupUUID());
     groupConfig.load(repository);
     groupConfig.setGroupCreation(groupCreation);
+    return groupConfig;
+  }
+
+  static GroupConfig loadForGroupNoOwnerUpdate(RevWalk rw, ObjectId id, AccountGroup.UUID groupUuid)
+      throws IOException, ConfigInvalidException {
+    GroupConfig groupConfig = new GroupConfig(groupUuid);
+    groupConfig.load(rw, id);
     return groupConfig;
   }
 
@@ -332,12 +347,12 @@ public class GroupConfig extends VersionedMetaData {
         Sets.difference(oldMembers, newMembers)
             .stream()
             .map(accountNameEmailRetriever)
-            .map("Remove: "::concat);
+            .map((FOOTER_REMOVE_MEMBER.getName() + ": ")::concat);
     Stream<String> addedMembers =
         Sets.difference(newMembers, oldMembers)
             .stream()
             .map(accountNameEmailRetriever)
-            .map("Add: "::concat);
+            .map((FOOTER_ADD_MEMBER.getName() + ": ")::concat);
     return Stream.concat(removedMembers, addedMembers);
   }
 
@@ -347,12 +362,12 @@ public class GroupConfig extends VersionedMetaData {
         Sets.difference(oldSubgroups, newSubgroups)
             .stream()
             .map(groupNameRetriever)
-            .map("Remove-group: "::concat);
+            .map((FOOTER_REMOVE_GROUP.getName() + ": ")::concat);
     Stream<String> addedMembers =
         Sets.difference(newSubgroups, oldSubgroups)
             .stream()
             .map(groupNameRetriever)
-            .map("Add-group: "::concat);
+            .map((FOOTER_ADD_GROUP.getName() + ": ")::concat);
     return Stream.concat(removedMembers, addedMembers);
   }
 }

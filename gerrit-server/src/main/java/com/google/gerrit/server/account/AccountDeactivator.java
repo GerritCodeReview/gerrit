328e86d0f6be6a8da82e28ccbb2797f416744ead
Owen Li <owen.a.li@ericsson.com>
2017-11-13 10:16:08 -0500

    Allow deactivation loop to continue running after an error
    Currently, the deactivation cron job stops if it fails to deactivate an
    account. Change the behaviour such that it logs which account failed,
    and continues instead of stopping prematurely.
    
    Change-Id: I8542e18368e47500cadb7cbcab27769175023181

--- gerrit-server/src/main/java/com/google/gerrit/server/account/AccountDeactivator.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/account/AccountDeactivator.java
@@ -96,21 +96,30 @@ public class AccountDeactivator implements Runnable {
 
   @Override
   public void run() {
-    log.debug("Running account deactivations");
+    log.info("Running account deactivations");
     try {
       int numberOfAccountsDeactivated = 0;
       for (AccountState acc : accountQueryProvider.get().query(AccountPredicates.isActive())) {
         log.debug("processing account " + acc.getUserName());
-        if (acc.getUserName() != null && !realm.isActive(acc.getUserName())) {
-          sif.deactivate(acc.getAccount().getId());
-          log.debug("deactivated accout " + acc.getUserName());
-          numberOfAccountsDeactivated++;
+        try {
+          if (acc.getUserName() != null && !realm.isActive(acc.getUserName())) {
+            sif.deactivate(acc.getAccount().getId());
+            log.info("deactivated account " + acc.getUserName());
+            numberOfAccountsDeactivated++;
+          }
+        } catch (Exception e) {
+          log.error(
+              "Error deactivating account: {} ({}) {}",
+              acc.getUserName(),
+              acc.getAccount().getId(),
+              e.getMessage(),
+              e);
         }
       }
       log.info(
           "Deactivations complete, {} account(s) were deactivated", numberOfAccountsDeactivated);
     } catch (Exception e) {
-      log.error("Failed to deactivate inactive accounts " + e.getMessage(), e);
+      log.error("Failed to complete deactivation of accounts: " + e.getMessage(), e);
     }
   }
 

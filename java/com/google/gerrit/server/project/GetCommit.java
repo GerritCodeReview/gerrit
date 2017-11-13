fc03a2d658c62f097ae4efdad186b346b6adccf5
Dave Borowitz <dborowitz@google.com>
2017-11-13 09:52:07 -0500

    Factor GetCommit#toCommitInfo to a utility class
    Ideally this would go in CommitInfo itself, but we can't use JGit from
    the extension API POJOs.
    
    Change-Id: I290ed0a0af876068a86c90a87c91c3ba00f9307d

--- java/com/google/gerrit/server/project/GetCommit.java
+++ java/com/google/gerrit/server/project/GetCommit.java
@@ -16,34 +16,14 @@ package com.google.gerrit.server.project;
 
 import com.google.gerrit.extensions.common.CommitInfo;
 import com.google.gerrit.extensions.restapi.RestReadView;
-import com.google.gerrit.server.CommonConverters;
+import com.google.gerrit.server.git.CommitUtil;
 import com.google.inject.Singleton;
-import java.util.ArrayList;
-import org.eclipse.jgit.revwalk.RevCommit;
 
 @Singleton
 public class GetCommit implements RestReadView<CommitResource> {
 
   @Override
   public CommitInfo apply(CommitResource rsrc) {
-    return toCommitInfo(rsrc.getCommit());
-  }
-
-  private static CommitInfo toCommitInfo(RevCommit commit) {
-    CommitInfo info = new CommitInfo();
-    info.commit = commit.getName();
-    info.author = CommonConverters.toGitPerson(commit.getAuthorIdent());
-    info.committer = CommonConverters.toGitPerson(commit.getCommitterIdent());
-    info.subject = commit.getShortMessage();
-    info.message = commit.getFullMessage();
-    info.parents = new ArrayList<>(commit.getParentCount());
-    for (int i = 0; i < commit.getParentCount(); i++) {
-      RevCommit p = commit.getParent(i);
-      CommitInfo parentInfo = new CommitInfo();
-      parentInfo.commit = p.getName();
-      parentInfo.subject = p.getShortMessage();
-      info.parents.add(parentInfo);
-    }
-    return info;
+    return CommitUtil.toCommitInfo(rsrc.getCommit());
   }
 }

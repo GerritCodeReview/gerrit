0428c4c7af8879941a8c910ce704dac94746e1dc
Han-Wen Nienhuys <hanwen@google.com>
2017-11-14 14:33:21 +0100

    Consistency checker for groups
    This checks for
    
     * Internal groups that includes themselves (transitively)
     * Dangling group references
     * Dangling group names.
     * Non-unique group names
     * Non-unique sequential group IDs.
    
    TODO:
     * skip if not using notedb
     * add a call to @tearDown of all groups tests
     * paranoia mode that checks each transaction
    
    Change-Id: I0f9fdea82128581f893ac3bfb5b3714fafffd858

--- java/com/google/gerrit/server/group/db/GroupConfig.java
+++ java/com/google/gerrit/server/group/db/GroupConfig.java
@@ -47,7 +47,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevSort;
 
-// TODO(aliceks): Add Javadoc descriptions to this file.
+/** Holds code for reading and writing internal group data for a single group to/from NoteDB. */
 public class GroupConfig extends VersionedMetaData {
   public static final String GROUP_CONFIG_FILE = "group.config";
   private static final String MEMBERS_FILE = "members";
@@ -82,6 +82,15 @@ public class GroupConfig extends VersionedMetaData {
       throws IOException, ConfigInvalidException {
     GroupConfig groupConfig = new GroupConfig(groupUuid);
     groupConfig.load(repository);
+    return groupConfig;
+  }
+
+  /** Loads a group at a specific revision. */
+  public static GroupConfig loadForGroupSnapshot(
+      Repository repository, AccountGroup.UUID groupUuid, ObjectId commitId)
+      throws IOException, ConfigInvalidException {
+    GroupConfig groupConfig = new GroupConfig(groupUuid);
+    groupConfig.load(repository, commitId);
     return groupConfig;
   }
 

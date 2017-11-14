b69e5fc7d0c9d7e4337b915c2837156f82ca58a1
Han-Wen Nienhuys <hanwen@google.com>
2017-11-14 16:16:48 +0100

    Split off config REST endpoints from giant server lib
    The implementations of the individual REST endpoints use the giant
    server library as clients.
    
    Change-Id: I724cb3efe160d2f62fa1a67823999abad589e246

--- java/com/google/gerrit/testing/InMemoryModule.java
+++ java/com/google/gerrit/testing/InMemoryModule.java
@@ -47,6 +47,7 @@ import com.google.gerrit.server.config.GerritServerId;
 import com.google.gerrit.server.config.SitePath;
 import com.google.gerrit.server.config.TrackingFooters;
 import com.google.gerrit.server.config.TrackingFootersProvider;
+import com.google.gerrit.server.config.rest.ConfigRestModule;
 import com.google.gerrit.server.git.GarbageCollection;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.LocalMergeSuperSetComputation;
@@ -166,6 +167,7 @@ public class InMemoryModule extends FactoryModule {
     install(new GerritApiModule());
     install(new PluginApiModule());
     install(new DefaultPermissionBackendModule());
+    install(new ConfigRestModule());
     install(new SearchingChangeCacheImpl.Module());
     factory(GarbageCollection.Factory.class);
 

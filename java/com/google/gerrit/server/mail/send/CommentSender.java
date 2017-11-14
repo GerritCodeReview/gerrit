4d60546ad971ebbac3d6706c7223eb484eb2f4af
Han-Wen Nienhuys <hanwen@google.com>
2017-11-14 18:04:28 +0100

    Split off mail data processing out of server package
    Much of the mail processing code is not specific to building a code
    review server, so it doesn't need to live in the server package.
    
    Change-Id: Ie5b22908fecdfa8b2e26eacb9dabb87123c39e27

--- java/com/google/gerrit/server/mail/send/CommentSender.java
+++ java/com/google/gerrit/server/mail/send/CommentSender.java
@@ -23,6 +23,8 @@ import com.google.gerrit.common.data.FilenameComparator;
 import com.google.gerrit.common.errors.EmailException;
 import com.google.gerrit.common.errors.NoSuchEntityException;
 import com.google.gerrit.extensions.api.changes.NotifyHandling;
+import com.google.gerrit.mail.MailHeader;
+import com.google.gerrit.mail.MailProcessingUtil;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.Comment;
@@ -32,8 +34,6 @@ import com.google.gerrit.reviewdb.client.RobotComment;
 import com.google.gerrit.server.CommentsUtil;
 import com.google.gerrit.server.account.ProjectWatches.NotifyType;
 import com.google.gerrit.server.config.GerritServerConfig;
-import com.google.gerrit.server.mail.MailHeader;
-import com.google.gerrit.server.mail.MailUtil;
 import com.google.gerrit.server.mail.receive.Protocol;
 import com.google.gerrit.server.patch.PatchFile;
 import com.google.gerrit.server.patch.PatchList;
@@ -566,7 +566,7 @@ public class CommentSender extends ReplyToChangeSender {
 
   private String getCommentTimestamp() {
     // Grouping is currently done by timestamp.
-    return MailUtil.rfcDateformatter.format(
+    return MailProcessingUtil.rfcDateformatter.format(
         ZonedDateTime.ofInstant(timestamp.toInstant(), ZoneId.of("UTC")));
   }
 

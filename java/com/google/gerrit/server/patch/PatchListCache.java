3da22613719613789a5ac3760831e8d76e18c4ad
Paladox none <thomasmulhall410@yahoo.com>
2017-11-16 18:27:46 +0000

    Revert "Split off library classes from server/patch/ into diff/"
    This reverts commit 0d157b35613c37907a65e3f99c5d94b8d6dffd50.
    
    It is causing reindex to fail with warnings
    
    Bug: Issue 7784
    Change-Id: I4580974e5f5f95fe74e22714bca5772538368b02

--- java/com/google/gerrit/server/patch/PatchListCache.java
+++ java/com/google/gerrit/server/patch/PatchListCache.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.diff;
+package com.google.gerrit.server.patch;
 
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSet;

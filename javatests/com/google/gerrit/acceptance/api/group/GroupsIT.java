864d3d906c1be8ca5e1ac63a59aab60a397b290c
Tung Quach <anhthuyen7939@gmail.com>
2017-11-21 19:22:20 +0000

    Revert "GroupsIT: Remove unnecessary @SuppressWarnings("deprecation")"
    This reverts commit dd512f86757b9c7ae6dda47e2835fd3a5d12b130.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Ibfc949848ea7ff8bcaeebac7565891f2dbaeeb3b

--- javatests/com/google/gerrit/acceptance/api/group/GroupsIT.java
+++ javatests/com/google/gerrit/acceptance/api/group/GroupsIT.java
@@ -420,6 +420,7 @@ public class GroupsIT extends AbstractDaemonTest {
     assertThat(gApi.groups().id(name).options().visibleToAll).isTrue();
   }
 
+  @SuppressWarnings("deprecation")
   @Test
   public void groupOwner() throws Exception {
     String name = name("group");

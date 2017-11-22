5e347f614b20d64f1a3f882e331b5ee8686ea33f
Edwin Kempin <ekempin@google.com>
2017-11-22 14:40:46 +0100

    Reset project state of All-Projects and All-Users after each test
    Tests that modify the config of All-Projects or All-Users (e.g. by
    changing permissions, capabilities, label definitions etc.) or that
    create or modify accounts either need to be sandboxed or do cleanups in
    a finally block so that the config and account changes don't affect
    other tests. Resetting the state for All-Projects and All-Users for each
    test makes this unnecessary.
    
    As result of this some @Sandbox annotations can be removed.
    
    In general resetting the state for All-Projects and All-Users for each
    test makes it less likely that tests have unintended side effects on
    each other.
    
    When resetting project states in can be controlled by ref patterns which
    branches should be reset.
    
    Resetting the project state is done by saving the states of all refs
    before the test and then resetting all refs to the saved states after
    the test. Refs that were newly created during the tests are deleted.
    
    If resetting touches refs/meta/config branches the corresponding
    projects are evicted from the project cache.
    
    If resetting touches user branches or the refs/meta/external-ids branch
    the corresponding accounts are evicted from the account cache and also
    if needed from the cache in AccountCreator.
    
    Group branches cannot be reset since this would make the group data
    between ReviewDb and NoteDb inconsistent. Once groups in ReviewDb are no
    longer supported we can also reset group branches.
    
    AbstractNotificationTest has local caching for accounts that should be
    reused across all test cases. Removing this caching makes this test very
    slow. To not affect this test resetting project states is disabled for
    this test.
    
    For GroupsIT don't reset All-Users since deleting users makes groups
    inconsistent (e.g. groups would contain members that no longer exist)
    and as result of this the group consistency checker that is executed
    after each test would fail.
    
    Some tests were annotated with @Sandboxed because they set a global
    flag. While we are at it remove @Sandboxed from these tests as well and
    instead use an Autoclosable to unset the flag at the end of the test.
    
    Removing almost all usages of @Sandboxed makes the tests slightly faster
    on my machine.
    
    Change-Id: I1bb46bb18d62a1497447d470c4e96aa859570cd3
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ java/com/google/gerrit/acceptance/ProjectResetter.java
@@ -0,0 +1,289 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance;
+
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.stream.Collectors.toSet;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+import com.google.common.collect.Sets;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.client.RefNames;
+import com.google.gerrit.server.account.AccountCache;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.index.RefState;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.project.RefPatternMatcher;
+import com.google.inject.Inject;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Saves the states of given projects and resets the project states on close.
+ *
+ * <p>Saving the project states is done by saving the states of all refs in the project. On close
+ * those refs are reset to the saved states. Refs that were newly created are deleted.
+ *
+ * <p>By providing ref patterns per project it can be controlled which refs should be reset on
+ * close.
+ *
+ * <p>If resetting touches {@code refs/meta/config} branches the corresponding projects are evicted
+ * from the project cache.
+ *
+ * <p>If resetting touches user branches or the {@code refs/meta/external-ids} branch the
+ * corresponding accounts are evicted from the account cache and also if needed from the cache in
+ * {@link AccountCreator}.
+ *
+ * <p>At the moment this class has the following limitations:
+ *
+ * <ul>
+ *   <li>Resetting group branches doesn't evict the corresponding groups from the group cache.
+ *   <li>Changes are not reindexed if starred-changes refs in All-Users are reset.
+ * </ul>
+ */
+public class ProjectResetter implements AutoCloseable {
+  public static class Builder {
+    public interface Factory {
+      Builder builder();
+    }
+
+    private final GitRepositoryManager repoManager;
+    private final AllUsersName allUsersName;
+    @Nullable private final AccountCreator accountCreator;
+    @Nullable private final AccountCache accountCache;
+    @Nullable private final ProjectCache projectCache;
+
+    private final Multimap<Project.NameKey, String> refsByProject;
+
+    @Inject
+    public Builder(
+        GitRepositoryManager repoManager,
+        AllUsersName allUsersName,
+        @Nullable AccountCreator accountCreator,
+        @Nullable AccountCache accountCache,
+        @Nullable ProjectCache projectCache) {
+      this.repoManager = repoManager;
+      this.allUsersName = allUsersName;
+      this.accountCreator = accountCreator;
+      this.accountCache = accountCache;
+      this.projectCache = projectCache;
+      this.refsByProject = MultimapBuilder.hashKeys().arrayListValues().build();
+    }
+
+    public Builder reset(Project.NameKey project, String... refPatterns) {
+      List<String> refPatternList = Arrays.asList(refPatterns);
+      if (refPatternList.isEmpty()) {
+        refPatternList = ImmutableList.of(RefNames.REFS + "*");
+      }
+      refsByProject.putAll(project, refPatternList);
+      return this;
+    }
+
+    public ProjectResetter build() throws IOException {
+      return new ProjectResetter(
+          repoManager, allUsersName, accountCreator, accountCache, projectCache, refsByProject);
+    }
+  }
+
+  private final GitRepositoryManager repoManager;
+  private final AllUsersName allUsersName;
+  @Nullable private final AccountCreator accountCreator;
+  @Nullable private final AccountCache accountCache;
+  @Nullable private final ProjectCache projectCache;
+  private final Multimap<Project.NameKey, String> refsPatternByProject;
+  private final Multimap<Project.NameKey, RefState> savedRefStatesByProject;
+
+  private ProjectResetter(
+      GitRepositoryManager repoManager,
+      AllUsersName allUsersName,
+      @Nullable AccountCreator accountCreator,
+      @Nullable AccountCache accountCache,
+      @Nullable ProjectCache projectCache,
+      Multimap<Project.NameKey, String> refPatternByProject)
+      throws IOException {
+    this.repoManager = repoManager;
+    this.allUsersName = allUsersName;
+    this.accountCreator = accountCreator;
+    this.accountCache = accountCache;
+    this.projectCache = projectCache;
+    this.refsPatternByProject = refPatternByProject;
+    this.savedRefStatesByProject = readRefStates();
+  }
+
+  @Override
+  public void close() throws Exception {
+    // Restore all saved refs if they were modified.
+    Multimap<Project.NameKey, String> keptRefsByProject =
+        MultimapBuilder.hashKeys().arrayListValues().build();
+    Multimap<Project.NameKey, String> restoredRefsByProject =
+        MultimapBuilder.hashKeys().arrayListValues().build();
+    for (Map.Entry<Project.NameKey, Collection<RefState>> e :
+        savedRefStatesByProject.asMap().entrySet()) {
+      try (Repository repo = repoManager.openRepository(e.getKey())) {
+        for (RefState refState : e.getValue()) {
+          if (refState.match(repo)) {
+            keptRefsByProject.put(e.getKey(), refState.ref());
+            continue;
+          }
+          Ref ref = repo.exactRef(refState.ref());
+          RefUpdate updateRef = repo.updateRef(refState.ref());
+          updateRef.setExpectedOldObjectId(ref != null ? ref.getObjectId() : ObjectId.zeroId());
+          updateRef.setNewObjectId(refState.id());
+          updateRef.setForceUpdate(true);
+          RefUpdate.Result result = updateRef.update();
+          checkState(
+              result == RefUpdate.Result.FORCED || result == RefUpdate.Result.NEW,
+              "resetting branch %s in %s failed",
+              refState.ref(),
+              e.getKey());
+          restoredRefsByProject.put(e.getKey(), refState.ref());
+        }
+      }
+    }
+
+    // Delete all other matching refs.
+    Multimap<Project.NameKey, String> deletedRefsByProject =
+        MultimapBuilder.hashKeys().arrayListValues().build();
+    for (Map.Entry<Project.NameKey, Collection<String>> e :
+        refsPatternByProject.asMap().entrySet()) {
+      try (Repository repo = repoManager.openRepository(e.getKey())) {
+        Collection<Ref> nonRestoredRefs =
+            repo.getAllRefs()
+                .values()
+                .stream()
+                .filter(
+                    r ->
+                        !keptRefsByProject.containsEntry(e.getKey(), r.getName())
+                            && !restoredRefsByProject.containsEntry(e.getKey(), r.getName()))
+                .collect(toSet());
+        for (String refPattern : e.getValue()) {
+          RefPatternMatcher matcher = RefPatternMatcher.getMatcher(refPattern);
+          for (Ref ref : nonRestoredRefs) {
+            if (matcher.match(ref.getName(), null)
+                && !deletedRefsByProject.containsEntry(e.getKey(), ref.getName())) {
+              RefUpdate updateRef = repo.updateRef(ref.getName());
+              updateRef.setExpectedOldObjectId(ref.getObjectId());
+              updateRef.setNewObjectId(ObjectId.zeroId());
+              updateRef.setForceUpdate(true);
+              RefUpdate.Result result = updateRef.delete();
+              checkState(
+                  result == RefUpdate.Result.FORCED,
+                  "deleting branch %s in %s failed",
+                  ref.getName(),
+                  e.getKey());
+              deletedRefsByProject.put(e.getKey(), ref.getName());
+            }
+          }
+        }
+      }
+    }
+
+    // Evict projects for which the config was changed.
+    if (projectCache != null) {
+      for (Project.NameKey project :
+          Sets.union(
+              projectsWithConfigChanges(restoredRefsByProject),
+              projectsWithConfigChanges(deletedRefsByProject))) {
+        projectCache.evict(project);
+      }
+    }
+
+    // Evict accounts that were modified.
+    Set<Account.Id> deletedAccounts = accountIds(deletedRefsByProject.get(allUsersName));
+    if (accountCreator != null && !deletedAccounts.isEmpty()) {
+      accountCreator.evict(deletedAccounts);
+    }
+    if (accountCache != null) {
+      Set<Account.Id> modifiedAccounts =
+          new HashSet<>(accountIds(restoredRefsByProject.get(allUsersName)));
+
+      if (restoredRefsByProject.get(allUsersName).contains(RefNames.REFS_EXTERNAL_IDS)
+          || deletedRefsByProject.get(allUsersName).contains(RefNames.REFS_EXTERNAL_IDS)) {
+        // The external IDs have been modified but we don't know which accounts were affected.
+        // Make sure all accounts are evicted and reindexed.
+        try (Repository repo = repoManager.openRepository(allUsersName)) {
+          for (Account.Id id :
+              accountIds(
+                  repo.getAllRefs().values().stream().map(r -> r.getName()).collect(toSet()))) {
+            accountCache.evict(id);
+          }
+        }
+
+        // Remove deleted accounts from the cache and index.
+        for (Account.Id id : deletedAccounts) {
+          accountCache.evict(id);
+        }
+      } else {
+        // Evict and reindex all modified and deleted accounts.
+        for (Account.Id id : Sets.union(modifiedAccounts, deletedAccounts)) {
+          accountCache.evict(id);
+        }
+      }
+    }
+
+    // TODO(ekempin): Evict groups from cache if group refs were modified.
+    // TODO(ekempin): Reindex changes if starred-changes refs in All-Users were modified.
+  }
+
+  /** Read the states of all matching refs. */
+  private Multimap<Project.NameKey, RefState> readRefStates() throws IOException {
+    Multimap<Project.NameKey, RefState> refStatesByProject =
+        MultimapBuilder.hashKeys().arrayListValues().build();
+    for (Map.Entry<Project.NameKey, Collection<String>> e :
+        refsPatternByProject.asMap().entrySet()) {
+      try (Repository repo = repoManager.openRepository(e.getKey())) {
+        Collection<Ref> refs = repo.getAllRefs().values();
+        for (String refPattern : e.getValue()) {
+          RefPatternMatcher matcher = RefPatternMatcher.getMatcher(refPattern);
+          for (Ref ref : refs) {
+            if (matcher.match(ref.getName(), null)) {
+              refStatesByProject.put(e.getKey(), RefState.create(ref.getName(), ref.getObjectId()));
+            }
+          }
+        }
+      }
+    }
+    return refStatesByProject;
+  }
+
+  private Set<Project.NameKey> projectsWithConfigChanges(
+      Multimap<Project.NameKey, String> projects) {
+    return projects
+        .entries()
+        .stream()
+        .filter(e -> e.getValue().contains(RefNames.REFS_CONFIG))
+        .map(Map.Entry::getKey)
+        .collect(toSet());
+  }
+
+  private Set<Account.Id> accountIds(Collection<String> refs) {
+    return refs.stream().map(r -> Account.Id.fromRef(r)).filter(Objects::nonNull).collect(toSet());
+  }
+}

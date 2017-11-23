dc6fc2547ff700e37944fbc17d14813cdf89841b
Han-Wen Nienhuys <hanwen@google.com>
2017-11-23 16:45:13 +0100

    Split global consistency checks from the per-group sanity checks
    For the NoteDb migration, we are more interested in the former
    problems, and it is unclear if our bulk migration tooling has the
    capacity to contact arbitrary group backends.
    
    Change-Id: I49eac82fb75a0e94148d262a76e82e97a1d3eb8d

--- java/com/google/gerrit/server/group/db/GroupsConsistencyChecker.java
+++ java/com/google/gerrit/server/group/db/GroupsConsistencyChecker.java
@@ -14,163 +14,81 @@
 
 package com.google.gerrit.server.group.db;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
-import com.google.gerrit.common.data.GroupReference;
+import static com.google.gerrit.extensions.api.config.ConsistencyCheckInfo.ConsistencyProblemInfo.error;
+import static com.google.gerrit.extensions.api.config.ConsistencyCheckInfo.ConsistencyProblemInfo.warning;
+
+import com.google.common.collect.ImmutableMap;
 import com.google.gerrit.extensions.api.config.ConsistencyCheckInfo.ConsistencyProblemInfo;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.AccountGroup;
-import com.google.gerrit.reviewdb.client.AccountGroup.NameKey;
-import com.google.gerrit.reviewdb.client.RefNames;
 import com.google.gerrit.server.account.Accounts;
 import com.google.gerrit.server.account.GroupBackend;
 import com.google.gerrit.server.config.AllUsersName;
-import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.group.InternalGroup;
 import com.google.gerrit.server.notedb.GroupsMigration;
 import com.google.gwtorm.server.OrmException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.eclipse.jgit.errors.ConfigInvalidException;
-import org.eclipse.jgit.lib.Config;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectLoader;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.notes.Note;
-import org.eclipse.jgit.notes.NoteMap;
-import org.eclipse.jgit.revwalk.RevWalk;
 
-/** Checks internal consistency of NoteDb storage of groups. */
+/**
+ * Checks individual groups for oddities, such as cycles, non-existent subgroups, etc. Only works if
+ * we are writing to NoteDb.
+ */
 @Singleton
 public class GroupsConsistencyChecker {
+  private final AllUsersName allUsersName;
   private final GroupBackend groupBackend;
   private final Accounts accounts;
-  private final AllUsersName allUsersName;
   private final GitRepositoryManager repoManager;
+  private final GroupsNoteDbConsistencyChecker globalChecker;
   private final GroupsMigration groupsMigration;
 
   @Inject
   GroupsConsistencyChecker(
-      Accounts accounts,
-      GroupBackend groupBackend,
       AllUsersName allUsersName,
+      GroupBackend groupBackend,
+      Accounts accounts,
       GitRepositoryManager repositoryManager,
-      GroupsMigration groupsMigration,
-      @GerritServerConfig Config config) {
-    this.accounts = accounts;
-    this.groupBackend = groupBackend;
+      GroupsNoteDbConsistencyChecker globalChecker,
+      GroupsMigration groupsMigration) {
     this.allUsersName = allUsersName;
+    this.groupBackend = groupBackend;
+    this.accounts = accounts;
     this.repoManager = repositoryManager;
+    this.globalChecker = globalChecker;
     this.groupsMigration = groupsMigration;
   }
 
-  /** Checks that all internal group references exist, and that no groups have cycles. */
   public List<ConsistencyProblemInfo> check() throws OrmException, IOException {
     if (!groupsMigration.writeToNoteDb()) {
       return new ArrayList<>();
     }
 
     try (Repository repo = repoManager.openRepository(allUsersName)) {
-      return check(repo);
-    }
-  }
-
-  public List<ConsistencyProblemInfo> check(Repository repo) throws OrmException, IOException {
-    // Get all refs in an attempt to avoid seeing half committed group updates.
-    Map<String, Ref> refs = repo.getAllRefs();
-
-    List<ConsistencyProblemInfo> problems = new ArrayList<>();
-    Map<AccountGroup.UUID, InternalGroup> byUUID = new HashMap<>();
-    BiMap<AccountGroup.UUID, String> nameMap = HashBiMap.create();
-
-    readGroups(repo, refs, problems, byUUID);
-    readGroupNames(repo, refs, problems, nameMap);
-
-    // The sequential IDs are not keys in NoteDb, so no need to check them.
-
-    // No use continuing if we couldn't read the data.
-    if (!problems.isEmpty()) {
+      List<ConsistencyProblemInfo> problems = globalChecker.check(repo);
+      if (problems.isEmpty()) {
+        ImmutableMap<AccountGroup.UUID, InternalGroup> byUUID = globalChecker.getUuidToGroupMap();
+        for (InternalGroup g : byUUID.values()) {
+          problems.addAll(checkGroup(g, byUUID));
+        }
+      }
       return problems;
     }
-
-    problems = checkGlobalConsistency(byUUID, nameMap);
-    if (!problems.isEmpty()) {
-      return problems;
-    }
-
-    // Check subgroups and members.
-    for (InternalGroup g : byUUID.values()) {
-      problems.addAll(checkGroup(g, byUUID));
-    }
-    return problems;
-  }
-
-  /** Check invariants of the group refs with the groupname refs. */
-  private List<ConsistencyProblemInfo> checkGlobalConsistency(
-      Map<AccountGroup.UUID, InternalGroup> byUUID, BiMap<AccountGroup.UUID, String> nameMap) {
-    List<ConsistencyProblemInfo> problems = new ArrayList<>();
-
-    // Check consistency between the data coming from different refs.
-    for (AccountGroup.UUID uuid : byUUID.keySet()) {
-      if (!nameMap.containsKey(uuid)) {
-        problems.add(error("group %s has no entry in name map", uuid));
-        continue;
-      }
-
-      String noteName = nameMap.get(uuid);
-      String groupRefName = byUUID.get(uuid).getName();
-      if (!Objects.equals(noteName, groupRefName)) {
-        problems.add(
-            error(
-                "inconsistent name for group %s (name map %s vs. group ref %s)",
-                uuid, noteName, groupRefName));
-      }
-    }
-
-    for (AccountGroup.UUID uuid : nameMap.keySet()) {
-      if (!byUUID.containsKey(uuid)) {
-        problems.add(
-            error(
-                "name map has entry (%s, %s), entry missing as group ref",
-                uuid, nameMap.get(uuid)));
-      }
-    }
-
-    // No use delving further into inconsistent data.
-    if (!problems.isEmpty()) {
-      return problems;
-    }
-
-    // Check ids.
-    Map<AccountGroup.Id, InternalGroup> groupById = new HashMap<>();
-    for (InternalGroup g : byUUID.values()) {
-      InternalGroup before = groupById.get(g.getId());
-      if (before != null) {
-        problems.add(
-            error(
-                "shared group id %s for %s (%s) and %s (%s)",
-                g.getId(), before.getName(), before.getGroupUUID(), g.getName(), g.getGroupUUID()));
-      }
-      groupById.put(g.getId(), g);
-    }
-    return problems;
   }
 
   /** Checks the metadata for a single group for problems. */
   private List<ConsistencyProblemInfo> checkGroup(
-      InternalGroup g, Map<AccountGroup.UUID, InternalGroup> byUUID) throws IOException {
+      InternalGroup g, ImmutableMap<AccountGroup.UUID, InternalGroup> byUUID) throws IOException {
     List<ConsistencyProblemInfo> problems = new ArrayList<>();
 
     problems.addAll(checkCycle(g, byUUID));
@@ -211,81 +129,9 @@ public class GroupsConsistencyChecker {
     return problems;
   }
 
-  private void readGroupNames(
-      Repository repo,
-      Map<String, Ref> refs,
-      List<ConsistencyProblemInfo> problems,
-      BiMap<AccountGroup.UUID, String> result)
-      throws IOException {
-    Ref ref = refs.get(RefNames.REFS_GROUPNAMES);
-    if (ref == null) {
-      problems.add(
-          new ConsistencyProblemInfo(
-              groupsMigration.readFromNoteDb()
-                  ? ConsistencyProblemInfo.Status.ERROR
-                  : ConsistencyProblemInfo.Status.WARNING,
-              String.format("ref %s does not exist", RefNames.REFS_GROUPNAMES)));
-      return;
-    }
-
-    try (RevWalk rw = new RevWalk(repo)) {
-      NoteMap nm = NoteMap.read(rw.getObjectReader(), rw.parseCommit(ref.getObjectId()));
-      for (Note note : nm) {
-        ObjectLoader ld = rw.getObjectReader().open(note.getData());
-        byte[] data = ld.getCachedBytes();
-
-        GroupReference gRef;
-        try {
-          gRef = GroupNameNotes.getFromNoteData(data);
-        } catch (ConfigInvalidException e) {
-          problems.add(
-              error(
-                  "notename entry %s: %s does not parse: %s",
-                  note, new String(data, StandardCharsets.UTF_8), e.getMessage()));
-          continue;
-        }
-
-        ObjectId nameKey = GroupNameNotes.getNoteKey(new NameKey(gRef.getName()));
-        if (!Objects.equals(nameKey, note)) {
-          problems.add(error("notename entry %s does not match name %s", note, gRef.getName()));
-        }
-
-        // We trust SHA1 to have no collisions, so no need to check uniqueness of name.
-        result.put(gRef.getUUID(), gRef.getName());
-      }
-    }
-  }
-
-  private void readGroups(
-      Repository repo,
-      Map<String, Ref> refs,
-      List<ConsistencyProblemInfo> problems,
-      Map<AccountGroup.UUID, InternalGroup> byUUID)
-      throws IOException {
-    for (Map.Entry<String, Ref> entry : refs.entrySet()) {
-      if (!entry.getKey().startsWith(RefNames.REFS_GROUPS)) {
-        continue;
-      }
-
-      AccountGroup.UUID uuid = AccountGroup.UUID.fromRef(entry.getKey());
-      if (uuid == null) {
-        problems.add(error("null UUID from %s", entry.getKey()));
-        continue;
-      }
-      try {
-        GroupConfig cfg =
-            GroupConfig.loadForGroupSnapshot(repo, uuid, entry.getValue().getObjectId());
-
-        byUUID.put(uuid, cfg.getLoadedGroup().get());
-      } catch (ConfigInvalidException e) {
-        problems.add(error("group %s does not parse: %s", uuid, e.getMessage()));
-      }
-    }
-  }
-
   /** checkCycle walks through root's subgroups recursively, and checks for cycles. */
   private List<ConsistencyProblemInfo> checkCycle(
-      InternalGroup root, Map<AccountGroup.UUID, InternalGroup> byUUID) {
+      InternalGroup root, ImmutableMap<AccountGroup.UUID, InternalGroup> byUUID) {
     List<ConsistencyProblemInfo> problems = new ArrayList<>();
     Set<InternalGroup> todo = new LinkedHashSet<>();
     Set<InternalGroup> seen = new HashSet<>();
@@ -318,15 +164,5 @@ public class GroupsConsistencyChecker {
       }
     }
     return problems;
-  }
-
-  private ConsistencyProblemInfo warning(String fmt, Object... args) {
-    return new ConsistencyProblemInfo(
-        ConsistencyProblemInfo.Status.WARNING, String.format(fmt, args));
-  }
-
-  private ConsistencyProblemInfo error(String fmt, Object... args) {
-    return new ConsistencyProblemInfo(
-        ConsistencyProblemInfo.Status.ERROR, String.format(fmt, args));
   }
 }
---
+++ java/com/google/gerrit/server/group/db/GroupsNoteDbConsistencyChecker.java
@@ -0,0 +1,206 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.group.db;
+
+import static com.google.gerrit.extensions.api.config.ConsistencyCheckInfo.ConsistencyProblemInfo.error;
+import static com.google.gerrit.extensions.api.config.ConsistencyCheckInfo.ConsistencyProblemInfo.warning;
+
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableMap;
+import com.google.gerrit.common.data.GroupReference;
+import com.google.gerrit.extensions.api.config.ConsistencyCheckInfo.ConsistencyProblemInfo;
+import com.google.gerrit.reviewdb.client.AccountGroup;
+import com.google.gerrit.reviewdb.client.AccountGroup.NameKey;
+import com.google.gerrit.reviewdb.client.RefNames;
+import com.google.gerrit.server.group.InternalGroup;
+import com.google.gerrit.server.notedb.GroupsMigration;
+import com.google.gwtorm.server.OrmException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import javax.inject.Inject;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.notes.Note;
+import org.eclipse.jgit.notes.NoteMap;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+/** Check the referential integrity of NoteDb group storage */
+public class GroupsNoteDbConsistencyChecker {
+  private final GroupsMigration groupsMigration;
+
+  private ImmutableMap<AccountGroup.UUID, InternalGroup> uuidToGroupMap = ImmutableMap.of();
+  private ImmutableBiMap<AccountGroup.UUID, String> uuidNameBiMap = ImmutableBiMap.of();
+
+  @Inject
+  GroupsNoteDbConsistencyChecker(GroupsMigration groupsMigration) {
+    this.groupsMigration = groupsMigration;
+  }
+
+  public ImmutableMap<AccountGroup.UUID, InternalGroup> getUuidToGroupMap() {
+    return uuidToGroupMap;
+  }
+
+  public List<ConsistencyProblemInfo> check(Repository repo) throws OrmException, IOException {
+    List<ConsistencyProblemInfo> problems = new ArrayList<>();
+    if (!groupsMigration.writeToNoteDb()) {
+      return problems;
+    }
+
+    // Get all refs in an attempt to avoid seeing half committed group updates.
+    Map<String, Ref> refs = repo.getAllRefs();
+    problems.addAll(readGroups(repo, refs));
+    problems.addAll(readGroupNames(repo, refs));
+
+    // The sequential IDs are not keys in NoteDb, so no need to check them.
+
+    if (problems.isEmpty()) {
+      // Continue checking if we could read data without problems.
+      problems.addAll(checkGlobalConsistency());
+    }
+    return problems;
+  }
+
+  private List<ConsistencyProblemInfo> readGroups(Repository repo, Map<String, Ref> refs)
+      throws IOException {
+    List<ConsistencyProblemInfo> problems = new ArrayList<>();
+    ImmutableBiMap.Builder<AccountGroup.UUID, InternalGroup> builder = ImmutableBiMap.builder();
+
+    for (Map.Entry<String, Ref> entry : refs.entrySet()) {
+      if (!entry.getKey().startsWith(RefNames.REFS_GROUPS)) {
+        continue;
+      }
+
+      AccountGroup.UUID uuid = AccountGroup.UUID.fromRef(entry.getKey());
+      if (uuid == null) {
+        problems.add(error("null UUID from %s", entry.getKey()));
+        continue;
+      }
+      try {
+        GroupConfig cfg =
+            GroupConfig.loadForGroupSnapshot(repo, uuid, entry.getValue().getObjectId());
+        builder.put(uuid, cfg.getLoadedGroup().get());
+      } catch (ConfigInvalidException e) {
+        problems.add(error("group %s does not parse: %s", uuid, e.getMessage()));
+      }
+    }
+
+    uuidToGroupMap = builder.build();
+    return problems;
+  }
+
+  private List<ConsistencyProblemInfo> readGroupNames(Repository repo, Map<String, Ref> refs)
+      throws IOException {
+    Ref ref = refs.get(RefNames.REFS_GROUPNAMES);
+    if (ref == null) {
+      String msg = String.format("ref %s does not exist", RefNames.REFS_GROUPNAMES);
+      return Arrays.asList(groupsMigration.readFromNoteDb() ? error(msg) : warning(msg));
+    }
+
+    List<ConsistencyProblemInfo> problems = new ArrayList<>();
+    ImmutableBiMap.Builder<AccountGroup.UUID, String> builder = ImmutableBiMap.builder();
+
+    try (RevWalk rw = new RevWalk(repo)) {
+      NoteMap nm = NoteMap.read(rw.getObjectReader(), rw.parseCommit(ref.getObjectId()));
+
+      for (Note note : nm) {
+        ObjectLoader ld = rw.getObjectReader().open(note.getData());
+        byte[] data = ld.getCachedBytes();
+
+        GroupReference gRef;
+        try {
+          gRef = GroupNameNotes.getFromNoteData(data);
+        } catch (ConfigInvalidException e) {
+          problems.add(
+              error(
+                  "notename entry %s: %s does not parse: %s",
+                  note, new String(data, StandardCharsets.UTF_8), e.getMessage()));
+          continue;
+        }
+
+        ObjectId nameKey = GroupNameNotes.getNoteKey(new NameKey(gRef.getName()));
+        if (!Objects.equals(nameKey, note)) {
+          problems.add(error("notename entry %s does not match name %s", note, gRef.getName()));
+        }
+
+        // We trust SHA1 to have no collisions, so no need to check uniqueness of name.
+        builder.put(gRef.getUUID(), gRef.getName());
+      }
+    }
+
+    uuidNameBiMap = builder.build();
+    return problems;
+  }
+
+  /** Check invariants of the group refs with the groupname refs. */
+  private List<ConsistencyProblemInfo> checkGlobalConsistency() {
+    List<ConsistencyProblemInfo> problems = new ArrayList<>();
+
+    // Check consistency between the data coming from different refs.
+    for (AccountGroup.UUID uuid : uuidToGroupMap.keySet()) {
+      if (!uuidNameBiMap.containsKey(uuid)) {
+        problems.add(error("group %s has no entry in name map", uuid));
+        continue;
+      }
+
+      String noteName = uuidNameBiMap.get(uuid);
+      String groupRefName = uuidToGroupMap.get(uuid).getName();
+      if (!Objects.equals(noteName, groupRefName)) {
+        problems.add(
+            error(
+                "inconsistent name for group %s (name map %s vs. group ref %s)",
+                uuid, noteName, groupRefName));
+      }
+    }
+
+    for (AccountGroup.UUID uuid : uuidNameBiMap.keySet()) {
+      if (!uuidToGroupMap.containsKey(uuid)) {
+        problems.add(
+            error(
+                "name map has entry (%s, %s), entry missing as group ref",
+                uuid, uuidNameBiMap.get(uuid)));
+      }
+    }
+
+    if (problems.isEmpty()) {
+      // Check ids.
+      Map<AccountGroup.Id, InternalGroup> groupById = new HashMap<>();
+      for (InternalGroup g : uuidToGroupMap.values()) {
+        InternalGroup before = groupById.get(g.getId());
+        if (before != null) {
+          problems.add(
+              error(
+                  "shared group id %s for %s (%s) and %s (%s)",
+                  g.getId(),
+                  before.getName(),
+                  before.getGroupUUID(),
+                  g.getName(),
+                  g.getGroupUUID()));
+        }
+        groupById.put(g.getId(), g);
+      }
+    }
+
+    return problems;
+  }
+}

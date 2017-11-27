c7fe48e352e12972f34de12e1bd9e528ebce99ce
Dave Borowitz <dborowitz@google.com>
2017-11-27 08:01:44 -0500

    Add REST endpoint to rebuild individual groups in NoteDb
    Change-Id: I84201c0c9d27bfce6a3953bde1e291795f5c5b72

--- javatests/com/google/gerrit/acceptance/rest/group/GroupsIT.java
+++ javatests/com/google/gerrit/acceptance/rest/group/GroupsIT.java
@@ -15,17 +15,68 @@
 package com.google.gerrit.acceptance.rest.group;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.TruthJUnit.assume;
 
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.extensions.common.GroupInfo;
+import com.google.gerrit.reviewdb.client.AccountGroup;
+import com.google.gerrit.reviewdb.client.RefNames;
+import com.google.gerrit.server.group.db.GroupBundle;
+import com.google.gerrit.server.notedb.GroupsMigration;
+import com.google.inject.Inject;
+import org.eclipse.jgit.junit.TestRepository;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
 import org.junit.Test;
 
 public class GroupsIT extends AbstractDaemonTest {
+  @Inject private GroupsMigration groupsMigration;
+  @Inject private GroupBundle.Factory bundleFactory;
+
   @Test
   public void invalidQueryOptions() throws Exception {
     RestResponse r = adminRestSession.put("/groups/?query=foo&query2=bar");
     r.assertBadRequest();
     assertThat(r.getEntityContent())
         .isEqualTo("\"query\" and \"query2\" options are mutually exclusive");
+  }
+
+  @Test
+  public void rebuild() throws Exception {
+    assume().that(groupsMigration.writeToNoteDb()).isTrue();
+    assume().that(groupsMigration.readFromNoteDb()).isFalse();
+
+    GroupInfo g = gApi.groups().create(name("group")).get();
+    AccountGroup.UUID uuid = new AccountGroup.UUID(g.id);
+    String refName = RefNames.refsGroups(uuid);
+    ObjectId oldId;
+    GroupBundle oldBundle;
+    try (Repository repo = repoManager.openRepository(allUsers)) {
+      oldId = repo.exactRef(refName).getObjectId();
+      oldBundle = bundleFactory.fromNoteDb(repo, uuid);
+      new TestRepository<>(repo).delete(refName);
+    }
+
+    assertThat(adminRestSession.postOK("/groups/" + uuid + "/rebuild", "").getEntityContent())
+        .isEqualTo("No differences between ReviewDb and NoteDb");
+
+    try (Repository repo = repoManager.openRepository(allUsers)) {
+      Ref ref = repo.exactRef(refName);
+      assertThat(ref).isNotNull();
+
+      // An artifact of the migration process makes the SHA-1 different, but it's actually ok
+      // because the bundles are equal.
+      assertThat(ref.getObjectId()).isNotEqualTo(oldId);
+
+      // Comparing NoteDb to NoteDb, so compare fields instead of using static compare method.
+      GroupBundle newBundle = bundleFactory.fromNoteDb(repo, uuid);
+      assertThat(newBundle.group()).isEqualTo(oldBundle.group());
+      assertThat(newBundle.members()).isEqualTo(oldBundle.members());
+      assertThat(newBundle.memberAudit()).isEqualTo(oldBundle.memberAudit());
+      assertThat(newBundle.byId()).isEqualTo(oldBundle.byId());
+      assertThat(newBundle.byIdAudit()).isEqualTo(oldBundle.byIdAudit());
+    }
   }
 }

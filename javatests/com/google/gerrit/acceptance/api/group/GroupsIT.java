147453fdfdd9d28925391c98f4433038b367722a
Edwin Kempin <ekempin@google.com>
2017-11-28 13:44:20 +0100

    Remove @Sandboxed from more tests
    Running a test in a sandbox adds about 1s to the test runtime when the
    test is executed from Eclipse. Saving this time justifies a little
    effort to avoid running tests in sandboxes.
    
    Some tests were annotated with @Sandboxed because they set a global
    flag. Remove @Sandboxed from these tests and instead use an AutoClosable
    to unset the flag at the end of the test.
    
    Change-Id: Idc77a18dd67a20de2d5ace5cd172b18160bc3724
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- javatests/com/google/gerrit/acceptance/api/group/GroupsIT.java
+++ javatests/com/google/gerrit/acceptance/api/group/GroupsIT.java
@@ -1144,8 +1144,7 @@ public class GroupsIT extends AbstractDaemonTest {
   @Sandboxed
   public void blockReviewDbUpdatesOnGroupCreation() throws Exception {
     assume().that(groupsInNoteDb()).isFalse();
-    cfg.setBoolean("user", null, "blockReviewDbGroupUpdates", true);
-    try {
+    try (AutoCloseable ctx = createBlockReviewDbGroupUpdatesContext()) {
       gApi.groups().create(name("foo"));
       fail("Expected RestApiException: Updates to groups in ReviewDb are blocked");
     } catch (RestApiException e) {
@@ -1159,8 +1158,7 @@ public class GroupsIT extends AbstractDaemonTest {
     assume().that(groupsInNoteDb()).isFalse();
     String group1 = gApi.groups().create(name("foo")).get().id;
     String group2 = gApi.groups().create(name("bar")).get().id;
-    cfg.setBoolean("user", null, "blockReviewDbGroupUpdates", true);
-    try {
+    try (AutoCloseable ctx = createBlockReviewDbGroupUpdatesContext()) {
       gApi.groups().id(group1).addGroups(group2);
       fail("Expected RestApiException: Updates to groups in ReviewDb are blocked");
     } catch (RestApiException e) {
@@ -1365,6 +1363,16 @@ public class GroupsIT extends AbstractDaemonTest {
 
   private boolean readGroupsFromNoteDb() {
     return groupsInNoteDb() && cfg.getBoolean(SECTION_NOTE_DB, GROUPS.key(), READ, false);
+  }
+
+  private AutoCloseable createBlockReviewDbGroupUpdatesContext() {
+    cfg.setBoolean("user", null, "blockReviewDbGroupUpdates", true);
+    return new AutoCloseable() {
+      @Override
+      public void close() {
+        cfg.setBoolean("user", null, "blockReviewDbGroupUpdates", false);
+      }
+    };
   }
 
   @Target({METHOD})

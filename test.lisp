b1f288844269a0921f5095bd7042a68bf74957ad
Luís Oliveira <luismbo@gmail.com>
2017-12-04 22:19:09 +0000

    WIP: example change for issue 7164
    https://bugs.chromium.org/p/gerrit/issues/detail?id=7164#c6
    
    Change-Id: I02d93d929e75633568e809a3e174be79ccd014fb

---
+++ test.lisp
@@ -0,0 +1,15 @@
+;;; -*- coding: latin-1-dos -*-
+;;;
+;;; A text file (sometimes spelled "textfile"; an old alternative name is
+;;; "flatfile") is a kind of computer file that is structured as a
+;;; sequence of lines of electronic text. A text file exists stored as
+;;; data within a computer file system. The end of a text file is often
+;;; denoted by placing one or more special characters, known as an
+;;; end-of-file marker, after the last line in a text file. Such markers
+;;; were required under the CP/M and MS-DOS operating systems. On modern
+;;; operating systems such as Windows and Unix-like systems, text files do
+;;; not contain any special EOF character.
+;;;
+
+(defun make-text-file (name)
+  (frobnicate-text))

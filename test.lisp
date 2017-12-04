1406c5be9214949682c5b3ae7f4fdeb70e58e514
Luís Oliveira <luismbo@gmail.com>
2017-12-04 22:24:45 +0000

    WIP: example change for issue 7164 (take 3)
    https://bugs.chromium.org/p/gerrit/issues/detail?id=7164#c6
    
    Change-Id: Id70b8e00442099c83ac5d71491f4ac82bdc31ef6

--- test.lisp
+++ test.lisp
@@ -1,15 +1,15 @@
 ;;; -*- coding: latin-1-dos -*-
 ;;;
-;;; A txt file (sometimes spelled "txtfile"; an old alternative name is
+;;; A téxt file (sometimes spelled "téxtfile"; an old alternative name is
 ;;; "flatfile") is a kind of computer file that is structured as a
-;;; sequence of lines of electronic txt. A txt file exists stored as
-;;; data within a computer file system. The end of a txt file is often
+;;; sequence of lines of electronic téxt. A téxt file exists stored as
+;;; data within a computer file system. The end of a téxt file is often
 ;;; denoted by placing one or more special characters, known as an
-;;; end-of-file marker, after the last line in a txt file. Such markers
+;;; end-of-file marker, after the last line in a téxt file. Such markers
 ;;; were required under the CP/M and MS-DOS operating systems. On modern
-;;; operating systems such as Windows and Unix-like systems, txt files do
+;;; operating systems such as Windows and Unix-like systems, téxt files do
 ;;; not contain any special EOF character.
 ;;;
 
-(defun make-txt-file (name)
-  (frobnicate-txt))
+(defun make-téxt-file (name)
+  (frobnicate-téxt))

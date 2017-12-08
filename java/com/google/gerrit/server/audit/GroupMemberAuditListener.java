a8ea8492394a7c50cc3c296c96da8c1d5307fee5
Changcheng Xiao <xchangcheng@google.com>
2017-12-08 14:31:12 +0100

    Remove ReviewDb types from GroupMemberAuditListener interface
    We are migrating groups to NoteDb. Soon we will try to get rid of
    those ReviewDb types. This change removes AccountGroupMember and
    AccountGroupById from the GroupMemberAuditListener interface.
    
    This change doesn't use new types to replace those ReviewDb types.
    But I'm open for better solutions.
    
    After this, we will support GroupMemberAuditListener for NoteDb.
    
    Change-Id: Ie9ad8ec3af0fb298556de677dcfcc0bf9f479a6c

--- java/com/google/gerrit/server/audit/GroupMemberAuditListener.java
+++ java/com/google/gerrit/server/audit/GroupMemberAuditListener.java
@@ -16,22 +16,66 @@ package com.google.gerrit.server.audit;
 
 import com.google.gerrit.extensions.annotations.ExtensionPoint;
 import com.google.gerrit.reviewdb.client.Account;
-import com.google.gerrit.reviewdb.client.AccountGroupById;
-import com.google.gerrit.reviewdb.client.AccountGroupMember;
+import com.google.gerrit.reviewdb.client.AccountGroup;
 import java.sql.Timestamp;
 import java.util.Collection;
 
 @ExtensionPoint
 public interface GroupMemberAuditListener {
 
+  /**
+   * Triggered when accounts are added to a group.
+   *
+   * @param actor the {@link Account.Id} of the user who adds these accounts to the group.
+   * @param addedAccounts the {@link Account.Id} collection added to the group.
+   * @param groupId the {@link AccountGroup.Id} of the group these accounts are added to.
+   * @param addedOn the {@link Timestamp} of the add event.
+   */
   void onAddAccountsToGroup(
-      Account.Id actor, Collection<AccountGroupMember> added, Timestamp addedOn);
+      Account.Id actor,
+      Collection<Account.Id> addedAccounts,
+      AccountGroup.Id groupId,
+      Timestamp addedOn);
 
+  /**
+   * Triggered when accounts are deleted from a group.
+   *
+   * @param actor the {@link Account.Id} of the user who deletes these accounts from the group.
+   * @param deletedAccounts the {@link Account.Id} collection deleted from the group.
+   * @param groupId the {@link AccountGroup.Id} of the group these accounts are deleted from.
+   * @param deletedOn the {@link Timestamp} of the delete event.
+   */
   void onDeleteAccountsFromGroup(
-      Account.Id actor, Collection<AccountGroupMember> removed, Timestamp removedOn);
+      Account.Id actor,
+      Collection<Account.Id> deletedAccounts,
+      AccountGroup.Id groupId,
+      Timestamp deletedOn);
 
-  void onAddGroupsToGroup(Account.Id actor, Collection<AccountGroupById> added, Timestamp addedOn);
+  /**
+   * Triggered when subgroups are added to a group.
+   *
+   * @param actor the {@link Account.Id} of the user who adds these subgroups to the group.
+   * @param addedSubgroups the {@link AccountGroup.UUID} collection added to the group.
+   * @param parentGroupId the {@link AccountGroup.Id} of the group these subgroups are added to.
+   * @param addedOn the {@link Timestamp} of the add event.
+   */
+  void onAddGroupsToGroup(
+      Account.Id actor,
+      Collection<AccountGroup.UUID> addedSubgroups,
+      AccountGroup.Id parentGroupId,
+      Timestamp addedOn);
 
+  /**
+   * Triggered when subgroups are deleted from a group.
+   *
+   * @param actor the {@link Account.Id} of the user who deletes these subgroups from the group.
+   * @param deletedSubgroups the {@link AccountGroup.UUID} collection deleted from the group.
+   * @param parentGroupId the {@link AccountGroup.Id} of the group these subgroups are deleted from.
+   * @param deletedOn the {@link Timestamp} of the delete event.
+   */
   void onDeleteGroupsFromGroup(
-      Account.Id actor, Collection<AccountGroupById> deleted, Timestamp removedOn);
+      Account.Id actor,
+      Collection<AccountGroup.UUID> deletedSubgroups,
+      AccountGroup.Id parentGroupId,
+      Timestamp deletedOn);
 }

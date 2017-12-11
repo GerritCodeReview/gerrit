2594e98693ffab24d3d0be8440647b2331d25d07
Becky Siegel <beckysiegel@google.com>
2017-12-11 15:58:57 -0800

    Allow saving access for review
    Note: The only things that can be modified at this time are existing
    access rules.
    
    Bug: Issue 6569
    Change-Id: I16d21d71a6c832dd5638877c7f07e572e6ce343f

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -1138,6 +1138,12 @@
           projectInfo);
     },
 
+    setProjectAccessRightsForReview(projectName, projectInfo) {
+      return this.send(
+          'PUT', `/projects/${encodeURIComponent(projectName)}/access:review`,
+          projectInfo);
+    },
+
     /**
      * @param {string} inputVal
      * @param {number} opt_n

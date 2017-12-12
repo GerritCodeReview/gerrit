bb7ae9653e9e6e911d2e53d313f54d477b4f17ea
Kasper Nilsson <kaspern@google.com>
2017-12-12 15:05:22 -0800

    Fetch more info about edit files
    The type of a file in a change edit is identified in a header on the
    getFileInChangeEdit response. This change modifies gr-rest-api to return
    the bare response, and upgrades the editor view to parse both the header
    identifying the file type and the response body containing the file
    content.
    
    Bug: Issue 4437
    Change-Id: Icdadeafb4189692cd369c74fe026442787173ef8

--- polygerrit-ui/app/elements/edit/gr-editor-view/gr-editor-view.js
+++ polygerrit-ui/app/elements/edit/gr-editor-view/gr-editor-view.js
@@ -36,6 +36,7 @@
       _changeEditDetail: Object,
       _changeNum: String,
       _path: String,
+      _type: String,
       _content: String,
       _newContent: String,
       _saveDisabled: {
@@ -81,11 +82,7 @@
       const promises = [];
 
       promises.push(this._getChangeDetail(this._changeNum));
-      promises.push(this._getFileContent(this._changeNum, this._path)
-          .then(fileContent => {
-            this._content = fileContent;
-            this._newContent = fileContent;
-          }));
+      promises.push(this._getFileData(this._changeNum, this._path));
       return Promise.all(promises);
     },
 
@@ -109,8 +106,14 @@
       Gerrit.Nav.navigateToChange(this._change, this.EDIT_NAME);
     },
 
-    _getFileContent(changeNum, path) {
-      return this.$.restAPI.getFileInChangeEdit(changeNum, path);
+    _getFileData(changeNum, path) {
+      return this.$.restAPI.getFileInChangeEdit(changeNum, path).then(res => {
+        if (!res.ok) { return; }
+
+        this._type = res.type;
+        this._newContent = res.content;
+        this._content = res.content;
+      });
     },
 
     _saveEdit() {

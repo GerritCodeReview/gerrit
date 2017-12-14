17250114dcce393995165c3be0dbdd82812cca28
Jacek Centkowski <jcentkowski@collab.net>
2017-12-14 12:49:20 +0100

    Fix schema_154 migration from version 2.13 and older
    Issue:
    When migration is performed from 2.13 (and older) column status is not
    available and due to migration limitation (described in [1]) process
    fails.
    
    Solution:
    Dynamically determine available columns and migrate only available data
    (status is by default null anyway).
    
    [1] https://groups.google.com/forum/#!msg/repo-discuss/9gMYlnCm0k8/dxfKLxkkCgAJ
    
    Change-Id: Ie2b2f1b84f37ff07904b8af5101583f37d2408fc
    Signed-off-by: Jacek Centkowski <jcentkowski@collab.net>

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_154.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_154.java
@@ -14,6 +14,9 @@
 
 package com.google.gerrit.server.schema;
 
+import static java.util.stream.Collectors.toMap;
+
+import com.google.common.collect.ImmutableMap;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.GerritPersonIdent;
@@ -22,23 +25,38 @@ import com.google.gerrit.server.config.AllUsersName;
 import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.MetaDataUpdate;
+import com.google.gwtorm.jdbc.JdbcSchema;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.ProgressMonitor;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.TextProgressMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Migrate accounts to NoteDb. */
 public class Schema_154 extends SchemaVersion {
+  private static final Logger log = LoggerFactory.getLogger(Schema_154.class);
+  private static final String TABLE = "accounts";
+  private static final Map<String, AccountSetter> ACCOUNT_FIELDS_MAP =
+      ImmutableMap.<String, AccountSetter>builder()
+          .put("full_name", (a, rs, field) -> a.setFullName(rs.getString(field)))
+          .put("preferred_email", (a, rs, field) -> a.setPreferredEmail(rs.getString(field)))
+          .put("status", (a, rs, field) -> a.setStatus(rs.getString(field)))
+          .put("inactive", (a, rs, field) -> a.setActive(rs.getString(field).equals("N")))
+          .build();
+
   private final GitRepositoryManager repoManager;
   private final AllUsersName allUsersName;
   private final Provider<PersonIdent> serverIdent;
@@ -76,28 +94,42 @@ public class Schema_154 extends SchemaVersion {
   }
 
   private Set<Account> scanAccounts(ReviewDb db, ProgressMonitor pm) throws SQLException {
+    Map<String, AccountSetter> fields = getFields(db);
+    String fieldsDelimiter = ", ";
+    if (fields.isEmpty()) {
+      fieldsDelimiter = "";
+      log.warn("Only account_id and registered_on fields are migrated for accounts");
+    }
+
+    String query =
+        "SELECT account_id, registered_on"
+            + fieldsDelimiter
+            + String.join(", ", fields.keySet())
+            + String.format(" FROM %s", TABLE);
     try (Statement stmt = newStatement(db);
-        ResultSet rs =
-            stmt.executeQuery(
-                "SELECT account_id,"
-                    + " registered_on,"
-                    + " full_name, "
-                    + " preferred_email,"
-                    + " status,"
-                    + " inactive"
-                    + " FROM accounts")) {
+        ResultSet rs = stmt.executeQuery(query)) {
       Set<Account> s = new HashSet<>();
       while (rs.next()) {
         Account a = new Account(new Account.Id(rs.getInt(1)), rs.getTimestamp(2));
-        a.setFullName(rs.getString(3));
-        a.setPreferredEmail(rs.getString(4));
-        a.setStatus(rs.getString(5));
-        a.setActive(rs.getString(6).equals("N"));
+        for (Map.Entry<String, AccountSetter> field : fields.entrySet()) {
+          field.getValue().set(a, rs, field.getKey());
+        }
         s.add(a);
         pm.update(1);
       }
       return s;
     }
+  }
+
+  private Map<String, AccountSetter> getFields(ReviewDb db) throws SQLException {
+    JdbcSchema schema = (JdbcSchema) db;
+    Connection connection = schema.getConnection();
+    Set<String> columns = schema.getDialect().listColumns(connection, TABLE);
+    return ACCOUNT_FIELDS_MAP
+        .entrySet()
+        .stream()
+        .filter(e -> columns.contains(e.getKey()))
+        .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
   }
 
   private void updateAccountInNoteDb(Repository allUsersRepo, Account account)
@@ -111,5 +143,10 @@ public class Schema_154 extends SchemaVersion {
     accountConfig.load(allUsersRepo);
     accountConfig.setAccount(account);
     accountConfig.commit(md);
+  }
+
+  @FunctionalInterface
+  private interface AccountSetter {
+    void set(Account a, ResultSet rs, String field) throws SQLException;
   }
 }

69dbc5f244499b8c502a04af1ad077edcc1348cb
Wyatt Allen <wyatta@google.com>
2017-12-18 17:18:31 -0800

    Allow replies on patches known not to be latest
    The reply dialog checks whether there are newer patches than the
    currently known latest patch so that users can avoid voting on
    non-latest patches, which would typically be a no-op and have no effect.
    Formerly, if the patch was known to be non-latest, the [Send] button
    would be disabled completely and a red warning with a [Reload] button
    shown.
    
    With this change, the [Send] button is no longer disabled, and replies
    to non-latest patches are allowed, but the warning still appears as an
    advisory message, and additional wording appears when users attempt to
    vote.
    
    Bug: Issue 6612
    Change-Id: Ib8f117dcc96b3fcba255e05a3cc72abb5fc1b14d

--- polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.js
+++ polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.js
@@ -406,12 +406,6 @@
     },
 
     send(includeComments, startReview) {
-      if (this.knownLatestState === 'not-latest') {
-        this.fire('show-alert',
-            {message: 'Cannot reply to non-latest patch.'});
-        return Promise.resolve({});
-      }
-
       const labels = this.$.labelScores.getLabelValues();
 
       const obj = {
@@ -835,16 +829,21 @@
       return savingComments ? 'saving' : '';
     },
 
-    _computeSendButtonDisabled(knownLatestState, buttonLabel, drafts, text,
-        reviewersMutated, labelsChanged, includeComments) {
-      if (this._isState(knownLatestState, LatestPatchState.NOT_LATEST)) {
-        return true;
-      }
+    _computeSendButtonDisabled(buttonLabel, drafts, text, reviewersMutated,
+        labelsChanged, includeComments) {
       if (buttonLabel === ButtonLabels.START_REVIEW) {
         return false;
       }
       const hasDrafts = includeComments && Object.keys(drafts).length;
       return !hasDrafts && !text.length && !reviewersMutated && !labelsChanged;
+    },
+
+    _computePatchSetWarning(patchNum, labelsChanged) {
+      let str = `Patch ${patchNum} is not latest.`;
+      if (labelsChanged) {
+        str += ' Voting on a non-latest patch will have no effect.';
+      }
+      return str;
     },
   });
 })();

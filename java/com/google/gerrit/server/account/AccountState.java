06e676fc95c1ed22bc00ccb8e0a2919b09b7a7ea
Edwin Kempin <ekempin@google.com>
2018-01-10 10:36:37 +0100

    Do not read external IDs to be deleted outside of account update transaction
    If the external IDs that should be deleted are read outside of the
    account update transaction it is possible that a race updates the
    external IDs after they have been read and before the account update is
    done. AccountUpdater already provides an AccountState that was
    consistently read and from which the external IDs of the account can be
    accessed. Get the external IDs from this AccountState instance instead
    of reading them outside of the transaction.
    
    Change-Id: I5d14840b1b7418831c789152ec37636241323a39
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/account/AccountState.java
+++ java/com/google/gerrit/server/account/AccountState.java
@@ -14,6 +14,7 @@
 
 package com.google.gerrit.server.account;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static com.google.gerrit.server.account.externalids.ExternalId.SCHEME_USERNAME;
 
 import com.google.common.base.Function;
@@ -135,6 +136,11 @@ public class AccountState {
   /** The external identities that identify the account holder. */
   public ImmutableSet<ExternalId> getExternalIds() {
     return externalIds;
+  }
+
+  /** The external identities that identify the account holder that match the given scheme. */
+  public ImmutableSet<ExternalId> getExternalIds(String scheme) {
+    return externalIds.stream().filter(e -> e.key().isScheme(scheme)).collect(toImmutableSet());
   }
 
   /** The project watches of the account. */

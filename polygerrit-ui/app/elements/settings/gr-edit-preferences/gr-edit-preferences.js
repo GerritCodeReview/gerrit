304e6f2bd36aea3f11dd6241850d99e312b336dd
Paladox none <thomasmulhall410@yahoo.com>
2018-01-10 22:16:50 +0000

    Add EditPreference component to gr-editor-view
    This allows users to change there edit preference when editing a change
    without having to go to settings to change it.
    
    Change-Id: I7af87c50657cd928403972a4b203671f6c0df586

--- polygerrit-ui/app/elements/settings/gr-edit-preferences/gr-edit-preferences.js
+++ polygerrit-ui/app/elements/settings/gr-edit-preferences/gr-edit-preferences.js
@@ -74,9 +74,10 @@
     },
 
     save() {
-      return this.$.restAPI.saveEditPreferences(this.editPrefs).then(res => {
-        this.hasUnsavedChanges = false;
-      });
+      return this.$.restAPI.saveEditPreferences(this.editPrefs)
+          .then(() => {
+            this.hasUnsavedChanges = false;
+          });
     },
   });
 })();

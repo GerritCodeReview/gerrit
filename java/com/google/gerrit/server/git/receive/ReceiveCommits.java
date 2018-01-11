93ee94bc766137903953821d35ebae1d3ffa2678
Martin Wallgren <martinwa@axis.com>
2018-01-11 15:38:11 +0100

    Compare incoming change-id target change
    Incoming commits should not have a change-id in the message footers that
    differs from the change-id of the target change.
    
    This change will add the target change (if any, else null) to the
    ChangeIdValidator, and check that the change-id footer is the same as
    the key of the target change.
    
    Change-Id: I92cdd3af04ac6e6548630483041c342509bb1224

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -1842,7 +1842,8 @@ class ReceiveCommits {
           logDebug("Creating new change for {} even though it is already tracked", name);
         }
 
-        if (!validCommit(rp.getRevWalk(), magicBranch.perm, magicBranch.dest, magicBranch.cmd, c)) {
+        if (!validCommit(
+            rp.getRevWalk(), magicBranch.perm, magicBranch.dest, magicBranch.cmd, c, null)) {
           // Not a change the user can propose? Abort as early as possible.
           newChanges = Collections.emptyList();
           logDebug("Aborting early due to invalid commit");
@@ -2416,7 +2417,7 @@ class ReceiveCommits {
       }
 
       PermissionBackend.ForRef perm = permissions.ref(change.getDest().get());
-      if (!validCommit(rp.getRevWalk(), perm, change.getDest(), inputCommand, newCommit)) {
+      if (!validCommit(rp.getRevWalk(), perm, change.getDest(), inputCommand, newCommit, change)) {
         return false;
       }
       rp.getRevWalk().parseBody(priorCommit);
@@ -2780,7 +2781,7 @@ class ReceiveCommits {
         }
         if (existing.keySet().contains(c)) {
           continue;
-        } else if (!validCommit(walk, perm, branch, cmd, c)) {
+        } else if (!validCommit(walk, perm, branch, cmd, c, null)) {
           break;
         }
 
@@ -2802,7 +2803,8 @@ class ReceiveCommits {
       PermissionBackend.ForRef perm,
       Branch.NameKey branch,
       ReceiveCommand cmd,
-      ObjectId id)
+      ObjectId id,
+      @Nullable Change change)
       throws IOException {
 
     if (validCommits.contains(id)) {
@@ -2822,7 +2824,7 @@ class ReceiveCommits {
           isMerged
               ? commitValidatorsFactory.forMergedCommits(perm, user.asIdentifiedUser())
               : commitValidatorsFactory.forReceiveCommits(
-                  perm, branch, user.asIdentifiedUser(), sshInfo, repo, rw);
+                  perm, branch, user.asIdentifiedUser(), sshInfo, repo, rw, change);
       messages.addAll(validators.validate(receiveEvent));
     } catch (CommitValidationException e) {
       logDebug("Commit validation failed on {}", c.name());

3b452cada950b52bd384504c62cbf261feb38bf5
Edwin Kempin <ekempin@google.com>
2018-01-15 10:26:07 +0100

    Double the retry timeout for index queries on auto-close of changes
    Accessing the secondary index may sometimes fail due to a temporary
    issue with the index. This is why in this case we retry scanning for
    changes to auto-close.
    
    This is especially important because the push succeeds even if
    auto-closing changes fails. If this happens the changes are in an
    inconsistent state, they are still open but the changes were actually
    already merged into the target branch. Since the push succeeded the user
    cannot retry the operation to get the auto-closing done.
    
    Temporary issues with accessing the index are not common with Lucene but
    at Google where we have a custom index implementation we have such
    short-term failures frequently. At Google we still have issues with
    failing change queries during auto-close although the failing change
    queries are retried. Doubling the retry timeout allows more retries
    and hopefully solves this issue for us.
    
    Change-Id: Id875f99a43c2d21c6ca045464ea3062cefa9174c
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -2954,7 +2954,11 @@ class ReceiveCommits {
 
   private <T> T executeIndexQuery(Action<T> action) throws OrmException {
     try {
-      return retryHelper.execute(ActionType.INDEX_QUERY, action, t -> t instanceof OrmException);
+      return retryHelper.execute(
+          ActionType.INDEX_QUERY,
+          action,
+          retryOptionsForIndexQuery(),
+          t -> t instanceof OrmException);
     } catch (Throwable t) {
       Throwables.throwIfUnchecked(t);
       Throwables.throwIfInstanceOf(t, OrmException.class);
@@ -2967,13 +2971,22 @@ class ReceiveCommits {
     try {
       // The request validation needs to do an account query to lookup accounts by preferred email,
       // if that index query fails the request validation should be retried.
-      return retryHelper.execute(ActionType.INDEX_QUERY, action, t -> t instanceof OrmException);
+      return retryHelper.execute(
+          ActionType.INDEX_QUERY,
+          action,
+          retryOptionsForIndexQuery(),
+          t -> t instanceof OrmException);
     } catch (Exception t) {
       Throwables.throwIfInstanceOf(t, IOException.class);
       Throwables.throwIfInstanceOf(t, PermissionBackendException.class);
       Throwables.throwIfInstanceOf(t, OrmException.class);
       throw new OrmException(t);
     }
+  }
+
+  private RetryHelper.Options retryOptionsForIndexQuery() {
+    // Double the timeout to allow more retries for failing index queries.
+    return RetryHelper.options().timeout(retryHelper.getDefaultTimeout().multipliedBy(2)).build();
   }
 
   private void updateAccountInfo() {

7f900af49a873fd5931cb2645e5af0c37a5f9bd0
Martin Wallgren <martinwa@axis.com>
2018-01-17 08:49:38 +0100

    Deprecate pushing to refs/changes
    This commit prevents pushing to refs/changes. It is still possible to
    allow push to refs/changes with the config flag
    receive.allowPushToRefsChanges
    
    Change-Id: I0f45589a1a1a9fee93f1718a13d01c414b3f67a8

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -98,6 +98,7 @@ import com.google.gerrit.server.account.AccountsUpdate;
 import com.google.gerrit.server.change.ChangeInserter;
 import com.google.gerrit.server.change.SetHashtagsOp;
 import com.google.gerrit.server.config.AllProjectsName;
+import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.ProjectConfigEntry;
 import com.google.gerrit.server.edit.ChangeEdit;
@@ -186,6 +187,7 @@ import java.util.regex.Matcher;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
@@ -347,6 +349,7 @@ class ReceiveCommits {
   private final ReceivePack rp;
 
   // Immutable fields derived from constructor arguments.
+  private final boolean allowPushToRefsChanges;
   private final LabelTypes labelTypes;
   private final NoteMap rejectCommits;
   private final PermissionBackend.ForProject permissions;
@@ -395,6 +398,7 @@ class ReceiveCommits {
       AccountsUpdate.Server accountsUpdate,
       AllProjectsName allProjectsName,
       BatchUpdate.Factory batchUpdateFactory,
+      @GerritServerConfig Config cfg,
       ChangeEditUtil editUtil,
       ChangeIndexer indexer,
       ChangeInserter.Factory changeInserterFactory,
@@ -477,6 +481,7 @@ class ReceiveCommits {
     this.rp = rp;
 
     // Immutable fields derived from constructor arguments.
+    allowPushToRefsChanges = cfg.getBoolean("receive", "allowPushToRefsChanges", false);
     repo = rp.getRepository();
     project = projectState.getProject();
     labelTypes = projectState.getLabelTypes();
@@ -857,10 +862,14 @@ class ReceiveCommits {
 
       Matcher m = NEW_PATCHSET_PATTERN.matcher(cmd.getRefName());
       if (m.matches()) {
-        // The referenced change must exist and must still be open.
-        //
-        Change.Id changeId = Change.Id.parse(m.group(1));
-        parseReplaceCommand(cmd, changeId);
+        if (allowPushToRefsChanges) {
+          // The referenced change must exist and must still be open.
+          //
+          Change.Id changeId = Change.Id.parse(m.group(1));
+          parseReplaceCommand(cmd, changeId);
+        } else {
+          reject(cmd, "upload to refs/changes not allowed");
+        }
         continue;
       }
 

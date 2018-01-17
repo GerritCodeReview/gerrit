dbc558f18a105f2b8b48a3856bdff3dc5890d909
Paladox none <thomasmulhall410@yahoo.com>
2018-01-17 19:23:26 +0000

    web-component-tester: Support headless mode
    This adds support for running chrome in headless mode.
    
    Support for headless firefox should work but currently dosen't.
    
    Moreover, i've added browser option handling to wct [1].
    
    [1] https://github.com/Polymer/wct-local/commit/b8388d1a8d3fa907a205dc1763524a13b017e780
    
    Bug: Issue 8023
    Change-Id: I47cd9cd69fc78739219223c74d7b2edf2f97d6c7

--- polygerrit-ui/app/run_test.sh
+++ polygerrit-ui/app/run_test.sh
@@ -20,6 +20,7 @@ bazel test \
       --test_env="WCT_ARGS=${WCT_ARGS}" \
       --test_env="NPM=${npm_bin}" \
       --test_env="DISPLAY=${DISPLAY}" \
+      --test_env="WCT_HEADLESS_MODE=${WCT_HEADLESS_MODE}" \
       "$@" \
       //polygerrit-ui/app:embed_test \
       //polygerrit-ui/app:wct_test

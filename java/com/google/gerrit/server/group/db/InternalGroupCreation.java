4d081aadbc96bb587594125f09c27e7e3dc1b1fb
Alice Kober-Sotzek <aliceks@google.com>
2018-01-17 17:23:20 +0100

    Add Javadoc descriptions for InternalGroupCreation
    Change-Id: Ied76c95dec91bf0c57056a4fedecc913ce99c57b

--- java/com/google/gerrit/server/group/db/InternalGroupCreation.java
+++ java/com/google/gerrit/server/group/db/InternalGroupCreation.java
@@ -17,14 +17,26 @@ package com.google.gerrit.server.group.db;
 import com.google.auto.value.AutoValue;
 import com.google.gerrit.reviewdb.client.AccountGroup;
 
-// TODO(aliceks): Add Javadoc descriptions to this file.
+/**
+ * Definition of all properties necessary for a group creation.
+ *
+ * <p>An instance of {@code InternalGroupCreation} specifies all mandatory properties which are
+ * necessary to create a group. It's kind of the blueprint for a group which should be created.
+ *
+ * <p>Optional properties are specified by an {@link InternalGroupUpdate} which is passed next to an
+ * {@code InternalGroupCreation}. Such properties default to the values mentioned for each of them
+ * in {@link GroupConfigEntry}.
+ */
 @AutoValue
 public abstract class InternalGroupCreation {
 
+  /** Defines the numeric ID the group should have. */
   public abstract AccountGroup.Id getId();
 
+  /** Defines the name the group should have. */
   public abstract AccountGroup.NameKey getNameKey();
 
+  /** Defines the UUID the group should have. */
   public abstract AccountGroup.UUID getGroupUUID();
 
   public static Builder builder() {
@@ -33,10 +45,13 @@ public abstract class InternalGroupCreation {
 
   @AutoValue.Builder
   public abstract static class Builder {
+    /** @see #getId() */
     public abstract InternalGroupCreation.Builder setId(AccountGroup.Id id);
 
+    /** @see #getNameKey() */
     public abstract InternalGroupCreation.Builder setNameKey(AccountGroup.NameKey name);
 
+    /** @see #getGroupUUID() */
     public abstract InternalGroupCreation.Builder setGroupUUID(AccountGroup.UUID groupUuid);
 
     public abstract InternalGroupCreation build();

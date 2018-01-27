6b0067b215994085ac78a9662071ed711efa960b
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-01-26 23:53:06 -0500

    Color the reviewer-background based on the vote.
    If a reviewer in the reviewer-list has a vote, then set the
    background-color for that reviewer accordingly.
    
    BUG: Issue 6872
    Change-Id: Ida16a04c8b62288c1ff43e525e8ea00e2887985d

--- polygerrit-ui/app/elements/shared/gr-account-chip/gr-account-chip.js
+++ polygerrit-ui/app/elements/shared/gr-account-chip/gr-account-chip.js
@@ -43,6 +43,7 @@
         type: Boolean,
         value: false,
       },
+      reviewVote: String,
       showAvatar: {
         type: Boolean,
         reflectToAttribute: true,
@@ -59,8 +60,8 @@
       });
     },
 
-    _getBackgroundClass(transparent) {
-      return transparent ? 'transparentBackground' : '';
+    _getBackgroundClass(transparent, review) {
+      return transparent ? 'transparentBackground' : review;
     },
 
     _handleRemoveTap(e) {

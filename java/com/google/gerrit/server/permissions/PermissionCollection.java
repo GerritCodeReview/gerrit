3506421afecbef1ee4f8a69400faa9a3e626adf4
Han-Wen Nienhuys <hanwen@google.com>
2018-02-01 19:30:30 +0100

    Rewrite permission evaluation
    The previous code is unintelligible, undocumented and probably wrong.
    
    Change-Id: If917e10d3320a45e7a910c4bf15414065cdc5db8

--- java/com/google/gerrit/server/permissions/PermissionCollection.java
+++ java/com/google/gerrit/server/permissions/PermissionCollection.java
@@ -14,14 +14,11 @@
 
 package com.google.gerrit.server.permissions;
 
-import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.gerrit.common.data.PermissionRule.Action.BLOCK;
 import static com.google.gerrit.server.project.RefPattern.isRE;
 
 import com.google.auto.value.AutoValue;
-import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.MultimapBuilder;
 import com.google.gerrit.common.Nullable;
 import com.google.gerrit.common.data.AccessSection;
 import com.google.gerrit.common.data.Permission;
@@ -133,71 +130,114 @@ public class PermissionCollection {
       // project closer to the current one come first.
       sorter.sort(ref, sections);
 
-      Set<SeenRule> seen = new HashSet<>();
-      Set<String> exclusiveGroupPermissions = new HashSet<>();
+      Set<String> permissionNames = new HashSet<>();
+      for (AccessSection s : sections) {
+        for (Permission p : s.getPermissions()) {
+          permissionNames.add(p.getName());
+        }
+      }
+      Map<String, List<PermissionRule>> rulesByPermission = new HashMap<>();
+      Map<String, List<BlockAccessSection>> blockByPermission = new HashMap<>();
 
-      HashMap<String, List<PermissionRule>> permissions = new HashMap<>();
-      HashMap<String, List<PermissionRule>> overridden = new HashMap<>();
-      Map<PermissionRule, ProjectRef> ruleProps = Maps.newIdentityHashMap();
-      ListMultimap<Project.NameKey, String> exclusivePermissionsByProject =
-          MultimapBuilder.hashKeys().arrayListValues().build();
-      for (AccessSection section : sections) {
-        Project.NameKey project = sectionToProject.get(section);
-        for (Permission permission : section.getPermissions()) {
-          boolean exclusivePermissionExists =
-              exclusiveGroupPermissions.contains(permission.getName());
+      for (String pn : permissionNames) {
+        List<BlockAccessSection> blocks = findBlocks(sections, pn);
+        if (!blocks.isEmpty()) {
+          blockByPermission.put(pn, blocks);
+        }
 
-          for (PermissionRule rule : permission.getRules()) {
-            SeenRule s = SeenRule.create(section, permission, rule);
-            boolean addRule;
-            if (rule.isBlock()) {
-              addRule = !exclusivePermissionsByProject.containsEntry(project, permission.getName());
-            } else {
-              addRule = seen.add(s) && !rule.isDeny() && !exclusivePermissionExists;
-            }
-
-            HashMap<String, List<PermissionRule>> p = null;
-            if (addRule) {
-              p = permissions;
-            } else if (!rule.isDeny() && !exclusivePermissionExists) {
-              p = overridden;
-            }
-
-            if (p != null) {
-              List<PermissionRule> r = p.get(permission.getName());
-              if (r == null) {
-                r = new ArrayList<>(2);
-                p.put(permission.getName(), r);
-              }
-              r.add(rule);
-              ruleProps.put(rule, ProjectRef.create(project, section.getName()));
-            }
-          }
-
-          if (permission.getExclusiveGroup()) {
-            exclusivePermissionsByProject.put(project, permission.getName());
-            exclusiveGroupPermissions.add(permission.getName());
-          }
+        List<PermissionRule> rules = findRules(sections, pn);
+        if (!rules.isEmpty()) {
+          rulesByPermission.put(pn, rules);
         }
       }
 
-      return new PermissionCollection(permissions, overridden, ruleProps, perUser);
+      return new PermissionCollection(rulesByPermission, blockByPermission, perUser);
     }
   }
 
-  private final Map<String, List<PermissionRule>> rules;
-  private final Map<String, List<PermissionRule>> overridden;
-  private final Map<PermissionRule, ProjectRef> ruleProps;
+  static List<PermissionRule> findRules(List<AccessSection> sections, String permName) {
+    List<PermissionRule> r = new ArrayList<>();
+    for (AccessSection s : sections) {
+      Permission p = s.getPermission(permName);
+      if (p == null) {
+        continue;
+      }
+      for (PermissionRule pr : p.getRules()) {
+        r.add(pr);
+      }
+      // We found an exclusive permission, so no need to further go up the hierarchy.
+      if (p.getExclusiveGroup()) {
+        break;
+      }
+    }
+    return r;
+  }
+
+  static List<BlockAccessSection> findBlocks(List<AccessSection> sections, String permName) {
+    List<Permission> blockPerms = new ArrayList<>();
+    for (AccessSection s : sections) {
+      Permission p = s.getPermission(permName);
+      if (p == null) {
+        continue;
+      }
+
+      for (PermissionRule r : p.getRules()) {
+        if (r.getAction() == BLOCK) {
+          blockPerms.add(p);
+          break;
+        }
+      }
+    }
+
+    // sections goes from project towards All-Projects, but BLOCK take effect from the other
+    // direction.
+    blockPerms = Lists.reverse(blockPerms);
+
+    List<BlockAccessSection> result = new ArrayList<>();
+    for (Permission p : blockPerms) {
+      BlockAccessSection sect = new BlockAccessSection();
+
+      for (PermissionRule r : p.getRules()) {
+        if (r.getAction() == BLOCK) {
+          sect.blocks.add(r);
+        } else {
+          sect.overrides.add(r);
+        }
+      }
+      result.add(sect);
+    }
+
+    return result;
+  }
+
+  static class BlockAccessSection {
+    List<PermissionRule> blocks;
+    List<PermissionRule> overrides;
+
+    BlockAccessSection() {
+      blocks = new ArrayList<>();
+      overrides = new ArrayList<>();
+    }
+  };
+
+  private final Map<String, List<PermissionRule>> rulesByPermission;
+  private final Map<String, List<BlockAccessSection>> blockByPermission;
   private final boolean perUser;
 
+  public List<PermissionRule> getRules(String perm) {
+    return rulesByPermission.get(perm);
+  }
+
+  public List<BlockAccessSection> getBlocks(String perm) {
+    return blockByPermission.get(perm);
+  }
+
   private PermissionCollection(
-      Map<String, List<PermissionRule>> rules,
-      Map<String, List<PermissionRule>> overridden,
-      Map<PermissionRule, ProjectRef> ruleProps,
+      Map<String, List<PermissionRule>> rulesByPermission,
+      Map<String, List<BlockAccessSection>> blockByPermission,
       boolean perUser) {
-    this.rules = rules;
-    this.overridden = overridden;
-    this.ruleProps = ruleProps;
+    this.rulesByPermission = rulesByPermission;
+    this.blockByPermission = blockByPermission;
     this.perUser = perUser;
   }
 
@@ -217,29 +257,8 @@ public class PermissionCollection {
    *     returned when there are no rules for the requested permission name.
    */
   public List<PermissionRule> getPermission(String permissionName) {
-    List<PermissionRule> r = rules.get(permissionName);
+    List<PermissionRule> r = rulesByPermission.get(permissionName);
     return r != null ? r : Collections.<PermissionRule>emptyList();
-  }
-
-  List<PermissionRule> getOverridden(String permissionName) {
-    return firstNonNull(overridden.get(permissionName), Collections.<PermissionRule>emptyList());
-  }
-
-  ProjectRef getRuleProps(PermissionRule rule) {
-    return ruleProps.get(rule);
-  }
-
-  /**
-   * Obtain all declared permission rules that match the reference.
-   *
-   * @return all rules. The collection will iterate a permission if it was declared in the project
-   *     configuration, either directly or inherited. If the project owner did not use a known
-   *     permission (for example {@link Permission#FORGE_SERVER}, then it will not be represented in
-   *     the result even if {@link #getPermission(String)} returns an empty list for the same
-   *     permission.
-   */
-  public Iterable<Map.Entry<String, List<PermissionRule>>> getDeclaredPermissions() {
-    return rules.entrySet();
   }
 
   /** (ref, permission, group) tuple. */

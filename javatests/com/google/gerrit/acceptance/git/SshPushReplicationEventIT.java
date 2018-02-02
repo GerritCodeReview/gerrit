cbd425b4ae358468c00e3df18126d71e74a03554
Alice Kober-Sotzek <aliceks@google.com>
2018-02-02 11:46:17 +0100

    Allow Git pushes of the replication user via HTTP in slave mode
    Currently, the replication plugin sends any replication events directly
    to the Git layer and thus doesn't depend on the way how Gerrit accepts
    commits. This mechanism only worked because some functionalities of
    Gerrit (e.g. the indices) are disabled in slave mode. In order to
    support groups in NoteDb in slave mode, we need a working and
    up-to-date group index, though. For this reason, we are going to send
    the replication events to Gerrit in the future and trigger special
    handling for them on their reception.
    
    Before we can do that, we need to allow any Git pushes of the
    replication user in slave mode as up to now all Git pushes were
    completely rejected when in slave mode. The replication user is
    identified by its ID, which must be configured in Gerrit's config.
    
    This change only cares about commits sent via HTTP. Adjustments for the
    SSH code path are covered by I4bfd298ce. Tests are provided for both
    within this change.
    
    One of the receive hooks required the change index to be present. This
    wasn't an issue previously as we didn't handle commits that far in slave
    mode. Now that we do, we need to guard against this case.
    
    Change-Id: Ic8f8dd100b76178f15bb6d821ead48432381b71b

---
+++ javatests/com/google/gerrit/acceptance/git/SshPushReplicationEventIT.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.git;
+
+import com.google.gerrit.acceptance.AcceptanceTestRequestScope;
+import com.google.gerrit.acceptance.GitUtil;
+import com.google.gerrit.acceptance.NoHttpd;
+import com.google.gerrit.acceptance.SshSession;
+import com.google.gerrit.acceptance.TestAccount;
+import com.google.gerrit.acceptance.UseSsh;
+import com.google.gerrit.reviewdb.client.Project;
+import com.jcraft.jsch.JSchException;
+import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
+import org.eclipse.jgit.junit.TestRepository;
+import org.junit.Ignore;
+
+@NoHttpd
+@UseSsh
+// TODO(aliceks): Find a way to make the SSH tests work.
+@Ignore("Doesn't work yet.")
+public class SshPushReplicationEventIT extends AbstractPushReplicationEventIT {
+  private SshSession currentUserSession;
+
+  @Override
+  protected void setProtocolUser(AcceptanceTestRequestScope.Context context, TestAccount user) {
+    currentUserSession = context.getSession();
+    try {
+      currentUserSession.open();
+    } catch (JSchException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  @Override
+  protected TestRepository<InMemoryRepository> createTestRepository(Project.NameKey project)
+      throws Exception {
+    String url = currentUserSession.getUrl();
+    return GitUtil.cloneProject(project, url + "/" + project.get());
+  }
+}

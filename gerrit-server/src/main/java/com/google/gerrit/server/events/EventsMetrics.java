9580f0ddb055dc0cebfec951c99533371f670818
Markus Duft <markus.duft@ssi-schaefer.com>
2018-02-05 10:47:17 +0100

    Events Metrics: introduce scoped events (project, user)
    The standard 'events' metric contains a counter per event fired in
    gerrit, but these counters do not carry any additional information.
    
    On servers with many projects it is interesting to find out who is
    producing which events on which project, to find problems as well as for
    statistical purposes.
    
    This change introduces the 'scoped-events' metric in addition to the
    'events' metric. It carries information about which project has been
    changed by which 'actor' (submitter, changer, restorer, etc. depending
    on the event type).
    
    In addition there are 'artificial' events generated for PatchSetCreated
    with number 1 (change-created), so correlating created patchsets and
    created changes is possible. Also providing an approval along with a
    comment on a change will create label specific events to allow
    statistical evaluation of votes.
    
    Change-Id: Icca8dd0b0b34a64bd9054c55cf42749f39507e43
    Signed-off-by: Markus Duft <markus.duft@ssi-schaefer.com>

--- gerrit-server/src/main/java/com/google/gerrit/server/events/EventsMetrics.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/events/EventsMetrics.java
@@ -16,27 +16,146 @@ package com.google.gerrit.server.events;
 
 import com.google.gerrit.common.EventListener;
 import com.google.gerrit.metrics.Counter1;
+import com.google.gerrit.metrics.Counter3;
 import com.google.gerrit.metrics.Description;
 import com.google.gerrit.metrics.Field;
 import com.google.gerrit.metrics.MetricMaker;
+import com.google.gerrit.server.data.ApprovalAttribute;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class EventsMetrics implements EventListener {
+
   private final Counter1<String> events;
+  private final Counter3<String, String, String> scopedEvents;
+
+  private static final Logger log = LoggerFactory.getLogger(EventsMetrics.class);
 
   @Inject
   public EventsMetrics(MetricMaker metricMaker) {
+    Field<String> type = Field.ofString("type");
+    Field<String> project = Field.ofString("project");
+    Field<String> actor = Field.ofString("actor");
+
+    this.scopedEvents =
+        metricMaker.newCounter(
+            "scoped-events",
+            new Description("Events per Project and Actor").setRate().setUnit("triggered events"),
+            type,
+            project,
+            actor);
+
     events =
         metricMaker.newCounter(
             "events",
             new Description("Triggered events").setRate().setUnit("triggered events"),
-            Field.ofString("type"));
+            type);
   }
 
   @Override
-  public void onEvent(com.google.gerrit.server.events.Event event) {
+  public void onEvent(Event event) {
     events.increment(event.getType());
+
+    try {
+      // scoped events (project & user). not all events carry this information, so there will be
+      // less scoped than scope-less events (thus tracking both).
+      if (event instanceof ProjectEvent) {
+        processProjectEvent((ProjectEvent) event);
+      }
+    } catch (Exception e) {
+      log.error("exception during metrics conversion", e);
+    }
+  }
+
+  private void processProjectEvent(ProjectEvent event) {
+    String prj = event.getProjectNameKey().get();
+    String user = null;
+
+    if (event instanceof RefEvent) {
+      user = personFromRefEvent(prj, (RefEvent) event);
+    }
+
+    if (user != null) {
+      scopedEvents.increment(event.getType(), prj, user);
+    }
+  }
+
+  private String personFromRefEvent(String prj, RefEvent event) {
+    if (event instanceof CommitReceivedEvent) {
+      return ((CommitReceivedEvent) event).user.getName();
+    } else if (event instanceof RefReceivedEvent) {
+      return ((RefReceivedEvent) event).user.getName();
+    } else if (event instanceof RefUpdatedEvent) {
+      return ((RefUpdatedEvent) event).submitter.get().name;
+    }
+
+    if (event instanceof ChangeEvent) {
+      return personFromChangeEvent(prj, (ChangeEvent) event);
+    }
+
+    return null;
+  }
+
+  private String personFromChangeEvent(String prj, ChangeEvent event) {
+    if (event instanceof AssigneeChangedEvent) {
+      return ((AssigneeChangedEvent) event).changer.get().name;
+    } else if (event instanceof HashtagsChangedEvent) {
+      return ((HashtagsChangedEvent) event).editor.get().name;
+    } else if (event instanceof PrivateStateChangedEvent) {
+      return ((PrivateStateChangedEvent) event).changer.get().name;
+    } else if (event instanceof TopicChangedEvent) {
+      return ((TopicChangedEvent) event).changer.get().name;
+    } else if (event instanceof WorkInProgressStateChangedEvent) {
+      return ((WorkInProgressStateChangedEvent) event).changer.get().name;
+    }
+
+    if (event instanceof PatchSetEvent) {
+      return personFromPatchSetEvent(prj, (PatchSetEvent) event);
+    }
+
+    return null;
+  }
+
+  private String personFromPatchSetEvent(String prj, PatchSetEvent event) {
+    if (event instanceof ChangeAbandonedEvent) {
+      return ((ChangeAbandonedEvent) event).abandoner.get().name;
+    } else if (event instanceof ChangeMergedEvent) {
+      return ((ChangeMergedEvent) event).submitter.get().name;
+    } else if (event instanceof ChangeRestoredEvent) {
+      return ((ChangeRestoredEvent) event).restorer.get().name;
+    } else if (event instanceof CommentAddedEvent) {
+      String actor = ((CommentAddedEvent) event).author.get().name;
+
+      // also inject artificial events for all approvals
+      ApprovalAttribute[] approvals = ((CommentAddedEvent) event).approvals.get();
+      if (approvals != null && approvals.length > 0) {
+        for (ApprovalAttribute approval : approvals) {
+          if (approval.value.equals(approval.oldValue)) {
+            continue; // no change
+          }
+          String apprId = approval.type + "-" + approval.value;
+          scopedEvents.increment(apprId.toLowerCase(), prj, actor);
+        }
+      }
+      return actor;
+    } else if (event instanceof PatchSetCreatedEvent) {
+      // also inject an artificial event if patchset number is 1 (change created)
+      String user = ((PatchSetCreatedEvent) event).uploader.get().name;
+      if (event.patchSet.get().number == 1) {
+        scopedEvents.increment("change-created", prj, user);
+      }
+      return user;
+    } else if (event instanceof ReviewerAddedEvent) {
+      // intentionally left blank
+    } else if (event instanceof ReviewerDeletedEvent) {
+      // intentionally left blank
+    } else if (event instanceof VoteDeletedEvent) {
+      return ((VoteDeletedEvent) event).remover.get().name;
+    }
+
+    return null;
   }
 }

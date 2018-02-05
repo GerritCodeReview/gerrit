74fe95eb47d0ab919f9fc289876bd8a7445ada3d
Becky Siegel <beckysiegel@google.com>
2018-02-05 10:27:32 -0800

    Add rough component for viewing comment threads in the change view
    Future changes will enhance the aesthetics of this component and insert
    it as a tab by the messages list
    
    Bug: Issue 8241
    Change-Id: Ia68519ba67aa0fa07d1687e731e1ed82e9979369

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -1112,6 +1112,8 @@
           .then(comments => {
             this._changeComments = comments;
             this._diffDrafts = Object.assign({}, this._changeComments.drafts);
+            this._commentThreads = this._changeComments.
+                getAllThreadsForChange();
           });
     },
 

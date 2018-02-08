85bb588a39602b512a253c4c2ac581b54e25fada
Edwin Kempin <ekempin@google.com>
2018-02-08 16:24:11 +0100

    Periodically reindex groups on slaves
    Replication to slaves happens on Git level so that Gerrit is not aware
    of incoming replication events. But slaves need an updated group index
    to resolve memberships of users for ACL validation. To keep the group
    index in slaves up-to-date a backgroup job periodically scans the group
    refs in the All-Users repository to reindex groups. The ref states of
    the group refs are cached so that on each run new, updated and deleted
    groups can be detect. Only those groups are then reindexed. On startup
    of a slave all groups are reindexed once. This means callers of slaves
    may observe outdated group information until the next indexing happens.
    The interval on which group indexing is done is configurable by setting
    'index.scheduledIndexer.interval' in 'gerrit.config'. By default group
    indexing is done every 5 minutes.
    
    Detecting group deletions that were replicated while the slave was
    offline is not supported. This means if group refs are deleted while the
    slave is offline these groups are not removed from the group index when
    the slave is started. However since group deletion is not supported this
    should never happen.
    
    Change-Id: Ibac7af13dea0be76f4708d2064373c212fae305a
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ java/com/google/gerrit/server/group/PeriodicGroupIndexer.java
@@ -0,0 +1,159 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.group;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static com.google.gerrit.server.config.ScheduleConfig.MISSING_CONFIG;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.lifecycle.LifecycleModule;
+import com.google.gerrit.reviewdb.client.AccountGroup;
+import com.google.gerrit.reviewdb.client.RefNames;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.ScheduleConfig;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.WorkQueue;
+import com.google.gerrit.server.index.group.GroupIndexer;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Runnable to schedule periodic group reindexing.
+ *
+ * <p>Periodic group indexing is intended to run only on slaves. Replication to slaves happens on
+ * Git level so that Gerrit is not aware of incoming replication events. But slaves need an updated
+ * group index to resolve memberships of users for ACL validation. To keep the group index in slaves
+ * up-to-date this class periodically scans the group refs in the All-Users repository to reindex
+ * groups. The ref states of the group refs are cached so that on each run new, updated and deleted
+ * groups can be detect. Only those groups are then reindexed. On startup of a slave all groups are
+ * reindexed once. This means callers of slaves may observe outdated group information until the
+ * next indexing happens. The interval on which group indexing is done is configurable by setting
+ * {@code index.scheduledIndexer.interval} in {@code gerrit.config}. By default group indexing is
+ * done every 5 minutes.
+ *
+ * <p>This class is not able to detect group deletions that were replicated while the slave was
+ * offline. This means if group refs are deleted while the slave is offline these groups are not
+ * removed from the group index when the slave is started. However since group deletion is not
+ * supported this should never happen.
+ */
+public class PeriodicGroupIndexer implements Runnable {
+  private static final Logger log = LoggerFactory.getLogger(PeriodicGroupIndexer.class);
+
+  public static class Module extends LifecycleModule {
+    @Override
+    protected void configure() {
+      listener().to(Lifecycle.class);
+    }
+  }
+
+  private static class Lifecycle implements LifecycleListener {
+    private final Config cfg;
+    private final WorkQueue queue;
+    private final PeriodicGroupIndexer runner;
+
+    @Inject
+    Lifecycle(@GerritServerConfig Config cfg, WorkQueue queue, PeriodicGroupIndexer runner) {
+      this.cfg = cfg;
+      this.queue = queue;
+      this.runner = runner;
+    }
+
+    @Override
+    public void start() {
+      ScheduleConfig scheduleConfig = new ScheduleConfig(cfg, "index", "scheduledIndexer");
+      scheduleConfig.setDefaults("00:00", TimeUnit.MINUTES.toMillis(5));
+      long interval = scheduleConfig.getInterval();
+      long delay = scheduleConfig.getInitialDelay();
+      checkState(
+          interval != MISSING_CONFIG && delay != MISSING_CONFIG,
+          "No slave.reindexer schedule configuration");
+      checkState(delay >= 0 && interval >= 0, "Invalid slave.reindexer schedule configuration");
+      @SuppressWarnings("unused")
+      Future<?> possiblyIgnoredError =
+          queue
+              .getDefaultQueue()
+              .scheduleAtFixedRate(runner, delay, interval, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void stop() {
+      // handled by WorkQueue.stop() already
+    }
+  }
+
+  private final AllUsersName allUsersName;
+  private final GitRepositoryManager repoManager;
+  private final Provider<GroupIndexer> groupIndexer;
+
+  private ImmutableMap<String, ObjectId> groupRefs;
+
+  @Inject
+  PeriodicGroupIndexer(
+      AllUsersName allUsersName,
+      GitRepositoryManager repoManager,
+      Provider<GroupIndexer> groupIndexer) {
+    this.allUsersName = allUsersName;
+    this.repoManager = repoManager;
+    this.groupIndexer = groupIndexer;
+  }
+
+  @Override
+  public synchronized void run() {
+    try (Repository allUsers = repoManager.openRepository(allUsersName)) {
+      ImmutableMap<String, ObjectId> newGroupRefs =
+          allUsers
+              .getRefDatabase()
+              .getRefs(RefNames.REFS_GROUPS)
+              .entrySet()
+              .stream()
+              .collect(toImmutableMap(Map.Entry::getKey, e -> e.getValue().getObjectId()));
+      int reindexCounter = 0;
+      for (Map.Entry<String, ObjectId> e : newGroupRefs.entrySet()) {
+        if (groupRefs == null
+            || !groupRefs.containsKey(e.getKey())
+            || !groupRefs.get(e.getKey()).equals(e.getValue())) {
+          // new group or updated group
+          groupIndexer.get().index(AccountGroup.UUID.fromRefPart(e.getKey()));
+          reindexCounter++;
+        }
+      }
+      if (groupRefs != null) {
+        for (String groupRefPart : groupRefs.keySet()) {
+          if (!newGroupRefs.containsKey(groupRefPart)) {
+            // deleted group
+            groupIndexer.get().index(AccountGroup.UUID.fromRefPart(groupRefPart));
+            reindexCounter++;
+          }
+        }
+      }
+      groupRefs = newGroupRefs;
+      log.info("Run group indexer, {} groups reindexed", reindexCounter);
+    } catch (IOException e) {
+      log.error("Failed to reindex groups", e);
+    }
+  }
+}

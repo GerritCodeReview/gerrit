6ada4d2e2f5cd8233d77e2c16509ec37aa8d464e
Paladox none <thomasmulhall410@yahoo.com>
2018-02-16 12:32:35 +0000

    Add reset button to my menu in settings
    Bug: Issue 8375
    Change-Id: Icb54c712e1bea6a625aca5e91c87207bf0dbd3b1

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -756,6 +756,10 @@
       });
     },
 
+    getDefaultPreferences() {
+      return this._fetchSharedCacheURL('/config/server/preferences');
+    },
+
     getPreferences() {
       return this.getLoggedIn().then(loggedIn => {
         if (loggedIn) {

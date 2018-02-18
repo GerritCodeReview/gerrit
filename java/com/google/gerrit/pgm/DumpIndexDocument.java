bbd46f38ea24d3ac7807874a9446d540aa290291
David Ostrovsky <david@ostrovsky.org>
2018-02-18 10:08:36 +0100

    Add site program to analyze secondary index document
    There are number of options to investigate the content of secondary
    index document. There are number of utilities, like Luke, or a index
    dump can be trivially created. Because of usage of protocoll buffers
    serializer, the content of many fields is binary and cannot be easily
    investigated, other then query the index in running gerrit instance.
    
    To simplify the content dump, add site program to dump the content
    of secondary index documents.
    
    Given that this utility is for developer purpose only, it doesn't
    supposed to be documented.
    
    Currently all documents in the index are written. Future extension
    should accept document id(s) and only dump the requested document(s).
    
      $ java -jar bazel-bin/gerrit.war dump-index-document --index changes \
        --index-schema-version 49 --site-path ../test_site --stdout
    [
      {
        "added": "stored<added:1>",
        "_approval": {
          "key": {
            "patchSetId": {
              "changeId": {
                "id": 1
              },
              "patchSetId": 4
            },
            "accountId": {
              "id": 1000000
            },
            "categoryId": {
              "id": "Verified"
            }
          },
          "value": 1,
          "granted": "Feb 18, 2018 7:17:21 PM",
          "tag": null,
          "realAccountId": null,
          "postSubmit": false
        },
    [...]
    
    Bug: Issue 8191
    Change-Id: I74e48ea3147a106e47bc8ba001b263dfa97c19a5

---
+++ java/com/google/gerrit/pgm/DumpIndexDocument.java
@@ -0,0 +1,122 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm;
+
+import static com.google.gerrit.server.index.change.ChangeField.APPROVAL_CODEC;
+import static com.google.gerrit.server.index.change.ChangeField.CHANGE_CODEC;
+import static com.google.gerrit.server.index.change.ChangeField.PATCH_SET_CODEC;
+
+import com.google.common.base.Joiner;
+import com.google.gerrit.pgm.util.SiteProgram;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.PatchSetApproval;
+import com.google.gerrit.server.index.change.ChangeField;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonWriter;
+import com.google.gwtorm.protobuf.ProtobufCodec;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.BytesRef;
+import org.kohsuke.args4j.Option;
+
+public class DumpIndexDocument extends SiteProgram {
+  private static final Pattern UNINTERESTED_FIELDS =
+      Pattern.compile(
+          Joiner.on("|")
+              .join(
+                  "ref_state_pattern",
+                  "ref_state",
+                  "full_submit_record_lenient",
+                  "full_submit_record_strict"));
+
+  @Option(name = "--index", usage = "Index kind (accounts, changes, groups)")
+  private String index;
+
+  @Option(name = "--index-schema-version", usage = "Schema version to dump")
+  private String indexVersion;
+
+  @Option(name = "--stdout", usage = "Dump to stdout (default is to write to index.json)")
+  private boolean stdout;
+
+  @Override
+  public int run() throws Exception {
+    mustHaveValidSite();
+
+    Gson g = new Gson();
+    JsonWriter w =
+        new JsonWriter(
+            stdout
+                ? new PrintWriter(System.out)
+                : new BufferedWriter(new FileWriter("index.json")));
+    w.setIndent("  ");
+
+    Path p = getSitePath().resolve("index").resolve(index + "_00" + indexVersion);
+    // TODO(davido): Handle closed sub index for change index
+    if (index.equals("changes")) {
+      p = p.resolve("open");
+    }
+
+    try (IndexReader r = DirectoryReader.open(FSDirectory.open(p))) {
+      w.beginArray();
+      for (int i = 0; i < r.numDocs(); i++) {
+        writeDocument(g, r.document(i), w);
+      }
+      w.endArray();
+    }
+    w.flush();
+
+    return 0;
+  }
+
+  private static void writeDocument(Gson g, Document d, JsonWriter w) throws IOException {
+    w.beginObject();
+    for (IndexableField f : d.getFields()) {
+      if (UNINTERESTED_FIELDS.matcher(f.name()).matches()) {
+        continue;
+      }
+      w.name(f.name());
+      writeValue(g, f, w);
+    }
+    w.endObject();
+  }
+
+  private static void writeValue(Gson g, IndexableField f, JsonWriter w) throws IOException {
+    String n = f.name();
+    if (n.equals(ChangeField.CHANGE.getName())) {
+      g.getAdapter(Change.class).write(w, decodeProtos(f, CHANGE_CODEC));
+    } else if (n.equals(ChangeField.PATCH_SET.getName())) {
+      g.getAdapter(PatchSet.class).write(w, decodeProtos(f, PATCH_SET_CODEC));
+    } else if (n.equals(ChangeField.APPROVAL.getName())) {
+      g.getAdapter(PatchSetApproval.class).write(w, decodeProtos(f, APPROVAL_CODEC));
+    } else {
+      w.value(f.toString());
+    }
+  }
+
+  private static <T> T decodeProtos(IndexableField f, ProtobufCodec<T> c) {
+    BytesRef r = f.binaryValue();
+    return c.decode(r.bytes, r.offset, r.length);
+  }
+}

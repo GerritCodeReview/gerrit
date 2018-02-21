9cf2953885923606d3355975e095d438870c9f44
Viktar Donich <viktard@google.com>
2018-02-20 16:18:42 -0800

    Run functional tests against Gerrit in a prebuilt Docker image
    docker-compose is used to bring up two containers:
     - `server` is a `gerritcodereview/gerrit` stable Docker image,
     prepopulated with some minimal data
     - `tester` is a `selenium/standalone-chrome-debug` Docker image, with
     added test runner (Karma) and a basic static web server
    
    Change-Id: I6d9667220b234849729c0ca4c2b1e7b281594c8c

--- polygerrit-ui/app/test/functional/test.js
+++ polygerrit-ui/app/test/functional/test.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-const {until} = require('selenium-webdriver');
+const {until, By, Key} = require('selenium-webdriver');
 const {setup, cleanup} = require('test-infra');
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
@@ -21,5 +21,31 @@ describe('example ', () => {
 
   it('should update title', () => {
     return driver.wait(until.titleIs('status:open · Gerrit Code Review'), 5000);
+  });
+
+  it('finds no merged changes', () => {
+    return driver.findElement(By.id('searchInput')).sendKeys(
+        Key.chord(Key.CONTROL, 'a'), 'status:merged', Key.RETURN).then(() => {
+          return driver.wait(
+              until.titleIs('status:merged · Gerrit Code Review'), 5000);
+        }).then(() => {
+          return driver.findElement(
+              By.css('#changeList .noChanges .cell')).getText().then(text => {
+                expect(text).toBe('No changes');
+              });
+        });
+  });
+
+  it('finds open changes', () => {
+    return driver.findElement(By.id('searchInput')).sendKeys(
+        Key.chord(Key.CONTROL, 'a'), 'status:open', Key.RETURN).then(() => {
+          return driver.wait(
+              until.titleIs('status:open · Gerrit Code Review'), 5000);
+        }).then(() => {
+          return driver.findElement(
+              By.css('gr-change-list-item .subject a')).getText().then(text => {
+                expect(text).toBe('Test change, please ignore');
+              });
+        });
   });
 });

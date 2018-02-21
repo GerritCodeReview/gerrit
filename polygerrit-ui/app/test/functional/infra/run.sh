ef406188e61c981ca04305ef4367ba03497d1614
Viktar Donich <viktard@google.com>
2018-02-20 16:18:42 -0800

    Run functional tests against Gerrit in a prebuilt Docker image
    docker-compose is used to bring up two containers:
     - `server` is a `gerritcodereview/gerrit` stable Docker image,
     prepopulated with some minimal data
     - `infra` is a `selenium/standalone-chrome-debug` Docker image, with
     added test runner (Karma) and a basic static web server
    
    Change-Id: I6d9667220b234849729c0ca4c2b1e7b281594c8c

--- polygerrit-ui/app/test/functional/infra/run.sh
+++ polygerrit-ui/app/test/functional/infra/run.sh
@@ -2,7 +2,7 @@
 echo Starting local server..
 cp /app/polygerrit_ui.zip .
 unzip -q polygerrit_ui.zip
-nohup http-server polygerrit_ui > /tmp/http-server.log 2>&1 &
+nohup http-server --cors -p 8081 polygerrit_ui > /tmp/http-server.log 2>&1 &
 
 echo Starting Webdriver..
 nohup /opt/bin/entry_point.sh > /tmp/webdriver.log 2>&1 &
@@ -10,5 +10,5 @@ nohup /opt/bin/entry_point.sh > /tmp/webdriver.log 2>&1 &
 # Wait for servers to start
 sleep 5
 
-cp $@ .
-jasmine $(basename $@)
+cp /tests/*test.js .
+jasmine *test.js
--- polygerrit-ui/app/test/functional/run_functional.sh
+++ polygerrit-ui/app/test/functional/run_functional.sh
@@ -2,9 +2,7 @@
 
 bazel build //polygerrit-ui/app:polygerrit_ui
 
-docker run --rm \
-  -p 5900:5900 \
-  -v `pwd`/polygerrit-ui/app/test/functional:/tests \
-  -v `pwd`/bazel-genfiles/polygerrit-ui/app:/app \
-  -it gerrit/polygerrit-functional:v1 \
-  /tests/test.js
+pushd ./polygerrit-ui/app/test/functional > /dev/null
+trap popd EXIT
+
+docker-compose up --abort-on-container-exit --force-recreate

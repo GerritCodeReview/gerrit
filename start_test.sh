faed5a4b2b85d288a61c6a83a3442c12b3a870a7
Han-Wen Nienhuys <hanwen@google.com>
2018-02-22 15:38:57 +0100

    Proof of concept of starting up Gerrit server.
    Useful for getting started with webtesting.
    
    Change-Id: I872607b3ce5707b8fc9e8b090292bc984bdfc535

---
+++ start_test.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -eu
+
+WAR=$TEST_SRCDIR/gerrit/polygerrit.war
+JAVA=$TEST_SRCDIR/local_jdk/bin/java
+
+DB=$(mktemp -d)
+
+$JAVA -jar ${WAR} \
+  init -d ${DB} --batch --dev --no-auto-start
+
+# TODO(viktard): pick unused port, set port into gerrit.config with git-config
+PORT=8080
+
+$JAVA -jar ${WAR} \
+  daemon -d ${DB} --console-log &
+
+PID=$(jobs -p '%1')
+echo $PID
+trap "kill -9 $PID" EXIT
+
+while true ; do
+  if curl -s -u admin:secret http://localhost:$PORT/a/accounts/self ; then
+    break
+  fi
+  sleep 1
+done
+
+echo "Gerrit is serving"
+
+# Insert some data.
+$TEST_SRCDIR/gerrit/contrib/populate-fixture-data.py -u 20 -p $PORT
+
+echo "inserted data"
+
+# Show the result.
+curl -s -u admin:secret http://localhost:$PORT/a/projects/
+
+# TODO(viktard): start selenium/webdriver. See
+# https://github.com/bazelbuild/rules_webtesting - I'm a fan of Go,
+# but Java is mature and well understood in Gerrit.

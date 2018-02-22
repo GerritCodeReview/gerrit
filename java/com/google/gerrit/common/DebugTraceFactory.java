2f104132ec2465a2a416d6c429d40ac0e5f93728
Edwin Kempin <ekempin@google.com>
2018-02-22 16:13:18 +0100

    Support tracing of single REST calls
    To investigate why a certain REST call failed one needs to find matching
    messages in the logs. Sometimes this is not as easy as it sounds:
    
    1. Error reports may not tell which REST endpoint was invoked and what
       was the resource ID.
    2. For busy servers finding logs that correspond to the failing REST
       call may be difficult because of the huge amount of logs. Matching
       logs by the timestamp of when the request was done may also not be
       straight-forward if the user is in a different timezone than the
       server.
    3. The error_log normally only contain errors, but no detailled debug
       information.
    4. Enabling debug logs effects all requests and usually results in a lot
       of log spam.
    
    The idea of this change is to enable debug traces on need for single
    requests:
    
    - A debug trace is enabled by setting 'trace' as request paramater
    - The response contains 'X-Gerrit-Trace' header with the trace ID
    - An administrator can use the trace ID to find all logs for this
      request in the error log.
    - The debug trace can contain additional debug information which is not
      written when tracing is disabled.
    
    Optionally a trace ID can be specified for further requests so that the
    logs for these requests are written to the existing debug trace. This is
    useful if an action (e.g. from the UI) triggers multiple REST calls that
    should be traced together (UI support for this is not part of this
    change).
    
    If tracing is enabled all log messages that correspond to the traced
    request are prefixed with `[<traceId>]', e.g.:
    
      [2018-02-23 15:04:27,134] [HTTP-75] INFO  com.google.gerrit.httpd.restapi.RestApiServlet : [5b6c76d4-f25e-4c8c-8aa0-3bcb57446b3a] Received REST request: GET /a/accounts/?trace&q=ad
      [2018-02-23 15:04:27,135] [HTTP-75] INFO  com.google.gerrit.httpd.restapi.RestApiServlet : [5b6c76d4-f25e-4c8c-8aa0-3bcb57446b3a] Calling user: admin
      ...
      [2018-02-23 15:04:27,232] [HTTP-75] INFO  com.google.gerrit.httpd.restapi.RestApiServlet : [5b6c76d4-f25e-4c8c-8aa0-3bcb57446b3a] REST call succeeded: 200
      [2018-02-23 15:04:27,234] [HTTP-75] INFO  com.google.gerrit.httpd.restapi.RestApiServlet : [5b6c76d4-f25e-4c8c-8aa0-3bcb57446b3a] JSON response body:
      )]}'
      [
        {
          "_account_id": 1000000
        }
      ]
    
    When tracing is requested a trace ID is generated and stored in a
    ThreadLocal context so that it is accessible whenever a log is written.
    
    For including addition debug information when tracing is enabled new
    'DEBUG' log are added. By default these logs don't show up in the
    error_log since by configuration only log entries with a level >= 'INFO'
    are logged. By setting the log level to 'DEBUG' or lower the 'DEBUG' log
    entries can be enabled. However then they show up for each request which
    creates a lot of log spam when only logs for a single request should be
    investigated. By enabling tracing all log entries on 'DEBUG' level and
    below that correspond to the traced request are promoted to the next
    higher log level. E.g. log entries on 'DEBUG' level will be written on
    'INFO' level. This way they now show up in the error_log if the log
    level is set to 'INFO'. This means request tracing only works if the
    overall log level is 'INFO'.
    
    Change-Id: I7b0880c6df16ac584a27608484a024f204c873c4
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ java/com/google/gerrit/common/DebugTraceFactory.java
@@ -0,0 +1,468 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.common;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+
+public class DebugTraceFactory {
+  private static final ThreadLocal<String> TRACE_ID = new ThreadLocal<>();
+
+  public static Logger getLogger(Class<?> clazz) {
+    return new LoggerWithTracing(LoggerFactory.getLogger(clazz));
+  }
+
+  public static boolean isTracing() {
+    return TRACE_ID.get() != null;
+  }
+
+  public static Optional<String> getTraceId() {
+    return Optional.ofNullable(TRACE_ID.get());
+  }
+
+  public static String enableTracing(@Nullable String traceId) {
+    checkState(TRACE_ID.get() == null, "trace ID already set");
+    if (traceId != null) {
+      // TODO validate traceId format
+    } else {
+      traceId = newTraceId();
+    }
+    TRACE_ID.set(traceId);
+    return traceId;
+  }
+
+  private static String newTraceId() {
+    return UUID.randomUUID().toString();
+  }
+
+  private static class LoggerWithTracing implements Logger {
+    private final Logger logger;
+
+    LoggerWithTracing(Logger logger) {
+      this.logger = checkNotNull(logger, "logger");
+    }
+
+    @Override
+    public String getName() {
+      return logger.getName();
+    }
+
+    @Override
+    public boolean isTraceEnabled() {
+      if (isTracing()) {
+        return logger.isDebugEnabled();
+      }
+      return logger.isTraceEnabled();
+    }
+
+    @Override
+    public void trace(String msg) {
+      if (isTracing()) {
+        logger.debug(withTraceId(msg));
+        return;
+      }
+      logger.trace(msg);
+    }
+
+    @Override
+    public void trace(String format, Object arg) {
+      if (isTracing()) {
+        logger.debug(withTraceId(format), arg);
+        return;
+      }
+      logger.trace(format, arg);
+    }
+
+    @Override
+    public void trace(String format, Object arg1, Object arg2) {
+      if (isTracing()) {
+        logger.debug(withTraceId(format), arg1, arg2);
+        return;
+      }
+      logger.trace(format, arg1, arg2);
+    }
+
+    @Override
+    public void trace(String format, Object... arguments) {
+      if (isTracing()) {
+        logger.debug(withTraceId(format), arguments);
+        return;
+      }
+      logger.trace(format, arguments);
+    }
+
+    @Override
+    public void trace(String msg, Throwable t) {
+      if (isTracing()) {
+        logger.debug(withTraceId(msg), t);
+        return;
+      }
+      logger.trace(msg, t);
+    }
+
+    @Override
+    public boolean isTraceEnabled(Marker marker) {
+      if (isTracing()) {
+        return logger.isDebugEnabled(marker);
+      }
+      return logger.isTraceEnabled(marker);
+    }
+
+    @Override
+    public void trace(Marker marker, String msg) {
+      if (isTracing()) {
+        logger.debug(marker, withTraceId(msg));
+        return;
+      }
+      logger.trace(marker, msg);
+    }
+
+    @Override
+    public void trace(Marker marker, String format, Object arg) {
+      if (isTracing()) {
+        logger.debug(marker, withTraceId(format), arg);
+        return;
+      }
+      logger.trace(marker, format, arg);
+    }
+
+    @Override
+    public void trace(Marker marker, String format, Object arg1, Object arg2) {
+      if (isTracing()) {
+        logger.debug(marker, withTraceId(format), arg1, arg2);
+        return;
+      }
+      logger.trace(marker, format, arg1, arg2);
+    }
+
+    @Override
+    public void trace(Marker marker, String format, Object... argArray) {
+      if (isTracing()) {
+        logger.debug(marker, withTraceId(format), argArray);
+        return;
+      }
+      logger.trace(marker, format, argArray);
+    }
+
+    @Override
+    public void trace(Marker marker, String msg, Throwable t) {
+      if (isTracing()) {
+        logger.debug(marker, withTraceId(msg), t);
+        return;
+      }
+      logger.trace(marker, msg, t);
+    }
+
+    @Override
+    public boolean isDebugEnabled() {
+      if (isTracing()) {
+        return logger.isInfoEnabled();
+      }
+      return logger.isDebugEnabled();
+    }
+
+    @Override
+    public void debug(String msg) {
+      if (isTracing()) {
+        logger.info(withTraceId(msg));
+        return;
+      }
+      logger.debug(msg);
+    }
+
+    @Override
+    public void debug(String format, Object arg) {
+      if (isTracing()) {
+        logger.info(withTraceId(format), arg);
+        return;
+      }
+      logger.debug(format, arg);
+    }
+
+    @Override
+    public void debug(String format, Object arg1, Object arg2) {
+      if (isTracing()) {
+        logger.info(withTraceId(format), arg1, arg2);
+        return;
+      }
+      logger.debug(format, arg1, arg2);
+    }
+
+    @Override
+    public void debug(String format, Object... arguments) {
+      if (isTracing()) {
+        logger.info(withTraceId(format), arguments);
+        return;
+      }
+      logger.debug(format, arguments);
+    }
+
+    @Override
+    public void debug(String msg, Throwable t) {
+      if (isTracing()) {
+        logger.info(withTraceId(msg), t);
+        return;
+      }
+      logger.debug(msg, t);
+    }
+
+    @Override
+    public boolean isDebugEnabled(Marker marker) {
+      if (isTracing()) {
+        return logger.isInfoEnabled(marker);
+      }
+      return logger.isDebugEnabled(marker);
+    }
+
+    @Override
+    public void debug(Marker marker, String msg) {
+      if (isTracing()) {
+        logger.info(marker, withTraceId(msg));
+        return;
+      }
+      logger.debug(marker, msg);
+    }
+
+    @Override
+    public void debug(Marker marker, String format, Object arg) {
+      if (isTracing()) {
+        logger.info(marker, withTraceId(format), arg);
+        return;
+      }
+      logger.debug(marker, format, arg);
+    }
+
+    @Override
+    public void debug(Marker marker, String format, Object arg1, Object arg2) {
+      if (isTracing()) {
+        logger.info(marker, withTraceId(format), arg1, arg2);
+        return;
+      }
+      logger.debug(marker, format, arg1, arg2);
+    }
+
+    @Override
+    public void debug(Marker marker, String format, Object... arguments) {
+      if (isTracing()) {
+        logger.info(marker, withTraceId(format), arguments);
+        return;
+      }
+      logger.debug(marker, format, arguments);
+    }
+
+    @Override
+    public void debug(Marker marker, String msg, Throwable t) {
+      if (isTracing()) {
+        logger.info(marker, withTraceId(msg), t);
+        return;
+      }
+      logger.debug(marker, msg, t);
+    }
+
+    @Override
+    public boolean isInfoEnabled() {
+      return logger.isInfoEnabled();
+    }
+
+    @Override
+    public void info(String msg) {
+      logger.info(withTraceId(msg));
+    }
+
+    @Override
+    public void info(String format, Object arg) {
+      logger.info(withTraceId(format), arg);
+    }
+
+    @Override
+    public void info(String format, Object arg1, Object arg2) {
+      logger.info(withTraceId(format), arg1, arg2);
+    }
+
+    @Override
+    public void info(String format, Object... arguments) {
+      logger.info(withTraceId(format), arguments);
+    }
+
+    @Override
+    public void info(String msg, Throwable t) {
+      logger.info(withTraceId(msg), t);
+    }
+
+    @Override
+    public boolean isInfoEnabled(Marker marker) {
+      return logger.isInfoEnabled(marker);
+    }
+
+    @Override
+    public void info(Marker marker, String msg) {
+      logger.info(marker, withTraceId(msg));
+    }
+
+    @Override
+    public void info(Marker marker, String format, Object arg) {
+      logger.info(marker, withTraceId(format), arg);
+    }
+
+    @Override
+    public void info(Marker marker, String format, Object arg1, Object arg2) {
+      logger.info(marker, withTraceId(format), arg1, arg2);
+    }
+
+    @Override
+    public void info(Marker marker, String format, Object... arguments) {
+      logger.info(marker, withTraceId(format), arguments);
+    }
+
+    @Override
+    public void info(Marker marker, String msg, Throwable t) {
+      logger.info(marker, withTraceId(msg), t);
+    }
+
+    @Override
+    public boolean isWarnEnabled() {
+      return logger.isWarnEnabled();
+    }
+
+    @Override
+    public void warn(String msg) {
+      logger.warn(withTraceId(msg));
+    }
+
+    @Override
+    public void warn(String format, Object arg) {
+      logger.warn(withTraceId(format), arg);
+    }
+
+    @Override
+    public void warn(String format, Object... arguments) {
+      logger.warn(withTraceId(format), arguments);
+    }
+
+    @Override
+    public void warn(String format, Object arg1, Object arg2) {
+      logger.warn(withTraceId(format), arg1, arg2);
+    }
+
+    @Override
+    public void warn(String msg, Throwable t) {
+      logger.warn(withTraceId(msg), t);
+    }
+
+    @Override
+    public boolean isWarnEnabled(Marker marker) {
+      return logger.isWarnEnabled(marker);
+    }
+
+    @Override
+    public void warn(Marker marker, String msg) {
+      logger.warn(marker, withTraceId(msg));
+    }
+
+    @Override
+    public void warn(Marker marker, String format, Object arg) {
+      logger.warn(marker, withTraceId(format), arg);
+    }
+
+    @Override
+    public void warn(Marker marker, String format, Object arg1, Object arg2) {
+      logger.warn(marker, withTraceId(format), arg1, arg2);
+    }
+
+    @Override
+    public void warn(Marker marker, String format, Object... arguments) {
+      logger.warn(marker, withTraceId(format), arguments);
+    }
+
+    @Override
+    public void warn(Marker marker, String msg, Throwable t) {
+      logger.warn(marker, withTraceId(msg), t);
+    }
+
+    @Override
+    public boolean isErrorEnabled() {
+      return logger.isErrorEnabled();
+    }
+
+    @Override
+    public void error(String msg) {
+      logger.error(withTraceId(msg));
+    }
+
+    @Override
+    public void error(String format, Object arg) {
+      logger.error(withTraceId(format), arg);
+    }
+
+    @Override
+    public void error(String format, Object arg1, Object arg2) {
+      logger.error(withTraceId(format), arg1, arg2);
+    }
+
+    @Override
+    public void error(String format, Object... arguments) {
+      logger.error(withTraceId(format), arguments);
+    }
+
+    @Override
+    public void error(String msg, Throwable t) {
+      logger.error(withTraceId(msg), t);
+    }
+
+    @Override
+    public boolean isErrorEnabled(Marker marker) {
+      return logger.isErrorEnabled(marker);
+    }
+
+    @Override
+    public void error(Marker marker, String msg) {
+      logger.error(marker, withTraceId(msg));
+    }
+
+    @Override
+    public void error(Marker marker, String format, Object arg) {
+      logger.error(marker, withTraceId(format), arg);
+    }
+
+    @Override
+    public void error(Marker marker, String format, Object arg1, Object arg2) {
+      logger.error(marker, withTraceId(format), arg1, arg2);
+    }
+
+    @Override
+    public void error(Marker marker, String format, Object... arguments) {
+      logger.error(marker, withTraceId(format), arguments);
+    }
+
+    @Override
+    public void error(Marker marker, String msg, Throwable t) {
+      logger.error(marker, withTraceId(msg), t);
+    }
+
+    private String withTraceId(String s) {
+      String traceId = TRACE_ID.get();
+      if (traceId == null) {
+        return s;
+      }
+      return "[" + traceId + "] " + s;
+    }
+  }
+}

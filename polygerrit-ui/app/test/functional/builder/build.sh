95952e94aa243839887aab2f391a5383663921d7
Viktar Donich <viktard@google.com>
2018-02-27 11:24:36 -0800

    Run functional tests locally
    This change builds Gerrit from the local checked out source tree.
    
    UPGRADE_SERVER=true ./run_functional.sh
    
    Change-Id: Ibd595b49af01915625fed2b4dbc7c42c4d7506b0

--- polygerrit-ui/app/test/functional/builder/build.sh
+++ polygerrit-ui/app/test/functional/builder/build.sh
@@ -1,18 +1,28 @@
 #!/bin/sh -e
 
-if [ -f /app/polygerrit_ui.zip ]
-then
-  echo "PolyGerrit UI already built"
-
+if [ -f /app/polygerrit_ui.zip ]; then
+    echo "PolyGerrit UI already built"
 else
-  echo "Building PolyGerrit UI ..."
-  cd && git clone https://gerrit.googlesource.com/gerrit
-  cd ~/gerrit
-  export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone"
-  bazel build //polygerrit-ui/app:polygerrit_ui
-  cp bazel-genfiles/polygerrit-ui/app/polygerrit_ui.zip /app/
+    if [ -d /src ]; then
+       echo "Building from dev sources"
+       cd /src
+    else
+        echo "Building from master"
+        cd && git clone https://gerrit.googlesource.com/gerrit
+        cd ~/gerrit
+    fi
+
+    if [ "$UPGRADE_SERVER" ]; then
+        echo "Building gerrit.war"
+        bazel build gerrit
+        cp bazel-bin/gerrit.war /app/
+    fi
+
+    echo "Building polygerrit_ui.zip"
+    export BAZEL_OPTS="--spawn_strategy=standalone --genrule_strategy=standalone"
+    bazel build //polygerrit-ui/app:polygerrit_ui
+    cp bazel-genfiles/polygerrit-ui/app/polygerrit_ui.zip /app/
 fi
 
 echo "PolyGerrit UI build complete"
 sleep 3600
-

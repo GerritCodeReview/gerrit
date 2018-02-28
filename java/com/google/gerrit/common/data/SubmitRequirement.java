7750126c482f047a5546c03176a1a8e438a1716a
Maxime Guerreiro <maximeg@google.com>
2018-02-28 14:33:18 +0100

    Add SubmitRequirement to hold the presubmit conditions not met
    Currently, labels are (mis-) used to communicate with the user regarding
    the presubmit failure reason. This leads to weird situations where the
    Gerrit interface tells "Needs labels: Not-Author-Review,
    CI-Build-Failure" which are not labels (and don't look like labels).
    
    The introduced class and variable, SubmitRequirement, contains two
    fields describing the issue and an optional label. This allows to
    implement the two former examples: the CI doesn't need a label, but the
    Non-Author review condition might be related to a Verified label, for
    instance.
    
    Change-Id: I83ab3fe52cc689416a26f9d4f91ebafb7337d428

---
+++ java/com/google/gerrit/common/data/SubmitRequirement.java
@@ -0,0 +1,80 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.common.data;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.gerrit.common.Nullable;
+import java.util.Objects;
+import java.util.Optional;
+
+/** Describes a requirement to submit a change. */
+public final class SubmitRequirement {
+  private final String shortReason;
+  private final String fullReason;
+  @Nullable private final String label;
+
+  public SubmitRequirement(String shortReason, String fullReason, @Nullable String label) {
+    this.shortReason = requireNonNull(shortReason);
+    this.fullReason = requireNonNull(fullReason);
+    this.label = label;
+  }
+
+  public String shortReason() {
+    return shortReason;
+  }
+
+  public String fullReason() {
+    return fullReason;
+  }
+
+  public Optional<String> label() {
+    return Optional.ofNullable(label);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || !(o instanceof SubmitRequirement)) {
+      return false;
+    }
+    SubmitRequirement that = (SubmitRequirement) o;
+    return Objects.equals(shortReason, that.shortReason)
+        && Objects.equals(fullReason, that.fullReason)
+        && Objects.equals(label, that.label);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(shortReason, fullReason, label);
+  }
+
+  @Override
+  public String toString() {
+    return "SubmitRequirement{"
+        + "shortReason='"
+        + shortReason
+        + '\''
+        + ", fullReason='"
+        + fullReason
+        + '\''
+        + ", label='"
+        + label
+        + '\''
+        + '}';
+  }
+}

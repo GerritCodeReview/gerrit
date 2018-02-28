b7c9b0203f36e1e2f6c98602082003ba62748f30
Hugo Arès <hugo.ares@ericsson.com>
2018-02-27 19:05:41 -0500

    Revert "Reduce chance of deadlock in account cache"
    This reverts commit 00fc15ac0073b86270e7c0f40d386f95dfe31e86.
    
    With Caffeine, accessing the cache from the cache loader is causing an
    infinite loop. On stable-2.14, the only circular dependency was removed
    but it looks like there is another one on stable-2.15. Revert the change
    until circular dependency is removed or a solution is found to access
    the cache from the cache loader.
    
    The test going into an infinite loop is
    RevisionDiffIT.rebaseHunksOneLineApartFromRegularHunkAreIdentified.
    
    Bug: Issue 8464
    Change-Id: If65560b4a9bfcf0a03decaedd83ad000c6b28f4f

--- gerrit-cache-h2/src/main/java/com/google/gerrit/server/cache/h2/H2CacheFactory.java
+++ gerrit-cache-h2/src/main/java/com/google/gerrit/server/cache/h2/H2CacheFactory.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.server.cache.h2;
 
-import com.github.benmanes.caffeine.guava.CaffeinatedGuava;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
@@ -169,7 +168,7 @@ class H2CacheFactory implements PersistentCacheFactory, LifecycleListener {
             executor,
             store,
             def.keyType(),
-            (Cache<K, ValueHolder<V>>) CaffeinatedGuava.build(defaultFactory.create(def, true)));
+            (Cache<K, ValueHolder<V>>) defaultFactory.create(def, true).build());
     synchronized (caches) {
       caches.add(cache);
     }
@@ -189,9 +188,9 @@ class H2CacheFactory implements PersistentCacheFactory, LifecycleListener {
         newSqlStore(def.name(), def.keyType(), limit, def.expireAfterWrite(TimeUnit.SECONDS));
     Cache<K, ValueHolder<V>> mem =
         (Cache<K, ValueHolder<V>>)
-            CaffeinatedGuava.build(
-                defaultFactory.create(def, true),
-                (CacheLoader<K, V>) new H2CacheImpl.Loader<>(executor, store, loader));
+            defaultFactory
+                .create(def, true)
+                .build((CacheLoader<K, V>) new H2CacheImpl.Loader<>(executor, store, loader));
     H2CacheImpl<K, V> cache = new H2CacheImpl<>(executor, store, def.keyType(), mem);
     caches.add(cache);
     return cache;

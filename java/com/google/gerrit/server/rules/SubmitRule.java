9b123ce8f90e4bc33a7dca8b05d150eb8d1df7f3
Maxime Guerreiro <maximeg@google.com>
2018-02-28 14:31:55 +0100

    Add SubmitRule extension point
    The creation of custom rules to allow the sending of modification
    presents major disadvantages: the rules must be written in Prolog,
    which is harder to master than other options, and not as modular as it
    could be.
    
    By providing an interface that plugins can use to implement custom
    presubmit rules, these disadvantages should disappear: Gerrit plugins
    have full access to the core systems, but also to external resources.
    Everything that was possible with Prolog should be possible, and easier
    to achieve.
    
    Plugins can implement this interface and react however they want: a
    plugin can for instance define a Prolog rule evaluator, while an other
    hardcodes the rules in the Java code.
    
    Further commits will be responsible for using the interface, defining
    explicitely it's final behavior and implementing the interface (for both
    Prolog and custom plugins).
    
    Change-Id: Ic0731321eb9d182ddbfa27d7c08eaeea9f3155e5

---
+++ java/com/google/gerrit/server/rules/SubmitRule.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.server.rules;
+
+import com.google.gerrit.common.data.SubmitRecord;
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+import com.google.gerrit.server.query.change.ChangeData;
+
+/**
+ * Allows plugins to define custom rules to accept or reject submission of a change. The returned
+ * value, hold in a SubmitRecord, should contain enough information for the end user to "fix".
+ * Several points should be noted:
+ *
+ * <p>- Multiple plugins may be called for a given change.
+ *
+ * <p>- Each plugin is responsible for handling (or not) inheritance of configurations.
+ *
+ * <p>In a nutshell, this interface makes it possible to write validation rules in Java, or even to
+ * write adapters for Script Engines (Prolog, JavaScript or even custom expression parsers).
+ */
+@ExtensionPoint
+public interface SubmitRule {
+  /** Returns a {@link SubmitRecord} status for the change. */
+  SubmitRecord evaluate(ChangeData changeData);
+}

eb48f260cf5bf47e72ac4c57aff9e9db05974348
Maxime Guerreiro <maximeg@google.com>
2018-02-28 14:31:55 +0100

    Add SubmitRule extension point
    The creation of custom rules to allow the sending of modification
    presents major disadvantages: the rules must be written in Prolog,
    which is harder to master than other options, and not as modular as it
    could be.
    
    By providing an interface that plugins can use to implement custom
    presubmit rules, these disadvantages should disappear: Gerrit plugins
    have full access to the core systems, but also to external resources.
    Everything that was possible with Prolog should be possible, and easier
    to achieve.
    
    Plugins can implement this interface and react however they want: a
    plugin can for instance define a Prolog rule evaluator, while an other
    hardcodes the rules in the Java code.
    
    Further commits will be responsible for using the interface, defining
    explicitely it's final behavior and implementing the interface (for
    both Prolog and custom plugins).
    
    A similar change was proposed by Saša Živkov during the Gerrit
    Hackathon 2016, cf [1] and the change Ifd5d2a in particular.
    The main difference between our changes lies in the design.
    The purpose of this change (here) is to allow the use of multiple
    validation plugins, so that one can handle the OWNERS file while an
    other checks the Labels, for instance.
    I truely believe plugin composition is important for the pre submit
    rules, as it allows smaller modular plugins while allowing greater
    power to the end user.
    Unfortunately, it also makes it very hard, if not impossible, to
    correctly handle "custom submit types". Thus, Prolog will be able to
    change it (backward compatibility) but new plugins won't have this
    option for now.
    (The issue here being that multiple plugins may not come to an
    agreement on what submit type should be used).
    
    Our SubmitRule interfaces look similar, with two slight differences:
    - We don't encourage throwing exceptions. Plugins should handle this
    case.
    - We don't pass a second argument "SubmitRulesFlag"
    
    It is also important to note that the second commit of this change
    introduces SubmitRequirements, allowing plugin authors to improve the
    interactions with the users, replacing the strange error messages
    like "Needs label: Author-Is-Maxime" with explanatory messages.
    
    The third commit introduces a change that is only required because
    of plugin composition: a change can only be submitted if all the
    plugins agree that it can be.
    
    In the third change, Saša Živkov implemented the LabelFunctions
    in Java. I think this is a good idea and plan to do the same.
    
    [1]: https://gerrit-review.googlesource.com/q/topic:non-prolog-submit-rules-gh16
    Change-Id: Ic0731321eb9d182ddbfa27d7c08eaeea9f3155e5

---
+++ java/com/google/gerrit/server/rules/SubmitRule.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.gerrit.server.rules;
+
+import com.google.gerrit.common.data.SubmitRecord;
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+import com.google.gerrit.server.query.change.ChangeData;
+import java.util.Collection;
+
+/**
+ * Allows plugins to decide whether a change is ready to be submitted or not.
+ *
+ * <p>For a given {@link ChangeData}, each plugin is called and returns a {@link Collection} of
+ * {@link SubmitRecord}. This collection can be empty, or contain one or several values.
+ *
+ * <p>A Change can only be submitted if all the plugins give their consent.
+ *
+ * <p>Each {@link SubmitRecord} represents a decision made by the plugin. If the plugin rejects a
+ * change, it should hold valuable informations to help the end user understand and correct the
+ * blocking points.
+ *
+ * <p>It should be noted that each plugin can handle rules inheritance.
+ *
+ * <p>This interface should be used to write pre-submit validation rules. This includes both simple
+ * checks, coded in Java, and more complex fully fledged expression evaluators (think: Prolog,
+ * JavaCC, or even JavaScript rules).
+ */
+@ExtensionPoint
+public interface SubmitRule {
+  /** Returns a {@link Collection} of {@link SubmitRecord} status for the change. */
+  Collection<SubmitRecord> evaluate(ChangeData changeData);
+}

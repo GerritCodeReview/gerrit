a72281d2f964f588f9502cddd5580860a4afabad
Viktor Kaufman <viktor.kaufman@sap.com>
2018-03-02 18:13:24 +0100

    Permit plugins to implement AllAuthenticatedRequestFilter
    Plugins extending AllAuthenticatedRequestFilter class and binding it as
    a member of the DynamicSet can filter HTTP requests, after the requests
    have been authenticated. The latter condition distinguishes the class
    from the otherwise similar AllRequestFilter. It is important for access
    control plugins etc. that need to rely on the Gerrit user information.
    
    Change-Id: I601da04695449fea961dddf6d1c3c3c05742a66a

--- java/com/google/gerrit/pgm/Daemon.java
+++ java/com/google/gerrit/pgm/Daemon.java
@@ -24,6 +24,7 @@ import com.google.gerrit.common.Nullable;
 import com.google.gerrit.elasticsearch.ElasticIndexModule;
 import com.google.gerrit.extensions.client.AuthType;
 import com.google.gerrit.gpg.GpgModule;
+import com.google.gerrit.httpd.AllAuthenticatedRequestFilter;
 import com.google.gerrit.httpd.AllRequestFilter;
 import com.google.gerrit.httpd.GetUserFilter;
 import com.google.gerrit.httpd.GitOverHttpModule;
@@ -588,6 +589,7 @@ public class Daemon extends SiteProgram {
       modules.add(new OAuthModule());
     }
     modules.add(sysInjector.getInstance(GetUserFilter.Module.class));
+    modules.add(AllAuthenticatedRequestFilter.module());
 
     // StaticModule contains a "/*" wildcard, place it last.
     modules.add(sysInjector.getInstance(StaticModule.class));

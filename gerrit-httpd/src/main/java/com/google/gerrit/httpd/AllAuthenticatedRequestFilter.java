12ef96b62ae889462d9ff36509ad6013767a472f
Viktor Kaufman <viktor.kaufman@sap.com>
2018-03-02 18:13:24 +0100

    Permit plugins to implement AllAuthenticatedRequestFilter
    Plugins extending AllAuthenticatedRequestFilter class and binding it as
    a member of the DynamicSet can filter HTTP requests, after the requests
    have been authenticated. The latter condition distinguishes the class
    from the otherwise similar AllRequestFilter. It is important for access
    control plugins etc. that need to rely on the Gerrit user information.
    
    Change-Id: I601da04695449fea961dddf6d1c3c3c05742a66a

---
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/AllAuthenticatedRequestFilter.java
@@ -0,0 +1,56 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd;
+
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.plugins.StopPluginListener;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.inject.internal.UniqueAnnotations;
+import com.google.inject.servlet.ServletModule;
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+
+/** Filters HTTP requests after authentication by the server. */
+public abstract class AllAuthenticatedRequestFilter implements Filter {
+  public static ServletModule module() {
+    return new ServletModule() {
+      @Override
+      protected void configureServlets() {
+        DynamicSet.setOf(binder(), AllAuthenticatedRequestFilter.class);
+        filter("/*").through(FilterProxy.class);
+
+        bind(StopPluginListener.class)
+            .annotatedWith(UniqueAnnotations.create())
+            .to(FilterProxy.class);
+      }
+    };
+  }
+
+  @Singleton
+  static class FilterProxy extends FilterProxyType<AllAuthenticatedRequestFilter> {
+    @Inject
+    FilterProxy(DynamicSet<AllAuthenticatedRequestFilter> filters) {
+      super(filters);
+    }
+  }
+
+  @Override
+  public void init(FilterConfig config) throws ServletException {}
+
+  @Override
+  public void destroy() {}
+}

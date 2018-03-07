3f1fe249ac38b9c37b1a2f50ad07af4ba79e2871
Becky Siegel <beckysiegel@google.com>
2018-03-07 15:44:42 -0800

    Add sorting to gr-thread-list
    Order as follows:
    - Unresolved threads with drafts (reverse chronological)
    - Unresolved threads without drafts (reverse chronological)
    - Resolved threads with drafts (reverse chronological)
    - Resolved threads without drafts (reverse chronological)
    
    Change-Id: I031eb948c5d330023463833fc6b7219541fc0112

--- polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list.js
+++ polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list.js
@@ -21,10 +21,67 @@
       change: Object,
       threads: Array,
       changeNum: String,
-      ignoreChanges: {
-        type: Boolean,
-        value: false,
-      },
+      _sortedThreads: Array,
+    },
+
+    observers: ['_computeSortedThreads(threads.*)'],
+
+    /**
+     * Order as follows:
+     *  - Unresolved threads with drafts (reverse chronological)
+     *  - Unresolved threads without drafts (reverse chronological)
+     *  - Resolved threads with drafts (reverse chronological)
+     *  - Resolved threads without drafts (reverse chronological)
+     * @param {!Array} threads
+     * @return {!Array}
+     */
+    _computeSortedThreads(threads) {
+      // In order to not trigger sub-property updates upon reordering, first
+      // reset everything so that the previous values of properties such as
+      // unresolved are undefined, and thus don't trigger an unwanted save
+      // event.
+      this._sortedThreads = [];
+      Polymer.dom.flush();
+      this._sortedThreads = this.threads.map(t =>
+          this.updateThreadProperties(t)).sort((c1, c2) => {
+            const c1Date = c1.__date || util.parseDate(c1.updated);
+            const c2Date = c2.__date || util.parseDate(c2.updated);
+            const dateCompare = c2Date - c1Date;
+            if (c2.unresolved || c1.unresolved) {
+              if (!c1.unresolved) { return 1; }
+              if (!c2.unresolved) { return -1; }
+            }
+            if (c2.hasDraft || c1.hasDraft) {
+              if (!c1.hasDraft) { return 1; }
+              if (!c2.hasDraft) { return -1; }
+            }
+
+            if (dateCompare === 0 && (!c1.id || !c1.id.localeCompare)) {
+              return 0;
+            }
+            return dateCompare ? dateCompare : c1.id.localeCompare(c2.id);
+          });
+    },
+
+    updateThreadProperties(thread) {
+      const lastComment = thread.comments[thread.comments.length - 1] || {};
+      thread.unresolved = !!lastComment.unresolved;
+      thread.hasDraft = !!lastComment.__draft;
+      thread.updated = lastComment.updated;
+      return thread;
+    },
+
+    _handleRemoveThread(e) {
+      this.removeThread(e.detail.rootId);
+    },
+
+    removeThread(rootId) {
+      for (let i = 0; i < this.threads.length; i++) {
+        if (this.threads[i].rootId === rootId) {
+          this.splice('threads', i, 1);
+          return;
+        }
+      }
     },
 
     _isOnParent(side) {

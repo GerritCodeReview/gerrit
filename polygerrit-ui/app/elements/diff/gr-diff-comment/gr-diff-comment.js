23d9584b265308cf1d947e55765f3e74f33893b0
Becky Siegel <beckysiegel@google.com>
2018-03-08 17:29:09 -0800

    Update gr-diff-comment unresolved toggle
    This changes updates the unresolved toggle so that it only attempts to
    save when explicitly tapped (as opposed to the value of resolved
    changing on its own).
    
    This was causing issues with comment synchronization between
    gr-thread-view and diff comments, and also re-rendering the order of
    comments in gr-thread-list within the dom-repeat.
    
    Change-Id: I6065c745a0eabcf19df38f3cd6fd2c0d7e7434f3

--- polygerrit-ui/app/elements/diff/gr-diff-comment/gr-diff-comment.js
+++ polygerrit-ui/app/elements/diff/gr-diff-comment/gr-diff-comment.js
@@ -113,10 +113,7 @@
       },
       commentSide: String,
 
-      resolved: {
-        type: Boolean,
-        observer: '_toggleResolved',
-      },
+      resolved: Boolean,
 
       _numPendingDraftRequests: {
         type: Object,
@@ -583,15 +580,11 @@
       this.resolved = !this.resolved;
     },
 
-    _toggleResolved(resolved, previousValue) {
-      // Do not proceed if this call is for the initial definition of the
-      // resolved property.
-      if (previousValue === undefined) { return; }
-
+    _maybeHandleSaveResolved(e) {
       // Modify payload instead of this.comment, as this.comment is passed from
       // the parent by ref.
       const payload = this._getEventPayload();
-      payload.comment.unresolved = !resolved;
+      payload.comment.unresolved = !this.$.resolvedCheckbox.checked;
       this.fire('comment-update', payload);
       if (!this.editing) {
         // Save the resolved state immediately.

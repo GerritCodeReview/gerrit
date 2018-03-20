eb8324873fad6913261372d18a434df63c36d495
Edwin Kempin <ekempin@google.com>
2018-03-20 11:41:45 +0100

    GroupBundle.Factory#fromReviewDb: Require UUID as input instead of ID
    This will make it easier to use this method from the schema migration
    that implements the migration of Gerrit groups to NoteDb.
    
    Change-Id: Ic15d54c240998796bb5e8ad91a8b8144674c8af0
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/group/db/GroupBundle.java
+++ java/com/google/gerrit/server/group/db/GroupBundle.java
@@ -153,9 +153,10 @@ public abstract class GroupBundle {
           auditLogReader.getSubgroupsAudit(repo, uuid));
     }
 
-    public static GroupBundle fromReviewDb(ReviewDb db, AccountGroup.Id groupId)
+    public static GroupBundle fromReviewDb(ReviewDb db, AccountGroup.UUID groupUuid)
         throws OrmException {
-      AccountGroup group = readAccountGroupFromReviewDb(db, groupId);
+      AccountGroup group = readAccountGroupFromReviewDb(db, groupUuid);
+      AccountGroup.Id groupId = group.getId();
 
       return create(
           Source.REVIEW_DB,
@@ -166,26 +167,26 @@ public abstract class GroupBundle {
           readAccountGroupSubgroupAuditsFromReviewDb(db, groupId));
     }
 
-    private static AccountGroup readAccountGroupFromReviewDb(ReviewDb db, AccountGroup.Id groupId)
-        throws OrmException {
+    private static AccountGroup readAccountGroupFromReviewDb(
+        ReviewDb db, AccountGroup.UUID groupUuid) throws OrmException {
       try (Statement stmt = ((JdbcSchema) db).getConnection().createStatement();
           ResultSet rs =
               stmt.executeQuery(
-                  "SELECT group_uuid,"
+                  "SELECT group_id,"
                       + " name,"
                       + " created_on,"
                       + " description,"
                       + " owner_group_uuid,"
                       + " visible_to_all"
                       + " FROM account_groups"
-                      + " WHERE group_id = '"
-                      + groupId.get()
+                      + " WHERE group_uuid = '"
+                      + groupUuid.get()
                       + "'")) {
         if (!rs.next()) {
-          throw new OrmException(String.format("Group %s not found", groupId));
+          throw new OrmException(String.format("Group %s not found", groupUuid));
         }
 
-        AccountGroup.UUID groupUuid = new AccountGroup.UUID(rs.getString(1));
+        AccountGroup.Id groupId = new AccountGroup.Id(rs.getInt(1));
         AccountGroup.NameKey groupName = new AccountGroup.NameKey(rs.getString(2));
         Timestamp createdOn = rs.getTimestamp(3);
         String description = rs.getString(4);
@@ -204,7 +205,7 @@ public abstract class GroupBundle {
         return group;
       } catch (SQLException e) {
         throw new OrmException(
-            String.format("Failed to read account group %s from ReviewDb", groupId.get()), e);
+            String.format("Failed to read account group %s from ReviewDb", groupUuid.get()), e);
       }
     }
 

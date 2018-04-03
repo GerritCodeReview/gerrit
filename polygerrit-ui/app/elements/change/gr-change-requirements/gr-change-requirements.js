5c06ea699a2baaa72f5c9c4b359a0bb39790e9bc
Maxime Guerreiro <maximeg@google.com>
2018-04-03 14:14:44 +0000

    Display Submit Requirements in the changes sidebar
    Add a "Submit Status" category to display Submit Requirements (defined
    by plugins), required labels and wether the change is a Work in Progress
    or not.
    
    Remove the "Label Status" section from the sidebar.
    
    Change-Id: I408f876597be41e8b0bae36bf6277a693775055b

---
+++ polygerrit-ui/app/elements/change/gr-change-requirements/gr-change-requirements.js
@@ -0,0 +1,103 @@
+/**
+ * @license
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-change-requirements',
+
+    properties: {
+      /** @type {?} */
+      change: Object,
+      requirements: {
+        type: Array,
+        computed: '_computeRequirements(change)',
+      },
+      labels: {
+        type: Array,
+        computed: '_computeLabels(change)',
+      },
+      _showWip: {
+        type: Boolean,
+        computed: '_computeShowWip(change)',
+      },
+      _showLabels: {
+        type: Boolean,
+        computed: '_computeShowLabelStatus(change)',
+      },
+    },
+
+    behaviors: [
+      Gerrit.RESTClientBehavior,
+    ],
+
+    _computeShowLabelStatus(change) {
+      const isNewChange = change.status === this.ChangeStatus.NEW;
+      return isNewChange;
+    },
+
+    _computeShowWip(change) {
+      return change.work_in_progress;
+    },
+
+    _computeRequirements(change) {
+      const _requirements = [];
+
+      if (change.requirements) {
+        for (const requirement of change.requirements) {
+          requirement.satisfied = requirement.status === 'OK';
+          _requirements.push(requirement);
+        }
+      }
+
+      return _requirements;
+    },
+
+    _computeLabels(change) {
+      const labels = change.labels;
+      const _labels = [];
+
+      for (const label in labels) {
+        if (!labels.hasOwnProperty(label)) { continue; }
+        const obj = labels[label];
+        if (obj.optional) { continue; }
+
+        const status = this._computeRequirementStatus(obj.approved);
+        const style = this._computeRequirementClass(obj.approved);
+        _labels.push({label, status, style});
+      }
+
+      return _labels;
+    },
+
+    _computeRequirementClass(requirementStatus) {
+      if (requirementStatus) {
+        return 'satisfied';
+      } else {
+        return 'unsatisfied';
+      }
+    },
+
+    _computeRequirementStatus(requirementStatus) {
+      if (requirementStatus) {
+        return '✓';
+      } else {
+        return '✕';
+      }
+    },
+  });
+})();

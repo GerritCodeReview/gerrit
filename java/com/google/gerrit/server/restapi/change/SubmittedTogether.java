2da291ea0c8411759b847172585e1a3b8c1d427c
Patrick Hiesel <hiesel@google.com>
2018-04-06 16:16:19 +0200

    Remove WalkSorter from SubmittedTogether
    We've improved the speed of /submitted_together inside Google recently
    and found two bottlenecks on the API layer resulting from opening every
    repo that we return changes for. For large topics like the one Android
    uses, this can take >1 minute.
    
    Change I763d1544 removed a default option that required lazy loading in
    ChangeJson. This commit is removing the last bit: WalkSorter which opens
    all of the repos to perform a topo-sort.
    
    We don't guarantee any sorting in the API docs for /submitted_together
    and sorting by project and numeric ID (hence change creation time)
    sounds like a reasonable and *much* faster alternative.
    
    Change-Id: I1155b936c19d31b1b1b58ff51f9e73aa4445779d

--- java/com/google/gerrit/server/restapi/change/SubmittedTogether.java
+++ java/com/google/gerrit/server/restapi/change/SubmittedTogether.java
@@ -28,8 +28,6 @@ import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.change.ChangeJson;
 import com.google.gerrit.server.change.ChangeResource;
-import com.google.gerrit.server.change.WalkSorter;
-import com.google.gerrit.server.change.WalkSorter.PatchSetData;
 import com.google.gerrit.server.permissions.PermissionBackendException;
 import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.query.change.InternalChangeQuery;
@@ -41,6 +39,7 @@ import com.google.inject.Provider;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.List;
 import org.kohsuke.args4j.Option;
@@ -56,11 +55,13 @@ public class SubmittedTogether implements RestReadView<ChangeResource> {
   private final EnumSet<ListChangesOption> jsonOpt =
       EnumSet.of(ListChangesOption.CURRENT_REVISION, ListChangesOption.SUBMITTABLE);
 
+  private static final Comparator<ChangeData> COMPARATOR =
+      Comparator.comparing(ChangeData::project).thenComparing(cd -> cd.getId().id);
+
   private final ChangeJson.Factory json;
   private final Provider<ReviewDb> dbProvider;
   private final Provider<InternalChangeQuery> queryProvider;
   private final Provider<MergeSuperSet> mergeSuperSet;
-  private final Provider<WalkSorter> sorter;
 
   private boolean lazyLoad = false;
 
@@ -89,13 +90,11 @@ public class SubmittedTogether implements RestReadView<ChangeResource> {
       ChangeJson.Factory json,
       Provider<ReviewDb> dbProvider,
       Provider<InternalChangeQuery> queryProvider,
-      Provider<MergeSuperSet> mergeSuperSet,
-      Provider<WalkSorter> sorter) {
+      Provider<MergeSuperSet> mergeSuperSet) {
     this.json = json;
     this.dbProvider = dbProvider;
     this.queryProvider = queryProvider;
     this.mergeSuperSet = mergeSuperSet;
-    this.sorter = sorter;
   }
 
   public SubmittedTogether addListChangesOption(EnumSet<ListChangesOption> o) {
@@ -143,13 +142,8 @@ public class SubmittedTogether implements RestReadView<ChangeResource> {
         throw new AuthException("change would be submitted with a change that you cannot see");
       }
 
-      if (cds.size() <= 1 && hidden == 0) {
-        cds = Collections.emptyList();
-      } else {
-        // Skip sorting for singleton lists, to avoid WalkSorter opening the
-        // repo just to fill out the commit field in PatchSetData.
-        cds = sort(cds);
-      }
+      cds = new ArrayList<>(cds);
+      Collections.sort(cds, COMPARATOR);
 
       SubmittedTogetherInfo info = new SubmittedTogetherInfo();
       info.changes = json.create(jsonOpt).lazyLoad(lazyLoad).formatChangeDatas(cds);
@@ -159,13 +153,5 @@ public class SubmittedTogether implements RestReadView<ChangeResource> {
       log.error("Error on getting a ChangeSet", e);
       throw e;
     }
-  }
-
-  private List<ChangeData> sort(List<ChangeData> cds) throws OrmException, IOException {
-    List<ChangeData> sorted = new ArrayList<>(cds.size());
-    for (PatchSetData psd : sorter.get().sort(cds)) {
-      sorted.add(psd.data());
-    }
-    return sorted;
   }
 }

41de83993eee188cce670cda4c0f2d3060449dc6
Wyatt Allen <wyatta@google.com>
2018-04-13 15:28:36 -0700

    Show submitted together table in confirm submit dialog
    The submitted together list is requested again at the time that the
    confirm dialog is shown. The wording is adjusted to be more informative
    and positive.
    
    Change-Id: I0acb1455066986d1c41d4985480595c894133653

--- polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.js
+++ polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.js
@@ -376,6 +376,7 @@
         type: Boolean,
         value: true,
       },
+      submittedTogether: Array,
     },
 
     ActionType,
@@ -925,13 +926,21 @@
           this._handleDownloadTap();
           break;
         case RevisionActions.SUBMIT:
-          if (!this._canSubmitChange()) { return; }
-          this._showActionDialog(this.$.confirmSubmitDialog);
+          this._showSubmitDialog();
           break;
         default:
           this._fireAction(this._prependSlash(key),
               this.revisionActions[key], true);
       }
+    },
+
+    _showSubmitDialog() {
+      if (!this._canSubmitChange()) { return; }
+      this.$.confirmSubmitDialog.load().then(() => {
+        Polymer.dom.flush();
+        this.$.overlay.refit();
+      });
+      this._showActionDialog(this.$.confirmSubmitDialog);
     },
 
     _prependSlash(key) {
@@ -1405,6 +1414,14 @@
 
     _computeHasIcon(action) {
       return action.icon ? '' : 'hidden';
+    },
+
+    _computeShowSubmittedTogether(list) {
+      return !!list.length;
+    },
+
+    _getChangeUrl(change) {
+      return Gerrit.Nav.getUrlForChange(change);
     },
   });
 })();

c9b7ba83f04831ede988aaaa925378dd4ecd4411
Gustaf Lundh <gustaflh@axis.com>
2018-04-16 10:56:03 +0200

    CommandLinkProvider: Support reloaded configurations
    Change-Id: Ibac61e1cac2974a8c07d0fbb5fab4b6332a8a0d2

--- java/com/google/gerrit/server/project/CommentLinkProvider.java
+++ java/com/google/gerrit/server/project/CommentLinkProvider.java
@@ -17,27 +17,30 @@ package com.google.gerrit.server.project;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.gerrit.extensions.api.projects.CommentLinkInfo;
+import com.google.gerrit.server.config.ConfigUpdatedEvent;
+import com.google.gerrit.server.config.GerritConfigListener;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.Singleton;
 import java.util.List;
 import java.util.Set;
 import org.eclipse.jgit.lib.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CommentLinkProvider implements Provider<List<CommentLinkInfo>> {
+@Singleton
+public class CommentLinkProvider implements Provider<List<CommentLinkInfo>>, GerritConfigListener {
   private static final Logger log = LoggerFactory.getLogger(CommentLinkProvider.class);
 
-  private final Config cfg;
+  private volatile List<CommentLinkInfo> commentLinks;
 
   @Inject
   CommentLinkProvider(@GerritServerConfig Config cfg) {
-    this.cfg = cfg;
+    this.commentLinks = parseConfig(cfg);
   }
 
-  @Override
-  public List<CommentLinkInfo> get() {
+  private List<CommentLinkInfo> parseConfig(Config cfg) {
     Set<String> subsections = cfg.getSubsections(ProjectConfig.COMMENTLINK);
     List<CommentLinkInfo> cls = Lists.newArrayListWithCapacity(subsections.size());
     for (String name : subsections) {
@@ -53,5 +56,18 @@ public class CommentLinkProvider implements Provider<List<CommentLinkInfo>> {
       }
     }
     return ImmutableList.copyOf(cls);
+  }
+
+  @Override
+  public List<CommentLinkInfo> get() {
+    return commentLinks;
+  }
+
+  @Override
+  public void configUpdated(ConfigUpdatedEvent event) {
+    if (event.isSectionUpdated(ProjectConfig.COMMENTLINK)) {
+      commentLinks = parseConfig(event.getNewConfig());
+      event.accept(ProjectConfig.COMMENTLINK);
+    }
   }
 }

GerritCodeReview/gerrit

commit 40744d99ff2d237cb43653bb4efa740009cb8b1d

lib/elasticsearch: remove unnecessary dependencies
Remove the hereby no longer used ones also from libs thus WORKSPACE.

Bug: Issue 6094
Change-Id: Icb0ee8dd89ae30c1256ae60481eb782798f252ef

WORKSPACE
@@ -459,12 +459,6 @@ maven_jar(
 )
 
 maven_jar(
-    name = "lucene_codecs",
-    artifact = "org.apache.lucene:lucene-codecs:" + LUCENE_VERS,
-    sha1 = "e01fe463d9490bb1b4a6a168e771f7b7255a50b1",
-)
-
-maven_jar(
     name = "backward_codecs",
     artifact = "org.apache.lucene:lucene-backward-codecs:" + LUCENE_VERS,
     sha1 = "c5cfcd7a8cf48a0144b61fb991c8e50a0bf868d5",
@@ -498,12 +492,6 @@ maven_jar(
     name = "lucene_memory",
     artifact = "org.apache.lucene:lucene-memory:" + LUCENE_VERS,
     sha1 = "7409db9863d8fbc265c27793c6cc7511304182c2",
-)
-
-maven_jar(
-    name = "lucene_sandbox",
-    artifact = "org.apache.lucene:lucene-sandbox:" + LUCENE_VERS,
-    sha1 = "30a91f120706ba66732d5a974b56c6971b3c8a16",
 )
 
 maven_jar(
@@ -964,24 +952,12 @@ maven_jar(
     sha1 = "84ccf145ac2215e6bfa63baa3101c0af41017cfc",
 )
 
-maven_jar(
-    name = "jna",
-    artifact = "net.java.dev.jna:jna:4.1.0",
-    sha1 = "1c12d070e602efd8021891cdd7fd18bc129372d4",
-)
-
 JACKSON_VERSION = "2.6.6"
 
 maven_jar(
     name = "jackson_core",
     artifact = "com.fasterxml.jackson.core:jackson-core:" + JACKSON_VERSION,
     sha1 = "02eb801df67aacaf5b1deb4ac626e1964508e47b",
-)
-
-maven_jar(
-    name = "jackson_dataformat_smile",
-    artifact = "com.fasterxml.jackson.dataformat:jackson-dataformat-smile:" + JACKSON_VERSION,
-    sha1 = "ccbfc948748ed2754a58c1af9e0a02b5cc1aed69",
 )
 
 maven_jar(

lib/elasticsearch/BUILD
@@ -7,20 +7,15 @@ java_library(
     runtime_deps = [
         ":compress-lzf",
         ":hppc",
-        ":jna",
         ":jsr166e",
         ":netty",
         ":t-digest",
         "//lib/jackson:jackson-core",
         "//lib/jackson:jackson-dataformat-cbor",
-        "//lib/jackson:jackson-dataformat-smile",
-        "//lib/joda:joda-time",
-        "//lib/lucene:lucene-codecs",
         "//lib/lucene:lucene-highlighter",
         "//lib/lucene:lucene-join",
         "//lib/lucene:lucene-memory",
         "//lib/lucene:lucene-queries",
-        "//lib/lucene:lucene-sandbox",
         "//lib/lucene:lucene-spatial",
         "//lib/lucene:lucene-suggest",
     ],
@@ -59,10 +54,4 @@ java_library(
     data = ["//lib:LICENSE-Apache2.0"],
     visibility = ["//lib/elasticsearch:__pkg__"],
     exports = ["@t_digest//jar"],
-)
-
-java_library(
-    name = "jna",
-    data = ["//lib:LICENSE-Apache2.0"],
-    exports = ["@jna//jar"],
 )

lib/jackson/BUILD
@@ -9,12 +9,6 @@ java_library(
 )
 
 java_library(
-    name = "jackson-dataformat-smile",
-    data = ["//lib:LICENSE-Apache2.0"],
-    exports = ["@jackson_dataformat_smile//jar"],
-)
-
-java_library(
     name = "jackson-dataformat-cbor",
     data = ["//lib:LICENSE-Apache2.0"],
     exports = ["@jackson_dataformat_cbor//jar"],

lib/lucene/BUILD
@@ -23,13 +23,6 @@ java_library(
 )
 
 java_library(
-    name = "lucene-codecs",
-    data = ["//lib:LICENSE-Apache2.0"],
-    visibility = ["//visibility:public"],
-    exports = ["@lucene_codecs//jar"],
-)
-
-java_library(
     name = "lucene-core",
     data = ["//lib:LICENSE-Apache2.0"],
     visibility = ["//visibility:public"],
@@ -68,12 +61,6 @@ java_library(
     name = "lucene-memory",
     data = ["//lib:LICENSE-Apache2.0"],
     exports = ["@lucene_memory//jar"],
-)
-
-java_library(
-    name = "lucene-sandbox",
-    data = ["//lib:LICENSE-Apache2.0"],
-    exports = ["@lucene_sandbox//jar"],
 )
 
 java_library(

commit 31c040a2339545caeebd56ace11c7526f1f8f465

lib/elasticsearch: restore jackson_dataformat_smile
As removing it in 40744d99f introduced a related NoClassDefFoundError
regression.

Bug: Issue 6094
Change-Id: I03c4801ae8c1cfb0b74ad39ef1398cd31225ba14

WORKSPACE
@@ -967,6 +967,12 @@ maven_jar(
 )
 
 maven_jar(
+    name = "jackson_dataformat_smile",
+    artifact = "com.fasterxml.jackson.dataformat:jackson-dataformat-smile:" + JACKSON_VERSION,
+    sha1 = "ccbfc948748ed2754a58c1af9e0a02b5cc1aed69",
+)
+
+maven_jar(
     name = "httpasyncclient",
     artifact = "org.apache.httpcomponents:httpasyncclient:4.1.2",
     sha1 = "95aa3e6fb520191a0970a73cf09f62948ee614be",

lib/elasticsearch/BUILD
@@ -12,6 +12,7 @@ java_library(
         ":t-digest",
         "//lib/jackson:jackson-core",
         "//lib/jackson:jackson-dataformat-cbor",
+        "//lib/jackson:jackson-dataformat-smile",
         "//lib/lucene:lucene-highlighter",
         "//lib/lucene:lucene-join",
         "//lib/lucene:lucene-memory",

lib/jackson/BUILD
@@ -13,3 +13,9 @@ java_library(
     data = ["//lib:LICENSE-Apache2.0"],
     exports = ["@jackson_dataformat_cbor//jar"],
 )
+
+java_library(
+    name = "jackson-dataformat-smile",
+    data = ["//lib:LICENSE-Apache2.0"],
+    exports = ["@jackson_dataformat_smile//jar"],
+)
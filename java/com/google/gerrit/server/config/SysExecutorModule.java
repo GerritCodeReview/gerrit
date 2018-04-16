340502564a417b7f6268530fe3677486dca28a1e
Patrick Hiesel <hiesel@hiesel-macbookpro2.roam.corp.google.com>
2018-04-16 12:04:02 +0200

    Parallelize ChangeJson#toChangeInfos
    99%ile latency of QueryChanges is between 20 seconds and
    80 seconds on googlesource.com depending on the time of
    day. There are two main reasons for that:
    1) Performing operations that require loading ChangeNotes
    2) Performing operations that require opening the repo
    3) Filling accounts with a cold AccountCache
    
    This commit parallelizes formatting the individual results
    to mitigate 1+2. 3 will be addressed by a different change
    that will make the AccountFiller parallelize, too.
    
    Change-Id: I36c6b92e31488ad001f5aea43efc837d31ba3021

--- java/com/google/gerrit/server/config/SysExecutorModule.java
+++ java/com/google/gerrit/server/config/SysExecutorModule.java
@@ -17,6 +17,7 @@ package com.google.gerrit.server.config;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.google.gerrit.server.change.ChangeJsonExecutor;
 import com.google.gerrit.server.git.WorkQueue;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
@@ -59,6 +60,18 @@ public class SysExecutorModule extends AbstractModule {
       return MoreExecutors.newDirectExecutorService();
     }
     return queues.createQueue(poolSize, "SendEmail");
+  }
+
+  @Provides
+  @Singleton
+  @ChangeJsonExecutor
+  public ExecutorService createChangeJsonExecutor(
+      @GerritServerConfig Config config, WorkQueue queues) {
+    int poolSize = config.getInt("change", "api", "formattingThreadPoolSize", 0);
+    if (poolSize == 0) {
+      return MoreExecutors.newDirectExecutorService();
+    }
+    return queues.createQueue(poolSize, "ChangeJson");
   }
 
   @Provides

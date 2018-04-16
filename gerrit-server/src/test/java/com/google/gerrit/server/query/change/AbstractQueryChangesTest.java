120cc281f0f7a3d739b2adce324d8c0d1a994104
David Pursehouse <dpursehouse@collab.net>
2018-04-16 13:20:59 +0200

    AbstractQueryChangesTest#submitRecords: Add coverage for submittable:closed
    Bug: Issue 8605
    Change-Id: Ifcb97475008b2f91901e800f706769333b2c47c0

--- gerrit-server/src/test/java/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
+++ gerrit-server/src/test/java/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
@@ -1748,6 +1748,10 @@ public abstract class AbstractQueryChangesTest extends GerritServerTests {
     // NEED records don't have associated users.
     assertQuery("label:CodE-RevieW=need,user1");
     assertQuery("label:CodE-RevieW=need,user");
+
+    gApi.changes().id(change1.getId().get()).current().submit();
+    assertQuery("submittable:ok");
+    assertQuery("submittable:closed", change1);
   }
 
   @Test

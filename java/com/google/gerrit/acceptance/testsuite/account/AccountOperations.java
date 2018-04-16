559f36054ecd4a8707c3bf981c7f57687cc5a7fc
Edwin Kempin <ekempin@google.com>
2018-04-16 16:10:18 +0200

    Add test API to create/update accounts
    The new API simplifies setting up account data for tests and makes the
    tests more readable.
    
    Only a few callers have been adapted to demonstrate the usage of the new
    API. Follow-up changes should adapt all tests to the new API and remove
    the AccountCreator and the old TestAccount class.
    
    Some methods in AbstractDaemonTest are duplicated for now. The old
    methods should be removed when all tests have been migrated to the new
    API.
    
    Ideally TestAccount shouldn't know about SSH specifics, but we didn’t
    find a good way to keep this concern separate yet.
    
    To execute account operations we directly use the internal API.
    One benefit of this is that we can also support operations like
    updates without index updates or deletions even though the REST or
    Java API don't allow them. Another benefit is that we bypass
    permissions. This means that tests need not adjust permissions just
    to create the correct test setup and thus hopefully will lead to
    simpler and more readable tests.
    
    Similar test APIs should be added to create/update groups, changes etc.
    As result of this the setup of test data will become more consistent.
    
    Change-Id: I8911660b027d09309879918543c063d51465b0bf
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ java/com/google/gerrit/acceptance/testsuite/account/AccountOperations.java
@@ -0,0 +1,75 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.testsuite.account;
+
+import com.google.gerrit.reviewdb.client.Account;
+import java.util.function.Consumer;
+
+/**
+ * An aggregation of operations on accounts for test purposes.
+ *
+ * <p>To execute the operations, no Gerrit permissions are necessary.
+ *
+ * <p><strong>Note:</strong> The methods of this class may only be used for creating/adjusting the
+ * desired test context. For testing a REST endpoint or a method of the Java API, directly call that
+ * endpoint/method.
+ */
+public interface AccountOperations {
+
+  /**
+   * Checks whether an account exists.
+   *
+   * @param accountId the ID of the account
+   * @return {@code true} if the account exists
+   */
+  boolean exists(Account.Id accountId) throws Exception;
+
+  /**
+   * Retrieves an account.
+   *
+   * <p><strong>Note:</strong> This call will fail with an exception if the requested account
+   * doesn't exist. If you want to check for the existence of an account, use {@link
+   * #exists(Account.Id)} instead.
+   *
+   * @param accountId the ID of the account
+   * @return the corresponding {@code TestAccount}
+   */
+  TestAccount get(Account.Id accountId) throws Exception;
+
+  /**
+   * Creates a new account.
+   *
+   * <p><strong>Note:</strong> If another account with the provided user name or preferred email
+   * address already exists, this call will fail.
+   *
+   * @param creation a {@code Consumer} to initialize the attributes of a new account
+   * @return the created {@code TestAccount}
+   */
+  TestAccount create(Consumer<TestAccountUpdate.Builder> creation) throws Exception;
+
+  /**
+   * Updates an existing account.
+   *
+   * <p><strong>Note:</strong> This call will fail with an exception if the account to update
+   * doesn't exist. If you want to check for the existence of an account, use {@link
+   * #exists(Account.Id)}.
+   *
+   * @param accountId the ID of the account
+   * @param update a {@code Consumer} to update the attributes of an account
+   * @return the updated {@code TestAccount}
+   */
+  TestAccount update(Account.Id accountId, Consumer<TestAccountUpdate.Builder> update)
+      throws Exception;
+}

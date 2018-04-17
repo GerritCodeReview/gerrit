8ca28fd17a93c171a85846868325c8415dd0a826
Han-Wen Nienhuys <hanwen@google.com>
2018-04-17 14:35:10 +0200

    introduce the viewAccess global capability.
    This capability can be assigned separately so users can run check.access.
    
    Change-Id: I29a7c8d637a606ce9f7cf353ba242b1c43c2682b

--- java/com/google/gerrit/server/permissions/GlobalPermission.java
+++ java/com/google/gerrit/server/permissions/GlobalPermission.java
@@ -50,7 +50,8 @@ public enum GlobalPermission implements GlobalOrPluginPermission {
   VIEW_CACHES,
   VIEW_CONNECTIONS,
   VIEW_PLUGINS,
-  VIEW_QUEUE;
+  VIEW_QUEUE,
+  VIEW_ACCESS;
 
   private static final Logger log = LoggerFactory.getLogger(GlobalPermission.class);
 

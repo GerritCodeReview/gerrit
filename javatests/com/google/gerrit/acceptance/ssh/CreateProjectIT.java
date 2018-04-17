31a266db0fe1ce099367755df5ee201395510452
Ardo Septama <aseptama@gmail.com>
2018-04-17 09:10:45 +0200

    Enable SshLog test for CreateProjectIT
    In addition to the normal test, validate the log produced by ssh
    command.
    
    Change-Id: If12dea29fe0d6d815fee4c6de3097e01b9d094b8
    Signed-off-by: Ardo Septama <aseptama@gmail.com>

--- javatests/com/google/gerrit/acceptance/ssh/CreateProjectIT.java
+++ javatests/com/google/gerrit/acceptance/ssh/CreateProjectIT.java
@@ -16,8 +16,11 @@ package com.google.gerrit.acceptance.ssh;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.Assert.assertTrue;
 
 import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.GerritConfig;
+import com.google.gerrit.acceptance.SshLogTestUtil;
 import com.google.gerrit.acceptance.UseSsh;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.project.ProjectState;
@@ -27,27 +30,33 @@ import org.junit.Test;
 public class CreateProjectIT extends AbstractDaemonTest {
 
   @Test
+  @GerritConfig(name = "sshd.requestLog", value = "true")
   public void withValidGroupName() throws Exception {
     String newGroupName = "newGroup";
     adminRestSession.put("/groups/" + newGroupName);
     String newProjectName = "newProject";
-    adminSshSession.exec(
-        "gerrit create-project --branch master --owner " + newGroupName + " " + newProjectName);
+    String command =
+        "gerrit create-project --branch master --owner " + newGroupName + " " + newProjectName;
+    adminSshSession.exec(command);
     assertWithMessage(adminSshSession.getError()).that(adminSshSession.hasError()).isFalse();
     ProjectState projectState = projectCache.get(new Project.NameKey(newProjectName));
     assertThat(projectState).isNotNull();
+    assertTrue(SshLogTestUtil.validateLog(server.getSitePath().toString(), command));
   }
 
   @Test
+  @GerritConfig(name = "sshd.requestLog", value = "true")
   public void withInvalidGroupName() throws Exception {
     String newGroupName = "newGroup";
     adminRestSession.put("/groups/" + newGroupName);
     String wrongGroupName = "newG";
     String newProjectName = "newProject";
-    adminSshSession.exec(
-        "gerrit create-project --branch master --owner " + wrongGroupName + " " + newProjectName);
+    String command =
+        "gerrit create-project --branch master --owner " + wrongGroupName + " " + newProjectName;
+    adminSshSession.exec(command);
     assertWithMessage(adminSshSession.getError()).that(adminSshSession.hasError()).isTrue();
     ProjectState projectState = projectCache.get(new Project.NameKey(newProjectName));
     assertThat(projectState).isNull();
+    assertTrue(SshLogTestUtil.validateLogWithRetry(server.getSitePath().toString(), command));
   }
 }

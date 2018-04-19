1ef8ee617201c5908cb946719bd1b02caf39893e
Dave Borowitz <dborowitz@google.com>
2018-04-19 10:39:14 +0200

    Factor KeyType singleton implementations into their own classes
    Change-Id: Ia27c1304e5c8bc19a5313508730aaf1a6a53f7f4

--- java/com/google/gerrit/server/cache/h2/ObjectKeyTypeImpl.java
+++ java/com/google/gerrit/server/cache/h2/ObjectKeyTypeImpl.java
@@ -15,9 +15,7 @@
 package com.google.gerrit.server.cache.h2;
 
 import com.google.common.hash.Funnel;
-import com.google.common.hash.Funnels;
 import com.google.common.hash.PrimitiveSink;
-import com.google.inject.TypeLiteral;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -26,40 +24,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
-class KeyTypeImpl<K> implements KeyType<K> {
-  private static final KeyType<?> OTHER = new KeyTypeImpl<>();
-  private static final KeyType<String> STRING =
-      new KeyTypeImpl<String>() {
-        @Override
-        public String columnType() {
-          return "VARCHAR(4096)";
-        }
-
-        @Override
-        public String get(ResultSet rs, int col) throws SQLException {
-          return rs.getString(col);
-        }
-
-        @Override
-        public void set(PreparedStatement ps, int col, String value) throws SQLException {
-          ps.setString(col, value);
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public Funnel<String> funnel() {
-          Funnel<?> s = Funnels.unencodedCharsFunnel();
-          return (Funnel<String>) s;
-        }
-      };
-
-  @SuppressWarnings("unchecked")
-  static <K> KeyType<K> create(TypeLiteral<K> type) {
-    if (type.getRawType() == String.class) {
-      return (KeyType<K>) STRING;
-    }
-    return (KeyType<K>) OTHER;
-  }
+enum ObjectKeyTypeImpl implements KeyType<Object> {
+  INSTANCE;
 
   @Override
   public String columnType() {
@@ -67,23 +33,22 @@ class KeyTypeImpl<K> implements KeyType<K> {
   }
 
   @Override
-  @SuppressWarnings("unchecked")
-  public K get(ResultSet rs, int col) throws SQLException {
-    return (K) rs.getObject(col);
+  public Object get(ResultSet rs, int col) throws SQLException {
+    return rs.getObject(col);
   }
 
   @Override
-  public void set(PreparedStatement ps, int col, K key) throws SQLException {
+  public void set(PreparedStatement ps, int col, Object key) throws SQLException {
     ps.setObject(col, key, Types.JAVA_OBJECT);
   }
 
   @Override
-  public Funnel<K> funnel() {
-    return new Funnel<K>() {
+  public Funnel<Object> funnel() {
+    return new Funnel<Object>() {
       private static final long serialVersionUID = 1L;
 
       @Override
-      public void funnel(K from, PrimitiveSink into) {
+      public void funnel(Object from, PrimitiveSink into) {
         try (ObjectOutputStream ser = new ObjectOutputStream(new SinkOutputStream(into))) {
           ser.writeObject(from);
           ser.flush();

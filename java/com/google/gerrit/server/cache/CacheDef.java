6eb6fe0d10a97d430c9a3f56b8e851ad938da295
Dave Borowitz <dborowitz@google.com>
2018-04-20 15:35:18 +0200

    Split setter/getter interfaces out of CacheBinding
    The CacheBinding interface now contains only setters. With "binding" in
    the name, it indicates that this is the thing used from within
    CacheModule to configure the interface.
    
    Move all the getter methods into a new interface, CacheDef. This is the
    type that is passed into {Memory,Persistent}CacheFactory, where the
    cache setup internals need to query the configured values, but calling
    setters at this point would be programmer error.
    
    This has the side advantage of eliminating the need for throwing
    RuntimeExceptions from H2CacheBindingProxy, and it is now clearer that
    this is simply a delegate for the getter half of the interface.
    
    Both of these interfaces are still implemented by the same CacheProvider
    class, and it still retains the freeze-during-get behavior. It may be
    possible to split it up, for example converting it to a normal builder
    which produces a read-only CacheDef from its build method, but I wasn't
    able to get that working with the asEagerSingleton binding magic used by
    the current implementation.
    
    Change-Id: I5c90f6eb17d257a127b2c52d70558c2f80655ec2

---
+++ java/com/google/gerrit/server/cache/CacheDef.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.cache;
+
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.Weigher;
+import com.google.gerrit.common.Nullable;
+import com.google.inject.TypeLiteral;
+import java.util.concurrent.TimeUnit;
+
+public interface CacheDef<K, V> {
+  String name();
+
+  /**
+   * Key to use when looking up configuration for this cache.
+   *
+   * <p>Typically, this will match the result of {@link #name()}, so that configuration is keyed by
+   * the actual cache name. However, it may be changed, for example to reuse the size limits of some
+   * other cache.
+   */
+  String configKey();
+
+  TypeLiteral<K> keyType();
+
+  TypeLiteral<V> valueType();
+
+  long maximumWeight();
+
+  long diskLimit();
+
+  @Nullable
+  Long expireAfterWrite(TimeUnit unit);
+
+  @Nullable
+  Weigher<K, V> weigher();
+
+  @Nullable
+  CacheLoader<K, V> loader();
+}

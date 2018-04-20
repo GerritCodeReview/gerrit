6914e9e962038fd1a8812d5bdc34347fcb26dbf8
Alice Kober-Sotzek <aliceks@google.com>
2018-04-20 10:46:06 +0200

    Extend test API for groups and add tests for it
    With this change, the test API for groups allows to update all
    properties of internal groups which are modifiable at the moment. In
    addition, all properties which may be specified upon group creation are
    supported.
    
    To ensure that the test API for groups is working as expected, tests for
    it are added. This means that other tests which use this API do not need
    to verify themselves that the desired state of the system was obtained.
    
    Change-Id: Iae7547e0f972d8d1eb0c96f8cc25dc701c0c633f

--- java/com/google/gerrit/acceptance/testsuite/group/TestGroupUpdate.java
+++ java/com/google/gerrit/acceptance/testsuite/group/TestGroupUpdate.java
@@ -15,27 +15,110 @@
 package com.google.gerrit.acceptance.testsuite.group;
 
 import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import com.google.gerrit.acceptance.testsuite.ThrowingConsumer;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountGroup;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
 
 @AutoValue
 public abstract class TestGroupUpdate {
 
+  public abstract Optional<String> name();
+
   public abstract Optional<String> description();
+
+  public abstract Optional<AccountGroup.UUID> ownerGroupUuid();
+
+  public abstract Optional<Boolean> visibleToAll();
+
+  public abstract Function<ImmutableSet<Account.Id>, Set<Account.Id>> memberModification();
+
+  public abstract Function<ImmutableSet<AccountGroup.UUID>, Set<AccountGroup.UUID>>
+      subgroupModification();
 
   abstract ThrowingConsumer<TestGroupUpdate> groupUpdater();
 
   public static Builder builder(ThrowingConsumer<TestGroupUpdate> groupUpdater) {
-    return new AutoValue_TestGroupUpdate.Builder().groupUpdater(groupUpdater);
+    return new AutoValue_TestGroupUpdate.Builder()
+        .groupUpdater(groupUpdater)
+        .memberModification(in -> in)
+        .subgroupModification(in -> in);
   }
 
   @AutoValue.Builder
   public abstract static class Builder {
 
+    public abstract Builder name(String name);
+
     public abstract Builder description(String description);
 
     public Builder clearDescription() {
       return description("");
+    }
+
+    public abstract Builder ownerGroupUuid(AccountGroup.UUID ownerGroupUUID);
+
+    public abstract Builder visibleToAll(boolean visibleToAll);
+
+    abstract Builder memberModification(
+        Function<ImmutableSet<Account.Id>, Set<Account.Id>> memberModification);
+
+    abstract Function<ImmutableSet<Account.Id>, Set<Account.Id>> memberModification();
+
+    public Builder clearMembers() {
+      return memberModification(originalMembers -> ImmutableSet.of());
+    }
+
+    public Builder addMember(Account.Id member) {
+      Function<ImmutableSet<Account.Id>, Set<Account.Id>> previousModification =
+          memberModification();
+      memberModification(
+          originalMembers ->
+              Sets.union(previousModification.apply(originalMembers), ImmutableSet.of(member)));
+      return this;
+    }
+
+    public Builder removeMember(Account.Id member) {
+      Function<ImmutableSet<Account.Id>, Set<Account.Id>> previousModification =
+          memberModification();
+      memberModification(
+          originalMembers ->
+              Sets.difference(
+                  previousModification.apply(originalMembers), ImmutableSet.of(member)));
+      return this;
+    }
+
+    abstract Builder subgroupModification(
+        Function<ImmutableSet<AccountGroup.UUID>, Set<AccountGroup.UUID>> subgroupModification);
+
+    abstract Function<ImmutableSet<AccountGroup.UUID>, Set<AccountGroup.UUID>>
+        subgroupModification();
+
+    public Builder clearSubgroups() {
+      return subgroupModification(originalMembers -> ImmutableSet.of());
+    }
+
+    public Builder addSubgroup(AccountGroup.UUID subgroup) {
+      Function<ImmutableSet<AccountGroup.UUID>, Set<AccountGroup.UUID>> previousModification =
+          subgroupModification();
+      subgroupModification(
+          originalSubgroups ->
+              Sets.union(previousModification.apply(originalSubgroups), ImmutableSet.of(subgroup)));
+      return this;
+    }
+
+    public Builder removeSubgroup(AccountGroup.UUID subgroup) {
+      Function<ImmutableSet<AccountGroup.UUID>, Set<AccountGroup.UUID>> previousModification =
+          subgroupModification();
+      subgroupModification(
+          originalSubgroups ->
+              Sets.difference(
+                  previousModification.apply(originalSubgroups), ImmutableSet.of(subgroup)));
+      return this;
     }
 
     abstract Builder groupUpdater(ThrowingConsumer<TestGroupUpdate> groupUpdater);

805baa86d0bcb3e28c11540d28324ff97b62a7d6
Dave Borowitz <dborowitz@google.com>
2018-04-20 11:41:23 +0200

    Move default memory cache implementation out of H2 package
    The memory cache doesn't depend on H2 at all, and this separation of
    packages makes it harder to reintroduce the kind of source-level
    dependency that was removed in I570fd54adf.
    
    Change-Id: Ie23e39cd7ea9a0813a914e65614b889c248342ae

--- java/com/google/gerrit/pgm/Daemon.java
+++ java/com/google/gerrit/pgm/Daemon.java
@@ -55,7 +55,8 @@ import com.google.gerrit.server.account.InternalAccountDirectory;
 import com.google.gerrit.server.api.GerritApiModule;
 import com.google.gerrit.server.api.PluginApiModule;
 import com.google.gerrit.server.cache.CacheOverrides;
-import com.google.gerrit.server.cache.h2.DefaultCacheFactory;
+import com.google.gerrit.server.cache.h2.H2CacheModule;
+import com.google.gerrit.server.cache.mem.DefaultMemoryCacheModule;
 import com.google.gerrit.server.change.ChangeCleanupRunner;
 import com.google.gerrit.server.config.AuthConfig;
 import com.google.gerrit.server.config.AuthConfigModule;
@@ -423,8 +424,8 @@ public class Daemon extends SiteProgram {
     modules.add(new SearchingChangeCacheImpl.Module(slave));
     modules.add(new InternalAccountDirectory.Module());
     modules.add(new DefaultPermissionBackendModule());
-    modules.add(new DefaultCacheFactory.MemoryCacheModule());
-    modules.add(new DefaultCacheFactory.PersistentCacheModule());
+    modules.add(new DefaultMemoryCacheModule());
+    modules.add(new H2CacheModule());
     modules.add(cfgInjector.getInstance(MailReceiver.Module.class));
     if (emailModule != null) {
       modules.add(emailModule);

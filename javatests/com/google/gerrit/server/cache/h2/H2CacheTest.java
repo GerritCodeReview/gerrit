7ae7fb1cc0984032016fe6ce1fa49b9735a98ed8
Dave Borowitz <dborowitz@google.com>
2018-04-21 11:25:41 -0400

    Allow alternative serialization strategies for persistent caches
    We want to have finer control over when persistent cache data is
    invalidated due to the stored format changing. Depending on the Java
    serialVersionUID, as we have done in the past, has two significant
    downsides:
     * Adding new fields which can have a reasonable default, or removing
       old obsolete fields, always requires bumping the serialVersionUID and
       thus flushing the cache. With an alternative serialization strategy
       like protobuf, we can continue reading old entries in some cases.
     * Trivial refactorings which do not change the data semantics, such as
       renaming classes or fields, also flush the cache.
    
    Both of these types of cache flushes have happened to PatchListCache in
    the past year. Other serialization strategies such as protocol buffers
    allows us to avoid unnecessary cache invalidations.
    
    This change introduces a new interface CacheSerializer, which is used to
    convert arbitrary objects to and from a simple byte array. This is
    backwards compatible with the existing OTHER column type in H2, which is
    implemented as a byte array, and accepts arbitrary data written with
    PreparedStatement#setBytes and similar, not just #setObject.
    
    Each SqlStore instance has a pair of CacheSerializers for the key and
    value types. However, note that when the key type is String, the
    existing StringKeyTypeImpl does *not* use a CacheSerializer for
    serialization, since unlike the OTHER column type, the VARCHAR type
    cannot just take a simple byte array. In the long term, it may make
    sense to drop the KeyType class entirely, and instead use the
    CacheSerializer to convert to bytes even for strings.
    
    Change-Id: I150c1c8b8cd35b98c1a0792572d2e1ded4172774

--- javatests/com/google/gerrit/server/cache/h2/H2CacheTest.java
+++ javatests/com/google/gerrit/server/cache/h2/H2CacheTest.java
@@ -14,13 +14,13 @@
 
 package com.google.gerrit.server.cache.h2;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static com.google.common.truth.Truth.assertThat;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.util.concurrent.MoreExecutors;
+import com.google.gerrit.server.cache.CacheSerializer;
 import com.google.gerrit.server.cache.h2.H2CacheImpl.SqlStore;
 import com.google.gerrit.server.cache.h2.H2CacheImpl.ValueHolder;
 import com.google.inject.TypeLiteral;
@@ -32,44 +32,78 @@ import org.junit.Test;
 public class H2CacheTest {
   private static int dbCnt;
 
-  private Cache<String, ValueHolder<Boolean>> mem;
-  private H2CacheImpl<String, Boolean> impl;
+  private Cache<String, ValueHolder<String>> mem;
+  private H2CacheImpl<String, String> impl;
 
   @Before
   public void setUp() {
     mem = CacheBuilder.newBuilder().build();
 
     TypeLiteral<String> keyType = new TypeLiteral<String>() {};
-    SqlStore<String, Boolean> store =
-        new SqlStore<>("jdbc:h2:mem:Test_" + (++dbCnt), keyType, 1 << 20, 0);
+    SqlStore<String, String> store =
+        new SqlStore<>(
+            "jdbc:h2:mem:Test_" + (++dbCnt),
+            keyType,
+            StringSerializer.INSTANCE,
+            StringSerializer.INSTANCE,
+            1 << 20,
+            0);
     impl = new H2CacheImpl<>(MoreExecutors.directExecutor(), store, keyType, mem);
   }
 
   @Test
   public void get() throws ExecutionException {
-    assertNull(impl.getIfPresent("foo"));
+    assertThat(impl.getIfPresent("foo")).isNull();
 
-    final AtomicBoolean called = new AtomicBoolean();
-    assertTrue(
-        impl.get(
-            "foo",
-            () -> {
-              called.set(true);
-              return true;
-            }));
-    assertTrue("used Callable", called.get());
-    assertTrue("exists in cache", impl.getIfPresent("foo"));
+    AtomicBoolean called = new AtomicBoolean();
+    assertThat(
+            impl.get(
+                "foo",
+                () -> {
+                  called.set(true);
+                  return "bar";
+                }))
+        .isEqualTo("bar");
+    assertThat(called.get()).named("Callable was called").isTrue();
+    assertThat(impl.getIfPresent("foo")).named("in-memory value").isEqualTo("bar");
     mem.invalidate("foo");
-    assertTrue("exists on disk", impl.getIfPresent("foo"));
+    assertThat(impl.getIfPresent("foo")).named("persistent value").isEqualTo("bar");
 
     called.set(false);
-    assertTrue(
-        impl.get(
-            "foo",
-            () -> {
-              called.set(true);
-              return true;
-            }));
-    assertFalse("did not invoke Callable", called.get());
+    assertThat(
+            impl.get(
+                "foo",
+                () -> {
+                  called.set(true);
+                  return "baz";
+                }))
+        .named("cached value")
+        .isEqualTo("bar");
+    assertThat(called.get()).named("Callable was called").isFalse();
+  }
+
+  @Test
+  public void stringSerializer() {
+    String input = "foo";
+    byte[] serialized = StringSerializer.INSTANCE.serialize(input);
+    assertThat(serialized).isEqualTo(new byte[] {'f', 'o', 'o'});
+    assertThat(StringSerializer.INSTANCE.deserialize(serialized)).isEqualTo(input);
+  }
+
+  // TODO(dborowitz): Won't be necessary when we use a real StringSerializer in the server code.
+  private enum StringSerializer implements CacheSerializer<String> {
+    INSTANCE;
+
+    @Override
+    public byte[] serialize(String object) {
+      return object.getBytes(UTF_8);
+    }
+
+    @Override
+    public String deserialize(byte[] in) {
+      // TODO(dborowitz): Consider using CharsetDecoder directly in the real implementation, to get
+      // checked exceptions.
+      return new String(in, UTF_8);
+    }
   }
 }

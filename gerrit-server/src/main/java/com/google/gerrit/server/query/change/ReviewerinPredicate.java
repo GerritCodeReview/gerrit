e4b7ccc393a21018298ab1f4d59e33ae53209ab4
David Pursehouse <dpursehouse@collab.net>
2018-04-25 21:06:19 +0900

    ReviewerinPredicate: Only return users in state REVIEWER
    On NoteDb, reviewer and CC are separate so the query result should not
    include users that were added as CC. However under ReviewDb there is
    no distinction between reviewer and CC so the results should include
    users that were added as CC.
    
    Expand the reviewerin test to cover this.
    
    Bug: Issue 8850
    Change-Id: I9f1d53d9ae6aef1e2c10da586951fb7d76978151
    (cherry picked from commit 5dd0174832489f29e9587931c5bd9e4e3f3c5742)

--- gerrit-server/src/main/java/com/google/gerrit/server/query/change/ReviewerinPredicate.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/query/change/ReviewerinPredicate.java
@@ -17,6 +17,7 @@ package com.google.gerrit.server.query.change;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.AccountGroup;
 import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.notedb.ReviewerStateInternal;
 import com.google.gwtorm.server.OrmException;
 
 public class ReviewerinPredicate extends ChangeOperatorPredicate {
@@ -35,7 +36,7 @@ public class ReviewerinPredicate extends ChangeOperatorPredicate {
 
   @Override
   public boolean match(ChangeData object) throws OrmException {
-    for (Account.Id accountId : object.reviewers().all()) {
+    for (Account.Id accountId : object.reviewers().byState(ReviewerStateInternal.REVIEWER)) {
       IdentifiedUser reviewer = userFactory.create(accountId);
       if (reviewer.getEffectiveGroups().contains(uuid)) {
         return true;

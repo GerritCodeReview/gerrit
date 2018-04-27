64d5c7cb792b56b17656ce437ee9dddaaacedb78
Maxime Guerreiro <maximeg@google.com>
2018-04-27 13:31:26 +0000

    Improve String operations
    Don't call #toString on strings.
    Don't use + in StringBuilder#append-s methods.
    
    Change-Id: I4e0f99ea417aabb72ecaaab170c66eb434d3ad37

--- javatests/com/google/gerrit/server/mail/receive/MailHeaderParserTest.java
+++ javatests/com/google/gerrit/server/mail/receive/MailHeaderParserTest.java
@@ -65,11 +65,11 @@ public class MailHeaderParserTest {
     b.subject("");
 
     StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append(MailHeader.CHANGE_NUMBER.withDelimiter() + "123\r\n");
-    stringBuilder.append("> " + MailHeader.PATCH_SET.withDelimiter() + "1\n");
-    stringBuilder.append(MailHeader.MESSAGE_TYPE.withDelimiter() + "comment\n");
-    stringBuilder.append(
-        MailHeader.COMMENT_DATE.withDelimiter() + "Tue, 25 Oct 2016 02:11:35 -0700\r\n");
+    stringBuilder.append(MailHeader.CHANGE_NUMBER.withDelimiter()).append("123\r\n");
+    stringBuilder.append("> ").append(MailHeader.PATCH_SET.withDelimiter()).append("1\n");
+    stringBuilder.append(MailHeader.MESSAGE_TYPE.withDelimiter()).append("comment\n");
+    stringBuilder.append(MailHeader.COMMENT_DATE.withDelimiter())
+        .append("Tue, 25 Oct 2016 02:11:35 -0700\r\n");
     b.textContent(stringBuilder.toString());
 
     Address author = new Address("Diffy", "test@gerritcodereview.com");
@@ -97,15 +97,13 @@ public class MailHeaderParserTest {
     b.subject("");
 
     StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append(
-        "<div id\"someid\">" + MailHeader.CHANGE_NUMBER.withDelimiter() + "123</div>");
-    stringBuilder.append("<div>" + MailHeader.PATCH_SET.withDelimiter() + "1</div>");
-    stringBuilder.append("<div>" + MailHeader.MESSAGE_TYPE.withDelimiter() + "comment</div>");
-    stringBuilder.append(
-        "<div>"
-            + MailHeader.COMMENT_DATE.withDelimiter()
-            + "Tue, 25 Oct 2016 02:11:35 -0700"
-            + "</div>");
+    stringBuilder.append("<div id\"someid\">").append(MailHeader.CHANGE_NUMBER.withDelimiter())
+        .append("123</div>");
+    stringBuilder.append("<div>").append(MailHeader.PATCH_SET.withDelimiter()).append("1</div>");
+    stringBuilder.append("<div>").append(MailHeader.MESSAGE_TYPE.withDelimiter())
+        .append("comment</div>");
+    stringBuilder.append("<div>").append(MailHeader.COMMENT_DATE.withDelimiter())
+        .append("Tue, 25 Oct 2016 02:11:35 -0700").append("</div>");
     b.htmlContent(stringBuilder.toString());
 
     Address author = new Address("Diffy", "test@gerritcodereview.com");

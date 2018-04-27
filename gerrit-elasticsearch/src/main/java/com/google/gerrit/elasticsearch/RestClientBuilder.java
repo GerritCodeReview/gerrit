98553c79faf9982ad9fbe9070ae26ef94f3504f5
Marco Miller <marco.miller@ericsson.com>
2018-04-27 17:23:46 -0400

    Elasticsearch: replace native API in prod w/ REST
    Remove jest API which depends on Elasticsearch native API (also
    removed), so no longer depend on lucene for this scope. Replace such
    removed dependencies with the Elasticsearch low-level API, which does
    not depend on Lucene. That now used API is a REST client with minimal
    dependencies, indeed.
    
    Do so in order to decouple the elasticsearch client from lucene in
    Gerrit. That REST client does not logically require Lucene anyway. Most
    importantly, such decoupling now enables upcoming support for more than
    just one Elasticsearch server version. This includes the new possibility
    of bumping the latter to multiple yet later versions, such as 5 and 6.x.
    The currently used version (2.4) should also still be kept, for a while.
    
    Doing the same for the elasticsearch tests in Gerrit is to be done using
    another change or more changes.
    
    Bug: Issue 6094
    Change-Id: I720c9885c9eab2388acc328eecb9eaa6940ced0c

---
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/RestClientBuilder.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.elasticsearch;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+
+@Singleton
+class RestClientBuilder {
+  private final ElasticConfiguration cfg;
+
+  @Inject
+  RestClientBuilder(ElasticConfiguration cfg) {
+    this.cfg = cfg;
+  }
+
+  RestClient build() {
+    HttpHost[] hosts = cfg.urls.toArray(new HttpHost[cfg.urls.size()]);
+    return RestClient.builder(hosts).build();
+  }
+}

4001780c29d99f87c50c662e5f7665af1ca2d0b4
Inderjot Kaur Ratol <inderjot.kaur.ratol@ericsson.com>
2018-05-01 13:31:23 -0400

    Fix the online index for changes when uprading ES gerrit site
    Previously, when using ElasticSearch as main index engine and
    upgrading the gerrit site from 2.14 to 2.15, the changes were not
    visible in the UI even when OnlineIndexer logs showed that indexing
    was successful.
    
    Reason: After migration, new indices were never created with the custom
    mappings and elasticsearch created the default mappings with the
    standard fields used in the indices. This resulted in a mismatch between
    the mapping types and therefore changes were not visible in the UI.
    
    For example:
    
    The "changes" index in gerrit use two types of mappings - "open_changes"
    and "closed_changes". When no index was created by gerrit and
    onlineIndexer was called while running gerrit with the migrated site,
    elasticsearch created only "open_changes" mapping type resulting in
    conflicts between two different versions of the same indices.
    
    This change resolves the inconsistencies between offline and online
    reindex by deleting all the previously existing indices and creating the
    new ones.
    
    Bug: Issue 8806
    Change-Id: I51ee5bfeb2a154630955b3c94b78fda7d050610d

--- gerrit-server/src/main/java/com/google/gerrit/server/index/OnlineReindexer.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/index/OnlineReindexer.java
@@ -63,6 +63,8 @@ public class OnlineReindexer<K, V, I extends Index<K, V>> {
               try {
                 reindex();
                 ok = true;
+              } catch (IOException e) {
+                log.error("Failed to reindex the changes online. Cause: {}", e);
               } finally {
                 running.set(false);
                 if (!ok) {
@@ -91,7 +93,7 @@ public class OnlineReindexer<K, V, I extends Index<K, V>> {
     return i.getSchema().getVersion();
   }
 
-  private void reindex() {
+  private void reindex() throws IOException {
     for (OnlineUpgradeListener listener : listeners) {
       listener.onStart(name, oldVersion, newVersion);
     }
@@ -106,6 +108,11 @@ public class OnlineReindexer<K, V, I extends Index<K, V>> {
         name,
         version(indexes.getSearchIndex()),
         version(index));
+
+    if (oldVersion != newVersion) {
+      index.markReady(false);
+      index.deleteAll();
+    }
     SiteIndexer.Result result = batchIndexer.indexAll(index);
     if (!result.success()) {
       log.error(

bffc14109fad0a52ec80ed54f94ecaf406c8b14f
Wyatt Allen <wyatta@google.com>
2018-04-30 18:24:45 -0700

    Cache diff requests
    Because a diff for a given change, (non-edit) patch range and path does
    not change over time, it can be cached. The exception is when the patch
    range indicates 'edit' in either the base or the revision: in these
    cases the request is uncached as before.
    
    Bug: Issue 8115
    Change-Id: I1fe4c51459594ee17cc7cac76d2eb62a4748ef3c

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -815,8 +815,9 @@
     /**
      * @param {string} url
      * @param {function(?Response, string=)=} opt_errFn
+     * @param {?Object=} opt_params URL params, key-value hash.
      */
-    _fetchSharedCacheURL(url, opt_errFn) {
+    _fetchSharedCacheURL(url, opt_errFn, opt_params) {
       if (this._sharedFetchPromises[url]) {
         return this._sharedFetchPromises[url];
       }
@@ -824,7 +825,8 @@
       if (this._cache[url] !== undefined) {
         return Promise.resolve(this._cache[url]);
       }
-      this._sharedFetchPromises[url] = this.fetchJSON(url, opt_errFn)
+      this._sharedFetchPromises[url] = this.fetchJSON(
+          url, opt_errFn, undefined, opt_params)
           .then(response => {
             if (response !== undefined) {
               this._cache[url] = response;
@@ -1589,10 +1591,8 @@
      * @param {number|string} patchNum
      * @param {string} path
      * @param {function(?Response, string=)=} opt_errFn
-     * @param {function()=} opt_cancelCondition
      */
-    getDiff(changeNum, basePatchNum, patchNum, path,
-        opt_errFn, opt_cancelCondition) {
+    getDiff(changeNum, basePatchNum, patchNum, path, opt_errFn) {
       const params = {
         context: 'ALL',
         intraline: null,
@@ -1605,8 +1605,18 @@
       }
       const endpoint = `/files/${encodeURIComponent(path)}/diff`;
 
-      return this._getChangeURLAndFetch(changeNum, endpoint, patchNum,
-          opt_errFn, opt_cancelCondition, params);
+      // If either patchset is an edit, then it should not be cached, since an
+      // edits can change w/r/t their URL.
+      if (this.patchNumEquals(patchNum, this.EDIT_NAME) ||
+          this.patchNumEquals(basePatchNum, this.EDIT_NAME)) {
+        return this._getChangeURLAndFetch(changeNum, endpoint, patchNum,
+            opt_errFn, undefined, params);
+      }
+
+      // Otherwise, request the diff through the cache, because non-edit diffs
+      // will be stable respective to their URL.
+      return this._changeBaseURL(changeNum, patchNum).then(url =>
+          this._fetchSharedCacheURL(url + endpoint, opt_errFn, params));
     },
 
     /**

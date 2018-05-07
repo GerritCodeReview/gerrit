f8252dc1b525f0c4af2978015376657e4b4b8305
Patrick Hiesel <hiesel@google.com>
2018-05-07 14:59:04 +0200

    Enforce a maximum PER_THREAD_CACHE_SIZE in PerThreadCache
    When calling operations that create a lot of ProjectControls (e.g.
    ListProjects) we observe OOMs in our Gerrit jobs from time to time. This
    commit puts a max. limit on the cache size to prevent this from
    happening.
    
    The approach implemented is not an LRU. On purpose, we want to keep
    PerThreadCache simple and don't add a dependency on e.g. Guava's LRU.
    
    Change-Id: Ic2f6e11719b52ee2112f72698e887cbf8ca254d0

--- javatests/com/google/gerrit/server/cache/PerThreadCacheTest.java
+++ javatests/com/google/gerrit/server/cache/PerThreadCacheTest.java
@@ -83,4 +83,20 @@ public class PerThreadCacheTest {
       PerThreadCache.create();
     }
   }
+
+  @Test
+  public void enforceMaxSize() {
+    try (PerThreadCache cache = PerThreadCache.create()) {
+      // Fill the cache
+      for (int i = 0; i < 50; i++) {
+        PerThreadCache.Key<String> key = PerThreadCache.Key.create(String.class, i);
+        cache.get(key, () -> "cached value");
+      }
+      // Assert that the value was not persisted
+      PerThreadCache.Key<String> key = PerThreadCache.Key.create(String.class, 1000);
+      cache.get(key, () -> "new value");
+      String value3 = cache.get(key, () -> "directly served");
+      assertThat(value3).isEqualTo("directly served");
+    }
+  }
 }

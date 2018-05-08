75c291df85d5ff36db90d9bde02aaa32944fc60a
Kasper Nilsson <kaspern@google.com>
2018-05-08 14:31:54 -0700

    Improve file list performance
    The _setReviewedFiles function in the file list was causing significant
    slowdown. It was responsible for managing the isReviewed flag on both
    the _files array and the _shownFiles array. It contained a call to
    this.set on _shownFiles to ensure that any toggling of the reviewed
    state was reflected in the UI.
    
    With this change, the isReviewed flag is now added as a step in the
    computation of the _files array, preventing the need for the heavy
    this.set operation. Toggling the reviewed state of a file is now done by
    modifying the file objects directly, instead of modifying the _reviewed
    array and updating the file arrays via an observer.
    
    ----------
    
    Test with 1001 files
    
    Without patch: 6.4s (roughly 4.9s scripting, 1.5s rendering)
    With patch: 2.5s (roughly 1.2s scripting, 1.3s rendering)
    ~4x improvement in scripting time
    
    ----------
    
    Test with 10001 files (extreme case)
    
    Without patch: 112s (roughly 100s scripting, 12s rendering)
    With patch: 30s (roughly 18s scripting, 12s rendering)
    ~5.5x improvement in scripting time
    
    Change-Id: I9a166dfb48a457bc79417bc0bb37c826313244af

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
@@ -101,7 +101,8 @@
       _filesByPath: Object,
       _files: {
         type: Array,
-        computed: '_computeFiles(_filesByPath, changeComments, patchRange)',
+        computed: '_computeFiles(_filesByPath, changeComments, patchRange, ' +
+            '_reviewed)',
         observer: '_filesChanged',
         value() { return []; },
       },
@@ -180,7 +181,6 @@
 
     observers: [
       '_expandedPathsChanged(_expandedFilePaths.splices)',
-      '_setReviewedFiles(_shownFiles, _files, _reviewed.*, _loggedIn)',
     ],
 
     keyBindings: {
@@ -421,18 +421,14 @@
       return GrCountStringFormatter.computeShortString(commentCount, 'c');
     },
 
-    _computeReviewed(file, _reviewed) {
-      return _reviewed.includes(file.__path);
-    },
-
     _reviewFile(path) {
       if (this.editMode) { return; }
-      const index = this._reviewed.indexOf(path);
-      const reviewed = index !== -1;
-      if (reviewed) {
-        this.splice('_reviewed', index, 1);
-      } else {
-        this.push('_reviewed', path);
+      const index = this._files.findIndex(file => file.__path === path);
+      const reviewed = this._files[index].isReviewed;
+
+      this.set(['_files', index, 'isReviewed'], !reviewed);
+      if (index < this._shownFiles.length) {
+        this.set(['_shownFiles', index, 'isReviewed'], !reviewed);
       }
 
       this._saveReviewedState(path, !reviewed);
@@ -772,7 +768,7 @@
           'gr-icons:expand-less' : 'gr-icons:expand-more';
     },
 
-    _computeFiles(filesByPath, changeComments, patchRange) {
+    _computeFiles(filesByPath, changeComments, patchRange, reviewed) {
       const commentedPaths = changeComments.getPaths(patchRange);
       const files = Object.assign({}, filesByPath);
       Object.keys(commentedPaths).forEach(commentedPath => {
@@ -781,6 +777,12 @@
         }
         files[commentedPath] = {status: 'U'};
       });
+      const reviewedSet = new Set(reviewed || []);
+      for (const filePath in files) {
+        if (!files.hasOwnProperty(filePath)) { continue; }
+        files[filePath].isReviewed = reviewedSet.has(filePath);
+      }
+
       return this._normalizeChangeFilesResponse(files);
     },
 
@@ -788,19 +790,6 @@
       const filesShown = files.base.slice(0, numFilesShown);
       this.fire('files-shown-changed', {length: filesShown.length});
       return filesShown;
-    },
-
-    _setReviewedFiles(shownFiles, files, reviewedRecord, loggedIn) {
-      if (!loggedIn) { return; }
-      const reviewed = reviewedRecord.base;
-      let fileReviewed;
-      for (let i = 0; i < files.length; i++) {
-        fileReviewed = this._computeReviewed(files[i], reviewed);
-        this._files[i].isReviewed = fileReviewed;
-        if (i < shownFiles.length) {
-          this.set(['_shownFiles', i, 'isReviewed'], fileReviewed);
-        }
-      }
     },
 
     _updateDiffCursor() {

cc94ab153ef975475429cd1f5c663c3e92dc3f38
Dave Borowitz <dborowitz@google.com>
2018-05-08 21:24:24 +0200

    Move default memory cache implementation out of H2 package
    The memory cache doesn't depend on H2 at all, and this separation of
    packages makes it harder to reintroduce the kind of source-level
    dependency that was removed in I570fd54adf.
    
    Change-Id: Ie23e39cd7ea9a0813a914e65614b889c248342ae

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/util/BatchProgramModule.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/util/BatchProgramModule.java
@@ -38,7 +38,8 @@ import com.google.gerrit.server.account.GroupCacheImpl;
 import com.google.gerrit.server.account.GroupIncludeCacheImpl;
 import com.google.gerrit.server.account.Realm;
 import com.google.gerrit.server.cache.CacheRemovalListener;
-import com.google.gerrit.server.cache.h2.DefaultCacheFactory;
+import com.google.gerrit.server.cache.h2.H2CacheModule;
+import com.google.gerrit.server.cache.mem.DefaultMemoryCacheModule;
 import com.google.gerrit.server.change.ChangeJson;
 import com.google.gerrit.server.change.ChangeKindCacheImpl;
 import com.google.gerrit.server.change.MergeabilityCacheImpl;
@@ -148,8 +149,8 @@ public class BatchProgramModule extends FactoryModule {
     factory(ProjectControl.AssistedFactory.class);
 
     install(new BatchGitModule());
-    install(new DefaultCacheFactory.MemoryCacheModule());
-    install(new DefaultCacheFactory.PersistentCacheModule());
+    install(new DefaultMemoryCacheModule());
+    install(new H2CacheModule());
     install(new GroupModule());
     install(new NoteDbModule(cfg));
     install(new PrologModule());

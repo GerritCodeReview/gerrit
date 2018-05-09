0d17315e34ce9cd5d8a05b2d8ae23c585dea3591
Inderjot Kaur Ratol <inderjot.kaur.ratol@ericsson.com>
2018-05-09 15:48:41 -0400

    Encryption API: add encryption/decryption capability for secrets
    Added a new functionality which allows plugins to leverage the
    encryption/decryption capability in Gerrit core. It takes a plaintext
    secret, generates a random salt, a random string and secure key to
    encrypt the secret. The encoded secure key is then saved in the
    `secure.config` file. Below is an example showing a saved key in
    `secure.config`:
    
    [encryption]
      key = 19dws7ao0KlIqpj4h1mhXQ==
    
    The key is created by using random string and a random salt in the
    beginning. The same encoded key is used to decode any encrypted
    passwords passed to this class. Note that, the generation of salt and
    the secure key happens only once.
    
    The default algorithm used for encryption is AES with the key length
    256. These parameters are configurable with `gerrit.config` file and the
    example configuration is shown below:
    
    [encryption]
      keyLength = 256
      iterations = 1000
      algorithm = AES
      hashAlgorithm = PBKDF2WithHmacSHA512
      transformation = AES/CBC/PKCS5Padding
    
    This change implements its own vanilla symmetric key encryption as the
    existing algorithms used in Gerrit is one-way hashing of passwords
    which is not intended to be used when the original plaintext secret
    needs to be recovered.
    
    Change-Id: Ia195e5b8bcfd73422e54323ff8116e3b91c094fc

---
+++ javatests/com/google/gerrit/server/api/config/StringEncryptorTest.java
@@ -0,0 +1,64 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.api.config;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.server.api.config.EncryptionConfig.ALGORITHM;
+import static com.google.gerrit.server.api.config.EncryptionConfig.TRANSFORMATION;
+import static org.easymock.EasyMock.createNiceMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+
+import javax.crypto.spec.SecretKeySpec;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringEncryptorTest {
+
+  private static final String PLAINTEXT_PASSWORD = "password_to_test";
+  private static final String ENCRYPTION_KEY = "19dws7ao0KlIqpj4h1mhXQ==";
+
+  private StringEncryptor encryptor;
+
+  @Before
+  public void setUp() throws Exception {
+    KeyManager manager = createNiceMock(KeyManager.class);
+    EncryptionConfig config = createNiceMock(EncryptionConfig.class);
+    expect(config.algorithm()).andReturn(ALGORITHM).anyTimes();
+    expect(config.transformation()).andReturn(TRANSFORMATION).anyTimes();
+    replay(config);
+
+    expect(manager.getSecretKey()).andReturn(ENCRYPTION_KEY);
+    expect(manager.getSecretKeySpec())
+        .andReturn(new SecretKeySpec(KeyManager.base64Decode(ENCRYPTION_KEY), config.algorithm()))
+        .anyTimes();
+    replay(manager);
+
+    encryptor = new StringEncryptor(config, manager);
+  }
+
+  @Test
+  public void testEncrypt() {
+    String encrypted = encryptor.encrypt(PLAINTEXT_PASSWORD);
+    assertThat(encrypted).isNotEqualTo(PLAINTEXT_PASSWORD);
+  }
+
+  @Test
+  public void testDecrypt() {
+    String encrypted = encryptor.encrypt(PLAINTEXT_PASSWORD);
+    String plaintext = encryptor.decrypt(encrypted);
+    assertThat(plaintext).isEqualTo(PLAINTEXT_PASSWORD);
+  }
+}

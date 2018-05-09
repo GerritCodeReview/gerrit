d104884a61ae717dea75d25fd2c4f66a4d97ba6a
Patrick Hiesel <hiesel@google.com>
2018-05-09 11:43:43 +0200

    Improve JavaDoc of PerThreadCache
    PerThreadCache needs to be used with care and callers should think
    deeply about the use cases before calling it. This commits improves the
    JavaDoc and removes the unnecessary get method from the API.
    
    This is a follow on review comments from Ic2f6e117.
    
    Change-Id: I8af37b20bbb9b9aacfb6f53bbdc8957ecd6d3e50

--- java/com/google/gerrit/server/cache/PerThreadCache.java
+++ java/com/google/gerrit/server/cache/PerThreadCache.java
@@ -19,7 +19,6 @@ import static com.google.common.base.Preconditions.checkState;
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
-import com.google.gerrit.common.Nullable;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -30,6 +29,16 @@ import java.util.function.Supplier;
  * the current request and potentially need to be instantiated multiple times while serving a
  * request.
  *
+ * <p><strong>Warning:</strong> This class must be used with absolute care. Before thinking about
+ * using it, check to see if there is another way of achieving the caching behavior you are looking
+ * for. Typical questions to ask in this design: Instead of caching the heavy object here, can the
+ * code be refactored to cache smaller state in a regular cache? Can we cut down on the
+ * instantiation cost of the object? In case the answer to these questions is <i>no</i>, make sure
+ * you understand the implications of caching an object here: The cached objects will be retained
+ * until the request is completed. Given the size of the object that should be newly cached, how
+ * much memory will this use at maximum? It is OK to use this class, once these concerns are
+ * addressed.
+ *
  * <p>This is different from the key-value storage in {@code CurrentUser}: {@code CurrentUser}
  * offers a key-value storage by providing thread-safe {@code get} and {@code put} methods. Once the
  * value is retrieved through {@code get} there is not thread-safety anymore - apart from the
@@ -38,7 +47,8 @@ import java.util.function.Supplier;
  *
  * <p>In comparison to that, this class guarantees thread safety even on non-thread-safe objects as
  * its cache is tied to the serving thread only. While allowing to cache non-thread-safe objects, it
- * has the downside of not sharing any objects with background threads or executors.
+ * has the downside of not sharing any objects with background threads or executors. When calling
+ * methods from background threads, the {@code Supplier} will be invoked directly.
  *
  * <p>Lastly, this class offers a cache, that requires callers to also provide a {@code Supplier} in
  * case the object is not present in the cache, while {@code CurrentUser} provides a storage where
@@ -68,7 +78,7 @@ public class PerThreadCache implements AutoCloseable {
 
     /**
      * Returns a key based on the value's class and an identifier that uniquely identify the value.
-     * The identifier needs to implement {@code equals()} and {@hashCode()}.
+     * The identifier needs to implement {@code equals()} and {@code hashCode()}.
      */
     public static <T> Key<T> create(Class<T> clazz, Object identifier) {
       return new Key<>(clazz, ImmutableList.of(identifier));
@@ -76,7 +86,7 @@ public class PerThreadCache implements AutoCloseable {
 
     /**
      * Returns a key based on the value's class and a set of identifiers that uniquely identify the
-     * value. Identifiers need to implement {@code equals()} and {@hashCode()}.
+     * value. Identifiers need to implement {@code equals()} and {@code hashCode()}.
      */
     public static <T> Key<T> create(Class<T> clazz, Object... identifiers) {
       return new Key<>(clazz, ImmutableList.copyOf(identifiers));
@@ -109,13 +119,8 @@ public class PerThreadCache implements AutoCloseable {
     return cache;
   }
 
-  @Nullable
-  public static PerThreadCache get() {
-    return CACHE.get();
-  }
-
   public static <T> T getOrCompute(Key<T> key, Supplier<T> loader) {
-    PerThreadCache cache = get();
+    PerThreadCache cache = CACHE.get();
     return cache != null ? cache.get(key, loader) : loader.get();
   }
 

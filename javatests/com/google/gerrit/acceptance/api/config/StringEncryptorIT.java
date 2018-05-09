a86d8ad8b22e25855c29bb29b5988ce7ffaaebb0
Inderjot Kaur Ratol <inderjot.kaur.ratol@ericsson.com>
2018-05-09 15:48:41 -0400

    Encryption API: add encryption/decryption capability for secrets
    Added a new functionality which allows plugins to leverage the
    encryption/decryption capability in Gerrit core. It takes a plaintext
    secret, generates a random salt, a random string and secure key to
    encrypt the secret. The encoded secure key is then saved in the
    `secure.config` file. Below is an example showing a saved key in
    `secure.config`:
    
    [encryption]
      key = 19dws7ao0KlIqpj4h1mhXQ==
    
    The key is created by using random string and a random salt in the
    beginning. The same encoded key is used to decode any encrypted
    passwords passed to this class. Note that, the generation of salt and
    the secure key happens only once.
    
    The default algorithm used for encryption is AES with the key length
    128. These parameters are configurable with `gerrit.config` file and the
    example configuration is shown below:
    
    [encryption]
      keyLength = 128
      iterations = 1000
      algorithm = AES
      hashAlgorithm = PBKDF2WithHmacSHA512
      transformation = AES/CBC/PKCS5Padding
    
    This change implements its own vanilla symmetric key encryption as the
    existing algorithms used in Gerrit is one-way hashing of passwords
    which is not intended to be used when the original plaintext secret
    needs to be recovered.
    
    Change-Id: Ia195e5b8bcfd73422e54323ff8116e3b91c094fc

---
+++ javatests/com/google/gerrit/acceptance/api/config/StringEncryptorIT.java
@@ -0,0 +1,49 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.api.config;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.NoHttpd;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.server.api.config.StringEncryptor;
+import com.google.gerrit.testing.ConfigSuite;
+import com.google.inject.Inject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ConfigSuite.class)
+@UseLocalDisk
+@NoHttpd
+public class StringEncryptorIT extends AbstractDaemonTest {
+
+  @Inject private StringEncryptor encryptor;
+
+  private static final String PLAINTEXT_PASSWORD = "password_to_test";
+
+  @Test
+  public void testEncrypt() throws Exception {
+    String encoded = encryptor.encrypt(PLAINTEXT_PASSWORD);
+    assertThat(encoded).isNotEqualTo(PLAINTEXT_PASSWORD);
+  }
+
+  @Test
+  public void testDecrypt() throws Exception {
+    String encoded = encryptor.encrypt(PLAINTEXT_PASSWORD);
+    String decoded = encryptor.decrypt(encoded);
+    assertThat(decoded).isEqualTo(PLAINTEXT_PASSWORD);
+  }
+}

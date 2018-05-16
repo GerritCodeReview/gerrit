d7a17e19a17fd5b1509a1648479ed240c1579968
Jacek Centkowski <jcentkowski@collab.net>
2018-05-16 12:15:04 +0200

    DropWizardMetricMaker: Introduce method to sanitize metric name
    The new method sanitizeMetricName can be used to create metric
    names that confirm to the naming rules.
    
    Change-Id: Ic0074b53f7d25edfc4f366cc45f3ad3a47e25057
    Signed-off-by: Jacek Centkowski <jcentkowski@collab.net>
    Signed-off-by: David Pursehouse <dpursehouse@collab.net>

---
+++ gerrit-server/src/test/java/com/google/gerrit/metrics/dropwizard/DropWizardMetricMakerTest.java
@@ -0,0 +1,42 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.metrics.dropwizard;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.metrics.dropwizard.DropWizardMetricMaker.sanitizeMetricName;
+
+import org.junit.Test;
+
+public class DropWizardMetricMakerTest {
+  @Test
+  public void shouldSanitizeUnwantedChars() throws Exception {
+    assertThat(sanitizeMetricName("very+confusing$long#metric@net/name^1"))
+        .isEqualTo("very_confusing_long_metric_net/name_1");
+    assertThat(sanitizeMetricName("/metric/submetric")).isEqualTo("_metric/submetric");
+  }
+
+  @Test
+  public void shouldReduceConsecutiveSlashesToOne() throws Exception {
+    assertThat(sanitizeMetricName("/metric//submetric1///submetric2/submetric3"))
+        .isEqualTo("_metric/submetric1/submetric2/submetric3");
+  }
+
+  @Test
+  public void shouldNotFinishWithSlash() throws Exception {
+    assertThat(sanitizeMetricName("metric/")).isEqualTo("metric");
+    assertThat(sanitizeMetricName("metric//")).isEqualTo("metric");
+    assertThat(sanitizeMetricName("metric/submetric/")).isEqualTo("metric/submetric");
+  }
+}

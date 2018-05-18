c15b8ef404e2c934550bb19917966b4168b9c4b6
Dave Borowitz <dborowitz@google.com>
2018-05-18 08:11:22 -0400

    Serialize ConflictsCache as proto
    Convert ConflictKey to AutoValue as well. Rewrite the normalization
    logic and add comments to make the intent clearer.
    
    Since boolean serialization doesn't support nulls, change the
    ConflictsCache interface to use primitive boolean in the put method.
    Inspecting the only caller in ConflictsCacheImpl shows that put can
    never be called with null.
    
    Change-Id: I293cffba7609362dabb93e48c1929a05cd0c76fc

--- javatests/com/google/gerrit/server/query/change/ConflictKeyTest.java
+++ javatests/com/google/gerrit/server/query/change/ConflictKeyTest.java
@@ -15,9 +15,15 @@
 package com.google.gerrit.server.query.change;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
 import static com.google.gerrit.extensions.client.SubmitType.FAST_FORWARD_ONLY;
 import static com.google.gerrit.extensions.client.SubmitType.MERGE_IF_NECESSARY;
+import static com.google.gerrit.server.cache.testing.CacheSerializerTestUtil.bytes;
+import static com.google.gerrit.server.cache.testing.SerializedClassSubject.assertThatSerializedClass;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gerrit.extensions.client.SubmitType;
+import com.google.gerrit.server.cache.proto.Cache.ConflictKeyProto;
 import org.eclipse.jgit.lib.ObjectId;
 import org.junit.Test;
 
@@ -29,8 +35,10 @@ public class ConflictKeyTest {
     ConflictKey id1First = ConflictKey.create(id1, id2, FAST_FORWARD_ONLY, true);
     ConflictKey id2First = ConflictKey.create(id2, id1, FAST_FORWARD_ONLY, true);
 
-    assertThat(id1First).isEqualTo(new ConflictKey(id1, id2, FAST_FORWARD_ONLY, true));
-    assertThat(id2First).isEqualTo(new ConflictKey(id2, id1, FAST_FORWARD_ONLY, true));
+    assertThat(id1First)
+        .isEqualTo(ConflictKey.createWithoutNormalization(id1, id2, FAST_FORWARD_ONLY, true));
+    assertThat(id2First)
+        .isEqualTo(ConflictKey.createWithoutNormalization(id2, id1, FAST_FORWARD_ONLY, true));
     assertThat(id1First).isNotEqualTo(id2First);
   }
 
@@ -40,9 +48,51 @@ public class ConflictKeyTest {
     ObjectId id2 = ObjectId.fromString("deadbeefdeadbeefdeadbeefdeadbeefdeadbeef");
     ConflictKey id1First = ConflictKey.create(id1, id2, MERGE_IF_NECESSARY, true);
     ConflictKey id2First = ConflictKey.create(id2, id1, MERGE_IF_NECESSARY, true);
-    ConflictKey expected = new ConflictKey(id1, id2, MERGE_IF_NECESSARY, true);
+    ConflictKey expected =
+        ConflictKey.createWithoutNormalization(id1, id2, MERGE_IF_NECESSARY, true);
 
     assertThat(id1First).isEqualTo(expected);
     assertThat(id2First).isEqualTo(expected);
+  }
+
+  @Test
+  public void serializer() throws Exception {
+    ConflictKey key =
+        ConflictKey.create(
+            ObjectId.fromString("badc0feebadc0feebadc0feebadc0feebadc0fee"),
+            ObjectId.fromString("deadbeefdeadbeefdeadbeefdeadbeefdeadbeef"),
+            SubmitType.MERGE_IF_NECESSARY,
+            false);
+    byte[] serialized = ConflictKey.Serializer.INSTANCE.serialize(key);
+    assertThat(ConflictKeyProto.parseFrom(serialized))
+        .isEqualTo(
+            ConflictKeyProto.newBuilder()
+                .setCommit(
+                    bytes(
+                        0xba, 0xdc, 0x0f, 0xee, 0xba, 0xdc, 0x0f, 0xee, 0xba, 0xdc, 0x0f, 0xee,
+                        0xba, 0xdc, 0x0f, 0xee, 0xba, 0xdc, 0x0f, 0xee))
+                .setOtherCommit(
+                    bytes(
+                        0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef,
+                        0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef))
+                .setSubmitType("MERGE_IF_NECESSARY")
+                .setContentMerge(false)
+                .build());
+    assertThat(ConflictKey.Serializer.INSTANCE.deserialize(serialized)).isEqualTo(key);
+  }
+
+  /**
+   * See {@link com.google.gerrit.server.cache.testing.SerializedClassSubject} for background and
+   * what to do if this test fails.
+   */
+  @Test
+  public void methods() throws Exception {
+    assertThatSerializedClass(ConflictKey.class)
+        .hasAutoValueMethods(
+            ImmutableMap.of(
+                "commit", ObjectId.class,
+                "otherCommit", ObjectId.class,
+                "submitType", SubmitType.class,
+                "contentMerge", boolean.class));
   }
 }

3efc7e6aa5c9f5aa8fab23cc9dd557d302ae51b5
David Ostrovsky <david@ostrovsky.org>
2018-05-21 23:50:02 +0200

    Acceptance tests: Replace embedded ES with docker testcontainer
    Testcontainers is a Java 8 library that supports JUnit tests, providing
    lightweight, throwaway instances of common databases, Selenium web
    browsers, or anything else that can run in a Docker container.
    
    This change replaces ES integration test that currently uses embedded
    ES mode with ES docker image using Testcontainers library. All this is
    done from within acceptance tests.
    
    This change removes dependency on ES server stack for the test code. As
    the consequence, this stack can be removed.
    
    Prerequisite for this change is installed docker service on the SUT. If
    docker service is not installed, assumption violation is raised so that
    the tests don't fail. Unfortunately, due to this missing Bazel feature,
    JUnit assumption violations are not reflected on the Bazel UI: [1] yet.
    
    [1] https://github.com/bazelbuild/bazel/issues/3476
    
    Change-Id: Iccf44310292cc44bff9173f2a4ea757b43f77183

--- gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticQueryGroupsTest.java
+++ gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticQueryGroupsTest.java
@@ -14,35 +14,43 @@
 
 package com.google.gerrit.elasticsearch;
 
+import com.google.gerrit.elasticsearch.testing.ElasticContainer;
 import com.google.gerrit.elasticsearch.testing.ElasticTestUtils;
 import com.google.gerrit.elasticsearch.testing.ElasticTestUtils.ElasticNodeInfo;
 import com.google.gerrit.server.query.group.AbstractQueryGroupsTest;
 import com.google.gerrit.testutil.InMemoryModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import java.util.concurrent.ExecutionException;
 import org.eclipse.jgit.lib.Config;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.internal.AssumptionViolatedException;
 
 public class ElasticQueryGroupsTest extends AbstractQueryGroupsTest {
   private static ElasticNodeInfo nodeInfo;
+  private static ElasticContainer<?> container;
 
   @BeforeClass
-  public static void startIndexService() throws InterruptedException, ExecutionException {
+  public static void startIndexService() {
     if (nodeInfo != null) {
       // do not start Elasticsearch twice
       return;
     }
-    nodeInfo = ElasticTestUtils.startElasticsearchNode();
+
+    try {
+      container = new ElasticContainer<>();
+      container.start();
+    } catch (Throwable t) {
+      throw new AssumptionViolatedException("Unable to start container[might be docker related]");
+    }
+
+    nodeInfo = new ElasticNodeInfo(container.getHttpHost().getPort());
   }
 
   @AfterClass
   public static void stopElasticsearchServer() {
-    if (nodeInfo != null) {
-      nodeInfo.node.close();
-      nodeInfo.elasticDir.delete();
-      nodeInfo = null;
+    if (container != null) {
+      container.stop();
     }
   }
 

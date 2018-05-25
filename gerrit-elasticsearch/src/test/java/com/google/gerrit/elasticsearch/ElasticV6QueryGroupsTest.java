ab11f8a575a719d320d6e9dd40ee4bc491598eb4
David Pursehouse <dpursehouse@collab.net>
2018-05-25 19:53:22 +0900

    Elasticsearch: Add tests for queries against versions 5 and 6
    Add separate test classes to test queries with Elasticsearch versions
    5 and 6.
    
    The entire test classes are basically copied from the existing ones,
    with the only difference being the version passed into the config and
    into the container creation. This cannot be achieved using an abstract
    class due to the container being created in the @BeforeClass annotated
    method which is static and cannot be overridden by a derived class.
    
    The byOwnerInvalidQuery test is moved up from ElasticQueryChangesTest
    to AbstractQueryChangesTest to avoid having to repeat it in all the
    Elasticsearch test classes.
    
    Bug: Issue 6094
    Change-Id: I9b91cbcbc24924dce43b1be9530543097b5fd174

--- gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticV6QueryGroupsTest.java
+++ gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticV6QueryGroupsTest.java
@@ -1,35 +1,17 @@
-// Copyright (C) 2014 The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.gerrit.elasticsearch;
 
 import com.google.gerrit.elasticsearch.testing.ElasticContainer;
 import com.google.gerrit.elasticsearch.testing.ElasticTestUtils;
 import com.google.gerrit.elasticsearch.testing.ElasticTestUtils.ElasticNodeInfo;
-import com.google.gerrit.server.query.change.AbstractQueryChangesTest;
+import com.google.gerrit.server.query.group.AbstractQueryGroupsTest;
 import com.google.gerrit.testutil.InMemoryModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.eclipse.jgit.lib.Config;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.rules.TestName;
 
-public class ElasticQueryChangesTest extends AbstractQueryChangesTest {
-  @Rule public final TestName testName = new TestName();
-
+public class ElasticV6QueryGroupsTest extends AbstractQueryGroupsTest {
   private static ElasticNodeInfo nodeInfo;
   private static ElasticContainer<?> container;
 
@@ -40,7 +22,7 @@ public class ElasticQueryChangesTest extends AbstractQueryChangesTest {
       return;
     }
 
-    container = ElasticContainer.createAndStart();
+    container = ElasticContainer.createAndStart(ElasticContainer.Version.V6);
     nodeInfo = new ElasticNodeInfo(container.getHttpHost().getPort());
   }
 

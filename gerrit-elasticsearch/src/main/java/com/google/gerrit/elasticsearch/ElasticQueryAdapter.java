b3b86c3f82f9c954a927cac45b2e2ec9f40a4524
David Pursehouse <dpursehouse@collab.net>
2018-06-01 12:27:36 +0900

    Elasticsearch: Add an adapter to support V5
    Add an adapter that adjusts the query content depending on the
    Elasticsearch version.
    
    Also-by: Marco Miller <marco.miller@ericsson.com>
    Bug: Issue 6094
    Change-Id: Ie3de3f5cec25c44a9c56eb3c430e63d0ff858894

---
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticQueryAdapter.java
@@ -0,0 +1,67 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.elasticsearch;
+
+import com.google.gson.JsonObject;
+
+public class ElasticQueryAdapter {
+  private final boolean ignoreUnmapped;
+  private final String searchFilteringName;
+  private final String indicesExistParam;
+  private final String uuidFieldType;
+  private final String stringFieldType;
+
+  ElasticQueryAdapter(ElasticVersion version) {
+    this.ignoreUnmapped = version == ElasticVersion.V2_4;
+    switch (version) {
+      case V5_6:
+      case V6_2:
+        this.searchFilteringName = "_source";
+        this.indicesExistParam = "?allow_no_indices=false";
+        this.uuidFieldType = "keyword";
+        this.stringFieldType = "text";
+        break;
+      case V2_4:
+      default:
+        this.searchFilteringName = "fields";
+        this.indicesExistParam = "";
+        this.uuidFieldType = "string";
+        this.stringFieldType = "string";
+        break;
+    }
+  }
+
+  void setIgnoreUnmapped(JsonObject properties) {
+    if (ignoreUnmapped) {
+      properties.addProperty("ignore_unmapped", true);
+    }
+  }
+
+  public String searchFilteringName() {
+    return this.searchFilteringName;
+  }
+
+  public String indicesExistParam() {
+    return indicesExistParam;
+  }
+
+  public String uuidFieldType() {
+    return uuidFieldType;
+  }
+
+  public String stringFieldType() {
+    return stringFieldType;
+  }
+}

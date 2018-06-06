107ff5ed539220d9c1e8f6e7dd959bdddbb124e8
Marco Miller <marco.miller@ericsson.com>
2018-06-06 10:37:25 -0400

    ElasticV6QueryChangesTest: remove testName warning
    Before this fix, Eclipse issued the warning [1]. This change removes
    that warning.
    
    After this change, ElasticV6QueryChangesTest#testName still uses the
    proper testName value, now from GerritBaseTests.
    
    [1] 'The field ElasticV6QueryChangesTest.testName is hiding a field from
    type GerritBaseTests'.
    
    Change-Id: I1df71b5b56beab29a779c639e0346459235a87aa

--- gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticV6QueryChangesTest.java
+++ gerrit-elasticsearch/src/test/java/com/google/gerrit/elasticsearch/ElasticV6QueryChangesTest.java
@@ -24,11 +24,8 @@ import com.google.inject.Injector;
 import org.eclipse.jgit.lib.Config;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.rules.TestName;
 
 public class ElasticV6QueryChangesTest extends AbstractQueryChangesTest {
-  @Rule public final TestName testName = new TestName();
 
   private static ElasticNodeInfo nodeInfo;
   private static ElasticContainer<?> container;

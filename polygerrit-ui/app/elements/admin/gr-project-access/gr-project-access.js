f08b442eb5d55409f886091ee38c2b78a3157b69
Paladox <thomasmulhall410@yahoo.com>
2018-06-09 17:30:29 +0100

    Fix javascript errors in gr-admin-view components
    To produce these errors, the repo must not exist, so it's as simple as
    going to /admin/repos/<project-that-does-not-exist>.
    
    Also this throws a 404 if it cannot fetch the configs.
    
    Change-Id: I6e95322d81ccebaf9ce3309e266c36e9bdf4d796

--- polygerrit-ui/app/elements/admin/gr-project-access/gr-project-access.js
+++ polygerrit-ui/app/elements/admin/gr-project-access/gr-project-access.js
@@ -54,23 +54,37 @@
      */
     _projectChanged(project) {
       if (!project) { return Promise.resolve(); }
+
       const promises = [];
+
+      const errFn = response => {
+        this.fire('page-error', {response});
+      };
+
       // Always reset sections when a project changes.
       this._sections = [];
-      promises.push(this.$.restAPI.getProjectAccessRights(project).then(res => {
-        this._inheritsFrom = res.inherits_from;
-        this._local = res.local;
-        this._groups = res.groups;
-        return this.toSortedArray(this._local);
-      }));
+      promises.push(this.$.restAPI.getProjectAccessRights(project, errFn)
+          .then(res => {
+            if (!res) { return Promise.resolve(); }
+            this._inheritsFrom = res.inherits_from;
+            this._local = res.local;
+            this._groups = res.groups;
+            return this.toSortedArray(this._local);
+          }));
 
-      promises.push(this.$.restAPI.getCapabilities().then(res => {
-        return res;
-      }));
+      promises.push(this.$.restAPI.getCapabilities(errFn)
+          .then(res => {
+            if (!res) { return Promise.resolve(); }
 
-      promises.push(this.$.restAPI.getProject(project).then(res => {
-        return res.labels;
-      }));
+            return res;
+          }));
+
+      promises.push(this.$.restAPI.getProject(project, errFn)
+          .then(res => {
+            if (!res) { return Promise.resolve(); }
+
+            return res.labels;
+          }));
 
       promises.push(this.$.restAPI.getIsAdmin().then(isAdmin => {
         this._isAdmin = isAdmin;

8cbecdc7c7c7b27f79d615ad4de21ad56e92c5e9
Paladox none <thomasmulhall410@yahoo.com>
2018-06-12 16:33:38 +0000

    Make PolyGerrit the default ui
    Change-Id: Ie5c658488962911c3a113b08ce1768858f12db07

--- java/com/google/gerrit/httpd/raw/StaticModule.java
+++ java/com/google/gerrit/httpd/raw/StaticModule.java
@@ -134,9 +134,9 @@ public class StaticModule extends ServletModule {
     if (!options.headless()) {
       install(new CoreStaticModule());
     }
-    if (options.enablePolyGerrit()) {
-      install(new PolyGerritModule());
-    }
+
+    install(new PolyGerritModule());
+
     if (options.enableGwtUi()) {
       install(new GwtUiModule());
     }
@@ -430,8 +430,6 @@ public class StaticModule extends ServletModule {
       this.polygerritUI = polygerritUI;
       this.bowerComponentServlet = bowerComponentServlet;
       this.fontServlet = fontServlet;
-      checkState(
-          options.enablePolyGerrit(), "can't install PolyGerritFilter when PolyGerrit is disabled");
     }
 
     @Override

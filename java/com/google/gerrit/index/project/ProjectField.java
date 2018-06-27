2d2877d0cce27a95df348a50f3fbf18a8a243647
Patrick Hiesel <hiesel@google.com>
2018-06-27 16:25:28 +0200

    Add ref_states to ProjectFields
    This commit adds a new field called ref_states to the ProjectIndex. This
    field can be used to perform staleness detection based on NoteDb ref
    states. We store the HEAD SHA1s in 40 digit hex notation for the project
    in question as well as all of it's ancestors.
    
    Storing the ancestors ref states as well is necessary because the index
    stores the names of all ancestors, so we need to detect when the
    project's hirarchy changes and we get out of sync.
    
    This approach follows what was done for changes, accounts and groups.
    
    A subsequent commit will build a staleness checker and wire it up.
    
    Change-Id: I08efd4286b4fd5c577c0863802bcdf051972f5cf

--- java/com/google/gerrit/index/project/ProjectField.java
+++ java/com/google/gerrit/index/project/ProjectField.java
@@ -14,17 +14,24 @@
 
 package com.google.gerrit.index.project;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.gerrit.index.FieldDef.exact;
 import static com.google.gerrit.index.FieldDef.fullText;
 import static com.google.gerrit.index.FieldDef.prefix;
+import static com.google.gerrit.index.FieldDef.storedOnly;
 
-import com.google.common.collect.Iterables;
 import com.google.gerrit.index.FieldDef;
+import com.google.gerrit.index.RefState;
 import com.google.gerrit.index.SchemaUtil;
 import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.client.RefNames;
 
 /** Index schema for projects. */
 public class ProjectField {
+  private static byte[] toRefState(Project project) {
+    return RefState.create(RefNames.REFS_CONFIG, project.getConfigRefState())
+        .toByteArray(project.getNameKey());
+  }
 
   public static final FieldDef<ProjectData, String> NAME =
       exact("name").stored().build(p -> p.getProject().getName());
@@ -39,6 +46,22 @@ public class ProjectField {
       prefix("name_part").buildRepeatable(p -> SchemaUtil.getNameParts(p.getProject().getName()));
 
   public static final FieldDef<ProjectData, Iterable<String>> ANCESTOR_NAME =
-      exact("ancestor_name")
-          .buildRepeatable(p -> Iterables.transform(p.getAncestors(), Project.NameKey::get));
+      exact("ancestor_name").buildRepeatable(p -> p.getParentNames());
+
+  /**
+   * All values of all refs that were used in the course of indexing this document. This covers
+   * {@code refs/meta/config} of the current project and all of its parents.
+   *
+   * <p>Emitted as UTF-8 encoded strings of the form {@code project:ref/name:[hex sha]}.
+   */
+  public static final FieldDef<ProjectData, Iterable<byte[]>> REF_STATE =
+      storedOnly("ref_state")
+          .buildRepeatable(
+              projectData ->
+                  projectData
+                      .tree()
+                      .stream()
+                      .filter(p -> p.getProject().getConfigRefState() != null)
+                      .map(p -> toRefState(p.getProject()))
+                      .collect(toImmutableList()));
 }

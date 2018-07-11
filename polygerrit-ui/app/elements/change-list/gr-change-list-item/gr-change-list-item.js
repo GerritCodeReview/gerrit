6f898a7e995a74b785d366660d176470adba6a37
Kasper Nilsson <kaspern@google.com>
2018-07-11 11:26:41 -0700

    Respect internalHost in dashboard project column
    Change-Id: I2461f7e130614cbefac9d957cceb5fe2f6f850b9

--- polygerrit-ui/app/elements/change-list/gr-change-list-item/gr-change-list-item.js
+++ polygerrit-ui/app/elements/change-list/gr-change-list-item/gr-change-list-item.js
@@ -137,9 +137,21 @@
       return Gerrit.Nav.getUrlForTopic(change.topic, change.internalHost);
     },
 
-    _computeTruncatedProject(project) {
-      if (!project) { return ''; }
-      return this.truncatePath(project, 2);
+    /**
+     * Computes the display string for the project column. If there is a host
+     * specified in the change detail, the string will be prefixed with it.
+     *
+     * @param {!Object} change
+     * @param {string=} truncate whether or not the project name should be
+     *     truncated. If this value is truthy, the name will be truncated.
+     * @return {string}
+     */
+    _computeProjectDisplay(change, truncate) {
+      if (!change || !change.project) { return ''; }
+      let str = '';
+      if (change.internalHost) { str += change.internalHost + '/'; }
+      str += truncate ? this.truncatePath(change.project, 2) : change.project;
+      return str;
     },
 
     _computeAccountStatusString(account) {

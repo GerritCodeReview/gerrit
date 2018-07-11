cfa0c9254601c2143a9df276b514ff64099b809b
Edwin Kempin <ekempin@google.com>
2018-07-11 16:49:55 +0200

    GroupReference: Mark UUID as nullable and add JavaDoc
    Change-Id: I2a07f6e9bdb8f96d67c8edc1fd18dea44fcce2c1
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/common/data/GroupReference.java
+++ java/com/google/gerrit/common/data/GroupReference.java
@@ -43,16 +43,23 @@ public class GroupReference implements Comparable<GroupReference> {
 
   protected GroupReference() {}
 
-  public GroupReference(AccountGroup.UUID uuid, String name) {
+  /**
+   * Create a group reference.
+   *
+   * @param uuid UUID of the group, may be {@code null} if the group name couldn't be resolved
+   * @param name the group name, must not be {@code null}
+   */
+  public GroupReference(@Nullable AccountGroup.UUID uuid, String name) {
     setUUID(uuid);
     setName(name);
   }
 
+  @Nullable
   public AccountGroup.UUID getUUID() {
     return uuid != null ? new AccountGroup.UUID(uuid) : null;
   }
 
-  public void setUUID(AccountGroup.UUID newUUID) {
+  public void setUUID(@Nullable AccountGroup.UUID newUUID) {
     uuid = newUUID != null ? newUUID.get() : null;
   }
 

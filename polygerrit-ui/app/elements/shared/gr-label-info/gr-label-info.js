35b2667e867bcc43f378452af3535ed73ec7a393
Kasper Nilsson <kaspern@google.com>
2018-07-13 19:14:27 +0000

    Modify voting and submit requirements UI
    Bug: Issue 8882
    Change-Id: If6e8e82327ecf9bf14122c1a583884760aa6f5e1

--- polygerrit-ui/app/elements/shared/gr-label-info/gr-label-info.js
+++ polygerrit-ui/app/elements/shared/gr-label-info/gr-label-info.js
@@ -72,12 +72,11 @@
             }
           }
           if (label._account_id === account._account_id) {
-            // Put self-votes at the top, and add a flag.
+            // Put self-votes at the top.
             result.unshift({
               value: labelValPrefix + label.value,
               className: labelClassName,
               account: label,
-              isCurrentUser: true,
             });
           } else {
             result.push({
@@ -165,10 +164,6 @@
       return labelInfo.values[score];
     },
 
-    _computeLabelContainerClass(label) {
-      return label.isCurrentUser ? 'currentUser' : '';
-    },
-
     /**
      * @param {!Object} labelInfo
      * @param {Object} changeLabelsRecord not used, but added as a parameter in
@@ -177,7 +172,9 @@
     _computeShowPlaceholder(labelInfo, changeLabelsRecord) {
       if (labelInfo.all) {
         for (const label of labelInfo.all) {
-          if (label.value) { return 'hidden'; }
+          if (label.value && label.value != labelInfo.default_value) {
+            return 'hidden';
+          }
         }
       }
       return '';

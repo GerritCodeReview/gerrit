cb6a899ce862403d53e64f92affc56145953dc2e
Dave Borowitz <dborowitz@google.com>
2018-07-23 15:28:22 -0700

    Serialize TagSetHolder with protobuf
    TagSetHolder and TagSet have some slightly exotic properties:
      * A subclass of AtomicReference<ObjectId> with additional fields,
        CachedRef.
      * An ObjectIdOwnerMap, which requires a custom subclass of OwnerId
        with additional fields.
      * BitSets.
      * Volatile field references.
    
    However, it still boils down to collections of value types, so the
    resulting TagSetProto structure is pretty straightforward.
    
    The most annoying thing is that the AtomicReference and ObjectId
    subclasses can't reasonably implement equals(), so the tests need to
    have more detailed assertions which reach into what would otherwise be
    private fields.
    
    While we're in there, eliminate the intermediate EntryVal class, which
    was serving no purpose other than to hold the readObject/writeObject
    methods for Java serialization.
    
    It is quite possible that this change will be slower to deserialize than
    using Java serialization, since it was previously able to directly
    deserialize the internal data structures, whereas we now have to build
    these structures piece by piece. However, as with the rest of the
    serialization code, we assume that proto is good enough until proven
    otherwise.
    
    Beyond that, we don't attempt to further rework the tag cache types or
    the cache as a whole. In particular:
      * Continue to use volatile types to handle incrementally updating
        specific cache entries.
      * Using composition instead of inheritance for CachedRef is out of
        scope. However, note that using protobuf for serialization means
        that we can make this change without flushing the cache.
      * Using a less exotic type than ObjectIdOwnerMap would probably
        require some benchmarking to prove that it's worth making the
        change.
    
    Change-Id: I08623b3f51ef1a0541559bbb2360c0d06a9de9d4

---
+++ javatests/com/google/gerrit/server/git/TagSetHolderTest.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.cache.proto.Cache.TagSetHolderProto;
+import org.junit.Test;
+
+public class TagSetHolderTest {
+  @Test
+  public void serializerWithTagSet() throws Exception {
+    TagSetHolder holder = new TagSetHolder(new Project.NameKey("project"));
+    holder.setTagSet(new TagSet(holder.getProjectName()));
+
+    byte[] serialized = TagSetHolder.Serializer.INSTANCE.serialize(holder);
+    assertThat(TagSetHolderProto.parseFrom(serialized))
+        .ignoringRepeatedFieldOrder()
+        .isEqualTo(
+            TagSetHolderProto.newBuilder()
+                .setProjectName("project")
+                .setTags(holder.getTagSet().toProto())
+                .build());
+  }
+
+  @Test
+  public void serializerWithoutTagSet() throws Exception {
+    TagSetHolder holder = new TagSetHolder(new Project.NameKey("project"));
+    byte[] serialized = TagSetHolder.Serializer.INSTANCE.serialize(holder);
+    assertThat(TagSetHolderProto.parseFrom(serialized))
+        .ignoringRepeatedFieldOrder()
+        .isEqualTo(TagSetHolderProto.newBuilder().setProjectName("project").build());
+  }
+}

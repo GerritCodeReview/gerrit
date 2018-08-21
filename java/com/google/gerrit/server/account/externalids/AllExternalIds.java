f84769f2eeaca8fea33bc9be3fc2f5fed40b5392
Dave Borowitz <dborowitz@google.com>
2018-08-21 13:43:10 -0700

    Optionally persist ExternalIdCache
    A significant source of latency on googlesource.com is reading in new
    pack files from persistent storage to parse refs/meta/external-ids,
    sometimes several seconds per load in the case of a virtual host with
    thousands of users. This crops up when external IDs are updated as well
    as on cold server starts. Both of these instances are pretty frequent:
    external IDs are updated every time a new user registers, and servers
    are typically updated daily. Serializing this cache is a relatively
    cheap engineering cost to avoid these multi-second stalls, which are
    quite annoying for users.
    
    A server updating the external-ids ref bypasses a cache load, and
    instead updates the state in-memory, then puts the result back into the
    cache at the new ObjectId. When persisting the cache, this put also puts
    the result into persistent storage. In a multi-master environment, there
    is a race window between updating the ref value and putting the new
    persistent object, which means other masters reading the ref value may
    not find the persisted object. If multiple servers are receiving
    concurrent requests, there's a good chance one or more masters will have
    to re-load the data from NoteDb. This can be improved, but requires more
    surgery to ExternalIdCache to replace the value in the cache prior to
    committing the ref update.
    
    Change-Id: I31d31d8ed490d01ce963a8162afba3daf9c1efff

--- java/com/google/gerrit/server/account/externalids/AllExternalIds.java
+++ java/com/google/gerrit/server/account/externalids/AllExternalIds.java
@@ -14,13 +14,23 @@
 
 package com.google.gerrit.server.account.externalids;
 
+import static java.util.stream.Collectors.toList;
+
 import com.google.auto.value.AutoValue;
 import com.google.common.base.Strings;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.MultimapBuilder;
+import com.google.common.collect.MultimapBuilder.SetMultimapBuilder;
 import com.google.common.collect.Multimaps;
 import com.google.common.collect.SetMultimap;
+import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Account.Id;
+import com.google.gerrit.server.cache.proto.Cache.AllExternalIdsProto;
+import com.google.gerrit.server.cache.proto.Cache.AllExternalIdsProto.ExternalIdProto;
+import com.google.gerrit.server.cache.serialize.CacheSerializer;
+import com.google.gerrit.server.cache.serialize.ProtoCacheSerializers;
+import com.google.gerrit.server.cache.serialize.ProtoCacheSerializers.ObjectIdConverter;
+import java.util.Collection;
 
 /**
  * Cache value containing all external IDs.
@@ -29,22 +39,86 @@ import com.google.gerrit.reviewdb.client.Account.Id;
  */
 @AutoValue
 public abstract class AllExternalIds {
-  static AllExternalIds create(Multimap<Id, ExternalId> byAccount) {
+  static AllExternalIds create(Multimap<Account.Id, ExternalId> byAccount) {
+    return create(
+        newByAccountBuilder(byAccount.size()).build(byAccount), copyByEmail(byAccount.values()));
+  }
+
+  private static AllExternalIds create(Collection<ExternalId> externalIds) {
+    SetMultimap<Account.Id, ExternalId> byAccount = newByAccountBuilder(externalIds.size()).build();
+    externalIds.forEach(id -> byAccount.put(id.accountId(), id));
+    return create(byAccount, copyByEmail(externalIds));
+  }
+
+  private static AllExternalIds create(
+      SetMultimap<Account.Id, ExternalId> byAccount, SetMultimap<String, ExternalId> byEmail) {
+    return new AutoValue_AllExternalIds(
+        Multimaps.unmodifiableSetMultimap(byAccount), Multimaps.unmodifiableSetMultimap(byEmail));
+  }
+
+  private static SetMultimapBuilder<Object, Object> newByAccountBuilder(int expectedAccounts) {
+    return MultimapBuilder.hashKeys(expectedAccounts).hashSetValues(5);
+  }
+
+  private static SetMultimap<String, ExternalId> copyByEmail(Collection<ExternalId> externalIds) {
     SetMultimap<String, ExternalId> byEmailCopy =
-        MultimapBuilder.hashKeys(byAccount.size()).hashSetValues(1).build();
-    byAccount
-        .values()
+        MultimapBuilder.hashKeys(externalIds.size()).hashSetValues(1).build();
+    externalIds
         .stream()
         .filter(e -> !Strings.isNullOrEmpty(e.email()))
         .forEach(e -> byEmailCopy.put(e.email(), e));
-
-    return new AutoValue_AllExternalIds(
-        Multimaps.unmodifiableSetMultimap(
-            MultimapBuilder.hashKeys(byAccount.size()).hashSetValues(5).build(byAccount)),
-        Multimaps.unmodifiableSetMultimap(byEmailCopy));
+    return byEmailCopy;
   }
 
-  public abstract SetMultimap<Id, ExternalId> byAccount();
+  public abstract SetMultimap<Account.Id, ExternalId> byAccount();
 
   public abstract SetMultimap<String, ExternalId> byEmail();
+
+  static enum Serializer implements CacheSerializer<AllExternalIds> {
+    INSTANCE;
+
+    @Override
+    public byte[] serialize(AllExternalIds object) {
+      ObjectIdConverter idConverter = ObjectIdConverter.create();
+      AllExternalIdsProto.Builder allBuilder = AllExternalIdsProto.newBuilder();
+      for (ExternalId id : object.byAccount().values()) {
+        ExternalIdProto.Builder b =
+            allBuilder
+                .addExternalIdBuilder()
+                .setKey(id.key().get())
+                .setAccountId(id.accountId().get());
+        if (id.email() != null) {
+          b.setEmail(id.email());
+        }
+        if (id.password() != null) {
+          b.setPassword(id.password());
+        }
+        if (id.blobId() != null) {
+          b.setBlobId(idConverter.toByteString(id.blobId()));
+        }
+      }
+      return ProtoCacheSerializers.toByteArray(allBuilder.build());
+    }
+
+    @Override
+    public AllExternalIds deserialize(byte[] in) {
+      ObjectIdConverter idConverter = ObjectIdConverter.create();
+      return create(
+          ProtoCacheSerializers.parseUnchecked(AllExternalIdsProto.parser(), in)
+              .getExternalIdList()
+              .stream()
+              .map(proto -> toExternalId(idConverter, proto))
+              .collect(toList()));
+    }
+
+    private static ExternalId toExternalId(ObjectIdConverter idConverter, ExternalIdProto proto) {
+      return ExternalId.create(
+          ExternalId.Key.parse(proto.getKey()),
+          new Id(proto.getAccountId()),
+          // ExternalId treats null and empty strings the same, so no need to distinguish here.
+          proto.getEmail(),
+          proto.getPassword(),
+          !proto.getBlobId().isEmpty() ? idConverter.fromByteString(proto.getBlobId()) : null);
+    }
+  }
 }

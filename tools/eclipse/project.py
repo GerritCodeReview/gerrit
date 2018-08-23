e8924fb1fee23d2be0538cd6e32ed7b826111813
David Ostrovsky <david@ostrovsky.org>
2018-08-23 08:25:24 +0200

    Bazel: Add support to Java 10
    Bazel@HEAD doesn't support Java 10 any more. The tool chain is extended
    to support building with Java 10 using host_javabase option and vanilla
    java builder. To use --host_javabase option we have to provide absolute
    path to the JDK 10. To keep that non-portable part out of the build file
    we use variable that is substitued during build invocation. Say, the
    location of the JDK 10 is: /usr/lib64/jvm/java-10, then the build is
    invoked with:
    
      $ bazel build --host_javabase=:absolute_javabase \
        --define=ABSOLUTE_JAVABASE=/usr/lib64/jvm/java-10 ...
    
    Test Plan:
    
      $ bazel build --host_javabase=:absolute_javabase \
        --define=ABSOLUTE_JAVABASE=/usr/lib64/jvm/java-10 \
        --define=USE_ABSOLUTE_JAVABASE=true \
        --host_java_toolchain=//:toolchain_vanilla \
        --java_toolchain=//:toolchain_vanilla \
        :release
    
    and replace ABSOLUTE_JAVABASE variable with the location of JDK 10.
    
    Change-Id: I5e652f7a19afbcf3607febd9a7a165dc07918bd0

--- tools/eclipse/project.py
+++ tools/eclipse/project.py
@@ -53,21 +53,29 @@ opts.add_option('--name', help='name of the generated project',
 opts.add_option('-b', '--batch', action='store_true',
                 dest='batch', help='Bazel batch option')
 opts.add_option('-j', '--java', action='store',
-                dest='java', help='Post Java 8 support (9|10|11|...)')
+                dest='java', help='Post Java 8 support (9)')
+opts.add_option('-e', '--edge_java', action='store',
+                dest='edge_java', help='Post Java 9 support (10|11|...)')
 args, _ = opts.parse_args()
 
 batch_option = '--batch' if args.batch else None
 custom_java = args.java
+edge_java = args.edge_java
 
 def _build_bazel_cmd(*args):
+    build = False
     cmd = ['bazel']
     if batch_option:
         cmd.append('--batch')
     for arg in args:
+        if arg == "build":
+            build = True
         cmd.append(arg)
-    if custom_java:
+    if custom_java and not edge_java:
         cmd.append('--host_java_toolchain=@bazel_tools//tools/jdk:toolchain_java%s' % custom_java)
         cmd.append('--java_toolchain=@bazel_tools//tools/jdk:toolchain_java%s' % custom_java)
+        if edge_java and build:
+            cmd.append(edge_java)
     return cmd
 
 

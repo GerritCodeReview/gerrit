5b1ddd2c78839b54e2d132112bed7307c2efe223
Edwin Kempin <ekempin@google.com>
2018-09-03 14:30:04 +0200

    DynamicSet: Return Iterable for entries instead of Iterator
    This makes iterating over the entries with a for loop easier.
    
    Change-Id: I55f7b4efe4ef2ba294116382409ffafd464925a6
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- javatests/com/google/gerrit/extensions/registration/DynamicSetTest.java
+++ javatests/com/google/gerrit/extensions/registration/DynamicSetTest.java
@@ -123,7 +123,7 @@ public class DynamicSetTest {
     ds.add("bar", 2);
     ds.add("bar", 3);
 
-    Iterator<DynamicSet.Entry<Integer>> entryIterator = ds.entryIterator();
+    Iterator<DynamicSet.Entry<Integer>> entryIterator = ds.entries().iterator();
     DynamicSet.Entry<Integer> next = entryIterator.next();
     assertThat(next.getPluginName()).isEqualTo("foo");
     assertThat(next.getProvider().get()).isEqualTo(1);

c102719eee96acfda81e97088fc153eb445d12d9
Han-Wen Nienhuys <hanwen@google.com>
2018-09-06 15:08:07 +0200

    CommitValidatorCache: rename to BranchCommitValidator
    Remove the commit cache, since the validator is instantiated per
    branch. This means the cache can only do something useful if there are
    two pushes to the same ref, eg.
    
      git push origin sha1-1:refs/head/master sha1-2:refs/head/master
    
    this is not a useful-enough action to warrant the complexity of a
    cache.
    
    Change-Id: I53f5807ef20538ad855cce81a89f3806a5016047

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -221,8 +221,8 @@ class ReceiveCommits {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   private static final String CODE_REVIEW_ERROR =
-        "You need 'Push' rights to upload code review requests.\n"
-            + "Verify that you are pushing to the right branch.";
+      "You need 'Push' rights to upload code review requests.\n"
+          + "Verify that you are pushing to the right branch.";
   private static final String CANNOT_DELETE_CHANGES = "Cannot delete from '" + REFS_CHANGES + "'";
   private static final String CANNOT_DELETE_CONFIG =
       "Cannot delete project configuration from '" + RefNames.REFS_CONFIG + "'";
@@ -301,7 +301,7 @@ class ReceiveCommits {
   private final ChangeNotes.Factory notesFactory;
   private final ChangeReportFormatter changeFormatter;
   private final CmdLineParser.Factory optionParserFactory;
-  private final CommitValidatorCache.Factory commitValidatorFactory;
+  private final BranchCommitValidator.Factory commitValidatorFactory;
   private final CreateGroupPermissionSyncer createGroupPermissionSyncer;
   private final CreateRefControl createRefControl;
   private final DynamicMap<ProjectConfigEntry> pluginConfigEntries;
@@ -378,7 +378,7 @@ class ReceiveCommits {
       ChangeNotes.Factory notesFactory,
       DynamicItem<ChangeReportFormatter> changeFormatterProvider,
       CmdLineParser.Factory optionParserFactory,
-      CommitValidatorCache.Factory commitValidatorFactory,
+      BranchCommitValidator.Factory commitValidatorFactory,
       CreateGroupPermissionSyncer createGroupPermissionSyncer,
       CreateRefControl createRefControl,
       DynamicMap<ProjectConfigEntry> pluginConfigEntries,
@@ -1878,11 +1878,11 @@ class ReceiveCommits {
       return;
     }
 
-    CommitValidatorCache validator = commitValidatorFactory.create(projectState, user);
+    BranchCommitValidator validator =
+        commitValidatorFactory.create(projectState, changeEnt.getDest(), user);
     try {
       if (validator.validCommit(
           receivePack.getRevWalk().getObjectReader(),
-          changeEnt.getDest(),
           cmd,
           newCommit,
           false,
@@ -1949,7 +1949,9 @@ class ReceiveCommits {
     GroupCollector groupCollector =
         GroupCollector.create(changeRefsById(), db, psUtil, notesFactory, project.getNameKey());
 
-    CommitValidatorCache validator = commitValidatorFactory.create(projectState, user);
+    BranchCommitValidator validator =
+        commitValidatorFactory.create(projectState, magicBranch.dest, user);
+
     try {
       NoteMap rejectCommits = loadRejectCommits();
       RevCommit start = setUpWalkForSelectingChanges();
@@ -2052,7 +2054,6 @@ class ReceiveCommits {
 
         if (!validator.validCommit(
             receivePack.getRevWalk().getObjectReader(),
-            magicBranch.dest,
             magicBranch.cmd,
             c,
             magicBranch.merged,
@@ -3017,7 +3018,7 @@ class ReceiveCommits {
       return;
     }
 
-    CommitValidatorCache validator = commitValidatorFactory.create(projectState, user);
+    BranchCommitValidator validator = commitValidatorFactory.create(projectState, branch, user);
     boolean missingFullName = Strings.isNullOrEmpty(user.getAccount().getFullName());
     RevWalk walk = receivePack.getRevWalk();
     walk.reset();
@@ -3048,7 +3049,7 @@ class ReceiveCommits {
         }
 
         if (!validator.validCommit(
-            walk.getObjectReader(), branch, cmd, c, false, messages, rejectCommits, null)) {
+            walk.getObjectReader(), cmd, c, false, messages, rejectCommits, null)) {
           break;
         }
 

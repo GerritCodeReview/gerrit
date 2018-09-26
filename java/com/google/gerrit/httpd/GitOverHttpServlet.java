adff20afcd76222fbaece7ca96041fe9559175a4
David Pursehouse <dpursehouse@collab.net>
2018-09-26 21:10:59 +0900

    WIP: Allow to enable git protocol version 2 for upload pack
    When receive.enableProtocolV2 is set to true, set extra parameters
    on the UploadPack from the request's Git-Protocol header (which is
    set by the client.
    
    When the repository on the server also has the configuration:
    
      [protocol]
        version = 2
    
    then the upload pack is handled with protocol V2.
    
    TODO:
     - Tests
     - Documentation
     - SSH (maybe in a follow up commit)
    
    Bug: Issue 9046
    Change-Id: I30290e8f060c1ee11b170aac2baeed10f213aad1

--- java/com/google/gerrit/httpd/GitOverHttpServlet.java
+++ java/com/google/gerrit/httpd/GitOverHttpServlet.java
@@ -45,6 +45,7 @@ import com.google.inject.TypeLiteral;
 import com.google.inject.name.Named;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -230,6 +231,13 @@ public class GitOverHttpServlet extends GitServlet {
       up.setTimeout(config.getTimeout());
       up.setPreUploadHook(PreUploadHookChain.newChain(Lists.newArrayList(preUploadHooks)));
       up.setPostUploadHook(PostUploadHookChain.newChain(Lists.newArrayList(postUploadHooks)));
+      if (config.enableProtocolV2()) {
+        String header = req.getHeader("Git-Protocol");
+        if (header != null) {
+          String[] params = header.split(":");
+          up.setExtraParameters(Arrays.asList(params));
+        }
+      }
       ProjectState state = (ProjectState) req.getAttribute(ATT_STATE);
       for (UploadPackInitializer initializer : uploadPackInitializers) {
         initializer.init(state.getNameKey(), up);

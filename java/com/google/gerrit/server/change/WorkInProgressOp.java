b2de7f8ad421ffacde3a58549ab7c18f7fc47eb9
Edwin Kempin <ekempin@google.com>
2018-10-02 10:32:33 +0200

    PostReview: Fix permission check for setting wip/ready
    The work-in-progress flag on a change can be toggled by the change
    owner, the project owner and admins.
    
    The SetWorkInProgress and SetReadyForReview REST endpoints properly
    checked the permissions and also the corresponding UI actions were
    enabled/disabled based on these permission. However when the
    work-in-progress flag was toggled from PostReview the request was denied
    if the user was a non-change-owner. As result of this project owners and
    admins could see the action in PolyGerrit but clicking on it failed.
    
    To fix this the permission checks are now implemented by a static method
    in WorkInProgressOp that can be reused from all the places.
    
    This changes the response code for PostReview if a user is not allowed
    to toggle the work-in-progress flag to 403 Forbidden. Previously it was
    returning 400 Bad Request which was wrong since 400 Bad Request should
    only be returned if the request has a malformed syntax, required input
    fields are missing or mutally exclusive input fields are used together,
    but not if permissions are missing.
    
    Bug: Issue 9787
    Change-Id: Ica169002e36c1d8070c403a3d7103a6f70060c28
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/change/WorkInProgressOp.java
+++ java/com/google/gerrit/server/change/WorkInProgressOp.java
@@ -20,14 +20,20 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.gerrit.common.Nullable;
 import com.google.gerrit.extensions.api.changes.NotifyHandling;
+import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.ChangeMessage;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.server.ChangeMessagesUtil;
+import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.PatchSetUtil;
 import com.google.gerrit.server.extensions.events.WorkInProgressStateChanged;
 import com.google.gerrit.server.notedb.ChangeNotes;
 import com.google.gerrit.server.notedb.ChangeUpdate;
+import com.google.gerrit.server.permissions.GlobalPermission;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.gerrit.server.permissions.ProjectPermission;
 import com.google.gerrit.server.update.BatchUpdateOp;
 import com.google.gerrit.server.update.ChangeContext;
 import com.google.gerrit.server.update.Context;
@@ -51,6 +57,34 @@ public class WorkInProgressOp implements BatchUpdateOp {
 
   public interface Factory {
     WorkInProgressOp create(boolean workInProgress, Input in);
+  }
+
+  public static void checkPermissions(
+      PermissionBackend permissionBackend, CurrentUser user, Change change)
+      throws PermissionBackendException, AuthException {
+    if (!user.isIdentifiedUser()) {
+      throw new AuthException("Authentication required");
+    }
+
+    if (change.getOwner().equals(user.asIdentifiedUser().getAccountId())) {
+      return;
+    }
+
+    try {
+      permissionBackend.currentUser().check(GlobalPermission.ADMINISTRATE_SERVER);
+      return;
+    } catch (AuthException e) {
+      // Skip.
+    }
+
+    try {
+      permissionBackend
+          .user(user)
+          .project(change.getProject())
+          .check(ProjectPermission.WRITE_CONFIG);
+    } catch (AuthException exp) {
+      throw new AuthException("not allowed to toggle work in progress");
+    }
   }
 
   private final ChangeMessagesUtil cmUtil;

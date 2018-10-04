b458da0ff00129b16a2277f8a64d54cfe7a17359
Edwin Kempin <ekempin@google.com>
2018-10-04 11:36:58 +0200

    Better message if change submit fails due to depencency on outdated patch set
    Add a test to verify that a change that depends on a commit that is an
    outdated patch set of a change cannot be submitted and improve the error
    message for this case.
    
    Change-Id: Ia2af0a9f29d23bbeeb6d4909783ad041454e7535
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/submit/CommitMergeStatus.java
+++ java/com/google/gerrit/server/submit/CommitMergeStatus.java
@@ -96,9 +96,16 @@ public enum CommitMergeStatus {
               + " Was the change of this commit deleted?",
           commit, otherCommit);
     } else if (changes.size() == 1) {
+      ChangeData change = changes.get(0);
+      if (change.currentPatchSet().getRevision().get().equals(otherCommit)) {
+        return String.format(
+            "Commit %s depends on commit %s of change %d which cannot be merged.",
+            commit, otherCommit, change.getId().get());
+      }
       return String.format(
-          "Commit %s depends on commit %s of change %d which cannot be merged.",
-          commit, otherCommit, changes.get(0).getId().get());
+          "Commit %s depends on commit %s of change %d which cannot be merged"
+              + " because it is an outdated patch set.",
+          commit, otherCommit, change.getId().get());
     } else {
       return String.format(
           "Commit %s depends on commit %s of changes %s which cannot be merged.",

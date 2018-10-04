6d94dae58deb36a32d302736522ae49adf96e861
Edwin Kempin <ekempin@google.com>
2018-10-04 11:00:41 +0200

    Better message if change submit fails due to depencency on deleted change
    Add a test to verify that a change that depends on a commit of a deleted
    change cannot be submitted and improve the error message for this case.
    
    Change-Id: I51acfea8e9cd2dae6d2234031638a1de67ccf996
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/submit/CommitMergeStatus.java
+++ java/com/google/gerrit/server/submit/CommitMergeStatus.java
@@ -92,7 +92,8 @@ public enum CommitMergeStatus {
 
     if (changes.isEmpty()) {
       return String.format(
-          "Commit %s depends on commit %s which cannot be merged.", commit, otherCommit);
+          "Commit %s depends on commit %s which cannot be merged. Was that change deleted?",
+          commit, otherCommit);
     } else if (changes.size() == 1) {
       return String.format(
           "Commit %s depends on commit %s of change %d which cannot be merged.",

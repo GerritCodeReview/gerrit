fe27174f7abfba9c29cd1d56be310c4690594f76
Ole Rehmsen <oler@google.com>
2018-10-12 16:20:57 +0200

    Move the creation to host.
    WIP need to:
     - newly created range comments added hovering does not work correctly
     - resolving any TODOs
    
    Change-Id: Ifdb5da86d6bf6160efba9bc59dd347b1c5e55e85

--- polygerrit-ui/app/elements/diff/gr-diff/gr-diff.js
+++ polygerrit-ui/app/elements/diff/gr-diff/gr-diff.js
@@ -180,6 +180,8 @@
       },
 
       _diffLength: Number,
+
+      _nodeObserver: Object,
     },
 
     behaviors: [
@@ -191,6 +193,11 @@
       'comment-update': '_handleCommentUpdate',
       'comment-save': '_handleCommentSave',
       'create-range-comment': '_handleCreateRangeComment',
+      'render-content': '_handleRenderContent',
+    },
+
+    detached() {
+      this._unobserveNodes();
     },
 
     /** Cancel any remaining diff builder rendering work. */
@@ -228,17 +235,6 @@
     _handleCommentSaveOrDiscard() {
       this.dispatchEvent(new CustomEvent('diff-comments-modified',
           {bubbles: true}));
-    },
-
-    /** @return {!Array<!HTMLElement>} */
-    getThreadEls() {
-      let threads = [];
-      const threadGroupEls = Polymer.dom(this.root)
-          .querySelectorAll('gr-diff-comment-thread-group');
-      for (const threadGroupEl of threadGroupEls) {
-        threads = threads.concat(threadGroupEl.threadEls);
-      }
-      return threads;
     },
 
     /** @return {string} */
@@ -354,17 +350,15 @@
       const patchForNewThreads = this._getPatchNumByLineAndContent(
           lineEl, contentEl);
       const isOnParent =
-        this._getIsParentCommentByLineAndContent(lineEl, contentEl);
-      const threadGroupEl = this._getOrCreateThreadGroup(contentEl);
+          this._getIsParentCommentByLineAndContent(lineEl, contentEl);
       this.dispatchEvent(new CustomEvent('create-comment', {
         bubbles: true,
         detail: {
-          threadGroupEl,
           lineNum,
           side,
-          range,
+          patchNum: patchForNewThreads,
           isOnParent,
-          patchForNewThreads,
+          range,
         },
       }));
     },
@@ -377,7 +371,7 @@
      * Gets or creates a comment thread group for a specific line and side on a
      * diff.
      * @param {!Object} contentEl
-     * @return {!Object}
+     * @return {!Node}
      */
     _getOrCreateThreadGroup(contentEl) {
       // Check if thread group exists.
@@ -388,7 +382,6 @@
       }
       return threadGroupEl;
     },
-
 
     /**
      * The value to be used for the patch number of new comments created at the
@@ -578,6 +571,7 @@
     },
 
     _renderDiffTable() {
+      this._unobserveNodes();
       if (!this.prefs) {
         this.dispatchEvent(new CustomEvent('render', {bubbles: true}));
         return;
@@ -594,6 +588,32 @@
       this.$.diffBuilder.render(this.comments, this._getBypassPrefs());
     },
 
+    _handleRenderContent() {
+      this._nodeObserver = Polymer.dom(this).observeNodes(info => {
+        const addedThreadEls = info.addedNodes.filter(
+            node => node.nodeType === Node.ELEMENT_NODE &&
+            node.classList.contains('comment-thread'));
+        // TODO(oler): Handle removed nodes, but distinguish from removals
+        //             caused by further distribution into the thread groups
+        for (const threadEl of addedThreadEls) {
+          const lineNum = Number(threadEl.getAttribute('line-num'));
+          const commentSide = threadEl.getAttribute('comment-side');
+          const lineEl = this.$.diffBuilder.getLineElByNumber(
+              lineNum, commentSide);
+          const contentText = this.$.diffBuilder.getContentByLineEl(lineEl);
+          const contentEl = contentText.parentElement;
+          const threadGroupEl = this._getOrCreateThreadGroup(contentEl);
+          Polymer.dom(threadGroupEl).appendChild(threadEl);
+        }
+      });
+    },
+
+    _unobserveNodes() {
+      if (this._nodeObserver) {
+        Polymer.dom(this).unobserveNodes(this._nodeObserver);
+      }
+    },
+
     /**
      * Get the preferences object including the safety bypass context (if any).
      */
@@ -605,6 +625,7 @@
     },
 
     clearDiffContent() {
+      this._unobserveNodes();
       this.$.diffTable.innerHTML = null;
     },
 

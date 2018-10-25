b4c01b435577aa50a3f2fa96477541bb7ff37fa1
Ole Rehmsen <oler@google.com>
2018-10-25 14:46:06 +0200

    Move thread element creation out of group
    Change-Id: Ifdbd9a948c73906880d3987269c1dcc24e49c4c0

--- polygerrit-ui/app/elements/diff/gr-diff-host/gr-diff-host.js
+++ polygerrit-ui/app/elements/diff/gr-diff-host/gr-diff-host.js
@@ -456,8 +456,10 @@
 
     /** @param {CustomEvent} e */
     _handleCreateComment(e) {
-      const {threadGroupEl, lineNum, side, range} = e.detail;
-      const threadEl = this._getOrCreateThread(threadGroupEl, side, range);
+      const {threadGroupEl, lineNum, side, range, isOnParent,
+          patchForNewThreads} = e.detail;
+      const threadEl = this._getOrCreateThread(threadGroupEl, side, range,
+          isOnParent, patchForNewThreads);
       threadEl.addOrEditDraft(lineNum, range);
       this.$.reporting.recordDraftInteraction();
     },
@@ -468,16 +470,25 @@
      *
      * @param {!Object} threadGroupEl
      * @param {string} commentSide
-     * @param {!Object=} range
+     * @param {?Object} range
+     * @param {boolean} isOnParent
+     * @param {string} patchForNewThreads
      * @return {!Object}
      */
-    _getOrCreateThread(threadGroupEl, commentSide, range=undefined) {
+    _getOrCreateThread(threadGroupEl, commentSide, range, isOnParent,
+        patchForNewThreads) {
       let threadEl = threadGroupEl.getThreadEl(commentSide, range);
 
       if (!threadEl) {
-        threadGroupEl.addNewThread(commentSide, range);
-        Polymer.dom.flush();
-        threadEl = threadGroupEl.getThreadEl(commentSide, range);
+        threadEl = Gerrit.createThreadElement(
+            {
+              comments: [],
+              commentSide,
+              patchNum: patchForNewThreads,
+              range,
+            }, isOnParent, this._parentIndex, this.changeNum,
+            this.path, this.projectName);
+        Polymer.dom(threadGroupEl).appendChild(threadEl);
       }
       return threadEl;
     },

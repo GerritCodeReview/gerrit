8215a0b35c52bf27082e680c337c451ec3e542c1
Ole Rehmsen <oler@google.com>
2018-10-25 13:04:36 +0200

    Switch to imperative comment thread creation
    We eventually need to move the comment thread creation to gr-diff-host,
    and there it cannot be done from a template because we need to hold onto
    instances of the thread elements as they are slotted into gr-diff.
    
    I realize that for now this does not look like an improvement, but
    having the whole refactoring into one change is to risky, so some of
    these intermediate steps look a bit odd. See
    https://gerrit-review.googlesource.com/c/gerrit/+/197112
    for a WIP CL of where this is going.
    
    Change-Id: If7919bc62ce5c5719ae1c0c682c11f9f73a9c5ec

--- polygerrit-ui/app/elements/diff/gr-diff-comment-thread-group/gr-diff-comment-thread-group.js
+++ polygerrit-ui/app/elements/diff/gr-diff-comment-thread-group/gr-diff-comment-thread-group.js
@@ -17,6 +17,44 @@
 (function() {
   'use strict';
 
+  window.Gerrit = window.Gerrit || {};
+
+  /**
+   * @param {Object} thread
+   * @param {boolean} isOnParent
+   * @param {number} parentIndex
+   * @param {number} changeNum
+   * @param {string} path
+   * @param {string} projectName
+   */
+  window.Gerrit.createThreadElement = function(
+      thread, isOnParent, parentIndex, changeNum, path, projectName) {
+    const threadEl = document.createElement('gr-diff-comment-thread');
+    threadEl.comments = thread.comments;
+    threadEl.commentSide = thread.commentSide;
+    threadEl.isOnParent = isOnParent;
+    threadEl.parentIndex = parentIndex;
+    threadEl.changeNum = changeNum;
+    threadEl.patchNum = thread.patchNum;
+    threadEl.lineNum = thread.lineNum;
+    const rootIdChangedListener = changeEvent => {
+      thread.rootId = changeEvent.detail.value;
+    };
+    threadEl.addEventListener('root-id-changed', rootIdChangedListener);
+    threadEl.path = path;
+    threadEl.projectName = projectName;
+    threadEl.range = thread.range;
+    const threadDiscardListener = e => {
+      const threadEl = /** @type {!Node} */ (e.currentTarget);
+      const parent = Polymer.dom(threadEl).parentNode;
+      threadEl.removeEventListener('root-id-changed', rootIdChangedListener);
+      threadEl.removeEventListener('thread-discard', threadDiscardListener);
+      Polymer.dom(parent).removeChild(threadEl);
+    };
+    threadEl.addEventListener('thread-discard', threadDiscardListener);
+    return threadEl;
+  };
+
   Polymer({
     is: 'gr-diff-comment-thread-group',
 
@@ -32,14 +70,28 @@
         type: Number,
         value: null,
       },
-      threads: {
-        type: Array,
-        value() { return []; },
-      },
+      path: String,
     },
 
     get threadEls() {
       return Polymer.dom(this.root).querySelectorAll('gr-diff-comment-thread');
+    },
+
+    /**
+     * Fetch the thread element at the given range, or the range-less thread
+     * element on the line if no range is provided, lineNum, and side.
+     *
+     * @param {string} side
+     * @param {!Object=} opt_range
+     * @return {!Object|undefined}
+     */
+    getThreadEl(side, opt_range) {
+      const threads = [].filter.call(this.threadEls,
+          thread => this._rangesEqual(thread.range, opt_range))
+          .filter(thread => thread.commentSide === side);
+      if (threads.length === 1) {
+        return threads[0];
+      }
     },
 
     /**
@@ -49,7 +101,7 @@
      * @param {!Object} opt_range
      */
     addNewThread(commentSide, opt_range) {
-      this.push('threads', {
+      this._appendThread({
         comments: [],
         commentSide,
         patchNum: this.patchForNewThreads,
@@ -57,34 +109,20 @@
       });
     },
 
-    removeThread(rootId) {
-      for (let i = 0; i < this.threads.length; i++) {
-        if (this.threads[i].rootId === rootId) {
-          this.splice('threads', i, 1);
-          return;
-        }
+    /** @param {!Array<!Object>} threads */
+    setThreads(threads) {
+      // This is temporary, and the only usage is adding a full new list of
+      // threads in builder, so not optimizing for reusing any DOM elements.
+      this.clearThreads();
+      for (const thread of threads) {
+        this._appendThread(thread);
       }
     },
 
-    /**
-     * Fetch the thread group at the given range, or the range-less thread
-     * on the line if no range is provided, lineNum, and side.
-     *
-     * @param {string} side
-     * @param {!Object=} opt_range
-     * @return {!Object|undefined}
-     */
-    getThread(side, opt_range) {
-      const threads = [].filter.call(this.threadEls,
-          thread => this._rangesEqual(thread.range, opt_range))
-          .filter(thread => thread.commentSide === side);
-      if (threads.length === 1) {
-        return threads[0];
+    clearThreads() {
+      while (this.lastChild) {
+        Polymer.dom(this.root).removeChild(this.lastChild);
       }
-    },
-
-    _handleThreadDiscard(e) {
-      this.removeThread(e.detail.rootId);
     },
 
     /**
@@ -103,6 +141,12 @@
           a.startChar === b.startChar &&
           a.endLine === b.endLine &&
           a.endChar === b.endChar;
+    },
+
+    _appendThread(thread) {
+      Polymer.dom(this.root).appendChild(Gerrit.createThreadElement(
+          thread, this.isOnParent, this.parentIndex, this.changeNum,
+          this.path, this.projectName));
     },
   });
 })();

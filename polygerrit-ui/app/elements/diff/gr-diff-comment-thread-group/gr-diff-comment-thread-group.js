fdb49b4f5c17d47c74053632b9e41819bab93f22
Ole Rehmsen <oler@google.com>
2018-11-08 09:07:35 +0100

    Move gr-diff-comment-thread factory to diff-host
    This is now only used from there, so it no longer needs to be globally
    accessible on Gerrit.
    
    Change-Id: I6facd64ece360f5b8ddf7b801621e86bd5f39697

--- polygerrit-ui/app/elements/diff/gr-diff-comment-thread-group/gr-diff-comment-thread-group.js
+++ polygerrit-ui/app/elements/diff/gr-diff-comment-thread-group/gr-diff-comment-thread-group.js
@@ -17,43 +17,6 @@
 (function() {
   'use strict';
 
-  window.Gerrit = window.Gerrit || {};
-
-  /**
-   * @param {Object} thread
-   * @param {number} parentIndex
-   * @param {number} changeNum
-   * @param {string} path
-   * @param {string} projectName
-   */
-  window.Gerrit.createThreadElement = function(
-      thread, parentIndex, changeNum, path, projectName) {
-    const threadEl = document.createElement('gr-diff-comment-thread');
-    threadEl.comments = thread.comments;
-    threadEl.commentSide = thread.commentSide;
-    threadEl.isOnParent = !!thread.isOnParent;
-    threadEl.parentIndex = parentIndex;
-    threadEl.changeNum = changeNum;
-    threadEl.patchNum = thread.patchNum;
-    threadEl.lineNum = thread.lineNum;
-    const rootIdChangedListener = changeEvent => {
-      thread.rootId = changeEvent.detail.value;
-    };
-    threadEl.addEventListener('root-id-changed', rootIdChangedListener);
-    threadEl.path = path;
-    threadEl.projectName = projectName;
-    threadEl.range = thread.range;
-    const threadDiscardListener = e => {
-      const threadEl = /** @type {!Node} */ (e.currentTarget);
-      const parent = Polymer.dom(threadEl).parentNode;
-      threadEl.removeEventListener('root-id-changed', rootIdChangedListener);
-      threadEl.removeEventListener('thread-discard', threadDiscardListener);
-      Polymer.dom(parent).removeChild(threadEl);
-    };
-    threadEl.addEventListener('thread-discard', threadDiscardListener);
-    return threadEl;
-  };
-
   Polymer({
     is: 'gr-diff-comment-thread-group',
 

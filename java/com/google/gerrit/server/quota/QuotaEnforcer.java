1e3c259c2cae82a595426c2bca47627ce28ae81f
Patrick Hiesel <hiesel@google.com>
2018-11-12 15:08:35 +0100

    Add QuotaBackend and QuotaEnforcer extension point
    In a recent discussion on the mailing list we agreed that Gerrit-core
    would benefit from a mechanism to enforce various kinds of quotas.
    
    As there is no gold-standard for quota checking and there are vastly
    different quotas that one might want to enforce (HTTP-QPS,
    number-of-projects, numBytesPerProject to name just a few) we provide
    this as an Extension point for plugins.
    
    This commit implements the extension point in core using AutoValue
    requests and responses to keep the interface as stable as possible. We
    abstract away from this interface by implementing QuotaBackend and
    DefaultQuotaBackend to centralize the plugin calling, provide a fluent
    API for Gerrit-core to interact with and be able to provide a different
    implementation that doesn't require plugins in tests and other settings
    that might have such a neeed. The QuotaBackend interface offers throwing
    and non-throwing methods to eliminate try-catch-throw boiler-plate in
    callers.
    
    Decoupling QuotaBackend from QuotaEnforcer also makes it so that we can
    improve the fluent interface as we get more callers to make it easy to
    use while keeping QuotaEnforcer stable to break as little plugins as
    possible.
    
    In the following paragraphs, I'll outline a bit of the thoughts that
    went into the design of the interfaces:
    
    For QuotaEnforcer, we don't want to throw exceptions, but have rich
    return types instead. One reason is that the general Java world is
    moving into the direction of UncheckedExceptions, another reason is that
    we can be richer in expressing what happened in a returned object.
    
    QuotaRequestContext contains all state besides the quota group and the
    number of tokens we want to deduct. This is because this state might
    change and be extended in the future. In this case we want existing
    plugins to remain compatible.
    
    Why are we passing Change.Id, Account.Id and Project.NameKey as Java
    objects to plugins instead of just encompassing them as IdStrings in
    the quota group (/projects/my-project/create)?
    Mainly because we don't want plugins to have to parse this back into an
    identifier they can use which would be wasteful and add complexity.
    
    Why are we passing these at all?
    There are uses cases where it matters which top-level entity the request
    is for. For example: Limiting the number of bytes per project. We want
    to support these as well.
    
    Why is QuotaEnforcer offering a refill mechanism?
    We offer a refill mechanism that basically tries to roll-back the last
    quota request. This is only triggered when one of the QuotaEnforcers
    fails, but other succeed. In this case, the succeeded ones might have
    deducted quota for the reqest, even though others did not and we let the
    request fail because of a lack of quota. In this case we want to try to
    undo the action. This is best-effort and plugins can choose not to do
    refilling.
    
    Why is QuotaBackend *not* offering a refill mechanism?
    We don't want to pollute core-Gerrit with try-catch-finally blocks that
    refill quota. This is a design decision to keep the code simple. An
    action in Gerrit that failed after it passed a quota check will have
    deducted quota. Most QuotaEnforcer implementations are refilling, so
    this is only a cosmetic issue that vanishes fast. Overall, we see the
    QuotaBackend as a monolith, that can support internal rollbacks without
    offering this mechanism to external callers.
    
    In subsequent commits, we want to add quota checks to Gerrit-core and
    adapt the Quota Plugin to use the new system instead of other hooks as
    it does right now.
    
    We will also provide tests where applicable.
    
    Change-Id: I0ac9d4de583871f92040db55f0df29465029ad55

---
+++ java/com/google/gerrit/server/quota/QuotaEnforcer.java
@@ -0,0 +1,69 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.quota;
+
+import com.google.gerrit.extensions.annotations.ExtensionPoint;
+
+/**
+ * Allows plugins to enforce different types of quota.
+ *
+ * <p>Enforcing quotas can be helpful in many scenarios. For example:
+ *
+ * <ul>
+ *   <li>Reducing the number of QPS a user can send to Gerrit on the REST API
+ *   <li>Limiting the size of a repository (project)
+ *   <li>Limiting the number of changes in a repository
+ *   <li>Limiting the number of actions that have the potential for spam, abuse or flooding if not
+ *       limited
+ * </ul>
+ *
+ * This endpoint gives plugins the capability to enforce any of these limits. The server will ask
+ * all plugins that registered this endpoint and collect all results. In case {@link
+ * #request(String, QuotaRequestContext, long)} was called and one or more plugins returned a result
+ * other than {@code OK}, the server will call {@link #refill(String, QuotaRequestContext, long)} on
+ * all plugins with the same parameters. Plugins that deducted tokens in the {@link #request(String,
+ * QuotaRequestContext, long)} call can refill them so that users don't get charged any quota for
+ * failed requests.
+ *
+ * <p>Not all implementations will need to deduct quota on {@link #request(String,
+ * QuotaRequestContext, long)}}. Implementations that work on top of instance-attributes, such as
+ * the number of projects per instance can choose not to keep any state and always check how many
+ * existing projects there are and if adding the inquired number would exceed the limit. In this
+ * case, {@link #request(String, QuotaRequestContext, long)} and {@link #requestNoDeduction(String,
+ * QuotaRequestContext, long)}} share the same implementation and {@link #refill(String,
+ * QuotaRequestContext, long)} is a no-op.
+ */
+@ExtensionPoint
+public interface QuotaEnforcer {
+  /**
+   * Checks if there is at least {@code numTokens} quota to fulfil the request. Bucket-based
+   * implementation can deduct the inquired number of tokens from the bucket.
+   */
+  QuotaResponse request(String quotaGroup, QuotaRequestContext ctx, long numTokens);
+
+  /**
+   * Checks if there is at least {@code numTokens} quota to fulfil the request. This is a pre-flight
+   * request, implementations should not deduct tokens from a bucket, yet.
+   */
+  QuotaResponse requestNoDeduction(String quotaGroup, QuotaRequestContext ctx, long numTokens);
+
+  /**
+   * A previously requested and deducted quota has to be refilled (if possible) because the request
+   * failed other quota checks. Implementations can choose to leave this a no-op in case they are
+   * the first line of defence (e.g. always deduct HTTP quota even if the request failed for other
+   * quota issues so that the user gets throttled).
+   */
+  void refill(String quotaGroup, QuotaRequestContext ctx, long numTokens);
+}

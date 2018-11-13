3c0fa327090ef66b5f9df864bd51ecd58363470a
Paladox none <thomasmulhall410@yahoo.com>
2018-11-13 21:12:36 +0000

    Add support for  ProjectConfigEntry in gr-repo*
    Change-Id: If6af5f2d7fcc4af1ee4d0fcca5f312696ddb91b7

--- polygerrit-ui/app/elements/admin/gr-repo/gr-repo.js
+++ polygerrit-ui/app/elements/admin/gr-repo/gr-repo.js
@@ -73,6 +73,7 @@
       },
       /** @type {?} */
       _repoConfig: Object,
+      _repoPluginConfig: Object,
       _readOnly: {
         type: Boolean,
         value: true,
@@ -155,6 +156,15 @@
               config.state = STATES.active.value;
             }
             this._repoConfig = config;
+            if (config.plugin_config) {
+              this._repoPluginConfig = Object.keys(config.plugin_config)
+               .map(key => {
+                 const plugin = config.plugin_config[key];
+                 plugin.name = key;
+                 return plugin;
+               });
+              console.log(this._repoPluginConfig);
+            }
             this._loading = false;
           }));
 
@@ -203,6 +213,23 @@
         }, {
           label: 'False',
           value: 'FALSE',
+        },
+      ];
+    },
+
+    _formatPluginBooleanSelect(item) {
+      if (!item) { return; }
+      return [
+        {
+          label: 'Enforced',
+          value: 'enforced',
+        },
+        {
+          label: 'True',
+          value: 'true',
+        }, {
+          label: 'False',
+          value: 'false',
         },
       ];
     },
@@ -321,6 +348,23 @@
 
     _computeChangesUrl(name) {
       return Gerrit.Nav.getUrlForProjectChanges(name);
+    },
+
+    _toArray(obj) {
+      var array = [];
+      for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          array.push({
+            key: key,
+            val: obj[key]
+          });
+        }
+      }
+      return array;
+    },
+
+    callingFunc(pluginConfig) {
+      console.log(pluginConfig);
     },
   });
 })();

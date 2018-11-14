af89c0845cf2c98e8304165a9531edb0de47b68e
Dave Borowitz <dborowitz@google.com>
2018-11-13 17:07:14 -0800

    Move RefUpdateUtil and LockFailureException to common git package
    Change-Id: I073c5c6fb86932a735ea9b43881283c2ce925bf8

--- java/com/google/gerrit/git/RefUpdateUtil.java
+++ java/com/google/gerrit/git/RefUpdateUtil.java
@@ -12,11 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.update;
-
+package com.google.gerrit.git;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.gerrit.server.git.LockFailureException;
 import java.io.IOException;
 import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.lib.BatchRefUpdate;

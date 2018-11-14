0c1b1303d3b1eae092d6c71074be3ee633d13379
Changcheng Xiao <xchangcheng@google.com>
2018-11-14 14:34:54 -0800

    Define 'AllProjectsCreatorInput' for 'AllProjectsCreator'
    This allows us to have better control over what is going to be initialized
    when an All-Projects is created. As a start, this commit just defines this
    @Auto_Value class with serveral simple properties.
    
    Change-Id: I909edb7a07232c09e86ffd40be11ce7fe5a905bb

--- java/com/google/gerrit/server/schema/ReviewDbSchemaCreator.java
+++ java/com/google/gerrit/server/schema/ReviewDbSchemaCreator.java
@@ -148,8 +148,9 @@ public class ReviewDbSchemaCreator {
 
     GroupReference admins = createGroupReference("Administrators");
     GroupReference batchUsers = createGroupReference("Non-Interactive Users");
-
-    allProjectsCreator.setAdministrators(admins).setBatchUsers(batchUsers).create();
+    AllProjectsCreatorInput allProjectsCreatorInput =
+        AllProjectsCreatorInput.builder().adminGroup(admins).batchGroup(batchUsers).build();
+    allProjectsCreator.create(allProjectsCreatorInput);
     // We have to create the All-Users repository before we can use it to store the groups in it.
     allUsersCreator.setAdministrators(admins).create();
 

f0888392462cc17f70c37a0055a2b44ca2e16912
Changcheng Xiao <xchangcheng@google.com>
2018-11-14 14:34:54 -0800

    Define 'AllProjectsCreatorInput' for 'AllProjectsCreator'
    This allows us to have better control over what is going to be initialized
    when an All-Projects is created. As a start, this commit just defines this
    @Auto_Value class with serveral simple properties.
    
    Change-Id: I909edb7a07232c09e86ffd40be11ce7fe5a905bb

---
+++ java/com/google/gerrit/server/schema/AllProjectsCreatorInput.java
@@ -0,0 +1,80 @@
+package com.google.gerrit.server.schema;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.common.data.GroupReference;
+import com.google.gerrit.common.data.LabelType;
+import com.google.gerrit.common.data.LabelValue;
+import com.google.gerrit.server.Sequences;
+import com.google.gerrit.server.UsedAt;
+
+@AutoValue
+public abstract class AllProjectsCreatorInput {
+  @UsedAt(UsedAt.Project.GOOGLE)
+  public static LabelType getDefaultCodeReviewLabel() {
+    LabelType type =
+        new LabelType(
+            "Code-Review",
+            ImmutableList.of(
+                new LabelValue((short) 2, "Looks good to me, approved"),
+                new LabelValue((short) 1, "Looks good to me, but someone else must approve"),
+                new LabelValue((short) 0, "No score"),
+                new LabelValue((short) -1, "I would prefer this is not merged as is"),
+                new LabelValue((short) -2, "This shall not be merged")));
+    type.setCopyMinScore(true);
+    type.setCopyAllScoresOnTrivialRebase(true);
+    return type;
+  }
+
+  /** The administrator group which gets default permissions granted. */
+  @Nullable
+  public abstract GroupReference administratorsGroup();
+
+  /** The group which gets stream-events permission granted and appropriate properties set. */
+  @Nullable
+  public abstract GroupReference batchUsersGroup();
+
+  /** The commit message used when commit the project config change. */
+  @Nullable
+  public abstract String commitMessage();
+
+  /** The first change-id used in this host. */
+  @UsedAt(UsedAt.Project.GOOGLE)
+  public abstract int firstChangeIdForNoteDb();
+
+  /** The "Code-Review" label to be defined in the All-Projects. */
+  @UsedAt(UsedAt.Project.GOOGLE)
+  public abstract LabelType codeReviewLabel();
+
+  /** Other labels to be defined in the All-Projects. */
+  @UsedAt(UsedAt.Project.GOOGLE)
+  @Nullable
+  public abstract ImmutableList<LabelType> additionalLabelType();
+
+  public abstract Builder toBuilder();
+
+  public static Builder builderWithDefaults() {
+    return new AutoValue_AllProjectsCreatorInput.Builder()
+        .codeReviewLabel(getDefaultCodeReviewLabel())
+        .firstChangeIdForNoteDb(Sequences.FIRST_CHANGE_ID);
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder administratorsGroup(GroupReference adminGroup);
+
+    public abstract Builder batchUsersGroup(GroupReference batchGroup);
+
+    public abstract Builder commitMessage(String commitMessage);
+
+    public abstract Builder firstChangeIdForNoteDb(int firstChangeId);
+
+    @UsedAt(UsedAt.Project.GOOGLE)
+    public abstract Builder codeReviewLabel(LabelType codeReviewLabel);
+
+    public abstract Builder additionalLabelType(ImmutableList<LabelType> additionalLabelType);
+
+    abstract AllProjectsCreatorInput build();
+  }
+}

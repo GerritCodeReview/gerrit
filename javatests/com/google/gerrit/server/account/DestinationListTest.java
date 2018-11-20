5c3494bd5557e303728dd8db8b22937d466d96e4
Dave Borowitz <dborowitz@google.com>
2018-11-20 10:44:34 -0800

    Make TestCases inherit GerritBaseTest instead
    These tests already use JUnit4-style @Test annotations, so inheriting
    from TestCase has no effect.
    
    Change-Id: Id176cfb68de9e9681934350d41ff38247b832183

--- javatests/com/google/gerrit/server/account/DestinationListTest.java
+++ javatests/com/google/gerrit/server/account/DestinationListTest.java
@@ -21,13 +21,13 @@ import static org.easymock.EasyMock.replay;
 import com.google.gerrit.reviewdb.client.Branch;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.git.ValidationError;
+import com.google.gerrit.testing.GerritBaseTests;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-import junit.framework.TestCase;
 import org.junit.Test;
 
-public class DestinationListTest extends TestCase {
+public class DestinationListTest extends GerritBaseTests {
   public static final String R_FOO = "refs/heads/foo";
   public static final String R_BAR = "refs/heads/bar";
 

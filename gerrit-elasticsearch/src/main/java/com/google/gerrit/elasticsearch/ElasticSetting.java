ae412874532d65c3dfa96b336f3891693dc1fd80
David Pursehouse <dpursehouse@collab.net>
2018-11-30 14:04:30 +0900

    ElasticSetting.Builder: Make methods return 'this'
    When the methods return the builder instance, calls to them can be
    chained together in a single statement.
    
    Change-Id: I8284a123969658e6501d7d396c0712cecd0a733a

--- gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticSetting.java
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticSetting.java
@@ -23,10 +23,7 @@ class ElasticSetting {
       ImmutableMap.of("\\u002E", "\\u0020", "\\u005F", "\\u0020");
 
   static SettingProperties createSetting() {
-    ElasticSetting.Builder settings = new ElasticSetting.Builder();
-    settings.addCharFilter();
-    settings.addAnalyzer();
-    return settings.build();
+    return new ElasticSetting.Builder().addCharFilter().addAnalyzer().build();
   }
 
   static class Builder {
@@ -39,16 +36,17 @@ class ElasticSetting {
       return properties;
     }
 
-    void addCharFilter() {
+    Builder addCharFilter() {
       FieldProperties charMapping = new FieldProperties("mapping");
       charMapping.mappings = getCustomCharMappings(CUSTOM_CHAR_MAPPING);
 
       FieldProperties charFilter = new FieldProperties();
       charFilter.customMapping = charMapping;
       fields.put("char_filter", charFilter);
+      return this;
     }
 
-    void addAnalyzer() {
+    Builder addAnalyzer() {
       FieldProperties customAnalyzer = new FieldProperties("custom");
       customAnalyzer.tokenizer = "standard";
       customAnalyzer.charFilter = new String[] {"custom_mapping"};
@@ -57,6 +55,7 @@ class ElasticSetting {
       FieldProperties analyzer = new FieldProperties();
       analyzer.customWithCharFilter = customAnalyzer;
       fields.put("analyzer", analyzer);
+      return this;
     }
 
     private static String[] getCustomCharMappings(ImmutableMap<String, String> map) {

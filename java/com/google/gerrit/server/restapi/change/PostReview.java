3bfd99028b52e85644d2eb2e39030f860c522369
Sabari Ajay Kumar <sabarik@codeaurora.org>
2018-12-05 03:31:24 -0800

    Provide a way to lock label changes
    Adds a new label function that provides a way to prevent labels from
    being edited for a change. Defining a label with this function and
    applying a +1 vote will prevent label changes including removal of
    existing labels. However, users with the remove reviewer permission,
    the branch owner, project owner and site admin can remove labels even
    if the label is locked. When a change is locked for label changes,
    users won't be able to see the voting buttons on the UI for labels
    other than the "Label-Lock" label.
    
    Change-Id: I4bc3e5160d542328da030e605c7ce8482f28435a

--- java/com/google/gerrit/server/restapi/change/PostReview.java
+++ java/com/google/gerrit/server/restapi/change/PostReview.java
@@ -874,7 +874,7 @@ public class PostReview
     @Override
     public boolean updateChange(ChangeContext ctx)
         throws OrmException, ResourceConflictException, UnprocessableEntityException, IOException,
-            PatchListNotAvailableException {
+            PatchListNotAvailableException, AuthException {
       user = ctx.getIdentifiedUser();
       notes = ctx.getNotes();
       ps = psUtil.get(ctx.getNotes(), psId);
@@ -1143,8 +1143,19 @@ public class PostReview
       return false;
     }
 
+    private void checkLabelLocked(Map.Entry<String, Short> ent, PatchSetApproval psa)
+        throws AuthException, OrmException, IOException {
+      if (!approvalsUtil.canUpdateLabel(
+          notes, psa.getLabelId(), firstNonNull(in.labels, Collections.emptyMap()))) {
+        throw new AuthException(
+            String.format(
+                "Applying label \"%s\": %d is restricted since change is label locked",
+                ent.getKey(), ent.getValue()));
+      }
+    }
+
     private boolean updateLabels(ProjectState projectState, ChangeContext ctx)
-        throws OrmException, ResourceConflictException, IOException {
+        throws OrmException, ResourceConflictException, IOException, AuthException {
       Map<String, Short> inLabels = firstNonNull(in.labels, Collections.emptyMap());
 
       // If no labels were modified and change is closed, abort early.
@@ -1175,6 +1186,7 @@ public class PostReview
           // User requested delete of this label.
           oldApprovals.put(normName, null);
           if (c != null) {
+            checkLabelLocked(ent, c);
             if (c.getValue() != 0) {
               addLabelDelta(normName, (short) 0);
               oldApprovals.put(normName, previous.get(normName));
@@ -1187,6 +1199,7 @@ public class PostReview
           c.setGranted(ctx.getWhen());
           c.setTag(in.tag);
           ctx.getUser().updateRealAccountId(c::setRealAccountId);
+          checkLabelLocked(ent, c);
           ups.add(c);
           addLabelDelta(normName, c.getValue());
           oldApprovals.put(normName, previous.get(normName));
@@ -1200,6 +1213,7 @@ public class PostReview
           c = ApprovalsUtil.newApproval(psId, user, lt.getLabelId(), ent.getValue(), ctx.getWhen());
           c.setTag(in.tag);
           c.setGranted(ctx.getWhen());
+          checkLabelLocked(ent, c);
           ups.add(c);
           addLabelDelta(normName, c.getValue());
           oldApprovals.put(normName, previous.get(normName));

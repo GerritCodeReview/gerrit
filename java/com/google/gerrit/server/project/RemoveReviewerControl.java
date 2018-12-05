3cdfd9a8ab71514bd02db481ef5e4e2e9f1165c1
Sabari Ajay Kumar <sabarik@codeaurora.org>
2018-12-05 03:31:24 -0800

    Provide a way to lock label changes
    Adds a new label function that provides a way to prevent labels from
    being edited for a change. Defining a label with this function and
    applying a +1 vote will prevent label changes including removal of
    existing labels. However, users with the remove reviewer permission,
    the branch owner, project owner and site admin can remove labels even
    if the label is locked. When a change is locked for label changes,
    users won't be able to see the voting buttons on the UI for labels
    other than the "Label-Lock" label.
    
    Change-Id: I4bc3e5160d542328da030e605c7ce8482f28435a

--- java/com/google/gerrit/server/project/RemoveReviewerControl.java
+++ java/com/google/gerrit/server/project/RemoveReviewerControl.java
@@ -18,6 +18,7 @@ import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSetApproval;
+import com.google.gerrit.server.ApprovalsUtil;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.notedb.ChangeNotes;
 import com.google.gerrit.server.permissions.ChangePermission;
@@ -29,14 +30,17 @@ import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gwtorm.server.OrmException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.io.IOException;
 
 @Singleton
 public class RemoveReviewerControl {
+  private final ApprovalsUtil approvalsUtil;
   private final PermissionBackend permissionBackend;
 
   @Inject
-  RemoveReviewerControl(PermissionBackend permissionBackend) {
+  RemoveReviewerControl(PermissionBackend permissionBackend, ApprovalsUtil approvalsUtil) {
     this.permissionBackend = permissionBackend;
+    this.approvalsUtil = approvalsUtil;
   }
 
   /**
@@ -47,7 +51,7 @@ public class RemoveReviewerControl {
    */
   public void checkRemoveReviewer(
       ChangeNotes notes, CurrentUser currentUser, PatchSetApproval approval)
-      throws PermissionBackendException, AuthException {
+      throws PermissionBackendException, AuthException, IOException, OrmException {
     checkRemoveReviewer(notes, currentUser, approval.getAccountId(), approval.getValue());
   }
 
@@ -59,27 +63,35 @@ public class RemoveReviewerControl {
    * @throws PermissionBackendException on failure of permission checks.
    */
   public void checkRemoveReviewer(ChangeNotes notes, CurrentUser currentUser, Account.Id reviewer)
-      throws PermissionBackendException, AuthException {
+      throws PermissionBackendException, AuthException, IOException, OrmException {
     checkRemoveReviewer(notes, currentUser, reviewer, 0);
   }
 
   /** @return true if the user is allowed to remove this reviewer. */
   public boolean testRemoveReviewer(
       ChangeData cd, CurrentUser currentUser, Account.Id reviewer, int value)
-      throws PermissionBackendException, OrmException {
+      throws PermissionBackendException, OrmException, IOException {
+    boolean isLabelLocked = approvalsUtil.isLabelLocked(cd.notes());
     if (canRemoveReviewerWithoutPermissionCheck(
-        permissionBackend, cd.change(), currentUser, reviewer, value)) {
+        permissionBackend, cd.change(), currentUser, reviewer, value, isLabelLocked)) {
       return true;
+    }
+    if (isLabelLocked) {
+      return false;
     }
     return permissionBackend.user(currentUser).change(cd).test(ChangePermission.REMOVE_REVIEWER);
   }
 
   private void checkRemoveReviewer(
       ChangeNotes notes, CurrentUser currentUser, Account.Id reviewer, int val)
-      throws PermissionBackendException, AuthException {
+      throws PermissionBackendException, AuthException, IOException, OrmException {
+    boolean isLabelLocked = approvalsUtil.isLabelLocked(notes);
     if (canRemoveReviewerWithoutPermissionCheck(
-        permissionBackend, notes.getChange(), currentUser, reviewer, val)) {
+        permissionBackend, notes.getChange(), currentUser, reviewer, val, isLabelLocked)) {
       return;
+    }
+    if (isLabelLocked) {
+      throw new AuthException("not allowed to remove reviewer since label lock is set");
     }
 
     permissionBackend.user(currentUser).change(notes).check(ChangePermission.REMOVE_REVIEWER);
@@ -90,13 +102,16 @@ public class RemoveReviewerControl {
       Change change,
       CurrentUser currentUser,
       Account.Id reviewer,
-      int value)
+      int value,
+      boolean isLabelLocked)
       throws PermissionBackendException {
     if (change.getStatus().equals(Change.Status.MERGED)) {
       return false;
     }
-
-    if (currentUser.isIdentifiedUser()) {
+    // If label locked, users shouldn't be able to remove labels
+    // regardless of whether they own the change or whether they are
+    // removing their own labels
+    if (!isLabelLocked && currentUser.isIdentifiedUser()) {
       Account.Id aId = currentUser.getAccountId();
       if (aId.equals(reviewer)) {
         return true; // A user can always remove themselves.

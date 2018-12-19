87bfe687af75a1c71844ca75b3f7ca092b7e2ca2
Dave Borowitz <dborowitz@google.com>
2018-12-18 17:59:29 -0800

    Move OutputFormat to its own top-level json package
    This has no dependencies other than GSON, and putting it in its own
    package simplifies the top-level server package, especially if we add
    more classes later.
    
    Not all of this class is actually JSON-related, namely the TEXT value.
    But most of it is, and this was easier than factoring out the
    GsonBuilder logic into a separate class.
    
    Change-Id: I6c9e49a52d8e27360f3dba2356db7a8d24beb6fe

--- java/com/google/gerrit/json/OutputFormat.java
+++ java/com/google/gerrit/json/OutputFormat.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server;
+package com.google.gerrit.json;
 
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;

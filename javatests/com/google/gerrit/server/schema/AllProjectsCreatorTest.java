025cd4325ae282e8b6ef2f861dac46f0589e3751
Changcheng Xiao <xchangcheng@google.com>
2019-01-21 15:39:12 +0100

    Move default AllProjects configs to a utility class for tests
    This would allow us to reuse those methods for internal tests.
    
    Change-Id: I95c012ed4d39c4eaad77e3336ffb14812b0e7937

--- javatests/com/google/gerrit/server/schema/AllProjectsCreatorTest.java
+++ javatests/com/google/gerrit/server/schema/AllProjectsCreatorTest.java
@@ -16,17 +16,19 @@ package com.google.gerrit.server.schema;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.gerrit.server.schema.AllProjectsInput.getDefaultCodeReviewLabel;
+import static com.google.gerrit.server.schema.testing.AllProjectsCreatorTestUtil.assertSectionEquivalent;
+import static com.google.gerrit.server.schema.testing.AllProjectsCreatorTestUtil.assertTwoConfigsEquivalent;
+import static com.google.gerrit.server.schema.testing.AllProjectsCreatorTestUtil.getAllProjectsWithoutDefaultAcls;
+import static com.google.gerrit.server.schema.testing.AllProjectsCreatorTestUtil.getDefaultAllProjectsWithAllDefaultSections;
+import static com.google.gerrit.server.schema.testing.AllProjectsCreatorTestUtil.readAllProjectsConfig;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Streams;
 import com.google.gerrit.common.data.GroupReference;
 import com.google.gerrit.common.data.LabelType;
 import com.google.gerrit.common.data.LabelValue;
 import com.google.gerrit.extensions.client.InheritableBoolean;
 import com.google.gerrit.reviewdb.client.AccountGroup;
 import com.google.gerrit.reviewdb.client.BooleanProjectConfig;
-import com.google.gerrit.reviewdb.client.RefNames;
 import com.google.gerrit.server.GerritPersonIdent;
 import com.google.gerrit.server.account.GroupUUID;
 import com.google.gerrit.server.config.AllProjectsName;
@@ -35,93 +37,13 @@ import com.google.gerrit.server.notedb.Sequences;
 import com.google.gerrit.testing.GerritBaseTests;
 import com.google.gerrit.testing.InMemoryModule;
 import com.google.inject.Inject;
-import java.io.IOException;
-import java.util.Set;
-import java.util.stream.Collectors;
-import org.eclipse.jgit.errors.ConfigInvalidException;
-import org.eclipse.jgit.lib.BlobBasedConfig;
 import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.PersonIdent;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.junit.Before;
 import org.junit.Test;
 
 public class AllProjectsCreatorTest extends GerritBaseTests {
-  private static final ImmutableList<String> DEFAULT_ALL_PROJECTS_PROJECT_SECTION =
-      ImmutableList.of("[project]", "  description = Access inherited by all other projects.");
-  private static final ImmutableList<String> DEFAULT_ALL_PROJECTS_RECEIVE_SECTION =
-      ImmutableList.of(
-          "[receive]",
-          "  requireContributorAgreement = false",
-          "  requireSignedOffBy = false",
-          "  requireChangeId = true",
-          "  enableSignedPush = false");
-  private static final ImmutableList<String> DEFAULT_ALL_PROJECTS_SUBMIT_SECTION =
-      ImmutableList.of("[submit]", "  mergeContent = true");
-  private static final ImmutableList<String> DEFAULT_ALL_PROJECTS_CAPABILITY_SECTION =
-      ImmutableList.of(
-          "[capability]",
-          "  administrateServer = group Administrators",
-          "  priority = batch group Non-Interactive Users",
-          "  streamEvents = group Non-Interactive Users");
-  private static final ImmutableList<String> DEFAULT_ALL_PROJECTS_ACCESS_SECTION =
-      ImmutableList.of(
-          "[access \"refs/*\"]",
-          "  read = group Administrators",
-          "  read = group Anonymous Users",
-          "[access \"refs/for/*\"]",
-          "  addPatchSet = group Registered Users",
-          "[access \"refs/for/refs/*\"]",
-          "  push = group Registered Users",
-          "  pushMerge = group Registered Users",
-          "[access \"refs/heads/*\"]",
-          "  create = group Administrators",
-          "  create = group Project Owners",
-          "  editTopicName = +force group Administrators",
-          "  editTopicName = +force group Project Owners",
-          "  forgeAuthor = group Registered Users",
-          "  forgeCommitter = group Administrators",
-          "  forgeCommitter = group Project Owners",
-          "  label-Code-Review = -2..+2 group Administrators",
-          "  label-Code-Review = -2..+2 group Project Owners",
-          "  label-Code-Review = -1..+1 group Registered Users",
-          "  push = group Administrators",
-          "  push = group Project Owners",
-          "  submit = group Administrators",
-          "  submit = group Project Owners",
-          "[access \"refs/meta/config\"]",
-          "  exclusiveGroupPermissions = read",
-          "  create = group Administrators",
-          "  create = group Project Owners",
-          "  label-Code-Review = -2..+2 group Administrators",
-          "  label-Code-Review = -2..+2 group Project Owners",
-          "  push = group Administrators",
-          "  push = group Project Owners",
-          "  read = group Administrators",
-          "  read = group Project Owners",
-          "  submit = group Administrators",
-          "  submit = group Project Owners",
-          "[access \"refs/tags/*\"]",
-          "  create = group Administrators",
-          "  create = group Project Owners",
-          "  createSignedTag = group Administrators",
-          "  createSignedTag = group Project Owners",
-          "  createTag = group Administrators",
-          "  createTag = group Project Owners");
-  private static final ImmutableList<String> DEFAULT_ALL_PROJECTS_LABEL_SECTION =
-      ImmutableList.of(
-          "[label \"Code-Review\"]",
-          "  function = MaxWithBlock",
-          "  defaultValue = 0",
-          "  copyMinScore = true",
-          "  copyAllScoresOnTrivialRebase = true",
-          "  value = -2 This shall not be merged",
-          "  value = -1 I would prefer this is not merged as is",
-          "  value = 0 No score",
-          "  value = +1 Looks good to me, but someone else must approve",
-          "  value = +2 Looks good to me, approved");
-
   private static final LabelType TEST_LABEL =
       new LabelType(
           "Test-Label",
@@ -174,20 +96,8 @@ public class AllProjectsCreatorTest extends GerritBaseTests {
             .build();
     allProjectsCreator.create(allProjectsInput);
 
-    Config config = readAllProjectsConfig();
+    Config config = readAllProjectsConfig(repoManager, allProjectsName);
     assertTwoConfigsEquivalent(config, expectedConfig);
-  }
-
-  private static String getDefaultAllProjectsWithAllDefaultSections() {
-    return Streams.stream(
-            Iterables.concat(
-                DEFAULT_ALL_PROJECTS_PROJECT_SECTION,
-                DEFAULT_ALL_PROJECTS_RECEIVE_SECTION,
-                DEFAULT_ALL_PROJECTS_SUBMIT_SECTION,
-                DEFAULT_ALL_PROJECTS_CAPABILITY_SECTION,
-                DEFAULT_ALL_PROJECTS_ACCESS_SECTION,
-                DEFAULT_ALL_PROJECTS_LABEL_SECTION))
-        .collect(Collectors.joining("\n"));
   }
 
   private GroupReference createGroupReference(String name) {
@@ -204,7 +114,7 @@ public class AllProjectsCreatorTest extends GerritBaseTests {
         AllProjectsInput.builder().codeReviewLabel(TEST_LABEL).build();
     allProjectsCreator.create(allProjectsInput);
 
-    Config config = readAllProjectsConfig();
+    Config config = readAllProjectsConfig(repoManager, allProjectsName);
     assertSectionEquivalent(config, expectedLabelConfig, "label");
   }
 
@@ -215,7 +125,7 @@ public class AllProjectsCreatorTest extends GerritBaseTests {
         AllProjectsInput.builder().projectDescription(testDescription).build();
     allProjectsCreator.create(allProjectsInput);
 
-    Config config = readAllProjectsConfig();
+    Config config = readAllProjectsConfig(repoManager, allProjectsName);
     assertThat(config.getString("project", null, "description")).isEqualTo(testDescription);
   }
 
@@ -231,7 +141,7 @@ public class AllProjectsCreatorTest extends GerritBaseTests {
             .build();
     allProjectsCreator.create(allProjectsInput);
 
-    Config config = readAllProjectsConfig();
+    Config config = readAllProjectsConfig(repoManager, allProjectsName);
     assertThat(config.getBoolean("submit", null, "rejectEmptyCommit", false)).isTrue();
   }
 
@@ -242,7 +152,7 @@ public class AllProjectsCreatorTest extends GerritBaseTests {
 
     Config expectedConfig = new Config();
     expectedConfig.fromText(getAllProjectsWithoutDefaultAcls());
-    Config config = readAllProjectsConfig();
+    Config config = readAllProjectsConfig(repoManager, allProjectsName);
     assertTwoConfigsEquivalent(config, expectedConfig);
   }
 
@@ -259,60 +169,7 @@ public class AllProjectsCreatorTest extends GerritBaseTests {
 
     Config expectedConfig = new Config();
     expectedConfig.setString("project", null, "description", description);
-    Config config = readAllProjectsConfig();
+    Config config = readAllProjectsConfig(repoManager, allProjectsName);
     assertTwoConfigsEquivalent(config, expectedConfig);
-  }
-
-  // Loads the "project.config" from the All-Projects repo.
-  private Config readAllProjectsConfig() throws IOException, ConfigInvalidException {
-    try (Repository repo = repoManager.openRepository(allProjectsName)) {
-      Ref configRef = repo.findRef(RefNames.REFS_CONFIG);
-      return new BlobBasedConfig(null, repo, configRef.getObjectId(), "project.config");
-    }
-  }
-
-  private static String getAllProjectsWithoutDefaultAcls() {
-    return Streams.stream(
-            Iterables.concat(
-                DEFAULT_ALL_PROJECTS_PROJECT_SECTION,
-                DEFAULT_ALL_PROJECTS_RECEIVE_SECTION,
-                DEFAULT_ALL_PROJECTS_SUBMIT_SECTION,
-                DEFAULT_ALL_PROJECTS_LABEL_SECTION))
-        .collect(Collectors.joining("\n"));
-  }
-
-  private static void assertTwoConfigsEquivalent(Config config1, Config config2) {
-    Set<String> sections1 = config1.getSections();
-    Set<String> sections2 = config2.getSections();
-    assertThat(sections1).containsExactlyElementsIn(sections2);
-
-    sections1.forEach(s -> assertSectionEquivalent(config1, config2, s));
-  }
-
-  private static void assertSectionEquivalent(Config config1, Config config2, String section) {
-    assertSubsectionEquivalent(config1, config2, section, null);
-
-    Set<String> subsections1 = config1.getSubsections(section);
-    Set<String> subsections2 = config2.getSubsections(section);
-    assertThat(subsections1)
-        .named("section \"%s\"", section)
-        .containsExactlyElementsIn(subsections2);
-
-    subsections1.forEach(s -> assertSubsectionEquivalent(config1, config2, section, s));
-  }
-
-  private static void assertSubsectionEquivalent(
-      Config config1, Config config2, String section, String subsection) {
-    Set<String> subsectionNames1 = config1.getNames(section, subsection);
-    Set<String> subsectionNames2 = config2.getNames(section, subsection);
-    String name = String.format("subsection \"%s\" of section \"%s\"", subsection, section);
-    assertThat(subsectionNames1).named(name).containsExactlyElementsIn(subsectionNames2);
-
-    subsectionNames1.forEach(
-        n ->
-            assertThat(config1.getStringList(section, subsection, n))
-                .named(name)
-                .asList()
-                .containsExactlyElementsIn(config2.getStringList(section, subsection, n)));
   }
 }

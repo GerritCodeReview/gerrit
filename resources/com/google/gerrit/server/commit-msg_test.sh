a21db58b00991cce347c6e1a8fdfb37e3ba9032e
James Tucker <raggi@google.com>
2019-01-22 13:41:05 -0800

    Handle commit messages when verbose commits are used.
    Commit messages with "verbose" include a diff and a content marker that must
    be ignored during empty checks. Stripspace already knows about the marker,
    but comment stripping removes the marker, so the marker must be handled
    before comment stripping, otherwise the diff ends up as part of the commit
    message.
    
    Change-Id: I63ea4b81e602dcc574ff2054953267a8bfd21500

--- resources/com/google/gerrit/server/commit-msg_test.sh
+++ resources/com/google/gerrit/server/commit-msg_test.sh
@@ -38,6 +38,76 @@ EOF
   fi
 }
 
+function test_empty_with_verbose_diff {
+  rm -f input
+  cat << "EOF" > input
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch master
+# Your branch is up to date with 'origin/master'.
+#
+# Changes to be committed:
+#	modified:   commit-msg
+#
+# ------------------------ >8 ------------------------
+# Do not modify or remove the line above.
+# Everything below it will be ignored.
+diff --git a/resources/com/google/gerrit/server/tools/root/hooks/commit-msg b/resources/com/google/gerrit/server/tools/root/hooks/commit-msg
+index 53f29950e1..ad7897a3b6 100755
+--- a/resources/com/google/gerrit/server/tools/root/hooks/commit-msg
++++ b/resources/com/google/gerrit/server/tools/root/hooks/commit-msg
+@@ -33,6 +33,17 @@ fi fo fum
+
+ bash
+
+
+EOF
+  if ${hook} input ; then
+    fail "must fail on empty message"
+  fi
+}
+
+function test_preserve_changeid_with_verbose_diff {
+rm -f input
+cat << "EOF" > input
+bla bla
+
+Change-Id: I123
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch master
+# Your branch is up to date with 'origin/master'.
+#
+# Changes to be committed:
+#	modified:   commit-msg
+#
+# ------------------------ >8 ------------------------
+# Do not modify or remove the line above.
+# Everything below it will be ignored.
+diff --git a/resources/com/google/gerrit/server/tools/root/hooks/commit-msg b/resources/com/google/gerrit/server/tools/root/hooks/commit-msg
+index 53f29950e1..ad7897a3b6 100755
+--- a/resources/com/google/gerrit/server/tools/root/hooks/commit-msg
++++ b/resources/com/google/gerrit/server/tools/root/hooks/commit-msg
+@@ -33,6 +33,17 @@ fi fo fum
+
+ bash
+
+EOF
+  ${hook} input || fail "failed hook execution"
+
+  found=$(grep -c '^Change-Id' input)
+  if [[ "${found}" != "1" ]]; then
+    fail "got ${found} Change-Ids, want 1"
+  fi
+  found=$(grep -c '^Change-Id: I123' input)
+  if [[ "${found}" != "1" ]]; then
+    fail "got ${found} Change-Id: I123, want 1"
+  fi
+}
+
 # a Change-Id already set is preserved.
 function test_preserve_changeid {
   cat << EOF > input

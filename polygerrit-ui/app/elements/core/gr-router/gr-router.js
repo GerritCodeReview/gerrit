a3b38b9accfaf28b51731bf3edaf92cac0bdbe17
Sven Selberg <svense@axis.com>
2019-01-23 12:02:49 +0100

    PG: Make commitlink selection configurable and consistent
    Previously the selection of which Weblink that should be used for
    inline commitlinks (like patchset, parent) was determined by a number
    of assumptions:
    (Known weblinks that links to codebrowsers are Gitweb and Gitiles)
    
    * It was assumed that the name of the weblink to gitiles would be
      'gitiles'.
    
      The default name has since changed to 'browse', however many
      installations have configured the name to be the old default
      'gitiles and therefore we keep this name as an identifier of a
      weblink that links to a codebrowser.
    
    * It was assumed that there would never be more than one weblink
      available to any of these code-browsers. If weblinks for both
      Gitiles and Gitweb was available PG would always select Gitweb
      for inline commitlinks.
    
      Use ServerInfo.gerrit.primaryWeblinkName setting to select
      codebrowser weblink for inline commit links.
    
      If primaryWeblinkName is not set try to find a codebrowser weblink
      by searching for known codebrowser weblinks in the order
      (Gitiles first):
    
        1. weblink.name == 'gitiles' - Old default Gitiles name
        2. weblink.name == 'browse'  - Current default Gitiles name
        3. weblink.name == 'gitweb'
    
    * None of the known codebrowser weblinks were visible in
      changeMetadata.links. Since it was assumed that there would never
      be more than one weblink available that linked to a codebrowser,
      all known weblinks linking to codebrowsers were excluded from this
      collection, even if they were not selected for inline commitlinks.
    
      Since selection of the weblink that is used for inline commitlinks
      is now well defined we can safely remove exactly that one from
      the available weblinks when populating changeMetadata.links which
      will enable for having weblinks to both Gitweb and Gitiles (when
      named 'gitiles') being displayed on the Changescreen. The primary
      as inline commitlinks and all other in changeMetadata.links.
    
    Bug: Issue 10355
    Change-Id: If3aaa629d06ff77f1faff0dff15f68fe1adad469

--- polygerrit-ui/app/elements/core/gr-router/gr-router.js
+++ polygerrit-ui/app/elements/core/gr-router/gr-router.js
@@ -281,32 +281,54 @@
 
     _getPatchSetWeblink(params) {
       const {commit, options} = params;
-      const {weblinks} = options || {};
+      const {weblinks, config} = options || {};
       const name = commit && commit.slice(0, 7);
-      const url = this._getSupportedWeblinkUrl(weblinks);
-      if (!url) {
+      const weblink = this._getBrowseCommitWeblink(weblinks, config);
+      if (!weblink || !weblink.url) {
         return {name};
       } else {
+        const url = weblink.url;
         return {name, url};
       }
     },
 
-    _isDirectCommit(link) {
-      // This is a whitelist of web link types that provide direct links to
-      // the commit in the url property.
-      return link.name === 'gitiles' || link.name === 'gitweb';
+    _firstCodeBrowserWeblink(weblinks) {
+      // This is an ordered whitelist of web link types that provide direct
+      // links to the commit in the url property.
+      const codeBrowserLinks = ['gitiles', 'browse', 'gitweb'];
+      let found;
+      codeBrowserLinks.forEach(linkName => {
+        if (!found) {
+          const weblink = weblinks.find(weblink => weblink.name === linkName);
+          if (weblink) { found = weblink; }
+        }
+      });
+      if (found) { return found; }
+      return null;
     },
 
-    _getSupportedWeblinkUrl(weblinks) {
+
+    _getBrowseCommitWeblink(weblinks, config) {
       if (!weblinks) { return null; }
-      const weblink = weblinks.find(this._isDirectCommit);
+      let weblink;
+      // Use primary weblink if configured and exists.
+      if (config && config.gerrit && config.gerrit.primary_weblink_name) {
+        weblink = weblinks.find(
+            weblink => weblink.name === config.gerrit.primary_weblink_name
+        );
+      }
+      if (!weblink) {
+        weblink = this._firstCodeBrowserWeblink(weblinks);
+      }
       if (!weblink) { return null; }
-      return weblink.url;
+      return weblink;
     },
 
-    _getChangeWeblinks({repo, commit, options: {weblinks}}) {
+    _getChangeWeblinks({repo, commit, options: {weblinks, config}}) {
       if (!weblinks || !weblinks.length) return [];
-      return weblinks.filter(weblink => !this._isDirectCommit(weblink)).map(
+      const inline = this._getBrowseCommitWeblink(weblinks, config);
+      return weblinks.filter(weblink =>
+        !inline || !inline.name || weblink.name !== inline.name).map(
           ({name, url}) => {
             if (!url.startsWith('https:') && !url.startsWith('http:')) {
               url = this.getBaseUrl() + (url.startsWith('/') ? '' : '/') + url;

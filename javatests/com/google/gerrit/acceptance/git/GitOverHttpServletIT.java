4b4f2820790eb1819a6a10bb03d695aa5091ee33
Dave Borowitz <dborowitz@google.com>
2019-01-24 07:35:13 -0800

    GitOverHttpServletIT: Don't sandbox tests
    The flakiness is caused by the timeout, not by test methods running
    concurrently. Leave the parts about clearing the FakeGroupAuditService,
    though.
    
    Change-Id: Ie5add8bb160071e9fe0ffbf5c2e8af072bc0dbfb

--- javatests/com/google/gerrit/acceptance/git/GitOverHttpServletIT.java
+++ javatests/com/google/gerrit/acceptance/git/GitOverHttpServletIT.java
@@ -18,7 +18,6 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableList;
-import com.google.gerrit.acceptance.Sandboxed;
 import com.google.gerrit.server.AnonymousUser;
 import com.google.gerrit.server.audit.HttpAuditEvent;
 import com.google.gerrit.testing.FakeGroupAuditService;
@@ -45,7 +44,6 @@ public class GitOverHttpServletIT extends AbstractPushForReview {
   }
 
   @Test
-  @Sandboxed
   public void receivePackAuditEventLog() throws Exception {
     auditService.clearEvents();
     testRepo
@@ -71,7 +69,6 @@ public class GitOverHttpServletIT extends AbstractPushForReview {
   }
 
   @Test
-  @Sandboxed
   public void uploadPackAuditEventLog() throws Exception {
     auditService.clearEvents();
     // testRepo is already a clone. Make a server-side change so we have something to fetch.

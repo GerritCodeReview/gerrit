faf68ac6b08b8c54f1630a9649106c88b5ecd795
Dave Borowitz <dborowitz@google.com>
2019-01-24 10:07:09 -0800

    Fix GitOverHttpServletIT flakiness
    The races here are non-obvious. Firs, in general, HTTP audit event
    dispatching looks synchronous in GitOverHttpServlet, but is actually
    async since it happens after writing data over an HTTP connection.
    There's no way to know whether the thread on the other end has read the
    data and continued executing before GitOverHttpServlet#doFilter returns.
    
    More specifically in the test environment, GitOverHttpServletIT performs
    some Git-over-HTTP operations during setup, namely cloning the repo into
    an InMemoryRepository. We don't know whether the audit events generated
    by this operation are processed before or after the @Test method starts
    executing. As a result, we can't use the cleaner alternative of having a
    test set up its own AuditListener binding.
    
    Combined with the fact that audit events are only sent after a request
    is processed, this means we really can't do better than waiting to see
    if any events are in-flight. So this change really boils down to
    introducing a few longer timeouts.
    
    That said, while we're in there, we can also use better synchronization
    tools (BlockingQueue) and improve the public interface of
    FakeGroupAuditService to return a copy of the queue sorted by timestamp.
    
    Now that we more reliably dequeue all events, it's clear that we
    actually do expect two HTTP requests in both the upload-pack and
    receive-pack cases.
    
    Verified that this reduces flakiness to <1% on my workstation with:
    
    $ bazel test --runs_per_test=100 --test_filter='GitOverHttpServletIT#.*AuditEventLog' //javatests/com/google/gerrit/acceptance/git:git
    
    It's possible that the timeouts may still need to be adjusted to work in
    other environments (slow laptops, CI).
    
    Change-Id: Idd57cc7f5e0b6504403b10bd3676e78d9f1cd792

--- java/com/google/gerrit/testing/FakeGroupAuditService.java
+++ java/com/google/gerrit/testing/FakeGroupAuditService.java
@@ -14,10 +14,16 @@
 
 package com.google.gerrit.testing;
 
+import static java.util.Comparator.comparing;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.stream.Collectors.joining;
+
+import com.google.common.collect.ImmutableList;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.server.AuditEvent;
 import com.google.gerrit.server.audit.AuditListener;
 import com.google.gerrit.server.audit.AuditService;
+import com.google.gerrit.server.audit.HttpAuditEvent;
 import com.google.gerrit.server.audit.group.GroupAuditListener;
 import com.google.gerrit.server.group.GroupAuditService;
 import com.google.gerrit.server.plugincontext.PluginSetContext;
@@ -25,38 +31,76 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @Singleton
 public class FakeGroupAuditService extends AuditService {
-  public final List<AuditEvent> auditEvents = new ArrayList<>();
-
   public static class Module extends AbstractModule {
     @Override
     public void configure() {
       DynamicSet.setOf(binder(), GroupAuditListener.class);
       DynamicSet.setOf(binder(), AuditListener.class);
+
+      // Use this fake service at the Guice level rather than depending on tests binding their own
+      // audit listeners. If we used per-test listeners, then there would be a race between
+      // dispatching the audit events from HTTP requests performed during test setup in
+      // AbstractDaemonTest, and the later test setup binding the audit listener. Using a separate
+      // audit service implementation ensures all events get recorded.
       bind(GroupAuditService.class).to(FakeGroupAuditService.class);
     }
   }
+
+  private final BlockingQueue<AuditEvent> auditEvents;
 
   @Inject
   FakeGroupAuditService(
       PluginSetContext<AuditListener> auditListeners,
       PluginSetContext<GroupAuditListener> groupAuditListeners) {
     super(auditListeners, groupAuditListeners);
-  }
-
-  public void clearEvents() {
-    auditEvents.clear();
+    auditEvents = new LinkedBlockingQueue<>();
   }
 
   @Override
   public void dispatch(AuditEvent action) {
     super.dispatch(action);
-    synchronized (auditEvents) {
-      auditEvents.add(action);
-      auditEvents.notifyAll();
+    auditEvents.add(action);
+  }
+
+  public void clearEvents() throws Exception {
+    // There's no way to know whether there might be events in-flight, for example in the finally
+    // block after writing data over an HTTP connection that the caller has read. We could
+    // potentially keep track of in-flight events, but that would be an invasive change to the
+    // AuditService interface and its callers. Instead, just sleep.
+    SECONDS.sleep(5);
+    auditEvents.clear();
+  }
+
+  public ImmutableList<AuditEvent> drainEvents(int expectedSize) throws Exception {
+    ArrayList<HttpAuditEvent> result = new ArrayList<>();
+    for (int i = 0; i < expectedSize; i++) {
+      AuditEvent e = auditEvents.poll(30, SECONDS);
+      if (e == null) {
+        throw new AssertionError(
+            String.format("Timeout after receiving %d/%d audit events", i, expectedSize));
+      }
+      result.add((HttpAuditEvent) e);
     }
+    result.sort(comparing(e -> e.when));
+
+    // Poll one more time, but use a shorter timeout to avoid slowing down tests too much. This can
+    // lead to false positives (the test flakily passing when it shouldn't). Unfortunately the
+    // current audit interface doesn't let us know that there are pending audit events that might be
+    // in flight; fixing that would require something like sending separate audit start/end events.
+    AuditEvent e = auditEvents.poll(1, SECONDS);
+    if (e != null) {
+      throw new AssertionError(
+          String.format(
+              "Audit event remaining in queue after draining item #%d:\n  %s\n"
+                  + "Previous events:\n  %s",
+              expectedSize, e, result.stream().map(Object::toString).collect(joining("\n  "))));
+    }
+
+    return ImmutableList.copyOf(result);
   }
 }

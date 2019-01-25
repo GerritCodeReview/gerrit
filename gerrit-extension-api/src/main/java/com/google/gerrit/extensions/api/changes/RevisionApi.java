d9bc1a8d5ad3df66175425294f9e791091f32d2f
David Pursehouse <dpursehouse@collab.net>
2019-01-25 13:57:14 +0900

    RevisionApi: Add method to list votes per revision
    The current API allows to get votes either:
    
    - By revision, but only the votes from a specific reviewer
    
    - By change, but only all votes on the current patch set
    
    Add a method that allows to get all the votes for a specific revision.
    
    The result is returned as a multimap of the label name to a list
    of approval infos for that label.
    
    Change-Id: I35cde09b3ee059e8b03aff1c4ee2747944c3d37c

--- gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/changes/RevisionApi.java
+++ gerrit-extension-api/src/main/java/com/google/gerrit/extensions/api/changes/RevisionApi.java
@@ -14,8 +14,10 @@
 
 package com.google.gerrit.extensions.api.changes;
 
+import com.google.common.collect.ListMultimap;
 import com.google.gerrit.extensions.client.SubmitType;
 import com.google.gerrit.extensions.common.ActionInfo;
+import com.google.gerrit.extensions.common.ApprovalInfo;
 import com.google.gerrit.extensions.common.CommentInfo;
 import com.google.gerrit.extensions.common.CommitInfo;
 import com.google.gerrit.extensions.common.EditInfo;
@@ -128,6 +130,8 @@ public interface RevisionApi {
   MergeListRequest getMergeList() throws RestApiException;
 
   RelatedChangesInfo related() throws RestApiException;
+
+  ListMultimap<String, ApprovalInfo> votes() throws RestApiException;
 
   abstract class MergeListRequest {
     private boolean addLinks;
@@ -358,6 +362,11 @@ public interface RevisionApi {
 
     @Override
     public RelatedChangesInfo related() throws RestApiException {
+      throw new NotImplementedException();
+    }
+
+    @Override
+    public ListMultimap<String, ApprovalInfo> votes() throws RestApiException {
       throw new NotImplementedException();
     }
 

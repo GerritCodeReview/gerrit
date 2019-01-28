0bfcabe8af0d5376de0479f1fd30bbfc468ae6a8
Changcheng Xiao <xchangcheng@google.com>
2019-01-28 15:09:23 +0100

    CreateChange: move checks for "merge" field of the ChangeInput to #processChangeInput
    This check is required for the input and can be done before
    lots of the above code being executed. Moving it to
    \#processChangeInput so that illegal request can be rejected
    early.
    
    Change-Id: I62ed7b18bfa441dc6d7addb06a105069932acd9d

--- java/com/google/gerrit/server/restapi/change/CreateChange.java
+++ java/com/google/gerrit/server/restapi/change/CreateChange.java
@@ -250,10 +250,6 @@ public class CreateChange
       RevCommit c;
       if (input.merge != null) {
         // create a merge commit
-        if (!(submitType.equals(SubmitType.MERGE_ALWAYS)
-            || submitType.equals(SubmitType.MERGE_IF_NECESSARY))) {
-          throw new BadRequestException("Submit type: " + submitType + " is not supported");
-        }
         c = newMergeCommit(git, oi, rw, projectState, mergeTip, input.merge, author, commitMessage);
       } else {
         // create an empty commit
@@ -337,6 +333,12 @@ public class CreateChange
                 || MoreObjects.firstNonNull(
                     me.state().getGeneralPreferences().workInProgressByDefault, false)
             : input.workInProgress;
+
+    if (input.merge != null
+        && !(submitType.equals(SubmitType.MERGE_ALWAYS)
+            || submitType.equals(SubmitType.MERGE_IF_NECESSARY))) {
+      throw new BadRequestException("Submit type: " + submitType + " is not supported");
+    }
   }
 
   private void checkRequiredPermissions(Project.NameKey project, String refName)

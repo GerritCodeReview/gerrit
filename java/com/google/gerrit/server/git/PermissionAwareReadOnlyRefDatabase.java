b91d80d03231d08a9f239ee6fab1fde4ef3dcd25
Patrick Hiesel <hiesel@google.com>
2019-01-28 15:15:48 +0100

    Implement PermissionAwareRefDatabase
    Gerrit had some trouble with enabling Git Protocol V2 with refs-in-wants
    support at the end of 2018. The origin for this trouble was that JGit
    and Gerrit maintainers had no defined API for how visiblity is enforced.
    In fact, JGit doesn't promise any visibility enforcements.
    
    Gerrit previously used an AdvertiseRefsHook to filter refs by visibility
    before advertising them. Only advertised refs were fetchable (given that
    no additional settings were provided). With the Git Protocol advancing,
    this assumption is not safe anymore and we need stronger guarantees to
    make sure Gerrit and JGit play well together when it comes to security.
    
    This change is a proposal to solve the dilemma by providing a custom
    repository to JGit. The repository provides a RefDatabase that is
    permission-aware and will only ever return refs that the user has access
    to.
    
    This way, Gerrit is resilient against whatever changes are made in JGit
    because we never expose invisible refs or rely on any JGit filter
    functionality.
    
    JGit's Repository object was unfortunately not meant to be extended and
    wrapped. It is an abstract class (not an interface) that has two direct
    implementations in JGit: FileRepository and DFSRepository (abstract). We
    can either subclass Repository directly - which is what this change is
    doing - or subclass the two implementations separately.
    
    Best case, we would change Repository to be an interface instead and
    stop bothering, but this seems unfeasible at least for now.
    
    This change preserves advertise refs hook: DefaultAdvertiseRefsHook. So
    that the checks are performed twice. Follow-up change will introduce a
    configuration option to select between DefaultAdvertiseRefsHook or
    permission aware ref database.
    
    Change-Id: Id10f74f20b79a2b7851fc462d6f64acb0c7413a4

---
+++ java/com/google/gerrit/server/git/PermissionAwareReadOnlyRefDatabase.java
@@ -0,0 +1,173 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.gerrit.server.permissions.PermissionBackend.RefFilterOptions;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.inject.Inject;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.eclipse.jgit.lib.BatchRefUpdate;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefRename;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Wrapper around {@link DelegateRefDatabase} that filters all refs using {@link
+ * com.google.gerrit.server.permissions.PermissionBackend}.
+ */
+public class PermissionAwareReadOnlyRefDatabase extends DelegateRefDatabase {
+
+  private final PermissionBackend.ForProject forProject;
+
+  @Inject
+  PermissionAwareReadOnlyRefDatabase(
+      Repository delegateRepository, PermissionBackend.ForProject forProject) {
+    super(delegateRepository);
+    this.forProject = forProject;
+  }
+
+  @Override
+  public boolean isNameConflicting(String name) {
+    throw new UnsupportedOperationException("PermissionAwareReadOnlyRefDatabase is read-only");
+  }
+
+  @Override
+  public RefUpdate newUpdate(String name, boolean detach) {
+    throw new UnsupportedOperationException("PermissionAwareReadOnlyRefDatabase is read-only");
+  }
+
+  @Override
+  public RefRename newRename(String fromName, String toName) {
+    throw new UnsupportedOperationException("PermissionAwareReadOnlyRefDatabase is read-only");
+  }
+
+  @Override
+  public Ref exactRef(String name) throws IOException {
+    Ref ref = getDelegate().getRefDatabase().exactRef(name);
+    if (ref == null) {
+      return null;
+    }
+
+    Map<String, Ref> result;
+    try {
+      result =
+          forProject.filter(ImmutableMap.of(name, ref), getDelegate(), RefFilterOptions.defaults());
+    } catch (PermissionBackendException e) {
+      if (e.getCause() instanceof IOException) {
+        throw (IOException) e.getCause();
+      }
+      throw new IOException(e);
+    }
+    if (result.isEmpty()) {
+      return null;
+    }
+
+    Preconditions.checkState(
+        result.size() == 1, "Only one element expected, but was: " + result.size());
+    return Iterables.getOnlyElement(result.values());
+  }
+
+  @SuppressWarnings("deprecation")
+  @Override
+  public Map<String, Ref> getRefs(String prefix) throws IOException {
+    Map<String, Ref> refs = getDelegate().getRefDatabase().getRefs(prefix);
+    if (refs.isEmpty()) {
+      return refs;
+    }
+
+    Map<String, Ref> result;
+    try {
+      result = forProject.filter(refs, getDelegate(), RefFilterOptions.defaults());
+    } catch (PermissionBackendException e) {
+      throw new IOException("");
+    }
+    return result;
+  }
+
+  @Override
+  public List<Ref> getRefsByPrefix(String prefix) throws IOException {
+    return super.getRefsByPrefix(prefix);
+  }
+
+  @Override
+  public Map<String, Ref> exactRef(String... refs) throws IOException {
+    return super.exactRef(refs);
+  }
+
+  @Override
+  public Ref firstExactRef(String... refs) throws IOException {
+    return super.firstExactRef(refs);
+  }
+
+  @Override
+  public List<Ref> getAdditionalRefs() throws IOException {
+    return super.getAdditionalRefs();
+  }
+
+  @Override
+  public Collection<String> getConflictingNames(String name) throws IOException {
+    return super.getConflictingNames(name);
+  }
+
+  @Override
+  public List<Ref> getRefs() throws IOException {
+    return super.getRefs();
+  }
+
+  @Override
+  public List<Ref> getRefsByPrefix(String... prefixes) throws IOException {
+    return super.getRefsByPrefix(prefixes);
+  }
+
+  @Override
+  public Set<Ref> getTipsWithSha1(ObjectId id) throws IOException {
+    return super.getTipsWithSha1(id);
+  }
+
+  @Override
+  public boolean hasRefs() throws IOException {
+    return super.hasRefs();
+  }
+
+  @Override
+  public boolean hasVersioning() {
+    return super.hasVersioning();
+  }
+
+  @Override
+  public BatchRefUpdate newBatchUpdate() {
+    return super.newBatchUpdate();
+  }
+
+  @Override
+  public Ref peel(Ref ref) throws IOException {
+    return super.peel(ref);
+  }
+
+  @Override
+  public boolean performsAtomicTransactions() {
+    return super.performsAtomicTransactions();
+  }
+}

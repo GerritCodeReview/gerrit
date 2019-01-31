8107347f20474d1092bc8e032bfb4577cd6629f0
Edwin Kempin <ekempin@google.com>
2019-01-31 13:22:43 +0100

    Protect read access to checker refs in All-Projects
    By default only administrators are allowed to read the checker refs in
    All-Projects. Ensure this for new sites by setting exclusive read
    permissions for admin on refs/checkers/* in All-Projects. For existing
    sites this permission is added by a schema migration (for all groups
    that have the 'Administrate Server' capability).
    
    So far capabilities never grant implicit access to refs. Even users with
    the 'Administrate Server' capability need explicit read access rights on
    refs to be able to see and fetch them. For project owners the situation
    is similar, although they are project owners they need explicit read
    access rights on the refs/meta/config branch to be able to see and fetch
    it. To be consistent with this the 'Administrate Checkers' capability
    does not imply implicit read access rights for the checker refs. To
    read checker refs these users must be given the read access right on
    refs/checkers/* in the All-Projects repository explicitly.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I4a79680387beeea901ff9ba674ff42772bb13b67

--- java/com/google/gerrit/server/schema/AllProjectsCreator.java
+++ java/com/google/gerrit/server/schema/AllProjectsCreator.java
@@ -204,6 +204,7 @@ public class AllProjectsCreator {
     AccessSection all = config.getAccessSection(AccessSection.ALL, true);
     AccessSection tags = config.getAccessSection("refs/tags/*", true);
     AccessSection meta = config.getAccessSection(RefNames.REFS_CONFIG, true);
+    AccessSection checkers = config.getAccessSection(RefNames.REFS_CHECKERS + "*", true);
 
     grant(config, capabilities, GlobalCapability.ADMINISTRATE_SERVER, adminsGroup);
     grant(config, all, Permission.READ, adminsGroup, anonymous);
@@ -224,6 +225,9 @@ public class AllProjectsCreator {
     grant(config, meta, Permission.CREATE, adminsGroup, owners);
     grant(config, meta, Permission.PUSH, adminsGroup, owners);
     grant(config, meta, Permission.SUBMIT, adminsGroup, owners);
+
+    checkers.getPermission(Permission.READ, true).setExclusiveGroup(true);
+    grant(config, checkers, Permission.READ, adminsGroup);
   }
 
   private void initSequences(Repository git, BatchRefUpdate bru, int firstChangeId)

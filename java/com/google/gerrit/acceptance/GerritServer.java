4bf95f62b73e74845f0d622ff6dee303d8946727
Luca Milanesio <luca.milanesio@gmail.com>
2019-01-31 13:59:03 +0100

    Index projects with in-memory test
    When running integration tests with in-memory repositories making
    sure that they are all indexed in Lucene.
    This enables the implementation of tests that can rely on a
    consistent projects index to run against.
    
    Change-Id: I2e628e4555ed7faa24fff44a0d226b57b5ce9909

--- java/com/google/gerrit/acceptance/GerritServer.java
+++ java/com/google/gerrit/acceptance/GerritServer.java
@@ -406,6 +406,7 @@ public class GerritServer implements AutoCloseable {
                 bind(GerritRuntime.class).toInstance(GerritRuntime.DAEMON);
               }
             }));
+    daemon.setAdditionalSysModuleForTesting(new ReindexInMemoryProjectsAtStartup.Module());
     daemon.start();
     return new GerritServer(desc, null, createTestInjector(daemon), daemon, null);
   }

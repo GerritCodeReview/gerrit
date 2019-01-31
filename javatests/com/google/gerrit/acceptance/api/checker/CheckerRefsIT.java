991b618b92dd1087b399e471d15d8fe6fff34f50
Edwin Kempin <ekempin@google.com>
2019-01-31 13:22:43 +0100

    Protect read access to verifier refs in All-Projects
    By default only administrators are allowed to read the verifier refs in
    All-Projects. Ensure this for new sites by setting exclusive read
    permissions for admin on refs/verifiers/* in All-Projects. For existing
    sites this permission is added by a schema migration (for all groups
    that have the 'Administrate Server' capability).
    
    So far capabilities never grant implicit access to refs. Even users with
    the 'Administrate Server' capability need explicit read access rights on
    refs to be able to see and fetch them. For project owners the situation
    is similar, although they are project owners they need explicit read
    access rights on the refs/meta/config branch to be able to see and fetch
    it. To be consistent with this the 'Administrate Verifiers' capability
    does not imply implicit read access rights for the verifier refs. To
    read verifier refs these users must be given the read access right on
    refs/verifiers/* in the All-Projects repository explicitly.
    
    Change-Id: I1ac3c8e803347068c36ec5caf9bef434a0bf4ecf
    Signed-off-by: Edwin Kempin <ekempin@google.com>

---
+++ javatests/com/google/gerrit/acceptance/api/checker/CheckerRefsIT.java
@@ -0,0 +1,78 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.api.checker;
+
+import static com.google.gerrit.acceptance.GitUtil.fetch;
+import static com.google.gerrit.server.group.SystemGroupBackend.REGISTERED_USERS;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.NoHttpd;
+import com.google.gerrit.acceptance.SkipProjectClone;
+import com.google.gerrit.acceptance.testsuite.request.RequestScopeOperations;
+import com.google.gerrit.acceptance.testsuite.checker.CheckerOperations;
+import com.google.gerrit.common.data.GlobalCapability;
+import com.google.gerrit.reviewdb.client.RefNames;
+import com.google.inject.Inject;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
+import org.eclipse.jgit.junit.TestRepository;
+import org.junit.Test;
+
+@NoHttpd
+@SkipProjectClone
+public class CheckerRefsIT extends AbstractDaemonTest {
+  @Inject private RequestScopeOperations requestScopeOperations;
+  @Inject private CheckerOperations checkerOperations;
+
+  @Test
+  public void adminCanReadCheckerRefsByDefault() throws Exception {
+    String checkerUuid = checkerOperations.newChecker().create();
+    String checkerRef = RefNames.refsCheckers(checkerUuid);
+
+    TestRepository<InMemoryRepository> repo = cloneProject(allProjects, admin);
+    fetch(repo, checkerRef + ":checkerRef");
+  }
+
+  @Test
+  public void nonAdminCannotReadCheckerRefs() throws Exception {
+    requestScopeOperations.setApiUser(user.getId());
+
+    String checkerUuid = checkerOperations.newChecker().create();
+    String checkerRef = RefNames.refsCheckers(checkerUuid);
+
+    TestRepository<InMemoryRepository> repo = cloneProject(allProjects, user);
+
+    exception.expect(TransportException.class);
+    exception.expectMessage(
+        String.format("Remote does not have %s available for fetch.", checkerRef));
+    fetch(repo, checkerRef + ":checkerRef");
+  }
+
+  @Test
+  public void checkerAdminsCannotReadCheckerRefsWithoutExplicitReadPermission() throws Exception {
+    allowGlobalCapabilities(REGISTERED_USERS, GlobalCapability.ADMINISTRATE_CHECKERS);
+    requestScopeOperations.setApiUser(user.getId());
+
+    String checkerUuid = checkerOperations.newChecker().create();
+    String checkerRef = RefNames.refsCheckers(checkerUuid);
+
+    TestRepository<InMemoryRepository> repo = cloneProject(allProjects, user);
+
+    exception.expect(TransportException.class);
+    exception.expectMessage(
+        String.format("Remote does not have %s available for fetch.", checkerRef));
+    fetch(repo, checkerRef + ":checkerRef");
+  }
+}

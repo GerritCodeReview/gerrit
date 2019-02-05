eecebc44d634448bbb40c046e77a3a4fa51a04c8
Dave Borowitz <dborowitz@google.com>
2019-02-05 15:22:58 -0800

    NotifyResolver: Use SetMultimap for accounts to notify
    An account ID should only appear once per RecipientType; allowing
    duplicates of the same account ID with the same RecipientType doesn't
    make sense. This shouldn't have any effect in practice, since these
    accounts are converted to Sets within OutgoingEmail, but the code is
    cleaner this way.
    
    Change-Id: I677def1384151bfe52fa20e291864b260a9df3fb

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -49,8 +49,8 @@ import com.google.common.base.Throwables;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.ListMultimap;
@@ -1586,7 +1586,7 @@ class ReceiveCommits {
     NotifyResolver.Result getNotifyForNewChange() {
       return NotifyResolver.Result.create(
           firstNonNull(notifyHandling, workInProgress ? NotifyHandling.OWNER : NotifyHandling.ALL),
-          ImmutableListMultimap.<RecipientType, Account.Id>builder()
+          ImmutableSetMultimap.<RecipientType, Account.Id>builder()
               .putAll(RecipientType.TO, notifyTo)
               .putAll(RecipientType.CC, notifyCc)
               .putAll(RecipientType.BCC, notifyBcc)

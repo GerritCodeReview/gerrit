8da2d38c47303b873ca975603cfb6fbbd7515dd8
Patrick Hiesel <hiesel@google.com>
2019-02-06 17:13:07 +0100

    Benchmark new change ref cache design
    Serialized key size: 38 bytes
    Serialized value size: 41 bytes
    Deserializing 2M changes: 1070ms
    Deserializing 8M changes: 3465ms
    Deserializing 2M changes: 853ms
    Deserializing 8M changes: 3420ms
    Deserializing 2M changes: 890ms
    Deserializing 8M changes: 3435ms
    Deserializing 2M changes: 861ms
    Deserializing 8M changes: 3484ms
    Deserializing 2M changes: 889ms
    Deserializing 8M changes: 3481ms
    Deserializing 2M changes: 887ms
    Deserializing 8M changes: 3538ms
    Deserializing 2M changes: 878ms
    Deserializing 8M changes: 3468ms
    Deserializing 2M changes: 881ms
    Deserializing 8M changes: 3543ms
    Deserializing 2M changes: 865ms
    Deserializing 8M changes: 3572ms
    Deserializing 2M changes: 859ms
    Deserializing 8M changes: 3491ms
    
    Change-Id: Ib13e70d5c99c76bbd2fbed432563ebe5cb65be99

---
+++ javatests/com/google/gerrit/server/ChangeRefCacheBenchmarkTest.java
@@ -0,0 +1,57 @@
+package com.google.gerrit.server;
+
+import com.google.gerrit.server.cache.proto.Cache.ChangeRefKeyProto;
+import com.google.gerrit.server.cache.proto.Cache.ChangeRefStateProto;
+import com.google.gerrit.testing.GerritBaseTests;
+import com.google.protobuf.ByteString;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.junit.Test;
+
+public class ChangeRefCacheBenchmarkTest extends GerritBaseTests {
+
+  @Test
+  public void benchmark() throws Exception {
+    ObjectId metaSha1 = ObjectId.fromString("1234567812345678123456781234567812345678");
+    byte[] buf = new byte[Constants.OBJECT_ID_LENGTH];
+    metaSha1.copyRawTo(buf, 0);
+
+    ChangeRefKeyProto key = ChangeRefKeyProto.newBuilder()
+        .setChangeId(12345)
+        .setRepoKey("chromium/chromium/src/nested/repo")
+        .build();
+    ChangeRefStateProto value = ChangeRefStateProto.newBuilder()
+        .setMetaSha1(ByteString.copyFrom(buf))
+        .setOwnerAccountId(12345)
+        .addReviewerAccountId(123444)
+        .addReviewerAccountId(33333)
+        .addCcAccountId(12312312)
+        .setIsPrivate(true)
+        .build();
+
+    System.err.println("Serialized key size: " + key.toByteArray().length + " bytes");
+    System.err.println("Serialized value size: " + value.toByteArray().length + " bytes");
+
+    for (int i = 0; i< 10; i++) {
+      System.err.println("Deserializing 2M changes: " + deserialize(key, value, 2000000) + "ms");
+      System.err.println("Deserializing 8M changes: " + deserialize(key, value, 8000000) + "ms");
+    }
+  }
+
+  /** Deserialize both key and value numObjects times and return the duration in ms. */
+  private static long deserialize(ChangeRefKeyProto key, ChangeRefStateProto value, int numObjects) throws Exception {
+    byte[] keyBytes = key.toByteArray();
+    byte[] valueBytes = value.toByteArray();
+
+    long start = System.currentTimeMillis();
+    for (int i = 0; i < numObjects; i++) {
+      if (!ChangeRefKeyProto.parseFrom(keyBytes).equals(key)) {
+        throw new RuntimeException("failed to parse key");
+      }
+      if (!ChangeRefStateProto.parseFrom(valueBytes).equals(value)) {
+        throw new RuntimeException("failed to parse value");
+      }
+    }
+    return System.currentTimeMillis() - start;
+  }
+}

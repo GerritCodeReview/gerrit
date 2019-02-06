4841c7d11f458d73bd2046f205d4bb53a9b2ccf3
Patrick Hiesel <hiesel@google.com>
2019-02-06 17:13:07 +0100

    Benchmark new change ref cache design
    Serialized key size: 38 bytes
    Serialized value size: 65 bytes
    Deserializing 2M changes: 1230ms
    Deserializing 8M changes: 4081ms
    Deserializing 2M changes: 1002ms
    Deserializing 8M changes: 4021ms
    Deserializing 2M changes: 1032ms
    Deserializing 8M changes: 4005ms
    Deserializing 2M changes: 997ms
    Deserializing 8M changes: 4040ms
    Deserializing 2M changes: 1003ms
    Deserializing 8M changes: 4048ms
    Deserializing 2M changes: 1020ms
    Deserializing 8M changes: 4023ms
    Deserializing 2M changes: 1013ms
    Deserializing 8M changes: 4031ms
    Deserializing 2M changes: 1006ms
    Deserializing 8M changes: 4026ms
    Deserializing 2M changes: 1001ms
    Deserializing 8M changes: 4054ms
    Deserializing 2M changes: 1004ms
    Deserializing 8M changes: 4031ms
    
    MAT heapdump analysis:
    - Key: 24 bytes shallow, 80 bytes retained
    - Value: 40 bytes shallow, 120 bytes retained
    
    8M x (120 + 80) = 1.68 GiB
    
    With Cache overhead: 8 x 176.3 MiB retained heap for 1M changes = 1.41
    GiB
    
    https://screenshot.googleplex.com/BeRNyZtSN0C
    
    Change-Id: Ib13e70d5c99c76bbd2fbed432563ebe5cb65be99

---
+++ javatests/com/google/gerrit/server/Dumper.java
@@ -0,0 +1,64 @@
+package com.google.gerrit.server;
+
+import com.sun.management.HotSpotDiagnosticMXBean;
+import java.lang.management.ManagementFactory;
+import javax.management.MBeanServer;
+
+public class Dumper {
+  // This is the name of the HotSpot Diagnostic MBean
+  private static final String HOTSPOT_BEAN_NAME = "com.sun.management:type=HotSpotDiagnostic";
+  // field to store the hotspot diagnostic MBean
+  private static volatile HotSpotDiagnosticMXBean hotspotMBean;
+
+  static void dumpHeap(String fileName, boolean live) {
+    // initialize hotspot diagnostic MBean
+    initHotspotMBean();
+    try {
+      hotspotMBean.dumpHeap(fileName, live);
+    } catch (RuntimeException re) {
+      throw re;
+    } catch (Exception exp) {
+      throw new RuntimeException(exp);
+    }
+  }
+  // initialize the hotspot diagnostic MBean field
+  private static void initHotspotMBean() {
+    if (hotspotMBean == null) {
+      synchronized (Dumper.class) {
+        if (hotspotMBean == null) {
+          hotspotMBean = getHotspotMBean();
+        }
+      }
+    }
+  }
+  // get the hotspot diagnostic MBean from the
+  // platform MBean server
+  private static HotSpotDiagnosticMXBean getHotspotMBean() {
+    try {
+      MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+      HotSpotDiagnosticMXBean bean =
+          ManagementFactory.newPlatformMXBeanProxy(
+              server, HOTSPOT_BEAN_NAME, HotSpotDiagnosticMXBean.class);
+      return bean;
+    } catch (RuntimeException re) {
+      throw re;
+    } catch (Exception exp) {
+      throw new RuntimeException(exp);
+    }
+  }
+  // public static void main(String[] args) {
+  //   // default heap dump file name
+  //   String fileName = "heap.bin";
+  //   // by default dump only the live objects
+  //   boolean live = true;
+  //   // simple command line options
+  //   switch (args.length) {
+  //     case 2:
+  //       live = args[1].equals("true");
+  //     case 1:
+  //       fileName = args[0];
+  //   }
+  //   // dump the heap
+  //   dumpHeap(fileName, live);
+  // }
+}

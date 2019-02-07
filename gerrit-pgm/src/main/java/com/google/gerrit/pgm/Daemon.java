57e4afc99fc4480cbc2d3c881d63ad2c0358bedf
David Pursehouse <dpursehouse@collab.net>
2019-02-07 17:15:09 +0900

    Daemon: Show status of slave and headless mode in 'ready' log
    When slave mode or headless mode is activated, include '[slave]'
    and '[headless]' respectively in the 'ready' info log.
    
    Change-Id: I5789b22d32e0260cb676a91d375e919838d642a1

--- gerrit-pgm/src/main/java/com/google/gerrit/pgm/Daemon.java
+++ gerrit-pgm/src/main/java/com/google/gerrit/pgm/Daemon.java
@@ -15,10 +15,12 @@
 package com.google.gerrit.pgm;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.gerrit.common.Version.getVersion;
 import static com.google.gerrit.server.schema.DataSourceProvider.Context.MULTI_USER;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
 import com.google.common.base.MoreObjects;
 import com.google.gerrit.audit.AuditModule;
 import com.google.gerrit.common.EventBroker;
@@ -369,7 +371,15 @@ public class Daemon extends SiteProgram {
   }
 
   private String myVersion() {
-    return com.google.gerrit.common.Version.getVersion();
+    List<String> versionParts = new ArrayList<>();
+    if (slave) {
+      versionParts.add("[slave]");
+    }
+    if (headless) {
+      versionParts.add("[headless]");
+    }
+    versionParts.add(getVersion());
+    return Joiner.on(" ").join(versionParts);
   }
 
   private Injector createCfgInjector() {

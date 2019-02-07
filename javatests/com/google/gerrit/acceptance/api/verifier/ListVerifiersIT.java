be1fc4071bc7a6416a259c7044071a8ae8d00062
Edwin Kempin <ekempin@google.com>
2019-02-07 14:35:48 +0100

    Reset verifier refs after test execution
    This ensures that each test only sees verifiers that it creates itself.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I8d08813b1696466bc8023cf7d38caf55c676c1c0

--- javatests/com/google/gerrit/acceptance/api/verifier/ListVerifiersIT.java
+++ javatests/com/google/gerrit/acceptance/api/verifier/ListVerifiersIT.java
@@ -20,7 +20,6 @@ import static java.util.stream.Collectors.toList;
 import com.google.common.collect.ImmutableList;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.NoHttpd;
-import com.google.gerrit.acceptance.Sandboxed;
 import com.google.gerrit.acceptance.SkipProjectClone;
 import com.google.gerrit.acceptance.testsuite.request.RequestScopeOperations;
 import com.google.gerrit.acceptance.testsuite.verifier.VerifierOperations;
@@ -37,7 +36,6 @@ import org.junit.Test;
 
 @NoHttpd
 @SkipProjectClone
-@Sandboxed
 public class ListVerifiersIT extends AbstractDaemonTest {
   @Inject private RequestScopeOperations requestScopeOperations;
   @Inject private VerifierOperations verifierOperations;

0e45618828c7c2d1a985f4141c02c34a33503957
Edwin Kempin <ekempin@google.com>
2019-02-07 11:46:58 +0100

    Allow listing checkers
    The implementation of listing checkers is very basic and doesn't scale
    for large numbers of checkers. For now all checkers are directly loaded
    from NoteDb on each request. Optimizations like caching, parallel
    loading or limiting the results should be implemented in the future.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I4f7b20cc136ddf6dd77218b6db5d18fd908a71fe

--- java/com/google/gerrit/server/restapi/checker/CheckersCollection.java
+++ java/com/google/gerrit/server/restapi/checker/CheckersCollection.java
@@ -41,6 +41,7 @@ public class CheckersCollection implements RestCollection<TopLevelResource, Chec
   private final Provider<CurrentUser> self;
   private final GlobalChecksConfig globalChecksConfig;
   private final PermissionBackend permissionBackend;
+  private final ListCheckers listCheckers;
   private final Checkers checkers;
   private final DynamicMap<RestView<CheckerResource>> views;
 
@@ -49,21 +50,20 @@ public class CheckersCollection implements RestCollection<TopLevelResource, Chec
       Provider<CurrentUser> self,
       GlobalChecksConfig globalChecksConfig,
       PermissionBackend permissionBackend,
+      ListCheckers listCheckers,
       Checkers checkers,
       DynamicMap<RestView<CheckerResource>> views) {
     this.self = self;
     this.globalChecksConfig = globalChecksConfig;
     this.permissionBackend = permissionBackend;
+    this.listCheckers = listCheckers;
     this.checkers = checkers;
     this.views = views;
   }
 
   @Override
   public RestView<TopLevelResource> list() throws RestApiException {
-    globalChecksConfig.checkThatApiIsEnabled();
-
-    // TODO(ekempin): implement this
-    throw new ResourceNotFoundException();
+    return listCheckers;
   }
 
   @Override

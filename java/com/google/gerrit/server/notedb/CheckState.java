bf63b084267008f77449b4299abb32afba6bfd49
Dave Borowitz <dborowitz@google.com>
2019-02-11 15:01:26 -0800

    Define enums for individual and combined checker states
    Change-Id: I693a1f983851cdc54380cd758bf90ce19ff4c05c

---
+++ java/com/google/gerrit/server/notedb/CheckState.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.notedb;
+
+/**
+ * State of a single check on a change.
+ *
+ * <p>This state applies to a single check; for the aggregated state associated with a change, see
+ * {@link CombinedCheckState}.
+ *
+ * <p>Ordering is not significant in this class, but for consistency's sake the ordering matches
+ * {@code CombinedCheckState} where applicable.
+ */
+public enum CheckState {
+  /** The check terminated and failed. */
+  FAILED,
+
+  /** The check is relevant to the change, but the checker has not started work. */
+  NOT_STARTED,
+
+  /**
+   * The checker has acknowledged that it has work to do on the change, and will start work in the
+   * future.
+   */
+  SCHEDULED,
+
+  /** The checker is currently running the check. */
+  RUNNING,
+
+  /** The check terminated and succeeded. */
+  SUCCESSFUL,
+
+  /** The check is not relevant for the change. */
+  NOT_RELEVANT
+}

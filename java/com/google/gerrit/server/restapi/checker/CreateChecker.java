b7f0911ac4b9462c98dbb455d97587c43bcb8686
Han-Wen Nienhuys <hanwen@google.com>
2019-02-20 16:09:44 +0100

    Add mandatory repository property for checkers
    The repository property of a checker defines for which repository the
    checker applies. For now this must be the exact name of an existing
    repository. In future we may consider to support checkers that apply to
    multiple repositories, either by specifying a repository list, a prefix
    or a regular expression.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I9d43129c062b75398042556f38ba82b7acb16989

--- java/com/google/gerrit/server/restapi/checker/CreateChecker.java
+++ java/com/google/gerrit/server/restapi/checker/CreateChecker.java
@@ -21,6 +21,8 @@ import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestCollectionModifyView;
 import com.google.gerrit.extensions.restapi.TopLevelResource;
+import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
+import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.UserInitiated;
 import com.google.gerrit.server.checker.Checker;
 import com.google.gerrit.server.checker.CheckerCreation;
@@ -34,6 +36,8 @@ import com.google.gerrit.server.checker.GlobalChecksConfig;
 import com.google.gerrit.server.permissions.GlobalPermission;
 import com.google.gerrit.server.permissions.PermissionBackend;
 import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.project.ProjectState;
 import com.google.gwtorm.server.OrmDuplicateKeyException;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -48,17 +52,20 @@ public class CreateChecker
   private final PermissionBackend permissionBackend;
   private final Provider<CheckersUpdate> checkersUpdate;
   private final CheckerJson checkerJson;
+  private final ProjectCache projectCache;
 
   @Inject
   public CreateChecker(
       GlobalChecksConfig globalChecksConfig,
       PermissionBackend permissionBackend,
       @UserInitiated Provider<CheckersUpdate> checkersUpdate,
-      CheckerJson checkerJson) {
+      CheckerJson checkerJson,
+      ProjectCache projectCache) {
     this.globalChecksConfig = globalChecksConfig;
     this.permissionBackend = permissionBackend;
     this.checkersUpdate = checkersUpdate;
     this.checkerJson = checkerJson;
+    this.projectCache = projectCache;
   }
 
   @Override
@@ -76,10 +83,14 @@ public class CreateChecker
     if (name.isEmpty()) {
       throw new BadRequestException("name is required");
     }
+    Project.NameKey repository = resolveRepository(input.repository);
 
     String checkerUuid = CheckerUuid.make(name);
     CheckerCreation.Builder checkerCreationBuilder =
-        CheckerCreation.builder().setCheckerUuid(checkerUuid).setName(name);
+        CheckerCreation.builder()
+            .setCheckerUuid(checkerUuid)
+            .setName(name)
+            .setRepository(repository);
     CheckerUpdate.Builder checkerUpdateBuilder = CheckerUpdate.builder();
     if (input.description != null && !input.description.trim().isEmpty()) {
       checkerUpdateBuilder.setDescription(input.description.trim());
@@ -92,5 +103,19 @@ public class CreateChecker
             .get()
             .createChecker(checkerCreationBuilder.build(), checkerUpdateBuilder.build());
     return Response.created(checkerJson.format(checker));
+  }
+
+  private Project.NameKey resolveRepository(String repository)
+      throws BadRequestException, UnprocessableEntityException, IOException {
+    if (repository == null || repository.trim().isEmpty()) {
+      throw new BadRequestException("repository is required");
+    }
+
+    ProjectState projectState = projectCache.checkedGet(new Project.NameKey(repository.trim()));
+    if (projectState == null) {
+      throw new UnprocessableEntityException(String.format("repository %s not found", repository));
+    }
+
+    return projectState.getNameKey();
   }
 }

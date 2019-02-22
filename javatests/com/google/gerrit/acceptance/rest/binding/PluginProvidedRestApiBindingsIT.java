13035e71cbc5a9248b2edbea68f9fbe41af7c88f
Dave Borowitz <dborowitz@google.com>
2019-02-22 14:00:50 -0800

    Add acceptance test for plugin URL bindings
    For now, just tests a top-level binding under /plugins/X. Future tests
    could check views bound in a RestApiModule under an existing collection,
    but I wasn't able to get those working in this change.
    
    These tests are in a separate class from the old
    PluginsRestApiBindingsIT because they are really testing a different
    type of endpoint: the former is about the remote admin endpoints, which
    is different from actual plugin-provided endpoints. Rename
    PluginsRestApiBindingsIT accordingly.
    
    Change-Id: I71b82a14ded11cc77baf1313d82bc714f8602f67

---
+++ javatests/com/google/gerrit/acceptance/rest/binding/PluginProvidedRestApiBindingsIT.java
@@ -0,0 +1,62 @@
+package com.google.gerrit.acceptance.rest.binding;
+
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.rest.util.RestApiCallHelper;
+import com.google.gerrit.acceptance.rest.util.RestCall;
+import com.google.inject.Singleton;
+import com.google.inject.servlet.ServletModule;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Test;
+
+/**
+ * Tests for checking plugin-provided REST API bindings.
+ *
+ * <p>These tests only verify that the plugin-provided REST endpoints are correctly bound, they do no test
+ * the functionality of the plugin REST endpoints.
+ */
+public class PluginProvidedRestApiBindingsIT extends AbstractDaemonTest {
+
+  /**
+   * Plugin REST endpoints bound by {@link MyPluginModule} with Guice serlvet definitions.
+   *
+   * <p>Each URL contains a placeholder for the plugin identifier.
+   *
+   * <p>Currently does not include any resource or documentation URLs, since those would require
+   * installing a plugin from a jar, which is trickier than just defining a module in this file.
+   */
+  private static final ImmutableList<RestCall> SERVER_TOP_LEVEL_PLUGIN_ENDPOINTS =
+      ImmutableList.of(RestCall.get("/plugins/%s/hello"));
+
+
+  static class MyPluginModule extends ServletModule {
+    @Override
+    public void configureServlets() {
+      serve("/hello").with(HelloServlet.class);
+    }
+  }
+
+  @Singleton
+  static class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+      res.setStatus(SC_OK);
+      res.getWriter().println("Hello world");
+    }
+  }
+
+  @Test
+  public void serverPluginTopLevelEndpoints() throws Exception {
+    String pluginName = "my-plugin";
+    try (AutoCloseable ignored = installPlugin(pluginName, null, MyPluginModule.class, null)) {
+      RestApiCallHelper.execute(adminRestSession, SERVER_TOP_LEVEL_PLUGIN_ENDPOINTS, pluginName);
+    }
+  }
+}

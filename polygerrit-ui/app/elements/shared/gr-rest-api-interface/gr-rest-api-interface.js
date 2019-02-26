8c5939f16f4ca5515955c70cd0daa1de5a8be469
Thomas Shafer <tshafer@google.com>
2019-02-25 16:22:56 -0800

    Add mechanism for plugins to modify rest api calls
    A plugin can registers a rest api hook to add plugin params to core rest api
    calls. The addParameterFunction only needs to return plugin specific params,
    not the entire params object.
    
    This will slow down the initial call to get changes and the change detail as
    the xhr now waits for the plugins to finish loading. However once plugins are
    loaded, the xhr will fire as quickly as before.
    
    This is currently only supported in the changes query and change detail.
    
    Change-Id: I894628663c6616152030d05e41a0008d1601af3f

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.js
@@ -1246,48 +1246,51 @@
      *     changeInfos.
      */
     getChanges(opt_changesPerPage, opt_query, opt_offset, opt_options) {
-      const options = opt_options || this.listChangesOptionsToHex(
-          this.ListChangesOption.LABELS,
-          this.ListChangesOption.DETAILED_ACCOUNTS
-      );
-      // Issue 4524: respect legacy token with max sortkey.
-      if (opt_offset === 'n,z') {
-        opt_offset = 0;
-      }
-      const params = {
-        O: options,
-        S: opt_offset || 0,
-      };
-      if (opt_changesPerPage) { params.n = opt_changesPerPage; }
-      if (opt_query && opt_query.length > 0) {
-        params.q = opt_query;
-      }
-      const iterateOverChanges = arr => {
-        for (const change of (arr || [])) {
-          this._maybeInsertInLookup(change);
+      return Gerrit.awaitPluginsLoaded().then(() => {
+        const options = opt_options || this.listChangesOptionsToHex(
+            this.ListChangesOption.LABELS,
+            this.ListChangesOption.DETAILED_ACCOUNTS
+        );
+        // Issue 4524: respect legacy token with max sortkey.
+        if (opt_offset === 'n,z') {
+          opt_offset = 0;
         }
-      };
-      const req = {
-        url: '/changes/',
-        params,
-        reportUrlAsIs: true,
-      };
-      return this._fetchJSON(req).then(response => {
-        // Response may be an array of changes OR an array of arrays of
-        // changes.
-        if (opt_query instanceof Array) {
-          // Normalize the response to look like a multi-query response
-          // when there is only one query.
-          if (opt_query.length === 1) {
-            response = [response];
-          }
-          for (const arr of response) {
-            iterateOverChanges(arr);
-          }
-        } else {
-          iterateOverChanges(response);
+        const params = {
+          O: options,
+          S: opt_offset || 0,
+        };
+        if (opt_changesPerPage) { params.n = opt_changesPerPage; }
+        if (opt_query && opt_query.length > 0) {
+          params.q = opt_query;
         }
-        return response;
+        const iterateOverChanges = arr => {
+          for (const change of (arr || [])) {
+            this._maybeInsertInLookup(change);
+          }
+        };
+        Object.assign(params, Gerrit._pluginParams('changes', params));
+        const req = {
+          url: '/changes/',
+          params,
+          reportUrlAsIs: true,
+        };
+        return this._fetchJSON(req).then(response => {
+          // Response may be an array of changes OR an array of arrays of
+          // changes.
+          if (opt_query instanceof Array) {
+            // Normalize the response to look like a multi-query response
+            // when there is only one query.
+            if (opt_query.length === 1) {
+              response = [response];
+            }
+            for (const arr of response) {
+              iterateOverChanges(arr);
+            }
+          } else {
+            iterateOverChanges(response);
+          }
+          return response;
+        });
       });
     },
 
@@ -1368,9 +1371,15 @@
      * @param {function()=} opt_cancelCondition
      */
     _getChangeDetail(changeNum, optionsHex, opt_errFn, opt_cancelCondition) {
-      return this.getChangeActionURL(changeNum, null, '/detail').then(url => {
+      return Promise.all([
+        Gerrit.awaitPluginsLoaded(),
+        this.getChangeActionURL(changeNum, null, '/detail'),
+      ]).then(([_,url]) => {
         const urlWithParams = this._urlWithParams(url, optionsHex);
+
         const params = {O: optionsHex};
+        Object.assign(params, Gerrit._pluginParams('change', params));
+
         const req = {
           url,
           errFn: opt_errFn,

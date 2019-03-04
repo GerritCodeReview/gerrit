d03fd4cb9589247eb44ea75fd020346784fcdce1
Patrick Hiesel <hiesel@google.com>
2019-03-04 16:00:42 +0100

    Fix child collection bindings in RestApiServlet and add tests
    The Gerrit Plugin injector and the RestApiServlet currently have
    different bugs. This commit pets over the most pressing ones and adds
    tests so that plugins can bind child collections and offer modifications
    directly on the collections (e.g. postOnCollection).
    
    When binding rest views from plugins, the plugin name is incorrectly set
    to "gerrit" which is what we use for core Gerrit views. This does not
    apply to collections though. These are bound with the correct plugin
    name "plugin-name".
    
    In RestApiServlet, this leads to a situation where the collection thinks
    it belongs to a different part of Gerrit. For operations that modify the
    collection (post, delete) this means we have to use "gerrit" as the
    component name when looking up child views. For root collections, we
    already do this and this is why it works for existing plugin root
    collections. For subcollections, however, we did not - which is what
    this commit changes. Now the two usages in RestApiServlet are
    consistent.
    
    To be clear: They are now consistently wrong across the different parts
    of Gerrit, but they are consistent which means they work.
    
    As future work, we want to fix this long standing issue by binding the
    correct plugin names from the beginning. This is non-trivial though
    because the DynamicMapProvider has no context on the plugin that is
    performing the binding.
    
    This commit adds the same logic that we have for core-collection
    delegation to plugin-collections in RestApiServlet.
    
    Change-Id: I954105155c4a354b6c577d8cc246d00998e33f57

--- javatests/com/google/gerrit/acceptance/rest/binding/PluginProvidedRestApiBindingsIT.java
+++ javatests/com/google/gerrit/acceptance/rest/binding/PluginProvidedRestApiBindingsIT.java
@@ -14,18 +14,37 @@
 
 package com.google.gerrit.acceptance.rest.binding;
 
+import static com.google.gerrit.server.change.RevisionResource.REVISION_KIND;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 
 import com.google.common.collect.ImmutableList;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.rest.util.RestApiCallHelper;
 import com.google.gerrit.acceptance.rest.util.RestCall;
+import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.ChildCollection;
+import com.google.gerrit.extensions.restapi.IdString;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.extensions.restapi.RestApiModule;
+import com.google.gerrit.extensions.restapi.RestCollectionModifyView;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.extensions.restapi.RestResource;
+import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.server.change.RevisionResource;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
 import com.google.inject.servlet.ServletModule;
 import java.io.IOException;
+import java.util.Optional;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -37,7 +56,7 @@ import org.junit.Test;
 public class PluginProvidedRestApiBindingsIT extends AbstractDaemonTest {
 
   /**
-   * Plugin REST endpoints bound by {@link MyPluginModule} with Guice serlvet definitions.
+   * Plugin REST endpoints bound by {@link MyPluginHttpModule} with Guice serlvet definitions.
    *
    * <p>Each URL contains a placeholder for the plugin identifier.
    *
@@ -47,13 +66,44 @@ public class PluginProvidedRestApiBindingsIT extends AbstractDaemonTest {
   private static final ImmutableList<RestCall> SERVER_TOP_LEVEL_PLUGIN_ENDPOINTS =
       ImmutableList.of(RestCall.get("/plugins/%s/hello"));
 
-  static class MyPluginModule extends ServletModule {
+  /** Resource to bind a child collection. */
+  public static final TypeLiteral<RestView<TestPluginResource>> TEST_KIND =
+      new TypeLiteral<RestView<TestPluginResource>>() {};
+
+  private static final String PLUGIN_NAME = "my-plugin";
+
+  /** Module for all HTTP bindings. */
+  static class MyPluginHttpModule extends ServletModule {
     @Override
     public void configureServlets() {
       serve("/hello").with(HelloServlet.class);
     }
   }
 
+  /**
+   * Module for all sys bindings.
+   *
+   * <p>TODO: This should actually just move into MyPluginHttpModule. However, that doesn't work
+   * currently. This TODO is for fixing this bug.
+   */
+  static class MyPluginSysModule extends AbstractModule {
+    @Override
+    public void configure() {
+      install(
+          new RestApiModule() {
+            @Override
+            public void configure() {
+              DynamicMap.mapOf(binder(), TEST_KIND);
+              child(REVISION_KIND, "test-collection").to(TestChildCollection.class);
+              postOnCollection(TEST_KIND).to(TestPostOnCollection.class);
+              post(TEST_KIND, "update").to(TestPost.class);
+              get(TEST_KIND, "detail").to(TestGet.class);
+            }
+          });
+    }
+  }
+
+  /** Servlet to bind a root collection for the plugin. */
   @Singleton
   static class HelloServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -65,11 +115,123 @@ public class PluginProvidedRestApiBindingsIT extends AbstractDaemonTest {
     }
   }
 
+  static class TestPluginResource implements RestResource {}
+
+  @Singleton
+  static class TestChildCollection
+      implements ChildCollection<RevisionResource, TestPluginResource> {
+    private final DynamicMap<RestView<TestPluginResource>> views;
+
+    @Inject
+    TestChildCollection(DynamicMap<RestView<TestPluginResource>> views) {
+      this.views = views;
+    }
+
+    @Override
+    public RestView<RevisionResource> list() throws RestApiException {
+      return (RestReadView<RevisionResource>) resource -> ImmutableList.of("one", "two");
+    }
+
+    @Override
+    public TestPluginResource parse(RevisionResource parent, IdString id) throws Exception {
+      return new TestPluginResource();
+    }
+
+    @Override
+    public DynamicMap<RestView<TestPluginResource>> views() {
+      return views;
+    }
+  }
+
+  @Singleton
+  static class TestPostOnCollection
+      implements RestCollectionModifyView<RevisionResource, TestPluginResource, String> {
+    @Override
+    public Object apply(RevisionResource parentResource, String input) throws Exception {
+      return "test";
+    }
+  }
+
+  @Singleton
+  static class TestPost implements RestModifyView<TestPluginResource, String> {
+    @Override
+    public String apply(TestPluginResource resource, String input) throws Exception {
+      return "test";
+    }
+  }
+
+  @Singleton
+  static class TestGet implements RestReadView<TestPluginResource> {
+    @Override
+    public String apply(TestPluginResource resource) throws Exception {
+      return "test";
+    }
+  }
+
+  private Optional<AutoCloseable> pluginContext = Optional.empty();
+
+  @Before
+  public void setUp() throws Exception {
+    pluginContext =
+        Optional.of(
+            installPlugin(PLUGIN_NAME, MyPluginSysModule.class, MyPluginHttpModule.class, null));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    if (pluginContext.isPresent()) {
+      pluginContext.get().close();
+      pluginContext = Optional.empty();
+    }
+  }
+
   @Test
   public void serverPluginTopLevelEndpoints() throws Exception {
-    String pluginName = "my-plugin";
-    try (AutoCloseable ignored = installPlugin(pluginName, null, MyPluginModule.class, null)) {
-      RestApiCallHelper.execute(adminRestSession, SERVER_TOP_LEVEL_PLUGIN_ENDPOINTS, pluginName);
-    }
+    RestApiCallHelper.execute(adminRestSession, SERVER_TOP_LEVEL_PLUGIN_ENDPOINTS, PLUGIN_NAME);
+  }
+
+  @Test
+  public void childCollectionToCoreCollection() throws Exception {
+    RestApiCallHelper.execute(adminRestSession, SERVER_TOP_LEVEL_PLUGIN_ENDPOINTS, PLUGIN_NAME);
+  }
+
+  @Test
+  public void listOnChildCollection() throws Exception {
+    PatchSet.Id patchSetId = createChange().getPatchSetId();
+    RestApiCallHelper.execute(
+        adminRestSession,
+        RestCall.get("/changes/%s/revisions/%s/test-collection/"),
+        String.valueOf(patchSetId.changeId.id),
+        String.valueOf(patchSetId.patchSetId));
+  }
+
+  @Test
+  public void getOnChildCollectionWithResource() throws Exception {
+    PatchSet.Id patchSetId = createChange().getPatchSetId();
+    RestApiCallHelper.execute(
+        adminRestSession,
+        RestCall.get("/changes/%s/revisions/%s/test-collection/1/detail"),
+        String.valueOf(patchSetId.changeId.id),
+        String.valueOf(patchSetId.patchSetId));
+  }
+
+  @Test
+  public void postOnChildCollection() throws Exception {
+    PatchSet.Id patchSetId = createChange().getPatchSetId();
+    RestApiCallHelper.execute(
+        adminRestSession,
+        RestCall.post("/changes/%s/revisions/%s/test-collection/"),
+        String.valueOf(patchSetId.changeId.id),
+        String.valueOf(patchSetId.patchSetId));
+  }
+
+  @Test
+  public void postOnChildCollectionWithResource() throws Exception {
+    PatchSet.Id patchSetId = createChange().getPatchSetId();
+    RestApiCallHelper.execute(
+        adminRestSession,
+        RestCall.post("/changes/%s/revisions/%s/test-collection/1/update"),
+        String.valueOf(patchSetId.changeId.id),
+        String.valueOf(patchSetId.patchSetId));
   }
 }

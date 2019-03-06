c0aafc15faf95b171f6119565b266e6ec70c0449
Han-Wen Nienhuys <hanwen@google.com>
2019-03-06 13:38:24 +0100

    AsyncReceiveCommits: add metric for total push latency
    This metric reflects the user experience of pushing to Gerrit. Add a
    new push type "NORMAL" to distinguish AUTOCLOSE from Gerrit serving
    standard git push (which should be much faster).
    
    Change-Id: I1aae3bc0c0fe430086221503b8e529fa06967517

--- java/com/google/gerrit/server/git/receive/AsyncReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/AsyncReceiveCommits.java
@@ -179,6 +179,7 @@ public class AsyncReceiveCommits implements PreReceiveHook {
   private static class Metrics {
     private final Histogram1<ResultChangeIds.Key> changes;
     private final Timer1<String> latencyPerChange;
+    private final Timer1<String> latencyPerPush;
     private final Counter0 timeouts;
 
     @Inject
@@ -188,16 +189,23 @@ public class AsyncReceiveCommits implements PreReceiveHook {
               "receivecommits/changes",
               new Description("number of changes uploaded in a single push.").setCumulative(),
               Field.ofEnum(
-                  ResultChangeIds.Key.class,
-                  "type",
-                  "type of update (replace, create, autoclose)"));
+                  ResultChangeIds.Key.class, "type", "type of push (replace, create, autoclose)"));
       latencyPerChange =
           metricMaker.newTimer(
               "receivecommits/latency",
-              new Description("average delay per updated change")
+              new Description(
+                      "processing delay per push, averaged over the updated changes in a push.")
                   .setUnit(Units.MILLISECONDS)
                   .setCumulative(),
-              Field.ofString("type", "type of update (create/replace, autoclose)"));
+              Field.ofString("type", "type of push (create/replace, autoclose)"));
+
+      latencyPerPush =
+          metricMaker.newTimer(
+              "receivecommits/push_latency",
+              new Description("processing delay for a processing single push")
+                  .setUnit(Units.MILLISECONDS)
+                  .setCumulative(),
+              Field.ofString("type", "type of push (create/replace, autoclose, normal)"));
 
       timeouts =
           metricMaker.newCounter(
@@ -350,16 +358,26 @@ public class AsyncReceiveCommits implements PreReceiveHook {
       totalChanges += replaced.size() + created.size();
     } else {
       List<Change.Id> autoclosed = resultChangeIds.get(ResultChangeIds.Key.AUTOCLOSED);
-      metrics.changes.record(ResultChangeIds.Key.AUTOCLOSED, autoclosed.size());
-      totalChanges += autoclosed.size();
+      if (!autoclosed.isEmpty()) {
+        metrics.changes.record(ResultChangeIds.Key.AUTOCLOSED, autoclosed.size());
+        totalChanges += autoclosed.size();
+      }
+    }
+
+    String pushType;
+    if (resultChangeIds.isMagicPush()) {
+      pushType = "CREATE_REPLACE";
+    } else if (totalChanges > 0) {
+      pushType = ResultChangeIds.Key.AUTOCLOSED.name();
+    } else {
+      pushType = "NORMAL";
     }
 
     if (totalChanges > 0) {
-      metrics.latencyPerChange.record(
-          resultChangeIds.isMagicPush() ? "CREATE_REPLACE" : ResultChangeIds.Key.AUTOCLOSED.name(),
-          deltaNanos / totalChanges,
-          NANOSECONDS);
+      metrics.latencyPerChange.record(pushType, deltaNanos / totalChanges, NANOSECONDS);
     }
+
+    metrics.latencyPerPush.record(pushType, deltaNanos, NANOSECONDS);
   }
 
   /** Returns the Change.Ids that were processed in onPreReceive */

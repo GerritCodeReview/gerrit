5381c22e63b9eb1cdd143ac6947e6ef2263971e3
David Pursehouse <dpursehouse@collab.net>
2019-03-08 17:39:21 +0900

    LogThreshold: Allow the annotation to be inherited
    With the current implementation the log threshold is only set when the
    annotation is done on the exact class, which means that in cases where
    an abstract test class is extended by several derived test classes, it
    is necessary to add the annotation on each derived class individually.
    
    Allow the annotation to be inherited, so that is is possible to only
    annotate the base class, and it will also be effective for all derived
    classes.
    
    Change-Id: I0501b2a617161a391478313b6f9891108bdf9969

--- java/com/google/gerrit/acceptance/LogThreshold.java
+++ java/com/google/gerrit/acceptance/LogThreshold.java
@@ -17,11 +17,13 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Target({TYPE, METHOD})
 @Retention(RUNTIME)
+@Inherited
 public @interface LogThreshold {
   String level() default "DEBUG";
 }

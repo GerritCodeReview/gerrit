3331c006f13763f92d65159444509cf4044af792
Paladox none <thomasmulhall410@yahoo.com>
2019-03-09 01:50:41 +0000

    Strip trailing slash at the end when creating repos
    Bug: Issue 7192
    Change-Id: I1106899b8afb52f1cf04e46b87b6658f4664af09

--- javatests/com/google/gerrit/acceptance/rest/project/CreateProjectIT.java
+++ javatests/com/google/gerrit/acceptance/rest/project/CreateProjectIT.java
@@ -177,6 +177,16 @@ public class CreateProjectIT extends AbstractDaemonTest {
     assertHead(newProjectName, "refs/heads/master");
   }
 
+  public void createProjectThatEndsWithSlash() throws Exception {
+    String newProjectName = name("newProject");
+    ProjectInfo p = gApi.projects().create(newProjectName + "/").get();
+    assertThat(p.name).isEqualTo(newProjectName);
+    ProjectState projectState = projectCache.get(new Project.NameKey(newProjectName));
+    assertThat(projectState).isNotNull();
+    assertProjectInfo(projectState.getProject(), p);
+    assertHead(newProjectName, "refs/heads/master");
+  }
+
   @Test
   public void createProjectWithProperties() throws Exception {
     String newProjectName = name("newProject");

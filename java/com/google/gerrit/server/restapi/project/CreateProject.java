dbdb94304e51c22bebca138059f56727df1e76cb
Paladox none <thomasmulhall410@yahoo.com>
2019-03-09 01:50:41 +0000

    Strip trailing slash at the end when creating repos
    Bug: Issue 7192
    Change-Id: I1106899b8afb52f1cf04e46b87b6658f4664af09

--- java/com/google/gerrit/server/restapi/project/CreateProject.java
+++ java/com/google/gerrit/server/restapi/project/CreateProject.java
@@ -168,7 +168,7 @@ public class CreateProject
     }
 
     CreateProjectArgs args = new CreateProjectArgs();
-    args.setProjectName(ProjectUtil.stripGitSuffix(name));
+    args.setProjectName(strip(name));
 
     String parentName =
         MoreObjects.firstNonNull(Strings.emptyToNull(input.parent), allProjects.get());
@@ -238,6 +238,12 @@ public class CreateProject
     } finally {
       nameLock.unlock();
     }
+  }
+
+  private static String strip(String name) {
+    projectName = ProjectUtil.stripGitSuffix(name);
+    projectName = ProjectUtil.stripEndSlash(projectName);
+    return projectName;
   }
 
   private ProjectState createProject(CreateProjectArgs args)

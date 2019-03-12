48e6ce00bf177160457af3528505acdaa8166bca
Dave Borowitz <dborowitz@google.com>
2019-03-12 16:58:26 -0700

    Extract a ListOption class, deduplicating fromBits method
    Remove the toBits method, which was unused in all cases. It was
    primarily for the GWT UI code.
    
    Change-Id: I755c4b4a752ba9fe2a6771f313c4a9361d4c071b

--- java/com/google/gerrit/extensions/client/ListAccountsOption.java
+++ java/com/google/gerrit/extensions/client/ListAccountsOption.java
@@ -14,11 +14,8 @@
 
 package com.google.gerrit.extensions.client;
 
-import java.util.EnumSet;
-import java.util.Set;
-
 /** Output options available for retrieval of account details. */
-public enum ListAccountsOption {
+public enum ListAccountsOption implements ListOption {
   /** Return detailed account properties. */
   DETAILS(0),
 
@@ -31,32 +28,8 @@ public enum ListAccountsOption {
     this.value = v;
   }
 
+  @Override
   public int getValue() {
     return value;
-  }
-
-  public static EnumSet<ListAccountsOption> fromBits(int v) {
-    EnumSet<ListAccountsOption> r = EnumSet.noneOf(ListAccountsOption.class);
-    for (ListAccountsOption o : ListAccountsOption.values()) {
-      if ((v & (1 << o.value)) != 0) {
-        r.add(o);
-        v &= ~(1 << o.value);
-      }
-      if (v == 0) {
-        return r;
-      }
-    }
-    if (v != 0) {
-      throw new IllegalArgumentException("unknown " + Integer.toHexString(v));
-    }
-    return r;
-  }
-
-  public static int toBits(Set<ListAccountsOption> set) {
-    int r = 0;
-    for (ListAccountsOption o : set) {
-      r |= 1 << o.value;
-    }
-    return r;
   }
 }

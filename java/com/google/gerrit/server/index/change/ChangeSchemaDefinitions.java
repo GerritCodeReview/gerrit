b004225a4b4d778731a5dabd9d3de70c44550996
David Ostrovsky <david@ostrovsky.org>
2019-03-29 09:22:45 +0100

    Bump Lucene version to 8.11.1
    Change-Id: Ib0d8b20c73b9b4752b36a31d938750144447f9b9

--- java/com/google/gerrit/server/index/change/ChangeSchemaDefinitions.java
+++ java/com/google/gerrit/server/index/change/ChangeSchemaDefinitions.java
@@ -101,7 +101,10 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
           ChangeField.WIP);
 
   // Upgrade Lucene to 7.x requires reindexing.
-  static final Schema<ChangeData> V75 = schema(V74);
+  @Deprecated static final Schema<ChangeData> V75 = schema(V74);
+
+  // Upgrade Lucene to 8.x requires reindexing.
+  static final Schema<ChangeData> V76 = schema(V75);
 
   /**
    * Name of the change index to be used when contacting index backends or loading configurations.

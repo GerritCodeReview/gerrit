b024db844db34971d36d01866de6952675e5dbce
Paladox none <thomasmulhall410@yahoo.com>
2019-04-12 18:07:58 +0000

    Test conversion of gr-app to PolymerElement
    Change-Id: I03b802a46dc0bbef2440ed7fdf981c9f5d1a6cde

--- polygerrit-ui/app/elements/gr-app.js
+++ polygerrit-ui/app/elements/gr-app.js
@@ -17,17 +17,186 @@
 (function() {
   'use strict';
 
-  Polymer({
-    is: 'gr-app',
-    _legacyUndefinedCheck: true,
+  class GrApp extends Polymer.Element {
+    static get template() {
+      return Polymer.html`
+        <style include="shared-styles">
+          :host {
+            background-color: var(--view-background-color);
+            display: flex;
+            flex-direction: column;
+            min-height: 100%;
+          }
+          gr-fixed-panel {
+            /**
+             * This one should be greater that the z-index in gr-diff-view
+             * because gr-main-header contains overlay.
+             */
+            z-index: 10;
+          }
+          gr-main-header,
+          footer {
+            color: var(--primary-text-color);
+          }
+          gr-main-header {
+            background-color: var(--header-background-color);
+            padding: 0 var(--default-horizontal-margin);
+            border-bottom: 1px solid var(--border-color);
+          }
+          gr-main-header.shadow {
+            /* Make it obvious for shadow dom testing */
+            border-bottom: 1px solid pink;
+          }
+          footer {
+            background-color: var(--footer-background-color);
+            border-top: 1px solid var(--border-color);
+            display: flex;
+            justify-content: space-between;
+            padding: .5rem var(--default-horizontal-margin);
+            z-index: 100;
+          }
+          main {
+            flex: 1;
+            padding-bottom: 2em;
+            position: relative;
+          }
+          .errorView {
+            align-items: center;
+            display: none;
+            flex-direction: column;
+            justify-content: center;
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+          }
+          .errorView.show {
+            display: flex;
+          }
+          .errorEmoji {
+            font-size: 2.6rem;
+          }
+          .errorText,
+          .errorMoreInfo {
+            margin-top: .75em;
+          }
+          .errorText {
+            font-size: 1.2rem;
+          }
+          .errorMoreInfo {
+            color: var(--deemphasized-text-color);
+          }
+          .feedback {
+            color: var(--error-text-color);
+          }
+        </style>
+        <gr-fixed-panel id="header">
+          <gr-main-header
+              id="mainHeader"
+              search-query="{{params.query}}"
+              class$="[[_computeShadowClass(_isShadowDom)]]">
+          </gr-main-header>
+        </gr-fixed-panel>
+        <main>
+          <template is="dom-if" if="[[_showChangeListView]]" restamp="true">
+            <gr-change-list-view
+                params="[[params]]"
+                account="[[_account]]"
+                view-state="{{_viewState.changeListView}}"></gr-change-list-view>
+          </template>
+          <template is="dom-if" if="[[_showDashboardView]]" restamp="true">
+            <gr-dashboard-view
+                account="[[_account]]"
+                params="[[params]]"
+                view-state="{{_viewState.dashboardView}}"></gr-dashboard-view>
+          </template>
+          <template is="dom-if" if="[[_showChangeView]]" restamp="true">
+            <gr-change-view
+                params="[[params]]"
+                view-state="{{_viewState.changeView}}"
+                back-page="[[_lastSearchPage]]"></gr-change-view>
+          </template>
+          <template is="dom-if" if="[[_showEditorView]]" restamp="true">
+            <gr-editor-view
+                params="[[params]]"></gr-editor-view>
+          </template>
+          <template is="dom-if" if="[[_showDiffView]]" restamp="true">
+              <gr-diff-view
+                  params="[[params]]"
+                  change-view-state="{{_viewState.changeView}}"></gr-diff-view>
+            </template>
+          <template is="dom-if" if="[[_showSettingsView]]" restamp="true">
+            <gr-settings-view
+                params="[[params]]"
+                on-account-detail-update="_handleAccountDetailUpdate">
+            </gr-settings-view>
+          </template>
+          <template is="dom-if" if="[[_showAdminView]]" restamp="true">
+            <gr-admin-view path="[[_path]]"
+                params=[[params]]></gr-admin-view>
+          </template>
+          <template is="dom-if" if="[[_showPluginScreen]]" restamp="true">
+            <gr-endpoint-decorator name="[[_pluginScreenName]]">
+              <gr-endpoint-param name="token" value="[[params.screen]]"></gr-endpoint-param>
+            </gr-endpoint-decorator>
+          </template>
+          <template is="dom-if" if="[[_showCLAView]]" restamp="true">
+            <gr-cla-view></gr-cla-view>
+          </template>
+          <template is="dom-if" if="[[_showDocumentationSearch]]" restamp="true">
+            <gr-documentation-search
+                params="[[params]]">
+            </gr-documentation-search>
+          </template>
+          <div id="errorView" class="errorView">
+            <div class="errorEmoji">[[_lastError.emoji]]</div>
+            <div class="errorText">[[_lastError.text]]</div>
+            <div class="errorMoreInfo">[[_lastError.moreInfo]]</div>
+          </div>
+        </main>
+        <footer r="contentinfo" class$="[[_computeShadowClass(_isShadowDom)]]">
+          <div>
+            Powered by <a href="https://www.gerritcodereview.com/" rel="noopener"
+            target="_blank">Gerrit Code Review</a>
+            ([[_version]])
+          </div>
+          <div>
+            <a class="feedback"
+                href$="[[_feedbackUrl]]"
+                rel="noopener" target="_blank">Send feedback</a>
+            | Press &ldquo;?&rdquo; for keyboard shortcuts
+          </div>
+        </footer>
+        <gr-overlay id="keyboardShortcuts" with-backdrop>
+          <gr-keyboard-shortcuts-dialog
+              view="[[params.view]]"
+              on-close="_handleKeyboardShortcutDialogClose"></gr-keyboard-shortcuts-dialog>
+        </gr-overlay>
+        <gr-overlay id="registrationOverlay" with-backdrop>
+          <gr-registration-dialog
+              id="registrationDialog"
+              settings-url="[[_settingsUrl]]"
+              on-account-detail-update="_handleAccountDetailUpdate"
+              on-close="_handleRegistrationDialogClose">
+          </gr-registration-dialog>
+        </gr-overlay>
+        <gr-endpoint-decorator name="plugin-overlay"></gr-endpoint-decorator>
+        <gr-error-manager id="errorManager"></gr-error-manager>
+        <gr-rest-api-interface id="restAPI"></gr-rest-api-interface>
+        <gr-reporting id="reporting"></gr-reporting>
+        <gr-router id="router"></gr-router>
+        <gr-plugin-host id="plugins"
+            config="[[_serverConfig]]">
+        </gr-plugin-host>
+        <gr-lib-loader id="libLoader"></gr-lib-loader>
+        <gr-external-style id="externalStyle" name="app-theme"></gr-external-style>
+      `;
+    }
 
-    /**
-     * Fired when the URL location changes.
-     *
-     * @event location-change
-     */
+    static get is() { return 'gr-app'; }
 
-    properties: {
+    static get properties() { return {
       /**
        * @type {{ query: string, view: string, screen: string }}
        */
@@ -84,7 +253,16 @@
         value: 'https://bugs.chromium.org/p/gerrit/issues/entry' +
           '?template=PolyGerrit%20Issue',
       },
-    },
+    }}
+
+    static get observers() { return [
+      '_viewChanged(params.view)',
+      '_paramsChanged(params.*)',
+    ]}
+
+    constructor() {
+      super();
+    }
 
     listeners: {
       'page-error': '_handlePageError',
@@ -92,11 +270,6 @@
       'location-change': '_handleLocationChange',
       'rpc-log': '_handleRpcLog',
     },
-
-    observers: [
-      '_viewChanged(params.view)',
-      '_paramsChanged(params.*)',
-    ],
 
     behaviors: [
       Gerrit.BaseUrlBehavior,
@@ -113,10 +286,14 @@
     },
 
     created() {
+      super.created();
+
       this._bindKeyboardShortcuts();
     },
 
     ready() {
+      super.ready();
+
       this._isShadowDom = Polymer.Settings.useShadow;
       this.$.router.start();
 
@@ -441,5 +618,7 @@
       this.$.reporting.reportRpcTiming(e.detail.anonymizedUrl,
           e.detail.elapsed);
     },
-  });
+  }
+
+  customElements.define(GrApp.is, GrApp);
 })();

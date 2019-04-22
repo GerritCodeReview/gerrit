8671b480d4d5a431133dddc60ec0322a919c3db2
Paladox none <thomasmulhall410@yahoo.com>
2019-04-22 13:47:29 +0000

    Send a email when removing a ssh key
    Change-Id: I84f4bc5df5b6a609c1a17bad7c4a327cd780aa10

--- java/com/google/gerrit/server/restapi/account/DeleteSshKey.java
+++ java/com/google/gerrit/server/restapi/account/DeleteSshKey.java
@@ -14,6 +14,8 @@
 
 package com.google.gerrit.server.restapi.account;
 
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.exceptions.EmailException;
 import com.google.gerrit.extensions.common.Input;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.Response;
@@ -21,6 +23,7 @@ import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.account.AccountResource;
 import com.google.gerrit.server.account.VersionedAuthorizedKeys;
+import com.google.gerrit.server.mail.send.DeleteKeySender;
 import com.google.gerrit.server.permissions.GlobalPermission;
 import com.google.gerrit.server.permissions.PermissionBackend;
 import com.google.gerrit.server.permissions.PermissionBackendException;
@@ -34,22 +37,26 @@ import org.eclipse.jgit.errors.RepositoryNotFoundException;
 
 @Singleton
 public class DeleteSshKey implements RestModifyView<AccountResource.SshKey, Input> {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   private final Provider<CurrentUser> self;
   private final PermissionBackend permissionBackend;
   private final VersionedAuthorizedKeys.Accessor authorizedKeys;
   private final SshKeyCache sshKeyCache;
+  private final DeleteKeySender.Factory deleteKeyFactory;
 
   @Inject
   DeleteSshKey(
       Provider<CurrentUser> self,
       PermissionBackend permissionBackend,
       VersionedAuthorizedKeys.Accessor authorizedKeys,
-      SshKeyCache sshKeyCache) {
+      SshKeyCache sshKeyCache,
+      DeleteKeySender.Factory deleteKeyFactory) {
     this.self = self;
     this.permissionBackend = permissionBackend;
     this.authorizedKeys = authorizedKeys;
     this.sshKeyCache = sshKeyCache;
+    this.deleteKeyFactory = deleteKeyFactory;
   }
 
   @Override
@@ -61,6 +68,15 @@ public class DeleteSshKey implements RestModifyView<AccountResource.SshKey, Inpu
     }
 
     authorizedKeys.deleteKey(rsrc.getUser().getAccountId(), rsrc.getSshKey().seq());
+
+    try {
+      deleteKeyFactory.create(rsrc.getUser(), "SSH").send();
+    } catch (EmailException e) {
+      logger.atSevere().withCause(e).log(
+          "Cannot send SSH key deletion message to %s",
+          rsrc.getUser().getAccount().getPreferredEmail());
+    }
+
     rsrc.getUser().getUserName().ifPresent(sshKeyCache::evict);
 
     return Response.none();

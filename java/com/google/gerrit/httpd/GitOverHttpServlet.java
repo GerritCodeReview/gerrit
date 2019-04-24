fc18411bf62c98018b78e0c35b70493a336f76f6
David Pursehouse <dpursehouse@collab.net>
2019-04-24 19:51:44 +0900

    Revert commits that enable git protovol v2
    It has been decided not to include protocol v2 in 3.0.0, but instead
    re-land it on master with more testing.
    
    This reverts commit b345f0a20891576546237c0b33dd6e1787079f5d.
    This reverts commit fb64381f7c7c1ad610dc58aa450cfc1f74b6576f.
    
    Change-Id: I42c0d8f96411f7b314a62ec9934910b3ecabe75d

--- java/com/google/gerrit/httpd/GitOverHttpServlet.java
+++ java/com/google/gerrit/httpd/GitOverHttpServlet.java
@@ -51,7 +51,6 @@ import com.google.inject.TypeLiteral;
 import com.google.inject.name.Named;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
@@ -328,11 +327,6 @@ public class GitOverHttpServlet extends GitServlet {
       up.setTimeout(config.getTimeout());
       up.setPreUploadHook(PreUploadHookChain.newChain(Lists.newArrayList(preUploadHooks)));
       up.setPostUploadHook(PostUploadHookChain.newChain(Lists.newArrayList(postUploadHooks)));
-      String header = req.getHeader("Git-Protocol");
-      if (header != null) {
-        String[] params = header.split(":");
-        up.setExtraParameters(Arrays.asList(params));
-      }
       ProjectState state = (ProjectState) req.getAttribute(ATT_STATE);
       uploadPackInitializers.runEach(initializer -> initializer.init(state.getNameKey(), up));
       return up;

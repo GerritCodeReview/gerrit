d8e6e7e3c7c235fde34187c373365e245cad5436
Luca Milanesio <luca.milanesio@gmail.com>
2019-04-25 23:05:49 +0100

    Demonstrate the breakage of AutoValue GSON serialization
    The introduction of auto-values for the basic keys of the Gerrit
    main objects (change, account, group, project name) has introduced
    a regression in the GSON deserialization.
    
    This has not been detected by our tests, because their coverage was
    very limited to a single event.
    
    Change-Id: I242b51530a388f4abf25e9cb86896dd4821f600f

--- javatests/com/google/gerrit/server/events/EventDeserializerTest.java
+++ javatests/com/google/gerrit/server/events/EventDeserializerTest.java
@@ -16,6 +16,8 @@ package com.google.gerrit.server.events;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.sql.Timestamp;
+
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.gerrit.server.data.AccountAttribute;
@@ -25,8 +27,23 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.junit.Test;
 
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Branch;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.Project;
+
+
 public class EventDeserializerTest extends GerritBaseTests {
 
+  Gson gsonSerializer =
+      new GsonBuilder().registerTypeAdapter(Supplier.class, new SupplierSerializer()).create();
+
+  Gson gsonDeserializer =
+      new GsonBuilder()
+          .registerTypeAdapter(Event.class, new EventDeserializer())
+          .registerTypeAdapter(Supplier.class, new SupplierDeserializer())
+          .create();
+  
   @Test
   public void refUpdatedEvent() {
     RefUpdatedEvent refUpdatedEvent = new RefUpdatedEvent();
@@ -39,16 +56,7 @@ public class EventDeserializerTest extends GerritBaseTests {
     accountAttribute.email = "some.user@domain.com";
     refUpdatedEvent.submitter = createSupplier(accountAttribute);
 
-    Gson gsonSerializer =
-        new GsonBuilder().registerTypeAdapter(Supplier.class, new SupplierSerializer()).create();
     String serializedEvent = gsonSerializer.toJson(refUpdatedEvent);
-
-    Gson gsonDeserializer =
-        new GsonBuilder()
-            .registerTypeAdapter(Event.class, new EventDeserializer())
-            .registerTypeAdapter(Supplier.class, new SupplierDeserializer())
-            .create();
-
     RefUpdatedEvent e = (RefUpdatedEvent) gsonDeserializer.fromJson(serializedEvent, Event.class);
 
     assertThat(e).isNotNull();
@@ -56,6 +64,24 @@ public class EventDeserializerTest extends GerritBaseTests {
     assertThat(e.refUpdate.get().refName).isEqualTo(refUpdatedAttribute.refName);
     assertThat(e.submitter).isInstanceOf(Supplier.class);
     assertThat(e.submitter.get().email).isEqualTo(accountAttribute.email);
+  }
+  
+  @Test
+  public void patchSetCreatedEvent() {
+    Change change = new Change(
+        Change.key("Iabcdef"),
+        Change.id(1000),
+        Account.id(1000),
+        Branch.nameKey(Project.nameKey("myproject"), "mybranch"),
+        new Timestamp(System.currentTimeMillis()));
+    PatchSetCreatedEvent orig = new PatchSetCreatedEvent(change);
+    
+    String serialized = gsonSerializer.toJson(orig);
+    PatchSetCreatedEvent e = (PatchSetCreatedEvent) gsonDeserializer.fromJson(serialized, Event.class);
+    
+    assertThat(e).isNotNull();
+    assertThat(e.change).isInstanceOf(Supplier.class);
+    assertThat(e.change.get()).isEqualTo(change);
   }
 
   private <T> Supplier<T> createSupplier(T value) {

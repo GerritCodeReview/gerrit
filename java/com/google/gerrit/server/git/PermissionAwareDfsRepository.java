3c5de1d03dc63d4c147a2e3cba31574362ae5afa
Stefano Galarraga <galarragas@gmail.com>
2019-04-26 11:20:21 +0100

    Fixes failing tests in change 212874
    This change fixes the three failings tests in change 212874.
    
    Tests are:
    
    - HttpPushForReviewIT.nonSignedPushRejectedWhenSignPushRequired
    - SshPushForReviewIT.nonSignedPushRejectedWhenSignPushRequired
    - GetCommitIT.getOpenChange_NotFound
    
    To fix the first two, I Had to introduce two different implementation of the
    PermissionAwareRepository to pass JGit instanceof checks in .
    
    The PermissionAware-Repositories are full proxies and they override the full
    public interface of Repository and forward to the inner delegate.
    
    The third test was failing because the initialization of the test was failing
    when pushing the change to the server. This was caused by the fact that the
    user has no read access to the refs so during the push operation, the
    getAdvertisedRefs call is returning an empty set and the client is therefore
    pushing both the initial commit done when creating the repo  and the one associated
    to the change. The initial commit has no changeId and causes the test to fail
    with a misleading Validation Error.
    
    Change-Id: I2ebd6b9d967a86995bf294500ce745fc7f418b21

--- java/com/google/gerrit/server/git/PermissionAwareDfsRepository.java
+++ java/com/google/gerrit/server/git/PermissionAwareDfsRepository.java
@@ -30,9 +30,11 @@ import org.eclipse.jgit.errors.NoWorkTreeException;
 import org.eclipse.jgit.errors.RevisionSyntaxException;
 import org.eclipse.jgit.events.ListenerList;
 import org.eclipse.jgit.events.RepositoryEvent;
+import org.eclipse.jgit.internal.storage.dfs.DfsObjDatabase;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepository;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryBuilder;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
 import org.eclipse.jgit.lib.AnyObjectId;
-import org.eclipse.jgit.lib.BaseRepositoryBuilder;
-import org.eclipse.jgit.lib.ObjectDatabase;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
 import org.eclipse.jgit.lib.ObjectLoader;
@@ -51,17 +53,22 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.util.FS;
 
 /**
- * Wrapper around {@link Repository} that delegates all calls to the wrapped {@link Repository}
- * except for {@link #getRefDatabase()} where it provides a {@link
+ * Wrapper around {@link DfsRepository} that delegates all calls to the wrapped {@link
+ * DfsRepository} except for {@link #getRefDatabase()} where it provides a {@link
  * PermissionAwareReadOnlyRefDatabase}.
+ *
+ * <p>This is an almost exact copy of {@link PermissionAwareRepository} that should disappear once
+ * the JGit {@link Repository} class would be converted into an interface and the code using it
+ * would not explicitly check the instance class to discriminate from Dfs or non Dfs repos
  */
-public class PermissionAwareRepository extends Repository
+public class PermissionAwareDfsRepository extends DfsRepository
     implements PermissionAwareRepositoryWrapper {
 
-  private final Repository delegate;
+  private final DfsRepository delegate;
   private final PermissionAwareReadOnlyRefDatabase permissionAwareReadOnlyRefDatabase;
 
-  public PermissionAwareRepository(Repository delegate, PermissionBackend.ForProject forProject) {
+  public PermissionAwareDfsRepository(
+      DfsRepository delegate, PermissionBackend.ForProject forProject) {
     super(toBuilder(delegate));
     this.delegate = delegate;
     this.permissionAwareReadOnlyRefDatabase =
@@ -73,18 +80,27 @@ public class PermissionAwareRepository extends Repository
     return delegate;
   }
 
-  private static BaseRepositoryBuilder toBuilder(Repository repo) {
-    BaseRepositoryBuilder b = new BaseRepositoryBuilder<>();
+  private static DfsRepositoryBuilder<DfsRepositoryBuilder, DfsRepository> toBuilder(
+      DfsRepository repo) {
+    DfsRepositoryBuilder<DfsRepositoryBuilder, DfsRepository> b =
+        new DfsRepositoryBuilder<DfsRepositoryBuilder, DfsRepository>() {
+          @Override
+          public DfsRepository build() throws IOException {
+            return null;
+          }
+        };
     b.setFS(repo.getFS());
     b.setGitDir(repo.getDirectory());
     if (!repo.isBare()) {
       b.setIndexFile(repo.getIndexFile());
       b.setWorkTree(repo.getWorkTree());
     }
+
     return b;
   }
 
-  ////// Overriding all public methods, not just abstract ones, in order to act as a pure proxy
+  ////// Overriding all Repository public methods, not just abstract ones, in order to act as a pure
+  // proxy
 
   @Override
   public ListenerList getListenerList() {
@@ -112,6 +128,11 @@ public class PermissionAwareRepository extends Repository
   }
 
   @Override
+  public DfsObjDatabase getObjectDatabase() {
+    return delegate.getObjectDatabase();
+  }
+
+  @Override
   public ObjectInserter newObjectInserter() {
     return delegate.newObjectInserter();
   }
@@ -119,11 +140,6 @@ public class PermissionAwareRepository extends Repository
   @Override
   public ObjectReader newObjectReader() {
     return delegate.newObjectReader();
-  }
-
-  @Override
-  public ObjectDatabase getObjectDatabase() {
-    return delegate.getObjectDatabase();
   }
 
   @Override
@@ -395,5 +411,15 @@ public class PermissionAwareRepository extends Repository
   @Override
   public void autoGC(ProgressMonitor monitor) {
     delegate.autoGC(monitor);
+  }
+
+  @Override
+  public DfsRepositoryDescription getDescription() {
+    return delegate.getDescription();
+  }
+
+  @Override
+  public boolean exists() throws IOException {
+    return delegate.exists();
   }
 }

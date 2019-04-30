8ee0aa63f16f4da66ff0d8585f46c5ae9029b404
Stefano Galarraga <galarragas@gmail.com>
2019-04-30 16:20:21 +0100

    Remove unwrap operation in PermissionAwareRepositoryManager
    Allow to use the PermissionAwareRepository as a proxy for the delegate repo in all cases
    without having to access the delegate.
    This requires to make the PermissionAwareReadOnlyRefDatabase capable of supporting writes
    
    Change-Id: I26b28530fff8571ef8823f1edd1e985f53619f29

--- javatests/com/google/gerrit/git/PermissionAwareRepositoryTest.java
+++ javatests/com/google/gerrit/git/PermissionAwareRepositoryTest.java
@@ -20,44 +20,43 @@ import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.reset;
 import static org.easymock.EasyMock.verify;
 
-import com.google.common.base.Defaults;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.server.git.PermissionAwareDfsRepository;
-import com.google.gerrit.server.git.PermissionAwareReadOnlyRefDatabase;
+import com.google.gerrit.server.git.PermissionAwareRefDatabase;
 import com.google.gerrit.server.git.PermissionAwareRepository;
 import com.google.gerrit.server.permissions.PermissionBackend;
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import org.easymock.EasyMockSupport;
-import org.easymock.IExpectationSetters;
-import org.easymock.internal.AssertionErrorWrapper;
 import org.eclipse.jgit.internal.storage.dfs.DfsRepository;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.util.FS;
 import org.junit.Before;
 import org.junit.Test;
 
-public class PermissionAwareRepositoryTest extends EasyMockSupport {
+public class PermissionAwareRepositoryTest extends PermissionAwareDelegateTestBase<Repository> {
   Repository repository;
   PermissionBackend.ForProject forProject;
 
-  private final Set<String> methodsWithSpecificTest =
-      ImmutableSet.of(
-          "exactRef", // final method, problems using EasyMock to test it
-          "findRef", // final method, problems using EasyMock to test it
-          "getRefDatabase", // returns a different object, has a special test
-          "toString" // ignored
-          );
+  @Override
+  protected Set<String> methodsWithSpecificTest() {
+    return ImmutableSet.of(
+        "getRefDatabase", // returns a different object, has a special test
+        "toString" // ignored
+        );
+  }
+
+  @Override
+  protected Object[] additionalMockObjectsForDelegateCheck() {
+    return new Object[] {forProject};
+  }
+
+  @Override
+  protected void setExpectationsForAdditionalMockObjects() {}
+
+  @Override
+  protected Repository delegate() {
+    return repository;
+  }
 
   @Before
   public void setUp() {
@@ -69,7 +68,7 @@ public class PermissionAwareRepositoryTest extends EasyMockSupport {
     PermissionAwareRepository permissionAwareRepository = buildPermissionAwareRepo();
     replay(repository, forProject);
     assertThat(permissionAwareRepository.getRefDatabase())
-        .isInstanceOf(PermissionAwareReadOnlyRefDatabase.class);
+        .isInstanceOf(PermissionAwareRefDatabase.class);
     verify(repository, forProject);
   }
 
@@ -78,7 +77,7 @@ public class PermissionAwareRepositoryTest extends EasyMockSupport {
     PermissionAwareRepository permissionAwareRepository = buildPermissionAwareRepo();
     replay(repository, forProject);
     assertThat(permissionAwareRepository.getRefDatabase())
-        .isInstanceOf(PermissionAwareReadOnlyRefDatabase.class);
+        .isInstanceOf(PermissionAwareRefDatabase.class);
     verify(repository, forProject);
   }
 
@@ -123,124 +122,13 @@ public class PermissionAwareRepositoryTest extends EasyMockSupport {
 
   @Test
   public void permissionAwareRepositoryShouldDelegateAllPublicMethods() {
+    repository = createMock(DfsRepository.class);
     shouldDelegateAllPublicMethods(this::buildPermissionAwareRepo);
   }
 
   @Test
   public void permissionAwareDfsRepositoryShouldDelegateAllPublicMethods() {
+    repository = createMock(DfsRepository.class);
     shouldDelegateAllPublicMethods(this::buildPermissionAwareDfsRepo);
-  }
-
-  protected void shouldDelegateAllPublicMethods(Supplier<Repository> testObjectBuilder) {
-    final List<String> notDelegatedMethods =
-        Arrays.stream(Repository.class.getMethods())
-            .filter(
-                method ->
-                    !(method.getDeclaringClass() != Repository.class
-                        || Modifier.isStatic(method.getModifiers())
-                        || hasSpecialTest(method)))
-            .filter(method -> !isDelegated(testObjectBuilder, method))
-            .map(Method::getName)
-            .collect(Collectors.toList());
-
-    assertThat(notDelegatedMethods).isEqualTo(ImmutableList.of());
-  }
-
-  private boolean isDelegated(Supplier<Repository> testObjectBuilder, Method repositoryMethod) {
-    try {
-      Repository toTest = testObjectBuilder.get();
-
-      reset(repository, forProject);
-
-      List<Object> parametersValue =
-          Arrays.stream(repositoryMethod.getParameters())
-              .map(parameter -> defaultValueFor(parameter.getType()))
-              .collect(Collectors.toList());
-
-      Object expected = defaultValueFor(repositoryMethod.getReturnType());
-
-      IExpectationSetters<Object> expectDelegateCalled =
-          expect(repositoryMethod.invoke(repository, parametersValue.toArray()));
-      if (repositoryMethod.getReturnType() != Void.TYPE) {
-        expectDelegateCalled.andReturn(expected);
-      }
-
-      replay(repository, forProject);
-
-      if (repositoryMethod.getReturnType() != Void.TYPE) {
-        assertThat(repositoryMethod.invoke(toTest, parametersValue.toArray())).isEqualTo(expected);
-      } else {
-        repositoryMethod.invoke(toTest, parametersValue.toArray());
-      }
-
-      verify(repository, forProject);
-
-      return true;
-    } catch (AssertionError | IllegalStateException | AssertionErrorWrapper methodNotDelegated) {
-      return false;
-    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-      throw new RuntimeException(
-          "Cannot invoke method " + repositoryMethod.getName() + " via reflection", e);
-    }
-  }
-
-  private boolean hasSpecialTest(Method repositoryMethod) {
-    return methodsWithSpecificTest.contains(repositoryMethod.getName());
-  }
-
-  private Object defaultValueFor(Class<?> type) {
-    if (type == String.class) return "";
-
-    if (type == File.class) return new File("");
-
-    return Defaults.defaultValue(type);
-  }
-
-  public static class SimpleRef implements Ref {
-    private final String name;
-
-    SimpleRef(String name) {
-      this.name = name;
-    }
-
-    @Override
-    public String getName() {
-      return name;
-    }
-
-    @Override
-    public boolean isSymbolic() {
-      return false;
-    }
-
-    @Override
-    public Ref getLeaf() {
-      return null;
-    }
-
-    @Override
-    public Ref getTarget() {
-      return null;
-    }
-
-    @Override
-    public ObjectId getObjectId() {
-      return ObjectId.zeroId();
-    }
-
-    @Override
-    public ObjectId getPeeledObjectId() {
-      return ObjectId.zeroId();
-    }
-
-    @Override
-    public boolean isPeeled() {
-      return false;
-    }
-
-    @Override
-    public Storage getStorage() {
-      return Storage.NETWORK;
-    }
   }
 }

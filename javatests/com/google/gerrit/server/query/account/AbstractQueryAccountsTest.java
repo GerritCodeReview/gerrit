7af1124b194f3d7c50de79df31fe9e2ed4cf6eed
David Ostrovsky <david@ostrovsky.org>
2019-05-17 20:25:57 +0200

    Remove account index schema versions <10
    stable-3.0 contains v10, so this is the minimum version 3.1 needs to
    support for an online schema upgrade during 3.0->3.1.
    
    Change-Id: I0f38d030a1ed0c8236464df4163f46bebab2cddb

--- javatests/com/google/gerrit/server/query/account/AbstractQueryAccountsTest.java
+++ javatests/com/google/gerrit/server/query/account/AbstractQueryAccountsTest.java
@@ -261,19 +261,7 @@ public abstract class AbstractQueryAccountsTest extends GerritServerTests {
     AccountInfo user2 = newAccount("user");
     requestContext.setContext(newRequestContext(Account.id(user2._accountId)));
 
-    if (getSchemaVersion() < 5) {
-      assertMissingField(AccountField.PREFERRED_EMAIL);
-      assertFailingQuery("email:foo", "'email' operator is not supported by account index version");
-      return;
-    }
-
-    // This at least needs the PREFERRED_EMAIL field which is available from schema version 5.
-    if (getSchemaVersion() >= 5) {
-      assertQuery(preferredEmail, user1);
-    } else {
-      assertQuery(preferredEmail);
-    }
-
+    assertQuery(preferredEmail, user1);
     assertQuery(secondaryEmail);
 
     assertQuery("email:" + preferredEmail, user1);
@@ -644,11 +632,6 @@ public abstract class AbstractQueryAccountsTest extends GerritServerTests {
 
     assertThat(rawFields).isPresent();
     assertThat(rawFields.get().getValue(AccountField.ID)).isEqualTo(userInfo._accountId);
-
-    // The field EXTERNAL_ID_STATE is only supported from schema version 6.
-    if (getSchemaVersion() < 6) {
-      return;
-    }
 
     List<AccountExternalIdInfo> externalIdInfos = gApi.accounts().self().getExternalIds();
     List<ByteArrayWrapper> blobs = new ArrayList<>();

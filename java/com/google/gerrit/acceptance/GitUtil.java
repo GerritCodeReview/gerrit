60f63a372c5e4d7754627767d974b01d0e8c29b5
David Ostrovsky <david@ostrovsky.org>
2019-05-21 22:59:10 +0200

    GitUtil: Only instantiate file system object if really needed
    This is a leftover of refactoring in: I99f78abd42.
    
    Change-Id: Iaad4de5df6269de7258629712b4ee9d3951455f0

--- java/com/google/gerrit/acceptance/GitUtil.java
+++ java/com/google/gerrit/acceptance/GitUtil.java
@@ -110,13 +110,11 @@ public class GitUtil {
       throws Exception {
     DfsRepositoryDescription desc = new DfsRepositoryDescription("clone of " + project.get());
 
-    FS fs = FS.detect();
-
-    // Avoid leaking user state into our tests.
-    fs.setUserHome(null);
-
     InMemoryRepository.Builder b = new InMemoryRepository.Builder().setRepositoryDescription(desc);
     if (uri.startsWith("ssh://")) {
+      FS fs = FS.detect();
+      // Avoid leaking user state into our tests.
+      fs.setUserHome(null);
       // SshTransport depends on a real FS to read ~/.ssh/config, but InMemoryRepository by default
       // uses a null FS.
       b.setFS(fs);

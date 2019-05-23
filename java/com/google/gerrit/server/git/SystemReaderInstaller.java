e63cf2ba0c2f236df73c9f5129689f3b135d9449
Dave Borowitz <dborowitz@google.com>
2019-05-23 14:24:21 -0700

    SystemReaderInstaller: Return a new instance from getSystemConfig
    This is the only way to respect the passed-in parent config, which may
    be different on each invocation. Additionally, returning a new instance
    matches the behavior of the default system reader, which downstream
    callers may be depending on.
    
    Change-Id: If95ccf4e97d4b969cdba7a6e85e613e9acc3ff0d

--- java/com/google/gerrit/server/git/SystemReaderInstaller.java
+++ java/com/google/gerrit/server/git/SystemReaderInstaller.java
@@ -47,8 +47,6 @@ public class SystemReaderInstaller implements LifecycleListener {
   private SystemReader customReader() {
     SystemReader current = SystemReader.getInstance();
 
-    FileBasedConfig jgitConfig = new FileBasedConfig(site.jgit_config.toFile(), FS.DETECTED);
-
     return new SystemReader() {
       @Override
       public String getHostname() {
@@ -72,7 +70,7 @@ public class SystemReaderInstaller implements LifecycleListener {
 
       @Override
       public FileBasedConfig openSystemConfig(Config parent, FS fs) {
-        return jgitConfig;
+        return new FileBasedConfig(parent, site.jgit_config.toFile(), FS.DETECTED);
       }
 
       @Override

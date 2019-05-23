1393972509a8b18c93645f526372275b3173f123
Marco Miller <marco.miller@ericsson.com>
2019-05-23 16:10:29 -0400

    IndexModule: Use lowercase IndexType values consistently
    Before this change, the init would fail as reported in the merged
    Ia129d3eb's trailing comments.
    
    Change-Id: I55af38995166c2b9f4dbb9fbc6196df083411a89

--- java/com/google/gerrit/server/index/IndexModule.java
+++ java/com/google/gerrit/server/index/IndexModule.java
@@ -74,13 +74,13 @@ import org.eclipse.jgit.lib.Config;
  */
 public class IndexModule extends LifecycleModule {
   public static class IndexType {
-    private static final String LUCENE = "LUCENE";
-    private static final String ELASTICSEARCH = "ELASTICSEARCH";
+    private static final String LUCENE = "lucene";
+    private static final String ELASTICSEARCH = "elasticsearch";
 
     private final String type;
 
     public IndexType(@Nullable String type) {
-      this.type = type == null ? getDefault() : type;
+      this.type = type == null ? getDefault() : type.toLowerCase();
     }
 
     public static String getDefault() {
@@ -92,11 +92,11 @@ public class IndexModule extends LifecycleModule {
     }
 
     public boolean isLucene() {
-      return type.equalsIgnoreCase(LUCENE);
+      return type.equals(LUCENE);
     }
 
     public boolean isElasticsearch() {
-      return type.equalsIgnoreCase(ELASTICSEARCH);
+      return type.equals(ELASTICSEARCH);
     }
 
     @Override

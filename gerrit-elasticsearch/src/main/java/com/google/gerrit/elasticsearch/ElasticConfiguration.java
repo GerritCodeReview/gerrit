f2a0b5b58748694658d59ba18e443f14ae79f4be
Marco Miller <marco.miller@ericsson.com>
2019-06-17 15:00:35 -0400

    Elasticsearch: Base default number of shards on ES version
    Before Elasticsearch 7.0, the default number of shards was 5. Since
    version 6, there is a warning about that default number changing to 1
    starting from 7.0.0. The related Issue 9768 first reported about it.
    
    Set the default number of shards to 1 starting with 7.0, to remove that
    index creation warning with V7, thus fit with what Elasticsearch 7.0+
    expects. Stick to the previous default for versions earlier than 7, to
    not potentially jeopardize existing deployments based on ES 6 or 5.
    
    Making this value non-default requires a corresponding configuration
    change on the Elasticsearch server side. And changing the number of
    shards for the latter is a non-trivial procedure. [1] introduces that
    and is already part of the Gerrit documentation.
    
    [1] https://www.elastic.co/guide/en/elasticsearch/reference/current/getting-started-concepts.html#getting-started-shards-and-replicas
    
    Bug: Issue 10499
    Change-Id: I9e91c471b48c4c4c893927c153a5661dfccc2bcb

--- gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticConfiguration.java
+++ gerrit-elasticsearch/src/main/java/com/google/gerrit/elasticsearch/ElasticConfiguration.java
@@ -43,7 +43,7 @@ class ElasticConfiguration {
   static final String KEY_NUMBER_OF_REPLICAS = "numberOfReplicas";
   static final String DEFAULT_PORT = "9200";
   static final String DEFAULT_USERNAME = "elastic";
-  static final int DEFAULT_NUMBER_OF_SHARDS = 5;
+  static final int DEFAULT_NUMBER_OF_SHARDS = 0;
   static final int DEFAULT_NUMBER_OF_REPLICAS = 1;
 
   private final Config cfg;
@@ -100,5 +100,12 @@ class ElasticConfiguration {
 
   String getIndexName(String name, int schemaVersion) {
     return String.format("%s%s_%04d", prefix, name, schemaVersion);
+  }
+
+  int getNumberOfShards(ElasticQueryAdapter adapter) {
+    if (numberOfShards == DEFAULT_NUMBER_OF_SHARDS) {
+      return adapter.getDefaultNumberOfShards();
+    }
+    return numberOfShards;
   }
 }

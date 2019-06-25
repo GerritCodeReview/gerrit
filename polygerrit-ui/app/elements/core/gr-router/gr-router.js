970b0422eff46f306f8456b964963ced592c1afa
Milutin Kristofic <milutin@google.com>
2019-06-25 13:42:56 +0200

    On initial load - FE decide if to show 404 otherwise it reloads
    Frontend decides on initial load only whether to render a 404
    
    After initial load, if the frontend router cannot match a URL,
    then it will let the URL pass through and be handled by the server.
    
    This obsoletes the need for marking URLs on the same host as
    internal or external. They will always be handled appropriately,
    e.g. customized links can point to pages within the app without
    causing a full page reload.
    
    Change-Id: I20adbf2ed18010e201a2075cb1c5a1ca9ac6dfdc

--- polygerrit-ui/app/elements/core/gr-router/gr-router.js
+++ polygerrit-ui/app/elements/core/gr-router/gr-router.js
@@ -212,6 +212,12 @@
         value: app,
       },
       _isRedirecting: Boolean,
+      // This variable is to different between internal navigation (false)
+      // and for first navigation in app after loaded from server (true).
+      _isInitialLoad: {
+        type: Boolean,
+        value: true,
+      },
     },
 
     behaviors: [
@@ -702,6 +708,7 @@
           this.$.reporting.beforeLocationChanged();
         }
         this._isRedirecting = false;
+        this._isInitialLoad = false;
         next();
       });
 
@@ -1471,7 +1478,11 @@
      * Catchall route for when no other route is matched.
      */
     _handleDefaultRoute() {
-      this._show404();
+      if (this._isInitialLoad) {
+        this._show404();
+      } else {
+        this._handlePassThroughRoute();
+      }
     },
 
     _show404() {

GerritCodeReview/gerrit

commit 6aaa18dd941fe7756ff716b11d60ebe714c1d1ed

Do not hide key locations
Probably a regression from properly supporting whitespace only changes.

Change-Id: I6cdeef3bb5c617c673222402643c3e2f98c101a3

polygerrit-ui/app/elements/diff/gr-diff-processor/gr-diff-processor.js
@@ -146,7 +146,7 @@
             };
 
             content = this._splitLargeChunks(content);
-            content = this._splitUnchangedChunksWithComments(content);
+            content = this._splitUnchangedChunksWithKeyLocations(content);
 
             let currentBatch = 0;
             const nextStep = () => {
@@ -200,15 +200,15 @@
     },
 
     /**
-     * Process the next uncommon section, or the next common sections.
+     * Process the next uncollapsible section, or the next collapsible sections.
      *
      * @param {!Object} state
      * @param {!Array<!Object>} sections
      */
     _processNext(state, sections) {
-      const firstUncommonSectionIndex = this._firstUncommonSectionIndex(
-          sections, state.sectionIndex);
-      if (firstUncommonSectionIndex === state.sectionIndex) {
+      const firstUncollapsibleSectionIndex =
+          this._firstUncollapsibleSectionIndex(sections, state.sectionIndex);
+      if (firstUncollapsibleSectionIndex === state.sectionIndex) {
         const section = sections[state.sectionIndex];
         return {
           lineDelta: {
@@ -221,25 +221,25 @@
         };
       }
 
-      return this._processCommonSections(
-          state, sections, firstUncommonSectionIndex);
+      return this._processCollapsibleSections(
+          state, sections, firstUncollapsibleSectionIndex);
     },
 
-    _firstUncommonSectionIndex(sections, offset) {
+    _firstUncollapsibleSectionIndex(sections, offset) {
       let sectionIndex = offset;
       while (sectionIndex < sections.length &&
-          this._isCommonSection(sections[sectionIndex])) {
+          this._isCollapsibleSection(sections[sectionIndex])) {
         sectionIndex++;
       }
       return sectionIndex;
     },
 
-    _isCommonSection(section) {
-      return section.ab || section.common;
+    _isCollapsibleSection(section) {
+      return (section.ab || section.common) && !section.keyLocation;
     },
 
     /**
-     * Process a stretch of common sections.
+     * Process a stretch of collapsible sections.
      *
      * Outputs up to three groups:
      *  1) Visible context before the hidden common code, unless it's the
@@ -250,22 +250,25 @@
      *
      * @param {!Object} state
      * @param {!Array<Object>} sections
-     * @param {number} firstUncommonSectionIndex
+     * @param {number} firstUncollapsibleSectionIndex
      * @return {!Object}
      */
-    _processCommonSections(state, sections, firstUncommonSectionIndex) {
-      const commonSections = sections.slice(
-          state.sectionIndex, firstUncommonSectionIndex);
-      const lineCount = commonSections.reduce(
+    _processCollapsibleSections(
+        state, sections, firstUncollapsibleSectionIndex) {
+      const collapsibleSections = sections.slice(
+          state.sectionIndex, firstUncollapsibleSectionIndex);
+      const lineCount = collapsibleSections.reduce(
           (sum, section) => sum + this._commonSectionLength(section), 0);
 
       let groups = this._sectionsToGroups(
-          commonSections, state.lineNums.left + 1, state.lineNums.right + 1);
+          collapsibleSections,
+          state.lineNums.left + 1,
+          state.lineNums.right + 1);
 
       if (this.context !== WHOLE_FILE) {
         const hiddenStart = state.sectionIndex === 0 ? 0 : this.context;
         const hiddenEnd = lineCount - (
-            firstUncommonSectionIndex === sections.length ?
+            firstUncollapsibleSectionIndex === sections.length ?
             0 : this.context);
         groups = GrDiffGroup.hideInContextControl(
             groups, hiddenStart, hiddenEnd);
@@ -277,14 +280,15 @@
           right: lineCount,
         },
         groups,
-        newSectionIndex: firstUncommonSectionIndex,
+        newSectionIndex: firstUncollapsibleSectionIndex,
       };
     },
 
     _commonSectionLength(section) {
       console.assert(section.ab || section.common);
       console.assert(
-          !section.a || (section.b && section.a.length === section.b.length));
+          !section.a || (section.b && section.a.length === section.b.length),
+          `common section needs same number of a and b lines: `, section);
       return (section.ab || section.a).length;
     },
 
@@ -302,6 +306,7 @@
       const type = section.ab ? GrDiffGroup.Type.BOTH : GrDiffGroup.Type.DELTA;
       const lines = this._linesFromSection(section, offsetLeft, offsetRight);
       const group = new GrDiffGroup(type, lines);
+      group.keyLocation = section.keyLocation;
       group.dueToRebase = section.due_to_rebase;
       group.ignoredWhitespaceOnly = section.common;
       return group;
@@ -414,13 +419,13 @@
     },
 
     /**
-     * In order to show comments out of the bounds of the selected context,
-     * treat them as separate chunks within the model so that the content (and
-     * context surrounding it) renders correctly.
+     * In order to show key locations, such as comments, out of the bounds of
+     * the selected context, treat them as separate chunks within the model so
+     * that the content (and context surrounding it) renders correctly.
      * @param {!Array<!Object>} chunks DiffContents as returned from server.
      * @return {!Array<!Object>} Finer grained DiffContents.
      */
-    _splitUnchangedChunksWithComments(chunks) {
+    _splitUnchangedChunksWithKeyLocations(chunks) {
       const result = [];
       let leftLineNum = 1;
       let rightLineNum = 1;
@@ -450,18 +455,24 @@
 
         if (chunk.ab) {
           result.push(...this._splitAtChunkEnds(chunk.ab, chunkEnds)
-              .map(lines => Object.assign({}, chunk, {ab: lines})));
+              .map(({lines, keyLocation}) =>
+                  Object.assign({}, chunk, {ab: lines, keyLocation})));
         } else if (chunk.common) {
           const aChunks = this._splitAtChunkEnds(chunk.a, chunkEnds);
           const bChunks = this._splitAtChunkEnds(chunk.b, chunkEnds);
-          result.push(...aChunks.map((lines, i) =>
-              Object.assign({}, chunk, {a: lines, b: bChunks[i]})));
+          result.push(...aChunks.map(({lines, keyLocation}, i) =>
+            Object.assign(
+                {}, chunk, {a: lines, b: bChunks[i].lines, keyLocation})));
         }
       }
 
       return result;
     },
 
+    /**
+     * @return {!Array<{offset: number, keyLocation: boolean}>} Offsets of the
+     *   new chunk ends, including whether it's a key location.
+     */
     _findChunkEndsAtKeyLocations(numLines, leftOffset, rightOffset) {
       const result = [];
       let lastChunkEnd = 0;
@@ -473,17 +484,17 @@
           // this non-collapse line, then add them as a chunk and start a new
           // one.
           if (i > lastChunkEnd) {
-            result.push(i);
+            result.push({offset: i, keyLocation: false});
             lastChunkEnd = i;
           }
 
           // Add the non-collapse line as its own chunk.
-          result.push(i + 1);
+          result.push({offset: i + 1, keyLocation: true});
         }
       }
 
       if (numLines > lastChunkEnd) {
-        result.push(numLines);
+        result.push({offset: numLines, keyLocation: false});
       }
 
       return result;
@@ -491,10 +502,11 @@
 
     _splitAtChunkEnds(lines, chunkEnds) {
       const result = [];
-      let lastChunkEnd = 0;
-      for (const chunkEnd of chunkEnds) {
-        result.push(lines.slice(lastChunkEnd, chunkEnd));
-        lastChunkEnd = chunkEnd;
+      let lastChunkEndOffset = 0;
+      for (const {offset, keyLocation} of chunkEnds) {
+        result.push(
+            {lines: lines.slice(lastChunkEndOffset, offset), keyLocation});
+        lastChunkEndOffset = offset;
       }
       return result;
     },

polygerrit-ui/app/elements/diff/gr-diff-processor/gr-diff-processor_test.html
@@ -460,10 +460,11 @@ limitations under the License.
           },
         ];
         const result =
-            element._splitUnchangedChunksWithComments(content);
+            element._splitUnchangedChunksWithKeyLocations(content);
         assert.deepEqual(result, [
           {
             ab: ['Copyright (C) 2015 The Android Open Source Project'],
+            keyLocation: true,
           },
           {
             ab: [
@@ -477,10 +478,12 @@ limitations under the License.
               '',
               'Unless required by applicable law or agreed to in writing, ',
             ],
+            keyLocation: false,
           },
           {
             ab: [
               'software distributed under the License is distributed on an '],
+            keyLocation: true,
           },
           {
             ab: [
@@ -489,6 +492,7 @@ limitations under the License.
               'language governing permissions and limitations under the ' +
                   'License.',
             ],
+            keyLocation: false,
           },
         ]);
       });
@@ -505,14 +509,16 @@ limitations under the License.
         assert.deepEqual(result[1].ab, content[0].ab.slice(120));
       });
 
-      test('does not break-down shared chunks w/ context', () => {
+      test('does not break-down common chunks w/ context', () => {
         const content = [{
           ab: _.times(75, () => { return `${Math.random()}`; }),
         }];
         element.context = 4;
         const result =
-            element._splitUnchangedChunksWithComments(content);
-        assert.deepEqual(result, content);
+            element._splitUnchangedChunksWithKeyLocations(content);
+        assert.equal(result.length, 1);
+        assert.deepEqual(result[0].ab, content[0].ab);
+        assert.isFalse(result[0].keyLocation);
       });
 
       test('intraline normalization', () => {
@@ -763,6 +769,32 @@ limitations under the License.
           // Results in one uncollapsed group with all rows.
           assert.equal(result.groups.length, 1, 'Results in one group');
           assert.equal(result.groups[0].lines.length, rows.length);
+        });
+
+        test('with key location', () => {
+          element.context = 10;
+          const state = {
+            lineNums: {left: 10, right: 100},
+            sectionIndex: 0,
+          };
+          const sections = [
+            {ab: rows},
+            {ab: ['foo'], keyLocation: true},
+            {ab: rows},
+          ];
+          const result = element._processNext(state, sections);
+
+          assert.equal(result.groups.length, 2);
+
+          // The first section has a single context-control line, the second
+          // section is the context before the key location not processed in
+          // this step
+          assert.equal(result.groups[0].lines.length, 1);
+          assert.equal(result.groups[1].lines.length, element.context);
+
+          // The collapsed group has the hidden lines as its context group.
+          assert.equal(result.groups[0].lines[0].contextGroups[0].lines.length,
+              rows.length - element.context);
         });
       });
 

polygerrit-ui/app/elements/diff/gr-diff/gr-diff-group.js
@@ -40,6 +40,12 @@
      */
     this.ignoredWhitespaceOnly = false;
 
+    /**
+     * True means it should not be collapsed (because it was in the URL, or
+     * there is a comment on that line)
+     */
+    this.keyLocation = false;
+
     /** @type {?HTMLElement} */
     this.element = null;
 

commit 2468117ecf7d0789e84f7f95af0f1a4e868b7045

Fix line number for ab chunks with key location
This would manifest itself as
 - duplicate line numbers
 - broken syntax highlighting
 - comments being attached to the wrong line

This issue was introduced in 6aaa18d.

Change-Id: If85f7eb914de9881bb7b2e3cb590316cc395e156

polygerrit-ui/app/elements/diff/gr-diff-processor/gr-diff-processor.js
@@ -212,8 +212,8 @@
         const section = sections[state.sectionIndex];
         return {
           lineDelta: {
-            left: section.a ? section.a.length : 0,
-            right: section.b ? section.b.length : 0,
+            left: this._linesLeft(section).length,
+            right: this._linesRight(section).length,
           },
           groups: [this._sectionToGroup(
               section, state.lineNums.left + 1, state.lineNums.right + 1)],
@@ -223,6 +223,14 @@
 
       return this._processCollapsibleSections(
           state, sections, firstUncollapsibleSectionIndex);
+    },
+
+    _linesLeft(section) {
+      return section.ab || section.a || [];
+    },
+
+    _linesRight(section) {
+      return section.ab || section.b || [];
     },
 
     _firstUncollapsibleSectionIndex(sections, offset) {

polygerrit-ui/app/elements/diff/gr-diff-processor/gr-diff-processor_test.html
@@ -771,30 +771,66 @@ limitations under the License.
           assert.equal(result.groups[0].lines.length, rows.length);
         });
 
-        test('with key location', () => {
-          element.context = 10;
-          const state = {
-            lineNums: {left: 10, right: 100},
-            sectionIndex: 0,
-          };
-          const sections = [
-            {ab: rows},
-            {ab: ['foo'], keyLocation: true},
-            {ab: rows},
-          ];
-          const result = element._processNext(state, sections);
+        suite('with key location', () => {
+          let state;
+          let sections;
 
-          assert.equal(result.groups.length, 2);
+          setup(() => {
+            state = {
+              lineNums: {left: 10, right: 100},
+            };
+            element.context = 10;
+            sections = [
+              {ab: rows},
+              {ab: ['foo'], keyLocation: true},
+              {ab: rows},
+            ];
+          });
 
-          // The first section has a single context-control line, the second
-          // section is the context before the key location not processed in
-          // this step
-          assert.equal(result.groups[0].lines.length, 1);
-          assert.equal(result.groups[1].lines.length, element.context);
+          test('context before', () => {
+            state.sectionIndex = 0;
+            const result = element._processNext(state, sections);
 
-          // The collapsed group has the hidden lines as its context group.
-          assert.equal(result.groups[0].lines[0].contextGroups[0].lines.length,
-              rows.length - element.context);
+            // The first section is split into two groups:
+            // 1) A context-control, hiding everything but the context before
+            //    the key location.
+            // 2) The context before the key location.
+            // The key location is not processed in this call to _processNext
+            assert.equal(result.groups.length, 2);
+            assert.equal(result.groups[0].lines.length, 1);
+            // The collapsed group has the hidden lines as its context group.
+            assert.equal(result.groups[0].lines[0].contextGroups[0].lines.length,
+                rows.length - element.context);
+            assert.equal(result.groups[1].lines.length, element.context);
+          });
+
+          test('key location itself', () => {
+            state.sectionIndex = 1;
+            const result = element._processNext(state, sections);
+
+            // The second section results in a single group, that is just the
+            // line with the key location
+            assert.equal(result.groups.length, 1);
+            assert.equal(result.groups[0].lines.length, 1);
+            assert.equal(result.lineDelta.left, 1);
+            assert.equal(result.lineDelta.right, 1);
+          });
+
+          test('context after', () => {
+            state.sectionIndex = 2;
+            const result = element._processNext(state, sections);
+
+            // The last section is split into two groups:
+            // 1) The context after the key location.
+            // 1) A context-control, hiding everything but the context after the
+            //    key location.
+            assert.equal(result.groups.length, 2);
+            assert.equal(result.groups[0].lines.length, element.context);
+            assert.equal(result.groups[1].lines.length, 1);
+            // The collapsed group has the hidden lines as its context group.
+            assert.equal(result.groups[1].lines[0].contextGroups[0].lines.length,
+                rows.length - element.context);
+          });
         });
       });
 
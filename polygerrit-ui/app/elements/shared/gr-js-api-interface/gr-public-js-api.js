2f05996e4de841813aeb6182f4a0742d8062612f
Ben Rohlfs <brohlfs@google.com>
2019-07-09 16:43:31 +0200

    Fix plugin install with Polymer 2 and polyfills
    In Safari the variable 'src' ends up being 'data:...' valued, while
    script.baseURI would be usable, so let's add an if statement to just
    use that. 'data:...' is not a suitable input for getPluginNameFromUrl().
    
    Bug: Issue 11128
    Change-Id: I83de47382d335cf67f0922c56f1b10a4b252e529

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-public-js-api.js
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-public-js-api.js
@@ -528,7 +528,11 @@
     // HTML import polyfill adds __importElement pointing to the import tag.
     const script = document.currentScript &&
         (document.currentScript.__importElement || document.currentScript);
-    const src = opt_src || (script && (script.src || script.baseURI));
+    
+    let src = opt_src || (script && script.src);
+    if (!src || src.startsWith('data:')) {
+      src = script && script.baseURI;
+    }
     const name = getPluginNameFromUrl(src);
 
     if (opt_version && opt_version !== API_VERSION) {

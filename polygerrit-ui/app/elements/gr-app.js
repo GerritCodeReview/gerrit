f26b3073461e73e09a79e0f5e0976962f6a6b7d7
Paladox <thomasmulhall410@yahoo.com>
2019-07-14 22:26:05 +0100

    Test: Do not merge
    Change-Id: Ie3ec4fa58929a673961f4649541521c900568d34

--- polygerrit-ui/app/elements/gr-app.js
+++ polygerrit-ui/app/elements/gr-app.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,6 @@
  */
 (function() {
   'use strict';
-
-  // Eagerly render Polymer components when backgrounded. (Skips
-  // requestAnimationFrame.)
-  // @see https://github.com/Polymer/polymer/issues/3851
-  // @see Issue 4699
-  Polymer.RenderStatus._makeReady();
 
   Polymer({
     is: 'gr-app',

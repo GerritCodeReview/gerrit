fe40da43b1a0e41691369f726e2eb1c95566a1fc
Marcin Czech <maczech@gmail.com>
2019-08-02 14:52:51 +0100

    Include submit records in etag calculation
    Submit button and status label are calculated based on submit records
    but submit records are not included in etag calculation so after
    re-indexing etag is not changed and UI is not reloaded. Which is causing
    inconsistent state.
    
    Bug: Issue 11207
    Change-Id: I4a3bd314711f08a14c4bb641b976356485af8902

--- java/com/google/gerrit/server/restapi/change/GetChange.java
+++ java/com/google/gerrit/server/restapi/change/GetChange.java
@@ -14,21 +14,33 @@
 
 package com.google.gerrit.server.restapi.change;
 
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.collect.Iterables;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
 import com.google.gerrit.extensions.client.ListChangesOption;
 import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.extensions.restapi.ETagView;
 import com.google.gerrit.extensions.restapi.Response;
-import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.change.ChangeJson;
 import com.google.gerrit.server.change.ChangeResource;
 import com.google.gerrit.server.change.RevisionResource;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.query.change.InternalChangeQuery;
 import com.google.gwtorm.server.OrmException;
+import com.google.gwtorm.server.OrmRuntimeException;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import java.util.EnumSet;
+import java.util.List;
 import org.kohsuke.args4j.Option;
 
-public class GetChange implements RestReadView<ChangeResource> {
+public class GetChange implements ETagView<ChangeResource> {
   private final ChangeJson.Factory json;
   private final EnumSet<ListChangesOption> options = EnumSet.noneOf(ListChangesOption.class);
+  private final Provider<InternalChangeQuery> queryProvider;
 
   @Option(name = "-o", usage = "Output options")
   void addOption(ListChangesOption o) {
@@ -41,8 +53,9 @@ public class GetChange implements RestReadView<ChangeResource> {
   }
 
   @Inject
-  GetChange(ChangeJson.Factory json) {
+  GetChange(ChangeJson.Factory json, Provider<InternalChangeQuery> queryProvider) {
     this.json = json;
+    this.queryProvider = queryProvider;
   }
 
   @Override
@@ -52,5 +65,25 @@ public class GetChange implements RestReadView<ChangeResource> {
 
   Response<ChangeInfo> apply(RevisionResource rsrc) throws OrmException {
     return Response.withMustRevalidate(json.create(options).format(rsrc));
+  }
+
+  @Override
+  public String getETag(ChangeResource rsrc) {
+    Hasher h = Hashing.murmur3_128().newHasher();
+    CurrentUser user = rsrc.getUser();
+    try {
+      rsrc.prepareETag(h, user);
+      // check if change data are not re-indexed
+      List<ChangeData> cds = queryProvider.get().byLegacyChangeId(rsrc.getChange().getId());
+      checkState(cds.size() <= 1, "Expected one or zero ChangeData, got " + cds.size());
+      ChangeData cd = Iterables.getFirst(cds, null);
+      if (cd != null) {
+        h.putLong(cd.change().getLastUpdatedOn().getTime());
+        h.putInt(cd.change().getRowVersion());
+      }
+    } catch (OrmException e) {
+      throw new OrmRuntimeException(e);
+    }
+    return h.hash().toString();
   }
 }

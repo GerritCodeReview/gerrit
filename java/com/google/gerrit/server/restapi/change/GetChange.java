35dbd3fd6b43ca39301b20ad712656bb1bc4d348
Marcin Czech <maczech@gmail.com>
2019-08-02 14:52:51 +0100

    Include submit records in etag calculation
    Submit button and status label are calculated based on submit records
    but submit records are not included in etag calculation so after
    re-indexing etag is not changed and UI is not reloaded. Which is causing
    inconsistent state.
    
    Bug: Issue 11207
    Change-Id: I4a3bd314711f08a14c4bb641b976356485af8902

--- java/com/google/gerrit/server/restapi/change/GetChange.java
+++ java/com/google/gerrit/server/restapi/change/GetChange.java
@@ -14,21 +14,39 @@
 
 package com.google.gerrit.server.restapi.change;
 
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import com.google.gerrit.common.data.SubmitRecord;
 import com.google.gerrit.extensions.client.ListChangesOption;
 import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.extensions.restapi.ETagView;
 import com.google.gerrit.extensions.restapi.Response;
-import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.change.ChangeJson;
 import com.google.gerrit.server.change.ChangeResource;
 import com.google.gerrit.server.change.RevisionResource;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.gerrit.server.project.SubmitRuleOptions;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.submit.ChangeSet;
+import com.google.gerrit.server.submit.MergeSuperSet;
 import com.google.gwtorm.server.OrmException;
+import com.google.gwtorm.server.OrmRuntimeException;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.io.IOException;
 import java.util.EnumSet;
+import org.eclipse.jgit.lib.Config;
 import org.kohsuke.args4j.Option;
 
-public class GetChange implements RestReadView<ChangeResource> {
+public class GetChange implements ETagView<ChangeResource> {
   private final ChangeJson.Factory json;
   private final EnumSet<ListChangesOption> options = EnumSet.noneOf(ListChangesOption.class);
+  private final Config config;
+  private final Provider<ReviewDb> dbProvider;
+  private final Provider<MergeSuperSet> mergeSuperSet;
+  private final ChangeResource.Factory changeResourceFactory;
 
   @Option(name = "-o", usage = "Output options")
   void addOption(ListChangesOption o) {
@@ -41,8 +59,17 @@ public class GetChange implements RestReadView<ChangeResource> {
   }
 
   @Inject
-  GetChange(ChangeJson.Factory json) {
+  GetChange(
+      ChangeJson.Factory json,
+      Config config,
+      Provider<ReviewDb> dbProvider,
+      Provider<MergeSuperSet> mergeSuperSet,
+      ChangeResource.Factory changeResourceFactory) {
     this.json = json;
+    this.config = config;
+    this.dbProvider = dbProvider;
+    this.mergeSuperSet = mergeSuperSet;
+    this.changeResourceFactory = changeResourceFactory;
   }
 
   @Override
@@ -52,5 +79,34 @@ public class GetChange implements RestReadView<ChangeResource> {
 
   Response<ChangeInfo> apply(RevisionResource rsrc) throws OrmException {
     return Response.withMustRevalidate(json.create(options).format(rsrc));
+  }
+
+  private static final SubmitRuleOptions SUBMIT_RULE_OPTIONS = SubmitRuleOptions.builder().build();
+
+  @Override
+  public String getETag(ChangeResource rsrc) {
+    Hasher h = Hashing.murmur3_128().newHasher();
+    CurrentUser user = rsrc.getUser();
+    try {
+      rsrc.prepareETag(h, user);
+      h.putBoolean(MergeSuperSet.wholeTopicEnabled(config));
+      ReviewDb db = dbProvider.get();
+      ChangeSet cs = mergeSuperSet.get().completeChangeSet(db, rsrc.getChange(), user);
+      Boolean allSubmitRulesOk = true;
+      Boolean allMeargable = true;
+      for (ChangeData cd : cs.changes()) {
+        changeResourceFactory.create(cd.notes(), user).prepareETag(h, user);
+        if (allSubmitRulesOk)
+          allSubmitRulesOk = SubmitRecord.allRecordsOK(cd.getSubmitRecords(SUBMIT_RULE_OPTIONS));
+        if (allMeargable) allMeargable = Boolean.TRUE.equals(cd.isMergeable());
+      }
+
+      h.putBoolean(allSubmitRulesOk);
+      h.putBoolean(allMeargable);
+
+    } catch (IOException | OrmException | PermissionBackendException e) {
+      throw new OrmRuntimeException(e);
+    }
+    return h.hash().toString();
   }
 }

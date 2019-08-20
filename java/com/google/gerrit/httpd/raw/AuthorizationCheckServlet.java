bfe098333e1d01ef0eff3e1e7dc4ba05dfdf64b8
David Pursehouse <dpursehouse@collab.net>
2019-08-20 20:18:37 +0900

    AuthorizationCheckServlet: Add missing serialVersionUID
    Change-Id: Ib90c14e5ff4862ca0c608716b6f166957af2965a

--- java/com/google/gerrit/httpd/raw/AuthorizationCheckServlet.java
+++ java/com/google/gerrit/httpd/raw/AuthorizationCheckServlet.java
@@ -32,6 +32,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Singleton
 public class AuthorizationCheckServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
   private final Provider<CurrentUser> user;
 
   @Inject

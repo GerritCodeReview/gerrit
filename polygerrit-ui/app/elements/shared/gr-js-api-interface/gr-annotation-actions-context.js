54ca082f426011bf2d837674d685c1e0142fa939
Dmitrii Filippov <dmfilippov@google.com>
2019-08-22 14:01:34 +0000

    Revert "Deprecate Gerrit.css() and provide a replacement"
    This reverts commit fe660e05e2ef91b0dcb8ca2056adb45965d8455e.
    
    Reason for revert: Merged by mistake - latest changes in pg-plugin-styles-api.txt was not merged
    
    Change-Id: Ia2301f0778479b752f4f9a6bc9f306b68781cab6

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-annotation-actions-context.js
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-annotation-actions-context.js
@@ -43,26 +43,25 @@
    * Method to add annotations to a content line.
    * @param {number} offset The char offset where the update starts.
    * @param {number} length The number of chars that the update covers.
-   * @param {GrStyleObject} styleObject The style object for the range.
+   * @param {string} cssClass The name of a CSS class created using Gerrit.css.
    * @param {string} side The side of the update. ('left' or 'right')
    */
   GrAnnotationActionsContext.prototype.annotateRange = function(
-      offset, length, styleObject, side) {
+      offset, length, cssClass, side) {
     if (this._contentEl && this._contentEl.getAttribute('data-side') == side) {
-      GrAnnotation.annotateElement(this._contentEl, offset, length,
-          styleObject.getClassName(this._contentEl));
+      GrAnnotation.annotateElement(this._contentEl, offset, length, cssClass);
     }
   };
 
   /**
    * Method to add a CSS class to the line number TD element.
-   * @param {GrStyleObject} styleObject The style object for the range.
+   * @param {string} cssClass The name of a CSS class created using Gerrit.css.
    * @param {string} side The side of the update. ('left' or 'right')
    */
   GrAnnotationActionsContext.prototype.annotateLineNumber = function(
-      styleObject, side) {
+      cssClass, side) {
     if (this._lineNumberEl && this._lineNumberEl.classList.contains(side)) {
-      styleObject.apply(this._lineNumberEl);
+      this._lineNumberEl.classList.add(cssClass);
     }
   };
 

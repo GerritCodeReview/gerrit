c36c7b037cac0976e1de270838fcd400ff97eab8
Matthias Sohn <matthias.sohn@sap.com>
2019-08-24 10:30:18 +0200

    Upgrade JGit to 5.1.10.201908230655-r
    Improvements in 5.1.9 and 5.1.10:
    - measure FileStoreAttributes per filesystem
      - improve measuring file timestamp resolution
      - measure minimal racy interval
      - use Instant to increase resolution to 1us (limited by FileTime)
      - persist FileStoreAttributes
      - optional asynchronous measurement of FileStoreAttributes
      - directory - FileStore cache
    - cache user and system level gitconfig
    - fix supportsAtomicCreateNewFile default to true
    - fix non-deterministic hash of archives created by ArchiveCommand
    
    Release notes:
    https://projects.eclipse.org/projects/technology.jgit/releases/5.1.9
    https://projects.eclipse.org/projects/technology.jgit/releases/5.1.10
    
    Change-Id: Ifc110f1f305d16cb7f3c2dbe55e3b4855c1b7ce1

--- java/com/google/gerrit/acceptance/StandaloneSiteTest.java
+++ java/com/google/gerrit/acceptance/StandaloneSiteTest.java
@@ -36,9 +36,12 @@ import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.Provider;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.storage.file.FileBasedConfig;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.SystemReader;
@@ -179,6 +182,16 @@ public abstract class StandaloneSiteTest {
           @Override
           public int getTimezone(long when) {
             return oldSystemReader.getTimezone(when);
+          }
+
+          @Override
+          public StoredConfig getUserConfig() throws IOException, ConfigInvalidException {
+            return oldSystemReader.getUserConfig();
+          }
+
+          @Override
+          public StoredConfig getSystemConfig() throws IOException, ConfigInvalidException {
+            return oldSystemReader.getSystemConfig();
           }
         });
     return oldSystemReader;

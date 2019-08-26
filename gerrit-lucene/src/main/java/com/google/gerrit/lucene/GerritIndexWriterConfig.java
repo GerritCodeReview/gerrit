8ec5c38a3e004a1457dfb59cda93fcb8557eb514
Trevor Getty <trevor.getty@wandisco.com>
2019-08-26 14:33:19 +0200

    Lucene index configuration and docs.
    Allow the override / configuration of the Lucene index queue and the threads it uses to merge (add/update) data into the index.  You can also turn off the default throttling it puts in place < 20Mb/s.  On storage areas etc, this is very useful to enable more than 1 thread as it detects the storage as spinning rust.
    Added documentation covering each new setting and example update for changes_open and changes_closed indexes.
    
    Bug: Issue 11356
    Change-Id: Ie1be338f5a31f5ad572d9fdec19775b2dd09aa5f

--- gerrit-lucene/src/main/java/com/google/gerrit/lucene/GerritIndexWriterConfig.java
+++ gerrit-lucene/src/main/java/com/google/gerrit/lucene/GerritIndexWriterConfig.java
@@ -22,6 +22,7 @@ import com.google.gerrit.server.config.ConfigUtil;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.index.ConcurrentMergeScheduler;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.eclipse.jgit.lib.Config;
@@ -47,6 +48,19 @@ class GerritIndexWriterConfig {
     luceneConfig = new IndexWriterConfig(analyzer)
         .setOpenMode(OpenMode.CREATE_OR_APPEND)
         .setCommitOnClose(true);
+
+    ConcurrentMergeScheduler mergeScheduler = new ConcurrentMergeScheduler();
+
+    int maxMergeCount = cfg.getInt("index", name, "maxMergeQueue", -1);
+    int maxThreadCount = cfg.getInt("index", name, "maxMergeThreads", -1);
+    if ( maxMergeCount != -1 || maxThreadCount != -1 ) {
+      mergeScheduler.setMaxMergesAndThreads(maxMergeCount, maxThreadCount);
+    }
+    if ( cfg.getBoolean("index", name, "enableAutoIOThrottle", true) == false ) {
+      mergeScheduler.disableAutoIOThrottle();
+    }
+    luceneConfig.setMergeScheduler(mergeScheduler);
+
     double m = 1 << 20;
     luceneConfig.setRAMBufferSizeMB(cfg.getLong(
         "index", name, "ramBufferSize",

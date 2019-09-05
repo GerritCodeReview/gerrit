5b19f023e63572edb0c80effce29642a1aa3902a
Saša Živkov <sasa.zivkov@sap.com>
2019-09-05 10:31:41 +0200

    Allow admins to index a change even if the branch is not Readable for them
    The ChangesCollection rejects the request if the current user doesn't have the
    Read permission on the loaded change. Therefore, an attempt to reindex a change:
    
      PUT /changes/myProject~12345/index
    
    gets rejected even before it reaches the indexing code.
    
    On the other side, the admin can reindex all changes of that project and thus
    also index the change 12345:
    
      POST /projects/myProject/index.changes
    
    Indexing all changes of a project in order to just index one change is too much.
    Add a new REST endpoint where a set of change-ids to index is given in the
    request body.
    
      POST /config/server/index.changes
      Content-Type: application/json; charset=UTF-8
    
      {changes: ["foo~101", "bar~202"]}
    
    Bug: issue 11205
    Change-Id: I05fcc5a024c7634a969a7baac9fd61f3d0f3d12a

---
+++ javatests/com/google/gerrit/acceptance/rest/config/IndexChangesIT.java
@@ -0,0 +1,59 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.rest.config;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.server.restapi.config.IndexChanges;
+import org.junit.Test;
+
+public class IndexChangesIT extends AbstractDaemonTest {
+
+  @Test
+  public void indexRequestFromNonAdminRejected() throws Exception {
+    String changeId = createChange().getChangeId();
+    IndexChanges.Input in = new IndexChanges.Input();
+    in.changes = ImmutableSet.of(changeId);
+    userRestSession.post("/config/server/index.changes", in).assertForbidden();
+  }
+
+  @Test
+  public void indexVisibleChange() throws Exception {
+    String changeId = createChange().getChangeId();
+    IndexChanges.Input in = new IndexChanges.Input();
+    in.changes = ImmutableSet.of(changeId);
+    adminRestSession.post("/config/server/index.changes", in).assertOK();
+  }
+
+  @Test
+  public void indexNonVisibleChange() throws Exception {
+    String changeId = createChange().getChangeId();
+    blockRead("refs/heads/master");
+    IndexChanges.Input in = new IndexChanges.Input();
+    in.changes = ImmutableSet.of(changeId);
+    adminRestSession.post("/config/server/index.changes", in).assertOK();
+  }
+
+  @Test
+  public void indexMultipleChange() throws Exception {
+    ImmutableSet.Builder<String> changeIds = ImmutableSet.builder();
+    for (int i = 0; i < 10; i++) {
+      changeIds.add(createChange().getChangeId());
+    }
+    IndexChanges.Input in = new IndexChanges.Input();
+    in.changes = changeIds.build();
+    adminRestSession.post("/config/server/index.changes", in).assertOK();
+  }
+}

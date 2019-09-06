e7169ddaec3c697393695a3614dab814b0454cff
David Pursehouse <dpursehouse@collab.net>
2019-09-06 12:09:37 +0900

    Stop using JGit's deprecated AnyObjectId.equals method
    The equals method was depecated in favor of isEqual in [1] which
    is included in JGit since version v5.4.1.201908211225-r.
    
    [1] https://git.eclipse.org/r/#/c/147315/
    
    Change-Id: I5366437a04a75135fcd522e65b80be5a34d7c334

--- java/com/google/gerrit/server/patch/PatchListLoader.java
+++ java/com/google/gerrit/server/patch/PatchListLoader.java
@@ -314,12 +314,12 @@ public class PatchListLoader implements Callable<PatchList> {
   }
 
   private static boolean areParentChild(RevCommit commitA, RevCommit commitB) {
-    return ObjectId.equals(commitA.getParent(0), commitB)
-        || ObjectId.equals(commitB.getParent(0), commitA);
+    return ObjectId.isEqual(commitA.getParent(0), commitB)
+        || ObjectId.isEqual(commitB.getParent(0), commitA);
   }
 
   private static boolean haveCommonParent(RevCommit commitA, RevCommit commitB) {
-    return ObjectId.equals(commitA.getParent(0), commitB.getParent(0));
+    return ObjectId.isEqual(commitA.getParent(0), commitB.getParent(0));
   }
 
   private static Set<String> getTouchedFilePaths(PatchListEntry patchListEntry) {

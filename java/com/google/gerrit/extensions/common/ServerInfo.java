4a93858cb2da7adb620f47c9a226d903cb5b9514
Gert van Dijk <gertvdijk@gmail.com>
2019-09-25 22:32:31 +0200

    Remove leftovers of unsupported urlAlias configuration settings
    Only GWT UI supported urlAlias configurations and in 3.0 GWT UI was
    removed.
    
    Bug: Issue 8054
    Change-Id: I70543d484f047922b31dfa804f354eaa0fa9865f

--- java/com/google/gerrit/extensions/common/ServerInfo.java
+++ java/com/google/gerrit/extensions/common/ServerInfo.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.extensions.common;
 
-import java.util.Map;
 
 public class ServerInfo {
   public AccountsInfo accounts;
@@ -26,7 +25,6 @@ public class ServerInfo {
   public PluginConfigInfo plugin;
   public SshdInfo sshd;
   public SuggestInfo suggest;
-  public Map<String, String> urlAliases;
   public UserConfigInfo user;
   public ReceiveInfo receive;
   public String defaultTheme;

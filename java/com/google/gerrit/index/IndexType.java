4c2c9ea113b0cc48ef48bb2aa92924d07f7812a9
Marco Miller <marco.miller@ericsson.com>
2019-09-30 15:12:57 -0400

    IndexType: Add tested support for custom index type
    As custom index types are supported -and required by, e.g., Google. Not
    covering that case used to break the latter, thus required urgent fixes.
    Before this change, breaking that case sometimes went unnoticed at code
    review hence merge time. Make IndexType consider any non-lucene and
    (non-)elasticsearch index type as "custom".
    
    Custom only means that the index type has a String value that differs
    from both of the two explicitly known type values ("lucene" and
    "elasticsearch"). Google is likely configuring a specific index type
    String value, albeit undisclosed externally. This is treated here as a
    (the) 'custom' index type.
    
    Adapt Daemon to not throw IllegalStateException anymore if such a custom
    index type gets configured, by, e.g., the added CustomIndexIT test. This
    test fails otherwise, indeed. The value of this added test is deemed
    superior to the potential loss of Daemon's initIndexType() robustness.
    
    Use lucene as default "custom" index type module for testing purposes.
    
    Change-Id: I805222fb3c40917b294e78b1110d0f45e9878f7c

--- java/com/google/gerrit/index/IndexType.java
+++ java/com/google/gerrit/index/IndexType.java
@@ -18,6 +18,15 @@ import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.common.Nullable;
 import org.eclipse.jgit.lib.Config;
 
+/**
+ * Index types supported by the secondary index.
+ *
+ * <p>The explicitly known index types are Lucene (the default) and Elasticsearch.
+ *
+ * <p>The third supported index type is any other type String value, deemed as custom. This is for
+ * configuring index types that are internal or not to be disclosed. Supporting custom index types
+ * allows to not break that case upon core implementation changes.
+ */
 public class IndexType {
   private static final String LUCENE = "lucene";
   private static final String ELASTICSEARCH = "elasticsearch";

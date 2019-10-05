42704cc75b8787b80de62c68c18d9e5fa6cf5fd8
Paladox none <thomasmulhall410@yahoo.com>
2019-10-05 15:32:57 +0000

    Add undefined check to _computeArchiveDownloadLink and _computeDownloadCommands
    Change-Id: I555f5a953b59065732778dca6c4a129a0d21d231

--- polygerrit-ui/app/elements/change/gr-download-dialog/gr-download-dialog.js
+++ polygerrit-ui/app/elements/change/gr-download-dialog/gr-download-dialog.js
@@ -71,6 +71,7 @@
 
     _computeDownloadCommands(change, patchNum, _selectedScheme) {
       let commandObj;
+      if (!change) return [];
       for (const rev of Object.values(change.revisions || {})) {
         if (this.patchNumEquals(rev._number, patchNum) &&
             rev.fetch.hasOwnProperty(_selectedScheme)) {
@@ -131,6 +132,7 @@
      */
     _computeDownloadFilename(change, patchNum, opt_zip) {
       let shortRev = '';
+      if (!change) return '';
       for (const rev in change.revisions) {
         if (this.patchNumEquals(change.revisions[rev]._number, patchNum)) {
           shortRev = rev.substr(0, 7);
@@ -152,6 +154,7 @@
     },
 
     _computeArchiveDownloadLink(change, patchNum, format) {
+      if (!change) return '';
       return this.changeBaseURL(change.project, change._number, patchNum) +
           '/archive?format=' + format;
     },

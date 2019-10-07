b9a2479c10a67696446ae299ea032f5e144ac939
Dmitrii Filippov <dmfilippov@google.com>
2019-10-07 08:23:26 +0200

    WIP: Implement preview fix endpoint
    Change-Id: I70366164d89bb798b435e31d2670261676b9fc04

--- java/com/google/gerrit/server/fixes/FixReplacementInterpreter.java
+++ java/com/google/gerrit/server/fixes/FixReplacementInterpreter.java
@@ -21,11 +21,14 @@ import com.google.gerrit.common.RawInputUtil;
 import com.google.gerrit.extensions.restapi.BinaryResult;
 import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.jgit.diff.ReplaceEdit;
 import com.google.gerrit.reviewdb.client.Comment;
+import com.google.gerrit.reviewdb.client.Comment.Range;
 import com.google.gerrit.reviewdb.client.FixReplacement;
 import com.google.gerrit.server.change.FileContentUtil;
 import com.google.gerrit.server.edit.tree.ChangeFileContentModification;
 import com.google.gerrit.server.edit.tree.TreeModification;
+import com.google.gerrit.server.patch.Text;
 import com.google.gerrit.server.project.ProjectState;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -34,6 +37,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import javax.swing.text.AbstractDocument.Content;
+import org.eclipse.jgit.diff.Edit;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 
@@ -49,6 +54,11 @@ public class FixReplacementInterpreter {
   @Inject
   public FixReplacementInterpreter(FileContentUtil fileContentUtil) {
     this.fileContentUtil = fileContentUtil;
+  }
+
+  public static Map<String, List<FixReplacement>> getFixReplacementsGroupByFilePath(List<FixReplacement> fixReplacements) {
+    return fixReplacements.stream().collect(groupingBy(fixReplacement -> fixReplacement.path));
+
   }
 
   /**
@@ -73,7 +83,7 @@ public class FixReplacementInterpreter {
     requireNonNull(fixReplacements, "Fix replacements must not be null");
 
     Map<String, List<FixReplacement>> fixReplacementsPerFilePath =
-        fixReplacements.stream().collect(groupingBy(fixReplacement -> fixReplacement.path));
+        getFixReplacementsGroupByFilePath(fixReplacements);
 
     List<TreeModification> treeModifications = new ArrayList<>();
     for (Map.Entry<String, List<FixReplacement>> entry : fixReplacementsPerFilePath.entrySet()) {
@@ -149,4 +159,114 @@ public class FixReplacementInterpreter {
     return String.format(
         "(%s:%s - %s:%s)", range.startLine, range.startChar, range.endLine, range.endChar);
   }
+
+  public static FixResult calculateFix(Text oldContent, List<FixReplacement> fixReplacements) {
+    List<FixReplacement> sortedReplacements = new ArrayList<>(fixReplacements);
+    sortedReplacements.sort(ASC_RANGE_FIX_REPLACEMENT_COMPARATOR);
+    ContentCollector collector = new ContentCollector(oldContent);
+    CharacterPosition nextContentPosition = new CharacterPosition(1, 0);
+    for (FixReplacement fixReplacement : sortedReplacements) {
+      CharacterPosition fixRangeStart = new CharacterPosition(fixReplacement.range.startLine, fixReplacement.range.startChar);
+      if(nextContentPosition.before(fixRangeStart)) {
+        collector.addOldContent(nextContentPosition, fixRangeStart);
+      }
+      CharacterPosition fixRangeEnd = new CharacterPosition(fixReplacement.range.endLine, fixReplacement.range.endChar);
+      collector.addReplacement(fixRangeStart, fixRangeEnd, fixReplacement.replacement);
+      nextContentPosition = fixRangeEnd;
+    }
+    CharacterPosition eof = oldContent.isMissingNewlineAtEnd() ?
+        new CharacterPosition(oldContent.size() - 1, oldContent.getString(oldContent.size() - 1).length())
+        : new CharacterPosition(oldContent.size(), 0);
+    if(nextContentPosition.before(eof)) {
+      collector.addOldContent(nextContentPosition, eof);
+    }
+
+    return new FixResult(new ArrayList<>(collector.edits), oldContent);
+  }
+
+  static class CharacterPosition {
+    int row;
+    int column;
+    public CharacterPosition(int row, int column) {
+      this.row = row;
+      this.column = column;
+    }
+    public boolean before(CharacterPosition other) {
+      return (row < other.row) ||
+          (row == other.row && column < other.column);
+    }
+  }
+
+  private static class ContentCollector {
+    private Text oldContent;
+    private StringBuilder newContent;
+    private int newContentRow;
+    private int newContentColumn;
+    List<Edit> edits;
+    Comment.Range oldContentRange;
+    Comment.Range newContentRange;
+
+
+    ContentCollector(Text oldContent) {
+      this.edits = new ArrayList<>();
+
+      this.oldContent = oldContent;
+      this.newContent = new StringBuilder();
+      newContentRow = 1;
+    }
+    public void addOldContent(CharacterPosition from, CharacterPosition to) {
+      int numOfCharsBefore = newContent.length();
+      String firstString = oldContent.getString(from.row - 1, from.row, false);
+      int lastRow = to.column == 0 ? to.row - 1 : to.row;
+      if(lastRow > from.row) {
+        newContent.append(firstString.substring(from.column));
+        newContentRow++;
+        for (int i = from.row + 1; i < lastRow; i++) {
+          newContent.append(oldContent.getString(i - 1, i, false));
+          newContentRow++;
+        }
+        String lastString = oldContent.getString(lastRow - 1, lastRow, false);
+        if(to.column > 0) {
+          newContent.append(lastString.substring(0, to.column));
+          newContentColumn = to.column;
+        } else {
+          newContent.append(lastString);
+          newContentRow++;
+          newContentColumn = 0;
+        }
+      } else {
+        //lastRow == from.row -- add check!!!
+        newContent.append(firstString.substring(from.column, to.column));
+        newContentColumn += to.column - from.column;
+      }
+      int numOfAddedChars = newContent.length() - numOfCharsBefore;
+    }
+    public void addReplacement(CharacterPosition oldContentStart, CharacterPosition oldContentEnd, String replacement) {
+      int firstRowIndex = newContentRow;
+      newContent.append(replacement);
+      int lineStart = 0;
+      while(true) {
+        int lineEnd = replacement.indexOf('\n', lineStart);
+        if (lineEnd < 0) {
+          break;
+        }
+        newContentRow++;
+        newContentColumn = 0;
+        lineStart = lineEnd + 1;
+      }
+      newContentColumn += replacement.length() - lineStart;
+
+      edits.add(new Edit(oldContentStart.row, oldContentEnd.row, firstRowIndex, newContentRow + 1));
+    }
+  }
+
+  public static class FixResult {
+    public final List<Edit> edits;
+    public final Text text;
+    public FixResult(List<Edit> edits, Text text) {
+      this.edits = edits;
+      this.text = text;
+    }
+  }
+
 }

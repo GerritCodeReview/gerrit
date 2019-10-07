2211fbf231abe0c22e10bab907bdee982a3f90bf
Julie Pan <juliepan@google.com>
2019-10-07 07:37:02 -0700

    Create gr-apply-fix-dialog and show fix preview in gr-diff component.
    in this change:
    - add "Show Fix" option to gr-comment when fixes exist
    - add GET /preview and POST /apply endpoints to gr-rest-api-interface
    - create gr-apply-fix-dialog that fetch preview and supports showing
    multiple diffs for a single fix, apply fix button that navigate to
    edit-change view, and cancel that close dialog
    
    Screenshots: https://imgur.com/a/zcjEfNF
    
    next:
    - support switching between multiple fixes
    
    Change-Id: I9ba5121dd3b5b51791540ac9758f98c8669b44b2

--- polygerrit-ui/app/elements/shared/gr-comment/gr-comment.js
+++ polygerrit-ui/app/elements/shared/gr-comment/gr-comment.js
@@ -42,6 +42,12 @@
      */
 
     /**
+     * Fired when the show fix preview action is triggered.
+     *
+     * @event open-fix-preview
+     */
+
+    /**
      * Fired when this comment is discarded.
      *
      * @event comment-discard
@@ -476,6 +482,18 @@
         composed: true,
         detail: this._getEventPayload(),
       }));
+    },
+
+    _handleShowFix() {
+      this.dispatchEvent(new CustomEvent('open-fix-preview', {
+        bubbles: true,
+        composed: true,
+        detail: this._getEventPayload(),
+      }));
+    },
+
+    _hasNoFix(comment) {
+      return !comment || !comment.fix_suggestions;
     },
 
     _handleDiscard(e) {

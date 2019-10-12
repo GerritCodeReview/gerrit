3bf8b42d51e3aa4d69ee7162d0b99818e42b9f1b
Dhruv Srivastava <dhruvsri@google.com>
2019-10-12 13:33:09 +0200

    Stop query from executing if predicate is empty
    Change-Id: I5b29caf3a025f8b0282185133e18de1b6f21c859

--- polygerrit-ui/app/elements/core/gr-search-bar/gr-search-bar.js
+++ polygerrit-ui/app/elements/core/gr-search-bar/gr-search-bar.js
@@ -96,7 +96,7 @@
 
   // All of the ops, with corresponding negations.
   const SEARCH_OPERATORS_WITH_NEGATIONS =
-      SEARCH_OPERATORS.concat(SEARCH_OPERATORS.map(op => `-${op}`));
+    SEARCH_OPERATORS.concat(SEARCH_OPERATORS.map(op => `-${op}`));
 
   const MAX_AUTOCOMPLETE_RESULTS = 10;
 
@@ -192,6 +192,12 @@
         target.blur();
       }
       if (this._inputVal) {
+        if (SEARCH_OPERATORS_WITH_NEGATIONS.some(op => {
+          return op.substr(op.length - 1) === ':'
+           && op === this._inputVal.trim();
+        })) {
+          return;
+        }
         this.dispatchEvent(new CustomEvent('handle-search', {
           detail: {inputVal: this._inputVal},
         }));

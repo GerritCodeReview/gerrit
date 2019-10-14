77bac90e7914eab2fb8c1c2007361ef6d155d74e
Dmitrii Filippov <dmfilippov@google.com>
2019-10-14 17:32:55 +0200

    WIP: converter to Polymer2 classes
    Change-Id: I60843b32bc56faa04ad2b70898ab1f5535a2ad71

---
+++ tools/polygerrit-updater/js/src/utils.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function unexpetedValue(x) {
+    throw new Error(`Unexpected value '${x}'`);
+}
+exports.unexpetedValue = unexpetedValue;
+//# sourceMappingURL=utils.js.map

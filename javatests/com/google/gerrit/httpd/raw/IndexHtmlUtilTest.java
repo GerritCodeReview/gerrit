bbbad28285e9028e91d8ea04166f0fd47667a927
Tao Zhou <taoalpha@google.com>
2019-10-14 16:22:04 +0200

    Switch from Polymer 1 to Polymer 2
    Bug: Issue 4699
    Change-Id: Ie6ba626a79eeef05798c23228ce7cff94a45e0a0

--- javatests/com/google/gerrit/httpd/raw/IndexHtmlUtilTest.java
+++ javatests/com/google/gerrit/httpd/raw/IndexHtmlUtilTest.java
@@ -17,69 +17,60 @@ package com.google.gerrit.httpd.raw;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.gerrit.httpd.raw.IndexHtmlUtil.staticTemplateData;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.template.soy.data.SanitizedContent;
 import com.google.template.soy.data.UnsafeSanitizedContentOrdainer;
 import java.util.HashMap;
 import org.junit.Test;
 
 public class IndexHtmlUtilTest {
-  @Test
-  public void polymer2() throws Exception {
-    assertThat(
-            staticTemplateData(
-                "http://example.com/",
-                null,
-                null,
-                ImmutableMap.of("p2", new String[0]),
-                IndexHtmlUtilTest::ordain))
-        .containsExactly("canonicalPath", "", "polymer2", "true", "staticResourcePath", ordain(""));
-  }
 
   @Test
   public void noPathAndNoCDN() throws Exception {
     assertThat(
-            staticTemplateData(
-                "http://example.com/", null, null, new HashMap<>(), IndexHtmlUtilTest::ordain))
+        staticTemplateData(
+            "http://example.com/", null, null, new HashMap<>(), IndexHtmlUtilTest::ordain))
         .containsExactly("canonicalPath", "", "staticResourcePath", ordain(""));
   }
 
   @Test
   public void pathAndNoCDN() throws Exception {
     assertThat(
-            staticTemplateData(
-                "http://example.com/gerrit/",
-                null,
-                null,
-                new HashMap<>(),
-                IndexHtmlUtilTest::ordain))
-        .containsExactly("canonicalPath", "/gerrit", "staticResourcePath", ordain("/gerrit"));
+        staticTemplateData(
+            "http://example.com/gerrit/",
+            null,
+            null,
+            new HashMap<>(),
+            IndexHtmlUtilTest::ordain))
+        .containsExactly("canonicalPath", "/gerrit", "staticResourcePath",
+            ordain("/gerrit"));
   }
 
   @Test
   public void noPathAndCDN() throws Exception {
     assertThat(
-            staticTemplateData(
-                "http://example.com/",
-                "http://my-cdn.com/foo/bar/",
-                null,
-                new HashMap<>(),
-                IndexHtmlUtilTest::ordain))
+        staticTemplateData(
+            "http://example.com/",
+            "http://my-cdn.com/foo/bar/",
+            null,
+            new HashMap<>(),
+            IndexHtmlUtilTest::ordain))
         .containsExactly(
-            "canonicalPath", "", "staticResourcePath", ordain("http://my-cdn.com/foo/bar/"));
+            "canonicalPath", "", "staticResourcePath",
+            ordain("http://my-cdn.com/foo/bar/"));
   }
 
   @Test
   public void pathAndCDN() throws Exception {
     assertThat(
-            staticTemplateData(
-                "http://example.com/gerrit",
-                "http://my-cdn.com/foo/bar/",
-                null,
-                new HashMap<>(),
-                IndexHtmlUtilTest::ordain))
+        staticTemplateData(
+            "http://example.com/gerrit",
+            "http://my-cdn.com/foo/bar/",
+            null,
+            new HashMap<>(),
+            IndexHtmlUtilTest::ordain))
         .containsExactly(
-            "canonicalPath", "/gerrit", "staticResourcePath", ordain("http://my-cdn.com/foo/bar/"));
+            "canonicalPath", "/gerrit", "staticResourcePath",
+            ordain("http://my-cdn.com/foo/bar/"));
   }
 
   private static SanitizedContent ordain(String s) {

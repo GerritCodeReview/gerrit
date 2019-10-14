4e58da123483220f4e8adb9d450607c1a75d63bf
Han-Wen Nienhuys <hanwen@google.com>
2019-10-14 17:47:42 +0200

    restapi: add an alternative CreateChange endpoint
    In distributed installations, such as googlesource.com, different
    tasks (running on different machines) are responsible for different
    projects. This new API endpoint has the project name in the URL, so
    load balancers can route requests to the task that has the data in
    memory.
    
    It might be more principled to add the whole /changes/ collection as a
    child to /projects/ , but this is hard because the changes collection
    is hard coded to be a a toplevel resource.
    
    Every other REST operations on changes includes a change identifier
    (eg. project~123), and can be routed based on that identifier.
    
    This change was earlier submitted and then reverted as
    I318603de0418e177f742684567867bf90571ac70, as it did not bring any speed
    improvements.
    
    I am resurrecting this change, because our oncall team prefers a URL on
    /projects. URLs are available more easily in logs than POST payloads, so this
    endpoint improves debuggability of our service.
    
    Change-Id: I2f24d7c44ae5a0b21356c7b2e400391ec14b5865

---
+++ javatests/com/google/gerrit/acceptance/rest/project/CreateChangeIT.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.rest.project;
+
+import static com.google.common.truth.Truth8.assertThat;
+import static com.google.gerrit.entities.RefNames.REFS_HEADS;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.extensions.api.projects.BranchInput;
+import com.google.gerrit.extensions.common.ChangeInput;
+import org.junit.Test;
+
+public class CreateChangeIT extends AbstractDaemonTest {
+
+  // Just a basic test. The real functionality is tested under the restapi.change acceptance tests.
+  @Test
+  public void basic() throws Exception {
+    BranchInput branchInput = new BranchInput();
+    branchInput.ref = "foo";
+    assertThat(gApi.projects().name(project.get()).branches().get().stream().map(i -> i.ref))
+        .doesNotContain(REFS_HEADS + branchInput.ref);
+    RestResponse r =
+        adminRestSession.put(
+            "/projects/" + project.get() + "/branches/" + branchInput.ref, branchInput);
+    r.assertCreated();
+
+    ChangeInput input = new ChangeInput();
+    input.branch = "foo";
+    input.subject = "subject";
+    RestResponse cr = adminRestSession.post("/projects/" + project.get() + "/create.change", input);
+    cr.assertCreated();
+  }
+}

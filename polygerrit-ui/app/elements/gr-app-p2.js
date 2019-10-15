84f079780445c15bc5ab3d712a6e83162dfec9e6
Tao Zhou <taoalpha@google.com>
2019-10-15 13:52:17 +0200

    Switch from Polymer 1 to Polymer 2
    Bug: Issue 4699
    Change-Id: Ie6ba626a79eeef05798c23228ce7cff94a45e0a0

--- polygerrit-ui/app/elements/gr-app-p2.js
+++ polygerrit-ui/app/elements/gr-app-p2.js
@@ -14,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// TODO(taoalpha): This file is only used by google,
+// remove once polymer fully rolled out.
 (function() {
   'use strict';
 

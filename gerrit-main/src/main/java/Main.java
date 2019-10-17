b2a523d20766c78c2260f1bf17fe47225565b308
Thomas Draebing <thomas.draebing@sap.com>
2019-10-17 13:30:36 +0200

    [DO NOT MERGE] testing the build pipeline
    Change-Id: Iea19da3f107f1189ef664e306ed5b3d021628086

--- gerrit-main/src/main/java/Main.java
+++ gerrit-main/src/main/java/Main.java
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+// another test
+
 public final class Main {
   // We don't do any real work here because we need to import
   // the archive lookup code and we cannot import a class in

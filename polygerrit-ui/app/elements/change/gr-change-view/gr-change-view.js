345834fa6802ae68b402906626b9434008d441c9
Milutin Kristofic <milutin@google.com>
2019-11-05 14:23:43 +0100

    Report tab change - mostly for checks plugin
    We report only when tab really changed.
    
    Change-Id: I06f285a3e29bdde0a53eb24d7926f93a2d512c55

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -415,15 +415,24 @@
       this._showMessagesView = this.$.commentTabs.selected === 0;
     },
 
-    _handleFileTabChange() {
+    _handleFileTabChange(event) {
       const selectedIndex = this.$$('#primaryTabs').selected;
       this._showFileTabContent = selectedIndex === 0;
       // Initial tab is the static files list.
-      this._selectedFilesTabPluginEndpoint =
+      const selectedFilesTabPluginEndpoint =
           this._dynamicTabContentEndpoints[selectedIndex - 1];
+      if (selectedFilesTabPluginEndpoint !==
+          this._selectedFilesTabPluginEndpoint) {
+        this._selectedFilesTabPluginEndpoint = selectedFilesTabPluginEndpoint;
+
+        const tabName = this._selectedFilesTabPluginEndpoint || 'files';
+        const source = event && event.type ? event.type : '';
+        this.$.reporting.reportInteraction('tab-changed',
+            `tabname: ${tabName}, source: ${source}`);
+      }
     },
 
-    _handleShowChecksTable(e) {
+    _handleShowTab(e) {
       const idx = this._dynamicTabContentEndpoints.indexOf(e.detail.tab);
       if (idx === -1) {
         console.warn(e.detail.tab + ' tab not found');
@@ -431,7 +440,7 @@
       }
       this.$$('#primaryTabs').selected = idx + 1;
       this.$$('#primaryTabs').scrollIntoView();
-      this._handleFileTabChange();
+      this.$.reporting.reportInteraction('show-tab', e.detail.tab);
     },
 
     _handleEditCommitMessage(e) {

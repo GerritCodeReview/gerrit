4afd70fd0334f016730ea0dbd056c62e5226befa
David Ostrovsky <david@ostrovsky.org>
2019-11-09 12:38:13 +0100

    Replace guava caches with caffeine
    Replacing Guava caches by Caffeine brings 2 things, it reduces the
    chances of having the deadlocks and improve the cache performance.
    
    This was already attempted in: I8d2b17a94d0, but got reverted in:
    If65560b4a9b due to cache loading issue. On current master that
    problem does not occur any more.
    
    Bug: Issue 7645
    Bug: Issue 11484
    Change-Id: I6af4c15d6c15f438becd62409b7d233c309be8de

--- java/com/google/gerrit/server/cache/mem/DefaultMemoryCacheFactory.java
+++ java/com/google/gerrit/server/cache/mem/DefaultMemoryCacheFactory.java
@@ -17,12 +17,15 @@ package com.google.gerrit.server.cache.mem;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalListener;
+import com.github.benmanes.caffeine.cache.Weigher;
+import com.github.benmanes.caffeine.guava.CaffeinatedGuava;
 import com.google.common.base.Strings;
 import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.cache.Weigher;
+import com.google.common.cache.RemovalNotification;
 import com.google.gerrit.common.Nullable;
 import com.google.gerrit.server.cache.CacheDef;
 import com.google.gerrit.server.cache.ForwardingRemovalListener;
@@ -47,28 +50,21 @@ class DefaultMemoryCacheFactory implements MemoryCacheFactory {
 
   @Override
   public <K, V> Cache<K, V> build(CacheDef<K, V> def) {
-    return create(def).build();
+    return CaffeinatedGuava.build(create(def));
   }
 
   @Override
   public <K, V> LoadingCache<K, V> build(CacheDef<K, V> def, CacheLoader<K, V> loader) {
-    return create(def).build(loader);
+    return CaffeinatedGuava.build(create(def), loader);
   }
 
-  @SuppressWarnings("unchecked")
-  private <K, V> CacheBuilder<K, V> create(CacheDef<K, V> def) {
-    CacheBuilder<K, V> builder = newCacheBuilder();
+  private <K, V> Caffeine<K, V> create(CacheDef<K, V> def) {
+    Caffeine<K, V> builder = newCacheBuilder();
     builder.recordStats();
     builder.maximumWeight(
         cfg.getLong("cache", def.configKey(), "memoryLimit", def.maximumWeight()));
-
-    builder = builder.removalListener(forwardingRemovalListenerFactory.create(def.name()));
-
-    Weigher<K, V> weigher = def.weigher();
-    if (weigher == null) {
-      weigher = unitWeight();
-    }
-    builder.weigher(weigher);
+    builder = builder.removalListener(newRemovalListener(def.name()));
+    builder.weigher(newWeigher(def.weigher()));
 
     Duration expireAfterWrite = def.expireAfterWrite();
     if (has(def.configKey(), "maxAge")) {
@@ -107,11 +103,29 @@ class DefaultMemoryCacheFactory implements MemoryCacheFactory {
   }
 
   @SuppressWarnings("unchecked")
-  private static <K, V> CacheBuilder<K, V> newCacheBuilder() {
-    return (CacheBuilder<K, V>) CacheBuilder.newBuilder();
+  private static <K, V> Caffeine<K, V> newCacheBuilder() {
+    return (Caffeine<K, V>) Caffeine.newBuilder();
   }
 
-  private static <K, V> Weigher<K, V> unitWeight() {
-    return (key, value) -> 1;
+  @SuppressWarnings("unchecked")
+  private <V, K> RemovalListener<K, V> newRemovalListener(String cacheName) {
+    return (k, v, cause) ->
+        forwardingRemovalListenerFactory
+            .create(cacheName)
+            .onRemoval(
+                RemovalNotification.create(
+                    k, v, com.google.common.cache.RemovalCause.valueOf(cause.name())));
+  }
+
+  private static <K, V> Weigher<K, V> newWeigher(
+      com.google.common.cache.Weigher<K, V> guavaWeigher) {
+    return guavaWeigher == null
+        ? Weigher.singletonWeigher()
+        : new com.github.benmanes.caffeine.cache.Weigher<K, V>() {
+          @Override
+          public int weigh(K key, V value) {
+            return guavaWeigher.weigh(key, value);
+          }
+        };
   }
 }

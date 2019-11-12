aa68fc4cd2d646210831a49f32eedeaca88c926f
David Ostrovsky <david@ostrovsky.org>
2019-11-12 05:49:23 -0800

    Switch PatchListCache to using legacy cache backend
    The implementation of PatchListLoader is using internally the
    PatchListCache, to retrieve rebase related edits. The loading function
    calling back into the cache violates the Caffeine interface contracts.
    Guava could, theoretically, be more tolerant than Caffeine. This is due
    to the hashtable design of locking and table expansion. However both are
    explicit that the cache loader cannot write into the cache. Caffeine
    will fail a fibonacci test, whereas Guava might pass. But that is
    taking a lot of risk by relying on implementation details and violating
    the contracts.
    
    Switch to using legacy cache backend for PatchListCache, and consider
    to switch using Caffeine cache backend and remove the legacy cache
    support altogether, when the PatchListLoader is fixed to be recursion
    free.
    
    Bug: Issue 11900
    Change-Id: I8f129dcf9b5574f024a600162d98aa381b3513cc
    (cherry picked from commit 1d8f5ca43512ea7bc293520bfdbca23f1ad663fd)

--- java/com/google/gerrit/server/patch/PatchListCacheImpl.java
+++ java/com/google/gerrit/server/patch/PatchListCacheImpl.java
@@ -15,6 +15,10 @@
 
 package com.google.gerrit.server.patch;
 
+import org.eclipse.jgit.errors.LargeObjectException;
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.ObjectId;
+
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.cache.Cache;
 import com.google.common.util.concurrent.UncheckedExecutionException;
@@ -22,6 +26,7 @@ import com.google.gerrit.extensions.client.DiffPreferencesInfo.Whitespace;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.cache.CacheBackend;
 import com.google.gerrit.server.cache.CacheModule;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.inject.Inject;
@@ -29,9 +34,6 @@ import com.google.inject.Module;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import java.util.concurrent.ExecutionException;
-import org.eclipse.jgit.errors.LargeObjectException;
-import org.eclipse.jgit.lib.Config;
-import org.eclipse.jgit.lib.ObjectId;
 
 /** Provides a cached list of {@link PatchListEntry}. */
 @Singleton
@@ -45,7 +47,9 @@ public class PatchListCacheImpl implements PatchListCache {
       @Override
       protected void configure() {
         factory(PatchListLoader.Factory.class);
-        persist(FILE_NAME, PatchListKey.class, PatchList.class)
+        // TODO(davido): Switch off using legacy cache backend, after fixing PatchListLoader
+        // to be recursion free.
+        persist(FILE_NAME, PatchListKey.class, PatchList.class, CacheBackend.GUAVA)
             .maximumWeight(10 << 20)
             .weigher(PatchListWeigher.class);
 

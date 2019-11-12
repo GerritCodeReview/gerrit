bba1dab5c214719d078f45f159e5ace1cd3474a5
David Ostrovsky <david@ostrovsky.org>
2019-11-12 05:49:23 -0800

    Switch PatchListCache to using legacy cache backend
    The implementation of PatchListLoader is using internally the
    PatchListCache, to retrieve rebase related edits. The loading function
    calling back into the cache violates the Caffeine interface contracts.
    Guava could, theoretically, be more tolerant than Caffeine. This is due
    to the hashtable design of locking and table expansion. However both are
    explicit that the cache loader cannot write into the cache. Caffeine
    will fail a fibonacci test, whereas Guava might pass. But that is
    taking a lot of risk by relying on implementation details and violating
    the contracts.
    
    Switch to using legacy cache backend for PatchListCache, and consider
    to switch using Caffeine cache backend and remove the legacy cache
    support altogether, when the PatchListLoader is fixed to be recursion
    free.
    
    Bug: Issue 11900
    Change-Id: I8f129dcf9b5574f024a600162d98aa381b3513cc

--- java/com/google/gerrit/server/cache/CacheBackend.java
+++ java/com/google/gerrit/server/cache/CacheBackend.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 The Android Open Source Project
+// Copyright (C) 2009 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,13 +14,11 @@
 
 package com.google.gerrit.server.cache;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+public enum CacheBackend {
+  CAFFEINE,
+  GUAVA;
 
-public interface MemoryCacheFactory {
-  <K, V> Cache<K, V> build(CacheDef<K, V> def, CacheBackend backend);
-
-  <K, V> LoadingCache<K, V> build(
-      CacheDef<K, V> def, CacheLoader<K, V> loader, CacheBackend backend);
+  public boolean isLegacyBackend() {
+    return this == GUAVA;
+  }
 }

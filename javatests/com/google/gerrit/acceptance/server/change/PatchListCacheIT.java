acba2f125218aa2975b2f3b12fa6f1d0caa627c3
David Ostrovsky <david@ostrovsky.org>
2019-11-12 05:49:23 -0800

    Switch PatchListCache to using legacy cache backend
    The implementation of PatchListLoader is using internally the
    PatchListCache, to retrieve rebase related edits. The loading
    function calling back into the cache violates the interface contracts.
    Guava could, theoretically, be more tolerant than Caffeine. This is due
    to the hashtable design of locking and table expansion. However both are
    explicit that the cache loader cannot write into the cache. Caffeine
    will fail a fibonacci test, whereas Guava might pass. But that is
    taking a lot of risk by relying on implementation details and violating
    the contracts.
    
    Switch to using legacy cache backend for PatchListCache, and consider
    to switch using Caffeine cache backend and remove the legacy cache
    support altogether, when the PatchListLoader is fixed to be recursion
    free.
    
    Bug: Issue 11900
    Change-Id: I8f129dcf9b5574f024a600162d98aa381b3513cc

--- javatests/com/google/gerrit/acceptance/server/change/PatchListCacheIT.java
+++ javatests/com/google/gerrit/acceptance/server/change/PatchListCacheIT.java
@@ -39,7 +39,9 @@ import com.google.gerrit.server.patch.PatchListKey;
 import com.google.gerrit.server.patch.Text;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -64,6 +66,29 @@ public class PatchListCacheIT extends AbstractDaemonTest {
   @Inject
   @Named("diff")
   private Cache<PatchListKey, PatchList> abstractPatchListCache;
+
+  @Test
+  public void ensureLegacyBackendIsUsedForFileCacheBackend() throws Exception {
+    Field fileCacheField = patchListCache.getClass().getDeclaredField("fileCache");
+    fileCacheField.setAccessible(true);
+    // Use the reflection to access "localCache" field that is only present in Guava backend.
+    assertThat(
+            Arrays.stream(fileCacheField.get(patchListCache).getClass().getDeclaredFields())
+                .filter(f -> f.getName().equals("localCache"))
+                .findAny()
+                .isPresent())
+        .isTrue();
+
+    // intraCache (and all other cache backends) should use Caffeine backend.
+    Field intraCacheField = patchListCache.getClass().getDeclaredField("intraCache");
+    intraCacheField.setAccessible(true);
+    assertThat(
+            Arrays.stream(intraCacheField.get(patchListCache).getClass().getDeclaredFields())
+                .filter(f -> f.getName().equals("localCache"))
+                .findAny()
+                .isPresent())
+        .isFalse();
+  }
 
   @Test
   public void listPatchesAgainstBase() throws Exception {

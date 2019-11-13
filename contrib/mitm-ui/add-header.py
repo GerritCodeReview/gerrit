ca48b37e1e2edc252b170095bdc84523232d9f1e
Tao Zhou <taoalpha@google.com>
2019-11-13 11:51:09 -0800

    Remove mitm-ui
    Change-Id: Ic60a9af8b6afe850e9a6952d0b3c28bdeceb4f53

--- contrib/mitm-ui/add-header.py
+++
@@ -1,5 +0,0 @@
-# mitmdump -s add-header.py
-def response(flow):
-    if flow.request.host == 'gerrit-review.googlesource.com' and flow.request.path == "/c/92000?1":
-        #flow.response.headers['any'] = '<meta.rdf>; rel=meta'
-        flow.response.headers['Link'] = '</changes/98000/detail?O=11640c>;rel="preload";crossorigin;'
--- contrib/mitm-ui/dev-chrome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [[ "$OSTYPE" != "darwin"* ]]; then
-    echo Only works on OSX.
-    exit 1
-fi
-
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --user-data-dir=${HOME}/devchrome --proxy-server="127.0.0.1:8888"

f8c1bcd25ac2860081d4ddffdd62a4c2afcf89e4
Gal Paikin <paiking@google.com>
2019-11-13 16:19:51 -0800

    Expose query submission ID to the user
    As a follow-up for RevertSubmission (b81f56c469), the users
    could wish to know the changes of a certain submission if they
    are considering to revert the entire submission, or if the submission
    was already reverted and they wish to find it quickly.
    
    Also, it is important for the frontend to know which changes belong
    to a certain submission, so that they can suggest a better commit
    message to the user when the user wants to revert a submission.
    
    Change-Id: I337f7f8aeea2bc4146ed7e642f41c8710a153634

--- javatests/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
+++ javatests/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
@@ -2502,6 +2502,19 @@ public abstract class AbstractQueryChangesTest extends GerritServerTests {
     assertQueryByIds("revertof:" + changeToRevert._number, Change.id(changeThatReverts._number));
   }
 
+  @Test
+  public void submissionId() throws Exception {
+    TestRepository<Repo> repo = createProject("repo");
+    Change change = insert(repo, newChange(repo));
+    // create irrelevant change
+    insert(repo, newChange(repo));
+    gApi.changes().id(change.getChangeId()).current().review(ReviewInput.approve());
+    gApi.changes().id(change.getChangeId()).current().submit();
+    String submissionId = gApi.changes().id(change.getChangeId()).get().submissionId;
+
+    assertQueryByIds("submissionid:\"" + submissionId + "\"", change.getId());
+  }
+
   /** Change builder for helping in tests for dashboard sections. */
   protected class DashboardChangeState {
     private final Account.Id ownerId;

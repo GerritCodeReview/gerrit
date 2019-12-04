a70c637786e43e8c7e66623dfa7e903ee8644178
Dmitrii Filippov <dmfilippov@google.com>
2019-12-04 17:15:10 +0100

    Add files to support legacy HTML imports
    The Polymer 3 doesn't support HTML import. To avoid massive code
    changes during transition to Polymer 3 bridges must be added.
    
    The bridges are not used in this change.
    
    Change-Id: Iedef8b081264ec17e84f9d91cc3ef435f41746b4

---
+++ polygerrit-ui/app/polymer_legacy_support/iron-selector/iron-multi-selectable_bridge.js
@@ -0,0 +1,32 @@
+/**
+@license
+Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+/**
+ * @fileoverview This file is a backwards-compatibility shim. Before Polymer
+ *     converted to ES Modules, it wrote its API out onto the global Polymer
+ *     object. The *_bridge.js files (like this one) maintain compatibility
+ *     with that API.
+ */
+ 
+import '../polymer/lib/utils/boot_bridge.js';
+import {IronMultiSelectableBehaviorImpl, IronMultiSelectableBehavior} from '@polymer/iron-selector/iron-multi-selectable.js';
+
+/**
+ * @const
+ */
+Polymer.IronMultiSelectableBehaviorImpl = IronMultiSelectableBehaviorImpl;
+/**
+ * @const
+ */
+Polymer.IronMultiSelectableBehavior = IronMultiSelectableBehavior;
+
+
+goog.declareModuleId('HtmlImportsNamespace.IronSelector.IronMultiSelectable');
+

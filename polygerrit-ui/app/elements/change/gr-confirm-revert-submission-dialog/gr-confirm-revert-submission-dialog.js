30a32f05465d3356b645e1768e16a32de3d4ac01
Dhruv Srivastava <dhruvsri@google.com>
2019-12-05 17:44:31 +0100

    Add submission ID and reverted changes to commit message
    https://imgur.com/a/ZLtPU0c
    Change-Id: I8fee2fb06acb27822e370c5b5ec4127bc27bb460

--- polygerrit-ui/app/elements/change/gr-confirm-revert-submission-dialog/gr-confirm-revert-submission-dialog.js
+++ polygerrit-ui/app/elements/change/gr-confirm-revert-submission-dialog/gr-confirm-revert-submission-dialog.js
@@ -47,15 +47,20 @@
       };
     }
 
-    populateRevertSubmissionMessage(message, commitHash) {
+    populateRevertSubmissionMessage(message, commitHash, submissionId,
+        changes) {
       // Follow the same convention of the revert
-      const revertTitle = 'Revert submission';
+      const revertTitle = 'Revert submission ' + submissionId;
       if (!commitHash) {
         this.fire('show-alert', {message: ERR_COMMIT_NOT_FOUND});
         return;
       }
       this.message = `${revertTitle}\n\n` +
           `Reason for revert: <INSERT REASONING HERE>\n`;
+      changes = changes || [];
+      changes.forEach(change => {
+        this.message = this.message + '\nChange: ' + change.change_id;
+      });
     }
 
     _handleConfirmTap(e) {

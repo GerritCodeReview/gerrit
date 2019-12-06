bd632a464591ecf4c49a5216df3908e509aa90ff
Tao Zhou <taoalpha@google.com>
2019-12-06 22:55:11 +0100

    Add a shared pre-commit to run eslintfix for frontend code changes
    This pre-commit hook will fail the commit when you have linter errors.
    It will also only run on changed staged files.
    
    Also cleaned up linter config in package and lint_test.
    
    Change-Id: I312b16d8ad2245b2cd848cb03b2a50e56372aa72

--- polygerrit-ui/app/lint_test.sh
+++ polygerrit-ui/app/lint_test.sh
@@ -19,4 +19,4 @@ if [ -z "$eslint_bin" ] || [ "$eslint_config" -eq "0" ] || [ "$eslint_plugin" -e
     exit 1
 fi
 
-${eslint_bin} --ignore-pattern 'bower_components/' --ignore-pattern 'gr-linked-text' --ignore-pattern 'scripts/vendor' --ext .html,.js .
+${eslint_bin} --ext .html,.js .

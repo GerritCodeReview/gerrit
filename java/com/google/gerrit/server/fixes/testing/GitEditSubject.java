65eebb530a51b80621ca05692ae703bce8eb7eec
Dmitrii Filippov <dmfilippov@google.com>
2019-12-06 03:13:06 +0100

    WIP: Implement preview fix endpoint
    Change-Id: I70366164d89bb798b435e31d2670261676b9fc04

---
+++ java/com/google/gerrit/server/fixes/testing/GitEditSubject.java
@@ -0,0 +1,86 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.fixes.testing;
+
+import static com.google.common.truth.Truth.assertAbout;
+import static com.google.gerrit.truth.ListSubject.elements;
+
+import com.google.common.truth.FailureMetadata;
+import com.google.common.truth.Subject;
+import com.google.gerrit.jgit.diff.ReplaceEdit;
+import com.google.gerrit.truth.ListSubject;
+import org.eclipse.jgit.diff.Edit;
+import org.eclipse.jgit.diff.Edit.Type;
+
+public class GitEditSubject extends Subject {
+  public static GitEditSubject assertThat(Edit edit) {
+    return assertAbout(gitEdits()).that(edit);
+  }
+
+  public static Subject.Factory<GitEditSubject, Edit> gitEdits() {
+    return GitEditSubject::new;
+  }
+
+  private final Edit edit;
+
+  private GitEditSubject(FailureMetadata failureMetadata, Edit edit) {
+    super(failureMetadata, edit);
+    this.edit = edit;
+  }
+
+  public GitEditSubject hasRegions(int beginA, int endA, int beginB, int endB) {
+    isNotNull();
+    check("beginA").that(edit.getBeginA()).isEqualTo(beginA);
+    check("endA").that(edit.getEndA()).isEqualTo(endA);
+    check("beginB").that(edit.getBeginB()).isEqualTo(beginB);
+    check("endB").that(edit.getEndB()).isEqualTo(endB);
+    return this;
+  }
+
+  public GitEditSubject hasType(Type type) {
+    isNotNull();
+    check("getType").that(edit.getType()).isEqualTo(type);
+    return this;
+  }
+
+  public GitEditSubject isInsert(int insertPos, int beginB, int insertedLength) {
+    isNotNull();
+    return hasType(Type.INSERT).hasRegions(insertPos, insertPos, beginB, beginB + insertedLength);
+  }
+
+  public GitEditSubject isDelete(int deletePos, int deletedLength, int posB) {
+    isNotNull();
+    return hasType(Type.DELETE).hasRegions(deletePos, deletePos + deletedLength, posB, posB);
+  }
+
+  public GitEditSubject isReplace(int originalPos, int originalLength, int newPos, int newLength) {
+    isNotNull();
+    return hasType(Type.REPLACE)
+        .hasRegions(originalPos, originalPos + originalLength, newPos, newPos + newLength);
+  }
+
+  public GitEditSubject isEmpty() {
+    isNotNull();
+    return hasType(Type.EMPTY);
+  }
+
+  public ListSubject<GitEditSubject, Edit> hasInternalEdits() {
+    isNotNull();
+    isInstanceOf(ReplaceEdit.class);
+    return check("internalEdits")
+        .about(elements())
+        .thatCustom(((ReplaceEdit) edit).getInternalEdits(), gitEdits());
+  }
+}
---
+++ javatests/com/google/gerrit/server/fixes/FixCalculatorTest.java
@@ -0,0 +1,157 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.fixes;
+
+import static com.google.gerrit.server.fixes.testing.FixResultSubject.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.entities.Comment.Range;
+import com.google.gerrit.entities.FixReplacement;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.server.fixes.FixCalculator.FixResult;
+import com.google.gerrit.server.patch.Text;
+import org.junit.Test;
+
+public class FixCalculatorTest {
+  private static final String emptySrc = "";
+  private static final String oneLineSrcNoEOL = "First line";
+  private static final String oneLineSrcHasEOL = "First line\n";
+  private static final String multiLineSrcNoEOL = "First line\nSecond line\nThird line";
+  private static final String multiLineSrcHasEol = "First line\nSecond line\nThird line\n";
+
+  private static final String emptyReplacement = "";
+  private static final String oneLineReplacementNoEOL = "ABC";
+  private static final String oneLineReplacementHasEOL = "ABC\n";
+  private static final String multiLineReplacementNoEol = "ABC\nDEFG\nHK";
+  private static final String multiLineReplacementEol = "ABC\nDEFG\nHK\n";
+  private static final String replacementStartsWithEOL = "\nABC\nDEFG\nHK\n";
+
+  public static FixResult calculateFixSingleReplacement(
+      String content, int startLine, int startChar, int endLine, int endChar, String replacement)
+      throws ResourceConflictException {
+    FixReplacement fixReplacement =
+        new FixReplacement(
+            "AnyPath", new Range(startLine, startChar, endLine, endChar), replacement);
+    return FixCalculator.calculateFix(
+        new Text(content.getBytes(UTF_8)), ImmutableList.of(fixReplacement));
+  }
+
+  @Test
+  public void lineNumberMustBePositive() {
+    assertThrows(
+        ResourceConflictException.class,
+        () -> calculateFixSingleReplacement("First line\nSecond line", 0, 0, 0, 0, "Abc"));
+  }
+
+  @Test
+  public void insertInTheEmptyContent() throws Exception {
+    FixResult fixResult = calculateFixSingleReplacement("", 1, 0, 1, 0, "Abc");
+    assertThat(fixResult).text().isEqualTo("Abc");
+    assertThat(fixResult)
+        .edits()
+        .onlyElement()
+        .isInsert(0, 0, 1)
+        .hasInternalEdits()
+        .onlyElement()
+        .isInsert(0, 0, 3);
+  }
+
+  @Test
+  public void insertAtTheStartOfSingleLineContentNoEOLMark() throws Exception {
+    FixResult fixResult = calculateFixSingleReplacement("First line", 1, 0, 1, 0, "Abc");
+    assertThat(fixResult).text().isEqualTo("AbcFirst line");
+    assertThat(fixResult)
+        .edits()
+        .onlyElement()
+        .isReplace(0, 1, 0, 1)
+        .hasInternalEdits()
+        .onlyElement()
+        .isInsert(0, 0, 3);
+  }
+
+  @Test
+  public void insertAtTheStartOfSingleLineContentHasEOLMark() throws Exception {
+    FixResult fixResult = calculateFixSingleReplacement("First line\n", 1, 0, 1, 0, "Abc");
+    assertThat(fixResult).text().isEqualTo("AbcFirst line\n");
+    assertThat(fixResult)
+        .edits()
+        .onlyElement()
+        .isReplace(0, 1, 0, 1)
+        .hasInternalEdits()
+        .onlyElement()
+        .isInsert(0, 0, 3);
+  }
+
+  @Test
+  public void insertInTheMiddleOfSingleLineContentNoEOLMark() throws Exception {
+    FixResult fixResult = calculateFixSingleReplacement("First line", 1, 5, 1, 5, "Abc");
+    assertThat(fixResult).text().isEqualTo("FirstAbc line");
+    assertThat(fixResult)
+        .edits()
+        .onlyElement()
+        .isReplace(0, 1, 0, 1)
+        .hasInternalEdits()
+        .onlyElement()
+        .isInsert(5, 5, 3);
+  }
+
+  @Test
+  public void insertInTheMiddleOfSingleLineContentHasEOLMark() throws Exception {
+    FixResult fixResult = calculateFixSingleReplacement("First line\n", 1, 5, 1, 5, "Abc");
+    assertThat(fixResult).text().isEqualTo("FirstAbc line\n");
+    assertThat(fixResult)
+        .edits()
+        .onlyElement()
+        .isReplace(0, 1, 0, 1)
+        .hasInternalEdits()
+        .onlyElement()
+        .isInsert(5, 5, 3);
+  }
+
+  @Test
+  public void insertAtTheEndOfSingleLineContentNoEOLMark() throws Exception {
+    FixResult fixResult = calculateFixSingleReplacement("First line", 1, 10, 1, 10, "Abc");
+    assertThat(fixResult).text().isEqualTo("First lineAbc");
+    assertThat(fixResult)
+        .edits()
+        .onlyElement()
+        .isReplace(0, 1, 0, 1)
+        .hasInternalEdits()
+        .onlyElement()
+        .isInsert(10, 10, 3);
+  }
+
+  @Test
+  public void insertAtTheEndOfSingleLineContentHasEOLMarkInvalidPosition() throws Exception {
+    assertThrows(
+        ResourceConflictException.class,
+        () -> calculateFixSingleReplacement("First line\n", 1, 11, 1, 11, "Abc"));
+  }
+
+  @Test
+  public void insertAtTheEndOfSingleLineContentHasEOLMark() throws Exception {
+    FixResult fixResult = calculateFixSingleReplacement("First line\n", 2, 0, 2, 0, "Abc");
+    assertThat(fixResult).text().isEqualTo("First line\nAbc");
+    assertThat(fixResult)
+        .edits()
+        .onlyElement()
+        .isInsert(1, 1, 1)
+        .hasInternalEdits()
+        .onlyElement()
+        .isInsert(0, 0, 3);
+  }
+}

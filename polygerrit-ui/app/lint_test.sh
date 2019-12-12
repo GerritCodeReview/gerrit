e3aaa350f4b3aaa7af65fa46cf0adc9ff87caac0
Tao Zhou <taoalpha@google.com>
2019-12-12 17:11:10 +0100

    fix linter errors under eslint 6.x
    Also changed the lint_test to only run within ui folder to speed it up.
    
    Change-Id: Ib65ae53176d7a6eece12bd85bcae3bb402d5ade8

--- polygerrit-ui/app/lint_test.sh
+++ polygerrit-ui/app/lint_test.sh
@@ -2,15 +2,15 @@
 
 set -ex
 
-eslint_bin=$(which npm)
-if [ -z "$eslint_bin" ]; then
+npm_bin=$(which npm) && true
+if [ -z "$npm_bin" ]; then
     echo "NPM must be on the path."
     exit 1
 fi
 
-eslint_bin=$(which eslint)
-eslint_config=$(npm list -g | grep -c eslint-config-google)
-eslint_plugin=$(npm list -g | grep -c eslint-plugin-html)
+eslint_bin=$(which eslint) && true
+eslint_config=$(npm list -g | grep -c eslint-config-google) && true
+eslint_plugin=$(npm list -g | grep -c eslint-plugin-html) && true
 if [ -z "$eslint_bin" ] || [ "$eslint_config" -eq "0" ] || [ "$eslint_plugin" -eq "0" ]; then
     echo "You must install ESLint and its dependencies from NPM."
     echo "> npm install -g eslint eslint-config-google eslint-plugin-html"
@@ -23,4 +23,4 @@ npm link eslint
 npm link eslint-config-google
 npm link eslint-plugin-html
 
-${eslint_bin} --ignore-pattern 'bower_components/' --ignore-pattern 'gr-linked-text' --ignore-pattern 'scripts/vendor' --ext .html,.js .
+${eslint_bin} -c ./polygerrit-ui/app/.eslintrc.json --ignore-pattern 'bower_components/' --ignore-pattern 'gr-linked-text' --ignore-pattern 'scripts/vendor' --ext .html,.js ./polygerrit-ui/app

626c6a2b9fddd4101324f489c5ca9cce2d5cae33
Paladox none <thomasmulhall410@yahoo.com>
2019-12-14 18:07:49 +0000

    Introduce new rest api for file uploads
    This will allow users to upload files using the rest api.
    
    Change-Id: Ifff6696692ce9feb71df349e0dea804ac71f3d7a

--- java/com/google/gerrit/server/restapi/change/ChangeEdits.java
+++ java/com/google/gerrit/server/restapi/change/ChangeEdits.java
@@ -14,7 +14,11 @@
 
 package com.google.gerrit.server.restapi.change;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.google.common.base.Strings;
+import com.google.common.io.CharStreams;
+import com.google.gerrit.common.RawInputUtil;
 import com.google.common.collect.ImmutableList;
 import com.google.gerrit.entities.Change;
 import com.google.gerrit.entities.PatchSet;
@@ -59,11 +63,15 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.util.Base64;
 import org.kohsuke.args4j.Option;
 
 @Singleton
@@ -291,6 +299,55 @@ public class ChangeEdits implements ChildCollection<ChangeResource, ChangeEditRe
         throws ResourceConflictException, AuthException, IOException, PermissionBackendException {
       if (Strings.isNullOrEmpty(path) || path.charAt(0) == '/') {
         throw new ResourceConflictException("Invalid path: " + path);
+      }
+
+      try (Repository repository = repositoryManager.openRepository(rsrc.getProject())) {
+        editModifier.modifyFile(repository, rsrc.getNotes(), path, newContent);
+      } catch (InvalidChangeOperationException e) {
+        throw new ResourceConflictException(e.getMessage());
+      }
+      return Response.none();
+    }
+  }
+
+  @Singleton
+  public static class FileUploads implements RestModifyView<ChangeEditResource, FileUploads.Input> {
+    public static class Input {
+      @DefaultInput public RawInput content;
+    }
+
+    private final ChangeEditModifier editModifier;
+    private final GitRepositoryManager repositoryManager;
+
+    @Inject
+    FileUploads(ChangeEditModifier editModifier, GitRepositoryManager repositoryManager) {
+      this.editModifier = editModifier;
+      this.repositoryManager = repositoryManager;
+    }
+
+    @Override
+    public Response<?> apply(ChangeEditResource rsrc, Input input)
+        throws AuthException, ResourceConflictException, IOException, PermissionBackendException {
+      return apply(rsrc.getChangeResource(), rsrc.getPath(), input.content);
+    }
+
+    public Response<?> apply(ChangeResource rsrc, String path, RawInput newContent)
+        throws ResourceConflictException, AuthException, IOException, PermissionBackendException {
+      if (Strings.isNullOrEmpty(path) || path.charAt(0) == '/') {
+        throw new ResourceConflictException("Invalid path: " + path);
+      }
+
+      String contentString =
+          CharStreams.toString(new InputStreamReader(newContent.getInputStream(), UTF_8));
+      Matcher m = Pattern.compile("data:([\\w/.-]+);([\\w]+),(.*)").matcher(contentString);
+      if (m.matches()) {
+        if ("base64".equals(m.group(2))) {
+          newContent = RawInputUtil.create(Base64.decode(m.group(3)));
+        } else {
+          throw new ResourceConflictException("file must be encoded with base64");
+        }
+      } else {
+        throw new ResourceConflictException("file must be encoded with base64");
       }
 
       try (Repository repository = repositoryManager.openRepository(rsrc.getProject())) {

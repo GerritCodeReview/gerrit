5ea5fc3b0f83874e39db7e39c73b96188b286958
Tao Zhou <taoalpha@google.com>
2019-12-16 10:53:52 +0100

    Add deprecated message to lint_test.sh and `lint_test` rule
    Change-Id: If59b75852c2dcc7f040057f623de5062e94e6a67

--- polygerrit-ui/app/lint_test.sh
+++ polygerrit-ui/app/lint_test.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# DEPRECATED: This file is only used by Gerrit CI for now
+# To run eslint test on FE code, run `npm run eslint` instead.
+# `bazel run lint_test` will be changed to be an alias to
+# `npm run eslint` soon.
+
 set -ex
+
+echo "DEPRECATED: please run `npm run eslint` instead.";
 
 npm_bin=$(which npm) && true
 if [ -z "$npm_bin" ]; then

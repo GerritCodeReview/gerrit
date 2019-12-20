b62f28d586fa833b44da2aa1c7c7826a20d6ff29
mildis <me@mildis.org>
2019-12-20 17:51:01 +0100

    Makes the generated HTTP password length configurable.
    Adds a new auth.httpPasswordLength configuration setting with
    enforced limits of 16<length<72. The lower boundary is arbitrarily
    choosen. The upper boundary is based on BouncyCastle's BCrypt
    generate() which takes up to 72 bytes.
    
    Change-Id: Idbcc5745f84cdbe931a9b9a67adda3e7569b8608

---
+++ javatests/com/google/gerrit/server/config/AuthConfigTest.java
@@ -0,0 +1,67 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.config;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
+
+import org.eclipse.jgit.lib.Config;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuthConfigTest {
+
+  private Config cfg;
+  private AuthConfig authCfg;
+
+  @Before
+  public void setUp() throws Exception {
+    cfg = new Config();
+    authCfg = new AuthConfig(cfg);
+  }
+
+  @Test
+  public void defaultHttpPasswordLengthWhenNotConfigured() {
+    assertThat(authCfg.getHttpPasswordLength()).isEqualTo(AuthConfig.DEFAULT_HTTP_PASSWORD_LENGTH);
+  }
+
+  @Test
+  public void exceptionWhenHttpPasswordLengthTooLow() {
+    cfg = new Config();
+    configureHttpPasswordLength(15);
+    try {
+      authCfg = new AuthConfig(cfg);
+      fail("AuthConfig instanciation should throw");
+    } catch (IllegalStateException e) {
+      assertThat(e).hasMessage("must be greater");
+    }
+  }
+
+  @Test
+  public void exceptionWhenHttpPasswordLengthTooHigh() {
+    cfg = new Config();
+    configureHttpPasswordLength(73);
+    try {
+      authCfg = new AuthConfig(cfg);
+      fail("AuthConfig instanciation should throw");
+    } catch (IllegalStateException e) {
+      assertThat(e).hasMessage("must be greater");
+    }
+  }
+
+  private void configureHttpPasswordLength(int httpPasswordLength) {
+    cfg.setString(AuthConfig.SECTION_NAME, null, "httpPasswordLength", httpPasswordLength);
+  }
+}

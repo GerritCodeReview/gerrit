7c6a06d2ae5b141a77b1940808a51cb34b3d126a
Edwin Kempin <ekempin@google.com>
2019-12-30 16:03:16 +0100

    Document NoteDbMetrics class
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Iccd024ea8acb583fabf9aec1699e53952c771657

--- java/com/google/gerrit/server/notedb/NoteDbMetrics.java
+++ java/com/google/gerrit/server/notedb/NoteDbMetrics.java
@@ -21,6 +21,9 @@ import com.google.gerrit.metrics.Timer0;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+/**
+ * Metrics for accessing and updating changes in NoteDb.
+ */
 @Singleton
 class NoteDbMetrics {
   /** End-to-end latency for writing a collection of updates. */

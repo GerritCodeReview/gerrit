cdb5010a98dd485725bd748060d786d651399076
Saša Živkov <sasa.zivkov@sap.com>
2020-01-17 11:12:31 +0100

    Set HTTP thread name to describe the request it is processing
    The HTTP worker threads had generic names: HTTP-100, HTTP-101, ...
    While we could conclude from the stack trace what this thread was doing
    we missed some important information like repository name, user name,
    etc...
    
    With this change the HTTP threads get descriptive names. For example, if
    there is an ongoing git-fetch operation we would see a thread named
    like:
    
    "HTTP POST /a/myProject/git-upload-pack (johndoe from 10.87.75.169)"
    
    This can be of a great help for Gerrit admins trying to figure out who
    is executing that many git-fetch requests and affecting the overall
    performance.  Note that we cannot reliably tell that from the httpd_log
    as requests are logged only once they are finished. The show-queue
    command also doesn't show tasks being processing in the HTTP thread
    pool.
    
    As additional justification, SSH thread names are already named with a
    similar pattern i.e:
    
    "SSH git-upload-pack /myProject (johndoe)"
    
    Change-Id: I98cf112018732e5d4075568e7da8614ffe0495a4

--- java/com/google/gerrit/pgm/Daemon.java
+++ java/com/google/gerrit/pgm/Daemon.java
@@ -37,6 +37,7 @@ import com.google.gerrit.httpd.RequestCleanupFilter;
 import com.google.gerrit.httpd.RequestContextFilter;
 import com.google.gerrit.httpd.RequestMetricsFilter;
 import com.google.gerrit.httpd.RequireSslFilter;
+import com.google.gerrit.httpd.SetThreadNameFilter;
 import com.google.gerrit.httpd.WebModule;
 import com.google.gerrit.httpd.WebSshGlueModule;
 import com.google.gerrit.httpd.auth.oauth.OAuthModule;
@@ -590,6 +591,7 @@ public class Daemon extends SiteProgram {
     }
     modules.add(RequestCleanupFilter.module());
     modules.add(AllRequestFilter.module());
+    modules.add(SetThreadNameFilter.module());
     modules.add(sysInjector.getInstance(WebModule.class));
     modules.add(sysInjector.getInstance(RequireSslFilter.Module.class));
     modules.add(new HttpPluginModule());

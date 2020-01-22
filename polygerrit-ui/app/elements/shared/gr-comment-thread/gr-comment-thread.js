d89fede86421644986a40e456dc6f6837fc9d574
Dmitrii Filippov <dmfilippov@google.com>
2020-01-22 13:58:36 +0100

    Fix missing comments in diff-view
    If there is a comment with an invalid line-number, diff view
    doesn't display some comments. The change:
    1) Fixes missing comments in diff-view
    2) Shows warning for comments that were posted on invalid line
    
    Bug:Issue 12214
    Change-Id: I7c9d62b76edcdaf368e9232484586dcc2ce0f666

--- polygerrit-ui/app/elements/shared/gr-comment-thread/gr-comment-thread.js
+++ polygerrit-ui/app/elements/shared/gr-comment-thread/gr-comment-thread.js
@@ -124,6 +124,11 @@
           type: Boolean,
           value: false,
         },
+        /** It is possible to add comment to non-existing line via API */
+        invalidLineNumber: {
+          type: Number,
+          reflectToAttribute: true,
+        },
         /** Necessary only if showFilePath is true or when used with gr-diff */
         lineNum: {
           type: Number,

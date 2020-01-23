441b3bfa89eea6a9848ea983a0fc22816a833e6b
Dmitrii Filippov <dmfilippov@google.com>
2020-01-23 18:14:51 +0100

    Use node modules in wct tests
    Update wct tests to use node modules instead of bower components
    
    Change-Id: I1533279c9bc92b56b2de29070e6b2cc2137e7b06

--- polygerrit-ui/app/polylint_test.sh
+++ polygerrit-ui/app/polylint_test.sh
@@ -2,30 +2,12 @@
 
 set -ex
 
-npm_bin=$(which npm)
-if [[ -z "$npm_bin" ]]; then
-    echo "NPM must be on the path."
-    exit 1
-fi
-
-node_bin=$(which node)
-if [[ -z "$node_bin" ]]; then
-    echo "node must be on the path."
-    exit 1
-fi
-
-polymer_bin=$(which polymer)
-if [[ -z "$polymer_bin" ]]; then
-  polymer_bin=$(abs_path ./node_modules/polymer-cli/bin/polymer.js);
-fi
-if [[ -z "$polymer_bin" ]]; then
-    echo "polymer must be set or polymer-cli locally installed (npm install polymer-cli)."
-    exit 1
-fi
-
-unzip -o polygerrit-ui/polygerrit_components.bower_components.zip -d polygerrit-ui/app
+DIR=$(pwd)
+ln -s $RUNFILES_DIR/ui_npm/node_modules $TEST_TMPDIR/node_modules
+cp $2 $TEST_TMPDIR/polymer.json
+cp -R -L polygerrit-ui/app/polylint-updated-links/polygerrit-ui/app/* $TEST_TMPDIR
 
 #Can't use --root with polymer.json - see https://github.com/Polymer/tools/issues/2616
 #Change current directory to the root folder
-cd polygerrit-ui/app
-$polymer_bin lint --component-dir 'bower_components' --verbose
+cd $TEST_TMPDIR/
+$DIR/$1 lint --verbose

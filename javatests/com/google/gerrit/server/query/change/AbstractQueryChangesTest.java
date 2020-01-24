464f4486411fb47d46d41477d7ab0e259fa5eb1d
Hamza Kaced <hamza.kaced@ericsson.com>
2020-01-24 12:59:09 -0500

    Enable DEBUG level for elasticsearch tests
    Set log4j rootLogger log level to DEBUG before running elasticsearch
    tests. Enable rootLogger default INFO log level after tests conclude.
    
    Bug: Issue 10498
    Change-Id: I75492de1c5ef8721ae745266e36b8cbfb0b67682

--- javatests/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
+++ javatests/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
@@ -139,6 +139,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
 import org.eclipse.jgit.junit.TestRepository;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
@@ -150,7 +153,9 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.util.SystemReader;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -213,6 +218,16 @@ public abstract class AbstractQueryChangesTest extends GerritServerTests {
           + "OR cc:${user})";
 
   protected abstract Injector createInjector();
+
+  @BeforeClass
+  public static void setLoggerStartLevel() {
+  LogManager.getRootLogger().setLevel(Level.DEBUG);
+  }
+
+  @AfterClass
+  public static void setLoggerEndLevel() {
+  LogManager.getRootLogger().setLevel(Level.INFO);
+  }
 
   @Before
   public void setUpInjector() throws Exception {

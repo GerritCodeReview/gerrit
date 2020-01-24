d6ff9a57212b860cb3fb3fd90416ecf097880ef5
Hamza Kaced <hamza.kaced@ericsson.com>
2020-01-24 12:59:09 -0500

    Enable DEBUG level for elasticsearch tests
    Set log4j rootLogger log level to DEBUG before running elasticsearch
    tests. Enable rootLogger default INFO log level after tests conclude.
    
    Bug: Issue 10498
    Change-Id: I75492de1c5ef8721ae745266e36b8cbfb0b67682

--- javatests/com/google/gerrit/server/query/project/AbstractQueryProjectsTest.java
+++ javatests/com/google/gerrit/server/query/project/AbstractQueryProjectsTest.java
@@ -68,8 +68,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -110,6 +114,16 @@ public abstract class AbstractQueryProjectsTest extends GerritServerTests {
   protected CurrentUser user;
 
   protected abstract Injector createInjector();
+
+  @BeforeClass
+  public static void setLoggerStartLevel() {
+    LogManager.getRootLogger().setLevel(Level.DEBUG);
+  }
+
+  @AfterClass
+  public static void setLoggerEndLevel() {
+    LogManager.getRootLogger().setLevel(Level.INFO);
+  }
 
   @Before
   public void setUpInjector() throws Exception {

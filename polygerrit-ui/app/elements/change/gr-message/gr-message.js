cda910b76e59687cf0dd9a234527fdcb559ed066
Ben Rohlfs <brohlfs@google.com>
2020-01-24 20:16:52 +0100

    Redesign Change Log
    Screenshots:
    https://imgur.com/a/2JrG9mh
    
    Render file name in gr-comment-list as a link instead of a bold font.
    And drop the 1px border for each file section. And reduce the padding.
    
    Do not render the avatar differently in collapsed/expanded messages.
    That was breaking the layout. Always use the small avatar.
    
    Add a min-width for the author, the label and the comment count. This
    makes the Change Log a little more tabular and easier to scan and read.
    
    Parse the change message and eliminate the "Patch Set X:" and
    "(Y comments)" strings. Instead render the patch set number to the far
    right next to the date. And render the number of comments in a
    dedicated div with a nice icon.
    
    In expanded messages eliminate wasted vertical height and padding. For
    example only show a "REPLY" button when there is something to reply to.
    
    When some messages are expanded and other are not, then it is often hard
    to see at a glance which content belongs to which message. To address
    that introduce alternating background colors, but only for expanded
    messages.
    
    Change-Id: I4ea61d972d6357e04f2f8d8c636ddefbc0b1b17b

--- polygerrit-ui/app/elements/change/gr-message/gr-message.js
+++ polygerrit-ui/app/elements/change/gr-message/gr-message.js
@@ -69,10 +69,6 @@
           type: Boolean,
           computed: '_computeIsAutomated(message)',
         },
-        showAvatar: {
-          type: Boolean,
-          computed: '_computeShowAvatar(author, config)',
-        },
         showOnBehalfOf: {
           type: Boolean,
           computed: '_computeShowOnBehalfOf(message)',
@@ -100,6 +96,14 @@
         _expanded: {
           type: Object,
           computed: '_computeExpanded(message.expanded)',
+        },
+        _messageContent: {
+          type: String,
+          computed: '_computeMessageContent(message.message)',
+        },
+        _numberOfComments: {
+          type: Number,
+          computed: '_computeNumberOfComments(comments)',
         },
         _loggedIn: {
           type: Boolean,
@@ -140,12 +144,43 @@
       }
     }
 
-    _computeAuthor(message) {
-      return message.author || message.updated_by;
+    _computeNumberOfComments(comments) {
+      if (!comments) return undefined;
+      let count = 0;
+      for (const file in comments) {
+        if (comments.hasOwnProperty(file)) {
+          const commentArray = comments[file] || [];
+          count += commentArray.length;
+        }
+      }
+      if (count === 0) {
+        return undefined;
+      } else if (count === 1) {
+        return '1 comment';
+      } else {
+        return count + ' comments';
+      }
     }
 
-    _computeShowAvatar(author, config) {
-      return !!(author && config && config.plugin && config.plugin.has_avatars);
+    _computeMessageContent(content) {
+      if (!content) return '';
+      const lines = content.split('\n');
+      const newLines = [];
+      lines.forEach(line => {
+        if (line.startsWith('Patch Set ')) return;
+        if (line.startsWith('(') && line.endsWith(' comment)')) return;
+        if (line.startsWith('(') && line.endsWith(' comments)')) return;
+        newLines.push(line);
+      });
+      return newLines.join('\n').trim();
+    }
+
+    _isMessageContentEmpty(content) {
+      return this._computeMessageContent(content).trim().length === 0;
+    }
+
+    _computeAuthor(message) {
+      return message.author || message.updated_by;
     }
 
     _computeShowOnBehalfOf(message) {
@@ -236,10 +271,9 @@
       return classes.join(' ');
     }
 
-    _computeClass(expanded, showAvatar, message) {
+    _computeClass(expanded) {
       const classes = [];
       classes.push(expanded ? 'expanded' : 'collapsed');
-      classes.push(showAvatar ? 'showAvatar' : 'hideAvatar');
       return classes.join(' ');
     }
 

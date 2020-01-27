eeab14986fe0c4034c36fc39410160ef48b35c5b
Thomas Draebing <thomas.draebing@sap.com>
2020-01-27 17:55:55 +0100

    Add UI element to display messages of the day
    Change-Id: I2cbbcbcd49734b645701b9fac62a016a269d37f2

---
+++ polygerrit-ui/app/elements/core/gr-message-header/gr-message-header.js
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'gr-message-header',
+
+    properties: {
+      message: {
+        type: Object,
+        reflectToAttribute: true,
+      },
+      _hidden: {
+        type: Boolean,
+        value: true,
+      },
+    },
+
+    attached() {
+      this._isHidden();
+      this.$.message.innerHTML = this.message.html;
+    },
+
+    _handleDismissMessage() {
+      document.cookie =
+        `msg-${this.message.id}=1; expires=${this.message.redisplay}`;
+      this._hidden = true;
+    },
+
+    _isHidden() {
+      if (!this.message) {
+        return;
+      }
+      this._hidden = window.util.getCookie(`msg-${this.message.id}`) === '1';
+    },
+  });
+})();

c77032cc74b2d766cb8eb69b65a205dd0d8a5887
Thomas Draebing <thomas.draebing@sap.com>
2020-01-27 11:20:28 +0100

    Add MessageOfTheDay-entries to ServerInfo
    The message of the day functionality was lost in the new PolyGerrit-UI,
    since the original implementation relied on the server side addition of
    the messages to the host page.
    
    This change adds the messages registered to the MessageOfTheDay-
    extension point to the ServerInfo, which can be fetched via Rest API.
    This allows accessing this information from the new UI.
    
    The UI elements that consume this information will be added in a
    followup change.
    
    Change-Id: I1deff5afbd812a1efaf6ad11e871544b36a5d88d

--- java/com/google/gerrit/extensions/common/MessageOfTheDayInfo.java
+++ java/com/google/gerrit/extensions/common/MessageOfTheDayInfo.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 The Android Open Source Project
+// Copyright (C) 2020 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,22 +14,14 @@
 
 package com.google.gerrit.extensions.common;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Date;
 
-public class ServerInfo {
-  public AccountsInfo accounts;
-  public AuthInfo auth;
-  public ChangeConfigInfo change;
-  public DownloadInfo download;
-  public GerritInfo gerrit;
-  public List<MessageOfTheDayInfo> messages;
-  public Boolean noteDbEnabled;
-  public PluginConfigInfo plugin;
-  public SshdInfo sshd;
-  public SuggestInfo suggest;
-  public Map<String, String> urlAliases;
-  public UserConfigInfo user;
-  public ReceiveInfo receive;
-  public String defaultTheme;
+/** REST API representation of a "message of the day". */
+public class MessageOfTheDayInfo {
+  /** The ID of the message. */
+  public String id;
+  /** The date and time the message will be displayed again after being dismissed by the user. */
+  public Date redisplay;
+  /** The message in HTML-format. */
+  public String html;
 }

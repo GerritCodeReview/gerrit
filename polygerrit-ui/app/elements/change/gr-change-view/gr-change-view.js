65ea21d05b88f90a99e3e639e25784799ce273a6
Dhruv Srivastava <dhruvsri@google.com>
2020-01-27 20:25:21 +0100

    Add a comment tab for robot comments
    Change-Id: Ic720a7e153cfb0c7648a438185644ae66584666a

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -62,6 +62,7 @@
   const CommentTabs = {
     CHANGE_LOG: 0,
     COMMENT_THREADS: 1,
+    ROBOT_COMMENTS: 2,
   };
 
   const CHANGE_DATA_TIMING_LABEL = 'ChangeDataLoaded';
@@ -137,6 +138,11 @@
           computed: '_computeDiffPrefsDisabled(disableDiffPrefs, _loggedIn)',
         },
         _commentThreads: Array,
+        _robotCommentThreads: {
+          type: Array,
+          computed: '_computeRobotCommentThreads(_commentThreads,'
+            + ' _currentRevision)',
+        },
         /** @type {?} */
         _serverConfig: {
           type: Object,
@@ -476,6 +482,10 @@
       return currentView === view;
     }
 
+    _computeShowRobotComments(currentView) {
+      return currentView === CommentTabs.ROBOT_COMMENTS;
+    }
+
     _handleFileTabChange(e) {
       const selectedIndex = this.shadowRoot
           .querySelector('#primaryTabs').selected;
@@ -573,6 +583,15 @@
       }
 
       return false;
+    }
+
+    _computeRobotCommentThreads(commentThreads, currentRevision) {
+      if (!commentThreads || !currentRevision) return;
+      return commentThreads.filter(thread => {
+        const comments = thread.comments || [];
+        return comments.length && comments[0].robot_id && (comments[0].patch_set
+          === currentRevision._number);
+      });
     }
 
     _handleReloadCommentThreads() {

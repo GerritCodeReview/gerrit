5e75d554be5fb75e34297740b3f4c1b5a553a549
Paladox none <thomasmulhall410@yahoo.com>
2020-02-02 01:40:48 +0000

    Replace deprecated OptionParser with ArgumentParser
    Change-Id: I8dbee8967edd9cfe1e2f9e2df97d786a2bd1ca94

--- tools/maven/mvn.py
+++ tools/maven/mvn.py
@@ -14,21 +14,21 @@
 # limitations under the License.
 
 from __future__ import print_function
-from optparse import OptionParser
+import argparse
 from os import path, environ
 from subprocess import check_output, CalledProcessError
 from sys import stderr
 
-opts = OptionParser()
-opts.add_option('--repository', help='maven repository id')
-opts.add_option('--url', help='maven repository url')
-opts.add_option('-o')
-opts.add_option('-a', help='action (valid actions are: install,deploy)')
-opts.add_option('-v', help='gerrit version')
-opts.add_option('-s', action='append', help='triplet of artifactId:type:path')
+opts = argparse.ArgumentParser()
+opts.add_argument('--repository', help='maven repository id')
+opts.add_argument('--url', help='maven repository url')
+opts.add_argument('-o')
+opts.add_argument('-a', help='action (valid actions are: install,deploy)')
+opts.add_argument('-v', help='gerrit version')
+opts.add_argument('-s', action='append', help='triplet of artifactId:type:path')
 
-args, ctx = opts.parse_args()
-if not args.v:
+args = vars(opts.parse_args())
+if not args['v']:
     print('version is empty', file=stderr)
     exit(1)
 
@@ -36,25 +36,25 @@ root = path.abspath(__file__)
 while not path.exists(path.join(root, 'WORKSPACE')):
     root = path.dirname(root)
 
-if 'install' == args.a:
+if 'install' == args['a']:
     cmd = [
         'mvn',
         'install:install-file',
-        '-Dversion=%s' % args.v,
+        '-Dversion=%s' % args['v'],
     ]
-elif 'deploy' == args.a:
+elif 'deploy' == args['a']:
     cmd = [
         'mvn',
         'gpg:sign-and-deploy-file',
-        '-Dversion=%s' % args.v,
-        '-DrepositoryId=%s' % args.repository,
-        '-Durl=%s' % args.url,
+        '-Dversion=%s' % args['v'],
+        '-DrepositoryId=%s' % args['repository'],
+        '-Durl=%s' % args['url'],
     ]
 else:
-    print("unknown action -a %s" % args.a, file=stderr)
+    print("unknown action -a %s" % args['a'], file=stderr)
     exit(1)
 
-for spec in args.s:
+for spec in args['s']:
     artifact, packaging_type, src = spec.split(':')
     exe = cmd + [
         '-DpomFile=%s' % path.join(root, 'tools', 'maven',
@@ -67,7 +67,7 @@ for spec in args.s:
             print(' '.join(exe), file=stderr)
         check_output(exe)
     except Exception as e:
-        print('%s command failed: %s\n%s' % (args.a, ' '.join(exe), e),
+        print('%s command failed: %s\n%s' % (args['a'], ' '.join(exe), e),
               file=stderr)
         if environ.get('VERBOSE') and isinstance(e, CalledProcessError):
             print('Command output\n%s' % e.output, file=stderr)
@@ -75,12 +75,12 @@ for spec in args.s:
 
 
 out = stderr
-if args.o:
-    out = open(args.o, 'w')
+if args['o']:
+    out = open(args['o'], 'w')
 
 with out as fd:
-    if args.repository:
-        print('Repository: %s' % args.repository, file=fd)
-    if args.url:
-        print('URL: %s' % args.url, file=fd)
-    print('Version: %s' % args.v, file=fd)
+    if args['repository']:
+        print('Repository: %s' % args['repository'], file=fd)
+    if args['url']:
+        print('URL: %s' % args['url'], file=fd)
+    print('Version: %s' % args['v'], file=fd)

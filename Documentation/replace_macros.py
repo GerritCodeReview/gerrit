d1435216dc603b94b251846e4995d239b4ba9877
Paladox none <thomasmulhall410@yahoo.com>
2020-02-02 01:40:48 +0000

    Replace deprecated OptionParser with ArgumentParser
    Change-Id: I8dbee8967edd9cfe1e2f9e2df97d786a2bd1ca94

--- Documentation/replace_macros.py
+++ Documentation/replace_macros.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from optparse import OptionParser
+import argparse
 import re
 import sys
 
@@ -219,23 +219,23 @@ LINK_SCRIPT = """
 
 """
 
-opts = OptionParser()
-opts.add_option('-o', '--out', help='output file')
-opts.add_option('-s', '--src', help='source file')
-opts.add_option('-x', '--suffix', help='suffix for included filenames')
-opts.add_option('-b', '--searchbox', action="store_true", default=True,
+opts = argparse.ArgumentParser()
+opts.add_argument('-o', '--out', help='output file')
+opts.add_argument('-s', '--src', help='source file')
+opts.add_argument('-x', '--suffix', help='suffix for included filenames')
+opts.add_argument('-b', '--searchbox', action="store_true", default=True,
                 help="generate the search boxes")
-opts.add_option('--no-searchbox', action="store_false", dest='searchbox',
+opts.add_argument('--no-searchbox', action="store_false", dest='searchbox',
                 help="don't generate the search boxes")
-options, _ = opts.parse_args()
+options = vars(opts.parse_args())
 
 try:
     try:
-        out_file = open(options.out, 'w', errors='ignore')
-        src_file = open(options.src, 'r', errors='ignore')
+        out_file = open(options['out'], 'w', errors='ignore')
+        src_file = open(options['src'], 'r', errors='ignore')
     except TypeError:
-        out_file = open(options.out, 'w')
-        src_file = open(options.src, 'r')
+        out_file = open(options['out'], 'w')
+        src_file = open(options['src'], 'r')
     last_line = ''
     ignore_next_line = False
     last_title = ''
@@ -246,14 +246,14 @@ try:
             last_line = ''
         elif PAT_SEARCHBOX.match(last_line):
             # Case of 'SEARCHBOX\n---------'
-            if options.searchbox:
+            if options['searchbox']:
                 out_file.write(SEARCH_BOX)
             last_line = ''
         elif PAT_INCLUDE.match(line):
             # Case of 'include::<filename>'
             match = PAT_INCLUDE.match(line)
             out_file.write(last_line)
-            last_line = match.group(1) + options.suffix + match.group(2) + '\n'
+            last_line = match.group(1) + options['suffix'] + match.group(2) + '\n'
         elif PAT_STARS.match(line):
             if PAT_TITLE.match(last_line):
                 # Case of the title in '.<title>\n****\nget::<url>\n****'
@@ -279,5 +279,5 @@ try:
     out_file.close()
 except IOError as err:
     sys.stderr.write(
-        "error while expanding %s to %s: %s" % (options.src, options.out, err))
+        "error while expanding %s to %s: %s" % (options['src'], options['out'], err))
     exit(1)

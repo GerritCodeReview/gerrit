0e29b5fc20f84bbaf0057847bb5de2256cbe00a1
Dhruv Srivastava <dhruvsri@google.com>
2020-02-04 14:52:12 +0100

    Test change - Please Ignore
    Change-Id: Iad9634eba202845452fae5d8aacfa1da7de8d872

--- polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.js
+++ polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.js
@@ -11,7 +11,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions an
  * limitations under the License.
  */
 import '../../admin/gr-create-change-dialog/gr-create-change-dialog.js';
@@ -116,7 +116,7 @@ const ActionLoadingLabels = {
   rebase: 'Rebasing...',
   restore: 'Restoring...',
   revert: 'Reverting...',
-  revert_submission: 'Reverting Submission...',
+  revert_submission: 'Reverting Submission....',
   submit: 'Submitting...',
 };
 

e1c6377bde23c0c3ebd839a39591f1fd0e6786f5
Milutin Kristofic <milutin@google.com>
2020-02-05 22:17:21 +0100

    Set IndexHtmlUtil methods as public
    These methods can be access by custom servlet implementation.
    
    Change-Id: I9a6211065ae4733245bac6481ef3a6a5749c79b0

--- java/com/google/gerrit/httpd/raw/IndexHtmlUtil.java
+++ java/com/google/gerrit/httpd/raw/IndexHtmlUtil.java
@@ -47,13 +47,18 @@ import java.util.regex.Pattern;
 /** Helper for generating parts of {@code index.html}. */
 public class IndexHtmlUtil {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-  static final String changeCanonicalUrl = ".*/c/(?<project>.+)/\\+/(?<changeNum>\\d+)";
-  static final String basePatchNumUrlPart = "(/(-?\\d+|edit)(\\.\\.(\\d+|edit))?)";
-  static final Pattern changeUrlPattern =
+  public static final String changeCanonicalUrl = ".*/c/(?<project>.+)/\\+/(?<changeNum>\\d+)";
+  public static final String basePatchNumUrlPart = "(/(-?\\d+|edit)(\\.\\.(\\d+|edit))?)";
+
+  @UsedAt(Project.GOOGLE)
+  public static final Pattern changeUrlPattern =
       Pattern.compile(changeCanonicalUrl + basePatchNumUrlPart + "?" + "/?$");
-  static final Pattern diffUrlPattern =
+
+  @UsedAt(Project.GOOGLE)
+  public static final Pattern diffUrlPattern =
       Pattern.compile(changeCanonicalUrl + basePatchNumUrlPart + "(/(.+))" + "/?$");
 
+  @UsedAt(Project.GOOGLE)
   public static String getDefaultChangeDetailHex() {
     Set<ListChangesOption> options =
         ImmutableSet.of(
@@ -70,6 +75,7 @@ public class IndexHtmlUtil {
     return ListOption.toHex(options);
   }
 
+  @UsedAt(Project.GOOGLE)
   public static String getDefaultDiffDetailHex() {
     Set<ListChangesOption> options =
         ImmutableSet.of(
@@ -78,6 +84,19 @@ public class IndexHtmlUtil {
             ListChangesOption.SKIP_DIFFSTAT);
 
     return ListOption.toHex(options);
+  }
+
+  @UsedAt(Project.GOOGLE)
+  public static String computeChangeRequestsPath(String requestedURL, Pattern pattern) {
+    Matcher matcher = pattern.matcher(requestedURL);
+    if (matcher.matches()) {
+      Integer changeId = Ints.tryParse(matcher.group("changeNum"));
+      if (changeId != null) {
+        return "changes/" + Url.encode(matcher.group("project")) + "~" + changeId;
+      }
+    }
+
+    return null;
   }
 
   /**
@@ -199,18 +218,6 @@ public class IndexHtmlUtil {
     }
 
     return data.build();
-  }
-
-  static String computeChangeRequestsPath(String requestedURL, Pattern pattern) {
-    Matcher matcher = pattern.matcher(requestedURL);
-    if (matcher.matches()) {
-      Integer changeId = Ints.tryParse(matcher.group("changeNum"));
-      if (changeId != null) {
-        return "changes/" + Url.encode(matcher.group("project")) + "~" + changeId;
-      }
-    }
-
-    return null;
   }
 
   private static String computeCanonicalPath(@Nullable String canonicalURL)

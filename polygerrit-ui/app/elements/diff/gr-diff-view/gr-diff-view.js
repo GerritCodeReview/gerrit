8bcf09c26ed2a59e4ffd18173a86c40e8ea8bb75
Paladox none <thomasmulhall410@yahoo.com>
2020-02-05 14:52:03 +0000

    Add "edit" button to diff view
    Bug: Issue 11493
    Change-Id: I9825c01ed0c94e29e440be6bbbeabd1fc824c8cd

--- polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.js
+++ polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.js
@@ -604,6 +604,12 @@
       return this._getDiffUrl(this._change, this._patchRange, newPath.path);
     }
 
+    _computeEditURL(change, patchRange, path) {
+      if (!change || !patchRange || !path) { return; }
+      return Gerrit.Nav.getEditUrlForDiff(
+          change, path, patchRange.patchNum);
+    }
+
     /**
      * Gives an object representing the target of navigating either left or
      * right through the change. The resulting object will have one of the
@@ -1217,6 +1223,10 @@
 
     _onChangeHeaderPanelHeightChanged(e) {
       this._scrollTopMargin = e.detail.value;
+    }
+
+    _computeIsLoggedIn(loggedIn) {
+      return loggedIn ? true : false;
     }
   }
 

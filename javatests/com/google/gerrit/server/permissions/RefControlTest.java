499950f82fd89e237ae345621f36c55e9e123d86
Guillaume Chauvel <guillaume.chauvel@gmail.com>
2020-02-09 18:46:32 +0100

    Fix refPattern when using username pattern with regexp
    Bug: Issue 3340
    Change-Id: Iedf3022f6ce3068f7e9534f224cf05bb99b0f970

--- javatests/com/google/gerrit/server/permissions/RefControlTest.java
+++ javatests/com/google/gerrit/server/permissions/RefControlTest.java
@@ -477,6 +477,20 @@ public class RefControlTest {
   }
 
   @Test
+  public void usernamePatternRegExpCanUpdate() throws Exception {
+    projectOperations
+        .project(localKey)
+        .forUpdate()
+        .add(
+            allow(PUSH)
+                .ref("^refs/heads/users/${username}/(public|private)/.+")
+                .group(REGISTERED_USERS))
+        .update();
+    ProjectControl u = user(localKey, "a-registered-user");
+    assertCanUpdate("refs/heads/users/a-registered-user/private/a", u);
+  }
+
+  @Test
   public void usernamePatternNonRegex() throws Exception {
     projectOperations
         .project(localKey)
@@ -1127,6 +1141,7 @@ public class RefControlTest {
     RefPattern.validate("^refs/heads/*");
     RefPattern.validate("^refs/tags/[0-9a-zA-Z-_.]+");
     RefPattern.validate("refs/heads/review/${username}/*");
+    RefPattern.validate("^refs/heads/review/${username}/.+");
   }
 
   @Test

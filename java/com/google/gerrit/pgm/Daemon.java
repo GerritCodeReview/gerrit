80d9b89436feb405d4818f6ebe4972bdca0de319
Dmitrii Filippov <dmfilippov@google.com>
2020-02-11 17:12:14 +0100

    Allow use server.go for testing against local site
    Gerrit had 2 different ways to serve files locally:
    - start server.go web server
    - start gerrit binary with --polygerrit-dev flag
    
    Both implementations did the same handling for local files.
    
    This change removes --polygerrit-dev flag and related java
    implementation and instead a new option --dev-cdn is added.
    This option setup cdn for static content.
    
    Change-Id: I40d4c6c048de018bc6d856c889d7ec59592282ed

--- java/com/google/gerrit/pgm/Daemon.java
+++ java/com/google/gerrit/pgm/Daemon.java
@@ -168,8 +168,12 @@ public class Daemon extends SiteProgram {
   @Option(name = "--headless", usage = "Don't start the UI frontend")
   private boolean headless;
 
-  @Option(name = "--polygerrit-dev", usage = "Force PolyGerrit UI for development")
-  private boolean polyGerritDev;
+  private String devCdn = "";
+
+  @Option(name = "--dev-cdn", usage = "Use specified cdn for serving static content.")
+  private void setDevCdn(String cdn) {
+    devCdn = cdn;
+  }
 
   @Option(
       name = "--init",
@@ -463,8 +467,7 @@ public class Daemon extends SiteProgram {
         new AbstractModule() {
           @Override
           protected void configure() {
-            bind(GerritOptions.class)
-                .toInstance(new GerritOptions(headless, replica, polyGerritDev));
+            bind(GerritOptions.class).toInstance(new GerritOptions(headless, replica, devCdn));
             if (inMemoryTest) {
               bind(String.class)
                   .annotatedWith(SecureStoreClassName.class)

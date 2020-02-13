d01f3a4b97aa3993e375d4a020f3df872138b77c
Dhruv Srivastava <dhruvsri@google.com>
2020-02-13 19:15:20 +0100

    Filter revert submission button from change actions
    Revert button allows user to choose between revert submission, changes.
    No need to display the revert submission button now.
    
    Change-Id: Ib339c1ef27bf51c1aa152dd54f34752065a2edc1

--- polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.js
+++ polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.js
@@ -197,6 +197,12 @@
     REVERT_SUBMISSION: 2,
   };
 
+  /* Revert submission is skipped as the normal revert dialog will now show
+  the user a choice between reverting single change or an entire submission.
+  Hence, a second button is not needed.
+  */
+  const SKIP_ACTIONS = [ChangeActions.REVERT_SUBMISSION];
+
   /**
    * @appliesMixin Gerrit.FireMixin
    * @appliesMixin Gerrit.PatchSetMixin
@@ -1470,7 +1476,8 @@
               action.icon = action.__key;
             }
             return action;
-          });
+          })
+          .filter(action => !this._shouldSkipAction(action.__key));
     }
 
     _getActionPriority(action) {
@@ -1506,6 +1513,10 @@
       } else {
         return priorityDelta;
       }
+    }
+
+    _shouldSkipAction(key) {
+      return SKIP_ACTIONS.includes(key);
     }
 
     _computeTopLevelActions(actionRecord, hiddenActionsRecord) {

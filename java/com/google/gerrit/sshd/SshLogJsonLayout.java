a99b07eb0cc0788fdbbfcfcb5c4a8781626d6924
Thomas Draebing <thomas.draebing@sap.com>
2020-02-13 17:51:42 +0100

    Add option to log SSH events in JSON format
    The sshd logs were only available in the text format. While easier
    to read for humans, this format is harder to parse by machines and
    it is thus harder to collect and index the logs with tools like
    fluentd.
    
    Now, the sshd logs will be formatted in the JSON format, if the
    log.jsonLogging option is set to true, as it is already the case
    for the error logs.
    
    Change-Id: Iafbbdf0dbd66b17b51efd55236d86429d6a8e1c7

---
+++ java/com/google/gerrit/sshd/SshLogJsonLayout.java
@@ -0,0 +1,144 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd;
+
+import static com.google.gerrit.sshd.SshLog.P_ACCOUNT_ID;
+import static com.google.gerrit.sshd.SshLog.P_AGENT;
+import static com.google.gerrit.sshd.SshLog.P_EXEC;
+import static com.google.gerrit.sshd.SshLog.P_MESSAGE;
+import static com.google.gerrit.sshd.SshLog.P_SESSION;
+import static com.google.gerrit.sshd.SshLog.P_STATUS;
+import static com.google.gerrit.sshd.SshLog.P_USER_NAME;
+import static com.google.gerrit.sshd.SshLog.P_WAIT;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class SshLogJsonLayout extends Layout {
+  private final SimpleDateFormat dateFormat;
+  private final Gson gson;
+  private long lastTimeMillis;
+  private String lastTimeString;
+
+  public SshLogJsonLayout() {
+    final TimeZone tz = TimeZone.getDefault();
+    dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS Z");
+    dateFormat.setTimeZone(tz);
+
+    lastTimeMillis = System.currentTimeMillis();
+    lastTimeString = dateFormat.format(new Date(lastTimeMillis));
+
+    gson = newGson();
+  }
+
+  @Override
+  public String format(LoggingEvent event) {
+    SshJsonLog logEntry = new SshJsonLog(event);
+    return gson.toJson(logEntry) + ",\n";
+  }
+
+  @Override
+  public boolean ignoresThrowable() {
+    return true;
+  }
+
+  @Override
+  public void activateOptions() {}
+
+  private Gson newGson() {
+    GsonBuilder gb =
+        new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .disableHtmlEscaping();
+    return gb.create();
+  }
+
+  @SuppressWarnings("unused")
+  private class SshJsonLog {
+    public String timestamp;
+    public String session;
+    public String thread;
+    public String user;
+    public String accountId;
+    public String message;
+    public String waitTime;
+    public String execTime;
+    public String status;
+    public String agent;
+    public String timeNegotiating;
+    public String timeSearchReuse;
+    public String timeSearchSizes;
+    public String timeCounting;
+    public String timeCompressing;
+    public String timeWriting;
+    public String timeTotal;
+    public String bitmapIndexMisses;
+    public String deltasTotal;
+    public String objectsTotal;
+    public String bytesTotal;
+
+    public SshJsonLog(LoggingEvent event) {
+      this.timestamp = formatDate(event.getTimeStamp());
+      this.session = getMdcString(event, P_SESSION);
+      this.thread = event.getThreadName();
+      this.user = getMdcString(event, P_USER_NAME);
+      this.accountId = getMdcString(event, P_ACCOUNT_ID);
+      this.message = (String) event.getMessage();
+      this.waitTime = getMdcString(event, P_WAIT);
+      this.execTime = getMdcString(event, P_EXEC);
+      this.status = getMdcString(event, P_STATUS);
+      this.agent = getMdcString(event, P_AGENT);
+
+      String metricString = getMdcString(event, P_MESSAGE);
+      if (metricString != null && !metricString.isEmpty()) {
+        String[] ssh_metrics = metricString.split(" ");
+        this.timeNegotiating = ssh_metrics[0];
+        this.timeSearchReuse = ssh_metrics[1];
+        this.timeSearchSizes = ssh_metrics[2];
+        this.timeCounting = ssh_metrics[3];
+        this.timeCompressing = ssh_metrics[4];
+        this.timeWriting = ssh_metrics[5];
+        this.timeTotal = ssh_metrics[6];
+        this.bitmapIndexMisses = ssh_metrics[7];
+        this.deltasTotal = ssh_metrics[8];
+        this.objectsTotal = ssh_metrics[9];
+        this.bytesTotal = ssh_metrics[10];
+      }
+    }
+
+    private String getMdcString(LoggingEvent event, String key) {
+      return (String) event.getMDC(key);
+    }
+
+    private String formatDate(long now) {
+      final long rounded = now - (int) (now % 1000);
+      if (rounded != lastTimeMillis) {
+        synchronized (dateFormat) {
+          lastTimeMillis = rounded;
+          lastTimeString = dateFormat.format(new Date(lastTimeMillis));
+          return lastTimeString;
+        }
+      }
+
+      return lastTimeString;
+    }
+  }
+}

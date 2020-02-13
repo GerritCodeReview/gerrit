7d81a8ed758da719ceebe1efa89044c35c6fe544
Dmitrii Filippov <dmfilippov@google.com>
2020-02-13 17:06:04 +0100

    Update links and remove links_update rule
    update_links rule was run on build phase. These change update links
    in source code explicitly and removes call to update_links rule
    from the BUILD files.
    
    All .html files were updated with links_updater.sh script.
    links_updater.sh runs update_links rule internally.
    The links_updater.sh must be run after each rebase.
    
    The following files were updated/added manually:
    * links_updater.sh
    * polygerrit-ui/app/BUILD
    * polygerrit-ui/server.go
    * polygerrit-ui/app/polylint_test.sh
    * polygerrit-ui/app/rules.bzl
    * polygerrit-ui/app/wct_test.sh
    
    Change-Id: If44c35fb24c1b88705e58d3f4b8fb6633fd81b26

---
+++ links_updater.sh
@@ -0,0 +1,7 @@
+find polygerrit-ui/app/ -name *.html -type f -not -path "*/node_modules/*" -exec sh -c 'git checkout origin/master -- {}' \;
+DIR=$(pwd)
+temp_file=$(mktemp)
+find $DIR/polygerrit-ui/app/ -name *.html -type f -not -path "*/node_modules/*" -exec sh -c 'echo {} && echo {}' \n \; >$temp_file
+bazel run tools/node_tools/polygerrit_app_preprocessor:links-updater-bin -- $temp_file $DIR/polygerrit-ui/app/redirects.json
+rm $temp_file
+

fcbcd659b080f82ccbcf3f94922e84d0a915f926
Thomas Draebing <thomas.draebing@sap.com>
2020-02-17 14:51:34 +0100

    Extract duplicated code of {Ssh|Http}LogJsonLayout
    The json layouts for the httpd and sshd logs contained duplicated
    code. This change extracts this code into abstract base classes.
    
    Change-Id: I11451928cb666000804d842deddfc1facb0a64e4

--- java/com/google/gerrit/sshd/SshLogJsonLayout.java
+++ java/com/google/gerrit/sshd/SshLogJsonLayout.java
@@ -23,57 +23,25 @@ import static com.google.gerrit.sshd.SshLog.P_STATUS;
 import static com.google.gerrit.sshd.SshLog.P_USER_NAME;
 import static com.google.gerrit.sshd.SshLog.P_WAIT;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
+import com.google.gerrit.server.logging.JsonLayout;
+import com.google.gerrit.server.logging.JsonLogEntry;
 import java.time.format.DateTimeFormatter;
-import org.apache.log4j.Layout;
 import org.apache.log4j.spi.LoggingEvent;
 
-public class SshLogJsonLayout extends Layout {
-  private final DateTimeFormatter dateFormatter;
-  private final Gson gson;
-  private final ZoneOffset timeOffset;
+public class SshLogJsonLayout extends JsonLayout {
 
-  private long lastTimeSeconds;
-  private String lastTimeString;
-
-  public SshLogJsonLayout() {
-    dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS Z");
-    timeOffset = OffsetDateTime.now().getOffset();
-
-    gson = newGson();
+  @Override
+  public DateTimeFormatter setDateTimeFormatter() {
+    return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS Z");
   }
 
   @Override
-  public String format(LoggingEvent event) {
-    SshJsonLog logEntry = new SshJsonLog(event);
-    return gson.toJson(logEntry) + ",\n";
-  }
-
-  @Override
-  public boolean ignoresThrowable() {
-    return true;
-  }
-
-  @Override
-  public void activateOptions() {}
-
-  private Gson newGson() {
-    GsonBuilder gb =
-        new GsonBuilder()
-            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-            .disableHtmlEscaping();
-    return gb.create();
+  public JsonLogEntry createLogEntry(LoggingEvent event) {
+    return new SshJsonLogEntry(event);
   }
 
   @SuppressWarnings("unused")
-  private class SshJsonLog {
+  private class SshJsonLogEntry extends JsonLogEntry {
     public String timestamp;
     public String session;
     public String thread;
@@ -96,7 +64,7 @@ public class SshLogJsonLayout extends Layout {
     public String objectsTotal;
     public String bytesTotal;
 
-    public SshJsonLog(LoggingEvent event) {
+    public SshJsonLogEntry(LoggingEvent event) {
       this.timestamp = formatDate(event.getTimeStamp());
       this.session = getMdcString(event, P_SESSION);
       this.thread = event.getThreadName();
@@ -123,22 +91,6 @@ public class SshLogJsonLayout extends Layout {
         this.objectsTotal = ssh_metrics[9];
         this.bytesTotal = ssh_metrics[10];
       }
-    }
-
-    private String getMdcString(LoggingEvent event, String key) {
-      return (String) event.getMDC(key);
-    }
-
-    private String formatDate(long now) {
-      long nowSeconds = now / 1000;
-      if (nowSeconds != lastTimeSeconds) {
-        lastTimeSeconds = nowSeconds;
-        lastTimeString =
-            ZonedDateTime.of(
-                    LocalDateTime.ofEpochSecond(now / 1000, 0, timeOffset), ZoneId.systemDefault())
-                .format(dateFormatter);
-      }
-      return lastTimeString;
     }
   }
 }

2b9d7165c1568b74d0022a171a69ef2dae8d53a7
Ben Rohlfs <brohlfs@google.com>
2020-02-18 17:40:01 +0100

    hovercards
    Change-Id: I14b57021b06a3f5450e0d168036c28e09ee61668

---
+++ polygerrit-ui/app/elements/shared/gr-hovercard-account/gr-hovercard-account.js
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function() {
+  'use strict'
+
+  /** @extends Polymer.Element */
+  class GrHovercardAccount extends Polymer.GestureEventListeners(
+      Polymer.LegacyElementMixin(
+          Polymer.Element)) {
+    static get is() { return 'gr-hovercard-account'; }
+
+    static get properties() {
+      return {
+        account: Object,
+      };
+    }
+
+    /** @override */
+    attached() {
+      super.attached();
+    }
+
+    /** @override */
+    created() {
+      super.created();
+    }
+
+    /** @override */
+    ready() {
+      super.ready();
+    }
+  }
+
+  customElements.define(GrHovercardAccount.is, GrHovercardAccount);
+})();

0c1cbe1f45ca09aff4cb6794dccf555813da19f5
Tao Zhou <taoalpha@google.com>
2020-02-18 14:32:52 +0100

    Demo for suggestion dialog of owner
    Change-Id: Ieeba0ece8720afab0638a92f6854ad8ae54e124c

--- polygerrit-ui/app/elements/change/gr-change-requirements/gr-change-requirements.js
+++ polygerrit-ui/app/elements/change/gr-change-requirements/gr-change-requirements.js
@@ -154,6 +154,45 @@
     _handleShowHide(e) {
       this._showOptionalLabels = !this._showOptionalLabels;
     }
+
+    _openSuggestDialogTwo() {
+      this._testSuggestionTitle = 'Needs Code Review approval';
+      this._testTwo = true;
+      this._testOne = false;
+      this._testApplyEnabled = false;
+      this._testSuggestionItems = [];
+      this._testFetched = false;
+      this._testSuggestButtonText = 'Suggest';
+      this.$.suggestionDialog.open();
+    }
+
+    _generateRandomId() {
+      return Math.random().toString(16)
+          .slice(0, 6);
+    }
+
+    _testGetSuggestion() {
+      this._testFetching = true;
+      this._testSuggestButtonText = 'Suggesting';
+      this._fetchSuggestions().then(suggestions => {
+        this._testSuggestionItems = suggestions;
+        this._testFetched = true;
+        this._testFetching = false;
+        this._testSuggestButtonText = 'Suggest';
+        Polymer.dom.flush();
+      });
+    }
+
+    _fetchSuggestions() {
+      return new Promise(r => setTimeout(r, 2000)).then(() => [
+        {name: this._generateRandomId()},
+        {name: this._generateRandomId()},
+      ]);
+    }
+
+    _testEnableApply() {
+      this._testApplyEnabled = true;
+    }
   }
 
   customElements.define(GrChangeRequirements.is, GrChangeRequirements);

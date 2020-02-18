2a84119f69186c5447cd90f4f6fe184814219d1e
Dhruv Srivastava <dhruvsri@google.com>
2020-02-18 16:25:03 +0100

    Refactor to use name instead of index when selecting file tab
    * After adding the findings tab, the index of it  will depend on if
    plugin endpoints exist or not.
    
    Change-Id: I550c70ca60a334580e1805778c160d8a11fd310f

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -334,6 +334,14 @@
         _currentRobotCommentsPatchSet: {
           type: Number,
         },
+        FILES: {
+          type: String,
+          value: 'files',
+        },
+        _currentTabName: {
+          type: String,
+          value: this.FILES,
+        },
       };
     }
 
@@ -504,29 +512,40 @@
       return currentView === view;
     }
 
-    _handleFileTabChange(e) {
-      const selectedIndex = this.shadowRoot
-          .querySelector('#primaryTabs').selected;
-      this._showFileTabContent = selectedIndex === 0;
-      // Initial tab is the static files list.
-      const newSelectedTab =
-          this._dynamicTabContentEndpoints[selectedIndex - 1];
-      if (newSelectedTab !== this._selectedFilesTabPluginEndpoint) {
-        this._selectedFilesTabPluginEndpoint = newSelectedTab;
+    _findIfTabMatches(currentTab, tabs) {
+      // Check if currentTab is included in tabs
+      // If tabs is not an array, then that means a single tab
+      return Array.isArray(tabs) ? tabs.includes(currentTab) :
+        (tabs === currentTab);
+    }
 
-        const tabName = this._selectedFilesTabPluginEndpoint || 'files';
-        const source = e && e.type ? e.type : '';
-        this.$.reporting.reportInteraction('tab-changed', {tabName, source});
+    _handleFileTabChange(e) {
+      const selectedIndex = e.target.selected;
+      this._currentTabName = e.target.querySelectorAll(
+          'paper-tab')[selectedIndex].dataset.name;
+      const source = e && e.type ? e.type : '';
+      const pluginIndex = this._dynamicTabHeaderEndpoints.indexOf(
+          this._currentTabName);
+      if (pluginIndex !== -1) {
+        this._selectedFilesTabPluginEndpoint = this._dynamicTabContentEndpoints[
+            pluginIndex];
       }
+      this.$.reporting.reportInteraction('tab-changed',
+          {tabName: this._currentTabName, source});
     }
 
     _handleShowTab(e) {
-      const idx = this._dynamicTabContentEndpoints.indexOf(e.detail.tab);
+      const tabs = this.shadowRoot.querySelector('#primaryTabs')
+          .querySelectorAll('paper-tab');
+      let idx = -1;
+      tabs.forEach((tab, index) => {
+        if (tab.dataset.name === e.detail.tab) idx = index;
+      });
       if (idx === -1) {
         console.warn(e.detail.tab + ' tab not found');
         return;
       }
-      this.shadowRoot.querySelector('#primaryTabs').selected = idx + 1;
+      this.shadowRoot.querySelector('#primaryTabs').selected = idx;
       this.shadowRoot.querySelector('#primaryTabs').scrollIntoView();
       this.$.reporting.reportInteraction('show-tab', {tabName: e.detail.tab});
     }

d21df3e8d7e58c92f5aa746f635b0139e347d18e
David Pursehouse <dpursehouse@collab.net>
2020-02-20 11:03:02 +0900

    ChangeQueryBuilder: Fix query of visibleto by display name
    If the given account identifier resolved to more than one user, the
    visibleto would only use the first user from the result. This was OK
    for queries by username or account ID because they always resolve to
    either no user or exactly one user. However when querying by display
    name it is possible that multiple users will be returned, and in this
    case it was incorrect to only use the first one.
    
    Fix it to correctly resolve multiple users, and extend the tests to
    cover this use case.
    
    This has been broken since change I87e58dda4 which was introduced in
    2012 (8 years ago!) and was discovered by Sonar Lint which reported
    the following message on the broken code:
    
      Loops with at most one iteration should be refactored (squid:S1751)
    
      A loop with at most one iteration is equivalent to the use of an if
      statement to conditionally execute one piece of code. No developer
      expects to find such a use of a loop statement. If the initial
      intention of the author was really to conditionally execute one piece
      of code, an if statement should be used instead.
    
      At worst that was not the initial intention of the author and so the
      body of the loop should be fixed to use the nested return, break or
      throw statements in a more appropriate way.
    
    Change-Id: Ibb16a70e6cc61c22516f5d9ac3e1b6b461c94dc2

--- java/com/google/gerrit/server/query/change/ChangeQueryBuilder.java
+++ java/com/google/gerrit/server/query/change/ChangeQueryBuilder.java
@@ -22,6 +22,7 @@ import static java.util.stream.Collectors.toSet;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Enums;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.primitives.Ints;
 import com.google.gerrit.common.data.GroupDescription;
@@ -913,10 +914,13 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData> {
       return isVisible();
     }
     Set<Account.Id> m = args.accountResolver.findAll(who);
+    if (m.size() == 1) {
+      return visibleto(args.userFactory.create(Iterables.getOnlyElement(m)));
+    }
     if (!m.isEmpty()) {
       List<Predicate<ChangeData>> p = Lists.newArrayListWithCapacity(m.size());
       for (Account.Id id : m) {
-        return visibleto(args.userFactory.create(id));
+        p.add(visibleto(args.userFactory.create(id)));
       }
       return Predicate.or(p);
     }

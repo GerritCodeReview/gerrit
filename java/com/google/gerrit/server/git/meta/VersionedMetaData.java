0582b29806042bc606de60b3b809cc4625e28f63
David Pursehouse <dpursehouse@collab.net>
2020-02-21 17:43:53 +0900

    VersionedMetaData: Emit debug log after update has been committed
    Log the commit message and the revision that was created.
    
    Also update the existing log to include the message in quotes.
    
    Change-Id: Id8c8eea622f54600a822d14abdd4da2db5d727b1

--- java/com/google/gerrit/server/git/meta/VersionedMetaData.java
+++ java/com/google/gerrit/server/git/meta/VersionedMetaData.java
@@ -409,7 +409,7 @@ public abstract class VersionedMetaData {
           // read the subject line and use it as reflog message
           ru.setRefLogMessage("commit: " + reader.readLine(), true);
         }
-        logger.atFine().log("Saving commit: %s", message.trim());
+        logger.atFine().log("Saving commit \"%s\"", message.trim());
         inserter.flush();
         RefUpdate.Result result = ru.update();
         switch (result) {
@@ -417,6 +417,8 @@ public abstract class VersionedMetaData {
           case FAST_FORWARD:
             revision = rw.parseCommit(ru.getNewObjectId());
             update.fireGitRefUpdatedEvent(ru);
+            logger.atFine().log(
+                "Saved commit \"%s\" as revision '%s'", message.trim(), revision.name());
             return revision;
           case LOCK_FAILURE:
             throw new LockFailureException(errorMsg(ru, db.getDirectory()), ru);

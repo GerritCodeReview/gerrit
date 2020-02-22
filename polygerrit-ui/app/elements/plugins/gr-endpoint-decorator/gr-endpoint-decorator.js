2b3b39141a8d759c854d4891323496e8e73c3384
Paladox none <thomasmulhall410@yahoo.com>
2020-02-22 20:54:05 +0000

    Remove `this.importHref`
    This is Polymer 1 code and no longer needed as we're using Polymer 3.
    
    Change-Id: Iadce10437de25faae03e730f7e1b98e53358f41d

--- polygerrit-ui/app/elements/plugins/gr-endpoint-decorator/gr-endpoint-decorator.js
+++ polygerrit-ui/app/elements/plugins/gr-endpoint-decorator/gr-endpoint-decorator.js
@@ -60,7 +60,7 @@
      */
     _import(url) {
       return new Promise((resolve, reject) => {
-        (this.importHref || Polymer.importHref)(url, resolve, reject);
+        Polymer.importHref(url, resolve, reject);
       });
     }
 

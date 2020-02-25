14fd203c4bcb296607b784146c1c4ae6a86e03fa
Dmitrii Filippov <dmfilippov@google.com>
2020-02-25 16:56:07 +0100

    The tool to convert polygerrit code to es6 modules
    This is a temporary change. It is reverted here
    https://gerrit-review.googlesource.com/c/gerrit/+/258560
    
    The entrypoint is the es6-modules-converter.sh script.
    The script creates(or updates) 2 changes on top of this change:
    1. Change to rename all .html files (except tests) to .js files
    2. Change to convert polygerrit source code to es6 modules.
    
    Important: the tool works only on linux. Doesn't work on mac.
    
    How to use:
    1. Rebase the relation chain to the latest master (up to and including
       this change)
    2. Checkout this change
    3. Run the tool:
    a) If you run it for the first time
       (or if you want to create completely new change)
    ./es6-modules-converter
    
    b) If you want to update existing change:
    ./es6-modules-converter --rename-change-id .... --convert-change-id ...
    Example:
    
    ./es6-modules-converter.sh \
      --rename-change-id Ic078c03c9ead018c80142ce1976d7192cb631964 \
      --convert-change-id I0c447dd8c05757741e2c940720652d01d9fb7d67
    
    Note: You can see some errors from polymer-modulizer - it is not
    an actual error.
    
    3. If the tool completes successful, push changes. If you have any
    error - fix them and repeat starting from the step 1 or 2.
    
    4. Rebase https://gerrit-review.googlesource.com/c/gerrit/+/258560 on
    top of the created/updated change.
    
    Change-Id: Ib186e2d28cd5eba234f65f94b9afa8fa50502515

---
+++ rename-html.sh
@@ -0,0 +1,16 @@
+html_filepath=$1
+
+base_name=$(basename $1 ".html")
+base_dir=$(dirname $1)
+
+js_filename="${base_name}.js"
+js_filepath="${base_dir}/${js_filename}"
+
+template_filename="${base_name}_html.js"
+template_filepath="${base_dir}/${template_filename}"
+
+if test -f "$js_filepath"; then
+  git mv $html_filepath $template_filepath
+else
+  git mv $html_filepath $js_filepath
+fi

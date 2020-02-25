883bd742468f37c5bb43cb2713d9229923a9f1a8
Dmitrii Filippov <dmfilippov@google.com>
2020-02-25 17:04:59 +0100

    Add files needed for ES6-module conversion
    Change-Id: I3faf5e637629ed5c6ed666e3f2ef55df7ba5b885

---
+++ tools/node_tools/polygerrit_app_preprocessor/script-tag-replacer.ts
@@ -0,0 +1,180 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as fs from "fs";
+import RewritingStream from "parse5-html-rewriting-stream";
+import * as dom5 from "dom5";
+import {HtmlFileUtils} from "./utils";
+import {Node} from 'dom5';
+import {readMultilineParamFile} from "../utils/command-line";
+import { fail } from "../utils/common";
+import * as path from "path";
+import {error} from "util";
+
+async function main() {
+  if (process.argv.length < 5) {
+    console.info("Usage:\n\tnode script-tag-replacer.js root_dir html_files_param_file output_list_file_name\n");
+    process.exit(1);
+  }
+
+  const root = path.normalize(process.argv[2]);
+  const htmlFiles = readMultilineParamFile(process.argv[3]);
+  const updater = new HtmlFileUpdater();
+  for(const htmlFile of htmlFiles) {
+    await updater.updateFile(root, htmlFile);
+  }
+  updater.writeListOfGeneratedHtmlFiles(process.argv[4]);
+  const errors = updater.getErrors();
+  if(errors.length > 0) {
+    fail(errors.join("\n"));
+  }
+
+}
+
+/** Update all links in HTML file based on redirects.
+ * Additionally, update references to web-component-tester */
+class HtmlFileUpdater {
+  private static readonly Predicates = {
+    isScriptWithSrcTag: (node: Node) => node.tagName === "script" && dom5.hasAttribute(node, "src"),
+  };
+
+  private readonly jsToGeneratedHtmlFileMap = new Map<string, string>();
+  private readonly errors: string[] = [];
+
+  public async updateFile(root: string, srcFile: string) {
+    const srcFilePath = path.join(root, srcFile);
+    const html = fs.readFileSync(srcFilePath, "utf-8");
+    const readStream = fs.createReadStream(srcFilePath, {encoding: "utf-8"});
+    const rewriterOutput = srcFilePath + ".tmp";
+    const writeStream = fs.createWriteStream(rewriterOutput, {encoding: "utf-8"});
+    const rewriter = new RewritingStream();
+    (rewriter as any).tokenizer.preprocessor.bufferWaterline = Infinity;
+
+    rewriter.on("startTag", (tag: any) => {
+     if (HtmlFileUpdater.Predicates.isScriptWithSrcTag(tag)) {
+        const htmlImportPath = this.getHtmlImportPathForScript(tag, root, srcFile);
+        if(htmlImportPath) {
+          rewriter.emitRaw(`<link rel="import" href="${htmlImportPath}"/>`);
+          // Ignore script close tag
+          if(!tag.selfClosing) {
+            rewriter.once("endTag", (tag: any) => {
+            });
+          }
+          return;
+        }
+      }
+      const location = tag.sourceCodeLocation;
+      const raw = html.substring(location.startOffset, location.endOffset);
+      rewriter.emitRaw(raw);
+      return;
+    });
+    let insideDomModule = false;
+    const initialErrorsCount = this.errors.length;
+    rewriter.on("startTag", (tag: any) => {
+      if(tag.tagName === "dom-module") {
+        if(insideDomModule) {
+          this.reportError(srcFile, 'Nested dom-module element');
+        }
+        insideDomModule = true;
+      } else if(tag.tagName == "script") {
+        if(insideDomModule) {
+          if(HtmlFileUpdater.Predicates.isScriptWithSrcTag(tag)) {
+            const src = dom5.getAttribute(tag, "src")!;
+            if(!this.isAllowedSrcInDomModule(srcFile, src)) {
+              this.reportError(srcFile, `Script src='${src}' is not valid inside dom-module. Move script tag outside of dom-module`);
+            }
+
+          } else {
+            this.reportError(srcFile, 'File can contain only one element script tag inside dom-module! Move all other scripts outside of dom-module');
+          }
+        }
+      }
+    });
+    rewriter.on("endTag", (tag: any) => {
+      if(tag.tagName === "dom-module") {
+        if(!insideDomModule) {
+          this.reportError(srcFile, 'dom-module closed without starting tag');
+        }
+        insideDomModule = false;
+      }
+    });
+
+    return new Promise<void>((resolve) => {
+      writeStream.on("close", () => {
+        writeStream.close();
+        if(this.errors.length === initialErrorsCount) {
+          fs.renameSync(rewriterOutput, srcFilePath);
+        } else {
+          // Errors during conversion - do not overwrite file
+          fs.unlinkSync(rewriterOutput);
+        }
+        resolve();
+      });
+      readStream.pipe(rewriter).pipe(writeStream);
+    });
+  }
+
+  private isAllowedSrcInDomModule(srcFile: string, scriptSrc: string) {
+    return path.basename(srcFile, ".html") + ".js" === scriptSrc;
+  }
+  private reportError(fileName: string, errorText: string): void {
+    this.errors.push(`${fileName}: ${errorText}`);
+  }
+
+  public getErrors(): string[] {
+    return this.errors;
+  }
+
+  private getHtmlImportPathForScript(node: Node, root: string, parentHtml: string): string | undefined {
+    const ref = dom5.getAttribute(node, "src");
+    if (!ref) {
+      fail(`Internal error - ${node} in ${parentHtml} doesn't have attribute 'src'`);
+    }
+    if(ref.startsWith("/")) {
+      return undefined;
+    }
+    const originalPath = path.join(root, HtmlFileUtils.getPathRelativeToRoot(parentHtml, ref));
+    const pathRelativeToRoot = path.relative(root, originalPath);
+    if(pathRelativeToRoot.startsWith('bower_components/') || pathRelativeToRoot.startsWith('node_modules/') || pathRelativeToRoot.startsWith('components')) {
+      return undefined;
+    }
+    const parsedPath = path.parse(originalPath);
+    parsedPath.base = path.basename(parsedPath.base, ".js") + ".html";
+    const htmlFilePath = path.format(parsedPath);
+    const parentHtmlFullPath = path.join(root, parentHtml);
+    if(!this.jsToGeneratedHtmlFileMap.has(originalPath)) {
+      if (fs.existsSync(htmlFilePath)) {
+        if (parentHtmlFullPath !== htmlFilePath) {
+          if(htmlFilePath.endsWith('/gr-etag-decorator.html')) {
+            // Has reference from test to .js; ignore this error
+            return undefined;
+          }
+          fail(`Can't create html for the ${originalPath} file. The html file already exists`);
+        }
+        return undefined;
+      }
+      const scriptFileName = path.basename(originalPath);
+      fs.writeFileSync(htmlFilePath, `<script src="${scriptFileName}"></script>`);
+      this.jsToGeneratedHtmlFileMap.set(originalPath, htmlFilePath)
+    }
+    return path.relative(path.dirname(parentHtmlFullPath), this.jsToGeneratedHtmlFileMap.get(originalPath)!);
+  }
+
+  public writeListOfGeneratedHtmlFiles(fileName: string) {
+    const allFiles = new Array(...this.jsToGeneratedHtmlFileMap.values()).join('\n');
+    fs.writeFileSync(fileName, allFiles);
+  }
+}
+
+main();

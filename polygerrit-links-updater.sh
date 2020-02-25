9dae6b9a7e36ad7cb78a13ca2c18d0e1ce744173
Dmitrii Filippov <dmfilippov@google.com>
2020-02-25 16:56:07 +0100

    Tool for a partial update links to bower_components
    Change-Id: Ib186e2d28cd5eba234f65f94b9afa8fa50502515

---
+++ polygerrit-links-updater.sh
@@ -0,0 +1,8 @@
+pattern=*.html
+#find polygerrit-ui/app/ -name $pattern -type f -not -path "*/node_modules/*" -exec sh -c 'git checkout origin/master -- {}' \;
+find polygerrit-ui/app/ -name $pattern -type f -not -path "*/node_modules/*" -exec sh -c 'git restore {}' \;
+DIR=$(pwd)
+temp_file=$(mktemp)
+find $DIR/polygerrit-ui/app/ -name $pattern -type f -not -path "*/node_modules/*" -exec sh -c 'echo {} && echo {}' \n \; >$temp_file
+bazel run tools/node_tools/polygerrit_app_preprocessor:links-updater-bin -- $temp_file $DIR/polygerrit-ui/app/partial-redirects.json
+rm $temp_file

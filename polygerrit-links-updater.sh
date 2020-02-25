5fa57b7d75eba9f87074445c7c3838a8b3561fc4
Dmitrii Filippov <dmfilippov@google.com>
2020-02-25 16:56:07 +0100

    Tool for a partial update links to bower_components
    Change-Id: Ib186e2d28cd5eba234f65f94b9afa8fa50502515

---
+++ polygerrit-links-updater.sh
@@ -0,0 +1,21 @@
+# Temporary file - needed only for es6 module conversion
+
+# The script updates some links according to the
+# polygerrit-ui/app/partial-redirects.json rules
+
+set -e
+
+if [[ `git status --porcelain` &&  "$1" -ne "--force" ]]; then
+  echo "Please commit your changes before continue or run the polygerrit-links-updater.sh with  --force flag"
+  exit 1
+fi
+
+DIR=$(pwd)
+html_files_list=$(mktemp)
+find $DIR/polygerrit-ui/app/ -name *.html -type f -not -path "*/node_modules/*" -exec sh -c 'echo {} && echo {}' \n \; >$html_files_list
+bazel run tools/node_tools/polygerrit_app_preprocessor:links-updater-bin -- $html_files_list $DIR/polygerrit-ui/app/partial-redirects.json
+
+find polygerrit-ui/app/ -name '*.html' -type f -not -path "*/node_modules/*" -exec sh -c 'sed -i "s/href=\"\/bower_components\/polymer-resin\/standalone\/polymer-resin.html\"/href=\"\/node_modules\/polymer-bridges\/polymer-resin\/standalone\/polymer-resin.html\"/g" {}' \n \;
+sed -i '/<link rel="import" href="\/node_modules\/polymer-bridges\/polymer-resin\/standalone\/polymer-resin.html"/i <link rel="import" href="\/bower_components\/polymer\/polymer.html">' polygerrit-ui/app/test/common-test-setup.html
+
+rm $html_files_list

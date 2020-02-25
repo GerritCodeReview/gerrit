7d184f00bba7b186ab478ab479be6bd58afe13e8
Dmitrii Filippov <dmfilippov@google.com>
2020-02-25 17:04:59 +0100

    Add files needed for ES6-module conversion
    Change-Id: I3faf5e637629ed5c6ed666e3f2ef55df7ba5b885

---
+++ polygerrit-ui/app/scripts/import-href.js
@@ -0,0 +1,100 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// run a callback when HTMLImports are ready or immediately if
+// this api is not available.
+function whenImportsReady(cb) {
+  if (window.HTMLImports) {
+    HTMLImports.whenReady(cb);
+  } else {
+    cb();
+  }
+}
+
+/**
+ * Convenience method for importing an HTML document imperatively.
+ *
+ * This method creates a new `<link rel="import">` element with
+ * the provided URL and appends it to the document to start loading.
+ * In the `onload` callback, the `import` property of the `link`
+ * element will contain the imported document contents.
+ *
+ * @memberof Polymer
+ * @param {string} href URL to document to load.
+ * @param {?function(!Event):void=} onload Callback to notify when an import successfully
+ *   loaded.
+ * @param {?function(!ErrorEvent):void=} onerror Callback to notify when an import
+ *   unsuccessfully loaded.
+ * @param {boolean=} optAsync True if the import should be loaded `async`.
+ *   Defaults to `false`.
+ * @return {!HTMLLinkElement} The link element for the URL to be loaded.
+ */
+export function importHref(href, onload, onerror, optAsync) {
+  let link = /** @type {HTMLLinkElement} */
+      (document.head.querySelector('link[href="' + href + '"][import-href]'));
+  if (!link) {
+    link = /** @type {HTMLLinkElement} */ (document.createElement('link'));
+    link.rel = 'import';
+    link.href = href;
+    link.setAttribute('import-href', '');
+  }
+  // always ensure link has `async` attribute if user specified one,
+  // even if it was previously not async. This is considered less confusing.
+  if (optAsync) {
+    link.setAttribute('async', '');
+  }
+  // NOTE: the link may now be in 3 states: (1) pending insertion,
+  // (2) inflight, (3) already loaded. In each case, we need to add
+  // event listeners to process callbacks.
+  let cleanup = function() {
+    link.removeEventListener('load', loadListener);
+    link.removeEventListener('error', errorListener);
+  };
+  let loadListener = function(event) {
+    cleanup();
+    // In case of a successful load, cache the load event on the link so
+    // that it can be used to short-circuit this method in the future when
+    // it is called with the same href param.
+    link.__dynamicImportLoaded = true;
+    if (onload) {
+      whenImportsReady(() => {
+        onload(event);
+      });
+    }
+  };
+  let errorListener = function(event) {
+    cleanup();
+    // In case of an error, remove the link from the document so that it
+    // will be automatically created again the next time `importHref` is
+    // called.
+    if (link.parentNode) {
+      link.parentNode.removeChild(link);
+    }
+    if (onerror) {
+      whenImportsReady(() => {
+        onerror(event);
+      });
+    }
+  };
+  link.addEventListener('load', loadListener);
+  link.addEventListener('error', errorListener);
+  if (link.parentNode == null) {
+    document.head.appendChild(link);
+    // if the link already loaded, dispatch a fake load event
+    // so that listeners are called and get a proper event argument.
+  } else if (link.__dynamicImportLoaded) {
+    link.dispatchEvent(new Event('load'));
+  }
+  return link;
+};
---
+++ tools/node_tools/polygerrit_app_preprocessor/modulizer-postprocessor.ts
@@ -0,0 +1,333 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {
+  ImportDeclaration,
+  Project,
+  QuoteKind,
+  SourceFile,
+  ts,
+  VariableDeclarationKind
+} from "ts-morph";
+import {fail} from "../utils/common";
+import * as path from "path";
+import {readMultilineParamFile} from "../utils/command-line";
+
+const root = path.normalize(process.argv[2]);
+const modulizedFiles = readMultilineParamFile(process.argv[3]);
+//const files = ["./elements/plugins/gr-external-style/gr-external-style.js"];
+//const files = ["./elements/core/gr-router/gr-router.js"];
+
+const modulizerOutRoot = path.join(root, "modulizer_out");
+
+interface NodeTransformer {
+  transformNode(node: ts.Node, relativePathToSourceFile: string): ts.Node | undefined;
+}
+
+class ImportHrefDeclarationTransformer implements NodeTransformer{
+  // Transforms
+  // import { importHref as importHref$0 } from "...";
+  // to
+  // import { importHref } from "../relative_path/scripts/import_href.js";
+
+  private static readonly importHrefPath = path.join(root, "scripts/import-href.js");
+
+  private isSupportedImportHrefDeclaration(node: ts.ImportDeclaration): boolean {
+    // Returns true if import has exactly the following form:
+    // import { importHref as importHref$0 } from "....";
+    // (path is not important)
+
+    if(!node.importClause) {
+      return false;
+    }
+    if(!node.importClause.namedBindings|| !ts.isNamedImports(node.importClause.namedBindings)) {
+      return false;
+    }
+    const namedImports = node.importClause.namedBindings;
+    if (namedImports.elements.length !== 1) {
+      return false;
+    }
+    if(namedImports.elements.length !== 1) {
+      return false;
+    }
+    const firstNamedImport = namedImports.elements[0];
+    if(!firstNamedImport.propertyName) {
+      return false;
+    }
+    return firstNamedImport.name.text === "importHref$0" && firstNamedImport.propertyName.text === "importHref";
+  }
+
+  public transformNode(node: ts.Node, relativePathToSourceFile: string): ts.ImportDeclaration | undefined {
+    if(!ts.isImportDeclaration(node)) {
+      return undefined;
+    }
+    if (!ts.isStringLiteral(node.moduleSpecifier)) {
+      fail(`Internal error`);
+    }
+
+    const moduleSpecifierText = node.moduleSpecifier.text;
+    if(moduleSpecifierText !== "@polymer/polymer/lib/utils/import-href.js") {
+      return undefined;
+    }
+
+    if(!this.isSupportedImportHrefDeclaration(node)) {
+      fail(`Unsupported import. Expected:
+  import { importHref as importHref$0 } from "...";
+Actual:
+  ${node.getText()}`);
+    }
+
+    const relativePath = path.relative(path.dirname(path.join(root, relativePathToSourceFile)), ImportHrefDeclarationTransformer.importHrefPath);
+    const namedBindings = ts.createNamedImports([ts.createImportSpecifier(undefined, ts.createIdentifier("importHref"))]);
+    return ts.updateImportDeclaration(node, node.decorators, node.modifiers, ts.createImportClause(undefined, namedBindings), ts.createStringLiteral(relativePath));
+  }
+}
+
+class ImportHrefCallExpressionTransformer implements NodeTransformer {
+  // Transforms
+  // (this.importHref || importHref$0)(...arguments..)
+  // to
+  // this.importHref(...arguments...)
+
+  private isImportHrefCallExpression(node: ts.CallExpression): boolean {
+    // Return true if call has one of the following forms:
+    // (this.importHref || importHref$0)(.....)
+    // (this.importHref || Base.importHref$0)(.....)
+    // (arguments are not important)
+
+    if(!ts.isParenthesizedExpression(node.expression)) {
+      return false;
+    }
+    if(!ts.isBinaryExpression(node.expression.expression)) {
+      return false;
+    }
+    const binareExprNode = node.expression.expression;
+
+    if(binareExprNode.operatorToken.kind !== ts.SyntaxKind.BarBarToken) {
+      return false;
+    }
+
+    if(!ts.isPropertyAccessExpression(binareExprNode.left) ||
+        !ts.isIdentifier(binareExprNode.right)) {
+      return false;
+    }
+    if(binareExprNode.left.getText() !== "this.importHref") {
+      return false;
+    }
+    if(binareExprNode.right.getText() !== "importHref$0") {
+      return false;
+    }
+    return true;
+  }
+
+  public transformNode(node: ts.Node): ts.CallExpression | undefined {
+    if(!ts.isCallExpression(node) || !this.isImportHrefCallExpression(node)) {
+      return undefined;
+    }
+    return ts.updateCall(node, ts.createIdentifier("importHref"), node.typeArguments, node.arguments);
+  }
+}
+
+class ImportPolymerLegacyDeclarationTransformer implements NodeTransformer {
+  // Transforms
+  // import '/node_modules/@polymer/polymer/polymer-legacy.js';
+  // to
+  // import '"../relative_path/scripts/bundled-polymer.js"'
+
+  private static readonly bundledPolymerPath = path.join(root, "scripts/bundled-polymer.js");
+
+  public transformNode(node: ts.Node, relativePathToSourceFile: string): ts.ImportDeclaration | undefined {
+    if(!ts.isImportDeclaration(node)) {
+      return undefined;
+    }
+
+    if(node.getText() !== "import '/node_modules/@polymer/polymer/polymer-legacy.js';") {
+      return undefined;
+    }
+    const relativePath = path.relative(path.dirname(path.join(root, relativePathToSourceFile)), ImportPolymerLegacyDeclarationTransformer.bundledPolymerPath);
+    return ts.updateImportDeclaration(node, node.decorators, node.modifiers, node.importClause, ts.createStringLiteral(relativePath));
+  }
+}
+
+class ExtractTemplateTransformer implements NodeTransformer {
+  // ExtractTemplateTransformer must be created for each file
+
+  private hasHtmlTemplate: boolean = false;
+  private importRelativePath: string = "";
+  private templateFileAbsPath: string = "";
+  private taggedTemplateText: string = "";
+
+  private isGetTemplateAccessorDeclaration(node: ts.Node): node is ts.GetAccessorDeclaration {
+    // Returns true if node is a get accessor of the form:
+    // static get template() { ... }
+    if(!ts.isGetAccessorDeclaration(node)) {
+      return false;
+    }
+    if(!ts.isIdentifier(node.name) || node.name.text !== 'template') {
+      return false;
+    }
+    const modifiers = node.modifiers;
+    if(!modifiers || modifiers.length !== 1) {
+      return false;
+    }
+    const firstModifier = modifiers[0];
+    return firstModifier.kind === ts.SyntaxKind.StaticKeyword;
+
+  }
+  private getTaggedTemplateExpression(node: ts.GetAccessorDeclaration): ts.TaggedTemplateExpression | undefined {
+    // Returns taggedTemplateExpression (i.e. html`...`) if node has exactly the following form
+    // static get template() {return html`...`;}
+    // Otherwise returns undefined
+    // Assumes that isGetTemplateAccessorDeclaration(node) returns true
+    if(!node.body || !ts.isBlock(node.body)) {
+      return undefined;
+    }
+    const statements = node.body.statements;
+    if(statements.length !== 1) {
+      return undefined;
+    }
+    const firstStatement = statements[0];
+    if(!ts.isReturnStatement(firstStatement)) {
+      return undefined;
+    }
+    const returnExpression = firstStatement.expression;
+    if(!returnExpression || !ts.isTaggedTemplateExpression(returnExpression)) {
+      return undefined;
+    }
+    if(!ts.isIdentifier(returnExpression.tag) || returnExpression.tag.text !== "html") {
+      return undefined;
+    }
+    if(returnExpression.typeArguments) {
+      return undefined;
+    }
+    if(!ts.isNoSubstitutionTemplateLiteral(returnExpression.template)) {
+      return undefined;
+    }
+    return returnExpression;
+  }
+
+  public transformNode(node: ts.Node, relativePathToSourceFile: string): ts.Node | undefined {
+    if(!this.isGetTemplateAccessorDeclaration(node)) {
+      return undefined;
+    }
+    const taggedTemplateExpression = this.getTaggedTemplateExpression(node);
+    if(!taggedTemplateExpression) {
+      fail(`Not supported. Expected template method in the form 'static get template() {return html\`...\`;}'`);
+    }
+    if(this.hasHtmlTemplate) {
+      fail(`More than one template in the file. Not Supported!`);
+    }
+    const returnStatement = ts.createReturn(ts.createIdentifier("htmlTemplate"));
+    this.importRelativePath = "./" + path.parse(relativePathToSourceFile).name + "_html.js";
+    this.hasHtmlTemplate = true;
+    this.templateFileAbsPath = path.join(modulizerOutRoot, path.dirname(relativePathToSourceFile), this.importRelativePath);
+    this.taggedTemplateText = taggedTemplateExpression.getText();
+    return ts.updateGetAccessor(node, undefined, node.modifiers, node.name, node.parameters, node.type, ts.createBlock([returnStatement]));
+  }
+
+  public updateTemplates(file: SourceFile, project: Project) {
+    if(!this.hasHtmlTemplate) {
+      return;
+    }
+    const htmlTagModuleSpecifier = "@polymer/polymer/lib/utils/html-tag.js";
+    const htmlTagImportDecl = file.getImportDeclaration(htmlTagModuleSpecifier);
+    if(!htmlTagImportDecl) {
+      fail(`Internal error. It is expected that file has import from '@polymer/polymer/lib/utils/html-tag.js'`);
+    }
+    htmlTagImportDecl.remove();
+    file.addImportDeclaration({
+      namedImports: ["htmlTemplate"],
+      moduleSpecifier: this.importRelativePath,
+    });
+
+    const templateSourceFile = project.createSourceFile(this.templateFileAbsPath);
+    templateSourceFile.addImportDeclaration({
+      namedImports: ["html"],
+      moduleSpecifier: htmlTagModuleSpecifier,
+    });
+    templateSourceFile.addVariableStatement({
+      declarationKind: VariableDeclarationKind.Const,
+      isExported: true,
+      declarations: [
+        {
+          name: "htmlTemplate",
+          initializer: this.taggedTemplateText,
+        }
+      ]
+    });
+    templateSourceFile.saveSync();
+  }
+}
+
+class TsFileUpdater {
+  private static readonly SimpleTransformers = [
+      new ImportHrefCallExpressionTransformer(),
+      new ImportHrefDeclarationTransformer(),
+      new ImportHrefCallExpressionTransformer(),
+      new ImportPolymerLegacyDeclarationTransformer(),
+  ];
+
+  private replaceImportPath(file: SourceFile, oldPath: string, newPath: string): ImportDeclaration | undefined {
+    if(oldPath.startsWith('.') || oldPath.startsWith('/')) {
+      fail(`Only module names supported`);
+    }
+    const importDecl = file.getImportDeclaration(oldPath);
+    if(!importDecl) {
+      return importDecl;
+    }
+    importDecl.setModuleSpecifier(newPath);
+    return importDecl;
+  }
+
+  private replaceImportAndSetGlobalValue(file: SourceFile, oldPath: string, newPath: string, globalVarName: string): ImportDeclaration | undefined {
+    const importDecl = this.replaceImportPath(file, oldPath, newPath);
+    if(!importDecl) {
+      return undefined;
+    }
+    importDecl.setDefaultImport(globalVarName);
+    file.insertStatements(importDecl.getChildIndex() + 1, `self.${globalVarName} = ${globalVarName};`);
+  }
+
+  public updateFile(relativePath: string) {
+    const project = new Project({manipulationSettings: {quoteKind: QuoteKind.Single }});
+    const sourceFile = project.addSourceFileAtPath(path.join(modulizerOutRoot, relativePath));
+    const extractTemplateTransformer = new ExtractTemplateTransformer();
+    const updatedSourceFile = sourceFile.transform(traversal => {
+      const node = traversal.visitChildren();
+      for (const transformer of TsFileUpdater.SimpleTransformers) {
+        const transformedNode = transformer.transformNode(node, relativePath);
+        if (transformedNode) {
+          return transformedNode;
+        }
+        const transformedTemplateNode = extractTemplateTransformer.transformNode(node, relativePath);
+        if(transformedTemplateNode) {
+          return transformedTemplateNode;
+        }
+      }
+      return node;
+    });
+    extractTemplateTransformer.updateTemplates(updatedSourceFile, project);
+    this.replaceImportPath(updatedSourceFile, 'es6-promise/dist/es6-promise.min.js', 'es6-promise/lib/es6-promise.js');
+    this.replaceImportPath(updatedSourceFile, 'whatwg-fetch/dist/fetch.umd.js', 'whatwg-fetch/fetch.js');
+    this.replaceImportAndSetGlobalValue(updatedSourceFile, 'page/page.js', 'page/page.mjs', 'page');
+    this.replaceImportAndSetGlobalValue(updatedSourceFile, 'moment/moment.js', 'moment/src/moment.js', 'moment');
+    updatedSourceFile.saveSync();
+  }
+}
+
+const tsFileUpdater = new TsFileUpdater();
+for(const file of modulizedFiles) {
+  console.log(`Updating ${file}`);
+  tsFileUpdater.updateFile(file);
+}

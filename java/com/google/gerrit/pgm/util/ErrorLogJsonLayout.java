046b329f374f59b4bcb1cbd4276a39332b786ac9
Thomas Draebing <thomas.draebing@sap.com>
2020-02-27 16:33:31 +0100

    Do not use jsonevent-layout for error logs in JSON format
    So far the net.logstash.log4j.jsonevent-layout package was used to
    format the error logs to JSON. This format was not suitable for the
    httpd- and sshd logs and thus was not used there. Also the format
    provided by the dependency was not configurable, e.g. did not allow
    to change the timestamp. This resulted in different timestamp formats
    for the text- and the json-formatted error logs.
    
    To be more flexible in the formatting of the json logs, this change
    removes the dependency and uses the same way to format the logs as
    the httpd- and the sshd-logs. This has the advantage of one dependency
    less and the timestamp is the same as in the text format. However, there
    are small differences in the log format to before:
    
    - The `@timestamp`-key is now named `timestamp`, as it was done for
      httpd- and sshd-logs.
    - The logstash-specific field `@version` was removed, since it was anyway
      hardcoded to 1 and would be useless for everybody not using logstash.
    - The `class`-key was renamed to `class_name`.
    
    This change also removes the dependency. The dependency was also referenced
    in the BUILD-files of several other packages, but not actually used and
    thus removal was deemed safe.
    
    Change-Id: I3a2bdba5d4b25067d0324fb63438e2a6ec726c68

---
+++ java/com/google/gerrit/pgm/util/ErrorLogJsonLayout.java
@@ -0,0 +1,86 @@
+package com.google.gerrit.pgm.util;
+
+import com.google.gerrit.util.logging.JsonLayout;
+import com.google.gerrit.util.logging.JsonLogEntry;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+
+public class ErrorLogJsonLayout extends JsonLayout {
+
+  @Override
+  public DateTimeFormatter createDateTimeFormatter() {
+    return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS");
+  }
+
+  @Override
+  public JsonLogEntry toJsonLogEntry(LoggingEvent event) {
+    return new ErrorJsonLogEntry(event);
+  }
+
+  @SuppressWarnings("unused")
+  private class ErrorJsonLogEntry extends JsonLogEntry {
+    public String timestamp;
+    public String sourceHost;
+    public String message;
+    public Map<String, String> exception;
+    public String file;
+    public String lineNumber;
+    public String className;
+    public String method;
+    public String loggerName;
+
+    @SuppressWarnings("rawtypes")
+    public Map mdc;
+
+    public String ndc;
+    public String level;
+    public String threadName;
+
+    public ErrorJsonLogEntry(LoggingEvent event) {
+      this.timestamp = formatDate(event.getTimeStamp());
+      this.sourceHost = getSourceHost();
+      this.message = event.getRenderedMessage();
+      if (event.getThrowableInformation() != null) {
+        this.exception = getException(event.getThrowableInformation());
+      }
+      this.file = event.getLocationInformation().getFileName();
+      this.lineNumber = event.getLocationInformation().getLineNumber();
+      this.className = event.getLocationInformation().getClassName();
+      this.method = event.getLocationInformation().getMethodName();
+      this.loggerName = event.getLoggerName();
+      this.mdc = event.getProperties();
+      this.ndc = event.getNDC();
+      this.level = event.getLevel().toString();
+      this.threadName = event.getThreadName();
+    }
+
+    private String getSourceHost() {
+      try {
+        return InetAddress.getLocalHost().getHostName();
+      } catch (UnknownHostException e) {
+        return "unknown-host";
+      }
+    }
+
+    private Map<String, String> getException(ThrowableInformation throwable) {
+      HashMap<String, String> exceptionInformation = new HashMap<>();
+      if (throwable.getThrowable().getClass().getCanonicalName() != null) {
+        exceptionInformation.put(
+            "exception_class", throwable.getThrowable().getClass().getCanonicalName());
+      }
+      if (throwable.getThrowable().getMessage() != null) {
+        exceptionInformation.put("exception_message", throwable.getThrowable().getMessage());
+      }
+      if (throwable.getThrowableStrRep() != null) {
+        String stackTrace = String.join("\n", throwable.getThrowableStrRep());
+        exceptionInformation.put("stacktrace", stackTrace);
+      }
+      return exceptionInformation;
+    }
+  }
+}

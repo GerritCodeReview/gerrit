80dd97c3bcc56eab42033e3858fdf4dc5d32bbd0
Dhruv Srivastava <dhruvsri@google.com>
2020-03-02 16:17:11 +0100

    Disable apply fix for older patchsets
    Change-Id: Iadceedc145e0e91e7e03b933928c38cf6c8ceb37

--- polygerrit-ui/app/elements/diff/gr-apply-fix-dialog/gr-apply-fix-dialog.js
+++ polygerrit-ui/app/elements/diff/gr-apply-fix-dialog/gr-apply-fix-dialog.js
@@ -30,7 +30,7 @@
 
     static get properties() {
       return {
-      // Diff rendering preference API response.
+        // Diff rendering preference API response.
         prefs: Array,
         // ChangeInfo API response object.
         change: Object,
@@ -50,6 +50,11 @@
         },
         // Index of currently showing suggested fix.
         _selectedFixIdx: Number,
+        _disableApplyFixButton: {
+          type: Boolean,
+          computed: '_computeDisableApplyFixButton(_isApplyFixLoading, change, '
+            + '_patchNum)',
+        },
       };
     }
 
@@ -180,6 +185,27 @@
 
     _getApplyFixButtonLabel(isLoading) {
       return isLoading ? 'Saving...' : 'Apply Fix';
+    }
+
+    _computeTooltip(disableApplyFixButton, change, patchNum) {
+      if (!change || disableApplyFixButton == undefined || patchNum ==
+        undefined) return '';
+      const currentPatchNum = change.revisions && change.current_revision ?
+        change.revisions[change.current_revision]._number : 0;
+      return (disableApplyFixButton && currentPatchNum !== patchNum) ?
+        'Fix can only be applied to the latest patchset' : '';
+    }
+
+    _computeDisableApplyFixButton(isApplyFixLoading, change, patchNum) {
+      if (!change || isApplyFixLoading == undefined || patchNum == undefined) {
+        return true;
+      }
+      const currentPatchNum = change.revisions[change.current_revision]._number;
+      if (patchNum !== currentPatchNum) {
+        // Disable apply fix button for older patchsets
+        return true;
+      }
+      return isApplyFixLoading;
     }
 
     _handleApplyFix(e) {

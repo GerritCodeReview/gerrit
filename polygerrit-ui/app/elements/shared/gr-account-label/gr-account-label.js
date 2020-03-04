6ced5c721ddab27aa057574a81c7db95b5418134
Ben Rohlfs <brohlfs@google.com>
2020-03-04 19:48:31 +0100

    Simplify showing name or email in account-label
    Just use the logic in getUserName() that falls back properly to using
    the email address, if there is no name set.
    
    Change-Id: I44804dba08d2415cd67c73df5411676039e9ce48

--- polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
+++ polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
@@ -67,7 +67,7 @@
     }
 
     _computeName(account, config) {
-      return this.getUserName(config, account, false);
+      return this.getUserName(config, account, true);
     }
 
     _computeAccountTitle(account, tooltip) {
@@ -98,21 +98,6 @@
       }
 
       return result;
-    }
-
-    _computeShowEmailClass(account) {
-      if (!account || account.name || !account.email) { return ''; }
-      return 'showEmail';
-    }
-
-    _computeEmailStr(account) {
-      if (!account || !account.email) {
-        return '';
-      }
-      if (account.name) {
-        return '(' + account.email + ')';
-      }
-      return account.email;
     }
 
     _computeHasTooltip(account) {

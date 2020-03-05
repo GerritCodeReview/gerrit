9bfa297e2a0cd72bea987152e14c3e1e9f3e198a
Ben Rohlfs <brohlfs@google.com>
2020-03-05 16:31:00 +0100

    Add reviewers column to dashboard
    Change-Id: Iebaa8387df630b7ecac93b4586f1e8a49990baf5

--- polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
+++ polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
@@ -38,6 +38,10 @@
           type: Boolean,
           value: false,
         },
+        hideStatus: {
+          type: Boolean,
+          value: false,
+        },
         _serverConfig: {
           type: Object,
           value: null,

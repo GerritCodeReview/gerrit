90f880ed7c8792ac17fd287c2bb0f8b4b7c9a4c1
Joerg Zieren <zieren@google.com>
2020-03-06 13:44:25 +0100

    REST endpoints PUT and DELETE for attention set
    Attention set is returned by default in change details.
    
    Background:
    https://www.gerritcodereview.com/design-docs/attention-set.html
    
    Upcoming further changes:
    - Add to index
    - Extend ReviewInput API to add reviewers
    - Add invariants (e.g. user must be reviewer)
    - Send notifications in *Attention*Op#postUpdate()
    - Consider adding NotificationHandling in AttentionInput
    - Consider adding validation listeners
    
    Change-Id: I52ae870a94852ac98f731fef63f65cd2a7064742

---
+++ java/com/google/gerrit/server/restapi/change/DeleteAttentionSet.java
@@ -0,0 +1,79 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.restapi.change;
+
+import com.google.common.base.Strings;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.extensions.api.changes.AttentionSetInput;
+import com.google.gerrit.extensions.common.AccountInfo;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountLoader;
+import com.google.gerrit.server.account.AccountResolver;
+import com.google.gerrit.server.change.ChangeResource;
+import com.google.gerrit.server.change.RemoveFromAttentionSetOp;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.gerrit.server.update.BatchUpdate;
+import com.google.gerrit.server.update.UpdateException;
+import com.google.gerrit.server.util.time.TimeUtil;
+import com.google.inject.Inject;
+import java.io.IOException;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+
+/** Removes a single user from the attention set. */
+public class DeleteAttentionSet implements RestModifyView<ChangeResource, AttentionSetInput> {
+  private final BatchUpdate.Factory updateFactory;
+  private final AccountResolver accountResolver;
+  private final RemoveFromAttentionSetOp.Factory opFactory;
+  private final AccountLoader.Factory accountLoaderFactory;
+
+  @Inject
+  DeleteAttentionSet(
+      BatchUpdate.Factory updateFactory,
+      AccountResolver accountResolver,
+      RemoveFromAttentionSetOp.Factory opFactory,
+      AccountLoader.Factory accountLoaderFactory) {
+    this.updateFactory = updateFactory;
+    this.accountResolver = accountResolver;
+    this.opFactory = opFactory;
+    this.accountLoaderFactory = accountLoaderFactory;
+  }
+
+  @Override
+  public Response<AccountInfo> apply(ChangeResource resource, AttentionSetInput input)
+      throws RestApiException, PermissionBackendException, IOException, ConfigInvalidException,
+          UpdateException {
+    input.user = Strings.nullToEmpty(input.user).trim();
+    if (input.user.isEmpty()) {
+      throw new BadRequestException("missing field: user");
+    }
+    input.reason = Strings.nullToEmpty(input.reason).trim();
+    if (input.reason.isEmpty()) {
+      throw new BadRequestException("missing field: reason");
+    }
+    Account.Id attentionUserId = accountResolver.resolve(input.user).asUnique().account().id();
+    try (BatchUpdate bu =
+        updateFactory.create(
+            resource.getChange().getProject(), resource.getUser(), TimeUtil.nowTs())) {
+      RemoveFromAttentionSetOp op = opFactory.create(attentionUserId, input.reason);
+      bu.addOp(resource.getId(), op);
+      bu.execute();
+      return Response.ok(accountLoaderFactory.create(true).fillOne(attentionUserId));
+    }
+  }
+}

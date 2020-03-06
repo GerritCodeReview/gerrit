a376311741c6e134ffa617a4d7c0eba596bec270
Tao Zhou <taoalpha@google.com>
2020-03-06 11:17:18 +0100

    Add show_revision_actions event once revisionActions is available
    After https://gerrit-review.googlesource.com/c/gerrit/+/238972,
    revisionActions is fetched separately and `showchange` is no longer wait for that.
    
    this change will add a show_revision_actions event so plugin can hook to this one
    to add / modify / remove revision actions
    
    Bug: Issue 12051
    Change-Id: I009f0b96b7f985bee1d53286c1dea427e41616fa

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-js-api-interface.js
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-js-api-interface.js
@@ -22,6 +22,7 @@
     LABEL_CHANGE: 'labelchange',
     SHOW_CHANGE: 'showchange',
     SUBMIT_CHANGE: 'submitchange',
+    SHOW_REVISION_ACTIONS: 'show_revision_actions',
     COMMIT_MSG_EDIT: 'commitmsgedit',
     COMMENT: 'comment',
     REVERT: 'revert',
@@ -81,6 +82,9 @@
             break;
           case EventType.LABEL_CHANGE:
             this._handleLabelChange(detail);
+            break;
+          case EventType.SHOW_REVISION_ACTIONS:
+            this._handleShowRevisionActions(detail);
             break;
           case EventType.HIGHLIGHTJS_LOADED:
             this._handleHighlightjsLoaded(detail);
@@ -171,6 +175,22 @@
       for (const cb of this._getEventCallbacks(EventType.SHOW_CHANGE)) {
         try {
           cb(change, revision, info);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+
+    /**
+     * @param {!{change: !Object, revisionActions: !Object}} detail
+     */
+    _handleShowRevisionActions(detail) {
+      const registeredCallbacks = this._getEventCallbacks(
+          EventType.SHOW_REVISION_ACTIONS
+      );
+      for (const cb of registeredCallbacks) {
+        try {
+          cb(detail.revisionActions, detail.change);
         } catch (err) {
           console.error(err);
         }

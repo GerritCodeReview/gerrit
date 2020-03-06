d9c8785b3ea53fc90c960f3b9233ca8e9a1abb5e
Dhruv Srivastava <dhruvsri@google.com>
2020-03-06 15:10:47 +0100

    Add support for cherry picking topics
    Change-Id: Idadf4a5ec209c0d801a9d2dfa6af3512d0c671fc

--- polygerrit-ui/app/elements/change/gr-confirm-cherrypick-dialog/gr-confirm-cherrypick-dialog.js
+++ polygerrit-ui/app/elements/change/gr-confirm-cherrypick-dialog/gr-confirm-cherrypick-dialog.js
@@ -18,6 +18,10 @@
   'use strict';
 
   const SUGGESTIONS_LIMIT = 15;
+  const CHERRY_PICK_TYPES = {
+    SINGLE_CHANGE: 1,
+    TOPIC: 2,
+  };
 
   /**
    * @appliesMixin Gerrit.FireMixin
@@ -50,11 +54,25 @@
         commitNum: String,
         message: String,
         project: String,
+        changes: Array,
         _query: {
           type: Function,
           value() {
             return this._getProjectBranchesSuggestions.bind(this);
           },
+        },
+        _showCherryPickTopic: {
+          type: Boolean,
+          value: false,
+        },
+        _changesCount: Number,
+        _cherryPickType: {
+          type: Number,
+          value: CHERRY_PICK_TYPES.SINGLE_CHANGE,
+        },
+        _duplicateProjectChanges: {
+          type: Boolean,
+          value: false,
         },
       };
     }
@@ -63,6 +81,41 @@
       return [
         '_computeMessage(changeStatus, commitNum, commitMessage)',
       ];
+    }
+
+    updateChanges(changes) {
+      this.changes = changes;
+      const projects = {};
+      this._duplicateProjectChanges = false;
+      changes.forEach(change => {
+        if (projects[change.project]) {
+          this._duplicateProjectChanges = true;
+        }
+        projects[change.project] = true;
+      });
+      this._changesCount = changes.length;
+      this._showCherryPickTopic = changes.length > 1;
+    }
+
+    _computeDisableCherryPick(cherryPickType, duplicateProjectChanges) {
+      return cherryPickType === CHERRY_PICK_TYPES.TOPIC &&
+        duplicateProjectChanges;
+    }
+
+    _computeIfSinglecherryPick(cherryPickType) {
+      return cherryPickType === CHERRY_PICK_TYPES.SINGLE_CHANGE;
+    }
+
+    _computeIfCherryPickTopic(cherryPickType) {
+      return cherryPickType === CHERRY_PICK_TYPES.TOPIC;
+    }
+
+    _handlecherryPickSingleChangeClicked(e) {
+      this._cherryPickType = CHERRY_PICK_TYPES.SINGLE_CHANGE;
+    }
+
+    _handlecherryPickTopicClicked(e) {
+      this._cherryPickType = CHERRY_PICK_TYPES.TOPIC;
     }
 
     _computeMessage(changeStatus, commitNum, commitMessage) {
@@ -86,7 +139,8 @@
     _handleConfirmTap(e) {
       e.preventDefault();
       e.stopPropagation();
-      this.fire('confirm', null, {bubbles: false});
+      this.fire('confirm', {type: this._cherryPickType, branch: this.branch},
+          {bubbles: false});
     }
 
     _handleCancelTap(e) {

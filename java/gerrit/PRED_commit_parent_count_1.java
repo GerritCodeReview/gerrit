92c381348ada74250fd25b69ef3f3d76e09a04b0
Lucas Bader <lucas.bader@sap.com>
2020-03-09 15:06:21 +0100

    Add prolog predicate to determine number of parents
    This change introduces a new prolog predicate for the
    number of parents of a given patch set. This can be used to determine
    from within the "rules.pl" file whether a change
    is a merge commit or not.
    
    This can be useful in order to evaluate a different set of
    submit rules for merge commits.
    
    Change-Id: I306163510e201606e936d95caea5ffa0bb957236

---
+++ java/gerrit/PRED_commit_parent_count_1.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2011 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gerrit;
+
+import com.google.gerrit.reviewdb.client.PatchSetInfo;
+import com.google.gerrit.rules.StoredValues;
+
+import com.googlecode.prolog_cafe.exceptions.PrologException;
+import com.googlecode.prolog_cafe.lang.Operation;
+import com.googlecode.prolog_cafe.lang.Predicate;
+import com.googlecode.prolog_cafe.lang.Prolog;
+import com.googlecode.prolog_cafe.lang.IntegerTerm;
+import com.googlecode.prolog_cafe.lang.Term;
+
+/**
+ * Returns the number of parents for a given commit
+ *
+ * <pre>
+ *   'commit_parent_count'
+ * </pre>
+ */
+public class PRED_commit_parent_count_1 extends Predicate.P1 {
+  public PRED_commit_parent_count_1(Term a1, Operation n) {
+    arg1 = a1;
+    cont = n;
+  }
+
+  @Override
+  public Operation exec(Prolog engine) throws PrologException {
+    engine.setB0();
+    Term a1 = arg1.dereference();
+
+    PatchSetInfo psInfo = StoredValues.PATCH_SET_INFO.get(engine);
+
+    if (!a1.unify(new IntegerTerm(psInfo.getParents().size()), engine.trail)) {
+      return engine.fail();
+    }
+    return cont;
+  }
+}

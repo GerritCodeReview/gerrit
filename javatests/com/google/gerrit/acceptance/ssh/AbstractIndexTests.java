7ecee0049f60c019bd35f76a701219250b9c707e
Hamza Kaced <hamza.kaced@ericsson.com>
2020-03-10 13:57:13 -0400

    Add Integration tests for index activate and start
    Bug: Issue 9168
    Change-Id: I7209787beddd38cec3debc84de990d2f55e33351

--- javatests/com/google/gerrit/acceptance/ssh/AbstractIndexTests.java
+++ javatests/com/google/gerrit/acceptance/ssh/AbstractIndexTests.java
@@ -120,6 +120,35 @@ public abstract class AbstractIndexTests extends AbstractDaemonTest {
     assertChangeQuery(change.getChange(), true);
   }
 
+  @Test
+  @GerritConfig(name = "index.onlineUpgrade", value = "false")
+  public void indexStart() throws Exception {
+    configureIndex(server.getTestInjector());
+
+    String[] indexes = {"groups", "accounts", "changes", "projects"};
+    for (String index : indexes) {
+  	  String cmd = Joiner.on(" ").join("gerrit", "index", "start", index);
+  	  adminSshSession.exec(cmd);
+  	  adminSshSession.assertSuccess();
+  	  adminSshSession.exec(cmd + "random");
+  	  adminSshSession.assertFailure();
+    }
+  }
+
+  @Test
+  public void indexActivate() throws Exception {
+	  configureIndex(server.getTestInjector());
+
+	  String[] indexes = {"groups", "accounts", "changes"};
+	  for (String index : indexes) {
+	  String cmd = Joiner.on(" ").join("gerrit", "index", "activate", index);
+	  adminSshSession.exec(cmd);
+	  adminSshSession.assertSuccess();
+	  adminSshSession.exec(cmd + "random");
+	  adminSshSession.assertFailure();
+	  }
+  }
+
   private void assertChangeQuery(ChangeData change, boolean assertTrue) throws Exception {
     List<Integer> ids = query("message:second").stream().map(c -> c._number).collect(toList());
     if (assertTrue) {

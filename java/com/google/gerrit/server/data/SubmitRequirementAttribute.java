3aabd1907f1198983307c91425de04d3c1e88be7
Patrick Hiesel <hiesel@google.com>
2020-03-12 16:10:27 +0100

    Remove 'data' field from SubmitRequirement
    The 'data' field was originally intended to be used to store more
    information that translates into richer messages on the UI.
    
    It's too general and not useful for that, so this commit removes it.
    
    This field was stored in the change index but the parser ignores
    unknown fields. There were not current API users, so we are removing
    the field without a deprecation notice.
    
    Change-Id: If6b392da8ce964f19c1a3c99868b5be70336faab

--- java/com/google/gerrit/server/data/SubmitRequirementAttribute.java
+++ java/com/google/gerrit/server/data/SubmitRequirementAttribute.java
@@ -14,14 +14,11 @@
 
 package com.google.gerrit.server.data;
 
-import java.util.Map;
-
 /**
  * Represents a {@link com.google.gerrit.common.data.SubmitRequirement} that does not depend on
  * Gerrit internal classes, to be serialized
  */
 public class SubmitRequirementAttribute {
-  public Map<String, String> data;
   public String type;
   public String fallbackText;
 }

272483df2c8860cefa77870b2a3edf7d17bc6884
Patrick Hiesel <hiesel@google.com>
2020-03-13 10:04:45 +0100

    Add Content-Length header to /auth-check
    The content-length header is optional as per the HTTP spec. However,
    it enables the client to close the connection by itself instead of
    having the server do that.
    
    We have seen traces where the client keeps the connection open for
    longer than necessary. Adding a content-length header should fix this
    for /auth-check.
    
    Change-Id: Ib3963558f10b9c03dce2054d93061a6c852af34a

--- javatests/com/google/gerrit/acceptance/rest/auth/AuthenticationCheckIT.java
+++ javatests/com/google/gerrit/acceptance/rest/auth/AuthenticationCheckIT.java
@@ -14,6 +14,8 @@
 
 package com.google.gerrit.acceptance.rest.auth;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.RestResponse;
 import com.google.gerrit.acceptance.RestSession;
@@ -24,6 +26,7 @@ public class AuthenticationCheckIT extends AbstractDaemonTest {
   public void authCheck_loggedInUser_returnsOk() throws Exception {
     RestResponse r = adminRestSession.get("/auth-check");
     r.assertNoContent();
+    // Jetty strips Content-Length when status is NO_CONTENT
   }
 
   @Test
@@ -31,5 +34,6 @@ public class AuthenticationCheckIT extends AbstractDaemonTest {
     RestSession anonymous = new RestSession(server, null);
     RestResponse r = anonymous.get("/auth-check");
     r.assertForbidden();
+    assertThat(r.getHeader("Content-Length")).isEqualTo("0");
   }
 }

1fa105162f08b177b7411951016c278823fc848b
Dmitrii Filippov <dmfilippov@google.com>
2020-03-18 15:23:30 +0100

    Always fall back to email address if account.name is not set
    Change-Id: I4e5876ed6ea1909796b47484fb973228984dc1ce

--- polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
+++ polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
@@ -68,7 +68,7 @@ class GrAccountLabel extends mixinBehaviors( [
   }
 
   _computeName(account, config) {
-    return this.getUserName(config, account, true);
+    return this.getUserName(config, account);
   }
 }
 

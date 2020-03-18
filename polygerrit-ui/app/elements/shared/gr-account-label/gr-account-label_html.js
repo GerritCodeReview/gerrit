762c70095761362ad04cfc6b142a194477ad0298
Dmitrii Filippov <dmfilippov@google.com>
2020-03-18 14:34:48 +0100

    Remove status from account-label
    Instead just rely on the status information being shown in the new
    hovercard.
    
    Add an icon instead to the account-label. And use that same icon in the
    hovercard.
    
    Screenshot: https://imgur.com/a/ACH4PBZ
    
    Change-Id: I99c6f36d99e24cbf3a42e2a9b2c0560941c6dda6

--- polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label_html.js
+++ polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label_html.js
@@ -42,6 +42,13 @@ export const htmlTemplate = html`
       .showEmail .email {
         display: inline-block;
       }
+      iron-icon {
+        width: 14px;
+        height: 14px;
+        vertical-align: top;
+        position: relative;
+        top: 2px;
+      }
     </style>
     <span>
       <gr-hovercard-account account="[[account]]" voteable-text="[[voteableText]]"></gr-hovercard-account>
@@ -55,8 +62,7 @@ export const htmlTemplate = html`
           [[_computeEmailStr(account)]]
         </span>
         <template is="dom-if" if="[[account.status]]">
-          (<gr-limited-text disable-tooltip="true" limit="[[_computeStatusTextLength(account, _serverConfig)]]" text="[[account.status]]">
-          </gr-limited-text>)
+          <iron-icon icon="gr-icons:calendar"></iron-icon>
         </template>
       </span>
     </span>

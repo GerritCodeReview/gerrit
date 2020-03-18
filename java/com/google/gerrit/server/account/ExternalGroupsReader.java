4ca18e71fcd923e8b81b704cb311761f2ac84bf0
Youssef Elghareeb <ghareeb@google.com>
2020-03-19 00:13:26 +0100

    Modify the groups external cache key to reflect the state of NoteDb
    The current groups external cache has only a single key "EXTERNAL_NAME"
    that maps to the list of all external groups. This key is evicted
    whenever an external group is updated/deleted (causing the whole cache
    to be evicted).
    
    In this change we modify this behavior by changing the cache key to a
    string that reflects the NoteDb state. Since external groups are stored
    in the "subgroups" file pointed to by the internal groups refs, then a
    state can be calculated as the concatenation of all internal groups
    refs. This is because any change (addition/deletion) to an external
    group as a subgroup of an internal group will cause the internal's
    group ref to be changed.
    
    This change is needed as a preparation to serialize the groups external
    cache for better performance.
    
    Change-Id: Iac48415589eabec479dc658eaa20c2dd4c7b4932

---
+++ java/com/google/gerrit/server/account/ExternalGroupsReader.java
@@ -0,0 +1,61 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.inject.Inject;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+
+public class ExternalGroupsReader {
+  private final GitRepositoryManager repoManager;
+  private final AllUsersName allUsersName;
+
+  @Inject
+  ExternalGroupsReader(
+      GitRepositoryManager repoManager, AllUsersName allUsersName, MetricMaker metricMaker) {
+    this.repoManager = repoManager;
+    this.allUsersName = allUsersName;
+  }
+
+  @VisibleForTesting
+  public String readRevision() throws IOException, ConfigInvalidException {
+    try (Repository repo = repoManager.openRepository(allUsersName)) {
+      return readRevision(repo);
+    }
+  }
+
+  public static String readRevision(Repository repo) throws IOException, ConfigInvalidException {
+    List<Ref> groupsRefs = repo.getRefDatabase().getRefsByPrefix("refs/groups");
+    ByteBuffer buf = ByteBuffer.allocate(groupsRefs.size() * Constants.OBJECT_ID_LENGTH);
+    for (Ref groupRef : repo.getRefDatabase().getRefsByPrefix("refs/groups")) {
+      groupRef.getObjectId().copyRawTo(buf);
+    }
+    Hasher h = Hashing.murmur3_128().newHasher();
+    h.putBytes(buf.array());
+    return h.hash().toString();
+  }
+}

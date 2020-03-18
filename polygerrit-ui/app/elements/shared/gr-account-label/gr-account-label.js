11a804fdc3faf9ed0a45eedca05751334309f3e4
Dmitrii Filippov <dmfilippov@google.com>
2020-03-18 15:43:56 +0100

    Add reviewers column to dashboard
    Change-Id: Iebaa8387df630b7ecac93b4586f1e8a49990baf5

--- polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
+++ polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
@@ -60,6 +60,10 @@ class GrAccountLabel extends mixinBehaviors( [
         type: Boolean,
         value: false,
       },
+      hideStatus: {
+        type: Boolean,
+        value: false,
+      },
       _serverConfig: {
         type: Object,
         value: null,

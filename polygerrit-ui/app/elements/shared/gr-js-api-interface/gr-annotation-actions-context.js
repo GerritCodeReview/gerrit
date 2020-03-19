8598b0f642add7c07db17ae00a44020fcac43702
Dmitrii Filippov <dmfilippov@google.com>
2020-03-19 17:25:38 +0100

    Get rid of global GrAnnotation
    * Replace the global GrAnnotation variable with named imports.
    * Update gr-app-global-var-init.js
    
    Change-Id: I2396d5972ca5a724599452d54a1991947d149042

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-annotation-actions-context.js
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-annotation-actions-context.js
@@ -14,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import {GrAnnotation} from '../../diff/gr-diff-highlight/gr-annotation.js';
+
 (function(window) {
   'use strict';
 

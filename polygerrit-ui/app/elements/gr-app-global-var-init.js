0e050452feb1d2f718cdf74b91d9cb284511b4bf
Dmitrii Filippov <dmfilippov@google.com>
2020-03-19 16:00:26 +0100

    Get rid off global GrDisplayNameUtils
    * Replace the global GrDisplayNameUtils variable with named imports.
    * Introduce the gr-app-global-var-init.js shim to setup global
      GrDisplayNameUtils for plugins.
    * Call the shim to setup global variables (in the gr-app.js file)
    
    Change-Id: I441f4558f3a93a901b342d65fc593fbf1c669d1e

---
+++ polygerrit-ui/app/elements/gr-app-global-var-init.js
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview This file is a backwards-compatibility shim.
+ * Before Polygerrit converted to ES Modules, it exposes some variables out onto
+ * the global namespace. Plugins can depend on these variables and we must
+ * expose these variables until plugins switch to direct import from polygerrit.
+ */
+
+import {GrDisplayNameUtils} from '../scripts/gr-display-name-utils/gr-display-name-utils.js';
+
+export function initGlobalVariables() {
+  window.GrDisplayNameUtils = GrDisplayNameUtils;
+}

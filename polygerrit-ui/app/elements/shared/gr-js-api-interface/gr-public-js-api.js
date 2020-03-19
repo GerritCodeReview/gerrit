d64387bd6876163b261603c547a3a552eb1aa1cd
Dmitrii Filippov <dmfilippov@google.com>
2020-03-19 17:34:43 +0100

    Get rid of global GrAttributeHelper
    * Replace the global GrAttributeHelper variable with named imports.
    * Update gr-app-global-var-init.js
    
    Change-Id: I02500a5fc3b8157e107850e1c83b332b8c089522

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-public-js-api.js
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-public-js-api.js
@@ -14,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import {GrAttributeHelper} from '../../plugins/gr-attribute-helper/gr-attribute-helper.js';
+
 (function(window) {
   'use strict';
 

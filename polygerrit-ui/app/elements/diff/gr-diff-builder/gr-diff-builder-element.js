4bbac8a1e558f8bf861eb841b6261cf6475e23ab
Dmitrii Filippov <dmfilippov@google.com>
2020-03-19 17:25:38 +0100

    Get rid of global GrAnnotation
    * Replace the global GrAnnotation variable with named imports.
    * Update gr-app-global-var-init.js
    
    Change-Id: I2396d5972ca5a724599452d54a1991947d149042

--- polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-element.js
+++ polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-element.js
@@ -24,7 +24,6 @@ import '../gr-ranged-comment-layer/gr-ranged-comment-layer.js';
 import '../../../scripts/util.js';
 import '../gr-diff/gr-diff-line.js';
 import '../gr-diff/gr-diff-group.js';
-import '../gr-diff-highlight/gr-annotation.js';
 import './gr-diff-builder.js';
 import './gr-diff-builder-side-by-side.js';
 import './gr-diff-builder-unified.js';
@@ -36,6 +35,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-diff-builder-element_html.js';
+import {GrAnnotation} from '../gr-diff-highlight/gr-annotation.js';
 
 const DiffViewMode = {
   SIDE_BY_SIDE: 'SIDE_BY_SIDE',

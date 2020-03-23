f94424c05ba5a7007517021c9ab5ed46cfe56cd0
Dmitrii Filippov <dmfilippov@google.com>
2020-03-23 20:15:45 +0100

    Get rid of global GrDiffLine
    * Replace the global GrDiffLine variable with named imports.
    * Update gr-app-global-var-init.js
    
    Change-Id: I7c9bc17bfa7c0d9bbd745219cd091d94cdb4cac4

--- polygerrit-ui/app/elements/diff/gr-diff/gr-diff-group.js
+++ polygerrit-ui/app/elements/diff/gr-diff/gr-diff-group.js
@@ -14,6 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import {GrDiffLine} from './gr-diff-line.js';
+
 (function(window) {
   'use strict';
 

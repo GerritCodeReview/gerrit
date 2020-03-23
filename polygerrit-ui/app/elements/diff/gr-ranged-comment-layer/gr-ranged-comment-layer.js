2d17fababe50a0cfe45adee20ae3f143d2535351
Dmitrii Filippov <dmfilippov@google.com>
2020-03-23 20:15:45 +0100

    Get rid of global GrDiffLine
    * Replace the global GrDiffLine variable with named imports.
    * Update gr-app-global-var-init.js
    
    Change-Id: I7c9bc17bfa7c0d9bbd745219cd091d94cdb4cac4

--- polygerrit-ui/app/elements/diff/gr-ranged-comment-layer/gr-ranged-comment-layer.js
+++ polygerrit-ui/app/elements/diff/gr-ranged-comment-layer/gr-ranged-comment-layer.js
@@ -21,6 +21,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-ranged-comment-layer_html.js';
+import {GrDiffLine} from '../gr-diff/gr-diff-line.js';
 
 // Polymer 1 adds # before array's key, while Polymer 2 doesn't
 const HOVER_PATH_PATTERN = /^(commentRanges\.\#?\d+)\.hovering$/;

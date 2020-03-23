c57b9a57701d1d1970992ff0f80bbcd459e660de
Dmitrii Filippov <dmfilippov@google.com>
2020-03-23 20:44:31 +0100

    Get rid of global GrDiffBuilder
    * Replace the global GrDiffBuilder variable with named imports.
    * Update gr-app-global-var-init.js
    
    Change-Id: I0d07cd3ef56bf2fefebbfc29d31ed515e13b9d27

--- polygerrit-ui/app/elements/diff/gr-diff-host/gr-diff-host.js
+++ polygerrit-ui/app/elements/diff/gr-diff-host/gr-diff-host.js
@@ -30,6 +30,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-diff-host_html.js';
+import {GrDiffBuilder} from '../gr-diff-builder/gr-diff-builder.js';
 
 const MSG_EMPTY_BLAME = 'No blame information for this diff.';
 

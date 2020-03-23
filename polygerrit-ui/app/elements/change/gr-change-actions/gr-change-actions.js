942e57e6d08863c5395777ebc26ec36fbc7ff045
Dhruv Srivastava <dhruvsri@google.com>
2020-03-23 10:19:52 +0100

    Escape spaces in topic when querying for changes.
    The search is still broken if there is a " in the topic.
    
    Change-Id: I2ef6061bd36a6b98232a4d843c9308c3754d1a34

--- polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.js
+++ polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.js
@@ -969,7 +969,7 @@ class GrChangeActions extends mixinBehaviors( [
   }
 
   showRevertDialog() {
-    const query = 'submissionid:' + this.change.submission_id;
+    const query = `submissionid: "${this.change.submission_id}"`;
     /* A chromium plugin expects that the modifyRevertMsg hook will only
     be called after the revert button is pressed, hence we populate the
     revert dialog after revert button is pressed. */

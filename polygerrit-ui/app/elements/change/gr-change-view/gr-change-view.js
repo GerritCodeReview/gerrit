082a101910705b4ec2790fafc786ad7587f41a6d
Dmitrii Filippov <dmfilippov@google.com>
2020-03-24 11:58:55 +0100

    GrChangeReplyInterface, GrEditConstants, GrFileListConstants
    Change-Id: I8f19739784789d1f26b893b9faab9d6a279bfc60

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -25,7 +25,6 @@ import '../../../styles/shared-styles.js';
 import '../../core/gr-navigation/gr-navigation.js';
 import '../../core/gr-reporting/gr-reporting.js';
 import '../../diff/gr-comment-api/gr-comment-api.js';
-import '../../edit/gr-edit-constants.js';
 import '../../plugins/gr-endpoint-decorator/gr-endpoint-decorator.js';
 import '../../plugins/gr-endpoint-param/gr-endpoint-param.js';
 import '../../shared/gr-account-link/gr-account-link.js';
@@ -62,6 +61,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-change-view_html.js';
+import {GrEditConstants} from '../../edit/gr-edit-constants.js';
 
 const CHANGE_ID_ERROR = {
   MISMATCH: 'mismatch',

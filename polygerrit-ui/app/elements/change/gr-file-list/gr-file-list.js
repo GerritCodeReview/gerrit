7a0bfe8c62ee7750d77f731df2295be48a641b96
Dmitrii Filippov <dmfilippov@google.com>
2020-03-24 16:31:04 +0100

    Use imported AsyncForeachBehavior
    This change removes usage of global Gerrit.AsyncForeachBehavior and
    replace it with direct import. Also, the AsyncForeachMixin is removed,
    because it was introduced as a workaround for polylint tests and is no
    longer needed.
    
    For compatibility reason the global Gerrit.AsyncForeachBehavior is
    assigned, but later it can be removed.
    
    Change-Id: I1ae9acc1f07beb588aa52739b52751185caa60de

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.js
@@ -16,7 +16,6 @@
  */
 import '../../../scripts/bundled-polymer.js';
 
-import '../../../behaviors/async-foreach-behavior/async-foreach-behavior.js';
 import '../../../behaviors/dom-util-behavior/dom-util-behavior.js';
 import '../../../behaviors/fire-behavior/fire-behavior.js';
 import '../../../behaviors/keyboard-shortcut-behavior/keyboard-shortcut-behavior.js';
@@ -44,6 +43,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-file-list_html.js';
+import {AsyncForeachBehavior} from '../../../behaviors/async-foreach-behavior/async-foreach-behavior.js';
 
 // Maximum length for patch set descriptions.
 const PATCH_DESC_MAX_LENGTH = 500;
@@ -70,7 +70,6 @@ const FileStatus = {
 };
 
 /**
- * @appliesMixin Gerrit.AsyncForeachMixin
  * @appliesMixin Gerrit.DomUtilMixin
  * @appliesMixin Gerrit.FireMixin
  * @appliesMixin Gerrit.KeyboardShortcutMixin
@@ -79,7 +78,7 @@ const FileStatus = {
  * @extends Polymer.Element
  */
 class GrFileList extends mixinBehaviors( [
-  Gerrit.AsyncForeachBehavior,
+  AsyncForeachBehavior,
   Gerrit.DomUtilBehavior,
   Gerrit.FireBehavior,
   Gerrit.KeyboardShortcutBehavior,

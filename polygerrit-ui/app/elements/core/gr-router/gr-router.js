9627bd695e390b86f35b0fc5ca7ded80489a172b
Dmitrii Filippov <dmfilippov@google.com>
2020-03-24 16:20:44 +0100

    moment, page
    Change-Id: I3e084539332efd8344fb76b56b405ffd69bec03f

--- polygerrit-ui/app/elements/core/gr-router/gr-router.js
+++ polygerrit-ui/app/elements/core/gr-router/gr-router.js
@@ -28,7 +28,6 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import page from 'page/page.mjs';
-self.page = page;
 import {htmlTemplate} from './gr-router_html.js';
 
 const RoutePattern = {

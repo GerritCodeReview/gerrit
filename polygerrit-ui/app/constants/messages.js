75503caf3fa25ac59c9176fc3de4cfdce805017f
Tao Zhou <taoalpha@google.com>
2020-04-01 20:48:49 +0200

    Support deep linking on tabs
    This change includes:
    1. added shared constants folder for constants and messages
    2. refactor the change-view page on switching tab behavior
    3. replace hardcoded logic for findings tab in gr-thread-list
    with emptyThreadMsg
    4. add a new middleware in gr-router to parse the querystring
    to queryMap
    5. allow active primary tab and secondary tab with url param
    
    A follow up change will support auto-update on url once tab switched,
    and also use the link in other places like submit dialog,
    this change itself should not affect any existing flow without the
    follow up change.
    
    Change-Id: I63e03a9acfb5ebab87fe8c256dcc027abfa876ea

---
+++ polygerrit-ui/app/constants/messages.js
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @desc Default message shown when no threads in gr-thread-list */
+export const NO_THREADS_MESSAGE =
+  'There are no inline comment threads on any diff for this change.';
+
+/** @desc Message shown when no threads in gr-thread-list for robot comments */
+export const NO_ROBOT_COMMENTS_THREADS_MESSAGE =
+  'There are no findings for this patchset.';
+

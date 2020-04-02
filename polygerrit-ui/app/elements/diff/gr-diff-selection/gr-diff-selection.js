d6fbd28fe75dcb9d901657f0e3d610108690177f
Dmitrii Filippov <dmfilippov@google.com>
2020-04-02 13:36:10 +0200

    Use imported DomUtilsBehavior
    This change removes usage of global Gerrit.DomUtilsBehavior and
    replace it with direct import. Also, the DomUtilMixin is removed,
    because it was introduced as a workaround for polylint tests and is no
    longer needed.
    
    Change-Id: I5cf0a24393c11e9bbb1eba8e68314483f52aef9c

--- polygerrit-ui/app/elements/diff/gr-diff-selection/gr-diff-selection.js
+++ polygerrit-ui/app/elements/diff/gr-diff-selection/gr-diff-selection.js
@@ -16,7 +16,6 @@
  */
 import '../../../scripts/bundled-polymer.js';
 
-import '../../../behaviors/dom-util-behavior/dom-util-behavior.js';
 import '../../../styles/shared-styles.js';
 import '../../../scripts/util.js';
 import '../gr-diff-highlight/gr-range-normalizer.js';
@@ -27,6 +26,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-diff-selection_html.js';
+import {DomUtilBehavior} from '../../../behaviors/dom-util-behavior/dom-util-behavior.js';
 
 /**
  * Possible CSS classes indicating the state of selection. Dynamically added/
@@ -42,11 +42,10 @@ const SelectionClass = {
 const getNewCache = () => { return {left: null, right: null}; };
 
 /**
- * @appliesMixin Gerrit.DomUtilMixin
  * @extends Polymer.Element
  */
 class GrDiffSelection extends mixinBehaviors( [
-  Gerrit.DomUtilBehavior,
+  DomUtilBehavior,
 ], GestureEventListeners(
     LegacyElementMixin(
         PolymerElement))) {

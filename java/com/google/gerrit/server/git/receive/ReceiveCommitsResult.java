ffaa440ec386f575bc974df5ab6fcd7668647dac
Patrick Hiesel <hiesel@google.com>
2020-04-02 11:12:08 +0200

    Refactor AsyncReceiveCommits and ReceiveCommits
    This commit refactors *ReceiveCommits. The main goal is
    to move error handling and metric reporting to a central
    place.
    
    As in other parts of Gerrit, we use unchecked exceptions
    for unrecoverable failures. They are handled in the
    PreReceiveHook and translated into a message on the Git
    wire.
    
    We also make ReceiveCommits return a result instead of
    accessing the result from a private member.
    
    Having both unrecoverable exceptions as well as the result
    of the operation - in case there we no exceptions - handled
    in a single spot allows googlesource.com to report more
    accurate metrics.
    
    To have ReceiveCommits return a result, we have to use a
    Callable. WorkQueue isn't ready for that and it seems
    thorny to build support for that. If we invest in that
    part of the code, we should rather work on reducing
    or deleting custom threadding code (WorkQueue,
    ProjectRunnable, ...) instead of further adding to it.
    
    Change-Id: I6a4b2731f8de77cc453b58f931a9ec7a3f5b6612

---
+++ java/com/google/gerrit/server/git/receive/ReceiveCommitsResult.java
@@ -0,0 +1,82 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git.receive;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.google.gerrit.entities.Change;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/** Keeps track of the change IDs thus far updated by {@link ReceiveCommits}. */
+@AutoValue
+public abstract class ReceiveCommitsResult {
+  /** Status of a change. Used to aggregate metrics. */
+  public enum ChangeStatus {
+    CREATED,
+    REPLACED,
+    AUTOCLOSED,
+  }
+
+  /**
+   * Returns change IDs of the given type for which the BatchUpdate succeeded, or empty list if
+   * there are none.
+   */
+  public abstract ImmutableMap<ChangeStatus, ImmutableSet<Change.Id>> changes();
+
+  /** Indicate that the ReceiveCommits call involved a magic branch, such as {@code refs/for/}. */
+  public abstract boolean magicPush();
+
+  public static Builder builder() {
+    return new AutoValue_ReceiveCommitsResult.Builder().magicPush(false);
+  }
+
+  public static ReceiveCommitsResult empty() {
+    return builder().build();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    private EnumMap<ChangeStatus, Set<Change.Id>> changes;
+
+    Builder() {
+      changes = Maps.newEnumMap(ChangeStatus.class);
+      Arrays.stream(ChangeStatus.values()).forEach(k -> changes.put(k, new HashSet<>()));
+    }
+
+    /** Record a change ID update as having completed. */
+    public Builder addChange(ChangeStatus key, Change.Id id) {
+      changes.get(key).add(id);
+      return this;
+    }
+
+    public abstract Builder magicPush(boolean isMagicPush);
+
+    public ReceiveCommitsResult build() {
+      ImmutableMap.Builder<ChangeStatus, ImmutableSet<Change.Id>> c = ImmutableMap.builder();
+      changes.entrySet().forEach(e -> c.put(e.getKey(), ImmutableSet.copyOf(e.getValue())));
+      changes(c.build());
+      return autoBuild();
+    }
+
+    protected abstract Builder changes(ImmutableMap<ChangeStatus, ImmutableSet<Change.Id>> changes);
+
+    protected abstract ReceiveCommitsResult autoBuild();
+  }
+}

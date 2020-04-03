7d2b66f73ac0f5c2922d27c726768d73024c57bc
Dmitrii Filippov <dmfilippov@google.com>
2020-04-03 17:44:57 +0200

    Use imported TooltipBehavior
    This change removes usage of global Gerrit.TooltipBehavior and
    replace it with direct import. Also, the TooltipMixin is removed,
    because it was introduced as a workaround for polylint tests and is no
    longer needed.
    
    Change-Id: Ie9da7fa5b01faeb0d5a358be79c511d8fc30b13b

--- polygerrit-ui/app/elements/shared/gr-tooltip-content/gr-tooltip-content.js
+++ polygerrit-ui/app/elements/shared/gr-tooltip-content/gr-tooltip-content.js
@@ -17,19 +17,18 @@
 import '../../../scripts/bundled-polymer.js';
 
 import '../gr-icons/gr-icons.js';
-import '../../../behaviors/gr-tooltip-behavior/gr-tooltip-behavior.js';
 import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-tooltip-content_html.js';
+import {TooltipBehavior} from '../../../behaviors/gr-tooltip-behavior/gr-tooltip-behavior.js';
 
 /**
- * @appliesMixin Gerrit.TooltipMixin
  * @extends Polymer.Element
  */
 class GrTooltipContent extends mixinBehaviors( [
-  Gerrit.TooltipBehavior,
+  TooltipBehavior,
 ], GestureEventListeners(
     LegacyElementMixin(
         PolymerElement))) {

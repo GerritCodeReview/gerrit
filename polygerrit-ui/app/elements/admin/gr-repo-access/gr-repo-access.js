fef55a9faeddc4e55122fb65512f39c7ab15dca7
Dmitrii Filippov <dmfilippov@google.com>
2020-04-03 17:54:22 +0200

    Use imported URLEncodingBehavior
    This change removes usage of global Gerrit.URLEncodingBehavior and
    replace it with direct import. Also, the URLEncodingMixin is removed,
    because it was introduced as a workaround for polylint tests and is no
    longer needed.
    
    Change-Id: Iea43f25d9bcf008d79ca1078681868303ccbb926

--- polygerrit-ui/app/elements/admin/gr-repo-access/gr-repo-access.js
+++ polygerrit-ui/app/elements/admin/gr-repo-access/gr-repo-access.js
@@ -16,7 +16,6 @@
  */
 import '../../../scripts/bundled-polymer.js';
 
-import '../../../behaviors/gr-url-encoding-behavior/gr-url-encoding-behavior.js';
 import '../../../styles/gr-menu-page-styles.js';
 import '../../../styles/gr-subpage-styles.js';
 import '../../../styles/shared-styles.js';
@@ -32,6 +31,7 @@ import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-repo-access_html.js';
 import {BaseUrlBehavior} from '../../../behaviors/base-url-behavior/base-url-behavior.js';
 import {AccessBehavior} from '../../../behaviors/gr-access-behavior/gr-access-behavior.js';
+import {URLEncodingBehavior} from '../../../behaviors/gr-url-encoding-behavior/gr-url-encoding-behavior.js';
 
 const Defs = {};
 
@@ -84,13 +84,12 @@ Defs.sections;
 Defs.projectAccessInput;
 
 /**
- * @appliesMixin Gerrit.URLEncodingMixin
  * @extends Polymer.Element
  */
 class GrRepoAccess extends mixinBehaviors( [
   AccessBehavior,
   BaseUrlBehavior,
-  Gerrit.URLEncodingBehavior,
+  URLEncodingBehavior,
 ], GestureEventListeners(
     LegacyElementMixin(
         PolymerElement))) {

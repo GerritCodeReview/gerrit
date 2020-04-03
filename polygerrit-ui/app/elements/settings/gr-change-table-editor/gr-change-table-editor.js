e3defbd7792ca0cc861b044d961289f33b655939
Dmitrii Filippov <dmfilippov@google.com>
2020-04-03 16:36:05 +0200

    Use imported ChangeTableBehavior
    This change removes usage of global Gerrit.ChangeTableBehavior and
    replace it with direct import. Also, the ChangeTableMixin is removed,
    because it was introduced as a workaround for polylint tests and is no
    longer needed.
    
    Change-Id: I831d7ba8ea680fe7d4f076e5bed47addd3406946

--- polygerrit-ui/app/elements/settings/gr-change-table-editor/gr-change-table-editor.js
+++ polygerrit-ui/app/elements/settings/gr-change-table-editor/gr-change-table-editor.js
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '../../../behaviors/gr-change-table-behavior/gr-change-table-behavior.js';
-
 import '../../../scripts/bundled-polymer.js';
 import '../../shared/gr-button/gr-button.js';
 import '../../shared/gr-date-formatter/gr-date-formatter.js';
@@ -28,13 +26,13 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-change-table-editor_html.js';
+import {ChangeTableBehavior} from '../../../behaviors/gr-change-table-behavior/gr-change-table-behavior.js';
 
 /**
- * @appliesMixin Gerrit.ChangeTableMixin
  * @extends Polymer.Element
  */
 class GrChangeTableEditor extends mixinBehaviors( [
-  Gerrit.ChangeTableBehavior,
+  ChangeTableBehavior,
 ], GestureEventListeners(
     LegacyElementMixin(
         PolymerElement))) {

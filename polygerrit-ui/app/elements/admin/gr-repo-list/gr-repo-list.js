343390c43d10ce4326d1964dd15ff124dd9bcfe6
Dmitrii Filippov <dmfilippov@google.com>
2020-04-03 17:20:35 +0200

    Use imported ListViewBehavior
    This change removes usage of global Gerrit.ListViewBehavior and
    replace it with direct import. Also, the ListViewMixin is removed,
    because it was introduced as a workaround for polylint tests and is no
    longer needed.
    
    Change-Id: If419e6b55c6de6bf87a7aa54e195e70f7c832f72

--- polygerrit-ui/app/elements/admin/gr-repo-list/gr-repo-list.js
+++ polygerrit-ui/app/elements/admin/gr-repo-list/gr-repo-list.js
@@ -16,7 +16,6 @@
  */
 import '../../../scripts/bundled-polymer.js';
 
-import '../../../behaviors/gr-list-view-behavior/gr-list-view-behavior.js';
 import '../../../styles/gr-table-styles.js';
 import '../../../styles/shared-styles.js';
 import '../../shared/gr-dialog/gr-dialog.js';
@@ -29,13 +28,14 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-repo-list_html.js';
+import {ListViewBehavior} from '../../../behaviors/gr-list-view-behavior/gr-list-view-behavior.js';
 
 /**
- * @appliesMixin Gerrit.ListViewMixin
+ * @appliesMixin ListViewMixin
  * @extends Polymer.Element
  */
 class GrRepoList extends mixinBehaviors( [
-  Gerrit.ListViewBehavior,
+  ListViewBehavior,
 ], GestureEventListeners(
     LegacyElementMixin(
         PolymerElement))) {

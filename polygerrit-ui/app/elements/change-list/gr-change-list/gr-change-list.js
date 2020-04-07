db30259fa64e40d741a2d7de5298deb00de444d2
Dmitrii Filippov <dmfilippov@google.com>
2020-04-07 09:59:22 +0200

    Gerrit._pluginLoader, Gerrit._endpoints, test reset plugins
    Change-Id: Ifc3454c293fc19830f12088168e83748f67d7cff

--- polygerrit-ui/app/elements/change-list/gr-change-list/gr-change-list.js
+++ polygerrit-ui/app/elements/change-list/gr-change-list/gr-change-list.js
@@ -35,6 +35,7 @@ import {URLEncodingBehavior} from '../../../behaviors/gr-url-encoding-behavior/g
 import {KeyboardShortcutBehavior} from '../../../behaviors/keyboard-shortcut-behavior/keyboard-shortcut-behavior.js';
 import {RESTClientBehavior} from '../../../behaviors/rest-client-behavior/rest-client-behavior.js';
 import {GerritNav} from '../../core/gr-navigation/gr-navigation.js';
+import {_endpoints} from '../../shared/gr-js-api-interface/gr-plugin-endpoints.js';
 
 const NUMBER_FIXED_COLUMNS = 3;
 const CLOSED_STATUS = ['MERGED', 'ABANDONED'];
@@ -168,7 +169,7 @@ class GrChangeList extends mixinBehaviors( [
   attached() {
     super.attached();
     Gerrit.awaitPluginsLoaded().then(() => {
-      this._dynamicHeaderEndpoints = Gerrit._endpoints.getDynamicEndpoints(
+      this._dynamicHeaderEndpoints = _endpoints.getDynamicEndpoints(
           'change-list-header');
     });
   }

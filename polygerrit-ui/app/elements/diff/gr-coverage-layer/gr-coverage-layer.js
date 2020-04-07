681cdfe090da113cf17253e720a5d0db12a42be8
Dmitrii Filippov <dmfilippov@google.com>
2020-04-07 13:39:25 +0200

    SUGGESTIONS_PROVIDERS_USERS_TYPES, RevisionInfo, CoverageType
    Change-Id: I74cf944f7cc2675d1c80deb06091ef1ff07f40e0

--- polygerrit-ui/app/elements/diff/gr-coverage-layer/gr-coverage-layer.js
+++ polygerrit-ui/app/elements/diff/gr-coverage-layer/gr-coverage-layer.js
@@ -16,17 +16,17 @@
  */
 import '../../../scripts/bundled-polymer.js';
 
-import '../../../types/types.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-coverage-layer_html.js';
+import {CoverageType} from '../../../types/types.js';
 
 const TOOLTIP_MAP = new Map([
-  [Gerrit.CoverageType.COVERED, 'Covered by tests.'],
-  [Gerrit.CoverageType.NOT_COVERED, 'Not covered by tests.'],
-  [Gerrit.CoverageType.PARTIALLY_COVERED, 'Partially covered by tests.'],
-  [Gerrit.CoverageType.NOT_INSTRUMENTED, 'Not instrumented by any tests.'],
+  [CoverageType.COVERED, 'Covered by tests.'],
+  [CoverageType.NOT_COVERED, 'Not covered by tests.'],
+  [CoverageType.PARTIALLY_COVERED, 'Partially covered by tests.'],
+  [CoverageType.NOT_INSTRUMENTED, 'Not instrumented by any tests.'],
 ]);
 
 /** @extends Polymer.Element */

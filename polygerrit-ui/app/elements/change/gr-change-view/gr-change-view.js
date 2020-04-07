f6e23325c394c439c5afb9ceeb3b5f50210d202a
Dmitrii Filippov <dmfilippov@google.com>
2020-04-07 12:14:11 +0200

    gr-gerrit.js
    Change-Id: I240680b46956e5d50eaf3857b14e331596c44e01

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -63,6 +63,7 @@ import {GrCountStringFormatter} from '../../shared/gr-count-string-formatter/gr-
 import {util} from '../../../scripts/util.js';
 import {GerritNav} from '../../core/gr-navigation/gr-navigation.js';
 import {_endpoints} from '../../shared/gr-js-api-interface/gr-plugin-endpoints.js';
+import {_pluginLoader} from '../../shared/gr-js-api-interface/gr-plugin-loader.js';
 
 import {PrimaryTabs, SecondaryTabs} from '../../../constants/constants.js';
 import {NO_ROBOT_COMMENTS_THREADS_MSG} from '../../../constants/messages.js';
@@ -484,7 +485,7 @@ class GrChangeView extends mixinBehaviors( [
       this._setDiffViewMode();
     });
 
-    Gerrit.awaitPluginsLoaded()
+    _pluginLoader.awaitPluginsLoaded()
         .then(() => {
           this._dynamicTabHeaderEndpoints =
           _endpoints.getDynamicEndpoints('change-view-tab-header');
@@ -1165,7 +1166,7 @@ class GrChangeView extends mixinBehaviors( [
   }
 
   _maybeShowRevertDialog() {
-    Gerrit.awaitPluginsLoaded()
+    _pluginLoader.awaitPluginsLoaded()
         .then(this._getLoggedIn.bind(this))
         .then(loggedIn => {
           if (!loggedIn || !this._change ||

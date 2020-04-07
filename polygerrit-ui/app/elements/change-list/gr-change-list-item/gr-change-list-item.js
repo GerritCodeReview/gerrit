c1a1e5c63b4f413ff6bf293ef96f7c29730b3cb5
Dmitrii Filippov <dmfilippov@google.com>
2020-04-07 12:14:11 +0200

    gr-gerrit.js
    Change-Id: I240680b46956e5d50eaf3857b14e331596c44e01

--- polygerrit-ui/app/elements/change-list/gr-change-list-item/gr-change-list-item.js
+++ polygerrit-ui/app/elements/change-list/gr-change-list-item/gr-change-list-item.js
@@ -38,6 +38,7 @@ import {URLEncodingBehavior} from '../../../behaviors/gr-url-encoding-behavior/g
 import {RESTClientBehavior} from '../../../behaviors/rest-client-behavior/rest-client-behavior.js';
 import {GerritNav} from '../../core/gr-navigation/gr-navigation.js';
 import {_endpoints} from '../../shared/gr-js-api-interface/gr-plugin-endpoints.js';
+import {_pluginLoader} from '../../shared/gr-js-api-interface/gr-plugin-loader.js';
 
 const CHANGE_SIZE = {
   XS: 10,
@@ -98,7 +99,7 @@ class GrChangeListItem extends mixinBehaviors( [
   /** @override */
   attached() {
     super.attached();
-    Gerrit.awaitPluginsLoaded().then(() => {
+    _pluginLoader.awaitPluginsLoaded().then(() => {
       this._dynamicCellEndpoints = _endpoints.getDynamicEndpoints(
           'change-list-item-cell');
     });

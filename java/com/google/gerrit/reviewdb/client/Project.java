18a181125daf5c7b63871a1f2af3c4fcc7bf65c6
David Pursehouse <dpursehouse@collab.net>
2020-04-13 15:07:34 +0900

    Project: Add implementation of toString
    Change-Id: Ie489c7b43a7752ffbae08f03273aec3798401503

--- java/com/google/gerrit/reviewdb/client/Project.java
+++ java/com/google/gerrit/reviewdb/client/Project.java
@@ -22,6 +22,7 @@ import com.google.gwtorm.client.StringKey;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /** Projects match a source code repository managed by Gerrit */
 public final class Project {
@@ -250,5 +251,10 @@ public final class Project {
   /** Sets the {@code ObjectId} as 40 digit hex of {@code refs/meta/config}'s HEAD. */
   public void setConfigRefState(String state) {
     configRefState = state;
+  }
+
+  @Override
+  public String toString() {
+    return Optional.of(getName()).orElse("<null>");
   }
 }

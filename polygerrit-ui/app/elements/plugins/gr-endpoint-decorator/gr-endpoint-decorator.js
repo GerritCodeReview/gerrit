7bb2eef5e6099f9f8f227dfb6352fac928a145d8
Milutin Kristofic <milutin@google.com>
2020-04-15 14:58:51 +0200

    Fix for Memory leak in gr-plugin-endpoints
    After 5 visited Change Pages, callback hold 14MB in js heap. It was 33%
    of whole js heap. It holds Detached DOM Elements. After fix we
    unsubscribe when element is detached.
    
    Bug: Issue 12385
    Change-Id: Ie7d8c994bd0f0b94a46ac38903b1595557d2ccf5

--- polygerrit-ui/app/elements/plugins/gr-endpoint-decorator/gr-endpoint-decorator.js
+++ polygerrit-ui/app/elements/plugins/gr-endpoint-decorator/gr-endpoint-decorator.js
@@ -46,6 +46,7 @@
       for (const [el, domHook] of this._domHooks) {
         domHook.handleInstanceDetached(el);
       }
+      Gerrit._endpoints.onDetachedEndpoint(this.name, this._endpointCallBack);
     },
 
     _import(url) {
@@ -137,7 +138,8 @@
     },
 
     ready() {
-      Gerrit._endpoints.onNewEndpoint(this.name, this._initModule.bind(this));
+      this._endpointCallBack = this._initModule.bind(this);
+      Gerrit._endpoints.onNewEndpoint(this.name, this._endpointCallBack);
       Gerrit.awaitPluginsLoaded().then(() => Promise.all(
           Gerrit._endpoints.getPlugins(this.name).map(
               pluginUrl => this._import(pluginUrl)))

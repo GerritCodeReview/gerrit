58338f91b3bb2800e3f69a5c5e295ec2a138cb39
Marco Miller <marco.miller@ericsson.com>
2020-04-17 14:04:08 -0400

    GerritSimulation: Support runtime factor property
    Add support for a core framework JAVA_OPTS property [1] that can be
    optionally set depending on the runtime environment. For slower or more
    complex execution environments, that property value can be increased, to
    make scenarios wait longer before considering their steps as completed.
    
    Document that new property, why and how to use it if necessary.
    
    [1] https://gerrit-review.googlesource.com/Documentation/dev-e2e-tests.html#_environment_properties
    
    Change-Id: Ie477a079f205b712643fce766b7043cbde5f1487

--- e2e-tests/src/test/scala/com/google/gerrit/scenarios/GerritSimulation.scala
+++ e2e-tests/src/test/scala/com/google/gerrit/scenarios/GerritSimulation.scala
@@ -31,8 +31,9 @@ class GerritSimulation extends Simulation {
   protected val body: String = s"$pathName-body.json"
   protected val unique: String = name + "-" + this.hashCode()
 
+  private val factor: Double = replaceProperty("factor", 1.0).toDouble
   private val secondsPerWeightUnit: Int = 2
-  val max: Int = secondsPerWeightUnit * weight
+  val max: Int = (secondsPerWeightUnit * weight * factor).toInt
 
   protected val httpRequest: HttpRequestBuilder = http(unique).post("${url}")
   protected val httpProtocol: HttpProtocolBuilder = http.basicAuth(
@@ -57,11 +58,15 @@ class GerritSimulation extends Simulation {
     replaceProperty(term, term, in)
   }
 
+  private def replaceProperty(term: String, default: Any): String = {
+    replaceProperty(term, default, term.toUpperCase)
+  }
+
   protected def replaceProperty(term: String, default: Any, in: String): String = {
     val property = pack + "." + term
     var value = default
     default match {
-      case _: String =>
+      case _: String | _: Double =>
         val propertyValue = Option(System.getProperty(property))
         if (propertyValue.nonEmpty) {
           value = propertyValue.get

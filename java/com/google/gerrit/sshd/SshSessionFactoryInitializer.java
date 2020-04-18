ec40b5c86d66e83f7d668ed37a93259ca25f2e46
David Ostrovsky <david@ostrovsky.org>
2020-04-18 08:50:45 +0200

    Git transport: Add option to switch to using MINA ssh client
    Allow to optionally switch to using Apache MINA sshd client. This option
    was recently added to JGit implementation. Per default JCraft JSch ssh
    client is still used, but in future releases the default will be swapped
    to Apache MINA ssh client.
    
    Additional jgit module ssh-apache and transitive dependency artifact
    sshd-sftp are added as new dependencies.
    
    Per default, acceptance tests are still using JSch ssh client. To switch
    using Apache MINA ssh client in acceptance tests, environment variable
    can be passed: SSH_CLIENT_IMPLEMENTATION=JSCH|APACHE. To set up preferred
    client type for the acceptance tests from the IDE, Java system property
    can be set: -Dgerrit.sshClientImplementation=JSCH|APACHE.
    
    Test Plan:
    
    1. To run the tests using JSch ssh client:
    
      $ bazel test --test_env=SSH_CLIENT_IMPLEMENTATION=JSCH //...
    
    2. To run the tests using Apache MINA ssh client:
    
      $ bazel test --test_env=SSH_CLIENT_IMPLEMENTATION=APACHE //...
    
    Feature: Issue 12599
    Change-Id: Ib4d288bbf390157becaebea870d48963a78d7811

---
+++ java/com/google/gerrit/sshd/SshSessionFactoryInitializer.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd;
+
+import static com.google.gerrit.server.config.SshClientImplementation.APACHE;
+import static com.google.gerrit.server.config.SshClientImplementation.JSCH;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.sshd.DefaultProxyDataFactory;
+import org.eclipse.jgit.transport.sshd.JGitKeyCache;
+import org.eclipse.jgit.transport.sshd.SshdSessionFactory;
+import org.eclipse.jgit.util.FS;
+
+public class SshSessionFactoryInitializer {
+  public static void init(Config config) {
+    if (APACHE == config.getEnum("ssh", null, "clientImplementation", JSCH)) {
+      SshdSessionFactory factory =
+          new SshdSessionFactory(new JGitKeyCache(), new DefaultProxyDataFactory());
+      factory.setHomeDirectory(FS.DETECTED.userHome());
+      SshSessionFactory.setInstance(factory);
+    }
+  }
+
+  private SshSessionFactoryInitializer() {}
+}

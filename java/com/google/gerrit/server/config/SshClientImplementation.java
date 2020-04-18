59294ace1f37c267fdbf3ae477f5e6c19695ce2e
David Ostrovsky <david@ostrovsky.org>
2020-04-18 08:50:45 +0200

    JGit transport: Add option to switch to using MINA sshd client
    Allow to optionally switch to using MINA sshd client. This option was
    recently added to JGit implementation. Per default JCraft JSch ssh
    client is still used, but in future releases the default will be swapped
    to MINA sshd client.
    
    Unconditionally switch to using MINA SSHD client for acceptance tests.
    
    Additional jgit module ssh-apache and transitive dependency artifact
    sshd-sftp are added as new dependencies.
    
    Feature: Issue 12599
    Change-Id: Ib4d288bbf390157becaebea870d48963a78d7811

--- java/com/google/gerrit/server/config/SshClientImplementation.java
+++ java/com/google/gerrit/server/config/SshClientImplementation.java
@@ -12,20 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.acceptance.git;
+package com.google.gerrit.server.config;
 
-import com.google.gerrit.acceptance.GitUtil;
-import org.eclipse.jgit.transport.CredentialsProvider;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.junit.Before;
+/* SSH implementation to use by JGit SSH client transport protocol. */
+public enum SshClientImplementation {
+  /** JCraft JSch implementation. */
+  JSCH,
 
-public class HttpSubmitOnPushIT extends AbstractSubmitOnPush {
-
-  @Before
-  public void cloneProjectOverHttp() throws Exception {
-    CredentialsProvider.setDefault(
-        new UsernamePasswordCredentialsProvider(admin.username(), admin.httpPassword()));
-    testRepo =
-        GitUtil.cloneProject(project, admin.getHttpUrl(server) + "/a/" + project.get(), null);
-  }
+  /** MINA SSHD implementation. */
+  MINA
 }

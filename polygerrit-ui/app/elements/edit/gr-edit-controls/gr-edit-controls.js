fb887f7b36bf43e1305071698a2976340a8d3568
Paladox none <thomasmulhall410@yahoo.com>
2020-04-20 14:10:24 +0000

    Remove contenteditable from #dragDropArea
    Also fix navigations by only using the changeNum.
    
    Change-Id: Ie2b192f06a1ca7da78b3a4821829cc2b26bd7f4d

--- polygerrit-ui/app/elements/edit/gr-edit-controls/gr-edit-controls.js
+++ polygerrit-ui/app/elements/edit/gr-edit-controls/gr-edit-controls.js
@@ -222,11 +222,8 @@ class GrEditControls extends mixinBehaviors( [
     this.$.restAPI.saveFileUploadChangeEdit(this.change._number, path,
         fileData).then(res => {
       if (!res.ok) { return; }
-
       this._closeDialog(this.$.openDialog, true);
-      const url = GerritNav.getUrlForChange(
-          this.change, this.patchNum, undefined, true);
-      GerritNav.navigateToRelativeUrl(url);
+      GerritNav.navigateToChange(this.change);
     });
   }
 

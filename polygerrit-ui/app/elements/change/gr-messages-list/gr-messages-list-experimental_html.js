bbc3c6b92abf23dc58e5cc49323fb7896f137748
Ben Rohlfs <brohlfs@google.com>
2020-04-24 12:02:06 +0200

    Refactor experimental ChangeLog
    Change the "Only comments" toggle to "Show All Activity". Invert the
    default to not show all activity. Rename all variables related to the
    toggle.
    
    Change-Id: I80c8af79696a47a61ac736f7412dee35765cdce0

--- polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list-experimental_html.js
+++ polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list-experimental_html.js
@@ -57,15 +57,15 @@ export const htmlTemplate = html`
   </style>
   <div class="header">
     <span
-      id="automatedMessageToggleContainer"
+      id="showAllActivityToggleContainer"
       class="container"
-      hidden$="[[!_hasAutomatedMessages(messages)]]"
+      hidden\\$="[[!_isVisibleShowAllActivityToggle(messages)]]"
     >
       <paper-toggle-button
-        id="automatedMessageToggle"
-        checked="{{_hideAutomated}}"
+        id="showAllActivityToggle"
+        checked="{{_showAllActivity}}"
       ></paper-toggle-button
-      >Only comments
+      >Show All Activity
       <span class="transparent separator"></span>
     </span>
     <gr-button
@@ -92,7 +92,7 @@ export const htmlTemplate = html`
       show-reply-button="[[showReplyButtons]]"
       on-message-anchor-tap="_handleAnchorClick"
       label-extremes="[[_labelExtremes]]"
-      data-message-id$="[[message.id]]"
+      data-message-id\\$="[[message.id]]"
     ></gr-message>
   </template>
   <gr-reporting id="reporting" category="message-list"></gr-reporting>

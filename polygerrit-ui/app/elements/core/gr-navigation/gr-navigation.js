bfa36f19ecd6167e9009d41de71485ac454b82de
Dhruv Srivastava <dhruvsri@google.com>
2020-04-28 12:16:56 +0200

    Change default patchset choice for comments
    Clicking on the link to a comment posted on patchset X took user to
    Base vs X. We now take the user to X vs Latest.
    
    A toast informs the user of the new choice and shows the keyboard
    shortcut to go to Base vs X.
    
    If user goes to Base vs X (X != latest), then we show a toast telling
    users they can go to Base vs Latest via keyboard shortcuts.
    
    Screenshots: https://imgur.com/a/0Ov0ygV
    Change-Id: Ia2182024917d119b9bf9bd2af8eededf0906b9c5

--- polygerrit-ui/app/elements/core/gr-navigation/gr-navigation.js
+++ polygerrit-ui/app/elements/core/gr-navigation/gr-navigation.js
@@ -441,6 +441,19 @@ export const GerritNav = {
    * @param {number} changeNum
    * @param {string} project The name of the project.
    * @param {string} path The file path.
+   * @param {number} patchNum
+   * @param {number} commentId
+   * @return {string}
+   */
+  getUrlForComment(changeNum, project, path, patchNum, commentId) {
+    return `/c/${project}/+/${changeNum}/${patchNum}/comment/` +
+      `${commentId}/${path}`;
+  },
+
+  /**
+   * @param {number} changeNum
+   * @param {string} project The name of the project.
+   * @param {string} path The file path.
    * @param {number=} opt_patchNum
    * @param {number|string=} opt_basePatchNum The string 'PARENT' can be
    *     used for none.

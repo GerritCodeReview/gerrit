d9d9e218cd688ec1196889a7de317ac98b277c9a
Dhruv Srivastava <dhruvsri@google.com>
2020-04-28 12:16:56 +0200

    Change navigation link for comments
    Change-Id: Ia2182024917d119b9bf9bd2af8eededf0906b9c5

--- polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list_html.js
+++ polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list_html.js
@@ -105,6 +105,7 @@ export const htmlTemplate = html`
           is-on-parent="[[_isOnParent(thread.commentSide)]]"
           line-num="[[thread.line]]"
           patch-num="[[thread.patchNum]]"
+          latest-patch-num="[[_computeLatestPatchNum(change)]]"
           path="[[thread.path]]"
           root-id="{{thread.rootId}}"
           on-thread-changed="_handleCommentsChanged"

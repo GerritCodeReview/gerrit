21f98a08f3f2df491e79847b6a221071d9e5d6a9
Christian Aistleitner <christian@quelltextlich.at>
2020-05-01 11:05:38 +0200

    Allow to listen for account deactivations
    Deactivating an account currently does not purge active SSH connections
    (E.g.: long-running stream-events) and does not purge the web session
    cache. So deactivating a user for real requires to also manually purge
    these connections and caches, which is unnecessarily tedious.
    
    Implementing such code directly in SetInactiveFlag is not straight
    forward as com.google.server.account does not have access to the sshd
    package, and naively adding it to the build introduces a circular
    dependency. So we add an extension point for account deactivation.
    Thereby, core components and plugins can self-cater cache purging and
    other house-holding tasks upon user deactivation without messing with
    the build.
    
    Change-Id: I1dfa1779acfe56e5c7f1666b1e83e83f954083e1

--- javatests/com/google/gerrit/acceptance/api/accounts/AccountListenersIT.java
+++ javatests/com/google/gerrit/acceptance/api/accounts/AccountListenersIT.java
@@ -15,11 +15,14 @@
 package com.google.gerrit.acceptance.api.accounts;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
 
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.TestPlugin;
 import com.google.gerrit.acceptance.testsuite.account.AccountOperations;
+import com.google.gerrit.extensions.events.AccountActivationListener;
 import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.server.account.AccountState;
 import com.google.gerrit.server.validators.AccountActivationValidationListener;
@@ -42,11 +45,16 @@ public class AccountListenersIT extends LightweightPluginDaemonTest {
 
   static Integer lastIdActivationValidation;
   static Integer lastIdDeactivationValidation;
+  static boolean failActivationValidation;
+  static boolean failDeactivationValidation;
+  static Integer lastIdActivated;
+  static Integer lastIdDeactivated;
 
   public static class Module extends AbstractModule {
     @Override
     protected void configure() {
       DynamicSet.bind(binder(), AccountActivationValidationListener.class).to(Validator.class);
+      DynamicSet.bind(binder(), AccountActivationListener.class).to(Listener.class);
     }
   }
 
@@ -54,25 +62,59 @@ public class AccountListenersIT extends LightweightPluginDaemonTest {
   public void testBindings() throws RestApiException {
     int id = accountOperations.newAccount().active().create().get();
 
+    // Successful deactivation
     resetListenerState();
     gApi.accounts().id(id).setActive(false);
     assertDeactivationValidation(id);
+    assertDeactivated(id);
     assertNoMoreEvents();
 
+    // Unsuccessful activation (validator denies)
+    resetListenerState();
+    failActivationValidation = true;
+    assertThrows(
+        ResourceConflictException.class,
+        () -> {
+          gApi.accounts().id(id).setActive(true);
+        });
+    assertActivationValidation(id);
+    // No call to activation listener as validation failed
+    assertNoMoreEvents();
+
+    // Successful activation
     resetListenerState();
     gApi.accounts().id(id).setActive(true);
     assertActivationValidation(id);
+    assertActivated(id);
+    assertNoMoreEvents();
+
+    // Unsuccessful deactivation (validator denies)
+    resetListenerState();
+    failDeactivationValidation = true;
+    assertThrows(
+        ResourceConflictException.class,
+        () -> {
+          gApi.accounts().id(id).setActive(false);
+        });
+    assertDeactivationValidation(id);
+    // No call to deactivation listener as validation failed
     assertNoMoreEvents();
   }
 
   private void resetListenerState() {
     lastIdActivationValidation = null;
     lastIdDeactivationValidation = null;
+    failActivationValidation = false;
+    failDeactivationValidation = false;
+    lastIdActivated = null;
+    lastIdDeactivated = null;
   }
 
   private void assertNoMoreEvents() {
     assertThat(lastIdActivationValidation).isNull();
     assertThat(lastIdDeactivationValidation).isNull();
+    assertThat(lastIdActivated).isNull();
+    assertThat(lastIdDeactivated).isNull();
   }
 
   private void assertActivationValidation(int id) {
@@ -85,17 +127,47 @@ public class AccountListenersIT extends LightweightPluginDaemonTest {
     lastIdDeactivationValidation = null;
   }
 
+  private void assertActivated(int id) {
+    assertThat(lastIdActivated).isEqualTo(id);
+    lastIdActivated = null;
+  }
+
+  private void assertDeactivated(int id) {
+    assertThat(lastIdDeactivated).isEqualTo(id);
+    lastIdDeactivated = null;
+  }
+
   public static class Validator implements AccountActivationValidationListener {
     @Override
     public void validateActivation(AccountState account) throws ValidationException {
       assertThat(lastIdActivationValidation).isNull();
       lastIdActivationValidation = account.account().id().get();
+      if (failActivationValidation) {
+        throw new ValidationException("testing validation failure");
+      }
     }
 
     @Override
     public void validateDeactivation(AccountState account) throws ValidationException {
       assertThat(lastIdDeactivationValidation).isNull();
       lastIdDeactivationValidation = account.account().id().get();
+      if (failDeactivationValidation) {
+        throw new ValidationException("testing validation failure");
+      }
+    }
+  }
+
+  public static class Listener implements AccountActivationListener {
+    @Override
+    public void onAccountActivated(int id) {
+      assertThat(lastIdActivated).isNull();
+      lastIdActivated = id;
+    }
+
+    @Override
+    public void onAccountDeactivated(int id) {
+      assertThat(lastIdDeactivated).isNull();
+      lastIdDeactivated = id;
     }
   }
 }

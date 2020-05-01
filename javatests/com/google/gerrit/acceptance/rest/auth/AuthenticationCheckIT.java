335e664be558faecf91fe9a12e98fafe486bc054
Christian Aistleitner <christian@quelltextlich.at>
2020-05-01 14:18:32 +0200

    Logout HTTP sessions of inactive accounts upon use
    When an account gets deactivated, the user could continue to use
    sessions that got started before the deactivation. Administrators had
    to purge the web_sessions cache to get inactive users to logout. This
    purging was of course disruptive as it logged all users out.
    
    As automatic purging upon deactivation is hindered by the inner
    workings of the cache, we resort to checking for inactive accounts
    upon use of sessions.
    
    Change-Id: I0b09bbf96dcad2c5d13f7faa07bcf69212355918

--- javatests/com/google/gerrit/acceptance/rest/auth/AuthenticationCheckIT.java
+++ javatests/com/google/gerrit/acceptance/rest/auth/AuthenticationCheckIT.java
@@ -17,6 +17,20 @@ package com.google.gerrit.acceptance.rest.auth;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.RestResponse;
 import com.google.gerrit.acceptance.RestSession;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.httpd.WebSession;
+import com.google.gerrit.server.account.AuthResult;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.inject.servlet.ServletModule;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.http.Header;
+import org.apache.http.message.BasicHeader;
 import org.junit.Test;
 
 public class AuthenticationCheckIT extends AbstractDaemonTest {
@@ -31,5 +45,92 @@ public class AuthenticationCheckIT extends AbstractDaemonTest {
     RestSession anonymous = new RestSession(server, null);
     RestResponse r = anonymous.get("/auth-check");
     r.assertForbidden();
+  }
+
+  @Test
+  public void authCheck_deactivatedUser() throws Exception {
+    String authCheckUrl = "/auth-check";
+
+    // Making sure anonymous user is not authenticated beforehand
+    anonymousRestSession.get(authCheckUrl).assertForbidden();
+
+    // Grabbing a session token for user
+    String sessionCookie = getSessionCookie(userRestSession);
+    Header authHeader = new BasicHeader("Cookie", sessionCookie);
+    // From now on, passing the authHeader allows to use the userRestSession.
+    // This not a "runAs", but this is mimicking the way api clients work for example is
+    // LDAP auth settings. There, the authentication happens once and the session cookie
+    // gets used in subsequent requests to re-use the session and by-pass manual authentication
+    // again.
+
+    // Checking that anonymous session on its own is still not authenticated
+    anonymousRestSession.get(authCheckUrl).assertForbidden();
+
+    // And that the header alone provides authentication
+    anonymousRestSession.getWithHeader(authCheckUrl, authHeader).assertNoContent();
+    anonymousRestSession.get(authCheckUrl).assertForbidden();
+
+    try {
+      gApi.accounts().id(user.id().get()).setActive(false);
+
+      // With the disabled account, authHeader is no longer valid
+      anonymousRestSession.getWithHeader(authCheckUrl, authHeader).assertForbidden();
+
+    } finally {
+      // Resetting the user status for good measure.
+      gApi.accounts().id(user.id().get()).setActive(true);
+    }
+  }
+
+  private String getSessionCookie(RestSession session) throws Exception {
+    String pluginName = "cached-session-login-plugin";
+    try (AutoCloseable ignored =
+        installPlugin(pluginName, null, CachedSessionLoginModule.class, null)) {
+      String loginUrl = "/plugins/" + pluginName + CachedSessionLoginModule.cacheSessionLoginPath;
+
+      RestResponse res = session.get(loginUrl);
+      res.assertNoContent();
+      String setCookie = res.getHeader("Set-Cookie");
+
+      // setCookie is "GerritAccount=[TOKEN]; Path=/; Expires=..."
+      // token will only be the "GerritAccount=[TOKEN]" part of setCookie
+      String token = setCookie.split(";")[0];
+
+      return token;
+    }
+  }
+
+  static class CachedSessionLoginModule extends ServletModule {
+    private static final String cacheSessionLoginPath = "/cache-session-login";
+
+    @Override
+    protected void configureServlets() {
+      serve(cacheSessionLoginPath).with(CacheSessionLoginServlet.class);
+    }
+  }
+
+  @Singleton
+  public static class CacheSessionLoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private final DynamicItem<WebSession> sessionProvider;
+
+    @Inject
+    CacheSessionLoginServlet(DynamicItem<WebSession> sessionProvider) {
+      this.sessionProvider = sessionProvider;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, IOException {
+      // We expect the user to be on a temporary authenticated session and will add that session to
+      // the cache by performing a proper login.
+      WebSession session = sessionProvider.get();
+      Account.Id id = session.getUser().getAccountId();
+
+      AuthResult authRes = new AuthResult(id, null, false);
+      session.login(authRes, true);
+
+      res.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
   }
 }

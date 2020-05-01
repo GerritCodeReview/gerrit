89c91a1111c088eb518344b502d1fce42dcd1d40
Christian Aistleitner <christian@quelltextlich.at>
2020-05-01 11:05:38 +0200

    Allow to listen for account deactivations
    Deactivating an account currently does not purge active SSH connections
    (E.g.: long-running stream-events) and does not purge the web session
    cache. So deactivating a user for real requires to also manually purge
    these connections and caches, which is unnecessarily tedious.
    
    Implementing such code directly in SetInactiveFlag is not straight
    forward as com.google.server.account does not have access to the sshd
    package, and naively adding it to the build introduces a circular
    dependency. So we add an extension point for account deactivation.
    Thereby, core components and plugins can self-cater cache purging and
    other house-holding tasks upon user deactivation without messing with
    the build.
    
    Change-Id: I1dfa1779acfe56e5c7f1666b1e83e83f954083e1

--- javatests/com/google/gerrit/acceptance/api/accounts/AccountListenersIT.java
+++ javatests/com/google/gerrit/acceptance/api/accounts/AccountListenersIT.java
@@ -15,12 +15,15 @@
 package com.google.gerrit.acceptance.api.accounts;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
 
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.TestPlugin;
 import com.google.gerrit.acceptance.testsuite.account.AccountOperations;
 import com.google.gerrit.entities.Account;
+import com.google.gerrit.extensions.events.AccountActivationListener;
 import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.server.account.AccountState;
 import com.google.gerrit.server.validators.AccountActivationValidationListener;
@@ -44,11 +47,14 @@ public class AccountListenersIT extends LightweightPluginDaemonTest {
 
   static AtomicInteger activationValidations = new AtomicInteger(0);
   static AtomicInteger deactivationValidations = new AtomicInteger(0);
+  static AtomicInteger activations = new AtomicInteger(0);
+  static AtomicInteger deactivations = new AtomicInteger(0);
 
   public static class Module extends AbstractModule {
     @Override
     protected void configure() {
       DynamicSet.bind(binder(), AccountActivationValidationListener.class).to(Validator.class);
+      DynamicSet.bind(binder(), AccountActivationListener.class).to(Listener.class);
     }
   }
 
@@ -57,23 +63,65 @@ public class AccountListenersIT extends LightweightPluginDaemonTest {
     Account.Id accountId = accountOperations.newAccount().active().create();
 
     assertThat(deactivationValidations.get()).isEqualTo(0);
+    assertThat(deactivations.get()).isEqualTo(0);
     gApi.accounts().id(accountId.get()).setActive(false);
     assertThat(deactivationValidations.get()).isEqualTo(1);
+    assertThat(deactivations.get()).isEqualTo(1);
 
     assertThat(activationValidations.get()).isEqualTo(0);
-    gApi.accounts().id(accountId.get()).setActive(true);
+    assertThat(activations.get()).isEqualTo(0);
+    assertThrows(
+        ResourceConflictException.class,
+        () -> {
+          gApi.accounts().id(accountId.get()).setActive(true);
+        });
     assertThat(activationValidations.get()).isEqualTo(1);
+    assertThat(activations.get()).isEqualTo(0);
+
+    gApi.accounts().id(accountId.get()).setActive(true);
+    assertThat(activationValidations.get()).isEqualTo(2);
+    assertThat(activations.get()).isEqualTo(1);
+
+    assertThat(deactivationValidations.get()).isEqualTo(1);
+    assertThat(deactivations.get()).isEqualTo(1);
+    assertThrows(
+        ResourceConflictException.class,
+        () -> {
+          gApi.accounts().id(accountId.get()).setActive(false);
+        });
+    assertThat(deactivationValidations.get()).isEqualTo(2);
+    assertThat(deactivations.get()).isEqualTo(1);
+    assertThat(activationValidations.get()).isEqualTo(2);
+    assertThat(activations.get()).isEqualTo(1);
   }
 
   public static class Validator implements AccountActivationValidationListener {
     @Override
     public void validateActivation(AccountState account) throws ValidationException {
       activationValidations.incrementAndGet();
+      if (activationValidations.get() == 1) {
+        throw new ValidationException("testing validation failure");
+      }
     }
 
     @Override
     public void validateDeactivation(AccountState account) throws ValidationException {
       deactivationValidations.incrementAndGet();
+      if (deactivationValidations.get() == 2) {
+        throw new ValidationException("testing validation failure");
+      }
+    }
+  }
+
+  public static class Listener implements AccountActivationListener {
+    @Override
+    public void onAccountActivated(int id) {
+      activations.incrementAndGet();
+    }
+
+    @Override
+    public void onAccountDeactivated(int id) {
+      deactivations.incrementAndGet();
     }
   }
 }

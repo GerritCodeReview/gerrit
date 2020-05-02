1d6c129089851646a5c507232de135edc79bca37
David Ostrovsky <david@ostrovsky.org>
2020-05-02 08:29:35 +0200

    Schema_154: Run full gc after every 10k account updates
    Change-Id: I76e6c2916d5c45220b94632c9ee65ea838e425bc

--- gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_154.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/schema/Schema_154.java
@@ -95,9 +95,14 @@ public class Schema_154 extends SchemaVersion {
         Set<Account> accounts = scanAccounts(db, pm);
         pm.endTask();
         pm.beginTask("Migrating accounts to NoteDb", accounts.size());
+        int i = 0;
         for (Account account : accounts) {
           updateAccountInNoteDb(repo, account);
           pm.update(1);
+          int count = i++;
+          if (count % 10000 == 0) {
+            gc(repo, ui);
+          }
         }
         pm.endTask();
 

60a7eab696e3df23832e2443fba93baaf280506f
Luca Milanesio <luca.milanesio@gmail.com>
2020-05-04 17:50:49 +0100

    Remove forced reindex of projects during init
    The forced reindex of the projects index is not needed
    anymore in v3.2 and can be removed.
    It was initially introduced in v2.15 because of the
    requirement to have a projects index also for existing
    sites.
    
    Since v2.16 it can be safely removed as requirement.
    Reindexing projects is still possible on demand through
    the 'java -jar gerrit.war reindex'.
    
    Remove also the '--no-reindex' option which would be
    instead done automatically: reindex will still happen
    when needed and NOT happen when projects index is
    already present.
    
    Preserve the reindex of projects only for new sites.
    
    Reindexing projects is a very expensive operation and
    can turn a simple upgrade to a long and painful operation
    because of the increase of the migration time and the
    amount of memory needed.
    
    Bug: Issue 12680
    Change-Id: I3a4f1d07405f7bb631467d1f005d48cb56ee867f

--- javatests/com/google/gerrit/acceptance/pgm/InitIT.java
+++ javatests/com/google/gerrit/acceptance/pgm/InitIT.java
@@ -17,6 +17,7 @@ package com.google.gerrit.acceptance.pgm;
 import static com.google.common.truth.Truth8.assertThat;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.truth.Truth;
 import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.StandaloneSiteTest;
 import com.google.gerrit.entities.Project;
@@ -26,7 +27,12 @@ import com.google.gerrit.index.project.ProjectData;
 import com.google.gerrit.index.project.ProjectIndexCollection;
 import com.google.gerrit.server.config.AllProjectsName;
 import com.google.gerrit.server.config.AllUsersName;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Stream;
 import org.junit.Test;
 
 @NoHttpd
@@ -47,5 +53,41 @@ public class InitIT extends StandaloneSiteTest {
       Optional<ProjectData> allUsersData = projectIndex.getSearchIndex().get(allUsers, opts);
       assertThat(allUsersData).isPresent();
     }
+  }
+
+  @Test
+  public void initDoesNotReindexProjectsOnExistingSItes() throws Exception {
+    runGerrit("init", "-d", sitePaths.site_path.toString(), "--show-stack-trace");
+    Optional<Long> initialProjectsPath = getProjectsIndexLastModified(sitePaths.index_dir);
+
+    // Make sure that the lastModified() returns different values if directory is rewritten
+    Thread.sleep(1000L);
+
+    runGerrit("init", "-d", sitePaths.site_path.toString(), "--show-stack-trace");
+    Optional<Long> afterInitProjectsPath = getProjectsIndexLastModified(sitePaths.index_dir);
+
+    Truth.assertThat(initialProjectsPath).isEqualTo(afterInitProjectsPath);
+  }
+
+  private Optional<Long> getProjectsIndexLastModified(Path indexDir) throws IOException {
+    Optional<Path> projectsIndexPath =
+        Files.walk(indexDir, 1)
+            .filter(Files::isDirectory)
+            .filter(p -> p.getFileName().toString().startsWith("projects_"))
+            .findFirst();
+
+    Optional<Stream<Long>> projectsIndexLastModified =
+        projectsIndexPath.map(
+            pd -> {
+              try {
+                return Files.walk(pd, 1).map(p -> p.toFile().lastModified());
+              } catch (IOException e) {
+                e.printStackTrace();
+                return Stream.empty();
+              }
+            });
+
+    return projectsIndexLastModified.flatMap(
+        mod -> mod.max(Comparator.comparingLong(Long::valueOf)));
   }
 }

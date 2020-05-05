ea39a3e7c767065193e73171cae3a4283fba59c1
Milutin Kristofic <milutin@google.com>
2020-05-05 16:21:05 +0200

    Remove moment.js
    Moment.js has large bundle size and cause huge performance overhead.
    
    Change-Id: Ida5c2642006d5018300527cb2b5eaab3c61772d2

--- polygerrit-ui/app/scripts/util.js
+++ polygerrit-ui/app/scripts/util.js
@@ -29,14 +29,6 @@ function getPathFromNode(el) {
 // TODO (dmfilippov): Each function must be exported separately. According to
 // the code style guide, a namespacing is not allowed.
 export const util = {
-  parseDate(dateStr) {
-    // Timestamps are given in UTC and have the format
-    // "'yyyy-mm-dd hh:mm:ss.fffffffff'" where "'ffffffffff'" represents
-    // nanoseconds.
-    // Munge the date into an ISO 8061 format and parse that.
-    return new Date(dateStr.replace(' ', 'T') + 'Z');
-  },
-
   getCookie(name) {
     const key = name + '=';
     const cookies = document.cookie.split(';');

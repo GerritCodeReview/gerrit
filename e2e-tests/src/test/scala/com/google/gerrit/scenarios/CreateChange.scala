bcdeea7515a6889406217ee45c52498203aedebd
Marco Miller <marco.miller@ericsson.com>
2020-05-12 14:18:27 -0400

    e2e-tests: Replace FileBasedFeederBuilder with FeederBuilder declaration
    Change-Id: Idbae72020cd34c9315322be577ca1f22b7cd7a74

--- e2e-tests/src/test/scala/com/google/gerrit/scenarios/CreateChange.scala
+++ e2e-tests/src/test/scala/com/google/gerrit/scenarios/CreateChange.scala
@@ -15,14 +15,14 @@
 package com.google.gerrit.scenarios
 
 import io.gatling.core.Predef.{atOnceUsers, _}
-import io.gatling.core.feeder.FileBasedFeederBuilder
+import io.gatling.core.feeder.FeederBuilder
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
 
 class CreateChange extends GerritSimulation {
-  private val data: FileBasedFeederBuilder[Any]#F#F = jsonFile(resource).convert(keys).queue
+  private val data: FeederBuilder = jsonFile(resource).convert(keys).queue
   private val default: String = name
   private val numberKey = "_number"
 

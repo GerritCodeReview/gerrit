d97e4a497a6b8f4a97044079d0cb26f926f92e06
Dhruv Srivastava <dhruvsri@google.com>
2020-05-15 19:20:44 +0200

    Add patchset comment summary to change log
    Change-Id: Ic58e9991cf02fc41bd25ba7cfe8a8447036af38b

--- polygerrit-ui/app/elements/change/gr-message/gr-message.js
+++ polygerrit-ui/app/elements/change/gr-message/gr-message.js
@@ -31,6 +31,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-message_html.js';
+import {SpecialFilePath} from '../../../constants/constants.js';
 
 const PATCH_SET_PREFIX_PATTERN = /^Patch Set \d+:\s*(.*)/;
 const LABEL_TITLE_SCORE_PATTERN = /^(-?)([A-Za-z0-9-]+?)([+-]\d+)?$/;
@@ -138,7 +139,8 @@ class GrMessage extends GestureEventListeners(
       _messageContentCollapsed: {
         type: String,
         computed:
-            '_computeMessageContentCollapsed(message.message, message.tag)',
+            '_computeMessageContentCollapsed(message.message, message.tag,' +
+            ' commentThreads)',
       },
       _commentCountText: {
         type: Number,
@@ -247,8 +249,32 @@ class GrMessage extends GestureEventListeners(
     return this._computeMessageContent(content, tag, true);
   }
 
-  _computeMessageContentCollapsed(content, tag) {
-    return this._computeMessageContent(content, tag, false);
+  _patchsetCommentSummary(commentThreads) {
+    const id = this.message.id;
+    if (!id) return;
+    const patchsetThreads = commentThreads.filter(thread =>
+      thread.path === SpecialFilePath.PATCHSET_LEVEL_COMMENTS);
+    for (const thread of patchsetThreads) {
+      // Find if there was a patchset level comment created through the reply
+      // dialog and use it to determine the summary
+      if (thread.comments[0].change_message_id === id) {
+        return thread.comments[0].message;
+      }
+    }
+    // Find if there is a reply to some patchset comment left
+    for (const thread of patchsetThreads) {
+      for (const comment of thread.comments) {
+        if (comment.change_message_id === id) { return comment.message; }
+      }
+    }
+    return '';
+  }
+
+  _computeMessageContentCollapsed(content, tag, commentThreads) {
+    const summary =
+      this._computeMessageContent(content, tag, false);
+    if (summary || !commentThreads) return;
+    return this._patchsetCommentSummary(commentThreads);
   }
 
   _computeMessageContent(content, tag, isExpanded) {

3a2181a71cc939216e27166f619136b0a711aae0
Dhruv Srivastava <dhruvsri@google.com>
2020-05-15 19:20:44 +0200

    Add patchset comment summary to change log
    Change-Id: Ic58e9991cf02fc41bd25ba7cfe8a8447036af38b

--- polygerrit-ui/app/elements/change/gr-message/gr-message.js
+++ polygerrit-ui/app/elements/change/gr-message/gr-message.js
@@ -31,6 +31,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-message_html.js';
+import {SpecialFilePath} from '../../../constants/constants.js';
 
 const PATCH_SET_PREFIX_PATTERN = /^Patch Set \d+:\s*(.*)/;
 const LABEL_TITLE_SCORE_PATTERN = /^(-?)([A-Za-z0-9-]+?)([+-]\d+)?$/;
@@ -138,7 +139,8 @@ class GrMessage extends GestureEventListeners(
       _messageContentCollapsed: {
         type: String,
         computed:
-            '_computeMessageContentCollapsed(message.message, message.tag)',
+            '_computeMessageContentCollapsed(message.message, message.tag,' +
+            ' commentThreads)',
       },
       _commentCountText: {
         type: Number,
@@ -246,8 +248,29 @@ class GrMessage extends GestureEventListeners(
     return this._computeMessageContent(content, tag, true);
   }
 
-  _computeMessageContentCollapsed(content, tag) {
-    return this._computeMessageContent(content, tag, false);
+  _patchsetCommentSummary(commentThreads) {
+    const idx = this.message.id;
+    if (!idx) return;
+    let summary = '';
+    commentThreads.forEach(thread => {
+      // figure out if this was created in this message(?)
+      // if user posts a draft reply then length === 2
+      if (thread.path == SpecialFilePath.PATCHSET_LEVEL_COMMENTS) {
+        thread.comments.forEach(comment => {
+          if (comment.change_message_id === idx) { summary = comment.message; }
+        });
+      }
+    });
+    return summary;
+  }
+
+  _computeMessageContentCollapsed(content, tag, commentThreads) {
+    const summary =
+      this._computeMessageContent(content, tag, false, commentThreads);
+    if (summary || !commentThreads) return;
+    // Find if there was a patchset level comment created through the reply
+    // dialog and use it to determine the summary
+    return this._patchsetCommentSummary(commentThreads);
   }
 
   _computeMessageContent(content, tag, isExpanded) {

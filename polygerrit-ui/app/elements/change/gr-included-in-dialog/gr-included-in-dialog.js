2645f4a1e46309c3cd0efa28a247829a3ab1b69f
Paladox <thomasmulhall410@yahoo.com>
2020-05-19 14:53:19 +0100

    Fix searching in "included in"
    * filterText can sometimes be undefined, so we remove the .length part
      as an empty string would still be false with '!'.
    
    * We move to using bindValue.
    
    Bug: Issue 12778
    Change-Id: I498c61fff930af4cc4e2846f6da672b97a3d322a

--- polygerrit-ui/app/elements/change/gr-included-in-dialog/gr-included-in-dialog.js
+++ polygerrit-ui/app/elements/change/gr-included-in-dialog/gr-included-in-dialog.js
@@ -65,7 +65,9 @@
     },
 
     _computeGroups(includedIn, filterText) {
-      if (!includedIn) { return []; }
+      if (!includedIn || filterText === undefined) {
+        return [];
+      }
 
       const filter = item => !filterText.length ||
           item.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
@@ -93,12 +95,6 @@
 
     _computeLoadingClass(loaded) {
       return loaded ? 'loading loaded' : 'loading';
-    },
-
-    _onFilterChanged() {
-      this.debounce('filter-change', () => {
-        this._filterText = this.$.filterInput.bindValue;
-      }, 100);
     },
   });
 })();

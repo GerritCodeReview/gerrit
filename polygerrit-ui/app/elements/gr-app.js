74140f58c5cc6d4d264d8fe275faf5412d862c74
Ben Rohlfs <brohlfs@google.com>
2020-05-19 09:26:47 +0200

    Remove obsolete <gr-embed-dashboard> component.
    Change-Id: Ic401b508414164837b96da32396a08dc426b7352

--- polygerrit-ui/app/elements/gr-app.js
+++ polygerrit-ui/app/elements/gr-app.js
@@ -32,7 +32,6 @@ setCancelSyntheticClickEvents(false);
 import 'polymer-resin/standalone/polymer-resin.js';
 import {initGlobalVariables} from './gr-app-global-var-init.js';
 import './gr-app-element.js';
-import './change-list/gr-embed-dashboard/gr-embed-dashboard.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';

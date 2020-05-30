292347a1c2db2f45f9040f930a90856f5e626b46
Christian Aistleitner <christian@quelltextlich.at>
2020-05-30 15:37:20 +0200

    Try to find tags matching Gerrit's version when describing plugins
    This will help getting meaningful versions for plugins. For example
    when building on the stable-2.16 branch, we before received a
    commit-message-length-validator described as v3.2.0-rc1, although the
    same commit also has a much better fitting (lower) v2.16.18 tag, which
    we use now. Or on the stable-3.0 branch, download-commands was
    described as v2.16.18, which we now describe with the better fitting
    (higher) v3.0.8 tag.
    
    Change-Id: Ib1681b2730cf2c443a3cb55fe6e282f6484e18de

--- tools/workspace_status_release.py
+++ tools/workspace_status_release.py
@@ -37,7 +37,7 @@ import re
 ROOT = os.path.abspath(__file__)
 while not os.path.exists(os.path.join(ROOT, 'WORKSPACE')):
     ROOT = os.path.dirname(ROOT)
-REVISION_CMD = ['git', 'describe', '--always', '--match', 'v[0-9].*', '--dirty']
+REVISION_CMD = ['git', 'describe', '--always', '--dirty']
 
 
 def run(command):
@@ -50,12 +50,36 @@ def run(command):
         # ignore "not a git repository error" to report unknown version
         return None
 
+def revision_with_match(match):
+    command = REVISION_CMD[:] # Copying, as adding elements should not change REVISION_CMD
+    if match:
+        command += ['--match', match + '*']
+    return run(command)
 
-def revision():
-    return run(REVISION_CMD)
+def revision(template=None):
+    if template:
+        # First, we turn a template like `v2.16.19-1-gec686a6352` into ['v2', '16', '19']
+        parts = template.split('-')[0].split('.')
+        # Then we use that list to match against tags in the current repo, starting with
+        # longest matches.
+        for length in range(len(parts),0,-1):
+            start = '.'.join(parts[0:length])
+            for variant in ['.', '']:
+                ret = revision_with_match(start + variant + '*')
+                if ret.startswith(start + variant):
+                    return ret
+
+    # None of the template based methods worked out, so we're falling back to a
+    # generic version match.
+    ret = revision_with_match('v[0-9].*')
+    if ret.startswith('v'):
+        return ret
+
+    # Still no good tag, so we re-try without any matching
+    return revision_with_match(None)
 
 
-def print_stamps_for_plugin(name):
+def print_stamps_for_plugin(name, template):
     workspace_status_script = os.path.join('tools', 'workspace_status_release.py')
     if os.path.isfile(workspace_status_script):
         # plugin has own workspace_status_command, so we use stamps from that
@@ -64,16 +88,17 @@ def print_stamps_for_plugin(name):
                 print(line)
     else:
         # plugin lacks own workspace_status_command no we create default stamp
-        v = revision()
+        v = revision(template)
         print('STABLE_BUILD_%s_LABEL %s' % (name.upper(),
                                             v if v else 'unknown'))
 
 
 os.chdir(ROOT)
-print("STABLE_BUILD_GERRIT_LABEL %s" % revision())
+GERRIT_VERSION=revision()
+print("STABLE_BUILD_GERRIT_LABEL %s" % GERRIT_VERSION)
 for d in os.listdir(os.path.join(ROOT, 'plugins')):
     p = os.path.join(ROOT, 'plugins', d)
     if os.path.isdir(p):
         os.chdir(p)
         name = os.path.basename(p)
-        print_stamps_for_plugin(name)
+        print_stamps_for_plugin(name, GERRIT_VERSION)

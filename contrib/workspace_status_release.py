c0dc1594d64f2e4b4ffa9e38c46667ccae062d12
Christian Aistleitner <christian@quelltextlich.at>
2020-05-30 11:46:14 +0200

    Allow plugins to provide the own workspace settings
    By calling the plugins' workspace file, they can set their version in
    the stable-status.txt file and thereby override the default version
    information used it the jar's manifest file.
    
    This is useful for plugins, where a plain `git describe` is not
    sufficient. For example its-phabricator depends on its-base. So the
    its-phabricator version number should also contain a hint to what
    its-base version it got built against. Now,`its-phabrictor` can do
    so in it's own workspace_status_command script.
    
    Change-Id: Id682d6d4dd594838ab73c8f34ce68f6b14739e07

--- contrib/workspace_status_release.py
+++ contrib/workspace_status_release.py
@@ -32,31 +32,44 @@ from __future__ import print_function
 import os
 import subprocess
 import sys
+import re
 
 ROOT = os.path.abspath(__file__)
 while not os.path.exists(os.path.join(ROOT, 'WORKSPACE')):
     ROOT = os.path.dirname(ROOT)
-CMD = ['git', 'describe', '--always', '--match', 'v[0-9].*', '--dirty']
+REVISION_CMD = ['git', 'describe', '--always', '--match', 'v[0-9].*', '--dirty']
 
 
-def revision(directory, parent):
+def run(command):
     try:
-        os.chdir(directory)
-        return subprocess.check_output(CMD).strip().decode("utf-8")
+        return subprocess.check_output(command).strip().decode("utf-8")
     except OSError as err:
-        print('could not invoke git: %s' % err, file=sys.stderr)
+        print('could not invoke %s: %s' % (command[0], err), file=sys.stderr)
         sys.exit(1)
     except subprocess.CalledProcessError as err:
         # ignore "not a git repository error" to report unknown version
         return None
-    finally:
-        os.chdir(parent)
 
 
-print("STABLE_BUILD_GERRIT_LABEL %s" % revision(ROOT, ROOT))
+def revision():
+    return run(REVISION_CMD)
+
+
+def run_plugin_workspace_status():
+    workspace_status_script = os.path.join('tools', 'workspace_status_release.py')
+    if os.path.isfile(workspace_status_script):
+        for line in run(["python", workspace_status_script]).split('\n'):
+            if re.search("^STABLE_[a-zA-Z0-9().:@/_ -]*$", line):
+                print(line)
+
+os.chdir(ROOT)
+print("STABLE_BUILD_GERRIT_LABEL %s" % revision())
 for d in os.listdir(os.path.join(ROOT, 'plugins')):
-    p = os.path.join('plugins', d)
+    p = os.path.join(ROOT, 'plugins', d)
     if os.path.isdir(p):
-        v = revision(p, ROOT)
+        os.chdir(p)
+
+        v = revision()
         print('STABLE_BUILD_%s_LABEL %s' % (os.path.basename(p).upper(),
                                             v if v else 'unknown'))
+        run_plugin_workspace_status()

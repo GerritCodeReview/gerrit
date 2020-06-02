d6869c5ffe71222000488156dfb23fc85a8d4e25
David Ostrovsky <david@ostrovsky.org>
2020-06-02 23:41:39 +0200

    Use MINA sshd library for key generation and export
    Since this feature request was implemented: [1], in this commit: [2],
    we don't have to depend on JCraft JSch library for key generation and
    export in OpenSSH format.
    
    Given that JSch cannot read private key material in OpenSSH format, use
    RSA instead of elliptic curve algorithm and export the private key in
    PKCS #8 format using Bouncy Castle library.
    
    [1] https://issues.apache.org/jira/browse/SSHD-984
    [2] https://github.com/apache/mina-sshd/commit/42df03db207be7be5479acf97335ea8ae1603c21
    
    Change-Id: I9bb35ac5f18c2463705fa52e33dc67df82f480d8

--- java/com/google/gerrit/acceptance/testsuite/account/TestSshKeys.java
+++ java/com/google/gerrit/acceptance/testsuite/account/TestSshKeys.java
@@ -15,22 +15,34 @@
 package com.google.gerrit.acceptance.testsuite.account;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.gerrit.server.config.SshClientImplementation.getFromEnvironment;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
 import com.google.gerrit.acceptance.SshEnabled;
 import com.google.gerrit.common.Nullable;
 import com.google.gerrit.entities.Account;
 import com.google.gerrit.server.account.VersionedAuthorizedKeys;
+import com.google.gerrit.server.config.SshClientImplementation;
 import com.google.gerrit.server.ssh.SshKeyCache;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.KeyPair;
 import java.io.ByteArrayOutputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.sshd.common.cipher.ECCurves;
+import org.apache.sshd.common.config.keys.KeyUtils;
+import org.apache.sshd.common.config.keys.writer.openssh.OpenSSHKeyPairResourceWriter;
+import org.apache.sshd.common.util.security.SecurityUtils;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.openssl.jcajce.JcaPKCS8Generator;
+import org.bouncycastle.util.io.pem.PemObject;
 
 @Singleton
 public class TestSshKeys {
@@ -92,21 +104,57 @@ public class TestSshKeys {
     return keyPair;
   }
 
-  public static KeyPair genSshKey() throws JSchException {
-    JSch jsch = new JSch();
-    return KeyPair.genKeyPair(jsch, KeyPair.ECDSA, 256);
+  public static KeyPair genSshKey() throws GeneralSecurityException {
+    SshClientImplementation client = getFromEnvironment();
+    KeyPairGenerator gen;
+    if (client == SshClientImplementation.APACHE) {
+      int size = 256;
+      gen = SecurityUtils.getKeyPairGenerator(KeyUtils.EC_ALGORITHM);
+      ECCurves curve = ECCurves.fromCurveSize(size);
+      if (curve == null) {
+        throw new InvalidKeySpecException("Unknown curve for key size=" + size);
+      }
+      gen.initialize(curve.getParameters());
+    } else {
+      gen = KeyPairGenerator.getInstance("RSA");
+      gen.initialize(1024, new SecureRandom());
+    }
+
+    return gen.generateKeyPair();
   }
 
   public static String publicKey(KeyPair sshKey, @Nullable String comment)
-      throws UnsupportedEncodingException {
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    sshKey.writePublicKey(out, comment);
-    return out.toString(US_ASCII.name()).trim();
+      throws IOException, GeneralSecurityException {
+    return preparePublicKey(sshKey, comment).toString(US_ASCII.name()).trim();
   }
 
-  public static byte[] privateKey(KeyPair keyPair) {
+  public static byte[] publicKeyBlob(KeyPair sshKey) throws IOException, GeneralSecurityException {
+    return preparePublicKey(sshKey, null).toByteArray();
+  }
+
+  private static ByteArrayOutputStream preparePublicKey(KeyPair sshKey, String comment)
+      throws IOException, GeneralSecurityException {
+    OpenSSHKeyPairResourceWriter keyPairWriter = new OpenSSHKeyPairResourceWriter();
     ByteArrayOutputStream out = new ByteArrayOutputStream();
-    keyPair.writePrivateKey(out);
-    return out.toByteArray();
+    keyPairWriter.writePublicKey(sshKey, comment, out);
+    return out;
+  }
+
+  public static byte[] privateKey(KeyPair keyPair) throws IOException, GeneralSecurityException {
+    SshClientImplementation client = getFromEnvironment();
+    if (client == SshClientImplementation.APACHE) {
+      OpenSSHKeyPairResourceWriter keyPairWriter = new OpenSSHKeyPairResourceWriter();
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      keyPairWriter.writePrivateKey(keyPair, null, null, out);
+      return out.toByteArray();
+    }
+    // unencrypted form of PKCS#8 file
+    JcaPKCS8Generator gen1 = new JcaPKCS8Generator(keyPair.getPrivate(), null);
+    PemObject obj1 = gen1.generate();
+    StringWriter sw1 = new StringWriter();
+    try (JcaPEMWriter pw = new JcaPEMWriter(sw1)) {
+      pw.writeObject(obj1);
+    }
+    return sw1.toString().getBytes(US_ASCII.name());
   }
 }

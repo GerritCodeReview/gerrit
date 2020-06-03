f66981a2e27df2267ce4c69f8f6d857107d4795f
Marco Miller <marco.miller@ericsson.com>
2020-06-03 17:18:36 -0400

    e2e-tests: Add CheckMasterBranchReplica1 scenarios
    Add the CheckMasterBranchReplica1 and GetMasterBranchRevision scenarios.
    Make the latter usable independently of the former which composes it.
    Adapt the existing SubmitChange scenario so it can be composed by
    CheckMasterBranchReplica1 just as well.
    
    CheckMasterBranchReplica1 is meant to test a gerrit (8080) replicating
    to another (8081) locally. The scenario assumes an already created and
    replicated test project, with a name specified through the already
    supported PROJECT property; cf. [1] (e.g., [2]). The core CreateProject
    scenario can be used prior, for that purpose. This is all based on the
    core replication plugin being installed and configured with at least the
    heads and meta refs pushed.
    
    Make this CheckMasterBranchReplica1 scenario use its own http password
    towards the 8081 replica, to not fail upon a 401 otherwise. The other
    CheckMasterBranchReplica1 test steps still use 8080's base http password
    for the admin test user. As usual with the core framework, this requires
    an http.password_replica entry in [3] locally; cf. [4].
    
    [1] https://gerrit-documentation.storage.googleapis.com/Documentation/3.0.9/dev-e2e-tests.html#_environment_properties
    [2] export JAVA_OPTS="-Dcom.google.gerrit.scenarios.project=CheckMasterBranchReplica1"
    [3] e2e-tests/src/test/resources/application.conf
    [4] https://gerrit-documentation.storage.googleapis.com/Documentation/3.0.9/dev-e2e-tests.html#_project_and_http_credentials
    
    Feature: Issue 12813
    Change-Id: I224d365f978bbd4cf419312ebf7116f9557d7c95

---
+++ e2e-tests/src/test/scala/com/google/gerrit/scenarios/GetMasterBranchRevision.scala
@@ -0,0 +1,41 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.scenarios
+
+import io.gatling.core.Predef._
+import io.gatling.core.feeder.FeederBuilder
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+
+class GetMasterBranchRevision extends ProjectSimulation {
+  private val data: FeederBuilder = jsonFile(resource).convert(keys).queue
+  var revision: Option[String] = None
+  val revisionKey = "revision"
+  val revisionPattern: String = "\"" + revisionKey + "\": \"(.+)\""
+
+  val test: ScenarioBuilder = scenario(unique)
+      .feed(data)
+      .exec(http(unique).get("${url}")
+          .check(regex(revisionPattern).saveAs(revisionKey)))
+      .exec(session => {
+        revision = Some(session(revisionKey).as[String])
+        session
+      })
+
+  setUp(
+    test.inject(
+      atOnceUsers(1)
+    )).protocols(httpProtocol)
+}

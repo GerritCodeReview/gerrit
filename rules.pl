c9937b08856be9eb933a07d2f9351ef9ff3b7f65
Patrick Hiesel <hiesel@google.com>
2020-06-05 08:02:37 +0000

    Require a Release-Notes footer
    As discussed in the last ESC/CM meeting, we want to establish
    a low-tech solution to make generating release notes easier.
    
    This commit is a hand-wavy attempt at checking the commit
    message for a line that starts with Release-Notes. The idea
    is that such a footer is required for all commits that
    go on a branch other than refs/meta/config.
    
    A separate change will document this new requirement.
    
    Change-Id: I72067d9006a0282a3bc1efb82f2a61f92717070b

--- rules.pl
+++ rules.pl
@@ -25,19 +25,30 @@ submit_rule(submit(CR)) :-
   !,
   gerrit:max_with_block(-2, 2, 'Code-Review', CR).
 
-submit_rule(submit(CR, CS, V, L)) :-
+submit_rule(submit(CR, CS, V, L, RN)) :-
   needs_library_compliance,
   !,
-  base(CR, CS, V),
+  base(CR, CS, V, RN),
   gerrit:max_with_block(-1, 1, 'Library-Compliance', L).
 
-submit_rule(submit(CR, CS, V)) :-
-  base(CR, CS, V).
+submit_rule(submit(CR, CS, V, RN)) :-
+  base(CR, CS, V, RN).
 
-base(CR, CS, V) :-
+base(CR, CS, V, RN) :-
+  release_notes(RN),
   gerrit:max_with_block(-2, 2, 'Code-Review', CR),
   gerrit:max_with_block(-1, 1, 'Code-Style', CS),
   gerrit:max_with_block(-1, 1, 'Verified', V).
+
+% Require a 'Release-Notes' footer.
+release_notes(RN) :-
+  gerrit:commit_message_matches('^Release-Notes:.*'),
+  !,
+  gerrit:commit_author(A),
+  RN = label(‘Contains-Release-Notes-Footer', ok(A)).
+
+release_notes(RN) :-
+  RN = label('Contains-Release-Notes-Footer', need(_)).
 
 needs_library_compliance :- gerrit:commit_delta('^lib/'), !.
 needs_library_compliance :- gerrit:commit_delta('^WORKSPACE$'), !.

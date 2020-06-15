17b16150f298d60edd0c0569ffb45ebf76e5c879
Marcin Czech <maczech@gmail.com>
2020-06-15 17:36:18 +0200

    Allow setting X-Frame-Options response header to avoid click-jacking vulnerability
    Add HTTP filter which is applied to all HTTP responses. Based on
    gerrit.canLoadInIFrame and gerrit.xframeOption properties filter adds
    the X-Frame-Options HTTP response header. The X-Frame-Options HTTP response
    header can be used to indicate whether or not a browser should be
    allowed to render a page in a <frame>, <iframe>, <embed> or <object>.
    Gerrit can use this to avoid click-jacking attacks, by ensuring that
    the content is not embedded into other sites.
    
    Bug: Issue 12926
    Change-Id: If3f6a770332ade9924b3d1a20c092637c9380e0c

---
+++ java/com/google/gerrit/httpd/AllowRenderInFrameFilter.java
@@ -0,0 +1,100 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jgit.lib.Config;
+
+@Singleton
+public class AllowRenderInFrameFilter extends AllRequestFilter {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  static final String X_FRAME_OPTIONS_HEADER_NAME = "X-Frame-Options";
+
+  enum X_FRAME_OPTIONS {
+    ALLOW,
+    SAMEORIGIN,
+    DENY;
+
+    public static List<String> names() {
+      return Arrays.stream(X_FRAME_OPTIONS.values()).map(Enum::name).collect(Collectors.toList());
+    }
+  }
+
+  private X_FRAME_OPTIONS xframeOption;
+  private boolean canLoadInIFrame;
+
+  public static Module module() {
+    return new AbstractModule() {
+
+      @Override
+      protected void configure() {
+        DynamicSet.bind(binder(), AllRequestFilter.class)
+            .to(AllowRenderInFrameFilter.class)
+            .in(Scopes.SINGLETON);
+      }
+    };
+  }
+
+  @Inject
+  public AllowRenderInFrameFilter(@GerritServerConfig Config cfg) {
+    canLoadInIFrame = cfg.getBoolean("gerrit", "canLoadInIFrame", false);
+    String frameOptionValue = cfg.getString("gerrit", null, "xframeOption");
+
+    if (!canLoadInIFrame) {
+      xframeOption = X_FRAME_OPTIONS.DENY;
+    } else {
+      try {
+        xframeOption =
+            Optional.ofNullable(frameOptionValue)
+                .map(value -> X_FRAME_OPTIONS.valueOf(value.toUpperCase()))
+                .orElse(X_FRAME_OPTIONS.ALLOW);
+      } catch (IllegalArgumentException e) {
+        logger.atWarning().log(
+            "Invalid xframeOption value, expected one of: [%s] but was: %s. xframeOption will be ignored.",
+            String.join(",", X_FRAME_OPTIONS.names()), frameOptionValue);
+        xframeOption = X_FRAME_OPTIONS.ALLOW;
+      }
+    }
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    if (response instanceof HttpServletResponse && !X_FRAME_OPTIONS.ALLOW.equals(xframeOption)) {
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+      httpResponse.addHeader(X_FRAME_OPTIONS_HEADER_NAME, xframeOption.name());
+      chain.doFilter(request, httpResponse);
+    } else {
+      chain.doFilter(request, response);
+    }
+  }
+}

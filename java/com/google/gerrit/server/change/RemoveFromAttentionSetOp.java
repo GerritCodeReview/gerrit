d70a876d280bc308b5b3c3a3bef36575f93d3cce
Gal Paikin <paiking@google.com>
2020-06-15 17:32:48 +0300

    Review endpoint with attention set
    This change contains all the relevant adjustments for the review endpoint
    to allow dogfooding the attention set feature.
    
    The first important change is about attentionSetOps: We now allow adding
    users that are already in the attention set, and we also allow removing
    users that are not in the attention set. The reasoning for this is to
    allow updating the reason for adding/removing a user to the attention set.
    
    Also, we have the logic of only accepting the first addition/removal of a
    specific user in a specific review: Thus, if we try to add a user and then
    also remove the user, the user will only be added. However, there is also
    the case that a user is already a part of the attention set, and we want
    to both add the user and remove it. Based on previous conclusion, the user
    should be added (or in this case, the user's status should not change). By
    allowing to override additions/removals, it is more natural to re-add the
    user and avoid removing them. In the previous implementation, the user would
    accidentally get removed since the first addition gets ignored (although we
    are only supposed to perform the first update). This problem can be solved
    in a more complicated way, but due to the first reason (allow editing reason)
    I feel that this solution is good.
    
    These are the changes for the review endpoint:
    1. Users can specify users to add and users to remove explictly, using
    ReviewInput's removeFromAttentionSet and addToAttentionSet. It's okay to
    specify the same user in both lists, and then the user will just be removed.
    Also, if a user is specified in any of the lists the additions/removals below
    will not be applied for that user.
    2. Replying removes the publishing user from the attention set.
    3. When the uploader replies, add the owner to the attention set.
    3. When the owner or uploader replies, add the reviewers to the attention set.
    4. When neither the uploader nor the owner replies, add the owner and uploader
    to the attention set.
    
    Note: users will not be added to the attention set if the change is work in progress.
    
    A follow-up change will include all the documentation.
    
    Change-Id: Ia4e3c0681d0fb94edcca35d1138c17bb1f5e76fc

--- java/com/google/gerrit/server/change/RemoveFromAttentionSetOp.java
+++ java/com/google/gerrit/server/change/RemoveFromAttentionSetOp.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.server.change;
 
-import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.collect.ImmutableSet;
@@ -29,8 +28,6 @@ import com.google.gerrit.server.update.BatchUpdateOp;
 import com.google.gerrit.server.update.ChangeContext;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
-import java.util.Map;
-import java.util.function.Function;
 
 /** Remove a specified user from the attention set. */
 public class RemoveFromAttentionSetOp implements BatchUpdateOp {
@@ -68,15 +65,6 @@ public class RemoveFromAttentionSetOp implements BatchUpdateOp {
 
   @Override
   public boolean updateChange(ChangeContext ctx) throws RestApiException {
-    ChangeData changeData = changeDataFactory.create(ctx.getNotes());
-    Map<Account.Id, AttentionSetUpdate> attentionMap =
-        changeData.attentionSet().stream()
-            .collect(toImmutableMap(AttentionSetUpdate::account, Function.identity()));
-    AttentionSetUpdate existingEntry = attentionMap.get(attentionUserId);
-    if (existingEntry == null || existingEntry.operation() == Operation.REMOVE) {
-      return false;
-    }
-
     ChangeUpdate update = ctx.getUpdate(ctx.getChange().currentPatchSetId());
     update.addToPlannedAttentionSetUpdates(
         ImmutableSet.of(

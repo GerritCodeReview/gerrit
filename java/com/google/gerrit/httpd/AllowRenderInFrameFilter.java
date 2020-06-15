d291d756d026da8577c803268bdb3a636334ba8f
Marcin Czech <maczech@gmail.com>
2020-06-15 17:36:18 +0200

    Allow setting X-Frame-Options response header to avoid click-jacking vulnerability
    Add HTTP filter which is applied to all HTTP responses.
    Based on gerrit.canLoadInIFrame and gerrit.xframeOption
    properties filter adds the X-Frame-Options HTTP
    response header. The X-Frame-Options HTTP response header
    can be used to indicate whether or not a browser should
    be allowed to render a page in a <frame>, <iframe>,
    <embed> or <object>. Gerrit can use this to avoid
    click-jacking attacks, by ensuring that the content is
    not embedded into other sites.
    
    Bug: Issue 12926
    Change-Id: If3f6a770332ade9924b3d1a20c092637c9380e0c

---
+++ java/com/google/gerrit/httpd/AllowRenderInFrameFilter.java
@@ -0,0 +1,75 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.httpd;
+
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jgit.lib.Config;
+
+public class AllowRenderInFrameFilter extends AllRequestFilter {
+  static final String X_FRAME_OPTIONS_HEADER_NAME = "X-Frame-Options";
+
+  enum XFrameOption {
+    ALLOW,
+    SAMEORIGIN;
+  }
+
+  private final String xframeOption;
+  private final boolean canLoadInIFrame;
+
+  public static Module module() {
+    return new AbstractModule() {
+
+      @Override
+      protected void configure() {
+        DynamicSet.bind(binder(), AllRequestFilter.class)
+            .to(AllowRenderInFrameFilter.class)
+            .in(Scopes.SINGLETON);
+      }
+    };
+  }
+
+  @Inject
+  public AllowRenderInFrameFilter(@GerritServerConfig Config cfg) {
+    canLoadInIFrame = cfg.getBoolean("gerrit", "canLoadInIFrame", false);
+    if (canLoadInIFrame) {
+      xframeOption = cfg.getEnum("gerrit", null, "xframeOption", XFrameOption.SAMEORIGIN).name();
+    } else {
+      xframeOption = "DENY";
+    }
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    if (response instanceof HttpServletResponse && !canLoadInIFrame) {
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+      httpResponse.addHeader(X_FRAME_OPTIONS_HEADER_NAME, xframeOption);
+      chain.doFilter(request, httpResponse);
+    } else {
+      chain.doFilter(request, response);
+    }
+  }
+}

73064bfd6e2e6d38245a770219fc71175627fec5
Dmitrii Filippov <dmfilippov@google.com>
2020-06-22 13:42:09 +0200

    Convert docs-url-behavior.js to typescript
    Change-Id: I712c3c0c61ce1294579e1d66930b7c1f47f8ec70

--- polygerrit-ui/app/behaviors/docs-url-behavior/docs-url-behavior.ts
+++ polygerrit-ui/app/behaviors/docs-url-behavior/docs-url-behavior.ts
@@ -14,45 +14,70 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {BaseUrlBehavior} from '../base-url-behavior/base-url-behavior.js';
+import {
+  BaseUrlBehavior,
+  BaseUrlBehaviorInterface,
+} from '../base-url-behavior/base-url-behavior';
 
 const PROBE_PATH = '/Documentation/index.html';
 const DOCS_BASE_PATH = '/Documentation';
 
-let cachedPromise;
+let cachedPromise: Promise<string | null> | undefined;
+
+// NOTE: Below we define 2 types (DocUrlBehaviorConfig and RestApi) to avoid
+// type 'any'. These are temporary definitions and they must be
+// updated/moved/removed when we start converting our codebase to typescript.
+// Right now we are using these types here just for adding typescript support to
+// our build/test infrastructure. Doing so we avoid massive code updates at this
+// stage.
+
+// TODO: introduce global gerrit config type instead of DocUrlBehaviorConfig.
+// The DocUrlBehaviorConfig is a temporary type
+interface DocUrlBehaviorConfig {
+  gerrit?: {doc_url?: string};
+}
+
+// TODO: implement RestApi type correctly and remove interface from this file
+interface RestApi {
+  probePath(url: string): Promise<boolean>;
+}
 
 /** @polymerBehavior DocsUrlBehavior */
-export const DocsUrlBehavior = [{
+export const DocsUrlBehavior = [
+  {
+    /**
+     * Get the docs base URL from either the server config or by probing.
+     *
+     * @param {Object} config The server config.
+     * @param {!Object} restApi A REST API instance
+     * @return {!Promise<string>} A promise that resolves with the docs base
+     *     URL.
+     */
+    getDocsBaseUrl(
+      this: BaseUrlBehaviorInterface,
+      config: DocUrlBehaviorConfig,
+      restApi: RestApi
+    ) {
+      if (!cachedPromise) {
+        cachedPromise = new Promise(resolve => {
+          if (config && config.gerrit && config.gerrit.doc_url) {
+            resolve(config.gerrit.doc_url);
+          } else {
+            restApi.probePath(this.getBaseUrl() + PROBE_PATH).then(ok => {
+              resolve(ok ? this.getBaseUrl() + DOCS_BASE_PATH : null);
+            });
+          }
+        });
+      }
+      return cachedPromise;
+    },
 
-  /**
-   * Get the docs base URL from either the server config or by probing.
-   *
-   * @param {Object} config The server config.
-   * @param {!Object} restApi A REST API instance
-   * @return {!Promise<string>} A promise that resolves with the docs base
-   *     URL.
-   */
-  getDocsBaseUrl(config, restApi) {
-    if (!cachedPromise) {
-      cachedPromise = new Promise(resolve => {
-        if (config && config.gerrit && config.gerrit.doc_url) {
-          resolve(config.gerrit.doc_url);
-        } else {
-          restApi.probePath(this.getBaseUrl() + PROBE_PATH).then(ok => {
-            resolve(ok ? (this.getBaseUrl() + DOCS_BASE_PATH) : null);
-          });
-        }
-      });
-    }
-    return cachedPromise;
+    /** For testing only. */
+    _clearDocsBaseUrlCache() {
+      cachedPromise = undefined;
+    },
   },
-
-  /** For testing only. */
-  _clearDocsBaseUrlCache() {
-    cachedPromise = undefined;
-  },
-},
-BaseUrlBehavior,
+  BaseUrlBehavior,
 ];
 
 // TODO(dmfilippov) Remove the following lines with assignments

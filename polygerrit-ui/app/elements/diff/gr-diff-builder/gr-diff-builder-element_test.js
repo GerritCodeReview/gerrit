3aed0a1f6e7acc59d5d10ba5af3cd4e9aced1693
Paladox none <thomasmulhall410@yahoo.com>
2020-07-03 20:40:40 +0000

    blame: Add base url to _getBlameForBaseLine
    Change-Id: I4f60801383ec07a1a0997da0226f685ad9faec16

--- polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-element_test.js
+++ polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-element_test.js
@@ -21,6 +21,7 @@ import './gr-diff-builder.js';
 import '../../shared/gr-rest-api-interface/gr-rest-api-interface.js';
 import {getMockDiffResponse} from '../../../test/mocks/diff-response.js';
 import './gr-diff-builder-element.js';
+import {BaseUrlBehavior} from '../../../behaviors/base-url-behavior/base-url-behavior.js';
 import {dom, flush} from '@polymer/polymer/lib/legacy/polymer.dom.js';
 import {GrAnnotation} from '../gr-diff-highlight/gr-annotation.js';
 import {GrDiffLine} from '../gr-diff/gr-diff-line.js';
@@ -62,6 +63,7 @@ suite('gr-diff-builder tests', () => {
       getLoggedIn() { return Promise.resolve(false); },
       getProjectConfig() { return Promise.resolve({}); },
     });
+    sinon.stub(BaseUrlBehavior, 'getBaseUrl').returns('/r');
     prefs = {
       line_length: 10,
       show_tabs: true,
@@ -1230,6 +1232,9 @@ suite('gr-diff-builder tests', () => {
           `Commit 1234567890<br>Author: Clark Kent<br>Date: ${date}`
         + '<br><br>Testing Commit'
       );
+
+      const url = blameNode.getElementsByClassName('blameDate');
+      assert.equal(url[0].getAttribute('href'), '/r/q/1234567890');
     });
   });
 });

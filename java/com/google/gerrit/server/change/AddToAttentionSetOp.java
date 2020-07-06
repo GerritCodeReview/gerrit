ef41f854ce1dca9c11cd2555963d60802eb3750d
Gal Paikin <paiking@google.com>
2020-07-06 16:34:36 +0300

    Add email sending for attention set updates
    Currently, we don't send any emails when updating the attention set.
    This change adds email sendings by default to OWNER, when adding
    users to the attention set or removing them from it, but only when
    added/removed explictly. Explict removals are through the review
    endpoint, or through the dedicated attention set endpoints.
    
    All of the explict updates allow specifying AttentionSetInput,
    and now it is also possible to specify notify and notify_details
    to change the receivers of the attention set emails.
    
    Change-Id: Ifca8e2dd5de92a608fa987ea894d6326d1e88135

--- java/com/google/gerrit/server/change/AddToAttentionSetOp.java
+++ java/com/google/gerrit/server/change/AddToAttentionSetOp.java
@@ -14,26 +14,44 @@
 
 package com.google.gerrit.server.change;
 
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.entities.Account;
 import com.google.gerrit.entities.AttentionSetUpdate;
+import com.google.gerrit.entities.Change;
 import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.server.account.AccountState;
+import com.google.gerrit.server.mail.send.AddToAttentionSetSender;
+import com.google.gerrit.server.mail.send.MessageIdGenerator;
 import com.google.gerrit.server.notedb.ChangeUpdate;
+import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.update.BatchUpdateOp;
 import com.google.gerrit.server.update.ChangeContext;
+import com.google.gerrit.server.update.Context;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
+import java.util.Map;
+import java.util.function.Function;
 
 /** Add a specified user to the attention set. */
 public class AddToAttentionSetOp implements BatchUpdateOp {
 
   public interface Factory {
-    AddToAttentionSetOp create(Account.Id attentionUserId, String reason);
+    AddToAttentionSetOp create(Account.Id attentionUserId, String reason, boolean sendEmails);
   }
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private final ChangeData.Factory changeDataFactory;
+  private final MessageIdGenerator messageIdGenerator;
+  private final AddToAttentionSetSender.Factory addToAttentionSetSender;
 
   private final Account.Id attentionUserId;
   private final String reason;
+
+  private Change change;
+  private boolean sendEmails;
 
   /**
    * Add a specified user to the attention set.
@@ -42,17 +60,64 @@ public class AddToAttentionSetOp implements BatchUpdateOp {
    * @param reason The reason for adding that user.
    */
   @Inject
-  AddToAttentionSetOp(@Assisted Account.Id attentionUserId, @Assisted String reason) {
+  AddToAttentionSetOp(
+      ChangeData.Factory changeDataFactory,
+      AddToAttentionSetSender.Factory addToAttentionSetSender,
+      MessageIdGenerator messageIdGenerator,
+      @Assisted Account.Id attentionUserId,
+      @Assisted String reason,
+      @Assisted boolean sendEmails) {
+    this.changeDataFactory = changeDataFactory;
+    this.addToAttentionSetSender = addToAttentionSetSender;
+    this.messageIdGenerator = messageIdGenerator;
     this.attentionUserId = requireNonNull(attentionUserId, "user");
     this.reason = requireNonNull(reason, "reason");
+    this.sendEmails = sendEmails;
   }
 
   @Override
   public boolean updateChange(ChangeContext ctx) throws RestApiException {
+    ChangeData changeData = changeDataFactory.create(ctx.getNotes());
+    Map<Account.Id, AttentionSetUpdate> attentionMap =
+        changeData.attentionSet().stream()
+            .collect(toImmutableMap(AttentionSetUpdate::account, Function.identity()));
+    AttentionSetUpdate existingEntry = attentionMap.get(attentionUserId);
+    if (existingEntry != null && existingEntry.operation() == AttentionSetUpdate.Operation.ADD) {
+      // We still need to perform this update to ensure that we don't remove the user in a follow-up
+      // operation, but no need to send an email about it.
+      sendEmails = false;
+    }
+
+    change = ctx.getChange();
+
     ChangeUpdate update = ctx.getUpdate(ctx.getChange().currentPatchSetId());
     update.addToPlannedAttentionSetUpdates(
         AttentionSetUpdate.createForWrite(
             attentionUserId, AttentionSetUpdate.Operation.ADD, reason));
     return true;
+  }
+
+  @Override
+  public void postUpdate(Context ctx) {
+    if (!sendEmails) {
+      return;
+    }
+    NotifyResolver.Result notify = ctx.getNotify(change.getId());
+    try {
+      AddToAttentionSetSender cm = addToAttentionSetSender.create(ctx.getProject(), change.getId());
+      AccountState accountState =
+          ctx.getUser().isIdentifiedUser() ? ctx.getUser().asIdentifiedUser().state() : null;
+      if (accountState != null) {
+        cm.setFrom(accountState.account().id());
+      }
+      cm.setNotify(notify);
+      cm.setAttentionSetUser(attentionUserId);
+      cm.setReason(reason);
+      cm.setMessageId(
+          messageIdGenerator.fromChangeUpdate(ctx.getRepoView(), change.currentPatchSetId()));
+      cm.send();
+    } catch (Exception e) {
+      logger.atSevere().withCause(e).log("Cannot email update for change %s", change.getId());
+    }
   }
 }

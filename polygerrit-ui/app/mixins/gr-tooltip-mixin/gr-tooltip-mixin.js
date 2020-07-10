f931e1d5a3784e505a8052e40d75db867a9effa0
Dmitrii Filippov <dmfilippov@google.com>
2020-07-10 21:32:47 +0200

    Convert gr-tooltip-behavior to mixin
    Change-Id: I08a28a475e492729fae9e183563ae7f47bcec319

--- polygerrit-ui/app/mixins/gr-tooltip-mixin/gr-tooltip-mixin.js
+++ polygerrit-ui/app/mixins/gr-tooltip-mixin/gr-tooltip-mixin.js
@@ -17,155 +17,163 @@
 import '../../elements/shared/gr-tooltip/gr-tooltip.js';
 import {flush} from '@polymer/polymer/lib/legacy/polymer.dom.js';
 import {getRootElement} from '../../scripts/rootElement.js';
+import {dedupingMixin} from '@polymer/polymer/lib/utils/mixin.js';
 
 const BOTTOM_OFFSET = 7.2; // Height of the arrow in tooltip.
 
-/** @polymerBehavior Gerrit.TooltipBehavior */
-export const TooltipBehavior = {
+/**
+ * @polymer
+ * @mixinFunction
+ */
+export const TooltipMixin = dedupingMixin(superClass => {
+  /**
+   * @polymer
+   * @mixinClass
+   */
+  class Mixin extends superClass {
+    static get properties() {
+      return {
+        hasTooltip: {
+          type: Boolean,
+          observer: '_setupTooltipListeners',
+        },
+        positionBelow: {
+          type: Boolean,
+          value: false,
+          reflectToAttribute: true,
+        },
 
-  properties: {
-    hasTooltip: {
-      type: Boolean,
-      observer: '_setupTooltipListeners',
-    },
-    positionBelow: {
-      type: Boolean,
-      value: false,
-      reflectToAttribute: true,
-    },
-
-    _isTouchDevice: {
-      type: Boolean,
-      value() {
-        return 'ontouchstart' in document.documentElement;
-      },
-    },
-    _tooltip: Object,
-    _titleText: String,
-    _hasSetupTooltipListeners: {
-      type: Boolean,
-      value: false,
-    },
-  },
-
-  /** @override */
-  detached() {
-    // NOTE: if you define your own `detached` in your component
-    // then this won't take affect (as its not a class yet)
-    this._handleHideTooltip();
-    this.removeEventListener('mouseenter', this._mouseenterHandler);
-  },
-
-  _setupTooltipListeners() {
-    if (!this._mouseenterHandler) {
-      this._mouseenterHandler = this._handleShowTooltip.bind(this);
+        _isTouchDevice: {
+          type: Boolean,
+          value() {
+            return 'ontouchstart' in document.documentElement;
+          },
+        },
+        _tooltip: Object,
+        _titleText: String,
+        _hasSetupTooltipListeners: {
+          type: Boolean,
+          value: false,
+        },
+      };
     }
 
-    if (!this.hasTooltip) {
-      // if attribute set to false, remove the listener
+    /** @override */
+    disconnectedCallback() {
+      super.disconnectedCallback();
+      // NOTE: if you define your own `detached` in your component
+      // then this won't take affect (as its not a class yet)
+      this._handleHideTooltip();
       this.removeEventListener('mouseenter', this._mouseenterHandler);
-      this._hasSetupTooltipListeners = false;
-      return;
     }
 
-    if (this._hasSetupTooltipListeners) {
-      return;
-    }
-    this._hasSetupTooltipListeners = true;
+    _setupTooltipListeners() {
+      if (!this._mouseenterHandler) {
+        this._mouseenterHandler = this._handleShowTooltip.bind(this);
+      }
 
-    this.addEventListener('mouseenter', this._mouseenterHandler);
-  },
+      if (!this.hasTooltip) {
+        // if attribute set to false, remove the listener
+        this.removeEventListener('mouseenter', this._mouseenterHandler);
+        this._hasSetupTooltipListeners = false;
+        return;
+      }
 
-  _handleShowTooltip(e) {
-    if (this._isTouchDevice) { return; }
+      if (this._hasSetupTooltipListeners) {
+        return;
+      }
+      this._hasSetupTooltipListeners = true;
 
-    if (!this.hasAttribute('title') ||
-        this.getAttribute('title') === '' ||
-        this._tooltip) {
-      return;
-    }
-
-    // Store the title attribute text then set it to an empty string to
-    // prevent it from showing natively.
-    this._titleText = this.getAttribute('title');
-    this.setAttribute('title', '');
-
-    const tooltip = document.createElement('gr-tooltip');
-    tooltip.text = this._titleText;
-    tooltip.maxWidth = this.getAttribute('max-width');
-    tooltip.positionBelow = this.getAttribute('position-below');
-
-    // Set visibility to hidden before appending to the DOM so that
-    // calculations can be made based on the element’s size.
-    tooltip.style.visibility = 'hidden';
-    getRootElement().appendChild(tooltip);
-    this._positionTooltip(tooltip);
-    tooltip.style.visibility = null;
-
-    this._tooltip = tooltip;
-    this.listen(window, 'scroll', '_handleWindowScroll');
-    this.listen(this, 'mouseleave', '_handleHideTooltip');
-    this.listen(this, 'click', '_handleHideTooltip');
-  },
-
-  _handleHideTooltip(e) {
-    if (this._isTouchDevice) { return; }
-    if (!this.hasAttribute('title') ||
-        this._titleText == null) {
-      return;
+      this.addEventListener('mouseenter', this._mouseenterHandler);
     }
 
-    this.unlisten(window, 'scroll', '_handleWindowScroll');
-    this.unlisten(this, 'mouseleave', '_handleHideTooltip');
-    this.unlisten(this, 'click', '_handleHideTooltip');
-    this.setAttribute('title', this._titleText);
-    if (this._tooltip && this._tooltip.parentNode) {
-      this._tooltip.parentNode.removeChild(this._tooltip);
+    _handleShowTooltip(e) {
+      if (this._isTouchDevice) { return; }
+
+      if (!this.hasAttribute('title') ||
+          this.getAttribute('title') === '' ||
+          this._tooltip) {
+        return;
+      }
+
+      // Store the title attribute text then set it to an empty string to
+      // prevent it from showing natively.
+      this._titleText = this.getAttribute('title');
+      this.setAttribute('title', '');
+
+      const tooltip = document.createElement('gr-tooltip');
+      tooltip.text = this._titleText;
+      tooltip.maxWidth = this.getAttribute('max-width');
+      tooltip.positionBelow = this.getAttribute('position-below');
+
+      // Set visibility to hidden before appending to the DOM so that
+      // calculations can be made based on the element’s size.
+      tooltip.style.visibility = 'hidden';
+      getRootElement().appendChild(tooltip);
+      this._positionTooltip(tooltip);
+      tooltip.style.visibility = null;
+
+      this._tooltip = tooltip;
+      this.listen(window, 'scroll', '_handleWindowScroll');
+      this.listen(this, 'mouseleave', '_handleHideTooltip');
+      this.listen(this, 'click', '_handleHideTooltip');
     }
-    this._tooltip = null;
-  },
 
-  _handleWindowScroll(e) {
-    if (!this._tooltip) { return; }
+    _handleHideTooltip(e) {
+      if (this._isTouchDevice) { return; }
+      if (!this.hasAttribute('title') ||
+          this._titleText == null) {
+        return;
+      }
 
-    this._positionTooltip(this._tooltip);
-  },
-
-  _positionTooltip(tooltip) {
-    // This flush is needed for tooltips to be positioned correctly in Firefox
-    // and Safari.
-    flush();
-    const rect = this.getBoundingClientRect();
-    const boxRect = tooltip.getBoundingClientRect();
-    const parentRect = tooltip.parentElement.getBoundingClientRect();
-    const top = rect.top - parentRect.top;
-    const left =
-        rect.left - parentRect.left + (rect.width - boxRect.width) / 2;
-    const right = parentRect.width - left - boxRect.width;
-    if (left < 0) {
-      tooltip.updateStyles({
-        '--gr-tooltip-arrow-center-offset': left + 'px',
-      });
-    } else if (right < 0) {
-      tooltip.updateStyles({
-        '--gr-tooltip-arrow-center-offset': (-0.5 * right) + 'px',
-      });
+      this.unlisten(window, 'scroll', '_handleWindowScroll');
+      this.unlisten(this, 'mouseleave', '_handleHideTooltip');
+      this.unlisten(this, 'click', '_handleHideTooltip');
+      this.setAttribute('title', this._titleText);
+      if (this._tooltip && this._tooltip.parentNode) {
+        this._tooltip.parentNode.removeChild(this._tooltip);
+      }
+      this._tooltip = null;
     }
-    tooltip.style.left = Math.max(0, left) + 'px';
 
-    if (!this.positionBelow) {
-      tooltip.style.top = Math.max(0, top) + 'px';
-      tooltip.style.transform = 'translateY(calc(-100% - ' + BOTTOM_OFFSET +
-          'px))';
-    } else {
-      tooltip.style.top = top + rect.height + BOTTOM_OFFSET + 'px';
+    _handleWindowScroll(e) {
+      if (!this._tooltip) { return; }
+
+      this._positionTooltip(this._tooltip);
     }
-  },
-};
 
-// TODO(dmfilippov) Remove the following lines with assignments
-// Plugins can use the behavior because it was accessible with
-// the global Gerrit... variable. To avoid breaking changes in plugins
-// temporary assign global variables.
-window.Gerrit = window.Gerrit || {};
-window.Gerrit.TooltipBehavior = TooltipBehavior;
+    _positionTooltip(tooltip) {
+      // This flush is needed for tooltips to be positioned correctly in Firefox
+      // and Safari.
+      flush();
+      const rect = this.getBoundingClientRect();
+      const boxRect = tooltip.getBoundingClientRect();
+      const parentRect = tooltip.parentElement.getBoundingClientRect();
+      const top = rect.top - parentRect.top;
+      const left =
+          rect.left - parentRect.left + (rect.width - boxRect.width) / 2;
+      const right = parentRect.width - left - boxRect.width;
+      if (left < 0) {
+        tooltip.updateStyles({
+          '--gr-tooltip-arrow-center-offset': left + 'px',
+        });
+      } else if (right < 0) {
+        tooltip.updateStyles({
+          '--gr-tooltip-arrow-center-offset': (-0.5 * right) + 'px',
+        });
+      }
+      tooltip.style.left = Math.max(0, left) + 'px';
+
+      if (!this.positionBelow) {
+        tooltip.style.top = Math.max(0, top) + 'px';
+        tooltip.style.transform = 'translateY(calc(-100% - ' + BOTTOM_OFFSET +
+            'px))';
+      } else {
+        tooltip.style.top = top + rect.height + BOTTOM_OFFSET + 'px';
+      }
+    }
+  }
+
+  return Mixin;
+});
+

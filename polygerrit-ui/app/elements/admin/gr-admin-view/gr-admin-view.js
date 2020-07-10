b202442a727ee5e4fc54343711fecbb0cae6ac38
Dmitrii Filippov <dmfilippov@google.com>
2020-07-10 11:52:14 +0200

    Convert admin-nav-behaviors to admin-nav-util
    Change-Id: I41179130bc4f6611521e9e9985a02f96460d707a

--- polygerrit-ui/app/elements/admin/gr-admin-view/gr-admin-view.js
+++ polygerrit-ui/app/elements/admin/gr-admin-view/gr-admin-view.js
@@ -33,26 +33,23 @@ import '../gr-repo-commands/gr-repo-commands.js';
 import '../gr-repo-dashboards/gr-repo-dashboards.js';
 import '../gr-repo-detail-list/gr-repo-detail-list.js';
 import '../gr-repo-list/gr-repo-list.js';
-import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-admin-view_html.js';
 import {getBaseUrl} from '../../../utils/url-util.js';
-import {AdminNavBehavior} from '../../../behaviors/gr-admin-nav-behavior/gr-admin-nav-behavior.js';
 import {GerritNav} from '../../core/gr-navigation/gr-navigation.js';
 import {pluginLoader} from '../../shared/gr-js-api-interface/gr-plugin-loader.js';
+import {getAdminLinks} from '../../../utils/admin-nav-util.js';
 
 const INTERNAL_GROUP_REGEX = /^[\da-f]{40}$/;
 
 /**
  * @extends PolymerElement
  */
-class GrAdminView extends mixinBehaviors( [
-  AdminNavBehavior,
-], GestureEventListeners(
+class GrAdminView extends GestureEventListeners(
     LegacyElementMixin(
-        PolymerElement))) {
+        PolymerElement)) {
   static get template() { return htmlTemplate; }
 
   static get is() { return 'gr-admin-view'; }
@@ -132,7 +129,7 @@ class GrAdminView extends mixinBehaviors( [
         };
       }
 
-      return this.getAdminLinks(this._account,
+      return getAdminLinks(this._account,
           this.$.restAPI.getAccountCapabilities.bind(this.$.restAPI),
           this.$.jsAPI.getAdminMenuLinks.bind(this.$.jsAPI),
           options)

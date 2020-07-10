52da2eae1f96f71b826f82d0ac53c02cbd4d26d0
Dmitrii Filippov <dmfilippov@google.com>
2020-07-10 21:32:47 +0200

    Convert gr-tooltip-behavior to mixin
    Change-Id: I08a28a475e492729fae9e183563ae7f47bcec319

--- polygerrit-ui/app/elements/shared/gr-tooltip-content/gr-tooltip-content.js
+++ polygerrit-ui/app/elements/shared/gr-tooltip-content/gr-tooltip-content.js
@@ -15,21 +15,19 @@
  * limitations under the License.
  */
 import '../gr-icons/gr-icons.js';
-import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-tooltip-content_html.js';
-import {TooltipBehavior} from '../../../behaviors/gr-tooltip-behavior/gr-tooltip-behavior.js';
+import {TooltipMixin} from '../../../mixins/gr-tooltip-mixin/gr-tooltip-mixin.js';
 
 /**
  * @extends PolymerElement
  */
-class GrTooltipContent extends mixinBehaviors( [
-  TooltipBehavior,
-], GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement))) {
+class GrTooltipContent extends TooltipMixin(
+    GestureEventListeners(
+        LegacyElementMixin(
+            PolymerElement))) {
   static get template() { return htmlTemplate; }
 
   static get is() { return 'gr-tooltip-content'; }

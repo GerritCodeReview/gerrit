78b8bf457166f2bb0890e6bfb9bdf521e86f71f8
Dmitrii Filippov <dmfilippov@google.com>
2020-07-10 14:00:36 +0200

    Convert gr-change-table-behavior to mixin
    Change-Id: I436f95b2771da0a33138709ad6ecf63f9229b970

--- polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-view.js
+++ polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-view.js
@@ -40,13 +40,12 @@ import '../gr-identities/gr-identities.js';
 import '../gr-menu-editor/gr-menu-editor.js';
 import '../gr-ssh-editor/gr-ssh-editor.js';
 import '../gr-watched-projects-editor/gr-watched-projects-editor.js';
-import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-settings-view_html.js';
 import {getDocsBaseUrl} from '../../../utils/url-util.js';
-import {ChangeTableBehavior} from '../../../behaviors/gr-change-table-behavior/gr-change-table-behavior.js';
+import {ChangeTableMixin} from '../../../mixins/gr-change-table-mixin/gr-change-table-mixin.js';
 
 const PREFS_SECTION_FIELDS = [
   'changes_per_page',
@@ -77,9 +76,7 @@ const HTTP_AUTH = [
 /**
  * @extends PolymerElement
  */
-class GrSettingsView extends mixinBehaviors( [
-  ChangeTableBehavior,
-], GestureEventListeners(
+class GrSettingsView extends ChangeTableMixin(GestureEventListeners(
     LegacyElementMixin(
         PolymerElement))) {
   static get template() { return htmlTemplate; }

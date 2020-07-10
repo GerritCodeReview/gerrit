6f848426fc3e2e5fedff27a038ea7bffd71c2515
Dmitrii Filippov <dmfilippov@google.com>
2020-07-10 19:09:55 +0200

    Move constants from repo-plugin-config-behavior to the element file
    The repo-plugin-config-behavior is used only in one place and can be
    removed.
    
    Change-Id: I522f6a31d5ddbfc46f32e30b9ac7d8b889688ebb

--- polygerrit-ui/app/elements/admin/gr-repo-plugin-config/gr-repo-plugin-config.js
+++ polygerrit-ui/app/elements/admin/gr-repo-plugin-config/gr-repo-plugin-config.js
@@ -24,21 +24,30 @@ import '../../shared/gr-select/gr-select.js';
 import '../../shared/gr-tooltip-content/gr-tooltip-content.js';
 import '../gr-plugin-config-array-editor/gr-plugin-config-array-editor.js';
 import {dom} from '@polymer/polymer/lib/legacy/polymer.dom.js';
-import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-repo-plugin-config_html.js';
-import {RepoPluginConfig} from '../../../behaviors/gr-repo-plugin-config-behavior/gr-repo-plugin-config-behavior.js';
+
+// Should be kept in sync with
+// gerrit/java/com/google/gerrit/extensions/api/projects/ProjectConfigEntryType.java.
+const CONFIG_ENTRY_TYPE = {
+  ARRAY: 'ARRAY',
+  BOOLEAN: 'BOOLEAN',
+  INT: 'INT',
+  LIST: 'LIST',
+  LONG: 'LONG',
+  STRING: 'STRING',
+};
+
+const PLUGIN_CONFIG_CHANGED_EVENT_NAME = 'plugin-config-changed';
 
 /**
  * @extends PolymerElement
  */
-class GrRepoPluginConfig extends mixinBehaviors( [
-  RepoPluginConfig,
-], GestureEventListeners(
+class GrRepoPluginConfig extends GestureEventListeners(
     LegacyElementMixin(
-        PolymerElement))) {
+        PolymerElement)) {
   static get template() { return htmlTemplate; }
 
   static get is() { return 'gr-repo-plugin-config'; }
@@ -70,22 +79,22 @@ class GrRepoPluginConfig extends mixinBehaviors( [
   }
 
   _isArray(type) {
-    return type === this.ENTRY_TYPES.ARRAY;
+    return type === CONFIG_ENTRY_TYPE.ARRAY;
   }
 
   _isBoolean(type) {
-    return type === this.ENTRY_TYPES.BOOLEAN;
+    return type === CONFIG_ENTRY_TYPE.BOOLEAN;
   }
 
   _isList(type) {
-    return type === this.ENTRY_TYPES.LIST;
+    return type === CONFIG_ENTRY_TYPE.LIST;
   }
 
   _isString(type) {
     // Treat numbers like strings for simplicity.
-    return type === this.ENTRY_TYPES.STRING ||
-        type === this.ENTRY_TYPES.INT ||
-        type === this.ENTRY_TYPES.LONG;
+    return type === CONFIG_ENTRY_TYPE.STRING ||
+        type === CONFIG_ENTRY_TYPE.INT ||
+        type === CONFIG_ENTRY_TYPE.LONG;
   }
 
   _computeDisabled(editable) {
@@ -147,8 +156,8 @@ class GrRepoPluginConfig extends mixinBehaviors( [
       notifyPath: `${name}.${notifyPath}`,
     };
 
-    this.dispatchEvent(new CustomEvent(
-        this.PLUGIN_CONFIG_CHANGED, {detail, bubbles: true, composed: true}));
+    this.dispatchEvent(new CustomEvent(PLUGIN_CONFIG_CHANGED_EVENT_NAME,
+        {detail, bubbles: true, composed: true}));
   }
 
   /**

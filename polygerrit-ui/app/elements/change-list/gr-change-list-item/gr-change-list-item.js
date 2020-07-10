bad8ee83a06f9b83ec1d4835e579d5347b14883f
Dmitrii Filippov <dmfilippov@google.com>
2020-07-10 14:00:36 +0200

    Convert gr-change-table-behavior to mixin
    Change-Id: I436f95b2771da0a33138709ad6ecf63f9229b970

--- polygerrit-ui/app/elements/change-list/gr-change-list-item/gr-change-list-item.js
+++ polygerrit-ui/app/elements/change-list/gr-change-list-item/gr-change-list-item.js
@@ -31,7 +31,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-change-list-item_html.js';
-import {ChangeTableBehavior} from '../../../behaviors/gr-change-table-behavior/gr-change-table-behavior.js';
+import {ChangeTableMixin} from '../../../mixins/gr-change-table-mixin/gr-change-table-mixin.js';
 import {PathListBehavior} from '../../../behaviors/gr-path-list-behavior/gr-path-list-behavior.js';
 import {RESTClientBehavior} from '../../../behaviors/rest-client-behavior/rest-client-behavior.js';
 import {GerritNav} from '../../core/gr-navigation/gr-navigation.js';
@@ -54,13 +54,12 @@ const PRIMARY_REVIEWERS_COUNT = 2;
  * @appliesMixin RESTClientMixin
  * @extends PolymerElement
  */
-class GrChangeListItem extends mixinBehaviors( [
-  ChangeTableBehavior,
+class GrChangeListItem extends ChangeTableMixin(mixinBehaviors( [
   PathListBehavior,
   RESTClientBehavior,
 ], GestureEventListeners(
     LegacyElementMixin(
-        PolymerElement))) {
+        PolymerElement)))) {
   static get template() { return htmlTemplate; }
 
   static get is() { return 'gr-change-list-item'; }

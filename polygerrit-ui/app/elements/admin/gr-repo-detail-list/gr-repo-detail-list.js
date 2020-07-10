6c748c08059ba155a59c426722ac63f4bd706f7b
Dmitrii Filippov <dmfilippov@google.com>
2020-07-10 14:38:50 +0200

    Convert gr-list-view-behavior to mixin
    Change-Id: I596265c1be604e22c22d3d6ae7bd5df271224761

--- polygerrit-ui/app/elements/admin/gr-repo-detail-list/gr-repo-detail-list.js
+++ polygerrit-ui/app/elements/admin/gr-repo-detail-list/gr-repo-detail-list.js
@@ -29,12 +29,11 @@ import '../../shared/gr-rest-api-interface/gr-rest-api-interface.js';
 import '../gr-create-pointer-dialog/gr-create-pointer-dialog.js';
 import '../gr-confirm-delete-item-dialog/gr-confirm-delete-item-dialog.js';
 import {flush} from '@polymer/polymer/lib/legacy/polymer.dom.js';
-import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-repo-detail-list_html.js';
-import {ListViewBehavior} from '../../../behaviors/gr-list-view-behavior/gr-list-view-behavior.js';
+import {ListViewMixin} from '../../../mixins/gr-list-view-mixin/gr-list-view-mixin.js';
 import {encodeURL} from '../../../utils/url-util.js';
 
 const DETAIL_TYPES = {
@@ -48,9 +47,7 @@ const PGP_START = '-----BEGIN PGP SIGNATURE-----';
  * @appliesMixin ListViewMixin
  * @extends PolymerElement
  */
-class GrRepoDetailList extends mixinBehaviors( [
-  ListViewBehavior,
-], GestureEventListeners(
+class GrRepoDetailList extends ListViewMixin(GestureEventListeners(
     LegacyElementMixin(
         PolymerElement))) {
   static get template() { return htmlTemplate; }

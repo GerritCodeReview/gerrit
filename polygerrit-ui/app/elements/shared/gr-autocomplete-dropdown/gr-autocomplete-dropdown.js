d900f38a291bad142a920397ff06b00e9fbe8c30
Dmitrii Filippov <dmfilippov@google.com>
2020-07-12 13:49:36 +0200

    Add simple mixins to allow easy typing in the future
    Change-Id: I65b786c31c806b4c5af16eeed3793e4b1c677dde

--- polygerrit-ui/app/elements/shared/gr-autocomplete-dropdown/gr-autocomplete-dropdown.js
+++ polygerrit-ui/app/elements/shared/gr-autocomplete-dropdown/gr-autocomplete-dropdown.js
@@ -15,25 +15,21 @@
  * limitations under the License.
  */
 import '@polymer/iron-dropdown/iron-dropdown.js';
-import {IronFitBehavior} from '@polymer/iron-fit-behavior/iron-fit-behavior.js';
 import '../gr-cursor-manager/gr-cursor-manager.js';
 import '../../../styles/shared-styles.js';
 import {flush} from '@polymer/polymer/lib/legacy/polymer.dom.js';
-import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-autocomplete-dropdown_html.js';
 import {KeyboardShortcutMixin} from '../../../mixins/keyboard-shortcut-mixin/keyboard-shortcut-mixin.js';
+import {IronFitMixin} from '../../../mixins/iron-fit-mixin/iron-fit-mixin.js';
 
 /**
  * @extends PolymerElement
  */
-class GrAutocompleteDropdown extends mixinBehaviors( [
-  IronFitBehavior,
-], KeyboardShortcutMixin(GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement)))) {
+class GrAutocompleteDropdown extends IronFitMixin(KeyboardShortcutMixin(
+    GestureEventListeners(LegacyElementMixin(PolymerElement)))) {
   static get template() { return htmlTemplate; }
 
   static get is() { return 'gr-autocomplete-dropdown'; }

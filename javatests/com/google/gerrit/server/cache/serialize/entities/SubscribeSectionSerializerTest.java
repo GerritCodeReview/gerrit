3c7e8fa8183f4af7fadc3dfbde12127d4df5cc40
Patrick Hiesel <hiesel@google.com>
2020-07-16 10:52:51 +0200

    Add serializer for SubscribeSection
    This commit adds a serializer for the SubscribeSection entitiy.
    The eventual goal is that we serialize CachedProjectConfig. The
    entity is too large to be serialized directly, though, so we
    divide an conquer.
    
    Change-Id: I0333f8cf8582eb9d3028ea3f5b35120b43d209c4

---
+++ javatests/com/google/gerrit/server/cache/serialize/entities/SubscribeSectionSerializerTest.java
@@ -0,0 +1,37 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.cache.serialize.entities;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.server.cache.serialize.entities.SubscribeSectionSerializer.deserialize;
+import static com.google.gerrit.server.cache.serialize.entities.SubscribeSectionSerializer.serialize;
+
+import com.google.gerrit.common.data.SubscribeSection;
+import com.google.gerrit.entities.Project;
+import org.junit.Test;
+
+public class SubscribeSectionSerializerTest {
+  @Test
+  public void roundTrip() {
+    SubscribeSection autoValue =
+        SubscribeSection.builder(Project.nameKey("project"))
+            .addMultiMatchRefSpec("multi")
+            .addMultiMatchRefSpec("multi2")
+            .addMatchingRefSpec("matching1")
+            .addMatchingRefSpec("matching2")
+            .build();
+    assertThat(deserialize(serialize(autoValue))).isEqualTo(autoValue);
+  }
+}

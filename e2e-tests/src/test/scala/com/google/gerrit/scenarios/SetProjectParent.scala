5cdd9ffc3af318ce518cf873e006a849ada9285e
Marco Miller <marco.miller@ericsson.com>
2020-07-22 10:45:28 -0400

    e2e-tests: Add functional SetProjectParent scenario
    Add an initial, functional SetProjectParent scenario, based on the
    endpoint [1]. Make it usable either a) alone by itself or b) through
    aggregating scenario composition.
    
    The latter case is enabled by making the scenario test value
    non-private. That case enables internal scenarios in need of composing
    this scenario. For example, the deployment case of requiring a specific
    parent project with push access permissions granted.
    
    The name of the parent project can be set using the hereby introduced
    environment property [2], using the already documented method [3].
    
    [1] https://gerrit-documentation.storage.googleapis.com/Documentation/3.0.11/rest-api-projects.html#set-project-parent
    
    [2] -Dcom.google.gerrit.scenarios.parent=parentProjectName
    
    [3] https://gerrit-documentation.storage.googleapis.com/Documentation/3.0.11/dev-e2e-tests.html#_environment_properties
    
    Change-Id: Iea43946a18ac42bb5533c17d1e839fd99798b185

---
+++ e2e-tests/src/test/scala/com/google/gerrit/scenarios/SetProjectParent.scala
@@ -0,0 +1,36 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.scenarios
+
+import io.gatling.core.Predef._
+import io.gatling.core.feeder.FeederBuilder
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef.http
+
+class SetProjectParent extends ProjectSimulation {
+  private val data: FeederBuilder = jsonFile(resource).convert(keys).queue
+
+  // non-private; used at Ericsson in internally aggregating scenarios.
+  val test: ScenarioBuilder = scenario(unique)
+      .feed(data)
+      .exec(http(unique)
+          .post("${url}")
+          .body(ElFileBody(body)).asJson)
+
+  setUp(
+    test.inject(
+      atOnceUsers(single)
+    )).protocols(httpProtocol)
+}

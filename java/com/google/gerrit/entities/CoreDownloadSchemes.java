a477478b1f7ac7ceb53067513ff48eaadb28644c
Mike Frysinger <vapier@google.com>
2020-07-22 03:02:30 -0400

    download-commands: rename REPO_DOWNLOAD to REPO
    The "download" suffix is redundant and reflects how `repo` used to
    be used in the download plugin -- as kind of a pseudo download
    bolted onto the side.  Since we've reworked the plugin, we can
    rename this constant to better reflect its usage.
    
    Change-Id: I948c7f2f427dec64c2617f834a5f7b35e7179543

--- java/com/google/gerrit/entities/CoreDownloadSchemes.java
+++ java/com/google/gerrit/entities/CoreDownloadSchemes.java
@@ -20,7 +20,7 @@ public class CoreDownloadSchemes {
   public static final String ANON_HTTP = "anonymous http";
   public static final String HTTP = "http";
   public static final String SSH = "ssh";
-  public static final String REPO_DOWNLOAD = "repo";
+  public static final String REPO = "repo";
 
   private CoreDownloadSchemes() {}
 }

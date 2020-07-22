84d38db8772091c8f3d4e37a1dec05dcd9f20249
Dhruv Srivastava <dhruvsri@google.com>
2020-07-22 16:18:09 +0200

    Track currently editing unsaved drafts
    Change-Id: I65f149f38147e11b5c7434c4b2216197b2b19f63

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.js
@@ -413,6 +413,10 @@ class GrChangeView extends KeyboardShortcutMixin(
         type: Boolean,
         value: false,
       },
+      _currentlyEditingDrafts: {
+        type: Object,
+        value: {},
+      },
     };
   }
 
@@ -479,6 +483,9 @@ class GrChangeView extends KeyboardShortcutMixin(
 
     this.addEventListener('open-reply-dialog',
         e => this._openReplyDialog());
+
+    this.addEventListener('editing-draft-changed',
+        e => this._handleEditingDraftChanged(e));
   }
 
   /** @override */
@@ -858,7 +865,7 @@ class GrChangeView extends KeyboardShortcutMixin(
   _handleCommentSave(e) {
     const draft = e.detail.comment;
     if (!draft.__draft) { return; }
-
+    this._currentlyEditingDrafts[draft.__draftID] = false;
     draft.patch_set = draft.patch_set || this._patchRange.patchNum;
 
     // The use of path-based notification helpers (set, push) can’t be used
@@ -887,10 +894,14 @@ class GrChangeView extends KeyboardShortcutMixin(
     this._diffDrafts = diffDrafts;
   }
 
+  _handleEditingDraftChanged(e) {
+    const {draftID, editing} = e.detail;
+    this._currentlyEditingDrafts[draftID] = editing;
+  }
+
   _handleCommentDiscard(e) {
     const draft = e.detail.comment;
     if (!draft.__draft) { return; }
-
     if (!this._diffDrafts[draft.path]) {
       return;
     }
@@ -1581,6 +1592,20 @@ class GrChangeView extends KeyboardShortcutMixin(
   }
 
   _handleReloadChange() {
+    if (Object.values(this._currentlyEditingDrafts).includes(true)) {
+      // user has a currently editing draft, so show a warning before reloading
+      this.dispatchEvent(new CustomEvent('show-alert', {
+        detail: {
+          message: 'You have unsaved drafts. Are you sure you want to reload?',
+          action: 'Reload',
+          callback: () => {
+            this.this._reload();
+          },
+        },
+        composed: true, bubbles: true,
+      }));
+      return;
+    }
     return this._reload();
   }
 
@@ -2145,9 +2170,9 @@ class GrChangeView extends KeyboardShortcutMixin(
             // Persist this alert.
             dismissOnNavigation: true,
             action: 'Reload',
-            callback: function() {
+            callback: () => {
               this._handleReloadChange();
-            }.bind(this),
+            },
           },
           composed: true, bubbles: true,
         }));

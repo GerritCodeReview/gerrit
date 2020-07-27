b0e16ba65536a6530ae73449b787ee7e702d9748
Thomas Draebing <thomas.draebing@sap.com>
2020-07-27 12:44:10 +0200

    Respect log.textLogging and log.jsonLogging using --console-log
    The gerrit.war daemon provided a --console-log flag to send the
    error logs to stderr. However, when using this flag, no logs were
    written to the error_log or error_log.json files, even if the
    corresponding option was set in the gerrit.config file.
    
    On the one hand, the configuration should be respected and on the
    other hand, there are usecases, where both output channels are
    useful. Mainly, if Gerrit is run in a docker container, it is
    useful to be able to use 'docker logs' to view the current logs
    and to have the logs persisted at the same time. While one can
    tail the log-file to do so, Gerrit is then not the main process
    of the container and will thus not be gracefully shutdown, when
    stopping the container. Being able to use the --console-log
    option together with {text|json}Logging being active would make
    this easier.
    
    Bug: Issue 13184
    Change-Id: I5c3dfa531a6ccd397d3ceb60defc6e83943e95f1

--- java/com/google/gerrit/pgm/util/ErrorLogFile.java
+++ java/com/google/gerrit/pgm/util/ErrorLogFile.java
@@ -49,11 +49,12 @@ public class ErrorLogFile {
     root.addAppender(dst);
   }
 
-  public static LifecycleListener start(Path sitePath, Config config) throws IOException {
+  public static LifecycleListener start(Path sitePath, Config config, boolean consoleLog)
+      throws IOException {
     Path logdir =
         FileUtil.mkdirsOrDie(new SitePaths(sitePath).logs_dir, "Cannot create log directory");
     if (SystemLog.shouldConfigure()) {
-      initLogSystem(logdir, config);
+      initLogSystem(logdir, config, consoleLog);
     }
 
     return new LifecycleListener() {
@@ -67,18 +68,28 @@ public class ErrorLogFile {
     };
   }
 
-  private static void initLogSystem(Path logdir, Config config) {
+  private static void initLogSystem(Path logdir, Config config, boolean consoleLog) {
     Logger root = LogManager.getRootLogger();
     root.removeAllAppenders();
 
+    PatternLayout errorLogLayout = new PatternLayout("[%d] [%t] %-5p %c %x: %m%n");
+
+    if (consoleLog) {
+      ConsoleAppender dst = new ConsoleAppender();
+      dst.setLayout(errorLogLayout);
+      dst.setTarget("System.err");
+      dst.setThreshold(Level.INFO);
+      dst.activateOptions();
+
+      root.addAppender(dst);
+    }
+
     boolean json = config.getBoolean("log", "jsonLogging", false);
-    boolean text = config.getBoolean("log", "textLogging", true) || !json;
+    boolean text = config.getBoolean("log", "textLogging", true) || !(json || consoleLog);
     boolean rotate = config.getBoolean("log", "rotate", true);
 
     if (text) {
-      root.addAppender(
-          SystemLog.createAppender(
-              logdir, LOG_NAME, new PatternLayout("[%d] [%t] %-5p %c %x: %m%n"), rotate));
+      root.addAppender(SystemLog.createAppender(logdir, LOG_NAME, errorLogLayout, rotate));
     }
 
     if (json) {

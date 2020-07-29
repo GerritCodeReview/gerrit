772d1d00c79a0f4cdf973e905024eaa3e8317b77
Dmitrii Filippov <dmfilippov@google.com>
2020-07-29 12:04:39 +0200

    Convert gr-auth to typescript
    Change-Id: I5aa9556010ee286dfb03300c8dcc3da5af115c06

--- polygerrit-ui/app/services/gr-auth/gr-auth_impl.ts
+++ polygerrit-ui/app/services/gr-auth/gr-auth_impl.ts
@@ -14,23 +14,73 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {getBaseUrl} from '../utils/url-util.js';
+import {getBaseUrl} from '../../utils/url-util';
+import {EventEmitterService} from '../gr-event-interface/gr-event-interface';
+import {
+  AuthRequestInit,
+  AuthService,
+  AuthStatus,
+  AuthType,
+  DefaultAuthOptions,
+  GetTokenCallback,
+  Token,
+} from './gr-auth';
 
 const MAX_AUTH_CHECK_WAIT_TIME_MS = 1000 * 30; // 30s
 const MAX_GET_TOKEN_RETRIES = 2;
 
+interface ValidToken extends Token {
+  access_token: string;
+  expires_at: string;
+}
+
+interface AuthRequestInitWithHeaders extends AuthRequestInit {
+  // RequestInit define headers as optional property with a type
+  // Headers | string[][] | Record<string, string>
+  // In Auth class headers property is always set and has type Headers
+  headers: Headers;
+}
+
 /**
  * Auth class.
  */
-export class Auth {
-  constructor(eventEmitter) {
-    this._type = null;
-    this._cachedTokenPromise = null;
-    this._defaultOptions = {};
-    this._retriesLeft = MAX_GET_TOKEN_RETRIES;
-    this._status = Auth.STATUS.UNDETERMINED;
-    this._authCheckPromise = null;
-    this._last_auth_check_time = Date.now();
+export class Auth implements AuthService {
+  // TODO(dmfilippov): Remove Type and Status properties, expose AuthType and
+  // AuthStatus to API
+  static TYPE = {
+    XSRF_TOKEN: AuthType.XSRF_TOKEN,
+    ACCESS_TOKEN: AuthType.ACCESS_TOKEN,
+  };
+
+  static STATUS = {
+    UNDETERMINED: AuthStatus.UNDETERMINED,
+    AUTHED: AuthStatus.AUTHED,
+    NOT_AUTHED: AuthStatus.NOT_AUTHED,
+    ERROR: AuthStatus.ERROR,
+  };
+
+  static CREDS_EXPIRED_MSG = 'Credentials expired.';
+
+  private _authCheckPromise?: Promise<Response>;
+
+  private _last_auth_check_time: number = Date.now();
+
+  private _status = AuthStatus.UNDETERMINED;
+
+  private _retriesLeft = MAX_GET_TOKEN_RETRIES;
+
+  private _cachedTokenPromise: Promise<Token | null> | null = null;
+
+  private _type?: AuthType;
+
+  private _defaultOptions: AuthRequestInit = {};
+
+  private _getToken: GetTokenCallback;
+
+  public eventEmitter: EventEmitterService;
+
+  constructor(eventEmitter: EventEmitterService) {
+    this._getToken = () => Promise.resolve(this._cachedTokenPromise);
     this.eventEmitter = eventEmitter;
   }
 
@@ -40,49 +90,48 @@ export class Auth {
 
   /**
    * Returns if user is authed or not.
-   *
-   * @returns {!Promise<boolean>}
    */
-  authCheck() {
-    if (!this._authCheckPromise ||
-      (Date.now() - this._last_auth_check_time > MAX_AUTH_CHECK_WAIT_TIME_MS)
+  authCheck(): Promise<boolean> {
+    if (
+      !this._authCheckPromise ||
+      Date.now() - this._last_auth_check_time > MAX_AUTH_CHECK_WAIT_TIME_MS
     ) {
       // Refetch after last check expired
       this._authCheckPromise = fetch(`${this.baseUrl}/auth-check`);
       this._last_auth_check_time = Date.now();
     }
 
-    return this._authCheckPromise.then(res => {
-      // auth-check will return 204 if authed
-      // treat the rest as unauthed
-      if (res.status === 204) {
-        this._setStatus(Auth.STATUS.AUTHED);
-        return true;
-      } else {
-        this._setStatus(Auth.STATUS.NOT_AUTHED);
+    return this._authCheckPromise
+      .then(res => {
+        // auth-check will return 204 if authed
+        // treat the rest as unauthed
+        if (res.status === 204) {
+          this._setStatus(Auth.STATUS.AUTHED);
+          return true;
+        } else {
+          this._setStatus(Auth.STATUS.NOT_AUTHED);
+          return false;
+        }
+      })
+      .catch(() => {
+        this._setStatus(AuthStatus.ERROR);
+        // Reset _authCheckPromise to avoid caching the failed promise
+        this._authCheckPromise = undefined;
         return false;
-      }
-    }).catch(e => {
-      this._setStatus(Auth.STATUS.ERROR);
-      // Reset _authCheckPromise to avoid caching the failed promise
-      this._authCheckPromise = null;
-      return false;
-    });
+      });
   }
 
   clearCache() {
-    this._authCheckPromise = null;
+    this._authCheckPromise = undefined;
   }
 
-  /**
-   * @param {Auth.STATUS} status
-   */
-  _setStatus(status) {
+  private _setStatus(status: AuthStatus) {
     if (this._status === status) return;
 
-    if (this._status === Auth.STATUS.AUTHED) {
+    if (this._status === AuthStatus.AUTHED) {
       this.eventEmitter.emit('auth-error', {
-        message: Auth.CREDS_EXPIRED_MSG, action: 'Refresh credentials',
+        message: Auth.CREDS_EXPIRED_MSG,
+        action: 'Refresh credentials',
       });
     }
     this._status = status;
@@ -96,60 +145,41 @@ export class Auth {
     return this._status === Auth.STATUS.AUTHED;
   }
 
-  _getToken() {
-    return Promise.resolve(this._cachedTokenPromise);
-  }
-
   /**
    * Enable cross-domain authentication using OAuth access token.
-   *
-   * @param {
-   *   function(): !Promise<{
-   *     access_token: string,
-   *     expires_at: number
-   *   }>
-   * } getToken
-   * @param {?{credentials:string}} defaultOptions
    */
-  setup(getToken, defaultOptions) {
+  setup(getToken: GetTokenCallback, defaultOptions: DefaultAuthOptions) {
     this._retriesLeft = MAX_GET_TOKEN_RETRIES;
     if (getToken) {
-      this._type = Auth.TYPE.ACCESS_TOKEN;
+      this._type = AuthType.ACCESS_TOKEN;
       this._cachedTokenPromise = null;
       this._getToken = getToken;
     }
     this._defaultOptions = {};
     if (defaultOptions) {
-      for (const p of ['credentials']) {
-        this._defaultOptions[p] = defaultOptions[p];
-      }
+      this._defaultOptions.credentials = defaultOptions.credentials;
     }
   }
 
   /**
    * Perform network fetch with authentication.
-   *
-   * @param {string} url
-   * @param {Object=} opt_options
-   * @return {!Promise<!Response>}
    */
-  fetch(url, opt_options) {
-    const options = {
+  fetch(url: string, opt_options?: AuthRequestInit): Promise<Response> {
+    const options: AuthRequestInitWithHeaders = {
       headers: new Headers(),
       ...this._defaultOptions,
       ...opt_options,
     };
-    if (this._type === Auth.TYPE.ACCESS_TOKEN) {
-      return this._getAccessToken().then(
-          accessToken =>
-            this._fetchWithAccessToken(url, options, accessToken)
+    if (this._type === AuthType.ACCESS_TOKEN) {
+      return this._getAccessToken().then(accessToken =>
+        this._fetchWithAccessToken(url, options, accessToken)
       );
     } else {
       return this._fetchWithXsrfToken(url, options);
     }
   }
 
-  _getCookie(name) {
+  private _getCookie(name: string): string {
     const key = name + '=';
     let result = '';
     document.cookie.split(';').some(c => {
@@ -163,17 +193,26 @@ export class Auth {
     return result;
   }
 
-  _isTokenValid(token) {
-    if (!token) { return false; }
-    if (!token.access_token || !token.expires_at) { return false; }
+  private _isTokenValid(token: Token | null): token is ValidToken {
+    if (!token) {
+      return false;
+    }
+    if (!token.access_token || !token.expires_at) {
+      return false;
+    }
 
     const expiration = new Date(parseInt(token.expires_at, 10) * 1000);
-    if (Date.now() >= expiration.getTime()) { return false; }
+    if (Date.now() >= expiration.getTime()) {
+      return false;
+    }
 
     return true;
   }
 
-  _fetchWithXsrfToken(url, options) {
+  private _fetchWithXsrfToken(
+    url: string,
+    options: AuthRequestInitWithHeaders
+  ): Promise<Response> {
     if (options.method && options.method !== 'GET') {
       const token = this._getCookie('XSRF_TOKEN');
       if (token) {
@@ -184,10 +223,7 @@ export class Auth {
     return fetch(url, options);
   }
 
-  /**
-   * @return {!Promise<string>}
-   */
-  _getAccessToken() {
+  private _getAccessToken(): Promise<string | null> {
     if (!this._cachedTokenPromise) {
       this._cachedTokenPromise = this._getToken();
     }
@@ -206,14 +242,19 @@ export class Auth {
     });
   }
 
-  _fetchWithAccessToken(url, options, accessToken) {
+  private _fetchWithAccessToken(
+    url: string,
+    options: AuthRequestInitWithHeaders,
+    accessToken: string | null
+  ): Promise<Response> {
     const params = [];
 
     if (accessToken) {
       params.push(`access_token=${accessToken}`);
       const baseUrl = this.baseUrl;
-      const pathname = baseUrl ?
-        url.substring(url.indexOf(baseUrl) + baseUrl.length) : url;
+      const pathname = baseUrl
+        ? url.substring(url.indexOf(baseUrl) + baseUrl.length)
+        : url;
       if (!pathname.startsWith('/a/')) {
         url = url.replace(pathname, '/a' + pathname);
       }
@@ -248,18 +289,3 @@ export class Auth {
     return fetch(url, options);
   }
 }
-
-Auth.TYPE = {
-  XSRF_TOKEN: 'xsrf_token',
-  ACCESS_TOKEN: 'access_token',
-};
-
-/** @enum {number} */
-Auth.STATUS = {
-  UNDETERMINED: 0,
-  AUTHED: 1,
-  NOT_AUTHED: 2,
-  ERROR: 3,
-};
-
-Auth.CREDS_EXPIRED_MSG = 'Credentials expired.';

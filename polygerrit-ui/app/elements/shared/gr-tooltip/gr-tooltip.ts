8b2412c32f509de8d6587f0b8944bf5c7b925bca
Dhruv Srivastava <dhruvsri@google.com>
2020-07-30 14:03:34 +0200

    Convert gr-tooltip.js to typescript
    Change-Id: Ibf422a85b75ef8ed135740d089d332bb16af5f85

--- polygerrit-ui/app/elements/shared/gr-tooltip/gr-tooltip.ts
+++ polygerrit-ui/app/elements/shared/gr-tooltip/gr-tooltip.ts
@@ -14,37 +14,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '../../../styles/shared-styles.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-tooltip_html.js';
+import '../../../styles/shared-styles';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-tooltip_html';
+import {customElement, property} from '@polymer/decorators';
 
-/** @extends PolymerElement */
-class GrTooltip extends GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement)) {
-  static get template() { return htmlTemplate; }
+export interface GrTooltip {
+  $: {};
+}
 
-  static get is() { return 'gr-tooltip'; }
-
-  static get properties() {
-    return {
-      text: String,
-      maxWidth: {
-        type: String,
-        observer: '_updateWidth',
-      },
-      positionBelow: {
-        type: Boolean,
-        reflectToAttribute: true,
-      },
-    };
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-tooltip': GrTooltip;
   }
+}
+
+@customElement('gr-tooltip')
+export class GrTooltip extends GestureEventListeners(
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
+  }
+
+  @property({type: String})
+  text = '';
+
+  @property({type: String, observer: '_updateWidth'})
+  maxWidth = '';
+
+  @property({type: Boolean, reflectToAttribute: true})
+  positionBelow = false;
 
   _updateWidth(maxWidth) {
     this.updateStyles({'--tooltip-max-width': maxWidth});
   }
 }
-
-customElements.define(GrTooltip.is, GrTooltip);

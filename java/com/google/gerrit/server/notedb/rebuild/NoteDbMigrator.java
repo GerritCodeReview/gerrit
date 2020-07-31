90065470ed918e92fce03545e03f779b21e5554c
Matthias Sohn <matthias.sohn@sap.com>
2020-07-31 14:01:14 +0200

    NoteDbMigrator: Fix contention on single database connection
    The migrator used a single database connection for all threads
    rebuilding changes. Instead use one database connection per rebuild
    thread.
    
    Change-Id: If785208cc571421b0a2bac65b4970c24a4c33e1f

--- java/com/google/gerrit/server/notedb/rebuild/NoteDbMigrator.java
+++ java/com/google/gerrit/server/notedb/rebuild/NoteDbMigrator.java
@@ -837,7 +837,6 @@ public class NoteDbMigrator implements AutoCloseable {
 
     ImmutableListMultimap<Project.NameKey, Change.Id> changesByProject = getChangesByProject();
     List<ListenableFuture<Boolean>> futures = new ArrayList<>();
-    try (ContextHelper contextHelper = new ContextHelper()) {
       List<Project.NameKey> projectNames =
           Ordering.usingToString().sortedCopy(changesByProject.keySet());
       for (Project.NameKey project : projectNames) {
@@ -849,23 +848,17 @@ public class NoteDbMigrator implements AutoCloseable {
         ReentrantLock gcLock = new ReentrantLock();
         for (List<Change.Id> slice : slices) {
           int sn = sliceNumber++;
-          ListenableFuture<Boolean> future =
-              executor.submit(
-                  () -> {
-                    try {
-                      return rebuildProjectSlice(
-                          contextHelper.getReviewDb(),
-                          project,
-                          slice,
-                          sn,
-                          count,
-                          gcCounter,
-                          gcLock);
-                    } catch (Exception e) {
-                      logger.atSevere().withCause(e).log("Error rebuilding project %s", project);
-                      return false;
-                    }
-                  });
+        ListenableFuture<Boolean> future =
+            executor.submit(
+                () -> {
+                  try (ContextHelper contextHelper = new ContextHelper()) {
+                    return rebuildProjectSlice(
+                        contextHelper.getReviewDb(), project, slice, sn, count, gcCounter, gcLock);
+                  } catch (Exception e) {
+                    logger.atSevere().withCause(e).log("Error rebuilding project %s", project);
+                    return false;
+                  }
+                });
         futures.add(future);
       }
       }
@@ -879,7 +872,6 @@ public class NoteDbMigrator implements AutoCloseable {
         throw new MigrationException("Rebuilding some changes failed, see log");
       }
     }
-  }
 
   private ImmutableListMultimap<Project.NameKey, Change.Id> getChangesByProject()
       throws OrmException {

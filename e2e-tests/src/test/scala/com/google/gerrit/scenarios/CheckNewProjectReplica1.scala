df4fef138d3d4e6bf1cf46f5fd464db8ad1ab482
Marco Miller <marco.miller@ericsson.com>
2020-08-03 11:20:55 -0400

    e2e-tests: Add a CheckNewProjectReplica1 scenario
    Add an initial CheckNewProjectReplica1 scenario, as an alternative to
    CheckMasterBranchReplica1. The former verifies using a git clone, while
    the latter verified using the master branch endpoint. That endpoint may
    not be available in some replica deployment environments, thus this new
    scenario to rely on git cloning of the expected replica instead.
    
    The hereby introduced scenario is also (mostly) based on the existing
    CloneUsingBothProtocols core scenario.
    
    Introduce a replication_delay [1] property [2]. Make its default value
    match the similar replication plugin's replicationDelay one (15 seconds,
    [3]). Add this new property to that same e2e-tests documentation page.
    
    [1] -Dcom.google.gerrit.scenarios.replication_delay=15
    [2] https://gerrit-documentation.storage.googleapis.com/Documentation/3.0.12/dev-e2e-tests.html#_environment_properties
    [3] https://gerrit.googlesource.com/plugins/replication/+doc/master/src/main/resources/Documentation/config.md
    
    Change-Id: I8966f0724cdd90b8433d6ffbb0afdbdff78c94ce

---
+++ e2e-tests/src/test/scala/com/google/gerrit/scenarios/CheckNewProjectReplica1.scala
@@ -0,0 +1,57 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.scenarios
+
+import io.gatling.core.Predef._
+import io.gatling.core.feeder.FeederBuilder
+import io.gatling.core.structure.ScenarioBuilder
+
+import scala.concurrent.duration._
+
+class CheckNewProjectReplica1 extends GitSimulation {
+  private val data: FeederBuilder = jsonFile(resource).convert(keys).queue
+  private val default: String = name
+  private val replicationDuration = replicationDelay + SecondsPerWeightUnit
+
+  override def relativeRuntimeWeight: Int = replicationDuration / SecondsPerWeightUnit
+
+  override def replaceOverride(in: String): String = {
+    var next = replaceProperty("http_port1", 8081, in)
+    next = replaceKeyWith("_project", default, next)
+    super.replaceOverride(next)
+  }
+
+  private val test: ScenarioBuilder = scenario(unique)
+      .feed(data)
+      .exec(gitRequest)
+
+  private val createProject = new CreateProject(default)
+  private val deleteProject = new DeleteProject(default)
+
+  setUp(
+    createProject.test.inject(
+      nothingFor(stepWaitTime(createProject) seconds),
+      atOnceUsers(single)
+    ),
+    test.inject(
+      nothingFor(stepWaitTime(this) + replicationDuration seconds),
+      atOnceUsers(single)
+    ).protocols(gitProtocol),
+    deleteProject.test.inject(
+      nothingFor(stepWaitTime(deleteProject) seconds),
+      atOnceUsers(single)
+    ),
+  ).protocols(httpProtocol)
+}

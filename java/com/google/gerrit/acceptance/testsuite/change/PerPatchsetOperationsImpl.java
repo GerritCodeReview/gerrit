2238e45bf340853b9dcd1a6bf0bc5ec4c07e1c1b
Alice Kober-Sotzek <aliceks@google.com>
2020-08-03 18:12:08 +0200

    Add comment creation to test API of changes
    Adding a new comment is as simple as:
    - changeOperations.change(changeId).patchset(patchsetId)
        .newComment().create()
    - changeOperations.change(changeId).currentPatchset()
        .newComment().message("Some text").create()
    - changeOperations.change(changeId).currentPatchset().newComment()
         .fromLine(2).charOffset(4).toLine(3).charOffset(5).ofFile("foo")
         .create()
    
    Change-Id: I94e9dd3090aabf12678d0c3dd094e8593b4443c6

--- java/com/google/gerrit/acceptance/testsuite/change/PerPatchsetOperationsImpl.java
+++ java/com/google/gerrit/acceptance/testsuite/change/PerPatchsetOperationsImpl.java
@@ -16,10 +16,36 @@
 
 package com.google.gerrit.acceptance.testsuite.change;
 
+import static com.google.gerrit.server.CommentsUtil.setCommentCommitId;
+
+import com.google.gerrit.acceptance.testsuite.change.TestCommentCreation.CommentSide;
+import com.google.gerrit.entities.HumanComment;
+import com.google.gerrit.entities.Patch;
 import com.google.gerrit.entities.PatchSet;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.client.Comment;
+import com.google.gerrit.extensions.client.Comment.Range;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
+import com.google.gerrit.server.CommentsUtil;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.IdentifiedUser.GenericFactory;
+import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gerrit.server.patch.PatchListCache;
+import com.google.gerrit.server.patch.PatchListNotAvailableException;
+import com.google.gerrit.server.update.BatchUpdate;
+import com.google.gerrit.server.update.BatchUpdateOp;
+import com.google.gerrit.server.update.ChangeContext;
+import com.google.gerrit.server.update.UpdateException;
+import com.google.gerrit.server.util.time.TimeUtil;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
+import java.io.IOException;
+import java.sql.Timestamp;
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevWalk;
 
 /**
  * The implementation of {@link PerPatchsetOperations}.
@@ -28,6 +54,12 @@ import com.google.inject.assistedinject.Assisted;
  * separation between interface and implementation to enhance clarity.
  */
 public class PerPatchsetOperationsImpl implements PerPatchsetOperations {
+  private final GitRepositoryManager repositoryManager;
+  private final IdentifiedUser.GenericFactory userFactory;
+  private final BatchUpdate.Factory batchUpdateFactory;
+  private final CommentsUtil commentsUtil;
+  private final PatchListCache patchListCache;
+
   private final ChangeNotes changeNotes;
   private final PatchSet.Id patchsetId;
 
@@ -37,7 +69,18 @@ public class PerPatchsetOperationsImpl implements PerPatchsetOperations {
 
   @Inject
   private PerPatchsetOperationsImpl(
-      @Assisted ChangeNotes changeNotes, @Assisted PatchSet.Id patchsetId) {
+      GitRepositoryManager repositoryManager,
+      GenericFactory userFactory,
+      BatchUpdate.Factory batchUpdateFactory,
+      CommentsUtil commentsUtil,
+      PatchListCache patchListCache,
+      @Assisted ChangeNotes changeNotes,
+      @Assisted PatchSet.Id patchsetId) {
+    this.repositoryManager = repositoryManager;
+    this.userFactory = userFactory;
+    this.batchUpdateFactory = batchUpdateFactory;
+    this.commentsUtil = commentsUtil;
+    this.patchListCache = patchListCache;
     this.changeNotes = changeNotes;
     this.patchsetId = patchsetId;
   }
@@ -46,5 +89,88 @@ public class PerPatchsetOperationsImpl implements PerPatchsetOperations {
   public TestPatchset get() {
     PatchSet patchset = changeNotes.getPatchSets().get(patchsetId);
     return TestPatchset.builder().patchsetId(patchsetId).commitId(patchset.commitId()).build();
+  }
+
+  @Override
+  public TestCommentCreation.Builder newComment() {
+    return TestCommentCreation.builder(this::createComment);
+  }
+
+  private String createComment(TestCommentCreation commentCreation)
+      throws IOException, RestApiException, UpdateException {
+    Project.NameKey project = changeNotes.getProjectName();
+
+    try (Repository repository = repositoryManager.openRepository(project);
+        ObjectInserter objectInserter = repository.newObjectInserter();
+        RevWalk revWalk = new RevWalk(objectInserter.newReader())) {
+      Timestamp now = TimeUtil.nowTs();
+
+      // Use identity of change owner until the API allows to specify the commenter.
+      IdentifiedUser changeOwner = userFactory.create(changeNotes.getChange().getOwner());
+      CommentAdditionOp commentAdditionOp = new CommentAdditionOp(commentCreation);
+      try (BatchUpdate batchUpdate = batchUpdateFactory.create(project, changeOwner, now)) {
+        batchUpdate.setRepository(repository, revWalk, objectInserter);
+        batchUpdate.addOp(changeNotes.getChangeId(), commentAdditionOp);
+        batchUpdate.execute();
+      }
+      return commentAdditionOp.createdCommentUuid;
+    }
+  }
+
+  private class CommentAdditionOp implements BatchUpdateOp {
+    private String createdCommentUuid;
+    private final TestCommentCreation commentCreation;
+
+    public CommentAdditionOp(TestCommentCreation commentCreation) {
+      this.commentCreation = commentCreation;
+    }
+
+    @Override
+    public boolean updateChange(ChangeContext context) throws Exception {
+      HumanComment comment = toNewComment(context, commentCreation);
+
+      context
+          .getUpdate(context.getChange().currentPatchSetId())
+          .putComment(HumanComment.Status.PUBLISHED, comment);
+
+      createdCommentUuid = comment.key.uuid;
+      return true;
+    }
+
+    private HumanComment toNewComment(ChangeContext context, TestCommentCreation commentCreation)
+        throws UnprocessableEntityException, PatchListNotAvailableException {
+      String message = commentCreation.message().orElse("The text of a test comment.");
+
+      String filePath = commentCreation.file().orElse(Patch.PATCHSET_LEVEL);
+      short side = commentCreation.side().orElse(CommentSide.PATCHSET_COMMIT).getNumericSide();
+      Boolean unresolved = commentCreation.unresolved().orElse(null);
+      String parentUuid = commentCreation.parentUuid().orElse(null);
+      HumanComment newComment =
+          commentsUtil.newHumanComment(
+              context, filePath, patchsetId, side, message, unresolved, parentUuid);
+
+      commentCreation.line().ifPresent(line -> newComment.setLineNbrAndRange(line, null));
+      // Specification of range trumps explicit line specification.
+      commentCreation
+          .range()
+          .map(this::toCommentRange)
+          .ifPresent(range -> newComment.setLineNbrAndRange(null, range));
+
+      setCommentCommitId(
+          newComment,
+          patchListCache,
+          context.getChange(),
+          changeNotes.getPatchSets().get(patchsetId));
+      return newComment;
+    }
+
+    private Comment.Range toCommentRange(TestRange range) {
+      Comment.Range commentRange = new Range();
+      commentRange.startLine = range.start().line();
+      commentRange.startCharacter = range.start().charOffset();
+      commentRange.endLine = range.end().line();
+      commentRange.endCharacter = range.end().charOffset();
+      return commentRange;
+    }
   }
 }

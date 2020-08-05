4d4e8f5463f349a5a29f609ddd79b566221e9be1
Dhruv Srivastava <dhruvsri@google.com>
2020-08-05 11:48:20 +0200

    Convert files to typescript
    * gr-change-metadata-api
    * gr-dom-hooks
    * gr-autocomplete-dropdown
    * gr-fixed-panel
    
    Change-Id: I23fd1f52b583514d1935bdadce5969f3f1613405

--- polygerrit-ui/app/elements/shared/gr-autocomplete-dropdown/gr-autocomplete-dropdown.ts
+++ polygerrit-ui/app/elements/shared/gr-autocomplete-dropdown/gr-autocomplete-dropdown.ts
@@ -14,25 +14,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '@polymer/iron-dropdown/iron-dropdown.js';
-import '../gr-cursor-manager/gr-cursor-manager.js';
-import '../../../styles/shared-styles.js';
-import {flush} from '@polymer/polymer/lib/legacy/polymer.dom.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-autocomplete-dropdown_html.js';
-import {KeyboardShortcutMixin} from '../../../mixins/keyboard-shortcut-mixin/keyboard-shortcut-mixin.js';
-import {IronFitMixin} from '../../../mixins/iron-fit-mixin/iron-fit-mixin.js';
+import '@polymer/iron-dropdown/iron-dropdown';
+import '../gr-cursor-manager/gr-cursor-manager';
+import '../../../styles/shared-styles';
+import {flush} from '@polymer/polymer/lib/legacy/polymer.dom';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-autocomplete-dropdown_html';
+import {KeyboardShortcutMixin} from '../../../mixins/keyboard-shortcut-mixin/keyboard-shortcut-mixin';
+import {IronFitMixin} from '../../../mixins/iron-fit-mixin/iron-fit-mixin';
+import {customElement, property, observe} from '@polymer/decorators';
+
+// TODO(TS): Update once GrCursorManager is upated
+export interface GrAutocompleteDropdown {
+  $: {
+    cursor: any;
+    suggestions: Element;
+  };
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-autocomplete-dropdown': GrAutocompleteDropdown;
+  }
+}
+
+interface Item {
+  dataValue?: string;
+  name?: string;
+  text?: string;
+  label?: string;
+  value?: string;
+}
 
 /**
  * @extends PolymerElement
  */
-class GrAutocompleteDropdown extends IronFitMixin(KeyboardShortcutMixin(
-    GestureEventListeners(LegacyElementMixin(PolymerElement)))) {
-  static get template() { return htmlTemplate; }
+@customElement('gr-autocomplete-dropdown')
+export class GrAutocompleteDropdown extends IronFitMixin(
+  KeyboardShortcutMixin(
+    GestureEventListeners(LegacyElementMixin(PolymerElement))
+  )
+) {
+  static get template() {
+    return htmlTemplate;
+  }
 
-  static get is() { return 'gr-autocomplete-dropdown'; }
   /**
    * Fired when the dropdown is closed.
    *
@@ -45,30 +73,23 @@ class GrAutocompleteDropdown extends IronFitMixin(KeyboardShortcutMixin(
    * @event item-selected
    */
 
-  static get properties() {
-    return {
-      index: Number,
-      isHidden: {
-        type: Boolean,
-        value: true,
-        reflectToAttribute: true,
-      },
-      verticalOffset: {
-        type: Number,
-        value: null,
-      },
-      horizontalOffset: {
-        type: Number,
-        value: null,
-      },
-      suggestions: {
-        type: Array,
-        value: () => [],
-        observer: '_resetCursorStops',
-      },
-      _suggestionEls: Array,
-    };
-  }
+  @property({type: Number})
+  index: number | null = null;
+
+  @property({type: Boolean, reflectToAttribute: true})
+  isHidden = true;
+
+  @property({type: Number})
+  verticalOffset: number | null = null;
+
+  @property({type: Number})
+  horizontalOffset: number | null = null;
+
+  @property({type: Array})
+  suggestions: Array<Item> = [];
+
+  @property({type: Array})
+  _suggestionEls: Array<Element> = [];
 
   get keyBindings() {
     return {
@@ -95,7 +116,7 @@ class GrAutocompleteDropdown extends IronFitMixin(KeyboardShortcutMixin(
     return this.getCursorTarget().dataset.value;
   }
 
-  _handleUp(e) {
+  _handleUp(e: Event) {
     if (!this.isHidden) {
       e.preventDefault();
       e.stopPropagation();
@@ -103,7 +124,7 @@ class GrAutocompleteDropdown extends IronFitMixin(KeyboardShortcutMixin(
     }
   }
 
-  _handleDown(e) {
+  _handleDown(e: Event) {
     if (!this.isHidden) {
       e.preventDefault();
       e.stopPropagation();
@@ -123,28 +144,34 @@ class GrAutocompleteDropdown extends IronFitMixin(KeyboardShortcutMixin(
     }
   }
 
-  _handleTab(e) {
+  _handleTab(e: Event) {
     e.preventDefault();
     e.stopPropagation();
-    this.dispatchEvent(new CustomEvent('item-selected', {
-      detail: {
-        trigger: 'tab',
-        selected: this.$.cursor.target,
-      },
-      composed: true, bubbles: true,
-    }));
+    this.dispatchEvent(
+      new CustomEvent('item-selected', {
+        detail: {
+          trigger: 'tab',
+          selected: this.$.cursor.target,
+        },
+        composed: true,
+        bubbles: true,
+      })
+    );
   }
 
-  _handleEnter(e) {
+  _handleEnter(e: Event) {
     e.preventDefault();
     e.stopPropagation();
-    this.dispatchEvent(new CustomEvent('item-selected', {
-      detail: {
-        trigger: 'enter',
-        selected: this.$.cursor.target,
-      },
-      composed: true, bubbles: true,
-    }));
+    this.dispatchEvent(
+      new CustomEvent('item-selected', {
+        detail: {
+          trigger: 'enter',
+          selected: this.$.cursor.target,
+        },
+        composed: true,
+        bubbles: true,
+      })
+    );
   }
 
   _handleEscape() {
@@ -152,39 +179,49 @@ class GrAutocompleteDropdown extends IronFitMixin(KeyboardShortcutMixin(
     this.close();
   }
 
-  _handleClickItem(e) {
+  _handleClickItem(e: Event) {
     e.preventDefault();
     e.stopPropagation();
-    let selected = e.target;
+    let selected = e.target! as Element;
     while (!selected.classList.contains('autocompleteOption')) {
-      if (!selected || selected === this) { return; }
-      selected = selected.parentElement;
+      if (!selected || selected === this) {
+        return;
+      }
+      selected = selected.parentElement!;
     }
-    this.dispatchEvent(new CustomEvent('item-selected', {
-      detail: {
-        trigger: 'click',
-        selected,
-      },
-      composed: true, bubbles: true,
-    }));
+    this.dispatchEvent(
+      new CustomEvent('item-selected', {
+        detail: {
+          trigger: 'click',
+          selected,
+        },
+        composed: true,
+        bubbles: true,
+      })
+    );
   }
 
   _fireClose() {
-    this.dispatchEvent(new CustomEvent('dropdown-closed', {
-      composed: true, bubbles: true,
-    }));
+    this.dispatchEvent(
+      new CustomEvent('dropdown-closed', {
+        composed: true,
+        bubbles: true,
+      })
+    );
   }
 
   getCursorTarget() {
     return this.$.cursor.target;
   }
 
+  @observe('suggestions')
   _resetCursorStops() {
     if (this.suggestions.length > 0) {
       if (!this.isHidden) {
         flush();
         this._suggestionEls = Array.from(
-            this.$.suggestions.querySelectorAll('li'));
+          this.$.suggestions.querySelectorAll('li')
+        );
         this._resetCursorIndex();
       }
     } else {
@@ -196,9 +233,7 @@ class GrAutocompleteDropdown extends IronFitMixin(KeyboardShortcutMixin(
     this.$.cursor.setCursorAtIndex(0);
   }
 
-  _computeLabelClass(item) {
+  _computeLabelClass(item: Item) {
     return item.label ? '' : 'hide';
   }
 }
-
-customElements.define(GrAutocompleteDropdown.is, GrAutocompleteDropdown);

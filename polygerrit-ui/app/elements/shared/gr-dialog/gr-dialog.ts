80afd3c6734ef3cba474ed4d8477fff3f2859c59
Milutin Kristofic <milutin@google.com>
2020-08-05 13:44:21 +0200

    Move gr-dialog to typescript
    Change-Id: Iba1f1a5506b03d9e1a77a70d30a92d3881173cff

--- polygerrit-ui/app/elements/shared/gr-dialog/gr-dialog.ts
+++ polygerrit-ui/app/elements/shared/gr-dialog/gr-dialog.ts
@@ -14,21 +14,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '../gr-button/gr-button.js';
-import '../../../styles/shared-styles.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-dialog_html.js';
+import '../gr-button/gr-button';
+import '../../../styles/shared-styles';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-dialog_html';
+import {customElement, property, observe} from '@polymer/decorators';
+import {GrButton} from '../gr-button/gr-button';
 
-/**
- * @extends PolymerElement
- */
-class GrDialog extends GestureEventListeners(
-    LegacyElementMixin(PolymerElement)) {
-  static get template() { return htmlTemplate; }
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-dialog': GrDialog;
+  }
+}
 
-  static get is() { return 'gr-dialog'; }
+export interface GrDialog {
+  $: {
+    confirm: GrButton;
+  };
+}
+
+@customElement('gr-dialog')
+export class GrDialog extends GestureEventListeners(
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
+  }
+
   /**
    * Fired when the confirm button is pressed.
    *
@@ -41,31 +55,21 @@ class GrDialog extends GestureEventListeners(
    * @event cancel
    */
 
-  static get properties() {
-    return {
-      confirmLabel: {
-        type: String,
-        value: 'Confirm',
-      },
-      // Supplying an empty cancel label will hide the button completely.
-      cancelLabel: {
-        type: String,
-        value: 'Cancel',
-      },
-      disabled: {
-        type: Boolean,
-        value: false,
-      },
-      confirmOnEnter: {
-        type: Boolean,
-        value: false,
-      },
-      confirmTooltip: {
-        type: String,
-        observer: '_handleConfirmTooltipUpdate',
-      },
-    };
-  }
+  @property({type: String})
+  confirmLabel = 'Confirm';
+
+  // Supplying an empty cancel label will hide the button completely.
+  @property({type: String})
+  cancelLabel = 'Cancel';
+
+  @property({type: Boolean})
+  disabled = false;
+
+  @property({type: Boolean})
+  confirmOnEnter = false;
+
+  @property({type: String})
+  confirmTooltip?: string;
 
   /** @override */
   ready() {
@@ -73,43 +77,52 @@ class GrDialog extends GestureEventListeners(
     this._ensureAttribute('role', 'dialog');
   }
 
-  _handleConfirmTooltipUpdate(confirmTooltip) {
+  @observe('confirmTooltip')
+  _handleConfirmTooltipUpdate(confirmTooltip?: string) {
     if (confirmTooltip) {
-      this.$.confirm.setAttribute('has-tooltip', true);
+      this.$.confirm.setAttribute('has-tooltip', 'true');
     } else {
       this.$.confirm.removeAttribute('has-tooltip');
     }
   }
 
-  _handleConfirm(e) {
-    if (this.disabled) { return; }
+  _handleConfirm(e: KeyboardEvent) {
+    if (this.disabled) {
+      return;
+    }
 
     e.preventDefault();
     e.stopPropagation();
-    this.dispatchEvent(new CustomEvent('confirm', {
-      composed: true, bubbles: false,
-    }));
+    this.dispatchEvent(
+      new CustomEvent('confirm', {
+        composed: true,
+        bubbles: false,
+      })
+    );
   }
 
-  _handleCancelTap(e) {
+  _handleCancelTap(e: MouseEvent) {
     e.preventDefault();
     e.stopPropagation();
-    this.dispatchEvent(new CustomEvent('cancel', {
-      composed: true, bubbles: false,
-    }));
+    this.dispatchEvent(
+      new CustomEvent('cancel', {
+        composed: true,
+        bubbles: false,
+      })
+    );
   }
 
-  _handleKeydown(e) {
-    if (this.confirmOnEnter && e.keyCode === 13) { this._handleConfirm(e); }
+  _handleKeydown(e: KeyboardEvent) {
+    if (this.confirmOnEnter && e.keyCode === 13) {
+      this._handleConfirm(e);
+    }
   }
 
   resetFocus() {
     this.$.confirm.focus();
   }
 
-  _computeCancelClass(cancelLabel) {
+  _computeCancelClass(cancelLabel: string) {
     return cancelLabel.length ? '' : 'hidden';
   }
 }
-
-customElements.define(GrDialog.is, GrDialog);

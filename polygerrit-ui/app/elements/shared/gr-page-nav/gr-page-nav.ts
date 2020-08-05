545c332e9afb86d84230dc7d6dd21d26e054b7ca
Tao Zhou <taoalpha@google.com>
2020-08-05 17:29:09 +0200

    Move gr-page-nav to typescript
    Change-Id: Ic7a36c0b8dc127038b62fde94f77adc45aadab3a

--- polygerrit-ui/app/elements/shared/gr-page-nav/gr-page-nav.ts
+++ polygerrit-ui/app/elements/shared/gr-page-nav/gr-page-nav.ts
@@ -14,60 +14,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '../../../styles/shared-styles.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-page-nav_html.js';
+import '../../../styles/shared-styles';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-page-nav_html';
+import {customElement, property} from '@polymer/decorators';
 
-/** @extends PolymerElement */
-class GrPageNav extends GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement)) {
-  static get template() { return htmlTemplate; }
-
-  static get is() { return 'gr-page-nav'; }
-
-  static get properties() {
-    return {
-      _headerHeight: Number,
-    };
+@customElement('gr-page-nav')
+export class GrPageNav extends GestureEventListeners(
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
   }
 
-  /** @override */
+  @property({type: Number})
+  _headerHeight?: number;
+
+  private readonly bodyScrollHandler: () => void;
+
+  constructor() {
+    super();
+    this.bodyScrollHandler = () => this._handleBodyScroll();
+  }
+
   attached() {
     super.attached();
-    this.listen(window, 'scroll', '_handleBodyScroll');
+    window.addEventListener('scroll', this.bodyScrollHandler);
   }
 
-  /** @override */
   detached() {
     super.detached();
-    this.unlisten(window, 'scroll', '_handleBodyScroll');
+    window.removeEventListener('scroll', this.bodyScrollHandler);
   }
 
   _handleBodyScroll() {
     if (this._headerHeight === undefined) {
       let top = this._getOffsetTop(this);
-      for (let offsetParent = this.offsetParent;
+      // TODO(TS): Element doesn't have offsetParent,
+      // while `offsetParent` are returning Element not HTMLElement
+      for (
+        let offsetParent = this.offsetParent as HTMLElement | undefined;
         offsetParent;
-        offsetParent = this._getOffsetParent(offsetParent)) {
+        offsetParent = this._getOffsetParent(offsetParent)
+      ) {
         top += this._getOffsetTop(offsetParent);
       }
       this._headerHeight = top;
     }
 
-    this.$.nav.classList.toggle('pinned',
-        this._getScrollY() >= this._headerHeight);
+    this.$.nav.classList.toggle(
+      'pinned',
+      this._getScrollY() >= (this._headerHeight || 0)
+    );
   }
 
   /* Functions used for test purposes */
-  _getOffsetParent(element) {
-    if (!element || !element.offsetParent) { return ''; }
-    return element.offsetParent;
+  _getOffsetParent(element?: HTMLElement) {
+    if (!element || !('offsetParent' in element)) {
+      return undefined;
+    }
+    return element.offsetParent as HTMLElement;
   }
 
-  _getOffsetTop(element) {
+  _getOffsetTop(element: HTMLElement) {
     return element.offsetTop;
   }
 
@@ -75,5 +86,3 @@ class GrPageNav extends GestureEventListeners(
     return window.scrollY;
   }
 }
-
-customElements.define(GrPageNav.is, GrPageNav);

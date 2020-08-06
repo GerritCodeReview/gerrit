92fcfdf6cbe59d305ed9626e2230c7ca40a13143
Dhruv Srivastava <dhruvsri@google.com>
2020-08-06 12:34:09 +0200

    Convert gr-defaut-editor to typescript
    Change-Id: Ie0ea507b7adaf4c3aa090209c7e58fb75b8e3e2a

--- polygerrit-ui/app/elements/edit/gr-default-editor/gr-default-editor.ts
+++ polygerrit-ui/app/elements/edit/gr-default-editor/gr-default-editor.ts
@@ -14,36 +14,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '../../../styles/shared-styles.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-default-editor_html.js';
+import '../../../styles/shared-styles';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-default-editor_html';
+import {customElement, property} from '@polymer/decorators';
 
+export interface GrDefaultEditor {
+  $: {};
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-default-editor': GrDefaultEditor;
+  }
+}
+
+@customElement('gr-default-editor')
 /** @extends PolymerElement */
-class GrDefaultEditor extends GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement)) {
-  static get template() { return htmlTemplate; }
+export class GrDefaultEditor extends GestureEventListeners(
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
+  }
 
-  static get is() { return 'gr-default-editor'; }
   /**
    * Fired when the content of the editor changes.
    *
    * @event content-change
    */
 
-  static get properties() {
-    return {
-      fileContent: String,
-    };
-  }
+  @property({type: String})
+  fileContent: string | null = null;
 
-  _handleTextareaInput(e) {
-    this.dispatchEvent(new CustomEvent(
-        'content-change',
-        {detail: {value: e.target.value}, bubbles: true, composed: true}));
+  _handleTextareaInput(e: Event) {
+    this.dispatchEvent(
+      new CustomEvent('content-change', {
+        detail: {value: (e.target as HTMLTextAreaElement).value},
+        bubbles: true,
+        composed: true,
+      })
+    );
   }
 }
-
-customElements.define(GrDefaultEditor.is, GrDefaultEditor);

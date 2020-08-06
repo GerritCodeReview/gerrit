38b094fca12bc508e6c9f31e7208be67eabaf298
Milutin Kristofic <milutin@google.com>
2020-08-06 16:32:52 +0200

    Move gr-settings-item to typescript
    Change-Id: I4f7701508e3895ac83b018205191e934ff4c6261

--- polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-item.ts
+++ polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-item.ts
@@ -14,25 +14,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-settings-item_html.js';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-settings-item_html';
+import { property, customElement } from '@polymer/decorators';
 
-/** @extends PolymerElement */
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-settings-item': GrSettingsItem;
+  }
+}
+
+@customElement('gr-settings-item')
 class GrSettingsItem extends GestureEventListeners(
     LegacyElementMixin(
         PolymerElement)) {
   static get template() { return htmlTemplate; }
 
-  static get is() { return 'gr-settings-item'; }
-
-  static get properties() {
-    return {
-      anchor: String,
-      title: String,
-    };
-  }
+  @property({type: String})
+  anchor?: string;
+  @property({type: String})
+  title: string = '';
 }
-
-customElements.define(GrSettingsItem.is, GrSettingsItem);

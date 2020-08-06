4e0d301aa5a359f5d00b6a1c57784c68c84e5881
Milutin Kristofic <milutin@google.com>
2020-08-06 16:10:45 +0200

    Move bundled-polymer to typescript
    Change-Id: Ia9bbd579abe31a5476c2063464cfec7ad63b834d

--- polygerrit-ui/app/scripts/bundled-polymer.ts
+++ polygerrit-ui/app/scripts/bundled-polymer.ts
@@ -21,51 +21,51 @@
 // expects that Polymer is available we must setup all Polymer global
 // variables
 //
-// The bundled-polymer.js imports all scripts in the same order as the
+// The bundled-polymer imports all scripts in the same order as the
 // polymer.html does and must be imported in all es6-modules instead
 // of the polymer.html file.
 
-import 'polymer-bridges/polymer/lib/utils/boot_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/resolve-url_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/settings_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/mixin_bridge.js';
-import 'polymer-bridges/polymer/lib/elements/dom-module_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/style-gather_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/path_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/case-map_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/async_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/wrap_bridge.js';
-import 'polymer-bridges/polymer/lib/mixins/properties-changed_bridge.js';
-import 'polymer-bridges/polymer/lib/mixins/property-accessors_bridge.js';
-import 'polymer-bridges/polymer/lib/mixins/template-stamp_bridge.js';
-import 'polymer-bridges/polymer/lib/mixins/property-effects_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/telemetry_bridge.js';
-import 'polymer-bridges/polymer/lib/mixins/properties-mixin_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/debounce_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/gestures_bridge.js';
-import 'polymer-bridges/polymer/lib/mixins/gesture-event-listeners_bridge.js';
-import 'polymer-bridges/polymer/lib/mixins/dir-mixin_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/render-status_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/unresolved_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/array-splice_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/flattened-nodes-observer_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/flush_bridge.js';
-import 'polymer-bridges/polymer/lib/legacy/polymer.dom_bridge.js';
-import 'polymer-bridges/polymer/lib/legacy/legacy-element-mixin_bridge.js';
-import 'polymer-bridges/polymer/lib/legacy/class_bridge.js';
-import 'polymer-bridges/polymer/lib/legacy/polymer-fn_bridge.js';
-import 'polymer-bridges/polymer/lib/mixins/mutable-data_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/templatize_bridge.js';
-import 'polymer-bridges/polymer/lib/legacy/templatizer-behavior_bridge.js';
-import 'polymer-bridges/polymer/lib/elements/dom-bind_bridge.js';
-import 'polymer-bridges/polymer/lib/utils/html-tag_bridge.js';
-import 'polymer-bridges/polymer/polymer-element_bridge.js';
-import 'polymer-bridges/polymer/lib/elements/dom-repeat_bridge.js';
-import 'polymer-bridges/polymer/lib/elements/dom-if_bridge.js';
-import 'polymer-bridges/polymer/lib/elements/array-selector_bridge.js';
-import 'polymer-bridges/polymer/lib/elements/custom-style_bridge.js';
-import 'polymer-bridges/polymer/lib/legacy/mutable-data-behavior_bridge.js';
-import 'polymer-bridges/polymer/polymer-legacy_bridge.js';
-import {importHref} from './import-href.js';
+import 'polymer-bridges/polymer/lib/utils/boot_bridge';
+import 'polymer-bridges/polymer/lib/utils/resolve-url_bridge';
+import 'polymer-bridges/polymer/lib/utils/settings_bridge';
+import 'polymer-bridges/polymer/lib/utils/mixin_bridge';
+import 'polymer-bridges/polymer/lib/elements/dom-module_bridge';
+import 'polymer-bridges/polymer/lib/utils/style-gather_bridge';
+import 'polymer-bridges/polymer/lib/utils/path_bridge';
+import 'polymer-bridges/polymer/lib/utils/case-map_bridge';
+import 'polymer-bridges/polymer/lib/utils/async_bridge';
+import 'polymer-bridges/polymer/lib/utils/wrap_bridge';
+import 'polymer-bridges/polymer/lib/mixins/properties-changed_bridge';
+import 'polymer-bridges/polymer/lib/mixins/property-accessors_bridge';
+import 'polymer-bridges/polymer/lib/mixins/template-stamp_bridge';
+import 'polymer-bridges/polymer/lib/mixins/property-effects_bridge';
+import 'polymer-bridges/polymer/lib/utils/telemetry_bridge';
+import 'polymer-bridges/polymer/lib/mixins/properties-mixin_bridge';
+import 'polymer-bridges/polymer/lib/utils/debounce_bridge';
+import 'polymer-bridges/polymer/lib/utils/gestures_bridge';
+import 'polymer-bridges/polymer/lib/mixins/gesture-event-listeners_bridge';
+import 'polymer-bridges/polymer/lib/mixins/dir-mixin_bridge';
+import 'polymer-bridges/polymer/lib/utils/render-status_bridge';
+import 'polymer-bridges/polymer/lib/utils/unresolved_bridge';
+import 'polymer-bridges/polymer/lib/utils/array-splice_bridge';
+import 'polymer-bridges/polymer/lib/utils/flattened-nodes-observer_bridge';
+import 'polymer-bridges/polymer/lib/utils/flush_bridge';
+import 'polymer-bridges/polymer/lib/legacy/polymer.dom_bridge';
+import 'polymer-bridges/polymer/lib/legacy/legacy-element-mixin_bridge';
+import 'polymer-bridges/polymer/lib/legacy/class_bridge';
+import 'polymer-bridges/polymer/lib/legacy/polymer-fn_bridge';
+import 'polymer-bridges/polymer/lib/mixins/mutable-data_bridge';
+import 'polymer-bridges/polymer/lib/utils/templatize_bridge';
+import 'polymer-bridges/polymer/lib/legacy/templatizer-behavior_bridge';
+import 'polymer-bridges/polymer/lib/elements/dom-bind_bridge';
+import 'polymer-bridges/polymer/lib/utils/html-tag_bridge';
+import 'polymer-bridges/polymer/polymer-element_bridge';
+import 'polymer-bridges/polymer/lib/elements/dom-repeat_bridge';
+import 'polymer-bridges/polymer/lib/elements/dom-if_bridge';
+import 'polymer-bridges/polymer/lib/elements/array-selector_bridge';
+import 'polymer-bridges/polymer/lib/elements/custom-style_bridge';
+import 'polymer-bridges/polymer/lib/legacy/mutable-data-behavior_bridge';
+import 'polymer-bridges/polymer/polymer-legacy_bridge';
+import {importHref} from './import-href';
 
 window.Polymer.importHref = importHref;

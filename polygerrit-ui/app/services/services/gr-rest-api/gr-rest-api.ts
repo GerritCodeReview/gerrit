022ffe84ba4f928af95587331baa138618dbc58c
Dhruv Srivastava <dhruvsri@google.com>
2020-08-06 10:09:18 +0200

    Convert patch-set-util to ts
    Change-Id: I9b3f71492cf3454ddef4b3972c5e4ff719187b7b

--- polygerrit-ui/app/services/services/gr-rest-api/gr-rest-api.ts
+++ polygerrit-ui/app/services/services/gr-rest-api/gr-rest-api.ts
@@ -23,6 +23,7 @@ import {
   NumericChangeId,
   ServerInfo,
   ProjectInfo,
+  ChangeInfo,
 } from '../../../types/common';
 
 export type ErrorCallback = (response?: Response | null, err?: Error) => void;
@@ -103,4 +104,9 @@ export interface RestApiService {
     n?: number,
     errFn?: ErrorCallback
   ): Promise<AccountInfo[]>;
+  getChangeDetail(
+    changeNum: number | string,
+    opt_errFn?: Function,
+    opt_cancelCondition?: Function
+  ): Promise<ChangeInfo>;
 }

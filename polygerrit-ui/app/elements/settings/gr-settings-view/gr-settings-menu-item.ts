6847357d337a439b527c424c1e735a5d2000a3b3
Milutin Kristofic <milutin@google.com>
2020-08-06 16:38:53 +0200

    Move gr-settings-menu-item to typescript
    Change-Id: Iacc07a6f174f2a0be7499f99eb026ce2d4c6c91a

--- polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-menu-item.ts
+++ polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-menu-item.ts
@@ -14,26 +14,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '../../../styles/gr-page-nav-styles.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-settings-menu-item_html.js';
+import '../../../styles/gr-page-nav-styles';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-settings-menu-item_html';
+import { property, customElement } from '@polymer/decorators';
 
-/** @extends PolymerElement */
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-settings-menu-item': GrSettingsMenuItem;
+  }
+}
+
+@customElement('gr-settings-menu-item')
 class GrSettingsMenuItem extends GestureEventListeners(
     LegacyElementMixin(
         PolymerElement)) {
   static get template() { return htmlTemplate; }
 
-  static get is() { return 'gr-settings-menu-item'; }
-
-  static get properties() {
-    return {
-      href: String,
-      title: String,
-    };
-  }
+  @property({type: String})
+  anchor?: string;
+  @property({type: String})
+  title: string = '';
 }
 
-customElements.define(GrSettingsMenuItem.is, GrSettingsMenuItem);

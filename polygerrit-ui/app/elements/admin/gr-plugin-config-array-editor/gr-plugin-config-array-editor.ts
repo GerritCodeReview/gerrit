e5cbc64f256e77ff244e2ede523e946a252d7cfb
Milutin Kristofic <milutin@google.com>
2020-08-07 15:54:25 +0200

    Move gr-plugin-config-array-editor to typescript
    Change-Id: I12ccf3c6c7548c1ff97e3d6be1e0cded8461273e

--- polygerrit-ui/app/elements/admin/gr-plugin-config-array-editor/gr-plugin-config-array-editor.ts
+++ polygerrit-ui/app/elements/admin/gr-plugin-config-array-editor/gr-plugin-config-array-editor.ts
@@ -14,58 +14,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '@polymer/iron-input/iron-input.js';
-import '@polymer/paper-toggle-button/paper-toggle-button.js';
-import '../../../styles/gr-form-styles.js';
-import '../../../styles/shared-styles.js';
-import '../../shared/gr-button/gr-button.js';
-import {dom} from '@polymer/polymer/lib/legacy/polymer.dom.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-plugin-config-array-editor_html.js';
+import '@polymer/iron-input/iron-input';
+import '@polymer/paper-toggle-button/paper-toggle-button';
+import '../../../styles/gr-form-styles';
+import '../../../styles/shared-styles';
+import '../../shared/gr-button/gr-button';
+import {dom, EventApi} from '@polymer/polymer/lib/legacy/polymer.dom';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-plugin-config-array-editor_html';
+import {property, customElement} from '@polymer/decorators';
 
-/** @extends PolymerElement */
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-plugin-config-array-editor': GrPluginConfigArrayEditor;
+  }
+}
+
+@customElement('gr-plugin-config-array-editor')
 class GrPluginConfigArrayEditor extends GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement)) {
-  static get template() { return htmlTemplate; }
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
+  }
 
-  static get is() { return 'gr-plugin-config-array-editor'; }
   /**
    * Fired when the plugin config option changes.
    *
    * @event plugin-config-option-changed
    */
 
-  static get properties() {
-    return {
-    /** @type {?} */
-      pluginOption: Object,
-      /** @type {boolean} */
-      disabled: {
-        type: Boolean,
-        computed: '_computeDisabled(pluginOption.*)',
-      },
-      /** @type {?} */
-      _newValue: {
-        type: String,
-        value: '',
-      },
-    };
+  @property({type: String})
+  _newValue = '';
+
+  // This property is never null, since this component in only about operations
+  // on pluginOption.
+  @property({type: Object})
+  pluginOption!: PluginOption;
+
+  @property({type: Boolean, computed: '_computeDisabled(pluginOption.*)'})
+  disabled?: boolean;
+
+  _computeDisabled(record: PluginOptionRecord) {
+    return !(
+      record &&
+      record.base &&
+      record.base.info &&
+      record.base.info.editable
+    );
   }
 
-  _computeDisabled(record) {
-    return !(record && record.base && record.base.info &&
-        record.base.info.editable);
-  }
-
-  _handleAddTap(e) {
+  _handleAddTap(e: MouseEvent) {
     e.preventDefault();
     this._handleAdd();
   }
 
-  _handleInputKeydown(e) {
+  _handleInputKeydown(e: KeyboardEvent) {
     // Enter.
     if (e.keyCode === 13) {
       e.preventDefault();
@@ -74,19 +80,24 @@ class GrPluginConfigArrayEditor extends GestureEventListeners(
   }
 
   _handleAdd() {
-    if (!this._newValue.length) { return; }
+    if (!this._newValue.length) {
+      return;
+    }
     this._dispatchChanged(
-        this.pluginOption.info.values.concat([this._newValue]));
+      this.pluginOption.info.values.concat([this._newValue])
+    );
     this._newValue = '';
   }
 
-  _handleDelete(e) {
-    const value = dom(e).localTarget.dataset.item;
+  _handleDelete(e: MouseEvent) {
+    const value = ((dom(e) as EventApi).localTarget as HTMLElement).dataset
+      .item;
     this._dispatchChanged(
-        this.pluginOption.info.values.filter(str => str !== value));
+      this.pluginOption.info.values.filter(str => str !== value)
+    );
   }
 
-  _dispatchChanged(values) {
+  _dispatchChanged(values: string[]) {
     const {_key, info} = this.pluginOption;
     const detail = {
       _key,
@@ -94,13 +105,23 @@ class GrPluginConfigArrayEditor extends GestureEventListeners(
       notifyPath: `${_key}.values`,
     };
     this.dispatchEvent(
-        new CustomEvent('plugin-config-option-changed', {detail}));
+      new CustomEvent('plugin-config-option-changed', {detail})
+    );
   }
 
-  _computeShowInputRow(disabled) {
+  _computeShowInputRow(disabled: boolean) {
     return disabled ? 'hide' : '';
   }
 }
 
-customElements.define(GrPluginConfigArrayEditor.is,
-    GrPluginConfigArrayEditor);
+interface PluginOption {
+  info: {
+    values: string[];
+    editable?: boolean;
+  };
+  _key: string;
+}
+
+interface PluginOptionRecord {
+  base: PluginOption;
+}

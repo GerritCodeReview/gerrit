86de208704e62552a0e8237db76fd9ba0140b0c5
Milutin Kristofic <milutin@google.com>
2020-08-18 09:30:42 +0200

    Convert gr-repo-header to typescript
    The change converts the following files to typescript:
    
    * elements/change-list/gr-repo-header/gr-repo-header.ts
    
    Change-Id: Ifcfd83eeea613e80b2f791469adbc239247a45bc

--- polygerrit-ui/app/elements/change-list/gr-repo-header/gr-repo-header.ts
+++ polygerrit-ui/app/elements/change-list/gr-repo-header/gr-repo-header.ts
@@ -15,37 +15,34 @@
  * limitations under the License.
  */
 
-import '../../../styles/dashboard-header-styles.js';
-import '../../../styles/shared-styles.js';
-import '../../shared/gr-date-formatter/gr-date-formatter.js';
-import '../../shared/gr-rest-api-interface/gr-rest-api-interface.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-repo-header_html.js';
-import {GerritNav} from '../../core/gr-navigation/gr-navigation.js';
+import '../../../styles/dashboard-header-styles';
+import '../../../styles/shared-styles';
+import '../../shared/gr-date-formatter/gr-date-formatter';
+import '../../shared/gr-rest-api-interface/gr-rest-api-interface';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-repo-header_html';
+import {GerritNav} from '../../core/gr-navigation/gr-navigation';
+import {customElement, property} from '@polymer/decorators';
+import {RepositoryName} from '../../../types/common';
 
 /** @extends PolymerElement */
+@customElement('gr-repo-header')
 class GrRepoHeader extends GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement)) {
-  static get template() { return htmlTemplate; }
-
-  static get is() { return 'gr-repo-header'; }
-
-  static get properties() {
-    return {
-    /** @type {?string} */
-      repo: {
-        type: String,
-        observer: '_repoChanged',
-      },
-      /** @type {string|null} */
-      _repoUrl: String,
-    };
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
   }
 
-  _repoChanged(repoName) {
+  @property({type: String, observer: '_repoChanged'})
+  repo?: string;
+
+  @property({type: String})
+  _repoUrl: string | null = null;
+
+  _repoChanged(repoName: RepositoryName) {
     if (!repoName) {
       this._repoUrl = null;
       return;
@@ -54,4 +51,8 @@ class GrRepoHeader extends GestureEventListeners(
   }
 }
 
-customElements.define(GrRepoHeader.is, GrRepoHeader);
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-repo-header': GrRepoHeader;
+  }
+}

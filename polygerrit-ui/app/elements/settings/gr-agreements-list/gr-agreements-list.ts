79dc3eca587ca5705185dc710826301a7895cb74
Milutin Kristofic <milutin@google.com>
2020-08-19 21:56:21 +0200

    Convert gr-agreements-list to typescript
    The change converts the following files to typescript:
    
    * elements/settings/gr-agreements-list/gr-agreements-list.ts
    
    Change-Id: I24d6f9a611288d27ded71edfe3d2f3af815541b9

--- polygerrit-ui/app/elements/settings/gr-agreements-list/gr-agreements-list.ts
+++ polygerrit-ui/app/elements/settings/gr-agreements-list/gr-agreements-list.ts
@@ -15,30 +15,33 @@
  * limitations under the License.
  */
 
-import '../../../styles/gr-form-styles.js';
-import '../../../styles/shared-styles.js';
-import '../../shared/gr-rest-api-interface/gr-rest-api-interface.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-agreements-list_html.js';
-import {getBaseUrl} from '../../../utils/url-util.js';
+import '../../../styles/gr-form-styles';
+import '../../../styles/shared-styles';
+import '../../shared/gr-rest-api-interface/gr-rest-api-interface';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-agreements-list_html';
+import {getBaseUrl} from '../../../utils/url-util';
+import {customElement, property} from '@polymer/decorators';
+import {GrRestApiInterface} from '../../shared/gr-rest-api-interface/gr-rest-api-interface';
+import {ContributorAgreementInfo} from '../../../types/common';
 
-/**
- * @extends PolymerElement
- */
-class GrAgreementsList extends GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement)) {
-  static get template() { return htmlTemplate; }
-
-  static get is() { return 'gr-agreements-list'; }
-
-  static get properties() {
-    return {
-      _agreements: Array,
-    };
+export interface GrAgreementsList {
+  $: {
+    restAPI: GrRestApiInterface;
+  };
+}
+@customElement('gr-agreements-list')
+export class GrAgreementsList extends GestureEventListeners(
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
   }
+
+  @property({type: Array})
+  _agreements?: ContributorAgreementInfo[];
 
   /** @override */
   attached() {
@@ -53,12 +56,16 @@ class GrAgreementsList extends GestureEventListeners(
   }
 
   getUrl() {
-    return getBaseUrl() + '/settings/new-agreement';
+    return `${getBaseUrl()}/settings/new-agreement`;
   }
 
-  getUrlBase(item) {
-    return getBaseUrl() + '/' + item;
+  getUrlBase(item: string) {
+    return `${getBaseUrl()}/${item}`;
   }
 }
 
-customElements.define(GrAgreementsList.is, GrAgreementsList);
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-agreements-list': GrAgreementsList;
+  }
+}

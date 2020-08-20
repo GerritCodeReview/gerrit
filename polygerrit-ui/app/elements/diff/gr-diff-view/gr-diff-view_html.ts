e7de9c489a98ce5a2423c9764cd1d2a2c9fb2d48
Milutin Kristofic <milutin@google.com>
2020-08-20 17:45:21 +0200

    A11y - label for file reviewed checkbox
    Change-Id: Ibf008d92c4597f8c1b480fe9733e18df15f91747

--- polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view_html.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view_html.ts
@@ -225,6 +225,7 @@ export const htmlTemplate = html`
           on-change="_handleReviewedChange"
           hidden$="[[!_loggedIn]]"
           hidden=""
+          aria-label="file reviewed"
         /><!--
        -->
         <div class="jumpToFileContainer">

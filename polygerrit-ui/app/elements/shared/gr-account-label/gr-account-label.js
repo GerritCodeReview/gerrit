a1ac2555205f421215effca0f121cbcb61f11027
Ben Rohlfs <brohlfs@google.com>
2020-08-26 18:00:49 +0200

    Make attention icon a click target for removing the user
    Change-Id: Ie7f986edf8b5d887d3ef21ddba5365a20c5a6639

--- polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
+++ polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.js
@@ -23,6 +23,7 @@ import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-l
 import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {htmlTemplate} from './gr-account-label_html.js';
+import {appContext} from '../../../services/app-context.js';
 import {getDisplayName} from '../../../utils/display-name-util.js';
 import {isServiceUser} from '../../../utils/account-util.js';
 
@@ -42,6 +43,7 @@ class GrAccountLabel extends GestureEventListeners(
        * @type {{ name: string, status: string }}
        */
       account: Object,
+      _selfAccount: Object,
       /**
        * Optional ChangeInfo object, typically comes from the change page or
        * from a row in a list of search results. This is needed for some change
@@ -102,10 +104,18 @@ class GrAccountLabel extends GestureEventListeners(
     };
   }
 
+  constructor() {
+    super();
+    this.reporting = appContext.reportingService;
+  }
+
   /** @override */
   ready() {
     super.ready();
     this.$.restAPI.getConfig().then(config => { this._config = config; });
+    this.$.restAPI.getAccount().then(account => {
+      this._selfAccount = account;
+    });
     this.addEventListener('attention-set-updated', e => {
       // For re-evaluation of everything that depends on 'change'.
       this.change = {...this.change};
@@ -134,6 +144,57 @@ class GrAccountLabel extends GestureEventListeners(
 
   _computeName(account, config, firstName) {
     return getDisplayName(config, account, firstName);
+  }
+
+  _handleRemoveAttentionClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    this.dispatchEvent(new CustomEvent('show-alert', {
+      detail: {
+        message: 'Saving attention set update ...',
+        dismissOnNavigation: true,
+      },
+      composed: true,
+      bubbles: true,
+    }));
+
+    // We are deliberately updating the UI before making the API call. It is a
+    // risk that we are taking to achieve a better UX for 99.9% of the cases.
+    const selfName = getDisplayName(this._config, this._selfAccount);
+    const reason = `Removed by ${selfName} by clicking the attention icon`;
+    delete this.change.attention_set[this.account._account_id];
+    // For re-evaluation of everything that depends on 'change'.
+    this.change = {...this.change};
+
+    this.reporting.reportInteraction('attention-icon-remove',
+        this._reportingDetails());
+    this.$.restAPI.removeFromAttentionSet(this.change._number,
+        this.account._account_id, reason).then(obj => {
+      this.dispatchEvent(
+          new CustomEvent('hide-alert', {bubbles: true, composed: true})
+      );
+    });
+  }
+
+  _reportingDetails() {
+    const targetId = this.account._account_id;
+    const ownerId = (this.change && this.change.owner
+        && this.change.owner._account_id) || -1;
+    const selfId = (this._selfAccount && this._selfAccount._account_id) || -1;
+    const reviewers = (
+      this.change && this.change.reviewers && this.change.reviewers.REVIEWER ?
+        [...this.change.reviewers.REVIEWER] : []);
+    const reviewerIds = reviewers
+        .map(r => r._account_id)
+        .filter(rId => rId !== ownerId);
+    return {
+      actionByOwner: selfId === ownerId,
+      actionByReviewer: reviewerIds.includes(selfId),
+      targetIsOwner: targetId === ownerId,
+      targetIsReviewer: reviewerIds.includes(targetId),
+      targetIsSelf: targetId === selfId,
+    };
   }
 }
 

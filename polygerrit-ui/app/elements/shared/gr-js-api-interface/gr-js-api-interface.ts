3a6c15c986ff78d431c04a441aba6d6b1e7201bb
Ben Rohlfs <brohlfs@google.com>
2020-08-31 11:44:18 +0200

    Convert files to typescript
    The change converts the following files to typescript:
    
    * elements/shared/gr-js-api-interface/gr-js-api-interface.ts
    
    Change-Id: Ia3bfa803f48f7265d35dd923dcc75b1e897a5b15

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-js-api-interface.ts
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-js-api-interface.ts
@@ -14,21 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '../gr-rest-api-interface/gr-rest-api-interface.js';
-import './gr-js-api-interface-element.js';
-import './gr-public-js-api.js';
-import './gr-gerrit.js';
-
-/*
-  Note: the order matters as files depend on each other.
-  1. gr-api-utils will be used in multiple files below.
-  2. gr-gerrit depends on gr-plugin-loader, gr-public-js-api and
-    also gr-plugin-endpoints
-  3. gr-public-js-api depends on gr-plugin-rest-api
-*/
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-
+import '../gr-rest-api-interface/gr-rest-api-interface';
+import './gr-js-api-interface-element';
+import './gr-public-js-api';
+import './gr-gerrit';

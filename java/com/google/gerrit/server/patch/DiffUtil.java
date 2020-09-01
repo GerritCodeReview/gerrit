ee621fa0fa5252e43b0a4410d7bc98b7abe5a988
Youssef Elghareeb <ghareeb@google.com>
2020-09-01 18:29:07 +0200

    Add the GitModifiedFilesCache and ModifiedFilesCache
    The two new caches are part of the refactoring done to the Diff cache,
    previously implemented in PatchListLoader.java.
    
    The 2 new caches compute the set of modified files (added, deleted,
    modified, etc...) between 2 specific commits.
    
    1) The GitModifiedFilesCache computes the modified files between two
    commits according to a Git (tree) diff. File contents are not available
    with the cache values. The cache key uses the SHA-1s of the trees
    instead of the SHA-1s of the commits. This allows the re-use of
    computations when commits keep their trees but change in other ways
    (e.g. for different commit messages).
    
    2) The ModifiedFilesCache is a wrapper on top of the first cache and
    adds extra Gerrit logic. This cache filters out the files that were not
    touched between the 2 commits, previously implemented in
    PatchListLoader. Similar to the first cache, the file contents and edits
    are not available with the cache values.
    
    For now, the magic file paths "/COMMIT_MSG", and "/MERGE_LIST" are not
    returned from this cache. In a follow up change, the single file diff
    caches will be added and the caller can query the magic file paths from
    them. I excluded them from here because:
      * Checking for the cases when we should compute the merge list (e.g.
      determining if the diff is against base / patchsets) would require
      adding some more logic.
      * For commit messages between patchsets, to determine if there's a
      change or not, we would need to parse the actual commit message text.
    
    In the next follow up change, I will implement the protobuf
    serialization for the cache entities as marked with TODOs in this
    change.
    
    Change-Id: I0c6fc4e015009624c4f93471a2e6335d180f6c82

---
+++ java/com/google/gerrit/server/patch/DiffUtil.java
@@ -0,0 +1,82 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.google.gerrit.server.patch;
+
+import com.google.gerrit.server.patch.diff.ModifiedFilesCache;
+import com.google.gerrit.server.patch.gitdiff.GitModifiedFilesCache;
+import java.io.IOException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+/**
+ * A utility class used by the diff cache interfaces {@link GitModifiedFilesCache} and {@link
+ * ModifiedFilesCache}.
+ */
+public class DiffUtil {
+
+  /**
+   * Returns the Git tree object ID pointed to by the commitId parameter.
+   *
+   * @param rw a {@link RevWalk} of an opened repository that is used to walk the commit graph.
+   * @param commitId 20 bytes commitId SHA-1 hash.
+   * @return Git tree object ID pointed to by the commitId.
+   */
+  public static ObjectId getTreeId(RevWalk rw, ObjectId commitId) throws IOException {
+    RevCommit current = rw.parseCommit(commitId);
+    return current.getTree().getId();
+  }
+
+  /**
+   * Returns the RevCommit object given the 20 bytes commitId SHA-1 hash.
+   *
+   * @param rw a {@link RevWalk} of an opened repository that is used to walk the commit graph.
+   * @param commitId 20 bytes commitId SHA-1 hash
+   * @return The RevCommit representing the commit in Git
+   * @throws IOException a pack file or loose object could not be read while parsing the commits.
+   */
+  public static RevCommit getRevCommit(RevWalk rw, ObjectId commitId) throws IOException {
+    return rw.parseCommit(commitId);
+  }
+
+  /**
+   * Returns true if the commitA and commitB parameters are parent/child, if they have a common
+   * parent, or if any of them is a root or merge commit.
+   */
+  public static boolean areRelated(RevCommit commitA, RevCommit commitB) {
+    if (commitA == null
+        || isRootOrMergeCommit(commitA)
+        || isRootOrMergeCommit(commitB)
+        || areParentAndChild(commitA, commitB)
+        || haveCommonParent(commitA, commitB)) {
+      return true;
+    }
+    return false;
+  }
+
+  private static boolean isRootOrMergeCommit(RevCommit commit) {
+    return commit.getParentCount() != 1;
+  }
+
+  private static boolean areParentAndChild(RevCommit commitA, RevCommit commitB) {
+    return ObjectId.isEqual(commitA.getParent(0), commitB)
+        || ObjectId.isEqual(commitB.getParent(0), commitA);
+  }
+
+  private static boolean haveCommonParent(RevCommit commitA, RevCommit commitB) {
+    return ObjectId.isEqual(commitA.getParent(0), commitB.getParent(0));
+  }
+}

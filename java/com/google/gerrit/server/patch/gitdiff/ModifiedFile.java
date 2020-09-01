98da1eb3e4fed150b8c32e785c61d003df47a0bc
Youssef Elghareeb <ghareeb@google.com>
2020-09-01 18:29:07 +0200

    Add the GitModifiedFilesCache and ModifiedFilesCache
    The two new caches are part of the refactoring done to the Diff cache,
    previously implemented in PatchListLoader.java.
    
    The 2 new caches compute the set of modified files (added, deleted,
    modified, etc...) between 2 specific commits.
    
    1) The GitModifiedFilesCache computes the modified files between two
    commits according to a Git (tree) diff. File contents are not available
    with the cache values. The cache key uses the SHA-1s of the trees
    instead of the SHA-1s of the commits. This allows the re-use of
    computations when commits keep their trees but change in other ways
    (e.g. for different commit messages).
    
    2) The ModifiedFilesCache is a wrapper on top of the first cache and
    adds extra Gerrit logic. This cache filters out the files that were not
    touched between the 2 commits, previously implemented in
    PatchListLoader. Similar to the first cache, the file contents and edits
    are not available with the cache values.
    
    For now, the magic file paths "/COMMIT_MSG", and "/MERGE_LIST" are not
    returned from this cache. In a follow up change, the single file diff
    caches will be added and the caller can query the magic file paths from
    them. I excluded them from here because:
      * Checking for the cases when we should compute the merge list (e.g.
      determining if the diff is against base / patchsets) would require
      adding some more logic.
      * For commit messages between patchsets, to determine if there's a
      change or not, we would need to parse the actual commit message text.
    
    In the next follow up change, I will implement the protobuf
    serialization for the cache entities as marked with TODOs in this
    change.
    
    Change-Id: I0c6fc4e015009624c4f93471a2e6335d180f6c82

---
+++ java/com/google/gerrit/server/patch/gitdiff/ModifiedFile.java
@@ -0,0 +1,90 @@
+//  Copyright (C) 2020 The Android Open Source Project
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package com.google.gerrit.server.patch.gitdiff;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.entities.Patch.ChangeType;
+import com.google.gerrit.server.cache.serialize.CacheSerializer;
+import java.util.Optional;
+import org.apache.commons.lang.NotImplementedException;
+
+/**
+ * An entity representing a Modified file due to a diff between 2 git trees. This entity contains
+ * the change type and the old & new paths, but does not include any actual content diff of the
+ * file.
+ */
+@AutoValue
+public abstract class ModifiedFile {
+  /**
+   * Returns the change type (i.e. add, delete, modify, rename, etc...) associated with this
+   * modified file.
+   */
+  public abstract ChangeType changeType();
+
+  /**
+   * Returns the old name associated with this file. An empty optional is returned if {@link
+   * #changeType()} is equal to {@link ChangeType#ADDED}.
+   */
+  public abstract Optional<String> oldPath();
+
+  /**
+   * Returns the new name associated with this file. An empty optional is returned if {@link
+   * #changeType()} is equal to {@link ChangeType#DELETED}
+   */
+  public abstract Optional<String> newPath();
+
+  public static Builder builder() {
+    return new AutoValue_ModifiedFile.Builder();
+  }
+
+  /** Computes this object's weight, which is its size in bytes. */
+  public int weight() {
+    int weight = 1; // the changeType field
+    if (oldPath().isPresent()) {
+      weight += oldPath().get().length();
+    }
+    if (newPath().isPresent()) {
+      weight += newPath().get().length();
+    }
+    return weight;
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+
+    public abstract Builder changeType(ChangeType value);
+
+    public abstract Builder oldPath(Optional<String> value);
+
+    public abstract Builder newPath(Optional<String> value);
+
+    public abstract ModifiedFile build();
+  }
+
+  // TODO(ghareeb): Implement protobuf serialization
+  enum Serializer implements CacheSerializer<ModifiedFile> {
+    INSTANCE;
+
+    @Override
+    public byte[] serialize(ModifiedFile object) {
+      throw new NotImplementedException("This method is not yet implemented");
+    }
+
+    @Override
+    public ModifiedFile deserialize(byte[] in) {
+      throw new NotImplementedException("This method is not yet implemented");
+    }
+  }
+}

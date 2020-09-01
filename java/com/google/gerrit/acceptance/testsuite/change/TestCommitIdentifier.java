4cb39eb5c5dbb89d2334aed069105368d4d48428
Alice Kober-Sotzek <aliceks@google.com>
2020-09-01 19:03:47 +0200

    Support parent specification (-> merge commits) in test API of changes
    With this change, the parent or parents of a change can be easily
    specified on change creation. The same holds for new patchsets
    (-> rebase situation).
    
    Thus, changes can deliberately be merge or non-merge commits or even
    switch between the two between patchsets. The API also supports more
    than 2 parents as we might want to test some of our code for robustness
    even for that unlikely situation (e.g. we have explicit code for octopus
    merges inside of the diff caches which is likely untested right now).
    
    At the moment, we support four different variants (commit SHA-1, branch
    name whose tip should be taken, change whose current patchset should
    be taken, patchset) for indicating parent commits. If necessary,
    further variants can be added with just a few lines of code in the
    future.
    
    Some of the added builder objects might seem unnecessarily complex.
    However, we decided to go for a flexible, fluent API which is as easy
    and guided as possible during its use. To achieve this, we need some
    more builder objects which we need to stick together in the right
    manner.
    
    Change-Id: I4bf2040798cad9fcdbfb46add55b93bf3c7023f7

---
+++ java/com/google/gerrit/acceptance/testsuite/change/TestCommitIdentifier.java
@@ -0,0 +1,61 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.testsuite.change;
+
+import com.google.auto.value.AutoOneOf;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.entities.PatchSet;
+import org.eclipse.jgit.lib.ObjectId;
+
+/** Attributes, each one uniquely identifying a commit. */
+@AutoOneOf(TestCommitIdentifier.Kind.class)
+public abstract class TestCommitIdentifier {
+  public enum Kind {
+    COMMIT_SHA_1,
+    BRANCH,
+    CHANGE_ID,
+    PATCHSET_ID
+  }
+
+  public abstract Kind getKind();
+
+  /** SHA-1 of the commit. */
+  public abstract ObjectId commitSha1();
+
+  /** Branch whose tip points to the desired commit. */
+  public abstract String branch();
+
+  /** Numeric ID of the change whose current patchset points to the desired commit. */
+  public abstract Change.Id changeId();
+
+  /** ID of the patchset representing the desired commit. */
+  public abstract PatchSet.Id patchsetId();
+
+  public static TestCommitIdentifier ofCommitSha1(ObjectId commitSha1) {
+    return AutoOneOf_TestCommitIdentifier.commitSha1(commitSha1);
+  }
+
+  public static TestCommitIdentifier ofBranch(String branchName) {
+    return AutoOneOf_TestCommitIdentifier.branch(branchName);
+  }
+
+  public static TestCommitIdentifier ofChangeId(Change.Id changeId) {
+    return AutoOneOf_TestCommitIdentifier.changeId(changeId);
+  }
+
+  public static TestCommitIdentifier ofPatchsetId(PatchSet.Id patchsetId) {
+    return AutoOneOf_TestCommitIdentifier.patchsetId(patchsetId);
+  }
+}

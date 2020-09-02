e7c8c04d48cc56fdb888c80330273b58ab837c59
Youssef Elghareeb <ghareeb@google.com>
2020-09-02 12:23:21 +0200

    Refactor EditTransformer to take new inputs
    EditTransformer is the class that we use to identify the edits due to
    rebase between 2 commits. These rebase edits are a function of the edits
    between these 2 commits, each of the commits and its parent, and the
    edits between the parent commits.
    
    The previous implementation of EditTransformer accepted a list of
    PatchListEntry(s) corresponding to multiple changed files between 2
    commits. This was not optimal, since EditTransformer uses only a small
    subset of fields of PatchListEntry.
    
    This change introduces a new entity class "FileEdits" containing only
    the fields needed by EditTransformer, namely the list of edits and the
    old/new file paths.
    
    New methods are introduced in EditTransformer to use the FileEdits
    entities, also some of the methods using the PatchListEntry are
    eliminated. PatchListLoader (the old diff cache implementation) is
    modified to use the new methods. Subsequent changes will introduce the
    git file diff and the file diff caches which should use the new methods.
    
    Change-Id: I3d31180b44d2495c4fa9ee8902f979f23c47cf4b

---
+++ java/com/google/gerrit/server/patch/entities/Edit.java
@@ -0,0 +1,45 @@
+//  Copyright (C) 2020 The Android Open Source Project
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package com.google.gerrit.server.patch.entities;
+
+import com.google.auto.value.AutoValue;
+
+/**
+ * A modified region between 2 versions of the same content. This is the Gerrit entity class
+ * corresponding to {@link org.eclipse.jgit.diff.Edit}.
+ */
+@AutoValue
+public abstract class Edit {
+  static Edit create(int beginA, int endA, int beginB, int endB) {
+    return new AutoValue_Edit(beginA, endA, beginB, endB);
+  }
+
+  public static Edit fromJGitEdit(org.eclipse.jgit.diff.Edit jgitEdit) {
+    return create(
+        jgitEdit.getBeginA(), jgitEdit.getEndA(), jgitEdit.getBeginB(), jgitEdit.getEndB());
+  }
+
+  /** Start of a region in sequence A. */
+  public abstract int beginA();
+
+  /** End of a region in sequence A. */
+  public abstract int endA();
+
+  /** Start of a region in sequence B. */
+  public abstract int beginB();
+
+  /** End of a region in sequence B. */
+  public abstract int endB();
+}

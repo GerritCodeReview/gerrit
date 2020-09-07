5b30b7015770333f3698f35a60abd5f9fae8fd51
Gal Paikin <paiking@google.com>
2020-09-07 18:17:43 +0300

    Removed reviewers fix
    Change-Id: I97b645cd3149206d4b7d4729f3b33ac5a9c5ae7d

--- java/com/google/gerrit/server/notedb/ChangeUpdate.java
+++ java/com/google/gerrit/server/notedb/ChangeUpdate.java
@@ -788,11 +788,10 @@ public class ChangeUpdate extends AbstractChangeUpdate {
                 reviewer.getKey(), AttentionSetUpdate.Operation.ADD, "Reviewer was added"));
       }
       // Treat both REMOVED and CC as "removed reviewers".
-      if (!reviewer.getValue().equals(ReviewerStateInternal.REVIEWER)
-          && currentReviewers.contains(reviewer.getKey())) {
+      if (reviewer.getValue().equals(ReviewerStateInternal.REMOVED)) {
         updates.add(
             AttentionSetUpdate.createForWrite(
-                reviewer.getKey(), AttentionSetUpdate.Operation.REMOVE, "Reviewer was removed"));
+                reviewer.getKey(), AttentionSetUpdate.Operation.REMOVE, "Reviewer/Cc was removed"));
       }
     }
     addToPlannedAttentionSetUpdates(updates);

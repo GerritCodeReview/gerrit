bc9a0577835cbf248302f6fb0d57badb5e2c92fb
Alice Kober-Sotzek <aliceks@google.com>
2020-09-10 12:59:52 +0200

    Extract and rewrite the logic for comment threads
    We need the logic for comment threads in more places.
    Instead of duplicating it and possibly getting it wrong, we now
    consolidate it in one place. This also includes the logic when a
    comment thread is considered as unresolved/resolved.
    
    A large benefit of this extraction is that we finally have proper test
    coverage for various situations around comment threads. The previous
    integration tests only verified a common case.
    
    The new logic should also cover the issues previously solved by
    I97c6aeb47 and I2788fdb22. In addition, the new logic also properly
    covers longer child branches for which comments are not left one after
    the other but with mixed dates. For just determining the unresolved
    state of the whole thread, this didn't matter as only the last comment
    (previously also determined by date for child branches) is relevant.
    Now that we build the full comment threads and hand them out, we should
    get the order of the contained comments right.
    
    Unfortunately, the frontend orders comments on its own into threads.
    That logic (currently contained in gr-comment-api.js) might differ from
    the one implemented in the backend. Improving this situation or aligning
    the logic is beyond the scope of this change, though.
    
    Change-Id: Ie2713ebdbe05b69a705d2c9c03375b206a248e01

---
+++ javatests/com/google/gerrit/server/change/CommentThreadTest.java
@@ -0,0 +1,80 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.Comment;
+import com.google.gerrit.entities.Comment.Key;
+import com.google.gerrit.entities.HumanComment;
+import java.sql.Timestamp;
+import org.junit.Test;
+
+public class CommentThreadTest {
+
+  @Test
+  public void threadMustContainAtLeastOneComment() {
+    assertThrows(IllegalStateException.class, () -> CommentThread.builder().build());
+  }
+
+  @Test
+  public void threadCanBeUnresolved() {
+    HumanComment root = unresolved(createComment("root"));
+    CommentThread<Comment> commentThread = CommentThread.builder().addComment(root).build();
+
+    assertThat(commentThread.unresolved()).isTrue();
+  }
+
+  @Test
+  public void threadCanBeResolved() {
+    HumanComment root = resolved(createComment("root"));
+    CommentThread<Comment> commentThread = CommentThread.builder().addComment(root).build();
+
+    assertThat(commentThread.unresolved()).isFalse();
+  }
+
+  @Test
+  public void lastCommentInThreadDeterminesUnresolvedStatus() {
+    HumanComment root = resolved(createComment("root"));
+    HumanComment child = unresolved(createComment("child"));
+    CommentThread<Comment> commentThread =
+        CommentThread.builder().addComment(root).addComment(child).build();
+
+    assertThat(commentThread.unresolved()).isTrue();
+  }
+
+  private static HumanComment createComment(String commentUuid) {
+    return new HumanComment(
+        new Key(commentUuid, "myFile", 1),
+        Account.id(100),
+        new Timestamp(1234),
+        (short) 1,
+        "Comment text",
+        "serverId",
+        true);
+  }
+
+  private static HumanComment resolved(HumanComment comment) {
+    comment.unresolved = false;
+    return comment;
+  }
+
+  private static HumanComment unresolved(HumanComment comment) {
+    comment.unresolved = true;
+    return comment;
+  }
+}

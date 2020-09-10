a37242d5f7b638b25624f1cd77a0da8435592621
Gal Paikin <paiking@google.com>
2020-09-10 17:17:34 +0300

    Block submission on changes with "BLOCK_SUBMISSION" hashtag
    Users often write "DO NOT SUBMIT" as part of the subject of a change.
    This is done to avoid submitting changes that shoukdn't be sumitted.
    Now, in addition to this behavior, it's possible to add a hashtag
    such as "BLOCK_SUBMISSION" which will block the submission until
    that hashtag is removed.
    
    This is also useful for bots that have permission to add and remove
    hashtags, such that those bots will now be able to block submission
    not only by voting -1 on blocking labels.
    
    Change-Id: Iac50a578adfebc58c68ee68da12debac99b65e8e

---
+++ javatests/com/google/gerrit/acceptance/server/rules/BlockSubmissionHashtagRuleIT.java
@@ -0,0 +1,64 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.server.rules;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.NoHttpd;
+import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.entities.SubmitRecord;
+import com.google.gerrit.entities.SubmitRequirement;
+import com.google.gerrit.extensions.api.changes.HashtagsInput;
+import com.google.gerrit.server.rules.BlockSubmissionHashtagRule;
+import com.google.inject.Inject;
+import java.util.Optional;
+import org.junit.Test;
+
+@NoHttpd
+public class BlockSubmissionHashtagRuleIT extends AbstractDaemonTest {
+  @Inject private BlockSubmissionHashtagRule rule;
+
+  @Test
+  public void blocksWhenBlockSubmissionHashtagIsPresent() throws Exception {
+    PushOneCommit.Result r = createChange();
+    HashtagsInput hashtagsInput = new HashtagsInput();
+    hashtagsInput.add = ImmutableSet.of("BLOCK_SUBMISSION");
+    gApi.changes().id(r.getChangeId()).setHashtags(hashtagsInput);
+
+    Optional<SubmitRecord> submitRecord = rule.evaluate(r.getChange());
+
+    assertThat(submitRecord).isPresent();
+    SubmitRecord result = submitRecord.get();
+    assertThat(result.status).isEqualTo(SubmitRecord.Status.NOT_READY);
+    assertThat(result.requirements)
+        .containsExactly(
+            SubmitRequirement.builder()
+                .setFallbackText("Change must not have BLOCK_SUBMISSION hashtag")
+                .setType("block_submission_hashtag")
+                .build());
+  }
+
+  @Test
+  public void doesNothingByDefault() throws Exception {
+    PushOneCommit.Result r = createChange();
+    approve(r.getChangeId());
+
+    Optional<SubmitRecord> submitRecord = rule.evaluate(r.getChange());
+    assertThat(submitRecord).isEmpty();
+  }
+}

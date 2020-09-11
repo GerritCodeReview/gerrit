e7d0f591b3edea9e06f70dd88197a7bd5e562c47
Dhruv Srivastava <dhruvsri@google.com>
2020-09-11 16:56:38 +0200

    Port comments across patchsets in diff view
    Port unresolved comments across patchsets in diff view. This change
    displays the comments in diff view and change view
    protected by an experiment flag.
    These ported comments are requested in a separate API call and rendered
    after the normal comments.
    Porting drafts will be done in a follow up change.
    
    Screenshot: https://imgur.com/a/ZLcrZhZ
    Change-Id: I2c774cfe6186702e1231c656c3a7a1c0bc737231

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.ts
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list.ts
@@ -65,6 +65,7 @@ import {
   PreferencesInfo,
   RevisionInfo,
   UrlEncodedCommentId,
+  PathToCommentsInfoMap,
 } from '../../../types/common';
 import {DiffPreferencesInfo} from '../../../types/diff';
 import {GrDiffHost} from '../../diff/gr-diff-host/gr-diff-host';
@@ -74,10 +75,15 @@ import {GrDiffCursor} from '../../diff/gr-diff-cursor/gr-diff-cursor';
 import {GrCursorManager} from '../../shared/gr-cursor-manager/gr-cursor-manager';
 import {PolymerSpliceChange} from '@polymer/polymer/interfaces';
 import {ChangeComments} from '../../diff/gr-comment-api/gr-comment-api';
-import {UIDraft} from '../../../utils/comment-util';
+import {
+  UIDraft,
+  getPortedCommentThreads,
+  CommentThread,
+} from '../../../utils/comment-util';
 import {ParsedChangeInfo} from '../../shared/gr-rest-api-interface/gr-reviewer-updates-parser';
-import {PatchSetFile} from '../../../types/types';
 import {CustomKeyboardEvent} from '../../../types/events';
+import {PatchSetFile} from '../../../types/types';
+import {KnownExperimentId} from '../../../services/flags/flags';
 
 export const DEFAULT_NUM_FILES_SHOWN = 200;
 
@@ -332,7 +338,14 @@ export class GrFileList extends KeyboardShortcutMixin(
   @property({type: Array})
   _dynamicPrependedContentEndpoints?: string[];
 
+  @property({type: Object})
+  portedComments?: PathToCommentsInfoMap;
+
+  private _isPortingCommentsExperimentEnabled = false;
+
   private readonly reporting = appContext.reportingService;
+
+  private readonly flagsService = appContext.flagsService;
 
   get keyBindings() {
     return {
@@ -376,6 +389,9 @@ export class GrFileList extends KeyboardShortcutMixin(
   /** @override */
   attached() {
     super.attached();
+    this._isPortingCommentsExperimentEnabled = this.flagsService.isEnabled(
+      KnownExperimentId.PORTING_COMMENTS
+    );
     getPluginLoader()
       .awaitPluginsLoaded()
       .then(() => {
@@ -1558,11 +1574,29 @@ export class GrFileList extends KeyboardShortcutMixin(
             'changeComments, patchRange and diffPrefs must be set'
           );
         }
-        diffElem.threads = this.changeComments.getThreadsBySideForFile(
-          file,
-          this.patchRange,
-          this.projectConfig
-        );
+        let portedThreads: CommentThread[] = [];
+        if (
+          this.changeComments.portedComments &&
+          this.changeComments &&
+          path &&
+          this._isPortingCommentsExperimentEnabled
+        ) {
+          portedThreads = getPortedCommentThreads(
+            this.changeComments.portedComments,
+            path,
+            this.changeComments,
+            this.patchRange
+          );
+        }
+
+        diffElem.threads = [
+          ...this.changeComments.getThreadsBySideForFile(
+            file,
+            this.patchRange,
+            this.projectConfig
+          ),
+          ...portedThreads,
+        ];
         const promises: Array<Promise<unknown>> = [diffElem.reload()];
         if (this._loggedIn && !this.diffPrefs.manual_review) {
           promises.push(this._reviewFile(path, true));

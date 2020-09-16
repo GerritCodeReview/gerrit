909b3a8c9c6d5b21e2db75321ab12f5934a90f20
Ivan Frade <ifrade@google.com>
2020-09-16 14:57:47 -0700

    ReceiveCommits: Read the updated branches from the BatchUpdate
    ReceiveCommits and MergeOp update superprojects but their way to do that
    is very different. We are trying to make that code similar looking to
    abstract later the "submission" and make the superproject update a "post
    submission" action.
    
    Read what branches have been updated from the BatchUpdate, instead of
    from its internal ReceiveCommands. MergeOp will do the same for its call
    to updateSuperprojects.
    
    Change-Id: I0172a06cca55db956b2ee7867404f57972053ce1

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -719,6 +719,7 @@ class ReceiveCommits {
         parseRegularCommand(cmd);
       }
 
+      Set<BranchNameKey> branches;
       try (BatchUpdate bu =
               batchUpdateFactory.create(
                   project.getNameKey(), user.materializedCopy(), TimeUtil.nowTs());
@@ -737,11 +738,14 @@ class ReceiveCommits {
         }
         logger.atFine().log("Added %d additional ref updates", added);
         bu.execute();
+        branches =
+            bu.getSuccessfullyUpdatedBranches(/* dryrun=*/ false)
+                .filter(branch -> isHead(branch) || isConfig(branch))
+                .collect(Collectors.toSet());
       } catch (UpdateException | RestApiException e) {
         throw new StorageException(e);
       }
 
-      Set<BranchNameKey> branches = new HashSet<>();
       for (ReceiveCommand c : cmds) {
         // Most post-update steps should happen in UpdateOneRefOp#postUpdate. The only steps that
         // should happen in this loops are things that can't happen within one BatchUpdate because
@@ -757,7 +761,6 @@ class ReceiveCommits {
               Task closeProgress = progress.beginSubTask("closed", UNKNOWN);
               autoCloseChanges(c, closeProgress);
               closeProgress.end();
-              branches.add(BranchNameKey.create(project.getNameKey(), c.getRefName()));
               break;
 
             case DELETE:
@@ -3462,8 +3465,16 @@ class ReceiveCommits {
     return cmd.getRefName().startsWith(Constants.R_HEADS);
   }
 
+  private static boolean isHead(BranchNameKey branchNameKey) {
+    return branchNameKey.branch().startsWith(Constants.R_HEADS);
+  }
+
   private static boolean isConfig(ReceiveCommand cmd) {
     return cmd.getRefName().equals(RefNames.REFS_CONFIG);
+  }
+
+  private static boolean isConfig(BranchNameKey branchNameKey) {
+    return branchNameKey.branch().equals(RefNames.REFS_CONFIG);
   }
 
   private static String commandToString(ReceiveCommand cmd) {

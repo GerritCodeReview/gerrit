9bdc3bd8357328318f348fa2bc0b0bce30a4420e
Ivan Frade <ifrade@google.com>
2020-09-16 16:12:49 -0700

    SubmoduleOp: Teach submoduleOps dryrun
    On dryrun, the refs are not really updated and
    submoduleOp cannot read the correct values from the repo manager.
    
    Pass the results of the update to submoduleOp and use them as branch
    tips for the submodule updates on dryrun.
    
    At the moment this has no effect as ReceiveCommits is the only caller
    (and doesn't use dryrun) but is required to use it from MergeOp.
    
    Change-Id: Id33c8eac2c5fb509fc0aefb94dfad9490c34e4ec

--- java/com/google/gerrit/server/submit/SubmoduleOp.java
+++ java/com/google/gerrit/server/submit/SubmoduleOp.java
@@ -19,6 +19,7 @@ import com.google.gerrit.entities.BranchNameKey;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.exceptions.StorageException;
 import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.server.git.CodeReviewCommit;
 import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.gerrit.server.submit.MergeOpRepoManager.OpenRepo;
 import com.google.gerrit.server.update.BatchUpdate;
@@ -50,31 +51,41 @@ public class SubmoduleOp {
         Map<BranchNameKey, ReceiveCommand> updatedBranches, MergeOpRepoManager orm)
         throws SubmoduleConflictException {
       return new SubmoduleOp(
+          updatedBranches,
           orm,
           subscriptionGraphFactory.compute(updatedBranches.keySet(), orm),
           submoduleCommitsFactory.create(orm));
     }
   }
 
+  private final Map<BranchNameKey, ReceiveCommand> updatedBranches;
   private final MergeOpRepoManager orm;
   private final SubscriptionGraph subscriptionGraph;
   private final SubmoduleCommits submoduleCommits;
   private final UpdateOrderCalculator updateOrderCalculator;
 
   private SubmoduleOp(
+      Map<BranchNameKey, ReceiveCommand> updatedBranches,
       MergeOpRepoManager orm,
       SubscriptionGraph subscriptionGraph,
       SubmoduleCommits submoduleCommits) {
+    this.updatedBranches = updatedBranches;
     this.orm = orm;
     this.subscriptionGraph = subscriptionGraph;
     this.submoduleCommits = submoduleCommits;
     this.updateOrderCalculator = new UpdateOrderCalculator(subscriptionGraph);
   }
 
-  public void updateSuperProjects() throws RestApiException {
+  public void updateSuperProjects(boolean dryrun) throws RestApiException {
     ImmutableSet<Project.NameKey> projects = updateOrderCalculator.getProjectsInOrder();
     if (projects == null) {
       return;
+    }
+
+    if (dryrun) {
+      // On dryrun, the refs hasn't been updated.
+      // force the new tips on submoduleCommits
+      forceRefTips(updatedBranches, submoduleCommits);
     }
 
     LinkedHashSet<Project.NameKey> superProjects = new LinkedHashSet<>();
@@ -92,9 +103,29 @@ public class SubmoduleOp {
           }
         }
       }
-      BatchUpdate.execute(orm.batchUpdates(superProjects), BatchUpdateListener.NONE, false);
+      BatchUpdate.execute(orm.batchUpdates(superProjects), BatchUpdateListener.NONE, dryrun);
     } catch (UpdateException | IOException | NoSuchProjectException e) {
       throw new StorageException("Cannot update gitlinks", e);
+    }
+  }
+
+  private void forceRefTips(
+      Map<BranchNameKey, ReceiveCommand> updatedBranches, SubmoduleCommits submoduleCommits) {
+    // This is dryrun, all commands succeeded (no need to filter success).
+    for (Map.Entry<BranchNameKey, ReceiveCommand> updateBranch : updatedBranches.entrySet()) {
+      try {
+        ReceiveCommand command = updateBranch.getValue();
+        if (command.getType() == ReceiveCommand.Type.DELETE) {
+          continue;
+        }
+
+        BranchNameKey branchNameKey = updateBranch.getKey();
+        OpenRepo openRepo = orm.getRepo(branchNameKey.project());
+        CodeReviewCommit fakeTip = openRepo.rw.parseCommit(command.getNewId());
+        submoduleCommits.addBranchTip(branchNameKey, fakeTip);
+      } catch (NoSuchProjectException | IOException e) {
+        throw new StorageException("Cannot find branch tip target in dryrun", e);
+      }
     }
   }
 }

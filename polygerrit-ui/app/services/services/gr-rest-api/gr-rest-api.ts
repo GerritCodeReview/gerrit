b0d28a9c2d878a235439746da4211c7ff47143b9
Milutin Kristofic <milutin@google.com>
2020-09-17 10:38:08 +0200

    Convert gr-comment to typescript
    The change converts the following files to typescript:
    
    * elements/shared/gr-comment/gr-comment.ts
    
    Change-Id: I34975acf43537d9317462eb721d64a82fd21b441

--- polygerrit-ui/app/services/services/gr-rest-api/gr-rest-api.ts
+++ polygerrit-ui/app/services/services/gr-rest-api/gr-rest-api.ts
@@ -78,6 +78,7 @@ import {
   GroupAuditEventInfo,
   EncodedGroupId,
   Base64FileContent,
+  UrlEncodedCommentId,
 } from '../../../types/common';
 import {ParsedChangeInfo} from '../../../elements/shared/gr-rest-api-interface/gr-reviewer-updates-parser';
 import {HttpMethod} from '../../../constants/constants';
@@ -602,6 +603,18 @@ export interface RestApiService {
     changeNum: ChangeNum,
     account: AccountId,
     label: string
+  ): Promise<Response>;
+
+  deleteComment(
+    changeNum: ChangeNum,
+    patchNum: PatchSetNum,
+    commentID: UrlEncodedCommentId,
+    reason: string
+  ): Promise<CommentInfo>;
+  deleteDiffDraft(
+    changeNum: ChangeNum,
+    patchNum: PatchSetNum,
+    draft: {id: UrlEncodedCommentId}
   ): Promise<Response>;
 
   deleteChangeCommitMessage(

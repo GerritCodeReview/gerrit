7ed03cb7563f82861eec4ef8dfedb5ebda1a9d66
Marco Miller <marco.miller@ericsson.com>
2020-09-21 16:57:22 -0400

    e2e-tests: Rename 'name' to className for explicitness
    While still keeping a relatively short name for this highly reused
    value. Preserve a fair enough balance between footprint and meaning.
    
    Change-Id: I3527fb39e414b81511990312b63175e42785c694

--- e2e-tests/src/test/scala/com/google/gerrit/scenarios/FlushProjectsCache.scala
+++ e2e-tests/src/test/scala/com/google/gerrit/scenarios/FlushProjectsCache.scala
@@ -22,7 +22,7 @@ import scala.concurrent.duration._
 
 class FlushProjectsCache extends CacheFlushSimulation {
   private val data: FeederBuilder = jsonFile(resource).convert(keys).queue
-  private val default = name
+  private val default = className
 
   override def relativeRuntimeWeight = 2
 

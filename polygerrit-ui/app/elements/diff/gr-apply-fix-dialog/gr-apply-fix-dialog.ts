2fd470a048d583b82e353e1bac14226a7e72ece1
Dmitrii Filippov <dmfilippov@google.com>
2020-09-22 16:51:18 +0200

    Convert files to typescript
    The change converts the following files to typescript:
    
    * elements/diff/gr-apply-fix-dialog/gr-apply-fix-dialog.ts
    
    Change-Id: I274ba656285897c89c42f299724e402758b8cfe3

--- polygerrit-ui/app/elements/diff/gr-apply-fix-dialog/gr-apply-fix-dialog.ts
+++ polygerrit-ui/app/elements/diff/gr-apply-fix-dialog/gr-apply-fix-dialog.ts
@@ -14,87 +14,106 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '@polymer/iron-icon/iron-icon.js';
-import '../../../styles/shared-styles.js';
-import '../../shared/gr-dialog/gr-dialog.js';
-import '../../shared/gr-overlay/gr-overlay.js';
-import '../../shared/gr-rest-api-interface/gr-rest-api-interface.js';
-import '../gr-diff/gr-diff.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-apply-fix-dialog_html.js';
-import {GerritNav} from '../../core/gr-navigation/gr-navigation.js';
+import '@polymer/iron-icon/iron-icon';
+import '../../../styles/shared-styles';
+import '../../shared/gr-dialog/gr-dialog';
+import '../../shared/gr-overlay/gr-overlay';
+import '../../shared/gr-rest-api-interface/gr-rest-api-interface';
+import '../gr-diff/gr-diff';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-apply-fix-dialog_html';
+import {GerritNav} from '../../core/gr-navigation/gr-navigation';
+import {customElement, property} from '@polymer/decorators';
+import {ParsedChangeInfo} from '../../shared/gr-rest-api-interface/gr-reviewer-updates-parser';
+import {FixSuggestionInfo, PatchSetNum, RobotId} from '../../../types/common';
+import {GrOverlay} from '../../shared/gr-overlay/gr-overlay';
 
-/**
- * @extends PolymerElement
- */
-class GrApplyFixDialog extends GestureEventListeners(
-    LegacyElementMixin(PolymerElement)) {
-  static get template() { return htmlTemplate; }
+export interface GrApplyFixDialog {
+  $: {
+    applyFixOverlay: GrOverlay;
+  }
+}
 
-  static get is() { return 'gr-apply-fix-dialog'; }
-
-  static get properties() {
-    return {
-      // Diff rendering preference API response.
-      prefs: Array,
-      // ChangeInfo API response object.
-      change: Object,
-      changeNum: String,
-      _patchNum: Number,
-      // robot ID associated with a robot comment.
-      _robotId: String,
-      // Selected FixSuggestionInfo entity from robot comment API response.
-      _currentFix: Object,
-      // Flattened /preview API response DiffInfo map object.
-      _currentPreviews: {type: Array, value: () => []},
-      // FixSuggestionInfo entities from robot comment API response.
-      _fixSuggestions: Array,
-      _isApplyFixLoading: {
-        type: Boolean,
-        value: false,
-      },
-      // Index of currently showing suggested fix.
-      _selectedFixIdx: Number,
-      _disableApplyFixButton: {
-        type: Boolean,
-        computed: '_computeDisableApplyFixButton(_isApplyFixLoading, change, '
-          + '_patchNum)',
-      },
-    };
+@customElement('gr-apply-fix-dialog')
+export class GrApplyFixDialog extends GestureEventListeners(
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
   }
 
+  @property({type: Array})
+  prefs?: unknown;
+
+  @property({type: Object})
+  change?: ParsedChangeInfo;
+
+  @property({type: String})
+  changeNum?: string;
+
+  @property({type: Number})
+  _patchNum?: PatchSetNum;
+
+  @property({type: String})
+  _robotId?: RobotId;
+
+  @property({type: Object})
+  _currentFix?: FixSuggestionInfo;
+
+  @property({type: Array})
+  _currentPreviews: unknown = (() => [])();
+
+  @property({type: Array})
+  _fixSuggestions?: FixSuggestionInfo[];
+
+  @property({type: Boolean})
+  _isApplyFixLoading = false;
+
+  @property({type: Number})
+  _selectedFixIdx = 0;
+
+  @property({
+    type: Boolean,
+    computed:
+      '_computeDisableApplyFixButton(_isApplyFixLoading, change, ' +
+      '_patchNum)',
+  })
+  _disableApplyFixButton?: boolean;
+
+  private refitOverlay?: () => void;
   /**
    * Given robot comment CustomEvent objevt, fetch diffs associated
    * with first robot comment suggested fix and open dialog.
    *
-   * @param {*} e CustomEvent to be passed from gr-comment with
+   * @param e CustomEvent to be passed from gr-comment with
    * robot comment detail.
-   * @return {Promise<undefined>} Promise that resolves either when all
+   * @return Promise that resolves either when all
    * preview diffs are fetched or no fix suggestions in custom event detail.
    */
   open(e) {
     this._patchNum = e.detail.patchNum;
     this._fixSuggestions = e.detail.comment.fix_suggestions;
     this._robotId = e.detail.comment.robot_id;
-    if (this._fixSuggestions == null || this._fixSuggestions.length == 0) {
+    if (!this._fixSuggestions || this._fixSuggestions.length == 0) {
       return Promise.resolve();
     }
     this._selectedFixIdx = 0;
     const promises = [];
     promises.push(
-        this._showSelectedFixSuggestion(this._fixSuggestions[0]),
-        this.$.applyFixOverlay.open()
+      this._showSelectedFixSuggestion(this._fixSuggestions[0]),
+      this.$.applyFixOverlay.open()
     );
-    return Promise.all(promises)
-        .then(() => {
-          // ensures gr-overlay repositions overlay in center
-          this.$.applyFixOverlay.dispatchEvent(
-              new CustomEvent('iron-resize', {
-                composed: true, bubbles: true,
-              }));
-        });
+    return Promise.all(promises).then(() => {
+      // ensures gr-overlay repositions overlay in center
+      this.$.applyFixOverlay.dispatchEvent(
+        new CustomEvent('iron-resize', {
+          composed: true,
+          bubbles: true,
+        })
+      );
+    });
   }
 
   attached() {
@@ -102,9 +121,11 @@ class GrApplyFixDialog extends GestureEventListeners(
     this.refitOverlay = () => {
       // re-center the dialog as content changed
       this.$.applyFixOverlay.dispatchEvent(
-          new CustomEvent('iron-resize', {
-            composed: true, bubbles: true,
-          }));
+        new CustomEvent('iron-resize', {
+          composed: true,
+          bubbles: true,
+        })
+      );
     };
     this.addEventListener('diff-context-expanded', this.refitOverlay);
   }
@@ -114,29 +135,29 @@ class GrApplyFixDialog extends GestureEventListeners(
     this.removeEventListener('diff-context-expanded', this.refitOverlay);
   }
 
-  _showSelectedFixSuggestion(fixSuggestion) {
+  _showSelectedFixSuggestion(fixSuggestion: FixSuggestionInfo) {
     this._currentFix = fixSuggestion;
     return this._fetchFixPreview(fixSuggestion.fix_id);
   }
 
   _fetchFixPreview(fixId) {
     return this.$.restAPI
-        .getRobotCommentFixPreview(this.changeNum, this._patchNum, fixId)
-        .then(res => {
-          if (res != null) {
-            this._currentPreviews = Object.keys(res).map(key => {
-              return {filepath: key, preview: res[key]};
-            });
-          }
-        })
-        .catch(err => {
-          this._close();
-          throw err;
-        });
+      .getRobotCommentFixPreview(this.changeNum, this._patchNum, fixId)
+      .then(res => {
+        if (res != null) {
+          this._currentPreviews = Object.keys(res).map(key => {
+            return {filepath: key, preview: res[key]};
+          });
+        }
+      })
+      .catch(err => {
+        this._close();
+        throw err;
+      });
   }
 
-  hasSingleFix(_fixSuggestions) {
-    return (_fixSuggestions || {}).length === 1;
+  hasSingleFix(_fixSuggestions?: FixSuggestionInfo[]) {
+    return (_fixSuggestions || []).length === 1;
   }
 
   overridePartialPrefs(prefs) {
@@ -151,35 +172,39 @@ class GrApplyFixDialog extends GestureEventListeners(
     this._close();
   }
 
-  addOneTo(_selectedFixIdx) {
+  addOneTo(_selectedFixIdx: number) {
     return _selectedFixIdx + 1;
   }
 
   _onPrevFixClick(e) {
     if (e) e.stopPropagation();
-    if (this._selectedFixIdx >= 1 && this._fixSuggestions != null) {
+    if (this._selectedFixIdx >= 1 && this._fixSuggestions) {
       this._selectedFixIdx -= 1;
       return this._showSelectedFixSuggestion(
-          this._fixSuggestions[this._selectedFixIdx]);
+        this._fixSuggestions[this._selectedFixIdx]
+      );
     }
   }
 
   _onNextFixClick(e) {
     if (e) e.stopPropagation();
-    if (this._fixSuggestions &&
-      this._selectedFixIdx < this._fixSuggestions.length) {
+    if (
+      this._fixSuggestions &&
+      this._selectedFixIdx < this._fixSuggestions.length
+    ) {
       this._selectedFixIdx += 1;
       return this._showSelectedFixSuggestion(
-          this._fixSuggestions[this._selectedFixIdx]);
+        this._fixSuggestions[this._selectedFixIdx]
+      );
     }
   }
 
-  _noPrevFix(_selectedFixIdx) {
+  _noPrevFix(_selectedFixIdx: number) {
     return _selectedFixIdx === 0;
   }
 
-  _noNextFix(_selectedFixIdx, fixSuggestions) {
-    if (fixSuggestions == null) return true;
+  _noNextFix(_selectedFixIdx: number, fixSuggestions?: FixSuggestionInfo[]) {
+    if (!fixSuggestions) return true;
     return _selectedFixIdx === fixSuggestions.length - 1;
   }
 
@@ -188,31 +213,40 @@ class GrApplyFixDialog extends GestureEventListeners(
     this._currentPreviews = [];
     this._isApplyFixLoading = false;
 
-    this.dispatchEvent(new CustomEvent('close-fix-preview', {
-      bubbles: true,
-      composed: true,
-    }));
+    this.dispatchEvent(
+      new CustomEvent('close-fix-preview', {
+        bubbles: true,
+        composed: true,
+      })
+    );
     this.$.applyFixOverlay.close();
   }
 
-  _getApplyFixButtonLabel(isLoading) {
+  _getApplyFixButtonLabel(isLoading: boolean) {
     return isLoading ? 'Saving...' : 'Apply Fix';
   }
 
-  _computeTooltip(change, patchNum) {
-    if (!change || patchNum == undefined) return '';
+  _computeTooltip(change?: ParsedChangeInfo, patchNum?: PatchSetNum) {
+    if (!change || patchNum === undefined) return '';
     // If change is defined, change.revisions and change.current_revisions
     // must be defined
-    const latestPatchNum = change.revisions[change.current_revision]._number;
-    return latestPatchNum !== patchNum ?
-      'Fix can only be applied to the latest patchset' : '';
+    const latestPatchNum = change.revisions![change.current_revision!]._number;
+    return latestPatchNum !== patchNum
+      ? 'Fix can only be applied to the latest patchset'
+      : '';
   }
 
-  _computeDisableApplyFixButton(isApplyFixLoading, change, patchNum) {
-    if (!change || isApplyFixLoading == undefined || patchNum == undefined) {
+  _computeDisableApplyFixButton(
+    isApplyFixLoading?: boolean,
+    change?: ParsedChangeInfo,
+    patchNum?: PatchSetNum
+  ) {
+    if (!change || isApplyFixLoading === undefined || patchNum === undefined) {
       return true;
     }
-    const currentPatchNum = change.revisions[change.current_revision]._number;
+    // If change is defined, change.revisions and change.current_revisions
+    // must be defined
+    const currentPatchNum = change.revisions![change.current_revision!]._number;
     if (patchNum !== currentPatchNum) {
       return true;
     }
@@ -223,27 +257,34 @@ class GrApplyFixDialog extends GestureEventListeners(
     if (e) {
       e.stopPropagation();
     }
-    if (this._currentFix == null || this._currentFix.fix_id == null) {
+    if (!this._currentFix || !this._currentFix.fix_id) {
       return;
     }
     this._isApplyFixLoading = true;
     return this.$.restAPI
-        .applyFixSuggestion(
-            this.changeNum, this._patchNum, this._currentFix.fix_id
-        )
-        .then(res => {
-          if (res && res.ok) {
-            GerritNav.navigateToChange(this.change, 'edit', this._patchNum);
-            this._close();
-          }
-          this._isApplyFixLoading = false;
-        });
+      .applyFixSuggestion(
+        this.changeNum,
+        this._patchNum,
+        this._currentFix.fix_id
+      )
+      .then(res => {
+        if (res && res.ok) {
+          GerritNav.navigateToChange(this.change, 'edit', this._patchNum);
+          this._close();
+        }
+        this._isApplyFixLoading = false;
+      });
   }
 
   getFixDescription(currentFix) {
-    return currentFix != null && currentFix.description ?
-      currentFix.description : '';
+    return currentFix && currentFix.description
+      ? currentFix.description
+      : '';
   }
 }
 
-customElements.define(GrApplyFixDialog.is, GrApplyFixDialog);
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-apply-fix-dialog': GrApplyFixDialog;
+  }
+}

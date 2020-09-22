28e8a3b4bcea50d4d40470c04099880ac1c3ceb7
Ben Rohlfs <brohlfs@google.com>
2020-09-22 08:12:45 +0200

    Convert gr-thread-list to typescript
    The change converts the following files to typescript:
    
    * elements/change/gr-thread-list/gr-thread-list.ts
    
    Change-Id: Ia03732aa88895225e6a4caacc46658f25a7a0d6c

--- polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list.ts
+++ polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list.ts
@@ -14,76 +14,77 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '@polymer/paper-toggle-button/paper-toggle-button.js';
-import '../../../styles/shared-styles.js';
-import '../../shared/gr-comment-thread/gr-comment-thread.js';
-import {flush} from '@polymer/polymer/lib/legacy/polymer.dom.js';
-import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
-import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin.js';
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {htmlTemplate} from './gr-thread-list_html.js';
-import {parseDate} from '../../../utils/date-util.js';
+import '@polymer/paper-toggle-button/paper-toggle-button';
+import '../../../styles/shared-styles';
+import '../../shared/gr-comment-thread/gr-comment-thread';
+import {flush} from '@polymer/polymer/lib/legacy/polymer.dom';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners';
+import {LegacyElementMixin} from '@polymer/polymer/lib/legacy/legacy-element-mixin';
+import {PolymerElement} from '@polymer/polymer/polymer-element';
+import {htmlTemplate} from './gr-thread-list_html';
+import {parseDate} from '../../../utils/date-util';
 
-import {NO_THREADS_MSG} from '../../../constants/messages.js';
-import {SpecialFilePath} from '../../../constants/constants.js';
+import {NO_THREADS_MSG} from '../../../constants/messages';
+import {CommentSide, SpecialFilePath} from '../../../constants/constants';
+import {customElement, property, observe} from '@polymer/decorators';
+import {CommentThread} from '../../diff/gr-comment-api/gr-comment-api';
+import {Comment, RobotComment} from '../../shared/gr-comment/gr-comment';
+import {PolymerSpliceChange} from '@polymer/polymer/interfaces';
+import {ChangeInfo} from '../../../types/common';
 
-/**
- * Fired when a comment is saved or deleted
- *
- * @event thread-list-modified
- * @extends PolymerElement
- */
-class GrThreadList extends GestureEventListeners(
-    LegacyElementMixin(
-        PolymerElement)) {
-  static get template() { return htmlTemplate; }
+interface CommentThreadWithInfo {
+  thread: CommentThread;
+  hasRobotComment: boolean;
+  hasHumanReplyToRobotComment: boolean;
+  unresolved: boolean;
+  isEditing: boolean;
+  hasDraft: boolean;
+  updated?: Date;
+}
 
-  static get is() { return 'gr-thread-list'; }
-
-  static get properties() {
-    return {
-      /** @type {?} */
-      change: Object,
-      threads: Array,
-      changeNum: String,
-      loggedIn: Boolean,
-      _sortedThreads: {
-        type: Array,
-      },
-      _unresolvedOnly: {
-        type: Boolean,
-        value: false,
-      },
-      _draftsOnly: {
-        type: Boolean,
-        value: false,
-      },
-      /* Boolean properties used must default to false if passed as attribute
-      by the parent */
-      onlyShowRobotCommentsWithHumanReply: {
-        type: Boolean,
-        value: false,
-      },
-      hideToggleButtons: {
-        type: Boolean,
-        value: false,
-      },
-      emptyThreadMsg: {
-        type: String,
-        value: NO_THREADS_MSG,
-      },
-    };
+@customElement('gr-thread-list')
+export class GrThreadList extends GestureEventListeners(
+  LegacyElementMixin(PolymerElement)
+) {
+  static get template() {
+    return htmlTemplate;
   }
 
-  static get observers() {
-    return ['_updateSortedThreads(threads, threads.splices)'];
-  }
+  @property({type: Object})
+  change?: ChangeInfo;
 
-  _computeShowDraftToggle(loggedIn) {
+  @property({type: Array})
+  threads: CommentThread[] = [];
+
+  @property({type: String})
+  changeNum?: string;
+
+  @property({type: Boolean})
+  loggedIn?: boolean;
+
+  @property({type: Array})
+  _sortedThreads: CommentThread[] = [];
+
+  @property({type: Boolean})
+  _unresolvedOnly = false;
+
+  @property({type: Boolean})
+  _draftsOnly = false;
+
+  @property({type: Boolean})
+  onlyShowRobotCommentsWithHumanReply = false;
+
+  @property({type: Boolean})
+  hideToggleButtons = false;
+
+  @property({type: String})
+  emptyThreadMsg = NO_THREADS_MSG;
+
+  _computeShowDraftToggle(loggedIn?: boolean) {
     return loggedIn ? 'show' : '';
   }
 
-  _compareThreads(c1, c2) {
+  _compareThreads(c1: CommentThreadWithInfo, c2: CommentThreadWithInfo) {
     if (c1.thread.path !== c2.thread.path) {
       // '/PATCHSET' will not come before '/COMMIT' when sorting
       // alphabetically so move it to the front explicitly
@@ -106,122 +107,171 @@ class GrThreadList extends GestureEventListeners(
     }
 
     if (c1.thread.patchNum !== c2.thread.patchNum) {
+      if (!c1.thread.patchNum) return 1;
+      if (!c2.thread.patchNum) return -1;
+      // TODO(TS): Explicit comparison for 'edit' and 'PARENT' missing?
       return c1.thread.patchNum > c2.thread.patchNum ? -1 : 1;
     }
 
     if (c2.unresolved !== c1.unresolved) {
-      if (!c1.unresolved) { return 1; }
-      if (!c2.unresolved) { return -1; }
+      if (!c1.unresolved) return 1;
+      if (!c2.unresolved) return -1;
     }
 
     if (c2.hasDraft !== c1.hasDraft) {
-      if (!c1.hasDraft) { return 1; }
-      if (!c2.hasDraft) { return -1; }
+      if (!c1.hasDraft) return 1;
+      if (!c2.hasDraft) return -1;
     }
 
-    const c1Date = c1.__date || parseDate(c1.updated);
-    const c2Date = c2.__date || parseDate(c2.updated);
-    const dateCompare = c2Date - c1Date;
-    if (dateCompare === 0 && (!c1.id || !c1.id.localeCompare)) {
-      return 0;
+    if (c2.updated !== c1.updated) {
+      if (!c1.updated) return 1;
+      if (!c2.updated) return -1;
+      return c2.updated.getTime() - c1.updated.getTime();
     }
-    return dateCompare ? dateCompare : c1.id.localeCompare(c2.id);
+
+    if (c2.thread.rootId !== c1.thread.rootId) {
+      if (!c1.thread.rootId) return 1;
+      if (!c2.thread.rootId) return -1;
+      return c1.thread.rootId.localeCompare(c2.thread.rootId);
+    }
+
+    return 0;
   }
 
   /**
    * Observer on threads and update _sortedThreads when needed.
    * Order as follows:
-   *  - Patchset level threads (descending based on patchset number)
-   *    - unresolved
-          - comments with drafts
-          - comments without drafts
-   *    - resolved
-          - comments with drafts
-          - comments without drafts
-   *  - File name
-   *    - Line number
-   *      - Unresolved (descending based on patchset number)
-   *        - comments with drafts
-   *        - comments without drafts
-   *      - Resolved (descending based on patchset number)
-   *        - comments with drafts
-   *        - comments without drafts
+   * - Patchset level threads (descending based on patchset number)
+   * - unresolved
+   * - comments with drafts
+   * - comments without drafts
+   * - resolved
+   * - comments with drafts
+   * - comments without drafts
+   * - File name
+   * - Line number
+   * - Unresolved (descending based on patchset number)
+   * - comments with drafts
+   * - comments without drafts
+   * - Resolved (descending based on patchset number)
+   * - comments with drafts
+   * - comments without drafts
    *
-   * @param {Array<Object>} threads
-   * @param {!Object} spliceRecord
+   * @param threads
+   * @param spliceRecord
    */
-  _updateSortedThreads(threads, spliceRecord) {
-    if (!threads) {
+  @observe('threads', 'threads.splices')
+  _updateSortedThreads(
+    threads: CommentThread[],
+    _: PolymerSpliceChange<CommentThread[]>
+  ) {
+    if (!threads || threads.length === 0) {
       this._sortedThreads = [];
       return;
     }
     // We only want to sort on thread additions / removals to avoid
-    // re-rendering on modifications (add new reply / edit draft etc)
-    //  https://polymer-library.polymer-project.org/3.0/docs/devguide/observers#array-observation
-    const isArrayMutation = spliceRecord &&
-      (spliceRecord.indexSplices.addedCount !== 0
-        || spliceRecord.indexSplices.removed.length);
-
-    if (this._sortedThreads
-        && this._sortedThreads.length === threads.length
-        && !isArrayMutation) {
+    // re-rendering on modifications (add new reply / edit draft etc.).
+    // https://polymer-library.polymer-project.org/3.0/docs/devguide/observers#array-observation
+    // TODO(TS): We have removed a buggy check of the splices here. A splice
+    // with addedCount > 0 or removed.length > 0 should also cause re-sorting
+    // and re-rendering, but apparently spliceRecord is always undefined for
+    // whatever reason.
+    if (this._sortedThreads.length === threads.length) {
       // Instead of replacing the _sortedThreads which will trigger a re-render,
-      // we override all threads inside of it
+      // we override all threads inside of it.
 
       for (const thread of threads) {
-        const idxInSortedThreads = this._sortedThreads
-            .findIndex(t => t.rootId === thread.rootId);
+        const idxInSortedThreads = this._sortedThreads.findIndex(
+          t => t.rootId === thread.rootId
+        );
         this.set(`_sortedThreads.${idxInSortedThreads}`, {...thread});
       }
       return;
     }
 
-    const threadsWithInfo = threads
-        .map(thread => this._getThreadWithStatusInfo(thread));
-    this._sortedThreads = threadsWithInfo.sort((t1, t2) =>
-      this._compareThreads(t1, t2)).map(threadInfo => threadInfo.thread);
+    const threadsWithInfo = threads.map(thread =>
+      this._getThreadWithStatusInfo(thread)
+    );
+    this._sortedThreads = threadsWithInfo
+      .sort((t1, t2) => this._compareThreads(t1, t2))
+      .map(threadInfo => threadInfo.thread);
   }
 
-  _isFirstThreadWithFileName(sortedThreads, thread, unresolvedOnly, draftsOnly,
-      onlyShowRobotCommentsWithHumanReply) {
-    const threads = sortedThreads.filter(t => this._shouldShowThread(
-        t, unresolvedOnly, draftsOnly,
-        onlyShowRobotCommentsWithHumanReply));
+  _isFirstThreadWithFileName(
+    sortedThreads: CommentThread[],
+    thread: CommentThread,
+    unresolvedOnly?: boolean,
+    draftsOnly?: boolean,
+    onlyShowRobotCommentsWithHumanReply?: boolean
+  ) {
+    const threads = sortedThreads.filter(t =>
+      this._shouldShowThread(
+        t,
+        unresolvedOnly,
+        draftsOnly,
+        onlyShowRobotCommentsWithHumanReply
+      )
+    );
     const index = threads.findIndex(t => t.rootId === thread.rootId);
     if (index === -1) {
       return false;
     }
-    return index === 0 || (threads[index - 1].path !== threads[index].path);
+    return index === 0 || threads[index - 1].path !== threads[index].path;
   }
 
-  _shouldRenderSeparator(sortedThreads, thread, unresolvedOnly, draftsOnly,
-      onlyShowRobotCommentsWithHumanReply) {
-    const threads = sortedThreads.filter(t => this._shouldShowThread(
-        t, unresolvedOnly, draftsOnly,
-        onlyShowRobotCommentsWithHumanReply));
+  _shouldRenderSeparator(
+    sortedThreads: CommentThread[],
+    thread: CommentThread,
+    unresolvedOnly?: boolean,
+    draftsOnly?: boolean,
+    onlyShowRobotCommentsWithHumanReply?: boolean
+  ) {
+    const threads = sortedThreads.filter(t =>
+      this._shouldShowThread(
+        t,
+        unresolvedOnly,
+        draftsOnly,
+        onlyShowRobotCommentsWithHumanReply
+      )
+    );
     const index = threads.findIndex(t => t.rootId === thread.rootId);
     if (index === -1) {
       return false;
     }
-    return index > 0 && this._isFirstThreadWithFileName(sortedThreads,
-        thread, unresolvedOnly, draftsOnly,
-        onlyShowRobotCommentsWithHumanReply);
+    return (
+      index > 0 &&
+      this._isFirstThreadWithFileName(
+        sortedThreads,
+        thread,
+        unresolvedOnly,
+        draftsOnly,
+        onlyShowRobotCommentsWithHumanReply
+      )
+    );
   }
 
-  _shouldShowThread(thread, unresolvedOnly, draftsOnly,
-      onlyShowRobotCommentsWithHumanReply) {
-    if ([
-      thread,
-      unresolvedOnly,
-      draftsOnly,
-      onlyShowRobotCommentsWithHumanReply,
-    ].includes(undefined)) {
+  _shouldShowThread(
+    thread: CommentThread,
+    unresolvedOnly?: boolean,
+    draftsOnly?: boolean,
+    onlyShowRobotCommentsWithHumanReply?: boolean
+  ) {
+    if (
+      [
+        thread,
+        unresolvedOnly,
+        draftsOnly,
+        onlyShowRobotCommentsWithHumanReply,
+      ].includes(undefined)
+    ) {
       return false;
     }
 
-    if (!draftsOnly
-        && !unresolvedOnly
-        && !onlyShowRobotCommentsWithHumanReply) {
+    if (
+      !draftsOnly &&
+      !unresolvedOnly &&
+      !onlyShowRobotCommentsWithHumanReply
+    ) {
       return true;
     }
 
@@ -231,9 +281,11 @@ class GrThreadList extends GestureEventListeners(
       return true;
     }
 
-    if (threadInfo.hasRobotComment
-       && onlyShowRobotCommentsWithHumanReply
-       && !threadInfo.hasHumanReplyToRobotComment) {
+    if (
+      threadInfo.hasRobotComment &&
+      onlyShowRobotCommentsWithHumanReply &&
+      !threadInfo.hasHumanReplyToRobotComment
+    ) {
       return false;
     }
 
@@ -249,13 +301,13 @@ class GrThreadList extends GestureEventListeners(
     return filtersCheck;
   }
 
-  _getThreadWithStatusInfo(thread) {
+  _getThreadWithStatusInfo(thread: CommentThread): CommentThreadWithInfo {
     const comments = thread.comments;
-    const lastComment = comments[comments.length - 1] || {};
+    const lastComment = (comments[comments.length - 1] || {}) as Comment;
     let hasRobotComment = false;
     let hasHumanReplyToRobotComment = false;
     comments.forEach(comment => {
-      if (comment.robot_id) {
+      if ((comment as RobotComment).robot_id) {
         hasRobotComment = true;
       } else if (hasRobotComment) {
         hasHumanReplyToRobotComment = true;
@@ -269,11 +321,13 @@ class GrThreadList extends GestureEventListeners(
       unresolved: !!lastComment.unresolved,
       isEditing: !!lastComment.__editing,
       hasDraft: !!lastComment.__draft,
-      updated: lastComment.updated || lastComment.__date,
+      updated: lastComment.updated
+        ? parseDate(lastComment.updated)
+        : lastComment.__date,
     };
   }
 
-  removeThread(rootId) {
+  removeThread(rootId: string) {
     for (let i = 0; i < this.threads.length; i++) {
       if (this.threads[i].rootId === rootId) {
         this.splice('threads', i, 1);
@@ -284,25 +338,34 @@ class GrThreadList extends GestureEventListeners(
     }
   }
 
-  _handleThreadDiscard(e) {
+  _handleThreadDiscard(e: CustomEvent) {
     this.removeThread(e.detail.rootId);
   }
 
-  _handleCommentsChanged(e) {
-    this.dispatchEvent(new CustomEvent('thread-list-modified',
-        {detail: {rootId: e.detail.rootId, path: e.detail.path}}));
+  _handleCommentsChanged(e: CustomEvent) {
+    this.dispatchEvent(
+      new CustomEvent('thread-list-modified', {
+        detail: {rootId: e.detail.rootId, path: e.detail.path},
+      })
+    );
   }
 
-  _isOnParent(side) {
+  _isOnParent(side?: CommentSide) {
+    // TODO(TS): That looks like a bug? CommentSide.REVISION will also be
+    // classified as parent??
     return !!side;
   }
 
   /**
    * Work around a issue on iOS when clicking turns into double tap
    */
-  _onTapUnresolvedToggle(e) {
+  _onTapUnresolvedToggle(e: Event) {
     e.preventDefault();
   }
 }
 
-customElements.define(GrThreadList.is, GrThreadList);
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-thread-list': GrThreadList;
+  }
+}

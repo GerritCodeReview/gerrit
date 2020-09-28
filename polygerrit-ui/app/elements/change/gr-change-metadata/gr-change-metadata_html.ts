7c35546dcd887f66351894834bfa2c6576789ce5
Dhruv Srivastava <dhruvsri@google.com>
2020-09-28 10:31:35 +0200

    Add limit to hashtag chip similar to repo and branch
    Having the same character limit for branch allows the width of
    metadata component to be consistent and not allow hashtag to take
    up majority of the page width
    
    Change-Id: Ic11033271ebecbca33f900d606406b985f40cb57

--- polygerrit-ui/app/elements/change/gr-change-metadata/gr-change-metadata_html.ts
+++ polygerrit-ui/app/elements/change/gr-change-metadata/gr-change-metadata_html.ts
@@ -321,6 +321,7 @@ export const htmlTemplate = html`
             href="[[_computeHashtagUrl(item)]]"
             removable="[[!_hashtagReadOnly]]"
             on-remove="_handleHashtagRemoved"
+            limit="40"
           >
           </gr-linked-chip>
         </template>

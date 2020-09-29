299dc6fb69ec2ab2880cd80fc6b4876824ff08cc
Hermann Loose <hermannloose@google.com>
2020-09-29 10:38:03 +0200

    WIP on above / below buttons
    Change-Id: I356c7af7b1da8883986dcb99b1e0295ea9f74220

--- polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-side-by-side.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-side-by-side.ts
@@ -15,19 +15,20 @@
  * limitations under the License.
  */
 
-import {GrDiffBuilder} from './gr-diff-builder';
-import {GrDiffGroup, GrDiffGroupType} from '../gr-diff/gr-diff-group';
-import {DiffInfo, DiffPreferencesInfo} from '../../../types/common';
-import {GrDiffLine, LineNumber} from '../gr-diff/gr-diff-line';
 import {Side} from '../../../constants/constants';
+import {DiffInfo, DiffPreferencesInfo} from '../../../types/common';
+import {GrDiffGroup, GrDiffGroupType} from '../gr-diff/gr-diff-group';
+import {GrDiffLine, LineNumber} from '../gr-diff/gr-diff-line';
+
+import {GrDiffBuilder} from './gr-diff-builder';
 
 export class GrDiffBuilderSideBySide extends GrDiffBuilder {
   constructor(
-    diff: DiffInfo,
-    prefs: DiffPreferencesInfo,
-    outputEl: HTMLElement,
-    // TODO(TS): Replace any by a layer interface.
-    readonly layers: any[] = []
+      diff: DiffInfo,
+      prefs: DiffPreferencesInfo,
+      outputEl: HTMLElement,
+      // TODO(TS): Replace any by a layer interface.
+      readonly layers: any[] = []
   ) {
     super(diff, prefs, outputEl, layers);
   }
@@ -48,9 +49,11 @@ export class GrDiffBuilderSideBySide extends GrDiffBuilder {
       sectionEl.classList.add('ignoredWhitespaceOnly');
     }
     if (group.type === GrDiffGroupType.CONTEXT_CONTROL) {
-      sectionEl.appendChild(
-        this._createContextRow(sectionEl, group.contextGroups)
-      );
+      const contextControlRows =
+          this._createContextRow(sectionEl, group.contextGroups);
+      for (const contextControlRow of contextControlRows) {
+        sectionEl.appendChild(contextControlRow);
+      }
       return sectionEl;
     }
 
@@ -103,37 +106,71 @@ export class GrDiffBuilderSideBySide extends GrDiffBuilder {
   }
 
   _appendPair(
-    row: HTMLElement,
-    line: GrDiffLine,
-    lineNumber: LineNumber,
-    side: Side
-  ) {
+      row: HTMLElement, line: GrDiffLine, lineNumber: LineNumber, side: Side) {
     const lineNumberEl = this._createLineEl(line, lineNumber, line.type, side);
     row.appendChild(lineNumberEl);
     row.appendChild(this._createTextEl(lineNumberEl, line, side));
   }
 
-  _createContextRow(section: HTMLElement, contextGroups: GrDiffGroup[]) {
-    const row = this._createElement('tr');
-    row.classList.add('diff-row', 'side-by-side');
-    row.setAttribute('left-type', GrDiffGroupType.CONTEXT_CONTROL);
-    row.setAttribute('right-type', GrDiffGroupType.CONTEXT_CONTROL);
-    row.tabIndex = -1;
+  _createContextRows(section: HTMLElement, contextGroups: GrDiffGroup[]) {
+    const {elements, hasAbove, hasBelow} =
+        this._createContextControls(section, contextGroups);
 
-    row.appendChild(this._createBlameCell(0));
-    row.appendChild(this._createElement('td', 'contextLineNum'));
-    row.appendChild(this._createContextControl(section, contextGroups));
-    row.appendChild(this._createElement('td', 'contextLineNum'));
-    row.appendChild(this._createContextControl(section, contextGroups));
-    return row;
+    const rows = [];
+
+    if (hasAbove) {
+      const rowAbove = this._createContextControlBackgroundRow();
+      rowAbove.classList.add('above');
+      rows.push(rowAbove);
+    }
+
+    const rowDivider = this._createElement('tr', 'contextDivider');
+    rowDivider.classList.add('diff-row', 'side-by-side');
+    rowDivider.setAttribute('left-type', GrDiffGroupType.CONTEXT_CONTROL);
+    rowDivider.setAttribute('right-type', GrDiffGroupType.CONTEXT_CONTROL);
+    rowDivider.tabIndex = -1;
+    const dividerCell = this._createElement('td', 'dividerCell');
+    rowDivider.appendChild(dividerCell);
+
+    for (const element of elements) {
+      dividerCell.appendChild(element);
+    }
+    if (!(hasAbove && hasBelow)) {
+      rowDivider.classList.add('collapsed');
+    }
+
+    rows.push(rowDivider);
+
+    if (hasBelow) {
+      const rowBelow = this._createContextControlBackgroundRow();
+      rowBelow.classList.add('below');
+      rows.push(rowBelow);
+    }
+
+    return rows;
   }
 
-  _getNextContentOnSide(content: HTMLElement, side: Side): HTMLElement | null {
+  _createContextControlBackgroundRow() {
+      const row = this._createElement('tr', 'contextBackground');
+      row.classList.add('diff-row', 'side-by-side');
+      row.setAttribute('left-type', GrDiffGroupType.CONTEXT_CONTROL);
+      row.setAttribute('right-type', GrDiffGroupType.CONTEXT_CONTROL);
+      row.tabIndex = -1;
+
+      row.appendChild(this._createBlameCell(0));
+      row.appendChild(this._createElement('td', 'contextLineNum'));
+      row.appendChild(this._createElement('td'));
+      row.appendChild(this._createElement('td', 'contextLineNum'));
+      row.appendChild(this._createElement('td'));
+
+      return row;
+  }
+
+  _getNextContentOnSide(content: HTMLElement, side: Side): HTMLElement|null {
     let tr: HTMLElement = content.parentElement!.parentElement!;
     while ((tr = tr.nextSibling as HTMLElement)) {
-      const nextContent = tr.querySelector(
-        'td.content .contentText[data-side="' + side + '"]'
-      );
+      const nextContent =
+          tr.querySelector('td.content .contentText[data-side="' + side + '"]');
       if (nextContent) return nextContent as HTMLElement;
     }
     return null;

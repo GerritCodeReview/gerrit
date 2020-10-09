b303430f4eb78725fa36c70218d2d85a11c5b3ae
Ole <oler@google.com>
2020-10-09 17:09:01 +0200

    Refactor reload 2: Await render promise
    This function is very hard to follow. Making small steps to ensure
    careful review.
    
    Use await to move this code block left.
    
    Change-Id: Ifbb29be1936c77100624dc7bdc67b610c1ba32ee

--- polygerrit-ui/app/elements/diff/gr-diff-host/gr-diff-host.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-host/gr-diff-host.ts
@@ -375,37 +375,36 @@ export class GrDiffHost extends GestureEventListeners(
     // Not waiting for coverage ranges intentionally as
     // plugin loading should not block the content rendering
     return Promise.all([diffRequest, assetRequest])
-      .then(results => {
+      .then(async results => {
         const diff = results[0];
         if (!diff) {
           return Promise.resolve();
         }
         this.filesWeblinks = this._getFilesWeblinks(diff);
         this.diff = diff;
-        return new Promise<CustomEvent>(resolve => {
+        const event = await new Promise<CustomEvent>(resolve => {
           const callback = (event: CustomEvent) => {
             this.removeEventListener('render', callback);
             resolve(event);
           };
           this.addEventListener('render', callback);
-        }).then((event: CustomEvent) => {
-          const needsSyntaxHighlighting =
-            event.detail && event.detail.contentRendered;
-          if (needsSyntaxHighlighting) {
-            this.reporting.time(TimingLabel.SYNTAX);
-            this.$.syntaxLayer.process().finally(() => {
-              this.reporting.timeEnd(TimingLabel.SYNTAX);
-              this.reporting.timeEnd(TimingLabel.TOTAL);
-            });
-          } else {
-            this.reporting.timeEnd(TimingLabel.TOTAL);
-          }
-          if (shouldReportMetric) {
-            // We report diffViewContentDisplayed only on reload caused
-            // by params changed - expected only on Diff Page.
-            this.reporting.diffViewContentDisplayed();
-          }
         });
+        const needsSyntaxHighlighting =
+          event.detail && event.detail.contentRendered;
+        if (needsSyntaxHighlighting) {
+          this.reporting.time(TimingLabel.SYNTAX);
+          this.$.syntaxLayer.process().finally(() => {
+            this.reporting.timeEnd(TimingLabel.SYNTAX);
+            this.reporting.timeEnd(TimingLabel.TOTAL);
+          });
+        } else {
+          this.reporting.timeEnd(TimingLabel.TOTAL);
+        }
+        if (shouldReportMetric) {
+          // We report diffViewContentDisplayed only on reload caused
+          // by params changed - expected only on Diff Page.
+          this.reporting.diffViewContentDisplayed();
+        }
       })
       .catch(err => {
         console.warn('Error encountered loading diff:', err);

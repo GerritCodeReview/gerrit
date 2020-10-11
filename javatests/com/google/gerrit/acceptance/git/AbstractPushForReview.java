2913d65fd9b061ee4945086890b32db7d8bd292d
Matthias Sohn <matthias.sohn@sap.com>
2020-10-11 21:14:35 +0200

    AbstractPushForReview: remove unused local variable c
    Change-Id: Ic7afc514716891af3ad69f859dbf0f9acca8364b

--- javatests/com/google/gerrit/acceptance/git/AbstractPushForReview.java
+++ javatests/com/google/gerrit/acceptance/git/AbstractPushForReview.java
@@ -1563,7 +1563,7 @@ public abstract class AbstractPushForReview extends AbstractDaemonTest {
     if (!url.endsWith("/")) {
       url += "/";
     }
-    RevCommit c = createCommit(testRepo, "test commit\n\nLink: " + url + "id/" + changeId);
+    createCommit(testRepo, "test commit\n\nLink: " + url + "id/" + changeId);
     pushForReviewOk(testRepo);
 
     List<ChangeMessageInfo> messages = getMessages(changeId);
@@ -1573,7 +1573,7 @@ public abstract class AbstractPushForReview extends AbstractDaemonTest {
   @Test
   public void pushWithWrongHostLinkFooter() throws Exception {
     String changeId = "I0123456789abcdef0123456789abcdef01234567";
-    RevCommit c = createCommit(testRepo, "test commit\n\nLink: https://wronghost/id/" + changeId);
+    createCommit(testRepo, "test commit\n\nLink: https://wronghost/id/" + changeId);
     pushForReviewRejected(testRepo, "missing Change-Id in message footer");
   }
 

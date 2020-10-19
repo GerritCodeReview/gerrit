69645d773c8df441946773f745774202c2e0c17a
Gal Paikin <paiking@google.com>
2020-10-19 19:26:13 +0200

    Use overridden ChangeEmail#add instead of OutgoingEmail#add
    There is a bug which assumes the callers always use ChangeEmail's
    method, and not the parent class OutgoingEmail. It's bad that we allow
    overriding one "add" method without overidding the others, so that
    should be removed.
    
    Some more context in the follow-up that fixes this problem.
    
    Change-Id: I8ae0a691ee9738b7875f4e27eff9ee24f6055a7d

--- java/com/google/gerrit/server/mail/send/ReplacePatchSetSender.java
+++ java/com/google/gerrit/server/mail/send/ReplacePatchSetSender.java
@@ -65,7 +65,8 @@ public class ReplacePatchSetSender extends ReplyToChangeSender {
       if (notify.handling() == NotifyHandling.ALL
           || notify.handling() == NotifyHandling.OWNER_REVIEWERS) {
         add(RecipientType.TO, reviewers);
-        add(RecipientType.CC, extraCC);
+        reviewers.stream().forEach(r -> add(RecipientType.TO, r));
+        extraCC.stream().forEach(cc -> add(RecipientType.CC, cc));
       }
       rcptToAuthors(RecipientType.CC);
     }

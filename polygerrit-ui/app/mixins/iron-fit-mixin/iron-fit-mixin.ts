234dbba31631b0ca959359d3d1d5bb9eb19cffe6
Dmitrii Filippov <dmfilippov@google.com>
2020-10-19 19:32:13 +0200

    Update mixins to avoid compilation issues
    The following 2 issues appears internally:
    https://github.com/microsoft/TypeScript/issues/15870
    https://github.com/microsoft/TypeScript/issues/9944
    
    Change-Id: I60b669846447ff7f54e7b0f741402333c2b59653

--- polygerrit-ui/app/mixins/iron-fit-mixin/iron-fit-mixin.ts
+++ polygerrit-ui/app/mixins/iron-fit-mixin/iron-fit-mixin.ts
@@ -22,8 +22,19 @@ import {Constructor} from '../../utils/common-util';
 
 // The mixinBehaviors clears all type information about superClass.
 // As a workaround, we define IronFitMixin with correct type.
+// Due to the following issues:
+// https://github.com/microsoft/TypeScript/issues/15870
+// https://github.com/microsoft/TypeScript/issues/9944
+// we have to import IronFitBehavior in the same file where IronFitMixin
+// is used. To ensure that this import can't be avoided, the second parameter
+// is added. Usage example:
+// class Element extends IronFitMixin(PolymerElement, IronFitBehavior as IronFitBehavior)
+// The code 'IronFitBehavior as IronFitBehavior' required, becuase IronFitBehavior
+// defined as an object, not as IronFitBehavior instance.
+
 export const IronFitMixin = <T extends Constructor<PolymerElement>>(
-  superClass: T
+  superClass: T,
+  _: IronFitBehavior
 ): T & Constructor<IronFitBehavior> =>
   // TODO(TS): mixinBehaviors in some lib is returning: `new () => T` instead
   // which will fail the type check due to missing IronFitBehavior interface
--- polygerrit-ui/app/mixins/iron-overlay-mixin/iron-overlay-mixin.ts
+++ polygerrit-ui/app/mixins/iron-overlay-mixin/iron-overlay-mixin.ts
@@ -22,9 +22,20 @@ import {Constructor} from '../../utils/common-util';
 
 // The mixinBehaviors clears all type information about superClass.
 // As a workaround, we define IronOverlayMixin with correct type.
+// Due to the following issues:
+// https://github.com/microsoft/TypeScript/issues/15870
+// https://github.com/microsoft/TypeScript/issues/9944
+// we have to import IronOverlayBehavior in the same file where IronOverlayMixin
+// is used. To ensure that this import can't be avoided, the second parameter
+// is added. Usage example:
+// class Element extends IronOverlayMixin(PolymerElement, IronOverlayBehavior as IronOverlayBehavior)
+// The code 'IronOverlayBehavior as IronOverlayBehavior' required, because
+// IronOverlayBehavior defined as an object, not as IronOverlayBehavior instance.
 export const IronOverlayMixin = <T extends Constructor<PolymerElement>>(
-  superClass: T
+  superClass: T,
+  _: IronOverlayBehavior
 ): T & Constructor<IronOverlayBehavior> =>
-  // TODO(TS): mixinBehaviors in some lib is returning: `new () => T` instead
-  // which will fail the type check due to missing IronOverlayBehavior interface
+  // TODO(TS): mixinBehaviors in some lib is returning: `new () => T`
+  // instead which will fail the type check due to missing
+  // IronOverlayBehavior interface
   mixinBehaviors([IronOverlayBehavior], superClass) as any;

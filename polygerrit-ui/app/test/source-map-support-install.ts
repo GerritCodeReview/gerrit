3132228a9525bf47727a54e126ed4adafa0978f7
Dmitrii Filippov <dmfilippov@google.com>
2020-10-21 15:35:41 +0200

    Allow to write and run Typescript tests
    Change-Id: I695901dca367b342705b7d86b001d2b400510112

--- polygerrit-ui/app/test/source-map-support-install.ts
+++ polygerrit-ui/app/test/source-map-support-install.ts
@@ -15,6 +15,19 @@
  * limitations under the License.
  */
 
+// Mark the file as a module. Otherwise typescript assumes this is a script
+// and doesn't allow "declare global".
+// See: https://www.typescriptlang.org/docs/handbook/modules.html
+export {};
+
+declare global {
+  interface Window {
+    sourceMapSupport: {
+      install(): void;
+    };
+  }
+}
+
 // The karma.conf.js file loads required module before any other modules
 // The source-map-support.js can't be imported with import ... statement
 window.sourceMapSupport.install();

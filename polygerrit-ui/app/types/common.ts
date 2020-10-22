477000ede4050cab4b6336b9227353ec86bccdbf
Dmitrii Filippov <dmfilippov@google.com>
2020-10-22 16:57:57 +0200

    Add test-data-generators and convert several test files to TS
    Change-Id: Icbcd6669d5de179f3c45c9d3969c6da40f00d11a

--- polygerrit-ui/app/types/common.ts
+++ polygerrit-ui/app/types/common.ts
@@ -1373,7 +1373,7 @@ export interface InheritedBooleanInfo {
 }
 
 /**
- * The MaxObjectSizeLimitInfo entity contains information about themax object
+ * The MaxObjectSizeLimitInfo entity contains information about the max object
  * size limit of a project.
  * https://gerrit-review.googlesource.com/Documentation/rest-api-projects.html#max-object-size-limit-info
  */

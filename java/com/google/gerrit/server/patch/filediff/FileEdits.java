429926afc71cf3de27497e952d891d4955d55c4f
Youssef Elghareeb <ghareeb@google.com>
2020-10-23 13:19:43 +0200

    Move some of the old diff cache entities to a new package
    In this change, we create a new package
    com.google.gerrit.server.patch.filediff and moved some of the old diff
    cache entities to it.
    These entities (mainly the EditTransformer) will be used in the next
    follow up change by the "File Diff" cache, which will be added to the
    same package. The other moved entities (Edit, FileEdtis,
    PatchListLoader) are also moved to the new package because they are used
    by EditTransformer. Some access modifiers were temporarily changed from
    package private to public because they depend on classes in
    com.google.gerrit.server.patch.
    
    Note that the PatchListEntry.java and PatchListLoader are
    implementations of the old diff cache and should be deprecated after the
    redesign.
    
    Change-Id: I5830d57f6cbabefc4604036b58752836673657a5

--- java/com/google/gerrit/server/patch/filediff/FileEdits.java
+++ java/com/google/gerrit/server/patch/filediff/FileEdits.java
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-package com.google.gerrit.server.patch.entities;
+package com.google.gerrit.server.patch.filediff;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 

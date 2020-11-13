a7437debf6f828f51938d235f81680429da67839
Ole <oler@google.com>
2020-11-13 15:19:03 +0100

    Update stops when a diff starts or stops loading
    Before, we would only update when rendering was done, leaving the old
    and invalid cursor stops around during rendering.
    Using render-start did not work as expected, because in some cases
    (e.g. file too large to render), it would not fire a render-start event.
    
    Change-Id: I97d5bc9e3336cfa9d21d19317c0bcdc39d8f99ff

--- polygerrit-ui/app/elements/diff/gr-diff/gr-diff.ts
+++ polygerrit-ui/app/elements/diff/gr-diff/gr-diff.ts
@@ -192,9 +192,18 @@ export class GrDiff extends GestureEventListeners(
   @property({type: Object})
   lineOfInterest?: LineOfInterest;
 
-  /** True when diff is changed, until the content is done rendering. */
-  @property({type: Boolean})
-  _loading = true;
+  /**
+   * True when diff is changed, until the content is done rendering.
+   *
+   * This is readOnly, meaning one can listen for the loading-changed event, but not
+   * write to it from the outside. Code in this class should use the "private"
+   * _setLoading method.
+   */
+  @property({type: Boolean, notify: true, readOnly: true})
+  loading!: boolean;
+
+  // Polymer generated when setting readOnly above.
+  _setLoading!: (loading: boolean) => void;
 
   @property({type: Boolean})
   loggedIn = false;
@@ -276,6 +285,7 @@ export class GrDiff extends GestureEventListeners(
   /** @override */
   created() {
     super.created();
+    this._setLoading(true);
     this.addEventListener('create-range-comment', (e: Event) =>
       this._handleCreateRangeComment(e as CustomEvent)
     );
@@ -462,7 +472,7 @@ export class GrDiff extends GestureEventListeners(
   getCursorStops(): Array<HTMLElement | AbortStop> {
     if (this.hidden && this.noAutoRender) return [];
 
-    if (this._loading) {
+    if (this.loading) {
       return [new AbortStop()];
     }
 
@@ -832,7 +842,7 @@ export class GrDiff extends GestureEventListeners(
   }
 
   _diffChanged(newValue?: DiffInfo) {
-    this._loading = true;
+    this._setLoading(true);
     this._cleanup();
     if (newValue) {
       this._diffLength = this.getDiffLength(newValue);
@@ -892,7 +902,7 @@ export class GrDiff extends GestureEventListeners(
   }
 
   _handleRenderContent() {
-    this._loading = false;
+    this._setLoading(false);
     this._unobserveIncrementalNodes();
     this._incrementalNodeObserver = (dom(
       this

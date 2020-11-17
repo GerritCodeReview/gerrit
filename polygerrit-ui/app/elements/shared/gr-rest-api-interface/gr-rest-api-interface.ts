899305037dc55d59387aade799c0d57ac33ce4af
Ole <oler@google.com>
2020-11-17 13:59:24 +0100

    Extract gr-diff types into a separate file
    gr-diff can be embedded into other UIs than Gerrit, and such clients
    might have the need to depends on the types in their own TypeScript
    bundle and load the gr-diff bundle at runtime.
    
    Having these types separate also makes it easier to keep track of
    dependencies and avoid API breaking changes without versioning.
    
    This is the first step in this direction - there may be other types that
    we wish to migrate here.
    
    This CL also inlines the documentation of the fields to make it
    available in IDEs.
    
    Change-Id: I61e67673e4eb2ef1d1bcc43869b0cc76ebaad971

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.ts
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-interface.ts
@@ -141,6 +141,7 @@ import {
   TopMenuEntryInfo,
   MergeableInfo,
 } from '../../../types/common';
+import {IgnoreWhitespaceType} from '../../../types/diff';
 import {
   CancelConditionCallback,
   ErrorCallback,
@@ -152,7 +153,6 @@ import {
   CommentSide,
   DiffViewMode,
   HttpMethod,
-  IgnoreWhitespaceType,
   ReviewerState,
 } from '../../../constants/constants';
 
@@ -828,7 +828,7 @@ export class GrRestApiInterface
         context: 10,
         cursor_blink_rate: 0,
         font_size: 12,
-        ignore_whitespace: IgnoreWhitespaceType.IGNORE_NONE,
+        ignore_whitespace: 'IGNORE_NONE',
         intraline_difference: true,
         line_length: 100,
         line_wrapping: false,
@@ -2525,7 +2525,7 @@ export class GrRestApiInterface
     const params: GetDiffParams = {
       context: 'ALL',
       intraline: null,
-      whitespace: whitespace || IgnoreWhitespaceType.IGNORE_NONE,
+      whitespace: whitespace || 'IGNORE_NONE',
     };
     if (isMergeParent(basePatchNum)) {
       params.parent = getParentIndex(basePatchNum);

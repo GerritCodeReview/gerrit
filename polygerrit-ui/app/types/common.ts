af044417fa9162d7098525e99acdc42c9ef87267
Ole <oler@google.com>
2020-11-17 15:03:36 +0100

    Make DiffIntralineInfo type more precise
    Change-Id: Ibab71b986a121eaa79c253b27c0a82ea91494e8c

--- polygerrit-ui/app/types/common.ts
+++ polygerrit-ui/app/types/common.ts
@@ -1241,6 +1241,26 @@ export interface LabelTypeInfo {
 export type LabelTypeInfoValues = {[value: string]: string};
 
 /**
+ * The DiffIntralineInfo entity contains information about intraline edits in a
+ * file.
+ *
+ * The information consists of a list of <skip length, mark length> pairs, where
+ * the skip length is the number of characters between the end of the previous
+ * edit and the start of this edit, and the mark length is the number of edited
+ * characters following the skip. The start of the edits is from the beginning
+ * of the related diff content lines.
+ *
+ * Note that the implied newline character at the end of each line is included
+ * in the length calculation, and thus it is possible for the edits to span
+ * newlines.
+ *
+ * https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#diff-intraline-info
+ */
+export type SkipLength = number;
+export type MarkLength = number;
+export type DiffIntralineInfo = [SkipLength, MarkLength];
+
+/**
  * The DiffContent entity contains information about the content differences in a file.
  * https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#diff-content
  */
@@ -1250,8 +1270,8 @@ export interface DiffContent {
   ab?: string[];
   // The inner array is always of length two. The first entry is the 'skip'
   // length. The second entry is the 'edit' length.
-  edit_a?: number[][];
-  edit_b?: number[][];
+  edit_a?: DiffIntralineInfo[];
+  edit_b?: DiffIntralineInfo[];
   due_to_rebase?: boolean;
   due_to_move?: boolean;
   skip?: number;

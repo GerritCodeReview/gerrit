034e96f21054178582563f95c6a99c8cdcab657b
Marija Savtchouk <mariasavtchouk@google.com>
2020-11-20 13:47:12 +0000

    Split commit
    Change-Id: I6b04c9df249628fa548d3ede409abf6e8512ca67

--- java/com/google/gerrit/server/notedb/ChangeNotesCache.java
+++ java/com/google/gerrit/server/notedb/ChangeNotesCache.java
@@ -61,7 +61,7 @@ public class ChangeNotesCache {
             .weigher(Weigher.class)
             .maximumWeight(10 << 20)
             .diskLimit(-1)
-            .version(2)
+            .version(3)
             .keySerializer(Key.Serializer.INSTANCE)
             .valueSerializer(ChangeNotesState.Serializer.INSTANCE);
       }
@@ -177,10 +177,9 @@ public class ChangeNotesCache {
           + list(state.changeMessages(), changeMessage())
           + P
           + map(state.publishedComments().asMap(), comment())
-          + 1 // isPrivate
-          + 1 // workInProgress
           + 1 // reviewStarted
-          + I; // updateCount
+          + I // updateCount
+          + T;// mergedOn
     }
 
     private static int str(String s) {

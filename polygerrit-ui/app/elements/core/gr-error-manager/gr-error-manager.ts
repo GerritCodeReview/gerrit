781e2756bcb4c718b0654ca78f3af0b5eeb34ce6
Milutin Kristofic <milutin@google.com>
2020-11-25 16:18:16 +0100

    A11y - improve announcing
    Change-Id: If264d14e1cf76a4ba1ba1133430766d9e6e1d9cf

--- polygerrit-ui/app/elements/core/gr-error-manager/gr-error-manager.ts
+++ polygerrit-ui/app/elements/core/gr-error-manager/gr-error-manager.ts
@@ -336,7 +336,12 @@ export class GrErrorManager extends GestureEventListeners(
     const el = this._createToastAlert();
     el.show(text, actionText, actionCallback);
     this._alertElement = el;
-    this.fire('iron-announce', {text}, {bubbles: true});
+    this.fire(
+      'iron-announce',
+      {text: `Alert: ${text.substr(0, 120)}`},
+      {bubbles: true}
+    );
+    //TODO investigate how to remove iron-announce
     this.reporting.reportInteraction('show-alert', {text});
   }
 

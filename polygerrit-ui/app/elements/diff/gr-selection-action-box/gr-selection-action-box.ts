b68c85f7b855f17468c1d1453df693c68f05d932
Dhruv Srivastava <dhruvsri@google.com>
2020-11-30 19:04:34 +0100

    Remove few usages of any
    Change-Id: I7bda9f50eabd90ca58232945e51c4a269feada45

--- polygerrit-ui/app/elements/diff/gr-selection-action-box/gr-selection-action-box.ts
+++ polygerrit-ui/app/elements/diff/gr-selection-action-box/gr-selection-action-box.ts
@@ -50,8 +50,7 @@ export class GrSelectionActionBox extends GestureEventListeners(
    */
 
   @property({type: Object})
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  keyEventTarget: Record<string, any> = document.body;
+  keyEventTarget = document.body;
 
   @property({type: Boolean})
   positionBelow = false;

0b2ea657551c72a439ff7f65752603dd7d0b62a9
Marco Miller <marco.miller@ericsson.com>
2020-12-07 15:57:38 -0500

    Add support for Elasticsearch version 7.10.*
    Upgrade elasticsearch-rest-client to 7.10.0.
    
    Add V7_10 to the version manager and use it for all the V7 tests.
    
    Change-Id: Ifb941eea63b90e107ae37c6a08ab1815d6c0f22b

--- javatests/com/google/gerrit/elasticsearch/ElasticVersionTest.java
+++ javatests/com/google/gerrit/elasticsearch/ElasticVersionTest.java
@@ -45,6 +45,9 @@ public class ElasticVersionTest {
 
     assertThat(ElasticVersion.forVersion("7.9.0")).isEqualTo(ElasticVersion.V7_9);
     assertThat(ElasticVersion.forVersion("7.9.1")).isEqualTo(ElasticVersion.V7_9);
+
+    assertThat(ElasticVersion.forVersion("7.10.0")).isEqualTo(ElasticVersion.V7_10);
+    assertThat(ElasticVersion.forVersion("7.10.1")).isEqualTo(ElasticVersion.V7_10);
   }
 
   @Test

27aa745f43c2a336f8b1edd06858231716a24cc4
Luca Milanesio <luca.milanesio@gmail.com>
2020-12-08 21:42:39 +0000

    Add initial stream-events acceptance tests
    Gerrit stream-events have never been systematically
    test before, giving the possibility to accidentally break
    external integrations (e.g. Jenkins, Zuul or any other CI system)
    without being noticed by the automatic change validation.
    
    Create an initial scaffolding for enabling future versions of
    Gerrit to benefit from automated acceptance tests of the
    expected stream events outcome of Gerrit operations.
    
    Bug: Issue 13799
    Change-Id: I6987d413ba911039e56950425b501573b4555204

---
+++ javatests/com/google/gerrit/acceptance/ssh/StreamEventsIT.java
@@ -0,0 +1,85 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.ssh;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.NoHttpd;
+import com.google.gerrit.acceptance.Sandboxed;
+import com.google.gerrit.acceptance.UseSsh;
+import com.google.gerrit.extensions.api.changes.ChangeApi;
+import com.google.gerrit.extensions.api.changes.ReviewInput;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.jcraft.jsch.ChannelExec;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.Test;
+
+@NoHttpd
+@UseSsh
+@Sandboxed
+@SuppressWarnings("unused")
+public class StreamEventsIT extends AbstractDaemonTest {
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private static final long TEST_TIMEOUT_SEC = 10;
+  private static final String TEST_REVIEW_COMMENT = "any comment";
+  private ChannelExec streamEventsSshChannel = null;
+  private BufferedReader streamEventsReader = null;
+
+  @Override
+  protected void initSsh() throws Exception {
+    super.initSsh();
+    streamEventsSshChannel = adminSshSession.execAndReturnChannel("gerrit stream-events");
+    streamEventsReader =
+        new BufferedReader(
+            new InputStreamReader(streamEventsSshChannel.getInputStream(), StandardCharsets.UTF_8));
+  }
+
+  @After
+  public void closeStreamEvents() throws IOException {
+    streamEventsReader.close();
+    streamEventsSshChannel.disconnect();
+  }
+
+  @Test
+  public void commentOnChangeShowsUpInStreamEvents() throws Exception {
+    ChangeData change = createChange().getChange();
+    ChangeApi changeApi = gApi.changes().id(change.getId().get());
+    changeApi.current().review(new ReviewInput().message(TEST_REVIEW_COMMENT));
+    changeApi.abandon();
+
+    assertThat(findReviewComment(streamEventsReader, TEST_REVIEW_COMMENT)).isNotEmpty();
+  }
+
+  private List<String> findReviewComment(BufferedReader reader, String reviewComment)
+      throws IOException {
+    List<String> events = new ArrayList<>();
+    while (reader.ready()) {
+      String event = reader.readLine();
+      if (event.contains(reviewComment)) {
+        events.add(reader.readLine());
+      }
+    }
+    return events;
+  }
+}

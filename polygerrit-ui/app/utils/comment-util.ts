8b03c9edbf12ff6cf3e6c8e782dbff0131fe0032
Dhruv Srivastava <dhruvsri@google.com>
2020-12-14 22:51:19 +0100

    Update comment route to handle comments created with side=PARENT
    Comments left on Base when comparing Base vs X are stored with
    patch_set as X and side=PARENT.
    These comments are only visible when comparing Base vs X.
    Hence, do not navigate to X vs Latest like we do for other comments.
    
    Change-Id: Ia73a98d594b38c07105781ed2461dc96f240e49d

--- polygerrit-ui/app/utils/comment-util.ts
+++ polygerrit-ui/app/utils/comment-util.ts
@@ -234,3 +234,30 @@ export function isInPatchRange(
     isInRevisionOfPatchRange(comment, range)
   );
 }
+
+export function getPatchRangeForCommentUrl(
+  comment: UIComment,
+  latestPatchNum: PatchSetNum
+) {
+  if (!comment.patch_set) throw new Error('Missing comment.patch_set');
+
+  if (comment.side === CommentSide.PARENT) {
+    // comment was created with side = PARENT on comment.patch_set
+    // hence navigating to comment.patch_set vs latest will mean
+    // comment is not rendered
+    return {
+      patchNum: comment.patch_set,
+      basePatchNum: ParentPatchSetNum,
+    };
+  } else if (patchNumEquals(latestPatchNum, comment.patch_set)) {
+    return {
+      patchNum: latestPatchNum,
+      basePatchNum: ParentPatchSetNum,
+    };
+  } else {
+    return {
+      patchNum: latestPatchNum,
+      basePatchNum: comment.patch_set,
+    };
+  }
+}

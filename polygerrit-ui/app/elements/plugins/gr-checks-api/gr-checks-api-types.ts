bca8b2b9e63e86f5e683ea0383e9d44b10bb291d
Ben Rohlfs <brohlfs@google.com>
2020-12-16 09:04:28 +0100

    Add top-level actions for Checks API
    Also fix the bug where results would still we part of the response, but
    it is now nested under runs.
    
    Change-Id: Ie1070dda0a2c120b3984eef83908874618a9f03a

--- polygerrit-ui/app/elements/plugins/gr-checks-api/gr-checks-api-types.ts
+++ polygerrit-ui/app/elements/plugins/gr-checks-api/gr-checks-api-types.ts
@@ -67,8 +67,12 @@ export interface FetchResponse {
    */
   loginCallback?: () => void;
 
+  /**
+   * Top-level actions that are not associated with a sepcific run or result.
+   * Will be shown as buttons in the header of the Checks tab.
+   */
+  actions: Action[];
   runs: CheckRun[];
-  results: CheckResult[];
 }
 
 export enum ResponseCode {

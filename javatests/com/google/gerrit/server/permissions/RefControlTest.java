123155393ba06be691dc47cf21c3993368555685
Sven Selberg <svense@axis.com>
2020-12-17 09:43:18 +0100

    ForRef#check should permit internal users to read all refs
    79d24d4 Make PermissionBackend#ForRef authoritative
    Introduced a regression where InternalUsers where not taken into
    consideration when checking READ permission.
    
    Bug: Issue 13786
    Change-Id: I3f18507f65044ac96321c1efecf1f2688f36859f

--- javatests/com/google/gerrit/server/permissions/RefControlTest.java
+++ javatests/com/google/gerrit/server/permissions/RefControlTest.java
@@ -44,6 +44,7 @@ import com.google.gerrit.entities.AccountGroup;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.exceptions.InvalidNameException;
 import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.InternalUser;
 import com.google.gerrit.server.account.GroupMembership;
 import com.google.gerrit.server.account.ListGroupMembership;
 import com.google.gerrit.server.config.AllProjectsName;
@@ -309,6 +310,11 @@ public class RefControlTest {
         .update();
 
     assertAllRefsAreNotVisible(user(allUsersName, DEVS));
+  }
+
+  @Test
+  public void userRefIsVisibleForInternalUser() throws Exception {
+    internalUser(localKey).controlForRef("refs/users/default").asForRef().check(RefPermission.READ);
   }
 
   @Test
@@ -1217,6 +1223,10 @@ public class RefControlTest {
 
   private ProjectState getProjectState(Project.NameKey nameKey) throws Exception {
     return projectCache.checkedGet(nameKey, true);
+  }
+
+  private ProjectControl internalUser(Project.NameKey localKey) throws Exception {
+    return projectControlFactory.create(new InternalUser(), getProjectState(localKey));
   }
 
   private ProjectControl user(Project.NameKey localKey, AccountGroup.UUID... memberOf)

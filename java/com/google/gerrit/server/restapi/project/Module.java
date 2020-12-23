03104f7689027b19c615e23a191232150926acca
Han-Wen Nienhuys <hanwen@google.com>
2020-12-23 16:44:59 +0100

    restapi/project: move CheckAccessReadView into CheckAccess
    No visible change: the POST endpoint was already removed in commit
    3989ac9ba (Nov 2018).
    
    Change-Id: Iaa2587889f630317cf521c3987fdf8925a7ad44e

--- java/com/google/gerrit/server/restapi/project/Module.java
+++ java/com/google/gerrit/server/restapi/project/Module.java
@@ -57,7 +57,7 @@ public class Module extends RestApiModule {
     get(PROJECT_KIND, "access").to(GetAccess.class);
     post(PROJECT_KIND, "access").to(SetAccess.class);
     put(PROJECT_KIND, "access:review").to(CreateAccessChange.class);
-    get(PROJECT_KIND, "check.access").to(CheckAccessReadView.class);
+    get(PROJECT_KIND, "check.access").to(CheckAccess.class);
 
     post(PROJECT_KIND, "check").to(Check.class);
 

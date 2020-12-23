8aa1dfd4e9a48c2b9b58d77685f503d3bd16ea50
Luca Milanesio <luca.milanesio@gmail.com>
2020-12-23 08:04:10 +0000

    Revert "Add support for Elasticsearch version 7.10.*"
    Revert submission 291062
    
    Reason for revert: requires an httpclient that Google cannot support
    Reverted Changes:
    I1fc91bce9:Align http dependencies with elasticsearch-rest-cl...
    Ifb941eea6:Add support for Elasticsearch version 7.10.*
    I7d76ece60:Add support for Elasticsearch version 7.9.*
    
    Change-Id: I740e06058469a0ab12b14c7c616940bf429d0de7

--- javatests/com/google/gerrit/elasticsearch/ElasticV7QueryGroupsTest.java
+++ javatests/com/google/gerrit/elasticsearch/ElasticV7QueryGroupsTest.java
@@ -36,7 +36,7 @@ public class ElasticV7QueryGroupsTest extends AbstractQueryGroupsTest {
   public static void startIndexService() {
     if (container == null) {
       // Only start Elasticsearch once
-      container = ElasticContainer.createAndStart(ElasticVersion.V7_10);
+      container = ElasticContainer.createAndStart(ElasticVersion.V7_9);
     }
   }
 

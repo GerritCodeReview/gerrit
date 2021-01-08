edf4f348bb58c4015cccd975be8d657c492225eb
David Ostrovsky <david@ostrovsky.org>
2021-01-08 21:21:07 +0100

    Add shadow-selection-polyfill
    This is a backport of change 293242 to stable-3.2 branch and to
    consume shadow-selection-polyfill module with npm.
    
    Bug: Issue 11811
    Change-Id: Iaf62137e48b7d119be3d8be96ad03a7f316a5e69

--- polygerrit-ui/app/elements/diff/gr-diff-highlight/gr-diff-highlight.js
+++ polygerrit-ui/app/elements/diff/gr-diff-highlight/gr-diff-highlight.js
@@ -217,6 +217,11 @@ class GrDiffHighlight extends GestureEventListeners(
    * })|null|!Object}
    */
   _getNormalizedRange(selection) {
+    /* On Safari the ShadowRoot.getSelection() isn't there and the only thing
+       we can get is a single Range */
+    if (selection instanceof Range) {
+      return this._normalizeRange(selection);
+    }
     const rangeCount = selection.rangeCount;
     if (rangeCount === 0) {
       return null;
@@ -377,12 +382,21 @@ class GrDiffHighlight extends GestureEventListeners(
   }
 
   _handleSelection(selection, isMouseUp) {
+    /* On Safari, the selection events may return a null range that should
+       be ignored */
+    if (!selection) {
+      return;
+    }
     const normalizedRange = this._getNormalizedRange(selection);
     if (!this._isRangeValid(normalizedRange)) {
       this._removeActionBox();
       return;
     }
-    const domRange = selection.getRangeAt(0);
+    /* On Safari the ShadowRoot.getSelection() isn't there and the only thing
+       we can get is a single Range */
+    const domRange = selection instanceof Range ?
+      selection :
+      selection.getRangeAt(0);
     const start = normalizedRange.start;
     const end = normalizedRange.end;
 

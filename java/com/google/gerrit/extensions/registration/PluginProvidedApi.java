7f97c02db7bd2f8f51d80343d04c262b60041f75
Prudhvi Akhil Alahari <prudhvi@codeaurora.org>
2021-01-11 18:32:19 +0530

    Introduce inter plugin communication with plugin provided API interface
    This change makes it possible to expose an API from one plugin such
    that, the same API is consumable by another plugin (given that the
    API is not exposed to Gerrit users directly). One can expose an API
    from a plugin by implementing the PluginProvidedApi interface. Also,
    bind the implementation class to PluginProvidedApi interface to
    expose the API using an export name for other plugins to look up.
    
    Example use-case: Consider the replication plugin exposing the
    "replication start" command as an API that another plugin could use.
    This would allow another plugin to run replication start for a custom
    set of projects only on server startup.
    
    Change-Id: I62e3b346f77b0c1ff19029b3aca2e607776a689c

---
+++ java/com/google/gerrit/extensions/registration/PluginProvidedApi.java
@@ -0,0 +1,78 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.extensions.registration;
+
+/**
+ * By implementing the PluginProvidedApi interface, a plugin can provide an API which can be
+ * consumed by another plugin. The API thus provided will only be accessible to other plugins and
+ * will not be exposed to Gerrit users directly. To provide an API for other plugins, bind a
+ * PluginProvidedApi. For example:
+ *
+ * <pre>
+ *   // Suppose the below binding is provided by a plugin named 'my-api-plugin'.
+ *   bind(com.google.gerrit.extensions.registration.PluginProvidedApi.class)
+ *     .annotatedWith(Exports.named("MyApi"))
+ *     .to(MyApi.class);
+ *
+ *   public class MyApi implements PluginProvidedApi {
+ *     public String getData() {
+ *       [...]
+ *       return data;
+ *     }
+ *   }
+ * </pre>
+ *
+ * To consume the API provided by a plugin from another plugin, inject the PluginProvidedApi
+ * DynamicMap and fetch the respective API by name. The required API must be available/registered
+ * when trying to request the API from PluginProvidedApi DynamicMap. The registered API is
+ * implemented by a class that is loaded in the implementing plugin's classloader which is different
+ * from the classloader of the consuming plugin. This means the consuming plugin cannot access the
+ * MyAPI class at compile-time since there will be a class miss-match when the same compile-time
+ * class is accessed via two different classloaders, i.e., if an attempt is made to assign the
+ * implementation from the providing plugin's classloader to a class variable, argument, or return
+ * value loaded by the consuming plugin's classloader. Due to this reason, reflection is used to
+ * invoke the required method. For example:
+ *
+ * <pre>
+ *   public class MyClass {
+ *     protected DynamicMap<PluginProvidedApi> pluginProvidedApis;
+ *
+ *     @Inject
+ *     public MyClass(DynamicMap<PluginProvidedApi> pluginProvidedApis) {
+ *       this.pluginProvidedApis = pluginProvidedApis;
+ *     }
+ *
+ *     public Optional<String> getMyData() {
+ *       // Casting to original compile time class like below will fail:
+ *       // MyApi api = (MyApi) pluginProvidedApis.get("my-api-plugin", "MyApi");
+ *       PluginProvidedApi pluginProvidedApi = pluginProvidedApis.get("my-api-plugin", "MyApi");
+ *       if (pluginProvidedApi == null) {
+ *         return Optional.empty();
+ *       }
+ *       return getDataFromApi(pluginProvidedApi);
+ *     }
+ *
+ *     protected Optional<String> getDataFromApi(PluginProvidedApi pluginProvidedApi) {
+ *       try {
+ *         return Optional.of((String) pluginProvidedApi.getClass().getMethod("getData").invoke(pluginProvidedApi));
+ *       } catch (ClassCastException | IllegalAccessException | InvocationTargetException
+ *           | NoSuchMethodException e) {
+ *         return Optional.empty();
+ *       }
+ *     }
+ *   }
+ * </pre>
+ */
+public interface PluginProvidedApi {}

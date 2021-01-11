af64ffad9a8593641f748aa3f20c866b8b81614c
Dhruv Srivastava <dhruvsri@google.com>
2021-01-11 09:53:58 +0100

    Add bug icon next to "Ported from" message
    For easy bug reporting, add a bug icon linking to the porting
    comments template in monorail next to the "Ported from patchset"
    text.
    
    Screenshot: https://imgur.com/a/Mk93bJp
    Change-Id: I92cc3cabf8836bdf749561f28db60364cfe9fdbf

--- polygerrit-ui/app/elements/shared/gr-comment/gr-comment_html.ts
+++ polygerrit-ui/app/elements/shared/gr-comment/gr-comment_html.ts
@@ -269,6 +269,12 @@ export const htmlTemplate = html`
               Ported from patchset [[comment.patch_set]]
             </span>
           </a>
+          <a
+            href="https://bugs.chromium.org/p/gerrit/issues/entry?template=Porting+Comments"
+            target="_blank"
+          >
+            <iron-icon icon="gr-icons:bug" title="report a problem"></iron-icon>
+          </a>
         </template>
         <gr-tooltip-content
           class="draftTooltip"

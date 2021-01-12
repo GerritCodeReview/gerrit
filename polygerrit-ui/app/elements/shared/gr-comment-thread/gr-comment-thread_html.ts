a7722e337ccd8e2eda609bfd3e5e88fbc4b341fb
Dhruv Srivastava <dhruvsri@google.com>
2021-01-12 11:45:16 +0100

    Show the lines of code around a comment as context
    Gerrit now supports requesting the lines of code around a comment
    to show the context around the comment.
    We show the line number as well as the line of code itself.
    Highlighting of the selected comment range will be added in a follow
    up change.
    We re-use the existing diff component to render the context
    The context is currently shown in the Comments Tab as well as the
    Change Log.
    
    Change-Id: Ieb6ea2ca6d9c071cb0f9b9e15238223314f2446b

--- polygerrit-ui/app/elements/shared/gr-comment-thread/gr-comment-thread_html.ts
+++ polygerrit-ui/app/elements/shared/gr-comment-thread/gr-comment-thread_html.ts
@@ -34,24 +34,28 @@ export const htmlTemplate = html`
       margin-left: auto;
       padding: var(--spacing-s) var(--spacing-m);
     }
-    #container {
+    .comment-box {
+      height: fit-content;
+      width: 80ch;
       background-color: var(--comment-background-color);
       color: var(--comment-text-color);
-      display: var(--gr-comment-thread-display, block);
-      margin: 0 var(--spacing-s) var(--spacing-s);
-      white-space: normal;
       box-shadow: var(--elevation-level-2);
       border-radius: var(--border-radius);
+    }
+    #container {
+      display: var(--gr-comment-thread-display, flex);
+      margin: 0 var(--spacing-s) var(--spacing-s);
+      white-space: normal;
       /** This is required for firefox to continue the inheritance */
       -webkit-user-select: inherit;
       -moz-user-select: inherit;
       -ms-user-select: inherit;
       user-select: inherit;
     }
-    #container.unresolved {
+    .comment-box.unresolved {
       background-color: var(--unresolved-comment-background-color);
     }
-    #container.robotComment {
+    .comment-box.robotComment {
       background-color: var(--robot-comment-background-color);
     }
     #commentInfoContainer {
@@ -75,94 +79,123 @@ export const htmlTemplate = html`
     .fileName {
       padding: var(--spacing-m) var(--spacing-s) var(--spacing-m);
     }
+    .thread-container {
+      display: flex;
+    }
+    .comment-container {
+      width: 160ch;
+    }
+    gr-diff {
+      margin-left: var(--spacing-l);
+    }
   </style>
-  <template is="dom-if" if="[[showFilePath]]">
-    <template is="dom-if" if="[[showFileName]]">
-      <div class="fileName">
-        <template is="dom-if" if="[[_isPatchsetLevelComment(path)]]">
-          <span> [[_computeDisplayPath(path)]] </span>
+
+  <div class="thread-container">
+    <div class="comment-container">
+      <template is="dom-if" if="[[showFilePath]]">
+        <template is="dom-if" if="[[showFileName]]">
+          <div class="fileName">
+            <template is="dom-if" if="[[_isPatchsetLevelComment(path)]]">
+              <span> [[_computeDisplayPath(path)]] </span>
+            </template>
+            <template is="dom-if" if="[[!_isPatchsetLevelComment(path)]]">
+              <a
+                href$="[[_getDiffUrlForPath(projectName, changeNum, path, patchNum)]]"
+              >
+                [[_computeDisplayPath(path)]]
+              </a>
+            </template>
+          </div>
         </template>
-        <template is="dom-if" if="[[!_isPatchsetLevelComment(path)]]">
-          <a
-            href$="[[_getDiffUrlForPath(projectName, changeNum, path, patchNum)]]"
-          >
-            [[_computeDisplayPath(path)]]
-          </a>
-        </template>
-      </div>
-    </template>
-    <div class="pathInfo">
-      <template is="dom-if" if="[[!_isPatchsetLevelComment(path)]]">
-        <a
-          href$="[[_getDiffUrlForComment(projectName, changeNum, path, patchNum)]]"
-          >[[_computeDisplayLine()]]</a
-        >
+        <div class="pathInfo">
+          <template is="dom-if" if="[[!_isPatchsetLevelComment(path)]]">
+            <a
+              href$="[[_getDiffUrlForComment(projectName, changeNum, path, patchNum)]]"
+              >[[_computeDisplayLine()]]</a
+            >
+          </template>
+        </div>
       </template>
-    </div>
-  </template>
-  <div
-    id="container"
-    class$="[[_computeHostClass(unresolved, isRobotComment)]]"
-  >
-    <template
-      id="commentList"
-      is="dom-repeat"
-      items="[[_orderedComments]]"
-      as="comment"
-    >
-      <gr-comment
-        comment="{{comment}}"
-        comments="{{comments}}"
-        robot-button-disabled="[[_shouldDisableAction(_showActions, _lastComment)]]"
-        change-num="[[changeNum]]"
-        project-name="[[projectName]]"
-        patch-num="[[patchNum]]"
-        draft="[[_isDraft(comment)]]"
-        show-actions="[[_showActions]]"
-        show-patchset="[[showPatchset]]"
-        show-ported-comment="[[_computeShowPortedComment(comment)]]"
-        side="[[comment.side]]"
-        project-config="[[_projectConfig]]"
-        on-create-fix-comment="_handleCommentFix"
-        on-comment-discard="_handleCommentDiscard"
-        on-comment-save="_handleCommentSavedOrDiscarded"
-      ></gr-comment>
-    </template>
-    <div
-      id="commentInfoContainer"
-      hidden$="[[_hideActions(_showActions, _lastComment)]]"
-    >
-      <span id="unresolvedLabel" hidden$="[[!unresolved]]">Unresolved</span>
-      <div id="actions">
-        <gr-button
-          id="replyBtn"
-          link=""
-          class="action reply"
-          on-click="_handleCommentReply"
-          >Reply</gr-button
+      <div id="container">
+        <div
+          class$="[[_computeHostClass(unresolved, isRobotComment)]] comment-box"
         >
-        <gr-button
-          id="quoteBtn"
-          link=""
-          class="action quote"
-          on-click="_handleCommentQuote"
-          >Quote</gr-button
-        >
-        <template is="dom-if" if="[[unresolved]]">
-          <gr-button
-            id="ackBtn"
-            link=""
-            class="action ack"
-            on-click="_handleCommentAck"
-            >Ack</gr-button
+          <template
+            id="commentList"
+            is="dom-repeat"
+            items="[[_orderedComments]]"
+            as="comment"
           >
-          <gr-button
-            id="doneBtn"
-            link=""
-            class="action done"
-            on-click="_handleCommentDone"
-            >Done</gr-button
+            <gr-comment
+              comment="{{comment}}"
+              comments="{{comments}}"
+              robot-button-disabled="[[_shouldDisableAction(_showActions, _lastComment)]]"
+              change-num="[[changeNum]]"
+              project-name="[[projectName]]"
+              patch-num="[[patchNum]]"
+              draft="[[_isDraft(comment)]]"
+              show-actions="[[_showActions]]"
+              show-patchset="[[showPatchset]]"
+              show-ported-comment="[[_computeShowPortedComment(comment)]]"
+              side="[[comment.side]]"
+              project-config="[[_projectConfig]]"
+              on-create-fix-comment="_handleCommentFix"
+              on-comment-discard="_handleCommentDiscard"
+              on-comment-save="_handleCommentSavedOrDiscarded"
+            ></gr-comment>
+          </template>
+          <div
+            id="commentInfoContainer"
+            hidden$="[[_hideActions(_showActions, _lastComment)]]"
           >
+            <span id="unresolvedLabel" hidden$="[[!unresolved]]"
+              >Unresolved</span
+            >
+            <div id="actions">
+              <gr-button
+                id="replyBtn"
+                link=""
+                class="action reply"
+                on-click="_handleCommentReply"
+                >Reply</gr-button
+              >
+              <gr-button
+                id="quoteBtn"
+                link=""
+                class="action quote"
+                on-click="_handleCommentQuote"
+                >Quote</gr-button
+              >
+              <template is="dom-if" if="[[unresolved]]">
+                <gr-button
+                  id="ackBtn"
+                  link=""
+                  class="action ack"
+                  on-click="_handleCommentAck"
+                  >Ack</gr-button
+                >
+                <gr-button
+                  id="doneBtn"
+                  link=""
+                  class="action done"
+                  on-click="_handleCommentDone"
+                  >Done</gr-button
+                >
+              </template>
+            </div>
+          </div>
+        </div>
+        <template is="dom-if" if="[[_shouldShowCommentContext()]]">
+          <gr-diff
+            id="diff"
+            change-num="[[changeNum]]"
+            diff="[[_getDiffFromContext(comments)]]"
+            path="[[path]]"
+            prefs="[[_prefs]]"
+            line-offset="[[_getLineOffsetFromContext(comments)]]"
+            on-render="_handleDiffRender"
+          >
+          </gr-diff>
         </template>
       </div>
     </div>

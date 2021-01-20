0de1d2855dedca891b228b59c3d454d1849147a7
Ben Rohlfs <brohlfs@google.com>
2021-01-20 09:15:40 +0100

    Refine the UI of the checks chips
    Total height of chips is changed to 20px. font-size is reduced within
    chips to 12px. Tweaking some vertical-aligna and padding rules for that.
    
    We never allow the checks summary line to wrap onto two lines.
    
    Changing the font color of this chips to not be black, but dark gray
    instead.
    
    Instead of allowing one error and one warning to expand we allow 2 chips
    to be expanded in total. It was weird to see 2 errors collapsed and 1
    warning expanded.
    
    Change-Id: Iff7d5570e04269220dd451d280d59d8c03e2b9e4

--- polygerrit-ui/app/styles/themes/app-theme.ts
+++ polygerrit-ui/app/styles/themes/app-theme.ts
@@ -58,6 +58,7 @@ const styleSheet = safeStyleSheet`
     --gray-100: #f1f3f4;
     --gray-50: #f8f9fa;
 
+    --chip-color: var(--gray-900);
     --error-color: var(--red-900);
     --error-foreground: var(--red-700);
     --error-background: var(--red-50);

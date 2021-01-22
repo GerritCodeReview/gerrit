0cc01de70c8084b29d6e8c15e052817bd77fd677
Alex Spradlin <alexaspradlin@google.com>
2021-01-22 13:47:14 -0800

    Create skeleton classes to diff ChangeNoteStates.
    For the Cloud PubSub feature being built, ChangeEvents will be emitted
    that contain the diff between the old state of a change's metadata and
    its new state. The ChangeNoteState class contains the information
    needed to accomplish this.
    
    This creates skeleton classes for a ChangeNoteState differ and a POJO
    describing the diff.
    
    Signed-off-by: Alex Spradlin <alexaspradlin@google.com>
    Change-Id: I49af776dd234a639078ee4fe10a2e8cd884e6305

---
+++ java/com/google/gerrit/server/notedb/ChangeNotesStateDiffer.java
@@ -0,0 +1,39 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.notedb;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Gets the diff between two {@link ChangeNotesState} instances.
+ */
+public final class ChangeNotesStateDiffer {
+
+  /**
+   * Returns the diff between two instances of {@link ChangeNotesState}.
+   * 
+   * @param oldState the previous state to diff against {@code newState}
+   * @param newState the state to diff against {@code oldState}
+   * @return the diff between the given states
+   */
+  public ChangeNotesStateDiff getDiff(ChangeNotesState oldState, ChangeNotesState newState) {
+    checkNotNull(oldState);
+    checkNotNull(newState);
+
+    // TODO(alexaspradlin): implement ChangeNotesStateDiffer.calculateDiff
+    throw new UnsupportedOperationException(
+        "ChangeNotesStateDiffer.calculateDiff is not implemented yet");
+  }
+}

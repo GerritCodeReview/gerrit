c4b0e104640c01a2b548b35032384e0a17aa7988
Dhruv Srivastava <dhruvsri@google.com>
2021-01-29 13:42:58 +0100

    Add repository browser link to repo detail page
    Currently in the repository detail page we have a link to view the
    changes but no link to browsing the repository itself.
    Add a link to browse the content of the repository.
    
    Update the simple links to use gr-button.
    The type for ProjectAccessInfo also was updated to correctly
    define config_web_links as an array of WebLinkInfo.
    
    Change-Id: I967e9f284aae502aca15eda894340836c118b18e

--- polygerrit-ui/app/elements/admin/gr-repo/gr-repo.ts
+++ polygerrit-ui/app/elements/admin/gr-repo/gr-repo.ts
@@ -46,6 +46,7 @@ import {PolymerDeepPropertyChange} from '@polymer/polymer/interfaces';
 import {hasOwnProperty} from '../../../utils/common-util';
 import {firePageError, fireTitleChange} from '../../../utils/event-util';
 import {appContext} from '../../../services/app-context';
+import {WebLinkInfo} from '../../../types/diff';
 
 const STATES = {
   active: {value: ProjectState.ACTIVE, label: 'Active'},
@@ -137,6 +138,9 @@ export class GrRepo extends GestureEventListeners(
   @property({type: Object})
   _schemesObj?: SchemesInfoMap;
 
+  @property({type: Array})
+  weblinks: WebLinkInfo[] = [];
+
   private restApiService = appContext.restApiService;
 
   /** @override */
@@ -187,6 +191,7 @@ export class GrRepo extends GestureEventListeners(
 
             // If the user is not an owner, is_owner is not a property.
             this._readOnly = !access[repo].is_owner;
+            this.weblinks = access[repo].config_web_links;
           });
         }
       })
@@ -415,6 +420,10 @@ export class GrRepo extends GestureEventListeners(
 
   _computeChangesUrl(name: RepoName) {
     return GerritNav.getUrlForProjectChanges(name);
+  }
+
+  _computeBrowseUrl(weblinks: WebLinkInfo[]) {
+    return weblinks[0].url;
   }
 
   _handlePluginConfigChanged({

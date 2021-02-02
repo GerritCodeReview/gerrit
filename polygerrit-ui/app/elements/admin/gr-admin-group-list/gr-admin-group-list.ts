77b507381daea7a1bfbc02d388fe85c2c0d7ba03
Dhruv Srivastava <dhruvsri@google.com>
2021-02-02 12:43:27 +0100

    Auto focus on the first input field
    When opening the Create New dialog in Browse > Repositories and
    Browse > Groups, auto focus on the first input field to avoid
    unnecessary clicks.
    
    Bug: Issue 12536
    Change-Id: Ieae5f1e5eac1cd51a101cc7fcab4a1d78582faaf

--- polygerrit-ui/app/elements/admin/gr-admin-group-list/gr-admin-group-list.ts
+++ polygerrit-ui/app/elements/admin/gr-admin-group-list/gr-admin-group-list.ts
@@ -178,7 +178,9 @@ export class GrAdminGroupList extends ListViewMixin(
   }
 
   _handleCreateClicked() {
-    this.$.createOverlay.open();
+    this.$.createOverlay.open().then(() => {
+      this.$.createNewModal.shadowRoot?.querySelector('input')?.focus();
+    });
   }
 
   _visibleToAll(item: GroupInfo) {

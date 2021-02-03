17ebb872f8b5d6ed7ebdfc2a13a0ac9e3fb25765
Dhruv Srivastava <dhruvsri@google.com>
2021-02-03 10:26:47 +0100

    Replace CommentThreadElement with GrCommentThread
    Change-Id: I8c23705099e89c15a58e0b58d410c2516ff831ca

--- polygerrit-ui/app/elements/diff/gr-diff-highlight/gr-diff-highlight.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-highlight/gr-diff-highlight.ts
@@ -31,6 +31,7 @@ import {GrSelectionActionBox} from '../gr-selection-action-box/gr-selection-acti
 import {GrDiffBuilderElement} from '../gr-diff-builder/gr-diff-builder-element';
 import {FILE} from '../gr-diff/gr-diff-line';
 import {getRange, getSide} from '../gr-diff/gr-diff-utils';
+import {GrCommentThread} from '../../shared/gr-comment-thread/gr-comment-thread';
 
 interface SidedRange {
   side: Side;
@@ -47,11 +48,6 @@ interface NormalizedPosition {
 interface NormalizedRange {
   start: NormalizedPosition | null;
   end: NormalizedPosition | null;
-}
-
-// TODO(TS): Replace by GrCommentThread once that is converted.
-interface CommentThreadElement extends HTMLElement {
-  rootId: string;
 }
 
 @customElement('gr-diff-highlight')
@@ -129,23 +125,20 @@ export class GrDiffHighlight extends GestureEventListeners(
     );
   }
 
-  _getThreadEl(e: Event): CommentThreadElement | null {
+  _getThreadEl(e: Event): GrCommentThread | null {
     const path = (dom(e) as EventApi).path || [];
     for (const pathEl of path) {
       if (
         pathEl instanceof HTMLElement &&
         pathEl.classList.contains('comment-thread')
       ) {
-        return pathEl as CommentThreadElement;
+        return pathEl as GrCommentThread;
       }
     }
     return null;
   }
 
-  _toggleRangeElHighlight(
-    threadEl: CommentThreadElement,
-    highlightRange = false
-  ) {
+  _toggleRangeElHighlight(threadEl: GrCommentThread, highlightRange = false) {
     // We don't want to re-create the line just for highlighting the range which
     // is creating annoying bugs: @see Issue 12934
     // As gr-ranged-comment-layer now does not notify the layer re-render and

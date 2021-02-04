a35cca7a42581d3030990a1cd4fe22f083866ce6
Milutin Kristofic <milutin@google.com>
2021-02-04 11:06:01 +0100

    Comments summary - unresolved comments + styling
    This is part of Change Summary Redesign and it's under feature flag.
    
    Change-Id: Ice24547044d7e369074db70e6b7149833f78f728

--- polygerrit-ui/app/styles/themes/app-theme.ts
+++ polygerrit-ui/app/styles/themes/app-theme.ts
@@ -74,6 +74,8 @@ const styleSheet = safeStyleSheet`
     --warning-background: var(--orange-50);
     --info-foreground: var(--blue-700);
     --info-background: var(--blue-50);
+    --info-deemphasized-foreground: var(--gray-300);
+    --info-deemphasized-background: var(--gray-50);
     --success-foreground: var(--green-700);
     --gray-foreground: var(--gray-700);
     --gray-background: var(--gray-100);

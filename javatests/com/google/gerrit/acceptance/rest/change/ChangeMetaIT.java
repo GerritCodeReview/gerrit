f421d0ef5cc908814b4f42d0488482abd519b406
Han-Wen Nienhuys <hanwen@google.com>
2021-02-08 21:04:49 +0100

    RFC: GetChange: provide meta=SHA1 option
    This change lets callers inspect a change at a given NoteDb commit.
    
        $ curl -u admin:secret http://localhost:8080/a/changes/101/'?pp=1&O=22'
        ..
          "current_revision": "f3cccef96155ed3f9de2aaed44d8cf00960a21ca",
          "revisions": {
            "f3cccef96155ed3f9de2aaed44d8cf00960a21ca": {
              "kind": "REWORK",
              "_number": 5,
    
        $ curl -u admin:secret http://localhost:8080/a/changes/101/'?pp=1&O=22&meta=8ebdac0e8a2047ee2d2433a09f05de230d3050f2'
        ..
          "current_revision": "e24af8d95295181064078a99a3bde31cf9b643a2",
          "revisions": {
            "e24af8d95295181064078a99a3bde31cf9b643a2": {
              "kind": "NO_CODE_CHANGE",
              "_number": 3,
    
    This is part of an alternate approach to event notifications:
    
    * One component provides a stream of events containing (change number,
    old meta SHA1, new meta SHA1)
    
    * A subscriber, when receiving a notification, can use
    /changes/ID?meta=XXX to get the old and new states of the ChangeInfo.
    By analyzing the difference between both ChangeInfos, subscribers can
    tell what happened.
    
    This has several advantages over the existing stream-events mechanism:
    
    * The event notification only contains opaque data, and can be sent to
    any subscriber without leaking data. The GetChange endpoint enforces
    visibility.
    
    * It uses the same endpoint as the UI, so there is no chance of new
    features being forgotten for stream-events (for example, stream-events
    does not support attention set updates).
    
    * A subscriber can protect against missed notifications, by keeping
    track of the meta SHA1s locally.  If the connection breaks, they can
    resynchronize by executing ls-remote.
    
    * It doesn't rely on having an SSH stream. Without streaming, one can
    still use polling on ls-remote to detect changes across a project
    efficiently.
    
    If the meta ref has a non-FF update (eg. deleted comment), getting the
    ChangeInfo at the old SHA1 will not work. Subscribers can guard
    against this by also persisting ChangeInfo, keyed by SHA1.
    
    A similar setup is planned for notifications at Google. At Google,
    refs are stored in a global, relational database. The database allows
    enqueueing notifications (ie. {change number, old sha, new sha})
    together with ref updates. The notifications are processed
    asynchronously by converting ChangeInfo to protocol buffer, diffing
    those, and sending the result to interested subscribers.
    
    Change-Id: Ia54acb37e1187bc64e190d5694de0b17674595f8

--- javatests/com/google/gerrit/acceptance/rest/change/ChangeMetaIT.java
+++ javatests/com/google/gerrit/acceptance/rest/change/ChangeMetaIT.java
@@ -20,8 +20,10 @@ import static com.google.gerrit.extensions.client.ListChangesOption.META_REF;
 
 import com.google.gerrit.acceptance.AbstractDaemonTest;
 import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.acceptance.RestResponse;
 import com.google.gerrit.entities.Change;
 import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gson.stream.JsonReader;
 import org.eclipse.jgit.lib.Repository;
 import org.junit.Test;
 
@@ -36,5 +38,40 @@ public class ChangeMetaIT extends AbstractDaemonTest {
           .isEqualTo(
               repo.exactRef(changeMetaRef(Change.id(before._number))).getObjectId().getName());
     }
+  }
+
+  @Test
+  public void ChangeInfo_metaSha1_parameter() throws Exception {
+    PushOneCommit.Result result = createChange();
+    String changeId = result.getChangeId();
+    gApi.changes().id(changeId).setMessage("before\n\n" + "Change-Id: " + result.getChangeId());
+    ChangeInfo before = gApi.changes().id(changeId).get(META_REF);
+    gApi.changes().id(changeId).setMessage("after\n\n" + "Change-Id: " + result.getChangeId());
+    ChangeInfo after = gApi.changes().id(changeId).get(META_REF);
+    assertThat(after.metaRef).isNotEqualTo(before.metaRef);
+
+    RestResponse resp =
+        adminRestSession.get("/changes/" + changeId + "/?meta=" + before.metaRef + "&O=1000000");
+    resp.assertOK();
+
+    ChangeInfo got;
+    try (JsonReader jsonReader = new JsonReader(resp.getReader())) {
+      jsonReader.setLenient(true);
+      got = newGson().fromJson(jsonReader, ChangeInfo.class);
+    }
+    assertThat(got.subject).isEqualTo(before.subject);
+  }
+
+  @Test
+  public void metaUnreachableSha1() throws Exception {
+    PushOneCommit.Result ch1 = createChange();
+    PushOneCommit.Result ch2 = createChange();
+
+    ChangeInfo info2 = gApi.changes().id(ch2.getChangeId()).get(META_REF);
+
+    RestResponse resp =
+        adminRestSession.get("/changes/" + ch1.getChangeId() + "/?meta=" + info2.metaRef);
+
+    resp.assertStatus(412);
   }
 }

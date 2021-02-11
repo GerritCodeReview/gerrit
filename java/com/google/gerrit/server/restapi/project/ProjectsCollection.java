582c58048f06924f2c79b4fa717b2c4f2d5138b0
Han-Wen Nienhuys <hanwen@google.com>
2021-02-11 18:47:23 +0100

    ProjectsCollection: remove @Singleton
    The query field is a per-request field.
    
    Change-Id: I3d030b47c344eee9cfc2a6313f7733f3b3379977

--- java/com/google/gerrit/server/restapi/project/ProjectsCollection.java
+++ java/com/google/gerrit/server/restapi/project/ProjectsCollection.java
@@ -41,11 +41,9 @@ import com.google.gerrit.server.project.ProjectResource;
 import com.google.gerrit.server.project.ProjectState;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import com.google.inject.Singleton;
 import java.io.IOException;
 import java.util.Optional;
 
-@Singleton
 public class ProjectsCollection
     implements RestCollection<TopLevelResource, ProjectResource>, NeedsParams {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();

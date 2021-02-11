0d16d4fdbd0f19f72168a88c0e23b6ceac3faee9
Han-Wen Nienhuys <hanwen@google.com>
2021-02-11 19:55:22 +0100

    GetChange: populate ChangeInfo.metaRef with the NoteDb meta ref SHA1
    The NoteDb meta SHA1 is the unique identifier the state of a change,
    together with the robot-comment SHA1.
    
    Change-Id: I8b4cf63f9e8255ab9c2d91ddf002820f46a288c8

---
+++ javatests/com/google/gerrit/acceptance/rest/change/ChangeMetaIT.java
@@ -0,0 +1,40 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.rest.change;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.entities.RefNames.changeMetaRef;
+import static com.google.gerrit.extensions.client.ListChangesOption.META_REF;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.extensions.common.ChangeInfo;
+import org.eclipse.jgit.lib.Repository;
+import org.junit.Test;
+
+public class ChangeMetaIT extends AbstractDaemonTest {
+  @Test
+  public void ChangeInfo_metaSha1() throws Exception {
+    PushOneCommit.Result result = createChange();
+    String changeId = result.getChangeId();
+    ChangeInfo before = gApi.changes().id(changeId).get(META_REF);
+    try (Repository repo = repoManager.openRepository(project)) {
+      assertThat(before.metaRef)
+          .isEqualTo(
+              repo.exactRef(changeMetaRef(Change.id(before._number))).getObjectId().getName());
+    }
+  }
+}

a322ff50294dc6d42fb4d537e1c4f1de2c24868e
Hermann Loose <hermannloose@google.com>
2021-02-16 16:08:10 +0100

    Let image diff builder know when to use new UI
    Change-Id: Iccf489c07b7738ee47da141146ea29227ef67543

--- polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-image.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-builder/gr-diff-builder-image.ts
@@ -20,6 +20,8 @@ import {ImageInfo} from '../../../types/common';
 import {DiffInfo, DiffPreferencesInfo} from '../../../types/diff';
 import {GrEndpointParam} from '../../plugins/gr-endpoint-param/gr-endpoint-param';
 import {RenderPreferences} from '../../../api/diff';
+import '../gr-diff-image-viewer/gr-image-viewer';
+import {GrImageViewer} from '../gr-diff-image-viewer/gr-image-viewer';
 
 // MIME types for images we allow showing. Do not include SVG, it can contain
 // arbitrary JavaScript.
@@ -32,7 +34,8 @@ export class GrDiffBuilderImage extends GrDiffBuilderSideBySide {
     outputEl: HTMLElement,
     private readonly _baseImage: ImageInfo | null,
     private readonly _revisionImage: ImageInfo | null,
-    renderPrefs?: RenderPreferences
+    renderPrefs?: RenderPreferences,
+    private readonly _useNewImageDiffUi: boolean = false
   ) {
     super(diff, prefs, outputEl, [], renderPrefs);
   }
@@ -40,11 +43,17 @@ export class GrDiffBuilderImage extends GrDiffBuilderSideBySide {
   public renderDiff() {
     const section = this._createElement('tbody', 'image-diff');
 
-    this._emitImagePair(section);
-    this._emitImageLabels(section);
+    if (this._useNewImageDiffUi) {
+      this._emitImageViewer(section);
 
-    this._outputEl.appendChild(section);
-    this._outputEl.appendChild(this._createEndpoint());
+      this._outputEl.appendChild(section);
+    } else {
+      this._emitImagePair(section);
+      this._emitImageLabels(section);
+
+      this._outputEl.appendChild(section);
+      this._outputEl.appendChild(this._createEndpoint());
+    }
   }
 
   private _createEndpoint() {
@@ -78,6 +87,27 @@ export class GrDiffBuilderImage extends GrDiffBuilderSideBySide {
     return endpointParam;
   }
 
+  private _emitImageViewer(section: HTMLElement) {
+    const tr = this._createElement('tr');
+    const td = this._createElement('td');
+    // TODO(hermannloose): Support blame for image diffs, see above.
+    td.setAttribute('colspan', '4');
+    const imageViewer = this._createElement('gr-image-viewer') as GrImageViewer;
+
+    imageViewer.baseUrl = this._getImageSrc(this._baseImage);
+    imageViewer.revisionUrl = this._getImageSrc(this._revisionImage);
+
+    td.appendChild(imageViewer);
+    tr.appendChild(td);
+    section.appendChild(tr);
+  }
+
+  private _getImageSrc(image: ImageInfo | null): string|undefined {
+    return image && IMAGE_MIME_PATTERN.test(image.type)
+      ? `data:${image.type};base64, ${image.body}`
+      : undefined;
+  }
+
   private _emitImagePair(section: HTMLElement) {
     const tr = this._createElement('tr');
 
@@ -98,18 +128,19 @@ export class GrDiffBuilderImage extends GrDiffBuilderSideBySide {
     section: HTMLElement
   ) {
     const td = this._createElement('td', className);
-    if (image && IMAGE_MIME_PATTERN.test(image.type)) {
+    const src = this._getImageSrc(image);
+    if (src) {
       const imageEl = this._createElement('img') as HTMLImageElement;
       imageEl.onload = () => {
         image._height = imageEl.naturalHeight;
         image._width = imageEl.naturalWidth;
         this._updateImageLabel(section, className, image);
       };
-      imageEl.setAttribute('src', `data:${image.type};base64, ${image.body}`);
       imageEl.addEventListener('error', (e: Event) => {
         imageEl.remove();
         td.textContent = '[Image failed to load] ' + e.type;
       });
+      imageEl.setAttribute('src', src);
       td.appendChild(imageEl);
     }
     return td;

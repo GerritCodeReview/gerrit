41c5a2a05c9a2b4dfeaa5f3c6069080f2d4f7be1
Youssef Elghareeb <ghareeb@google.com>
2021-02-25 15:29:51 +0100

    Fix comment context for git submodule files
    If comment context is requested for a git submodule file, an exception
    is thrown in CommentContextLoader because the object type is not a blob.
    This change fixes this case.
    
    Since this check was also done by the Get Diff endpoint, I extracted the
    logic in a common class that can be reused by both classes.
    
    Change-Id: Ibbfa00212386322d70289ed8077d84357083e0c8

--- java/com/google/gerrit/server/patch/PatchScriptBuilder.java
+++ java/com/google/gerrit/server/patch/PatchScriptBuilder.java
@@ -14,8 +14,6 @@
 
 package com.google.gerrit.server.patch;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.common.data.PatchScript;
@@ -352,16 +350,8 @@ class PatchScriptBuilder {
         byte[] srcContent;
         if (reuse) {
           srcContent = other.srcContent;
-
-        } else if (mode.getObjectType() == Constants.OBJ_BLOB) {
-          srcContent = Text.asByteArray(db.open(id, Constants.OBJ_BLOB));
-
-        } else if (mode.getObjectType() == Constants.OBJ_COMMIT) {
-          String strContent = "Subproject commit " + ObjectId.toString(id);
-          srcContent = strContent.getBytes(UTF_8);
-
         } else {
-          srcContent = Text.NO_BYTES;
+          srcContent = SrcContentResolver.getSourceContent(db, id, mode);
         }
         String mimeType = MimeUtil2.UNKNOWN_MIME_TYPE.toString();
         DisplayMethod displayMethod = DisplayMethod.DIFF;

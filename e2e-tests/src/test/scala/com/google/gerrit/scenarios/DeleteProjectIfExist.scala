386cd1604e69d5cd6c8be595fb15053bd6476811
Nguyen Tuan Khang Phan <nguyen.tuan.khang.phan@ericsson.com>
2021-03-02 07:10:18 -0500

    e2e-tests: Add a clean-up scenario
    This scenario can be used as a last step in the test run. It deletes the
    project if the project still exist making it possible to run tests in
    succession even if the previous test failed and left open changes in
    a test project. Current delete project scenario does not use forced
    option for delete operation.
    
    Change-Id: I2b84419b3665b85465a441a214bf23c420fae0c5

---
+++ e2e-tests/src/test/scala/com/google/gerrit/scenarios/DeleteProjectIfExist.scala
@@ -0,0 +1,48 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.scenarios
+
+import io.gatling.core.Predef.{atOnceUsers, exec, _}
+import io.gatling.core.feeder.FeederBuilder
+import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
+import io.gatling.http.Predef._
+
+
+class DeleteProjectIfExist extends ProjectSimulation {
+  private val data: FeederBuilder = jsonFile(resource).convert(keys).queue
+
+  def this(projectName: String) {
+    this()
+    this.projectName = projectName
+  }
+
+  val test: ScenarioBuilder = scenario(uniqueName)
+      .feed(data)
+      .exec(
+        http(uniqueName)
+            .get("${url}")
+            .check(
+              status.transform(status => 200.equals(status))
+                  .saveAs("FOUND")))
+      .doIf(session => session.attributes("FOUND").equals(true)) {
+        exec(http(uniqueName).post("${url}/delete-project~delete").body(ElFileBody(body)).asJson)
+      }
+
+
+  setUp(
+    test.inject(
+      atOnceUsers(single)
+    )).protocols(httpProtocol)
+}

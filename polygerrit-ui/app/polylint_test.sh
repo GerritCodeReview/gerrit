7d55749c3048b08c949692e254b9c60c220c2de3
Dmitrii Filippov <dmfilippov@google.com>
2021-03-02 15:17:03 +0100

    Test commit
    Change-Id: I2b886bccbdf05b58d109c76259760c39424efaae

--- polygerrit-ui/app/polylint_test.sh
+++ polygerrit-ui/app/polylint_test.sh
@@ -10,4 +10,10 @@ cp -R -L polygerrit-ui/app/_pg_ts_out/* $TEST_TMPDIR
 #Can't use --root with polymer.json - see https://github.com/Polymer/tools/issues/2616
 #Change current directory to the root folder
 cd $TEST_TMPDIR/
+cat polymer.json
+ls -l
+ls -l elements/
+ls -l elements/shared/
+ls -l elements/shared/gr-js-api-interface/
+ls -l elements/shared/gr-js-api-interface/gr-plugin-rest-api.js
 $DIR/$1 lint --verbose

e06fb71da13115e8903385dee25007f4797d96f9
Dhruv Srivastava <dhruvsri@google.com>
2021-03-04 14:31:52 +0100

    Allow comment context to take up remaining width
    After comment card has rendered, let comment context take up the
    remaining width available(up to maximum 1200 px).
    
    Change-Id: I1dab57df26b0fed98efead1a18083d9a504ed40f

--- polygerrit-ui/app/elements/shared/gr-comment-thread/gr-comment-thread.ts
+++ polygerrit-ui/app/elements/shared/gr-comment-thread/gr-comment-thread.ts
@@ -234,9 +234,11 @@ export class GrCommentThread extends KeyboardShortcutMixin(PolymerElement) {
       if (!prefs) return;
       this._prefs = {
         ...prefs,
-        // override explicitly so that diff doesn't take too much width
-        // compared to the context
-        line_wrapping: false,
+        // set line_wrapping to true so that the context can take all the
+        // remaining space after comment card has rendered
+        line_wrapping: true,
+        expand_diff_line: true, // allow the diff line to take the entire
+        // width of the comment context
       };
     });
     this._setInitialExpandedState();

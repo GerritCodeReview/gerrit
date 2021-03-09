c97ee803045f18983c5d5f4af004b060da1c3605
Mark Bekhet <mark.bekhet@ericsson.com>
2021-03-09 11:24:59 -0500

    Adjust NoShell command to return the command line
    Before this change, the channel wasn't closed inside the command, only
    the streams were closed. This caused an error because the channel was
    reported as not closed, in which case the out instance was used by other
    dependencies during the exit call. Therefore, the error log message was
    that the stream length=0, meaning it is already closed. As a result, the
    command hung until the user pressed any button.
    
    The error message can be reproduced using the Gerrit daemon with the
    option console-log.
    
    This change flushes the out instance and closes the channel before
    calling the exit method. The calls for channel methods are following the
    logic in [1], where an end of file is sent before closing the channel.
    
    A test is made in order to verify that there are no leaked resources. In
    the test, the NoShell command is triggered multiple times inside a loop.
    Before and after the loop, the number of open file descriptors is stored
    in variables. After the loop, a comparison between the two results is
    made. The result of this test is that there are no leaked resources.
    Also, the test checks if the command has terminated successfully.
    
    [1] https://apache.googlesource.com/mina-sshd/+/refs/tags/sshd-2.3.0/sshd-core/src/main/java/org/apache/sshd/server/channel/ChannelSession.java#871
    
    Bug: Issue 11142
    Change-Id: I14b173a6117ec2c5f75508c662345b8eee7e9828

---
+++ resources/com/google/gerrit/sshd/NoShell_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+generalMessage="
+./NoShell_test.sh process_id ssh_port user_name host_name number_of_times
+
+Compares the number of file descriptors before and after running the NoShell command
+toward the specified Gerrit process, for the given number of times. Example:
+
+./NoShell_test.sh 12345 29418 admin localhost 100
+"
+
+if [ "$#" != 5 ]; then
+  echo "$generalMessage"
+  exit 1
+fi
+
+pid="$1"
+port=$2
+user=$3
+host=$4
+times=$5
+
+fdCountBeforeLoop=$(find /proc/"$pid"/fd | wc -l)
+for ((iterations = 0; iterations < "$times"; iterations++)); do
+  ssh -p "$port" "$user"@"$host"
+done
+fdCountAfterLoop=$(find /proc/"$pid"/fd | wc -l)
+
+beforeFdCountMessage="File descriptor count before the loop: ${fdCountBeforeLoop}."
+afterFdCountMessage="File descriptor count after the loop: ${fdCountAfterLoop}."
+
+if [ "$fdCountBeforeLoop" == "$fdCountAfterLoop" ]; then
+  echo "The test is successful. The counts of file descriptors are equal."
+else
+  failureMessage="The test fails. The counts of file descriptors are different."
+  printf "%s\n%s\n%s\n" "${failureMessage}" "${beforeFdCountMessage}" "${afterFdCountMessage}"
+fi
+
+terminationStatus="$?"
+if [ "$terminationStatus" != 0 ]; then
+  echo "The last executed ssh command exited with non-zero status ${terminationStatus}."
+fi

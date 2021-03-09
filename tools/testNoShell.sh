4000b16d180e9d25089b4decf9f963c8dcbfd08d
Mark Bekhet <mark.bekhet@ericsson.com>
2021-03-09 11:24:59 -0500

    Adjust NoShell command to return the command line
    Before this change, the output stream and the error stream were closed
    at the end of the NoShell command. This caused an error as both streams
    are pointing to the output stream of the socket and the error log
    message was that the stream length=0, meaning it is already closed.
    Therefore, the command hung until the user pressed any button.
    
    The error message can be reproduced using the Gerrit daemon with the
    option console-log.
    
    This change closes only the err output stream instance. Closing the out
    instance produces the error.
    
    A test is made in order to verify that there is no leaked resources. In
    the test, the NoShell command is triggered multiple times inside a loop.
    Before and after the loop, the number of open file descriptors is stored
    in variables. After the loop, a comparison between the two results is
    made. The result of this test is that there is no leaked resources.
    
    Bug: Issue 11142
    Change-Id: I14b173a6117ec2c5f75508c662345b8eee7e9828

---
+++ tools/testNoShell.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+generalMessage="
+The test takes five parameters to be able to execute. The first
+parameter is the process that runs gerrit, this parameter helps to
+access the process directory and get the number of open file
+descriptors. The second parameter is the ssh port, the third parameter
+is the username and the last parameter is the host.
+The fifth parameter describes the number of times to trigger the command.
+"
+argsLength=$#
+
+if [ "$argsLength" == 5 ]
+then
+  processNumber="$1"
+  portNumber=$2
+  username=$3
+  host=$4
+  times=$5
+
+  firstResult=$(find /proc/"$processNumber"/fd| wc -l)
+  for ((iterations=0; iterations<"$times"; iterations++))
+   do
+     ssh -p "$portNumber" "$username"@"$host"
+   done
+  secondResult=$(find /proc/"$processNumber"/fd | wc -l)
+
+  firstResultMessage="The result of the first find command (before the loop) is ${firstResult}."
+  secondResultMessage="The result of the second find command (after the loop) is ${secondResult}."
+  echo "$firstResultMessage"
+  echo "$secondResultMessage"
+else
+  echo "$generalMessage"
+fi

91a71be0b0486a8e7cf6b8ff49f2c66f4f765082
Frank Borden <frankborden@google.com>
2021-03-10 20:56:57 -0800

    Convert gr-app_test to typescript
    exports added for  GrPluginHost and GrApp for typing.
    
    Change-Id: I58cbcd790db34dab0de3cfd966cf6ffe90ad8c12

---
+++ polygerrit-ui/app/elements/gr-app_test.ts
@@ -0,0 +1,106 @@
+/**
+ * @license
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import '../test/common-test-setup-karma.js';
+import {GrApp} from './gr-app.js';
+import {appContext} from '../services/app-context.js';
+import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+import {queryAndAssert, stubRestApi} from '../test/test-utils.js';
+import {
+  createAccountDetailWithId,
+  createPreferences,
+  createServerInfo,
+} from '../test/test-data-generators.js';
+import {GrAppElement} from './gr-app-element.js';
+import {GrPluginHost} from './plugins/gr-plugin-host/gr-plugin-host';
+import {ServerInfo} from '../types/common.js';
+import {GrRouter} from './core/gr-router/gr-router.js';
+import {GerritView} from '../services/router/router-model.js';
+import {
+  AppElementChangeViewParams,
+  AppElementSearchParam,
+} from './gr-app-types.js';
+
+const basicFixture = fixtureFromTemplate(html`<gr-app id="app"></gr-app>`);
+
+suite('gr-app tests', () => {
+  let element: GrApp;
+  let configStub: sinon.SinonStub;
+  let appStartedStub: sinon.SinonStub;
+
+  setup(done => {
+    appStartedStub = sinon.stub(appContext.reportingService, 'appStarted');
+    stub('gr-account-dropdown', {
+      _getTopContent: sinon.stub(),
+    });
+    stub('gr-router', {
+      start: sinon.stub(),
+    });
+
+    stubRestApi('getAccount').returns(
+      Promise.resolve(createAccountDetailWithId())
+    );
+    stubRestApi('getAccountCapabilities').returns(Promise.resolve({}));
+    configStub = stubRestApi('getConfig').returns(
+      Promise.resolve(createServerInfo())
+    );
+    stubRestApi('getPreferences').returns(Promise.resolve(createPreferences()));
+    stubRestApi('getVersion').returns(Promise.resolve('42'));
+    stubRestApi('probePath').returns(Promise.resolve(false));
+
+    element = basicFixture.instantiate() as GrApp;
+    flush(done);
+  });
+
+  const appElement = () =>
+    queryAndAssert<GrAppElement>(element, '#app-element');
+
+  test('reporting', () => {
+    assert.isTrue(appStartedStub.calledOnce);
+  });
+
+  test('reporting called before router start', () => {
+    const routerStartStub = queryAndAssert<GrRouter>(
+      queryAndAssert(element, '#app-element'),
+      'gr-router'
+    ).start as sinon.SinonStub;
+    sinon.assert.callOrder(appStartedStub, routerStartStub);
+  });
+
+  test('passes config to gr-plugin-host', () =>
+    configStub.lastCall.returnValue.then((config: ServerInfo) => {
+      assert.deepEqual(
+        queryAndAssert<GrPluginHost>(appElement(), 'gr-plugin-host').config,
+        config
+      );
+    }));
+
+  test('_paramsChanged sets search page', () => {
+    appElement()._paramsChanged({
+      path: '',
+      value: undefined,
+      base: {view: GerritView.CHANGE} as AppElementChangeViewParams,
+    });
+    assert.notOk(appElement()._lastSearchPage);
+    appElement()._paramsChanged({
+      path: '',
+      value: undefined,
+      base: {view: GerritView.SEARCH} as AppElementSearchParam,
+    });
+    assert.ok(appElement()._lastSearchPage);
+  });
+});

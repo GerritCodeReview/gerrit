fcff3996f3e836250bbd81db326a33655f006d11
Dhruv Srivastava <dhruvsri@google.com>
2021-03-16 21:16:45 +0100

    Navigate to previous unreviewed file when pressing "p"
    Allow pressing of "p" at the start of file to navigate to previous
    unreviewed file(similar to "n") after showing a toast.
    
    Change-Id: I310a2d417a8cf65d75b9de0ec9280e7f03386062

--- polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view_html.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view_html.ts
@@ -428,6 +428,7 @@ export const htmlTemplate = html`
   <gr-diff-cursor
     id="cursor"
     on-navigate-to-next-unreviewed-file="_navigateToNextUnreviewedFile"
+    on-navigate-to-previous-unreviewed-file="_navigateToPreviousUnreviewedFile"
     on-navigate-to-next-file-with-comments="_navigateToNextFileWithCommentThread"
   ></gr-diff-cursor>
   <gr-comment-api id="commentAPI"></gr-comment-api>

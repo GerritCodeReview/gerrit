1dff12d85b96c50a501d3bbdd02a24ba2c7481f3
Dhruv Srivastava <dhruvsri@google.com>
2021-03-18 13:49:28 +0100

    Use "yourself" for Attention Set removal tooltip
    AS Hovercard says "Your Turn" when it's your turn to take action.
    Use similar pronoun for removing yourself from the Attention set.
    
    Change-Id: I9cc8f71fa96a9bd6f17188e21be56abe3534b654

--- polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.ts
+++ polygerrit-ui/app/elements/shared/gr-account-label/gr-account-label.ts
@@ -297,8 +297,12 @@ export class GrAccountLabel extends PolymerElement {
       selfAccount,
       selected
     );
+    const removeFromASTooltip =
+      account._account_id === selfAccount._account_id
+        ? 'Click to remove yourself from the attention set'
+        : 'Click to remove the user from the attention set';
     return enabled
-      ? 'Click to remove the user from the attention set'
+      ? removeFromASTooltip
       : force
       ? 'Disabled. Use "Modify" to make changes.'
       : 'Disabled. Only involved users can change.';

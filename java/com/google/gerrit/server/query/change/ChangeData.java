GerritCodeReview/gerrit

commit faf830b3b80e970f517865039fb403281aa411ae

ChangeData: use the 'merge' variable
Change-Id: I9967951930e9ed3a3b9bb187401499103e1263f2

java/com/google/gerrit/server/query/change/ChangeData.java
@@ -600,6 +600,7 @@ public class ChangeData {
       author = c.getAuthorIdent();
       committer = c.getCommitterIdent();
       parentCount = c.getParentCount();
+      merge = parentCount > 0;
     } catch (IOException e) {
       throw new StorageException(
           String.format(
@@ -961,7 +962,7 @@ public class ChangeData {
         return null;
       }
     }
-    return parentCount > 1;
+    return merge;
   }
 
   public Set<Account.Id> editsByUser() {

commit d3acbc621cdc5f100f573fa531a80ea19acd6f25

Fix computation of ChangeData#merge
I broke this in commit faf830b3b80e970f517865039fb403281aa411ae
("ChangeData: use the 'merge' variable")

Add a test that covers this case.

Change-Id: I9cf06346946e9b45e7feae39d6bb83e06a246114

java/com/google/gerrit/server/query/change/ChangeData.java
@@ -607,7 +607,7 @@ public class ChangeData {
       author = c.getAuthorIdent();
       committer = c.getCommitterIdent();
       parentCount = c.getParentCount();
-      merge = parentCount > 0;
+      merge = parentCount > 1;
     } catch (IOException e) {
       throw new StorageException(
           String.format(

javatests/com/google/gerrit/server/query/change/AbstractQueryChangesTest.java
@@ -2370,21 +2370,24 @@ public abstract class AbstractQueryChangesTest extends GerritServerTests {
     TestRepository<Repo> repo = createProject("repo");
     RevCommit commit1 = repo.parseBody(repo.commit().add("file1", "contents1").create());
     RevCommit commit2 = repo.parseBody(repo.commit().add("file1", "contents2").create());
+    RevCommit commit3 =
+        repo.parseBody(repo.commit().parent(commit2).add("file1", "contents3").create());
     Change change1 = insert(repo, newChangeForCommit(repo, commit1));
     Change change2 = insert(repo, newChangeForCommit(repo, commit2));
+    Change change3 = insert(repo, newChangeForCommit(repo, commit3));
     RevCommit mergeCommit =
         repo.branch("master")
             .commit()
             .message("Merge commit")
             .parent(commit1)
-            .parent(commit2)
+            .parent(commit3)
             .insertChangeId()
             .create();
     Change mergeChange = insert(repo, newChangeForCommit(repo, mergeCommit));
 
     assertQuery("status:open is:merge", mergeChange);
-    assertQuery("status:open -is:merge", change2, change1);
-    assertQuery("status:open", mergeChange, change2, change1);
+    assertQuery("status:open -is:merge", change3, change2, change1);
+    assertQuery("status:open", mergeChange, change3, change2, change1);
   }
 
   @Test
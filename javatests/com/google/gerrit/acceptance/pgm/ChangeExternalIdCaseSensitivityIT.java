f676f7f60b3c76a6c37d190285c51b4b8f3be785
Thomas Draebing <thomas.draebing@sap.com>
2021-03-31 11:30:23 +0200

    ChangeExternalIdCaseSensitivity: Allow to migrate to case sensitive
    Change-Id: Ib844847f855937452dece4036a8ffaab0b1c359a

--- javatests/com/google/gerrit/acceptance/pgm/ChangeExternalIdCaseSensitivityIT.java
+++ javatests/com/google/gerrit/acceptance/pgm/ChangeExternalIdCaseSensitivityIT.java
@@ -70,6 +70,18 @@ public class ChangeExternalIdCaseSensitivityIT extends StandaloneSiteTest {
   }
 
   @Test
+  public void externalIdNoteNameIsMigratedToCaseSensitive() throws Exception {
+    prepareExternalIdNotes(true);
+
+    ctx.close();
+    runChangeExternalIdCaseSensitivity();
+    ctx = startServer();
+    extIdNotes = getExternalIdNotes(ctx);
+
+    assertExternalIdNotes(false);
+  }
+
+  @Test
   public void migrationFailsWithDuplicates() throws Exception {
     prepareExternalIdNotes(false);
     extIdNotes.insert(extIdFactory.create(SCHEME_USERNAME, "JohnDoe", Account.id(1)));
@@ -97,6 +109,9 @@ public class ChangeExternalIdCaseSensitivityIT extends StandaloneSiteTest {
     runChangeExternalIdCaseSensitivity();
     config.load();
     assertThat(config.getBoolean("auth", "userNameCaseInsensitive", false)).isTrue();
+    runChangeExternalIdCaseSensitivity();
+    config.load();
+    assertThat(config.getBoolean("auth", "userNameCaseInsensitive", false)).isFalse();
   }
 
   private void prepareExternalIdNotes(boolean userNameCaseInsensitive) throws Exception {

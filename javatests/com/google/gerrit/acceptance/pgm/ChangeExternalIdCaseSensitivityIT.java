ff6c0d81eadce03b8c464398d736d82607150005
Thomas Draebing <thomas.draebing@sap.com>
2021-03-31 12:10:31 +0200

    ChangeExternalIdCaseSensitivity: Add --dryrun option
    Change-Id: Ie14db76493b39ea1a4028daedc4317909a851e1c

--- javatests/com/google/gerrit/acceptance/pgm/ChangeExternalIdCaseSensitivityIT.java
+++ javatests/com/google/gerrit/acceptance/pgm/ChangeExternalIdCaseSensitivityIT.java
@@ -119,6 +119,19 @@ public class ChangeExternalIdCaseSensitivityIT extends StandaloneSiteTest {
     assertThat(config.getBoolean("auth", "userNameCaseInsensitive", false)).isFalse();
   }
 
+  @Test
+  public void dryrunDoesNotPersistChanges() throws Exception {
+    prepareExternalIdNotes(CASE_SENSITIVE);
+    ctx.close();
+    runGerrit("ChangeExternalIdCaseSensitivity", "-d", sitePaths.site_path.toString(), "--dryrun");
+
+    config.load();
+    assertThat(config.getBoolean("auth", "userNameCaseInsensitive", false)).isFalse();
+
+    ctx = startServer();
+    assertExternalIdNotes(CASE_SENSITIVE);
+  }
+
   private void prepareExternalIdNotes(boolean userNameCaseInsensitive) throws Exception {
     configureUserNameCaseInsensitive(userNameCaseInsensitive);
     initSite();

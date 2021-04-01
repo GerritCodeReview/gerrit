6051236dea544592d421c76ef34ab73059fda529
Marija Savtchouk <mariasavtchouk@google.com>
2021-04-01 09:47:54 +0100

    Do not commit detailed user info on DeleteVote
    Change-Id: Ie64287eece91db785b1b2c4811f40aa552a1ac23

--- javatests/com/google/gerrit/acceptance/api/change/ChangeIT.java
+++ javatests/com/google/gerrit/acceptance/api/change/ChangeIT.java
@@ -2591,7 +2591,7 @@ public class ChangeIT extends AbstractDaemonTest {
 
     ChangeMessageInfo message = Iterables.getLast(c.messages);
     assertThat(message.author._accountId).isEqualTo(admin.id().get());
-    assertThat(message.message).isEqualTo("Removed Code-Review+1 by User <user@example.com>\n");
+    assertThat(message.message).isEqualTo("Removed Code-Review+1");
     assertThat(getReviewers(c.reviewers.get(REVIEWER)))
         .containsExactlyElementsIn(ImmutableSet.of(admin.id(), user.id()));
   }

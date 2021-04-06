8c6f8353e9a13a40030c548646eb1ce09f614d24
Mark Bekhet <mark.bekhet@ericsson.com>
2021-04-06 15:01:42 -0400

    Adapt SendMessage of the NoShell command to AsyncCommand type
    Before this change, the SendMessage class inside the NoShell command
    implemented the Command interface. Therefore, the command was using
    instances of OutputStream and closing them at the end of the command.
    Given that the channel isn't closed at this stage, this produced a
    problem as apache-sshd calls the flush method of one of the output
    streams. Therefore, the command was hanging and didn't release the
    terminal until the user pressed a key.
    
    This change adapts the SendMessage class to an AyncCommand type.
    Therefore, the Apache library treats the command as AsyncCommand and
    uses the newly introduced I/O streams instead of normal streams. As a
    result, the problematic flush call of the output stream doesn't happen,
    because the normal output stream is not initialized, instead the I/O
    streams are initialized and used.
    
    A test is made in order to verify that there are no leaked resources. In
    the test, the NoShell command is triggered multiple times inside a loop.
    Before and after the loop, the number of open file descriptors is stored
    in variables. After the loop, a comparison between the two results is
    made. Leaked resources can happen if the command doesn't properly close
    all streams.
    
    Bug: Issue 11142
    Change-Id: Ia6ed0d4ee264d2e901eaa17ea444bf715e3b44db

---
+++ resources/com/google/gerrit/sshd/NoShell_test.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Compares the number of file descriptors before and after running the NoShell command
+# toward the specified Gerrit process, for the given number of times. A user can run
+# this script to make sure that the NoShell command doesn't leave any open resources
+# after it ends. It also verifies that the command doesn't time out.
+
+usage="
+./NoShell_test.sh process_id ssh_port user_name host_name number_of_times
+Example:
+./NoShell_test.sh 12345 29418 admin localhost 100
+"
+
+pid=$1
+port=$2
+user=$3
+host=$4
+times=$5
+
+if [ "$#" != 5 ]; then
+  echo "$usage"
+  exit 1
+fi
+
+pidCommand=$(ps -p "${pid}" -o command)
+if [[ $pidCommand != *"gerrit.war"* ]]; then
+  echo "process_id should be an up and running Gerrit process id (pid)."
+  exit 1
+fi
+
+fdBefore=$(lsof -p "$pid" | wc -l)
+
+messageReceived=$({ timeout --foreground 30s ssh -p "$port" "$user"@"$host"; } 2>&1)
+sshErrorMessage="
+There was an error in the ssh connection to the site.
+Make sure that the ssh key is properly registered in Gerrit, that ssh_port
+is right and that the host is registered as a known host. For the latter,
+this command can be used:
+ssh-keyscan -t rsa -p 29418 localhost >> ~/.ssh/known_hosts
+-More:
+https://gerrit-review.googlesource.com/Documentation/user-upload.html#ssh"
+
+if [[ "$messageReceived" != *"successfully connected"* ]]; then
+  echo "$sshErrorMessage"
+  exit 1
+fi
+
+timeoutError="The test fails. The connection has timed out."
+connectionTimeoutSuccess="The connection didn't time out.
+Therefore, it passed the first verification."
+
+if [[ "$messageReceived" != *"closed"* ]]; then
+  echo "$timeoutError"
+  exit 1
+else
+  echo "$connectionTimeoutSuccess"
+fi
+
+for ((iterations = 1; iterations < "$times"; iterations++)); do
+  ssh -p "$port" "$user"@"$host" &>/dev/null
+done
+
+fdAfter=$(lsof -p "$pid" | wc -l)
+fdDifference=$((fdAfter - fdBefore))
+
+beforeFdCountMessage="File descriptor count before the loop: ${fdBefore}."
+afterFdCountMessage="File descriptor count after the loop: ${fdAfter}."
+
+if [ "$fdDifference" -le 0 ]; then
+  echo "The test is successful. The count of file descriptors didn't increase."
+else
+  failureMessage="The test fails. The count of file descriptors increased by ${fdDifference}."
+  failureNote="-Note that something else from the application might have caused this increase."
+  printf "%s\n" "${failureMessage}" "${failureNote}" \
+    "${beforeFdCountMessage}" "${afterFdCountMessage}"
+fi

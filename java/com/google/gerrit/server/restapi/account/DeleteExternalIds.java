33bfeb9ae893bb7affe9301223af4cc28e88c838
Matthias Sohn <matthias.sohn@sap.com>
2021-04-08 14:58:50 +0200

    Allow to delete externalIds also in scheme username
    In order to cleanup duplicate accounts which only differ in case we need
    a way to delete externalIds in scheme username. This is needed for the
    implementation of case-insensitive user matching [1].
    
    Since the user may not be able to login anymore, after the removal of
    the external id with scheme 'username:' require capability 'Administrate
    Server' for deleting external ids in this scheme.
    
    [1] https://gerrit-review.googlesource.com/c/homepage/+/300825
    
    Change-Id: I61f52486e1b91e29b2063eeffe3d739702480dbd

--- java/com/google/gerrit/server/restapi/account/DeleteExternalIds.java
+++ java/com/google/gerrit/server/restapi/account/DeleteExternalIds.java
@@ -98,8 +98,10 @@ public class DeleteExternalIds implements RestModifyView<AccountResource, List<S
             String.format("External id %s does not exist", externalIdStr));
       }
 
-      if ((!id.isScheme(SCHEME_USERNAME))
-          && (!last.isPresent() || (!last.get().equals(id.key())))) {
+      if (!last.isPresent() || !last.get().equals(id.key())) {
+        if (id.isScheme(SCHEME_USERNAME)) {
+          permissionBackend.currentUser().check(GlobalPermission.ADMINISTRATE_SERVER);
+        }
         toDelete.add(id);
       } else {
         throw new ResourceConflictException(
--- javatests/com/google/gerrit/acceptance/rest/account/ExternalIdIT.java
+++ javatests/com/google/gerrit/acceptance/rest/account/ExternalIdIT.java
@@ -265,14 +265,28 @@ public class ExternalIdIT extends AbstractDaemonTest {
   }
 
   @Test
-  public void deleteExternalIds_Conflict() throws Exception {
+  public void deleteExternalIdOfUsernameByUserForbidden() throws Exception {
     List<String> toDelete = new ArrayList<>();
     String externalIdStr = "username:" + user.username();
     toDelete.add(externalIdStr);
     RestResponse response = userRestSession.post("/accounts/self/external.ids:delete", toDelete);
-    response.assertConflict();
-    assertThat(response.getEntityContent())
-        .isEqualTo(String.format("External id %s cannot be deleted", externalIdStr));
+    response.assertForbidden();
+  }
+
+  @Test
+  public void deleteExternalIdOfUsernameByAdmin() throws Exception {
+    projectOperations
+        .allProjectsForUpdate()
+        .add(allowCapability(GlobalCapability.ADMINISTRATE_SERVER).group(REGISTERED_USERS))
+        .update();
+    List<String> toDelete = new ArrayList<>();
+    String externalIdStr = "username:" + user.username();
+    toDelete.add(externalIdStr);
+    RestResponse response = userRestSession.post("/accounts/self/external.ids:delete", toDelete);
+    response.assertNoContent();
+    List<AccountExternalIdInfo> results = gApi.accounts().id(user.id().get()).getExternalIds();
+    assertThat(results).hasSize(1);
+    assertThat(results.get(0).identity).isEqualTo("mailto:user@example.com");
   }
 
   @Test

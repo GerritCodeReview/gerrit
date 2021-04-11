dc32e224286f380c7559517298f94a9d451478cc
Paladox none <thomasmulhall410@yahoo.com>
2021-04-11 18:49:06 +0000

    Add web links to project:<project>
    Requsted on [1].
    
    [1] https://groups.google.com/g/repo-discuss/c/hbvSHHM8sVQ
    
    Change-Id: Iefda4bb5571d917d5fa9f24c02c8e4a3be2c6c52

--- polygerrit-ui/app/elements/change-list/gr-repo-header/gr-repo-header.ts
+++ polygerrit-ui/app/elements/change-list/gr-repo-header/gr-repo-header.ts
@@ -23,6 +23,8 @@ import {htmlTemplate} from './gr-repo-header_html';
 import {GerritNav} from '../../core/gr-navigation/gr-navigation';
 import {customElement, property} from '@polymer/decorators';
 import {RepoName} from '../../../types/common';
+import {appContext} from '../../../services/app-context';
+import {WebLinkInfo} from '../../../types/diff';
 
 @customElement('gr-repo-header')
 class GrRepoHeader extends PolymerElement {
@@ -33,15 +35,26 @@ class GrRepoHeader extends PolymerElement {
   @property({type: String, observer: '_repoChanged'})
   repo?: string;
 
+  @property({type: Array})
+  webLinks: WebLinkInfo[] = [];
+
   @property({type: String})
   _repoUrl: string | null = null;
+
+  private readonly restApiService = appContext.restApiService;
 
   _repoChanged(repoName: RepoName) {
     if (!repoName) {
       this._repoUrl = null;
       return;
     }
+
     this._repoUrl = GerritNav.getUrlForRepo(repoName);
+
+    this.restApiService.getRepo(repoName).then(repo => {
+      if (!repo?.web_links) return;
+      this.webLinks = repo.web_links;
+    });
   }
 }
 

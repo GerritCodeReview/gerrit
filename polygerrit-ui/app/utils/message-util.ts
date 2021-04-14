71f2951f3daa340b8bd079ec1b5bd11e0cf32964
Dhruv Srivastava <dhruvsri@google.com>
2021-04-14 18:23:00 +0200

    Show Revert Submitted chip if change revert was submitted
    Change-Id: Ia938b6ab7e15c870a4a1f4d7d0fcb3b7c57a99d8

--- polygerrit-ui/app/utils/message-util.ts
+++ polygerrit-ui/app/utils/message-util.ts
@@ -15,18 +15,39 @@
  * limitations under the License.
  */
 
-import {MessageTag} from '../constants/constants';
+import {ChangeStatus, MessageTag} from '../constants/constants';
+import {RestApiService} from '../services/gr-rest-api/gr-rest-api';
 import {ChangeMessageInfo} from '../types/common';
+import {ParsedChangeInfo} from '../types/types';
 
-export function getRevertCommitHash(messages?: ChangeMessageInfo[]) {
-  const msg = messages?.find(m => m.tag === MessageTag.TAG_REVERT);
-  if (!msg) return undefined;
+function getCommitFromMessage(msg: ChangeMessageInfo) {
   const REVERT_REGEX = /^Created a revert of this change as (.*)$/;
   const commit = msg.message.match(REVERT_REGEX)?.[1];
   if (!commit) throw new Error('revert commit not found');
   return commit;
 }
 
+export function getRevertCommitHash(messages?: ChangeMessageInfo[]) {
+  const msg = messages?.find(m => m.tag === MessageTag.TAG_REVERT);
+  if (!msg) return undefined;
+  return getCommitFromMessage(msg);
+}
+
 export function isRevertCreated(messages?: ChangeMessageInfo[]) {
   return messages?.some(m => m.tag === MessageTag.TAG_REVERT);
+}
+
+export function isRevertSubmitted(
+  messages: ChangeMessageInfo[],
+  restAPI: RestApiService
+) {
+  const revertMessages = messages?.filter(m => m.tag === MessageTag.TAG_REVERT);
+  const promises: Promise<ParsedChangeInfo | undefined | null>[] = [];
+  revertMessages.forEach(revertMessage => {
+    const commit = getCommitFromMessage(revertMessage);
+    promises.push(restAPI.getChangeDetail(commit));
+  });
+  return Promise.all(promises).then(changes => {
+    return changes.some(change => change?.status === ChangeStatus.MERGED);
+  });
 }

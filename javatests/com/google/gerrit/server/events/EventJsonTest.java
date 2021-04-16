45cfbf47ba7cc5b5d3fb7a17a06bae13ccb8f053
David Ostrovsky <david@ostrovsky.org>
2021-04-16 07:50:35 +0200

    EventJsonTest: Add test for custom event serialization
    Change-Id: I7e45f77aa980c03896e3891c2d48680e31d2e0a6

--- javatests/com/google/gerrit/server/events/EventJsonTest.java
+++ javatests/com/google/gerrit/server/events/EventJsonTest.java
@@ -52,9 +52,30 @@ public class EventJsonTest {
 
   private final Gson gson = new EventGsonProvider().get();
 
+  static class CustomEvent extends Event {
+    static String TYPE = "custom-type";
+
+    public String customField;
+
+    protected CustomEvent() {
+      super(TYPE);
+    }
+  }
+
   @Before
   public void setTimeForTesting() {
     TestTimeUtil.resetWithClockStep(1, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void customEvent() {
+    EventTypes.register(CustomEvent.TYPE, CustomEvent.class);
+    CustomEvent event = new CustomEvent();
+    event.customField = "customValue";
+    String json = gson.toJson(event);
+    CustomEvent resullt = gson.fromJson(json, CustomEvent.class);
+    assertThat(resullt.type).isEqualTo(CustomEvent.TYPE);
+    assertThat(resullt.customField).isEqualTo(event.customField);
   }
 
   @Test

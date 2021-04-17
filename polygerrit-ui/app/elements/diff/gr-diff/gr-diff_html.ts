10f003cb4c4951d80c029dc95298d9b30bb81205
Paladox none <thomasmulhall410@yahoo.com>
2021-04-17 14:56:03 +0000

    Fix diff view on mobile
    Removing height fixed an issue where by the diff
    couldn't be copied on safari (including on mobile).
    
    Also fixes an issue where the buttons were out of place.
    
    Change-Id: Ifd3f38ad548e48e1dae6d8d1a847c5162d148cbf

--- polygerrit-ui/app/elements/diff/gr-diff/gr-diff_html.ts
+++ polygerrit-ui/app/elements/diff/gr-diff/gr-diff_html.ts
@@ -337,7 +337,6 @@ export const htmlTemplate = html`
     }
     .dividerCell {
       width: 100%;
-      height: 100%;
       display: flex;
       justify-content: center;
       position: absolute;

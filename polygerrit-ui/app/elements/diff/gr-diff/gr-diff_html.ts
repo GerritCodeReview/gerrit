fdea988ddb9792b7e971ebcbc0ac65d9fee9c53b
Hermann Loose <hermannloose@google.com>
2021-04-19 10:15:49 +0200

    Fix white paper-card in dark mode
    Given dark mode is in alpha, I picked something that seems like
    a reasonable default for now. Background color for paper-card is
    internally set as:
    
    var(--paper-card-background-color, var(--primary-background-color))
    
    This leaves open the option of styling it explicitly when using gr-diff
    in isolation and probably also provides a sensible fallback for other
    components.
    
    Change-Id: I8f9128879b30b6d5c5fa12410269b0fff3e4bc2a

--- polygerrit-ui/app/elements/diff/gr-diff/gr-diff_html.ts
+++ polygerrit-ui/app/elements/diff/gr-diff/gr-diff_html.ts
@@ -109,6 +109,14 @@ export const htmlTemplate = html`
       height: 100%;
       max-width: var(--image-viewer-max-width, 95vw);
       max-height: var(--image-viewer-max-height, 90vh);
+      /* 
+        Defined by paper-styles default-theme and used in various components.
+        background-color-secondary is a compromise between fairly light in
+        light theme (where we ideally would want background-color-primary) yet
+        slightly offset against the app background in dark mode, where drop
+        shadows e.g. around paper-card are almost invisible.
+        */
+      --primary-background-color: var(--background-color-secondary);
     }
     .image-diff .gr-diff {
       text-align: center;

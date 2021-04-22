56059b2182581e4776750656e513eb8fb04d8d8a
Marija Savtchouk <mariasavtchouk@google.com>
2021-04-22 16:59:04 +0100

    Backfill NoteDB history by rewriting commits that contain user data
    Added a class that rewrites commit history of change meta branch for
    the cases where we used to put user data in change updates.
    The commits are fixed case by case, and only for previously discovered
    cases (can be found at
    https://gerrit-review.googlesource.com/q/hashtag:user-data-cleanup).
    
    This is currently scoped to some of the NoteDb footers, commit messages
    and commit author.
    The goal is to keep the commits same as before, and only replace the
    parts of commits that are known to contain user data.
    This approach was chosen so that the rewriter does not accidentally
    break changes. Because
    user details of original accounts could have changed since the commit
    was written, ChangeMessages are constructed from footers, if possible,
    otherwise just cut or droped.
    
    Optionally, the rewriter verifies the meta branches do not contain user
    data of accounts, associated with the change. Only the accounts that are
    linked to the change are used, to make the verification less expensive.
    While the verification is not comprehensive, it can help to detect
    missed cases.
    
    This is still WIP, I only added tests and fixes for some cases, to adjust
    based on early feedback.
    This can be initially run in dry-run, and the remaining cases added in a
    separate change.
    
    Change-Id: Ia850ba533f4d33ca61cdb64acb6ebc166aa2af37

---
+++ java/com/google/gerrit/server/project/NullProjectCache.java
@@ -0,0 +1,83 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.project;
+
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.gerrit.entities.AccountGroup.UUID;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.entities.Project.NameKey;
+import com.google.gerrit.exceptions.StorageException;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Set;
+
+/** An implementation of {@link ProjectCache} with no operations implemented. */
+public class NullProjectCache implements ProjectCache {
+
+  @Override
+  public ProjectState getAllProjects() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ProjectState getAllUsers() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Optional<ProjectState> get(NameKey projectName) throws StorageException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void evict(Project p) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void evict(NameKey p) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void remove(Project p) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void remove(NameKey name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ImmutableSortedSet<NameKey> all() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Set<UUID> guessRelevantGroupUUIDs() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ImmutableSortedSet<NameKey> byName(String prefix) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void onCreateProject(NameKey newProjectName) throws IOException {
+    throw new UnsupportedOperationException();
+  }
+}

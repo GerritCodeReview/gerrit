3561ea71681d46fcb63a29c9f0008094377f00c4
Ole Rehmsen <oler@google.com>
2021-04-23 12:01:22 +0200

    Expose the TokenHighlightLayer to embedders
    By default, gr-diff does not turn this layer on, but embedding clients
    can pass an instance of it in through the `layers` property.
    
    Change-Id: Ic2f288a2733364614c4c2db3d640cddeed636cde

--- polygerrit-ui/app/embed/gr-diff.ts
+++ polygerrit-ui/app/embed/gr-diff.ts
@@ -27,6 +27,7 @@ import '../elements/diff/gr-diff/gr-diff';
 import '../elements/diff/gr-diff-cursor/gr-diff-cursor';
 import {initDiffAppContext} from './gr-diff-app-context-init';
 import {GrAnnotation} from '../elements/diff/gr-diff-highlight/gr-annotation';
+import {TokenHighlightLayer} from '../elements/diff/gr-diff-builder/token-highlight-layer';
 
 // Setup appContext for diff.
 // TODO (dmfilippov): find a better solution
@@ -34,6 +35,7 @@ initDiffAppContext();
 // Setup global variables for existing usages of this component
 window.grdiff = {
   GrAnnotation,
+  TokenHighlightLayer,
 };
 
 // TODO(oler): Remove when clients have adjusted to namespaced globals above

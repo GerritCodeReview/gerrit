36b77355c955f009a5cb1b96c7552ef94cde61f1
Paladox none <thomasmulhall410@yahoo.com>
2021-04-25 15:59:52 +0000

    Fix gr-registration-dialog
    Change-Id: I2a8a5af6495cb43de09fd75a17445e3e8285c9e5

--- polygerrit-ui/app/elements/settings/gr-registration-dialog/gr-registration-dialog_html.ts
+++ polygerrit-ui/app/elements/settings/gr-registration-dialog/gr-registration-dialog_html.ts
@@ -59,9 +59,6 @@ export const htmlTemplate = html`
     input {
       width: 20em;
     }
-    section.hide {
-      display: none;
-    }
   </style>
   <div class="container gr-form-styles">
     <header>Please confirm your contact information</header>
@@ -75,35 +72,43 @@ export const htmlTemplate = html`
       </p>
       <hr />
       <section>
-        <div class="title">Full Name</div>
-        <iron-input bind-value="{{_account.name}}">
-          <input
-            is="iron-input"
-            id="name"
-            bind-value="{{_account.name}}"
-            disabled="[[_saving]]"
-          />
-        </iron-input>
-      </section>
-      <section class$="[[_computeUsernameClass(_usernameMutable)]]">
-        <div class="title">Username</div>
-        <iron-input bind-value="{{_account.username}}">
-          <input
-            is="iron-input"
-            id="username"
-            bind-value="{{_account.username}}"
-            disabled="[[_saving]]"
-          />
-        </iron-input>
+        <span class="title">Full Name</span>
+        <span class="value">
+          <iron-input bind-value="{{_account.name}}">
+            <input
+              is="iron-input"
+              id="name"
+              bind-value="{{_account.name}}"
+              disabled="[[_saving]]"
+            />
+          </iron-input>
+        </span>
       </section>
       <section>
-        <div class="title">Preferred Email</div>
-        <select id="email" disabled="[[_saving]]">
-          <option value="[[_account.email]]">[[_account.email]]</option>
-          <template is="dom-repeat" items="[[_account.secondary_emails]]">
-            <option value="[[item]]">[[item]]</option>
-          </template>
-        </select>
+        <span class="title">Username</span>
+        <span hidden$="[[usernameMutable]]" class="value">[[_username]]</span>
+        <span hidden$="[[!usernameMutable]]" class="value">
+          <iron-input
+            bind-value="{{_username}}"
+          >
+            <input
+              is="iron-input"
+              id="usernameInput"
+              disabled="[[_saving]]"
+            />
+          </iron-input>
+        </span>
+      </section>
+      <section>
+        <span class="title">Preferred Email</span>
+        <span class="value">
+          <select id="email" disabled="[[_saving]]">
+            <option value="[[_account.email]]">[[_account.email]]</option>
+            <template is="dom-repeat" items="[[_account.secondary_emails]]">
+              <option value="[[item]]">[[item]]</option>
+            </template>
+          </select>
+        </span>
       </section>
       <hr />
       <p>
@@ -123,7 +128,7 @@ export const htmlTemplate = html`
         id="saveButton"
         primary=""
         link=""
-        disabled="[[_computeSaveDisabled(_account.name, _account.email, _saving)]]"
+        disabled="[[_computeSaveDisabled(_account.name, _username, _account.email, _saving)]]"
         on-click="_handleSave"
         >Save</gr-button
       >

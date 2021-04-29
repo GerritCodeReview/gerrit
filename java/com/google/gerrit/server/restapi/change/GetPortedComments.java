702db687fb58963cfa023592d1d544a7ae74e737
Youssef Elghareeb <ghareeb@google.com>
2021-04-29 21:31:56 +0200

    Add an endpoint to request ported comments for a specific file path
    The existing List Ported Comments endpoint ports all existing comments
    on previous patchsets to a target patchset. This computation has some
    redundancy if callers are only interested in ported comments for a
    specific file path. One such caller is the diff view of Gerrit's
    frontend, because it displays only comments that are written for a
    specific file path.
    
    This endpoint filters only comments written on that specific file path
    before passing them to the comment porter. One caveat in the new
    endpoint is that it doesn't handle rename detections, i.e. if a file is
    renamed and we request ported comments using the new file path, then any
    comments written on the old file path in any of the previous patchsets
    will not be returned.
    
    Change-Id: I2c109fd57ec35a0badcac80b51d85d02eaba9df3

--- java/com/google/gerrit/server/restapi/change/GetPortedComments.java
+++ java/com/google/gerrit/server/restapi/change/GetPortedComments.java
@@ -1,2 +1,65 @@
-package com.google.gerrit.server.restapi.change;public class GetPortedComments {
+package com.google.gerrit.server.restapi.change;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.entities.HumanComment;
+import com.google.gerrit.entities.PatchSet;
+import com.google.gerrit.extensions.common.CommentInfo;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.CommentsUtil;
+import com.google.gerrit.server.change.FileResource;
+import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import java.util.List;
+import java.util.Map;
+
+/** Computes the ported comments for a specific file path. */
+@Singleton
+public class GetPortedComments implements RestReadView<FileResource> {
+  private final CommentsUtil commentsUtil;
+  private final CommentPorter commentPorter;
+  private final Provider<CommentJson> commentJson;
+
+  @Inject
+  public GetPortedComments(
+      Provider<CommentJson> commentJson, CommentsUtil commentsUtil, CommentPorter commentPorter) {
+    this.commentJson = commentJson;
+    this.commentsUtil = commentsUtil;
+    this.commentPorter = commentPorter;
+  }
+
+  @Override
+  public Response<Map<String, List<CommentInfo>>> apply(FileResource fileResource)
+      throws PermissionBackendException {
+    PatchSet targetPatchset = fileResource.getRevision().getPatchSet();
+    ChangeNotes notes = fileResource.getRevision().getNotes();
+
+    List<HumanComment> allComments = commentsUtil.publishedHumanCommentsByChange(notes);
+
+    // Filter comments for a specific file path
+    allComments =
+        allComments.stream()
+            .filter(c -> c.key.filename.equals(fileResource.getPatchKey().fileName()))
+            .collect(toImmutableList());
+
+    ImmutableList<HumanComment> portedComments =
+        commentPorter.portComments(
+            notes, targetPatchset, allComments, ImmutableList.of(new UnresolvedCommentFilter()));
+    return Response.ok(format(portedComments));
+  }
+
+  private Map<String, List<CommentInfo>> format(List<HumanComment> comments)
+      throws PermissionBackendException {
+    return commentJson
+        .get()
+        .setFillAccounts(true)
+        .setFillPatchSet(true)
+        .newHumanCommentFormatter()
+        .format(comments);
+  }
 }

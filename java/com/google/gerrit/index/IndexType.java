04c3acd33a60cc0e17a3fc224f549d0c947818b3
Patrick Hiesel <hiesel@google.com>
2021-04-30 15:49:12 +0200

    WiP: Wire up Fake index in integration tests
    This change will enable the open-source project to upgrade Lucene without
    requiring a Library-Compliance vote. It allows googlesource.com to drop
    the dependency on Lucene completely.
    
    TODO: Make index backend configurable in tests.
    
    Change-Id: Ic6ebb6587cb8ab0979a0f00d5e3e340d27115c89

--- java/com/google/gerrit/index/IndexType.java
+++ java/com/google/gerrit/index/IndexType.java
@@ -14,8 +14,12 @@
 
 package com.google.gerrit.index;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.common.Nullable;
+import java.util.Optional;
 
 /**
  * Index types supported by the secondary index.
@@ -32,7 +36,37 @@ public class IndexType {
   private static final String ELASTICSEARCH = "elasticsearch";
   private static final String FAKE = "fake";
 
+  private static final String ENV_VAR = "GERRIT_INDEX_TYPE";
+  private static final String SYS_PROP = "gerrit.index.type";
+
   private final String type;
+
+  /**
+   * Returns the index type in case it was set by an environment variable. This is useful to run
+   * tests against a certain index backend.
+   */
+  public static Optional<IndexType> fromEnvironment() {
+    String value = System.getenv(ENV_VAR);
+    if (Strings.isNullOrEmpty(value)) {
+      value = System.getProperty(SYS_PROP);
+    }
+    if (Strings.isNullOrEmpty(value)) {
+      return Optional.empty();
+    }
+    value = value.toUpperCase().replace("-", "_");
+    IndexType type = new IndexType(value);
+    if (!Strings.isNullOrEmpty(System.getenv(ENV_VAR))) {
+      checkArgument(
+          type != null, "Invalid value for env variable %s: %s", ENV_VAR, System.getenv(ENV_VAR));
+    } else {
+      checkArgument(
+          type != null,
+          "Invalid value for system property %s: %s",
+          SYS_PROP,
+          System.getProperty(SYS_PROP));
+    }
+    return Optional.of(type);
+  }
 
   public IndexType(@Nullable String type) {
     this.type = type == null ? getDefault() : type.toLowerCase();

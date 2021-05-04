c98e6099b09aac0ddfcb7e8e857d387e011c690d
Antoine Musso <hashar@free.fr>
2021-05-04 11:14:05 +0200

    download_file: honor XDG_CACHE_HOME
    When building a plugin as user `nobody`, download_file is unable to
    write the artifacts to the cache since the user does not have a home
    directory.  For a CI build I also need to be set the cache directory to
    a predetermined value.
    
    Instead of hardcoding `$HOME/.gerritcodereview` to store materials,
    switch to use the FreeDesktop XDG Base Directory Specificiation:
    https: //specifications.freedesktop.org/basedir-spec/latest/
    
    The specification adds an extra layer of directories to split the
    application materaisl by type (cache, config, data) which can be set via
    an environment variable.
    
    Respect `XDG_CACHE_HOME` and use a non dot directory under it matching
    the application name. So if it is set to `/cache` files get written to
    `/cache/gerritcodereview/bazel-cache/downloaded-artifacts`.
    
    When the environment variable is not set, behavior is unchanged and
    cache files are written under `~/.gerritcodereview`.  The spec
    recommends using `$HOME/.cache` but that would be inconsistent with the
    Bazel build cache which are set to use `~/.gerritcodereview`.
    
    The `local.properties` file should probably relies on `XDG_CONFIG_HOME`
    (default to `$HOME/.config`), but I can't tell what it could break.
    
    Update documentation to mention support for `XDG_CACHE_HOME`. While at
    it, explains how to override the bazel repository and disk cache, would
    have same a bit of time the first time I had to tweak them.
    
    Change-Id: Ie4fac83928527e0e71b159b9500983234c2261ac

--- tools/download_file.py
+++ tools/download_file.py
@@ -17,7 +17,7 @@ from __future__ import print_function
 
 import argparse
 from hashlib import sha1
-from os import link, makedirs, path, remove
+from os import environ, link, makedirs, path, remove
 import shutil
 from subprocess import check_call, CalledProcessError
 from sys import stderr
@@ -25,7 +25,14 @@ from util import hash_file, resolve_url
 from zipfile import ZipFile, BadZipfile, LargeZipFile
 
 GERRIT_HOME = path.expanduser('~/.gerritcodereview')
-CACHE_DIR = path.join(GERRIT_HOME, 'bazel-cache', 'downloaded-artifacts')
+
+if 'XDG_CACHE_HOME' in environ:
+    CACHE_DIR = path.join(
+        environ.get('XDG_CACHE_HOME'),
+        'gerritcodereview', 'bazel-cache', 'downloaded-artifacts')
+else:
+    CACHE_DIR = path.join(GERRIT_HOME, 'bazel-cache', 'downloaded-artifacts')
+
 LOCAL_PROPERTIES = 'local.properties'
 
 

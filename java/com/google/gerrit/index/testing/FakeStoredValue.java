3fb9d7469a3c6d607cda27831091c08e4ff03519
Patrick Hiesel <hiesel@google.com>
2021-05-10 16:47:33 +0200

    Implement a FakeIndex for testing
    The open-source Gerrit project wants to upgrade from Lucene 6
    to Lucene 8. That upgrade is currently blocked by Google being
    stuck on Lucene 6 and the library compliance policy enforces
    that we must have the same library versions deployed on
    googlesource.com that we keep in the open-source project.
    
    The last upgrade of Lucene I did internally took me over a week
    of engineering time which is hard to justify when there aren't
    strong reasons to upgrade (i.e. just wanting to be current vs
    security fixes).
    
    From a code-health perspective, we have a desire to clearly
    separate the lucene index as one specific index implementation
    from the generic index interface that we have.
    
    We have great Query tests for each index backend, that is Lucene
    and Elastic Search in open-source and a prorietary index on
    googlesource.com. There is no need to test all integration tests
    using Lucene. In fact, I think it's better to test it against a
    fake index implementation that implements our generic interface
    exactly as specified by the API contract.
    
    This commit implements a Fake, in-memory index to be used in
    tests. It adds QueryTests for the fake index but does not yet
    wire it up in integration tests.
    
    Query tests are using assume() and @Ingore statements to signal
    that the existing Predicates have bugs or other shortcomings. We
    did that because we wanted to test the current state of the
    system and only then fix bugs one-by-one.
    
    The fake index uses calls #match methods on predicates which
    has the nice side-effect that through running Query tests on
    the fake index we get test coverage for all #match
    implementations that have a test case in Abstract*QueryTest.
    
    Change-Id: Id3c1d3d85a783190fae94f9802bc9223afd8d3b8

---
+++ java/com/google/gerrit/index/testing/FakeStoredValue.java
@@ -0,0 +1,76 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.index.testing;
+
+import com.google.gerrit.index.StoredValue;
+import java.sql.Timestamp;
+
+/** Bridge to recover fields from the fake index. */
+public class FakeStoredValue implements StoredValue {
+  private final Object field;
+
+  FakeStoredValue(Object field) {
+    this.field = field;
+  }
+
+  @Override
+  public String asString() {
+    return (String) field;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Iterable<String> asStrings() {
+    return (Iterable<String>) field;
+  }
+
+  @Override
+  public Integer asInteger() {
+    return (Integer) field;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Iterable<Integer> asIntegers() {
+    return (Iterable<Integer>) field;
+  }
+
+  @Override
+  public Long asLong() {
+    return (Long) field;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Iterable<Long> asLongs() {
+    return (Iterable<Long>) field;
+  }
+
+  @Override
+  public Timestamp asTimestamp() {
+    return (Timestamp) field;
+  }
+
+  @Override
+  public byte[] asByteArray() {
+    return (byte[]) field;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Iterable<byte[]> asByteArrays() {
+    return (Iterable<byte[]>) field;
+  }
+}

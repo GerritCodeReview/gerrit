4224dbb42a0c9d4db72478ceace1fb4632565f2d
Patrick Hiesel <hiesel@google.com>
2021-05-19 16:04:02 +0200

    Implement generic matching for integer fields in ChangePredicate
    This commit removes a lot of boiler-plate predicates in favor of a
    generic match implementation for integer fields in ChangePredicates.
    
    Change-Id: Iaee25b78a8a49922095aa8d06509e6bf554e2a0b

--- java/com/google/gerrit/server/query/change/FuzzyTopicPredicate.java
+++ java/com/google/gerrit/server/query/change/FuzzyTopicPredicate.java
@@ -45,7 +45,7 @@ public class FuzzyTopicPredicate extends ChangeIndexPredicate {
     try {
       Predicate<ChangeData> thisId =
           index.getSchema().useLegacyNumericFields()
-              ? new LegacyChangeIdPredicate(cd.getId())
+              ? ChangePredicates.id(cd.getId())
               : new LegacyChangeIdStrPredicate(cd.getId());
       Iterable<ChangeData> results =
           index.getSource(and(thisId, this), IndexedChangeQuery.oneResult()).read();

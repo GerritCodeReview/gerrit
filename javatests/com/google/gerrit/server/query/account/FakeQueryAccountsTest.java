e2af3a0d3a2545575da0fda6df5c9ba45e4b3e59
Patrick Hiesel <hiesel@google.com>
2021-05-20 14:18:30 +0200

    Move #match logic from ChangeIndexPredicate to IndexPredicate
    This commit moves the new generic matching logic up to IndexPredicate
    so that we can reuse it for all predicates including Account, Group
    and Project.
    
    This allows us to start running all tests (even the ones previously
    ignored) against the fake index.
    
    This commit makes it so that all #match functions are now tested
    by running our query tests against them using the fake index.
    
    Change-Id: I07106b7b03084ad6e8203279aff0f5833490b60a

--- javatests/com/google/gerrit/server/query/account/FakeQueryAccountsTest.java
+++ javatests/com/google/gerrit/server/query/account/FakeQueryAccountsTest.java
@@ -14,10 +14,6 @@
 
 package com.google.gerrit.server.query.account;
 
-import static org.junit.Assume.assumeFalse;
-
-import com.google.gerrit.entities.Account;
-import com.google.gerrit.server.account.AccountState;
 import com.google.gerrit.server.index.account.AccountSchemaDefinitions;
 import com.google.gerrit.testing.ConfigSuite;
 import com.google.gerrit.testing.InMemoryModule;
@@ -25,7 +21,6 @@ import com.google.gerrit.testing.IndexConfig;
 import com.google.gerrit.testing.IndexVersions;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 import org.eclipse.jgit.lib.Config;
@@ -51,17 +46,5 @@ public class FakeQueryAccountsTest extends AbstractQueryAccountsTest {
     InMemoryModule.setDefaults(fakeConfig);
     fakeConfig.setString("index", null, "type", "fake");
     return Guice.createInjector(new InMemoryModule(fakeConfig));
-  }
-
-  @Override
-  protected void validateAssumptions() {
-    // TODO(hiesel): Account predicates are always matching (they return true on match), so we need
-    // to skip all tests here. We are doing this to document existing behavior. We want to remove
-    // this assume statement and make group predicates matchable.
-    assumeFalse(
-        AccountPredicates.equalsName("test")
-            .asMatchable()
-            .match(
-                AccountState.forAccount(Account.builder(Account.id(1), new Timestamp(0)).build())));
   }
 }

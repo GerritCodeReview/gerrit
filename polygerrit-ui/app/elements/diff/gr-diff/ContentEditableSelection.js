6a22dab4454f28179812a9e2610a14d5fdec4206
Antonio Barone <syntonyze@gmail.com>
2021-05-24 16:30:38 +0300

    Use alternative approach to shadow-selection-polyfill
    Since the selection shadow DOM is not implemented in Safari, a fix was
    introduced in January 2021[1] to use the shadow-selection-polyfill
    library to provide that functionality and allow selecting text to
    comment.
    
    As of March 2021 however, an fix to Safari[2] makes the
    shadow-selection-polyfill not working from Safari v14.1 onwards.
    
    It is not clear whether the library will ever work again in Safari, so
    much so that a warning, explaining this, as been added to the
    shadow-selection-polyfill README.md.
    
    An alternative approach however has been suggested. The main idea is:
    * Listen to 'selectionchange' events of 'contenteditable' areas.
    * Trigger a 'beforeinput' event by running and immediately terminating
      an `execCommand`.
    * use the getTargetRanges() method to get a list of static ranges
    
    This change is the porting of that idea (as explained here[4]) to
    Gerrit, as a proof of concept of the possible approach.
    
    Notes:
    - In order for this to work the selectable areas need to be set with
      `contentEditable="true"`. This has two problems:
       * the diff content is now editable and it should be made readonly
       * the styling is off.
    - The newly introduced ContentEditableSelection.js needs to be
      typescripted.
    
    [1] https://gerrit-review.googlesource.com/c/gerrit/+/293242
    [2] https://trac.webkit.org/changeset?reponame=webkit&new=269662
    [3] https://github.com/GoogleChromeLabs/shadow-selection-polyfill
    [4] https://github.com/GoogleChromeLabs/shadow-selection-polyfill/issues/11
    
    Bug: Issue 13955
    Change-Id: I4daebbfd571dc3f11c2897a2282d6b6b3619a2c0

---
+++ polygerrit-ui/app/elements/diff/gr-diff/ContentEditableSelection.js
@@ -0,0 +1,93 @@
+/**
+ * @license
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {isSafari, findActiveElement} from '../../../utils/dom-util.js';
+
+const SUPPORTS_SHADOW_SELECTION =
+    typeof window.ShadowRoot.prototype.getSelection === 'function';
+const SUPPORTS_BEFORE_INPUT =
+    typeof window.InputEvent.prototype.getTargetRanges === 'function';
+
+class ContentEditableSelection {
+  constructor() {
+    this._range = null;
+  }
+
+  getRange() {
+    return this._range;
+  }
+
+  setRange(range) {
+    this._range = range;
+  }
+
+  removeRange() {
+    this._range = null;
+  }
+}
+
+let processing = false;
+const selection = new ContentEditableSelection();
+
+if (isSafari() && !SUPPORTS_SHADOW_SELECTION && SUPPORTS_BEFORE_INPUT) {
+  window.addEventListener('selectionchange', () => {
+    if (!processing) {
+      processing = true;
+      const active = findActiveElement(document, true);
+      if (active && (active.id === 'diffTable')) {
+        document.execCommand('indent');
+      } else {
+        selection.removeRange();
+      }
+      processing = false;
+    }
+  }, true);
+
+  window.addEventListener('beforeinput', event => {
+    if (processing) {
+      // selecting
+      // console.log("selecting: " + event.inputType);
+      const ranges = event.getTargetRanges();
+      const range = ranges[0];
+
+      const newRange = new Range();
+
+      newRange.setStart(range.startContainer, range.startOffset);
+      newRange.setEnd(range.endContainer, range.endOffset);
+
+      selection.setRange(newRange);
+
+      event.preventDefault();
+      event.stopImmediatePropagation();
+    } else {
+      // typing
+      const active = findActiveElement(document, true);
+      // console.log("Typing text:" + event.inputType + " : " + active.id);
+      if (active.id === 'diffTable') {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+      }
+    }
+  }, true);
+
+  window.addEventListener('selectstart', event => {
+    selection.removeRange();
+  }, true);
+}
+
+export function getContentEditableRange() {
+  return selection.getRange();
+}

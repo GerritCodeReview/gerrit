0d329be1b6493b73f69e326cc28ba283381866bf
Patrick Hiesel <hiesel@google.com>
2021-05-25 11:42:49 +0200

    Implement query parser for approval queries
    This commit implements a query parser based on existing logic that runs
    predicates over PatchSetApprovals and some related metadata
    (ApprovalContext). The intention is that we can replace the existing
    boolean configs for copying approvals from one patch set to the next
    with simple query strings.
    
    Example:
    
    [label "Foo"]
      copyMaxScore = true
      copyIfTrivialRebase = true
    
    becomes
    
    [label "Foo"]
      copyIf = changekind:trivial-rebase OR is:max
    
    This allows us to be more flexible: The current configs are OR'd
    together. Using a query syntax here allows project owners to be
    more expressive and allows us to add more predicates in the future
    without polluting configs all that much.
    
    A likely candidate for future predicates are group checks on
    accounts that gave an approval (e.g. approver-group:Project-Owners).
    
    This commit adds the parser and tests but does not wire it up yet
    with project configs. Hence it does not mention anything in Gerrit's
    docs yet.
    
    Change-Id: I2d10af61a72197d4dc98b66976257f3b366076e4

---
+++ java/com/google/gerrit/server/query/approval/ApprovalContext.java
@@ -0,0 +1,50 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.approval;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.entities.PatchSet;
+import com.google.gerrit.entities.PatchSetApproval;
+import com.google.gerrit.entities.Project;
+
+/** Entity representing all required information to match predicates for copying approvals. */
+@AutoValue
+public abstract class ApprovalContext {
+  /** Project that approvals are copied in. */
+  public abstract Project.NameKey project();
+
+  /** Approval on the source patch set to be copied. */
+  public abstract PatchSetApproval patchSetApproval();
+
+  /** Target change and patch set for the approval. */
+  public abstract PatchSet.Id target();
+
+  public static ApprovalContext create(
+      Project.NameKey project, PatchSetApproval psa, PatchSet.Id id) {
+    checkState(
+        psa.patchSetId().changeId().equals(id.changeId()),
+        "approval and target must be the same change. got: %s, %s",
+        psa.patchSetId(),
+        id);
+    checkState(
+        psa.patchSetId().get() + 1 == id.get(),
+        "approvals can only be copied to the next consecutive patch set. got: %s, %s",
+        psa.patchSetId(),
+        id);
+    return new AutoValue_ApprovalContext(project, psa, id);
+  }
+}

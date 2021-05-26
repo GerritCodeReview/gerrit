16e5ee4f2c68c2419fc6e8987150b43014a79198
Dhruv Srivastava <dhruvsri@google.com>
2021-05-26 17:06:10 +0200

    Navigate to comment inside Comments Tab from comments url
    Change-Id: I99b69f1bccf6208348eff8c3c625a05693d2c287

--- polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list_html.ts
+++ polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list_html.ts
@@ -167,6 +167,7 @@ export const htmlTemplate = html`
         patch-num="[[thread.patchNum]]"
         path="[[thread.path]]"
         root-id="{{thread.rootId}}"
+        should-scroll-into-view="[[computeShouldScrollIntoView(thread.comments, scrollCommentId)]]"
         on-thread-changed="_handleCommentsChanged"
         on-thread-discard="_handleThreadDiscard"
       ></gr-comment-thread>

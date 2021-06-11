fcf145456f0367e1f0618be771d0d99eb489b709
Ben Rohlfs <brohlfs@google.com>
2021-06-11 09:24:34 +0200

    Add ordinal suffix to attempt numbers and sort from low to high
    Screenshot https://imgur.com/a/yaJkYy0
    
    Change-Id: I604808694e608c121a0448eaee7387740ae1bd45

--- polygerrit-ui/app/elements/checks/gr-hovercard-run_html.ts
+++ polygerrit-ui/app/elements/checks/gr-hovercard-run_html.ts
@@ -83,10 +83,16 @@ export const htmlTemplate = html`
       position: relative;
       top: 2px;
     }
+    div.sectionContent .attemptIcon iron-icon {
+      margin-right: 0;
+    }
+    .attemptIcon,
     .attemptNumber {
       margin-right: var(--spacing-s);
       color: var(--deemphasized-text-color);
       text-align: center;
+      width: 20px;
+      font-size: var(--font-size-small);
     }
     div.action {
       border-top: 1px solid var(--border-color);
@@ -155,7 +161,7 @@ export const htmlTemplate = html`
                   icon="gr-icons:[[item.icon]]"
                 ></iron-icon>
               </div>
-              <div class="attemptNumber">[[item.attempt]]</div>
+              <div class="attemptNumber">[[computeAttempt(item.attempt)]]</div>
             </div>
           </template>
         </div>
--- polygerrit-ui/app/utils/string-util_test.ts
+++ polygerrit-ui/app/utils/string-util_test.ts
@@ -16,7 +16,7 @@
  */
 
 import '../test/common-test-setup-karma';
-import {pluralize} from './string-util';
+import {pluralize, ordinal} from './string-util';
 
 suite('formatter util tests', () => {
   test('pluralize', () => {
@@ -24,5 +24,19 @@ suite('formatter util tests', () => {
     assert.equal(pluralize(0, noun), '');
     assert.equal(pluralize(1, noun), '1 comment');
     assert.equal(pluralize(2, noun), '2 comments');
+  });
+
+  test('ordinal', () => {
+    assert.equal(ordinal(0), '0th');
+    assert.equal(ordinal(1), '1st');
+    assert.equal(ordinal(2), '2nd');
+    assert.equal(ordinal(3), '3rd');
+    assert.equal(ordinal(4), '4th');
+    assert.equal(ordinal(10), '10th');
+    assert.equal(ordinal(11), '11th');
+    assert.equal(ordinal(12), '12th');
+    assert.equal(ordinal(13), '13th');
+    assert.equal(ordinal(44413), '44413th');
+    assert.equal(ordinal(44451), '44451st');
   });
 });

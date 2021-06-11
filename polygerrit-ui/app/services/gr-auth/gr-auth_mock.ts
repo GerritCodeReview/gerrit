97c318b42c44b508044849b186913b8c2f6fb554
Dhruv Srivastava <dhruvsri@google.com>
2021-06-11 16:33:35 +0200

    Convert gr-error-manager_test to typescript
    Change-Id: I7c30688a4aefa6b5ad3a36377ba87a51614a2bb2

--- polygerrit-ui/app/services/gr-auth/gr-auth_mock.ts
+++ polygerrit-ui/app/services/gr-auth/gr-auth_mock.ts
@@ -15,23 +15,63 @@
  * limitations under the License.
  */
 
+import { EventEmitterService } from '../gr-event-interface/gr-event-interface';
 import {
   AuthRequestInit,
   AuthService,
+  AuthStatus,
   DefaultAuthOptions,
   GetTokenCallback,
 } from './gr-auth';
+import {Auth} from './gr-auth_impl';
 
-export const grAuthMock: AuthService = {
-  baseUrl: '',
-  isAuthed: false,
+export class GrAuthMock implements AuthService {
+  baseUrl = '';
 
-  authCheck: () => Promise.resolve(false),
-  clearCache: () => {},
-  setup: (
-    _getToken: GetTokenCallback,
-    _defaultOptions: DefaultAuthOptions
-  ) => {},
-  fetch: (_url: string, _opt_options?: AuthRequestInit): Promise<Response> =>
-    Promise.resolve(new Response()),
-};
+  private _status = AuthStatus.UNDETERMINED;
+
+  public eventEmitter: EventEmitterService;
+
+  constructor(eventEmitter: EventEmitterService) {
+    this.eventEmitter = eventEmitter;
+  }
+
+  get isAuthed() {
+    return this._status === Auth.STATUS.AUTHED;
+  }
+
+  private _setStatus(status: AuthStatus) {
+    if (this._status === status) return;
+    if (this._status === AuthStatus.AUTHED) {
+      this.eventEmitter.emit('auth-error', {
+        message: Auth.CREDS_EXPIRED_MSG,
+        action: 'Refresh credentials',
+      });
+    }
+    this._status = status;
+  }
+
+  get status() {
+    return this._status;
+  }
+
+  authCheck() {
+    return this.fetch(`${this.baseUrl}/auth-check`).then(res => {
+      if (res.status === 204) {
+        this._setStatus(Auth.STATUS.AUTHED);
+        return true;
+      } else {
+        this._setStatus(Auth.STATUS.NOT_AUTHED);
+        return false;
+      }
+    });
+  }
+
+  clearCache() {}
+
+  setup(_getToken: GetTokenCallback, _defaultOptions: DefaultAuthOptions) {}
+
+  fetch(_url: string, _opt_options?: AuthRequestInit): Promise<Response> {
+    return Promise.resolve(new Response());
+  }
+}

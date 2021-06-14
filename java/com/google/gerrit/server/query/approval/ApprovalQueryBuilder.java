df60aa88056d7b0aa74701680dc6fd44d23173f7
Youssef Elghareeb <ghareeb@google.com>
2021-06-14 16:39:59 +0200

    Remove unused exception in ApprovalQueryBuilder
    The AutoAnnotationProcessor fails while building Gerrit with Bazel with
    the following error:
    
    error: [UnusedException] This catch block catches an symbol and
    re-throws another, but swallows the caught symbol rather than setting it
    as a cause.
    
    Using the exception in the catch block and removing the
    @SuppressWarnings annotation to avoid this error.
    
    Change-Id: I1dd0c0e0b8dbe01ecdf9ab056f9b6045cc5042f6

--- java/com/google/gerrit/server/query/approval/ApprovalQueryBuilder.java
+++ java/com/google/gerrit/server/query/approval/ApprovalQueryBuilder.java
@@ -85,13 +85,11 @@ public class ApprovalQueryBuilder extends QueryBuilder<ApprovalContext, Approval
       throws QueryParseException {
     try {
       return Enum.valueOf(clazz, term.toUpperCase().replace('-', '_'));
-    } catch (
-        @SuppressWarnings("UnusedException")
-        IllegalArgumentException unused) {
+    } catch (IllegalArgumentException e) {
       throw new QueryParseException(
           String.format(
               "%s is not a valid term. valid options: %s",
-              term, Arrays.asList(clazz.getEnumConstants())));
+              term, Arrays.asList(clazz.getEnumConstants())), e);
     }
   }
 

1cc62b5d0ac884667d8b640fd2c3bde42ed3986c
Milutin Kristofic <milutin@google.com>
2021-06-15 08:35:02 +0200

    Convert gr-group-audit_test to typescript
    Change-Id: Ifbf6280153b8a36533aec90123fc3590a7998bc4

--- polygerrit-ui/app/elements/admin/gr-group-audit-log/gr-group-audit-log_test.ts
+++ polygerrit-ui/app/elements/admin/gr-group-audit-log/gr-group-audit-log_test.ts
@@ -17,12 +17,23 @@
 
 import '../../../test/common-test-setup-karma.js';
 import './gr-group-audit-log.js';
-import {stubRestApi, addListenerForTest, mockPromise} from '../../../test/test-utils.js';
+import {
+  stubRestApi,
+  addListenerForTest,
+  mockPromise,
+} from '../../../test/test-utils.js';
+import {GrGroupAuditLog} from './gr-group-audit-log.js';
+import {EncodedGroupId, GroupInfo, GroupName} from '../../../types/common.js';
+import {
+  createAccountWithId,
+  createGroupInfo,
+} from '../../../test/test-data-generators.js';
+import {PageErrorEvent} from '../../../types/events.js';
 
 const basicFixture = fixtureFromElement('gr-group-audit-log');
 
 suite('gr-group-audit-log tests', () => {
-  let element;
+  let element: GrGroupAuditLog;
 
   setup(() => {
     element = basicFixture.instantiate();
@@ -30,19 +41,14 @@ suite('gr-group-audit-log tests', () => {
 
   suite('members', () => {
     test('test _getNameForGroup', () => {
-      let group = {
-        member: {
-          name: 'test-name',
-        },
+      let member: GroupInfo = {
+        ...createGroupInfo(),
+        name: 'test-name' as GroupName,
       };
-      assert.equal(element._getNameForGroup(group.member), 'test-name');
+      assert.equal(element._getNameForGroup(member), 'test-name');
 
-      group = {
-        member: {
-          id: 'test-id',
-        },
-      };
-      assert.equal(element._getNameForGroup(group.member), 'test-id');
+      member = createGroupInfo('test-id');
+      assert.equal(element._getNameForGroup(member), 'test-id');
     });
 
     test('test _isGroupEvent', () => {
@@ -56,13 +62,11 @@ suite('gr-group-audit-log tests', () => {
 
   suite('users', () => {
     test('test _getIdForUser', () => {
-      const account = {
-        user: {
-          username: 'test-user',
-          _account_id: 12,
-        },
+      const user = {
+        ...createAccountWithId(12),
+        username: 'test-user',
       };
-      assert.equal(element._getIdForUser(account.user), ' (12)');
+      assert.equal(element._getIdForUser(user), ' (12)');
     });
 
     test('test _account_id not present', () => {
@@ -77,18 +81,18 @@ suite('gr-group-audit-log tests', () => {
 
   suite('404', () => {
     test('fires page-error', async () => {
-      element.groupId = 1;
+      element.groupId = '1' as EncodedGroupId;
       await flush();
 
-      const response = {status: 404};
-      stubRestApi('getGroupAuditLog').callsFake((group, errFn) => {
-        errFn(response);
+      const response = {...new Response(), status: 404};
+      stubRestApi('getGroupAuditLog').callsFake((_group, errFn) => {
+        if (errFn) errFn(response);
         return Promise.resolve(undefined);
       });
 
       const pageErrorCalled = mockPromise();
       addListenerForTest(document, 'page-error', e => {
-        assert.deepEqual(e.detail.response, response);
+        assert.deepEqual((e as PageErrorEvent).detail.response, response);
         pageErrorCalled.resolve();
       });
 
@@ -97,4 +101,3 @@ suite('gr-group-audit-log tests', () => {
     });
   });
 });
-

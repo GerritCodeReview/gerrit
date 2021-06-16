bddcd34dc7d841de366bd80031f4990614c44d1c
Antonio Barone <syntonyze@gmail.com>
2021-06-16 19:50:17 +0300

    Add test to expose OOM error when running many acceptance tests
    Running many acceptance tests using @UseLocalDisk annotation, eventually
    causes an OOM exception.
    
    Bug: Issue 14678
    Change-Id: Iac3fe046a9ba4b954cfc4f22d6c58f48c5c351fd

---
+++ javatests/com/google/gerrit/acceptance/oom/TestShowingOOMUponUsingLocalDiskIT.java
@@ -0,0 +1,143 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.oom;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import org.junit.Test;
+
+public class TestShowingOOMUponUsingLocalDiskIT extends AbstractDaemonTest {
+  @Test
+  @UseLocalDisk
+  public void shouldPass1() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass2() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass3() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass4() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass5() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass6() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass7() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass8() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass9() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass10() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass11() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass12() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass13() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass14() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass15() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass16() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass17() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass18() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass19() throws Exception {
+    assertThat(true).isTrue();
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldPass20() throws Exception {
+    assertThat(true).isTrue();
+  }
+}

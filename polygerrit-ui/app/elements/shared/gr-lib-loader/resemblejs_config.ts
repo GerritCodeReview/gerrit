8ef32304b4feece09534e619416ca0d9bcebcd3b
Hermann Loose <hermannloose@google.com>
2021-06-17 16:44:03 +0200

    Add GrLibLoader library config for Resemble.js
    This will be used by the new image diff UI for highlighting changes
    between the base and revision image when requested.
    
    Change-Id: I6487039ee4af0a1ed6d02f97d940a146a2914dea

---
+++ polygerrit-ui/app/elements/shared/gr-lib-loader/resemblejs_config.ts
@@ -0,0 +1,39 @@
+/**
+ * @license
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {LibraryConfig} from './gr-lib-loader';
+
+declare global {
+  interface Window {
+    resemble?: any;
+  }
+}
+
+export const RESEMBLEJS_LIBRARY_CONFIG: LibraryConfig = {
+  src: '/bower_components/resemblejs/resemble.js',
+  checkPresent: () => window.resemble !== undefined,
+  configureCallback: () => {
+    window.resemble!.outputSettings({
+      errorColor: {red: 255, green: 0, blue: 255},
+      errorType: 'flat',
+      transparency: 0,
+      // Disable large image threshold; by default this otherwise skips pixels
+      // if width or height exceed 1200 pixels.
+      largeImageThreshold: 0,
+    });
+    return window.resemble;
+  },
+};

674f13fff5a2a4d978478297fd46b4a2d54aa1d4
Edwin Kempin <ekempin@google.com>
2021-06-21 13:42:56 +0200

    Submit requirements: Add support for querying labels with MAX/MIN/ANY
    This change adds support for creating different submit requirement
    expressions using MAX, MIN or ANY for label values, the syntax works
    like the following:
    
      submittability_expression = label:code-review=MAX
    
    This would allow us to express the current MaxWithBlock function as
    
      submittability_expression =
        label:code-review=MAX AND -label:code-review=MIN
    
    This change adds this support for the change query language as well,
    since the change query language is used by submit requirements.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I702f06baf93e952d547a26a70a94880c8aa72c51

---
+++ java/com/google/gerrit/server/query/change/MagicLabelVote.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.change;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Strings;
+import com.google.gerrit.entities.LabelType;
+import java.util.Locale;
+
+@AutoValue
+public abstract class MagicLabelVote {
+  public static MagicLabelVote parseWithEquals(String text) {
+    checkArgument(!Strings.isNullOrEmpty(text), "Empty label vote");
+    int e = text.lastIndexOf('=');
+    checkArgument(e >= 0, "Label vote missing '=': %s", text);
+    String label = text.substring(0, e);
+    String voteValue = text.substring(e + 1);
+    return create(label, MagicLabelValue.valueOf(voteValue));
+  }
+
+  public static MagicLabelVote create(String label, MagicLabelValue value) {
+    return new AutoValue_MagicLabelVote(LabelType.checkNameInternal(label), value);
+  }
+
+  public abstract String label();
+
+  public abstract MagicLabelValue value();
+
+  public boolean isMax() {
+    return MagicLabelValue.MAX == value();
+  }
+
+  public boolean isMin() {
+    return MagicLabelValue.MIN == value();
+  }
+
+  public boolean isAny() {
+    return MagicLabelValue.ANY == value();
+  }
+
+  public String formatLabel() {
+    return label().toLowerCase(Locale.US) + "=" + value().name();
+  }
+}

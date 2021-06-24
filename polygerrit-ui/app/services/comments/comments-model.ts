68a09c76f2d53b198523e3e7664a61217ef57732
Dhruv Srivastava <dhruvsri@google.com>
2021-06-24 15:20:29 +0200

    Create comments model and service
    Allow components to communicate changes in drafts via the comments
    service.
    The fetching of the comments is moved to the comments service.
    
    A follow up change will move gr-comment-api to comments-service.
    
    Change-Id: Ice6ca3b2886d06ac20299e2ccb13848989c5e25d

---
+++ polygerrit-ui/app/services/comments/comments-model.ts
@@ -0,0 +1,50 @@
+/**
+ * @license
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {BehaviorSubject, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {ChangeComments} from '../../elements/diff/gr-comment-api/gr-comment-api';
+import { PathToCommentsInfoMap } from '../../types/common';
+import { UIDraft, UIHuman, UIRobot } from '../../utils/comment-util';
+
+interface CommentState {
+  comments?: {[path: string]: UIHuman[]},
+  robotComments?: {[path: string]: UIRobot[]},
+  drafts?: {[path: string]: UIDraft[]},
+  portedComments?: PathToCommentsInfoMap,
+  portedDrafts?: PathToCommentsInfoMap,
+}
+
+const initialState: CommentState = {};
+
+const privateState$ = new BehaviorSubject(initialState);
+
+// Re-exporting as Observable so that you can only subscribe, but not emit.
+export const commentState$: Observable<CommentState> = privateState$;
+
+export const changeComments$ = commentState$.pipe(
+  map(commentState => commentState.changeComments)
+);
+
+export const drafts$ = changeComments$.pipe(
+  map(commentState => commentState?.drafts)
+);
+
+export function updateState(changeComments: ChangeComments) {
+  const current = privateState$.getValue();
+  privateState$.next({...current, changeComments});
+}

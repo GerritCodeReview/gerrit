bbc35956fa979eb531a5c3c651f73d551ba47307
Paladox none <thomasmulhall410@yahoo.com>
2021-06-30 13:41:50 +0000

    gr-editor-view: Remove null from _change
    Change-Id: I8d1b9022cbed21300f31729cc9e41e4e295da7bb

--- polygerrit-ui/app/utils/change-util.ts
+++ polygerrit-ui/app/utils/change-util.ts
@@ -131,16 +131,16 @@ export function changePath(changeNum: NumericChangeId) {
   return `${getBaseUrl()}/c/${changeNum}`;
 }
 
-export function changeIsOpen(change?: ChangeInfo | ParsedChangeInfo | null) {
+export function changeIsOpen(change?: ChangeInfo | ParsedChangeInfo) {
   return change?.status === ChangeStatus.NEW;
 }
 
-export function changeIsMerged(change?: ChangeInfo | ParsedChangeInfo | null) {
+export function changeIsMerged(change?: ChangeInfo | ParsedChangeInfo) {
   return change?.status === ChangeStatus.MERGED;
 }
 
 export function changeIsAbandoned(
-  change?: ChangeInfo | ParsedChangeInfo | null
+  change?: ChangeInfo | ParsedChangeInfo
 ) {
   return change?.status === ChangeStatus.ABANDONED;
 }

ddbc459c840323285483873e776913663d88f25a
Hermann Loose <hermannloose@google.com>
2021-06-30 18:05:14 +0200

    Show image dimensions below magnified image
    Change-Id: I612ba45acdc5f8874ceb422bc70c6c043388d3b6

--- polygerrit-ui/app/elements/diff/gr-diff-image-viewer/gr-image-viewer.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-image-viewer/gr-image-viewer.ts
@@ -173,7 +173,9 @@ export class GrImageViewer extends LitElement {
 
   static styles = css`
     :host {
-      display: flex;
+      display: grid;
+      grid-template-rows: auto auto;
+      grid-template-columns: 1fr auto;
       width: 100%;
       height: 100%;
       box-sizing: border-box;
@@ -182,6 +184,8 @@ export class GrImageViewer extends LitElement {
       --image-border-width: 2px;
     }
     .imageArea {
+      grid-row-start: 1;
+      grid-column-start: 1;
       box-sizing: border-box;
       flex-grow: 1;
       overflow: hidden;
@@ -242,7 +246,21 @@ export class GrImageViewer extends LitElement {
         var(--square-size) calc(-1 * var(--square-size)),
         calc(-1 * var(--square-size)) 0;
     }
+    .dimensions {
+      grid-row-start: 2;
+      justify-self: center;
+      align-self: center;
+      background: var(--primary-button-background-color);
+      color: var(--primary-button-text-color);
+      font-family: var(--font-family);
+      font-size: 10px;
+      line-height: 15px;
+      border-radius: 4px;
+      margin: var(--spacing-s);
+      padding: 0 var(--spacing-s);
+    }
     .controls {
+      grid-column-start: 2;
       flex-grow: 0;
       display: flex;
       flex-direction: column;
@@ -609,6 +627,10 @@ export class GrImageViewer extends LitElement {
           ${sourceImageWithHighlight}
         </gr-zoomed-image>
         ${this.baseUrl && this.revisionUrl ? automaticBlink : ''} ${spacer}
+      </div>
+
+      <div class="dimensions">
+        ${this.imageSize.width} x ${this.imageSize.height}
       </div>
 
       <paper-card class="controls">

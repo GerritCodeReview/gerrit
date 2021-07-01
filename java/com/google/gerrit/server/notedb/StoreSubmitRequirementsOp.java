4ee76e2e167e1739d156efc414ae0b290f2e3f69
Youssef Elghareeb <ghareeb@google.com>
2021-07-01 13:23:09 +0200

    Store submit requirements for merged changes in NoteDb
    Upon merging a change, we check for the changeset (list of changes) to
    be merged, and add a batch update op for each change to store the
    evaluated submit requirements in NoteDb.
    
    Submit requirements are stored in the change meta ref
    (refs/changes/$short-id/$id/meta) in the revision notes of the latest
    patchset. The added test in ChangeIT makes sure submit requirements are
    stored in the change notes when the change is submitted.
    
    In a follow up change, we will update the 'Get change' endpoint to use
    the logic added in this change:
    * If the change is active, we will evaluate submit requirements (this is
    how it's done now).
    * If the change is merged, we will lookup the evaluated submit
    requirements from NoteDb and return them to the user.
    
    Change-Id: I2a63f725b9f96647bb438f9f546a48b0fbab4e12

---
+++ java/com/google/gerrit/server/notedb/StoreSubmitRequirementsOp.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.notedb;
+
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.update.BatchUpdateOp;
+import com.google.gerrit.server.update.ChangeContext;
+
+/** A {@link BatchUpdateOp} that stores the evaluated submit requirements of a change in NoteDb. */
+public class StoreSubmitRequirementsOp implements BatchUpdateOp {
+  private final ChangeData.Factory changeDataFactory;
+
+  public StoreSubmitRequirementsOp(ChangeData.Factory changeDataFactory) {
+    this.changeDataFactory = changeDataFactory;
+  }
+
+  @Override
+  public boolean updateChange(ChangeContext ctx) throws Exception {
+    Change change = ctx.getChange();
+    ChangeData changeData = changeDataFactory.create(change);
+    ChangeUpdate update = ctx.getUpdate(change.currentPatchSetId());
+    update.putSubmitRequirementResults(changeData.submitRequirements().values());
+    return !changeData.submitRequirements().isEmpty();
+  }
+}

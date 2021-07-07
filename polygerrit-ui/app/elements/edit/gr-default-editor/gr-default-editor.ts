20fb58bf46bc1de7450227e4c45d98b7368d4300
Paladox none <thomasmulhall410@yahoo.com>
2021-07-07 19:24:17 +0000

    Fix template problems with gr-editor-view
    Change-Id: I09f75e0c137f1dd007e627d683c044d4d496c827

--- polygerrit-ui/app/elements/edit/gr-default-editor/gr-default-editor.ts
+++ polygerrit-ui/app/elements/edit/gr-default-editor/gr-default-editor.ts
@@ -42,7 +42,7 @@ export class GrDefaultEditor extends PolymerElement {
    */
 
   @property({type: String})
-  fileContent: string | null = null;
+  fileContent?: string;
 
   _handleTextareaInput(e: Event) {
     this.dispatchEvent(

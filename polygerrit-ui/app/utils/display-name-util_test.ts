5edd33b9da97525d6150776a102d4e5cab015689
Frank Borden <frankborden@google.com>
2021-07-29 14:45:27 +0000

    Convert display-name-util_test to Typescript
    Change-Id: I4fd9f6f48dffad1f7a21eaed8a880fd471bc900f

--- polygerrit-ui/app/utils/display-name-util_test.ts
+++ polygerrit-ui/app/utils/display-name-util_test.ts
@@ -15,12 +15,31 @@
  * limitations under the License.
  */
 
-import '../test/common-test-setup-karma.js';
-import {getDisplayName, getUserName, getGroupDisplayName, getAccountDisplayName, _testOnly_accountEmail} from './display-name-util.js';
+import {
+  AccountInfo,
+  DefaultDisplayNameConfig,
+  EmailAddress,
+  GroupName,
+  ServerInfo,
+} from '../api/rest-api';
+import '../test/common-test-setup-karma';
+import {
+  getDisplayName,
+  getUserName,
+  getGroupDisplayName,
+  getAccountDisplayName,
+  _testOnly_accountEmail,
+} from './display-name-util';
+import {
+  createAccountsConfig,
+  createGroupInfo,
+  createServerInfo,
+} from '../test/test-data-generators';
 
 suite('display-name-utils tests', () => {
   // eslint-disable-next-line no-unused-vars
-  const config = {
+  const config: ServerInfo = {
+    ...createServerInfo(),
     user: {
       anonymous_coward_name: 'Anonymous Coward',
     },
@@ -30,8 +49,7 @@ suite('display-name-utils tests', () => {
     const account = {
       name: 'test-name',
     };
-    assert.equal(getDisplayName(config, account),
-        'test-name');
+    assert.equal(getDisplayName(config, account), 'test-name');
   });
 
   test('getDisplayName prefer displayName', () => {
@@ -39,8 +57,7 @@ suite('display-name-utils tests', () => {
       name: 'test-name',
       display_name: 'better-name',
     };
-    assert.equal(getDisplayName(config, account),
-        'better-name');
+    assert.equal(getDisplayName(config, account), 'better-name');
   });
 
   test('getDisplayName prefer username default', () => {
@@ -48,13 +65,14 @@ suite('display-name-utils tests', () => {
       name: 'test-name',
       username: 'user-name',
     };
-    const config = {
+    const config: ServerInfo = {
+      ...createServerInfo(),
       accounts: {
-        default_display_name: 'USERNAME',
+        ...createAccountsConfig(),
+        default_display_name: DefaultDisplayNameConfig.USERNAME,
       },
     };
-    assert.equal(getDisplayName(config, account),
-        'user-name');
+    assert.equal(getDisplayName(config, account), 'user-name');
   });
 
   test('getDisplayName firstNameOnly', () => {
@@ -68,133 +86,141 @@ suite('display-name-utils tests', () => {
     const account = {
       name: 'firstname lastname',
     };
-    const config = {
+    const config: ServerInfo = {
+      ...createServerInfo(),
       accounts: {
-        default_display_name: 'FIRST_NAME',
+        ...createAccountsConfig(),
+        default_display_name: DefaultDisplayNameConfig.FIRST_NAME,
       },
     };
-    assert.equal(getDisplayName(config, account),
-        'firstname');
+    assert.equal(getDisplayName(config, account), 'firstname');
   });
 
   test('getDisplayName ignore leading whitespace for first name', () => {
     const account = {
       name: '   firstname lastname',
     };
-    const config = {
+    const config: ServerInfo = {
+      ...createServerInfo(),
       accounts: {
-        default_display_name: 'FIRST_NAME',
+        ...createAccountsConfig(),
+        default_display_name: DefaultDisplayNameConfig.FIRST_NAME,
       },
     };
-    assert.equal(getDisplayName(config, account),
-        'firstname');
+    assert.equal(getDisplayName(config, account), 'firstname');
   });
 
   test('getDisplayName full name default', () => {
     const account = {
       name: 'firstname lastname',
     };
-    const config = {
+    const config: ServerInfo = {
+      ...createServerInfo(),
       accounts: {
-        default_display_name: 'FULL_NAME',
+        ...createAccountsConfig(),
+        default_display_name: DefaultDisplayNameConfig.FULL_NAME,
       },
     };
-    assert.equal(getDisplayName(config, account),
-        'firstname lastname');
+    assert.equal(getDisplayName(config, account), 'firstname lastname');
   });
 
   test('getDisplayName name only', () => {
     const account = {
       name: 'test-name',
     };
-    assert.deepEqual(getUserName(config, account),
-        'test-name');
+    assert.deepEqual(getUserName(config, account), 'test-name');
   });
 
   test('getUserName username only', () => {
     const account = {
       username: 'test-user',
     };
-    assert.deepEqual(getUserName(config, account),
-        'test-user');
+    assert.deepEqual(getUserName(config, account), 'test-user');
   });
 
   test('getUserName email only', () => {
-    const account = {
-      email: 'test-user@test-url.com',
+    const account: AccountInfo = {
+      email: 'test-user@test-url.com' as EmailAddress,
     };
-    assert.deepEqual(getUserName(config, account),
-        'test-user@test-url.com');
+    assert.deepEqual(getUserName(config, account), 'test-user@test-url.com');
   });
 
   test('getUserName returns not Anonymous Coward as the anon name', () => {
-    assert.deepEqual(getUserName(config, null),
-        'Anonymous');
+    assert.deepEqual(getUserName(config, undefined), 'Anonymous');
   });
 
   test('getUserName for the config returning the anon name', () => {
-    const config = {
+    const config: ServerInfo = {
+      ...createServerInfo(),
       user: {
         anonymous_coward_name: 'Test Anon',
       },
     };
-    assert.deepEqual(getUserName(config, null),
-        'Test Anon');
+    assert.deepEqual(getUserName(config, undefined), 'Test Anon');
   });
 
   test('getAccountDisplayName - account with name only', () => {
     assert.equal(
-        getAccountDisplayName(config,
-            {name: 'Some user name'}),
-        'Some user name');
+      getAccountDisplayName(config, {name: 'Some user name'}),
+      'Some user name'
+    );
   });
 
   test('getAccountDisplayName - account with email only', () => {
     assert.equal(
-        getAccountDisplayName(config,
-            {email: 'my@example.com'}),
-        'my@example.com <my@example.com>');
+      getAccountDisplayName(config, {
+        email: 'my@example.com' as EmailAddress,
+      }),
+      'my@example.com <my@example.com>'
+    );
   });
 
   test('getAccountDisplayName - account with name and status', () => {
     assert.equal(
-        getAccountDisplayName(config, {
-          name: 'Some name',
-          status: 'OOO',
-        }),
-        'Some name (OOO)');
+      getAccountDisplayName(config, {
+        name: 'Some name',
+        status: 'OOO',
+      }),
+      'Some name (OOO)'
+    );
   });
 
   test('getAccountDisplayName - account with name and email', () => {
     assert.equal(
-        getAccountDisplayName(config, {
-          name: 'Some name',
-          email: 'my@example.com',
-        }),
-        'Some name <my@example.com>');
+      getAccountDisplayName(config, {
+        name: 'Some name',
+        email: 'my@example.com' as EmailAddress,
+      }),
+      'Some name <my@example.com>'
+    );
   });
 
   test('getAccountDisplayName - account with name, email and status', () => {
     assert.equal(
-        getAccountDisplayName(config, {
-          name: 'Some name',
-          email: 'my@example.com',
-          status: 'OOO',
-        }),
-        'Some name <my@example.com> (OOO)');
+      getAccountDisplayName(config, {
+        name: 'Some name',
+        email: 'my@example.com' as EmailAddress,
+        status: 'OOO',
+      }),
+      'Some name <my@example.com> (OOO)'
+    );
   });
 
   test('getGroupDisplayName', () => {
     assert.equal(
-        getGroupDisplayName({name: 'Some user name'}),
-        'Some user name (group)');
+      getGroupDisplayName({
+        ...createGroupInfo(),
+        name: 'Some user name' as GroupName,
+      }),
+      'Some user name (group)'
+    );
   });
 
   test('_accountEmail', () => {
     assert.equal(
-        _testOnly_accountEmail('email@gerritreview.com'),
-        '<email@gerritreview.com>');
+      _testOnly_accountEmail('email@gerritreview.com'),
+      '<email@gerritreview.com>'
+    );
     assert.equal(_testOnly_accountEmail(undefined), '');
   });
 });
-

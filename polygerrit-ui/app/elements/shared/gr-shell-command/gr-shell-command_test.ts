a6475d7f1d00a45f16bdfa3a83deb68b2b1015e8
Paladox none <thomasmulhall410@yahoo.com>
2021-08-10 00:31:39 +0000

    Convert gr-shell-command to lit element
    Change-Id: I1fde0bd8e10ef85409cbdc4f62bcdc0eb3135897

--- polygerrit-ui/app/elements/shared/gr-shell-command/gr-shell-command_test.ts
+++ polygerrit-ui/app/elements/shared/gr-shell-command/gr-shell-command_test.ts
@@ -18,22 +18,24 @@
 import '../../../test/common-test-setup-karma';
 import './gr-shell-command';
 import {GrShellCommand} from './gr-shell-command';
+import {GrCopyClipboard} from '../gr-copy-clipboard/gr-copy-clipboard';
+import {queryAndAssert} from '../../../test/test-utils';
 
 const basicFixture = fixtureFromElement('gr-shell-command');
 
 suite('gr-shell-command tests', () => {
   let element: GrShellCommand;
 
-  setup(() => {
+  setup(async () => {
     element = basicFixture.instantiate();
     element.command = `git fetch http://gerrit@localhost:8080/a/test-project
         refs/changes/05/5/1 && git checkout FETCH_HEAD`;
-    flush();
+    await flush();
   });
 
   test('focusOnCopy', () => {
     const focusStub = sinon.stub(
-      element.shadowRoot!.querySelector('gr-copy-clipboard')!,
+      queryAndAssert<GrCopyClipboard>(element, 'gr-copy-clipboard')!,
       'focusOnCopy'
     );
     element.focusOnCopy();

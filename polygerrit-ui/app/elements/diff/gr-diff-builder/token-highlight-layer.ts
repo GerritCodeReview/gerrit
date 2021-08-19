ade5cb7dc65762f6360aca2cfdac1fcd1aae5a89
Chris Poucet <poucet@google.com>
2021-08-19 20:51:32 +0000

    Improve token-highlight hover behavior.
    Do not change the highlighting if the mouse doesn't hover long enough
    over a given element.
    
    Change-Id: I83b51aedcc761b1c2a917027e30d983ee0d5fa42

--- polygerrit-ui/app/elements/diff/gr-diff-builder/token-highlight-layer.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-builder/token-highlight-layer.ts
@@ -92,10 +92,11 @@ export class TokenHighlightLayer implements DiffLayer {
 
   private tokenToLinesRight = new Map<string, Set<number>>();
 
+  private hoveredElement?: Element;
   private updateTokenTask?: DelayedTask;
 
   constructor() {
-    window.addEventListener('click', _ => this.handleWindowClick());
+    window.addEventListener('click', _ => { this.handleWindowClick(); });
   }
 
   annotate(
@@ -132,7 +133,8 @@ export class TokenHighlightLayer implements DiffLayer {
       // These listeners do not have to be cleaned, because listeners are
       // garbage collected along with the element itself once it is not attached
       // to the DOM anymore and no references exist anymore.
-      el.addEventListener('mouseover', e => this.handleMouseOver(e));
+      el.addEventListener('mouseover', e => { this.handleMouseOver(e); });
+      el.addEventListener('mouseout', e => { this.handleMouseOut(e); });
     }
   }
 
@@ -153,17 +155,28 @@ export class TokenHighlightLayer implements DiffLayer {
     numbers.add(Number(lineNumber));
   }
 
+  private handleMouseOut(e: MouseEvent) {
+    if (this.interferesWithSelection(e)) return;
+    const {element} = this.findTokenAncestor(e?.target);
+    if (!element) return;
+    if (element === this.hoveredElement) {
+      this.hoveredElement = undefined;
+      this.updateTokenTask?.cancel();
+    }
+  }
+
   private handleMouseOver(e: MouseEvent) {
     if (this.interferesWithSelection(e)) return;
-    const {line, token} = this.findTokenAncestor(e?.target);
+    const {line, token, element} = this.findTokenAncestor(e?.target);
     if (!token) return;
     const oldHighlight = this.currentHighlight;
     const newHighlight = token;
     if (!newHighlight || newHighlight === oldHighlight) return;
     if (this.countOccurrences(newHighlight) <= 1) return;
+    this.hoveredElement = element;
     this.updateTokenTask = debounce(
       this.updateTokenTask,
-      () => this._updateTokenHighlight(line, newHighlight),
+      () => { this.updateTokenHighlight(line, newHighlight); },
       HOVER_DELAY_MS
     );
   }
@@ -175,11 +188,12 @@ export class TokenHighlightLayer implements DiffLayer {
   }
 
   private handleWindowClick() {
+    this.hoveredElement = undefined;
     this.updateTokenTask?.cancel();
-    this._updateTokenHighlight(undefined, undefined);
+    this.updateTokenHighlight(undefined, undefined);
   }
 
-  private _updateTokenHighlight(
+  private updateTokenHighlight(
     newLineNumber: number | undefined,
     newHighlight: string | undefined
   ) {
@@ -194,18 +208,22 @@ export class TokenHighlightLayer implements DiffLayer {
   findTokenAncestor(el?: EventTarget | Element | null): {
     token?: string;
     line: number;
+    element?: Element;
   } {
-    if (!(el instanceof Element)) return {line: 0, token: undefined};
+    if (!(el instanceof Element))
+      return {line: 0, token: undefined, element: undefined};
     if (
       el.classList.contains(CSS_TOKEN) ||
       el.classList.contains(CSS_HIGHLIGHT)
     ) {
       const tkClass = [...el.classList].find(c => c.startsWith('tk-'));
       const line = lineNumberToNumber(getLineNumberByChild(el));
-      if (!line || !tkClass) return {line: 0, token: undefined};
-      return {line, token: tkClass.substring(3)};
+      if (!line || !tkClass)
+        return {line: 0, token: undefined, element: undefined};
+      return {line, token: tkClass.substring(3), element: el};
     }
-    if (el.tagName === 'TD') return {line: 0, token: undefined};
+    if (el.tagName === 'TD')
+      return {line: 0, token: undefined, element: undefined};
     return this.findTokenAncestor(el.parentElement);
   }
 

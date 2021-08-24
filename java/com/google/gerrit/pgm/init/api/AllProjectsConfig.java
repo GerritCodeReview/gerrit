2baf8821526cc1d1e430cb8ead9c1f1a8b8f8994
Dmitrii Filippov <dmfilippov@google.com>
2021-08-24 16:03:15 +0200

    Extract AllProjectsConfigProvider
    Google doesn't use file-based project.config. After switching SitePaths
    to in-memory filesystem for google hosted sites, we can't use
    FileBasedConfig anymore. The AllProjectsConfigProvider allows to replace
    FileBasedConfig implementation.
    
    Change-Id: Ica111643521a00d0fee8423e7cb0b4f3b007f05d

--- java/com/google/gerrit/pgm/init/api/AllProjectsConfig.java
+++ java/com/google/gerrit/pgm/init/api/AllProjectsConfig.java
@@ -17,6 +17,7 @@ package com.google.gerrit.pgm.init.api;
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.server.config.AllProjectsConfigProvider;
 import com.google.gerrit.server.config.AllProjectsName;
 import com.google.gerrit.server.config.SitePaths;
 import com.google.gerrit.server.project.GroupList;
@@ -39,11 +40,13 @@ public class AllProjectsConfig extends VersionedMetaDataOnInit {
   private GroupList groupList;
 
   @Inject
-  AllProjectsConfig(AllProjectsNameOnInitProvider allProjects, SitePaths site, InitFlags flags) {
+  AllProjectsConfig(
+      AllProjectsNameOnInitProvider allProjects,
+      AllProjectsConfigProvider allProjectsConfigProvider,
+      SitePaths site,
+      InitFlags flags) {
     super(flags, site, allProjects.get(), RefNames.REFS_CONFIG);
-    this.baseConfig =
-        ProjectConfig.Factory.getBaseConfig(
-            site, new AllProjectsName(allProjects.get()), Project.nameKey(allProjects.get()));
+    this.baseConfig = allProjectsConfigProvider.get(new AllProjectsName(allProjects.get()));
   }
 
   public Config getConfig() {

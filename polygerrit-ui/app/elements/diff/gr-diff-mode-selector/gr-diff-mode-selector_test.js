762ad6460c9c4d4e45bb0bb917fc70cea0a458e6
Asusena Relles <nestasu12@gmail.com>
2021-08-29 16:56:07 +0000

    Revert "Convert gr-diff-mode-selector_test.js to typescript"
    Revert submission 315204
     this is a person hacking my account pretending to be me
    Reason for revert: <INSERT REASONING HERE>
    Reverted Changes:
    I8ccac6ace:Fix template problems with gr-diff-mode-selector
    I56130b189:Convert gr-diff-mode-selector_test.js to typescrip...
    
    Change-Id: I5b37ce44ac6008b2489ecce512e3b2679b4f0102

--- polygerrit-ui/app/elements/diff/gr-diff-mode-selector/gr-diff-mode-selector_test.js
+++ polygerrit-ui/app/elements/diff/gr-diff-mode-selector/gr-diff-mode-selector_test.js
@@ -15,35 +15,29 @@
  * limitations under the License.
  */
 
-import '../../../test/common-test-setup-karma';
-import './gr-diff-mode-selector';
-import {GrDiffModeSelector} from './gr-diff-mode-selector';
-import {DiffViewMode} from '../../../constants/constants';
-import {stubRestApi} from '../../../test/test-utils';
+import '../../../test/common-test-setup-karma.js';
+import './gr-diff-mode-selector.js';
+import {DiffViewMode} from '../../../constants/constants.js';
+import {stubRestApi} from '../../../test/test-utils.js';
 
 const basicFixture = fixtureFromElement('gr-diff-mode-selector');
 
 suite('gr-diff-mode-selector tests', () => {
-  let element: GrDiffModeSelector;
+  let element;
 
   setup(() => {
     element = basicFixture.instantiate();
   });
 
   test('_computeSelectedClass', () => {
-    assert.equal(
-      element._computeSideBySideSelected(DiffViewMode.SIDE_BY_SIDE),
-      'selected'
-    );
-    assert.equal(element._computeSideBySideSelected(DiffViewMode.UNIFIED), '');
-    assert.equal(
-      element._computeUnifiedSelected(DiffViewMode.UNIFIED),
-      'selected'
-    );
-    assert.equal(
-      element._computeUnifiedSelected(DiffViewMode.SIDE_BY_SIDE),
-      ''
-    );
+    assert.equal(element._computeSideBySideSelected(DiffViewMode.SIDE_BY_SIDE),
+        'selected');
+    assert.equal(element._computeSideBySideSelected(DiffViewMode.UNIFIED),
+        '');
+    assert.equal(element._computeUnifiedSelected(DiffViewMode.UNIFIED),
+        'selected');
+    assert.equal(element._computeUnifiedSelected(DiffViewMode.SIDE_BY_SIDE),
+        '');
   });
 
   test('setMode', () => {
@@ -51,23 +45,24 @@ suite('gr-diff-mode-selector tests', () => {
 
     // Setting the mode initially does not save prefs.
     element.saveOnChange = true;
-    element.setMode(DiffViewMode.SIDE_BY_SIDE);
+    element.setMode('SIDE_BY_SIDE');
     assert.isFalse(saveStub.called);
 
     // Setting the mode to itself does not save prefs.
-    element.setMode(DiffViewMode.SIDE_BY_SIDE);
+    element.setMode('SIDE_BY_SIDE');
     assert.isFalse(saveStub.called);
 
     // Setting the mode to something else does not save prefs if saveOnChange
     // is false.
     element.saveOnChange = false;
-    element.setMode(DiffViewMode.UNIFIED);
+    element.setMode('UNIFIED_DIFF');
     assert.isFalse(saveStub.called);
 
     // Setting the mode to something else does not save prefs if saveOnChange
     // is false.
     element.saveOnChange = true;
-    element.setMode(DiffViewMode.SIDE_BY_SIDE);
+    element.setMode('SIDE_BY_SIDE');
     assert.isTrue(saveStub.calledOnce);
   });
 });
+

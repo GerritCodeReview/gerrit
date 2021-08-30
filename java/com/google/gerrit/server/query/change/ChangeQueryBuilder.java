08e48c85ac84aecfa6e8478c5564022acb89c30b
Youssef Elghareeb <ghareeb@google.com>
2021-08-30 14:16:08 +0200

    Add a rule:$ruleName change predicate
    We allow searching for changes for which a specific submit rule
    evaluates to some status. The following formats are supported:
    1) rule:$ruleName
    2) rule:$ruleName=$status
    
    The former matches with changes for which the submit rule returns a
    fulfilled submit record (status in {OK, FORCED}). The latter matches
    with a specific submit record status. The $ruleName should be in the
    form "$plugin_name~$rule_name" where $rule_name is the name of the class
    that implements the submit rule.
    
    Change-Id: I0602cbff769425beab662f2f492f0395cc15a55a

--- java/com/google/gerrit/server/query/change/ChangeQueryBuilder.java
+++ java/com/google/gerrit/server/query/change/ChangeQueryBuilder.java
@@ -78,8 +78,11 @@ import com.google.gerrit.server.index.change.ChangeIndexRewriter;
 import com.google.gerrit.server.notedb.ReviewerStateInternal;
 import com.google.gerrit.server.patch.PatchListCache;
 import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.gerrit.server.plugincontext.PluginSetContext;
+import com.google.gerrit.server.plugincontext.PluginSetEntryContext;
 import com.google.gerrit.server.project.ChildProjects;
 import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.rules.SubmitRule;
 import com.google.gerrit.server.submit.SubmitDryRun;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -87,9 +90,11 @@ import com.google.inject.ProvisionException;
 import com.google.inject.util.Providers;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -247,6 +252,7 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData, ChangeQueryBuil
     final boolean indexMergeable;
     final boolean conflictsPredicateEnabled;
     final HasOperandAliasConfig hasOperandAliasConfig;
+    final PluginSetContext<SubmitRule> submitRules;
 
     private final Provider<CurrentUser> self;
 
@@ -281,7 +287,8 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData, ChangeQueryBuil
         OperatorAliasConfig operatorAliasConfig,
         @GerritServerConfig Config gerritConfig,
         HasOperandAliasConfig hasOperandAliasConfig,
-        ChangeIsVisibleToPredicate.Factory changeIsVisbleToPredicateFactory) {
+        ChangeIsVisibleToPredicate.Factory changeIsVisbleToPredicateFactory,
+        PluginSetContext<SubmitRule> submitRules) {
       this(
           queryProvider,
           rewriter,
@@ -312,7 +319,8 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData, ChangeQueryBuil
           MergeabilityComputationBehavior.fromConfig(gerritConfig).includeInIndex(),
           gerritConfig.getBoolean("change", null, "conflictsPredicateEnabled", true),
           hasOperandAliasConfig,
-          changeIsVisbleToPredicateFactory);
+          changeIsVisbleToPredicateFactory,
+          submitRules);
     }
 
     private Arguments(
@@ -345,7 +353,8 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData, ChangeQueryBuil
         boolean indexMergeable,
         boolean conflictsPredicateEnabled,
         HasOperandAliasConfig hasOperandAliasConfig,
-        ChangeIsVisibleToPredicate.Factory changeIsVisbleToPredicateFactory) {
+        ChangeIsVisibleToPredicate.Factory changeIsVisbleToPredicateFactory,
+        PluginSetContext<SubmitRule> submitRules) {
       this.queryProvider = queryProvider;
       this.rewriter = rewriter;
       this.opFactories = opFactories;
@@ -376,6 +385,7 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData, ChangeQueryBuil
       this.indexMergeable = indexMergeable;
       this.conflictsPredicateEnabled = conflictsPredicateEnabled;
       this.hasOperandAliasConfig = hasOperandAliasConfig;
+      this.submitRules = submitRules;
     }
 
     Arguments asUser(CurrentUser otherUser) {
@@ -409,7 +419,8 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData, ChangeQueryBuil
           indexMergeable,
           conflictsPredicateEnabled,
           hasOperandAliasConfig,
-          changeIsVisbleToPredicateFactory);
+          changeIsVisbleToPredicateFactory,
+          submitRules);
     }
 
     Arguments asUser(Account.Id otherId) {
@@ -562,6 +573,35 @@ public class ChangeQueryBuilder extends QueryBuilder<ChangeData, ChangeQueryBuil
 
   public Predicate<ChangeData> statusOpen() {
     return ChangeStatusPredicate.open();
+  }
+
+  @Operator
+  public Predicate<ChangeData> rule(String value) throws QueryParseException {
+    String ruleNameArg = value;
+    String statusArg = null;
+    String[] queryArgs = value.split("=");
+    if (queryArgs.length > 2) {
+      throw new QueryParseException("Invalid query arguments");
+    }
+    if (queryArgs.length == 2) {
+      ruleNameArg = queryArgs[0];
+      statusArg = queryArgs[1];
+    }
+    Iterator<PluginSetEntryContext<SubmitRule>> submitRulesItr = args.submitRules.iterator();
+    while (submitRulesItr.hasNext()) {
+      PluginSetEntryContext<SubmitRule> context = submitRulesItr.next();
+      SubmitRule submitRule = context.get();
+      String ruleName = context.getPluginName() + "~" + submitRule.getClass().getSimpleName();
+      if (ruleNameArg.equals(ruleName)) {
+        return statusArg == null
+            ? Predicate.or(
+                Arrays.asList(
+                    ChangePredicates.submitRuleStatus(ruleName + "=" + SubmitRecord.Status.OK),
+                    ChangePredicates.submitRuleStatus(ruleName + "=" + SubmitRecord.Status.FORCED)))
+            : ChangePredicates.submitRuleStatus(ruleName + "=" + statusArg);
+      }
+    }
+    throw new IllegalArgumentException("Invalid submit rule name");
   }
 
   @Operator

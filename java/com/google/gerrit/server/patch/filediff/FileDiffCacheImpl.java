16b59944656dbc100f0867f0ef0d2a079d524ee7
Youssef Elghareeb <ghareeb@google.com>
2021-09-03 15:07:34 +0200

    Fix caching of negative file diffs
    Background: In DiffOperations we request file diffs from the
    FileDiffCache using the default diff algorithm. If the cache loader
    times out during the computation, we cache and return a negative result.
    The caller (in DiffOperationsImpl) detects negative results and
    re-request the diff using the fallback algorithm.
    
    There was a tiny bug in FileDiffOutput#createNegative(): we created the
    negative entry but forgot to set the negative boolean. Caller then
    thinks that the result was empty and filters out the file without
    invoking the fallback algorithm.
    
    Bug: Google b/198744420
    Change-Id: I67a0e6c18cdc6b1bc1b867cf0759adba109055dd

--- java/com/google/gerrit/server/patch/filediff/FileDiffCacheImpl.java
+++ java/com/google/gerrit/server/patch/filediff/FileDiffCacheImpl.java
@@ -97,7 +97,7 @@ public class FileDiffCacheImpl implements FileDiffCache {
         persist(DIFF, FileDiffCacheKey.class, FileDiffOutput.class)
             .maximumWeight(10 << 20)
             .weigher(FileDiffWeigher.class)
-            .version(7)
+            .version(8)
             .keySerializer(FileDiffCacheKey.Serializer.INSTANCE)
             .valueSerializer(FileDiffOutput.Serializer.INSTANCE)
             .loader(FileDiffLoader.class);

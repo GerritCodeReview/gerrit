827f5a3fbb70a2751fc4b866831e0c9c6a71053b
Dhruv Srivastava <dhruvsri@google.com>
2021-09-22 18:31:02 +0200

    Add auto reloading to change list view
    Change-Id: I39b0e479226c15babe2d1bb2a0d0b7629e3366cc

--- polygerrit-ui/app/elements/change-list/gr-change-list-view/gr-change-list-view.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-view/gr-change-list-view.ts
@@ -36,9 +36,10 @@ import {
 } from '../../../types/common';
 import {ChangeStarToggleStarDetail} from '../../shared/gr-change-star/gr-change-star';
 import {ChangeListViewState} from '../../../types/types';
-import {fireTitleChange} from '../../../utils/event-util';
+import {fireTitleChange, fireReload} from '../../../utils/event-util';
 import {appContext} from '../../../services/app-context';
 import {GerritView} from '../../../services/router/router-model';
+import { RELOAD_DASHBOARD_INTERVAL_MS } from '../gr-dashboard-view/gr-dashboard-view';
 
 const LOOKUP_QUERY_PATTERNS: RegExp[] = [
   /^\s*i?[0-9a-f]{7,40}\s*$/i, // CHANGE_ID
@@ -112,10 +113,24 @@ export class GrChangeListView extends PolymerElement {
 
   private reporting = appContext.reportingService;
 
+  private lastVisibleTimestampMs = 0;
+
   constructor() {
     super();
     this.addEventListener('next-page', () => this._handleNextPage());
     this.addEventListener('previous-page', () => this._handlePreviousPage());
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'visible') {
+        if (
+          Date.now() - this.lastVisibleTimestampMs >
+          RELOAD_DASHBOARD_INTERVAL_MS
+        )
+          fireReload(this);
+      } else {
+        this.lastVisibleTimestampMs = Date.now();
+      }
+    });
+
   }
 
   override connectedCallback() {

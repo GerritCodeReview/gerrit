9dcdaf8343d4133afd5029d2bb5f3dcd8357046b
Luca Milanesio <luca.milanesio@gmail.com>
2021-09-23 21:56:10 +0100

    PermissionAwareReadOnlyRefDatabase: remove copied getRefsByPrefix
    The getRefsByPrefix method is a mere copy&paste of the JGit's
    implementation in RefDatabase.
    
    There is no value in overriding a method and copying its body
    as-is, without any change in its logic.
    
    Change-Id: Icdc31afd2a766d6fc65498eb2db2b35ef606b882

--- java/com/google/gerrit/server/git/PermissionAwareReadOnlyRefDatabase.java
+++ java/com/google/gerrit/server/git/PermissionAwareReadOnlyRefDatabase.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.server.git;
 
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 import com.google.common.base.Preconditions;
@@ -121,30 +120,6 @@ public class PermissionAwareReadOnlyRefDatabase extends DelegateRefDatabase {
     }
 
     return refs.stream().collect(toMap(Ref::getName, r -> r));
-  }
-
-  @Override
-  public List<Ref> getRefsByPrefix(String prefix) throws IOException {
-    Map<String, Ref> coarseRefs;
-    int lastSlash = prefix.lastIndexOf('/');
-    if (lastSlash == -1) {
-      coarseRefs = getRefs(ALL);
-    } else {
-      coarseRefs = getRefs(prefix.substring(0, lastSlash + 1));
-    }
-
-    List<Ref> result;
-    if (lastSlash + 1 == prefix.length()) {
-      result = coarseRefs.values().stream().collect(toList());
-    } else {
-      String p = prefix.substring(lastSlash + 1);
-      result =
-          coarseRefs.entrySet().stream()
-              .filter(e -> e.getKey().startsWith(p))
-              .map(e -> e.getValue())
-              .collect(toList());
-    }
-    return Collections.unmodifiableList(result);
   }
 
   @Override

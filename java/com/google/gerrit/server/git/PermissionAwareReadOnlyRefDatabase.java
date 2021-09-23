d81a4fb765cad64fead38a11c179077d1238438a
Luca Milanesio <luca.milanesio@gmail.com>
2021-09-23 22:19:19 +0100

    Unify getRefs() and getRefsByPrefix() in permission-aware refdb
    PermissionAwareReadOnlyRefDatabase had a copy&paste of the JGit's
    RefDatabase.getRefsByPrefix() which is an unnecessary duplication
    of logic.
    
    The current getRefs() implementation already delegates its execution
    to the underlying RefDatabase.getRefsByPrefix() hence the copy&paste
    of the logic isn't needed.
    
    Adapt the result of getRefs() to the data-type returned by
    getRefsByPrefix().
    
    Change-Id: Ie7597ad7e5d585d98833cb9fe062e88ccfd1a071

--- java/com/google/gerrit/server/git/PermissionAwareReadOnlyRefDatabase.java
+++ java/com/google/gerrit/server/git/PermissionAwareReadOnlyRefDatabase.java
@@ -125,26 +125,7 @@ public class PermissionAwareReadOnlyRefDatabase extends DelegateRefDatabase {
 
   @Override
   public List<Ref> getRefsByPrefix(String prefix) throws IOException {
-    Map<String, Ref> coarseRefs;
-    int lastSlash = prefix.lastIndexOf('/');
-    if (lastSlash == -1) {
-      coarseRefs = getRefs(ALL);
-    } else {
-      coarseRefs = getRefs(prefix.substring(0, lastSlash + 1));
-    }
-
-    List<Ref> result;
-    if (lastSlash + 1 == prefix.length()) {
-      result = coarseRefs.values().stream().collect(toList());
-    } else {
-      String p = prefix.substring(lastSlash + 1);
-      result =
-          coarseRefs.entrySet().stream()
-              .filter(e -> e.getKey().startsWith(p))
-              .map(e -> e.getValue())
-              .collect(toList());
-    }
-    return Collections.unmodifiableList(result);
+    return getRefs(prefix).values().stream().collect(toList());
   }
 
   @Override

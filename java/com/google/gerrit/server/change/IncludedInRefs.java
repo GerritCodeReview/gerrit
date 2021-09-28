7a70fde7e7e40731cc70be1cff72659472ce5b20
Prudhvi Akhil Alahari <prudhvi@codeaurora.org>
2021-09-28 18:27:44 +0530

    Add REST endpoint for commits included in refs
    Add a REST endpoint /projects/{project-name}/commits:in which takes
    one or more commits and one or more refs and returns a map of commits
    to subset of unshortened refs from those input refs which contain the
    commit. We have an /in API which returns all branches and tags the
    commit is part of. But /in API is less performant if the caller is
    interested in a specific ref(s) and is limited to branches and tags.
    Thus, this change makes it possible to check for any specific ref(s).
    The CommitsCollection checks whether the commit is reachable from any
    of the refs visible to the user. The commits:in API checks if the commit
    is reachable from any of the input refs. To avoid performing
    reachability check twice, this API is exposed at /projects/{project-
    name}/ level instead of /projects/{project-name}/commits/{commit-id}/.
    Also allows us to enhance the API to take more than one commit. Adds
    acceptance tests and documentation.
    
    Change-Id: I756c55f87a203cc6693ada300c8441cfed15c9c8

---
+++ java/com/google/gerrit/server/change/IncludedInRefs.java
@@ -0,0 +1,115 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.change;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static java.util.stream.Collectors.toSet;
+
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.gerrit.server.permissions.PermissionBackend.RefFilterOptions;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.inject.Inject;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefDatabase;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+public class IncludedInRefs {
+  protected final GitRepositoryManager repoManager;
+  protected final PermissionBackend permissionBackend;
+
+  @Inject
+  IncludedInRefs(GitRepositoryManager repoManager, PermissionBackend permissionBackend) {
+    this.repoManager = repoManager;
+    this.permissionBackend = permissionBackend;
+  }
+
+  public Map<String, Collection<String>> apply(
+      Project.NameKey project, Set<String> commits, Set<String> refNames)
+      throws ResourceConflictException, BadRequestException, IOException,
+          PermissionBackendException, ResourceNotFoundException, AuthException {
+    try (Repository repo = repoManager.openRepository(project);
+        RevWalk revWalk = new RevWalk(repo)) {
+      RefDatabase refDb = repo.getRefDatabase();
+      Set<Ref> refs = new HashSet<>();
+      for (String refName : refNames) {
+        try {
+          Ref ref = refDb.exactRef(refName);
+          if (ref != null) {
+            refs.add(ref);
+          }
+        } catch (IOException e) {
+        }
+      }
+      // Filter refs according to their visibility by the user
+      Set<Ref> visibleRefs = filterReadableRefs(project, refs, repo);
+      if (!visibleRefs.isEmpty()) {
+        revWalk.setRetainBody(false);
+        Set<RevCommit> revCommits = new HashSet<>();
+        for (String commit : commits) {
+          try {
+            revCommits.add(revWalk.parseCommit(ObjectId.fromString(commit)));
+          } catch (MissingObjectException
+              | IncorrectObjectTypeException
+              | IllegalArgumentException e) {
+          }
+        }
+
+        if (!revCommits.isEmpty()) {
+          return IncludedInResolver.resolve(repo, revWalk, revCommits, visibleRefs).entrySet()
+              .stream()
+              .filter((entry) -> !entry.getValue().isEmpty())
+              .collect(
+                  toImmutableMap(
+                      entry -> entry.getKey().getName(),
+                      entry ->
+                          entry.getValue().stream().map(Ref::getName).collect(toImmutableList())));
+        }
+      }
+    }
+    return Collections.EMPTY_MAP;
+  }
+
+  /**
+   * Filter readable refs according to the caller's refs visibility.
+   *
+   * @param project specific Gerrit project.
+   * @param inputRefs a list of refs
+   * @param repo repository opened for the Gerrit project.
+   * @return set of visible refs to the caller
+   */
+  private Set<Ref> filterReadableRefs(Project.NameKey project, Set<Ref> inputRefs, Repository repo)
+      throws PermissionBackendException {
+    PermissionBackend.ForProject perm = permissionBackend.currentUser().project(project);
+    return perm.filter(inputRefs, repo, RefFilterOptions.defaults()).stream().collect(toSet());
+  }
+}

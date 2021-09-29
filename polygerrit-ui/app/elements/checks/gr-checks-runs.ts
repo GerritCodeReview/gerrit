82058f2dc62ad8601839646d5abdbba9420b88a4
Ben Rohlfs <brohlfs@google.com>
2021-09-29 11:20:08 +0200

    Add zero state for Check Runs
    Screenshot https://imgur.com/a/b4uS1CM
    
    Google-Bug-Id: b/186495870
    Change-Id: Iee21a9dc0f95389a9c5593ace6aa46fcbf5ddc89

--- polygerrit-ui/app/elements/checks/gr-checks-runs.ts
+++ polygerrit-ui/app/elements/checks/gr-checks-runs.ts
@@ -475,6 +475,11 @@ export class GrChecksRuns extends LitElement {
         .testing:hover * {
           visibility: visible;
         }
+        .zero {
+          padding: var(--spacing-m) 0;
+          color: var(--primary-text-color);
+          margin-top: var(--spacing-m);
+        }
         .login,
         .error {
           padding: var(--spacing-m);
@@ -534,7 +539,7 @@ export class GrChecksRuns extends LitElement {
         <div class="flex-space"></div>
         ${this.renderTitleButtons()} ${this.renderCollapseButton()}
       </h2>
-      ${this.renderErrors()} ${this.renderSignIn()}
+      ${this.renderErrors()} ${this.renderSignIn()} ${this.renderZeroState()}
       <input
         id="filterInput"
         type="text"
@@ -546,6 +551,11 @@ export class GrChecksRuns extends LitElement {
       ${this.renderSection(RunStatus.COMPLETED)}
       ${this.renderSection(RunStatus.RUNNABLE)} ${this.renderFakeControls()}
     `;
+  }
+
+  private renderZeroState() {
+    if (this.runs.length > 0) return;
+    return html`<div class="zero">No Check Run to show</div>`;
   }
 
   private renderErrors() {

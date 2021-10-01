7b09c6963b49846e142ff3371c7d29a24405410b
bro314 <ben.rohlfs@gmail.com>
2021-10-01 10:26:08 +0200

    Add a plugin API method for setting the content of a popup
    The PluginApi offers `popup()` to be called without a `moduleName`
    argument. That only makes sense if the caller has the ability to set
    the content of the popup by other means.
    
    This change adds a `appendContent()` method to the popup API.
    
    This was triggered by working on the automerger plugin, which currently
    has to rely on the private `_getElement()` method.
    
    Change-Id: Ic3aa6de9fe8afa7ea9542bd40bc98488e9947f88

--- polygerrit-ui/app/api/popup.ts
+++ polygerrit-ui/app/api/popup.ts
@@ -17,9 +17,10 @@
 
 export declare interface PopupPluginApi {
   /**
-   * Opens the popup, inserts it into DOM over current UI.
-   * Creates the popup if not previously created. Creates popup content element,
-   * if it was provided with constructor.
+   * Opens the popup, inserts it into the DOM over current UI.
+   * Creates the popup if not previously created. Creates and inserts the popup
+   * content element, if a `moduleName` was provided in the constructor.
+   * Otherwise you have to call `setContent()` when the promise resolves.
    */
   open(): Promise<PopupPluginApi>;
 
@@ -27,4 +28,10 @@ export declare interface PopupPluginApi {
    * Hides the popup.
    */
   close(): void;
+
+  /**
+   * Appends the given element as a child to the popup. Only call this method
+   * when you have called `popup()` without a `moduleName`.
+   */
+  appendContent(el: HTMLElement): void;
 }

4901e2ee1b8c25287dbca916b2cd504fe37800b4
Luca Milanesio <luca.milanesio@gmail.com>
2021-10-02 00:10:21 +0100

    Load an arbitrary version of change notes
    The SHA1 of the change notes to load may be already
    known to the caller of ChangeNotes.create() method hence
    it can be passed as a version parameter to avoid an unneeded
    ref lookup operation.
    
    When the version passed isn't known, it can be given as
    null parameter causing the lookup of the latest /meta ref
    SHA1 from the repository's ref-database.
    
    Allow skipping potentially hundreds of thousands of refs lookups
    for large mono-repos when filtering refs from the repository based
    on the change status.
    
    Also document the different variants of ChangeNotes.create()
    methods, highlighting their specific purpose and use-case.
    
    This is a partial cherry-pick of Ia54acb37e1187.
    
    Original-Author: Han-Wen Nienhuys <hanwen@google.com>
    Change-Id: I359578a05ecad595fb64909398ea3fd069ff1a5f

--- java/com/google/gerrit/testing/TestChanges.java
+++ java/com/google/gerrit/testing/TestChanges.java
@@ -91,7 +91,8 @@ public class TestChanges {
         injector
             .getInstance(ChangeUpdate.Factory.class)
             .create(
-                new ChangeNotes(injector.getInstance(AbstractChangeNotes.Args.class), c).load(),
+                new ChangeNotes(injector.getInstance(AbstractChangeNotes.Args.class), c, null, null)
+                    .load(),
                 user,
                 TimeUtil.nowTs(),
                 Ordering.<String>natural());

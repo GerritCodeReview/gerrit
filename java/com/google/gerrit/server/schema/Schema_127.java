02acf10073b6c977713adaab05106c3680087b93
Kaushik Lingarkar <kaushikl@codeaurora.org>
2021-10-06 11:20:52 -0700

    Parallelize inserts into accountPatchReviewDb in schema 127
    On large sites, parallelizing inserts helps reduce the migration
    time of this schema significantly. For example, on a postgres
    based site with ~25M records in account_patch_reviews, this change
    helps bring down the time from ~600s to 270s.
    
    Change-Id: I2e3d1aa28c94acd4278a7aa4450d302f1ce31865

--- java/com/google/gerrit/server/schema/Schema_127.java
+++ java/com/google/gerrit/server/schema/Schema_127.java
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package com.google.gerrit.server.schema;
 
+import com.google.common.collect.Lists;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.config.SitePaths;
@@ -26,11 +26,18 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import org.eclipse.jgit.lib.Config;
 
 public class Schema_127 extends SchemaVersion {
+  private static final int SLICE_SIZE = 10000;
   private static final int MAX_BATCH_SIZE = 1000;
-
+  // Use 10x threads for optimal performance
+  private static final int THREADS = Runtime.getRuntime().availableProcessors() * 10;
   private final SitePaths sitePaths;
   private final Config cfg;
   private final ThreadSettingsConfig threadSettingsConfig;
@@ -54,34 +61,77 @@ public class Schema_127 extends SchemaVersion {
             cfg, sitePaths, threadSettingsConfig);
     jdbcAccountPatchReviewStore.dropTableIfExists();
     jdbcAccountPatchReviewStore.createTableIfNotExists();
-    try (Connection con = jdbcAccountPatchReviewStore.getConnection();
-        PreparedStatement stmt =
-            con.prepareStatement(
-                "INSERT INTO account_patch_reviews "
-                    + "(account_id, change_id, patch_set_id, file_name) VALUES "
-                    + "(?, ?, ?, ?)")) {
-      int batchCount = 0;
-
-      try (Statement s = newStatement(db);
-          ResultSet rs = s.executeQuery("SELECT * from account_patch_reviews")) {
-        while (rs.next()) {
-          stmt.setInt(1, rs.getInt("account_id"));
-          stmt.setInt(2, rs.getInt("change_id"));
-          stmt.setInt(3, rs.getInt("patch_set_id"));
-          stmt.setString(4, rs.getString("file_name"));
-          stmt.addBatch();
-          batchCount++;
-          if (batchCount >= MAX_BATCH_SIZE) {
-            stmt.executeBatch();
-            batchCount = 0;
-          }
-        }
-      }
-      if (batchCount > 0) {
-        stmt.executeBatch();
+    List<AccountPatchReview> accountPatchReviews = Lists.newArrayList();
+    try (Statement s = newStatement(db);
+        ResultSet rs = s.executeQuery("SELECT * from account_patch_reviews")) {
+      while (rs.next()) {
+        accountPatchReviews.add(
+            new AccountPatchReview(
+                rs.getInt("account_id"),
+                rs.getInt("change_id"),
+                rs.getInt("patch_set_id"),
+                rs.getString("file_name")));
       }
     } catch (SQLException e) {
-      throw jdbcAccountPatchReviewStore.convertError("insert", e);
+      throw new RuntimeException(e);
+    }
+
+    ExecutorService pool = Executors.newFixedThreadPool(THREADS);
+    try {
+      for (List<AccountPatchReview> slice : Lists.partition(accountPatchReviews, SLICE_SIZE)) {
+        @SuppressWarnings("unused")
+        Future<?> unused =
+            pool.submit(
+                () -> {
+                  try {
+                    try (Connection con = jdbcAccountPatchReviewStore.getConnection();
+                        PreparedStatement stmt =
+                            con.prepareStatement(
+                                "INSERT INTO account_patch_reviews "
+                                    + "(account_id, change_id, patch_set_id, file_name) VALUES "
+                                    + "(?, ?, ?, ?)")) {
+                      int batchCount = 0;
+                      for (AccountPatchReview accountPatchReview : slice) {
+                        stmt.setInt(1, accountPatchReview.accountId);
+                        stmt.setInt(2, accountPatchReview.changeId);
+                        stmt.setInt(3, accountPatchReview.patchSetId);
+                        stmt.setString(4, accountPatchReview.fileName);
+                        stmt.addBatch();
+                        batchCount++;
+                        if (batchCount >= MAX_BATCH_SIZE) {
+                          stmt.executeBatch();
+                          batchCount = 0;
+                        }
+                      }
+                      if (batchCount > 0) {
+                        stmt.executeBatch();
+                      }
+                    } catch (SQLException e) {
+                      throw jdbcAccountPatchReviewStore.convertError("insert", e);
+                    }
+                  } catch (OrmException e) {
+                    throw new RuntimeException(e);
+                  }
+                });
+      }
+      pool.shutdown();
+      pool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static final class AccountPatchReview {
+    public final int accountId;
+    public final int changeId;
+    public final int patchSetId;
+    public final String fileName;
+
+    public AccountPatchReview(int accountId, int changeId, int patchSetId, String fileName) {
+      this.accountId = accountId;
+      this.changeId = changeId;
+      this.patchSetId = patchSetId;
+      this.fileName = fileName;
     }
   }
 }

f80bf5ec28c32f219b18f43bb23e7e16c146f575
Youssef Elghareeb <ghareeb@google.com>
2021-10-11 12:10:41 +0200

    gerrit: Document replacing prolog rules with submit requirements
    Put comments of how we can remove rules.pl from the gerrit repository
    and substitute it with submit requirements. We will apply these
    modifications in a follow up change.
    
    Change-Id: I9343dae42d03952c2d4c547eeedf47b5d821efcb

--- rules.pl
+++ rules.pl
@@ -1,19 +1,35 @@
-submit_rule(submit(CR)) :-
-  gerrit:change_branch('refs/meta/config'),
-  !,
-  gerrit:max_with_block(-2, 2, 'Code-Review', CR).
+% This file can be replaced with the following submit requirements
+%
+% 1) Code-Style (not applicable for the refs/meta/config branch)
+%   applicability_expr = -branch:refs/meta/config
+%   submittability_expr = label:Code-Style=MAX
+%   		     AND -label:Code-Style=MIN
+%   override_expr =
+%
+% 2) Verified (not applicable for the refs/meta/config branch)
+%   applicability_expr = -branch:refs/meta/config
+%   submittability_expr = label:Verified=MAX
+%   		     AND -label:Verified=MIN
+%   override_expr =
+%
+% 3) Library-Compliance
+%   applicability_expr = -branch:refs/meta/config AND (file:^lib/ OR
+%   			 file:^WORKSPACE$ OR file:^.gitmodules$)
+%   submittability_expr = label:Library-Compliance=MAX
+%   		    AND  -label:Library-Compliance=MIN
+%   override_expr =
+%
 
-submit_rule(submit(CR, CS, V, L)) :-
+submit_rule(submit(CS, V, L)) :-
   needs_library_compliance,
   !,
-  base(CR, CS, V),
+  base(CS, V),
   gerrit:max_with_block(-1, 1, 'Library-Compliance', L).
 
-submit_rule(submit(CR, CS, V)) :-
-  base(CR, CS, V).
+submit_rule(submit(CS, V)) :-
+  base(CS, V).
 
-base(CR, CS, V) :-
-  gerrit:max_with_block(-2, 2, 'Code-Review', CR),
+base(CS, V) :-
   gerrit:max_with_block(-1, 1, 'Code-Style', CS),
   gerrit:max_with_block(-1, 1, 'Verified', V).
 

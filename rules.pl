b9ca88def519060ce11932e63d4d7b88f372a1ba
Youssef Elghareeb <ghareeb@google.com>
2021-10-11 12:10:41 +0200

    gerrit: Document replacing prolog rules with submit requirements
    Put comments of how we can remove rules.pl from the gerrit repository
    and substitute it with submit requirements. We will apply these
    modifications in a follow up change.
    
    Change-Id: I9343dae42d03952c2d4c547eeedf47b5d821efcb

--- rules.pl
+++ rules.pl
@@ -1,3 +1,31 @@
+% This file can be replaced with the following submit requirements
+%
+% Code-Review
+%   applicability_expr =
+%   submittability_expr = label:Code-Review=MAX
+%   			 -label:Code-Review=MIN
+%   override_expr =
+%
+% Code-Style (not applicable for the refs/meta/config branch)
+%   applicability_expr = -branch:refs/meta/config
+%   submittability_expr = label:Code-Style=MAX
+%   			 -label:Code-Style=MIN
+%   override_expr =
+%
+% Verified (not applicable for the refs/meta/config branch)
+%   applicability_expr = -branch:refs/meta/config
+%   submittability_expr = label:Verified=MAX
+%   			 -label:Verified=MIN
+%   override_expr =
+%
+% Library-Compliance
+%   applicability_expr = -branch:refs/meta/config AND (file:^lib/ OR
+%   			 file:^WORKSPACE$ OR file:^.gitmodules$)
+%   submittability_expr = label:Library-Compliance=MAX
+%   			 -label:Library-Compliance=MIN
+%   override_expr = 
+%
+
 submit_rule(submit(CR)) :-
   gerrit:change_branch('refs/meta/config'),
   !,

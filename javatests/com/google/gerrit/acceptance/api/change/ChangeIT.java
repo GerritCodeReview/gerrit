969151e3eeae893f4e303f1823848292d00bddd1
Luca Milanesio <luca.milanesio@gmail.com>
2021-10-15 00:03:08 +0100

    ChangeIT: Use <project>~<change-num> for accessing changes
    Locating changes by <project>~<change-num> does not involve any
    lookup on secondary indexes, leading to a more precise and reliable
    retrieval of change data in tests.
    
    Also remove the duplication of code for building the String for
    a change identifier based on the result of the push of a new change,
    making the code more readable.
    
    Change-Id: I0b80b328b4ad03cd3690da7239c7e968b16fba3d

--- javatests/com/google/gerrit/acceptance/api/change/ChangeIT.java
+++ javatests/com/google/gerrit/acceptance/api/change/ChangeIT.java
@@ -260,8 +260,7 @@ public class ChangeIT extends AbstractDaemonTest {
     String fileName = "a_new_file.txt";
     String fileContent = "First line\nSecond line\n";
     PushOneCommit.Result result = createChange("Add a file", fileName, fileContent);
-    String triplet = project.get() + "~master~" + result.getChangeId();
-    ChangeInfo change = gApi.changes().id(triplet).get();
+    ChangeInfo change = gApi.changes().id(projectAndChangeNumId(result)).get();
     assertThat(change.insertions).isNotNull();
     assertThat(change.deletions).isNotNull();
   }
@@ -271,9 +270,10 @@ public class ChangeIT extends AbstractDaemonTest {
     String fileName = "a_new_file.txt";
     String fileContent = "First line\nSecond line\n";
     PushOneCommit.Result result = createChange("Add a file", fileName, fileContent);
-    String triplet = project.get() + "~master~" + result.getChangeId();
     ChangeInfo change =
-        gApi.changes().id(triplet).get(ImmutableList.of(ListChangesOption.SKIP_DIFFSTAT));
+        gApi.changes()
+            .id(projectAndChangeNumId(result))
+            .get(ImmutableList.of(ListChangesOption.SKIP_DIFFSTAT));
     assertThat(change.insertions).isNull();
     assertThat(change.deletions).isNull();
   }
@@ -283,11 +283,12 @@ public class ChangeIT extends AbstractDaemonTest {
     String fileName = "a_new_file.txt";
     String fileContent = "First line\nSecond line\n";
     PushOneCommit.Result result = createChange("Add a file", fileName, fileContent);
-    String triplet = project.get() + "~master~" + result.getChangeId();
     CacheStats startPatch = cloneStats(fileCache.stats());
     CacheStats startIntra = cloneStats(intraCache.stats());
     CacheStats startSummary = cloneStats(diffSummaryCache.stats());
-    gApi.changes().id(triplet).get(ImmutableList.of(ListChangesOption.SKIP_DIFFSTAT));
+    gApi.changes()
+        .id(projectAndChangeNumId(result))
+        .get(ImmutableList.of(ListChangesOption.SKIP_DIFFSTAT));
 
     assertThat(fileCache.stats()).since(startPatch).hasMissCount(0);
     assertThat(fileCache.stats()).since(startPatch).hasHitCount(0);
@@ -3156,9 +3157,9 @@ public class ChangeIT extends AbstractDaemonTest {
   @Test
   public void votable() throws Exception {
     PushOneCommit.Result r = createChange();
-    String triplet = project.get() + "~master~" + r.getChangeId();
-    gApi.changes().id(triplet).addReviewer(user.username());
-    ChangeInfo c = gApi.changes().id(triplet).get(DETAILED_LABELS);
+    String projectChangeId = projectAndChangeNumId(r);
+    gApi.changes().id(projectChangeId).addReviewer(user.username());
+    ChangeInfo c = gApi.changes().id(projectChangeId).get(DETAILED_LABELS);
     LabelInfo codeReview = c.labels.get(LabelId.CODE_REVIEW);
     assertThat(codeReview.all).hasSize(1);
     ApprovalInfo approval = codeReview.all.get(0);
@@ -3175,7 +3176,7 @@ public class ChangeIT extends AbstractDaemonTest {
                 .range(-1, 1))
         .update();
 
-    c = gApi.changes().id(triplet).get(DETAILED_LABELS);
+    c = gApi.changes().id(projectChangeId).get(DETAILED_LABELS);
     codeReview = c.labels.get(LabelId.CODE_REVIEW);
     assertThat(codeReview.all).hasSize(1);
     approval = codeReview.all.get(0);
@@ -3213,15 +3214,15 @@ public class ChangeIT extends AbstractDaemonTest {
     ChangeInfo info = gApi.changes().id(r.getChangeId()).get();
     assertThat(info.changeId).isEqualTo(r.getChangeId());
 
-    String triplet = project.get() + "~master~" + r.getChangeId();
-    info = gApi.changes().id(triplet).get();
+    String projectChangeid = projectAndChangeNumId(r);
+    info = gApi.changes().id(projectChangeid).get();
     assertThat(info.changeId).isEqualTo(r.getChangeId());
 
     info = gApi.changes().id(info._number).get();
     assertThat(info.changeId).isEqualTo(r.getChangeId());
     assertThrows(
         AuthException.class,
-        () -> gApi.changes().id(triplet).current().review(ReviewInput.approve()));
+        () -> gApi.changes().id(projectChangeid).current().review(ReviewInput.approve()));
   }
 
   @Test
@@ -3649,11 +3650,11 @@ public class ChangeIT extends AbstractDaemonTest {
     String heads = "refs/heads/*";
 
     PushOneCommit.Result r = createChange();
-    String triplet = project.get() + "~master~" + r.getChangeId();
+    String projectChangeId = projectAndChangeNumId(r);
 
-    gApi.changes().id(triplet).addReviewer(user.username());
+    gApi.changes().id(projectChangeId).addReviewer(user.username());
 
-    ChangeInfo c = gApi.changes().id(triplet).get(DETAILED_LABELS);
+    ChangeInfo c = gApi.changes().id(projectChangeId).get(DETAILED_LABELS);
     LabelInfo codeReview = c.labels.get(LabelId.CODE_REVIEW);
     assertThat(codeReview.all).hasSize(1);
     ApprovalInfo approval = codeReview.all.get(0);
@@ -3673,7 +3674,7 @@ public class ChangeIT extends AbstractDaemonTest {
                 .range(minPermittedValue, maxPermittedValue))
         .update();
 
-    c = gApi.changes().id(triplet).get(DETAILED_LABELS);
+    c = gApi.changes().id(projectChangeId).get(DETAILED_LABELS);
     codeReview = c.labels.get(LabelId.CODE_REVIEW);
     assertThat(codeReview.all).hasSize(1);
     approval = codeReview.all.get(0);
@@ -3696,11 +3697,11 @@ public class ChangeIT extends AbstractDaemonTest {
         .update();
 
     PushOneCommit.Result r = createChange();
-    String triplet = project.get() + "~master~" + r.getChangeId();
+    String projectChangeId = projectAndChangeNumId(r);
 
-    gApi.changes().id(triplet).addReviewer(user.username());
+    gApi.changes().id(projectChangeId).addReviewer(user.username());
 
-    ChangeInfo c = gApi.changes().id(triplet).get(DETAILED_LABELS);
+    ChangeInfo c = gApi.changes().id(projectChangeId).get(DETAILED_LABELS);
     LabelInfo codeReview = c.labels.get(LabelId.CODE_REVIEW);
     assertThat(codeReview.all).hasSize(1);
     ApprovalInfo approval = codeReview.all.get(0);
@@ -4188,17 +4189,17 @@ public class ChangeIT extends AbstractDaemonTest {
     try (Registration registration =
         extensionRegistry.newRegistration().add(changeIndexedCounter)) {
       PushOneCommit.Result r = createChange();
-      String triplet = project.get() + "~master~" + r.getChangeId();
+      String projectChangeId = projectAndChangeNumId(r);
       changeIndexedCounter.clear();
 
-      gApi.accounts().self().starChange(triplet);
-      ChangeInfo change = info(triplet);
+      gApi.accounts().self().starChange(projectChangeId);
+      ChangeInfo change = info(projectChangeId);
       assertThat(change.starred).isTrue();
       assertThat(change.stars).contains(DEFAULT_LABEL);
       changeIndexedCounter.assertReindexOf(change);
 
-      gApi.accounts().self().unstarChange(triplet);
-      change = info(triplet);
+      gApi.accounts().self().unstarChange(projectChangeId);
+      change = info(projectChangeId);
       assertThat(change.starred).isNull();
       assertThat(change.stars).isNull();
       changeIndexedCounter.assertReindexOf(change);
@@ -4504,5 +4505,14 @@ public class ChangeIT extends AbstractDaemonTest {
       this.wip =
           event.getChange().workInProgress != null ? event.getChange().workInProgress : false;
     }
+  }
+
+  /**
+   * Returns a change-id string compose by <project>~<change-num> which is the best way to find a
+   * change without relying on index.
+   */
+  private String projectAndChangeNumId(PushOneCommit.Result r) {
+    Change change = r.getChange().change();
+    return change.getProject() + "~" + change.getChangeId();
   }
 }

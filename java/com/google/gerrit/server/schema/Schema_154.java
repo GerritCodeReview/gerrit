09d31ead0d552d885dc3188913384642933f57d1
Kaushik Lingarkar <kaushikl@codeaurora.org>
2021-10-18 15:51:37 -0700

    Avoid creating loose objects in Schema 154
    Use a PackInserter to create a single pack with all the updates
    instead of creating several thousand loose objects. Doing this
    is especially helpful when the repositories are based on NFS.
    Reducing loose objects keeps All-Users.git in a good state for
    subsequent schemas and also builds towards removing gc in other
    schemas(146 for example).
    
    On a postgres based site with repositories on NFS and with ~25k
    accounts, this change avoids creating ~25k loose refs and ~75k
    loose objects and also brings down the runtime of this schema
    from ~800s to ~150s.
    
    Change-Id: Icb2856dce4f5d3874088626096a4f97b18a0a0a4

--- java/com/google/gerrit/server/schema/Schema_154.java
+++ java/com/google/gerrit/server/schema/Schema_154.java
@@ -36,22 +36,22 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.internal.storage.file.GC;
+import org.eclipse.jgit.internal.storage.file.PackInserter;
+import org.eclipse.jgit.lib.BatchRefUpdate;
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.ProgressMonitor;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.TextProgressMonitor;
-import org.eclipse.jgit.storage.pack.PackConfig;
+import org.eclipse.jgit.revwalk.RevWalk;
 
 /** Migrate accounts to NoteDb. */
 public class Schema_154 extends SchemaVersion {
@@ -86,20 +86,22 @@ public class Schema_154 extends SchemaVersion {
   @Override
   protected void migrateData(ReviewDb db, UpdateUI ui) throws OrmException, SQLException {
     try {
-      try (Repository repo = repoManager.openRepository(allUsersName)) {
+      try (Repository repo = repoManager.openRepository(allUsersName);
+          PackInserter packInserter =
+              ((FileRepository) repo).getObjectDatabase().newPackInserter();
+          ObjectReader reader = packInserter.newReader();
+          RevWalk rw = new RevWalk(reader)) {
+        BatchRefUpdate bru = repo.getRefDatabase().newBatchUpdate();
         ProgressMonitor pm = new TextProgressMonitor();
         pm.beginTask("Collecting accounts", ProgressMonitor.UNKNOWN);
         Set<Account> accounts = scanAccounts(db, pm);
         pm.endTask();
         pm.beginTask("Migrating accounts to NoteDb", accounts.size());
-        int i = 0;
         for (Account account : accounts) {
-          updateAccountInNoteDb(repo, account);
-          pm.update(1);
-          if (++i % 100000 == 0) {
-            gc(repo, ui);
-          }
+          updateAccountInNoteDb(repo, account, bru, packInserter, reader, rw);
         }
+        packInserter.flush();
+        bru.execute(rw, pm);
         pm.endTask();
       }
     } catch (IOException | ConfigInvalidException e) {
@@ -142,53 +144,27 @@ public class Schema_154 extends SchemaVersion {
         .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
   }
 
-  private void updateAccountInNoteDb(Repository allUsersRepo, Account account)
+  private void updateAccountInNoteDb(
+      Repository allUsersRepo,
+      Account account,
+      BatchRefUpdate bru,
+      ObjectInserter oi,
+      ObjectReader reader,
+      RevWalk rw)
       throws IOException, ConfigInvalidException {
     MetaDataUpdate md =
-        new MetaDataUpdate(GitReferenceUpdated.DISABLED, allUsersName, allUsersRepo);
+        new MetaDataUpdate(GitReferenceUpdated.DISABLED, allUsersName, allUsersRepo, bru);
     PersonIdent ident = serverIdent.get();
     md.getCommitBuilder().setAuthor(ident);
     md.getCommitBuilder().setCommitter(ident);
     new AccountConfig(account.getId(), allUsersName, allUsersRepo)
         .load()
         .setAccount(account)
-        .commit(md);
+        .commit(md, oi, reader, rw);
   }
 
   @FunctionalInterface
   private interface AccountSetter {
     void set(Account a, ResultSet rs, String field) throws SQLException;
-  }
-
-  private double elapsed() {
-    return sw.elapsed(TimeUnit.MILLISECONDS) / 1000d;
-  }
-
-  private void gc(Repository repo, UpdateUI ui) {
-    if (repo instanceof FileRepository) {
-      ProgressMonitor pm = null;
-      try {
-        pm = new TextProgressMonitor();
-        FileRepository r = (FileRepository) repo;
-        GC gc = new GC(r);
-        // TODO(davido): Enable bitmap index when this JGit performance issue is fixed:
-        // https://bugs.eclipse.org/bugs/show_bug.cgi?id=562740
-        PackConfig pconfig = new PackConfig(repo);
-        pconfig.setBuildBitmaps(false);
-        gc.setPackConfig(pconfig);
-        gc.setProgressMonitor(pm);
-        pm.beginTask("gc", ProgressMonitor.UNKNOWN);
-        ui.message(String.format("... (%.3f s) gc --prune=now", elapsed()));
-        gc.setExpire(new Date());
-        gc.gc();
-        ui.message(String.format("... (%.3f s) full gc completed", elapsed()));
-      } catch (IOException | ParseException e) {
-        throw new RuntimeException(e);
-      } finally {
-        if (pm != null) {
-          pm.endTask();
-        }
-      }
-    }
   }
 }

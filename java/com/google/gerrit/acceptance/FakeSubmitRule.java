e8f218b9f2b9a2830243329159083cd45d8dad12
Edwin Kempin <ekempin@google.com>
2021-10-19 15:16:34 +0200

    Remove unused FakeSubmitRule.Module class
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: If8d7b911826890c15d26d1748bd01724a562cdc6

--- java/com/google/gerrit/acceptance/FakeSubmitRule.java
+++ java/com/google/gerrit/acceptance/FakeSubmitRule.java
@@ -16,23 +16,14 @@ package com.google.gerrit.acceptance;
 
 import com.google.gerrit.entities.SubmitRecord;
 import com.google.gerrit.entities.SubmitRecord.Status;
-import com.google.gerrit.extensions.annotations.Exports;
 import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.rules.SubmitRule;
-import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import java.util.Optional;
 
 /** Fake submit rule that returns OK if the change contains one or more hashtags. */
 @Singleton
 public class FakeSubmitRule implements SubmitRule {
-  public static class Module extends AbstractModule {
-    @Override
-    public void configure() {
-      bind(SubmitRule.class).annotatedWith(Exports.named("Fake")).to(FakeSubmitRule.class);
-    }
-  }
-
   @Override
   public Optional<SubmitRecord> evaluate(ChangeData cd) {
     SubmitRecord record = new SubmitRecord();

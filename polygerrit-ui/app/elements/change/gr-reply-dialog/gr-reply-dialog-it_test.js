674d6a3fa14071404dac134f4d5dbf8e41ee743f
Chris Poucet <poucet@google.com>
2021-10-19 19:13:01 +0200

    Fix aria issue with gr-label-score-row
    This broke in https://gerrit-review.googlesource.com/c/gerrit/+/319355
    Rather than wrap the gr-button inside of a gr-content-tooltip, do the
    inverse and ensure there's enough padding so that the hover-target is
    not too small.
    
    Google-Bug-Id: b/203460237
    Change-Id: I47a476a2270971200400880b9be356a603868e87

--- polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog-it_test.js
+++ polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog-it_test.js
@@ -127,7 +127,7 @@ suite('gr-reply-dialog-it tests', () => {
     const labelScoreRows = element.getLabelScores().shadowRoot
         .querySelector('gr-label-score-row[name="Code-Review"]');
     const selectedBtn = labelScoreRows.shadowRoot
-        .querySelector('gr-tooltip-content[data-value="+1"] > gr-button');
+        .querySelector('gr-button[data-value="+1"]');
     assert.isOk(selectedBtn);
   });
 });

661d90c758c8d1129f798ca2a371ebcdef1531a5
Patrick Hiesel <hiesel@google.com>
2021-10-20 19:35:54 +0200

    Submit requirements: Always compute requirements to populate dashboards
    We are validating SRs with users next week. As part of this evaluation
    we want to show redesigned dashboards as well. We don't want to upgrade
    the change index just yet as the data format might change.
    
    This commit makes it so that users that have enabled the experiment
    will always see their submit requirements freshly computed.
    
    This will slow down dashboards of users who have opted in to the
    experiment.
    
    Change-Id: I8cbc3f4f07c2b0e415b1232935d31e236613988d

--- java/com/google/gerrit/server/query/change/ChangeData.java
+++ java/com/google/gerrit/server/query/change/ChangeData.java
@@ -985,7 +985,15 @@ public class ChangeData {
 
   public void setSubmitRequirements(
       Map<SubmitRequirement, SubmitRequirementResult> submitRequirements) {
-    this.submitRequirements = submitRequirements;
+    if (!experimentFeatures.isFeatureEnabled(
+        ExperimentFeaturesConstants
+            .GERRIT_BACKEND_REQUEST_FEATURE_ENABLE_SUBMIT_REQUIREMENTS_BACKFILLING_ON_DASHBOARD)) {
+      // Only set back values from the index if the experiment is not active. While the experiment
+      // is active, we want
+      // to compute SRs from scratch to ensure fresh results.
+      // TODO(ghareeb, hiesel): Remove this.
+      this.submitRequirements = submitRequirements;
+    }
   }
 
   public List<SubmitRecord> submitRecords(SubmitRuleOptions options) {

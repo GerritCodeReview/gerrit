ce0b83be5f2be63d94bea799054a3c90aecec3d6
Kenyon Ralph <kenyon@kenyonralph.com>
2021-10-24 14:32:43 -0700

    sshd/HostKeyProvider: improve ssh-rsa host key warning message
    I believe this message is a better expression of the situation
    occurring in this code block.
    
    Change-Id: Ie17087eabdb411c594a8f3db21ca685a63f7ae1c

--- java/com/google/gerrit/sshd/HostKeyProvider.java
+++ java/com/google/gerrit/sshd/HostKeyProvider.java
@@ -73,9 +73,8 @@ class HostKeyProvider implements Provider<KeyPairProvider> {
         SimpleGeneratorHostKeyProvider p = new SimpleGeneratorHostKeyProvider();
         p.setAlgorithm(KeyUtils.RSA_ALGORITHM);
         p.setPath(objKey.toAbsolutePath());
-        logger.atWarning().log(
-            "Defaulting to RSA algorithm for SSH key exchange."
-                + "This is a weak security setting, consider changing it (see 'sshd.kex' documentation section).");
+        logger.atWarning().log("Only an ssh-rsa host key type exists. "
+            + "This is a weak key type, consider adding newer key types by running gerrit init.");
         return p;
       }
       stdKeys.add(objKey);

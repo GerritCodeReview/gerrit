0c69c60a7a65e16df3c940a887c3106c6c1fa625
Patrick Hiesel <hiesel@google.com>
2021-10-25 10:13:43 +0200

    Submit requirements: Use correct upper case Code-Review label consistently
    We observed test flakes where label votes where not applied correctly. This
    change makes casing consistent in tests and removes the @Ignore annotation.
    
    Change-Id: I6fdcb3b8460a0c05939b177b9bda06e09d7d6912

--- javatests/com/google/gerrit/acceptance/rest/project/CreateLabelIT.java
+++ javatests/com/google/gerrit/acceptance/rest/project/CreateLabelIT.java
@@ -109,11 +109,11 @@ public class CreateLabelIT extends AbstractDaemonTest {
             () ->
                 gApi.projects()
                     .name(allProjects.get())
-                    .label("code-review")
+                    .label("Code-Review")
                     .create(new LabelDefinitionInput()));
     assertThat(thrown)
         .hasMessageThat()
-        .contains("label code-review conflicts with existing label Code-Review");
+        .contains("label Code-Review conflicts with existing label Code-Review");
   }
 
   @Test

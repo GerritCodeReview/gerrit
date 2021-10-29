0d14d8647933d5f5cd032ac5b9a796802b90866d
Paladox none <thomasmulhall410@yahoo.com>
2021-10-29 21:52:00 +0000

    gr-group: Use gr-textarea
    This fixes an issue where the background for the description
    box was grey. It should be white if you can edit and darkened
    if you cannot.
    
    Release-Notes: skip
    Change-Id: I38ea8a2543b53c8319264fec686a709a69c43234
    (cherry picked from commit 278bbfb32cf976b82da47354f2b141f6b506de62)

--- polygerrit-ui/app/elements/admin/gr-group/gr-group_html.ts
+++ polygerrit-ui/app/elements/admin/gr-group/gr-group_html.ts
@@ -110,12 +110,14 @@ export const htmlTemplate = html`
           </h3>
           <fieldset>
             <div>
-              <iron-autogrow-textarea
+              <gr-textarea
                 class="description"
                 autocomplete="on"
-                bind-value="{{_groupConfig.description}}"
+                rows="4"
+                monospace=""
+                text="{{_groupConfig.description}}"
                 disabled="[[_computeGroupDisabled(_groupOwner, _isAdmin, _groupIsInternal)]]"
-              ></iron-autogrow-textarea>
+              ></gr-textarea>
             </div>
             <span
               class="value"

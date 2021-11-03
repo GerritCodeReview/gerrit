1ae7e45598eb484060fa71424a926303f69b1ff9
Dhruv Srivastava <dhruvsri@google.com>
2021-11-03 16:42:25 +0100

    Only mark file as reviewed on the initial diff load
    setReviewed observer marks the file as reviewed if the user has the
    setting enabled, observing on the reviewed files, prefs and path.
    
    This is incorrect since the method will be called everytime one of the
    properties is updated. After moving diff preferences to model, any
    update on the preferences triggers this observer leading to
    unnecessary calls.
    Hence call the observer only once when the initial properties are all
    set.
    
    Change-Id: Iae67f6eb18c7b8b59f2e92d8328bdc94d4359173

--- polygerrit-ui/app/services/router/router-model.ts
+++ polygerrit-ui/app/services/router/router-model.ts
@@ -57,6 +57,14 @@ export function _testOnly_resetState() {
 // Re-exporting as Observable so that you can only subscribe, but not emit.
 export const routerState$: Observable<RouterState> = privateState$;
 
+export function _testOnly_setState(state: RouterState) {
+  privateState$.next(state);
+}
+
+export function _testOnly_getState() {
+  return privateState$.getValue();
+}
+
 // Must only be used by the router service or whatever is in control of this
 // model.
 export function updateState(

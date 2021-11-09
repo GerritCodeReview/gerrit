1313b2f9e3f5616129b31f17cbe772ad8a878dfc
Youssef Elghareeb <ghareeb@google.com>
2021-11-09 14:24:22 +0100

    Add a JSON type adapter to submit requirement entities
    Bug: Google b/202927636
    Change-Id: I91d0ceaf8dd0b1c4d5b0245ac97258e1a956fc8e

--- java/com/google/gerrit/server/notedb/ChangeNoteJson.java
+++ java/com/google/gerrit/server/notedb/ChangeNoteJson.java
@@ -16,6 +16,7 @@ package com.google.gerrit.server.notedb;
 
 import com.google.common.collect.ImmutableList;
 import com.google.gerrit.entities.EntitiesAdapterFactory;
+import com.google.gerrit.entities.SubmitRequirementResult;
 import com.google.gerrit.json.EnumTypeAdapterFactory;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -39,6 +40,7 @@ public class ChangeNoteJson {
         .registerTypeAdapter(
             new TypeLiteral<ImmutableList<String>>() {}.getType(),
             new ImmutableListAdapter().nullSafe())
+        .registerTypeAdapter(SubmitRequirementResult.class, SubmitRequirementResult.typeAdapter())
         .setPrettyPrinting()
         .create();
   }

f0656987f94f5466bd91463d4a97e3a55c76017f
Youssef Elghareeb <ghareeb@google.com>
2021-11-09 14:24:22 +0100

    Add a JSON type adapter to submit requirement entities
    Bug: Google b/202927636
    Change-Id: I91d0ceaf8dd0b1c4d5b0245ac97258e1a956fc8e

--- java/com/google/gerrit/entities/SubmitRequirement.java
+++ java/com/google/gerrit/entities/SubmitRequirement.java
@@ -15,8 +15,13 @@
 package com.google.gerrit.entities;
 
 import com.google.auto.value.AutoValue;
-import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
 import java.util.Optional;
 
 /** Entity describing a requirement that should be met for a change to become submittable. */
@@ -64,8 +69,85 @@ public abstract class SubmitRequirement {
     return new AutoValue_SubmitRequirement.Builder();
   }
 
-  public static TypeAdapter<SubmitRequirement> typeAdapter(Gson gson) {
-    return new AutoValue_SubmitRequirement.GsonTypeAdapter(gson);
+  public static TypeAdapter<SubmitRequirement> typeAdapter() {
+    return new GsonTypeAdapter();
+  }
+
+  /** Json serializer for {@link SubmitRequirement}. */
+  static class GsonTypeAdapter extends TypeAdapter<SubmitRequirement> {
+    private static final String KEY_NAME = "name";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_APPLICABILITY_EXPRESSION = "applicabilityExpression";
+    private static final String KEY_SUBMITTABILITY_EXPRESSION = "submittabilityExpression";
+    private static final String KEY_OVERRIDE_EXPRESSION = "overrideExpression";
+    private static final String KEY_ALLLOW_OVERRIDE_IN_CHILD_PROJECTS =
+        "allowOverrideInChildProjects";
+
+    @Override
+    public void write(JsonWriter out, SubmitRequirement req) throws IOException {
+      TypeAdapter<SubmitRequirementExpression> expressionAdapter =
+          SubmitRequirementExpression.typeAdapter();
+      out.beginObject();
+      out.name(KEY_NAME).value(req.name());
+      if (req.description().isPresent()) {
+        out.name(KEY_DESCRIPTION).value(req.description().get());
+      }
+      if (req.applicabilityExpression().isPresent()) {
+        out.name(KEY_APPLICABILITY_EXPRESSION)
+            .jsonValue(expressionAdapter.toJson(req.applicabilityExpression().get()));
+      }
+      out.name(KEY_SUBMITTABILITY_EXPRESSION)
+          .jsonValue(expressionAdapter.toJson(req.submittabilityExpression()));
+      if (req.overrideExpression().isPresent()) {
+        out.name(KEY_OVERRIDE_EXPRESSION)
+            .jsonValue(expressionAdapter.toJson(req.overrideExpression().get()));
+      }
+      out.name(KEY_ALLLOW_OVERRIDE_IN_CHILD_PROJECTS).value(req.allowOverrideInChildProjects());
+      out.endObject();
+    }
+
+    @Override
+    public SubmitRequirement read(JsonReader in) throws IOException {
+      JsonObject parsed = new JsonParser().parse(in).getAsJsonObject();
+      Builder builder = SubmitRequirement.builder();
+      if (parsed.has(KEY_NAME)) {
+        builder.setName(unpack(parsed.get(KEY_NAME)).getAsString());
+      }
+      if (parsed.has(KEY_DESCRIPTION)) {
+        builder.setDescription(Optional.of(unpack(parsed.get(KEY_DESCRIPTION)).getAsString()));
+      }
+      if (parsed.has(KEY_APPLICABILITY_EXPRESSION)) {
+        builder.setApplicabilityExpression(
+            Optional.of(deserializeExpression(parsed.get(KEY_APPLICABILITY_EXPRESSION))));
+      }
+      builder.setSubmittabilityExpression(
+          deserializeExpression(parsed.get(KEY_SUBMITTABILITY_EXPRESSION)));
+      if (parsed.has(KEY_OVERRIDE_EXPRESSION)) {
+        builder.setOverrideExpression(
+            Optional.of(deserializeExpression(parsed.get(KEY_OVERRIDE_EXPRESSION))));
+      }
+      builder.setAllowOverrideInChildProjects(
+          unpack(parsed.get(KEY_ALLLOW_OVERRIDE_IN_CHILD_PROJECTS)).getAsBoolean());
+      return builder.build();
+    }
+
+    private static SubmitRequirementExpression deserializeExpression(JsonElement elem) {
+      return SubmitRequirementExpression.typeAdapter().fromJsonTree(unpack(elem));
+    }
+
+    /**
+     * Unpack the {@code in} {@link JsonElement}, i.e. if the element has a single "value" child
+     * return it. We've previously used the default Gson serializer for serializing submit
+     * requirements entities. This unpacking is needed to preserve backward compatibility while
+     * deserializing entities that were already serialized by the default serializer.
+     */
+    private static JsonElement unpack(JsonElement in) {
+      if (!in.isJsonObject()) {
+        return in;
+      }
+      JsonObject asJsonObject = in.getAsJsonObject();
+      return asJsonObject.has("value") && asJsonObject.size() == 1 ? asJsonObject.get("value") : in;
+    }
   }
 
   @AutoValue.Builder

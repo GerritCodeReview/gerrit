1ac97005e3dbeaef4e4b70a5267c00f71893f606
Youssef Elghareeb <ghareeb@google.com>
2021-11-09 14:24:22 +0100

    Submit requirements: Add a custom serializer for ObjectId
    Submit requirement results are serialized and stored as Json in NoteDb
    when the change is merged. We use a custom Gson serializer (defined in
    ChangeNoteJson) that registers adapters to different objects. We have
    not previously registered an adapter for the ObjectId class, resulting
    in serializing the ObjectId using the JGit format {w1, w2, w3, w4, w5}.
    
    In this change, we implement a custom adapter for ObjectId and ensure
    that the "deserialize" method can parse both the JGit and the new format
    to preserve backward compatibility.
    
    I also added tests to ensure proper serialization for all submit
    requirement entities: SubmitRequirement, SubmitRequirementExpression,
    SubmitRequirementResult and SubmitRequirementExpressionResult.
    
    Bug: Google b/202927636
    Change-Id: I91d0ceaf8dd0b1c4d5b0245ac97258e1a956fc8e

--- java/com/google/gerrit/server/notedb/ChangeNoteJson.java
+++ java/com/google/gerrit/server/notedb/ChangeNoteJson.java
@@ -19,6 +19,9 @@ import com.google.gerrit.entities.EntitiesAdapterFactory;
 import com.google.gerrit.json.EnumTypeAdapterFactory;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
@@ -26,6 +29,9 @@ import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.List;
+import org.eclipse.jgit.lib.ObjectId;
 
 @Singleton
 public class ChangeNoteJson {
@@ -39,12 +45,44 @@ public class ChangeNoteJson {
         .registerTypeAdapter(
             new TypeLiteral<ImmutableList<String>>() {}.getType(),
             new ImmutableListAdapter().nullSafe())
+        .registerTypeAdapter(ObjectId.class, new ObjectIdAdapter())
         .setPrettyPrinting()
         .create();
   }
 
   public Gson getGson() {
     return gson;
+  }
+
+  /** Json serializer for the {@link ObjectId} class. */
+  static class ObjectIdAdapter extends TypeAdapter<ObjectId> {
+    private static final List<String> legacyFields = Arrays.asList("w1", "w2", "w3", "w4", "w5");
+
+    @Override
+    public void write(JsonWriter out, ObjectId value) throws IOException {
+      out.value(value.name());
+    }
+
+    @Override
+    public ObjectId read(JsonReader in) throws IOException {
+      JsonElement parsed = new JsonParser().parse(in);
+      if (parsed.isJsonObject() && isJGitFormat(parsed)) {
+        // Some object IDs may have been serialized using the JGit format using the five integers
+        // w1, w2, w3, w4, w5. Detect this case so that we can deserialize properly.
+        int[] raw =
+            legacyFields.stream()
+                .mapToInt(field -> parsed.getAsJsonObject().get(field).getAsInt())
+                .toArray();
+        return ObjectId.fromRaw(raw);
+      }
+      return ObjectId.fromString(parsed.getAsString());
+    }
+
+    /** Return true if the json element contains the JGit serialized format of the Object ID. */
+    private boolean isJGitFormat(JsonElement elem) {
+      JsonObject asObj = elem.getAsJsonObject();
+      return legacyFields.stream().allMatch(field -> asObj.has(field));
+    }
   }
 
   static class ImmutableListAdapter extends TypeAdapter<ImmutableList<String>> {

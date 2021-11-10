aa3acfe53c841036753373f906dd64b33bccd7c1
Youssef Elghareeb <ghareeb@google.com>
2021-11-10 14:35:33 +0100

    MergeOp: request submit records with allowClosed=false if change is new
    If the change is new, we can always pass the variable allowClosed=false
    while requesting submit records, since submit records are always
    evaluated for open changes, hence this parameter is redundant.
    
    While at it, we also remove the "allowClosed" from
    MergeOp#checkSubmitRule since all callers now pass false.
    
    Change-Id: I5b4ada129947c96b34155a08f2b874679b834e0f

--- java/com/google/gerrit/server/restapi/change/Submit.java
+++ java/com/google/gerrit/server/restapi/change/Submit.java
@@ -257,7 +257,7 @@ public class Submit
           return "Change " + c.getId() + " is marked work in progress";
         }
         try {
-          MergeOp.checkSubmitRule(c, false);
+          MergeOp.checkSubmitRule(c);
         } catch (ResourceConflictException e) {
           return "Change " + c.getId() + " is not ready: " + e.getMessage();
         }
@@ -299,7 +299,7 @@ public class Submit
 
     ChangeData cd = resource.getChangeResource().getChangeData();
     try {
-      MergeOp.checkSubmitRule(cd, false);
+      MergeOp.checkSubmitRule(cd);
     } catch (ResourceConflictException e) {
       return null; // submit not visible
     }

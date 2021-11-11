a686ea944b784f4f5310cc1f0b9a9b3f74b461b6
Frank Borden <frankborden@google.com>
2021-11-11 17:35:00 +0100

    topic summary and tree skeletons
    - Currently data is fetched in gr-topic-summary and gr-topic-tree, but
    both will happen in the model when it is added later.
    - There will likely be another subsection component between
    gr-topic-tree-project and gr-topic-tree-row to separate stacks of
    related change within a project.
    - No styling yet, for spacing alignment everything will probably be
    put in a <table> with <template> elements separating the components
    similar to gr-change-list.
    
    https://imgur.com/a/bbPWDEt
    
    Google-Bug-Id: b/201529574
    Change-Id: Idfc5c8c26f660b8e9fa21b7d592ac349fe18c41e

---
+++ polygerrit-ui/app/elements/topic/gr-topic-tree-project.ts
@@ -0,0 +1,55 @@
+/**
+ * @license
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import './gr-topic-tree-row';
+import {customElement, property} from 'lit/decorators';
+import {LitElement, html} from 'lit-element/lit-element';
+import '../shared/gr-button/gr-button';
+import {ChangeInfo} from '../../api/rest-api';
+
+/**
+ * A view of changes that all belong to the same project.
+ */
+@customElement('gr-topic-tree-project')
+export class GrTopicTreeProject extends LitElement {
+  @property({type: String})
+  projectName?: string;
+
+  @property({type: Array})
+  changes?: ChangeInfo[];
+
+  override render() {
+    if (this.projectName === undefined || this.changes === undefined) {
+      return html``;
+    }
+    // TODO: Groups of related changes should be separated within the project.
+    return html`
+      <h2>Project ${this.projectName}</h2>
+      ${this.changes.map(
+        change => html`
+          <gr-topic-tree-row .change=${change}></gr-topic-tree-row>
+        `
+      )}
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-topic-tree-project': GrTopicTreeProject;
+  }
+}

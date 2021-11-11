0633ae11fb432af4992beda468d87863c075b408
Dhruv Srivastava <dhruvsri@google.com>
2021-11-11 15:16:47 +0100

    Refactor navigateToChange to take options
    Instead of specifying 5 undefined parameters if we want to do a force
    reload, specify the necessary parameter only.
    
    Change-Id: Ibae0a57c29f115ef3fdb5c7d381ff1b350b2775d

--- polygerrit-ui/app/elements/core/gr-navigation/gr-navigation.ts
+++ polygerrit-ui/app/elements/core/gr-navigation/gr-navigation.ts
@@ -439,6 +439,14 @@ export enum WeblinkType {
   RESOLVE_CONFLICTS = 'resolve-conflicts',
 }
 
+interface NavigateToChangeParams {
+  patchNum?: PatchSetNum;
+  basePatchNum?: BasePatchSetNum;
+  isEdit?: boolean;
+  redirect?: boolean;
+  forceReload?: boolean;
+}
+
 // TODO(dmfilippov) Convert to class, extract consts, give better name and
 // expose as a service from appContext
 export const GerritNav = {
@@ -659,15 +667,11 @@ export const GerritNav = {
    *     of the view. In certain cases we want to force the view to reload
    *     and re-render everything.
    */
-  // TODO(dhruvsri): move the arguments into one options object
   navigateToChange(
     change: Pick<ChangeInfo, '_number' | 'project' | 'internalHost'>,
-    patchNum?: PatchSetNum,
-    basePatchNum?: BasePatchSetNum,
-    isEdit?: boolean,
-    redirect?: boolean,
-    forceReload?: boolean
+    options: NavigateToChangeParams = {}
   ) {
+    const {patchNum, basePatchNum, isEdit, forceReload, redirect} = options;
     this._navigate(
       this.getUrlForChange(
         change,

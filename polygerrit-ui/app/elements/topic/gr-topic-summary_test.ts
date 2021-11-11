21192ac8b44ee038178f41e6269183c91474bb8d
Frank Borden <frankborden@google.com>
2021-11-11 17:35:00 +0100

    topic summary and tree skeletons
    The data flow will change once the model is added.
    No styling yet.
    https://imgur.com/a/bbPWDEt
    
    Change-Id: Idfc5c8c26f660b8e9fa21b7d592ac349fe18c41e

---
+++ polygerrit-ui/app/elements/topic/gr-topic-summary_test.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import '../../test/common-test-setup-karma';
+import {createChange} from '../../test/test-data-generators';
+import {queryAll, stubRestApi} from '../../test/test-utils';
+import './gr-topic-summary';
+import {GrTopicSummary} from './gr-topic-summary';
+
+const basicFixture = fixtureFromElement('gr-topic-summary');
+
+suite('gr-topic-summary tests', () => {
+  let element: GrTopicSummary;
+
+  setup(async () => {
+    stubRestApi('getChanges')
+      .withArgs(25, 'topic:myTopic')
+      .resolves([createChange(),createChange(), createChange()]);
+    element = basicFixture.instantiate();
+    element.topicName = 'myTopic';
+    await element.updateComplete;
+  });
+
+  test('shows topic information', () => {
+    const labels = queryAll<HTMLSpanElement>(element, 'span');
+    assert.equal(labels[0].textContent, 'Topic: myTopic');
+    assert.equal(labels[1].textContent, '3 changes');
+  });
+});

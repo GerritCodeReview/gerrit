57e32a9937fd93e43bb428cdd66149d1f98c11ac
Paladox none <thomasmulhall410@yahoo.com>
2021-11-14 22:05:24 +0000

    Convert gr-admin-group-list to lit
    Change-Id: I0c2cd217a04383ac862ebc862d258dd02eaf4c05

--- polygerrit-ui/app/elements/admin/gr-admin-group-list/gr-admin-group-list_test.ts
+++ polygerrit-ui/app/elements/admin/gr-admin-group-list/gr-admin-group-list_test.ts
@@ -30,6 +30,7 @@ import {GerritView} from '../../../services/router/router-model';
 import {GrListView} from '../../shared/gr-list-view/gr-list-view';
 import {GrDialog} from '../../shared/gr-dialog/gr-dialog';
 import {GrOverlay} from '../../shared/gr-overlay/gr-overlay';
+import {SHOWN_ITEMS_COUNT} from '../../../constants/constants';
 
 const basicFixture = fixtureFromElement('gr-admin-group-list');
 
@@ -70,11 +71,12 @@ suite('gr-admin-group-list tests', () => {
 
   const value: AppElementAdminParams = {view: GerritView.ADMIN, adminView: ''};
 
-  setup(() => {
+  setup(async () => {
     element = basicFixture.instantiate();
+    await element.updateComplete;
   });
 
-  test('_computeGroupUrl', () => {
+  test('computeGroupUrl', () => {
     let urlStub = sinon
       .stub(GerritNav, 'getUrlForGroup')
       .callsFake(
@@ -83,7 +85,7 @@ suite('gr-admin-group-list tests', () => {
 
     let group = 'e2cd66f88a2db4d391ac068a92d987effbe872f5';
     assert.equal(
-      element._computeGroupUrl(group),
+      element.computeGroupUrl(group),
       '/admin/groups/e2cd66f88a2db4d391ac068a92d987effbe872f5'
     );
 
@@ -94,7 +96,7 @@ suite('gr-admin-group-list tests', () => {
       .callsFake(() => '/admin/groups/user/test');
 
     group = 'user%2Ftest';
-    assert.equal(element._computeGroupUrl(group), '/admin/groups/user/test');
+    assert.equal(element.computeGroupUrl(group), '/admin/groups/user/test');
 
     urlStub.restore();
   });
@@ -103,30 +105,31 @@ suite('gr-admin-group-list tests', () => {
     setup(async () => {
       groups = createGroupObjectList('test', 26);
       stubRestApi('getGroups').returns(Promise.resolve(groups));
-      element._paramsChanged(value);
-      await flush();
+      element.params = value;
+      element.paramsChanged();
+      await element.updateComplete;
     });
 
     test('test for test group in the list', () => {
-      assert.equal(element._groups[1].name, 'test1' as GroupName);
-      assert.equal(element._groups[1].options!.visible_to_all, false);
+      assert.equal(element.groups[1].name, 'test1' as GroupName);
+      assert.equal(element.groups[1].options!.visible_to_all, false);
     });
 
-    test('_shownGroups', () => {
-      assert.equal(element._shownGroups.length, 25);
+    test('groups', () => {
+      assert.equal(element.groups.slice(0, SHOWN_ITEMS_COUNT).length, 25);
     });
 
-    test('_maybeOpenCreateOverlay', () => {
+    test('maybeOpenCreateOverlay', () => {
       const overlayOpen = sinon.stub(
         queryAndAssert<GrOverlay>(element, '#createOverlay'),
         'open'
       );
-      element._maybeOpenCreateOverlay();
+      element.maybeOpenCreateOverlay();
       assert.isFalse(overlayOpen.called);
-      element._maybeOpenCreateOverlay(undefined);
+      element.maybeOpenCreateOverlay(undefined);
       assert.isFalse(overlayOpen.called);
       value.openCreateModal = true;
-      element._maybeOpenCreateOverlay(value);
+      element.maybeOpenCreateOverlay(value);
       assert.isTrue(overlayOpen.called);
     });
   });
@@ -135,41 +138,41 @@ suite('gr-admin-group-list tests', () => {
     setup(async () => {
       groups = createGroupObjectList('test', 25);
       stubRestApi('getGroups').returns(Promise.resolve(groups));
-      await element._paramsChanged(value);
-      await flush();
+      element.params = value;
+      await element.paramsChanged();
+      await element.updateComplete;
     });
 
-    test('_shownGroups', () => {
-      assert.equal(element._shownGroups.length, 25);
+    test('groups', () => {
+      assert.equal(element.groups.slice(0, SHOWN_ITEMS_COUNT).length, 25);
     });
   });
 
   suite('filter', () => {
-    test('_paramsChanged', async () => {
+    test('paramsChanged', async () => {
       const getGroupsStub = stubRestApi('getGroups');
       getGroupsStub.returns(Promise.resolve(groups));
       value.filter = 'test';
       value.offset = 25;
-      await element._paramsChanged(value);
+      element.params = value;
+      await element.paramsChanged();
       assert.isTrue(getGroupsStub.lastCall.calledWithExactly('test', 25, 25));
     });
   });
 
   suite('loading', async () => {
     test('correct contents are displayed', async () => {
-      assert.isTrue(element._loading);
-      assert.equal(element.computeLoadingClass(element._loading), 'loading');
+      assert.isTrue(element.loading);
       assert.equal(
         getComputedStyle(queryAndAssert<HTMLTableElement>(element, '#loading'))
           .display,
         'block'
       );
 
-      element._loading = false;
-      element._groups = createGroupList('test', 25);
+      element.loading = false;
+      element.groups = createGroupList('test', 25);
 
-      await flush();
-      assert.equal(element.computeLoadingClass(element._loading), '');
+      await element.updateComplete;
       assert.equal(
         getComputedStyle(queryAndAssert<HTMLTableElement>(element, '#loading'))
           .display,
@@ -179,10 +182,10 @@ suite('gr-admin-group-list tests', () => {
   });
 
   suite('create new', () => {
-    test('_handleCreateClicked called when create-click fired', () => {
+    test('handleCreateClicked called when create-click fired', () => {
       const handleCreateClickedStub = sinon.stub(
         element,
-        '_handleCreateClicked'
+        'handleCreateClicked'
       );
       queryAndAssert<GrListView>(element, 'gr-list-view').dispatchEvent(
         new CustomEvent('create-clicked', {
@@ -193,16 +196,16 @@ suite('gr-admin-group-list tests', () => {
       assert.isTrue(handleCreateClickedStub.called);
     });
 
-    test('_handleCreateClicked opens modal', () => {
+    test('handleCreateClicked opens modal', () => {
       const openStub = sinon
         .stub(queryAndAssert<GrOverlay>(element, '#createOverlay'), 'open')
         .returns(Promise.resolve());
-      element._handleCreateClicked();
+      element.handleCreateClicked();
       assert.isTrue(openStub.called);
     });
 
-    test('_handleCreateGroup called when confirm fired', () => {
-      const handleCreateGroupStub = sinon.stub(element, '_handleCreateGroup');
+    test('handleCreateGroup called when confirm fired', () => {
+      const handleCreateGroupStub = sinon.stub(element, 'handleCreateGroup');
       queryAndAssert<GrDialog>(element, '#createDialog').dispatchEvent(
         new CustomEvent('confirm', {
           composed: true,
@@ -212,8 +215,8 @@ suite('gr-admin-group-list tests', () => {
       assert.isTrue(handleCreateGroupStub.called);
     });
 
-    test('_handleCloseCreate called when cancel fired', () => {
-      const handleCloseCreateStub = sinon.stub(element, '_handleCloseCreate');
+    test('handleCloseCreate called when cancel fired', () => {
+      const handleCloseCreateStub = sinon.stub(element, 'handleCloseCreate');
       queryAndAssert<GrDialog>(element, '#createDialog').dispatchEvent(
         new CustomEvent('cancel', {
           composed: true,

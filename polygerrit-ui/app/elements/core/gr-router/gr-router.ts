2f59127ddd5cb2f9cafdb5057fc6e6469ebf2dbd
Andrew Z Allen <me@andrewzallen.com>
2021-11-15 04:04:22 +0000

    Add quick-change feature
    Add a straw-man implementation of a deep linkable way to create a change
    in Gerrit and drop the user into the editor for a specific file. This is
    useful when you have something like a documentation website that would
    like to have an "Edit this page" button on it. With this feature you can
    link the user to
    
    https://gerrit.mycompany.whatever/admin/repos/project,quickchange/?path=path/to/file&project=project&branch=main
    
    and the user will immediately be dropped into a code editor with that
    file open targeting that branch.
    
    Bug: Issue 11528
    Change-Id: I93839938d9b9dadbebb62b431f3eb3bf8a055533

--- polygerrit-ui/app/elements/core/gr-router/gr-router.ts
+++ polygerrit-ui/app/elements/core/gr-router/gr-router.ts
@@ -56,6 +56,7 @@ import {
   NumericChangeId,
   PatchSetNum,
   RepoName,
+  BranchName,
   ServerInfo,
   UrlEncodedCommentId,
   ParentPatchSetNum,
@@ -67,7 +68,7 @@ import {
 } from '../../gr-app-types';
 import {LocationChangeEventDetail} from '../../../types/events';
 import {GerritView} from '../../../services/router/router-model';
-import {firePageError} from '../../../utils/event-util';
+import {fireAlert, firePageError} from '../../../utils/event-util';
 import {addQuotesWhen} from '../../../utils/string-util';
 import {windowLocationReload} from '../../../utils/dom-util';
 import {
@@ -137,6 +138,9 @@ const RoutePattern = {
   REPO_COMMANDS: /^\/admin\/repos\/(.+),commands$/,
 
   REPO_GENERAL: /^\/admin\/repos\/(.+),general$/,
+
+  // Matches /admin/repos/<repo>,quickchange[/path/of/a/file/to/edit][#line-no]
+  REPO_QUICKCHANGE: /^\/admin\/repos\/(.+),quickchange\/?(.*)/,
 
   // Matches /admin/repos/<repos>,access.
   REPO_ACCESS: /^\/admin\/repos\/(.+),access$/,
@@ -264,6 +268,11 @@ const LEGACY_LINENUM_PATTERN = /@([ab]?\d+)$/;
 const LEGACY_QUERY_SUFFIX_PATTERN = /,n,z$/;
 
 const REPO_TOKEN_PATTERN = /\${(project|repo)}/g;
+
+const QUICK_CHANGE_FAILURE_MESSAGE = 'Failed to create quick change.';
+const QUICK_CHANGE_SUCCESS_MESSAGE = 'Navigating to quick change';
+const QUICK_CHANGE_INITIAL_PATCHSET = 1 as PatchSetNum;
+
 
 // Polymer makes `app` intrinsically defined on the window by virtue of the
 // custom element having the id "app", but it is made explicit here.
@@ -965,6 +974,8 @@ export class GrRouter extends PolymerElement {
     this._mapRoute(RoutePattern.GROUP, '_handleGroupRoute', true);
 
     this._mapRoute(RoutePattern.PROJECT_OLD, '_handleProjectsOldRoute');
+
+    this._mapRoute(RoutePattern.REPO_QUICKCHANGE, '_handleRepoQuickchange', true)
 
     this._mapRoute(
       RoutePattern.REPO_COMMANDS,
@@ -1716,6 +1727,50 @@ export class GrRouter extends PolymerElement {
     this.reporting.setRepoName(project);
     this.reporting.setChangeId(changeNum);
   }
+
+  _handleRepoQuickchange(ctx: PageContextWithQueryMap) {
+    const requiredQueryParams = {
+      "project": "It should be the project in which the change will be created.",
+      "branch": "It should be the branch in which the change will be created.",
+      "path": "It should be the path to the file that should be immediately opened when the pending change is created.",
+    };
+    for (const [param, description] of Object.entries(requiredQueryParams)) {
+      if (!ctx.queryMap.get(param)) {
+        const msg : string = `${param} is required for quick change creation. ${description}`;
+        firePageError(new Response(msg, {status: 400}));
+        return
+      }
+    } 
+
+    // Parameter order is based on the regex group number matched.
+    const project = ctx.params[0] as RepoName;
+    const branch = (ctx.queryMap.get("branch") || "") as BranchName;
+    const path = ctx.queryMap.get("path") || "";
+
+    return this.restApiService
+      .createChange(
+        project,
+        branch,
+        path,
+        undefined,
+        false,
+        true
+      )
+      .then(change => {
+        const message = change
+          ? QUICK_CHANGE_SUCCESS_MESSAGE
+          : QUICK_CHANGE_FAILURE_MESSAGE;
+        fireAlert(this, message);
+        if (!change) {
+          return;
+        }
+
+        GerritNav.navigateToRelativeUrl(
+          GerritNav.getEditUrlForDiff(change, path, QUICK_CHANGE_INITIAL_PATCHSET)
+        );
+      });
+  }
+
 
   _handleChangeEditRoute(ctx: PageContextWithQueryMap) {
     // Parameter order is based on the regex group number matched.

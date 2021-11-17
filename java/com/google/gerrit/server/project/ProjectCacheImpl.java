f85f00ab8c30c0c34b7228b0ea0c9c12495e1c7c
Youssef Elghareeb <ghareeb@google.com>
2021-11-17 13:01:08 +0100

    Add a "description" field to the label definition
    With submit requirements, we'll have some labels associated with submit
    requirements while others aren't (for now we name them 'trigger votes').
    This change is to provide more clarity/explanation of what trigger votes
    mean and show this explicit description in the UI.
    
    Bug: Google b/205810851
    Change-Id: Ic7bc1a6f5938958a3c1a6de206ee226b2b1fb320

--- java/com/google/gerrit/server/project/ProjectCacheImpl.java
+++ java/com/google/gerrit/server/project/ProjectCacheImpl.java
@@ -130,7 +130,7 @@ public class ProjectCacheImpl implements ProjectCache {
             .keySerializer(new ProtobufSerializer<>(Cache.ProjectCacheKeyProto.parser()))
             .valueSerializer(PersistedProjectConfigSerializer.INSTANCE)
             .diskLimit(1 << 30) // 1 GiB
-            .version(2)
+            .version(3)
             .maximumWeight(0);
 
         cache(CACHE_LIST, ListKey.class, new TypeLiteral<ImmutableSortedSet<Project.NameKey>>() {})

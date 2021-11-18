2beaebb68ccf9fc0844b9277741fa5ba73b007ff
Youssef Elghareeb <ghareeb@google.com>
2021-11-18 15:53:27 +0100

    Extend the 'file' operator to include file diffs for SRs
    The 'file' operator was previously used with a single file pattern
    argument to search for changes which modified some file path. We extend
    this operator to support an additional "withDiffContaining" argument
    that can be used as:
    file:"'<filePattern>',withDiffContaining='<contentPattern>'"
    
    The whole argument needs to be enclosed with double quotes.
    <filePattern> and <contentPattern> should be enclosed with single
    quotes. Both arguments can start with "^" to signal a regular expression
    matching, otherwise normal string matching is used.
    
    This new extension is equivalent to the "commit_edits" prolog fact (see
    [1]). It accepts two arguments: file_path_pattern and content_pattern.
    The predicate evaluates to true if the diff between the latest PS vs.
    base has a modification in the provided file_path_pattern parameter that
    includes a modified content (added/deleted/modified) matching the
    content_pattern parameter.
    
    We implement the new extension as a submit requirement predicate, i.e.
    will only be available for SR expressions but not for search queries.
    The logic of the predicate is copied from the prolog fact in
    "PRED_commit_edits_2.java".
    
    Documentation needs to be added for this predicate. Will do in a future
    change.
    
    [1]
    https://gerrit-review.googlesource.com/Documentation/prolog-change-facts.html
    
    Bug: Google b/206783349
    Release-Notes: Add support for requesting file diffs with the file operator.
    Change-Id: I8219135bdba8bb4f1d3eebc1c0eba32240454687

--- java/com/google/gerrit/server/query/change/SubmitRequirementChangeQueryBuilder.java
+++ java/com/google/gerrit/server/query/change/SubmitRequirementChangeQueryBuilder.java
@@ -18,7 +18,12 @@ import com.google.gerrit.index.FieldDef;
 import com.google.gerrit.index.query.Predicate;
 import com.google.gerrit.index.query.QueryBuilder;
 import com.google.gerrit.index.query.QueryParseException;
+import com.google.gerrit.server.query.FileEditsPredicate;
+import com.google.gerrit.server.query.FileEditsPredicate.FileEditsArgs;
 import com.google.inject.Inject;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * A query builder for submit requirement expressions that includes all {@link ChangeQueryBuilder}
@@ -33,11 +38,26 @@ public class SubmitRequirementChangeQueryBuilder extends ChangeQueryBuilder {
 
   private final DistinctVotersPredicate.Factory distinctVotersPredicateFactory;
 
+  /**
+   * Regular expression for the {@link #file(String)} operator. Field value is of the form:
+   *
+   * <p>'$fileRegex',withDiffContaining='$contentRegex'
+   *
+   * <p>Both $fileRegex and $contentRegex may contain escaped single or double quotes.
+   */
+  private static final Pattern FILE_EDITS_PATTERN =
+      Pattern.compile("'((?:(?:\\\\')|(?:[^']))*)',withDiffContaining='((?:(?:\\\\')|(?:[^']))*)'");
+
+  private final FileEditsPredicate.Factory fileEditsPredicateFactory;
+
   @Inject
   SubmitRequirementChangeQueryBuilder(
-      Arguments args, DistinctVotersPredicate.Factory distinctVotersPredicateFactory) {
+      Arguments args,
+      DistinctVotersPredicate.Factory distinctVotersPredicateFactory,
+      FileEditsPredicate.Factory fileEditsPredicateFactory) {
     super(def, args);
     this.distinctVotersPredicateFactory = distinctVotersPredicateFactory;
+    this.fileEditsPredicateFactory = fileEditsPredicateFactory;
   }
 
   @Override
@@ -67,5 +87,45 @@ public class SubmitRequirementChangeQueryBuilder extends ChangeQueryBuilder {
   @Operator
   public Predicate<ChangeData> distinctvoters(String value) throws QueryParseException {
     return distinctVotersPredicateFactory.create(value);
+  }
+
+  /**
+   * A SR operator that can match with file path and content pattern. The value should be of the
+   * form:
+   *
+   * <p>file:"'$filePattern',withDiffContaining='$contentPattern'"
+   *
+   * <p>The operator matches with changes that have their latest PS vs. base diff containing a file
+   * path matching the {@code filePattern} with an edit (added, deleted, modified) matching the
+   * {@code contentPattern}. {@code filePattern} and {@code contentPattern} can start with "^" to
+   * use regular expression matching.
+   *
+   * <p>If the specified value does not match this form, we fall back to the operator's
+   * implementation in {@link ChangeQueryBuilder}.
+   */
+  @Override
+  public Predicate<ChangeData> file(String value) throws QueryParseException {
+    Matcher matcher = FILE_EDITS_PATTERN.matcher(value);
+    if (!matcher.find()) {
+      return super.file(value);
+    }
+    String filePattern = matcher.group(1);
+    String contentPattern = matcher.group(2);
+    if (filePattern.startsWith("^")) {
+      validateRegularExpression(filePattern, "Invalid file pattern.");
+    }
+    if (contentPattern.startsWith("^")) {
+      validateRegularExpression(contentPattern, "Invalid content pattern.");
+    }
+    return fileEditsPredicateFactory.create(FileEditsArgs.create(filePattern, contentPattern));
+  }
+
+  private static void validateRegularExpression(String pattern, String errorMessage)
+      throws QueryParseException {
+    try {
+      Pattern.compile(pattern);
+    } catch (PatternSyntaxException e) {
+      throw new QueryParseException(errorMessage, e);
+    }
   }
 }

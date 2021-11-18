60541f1b574e19dc53411394cd558c49e7ca384a
Youssef Elghareeb <ghareeb@google.com>
2021-11-18 15:53:27 +0100

    Extend the 'file' operator to include file diffs for SRs
    The 'file' operator was previously used with a single file pattern
    argument to search for changes which modified some file path. We extend
    this operator to support an additional "withDiffContaining" argument
    that can be used as:
    file:"'<filePattern>',withDiffContaining='<contentPattern>'"
    
    The whole argument needs to be enclosed with double quotes.
    <filePattern> and <contentPattern> should be enclosed with single
    quotes. Both arguments can start with "^" to signal a regular expression
    matching, otherwise normal string matching is used.
    
    This new extension is equivalent to the "commit_edits" prolog fact (see
    [1]). It accepts two arguments: file_path_pattern and content_pattern.
    The predicate evaluates to true if the diff between the latest PS vs.
    base has a modification in the provided file_path_pattern parameter that
    includes a modified content (added/deleted/modified) matching the
    content_pattern parameter.
    
    We implement the new extension as a submit requirement predicate, i.e.
    will only be available for SR expressions but not for search queries.
    The logic of the predicate is copied from the prolog fact in
    "PRED_commit_edits_2.java".
    
    Documentation needs to be added for this predicate. Will do in a future
    change.
    
    [1]
    https://gerrit-review.googlesource.com/Documentation/prolog-change-facts.html
    
    Bug: Google b/206783349
    Change-Id: I4cd9a870a131676499ce23ac186a386e0948aec7

--- java/com/google/gerrit/pgm/util/BatchProgramModule.java
+++ java/com/google/gerrit/pgm/util/BatchProgramModule.java
@@ -81,6 +81,7 @@ import com.google.gerrit.server.project.ProjectCacheImpl;
 import com.google.gerrit.server.project.ProjectState;
 import com.google.gerrit.server.project.SubmitRequirementsEvaluatorImpl;
 import com.google.gerrit.server.project.SubmitRuleEvaluator;
+import com.google.gerrit.server.query.FileEditsPredicate;
 import com.google.gerrit.server.query.approval.ApprovalModule;
 import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.query.change.ChangeIsVisibleToPredicate;
@@ -205,6 +206,8 @@ public class BatchProgramModule extends FactoryModule {
     modules.add(new PrologModule());
     modules.add(new DefaultSubmitRuleModule());
     modules.add(new IgnoreSelfApprovalRuleModule());
+
+    factory(FileEditsPredicate.Factory.class);
 
     bind(ChangeJson.Factory.class).toProvider(Providers.of(null));
     bind(EventUtil.class).toProvider(Providers.of(null));

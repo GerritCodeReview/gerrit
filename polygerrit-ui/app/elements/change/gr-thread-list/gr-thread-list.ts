426bf7e48fe7ab6aef32b0d7d7691197b0b77ac7
Dhruv Srivastava <dhruvsri@google.com>
2021-11-19 14:45:40 +0100

    Render all comments initially if user wants to scroll to some
    Change 321979 updated the default rendering status of comments in the
    comments tab to render only unresolved threads if there are any.
    This led to a bug where resolved comments when accessed via the
    /comments/<commentId> route won't be scrolled into view as they are not
    attached to the DOM.
    
    Google-bug-id: b/206655865
    Change-Id: Ia575b2807f9791b4c417a32ff701543722c0e0b2

--- polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list.ts
+++ polygerrit-ui/app/elements/change/gr-thread-list/gr-thread-list.ts
@@ -211,6 +211,7 @@ export class GrThreadList extends LitElement {
 
   override willUpdate(changed: PropertyValues) {
     if (changed.has('commentTabState')) this.onCommentTabStateUpdate();
+    if (changed.has('scrollCommentId')) this.onScrollCommentIdUpdate();
   }
 
   private onCommentTabStateUpdate() {
@@ -225,6 +226,14 @@ export class GrThreadList extends LitElement {
         this.handleAllComments();
         break;
     }
+  }
+
+  /**
+   * When user wants to scroll to a comment, render all comments so that the
+   * appropriate comment can be scrolled into view.
+   */
+  private onScrollCommentIdUpdate() {
+    if (this.scrollCommentId) this.handleAllComments();
   }
 
   static override get styles() {

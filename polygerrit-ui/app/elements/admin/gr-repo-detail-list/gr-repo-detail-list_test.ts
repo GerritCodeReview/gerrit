0067bdba6412181f59f06c597ef637db6011ad0e
Paladox none <thomasmulhall410@yahoo.com>
2021-11-19 16:52:31 +0000

    Convert gr-repo-detail-list to lit
    Change-Id: I1cec522d32c9831e31e797dac42fdc73e5cbee5d

--- polygerrit-ui/app/elements/admin/gr-repo-detail-list/gr-repo-detail-list_test.ts
+++ polygerrit-ui/app/elements/admin/gr-repo-detail-list/gr-repo-detail-list_test.ts
@@ -41,13 +41,13 @@ import {
   TimezoneOffset,
 } from '../../../types/common';
 import {GerritView} from '../../../services/router/router-model';
-import {AppElementRepoParams} from '../../gr-app-types';
 import {GrButton} from '../../shared/gr-button/gr-button';
 import * as MockInteractions from '@polymer/iron-test-helpers/mock-interactions';
 import {PageErrorEvent} from '../../../types/events';
 import {GrOverlay} from '../../shared/gr-overlay/gr-overlay';
 import {GrDialog} from '../../shared/gr-dialog/gr-dialog';
 import {GrListView} from '../../shared/gr-list-view/gr-list-view';
+import {SHOWN_ITEMS_COUNT} from '../../../constants/constants';
 
 const basicFixture = fixtureFromElement('gr-repo-detail-list');
 
@@ -106,8 +106,9 @@ suite('gr-repo-detail-list', () => {
     let element: GrRepoDetailList;
     let branches: BranchInfo[];
 
-    setup(() => {
+    setup(async () => {
       element = basicFixture.instantiate();
+      await element.updateComplete;
       element.detailType = RepoDetailView.BRANCHES;
       sinon.stub(page, 'show');
     });
@@ -122,39 +123,39 @@ suite('gr-repo-detail-list', () => {
         ].concat(createBranchesList(25));
         stubRestApi('getRepoBranches').returns(Promise.resolve(branches));
 
-        const params: AppElementRepoParams = {
+        element.params = {
           view: GerritView.REPO,
           repo: 'test' as RepoName,
           detail: RepoDetailView.BRANCHES,
         };
-        await element._paramsChanged(params);
-        await flush();
+        await element.paramsChanged();
+        await element.updateComplete;
       });
 
       test('test for branch in the list', () => {
-        assert.equal(element._items![3].ref, 'refs/heads/test2');
+        assert.equal(element.items![3].ref, 'refs/heads/test2');
       });
 
       test('test for web links in the branches list', () => {
         assert.equal(
-          element._items![3].web_links![0].url,
+          element.items![3].web_links![0].url,
           'https://git.example.org/branch/test;refs/heads/test2'
         );
       });
 
       test('test for refs/heads/ being striped from ref', () => {
         assert.equal(
-          element._stripRefs(element._items![3].ref, element.detailType),
+          element.stripRefs(element.items![3].ref, element.detailType),
           'test2'
         );
       });
 
-      test('_shownItems', () => {
-        assert.equal(element._shownItems!.length, 25);
+      test('items', () => {
+        assert.equal(element.items!.slice(0, SHOWN_ITEMS_COUNT)!.length, 25);
       });
 
       test('Edit HEAD button not admin', async () => {
-        sinon.stub(element, '_getLoggedIn').returns(Promise.resolve(true));
+        sinon.stub(element, 'getLoggedIn').returns(Promise.resolve(true));
         stubRestApi('getRepoAccess').returns(
           Promise.resolve({
             test: {
@@ -179,7 +180,7 @@ suite('gr-repo-detail-list', () => {
           } as ProjectAccessInfoMap)
         );
         await element._determineIfOwner('test' as RepoName);
-        assert.equal(element._isOwner, false);
+        assert.equal(element.isOwner, false);
         assert.equal(
           getComputedStyle(
             queryAndAssert<HTMLSpanElement>(element, '.revisionNoEditing')
@@ -207,7 +208,7 @@ suite('gr-repo-detail-list', () => {
           '.revisionWithEditing'
         );
 
-        sinon.stub(element, '_getLoggedIn').returns(Promise.resolve(true));
+        sinon.stub(element, 'getLoggedIn').returns(Promise.resolve(true));
         stubRestApi('getRepoAccess').returns(
           Promise.resolve({
             test: {
@@ -234,10 +235,10 @@ suite('gr-repo-detail-list', () => {
         );
         const handleSaveRevisionStub = sinon.stub(
           element,
-          '_handleSaveRevision'
+          'handleSaveRevision'
         );
         await element._determineIfOwner('test' as RepoName);
-        assert.equal(element._isOwner, true);
+        assert.equal(element.isOwner, true);
         // The revision container for non-editing enabled row is not visible.
         assert.equal(getComputedStyle(revisionNoEditing).display, 'none');
 
@@ -264,7 +265,7 @@ suite('gr-repo-detail-list', () => {
         }
 
         MockInteractions.tap(editBtn);
-        await flush();
+        await element.updateComplete;
         // The revision and edit button are not visible.
         assert.equal(getComputedStyle(revisionWithEditing).display, 'none');
         assert.equal(getComputedStyle(editBtn).display, 'none');
@@ -275,17 +276,19 @@ suite('gr-repo-detail-list', () => {
         }
 
         // The revised ref was set correctly
-        assert.equal(element._revisedRef, 'master' as GitRef);
+        assert.equal(element.revisedRef, 'master' as GitRef);
 
         assert.isFalse(saveBtn.disabled);
 
         // Delete the ref.
-        element._revisedRef = '' as GitRef;
+        element.revisedRef = '' as GitRef;
+        await element.updateComplete;
         assert.isTrue(saveBtn.disabled);
 
         // Change the ref to something else
-        element._revisedRef = 'newRef' as GitRef;
-        element._repo = 'test' as RepoName;
+        element.revisedRef = 'newRef' as GitRef;
+        element.repo = 'test' as RepoName;
+        await element.updateComplete;
         assert.isFalse(saveBtn.disabled);
 
         // Save button calls handleSave. since this is stubbed, the edit
@@ -295,7 +298,7 @@ suite('gr-repo-detail-list', () => {
 
         // When cancel is tapped, the edit secion closes.
         MockInteractions.tap(cancelBtn);
-        await flush();
+        await element.updateComplete;
 
         // The revision and edit button are visible.
         assert.notEqual(getComputedStyle(revisionWithEditing).display, 'none');
@@ -307,56 +310,36 @@ suite('gr-repo-detail-list', () => {
         }
       });
 
-      test('_handleSaveRevision with invalid rev', async () => {
-        // We need to replicate an event to get all the properties that
-        // go with it.
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const event: any = new Event('polymer-dom-repeat');
-        event.model = {set: sinon.stub()};
-        element._isEditing = true;
+      test('handleSaveRevision with invalid rev', async () => {
+        element.isEditing = true;
         stubRestApi('setRepoHead').returns(
           Promise.resolve({
             status: 400,
           } as Response)
         );
 
-        await element._setRepoHead(
-          'test' as RepoName,
-          'newRef' as GitRef,
-          event
-        );
-        assert.isTrue(element._isEditing);
-        assert.isFalse(event.model.set.called);
+        await element.setRepoHead('test' as RepoName, 'newRef' as GitRef, 1);
+        assert.isTrue(element.isEditing);
       });
 
-      test('_handleSaveRevision with valid rev', async () => {
-        // We need to replicate an event to get all the properties that
-        // go with it.
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const event: any = new Event('polymer-dom-repeat');
-        event.model = {set: sinon.stub()};
-        element._isEditing = true;
+      test('handleSaveRevision with valid rev', async () => {
+        element.isEditing = true;
         stubRestApi('setRepoHead').returns(
           Promise.resolve({
             status: 200,
           } as Response)
         );
 
-        await element._setRepoHead(
-          'test' as RepoName,
-          'newRef' as GitRef,
-          event
-        );
-        assert.isFalse(element._isEditing);
-        assert.isTrue(event.model.set.called);
+        await element.setRepoHead('test' as RepoName, 'newRef' as GitRef, 1);
+        assert.isFalse(element.isEditing);
       });
 
-      test('test _computeItemName', () => {
+      test('test computeItemName', () => {
         assert.deepEqual(
-          element._computeItemName(RepoDetailView.BRANCHES),
+          element.computeItemName(RepoDetailView.BRANCHES),
           'Branch'
         );
-        assert.deepEqual(element._computeItemName(RepoDetailView.TAGS), 'Tag');
+        assert.deepEqual(element.computeItemName(RepoDetailView.TAGS), 'Tag');
       });
     });
 
@@ -365,34 +348,34 @@ suite('gr-repo-detail-list', () => {
         branches = createBranchesList(25);
         stubRestApi('getRepoBranches').returns(Promise.resolve(branches));
 
-        const params: AppElementRepoParams = {
+        element.params = {
           view: GerritView.REPO,
           repo: 'test' as RepoName,
           detail: RepoDetailView.BRANCHES,
         };
 
-        await element._paramsChanged(params);
-        await flush();
+        await element.paramsChanged();
+        await element.updateComplete;
       });
 
-      test('_shownItems', () => {
-        assert.equal(element._shownItems!.length, 25);
+      test('items', () => {
+        assert.equal(element.items!.slice(0, SHOWN_ITEMS_COUNT)!.length, 25);
       });
     });
 
     suite('filter', () => {
-      test('_paramsChanged', async () => {
+      test('paramsChanged', async () => {
         const stub = stubRestApi('getRepoBranches').returns(
           Promise.resolve(branches)
         );
-        const params: AppElementRepoParams = {
+        element.params = {
           view: GerritView.REPO,
           repo: 'test' as RepoName,
           detail: RepoDetailView.BRANCHES,
           filter: 'test',
           offset: 25,
         };
-        await element._paramsChanged(params);
+        await element.paramsChanged();
         assert.equal(stub.lastCall.args[0], 'test');
         assert.equal(stub.lastCall.args[1], 'test');
         assert.equal(stub.lastCall.args[2], 25);
@@ -418,14 +401,14 @@ suite('gr-repo-detail-list', () => {
           promise.resolve();
         });
 
-        const params: AppElementRepoParams = {
+        element.params = {
           view: GerritView.REPO,
           repo: 'test' as RepoName,
           detail: RepoDetailView.BRANCHES,
           filter: 'test',
           offset: 25,
         };
-        element._paramsChanged(params);
+        element.paramsChanged();
         await promise;
       });
     });
@@ -435,30 +418,11 @@ suite('gr-repo-detail-list', () => {
     let element: GrRepoDetailList;
     let tags: TagInfo[];
 
-    setup(() => {
+    setup(async () => {
       element = basicFixture.instantiate();
+      await element.updateComplete;
       element.detailType = RepoDetailView.TAGS;
       sinon.stub(page, 'show');
-    });
-
-    test('_computeMessage', () => {
-      let message =
-        'v2.15-rc1↵-----BEGIN PGP SIGNATURE-----↵Version: GnuPG v' +
-        '1↵↵iQIcBAABAgAGBQJZ27O7AAoJEF/XxZqaEoiMy6kQAMoQCpGr3J6JITI4BVWsr7QM↵xy' +
-        'EcWH5YPUko5EPTbkABHmaVyFmKGkuIQdn6c+NIbqJOk+5XT4oUyRSo1T569HPJ↵3kyxEJi' +
-        'T1ryvp5BIHwdvHx58fjw1+YkiWLZuZq1FFkUYqnWTYCrkv7Fok98pdOmV↵CL1Hgugi5uK8' +
-        '/kxf1M7+Nv6piaZ140pwSb1h6QdAjaZVfaBCnoxlG4LRUqHvEYay↵f4QYgFT67auHIGkZ4' +
-        'moUcsp2Du/1jSsCWL/CPwjPFGbbckVAjLCMT9yD3NKwpEZF↵pfsiZyHI9dL0M+QjVrM+RD' +
-        'HwIIJwra8R0IMkDlQ6MDrFlKNqNBbo588S6UPrm71L↵YuiwWlcrK9ZIybxT6LzbR65Rvez' +
-        'DSitQ+xeIfpZE19/X6BCnvlARLE8k/tC2JksI↵lEZi7Lf3FQdIcwwyt98tJkS9HX9v9jbC' +
-        '5QXifnoj3Li8tHSLuQ1dJCxHQiis6ojI↵OWUFkm0IHBXVNHA2dqYBdM+pL12mlI3wp6Ica' +
-        '4cdEVDwzu+j1xnVSFUa+d+Y2xJF↵7mytuyhHiKG4hm+zbhMv6WD8Q3FoDsJZeLY99l0hYQ' +
-        'SnnkMduFVroIs45pAs8gUA↵RvYla8mm9w/543IJAPzzFarPVLSsSyQ7tJl3UBzjKRNH/rX' +
-        'W+F22qyWD1zyHPUIR↵C00ItmwlAvveImYKpQAH↵=L+K9↵-----END PGP SIGNATURE---' +
-        '--';
-      assert.equal(element._computeMessage(message), 'v2.15-rc1↵');
-      message = 'v2.15-rc1';
-      assert.equal(element._computeMessage(message), 'v2.15-rc1');
     });
 
     suite('list of repo tags', () => {
@@ -466,25 +430,22 @@ suite('gr-repo-detail-list', () => {
         tags = createTagsList(26);
         stubRestApi('getRepoTags').returns(Promise.resolve(tags));
 
-        const params: AppElementRepoParams = {
+        element.params = {
           view: GerritView.REPO,
           repo: 'test' as RepoName,
           detail: RepoDetailView.TAGS,
         };
 
-        await element._paramsChanged(params);
-        await flush();
+        await element.paramsChanged();
+        await element.updateComplete;
       });
 
       test('test for tag in the list', async () => {
-        assert.equal(element._items![2].ref, 'refs/tags/test2');
+        assert.equal(element.items![2].ref, 'refs/tags/test2');
       });
 
       test('test for tag message in the list', async () => {
-        assert.equal(
-          (element._items as TagInfo[])![2].message,
-          'Annotated tag'
-        );
+        assert.equal((element.items as TagInfo[])![2].message, 'Annotated tag');
       });
 
       test('test for tagger in the tag list', async () => {
@@ -495,37 +456,25 @@ suite('gr-repo-detail-list', () => {
           tz: 540 as TimezoneOffset,
         };
 
-        assert.deepEqual((element._items as TagInfo[])![2].tagger, tagger);
+        assert.deepEqual((element.items as TagInfo[])![2].tagger, tagger);
       });
 
       test('test for web links in the tags list', async () => {
         assert.equal(
-          element._items![2].web_links![0].url,
+          element.items![2].web_links![0].url,
           'https://git.example.org/tag/test;refs/tags/test2'
         );
       });
 
       test('test for refs/tags/ being striped from ref', async () => {
         assert.equal(
-          element._stripRefs(element._items![2].ref, element.detailType),
+          element.stripRefs(element.items![2].ref, element.detailType),
           'test2'
         );
       });
 
-      test('_shownItems', () => {
-        assert.equal(element._shownItems!.length, 25);
-      });
-
-      test('_computeHideTagger', () => {
-        const testObject1 = {
-          name: 'Test User',
-          email: 'test.user@gmail.com' as EmailAddress,
-          date: '2017-09-19 14:54:00.000000000' as Timestamp,
-          tz: 540 as TimezoneOffset,
-        };
-        assert.equal(element._computeHideTagger(testObject1), '');
-
-        assert.equal(element._computeHideTagger(undefined), 'hide');
+      test('items', () => {
+        assert.equal(element.items!.slice(0, SHOWN_ITEMS_COUNT)!.length, 25);
       });
     });
 
@@ -534,32 +483,32 @@ suite('gr-repo-detail-list', () => {
         tags = createTagsList(25);
         stubRestApi('getRepoTags').returns(Promise.resolve(tags));
 
-        const params: AppElementRepoParams = {
+        element.params = {
           view: GerritView.REPO,
           repo: 'test' as RepoName,
           detail: RepoDetailView.TAGS,
         };
 
-        await element._paramsChanged(params);
-        await flush();
+        await element.paramsChanged();
+        await element.updateComplete;
       });
 
-      test('_shownItems', () => {
-        assert.equal(element._shownItems!.length, 25);
+      test('items', () => {
+        assert.equal(element.items!.slice(0, SHOWN_ITEMS_COUNT)!.length, 25);
       });
     });
 
     suite('filter', () => {
-      test('_paramsChanged', async () => {
+      test('paramsChanged', async () => {
         const stub = stubRestApi('getRepoTags').returns(Promise.resolve(tags));
-        const params: AppElementRepoParams = {
+        element.params = {
           view: GerritView.REPO,
           repo: 'test' as RepoName,
           detail: RepoDetailView.TAGS,
           filter: 'test',
           offset: 25,
         };
-        await element._paramsChanged(params);
+        await element.paramsChanged();
         assert.equal(stub.lastCall.args[0], 'test');
         assert.equal(stub.lastCall.args[1], 'test');
         assert.equal(stub.lastCall.args[2], 25);
@@ -568,10 +517,10 @@ suite('gr-repo-detail-list', () => {
     });
 
     suite('create new', () => {
-      test('_handleCreateClicked called when create-click fired', () => {
+      test('handleCreateClicked called when create-click fired', () => {
         const handleCreateClickedStub = sinon.stub(
           element,
-          '_handleCreateClicked'
+          'handleCreateClicked'
         );
         queryAndAssert<GrListView>(element, 'gr-list-view').dispatchEvent(
           new CustomEvent('create-clicked', {
@@ -582,18 +531,18 @@ suite('gr-repo-detail-list', () => {
         assert.isTrue(handleCreateClickedStub.called);
       });
 
-      test('_handleCreateClicked opens modal', () => {
+      test('handleCreateClicked opens modal', () => {
         queryAndAssert<GrOverlay>(element, '#createOverlay');
         const openStub = sinon.stub(
           queryAndAssert<GrOverlay>(element, '#createOverlay'),
           'open'
         );
-        element._handleCreateClicked();
+        element.handleCreateClicked();
         assert.isTrue(openStub.called);
       });
 
-      test('_handleCreateItem called when confirm fired', () => {
-        const handleCreateItemStub = sinon.stub(element, '_handleCreateItem');
+      test('handleCreateItem called when confirm fired', () => {
+        const handleCreateItemStub = sinon.stub(element, 'handleCreateItem');
         queryAndAssert<GrDialog>(element, '#createDialog').dispatchEvent(
           new CustomEvent('confirm', {
             composed: true,
@@ -633,27 +582,21 @@ suite('gr-repo-detail-list', () => {
           promise.resolve();
         });
 
-        const params: AppElementRepoParams = {
+        element.params = {
           view: GerritView.REPO,
           repo: 'test' as RepoName,
           detail: RepoDetailView.TAGS,
           filter: 'test',
           offset: 25,
         };
-        element._paramsChanged(params);
+        element.paramsChanged();
         await promise;
       });
     });
 
-    test('test _computeHideDeleteClass', () => {
-      assert.deepEqual(element._computeHideDeleteClass(true, false), 'show');
-      assert.deepEqual(element._computeHideDeleteClass(false, true), 'show');
-      assert.deepEqual(element._computeHideDeleteClass(false, false), '');
-    });
-
-    test('_computeItemName', () => {
-      assert.equal(element._computeItemName(RepoDetailView.BRANCHES), 'Branch');
-      assert.equal(element._computeItemName(RepoDetailView.TAGS), 'Tag');
+    test('computeItemName', () => {
+      assert.equal(element.computeItemName(RepoDetailView.BRANCHES), 'Branch');
+      assert.equal(element.computeItemName(RepoDetailView.TAGS), 'Tag');
     });
   });
 });

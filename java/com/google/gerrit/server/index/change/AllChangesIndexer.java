991a7852664b18115cccb6b3ef8a9161c9f5f150
Kaushik Lingarkar <kaushikl@codeaurora.org>
2021-12-06 16:50:10 -0800

    MultiProgressMonitor: Support waiting for intermediate tasks
    Update MultiProgressMonitor, so that it can be used to track
    intermediate tasks as needed. waitFor(), which does not end
    the monitor, can be used to now track intermediate tasks. Use
    waitForAndDone() to track the final task.
    
    This change is needed for a future update to reindex which adds
    more parallelization to improve its performance.
    
    Change-Id: I55ecf15ff8be4c90714872995da2afab8680a1dd

--- java/com/google/gerrit/server/index/change/AllChangesIndexer.java
+++ java/com/google/gerrit/server/index/change/AllChangesIndexer.java
@@ -20,11 +20,8 @@ import static com.google.common.util.concurrent.Futures.transform;
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static com.google.gerrit.server.git.QueueProvider.QueueType.BATCH;
 
-import com.google.auto.value.AutoValue;
 import com.google.common.base.Stopwatch;
-import com.google.common.collect.Sets;
 import com.google.common.flogger.FluentLogger;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.UncheckedExecutionException;
@@ -46,13 +43,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import org.eclipse.jgit.lib.ProgressMonitor;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.TextProgressMonitor;
@@ -65,14 +58,6 @@ import org.eclipse.jgit.lib.TextProgressMonitor;
 public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, ChangeIndex> {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
   private static final int PROJECT_SLICE_MAX_REFS = 1000;
-
-  private static class ProjectsCollectionFailure extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public ProjectsCollectionFailure(String message) {
-      super(message);
-    }
-  }
 
   private final ChangeData.Factory changeDataFactory;
   private final GitRepositoryManager repoManager;
@@ -97,53 +82,92 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
     this.projectCache = projectCache;
   }
 
-  @AutoValue
-  public abstract static class ProjectSlice {
-    public abstract Project.NameKey name();
+  private static class ProjectSlice {
+    private final Project.NameKey name;
+    private final int slice;
+    private final int slices;
+    private final ScanResult sr;
 
-    public abstract int slice();
+    ProjectSlice(Project.NameKey name, int slice, int slices, ScanResult sr) {
+      this.name = name;
+      this.slice = slice;
+      this.slices = slices;
+      this.sr = sr;
+    }
 
-    public abstract int slices();
+    public Project.NameKey getName() {
+      return name;
+    }
 
-    public abstract ScanResult scanResult();
+    public int getSlice() {
+      return slice;
+    }
 
-    private static ProjectSlice create(Project.NameKey name, int slice, int slices, ScanResult sr) {
-      return new AutoValue_AllChangesIndexer_ProjectSlice(name, slice, slices, sr);
+    public int getSlices() {
+      return slices;
+    }
+
+    public ScanResult getScanResult() {
+      return sr;
     }
   }
 
   @Override
   public Result indexAll(ChangeIndex index) {
-    // The simplest approach to distribute indexing would be to let each thread grab a project
-    // and index it fully. But if a site has one big project and 100s of small projects, then
-    // in the beginning all CPUs would be busy reindexing projects. But soon enough all small
-    // projects have been reindexed, and only the thread that reindexes the big project is
-    // still working. The other threads would idle. Reindexing the big project on a single
-    // thread becomes the critical path. Bringing in more CPUs would not speed up things.
-    //
-    // To avoid such situations, we split big repos into smaller parts and let
-    // the thread pool index these smaller parts. This splitting introduces an overhead in the
-    // workload setup and there might be additional slow-downs from multiple threads
-    // concurrently working on different parts of the same project. But for Wikimedia's Gerrit,
-    // which had 2 big projects, many middle sized ones, and lots of smaller ones, the
-    // splitting of repos into smaller parts reduced indexing time from 1.5 hours to 55 minutes
-    // in 2020.
-
+    ProgressMonitor pm = new TextProgressMonitor();
+    pm.beginTask("Collecting projects", ProgressMonitor.UNKNOWN);
+    List<ProjectSlice> projectSlices = new ArrayList<>();
+    int changeCount = 0;
     Stopwatch sw = Stopwatch.createStarted();
-    List<ProjectSlice> projectSlices;
-    try {
-      projectSlices = new SliceCreator().create();
-    } catch (ProjectsCollectionFailure | InterruptedException | ExecutionException e) {
-      logger.atSevere().log(e.getMessage());
-      return Result.create(sw, false, 0, 0);
+    int projectsFailed = 0;
+    for (Project.NameKey name : projectCache.all()) {
+      try (Repository repo = repoManager.openRepository(name)) {
+        // The simplest approach to distribute indexing would be to let each thread grab a project
+        // and index it fully. But if a site has one big project and 100s of small projects, then
+        // in the beginning all CPUs would be busy reindexing projects. But soon enough all small
+        // projects have been reindexed, and only the thread that reindexes the big project is
+        // still working. The other threads would idle. Reindexing the big project on a single
+        // thread becomes the critical path. Bringing in more CPUs would not speed up things.
+        //
+        // To avoid such situations, we split big repos into smaller parts and let
+        // the thread pool index these smaller parts. This splitting introduces an overhead in the
+        // workload setup and there might be additional slow-downs from multiple threads
+        // concurrently working on different parts of the same project. But for Wikimedia's Gerrit,
+        // which had 2 big projects, many middle sized ones, and lots of smaller ones, the
+        // splitting of repos into smaller parts reduced indexing time from 1.5 hours to 55 minutes
+        // in 2020.
+        ScanResult sr = ChangeNotes.Factory.scanChangeIds(repo);
+        int size = sr.all().size();
+        if (size == 0) {
+          pm.update(1);
+          continue;
+        }
+        changeCount += size;
+        int slices = 1 + size / PROJECT_SLICE_MAX_REFS;
+        if (slices > 1) {
+          verboseWriter.println("Submitting " + name + " for indexing in " + slices + " slices");
+        }
+        for (int slice = 0; slice < slices; slice++) {
+          projectSlices.add(new ProjectSlice(name, slice, slices, sr));
+        }
+      } catch (IOException e) {
+        logger.atSevere().withCause(e).log("Error collecting project %s", name);
+        projectsFailed++;
+        if (projectsFailed > projectCache.all().size() / 2) {
+          logger.atSevere().log("Over 50%% of the projects could not be collected: aborted");
+          return Result.create(sw, false, 0, 0);
+        }
+      }
+      pm.update(1);
     }
+    pm.endTask();
+    setTotalWork(changeCount);
 
-    // Since project slices are created in parallel, they are somewhat shuffled already. However,
-    // the number of threads used to create the project slices doesn't guarantee good randomization.
-    // If the slices are not shuffled well, then multiple threads would typically work concurrently
-    // on different slices of the same project. While this is not a big issue, shuffling the list
-    // beforehand helps with ungrouping the project slices, so different slices are less likely to
-    // be worked on concurrently.
+    // projectSlices are currently grouped by projects. First all slices for project1, followed
+    // by all slices for project2, and so on. As workers pick tasks sequentially, multiple threads
+    // would typically work concurrently on different slices of the same project. While this is not
+    // a big issue, shuffling the list beforehand helps with ungrouping the project slices, so
+    // different slices are less likely to be worked on concurrently.
     // This shuffling gave a 6% runtime reduction for Wikimedia's Gerrit in 2020.
     Collections.shuffle(projectSlices);
     return indexAll(index, projectSlices);
@@ -161,9 +185,9 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
     AtomicBoolean ok = new AtomicBoolean(true);
 
     for (ProjectSlice projectSlice : projectSlices) {
-      Project.NameKey name = projectSlice.name();
-      int slice = projectSlice.slice();
-      int slices = projectSlice.slices();
+      Project.NameKey name = projectSlice.getName();
+      int slice = projectSlice.getSlice();
+      int slices = projectSlice.getSlices();
       ListenableFuture<?> future =
           executor.submit(
               reindexProject(
@@ -171,7 +195,7 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
                   name,
                   slice,
                   slices,
-                  projectSlice.scanResult(),
+                  projectSlice.getScanResult(),
                   doneTask,
                   failedTask));
       String description = "project " + name + " (" + slice + "/" + slices + ")";
@@ -180,7 +204,7 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
     }
 
     try {
-      mpm.waitFor(
+      mpm.waitForAndDone(
           transform(
               successfulAsList(futures),
               x -> {
@@ -224,10 +248,10 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
       Project.NameKey project,
       int slice,
       int slices,
-      ScanResult scanResult,
+      ScanResult sr,
       Task done,
       Task failed) {
-    return new ProjectIndexer(indexer, project, slice, slices, scanResult, done, failed);
+    return new ProjectIndexer(indexer, project, slice, slices, sr, done, failed);
   }
 
   private class ProjectIndexer implements Callable<Void> {
@@ -235,7 +259,7 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
     private final Project.NameKey project;
     private final int slice;
     private final int slices;
-    private final ScanResult scanResult;
+    private final ScanResult sr;
     private final ProgressMonitor done;
     private final ProgressMonitor failed;
 
@@ -244,14 +268,14 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
         Project.NameKey project,
         int slice,
         int slices,
-        ScanResult scanResult,
+        ScanResult sr,
         ProgressMonitor done,
         ProgressMonitor failed) {
       this.indexer = indexer;
       this.project = project;
       this.slice = slice;
       this.slices = slices;
-      this.scanResult = scanResult;
+      this.sr = sr;
       this.done = done;
       this.failed = failed;
     }
@@ -264,9 +288,7 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
       // It does mean that reindexing after invalidating the DiffSummary cache will be expensive,
       // but the goal is to invalidate that cache as infrequently as we possibly can. And besides,
       // we don't have concrete proof that improving packfile locality would help.
-      notesFactory
-          .scan(scanResult, project, id -> (id.get() % slices) == slice)
-          .forEach(r -> index(r));
+      notesFactory.scan(sr, project, id -> (id.get() % slices) == slice).forEach(r -> index(r));
       OnlineReindexMode.end();
       return null;
     }
@@ -305,65 +327,6 @@ public class AllChangesIndexer extends SiteIndexer<Change.Id, ChangeData, Change
     @Override
     public String toString() {
       return "Index all changes of project " + project.get();
-    }
-  }
-
-  private class SliceCreator {
-    final Set<ProjectSlice> projectSlices = Sets.newConcurrentHashSet();
-    final AtomicInteger changeCount = new AtomicInteger(0);
-    final AtomicInteger projectsFailed = new AtomicInteger(0);
-    final ProgressMonitor pm = new TextProgressMonitor();
-
-    private List<ProjectSlice> create()
-        throws ProjectsCollectionFailure, InterruptedException, ExecutionException {
-      List<ListenableFuture<?>> futures = new ArrayList<>();
-      pm.beginTask("Collecting projects", ProgressMonitor.UNKNOWN);
-      for (Project.NameKey name : projectCache.all()) {
-        futures.add(executor.submit(new ProjectSliceCreator(name)));
-      }
-
-      Futures.allAsList(futures).get();
-
-      if (projectsFailed.get() > projectCache.all().size() / 2) {
-        throw new ProjectsCollectionFailure(
-            "Over 50%% of the projects could not be collected: aborted");
-      }
-
-      pm.endTask();
-      setTotalWork(changeCount.get());
-      return projectSlices.stream().collect(Collectors.toList());
-    }
-
-    private class ProjectSliceCreator implements Callable<Void> {
-      final Project.NameKey name;
-
-      public ProjectSliceCreator(Project.NameKey name) {
-        this.name = name;
-      }
-
-      @Override
-      public Void call() throws IOException {
-        try (Repository repo = repoManager.openRepository(name)) {
-          ScanResult sr = ChangeNotes.Factory.scanChangeIds(repo);
-          int size = sr.all().size();
-          if (size > 0) {
-            changeCount.addAndGet(size);
-            int slices = 1 + size / PROJECT_SLICE_MAX_REFS;
-            if (slices > 1) {
-              verboseWriter.println(
-                  "Submitting " + name + " for indexing in " + slices + " slices");
-            }
-            for (int slice = 0; slice < slices; slice++) {
-              projectSlices.add(ProjectSlice.create(name, slice, slices, sr));
-            }
-          }
-        } catch (IOException e) {
-          logger.atSevere().withCause(e).log("Error collecting project %s", name);
-          projectsFailed.incrementAndGet();
-        }
-        pm.update(1);
-        return null;
-      }
     }
   }
 }

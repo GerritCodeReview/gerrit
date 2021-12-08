29e5feef1162a40c99d4a06ecf3a9b5beb9123d2
Milutin Kristofic <milutin@google.com>
2021-12-08 12:19:18 +0100

    Submit Requirements - Show legacy requirements with flag on
    Google-Bug-Id: b/209750294
    Change-Id: If3585b2754f09ceb090e1d5a2c4fb7e91c348648

--- polygerrit-ui/app/utils/label-util.ts
+++ polygerrit-ui/app/utils/label-util.ts
@@ -263,9 +263,9 @@ export function iconForStatus(status: SubmitRequirementStatus) {
  * If there is at least one non-legacy requirement, filter legacy requirements.
  */
 export function getRequirements(change?: ParsedChangeInfo | ChangeInfo) {
-  return (change?.submit_requirements ?? [])
-    .filter(req => req.status !== SubmitRequirementStatus.NOT_APPLICABLE)
-    .filter(req => req.is_legacy === false);
+  return (change?.submit_requirements ?? []).filter(
+    req => req.status !== SubmitRequirementStatus.NOT_APPLICABLE
+  );
 }
 
 // TODO(milutin): This may be temporary for demo purposes

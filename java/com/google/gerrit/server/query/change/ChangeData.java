ce5a939574a124d9b49c80990efbe6ec0ef3affe
Patrick Hiesel <hiesel@google.com>
2021-12-10 08:35:53 +0000

    Revert "Submit requirements: Always compute requirements to populate dashboards"
    This reverts commit 29078b22495ad0959f7bf3d609788b909df14fde.
    
    Reason for revert: No required anymore after we upgraded the change index
    
    Change-Id: I5f85152ea5d8befa25f07b56e02b11981785c6c6

--- java/com/google/gerrit/server/query/change/ChangeData.java
+++ java/com/google/gerrit/server/query/change/ChangeData.java
@@ -979,15 +979,7 @@ public class ChangeData {
 
   public void setSubmitRequirements(
       Map<SubmitRequirement, SubmitRequirementResult> submitRequirements) {
-    if (!experimentFeatures.isFeatureEnabled(
-        ExperimentFeaturesConstants
-            .GERRIT_BACKEND_REQUEST_FEATURE_ENABLE_SUBMIT_REQUIREMENTS_BACKFILLING_ON_DASHBOARD)) {
-      // Only set back values from the index if the experiment is not active. While the experiment
-      // is active, we want
-      // to compute SRs from scratch to ensure fresh results.
-      // TODO(ghareeb, hiesel): Remove this.
-      this.submitRequirements = submitRequirements;
-    }
+    this.submitRequirements = submitRequirements;
   }
 
   public List<SubmitRecord> submitRecords(SubmitRuleOptions options) {

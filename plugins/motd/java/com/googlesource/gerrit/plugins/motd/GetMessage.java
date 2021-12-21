ea5196fd961a5041bbf7d36b6889e6f0e7aa99e7
Han-Wen Nienhuys <hanwen@google.com>
2021-12-21 18:57:40 +0100

    Demo of using the `banner` endpoint
    DO NOT SUBMIT
    
    Console:
    
    gr-app.js:23044 TypeError: Cannot read properties of undefined (reading 'restApi')
        at X.connectedCallback (gr-motd.js:43:78)
        at Yv._appendChild (gr-app.js:4759:3000)
        at gr-app.js:4759:1887
    
    Change-Id: Ie35c98873276f5d7af1a2b494dc9f8c570c200e2

---
+++ plugins/motd/java/com/googlesource/gerrit/plugins/motd/GetMessage.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.motd;
+
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.config.ConfigResource;
+import com.google.gerrit.server.config.GerritServerConfig;
+import javax.inject.Inject;
+import org.eclipse.jgit.lib.Config;
+
+class GetMessage implements RestReadView<ConfigResource> {
+  String message;
+
+  @Inject
+  GetMessage(@GerritServerConfig Config config) {
+    this.message = config.getString("plugin", "motd", "message");
+  }
+
+  @Override
+  public Response<String> apply(ConfigResource rsrc) {
+    return Response.ok(this.message);
+  }
+}

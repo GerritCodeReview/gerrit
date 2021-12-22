e4f5b57eed8e5350b243453110144917805a7535
Paladox none <thomasmulhall410@yahoo.com>
2021-12-22 20:12:51 +0000

    Convert gr-dashboard-view_test.js to typescript
    Change-Id: Ie3890600379dbcfb764700fe35e7e0c90d4a40ad

--- polygerrit-ui/app/elements/change-list/gr-dashboard-view/gr-dashboard-view.ts
+++ polygerrit-ui/app/elements/change-list/gr-dashboard-view/gr-dashboard-view.ts
@@ -43,7 +43,7 @@ import {
   PreferencesInput,
   RepoName,
 } from '../../../types/common';
-import {AppElementDashboardParams, AppElementParams} from '../../gr-app-types';
+import {AppElementDashboardParams} from '../../gr-app-types';
 import {GrDialog} from '../../shared/gr-dialog/gr-dialog';
 import {GrCreateCommandsDialog} from '../gr-create-commands-dialog/gr-create-commands-dialog';
 import {
@@ -98,7 +98,7 @@ export class GrDashboardView extends PolymerElement {
   viewState?: DashboardViewState;
 
   @property({type: Object})
-  params?: AppElementParams;
+  params?: AppElementDashboardParams;
 
   @property({type: Array})
   _results?: DashboardChange[];
@@ -115,11 +115,12 @@ export class GrDashboardView extends PolymerElement {
   @property({type: Number})
   _selectedChangeIndex?: number;
 
-  private reporting = getAppContext().reportingService;
-
   private readonly restApiService = getAppContext().restApiService;
 
   private lastVisibleTimestampMs = 0;
+
+  // private but used in test
+  reporting = getAppContext().reportingService;
 
   constructor() {
     super();
@@ -192,7 +193,7 @@ export class GrDashboardView extends PolymerElement {
     return 'Dashboard for ' + user;
   }
 
-  _isViewActive(params: AppElementParams): params is AppElementDashboardParams {
+  _isViewActive(params: AppElementDashboardParams) {
     return params.view === GerritView.DASHBOARD;
   }
 
@@ -217,7 +218,7 @@ export class GrDashboardView extends PolymerElement {
   /**
    * Reloads the element.
    */
-  _reload(params?: AppElementParams) {
+  _reload(params?: AppElementDashboardParams) {
     if (!params || !this._isViewActive(params)) {
       return Promise.resolve();
     }
@@ -350,7 +351,7 @@ export class GrDashboardView extends PolymerElement {
     return `(${numChanges}${andMore})`;
   }
 
-  _computeUserHeaderClass(params: AppElementParams) {
+  _computeUserHeaderClass(params?: AppElementDashboardParams) {
     if (
       !params ||
       params.view !== GerritView.DASHBOARD ||

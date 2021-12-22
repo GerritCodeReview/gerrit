45608bfc59b8ef128ff9ee66ff5f174c7f0c75e9
Ben Rohlfs <brohlfs@google.com>
2021-12-22 11:37:41 +0100

    Add support for deduping reporting events per session/change
    A lot of reporting events may occur many times per change or per
    session. We do not want to report each of these, but most of the
    time we just want to report once per change or per session.
    
    We are adding dedicated support for this to `reportInteraction`
    and `reportExecution`.
    
    As a first example we are also using the new feature for reporting
    when the checks tab is rendered.
    
    Google-Bug-Id: b/201618770
    Change-Id: I41bd6b620adac51f82df4aca8ffa71b4c0b1a65b

--- polygerrit-ui/app/api/reporting.ts
+++ polygerrit-ui/app/api/reporting.ts
@@ -18,6 +18,27 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type EventDetails = any;
 
+export enum Deduping {
+  /**
+   * Only report the event once per session, even if the event details are
+   * different.
+   */
+  EVENT_ONCE_PER_SESSION = 'EVENT_ONCE_PER_SESSION',
+  /**
+   * Only report the event once per change, even if the event details are
+   * different.
+   */
+  EVENT_ONCE_PER_CHANGE = 'EVENT_ONCE_PER_CHANGE',
+  /** Only report these exact event details once per session. */
+  DETAILS_ONCE_PER_SESSION = 'DETAILS_ONCE_PER_SESSION',
+  /** Only report these exact event details once per change. */
+  DETAILS_ONCE_PER_CHANGE = 'DETAILS_ONCE_PER_CHANGE',
+}
+export declare interface ReportingOptions {
+  /** Set this, if you don't want to report *every* time. */
+  deduping?: Deduping;
+}
+
 export declare interface ReportingPluginApi {
   reportInteraction(eventName: string, details?: EventDetails): void;
 

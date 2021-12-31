12f5650979f551960ba6f36c1ca8519ccbace90c
David Ostrovsky <david@ostrovsky.org>
2021-12-31 07:51:28 +0100

    Bump Jetty version to 12.0.10 and servlet api to 6.0
    Eclipse Jetty 12 has been released and certified as compatible with the
    Jakarta Servlet v6.0 specification, released as part of Jakarta EE 10:
    [1]. This new version of the Servlet specification uses the new jakarta
    namespace, instead of the old javax.servlet namespace.
    
    To update Gerrit to Jetty 12, we have to replace servlet-api with
    jakarta servlet API.
    
    Gitiles was updated to Jetty 12.0.18 in this change:[3] and
    gitiles-servlet was bumped to 2.0.0 in this pending change: [4].
    
    Bump Google Guice to 7.0.0, see this issue: [5] and the 7.0 release
    notes [6] for more details.
    
    Add new dependency on jetty-session library.
    
    JGit was updated to pending merge of the JGit next branch [7] and the
    JGit submodule was updated to point to it.
    
    There were some test breakages, that were fixed.
    
    HttpLogoutServletIT#shouldHonourCanonicalWebUrlProxyWhenRedirectAfterLogout
    This needs further investigation, why the return URL differs.
    
    NotFoundIT#nonExistingRootCollectionPost
    As explained in ths issue [8], the old behaviour to return 404 non found
    was a bug in older Jetty releases, that is now fixed in Jetty 12. Hence
    we move this method to MethodNotAllowedIT.
    
    CreateChangeIT, DeleteBranchIT and ChangeIdIT tests had failures due
    to usage of ambiguous path segments and ambiguous path encoding.
    
    As explained in this issue [9], Servlet API 6.0 doesn't allow
    ambiguous path segments and ambiguous path encoding, see: [10], [11].
    
    Gerrit is currently doesn't comply with Servlet API 6.0, because encoded
    '/' character is extensively used as path separator, for example:
    DELETE '/projects/foo%2Fbar%2Fbaz/branches/refs%2Fheads%2Ftest'
    is deleting the branch refs/heads/test for the project with name:
    foo/bar/baz. To make gerrit Servlet API 6.0 compliant, this request
    would need to be rewritten as:
      DELETE '/projects/foo/bar/baz/branches/refs/heads/test'
    
    Alternatively pass project and branch names not as part of the path
    section, but pass that encoded reference as part of the query section
    of a URI.
    
    However this would require substantial rework of the current code and
    would also impact other gerrit clients.
    
    As a temporarily workaround we disable compliance with this part of the
    Servlet API 6.0 in Jetty 12.
    
    [1] https://jakarta.ee/release/10
    [2] https://eclipse.gerrithub.io/c/eclipse-jgit/jgit/+/189213
    [3] https://gerrit-review.googlesource.com/c/gitiles/+/428937
    [4] https://gerrit-review.googlesource.com/c/gitiles/+/424638
    [5] https://github.com/google/guice/issues/1490
    [6] https://github.com/google/guice/wiki/Guice700
    [7] https://eclipse.gerrithub.io/c/eclipse-jgit/jgit/+/1195683
    [8] https://github.com/jetty/jetty.project/issues/11889
    [9] https://github.com/jetty/jetty.project/issues/11890
    [10] https://github.com/jakartaee/servlet/issues/18
    [11] https://github.com/jakartaee/servlet/blob/6.0.0-RELEASE/spec/src/main/asciidoc/servlet-spec-body.adoc#352-uri-path-canonicalization
    
    Release-Notes: Update Jetty version to 12.0.10 and servlet api to 6.0
    Change-Id: I8984ab3eb4648caf8cd9d2c1dc96cb5a2f6b4add

--- java/com/google/gerrit/httpd/auth/openid/OAuthWebFilterOverOpenID.java
+++ java/com/google/gerrit/httpd/auth/openid/OAuthWebFilterOverOpenID.java
@@ -20,17 +20,17 @@ import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.NavigableMap;
 import java.util.NavigableSet;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /** OAuth web filter uses active OAuth session to perform OAuth requests */
 @Singleton

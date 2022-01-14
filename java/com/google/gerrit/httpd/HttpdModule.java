7eef3e2615796930c20f75c55929ffa98817e586
Thomas Draebing <thomas.draebing@sap.com>
2022-01-14 10:50:09 +0100

    Internally handle refs updated in a BatchRefUpdate by a single event
    Some refs are updated using a BatchRefUpdate to allow atomic changes
    of multiple refs at the same time. However, for each ref a separate
    event was fired. Thus, the events were not representing the ref update
    correctly and that those refs are connected. This could lead to issues.
    For example, in the replication plugin a change-ref could be pushed
    before the associated meta-ref. However, without the meta-ref the change
    cannot be fetched, since the permissions cannot be computed.
    
    This change changes the event fired internally upon a ref update. The
    event now contains all refs updated in a single operation. Listeners
    can then decide how to deal with this event. For now, stream events
    will still emit an event for every single ref to provide compatibility
    to external tools. However, internally, now the information exists
    which refs were created together. Thus, for example, the replication
    plugin will be able to schedule the change-ref in the same push as the
    meta-ref, fixing the issue described earlier.
    
    Bug: Issue 15194
    Change-Id: I79eefbee9588879183d418ee32b7f7d390220ba7

--- java/com/google/gerrit/httpd/HttpdModule.java
+++ java/com/google/gerrit/httpd/HttpdModule.java
@@ -2,13 +2,13 @@ package com.google.gerrit.httpd;
 
 import com.google.gerrit.extensions.annotations.Exports;
 import com.google.gerrit.extensions.config.FactoryModule;
-import com.google.gerrit.extensions.events.GitReferenceUpdatedListener;
+import com.google.gerrit.extensions.events.GitReferencesUpdatedListener;
 
 public class HttpdModule extends FactoryModule {
   @Override
   protected void configure() {
-    bind(GitReferenceUpdatedListener.class)
-        .annotatedWith(Exports.named(GitReferenceUpdatedTracker.class.getSimpleName()))
-        .to(GitReferenceUpdatedTracker.class);
+    bind(GitReferencesUpdatedListener.class)
+        .annotatedWith(Exports.named(GitReferencesUpdatedTracker.class.getSimpleName()))
+        .to(GitReferencesUpdatedTracker.class);
   }
 }

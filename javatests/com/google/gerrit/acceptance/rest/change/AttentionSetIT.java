c4a06a0778b179614e0d3b61bdd1d83dec28ebb2
David Åkerman <davidak@axis.com>
2022-01-17 16:26:19 +0100

    Separate reviewer-recipients from project-watch-recipients
    To ensure that project-watch emails are sent regardless of other
    notfication configurations. Without this fix you will not get any
    email notifications for watched projects when you have set
    "Only when I am in the Attention-Set" and you are outside of
    the attention-set.
    
    Release-Notes: Fixed lost watch notifications when attention-set-only is configured
    Bug: Issue 40014061
    Change-Id: I0707c588f23f13c617cdfb565171adff12833ea9
    (cherry picked from commit 03ba8b11c9f0d5c4a87a8bf58fb30e4270e37a54)

--- javatests/com/google/gerrit/acceptance/rest/change/AttentionSetIT.java
+++ javatests/com/google/gerrit/acceptance/rest/change/AttentionSetIT.java
@@ -1519,11 +1519,7 @@ public class AttentionSetIT extends AbstractDaemonTest {
 
     // Add preference for the user such that they only receive an email on changes that require
     // their attention.
-    requestScopeOperations.setApiUser(user.id());
-    GeneralPreferencesInfo prefs = gApi.accounts().self().getPreferences();
-    prefs.emailStrategy = EmailStrategy.ATTENTION_SET_ONLY;
-    gApi.accounts().self().setPreferences(prefs);
-    requestScopeOperations.setApiUser(admin.id());
+    setEmailStrategyForUser(EmailStrategy.ATTENTION_SET_ONLY);
 
     // Add user to attention set. They receive an email since they are in the attention set.
     change(r).addReviewer(user.id().toString());
@@ -1597,11 +1593,7 @@ public class AttentionSetIT extends AbstractDaemonTest {
   public void attentionSetWithEmailFilterImpactingOnlyChangeEmails() throws Exception {
     // Add preference for the user such that they only receive an email on changes that require
     // their attention.
-    requestScopeOperations.setApiUser(user.id());
-    GeneralPreferencesInfo prefs = gApi.accounts().self().getPreferences();
-    prefs.emailStrategy = EmailStrategy.ATTENTION_SET_ONLY;
-    gApi.accounts().self().setPreferences(prefs);
-    requestScopeOperations.setApiUser(admin.id());
+    setEmailStrategyForUser(EmailStrategy.ATTENTION_SET_ONLY);
 
     // Ensure emails that don't relate to changes are still sent.
     gApi.accounts().id(user.id().get()).generateHttpPassword();
@@ -1756,6 +1748,27 @@ public class AttentionSetIT extends AbstractDaemonTest {
     change(r).attention(admin.id().toString()).remove(new AttentionSetInput("removed"));
     assertThat(Iterables.getOnlyElement(getAttentionSetUpdatesForUser(r, admin)).operation())
         .isEqualTo(Operation.REMOVE);
+  }
+
+  @Test
+  public void outsideAttentionSet_watchProjectEmailReceived() throws Exception {
+    setEmailStrategyForUser(EmailStrategy.ATTENTION_SET_ONLY);
+
+    requestScopeOperations.setApiUser(user.id());
+    watch(project.get());
+
+    createChange();
+
+    assertThat(sender.getMessages()).isNotEmpty();
+    sender.clear();
+  }
+
+  private void setEmailStrategyForUser(EmailStrategy es) throws Exception {
+    requestScopeOperations.setApiUser(user.id());
+    GeneralPreferencesInfo prefs = gApi.accounts().self().getPreferences();
+    prefs.emailStrategy = es;
+    gApi.accounts().self().setPreferences(prefs);
+    requestScopeOperations.setApiUser(admin.id());
   }
 
   private List<AttentionSetUpdate> getAttentionSetUpdatesForUser(

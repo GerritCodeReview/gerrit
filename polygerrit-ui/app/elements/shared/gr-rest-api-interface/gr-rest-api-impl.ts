934b3afc07881bf148d2b195c3bffe06b91c0763
Ben Rohlfs <brohlfs@google.com>
2022-01-21 14:30:30 +0100

    Prevent running into the error `'conflicts:' operator is not supported`
    The config value `conflicts_predicate_enabled` was introduced in
    change 306509. Since then the change page logs an error every time that
    a change page is loaded. We can easily check whether the config value
    is set, so only make the call when we know that it is supported.
    
    Google-Bug-Id: b/201122253
    Change-Id: I1ae19c2655de504f398b8a836c4299adad14ed38

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-impl.ts
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-api-impl.ts
@@ -1701,9 +1701,13 @@ export class GrRestApiServiceImpl
     }) as Promise<SubmittedTogetherInfo | undefined>;
   }
 
-  getChangeConflicts(
+  async getChangeConflicts(
     changeNum: NumericChangeId
   ): Promise<ChangeInfo[] | undefined> {
+    const config = await this.getConfig(false);
+    if (!config?.change?.conflicts_predicate_enabled) {
+      return [];
+    }
     const options = listChangesOptionsToHex(
       ListChangesOption.CURRENT_REVISION,
       ListChangesOption.CURRENT_COMMIT

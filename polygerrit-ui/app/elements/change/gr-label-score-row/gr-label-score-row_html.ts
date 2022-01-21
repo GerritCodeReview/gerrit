db7fb3d57ef6ef22a4380e69495031469226c3f9
Chris Poucet <poucet@google.com>
2022-01-21 09:18:13 +0100

    Move to new UI spec for vote-chips
    Google-Bug-Id: b/203504371
    Change-Id: I8fe8ebc4e226cdc05514b017d4ae730f4b368420

--- polygerrit-ui/app/elements/change/gr-label-score-row/gr-label-score-row_html.ts
+++ polygerrit-ui/app/elements/change/gr-label-score-row/gr-label-score-row_html.ts
@@ -53,26 +53,33 @@ export const htmlTemplate = html`
       );
       padding: 0 var(--spacing-m);
     }
+    gr-button::part(paper-button) {
+      border-color: var(--vote-outline-color-unselected);
+    }
     gr-button[vote='max'].iron-selected {
+      --vote-text-color: black;
       --button-background-color: var(--vote-color-approved);
     }
     gr-button[vote='positive'].iron-selected {
+      --vote-text-color: black;
       --button-background-color: var(--vote-color-recommended);
     }
-    gr-button[vote='min'].iron-selected {
-      --button-background-color: var(--vote-color-rejected);
-    }
-    gr-button[vote='negative'].iron-selected {
-      --button-background-color: var(--vote-color-disliked);
-    }
     gr-button[vote='neutral'].iron-selected {
+      --vote-text-color: black;
       --button-background-color: var(--vote-color-neutral);
     }
-    gr-button[vote='positive'].iron-selected::part(paper-button) {
-      border-color: var(--vote-outline-recommended);
+    gr-button[vote='negative'].iron-selected {
+      --vote-text-color: black;
+      --button-background-color: var(--vote-color-disliked);
     }
-    gr-button[vote='negative'].iron-selected::part(paper-button) {
-      border-color: var(--vote-outline-disliked);
+    gr-button[vote='min'].iron-selected {
+      --vote-text-color: black;
+      --button-background-color: var(--vote-color-rejected);
+    }
+
+    gr-button.iron-selected::part(paper-button) {
+      border-color: transparent;
+      box-shadow: var(--elevation-level-1);
     }
     gr-button > gr-tooltip-content {
       margin: 0px -10px;
@@ -128,6 +135,7 @@ export const htmlTemplate = html`
           data-value$="[[value]]"
           aria-label$="[[value]]"
           voteChip
+          flatten
         >
           <gr-tooltip-content
             has-tooltip

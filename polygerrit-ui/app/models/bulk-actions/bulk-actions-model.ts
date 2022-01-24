8743a6b88e1b53a96e07df6e48c18ddc730893f6
Dhruv Srivastava <dhruvsri@google.com>
2022-01-24 11:39:21 +0100

    Add bulk actions model
    The model maintains the list of selected changes that the user has
    selected on the change page/search page and wants to apply the actions
    on.
    
    Change-Id: Ic50bc9d4f0c5dde1f804cf4ca5c428b31e9cddcd

---
+++ polygerrit-ui/app/models/bulk-actions/bulk-actions-model.ts
@@ -0,0 +1,54 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {ChangeInfo} from '../../api/rest-api';
+import {Model} from '../model';
+import {Finalizable} from '../../services/registry';
+import {RestApiService} from '../../services/gr-rest-api/gr-rest-api';
+
+export interface BulkActionsState {
+  selectedChanges: ChangeInfo[];
+}
+
+const initialState: BulkActionsState = {
+  selectedChanges: [],
+};
+
+export class BulkActionsModel
+  extends Model<BulkActionsState>
+  implements Finalizable
+{
+  constructor(_restApiService: RestApiService) {
+    super(initialState);
+  }
+
+  addSelectedChange(change: ChangeInfo) {
+    const current = this.subject$.getValue();
+    const selectedChanges = [...current.selectedChanges];
+    selectedChanges.push(change);
+    this.setState({...current, selectedChanges});
+  }
+
+  removeSelectedChange(change: ChangeInfo) {
+    const current = this.subject$.getValue();
+    const selectedChanges = [...current.selectedChanges];
+    const index = selectedChanges.findIndex(item => item.id === change.id);
+    if (index === -1) return;
+    selectedChanges.splice(index, 1);
+    this.setState({...current, selectedChanges});
+  }
+
+  /** Required for testing */
+  getState() {
+    return this.subject$.getValue();
+  }
+
+  setState(state: BulkActionsState) {
+    this.subject$.next(state);
+  }
+
+  finalize() {}
+}

f9836ca9f37710f30fee527e6530e406c96b0c9c
Frank Borden <frankborden@google.com>
2022-01-24 18:09:51 +0100

    Introduce hovercard status plugin endpoint
    Change-Id: If35f0e5b915d95d89912675d47383801eb1b2994

--- polygerrit-ui/app/elements/shared/gr-hovercard-account/gr-hovercard-account.ts
+++ polygerrit-ui/app/elements/shared/gr-hovercard-account/gr-hovercard-account.ts
@@ -18,6 +18,8 @@
 import '@polymer/iron-icon/iron-icon';
 import '../gr-avatar/gr-avatar';
 import '../gr-button/gr-button';
+import '../../plugins/gr-endpoint-decorator/gr-endpoint-decorator';
+import '../../plugins/gr-endpoint-param/gr-endpoint-param';
 import {getAppContext} from '../../../services/app-context';
 import {accountKey, isSelf} from '../../../utils/account-util';
 import {customElement, property} from 'lit/decorators';
@@ -102,6 +104,7 @@ export class GrHovercardAccount extends base {
         .top,
         .attention,
         .status,
+        .statusPlugin,
         .voteable {
           padding: var(--spacing-s) var(--spacing-l);
         }
@@ -172,7 +175,7 @@ export class GrHovercardAccount extends base {
           <div class="email">${this.account.email}</div>
         </div>
       </div>
-      ${this.renderAccountStatus()}
+      ${this.renderAccountStatusPlugins()} ${this.renderAccountStatus()}
       ${this.voteableText
         ? html`
             <div class="voteable">
@@ -209,6 +212,21 @@ export class GrHovercardAccount extends base {
         >
           ${this.computeChangeReviewerOrCCText()}
         </gr-button>
+      </div>
+    `;
+  }
+
+  private renderAccountStatusPlugins() {
+    return html`
+      <div class="statusPlugin">
+        <span class="title">
+          <gr-endpoint-decorator name="hovercard-status">
+            <gr-endpoint-param
+              name="accountId"
+              .value="${this.account._account_id}"
+            ></gr-endpoint-param>
+          </gr-endpoint-decorator>
+        </span>
       </div>
     `;
   }

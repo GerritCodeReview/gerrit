7e01217f6f8be6f08bf9c76107f9834897a987c4
Dhruv Srivastava <dhruvsri@google.com>
2022-02-01 15:53:38 +0100

    Store selected changes in the model
    When a change is selected in the dashboard/search page, we update the
    model of the section with the change.
    The checked state of the checkbox is calculated by subscribing to the
    model state.
    
    The model state is reset everytime a new section component is rendered
    which means reloading the page via the browser means the model state
    is reset but the automatic reload of the dashboard happens without
    re-rendering the components, only the list of changes is updated so
    the model retains the list of changes upon reload.
    
    Once an automatic reload happens, it is possible a change that was
    selected by the user is now no longer visible on the dashboard, so it
    need to be removed from the model as a special case.
    
    Change-Id: I869eca780688807311f8d43fde123a6ff03518dc

--- polygerrit-ui/app/models/bulk-actions/bulk-actions-model_test.ts
+++ polygerrit-ui/app/models/bulk-actions/bulk-actions-model_test.ts
@@ -9,6 +9,7 @@ import {ChangeInfoId} from '../../api/rest-api';
 import {BulkActionsModel} from './bulk-actions-model';
 import {getAppContext} from '../../services/app-context';
 import '../../test/common-test-setup-karma';
+import {waitUntilObserved} from '../../test/test-utils';
 
 suite('bulk actions model test', () => {
   test('add and remove selected changes', () => {
@@ -37,5 +38,28 @@ suite('bulk actions model test', () => {
 
     bulkActionsModel.removeSelectedChangeId(c2.id);
     assert.deepEqual(bulkActionsModel.getState().selectedChangeIds, []);
+  });
+
+  test('stale changes are removed from the model', async () => {
+    const bulkActionsModel = new BulkActionsModel(
+      getAppContext().restApiService
+    );
+
+    bulkActionsModel.addSelectedChangeId('0' as ChangeInfoId);
+    bulkActionsModel.addSelectedChangeId('1' as ChangeInfoId);
+
+    let selectedChangeIds = await waitUntilObserved(
+      bulkActionsModel!.selectedChangeIds$,
+      s => s.length === 2
+    );
+
+    assert.deepEqual(selectedChangeIds, ['0', '1']);
+
+    bulkActionsModel.sync(['1' as ChangeInfoId]);
+    selectedChangeIds = await waitUntilObserved(
+      bulkActionsModel!.selectedChangeIds$,
+      s => s.length === 1
+    );
+    assert.deepEqual(selectedChangeIds, ['1']);
   });
 });

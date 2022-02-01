69d35592a12e3a20fdb1265c37cca262261c5d9c
Dhruv Srivastava <dhruvsri@google.com>
2022-02-01 15:53:38 +0100

    Store selected changes in the model
    When a change is selected in the dashboard/search page, we update the
    model of the section with the change.
    The checked state of the checkbox is calculated by subscribing to the
    model state.
    
    The model state is reset everytime a new section component is rendered
    which means reloading the page via the browser means the model state
    is reset but the automatic reload of the dashboard happens without
    re-rendering the components, only the list of changes is updated so
    the model retains the list of changes upon reload.
    
    Once an automatic reload happens, it is possible a change that was
    selected by the user is now no longer visible on the dashboard, so it
    need to be removed from the model as a special case.
    
    Change-Id: I869eca780688807311f8d43fde123a6ff03518dc

--- polygerrit-ui/app/elements/change-list/gr-change-list-item/gr-change-list-item_test.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-item/gr-change-list-item_test.ts
@@ -17,7 +17,7 @@
 
 import {fixture} from '@open-wc/testing-helpers';
 import {html} from 'lit';
-import {SubmitRequirementResultInfo} from '../../../api/rest-api';
+import {SubmitRequirementResultInfo, ChangeInfoId} from '../../../api/rest-api';
 import {getAppContext} from '../../../services/app-context';
 import '../../../test/common-test-setup-karma';
 import {
@@ -33,6 +33,7 @@ import {
   queryAndAssert,
   stubRestApi,
   stubFlags,
+  waitUntilObserved,
 } from '../../../test/test-utils';
 import {
   AccountId,
@@ -294,12 +295,43 @@ suite('gr-change-list-item tests', () => {
     }
   });
 
-  test('selection checkbox is only shown if experiment is enabled', async () => {
-    assert.isNotOk(query(element, '.selection'));
-    stubFlags('isEnabled').returns(true);
-    element = basicFixture.instantiate();
-    await element.updateComplete;
-    assert.isOk(query(element, '.selection'));
+  suite('checkbox', () => {
+    test('selection checkbox is only shown if experiment is enabled', async () => {
+      assert.isNotOk(query(element, '.selection'));
+      stubFlags('isEnabled').returns(true);
+      element = basicFixture.instantiate();
+      await element.updateComplete;
+      assert.isOk(query(element, '.selection'));
+    });
+
+    test('checkbox state updates with model updates', async () => {
+      stubFlags('isEnabled').returns(true);
+      element.requestUpdate();
+      await element.updateComplete;
+
+      element.change = {...createChange(), id: '1' as ChangeInfoId};
+      element.getBulkActionsModel().addSelectedChangeId(element.change.id);
+      await waitUntilObserved(
+        element.getBulkActionsModel().selectedChangeIds$,
+        s => s.length === 1
+      );
+      await element.updateComplete;
+
+      const checkbox = queryAndAssert<HTMLInputElement>(
+        element,
+        '.selection > input'
+      );
+      assert.isTrue(checkbox.checked);
+
+      element.getBulkActionsModel().removeSelectedChangeId(element.change.id);
+      await waitUntilObserved(
+        element.getBulkActionsModel().selectedChangeIds$,
+        s => s.length === 0
+      );
+      await element.updateComplete;
+
+      assert.isFalse(checkbox.checked);
+    });
   });
 
   test('repo column hidden', async () => {

47fbf4189b872947a1c6a9be8efec6fb77a4c2a2
Dhruv Srivastava <dhruvsri@google.com>
2022-02-02 10:33:00 +0000

    Apply "Extract dashboard section into its own component"
    This reverts commit 543a9f8ce35dee866aba6323d807fbb04c4e7c24 and adds
    the fix on top.
    Original change had to be reverted because the empty state slot was
    not being applied 2 levels up.
    Hence add a pass through slot in gr-change-list.
    
    Testing mainly relies on unit tests and manually verifying slots are
    rendered on the dashboard.
    Specific steps for testing are:
    - Verify special empty slots for Your Turn and Outgoing Review section
    (screenshot attached)
    - Verify generic empty slots for Incoming Review (screenshot attached)
    - Add missing test coverage for showStar and showNumber property in
     GrChangeList test
    - Fix regression in change list navigation Change 329951
    - Add Shadow DOM UT for GrChangeList Change 329962
    - Add Shadow DOM UT for GrChangeListItem Change 329964
    - Hovercard actions from the dashboard (Add to AS / Remove from AS)
    - Dashboard when logged out, star column is hidden (screenshot attached)
    - Custom dashboard (screenshot attached)
    
    Screenshots: https://imgur.com/a/IlCH1yc
    Change-Id: I7fedb6cc52b819960a17c6f9cfd6bd310deb6e21

---
+++ polygerrit-ui/app/elements/change-list/gr-change-list-section/gr-change-list-section_test.ts
@@ -0,0 +1,223 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {GrChangeListSection} from './gr-change-list-section';
+import '../../../test/common-test-setup-karma';
+import './gr-change-list-section';
+import {
+  createChange,
+  createAccountDetailWithId,
+  createServerInfo,
+} from '../../../test/test-data-generators';
+import {NumericChangeId} from '../../../api/rest-api';
+import {queryAll, query, queryAndAssert} from '../../../test/test-utils';
+import {GrChangeListItem} from '../gr-change-list-item/gr-change-list-item';
+import {columnNames} from '../gr-change-list/gr-change-list';
+
+const basicFixture = fixtureFromElement('gr-change-list-section');
+
+suite('dashboard queries', () => {
+  let element: GrChangeListSection;
+
+  setup(() => {
+    element = basicFixture.instantiate();
+  });
+
+  teardown(() => {
+    sinon.restore();
+  });
+
+  test('query without age and limit unchanged', () => {
+    const query = 'status:closed owner:me';
+    assert.deepEqual(element.processQuery(query), query);
+  });
+
+  test('query with age and limit', () => {
+    const query = 'status:closed age:1week limit:10 owner:me';
+    const expectedQuery = 'status:closed owner:me';
+    assert.deepEqual(element.processQuery(query), expectedQuery);
+  });
+
+  test('query with age', () => {
+    const query = 'status:closed age:1week owner:me';
+    const expectedQuery = 'status:closed owner:me';
+    assert.deepEqual(element.processQuery(query), expectedQuery);
+  });
+
+  test('query with limit', () => {
+    const query = 'status:closed limit:10 owner:me';
+    const expectedQuery = 'status:closed owner:me';
+    assert.deepEqual(element.processQuery(query), expectedQuery);
+  });
+
+  test('query with age as value and not key', () => {
+    const query = 'status:closed random:age';
+    const expectedQuery = 'status:closed random:age';
+    assert.deepEqual(element.processQuery(query), expectedQuery);
+  });
+
+  test('query with limit as value and not key', () => {
+    const query = 'status:closed random:limit';
+    const expectedQuery = 'status:closed random:limit';
+    assert.deepEqual(element.processQuery(query), expectedQuery);
+  });
+
+  test('query with -age key', () => {
+    const query = 'status:closed -age:1week';
+    const expectedQuery = 'status:closed';
+    assert.deepEqual(element.processQuery(query), expectedQuery);
+  });
+});
+
+suite('gr-change-list sections', () => {
+  let element: GrChangeListSection;
+
+  setup(() => {
+    element = basicFixture.instantiate();
+    element.account = createAccountDetailWithId(1);
+    element.config = createServerInfo();
+    element.visibleChangeTableColumns = columnNames;
+    element.sectionIndex = 0;
+  });
+
+  test('computeItemAbsoluteIndex', () => {
+    element.sections = [
+      {results: new Array(1)},
+      {results: new Array(2)},
+      {results: new Array(3)},
+    ];
+
+    element.sectionIndex = 0;
+    assert.equal(element.computeItemAbsoluteIndex(0), 0);
+    // Out of range but no matter.
+
+    element.sectionIndex = 0;
+    assert.equal(element.computeItemAbsoluteIndex(1), 1);
+
+    element.sectionIndex = 1;
+    assert.equal(element.computeItemAbsoluteIndex(0), 1);
+
+    element.sectionIndex = 1;
+    assert.equal(element.computeItemAbsoluteIndex(1), 2);
+
+    element.sectionIndex = 1;
+    assert.equal(element.computeItemAbsoluteIndex(2), 3);
+
+    element.sectionIndex = 2;
+    assert.equal(element.computeItemAbsoluteIndex(0), 3);
+
+    element.sectionIndex = 3;
+    assert.equal(element.computeItemAbsoluteIndex(0), 6);
+  });
+
+  suite('empty section slots', () => {
+    test('are shown on empty sections with slot name', async () => {
+      const section = {
+        name: 'test',
+        query: 'test',
+        results: [],
+        emptyStateSlotName: 'test',
+      };
+      element.sections = [section];
+      element.changeSection = section;
+      await element.updateComplete;
+
+      assert.isEmpty(queryAll(element, 'gr-change-list-item'));
+      queryAndAssert(element, 'slot[name="test"]');
+    });
+
+    test('are not shown on empty sections without slot name', async () => {
+      const section = {name: 'test', query: 'test', results: []};
+      element.sections = [section];
+      element.changeSection = section;
+      await element.updateComplete;
+
+      assert.isEmpty(queryAll(element, 'gr-change-list-item'));
+      assert.notExists(query(element, 'slot[name="test"]'));
+    });
+
+    test('are not shown on non-empty sections with slot name', async () => {
+      const section = {
+        name: 'test',
+        query: 'test',
+        emptyStateSlotName: 'test',
+        results: [
+          {
+            ...createChange(),
+            _number: 0 as NumericChangeId,
+            labels: {Verified: {approved: {}}},
+          },
+        ],
+      };
+      element.sections = [section];
+      element.changeSection = section;
+      await element.updateComplete;
+
+      assert.isNotEmpty(queryAll(element, 'gr-change-list-item'));
+      assert.notExists(query(element, 'slot[name="test"]'));
+    });
+  });
+});
+
+suite('gr-change-list-section basic tests', () => {
+  let element: GrChangeListSection;
+
+  setup(() => {
+    element = basicFixture.instantiate();
+    element.account = createAccountDetailWithId(1);
+    element.config = createServerInfo();
+    element.visibleChangeTableColumns = columnNames;
+    element.sectionIndex = 0;
+    element.selectedIndex = 0;
+  });
+
+  test('colspans', async () => {
+    element.visibleChangeTableColumns = [];
+    element.sections = [{results: [{...createChange()}]}];
+    element.changeSection = {results: [{...createChange()}]};
+    await element.updateComplete;
+    const tdItemCount = queryAll<HTMLTableElement>(element, 'td').length;
+
+    element.labelNames = [];
+    assert.equal(tdItemCount, element.computeColspan(element.computeColumns()));
+  });
+
+  test('computed fields', () => {
+    assert.equal(element.computeLabelShortcut('Code-Review'), 'CR');
+    assert.equal(element.computeLabelShortcut('Verified'), 'V');
+    assert.equal(element.computeLabelShortcut('Library-Compliance'), 'LC');
+    assert.equal(element.computeLabelShortcut('PolyGerrit-Review'), 'PR');
+    assert.equal(element.computeLabelShortcut('polygerrit-review'), 'PR');
+    assert.equal(
+      element.computeLabelShortcut('Invalid-Prolog-Rules-Label-Name--Verified'),
+      'V'
+    );
+    assert.equal(element.computeLabelShortcut('Some-Special-Label-7'), 'SSL7');
+    assert.equal(
+      element.computeLabelShortcut('--Too----many----dashes---'),
+      'TMD'
+    );
+    assert.equal(
+      element.computeLabelShortcut(
+        'Really-rather-entirely-too-long-of-a-label-name'
+      ),
+      'RRETL'
+    );
+  });
+
+  test('empty section', async () => {
+    element.sections = [{results: []}, {results: []}];
+    element.changeSection = {results: []};
+    await element.updateComplete;
+    const listItems = queryAll<GrChangeListItem>(
+      element,
+      'gr-change-list-item'
+    );
+    assert.equal(listItems.length, 0);
+    const noChangesMsg = queryAll<HTMLTableRowElement>(element, '.noChanges');
+    assert.equal(noChangesMsg.length, 1);
+  });
+});

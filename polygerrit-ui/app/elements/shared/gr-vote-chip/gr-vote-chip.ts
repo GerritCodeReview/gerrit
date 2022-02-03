a13964f8ba6201dd81cdf5814f03706430a9f089
Milutin Kristofic <milutin@google.com>
2022-02-03 14:37:46 +0100

    Submit Requirements - add tooltips to votes
    Functionally same as in old design - like _computeValueTooltip in
    gr-label-info.
    
    Google-Bug-Id: b/217676831
    Change-Id: Ia07a794f49f155775cd0331a786dd41c687a7ba6

--- polygerrit-ui/app/elements/shared/gr-vote-chip/gr-vote-chip.ts
+++ polygerrit-ui/app/elements/shared/gr-vote-chip/gr-vote-chip.ts
@@ -125,14 +125,18 @@ export class GrVoteChip extends LitElement {
     const renderValue = this.renderValue();
     if (!renderValue) return;
 
-    return html`<span class="container ${this.more ? 'more' : ''}">
+    return html`<gr-tooltip-content
+      class="container ${this.more ? 'more' : ''}"
+      title="${this.computeTooltip()}"
+      has-tooltip
+    >
       <div class="vote-chip ${this.computeClass()}">${renderValue}</div>
       ${this.more
         ? html`<div class="chip-angle ${this.computeClass()}">
             ${renderValue}
           </div>`
         : ''}
-    </span>`;
+    </gr-tooltip-content>`;
   }
 
   private renderValue() {
@@ -161,5 +165,12 @@ export class GrVoteChip extends LitElement {
       const status = getLabelStatus(this.label, this.vote?.value);
       return classForLabelStatus(status);
     }
+  }
+
+  private computeTooltip() {
+    if (!this.label || !isDetailedLabelInfo(this.label)) {
+      return '';
+    }
+    return this.label.values?.[valueString(this.vote?.value)] ?? '';
   }
 }

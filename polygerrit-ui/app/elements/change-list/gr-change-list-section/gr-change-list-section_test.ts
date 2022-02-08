3554b8320fa045506b2c0936fbb09fd0fbe99f06
Dhruv Srivastava <dhruvsri@google.com>
2022-02-08 15:25:09 +0100

    Add shadow dom UT for gr-change-list-section
    Change-Id: Ic35e8eefd4443456fd790a7cb7a4ce5e0eb83e6b

--- polygerrit-ui/app/elements/change-list/gr-change-list-section/gr-change-list-section_test.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-section/gr-change-list-section_test.ts
@@ -19,205 +19,278 @@ import {columnNames} from '../gr-change-list/gr-change-list';
 
 const basicFixture = fixtureFromElement('gr-change-list-section');
 
-suite('dashboard queries', () => {
-  let element: GrChangeListSection;
+suite('gr-change-list-section tests', () => {
+  suite('dashboard queries', () => {
+    let element: GrChangeListSection;
 
-  setup(() => {
-    element = basicFixture.instantiate();
-  });
-
-  teardown(() => {
-    sinon.restore();
-  });
-
-  test('query without age and limit unchanged', () => {
-    const query = 'status:closed owner:me';
-    assert.deepEqual(element.processQuery(query), query);
-  });
-
-  test('query with age and limit', () => {
-    const query = 'status:closed age:1week limit:10 owner:me';
-    const expectedQuery = 'status:closed owner:me';
-    assert.deepEqual(element.processQuery(query), expectedQuery);
-  });
-
-  test('query with age', () => {
-    const query = 'status:closed age:1week owner:me';
-    const expectedQuery = 'status:closed owner:me';
-    assert.deepEqual(element.processQuery(query), expectedQuery);
-  });
-
-  test('query with limit', () => {
-    const query = 'status:closed limit:10 owner:me';
-    const expectedQuery = 'status:closed owner:me';
-    assert.deepEqual(element.processQuery(query), expectedQuery);
-  });
-
-  test('query with age as value and not key', () => {
-    const query = 'status:closed random:age';
-    const expectedQuery = 'status:closed random:age';
-    assert.deepEqual(element.processQuery(query), expectedQuery);
-  });
-
-  test('query with limit as value and not key', () => {
-    const query = 'status:closed random:limit';
-    const expectedQuery = 'status:closed random:limit';
-    assert.deepEqual(element.processQuery(query), expectedQuery);
-  });
-
-  test('query with -age key', () => {
-    const query = 'status:closed -age:1week';
-    const expectedQuery = 'status:closed';
-    assert.deepEqual(element.processQuery(query), expectedQuery);
-  });
-});
-
-suite('gr-change-list sections', () => {
-  let element: GrChangeListSection;
-
-  setup(() => {
-    element = basicFixture.instantiate();
-    element.account = createAccountDetailWithId(1);
-    element.config = createServerInfo();
-    element.visibleChangeTableColumns = columnNames;
-    element.sectionIndex = 0;
-  });
-
-  test('computeItemAbsoluteIndex', () => {
-    element.sections = [
-      {results: new Array(1)},
-      {results: new Array(2)},
-      {results: new Array(3)},
-    ];
-
-    element.sectionIndex = 0;
-    assert.equal(element.computeItemAbsoluteIndex(0), 0);
-    // Out of range but no matter.
-
-    element.sectionIndex = 0;
-    assert.equal(element.computeItemAbsoluteIndex(1), 1);
-
-    element.sectionIndex = 1;
-    assert.equal(element.computeItemAbsoluteIndex(0), 1);
-
-    element.sectionIndex = 1;
-    assert.equal(element.computeItemAbsoluteIndex(1), 2);
-
-    element.sectionIndex = 1;
-    assert.equal(element.computeItemAbsoluteIndex(2), 3);
-
-    element.sectionIndex = 2;
-    assert.equal(element.computeItemAbsoluteIndex(0), 3);
-
-    element.sectionIndex = 3;
-    assert.equal(element.computeItemAbsoluteIndex(0), 6);
-  });
-
-  suite('empty section slots', () => {
-    test('are shown on empty sections with slot name', async () => {
-      const section = {
-        name: 'test',
-        query: 'test',
-        results: [],
-        emptyStateSlotName: 'test',
-      };
-      element.sections = [section];
-      element.changeSection = section;
-      await element.updateComplete;
-
-      assert.isEmpty(queryAll(element, 'gr-change-list-item'));
-      queryAndAssert(element, 'slot[name="test"]');
+    setup(() => {
+      element = basicFixture.instantiate();
     });
 
-    test('are not shown on empty sections without slot name', async () => {
-      const section = {name: 'test', query: 'test', results: []};
-      element.sections = [section];
-      element.changeSection = section;
-      await element.updateComplete;
-
-      assert.isEmpty(queryAll(element, 'gr-change-list-item'));
-      assert.notExists(query(element, 'slot[name="test"]'));
+    teardown(() => {
+      sinon.restore();
     });
 
-    test('are not shown on non-empty sections with slot name', async () => {
-      const section = {
-        name: 'test',
-        query: 'test',
-        emptyStateSlotName: 'test',
-        results: [
-          {
-            ...createChange(),
-            _number: 0 as NumericChangeId,
-            labels: {Verified: {approved: {}}},
-          },
-        ],
-      };
-      element.sections = [section];
-      element.changeSection = section;
-      await element.updateComplete;
+    test('query without age and limit unchanged', () => {
+      const query = 'status:closed owner:me';
+      assert.deepEqual(element.processQuery(query), query);
+    });
 
-      assert.isNotEmpty(queryAll(element, 'gr-change-list-item'));
-      assert.notExists(query(element, 'slot[name="test"]'));
+    test('query with age and limit', () => {
+      const query = 'status:closed age:1week limit:10 owner:me';
+      const expectedQuery = 'status:closed owner:me';
+      assert.deepEqual(element.processQuery(query), expectedQuery);
+    });
+
+    test('query with age', () => {
+      const query = 'status:closed age:1week owner:me';
+      const expectedQuery = 'status:closed owner:me';
+      assert.deepEqual(element.processQuery(query), expectedQuery);
+    });
+
+    test('query with limit', () => {
+      const query = 'status:closed limit:10 owner:me';
+      const expectedQuery = 'status:closed owner:me';
+      assert.deepEqual(element.processQuery(query), expectedQuery);
+    });
+
+    test('query with age as value and not key', () => {
+      const query = 'status:closed random:age';
+      const expectedQuery = 'status:closed random:age';
+      assert.deepEqual(element.processQuery(query), expectedQuery);
+    });
+
+    test('query with limit as value and not key', () => {
+      const query = 'status:closed random:limit';
+      const expectedQuery = 'status:closed random:limit';
+      assert.deepEqual(element.processQuery(query), expectedQuery);
+    });
+
+    test('query with -age key', () => {
+      const query = 'status:closed -age:1week';
+      const expectedQuery = 'status:closed';
+      assert.deepEqual(element.processQuery(query), expectedQuery);
     });
   });
-});
 
-suite('gr-change-list-section basic tests', () => {
-  let element: GrChangeListSection;
+  suite('gr-change-list sections', () => {
+    let element: GrChangeListSection;
 
-  setup(() => {
-    element = basicFixture.instantiate();
-    element.account = createAccountDetailWithId(1);
-    element.config = createServerInfo();
-    element.visibleChangeTableColumns = columnNames;
-    element.sectionIndex = 0;
-    element.selectedIndex = 0;
+    setup(() => {
+      element = basicFixture.instantiate();
+      element.account = createAccountDetailWithId(1);
+      element.config = createServerInfo();
+      element.visibleChangeTableColumns = columnNames;
+      element.sectionIndex = 0;
+    });
+
+    test('computeItemAbsoluteIndex', () => {
+      element.sections = [
+        {results: new Array(1)},
+        {results: new Array(2)},
+        {results: new Array(3)},
+      ];
+
+      element.sectionIndex = 0;
+      assert.equal(element.computeItemAbsoluteIndex(0), 0);
+      // Out of range but no matter.
+
+      element.sectionIndex = 0;
+      assert.equal(element.computeItemAbsoluteIndex(1), 1);
+
+      element.sectionIndex = 1;
+      assert.equal(element.computeItemAbsoluteIndex(0), 1);
+
+      element.sectionIndex = 1;
+      assert.equal(element.computeItemAbsoluteIndex(1), 2);
+
+      element.sectionIndex = 1;
+      assert.equal(element.computeItemAbsoluteIndex(2), 3);
+
+      element.sectionIndex = 2;
+      assert.equal(element.computeItemAbsoluteIndex(0), 3);
+
+      element.sectionIndex = 3;
+      assert.equal(element.computeItemAbsoluteIndex(0), 6);
+    });
+
+    suite('empty section slots', () => {
+      test('are shown on empty sections with slot name', async () => {
+        const section = {
+          name: 'test',
+          query: 'test',
+          results: [],
+          emptyStateSlotName: 'test',
+        };
+        element.sections = [section];
+        element.changeSection = section;
+        await element.updateComplete;
+
+        assert.isEmpty(queryAll(element, 'gr-change-list-item'));
+        queryAndAssert(element, 'slot[name="test"]');
+      });
+
+      test('are not shown on empty sections without slot name', async () => {
+        const section = {name: 'test', query: 'test', results: []};
+        element.sections = [section];
+        element.changeSection = section;
+        await element.updateComplete;
+
+        assert.isEmpty(queryAll(element, 'gr-change-list-item'));
+        assert.notExists(query(element, 'slot[name="test"]'));
+      });
+
+      test('are not shown on non-empty sections with slot name', async () => {
+        const section = {
+          name: 'test',
+          query: 'test',
+          emptyStateSlotName: 'test',
+          results: [
+            {
+              ...createChange(),
+              _number: 0 as NumericChangeId,
+              labels: {Verified: {approved: {}}},
+            },
+          ],
+        };
+        element.sections = [section];
+        element.changeSection = section;
+        await element.updateComplete;
+
+        assert.isNotEmpty(queryAll(element, 'gr-change-list-item'));
+        assert.notExists(query(element, 'slot[name="test"]'));
+      });
+    });
   });
 
-  test('colspans', async () => {
-    element.visibleChangeTableColumns = [];
-    element.sections = [{results: [{...createChange()}]}];
-    element.changeSection = {results: [{...createChange()}]};
-    await element.updateComplete;
-    const tdItemCount = queryAll<HTMLTableElement>(element, 'td').length;
+  suite('shadow dom test', () => {
+    let element: GrChangeListSection;
+    let table: HTMLTableElement;
 
-    element.labelNames = [];
-    assert.equal(tdItemCount, element.computeColspan(element.computeColumns()));
+    setup(async () => {
+      table = document.createElement('table');
+      element = basicFixture.instantiate() as any;
+      element.account = createAccountDetailWithId(1);
+      element.config = createServerInfo();
+      element.visibleChangeTableColumns = columnNames;
+      element.sectionIndex = 0;
+      element.selectedIndex = 0;
+      element.changeSection = {results: [{...createChange()}], name: 'a'};
+      await element.updateComplete;
+      // element needs to be wrapped inside a table to get proper dom testing
+      // table and tbody element cannot be separated by shadowDom so
+      // table.appendChild(element) does not work
+      table.appendChild(element.shadowRoot!);
+    });
+
+    test('renders', () => {
+      expect(table).dom.to.equal(/* HTML */ `
+        <gr-change-list-item aria-label="Test subject, section: a" selected="">
+        </gr-change-list-item>
+        <table>
+          <tbody>
+            <tr class="groupHeader">
+              <td aria-hidden="true" class="leftPadding"></td>
+              <td aria-hidden="true" class="star" hidden=""></td>
+              <td class="cell" colspan="13">
+                <h2 class="heading-3">
+                  <a class="section-title" href="">
+                    <span class="section-name"> a </span>
+                    <span class="section-count-label"> </span>
+                  </a>
+                </h2>
+              </td>
+            </tr>
+          </tbody>
+          <tbody class="groupContent">
+            <tr class="groupTitle">
+              <td aria-hidden="true" class="leftPadding"></td>
+              <td aria-label="Star status column" class="star" hidden=""></td>
+              <td class="number" hidden="">#</td>
+              <td class="subject">Subject</td>
+              <td class="status">Status</td>
+              <td class="owner">Owner</td>
+              <td class="reviewers">Reviewers</td>
+              <td class="comments">Comments</td>
+              <td class="repo">Repo</td>
+              <td class="branch">Branch</td>
+              <td class="updated">Updated</td>
+              <td class="size">Size</td>
+              <td class="status">Status</td>
+            </tr>
+          </tbody>
+        </table>
+      `);
+    });
   });
 
-  test('computed fields', () => {
-    assert.equal(element.computeLabelShortcut('Code-Review'), 'CR');
-    assert.equal(element.computeLabelShortcut('Verified'), 'V');
-    assert.equal(element.computeLabelShortcut('Library-Compliance'), 'LC');
-    assert.equal(element.computeLabelShortcut('PolyGerrit-Review'), 'PR');
-    assert.equal(element.computeLabelShortcut('polygerrit-review'), 'PR');
-    assert.equal(
-      element.computeLabelShortcut('Invalid-Prolog-Rules-Label-Name--Verified'),
-      'V'
-    );
-    assert.equal(element.computeLabelShortcut('Some-Special-Label-7'), 'SSL7');
-    assert.equal(
-      element.computeLabelShortcut('--Too----many----dashes---'),
-      'TMD'
-    );
-    assert.equal(
-      element.computeLabelShortcut(
-        'Really-rather-entirely-too-long-of-a-label-name'
-      ),
-      'RRETL'
-    );
-  });
+  suite('gr-change-list-section basic tests', () => {
+    let element: GrChangeListSection;
 
-  test('empty section', async () => {
-    element.sections = [{results: []}, {results: []}];
-    element.changeSection = {results: []};
-    await element.updateComplete;
-    const listItems = queryAll<GrChangeListItem>(
-      element,
-      'gr-change-list-item'
-    );
-    assert.equal(listItems.length, 0);
-    const noChangesMsg = queryAll<HTMLTableRowElement>(element, '.noChanges');
-    assert.equal(noChangesMsg.length, 1);
+    setup(async () => {
+      element = basicFixture.instantiate() as any;
+      element.account = createAccountDetailWithId(1);
+      element.config = createServerInfo();
+      element.visibleChangeTableColumns = columnNames;
+      element.sectionIndex = 0;
+      element.selectedIndex = 0;
+      element.changeSection = {results: [{...createChange()}], name: 'a'};
+      await element.updateComplete;
+    });
+
+    test('colspans', async () => {
+      element.visibleChangeTableColumns = [];
+      element.sections = [{results: [{...createChange()}]}];
+      element.changeSection = {results: [{...createChange()}]};
+      await element.updateComplete;
+      const tdItemCount = queryAll<HTMLTableElement>(element, 'td').length;
+
+      element.labelNames = [];
+      assert.equal(
+        tdItemCount,
+        element.computeColspan(element.computeColumns())
+      );
+    });
+
+    test('computed fields', () => {
+      assert.equal(element.computeLabelShortcut('Code-Review'), 'CR');
+      assert.equal(element.computeLabelShortcut('Verified'), 'V');
+      assert.equal(element.computeLabelShortcut('Library-Compliance'), 'LC');
+      assert.equal(element.computeLabelShortcut('PolyGerrit-Review'), 'PR');
+      assert.equal(element.computeLabelShortcut('polygerrit-review'), 'PR');
+      assert.equal(
+        element.computeLabelShortcut(
+          'Invalid-Prolog-Rules-Label-Name--Verified'
+        ),
+        'V'
+      );
+      assert.equal(
+        element.computeLabelShortcut('Some-Special-Label-7'),
+        'SSL7'
+      );
+      assert.equal(
+        element.computeLabelShortcut('--Too----many----dashes---'),
+        'TMD'
+      );
+      assert.equal(
+        element.computeLabelShortcut(
+          'Really-rather-entirely-too-long-of-a-label-name'
+        ),
+        'RRETL'
+      );
+    });
+
+    test('empty section', async () => {
+      element.sections = [{results: []}, {results: []}];
+      element.changeSection = {results: []};
+      await element.updateComplete;
+      const listItems = queryAll<GrChangeListItem>(
+        element,
+        'gr-change-list-item'
+      );
+      assert.equal(listItems.length, 0);
+      const noChangesMsg = queryAll<HTMLTableRowElement>(element, '.noChanges');
+      assert.equal(noChangesMsg.length, 1);
+    });
   });
 });

7f5579cef70f9d31031d9feac58d3c5ceafbf855
Chris Poucet <poucet@google.com>
2022-02-09 08:55:26 +0100

    Correct timing of SendReply
    Make sure to wait for all work to finish before firing
    change-details-loaded.  This will affect the timing of SendReply.
    
    Google-Issue-Id: b/218474716
    Change-Id: Ib77c30dac3b80487ada941e69bb95c8fc7043e48

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -2103,24 +2103,14 @@ export class GrChangeView extends base {
     // Resolves when the change detail and the edit patch set (if available)
     // are loaded.
     const detailCompletes = this.untilModelLoaded();
-    this.performPostChangeLoadTasks();
     allDataPromises.push(detailCompletes);
 
     // Resolves when the loading flag is set to false, meaning that some
     // change content may start appearing.
-    const loadingFlagSet = detailCompletes
-      .then(() => {
-        this._loading = false;
-        fireEvent(this, 'change-details-loaded');
-      })
-      .then(() => {
-        this.reporting.timeEnd(Timing.CHANGE_RELOAD);
-        if (isLocationChange) {
-          this.reporting.changeDisplayed(
-            roleDetails(this._change, this._account)
-          );
-        }
-      });
+    const loadingFlagSet = detailCompletes.then(() => {
+      this._loading = false;
+      this.performPostChangeLoadTasks();
+    });
 
     // Resolves when the project config has successfully loaded.
     const projectConfigLoaded = detailCompletes.then(() => {
@@ -2170,6 +2160,15 @@ export class GrChangeView extends base {
     }
 
     allDataPromises.push(coreDataPromise);
+    coreDataPromise.then(() => {
+      fireEvent(this, 'change-details-loaded');
+      this.reporting.timeEnd(Timing.CHANGE_RELOAD);
+      if (isLocationChange) {
+        this.reporting.changeDisplayed(
+          roleDetails(this._change, this._account)
+        );
+      }
+    });
 
     if (isLocationChange) {
       this._editingCommitMessage = false;

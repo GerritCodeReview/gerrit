6494d83a3bb943934e66a5efda73c980f58e8a7f
Dhruv Srivastava <dhruvsri@google.com>
2022-02-14 12:37:41 +0100

    Show top level actions when changes are selected
    Change-Id: I0f3dd3639703e919f0c740f8e984edcfc0fe3552

--- polygerrit-ui/app/elements/change-list/gr-change-list-section/gr-change-list-section.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-section/gr-change-list-section.ts
@@ -5,7 +5,7 @@
  */
 
 import {LitElement, html, css, PropertyValues} from 'lit';
-import {customElement, property} from 'lit/decorators';
+import {customElement, property, state} from 'lit/decorators';
 import {ChangeListSection} from '../gr-change-list/gr-change-list';
 import {
   CLOSED,
@@ -85,6 +85,8 @@ export class GrChangeListSection extends LitElement {
   @property({type: Object})
   account: AccountInfo | undefined = undefined;
 
+  @state() showBulkActionsHeader = false;
+
   private readonly flagsService = getAppContext().flagsService;
 
   bulkActionsModel: BulkActionsModel = new BulkActionsModel(
@@ -114,6 +116,10 @@ export class GrChangeListSection extends LitElement {
   constructor() {
     super();
     provide(this, bulkActionsModelToken, () => this.bulkActionsModel);
+    this.bulkActionsModel.selectedChangeNums$.subscribe(
+      selectedChanges =>
+        (this.showBulkActionsHeader = selectedChanges.length > 0)
+    );
   }
 
   override willUpdate(changedProperties: PropertyValues) {
@@ -193,24 +199,40 @@ export class GrChangeListSection extends LitElement {
     `;
   }
 
-  private renderColumnHeaders(columns: string[]) {
+  // TODO: move to it's own component
+  private renderAbandonAction() {
     return html`
-      <tr class="groupTitle">
-        <td class="leftPadding" aria-hidden="true"></td>
-        ${this.renderSelectionHeader()}
-        <td
-          class="star"
-          aria-label="Star status column"
-          ?hidden=${!this.showStar}
-        ></td>
-        <td class="number" ?hidden=${!this.showNumber}>#</td>
-        ${columns.map(item => this.renderHeaderCell(item))}
-        ${this.labelNames?.map(labelName => this.renderLabelHeader(labelName))}
-        ${this.dynamicHeaderEndpoints?.map(pluginHeader =>
-          this.renderEndpointHeader(pluginHeader)
-        )}
-      </tr>
+      <td class="spacing"></td>
+      <td>
+        <gr-button>Abandon</gr-button>
+      </td>
     `;
+  }
+
+  private renderBulkActionsHeader() {
+    return html`${this.renderAbandonAction()}`;
+  }
+
+  private renderColumnHeaders(columns: string[]) {
+    return html` <tr class="groupTitle">
+      ${this.showBulkActionsHeader
+        ? this.renderBulkActionsHeader()
+        : html` <td class="leftPadding" aria-hidden="true"></td>
+            ${this.renderSelectionHeader()}
+            <td
+              class="star"
+              aria-label="Star status column"
+              ?hidden=${!this.showStar}
+            ></td>
+            <td class="number" ?hidden=${!this.showNumber}>#</td>
+            ${columns.map(item => this.renderHeaderCell(item))}
+            ${this.labelNames?.map(labelName =>
+              this.renderLabelHeader(labelName)
+            )}
+            ${this.dynamicHeaderEndpoints?.map(pluginHeader =>
+              this.renderEndpointHeader(pluginHeader)
+            )}`}
+    </tr>`;
   }
 
   private renderSelectionHeader() {

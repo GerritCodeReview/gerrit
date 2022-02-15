c712bd4172eb198a064fcbbd34f13fa50677d945
Ben Rohlfs <brohlfs@google.com>
2022-02-15 15:39:59 +0100

    Make gr-syntax-theme css rules available to LitElements
    Also remove some obsoelte `export {}` calls, which are only required
    when you don't export anything else.
    
    Release-Notes: skip
    Change-Id: I1ffa24b19c0fd9efecb688802b26642a6ee20229

--- polygerrit-ui/app/styles/dashboard-header-styles.ts
+++ polygerrit-ui/app/styles/dashboard-header-styles.ts
@@ -14,13 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import {css} from 'lit';
-
-// Mark the file as a module. Otherwise typescript assumes this is a script
-// and $_documentContainer is a global variable.
-// See: https://www.typescriptlang.org/docs/handbook/modules.html
-export {};
 
 const $_documentContainer = document.createElement('template');
 

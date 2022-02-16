4e635371aae515eced802b1bcac0b52d8f0b7460
David Åkerman <davidak@axis.com>
2022-02-16 11:28:34 +0100

    Add property context_path
    This makes it easier to run the e2e-tests on Gerrit setups where
    context paths are used in the URL to access Gerrit.
    
    Release-Notes: Added property context_path to e2e-tests
    Change-Id: Ieaaa315dd91231b0b00ab84840caa84edc7d36e7

--- e2e-tests/src/test/scala/com/google/gerrit/scenarios/GerritSimulation.scala
+++ e2e-tests/src/test/scala/com/google/gerrit/scenarios/GerritSimulation.scala
@@ -83,6 +83,7 @@ class GerritSimulation extends Simulation {
       in = replaceProperty("http_port", 8080, in)
       in = replaceProperty("http_scheme", "http", in)
       in = replaceProperty("username", "admin", in)
+      in = replaceProperty("context_path", "", in)
       replaceProperty("ssh_port", 29418, in)
   }
 

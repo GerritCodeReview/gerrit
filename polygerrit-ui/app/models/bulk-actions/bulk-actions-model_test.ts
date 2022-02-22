23e08b467c6bc219bb5497d7210fe69eac7265a4
Chris Poucet <poucet@google.com>
2022-02-22 18:00:25 +0100

    Fetch changes when the bulk-actions-model is synced
    Release-Notes: skip
    Change-Id: Ib1458a7fe14bcb560decf1ea48c4e13afe0ec34b

--- polygerrit-ui/app/models/bulk-actions/bulk-actions-model_test.ts
+++ polygerrit-ui/app/models/bulk-actions/bulk-actions-model_test.ts
@@ -5,11 +5,16 @@
  */
 
 import {createChange} from '../../test/test-data-generators';
-import {ChangeInfoId} from '../../api/rest-api';
+import {ChangeInfo, NumericChangeId} from '../../api/rest-api';
 import {BulkActionsModel} from './bulk-actions-model';
 import {getAppContext} from '../../services/app-context';
 import '../../test/common-test-setup-karma';
-import {waitUntilObserved} from '../../test/test-utils';
+import {stubRestApi, waitUntilObserved} from '../../test/test-utils';
+import {mockPromise, MockPromise} from '../../test/test-utils';
+import {
+  listChangesOptionsToHex,
+  ListChangesOption,
+} from '../../utils/change-util';
 
 suite('bulk actions model test', () => {
   let bulkActionsModel: BulkActionsModel;
@@ -19,70 +24,186 @@ suite('bulk actions model test', () => {
 
   test('add changes before sync', () => {
     const c1 = createChange();
-    c1.id = '1' as ChangeInfoId;
+    c1._number = 1 as NumericChangeId;
     const c2 = createChange();
-    c2.id = '2' as ChangeInfoId;
+    c2._number = 2 as NumericChangeId;
 
-    assert.isEmpty(bulkActionsModel.getState().selectedChangeIds);
+    assert.isEmpty(bulkActionsModel.getState().selectedChangeNums);
 
-    assert.throws(() => bulkActionsModel.addSelectedChangeId(c1.id));
-    assert.isEmpty(bulkActionsModel.getState().selectedChangeIds);
+    assert.throws(() => bulkActionsModel.addSelectedChangeNum(c1._number));
+    assert.isEmpty(bulkActionsModel.getState().selectedChangeNums);
     bulkActionsModel.sync([c1, c2]);
 
-    bulkActionsModel.addSelectedChangeId(c2.id);
-    assert.sameMembers(bulkActionsModel.getState().selectedChangeIds, [c2.id]);
+    bulkActionsModel.addSelectedChangeNum(c2._number);
+    assert.sameMembers(bulkActionsModel.getState().selectedChangeNums, [
+      c2._number,
+    ]);
 
-    bulkActionsModel.removeSelectedChangeId(c2.id);
-    assert.isEmpty(bulkActionsModel.getState().selectedChangeIds);
+    bulkActionsModel.removeSelectedChangeNum(c2._number);
+    assert.isEmpty(bulkActionsModel.getState().selectedChangeNums);
   });
 
   test('add and remove selected changes', () => {
     const c1 = createChange();
-    c1.id = '1' as ChangeInfoId;
+    c1._number = 1 as NumericChangeId;
     const c2 = createChange();
-    c2.id = '2' as ChangeInfoId;
+    c2._number = 2 as NumericChangeId;
     bulkActionsModel.sync([c1, c2]);
 
-    assert.isEmpty(bulkActionsModel.getState().selectedChangeIds);
+    assert.isEmpty(bulkActionsModel.getState().selectedChangeNums);
 
-    bulkActionsModel.addSelectedChangeId(c1.id);
-    assert.sameMembers(bulkActionsModel.getState().selectedChangeIds, [c1.id]);
-
-    bulkActionsModel.addSelectedChangeId(c2.id);
-    assert.sameMembers(bulkActionsModel.getState().selectedChangeIds, [
-      c1.id,
-      c2.id,
+    bulkActionsModel.addSelectedChangeNum(c1._number);
+    assert.sameMembers(bulkActionsModel.getState().selectedChangeNums, [
+      c1._number,
     ]);
 
-    bulkActionsModel.removeSelectedChangeId(c1.id);
-    assert.sameMembers(bulkActionsModel.getState().selectedChangeIds, [c2.id]);
+    bulkActionsModel.addSelectedChangeNum(c2._number);
+    assert.sameMembers(bulkActionsModel.getState().selectedChangeNums, [
+      c1._number,
+      c2._number,
+    ]);
 
-    bulkActionsModel.removeSelectedChangeId(c2.id);
-    assert.isEmpty(bulkActionsModel.getState().selectedChangeIds);
+    bulkActionsModel.removeSelectedChangeNum(c1._number);
+    assert.sameMembers(bulkActionsModel.getState().selectedChangeNums, [
+      c2._number,
+    ]);
+
+    bulkActionsModel.removeSelectedChangeNum(c2._number);
+    assert.isEmpty(bulkActionsModel.getState().selectedChangeNums);
   });
 
   test('stale changes are removed from the model', async () => {
     const c1 = createChange();
-    c1.id = '1' as ChangeInfoId;
+    c1._number = 1 as NumericChangeId;
     const c2 = createChange();
-    c2.id = '2' as ChangeInfoId;
+    c2._number = 2 as NumericChangeId;
     bulkActionsModel.sync([c1, c2]);
 
-    bulkActionsModel.addSelectedChangeId(c1.id);
-    bulkActionsModel.addSelectedChangeId(c2.id);
+    bulkActionsModel.addSelectedChangeNum(c1._number);
+    bulkActionsModel.addSelectedChangeNum(c2._number);
 
-    let selectedChangeIds = await waitUntilObserved(
-      bulkActionsModel!.selectedChangeIds$,
+    let selectedChangeNums = await waitUntilObserved(
+      bulkActionsModel!.selectedChangeNums$,
       s => s.length === 2
     );
 
-    assert.sameMembers(selectedChangeIds, [c1.id, c2.id]);
+    assert.sameMembers(selectedChangeNums, [c1._number, c2._number]);
 
     bulkActionsModel.sync([c1]);
-    selectedChangeIds = await waitUntilObserved(
-      bulkActionsModel!.selectedChangeIds$,
+    selectedChangeNums = await waitUntilObserved(
+      bulkActionsModel!.selectedChangeNums$,
       s => s.length === 1
     );
-    assert.sameMembers(selectedChangeIds, [c1.id]);
+    assert.sameMembers(selectedChangeNums, [c1._number]);
+  });
+
+  test('sync fetches new changes', async () => {
+    const c1 = createChange();
+    c1._number = 1 as NumericChangeId;
+    const c2 = createChange();
+    c2._number = 2 as NumericChangeId;
+
+    const responsePromise: MockPromise<ChangeInfo[]> = mockPromise();
+    const getChangesStub = stubRestApi('getChanges').callsFake(
+      (changesPerPage, query, offset, options) => {
+        assert.isUndefined(changesPerPage);
+        assert.strictEqual(query, 'change:1 OR change:2');
+        assert.isUndefined(offset);
+        assert.strictEqual(
+          options,
+          listChangesOptionsToHex(
+            ListChangesOption.CHANGE_ACTIONS,
+            ListChangesOption.CURRENT_ACTIONS,
+            ListChangesOption.CURRENT_REVISION,
+            ListChangesOption.DETAILED_LABELS
+          )
+        );
+        return responsePromise;
+      }
+    );
+    bulkActionsModel.sync([c1, c2]);
+    assert.strictEqual(getChangesStub.callCount, 1);
+    await waitUntilObserved(bulkActionsModel!.loading$, s => s === true);
+
+    responsePromise.resolve([
+      {...createChange(), _number: 1, subject: 'Subject 1'},
+      {...createChange(), _number: 2, subject: 'Subject 2'},
+    ] as ChangeInfo[]);
+    await waitUntilObserved(bulkActionsModel!.loading$, s => s === false);
+    assert.strictEqual(
+      bulkActionsModel.allChanges.get(1 as NumericChangeId)?.subject,
+      'Subject 1'
+    );
+    assert.strictEqual(
+      bulkActionsModel.allChanges.get(2 as NumericChangeId)?.subject,
+      'Subject 2'
+    );
+  });
+
+  test('sync respects last fetch', async () => {
+    const c1 = createChange();
+    c1._number = 1 as NumericChangeId;
+    const c2 = createChange();
+    c2._number = 2 as NumericChangeId;
+
+    const responsePromise1: MockPromise<ChangeInfo[]> = mockPromise();
+    let promise = responsePromise1;
+    const getChangesStub = stubRestApi('getChanges').callsFake(
+      (changesPerPage, query, offset, options) => {
+        assert.isUndefined(changesPerPage);
+        assert.strictEqual(query, 'change:1 OR change:2');
+        assert.isUndefined(offset);
+        assert.strictEqual(
+          options,
+          listChangesOptionsToHex(
+            ListChangesOption.CHANGE_ACTIONS,
+            ListChangesOption.CURRENT_ACTIONS,
+            ListChangesOption.CURRENT_REVISION,
+            ListChangesOption.DETAILED_LABELS
+          )
+        );
+        return promise;
+      }
+    );
+    bulkActionsModel.sync([c1, c2]);
+    assert.strictEqual(getChangesStub.callCount, 1);
+    await waitUntilObserved(bulkActionsModel!.loading$, s => s === true);
+    const responsePromise2: MockPromise<ChangeInfo[]> = mockPromise();
+
+    promise = responsePromise2;
+    bulkActionsModel.sync([c1, c2]);
+    assert.strictEqual(getChangesStub.callCount, 2);
+
+    responsePromise2.resolve([
+      {...createChange(), _number: 1, subject: 'Subject 1'},
+      {...createChange(), _number: 2, subject: 'Subject 2'},
+    ] as ChangeInfo[]);
+
+    await waitUntilObserved(bulkActionsModel!.loading$, s => s === false);
+    assert.strictEqual(
+      bulkActionsModel.allChanges.get(1 as NumericChangeId)?.subject,
+      'Subject 1'
+    );
+    assert.strictEqual(
+      bulkActionsModel.allChanges.get(2 as NumericChangeId)?.subject,
+      'Subject 2'
+    );
+
+    // Resolve the old promise.
+    responsePromise1.resolve([
+      {...createChange(), _number: 1, subject: 'Subject 1-old'},
+      {...createChange(), _number: 2, subject: 'Subject 2-old'},
+    ] as ChangeInfo[]);
+    await flush();
+
+    // No change should happen.
+    assert.strictEqual(
+      bulkActionsModel.allChanges.get(1 as NumericChangeId)?.subject,
+      'Subject 1'
+    );
+    assert.strictEqual(
+      bulkActionsModel.allChanges.get(2 as NumericChangeId)?.subject,
+      'Subject 2'
+    );
   });
 });

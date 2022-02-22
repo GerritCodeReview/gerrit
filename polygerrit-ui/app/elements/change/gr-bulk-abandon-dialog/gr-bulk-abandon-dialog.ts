f9227cec8b5c696f830b899d62e581e32b76f40d
Dhruv Srivastava <dhruvsri@google.com>
2022-02-22 15:12:34 +0100

    Add bulk abandon dialog
    Change-Id: I4164d6d963ed978f87da53b49224f97624e82026

---
+++ polygerrit-ui/app/elements/change/gr-bulk-abandon-dialog/gr-bulk-abandon-dialog.ts
@@ -0,0 +1,68 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {customElement, state} from 'lit/decorators';
+import {LitElement, html, css} from 'lit';
+import {resolve} from '../../../models/dependency';
+import {bulkActionsModelToken} from '../../../models/bulk-actions/bulk-actions-model';
+import {ChangeInfoId} from '../../../api/rest-api';
+import {fireEvent} from '../../../utils/event-util';
+
+@customElement('gr-bulk-abandon-dialog')
+export class GrBulkAbandonDialog extends LitElement {
+  private readonly getBulkActionsModel = resolve(this, bulkActionsModelToken);
+
+  @state() changeIds: ChangeInfoId[] = [];
+
+  static override get styles() {
+    return [
+      css`
+        :host {
+          display: block;
+        }
+        section {
+          padding: var(--spacing-m);
+        }
+      `,
+    ];
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.getBulkActionsModel().selectedChangeIds$.subscribe(
+      selectedChangeIds => (this.changeIds = selectedChangeIds)
+    );
+  }
+
+  override render() {
+    return html`
+      <section>
+        <h3 class="heading-3">${this.changeIds.length} changes to abandon</h3>
+      </section>
+      <section>
+        ${this.changeIds.map(
+          changeId => html` <div>Change: ${changeId}</div> `
+        )}
+      </section>
+      <section>
+        <gr-button @click=${() => fireEvent(this, 'cancel')}>
+          Cancel
+        </gr-button>
+        <gr-button @click=${() => this.handleConfirm()}> Confirm </gr-button>
+      </section>
+    `;
+  }
+
+  private handleConfirm() {
+    // this.getBulkActionsModel().abandonChanges();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-bulk-abandon-dialog': GrBulkAbandonDialog;
+  }
+}

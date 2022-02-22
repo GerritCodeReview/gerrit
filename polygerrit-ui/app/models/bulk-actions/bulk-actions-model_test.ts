39c93e7b414294dbdaea8a549e40201ba40428ca
Chris Poucet <poucet@google.com>
2022-02-22 15:12:20 +0100

    Keep the bulk-actions model synchronized with the full set of changes
    This will be required for follow-up work to fetch the actual changes.
    I've also changed the data-structure to a map for faster lookup.
    
    Change-Id: I9e4ed61e8263b68038827eecfaf4c8789bc6b14f

--- polygerrit-ui/app/models/bulk-actions/bulk-actions-model_test.ts
+++ polygerrit-ui/app/models/bulk-actions/bulk-actions-model_test.ts
@@ -12,6 +12,29 @@ import '../../test/common-test-setup-karma';
 import {waitUntilObserved} from '../../test/test-utils';
 
 suite('bulk actions model test', () => {
+  test('add changes before update', () => {
+    const c1 = createChange();
+    c1.id = '1' as ChangeInfoId;
+    const c2 = createChange();
+    c2.id = '2' as ChangeInfoId;
+
+    const bulkActionsModel = new BulkActionsModel(
+      getAppContext().restApiService
+    );
+
+    assert.deepEqual(bulkActionsModel.getState().selectedChangeIds, []);
+
+    bulkActionsModel.addSelectedChangeId(c1.id);
+    assert.deepEqual(bulkActionsModel.getState().selectedChangeIds, []);
+    bulkActionsModel.sync([c1, c2]);
+
+    bulkActionsModel.addSelectedChangeId(c2.id);
+    assert.deepEqual(bulkActionsModel.getState().selectedChangeIds, [c2.id]);
+
+    bulkActionsModel.removeSelectedChangeId(c2.id);
+    assert.deepEqual(bulkActionsModel.getState().selectedChangeIds, []);
+  });
+
   test('add and remove selected changes', () => {
     const c1 = createChange();
     c1.id = '1' as ChangeInfoId;
@@ -21,6 +44,7 @@ suite('bulk actions model test', () => {
     const bulkActionsModel = new BulkActionsModel(
       getAppContext().restApiService
     );
+    bulkActionsModel.sync([c1, c2]);
 
     assert.deepEqual(bulkActionsModel.getState().selectedChangeIds, []);
 
@@ -41,21 +65,26 @@ suite('bulk actions model test', () => {
   });
 
   test('stale changes are removed from the model', async () => {
+    const c1 = createChange();
+    c1.id = '1' as ChangeInfoId;
+    const c2 = createChange();
+    c2.id = '2' as ChangeInfoId;
     const bulkActionsModel = new BulkActionsModel(
       getAppContext().restApiService
     );
+    bulkActionsModel.sync([c1, c2]);
 
-    bulkActionsModel.addSelectedChangeId('0' as ChangeInfoId);
     bulkActionsModel.addSelectedChangeId('1' as ChangeInfoId);
+    bulkActionsModel.addSelectedChangeId('2' as ChangeInfoId);
 
     let selectedChangeIds = await waitUntilObserved(
       bulkActionsModel!.selectedChangeIds$,
       s => s.length === 2
     );
 
-    assert.deepEqual(selectedChangeIds, ['0', '1']);
+    assert.deepEqual(selectedChangeIds, ['1', '2']);
 
-    bulkActionsModel.sync(['1' as ChangeInfoId]);
+    bulkActionsModel.sync([c1]);
     selectedChangeIds = await waitUntilObserved(
       bulkActionsModel!.selectedChangeIds$,
       s => s.length === 1

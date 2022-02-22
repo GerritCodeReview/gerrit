a21b4ca6ebcff33049eab7d930428741602bea69
Chris Poucet <poucet@google.com>
2022-02-22 18:00:25 +0100

    Fetch changes when the bulk-actions-model is synced
    Release-Notes: skip
    Change-Id: Ib1458a7fe14bcb560decf1ea48c4e13afe0ec34b

--- polygerrit-ui/app/models/bulk-actions/bulk-actions-model.ts
+++ polygerrit-ui/app/models/bulk-actions/bulk-actions-model.ts
@@ -4,22 +4,28 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {ChangeInfo, ChangeInfoId} from '../../api/rest-api';
+import {ChangeInfo, NumericChangeId} from '../../api/rest-api';
 import {Model} from '../model';
 import {Finalizable} from '../../services/registry';
 import {RestApiService} from '../../services/gr-rest-api/gr-rest-api';
 import {define} from '../dependency';
 import {select} from '../../utils/observable-util';
+import {
+  listChangesOptionsToHex,
+  ListChangesOption,
+} from '../../utils/change-util';
 
 export const bulkActionsModelToken =
   define<BulkActionsModel>('bulk-actions-model');
 
 export interface BulkActionsState {
-  selectedChangeIds: ChangeInfoId[];
+  loading: boolean;
+  selectedChangeNums: NumericChangeId[];
 }
 
 const initialState: BulkActionsState = {
-  selectedChangeIds: [],
+  loading: false,
+  selectedChangeNums: [],
 };
 
 export class BulkActionsModel
@@ -27,50 +33,76 @@ export class BulkActionsModel
   implements Finalizable
 {
   // A map of all the changes in a section that can be bulk-acted upon.
-  private allChanges: Map<ChangeInfoId, ChangeInfo> = new Map();
+  // Private but used in tests.
+  allChanges: Map<NumericChangeId, ChangeInfo> = new Map();
 
-  constructor(_restApiService: RestApiService) {
+  constructor(private readonly restApiService: RestApiService) {
     super(initialState);
   }
 
-  public readonly selectedChangeIds$ = select(
+  public readonly selectedChangeNums$ = select(
     this.state$,
-    bulkActionsState => bulkActionsState.selectedChangeIds
+    bulkActionsState => bulkActionsState.selectedChangeNums
   );
 
-  addSelectedChangeId(changeId: ChangeInfoId) {
-    if (!this.allChanges.has(changeId)) {
+  public readonly loading$ = select(
+    this.state$,
+    bulkActionsState => bulkActionsState.loading
+  );
+
+  addSelectedChangeNum(changeNum: NumericChangeId) {
+    if (!this.allChanges.has(changeNum)) {
       throw new Error(
-        `Trying to add change ${changeId} that is not part of bulk-actions model`
+        `Trying to add change ${changeNum} that is not part of bulk-actions model`
       );
     }
     const current = this.subject$.getValue();
-    const selectedChangeIds = [...current.selectedChangeIds];
-    selectedChangeIds.push(changeId);
-    this.setState({...current, selectedChangeIds});
+    const selectedChangeNums = [...current.selectedChangeNums];
+    selectedChangeNums.push(changeNum);
+    this.setState({...current, selectedChangeNums});
   }
 
-  removeSelectedChangeId(changeId: ChangeInfoId) {
-    if (!this.allChanges.has(changeId)) {
+  removeSelectedChangeNum(changeNum: NumericChangeId) {
+    if (!this.allChanges.has(changeNum)) {
       throw new Error(
-        `Trying to remove change ${changeId} that is not part of bulk-actions model`
+        `Trying to remove change ${changeNum} that is not part of bulk-actions model`
       );
     }
     const current = this.subject$.getValue();
-    const selectedChangeIds = [...current.selectedChangeIds];
-    const index = selectedChangeIds.findIndex(item => item === changeId);
+    const selectedChangeNums = [...current.selectedChangeNums];
+    const index = selectedChangeNums.findIndex(item => item === changeNum);
     if (index === -1) return;
-    selectedChangeIds.splice(index, 1);
-    this.setState({...current, selectedChangeIds});
+    selectedChangeNums.splice(index, 1);
+    this.setState({...current, selectedChangeNums});
   }
 
-  sync(changes: ChangeInfo[]) {
-    this.allChanges = new Map(changes.map(c => [c.id, c]));
+  async sync(changes: ChangeInfo[]) {
+    const newChanges = new Map(changes.map(c => [c._number, c]));
+    this.allChanges = newChanges;
     const current = this.subject$.getValue();
-    const selectedChangeIds = current.selectedChangeIds.filter(changeId =>
-      this.allChanges.has(changeId)
+    const selectedChangeNums = current.selectedChangeNums.filter(changeNum =>
+      newChanges.has(changeNum)
     );
-    this.setState({...current, selectedChangeIds});
+    this.setState({...current, loading: true, selectedChangeNums});
+
+    const query = changes.map(c => `change:${c._number}`).join(' OR ');
+    const changeDetails = await this.restApiService.getChanges(
+      undefined,
+      query,
+      undefined,
+      listChangesOptionsToHex(
+        ListChangesOption.CHANGE_ACTIONS,
+        ListChangesOption.CURRENT_ACTIONS,
+        ListChangesOption.CURRENT_REVISION,
+        ListChangesOption.DETAILED_LABELS
+      )
+    );
+    // Return early if sync has been called again since starting the load.
+    if (newChanges !== this.allChanges) return;
+    for (const change of changeDetails ?? []) {
+      this.allChanges.set(change._number, change);
+    }
+    this.setState({...this.subject$.getValue(), loading: false});
   }
 
   /** Required for testing */

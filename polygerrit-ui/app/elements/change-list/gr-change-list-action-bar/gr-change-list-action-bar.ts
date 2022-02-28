dc604fd1d83bed5d0a4f8a2a83b23c400d1f69b6
Frank Borden <frankborden@google.com>
2022-02-28 18:21:43 +0100

    Extract action-bar component from dash section
    Also add placeholders for all actions.
    
    before&after: https://i.imgur.com/0WTRxP5.png
    
    Release-notes: skip
    Change-Id: I17c0935932a6ae3b92340e64bacd8dc794645994

---
+++ polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar.ts
@@ -0,0 +1,56 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {css, html, LitElement} from 'lit';
+import {customElement} from 'lit/decorators';
+import '../../shared/gr-button/gr-button';
+
+/**
+ * An action bar for the top of a <gr-change-list-section> element. Assumes it
+ * will be used inside a <tr> element.
+ */
+@customElement('gr-change-list-action-bar')
+export class GrChangeListActionBar extends LitElement {
+  static override get styles() {
+    return css`
+      :host {
+        display: contents;
+      }
+      .container {
+        display: flex;
+        justify-content: flex-end;
+      }
+    `;
+  }
+
+  override render() {
+    return html`
+      <!--
+        500 chosen to be more than the actual number of columns but less than
+        1000 where the browser apparently decides it is an error and reverts
+        back to colspan="1"
+      -->
+      <td colspan="500">
+        <div class="container">
+          <gr-button>submit</gr-button>
+          <gr-button>create topic</gr-button>
+          <gr-button>add to topic</gr-button>
+          <gr-button>add changes</gr-button>
+          <gr-button>remove changes</gr-button>
+          <gr-button>abandon</gr-button>
+          <gr-button>add reviewer/cc</gr-button>
+          <gr-button>vote</gr-button>
+        </div>
+      </td>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-change-list-action-bar': GrChangeListActionBar;
+  }
+}

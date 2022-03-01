c47f3957ffa6faeb43350ee371abff55ad9d53c8
Frank Borden <frankborden@google.com>
2022-03-01 15:21:39 +0100

    show selection count and controls in action bar
    TODOs added for cases where BulkActionsModel needs to expose more data
    and operations. The number of total changes in the section is needed to
    determine the checkbox state (checked vs indeterminate), and clear()
    behavior is needed for removing all changes. Both will be added in the
    next change.
    
    https://i.imgur.com/wFJkSvy.png
    
    Release-Notes: skip
    Change-Id: Ia104af1f4d19b924b3f9cffe67fbddbf104e70a4

--- polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar.ts
@@ -4,8 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {css, html, LitElement} from 'lit';
-import {customElement} from 'lit/decorators';
+import {css, html, LitElement, nothing} from 'lit';
+import {customElement, state} from 'lit/decorators';
+import {bulkActionsModelToken} from '../../../models/bulk-actions/bulk-actions-model';
+import {resolve} from '../../../models/dependency';
+import {pluralize} from '../../../utils/string-util';
+import {subscribe} from '../../lit/subscription-controller';
 import '../../shared/gr-button/gr-button';
 
 interface ActionButton {
@@ -26,17 +30,55 @@ export class GrChangeListActionBar extends LitElement {
       }
       .container {
         display: flex;
-        justify-content: flex-end;
+        justify-content: space-between;
+        align-items: center;
+      }
+      /*
+       * checkbox styles match checkboxes in <gr-change-list-item> rows to
+       * vertically align with them.
+       */
+      input {
+        background-color: var(--background-color-primary);
+        border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        box-sizing: border-box;
+        color: var(--primary-text-color);
+        margin: 0px;
+        padding: var(--spacing-s);
+        vertical-align: middle;
       }
     `;
   }
 
-  private actionButtons: ActionButton[] = [
+  @state()
+  private numSelected = 0;
+
+  private readonly getBulkActionsModel = resolve(this, bulkActionsModelToken);
+
+  private readonly actionButtons: ActionButton[] = [
     {name: 'abandon', onClick: () => this.onAbandonClicked()},
   ];
 
+  override connectedCallback(): void {
+    super.connectedCallback();
+    subscribe(
+      this,
+      this.getBulkActionsModel().selectedChangeNums$,
+      selectedChangeNums => (this.numSelected = selectedChangeNums.length)
+    );
+  }
+
   override render() {
+    const numSelectedLabel = `${pluralize(
+      this.numSelected,
+      'change'
+    )} selected`;
     return html`
+      <!-- Empty cell added for spacing just like gr-change-list-item rows -->
+      <td></td>
+      <!-- TODO: Make checkbox reflect the overall selection of the section -->
+      <!-- TODO: Clear behavior -->
+      <td><input type="checkbox" /></td>
       <!--
         500 chosen to be more than the actual number of columns but less than
         1000 where the browser apparently decides it is an error and reverts
@@ -44,10 +86,17 @@ export class GrChangeListActionBar extends LitElement {
       -->
       <td colspan="500">
         <div class="container">
-          ${this.actionButtons.map(
-            ({name, onClick}) =>
-              html`<gr-button @click=${onClick}>${name}</gr-button>`
-          )}
+          <div class="selectionInfo">
+            ${this.numSelected
+              ? html`<span>${numSelectedLabel}</span>`
+              : nothing}
+          </div>
+          <div class="actionButtons">
+            ${this.actionButtons.map(
+              ({name, onClick}) =>
+                html`<gr-button flatten @click=${onClick}>${name}</gr-button>`
+            )}
+          </div>
         </div>
       </td>
     `;
--- polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar_test.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar_test.ts
@@ -4,36 +4,113 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import {fixture, html} from '@open-wc/testing-helpers';
+import {
+  BulkActionsModel,
+  bulkActionsModelToken,
+} from '../../../models/bulk-actions/bulk-actions-model';
+import {wrapInProvider} from '../../../models/di-provider-element';
+import {getAppContext} from '../../../services/app-context';
 import '../../../test/common-test-setup-karma';
-import {queryAndAssert} from '../../../test/test-utils';
+import {createChange} from '../../../test/test-data-generators';
+import {
+  query,
+  queryAndAssert,
+  waitUntilObserved,
+} from '../../../test/test-utils';
+import {ChangeInfo, NumericChangeId} from '../../../types/common';
 import {GrButton} from '../../shared/gr-button/gr-button';
 import './gr-change-list-action-bar';
 import type {GrChangeListActionBar} from './gr-change-list-action-bar';
 
+const change1 = {...createChange(), _number: 1 as NumericChangeId};
+const change2 = {...createChange(), _number: 2 as NumericChangeId};
+
 suite('gr-change-list-action-bar tests', () => {
   let element: GrChangeListActionBar;
-  setup(async () => {
-    element = await fixture(
-      html`<gr-change-list-action-bar></gr-change-list-action-bar>`
+  let model: BulkActionsModel;
+
+  async function selectChange(change: ChangeInfo) {
+    model.addSelectedChangeNum(change._number);
+    await waitUntilObserved(model.selectedChangeNums$, selectedChangeNums =>
+      selectedChangeNums.includes(change._number)
     );
+    await element.updateComplete;
+  }
+
+  setup(async () => {
+    model = new BulkActionsModel(getAppContext().restApiService);
+    model.sync([change1, change2]);
+
+    element = (
+      await fixture(
+        wrapInProvider(
+          html`<gr-change-list-action-bar></gr-change-list-action-bar>`,
+          bulkActionsModelToken,
+          model
+        )
+      )
+    ).querySelector('gr-change-list-action-bar')!;
     await element.updateComplete;
   });
 
-  test('renders all buttons', () => {
+  test('renders all buttons', async () => {
+    await selectChange(change1);
+
     expect(element).shadowDom.to.equal(/* HTML */ `
+      <td></td>
+      <td><input type="checkbox" /></td>
       <td>
         <div class="container">
-          <gr-button aria-disabled="false" role="button" tabindex="0"
-            >abandon</gr-button
-          >
+          <div class="selectionInfo">
+            <span>1 change selected</span>
+          </div>
+          <div class="actionButtons">
+            <gr-button
+              flatten=""
+              aria-disabled="false"
+              role="button"
+              tabindex="0"
+              >abandon</gr-button
+            >
+          </div>
         </div>
       </td>
     `);
   });
 
-  test('abandoned clicked', () => {
+  test('label reflects number of selected changes', async () => {
+    // zero case
+    let numSelectedLabel = query<HTMLSpanElement>(
+      element,
+      '.selectionInfo span'
+    );
+    assert.isUndefined(numSelectedLabel);
+
+    // single case
+    await selectChange(change1);
+    numSelectedLabel = queryAndAssert<HTMLSpanElement>(
+      element,
+      '.selectionInfo span'
+    );
+    assert.equal(numSelectedLabel.innerText, '1 change selected');
+
+    // plural case
+    await selectChange(change2);
+
+    numSelectedLabel = queryAndAssert<HTMLSpanElement>(
+      element,
+      '.selectionInfo span'
+    );
+    assert.equal(numSelectedLabel.innerText, '2 changes selected');
+  });
+
+  test('abandoned clicked', async () => {
+    await selectChange(change1);
     const consoleInfoSpy = sinon.spy(console, 'info');
-    const button = queryAndAssert<GrButton>(element, 'gr-button');
+    const button = queryAndAssert<GrButton>(
+      element,
+      '.actionButtons gr-button'
+    );
 
     button.click();
 

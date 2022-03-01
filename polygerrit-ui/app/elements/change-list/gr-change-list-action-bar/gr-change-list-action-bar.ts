090ecab290f2196744b4e29c6211a8beae71c234
Frank Borden <frankborden@google.com>
2022-03-01 13:55:20 +0100

    show selection count and controls in action bar
    Release-Notes: skip
    Change-Id: I947f20bf70dca9a6a5fa76e9dcf8c983004fd71e

--- polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar.ts
@@ -4,8 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {css, html, LitElement} from 'lit';
-import {customElement} from 'lit/decorators';
+import {css, html, LitElement, nothing} from 'lit';
+import {customElement, state} from 'lit/decorators';
+import {bulkActionsModelToken} from '../../../models/bulk-actions/bulk-actions-model';
+import {resolve} from '../../../models/dependency';
+import {pluralize} from '../../../utils/string-util';
+import {subscribe} from '../../lit/subscription-controller';
 import '../../shared/gr-button/gr-button';
 
 /**
@@ -19,14 +23,32 @@ export class GrChangeListActionBar extends LitElement {
       :host {
         display: contents;
       }
-      .container {
-        display: flex;
+      .actionButtons {
+        display: inline-flex;
         justify-content: flex-end;
       }
     `;
   }
 
+  @state()
+  private numSelected = 0;
+
+  private readonly getBulkActionsModel = resolve(this, bulkActionsModelToken);
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+    subscribe(
+      this,
+      this.getBulkActionsModel().selectedChangeNums$,
+      selectedChangeNums => (this.numSelected = selectedChangeNums.length)
+    );
+  }
+
   override render() {
+    const numSelectedLabel = `${pluralize(
+      this.numSelected,
+      'change'
+    )} selected`;
     return html`
       <!--
         500 chosen to be more than the actual number of columns but less than
@@ -34,7 +56,8 @@ export class GrChangeListActionBar extends LitElement {
         back to colspan="1"
       -->
       <td colspan="500">
-        <div class="container">
+        ${this.numSelected ? html`<span>${numSelectedLabel}</span>` : nothing}
+        <div class="actionButtons">
           <gr-button>submit</gr-button>
           <gr-button>create topic</gr-button>
           <gr-button>add to topic</gr-button>

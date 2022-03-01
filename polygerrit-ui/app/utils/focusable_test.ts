df6e1aaa30f7dcc72525951eb15988df8e584bca
Chris Poucet <poucet@google.com>
2022-03-02 00:06:33 +0100

    Add a utility function to get focusable elements in order
    Change-Id: I3dfd9be2db886a5124c2d45d5c7cf27fcf08909b

---
+++ polygerrit-ui/app/utils/focusable_test.ts
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import '../test/common-test-setup-karma';
+import {getFocusableElements} from './focusable';
+import {html, render} from 'lit';
+import {fixture} from '@open-wc/testing-helpers';
+
+
+
+suite('focusable', () => {
+  let container: HTMLElement;
+  setup(async () => {
+    container = await fixture<HTMLDivElement>(html`<div></div>`);
+    const shadow = container.attachShadow({mode: 'open'});
+    render(html`
+      <a href="" id="first"></a>
+      <slot></slot>
+      <button id="third"></button>
+      <button class="not" style="display: none"></button>
+      <button class="not" style="visibility: hidden"></button>
+      <span id="fourth" tabindex="0"></span>
+      <textarea id="fifth"></textarea>
+    `, shadow);
+    const slottedContent = document.createElement('div');
+    const slottedShadow = slottedContent.attachShadow({mode: 'open'});
+    render(html`
+      <textarea id="second"></textarea>
+    `, slottedShadow)
+    const slot: HTMLSlotElement | null = shadow.querySelector('slot');
+    //
+    (slot! as any).assign(slottedContent);
+  });
+
+  test('Finds all focusables in-order', () => {
+    expect(container).shadowDom.to.equal(/* HTML */``);
+    const results = [...getFocusableElements(container)];
+    expect(results.map(e => e.id)).to.have.ordered.members(
+      ["first", "second", "third", "fourth", "fifth"]
+    );
+  })
+});

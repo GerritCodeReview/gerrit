0d517365870183e0784c7b80afcdd731cf12fc5b
Dhruv Srivastava <dhruvsri@google.com>
2022-03-08 13:44:26 +0100

    Split getChanges methods based on multiple queries in RestApi
    Instead of having one method which looks at the query parameter
    and then decides the return split the methods into two separate
    methods.
    This allows us to better stub the test methods.
    
    Before this change it was possible to stub the getChanges method
    to return both ChangeInfo[] and ChangeInfo[][] which would lead to
    subtle bugs tricky to catch.
    Promise.resolve([createChange()]);
    Promise.resolve([[createChange()]]);
    Compiler wouldn't catch the extra brackets here as both return types
    were initially supported.
    
    This also allows for adding a custom stub for getChanges without
    stubbing the getChangesForMultipleQueries and adding logic for it.
    
    Release-Notes: skip
    Change-Id: I63e8e37aaafd8e2d5c59c2928220fca24c47200f

--- polygerrit-ui/app/test/mocks/gr-rest-api_mock.ts
+++ polygerrit-ui/app/test/mocks/gr-rest-api_mock.ts
@@ -256,6 +256,9 @@ export const grRestApiMock: RestApiService = {
   getChanges() {
     return Promise.resolve([]);
   },
+  getChangesForMultipleQueries() {
+    return Promise.resolve([]);
+  },
   getChangesSubmittedTogether(): Promise<SubmittedTogetherInfo | undefined> {
     return Promise.resolve(createSubmittedTogetherInfo());
   },

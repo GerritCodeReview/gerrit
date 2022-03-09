4c31cbe2249c43e74db4c548b93a483c96cfef68
Dhruv Srivastava <dhruvsri@google.com>
2022-03-09 18:00:55 +0100

    Enable bulk abandon button if change is abandoned
    In case user selects an already abandoned change, then let the button
    remain enabled and resolve the promise as successful immediately.
    
    Release-Notes: skip
    Change-Id: I8434f0c265b6513683240df88dcb94be273fdb40

--- polygerrit-ui/app/elements/change-list/gr-change-list-bulk-abandon-flow/gr-change-list-bulk-abandon-flow_test.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-bulk-abandon-flow/gr-change-list-bulk-abandon-flow_test.ts
@@ -5,7 +5,13 @@
  */
 
 import {createChange} from '../../../test/test-data-generators';
-import {NumericChangeId, ChangeInfo} from '../../../api/rest-api';
+import {
+  NumericChangeId,
+  ChangeInfo,
+  ChangeStatus,
+  HttpMethod,
+  PatchSetNum,
+} from '../../../api/rest-api';
 import {GrChangeListBulkAbandonFlow} from './gr-change-list-bulk-abandon-flow';
 import '../../../test/common-test-setup-karma';
 import {
@@ -28,6 +34,8 @@ import {
 import {GrButton} from '../../shared/gr-button/gr-button';
 import {tap} from '@polymer/iron-test-helpers/mock-interactions';
 import {ProgressStatus} from '../../../constants/constants';
+import {RequestPayload} from '../../../types/common';
+import {ErrorCallback} from '../../../api/rest';
 
 const change1: ChangeInfo = {...createChange(), _number: 1 as NumericChangeId};
 const change2: ChangeInfo = {...createChange(), _number: 2 as NumericChangeId};
@@ -87,6 +95,32 @@ suite('gr-change-list-bulk-abandon-flow tests', () => {
     await element.updateComplete;
 
     assert.isTrue(queryAndAssert<GrButton>(element, '#abandon').disabled);
+  });
+
+  test('abandon button is enabled if change is already abandoned', async () => {
+    const changes: ChangeInfo[] = [
+      {...change1, actions: {}, status: ChangeStatus.ABANDONED},
+    ];
+    getChangesStub.returns(changes);
+    model.sync(changes);
+    await waitUntilObserved(
+      model.loadingState$,
+      state => state === LoadingState.LOADED
+    );
+    await selectChange(change1);
+    await element.updateComplete;
+    await flush();
+    assert.isFalse(queryAndAssert<GrButton>(element, '#abandon').disabled);
+
+    tap(queryAndAssert(query(element, 'gr-dialog'), '#confirm'));
+
+    await waitUntil(
+      () =>
+        queryAndAssert<HTMLTableDataCellElement>(
+          element,
+          '#status'
+        ).innerText.trim() === `Status: ${ProgressStatus.SUCCESSFUL}`
+    );
   });
 
   test('progress updates as request is resolved', async () => {
@@ -156,8 +190,16 @@ suite('gr-change-list-bulk-abandon-flow tests', () => {
       `Status: ${ProgressStatus.NOT_STARTED}`
     );
 
-    const executeChangeAction = mockPromise<Response>();
-    stubRestApi('executeChangeAction').returns(executeChangeAction);
+    stubRestApi('executeChangeAction').callsFake(
+      (
+        _changeNum: NumericChangeId,
+        _method: HttpMethod | undefined,
+        _endpoint: string,
+        _patchNum?: PatchSetNum,
+        _payload?: RequestPayload,
+        _errFn?: ErrorCallback
+      ) => Promise.reject(new Error(''))
+    );
 
     tap(queryAndAssert(query(element, 'gr-dialog'), '#confirm'));
     await element.updateComplete;
@@ -170,7 +212,6 @@ suite('gr-change-list-bulk-abandon-flow tests', () => {
       `Status: ${ProgressStatus.RUNNING}`
     );
 
-    executeChangeAction.resolve({...new Response(), status: 500});
     await waitUntil(
       () => element.progress.get(1 as NumericChangeId) === ProgressStatus.FAILED
     );

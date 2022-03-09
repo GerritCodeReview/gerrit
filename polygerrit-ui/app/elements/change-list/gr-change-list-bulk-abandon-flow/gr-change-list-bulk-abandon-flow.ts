d9d1e9433bd555d0e503e192987d27a368acee15
Dhruv Srivastava <dhruvsri@google.com>
2022-03-09 18:00:55 +0100

    Enable bulk abandon button if change is abandoned
    In case user selects an already abandoned change, then let the button
    remain enabled and resolve the promise as successful immediately.
    
    Release-Notes: skip
    Change-Id: I8434f0c265b6513683240df88dcb94be273fdb40

--- polygerrit-ui/app/elements/change-list/gr-change-list-bulk-abandon-flow/gr-change-list-bulk-abandon-flow.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-bulk-abandon-flow/gr-change-list-bulk-abandon-flow.ts
@@ -8,7 +8,7 @@ import {customElement, state, query} from 'lit/decorators';
 import {LitElement, html, css} from 'lit';
 import {resolve} from '../../../models/dependency';
 import {bulkActionsModelToken} from '../../../models/bulk-actions/bulk-actions-model';
-import {NumericChangeId, ChangeInfo} from '../../../api/rest-api';
+import {NumericChangeId, ChangeInfo, ChangeStatus} from '../../../api/rest-api';
 import {ProgressStatus} from '../../change/gr-confirm-cherrypick-dialog/gr-confirm-cherrypick-dialog';
 import {subscribe} from '../../lit/subscription-controller';
 import {GrOverlay} from '../../shared/gr-overlay/gr-overlay';
@@ -95,8 +95,7 @@ export class GrChangeListBulkAbandonFlow extends LitElement {
   private isEnabled() {
     return this.selectedChanges.every(
       change =>
-        // TODO: Add handling for when change is already abandoned
-        !!change.actions?.abandon
+        !!change.actions?.abandon || change.status === ChangeStatus.ABANDONED
     );
   }
 

5b587515839002b6243b78fc59ccc23ffc7ef4cb
Dhruv Srivastava <dhruvsri@google.com>
2022-03-10 12:00:44 +0100

    Reload page if all requests succeed
    Once all requests are complete and the user closes the dialog, we
    reload the page.
    Closing the dialog while requests are in progress is disabled.
    
    Release-Notes: skip
    Change-Id: I4364529d66179a1a2104ce943f0543dcb3db474f

--- polygerrit-ui/app/elements/change-list/gr-change-list-bulk-abandon-flow/gr-change-list-bulk-abandon-flow.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-bulk-abandon-flow/gr-change-list-bulk-abandon-flow.ts
@@ -12,6 +12,7 @@ import {NumericChangeId, ChangeInfo, ChangeStatus} from '../../../api/rest-api';
 import {subscribe} from '../../lit/subscription-controller';
 import {GrOverlay} from '../../shared/gr-overlay/gr-overlay';
 import {ProgressStatus} from '../../../constants/constants';
+import {fireAlert, fireReload} from '../../../utils/event-util';
 
 @customElement('gr-change-list-bulk-abandon-flow')
 export class GrChangeListBulkAbandonFlow extends LitElement {
@@ -54,7 +55,8 @@ export class GrChangeListBulkAbandonFlow extends LitElement {
       <gr-overlay id="actionOverlay" with-backdrop="">
         <gr-dialog
           @confirm=${() => this.handleConfirm()}
-          @cancel=${() => this.actionOverlay.close()}
+          @cancel=${() => this.handleClose()}
+          .cancelLabel=${'Close'}
         >
           <div slot="header">
             ${this.selectedChanges.length} changes to abandon
@@ -119,6 +121,19 @@ export class GrChangeListBulkAbandonFlow extends LitElement {
         this.requestUpdate();
       });
     }
+  }
+
+  private handleClose() {
+    // If all requests succeeded then trigger a reload
+
+    const allSuccess = Array.from(this.progress.values()).every(
+      status => status === ProgressStatus.SUCCESSFUL
+    );
+    if (allSuccess) {
+      fireAlert(this, 'Reloading page..');
+      fireReload(this, true);
+    }
+    this.actionOverlay.close();
   }
 }
 

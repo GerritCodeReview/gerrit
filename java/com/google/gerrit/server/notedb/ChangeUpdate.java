ecf033e5ef52eab3b0a8d4d6aef9d2799a467020
Youssef Elghareeb <ghareeb@google.com>
2022-03-14 19:15:41 +0100

    ChangeUpdate: fix updating change meta ref when the update is no/op
    There was a bug in RemoveAttentionSetOp in the sense that it always
    returned true signalling to callers to perform an update regardless of
    whether the attention set will get updated or not.
    
    At the time we build the change meta ref commit, we append all updates
    to the commit message. The method ChangeUpdate#isEmpty checks if there
    are no updates and bails out at [1]. Sometimes the variable
    plannedAttentionSetUpdates contained some updates, but this variable is
    processed in [2] while writing the commit message and in some cases no
    updates are written after all. This triggered the creation of a new
    commit in the change meta ref which is redundant, hence fixing in this
    change and adapting the test in PostReviewIT to ensure the change meta
    ref is not updated in this case.
    
    We fix this by factoring out plannedAttentionSetUpdates from #isEmpty
    and explicitly checking that [2] did not include any attention set
    updates. We do that because #isEmpty is also called from [3] in
    AbstractChangeUpdate#apply (at the beginning of the processing) and it
    bails out quickly if plannedAttentionSetUpdates is null.
    
    [1]
    https://gerrit.googlesource.com/gerrit/+/23678de2cdc56b676fcd80ea5d76b10af8c77304/java/com/google/gerrit/server/notedb/ChangeUpdate.java#1085
    
    [2]
    https://gerrit.googlesource.com/gerrit/+/23678de2cdc56b676fcd80ea5d76b10af8c77304/java/com/google/gerrit/server/notedb/ChangeUpdate.java#984
    
    [3]
    https://gerrit.googlesource.com/gerrit/+/23678de2cdc56b676fcd80ea5d76b10af8c77304/java/com/google/gerrit/server/notedb/AbstractChangeUpdate.java#220
    
    Release-Notes: skip
    Google-Bug-Id: b/221124220
    Change-Id: I4086c614ff670e3bc43f772c3d004c65477db4eb

--- java/com/google/gerrit/server/notedb/ChangeUpdate.java
+++ java/com/google/gerrit/server/notedb/ChangeUpdate.java
@@ -820,7 +820,10 @@ public class ChangeUpdate extends AbstractChangeUpdate {
       }
     }
 
-    updateAttentionSet(msg);
+    boolean hasAttentionSeUpdates = updateAttentionSet(msg);
+    if (isEmptyWithoutAttentionSet() && !hasAttentionSeUpdates) {
+      return NO_OP_UPDATE;
+    }
 
     CommitBuilder cb = new CommitBuilder();
     cb.setMessage(msg.toString());
@@ -954,8 +957,11 @@ public class ChangeUpdate extends AbstractChangeUpdate {
    * <p>Changing the behaviour of this method might affect the way a ChangeUpdate is considered to
    * be an "Attention Set Change Only". Make sure the {@link #isAttentionSetChangeOnly} logic is
    * amended as well if needed.
+   *
+   * @return True if one or more attention set updates are appended to the {@code msg}, and false
+   *     otherwise.
    */
-  private void updateAttentionSet(StringBuilder msg) {
+  private boolean updateAttentionSet(StringBuilder msg) {
     if (plannedAttentionSetUpdates == null) {
       plannedAttentionSetUpdates = new HashMap<>();
     }
@@ -980,6 +986,8 @@ public class ChangeUpdate extends AbstractChangeUpdate {
             .collect(ImmutableSet.toImmutableSet()));
 
     removeInactiveUsersFromAttentionSet(currentReviewers);
+
+    boolean hasUpdates = false;
 
     for (AttentionSetUpdate attentionSetUpdate : plannedAttentionSetUpdates.values()) {
       if (attentionSetUpdate.operation() == AttentionSetUpdate.Operation.ADD
@@ -1015,7 +1023,9 @@ public class ChangeUpdate extends AbstractChangeUpdate {
       }
 
       addFooter(msg, FOOTER_ATTENTION, noteUtil.attentionSetUpdateToJson(attentionSetUpdate));
+      hasUpdates = true;
     }
+    return hasUpdates;
   }
 
   private void removeInactiveUsersFromAttentionSet(Set<Account.Id> currentReviewers) {
@@ -1083,6 +1093,10 @@ public class ChangeUpdate extends AbstractChangeUpdate {
 
   @Override
   public boolean isEmpty() {
+    return isEmptyWithoutAttentionSet() && plannedAttentionSetUpdates == null;
+  }
+
+  private boolean isEmptyWithoutAttentionSet() {
     return commitSubject == null
         && approvals.isEmpty()
         && copiedApprovals.isEmpty()
@@ -1095,7 +1109,6 @@ public class ChangeUpdate extends AbstractChangeUpdate {
         && status == null
         && submissionId == null
         && submitRecords == null
-        && plannedAttentionSetUpdates == null
         && assignee == null
         && hashtags == null
         && topic == null

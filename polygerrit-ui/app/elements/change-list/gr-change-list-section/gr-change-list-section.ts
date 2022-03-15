cac18a81a1f70bd595704faa5b637bd705272ec0
Chris Poucet <poucet@google.com>
2022-03-15 10:33:24 +0100

    Don't make expensive bulk-actions query when disabled
    The sync method triggers an expensive backend query. We should not be
    making this request if bulk-actions is disabled.
    
    Release-Notes: skip
    Change-Id: I769645ba51a4953c9639d1de8872d46975399d1a

--- polygerrit-ui/app/elements/change-list/gr-change-list-section/gr-change-list-section.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-section/gr-change-list-section.ts
@@ -135,7 +135,9 @@ export class GrChangeListSection extends LitElement {
       // In case the list of changes is updated due to auto reloading, we want
       // to ensure the model removes any stale change that is not a part of the
       // new section changes.
-      this.bulkActionsModel.sync(this.changeSection.results);
+      if (this.flagsService.isEnabled(KnownExperimentId.BULK_ACTIONS)) {
+        this.bulkActionsModel.sync(this.changeSection.results);
+      }
     }
   }
 

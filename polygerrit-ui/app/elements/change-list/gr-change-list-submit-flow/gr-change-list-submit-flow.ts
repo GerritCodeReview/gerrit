1e980c4555fc8caf08174fac36497f03d3019117
Frank Borden <frankborden@google.com>
2022-03-17 11:57:39 +0100

    Introduce submit flow
    As a starting point, this is identical to the mark active flow and will
    be customized in a later change.
    
    Google-Bug-Id: b/216478679
    Release-Notes: skip
    Change-Id: Idd4818a81bb173e80842051270d2c2ed32507efd

---
+++ polygerrit-ui/app/elements/change-list/gr-change-list-submit-flow/gr-change-list-submit-flow.ts
@@ -0,0 +1,141 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {html, LitElement} from 'lit';
+import {customElement, query, state} from 'lit/decorators';
+import {ProgressStatus} from '../../../constants/constants';
+import {bulkActionsModelToken} from '../../../models/bulk-actions/bulk-actions-model';
+import {resolve} from '../../../models/dependency';
+import {ChangeInfo} from '../../../types/common';
+import {pluralize} from '../../../utils/string-util';
+import {subscribe} from '../../lit/subscription-controller';
+import '../../shared/gr-overlay/gr-overlay';
+import '../../shared/gr-dialog/gr-dialog';
+import {GrOverlay} from '../../shared/gr-overlay/gr-overlay';
+
+@customElement('gr-change-list-submit-flow')
+export class GrChangeListSubmitFlow extends LitElement {
+  @state() private selectedChanges: ChangeInfo[] = [];
+
+  @state()
+  private progressByChange = new Map<ChangeInfo, ProgressStatus>();
+
+  @query('gr-overlay') private overlay!: GrOverlay;
+
+  private readonly getBulkActionsModel = resolve(this, bulkActionsModelToken);
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+    subscribe(
+      this,
+      this.getBulkActionsModel().selectedChanges$,
+      selectedChanges => {
+        this.selectedChanges = selectedChanges;
+        this.progressByChange = new Map(
+          this.selectedChanges.map(change => [
+            change,
+            ProgressStatus.NOT_STARTED,
+          ])
+        );
+      }
+    );
+  }
+
+  override render() {
+    // TODO: permissions should control button status
+    const enabled = this.selectedChanges.length > 0;
+    const confirmLabel = this.isReady()
+      ? 'Apply'
+      : this.isDone()
+      ? 'Close'
+      : 'Running';
+    return html`
+      <gr-button
+        .disabled=${!enabled}
+        flatten
+        @click=${() => this.overlay.open()}
+        >submit</gr-button
+      >
+      <gr-overlay>
+        <gr-dialog
+          @cancel=${() => this.overlay.close()}
+          @confirm=${() => this.onConfirm()}
+          .confirmLabel=${confirmLabel}
+          .disabled=${!this.isReady() && !this.isDone()}
+        >
+          <div slot="header">
+            Submit ${pluralize(this.selectedChanges.length, 'change')}
+          </div>
+          <div slot="main">
+            <table>
+              <thead>
+                <tr>
+                  <th>Subject</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                ${this.selectedChanges.map(
+                  change => html`
+                    <tr>
+                      <td>${change.subject}</td>
+                      <td>${this.progressByChange.get(change)}</td>
+                    </tr>
+                  `
+                )}
+              </tbody>
+            </table>
+          </div>
+        </gr-dialog>
+      </gr-overlay>
+    `;
+  }
+
+  private onConfirm() {
+    if (this.isReady()) {
+      this.submitChanges();
+    } else if (this.isDone()) {
+      this.overlay.close();
+    }
+  }
+
+  private submitChanges() {
+    this.progressByChange = new Map(
+      this.selectedChanges.map(change => [change, ProgressStatus.RUNNING])
+    );
+    const inFlightActions = this.getBulkActionsModel().submitChanges();
+    for (let index = 0; index < this.selectedChanges.length; index++) {
+      const change = this.selectedChanges[index];
+      inFlightActions[index]
+        .then(() => {
+          this.progressByChange.set(change, ProgressStatus.SUCCESSFUL);
+          this.requestUpdate();
+        })
+        .catch(() => {
+          this.progressByChange.set(change, ProgressStatus.FAILED);
+          this.requestUpdate();
+        });
+    }
+  }
+
+  private isReady() {
+    return Array.from(this.progressByChange.values()).every(
+      status => status === ProgressStatus.NOT_STARTED
+    );
+  }
+
+  private isDone() {
+    return Array.from(this.progressByChange.values()).every(status =>
+      [ProgressStatus.FAILED, ProgressStatus.SUCCESSFUL].includes(status)
+    );
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-change-list-submit-flow': GrChangeListSubmitFlow;
+  }
+}
---
+++ polygerrit-ui/app/elements/change-list/gr-change-list-submit-flow/gr-change-list-submit-flow_test.ts
@@ -0,0 +1,205 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import {fixture, html} from '@open-wc/testing-helpers';
+import {
+  BulkActionsModel,
+  bulkActionsModelToken,
+} from '../../../models/bulk-actions/bulk-actions-model';
+import {wrapInProvider} from '../../../models/di-provider-element';
+import {getAppContext} from '../../../services/app-context';
+import '../../../test/common-test-setup-karma';
+import {createChange} from '../../../test/test-data-generators';
+import {
+  MockPromise,
+  mockPromise,
+  queryAll,
+  queryAndAssert,
+  stubRestApi,
+  waitUntilObserved,
+} from '../../../test/test-utils';
+import {ChangeInfo, NumericChangeId} from '../../../types/common';
+import {GrButton} from '../../shared/gr-button/gr-button';
+import {GrDialog} from '../../shared/gr-dialog/gr-dialog';
+import {GrOverlay} from '../../shared/gr-overlay/gr-overlay';
+import './gr-change-list-submit-flow';
+import type {GrChangeListSubmitFlow} from './gr-change-list-submit-flow';
+
+const changes: ChangeInfo[] = [
+  {
+    ...createChange(),
+    _number: 1 as NumericChangeId,
+    subject: 'Subject 1',
+    work_in_progress: true,
+    actions: {submit: {}},
+  },
+  {
+    ...createChange(),
+    _number: 2 as NumericChangeId,
+    subject: 'Subject 2',
+    work_in_progress: true,
+    actions: {submit: {}},
+  },
+];
+
+suite('gr-change-list-submit-flow tests', () => {
+  let element: GrChangeListSubmitFlow;
+  let model: BulkActionsModel;
+
+  async function selectChange(change: ChangeInfo) {
+    model.addSelectedChangeNum(change._number);
+    await waitUntilObserved(model.selectedChangeNums$, selectedChangeNums =>
+      selectedChangeNums.includes(change._number)
+    );
+    await element.updateComplete;
+  }
+
+  setup(async () => {
+    stubRestApi('getDetailedChangesWithActions').resolves(changes);
+    model = new BulkActionsModel(getAppContext().restApiService);
+    model.sync(changes);
+
+    element = (
+      await fixture(
+        wrapInProvider(
+          html`<gr-change-list-submit-flow></gr-change-list-submit-flow>`,
+          bulkActionsModelToken,
+          model
+        )
+      )
+    ).querySelector('gr-change-list-submit-flow')!;
+    await selectChange(changes[0]);
+    await selectChange(changes[1]);
+    await element.updateComplete;
+  });
+
+  test('renders flow', async () => {
+    expect(element).shadowDom.to.equal(/* HTML */ `
+      <gr-button flatten="" aria-disabled="false" role="button" tabindex="0"
+        >submit</gr-button
+      >
+      <gr-overlay aria-hidden="true" style="outline: none; display: none;">
+        <gr-dialog role="dialog">
+          <div slot="header">Submit 2 changes</div>
+          <div slot="main">
+            <table>
+              <thead>
+                <tr>
+                  <th>Subject</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Subject 1</td>
+                  <td>NOT STARTED</td>
+                </tr>
+                <tr>
+                  <td>Subject 2</td>
+                  <td>NOT STARTED</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </gr-dialog>
+      </gr-overlay>
+    `);
+  });
+
+  test('flow button enabled when changes selected', async () => {
+    const button = queryAndAssert<GrButton>(element, 'gr-button');
+    assert.isFalse(button.disabled);
+  });
+
+  test('flow button disabled when no changes selected', async () => {
+    model.clearSelectedChangeNums();
+    await waitUntilObserved(model.selectedChangeNums$, s => s.length === 0);
+    await element.updateComplete;
+
+    const button = queryAndAssert<GrButton>(element, 'gr-button');
+    assert.isTrue(button.disabled);
+  });
+
+  test('overlay hidden before flow button clicked', async () => {
+    const overlay = queryAndAssert<GrOverlay>(element, 'gr-overlay');
+    assert.isFalse(overlay.opened);
+  });
+
+  test('flow button click shows overlay', async () => {
+    const button = queryAndAssert<GrButton>(element, 'gr-button');
+
+    button.click();
+    await element.updateComplete;
+
+    const overlay = queryAndAssert<GrOverlay>(element, 'gr-overlay');
+    assert.isTrue(overlay.opened);
+  });
+
+  suite('dialog', () => {
+    let submitPromises: MockPromise<Response>[];
+    let changeActionStub: sinon.SinonStub;
+    let dialog: GrDialog;
+
+    async function resolvePromises() {
+      submitPromises[0].resolve(new Response());
+      submitPromises[1].reject('not allowed');
+      await element.updateComplete;
+    }
+
+    function getStatusLabels() {
+      return Array.from(
+        queryAll<HTMLTableCellElement>(element, 'tbody td:last-child')
+      ).map(td => td.innerText);
+    }
+
+    setup(async () => {
+      submitPromises = [];
+      changeActionStub = stubRestApi('executeChangeAction');
+      for (let i = 0; i < changes.length; i++) {
+        const promise = mockPromise<Response>();
+        submitPromises.push(promise);
+        changeActionStub
+          .withArgs(changes[i]._number, sinon.match.any, sinon.match.any)
+          .returns(promise);
+      }
+
+      queryAndAssert<GrButton>(element, 'gr-button').click();
+      await element.updateComplete;
+      dialog = queryAndAssert<GrDialog>(element, 'gr-dialog');
+      await dialog.updateComplete;
+    });
+
+    test('confirm button text updates', async () => {
+      assert.equal(dialog.confirmLabel, 'Apply');
+
+      dialog.confirmButton!.click();
+      await element.updateComplete;
+
+      assert.equal(dialog.confirmLabel, 'Running');
+
+      await resolvePromises();
+      await element.updateComplete;
+
+      assert.equal(dialog.confirmLabel, 'Close');
+    });
+
+    test('status column updates', async () => {
+      assert.sameOrderedMembers(getStatusLabels(), [
+        'NOT STARTED',
+        'NOT STARTED',
+      ]);
+
+      dialog.confirmButton!.click();
+      await element.updateComplete;
+
+      assert.sameOrderedMembers(getStatusLabels(), ['RUNNING', 'RUNNING']);
+
+      await resolvePromises();
+      await element.updateComplete;
+
+      assert.sameOrderedMembers(getStatusLabels(), ['SUCCESSFUL', 'FAILED']);
+    });
+  });
+});

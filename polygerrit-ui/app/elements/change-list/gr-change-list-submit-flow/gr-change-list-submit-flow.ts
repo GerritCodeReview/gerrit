dcb75ca5d44766a251aa480b7897c1d7ebf2319d
Frank Borden <frankborden@google.com>
2022-03-17 11:57:39 +0100

    Introduce submit flow
    As a starting point, this is identical to the mark active flow and will
    be customized in a later change.
    
    Google-Bug-Id: b/216478679
    Release-Notes: skip
    Change-Id: Idd4818a81bb173e80842051270d2c2ed32507efd

--- polygerrit-ui/app/elements/change-list/gr-change-list-submit-flow/gr-change-list-submit-flow.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-submit-flow/gr-change-list-submit-flow.ts
@@ -3,6 +3,7 @@
  * Copyright 2022 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
+
 import {html, LitElement} from 'lit';
 import {customElement, query, state} from 'lit/decorators';
 import {ProgressStatus} from '../../../constants/constants';
@@ -15,8 +16,8 @@ import '../../shared/gr-overlay/gr-overlay';
 import '../../shared/gr-dialog/gr-dialog';
 import {GrOverlay} from '../../shared/gr-overlay/gr-overlay';
 
-@customElement('gr-change-list-mark-active-flow')
-export class GrChangeListMarkActiveFlow extends LitElement {
+@customElement('gr-change-list-submit-flow')
+export class GrChangeListSubmitFlow extends LitElement {
   @state() private selectedChanges: ChangeInfo[] = [];
 
   @state()
@@ -46,28 +47,30 @@ export class GrChangeListMarkActiveFlow extends LitElement {
   override render() {
     const enabled =
       this.selectedChanges.length > 0 &&
-      this.selectedChanges.every(change => change.actions?.ready);
-    const confirmLabel = this.isReady()
-      ? 'Apply'
-      : this.isDone()
-      ? 'Close'
-      : 'Running';
+      this.selectedChanges.every(change => change.actions?.submit);
+    const status = this.getOverallStatus();
+    const confirmLabel =
+      status === ProgressStatus.NOT_STARTED
+        ? 'Apply'
+        : status !== ProgressStatus.RUNNING
+        ? 'Close'
+        : 'Running';
     return html`
       <gr-button
         .disabled=${!enabled}
         flatten
         @click=${() => this.overlay.open()}
-        >mark as active</gr-button
+        >submit</gr-button
       >
-      <gr-overlay>
+      <gr-overlay with-backdrop>
         <gr-dialog
           @cancel=${() => this.overlay.close()}
           @confirm=${() => this.onConfirm()}
           .confirmLabel=${confirmLabel}
-          .disabled=${!this.isReady() && !this.isDone()}
+          .disabled=${status === ProgressStatus.RUNNING}
         >
           <div slot="header">
-            Mark ${pluralize(this.selectedChanges.length, 'change')} as active
+            Submit ${pluralize(this.selectedChanges.length, 'change')}
           </div>
           <div slot="main">
             <table>
@@ -95,18 +98,19 @@ export class GrChangeListMarkActiveFlow extends LitElement {
   }
 
   private onConfirm() {
-    if (this.isReady()) {
-      this.markChangesActive();
-    } else if (this.isDone()) {
+    const status = this.getOverallStatus();
+    if (status === ProgressStatus.NOT_STARTED) {
+      this.submitChanges();
+    } else if (status !== ProgressStatus.RUNNING) {
       this.overlay.close();
     }
   }
 
-  private markChangesActive() {
+  private submitChanges() {
     this.progressByChange = new Map(
       this.selectedChanges.map(change => [change, ProgressStatus.RUNNING])
     );
-    const inFlightActions = this.getBulkActionsModel().markChangesActive();
+    const inFlightActions = this.getBulkActionsModel().submitChanges();
     for (let index = 0; index < this.selectedChanges.length; index++) {
       const change = this.selectedChanges[index];
       inFlightActions[index]
@@ -121,21 +125,20 @@ export class GrChangeListMarkActiveFlow extends LitElement {
     }
   }
 
-  private isReady() {
-    return Array.from(this.progressByChange.values()).every(
-      status => status === ProgressStatus.NOT_STARTED
-    );
-  }
-
-  private isDone() {
-    return Array.from(this.progressByChange.values()).every(status =>
-      [ProgressStatus.FAILED, ProgressStatus.SUCCESSFUL].includes(status)
-    );
+  private getOverallStatus() {
+    const statuses = Array.from(this.progressByChange.values());
+    if (statuses.every(s => s === ProgressStatus.NOT_STARTED)) {
+      return ProgressStatus.NOT_STARTED;
+    }
+    if (statuses.some(s => s === ProgressStatus.RUNNING)) {
+      return ProgressStatus.RUNNING;
+    }
+    return ProgressStatus.SUCCESSFUL;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'gr-change-list-mark-active-flow': GrChangeListMarkActiveFlow;
+    'gr-change-list-submit-flow': GrChangeListSubmitFlow;
   }
 }

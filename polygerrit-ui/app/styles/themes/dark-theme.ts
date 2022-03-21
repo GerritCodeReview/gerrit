dc55e30176f189974968a07aac53a299089b068e
Paladox none <thomasmulhall410@yahoo.com>
2022-03-21 21:43:08 +0000

    Fix an issue with highlighting 'this' after upgrade to hljs 11.5.0
    In [0] the highlighting of 'this' and a few other keywords was moved
    to a different class.
    We whitelist that class and we get it to use the built_in class colour
    as it was done previously.
    
    Another issue that is fixed is the highlighting 'properties'. The colour
    is yellow which is annoying so we change it to use primary-text-color.
    
    [0] https://github.com/highlightjs/highlight.js/pull/3169
    
    Release-Notes: skip
    Change-Id: I68e7f0e84b3564f6cfa978c826a132cc1c543679

--- polygerrit-ui/app/styles/themes/dark-theme.ts
+++ polygerrit-ui/app/styles/themes/dark-theme.ts
@@ -246,19 +246,19 @@ const darkThemeCss = safeStyleSheet`
     --syntax-meta-keyword-color: #eefff7;
     --syntax-number-color: #00998a;
     --syntax-params-color: var(--primary-text-color);
+    --syntax-property-color: #ab5656;
     --syntax-regexp-color: #f77669;
     --syntax-selector-attr-color: #80cbbf;
     --syntax-selector-class-color: #ffcb68;
     --syntax-selector-id-color: #f77669;
     --syntax-selector-pseudo-color: #c792ea;
-    --syntax-property-color: #c792ea;
     --syntax-string-color: #c3e88d;
     --syntax-tag-color: #f77669;
     --syntax-template-tag-color: #c792ea;
     --syntax-template-variable-color: #f77669;
     --syntax-title-color: #75a5ff;
     --syntax-type-color: #dd5f5f;
-    --syntax-variable-color: #f77669;
+    --syntax-variable-color: #ab5656;
 
     /* misc */
     --line-length-indicator-color: #d7aefb;

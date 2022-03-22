74e279db1201a3102019e0d3bd80c18360b64f9b
Edwin Kempin <ekempin@google.com>
2022-03-22 11:14:33 +0100

    Add approval copy predicate 'is:<int>' to match approvals with an exact vote
    Copy conditions for approvals, that decide whether an approval is copied
    to a new patch set, should be expressed as a query [1]. Using a copy
    condition query is preferred over using the deprecated boolean flags
    that also control when approvals are copied. However an equivalent
    predicate for label.Label-Name.copyValue [2] that allows to make
    arbitrary voting values sticky was still missing. With this change we
    support 'is:<VALUE>' as a predicate in copy conditions now so that
    making arbitrary voting values sticky gets possible by a copy condition
    query too.
    
    Since using copy condition queries is preferred and the boolean flags
    are already deprecated we deprecate using label.Label-Name.copyValue too
    now.
    
    [1] https://gerrit-review.googlesource.com/Documentation/config-labels.html#label_copyCondition
    [2] https://gerrit-review.googlesource.com/Documentation/config-labels.html#label_copyValue
    
    Release-Notes: Added support for 'is:<VALUE>' in approval copy condition queries to make arbitrary voting values sticky
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Ibc79273c1ec85df709e01efb63bbb0029c1a1594

---
+++ java/com/google/gerrit/server/query/approval/ExactValuePredicate.java
@@ -0,0 +1,49 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.query.approval;
+
+import com.google.gerrit.index.query.Predicate;
+import java.util.Collection;
+
+/** Predicate that matches patch set approvals that have a given voting value. */
+public class ExactValuePredicate extends ApprovalPredicate {
+  private final short votingValue;
+
+  public ExactValuePredicate(short votingValue) {
+    this.votingValue = votingValue;
+  }
+
+  @Override
+  public boolean match(ApprovalContext approvalContext) {
+    return votingValue == approvalContext.patchSetApproval().value();
+  }
+
+  @Override
+  public Predicate<ApprovalContext> copy(
+      Collection<? extends Predicate<ApprovalContext>> children) {
+    return new ExactValuePredicate(votingValue);
+  }
+
+  @Override
+  public int hashCode() {
+    return Short.valueOf(votingValue).hashCode();
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    return (other instanceof ExactValuePredicate)
+        && votingValue == ((ExactValuePredicate) other).votingValue;
+  }
+}

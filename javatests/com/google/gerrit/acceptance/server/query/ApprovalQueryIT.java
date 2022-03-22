2ad7c31af252d0c519d132f1fd4d27db38d91e36
Edwin Kempin <ekempin@google.com>
2022-03-22 11:11:54 +0100

    Approval queries: Include operator name into error messages
    Include the name of the operator for which we got an invalid value into
    the error message so that users can easier understand which part of the
    query is wrong.
    
    Change-Id: I2c9ba76e936a2171f319fd8ad993bdb2f2eda012
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- javatests/com/google/gerrit/acceptance/server/query/ApprovalQueryIT.java
+++ javatests/com/google/gerrit/acceptance/server/query/ApprovalQueryIT.java
@@ -69,7 +69,7 @@ public class ApprovalQueryIT extends AbstractDaemonTest {
         assertThrows(QueryParseException.class, () -> queryBuilder.parse("is:INVALID"));
     assertThat(thrown)
         .hasMessageThat()
-        .contains("INVALID is not a valid value. Valid values: ANY, MAX, MIN");
+        .contains("INVALID is not a valid value for operator 'is'. Valid values: ANY, MAX, MIN");
   }
 
   @Test
@@ -139,7 +139,7 @@ public class ApprovalQueryIT extends AbstractDaemonTest {
     assertThat(thrown)
         .hasMessageThat()
         .contains(
-            "INVALID is not a valid value. Valid values:"
+            "INVALID is not a valid value for operator 'changekind'. Valid values:"
                 + " MERGE_FIRST_PARENT_UPDATE, NO_CHANGE, NO_CODE_CHANGE, REWORK, TRIVIAL_REBASE");
   }
 

86ae1dc1c55aafbad01a2a34909c67a7e48796f5
Frank Borden <frankborden@google.com>
2022-03-23 14:07:37 +0100

    Remove bulk mark active flow
    Release-Notes: skip
    
    Change-Id: I2b344a7a755af094002dff172d7eb528f24cf2cb

--- polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-action-bar/gr-change-list-action-bar.ts
@@ -11,7 +11,6 @@ import {resolve} from '../../../models/dependency';
 import {pluralize} from '../../../utils/string-util';
 import {subscribe} from '../../lit/subscription-controller';
 import '../../shared/gr-button/gr-button';
-import '../gr-change-list-mark-active-flow/gr-change-list-mark-active-flow';
 import '../gr-change-list-bulk-abandon-flow/gr-change-list-bulk-abandon-flow';
 
 /**
@@ -108,7 +107,6 @@ export class GrChangeListActionBar extends LitElement {
               : nothing}
           </div>
           <div class="actionButtons">
-            <gr-change-list-mark-active-flow></gr-change-list-mark-active-flow>
             <gr-change-list-bulk-abandon-flow>
             </gr-change-list-bulk-abandon-flow>
           </div>

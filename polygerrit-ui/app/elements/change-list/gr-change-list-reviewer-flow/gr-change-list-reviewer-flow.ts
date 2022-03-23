a5aacf8ffa62e8e8926b83f2b9a4b4a73fa20054
Frank Borden <frankborden@google.com>
2022-03-23 11:50:43 +0100

    Add CC with bulk action
    https://imgur.com/a/6qR0E9J
    
    Google-Bug-Id: b/216478677
    Release-Notes: skip
    Change-Id: I376709c5db4d3e8c61d073c5ad9878d95ea35532

--- polygerrit-ui/app/elements/change-list/gr-change-list-reviewer-flow/gr-change-list-reviewer-flow.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-reviewer-flow/gr-change-list-reviewer-flow.ts
@@ -5,7 +5,7 @@
  */
 import {html, LitElement} from 'lit';
 import {customElement, query, state} from 'lit/decorators';
-import {ProgressStatus} from '../../../constants/constants';
+import {ProgressStatus, ReviewerState} from '../../../constants/constants';
 import {bulkActionsModelToken} from '../../../models/bulk-actions/bulk-actions-model';
 import {resolve} from '../../../models/dependency';
 import {AccountInfo, ChangeInfo} from '../../../types/common';
@@ -21,12 +21,29 @@ import {
 } from '../../../scripts/gr-reviewer-suggestions-provider/gr-reviewer-suggestions-provider';
 import '../../shared/gr-account-list/gr-account-list';
 
+const SUGGESTIONS_PROVIDERS_USERS_TYPES_BY_REVIEWER_STATE: Record<
+  ReviewerState,
+  SUGGESTIONS_PROVIDERS_USERS_TYPES
+> = {
+  REVIEWER: SUGGESTIONS_PROVIDERS_USERS_TYPES.REVIEWER,
+  CC: SUGGESTIONS_PROVIDERS_USERS_TYPES.CC,
+  REMOVED: SUGGESTIONS_PROVIDERS_USERS_TYPES.ANY,
+};
+
 @customElement('gr-change-list-reviewer-flow')
 export class GrChangeListReviewerFlow extends LitElement {
   @state() private selectedChanges: ChangeInfo[] = [];
 
-  // given to gr-account-list to mutate
-  @state() private updatedReviewers: AccountInfo[] = [];
+  // contents are given to gr-account-lists to mutate
+  @state() private updatedAccountsByReviewerState: Map<
+    ReviewerState,
+    AccountInfo[]
+  > = new Map();
+
+  @state() private suggestionsProviderByReviewerState: Map<
+    ReviewerState,
+    ReviewerSuggestionsProvider
+  > = new Map();
 
   @state() private progressByChange = new Map<ChangeInfo, ProgressStatus>();
 
@@ -35,8 +52,6 @@ export class GrChangeListReviewerFlow extends LitElement {
   private getBulkActionsModel = resolve(this, bulkActionsModelToken);
 
   private restApiService = getAppContext().restApiService;
-
-  private suggestionsProvider?: ReviewerSuggestionsProvider;
 
   override connectedCallback(): void {
     super.connectedCallback();
@@ -71,14 +86,16 @@ export class GrChangeListReviewerFlow extends LitElement {
           <div slot="header">Add Reviewer / CC</div>
           <div slot="main">
             <div>
-              <span>Reviewers:</span>
-              <gr-account-list
-                .accounts=${this.updatedReviewers}
-                .removableValues=${[]}
-                .suggestionsProvider=${this.suggestionsProvider}
-                .placeholder=${'Add reviewer...'}
-              >
-              </gr-account-list>
+              <span>Reviewers</span>
+              ${this.renderAccountList(
+                ReviewerState.REVIEWER,
+                'reviewer-list',
+                'Add reviewer'
+              )}
+            </div>
+            <div>
+              <span>CC</span>
+              ${this.renderAccountList(ReviewerState.CC, 'cc-list', 'Add CC')}
             </div>
           </div>
         </gr-dialog>
@@ -86,27 +103,53 @@ export class GrChangeListReviewerFlow extends LitElement {
     `;
   }
 
+  private renderAccountList(
+    reviewerState: ReviewerState,
+    id: string,
+    placeholder: string
+  ) {
+    const updatedAccounts =
+      this.updatedAccountsByReviewerState.get(reviewerState);
+    const suggestionsProvider =
+      this.suggestionsProviderByReviewerState.get(reviewerState);
+    if (!updatedAccounts || !suggestionsProvider) {
+      return;
+    }
+    return html`
+      <gr-account-list
+        id=${id}
+        .accounts=${updatedAccounts}
+        .removableValues=${[]}
+        .suggestionsProvider=${suggestionsProvider}
+        .placeholder=${placeholder}
+      >
+      </gr-account-list>
+    `;
+  }
+
   private resetFlow() {
     this.progressByChange = new Map(
       this.selectedChanges.map(change => [change, ProgressStatus.NOT_STARTED])
     );
-    this.updatedReviewers = this.getCurrentReviewers();
-    if (this.selectedChanges.length === 0) {
-      return;
+    for (const state of [ReviewerState.REVIEWER, ReviewerState.CC]) {
+      this.updatedAccountsByReviewerState.set(
+        state,
+        this.getCurrentAccounts(state)
+      );
+      if (this.selectedChanges.length > 0) {
+        this.suggestionsProviderByReviewerState.set(
+          state,
+          this.createSuggestionsProvider(state)
+        );
+      }
     }
-    this.suggestionsProvider = GrReviewerSuggestionsProvider.create(
-      this.restApiService,
-      // TODO: fan out and get suggestions allowed by all changes
-      this.selectedChanges[0]._number,
-      SUGGESTIONS_PROVIDERS_USERS_TYPES.REVIEWER
-    );
-    this.suggestionsProvider.init();
+    this.requestUpdate();
   }
 
   private onConfirm(overallStatus: ProgressStatus) {
     switch (overallStatus) {
       case ProgressStatus.NOT_STARTED:
-        this.saveReviewers();
+        this.saveChanges();
         break;
       case ProgressStatus.SUCCESSFUL:
         this.overlay.close();
@@ -114,12 +157,12 @@ export class GrChangeListReviewerFlow extends LitElement {
     }
   }
 
-  private saveReviewers() {
+  private saveChanges() {
     this.progressByChange = new Map(
       this.selectedChanges.map(change => [change, ProgressStatus.RUNNING])
     );
     const inFlightActions = this.getBulkActionsModel().addReviewers(
-      this.getAddedReviewers()
+      this.updatedAccountsByReviewerState
     );
     for (let index = 0; index < this.selectedChanges.length; index++) {
       const change = this.selectedChanges[index];
@@ -149,9 +192,9 @@ export class GrChangeListReviewerFlow extends LitElement {
       : 'Close';
   }
 
-  private getCurrentReviewers() {
-    const reviewersPerChange = this.selectedChanges.map(change =>
-      Object.values(change.reviewers).flat()
+  private getCurrentAccounts(reviewerState: ReviewerState) {
+    const reviewersPerChange = this.selectedChanges.map(
+      change => change.reviewers[reviewerState] ?? []
     );
     if (reviewersPerChange.length === 0) {
       return [];
@@ -162,11 +205,17 @@ export class GrChangeListReviewerFlow extends LitElement {
     );
   }
 
-  private getAddedReviewers(): AccountInfo[] {
-    const oldReviewers = this.getCurrentReviewers();
-    return this.updatedReviewers.filter(
-      reviewer => !oldReviewers.includes(reviewer)
+  private createSuggestionsProvider(
+    state: ReviewerState
+  ): ReviewerSuggestionsProvider {
+    const suggestionsProvider = GrReviewerSuggestionsProvider.create(
+      this.restApiService,
+      // TODO: fan out and get suggestions allowed by all changes
+      this.selectedChanges[0]._number,
+      SUGGESTIONS_PROVIDERS_USERS_TYPES_BY_REVIEWER_STATE[state]
     );
+    suggestionsProvider.init();
+    return suggestionsProvider;
   }
 
   private getOverallStatus() {

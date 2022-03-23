2772cb268e80c024925a683f86ad4c8dc5dd36ef
Dhruv Srivastava <dhruvsri@google.com>
2022-03-23 15:00:11 +0530

    Add voting request and progess tracking
    Similar to the abandon flow, add progress tracking per change in the
    voting dialog and make the API request to save the votes.
    
    Google-bug-id: b/216478680
    Release-Notes: skip
    Change-Id: I5aaa4ea70bbd70c809466d120a9130b09afc0631

--- polygerrit-ui/app/models/bulk-actions/bulk-actions-model.ts
+++ polygerrit-ui/app/models/bulk-actions/bulk-actions-model.ts
@@ -129,6 +129,25 @@ export class BulkActionsModel
     });
   }
 
+  voteChanges(
+    reviewInput: ReviewInput,
+    // errorFn is needed to avoid showing an error dialog
+    errFn?: (changeNum: NumericChangeId) => void
+  ) {
+    const current = this.subject$.getValue();
+    return current.selectedChangeNums.map(changeNum => {
+      if (!current.allChanges.get(changeNum))
+        throw new Error('invalid change id');
+      const change = current.allChanges.get(changeNum)!;
+      return this.restApiService.saveChangeReview(
+        change._number,
+        'current',
+        reviewInput,
+        () => errFn && errFn(change._number)
+      );
+    });
+  }
+
   addReviewers(
     changedReviewers: Map<ReviewerState, AccountInfo[]>
   ): Promise<Response>[] {

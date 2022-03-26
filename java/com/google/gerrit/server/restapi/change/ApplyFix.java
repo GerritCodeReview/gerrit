11e46fc76ec63b31b843b1ee91c61f4ae56aab89
Sharad Bagri <sharadbagri@google.com>
2022-03-26 01:32:35 -0700

    One implementation of Apply Direct Fix.
    If this implementation looks good. Will add more tests, similar to the
    ones existing currently. Will Also make similar changes in GetFixPreview.
    
    Change-Id: I0cbd1b6c1deafb4af03c262d1a81a7ce7ee44aef

--- java/com/google/gerrit/server/restapi/change/ApplyFix.java
+++ java/com/google/gerrit/server/restapi/change/ApplyFix.java
@@ -16,9 +16,13 @@ package com.google.gerrit.server.restapi.change;
 
 import static com.google.gerrit.server.project.ProjectCache.illegalState;
 
+import com.google.gerrit.entities.Comment.Range;
+import com.google.gerrit.entities.FixReplacement;
 import com.google.gerrit.entities.PatchSet;
 import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.common.DirectFixInput;
 import com.google.gerrit.extensions.common.EditInfo;
+import com.google.gerrit.extensions.common.FixReplacementInfo;
 import com.google.gerrit.extensions.common.Input;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
@@ -26,6 +30,7 @@ import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.extensions.restapi.RestResource;
 import com.google.gerrit.server.change.FixResource;
 import com.google.gerrit.server.change.RevisionResource;
 import com.google.gerrit.server.edit.ChangeEdit;
@@ -41,50 +46,109 @@ import com.google.gerrit.server.project.ProjectState;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.eclipse.jgit.lib.Repository;
 
 @Singleton
-public class ApplyFix implements RestModifyView<FixResource, Input> {
-
-  private final GitRepositoryManager gitRepositoryManager;
-  private final FixReplacementInterpreter fixReplacementInterpreter;
-  private final ChangeEditModifier changeEditModifier;
-  private final ChangeEditJson changeEditJson;
-  private final ProjectCache projectCache;
-
-  @Inject
-  public ApplyFix(
-      GitRepositoryManager gitRepositoryManager,
-      FixReplacementInterpreter fixReplacementInterpreter,
-      ChangeEditModifier changeEditModifier,
-      ChangeEditJson changeEditJson,
-      ProjectCache projectCache) {
-    this.gitRepositoryManager = gitRepositoryManager;
-    this.fixReplacementInterpreter = fixReplacementInterpreter;
-    this.changeEditModifier = changeEditModifier;
-    this.changeEditJson = changeEditJson;
-    this.projectCache = projectCache;
+public class ApplyFix implements RestModifyView {
+  // This should never be called, just put for syntactic correctness.
+  @Override
+  public Response<?> apply(RestResource resource, Object input)
+      throws AuthException, BadRequestException, ResourceConflictException, Exception {
+    return null;
   }
 
-  @Override
-  public Response<EditInfo> apply(FixResource fixResource, Input nothing)
-      throws AuthException, BadRequestException, ResourceConflictException, IOException,
-          ResourceNotFoundException, PermissionBackendException {
-    RevisionResource revisionResource = fixResource.getRevisionResource();
-    Project.NameKey project = revisionResource.getProject();
-    ProjectState projectState = projectCache.get(project).orElseThrow(illegalState(project));
-    PatchSet patchSet = revisionResource.getPatchSet();
+  public static class InputNothing implements RestModifyView<FixResource, Input> {
+    private final GitRepositoryManager gitRepositoryManager;
+    private final FixReplacementInterpreter fixReplacementInterpreter;
+    private final ChangeEditModifier changeEditModifier;
+    private final ChangeEditJson changeEditJson;
+    private final ProjectCache projectCache;
 
-    try (Repository repository = gitRepositoryManager.openRepository(project)) {
-      CommitModification commitModification =
-          fixReplacementInterpreter.toCommitModification(
-              repository, projectState, patchSet.commitId(), fixResource.getFixReplacements());
-      ChangeEdit changeEdit =
-          changeEditModifier.combineWithModifiedPatchSetTree(
-              repository, revisionResource.getNotes(), patchSet, commitModification);
-      return Response.ok(changeEditJson.toEditInfo(changeEdit, false));
-    } catch (InvalidChangeOperationException e) {
-      throw new ResourceConflictException(e.getMessage());
+    @Inject
+    public InputNothing(
+        GitRepositoryManager gitRepositoryManager,
+        FixReplacementInterpreter fixReplacementInterpreter,
+        ChangeEditModifier changeEditModifier,
+        ChangeEditJson changeEditJson,
+        ProjectCache projectCache) {
+      this.gitRepositoryManager = gitRepositoryManager;
+      this.fixReplacementInterpreter = fixReplacementInterpreter;
+      this.changeEditModifier = changeEditModifier;
+      this.changeEditJson = changeEditJson;
+      this.projectCache = projectCache;
+    }
+
+    @Override
+    public Response<EditInfo> apply(FixResource fixResource, Input nothing)
+        throws AuthException, BadRequestException, ResourceConflictException, IOException,
+            ResourceNotFoundException, PermissionBackendException {
+      RevisionResource revisionResource = fixResource.getRevisionResource();
+      Project.NameKey project = revisionResource.getProject();
+      ProjectState projectState = projectCache.get(project).orElseThrow(illegalState(project));
+      PatchSet patchSet = revisionResource.getPatchSet();
+
+      try (Repository repository = gitRepositoryManager.openRepository(project)) {
+        CommitModification commitModification =
+            fixReplacementInterpreter.toCommitModification(
+                repository, projectState, patchSet.commitId(), fixResource.getFixReplacements());
+        ChangeEdit changeEdit =
+            changeEditModifier.combineWithModifiedPatchSetTree(
+                repository, revisionResource.getNotes(), patchSet, commitModification);
+        return Response.ok(changeEditJson.toEditInfo(changeEdit, false));
+      } catch (InvalidChangeOperationException e) {
+        throw new ResourceConflictException(e.getMessage());
+      }
+    }
+  }
+
+  public static class InputDirectFix implements RestModifyView<RevisionResource, DirectFixInput> {
+    private final GitRepositoryManager gitRepositoryManager;
+    private final FixReplacementInterpreter fixReplacementInterpreter;
+    private final ChangeEditModifier changeEditModifier;
+    private final ChangeEditJson changeEditJson;
+    private final ProjectCache projectCache;
+
+    @Inject
+    public InputDirectFix(
+        GitRepositoryManager gitRepositoryManager,
+        FixReplacementInterpreter fixReplacementInterpreter,
+        ChangeEditModifier changeEditModifier,
+        ChangeEditJson changeEditJson,
+        ProjectCache projectCache) {
+      this.gitRepositoryManager = gitRepositoryManager;
+      this.fixReplacementInterpreter = fixReplacementInterpreter;
+      this.changeEditModifier = changeEditModifier;
+      this.changeEditJson = changeEditJson;
+      this.projectCache = projectCache;
+    }
+
+    @Override
+    public Response<EditInfo> apply(
+        RevisionResource revisionResource, DirectFixInput directFixInput)
+        throws AuthException, BadRequestException, ResourceConflictException, IOException,
+            ResourceNotFoundException, PermissionBackendException {
+      Project.NameKey project = revisionResource.getProject();
+      ProjectState projectState = projectCache.get(project).orElseThrow(illegalState(project));
+      PatchSet patchSet = revisionResource.getPatchSet();
+      List<FixReplacement> fixReplacementList = new ArrayList<FixReplacement>();
+      try (Repository repository = gitRepositoryManager.openRepository(project)) {
+        for (FixReplacementInfo fixI : directFixInput.getFixReplacementInfos()) {
+          Range temp_range = new Range(fixI.range);
+          FixReplacement fixR = new FixReplacement(fixI.path, temp_range, fixI.replacement);
+          fixReplacementList.add(fixR);
+        }
+        CommitModification commitModification =
+            fixReplacementInterpreter.toCommitModification(
+                repository, projectState, patchSet.commitId(), fixReplacementList);
+        ChangeEdit changeEdit =
+            changeEditModifier.combineWithModifiedPatchSetTree(
+                repository, revisionResource.getNotes(), patchSet, commitModification);
+        return Response.ok(changeEditJson.toEditInfo(changeEdit, false));
+      } catch (InvalidChangeOperationException e) {
+        throw new ResourceConflictException(e.getMessage());
+      }
     }
   }
 }

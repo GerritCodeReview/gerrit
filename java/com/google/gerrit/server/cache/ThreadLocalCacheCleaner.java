6645ebcea53e4b041f980266a4313490543e5988
Luca Milanesio <luca.milanesio@gmail.com>
2022-03-27 19:18:10 +0100

    Avoid resolving change meta ref multiple times per thread
    The resolution of change meta refs can be costly (up to 2s per ref)
    on large mono-repos with hundreds of thousands of changes on a
    shared NFS volume; see the full analysis and presentation on [1].
    
    Introduce a thread-local per-repo in-memory cache for avoiding
    multiple lookups for the same repository, preventing the performance
    degradation when moving to NoteDb.
    
    Example: the reindex of a single change is reduced from 10 lookups
    to just one.
    
    From an E2E perspective, there are the following improvements:
    - reindex of a change: 400% faster
    - posting comments: 200% faster
    - get available actions: 300% faster
    - get changes details: 50% faster
    
    Allow to enable the new caching behaviour by setting to gerrit.config:
    
    [notedb]
      cacheMetaRef = true
    
    Document the new behaviour, specifying that the performance improvement
    is expected when having a repository with hundreds of thousands of refs.
    
    [1] https://youtu.be/eANday2TsOk
    
    Release-Notes: improve change meta refs lookup latency by 10x times.
    Change-Id: I1405f3f229b4d8ab697e0a88233c5fc7fd88d1f7

---
+++ java/com/google/gerrit/server/cache/ThreadLocalCacheCleaner.java
@@ -0,0 +1,62 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.cache;
+
+import com.google.gerrit.extensions.events.GitReferenceUpdatedListener;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Thread local handler that calls <code>clean</code> on each registered cleaner upon current
+ * execution ends in Thread.
+ */
+public class ThreadLocalCacheCleaner implements GitReferenceUpdatedListener {
+  /**
+   * Each class that wants to use thread's cache and perform cleaning afterwards should register
+   * CacheCleaner implementation.
+   */
+  @FunctionalInterface
+  public interface CacheCleaner {
+    void clean();
+  }
+
+  private static ThreadLocal<ThreadLocalCacheCleaner> handler =
+      ThreadLocal.withInitial(ThreadLocalCacheCleaner::new);
+
+  public static ThreadLocalCacheCleaner get() {
+    return handler.get();
+  }
+
+  private final Set<CacheCleaner> cleaners;
+
+  ThreadLocalCacheCleaner() {
+    this.cleaners = new HashSet<>();
+  }
+
+  public void registerCleaner(CacheCleaner cleaner) {
+    cleaners.add(cleaner);
+  }
+
+  public void cleanThreadCache() {
+    cleaners.forEach(CacheCleaner::clean);
+    cleaners.clear();
+    handler.remove();
+  }
+
+  @Override
+  public void onGitReferenceUpdated(Event event) {
+    get().cleanThreadCache();
+  }
+}

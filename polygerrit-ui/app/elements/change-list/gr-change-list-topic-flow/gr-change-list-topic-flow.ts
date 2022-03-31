4b8a7c2860c556a4392b2f997744ef652a2aa365
Frank Borden <frankborden@google.com>
2022-03-31 17:31:17 +0200

    Introduce topic bulk action flow
    https://imgur.com/a/ZoZET82
    
    Google-Bug-Id: b/227605744
    Release-Notes: skip
    Change-Id: I6c772b5f195257a9f3b614a83aaed232a8f76cf4

---
+++ polygerrit-ui/app/elements/change-list/gr-change-list-topic-flow/gr-change-list-topic-flow.ts
@@ -0,0 +1,175 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import {css, html, LitElement} from 'lit';
+import {customElement, query, state} from 'lit/decorators';
+import {bulkActionsModelToken} from '../../../models/bulk-actions/bulk-actions-model';
+import {resolve} from '../../../models/dependency';
+import {ChangeInfo} from '../../../types/common';
+import {subscribe} from '../../lit/subscription-controller';
+import '../../shared/gr-button/gr-button';
+import '../../shared/gr-autocomplete/gr-autocomplete';
+import '@polymer/iron-dropdown/iron-dropdown';
+import {IronDropdownElement} from '@polymer/iron-dropdown/iron-dropdown';
+import {getAppContext} from '../../../services/app-context';
+import {notUndefined} from '../../../types/types';
+import {unique} from '../../../utils/common-util';
+import {AutocompleteSuggestion} from '../../shared/gr-autocomplete/gr-autocomplete';
+import {when} from 'lit/directives/when';
+
+@customElement('gr-change-list-topic-flow')
+export class GrChangeListTopicFlow extends LitElement {
+  @state() private selectedChanges: ChangeInfo[] = [];
+
+  @state() private topicName = '';
+
+  @state() private existingTopics: string[] = [];
+
+  @state() private isDropdownOpen = false;
+
+  @query('iron-dropdown') private dropdown!: IronDropdownElement;
+
+  private getBulkActionsModel = resolve(this, bulkActionsModelToken);
+
+  private restApiService = getAppContext().restApiService;
+
+  static override get styles() {
+    return css`
+      iron-dropdown {
+        box-shadow: var(--elevation-level-2);
+        width: 400px;
+        background-color: var(--dialog-background-color);
+      }
+      [slot='dropdown-content'] {
+        padding: var(--spacing-xl) var(--spacing-l) var(--spacing-l);
+      }
+      gr-autocomplete {
+        --border-color: var(--gray-800);
+      }
+      .buttons {
+        padding-top: var(--spacing-m);
+        display: flex;
+        justify-content: flex-end;
+        gap: var(--spacing-m);
+      }
+    `;
+  }
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+    subscribe(
+      this,
+      this.getBulkActionsModel().selectedChanges$,
+      selectedChanges => {
+        this.selectedChanges = selectedChanges;
+      }
+    );
+  }
+
+  override render() {
+    return html`
+      <gr-button id="start-flow" flatten @click=${() => this.openDropdown()}
+        >Topic</gr-button
+      >
+      <iron-dropdown
+        .horizontalAlign=${'auto'}
+        .verticalAlign=${'auto'}
+        .verticalOffset=${24 /* roughly line height in pixels */}
+        @opened-changed=${(e: CustomEvent) =>
+          (this.isDropdownOpen = e.detail.value)}
+      >
+        ${when(
+          this.isDropdownOpen,
+          () =>
+            html`
+              <div slot="dropdown-content">
+                <!--
+                  The .query function needs to be bound to this because lit's
+                  autobind seems to work only for @event handlers.
+                  'this.getTopicSuggestions.bind(this)' gets in trouble with
+                  our linter even though the bind is necessary here, so an
+                  anonymous function is used instead.
+                -->
+                <gr-autocomplete
+                  .text=${this.topicName}
+                  .query=${(query: string) => this.getTopicSuggestions(query)}
+                  placeholder="Type topic name to create or filter topics"
+                  @text-changed=${(e: CustomEvent) =>
+                    (this.topicName = e.detail.value)}
+                ></gr-autocomplete>
+                <div class="buttons">
+                  <gr-button
+                    id="create-new-topic-button"
+                    flatten
+                    @click=${() => this.applyTopic()}
+                    .disabled=${this.isCreateNewTopicDisabled()}
+                    >Create new topic</gr-button
+                  >
+                  <gr-button
+                    id="apply-topic-button"
+                    flatten
+                    @click=${() => this.applyTopic()}
+                    .disabled=${this.isApplyDisabled()}
+                    >Apply</gr-button
+                  >
+                </div>
+              </div>
+            `
+        )}
+      </iron-dropdown>
+    `;
+  }
+
+  private openDropdown() {
+    this.topicName = '';
+    this.isDropdownOpen = true;
+    this.dropdown.open();
+  }
+
+  private async getTopicSuggestions(
+    query: string
+  ): Promise<AutocompleteSuggestion[]> {
+    const suggestions = await this.restApiService.getChangesWithSimilarTopic(
+      query
+    );
+    this.existingTopics = (suggestions ?? [])
+      .map(change => change.topic)
+      .filter(notUndefined)
+      .filter(unique);
+    return this.existingTopics.map(topic => {
+      return {name: topic, value: topic};
+    });
+  }
+
+  private async applyTopic() {
+    const promises = this.selectedChanges.map(change =>
+      this.restApiService.setChangeTopic(change._number, this.topicName)
+    );
+    const results = await Promise.allSettled(promises);
+    // TODO: show error when some are rejected
+    if (results.every(result => result.status === 'fulfilled')) {
+      this.dropdown.close();
+      this.isDropdownOpen = false;
+    }
+  }
+
+  private isCreateNewTopicDisabled() {
+    return (
+      this.topicName === '' || this.existingTopics.includes(this.topicName)
+    );
+  }
+
+  private isApplyDisabled() {
+    return (
+      this.topicName !== '' && !this.existingTopics.includes(this.topicName)
+    );
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-change-list-topic-flow': GrChangeListTopicFlow;
+  }
+}

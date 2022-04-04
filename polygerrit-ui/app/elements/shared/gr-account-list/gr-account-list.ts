598945f5c6e1b5c7cc7fc9bbf0623fdc9900a07b
Milutin Kristofic <milutin@google.com>
2022-04-04 14:15:43 +0200

    Prepare gr-account-list to lit
    Change-Id: I16dc2fe017b921320e20682427eebbb1365fee71

--- polygerrit-ui/app/elements/shared/gr-account-list/gr-account-list.ts
+++ polygerrit-ui/app/elements/shared/gr-account-list/gr-account-list.ts
@@ -38,6 +38,8 @@ import {PolymerDeepPropertyChange} from '@polymer/polymer/interfaces';
 import {PaperInputElementExt} from '../../../types/types';
 import {fireAlert} from '../../../utils/event-util';
 import {accountOrGroupKey} from '../../../utils/account-util';
+import {css, html} from 'lit';
+import {sharedStyles} from '../../../styles/shared-styles';
 
 const VALID_EMAIL_ALERT = 'Please input a valid email.';
 
@@ -187,6 +189,61 @@ export class GrAccountList extends PolymerElement {
     this.addEventListener('remove', e =>
       this._handleRemove(e as CustomEvent<{account: AccountInput}>)
     );
+  }
+
+  static styles = [
+    sharedStyles,
+    css`
+      gr-account-chip {
+        display: inline-block;
+        margin: var(--spacing-xs) var(--spacing-xs) var(--spacing-xs) 0;
+      }
+      gr-account-entry {
+        display: flex;
+        flex: 1;
+        min-width: 10em;
+        margin: var(--spacing-xs) var(--spacing-xs) var(--spacing-xs) 0;
+      }
+      .group {
+        --account-label-suffix: ' (group)';
+      }
+      .pending-add {
+        font-style: italic;
+      }
+      .list {
+        align-items: center;
+        display: flex;
+        flex-wrap: wrap;
+      }
+    `,
+  ];
+
+  render() {
+    return html`<div class="list">
+        <template id="chips" is="dom-repeat" items="[[accounts]]" as="account">
+          <gr-account-chip
+            account="[[account]]"
+            class$="[[_computeChipClass(account)]]"
+            data-account-id$="[[account._account_id]]"
+            removable="[[_computeRemovable(account, readonly)]]"
+            on-keydown="_handleChipKeydown"
+            tabindex="-1"
+          >
+          </gr-account-chip>
+        </template>
+      </div>
+      <gr-account-entry
+        borderless=""
+        hidden$="[[_computeEntryHidden(maxCount, accounts.*, readonly)]]"
+        id="entry"
+        placeholder="[[placeholder]]"
+        on-add="_handleAdd"
+        on-input-keydown="_handleInputKeydown"
+        allow-any-input="[[allowAnyInput]]"
+        query-suggestions="[[_querySuggestions]]"
+      >
+      </gr-account-entry>
+      <slot></slot>`;
   }
 
   get accountChips() {

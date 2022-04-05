87af2f714c8c03d1b42af22c3ee33fcd0b6038cb
Ben Rohlfs <brohlfs@google.com>
2022-04-05 14:35:46 +0200

    Migrate gr-ranged-comment-layer from polymer to lit
    Release-Notes: skip
    Change-Id: I90d368642c47f9326d4dfaeede6f10e3fcd70abd

--- polygerrit-ui/app/embed/diff/gr-ranged-comment-layer/gr-ranged-comment-layer.ts
+++ polygerrit-ui/app/embed/diff/gr-ranged-comment-layer/gr-ranged-comment-layer.ts
@@ -1,30 +1,12 @@
 /**
  * @license
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
  */
 import {GrAnnotation} from '../gr-diff-highlight/gr-annotation';
-import {PolymerElement} from '@polymer/polymer/polymer-element';
-import {htmlTemplate} from './gr-ranged-comment-layer_html';
 import {GrDiffLine, GrDiffLineType} from '../gr-diff/gr-diff-line';
 import {strToClassName} from '../../../utils/dom-util';
-import {customElement, property, observe} from '@polymer/decorators';
 import {Side} from '../../../constants/constants';
-import {
-  PolymerDeepPropertyChange,
-  PolymerSpliceChange,
-} from '@polymer/polymer/interfaces';
 import {CommentRange} from '../../../types/common';
 import {DiffLayer, DiffLayerListener} from '../../../types/types';
 import {isLongCommentRange} from '../gr-diff/gr-diff-utils';
@@ -50,7 +32,9 @@ interface CommentRangeLineLayer {
   hovering: boolean;
   longRange: boolean;
   rootId: string;
+  // start char (0-based)
   start: number;
+  // end char (0-based)
   end: number;
 }
 
@@ -62,40 +46,16 @@ type RangesMap = {
   [side in Side]: LinesMap;
 };
 
-// Polymer 1 adds # before array's key, while Polymer 2 doesn't
-const HOVER_PATH_PATTERN = /^(commentRanges\.#?\d+)\.hovering$/;
-
 const RANGE_BASE_ONLY = 'style-scope gr-diff range';
 const RANGE_HIGHLIGHT = 'style-scope gr-diff range rangeHighlight';
 const HOVER_HIGHLIGHT = 'style-scope gr-diff range rangeHoverHighlight';
 
-@customElement('gr-ranged-comment-layer')
-export class GrRangedCommentLayer extends PolymerElement implements DiffLayer {
-  static get template() {
-    return htmlTemplate;
-  }
+export class GrRangedCommentLayer implements DiffLayer {
+  private knownRanges: CommentRangeLayer[] = [];
 
-  /**
-   * Fired when the range in a range comment was malformed and had to be
-   * normalized.
-   *
-   * It's `detail` has a `lineNum` and `side` parameter.
-   *
-   * @event normalize-range
-   */
+  private listeners: DiffLayerListener[] = [];
 
-  @property({type: Array})
-  commentRanges: CommentRangeLayer[] = [];
-
-  @property({type: Array})
-  _listeners: DiffLayerListener[] = [];
-
-  @property({type: Object})
-  _rangesMap: RangesMap = {left: {}, right: {}};
-
-  get styleModuleName() {
-    return 'gr-ranged-comment-styles';
-  }
+  private rangesMap: RangesMap = {left: {}, right: {}};
 
   /**
    * Layer method to add annotations to a line.
@@ -107,16 +67,16 @@ export class GrRangedCommentLayer extends PolymerElement implements DiffLayer {
     if (
       line.type === GrDiffLineType.REMOVE ||
       (line.type === GrDiffLineType.BOTH &&
-        el.getAttribute('data-side') !== 'right')
+        el.getAttribute('data-side') !== Side.RIGHT)
     ) {
-      ranges = ranges.concat(this._getRangesForLine(line, Side.LEFT));
+      ranges = this.getRangesForLine(line, Side.LEFT);
     }
     if (
       line.type === GrDiffLineType.ADD ||
       (line.type === GrDiffLineType.BOTH &&
-        el.getAttribute('data-side') !== 'left')
+        el.getAttribute('data-side') !== Side.LEFT)
     ) {
-      ranges = ranges.concat(this._getRangesForLine(line, Side.RIGHT));
+      ranges = this.getRangesForLine(line, Side.RIGHT);
     }
 
     for (const range of ranges) {
@@ -137,115 +97,75 @@ export class GrRangedCommentLayer extends PolymerElement implements DiffLayer {
    * Register a listener for layer updates.
    */
   addListener(listener: DiffLayerListener) {
-    this._listeners.push(listener);
+    this.listeners.push(listener);
   }
 
   removeListener(listener: DiffLayerListener) {
-    this._listeners = this._listeners.filter(f => f !== listener);
+    this.listeners = this.listeners.filter(f => f !== listener);
   }
 
   /**
    * Notify Layer listeners of changes to annotations.
    */
-  _notifyUpdateRange(start: number, end: number, side: Side) {
-    for (const listener of this._listeners) {
+  private notifyUpdateRange(start: number, end: number, side: Side) {
+    for (const listener of this.listeners) {
       listener(start, end, side);
     }
   }
 
-  /**
-   * Handle change in the ranges by updating the ranges maps and by
-   * emitting appropriate update notifications.
-   */
-  @observe('commentRanges.*')
-  _handleCommentRangesChange(
-    record: PolymerDeepPropertyChange<
-      CommentRangeLayer[],
-      PolymerSpliceChange<CommentRangeLayer[]>
-    >
-  ) {
-    if (!record) return;
+  updateRanges(newRanges: CommentRangeLayer[]) {
+    const newIds = new Set(newRanges.map(r => r.rootId));
+    const knownIds = new Set(this.knownRanges.map(r => r.rootId));
 
-    // If the entire set of comments was changed.
-    if (record.path === 'commentRanges') {
-      const value = record.value as CommentRangeLayer[];
-      this._rangesMap = {left: {}, right: {}};
-      for (const {side, range, rootId, hovering} of value) {
-        const longRange = isLongCommentRange(range);
-        this._updateRangesMap({
-          side,
-          range,
-          hovering,
-          operation: (forLine, start, end, hovering) => {
-            forLine.push({start, end, hovering, rootId, longRange});
-          },
-        });
-      }
+    for (const newRange of newRanges) {
+      if (!newRange.rootId) continue;
+      if (knownIds.has(newRange.rootId)) continue;
+      this.addRange(newRange);
     }
 
-    // If the change only changed the `hovering` property of a comment.
-    const match = record.path.match(HOVER_PATH_PATTERN);
-    if (match) {
-      // The #number indicates the key of that item in the array
-      // not the index, especially in polymer 1.
-      const {side, range, hovering, rootId} = this.get(match[1]);
-
-      this._updateRangesMap({
-        side,
-        range,
-        hovering,
-        skipLayerUpdate: true,
-        operation: (forLine, start, end, hovering) => {
-          const index = forLine.findIndex(
-            lineRange => lineRange.start === start && lineRange.end === end
-          );
-          forLine[index].hovering = hovering;
-          forLine[index].rootId = rootId;
-        },
-      });
+    for (const knownRange of this.knownRanges) {
+      if (newIds.has(knownRange.rootId)) continue;
+      this.removeRange(knownRange);
     }
 
-    // If comments were spliced in or out.
-    if (record.path === 'commentRanges.splices') {
-      const value = record.value as PolymerSpliceChange<CommentRangeLayer[]>;
-      for (const indexSplice of value.indexSplices) {
-        const removed = indexSplice.removed;
-        for (const {side, range, hovering, rootId} of removed) {
-          this._updateRangesMap({
-            side,
-            range,
-            hovering,
-            operation: (forLine, start, end) => {
-              const index = forLine.findIndex(
-                lineRange =>
-                  lineRange.start === start &&
-                  lineRange.end === end &&
-                  rootId === lineRange.rootId
-              );
-              forLine.splice(index, 1);
-            },
-          });
-        }
-        const added = indexSplice.object.slice(
-          indexSplice.index,
-          indexSplice.index + indexSplice.addedCount
-        );
-        for (const {side, range, hovering, rootId} of added) {
-          const longRange = isLongCommentRange(range);
-          this._updateRangesMap({
-            side,
-            range,
-            hovering,
-            operation: (forLine, start, end, hovering) => {
-              forLine.push({start, end, hovering, rootId, longRange});
-            },
-          });
-        }
-      }
-    }
+    this.knownRanges = [...newRanges];
   }
 
-  _updateRangesMap(options: {
+  private addRange(commentRange: CommentRangeLayer) {
+    const {side, range, rootId, hovering} = commentRange;
+    const longRange = isLongCommentRange(range);
+    this.updateRangesMap({
+      side,
+      range,
+      hovering,
+      operation: (forLine, startChar, endChar, hovering) => {
+        forLine.push({
+          start: startChar,
+          end: endChar,
+          hovering,
+          rootId,
+          longRange,
+        });
+      },
+    });
+  }
+
+  private removeRange(commentRange: CommentRangeLayer) {
+    const {side, range, rootId, hovering} = commentRange;
+    this.updateRangesMap({
+      side,
+      range,
+      hovering,
+      operation: forLine => {
+        const index = forLine.findIndex(
+          lineRange => rootId === lineRange.rootId
+        );
+        forLine.splice(index, 1);
+      },
+    });
+  }
+
+  private updateRangesMap(options: {
     side: Side;
     range: CommentRange;
     hovering: boolean;
@@ -255,25 +175,23 @@ export class GrRangedCommentLayer extends PolymerElement implements DiffLayer {
       end: number,
       hovering: boolean
     ) => void;
-    skipLayerUpdate?: boolean;
   }) {
-    const {side, range, hovering, operation, skipLayerUpdate} = options;
-    const forSide = this._rangesMap[side] || (this._rangesMap[side] = {});
+    const {side, range, hovering, operation} = options;
+    const forSide = this.rangesMap[side] || (this.rangesMap[side] = {});
     for (let line = range.start_line; line <= range.end_line; line++) {
       const forLine = forSide[line] || (forSide[line] = []);
       const start = line === range.start_line ? range.start_character : 0;
       const end = line === range.end_line ? range.end_character : -1;
       operation(forLine, start, end, hovering);
     }
-    if (!skipLayerUpdate) {
-      this._notifyUpdateRange(range.start_line, range.end_line, side);
-    }
+    this.notifyUpdateRange(range.start_line, range.end_line, side);
   }
 
-  _getRangesForLine(line: GrDiffLine, side: Side) {
+  // visible for testing
+  getRangesForLine(line: GrDiffLine, side: Side): CommentRangeLineLayer[] {
     const lineNum = side === Side.LEFT ? line.beforeNumber : line.afterNumber;
-    const ranges: CommentRangeLineLayer[] =
-      this.get(['_rangesMap', side, lineNum]) || [];
+    if (lineNum === 'FILE' || lineNum === 'LOST') return [];
+    const ranges: CommentRangeLineLayer[] = this.rangesMap[side][lineNum] || [];
     return (
       ranges
         .map(range => {
@@ -287,13 +205,6 @@ export class GrRangedCommentLayer extends PolymerElement implements DiffLayer {
           // @see Issue 5744
           if (range.start >= range.end && range.start < line.text.length) {
             range.end = line.text.length;
-            this.dispatchEvent(
-              new CustomEvent('normalize-range', {
-                bubbles: true,
-                composed: true,
-                detail: {lineNum, side},
-              })
-            );
           }
 
           return range;
@@ -301,11 +212,5 @@ export class GrRangedCommentLayer extends PolymerElement implements DiffLayer {
         // Sort the ranges so that hovering highlights are on top.
         .sort((a, b) => (a.hovering && !b.hovering ? 1 : 0))
     );
-  }
-}
-
-declare global {
-  interface HTMLElementTagNameMap {
-    'gr-ranged-comment-layer': GrRangedCommentLayer;
   }
 }

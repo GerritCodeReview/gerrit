bdd9673c4caad2ba3c17cb7d79ad5a9b439651ad
Edwin Kempin <ekempin@google.com>
2022-04-05 09:54:30 +0200

    Use copy condition instead of boolean flags for Verified label defition
    There is an init step to optionally configure the Verified label. This
    label should be setup with a copy conditon rather than the boolean flags
    since the boolean flags are deprecated and should no longer be used.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I8c92f7fe8ceef7410c0ebc3a4e0a42d8a4fa8ea6
    Release-Notes: skip

--- java/com/google/gerrit/pgm/init/InitLabels.java
+++ java/com/google/gerrit/pgm/init/InitLabels.java
@@ -26,7 +26,7 @@ import org.eclipse.jgit.lib.Config;
 
 @Singleton
 public class InitLabels implements InitStep {
-  private static final String KEY_COPY_ALL_SCORES_IF_NO_CODE_CHANGE = "copyAllScoresIfNoCodeChange";
+  private static final String KEY_COPY_CONDITION = "copyCondition";
   private static final String KEY_LABEL = "label";
   private static final String KEY_FUNCTION = "function";
   private static final String KEY_VALUE = "value";
@@ -62,7 +62,7 @@ public class InitLabels implements InitStep {
           LABEL_VERIFIED,
           KEY_VALUE,
           Arrays.asList("-1 Fails", "0 No score", "+1 Verified"));
-      cfg.setBoolean(KEY_LABEL, LABEL_VERIFIED, KEY_COPY_ALL_SCORES_IF_NO_CODE_CHANGE, true);
+      cfg.setString(KEY_LABEL, LABEL_VERIFIED, KEY_COPY_CONDITION, "changekind:NO_CODE_CHANGE");
       allProjectsConfig.save("Configure 'Verified' label");
     }
   }

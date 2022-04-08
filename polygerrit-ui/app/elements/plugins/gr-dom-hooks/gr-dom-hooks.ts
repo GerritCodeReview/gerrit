bc338c689c37e92f4218ecee20253951256975bf
Ben Rohlfs <brohlfs@google.com>
2022-04-08 06:40:53 +0000

    Migrate gr-dom-hook placeholder to lit (2nd attempt)
    The first attempt was change 334342. This was rolled back, because it
    broke plugins using plugin popups. Before that change a PolymerElement
    was used without a template() method, which meant that no shadow DOM
    was created. But the LitElement would create a shadow DOM and thus not
    allow children being appended directly.
    
    Change-Id: Icd57b7e57dfecd49e3ad40261ae5acb89bda8657

--- polygerrit-ui/app/elements/plugins/gr-dom-hooks/gr-dom-hooks.ts
+++ polygerrit-ui/app/elements/plugins/gr-dom-hooks/gr-dom-hooks.ts
@@ -1,22 +1,12 @@
 /**
  * @license
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
  */
-import {PolymerElement} from '@polymer/polymer/polymer-element';
 import {PluginApi} from '../../../api/plugin';
 import {HookApi, HookCallback, PluginElement} from '../../../api/hook';
+import {html, LitElement} from 'lit';
+import {property} from 'lit/decorators';
 
 export class GrDomHooksManager {
   private hooks: Record<string, GrDomHook<PluginElement>>;
@@ -81,20 +71,23 @@ export class GrDomHook<T extends PluginElement> implements HookApi<T> {
   }
 
   _createPlaceholder(hookName: string) {
-    class HookPlaceholder extends PolymerElement {
-      static get is() {
-        return hookName;
-      }
+    /**
+     * See gr-endpoint-decorator.ts for how hooks are instantiated and
+     * initialized.
+     */
+    class HookPlaceholder extends LitElement {
+      @property({type: Object})
+      plugin?: PluginApi;
 
-      static get properties() {
-        return {
-          plugin: Object,
-          content: Object,
-        };
+      @property({type: Object})
+      content?: Element | null;
+
+      override createRenderRoot() {
+        return this;
       }
     }
 
-    customElements.define(HookPlaceholder.is, HookPlaceholder);
+    customElements.define(hookName, HookPlaceholder);
   }
 
   handleInstanceDetached(instance: T) {

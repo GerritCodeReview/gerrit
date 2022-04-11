1e1b4cc5c3e7c32b06da6ed123efb1dc6c20b9ab
Milutin Kristofic <milutin@google.com>
2022-04-11 14:52:25 +0200

    gr-account-info to lit
    Change-Id: Id09f79d3c2c135bbe471a1e095175014d857172e

--- polygerrit-ui/app/elements/settings/gr-account-info/gr-account-info.ts
+++ polygerrit-ui/app/elements/settings/gr-account-info/gr-account-info.ts
@@ -26,6 +26,8 @@ import {AccountDetailInfo, ServerInfo} from '../../../types/common';
 import {EditableAccountField} from '../../../constants/constants';
 import {getAppContext} from '../../../services/app-context';
 import {fireEvent} from '../../../utils/event-util';
+import {css, html} from 'lit';
+import {sharedStyles} from '../../../styles/shared-styles';
 
 @customElement('gr-account-info')
 export class GrAccountInfo extends PolymerElement {
@@ -93,6 +95,120 @@ export class GrAccountInfo extends PolymerElement {
   _avatarChangeUrl = '';
 
   private readonly restApiService = getAppContext().restApiService;
+
+  static styles = [
+    sharedStyles,
+    formStyles,
+    css`
+      gr-avatar {
+        height: 120px;
+        width: 120px;
+        margin-right: var(--spacing-xs);
+        vertical-align: -0.25em;
+      }
+      div section.hide {
+        display: none;
+      }
+    `,
+  ];
+
+  render() {
+    return html`<div class="gr-form-styles">
+      <section>
+        <span class="title"></span>
+        <span class="value">
+          <gr-avatar account="[[_account]]" imageSize="120"></gr-avatar>
+        </span>
+      </section>
+      <section class$="[[_hideAvatarChangeUrl(_avatarChangeUrl)]]">
+        <span class="title"></span>
+        <span class="value">
+          <a href$="[[_avatarChangeUrl]]"> Change avatar </a>
+        </span>
+      </section>
+      <section>
+        <span class="title">ID</span>
+        <span class="value">[[_account._account_id]]</span>
+      </section>
+      <section>
+        <span class="title">Email</span>
+        <span class="value">[[_account.email]]</span>
+      </section>
+      <section>
+        <span class="title">Registered</span>
+        <span class="value">
+          <gr-date-formatter
+            withTooltip
+            date-str="[[_account.registered_on]]"
+          ></gr-date-formatter>
+        </span>
+      </section>
+      <section id="usernameSection">
+        <span class="title">Username</span>
+        <span hidden$="[[usernameMutable]]" class="value">[[_username]]</span>
+        <span hidden$="[[!usernameMutable]]" class="value">
+          <iron-input
+            on-keydown="_handleKeydown"
+            bind-value="{{_username}}"
+            id="usernameIronInput"
+          >
+            <input
+              id="usernameInput"
+              disabled="[[_saving]]"
+              on-keydown="_handleKeydown"
+            />
+          </iron-input>
+        </span>
+      </section>
+      <section id="nameSection">
+        <label class="title" for="nameInput">Full name</label>
+        <span hidden$="[[nameMutable]]" class="value">[[_account.name]]</span>
+        <span hidden$="[[!nameMutable]]" class="value">
+          <iron-input
+            on-keydown="_handleKeydown"
+            bind-value="{{_account.name}}"
+            id="nameIronInput"
+          >
+            <input
+              id="nameInput"
+              disabled="[[_saving]]"
+              on-keydown="_handleKeydown"
+            />
+          </iron-input>
+        </span>
+      </section>
+      <section>
+        <label class="title" for="displayNameInput">Display name</label>
+        <span class="value">
+          <iron-input
+            on-keydown="_handleKeydown"
+            bind-value="{{_account.display_name}}"
+          >
+            <input
+              id="displayNameInput"
+              disabled="[[_saving]]"
+              on-keydown="_handleKeydown"
+            />
+          </iron-input>
+        </span>
+      </section>
+      <section>
+        <label class="title" for="statusInput">About me (e.g. employer)</label>
+        <span class="value">
+          <iron-input
+            on-keydown="_handleKeydown"
+            bind-value="{{_account.status}}"
+          >
+            <input
+              id="statusInput"
+              disabled="[[_saving]]"
+              on-keydown="_handleKeydown"
+            />
+          </iron-input>
+        </span>
+      </section>
+    </div>`;
+  }
 
   loadData() {
     const promises = [];

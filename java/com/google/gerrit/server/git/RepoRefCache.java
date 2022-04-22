9741ca39e7df02d9c6f1df51287a06fa27da0a6d
Luca Milanesio <luca.milanesio@gmail.com>
2022-04-22 08:36:43 +0100

    Revert "Cache change /meta ref SHA1 for each REST API request"
    This reverts commit 23db8a546b800b2c91629d80b6f5117ff635238a.
    
    Reason: refs caching on stable-3.2 can be achieve using [1].
    
    [1] https://gerrit.googlesource.com/modules/cached-refdb/
    
    Release-Notes: skip
    Change-Id: I44ad6fbe165bed57c3d639a6a0878f704139b7b3

--- java/com/google/gerrit/server/git/RepoRefCache.java
+++ java/com/google/gerrit/server/git/RepoRefCache.java
@@ -14,7 +14,6 @@
 
 package com.google.gerrit.server.git;
 
-import com.google.gerrit.server.cache.PerThreadCache;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
@@ -29,17 +28,6 @@ import org.eclipse.jgit.lib.Repository;
 public class RepoRefCache implements RefCache {
   private final RefDatabase refdb;
   private final Map<String, Optional<ObjectId>> ids;
-
-  public static Optional<RefCache> getOptional(Repository repo) {
-    PerThreadCache cache = PerThreadCache.get();
-    if (cache != null && cache.hasReadonlyRequest()) {
-      return Optional.of(
-          cache.get(
-              PerThreadCache.Key.create(RepoRefCache.class, repo), () -> new RepoRefCache(repo)));
-    }
-
-    return Optional.empty();
-  }
 
   public RepoRefCache(Repository repo) {
     this.refdb = repo.getRefDatabase();

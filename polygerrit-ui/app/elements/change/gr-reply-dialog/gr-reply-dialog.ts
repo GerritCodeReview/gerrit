7281dbbaf7ade0ad9d90b8e2ed88544e407d1ab5
Frank Borden <frankborden@google.com>
2022-05-16 10:52:54 +0200

    Confirm dialog for large groups in reviewer flow
    This dialog matches the same confirm flow in the reply-dialog.
    
    https://imgur.com/a/al7n41m
    
    Release-Notes: skip
    Change-Id: I11273b3872dada2fade2d7be822627c1c01c40f9

--- polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.ts
+++ polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.ts
@@ -1276,6 +1276,8 @@ export class GrReplyDialog extends LitElement {
     return selectorEl?.selectedValue;
   }
 
+  // TODO: Combine logic into handleReviewersChanged & handleCCsChanged and
+  // remove account-added event from GrAccountList.
   accountAdded(e: CustomEvent<AccountInputDetail>) {
     const account = e.detail.account;
     const key = accountOrGroupKey(account);

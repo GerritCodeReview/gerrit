0bedc9187fca14a63ae981f0fcde7b9105a77224
Adithya Chakilam <adithya.chakilam@linaro.org>
2022-05-19 02:21:07 +0530

    Use sane value for QueryProcessor's effective limit on "--no-limit"
    Currently, we set Integer.MAX_VAL as the effective limit for an index
    query processor when "--no-limit" option is specified. This throws
    following exceptions on ElasticSearch index backend:
    
         Result window is too large, from + size must be less
         than or equal to: [2147483647] but was [2147501664]
    
    Modify it make use of current index size + some extra buffer so that
    ES server-side memory use is appropriately sized. Also add the API
    in RepoSequence to return the last returned id.
    
    Change-Id: I521b55a7c14c3780d1a91eb102cd56b1122e2061
    Release-Notes: Fixed ES 'Result window is too large' error with --no-limit query option

--- java/com/google/gerrit/server/notedb/Sequences.java
+++ java/com/google/gerrit/server/notedb/Sequences.java
@@ -55,6 +55,9 @@ public class Sequences {
   private final RepoSequence changeSeq;
   private final RepoSequence groupSeq;
   private final Timer2<SequenceType, Boolean> nextIdLatency;
+  private final int accountBatchSize;
+  private final int changeBatchSize;
+  private final int groupBatchSize = 1;
 
   @Inject
   public Sequences(
@@ -65,7 +68,7 @@ public class Sequences {
       AllUsersName allUsers,
       MetricMaker metrics) {
 
-    int accountBatchSize =
+    accountBatchSize =
         cfg.getInt(
             SECTION_NOTEDB,
             NAME_ACCOUNTS,
@@ -80,7 +83,7 @@ public class Sequences {
             () -> FIRST_ACCOUNT_ID,
             accountBatchSize);
 
-    int changeBatchSize =
+    changeBatchSize =
         cfg.getInt(
             SECTION_NOTEDB,
             NAME_CHANGES,
@@ -95,7 +98,6 @@ public class Sequences {
             () -> FIRST_CHANGE_ID,
             changeBatchSize);
 
-    int groupBatchSize = 1;
     groupSeq =
         new RepoSequence(
             repoManager,
@@ -144,6 +146,18 @@ public class Sequences {
     }
   }
 
+  public int changeBatchSize() {
+    return changeBatchSize;
+  }
+
+  public int groupBatchSize() {
+    return groupBatchSize;
+  }
+
+  public int accountBatchSize() {
+    return accountBatchSize;
+  }
+
   public int currentChangeId() {
     return changeSeq.current();
   }
@@ -154,6 +168,18 @@ public class Sequences {
 
   public int currentGroupId() {
     return groupSeq.current();
+  }
+
+  public int lastChangeId() {
+    return changeSeq.last();
+  }
+
+  public int lastGroupId() {
+    return groupSeq.last();
+  }
+
+  public int lastAccountId() {
+    return accountSeq.last();
   }
 
   public void setChangeIdValue(int value) {

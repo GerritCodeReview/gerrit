0f0403c5fad9a4f536c32c7a76bb81b06d00039c
Mark Bekhet <mark.a.bekhet@ericsson.com>
2022-05-24 13:52:03 -0400

    Return a list of projects in response to the match operator
    Before this change, the Rest Api call sent from the client side was
    using the query to return matching projects. This caused a regression
    bug because the behaviour in Polygerrit was different than the behaviour
    on old deprecated GWT, more unrelated projects were returned from the
    filtering.
    
    This change restores the use of the matching operator by adapting it to
    return a list which can be used by the front-end to display the filtered
    projects instead of returning a map.
    
    This change removes the second filter previously applied in [1] as it is
    not needed.
    
    [1] https://gerrit-review.googlesource.com/c/gerrit/+/299682
    
    Bug: Issue 15134
    Change-Id: I1d9a03c4e9421d96c82dc45bae291fc6de3837cd

--- polygerrit-ui/app/elements/admin/gr-repo-list/gr-repo-list.ts
+++ polygerrit-ui/app/elements/admin/gr-repo-list/gr-repo-list.ts
@@ -144,9 +144,6 @@ export class GrRepoList extends ListViewMixin(PolymerElement) {
         if (filter !== this._filter || !repos) {
           return;
         }
-        this._repos = repos.filter(repo =>
-          repo.name.toLowerCase().includes(filter.toLowerCase())
-        );
         this._loading = false;
       });
   }

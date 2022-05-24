095bde5243c04377224ce1ab7adc3ed5d011cada
Mark Bekhet <mark.a.bekhet@ericsson.com>
2022-05-24 13:52:03 -0400

    Return a list of projects in response to the match operator
    Before this change, the Rest Api call sent from client side was using
    the query to return matching projects. This caused a regression bug
    because the behavior in Polygerrit was different than the behavior on
    old deprecated GWT, more unrelated projects were returned from the
    filtering.
    
    This change restores the use of the matching operator by adapting it to
    return a list which can be used by the front-end instead of returning a
    map.
    
    Change-Id: I1d9a03c4e9421d96c82dc45bae291fc6de3837cd

--- java/com/google/gerrit/server/restapi/project/ListProjects.java
+++ java/com/google/gerrit/server/restapi/project/ListProjects.java
@@ -318,7 +318,7 @@ public class ListProjects implements RestReadView<TopLevelResource> {
               .setContentType("text/plain")
               .setCharacterEncoding(UTF_8));
     }
-    return Response.ok(apply());
+    return Response.ok(applyAsList());
   }
 
   public SortedMap<String, ProjectInfo> apply()
@@ -330,6 +330,17 @@ public class ListProjects implements RestReadView<TopLevelResource> {
 
     format = OutputFormat.JSON;
     return display(null);
+  }
+
+  private List<ProjectInfo> applyAsList() throws BadRequestException, PermissionBackendException {
+    SortedMap<String, ProjectInfo> result = apply();
+    List<ProjectInfo> output = new ArrayList<>();
+    for (Map.Entry<String, ProjectInfo> entry : result.entrySet()) {
+      ProjectInfo project = entry.getValue();
+      project.name = entry.getKey();
+      output.add(project);
+    }
+    return output;
   }
 
   private Optional<String> expressAsProjectsQuery() {

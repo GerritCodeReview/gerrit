c200804966bfc0823346cbe45c50acf0a7a3697c
Ben Rohlfs <brohlfs@google.com>
2022-05-25 10:42:03 +0200

    Refactor gr-file-status-chip
    Release-Notes: skip
    Change-Id: Ice4bcdc7b8d2253915dc5cc214b7c91b6b6da042

--- polygerrit-ui/app/elements/shared/gr-file-status-chip/gr-file-status-chip.ts
+++ polygerrit-ui/app/elements/shared/gr-file-status-chip/gr-file-status-chip.ts
@@ -1,35 +1,13 @@
 /**
  * @license
- * Copyright (C) 2015 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2015 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
  */
-import {SpecialFilePath} from '../../../constants/constants';
+import {FileInfoStatus, SpecialFilePath} from '../../../constants/constants';
 import {NormalizedFileInfo} from '../../change/gr-file-list/gr-file-list';
-import {hasOwnProperty} from '../../../utils/common-util';
 import {sharedStyles} from '../../../styles/shared-styles';
 import {LitElement, css, html} from 'lit';
 import {customElement, property} from 'lit/decorators';
-
-const FileStatus = {
-  A: 'Added',
-  C: 'Copied',
-  D: 'Deleted',
-  M: 'Modified',
-  R: 'Renamed',
-  W: 'Rewritten',
-  U: 'Unchanged',
-};
 
 @customElement('gr-file-status-chip')
 export class GrFileStatusChip extends LitElement {
@@ -67,12 +45,12 @@ export class GrFileStatusChip extends LitElement {
 
   override render() {
     return html` <span
-      class=${this._computeStatusClass(this.file)}
+      class=${this.computeStatusClass(this.file)}
       tabindex="0"
-      title=${this._computeFileStatusLabel(this.file?.status)}
-      aria-label=${this._computeFileStatusLabel(this.file?.status)}
+      title=${this.computeLabel(this.file?.status)}
+      aria-label=${this.computeLabel(this.file?.status)}
     >
-      ${this._computeFileStatusLabel(this.file?.status)}
+      ${this.computeLabel(this.file?.status)}
     </span>`;
   }
 
@@ -80,14 +58,29 @@ export class GrFileStatusChip extends LitElement {
    * Get a descriptive label for use in the status indicator's tooltip and
    * ARIA label.
    */
-  _computeFileStatusLabel(status?: keyof typeof FileStatus) {
-    const statusCode = this._computeFileStatus(status);
-    return hasOwnProperty(FileStatus, statusCode)
-      ? FileStatus[statusCode]
-      : 'Status Unknown';
+  // visible for testing
+  computeLabel(status?: FileInfoStatus) {
+    const statusCode = this.computeFileStatus(status);
+    switch (statusCode) {
+      case FileInfoStatus.ADDED:
+        return 'Added';
+      case FileInfoStatus.COPIED:
+        return 'Copied';
+      case FileInfoStatus.DELETED:
+        return 'Deleted';
+      case FileInfoStatus.MODIFIED:
+        return 'Modified';
+      case FileInfoStatus.RENAMED:
+        return 'Renamed';
+      case FileInfoStatus.REWRITTEN:
+        return 'Rewritten';
+      case FileInfoStatus.UNMODIFIED:
+        return 'Unchanged';
+    }
   }
 
-  _computeClass(baseClass?: string, path?: string) {
+  // visible for testing
+  computeClass(baseClass?: string, path?: string) {
     const classes = [];
     if (baseClass) {
       classes.push(baseClass);
@@ -101,16 +94,15 @@ export class GrFileStatusChip extends LitElement {
     return classes.join(' ');
   }
 
-  _computeFileStatus(
-    status?: keyof typeof FileStatus
-  ): keyof typeof FileStatus {
-    return status || 'M';
+  // visible for testing
+  computeFileStatus(status?: FileInfoStatus): FileInfoStatus {
+    return status || FileInfoStatus.MODIFIED;
   }
 
-  _computeStatusClass(file?: NormalizedFileInfo) {
+  private computeStatusClass(file?: NormalizedFileInfo) {
     if (!file) return '';
-    const classStr = this._computeClass('status', file.__path);
-    return `${classStr} ${this._computeFileStatus(file.status)}`;
+    const classStr = this.computeClass('status', file.__path);
+    return `${classStr} ${this.computeFileStatus(file.status)}`;
   }
 }
 
--- polygerrit-ui/app/elements/shared/gr-file-status-chip/gr-file-status-chip_test.ts
+++ polygerrit-ui/app/elements/shared/gr-file-status-chip/gr-file-status-chip_test.ts
@@ -1,46 +1,56 @@
 /**
  * @license
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
  */
-
 import '../../../test/common-test-setup-karma';
 import './gr-file-status-chip';
 import {GrFileStatusChip} from './gr-file-status-chip';
-
-const fixture = fixtureFromElement('gr-file-status-chip');
+import {fixture} from '@open-wc/testing-helpers';
+import {FileInfo} from '../../../api/rest-api';
+import {NormalizedFileInfo} from '../../change/gr-file-list/gr-file-list';
 
 suite('gr-file-status-chip tests', () => {
   let element: GrFileStatusChip;
 
-  setup(() => {
-    element = fixture.instantiate();
-  });
-
-  test('computed properties', () => {
-    assert.equal(element._computeFileStatus('A'), 'A');
-    assert.equal(element._computeFileStatus(undefined), 'M');
-
-    assert.equal(element._computeClass('clazz', '/foo/bar/baz'), 'clazz');
-    assert.equal(
-      element._computeClass('clazz', '/COMMIT_MSG'),
-      'clazz invisible'
+  setup(async () => {
+    element = await fixture<GrFileStatusChip>(
+      '<gr-file-status-chip></gr-file-status-chip>'
     );
+    await element.updateComplete;
   });
 
-  test('_computeFileStatusLabel', () => {
-    assert.equal(element._computeFileStatusLabel('A'), 'Added');
-    assert.equal(element._computeFileStatusLabel('M'), 'Modified');
+  const setFile = async (file: Partial<NormalizedFileInfo>) => {
+    element.file = {
+      ...createFileInfo(),
+      ...file,
+    };
+    await element.updateComplete;
+  };
+
+  suite('semantic dom diff tests', () => {
+    test('modified', () => {
+      expect(element).shadowDom.to.equal(/* HTML */ `
+        <span aria-label="Modified" tabindex="0" title="Modified"
+          >Modified</span
+        >
+      `);
+    });
+
+    test('computed properties', () => {
+      assert.equal(element.computeFileStatus('A'), 'A');
+      assert.equal(element.computeFileStatus(undefined), 'M');
+
+      assert.equal(element.computeClass('clazz', '/foo/bar/baz'), 'clazz');
+      assert.equal(
+        element.computeClass('clazz', '/COMMIT_MSG'),
+        'clazz invisible'
+      );
+    });
+
+    test('computeFileStatusLabel', () => {
+      assert.equal(element.computeLabel('A'), 'Added');
+      assert.equal(element.computeLabel('M'), 'Modified');
+    });
   });
 });

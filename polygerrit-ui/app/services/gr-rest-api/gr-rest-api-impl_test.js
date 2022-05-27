a3d08e25e0c022b9b2bfe702fef43f90896ffdac
Ben Rohlfs <brohlfs@google.com>
2022-05-27 16:07:27 +0200

    Refactoring: Replace `EditPatchSetNum` with EDIT
    Release-Notes: skip
    Change-Id: I9cd5ac3fa7b99b542a663b63618cddb40a7b5730

--- polygerrit-ui/app/services/gr-rest-api/gr-rest-api-impl_test.js
+++ polygerrit-ui/app/services/gr-rest-api/gr-rest-api-impl_test.js
@@ -656,11 +656,11 @@ suite('gr-rest-api-service-impl tests', () => {
   test('queryChangeFiles', () => {
     const fetchStub = sinon.stub(element, '_getChangeURLAndFetch')
         .returns(Promise.resolve());
-    return element.queryChangeFiles('42', 'edit', 'test/path.js').then(() => {
+    return element.queryChangeFiles('42', EDIT, 'test/path.js').then(() => {
       assert.equal(fetchStub.lastCall.args[0].changeNum, '42');
       assert.equal(fetchStub.lastCall.args[0].endpoint,
           '/files?q=test%2Fpath.js');
-      assert.equal(fetchStub.lastCall.args[0].revision, 'edit');
+      assert.equal(fetchStub.lastCall.args[0].revision, EDIT);
     });
   });
 
@@ -1278,7 +1278,7 @@ suite('gr-rest-api-service-impl tests', () => {
     const getChangeEditFilesStub = sinon.stub(element, 'getChangeEditFiles')
         .returns(Promise.resolve({}));
 
-    return fn('1', {patchNum: 'edit'}).then(() => {
+    return fn('1', {patchNum: EDIT}).then(() => {
       assert.isTrue(getChangeEditFilesStub.calledOnce);
       assert.isFalse(getChangeFilesStub.called);
       return fn('1', {patchNum: '1'}).then(() => {

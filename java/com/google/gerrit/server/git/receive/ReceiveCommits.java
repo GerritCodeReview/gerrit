228e6b14b8add897484e9184c3da4ea28991abf1
Luca Milanesio <luca.milanesio@gmail.com>
2022-05-29 21:56:14 +0100

    Lazy load change notes when submit by push
    Lazy load the change notes associated with the change to merge
    when one or more changes are submitted by a git push of
    the change onto its target branch.
    
    When a single change is merged through git push, we need to find
    which one is the corresponding change meta to be checked and
    merged. However, the building of the entire set of open changes
    against the target branch, indexed by their change key was having
    the side-effect of loading eagerly changes notes from their /meta ref
    
    Loading all changes notes can be very slow, O(mins), on
    repositories with a large number of refs on slow storage and, also,
    it is completely unneeded in this use case.
    
    The consequence of this full scanning was the increase of the
    time to submit a change from a few seconds (via the GUI) to a
    few minutes (via git push).
    
    Change-Id: I646bc7b13f3359816b6be6354e15ddb5412bfa38

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -3105,7 +3105,7 @@ class ReceiveCommits {
               }
 
               ListMultimap<ObjectId, Ref> byCommit = changeRefsById();
-              Map<Change.Key, ChangeNotes> byKey = null;
+              Map<Change.Key, ChangeData> byKey = null;
               List<ReplaceRequest> replaceAndClose = new ArrayList<>();
 
               int existingPatchSets = 0;
@@ -3132,13 +3132,14 @@ class ReceiveCommits {
                     byKey = executeIndexQuery(() -> openChangesByKeyByBranch(branch));
                   }
 
-                  ChangeNotes onto = byKey.get(new Change.Key(changeId.trim()));
+                  ChangeData onto = byKey.get(new Change.Key(changeId.trim()));
                   if (onto != null) {
                     newPatchSets++;
                     // Hold onto this until we're done with the walk, as the call to
                     // req.validate below calls isMergedInto which resets the walk.
-                    ReplaceRequest req = new ReplaceRequest(onto.getChangeId(), c, cmd, false);
-                    req.notes = onto;
+                    ChangeNotes ontoNotes = onto.notes();
+                    ReplaceRequest req = new ReplaceRequest(ontoNotes.getChangeId(), c, cmd, false);
+                    req.notes = ontoNotes;
                     replaceAndClose.add(req);
                     continue COMMIT;
                   }
@@ -3207,12 +3208,12 @@ class ReceiveCommits {
     }
   }
 
-  private Map<Change.Key, ChangeNotes> openChangesByKeyByBranch(Branch.NameKey branch)
+  private Map<Change.Key, ChangeData> openChangesByKeyByBranch(Branch.NameKey branch)
       throws OrmException {
-    Map<Change.Key, ChangeNotes> r = new HashMap<>();
+    Map<Change.Key, ChangeData> r = new HashMap<>();
     for (ChangeData cd : queryProvider.get().byBranchOpen(branch)) {
       try {
-        r.put(cd.change().getKey(), cd.notes());
+        r.put(cd.change().getKey(), cd);
       } catch (NoSuchChangeException e) {
         // Ignore deleted change
       }

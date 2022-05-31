4e4041d79788e1bc2f05c8867b0a1f08c0c95151
Youssef Elghareeb <ghareeb@google.com>
2022-05-31 21:53:06 +0200

    Wrap Enums.stringConverter in a SafeEnumStringConverter
    Serializing enum values to proto as strings was dangerous. During
    deserialization, we use Enums.stringConverter to convert the serialized
    string values to their corresponding enum constants. If the string value
    did not exist in the enum, an IllegalArgumentException was thrown.
    
    In this change, we wrap the implementation of the string converter in a
    safe enum string converter. The wrapper falls back to the default
    (first) enum value if the string does not exist. That's the same
    behaviour that proto best practices recommend (using the first enum
    value as default).
    
    In this change, we migrate LabelTypeSerializer to the safe string
    converter. In a follow-up change, we will move other enum serialized
    entities to the new wrapper.
    
    This change will prevent outages resulting from adding new enum values.
    The outage happened in the past when different binaries were running
    simultaneously, with new binaries writing the new enum value and old
    binaries fail to parse it.
    
    Release-Notes: skip
    Change-Id: I6f3378a49615f9737858352a9e19aeef3e931bb6

---
+++ javatests/com/google/gerrit/entities/converter/SafeEnumStringConverterTest.java
@@ -0,0 +1,48 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.entities.converter;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+
+/** Test for {@link com.google.gerrit.entities.converter.SafeEnumStringConverter}. */
+public class SafeEnumStringConverterTest {
+  private enum TestEnum {
+    A,
+    B,
+    C
+  }
+
+  private static final SafeEnumStringConverter<TestEnum> converter =
+      new SafeEnumStringConverter<>(TestEnum.class);
+
+  @Test
+  public void convertDefinedFields() {
+    assertThat(converter.convert("A")).isEqualTo(TestEnum.A);
+    assertThat(converter.convert("B")).isEqualTo(TestEnum.B);
+    assertThat(converter.convert("C")).isEqualTo(TestEnum.C);
+  }
+
+  @Test
+  public void convertUndefinedFields() {
+    assertThat(converter.convert("UNKNOWN")).isEqualTo(TestEnum.A);
+  }
+
+  @Test
+  public void reverseConvert() {
+    assertThat(converter.reverseConvert(TestEnum.A)).isEqualTo("A");
+  }
+}

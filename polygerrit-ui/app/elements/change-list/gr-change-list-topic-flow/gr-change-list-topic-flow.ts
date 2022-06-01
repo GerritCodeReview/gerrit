45ccc66353d0482ba2cb1472352beb70876a40cd
Dhruv <dhruvsri@google.com>
2022-06-01 17:06:35 +0100

    Only show Apply All button if more than one change is selected
    Release-Notes: skip
    Google-bug-id: b/234578022
    Change-Id: Ic0ebb06ba7d9096d268cf65355addc415af8acd8

--- polygerrit-ui/app/elements/change-list/gr-change-list-topic-flow/gr-change-list-topic-flow.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-topic-flow/gr-change-list-topic-flow.ts
@@ -175,7 +175,9 @@ export class GrChangeListTopicFlow extends LitElement {
             flatten
             ?disabled=${this.selectedExistingTopics.size !== 1}
             @click=${this.applyTopicToAll}
-            >Apply to all</gr-button
+            >Apply${this.selectedChanges.length > 1
+              ? ' to all'
+              : nothing}</gr-button
           >
           <gr-button
             id="remove-topics-button"

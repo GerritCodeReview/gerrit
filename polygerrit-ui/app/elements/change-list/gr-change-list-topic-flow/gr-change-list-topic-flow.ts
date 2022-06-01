d2f1b60038d5e82eb3d83962812debe8be5466c9
Dhruv <dhruvsri@google.com>
2022-06-01 18:55:30 +0100

    Add snackbars to hashtag and topic flow
    Release-Notes: skip
    Change-Id: I8c14ba7159884b52314c927f5f5a93c114e38a05

--- polygerrit-ui/app/elements/change-list/gr-change-list-topic-flow/gr-change-list-topic-flow.ts
+++ polygerrit-ui/app/elements/change-list/gr-change-list-topic-flow/gr-change-list-topic-flow.ts
@@ -24,6 +24,8 @@ import {spinnerStyles} from '../../../styles/gr-spinner-styles';
 import {ProgressStatus} from '../../../constants/constants';
 import {allSettled} from '../../../utils/async-util';
 import {fireReload} from '../../../utils/event-util';
+import {fireAlert} from '../../../utils/event-util';
+import {pluralize} from '../../../utils/string-util';
 
 @customElement('gr-change-list-topic-flow')
 export class GrChangeListTopicFlow extends LitElement {
@@ -335,12 +337,11 @@ export class GrChangeListTopicFlow extends LitElement {
 
   private applyTopicToAll() {
     this.loadingText = 'Applying to all';
+    const topic = Array.from(this.selectedExistingTopics.values())[0];
     this.trackPromises(
-      this.selectedChanges.map(change =>
-        this.restApiService.setChangeTopic(
-          change._number,
-          Array.from(this.selectedExistingTopics.values())[0]
-        )
+      this.selectedChanges.map(
+        change => this.restApiService.setChangeTopic(change._number, topic),
+        `${topic} applied to all changes`
       )
     );
   }
@@ -350,17 +351,23 @@ export class GrChangeListTopicFlow extends LitElement {
     this.trackPromises(
       this.selectedChanges.map(change =>
         this.restApiService.setChangeTopic(change._number, this.topicToAdd)
-      )
+      ),
+      `${pluralize(this.selectedChanges.length, 'Change')} added to ${
+        this.topicToAdd
+      }`
     );
   }
 
-  private async trackPromises(promises: Promise<string>[]) {
+  private async trackPromises(promises: Promise<string>[], alert?: string) {
     this.overallProgress = ProgressStatus.RUNNING;
     const results = await allSettled(promises);
     if (results.every(result => result.status === 'fulfilled')) {
       this.overallProgress = ProgressStatus.SUCCESSFUL;
       this.dropdown?.close();
       this.isDropdownOpen = false;
+      if (alert) {
+        fireAlert(this, alert);
+      }
       fireReload(this);
     } else {
       this.overallProgress = ProgressStatus.FAILED;

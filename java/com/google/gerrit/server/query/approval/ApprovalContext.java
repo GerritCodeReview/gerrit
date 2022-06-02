65184135ad9cef6bf06ba8e84b2bcbe6082805eb
Edwin Kempin <ekempin@google.com>
2022-06-02 10:01:14 +0200

    ApprovalCopier, ApprovalContext: Improve readability
    Improve readability by:
    * using more readable variable and method names
    * using explicit vars for source patch set ID, approver and label
      instead of using PatchSetApproval.Key (since the key contains the patch
      set ID it's always bound to a specific patch set, e.g. the source
      patch set, which makes reading code that uses the key hard)
    * passing in LabelType into ApprovalContext (removes the need to handle
      a missing label type in MagicValuePredicate)
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I9077b196f313474f7dbf2f7229f04a58fa3f27c4
    Release-Notes: skip

--- java/com/google/gerrit/server/query/approval/ApprovalContext.java
+++ java/com/google/gerrit/server/query/approval/ApprovalContext.java
@@ -17,8 +17,9 @@ package com.google.gerrit.server.query.approval;
 import static com.google.common.base.Preconditions.checkState;
 
 import com.google.auto.value.AutoValue;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.LabelType;
 import com.google.gerrit.entities.PatchSet;
-import com.google.gerrit.entities.PatchSetApproval;
 import com.google.gerrit.extensions.client.ChangeKind;
 import com.google.gerrit.server.notedb.ChangeNotes;
 import org.eclipse.jgit.lib.Config;
@@ -27,18 +28,21 @@ import org.eclipse.jgit.revwalk.RevWalk;
 /** Entity representing all required information to match predicates for copying approvals. */
 @AutoValue
 public abstract class ApprovalContext {
-  /** Key of the approval on the source patch set to be copied. */
-  public abstract PatchSetApproval.Key patchSetApprovalKey();
+  public abstract PatchSet.Id sourcePatchSetId();
+
+  public abstract Account.Id approverId();
+
+  public abstract LabelType labelType();
 
   /** Value of the approval on the source patch set to be copied. */
-  public abstract short patchSetApprovalValue();
+  public abstract short approvalValue();
 
   /**
    * Target change and patch set for the approval. This must be used instead of getting the PatchSet
    * from {@link #changeNotes()} because it is possible we are now creating the patch-set, so it
    * doesn't exist in changeNotes yet.
    */
-  public abstract PatchSet target();
+  public abstract PatchSet targetPatchSet();
 
   /** {@link ChangeNotes} of the change in question. */
   public abstract ChangeNotes changeNotes();
@@ -57,18 +61,20 @@ public abstract class ApprovalContext {
 
   public static ApprovalContext create(
       ChangeNotes changeNotes,
-      PatchSetApproval.Key psaKey,
-      short psaValue,
-      PatchSet patchSet,
+      PatchSet.Id sourcePatchSetId,
+      Account.Id approverId,
+      LabelType labelType,
+      short approvalValue,
+      PatchSet targetPatchSet,
       ChangeKind changeKind,
       boolean isMerge,
       RevWalk revWalk,
       Config repoConfig) {
     checkState(
-        psaKey.patchSetId().changeId().equals(patchSet.id().changeId()),
+        sourcePatchSetId.changeId().equals(targetPatchSet.id().changeId()),
         "approval and target must be the same change. got: %s, %s",
-        psaKey.patchSetId(),
-        patchSet.id());
+        sourcePatchSetId,
+        targetPatchSet.id());
     // TODO(ekempin): Use checkState to verify that psa.patchSetId().get() + 1 == id.get() so that
     // it's ensured that approvals are only copied to the next consecutive patch set. To add back
     // this verification https://gerrit-review.googlesource.com/c/gerrit/+/312633 can be reverted.
@@ -76,6 +82,15 @@ public abstract class ApprovalContext {
     // are no changes with gaps in patch set numbers. Since it's planned to fix-up old changes with
     // gaps in patch set numbers, this todo is a reminder to add back the check once this is done.
     return new AutoValue_ApprovalContext(
-        psaKey, psaValue, patchSet, changeNotes, changeKind, isMerge, revWalk, repoConfig);
+        sourcePatchSetId,
+        approverId,
+        labelType,
+        approvalValue,
+        targetPatchSet,
+        changeNotes,
+        changeKind,
+        isMerge,
+        revWalk,
+        repoConfig);
   }
 }

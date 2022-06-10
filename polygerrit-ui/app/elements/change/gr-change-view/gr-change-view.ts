9838898f76a9c5e56696872344df8e5166555831
Ben Rohlfs <brohlfs@google.com>
2022-06-10 12:17:28 +0200

    Use new files model in `gr-file-list` element
    Instead of calling the Rest API for getting file information
    `<gr-file-list>` just subscribes to the new files model.
    
    Remove the `reload()` method from `<gr-file-list>`. The model loads
    new file information reactively. No need for telling the file list to
    reload directly.
    
    The files model deals with proper normalization of file infos, and with
    adding files for unmodified (but commented on) files.
    
    Release-Notes: skip
    Change-Id: Ia5d59c176e2740b14a4c484933f035d5ec03e419

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -176,6 +176,7 @@ import {ShortcutController} from '../../lit/shortcut-controller';
 import {FilesExpandedState} from '../gr-file-list-constants';
 import {subscribe} from '../../lit/subscription-controller';
 import {configModelToken} from '../../../models/config/config-model';
+import {filesModelToken} from '../../../models/change/files-model';
 
 const MIN_LINES_FOR_COMMIT_COLLAPSE = 18;
 
@@ -552,6 +553,8 @@ export class GrChangeView extends LitElement {
   readonly getCommentsModel = resolve(this, commentsModelToken);
 
   private readonly getConfigModel = resolve(this, configModelToken);
+
+  private readonly getFilesModel = resolve(this, filesModelToken);
 
   private readonly getShortcutsService = resolve(this, shortcutsServiceToken);
 
@@ -2887,13 +2890,6 @@ export class GrChangeView extends LitElement {
       // have loaded.
       coreDataPromise = Promise.all([patchResourcesLoaded, loadingFlagSet]);
     } else {
-      // Resolves when the file list has loaded.
-      const fileListReload = loadingFlagSet.then(async () => {
-        assertIsDefined(this.fileList);
-        await this.fileList.reload();
-      });
-      allDataPromises.push(fileListReload);
-
       const latestCommitMessageLoaded = loadingFlagSet.then(() => {
         // If the latest commit message is known, there is nothing to do.
         if (this.latestCommitMessage) {
@@ -2942,6 +2938,7 @@ export class GrChangeView extends LitElement {
       return this.getRelatedChangesList()?.reload(relatedChangesPromise);
     });
     allDataPromises.push(relatedChangesLoaded);
+    allDataPromises.push(until(this.getFilesModel().files$, f => f.length > 0));
 
     Promise.all(allDataPromises).then(() => {
       // Loading of commments data is no longer part of this reporting
@@ -2978,12 +2975,7 @@ export class GrChangeView extends LitElement {
   reloadPatchNumDependentResources(patchNumChanged?: boolean) {
     assertIsDefined(this.changeNum, 'changeNum');
     if (!this.patchRange?.patchNum) throw new Error('missing patchNum');
-    // Filelist might not be rendered yet, so we wait for it to be rendered
-    // before we ask it to reload.
-    const promises = [
-      this.loadAndSetCommitInfo(),
-      this.updateComplete.then(() => this.fileList!.reload()),
-    ];
+    const promises = [this.loadAndSetCommitInfo()];
     if (patchNumChanged) {
       promises.push(
         this.getCommentsModel().reloadPortedComments(
--- polygerrit-ui/app/models/change/files-model.ts
+++ polygerrit-ui/app/models/change/files-model.ts
@@ -9,30 +9,65 @@ import {switchMap, map} from 'rxjs/operators';
 import {RestApiService} from '../../services/gr-rest-api/gr-rest-api';
 import {Finalizable} from '../../services/registry';
 import {select} from '../../utils/observable-util';
+import {FileInfoStatus, SpecialFilePath} from '../../constants/constants';
 import {specialFilePathCompare} from '../../utils/path-list-util';
 import {Model} from '../model';
 import {define} from '../dependency';
 import {ChangeModel} from './change-model';
+import {CommentsModel} from '../comments/comments-model';
 
 export interface NormalizedFileInfo extends FileInfo {
   __path: string;
+  // Compared to `FileInfo` these four props are required here.
+  lines_inserted: number;
+  lines_deleted: number;
+  size_delta: number; // in bytes
+  size: number; // in bytes
+}
+
+export function normalize(file: FileInfo, path: string): NormalizedFileInfo {
+  return {
+    __path: path,
+    // These 4 props are required in NormalizedFileInfo, but optional in
+    // FileInfo. So let's set a default value, if not already set.
+    lines_inserted: 0,
+    lines_deleted: 0,
+    size_delta: 0,
+    size: 0,
+    ...file,
+  };
 }
 
 function mapToList(map?: FileNameToFileInfoMap): NormalizedFileInfo[] {
   const list: NormalizedFileInfo[] = [];
   for (const [key, value] of Object.entries(map ?? {})) {
-    list.push({
-      ...value,
-      __path: key,
-      // These 3 props are required in NormalizedFileInfo, but optional in
-      // FileInfo. So let's set a default value, if not already set.
-      lines_inserted: value.lines_inserted ?? 0,
-      lines_deleted: value.lines_deleted ?? 0,
-      size_delta: value.size_delta ?? 0,
-    });
+    list.push(normalize(value, key));
   }
   list.sort((f1, f2) => specialFilePathCompare(f1.__path, f2.__path));
   return list;
+}
+
+export function addUnmodified(
+  files: NormalizedFileInfo[],
+  commentedPaths: string[]
+) {
+  const combined = [...files];
+  for (const commentedPath of commentedPaths) {
+    if (commentedPath === SpecialFilePath.PATCHSET_LEVEL_COMMENTS) continue;
+    if (files.some(f => f.__path === commentedPath)) continue;
+    if (
+      files.some(
+        f => f.status === FileInfoStatus.RENAMED && f.old_path === commentedPath
+      )
+    ) {
+      continue;
+    }
+    combined.push(
+      normalize({status: FileInfoStatus.UNMODIFIED}, commentedPath)
+    );
+  }
+  combined.sort((f1, f2) => specialFilePathCompare(f1.__path, f2.__path));
+  return combined;
 }
 
 export interface FilesState {
@@ -51,21 +86,28 @@ export const filesModelToken = define<FilesModel>('files-model');
 export class FilesModel extends Model<FilesState> implements Finalizable {
   public readonly files$ = select(this.state$, state => state.files);
 
+  public readonly filesWithUnmodified$ = select(
+    combineLatest([this.files$, this.commentsModel.commentedPaths$]),
+    ([files, commentedPaths]) => addUnmodified(files, commentedPaths)
+  );
+
   private subscriptions: Subscription[] = [];
 
   constructor(
     readonly changeModel: ChangeModel,
+    readonly commentsModel: CommentsModel,
     readonly restApiService: RestApiService
   ) {
     super(initialState);
     this.subscriptions = [
       combineLatest([
+        this.changeModel.reload$,
         this.changeModel.changeNum$,
         this.changeModel.basePatchNum$,
         this.changeModel.patchNum$,
       ])
         .pipe(
-          switchMap(([changeNum, basePatchNum, patchNum]) => {
+          switchMap(([_, changeNum, basePatchNum, patchNum]) => {
             if (!changeNum || !patchNum) return of({});
             return from(
               this.restApiService.getChangeOrEditFiles(changeNum, {

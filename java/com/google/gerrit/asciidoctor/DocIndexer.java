0d7550688cefcb5491b740ed46c428efeae50eeb
Antoine Musso <hashar@free.fr>
2022-06-15 14:16:49 +0200

    AsciiDoctor: skip attribute when looking for title
    A document search for "push" would yield search results titled
    ":linkattrs:" instead of the matching document title.
    
    The link attributes have been enabled by inserting them at the very top
    of each document and the DocIndexer uses the first line to determine the
    title (unless it is a [[tag]]).
    
    Also skip lines having an ":attribute:".
    
    Use a while loop to skip several tags/attributes.
    
    We can later on remove ":linkattrs:" which got inserted for Issue 12068
    since AsciiDoctor 1.5.7+ always enables it. However Google source uses
    version 1.5.2.
    
    Bug: Issue 16001
    Change-Id: I10d56273d87d2fb8b09c9327fcff436005ba328a

--- java/com/google/gerrit/asciidoctor/DocIndexer.java
+++ java/com/google/gerrit/asciidoctor/DocIndexer.java
@@ -107,11 +107,18 @@ public class DocIndexer {
 
         String title;
         try (BufferedReader titleReader = Files.newBufferedReader(file.toPath(), UTF_8)) {
-          title = titleReader.readLine();
-          if (title != null && title.startsWith("[[")) {
+          while ((title = titleReader.readLine()) != null) {
             // Generally the first line of the txt is the title. In a few cases the
-            // first line is a "[[tag]]" and the second line is the title.
+            // first lines are "[[tag]]" and or ":attribute:" and the next line
+            // after those  is the title.
+            if (title.startsWith("[[")) {
+              continue;
+            }
+            if (title.startsWith(":") && title.endsWith(":")) {
+              continue;
+            }
             title = titleReader.readLine();
+            break;
           }
         }
         Matcher matcher = SECTION_HEADER.matcher(title);

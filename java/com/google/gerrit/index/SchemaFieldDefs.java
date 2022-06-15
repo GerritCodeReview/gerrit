831d8e3f6c1c33a43f9c4d89680fbb219060c8ab
Marija Savtchouk <mariasavtchouk@google.com>
2022-06-15 06:46:06 +0100

    Rework Scheme FieldDefs to allow separate storage of indexed fields and their corresponding index tokens.
    Currently, we have many fields in indexes that store same data, but are
    used for different searches.  Some examples from the change index:
    EXACT_TOPIC, FUZZY_TOPIC, PREFIX_TOPIC
    
    In our index implementation, we would like to store the actual field
    data once (in index primary storage), and enable different token
    generation strategies on the same field, referencing the base storage
    from the token indexes.
    
    This change adds a new class IndexedField, that is an abstraction for
    the field data, contained in secondary index and the corresponding
    SearchSpec, that represent the way that field is indexed and can be
    searched (which defines how index tokens are generated).
    
    Migrating all Fields to the new format in the single change is not
    possible (we have >64 field just in change index, replacing all of them
    at once will result in a huge change that will be hard to review).  To
    address that,  added an interface SchemaField for old (FieldDef) and new
    (SearchSpec) definitions of the field.
    
    Each new FieldDef will be mapped exactly to a new SearchSpec, and all
    FieldDefs will be eventually removed. See the follow up changes for the
    code migration strategy.
    
    The IndexField logic will be covered by AbstractQuery* tests, same as
    FieldDefs today.
    
    Change-Id: Ie8283411d90a2927c8dd8dc3a2af9e53048c6759
    Release-Notes: skip

---
+++ java/com/google/gerrit/index/SchemaFieldDefs.java
@@ -0,0 +1,90 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.index;
+
+import com.google.gerrit.common.Nullable;
+import java.io.IOException;
+
+/** Interfaces that define properties of fields in {@link Schema}. */
+public class SchemaFieldDefs {
+
+  /**
+   * Definition of a field stored in the secondary index.
+   *
+   * @param <I> input type from which documents are created and search results are returned.
+   * @param <T> type that should be extracted from the input object when converting to an index
+   *     document.
+   */
+  public interface SchemaField<T, I> {
+
+    /** Returns whether the field should be stored in the index. */
+    boolean isStored();
+
+    /** Returns whether the field is repeatable. */
+    boolean isRepeatable();
+
+    /**
+     * Get the field contents from the input object.
+     *
+     * @param input input object.
+     * @return the field value(s) to index.
+     */
+    @Nullable
+    I get(T input);
+
+    /** Returns the name of the field. */
+    String getName();
+
+    /**
+     * Returns type of the field; for repeatable fields, the inner type, not the iterable type.
+     * TODO(mariasavtchuk): remove after migrating to the new field formats
+     */
+    FieldType<?> getType();
+
+    /**
+     * Set the field contents back to an object. Used to reconstruct fields from indexed values.
+     * No-op if the field can't be reconstructed.
+     *
+     * @param object input object.
+     * @param doc indexed document
+     * @return {@code true} if the field was set, {@code false} otherwise
+     */
+    boolean setIfPossible(T object, StoredValue doc);
+  }
+
+  /**
+   * Getter to extract value that should be stored in index from the input object.
+   *
+   * @param <I> type from which documents are created and search results are returned.
+   * @param <T> type that should be extracted from the input object to an index field.
+   */
+  @FunctionalInterface
+  public interface Getter<I, T> {
+    @Nullable
+    T get(I input) throws IOException;
+  }
+
+  /**
+   * Setter to reconstruct fields from indexed values back to an object.
+   *
+   * @param <I> type from which documents are created and search results are returned.
+   * @param <T> type that should be extracted from the input object when converting toto an index
+   *     field.
+   */
+  @FunctionalInterface
+  public interface Setter<I, T> {
+    void set(I object, T value);
+  }
+}

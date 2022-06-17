c7fac7181c52a168c79cddc87ff45adcc69fcfab
Ben Rohlfs <brohlfs@google.com>
2022-06-17 11:46:54 +0200

    Add a new `<gr-file-status>` component
    This is going to replace the `<gr-file-status-chip>` component. At the
    moment its usage is restricted to the `more file info` experiment.
    
    Screenshot https://imgur.com/a/IVCnhuM
    
    Release-Notes: skip
    Google-Bug-Id: b/216343564
    Change-Id: I58d7c9872c371a8b6b6c8fbafe4bbfdc02e84aa9

---
+++ polygerrit-ui/app/elements/shared/gr-file-status-chip/gr-file-status_test.ts
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import '../../../test/common-test-setup-karma';
+import './gr-file-status';
+import {GrFileStatus} from './gr-file-status';
+import {fixture} from '@open-wc/testing-helpers';
+import {FileInfoStatus} from '../../../api/rest-api';
+
+suite('gr-file-status tests', () => {
+  let element: GrFileStatus;
+
+  setup(async () => {
+    element = await fixture<GrFileStatus>('<gr-file-status></gr-file-status>');
+    await setStatus();
+  });
+
+  const setStatus = async (status?: FileInfoStatus, newly = false) => {
+    element.status = status;
+    element.new = newly;
+    await element.updateComplete;
+  };
+
+  suite('semantic dom diff tests', () => {
+    test('empty status', async () => {
+      expect(element).shadowDom.to.equal(/* HTML */ `
+        <div class="status" aria-label="" tabindex="0" title=""></div>
+      `);
+    });
+
+    test('added', async () => {
+      await setStatus(FileInfoStatus.ADDED);
+      expect(element).shadowDom.to.equal(/* HTML */ `
+        <div class="A status" aria-label="Added" tabindex="0" title="Added">
+          A
+        </div>
+      `);
+    });
+
+    test('newly added', async () => {
+      await setStatus(FileInfoStatus.ADDED, true);
+      expect(element).shadowDom.to.equal(/* HTML */ `
+        <iron-icon class="new size-16" icon="gr-icons:new"></iron-icon>
+        <div class="A status" aria-label="Added" tabindex="0" title="Added">
+          A
+        </div>
+      `);
+    });
+  });
+});

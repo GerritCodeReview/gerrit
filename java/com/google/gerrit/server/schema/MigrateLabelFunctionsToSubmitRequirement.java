a2dd3dc266771d37696f51206e0586498c25f040
Youssef Elghareeb <ghareeb@google.com>
2022-06-20 16:24:12 +0200

    Add a pgm to migrate label functions to submit requirements
    This commit adds the logic and tests to migrate label functions to
    submit requirements. The migration is only run if the host does not have
    rules.pl; i.e. if at least one project has a rules.pl file then the
    migration is skipped.
    
    The migration is done as follows:
      * MaxWithBlock is translated to label:$lbl=MAX AND -label:$lbl=MIN.
      * AnyWithBlock is translated to -label:$lbl=MIN.
      * MaxNoBlock is translated to label:$lbl=MAX.
      * PatchSetLock labels are skipped.
      * NoBlock / NoOp are translated to non_applicable SRs.
      * If ignoreSelfApproval is true, the submit requirement is adapted.
      * The SR's canOverride follows the label's canOverride boolean.
    
    The logic is idempotent, i.e. if the migration is run twice then he
    second run will have no effect. This is done by checking first if
    there's a submit requirement with the same name as the label function.
    
    We'll run this migration in google; we'll also add a new schema
    migration in a follow-up change s.t. this migration runs with the next
    gerrit releases.
    
    Release-Notes: skip
    Change-Id: I845aa237f9efc5628a428892d1073a9ecf5177ac

---
+++ java/com/google/gerrit/server/schema/MigrateLabelFunctionsToSubmitRequirement.java
@@ -0,0 +1,230 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.schema;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.LabelFunction;
+import com.google.gerrit.entities.LabelType;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.entities.SubmitRequirement;
+import com.google.gerrit.entities.SubmitRequirementExpression;
+import com.google.gerrit.server.git.meta.MetaDataUpdate;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.project.ProjectConfig;
+import com.google.gerrit.server.project.ProjectState;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+
+/**
+ * A pgm that migrates existing label functions to submit requirements and converts the label
+ * functions to {@link LabelFunction#NO_BLOCK}. The migration is only run if all projects in this
+ * gerrit installation have no Prolog submit rules (i.e. a rules.pl file in refs/meta/config). If at
+ * least one project has prolog submit rules the migration is skipped.
+ *
+ * <p>The conversion is done as follows:
+ *
+ * <ul>
+ *   <li>MaxWithBlock is translated to submittableIf = label:$lbl=MAX AND -label:$lbl=MIN
+ *   <li>MaxNoBlock is translated to submittableIf = label:$lbl=MAX
+ *   <li>AnyWithBlock is translated to submittableIf = -label:$lbl=MIN
+ *   <li>NoBlock/NoOp are translated to applicableIf = is:false (not applicable)
+ *   <li>PatchSetLock labels are left as is
+ * </ul>
+ *
+ * If the label has {@link LabelType#isIgnoreSelfApproval()}, the max vote is appended with the
+ * 'user=non_uploader' argument.
+ */
+public class MigrateLabelFunctionsToSubmitRequirement {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final ProjectCache projectCache;
+  private final MetaDataUpdate.Server metaDataUpdateFactory;
+  private final ProjectConfig.Factory projectConfigFactory;
+
+  @Inject
+  public MigrateLabelFunctionsToSubmitRequirement(
+      ProjectCache projectCache,
+      MetaDataUpdate.Server metaDataUpdateFactory,
+      ProjectConfig.Factory projectConfigFactory) {
+    this.projectCache = projectCache;
+    this.metaDataUpdateFactory = metaDataUpdateFactory;
+    this.projectConfigFactory = projectConfigFactory;
+  }
+
+  public void execute() throws IOException, ConfigInvalidException {
+    List<Project.NameKey> allProjects = projectCache.all().stream().collect(Collectors.toList());
+    boolean hasProlog = anyProjectHasProlog(allProjects);
+    if (hasProlog) {
+      logger.atInfo().log(
+          "Skipping the migration because this gerrit installation has prolog rules");
+      return;
+    }
+    List<Project.NameKey> sortedProjects = sortProjectsWithInheritance(allProjects);
+    for (Project.NameKey project : sortedProjects) {
+      executeMigration(project);
+    }
+  }
+
+  /**
+   * For each label function, create a corresponding submit-requirement and set the label function
+   * to NO_BLOCK. Blocking label functions are substituted with blocking submit-requirements.
+   * Non-blocking label functions are substituted with non-applicable submit requirements, allowing
+   * the label vote to be surfaced as a trigger vote (optional label).
+   */
+  private void executeMigration(Project.NameKey project)
+      throws IOException, ConfigInvalidException {
+    MetaDataUpdate md = metaDataUpdateFactory.create(project);
+    ProjectConfig config = projectConfigFactory.read(md);
+    List<LabelType> labelTypes =
+        config.getLabelSections().values().stream().collect(Collectors.toList());
+    boolean updated = false;
+    for (LabelType lt : labelTypes) {
+      // Make the operation idempotent by skipping creating the submit-requirement if one was
+      // already created or previously existed.
+      if (hasSubmitRequirement(project, lt.getName())) {
+        continue;
+      }
+      if (lt.getFunction().equals(LabelFunction.PATCH_SET_LOCK)) {
+        // PATCH_SET_LOCK functions should be left as is
+        continue;
+      }
+      updated = true;
+      SubmitRequirement sr = convertToSubmitRequirement(lt);
+      config.upsertSubmitRequirement(sr);
+      LabelType updatedLt = lt.toBuilder().setFunction(LabelFunction.NO_BLOCK).build();
+      config.upsertLabelType(updatedLt);
+    }
+    if (updated) {
+      commit(config, md);
+    }
+  }
+
+  private void commit(ProjectConfig projectConfig, MetaDataUpdate md) throws IOException {
+    md.setMessage(String.format("Migrate label functions to submit requirements"));
+    projectConfig.commit(md);
+    projectCache.evictAndReindex(projectConfig.getName());
+  }
+
+  private static SubmitRequirement convertToSubmitRequirement(LabelType lt) {
+    if (!isLabelBlocking(lt)) {
+      return createNonApplicableSr(lt);
+    }
+    return createBlockingSr(lt);
+  }
+
+  private static SubmitRequirement createNonApplicableSr(LabelType lt) {
+    return SubmitRequirement.builder()
+        .setName(lt.getName())
+        .setApplicabilityExpression(SubmitRequirementExpression.of("is:false"))
+        .setSubmittabilityExpression(SubmitRequirementExpression.create("is:true"))
+        .setAllowOverrideInChildProjects(lt.isCanOverride())
+        .build();
+  }
+
+  private static SubmitRequirement createBlockingSr(LabelType lt) {
+    SubmitRequirement.Builder builder =
+        SubmitRequirement.builder()
+            .setName(lt.getName())
+            .setAllowOverrideInChildProjects(lt.isCanOverride());
+    String maxPart =
+        String.format("label:%s=MAX", lt.getName())
+            + (lt.isIgnoreSelfApproval() ? ",user=non_uploader" : "");
+    switch (lt.getFunction()) {
+      case MAX_WITH_BLOCK:
+        builder.setSubmittabilityExpression(
+            SubmitRequirementExpression.create(
+                String.format("%s AND -label:%s=MIN", maxPart, lt.getName())));
+        break;
+      case ANY_WITH_BLOCK:
+        builder.setSubmittabilityExpression(
+            SubmitRequirementExpression.create(String.format("-label:%s=MIN", lt.getName())));
+        break;
+      case MAX_NO_BLOCK:
+        builder.setSubmittabilityExpression(SubmitRequirementExpression.create(maxPart));
+        break;
+      default:
+        break;
+    }
+    return builder.build();
+  }
+
+  private static boolean isLabelBlocking(LabelType labelType) {
+    return labelType.getFunction().isBlock() || labelType.getFunction().isRequired();
+  }
+
+  /**
+   * Projects should be processed bottom up, i.e. child projects should be migrated first before
+   * their parent projects. This method sorts the list of input projects with respect to their
+   * inheritance hierarchy.
+   *
+   * @param projects a list of input projects.
+   * @return the same list of input projects sorted with respect to the inheritance hierarchy. Child
+   *     projects come first before their parent projects.
+   */
+  private List<Project.NameKey> sortProjectsWithInheritance(List<Project.NameKey> projects) {
+    List<Project.NameKey> sorted = new ArrayList<>(projects);
+    Collections.sort(
+        sorted,
+        (p1, p2) -> {
+          Optional<ProjectState> projectState1 = projectCache.get(p1);
+          Optional<ProjectState> projectState2 = projectCache.get(p2);
+          if (!projectState1.isPresent()) {
+            return -1;
+          }
+          if (!projectState2.isPresent()) {
+            return 1;
+          }
+          ProjectState project1 = projectState1.get();
+          ProjectState project2 = projectState2.get();
+          if (project1.parents().anyMatch(parent -> parent.getNameKey().equals(p2))) {
+            // project 1 is a child of project 2.
+            return -1;
+          }
+          if (project2.parents().anyMatch(parent -> parent.getNameKey().equals(p1))) {
+            // project 2 is a child of project 1.
+            return 1;
+          }
+          return 0;
+        });
+    return sorted;
+  }
+
+  private boolean hasSubmitRequirement(Project.NameKey project, String srName) {
+    Optional<ProjectState> projectState = projectCache.get(project);
+    if (!projectState.isPresent()) {
+      return false;
+    }
+    return projectState.get().getSubmitRequirements().containsKey(srName.toLowerCase(Locale.ROOT));
+  }
+
+  private boolean anyProjectHasProlog(List<Project.NameKey> allProjects) {
+    return allProjects.stream().anyMatch(p -> hasPrologRules(p));
+  }
+
+  private boolean hasPrologRules(Project.NameKey project) {
+    Optional<ProjectState> projectStateOptional = projectCache.get(project);
+    if (!projectStateOptional.isPresent()) {
+      return false;
+    }
+    return projectStateOptional.get().hasPrologRules();
+  }
+}

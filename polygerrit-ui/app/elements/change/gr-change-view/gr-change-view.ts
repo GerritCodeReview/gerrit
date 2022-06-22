b3865863a428f4be9b4bb16715b6ba2d416a80be
Milutin Kristofic <milutin@google.com>
2022-06-22 09:35:01 +0000

    Revert "Fix file-list is not reloaded when clicking on change number"
    This reverts commit 025bf8fa9cafba9ec6ae2540e84beafaf75f289d.
    
    Reason for revert: Rebasing change does not load the latest patchset
    
    Change-Id: I35cea89c100f62a72f9be2dfdfe4adcbf093dad9

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -1284,10 +1284,6 @@ export class GrChangeView extends base {
     if (value.basePatchNum === undefined)
       value.basePatchNum = ParentPatchSetNum;
 
-    if (value.patchNum === undefined) {
-      value.patchNum = computeLatestPatchNum(this._allPatchSets);
-    }
-
     const patchChanged = this.hasPatchRangeChanged(value);
     let patchNumChanged = this.hasPatchNumChanged(value);
 

05f6f48fd1ee7979350b204d5e8355872bf98e48
Marija Savtchouk <mariasavtchouk@google.com>
2022-06-24 17:53:20 +0100

    Add support to store protos in indexes
    Today, Protos are stored as bytes in all indexes. This change adds
    support to store Protos in index implementations. The existing
    implementation will continue storing bytes, which will be converted to
    protos on Document parsing (IndexedField#setIfPossible), then assigned
    back to the object data fields with the regular IndexedField setter.
    
    This change only adds support to the new (IndexedField abstract). It
    does not affect old FieldDef code.
    
    The proto fields will be added in the follow up changes.
    They will be test covered with AbstractQuery* tests, same as other
    fields.
    
    Change-Id: Ieadcb3b097fd6bed34b47828681cd693ad83d2ba

--- javatests/com/google/gerrit/server/index/IndexedFieldTest.java
+++ javatests/com/google/gerrit/server/index/IndexedFieldTest.java
@@ -14,10 +14,13 @@
 
 package com.google.gerrit.server.index;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.reflect.TypeToken;
+import com.google.gerrit.entities.converter.ChangeProtoConverter;
 import com.google.gerrit.index.IndexedField;
 import com.google.gerrit.index.IndexedField.SearchSpec;
 import com.google.gerrit.index.SchemaFieldDefs.Getter;
@@ -25,11 +28,17 @@ import com.google.gerrit.index.SchemaFieldDefs.SchemaField;
 import com.google.gerrit.index.SchemaFieldDefs.Setter;
 import com.google.gerrit.index.StoredValue;
 import com.google.gerrit.index.testing.FakeStoredValue;
+import com.google.gerrit.proto.Entities;
+import com.google.gerrit.proto.Entities.Change;
+import com.google.gerrit.proto.Entities.Change_Id;
+import com.google.gerrit.proto.Protos;
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
+import java.util.List;
 import java.util.Map.Entry;
+import org.junit.Test;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.FromDataPoints;
 import org.junit.experimental.theories.Theories;
@@ -120,6 +129,23 @@ public class IndexedFieldTest {
 
   static SearchSpec STORED_BYTE_SPEC = STORED_BYTE_FIELD.storedOnly("test");
 
+  static IndexedField<TestIndexedData, Entities.Change> STORED_PROTO_FIELD =
+      IndexedField.<TestIndexedData, Entities.Change>builder(
+              "TestChange", new TypeToken<Entities.Change>() {})
+          .stored()
+          .build(getter(), setter(), ChangeProtoConverter.INSTANCE);
+
+  static SearchSpec STORED_PROTO_FIELD_SPEC = STORED_PROTO_FIELD.storedOnly("test_change");
+
+  static IndexedField<TestIndexedData, Iterable<Entities.Change>> ITERABLE_STORED_PROTO_FIELD =
+      IndexedField.<TestIndexedData, Iterable<Entities.Change>>builder(
+              "TestChange", new TypeToken<Iterable<Entities.Change>>() {})
+          .stored()
+          .build(getter(), setter(), ChangeProtoConverter.INSTANCE);
+
+  static SearchSpec ITERABLE_PROTO_FIELD_SPEC =
+      ITERABLE_STORED_PROTO_FIELD.storedOnly("test_change");
+
   @DataPoints("nonProtoTypes")
   public static final ImmutableList<Entry<IndexedField.SearchSpec, Serializable>>
       fieldToStoredValue =
@@ -138,6 +164,17 @@ public class IndexedFieldTest {
               .entrySet()
               .asList();
 
+  @DataPoints("protoTypes")
+  public static final ImmutableList<Entry<IndexedField.SearchSpec, Serializable>>
+      protoFieldToStoredValue =
+          ImmutableMap.of(
+                  STORED_PROTO_FIELD_SPEC,
+                  createChangeProto(12345),
+                  ITERABLE_PROTO_FIELD_SPEC,
+                  ImmutableList.of(createChangeProto(12345), createChangeProto(54321)))
+              .entrySet()
+              .asList();
+
   @Theory
   public void testSetIfPossible(
       @FromDataPoints("nonProtoTypes") Entry<SearchSpec, Object> fieldToStoredValue) {
@@ -147,5 +184,68 @@ public class IndexedFieldTest {
     TestIndexedData testIndexedData = new TestIndexedData();
     searchSpec.setIfPossible(testIndexedData, storedValue);
     assertThat(testIndexedData.getTestField()).isEqualTo(docValue);
+  }
+
+  @Test
+  public void testSetIfPossible_protoFromBytes() {
+    Entities.Change changeProto = createChangeProto(12345);
+    StoredValue storedValue = new FakeStoredValue(Protos.toByteArray(changeProto));
+    TestIndexedData testIndexedData = new TestIndexedData();
+    STORED_PROTO_FIELD_SPEC.setIfPossible(testIndexedData, storedValue);
+    assertThat(testIndexedData.getTestField()).isEqualTo(changeProto);
+  }
+
+  @Test
+  public void testSetIfPossible_iterableProtoFromIterableBytes() {
+    List<Entities.Change> changeProtos =
+        ImmutableList.of(createChangeProto(12345), createChangeProto(54321));
+    StoredValue storedValue =
+        new FakeStoredValue(
+            changeProtos.stream()
+                .map(proto -> Protos.toByteArray(proto))
+                .collect(toImmutableList()));
+    TestIndexedData testIndexedData = new TestIndexedData();
+    ITERABLE_STORED_PROTO_FIELD.setIfPossible(testIndexedData, storedValue);
+    assertThat(testIndexedData.getTestField()).isEqualTo(changeProtos);
+  }
+
+  @Theory
+  public void testSetIfPossible_fromProto(
+      @FromDataPoints("protoTypes") Entry<SearchSpec, Object> fieldToStoredValue) {
+    Object docValue = fieldToStoredValue.getValue();
+    SchemaField searchSpec = fieldToStoredValue.getKey();
+    StoredValue storedValue = new FakeStoredValue(fieldToStoredValue.getValue(), /*isProto=*/ true);
+    TestIndexedData testIndexedData = new TestIndexedData();
+    searchSpec.setIfPossible(testIndexedData, storedValue);
+    assertThat(testIndexedData.getTestField()).isEqualTo(docValue);
+  }
+
+  @Test
+  public void test_isProtoType() {
+    assertThat(STORED_PROTO_FIELD.isProtoType()).isTrue();
+
+    assertThat(ITERABLE_STORED_PROTO_FIELD.isProtoType()).isFalse();
+    assertThat(INTEGER_FIELD.isProtoType()).isFalse();
+    assertThat(ITERABLE_STRING_FIELD.isProtoType()).isFalse();
+    assertThat(STORED_BYTE_FIELD.isProtoType()).isFalse();
+    assertThat(ITERABLE_STORED_BYTE_FIELD.isProtoType()).isFalse();
+  }
+
+  @Test
+  public void test_isProtoIterableType() {
+
+    assertThat(ITERABLE_STORED_PROTO_FIELD.isProtoIterableType()).isTrue();
+
+    assertThat(STORED_PROTO_FIELD.isProtoIterableType()).isFalse();
+    assertThat(INTEGER_FIELD.isProtoIterableType()).isFalse();
+    assertThat(ITERABLE_STRING_FIELD.isProtoIterableType()).isFalse();
+    assertThat(STORED_BYTE_FIELD.isProtoIterableType()).isFalse();
+    assertThat(ITERABLE_STORED_BYTE_FIELD.isProtoType()).isFalse();
+  }
+
+  private static Change createChangeProto(int id) {
+    return Entities.Change.newBuilder()
+        .setChangeId(Change_Id.newBuilder().setId(id).build())
+        .build();
   }
 }

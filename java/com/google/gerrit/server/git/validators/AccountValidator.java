a8f6feba84c309b119f38698257577a4a50834ce
David Ostrovsky <david@ostrovsky.org>
2022-06-26 16:41:14 +0200

    Push to refs/users/*: Allow to deactivate own account
    The check implemented in Ie08e50ea92f doesn't work when pushing to
    refs/user/* over HTTP, because WebSession isn't propagated.
    
    Bug: Issue 16038
    Change-Id: I60732adc856fb178ad549e0c27fd494b65351155

--- java/com/google/gerrit/server/git/validators/AccountValidator.java
+++ java/com/google/gerrit/server/git/validators/AccountValidator.java
@@ -19,14 +19,12 @@ import static java.util.stream.Collectors.toSet;
 import com.google.common.collect.ImmutableList;
 import com.google.gerrit.common.Nullable;
 import com.google.gerrit.entities.Account;
-import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.account.AccountConfig;
 import com.google.gerrit.server.account.AccountProperties;
 import com.google.gerrit.server.config.AllUsersName;
 import com.google.gerrit.server.git.ValidationError;
 import com.google.gerrit.server.mail.send.OutgoingEmailValidator;
 import com.google.inject.Inject;
-import com.google.inject.Provider;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -42,16 +40,11 @@ import org.eclipse.jgit.revwalk.RevWalk;
  */
 public class AccountValidator {
 
-  private final Provider<IdentifiedUser> self;
   private final AllUsersName allUsersName;
   private final OutgoingEmailValidator emailValidator;
 
   @Inject
-  public AccountValidator(
-      Provider<IdentifiedUser> self,
-      AllUsersName allUsersName,
-      OutgoingEmailValidator emailValidator) {
-    this.self = self;
+  public AccountValidator(AllUsersName allUsersName, OutgoingEmailValidator emailValidator) {
     this.allUsersName = allUsersName;
     this.emailValidator = emailValidator;
   }
@@ -89,10 +82,6 @@ public class AccountValidator {
 
     if (!newAccount.isPresent()) {
       return ImmutableList.of(String.format("account '%s' does not exist", accountId.get()));
-    }
-
-    if (accountId.equals(self.get().getAccountId()) && !newAccount.get().isActive()) {
-      messages.add("cannot deactivate own account");
     }
 
     String newPreferredEmail = newAccount.get().preferredEmail();

60db3e83b4595bbe03e23940f8e01188926962cc
Nitzan Gur-Furman <nitzan@google.com>
2022-06-28 14:53:03 +0300

    Implement ApplyPatch Gerrit API method.
    Change-Id: Ic18a650133d591ddb7e57093156a05dd6c623e11

--- java/com/google/gerrit/server/restapi/change/ApplyPatch.java
+++ java/com/google/gerrit/server/restapi/change/ApplyPatch.java
@@ -17,49 +17,109 @@ package com.google.gerrit.server.restapi.change;
 import static com.google.gerrit.extensions.conditions.BooleanCondition.and;
 import static com.google.gerrit.server.project.ProjectCache.illegalState;
 
+import com.google.gerrit.entities.BooleanProjectConfig;
 import com.google.gerrit.entities.BranchNameKey;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.entities.PatchSet;
 import com.google.gerrit.entities.Project.NameKey;
 import com.google.gerrit.extensions.api.changes.ApplyPatchInput;
 import com.google.gerrit.extensions.common.ChangeInfo;
-import com.google.gerrit.extensions.restapi.AuthException;
-import com.google.gerrit.extensions.restapi.NotImplementedException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.MergeConflictException;
 import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.extensions.webui.UiAction;
+import com.google.gerrit.server.ChangeUtil;
+import com.google.gerrit.server.GerritPersonIdent;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.change.ChangeJson;
 import com.google.gerrit.server.change.ChangeResource;
+import com.google.gerrit.server.change.PatchSetInserter;
+import com.google.gerrit.server.git.CodeReviewCommit;
+import com.google.gerrit.server.git.CodeReviewCommit.CodeReviewRevWalk;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.notedb.ChangeNotes;
 import com.google.gerrit.server.permissions.PermissionBackend;
 import com.google.gerrit.server.permissions.PermissionBackendException;
 import com.google.gerrit.server.permissions.ProjectPermission;
 import com.google.gerrit.server.permissions.RefPermission;
 import com.google.gerrit.server.project.ContributorAgreementsChecker;
+import com.google.gerrit.server.project.InvalidChangeOperationException;
+import com.google.gerrit.server.project.NoSuchChangeException;
+import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.server.project.ProjectState;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.submit.IntegrationConflictException;
+import com.google.gerrit.server.submit.MergeIdenticalTreeException;
+import com.google.gerrit.server.update.BatchUpdate;
+import com.google.gerrit.server.update.UpdateException;
+import com.google.gerrit.server.util.CommitMessageUtil;
+import com.google.gerrit.server.util.time.TimeUtil;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import modules.jgit.org.eclipse.jgit.src.org.eclipse.jgit.patch.PatchApplier;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.CommitBuilder;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
 
 @Singleton
 public class ApplyPatch
     implements RestModifyView<ChangeResource, ApplyPatchInput>, UiAction<ChangeResource> {
+  // DO NOT SUBMIT
+  //  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   private final PermissionBackend permissionBackend;
+  private final ChangeJson.Factory json;
   private final ContributorAgreementsChecker contributorAgreements;
   private final ProjectCache projectCache;
+  private final Provider<IdentifiedUser> user;
+  private final Provider<PersonIdent> serverIdent;
+  private final GitRepositoryManager gitManager;
+  private final BatchUpdate.Factory batchUpdateFactory;
+  private final PatchSetInserter.Factory patchSetInserterFactory;
 
   @Inject
   ApplyPatch(
       PermissionBackend permissionBackend,
+      ChangeJson.Factory json,
       ContributorAgreementsChecker contributorAgreements,
-      ProjectCache projectCache) {
+      ProjectCache projectCache,
+      Provider<IdentifiedUser> user,
+      @GerritPersonIdent Provider<PersonIdent> serverIdent,
+      GitRepositoryManager gitManager,
+      BatchUpdate.Factory batchUpdateFactory,
+      PatchSetInserter.Factory patchSetInserterFactory) {
     this.permissionBackend = permissionBackend;
+    this.json = json;
     this.contributorAgreements = contributorAgreements;
     this.projectCache = projectCache;
+    this.user = user;
+    this.serverIdent = serverIdent;
+    this.gitManager = gitManager;
+    this.batchUpdateFactory = batchUpdateFactory;
+    this.patchSetInserterFactory = patchSetInserterFactory;
   }
 
   @Override
   public Response<ChangeInfo> apply(ChangeResource rsrc, ApplyPatchInput input)
-      throws AuthException, IOException, ResourceConflictException, PermissionBackendException {
+      throws IOException, UpdateException, RestApiException, PermissionBackendException,
+          ConfigInvalidException, NoSuchProjectException, InvalidChangeOperationException {
     NameKey project = rsrc.getProject();
     contributorAgreements.check(project, rsrc.getUser());
 
@@ -71,7 +131,93 @@ public class ApplyPatch
         .check(RefPermission.CREATE_CHANGE);
     projectCache.get(project).orElseThrow(illegalState(rsrc.getProject())).checkStatePermitsWrite();
 
-    throw new NotImplementedException("ApplyPatch is not yet implemented.");
+    IdentifiedUser identifiedUser = user.get();
+    try (Repository repo = gitManager.openRepository(project);
+        // This inserter and revwalk *must* be passed to any BatchUpdates
+        // created later on, to ensure the applied commit is flushed
+        // before patch sets are updated.
+        ObjectInserter oi = repo.newObjectInserter();
+        // DO NOT SUBMIT - pass the reader to the applier?
+        ObjectReader reader = oi.newReader();
+        CodeReviewRevWalk revWalk = CodeReviewCommit.newRevWalk(reader)) {
+      Ref destRef = repo.getRefDatabase().exactRef(destBranch.branch());
+      if (destRef == null) {
+        throw new InvalidChangeOperationException(
+            String.format("Branch %s does not exist.", destBranch.branch()));
+      }
+      ChangeData destChange = rsrc.getChangeData();
+      if (destChange == null) {
+        throw new InvalidChangeOperationException(
+            "Apply-patch cannot be called for an non-existing change.");
+      }
+
+      if (destChange.change().isClosed()) {
+        throw new InvalidChangeOperationException(
+            String.format(
+                "Apply-patch with Change-Id %s could not update the existing change %d "
+                    + "in destination branch %s of project %s, because the change was closed (%s)",
+                destChange.getId(),
+                destChange.getId().get(),
+                destBranch.branch(),
+                destBranch.project(),
+                destChange.change().getStatus().name()));
+      }
+
+      String commitMessage =
+          CommitMessageUtil.checkAndSanitizeCommitMessage(destChange.commitMessage());
+
+      ObjectId treeId;
+      try {
+        RevCommit baseCommit = revWalk.parseCommit(destRef.getObjectId());
+        RevTree tip = baseCommit.getTree();
+        InputStream patchStream =
+            new ByteArrayInputStream(input.patch.getBytes(StandardCharsets.UTF_8));
+        PatchApplier applier = new PatchApplier(repo, tip, oi);
+        PatchApplier.ApplyPatchResult applyResult = applier.applyPatch(patchStream);
+        treeId = applyResult.appliedTree;
+      } catch (GitAPIException e) {
+        throw new IOException("Cannot apply patch", e);
+      }
+      PersonIdent committerIdent = serverIdent.get();
+      PersonIdent authorIdent =
+          user.get()
+              .asIdentifiedUser()
+              .newCommitterIdent(TimeUtil.now(), committerIdent.getTimeZone().toZoneId());
+
+      CommitBuilder appliedCommit = new CommitBuilder();
+      appliedCommit.setTreeId(treeId);
+      appliedCommit.setParentId(destChange.currentPatchSet().commitId());
+      appliedCommit.setAuthor(authorIdent);
+      appliedCommit.setCommitter(committerIdent);
+      appliedCommit.setMessage(commitMessage);
+      matchAuthorToCommitterDate(
+          projectCache.get(rsrc.getProject()).orElseThrow(illegalState(rsrc.getProject())),
+          appliedCommit);
+      CodeReviewCommit commit = revWalk.parseCommit(oi.insert(appliedCommit));
+      commit.setFilesWithGitConflicts(commit.getFilesWithGitConflicts());
+      oi.flush();
+
+      try (BatchUpdate bu = batchUpdateFactory.create(project, identifiedUser, TimeUtil.now())) {
+        bu.setRepository(repo, revWalk, oi);
+        Change.Id changeId;
+        // The change key exists on the destination branch. The cherry pick
+        // will be added as a new patch set.
+        changeId = insertPatchSet(bu, repo, patchSetInserterFactory, destChange.notes(), commit);
+        bu.execute();
+        // DO NOT SUBMIT - where was that copied from?
+        ChangeInfo changeInfo = json.noOptions().format(rsrc.getProject(), changeId);
+        changeInfo.containsGitConflicts =
+            !commit.getFilesWithGitConflicts().isEmpty() ? true : null;
+        // DO NOT SUBMIT - should handle conflicts otherwise. See CherryPickChange::cherryPick
+        return Response.ok(changeInfo);
+      }
+    } catch (MergeIdenticalTreeException | MergeConflictException e) {
+      throw new IntegrationConflictException("Cherry pick failed: " + e.getMessage(), e);
+    } catch (InvalidChangeOperationException e) {
+      throw new BadRequestException(e.getMessage());
+    } catch (NoSuchChangeException e) {
+      throw new ResourceConflictException(e.getMessage());
+    }
   }
 
   @Override
@@ -88,5 +234,30 @@ public class ApplyPatch
                     .currentUser()
                     .project(rsrc.getProject())
                     .testCond(ProjectPermission.CREATE_CHANGE)));
+  }
+
+  private static void matchAuthorToCommitterDate(ProjectState project, CommitBuilder commit) {
+    if (project.is(BooleanProjectConfig.MATCH_AUTHOR_TO_COMMITTER_DATE)) {
+      commit.setAuthor(
+          new PersonIdent(
+              commit.getAuthor(),
+              commit.getCommitter().getWhen(),
+              commit.getCommitter().getTimeZone()));
+    }
+  }
+
+  private static Change.Id insertPatchSet(
+      BatchUpdate bu,
+      Repository git,
+      PatchSetInserter.Factory patchSetInserterFactory,
+      ChangeNotes destNotes,
+      CodeReviewCommit commit)
+      throws IOException {
+    Change destChange = destNotes.getChange();
+    PatchSet.Id psId = ChangeUtil.nextPatchSetId(git, destChange.currentPatchSetId());
+    PatchSetInserter inserter = patchSetInserterFactory.create(destNotes, psId, commit);
+    inserter.setMessage("Uploaded patch set " + inserter.getPatchSetId().get() + ".");
+    bu.addOp(destChange.getId(), inserter);
+    return destChange.getId();
   }
 }
--- javatests/com/google/gerrit/acceptance/api/change/ApplyPatchIT.java
+++ javatests/com/google/gerrit/acceptance/api/change/ApplyPatchIT.java
@@ -14,31 +14,193 @@
 
 package com.google.gerrit.acceptance.api.change;
 
-import static com.google.common.truth.Truth.assertThat;
-import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+import static org.eclipse.jgit.lib.Constants.HEAD;
 
 import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.entities.BranchNameKey;
 import com.google.gerrit.extensions.api.changes.ApplyPatchInput;
-import com.google.gerrit.extensions.api.changes.ChangeApi;
+import com.google.gerrit.extensions.common.ChangeInfo;
 import com.google.gerrit.extensions.common.ChangeInput;
-import com.google.gerrit.extensions.restapi.NotImplementedException;
+import com.google.gerrit.extensions.common.DiffInfo;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ApplyPatchIT extends AbstractDaemonTest {
 
-  private static final String COMMIT_MESSAGE = "change to apply the patch in";
+  private static final String COMMIT_MESSAGE = "Applying patch";
+  private static final String DESTINATION_BRANCH = "destBranch";
+  private static final String ADDED_FILE_NAME = "a_new_file.txt";
+  private static final String ADDED_FILE_CONTENT = "First added line\nSecond added line\n";
+  private static final String MODIFIED_FILE_NAME = "modified_file.txt";
+  private static final String MODIFIED_FILE_ORIGINAL_CONTENT =
+      "First original line\nSecond original line";
+  private static final String MODIFIED_FILE_NEW_CONTENT = "Modified line\n";
+  private static final String DELETED_FILE_NAME = "deleted_file.txt";
+  private static final String DELETED_FILE_ORIGINAL_CONTENT = "content to be deleted.\n";
+  private static final String RENAMED_FILE_ORIGINAL_NAME = "renamed_file_origin.txt";
+  private static final String RENAMED_FILE_NEW_NAME = "renamed_file_new.txt";
+  private static final String COPIED_FILE_ORIGINAL_NAME = "copied_file_origin.txt";
+  private static final String COPIED_FILE_NEW_NAME = "copied_file_new.txt";
+
+  private static final String ADDED_FILE_DIFF =
+      "diff --git a/a_new_file.txt b/a_new_file.txt\n"
+          + "new file mode 100644\n"
+          + "index 0000000..f0eec86\n"
+          + "--- /dev/null\n"
+          + "+++ b/a_new_file.txt\n"
+          + "@@ -0,0 +1,2 @@\n"
+          + "+First added line\n"
+          + "+Second added line\n";
+  private static final String MODIFIED_FILE_DIFF =
+      "diff --git a/modified_file.txt b/modified_file.txt\n"
+          + "new file mode 100644\n"
+          + "--- a/modified_file.txt\n"
+          + "+++ b/modified_file.txt\n"
+          + "@@ -1,2 +1 @@\n"
+          + "-First original line\n"
+          + "-Second original line\n"
+          + "+Modified line\n";
+  private static final String DELETED_FILE_DIFF =
+      "diff --git a/deleted_file.txt b/deleted_file.txt\n"
+          + "index fe6a849..0000000\n"
+          + "--- a/deleted_file.txt\n"
+          + "+++ /dev/null\n";
+  private static final String RENAMED_FILE_DIFF =
+      "diff --git a/renamed_file_origin.txt b/renamed_file_new.txt\n"
+          + "rename from renamed_file_origin.txt\n"
+          + "rename to renamed_file_new.txt\n"
+          + "--- a/renamed_file_origin.txt\n"
+          + "+++ b/renamed_file_new.txt\n"
+          + "@@ -1,2 +1 @@\n"
+          + "-First original line\n"
+          + "-Second original line\n"
+          + "+Modified line\n";
+  private static final String COPIED_FILE_DIFF =
+      "diff --git a/copied_file_origin.txt b/copied_file_new.txt\n"
+          + "copy from copied_file_origin.txt\n"
+          + "copy to copied_file_new.txt\n"
+          + "--- a/copied_file_origin.txt\n"
+          + "+++ b/copied_file_new.txt\n"
+          + "@@ -1,2 +1 @@\n"
+          + "-First original line\n"
+          + "-Second original line\n"
+          + "+Modified line\n";
+
+  @Before
+  public void setup() throws Exception {
+    PushOneCommit.Result baseCommit =
+        createChange("Add file for deletion", DELETED_FILE_NAME, DELETED_FILE_ORIGINAL_CONTENT);
+    baseCommit =
+        amendChange(
+            baseCommit.getChangeId(),
+            "Add file for modification",
+            MODIFIED_FILE_NAME,
+            MODIFIED_FILE_ORIGINAL_CONTENT);
+    baseCommit =
+        amendChange(
+            baseCommit.getChangeId(),
+            "Add file for renaming",
+            RENAMED_FILE_ORIGINAL_NAME,
+            MODIFIED_FILE_ORIGINAL_CONTENT);
+    baseCommit =
+        amendChange(
+            baseCommit.getChangeId(),
+            "Add file for copying",
+            COPIED_FILE_ORIGINAL_NAME,
+            MODIFIED_FILE_ORIGINAL_CONTENT);
+    baseCommit.assertOkStatus();
+
+    String head = getHead(repo(), HEAD).name();
+    createBranchWithRevision(BranchNameKey.create(project, DESTINATION_BRANCH), head);
+  }
 
   @Test
-  public void exampleApplyPatchUsage() throws Exception {
+  public void applyAddedFilePatch() throws Exception {
     ApplyPatchInput in = new ApplyPatchInput();
-    in.patch =
-        "Patch compatible with `git diff` output, for example Gerrit 'Download patch' output.";
-    ChangeApi changeApi =
-        gApi.changes().create(new ChangeInput(project.get(), "master", COMMIT_MESSAGE));
+    in.patch = ADDED_FILE_DIFF;
 
-    NotImplementedException exception =
-        assertThrows(NotImplementedException.class, () -> changeApi.applyPatch(in));
+    ChangeInfo result =
+        gApi.changes()
+            .create(new ChangeInput(project.get(), DESTINATION_BRANCH, COMMIT_MESSAGE))
+            .applyPatch(in);
 
-    assertThat(exception).hasMessageThat().isEqualTo("ApplyPatch is not yet implemented.");
+    DiffInfo diff = gApi.changes().id(result.id).current().file(ADDED_FILE_NAME).diff();
+    assertDiffForNewFile(diff, null, ADDED_FILE_NAME, ADDED_FILE_CONTENT);
   }
+
+  @Test
+  public void applyModifiedFilePatch() throws Exception {
+    ApplyPatchInput in = new ApplyPatchInput();
+    in.patch = MODIFIED_FILE_DIFF;
+
+    ChangeInfo result =
+        gApi.changes()
+            .create(new ChangeInput(project.get(), DESTINATION_BRANCH, COMMIT_MESSAGE))
+            .applyPatch(in);
+
+    DiffInfo diff = gApi.changes().id(result.id).current().file(MODIFIED_FILE_NAME).diff();
+    assertDiffForFullyModifiedFile(
+        diff, null, MODIFIED_FILE_NAME, MODIFIED_FILE_ORIGINAL_CONTENT, MODIFIED_FILE_NEW_CONTENT);
+  }
+
+  @Test
+  public void applyDeletedFilePatch() throws Exception {
+    ApplyPatchInput in = new ApplyPatchInput();
+    in.patch = DELETED_FILE_DIFF;
+
+    ChangeInfo result =
+        gApi.changes()
+            .create(new ChangeInput(project.get(), DESTINATION_BRANCH, COMMIT_MESSAGE))
+            .applyPatch(in);
+
+    DiffInfo diff = gApi.changes().id(result.id).current().file(DELETED_FILE_NAME).diff();
+    assertDiffForDeletedFile(diff, null, DELETED_FILE_NAME, DELETED_FILE_ORIGINAL_CONTENT);
+  }
+
+  @Test
+  public void applyRenamedFilePatch() throws Exception {
+    ApplyPatchInput in = new ApplyPatchInput();
+    in.patch = RENAMED_FILE_DIFF;
+
+    ChangeInfo result =
+        gApi.changes()
+            .create(new ChangeInput(project.get(), DESTINATION_BRANCH, COMMIT_MESSAGE))
+            .applyPatch(in);
+
+    DiffInfo originalFileDiff =
+        gApi.changes().id(result.id).current().file(RENAMED_FILE_ORIGINAL_NAME).diff();
+    assertDiffForDeletedFile(
+        originalFileDiff, null, RENAMED_FILE_ORIGINAL_NAME, MODIFIED_FILE_ORIGINAL_CONTENT);
+    DiffInfo newFileDiff =
+        gApi.changes().id(result.id).current().file(RENAMED_FILE_NEW_NAME).diff();
+    assertDiffForNewFile(newFileDiff, null, RENAMED_FILE_NEW_NAME, MODIFIED_FILE_NEW_CONTENT);
+  }
+
+  @Test
+  public void applyCopiedFilePatch() throws Exception {
+    ApplyPatchInput in = new ApplyPatchInput();
+    in.patch = COPIED_FILE_DIFF;
+
+    ChangeInfo result =
+        gApi.changes()
+            .create(new ChangeInput(project.get(), DESTINATION_BRANCH, COMMIT_MESSAGE))
+            .applyPatch(in);
+
+    DiffInfo originalFileDiff =
+        gApi.changes().id(result.id).current().file(COPIED_FILE_ORIGINAL_NAME).diff();
+    assertDiffForDeletedFile(
+        originalFileDiff, null, COPIED_FILE_ORIGINAL_NAME, MODIFIED_FILE_ORIGINAL_CONTENT);
+    DiffInfo newFileDiff = gApi.changes().id(result.id).current().file(COPIED_FILE_NEW_NAME).diff();
+    assertDiffForNewFile(newFileDiff, null, COPIED_FILE_NEW_NAME, MODIFIED_FILE_NEW_CONTENT);
+  }
+
+  //  @Test
+  //  public void applyMultipleFilesPatch() throws Exception {
+  //  }
+  //
+  //
+  //  @Test
+  //  public void applyGerritBasedPatch() throws Exception {
+  //  }
 }

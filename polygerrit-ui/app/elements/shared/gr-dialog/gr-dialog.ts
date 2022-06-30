c481d1e52a2f5995489efafe9872ab50c1c3596a
Dhruv <dhruvsri@google.com>
2022-06-30 14:54:25 +0200

    Add aria label and role to GrDialog spinning state
    Release-Notes: skip
    Google-bug-id: b/235477389
    Change-Id: I5311e15d628b43f30695866fedad42365130af21

--- polygerrit-ui/app/elements/shared/gr-dialog/gr-dialog.ts
+++ polygerrit-ui/app/elements/shared/gr-dialog/gr-dialog.ts
@@ -139,7 +139,11 @@ export class GrDialog extends LitElement {
           ${when(
             this.loading,
             () => html`
-              <span class="loadingSpin"></span>
+              <span
+                class="loadingSpin"
+                role="progressbar"
+                aria-label=${this.loadingLabel}
+              ></span>
               <span class="loadingLabel"> ${this.loadingLabel} </span>
             `
           )}

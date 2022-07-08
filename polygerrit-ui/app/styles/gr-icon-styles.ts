93710997b25cba8d5f5f530b8ed4c41354e36ebf
Chris Poucet <poucet@google.com>
2022-07-08 09:23:12 +0200

    Replace gr-icons:help-outline with material icon
    The only exception where this is not applied is gr-checks-results as
    iconForLink needs to be migrated wholesale to this new pattern.
    
    Release-Notes: skip
    Change-Id: I9db1bfad1cf84bf4dd7e77bd7cb7227a31888aa9

--- polygerrit-ui/app/styles/gr-icon-styles.ts
+++ polygerrit-ui/app/styles/gr-icon-styles.ts
@@ -19,4 +19,36 @@ export const iconStyles = css`
     position: relative;
     top: 2px;
   }
+
+  .material-icon {
+    color: var(--deemphasized-text-color);
+    font-family: 'Material Symbols Outlined';
+    font-weight: normal;
+    font-style: normal;
+    font-size: 20px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 20;
+  }
+
+  .material-icon-filled {
+    color: var(--deemphasized-text-color);
+    font-family: 'Material Symbols Outlined';
+    font-weight: normal;
+    font-style: normal;
+    font-size: 20px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    font-variation-settings: 'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 20;
+  }
 `;

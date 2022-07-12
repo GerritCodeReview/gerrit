bf7ec9c02470fb1eb32cef071b03855f441cc6c8
Chris Poucet <poucet@google.com>
2022-07-12 16:43:28 +0200

    Move gr-change-actions to use material-icons
    Screenshot: https://imgur.com/a/8JlbIqe
    Google-Bug-Id: b/237255434
    Release-Notes: skip
    Change-Id: Ic14d8e203afb7c4e7503f03dfda7f6333e4db8e6

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-change-actions-js-api.ts
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-change-actions-js-api.ts
@@ -22,6 +22,8 @@ export interface UIActionInfo extends RequireProperties<ActionInfo, 'label'> {
   __primary?: boolean;
   __type: ActionType;
   icon?: string;
+  filled?: boolean;
+  custom?: boolean;
 }
 
 // This interface is required to avoid circular dependencies between files;

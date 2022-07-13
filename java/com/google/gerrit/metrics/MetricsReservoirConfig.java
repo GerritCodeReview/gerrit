0c2d32502803e655cf2144df83dd35d46adab286
Luca Milanesio <luca.milanesio@gmail.com>
2022-07-13 01:26:08 +0100

    Allow different data reservoirs for metrics
    The default reservoir for collecting data with DropWizard may not
    be suitable for all types of metrics collection.
    
    The default ExponentiallyDecayingReservoir is typically suitable
    for data with a lots of values per unit whilst shows inaccurancy
    for smaller data sets spread over a longer period of time.
    
    Introduce a new DropWizardReservoirProvider which reads the
    reservoir type from gerrit.config in a new section called [metrics].
    The reservoir can be subsequently configured in the subsection
    [metrics "<reservoir>"] and its parameters tailored to the Gerrit
    admin's metrics collection system.
    
    For example, when using Prometheus scraper on a 1 mins interval
    basis, it is possible to swap the default reservoir with a more
    suitable SlidingTimeWindow:
    
    [metrics]
      reservoir = SlidingTimeWindow
    
    [metrics "SlidingTimeWindow"]
      window = 1 min
    
    Release-Notes: introduce metrics configuration for different data reservoirs
    Change-Id: Iaf73bc5c1a73423d06f423ea813df2d96e151f9e

---
+++ java/com/google/gerrit/metrics/MetricsReservoirConfig.java
@@ -0,0 +1,28 @@
+package com.google.gerrit.metrics;
+
+import java.time.Duration;
+
+/** Configuration of the Metrics' reservoir type and size. */
+public interface MetricsReservoirConfig {
+
+  /** Type of reservoir for collecting metrics into. */
+  public enum ReservoirType {
+    ExponentiallyDecaying,
+    SlidingTimeWindowArray,
+    SlidingTimeWindow,
+    SlidingWindow,
+    Uniform;
+  }
+
+  /** @return the reservoir type. */
+  ReservoirType reservoirType();
+
+  /** @return the reservoir window duration. */
+  Duration reservoirWindow();
+
+  /** @return the number of samples that the reservoir can contain */
+  int reservoirSize();
+
+  /** @return the alpha parameter of the ExponentiallyDecaying reservoir */
+  double reservoirAlpha();
+}

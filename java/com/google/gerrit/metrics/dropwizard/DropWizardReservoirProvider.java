c4609f7064b026ed97d93ffe33c7cf033a128511
Luca Milanesio <luca.milanesio@gmail.com>
2022-07-13 01:26:08 +0100

    Allow different data reservoirs for metrics
    The default reservoir for collecting data with DropWizard may not
    be suitable for all types of metrics collection.
    
    The default ExponentiallyDecayingReservoir is typically suitable
    for data with a lots of values per unit whilst shows inaccurancy
    for smaller data sets spread over a longer period of time.
    
    Introduce a new DropWizardReservoirProvider which reads the
    reservoir from gerrit.config in a new section called [metrics].
    The reservoir can be subsequently configured in the subsection
    [metrics "<reservoir>"] and its parameters tailored to the Gerrit
    admin's metrics collection system.
    
    For example, when using Prometheus scraper on a 1 mins interval
    basis, it is possible to swap the default reservoir with a more
    suitable SlidingTimeWindow:
    
    [metrics]
      reservoir = SlidingTimeWindow
    
    [metrics "SlidingTimeWindow"]
      window = 1 min
    
    Change-Id: Iaf73bc5c1a73423d06f423ea813df2d96e151f9e

---
+++ java/com/google/gerrit/metrics/dropwizard/DropWizardReservoirProvider.java
@@ -0,0 +1,63 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.metrics.dropwizard;
+
+import com.codahale.metrics.ExponentiallyDecayingReservoir;
+import com.codahale.metrics.Reservoir;
+import com.codahale.metrics.SlidingTimeWindowArrayReservoir;
+import com.codahale.metrics.SlidingTimeWindowReservoir;
+import com.codahale.metrics.SlidingWindowReservoir;
+import com.codahale.metrics.UniformReservoir;
+import com.google.gerrit.server.config.MetricsConfig;
+import com.google.gerrit.server.config.MetricsConfig.ReservoirType;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.util.concurrent.TimeUnit;
+
+public class DropWizardReservoirProvider implements Provider<Reservoir> {
+  private final MetricsConfig config;
+
+  @Inject
+  public DropWizardReservoirProvider(MetricsConfig config) {
+    this.config = config;
+  }
+
+  @Override
+  public Reservoir get() {
+    ReservoirType reservoirType = config.reservoirType();
+    switch (reservoirType) {
+      case ExponentiallyDecaying:
+        return config.hasCustomReservoirConfig()
+            ? new ExponentiallyDecayingReservoir(config.reservoirSize(), config.reservoirAlpha())
+            : new ExponentiallyDecayingReservoir();
+      case SlidingTimeWindowArray:
+        return new SlidingTimeWindowArrayReservoir(
+            config.reservoirWindow().toMillis(), TimeUnit.MILLISECONDS);
+      case SlidingTimeWindowReservoir:
+        return new SlidingTimeWindowReservoir(
+            config.reservoirWindow().toMillis(), TimeUnit.MILLISECONDS);
+      case SlidingWindowReservoir:
+        return new SlidingWindowReservoir(config.reservoirSize());
+      case UniformReservoir:
+        return config.hasCustomReservoirConfig()
+            ? new UniformReservoir(config.reservoirSize())
+            : new UniformReservoir();
+
+      default:
+        throw new IllegalArgumentException(
+            "Unsupported metrics reservoir type " + reservoirType.name());
+    }
+  }
+}

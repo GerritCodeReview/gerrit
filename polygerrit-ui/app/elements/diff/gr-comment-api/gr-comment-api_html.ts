5fe9a97bdf31faff9fa285ea4911578f66201b5e
Frank Borden <frankborden@google.com>
2022-07-20 11:24:38 +0200

    Convert gr-comment-api_test to typescript
    Rewrite tests since gr-comment-api is not an element anymore. Compare
    against patchset 1 for better diffing.
    
    Also remove _html file since it is not used.
    
    This file+test should be moved out of elements/ but I don't know where
    to put it. Suggestions welcome.
    
    Release-Notes: skip
    
    Change-Id: I2814bd657c0df9ab30510b1b7f3586730bcf1e2c

--- polygerrit-ui/app/elements/diff/gr-comment-api/gr-comment-api_html.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright 2020 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-import {html} from '@polymer/polymer/lib/utils/html-tag';
-
-export const htmlTemplate = html``;

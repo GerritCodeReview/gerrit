7ec664fa8b8d2e5fc62ba0dbe8238da347a45922
Milutin Kristofic <milutin@google.com>
2022-07-25 14:49:29 +0200

    Service worker - show notification
    Change-Id: I8436c00d7bbd47dc30eeda3dea2e6428673f40e7

--- polygerrit-ui/app/workers/service-worker.ts
+++ polygerrit-ui/app/workers/service-worker.ts
@@ -4,5 +4,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// eslint-disable-next-line no-console
+import {readResponsePayload} from '../elements/shared/gr-rest-api-interface/gr-rest-apis/gr-rest-api-helper';
+import {ParsedChangeInfo} from '../types/types';
+/* eslint-disable no-console*/
+
 console.log('Hello World!');
+
+function getGlobalScope() {
+  return self as {} as ServiceWorkerGlobalScope;
+}
+
+self.addEventListener('push', async () => {
+  console.log('push');
+  //   const response = await fetch('http://localhost:8080/q/attention:self');
+  const response = await fetch(
+    'http://localhost:8080/changes/?O=1000081&S=0&n=25&q=status%3Aopen%20-is%3Awip'
+  );
+  console.log(response);
+  const payload = await readResponsePayload(response);
+  const changes = (await payload.parsed) as unknown as ParsedChangeInfo[];
+  console.log(changes);
+  if (changes.length > 0) {
+    console.log('Changes!');
+    const options = {};
+    getGlobalScope().registration.showNotification(changes[0].subject, options);
+  }
+});

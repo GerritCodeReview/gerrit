c51b5b25d1cd2f0ad9b9d6698faf5b87a4483963
Milutin Kristofic <milutin@google.com>
2022-07-29 15:30:57 +0200

    Service Worker - Do not notify twice on same attention set change
    The service worker should show changes with last_update in the time
    slot between last scheduled notification and now. Therefore we do not
    notify more than once about attention set change.
    
    Google-Bug-Id: b/202288419
    Release-Notes: skip
    Change-Id: I47340e7c64202b1810441787783130e2bb241b5d

---
+++ polygerrit-ui/app/utils/service-worker-util_test.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import {Timestamp} from '../api/rest-api';
+import '../test/common-test-setup-karma';
+import {
+  createAccountDetailWithId,
+  createParsedChange,
+} from '../test/test-data-generators';
+import {ParsedChangeInfo} from '../types/types';
+import {filterAttentionChangesAfter} from './service-worker-util';
+
+suite('service worker util tests', () => {
+  test('filterAttentionChangesAfter', () => {
+    const account = createAccountDetailWithId();
+    const changeBefore: ParsedChangeInfo = {
+      ...createParsedChange(),
+      attention_set: {
+        [`${account._account_id}`]: {
+          account,
+          last_update: '2016-01-12 20:24:49.448000000' as Timestamp,
+        },
+      },
+    };
+    const changeAfter: ParsedChangeInfo = {
+      ...createParsedChange(),
+      attention_set: {
+        [`${account._account_id}`]: {
+          account,
+          last_update: '2016-01-12 20:24:50.448000000' as Timestamp,
+        },
+      },
+    };
+    const changes = [changeBefore, changeAfter];
+
+    const filteredChanges = filterAttentionChangesAfter(
+      changes,
+      account,
+      1452630289548
+    );
+
+    assert.equal(filteredChanges.length, 1);
+    assert.equal(filteredChanges[0], changeAfter);
+  });
+});

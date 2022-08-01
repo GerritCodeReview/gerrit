5a23175583dcb1d95cf435fc5c459d5ac12ee379
Ben Rohlfs <brohlfs@google.com>
2022-08-01 14:33:02 +0200

    Change all pencil icons from 18px to 16px
    The 18px pencil on the change page seemed a bit large. Generally we want
    to consistently use either 20px or 16px icons and never 18px ones.
    
    16px is set by using the `small` attribute on `<gr-icon>`.
    
    Note that we are introducing some <div>s in <gr-button> slots. The
    reason is that <gr-button> applies a flex layout to slotted children.
    And that does not work well with inline children. It is much easier to
    reason about the layout, if there is a dedicated block between flex
    parent and inline child.
    
    Screenshots
    https://imgur.com/a/9Rgi99T
    https://imgur.com/a/73bNmyQ
    https://imgur.com/a/i4W6QCM
    https://imgur.com/a/2X9u4Kr
    
    Release-Notes: skip
    Change-Id: Idad10184366d0fa747960aedeffa8ea72ef48ea7

--- polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.ts
+++ polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.ts
@@ -492,6 +492,10 @@ export class GrReplyDialog extends LitElement {
       }
       .attention .edit-attention-button gr-icon {
         color: inherit;
+        /* The line-height:26px hack (see below) requires us to do this.
+           Normally the gr-icon would account for a proper positioning
+           within the standard line-height:20px context. */
+        top: 5px;
       }
       .attention a,
       .attention-detail a {
@@ -968,8 +972,10 @@ export class GrReplyDialog extends LitElement {
                 role="button"
                 tabindex="0"
               >
-                <gr-icon icon="edit" filled></gr-icon>
-                Modify
+                <div>
+                  <gr-icon icon="edit" filled small></gr-icon>
+                  <span>Modify</span>
+                </div>
               </gr-button>
             </gr-tooltip-content>
           </div>

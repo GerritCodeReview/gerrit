4565b05632ef0b165aa6e3c821e7768c509fb903
Kamil Musin <kamilm@google.com>
2022-08-03 15:25:04 +0200

    Fix changing of copy button into checkmark.
    When migrated from span to gr-icon, the logic got broken
    
    Google-Bug-Id: b/241224147
    Release-Notes: skip
    Change-Id: Ief29c7509d9bc1bdaea2937dbc4480231a66fb8a

--- polygerrit-ui/app/elements/shared/gr-copy-clipboard/gr-copy-clipboard.ts
+++ polygerrit-ui/app/elements/shared/gr-copy-clipboard/gr-copy-clipboard.ts
@@ -12,6 +12,7 @@ import {ifDefined} from 'lit/directives/if-defined';
 import {LitElement, css, html} from 'lit';
 import {customElement, property} from 'lit/decorators';
 import {GrButton} from '../gr-button/gr-button';
+import {GrIcon} from '../gr-icon/gr-icon';
 
 const COPY_TIMEOUT_MS = 1000;
 
@@ -119,12 +120,12 @@ export class GrCopyClipboard extends LitElement {
 
     this.text = queryAndAssert<HTMLInputElement>(this, '#input').value;
     assertIsDefined(this.text, 'text');
-    this.iconEl.innerText = 'check';
+    this.iconEl.icon = 'check';
     navigator.clipboard.writeText(this.text);
-    setTimeout(() => (this.iconEl.innerText = 'content_copy'), COPY_TIMEOUT_MS);
+    setTimeout(() => (this.iconEl.icon = 'content_copy'), COPY_TIMEOUT_MS);
   }
 
-  private get iconEl(): HTMLSpanElement {
-    return queryAndAssert<HTMLSpanElement>(this, '#icon');
+  private get iconEl(): GrIcon {
+    return queryAndAssert<GrIcon>(this, '#icon');
   }
 }

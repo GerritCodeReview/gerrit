eb8f53574b09136e5a6f63172ebfe524569a6667
Ben Rohlfs <brohlfs@google.com>
2022-08-03 09:28:32 +0200

    Better use of horizontal space for check runs
    Change the appearance of the collapsed version of the check runs panel:
    Do not completely collapse it, but show a 90px version without buttons
    and event listeners, etc. Allow the user to click anywhere for expanding
    it.
    
    Automatically switch between the collapsed and expanded state of the
    check runs panel when the threshold of 1200 pixels screen width is
    crossed.
    
    In expanded state set the width of the runs panel to 20%, which is
    smaller than it was before. This allows users with medium screen
    width (1200-1500px) to see more content of check results.
    
    Drop the max-width:1600px rule for check results. So for really large
    screen we just use all the available spaec for showing the results
    table.
    
    Release-Notes: skip
    Google-Bug-Id: b/235185417
    Change-Id: Ie915a1e56a8ea80b705562641fa25b36180f4224

--- polygerrit-ui/app/elements/checks/gr-checks-results.ts
+++ polygerrit-ui/app/elements/checks/gr-checks-results.ts
@@ -853,7 +853,6 @@ export class GrChecksResults extends LitElement {
         }
         .headerTopRow,
         .headerBottomRow {
-          max-width: 1600px;
           display: flex;
           justify-content: space-between;
           align-items: flex-end;
@@ -979,7 +978,6 @@ export class GrChecksResults extends LitElement {
         }
         .noResultsMessage {
           width: 100%;
-          max-width: 1600px;
           margin-top: var(--spacing-m);
           background-color: var(--background-color-primary);
           box-shadow: var(--elevation-level-1);
@@ -988,7 +986,6 @@ export class GrChecksResults extends LitElement {
         }
         table.resultsTable {
           width: 100%;
-          max-width: 1600px;
           table-layout: fixed;
           margin-top: var(--spacing-m);
           background-color: var(--background-color-primary);

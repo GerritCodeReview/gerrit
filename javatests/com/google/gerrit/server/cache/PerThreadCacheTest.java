d72fcb05a0da4b309423e71ef7a17949f017769e
Edwin Kempin <ekempin@google.com>
2022-08-05 10:37:05 +0200

    Fix warnings
    * CommitValidators: Statement unnecessarily nested within else clause.
    * PerThreadCacheTest: The import
      com.google.common.truth.Truth8.assertThat is never used
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Release-Notes: skip
    Change-Id: I0aa9c765e21fa9c2b83b7e9216113287a9967cb2

--- javatests/com/google/gerrit/server/cache/PerThreadCacheTest.java
+++ javatests/com/google/gerrit/server/cache/PerThreadCacheTest.java
@@ -15,7 +15,6 @@
 package com.google.gerrit.server.cache;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth8.assertThat;
 import static com.google.gerrit.testing.GerritJUnit.assertThrows;
 
 import java.util.function.Supplier;

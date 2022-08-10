c3e9468b9a60f5677aab7d9808a69560efb7fa39
Milutin Kristofic <milutin@google.com>
2022-08-10 11:28:32 +0200

    Push Notifications - fix race condition
    Since this code can be called from multiple clients (tabs), race
    condition was quite common.
    
    Race condition: latestUpdateTimestampMs is set after await,
    so until this promise is resolved, many other callers can call
    this function and call promise. Unit test shows the case and how
    loading flag fix this issue.
    
    Google-Bug-Id: b/241946359
    Release-Notes: skip
    Change-Id: I0505cd243f33f09fe1bfd5c4f142ef6a3606cb5c

--- polygerrit-ui/app/workers/service-worker-class_test.ts
+++ polygerrit-ui/app/workers/service-worker-class_test.ts
@@ -9,6 +9,7 @@ import {
   createAccountDetailWithId,
   createParsedChange,
 } from '../test/test-data-generators';
+import {mockPromise} from '../test/test-utils';
 import {ParsedChangeInfo} from '../types/types';
 import {parseDate} from '../utils/date-util';
 import {ServiceWorker} from './service-worker-class';
@@ -46,5 +47,30 @@ suite('service worker class tests', () => {
       .returns(Promise.resolve([change]));
     const changes = await serviceWorker.getChangesToNotify(account);
     assert.equal(changes[0], change);
+  });
+
+  test('check race condition', async () => {
+    const promise = mockPromise<ParsedChangeInfo[]>();
+    const getLatestAttentionSetChangesStub = sinon
+      .stub(serviceWorker, 'getLatestAttentionSetChanges')
+      .returns(promise);
+    const account = createAccountDetailWithId();
+    const t1 = parseDate('2016-01-12 20:20:00' as Timestamp).getTime();
+    const t2 = '2016-01-12 20:30:00' as Timestamp;
+    serviceWorker.latestUpdateTimestampMs = t1;
+    const change: ParsedChangeInfo = {
+      ...createParsedChange(),
+      attention_set: {
+        [`${account._account_id}`]: {
+          account,
+          last_update: t2,
+        },
+      },
+    };
+    serviceWorker.getChangesToNotify(account);
+    serviceWorker.getChangesToNotify(account);
+    promise.resolve([change]);
+    await serviceWorker.getChangesToNotify(account);
+    assert.isTrue(getLatestAttentionSetChangesStub.calledOnce);
   });
 });

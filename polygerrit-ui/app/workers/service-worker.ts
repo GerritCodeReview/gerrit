672451fe7a6ace45b4029c0015a23d771fe60ff3
Milutin Kristofic <milutin@google.com>
2022-08-11 10:51:27 +0200

    Save and load Service worker state with indexDb
    We cannot rely on a state in a service worker, because every time
    service worker starts or stops, new instance is created.
    
    Service workers don't have access to local storage, but they have
    access to indexDb, so we store state in it.
    
    Release-Notes: skip
    Change-Id: Ie2eccbfcf7ca04e83975bbd04c41af9f87dcdc57

--- polygerrit-ui/app/workers/service-worker.ts
+++ polygerrit-ui/app/workers/service-worker.ts
@@ -6,7 +6,10 @@
 
 import {AccountDetailInfo} from '../api/rest-api';
 import {ServiceWorkerMessageType} from '../services/service-worker-installer';
+import {getServiceWorkerState} from '../utils/indexdb-util';
 import {ServiceWorker} from './service-worker-class';
+
+// TODO(Milutin): Put all the business logic of setting things up into the class.
 
 /**
  * `self` is for a worker what `window` is for the web app. It is called
@@ -15,21 +18,30 @@ import {ServiceWorker} from './service-worker-class';
  */
 const ctx = self as {} as ServiceWorkerGlobalScope;
 
-ctx.addEventListener('message', async event => {
-  if (event.data?.type !== ServiceWorkerMessageType.TRIGGER_NOTIFICATIONS) {
+ctx.addEventListener('message', e => {
+  if (e.data?.type !== ServiceWorkerMessageType.TRIGGER_NOTIFICATIONS) {
     // Only this notification message type exists, but we do not throw error.
     return;
   }
-  const account = event.data?.account as AccountDetailInfo | undefined;
-  // Message always contains account, but we do not throw error.
-  if (!account?._account_id) return;
-  const latestAttentionChanges = await serviceWorker.getChangesToNotify(
-    account
-  );
-  // TODO(milutin): Implement handling more than 1 change
-  if (latestAttentionChanges && latestAttentionChanges.length > 0) {
-    serviceWorker.showNotification(latestAttentionChanges[0], account);
-  }
+  const showNotification = async () => {
+    const account = e.data?.account as AccountDetailInfo | undefined;
+    // Message always contains account, but we do not throw error.
+    if (!account?._account_id) return;
+    /**
+     * We cannot rely on a state in a service worker, because every time
+     * service worker starts or stops, new instance is created. So every time
+     * there is new instance we load state from indexdb.
+     */
+    serviceWorker.loadState(await getServiceWorkerState());
+    const latestAttentionChanges = await serviceWorker.getChangesToNotify(
+      account
+    );
+    // TODO(milutin): Implement handling more than 1 change
+    if (latestAttentionChanges && latestAttentionChanges.length > 0) {
+      serviceWorker.showNotification(latestAttentionChanges[0], account);
+    }
+  };
+  e.waitUntil(showNotification());
 });
 
 // Code based on code sample from

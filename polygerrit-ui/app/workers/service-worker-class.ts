235acb78435d3de5602252a9509778b7aae6148e
Milutin Kristofic <milutin@google.com>
2022-08-11 10:51:27 +0200

    Save and load Service worker state with indexDb
    We cannot rely on a state in a service worker, because every time
    service worker starts or stops, new instance is created.
    
    Service workers don't have access to local storage, but they have
    access to indexDb, so we store state in it.
    
    This change also put all the business logic of setting things up
    into the class.
    
    Release-Notes: skip
    Change-Id: Ie2eccbfcf7ca04e83975bbd04c41af9f87dcdc57

--- polygerrit-ui/app/workers/service-worker-class.ts
+++ polygerrit-ui/app/workers/service-worker-class.ts
@@ -9,16 +9,97 @@ import {getReason} from '../utils/attention-set-util';
 import {readResponsePayload} from '../elements/shared/gr-rest-api-interface/gr-rest-apis/gr-rest-api-helper';
 import {filterAttentionChangesAfter} from '../utils/service-worker-util';
 import {AccountDetailInfo} from '../api/rest-api';
-import {TRIGGER_NOTIFICATION_UPDATES_MS} from '../services/service-worker-installer';
+import {
+  ServiceWorkerMessageType,
+  TRIGGER_NOTIFICATION_UPDATES_MS,
+} from '../services/service-worker-installer';
 import {GerritView} from '../services/router/router-model';
 import {generateUrl} from '../utils/router-util';
+import {
+  getServiceWorkerState,
+  putServiceWorkerState,
+} from './service-worker-indexdb';
 
 export class ServiceWorker {
   constructor(private ctx: ServiceWorkerGlobalScope) {}
 
-  latestUpdateTimestampMs = Date.now();
+  private latestUpdateTimestampMs = Date.now();
 
-  showNotification(change: ParsedChangeInfo, account: AccountDetailInfo) {
+  /**
+   * We cannot rely on a state in a service worker, because every time
+   * service worker starts or stops, new instance is created. So every time
+   * there is new instance we load state from indexdb.
+   */
+  async init() {
+    await this.loadState();
+    this.ctx.addEventListener('message', e => this.onMessage(e));
+    this.ctx.addEventListener('notificationclick', e =>
+      this.onNotificationClick(e)
+    );
+  }
+
+  // private but used in test
+  saveState() {
+    return putServiceWorkerState({
+      latestUpdateTimestampMs: this.latestUpdateTimestampMs,
+    });
+  }
+
+  private async loadState() {
+    const state = await getServiceWorkerState();
+    if (state) {
+      this.latestUpdateTimestampMs = state.latestUpdateTimestampMs;
+    }
+  }
+
+  private onMessage(e: ExtendableMessageEvent) {
+    if (e.data?.type !== ServiceWorkerMessageType.TRIGGER_NOTIFICATIONS) {
+      // Only this notification message type exists, but we do not throw error.
+      return;
+    }
+    e.waitUntil(
+      this.showLatestAttentionChangeNotification(
+        e.data?.account as AccountDetailInfo | undefined
+      )
+    );
+  }
+
+  private onNotificationClick(e: NotificationEvent) {
+    e.notification.close();
+    e.waitUntil(this.openWindow(e.notification.data.url));
+  }
+
+  private async showLatestAttentionChangeNotification(
+    account?: AccountDetailInfo
+  ) {
+    // Message always contains account, but we do not throw error.
+    if (!account?._account_id) return;
+    const latestAttentionChanges = await this.getChangesToNotify(account);
+    // TODO(milutin): Implement handling more than 1 change
+    if (latestAttentionChanges && latestAttentionChanges.length > 0) {
+      this.showNotification(latestAttentionChanges[0], account);
+    }
+  }
+
+  // Code based on code sample from
+  // https://developer.mozilla.org/en-US/docs/Web/API/Clients/openWindow
+  private async openWindow(url?: string) {
+    if (!url) return;
+    const clientsArr = await this.ctx.clients.matchAll({type: 'window'});
+    try {
+      let client = clientsArr.find(c => c.url === url);
+      if (!client)
+        client = (await this.ctx.clients.openWindow(url)) ?? undefined;
+      await client?.focus();
+    } catch (e) {
+      console.error(`Cannot open window about notified change - ${e}`);
+    }
+  }
+
+  private showNotification(
+    change: ParsedChangeInfo,
+    account: AccountDetailInfo
+  ) {
     const body = getReason(undefined, account, change);
     const changeUrl = generateUrl({
       view: GerritView.CHANGE,
@@ -35,6 +116,7 @@ export class ServiceWorker {
     this.ctx.registration.showNotification(change.subject, {body, data});
   }
 
+  // private but used in test
   async getChangesToNotify(account: AccountDetailInfo) {
     // We throttle polling, since there can be many clients triggerring
     // always only one service worker.
@@ -45,6 +127,7 @@ export class ServiceWorker {
     }
     const prevLatestUpdateTimestampMs = this.latestUpdateTimestampMs;
     this.latestUpdateTimestampMs = Date.now();
+    await this.saveState();
     const changes = await this.getLatestAttentionSetChanges();
     const latestAttentionChanges = filterAttentionChangesAfter(
       changes,
@@ -54,6 +137,7 @@ export class ServiceWorker {
     return latestAttentionChanges;
   }
 
+  // private but used in test
   async getLatestAttentionSetChanges(): Promise<ParsedChangeInfo[]> {
     // TODO(milutin): Implement more generic query builder
     const response = await fetch(

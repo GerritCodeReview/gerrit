98ead7efa4cd873391355ac34bf5be0a0f233a1c
Ben Rohlfs <brohlfs@google.com>
2022-08-16 09:36:01 +0200

    Improve performance of `HtmlPatched`
    It was recommended in change 342881 that we exit early, if we have
    already seen an instance of a `TemplateStringsArray`. This saves
    us the need to do the string concatenation for they key and the map
    lookup.
    
    Also add tests.
    
    Release-Notes: skip
    Change-Id: Idefbb47ca6ed4801584602a443b5979f0dfab013

--- polygerrit-ui/app/utils/lit-util.ts
+++ polygerrit-ui/app/utils/lit-util.ts
@@ -5,22 +5,46 @@
  */
 import {html, TemplateResult} from 'lit';
 
+/**
+ * This is a patched version of html`` to work around this Chrome bug:
+ * https://bugs.chromium.org/p/v8/issues/detail?id=13190.
+ *
+ * The problem is that Chrome should guarantee that the TemplateStringsArray
+ * is always the same instance, if the strings themselves are equal, but that
+ * guarantee seems to be broken. So we are maintaining a map from
+ * "concatenated strings" to TemplateStringsArray. If "concatenated strings"
+ * are equal, then rather return the already known instance of
+ * TemplateStringsArray, so html`` can use its strict equality check on it.
+ */
 export class HtmlPatched {
   constructor(private readonly reporter?: (key: string) => void) {}
 
-  private readonly lookup = new Map<string, TemplateStringsArray>();
+  /**
+   * If `strings` are in this set, then we are sure that they are also in the
+   * map, and that we will not run into the issue of "same key, but different
+   * strings array". So this set allows us to optimize performance a bit, and
+   * call the normal html`` early.
+   */
+  private readonly lookupSet = new Set<TemplateStringsArray>();
+
+  private readonly lookupMap = new Map<string, TemplateStringsArray>();
 
   html: (
     strings: TemplateStringsArray,
     ...values: unknown[]
   ) => TemplateResult<1> = (strings, ...values) => {
+    if (this.lookupSet.has(strings)) {
+      return this.nativeHtml(strings, ...values);
+    }
+
     const key = strings.join('\0');
-    const oldStrings = this.lookup.get(key);
+    const oldStrings = this.lookupMap.get(key);
     const newStrings = strings;
 
     if (oldStrings === undefined) {
-      this.lookup.set(key, newStrings);
-      return html(newStrings, ...values);
+      this.lookupSet.add(newStrings);
+      this.lookupMap.set(key, newStrings);
+      return this.nativeHtml(newStrings, ...values);
     }
 
     if (oldStrings !== newStrings) {
@@ -29,6 +53,11 @@ export class HtmlPatched {
       console.warn(`HtmlPatcher was required for '${key.substring(0, 100)}'.`);
       this.reporter?.(key);
     }
-    return html(oldStrings, ...values);
+    return this.nativeHtml(oldStrings, ...values);
   };
+
+  // Allows spying on calls in tests.
+  nativeHtml(strings: TemplateStringsArray, ...values: unknown[]) {
+    return html(strings, values);
+  }
 }

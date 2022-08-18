eff6e6a5173ee525ca0a5f32e47816b4e78efd66
Chris Poucet <poucet@google.com>
2022-08-18 12:44:58 +0200

    Simplify star API in backend.
    Google-Bug-Id: b/242827284
    Release-Notes: skip
    Change-Id: I72c8990d590b75a517fb20e66f4df27ce1a58d53

--- java/com/google/gerrit/server/StarredChangesUtil.java
+++ java/com/google/gerrit/server/StarredChangesUtil.java
@@ -111,6 +111,11 @@ public class StarredChangesUtil {
     }
   }
 
+  public enum Operation {
+    ADD,
+    REMOVE
+  }
+
   @AutoValue
   public abstract static class StarRef {
     private static final StarRef MISSING =
@@ -159,8 +164,6 @@ public class StarredChangesUtil {
   }
 
   public static final String DEFAULT_LABEL = "star";
-  public static final ImmutableSortedSet<String> DEFAULT_LABELS =
-      ImmutableSortedSet.of(DEFAULT_LABEL);
 
   private final GitRepositoryManager repoManager;
   private final GitReferenceUpdated gitRefUpdated;
@@ -201,22 +204,20 @@ public class StarredChangesUtil {
   }
 
   public NavigableSet<String> star(
-      Account.Id accountId,
-      Project.NameKey project,
-      Change.Id changeId,
-      Set<String> labelsToAdd,
-      Set<String> labelsToRemove)
+      Account.Id accountId, Project.NameKey project, Change.Id changeId, Operation op)
       throws IllegalLabelException {
     try (Repository repo = repoManager.openRepository(allUsers)) {
       String refName = RefNames.refsStarredChanges(changeId, accountId);
       StarRef old = readLabels(repo, refName);
 
       NavigableSet<String> labels = new TreeSet<>(old.labels());
-      if (labelsToAdd != null) {
-        labels.addAll(labelsToAdd);
-      }
-      if (labelsToRemove != null) {
-        labels.removeAll(labelsToRemove);
+      switch (op) {
+        case ADD:
+          labels.add(DEFAULT_LABEL);
+          break;
+        case REMOVE:
+          labels.remove(DEFAULT_LABEL);
+          break;
       }
 
       if (labels.isEmpty()) {

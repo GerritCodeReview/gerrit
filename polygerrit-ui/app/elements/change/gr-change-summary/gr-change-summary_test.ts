e1c86cc8c192e27062fb2d445277ad2086f86c0c
Kamil Musin <kamilm@google.com>
2022-08-23 16:47:20 +0200

    Improve change summary chips.
    Following UX mocks following changes are made:
    - Drafts are grey
    - Drafts are positioned last
    - Drafts changed icon to rate_review
    - Icon for resolved threads is filled
    
    Screenshot: https://imgur.com/a/JOxPbFB
    Release-Notes: skip
    Google-Bug-Id: b/187487742
    Change-Id: I976b739b04e6284a52f31733d96c63c68e025df9

--- polygerrit-ui/app/elements/change/gr-change-summary/gr-change-summary_test.ts
+++ polygerrit-ui/app/elements/change/gr-change-summary/gr-change-summary_test.ts
@@ -48,13 +48,6 @@ suite('gr-change-summary test', () => {
             <tr>
               <td class="key">Comments</td>
               <td class="value">
-                <gr-summary-chip
-                  category="drafts"
-                  icon="edit"
-                  styletype="warning"
-                >
-                  3 drafts
-                </gr-summary-chip>
                 <gr-summary-chip category="unresolved" styletype="warning">
                   <gr-avatar-stack
                     avatarSize="16"
@@ -73,9 +66,18 @@ suite('gr-change-summary test', () => {
                 <gr-summary-chip
                   category="show all"
                   icon="mark_chat_read"
+                  iconFilled
                   styletype="check"
                 >
                   1 resolved
+                </gr-summary-chip>
+                <gr-summary-chip
+                  category="drafts"
+                  icon="rate_review"
+                  iconFilled
+                  styletype="check"
+                >
+                  3 drafts
                 </gr-summary-chip>
               </td>
             </tr>

e92ac3ab22e021fcd63bc898bf6a94e017ae1a2f
Dhruv Srivastava <dhruvsri@google.com>
2022-08-23 13:21:26 +0000

    Apply "Show avatar for mentioned users"
    Change-Id: I553abdac5af46f25afaa859dde4faaab900d7ae0

--- polygerrit-ui/app/utils/account-util.ts
+++ polygerrit-ui/app/utils/account-util.ts
@@ -79,6 +79,10 @@ export function uniqueDefinedAvatar(
   );
 }
 
+export function isDetailedAccount(account?: AccountInfo) {
+  return account && account._account_id;
+}
+
 /**
  * Get account in pseudonymized form, that can be send to the backend.
  *

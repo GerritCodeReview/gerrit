8aaed618031d2d90e29fbde49cf960876e7cb56a
Frank Borden <frankborden@google.com>
2022-08-25 16:58:49 +0200

    Style gr-markdown to match gr-formatted-text
    Also fixes block quotes because '>' was being escaped.
    
    Examples: https://imgur.com/a/SFnL64P
    
    Release-Notes: skip
    Change-Id: I9017ef3d69a31c0ec2c4de50b683788e2207c18b

--- polygerrit-ui/app/elements/shared/gr-markdown/gr-markdown.ts
+++ polygerrit-ui/app/elements/shared/gr-markdown/gr-markdown.ts
@@ -20,11 +20,13 @@ import {
 /**
  * This element renders markdown and also applies some regex replacements to
  * linkify key parts of the text defined by the host's config.
+ *
+ * TODO: Remove gr-formatted-text once this is rolled out.
  */
 @customElement('gr-markdown')
 export class GrMarkdown extends LitElement {
   @property({type: String})
-  markdown = '';
+  markdown?: string;
 
   @state()
   private repoCommentLinks: CommentLinks = {};
@@ -37,8 +39,40 @@ export class GrMarkdown extends LitElement {
    */
   static override styles = [
     css`
-      [slot='markdown-html'] p {
-        margin: 0;
+      a {
+        color: var(--link-color);
+      }
+      p,
+      ul,
+      code,
+      blockquote {
+        margin: 0 0 var(--spacing-m) 0;
+        max-width: var(--gr-formatted-text-prose-max-width, none);
+      }
+      blockquote {
+        border-left: var(--spacing-xxs) solid var(--comment-quote-marker-color);
+        padding: 0 var(--spacing-m);
+      }
+      code {
+        background-color: var(--background-color-secondary);
+        border: var(--spacing-xxs) solid var(--border-color);
+        display: block;
+        font-family: var(--monospace-font-family);
+        font-size: var(--font-size-code);
+        line-height: var(--line-height-mono);
+        margin: var(--spacing-m) 0;
+        padding: var(--spacing-xxs) var(--spacing-s);
+        overflow-x: auto;
+        /* pre will preserve whitespace and linebreaks but not wrap */
+        white-space: pre;
+      }
+      /* code within a sentence needs display:inline to shrink and not take a
+         whole row */
+      p code {
+        display: inline;
+      }
+      li {
+        margin-left: var(--spacing-xl);
       }
     `,
   ];
@@ -58,7 +92,8 @@ export class GrMarkdown extends LitElement {
 
     // Escaping the message should be done first to make sure user's literal
     // input does not get rendered without affecting html added in later steps.
-    const escaped = htmlEscape(this.markdown).toString();
+    const escaped = htmlEscape(this.markdown ?? '').toString();
+
     // Turn universally identifiable URLs into links. Ex: www.google.com. The
     // markdown library inside marked-element does this too, but is more
     // conservative and misses some URLs like "google.com" without "www" prefix.
@@ -69,6 +104,7 @@ export class GrMarkdown extends LitElement {
       linkedNormalUrls,
       this.repoCommentLinks
     );
+
     // Apply the host's config-specific regex replacements to write arbitrary
     // html. Most examples seen in the wild are also used for linking but with
     // finer control over the rendered text. Ex: "Bug 12345" => "#12345"
@@ -76,14 +112,21 @@ export class GrMarkdown extends LitElement {
       linkedFromConfig,
       this.repoCommentLinks
     );
+
     // Final sanitization should preserve our modifications but sort out any XSS
     // attacks that may sneak in. Many polymer and lit parsers do not expect a
     // TrustedHTML object from sanitization and so it is manually stringified.
+    // marked-element has sanitize functionality but it's marked 'do not use'.
+    // See: https://marked.js.org/#usage
     const sanitized = sanitizeHtml(htmledFromConfig).toString();
+
+    // Unescape block quotes '>'. This is slightly dangerous as '>' can be used
+    // in HTML fragments, but it is insufficient on it's own.
+    const quotesUnescaped = sanitized.replace(/(^|\n)&gt;/g, '$1>');
 
     // The child with slot is optional but allows us control over the styling.
     return html`
-      <marked-element .markdown=${sanitized} .breaks=${true}>
+      <marked-element .markdown=${quotesUnescaped} .breaks=${true}>
         <div slot="markdown-html"></div>
       </marked-element>
     `;

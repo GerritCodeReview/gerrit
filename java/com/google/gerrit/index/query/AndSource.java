b661424cd5534a667eb746deff6455cfd33b435e
Kaushik Lingarkar <kaushik.lingarkar@linaro.org>
2022-08-31 16:53:04 -0700

    Paginate no-limit queries
    Currently, no-limit queries do a single index search with a rather
    large limit (around the size of change index). For Elasticsearch
    index backend, this can be problematic as Elasticsearch's REST
    client will fail with an error like [1] if too many changes have
    to be returned. The REST client has a default limit of 100MB for
    content it can process. This effectively implies that no-limit
    queries with ES index backend are likely non-functional for most
    sites as the 100MB limit is reached by as low as ~50k changes.
    
    Changing this default limit is not recommended as it can overload
    ES data nodes and the client as well. Instead, this change updates
    no-limit queries to paginate rather than doing a single index search
    with a large limit. It is recommended to set an appropriate value
    for 'index.maxPageSize' to avoid the error [1] especially when
    'index.pageSizeMultiplier' is set to value greater than 1.
    
    Here are some stats with a Lucene based site and ~20k docs in open
    changes index and ~4M in closed changes index (with ~1M abandoned
    and ~3M merged).
    
                                   status:open     staus:abandoned
                                   no-limit        no-limit
    
    without this change            7.6s            436s
    
    with change
    paginationType=OFFSET          7.9s            2622s
    pageSizeMultiplier=1
    
    with change
    paginationType=SEARCH_AFTER    7.8s            480s
    pageSizeMultiplier=1
    
    with change
    paginationType=OFFSET          7.4s            417s
    pageSizeMultiplier=10
    
    with change
    paginationType=SEARCH_AFTER    7.7s            418s
    pageSizeMultiplier=10
    
    If 'index.pageSizeMultiplier' is set to 1 (default), we update it to
    10 for no-limit queries as it helps improve performance and also
    prevents no-limit queries from severely degrading when pagination type
    is OFFSET.
    
    [1] entity content is too long [..] for the configured buffer limit [104857600]
    
    Release-Notes: no-limit queries are now usable with Elasticsearch index backend
    Change-Id: Ifb1f6f5411140c430f2520fb252e688b67d5333c

--- java/com/google/gerrit/index/query/AndSource.java
+++ java/com/google/gerrit/index/query/AndSource.java
@@ -24,6 +24,7 @@ import com.google.common.collect.Ordering;
 import com.google.gerrit.exceptions.StorageException;
 import com.google.gerrit.index.IndexConfig;
 import com.google.gerrit.index.PaginationType;
+import com.google.gerrit.index.QueryOptions;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
@@ -107,18 +108,21 @@ public class AndSource<T> extends AndPredicate<T>
           }
 
           if (last != null && source instanceof Paginated) {
-            // If our source is a paginated source and we skipped at
-            // least one of its results, we may not have filled the full
-            // limit the caller wants.  Restart the source and continue.
+            // If our source is paginated, restart the source and continue
+            // when we skip at least one of the results or when page size of
+            // the initial search is less than the limit (which is true in
+            // case of no-limit queries).
             //
             @SuppressWarnings("unchecked")
             Paginated<T> p = (Paginated<T>) source;
-            final int limit = p.getOptions().limit();
+            QueryOptions opts = p.getOptions();
+            final int limit = opts.limit();
+            int pageSize = opts.pageSize();
+            int pageSizeMultiplier = opts.pageSizeMultiplier();
             Object searchAfter = resultSet.searchAfter();
-            int pageSize = limit;
             int nextStart = pageResultSize;
             while (pageResultSize == pageSize && r.size() < limit) {
-              pageSize = getNextPageSize(pageSize);
+              pageSize = getNextPageSize(pageSize, pageSizeMultiplier);
               ResultSet<T> next =
                   indexConfig.paginationType().equals(PaginationType.SEARCH_AFTER)
                       ? p.restart(searchAfter, pageSize)
@@ -209,10 +213,10 @@ public class AndSource<T> extends AndPredicate<T>
     return (DataSource<T>) pred;
   }
 
-  private int getNextPageSize(int pageSize) {
+  private int getNextPageSize(int pageSize, int pageSizeMultiplier) {
     List<Integer> possiblePageSizes = new ArrayList<>(3);
     try {
-      possiblePageSizes.add(Math.multiplyExact(pageSize, indexConfig.pageSizeMultiplier()));
+      possiblePageSizes.add(Math.multiplyExact(pageSize, pageSizeMultiplier));
     } catch (ArithmeticException e) {
       possiblePageSizes.add(Integer.MAX_VALUE);
     }

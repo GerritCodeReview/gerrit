9a79cdad7e6b3847beaf67e48641c7206503c671
Ben Rohlfs <brohlfs@google.com>
2022-09-01 17:22:24 +0200

    Add attempt dropdown to Checks UI
    Each CheckRun can report results for each patchset of the change. But
    for each patchset a run can have multiple 'attempts'. If that is the
    case, then the Checks UI shows only the latest result by default. The
    only way for the user to get at other attempts was to click the run
    chip on the left and then select the radio button for the attempt that
    they want to look at.
    
    This was neither discoverable nor was is working very well for browsing
    attempts of more than one run. Thus users were asking for a dropdown to
    choose from all available attempts (see referenced bugs).
    
    This change introduces a dropdown menu in the top right corner next to
    the patchset choice dropdown. But this dropdown is only visible, if
    there is at least one run with more than one attempt available.
    
    We are adding the attempt choice to the model and use that also for
    the radio choice of individual runs, which makes the implementation a
    lot simpler compared to the `selectedAttempts` map that we were using.
    The users will lose the ability to select mutiple runs and different
    attempts for each run. But this is very unlikely to be of interest for
    users.
    
    The 'attempt choice' is modeled as either being a number or one of two
    enum values LATEST or ALL. So we also offer the user to see all
    attempts (of all runs even) at once.
    
    There is also a URL parameter `attempt` that we are tying to the new
    model instead of just using the parameter adhoc. But there will be some
    follow-ups regarding URL parameters for Checks, so this will get some
    polish in other changes.
    
    We are also adding some tests for yet untested stuff.
    
    Screenshots https://imgur.com/a/y9GX6Ni
    
    Release-Notes: skip
    Google-Bug-Id: b/197733923
    Google-Bug-Id: b/217423588
    Change-Id: Id1142d48345b23d0ffa2b0eafa63007425f6ac1f

--- polygerrit-ui/app/elements/checks/gr-checks-results.ts
+++ polygerrit-ui/app/elements/checks/gr-checks-results.ts
@@ -23,6 +23,13 @@ import {
 import {sharedStyles} from '../../styles/shared-styles';
 import {CheckRun, RunResult} from '../../models/checks/checks-model';
 import {
+  ALL_ATTEMPTS,
+  AttemptChoice,
+  attemptChoiceLabel,
+  isAttemptChoice,
+  LATEST_ATTEMPT,
+  sortAttemptChoices,
+  stringToAttemptChoice,
   allResults,
   createFixAction,
   firstPrimaryLink,
@@ -34,7 +41,7 @@ import {
   secondaryLinks,
   tooltipForLink,
 } from '../../models/checks/checks-util';
-import {assertIsDefined, assert} from '../../utils/common-util';
+import {assertIsDefined, assert, unique} from '../../utils/common-util';
 import {modifierPressed, toggleClass, whenVisible} from '../../utils/dom-util';
 import {durationString} from '../../utils/date-util';
 import {charsOnly} from '../../utils/string-util';
@@ -66,6 +73,7 @@ import {getAppContext} from '../../services/app-context';
 import {when} from 'lit/directives/when.js';
 import {KnownExperimentId} from '../../services/flags/flags';
 import {HtmlPatched} from '../../utils/lit-util';
+import {DropdownItem} from '../shared/gr-dropdown-list/gr-dropdown-list';
 
 /**
  * Firing this event sets the regular expression of the results filter.
@@ -771,12 +779,8 @@ export class GrChecksResults extends LitElement {
   @state()
   latestPatchsetNumber: PatchSetNumber | undefined = undefined;
 
-  /** Maps checkName to selected attempt number. `undefined` means `latest`. */
-  @property({attribute: false})
-  selectedAttempts: Map<string, number | undefined> = new Map<
-    string,
-    number | undefined
-  >();
+  @state()
+  selectedAttempt: AttemptChoice = LATEST_ATTEMPT;
 
   /** Maintains the state of which result sections should show all results. */
   @state()
@@ -825,6 +829,11 @@ export class GrChecksResults extends LitElement {
       this,
       () => this.getChecksModel().checksSelectedPatchsetNumber$,
       x => (this.checksPatchsetNumber = x)
+    );
+    subscribe(
+      this,
+      () => this.getChecksModel().checksSelectedAttemptNumber$,
+      x => (this.selectedAttempt = x)
     );
     subscribe(
       this,
@@ -899,8 +908,10 @@ export class GrChecksResults extends LitElement {
         .notLatest .headerTopRow .right .goToLatest {
           display: block;
         }
+        .headerTopRow .right > * {
+          margin-left: var(--spacing-m);
+        }
         .headerTopRow .right .goToLatest gr-button {
-          margin-right: var(--spacing-m);
           --gr-button-padding: var(--spacing-s) var(--spacing-m);
         }
         .headerBottomRow gr-icon {
@@ -1071,6 +1082,7 @@ export class GrChecksResults extends LitElement {
       header: true,
       notLatest: !!this.checksPatchsetNumber,
     };
+    const attemptItems = this.createAttemptDropdownItems();
     return html`
       <div class=${classMap(headerClasses)}>
         <div class="headerTopRow">
@@ -1087,6 +1099,14 @@ export class GrChecksResults extends LitElement {
                 >Go to latest patchset</gr-button
               >
             </div>
+            ${when(
+              attemptItems.length > 0,
+              () => html` <gr-dropdown-list
+                value=${this.selectedAttempt ?? 0}
+                .items=${attemptItems}
+                @value-change=${this.onAttemptSelected}
+              ></gr-dropdown-list>`
+            )}
             <gr-dropdown-list
               value=${this.checksPatchsetNumber ??
               this.latestPatchsetNumber ??
@@ -1211,6 +1231,12 @@ export class GrChecksResults extends LitElement {
     ></gr-checks-action>`;
   }
 
+  private onAttemptSelected(e: CustomEvent<{value: string | undefined}>) {
+    const attempt = stringToAttemptChoice(e.detail.value);
+    assertIsDefined(attempt, `unexpected attempt choice ${e.detail.value}`);
+    this.getChecksModel().updateStateSetAttempt(attempt);
+  }
+
   private onPatchsetSelected(e: CustomEvent<{value: string}>) {
     const patchset = Number(e.detail.value);
     assert(!isNaN(patchset), 'selected patchset must be a number');
@@ -1219,6 +1245,23 @@ export class GrChecksResults extends LitElement {
 
   private goToLatestPatchset() {
     this.getChecksModel().setPatchset(undefined);
+  }
+
+  private createAttemptDropdownItems() {
+    if (this.runs.every(run => run.isSingleAttempt)) return [];
+    const attempts: AttemptChoice[] = this.runs
+      .map(run => run.attempt ?? 0)
+      .filter(isAttemptChoice)
+      .filter(unique);
+    attempts.push(LATEST_ATTEMPT);
+    attempts.push(ALL_ATTEMPTS);
+    const items: DropdownItem[] = attempts.sort(sortAttemptChoices).map(a => {
+      return {
+        value: a,
+        text: attemptChoiceLabel(a),
+      };
+    });
+    return items;
   }
 
   private createPatchsetDropdownItems() {
@@ -1244,7 +1287,7 @@ export class GrChecksResults extends LitElement {
   renderFilter() {
     const runs = this.runs.filter(
       run =>
-        this.isRunSelected(run) && isAttemptSelected(this.selectedAttempts, run)
+        this.isRunSelected(run) && isAttemptSelected(this.selectedAttempt, run)
     );
     if (this.selectedRuns.length === 0 && allResults(runs).length <= 3) {
       if (this.filterRegExp.source.length > 0) {
@@ -1279,7 +1322,7 @@ export class GrChecksResults extends LitElement {
     const isWarningOrError =
       category === Category.WARNING || category === Category.ERROR;
     const allRuns = this.runs.filter(run =>
-      isAttemptSelected(this.selectedAttempts, run)
+      isAttemptSelected(this.selectedAttempt, run)
     );
     const all = allRuns.reduce(
       (results: RunResult[], run) => [

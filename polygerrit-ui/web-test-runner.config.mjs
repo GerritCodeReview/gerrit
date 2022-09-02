42bd3a0bc8f317d365c75cdb27d4eb10ed55847a
Frank Borden <frankborden@google.com>
2022-09-02 11:05:03 +0200

    Bazel target for Web Test Runner
    Release-Notes: skip
    Change-Id: Ifa761684f9bfa1a9c5f8467feab2620e7ee9e0ce

--- polygerrit-ui/web-test-runner.config.mjs
+++ polygerrit-ui/web-test-runner.config.mjs
@@ -2,7 +2,9 @@ import { esbuildPlugin } from "@web/dev-server-esbuild";
 
 /** @type {import('@web/test-runner').TestRunnerConfig} */
 const config = {
-  files: ["app/**/*_test.{ts,js}", "!**/node_modules/**/*"],
+  // files: ["app/**/*_test.{ts,js}", "!**/node_modules/**/*"],
+  files: "**/gr-avatar_test.ts",
+  rootDir: "polygerrit-fi",
   port: 9876,
   nodeResolve: true,
   testFramework: {
@@ -14,8 +16,9 @@ const config = {
   plugins: [
     esbuildPlugin({
       ts: true,
-      target: "es2020",
-      tsconfig: "app/tsconfig.json",
+      // target: "es2020",
+      target: "auto",
+      // tsconfig: "app/tsconfig.json",
     }),
   ],
 };
---
+++ polygerrit-ui/wtr_test.sh
@@ -0,0 +1,10 @@
+cd polygerrit-ui
+pwd
+ls
+
+echo $1
+echo $2
+cat wtr_bin
+
+# $1 --config foo.config.mjs 
+# wtr_bin --config foo.config.mjs

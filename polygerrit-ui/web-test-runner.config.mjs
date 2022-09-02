4ef8554ebbeda645d67a6779eb3d965757105070
Frank Borden <frankborden@google.com>
2022-09-02 11:05:03 +0200

    Bazel target for Web Test Runner
    Release-Notes: skip
    Change-Id: Ifa761684f9bfa1a9c5f8467feab2620e7ee9e0ce

--- polygerrit-ui/web-test-runner.config.mjs
+++ polygerrit-ui/web-test-runner.config.mjs
@@ -1,8 +1,9 @@
-import { esbuildPlugin } from "@web/dev-server-esbuild";
+// import { esbuildPlugin } from "@web/dev-server-esbuild";
 
 /** @type {import('@web/test-runner').TestRunnerConfig} */
 const config = {
   files: ["app/**/*_test.{ts,js}", "!**/node_modules/**/*"],
+  rootDir: "app",
   port: 9876,
   nodeResolve: true,
   testFramework: {
@@ -11,12 +12,12 @@ const config = {
       timeout: 5000,
     },
   },
-  plugins: [
-    esbuildPlugin({
-      ts: true,
-      target: "es2020",
-      tsconfig: "app/tsconfig.json",
-    }),
-  ],
+  // plugins: [
+  //   esbuildPlugin({
+  //     ts: true,
+  //     target: "es2020",
+  //     tsconfig: "app/tsconfig.json",
+  //   }),
+  // ],
 };
 export default config;

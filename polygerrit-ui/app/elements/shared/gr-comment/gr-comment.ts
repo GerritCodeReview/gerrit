baf7501e7b0ba7a8781feb3109a883720cea4317
Dhruv Srivastava <dhruvsri@google.com>
2022-09-08 12:18:33 +0200

    Allow save and cancel events to propogate in GrComment
    GrReplyDialog listens to the events to close/send the reply hence
    allow the events to propogate.
    
    Release-Notes: skip
    Google-bug-id: b/242982372
    Change-Id: I7ccb13db172b166018eb214a11918b6d1be0cef1

--- polygerrit-ui/app/elements/shared/gr-comment/gr-comment.ts
+++ polygerrit-ui/app/elements/shared/gr-comment/gr-comment.ts
@@ -263,12 +263,18 @@ export class GrComment extends LitElement {
 
   constructor() {
     super();
-    this.shortcuts.addLocal({key: Key.ESC}, () => this.handleEsc());
+    this.shortcuts.addLocal({key: Key.ESC}, () => this.handleEsc(), {
+      preventDefault: false,
+    });
     for (const key of ['s', Key.ENTER]) {
       for (const modifier of [Modifier.CTRL_KEY, Modifier.META_KEY]) {
-        this.shortcuts.addLocal({key, modifiers: [modifier]}, () => {
-          this.save();
-        });
+        this.shortcuts.addLocal(
+          {key, modifiers: [modifier]},
+          () => {
+            this.save();
+          },
+          {preventDefault: false}
+        );
       }
     }
     if (this.flagsService.isEnabled(KnownExperimentId.MENTION_USERS)) {

2eaa1f62a68dc5ed0357feaa7d2d138f5061b73b
Youssef Elghareeb <ghareeb@google.com>
2022-09-12 10:44:42 +0200

    Add a matchMany method to the Matchable interface
    Release-Notes: skip
    Change-Id: Iceb5d1a6017ece97f6a286ab72d39b96ab511e0a

--- java/com/google/gerrit/server/query/change/EditPredicate.java
+++ java/com/google/gerrit/server/query/change/EditPredicate.java
@@ -14,13 +14,19 @@
 
 package com.google.gerrit.server.query.change;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.entities.Account;
 import com.google.gerrit.entities.Change;
+import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.RefNames;
 import com.google.gerrit.exceptions.StorageException;
 import com.google.gerrit.index.query.PostFilterPredicate;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 
@@ -41,17 +47,31 @@ public class EditPredicate extends PostFilterPredicate<ChangeData> {
 
   @Override
   public boolean match(ChangeData cd) {
-    try (Repository repo = repoManager.openRepository(cd.project())) {
-      for (Ref ref : repo.getRefDatabase().getRefsByPrefix(RefNames.refsEditPrefix(accountId))) {
-        Change.Id changeId = Change.Id.fromEditRefPart(ref.getName());
-        if (changeId.equals(cd.getId())) {
-          return true;
-        }
+    throw new IllegalStateException(
+        "Callers should not invoke the 'match' method. Please use 'matchAll' instead");
+  }
+
+  @Override
+  public Set<ChangeData> matchAll(Set<ChangeData> in) {
+    ImmutableSet.Builder<ChangeData> result = ImmutableSet.builder();
+    Map<Project.NameKey, List<ChangeData>> cdByProject =
+        in.stream().collect(Collectors.groupingBy(ChangeData::project));
+    for (Project.NameKey project : cdByProject.keySet()) {
+      List<ChangeData> cds = cdByProject.get(project);
+      try (Repository repo = repoManager.openRepository(project)) {
+        List<Ref> editRefs =
+            repo.getRefDatabase().getRefsByPrefix(RefNames.refsEditPrefix(accountId));
+        Set<Change.Id> changeIdsWithEdits =
+            editRefs.stream()
+                .map(Ref::getName)
+                .map(Change.Id::fromEditRefPart)
+                .collect(Collectors.toSet());
+        cds.stream().filter(cd -> changeIdsWithEdits.contains(cd.getId())).forEach(result::add);
+      } catch (IOException e) {
+        throw new StorageException(e);
       }
-      return false;
-    } catch (IOException e) {
-      throw new StorageException(e);
     }
+    return result.build();
   }
 
   @Override

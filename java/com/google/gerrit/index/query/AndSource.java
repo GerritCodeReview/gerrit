40ab61c15299ae389370182ed1ec28143f1b18f5
Youssef Elghareeb <ghareeb@google.com>
2022-09-12 10:44:42 +0200

    Add a matchAll method to the Matchable interface
    In the previous change we refactored the implementation of the
    'has:edit' change query as an index search for all open changes with
    caller in {owner, uploader, reviewer or CC}, then doing a post-filtering
    on the resulting change. The post-filtering involved checking each
    resulting change one by one, and for each opening the project repository
    for the change and doing a ref prefix scan for the refs edits of the
    user. This operation is not performant.
    
    In this change we add a #matchAll method to the matchable interface and
    provide an optimized implementation in the new EditPredicate (the
    post-filter predicate). The optimized method groups the input changes by
    project and opens each project exactly once doing the prefix scan. We
    also implemented #matchAll in AndPredicate, OrPredicate and AndSource
    since they are on the execution path of the new implementation of
    'has:edits'.
    
    We now throw an IllegalStateException in EditPredicate#match to make
    sure this method is never called.
    
    Release-Notes: skip
    Change-Id: I9c65d73d0e8e54a4c600a3d12a5d99396b16b8ef

--- java/com/google/gerrit/index/query/AndSource.java
+++ java/com/google/gerrit/index/query/AndSource.java
@@ -15,58 +15,57 @@
 package com.google.gerrit.index.query;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
-import com.google.gerrit.index.IndexConfig;
+import com.google.common.collect.Iterables;
+import com.google.gerrit.exceptions.StorageException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class AndSource<T> extends AndPredicate<T> implements DataSource<T> {
+public class AndSource<T> extends AndPredicate<T>
+    implements DataSource<T>, Comparator<Predicate<T>> {
   protected final DataSource<T> source;
 
   private final IsVisibleToPredicate<T> isVisibleToPredicate;
   private final int start;
   private final int cardinality;
-  private final IndexConfig indexConfig;
 
-  public AndSource(Collection<? extends Predicate<T>> that, IndexConfig indexConfig) {
-    this(that, null, 0, indexConfig);
+  public AndSource(Collection<? extends Predicate<T>> that) {
+    this(that, null, 0);
   }
 
-  public AndSource(
-      Predicate<T> that, IsVisibleToPredicate<T> isVisibleToPredicate, IndexConfig indexConfig) {
-    this(that, isVisibleToPredicate, 0, indexConfig);
+  public AndSource(Predicate<T> that, IsVisibleToPredicate<T> isVisibleToPredicate) {
+    this(that, isVisibleToPredicate, 0);
   }
 
-  public AndSource(
-      Predicate<T> that,
-      IsVisibleToPredicate<T> isVisibleToPredicate,
-      int start,
-      IndexConfig indexConfig) {
-    this(ImmutableList.of(that), isVisibleToPredicate, start, indexConfig);
+  public AndSource(Predicate<T> that, IsVisibleToPredicate<T> isVisibleToPredicate, int start) {
+    this(ImmutableList.of(that), isVisibleToPredicate, start);
   }
 
   public AndSource(
       Collection<? extends Predicate<T>> that,
       IsVisibleToPredicate<T> isVisibleToPredicate,
-      int start,
-      IndexConfig indexConfig) {
+      int start) {
     super(that);
     checkArgument(start >= 0, "negative start: %s", start);
     this.isVisibleToPredicate = isVisibleToPredicate;
     this.start = start;
-    this.indexConfig = indexConfig;
 
     int c = Integer.MAX_VALUE;
     DataSource<T> s = null;
     int minCost = Integer.MAX_VALUE;
-    for (Predicate<T> p : getChildren()) {
+    for (Predicate<T> p : sort(getChildren())) {
       if (p instanceof DataSource) {
         c = Math.min(c, ((DataSource<?>) p).getCardinality());
 
         int cost = p.estimateCost();
         if (cost < minCost) {
-          s = toPaginatingSource(p);
+          s = toDataSource(p);
           minCost = cost;
         }
       }
@@ -77,12 +76,57 @@ public class AndSource<T> extends AndPredicate<T> implements DataSource<T> {
 
   @Override
   public ResultSet<T> read() {
-    return source.read();
+    if (source == null) {
+      throw new StorageException("No DataSource: " + this);
+    }
+
+    // ResultSets are lazy. Calling #read here first and then dealing with ResultSets only when
+    // requested allows the index to run asynchronous queries.
+    ResultSet<T> resultSet = source.read();
+    return new LazyResultSet<>(
+        () -> {
+          List<T> r = new ArrayList<>();
+
+          List<T> resultList = new ArrayList<>();
+          buffer(resultSet).forEach(t -> resultList.add(t));
+          T last = resultList.isEmpty() ? null : Iterables.getLast(resultList);
+          int nextStart = resultList.size();
+          List<T> matched = matchAll(resultList);
+          matched.forEach(m -> r.add(m));
+          boolean skipped = matched.size() != resultList.size();
+
+          if (skipped && last != null && source instanceof Paginated) {
+            // If our source is a paginated source and we skipped at
+            // least one of its results, we may not have filled the full
+            // limit the caller wants.  Restart the source and continue.
+            //
+            @SuppressWarnings("unchecked")
+            Paginated<T> p = (Paginated<T>) source;
+            while (skipped && r.size() < p.getOptions().limit() + start) {
+              skipped = false;
+              ResultSet<T> next = p.restart(nextStart);
+
+              List<T> rl = new ArrayList<>();
+              buffer(next).forEach(e -> rl.add(e));
+              nextStart += rl.size();
+              matched = matchAll(rl);
+              matched.forEach(m -> r.add(m));
+            }
+          }
+
+          if (start >= r.size()) {
+            return ImmutableList.of();
+          } else if (start > 0) {
+            return ImmutableList.copyOf(r.subList(start, r.size()));
+          }
+          return ImmutableList.copyOf(r);
+        });
   }
 
   @Override
   public ResultSet<FieldBundle> readRaw() {
-    return source.readRaw();
+    // TOOD(hiesel): Implement
+    throw new UnsupportedOperationException("not implemented");
   }
 
   @Override
@@ -101,6 +145,21 @@ public class AndSource<T> extends AndPredicate<T> implements DataSource<T> {
     }
 
     return true;
+  }
+
+  @Override
+  public List<T> matchAll(List<T> in) {
+    List<T> filtered = in;
+    if (isVisibleToPredicate != null) {
+      filtered =
+          in.stream().filter(data -> isVisibleToPredicate.match(data)).collect(Collectors.toList());
+    }
+    return super.isMatchable() ? super.matchAll(filtered) : filtered;
+  }
+
+  private Iterable<T> buffer(ResultSet<T> scanner) {
+    return FluentIterable.from(Iterables.partition(scanner, 50))
+        .transformAndConcat(this::transformBuffer);
   }
 
   protected List<T> transformBuffer(List<T> buffer) {

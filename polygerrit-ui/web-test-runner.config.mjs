614fa9d868e7f852898b62764c1df3fbb5757e49
Frank Borden <frankborden@google.com>
2022-09-14 14:52:11 +0200

    Screenshot test support
    See gr-file-list_test.ts for an example of how to create a screenshot
    test. When a test fails it creates a gitignore'd file of the diff
    heatmap.
    
    Example command usage for updating goldens:
    > yarn test:screenshot-update "**/gr-file-list_test.ts"
    
    Release-Notes: skip
    Change-Id: If9e755f051e2af3906a27b6306ff0811a78a3c68

--- polygerrit-ui/web-test-runner.config.mjs
+++ polygerrit-ui/web-test-runner.config.mjs
@@ -1,24 +1,55 @@
 import { esbuildPlugin } from "@web/dev-server-esbuild";
 import { defaultReporter, summaryReporter } from "@web/test-runner";
+import { visualRegressionPlugin } from "@web/test-runner-visual-regression/plugin";
 
 /** @type {import('@web/test-runner').TestRunnerConfig} */
 const config = {
   files: ["app/**/*_test.{ts,js}", "!**/node_modules/**/*"],
   port: 9876,
   nodeResolve: true,
-  testFramework: {
-    config: {
-      ui: "tdd",
-      timeout: 5000,
-    },
-  },
+  testFramework: { config: { ui: "tdd", timeout: 5000 } },
   plugins: [
     esbuildPlugin({
       ts: true,
       target: "es2020",
       tsconfig: "app/tsconfig.json",
     }),
+    visualRegressionPlugin({
+      diffOptions: {
+        threshold: 0.2,
+      },
+      update: process.argv.includes("--update-screenshots"),
+    }),
   ],
+  // serve from gerrit root directory so that we can serve fonts from
+  // /lib/fonts/, see middleware.
+  rootDir: "..",
   reporters: [defaultReporter(), summaryReporter()],
+  middleware: [
+    // Fonts are in /lib/fonts/, but css tries to load from
+    // /polygerrit-ui/app/fonts/. In production this works because our build
+    // copies them over, see /polygerrit-ui/BUILD
+    async (context, next) => {
+      if (context.url.startsWith("/polygerrit-ui/app/fonts/")) {
+        context.url = context.url.replace("/polygerrit-ui/app/", "/lib/");
+      }
+      await next();
+    },
+  ],
+  testRunnerHtml: (testFramework) => `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <link rel="stylesheet" href="polygerrit-ui/app/styles/main.css">
+        <link rel="stylesheet" href="polygerrit-ui/app/styles/fonts.css">
+        <link
+          rel="stylesheet"
+          href="polygerrit-ui/app/styles/material-icons.css">
+      </head>
+      <body>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>
+  `,
 };
 export default config;

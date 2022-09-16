62b54eac0244e61304ba456f8ad578fe360dd4e9
Ben Rohlfs <brohlfs@google.com>
2022-09-16 11:02:01 +0200

    Complete moving the change url creation into change.ts view model
    Release-Notes: skip
    Google-Bug-Id: b/244279450
    Change-Id: I3f2b11f5981e3e2913fd40a6ac0671d8c519f67c

--- polygerrit-ui/app/utils/router-util_test.ts
+++ polygerrit-ui/app/utils/router-util_test.ts
@@ -8,10 +8,8 @@ import {
   RepoName,
   NumericChangeId,
   RevisionPatchSetNum,
-  BasePatchSetNum,
   EDIT,
 } from '../api/rest-api';
-import {ChangeViewState} from '../models/views/change';
 import {EditViewState} from '../models/views/edit';
 import {GerritView} from '../services/router/router-model';
 import '../test/common-test-setup';
@@ -19,34 +17,6 @@ import {generateUrl} from './router-util';
 
 suite('router-util tests', () => {
   suite('generateUrl', () => {
-    test('change', () => {
-      const params: ChangeViewState = {
-        view: GerritView.CHANGE,
-        changeNum: 1234 as NumericChangeId,
-        project: 'test' as RepoName,
-      };
-
-      assert.equal(generateUrl(params), '/c/test/+/1234');
-
-      params.patchNum = 10 as RevisionPatchSetNum;
-      assert.equal(generateUrl(params), '/c/test/+/1234/10');
-
-      params.basePatchNum = 5 as BasePatchSetNum;
-      assert.equal(generateUrl(params), '/c/test/+/1234/5..10');
-
-      params.messageHash = '#123';
-      assert.equal(generateUrl(params), '/c/test/+/1234/5..10#123');
-    });
-
-    test('change with repo name encoding', () => {
-      const params: ChangeViewState = {
-        view: GerritView.CHANGE,
-        changeNum: 1234 as NumericChangeId,
-        project: 'x+/y+/z+/w' as RepoName,
-      };
-      assert.equal(generateUrl(params), '/c/x%252B/y%252B/z%252B/w/+/1234');
-    });
-
     test(EDIT, () => {
       const params: EditViewState = {
         view: GerritView.EDIT,

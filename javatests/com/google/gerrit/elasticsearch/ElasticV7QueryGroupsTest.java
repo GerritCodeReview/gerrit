bbd9380f6b00256598775662c4998d036c140fb4
Nasser Grainawi <nasser.grainawi@linaro.org>
2022-10-03 12:27:55 -0700

    Elasticsearch tests: Don't assume container persists for all tests
    Since Change If3f8d914d5f there are multiple index configs executed in
    different config suites in the tests and that means the ElasticContainer
    will be stopped after each suite completes. Handle that by always
    recreating the container at the start of a suite.
    
    Also fix a couple places that were using the default index config
    instead of the config from the ConfigSuite.
    
    Release-Notes: skip
    Bug: Issue 16300
    Change-Id: I39926a3dc65a2313fbcfc566f6b2b72238c6fff6

--- javatests/com/google/gerrit/elasticsearch/ElasticV7QueryGroupsTest.java
+++ javatests/com/google/gerrit/elasticsearch/ElasticV7QueryGroupsTest.java
@@ -41,10 +41,7 @@ public class ElasticV7QueryGroupsTest extends AbstractQueryGroupsTest {
 
   @BeforeClass
   public static void startIndexService() {
-    if (container == null) {
-      // Only start Elasticsearch once
-      container = ElasticContainer.createAndStart(ElasticVersion.V7_16);
-    }
+    container = ElasticContainer.createAndStart(ElasticVersion.V7_16);
   }
 
   @AfterClass

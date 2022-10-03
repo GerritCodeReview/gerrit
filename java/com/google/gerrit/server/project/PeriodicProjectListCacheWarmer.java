587ea0db84fbf5e12b0c7ac9819e3168c4c3b51c
Prudhvi Akhil Alahari <prudhvi.alahari@linaro.org>
2022-10-04 00:41:09 +0530

    Add an option to periodically warm the project_list cache
    The project_list cache is used while loading the change detail page. On
    a Gerrit site with ~20k projects on NFS, the project_list cache takes
    ~33 seconds to load. When there is no activity on the site, entries in
    the project_list cache are evicted after maxAge duration (possibly used
    to support an HA configuration). From when this happens until the cache
    is loaded, the cost of loading the cache is borne by all the clients who
    access a change detail page.
    
    Reduce the likelihood of this happening by providing an option to
    periodically warm the project_list cache. When this warmer is enabled,
    change detail page loads are consistently faster.
    
    Release-Notes: The project_list cache can be periodically warmed
    Release-Notes: The project_list cache warmer is enabled by default when `cache.project_list.maxAge` is set
    Change-Id: I2dd8eeabee5e808b584c05227fc4055555098c91

---
+++ java/com/google/gerrit/server/project/PeriodicProjectListCacheWarmer.java
@@ -0,0 +1,95 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.project;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.ScheduleConfig;
+import com.google.gerrit.server.git.WorkQueue;
+import com.google.inject.Inject;
+import java.time.Duration;
+import org.eclipse.jgit.lib.Config;
+
+public class PeriodicProjectListCacheWarmer implements Runnable {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  public static class LifeCycle implements LifecycleListener {
+    protected final Config config;
+    protected final WorkQueue queue;
+    protected final PeriodicProjectListCacheWarmer runner;
+
+    @Inject
+    LifeCycle(
+        @GerritServerConfig Config config, WorkQueue queue, PeriodicProjectListCacheWarmer runner) {
+      this.config = config;
+      this.queue = queue;
+      this.runner = runner;
+    }
+
+    @Override
+    public void start() {
+      long maxAge =
+          config.getTimeUnit("cache", ProjectCacheImpl.CACHE_LIST, "maxAge", -1L, MILLISECONDS);
+      long interval =
+          config.getTimeUnit(
+              "cache",
+              ProjectCacheImpl.CACHE_LIST,
+              "interval",
+              getHalfDuration(maxAge),
+              MILLISECONDS);
+      if (interval == -1L) {
+        logger.atWarning().log("project_list cache warmer is disabled");
+        return;
+      }
+
+      runner.run();
+      ScheduleConfig.Schedule schedule =
+          ScheduleConfig.builder(config, "cache")
+              .setSubsection(ProjectCacheImpl.CACHE_LIST)
+              .buildSchedule()
+              .orElseGet(() -> ScheduleConfig.Schedule.createOrFail(interval, "00:00"));
+      queue.scheduleAtFixedRate(runner, schedule);
+    }
+
+    @Override
+    public void stop() {
+      // handled by WorkQueue.stop() already
+    }
+
+    private long getHalfDuration(long duration) {
+      if (duration < 0) {
+        return duration;
+      }
+      return Duration.ofMillis(duration).dividedBy(2L).toMillis();
+    }
+  }
+
+  protected final ProjectCache cache;
+
+  @Inject
+  PeriodicProjectListCacheWarmer(ProjectCache cache) {
+    this.cache = cache;
+  }
+
+  @Override
+  public void run() {
+    logger.atFine().log("Loading project_list cache");
+    cache.all();
+    logger.atFine().log("Finished loading project_list cache");
+  }
+}

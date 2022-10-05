94ad21940e61bd429aff8bb9ba7806f60fe0149d
Dhruv Srivastava <dhruvsri@google.com>
2022-10-05 11:58:50 +0200

    Convert GrAutocompleteDropdown to Lit
    Change-Id: I8fb30e5cb82b43d9d575707a33030988da9b96bd

--- polygerrit-ui/app/elements/lit/fit-controller.ts
+++ polygerrit-ui/app/elements/lit/fit-controller.ts
@@ -6,20 +6,160 @@
 
 import {ReactiveController, ReactiveControllerHost} from 'lit';
 
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+export interface FitControllerHost {
+  /**
+   * The orientation against which to align the element horizontally
+   * relative to the `positionTarget`. Possible values are "left", "right",
+   * "center", "auto".
+   */
+  horizontalAlign?: string | null;
+  /**
+   * The orientation against which to align the element vertically
+   * relative to the `positionTarget`. Possible values are "top", "bottom",
+   * "middle", "auto".
+   */
+  verticalAlign?: string | null;
+  /**
+   * A pixel value that will be added to the position calculated for the
+   * given `horizontalAlign`, in the direction of alignment. You can think
+   * of it as increasing or decreasing the distance to the side of the
+   * screen given by `horizontalAlign`.
+   *
+   * If `horizontalAlign` is "left" or "center", this offset will increase or
+   * decrease the distance to the left side of the screen: a negative offset
+   * will move the dropdown to the left; a positive one, to the right.
+   *
+   * Conversely if `horizontalAlign` is "right", this offset will increase
+   * or decrease the distance to the right side of the screen: a negative
+   * offset will move the dropdown to the right; a positive one, to the left.
+   */
+  horizontalOffset: number;
+  /**
+   * A pixel value that will be added to the position calculated for the
+   * given `verticalAlign`, in the direction of alignment. You can think
+   * of it as increasing or decreasing the distance to the side of the
+   * screen given by `verticalAlign`.
+   *
+   * If `verticalAlign` is "top" or "middle", this offset will increase or
+   * decrease the distance to the top side of the screen: a negative offset
+   * will move the dropdown upwards; a positive one, downwards.
+   *
+   * Conversely if `verticalAlign` is "bottom", this offset will increase
+   * or decrease the distance to the bottom side of the screen: a negative
+   * offset will move the dropdown downwards; a positive one, upwards.
+   */
+  verticalOffset: number;
+}
+
+// Information needed to position and size the target element
+interface FitInfo {
+  inlineStyle: {
+    top: string;
+    left: string;
+    position: string;
+  };
+  sizerInlineStyle: {
+    maxWidth: string;
+    maxHeight: string;
+    boxSizing: string;
+  };
+  positionedBy: {
+    vertically: string | null;
+    horizontally: string | null;
+  };
+  sizedBy: {
+    height: boolean;
+    width: boolean;
+    minWidth: number;
+    minHeight: number;
+  };
+  margin: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+  };
+}
+
+interface Positions {
+  verticalAlign: string;
+  horizontalAlign: string;
+  top: number;
+  left: number;
+  offscreenArea?: number;
+}
+=======
 /**
  * horizontalAlign
  * The orientation against which to align the element horizontally
  * relative to the `positionTarget`.
  * The only value supported initially is left
  */
+>>>>>>> BASE      (2911f6 Add basic FitController)
 
 /**
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+ `FitController` fits an element in another element using `max-height`
+ and `max-width`, and optionally centers it in the window or another element.
+
+ The element will only be sized and/or positioned if it has not already been
+ sized and/or positioned by CSS.
+
+ CSS properties            | Action
+ --------------------------|-------------------------------------------
+ `position` set            | Element is not centered horizontally or vertically
+ `top` or `bottom` set     | Element is not vertically centered
+ `left` or `right` set     | Element is not horizontally centered
+ `max-height` set          | Element respects `max-height`
+ `max-width` set           | Element respects `max-width`
+
+ `FitController` can position an element into another element using
+ `verticalAlign` and `horizontalAlign`. This will override the element's css
+ position.
+
+     <div class="container">
+       <iron-fit-impl vertical-align="top" horizontal-align="auto">
+         Positioned into the container
+       </iron-fit-impl>
+     </div>
+
+ Use `noOverlap` to position the element around another element without
+ overlapping it.
+
+     <div class="container">
+       <iron-fit-impl no-overlap vertical-align="auto" horizontal-align="auto">
+         Positioned around the container
+       </iron-fit-impl>
+     </div>
+
+ Use `horizontalOffset, verticalOffset` to offset the element from its
+ `positionTarget`; `FitController` will collapse these in order to
+ keep the element within `window` boundaries, while preserving the element's
+ CSS margin values.
+
+     <div class="container">
+       <iron-fit-impl vertical-align="top" vertical-offset="20">
+         With vertical offset
+       </iron-fit-impl>
+     </div>
+
+=======
  * verticalAlign;
  * The orientation against which to align the element vertically
  * relative to the `positionTarget`.
  * The only value supported initially is top.
+>>>>>>> BASE      (2911f6 Add basic FitController)
  */
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+export class FitController implements ReactiveController {
+  host: ReactiveControllerHost & HTMLElement & FitControllerHost;
+=======
+>>>>>>> BASE      (2911f6 Add basic FitController)
 
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+  private positionTarget?: HTMLElement;
+=======
 export interface FitControllerHost {
   /**
    * A pixel value that will be added to the position calculated for the
@@ -104,13 +244,21 @@ interface FitInfo {
 
 export class FitController implements ReactiveController {
   host: ReactiveControllerHost & HTMLElement & FitControllerHost;
+>>>>>>> BASE      (2911f6 Add basic FitController)
 
   private fitInfo?: FitInfo | null;
 
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+=======
   private originalStyles = {};
 
+>>>>>>> BASE      (2911f6 Add basic FitController)
   constructor(host: ReactiveControllerHost & HTMLElement & FitControllerHost) {
     (this.host = host).addController(this);
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+    this.positionTarget = this.positionTarget || this._defaultPositionTarget;
+=======
+>>>>>>> BASE      (2911f6 Add basic FitController)
   }
 
   hostConnected() {}
@@ -118,10 +266,43 @@ export class FitController implements ReactiveController {
   hostDisconnected() {}
 
   /**
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+   * The element that should be used to position the element,
+   * if no position target is configured.
+   */
+  get _defaultPositionTarget() {
+    let parent = this.host.parentNode;
+
+    if (parent && parent.nodeType === Node.DOCUMENT_FRAGMENT_NODE) {
+      parent = (parent as ShadowRoot).host;
+    }
+
+    return parent as HTMLElement;
+  }
+
+  /**
+   * True if the element should be positioned instead of centered.
+   *
+   * @private
+   */
+  get shouldPosition() {
+    return (
+      (this.host.horizontalAlign || this.host.verticalAlign) &&
+      this.positionTarget
+    );
+  }
+
+  /**
+=======
+>>>>>>> BASE      (2911f6 Add basic FitController)
    * Positions and fits the element into the `window` element.
    */
   fit() {
     this.position();
+  }
+
+  setPositionTarget(target: HTMLElement) {
+    this.positionTarget = target;
   }
 
   /**
@@ -161,9 +342,18 @@ export class FitController implements ReactiveController {
    * Resets the target element's position and size constraints, and clear
    * the memoized data.
    */
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+  private resetFit() {
+    const info = this.fitInfo;
+    if (info) {
+      Object.assign(this.host.style, info.sizerInlineStyle);
+      Object.assign(this.host.style, info.inlineStyle);
+    }
+=======
   private resetTargetPosition() {
     Object.assign(this.host.style, this.originalStyles);
     this.originalStyles = {};
+>>>>>>> BASE      (2911f6 Add basic FitController)
     this.fitInfo = null;
   }
 
@@ -197,6 +387,11 @@ export class FitController implements ReactiveController {
     this.host.style.top = '0px';
 
     const rect = this.host.getBoundingClientRect();
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+    // TODO(dhruvsi): verify cast
+    const positionRect = this.getNormalizedRect(this.positionTarget!);
+=======
+>>>>>>> BASE      (2911f6 Add basic FitController)
     const fitRect = this.getNormalizedRect(window);
 
     const margin = this.fitInfo!.margin;
@@ -241,6 +436,115 @@ export class FitController implements ReactiveController {
     this.host.style.top = `${topPosition}px`;
   }
 
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+  /**
+   * Constrains the size of the element to `window` by setting `max-height`
+   * and/or `max-width`.
+   */
+  private constrain() {
+    if (this.shouldPosition) {
+      return;
+    }
+    this.discoverInfo();
+
+    const info = this.fitInfo!;
+    // position at (0px, 0px) if not already positioned, so we can measure the
+    // natural size.
+    if (!info.positionedBy.vertically) {
+      this.host.style.position = 'fixed';
+      this.host.style.top = '0px';
+    }
+    if (!info.positionedBy.horizontally) {
+      this.host.style.position = 'fixed';
+      this.host.style.left = '0px';
+    }
+
+    // need border-box for margin/padding
+    this.host.style.boxSizing = 'border-box';
+    // constrain the width and height if not already set
+    const rect = this.host.getBoundingClientRect();
+    if (!info.sizedBy.height) {
+      this.sizeDimension(
+        rect,
+        info.positionedBy.vertically,
+        'top',
+        'bottom',
+        'Height'
+      );
+    }
+    if (!info.sizedBy.width) {
+      this.sizeDimension(
+        rect,
+        info.positionedBy.horizontally,
+        'left',
+        'right',
+        'Width'
+      );
+    }
+  }
+
+  private sizeDimension(
+    rect: DOMRect,
+    positionedBy: string | null,
+    start: 'top' | 'left',
+    end: 'bottom' | 'right',
+    extent: 'Height' | 'Width'
+  ) {
+    const info = this.fitInfo;
+    const fitRect = this.getNormalizedRect(window);
+    const max = extent === 'Width' ? fitRect.width : fitRect.height;
+    const flip = positionedBy === end;
+    const offset = flip ? max - rect[end] : rect[start];
+    const margin = info?.margin[flip ? start : end] ?? 0;
+    // TODO(dhruvsri): verify sizingOffset is 0
+    const sizingOffset = 0;
+    const key = ('max' + extent) as keyof CSSStyleDeclaration;
+    Object.assign(this.host.style, {
+      [key]: max - margin - offset - sizingOffset + 'px',
+    });
+  }
+
+  /**
+   * Centers horizontally and vertically if not already positioned. This also
+   * sets `position:fixed`.
+   */
+  private center() {
+    if (this.shouldPosition) {
+      return;
+    }
+    this.discoverInfo();
+
+    const positionedBy = this.fitInfo!.positionedBy;
+    if (positionedBy.vertically && positionedBy.horizontally) {
+      // Already positioned.
+      return;
+    }
+    // Need position:fixed to center
+    this.host.style.position = 'fixed';
+    // Take into account the offset caused by parents that create stacking
+    // contexts (e.g. with transform: translate3d). Translate to 0,0 and
+    // measure the bounding rect.
+    if (!positionedBy.vertically) {
+      this.host.style.top = '0px';
+    }
+    if (!positionedBy.horizontally) {
+      this.host.style.left = '0px';
+    }
+    // It will take in consideration margins and transforms
+    const rect = this.host.getBoundingClientRect();
+    const fitRect = this.getNormalizedRect(window);
+    if (!positionedBy.vertically) {
+      const top = fitRect.top - rect.top + (fitRect.height - rect.height) / 2;
+      this.host.style.top = top + 'px';
+    }
+    if (!positionedBy.horizontally) {
+      const left = fitRect.left - rect.left + (fitRect.width - rect.width) / 2;
+      this.host.style.left = left + 'px';
+    }
+  }
+
+=======
+>>>>>>> BASE      (2911f6 Add basic FitController)
   private getNormalizedRect(target: HTMLElement | Window): DOMRect {
     if (target === document.documentElement || target === window) {
       return {
@@ -254,4 +558,184 @@ export class FitController implements ReactiveController {
     }
     return (target as HTMLElement).getBoundingClientRect();
   }
+<<<<<<< PATCH SET (a8c8bc Convert GrAutocompleteDropdown to Lit)
+
+  private getOffscreenArea(
+    position: Positions,
+    size: {
+      width: number;
+      height: number;
+    },
+    fitRect: DOMRect
+  ) {
+    const verticalCrop =
+      Math.min(0, position.top) +
+      Math.min(0, fitRect.bottom - (position.top + size.height));
+    const horizontalCrop =
+      Math.min(0, position.left) +
+      Math.min(0, fitRect.right - (position.left + size.width));
+    return (
+      Math.abs(verticalCrop) * size.width +
+      Math.abs(horizontalCrop) * size.height
+    );
+  }
+
+  private getPosition(
+    size: {
+      width: number;
+      height: number;
+    },
+    sizeNoMargins: DOMRect,
+    positionRect: DOMRect,
+    fitRect: DOMRect
+  ) {
+    // All the possible configurations.
+    // Ordered as top-left, top-right, bottom-left, bottom-right.
+    const positions: Positions[] = [
+      {
+        verticalAlign: 'top',
+        horizontalAlign: 'left',
+        top: positionRect.top + this.host.verticalOffset,
+        left: positionRect.left + this.host.horizontalOffset,
+      },
+      {
+        verticalAlign: 'top',
+        horizontalAlign: 'right',
+        top: positionRect.top + this.host.verticalOffset,
+        left: positionRect.right - size.width - this.host.horizontalOffset,
+      },
+      {
+        verticalAlign: 'bottom',
+        horizontalAlign: 'left',
+        top: positionRect.bottom - size.height - this.host.verticalOffset,
+        left: positionRect.left + this.host.horizontalOffset,
+      },
+      {
+        verticalAlign: 'bottom',
+        horizontalAlign: 'right',
+        top: positionRect.bottom - size.height - this.host.verticalOffset,
+        left: positionRect.right - size.width - this.host.horizontalOffset,
+      },
+    ];
+
+    // Consider auto as null for coding convenience.
+    this.host.verticalAlign =
+      this.host.verticalAlign === 'auto' ? null : this.host.verticalAlign;
+    this.host.horizontalAlign =
+      this.host.horizontalAlign === 'auto' ? null : this.host.horizontalAlign;
+
+    if (!this.host.horizontalAlign || this.host.horizontalAlign === 'center') {
+      positions.push({
+        verticalAlign: 'top',
+        horizontalAlign: 'center',
+        top: positionRect.top + this.host.verticalOffset,
+        left:
+          positionRect.left -
+          sizeNoMargins.width / 2 +
+          positionRect.width / 2 +
+          this.host.horizontalOffset,
+      });
+      positions.push({
+        verticalAlign: 'bottom',
+        horizontalAlign: 'center',
+        top: positionRect.bottom - size.height - this.host.verticalOffset,
+        left:
+          positionRect.left -
+          sizeNoMargins.width / 2 +
+          positionRect.width / 2 +
+          this.host.horizontalOffset,
+      });
+    }
+
+    if (!this.host.verticalAlign || this.host.verticalAlign === 'middle') {
+      positions.push({
+        verticalAlign: 'middle',
+        horizontalAlign: 'left',
+        top:
+          positionRect.top -
+          sizeNoMargins.height / 2 +
+          positionRect.height / 2 +
+          this.host.verticalOffset,
+        left: positionRect.left + this.host.horizontalOffset,
+      });
+      positions.push({
+        verticalAlign: 'middle',
+        horizontalAlign: 'right',
+        top:
+          positionRect.top -
+          sizeNoMargins.height / 2 +
+          positionRect.height / 2 +
+          this.host.verticalOffset,
+        left: positionRect.right - size.width - this.host.horizontalOffset,
+      });
+    }
+
+    if (
+      this.host.verticalAlign === 'middle' &&
+      this.host.horizontalAlign === 'center'
+    ) {
+      positions.push({
+        verticalAlign: 'middle',
+        horizontalAlign: 'center',
+        top:
+          positionRect.top -
+          sizeNoMargins.height / 2 +
+          positionRect.height / 2 +
+          this.host.verticalOffset,
+        left:
+          positionRect.left -
+          sizeNoMargins.width / 2 +
+          positionRect.width / 2 +
+          this.host.horizontalOffset,
+      });
+    }
+
+    let position;
+    for (let i = 0; i < positions.length; i++) {
+      const candidate = positions[i];
+      const vAlignOk = candidate.verticalAlign === this.host.verticalAlign;
+      const hAlignOk = candidate.horizontalAlign === this.host.horizontalAlign;
+
+      // If both this.host.verticalAlign and this.host.horizontalAlign are defined, return exact match.
+      // For dynamicAlign and noOverlap we'll have more than one candidate, so
+      // we'll have to check the offscreenArea to make the best choice.
+      if (vAlignOk && hAlignOk) {
+        position = candidate;
+        break;
+      }
+
+      // Align is ok if alignment preferences are respected. If no preferences,
+      // it is considered ok.
+      const alignOk =
+        (!this.host.verticalAlign || vAlignOk) &&
+        (!this.host.horizontalAlign || hAlignOk);
+
+      // Filter out elements that don't match the alignment (if defined).
+      // With dynamicAlign, we need to consider all the positions to find the
+      // one that minimizes the cropped area.
+      if (!alignOk) {
+        continue;
+      }
+
+      candidate.offscreenArea = this.getOffscreenArea(candidate, size, fitRect);
+      // If not cropped and respects the align requirements, keep it.
+      // This allows to prefer positions overlapping horizontally over the
+      // ones overlapping vertically.
+      if (candidate.offscreenArea === 0 && alignOk) {
+        position = candidate;
+        break;
+      }
+      position = position || candidate;
+      const diff = candidate.offscreenArea - (position.offscreenArea ?? 0);
+      // Check which crops less. If it crops equally, check if at least one
+      // align setting is ok.
+      if (diff < 0 || (diff === 0 && (vAlignOk || hAlignOk))) {
+        position = candidate;
+      }
+    }
+
+    return position;
+  }
+=======
+>>>>>>> BASE      (2911f6 Add basic FitController)
 }

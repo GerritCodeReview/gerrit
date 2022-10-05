fa5787e919af21c5def0a27aa086b7b51fa7781e
Nasser Grainawi <nasser.grainawi@linaro.org>
2022-10-05 11:01:57 -0600

    Fix javadoc summary for Google Java Style
    The summary shouldn't use @return and should use "Returns".
    
    [1] https://google.github.io/styleguide/javaguide.html#s7.2-summary-fragment
    
    Change-Id: I631f0be5c048c2f93d62993c41583fb382d714ac
    Release-Notes: skip

--- java/com/google/gerrit/metrics/MetricsReservoirConfig.java
+++ java/com/google/gerrit/metrics/MetricsReservoirConfig.java
@@ -19,15 +19,15 @@ import java.time.Duration;
 /** Configuration of the Metrics' reservoir type and size. */
 public interface MetricsReservoirConfig {
 
-  /** @return the reservoir type. */
+  /** Returns the reservoir type. */
   ReservoirType reservoirType();
 
-  /** @return the reservoir window duration. */
+  /** Returns the reservoir window duration. */
   Duration reservoirWindow();
 
-  /** @return the number of samples that the reservoir can contain */
+  /** Returns the number of samples that the reservoir can contain */
   int reservoirSize();
 
-  /** @return the alpha parameter of the ExponentiallyDecaying reservoir */
+  /** Returns the alpha parameter of the ExponentiallyDecaying reservoir */
   double reservoirAlpha();
 }

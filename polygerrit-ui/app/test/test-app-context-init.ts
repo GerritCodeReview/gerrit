11689aef64467e9d407c6f5ff24cfef86fb934b8
Ben Rohlfs <brohlfs@google.com>
2022-10-11 15:26:45 +0000

    Move behavior from component into view model for change-list-view
    Rolling forward change 346616.
    
    Change-Id: I30dcb9a37c49afe7f97d51adc7e00836c5c6e0bf
    Release-Notes: skip
    Google-Bug-Id: b/248247819

--- polygerrit-ui/app/test/test-app-context-init.ts
+++ polygerrit-ui/app/test/test-app-context-init.ts
@@ -138,7 +138,10 @@ export function createTestDependencies(
   dependencies.set(pluginViewModelToken, pluginViewModelCreator);
   const repoViewModelCreator = () => new RepoViewModel();
   dependencies.set(repoViewModelToken, repoViewModelCreator);
-  const searchViewModelCreator = () => new SearchViewModel();
+  const searchViewModelCreator = () =>
+    new SearchViewModel(appContext.restApiService, appContext.userModel, () =>
+      resolver(navigationToken)
+    );
   dependencies.set(searchViewModelToken, searchViewModelCreator);
   const settingsViewModelCreator = () => new SettingsViewModel();
   dependencies.set(settingsViewModelToken, settingsViewModelCreator);

b0da8042bdb010dd11d10d0cda940343deb1d5f0
Luca Milanesio <luca.milanesio@gmail.com>
2022-10-17 16:19:29 +0100

    Remove the 'Require Change-Id in commit message' option from project
    The ability to post changes without a Change-Id has been deprecated
    for years: it has come the time to remove it and simplify the Gerrit
    workflow and Project's configuration.
    
    All existing Projects' configuration about requireChangeId is ignored
    and is considered as it was set to true.
    
    Forward-Compatible: checked
    Release-Notes: Breaking change: discontinue requireChangeId in project.config as enforce Change-Id on all incoming changes
    Change-Id: I377d4f335b5889a3fe0cbdda7212378398dc3bca

--- javatests/com/google/gerrit/acceptance/rest/project/CreateProjectIT.java
+++ javatests/com/google/gerrit/acceptance/rest/project/CreateProjectIT.java
@@ -222,7 +222,6 @@ public class CreateProjectIT extends AbstractDaemonTest {
     in.useContributorAgreements = InheritableBoolean.TRUE;
     in.useSignedOffBy = InheritableBoolean.TRUE;
     in.useContentMerge = InheritableBoolean.TRUE;
-    in.requireChangeId = InheritableBoolean.TRUE;
     ProjectInfo p = gApi.projects().create(in).get();
     assertThat(p.name).isEqualTo(newProjectName);
     Project project = projectCache.get(Project.nameKey(newProjectName)).get().getProject();
@@ -235,8 +234,6 @@ public class CreateProjectIT extends AbstractDaemonTest {
         .isEqualTo(in.useSignedOffBy);
     assertThat(project.getBooleanConfig(BooleanProjectConfig.USE_CONTENT_MERGE))
         .isEqualTo(in.useContentMerge);
-    assertThat(project.getBooleanConfig(BooleanProjectConfig.REQUIRE_CHANGE_ID))
-        .isEqualTo(in.requireChangeId);
   }
 
   @Test

b4cdb2d48a424b58f09ae3f627d5089d29726da6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 12:32:02 +0000

    Adding tarfile member sanitization to extractall()

--- tools/js/npm_pack.py
+++ tools/js/npm_pack.py
@@ -65,7 +65,26 @@ def main(args):
         if is_bundled(tar):
             print('%s already has bundled node_modules' % filename)
             return 1
-        tar.extractall(path=tmpdir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=tmpdir)
 
     oldpwd = os.getcwd()
     os.chdir(os.path.join(tmpdir, 'package'))

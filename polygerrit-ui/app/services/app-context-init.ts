b6c9527e67bd95c907d135e4e5df902b543f1005
Chris Poucet <christophe.poucet@gmail.com>
2022-10-25 00:29:19 +0200

    Remove unnecessary flagsService parameter from GrRestApiImpl
    Release-Notes: skip
    Change-Id: I13156b6c9111289f499da052381cb79d387f0cd1

--- polygerrit-ui/app/services/app-context-init.ts
+++ polygerrit-ui/app/services/app-context-init.ts
@@ -83,8 +83,7 @@ export function createAppContext(): AppContext & Finalizable {
     },
     restApiService: (ctx: Partial<AppContext>) => {
       assertIsDefined(ctx.authService, 'authService');
-      assertIsDefined(ctx.flagsService, 'flagsService');
-      return new GrRestApiServiceImpl(ctx.authService, ctx.flagsService);
+      return new GrRestApiServiceImpl(ctx.authService);
     },
     jsApiService: (ctx: Partial<AppContext>) => {
       const reportingService = ctx.reportingService;

1282e1aafc97a40156b4ca8af9d498b65cb3dc85
Marija Savtchouk <mariasavtchouk@google.com>
2022-10-31 15:28:18 +0000

    Fix Error Prone warning
    Change-Id: I3d045d3047801367963b8bbf416be60fd4967d1e
    Release-Notes: skip

--- java/com/google/gerrit/httpd/gitweb/GitwebServlet.java
+++ java/com/google/gerrit/httpd/gitweb/GitwebServlet.java
@@ -110,6 +110,7 @@ class GitwebServlet extends HttpServlet {
   private final Provider<CurrentUser> userProvider;
   private final EnvList _env;
 
+  @SuppressWarnings("CanIgnoreReturnValueSuggester")
   @Inject
   GitwebServlet(
       GitRepositoryManager repoManager,

1a8a3ca971133b93d30ef1f1b4c65b04bcf5213a
Marija Savtchouk <mariasavtchouk@google.com>
2022-10-31 14:32:49 +0000

    Do not set cherryPickOf on RevertSubmission
    Change-Id: I27bb03987a3b0a1fde69facbda6577f58017b94c
    Release-Notes: skip
    Google-Bug-Id: b/255828001

--- java/com/google/gerrit/server/restapi/change/RevertSubmission.java
+++ java/com/google/gerrit/server/restapi/change/RevertSubmission.java
@@ -306,7 +306,7 @@ public class RevertSubmission
       if (cherryPickInput.base.equals(changeNotes.getCurrentPatchSet().commitId().getName())) {
         // This is the code in case this is the first revert of this project + branch, and the
         // revert would be on top of the change being reverted.
-        craeteNormalRevert(revertInput, changeNotes, timestamp);
+        createNormalRevert(revertInput, changeNotes, timestamp);
       } else {
         createCherryPickedRevert(revertInput, project, changeNotes, timestamp);
       }
@@ -345,7 +345,7 @@ public class RevertSubmission
     }
   }
 
-  private void craeteNormalRevert(
+  private void createNormalRevert(
       RevertInput revertInput, ChangeNotes changeNotes, Instant timestamp)
       throws IOException, RestApiException, UpdateException, ConfigInvalidException {
 

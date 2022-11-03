37858ce8f88fd074d13d36c31d6226fc3de57c6a
Dhruv Srivastava <dhruvsri@google.com>
2022-11-03 10:47:51 +0100

    Add missing styles from main.css
    Dialog exists in the top-layer outside the body hence the styles
    in main.css were not being applied resulting in incorrect text
    color in dark theme.
    
    Google-bug-id: b/257083628
    Release-Notes: skip
    Change-Id: I5604c85742fa5ecf3b4a5eebc3d6a42e41282b5e

--- polygerrit-ui/app/styles/gr-modal-styles.ts
+++ polygerrit-ui/app/styles/gr-modal-styles.ts
@@ -12,7 +12,24 @@ export const modalStyles = css`
     border-radius: var(--border-radius);
     background: var(--dialog-background-color);
     box-shadow: var(--elevation-level-5);
+
+    /*
+     * These styles are taken from main.css
+     */
+
+    /*
+     * IE has shoddy support for the font shorthand property.
+     * Work around this using font-size and font-family.
+     */
+    -webkit-text-size-adjust: none;
+    font-family: var(--font-family, ''), 'Roboto', Arial, sans-serif;
+    font-size: var(--font-size-normal, 1rem);
+    line-height: var(--line-height-normal, 1.4);
+    color: var(--primary-text-color, black);
+    height: 100%;
+    transition: none; /* Override the default Polymer fade-in. */
   }
+
   dialog::backdrop {
     background-color: black;
     opacity: var(--modal-opacity, 0.6);

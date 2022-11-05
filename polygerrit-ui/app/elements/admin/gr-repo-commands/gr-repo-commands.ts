95d54fc17b4cc72da772229ead45a3b4b90a7f2c
Luca Milanesio <luca.milanesio@gmail.com>
2022-11-05 00:07:24 +0000

    Re-establish the vertical spacing in the repository commands
    The beautify of the repository commands made in Ida9f3efc1
    has accidentally removed the vertical spacing between buttons,
    making the organisation of all other repository actions
    (e.g., GC and the other provided by plugins) attached together
    with their titles and with themselves.
    
    Rectify the situation and give the same space the buttons had
    in earlier versions of Gerrit by adding a top-margin to `h3`
    as well, that are the titles of the other generic buttons.
    
    Bug: Issue 16389
    Release-Notes: skip
    Change-Id: If6af1dfe9aeaabf4976f86d4938a7adde6aa896d

--- polygerrit-ui/app/elements/admin/gr-repo-commands/gr-repo-commands.ts
+++ polygerrit-ui/app/elements/admin/gr-repo-commands/gr-repo-commands.ts
@@ -89,7 +89,8 @@ export class GrRepoCommands extends LitElement {
       subpageStyles,
       sharedStyles,
       css`
-        #form h2 {
+        #form h2,
+        h3 {
           margin-top: var(--spacing-xxl);
         }
         p {

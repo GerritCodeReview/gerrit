ae93f69f3b1f4e939319a1f49b0cb3db2a9e58d1
Ben Rohlfs <brohlfs@google.com>
2022-11-09 22:07:03 +0100

    Refactor the `handleDown()` method
    Just make this method a bit more readable. Pure refactoring.
    
    Release-Notes: skip
    Change-Id: I93606bd9d928e033469f165d6236c0457da0cda3

--- polygerrit-ui/app/embed/diff/gr-diff-selection/gr-diff-selection.ts
+++ polygerrit-ui/app/embed/diff/gr-diff-selection/gr-diff-selection.ts
@@ -30,6 +30,10 @@ const SelectionClass = {
   BLAME: 'selected-blame',
 };
 
+function selClass(side?: Side) {
+  return side === Side.LEFT ? SelectionClass.LEFT : SelectionClass.RIGHT;
+}
+
 interface LinesCache {
   left: string[] | null;
   right: string[] | null;
@@ -70,34 +74,22 @@ export class GrDiffSelection {
     if (!(target instanceof Element)) return;
 
     if (isThreadEl(target)) {
-      this.setClasses([
-        SelectionClass.COMMENT,
-        getSide(target) === Side.LEFT
-          ? SelectionClass.LEFT
-          : SelectionClass.RIGHT,
-      ]);
+      this.setClasses([SelectionClass.COMMENT, selClass(getSide(target))]);
       return;
     }
 
-    const lineEl = getLineElByChild(target);
     const blameSelected = descendedFromClass(target, 'blame', this.diffTable);
-    if (!lineEl && !blameSelected) {
+    if (blameSelected) {
+      this.setClasses([SelectionClass.BLAME]);
       return;
     }
 
-    const targetClasses = [];
-
-    if (blameSelected) {
-      targetClasses.push(SelectionClass.BLAME);
-    } else if (lineEl) {
-      const side = getSideByLineEl(lineEl);
-
-      targetClasses.push(
-        side === 'left' ? SelectionClass.LEFT : SelectionClass.RIGHT
-      );
+    // This works for both, the content and the line number cells.
+    const lineEl = getLineElByChild(target);
+    if (lineEl) {
+      this.setClasses([selClass(getSideByLineEl(lineEl))]);
+      return;
     }
-
-    this.setClasses(targetClasses);
   };
 
   /**

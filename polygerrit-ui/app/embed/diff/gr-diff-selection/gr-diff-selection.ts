d19216f7423ef6ff23ff089d0a95f7c830f1fa07
Ben Rohlfs <brohlfs@google.com>
2022-11-09 21:58:12 +0100

    Inline method `handleDownOnRangeComment()`
    Pure Refactoring
    
    Release-Notes: skip
    Change-Id: I15af304c99903a23b6f5de342fc16c5ae57ca783

--- polygerrit-ui/app/embed/diff/gr-diff-selection/gr-diff-selection.ts
+++ polygerrit-ui/app/embed/diff/gr-diff-selection/gr-diff-selection.ts
@@ -65,24 +65,20 @@ export class GrDiffSelection {
     this.diffTable.removeEventListener('mousedown', this.handleDown);
   }
 
-  handleDownOnRangeComment(node: Element) {
-    if (isThreadEl(node)) {
-      this.setClasses([
-        SelectionClass.COMMENT,
-        getSide(node) === Side.LEFT
-          ? SelectionClass.LEFT
-          : SelectionClass.RIGHT,
-      ]);
-      return true;
-    }
-    return false;
-  }
-
   handleDown = (e: Event) => {
     const target = e.target;
     if (!(target instanceof Element)) return;
-    const handled = this.handleDownOnRangeComment(target);
-    if (handled) return;
+
+    if (isThreadEl(target)) {
+      this.setClasses([
+        SelectionClass.COMMENT,
+        getSide(target) === Side.LEFT
+          ? SelectionClass.LEFT
+          : SelectionClass.RIGHT,
+      ]);
+      return;
+    }
+
     const lineEl = getLineElByChild(target);
     const blameSelected = descendedFromClass(target, 'blame', this.diffTable);
     if (!lineEl && !blameSelected) {

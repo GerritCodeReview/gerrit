e6e4a4d5f0d152ee54e7aaa671a90984651532f5
Ben Rohlfs <brohlfs@google.com>
2022-12-08 12:31:18 +0100

    Migrate gr-diff-selection to lit based diff
    Change-Id: I3d2acd4bc796f13d34e6d1252297058b623c89fa

--- polygerrit-ui/app/embed/diff/gr-diff-selection/gr-diff-selection_test.ts
+++ polygerrit-ui/app/embed/diff/gr-diff-selection/gr-diff-selection_test.ts
@@ -5,96 +5,46 @@
  */
 import '../../../test/common-test-setup';
 import './gr-diff-selection';
+import '../gr-diff/gr-diff';
+import '../../../elements/shared/gr-comment-thread/gr-comment-thread';
 import {GrDiffSelection} from './gr-diff-selection';
-import {createDiff} from '../../../test/test-data-generators';
+import {
+  createComment,
+  createDiff,
+  createThread,
+} from '../../../test/test-data-generators';
 import {DiffInfo, Side} from '../../../api/diff';
 import {GrFormattedText} from '../../../elements/shared/gr-formatted-text/gr-formatted-text';
 import {fixture, html, assert} from '@open-wc/testing';
-import {mouseDown} from '../../../test/test-utils';
+import {mouseDown, queryAndAssert} from '../../../test/test-utils';
+import {GrDiff} from '../gr-diff/gr-diff';
+import {waitForEventOnce} from '../../../utils/event-util';
+import {createDefaultDiffPrefs} from '../../../constants/constants';
+import {CommentThread} from '../../../utils/comment-util';
+import {LitElement} from 'lit';
 
-const diffTableTemplate = html`
-  <table id="diffTable" class="side-by-side">
-    <tr class="diff-row">
-      <td class="blame" data-line-number="1"></td>
-      <td class="lineNum left" data-value="1">1</td>
-      <td class="content">
-        <div class="contentText" data-side="left">ba ba</div>
-        <div data-side="left">
-          <div class="comment-thread">
-            <div class="gr-formatted-text message">
-              <span id="output" class="gr-formatted-text"
-                >This is a comment</span
-              >
-            </div>
-          </div>
-        </div>
-      </td>
-      <td class="lineNum right" data-value="1">1</td>
-      <td class="content">
-        <div class="contentText" data-side="right">some other text</div>
-      </td>
-    </tr>
-    <tr class="diff-row">
-      <td class="blame" data-line-number="2"></td>
-      <td class="lineNum left" data-value="2">2</td>
-      <td class="content">
-        <div class="contentText" data-side="left">zin</div>
-      </td>
-      <td class="lineNum right" data-value="2">2</td>
-      <td class="content">
-        <div class="contentText" data-side="right">more more more</div>
-        <div data-side="right">
-          <div class="comment-thread">
-            <div class="gr-formatted-text message">
-              <span id="output" class="gr-formatted-text"
-                >This is a comment on the right</span
-              >
-            </div>
-          </div>
-        </div>
-      </td>
-    </tr>
-    <tr class="diff-row">
-      <td class="blame" data-line-number="3"></td>
-      <td class="lineNum left" data-value="3">3</td>
-      <td class="content">
-        <div class="contentText" data-side="left">ga ga</div>
-        <div data-side="left">
-          <div class="comment-thread">
-            <div class="gr-formatted-text message">
-              <span id="output" class="gr-formatted-text"
-                >This is <a>a</a> different comment 💩 unicode is fun</span
-              >
-            </div>
-          </div>
-        </div>
-      </td>
-      <td class="lineNum right" data-value="3">3</td>
-    </tr>
-    <tr class="diff-row">
-      <td class="blame" data-line-number="4"></td>
-      <td class="lineNum left" data-value="4">4</td>
-      <td class="content">
-        <div class="contentText" data-side="left">ga ga</div>
-        <div data-side="left">
-          <div class="comment-thread">
-            <textarea data-side="right">test for textarea copying</textarea>
-          </div>
-        </div>
-      </td>
-      <td class="lineNum right" data-value="4">4</td>
-    </tr>
-    <tr class="not-diff-row">
-      <td class="other">
-        <div class="contentText" data-side="right">some other text</div>
-      </td>
-    </tr>
-  </table>
-`;
+function createThreadEl(thread: CommentThread, side: Side) {
+  const threadEl = document.createElement('gr-comment-thread');
+  threadEl.className = 'comment-thread';
+  threadEl.rootId = thread.rootId;
+  threadEl.thread = thread;
+  threadEl.showPatchset = false;
+  threadEl.showPortedComment = !!thread.ported;
+  // These attributes are the "interface" between comment threads and gr-diff.
+  // <gr-comment-thread> does not care about them and is not affected by them.
+  threadEl.setAttribute('slot', `${side}-${thread.line || 'LOST'}`);
+  threadEl.setAttribute('diff-side', `${side}`);
+  threadEl.setAttribute('line-num', `${thread.line || 'LOST'}`);
+  if (thread.range) {
+    threadEl.setAttribute('range', `${JSON.stringify(thread.range)}`);
+  }
+  return threadEl;
+}
 
 suite('gr-diff-selection', () => {
   let element: GrDiffSelection;
-  let diffTable: HTMLTableElement;
+  let diffTable: HTMLElement;
+  let grDiff: GrDiff;
 
   const emulateCopyOn = function (target: HTMLElement | null) {
     const fakeEvent = {
@@ -112,8 +62,8 @@ suite('gr-diff-selection', () => {
   };
 
   setup(async () => {
-    element = new GrDiffSelection();
-    diffTable = await fixture<HTMLTableElement>(diffTableTemplate);
+    grDiff = await fixture<GrDiff>(html`<gr-diff></gr-diff>`);
+    element = grDiff.diffSelection;
 
     const diff: DiffInfo = {
       ...createDiff(),
@@ -132,51 +82,64 @@ suite('gr-diff-selection', () => {
         },
       ],
     };
-    element.init(diff, diffTable);
+    grDiff.prefs = createDefaultDiffPrefs();
+    grDiff.diff = diff;
+    grDiff.appendChild(
+      createThreadEl({...createThread(createComment()), line: 1}, Side.LEFT)
+    );
+    grDiff.appendChild(
+      createThreadEl({...createThread(createComment()), line: 2}, Side.RIGHT)
+    );
+    grDiff.appendChild(
+      createThreadEl({...createThread(createComment()), line: 3}, Side.LEFT)
+    );
+    await waitForEventOnce(grDiff, 'render');
+    assert.isOk(element.diffTable);
+    diffTable = element.diffTable!;
   });
 
   test('applies selected-left on left side click', () => {
-    element.diffTable!.classList.add('selected-right');
+    diffTable.classList.add('selected-right');
     const lineNumberEl = diffTable.querySelector<HTMLElement>('.lineNum.left');
     if (!lineNumberEl) assert.fail('line number element missing');
     mouseDown(lineNumberEl);
     assert.isTrue(
-      element.diffTable!.classList.contains('selected-left'),
+      diffTable.classList.contains('selected-left'),
       'adds selected-left'
     );
     assert.isFalse(
-      element.diffTable!.classList.contains('selected-right'),
+      diffTable.classList.contains('selected-right'),
       'removes selected-right'
     );
   });
 
   test('applies selected-right on right side click', () => {
-    element.diffTable!.classList.add('selected-left');
+    diffTable.classList.add('selected-left');
     const lineNumberEl = diffTable.querySelector<HTMLElement>('.lineNum.right');
     if (!lineNumberEl) assert.fail('line number element missing');
     mouseDown(lineNumberEl);
     assert.isTrue(
-      element.diffTable!.classList.contains('selected-right'),
+      diffTable.classList.contains('selected-right'),
       'adds selected-right'
     );
     assert.isFalse(
-      element.diffTable!.classList.contains('selected-left'),
+      diffTable.classList.contains('selected-left'),
       'removes selected-left'
     );
   });
 
   test('applies selected-blame on blame click', () => {
-    element.diffTable!.classList.add('selected-left');
+    diffTable.classList.add('selected-left');
     const blameDiv = document.createElement('div');
     blameDiv.classList.add('blame');
-    element.diffTable!.appendChild(blameDiv);
+    diffTable.appendChild(blameDiv);
     mouseDown(blameDiv);
     assert.isTrue(
-      element.diffTable!.classList.contains('selected-blame'),
+      diffTable.classList.contains('selected-blame'),
       'adds selected-right'
     );
     assert.isFalse(
-      element.diffTable!.classList.contains('selected-left'),
+      diffTable.classList.contains('selected-left'),
       'removes selected-left'
     );
   });
@@ -216,25 +179,25 @@ suite('gr-diff-selection', () => {
   });
 
   test('setClasses adds given SelectionClass values, removes others', () => {
-    element.diffTable!.classList.add('selected-right');
+    diffTable.classList.add('selected-right');
     element.setClasses(['selected-comment', 'selected-left']);
-    assert.isTrue(element.diffTable!.classList.contains('selected-comment'));
-    assert.isTrue(element.diffTable!.classList.contains('selected-left'));
-    assert.isFalse(element.diffTable!.classList.contains('selected-right'));
-    assert.isFalse(element.diffTable!.classList.contains('selected-blame'));
+    assert.isTrue(diffTable.classList.contains('selected-comment'));
+    assert.isTrue(diffTable.classList.contains('selected-left'));
+    assert.isFalse(diffTable.classList.contains('selected-right'));
+    assert.isFalse(diffTable.classList.contains('selected-blame'));
 
     element.setClasses(['selected-blame']);
-    assert.isFalse(element.diffTable!.classList.contains('selected-comment'));
-    assert.isFalse(element.diffTable!.classList.contains('selected-left'));
-    assert.isFalse(element.diffTable!.classList.contains('selected-right'));
-    assert.isTrue(element.diffTable!.classList.contains('selected-blame'));
+    assert.isFalse(diffTable.classList.contains('selected-comment'));
+    assert.isFalse(diffTable.classList.contains('selected-left'));
+    assert.isFalse(diffTable.classList.contains('selected-right'));
+    assert.isTrue(diffTable.classList.contains('selected-blame'));
   });
 
   test('setClasses removes before it ads', () => {
-    element.diffTable!.classList.add('selected-right');
-    const addStub = sinon.stub(element.diffTable!.classList, 'add');
+    diffTable.classList.add('selected-right');
+    const addStub = sinon.stub(diffTable.classList, 'add');
     const removeStub = sinon
-      .stub(element.diffTable!.classList, 'remove')
+      .stub(diffTable.classList, 'remove')
       .callsFake(() => {
         assert.isFalse(addStub.called);
       });
@@ -244,49 +207,61 @@ suite('gr-diff-selection', () => {
   });
 
   test('copies content correctly', () => {
-    element.diffTable!.classList.add('selected-left');
-    element.diffTable!.classList.remove('selected-right');
+    diffTable.classList.add('selected-left');
+    diffTable.classList.remove('selected-right');
 
     const selection = document.getSelection();
     if (selection === null) assert.fail('no selection');
     selection.removeAllRanges();
     const range = document.createRange();
-    range.setStart(diffTable.querySelector('div.contentText')!.firstChild!, 3);
+    range.setStart(
+      diffTable.querySelector('div.contentText')!.firstChild!.firstChild!,
+      3
+    );
     range.setEnd(
-      diffTable.querySelectorAll('div.contentText')[4]!.firstChild!,
+      diffTable.querySelectorAll('div.contentText')[4]!.firstChild!.firstChild!,
       2
     );
     selection.addRange(range);
     assert.equal(element.getSelectedText(Side.LEFT, false), 'ba\nzin\nga');
   });
 
-  test('copies comments', () => {
-    element.diffTable!.classList.add('selected-left');
-    element.diffTable!.classList.add('selected-comment');
-    element.diffTable!.classList.remove('selected-right');
+  test.only('copies comments', async () => {
+    diffTable.classList.add('selected-left');
+    diffTable.classList.add('selected-comment');
+    diffTable.classList.remove('selected-right');
     const selection = document.getSelection();
     if (selection === null) assert.fail('no selection');
     selection.removeAllRanges();
     const range = document.createRange();
+    const slots = [...diffTable.querySelectorAll<HTMLSlotElement>('slot')];
+    const slot1 = slots.find(s => s.getAttribute('name') === 'left-1');
+    const slot3 = slots.find(s => s.getAttribute('name') === 'left-3');
+    assert.isOk(slot1);
+    assert.equal(slot1?.assignedElements().length, 1);
+    const commentThreadEl1 = slot1?.assignedElements()[0] as LitElement;
+    const commentEl1 = queryAndAssert(commentThreadEl1, 'gr-comment');
     range.setStart(
-      diffTable.querySelector('.gr-formatted-text *')!.firstChild!,
+      queryAndAssert(commentEl1, '.collapsedContent').firstChild!,
       3
     );
+    const commentThreadEl3 = slot3?.assignedElements()[0] as LitElement;
+    const commentEl3 = queryAndAssert(commentThreadEl3, 'gr-comment');
     range.setEnd(
-      diffTable.querySelectorAll('.gr-formatted-text *')[2].childNodes[2],
+      queryAndAssert(commentEl3, '.collapsedContent').firstChild!,
       7
     );
     selection.addRange(range);
     assert.equal(
-      's is a comment\nThis is a differ',
-      element.getSelectedText(Side.LEFT, true)
+      element.getSelectedText(Side.LEFT, true),
+      's is a comment\nThis is a differ'
     );
   });
 
   test('respects astral chars in comments', () => {
-    element.diffTable!.classList.add('selected-left');
-    element.diffTable!.classList.add('selected-comment');
-    element.diffTable!.classList.remove('selected-right');
+    diffTable.classList.add('selected-left');
+    diffTable.classList.add('selected-comment');
+    diffTable.classList.remove('selected-right');
     const selection = document.getSelection();
     if (selection === null) assert.fail('no selection');
     selection.removeAllRanges();
@@ -299,27 +274,27 @@ suite('gr-diff-selection', () => {
   });
 
   test('defers to default behavior for textarea', () => {
-    element.diffTable!.classList.add('selected-left');
-    element.diffTable!.classList.remove('selected-right');
+    diffTable.classList.add('selected-left');
+    diffTable.classList.remove('selected-right');
     const selectedTextSpy = sinon.spy(element, 'getSelectedText');
     emulateCopyOn(diffTable.querySelector('textarea'));
     assert.isFalse(selectedTextSpy.called);
   });
 
   test('regression test for 4794', () => {
-    element.diffTable!.classList.add('selected-right');
-    element.diffTable!.classList.remove('selected-left');
+    diffTable.classList.add('selected-right');
+    diffTable.classList.remove('selected-left');
 
     const selection = document.getSelection();
     if (!selection) assert.fail('no selection');
     selection.removeAllRanges();
     const range = document.createRange();
     range.setStart(
-      diffTable.querySelectorAll('div.contentText')[1]!.firstChild!,
+      diffTable.querySelectorAll('div.contentText')[1]!.firstChild!.firstChild!,
       4
     );
     range.setEnd(
-      diffTable.querySelectorAll('div.contentText')[1]!.firstChild!,
+      diffTable.querySelectorAll('div.contentText')[1]!.firstChild!.firstChild!,
       10
     );
     selection.addRange(range);
@@ -327,15 +302,18 @@ suite('gr-diff-selection', () => {
   });
 
   test('copies to end of side (issue 7895)', () => {
-    element.diffTable!.classList.add('selected-left');
-    element.diffTable!.classList.remove('selected-right');
+    diffTable.classList.add('selected-left');
+    diffTable.classList.remove('selected-right');
     const selection = document.getSelection();
     if (selection === null) assert.fail('no selection');
     selection.removeAllRanges();
     const range = document.createRange();
-    range.setStart(diffTable.querySelector('div.contentText')!.firstChild!, 3);
+    range.setStart(
+      diffTable.querySelector('div.contentText')!.firstChild!.firstChild!,
+      3
+    );
     range.setEnd(
-      diffTable.querySelectorAll('div.contentText')[4]!.firstChild!,
+      diffTable.querySelectorAll('div.contentText')[4]!.firstChild!.firstChild!,
       2
     );
     selection.addRange(range);
@@ -348,9 +326,9 @@ suite('gr-diff-selection', () => {
     let nodes: NodeListOf<GrFormattedText>;
 
     setup(() => {
-      element.diffTable!.classList.add('selected-left');
-      element.diffTable!.classList.add('selected-comment');
-      element.diffTable!.classList.remove('selected-right');
+      diffTable.classList.add('selected-left');
+      diffTable.classList.add('selected-comment');
+      diffTable.classList.remove('selected-right');
       const s = document.getSelection();
       if (s === null) assert.fail('no selection');
       selection = s;
@@ -364,7 +342,7 @@ suite('gr-diff-selection', () => {
       range.setEnd(nodes[2].childNodes[2], 7);
       selection.addRange(range);
       assert.equal(
-        element.getTextContentForRange(diffTable, selection, range),
+        element.getTextContentForRange(grDiff, selection, range),
         'his is a differ'
       );
     });
@@ -374,7 +352,7 @@ suite('gr-diff-selection', () => {
       range.setEnd(nodes[2].childNodes[2], 7);
       selection.addRange(range);
       assert.equal(
-        element.getTextContentForRange(diffTable, selection, range),
+        element.getTextContentForRange(grDiff, selection, range),
         'a differ'
       );
     });
@@ -384,7 +362,7 @@ suite('gr-diff-selection', () => {
       range.setEnd(nodes[0].firstChild!, 12);
       selection.addRange(range);
       assert.equal(
-        element.getTextContentForRange(diffTable, selection, range),
+        element.getTextContentForRange(grDiff, selection, range),
         'is is a co'
       );
     });

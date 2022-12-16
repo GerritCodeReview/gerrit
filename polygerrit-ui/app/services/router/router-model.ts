97515f96a5ce74075b4c016285a1307c6ded98f3
Ben Rohlfs <brohlfs@google.com>
2022-12-16 12:19:13 +0100

    Move basic change state from router to change view model
    Release-Notes: skip
    Google-Bug-Id: b/247042673
    Change-Id: I3c59896837b18709fbc4bb8584d3abb7354492f1

--- polygerrit-ui/app/services/router/router-model.ts
+++ polygerrit-ui/app/services/router/router-model.ts
@@ -4,11 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import {Observable} from 'rxjs';
-import {
-  NumericChangeId,
-  RevisionPatchSetNum,
-  BasePatchSetNum,
-} from '../../types/common';
 import {Model} from '../../models/model';
 import {select} from '../../utils/observable-util';
 import {define} from '../../models/dependency';
@@ -29,12 +24,6 @@ export enum GerritView {
 export interface RouterState {
   // Note that this router model view must be updated before view model state.
   view?: GerritView;
-  // TODO: Move into ChangeViewState.
-  changeNum?: NumericChangeId;
-  // TODO: Move into ChangeViewState.
-  patchNum?: RevisionPatchSetNum;
-  // TODO: Move into ChangeViewState.
-  basePatchNum?: BasePatchSetNum;
 }
 
 export const routerModelToken = define<RouterModel>('router-model');
@@ -43,20 +32,6 @@ export class RouterModel extends Model<RouterState> {
     this.state$,
     state => state.view
   );
-
-  // TODO: Move into ChangeViewState.
-  readonly routerChangeNum$: Observable<NumericChangeId | undefined> = select(
-    this.state$,
-    state => state.changeNum
-  );
-
-  // TODO: Move into ChangeViewState.
-  readonly routerPatchNum$: Observable<RevisionPatchSetNum | undefined> =
-    select(this.state$, state => state.patchNum);
-
-  // TODO: Move into ChangeViewState.
-  readonly routerBasePatchNum$: Observable<BasePatchSetNum | undefined> =
-    select(this.state$, state => state.basePatchNum);
 
   constructor() {
     super({});

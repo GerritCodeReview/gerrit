8ea8d0ceed6b8d0e770322bc42e87277b9a341cd
Chris Poucet <poucet@google.com>
2023-01-16 09:57:02 +0100

    Throttle star requests.
    This solution is not ideal, because ideally we simply do not send star updates for double clicks.
    
    Additionally, it's still possible to break if you click sufficiently
    fast and more than twice. That being said, this is a good enough
    stop-gap until this gets stored in the model and the model can ensure to
    only save every so often.
    
    Change-Id: Ic9b83b6979a62a840b0ef092f9bffd82b0a0b4ca
    Google-Bug-Id: b/141449307

---
+++ polygerrit-ui/app/services/scheduler/throttling-scheduler_test.ts
@@ -0,0 +1,88 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import '../../test/common-test-setup';
+import {assertFails, waitEventLoop} from '../../test/test-utils';
+import {Scheduler} from './scheduler';
+import {FakeScheduler} from './fake-scheduler';
+import {assert} from '@open-wc/testing';
+import {ThrottlingScheduler} from './throttling-scheduler';
+import {SinonFakeTimers} from 'sinon';
+
+suite('throttling scheduler', () => {
+  let fakeScheduler: FakeScheduler<number>;
+  let scheduler: Scheduler<number>;
+  let clock: SinonFakeTimers;
+  setup(() => {
+    fakeScheduler = new FakeScheduler<number>();
+    scheduler = new ThrottlingScheduler<number>(fakeScheduler, 100);
+    clock = sinon.useFakeTimers();
+    clock.tick(1000);
+  });
+
+  teardown(() => {
+    clock.restore();
+  });
+
+  test('executes tasks', async () => {
+    const promise = scheduler.schedule(async () => 1);
+    assert.equal(fakeScheduler.scheduled.length, 1);
+    fakeScheduler.resolve();
+    assert.equal(fakeScheduler.scheduled.length, 0);
+    const val = await promise;
+    assert.equal(val, 1);
+  });
+
+  test('propagates errors', async () => {
+    const error = new Error('This is an error');
+    const promise = scheduler.schedule(async () => {
+      throw error;
+    });
+    assert.equal(fakeScheduler.scheduled.length, 1);
+    assertFails(promise, error);
+    fakeScheduler.resolve();
+    assert.equal(fakeScheduler.scheduled.length, 0);
+    await promise.catch((reason: Error) => {
+      assert.equal(reason, error);
+    });
+  });
+
+  test('propagates subscheduler errors', async () => {
+    const error = new Error('This is an error');
+    const promise = scheduler.schedule(async () => 1);
+    assert.equal(fakeScheduler.scheduled.length, 1);
+    assertFails(promise, error);
+    fakeScheduler.reject(error);
+    assert.equal(fakeScheduler.scheduled.length, 0);
+    await promise.catch((reason: Error) => {
+      assert.equal(reason, error);
+    });
+  });
+
+  test('throttles RPCs', async () => {
+    const results = [];
+    for (let i = 0; i < 3; ++i) {
+      scheduler.schedule(async () => i).then(() => results.push(i));
+    }
+    assert.equal(fakeScheduler.scheduled.length, 1);
+    fakeScheduler.resolve();
+    assert.equal(fakeScheduler.scheduled.length, 0);
+    await waitEventLoop();
+    assert.equal(fakeScheduler.scheduled.length, 0);
+    clock.tick(99);
+    assert.equal(fakeScheduler.scheduled.length, 0);
+    clock.tick(1);
+    assert.equal(fakeScheduler.scheduled.length, 1);
+    fakeScheduler.resolve();
+    assert.equal(fakeScheduler.scheduled.length, 0);
+    await waitEventLoop();
+    clock.tick(50);
+    assert.equal(fakeScheduler.scheduled.length, 0);
+    clock.tick(50);
+    assert.equal(fakeScheduler.scheduled.length, 1);
+    fakeScheduler.resolve();
+    assert.equal(fakeScheduler.scheduled.length, 0);
+  });
+});

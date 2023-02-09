bfa962d1e1e9d19e3326d7cbbe23be0b86077050
Chris Poucet <poucet@google.com>
2023-02-09 09:52:57 +0000

    Revert "Get rid of ba-linkify."
    This reverts commit b43739843d79486b5cb66f14566235484b3c874c.
    
    Reason for revert: This breaks commit-msg.
    
    Change-Id: If526f8d86bc1b2e66ae6b7f2e693a4b697a366c5

--- polygerrit-ui/app/utils/link-util.ts
+++ polygerrit-ui/app/utils/link-util.ts
@@ -3,6 +3,7 @@
  * Copyright 2022 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
+import 'ba-linkify/ba-linkify';
 import {CommentLinkInfo, CommentLinks} from '../types/common';
 import {getBaseUrl} from './url-util';
 
@@ -15,8 +16,21 @@ export function linkifyUrlsAndApplyRewrite(
   base: string,
   repoCommentLinks: CommentLinks
 ): string {
-  const rewriteResults = getRewriteResultsFromConfig(base, repoCommentLinks);
-  return applyRewrites(base, rewriteResults);
+  const parts: string[] = [];
+  window.linkify(insertZeroWidthSpace(base), {
+    callback: (text, href) => {
+      if (href) {
+        parts.push(removeZeroWidthSpace(createLinkTemplate(href, text)));
+      } else {
+        const rewriteResults = getRewriteResultsFromConfig(
+          text,
+          repoCommentLinks
+        );
+        parts.push(removeZeroWidthSpace(applyRewrites(text, rewriteResults)));
+      }
+    },
+  });
+  return parts.join('');
 }
 
 /**
@@ -125,6 +139,22 @@ function getReplacementText(
       rewrite.suffix
     )
   );
+}
+
+/**
+ * Some tools are known to look for reviewers/CCs by finding lines such as
+ * "R=foo@gmail.com, bar@gmail.com". However, "=" is technically a valid email
+ * character, so ba-linkify interprets the entire string "R=foo@gmail.com" as an
+ * email address. To fix this, we insert a zero width space character \u200B
+ * before linking that prevents ba-linkify from associating the prefix with the
+ * email. After linking we remove the zero width space.
+ */
+function insertZeroWidthSpace(base: string) {
+  return base.replace(/^(R=|CC=)/g, '$&\u200B');
+}
+
+function removeZeroWidthSpace(base: string) {
+  return base.replace(/\u200B/g, '');
 }
 
 function createLinkTemplate(

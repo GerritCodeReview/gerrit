3f5d2c27c4cab96b5ad2f50e38cb059a6195fcec
Paladox none <thomasmulhall410@yahoo.com>
2023-02-21 10:41:49 +0000

    Rename cancel to close in gr-editor-view
    Cancel could mean cancelling the already changed file
    to someone.
    
    Lets rename it to close to remove any other possible meanings
    to users.
    
    We even use "Close" within the code.
    
    Change-Id: I9bf004d666ae104969e511592060536fdcc5f42a

--- polygerrit-ui/app/elements/edit/gr-editor-view/gr-editor-view_test.ts
+++ polygerrit-ui/app/elements/edit/gr-editor-view/gr-editor-view_test.ts
@@ -80,7 +80,7 @@ suite('gr-editor-view tests', () => {
                 role="button"
                 tabindex="0"
               >
-                Cancel
+                Close
               </gr-button>
               <gr-button
                 aria-disabled="true"

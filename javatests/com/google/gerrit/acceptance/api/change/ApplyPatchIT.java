a7265342c6b21cf4fd663c7040ed9f7671c9086b
Nitzan Gur-Furman <nitzan@google.com>
2023-02-22 15:31:23 +0100

    patch:apply - fix base commit picking.
    If `input.base` is provided, it's used as a base.
    Otherwise, the base is set to be the latest patch-set parent.
    
    Patch applying is sensitive to rebasing. Avoiding implicit rebasing
    reduces our error-proneness.
    
    Release-Notes: skip
    Change-Id: Ia00129921525fd74e98ccdc137e8700c084e2fbd

--- javatests/com/google/gerrit/acceptance/api/change/ApplyPatchIT.java
+++ javatests/com/google/gerrit/acceptance/api/change/ApplyPatchIT.java
@@ -17,6 +17,9 @@ package com.google.gerrit.acceptance.api.change;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.gerrit.acceptance.testsuite.project.TestProjectUpdate.block;
 import static com.google.gerrit.acceptance.testsuite.project.TestProjectUpdate.permissionKey;
+import static com.google.gerrit.extensions.client.ListChangesOption.CURRENT_COMMIT;
+import static com.google.gerrit.extensions.client.ListChangesOption.CURRENT_FILES;
+import static com.google.gerrit.extensions.client.ListChangesOption.CURRENT_REVISION;
 import static com.google.gerrit.server.group.SystemGroupBackend.REGISTERED_USERS;
 import static com.google.gerrit.testing.GerritJUnit.assertThrows;
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -47,6 +50,7 @@ import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.inject.Inject;
 import java.io.IOException;
 import org.eclipse.jgit.api.errors.PatchApplyException;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.util.Base64;
 import org.junit.Test;
 
@@ -167,7 +171,7 @@ public class ApplyPatchIT extends AbstractDaemonTest {
     ChangeInfo result = applyPatch(in);
 
     BinaryResult resultPatch = gApi.changes().id(result.id).current().patch();
-    assertThat(removeHeader(resultPatch)).isEqualTo(removeHeader(originalPatch));
+    assertThat(cleanPatch(resultPatch)).isEqualTo(cleanPatch(originalPatch));
   }
 
   @Test
@@ -189,52 +193,54 @@ public class ApplyPatchIT extends AbstractDaemonTest {
     ChangeInfo result = applyPatch(in);
 
     BinaryResult resultPatch = gApi.changes().id(result.id).current().patch();
-    assertThat(removeHeader(resultPatch)).isEqualTo(removeHeader(originalPatch));
+    assertThat(cleanPatch(resultPatch)).isEqualTo(cleanPatch(originalPatch));
   }
 
   @Test
   public void applyGerritBasedPatchUsingRest_success() throws Exception {
     String head = getHead(repo(), HEAD).name();
-    createBranchWithRevision(BranchNameKey.create(project, "branch"), head);
-    PushOneCommit.Result baseCommit = createChange("Add file", ADDED_FILE_NAME, ADDED_FILE_CONTENT);
-    baseCommit.assertOkStatus();
     createBranchWithRevision(BranchNameKey.create(project, DESTINATION_BRANCH), head);
+    PushOneCommit.Result destChange = createChange("refs/for/" + DESTINATION_BRANCH);
+    createBranchWithRevision(BranchNameKey.create(project, "branch"), head);
+    PushOneCommit.Result baseCommit =
+        createChange(testRepo, "branch", "Add file", ADDED_FILE_NAME, ADDED_FILE_CONTENT, "");
+    baseCommit.assertOkStatus();
     RestResponse patchResp =
         userRestSession.get("/changes/" + baseCommit.getChangeId() + "/revisions/current/patch");
     patchResp.assertOK();
     String originalPatch = new String(Base64.decode(patchResp.getEntityContent()), UTF_8);
     ApplyPatchPatchSetInput in = buildInput(originalPatch);
-    PushOneCommit.Result destChange = createChange();
 
     RestResponse resp =
         adminRestSession.post("/changes/" + destChange.getChangeId() + "/patch:apply", in);
 
     resp.assertOK();
     BinaryResult resultPatch = gApi.changes().id(destChange.getChangeId()).current().patch();
-    assertThat(removeHeader(resultPatch)).isEqualTo(removeHeader(originalPatch));
+    assertThat(cleanPatch(resultPatch)).isEqualTo(cleanPatch(originalPatch));
   }
 
   @Test
   public void applyGerritBasedPatchUsingRestWithEncodedPatch_success() throws Exception {
     String head = getHead(repo(), HEAD).name();
-    createBranchWithRevision(BranchNameKey.create(project, "branch"), head);
-    PushOneCommit.Result baseCommit = createChange("Add file", ADDED_FILE_NAME, ADDED_FILE_CONTENT);
-    baseCommit.assertOkStatus();
     createBranchWithRevision(BranchNameKey.create(project, DESTINATION_BRANCH), head);
+    PushOneCommit.Result destChange = createChange("refs/for/" + DESTINATION_BRANCH);
+    createBranchWithRevision(BranchNameKey.create(project, "branch"), head);
+    PushOneCommit.Result baseCommit =
+        createChange(testRepo, "branch", "Add file", ADDED_FILE_NAME, ADDED_FILE_CONTENT, "");
+    baseCommit.assertOkStatus();
     RestResponse patchResp =
         userRestSession.get("/changes/" + baseCommit.getChangeId() + "/revisions/current/patch");
     patchResp.assertOK();
     String originalEncodedPatch = patchResp.getEntityContent();
     String originalDecodedPatch = new String(Base64.decode(patchResp.getEntityContent()), UTF_8);
     ApplyPatchPatchSetInput in = buildInput(originalEncodedPatch);
-    PushOneCommit.Result destChange = createChange();
 
     RestResponse resp =
         adminRestSession.post("/changes/" + destChange.getChangeId() + "/patch:apply", in);
 
     resp.assertOK();
     BinaryResult resultPatch = gApi.changes().id(destChange.getChangeId()).current().patch();
-    assertThat(removeHeader(resultPatch)).isEqualTo(removeHeader(originalDecodedPatch));
+    assertThat(cleanPatch(resultPatch)).isEqualTo(cleanPatch(originalDecodedPatch));
   }
 
   @Test
@@ -395,6 +401,45 @@ public class ApplyPatchIT extends AbstractDaemonTest {
     GitPersonSubject.assertThat(person).email().isEqualTo(admin.email());
   }
 
+  @Test
+  public void applyPatchWithExplicitBase_OverrideParentId() throws Exception {
+    PushOneCommit.Result inputParent = createChange("Input parent", "file1", "content");
+    PushOneCommit.Result parent = createChange("Parent Change", "file2", "content");
+    parent.assertOkStatus();
+    PushOneCommit.Result dest = createChange("Destination Change", "file3", "content");
+    ApplyPatchPatchSetInput in = buildInput(ADDED_FILE_DIFF);
+    in.base = inputParent.getCommit().name();
+
+    gApi.changes().id(dest.getChangeId()).applyPatch(in);
+
+    ChangeInfo result = get(dest.getChangeId(), CURRENT_REVISION, CURRENT_COMMIT);
+    assertThat(result.revisions.get(result.currentRevision).commit.parents.get(0).commit)
+        .isEqualTo(inputParent.getCommit().name());
+
+    BinaryResult resultPatch = gApi.changes().id(dest.getChangeId()).current().patch();
+    assertThat(cleanPatch(resultPatch)).isEqualTo(ADDED_FILE_DIFF.trim());
+  }
+
+  @Test
+  public void applyPatchWithNoExplicitBase_overwritesLatestPatch() throws Exception {
+    PushOneCommit.Result dest = createChange("Destination Change", "ps1.txt", "ps1 content");
+    RevCommit originalParentCommit = dest.getCommit().getParent(0);
+    ApplyPatchPatchSetInput in = buildInput(ADDED_FILE_DIFF);
+
+    gApi.changes().id(dest.getChangeId()).applyPatch(in);
+
+    ChangeInfo result = get(dest.getChangeId(), CURRENT_REVISION, CURRENT_COMMIT, CURRENT_FILES);
+    assertThat(result.revisions.get(result.currentRevision).commit.parents.get(0).commit)
+        .isEqualTo(originalParentCommit.name());
+    assertThat(result.revisions.get(result.currentRevision).files.keySet())
+        .containsExactly(ADDED_FILE_NAME);
+    assertDiffForNewFile(
+        fetchDiffForFile(result, ADDED_FILE_NAME),
+        result.currentRevision,
+        ADDED_FILE_NAME,
+        ADDED_FILE_CONTENT);
+  }
+
   private void initDestBranch() throws Exception {
     String head = getHead(repo(), HEAD).name();
     createBranchWithRevision(BranchNameKey.create(project, ApplyPatchIT.DESTINATION_BRANCH), head);
@@ -425,11 +470,19 @@ public class ApplyPatchIT extends AbstractDaemonTest {
     return gApi.changes().id(result.id).current().file(fileName).diff();
   }
 
-  private String removeHeader(BinaryResult bin) throws IOException {
-    return removeHeader(bin.asString());
+  private String cleanPatch(BinaryResult bin) throws IOException {
+    return cleanPatch(bin.asString());
   }
 
-  private String removeHeader(String s) {
-    return s.substring(s.lastIndexOf("\ndiff --git"), s.length() - 1);
+  private String cleanPatch(String s) {
+    return s
+        // Remove the header
+        .substring(s.lastIndexOf("\ndiff --git"), s.length() - 1)
+        // Remove "index NN..NN lines
+        .replaceAll("(?m)^index.*", "")
+        // Remove empty lines
+        .replaceAll("\n+", "\n")
+        // Trim
+        .trim();
   }
 }

3c74cee6b5edc07e56d46ba947c2b0015c458dda
Ben Rohlfs <brohlfs@google.com>
2023-02-23 16:32:43 +0100

    Make showing reply dialog model based
    The ultimate goal is that change-view does not have to do anything
    in `viewStateChanged()`. Let's reduce it step by step.
    
    Release-Notes: skip
    Google-Bug-Id: b/247042673
    Change-Id: I2794a76e7c910e39a51daf1ba33cf6ef1ebf8e1b

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -675,6 +675,15 @@ export class GrChangeView extends LitElement {
     );
     subscribe(
       this,
+      () => this.getViewModel().openReplyDialog$,
+      openReplyDialog => {
+        // Here we are relying on `this.loggedIn` being set *before*
+        // `openReplyDialog`, but that is fine for this feature.
+        if (openReplyDialog && this.loggedIn) this.handleOpenReplyDialog();
+      }
+    );
+    subscribe(
+      this,
       () => this.getChecksModel().aPluginHasRegistered$,
       b => {
         this.showChecksTab = b;
@@ -2158,7 +2167,6 @@ export class GrChangeView extends LitElement {
   }
 
   private performPostLoadTasks() {
-    this.maybeShowReplyDialog();
     this.maybeShowRevertDialog();
 
     this.sendShowChangeEvent();
@@ -2228,13 +2236,6 @@ export class GrChangeView extends LitElement {
           this.actions.showRevertDialog();
         }
       });
-  }
-
-  private maybeShowReplyDialog() {
-    if (!this.loggedIn) return;
-    if (this.viewState?.openReplyDialog) {
-      this.openReplyDialog(FocusTarget.ANY);
-    }
   }
 
   private updateTitle(change?: ChangeInfo | ParsedChangeInfo) {

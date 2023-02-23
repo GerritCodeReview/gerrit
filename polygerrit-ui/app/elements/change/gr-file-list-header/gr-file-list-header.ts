f1bf09aec4203d8b80686e5b39ac5fc61e40a806
paladox <thomasmulhall410@yahoo.com>
2023-02-23 20:22:12 +0000

    gr-file-list-header: don't use class to hide element
    Change-Id: Iac1b4fdc366208f57e34454aa39c1ea38221b737

--- polygerrit-ui/app/elements/change/gr-file-list-header/gr-file-list-header.ts
+++ polygerrit-ui/app/elements/change/gr-file-list-header/gr-file-list-header.ts
@@ -26,7 +26,7 @@ import {DiffPreferencesInfo} from '../../../types/diff';
 import {GrDiffModeSelector} from '../../../embed/diff/gr-diff-mode-selector/gr-diff-mode-selector';
 import {GrButton} from '../../shared/gr-button/gr-button';
 import {fire, fireNoBubbleNoCompose} from '../../../utils/event-util';
-import {css, html, LitElement} from 'lit';
+import {css, html, LitElement, nothing} from 'lit';
 import {sharedStyles} from '../../../styles/shared-styles';
 import {when} from 'lit/directives/when.js';
 import {ifDefined} from 'lit/directives/if-defined.js';
@@ -161,15 +161,6 @@ export class GrFileListHeader extends LitElement {
         display: flex;
         flex-wrap: wrap;
       }
-      .patchInfo-header .container.latestPatchContainer {
-        display: none;
-      }
-      .patchInfoOldPatchSet .container.latestPatchContainer {
-        display: initial;
-      }
-      .editMode.patchInfoOldPatchSet .container.latestPatchContainer {
-        display: none;
-      }
       .latestPatchContainer a {
         text-decoration: none;
       }
@@ -218,16 +209,7 @@ export class GrFileListHeader extends LitElement {
         margin: 0;
         --gr-button-padding: 2px 4px;
       }
-      .editMode .hideOnEdit {
-        display: none;
-      }
-      .showOnEdit {
-        display: none;
-      }
-      .editMode .showOnEdit {
-        display: initial;
-      }
-      .editMode .showOnEdit.flexContainer {
+      .flexContainer {
         align-items: center;
         display: flex;
       }
@@ -254,11 +236,9 @@ export class GrFileListHeader extends LitElement {
     if (!this.change || !this.diffPrefs) {
       return;
     }
-    const editModeClass = this.computeEditModeClass(this.editMode);
-    const patchInfoClass = this.computePatchInfoClass();
     const expandedClass = this.computeExpandedClass(this.filesExpanded);
     return html`
-      <div class="patchInfo-header ${editModeClass} ${patchInfoClass}">
+      <div class="patchInfo-header">
         <div class="patchInfo-left">
           <div class="patchInfoContent">
             <gr-patch-range-select
@@ -268,17 +248,14 @@ export class GrFileListHeader extends LitElement {
             </gr-patch-range-select>
             <span class="separator"></span>
             <gr-commit-info .commitInfo=${this.commitInfo}></gr-commit-info>
-            <span class="container latestPatchContainer">
-              <span class="separator"></span>
-              <a href=${ifDefined(this.changeUrl)}>Go to latest patch set</a>
-            </span>
+            ${this.renderLatestPatchContainer()}
           </div>
         </div>
         <div class="rightControls ${expandedClass}">
           ${when(
             this.editMode,
             () => html`
-              <span class="showOnEdit flexContainer">
+              <span class="flexContainer">
                 <gr-edit-controls
                   id="editControls"
                   .patchNum=${this.patchNum}
@@ -297,16 +274,7 @@ export class GrFileListHeader extends LitElement {
                   id="modeSelect"
                   .saveOnChange=${true}
                 ></gr-diff-mode-selector>
-                <span id="diffPrefsContainer" class="hideOnEdit">
-                  <gr-tooltip-content has-tooltip title="Diff preferences">
-                    <gr-button
-                      link
-                      class="prefsButton desktop"
-                      @click=${this.handlePrefsTap}
-                      ><gr-icon icon="settings" filled></gr-icon
-                    ></gr-button>
-                  </gr-tooltip-content>
-                </span>
+                ${this.renderDiffPrefsContainer()}
                 <span class="separator"></span>
               </div>
             `
@@ -362,6 +330,36 @@ export class GrFileListHeader extends LitElement {
     `;
   }
 
+  private renderLatestPatchContainer() {
+    if (
+      (this.editMode && this.patchNum !== this.latestPatchNum) ||
+      this.patchNum !== this.latestPatchNum
+    )
+      return nothing;
+    return html`
+      <span class="container latestPatchContainer">
+        <span class="separator"></span>
+        <a href=${ifDefined(this.changeUrl)}>Go to latest patch set</a>
+      </span>
+    `;
+  }
+
+  private renderDiffPrefsContainer() {
+    if (this.editMode) return nothing;
+    return html`
+      <span id="diffPrefsContainer">
+        <gr-tooltip-content has-tooltip title="Diff preferences">
+          <gr-button
+            link
+            class="prefsButton desktop"
+            @click=${this.handleDiffPrefsTap}
+            ><gr-icon icon="settings" filled></gr-icon
+          ></gr-button>
+        </gr-tooltip-content>
+      </span>
+    `;
+  }
+
   private expandAllDiffs() {
     fire(this, 'expand-diffs', {});
   }
@@ -402,7 +400,7 @@ export class GrFileListHeader extends LitElement {
     );
   }
 
-  private handlePrefsTap(e: Event) {
+  private handleDiffPrefsTap(e: Event) {
     e.preventDefault();
     fire(this, 'open-diff-prefs', {});
   }
@@ -411,17 +409,6 @@ export class GrFileListHeader extends LitElement {
     e.preventDefault();
     e.stopPropagation();
     fireNoBubbleNoCompose(this, 'open-download-dialog', {});
-  }
-
-  private computeEditModeClass(editMode?: boolean) {
-    return editMode ? 'editMode' : '';
-  }
-
-  computePatchInfoClass() {
-    if (this.patchNum === this.latestPatchNum) {
-      return '';
-    }
-    return 'patchInfoOldPatchSet';
   }
 
   private createTitle(shortcutName: Shortcut, section: ShortcutSection) {

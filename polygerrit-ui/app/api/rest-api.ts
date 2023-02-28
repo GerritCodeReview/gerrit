84fa59ebd45ddde78a6dfedbe3c159ea04860492
Dhruv Srivastava <dhruvsri@google.com>
2023-02-28 19:19:09 +0100

    Move CommentRange from core to rest-api.ts
    Google-bug-id: b/260980834
    Release-Notes: skip
    Change-Id: I17ef275db6e94155323e7c68a4c63bc65d87c23c

--- polygerrit-ui/app/api/rest-api.ts
+++ polygerrit-ui/app/api/rest-api.ts
@@ -515,6 +515,33 @@ export declare interface ConfigInfo {
   enable_reviewer_by_email: InheritedBooleanInfo;
 }
 
+/**
+ * The CommentRange entity describes the range of an inline comment.
+ * https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#comment-range
+ *
+ * The range includes all characters from the start position, specified by
+ * start_line and start_character, to the end position, specified by end_line
+ * and end_character. The start position is inclusive and the end position is
+ * exclusive.
+ *
+ * So, a range over part of a line will have start_line equal to end_line;
+ * however a range with end_line set to 5 and end_character equal to 0 will not
+ * include any characters on line 5.
+ */
+export declare interface CommentRange {
+  /** The start line number of the range. (1-based) */
+  start_line: number;
+
+  /** The character position in the start line. (0-based) */
+  start_character: number;
+
+  /** The end line number of the range. (1-based) */
+  end_line: number;
+
+  /** The character position in the end line. (0-based) */
+  end_character: number;
+}
+
 export declare interface ConfigListParameterInfo
   extends ConfigParameterInfoBase {
   type: ConfigParameterInfoType.LIST;

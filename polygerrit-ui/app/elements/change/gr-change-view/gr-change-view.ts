57e79274425b8ebf50dc5a0dab45dcbc460f82d5
Ben Rohlfs <brohlfs@google.com>
2023-02-28 19:55:52 +0100

    Remove 7 year old fix of coalescing reviewer_updates to `null`
    See change 80630 for the history.
    
    There is no comment in the original change, but the idea was likely the
    same as in change 79202. I have gone through all usages of
    `reviewer_updates` and was not able to find any location that would
    require `null` to be set.
    
    I think keeping this "fix" around will more likely create bugs than it
    prevents today, because the type of `reviewer_updates` does not even
    allow `null`. And we should consider the `change` object to be
    immutable. Two very good reasons to try without this hack.
    
    Release-Notes: skip
    Change-Id: I498e58e51616e8b3397b67a007d2f7ce6798935d

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -1628,7 +1628,7 @@ export class GrChangeView extends LitElement {
         <gr-messages-list
           .labels=${this.change?.labels}
           .messages=${this.change?.messages}
-          .reviewerUpdates=${this.change?.reviewer_updates}
+          .reviewerUpdates=${this.change?.reviewer_updates ?? []}
           @message-anchor-tap=${this.handleMessageAnchorTap}
           @reply=${this.handleMessageReply}
         ></gr-messages-list>
@@ -2629,9 +2629,6 @@ export class GrChangeView extends LitElement {
     if (!this.change) return;
 
     this.processEdit(this.change);
-    if (!this.change.reviewer_updates) {
-      this.change.reviewer_updates = null as unknown as undefined;
-    }
   }
 
   private isParentCurrent() {

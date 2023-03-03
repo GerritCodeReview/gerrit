d7b1070cac37e5fe0217221748de617f33a2c395
Ben Rohlfs <brohlfs@google.com>
2023-03-03 12:40:39 +0100

    Move file list reset from change view into file list itself
    Release-Notes: skip
    Google-Bug-Id: b/247042673
    Change-Id: I16735f1ccb0b501acd3997ee93bbaf4f2b2b6090

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view_test.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view_test.ts
@@ -80,7 +80,6 @@ import {FocusTarget} from '../gr-reply-dialog/gr-reply-dialog';
 import {GrChangeStar} from '../../shared/gr-change-star/gr-change-star';
 import {GrThreadList} from '../gr-thread-list/gr-thread-list';
 import {assertIsDefined} from '../../../utils/common-util';
-import {DEFAULT_NUM_FILES_SHOWN} from '../gr-file-list/gr-file-list';
 import {fixture, html, assert} from '@open-wc/testing';
 import {deepClone} from '../../../utils/deep-util';
 import {Modifier} from '../../../utils/dom-util';
@@ -1243,40 +1242,12 @@ suite('gr-change-view tests', () => {
     assert.equal(getLabel(true), 'Start Review (3)');
   });
 
-  test('change num change', async () => {
-    const change = {
-      ...createChangeViewChange(),
-      labels: {},
-    } as ParsedChangeInfo;
-    element.changeNum = undefined;
-    element.patchRange = {
-      basePatchNum: PARENT,
-      patchNum: 2 as RevisionPatchSetNum,
-    };
-    element.change = change;
-    assertIsDefined(element.fileList);
-    assert.equal(element.fileList.numFilesShown, DEFAULT_NUM_FILES_SHOWN);
-    element.fileList.numFilesShown = 150;
-    element.fileList.selectedIndex = 15;
-    await element.updateComplete;
-
-    element.changeNum = 2 as NumericChangeId;
-    element.viewState = {
-      ...createChangeViewState(),
-      changeNum: 2 as NumericChangeId,
-    };
-    await element.updateComplete;
-    assert.equal(element.fileList.numFilesShown, DEFAULT_NUM_FILES_SHOWN);
-    assert.equal(element.fileList.selectedIndex, 0);
-  });
-
   test('don’t reload entire page when patchRange changes', async () => {
     const reloadStub = sinon
       .stub(element, 'loadData')
       .callsFake(() => Promise.resolve());
     assertIsDefined(element.fileList);
     await element.fileList.updateComplete;
-    const collapseStub = sinon.stub(element.fileList, 'collapseAllDiffs');
     const value: ChangeViewState = {
       ...createChangeViewState(),
       view: GerritView.CHANGE,
@@ -1288,7 +1259,6 @@ suite('gr-change-view tests', () => {
     assert.isTrue(reloadStub.calledOnce);
 
     element.initialLoadComplete = true;
-    element.fileList.selectedIndex = 15;
     element.change = {
       ...createChangeViewChange(),
       revisions: {
@@ -1302,9 +1272,7 @@ suite('gr-change-view tests', () => {
     element.viewState = {...value};
     await element.updateComplete;
     await waitEventLoop();
-    assert.equal(element.fileList.selectedIndex, 0);
     assert.isFalse(reloadStub.calledTwice);
-    assert.isTrue(collapseStub.calledTwice);
   });
 
   test('do not reload entire page when patchRange doesnt change', async () => {

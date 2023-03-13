9b92d1d3ad7fc6fec0f153c4a25877c2851c1193
Marija Savtchouk <mariasavtchouk@google.com>
2023-03-13 11:11:12 +0100

    Migrate last ProjectIndex FieldDef to IndexField
    Change-Id: Ie9e3a1acac27061774ea1f79038dd63f886927c9
    Release-Notes:skip

--- java/com/google/gerrit/index/project/ProjectSchemaDefinitions.java
+++ java/com/google/gerrit/index/project/ProjectSchemaDefinitions.java
@@ -50,9 +50,11 @@ public class ProjectSchemaDefinitions extends SchemaDefinitions<ProjectData> {
   static final Schema<ProjectData> V2 =
       schema(
           V1,
-          ImmutableList.of(ProjectField.REF_STATE),
-          ImmutableList.<IndexedField<ProjectData, ?>>of(ProjectField.STATE_FIELD),
-          ImmutableList.<IndexedField<ProjectData, ?>.SearchSpec>of(ProjectField.STATE_SPEC));
+          ImmutableList.of(),
+          ImmutableList.<IndexedField<ProjectData, ?>>of(
+              ProjectField.STATE_FIELD, ProjectField.REF_STATE_FIELD),
+          ImmutableList.<IndexedField<ProjectData, ?>.SearchSpec>of(
+              ProjectField.STATE_SPEC, ProjectField.REF_STATE_SPEC));
 
   // Bump Lucene version requires reindexing
   @Deprecated static final Schema<ProjectData> V3 = schema(V2);

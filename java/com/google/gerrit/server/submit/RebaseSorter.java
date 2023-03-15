691b5f146d2858dd844b0e2cc832f277e104c025
Thomas Draebing <thomas.draebing@sap.com>
2023-03-15 15:46:54 +0100

    RebaseIfNecessary: Limit check if commit is merged in any branch
    During RebaseIfNecessary it was checked for each commit, whether
    it was merged into any branch of the repository. This could mean
    a lot of expensive revwalks. This check is however only necessary,
    if the change series to be submitted contains a merge commit.
    
    Release-Notes: skip
    Change-Id: Icbb91683c836b835229353dc5b456e04b9828f3a

--- java/com/google/gerrit/server/submit/RebaseSorter.java
+++ java/com/google/gerrit/server/submit/RebaseSorter.java
@@ -65,6 +65,7 @@ public class RebaseSorter {
   public List<CodeReviewCommit> sort(Collection<CodeReviewCommit> toSort) throws IOException {
     final List<CodeReviewCommit> sorted = new ArrayList<>();
     final Set<CodeReviewCommit> sort = new HashSet<>(toSort);
+    boolean toSortContainsMerge = sort.stream().anyMatch(c -> c.getParentCount() > 1);
     while (!sort.isEmpty()) {
       final CodeReviewCommit n = removeOne(sort);
 
@@ -77,9 +78,30 @@ public class RebaseSorter {
       CodeReviewCommit c;
       final List<CodeReviewCommit> contents = new ArrayList<>();
       while ((c = rw.next()) != null) {
+        // Check whether the commit is the latest patchset of a change to
+        // be submitted and whether it is mergeable, i.e. the data in git
+        // is consistent with Gerrit's change model.
+        //
+        // TODO(Thomas): The `toSortContainsMerge` check was only added because in
+        // some rare cases, it could happen that this check falsely evaluated
+        // to true. This seems to be an issue with thread safety that can occur
+        // on projects with a high submit rate on servers seeing a high load.
+        // This could lead to very long submit times >20 min. The current solution
+        // is a workaround and should be replaced by a proper fix.
         if (!c.has(canMergeFlag) || !incoming.contains(c)) {
+          // In some cases, we are not missing a dependency even if the commit
+          // is not the latest patchset of a change:
+          // a) The commit is the latest patchset of a change that was already
+          //    merged into the target branch.
+          // b) The change series to commit contains a merge and the commit is
+          //    one commit that is merged into the target branch by that change.
+          // In both cases, the rebase can only cover the commits up to this commit
+          // in the RevWalk, thus mark this commit as uninteresting.
+          //
+          // In all other cases, we are missing a dependency and can't perform
+          // the submit.
           if (isMergedInBranchAsSubmittedChange(c, n.change().getDest())
-              || isAlreadyMergedInAnyBranch(c)) {
+              || (toSortContainsMerge && isAlreadyMergedInAnyBranch(c))) {
             rw.markUninteresting(c);
           } else {
             // We cannot merge n as it would bring something we

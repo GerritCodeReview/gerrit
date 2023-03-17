a617774c1c2ac90d9216c7c21a4d1d4484542585
Ben Rohlfs <brohlfs@google.com>
2023-03-17 13:00:02 +0100

    Implement optimistic updates for comment operations
    Release-Notes: skip
    Google-Bug-Id: b/262228572
    Change-Id: I1d79f6f233d9bb72408e1fff18ba5ac0965a65cc

--- polygerrit-ui/app/elements/shared/gr-comment/gr-comment.ts
+++ polygerrit-ui/app/elements/shared/gr-comment/gr-comment.ts
@@ -333,11 +333,6 @@ export class GrComment extends LitElement {
         :host([collapsed]) {
           padding: var(--spacing-s) var(--spacing-m);
         }
-        :host([saving]) {
-          pointer-events: none;
-        }
-        :host([saving]) .actions,
-        :host([saving]) .robotActions,
         :host([saving]) .date {
           opacity: 0.5;
         }
@@ -818,11 +813,7 @@ export class GrComment extends LitElement {
 
   private renderEditButton() {
     if (this.editing) return;
-    return html`<gr-button
-      link
-      ?disabled=${this.saving}
-      class="action edit"
-      @click=${this.edit}
+    return html`<gr-button link class="action edit" @click=${this.edit}
       >Edit</gr-button
     >`;
   }
@@ -877,7 +868,6 @@ export class GrComment extends LitElement {
         link
         secondary
         class="action show-fix"
-        ?disabled=${this.saving}
         @click=${() => this.handleShowFix()}
       >
         Show Fix
@@ -1181,6 +1171,9 @@ export class GrComment extends LitElement {
     try {
       this.saving = true;
       this.unableToSave = false;
+      if (!this.permanentEditingMode) {
+        this.editing = false;
+      }
       if (this.autoSaving) {
         this.comment = await this.autoSaving;
       }
@@ -1201,10 +1194,8 @@ export class GrComment extends LitElement {
           await this.rawSave(messageToSave, {showToast: true});
         }
       }
-      if (!this.permanentEditingMode) {
-        this.editing = false;
-      }
     } catch (e) {
+      this.editing = true;
       this.unableToSave = true;
       throw e;
     } finally {

2c350b1842ae237319c91ea4ff118ab829dd73fa
Thomas Draebing <thomas.draebing@sap.com>
2023-03-28 15:30:32 +0200

    Prevent concurrent submits of the same change
    Usually submitting a change is quite fast and the chance for a submit
    being requested while another submit for the same change is still
    running is very low. Even if it happens, the second submit will fail
    to complete.
    
    However, in large mono repos submits might take considerable time and
    the likelihood for concurrent submits of the same change is increased,
    especially since users might be impatient and try again. This then
    allocates another thread doing unnecessary work.
    
    Now the MergeOP creates a lock for the change to be submitted. The
    currently available implementation for locks uses Guava's Striped
    locks to achieve a change-specific lock. If a submit is ongoing, the
    user is told that a submit is already ongoing.
    
    Currently, only the change that was submitted by the user and not
    potential parent changes in a change series are locked. This means,
    in the case of a change series `change2 -> change1 -> master`, one
    user could submit change 1 and another user change2 while the submit
    of change 1 is still running, even though this submit would then also
    include the submit of change1. This might be added in a later change.
    
    If the MergeOp is run with the dryrun option, the submit will not be
    locked.
    
    Release-Notes: Prevent concurrent submits of same change
    Change-Id: If46e770acbca023b4f440bb9f2d90ad284808c4f

---
+++ java/com/google/gerrit/server/submit/StripedSubmitLock.java
@@ -0,0 +1,37 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.submit;
+
+import com.google.common.util.concurrent.Striped;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.server.config.ThreadSettingsConfig;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.util.concurrent.locks.Lock;
+
+@Singleton
+public class StripedSubmitLock implements SubmitLock {
+  Striped<Lock> lock;
+
+  @Inject
+  public StripedSubmitLock(ThreadSettingsConfig cfg) {
+    this.lock = Striped.lazyWeakLock(cfg.getSshdThreads() + cfg.getHttpdMaxThreads());
+  }
+
+  @Override
+  public Lock get(Change c) {
+    return lock.get(c.getId());
+  }
+}

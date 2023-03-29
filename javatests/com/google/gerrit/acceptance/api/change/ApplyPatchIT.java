6ab763ec2f1d9d2cf8610cdc0a244c49af0c4bea
Nitzan Gur-Furman <nitzan@google.com>
2023-03-29 11:29:16 +0200

    ApplyPatch - fix error handling following the recent jGit change.
    File specific errors (e.g. conflicts) are not causing a failure anymore.
    Instead, they are surfaced as part of the commit message.
    
    Change-Id: Ibb863c47bd819338bdf87362d76275274d67f87a
    Release-Notes: skip
    Bug: Google b/271474227

--- javatests/com/google/gerrit/acceptance/api/change/ApplyPatchIT.java
+++ javatests/com/google/gerrit/acceptance/api/change/ApplyPatchIT.java
@@ -242,6 +242,31 @@ public class ApplyPatchIT extends AbstractDaemonTest {
   }
 
   @Test
+  public void applyPatchWithConflict_appendErrorsToCommitMessage() throws Exception {
+    initBaseWithFile(MODIFIED_FILE_NAME, "Unexpected base content");
+    String patch = ADDED_FILE_DIFF + MODIFIED_FILE_DIFF;
+    ApplyPatchPatchSetInput in = buildInput(patch);
+    in.commitMessage = "subject";
+
+    ChangeInfo result = applyPatch(in);
+
+    assertThat(gApi.changes().id(result.id).current().commit(false).message)
+        .isEqualTo(
+            in.commitMessage
+                + "\n\nNOTE FOR REVIEWERS - errors occurred while applying the patch."
+                + "\nPLEASE REVIEW CAREFULLY.\nErrors:\nError applying patch in "
+                + MODIFIED_FILE_NAME
+                + ", hunk HunkHeader[1,2->1,1]: Hunk cannot be applied\n\nOriginal patch:\n "
+                + cleanPatch(patch)
+                + "\n\nChange-Id: "
+                + result.changeId
+                + "\n");
+    // Error in MODIFIED_FILE should not affect ADDED_FILE results.
+    DiffInfo diff = fetchDiffForFile(result, ADDED_FILE_NAME);
+    assertDiffForNewFile(diff, result.currentRevision, ADDED_FILE_NAME, ADDED_FILE_CONTENT);
+  }
+
+  @Test
   public void applyPatchWithoutAddPatchSetPermissions_fails() throws Exception {
     initDestBranch();
     ApplyPatchPatchSetInput in = buildInput(ADDED_FILE_DIFF);

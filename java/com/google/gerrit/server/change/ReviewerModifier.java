17022251627494d22d0429b44ed8e9c226723cbc
Dmitrii Filippov <dmfilippov@google.com>
2023-04-12 13:47:29 +0000

    Revert "Disallow adding inactive reviewer"
    This reverts commit e84b33e5b345b31a130890a80ebd55451f335fa7.
    
    Reason for revert: the following use-cases were broken by the original change:
    
    1. Revert a change where one of reviewers/cc is inactive.
    2. Removing an inactive reviewer from the "Reply" dialog.
    
    Google-Bug-Id: b/277053099
    Change-Id: I7ff68c37e78c32a4dc9ce07208918862ebf118c1

--- java/com/google/gerrit/server/change/ReviewerModifier.java
+++ java/com/google/gerrit/server/change/ReviewerModifier.java
@@ -113,7 +113,6 @@ public class ReviewerModifier {
 
   private enum FailureType {
     NOT_FOUND,
-    INACTIVE,
     OTHER;
   }
 
@@ -233,9 +232,6 @@ public class ReviewerModifier {
 
       ReviewerModification byAccountId = byAccountId(input, notes, user);
 
-      if (byAccountId.isFailure() && byAccountId.failureType.equals(FailureType.INACTIVE)) {
-        return byAccountId;
-      }
       ReviewerModification wholeGroup = null;
       if (!byAccountId.exactMatchFound) {
         wholeGroup = addWholeGroup(input, notes, user, confirmed, allowGroup, allowByEmail);
@@ -288,13 +284,6 @@ public class ReviewerModifier {
             accountResolver.resolveIncludeInactiveIgnoreVisibility(input.reviewer).asUniqueUser();
       } else {
         reviewerUser = accountResolver.resolveIncludeInactive(input.reviewer).asUniqueUser();
-      }
-      if (reviewerUser.getAccount().inactive()) {
-        return fail(
-            input,
-            FailureType.INACTIVE,
-            String.format(
-                "Cannot add inactive account %s, %s", reviewerUser.getAccountId(), input.reviewer));
       }
       if (input.reviewer.equalsIgnoreCase(reviewerUser.getName())
           || input.reviewer.equals(String.valueOf(reviewerUser.getAccountId()))) {

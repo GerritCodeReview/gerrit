3849a14dce9ebd51125b04d57049ff6435b524fc
Ben Rohlfs <brohlfs@google.com>
2023-04-19 14:35:24 +0200

    Remove resetting `this.editingCommitMessage`
    This is obsolete, because `isLocationChange` is only true once during
    initial page load. And in that case the initial property value is just
    fine to use.
    
    Release-Notes: skip
    Change-Id: Id6049d98f5b8b181c71120414812350c21583624

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -2554,9 +2554,6 @@ export class GrChangeView extends LitElement {
       }
     });
 
-    if (isLocationChange) {
-      this.editingCommitMessage = false;
-    }
     allDataPromises.push(this.filesLoaded());
 
     Promise.all(allDataPromises).then(() => {

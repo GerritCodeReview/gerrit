3bde222f918df8d4f6a0b2eca0ff1eabb28adb52
Ben Rohlfs <brohlfs@google.com>
2023-05-02 10:32:17 +0200

    Consolidate gr-diff-builder and gr-diff-builder-lit into one class
    The abstract base class `GrDiffBuilder` was only introduced for finding
    common ground between legacy and lit diff builders. Legacy diff is gone,
    so let's remove this extra hierarchy level. It allows quite some
    simplifications.
    
    Release-Notes: skip
    Change-Id: Id92e18526731768860579dbb658d5d20fab50b73

--- polygerrit-ui/app/embed/diff/gr-diff-builder/gr-diff-builder-binary.ts
+++ polygerrit-ui/app/embed/diff/gr-diff-builder/gr-diff-builder-binary.ts
@@ -3,7 +3,7 @@
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-import {GrDiffBuilderLit} from './gr-diff-builder-lit';
+import {GrDiffBuilder} from './gr-diff-builder';
 import {DiffInfo, DiffPreferencesInfo} from '../../../types/diff';
 import {createElementDiff} from '../gr-diff/gr-diff-utils';
 import {GrDiffGroup} from '../gr-diff/gr-diff-group';
@@ -11,7 +11,7 @@ import {html, render} from 'lit';
 import {BinaryDiffBuilder} from './gr-diff-builder';
 
 export class GrDiffBuilderBinary
-  extends GrDiffBuilderLit
+  extends GrDiffBuilder
   implements BinaryDiffBuilder
 {
   constructor(

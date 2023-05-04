0e66233d41b45aed6537d84c830e25579c256a0e
Luca Milanesio <luca.milanesio@gmail.com>
2023-05-04 18:27:01 +0100

    RestApiServlet: Do not compute ETag when it is not useful
    There are several REST-API that will never have to
    calculate and return the ETag, even if the resource and view
    would theoretically support the calculation:
    
    - REST-APIs that are not a GET or HEAD
    - Requests and resources with time-based caching
    - Requests with a 'Pragma: no-cache'
    - File or Revision resources that aren't cacheable
    
    For all of those REST-API requests, do not calculate the ETag
    and save precious computing time.
    
    This fixes a REST-API performance regression introduced
    by Ifce700d3e68b where the ETag calculation was moved to
    the beginning of the execution phase.
    
    Bug: Issue 16905
    Change-Id: Ia5f0a6606ccea304583d84f2370057eb7495102a

--- java/com/google/gerrit/server/change/ChangeEditResource.java
+++ java/com/google/gerrit/server/change/ChangeEditResource.java
@@ -15,6 +15,7 @@
 package com.google.gerrit.server.change;
 
 import com.google.gerrit.extensions.restapi.RestResource;
+import com.google.gerrit.extensions.restapi.RestResource.Cacheable;
 import com.google.gerrit.extensions.restapi.RestView;
 import com.google.gerrit.server.edit.ChangeEdit;
 import com.google.inject.TypeLiteral;
@@ -29,7 +30,7 @@ import com.google.inject.TypeLiteral;
  *
  * distinguished by whether path is null or not.
  */
-public class ChangeEditResource implements RestResource {
+public class ChangeEditResource implements RestResource, Cacheable {
   public static final TypeLiteral<RestView<ChangeEditResource>> CHANGE_EDIT_KIND =
       new TypeLiteral<RestView<ChangeEditResource>>() {};
 
@@ -45,6 +46,7 @@ public class ChangeEditResource implements RestResource {
 
   // TODO(davido): Make this cacheable.
   // Should just depend on the SHA-1 of the edit itself.
+  @Override
   public boolean isCacheable() {
     return false;
   }

61ec099baa6ff45936b87c272147ba25ddbc91b0
Ben Rohlfs <brohlfs@google.com>
2023-05-09 21:23:25 +0200

    Move click handling from gr-diff into gr-diff-row
    This is part of a general refactoring effort to move gr-diff complexity
    closer to where events are happening. The legacy approach was to put all
    logic into gr-diff. The Lit approach is to make individual components
    take care of themselves.
    
    Release-Notes: skip
    Change-Id: I80b29a67d0b6da403d7457314fd2abe2d40c56df

--- polygerrit-ui/app/embed/diff/gr-diff-model/gr-diff-model.ts
+++ polygerrit-ui/app/embed/diff/gr-diff-model/gr-diff-model.ts
@@ -10,7 +10,10 @@ import {
   DiffPreferencesInfo,
   DiffViewMode,
   DisplayLine,
+  LineNumber,
+  LineSelectedEventDetail,
   RenderPreferences,
+  Side,
 } from '../../../api/diff';
 import {define} from '../../../models/dependency';
 import {Model} from '../../../models/model';
@@ -29,6 +32,8 @@ import {
 } from '../gr-diff-processor/gr-diff-processor';
 import {GrDiffGroup} from '../gr-diff/gr-diff-group';
 import {createDefaultDiffPrefs} from '../../../constants/constants';
+import {fire} from '../../../utils/event-util';
+import {CreateCommentEventDetail} from '../gr-diff/gr-diff';
 
 export interface DiffState {
   diff?: DiffInfo;
@@ -157,5 +162,25 @@ export class DiffModel extends Model<DiffState> {
     if (i === -1) throw new Error('cannot find context control group');
     groups.splice(i, 1, ...newGroups);
     this.updateState({groups});
+  }
+
+  selectLine(target: EventTarget, number: LineNumber, side: Side) {
+    const path = this.getState().path;
+    if (!path) return;
+
+    const detail: LineSelectedEventDetail = {number, side, path};
+    fire(target, 'line-selected', detail);
+  }
+
+  createComment(target: EventTarget, lineNum: LineNumber, side: Side) {
+    const path = this.getState().path;
+    if (!path) return;
+    const detail: CreateCommentEventDetail = {
+      path,
+      side,
+      lineNum,
+      range: undefined,
+    };
+    fire(target, 'create-comment', detail);
   }
 }

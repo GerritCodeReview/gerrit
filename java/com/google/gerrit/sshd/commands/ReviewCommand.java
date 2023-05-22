63668dd7081ccf21ed0f312b4aa2f276eac5a1a1
Fabio Ponciroli <ponch78@gmail.com>
2023-05-22 23:12:06 +0200

    Log cause when handling SSH submit commands' errors
    When failing because of a RestApiException the exception's
    cause is not used.
    
    At the moment we only send back the exception message to
    the client which is not always set.
    
    It can happen, for example, when running a submit via SSH
    that an error like this is returned to the client:
    "fatal: Cannot submit change fatal: one or more reviews failed;
    review output above".
    
    Added and handled a submit-specific exception (SubmitException)
    to minimize the logging and yet log the exception cause triggering
    the submit error in the error_log.
    
    Change-Id: I0639def8fc6093fa28b8a52c99ad2f1efca518d4
    Release-Notes: Log cause when handling SSH submit commands' errors

--- java/com/google/gerrit/sshd/commands/ReviewCommand.java
+++ java/com/google/gerrit/sshd/commands/ReviewCommand.java
@@ -36,6 +36,7 @@ import com.google.gerrit.server.config.AllProjectsName;
 import com.google.gerrit.server.project.NoSuchChangeException;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.server.project.ProjectState;
+import com.google.gerrit.server.restapi.change.Submit;
 import com.google.gerrit.server.util.LabelVote;
 import com.google.gerrit.sshd.CommandMetaData;
 import com.google.gerrit.sshd.SshCommand;
@@ -215,6 +216,10 @@ public class ReviewCommand extends SshCommand {
         } else {
           reviewPatchSet(patchSet);
         }
+      } catch (Submit.SubmitException e) {
+        ok = false;
+        writeError("error", e.getMessage() + "\n");
+        logger.atSevere().withCause(e).log("submit error while reviewing %s", patchSet.getId());
       } catch (RestApiException | UnloggedFailure e) {
         ok = false;
         writeError("error", e.getMessage() + "\n");

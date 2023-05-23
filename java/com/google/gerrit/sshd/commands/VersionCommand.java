82c4178c4c2cc35c8881d5f7d3bb8f6e8bbf4c51
Saša Živkov <sasa.zivkov@sap.com>
2023-05-23 19:47:32 +0200

    Verbose version output, provide noteDb and index versions
    Extend the "version" program, the "version" SSH command and the
    /config/server/version REST API to optionally provide verbose output
    which, in addition to the Gerrit version, provides the NoteDb version
    and the version of each index.
    
    Example for the SSH command:
    $ ssh <gerrit> version
    gerrit version 3.8.0-335-g711a875804-dirty
    
    $ ssh <gerrit> version --verbose
    NoteDb version 185
    index versions
       83 changes
       13 accounts
        6 projects
       10 groups
    
    Change-Id: Ic6021df637df57d6e903c3520df77f67ec6aac70

--- java/com/google/gerrit/sshd/commands/VersionCommand.java
+++ java/com/google/gerrit/sshd/commands/VersionCommand.java
@@ -16,21 +16,34 @@ package com.google.gerrit.sshd.commands;
 
 import static com.google.gerrit.sshd.CommandMetaData.Mode.MASTER_OR_SLAVE;
 
-import com.google.gerrit.common.Version;
+import com.google.gerrit.extensions.common.VersionInfo;
 import com.google.gerrit.sshd.CommandMetaData;
 import com.google.gerrit.sshd.SshCommand;
+import com.google.inject.Inject;
+import org.kohsuke.args4j.Option;
 
 @CommandMetaData(name = "version", description = "Display gerrit version", runsAt = MASTER_OR_SLAVE)
 final class VersionCommand extends SshCommand {
 
+  @Option(
+      name = "--verbose",
+      aliases = {"-v"},
+      usage = "verbose version info")
+  private boolean verbose;
+
+  @Inject private VersionInfo versionInfo;
+
   @Override
   protected void run() throws Failure {
     enableGracefulStop();
-    String v = Version.getVersion();
-    if (v == null) {
+    if (versionInfo.gerritVersion == null) {
       throw new Failure(1, "fatal: version unavailable");
     }
 
-    stdout.println("gerrit version " + v);
+    if (verbose) {
+      stdout.print(versionInfo.verbose());
+    } else {
+      stdout.print(versionInfo.compact());
+    }
   }
 }

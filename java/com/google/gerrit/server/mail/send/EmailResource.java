4eafcae7b86dfc9fc933fb8a3723c0cbc1acc564
Kamil Musin <kamilm@google.com>
2023-05-23 12:52:05 +0200

    Add the functionality to include inline attachments to EmailSender.
    Change-Id: I07d8b5adffc7ee2f5b260d9410986cb4cea4bea6

---
+++ java/com/google/gerrit/server/mail/send/EmailResource.java
@@ -0,0 +1,25 @@
+package com.google.gerrit.server.mail.send;
+
+import com.google.auto.value.AutoValue;
+import com.google.protobuf.ByteString;
+
+/**
+ * Email resource that can be attached to an email.
+ *
+ * Can be used for images included in html body of the email.
+ */
+@AutoValue
+public abstract class EmailResource {
+  public static EmailResource create(String contentId, String contentType, ByteString content) {
+    return new AutoValue_EmailResource(contentId, contentType, content);
+  }
+
+  /** Value of Content-ID header used for refering to the resource from html body of the email. */
+  public abstract String contentId();
+
+  /** MIME type of the resource. */
+  public abstract String contentType();
+
+  /** Unencoded data that should be added to the email */
+  public abstract ByteString content();
+}

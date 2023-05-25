023c7223bebdbed663495e87f4ac59b1ebe47bd6
Christoforos Miliotis <christoforos.miliotis.dev@gmail.com>
2023-05-25 16:40:52 +0100

    Support Timer1 metrics in unit tests
    Currently, there is support available for metrics in unit tests through
    a stub implementation of the `MetricMaker`, the `TestMetricMaker`.
    The class allows recording how often metrics are invoked, or checking
    how much a metric has increased following an operation. This is
    available for `Counter0` type metrics.
    
    Add support for the `Timer1` metric, which we use in the
    pull-replication plugin (see change 375159).
    
    Release-Notes: skip
    Change-Id: I0c03ff4ed442a884810ea4f896d8a14496c52ec1

--- java/com/google/gerrit/acceptance/TestMetricMaker.java
+++ java/com/google/gerrit/acceptance/TestMetricMaker.java
@@ -14,20 +14,25 @@
 
 package com.google.gerrit.acceptance;
 
+import com.google.gerrit.common.UsedAt;
 import com.google.gerrit.metrics.Counter0;
 import com.google.gerrit.metrics.Description;
 import com.google.gerrit.metrics.DisabledMetricMaker;
+import com.google.gerrit.metrics.Field;
+import com.google.gerrit.metrics.Timer1;
 import com.google.inject.Singleton;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang3.mutable.MutableLong;
 
 /**
  * {@link com.google.gerrit.metrics.MetricMaker} to be bound in tests.
  *
- * <p>Records how often {@link Counter0} metrics are invoked. Metrics of other types are not
- * recorded.
+ * <p>Records how often {@link Counter0} and {@link Timer1} metrics are invoked. Metrics for other
+ * types are not recorded.
  *
- * <p>Allows test to check how much a {@link Counter0} metrics is increased by an operation.
+ * <p>Allows test to check how much a {@link Counter0} and {@link Timer1} metric is increased by an
+ * operation.
  *
  * <p>Example:
  *
@@ -49,17 +54,27 @@ import org.apache.commons.lang3.mutable.MutableLong;
 @Singleton
 public class TestMetricMaker extends DisabledMetricMaker {
   private final ConcurrentHashMap<String, MutableLong> counts = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<String, MutableLong> timers = new ConcurrentHashMap<>();
 
   public long getCount(String counter0Name) {
-    return get(counter0Name).longValue();
+    return getCounterValue(counter0Name).longValue();
+  }
+
+  public long getTimer(String timerName) {
+    return getTimerValue(timerName).longValue();
   }
 
   public void reset() {
     counts.clear();
+    timers.clear();
   }
 
-  private MutableLong get(String counter0Name) {
+  private MutableLong getCounterValue(String counter0Name) {
     return counts.computeIfAbsent(counter0Name, name -> new MutableLong(0));
+  }
+
+  private MutableLong getTimerValue(String timerName) {
+    return counts.computeIfAbsent(timerName, name -> new MutableLong(0));
   }
 
   @Override
@@ -67,7 +82,21 @@ public class TestMetricMaker extends DisabledMetricMaker {
     return new Counter0() {
       @Override
       public void incrementBy(long value) {
-        get(name).add(value);
+        getCounterValue(name).add(value);
+      }
+
+      @Override
+      public void remove() {}
+    };
+  }
+
+  @Override
+  @UsedAt(UsedAt.Project.PLUGIN_PULL_REPLICATION)
+  public <F1> Timer1<F1> newTimer(String name, Description desc, Field<F1> field1) {
+    return new Timer1<>(name, field1) {
+      @Override
+      protected void doRecord(F1 field1, long value, TimeUnit unit) {
+        getTimerValue(name).add(value);
       }
 
       @Override

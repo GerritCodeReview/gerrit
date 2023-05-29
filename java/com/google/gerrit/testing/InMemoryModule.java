0aa440793a8470ec0ed971bd1af76d1c819c49b1
Phan Tran Tuan Khai <tran.tuan.khai.phan@ericsson.com>
2023-05-29 02:34:53 -0400

    Support of group deletion via REST
    A group can now be remove by DELETE on '/groups/<group>'.
    The implementation includes sanity checks that it is possible to
    delete the group:
    
    - The calling user is an administrator
    - The given group is not a system group
    - The given group is an internal group (it is assumed that we will not
      allow to delete external groups)
    - The given group is not the owner of any other group(s)
    - Check that the group is not used in ref permissions
    - Actually delete the group
    - Add documentation
    
    Release-Notes: Implement a new REST-API to remove Gerrit groups
    Feature: Issue 44
    Change-Id: I4ca85843f6d24740d1431f5cdd4462e4ffc0bd0c

--- java/com/google/gerrit/testing/InMemoryModule.java
+++ java/com/google/gerrit/testing/InMemoryModule.java
@@ -293,7 +293,7 @@ public class InMemoryModule extends FactoryModule {
     }
     bind(ServerInformationImpl.class);
     bind(ServerInformation.class).to(ServerInformationImpl.class);
-    install(new RestApiModule());
+    install(new RestApiModule(cfg));
     install(new OAuthRestModule());
     install(new DefaultProjectNameLockManagerModule());
     install(new FileInfoJsonModule());

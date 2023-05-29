6df432a5f73762ac1f23821bbb50da6bd1288ae3
Phan Tran Tuan Khai <tran.tuan.khai.phan@ericsson.com>
2023-05-29 02:34:53 -0400

    Support of group deletion via REST
    A group can now be remove by DELETE on '/groups/<group>'.
    Code is based on previous changes [1].
    
    [1] https://gerrit-review.googlesource.com/c/gerrit/+/129130
    
    Release-Notes: Group Deletion
    Feature: Issue 44
    Change-Id: I4ca85843f6d24740d1431f5cdd4462e4ffc0bd0c

--- javatests/com/google/gerrit/acceptance/api/group/GroupsIT.java
+++ javatests/com/google/gerrit/acceptance/api/group/GroupsIT.java
@@ -66,6 +66,10 @@ import com.google.gerrit.entities.LabelId;
 import com.google.gerrit.entities.Permission;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.extensions.api.access.AccessSectionInfo;
+import com.google.gerrit.extensions.api.access.PermissionInfo;
+import com.google.gerrit.extensions.api.access.PermissionRuleInfo;
+import com.google.gerrit.extensions.api.access.ProjectAccessInput;
 import com.google.gerrit.extensions.api.changes.ReviewInput;
 import com.google.gerrit.extensions.api.groups.GroupApi;
 import com.google.gerrit.extensions.api.groups.GroupInput;
@@ -113,6 +117,7 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -152,6 +157,17 @@ public class GroupsIT extends AbstractDaemonTest {
   @Inject private StalenessChecker stalenessChecker;
   @Inject private ExtensionRegistry extensionRegistry;
   @Inject private GroupsSnapshotReader groupsSnapshotReader;
+
+  private static Correspondence<AccountInfo, String> getAccountToUsernameCorrespondence() {
+    return NullAwareCorrespondence.transforming(
+        accountInfo -> accountInfo.username, "has username");
+  }
+
+  private static void assertIncludes(List<GroupInfo> includes, String... expectedNames) {
+    List<String> names = includes.stream().map(i -> i.name).collect(toImmutableList());
+    assertThat(names).containsExactlyElementsIn(Arrays.asList(expectedNames));
+    assertThat(names).isInOrder();
+  }
 
   @Override
   public Module createModule() {
@@ -722,6 +738,86 @@ public class GroupsIT extends AbstractDaemonTest {
 
     GroupInfo group2 = gApi.groups().create(name).get();
     assertThat(group2.id).isNotEqualTo(group1.id);
+  }
+
+  @Test
+  public void groupDeleted() throws Exception {
+    String name = name("groupToDelete");
+    GroupInfo group = gApi.groups().create(name).get();
+    gApi.groups().id(group.id).delete();
+    assertGroupDoesNotExist(name);
+  }
+
+  @Test
+  public void groupDeletedGroupIsOwner() throws Exception {
+    String groupOwnerName = name("groupIsOwner");
+    String memberGroupName = name("groupIsMember");
+
+    GroupInput groupInput = new GroupInput();
+    groupInput.name = memberGroupName;
+    groupInput.ownerId = groupOwnerName;
+    gApi.groups().create(groupOwnerName).get();
+    gApi.groups().create(groupInput).get();
+
+    Throwable exception =
+        assertThrows(
+            ResourceConflictException.class, () -> gApi.groups().id(groupOwnerName).delete());
+    assertThat(exception.getMessage())
+        .isEqualTo(
+            "Cannot delete group that is owner of other groups: \n" + "[" + memberGroupName + "]");
+  }
+
+  @Test
+  public void groupDeletedGroupIsReferenced() throws Exception {
+    String name = name("groupToDelete");
+    String REFS_HEADS = Constants.R_HEADS + "*";
+    gApi.groups().create(name);
+
+    ProjectAccessInput accessInput = new ProjectAccessInput();
+    accessInput.add = new HashMap<>();
+    accessInput.remove = new HashMap<>();
+    AccessSectionInfo accessSectionInfo = new AccessSectionInfo();
+    accessSectionInfo.permissions = new HashMap<>();
+    PermissionInfo push = new PermissionInfo(null, null);
+    push.rules = new HashMap<>();
+    PermissionRuleInfo pri = new PermissionRuleInfo(PermissionRuleInfo.Action.ALLOW, false);
+    push.rules.put(name, pri);
+    accessSectionInfo.permissions.put(Permission.PUSH, push);
+    accessInput.add.put(REFS_HEADS, accessSectionInfo);
+
+    gApi.projects().name(allProjects.get()).access(accessInput);
+
+    Throwable exception =
+        assertThrows(ResourceConflictException.class, () -> gApi.groups().id(name).delete());
+    assertThat(exception.getMessage())
+        .isEqualTo(
+            "Cannot delete group that is referenced in access permissions for project: \n"
+                + "["
+                + allProjects.get()
+                + "]");
+  }
+
+  @Test
+  public void groupDeletedGroupIsSubgroup() throws Exception {
+    String groupOwnerName = name("groupIsOwner");
+    String memberGroupName = name("groupIsMember");
+
+    GroupInput groupInput = new GroupInput();
+    groupInput.name = memberGroupName;
+    groupInput.ownerId = groupOwnerName;
+    gApi.groups().create(groupOwnerName).get();
+    gApi.groups().create(groupInput).get();
+    gApi.groups().id(groupOwnerName).addGroups(memberGroupName);
+
+    Throwable exception =
+        assertThrows(
+            ResourceConflictException.class, () -> gApi.groups().id(memberGroupName).delete());
+    assertThat(exception.getMessage())
+        .isEqualTo(
+            "Cannot delete group that is subgroup of another group: \n"
+                + "["
+                + groupOwnerName
+                + "]");
   }
 
   @Test
@@ -1568,11 +1664,6 @@ public class GroupsIT extends AbstractDaemonTest {
     }
   }
 
-  private static Correspondence<AccountInfo, String> getAccountToUsernameCorrespondence() {
-    return NullAwareCorrespondence.transforming(
-        accountInfo -> accountInfo.username, "has username");
-  }
-
   private void assertStaleGroupAndReindex(AccountGroup.UUID groupUuid) throws IOException {
     // Evict group from cache to be sure that we use the index state for staleness checks.
     groupCache.evict(groupUuid);
@@ -1687,12 +1778,6 @@ public class GroupsIT extends AbstractDaemonTest {
     assertThat(Iterables.transform(members, i -> i.name))
         .containsExactlyElementsIn(Arrays.asList(expectedNames))
         .inOrder();
-  }
-
-  private static void assertIncludes(List<GroupInfo> includes, String... expectedNames) {
-    List<String> names = includes.stream().map(i -> i.name).collect(toImmutableList());
-    assertThat(names).containsExactlyElementsIn(Arrays.asList(expectedNames));
-    assertThat(names).isInOrder();
   }
 
   private void assertBadRequest(ListRequest req) throws Exception {

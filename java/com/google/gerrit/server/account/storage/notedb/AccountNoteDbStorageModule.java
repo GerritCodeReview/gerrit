c24d1d051860d381033f55514813a2ccfddeb5c8
Nitzan Gur-Furman <nitzan@google.com>
2023-06-02 14:42:40 +0200

    Make `ExternalIds` an interface
    Move implementation to `ExternalIdsNoteDbImpl` under a new
    `storage.notedb` subpackage. Also, move `ExternalIdReader` to the same
    package as part of this change, to avoid making all of its methods
    public.
    
    The following `ExternalIds` methods had no use, and therefore they are
    removed:
    - `get(ExternalId.Key key, ObjectId rev)`
    - `byAccount(Account.Id accountId, String scheme, ObjectId rev)`
    - `allByEmail()`
    
    The following `ExternalIds` are storage specific, and therefore are only
    part of the NoteDb implementation:
    - `all(ObjectId rev)`
    - `byAccount(Account.Id accountId, ObjectId rev)`
    
    This is a part of a series of changes intended to add support for
    different account storage systems.
    
    Bug: Google b/274585632
    Change-Id: I9288579b7480787f1bfacc01ae593058716873e9
    Release-Notes: skip

--- java/com/google/gerrit/server/account/storage/notedb/AccountNoteDbStorageModule.java
+++ java/com/google/gerrit/server/account/storage/notedb/AccountNoteDbStorageModule.java
@@ -14,19 +14,18 @@
 
 package com.google.gerrit.server.account.storage.notedb;
 
-import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.server.account.Accounts;
 import com.google.gerrit.server.account.AccountsUpdate;
-import com.google.gerrit.server.account.externalids.ExternalIdUpsertPreprocessor;
+import com.google.gerrit.server.account.externalids.storage.notedb.ExternalIdNoteDbStorageModule;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
 public class AccountNoteDbStorageModule extends AbstractModule {
   @Override
   protected void configure() {
-    bind(Accounts.class).to(AccountsNoteDbImpl.class).in(Singleton.class);
+    install(new ExternalIdNoteDbStorageModule());
 
-    DynamicMap.mapOf(binder(), ExternalIdUpsertPreprocessor.class);
+    bind(Accounts.class).to(AccountsNoteDbImpl.class).in(Singleton.class);
 
     bind(AccountsUpdate.AccountsUpdateLoader.class)
         .annotatedWith(AccountsUpdate.AccountsUpdateLoader.WithReindex.class)

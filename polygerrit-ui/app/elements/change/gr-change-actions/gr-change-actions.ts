45a2c3992a7cfdb34704f4e4ec6a2d123582e1df
Kaushik Lingarkar <kaushik.lingarkar@linaro.org>
2023-06-06 15:20:01 -0700

    Allow rebase with a secondary email via UI
    Update the UI to add email dropdown menu to gr-confirm-rebase-dialog.
    This dropdown menu provides an options to select the secondary email
    of the current user or latest uploader as commit email while rebasing
    a change. The cases covered for this use case are mentioned below:
    
    Case 1: The dropdown menu is hidden from the UI while rebasing a chain
    of changes are allowed.
    
    Case 2: When conflicts are allowed while rebasing a change and rebasing
    a chain of changes is prevented. The rebase is done on behalf of current
    user and this option is available and visible with current user's
    emails are available for selection for rebasing in the dropdown menu.
    
    Case 3: When conflicts are not allowed and rebasing a chain of changes
    is prevented. The rebase is done on behalf of uploader and this option
    is available and visible with uploader's emails are available for
    selection for rebasing in the dropdown menu.
    
    Release-Notes: rebase can be performed using a secondary email via UI
    Change-Id: Ie6dfbeda66138d65b07095b10147cf7f661114f8

--- polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.ts
+++ polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.ts
@@ -1569,6 +1569,7 @@ export class GrChangeActions
       base: e.detail.base,
       allow_conflicts: e.detail.allowConflicts,
       on_behalf_of_uploader: e.detail.onBehalfOfUploader,
+      committer_email: e.detail.committerEmail,
     };
     const rebaseChain = !!e.detail.rebaseChain;
     this.fireAction(

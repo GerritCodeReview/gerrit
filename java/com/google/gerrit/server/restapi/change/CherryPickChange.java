a7dcf0afb8392c7f454c332f7074903c05fca24f
Yash Chaturvedi <yash.chaturvedi@linaro.org>
2023-06-14 21:48:45 +0530

    Add support to cherry-pick using secondary email via REST API
    Update cherry-pick rest api to accept 'committer_email' as input and use
    this email to commit the change if it is a registered email of the
    current user. This will help in case the user wants to use a secondary
    email instead of their preferred email while cherry-picking a change. UI
    updates will be done in a follow up change.
    
    Release-Notes: REST API supports cherry-pick using a secondary email
    Change-Id: I2c97e714e4bd9d5f4b6c38cd16207928ae5ff153

--- java/com/google/gerrit/server/restapi/change/CherryPickChange.java
+++ java/com/google/gerrit/server/restapi/change/CherryPickChange.java
@@ -248,7 +248,8 @@ public class CherryPickChange {
    *     key exist in the branch. Also thrown when idForNewChange is not null but cherry-pick only
    *     creates a new patchset rather than a new change.
    * @throws UpdateException Problem updating the database using batchUpdateFactory.
-   * @throws RestApiException Error such as invalid SHA1
+   * @throws RestApiException Error such as invalid SHA1, or committer_email not among the
+   *     registered emails of the current user.
    * @throws ConfigInvalidException Can't find account to notify.
    * @throws NoSuchProjectException Can't find project state.
    */
@@ -327,6 +328,7 @@ public class CherryPickChange {
       CodeReviewCommit cherryPickCommit;
       ProjectState projectState =
           projectCache.get(dest.project()).orElseThrow(noSuchProject(dest.project()));
+
       PersonIdent committerIdent =
           Optional.ofNullable(commitToCherryPick.getCommitterIdent())
               .map(
@@ -336,6 +338,20 @@ public class CherryPickChange {
                           .orElseGet(
                               () -> identifiedUser.newCommitterIdent(timestamp, serverZoneId)))
               .orElseGet(() -> identifiedUser.newCommitterIdent(timestamp, serverZoneId));
+
+      if (input.committerEmail != null) {
+        committerIdent =
+            identifiedUser
+                .newCommitterIdent(input.committerEmail, timestamp, serverZoneId)
+                .orElseThrow(
+                    () ->
+                        new BadRequestException(
+                            String.format(
+                                "Cannot cherry-pick using committer email %s, "
+                                    + "as it is not among the registered emails of account %s",
+                                input.committerEmail, identifiedUser.getAccountId().get())));
+      }
+
       try {
         MergeUtil mergeUtil;
         if (input.allowConflicts) {

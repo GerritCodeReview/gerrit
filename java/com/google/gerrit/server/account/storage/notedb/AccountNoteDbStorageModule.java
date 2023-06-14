d2920ed346240fc0be53b159c37921dae9715600
Nitzan Gur-Furman <nitzan@google.com>
2023-06-14 09:29:48 +0000

    DO NOT SUBMIT
    Do not store Gerrit server PersonIdent in singleton classes
    
    The Gerrit server PersonIdent contains a timestamp that is used as the
    commit timestamp when a commit is created in the name of the Gerrit
    server. If that PersonIdent is stored in a singleton class all commits
    will have the same timestamp.
    
    When AccountUpdate was refactored into an interface in change
    Ic4dd13703 (relanded in change
    I12cae821e83f40cc4f692c8cb5c9180f8986881c) the Gerrit server PersonIdent
    was stored in the AccountsUpdateNoteDbImpl.Factory and
    AccountsUpdateNoteDbImpl.FactoryNoReindex singleton classes. Due to this
    commit timestamps when updating external IDs were always the same. Fix
    this by using a provider instead.
    
    Change-Id: I22a6a928c645a27eca75545e42bae86b003ae52d
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Release-Notes: skip
    
    Mark fields in AccountsUpdateNoteDbImpl.AbstractFactory as private final
    
    These fields are only assigned during initialization, hence it's better
    to make them final. Since these fields are only accessed within this
    class they can be private too.
    
    Release-Notes: skip
    Change-Id: I50176c69e4b33337ee85707c424f0eb5749f48d9
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    
    Reland "Refactor AccountUpdate to be an interface"
    
    This reverts commit 847ab1456bda4a77c345838617b21370c4f51537.
    
    Release-Notes: skip
    Change-Id: I2bba3526b16011fd8d768931822b1b53b85f737f

--- java/com/google/gerrit/server/account/storage/notedb/AccountNoteDbStorageModule.java
+++ java/com/google/gerrit/server/account/storage/notedb/AccountNoteDbStorageModule.java
@@ -14,13 +14,25 @@
 
 package com.google.gerrit.server.account.storage.notedb;
 
+import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.server.account.Accounts;
+import com.google.gerrit.server.account.AccountsUpdate;
+import com.google.gerrit.server.account.externalids.ExternalIdUpsertPreprocessor;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
 public class AccountNoteDbStorageModule extends AbstractModule {
   @Override
   protected void configure() {
-    binder().bind(Accounts.class).to(AccountsNoteDbImpl.class).in(Singleton.class);
+    bind(Accounts.class).to(AccountsNoteDbImpl.class).in(Singleton.class);
+
+    DynamicMap.mapOf(binder(), ExternalIdUpsertPreprocessor.class);
+
+    bind(AccountsUpdate.AccountsUpdateLoader.class)
+        .annotatedWith(AccountsUpdate.AccountsUpdateLoader.WithReindex.class)
+        .to(AccountsUpdateNoteDbImpl.Factory.class);
+    bind(AccountsUpdate.AccountsUpdateLoader.class)
+        .annotatedWith(AccountsUpdate.AccountsUpdateLoader.NoReindex.class)
+        .to(AccountsUpdateNoteDbImpl.FactoryNoReindex.class);
   }
 }

9e909a38c3bb137ac40a2dc6a37224f9d7e60db3
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2023-06-15 16:32:48 +0200

    Add tests for BucketedCallback
    Provide test coverage for BucketedCallback to define the behaviour of
    this abstract class in particular with respect to how submetric
    collisions are handled - we want to error on these such that they are
    not missed.
    
    Note that while BucketedCallback's only current implementation should
    avoid collisions as of change 377954, BucketedCallback can't have
    guarantees of this.  This means we have to store the raw `key` in
    `cells` (as we currently do) instead of `submetric(key)`, or we'll hide
    an issue where multiple keys map to the same submetric.
    
    Bug: Issue 40015584
    Release-Notes: skip
    Change-Id: Ieb4f49046c075868c8f0c114f8c390ac91bca32c

---
+++ javatests/com/google/gerrit/metrics/dropwizard/BucketedCallbackTest.java
@@ -0,0 +1,98 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.metrics.dropwizard;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+
+import com.codahale.metrics.MetricRegistry;
+import com.google.gerrit.metrics.Description;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BucketedCallbackTest {
+
+  private MetricRegistry registry;
+
+  private DropWizardMetricMaker metrics;
+
+  private static final String CODE_NAME = "name";
+  private static final String KEY_NAME = "foo";
+  private static final String COLLIDING_KEY_NAME = "setToFoo";
+
+  private String metricName(String fieldValues) {
+    return CODE_NAME + "/" + fieldValues;
+  }
+
+  @Before
+  public void setup() {
+    registry = new MetricRegistry();
+    metrics = new DropWizardMetricMaker(registry, null);
+  }
+
+  @Test
+  public void shouldRegisterMetricWithNewKey() {
+    BucketedCallback<Long> bc = new CallbackMetricTestImpl();
+
+    bc.getOrCreate(KEY_NAME);
+    assertThat(registry.getNames()).containsExactly(metricName(KEY_NAME));
+
+    String newKey = "bar";
+    bc.getOrCreate(newKey);
+    assertThat(registry.getNames()).containsExactly(metricName(KEY_NAME), metricName(newKey));
+  }
+
+  @Test
+  public void shouldNotReRegisterPreviouslyRegisteredMetric() {
+    BucketedCallback<Long> bc = new CallbackMetricTestImpl();
+    bc.getOrCreate(KEY_NAME);
+    bc.getOrCreate(KEY_NAME);
+    assertThat(registry.getNames()).containsExactly(metricName(KEY_NAME));
+  }
+
+  @Test
+  public void shouldStoreKeyValueInCellsAndRegisterSubmetricName() {
+    BucketedCallback<Long> bc = new CallbackMetricTestImpl();
+    bc.getOrCreate(COLLIDING_KEY_NAME);
+    assertThat(bc.getCells().keySet()).containsExactly(COLLIDING_KEY_NAME);
+    assertThat(registry.getNames()).containsExactly(metricName(KEY_NAME));
+  }
+
+  @Test
+  public void shouldErrorIfKeyIsDifferentButNameCollides() {
+    BucketedCallback<Long> bc = new CallbackMetricTestImpl();
+    bc.getOrCreate(KEY_NAME);
+
+    assertThrows(IllegalArgumentException.class, () -> bc.getOrCreate(COLLIDING_KEY_NAME));
+    assertThat(bc.getCells().keySet()).containsExactly(KEY_NAME);
+    assertThat(registry.getNames()).containsExactly(metricName(KEY_NAME));
+  }
+
+  private class CallbackMetricTestImpl extends BucketedCallback<Long> {
+
+    CallbackMetricTestImpl() {
+      super(metrics, registry, CODE_NAME, Long.class, new Description("description"));
+    }
+
+    @Override
+    String name(Object key) {
+      if (key.equals(COLLIDING_KEY_NAME)) {
+        return KEY_NAME;
+      } else {
+        return key.toString();
+      }
+    }
+  }
+}

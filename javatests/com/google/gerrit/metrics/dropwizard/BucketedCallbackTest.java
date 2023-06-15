75b1a3830df347c8d9c7911396d127c51417c11f
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2023-06-15 16:32:48 +0200

    Add tests for BucketedCallback
    Provide test coverage for BucketedCallback to define the behaviour of
    this abstract class in particular with respect to how submetric
    collisions are handled - we want to error on these such that they are
    not missed.
    
    Note that while BucketedCallback's only current implementation should
    avoid clashes as of [1], BucketedCallback can't have guarantees of this.
    This means we have to store the raw `key` in `cells` (as we currently
    do) instead of `submetric(key)`, or we'll hide an issue where multiple
    keys map to the same submetric.
    
    [1] https://gerrit-review.googlesource.com/c/gerrit/+/377954
    
    Bug: Issue 40015584
    Release-Notes: skip
    Change-Id: Ieb4f49046c075868c8f0c114f8c390ac91bca32c

---
+++ javatests/com/google/gerrit/metrics/dropwizard/BucketedCallbackTest.java
@@ -0,0 +1,93 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.metrics.dropwizard;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+
+import com.codahale.metrics.MetricRegistry;
+import com.google.gerrit.metrics.Description;
+import com.google.gerrit.metrics.MetricsReservoirConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class BucketedCallbackTest {
+
+  @Mock MetricsReservoirConfig reservoirConfigMock;
+
+  MetricRegistry registry;
+
+  DropWizardMetricMaker metrics;
+
+  @Before
+  public void setupMocks() {
+    registry = new MetricRegistry();
+    metrics = new DropWizardMetricMaker(registry, reservoirConfigMock);
+  }
+
+  @Test
+  public void shouldRegisterMetricWithNewKey() {
+    BucketedCallback<Long> bc = new CallbackMetricTestImpl();
+
+    bc.getOrCreate("foo");
+    assertThat(registry.getNames()).containsExactly("name/foo");
+
+    bc.getOrCreate("bar");
+    assertThat(registry.getNames()).containsExactly("name/foo", "name/bar");
+  }
+
+  @Test
+  public void shouldNotReregisterPreviouslyRegisteredMetric() {
+    BucketedCallback<Long> bc = new CallbackMetricTestImpl();
+    bc.getOrCreate("foo");
+    bc.getOrCreate("foo");
+    assertThat(registry.getNames()).containsExactly("name/foo");
+  }
+
+  @Test
+  public void shouldStoreKeyValueInCellsAndRegisterSubmetricName() {
+    BucketedCallback<Long> bc = new CallbackMetricTestImpl();
+    bc.getOrCreate("setToFoo");
+    assertThat(bc.getCells().keySet()).containsExactly("setToFoo");
+    assertThat(registry.getNames()).containsExactly("name/foo");
+  }
+
+  @Test
+  public void shouldErrorIfKeyIsDifferentButNameClashes() {
+    BucketedCallback<Long> bc = new CallbackMetricTestImpl();
+    bc.getOrCreate("foo");
+
+    assertThrows(IllegalArgumentException.class, () -> bc.getOrCreate("setToFoo"));
+    assertThat(bc.getCells().keySet()).containsExactly("foo");
+    assertThat(registry.getNames()).containsExactly("name/foo");
+  }
+
+  private class CallbackMetricTestImpl extends BucketedCallback<Long> {
+
+    CallbackMetricTestImpl() {
+      super(metrics, registry, "name", Long.class, new Description("description"));
+    }
+
+    @Override
+    String name(Object key) {
+      if (key.equals("setToFoo")) {
+        return "foo";
+      } else {
+        return key.toString();
+      }
+    }
+  }
+}

f8e9944561fe1aa3114186f4d667e3c1cfe8cdf0
Youssef Elghareeb <ghareeb@google.com>
2023-06-15 18:16:42 +0200

    Add the 'branch' as a field of RevisionInfo
    Currently the branch field is only part of the ChangeInfo/ChangeData,
    however the target branch can be different if the change is moved
    between patchsets.
    
    We keep history of the target branch changes in NoteDb. In this change,
    we parse and include this information as part of RevisionInfo/PatchSet.
    
    A note on compatibility: PatchSet is an entity that's serialized to
    proto in two places:
    1) in ChangeField to be stored in the change index.
    2) as a field in ChangeNotesState, then stored as value in
       ChangeNotesCache.
    
    The modification to the serialization in this change should not be
    harmful, since we first check if the new field exists in the serialized
    proto before reading it on the server. Also an old version of the server
    code will be able to parse the serialized proto.
    
    Until we upgrade the version of the change index, existing values will
    not have the new field set, hence any requests for patchsets data that
    go through the change index will not go through this field. This is also
    similar for cached ChangeNotes that are read from the cache instead of
    being parsed from NoteDb. This means that old changes won't include
    details of the new target branch field, but moving forward, new changes
    will include it. As a band-aid, In RevisionJson, we set this field as
    the current 'change branch' as a fallback if the 'per revision branch'
    does not exist.
    
    Release-Notes: skip
    Forward-Compatible: checked
    Change-Id: I166e86d0e52dbaa9b9c3757d2034827653f745fb

--- javatests/com/google/gerrit/entities/converter/PatchSetProtoConverterTest.java
+++ javatests/com/google/gerrit/entities/converter/PatchSetProtoConverterTest.java
@@ -191,6 +191,7 @@ public class PatchSetProtoConverterTest {
                 .put("groups", new TypeLiteral<ImmutableList<String>>() {}.getType())
                 .put("pushCertificate", new TypeLiteral<Optional<String>>() {}.getType())
                 .put("description", new TypeLiteral<Optional<String>>() {}.getType())
+                .put("targetBranch", new TypeLiteral<Optional<String>>() {}.getType())
                 .build());
   }
 }

9dbfbcf3c555a7cd678f7d0a3fa50552350c6ecd
Jacek Centkowski <geminica.programs@gmail.com>
2023-06-20 09:19:07 +0200

    Modify 'sanitizeMetricName' to avoid collisions
    At present there is a simple 'DropWizardMetricMaker.sanitizeMetricName'
    implementation that:
    * replaces the leading `/` with `_`
    * reduces the repeated `/` chars to a single `/`
    * removes the ending `/`
    * replaces not supported chars with `_`
    
    As a result collision between the sanitized metric names can be easily
    created e.g. `foo_bar` will collide with `foo+bar`.
    
    In order to avoid collisions keep the rules about slashes (they are
    needed anyway) and:
    * replace not supported chars with `_[hex code]_` string
    
    As a result:
    `foo_bar` stays `foo_bar`
    `foo+bar` becomes `foo_2b_bar`
    and collision is no longer possible
    
    The replace algorithm was inspired by the work done in If11e6576eff
    (kudos to Dani).
    
    Bug: Issue 40015585
    Release-Notes: Enhance metric name sanitize function to remove collision on '_' between metrics. Gerrit core metrics remain unchanged but plugins metrics could be affected.
    Change-Id: I6a5366fa0f1fd494006e2234565371d279e8a7f3

--- javatests/com/google/gerrit/metrics/dropwizard/DropWizardMetricMakerTest.java
+++ javatests/com/google/gerrit/metrics/dropwizard/DropWizardMetricMakerTest.java
@@ -44,15 +44,28 @@ public class DropWizardMetricMakerTest {
   }
 
   @Test
+  public void shouldNotSanitizeSafeName() throws Exception {
+    assertThat(metrics.sanitizeMetricName("metric/submetric1/submetric2/submetric3"))
+        .isEqualTo("metric/submetric1/submetric2/submetric3");
+  }
+
+  @Test
+  public void shouldNotCreateSimpleCollisions() throws Exception {
+    String sanitizedCase1 = metrics.sanitizeMetricName("foo_bar");
+    String sanitizedCase2 = metrics.sanitizeMetricName("foo+bar");
+    assertThat(sanitizedCase1).isNotEqualTo(sanitizedCase2);
+  }
+
+  @Test
   public void shouldSanitizeUnwantedChars() throws Exception {
     assertThat(metrics.sanitizeMetricName("very+confusing$long#metric@net/name^1"))
-        .isEqualTo("very_confusing_long_metric_net/name_1");
+        .isEqualTo("very_2b_confusing_24_long_23_metric_40_net/name_5e_1");
     assertThat(metrics.sanitizeMetricName("/metric/submetric")).isEqualTo("_metric/submetric");
   }
 
   @Test
   public void shouldReduceConsecutiveSlashesToOne() throws Exception {
-    assertThat(metrics.sanitizeMetricName("/metric//submetric1///submetric2/submetric3"))
+    assertThat(metrics.sanitizeMetricName("/metric///submetric1///submetric2/submetric3"))
         .isEqualTo("_metric/submetric1/submetric2/submetric3");
   }
 

4b95e7b8dc21ab09573034394e9a8028e4a431ed
Dhruv Srivastava <dhruvsri@google.com>
2023-06-21 13:35:08 +0000

    Test commit
    Change-Id: Ia30c59de040ef99f4d4577a1d1196107a715e772

--- polygerrit-ui/app/constants/constants.ts
+++ polygerrit-ui/app/constants/constants.ts
@@ -7,19 +7,6 @@
 /**
  * Tab names for primary tabs on change view page.
  */
-import {DiffViewMode} from '../api/diff';
-import {DiffPreferencesInfo} from '../types/diff';
-import {EditPreferencesInfo, PreferencesInfo} from '../types/common';
-import {
-  AuthType,
-  ChangeStatus,
-  ConfigParameterInfoType,
-  DefaultDisplayNameConfig,
-  EditableAccountField,
-  FileInfoStatus,
-  GpgKeyInfoStatus,
-  HttpMethod,
-  InheritedBooleanInfoConfiguredValue,
   MergeabilityComputationBehavior,
   ProblemInfoStatus,
   RepoState,

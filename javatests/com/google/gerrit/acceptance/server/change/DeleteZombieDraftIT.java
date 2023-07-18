ca4e99b460d31402553711324a2285f033b8195a
Nitzan Gur-Furman <nitzan@google.com>
2023-07-18 16:12:06 +0200

    Split DeleteZombieCommentsRefs implementation with an abstract class
    The main flow is owned by the parent class, storage access is managed by
    the child class.
    
    This is a part of a series of changes intended to add support for
    different account storage systems.
    
    Bug: Google b/289356568
    Release-Notes: skip
    Change-Id: I44b6266c01dbcbdb9d6f3c438f5ffa0a9c544a5d

--- javatests/com/google/gerrit/acceptance/server/change/DeleteZombieDraftIT.java
+++ javatests/com/google/gerrit/acceptance/server/change/DeleteZombieDraftIT.java
@@ -100,7 +100,9 @@ public class DeleteZombieDraftIT extends AbstractDaemonTest {
     // Run the cleanup logic. The zombie draft is cleared. The published comment is untouched.
     DeleteZombieCommentsRefs worker =
         deleteZombieDraftsFactory.create(/* cleanupPercentage= */ 100, dryRun);
-    assertThat(worker.deleteDraftCommentsThatAreAlsoPublished()).isEqualTo(1);
+    worker.setup();
+    assertThat(worker.listDraftCommentsThatAreAlsoPublished()).hasSize(1);
+    worker.execute();
     if (dryRun) {
       assertThat(getDraftsByParsingDraftRef(draftRef.getName(), revId)).hasSize(1);
     } else {
@@ -136,7 +138,9 @@ public class DeleteZombieDraftIT extends AbstractDaemonTest {
     // Run the zombie cleanup logic. Zombie draft ref for PS2 will be removed.
     DeleteZombieCommentsRefs worker =
         deleteZombieDraftsFactory.create(/* cleanupPercentage= */ 100, dryRun);
-    assertThat(worker.deleteDraftCommentsThatAreAlsoPublished()).isEqualTo(1);
+    worker.setup();
+    assertThat(worker.listDraftCommentsThatAreAlsoPublished()).hasSize(1);
+    worker.execute();
     assertThat(getDraftsByParsingDraftRef(draftRef.getName(), r1.getCommit().name())).hasSize(1);
     if (dryRun) {
       assertThat(getDraftsByParsingDraftRef(draftRef.getName(), r2.getCommit().name())).hasSize(1);
@@ -146,7 +150,8 @@ public class DeleteZombieDraftIT extends AbstractDaemonTest {
     assertNumPublishedComments(changeId, 1);
 
     // Re-run the worker: nothing happens.
-    assertThat(worker.deleteDraftCommentsThatAreAlsoPublished()).isEqualTo(dryRun ? 1 : 0);
+    assertThat(worker.listDraftCommentsThatAreAlsoPublished()).hasSize(dryRun ? 1 : 0);
+    worker.execute();
     assertNumDrafts(changeId, 1);
     assertThat(getDraftsByParsingDraftRef(draftRef.getName(), r1.getCommit().name())).hasSize(1);
     if (dryRun) {

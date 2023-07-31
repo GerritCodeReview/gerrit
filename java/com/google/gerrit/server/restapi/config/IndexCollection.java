093f51d1422e8140375c57872d262c8aa36da75c
Thomas Draebing <thomas.draebing@sap.com>
2023-07-31 08:23:08 +0200

    Add REST API to create snapshots of Lucene indexes
    To safely backup indexes in Gerrit, Gerrit had to be shut down.
    This is required, since an index consists of multiple files and
    copying those while the index changes can lead to a broken
    inconsistent state of the index. Lucene however provides a
    mechanism to create a snapshot at runtime.
    
    This change adds REST API endpoints to create snapshots of all
    write indexes of a given index type. These snapshots will be
    stored on the server in the Gerrit site under `$SITE/index/snapshots`
    and can be used to restore the index state.
    
    Release-Notes: REST API to create snapshot of Lucene indexes
    Change-Id: I50f04e382d02d9dae9dee8b7c98467cd1c0c8cdb

--- java/com/google/gerrit/server/restapi/config/IndexCollection.java
+++ java/com/google/gerrit/server/restapi/config/IndexCollection.java
@@ -16,6 +16,8 @@ package com.google.gerrit.server.restapi.config;
 
 import static com.google.gerrit.common.data.GlobalCapability.MAINTAIN_SERVER;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
 import com.google.gerrit.extensions.annotations.RequiresCapability;
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.restapi.ChildCollection;
@@ -30,6 +32,8 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
 
 @RequiresCapability(MAINTAIN_SERVER)
 @Singleton
@@ -50,11 +54,25 @@ public class IndexCollection implements ChildCollection<ConfigResource, IndexRes
 
   @Override
   public IndexResource parse(ConfigResource parent, IdString id) throws ResourceNotFoundException {
-    String indexName = id.get();
+    if (id.toString().toLowerCase(Locale.US).equals("all")) {
+      ImmutableList.Builder<com.google.gerrit.index.IndexCollection<?, ?, ?>> allIndexes =
+          ImmutableList.builder();
+      for (IndexDefinition<?, ?, ?> def : defs) {
+        allIndexes.add(def.getIndexCollection());
+      }
+      return new IndexResource(allIndexes.build());
+    }
+
+    List<String> segments = Splitter.on('~').splitToList(id.toString());
+    if (segments.size() < 1 || 2 < segments.size()) {
+      throw new ResourceNotFoundException(id);
+    }
+    String indexName = segments.get(0);
+    Integer version = segments.size() == 2 ? Integer.valueOf(segments.get(1)) : null;
 
     for (IndexDefinition<?, ?, ?> def : defs) {
       if (def.getName().equals(indexName)) {
-        return new IndexResource(def.getIndexCollection());
+        return new IndexResource(def.getIndexCollection(), version);
       }
     }
     throw new ResourceNotFoundException("Unknown index requested: " + indexName);

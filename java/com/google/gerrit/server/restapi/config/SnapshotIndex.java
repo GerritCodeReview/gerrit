dccf03a8c08042193b86cacab96dbd9abaa4e5ac
Thomas Draebing <thomas.draebing@sap.com>
2023-07-31 08:23:08 +0200

    Add REST API to create snapshots of Lucene indexes
    To safely backup indexes in Gerrit, Gerrit had to be shut down.
    This is required, since an index consists of multiple files and
    copying those while the index changes can lead to a broken
    inconsistent state of the index. Lucene however provides a
    mechanism to create a snapshot at runtime.
    
    This change adds REST API endpoints to create snapshots of all
    write indexes of a given index type. These snapshots will be
    stored on the server in the Gerrit site under `$SITE/index/snapshots`
    and can be used to restore the index state.
    
    Change-Id: I50f04e382d02d9dae9dee8b7c98467cd1c0c8cdb

---
+++ java/com/google/gerrit/server/restapi/config/SnapshotIndex.java
@@ -0,0 +1,61 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.restapi.config;
+
+import static com.google.gerrit.common.data.GlobalCapability.MAINTAIN_SERVER;
+import static javax.servlet.http.HttpServletResponse.SC_CONFLICT;
+
+import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.index.Index;
+import com.google.gerrit.server.config.IndexResource;
+import com.google.gerrit.server.restapi.config.SnapshotIndex.Input;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+
+@RequiresCapability(MAINTAIN_SERVER)
+@Singleton
+public class SnapshotIndex implements RestModifyView<IndexResource, Input> {
+  private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss");
+
+  @Override
+  public Response<?> apply(IndexResource rsrc, Input input) throws IOException {
+    Collection<Index<?, ?>> indexes = rsrc.getIndexes();
+    String id = input.id;
+    if (id == null) {
+      id = LocalDateTime.now(ZoneId.systemDefault()).format(formatter);
+    }
+    for (Index<?, ?> index : indexes) {
+      try {
+        index.snapshot(id);
+      } catch (FileAlreadyExistsException e) {
+        return Response.withStatusCode(SC_CONFLICT, "Snapshot with same ID already exists.");
+      }
+    }
+    SnapshotInfo info = new SnapshotInfo();
+    info.id = id;
+    return Response.ok(info);
+  }
+
+  public static class Input {
+    String id;
+  }
+}

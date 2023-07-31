3b24e5b412713398b22e3c8429b207f7c638da90
Ben Rohlfs <brohlfs@google.com>
2023-07-31 14:46:30 +0000

    Add a `topicLimit` server config and validate topic size
    Very large topics can degrade system performance and stability. Thus we
    are introducing the ability to limit the size of topics.
    
    We cannot make index queries in `ChangeUpdate`, so we are validating
    the topic size at all 4 call sites of `ChangeUpdate.setTopic()`.
    
    We are introducing a new TopicValidator helper class, because we don't
    want to repeat the code in all 4 call sites.
    
    Release-Notes: Add a `topicLimit` server config and validate topic size
    Google-Bug-Id: b/277917904
    Change-Id: I303eafa9232574a524aef965ddf73f7991609511

---
+++ java/com/google/gerrit/server/git/validators/TopicValidator.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git.validators;
+
+import com.google.common.base.Strings;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.query.change.InternalChangeQuery;
+import com.google.gerrit.server.validators.ValidationException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import org.eclipse.jgit.lib.Config;
+
+/** Validator for topic changes. */
+@Singleton
+public class TopicValidator {
+
+  private final Provider<InternalChangeQuery> queryProvider;
+  private final int topicLimit;
+
+  @Inject
+  public TopicValidator(
+      @GerritServerConfig Config serverConfig, Provider<InternalChangeQuery> queryProvider) {
+    this.queryProvider = queryProvider;
+    this.topicLimit = serverConfig.getInt("change", "topicLimit", 5_000);
+    if (this.topicLimit <= 0) {
+      this.topicLimit = Integer.MAX_VALUE;
+    }
+  }
+
+  public void validateSize(@Nullable String topic) throws ValidationException {
+    if (Strings.isNullOrEmpty(topic)) {
+      return;
+    }
+    int topicSize = queryProvider.get().noFields().byTopicOpen(topic).size();
+    if (topicSize >= topicLimit) {
+      throw new ValidationException(
+          String.format(
+              "Topic '%s' already contains maximum number of allowed changes per 'topicLimit'"
+                  + " server config value %d.",
+              topic, topicLimit));
+    }
+  }
+}

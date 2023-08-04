872c86c6b57488a88705f178c153fead73810be9
Edwin Kempin <ekempin@google.com>
2023-08-04 09:02:44 +0000

    Fix setting parent field in project index for direct children of All-Projects
    We used Project.getParent() to populate the parent field in the project
    index. This method returns null for the All-Projects project, but in
    addition it may also return null for projects that are direct children
    under All-Projects (see javadoc on Project.getParent()). E.g. null is
    returned for projects that are direct children under All-Projects if the
    parent pointer in project.config is missing. Due to this projects that
    are direct children under All-Projects may not have been found when
    querying by "parent:All-Projects". Fix this by using
    ProjectData.getParent() for populating the parent field instead.
    
    To fix the parent field for all possibly affected projects we must bump
    the project index version so that all projects are reindexed.
    
    The fix also affects old index versions, as it fixes the parent field
    whenever an affected project is reindexed. Since the format of the
    parent field is not changed, this fine. FWIW the same index update would
    happen if the parent would be explicitly set in project.config, as it's
    already the case for most projects.
    
    Release-Notes: Fixed setting parent field in project index for direct children of All-Projects
    Change-Id: Ic1199d750eb6f1d5f15d068a1de322ff010e1173
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/index/project/ProjectSchemaDefinitions.java
+++ java/com/google/gerrit/index/project/ProjectSchemaDefinitions.java
@@ -65,7 +65,10 @@ public class ProjectSchemaDefinitions extends SchemaDefinitions<ProjectData> {
   @Deprecated static final Schema<ProjectData> V5 = schema(V4);
 
   // Upgrade Lucene to 8.x requires reindexing.
-  static final Schema<ProjectData> V6 = schema(V5);
+  @Deprecated static final Schema<ProjectData> V6 = schema(V5);
+
+  // Fixing the population of the 'parent' field requires reindexing.
+  static final Schema<ProjectData> V7 = schema(V6);
 
   /**
    * Name of the project index to be used when contacting index backends or loading configurations.

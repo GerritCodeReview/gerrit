595bc14d5a2d693dbae583dde6a0836d04118e64
Martin Fick <martin.fick@linaro.org>
2023-08-09 10:04:58 -0600

    Add in memory change data cache by project
    This cache only stores the data needed to speedup ref advertisements.
    The index is used to populate this cache when it is available otherwise
    it falls back to scanning noteDb (generally on replicas). This cache is
    most useful when using 'git upload-pack' with projects with large change
    counts on replicas.
    
    The following times were seen when running 'git ls-remote' on a project
    with around 380K changes stored on NFS:
    
    With ES index (primary):
     Before this change: (cold 1m3s) 12-17s (with large enough "changes" cache)
     After this change:  (cold 1m3s) 12-17s (with large enough "changes" cache)
    No Index (replica):
     Before this change: (cold ~4m)   ~4m
     After this change:  (cold 2m25s) 12-17s
    
    Although the numbers above do not show an improvement on "best" times
    for primaries using ES, that's because those numbers represent the times
    achieved with the existing "changes" cache enabled. However, since the
    existing "changes" cache is disabled by default, most sites will see an
    improvement after this change with 'git upload-pack' on primaries also,
    and additionally with 'git receive-pack'. Thus the warm times after this
    change are more achievable and more likely to stay fast, even when other
    projects are accessed in between. Before this change it is harder to
    achieve and maintain the warm times.
    
    The existing "changes" cache also is used to help cache changes by
    project, however, it is documented as not being appropriate for
    multi-primary setups since it will be out of date if alterations are
    made to changes outside of the current server. The existing cache is
    also not useable on replicas where there is no index, but when it is
    available, it is used to populate the new cache the first time since it
    should be up-to-date the first time it is used.
    
    The new cache is useable on replicas and is multi-primary friendly
    because it stores the meta-ref id for each change and out-of-date
    changes are detected on each cache fetch resulting in the cache being
    incrementally kept up-to-date. The new cache is much more space
    efficient than the existing "changes" cache as it takes advantage of the
    fact that determining visibility for public changes requires access only
    to a change's destination branch and that many changes are generally
    destined for each branch. So although the existing cache is fairly
    compact, this new cache stores even fewer change data fields. This makes
    it likely possible to keep all the projects on a server cached in
    memory, even on servers with large project and change counts. It may
    make sense to eventually remove the existing changes cache.
    
    Release-Notes: Add cache to speedup ref advertisements (particularly on replicas) and receive-pack
    Forward-Compatible: checked
    Change-Id: Ib9c77ee03d9012cdeb6ad05eed3492c3009e4334

---
+++ java/com/google/gerrit/server/git/ChangesByProjectCacheImpl.java
@@ -0,0 +1,230 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.git;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.cache.Cache;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.entities.BranchNameKey;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.server.ReviewerSet;
+import com.google.gerrit.server.cache.CacheModule;
+import com.google.gerrit.server.git.ChangesByProjectCache.UseIndex;
+import com.google.gerrit.server.index.change.ChangeField;
+import com.google.gerrit.server.logging.Metadata;
+import com.google.gerrit.server.logging.TraceContext;
+import com.google.gerrit.server.logging.TraceContext.TraceTimer;
+import com.google.gerrit.server.notedb.ChangeNotes;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.query.change.InternalChangeQuery;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Lightweight cache of changes in each project.
+ *
+ * <p>This cache is intended to be used when filtering references and stores only the minimal fields
+ * required for a read permission check.
+ */
+@Singleton
+public class ChangesByProjectCacheImpl implements ChangesByProjectCache {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private static final String CACHE_NAME = "changes_by_project";
+
+  public static class Module extends CacheModule {
+    @Override
+    protected void configure() {
+      cache(CACHE_NAME, Project.NameKey.class, CachedProjectChanges.class);
+      bind(ChangesByProjectCache.class).to(ChangesByProjectCacheImpl.class);
+    }
+  }
+
+  private final Cache<Project.NameKey, CachedProjectChanges> cache;
+  private final ChangeData.Factory cdFactory;
+  private final UseIndex useIndex;
+  private final Provider<InternalChangeQuery> queryProvider;
+
+  @Inject
+  ChangesByProjectCacheImpl(
+      @Named(CACHE_NAME) Cache<Project.NameKey, CachedProjectChanges> cache,
+      ChangeData.Factory cdFactory,
+      UseIndex useIndex,
+      Provider<InternalChangeQuery> queryProvider) {
+    this.cache = cache;
+    this.cdFactory = cdFactory;
+    this.useIndex = useIndex;
+    this.queryProvider = queryProvider;
+  }
+
+  @Override
+  public Collection<ChangeData> getChangeDatas(Project.NameKey project, Repository repo)
+      throws IOException {
+    CachedProjectChanges projectChanges = cache.getIfPresent(project);
+    if (projectChanges != null) {
+      return projectChanges.getUpdatedChangeDatas(
+          project, repo, cdFactory, ChangeNotes.Factory.scanChangeIds(repo), "Updating");
+    }
+    if (UseIndex.TRUE.equals(useIndex)) {
+      return queryChangeDatasAndLoad(project);
+    }
+    return scanChangeDatasAndLoad(project, repo);
+  }
+
+  private Collection<ChangeData> scanChangeDatasAndLoad(Project.NameKey project, Repository repo)
+      throws IOException {
+    CachedProjectChanges ours = new CachedProjectChanges();
+    CachedProjectChanges projectChanges = ours;
+    try {
+      projectChanges = cache.get(project, () -> ours);
+    } catch (ExecutionException e) {
+      logger.atWarning().withCause(e).log("Cannot load %s for %s", CACHE_NAME, project.get());
+    }
+    return projectChanges.getUpdatedChangeDatas(
+        project,
+        repo,
+        cdFactory,
+        ChangeNotes.Factory.scanChangeIds(repo),
+        ours == projectChanges ? "Scanning" : "Updating");
+  }
+
+  private Collection<ChangeData> queryChangeDatasAndLoad(Project.NameKey project) {
+    Collection<ChangeData> cds = queryChangeDatas(project);
+    cache.put(project, new CachedProjectChanges(cds));
+    return cds;
+  }
+
+  private Collection<ChangeData> queryChangeDatas(Project.NameKey project) {
+    try (TraceTimer timer =
+        TraceContext.newTimer(
+            "Querying changes of project", Metadata.builder().projectName(project.get()).build())) {
+      return queryProvider
+          .get()
+          .setRequestedFields(ChangeField.CHANGE, ChangeField.REVIEWER, ChangeField.REF_STATE)
+          .byProject(project);
+    }
+  }
+
+  private static class CachedProjectChanges {
+    Map<BranchNameKey, Map<Change.Id, ObjectId>> metaObjectIdByNonPrivateChangeByBranch =
+        new ConcurrentHashMap<>();
+    Map<Change.Id, PrivateChange> privateChangeById = new ConcurrentHashMap<>();
+
+    public CachedProjectChanges() {}
+
+    public CachedProjectChanges(Collection<ChangeData> cds) {
+      cds.stream().forEach(cd -> insert(cd));
+    }
+
+    public Collection<ChangeData> getUpdatedChangeDatas(
+        Project.NameKey project,
+        Repository repo,
+        ChangeData.Factory cdFactory,
+        Map<Change.Id, ObjectId> metaObjectIdByChange,
+        String operation) {
+      try (TraceTimer timer =
+          TraceContext.newTimer(
+              operation + " changes of project",
+              Metadata.builder().projectName(project.get()).build())) {
+        Map<Change.Id, ChangeData> cachedCdByChange = getChangeDataByChange(cdFactory);
+        List<ChangeData> cds = new ArrayList<>();
+        for (Map.Entry<Change.Id, ObjectId> e : metaObjectIdByChange.entrySet()) {
+          Change.Id id = e.getKey();
+          ChangeData cached = cachedCdByChange.get(id);
+          ChangeData cd = cached;
+          if (cd == null || !cached.metaRevisionOrThrow().equals(e.getValue())) {
+            cd = cdFactory.create(project, id);
+            update(cached, cd);
+          }
+          cds.add(cd);
+        }
+        return cds;
+      }
+    }
+
+    public CachedProjectChanges update(ChangeData old, ChangeData updated) {
+      if (old != null) {
+        if (old.isPrivateOrThrow()) {
+          privateChangeById.remove(old.getId());
+        } else {
+          Map<Change.Id, ObjectId> metaObjectIdByNonPrivateChange =
+              metaObjectIdByNonPrivateChangeByBranch.get(old.branchOrThrow());
+          if (metaObjectIdByNonPrivateChange != null) {
+            metaObjectIdByNonPrivateChange.remove(old.getId());
+          }
+        }
+      }
+      return insert(updated);
+    }
+
+    public CachedProjectChanges insert(ChangeData cd) {
+      if (cd.isPrivateOrThrow()) {
+        privateChangeById.put(
+            cd.getId(),
+            new AutoValue_ChangesByProjectCacheImpl_PrivateChange(
+                cd.change(), cd.reviewers(), cd.metaRevisionOrThrow()));
+      } else {
+        metaObjectIdByNonPrivateChangeByBranch
+            .computeIfAbsent(cd.branchOrThrow(), b -> new ConcurrentHashMap<>())
+            .put(cd.getId(), cd.metaRevisionOrThrow());
+      }
+      return this;
+    }
+
+    public Map<Change.Id, ChangeData> getChangeDataByChange(ChangeData.Factory cdFactory) {
+      Map<Change.Id, ChangeData> cdByChange = new HashMap<>(privateChangeById.size());
+      for (PrivateChange pc : privateChangeById.values()) {
+        ChangeData cd = cdFactory.create(pc.change());
+        cd.setReviewers(pc.reviewers());
+        cd.setMetaRevision(pc.metaRevision());
+        cdByChange.put(cd.getId(), cd);
+      }
+
+      for (Map.Entry<BranchNameKey, Map<Change.Id, ObjectId>> e :
+          metaObjectIdByNonPrivateChangeByBranch.entrySet()) {
+        for (Map.Entry<Change.Id, ObjectId> e2 : e.getValue().entrySet()) {
+          Change.Id id = e2.getKey();
+          cdByChange.put(id, cdFactory.createNonPrivate(e.getKey(), id, e2.getValue()));
+        }
+      }
+      return cdByChange;
+    }
+  }
+
+  @AutoValue
+  abstract static class PrivateChange {
+    // Fields needed to serve permission checks on private Changes
+    abstract Change change();
+
+    @Nullable
+    abstract ReviewerSet reviewers();
+
+    abstract ObjectId metaRevision(); // Needed to confirm whether up-to-date
+  }
+}

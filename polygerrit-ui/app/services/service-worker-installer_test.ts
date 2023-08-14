ba134aa8e6e15cce4597d0bac8fed57331221c0a
Chris Poucet <poucet@google.com>
2023-08-14 11:42:06 +0200

    Resolve TODO in gr-rest-api-mock.
    Unfortunately, this leads to a race-condition in
    service-worker-installer_test.ts
    
    Added a TODO there to fix this.
    
    Release-Notes: skip
    Change-Id: I26a3738d43c38c44ec24fb107342c6c3e93f94f2

--- polygerrit-ui/app/services/service-worker-installer_test.ts
+++ polygerrit-ui/app/services/service-worker-installer_test.ts
@@ -20,19 +20,31 @@ suite('service worker installer tests', () => {
     const userModel = testResolver(userModelToken);
     sinon.stub(flagsService, 'isEnabled').returns(true);
     new ServiceWorkerInstaller(flagsService, reportingService, userModel);
+    // TODO: There is a race-condition betweeen preferences being set here
+    // and being loaded from the rest-api-service when the user-model gets created.
+    // So we explicitly wait for the allow_browser_notifications to be false 
+    // before continuing with the test.
+    // Ideally there's a way to wait for models to stabilize.
     const prefs = {
+      ...createDefaultPreferences(),
+      allow_browser_notifications: false,
+    };
+    await waitUntilObserved(
+      userModel.preferences$,
+      pref => pref.allow_browser_notifications === false
+    );
+    userModel.setPreferences(prefs);
+
+    const prefs2 = {
       ...createDefaultPreferences(),
       allow_browser_notifications: true,
     };
-    userModel.setPreferences(prefs);
+    userModel.setPreferences(prefs2);
     await waitUntilObserved(
       userModel.preferences$,
       pref => pref.allow_browser_notifications === true
     );
-    await waitUntilObserved(
-      userModel.preferences$,
-      pref => pref.allow_browser_notifications === true
-    );
+
     assert.isTrue(registerStub.called);
   });
 });

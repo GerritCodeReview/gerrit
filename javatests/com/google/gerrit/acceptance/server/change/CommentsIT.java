b46bfaa8cde9c4c5b7dcef7ea7fc706e25ddb7f7
Fabio Ponciroli <ponch78@gmail.com>
2023-08-15 17:55:09 +0200

    Send ref-updated events on draft operations
    Currently no events are sent when creating, updating and
    deleting drafts. In a multi-master scenario this can be an issue
    since a user might start a review on a node, creating drafts,
    and finish it on another node.
    
    Because no events are produced the nodes won't synchronize drafts.
    
    Due to the nature of drafts, the amount of `ref-updated` events
    created could be high. Hence a flag has been added to enable/disable
    events production on draft operations.
    
    By default no events are produced, to avoid impacting the system
    when upgrading.
    
    Bug: Issue 293918043
    Relase-Notes: Send ref-updated events on draft operations
    
    Change-Id: Icfa11d979cd2e1739252b795f56f05ddadc86fad

--- javatests/com/google/gerrit/acceptance/server/change/CommentsIT.java
+++ javatests/com/google/gerrit/acceptance/server/change/CommentsIT.java
@@ -29,8 +29,10 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.ExtensionRegistry;
 import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.acceptance.config.GerritConfig;
 import com.google.gerrit.acceptance.testsuite.account.AccountOperations;
 import com.google.gerrit.acceptance.testsuite.change.ChangeOperations;
 import com.google.gerrit.acceptance.testsuite.change.TestHumanComment;
@@ -54,6 +56,7 @@ import com.google.gerrit.extensions.client.Side;
 import com.google.gerrit.extensions.common.ChangeInfo;
 import com.google.gerrit.extensions.common.ChangeInput;
 import com.google.gerrit.extensions.common.CommentInfo;
+import com.google.gerrit.extensions.events.GitReferenceUpdatedListener;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.IdString;
@@ -89,6 +92,8 @@ import org.junit.Test;
 @NoHttpd
 public class CommentsIT extends AbstractDaemonTest {
   @Inject private ChangeNoteUtil noteUtil;
+
+  @Inject private ExtensionRegistry extensionRegistry;
   @Inject private FakeEmailSender email;
   @Inject private ProjectOperations projectOperations;
   @Inject private Provider<ChangesCollection> changes;
@@ -96,6 +101,21 @@ public class CommentsIT extends AbstractDaemonTest {
   @Inject private ChangeOperations changeOperations;
   @Inject private AccountOperations accountOperations;
   @Inject private RequestScopeOperations requestScopeOperations;
+
+  private static class TestListener implements GitReferenceUpdatedListener {
+
+    private GitReferenceUpdatedListener.Event lastCommentAddedEvent;
+
+    @Override
+    public void onGitReferenceUpdated(Event event) {
+      lastCommentAddedEvent = event;
+    }
+
+    public GitReferenceUpdatedListener.Event getLastCommentAddedEvent() {
+      assertThat(lastCommentAddedEvent).isNotNull();
+      return lastCommentAddedEvent;
+    }
+  }
 
   private final Integer[] lines = {0, 1};
 
@@ -132,6 +152,66 @@ public class CommentsIT extends AbstractDaemonTest {
       assertThat(list).hasSize(1);
       actual = list.get(0);
       assertThat(comment).isEqualTo(infoToDraft(path).apply(actual));
+    }
+  }
+
+  @GerritConfig(name = "event.stream-events.enableDraftEvents", value = "false")
+  @Test
+  public void doNotFireEventsOnDraftsOperation() throws Exception {
+    TestListener listener = new TestListener();
+    try (ExtensionRegistry.Registration registration =
+        extensionRegistry.newRegistration().add(listener)) {
+      PushOneCommit.Result r = createChange();
+      DraftInput comment =
+          CommentsUtil.newDraftWithOnlyMandatoryFields(PATCHSET_LEVEL, "comment 1");
+      addDraft(r.getChangeId(), r.getCommit().getName(), comment);
+
+      GitReferenceUpdatedListener.Event e = listener.getLastCommentAddedEvent();
+      assertThat(e.getRefName()).doesNotContain("refs/draft-comments/");
+    }
+  }
+
+  @GerritConfig(name = "event.stream-events.enableDraftEvents", value = "true")
+  @Test
+  public void fireEventsForOperationsOnDrafts() throws Exception {
+    TestListener listener = new TestListener();
+    try (ExtensionRegistry.Registration registration =
+        extensionRegistry.newRegistration().add(listener)) {
+      PushOneCommit.Result r = createChange();
+      String changeId = r.getChangeId();
+      String revId = r.getCommit().getName();
+      String path = "file1";
+      DraftInput comment =
+          CommentsUtil.newDraftWithOnlyMandatoryFields(PATCHSET_LEVEL, "comment 1");
+      CommentInfo ci = addDraft(changeId, revId, comment);
+
+      List<CommentInfo> list = getDraftCommentsAsList(changeId);
+      assertThat(list).hasSize(1);
+
+      GitReferenceUpdatedListener.Event e = listener.getLastCommentAddedEvent();
+      assertThat(e.getRefName()).startsWith("refs/draft-comments/");
+      assertThat(e.getProjectName()).isEqualTo(allUsers.get());
+      assertThat(e.isCreate()).isTrue();
+
+      Map<String, List<CommentInfo>> results = getDraftComments(changeId, revId);
+      DraftInput update = CommentsUtil.newDraft(path, Side.REVISION, 1, "comment 2");
+      update.id = Iterables.getOnlyElement(results.get(PATCHSET_LEVEL)).id;
+      update.line = 1;
+
+      updateDraft(changeId, revId, update, ci.id);
+
+      GitReferenceUpdatedListener.Event e2 = listener.getLastCommentAddedEvent();
+      assertThat(e2.getRefName()).startsWith("refs/draft-comments/");
+      assertThat(e2.getProjectName()).isEqualTo(allUsers.get());
+      assertThat(e2.isCreate()).isFalse();
+      assertThat(e2.isDelete()).isFalse();
+
+      deleteDraft(changeId, revId, ci.id);
+
+      GitReferenceUpdatedListener.Event e1 = listener.getLastCommentAddedEvent();
+      assertThat(e1.getRefName()).startsWith("refs/draft-comments/");
+      assertThat(e1.getProjectName()).isEqualTo(allUsers.get());
+      assertThat(e1.isDelete()).isTrue();
     }
   }
 

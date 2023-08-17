c32839cc3afa963854be054fe75aa3306d3db75c
Chris Poucet <poucet@google.com>
2023-08-17 13:32:16 +0200

    Be more permissive in how we match the revert message.
    Google-Bug-Id: b/296321789
    Release-Notes: skip
    Change-Id: I3675772873656e4a47149baf1739448314a7fe51

--- polygerrit-ui/app/utils/message-util_test.ts
+++ polygerrit-ui/app/utils/message-util_test.ts
@@ -29,4 +29,24 @@ suite('message-util tests', () => {
       'If02ca1cd494579d6bb92a157bf1819e3689cd6b1' as ChangeId,
     ]);
   });
+
+  test('getRevertCreatedChangeIds with extra spam', () => {
+    const messages = [
+      {
+        ...createChangeMessage(),
+        message:
+          'Created a revert of this change as IIf02ca1cd494579d6bb92a157bf1819e3689cd6b1',
+        tag: MessageTag.TAG_REVERT as ReviewInputTag,
+      },
+      {
+        ...createChangeMessage(),
+        message: 'Created a revert of this change as abc',
+        tag: undefined,
+      },
+    ];
+
+    assert.deepEqual(getRevertCreatedChangeIds(messages), [
+      'If02ca1cd494579d6bb92a157bf1819e3689cd6b1' as ChangeId,
+    ]);
+  });
 });

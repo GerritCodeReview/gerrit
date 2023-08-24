042c3ed1946b44b9b4e66b3f80ef20ba9df7e352
Ben Rohlfs <brohlfs@google.com>
2023-08-24 09:50:29 +0200

    Fetch docs base url from model
    Release-Notes: skip
    Change-Id: I6b42a0134e0e18ca4ba1a75d64c7613ef74e8b58

--- polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-view.ts
+++ polygerrit-ui/app/elements/settings/gr-settings-view/gr-settings-view.ts
@@ -68,6 +68,7 @@ import {userModelToken} from '../../../models/user/user-model';
 import {modalStyles} from '../../../styles/gr-modal-styles';
 import {navigationToken} from '../../core/gr-navigation/gr-navigation';
 import {rootUrl} from '../../../utils/url-util';
+import {configModelToken} from '../../../models/config/config-model';
 
 const GERRIT_DOCS_BASE_URL =
   'https://gerrit-review.googlesource.com/' + 'Documentation';
@@ -207,6 +208,8 @@ export class GrSettingsView extends LitElement {
 
   private readonly getUserModel = resolve(this, userModelToken);
 
+  private readonly getConfigModel = resolve(this, configModelToken);
+
   // private but used in test
   readonly flagsService = getAppContext().flagsService;
 
@@ -230,6 +233,11 @@ export class GrSettingsView extends LitElement {
       acc => {
         this.account = acc;
       }
+    );
+    subscribe(
+      this,
+      () => this.getConfigModel().docsBaseUrl$,
+      docsBaseUrl => (this.docsBaseUrl = docsBaseUrl)
     );
     subscribe(
       this,
@@ -293,12 +301,6 @@ export class GrSettingsView extends LitElement {
             this.gpgEditorPromise.then(gpgEditor => gpgEditor.loadData())
           );
         }
-
-        configPromises.push(
-          this.restApiService.getDocsBaseUrl(config).then(baseUrl => {
-            this.docsBaseUrl = baseUrl;
-          })
-        );
 
         return Promise.all(configPromises);
       })

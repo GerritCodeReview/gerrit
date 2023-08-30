b1176f8224b0e0dfa488a75d0d57de20ad6d088b
Chris Poucet <poucet@google.com>
2023-08-30 15:50:02 +0200

    Make sure to only save detailed accounts
    Otherwise this results in an infinite-loop through updated and fetching.
    Google-Bug-Id: b/298146080
    Release-Notes: skip
    
    Change-Id: I7628fffa550e3e1414394a760a2c8c51faf87db4

--- polygerrit-ui/app/elements/shared/gr-avatar/gr-avatar-stack_test.ts
+++ polygerrit-ui/app/elements/shared/gr-avatar/gr-avatar-stack_test.ts
@@ -12,7 +12,7 @@ import {
 import {fixture, html, assert} from '@open-wc/testing';
 import {stubRestApi} from '../../../test/test-utils';
 import {LitElement} from 'lit';
-import {Timestamp} from '../../../api/rest-api';
+import {AccountId, Timestamp} from '../../../api/rest-api';
 
 suite('gr-avatar tests', () => {
   suite('config with avatars', () => {
@@ -165,6 +165,7 @@ suite('gr-avatar tests', () => {
         ></gr-avatar-stack>`
       );
       await element.updateComplete;
+      await element.updateComplete;
 
       assert.equal(stub.called, true);
       assert.shadowDom.equal(
@@ -179,6 +180,29 @@ suite('gr-avatar tests', () => {
       const avatars = element.shadowRoot!.querySelectorAll('gr-avatar');
       assert.strictEqual(avatars.length, 1);
       assert.strictEqual(window.getComputedStyle(avatars[0]).marginLeft, '0px');
+    });
+
+
+    test('fetches account details. does not infinite loop', async () => {
+      const stub = stubRestApi('getAccountDetails').resolves({
+        _account_id: 1 as AccountId,
+        registered_on: '1234' as Timestamp,
+      });
+      const element: LitElement = await fixture(
+        html`<gr-avatar-stack
+          .accounts=${[{_account_id: 1}]}
+          .forceFetch=${true}
+          .imageSize=${32}
+        ></gr-avatar-stack>`
+      );
+      await element.updateComplete;
+
+      assert.equal(stub.called, true);
+      assert.shadowDom.equal(
+        element,
+        /* HTML */ `<slot name="fallback"></slot>`
+      );
+
     });
 
     test('renders many accounts fallback', async () => {

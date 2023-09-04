2538ecd8237690064e4bd26daefef3e043d01d06
Dhruv Srivastava <dhruvsri@google.com>
2023-09-04 17:18:45 +0200

    Add API to suggest reviewers for projects
    Change-Id: If74dc2084dd8456902cb73be928441c56519da91

---
+++ javatests/com/google/gerrit/acceptance/rest/project/SuggestBranchReviewersIT.java
@@ -0,0 +1,99 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.rest.project;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.TestAccount;
+import com.google.gerrit.acceptance.testsuite.group.GroupOperations;
+import com.google.gerrit.entities.AccountGroup;
+import com.google.gerrit.entities.BranchNameKey;
+import com.google.gerrit.extensions.api.projects.BranchInput;
+import com.google.gerrit.extensions.common.SuggestedReviewerInfo;
+import com.google.inject.Inject;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SuggestBranchReviewersIT extends AbstractDaemonTest {
+
+  @Inject private GroupOperations groupOperations;
+
+  private AccountGroup.UUID group1;
+  private AccountGroup.UUID group2;
+  private AccountGroup.UUID group3;
+
+  private TestAccount user1;
+  private TestAccount user2;
+  private TestAccount user3;
+  private TestAccount user4;
+
+  private TestAccount user(String name, String fullName, String emailName) throws Exception {
+    return accountCreator.create(name(name), name(emailName) + "@example.com", fullName, null);
+  }
+
+  private TestAccount user(String name, String fullName) throws Exception {
+    return user(name, fullName, name);
+  }
+
+  private BranchNameKey testBranch;
+
+  @Before
+  public void setUp() throws Exception {
+    user1 = user("user1", "First1 Last1");
+    user2 = user("user2", "First2 Last2");
+    user3 = user("user3", "First3 Last3");
+    user4 = user("jdoe", "John Doe", "JDOE");
+
+    group1 =
+        groupOperations.newGroup().name(name("users1")).members(user1.id(), user3.id()).create();
+    group2 =
+        groupOperations.newGroup().name(name("users2")).members(user2.id(), user3.id()).create();
+    group3 = groupOperations.newGroup().name(name("users3")).members(user1.id()).create();
+
+    testBranch = BranchNameKey.create(project, "test");
+  }
+
+  private List<SuggestedReviewerInfo> suggestReviewers(String query) throws Exception {
+    return gApi.projects()
+        .name(project.get())
+        .branch("refs/heads/visible")
+        .suggestReviewers(query)
+        .get();
+  }
+
+  private List<SuggestedReviewerInfo> suggestReviewers(String query, int n) throws Exception {
+    return gApi.projects()
+        .name(project.get())
+        .branch("refs/heads/visible")
+        .suggestReviewers(query)
+        .withLimit(n)
+        .get();
+  }
+
+  @Test
+  public void suggestReviewersWithoutLimitOptionSpecified() throws Exception {
+    String query = user3.username();
+    List<SuggestedReviewerInfo> suggestedReviewerInfos =
+        gApi.projects()
+            .name(project.get())
+            .branch("refs/heads/visible")
+            .create(new BranchInput())
+            .suggestReviewers(query)
+            .get();
+    assertThat(suggestedReviewerInfos).hasSize(1);
+  }
+}

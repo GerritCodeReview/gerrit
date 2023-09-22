bfe835e1a9a084d05b767cbbb04c4fe900f08d5e
Ben Rohlfs <brohlfs@google.com>
2023-09-22 13:36:37 +0200

    Further refine the revision parents UI
    <gr-revision-parents> is a new (flag protected) component that shows
    additional information about the parents (aka bases) when comparing
    two patchsets. The component is shown above the file list and below
    the patchset picker on the change page. A small amount of information
    is also added to the patchset picker (<gr-patch-range-select>).
    
    This is the outcome of discussions with Arnab and Youssef in
    go/revision-parents.
    
    This change is still flag protected, so no users are directly
    affected.
    
    I would prefer to have detailed UX discussions once this is live in
    canary.
    
    It is expected that this still needs some testing and polish.
    
    Example screenshot: https://imgur.com/a/vhR40hj
    
    Release-Notes: skip
    Google-Bug-Id: b/289177174
    Change-Id: I2dd65f8bde157235875c938fb13bb2b3555f5a41

--- polygerrit-ui/app/elements/change/gr-revision-parents/gr-revision-parents.ts
+++ polygerrit-ui/app/elements/change/gr-revision-parents/gr-revision-parents.ts
@@ -4,20 +4,33 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import {customElement, state} from 'lit/decorators.js';
-import {css, html, LitElement} from 'lit';
+import {css, html, HTMLTemplateResult, LitElement} from 'lit';
 import {resolve} from '../../../models/dependency';
 import {subscribe} from '../../lit/subscription-controller';
 import {changeModelToken} from '../../../models/change/change-model';
-import {EDIT, RevisionInfo} from '../../../api/rest-api';
+import {
+  CommitId,
+  EDIT,
+  NumericChangeId,
+  ParentInfo,
+  PatchSetNumber,
+  RepoName,
+  RevisionInfo,
+} from '../../../api/rest-api';
 import {fontStyles} from '../../../styles/gr-font-styles';
-import {branchName, shorten} from '../../../utils/patch-set-util';
+import {branchName} from '../../../utils/patch-set-util';
 import {when} from 'lit/directives/when.js';
+import {createChangeUrl} from '../../../models/views/change';
 
 @customElement('gr-revision-parents')
 export class GrRevisionParents extends LitElement {
+  @state() repo?: RepoName;
+
   @state() revision?: RevisionInfo;
 
   @state() baseRevision?: RevisionInfo;
+
+  @state() showDetails = false;
 
   private readonly getChangeModel = resolve(this, changeModelToken);
 
@@ -30,6 +43,11 @@ export class GrRevisionParents extends LitElement {
         if (x?._number === EDIT) x = undefined;
         this.revision = x as RevisionInfo | undefined;
       }
+    );
+    subscribe(
+      this,
+      () => this.getChangeModel().repo$,
+      x => (this.repo = x)
     );
     subscribe(
       this,
@@ -63,50 +81,300 @@ export class GrRevisionParents extends LitElement {
         .title {
           font-weight: var(--font-weight-bold);
         }
+        .messageContainer {
+          display: flex;
+          padding: var(--spacing-m) var(--spacing-l);
+          border-top: 1px solid var(--border-color);
+        }
+        .messageContainer.info {
+          background-color: var(--info-background);
+        }
+        .messageContainer.warning {
+          background-color: var(--warning-background);
+        }
+        .messageContainer gr-icon {
+          margin-right: var(--spacing-m);
+        }
+        .messageContainer.info gr-icon {
+          color: var(--info-foreground);
+        }
+        .messageContainer.warning gr-icon {
+          color: var(--warning-foreground);
+        }
+        .messageContainer .text {
+          max-width: 600px;
+        }
+        .messageContainer .text p {
+          margin: 0;
+        }
+        .messageContainer .text gr-button {
+          margin-left: -4px;
+        }
+        gr-commit-info {
+          display: inline-block;
+        }
       `,
     ];
   }
 
   override render() {
-    // TODO(revision-parents): Figure out what to do about multiple parents.
-    const baseParent = this.baseRevision?.parents_data?.[0];
-    const parent = this.revision?.parents_data?.[0];
-    if (!parent || !baseParent) return;
-    // TODO(revision-parents): Design something nicer for the various cases.
+    return html`${this.renderMessage()}${this.renderDetails()}`;
+  }
+
+  private renderMessage() {
+    // For merges we are only interested in the target branch parent, which is [0].
+    const parentLeft = this.baseRevision?.parents_data?.[0];
+    const parentRight = this.revision?.parents_data?.[0];
+    if (!parentLeft || !parentRight) return;
+
+    const psLeft = this.baseRevision?._number;
+    const psRight = this.revision?._number;
+    const parentCommitLeft = parentLeft.commit_id;
+    const parentCommitRight = parentRight.commit_id;
+    const branchLeft = branchName(parentLeft.branch_name);
+    const branchRight = branchName(parentRight.branch_name);
+    const isMergedLeft = parentLeft.is_merged_in_target_branch;
+    const isMergedRight = parentRight.is_merged_in_target_branch;
+    const changeNumLeft = parentLeft.change_number;
+    const changeNumRight = parentRight.change_number;
+    const changePsLeft = parentLeft.patch_set_number;
+    const changePsRight = parentRight.patch_set_number;
+
+    if (parentCommitLeft === parentCommitRight) return;
+
+    // Subsequently: different commit
+
+    if (branchLeft !== branchRight) {
+      return html`
+        ${this.renderWarning(
+          'warning',
+          html`
+            Patchset ${psLeft} and ${psRight} are targeting different branches.
+          `
+        )}
+      `;
+    }
+
+    // Subsequently: different commit, same target branch
+
+    // Such a situation is really rare and weird. You have to do something like committing to one
+    // branch and then uploading to another. This warning should actually also be shown also, if
+    // you are not comparing PS X and PS Y, because it is generally a weird patchset state.
+    const isWeirdLeft = !isMergedLeft && !changeNumLeft;
+    const isWeirdRight = !isMergedRight && !changeNumRight;
+    if (isWeirdLeft || isWeirdRight) {
+      const weirdPs =
+        isWeirdLeft && isWeirdRight
+          ? `${psLeft} and ${psRight} are`
+          : isWeirdLeft
+          ? `${psLeft} is`
+          : `${psRight} is`;
+      return html`
+        ${this.renderWarning(
+          'warning',
+          html`
+            Patchset ${weirdPs} based on a commit that neither exists in its
+            target branch, nor is it a commit of another active change.
+          `
+        )}
+      `;
+    }
+
+    if (
+      changeNumLeft &&
+      changeNumRight &&
+      changeNumLeft === changeNumRight &&
+      // This check is probably redundant, because "same change and ps" should mean "same commit".
+      psLeft !== psRight
+    ) {
+      return html`
+        ${this.renderWarning(
+          'info',
+          html`
+            The change was rebased from patchset
+            ${this.renderPatchsetLink(changeNumLeft, changePsLeft)} onto
+            patchset ${this.renderPatchsetLink(changeNumLeft, changePsRight)} of
+            change ${this.renderChangeLink(changeNumLeft)}
+            ${when(isMergedRight, () => html` (MERGED)`)}.
+          `
+        )}
+      `;
+    }
+
+    // No additional info? Then "different commit" and "same branch" means "standard rebase".
+    if (isMergedLeft && isMergedRight) {
+      return html`
+        ${this.renderWarning(
+          'info',
+          html`
+            The change was rebased from
+            ${this.renderCommitLink(parentCommitLeft, false)} onto
+            ${this.renderCommitLink(parentCommitRight, false)}.
+          `
+        )}
+      `;
+    }
+
+    // By now we know that we have different commit, same target branch, no weird parent,
+    // and not a standard rebase. So let's spell out what the left and right side are based on.
+    return this.renderWarning(
+      'warning',
+      html`${this.renderInfo(parentLeft)}<br />${this.renderInfo(parentRight)}`
+    );
+  }
+
+  private renderInfo(parent: ParentInfo) {
+    const ps = this.baseRevision?._number;
+    const isMerged = parent.is_merged_in_target_branch;
+    const changeNum = parent.change_number;
+
+    if (changeNum && !isMerged) {
+      return html`
+        Patchset ${ps} is based on patchset
+        ${this.renderPatchsetLink(changeNum, parent.patch_set_number)} of change
+        ${this.renderChangeLink(changeNum)}.
+      `;
+    } else {
+      return html`
+        Patchset ${ps} is based on commit
+        ${this.renderCommitLink(parent.commit_id, false)} in the target branch
+        (${branchName(parent.branch_name)}).
+      `;
+    }
+  }
+
+  private renderWarning(icon: string, message: HTMLTemplateResult) {
+    const isWarning = icon === 'warning';
+    return html`
+      <div class="messageContainer ${icon}">
+        <div class="icon">
+          <gr-icon icon=${icon}></gr-icon>
+        </div>
+        <div class="text">
+          <p>
+            ${message}${when(
+              isWarning,
+              () => html`
+                <br />
+                The diff below may not be meaningful and may even be hiding
+                relevant changes.
+              `
+            )}
+          </p>
+          ${when(
+            isWarning,
+            () => html`
+              <p>
+                <gr-button
+                  link
+                  @click=${() => (this.showDetails = !this.showDetails)}
+                  >${this.showDetails ? 'Hide' : 'Show'} details</gr-button
+                >
+              </p>
+            `
+          )}
+        </div>
+      </div>
+    `;
+  }
+
+  private renderDetails() {
+    if (!this.showDetails) return;
+    if (!this.baseRevision || !this.revision) return;
+    const parentLeft = this.baseRevision.parents_data?.[0];
+    const parentRight = this.revision.parents_data?.[0];
+    if (!parentRight || !parentLeft) return;
+
     return html`
       <div class="container">
         <div class="flex">
-          <div class="section">
-            <h4 class="heading-4">Patchset ${this.baseRevision?._number}</h4>
-            <div>Branch: ${branchName(parent.branch_name)}</div>
-            <div>Commit: ${shorten(parent.commit_id)}</div>
-            <div>Is Merged: ${parent.is_merged_in_target_branch}</div>
-            ${when(
-              !!parent.change_number,
-              () => html` <div>
-                  Change ID: ${parent.change_id?.substring(0, 10)}
-                </div>
-                <div>Change Number: ${parent.change_number}</div>
-                <div>Patchset Number: ${parent.patch_set_number}</div>
-                <div>Change Status: ${parent.change_status}</div>`
-            )}
-          </div>
-          <div class="section">
-            <h4 class="heading-4">Patchset ${this.revision?._number}</h4>
-            <div>Branch: ${branchName(baseParent.branch_name)}</div>
-            <div>Commit: ${shorten(baseParent.commit_id)}</div>
-            <div>Is Merged: ${baseParent.is_merged_in_target_branch}</div>
-            ${when(
-              !!baseParent.change_number,
-              () => html`<div>
-                  Change ID: ${baseParent.change_id?.substring(0, 10)}
-                </div>
-                <div>Change Number: ${baseParent.change_number}</div>
-                <div>Patchset Number: ${baseParent.patch_set_number}</div>
-                <div>Change Status: ${baseParent.change_status}</div>`
-            )}
-          </div>
+          ${this.renderSection(
+            this.baseRevision,
+            parentLeft,
+            parentLeft.change_number === parentRight.change_number
+          )}
+          ${this.renderSection(
+            this.revision,
+            parentRight,
+            parentLeft.change_number === parentRight.change_number
+          )}
         </div>
+      </div>
+    `;
+  }
+
+  private renderCommitLink(commit?: CommitId, showCopyButton = true) {
+    if (!commit) return;
+    return html`<gr-commit-info
+      .commitInfo=${{commit}}
+      .showCopyButton=${showCopyButton}
+    ></gr-commit-info>`;
+  }
+
+  private renderChangeLink(changeNum: NumericChangeId) {
+    return html`
+      <a href=${createChangeUrl({changeNum, repo: this.repo!})}>${changeNum}</a>
+    `;
+  }
+
+  private renderPatchsetLink(
+    changeNum: NumericChangeId,
+    patchNum?: PatchSetNumber
+  ) {
+    if (!patchNum) return;
+    return html`
+      <a
+        href=${createChangeUrl({
+          changeNum,
+          repo: this.repo!,
+          patchNum,
+        })}
+        >${patchNum}</a
+      >
+    `;
+  }
+
+  private renderSection(
+    revision: RevisionInfo,
+    parent: ParentInfo,
+    sameChange: boolean
+  ) {
+    const ps = revision._number;
+    const commit = parent.commit_id;
+    const branch = branchName(parent.branch_name);
+    const isMerged = parent.is_merged_in_target_branch;
+    const changeNum = parent.change_number as NumericChangeId;
+    const changePs = parent.patch_set_number;
+
+    createChangeUrl({changeNum, repo: this.repo!});
+
+    return html`
+      <div class="section">
+        <h4 class="heading-4">Patchset ${ps}</h4>
+        <div>Target branch: ${branch}</div>
+        <div>Base commit: ${this.renderCommitLink(commit)}</div>
+        ${when(
+          !changeNum && !isMerged,
+          () => html`
+            <div>
+              <gr-icon icon="warning"></gr-icon>
+              <span
+                >Warning: The base commit is not known (aka reachable) in the
+                target branch.</span
+              >
+            </div>
+          `
+        )}
+        ${when(
+          changeNum && (sameChange || !isMerged),
+          () => html`
+            <div>
+              Base change: ${this.renderChangeLink(changeNum)}, patchset
+              ${this.renderPatchsetLink(changeNum, changePs)}
+              ${when(isMerged, () => html`(MERGED)`)}
+            </div>
+          `
+        )}
       </div>
     `;
   }

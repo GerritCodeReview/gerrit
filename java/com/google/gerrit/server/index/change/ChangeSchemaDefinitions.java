695649304da91fc7151fc46a78bd900db4a00e1d
David Ostrovsky <david@ostrovsky.org>
2023-09-27 09:46:47 +0200

    Remove old change index schema versions and clean up the code
    Removing outdated change index schema versions allows us to also remove
    not used fields and the related code. From the upgrade gerrit perspective
    it's enough to preserve the current and the previous schema version, used
    in the previous stable release.
    
    Draft comments and starred change related fields have been removed from
    the index. So that the code is not needed any more.
    
    Release-Notes: skip
    Change-Id: I53c0c5cda25d836190085e9bbc2bc2dcf5627df2

--- java/com/google/gerrit/server/index/change/ChangeSchemaDefinitions.java
+++ java/com/google/gerrit/server/index/change/ChangeSchemaDefinitions.java
@@ -29,15 +29,13 @@ import com.google.gerrit.server.query.change.ChangeData;
  * com.google.gerrit.index.IndexUpgradeValidator}.
  */
 public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
-  /** Added new field {@link ChangeField#IS_SUBMITTABLE_SPEC} based on submit requirements. */
   @Deprecated
-  static final Schema<ChangeData> V74 =
+  static final Schema<ChangeData> V84 =
       schema(
-          /* version= */ 74,
+          /* version= */ 84,
           ImmutableList.<IndexedField<ChangeData, ?>>of(
               ChangeField.ADDED_LINES_FIELD,
               ChangeField.APPROVAL_FIELD,
-              ChangeField.ASSIGNEE_FIELD,
               ChangeField.ATTENTION_SET_FULL_FIELD,
               ChangeField.ATTENTION_SET_USERS_COUNT_FIELD,
               ChangeField.ATTENTION_SET_USERS_FIELD,
@@ -52,15 +50,17 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
               ChangeField.COMMITTER_PARTS_FIELD,
               ChangeField.COMMIT_FIELD,
               ChangeField.COMMIT_MESSAGE_FIELD,
+              ChangeField.COMMIT_MESSAGE_EXACT_FIELD,
+              ChangeField.CUSTOM_KEYED_VALUES_FIELD,
               ChangeField.DELETED_LINES_FIELD,
               ChangeField.DELTA_LINES_FIELD,
               ChangeField.DIRECTORY_FIELD,
-              ChangeField.DRAFTBY_FIELD,
               ChangeField.EDITBY_FIELD,
               ChangeField.EXACT_AUTHOR_FIELD,
               ChangeField.EXACT_COMMITTER_FIELD,
               ChangeField.EXTENSION_FIELD,
               ChangeField.FILE_PART_FIELD,
+              ChangeField.FOOTER_NAME_FIELD,
               ChangeField.FOOTER_FIELD,
               ChangeField.GROUP_FIELD,
               ChangeField.HASHTAG_CASE_AWARE_FIELD,
@@ -87,13 +87,12 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
               ChangeField.REVIEWEDBY_FIELD,
               ChangeField.REVIEWER_BY_EMAIL_FIELD,
               ChangeField.REVIEWER_FIELD,
-              ChangeField.STARBY_FIELD,
               ChangeField.STARTED_FIELD,
-              ChangeField.STAR_FIELD,
               ChangeField.STATUS_FIELD,
               ChangeField.STORED_SUBMIT_RECORD_LENIENT_FIELD,
               ChangeField.STORED_SUBMIT_RECORD_STRICT_FIELD,
               ChangeField.STORED_SUBMIT_REQUIREMENTS_FIELD,
+              ChangeField.SUBJECT_FIELD,
               ChangeField.SUBMISSIONID_FIELD,
               ChangeField.SUBMIT_RECORD_FIELD,
               ChangeField.SUBMIT_RULE_RESULT_FIELD,
@@ -107,7 +106,6 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
           ImmutableList.<IndexedField<ChangeData, ?>.SearchSpec>of(
               ChangeField.ADDED_LINES_SPEC,
               ChangeField.APPROVAL_SPEC,
-              ChangeField.ASSIGNEE_SPEC,
               ChangeField.ATTENTION_SET_FULL_SPEC,
               ChangeField.ATTENTION_SET_USERS,
               ChangeField.ATTENTION_SET_USERS_COUNT,
@@ -121,11 +119,12 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
               ChangeField.COMMENT_SPEC,
               ChangeField.COMMITTER_PARTS_SPEC,
               ChangeField.COMMIT_MESSAGE,
+              ChangeField.COMMIT_MESSAGE_EXACT,
               ChangeField.COMMIT_SPEC,
+              ChangeField.CUSTOM_KEYED_VALUES_SPEC,
               ChangeField.DELETED_LINES_SPEC,
               ChangeField.DELTA_LINES_SPEC,
               ChangeField.DIRECTORY_SPEC,
-              ChangeField.DRAFTBY_SPEC,
               ChangeField.EDITBY_SPEC,
               ChangeField.EXACT_AUTHOR_SPEC,
               ChangeField.EXACT_COMMITTER_SPEC,
@@ -133,6 +132,7 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
               ChangeField.EXACT_TOPIC,
               ChangeField.EXTENSION_SPEC,
               ChangeField.FILE_PART_SPEC,
+              ChangeField.FOOTER_NAME,
               ChangeField.FOOTER_SPEC,
               ChangeField.FUZZY_HASHTAG,
               ChangeField.FUZZY_TOPIC,
@@ -152,6 +152,9 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
               ChangeField.PATH_SPEC,
               ChangeField.PENDING_REVIEWER_BY_EMAIL,
               ChangeField.PENDING_REVIEWER_SPEC,
+              ChangeField.PREFIX_HASHTAG,
+              ChangeField.PREFIX_TOPIC,
+              ChangeField.PREFIX_SUBJECT_SPEC,
               ChangeField.PRIVATE_SPEC,
               ChangeField.PROJECTS_SPEC,
               ChangeField.PROJECT_SPEC,
@@ -162,13 +165,12 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
               ChangeField.REVIEWEDBY_SPEC,
               ChangeField.REVIEWER_BY_EMAIL,
               ChangeField.REVIEWER_SPEC,
-              ChangeField.STARBY_SPEC,
               ChangeField.STARTED_SPEC,
-              ChangeField.STAR_SPEC,
               ChangeField.STATUS_SPEC,
               ChangeField.STORED_SUBMIT_RECORD_LENIENT_SPEC,
               ChangeField.STORED_SUBMIT_RECORD_STRICT_SPEC,
               ChangeField.STORED_SUBMIT_REQUIREMENTS_SPEC,
+              ChangeField.SUBJECT_SPEC,
               ChangeField.SUBMISSIONID_SPEC,
               ChangeField.SUBMIT_RECORD_SPEC,
               ChangeField.SUBMIT_RULE_RESULT_SPEC,
@@ -179,87 +181,7 @@ public class ChangeSchemaDefinitions extends SchemaDefinitions<ChangeData> {
               ChangeField.UPLOADER_SPEC,
               ChangeField.WIP_SPEC));
 
-  /**
-   * Added new field {@link ChangeField#PREFIX_HASHTAG} and {@link ChangeField#PREFIX_TOPIC} to
-   * allow easier search for topics.
-   */
-  @Deprecated
-  static final Schema<ChangeData> V75 =
-      new Schema.Builder<ChangeData>()
-          .add(V74)
-          .addSearchSpecs(ChangeField.PREFIX_HASHTAG)
-          .addSearchSpecs(ChangeField.PREFIX_TOPIC)
-          .build();
-
-  /** Added new field {@link ChangeField#FOOTER_NAME}. */
-  @Deprecated
-  static final Schema<ChangeData> V76 =
-      new Schema.Builder<ChangeData>()
-          .add(V75)
-          .addIndexedFields(ChangeField.FOOTER_NAME_FIELD)
-          .addSearchSpecs(ChangeField.FOOTER_NAME)
-          .build();
-
-  /** Added new field {@link ChangeField#COMMIT_MESSAGE_EXACT}. */
-  @Deprecated
-  static final Schema<ChangeData> V77 =
-      new Schema.Builder<ChangeData>()
-          .add(V76)
-          .addIndexedFields(ChangeField.COMMIT_MESSAGE_EXACT_FIELD)
-          .addSearchSpecs(ChangeField.COMMIT_MESSAGE_EXACT)
-          .build();
-
-  // Upgrade Lucene to 7.x requires reindexing.
-  @Deprecated static final Schema<ChangeData> V78 = schema(V77);
-
-  /** Remove draft and star fields. */
-  @Deprecated
-  static final Schema<ChangeData> V79 =
-      new Schema.Builder<ChangeData>()
-          .add(V78)
-          .remove(ChangeField.STAR_SPEC, ChangeField.STARBY_SPEC, ChangeField.DRAFTBY_SPEC)
-          .remove(ChangeField.STAR_FIELD, ChangeField.STARBY_FIELD, ChangeField.DRAFTBY_FIELD)
-          .build();
-
-  /** Add subject field. */
-  @Deprecated
-  static final Schema<ChangeData> V80 =
-      new Schema.Builder<ChangeData>()
-          .add(V79)
-          .addIndexedFields(ChangeField.SUBJECT_FIELD)
-          .addSearchSpecs(ChangeField.SUBJECT_SPEC)
-          .build();
-
-  /** Add prefixsubject field. */
-  @Deprecated
-  static final Schema<ChangeData> V81 =
-      new Schema.Builder<ChangeData>()
-          .add(V80)
-          .addSearchSpecs(ChangeField.PREFIX_SUBJECT_SPEC)
-          .build();
-
-  /** Remove assignee field. */
-  @Deprecated
-  static final Schema<ChangeData> V82 =
-      new Schema.Builder<ChangeData>()
-          .add(V81)
-          .remove(ChangeField.ASSIGNEE_SPEC)
-          .remove(ChangeField.ASSIGNEE_FIELD)
-          .build();
-
-  /** Upgrade Lucene to 8.x requires reindexing. */
-  @Deprecated static final Schema<ChangeData> V83 = schema(V82);
-
-  @Deprecated
-  static final Schema<ChangeData> V84 =
-      new Schema.Builder<ChangeData>()
-          .add(V83)
-          .addIndexedFields(ChangeField.CUSTOM_KEYED_VALUES_FIELD)
-          .addSearchSpecs(ChangeField.CUSTOM_KEYED_VALUES_SPEC)
-          .build();
-
   /** Upgrade Lucene to 9.x requires reindexing. */
-  @SuppressWarnings("deprecation")
   static final Schema<ChangeData> V85 = schema(V84);
 
   /**

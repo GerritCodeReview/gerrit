9e43de1a1bbb3465fce654cd055956fe76e9e1fc
Dhruv Srivastava <dhruvsri@google.com>
2023-09-27 16:52:44 +0000

    new changes
    test amend
    
    Change-Id: Ia1579ffd2ebcaaffb8c5990c01680ff656099313

--- polygerrit-ui/app/constants/messages.ts
+++ polygerrit-ui/app/constants/messages.ts
@@ -1,9 +1,9 @@
+<<<<<<< PATCH SET (5a4000 test change)
+asdas
+=======
 /**
- * @license
- * Copyright 2020 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
 
 /** Message shown when no threads in gr-thread-list for robot comments */
 export const NO_ROBOT_COMMENTS_THREADS_MSG =
   'There are no findings for this patchset.';
+>>>>>>> BASE      (86b1dc Merge "Support named destinations on group refs")

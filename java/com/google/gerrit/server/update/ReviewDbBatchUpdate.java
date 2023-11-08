b7ee75519658daa2fb7604fd7602986879ce76c2
Antonio Barone <syntonyze@gmail.com>
2023-11-08 14:32:16 +0100

    Honour 'notedb.onlineMigrationProjects' for new changes
    Change I31bcded11 introduced the concept of limiting the NoteDB
    migration only to the configured projects.
    
    Whilst this was honouring in building NoteDB state for the _existing_
    changes, it was not honouring the setting for _new_ changes.
    
    Before this fix, if a new change was created, no matter for which
    project, it was going to be stored also on NoteDB.
    
    Introduce a new singleton object with the aim of checking whether the
    online project migration is enabled, speficially for a provided project,
    so that the creation of new NoteDB meta refs honours the
    `notedb.onlineMigrationProjects` config.
    
    Release-Notes: Honour 'notedb.onlineMigrationProjects' for new change
    Forward-Compatible: checked
    Bug: Issue 299503090
    Change-Id: I5225d411c1562a14fa1ce590221f96a358eb2391

--- java/com/google/gerrit/server/update/ReviewDbBatchUpdate.java
+++ java/com/google/gerrit/server/update/ReviewDbBatchUpdate.java
@@ -58,6 +58,7 @@ import com.google.gerrit.server.notedb.NoteDbChangeState.PrimaryStorage;
 import com.google.gerrit.server.notedb.NoteDbUpdateManager;
 import com.google.gerrit.server.notedb.NoteDbUpdateManager.MismatchedStateException;
 import com.google.gerrit.server.notedb.NotesMigration;
+import com.google.gerrit.server.notedb.OnlineProjectsMigrationChecker;
 import com.google.gwtorm.server.OrmException;
 import com.google.gwtorm.server.SchemaFactory;
 import com.google.inject.Inject;
@@ -308,6 +309,7 @@ public class ReviewDbBatchUpdate extends BatchUpdate {
   private final Metrics metrics;
   private final NoteDbUpdateManager.Factory updateManagerFactory;
   private final NotesMigration notesMigration;
+  private final OnlineProjectsMigrationChecker onlineProjectsMigrationChecker;
   private final ReviewDb db;
   private final SchemaFactory<ReviewDb> schemaFactory;
   private final long skewMs;
@@ -329,7 +331,7 @@ public class ReviewDbBatchUpdate extends BatchUpdate {
       GitRepositoryManager repoManager,
       Metrics metrics,
       NoteDbUpdateManager.Factory updateManagerFactory,
-      NotesMigration notesMigration,
+      OnlineProjectsMigrationChecker onlineProjectsMigrationChecker,
       SchemaFactory<ReviewDb> schemaFactory,
       @Assisted ReviewDb db,
       @Assisted Project.NameKey project,
@@ -343,11 +345,12 @@ public class ReviewDbBatchUpdate extends BatchUpdate {
     this.gitRefUpdated = gitRefUpdated;
     this.indexer = indexer;
     this.metrics = metrics;
-    this.notesMigration = notesMigration;
+    this.notesMigration = onlineProjectsMigrationChecker.getNotesMigration();
     this.schemaFactory = schemaFactory;
     this.updateManagerFactory = updateManagerFactory;
     this.db = db;
     skewMs = NoteDbChangeState.getReadOnlySkew(cfg);
+    this.onlineProjectsMigrationChecker = onlineProjectsMigrationChecker;
   }
 
   @Override
@@ -444,7 +447,8 @@ public class ReviewDbBatchUpdate extends BatchUpdate {
 
       tasks = new ArrayList<>(ops.keySet().size());
       try {
-        if (notesMigration.commitChangeWrites() && repoView != null) {
+        if (onlineProjectsMigrationChecker.commitChangeWritesForProject(project)
+            && repoView != null) {
           // A NoteDb change may have been rebuilt since the repo was originally
           // opened, so make sure we see that.
           logDebug("Preemptively scanning for repo changes");
@@ -472,7 +476,7 @@ public class ReviewDbBatchUpdate extends BatchUpdate {
         }
         Futures.allAsList(futures).get();
 
-        if (notesMigration.commitChangeWrites()) {
+        if (onlineProjectsMigrationChecker.commitChangeWritesForProject(project)) {
           if (!dryrun) {
             executeNoteDbUpdates(tasks);
           }
@@ -680,7 +684,7 @@ public class ReviewDbBatchUpdate extends BatchUpdate {
           }
 
           // Stage the NoteDb update and store its state in the Change.
-          if (notesMigration.commitChangeWrites()) {
+          if (onlineProjectsMigrationChecker.commitChangeWritesForProject(project)) {
             updateManager = stageNoteDbUpdate(ctx, deleted);
           }
 
@@ -720,7 +724,7 @@ public class ReviewDbBatchUpdate extends BatchUpdate {
           // Should have failed above if NoteDb is disabled.
           checkState(notesMigration.commitChangeWrites());
           noteDbResult = updateManager.stage().get(id);
-        } else if (notesMigration.commitChangeWrites()) {
+        } else if (onlineProjectsMigrationChecker.commitChangeWritesForProject(project)) {
           try {
             noteDbResult = updateManager.stage().get(id);
           } catch (IOException ex) {

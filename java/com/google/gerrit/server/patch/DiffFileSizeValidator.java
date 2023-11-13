b9cfec8cae8bf0eb66dd95829b3c01001fba8715
Youssef Elghareeb <ghareeb@google.com>
2023-11-13 14:16:40 +0100

    Skip file size validation for binary files
    In change I4b280bb8 we relanded the change to validate file sizes on
    file diff requests. The previous behaviour for binary files prior to
    I4b280bb8 used to show a message of "Difference in binary files" in the
    web UI (see [1]). After adding the the file size validation, it resulted
    in displaying a bad request message in the UI.
    
    In this change, we skip validation for binary files since it's handled
    already by the front-end.
    
    [1] https://gerrit.googlesource.com/gerrit/+/e261a84fb259c7e87c933155677d2311fe0e33d2/polygerrit-ui/app/embed/diff/gr-diff/gr-diff-element.ts#365
    
    Google-Bug-Id: b/288895561
    Change-Id: Iefc4adf3f09032043e24ecc12ecb3560d38492c0

--- java/com/google/gerrit/server/patch/DiffFileSizeValidator.java
+++ java/com/google/gerrit/server/patch/DiffFileSizeValidator.java
@@ -15,6 +15,7 @@
 package com.google.gerrit.server.patch;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.gerrit.entities.Patch.PatchType;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.git.LargeObjectException;
 import com.google.gerrit.server.patch.filediff.FileDiffOutput;
@@ -43,8 +44,12 @@ public class DiffFileSizeValidator implements DiffValidator {
 
   @Override
   public void validate(FileDiffOutput fileDiff) throws LargeObjectException {
-    if (maxFileSize <= 0) {
-      // Do not apply limits if the config is not set.
+    if (maxFileSize <= 0
+        || fileDiff.patchType().isPresent()
+            && fileDiff.patchType().get().equals(PatchType.BINARY)) {
+      // Do not check file size for binary files. The front-end omits the diff for binary files
+      // and displays a "Difference in binary files" message.
+      // Also do not apply limits if the config is not set.
       return;
     }
     if (fileDiff.size() > maxFileSize) {

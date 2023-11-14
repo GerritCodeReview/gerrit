7c2fd659532f136d68522a8df7df1152134062c5
Edwin Kempin <ekempin@google.com>
2023-11-14 07:48:22 +0000

    ChangeJson: Drop logging for when currentRevision is unexpectedly not set
    These logs were added to investigate a bug where currentRevision in
    ChangeInfo was unexpectedly not set. This bug has been fixed by change
    Ic99c96fe9 and hence these logs are no longer needed.
    
    Release-Notes: skip
    Bug: Google b/304369791
    Change-Id: I0903a6869a9eb463e2a3e126858843f8f0a692f3
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/change/ChangeJson.java
+++ java/com/google/gerrit/server/change/ChangeJson.java
@@ -106,7 +106,6 @@ import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.config.TrackingFooters;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.index.change.ChangeField;
-import com.google.gerrit.server.logging.CallerFinder;
 import com.google.gerrit.server.notedb.ChangeNotes;
 import com.google.gerrit.server.notedb.ReviewerStateInternal;
 import com.google.gerrit.server.patch.PatchListNotAvailableException;
@@ -766,26 +765,6 @@ public class ChangeJson {
         if (entry.getValue().isCurrent) {
           out.currentRevision = entry.getKey();
           break;
-        }
-      }
-      if (limitToPsId.isEmpty() && out.currentRevision == null) {
-        logger.atSevere().log(
-            "current revision for change %s not found"
-                + " (current patch set ID = %s, patch sets = %s, meta revision = %s,"
-                + " options = %s, caller = %s)",
-            cd.getId(),
-            cd.change().currentPatchSetId(),
-            src.entrySet().stream()
-                .collect(toImmutableMap(Map.Entry::getKey, e -> e.getValue().commitId().name())),
-            getMetaRevisionIfAvailable(cd).map(ObjectId::name).orElse("n/a"),
-            options,
-            CallerFinder.builder().addTarget(ChangeJson.class).build().findCallerLazy());
-        PatchSet.Id currentPatchSetFromChangeData =
-            Optional.ofNullable(cd.currentPatchSet()).map(PatchSet::id).orElse(null);
-        if (!cd.change().currentPatchSetId().equals(currentPatchSetFromChangeData)) {
-          logger.atSevere().log(
-              "mismatch between current patch set in Change (%s) and ChangeData (%s)",
-              cd.change().currentPatchSetId(), currentPatchSetFromChangeData);
         }
       }
     }

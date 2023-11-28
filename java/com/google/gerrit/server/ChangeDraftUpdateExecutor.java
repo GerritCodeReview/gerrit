9cb536101d77ea9c47a19605e3b3af4519406741
Luca Milanesio <luca.milanesio@gmail.com>
2023-11-28 10:29:25 +0000

    Revert "Small fixes to ChangeDraftUpdateExecutor"
    This reverts commit cdc358515138396501cef2c243903ab531cb7788.
    
    Change-Id: I870177c1981b084c41ebd681663e98b6bc6e219c
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

--- java/com/google/gerrit/server/ChangeDraftUpdateExecutor.java
+++ java/com/google/gerrit/server/ChangeDraftUpdateExecutor.java
@@ -14,7 +14,7 @@
 
 package com.google.gerrit.server;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
+import static autovalue.shaded.com.google$.common.collect.$ImmutableList.toImmutableList;
 
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.MultimapBuilder;
@@ -45,17 +45,7 @@ import org.eclipse.jgit.transport.PushCertificate;
  * </ol>
  */
 public interface ChangeDraftUpdateExecutor {
-  interface AbstractFactory {
-    // Guice cannot bind either:
-    // - A parameterized entity.
-    // - A factory creating an interface (rather than a class).
-    // To overcome this - we declare the create method in this non-parameterized interface, then
-    // extend it with a factory returning an actual class.
-    ChangeDraftUpdateExecutor create();
-  }
-
-  interface Factory<T extends ChangeDraftUpdateExecutor> extends AbstractFactory {
-    @Override
+  interface AbstractFactory<T extends ChangeDraftUpdateExecutor> {
     T create();
   }
 

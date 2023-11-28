6f2c53ac81a80188b1ff7b2c1eca46a062e95bbf
Luca Milanesio <luca.milanesio@gmail.com>
2023-11-28 10:29:34 +0000

    Revert "Remove the omnipresent `gr-diff` css class"
    This reverts commit ea6db3f8e42f3ad9d5869afdbb061b56383deaa9.
    
    Release-Notes: skip
    Change-Id: Ifc641628fec5eef9c90c744623fabc7f7f4bbc8a
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

--- polygerrit-ui/app/embed/diff/gr-diff/gr-diff-utils.ts
+++ polygerrit-ui/app/embed/diff/gr-diff/gr-diff-utils.ts
@@ -318,11 +318,9 @@ export function isThreadEl(
 /**
  * Simple helper method for creating element classes in the context of
  * gr-diff. This is just a super simple convenience function.
- *
- * TODO: Maybe rename this simple function or inline it everywhere?
  */
 export function diffClasses(...additionalClasses: string[]) {
-  return additionalClasses.join(' ');
+  return ['gr-diff', ...additionalClasses].join(' ');
 }
 
 /**
@@ -335,6 +333,7 @@ export function createElementDiff(
 ): HTMLElement {
   const el = document.createElement(tagName);
 
+  el.classList.add('gr-diff');
   if (classStr) {
     for (const className of classStr.split(' ')) {
       el.classList.add(className);

eefb4600ac6b0c7ef4791b7612adb89171afa48e
Luca Milanesio <luca.milanesio@gmail.com>
2023-11-28 10:29:21 +0000

    Revert "Extract AbstractChangeUpdate::verifyComment method to a utility class"
    This reverts commit 883968744475d8e2401cbb7260807a3aa9cec85d.
    
    Release-Notes: skip
    Change-Id: I6c4c3d44781094e03834bb8f7bc1f349bc65a58b
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

--- java/com/google/gerrit/server/notedb/AbstractChangeUpdate.java
+++ java/com/google/gerrit/server/notedb/AbstractChangeUpdate.java
@@ -24,7 +24,6 @@ import com.google.gerrit.entities.Change;
 import com.google.gerrit.entities.Comment;
 import com.google.gerrit.entities.PatchSet;
 import com.google.gerrit.entities.Project;
-import com.google.gerrit.server.CommentVerifier;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.InternalUser;
@@ -56,7 +55,7 @@ public abstract class AbstractChangeUpdate {
   private ObjectId result;
   boolean rootOnly;
 
-  protected AbstractChangeUpdate(
+  AbstractChangeUpdate(
       ChangeNotes notes,
       CurrentUser user,
       PersonIdent serverIdent,
@@ -66,11 +65,11 @@ public abstract class AbstractChangeUpdate {
     this.serverIdent = new PersonIdent(serverIdent, when);
     this.notes = notes;
     this.change = notes.getChange();
-    this.when = when;
     this.accountId = accountId(user);
     Account.Id realAccountId = accountId(user.getRealUser());
     this.realAccountId = realAccountId != null ? realAccountId : accountId;
     this.authorIdent = ident(noteUtil, serverIdent, user, when);
+    this.when = when;
   }
 
   AbstractChangeUpdate(
@@ -276,7 +275,20 @@ public abstract class AbstractChangeUpdate {
     return ins.insert(Constants.OBJ_TREE, new byte[] {});
   }
 
-  protected void verifyComment(Comment c) {
-    CommentVerifier.verify(c, accountId, realAccountId, authorIdent);
+  void verifyComment(Comment c) {
+    checkArgument(c.getCommitId() != null, "commit ID required for comment: %s", c);
+    checkArgument(
+        c.author.getId().equals(getAccountId()),
+        "The author for the following comment does not match the author of this %s (%s): %s",
+        getClass().getSimpleName(),
+        getAccountId(),
+        c);
+    checkArgument(
+        c.getRealAuthor().getId().equals(realAccountId),
+        "The real author for the following comment does not match the real"
+            + " author of this %s (%s): %s",
+        getClass().getSimpleName(),
+        realAccountId,
+        c);
   }
 }

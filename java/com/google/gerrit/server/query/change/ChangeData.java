e1eb139967df7438dbf42e0e92a36bf2188e3451
Luca Milanesio <luca.milanesio@gmail.com>
2023-11-28 10:29:35 +0000

    Revert "ChangeData: Clear cached patch sets when change notes are loaded"
    This reverts commit 4838938ad81209babe665ab98914c7a74aeb7ff2.
    
    Release-Notes: skip
    Change-Id: I9f55996d77c32de1ae3f90d2eddf3129c082756e
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>

--- java/com/google/gerrit/server/query/change/ChangeData.java
+++ java/com/google/gerrit/server/query/change/ChangeData.java
@@ -748,7 +748,6 @@ public class ChangeData {
       }
       notes = notesFactory.create(project(), legacyId, metaRevision);
       change = notes.getChange();
-      setPatchSets(null);
     }
     return notes;
   }

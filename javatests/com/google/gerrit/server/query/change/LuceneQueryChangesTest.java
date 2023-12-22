07498bf0e445f466ed3cbd02408428c2b0e8455b
Thomas Dräbing <thomas.draebing@sap.com>
2023-12-22 10:28:22 +0100

    Allow to query changes using the `project~changeNumber` format
    So far querying changes using the `changes:` predicate did not
    allow to use the `project~changeNumber` format. However, since
    I32a3131b15948f2ceda32dddcd3c5038d0f89dfd this is the default ID
    format returned in the ChangeInfo. Being able to use it directly for
    further queries would be advantageous.
    
    This change provides the above named functionality. This does not
    require a new index schema.
    
    Release-Notes: Support `change:project~changeNumber` query
    Change-Id: Ifceb7df67900d700605689b68e18da0cc333fe4a

--- javatests/com/google/gerrit/server/query/change/LuceneQueryChangesTest.java
+++ javatests/com/google/gerrit/server/query/change/LuceneQueryChangesTest.java
@@ -61,6 +61,23 @@ public abstract class LuceneQueryChangesTest extends AbstractQueryChangesTest {
   }
 
   @Test
+  public void byChangeId() throws Exception {
+    Project.NameKey project = Project.nameKey("repo");
+    repo = createAndOpenProject(project);
+    RevCommit commit1 = repo.parseBody(repo.commit().message("foo_bar_foo").create());
+    Change change1 = insert(project, newChangeForCommit(repo, commit1));
+
+    assertQuery(String.format("change:%s", change1.getChangeId()), change1);
+    assertQuery(String.format("change:%s", change1.getId()), change1);
+    assertQuery(
+        String.format("change:%s~%s", change1.getProject(), change1.getChangeId()), change1);
+    assertQuery(
+        String.format(
+            "change:%s~%s~%s", change1.getProject(), change1.getDest().branch(), change1.getKey()),
+        change1);
+  }
+
+  @Test
   public void invalidQuery() throws Exception {
     BadRequestException thrown =
         assertThrows(BadRequestException.class, () -> newQuery("\\").get());

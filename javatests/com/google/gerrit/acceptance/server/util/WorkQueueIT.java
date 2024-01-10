c13b9641cf1cee3add02dbc7c6f8347d50874ff4
tianwen lin <lintw0302@gmail.com>
2024-01-10 01:57:50 +0000

    WorkQueue function Test
    To test the functionality issues of the workqueue, simplify the
    Executor and Task classes. Create an Executor thread pool to execute a
    scheduleAtFixedRate task with a period of 1 second.
    
    After waiting for ten seconds in the main thread, considering the
    additional 1 seconds spent in onStop, num should ideally be about 10.
    However, the assertion reveals that num will not larger than 1.
    
    Bug: Issue 317985672
    Change-Id: I01cfc65d7da48fcded7dec0011878c5484be155b
    Signed-off-by: lintianwen <lintw0302@gmail.com>
    Release-Notes: skip

---
+++ javatests/com/google/gerrit/acceptance/server/util/WorkQueueIT.java
@@ -0,0 +1,56 @@
+package com.google.gerrit.acceptance.server.util;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.server.git.WorkQueue;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+public class WorkQueueIT extends AbstractDaemonTest {
+  @Inject private WorkQueue workQueue;
+
+  public static class testListener implements WorkQueue.TaskListener {
+
+    public static class testModule extends AbstractModule {
+      @Override
+      public void configure() {
+        bind(WorkQueue.TaskListener.class)
+            .annotatedWith(Exports.named("listener"))
+            .to(testListener.class);
+      }
+    }
+
+    @Override
+    public void onStart(WorkQueue.Task<?> task) {}
+
+    @Override
+    public void onStop(WorkQueue.Task<?> task) {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  @Test
+  public void testScheduleAtFixedRate() throws ExecutionException, InterruptedException {
+    ScheduledExecutorService testExecutor = workQueue.createQueue(8, "test-Queue");
+    ScheduledFuture<?> unusedFuture =
+        testExecutor.scheduleAtFixedRate(() -> {}, 0, 1, TimeUnit.SECONDS);
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    assertThat(workQueue.getTasks().get(0).getState()).isNotEqualTo(WorkQueue.Task.State.SLEEPING);
+    testExecutor.shutdownNow();
+  }
+}

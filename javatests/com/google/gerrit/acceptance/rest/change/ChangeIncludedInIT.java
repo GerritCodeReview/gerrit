010b972bc4b99eb84a278ac7fbb45b3850740378
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2024-01-14 12:52:56 +0200

    IncludedIn: Support filtering branches/tags in plugins
    Some project have many branches and/or tags. The "Included In" list for
    old changes shows many entries, and it's hard to find the relevant ones
    in the haystack.
    
    For example, some projects have branches per user (user/branch), and it
    is usually not interesting to list these in "Included In".
    
    Some projects use tags extensively, and it can be useful to filter only
    the first tag in each branch.
    
    Implementation in plugins enables the user to customize filreting logic
    to any project.
    
    Bug: Issue 40004507
    Release-Notes: Added support for filtering branches/tags in plugins.
    Change-Id: Ifa0e7ff0e924378d97e9ce8cc399ab9cbffb1974

--- javatests/com/google/gerrit/acceptance/rest/change/ChangeIncludedInIT.java
+++ javatests/com/google/gerrit/acceptance/rest/change/ChangeIncludedInIT.java
@@ -24,15 +24,40 @@ import com.google.gerrit.acceptance.PushOneCommit.Result;
 import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
 import com.google.gerrit.entities.BranchNameKey;
 import com.google.gerrit.entities.Permission;
+import com.google.gerrit.entities.Project;
 import com.google.gerrit.extensions.api.changes.ReviewInput;
 import com.google.gerrit.extensions.api.projects.TagInput;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.change.FilterIncludedIn;
+import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
+import java.util.function.Predicate;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.junit.Test;
 
 @NoHttpd
 public class ChangeIncludedInIT extends AbstractDaemonTest {
 
   @Inject private ProjectOperations projectOperations;
+
+  private static class PluginFilterIncludedInModule extends AbstractModule {
+    @Override
+    public void configure() {
+      DynamicSet.bind(binder(), FilterIncludedIn.class).to(TestFilter.class);
+    }
+  }
+
+  private static class TestFilter implements FilterIncludedIn {
+    @Override
+    public Predicate<String> getBranchFilter(Project.NameKey project, RevCommit commit) {
+      return branch -> !branch.startsWith("t");
+    }
+
+    @Override
+    public Predicate<String> getTagFilter(Project.NameKey project, RevCommit commit) {
+      return tag -> !tag.startsWith("bad");
+    }
+  }
 
   @Test
   public void includedInOpenChange() throws Exception {
@@ -41,8 +66,7 @@ public class ChangeIncludedInIT extends AbstractDaemonTest {
     assertThat(gApi.changes().id(result.getChangeId()).includedIn().tags).isEmpty();
   }
 
-  @Test
-  public void includedInMergedChange() throws Exception {
+  String testCase() throws Exception {
     Result result = createChange();
     gApi.changes()
         .id(result.getChangeId())
@@ -61,12 +85,29 @@ public class ChangeIncludedInIT extends AbstractDaemonTest {
         .update();
     gApi.projects().name(project.get()).tag("test-tag").create(new TagInput());
 
-    assertThat(gApi.changes().id(result.getChangeId()).includedIn().tags)
-        .containsExactly("test-tag");
-
     createBranch(BranchNameKey.create(project, "test-branch"));
+    return result.getChangeId();
+  }
 
-    assertThat(gApi.changes().id(result.getChangeId()).includedIn().branches)
+  @Test
+  public void includedInMergedChange() throws Exception {
+    String changeId = testCase();
+    assertThat(gApi.changes().id(changeId).includedIn().tags).containsExactly("test-tag");
+
+    assertThat(gApi.changes().id(changeId).includedIn().branches)
         .containsExactly("master", "test-branch");
+  }
+
+  @Test
+  public void includedInFiltered() throws Exception {
+    String changeId = testCase();
+    gApi.projects().name(project.get()).tag("bad-tag").create(new TagInput());
+    assertThat(gApi.changes().id(changeId).includedIn().tags)
+        .containsExactly("bad-tag", "test-tag");
+    try (AutoCloseable ignored = installPlugin("my-plugin", PluginFilterIncludedInModule.class)) {
+      assertThat(gApi.changes().id(changeId).includedIn().tags).containsExactly("test-tag");
+
+      assertThat(gApi.changes().id(changeId).includedIn().branches).containsExactly("master");
+    }
   }
 }

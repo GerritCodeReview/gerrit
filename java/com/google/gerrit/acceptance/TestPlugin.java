fce223951302a18ce2799fa8a1188a22b3859d08
Fredde Ribeiro <fredde@google.com>
2024-01-17 09:44:47 +0000

    TEST
    Change-Id: I2905681cc241c589b62e9351ffbf5349553a8bc2

--- java/com/google/gerrit/acceptance/TestPlugin.java
+++ java/com/google/gerrit/acceptance/TestPlugin.java
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
 @Retention(RUNTIME)
 public @interface TestPlugin {
   String name();
-
+// Google7
   String sysModule() default "";
 
   String httpModule() default "";

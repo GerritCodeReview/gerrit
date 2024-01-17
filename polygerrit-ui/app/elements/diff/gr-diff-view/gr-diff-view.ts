5f2bbd32c273e9f4ebcad3353780c0bbb3233b98
Chris Poucet <poucet@google.com>
2024-01-17 13:29:03 +0100

    Only show the "Show Blame" button if blame is enabled.
    Google-Bug-Id: b/316456978
    Release-Notes: skip
    Change-Id: I9d125ffb70f1f8d9ee994e7db1d5e83570ebdeeb

--- polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.ts
+++ polygerrit-ui/app/elements/diff/gr-diff-view/gr-diff-view.ts
@@ -107,6 +107,7 @@ import {
 import {isImageDiff} from '../../../utils/diff-util';
 import {formStyles} from '../../../styles/form-styles';
 import {NormalizedFileInfo} from '../../change/gr-file-list/gr-file-list';
+import {configModelToken} from '../../../models/config/config-model';
 
 const LOADING_BLAME = 'Loading blame...';
 const LOADED_BLAME = 'Blame loaded';
@@ -208,6 +209,10 @@ export class GrDiffView extends LitElement {
   // Private but used in tests.
   @state() isActiveChildView = false;
 
+  // Whether to allow the "Show Blame button"
+  @state()
+  allowBlame = false;
+
   // Private but used in tests.
   @state()
   loggedIn = false;
@@ -260,6 +265,8 @@ export class GrDiffView extends LitElement {
   private readonly getShortcutsService = resolve(this, shortcutsServiceToken);
 
   private readonly getViewModel = resolve(this, changeViewModelToken);
+
+  private readonly getConfigModel = resolve(this, configModelToken);
 
   private throttledToggleFileReviewed?: (e: KeyboardEvent) => void;
 
@@ -455,6 +462,13 @@ export class GrDiffView extends LitElement {
       ([path, files]) => {
         this.reviewed = !!path && !!files && files.includes(path);
       }
+    );
+
+    subscribe(
+      this,
+      () => this.getConfigModel().serverConfig$,
+      serverConfig =>
+        (this.allowBlame = serverConfig?.change.allow_blame ?? false)
     );
 
     // When user initially loads the diff view, we want to automatically mark
@@ -1054,26 +1068,9 @@ export class GrDiffView extends LitElement {
   }
 
   private renderRightControls() {
-    const blameLoaderClass =
-      !isMagicPath(this.path) && !isImageDiff(this.diff) ? 'show' : '';
-    const blameToggleLabel =
-      this.isBlameLoaded && !this.isBlameLoading ? 'Hide blame' : 'Show blame';
     const diffModeSelectorClass = !this.diff || this.diff.binary ? 'hide' : '';
     return html` <div class="rightControls">
-      ${this.renderSidebarTriggers()}
-      <span class="blameLoader ${blameLoaderClass}">
-        <gr-button
-          link=""
-          id="toggleBlame"
-          title=${this.createTitle(
-            Shortcut.TOGGLE_BLAME,
-            ShortcutSection.DIFFS
-          )}
-          ?disabled=${this.isBlameLoading}
-          @click=${this.toggleBlame}
-          >${blameToggleLabel}</gr-button
-        >
-      </span>
+      ${this.renderSidebarTriggers()} ${this.renderBlameButton()}
       ${when(
         this.computeCanEdit(),
         () => html`
@@ -1141,6 +1138,24 @@ export class GrDiffView extends LitElement {
         </span>
       </gr-endpoint-decorator>
     </div>`;
+  }
+
+  private renderBlameButton() {
+    if (!this.allowBlame) return;
+    const blameLoaderClass =
+      !isMagicPath(this.path) && !isImageDiff(this.diff) ? 'show' : '';
+    const blameToggleLabel =
+      this.isBlameLoaded && !this.isBlameLoading ? 'Hide blame' : 'Show blame';
+    return html` <span class="blameLoader ${blameLoaderClass}">
+      <gr-button
+        link=""
+        id="toggleBlame"
+        title=${this.createTitle(Shortcut.TOGGLE_BLAME, ShortcutSection.DIFFS)}
+        ?disabled=${this.isBlameLoading}
+        @click=${this.toggleBlame}
+        >${blameToggleLabel}</gr-button
+      >
+    </span>`;
   }
 
   private renderDialogs() {
@@ -1771,12 +1786,13 @@ export class GrDiffView extends LitElement {
    * Otherwise hide it.
    */
   private toggleBlame() {
+    if (!this.allowBlame) return;
     assertIsDefined(this.diffHost, 'diffHost');
     if (this.isBlameLoaded) {
       this.diffHost.clearBlame();
-      return;
+    } else {
+      this.loadBlame();
     }
-    this.loadBlame();
   }
 
   private handleToggleHideAllCommentThreads() {

dab8fb63fc056e178fbe671d4888e6908e7e9f93
Fabio Ponciroli <ponch78@gmail.com>
2024-01-22 11:53:32 +0000

    Use changenumber field when querying for change number
    Stop using virtual Id when querying for change number.
    Rely on the real change number to allow discoverability of
    imported changes.
    
    Bug: Issue 318396515
    Release-Notes: Use changenumber field when querying for change number
    Change-Id: I64f55e8af61e6708e9ae120a411185ba7adedf75

--- javatests/com/google/gerrit/server/query/change/FakeQueryChangesTest.java
+++ javatests/com/google/gerrit/server/query/change/FakeQueryChangesTest.java
@@ -238,7 +238,8 @@ public abstract class FakeQueryChangesTest extends AbstractQueryChangesTest {
 
   @SuppressWarnings("unused")
   private void executeQuery(String query) throws QueryParseException {
-    List<ChangeData> unused = queryProvider.get().query(queryBuilder.parse(query));
+    List<ChangeData> unused =
+        queryProvider.get().query(changeQueryBuilderProvider.get().parse(query));
   }
 
   private void assertThatSearchQueryWasNotPaginated(int queryCount) {

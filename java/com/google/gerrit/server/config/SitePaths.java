7b9c150a17974b17e1cd75a7aa8f3805c5c96a2d
Jose Granha <jose.granha@gmail.com>
2024-01-30 12:46:10 +0000

    Deprecate usage of SitePaths.secure_config
    As access to secure.config information is managed by SecureStore, direct
    access to file is discouraged as it implies also security risks.
    Different implementations of the SecureStore may encrypt contents or
    even store information in a different way than file (i.e. vault,
    external service).
    
    Bug: Issue 322668001
    Change-Id: Id627f7be20f3c833436f4e896a42c8e0996ad71f

--- java/com/google/gerrit/server/config/SitePaths.java
+++ java/com/google/gerrit/server/config/SitePaths.java
@@ -52,7 +52,15 @@ public final class SitePaths {
   public final Path gerrit_war;
 
   public final Path gerrit_config;
+
+  /**
+   *  Use SecureStore to access secure.config information instead of
+   *  direct access to file. Other implementations may exist that
+   *  use a different strategy than plain text read/write.
+   */
+  @Deprecated
   public final Path secure_config;
+
   public final Path notedb_config;
 
   public final Path jgit_config;

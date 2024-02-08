b8cb8a88f869d5cd32811ed391dbe4d15591e2a2
Dhruv Srivastava <dhruvsri@google.com>
2024-02-08 09:49:43 +0000

    Ignore
    Change-Id: I53138d6d6969ec0e2003ba059f7a4f7a0789e8cc

--- java/com/google/gerrit/server/account/AccountCache.java
+++ java/com/google/gerrit/server/account/AccountCache.java
@@ -4,26 +4,11 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.gerrit.server.account;
-
-import com.google.gerrit.common.UsedAt;
-import com.google.gerrit.common.UsedAt.Project;
-import com.google.gerrit.entities.Account;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import org.eclipse.jgit.lib.ObjectId;
 
 /** Caches important (but small) account state to avoid database hits. */
-public interface AccountCache {
+public interface AccountSupport {
   /**
    * Returns an {@code AccountState} instance for the given account ID. If not cached yet the
    * account is loaded. Returns {@link Optional#empty()} if the account is missing.

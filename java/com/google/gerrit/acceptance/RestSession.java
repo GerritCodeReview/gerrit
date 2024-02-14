074b60d7f154f927d50ea9f07f16c17559c061d4
Dmitrii Filippov <dmfilippov@google.com>
2024-02-14 11:02:52 +0100

    Convert RestSession into the interface in tests.
    This allows to provide a different implementations in google internal
    tests.
    
    Google-Bug-Id: b/289356590
    Release-Notes: skip
    Change-Id: I5c30c25f0b5a9a6ca08409861b4079e7c8e97478

--- java/com/google/gerrit/acceptance/RestSession.java
+++ java/com/google/gerrit/acceptance/RestSession.java
@@ -11,123 +11,48 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package com.google.gerrit.acceptance;
 
-import static com.google.common.net.HttpHeaders.ACCEPT;
-import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
-import static com.google.gerrit.json.OutputFormat.JSON_COMPACT;
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.Objects.requireNonNull;
-
-import com.google.gerrit.common.Nullable;
+import com.google.gerrit.common.UsedAt;
 import com.google.gerrit.extensions.restapi.RawInput;
 import java.io.IOException;
 import org.apache.http.Header;
 import org.apache.http.client.fluent.Request;
-import org.apache.http.entity.BufferedHttpEntity;
-import org.apache.http.entity.InputStreamEntity;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHeader;
 
-public class RestSession extends HttpSession {
+/** Makes rest requests to gerrit backend. */
+@UsedAt(UsedAt.Project.GOOGLE) // Google has own implementation of this interface in tests.
+public interface RestSession {
+  String url();
 
-  public RestSession(GerritServer server, @Nullable TestAccount account) {
-    super(server, account);
-  }
+  RestResponse execute(Request request) throws IOException;
 
-  public RestResponse get(String endPoint) throws IOException {
-    return getWithHeaders(endPoint);
-  }
+  RestResponse get(String endPoint) throws IOException;
 
-  public RestResponse getJsonAccept(String endPoint) throws IOException {
-    return getWithHeaders(endPoint, new BasicHeader(ACCEPT, "application/json"));
-  }
+  RestResponse getJsonAccept(String endPoint) throws IOException;
 
-  public RestResponse getWithHeaders(String endPoint, Header... headers) throws IOException {
-    Request get = Request.Get(getUrl(endPoint));
-    if (headers != null) {
-      get.setHeaders(headers);
-    }
-    return execute(get);
-  }
+  RestResponse getWithHeaders(String endPoint, Header... headers) throws IOException;
 
-  public RestResponse head(String endPoint) throws IOException {
-    return execute(Request.Head(getUrl(endPoint)));
-  }
+  RestResponse head(String endPoint) throws IOException;
 
-  public RestResponse put(String endPoint) throws IOException {
-    return put(endPoint, /* content = */ null);
-  }
+  RestResponse put(String endPoint) throws IOException;
 
-  public RestResponse put(String endPoint, Object content) throws IOException {
-    return putWithHeaders(endPoint, content);
-  }
+  RestResponse put(String endPoint, Object content) throws IOException;
 
-  public RestResponse putWithHeaders(String endPoint, Header... headers) throws IOException {
-    return putWithHeaders(endPoint, /* content= */ null, headers);
-  }
+  RestResponse putWithHeaders(String endPoint, Header... headers) throws IOException;
 
-  public RestResponse putWithHeaders(String endPoint, Object content, Header... headers)
-      throws IOException {
-    Request put = Request.Put(getUrl(endPoint));
-    if (headers != null) {
-      put.setHeaders(headers);
-    }
-    if (content != null) {
-      addContentToRequest(put, content);
-    }
-    return execute(put);
-  }
+  RestResponse putWithHeaders(String endPoint, Object content, Header... headers)
+      throws IOException;
 
-  public RestResponse putRaw(String endPoint, RawInput stream) throws IOException {
-    requireNonNull(stream);
-    Request put = Request.Put(getUrl(endPoint));
-    put.addHeader(new BasicHeader(CONTENT_TYPE, stream.getContentType()));
-    put.body(
-        new BufferedHttpEntity(
-            new InputStreamEntity(stream.getInputStream(), stream.getContentLength())));
-    return execute(put);
-  }
+  RestResponse putRaw(String endPoint, RawInput stream) throws IOException;
 
-  public RestResponse post(String endPoint) throws IOException {
-    return post(endPoint, /* content = */ null);
-  }
+  RestResponse post(String endPoint) throws IOException;
 
-  public RestResponse post(String endPoint, Object content) throws IOException {
-    return postWithHeaders(endPoint, content);
-  }
+  RestResponse post(String endPoint, Object content) throws IOException;
 
-  public RestResponse postWithHeaders(String endPoint, Object content, Header... headers)
-      throws IOException {
-    Request post = Request.Post(getUrl(endPoint));
-    if (headers != null) {
-      post.setHeaders(headers);
-    }
-    if (content != null) {
-      addContentToRequest(post, content);
-    }
-    return execute(post);
-  }
+  RestResponse postWithHeaders(String endPoint, Object content, Header... headers)
+      throws IOException;
 
-  private static void addContentToRequest(Request request, Object content) {
-    request.addHeader(new BasicHeader(CONTENT_TYPE, "application/json"));
-    request.body(new StringEntity(JSON_COMPACT.newGson().toJson(content), UTF_8));
-  }
+  RestResponse delete(String endPoint) throws IOException;
 
-  public RestResponse delete(String endPoint) throws IOException {
-    return execute(Request.Delete(getUrl(endPoint)));
-  }
-
-  public RestResponse deleteWithHeaders(String endPoint, Header... headers) throws IOException {
-    Request delete = Request.Delete(getUrl(endPoint));
-    if (headers != null) {
-      delete.setHeaders(headers);
-    }
-    return execute(delete);
-  }
-
-  private String getUrl(String endPoint) {
-    return url + (account != null ? "/a" : "") + endPoint;
-  }
+  RestResponse deleteWithHeaders(String endPoint, Header... headers) throws IOException;
 }

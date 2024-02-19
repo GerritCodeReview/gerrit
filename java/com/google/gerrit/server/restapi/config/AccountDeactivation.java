b2f20b871668e2c798b3da8d2a836a42e9ed3433
Wendy Wang <wendy.wang10@sap.com>
2024-02-19 11:56:22 +0100

    Introduce `AccountDeactivation` as REST API endpoint
    Allow manual triggering of AccountDeactivator which is generally
    scheduled in gerrit.config. This is useful for situations requiring
    accounts to be synced with the backend provider outside of schedule.
    
    Release-Notes: New REST API endpoint for AccountDeactivation
    Change-Id: Ie4cd939b00e6052576476773e6a5f413ade01253

---
+++ java/com/google/gerrit/server/restapi/config/AccountDeactivation.java
@@ -0,0 +1,42 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.restapi.config;
+
+import com.google.gerrit.common.data.GlobalCapability;
+import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gerrit.extensions.common.Input;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.account.AccountDeactivator;
+import com.google.gerrit.server.config.ConfigResource;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@RequiresCapability(GlobalCapability.ADMINISTRATE_SERVER)
+@Singleton
+public class AccountDeactivation implements RestModifyView<ConfigResource, Input> {
+  private final AccountDeactivator deactivator;
+
+  @Inject
+  AccountDeactivation(AccountDeactivator deactivator) {
+    this.deactivator = deactivator;
+  }
+
+  @Override
+  public Response<?> apply(ConfigResource rsrc, Input unusedInput) {
+    deactivator.run();
+    return Response.ok("Account deactivator run.");
+  }
+}

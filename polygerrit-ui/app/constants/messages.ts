1ea681d0213ce088f229bf39b6894d89e92df4a0
Dhruv Srivastava <dhruvsri@google.com>
2024-02-19 12:26:05 +0000

    Ignore Updates
    NOTE FOR REVIEWERS - errors occurred while applying the patch.
    PLEASE REVIEW CAREFULLY.
    Errors:
    Error applying patch in polygerrit-ui/app/constants/messages.ts, hunk HunkHeader[4,5->4,3]: Hunk cannot be applied
    
    Original patch:
     diff --git a/polygerrit-ui/app/constants/messages.ts b/polygerrit-ui/app/constants/messages.ts
    old mode 100644
    new mode 100644
    --- a/polygerrit-ui/app/constants/messages.ts
    +++ b/polygerrit-ui/app/constants/messages.ts
    @@ -4,5 +4,3 @@
      */
     
     /** Message shown when no threads in gr-thread-list for robot comments */
    -export const NO_ROBOT_COMMENTS_THREADS_MSG =
    -  'There are no findings for this patchset.';
    
    Change-Id: I53138d6d6969ec0e2003ba059f7a4f7a0789e8cc

--- polygerrit-ui/app/constants/messages.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2020 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/** Message shown when no threads in gr-thread-list for robot comments */
-export const NO_ROBOT_COMMENTS_THREADS_MSG =
-  'There are no findings for this patchset.';

b13ff1f625ca870fdfe6e0ce67c7156c4a694926
Kamil Musin <kamilm@google.com>
2024-02-23 16:22:24 +0100

    Remove remaining references to old helper.
    All existing usages have been moved. Remove the files and rename
    _restApiHelperNew to _restApiHelper.
    
    There is only few usages left:
    
    - There are a couple of tests in gr-rest-api-impl that are for the
    helper. Move them to the gr-rest-api-helper_test.ts file.
    - Service worker class uses readResponsePayload: Point to a new one and
      add try-catch to replicate existing behaviour. This method ideally
      needs a more involved error handling for the fetch response, but this
      is out of scope for this change.
    
    Google-Bug-Id: b/297849592
    Release-Notes: skip
    Change-Id: Ic2226b996bd61811a02fe692b7aab293b320f903

--- polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-apis/gr-rest-api-helper_test.ts
+++ polygerrit-ui/app/elements/shared/gr-rest-api-interface/gr-rest-apis/gr-rest-api-helper_test.ts
@@ -25,6 +25,7 @@ import {assert} from '@open-wc/testing';
 import {AuthService} from '../../../../services/gr-auth/gr-auth';
 import {GrAuthMock} from '../../../../services/gr-auth/gr-auth_mock';
 import {ParsedJSON} from '../../../../types/common';
+import {getBaseUrl} from '../../../../utils/url-util';
 
 function makeParsedJSON<T>(val: T): ParsedJSON {
   return val as unknown as ParsedJSON;
@@ -127,6 +128,30 @@ suite('gr-rest-api-helper tests', () => {
       await assertWriteRequest();
       const res: Response = await promise;
       assert.equal(await res.text(), 'Yay');
+    });
+
+    test('fetch calls auth fetch and logs', async () => {
+      const logStub = sinon.stub(helper, 'logCall');
+      const response = new Response(undefined, {status: 404});
+      const url = '/my/url';
+      const fetchOptions = {method: 'DELETE'};
+      authFetchStub.resolves(response);
+      const startTime = 123;
+      sinon.stub(Date, 'now').returns(startTime);
+      helper.fetch({url, fetchOptions});
+
+      await assertWriteRequest();
+      assert.isTrue(logStub.calledOnce);
+      const expectedReq = {
+        url: getBaseUrl() + url,
+        fetchOptions,
+        anonymizedUrl: getBaseUrl() + url,
+      };
+      assert.deepEqual(logStub.lastCall.args, [
+        expectedReq,
+        startTime,
+        response.status,
+      ]);
     });
   });
 
@@ -487,5 +512,18 @@ suite('gr-rest-api-helper tests', () => {
       const result = parsePrefixedJSON(serial);
       assert.deepEqual(result, obj);
     });
+  });
+
+  test('logCall only reports requests with anonymized URLs', async () => {
+    sinon.stub(Date, 'now').returns(200);
+    const handler = sinon.stub();
+    addListenerForTest(document, 'gr-rpc-log', handler);
+
+    helper.logCall({url: 'url'}, 100, 200);
+    assert.isFalse(handler.called);
+
+    helper.logCall({url: 'url', anonymizedUrl: 'not url'}, 100, 200);
+    await waitEventLoop();
+    assert.isTrue(handler.calledOnce);
   });
 });

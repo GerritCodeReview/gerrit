31dba1955c8492f5bed99e164e81649bd295e2df
Ben Rohlfs <brohlfs@google.com>
2024-02-26 19:23:01 +0100

    Let gr-change-actions stay in loading state until `mergeable` is set
    We are waiting for mergeability to be decided before firing the
    `SHOW_CHANGE` event for plugins. Plugins may decide to hide certain
    actions, so we should be consistent and also wait for mergeability
    before showing action buttons
    
    Release-Notes: skip
    Google-Bug-Id: b/316650310
    Change-Id: Ia4f05bf98b6a8c7ae8c86e255ed235a06e41e6c4

--- polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions_test.ts
+++ polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions_test.ts
@@ -142,6 +142,7 @@ suite('gr-change-actions tests', () => {
         },
       };
       element.changeNum = 42 as NumericChangeId;
+      element.mergeable = false;
       element.latestPatchNum = 2 as PatchSetNumber;
       element.account = {
         _account_id: 123 as AccountId,
@@ -302,6 +303,25 @@ suite('gr-change-actions tests', () => {
           </dialog>
         `
       );
+    });
+
+    test('isLoading', async () => {
+      const isLoading = () => {
+        const loadingButton = queryAndAssert(
+          element,
+          'div#mainContent > gr-button'
+        );
+        assert.equal(loadingButton.textContent, 'Loading actions...');
+        return loadingButton.getAttribute('hidden') === null;
+      };
+
+      element.mergeable = undefined;
+      await element.updateComplete;
+      assert.isTrue(isLoading());
+
+      element.mergeable = true;
+      await element.updateComplete;
+      assert.isFalse(isLoading());
     });
 
     test('show-revision-actions event should fire', async () => {

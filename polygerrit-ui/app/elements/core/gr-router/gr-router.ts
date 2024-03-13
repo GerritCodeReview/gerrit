6169f7024581fa7fb59eb92815a5839b04e150ee
Ben Rohlfs <brohlfs@google.com>
2024-03-13 08:42:32 +0100

    Fix browser BACK button when going to plugin pages
    We have to make sure that we only use `pushState()` of the browser
    History API for URLs that gr-router will actually handle.
    Calling `pushState()` tells the browser that both the previous and the
    next URL are handled by the same single page application with a
    popstate event handler. But if we call `pushState()` and then
    later `window.location.reload()` from the router and a separate page
    and document are loaded, then the `BACK` button will stop working.
    
    Bug: Issue 40015337
    Release-Notes: Fix browser BACK button when going to plugin pages
    Change-Id: I3b2773a1d1080f67ef325b54f92069aced1a1907

--- polygerrit-ui/app/elements/core/gr-router/gr-router.ts
+++ polygerrit-ui/app/elements/core/gr-router/gr-router.ts
@@ -120,12 +120,6 @@ const RoutePattern = {
   NEW_AGREEMENTS: /^\/settings\/new-agreement\/?/,
   REGISTER: /^\/register(\/.*)?$/,
 
-  // Pattern for login and logout URLs intended to be passed-through. May
-  // include a return URL.
-  // TODO: Maybe this pattern and its handler can just be removed, because
-  // passing through is what the default router would eventually do anyway.
-  LOG_IN_OR_OUT: /^\/log(in|out)(\/(.+))?$/,
-
   // Pattern for a catchall route when no other pattern is matched.
   DEFAULT: /.*/,
 
@@ -170,8 +164,6 @@ const RoutePattern = {
 
   // Matches /admin/repos/<repos>,access.
   REPO_DASHBOARDS: /^\/admin\/repos\/(.+),dashboards$/,
-
-  PLUGINS: /^\/plugins\/(.+)$/,
 
   // Matches /admin/plugins with optional filter and offset.
   PLUGIN_LIST: /^\/admin\/plugins\/?(?:\/q\/filter:(.*?))?(?:,(\d+))?$/,
@@ -460,7 +452,10 @@ export class GrRouter implements Finalizable, NavigationService {
    */
   redirectToLogin(returnUrl: string) {
     const basePath = getBaseUrl() || '';
-    this.setUrl(
+    // We are not using `this.getNavigation().setUrl()`, because the login
+    // page is served directly from the backend and is not part of the web
+    // app.
+    window.location.assign(
       '/login/' + encodeURIComponent(returnUrl.substring(basePath.length))
     );
   }
@@ -583,6 +578,8 @@ export class GrRouter implements Finalizable, NavigationService {
    * page.show() eventually just calls `window.history.pushState()`.
    */
   setUrl(url: string) {
+    // TODO: Use window.location.assign() instead of page.show(), if the URL is
+    // external, i.e. not handled by the router.
     this.page.show(url);
   }
 
@@ -593,6 +590,8 @@ export class GrRouter implements Finalizable, NavigationService {
    * this.page.redirect() eventually just calls `window.history.replaceState()`.
    */
   replaceUrl(url: string) {
+    // TODO: Use window.location.replace() instead of page.redirect(), if the
+    // URL is external, i.e. not handled by the router.
     this.redirect(url);
   }
 
@@ -809,10 +808,6 @@ export class GrRouter implements Finalizable, NavigationService {
       this.handleRepoRoute(ctx)
     );
 
-    this.mapRoute(RoutePattern.PLUGINS, 'handlePassThroughRoute', () =>
-      this.handlePassThroughRoute()
-    );
-
     this.mapRoute(
       RoutePattern.PLUGIN_LIST,
       'handlePluginListFilterRoute',
@@ -919,10 +914,6 @@ export class GrRouter implements Finalizable, NavigationService {
 
     this.mapRoute(RoutePattern.REGISTER, 'handleRegisterRoute', ctx =>
       this.handleRegisterRoute(ctx)
-    );
-
-    this.mapRoute(RoutePattern.LOG_IN_OR_OUT, 'handlePassThroughRoute', () =>
-      this.handlePassThroughRoute()
     );
 
     this.mapRoute(

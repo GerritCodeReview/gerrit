2daef75c5bfc28492a600f61634399d914f6d314
Kamil Musin <kamilm@google.com>
2024-03-27 17:26:16 +0100

    Improve change-model load change consistency
    1. Only update the change if change is undefined, or if the change number
    is the same:
    There must be a consistency between ChangeViewModel and
    ChangeModel. Only change to a ChangeViewModel is allowed to change the
    changeNum, while all other calls to updateStateChange can only
    update the info of the current change to a more recent one. The update
    through rxjs can load a different change since updateLoadingState sets
    the change to undefined.
    
    2. Simplify LoadingState and remove RELOADING:
    RELOADING state previously could only happen as the result of a
    following race condition ChangeViewModel forceLoad sets changNum to
    undefined and then async schedules setting it back to the original
    state. The RELOADING is only set if the processing of restApi results
    (no actual calls are performed since changeNum is undefined) is
    scheduled later in the Event Queue than the restoration of the
    original state. Moreover the actual difference between LOADING and
    RELOADING never matters in code.
    
    We simplify the logic by removing RELOADING state. Passing a undefined
    changeNum, will immediately set the change to undefined and LoadingState
    to NOT_LOADED. Previously it would be "change is undefined", loading
    state "LOADED", until it changes to "NOT_LOADED" in the next Event Queue
    cycle (or "RELOADING" if the race is won by the forceLoad)
    
    3. Move updateRevisionsWithCommitShas into updateStateChange:
    This makes the state more consistent. Now the change in ChangeModel
    always has the values set, regardless whether the value came from a
    manual call or subscription.
    
    4. Add catchError, to reset LoadingState if the RestApi calls throw an
    error
    
    Google-Bug-Id: b/328632912
    Release-Notes: skip
    Change-Id: I0ed368df7152150ed974c0b337894758ee531344

--- polygerrit-ui/app/models/checks/checks-model_test.ts
+++ polygerrit-ui/app/models/checks/checks-model_test.ts
@@ -30,12 +30,16 @@ import {
 } from '../../test/test-data-generators';
 import {waitUntil, waitUntilCalled} from '../../test/test-utils';
 import {ParsedChangeInfo} from '../../types/types';
-import {changeModelToken} from '../change/change-model';
+import {
+  changeModelToken,
+  updateRevisionsWithCommitShas,
+} from '../change/change-model';
 import {assert} from '@open-wc/testing';
 import {testResolver} from '../../test/common-test-setup';
 import {changeViewModelToken} from '../views/change';
 import {NumericChangeId, PatchSetNumber} from '../../api/rest-api';
 import {pluginLoaderToken} from '../../elements/shared/gr-js-api-interface/gr-plugin-loader';
+import {deepEqual} from '../../utils/deep-util';
 
 const PLUGIN_NAME = 'test-plugin';
 
@@ -106,17 +110,17 @@ suite('checks-model tests', () => {
     });
     await waitUntil(() => change === undefined);
 
-    const testChange = createParsedChange();
+    const testChange = updateRevisionsWithCommitShas(createParsedChange());
     testResolver(changeModelToken).updateStateChange(testChange);
-    await waitUntil(() => change === testChange);
+    await waitUntil(() => deepEqual(change, testChange));
     await waitUntilCalled(fetchSpy, 'fetch');
 
     assert.equal(
       model.latestPatchNum,
-      testChange.revisions[testChange.current_revision]
+      testChange!.revisions[testChange!.current_revision]
         ._number as PatchSetNumber
     );
-    assert.equal(model.changeNum, testChange._number);
+    assert.equal(model.changeNum, testChange!._number);
   });
 
   test('fetch throttle', async () => {
@@ -133,9 +137,9 @@ suite('checks-model tests', () => {
     });
     await waitUntil(() => change === undefined);
 
-    const testChange = createParsedChange();
+    const testChange = updateRevisionsWithCommitShas(createParsedChange());
     testResolver(changeModelToken).updateStateChange(testChange);
-    await waitUntil(() => change === testChange);
+    await waitUntil(() => deepEqual(change, testChange));
 
     model.reload('test-plugin');
     model.reload('test-plugin');
@@ -339,9 +343,9 @@ suite('checks-model tests', () => {
     });
     await waitUntil(() => change === undefined);
     clock.tick(1);
-    const testChange = createParsedChange();
+    const testChange = updateRevisionsWithCommitShas(createParsedChange());
     testResolver(changeModelToken).updateStateChange(testChange);
-    await waitUntil(() => change === testChange);
+    await waitUntil(() => deepEqual(change, testChange));
     clock.tick(600); // need to wait for 500ms throttle
     await waitUntilCalled(fetchSpy, 'fetch');
     const pollCount = fetchSpy.callCount;
@@ -366,9 +370,9 @@ suite('checks-model tests', () => {
     });
     await waitUntil(() => change === undefined);
     clock.tick(1);
-    const testChange = createParsedChange();
+    const testChange = updateRevisionsWithCommitShas(createParsedChange());
     testResolver(changeModelToken).updateStateChange(testChange);
-    await waitUntil(() => change === testChange);
+    await waitUntil(() => deepEqual(change, testChange));
     clock.tick(600); // need to wait for 500ms throttle
     await waitUntilCalled(fetchSpy, 'fetch');
     clock.tick(1);

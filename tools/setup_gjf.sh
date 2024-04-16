22566cdd62f3b522416b4de85e794a7f4738248f
Matthias Sohn <matthias.sohn@sap.com>
2024-04-16 12:58:22 +0200

    Delete old gjf versions
    Release-Notes: skip
    Change-Id: Ifa2b7792edbd9e602f6a98b8c52e457dbe0ab596

--- tools/setup_gjf.sh
+++ tools/setup_gjf.sh
@@ -20,18 +20,6 @@ set -eu
 VERSION=${1:-1.7}
 
 case "$VERSION" in
-1.3)
-    SHA1="a73cfe6f9af01bd6ff150c0b50c9d620400f784c"
-    TAG_PREFIX="google-java-format-"
-    ;;
-1.5)
-    SHA1="b1f79e4d39a3c501f07c0ce7e8b03ac6964ed1f1"
-    TAG_PREFIX="google-java-format-"
-    ;;
-1.6)
-    SHA1="02b3e84e52d2473e2c4868189709905a51647d03"
-    TAG_PREFIX="google-java-format-"
-    ;;
 1.7)
     SHA1="b6d34a51e579b08db7c624505bdf9af4397f1702"
     TAG_PREFIX="google-java-format-"

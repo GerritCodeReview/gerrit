f76af941c8add0d0d612fcb85d2b87667c48ee0e
Nitzan Gur-Furman <nitzan@google.com>
2024-04-26 12:28:29 +0000

    Introduce SafeProtoConverter
    This interface extends the proto converter interface, with the
    difference of having a test that verifies the fields are kept in sync.
    I.e. all fields in the Java entity exists in the proto one and vise
    versa. This is to avoid loosing data when converting between the types
    (For example, when caching preferences as protos, which are otherwise
    used by their Java entities' representations, and their definitions get
    expended frequently).
    
    The interface also includes a `ConvertibleToProto` interface, in order
    to raise the visibility on that expectation when editing the java class.
    
    The equality is checked using the regular `equals()` method, which adds
    a pitfall if a newly introduced field is not added to this method.
    However, this kind of eng-errors are easy to catch in a code review.
    
    This change converts `AccountIdProtoConverter` and
    `HumanCommentProtoConverter` to `SafeProtoConverter`s as a proof of
    concept. Other eligible converter classes will be converted in follow-up
    changes.
    
    Test implementation notes:
    This test unfortunately uses java reflections. Most of its code was
    copied from `ChangeInfoDiffer` and `ChangeInfoDifferTest`, with the
    addition of AutoValue and primitives handling.
    
    I have manually tested this flow WAI by adding/removing filed in the
    affected classes.
    
    Release-Notes: skip
    Google-Bug-Id: b/335372403
    Change-Id: If6399d9b594015a14cb9e36d0f30c5f68ceaabef

---
+++ javatests/com/google/gerrit/entities/converter/SafeProtoConverterTest.java
@@ -0,0 +1,189 @@
+package com.google.gerrit.entities.converter;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.primitives.Primitives;
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ClassInfo;
+import com.google.gerrit.common.Nullable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.junit.Test;
+
+public class SafeProtoConverterTest {
+  /**
+   * If this test fails, it's likely that you added a field to a Java class that has a {@link
+   * SafeProtoConverter} set, or that you have changed the default value for such a field. Please
+   * update the corresponding proto accordingly.
+   */
+  @Test
+  public void javaDefaultsKeptOnDoubleConversion() throws Exception {
+    for (SafeProtoConverter converter : listSafeConverters()) {
+      Object orig;
+      try {
+        orig = buildObjectWithFullFieldsOrThrow(converter.getEntityClass());
+      } catch (Exception e) {
+        throw new IllegalStateException(
+            String.format(
+                "Failed to build object for type %s, this likely means the buildObjectWithFullFieldsOrThrow should be adapted.",
+                converter.getEntityClass().getName()),
+            e);
+      }
+      Object res = converter.fromProto(converter.toProto(orig));
+      assertThat(orig).isEqualTo(res);
+    }
+  }
+
+  /**
+   * If this test fails, it's likely that you added a field to a proto that has a {@link
+   * SafeProtoConverter} set, or that you have changed the default value for such a field. Please
+   * update the corresponding Java class accordingly.
+   */
+  @Test
+  public void protoDefaultsKeptOnDoubleConversion() {
+    // TODO(b/335372403) - implement
+  }
+
+  List<SafeProtoConverter> listSafeConverters() throws Exception {
+    return ClassPath.from(getClass().getClassLoader())
+        .getTopLevelClassesRecursive("com.google.gerrit.entities").stream()
+        .map(ClassInfo::load)
+        .filter(clz -> SafeProtoConverter.class.isAssignableFrom(clz))
+        .filter(clz -> !SafeProtoConverter.class.equals(clz))
+        .filter(clz -> clz.isEnum())
+        .map(clz -> (SafeProtoConverter) clz.getEnumConstants()[0])
+        .collect(Collectors.toList());
+  }
+
+  @Nullable
+  private static Object buildObjectWithFullFieldsOrThrow(Class<?> clz) throws Exception {
+    if (clz == null) {
+      return null;
+    }
+    if (isSimple(clz)) {
+      return construct(clz);
+    }
+    if (isAutoValueCLass(clz)) {
+      return construct(toRepresentingAutoValueClass(clz).get());
+    }
+    Object toPopulate = construct(clz);
+    for (Field field : toPopulate.getClass().getDeclaredFields()) {
+      Class<?> parameterizedType = getParameterizedType(field);
+      if (!field.getType().isArray()
+          && !Map.class.isAssignableFrom(field.getType())
+          && !Collection.class.isAssignableFrom(field.getType())) {
+        if (!field.trySetAccessible()) {
+          return null;
+        }
+        field.set(toPopulate, buildObjectWithFullFieldsOrThrow(field.getType()));
+      } else if (Collection.class.isAssignableFrom(field.getType()) && parameterizedType != null) {
+        field.set(
+            toPopulate, ImmutableList.of(buildObjectWithFullFieldsOrThrow(parameterizedType)));
+      }
+    }
+    return toPopulate;
+  }
+
+  static boolean isAutoValueCLass(Class<?> clz) {
+    return toRepresentingAutoValueClass(clz).isPresent();
+  }
+
+  /**
+   * AutoValue annotations are not retained on runtime. We can onlt find out if a class is an
+   * AutoValue, by trying to load the expected AutoValue class.
+   *
+   * <p>For the class {@code package.Clz}, the AutoValue class name is {@code
+   * package.AutoValue_Clz}, for {@code package.Enclosing$Clz}, it is {@code
+   * package.AutoValue_Enclosing_Clz}
+   */
+  static Optional<Class<?>> toRepresentingAutoValueClass(Class<?> clz) {
+    String origClzName = clz.getName();
+    String autoValueClzName =
+        origClzName.substring(0, origClzName.lastIndexOf("."))
+            + ".AutoValue_"
+            + origClzName.substring(origClzName.lastIndexOf(".") + 1);
+    autoValueClzName = autoValueClzName.replace('$', '_');
+    try {
+      return Optional.of(clz.getClassLoader().loadClass(autoValueClzName));
+    } catch (Exception e) {
+      return Optional.empty();
+    }
+  }
+
+  @Nullable
+  private static Class<?> getParameterizedType(Field field) {
+    if (!Collection.class.isAssignableFrom(field.getType())) {
+      return null;
+    }
+    Type genericType = field.getGenericType();
+    if (genericType instanceof ParameterizedType) {
+      return (Class<?>) ((ParameterizedType) genericType).getActualTypeArguments()[0];
+    }
+    return null;
+  }
+
+  @Nullable
+  static Object construct(Class<?> clz) {
+    try {
+      if (clz == null) {
+        return null;
+      }
+      if (clz.isPrimitive()) {
+        return construct(Primitives.wrap(clz));
+      }
+      if (Primitives.isWrapperType(clz)) {
+        if (Boolean.class.isAssignableFrom(clz)) {
+          return true;
+        }
+        return clz.getDeclaredMethod("valueOf", String.class).invoke(null, "0");
+      }
+      if (clz.isEnum()) {
+        return clz.getEnumConstants()[0];
+      }
+      if (java.time.Instant.class.isAssignableFrom(clz)) {
+        return java.time.Instant.ofEpochSecond(42);
+      }
+      if (isAutoValueCLass(clz)) {
+        return construct(toRepresentingAutoValueClass(clz).get());
+      }
+      Constructor<?> constructor =
+          Arrays.stream(clz.getDeclaredConstructors())
+              // Filter out copy constructors
+              .filter(
+                  c ->
+                      c.getParameterCount() != 1 || !c.getParameterTypes()[0].isAssignableFrom(clz))
+              // Filter out private constructors which cannot be set accessible.
+              .filter(c -> c.canAccess(null) || c.trySetAccessible())
+              .min(Comparator.comparingInt(Constructor::getParameterCount))
+              .get();
+      List<Object> args = new ArrayList<>();
+      for (Class<?> f : constructor.getParameterTypes()) {
+        args.add(construct(f));
+      }
+      return constructor.newInstance(args.stream().toArray());
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to construct class " + clz.getName(), e);
+    }
+  }
+
+  static boolean isSimple(Class<?> c) {
+    return c.isPrimitive()
+        || c.isEnum()
+        || String.class.isAssignableFrom(c)
+        || Number.class.isAssignableFrom(c)
+        || Boolean.class.isAssignableFrom(c)
+        || Timestamp.class.isAssignableFrom(c);
+  }
+}

88008fde607a891042c7b3dfba5b4ac86d27d4d2
David Ostrovsky <david@ostrovsky.org>
2024-05-05 15:08:59 +0200

    WIP Bump Jetty version to 12.0.8 and servlet api to 6.0
    Add new dependency on jetty-session library.
    
    HiddenErrorHandler doesn't build.
    
    Also some tests don't compile yet.
    
    Release-Notes: Update Jetty version to 12.0.8 and servlet api to 6.0
    Change-Id: I8984ab3eb4648caf8cd9d2c1dc96cb5a2f6b4add

--- java/com/google/gerrit/httpd/ProjectBasicAuthFilter.java
+++ java/com/google/gerrit/httpd/ProjectBasicAuthFilter.java
@@ -270,13 +270,6 @@ class ProjectBasicAuthFilter implements Filter {
     }
 
     @Override
-    @Deprecated
-    public void setStatus(int sc, String sm) {
-      status(sc);
-      super.setStatus(sc, sm);
-    }
-
-    @Override
     public void setStatus(int sc) {
       status(sc);
       super.setStatus(sc);

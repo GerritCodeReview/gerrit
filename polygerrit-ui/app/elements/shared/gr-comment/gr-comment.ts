0339c734f150ccc66a75f7ff4a0806ae19826f6b
Milutin Kristofic <milutin@google.com>
2024-05-07 15:23:18 +0200

    Fix file extension
    Change-Id: I56ec7aa533ef1af7002fa97ea66052f974e7a599

--- polygerrit-ui/app/elements/shared/gr-comment/gr-comment.ts
+++ polygerrit-ui/app/elements/shared/gr-comment/gr-comment.ts
@@ -84,7 +84,6 @@ import {Suggestion, SuggestionsProvider} from '../../../api/suggestions';
 import {when} from 'lit/directives/when.js';
 import {getDocUrl} from '../../../utils/url-util';
 import {configModelToken} from '../../../models/config/config-model';
-import {getFileExtension} from '../../../utils/file-util';
 import {storageServiceToken} from '../../../services/storage/gr-storage_impl';
 import {deepEqual} from '../../../utils/deep-util';
 import {GrSuggestionDiffPreview} from '../gr-suggestion-diff-preview/gr-suggestion-diff-preview';
@@ -1044,7 +1043,7 @@ export class GrComment extends LitElement {
       this.comment.path !== SpecialFilePath.COMMIT_MESSAGE &&
       (!this.suggestionsProvider.supportedFileExtensions ||
         this.suggestionsProvider.supportedFileExtensions.includes(
-          getFileExtension(this.comment.path)
+          this.comment.path.substring(this.comment.path.lastIndexOf('.') + 1)
         )) &&
       this.comment === this.comments?.[0] && // Is first comment
       (this.comment.range || this.comment.line) && // Disabled for File comments

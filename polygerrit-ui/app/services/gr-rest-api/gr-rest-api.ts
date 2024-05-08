04b3b32884dc682aa16a37f818d517dde56a99e8
paladox <thomasmulhall410@yahoo.com>
2024-05-08 15:38:17 +0100

    Add getAccountEmails to user-model
    And use it for the email editor.
    
    Release-Notes: skip
    Change-Id: I2b788ad28c3fce8824f10a36d1bee40c3077ac5b

--- polygerrit-ui/app/services/gr-rest-api/gr-rest-api.ts
+++ polygerrit-ui/app/services/gr-rest-api/gr-rest-api.ts
@@ -231,7 +231,7 @@ export interface RestApiService extends Finalizable {
 
   saveEditPreferences(prefs: EditPreferencesInfo): Promise<Response>;
 
-  getAccountEmails(): Promise<EmailInfo[] | undefined>;
+  getAccountEmails(noCache?: boolean): Promise<EmailInfo[] | undefined>;
   getAccountEmailsFor(
     email: string,
     errFn?: ErrorCallback

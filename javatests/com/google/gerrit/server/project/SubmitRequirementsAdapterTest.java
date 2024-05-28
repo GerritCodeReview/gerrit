7369fe7d130656d5d372d147e26cfecfd514a98b
Luca Milanesio <luca.milanesio@gmail.com>
2024-05-28 14:36:18 +0100

    Demonstrate that a SubmitRecord status is wrong ignored
    When translating a SubmitRecord into a SubmitRequirement, the status
    field was forgotten in the translation and effectively ignored.
    A custom Prolog rule returning a status "OK" with extra labels was
    therefore considered as not satisifed requirement whilst it should have
    been passed them.
    
    Release-Notes: skip
    Change-Id: Ib0ac6c7638dece3d14261a3ac45c43825e7ecec8

--- javatests/com/google/gerrit/server/project/SubmitRequirementsAdapterTest.java
+++ javatests/com/google/gerrit/server/project/SubmitRequirementsAdapterTest.java
@@ -333,6 +333,21 @@ public class SubmitRequirementsAdapterTest {
   }
 
   @Test
+  public void customSubmitRule_WithLabels_WithStatusReady() {
+    SubmitRecord submitRecord =
+        createSubmitRecord(
+            "gerrit~PrologRule",
+            Status.OK,
+            Arrays.asList(
+                createLabel("custom-label-1", Label.Status.NEED),
+                createLabel("custom-label-2", Label.Status.REJECT)));
+
+    List<SubmitRequirementResult> requirements =
+        SubmitRequirementsAdapter.createResult(submitRecord, labelTypes, psCommitId);
+
+    assertThat(requirements).isEmpty();
+  }
+
   public void customSubmitRule_withMixOfPassingAndFailingLabels() {
     SubmitRecord submitRecord =
         createSubmitRecord(

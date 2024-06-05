a2cefae87d4cd50fb4dd4bcf442f1d39ccb23465
David Åkerman <davidak@axis.com>
2024-06-05 10:50:22 +0200

    Match hashtags suggestions with regex as well
    In previous change 428377 we missed to handle the case
    where the input is a regex. This will fix that.
    
    Solves: bug 344608194
    Release-Notes: skip
    Change-Id: I8f1588c09459c76e888b22b7f992b9a8f0064c99

--- polygerrit-ui/app/elements/change/gr-change-metadata/gr-change-metadata.ts
+++ polygerrit-ui/app/elements/change/gr-change-metadata/gr-change-metadata.ts
@@ -1156,7 +1156,11 @@ export class GrChangeMetadata extends LitElement {
           .flatMap(change => change.hashtags ?? [])
           .filter(isDefined)
           .filter(unique)
-          .filter(hashtag => hashtag.includes(input))
+          .filter(hashtag =>
+            input.startsWith('^')
+            ? new RegExp(input).test(hashtag)
+            : hashtag.includes(input)
+          )
           .map(hashtag => {
             return {name: hashtag, value: hashtag};
           })

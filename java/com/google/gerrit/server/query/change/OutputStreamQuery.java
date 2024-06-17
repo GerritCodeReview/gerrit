fc24e8c804071e0178156eb065e2a10944aa6d67
Nasser Grainawi <nasser.grainawi@linaro.org>
2024-06-17 14:24:38 -0600

    OutputStreamQuery: Add patchset comments when adding patchsets
    This keeps more of the per-patchset logic together for later refactors.
    
    Change-Id: I3d98e55fd77363c6403f5d4f46fa70959f00359f
    Release-Notes: skip
    (cherry picked from commit a261cb9c7dcf515289dd1197829ed2dfdcebfe01)

--- java/com/google/gerrit/server/query/change/OutputStreamQuery.java
+++ java/com/google/gerrit/server/query/change/OutputStreamQuery.java
@@ -309,6 +309,11 @@ public class OutputStreamQuery {
           d,
           labelTypes,
           accountLoader);
+      if (includeComments) {
+        for (PatchSetAttribute attribute : c.patchSets) {
+          eventFactory.addPatchSetComments(attribute, d.publishedComments(), accountLoader);
+        }
+      }
     }
 
     if (includeCurrentPatchSet) {
@@ -334,11 +339,6 @@ public class OutputStreamQuery {
 
     if (includeComments) {
       eventFactory.addComments(c, d.messages(), accountLoader);
-      if (includePatchSets) {
-        for (PatchSetAttribute attribute : c.patchSets) {
-          eventFactory.addPatchSetComments(attribute, d.publishedComments(), accountLoader);
-        }
-      }
     }
 
     if (includeDependencies) {

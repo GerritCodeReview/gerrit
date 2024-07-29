6f0ad8a0ae5293173700bfe8d5f976514dfef1f6
Rohan S Jamadagni <rohanj@arista.com>
2024-07-29 11:11:56 -0400

    GitBasicAuthPolicy: Add support for HTTP_OAUTH
    Currently, Gerrit doesn't support using OAUTH and HTTP passwords at the
    same time. This is needed for robot users that don't have an identity in
    an OAuth provider. This change adds support for a new configuration
    option HTTP_OAUTH that makes Gerrit first check the password against
    HTTP passwords and upon failure sends the password to the OAuth plugin
    for verification.
    
    Release-Notes: HTTP_OAUTH: allow service user to use http-password when using OAuth.
    Change-Id: I4e87b9ce69ffea544b7cf3b12b1ac0f168e25b73

--- java/com/google/gerrit/pgm/init/InitAuth.java
+++ java/com/google/gerrit/pgm/init/InitAuth.java
@@ -16,6 +16,7 @@ package com.google.gerrit.pgm.init;
 
 import static com.google.gerrit.extensions.client.GitBasicAuthPolicy.HTTP;
 import static com.google.gerrit.extensions.client.GitBasicAuthPolicy.HTTP_LDAP;
+import static com.google.gerrit.extensions.client.GitBasicAuthPolicy.HTTP_OAUTH;
 import static com.google.gerrit.extensions.client.GitBasicAuthPolicy.LDAP;
 import static com.google.gerrit.extensions.client.GitBasicAuthPolicy.OAUTH;
 import static com.google.gerrit.pgm.init.api.InitUtil.dnOf;
@@ -93,15 +94,21 @@ class InitAuth implements InitStep {
               "gitBasicAuthPolicy",
               HTTP,
               EnumSet.of(HTTP, HTTP_LDAP, LDAP));
-      case OAUTH -> {
-        GitBasicAuthPolicy gitBasicAuth =
-            auth.select(
-                "Git/HTTP authentication", "gitBasicAuthPolicy", HTTP, EnumSet.of(HTTP, OAUTH));
+      case OAUTH:
+        {
+          GitBasicAuthPolicy gitBasicAuth =
+              auth.select(
+                  "Git/HTTP authentication",
+                  "gitBasicAuthPolicy",
+                  HTTP,
+                  EnumSet.of(HTTP, OAUTH, HTTP_OAUTH));
 
-        if (gitBasicAuth == OAUTH) {
-          ui.message(
-              "*WARNING* Please make sure that your chosen OAuth provider\n"
-                  + "supports Git token authentication.\n");
+          if (gitBasicAuth == OAUTH || gitBasicAuth == HTTP_OAUTH) {
+            ui.message(
+                "*WARNING* Please make sure that your chosen OAuth provider\n"
+                    + "supports Git token authentication.\n");
+          }
+          break;
         }
       }
       case CLIENT_SSL_CERT_LDAP,

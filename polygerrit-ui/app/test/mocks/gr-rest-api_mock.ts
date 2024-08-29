b90784d3baf9cd0927f352ff4a5bae353601d91a
Edwin Kempin <ekempin@google.com>
2024-08-29 13:34:11 +0000

    Add a 'DUMP ACCOUNT STATE' button to the user profile
    Clicking on the 'DUMP ACCOUNT STATE' button opens a dialog that allows
    the user to copy the account state to the clipboard. This makes it easy
    to share the account state when it's needed to investigate account and
    permission issues.
    
    Bug: Google b/330836100
    Release-Notes: skip
    Change-Id: I768ab5c7a6c52774b8394f9bab2965c89f4471bd
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- polygerrit-ui/app/test/mocks/gr-rest-api_mock.ts
+++ polygerrit-ui/app/test/mocks/gr-rest-api_mock.ts
@@ -9,6 +9,7 @@ import {
   AccountDetailInfo,
   AccountExternalIdInfo,
   AccountInfo,
+  AccountStateInfo,
   ServerInfo,
   ProjectInfo,
   AccountCapabilityInfo,
@@ -198,6 +199,9 @@ export const grRestApiMock: RestApiService = {
   },
   getAccountSSHKeys(): Promise<SshKeyInfo[] | undefined> {
     return Promise.resolve([]);
+  },
+  getAccountState(): Promise<AccountStateInfo | undefined> {
+    return Promise.resolve({});
   },
   getAccountStatus(): Promise<string | undefined> {
     return Promise.resolve('');

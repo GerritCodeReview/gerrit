3abfb18f0a34406c3ba7dd057f766f8c663c6ff1
Edwin Kempin <ekempin@google.com>
2024-09-02 11:57:06 +0000

    AccountIT.getOwnAccountState: Allow to replace expected external IDs
    At Google we run AccountIT against our internal stack where accounts
    have different expected external IDs. To make the assertion of external
    IDs work, we need to replace the expected external IDs. We did this in
    assertExternalIds, but we can't use this method as it is for
    getOwnAccountState since it retrieves it from the server via the Get
    Account External IDs REST endpoint. To assert that external IDs that are
    returned from Get Account State we need an assertExternalIds method that
    gets the actual external IDs passed in.
    
    For this to work secondary email must use "non.google" as domain.
    
    Release-Notes: skip
    Change-Id: Icd11f8a09a17789d41d775b750c3bb4c49eb7624
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- javatests/com/google/gerrit/acceptance/api/accounts/AccountIT.java
+++ javatests/com/google/gerrit/acceptance/api/accounts/AccountIT.java
@@ -3466,7 +3466,7 @@ public class AccountIT extends AbstractDaemonTest {
     String name = "Foo";
     String username = name("foo");
     TestAccount foo = accountCreator.create(username, email, name, null);
-    String secondaryEmail = "secondary@example.com";
+    String secondaryEmail = "secondary@non.google";
     EmailInput input = newEmailInput(secondaryEmail);
     gApi.accounts().id(foo.id().get()).addEmail(input);
 
@@ -3513,8 +3513,9 @@ public class AccountIT extends AbstractDaemonTest {
           .comparingElementsUsing(getGroupToNameCorrespondence())
           .containsAtLeast("Anonymous Users", "Registered Users", groupName);
 
-      assertThat(state.externalIds.stream().map(e -> e.identity).collect(toImmutableSet()))
-          .containsExactly("mailto:" + email, "username:" + username, "mailto:" + secondaryEmail);
+      assertExternalIds(
+          state.externalIds.stream().map(e -> e.identity).collect(toImmutableSet()),
+          ImmutableSet.of("mailto:" + email, "username:" + username, "mailto:" + secondaryEmail));
 
       assertThat(state.metadata)
           .containsExactly(metadata1, metadata2, metadata3, metadata4)
@@ -3585,13 +3586,19 @@ public class AccountIT extends AbstractDaemonTest {
     return testGroupBackend;
   }
 
-  protected void assertExternalIds(Account.Id accountId, ImmutableSet<String> extIds)
+  private void assertExternalIds(Account.Id accountId, ImmutableSet<String> extIds)
       throws Exception {
-    assertThat(
-            gApi.accounts().id(accountId.get()).getExternalIds().stream()
-                .map(e -> e.identity)
-                .collect(toImmutableSet()))
-        .isEqualTo(extIds);
+    assertExternalIds(
+        gApi.accounts().id(accountId.get()).getExternalIds().stream()
+            .map(e -> e.identity)
+            .collect(toImmutableSet()),
+        extIds);
+  }
+
+  protected void assertExternalIds(
+      ImmutableSet<String> actualExternalIds, ImmutableSet<String> expectedExternalIds)
+      throws Exception {
+    assertThat(actualExternalIds).isEqualTo(expectedExternalIds);
   }
 
   private void assertExternalEmails(Account.Id accountId, ImmutableSet<String> extIds)

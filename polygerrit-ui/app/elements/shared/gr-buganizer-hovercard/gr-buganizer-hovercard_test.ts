5bd171e350863410c48c978bff163057577b24c0
nihardamar <nihardamar@google.com>
2024-09-10 16:31:10 -0400

    Add buganizer hovercard skeleton component
    Change-Id: I4640227967ba4345e001e152c75ceb2c18de6e49
    Bug: 362553833

---
+++ polygerrit-ui/app/elements/shared/gr-buganizer-hovercard/gr-buganizer-hovercard_test.ts
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import '../../../test/common-test-setup';
+import {fixture, assert} from '@open-wc/testing';
+import {html} from 'lit';
+import './gr-buganizer-hovercard';
+import {GrBuganizerHovercard} from './gr-buganizer-hovercard';
+import {queryAndAssert} from '../../../test/test-utils';
+
+suite('gr-buganizer-hovercard tests', () => {
+  let element: GrBuganizerHovercard;
+
+  setup(async () => {
+    element = await fixture<GrBuganizerHovercard>(
+      html`<gr-buganizer-hovercard
+        issueNumber="12345"
+        match="bug: 12345"
+      ></gr-buganizer-hovercard>`
+    );
+    await element.updateComplete;
+  });
+
+  test('hovering over link shows hovercard', async () => {
+    const link = queryAndAssert(element, 'span');
+    const hovercard = queryAndAssert(element, '.hovercard');
+    assert.equal(getComputedStyle(hovercard).display, 'none');
+    link.dispatchEvent(new MouseEvent('mouseenter'));
+    await element.updateComplete;
+    assert.notEqual(getComputedStyle(hovercard).display, 'none');
+  });
+
+  test('hovering out of link hides hovercard', async () => {
+    const link = queryAndAssert(element, 'span');
+    const hovercard = queryAndAssert(element, '.hovercard');
+    link.dispatchEvent(new MouseEvent('mouseenter'));
+    await element.updateComplete;
+    assert.notEqual(getComputedStyle(hovercard).display, 'none');
+    link.dispatchEvent(new MouseEvent('mouseleave'));
+    await element.updateComplete;
+    assert.equal(getComputedStyle(hovercard).display, 'none');
+  });
+});

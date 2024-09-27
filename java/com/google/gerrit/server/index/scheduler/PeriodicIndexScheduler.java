a62b1380be51dc87783cbb719dfe9e5cdeb77e89
Saša Živkov <sasa.zivkov@sap.com>
2024-09-27 16:22:07 +0200

    Allow to schedule periodic project reindexing
    This may be useful in high-availability scenarios where each primary
    maintains own index. For example, if messages between the primaries are
    not delivered due to prolonged downtime on one of the primaries the
    projects index will become consistent on the next run of the periodic
    project indexer.
    
    Change-Id: I19e00ed128bfb7b9f4a256f1f75efb0a8af2f7b5

--- java/com/google/gerrit/server/index/scheduler/PeriodicIndexScheduler.java
+++ java/com/google/gerrit/server/index/scheduler/PeriodicIndexScheduler.java
@@ -16,6 +16,7 @@ package com.google.gerrit.server.index.scheduler;
 
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.index.IndexDefinition;
 import com.google.gerrit.lifecycle.LifecycleModule;
 import com.google.gerrit.server.config.GerritIsReplica;
 import com.google.gerrit.server.config.GerritServerConfig;
@@ -23,7 +24,11 @@ import com.google.gerrit.server.config.ScheduleConfig;
 import com.google.gerrit.server.config.ScheduleConfig.Schedule;
 import com.google.gerrit.server.git.WorkQueue;
 import com.google.gerrit.server.group.PeriodicGroupIndexer;
+import com.google.gerrit.server.project.PeriodicProjectIndexer;
 import com.google.inject.Inject;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.eclipse.jgit.lib.Config;
@@ -40,32 +45,50 @@ public class PeriodicIndexScheduler implements LifecycleListener {
 
   private final Config cfg;
   private final WorkQueue queue;
-  private final PeriodicGroupIndexer groupIndexer;
+  private final Map<String, Runnable> indexers = new HashMap<>();
   private final boolean isReplica;
+  private final Collection<IndexDefinition<?, ?, ?>> defs;
 
   @Inject
   PeriodicIndexScheduler(
       @GerritServerConfig Config cfg,
       WorkQueue queue,
       PeriodicGroupIndexer groupIndexer,
-      @GerritIsReplica boolean isReplica) {
+      PeriodicProjectIndexer projectIndexer,
+      @GerritIsReplica boolean isReplica,
+      Collection<IndexDefinition<?, ?, ?>> defs) {
     this.cfg = cfg;
     this.queue = queue;
-    this.groupIndexer = groupIndexer;
+    indexers.put("groups", groupIndexer);
+    indexers.put("projects", projectIndexer);
     this.isReplica = isReplica;
+    this.defs = defs;
   }
 
   @Override
   public void start() {
-    Subsection s = determineConfigSubsection();
-    boolean runOnStartup = cfg.getBoolean(s.section, s.subsection, "runOnStartup", isReplica);
+    for (IndexDefinition<?, ?, ?> def : defs) {
+      start(def.getName());
+    }
+  }
+
+  private void start(String indexName) {
+    Runnable indexer = indexers.get(indexName);
+    if (indexer == null) {
+      logger.atFine().log("Periodic reindexig for %s not supported", indexName);
+      return;
+    }
+    Subsection s = determineConfigSubsection(indexName);
+    boolean runOnStartup =
+        cfg.getBoolean(s.section, s.subsection, "runOnStartup", defaultRunOnStartup(indexName));
     if (runOnStartup) {
-      groupIndexer.run();
+      indexer.run();
     }
 
-    boolean isEnabled = cfg.getBoolean(s.section, s.subsection, "enabled", isReplica);
+    boolean isEnabled =
+        cfg.getBoolean(s.section, s.subsection, "enabled", defaultIsEnabled(indexName));
     if (!isEnabled) {
-      logger.atWarning().log("index.scheduledIndexer is disabled");
+      logger.atWarning().log("index.scheduledIndexer for %s is disabled", indexName);
       return;
     }
 
@@ -74,15 +97,30 @@ public class PeriodicIndexScheduler implements LifecycleListener {
             .setSubsection(s.subsection)
             .buildSchedule()
             .orElseGet(() -> Schedule.createOrFail(TimeUnit.MINUTES.toMillis(5), "00:00"));
-    queue.scheduleAtFixedRate(groupIndexer, schedule);
+    queue.scheduleAtFixedRate(indexer, schedule);
   }
 
-  private Subsection determineConfigSubsection() {
+  private Subsection determineConfigSubsection(String indexName) {
+    if (!"groups".equals(indexName)) {
+      return new Subsection("scheduledIndexer", indexName);
+    }
     Set<String> scheduledIndexerConfig = cfg.getSubsections("scheduledIndexer");
     if (scheduledIndexerConfig.contains("groups")) {
       return new Subsection("scheduledIndexer", "groups");
     }
     return new Subsection("index", "scheduledIndexer");
+  }
+
+  private boolean defaultRunOnStartup(String indexName) {
+    return isGroupsIndexOnReplica(indexName);
+  }
+
+  private boolean defaultIsEnabled(String indexName) {
+    return isGroupsIndexOnReplica(indexName);
+  }
+
+  private boolean isGroupsIndexOnReplica(String indexName) {
+    return "groups".equals(indexName) && isReplica;
   }
 
   private static class Subsection {

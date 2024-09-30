8ff4aec1795dbb13a3a214051c167db53fd339de
Thomas Dräbing <thomas.draebing@sap.com>
2024-09-30 09:06:53 +0200

    Add CLI for maintenance task in Gerrit site
    The k8sgerrit project requires and provided a set of scripts to
    automate the run of some maintenance tasks for Gerrit sites.
    A recent effort started to consolidate these scripts into a single
    CLI tool written in python to improve maintainability and usability.
    Since this tool's usefulness would not be limited to running
    Gerrit on Kubernetes, this change moves the effort to the gerrit
    project.
    
    Change-Id: I91f71f05cad2ae6a945c8722f3c9859a7ba2fe4f

---
+++ contrib/maintenance/gerrit-maintenance.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+# Copyright (C) 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import logging
+import sys
+
+import cli.gc
+
+from gerrit.site import Site
+from gerrit.tasks.gc import BatchGitGarbageGollection
+from git.gc import GitGarbageCollectionProvider
+
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+
+
+def _run_projects_gc(args):
+    site = Site(args[0].site)
+    projects = (
+        args[0].projects
+        if args[0].projects
+        else site.get_projects(args[0].skip_projects)
+    )
+    BatchGitGarbageGollection(
+        site,
+        projects,
+        GitGarbageCollectionProvider.get(args[0].pack_refs, args[0].config),
+    ).run(args[1])
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--site",
+        help="Path to Gerrit site",
+        dest="site",
+        action="store",
+        default="/var/gerrit",
+    )
+    parser.set_defaults(func=lambda x: parser.print_usage())
+
+    subparsers = parser.add_subparsers()
+
+    parser_projects = subparsers.add_parser(
+        "projects",
+        help="Tools for working with Gerrit projects.",
+    )
+    parser_projects.add_argument(
+        "-p",
+        "--project",
+        help=(
+            "Which project to gc. Can be used multiple times. If not given, all "
+            "attrs=projects (except for `--skipped` ones) will be gc'ed."
+        ),
+        dest="projects",
+        action="append",
+        default=[],
+    )
+    parser_projects.add_argument(
+        "-s",
+        "--skip",
+        help="Which project to skip. Can be used multiple times.",
+        dest="skip_projects",
+        action="append",
+        default=[],
+    )
+    parser_projects.set_defaults(func=lambda x: parser_projects.print_usage())
+
+    subparsers_projects = parser_projects.add_subparsers()
+    parser_projects_gc = subparsers_projects.add_parser(
+        "gc",
+        prog=cli.gc.PROG,
+        description=cli.gc.DESCRIPTION,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    cli.gc.add_arguments(parser_projects_gc)
+    parser_projects_gc.add_argument(
+        "-c",
+        "--config",
+        help="Git config options to apply.",
+        dest="config",
+        action="append",
+        default=[],
+    )
+    parser_projects_gc.set_defaults(func=_run_projects_gc)
+
+    args = parser.parse_known_args()
+    args[0].func(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ contrib/maintenance/gerrit/tasks/gc.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os.path
+import sys
+
+sys.path.append("../..")
+
+from git.repo import GIT_SUFFIX
+
+
+class BatchGitGarbageGollection:
+    def __init__(self, site, projects, gc_runner):
+        self.site = site
+        self.projects = projects
+        self.gc_runner = gc_runner
+
+    def run(self, gc_args):
+        base_path = self.site.get_base_path()
+        for project in self.projects:
+            self.gc_runner.run(os.path.join(base_path, project + GIT_SUFFIX), gc_args)

9a8372bad89b9ccae689df4a69715b675690403c
Nasser Grainawi <nasser.grainawi@linaro.org>
2024-11-01 16:50:41 -0600

    Email: Support the Microsoft 'Thread-Index' header
    Microsoft clients don't use the standard In-Reply-To or References
    headers and instead have their own Thread-Index header [1]. Plenty of
    Gerrit users are using those clients, so default to including this
    header, but add a config for admins to disable it.
    
    [1] https://bugzilla.mozilla.org/show_bug.cgi?id=411601
    
    Change-Id: I4cc326605158553f569506710a71dfe01f24bd94
    Release-Notes: Fixed change email threading in Microsoft clients

--- java/com/google/gerrit/server/mail/EmailSettings.java
+++ java/com/google/gerrit/server/mail/EmailSettings.java
@@ -38,6 +38,7 @@ public class EmailSettings {
   public final Encryption encryption;
   public final long fetchInterval; // in milliseconds
   public final boolean sendNewPatchsetEmails;
+  public final boolean includeThreadIndexHeader;
 
   @Inject
   EmailSettings(@GerritServerConfig Config cfg) {
@@ -60,5 +61,6 @@ public class EmailSettings {
             TimeUnit.MILLISECONDS.convert(60, TimeUnit.SECONDS),
             TimeUnit.MILLISECONDS);
     sendNewPatchsetEmails = cfg.getBoolean("change", null, "sendNewPatchsetEmails", true);
+    includeThreadIndexHeader = cfg.getBoolean(SEND_EMAIL, null, "includeThreadIndexHeader", true);
   }
 }

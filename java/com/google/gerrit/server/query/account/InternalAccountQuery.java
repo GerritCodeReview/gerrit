c513a112b1fe666fd9ce17ef1c62d0c2cd47e8bf
Fabio Ponciroli <ponch78@gmail.com>
2024-11-05 16:13:12 +0100

    Add configuration for case-insensitive email matching in Account API
    Emails are inherently case-insensitive, but the current Account API
    treats them as case-sensitive.
    
    As a result, looking up an account by an email with different casing
    (e.g., example@domain.com vs. Example@Domain.com) results in a 404
    error, even if the email already exists with a different case.
    
    Introduce a configuration (`accounts.caseInsensitiveLocalPart`)
    to list domains of emails to treat as case-insensitive.
    
    When querying by email, the case sensitivity of the email local part
    depends on the domains specified in the this list.
    
    Users can register emails with mixed case, and if the email’s
    domain matches one in the configured list, the local part is treated as
    case-insensitive.
    
    This ensures that emails with different cases, such as User@mail.com
    and user@mail.com, are associated with the same user.
    For domains not listed in the configuration, email matching remains
    case-sensitive.
    
    This feature helps unify user assignments, ensuring that commit authors
    and change owners are correctly linked to the same account when emails
    match.
    
    Bug: Issue 377294481
    Release-Notes: Add configuration for case-insensitive email matching in Account API
    Change-Id: Id676e8b2cf0b3ed13c26583d959b2f5217d74ebe

--- java/com/google/gerrit/server/query/account/InternalAccountQuery.java
+++ java/com/google/gerrit/server/query/account/InternalAccountQuery.java
@@ -34,10 +34,13 @@ import com.google.gerrit.index.query.InternalQuery;
 import com.google.gerrit.server.account.AccountState;
 import com.google.gerrit.server.account.externalids.ExternalId;
 import com.google.gerrit.server.account.externalids.ExternalIdKeyFactory;
+import com.google.gerrit.server.config.AccountConfig;
 import com.google.gerrit.server.index.account.AccountField;
 import com.google.gerrit.server.index.account.AccountIndexCollection;
 import com.google.inject.Inject;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 /**
@@ -49,6 +52,7 @@ import java.util.Set;
 public class InternalAccountQuery extends InternalQuery<AccountState, InternalAccountQuery> {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
+  private final AccountConfig accountConfig;
   private final ExternalIdKeyFactory externalIdKeyFactory;
 
   @Inject
@@ -56,8 +60,11 @@ public class InternalAccountQuery extends InternalQuery<AccountState, InternalAc
       AccountQueryProcessor queryProcessor,
       AccountIndexCollection indexes,
       IndexConfig indexConfig,
-      ExternalIdKeyFactory externalIdKeyFactory) {
+      ExternalIdKeyFactory externalIdKeyFactory,
+      AccountConfig accountConfig) {
+
     super(queryProcessor, indexes, indexConfig);
+    this.accountConfig = accountConfig;
     this.externalIdKeyFactory = externalIdKeyFactory;
   }
 
@@ -95,27 +102,50 @@ public class InternalAccountQuery extends InternalQuery<AccountState, InternalAc
   }
 
   /**
-   * Queries for accounts that have a preferred email that exactly matches the given email.
+   * Queries for accounts that have a preferred email that matches the given email.
+   *
+   * <p>The local part of the email is compared either in a case-insensitive or case-sensitive
+   * manner, depending on the configuration parameter {@code accounts.caseInsensitiveLocalPart}.
+   * Check the configuration documentation for more details.
    *
    * @param email preferred email by which accounts should be found
    * @return list of accounts that have a preferred email that exactly matches the given email
    */
   public List<AccountState> byPreferredEmail(String email) {
+    String inputEmail =
+        Arrays.asList(accountConfig.getCaseInsensitiveLocalParts())
+                .contains(getLowerCaseEmailDomain(email))
+            ? email.toLowerCase(Locale.US)
+            : email;
     if (hasPreferredEmailExact()) {
-      return query(AccountPredicates.preferredEmailExact(email));
+      return query(AccountPredicates.preferredEmailExact(inputEmail));
     }
 
     if (!hasPreferredEmail()) {
       return ImmutableList.of();
     }
 
-    return query(AccountPredicates.preferredEmail(email)).stream()
-        .filter(a -> a.account().preferredEmail().equals(email))
+    return query(AccountPredicates.preferredEmail(inputEmail)).stream()
+        .filter(a -> a.account().preferredEmail().equals(inputEmail))
         .collect(toList());
   }
 
+  private String getLowerCaseEmailDomain(String email) {
+    String[] parts = email.split("@", 2);
+    // The caller method byPreferredEmail can be invoked with the local part
+    // of the email only. Handle this case by just returning it.
+    if (parts.length != 2) {
+      return email;
+    }
+    return parts[1].toLowerCase(Locale.US);
+  }
+
   /**
-   * Makes multiple queries for accounts by preferred email (exact match).
+   * Makes multiple queries for accounts by preferred email.
+   *
+   * <p>The local part of the email is compared either in a case-insensitive or case-sensitive
+   * manner, depending on the configuration parameter {@code accounts.caseInsensitiveLocalPart}.
+   * Check the configuration documentation for more details.
    *
    * @param emails preferred emails by which accounts should be found
    * @return multimap of the given emails to accounts that have a preferred email that exactly

ddbf22141e4caf6f879bb24274f0f15ecc86af12
Matthias Sohn <matthias.sohn@sap.com>
2024-11-11 11:22:46 +0100

    Add metrics for ssh requests
    - break them down by ssh command name
    - include aggregate counts and errors by exception type
    
    Release-Notes: Add metrics for ssh requests
    Change-Id: I92ad23c67284f8215b21f57345ba2051f9eb86ab

---
+++ java/com/google/gerrit/sshd/SshMetrics.java
@@ -0,0 +1,61 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.sshd;
+
+import com.google.gerrit.metrics.Counter1;
+import com.google.gerrit.metrics.Counter2;
+import com.google.gerrit.metrics.Description;
+import com.google.gerrit.metrics.Field;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.gerrit.server.RequestInfo;
+import com.google.gerrit.server.logging.Metadata;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class SshMetrics {
+  private final Counter1<String> count;
+  private final Counter2<String, String> errorCount;
+
+  @Inject
+  SshMetrics(MetricMaker metrics) {
+    this.count =
+        metrics.newCounter(
+            "ssh/count_success",
+            new Description("Count of successful ssh requests").setRate(),
+            Field.ofString("command_name", Metadata.Builder::requestType)
+                .description("The command name of the request.")
+                .build());
+
+    this.errorCount =
+        metrics.newCounter(
+            "ssh/count_failed",
+            new Description("Number of failed requests").setRate(),
+            Field.ofString("request_type", Metadata.Builder::requestType)
+                .description("The type of the failed request.")
+                .build(),
+            Field.ofString("exception", Metadata.Builder::restViewName)
+                .description("Exception that failed the request.")
+                .build());
+  }
+
+  public void countRequest(RequestInfo requestInfo, Throwable error) {
+    if (error == null) {
+      count.increment(requestInfo.commandName());
+    } else {
+      errorCount.increment(requestInfo.commandName(), error.getClass().getSimpleName());
+    }
+  }
+}

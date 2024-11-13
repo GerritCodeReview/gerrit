661142fa86cbfca14ecd317e434f4d551308ab8c
Saša Živkov <sasa.zivkov@sap.com>
2024-11-13 17:24:19 +0100

    Assign TRACE_ID to every request
    The request tracing option does two things:
    * assign TRACE_ID to the tracing context
    * force logging for all loggers regardless of the used logging level
    
    This change decouples this two decisions and makes it possible to
    assign TRACE_ID without forced logging.
    
    Assigning TRACE_ID to the logging context of every request is
    universally useful, regardless if user initiated tracing or not. It
    helps us relate log entries easily by filtering by trace-id. The need
    for matching log entries by timestamps should be largely eliminated
    with this feature.
    
    The old behaviour is fully preserved: if client explicitly specifies
    tracing option then the logging is forced.
    
    Release-Notes: Assign TRACE_ID for every request
    Change-Id: I1aa4f289a0e4dc05e637dd60361b870d5eff25f8

--- java/com/google/gerrit/server/logging/TraceContext.java
+++ java/com/google/gerrit/server/logging/TraceContext.java
@@ -118,7 +118,7 @@ public class TraceContext implements AutoCloseable {
    *
    * <p>No-op if {@code trace} is {@code false}.
    *
-   * @param trace whether tracing should be started
+   * @param forceLogging whether logging should be forced
    * @param traceId trace ID that should be used for tracing, if {@code null} a trace ID is
    *     generated
    * @param traceIdConsumer consumer for the trace ID, should be used to return the generated trace
@@ -126,29 +126,23 @@ public class TraceContext implements AutoCloseable {
    * @return the trace context
    */
   public static TraceContext newTrace(
-      boolean trace, @Nullable String traceId, TraceIdConsumer traceIdConsumer) {
-    if (!trace) {
-      // Create an empty trace context.
-      return open();
-    }
-
+      boolean forceLogging, @Nullable String traceId, TraceIdConsumer traceIdConsumer) {
+    String effectiveId;
     if (!Strings.isNullOrEmpty(traceId)) {
-      traceIdConsumer.accept(RequestId.Type.TRACE_ID.name(), traceId);
-      return open().addTag(RequestId.Type.TRACE_ID, traceId).forceLogging();
+      effectiveId = traceId;
+    } else {
+      Optional<String> existingTraceId =
+          LoggingContext.getInstance().getTagsAsMap().get(RequestId.Type.TRACE_ID.name()).stream()
+              .findAny();
+      effectiveId = existingTraceId.orElse(new RequestId().toString());
     }
 
-    Optional<String> existingTraceId =
-        LoggingContext.getInstance().getTagsAsMap().get(RequestId.Type.TRACE_ID.name()).stream()
-            .findAny();
-    if (existingTraceId.isPresent()) {
-      // request tracing was already started, no need to generate a new trace ID
-      traceIdConsumer.accept(RequestId.Type.TRACE_ID.name(), existingTraceId.get());
-      return open();
+    traceIdConsumer.accept(RequestId.Type.TRACE_ID.name(), effectiveId);
+    TraceContext traceContext = open().addTag(RequestId.Type.TRACE_ID, effectiveId);
+    if (forceLogging) {
+      return traceContext.forceLogging();
     }
-
-    RequestId newTraceId = new RequestId();
-    traceIdConsumer.accept(RequestId.Type.TRACE_ID.name(), newTraceId.toString());
-    return open().addTag(RequestId.Type.TRACE_ID, newTraceId).forceLogging();
+    return traceContext;
   }
 
   @FunctionalInterface

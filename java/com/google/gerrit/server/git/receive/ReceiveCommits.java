b2c68089a3993a813e2c3d944d5cfbcc66fb4591
Saša Živkov <sasa.zivkov@sap.com>
2024-11-13 17:24:19 +0100

    Assign TRACE_ID to every request
    The request tracing option does two things:
    * assign TRACE_ID to the tracing context
    * force logging for all loggers regardless of the used logging level
    
    This change decouples this two decisions and makes it possible to
    assign TRACE_ID without forced logging.
    
    Assigning TRACE_ID to the logging context of every request is
    universally useful, regardless if user initiated tracing or not. It
    helps us relate log entries easily by filtering by trace-id. The need
    for matching log entries by timestamps should be largely eliminated
    with this feature.
    
    The old behaviour is fully preserved: if client explicitly specifies
    tracing option then the logging is forced.
    
    Release-Notes: Assign TRACE_ID for every request
    Change-Id: I1aa4f289a0e4dc05e637dd60361b870d5eff25f8

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -470,7 +470,7 @@ class ReceiveCommits {
   private boolean newChangeForAllNotInTarget;
   private boolean setChangeAsPrivate;
   private Optional<NoteDbPushOption> noteDbPushOption;
-  private Optional<String> tracePushOption;
+  private Optional<String> tracePushOption = Optional.empty();
 
   private MessageSender messageSender;
   private ReceiveCommitsResult.Builder result;
@@ -678,7 +678,7 @@ class ReceiveCommits {
   void sendMessages() {
     try (TraceContext traceContext =
         TraceContext.newTrace(
-            loggingTags.containsKey(RequestId.Type.TRACE_ID.name()),
+            tracePushOption.isPresent(),
             loggingTags.get(RequestId.Type.TRACE_ID.name()),
             (tagName, traceId) -> {})) {
       loggingTags.forEach((tagName, tagValue) -> traceContext.addTag(tagName, tagValue));
@@ -705,7 +705,11 @@ class ReceiveCommits {
             TraceContext.newTrace(
                 tracePushOption.isPresent(),
                 tracePushOption.orElse(null),
-                (tagName, traceId) -> addMessage(tagName + ": " + traceId));
+                (tagName, traceId) -> {
+                  if (tracePushOption.isPresent()) {
+                    addMessage(tagName + ": " + traceId);
+                  }
+                });
         PerformanceLogContext performanceLogContext =
             new PerformanceLogContext(config, performanceLoggers);
         TraceTimer traceTimer =
@@ -1368,8 +1372,6 @@ class ReceiveCommits {
     List<String> traceValues = pushOptions.get("trace");
     if (!traceValues.isEmpty()) {
       tracePushOption = Optional.of(Iterables.getLast(traceValues));
-    } else {
-      tracePushOption = Optional.empty();
     }
   }
 

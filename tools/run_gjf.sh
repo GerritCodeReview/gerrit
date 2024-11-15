1029347e609b6c24eddcdf61e55fe7ad56f9a3c5
Sven Selberg <svense@axis.com>
2024-11-15 15:18:30 +0100

    Consolidate setup_gjf.sh and run_gjf.sh into gjf.sh
    Relay calls to deprecated scripts to new gjf.sh.
    
    Release-Notes: Replace setup_gjf.sh and run_gjf.sh with one script gjf.sh
    Change-Id: I0c2192b730313f948615d2c83889a43e50cfdd92

--- tools/run_gjf.sh
+++ tools/run_gjf.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
 #
-# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,13 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eu
+NEW_SCRIPT=$(dirname $0)/gjf.sh
+VERSION=${1:-""}
 
-GJF_VERSION=$(grep -o "^VERSION=.*$" tools/setup_gjf.sh | grep -o '[0-9][0-9]*\.[0-9][0-9]*[\.0-9]*')
-GJF="tools/format/google-java-format-$GJF_VERSION"
-if [ ! -f "$GJF" ]; then
-  tools/setup_gjf.sh
-  GJF=$(find 'tools/format' -regex '.*/google-java-format-[0-9][0-9]*\.[0-9][0-9]*')
-fi
-echo 'Running google-java-format check...'
-git show --diff-filter=AM --name-only --pretty="" HEAD | grep java$ | xargs $GJF -r
+echo "Calling $0 is deprecated, call \"$NEW_SCRIPT run $VERSION\" instead".
+
+$NEW_SCRIPT run $VERSION

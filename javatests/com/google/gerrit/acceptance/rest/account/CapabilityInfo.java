c29bd30a69196ab93f5bcac76fc880f764367d12
Prakash Aswal <prakash.aswal@linaro.org>
2024-11-18 16:38:05 +0530

    Allow restricting users from deleting their own accounts
    Define a new global capability 'Delete Own Account' that can be used by admins to restrict registered users from deleting their own accounts in
    managed environments.
    
    The default behavior, which allows users to delete their own accounts,
    remains unchanged.
    
    Release-Notes: skip
    Change-Id: I0dbe10382b24b2c04e298fc91cd14626d062e668

--- javatests/com/google/gerrit/acceptance/rest/account/CapabilityInfo.java
+++ javatests/com/google/gerrit/acceptance/rest/account/CapabilityInfo.java
@@ -22,6 +22,7 @@ class CapabilityInfo {
   public boolean createGroup;
   public boolean deleteGroup;
   public boolean createProject;
+  public boolean deleteOwnAccount;
   public boolean emailReviewers;
   public boolean flushCaches;
   public boolean killTask;

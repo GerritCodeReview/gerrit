2c6dcb18be91a56f93153b6e0155530a0d9d8aab
Yash Chaturvedi <zeref@qti.qualcomm.com>
2024-11-19 11:15:02 +0000

    Fix save button visibility for repo access UI
    Before this change, `ownerOf` property was being used to determine
    whether to display save button on the repo access page or not. The
    `ownerOf` property does not take into account the owners defined in the
    parent projects. Use `isOwner` property to fix this behavior.
    
    Change-Id: Ifb8c4db0c8a0eeb4f533ad98cb7ff4ba9f35aed8
    Release-Notes: skip

--- polygerrit-ui/app/elements/admin/gr-repo-access/gr-repo-access_test.ts
+++ polygerrit-ui/app/elements/admin/gr-repo-access/gr-repo-access_test.ts
@@ -259,13 +259,13 @@ suite('gr-repo-access tests', () => {
   });
 
   test('computeMainClass', () => {
-    element.ownerOf = ['refs/*'] as GitRef[];
+    element.isOwner = true;
     element.editing = false;
     element.canUpload = false;
     assert.equal(element.computeMainClass(), 'admin');
     element.editing = true;
     assert.equal(element.computeMainClass(), 'admin editing');
-    element.ownerOf = [];
+    element.isOwner = false;
     element.editing = false;
     assert.equal(element.computeMainClass(), '');
     element.editing = true;
@@ -516,13 +516,13 @@ suite('gr-repo-access tests', () => {
     });
 
     test('button visibility for ref owner', async () => {
-      element.ownerOf = ['refs/for/*'] as GitRef[];
+      element.isOwner = true;
       await element.updateComplete;
       testEditSaveCancelBtns(true, false);
     });
 
     test('button visibility for ref owner and upload', async () => {
-      element.ownerOf = ['refs/for/*'] as GitRef[];
+      element.isOwner = true;
       element.canUpload = true;
       await element.updateComplete;
       testEditSaveCancelBtns(true, false);

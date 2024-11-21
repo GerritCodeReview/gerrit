7a974f41e4d258cdae5bc70d096863224f92daf1
Nitzan Gur-Furman <nitzan@google.com>
2024-11-21 12:37:17 +0000

    Make AccountMergeValidator a plugin validator
    Since Google is moving the account storage away from NoteDb, this
    validator should not run anymore. Making it a plugin validator allows
    easy conditional bindings.
    
    Note: This change removes AccountMergeValidator.Factory.
    The factory doesn't have any logic. In addition, no state is saved
    during the validation, and therefore there is no need in creating a new
    instance for each method invocation. The merge-validators factory
    pattern goes all the way back to
    72e898377a21e5730ff58bba6af26c3efe05056a (for ProjectConfigValidator),
    but no explanation was given there.
    
    Release-Notes: skip
    Google-Bug-Id: b/289357020
    Change-Id: I05b527e7a02db03fe2ded38448e7588da4ecc201

--- java/com/google/gerrit/server/account/storage/notedb/AccountNoteDbWriteStorageModule.java
+++ java/com/google/gerrit/server/account/storage/notedb/AccountNoteDbWriteStorageModule.java
@@ -19,8 +19,20 @@ import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.server.account.AccountsUpdate;
 import com.google.gerrit.server.account.externalids.storage.notedb.ExternalIdNoteDbWriteStorageModule;
 import com.google.gerrit.server.account.storage.notedb.validators.AccountCommitValidator;
+<<<<<<< PATCH SET (47db64 Make AccountMergeValidator a plugin validator)
+<<<<<<< PATCH SET (e2ae5b Make AccountMergeValidator a plugin validator)
+import com.google.gerrit.server.account.storage.notedb.validators.AccountMergeValidator;
 import com.google.gerrit.server.account.storage.notedb.validators.ExternalIdUpdateValidator;
+||||||| BASE
+import com.google.gerrit.server.account.storage.notedb.validators.ExternalIdUpdateValidator;
+=======
+>>>>>>> BASE      (4c2281 Make AccountCommitValidator a plugin validator)
+||||||| BASE
+=======
+import com.google.gerrit.server.account.storage.notedb.validators.ExternalIdUpdateValidator;
+>>>>>>> BASE      (d2eecf Merge "Make ExternalIdUpdateListener a plugin validator and )
 import com.google.gerrit.server.git.validators.CommitValidationListener;
+import com.google.gerrit.server.git.validators.MergeValidationListener;
 import com.google.gerrit.server.index.account.ReindexAccountsAfterRefUpdate;
 import com.google.inject.AbstractModule;
 
@@ -40,6 +52,17 @@ public class AccountNoteDbWriteStorageModule extends AbstractModule {
 
     // Validators
     DynamicSet.bind(binder(), CommitValidationListener.class).to(AccountCommitValidator.class);
+<<<<<<< PATCH SET (47db64 Make AccountMergeValidator a plugin validator)
+<<<<<<< PATCH SET (e2ae5b Make AccountMergeValidator a plugin validator)
     DynamicSet.bind(binder(), CommitValidationListener.class).to(ExternalIdUpdateValidator.class);
+    DynamicSet.bind(binder(), MergeValidationListener.class).to(AccountMergeValidator.class);
+||||||| BASE
+    DynamicSet.bind(binder(), CommitValidationListener.class).to(ExternalIdUpdateValidator.class);
+=======
+>>>>>>> BASE      (4c2281 Make AccountCommitValidator a plugin validator)
+||||||| BASE
+=======
+    DynamicSet.bind(binder(), CommitValidationListener.class).to(ExternalIdUpdateValidator.class);
+>>>>>>> BASE      (d2eecf Merge "Make ExternalIdUpdateListener a plugin validator and )
   }
 }

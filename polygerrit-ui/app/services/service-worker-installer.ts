1290633cb22716c39f9a34ac3d586ead9ad44d87
Paladox none <thomasmulhall410@yahoo.com>
2024-12-06 14:23:51 +0000

    Fix "undefined is not an object (evaluating 'navigator.serviceWorker.controller')"
    Release-Notes: Fix "undefined is not an object (evaluating 'navigator.serviceWorker.controller')"
    Change-Id: Ica02f1f9ec45c840e6130d80f1615303613324e5
    (cherry picked from commit 19ffcbb591b2acc3140347d8cb1d823c90e714e7)

--- polygerrit-ui/app/services/service-worker-installer.ts
+++ polygerrit-ui/app/services/service-worker-installer.ts
@@ -80,6 +80,7 @@ export class ServiceWorkerInstaller extends Model<ServiceWorkerInstallerState> {
       ) {
         this.allowBrowserNotificationsPreference =
           prefs.allow_browser_notifications;
+<<<<<<< HEAD   (0b5bc8 Merge branch 'stable-3.9' into stable-3.10)
         // flag can disable notifications similar to user setting
         navigator.serviceWorker.controller?.postMessage({
           type: ServiceWorkerMessageType.USER_PREFERENCE_CHANGE,
@@ -87,6 +88,23 @@ export class ServiceWorkerInstaller extends Model<ServiceWorkerInstallerState> {
             this.allowBrowserNotificationsPreference &&
             this.flagsService.isEnabled(KnownExperimentId.PUSH_NOTIFICATIONS),
         });
+||||||| BASE
+        // flag can disable notifications similar to user setting
+        navigator.serviceWorker.controller?.postMessage({
+          type: ServiceWorkerMessageType.USER_PREFERENCE_CHANGE,
+          allowBrowserNotificationsPreference:
+            this.allowBrowserNotificationsPreference,
+        });
+=======
+        if (this.initialized) {
+          // flag can disable notifications similar to user setting
+          navigator.serviceWorker.controller?.postMessage({
+            type: ServiceWorkerMessageType.USER_PREFERENCE_CHANGE,
+            allowBrowserNotificationsPreference:
+              this.allowBrowserNotificationsPreference,
+          });
+        }
+>>>>>>> CHANGE (19ffcb Fix "undefined is not an object (evaluating 'navigator.servi)
       }
     });
     Promise.all([

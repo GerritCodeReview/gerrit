f946f75f1b7d954a55475ec83b32b1619cb06907
Saša Živkov <sasa.zivkov@sap.com>
2024-12-20 12:10:38 +0100

    Remove the "running offline" concept from the SqlStore class
    The notion of "running offline" shouldn't proliferate inside the
    server/cache classes. This change removes it from the SqlStore class by
    renaming the isOfflineReindex flag into the maintainLastAccess, which is
    a concept that matches the level of abstraction of the SqlStore class.
    
    Change-Id: I3164f8812d1279a0a27db23734fc5a828b66a3a9

--- java/com/google/gerrit/server/cache/h2/H2CacheImpl.java
+++ java/com/google/gerrit/server/cache/h2/H2CacheImpl.java
@@ -359,7 +359,7 @@ public class H2CacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
     private volatile BloomFilter<K> bloomFilter;
     private int estimatedSize;
     private boolean buildBloomFilter;
-    private boolean isOfflineReindex;
+    private boolean maintainLastAccess;
 
     SqlStore(
         String jdbcUrl,
@@ -371,7 +371,7 @@ public class H2CacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
         @Nullable Duration expireAfterWrite,
         @Nullable Duration refreshAfterWrite,
         boolean buildBloomFilter,
-        boolean isOfflineReindex) {
+        boolean maintainLastAccess) {
       this.url = jdbcUrl;
       this.keyType = createKeyType(keyType, keySerializer);
       this.valueSerializer = valueSerializer;
@@ -380,7 +380,7 @@ public class H2CacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
       this.expireAfterWrite = expireAfterWrite;
       this.refreshAfterWrite = refreshAfterWrite;
       this.buildBloomFilter = buildBloomFilter;
-      this.isOfflineReindex = isOfflineReindex;
+      this.maintainLastAccess = maintainLastAccess;
 
       int cores = Runtime.getRuntime().availableProcessors();
       int keep = Math.min(cores, 16);
@@ -504,7 +504,7 @@ public class H2CacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
           ValueHolder<V> h = new ValueHolder<>(val, created.toInstant());
           h.clean = true;
           hitCount.incrementAndGet();
-          if (!isOfflineReindex) {
+          if (maintainLastAccess) {
             touch(c, key);
           }
           return h;

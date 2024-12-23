b6b365408cfab732067e2e27d719d64f1283eb20
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2024-12-23 17:51:20 +0100

    Provide new metrics for account, change, group and project indexes
    Introduce four new Gauge metrics to track the number of documents
    in the respective search indexes:
    
    * indexes/accounts
    * indexes/changes
    * indexes/groups
    * indexes/projects
    
    These metrics are particularly useful in multi-site scenarios, as they
    provide visibility into the number of documents across different
    indexes, aiding in the diagnosis of potential indexing issues.
    
    Release-Notes: Provide index metric for accounts/changes/groups/projects
    Bug: Issue 381216361
    Change-Id: If7ed9c5153f0915a25cbc2e91d28ba5041b942ab

--- java/com/google/gerrit/server/index/group/GroupIndexCollection.java
+++ java/com/google/gerrit/server/index/group/GroupIndexCollection.java
@@ -18,12 +18,22 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.gerrit.entities.AccountGroup;
 import com.google.gerrit.entities.InternalGroup;
 import com.google.gerrit.index.IndexCollection;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 /** Collection of active group indices. See {@link IndexCollection} for details on collections. */
 @Singleton
 public class GroupIndexCollection
     extends IndexCollection<AccountGroup.UUID, InternalGroup, GroupIndex> {
+  @Inject
   @VisibleForTesting
-  public GroupIndexCollection() {}
+  public GroupIndexCollection(MetricMaker metrics) {
+    super(metrics);
+  }
+
+  @Override
+  protected IndexType getIndexName() {
+    return IndexType.GROUPS;
+  }
 }

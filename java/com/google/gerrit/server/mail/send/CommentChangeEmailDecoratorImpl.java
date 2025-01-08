7fae488bbd2872e76df7832d56eee9fd8dfbea8e
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2025-01-08 12:46:47 +0000

    Fix the email notification for comments in unchanged files
    The issue occurred when replying to comments on unchanged files. The
    email notification failed to identify unchanged files, resulting in
    the exception `org.eclipse.jgit.errors.MissingObjectException` and
    malformed email notifications. The new logic ensures proper
    identification of unchanged files and corrects the email notification
    formatting.
    
    Bug: Issue 387440281
    Change-Id: I095e307abf067392e8ad2efbbff010d30719f481

--- java/com/google/gerrit/server/mail/send/CommentChangeEmailDecoratorImpl.java
+++ java/com/google/gerrit/server/mail/send/CommentChangeEmailDecoratorImpl.java
@@ -219,7 +219,9 @@ public class CommentChangeEmailDecoratorImpl implements CommentChangeEmailDecora
         groups.add(currentGroup);
         if (modifiedFiles != null && !modifiedFiles.isEmpty()) {
           try {
-            currentGroup.fileData = new PatchFile(repo, modifiedFiles, c.key.filename);
+            currentGroup.fileData =
+                new PatchFile(
+                    repo, modifiedFiles, c.key.filename, changeEmail.getPatchSet().commitId());
           } catch (IOException e) {
             logger.atWarning().withCause(e).log(
                 "Cannot load %s from %s in %s",

83021fbd5758570d26cbdf18edb57b8c61693c2d
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2025-01-08 12:46:47 +0000

    Fix the email notification for comments in unchanged files
    The issue occurred when replying to comments on unchanged files. The
    email notification failed to identify unchanged files, resulting in
    the exception `org.eclipse.jgit.errors.MissingObjectException` and
    malformed email notifications. The new logic ensures proper
    identification of unchanged files and corrects the email notification
    formatting.
    
    Release-Notes: Fix the email notification for comments in unchanged files. Breaking change.
    Bug: Issue 387440281
    Change-Id: I095e307abf067392e8ad2efbbff010d30719f481

--- java/com/google/gerrit/server/patch/PatchFile.java
+++ java/com/google/gerrit/server/patch/PatchFile.java
@@ -53,7 +53,11 @@ public class PatchFile {
   private Text a;
   private Text b;
 
-  public PatchFile(Repository repo, Map<String, FileDiffOutput> modifiedFiles, String fileName)
+  public PatchFile(
+      Repository repo,
+      Map<String, FileDiffOutput> modifiedFiles,
+      String fileName,
+      ObjectId patchSetCommitId)
       throws IOException {
     this.repo = repo;
     this.diff =
@@ -61,7 +65,7 @@ public class PatchFile {
             .filter(f -> f.getKey().equals(fileName))
             .map(Map.Entry::getValue)
             .findFirst()
-            .orElseGet(() -> FileDiffOutput.empty(fileName, ObjectId.zeroId(), ObjectId.zeroId()));
+            .orElseGet(() -> FileDiffOutput.empty(fileName, patchSetCommitId, patchSetCommitId));
 
     if (Patch.PATCHSET_LEVEL.equals(fileName)) {
       aTree = null;

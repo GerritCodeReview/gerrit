aff5817836778289dbf3621cf38855e8e8240779
Dhruv Srivastava <dhruvsri@google.com>
2025-01-15 15:06:32 +0100

    Add REST API for fetching plugin validation options
    The plugin validation options provide options to the user for creating
    new patchsets(when creating a new change/revert).
    We want to show these options to users when they create new patchsets
    via the UI(Rebase/Revert)
    
    WANT_LGTM=all
    Release-Notes: skip
    Google-bug-id:b/388779000
    Change-Id: I6f8954946eedcab6c70288ae28876665f0ffd9f0

---
+++ java/com/google/gerrit/server/plugins/GetPluginPushOptions.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.plugins;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.gerrit.extensions.common.PluginPushOptionsInfo;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.registration.Extension;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.git.receive.PluginPushOption;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.util.stream.StreamSupport;
+
+@Singleton
+public class GetPluginPushOptions implements RestReadView<PluginResource> {
+  private final DynamicSet<PluginPushOption> pluginPushOptions;
+
+  @Inject
+  GetPluginPushOptions(DynamicSet<PluginPushOption> pluginPushOption) {
+    this.pluginPushOptions = pluginPushOption;
+  }
+
+  @Override
+  public Response<PluginPushOptionsInfo> apply(PluginResource resource) {
+    return Response.ok(
+        new PluginPushOptionsInfo(
+            new StreamSupport.stream(
+                    this.pluginPushOptions.entries().spliterator(), /* parallel= */ false)
+                .map(Extension<PluginPushOption>::get)
+                .collect(toImmutableList())));
+  }
+}

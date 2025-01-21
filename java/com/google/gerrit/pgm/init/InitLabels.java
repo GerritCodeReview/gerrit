17477f804d83dc15d01c401997c251584f67d73d
Daniele Sassoli <danielesassoli@gmail.com>
2025-01-21 14:39:04 +0000

    Set default label function to NoBlock
    Label functions where officially deprecated with I25cd93c1, I2faf081ce
    went a step further and banned updating functions to blocking values,
    however, the default Code-Review label function was never updated.
    
    At the same time, add a default submit requirements to effectively
    replace the now removed label function.
    
    Release-Notes: Set default label function to NoBlock
    Change-Id: I428205bc4828f5eb5ec0a5251a7f1aabc3c37062

--- java/com/google/gerrit/pgm/init/InitLabels.java
+++ java/com/google/gerrit/pgm/init/InitLabels.java
@@ -14,26 +14,49 @@
 
 package com.google.gerrit.pgm.init;
 
-import static com.google.gerrit.entities.LabelFunction.MAX_WITH_BLOCK;
+import static com.google.gerrit.entities.LabelId.VERIFIED;
+import static com.google.gerrit.server.schema.AclUtil.grant;
 
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.entities.AccessSection;
+import com.google.gerrit.entities.GroupReference;
+import com.google.gerrit.entities.LabelType;
+import com.google.gerrit.entities.LabelValue;
+import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.entities.SubmitRequirement;
+import com.google.gerrit.entities.SubmitRequirementExpression;
 import com.google.gerrit.pgm.init.api.AllProjectsConfig;
 import com.google.gerrit.pgm.init.api.ConsoleUI;
 import com.google.gerrit.pgm.init.api.InitStep;
+import com.google.gerrit.server.GerritPersonIdent;
+import com.google.gerrit.server.config.AllProjectsName;
+import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.meta.MetaDataUpdate;
+import com.google.gerrit.server.group.SystemGroupBackend;
+import com.google.gerrit.server.project.ProjectConfig;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import java.util.Arrays;
+import java.io.IOException;
+import java.util.Optional;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
 
 @Singleton
 public class InitLabels implements InitStep {
-  private static final String KEY_COPY_CONDITION = "copyCondition";
   private static final String KEY_LABEL = "label";
-  private static final String KEY_FUNCTION = "function";
-  private static final String KEY_VALUE = "value";
-  private static final String LABEL_VERIFIED = "Verified";
 
   private final ConsoleUI ui;
   private final AllProjectsConfig allProjectsConfig;
+  private GitRepositoryManager repositoryManager;
+  private AllProjectsName allProjectsName;
+  private PersonIdent serverUser;
+  private ProjectConfig.Factory projectConfigFactory;
+  private SystemGroupBackend systemGroupBackend;
 
   private boolean installVerified;
 
@@ -43,10 +66,35 @@ public class InitLabels implements InitStep {
     this.allProjectsConfig = allProjectsConfig;
   }
 
+  @Inject
+  void setGitRepositoryManager(@Nullable GitRepositoryManager repositoryManager) {
+    this.repositoryManager = repositoryManager;
+  }
+
+  @Inject(optional = true)
+  void setAllProjectsName(AllProjectsName allProjectsName) {
+    this.allProjectsName = allProjectsName;
+  }
+
+  @Inject(optional = true)
+  void setProjectConfigFactory(ProjectConfig.Factory projectConfigFactory) {
+    this.projectConfigFactory = projectConfigFactory;
+  }
+
+  @Inject(optional = true)
+  void setGerritPersonIdent(@GerritPersonIdent PersonIdent serverUser) {
+    this.serverUser = serverUser;
+  }
+
+  @Inject(optional = true)
+  void setSystemGroupBackend(SystemGroupBackend systemGroupBackend) {
+    this.systemGroupBackend = systemGroupBackend;
+  }
+
   @Override
   public void run() throws Exception {
     Config cfg = allProjectsConfig.load().getConfig();
-    if (cfg == null || !cfg.getSubsections(KEY_LABEL).contains(LABEL_VERIFIED)) {
+    if (cfg == null || !cfg.getSubsections(KEY_LABEL).contains(VERIFIED)) {
       ui.header("Review Labels");
       installVerified = ui.yesno(false, "Install Verified label");
     }
@@ -54,20 +102,60 @@ public class InitLabels implements InitStep {
 
   @Override
   public void postRun() throws Exception {
-    Config cfg = allProjectsConfig.load().getConfig();
     if (installVerified) {
-      cfg.setString(KEY_LABEL, LABEL_VERIFIED, KEY_FUNCTION, MAX_WITH_BLOCK.getFunctionName());
-      cfg.setStringList(
-          KEY_LABEL,
-          LABEL_VERIFIED,
-          KEY_VALUE,
-          Arrays.asList("-1 Fails", "0 No score", "+1 Verified"));
-      cfg.setString(
-          KEY_LABEL,
-          LABEL_VERIFIED,
-          KEY_COPY_CONDITION,
-          "changekind:NO_CHANGE OR changekind:NO_CODE_CHANGE");
-      allProjectsConfig.save("Configure 'Verified' label");
+      installVerified();
     }
+  }
+
+  private void installVerified()
+      throws RepositoryNotFoundException, IOException, ConfigInvalidException {
+    try (Repository git = repositoryManager.openRepository(allProjectsName);
+        MetaDataUpdate md =
+            new MetaDataUpdate(GitReferenceUpdated.DISABLED, allProjectsName, git)) {
+      md.getCommitBuilder().setAuthor(serverUser);
+      md.getCommitBuilder().setCommitter(serverUser);
+      md.setMessage("Configured 'Verified' submit requirement");
+      ProjectConfig config = projectConfigFactory.read(md);
+      LabelType verifiedLabel = getDefaultVerifiedReviewLabel();
+      config.upsertLabelType(verifiedLabel);
+      config.upsertSubmitRequirement(getDefaultVerifiedSubmitRequirement());
+      GroupReference owners = systemGroupBackend.getGroup(SystemGroupBackend.PROJECT_OWNERS);
+      GroupReference admins = GroupReference.create("Administrators");
+      config.upsertAccessSection(
+          AccessSection.HEADS,
+          heads -> {
+            grant(config, heads, verifiedLabel, -1, 1, admins, owners);
+          });
+      config.upsertAccessSection(
+          RefNames.REFS_CONFIG,
+          meta -> {
+            grant(config, meta, verifiedLabel, -1, 1, admins, owners);
+          });
+      config.commit(md);
+    }
+  }
+
+  private static LabelType getDefaultVerifiedReviewLabel() {
+    return LabelType.builder(
+            VERIFIED,
+            ImmutableList.of(
+                LabelValue.create((short) 1, "Verified"),
+                LabelValue.create((short) 0, "No score"),
+                LabelValue.create((short) -1, "Fails")))
+        .setCopyCondition("changekind:NO_CHANGE OR changekind:NO_CODE_CHANGE")
+        .build();
+  }
+
+  private static SubmitRequirement getDefaultVerifiedSubmitRequirement() {
+    return SubmitRequirement.builder()
+        .setName(VERIFIED)
+        .setDescription(
+            Optional.of(
+                String.format("At least one maximum vote for label '%s' is required", VERIFIED)))
+        .setSubmittabilityExpression(
+            SubmitRequirementExpression.create(
+                String.format("label:%s=MAX AND -label:%s=MIN", VERIFIED, VERIFIED)))
+        .setAllowOverrideInChildProjects(true)
+        .build();
   }
 }

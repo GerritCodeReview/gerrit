a6250b4dbc6f11e11c6b73e4068616ec4136b94d
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2025-01-22 17:02:25 +0000

    Add new metrics for account, change, group and project Lucene indexes
    Introduce four new Gauge metrics to track the number of documents
    in the respective search indexes:
    
    * index/lucene/accounts
    * index/lucene/changes
    * index/lucene/groups
    * index/lucene/projects
    
    These metrics are particularly useful in multi-site scenarios, as they
    provide visibility into the number of documents across different
    indexes, aiding in the diagnosis of potential indexing issues.
    
    Release-Notes: Provide index metric for accounts/changes/groups/projects
    Bug: Issue 381216361
    Change-Id: I92d5d62614f1bbf367e519e2aa2192fc5383d2fe

---
+++ java/com/google/gerrit/lucene/LuceneIndexMetrics.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.lucene;
+
+import com.google.gerrit.index.IndexDefinition;
+import com.google.gerrit.metrics.Description;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.inject.Inject;
+import java.util.Collection;
+
+class LuceneIndexMetrics {
+
+  @Inject
+  LuceneIndexMetrics(MetricMaker metrics, Collection<IndexDefinition<?, ?, ?>> defs) {
+    for (IndexDefinition<?, ?, ?> def : defs) {
+      String indexName = def.getName();
+
+      metrics.newCallbackMetric(
+          String.format("index/lucene/%s", indexName),
+          Integer.class,
+          new Description(String.format("%s Lucene Index documents", indexName))
+              .setGauge()
+              .setUnit("documents"),
+          () -> {
+            if (def.getIndexCollection().getSearchIndex() == null) {
+              return -1;
+            } else {
+              return def.getIndexCollection().getSearchIndex().numDocs();
+            }
+          });
+    }
+  }
+}

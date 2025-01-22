b3fc1b2c898f711750d825ca888076d05ee4f7c4
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2025-01-22 17:02:25 +0000

    Add new metrics for account, change, group and project Lucene indexes
    Introduce four new Gauge metrics to track the number of documents
    in the respective search indexes:
    
    * indexes/accounts
    * indexes/changes
    * indexes/groups
    * indexes/projects
    
    These metrics are particularly useful in multi-site scenarios, as they
    provide visibility into the number of documents across different
    indexes, aiding in the diagnosis of potential indexing issues.
    
    Release-Notes: Provide index metric for accounts/changes/groups/projects
    Bug: Issue 381216361
    Change-Id: I92d5d62614f1bbf367e519e2aa2192fc5383d2fe

---
+++ javatests/com/google/gerrit/acceptance/testsuite/index/LuceneIndexMetricsIT.java
@@ -0,0 +1,66 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.testsuite.index;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.TestMetricMaker;
+import com.google.gerrit.index.IndexType;
+import com.google.gerrit.testing.SystemPropertiesTestRule;
+import com.google.inject.Inject;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class LuceneIndexMetricsIT extends AbstractDaemonTest {
+  @ClassRule
+  public static SystemPropertiesTestRule systemProperties =
+      new SystemPropertiesTestRule(IndexType.SYS_PROP, "lucene");
+
+  @Inject protected TestMetricMaker testMetricMaker;
+
+  @Test
+  public void checkProjectsIndexMetric() throws Exception {
+    int numberProjects = intMetricValueOf("indexes/projects");
+    gApi.projects().create("some_project");
+    assertThat(intMetricValueOf("indexes/projects")).isEqualTo(numberProjects + 1);
+  }
+
+  @Test
+  public void checkChangesIndexMetric() throws Exception {
+    int numberChanges = intMetricValueOf("indexes/changes");
+    createChange();
+    assertThat(intMetricValueOf("indexes/changes")).isEqualTo(numberChanges + 1);
+  }
+
+  @Test
+  public void checkAccountsIndexMetric() throws Exception {
+    int numberAccounts = intMetricValueOf("indexes/accounts");
+    gApi.accounts().create("some_account");
+    assertThat(intMetricValueOf("indexes/accounts")).isEqualTo(numberAccounts + 1);
+  }
+
+  @Test
+  public void checkGroupsIndexMetric() throws Exception {
+    int numberGroups = intMetricValueOf("indexes/groups");
+    gApi.groups().create("some_group");
+    assertThat(intMetricValueOf("indexes/groups")).isEqualTo(numberGroups + 1);
+  }
+
+  private int intMetricValueOf(String metric) {
+    return (int) testMetricMaker.getCallbackMetricValue(metric);
+  }
+}
+

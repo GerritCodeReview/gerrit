00be0e222f7a22c570fc63b1662a32b06ad78a21
Daniele Sassoli <danielesassoli@gmail.com>
2025-01-27 18:35:36 +0000

    Add new permission to allow posting reviews
    This is done, mainly, with the intention of allowing Gerrit admins to prevent users from posting reviews if they are found posting spam
    content.
    
    Bug: Issue 391666234
    Release-Notes: Add new permission to allow posting reviews
    Forward-Compatible: checked
    Change-Id: I91755296aef3917f99760d0451f7c0b376825540

--- java/com/google/gerrit/server/restapi/change/PostReview.java
+++ java/com/google/gerrit/server/restapi/change/PostReview.java
@@ -227,6 +227,8 @@ public class PostReview implements RestModifyView<RevisionResource, ReviewInput>
           PermissionBackendException,
           ConfigInvalidException,
           PatchListNotAvailableException {
+    revision.getChangeResource().permissions().check(ChangePermission.REVIEW);
+
     // Respect timestamp, but truncate at change created-on time.
     ts = Ordering.natural().max(ts, revision.getChange().getCreatedOn());
     if (revision.getEdit().isPresent()) {

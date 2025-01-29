2531386026678a807abfd0ab866a1c1c038d902a
Dhruv Srivastava <dhruvsri@google.com>
2025-01-29 20:58:49 +0100

    Fetch validation options to show in the revert dialog
    Change-Id: I7bb348f578c44c806d30766281038c99367ad49a

---
+++ polygerrit-ui/app/elements/change/gr-validation-options/gr-validation-options.ts
@@ -0,0 +1,66 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {
+  ValidationOptionsInfo,
+  ValidationOptionInfo,
+} from '../../../api/rest-api';
+import {repeat} from 'lit/directives/repeat.js';
+
+@customElement('gr-validation-options')
+export class GrValidationOptions extends LitElement {
+  @property({type: Object}) validationOptions?: ValidationOptionsInfo;
+
+  private isOptionSelected: Map<string, boolean> = new Map();
+
+  static override get styles() {
+    return [
+      css`
+        .selectionLabel {
+          padding: 10px;
+          margin: -10px;
+          display: block;
+        }
+      `,
+    ];
+  }
+
+  init() {
+    this.isOptionSelected = new Map();
+  }
+
+  override render() {
+    if (!this.validationOptions) return;
+    return html`${repeat(
+      this.validationOptions.validation_options,
+      option => option.name,
+      option => this.renderValidationOption(option)
+    )}`;
+  }
+
+  private renderValidationOption(option: ValidationOptionInfo) {
+    return html` <div class="validationOptionContainer">
+      <label class="selectionLabel">
+        <input
+          type="checkbox"
+          .checked=${false}
+          @click=${() => this.toggleCheckbox(option)}
+        />
+        ${option.description}
+      </label>
+      <div></div>
+    </div>`;
+  }
+
+  private toggleCheckbox(option: ValidationOptionInfo) {
+    this.isOptionSelected.set(
+      option.name,
+      !this.isOptionSelected.get(option.name)
+    );
+  }
+}

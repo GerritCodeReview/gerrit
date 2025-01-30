d23b47283c30ac231826d17030ef61983a3047fe
Thomas Dräbing <thomas.draebing@sap.com>
2025-01-30 10:19:52 +0100

    Make updating disk caches during offline reindexing optional
    Change-Id: I91e40ec08f2b97c62e1008a773a3b2a961f8cd30

--- java/com/google/gerrit/server/cache/h2/H2CacheImpl.java
+++ java/com/google/gerrit/server/cache/h2/H2CacheImpl.java
@@ -355,7 +355,8 @@ public class H2CacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
         @Nullable Duration expireAfterWrite,
         @Nullable Duration refreshAfterWrite,
         boolean buildBloomFilter,
-        boolean trackLastAccess) {
+        boolean trackLastAccess,
+        CacheAccessMode cacheAccessMode) {
       this.url = jdbcUrl;
       this.keyType = createKeyType(keyType, keySerializer);
       this.valueSerializer = valueSerializer;
@@ -366,16 +367,25 @@ public class H2CacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
       this.trackLastAccess = trackLastAccess;
       this.handles = new SqlHandles<>(this.url, this.keyType);
 
-      this.writer =
-          new WriterImpl<>(
-              maxSize,
-              jdbcUrl,
-              this.keyType,
-              valueSerializer,
-              version,
-              expireAfterWrite,
-              handles,
-              bloomFilter);
+      switch (cacheAccessMode) {
+        case READWRITE:
+          this.writer =
+              new WriterImpl<>(
+                  maxSize,
+                  jdbcUrl,
+                  this.keyType,
+                  valueSerializer,
+                  version,
+                  expireAfterWrite,
+                  handles,
+                  bloomFilter);
+          break;
+        case READONLY:
+          this.writer = new NoopWriterImpl<>();
+          break;
+        default:
+          throw new IllegalArgumentException("Unsupported cache access mode: " + cacheAccessMode);
+      }
     }
 
     void close() {
@@ -602,6 +612,23 @@ public class H2CacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
       public void invalidateAll() throws SQLException;
 
       public void prune(Cache<K, ?> mem);
+    }
+
+    static class NoopWriterImpl<K, V> implements Writer<K, V> {
+      @Override
+      public void touch(SqlHandle c, K key) {}
+
+      @Override
+      public void put(K key, ValueHolder<V> holder) {}
+
+      @Override
+      public void invalidate(K key) {}
+
+      @Override
+      public void invalidateAll() {}
+
+      @Override
+      public void prune(Cache<K, ?> mem) {}
     }
 
     static class WriterImpl<K, V> implements Writer<K, V> {

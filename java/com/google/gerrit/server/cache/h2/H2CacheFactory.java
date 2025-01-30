a1b6101714681a74239bad5e15dd363eef6d3698
Thomas Dräbing <thomas.draebing@sap.com>
2025-01-30 10:19:52 +0100

    Make updating disk caches during offline reindexing optional
    Persisted caches could grow to more than 100GB during offline
    reindexing in large sites. A lot of this data would be concerned
    with old data that is not often accessed anymore in interactive
    usage, e.g. older closed changes. This data thus does not need
    to be cached to improve performance and would likely be pruned
    during the next cache prune. The cache writes however slow down
    reindexing.
    
    This change adds a read-only H2 cache store that can optionally
    be used during offline reindexing. Using that, values will still
    be cached in memory, but the persisted caches will not be updated
    and only read from.
    
    Release-Notes: Updating caches during offline reindexing becomes optional
    Change-Id: Ie66f086f1517bfe4f04c4fc702586ab66c7c28d2

--- java/com/google/gerrit/server/cache/h2/H2CacheFactory.java
+++ java/com/google/gerrit/server/cache/h2/H2CacheFactory.java
@@ -31,6 +31,7 @@ import com.google.gerrit.server.cache.PersistentCacheDef;
 import com.google.gerrit.server.cache.h2.H2CacheImpl.NoopSqlStoreBloomFilter;
 import com.google.gerrit.server.cache.h2.H2CacheImpl.SqlHandles;
 import com.google.gerrit.server.cache.h2.H2CacheImpl.SqlStore;
+import com.google.gerrit.server.cache.h2.H2CacheImpl.SqlStore.NoopWriterImpl;
 import com.google.gerrit.server.cache.h2.H2CacheImpl.SqlStore.Writer;
 import com.google.gerrit.server.cache.h2.H2CacheImpl.SqlStore.WriterImpl;
 import com.google.gerrit.server.cache.h2.H2CacheImpl.SqlStoreBloomFilter;
@@ -251,16 +252,23 @@ class H2CacheFactory extends PersistentCacheBaseFactory implements LifecycleList
     } else {
       bloomFilter = new NoopSqlStoreBloomFilter<>();
     }
-    Writer<K, V> writer =
-        new WriterImpl<>(
-            maxSize,
-            url.toString(),
-            keyType,
-            def.valueSerializer(),
-            def.version(),
-            expireAfterWrite,
-            handles,
-            bloomFilter);
+
+    Writer<K, V> writer;
+    if (options.contains(CacheOptions.READ_ONLY)) {
+      writer = new NoopWriterImpl<>();
+    } else {
+      writer =
+          new WriterImpl<>(
+              maxSize,
+              url.toString(),
+              keyType,
+              def.valueSerializer(),
+              def.version(),
+              expireAfterWrite,
+              handles,
+              bloomFilter);
+    }
+
     return new SqlStore<>(
         url.toString(),
         keyType,

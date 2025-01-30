25836333bbbb7979ae93494855727daa7dd2cb84
Luca Milanesio <luca.milanesio@gmail.com>
2025-01-30 20:25:19 +0000

    Introduce repository reference counting in tests
    Allow Gerrit tests to count the references to repositories
    open and closed during the execution of tests, allowing to
    make assertion on all repositories being closed when the
    tests are finished.
    
    Release-Notes: skip
    Change-Id: I8d288fa3964b9b6655b7e2078898c80a936b0150

---
+++ java/com/google/gerrit/server/git/GitRepositoryReferenceCountingManager.java
@@ -0,0 +1,104 @@
+package com.google.gerrit.server.git;
+
+import com.google.gerrit.entities.Project;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NavigableSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.Repository;
+
+public class GitRepositoryReferenceCountingManager implements GitRepositoryManager {
+  private final GitRepositoryManager delegate;
+  private final Map<Project.NameKey, RepositoryTracking> trackedRepositories;
+
+  public static class RepositoryTracking {
+
+    private final StackTraceElement[] stackTrace;
+    private final Repository repository;
+    private final Project.NameKey name;
+
+    RepositoryTracking(Project.NameKey name, Repository repository) {
+      this.name = name;
+      this.stackTrace = Thread.currentThread().getStackTrace();
+      this.repository = repository;
+    }
+
+    @Override
+    public String toString() {
+      return "Project "
+          + name
+          + " created by "
+          + Stream.of(stackTrace)
+              .map(
+                  stack ->
+                      stack.getClassName()
+                          + "."
+                          + stack.getMethodName()
+                          + ":"
+                          + stack.getLineNumber())
+              .collect(Collectors.joining("\n"));
+    }
+
+    @Override
+    public int hashCode() {
+      return repository.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (o == this) return true;
+      if (!(o instanceof RepositoryTracking)) return false;
+      RepositoryTracking other = (RepositoryTracking) o;
+      return repository.equals(other.repository);
+    }
+  }
+
+  public GitRepositoryReferenceCountingManager(GitRepositoryManager delegate) {
+    this.delegate = delegate;
+    trackedRepositories = new HashMap<>();
+  }
+
+  @Override
+  public Repository openRepository(Project.NameKey name)
+      throws RepositoryNotFoundException, IOException {
+    return trackRepository(name, delegate.openRepository(name));
+  }
+
+  @Override
+  public Repository createRepository(Project.NameKey name)
+      throws RepositoryNotFoundException, RepositoryExistsException, IOException {
+    return trackRepository(name, delegate.createRepository(name));
+  }
+
+  @Override
+  public NavigableSet<Project.NameKey> list() {
+    return delegate.list();
+  }
+
+  @Override
+  public Boolean canPerformGC() {
+    return delegate.canPerformGC();
+  }
+
+  public Map<RepositoryTracking, Integer> openRepositoriesWithReferenceCount() {
+    return trackedRepositories.entrySet().stream()
+        .filter(repositoryTracking -> repositoryTracking.getValue().repository.useCnt.get() > 1)
+        .collect(
+            Collectors.toMap(
+                Map.Entry::getValue,
+                repositoryTracking -> repositoryTracking.getValue().repository.useCnt.get()));
+  }
+
+  @Override
+  public Status getRepositoryStatus(Project.NameKey name) {
+    return delegate.getRepositoryStatus(name);
+  }
+
+  private Repository trackRepository(Project.NameKey name, Repository repository) {
+    trackedRepositories.put(name, new RepositoryTracking(name, repository));
+    return repository;
+  }
+}

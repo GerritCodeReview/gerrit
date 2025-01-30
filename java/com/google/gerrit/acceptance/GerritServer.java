c823ae53e77b507d036b10767d94ecf582f8502d
Luca Milanesio <luca.milanesio@gmail.com>
2025-01-30 20:25:19 +0000

    Introduce repository reference counting in tests
    Allow Gerrit tests to count the references to repositories
    open and closed during the execution of tests, allowing to
    make assertion on all repositories being closed when the
    tests are finished.
    
    Release-Notes: skip
    Change-Id: I8d288fa3964b9b6655b7e2078898c80a936b0150

--- java/com/google/gerrit/acceptance/GerritServer.java
+++ java/com/google/gerrit/acceptance/GerritServer.java
@@ -16,6 +16,7 @@ package com.google.gerrit.acceptance;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.truth.Truth.assertThat;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static java.util.Objects.requireNonNull;
 
@@ -63,6 +64,7 @@ import com.google.gerrit.server.config.GerritRuntime;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.config.SitePath;
 import com.google.gerrit.server.experiments.ConfigExperimentFeatures.ConfigExperimentFeaturesModule;
+import com.google.gerrit.server.git.GitRepositoryReferenceCountingManager;
 import com.google.gerrit.server.git.receive.AsyncReceiveCommits.AsyncReceiveCommitsModule;
 import com.google.gerrit.server.git.validators.CommitValidationListener;
 import com.google.gerrit.server.index.AbstractIndexModule;
@@ -74,6 +76,7 @@ import com.google.gerrit.server.util.SocketUtil;
 import com.google.gerrit.server.util.SystemLog;
 import com.google.gerrit.testing.FakeAccountPatchReviewStore.FakeAccountPatchReviewStoreModule;
 import com.google.gerrit.testing.FakeEmailSender.FakeEmailSenderModule;
+import com.google.gerrit.testing.InMemoryRepositoryCountingManager;
 import com.google.gerrit.testing.InMemoryRepositoryManager;
 import com.google.gerrit.testing.SshMode;
 import com.google.gerrit.testing.TestLoggingActivator;
@@ -770,7 +773,15 @@ public class GerritServer implements AutoCloseable {
 
   @Override
   public void close() throws Exception {
-    daemon.ifPresent(d -> d.getLifecycleManager().stop());
+    if (daemon.isPresent()) {
+      Daemon currDaemon = daemon.get();
+      InMemoryRepositoryCountingManager repositoryCountingManager =
+          testInjector.getInstance(InMemoryRepositoryCountingManager.class);
+      Map<GitRepositoryReferenceCountingManager.RepositoryTracking, Integer> openRepositories =
+          repositoryCountingManager.openRepositoriesWithReferenceCount();
+      assertThat(openRepositories).isEmpty();
+      currDaemon.getLifecycleManager().stop();
+    }
     if (daemonService != null) {
       System.out.println("Gerrit Server Shutdown");
       daemonService.shutdownNow();

cfb4f1fd44f650e908df2eb88a21d62bdbe02be6
Luca Milanesio <luca.milanesio@gmail.com>
2025-01-30 20:25:19 +0000

    Introduce repository reference counting in tests
    Allow Gerrit tests to count the references to repositories
    open and closed during their executions, allowing to
    make assertion on all repositories being closed when the
    tests are finished.
    
    Clear any possible discrepancy on the open Repository
    object at the end of the test execution, so that one test leaving
    open resources would not impact the execution of the other tests.
    
    The error message contains the following information:
    - Name of the repository leaked
    - Callers (with stack trace) of the open/incrementOpen on the repo
    - Callers (with stack trace) of the close on the repo
    
    The developer is also given a hint on how to resolve the issue
    and fix his code accordingly.
    
    NOTE: Because of the lack of checks on repository leaks, existing
    plugins may need fixing should they have left open repositories
    at the end of the their tests on inside their production code.
    
    Release-Notes: Introduce Repository leak detection when using AbstractDaemonTest
    Change-Id: I303706438811e6666b02c15b17234bf8d96e0f6e

--- java/com/google/gerrit/acceptance/AbstractDaemonTest.java
+++ java/com/google/gerrit/acceptance/AbstractDaemonTest.java
@@ -148,6 +148,7 @@ import com.google.gerrit.server.util.ThreadLocalRequestContext;
 import com.google.gerrit.testing.ConfigSuite;
 import com.google.gerrit.testing.FakeEmailSender;
 import com.google.gerrit.testing.FakeEmailSender.Message;
+import com.google.gerrit.testing.GitRepositoryReferenceCountingManager;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.Module;
@@ -648,6 +649,17 @@ public abstract class AbstractDaemonTest {
   protected void afterTest() throws Exception {
     Transport.unregister(inProcessProtocol);
     closeTestRepositories();
+
+    GitRepositoryManager repositoryManager =
+        server.getTestInjector().getInstance(GitRepositoryManager.class);
+    if (repositoryManager
+        instanceof GitRepositoryReferenceCountingManager repositoryCountingManager) {
+      try {
+        assertThat(repositoryCountingManager.openRepositories()).isEmpty();
+      } finally {
+        repositoryCountingManager.clear();
+      }
+    }
 
     // Set useDefaultTicker in afterTest, so the next beforeTest will use the default ticker
     testTicker.useDefaultTicker();

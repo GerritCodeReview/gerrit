5b7da9fdee9ee61ae0af4660dc83dfdf18abde26
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2025-02-05 11:12:19 +0000

    Move LuceneIndexMetricsIT.java to new lucene target
    Introduce a new lucene target for better organization. In
    the future, all Lucene-related tests should be placed in
    this target to enable more granular control over the test
    execution.
    
    Change-Id: I156cc4dbe612d5a8cbb1335209b3b2df91237ad7

--- javatests/com/google/gerrit/acceptance/lucene/LuceneIndexMetricsIT.java
+++ javatests/com/google/gerrit/acceptance/lucene/LuceneIndexMetricsIT.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.acceptance.testsuite.index;
+package com.google.gerrit.acceptance.lucene;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.TruthJUnit.assume;

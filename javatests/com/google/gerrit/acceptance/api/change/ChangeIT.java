511c8bb9437fed7e6300016a85f35d8b10e2428e
Daniele Sassoli <danielesassoli@gmail.com>
2025-02-06 22:49:57 +0000

    Allow users to post reviews if they can vote on any label
    This will allow too deploy the new label permission without having to
    run migration scripts, as a user that is able to vote on any label on a
    change will be able to keep posting reviews.
    
    Change-Id: Iab2e5aa44040cf910c6421427086956ac97b922a

--- javatests/com/google/gerrit/acceptance/api/change/ChangeIT.java
+++ javatests/com/google/gerrit/acceptance/api/change/ChangeIT.java
@@ -632,10 +632,8 @@ public class ChangeIT extends AbstractDaemonTest {
   }
 
   @Test
-  public void blockReviewerToPostReviewWithoutPermission() throws Exception {
+  public void allowReviewerToPostReviewIfCanApplyLabel() throws Exception {
     PushOneCommit.Result r = createChange();
-    ReviewInput in = ReviewInput.approve().reviewer(user.email());
-    gApi.changes().id(r.getChangeId()).current().review(in);
 
     // revoke permissions to review the change
     projectOperations
@@ -644,6 +642,35 @@ public class ChangeIT extends AbstractDaemonTest {
         .add(block(Permission.REVIEW).ref("refs/*").group(REGISTERED_USERS))
         .update();
 
+    ReviewInput input = new ReviewInput().message("Some message");
+    int numMessages = gApi.changes().id(r.getChangeId()).get().messages.size();
+    gApi.changes().id(r.getChangeId()).current().review(input);
+    assertThat(gApi.changes().id(r.getChangeId()).get().messages).hasSize(numMessages + 1);
+  }
+
+  @Test
+  public void blockReviewerFromPostingReviewIfCannotApplyLabelAndNoReviewPermission()
+      throws Exception {
+    PushOneCommit.Result r = createChange();
+
+    // revoke permissions to review the change and to vote on labels
+    projectOperations
+        .project(project)
+        .forUpdate()
+        .add(block(Permission.REVIEW).ref("refs/*").group(REGISTERED_USERS))
+        .add(
+            blockLabel(LabelId.CODE_REVIEW)
+                .ref("refs/heads/*")
+                .group(REGISTERED_USERS)
+                .range(-2, 2))
+        .add(
+            blockLabel(LabelId.CODE_REVIEW)
+                .ref("refs/heads/*")
+                .group(REGISTERED_USERS)
+                .range(-1, 1))
+        .update();
+
+    ReviewInput in = ReviewInput.create().message("Nothing to see here").reviewer(user.email());
     AuthException thrown =
         assertThrows(
             AuthException.class, () -> gApi.changes().id(r.getChangeId()).current().review(in));

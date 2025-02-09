0297b01bda51d8139491e0eab7d1c819230b73b9
Jacek Centkowski <geminica.programs@gmail.com>
2025-02-09 15:11:57 +0100

    Add configuration for number of change refs advertised on push
    Introduce the global `receive.advertiseOpenChangesRefs` (by default the
    existing value 32 is preserved) configuration that specifies how many
    change refs (as additional "haves") are advertised on push.
    
    If `receive.advertiseOpenChangesRefs` is set to `0`, no additional
    "haves" will be advertised to the client.
    
    Bug: Issue 390453038
    Release-Notes: Configure the number of change refs advertised on push to repository.
    Change-Id: I1073905df3f680a4034836ab47f5d96a999c4a50

--- javatests/com/google/gerrit/acceptance/git/RefAdvertisementIT.java
+++ javatests/com/google/gerrit/acceptance/git/RefAdvertisementIT.java
@@ -86,6 +86,7 @@ public class RefAdvertisementIT extends AbstractDaemonTest {
   @Inject private ProjectOperations projectOperations;
   @Inject private RequestScopeOperations requestScopeOperations;
   @Inject private IndexOperations.Change changeIndexOperations;
+  @Inject private ReceiveCommitsAdvertiseRefsHookChain.ForTestProvider chainProvider;
 
   private AccountGroup.UUID admins;
   private AccountGroup.UUID serviceUsers;
@@ -1134,6 +1135,40 @@ public class RefAdvertisementIT extends AbstractDaemonTest {
   }
 
   @Test
+  @GerritConfig(name = "receive.advertiseOpenChangesRefs", value = "0")
+  public void receivePackHasNoAdditionalHavesWhenAdvertiseOpenChangesRefsIsDisabled()
+      throws Exception {
+    TestRefAdvertiser.Result r = getReceivePackRefs();
+    assertThat(r.allRefs().keySet())
+        .containsExactly(
+            // meta refs are excluded
+            "refs/heads/branch",
+            "refs/heads/master",
+            "refs/meta/config",
+            "refs/tags/branch-tag",
+            "refs/tags/master-tag",
+            "refs/tags/tree-tag");
+    assertThat(r.additionalHaves()).isEmpty();
+  }
+
+  @Test
+  @GerritConfig(name = "receive.advertiseOpenChangesRefs", value = "1")
+  public void receivePackHasAdditionalHavesAccordingToAdvertiseOpenChangesRefsConfiguredValue()
+      throws Exception {
+    TestRefAdvertiser.Result r = getReceivePackRefs();
+    assertThat(r.allRefs().keySet())
+        .containsExactly(
+            // meta refs are excluded
+            "refs/heads/branch",
+            "refs/heads/master",
+            "refs/meta/config",
+            "refs/tags/branch-tag",
+            "refs/tags/master-tag",
+            "refs/tags/tree-tag");
+    assertThat(r.additionalHaves()).hasSize(1);
+  }
+
+  @Test
   public void receivePackRespectsVisibilityOfOpenChanges() throws Exception {
     projectOperations
         .project(project)
@@ -1517,8 +1552,7 @@ public class RefAdvertisementIT extends AbstractDaemonTest {
   private TestRefAdvertiser.Result getReceivePackRefs() throws Exception {
     try (Repository repo = repoManager.openRepository(project)) {
       AdvertiseRefsHook adv =
-          ReceiveCommitsAdvertiseRefsHookChain.createForTest(
-              queryProvider, project, identifiedUserFactory.create(admin.id()));
+          chainProvider.get(queryProvider, project, identifiedUserFactory.create(admin.id()));
       ReceivePack rp = new ReceivePack(repo);
       rp.setAdvertiseRefsHook(adv);
       TestRefAdvertiser advertiser = new TestRefAdvertiser(repo);

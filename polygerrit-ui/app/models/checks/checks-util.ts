57bfde8958f6d29a4086b22b8d367349b242b2ed
Ben Rohlfs <brohlfs@google.com>
2025-02-11 09:57:14 +0100

    Fix conversion of check results to comments
    b/394895870 has shown that some check results are reported with bogus
    ranges: All line and char values may be 0. We did not check this when
    converting check results to comments for the "please fix" flow.
    
    We are introducing a new `toComment()` function for conversion of
    check results that have a code pointer to a draft comment. We use
    this canonical function is the two places where it currently matters.
    
    The `line` and `range` of the draft comment are only set after
    properly validating the values. If the values do not pass this
    validation, then we treat the new draft as a FILE comment. That is
    actually a nice feature, because that allows us to support check
    results on a FILE level. Using 0 as the start and end line turns
    from a bug into a feature. :-)
    
    Google-Bug-Id: b/394895870
    Release-Notes: skip
    Change-Id: Id9fd662b7d5bd63e7eed83b53db2488b9a1937c4

--- polygerrit-ui/app/models/checks/checks-util.ts
+++ polygerrit-ui/app/models/checks/checks-util.ts
@@ -99,17 +99,42 @@ function pleaseFixMessage(result: RunResult) {
 ${result.message}`;
 }
 
-export function createPleaseFixComment(result: RunResult): DraftInfo {
+export function toComment(
+  result: RunResult,
+  message?: string,
+  unresolved?: boolean
+): DraftInfo {
   const pointer = result.codePointers?.[0];
-  assertIsDefined(pointer, 'codePointer');
-  return {
-    ...createNew(pleaseFixMessage(result), true),
+  assertIsDefined(pointer, 'code pointer required for conversion to comment');
+
+  const draft: DraftInfo = {
+    ...createNew(message, unresolved),
     path: pointer.path,
     patch_set: result.patchset as RevisionPatchSetNum,
     side: CommentSide.REVISION,
-    line: pointer.range.end_line ?? pointer.range.start_line,
-    range: pointer.range,
   };
+
+  if (
+    pointer.range?.start_line > 0 &&
+    pointer.range?.end_line > 0 &&
+    pointer.range?.start_character >= 0 &&
+    pointer.range?.end_character >= 0
+  ) {
+    draft.range = pointer.range;
+  }
+
+  if (pointer.range?.end_line > 0) {
+    draft.line = pointer.range.end_line;
+  } else if (pointer.range?.start_line > 0) {
+    draft.line = pointer.range.start_line;
+  }
+  // Otherwise the draft will be a FILE comment.
+
+  return draft;
+}
+
+export function createPleaseFixComment(result: RunResult): DraftInfo {
+  return toComment(result, pleaseFixMessage(result), true);
 }
 
 export function createFixAction(

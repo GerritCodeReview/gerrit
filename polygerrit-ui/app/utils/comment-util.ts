7b5936c48ae7b9ea1ab8e9c53a8a9e3ab2610f1d
Milutin Kristofic <milutin@google.com>
2025-02-25 20:19:41 +0100

    Fix(User suggestions): Disable on file level comments
    It doesn't make sense on whole file and it even throws error when used
    on file level comments.
    
    Google-Bug-Id: b/399103624
    Release-Notes: skip
    Change-Id: I6d58c6707b5191da613ec6a861e9d1bd8611ec99

--- polygerrit-ui/app/utils/comment-util.ts
+++ polygerrit-ui/app/utils/comment-util.ts
@@ -247,7 +247,7 @@ export function createCommentThreads(comments: Comment[]) {
       range: comment.range,
       rootId: id(comment),
     };
-    if (!comment.line && !comment.range) {
+    if (isFileLevelComment(comment)) {
       newThread.line = FILE;
     }
     threads.push(newThread);
@@ -691,4 +691,11 @@ export function convertToCommentInput(comment: Comment): CommentInput {
     output.fix_suggestions = comment.fix_suggestions;
   }
   return output;
+}
+
+/**
+ * Returns true if the comment is a file comment, i.e. it has no line or range.
+ */
+export function isFileLevelComment(comment: Comment) {
+  return !comment.line && !comment.range;
 }

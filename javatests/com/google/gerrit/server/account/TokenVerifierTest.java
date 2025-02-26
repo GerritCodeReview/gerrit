2c80e5795e96e411d251401dac7eb11bbcebc117
Thomas Dräbing <thomas.draebing@sap.com>
2025-02-26 14:22:32 +0100

    Use dedicated cache for HTTP passwords / tokens
    So far HTTP passwords were cached as part of the `username`
    ExternalID. The current plan is to implement the use of
    multiple tokens with limited lifetime instead of never
    expiring HTTP passwords. To be able to do that, the way
    the tokens are stored will change in a way that they are
    not associated with an ExternalID anymore. Thus, they
    should be cached independently.
    
    The new token cache will still read the password from the
    ExternalIDs. The cache will only be enabled, if the
    option `auth.gitBasicAuthPolicy` is either `HTTP` or
    `HTTP_LDAP`.
    
    Release-Notes: Use dedicated cache for HTTP passwords / tokens
    Change-Id: I8068018780e8603e041744a9bfba88d553ce5013

---
+++ javatests/com/google/gerrit/server/account/TokenVerifierTest.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+import com.google.gerrit.entities.Account;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TokenVerifierTest {
+  private static final Account.Id ACCOUNT_ID = Account.id(1);
+  private static final List<TokenCacheEntry> TOKEN_CACHE_ENTRIES =
+      List.of(
+          new TokenCacheEntry(ACCOUNT_ID, Token.createWithPlainToken("hashedToken")),
+          new TokenCacheEntry(ACCOUNT_ID, Token.createWithPlainToken("another_Token")));
+  private TokenVerifier tokenVerifier;
+  @Mock private TokenCache tokenCache;
+
+  @Before
+  public void setUp() {
+    tokenVerifier = new TokenVerifier(tokenCache);
+    doReturn(TOKEN_CACHE_ENTRIES).when(tokenCache).get(ACCOUNT_ID);
+  }
+
+  @Test
+  public void checkToken() {
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "hashedToken")).isTrue();
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "another_Token")).isTrue();
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "invalid")).isFalse();
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "another_token")).isFalse();
+  }
+}

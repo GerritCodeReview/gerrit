786f1ff4e5a52dfbfdb7aa75034288f638690f07
Thomas Dräbing <thomas.draebing@sap.com>
2025-02-26 14:22:32 +0100

    Use dedicated cache for HTTP passwords / tokens
    So far HTTP passwords were cached as part of the `username`
    ExternalID. The current plan is to implement the use of
    multiple tokens with limited lifetime instead of never
    expiring HTTP passwords. To be able to do that, the way
    the tokens are stored will change in a way that they are
    not associated with an ExternalID anymore. Thus, they
    should be cached independently.
    
    The new token cache will still read the password from the
    ExternalIDs. The cache will only be enabled, if the
    option `auth.gitBasicAuthPolicy` is either `HTTP` or
    `HTTP_LDAP`.
    
    Release-Notes: Use dedicated cache for HTTP passwords / tokens
    Change-Id: I8068018780e8603e041744a9bfba88d553ce5013

---
+++ java/com/google/gerrit/server/account/TokenCacheImpl.java
@@ -0,0 +1,124 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import static com.google.gerrit.server.account.externalids.ExternalId.SCHEME_USERNAME;
+
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.server.account.externalids.ExternalId;
+import com.google.gerrit.server.cache.CacheModule;
+import com.google.gerrit.server.logging.Metadata;
+import com.google.gerrit.server.logging.TraceContext;
+import com.google.gerrit.server.logging.TraceContext.TraceTimer;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Named;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+public class TokenCacheImpl implements TokenCache {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private static final String CACHE_NAME = "tokens";
+  static final Iterable<TokenCacheEntry> NO_SUCH_USER = none();
+  static final Iterable<TokenCacheEntry> NO_TOKENS = none();
+
+  private final LoadingCache<Account.Id, Iterable<TokenCacheEntry>> cache;
+
+  public static Module module() {
+    return new CacheModule() {
+      @Override
+      protected void configure() {
+        cache(CACHE_NAME, Account.Id.class, new TypeLiteral<Iterable<TokenCacheEntry>>() {})
+            .loader(Loader.class);
+        bind(TokenCache.class).to(TokenCacheImpl.class);
+      }
+    };
+  }
+
+  private static Iterable<TokenCacheEntry> none() {
+    return Collections.unmodifiableCollection(Arrays.asList(new TokenCacheEntry[0]));
+  }
+
+  @Inject
+  TokenCacheImpl(@Named(CACHE_NAME) LoadingCache<Account.Id, Iterable<TokenCacheEntry>> cache) {
+    this.cache = cache;
+  }
+
+  @Override
+  public Iterable<TokenCacheEntry> get(Account.Id accountId) {
+    try {
+      return cache.get(accountId);
+    } catch (ExecutionException e) {
+      logger.atWarning().withCause(e).log(
+          "Cannot load authentication tokens for %d", accountId.get());
+      return Collections.emptyList();
+    }
+  }
+
+  @Override
+  public void evict(Account.Id accountId) {
+    if (accountId != null) {
+      logger.atFine().log("Evict authentication token for username %d", accountId.get());
+      cache.invalidate(accountId);
+    }
+  }
+
+  static class Loader extends CacheLoader<Account.Id, Iterable<TokenCacheEntry>> {
+    private final AccountCache accountCache;
+
+    @Inject
+    Loader(AccountCache accountCache) {
+      this.accountCache = accountCache;
+    }
+
+    @Override
+    public Iterable<TokenCacheEntry> load(Account.Id accountId) throws Exception {
+      try (TraceTimer timer =
+          TraceContext.newTimer(
+              "Loading authentication tokens for account with username",
+              Metadata.builder().accountId(accountId.get()).build())) {
+        AccountState accountState = accountCache.getEvenIfMissing(accountId);
+        Optional<ExternalId> optUser =
+            accountState.externalIds().stream()
+                .filter(e -> e.key().scheme().equals(SCHEME_USERNAME))
+                .findFirst();
+        if (!optUser.isPresent()) {
+          return NO_SUCH_USER;
+        }
+
+        List<TokenCacheEntry> tokens = new ArrayList<>(1);
+        ExternalId user = optUser.get();
+        String password = user.password();
+        if (password != null) {
+          tokens.add(new TokenCacheEntry(user.accountId(), Token.create(password)));
+        }
+
+        if (tokens.isEmpty()) {
+          return NO_TOKENS;
+        }
+        return Collections.unmodifiableList(tokens);
+      }
+    }
+  }
+}

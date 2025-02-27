614c03e9b50f6e7db1cf8b6bf21dc0d9bbbc7b68
Dhruv Srivastava <dhruvsri@google.com>
2025-02-27 15:54:42 +0100

    Add updateForUserManagementRequests to AccountsUpdate
    Release-Notes: skip
    Google-bug-id: b/330830741
    Change-Id: I059415cd42db049f83d128d405acf7ae0108541a

--- java/com/google/gerrit/server/account/AccountsUpdate.java
+++ java/com/google/gerrit/server/account/AccountsUpdate.java
@@ -229,6 +229,16 @@ public abstract class AccountsUpdate {
   }
 
   /**
+   * Perform update provided. Some implementations of AccountsUpdate want the ability to delete the
+   * externalIds instead of hiding them hence we allow those implementations to override this
+   * method.
+   */
+  public default void updateForUserManagementRequests(
+      String message, Account.Id accountId, ConfigureDeltaFromStateAndContext configureDelta) {
+    return this.update(message, accountId, configureDelta);
+  }
+
+  /**
    * Inserts a new account.
    *
    * <p>If the current account state is not needed, use {@link #insert(String, Account.Id,

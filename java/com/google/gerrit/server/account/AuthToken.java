886eb93860843792d148c069d52d28fe1e62a47e
Thomas Dräbing <thomas.draebing@sap.com>
2025-02-27 15:03:00 +0100

    Implement support for storing authentication tokens
    So far HTTP passwords were cached as part of the `username` ExternalID.
    The current plan is to implement the use of multiple tokens with
    limited lifetime instead of never expiring HTTP passwords.
    
    This change adds the new notedb schema version that supports tokens.
    These tokens are stored in a file `tokens.config` in the user's ref in
    All-Users. They are stored in git config format:
    
    ```
    [token "some-token-id"]
      hash = bcrypt0:4:....
    ```
    
    This change also adds REST API endpoints to create, list and delete
    tokens.
    
    Release-Notes: Implement support for authentication tokens
    Change-Id: I0d90c88d48f16e192f1b025f14becf8f1d74ff15

---
+++ java/com/google/gerrit/server/account/AuthToken.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Strings;
+import com.google.gerrit.common.Nullable;
+import java.util.regex.Pattern;
+
+@AutoValue
+public abstract class AuthToken {
+  private static final Pattern TOKEN_ID_PATTERN = Pattern.compile("^[a-zA-Z][a-zA-Z0-9-_]*$");
+
+  public static AuthToken createWithPlainToken(@Nullable String id, String plainToken)
+      throws InvalidAuthTokenException {
+    return create(id, HashedPassword.fromPassword(plainToken).encode());
+  }
+
+  public static AuthToken create(@Nullable String id, String hashedToken)
+      throws InvalidAuthTokenException {
+    if (Strings.isNullOrEmpty(id)) {
+      id = "token_" + System.currentTimeMillis();
+    } else {
+      validateId(id);
+    }
+    return new AutoValue_AuthToken(id, hashedToken);
+  }
+
+  public abstract String id();
+
+  public abstract String hashedToken();
+
+  private static void validateId(String id) throws InvalidAuthTokenException {
+    if (!TOKEN_ID_PATTERN.matcher(id).matches()) {
+      throw new InvalidAuthTokenException(
+          "Token ID must contain only letters, numbers, hyphens and underscores.");
+    }
+  }
+}
---
+++ javatests/com/google/gerrit/acceptance/rest/account/TokenIT.java
@@ -0,0 +1,155 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.acceptance.rest.account;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.extensions.auth.AuthTokenInput;
+import com.google.gerrit.server.account.AuthTokenAccessor;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.inject.Inject;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TokenIT extends AbstractDaemonTest {
+  @Inject AuthTokenAccessor tokenAccessor;
+
+  private AuthTokenInput authTokenInput;
+
+  @Before
+  public void setup() throws Exception {
+    addUserTokens();
+    addAdminTokens();
+
+    String id = "testToken";
+    authTokenInput = new AuthTokenInput();
+    authTokenInput.id = id;
+  }
+
+  @Test
+  public void assertGenerateOwnTokenSucceeds() throws Exception {
+    RestResponse resp =
+        userRestSession.put(
+            String.format("/accounts/self/tokens/%s", authTokenInput.id), authTokenInput);
+    resp.assertCreated();
+
+    JsonObject createdToken = JsonParser.parseReader(resp.getReader()).getAsJsonObject();
+    assertThat(createdToken.get("id").getAsString()).isEqualTo(authTokenInput.id);
+    assertThat(createdToken.get("token").getAsString()).isNotNull();
+
+    assertThat(tokenAccessor.getToken(user.id(), authTokenInput.id)).isPresent();
+  }
+
+  @Test
+  public void assertCreateTokenForOtherUserFailsForNonAdmins() throws Exception {
+    userRestSession
+        .put(
+            String.format("/accounts/%d/tokens/%s", admin.id().get(), authTokenInput.id),
+            authTokenInput)
+        .assertForbidden();
+  }
+
+  @Test
+  public void assertCreateTokenForOtherUserSucceedsForAdmins() throws Exception {
+    adminRestSession
+        .put(
+            String.format("/accounts/%d/tokens/%s", user.id().get(), authTokenInput.id),
+            authTokenInput)
+        .assertCreated();
+  }
+
+  @Test
+  public void assertSetSpecificTokenFailsForNonAdmins() throws Exception {
+    authTokenInput.token = "secret";
+    userRestSession
+        .put(
+            String.format("/accounts/%d/tokens/%s", user.id().get(), authTokenInput.id),
+            authTokenInput)
+        .assertForbidden();
+  }
+
+  @Test
+  public void assertSetSpecificTokenSucceedsForAdmins() throws Exception {
+    authTokenInput.token = "secret";
+    RestResponse resp =
+        adminRestSession.put(
+            String.format("/accounts/%d/tokens/%s", user.id().get(), authTokenInput.id),
+            authTokenInput);
+
+    resp.assertCreated();
+
+    JsonObject createdToken = JsonParser.parseReader(resp.getReader()).getAsJsonObject();
+    assertThat(createdToken.get("id").getAsString()).isEqualTo(authTokenInput.id);
+    assertThat(createdToken.get("token").getAsString()).isEqualTo(authTokenInput.token);
+
+    assertThat(tokenAccessor.getToken(user.id(), authTokenInput.id)).isPresent();
+  }
+
+  @Test
+  public void assertListTokensSucceeds() throws Exception {
+    RestResponse resp = userRestSession.get(String.format("/accounts/%d/tokens", user.id().get()));
+    resp.assertOK();
+
+    JsonArray json = JsonParser.parseReader(resp.getReader()).getAsJsonArray();
+    assertThat(json.size()).isEqualTo(1);
+    assertThat(json.get(0).getAsJsonObject().get("id").getAsString()).isEqualTo("userToken1");
+  }
+
+  @Test
+  public void assertListTokensForOtherUserSucceedsForAdmins() throws Exception {
+    adminRestSession.get(String.format("/accounts/%d/tokens", user.id().get())).assertOK();
+  }
+
+  @Test
+  public void assertListTokensForOtherUserFailsForNonAdmins() throws Exception {
+    userRestSession.get(String.format("/accounts/%d/tokens", admin.id().get())).assertForbidden();
+  }
+
+  @Test
+  public void assertDeleteTokenSucceeds() throws Exception {
+    userRestSession
+        .delete(String.format("/accounts/%d/tokens/userToken1", user.id().get()))
+        .assertNoContent();
+    assertThat(tokenAccessor.getToken(user.id(), "userToken1")).isEmpty();
+  }
+
+  @Test
+  public void assertDeleteTokenForOtherUserSucceedsForAdmins() throws Exception {
+    adminRestSession
+        .delete(String.format("/accounts/%d/tokens/userToken1", user.id().get()))
+        .assertNoContent();
+  }
+
+  @Test
+  public void assertDeleteTokenForOtherUserFailsForNonAdmins() throws Exception {
+    userRestSession
+        .delete(String.format("/accounts/%d/tokens/adminToken1", admin.id().get()))
+        .assertForbidden();
+  }
+
+  private void addUserTokens() throws Exception {
+    @SuppressWarnings("unused")
+    var unused = tokenAccessor.addPlainToken(user.id(), "userToken1", "http-pass");
+  }
+
+  private void addAdminTokens() throws Exception {
+    @SuppressWarnings("unused")
+    var unused = tokenAccessor.addPlainToken(admin.id(), "adminToken1", "http-pass");
+  }
+}

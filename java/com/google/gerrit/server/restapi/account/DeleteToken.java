da43e116e218fda6f3738d0a594d14401a278b2d
Thomas Dräbing <thomas.draebing@sap.com>
2025-02-27 15:03:00 +0100

    Implement support for storing authentication tokens
    So far HTTP passwords were cached as part of the `username` ExternalID.
    The current plan is to implement the use of multiple tokens with
    limited lifetime instead of never expiring HTTP passwords.
    
    This change adds the new notedb schema version that supports tokens.
    These tokens are stored in a file `tokens.config` in the user's ref in
    All-Users. They are stored in git config format:
    
    ```
    [token "some-token-id"]
      hash = bcrypt0:4:....
    ```
    
    This change also adds REST API endpoints to create, list and delete
    tokens.
    
    Release-Notes: Implement support for authentication tokens
    Change-Id: I0d90c88d48f16e192f1b025f14becf8f1d74ff15

---
+++ java/com/google/gerrit/server/restapi/account/DeleteToken.java
@@ -0,0 +1,98 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.restapi.account;
+
+import static com.google.gerrit.server.mail.EmailFactories.PASSWORD_UPDATED;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.exceptions.EmailException;
+import com.google.gerrit.extensions.common.Input;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountResource;
+import com.google.gerrit.server.account.AuthTokenAccessor;
+import com.google.gerrit.server.mail.EmailFactories;
+import com.google.gerrit.server.permissions.GlobalPermission;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+
+/**
+ * REST endpoint to delete a token of an account.
+ *
+ * <p>This REST endpoint handles {@code DELETE /accounts/<account-identifier>/tokens/<token-id>}
+ * requests.
+ */
+@Singleton
+public class DeleteToken implements RestModifyView<AccountResource.Token, Input> {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final Provider<CurrentUser> self;
+  private final PermissionBackend permissionBackend;
+  private final AuthTokenAccessor tokenAccessor;
+  private final EmailFactories emailFactories;
+
+  @Inject
+  DeleteToken(
+      Provider<CurrentUser> self,
+      PermissionBackend permissionBackend,
+      AuthTokenAccessor tokenAccessor,
+      EmailFactories emailFactories) {
+    this.self = self;
+    this.permissionBackend = permissionBackend;
+    this.tokenAccessor = tokenAccessor;
+    this.emailFactories = emailFactories;
+  }
+
+  @Override
+  public Response<String> apply(AccountResource.Token rsrc, Input input)
+      throws AuthException,
+          RepositoryNotFoundException,
+          IOException,
+          ConfigInvalidException,
+          PermissionBackendException {
+    if (!self.get().hasSameAccountId(rsrc.getUser())) {
+      permissionBackend.currentUser().check(GlobalPermission.ADMINISTRATE_SERVER);
+    }
+
+    return apply(rsrc.getUser(), rsrc.getId());
+  }
+
+  private Response<String> apply(IdentifiedUser user, String id)
+      throws RepositoryNotFoundException, IOException, ConfigInvalidException {
+    Account.Id accountId = user.getAccountId();
+    tokenAccessor.deleteToken(accountId, id);
+    try {
+      emailFactories
+          .createOutgoingEmail(
+              PASSWORD_UPDATED, emailFactories.createHttpPasswordUpdateEmail(user, "deleted"))
+          .send();
+    } catch (EmailException e) {
+      logger.atSevere().withCause(e).log(
+          "Cannot send token deletion message to %s", user.getAccount().preferredEmail());
+    }
+
+    return Response.none();
+  }
+}

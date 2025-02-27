46f090ff74a8422264bcb22c22b0ff5d89692632
Saša Živkov <sasa.zivkov@sap.com>
2025-02-27 10:37:43 +0100

    Fix Guice bindings for the DeleteZombieDrafts site program
    This site program was broken because the Guice bindings it created were
    incomplete and the program failed to start.
    
    Release-Notes: skip
    Change-Id: Iccd1896141560d8fab00d340f92dd0aa767c39e2

--- java/com/google/gerrit/pgm/DeleteZombieDrafts.java
+++ java/com/google/gerrit/pgm/DeleteZombieDrafts.java
@@ -14,25 +14,21 @@
 
 package com.google.gerrit.pgm;
 
-import com.google.gerrit.metrics.DisabledMetricMaker;
-import com.google.gerrit.metrics.MetricMaker;
-import com.google.gerrit.pgm.init.api.ConsoleUI;
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.lucene.LuceneIndexModule;
+import com.google.gerrit.pgm.util.BatchProgramModule;
 import com.google.gerrit.pgm.util.SiteProgram;
-import com.google.gerrit.server.config.GerritServerConfigModule;
-import com.google.gerrit.server.config.SitePath;
+import com.google.gerrit.server.account.externalids.storage.notedb.ExternalIdCacheImpl;
+import com.google.gerrit.server.account.storage.notedb.AccountNoteDbReadStorageModule;
+import com.google.gerrit.server.change.ChangeResource;
+import com.google.gerrit.server.git.WorkQueue.WorkQueueModule;
+import com.google.gerrit.server.index.options.AutoFlush;
 import com.google.gerrit.server.notedb.DeleteZombieCommentsRefs;
-import com.google.gerrit.server.schema.SchemaModule;
-import com.google.gerrit.server.securestore.SecureStoreClassName;
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
+import com.google.gerrit.server.notedb.NoteDbDraftCommentsModule;
+import com.google.gerrit.server.notedb.NoteDbStarredChangesModule;
 import com.google.inject.Injector;
-import com.google.inject.Module;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
-import com.google.inject.util.Providers;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import org.kohsuke.args4j.Option;
 
 /**
@@ -51,30 +47,25 @@ public class DeleteZombieDrafts extends SiteProgram {
   @Override
   public int run() throws IOException {
     mustHaveValidSite();
-    Injector sysInjector = getSysInjector();
+    Injector sysInjector = createSysInjector();
     DeleteZombieCommentsRefs cleanup =
         sysInjector.getInstance(DeleteZombieCommentsRefs.Factory.class).create(cleanupPercentage);
     cleanup.execute();
     return 0;
   }
 
-  private Injector getSysInjector() {
-    List<Module> modules = new ArrayList<>();
-    modules.add(
-        new AbstractModule() {
-          @Override
-          protected void configure() {
-            bind(Path.class).annotatedWith(SitePath.class).toInstance(getSitePath());
-            bind(ConsoleUI.class).toInstance(ConsoleUI.getInstance(false));
-            bind(String.class)
-                .annotatedWith(SecureStoreClassName.class)
-                .toProvider(Providers.of(getConfiguredSecureStoreClass()));
-            bind(MetricMaker.class).to(DisabledMetricMaker.class);
-            install(new FactoryModuleBuilder().build(DeleteZombieCommentsRefs.Factory.class));
-          }
-        });
-    modules.add(new GerritServerConfigModule());
-    modules.add(new SchemaModule());
-    return Guice.createInjector(modules);
+  private Injector createSysInjector() {
+    return createDbInjector()
+        .createChildInjector(
+            new WorkQueueModule(),
+            LuceneIndexModule.latestVersion(false, AutoFlush.ENABLED),
+            new BatchProgramModule(createDbInjector(), ImmutableSet.of()),
+            new AccountNoteDbReadStorageModule(),
+            new ExternalIdCacheImpl.ExternalIdCacheModule(),
+            new ExternalIdCacheImpl.ExternalIdCacheBindingModule(),
+            new NoteDbDraftCommentsModule(),
+            new NoteDbStarredChangesModule(),
+            new FactoryModuleBuilder().build(ChangeResource.Factory.class),
+            new FactoryModuleBuilder().build(DeleteZombieCommentsRefs.Factory.class));
   }
 }

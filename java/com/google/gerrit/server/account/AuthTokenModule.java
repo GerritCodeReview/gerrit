03f00c2c5254951bd766631fede7a503368c3e08
Thomas Dräbing <thomas.draebing@sap.com>
2025-02-27 15:03:00 +0100

    Implement support for storing authentication tokens
    So far HTTP passwords were cached as part of the `username` ExternalID.
    The current plan is to implement the use of multiple tokens with
    limited lifetime instead of never expiring HTTP passwords.
    
    This change adds the new notedb schema version that supports tokens.
    These tokens are stored in a file `tokens.config` in the user's ref in
    All-Users. They are stored in git config format:
    
    ```
    [token "some-token-id"]
      hash = bcrypt0:4:....
    ```
    
    This change also adds REST API endpoints to create, list and delete
    tokens.
    
    Release-Notes: Implement support for authentication tokens
    Change-Id: I0d90c88d48f16e192f1b025f14becf8f1d74ff15

--- java/com/google/gerrit/server/account/AuthTokenModule.java
+++ java/com/google/gerrit/server/account/AuthTokenModule.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 The Android Open Source Project
+// Copyright (C) 2025 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,10 +16,9 @@ package com.google.gerrit.server.account;
 
 import com.google.inject.AbstractModule;
 
-public class AccountModule extends AbstractModule {
+public class AuthTokenModule extends AbstractModule {
   @Override
-  protected void configure() {
-    bind(AuthRequest.Factory.class);
-    bind(AuthTokenVerifier.class);
+  public void configure() {
+    bind(AuthTokenAccessor.class).to(DirectAuthTokenAccessor.class);
   }
 }

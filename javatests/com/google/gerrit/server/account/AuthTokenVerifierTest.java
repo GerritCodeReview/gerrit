4f217124500226d2026480e2d62a406b5f6ac643
Thomas Dräbing <thomas.draebing@sap.com>
2025-02-27 15:03:00 +0100

    Implement support for authentication tokens
    So far HTTP passwords were cached as part of the `username` ExternalID.
    The current plan is to implement the use of multiple tokens with
    limited lifetime instead of never expiring HTTP passwords.
    
    This change adds the new notedb schema version that supports tokens.
    These tokens are stored in a file `tokens.config` in the user's ref in
    All-Users. They are stored in git config format:
    
    ```
    [token "some-token-id"]
      hash = bcrypt0:4:....
    ```
    
    This change also adds REST API endpoints to create, list and delete
    tokens.
    
    Release-Notes: Implement support for authentication tokens
    Change-Id: I0d90c88d48f16e192f1b025f14becf8f1d74ff15

---
+++ javatests/com/google/gerrit/server/account/AuthTokenVerifierTest.java
@@ -0,0 +1,71 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.account;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.server.account.externalids.ExternalId.SCHEME_USERNAME;
+import static org.mockito.Mockito.doReturn;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.server.account.externalids.ExternalId;
+import com.google.gerrit.server.account.externalids.ExternalIds;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuthTokenVerifierTest {
+  private static final Account.Id ACCOUNT_ID = Account.id(1);
+  private static final List<AuthToken> TOKENS =
+      List.of(
+          AuthToken.createWithPlainToken("id1", "hashedToken"),
+          AuthToken.createWithPlainToken("id2", "another_Token"));
+  private AuthTokenVerifier tokenVerifier;
+  @Mock private VersionedAuthTokens.Accessor tokenAccessor;
+  @Mock private ExternalIds externalIds;
+
+  @Before
+  public void setUp() throws Exception {
+    tokenVerifier = new AuthTokenVerifier(tokenAccessor, externalIds);
+    doReturn(TOKENS).when(tokenAccessor).getTokens(ACCOUNT_ID);
+    doReturn(
+            ImmutableSet.of(
+                ExternalId.create(
+                    ExternalId.Key.create(SCHEME_USERNAME, "user", false),
+                    ACCOUNT_ID,
+                    null,
+                    HashedPassword.fromPassword("secret").encode(),
+                    null)))
+        .when(externalIds)
+        .byAccount(ACCOUNT_ID, SCHEME_USERNAME);
+  }
+
+  @Test
+  public void checkToken() {
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "hashedToken")).isTrue();
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "another_Token")).isTrue();
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "invalid")).isFalse();
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "another_token")).isFalse();
+  }
+
+  @Test
+  public void checkHttpPassword() {
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "secret")).isTrue();
+  }
+}

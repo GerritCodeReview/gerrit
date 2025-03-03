da7ad5f432733430fe680421e96b017fce4c916a
Edwin Kempin <ekempin@google.com>
2025-03-03 11:02:21 +0000

    Add RetryListener that logs retries on info level
    It's useful to see in the logs when retries are happening and what
    caused them.
    
    Release-Notes: skip
    Change-Id: I83f205b8e6c1b35d4744af4c6a41759dc5594bcf
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/config/GerritGlobalModule.java
+++ java/com/google/gerrit/server/config/GerritGlobalModule.java
@@ -223,6 +223,7 @@ import com.google.gerrit.server.submitrequirement.predicate.HasSubmoduleUpdatePr
 import com.google.gerrit.server.submitrequirement.predicate.SubmitRequirementLabelExtensionPredicate;
 import com.google.gerrit.server.tools.ToolsCatalog;
 import com.google.gerrit.server.update.BatchUpdate;
+import com.google.gerrit.server.update.LoggingRetryListener;
 import com.google.gerrit.server.update.RetryListener;
 import com.google.gerrit.server.util.IdGenerator;
 import com.google.gerrit.server.util.ThreadLocalRequestContext;
@@ -475,6 +476,7 @@ public class GerritGlobalModule extends FactoryModule {
     DynamicSet.setOf(binder(), AttentionSetListener.class);
     DynamicSet.setOf(binder(), ValidationOptionsListener.class);
     DynamicSet.setOf(binder(), RetryListener.class);
+    DynamicSet.bind(binder(), RetryListener.class).to(LoggingRetryListener.class);
 
     DynamicMap.mapOf(binder(), MailFilter.class);
     bind(MailFilter.class).annotatedWith(Exports.named("ListMailFilter")).to(ListMailFilter.class);

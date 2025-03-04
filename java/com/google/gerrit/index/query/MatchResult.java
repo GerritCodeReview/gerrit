ba0fcffccec67b2fccc68b480ce75460bed4eb70
Allen Li <ayatane@chromium.org>
2025-03-04 00:27:37 +0000

    Expose atom explanation in submit requirement check.
    To improve debuggability of unsatisfied submit requirements, we expose
    explanations for submit requirement predicate atoms, which can then be
    exposed by the UI.
    
    The API that will be affected:
    https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#check-submit-requirement
    
    Bug: b/369788358
    Change-Id: Iafad2063ced0fdba62cf3c2fb15406123e282d9e

--- java/com/google/gerrit/index/query/MatchResult.java
+++ java/com/google/gerrit/index/query/MatchResult.java
@@ -14,17 +14,16 @@
 
 package com.google.gerrit.index.query;
 
-public interface Matchable<T> {
-  /** Does this predicate match this object? */
-  boolean match(T object);
+/** Result returned by {@link Matchable}. */
+public class MatchResult {
+  /** true if matches */
+  public final boolean status;
 
-  /** Returns detailed result for predicate matching an object */
-  MatchResult matchResult(T object);
+  /** explanation for why it matched or not */
+  public final String explanation;
 
-  /** Returns a cost estimate to run this predicate, higher figures cost more. */
-  int getCost();
-
-  default MatchResult matchResult(T object) {
-    return new MatchResult(match(object), "");
+  public MatchResult(boolean status, String explanation) {
+    this.status = status;
+    this.explanation = explanation;
   }
 }

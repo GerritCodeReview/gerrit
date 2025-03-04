8eb9969ad0f5cc029e0a65699fdf6f59f8564b96
Allen Li <ayatane@chromium.org>
2025-03-04 00:27:37 +0000

    Expose atom explanation in submit requirement check.
    To improve debuggability of unsatisfied submit requirements, we expose
    explanations for submit requirement predicate atoms, which can then be
    exposed by the UI.
    
    The API that will be affected:
    https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#check-submit-requirement
    
    Bug: b/369788358
    Change-Id: Iafad2063ced0fdba62cf3c2fb15406123e282d9e

--- java/com/google/gerrit/extensions/common/SubmitRequirementExpressionInfo.java
+++ java/com/google/gerrit/extensions/common/SubmitRequirementExpressionInfo.java
@@ -15,6 +15,7 @@
 package com.google.gerrit.extensions.common;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -45,14 +46,26 @@ public class SubmitRequirementExpressionInfo {
   public List<String> failingAtoms;
 
   /**
+   * Map of leaf predicates to their explanations.
+   *
+   * <p>
+   * This is used to provide more information about complex atoms, which may otherwise be opaque and
+   * hard to debug.
+   *
+   * <p>
+   * This will only be populated/implemented for some atoms.
+   */
+  public Map<String, String> atomExplanations;
+
+  /**
    * Optional error message. Contains an explanation of why the submit requirement expression failed
    * during its evaluation.
    */
   public String errorMessage;
 
   /**
-   * Values in this enum should match with values in {@link
-   * com.google.gerrit.entities.SubmitRequirementExpressionResult.Status}.
+   * Values in this enum should match with values in
+   * {@link com.google.gerrit.entities.SubmitRequirementExpressionResult.Status}.
    */
   public enum Status {
     /** Expression was evaluated and the result was true. */
@@ -77,15 +90,16 @@ public class SubmitRequirementExpressionInfo {
       return false;
     }
     SubmitRequirementExpressionInfo that = (SubmitRequirementExpressionInfo) o;
-    return fulfilled == that.fulfilled
-        && Objects.equals(expression, that.expression)
+    return fulfilled == that.fulfilled && Objects.equals(expression, that.expression)
         && Objects.equals(passingAtoms, that.passingAtoms)
         && Objects.equals(failingAtoms, that.failingAtoms)
+        && Objects.equals(atomExplanations, that.atomExplanations)
         && Objects.equals(errorMessage, that.errorMessage);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(expression, fulfilled, passingAtoms, failingAtoms, errorMessage);
+    return Objects.hash(expression, fulfilled, passingAtoms, failingAtoms, atomExplanations,
+        errorMessage);
   }
 }

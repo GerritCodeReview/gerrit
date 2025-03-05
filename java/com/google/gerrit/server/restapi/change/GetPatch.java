eed21de3259fab9d288428d660358e647d6c3c46
Luca Milanesio <luca.milanesio@gmail.com>
2025-03-05 09:04:01 +0000

    Remove noop BinaryResult.close() in GetPatch
    The cleanup of the resources is done with the standard try-with-resource
    construct in the GetPatch API call; there is no need anymore to manually
    trace and call any close method.
    
    Also, the BinaryResult.close() is empty by default; therefore the call
    is a no-op.
    
    Release-Notes: skip
    Change-Id: Id3263918ddd3bc4c25974c46e8812e439f91ba1a

--- java/com/google/gerrit/server/restapi/change/GetPatch.java
+++ java/com/google/gerrit/server/restapi/change/GetPatch.java
@@ -100,82 +100,75 @@ public class GetPatch implements RestReadView<RevisionResource> {
 
     try (Repository repo = repoManager.openRepository(rsrc.getProject());
         RevWalk rw = new RevWalk(repo)) {
-      BinaryResult bin = null;
-      try {
-        final RevCommit commit = rw.parseCommit(rsrc.getPatchSet().commitId());
-        RevCommit[] parents = commit.getParents();
-        if (parentNum == null && parents.length > 1) {
-          throw new ResourceConflictException("Revision has more than 1 parent.");
-        }
-        if (parents.length == 0) {
-          throw new ResourceConflictException("Revision has no parent.");
-        }
-        if (parentNum != null && (parentNum < 1 || parentNum > parents.length)) {
-          throw new BadRequestException(String.format("invalid parent number: %d", parentNum));
-        }
-        final RevCommit base = parents[parentNum == null ? 0 : parentNum - 1];
-        rw.parseBody(base);
-
-        bin =
-            new BinaryResult() {
-              @Override
-              public void writeTo(OutputStream out) throws IOException {
-                try (Repository repo = repoManager.openRepository(rsrc.getProject());
-                    RevWalk rw = new RevWalk(repo)) {
-                  switch (outputType) {
-                    case ZIP:
-                      ZipOutputStream zos = new ZipOutputStream(out);
-                      ZipEntry e = new ZipEntry(fileName(rw, commit));
-                      e.setTime(commit.getCommitTime() * 1000L);
-                      zos.putNextEntry(e);
-                      format(zos);
-                      zos.closeEntry();
-                      zos.finish();
-                      break;
-                    case RAW:
-                    case BASE64:
-                      format(out);
-                      break;
-                  }
-                }
-              }
-
-              private void format(OutputStream out) throws IOException {
-                // Only add header if no path is specified
-                if (path == null) {
-                  out.write(formatEmailHeader(commit).getBytes(UTF_8));
-                }
-                DiffUtil.getFormattedDiff(repo, base, commit, path, out);
-              }
-            };
-
-        if (path != null && bin.asString().isEmpty()) {
-          throw new ResourceNotFoundException(String.format("File not found: %s.", path));
-        }
-
-        switch (outputType) {
-          case ZIP:
-            bin.disableGzip()
-                .setContentType("application/zip")
-                .setAttachmentName(fileName(rw, commit) + ".zip");
-            break;
-          case BASE64:
-            bin.base64()
-                .setContentType("application/mbox")
-                .setAttachmentName(download ? fileName(rw, commit) + ".base64" : null);
-            break;
-          case RAW:
-            bin.setContentType("text/plain")
-                .setAttachmentName(download ? fileName(rw, commit) : null);
-            break;
-        }
-
-        return Response.ok(bin);
-      } finally {
-        if (bin != null) {
-          bin.close();
-        }
+      final RevCommit commit = rw.parseCommit(rsrc.getPatchSet().commitId());
+      RevCommit[] parents = commit.getParents();
+      if (parentNum == null && parents.length > 1) {
+        throw new ResourceConflictException("Revision has more than 1 parent.");
       }
+      if (parents.length == 0) {
+        throw new ResourceConflictException("Revision has no parent.");
+      }
+      if (parentNum != null && (parentNum < 1 || parentNum > parents.length)) {
+        throw new BadRequestException(String.format("invalid parent number: %d", parentNum));
+      }
+      final RevCommit base = parents[parentNum == null ? 0 : parentNum - 1];
+      rw.parseBody(base);
+
+      BinaryResult bin =
+          new BinaryResult() {
+            @Override
+            public void writeTo(OutputStream out) throws IOException {
+              try (Repository repo = repoManager.openRepository(rsrc.getProject());
+                  RevWalk rw = new RevWalk(repo)) {
+                switch (outputType) {
+                  case ZIP:
+                    ZipOutputStream zos = new ZipOutputStream(out);
+                    ZipEntry e = new ZipEntry(fileName(rw, commit));
+                    e.setTime(commit.getCommitTime() * 1000L);
+                    zos.putNextEntry(e);
+                    format(zos);
+                    zos.closeEntry();
+                    zos.finish();
+                    break;
+                  case RAW:
+                  case BASE64:
+                    format(out);
+                    break;
+                }
+              }
+            }
+
+            private void format(OutputStream out) throws IOException {
+              // Only add header if no path is specified
+              if (path == null) {
+                out.write(formatEmailHeader(commit).getBytes(UTF_8));
+              }
+              DiffUtil.getFormattedDiff(repo, base, commit, path, out);
+            }
+          };
+
+      if (path != null && bin.asString().isEmpty()) {
+        throw new ResourceNotFoundException(String.format("File not found: %s.", path));
+      }
+
+      switch (outputType) {
+        case ZIP:
+          bin.disableGzip()
+              .setContentType("application/zip")
+              .setAttachmentName(fileName(rw, commit) + ".zip");
+          break;
+        case BASE64:
+          bin.base64()
+              .setContentType("application/mbox")
+              .setAttachmentName(download ? fileName(rw, commit) + ".base64" : null);
+          break;
+        case RAW:
+          bin.setContentType("text/plain")
+              .setAttachmentName(download ? fileName(rw, commit) : null);
+          break;
+      }
+
+      return Response.ok(bin);
     }
   }
 

81de636dc26e1939566324f2974d4766e7a4a731
Thomas Dräbing <thomas.draebing@sap.com>
2025-03-05 15:10:00 +0100

    Allow to set tokens when creating a new account
    Change-Id: I99db669dca89b0fc5d3ce7267f9aaff157bc5e98

--- java/com/google/gerrit/server/account/VersionedAuthTokens.java
+++ java/com/google/gerrit/server/account/VersionedAuthTokens.java
@@ -32,6 +32,7 @@ import com.google.inject.Singleton;
 import com.google.inject.assistedinject.Assisted;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.CommitBuilder;
@@ -93,6 +94,15 @@ public class VersionedAuthTokens extends VersionedMetaData {
       AuthToken token = authokens.addToken(id, hashedToken);
       commit(authokens);
       return token;
+    }
+
+    public synchronized void addTokens(Account.Id accountId, Collection<AuthToken> tokens)
+        throws IOException, ConfigInvalidException, AuthTokenConflictException {
+      VersionedAuthTokens authorizedTokens = read(accountId);
+      for (AuthToken token : tokens) {
+        authorizedTokens.addToken(token);
+      }
+      commit(authorizedTokens);
     }
 
     public synchronized void deleteToken(Account.Id accountId, String id)

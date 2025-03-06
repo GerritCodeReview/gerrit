be99d3cd9e616f9b740550005c31b9daf2a1a2d8
Thomas Dräbing <thomas.draebing@sap.com>
2025-03-06 13:51:29 +0100

    Create initial admin account with token instead of HTTP password
    Change-Id: I145637ec8ba769864259e9c10320a4793d8ca736

---
+++ java/com/google/gerrit/pgm/init/VersionedAuthorizationTokensOnInit.java
@@ -0,0 +1,86 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm.init;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.base.Strings;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.pgm.init.api.AllUsersNameOnInitProvider;
+import com.google.gerrit.pgm.init.api.InitFlags;
+import com.google.gerrit.pgm.init.api.VersionedMetaDataOnInit;
+import com.google.gerrit.server.account.Token;
+import com.google.gerrit.server.account.VersionedAuthorizationTokens;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import java.io.IOException;
+import java.util.List;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.CommitBuilder;
+import org.eclipse.jgit.lib.Config;
+
+public class VersionedAuthorizationTokensOnInit extends VersionedMetaDataOnInit {
+  public interface Factory {
+    VersionedAuthorizationTokensOnInit create(Account.Id accountId);
+  }
+
+  private List<Token> tokens;
+
+  @Inject
+  public VersionedAuthorizationTokensOnInit(
+      AllUsersNameOnInitProvider allUsers,
+      SitePaths site,
+      InitFlags flags,
+      @Assisted Account.Id accountId) {
+    super(flags, site, allUsers.get(), RefNames.refsUsers(accountId));
+  }
+
+  @Override
+  public VersionedAuthorizationTokensOnInit load() throws IOException, ConfigInvalidException {
+    super.load();
+    return this;
+  }
+
+  @Override
+  protected void onLoad() throws IOException, ConfigInvalidException {
+    tokens = VersionedAuthorizationTokens.parse(readUTF8(VersionedAuthorizationTokens.FILE_NAME));
+  }
+
+  @CanIgnoreReturnValue
+  public Token addToken(String id, String t) {
+    checkState(tokens != null, "Tokens not loaded yet");
+    Token token = Token.createWithPlainToken(id, t);
+    tokens.add(token);
+    return token;
+  }
+
+  @Override
+  protected boolean onSave(CommitBuilder commit) throws IOException {
+    if (Strings.isNullOrEmpty(commit.getMessage())) {
+      commit.setMessage("Updated tokens\n");
+    }
+
+    Config tokenConfig = new Config();
+    for (Token token : tokens) {
+      tokenConfig.setString("token", token.id(), "hash", token.hashedToken());
+    }
+
+    saveUTF8(VersionedAuthorizationTokens.FILE_NAME, tokenConfig.toText());
+    return true;
+  }
+}

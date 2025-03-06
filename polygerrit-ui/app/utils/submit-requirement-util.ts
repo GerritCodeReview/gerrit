545bd259042d3280a48b23c817dd4968bf708ed0
Allen Li <ayatane@google.com>
2025-03-06 01:24:09 +0000

    Expose submit requirement explanations in UI.
    This exposes the predicate atom explanations for submit requirements
    in the Web UI, which was added to the submit requirement check API
    backend in https://gerrit-review.git.corp.google.com/c/gerrit/+/457722
    
    Change-Id: Ide8c4df6e786c95a6f7848ab3ae6168b56cc838f

--- polygerrit-ui/app/utils/submit-requirement-util.ts
+++ polygerrit-ui/app/utils/submit-requirement-util.ts
@@ -16,18 +16,22 @@ export interface SubmitRequirementExpressionPart {
   isAtom: boolean;
   // Defined iff isAtom is true.
   atomStatus?: SubmitRequirementExpressionAtomStatus;
+  // Defined iff isAtom is true, but may be empty.
+  atomExplanation?: string;
 }
 
 interface AtomMatch {
   start: number;
   end: number;
   isPassing: boolean;
+  explanation: string;
 }
 
 function appendAllOccurrences(
   text: string,
   match: string,
   isPassing: boolean,
+  explanation: string,
   matchedAtoms: AtomMatch[]
 ) {
   for (let searchStartIndex = 0; ; ) {
@@ -46,6 +50,7 @@ function appendAllOccurrences(
       start: index,
       end: searchStartIndex,
       isPassing: atomIsPassing,
+      explanation,
     });
   }
 }
@@ -56,7 +61,7 @@ function splitExpressionIntoParts(
 ): SubmitRequirementExpressionPart[] {
   const result: SubmitRequirementExpressionPart[] = [];
   let currentIndex = 0;
-  for (const {start, end, isPassing} of matchedAtoms) {
+  for (const {start, end, isPassing, explanation} of matchedAtoms) {
     // We don't handle overlapping matches, but this can happen.
     if (start < currentIndex) continue;
     if (start > currentIndex) {
@@ -71,6 +76,7 @@ function splitExpressionIntoParts(
       atomStatus: isPassing
         ? SubmitRequirementExpressionAtomStatus.PASSING
         : SubmitRequirementExpressionAtomStatus.FAILING,
+      atomExplanation: explanation,
     });
     currentIndex = end;
   }
@@ -96,20 +102,14 @@ export function atomizeExpression(
   const matchedAtoms: AtomMatch[] = [];
   expression.passing_atoms?.forEach(atom =>
     appendAllOccurrences(
-      expression.expression,
-      atom,
-      /* isPassing=*/ true,
-      matchedAtoms
-    )
-  );
+      expression.expression, atom,
+      /* isPassing=*/ true, expression.atom_explanations?.[atom] ?? '',
+      matchedAtoms));
   expression.failing_atoms?.forEach(atom =>
     appendAllOccurrences(
-      expression.expression,
-      atom,
-      /* isPassing=*/ false,
-      matchedAtoms
-    )
-  );
+      expression.expression, atom,
+      /* isPassing=*/ false, expression.atom_explanations?.[atom] ?? '',
+      matchedAtoms));
   matchedAtoms.sort((a, b) => a.start - b.start);
 
   return splitExpressionIntoParts(expression.expression, matchedAtoms);

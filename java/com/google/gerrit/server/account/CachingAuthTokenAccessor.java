685e964ec80232c217575b6f8b3d53d57f02410b
Thomas Dräbing <thomas.draebing@sap.com>
2025-03-07 12:11:53 +0100

    Add offline migration tool for migrating HTTP passwords to tokens
    This change adds a tool to migrate HTTP passwords of all accounts
    to authentication tokens offline.
    
    Release-Notes: Offline migration of HTTP passwords to tokens
    Change-Id: I3c9a1bd5ef415f08075535414c42c540459653f5

--- java/com/google/gerrit/server/account/CachingAuthTokenAccessor.java
+++ java/com/google/gerrit/server/account/CachingAuthTokenAccessor.java
@@ -15,6 +15,7 @@
 package com.google.gerrit.server.account;
 
 import com.google.common.collect.ImmutableList;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import com.google.gerrit.entities.Account;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -62,6 +63,15 @@ public class CachingAuthTokenAccessor implements AuthTokenAccessor {
       throws IOException, ConfigInvalidException, AuthTokenConflictException {
     directAuthTokenAccessor.addTokens(accountId, tokens);
     authTokenCache.evict(accountId);
+  }
+
+  @Override
+  @CanIgnoreReturnValue
+  public synchronized AuthToken addToken(Account.Id accountId, String id, String hashedToken)
+      throws IOException, ConfigInvalidException, InvalidAuthTokenException {
+    AuthToken token = directAuthTokenAccessor.addToken(accountId, id, hashedToken);
+    authTokenCache.evict(accountId);
+    return token;
   }
 
   @Override

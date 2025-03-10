2156f39281c55b6bd12a16320463590fff8ff40b
Youssef Elghareeb <ghareeb@google.com>
2025-03-10 05:45:09 -0700

    gr-selection-action-box: handle slotted content
    The change allows users to dynamically add content to the tooltip, which is useful to replace current tooltip with "Press c to comment" with any other tooltip. This change maintains current behaviour for Gerrit.
    
    Change-Id: Ife375e784c7f3ba61a1a461e581f0d3a512bd3fc
    Release-Notes: skip
    Google-Bug-Id: b/400414094

--- polygerrit-ui/app/embed/diff/gr-selection-action-box/gr-selection-action-box_test.ts
+++ polygerrit-ui/app/embed/diff/gr-selection-action-box/gr-selection-action-box_test.ts
@@ -32,16 +32,18 @@ suite('gr-selection-action-box', () => {
   });
 
   test('renders', () => {
-    assert.shadowDom.equal(
-      element,
+    assert.equal(
+      element.innerHTML,
       /* HTML */ `
-        <gr-tooltip
-          invisible
-          id="tooltip"
-          text="Press c to comment"
-        ></gr-tooltip>
-      `
-    );
+        <slot name="selectionActionBox" style="visibility: 'hidden'">
+          <gr-tooltip
+            invisible
+            id="tooltip"
+            text="Press c to comment"
+          ></gr-tooltip>
+        </slot>
+
+        `);
   });
 
   test('ignores regular keys', () => {
@@ -111,10 +113,12 @@ suite('gr-selection-action-box', () => {
     test('renders visible', async () => {
       await element.placeAbove(target);
       await element.updateComplete;
-      assert.shadowDom.equal(
-        element,
+      assert.equal(
+        element.innerHTML,
         /* HTML */ `
-          <gr-tooltip id="tooltip" text="Press c to comment"></gr-tooltip>
+          <slot name="selectionActionBox" style="visibility: 'visible'">
+            <gr-tooltip id="tooltip" text="Press c to comment"></gr-tooltip>
+          </slot>
         `
       );
     });

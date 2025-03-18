8a1f3064e5cda932cf642cfcb76e28bac0172781
Thomas Dräbing <thomas.draebing@sap.com>
2025-03-18 14:19:39 +0100

    Use auth token API instead of HTTP password API in UI
    The web UI was still using the deprecated HTTP Password API and was
    only able to display this one token.
    
    Now, the UI uses the new token API and will list all existing tokens.
    The IDs of tokens are shown in a table and each token can be deleted.
    New tokens can be generated and will be shown once in a dialog as
    before.
    
    Release-Notes: UI adapted to authentication tokens
    Change-Id: I013bb2a95ae56042fa5a3819dd69b9dc95b97c95

--- polygerrit-ui/app/services/gr-rest-api/gr-rest-api-impl.ts
+++ polygerrit-ui/app/services/gr-rest-api/gr-rest-api-impl.ts
@@ -75,7 +75,6 @@ import {
   NumericChangeId,
   PARENT,
   ParsedJSON,
-  Password,
   PatchRange,
   PatchSetNum,
   PathToRobotCommentsInfoMap,
@@ -107,6 +106,7 @@ import {
   ListChangesOption,
   ReviewResult,
   SubmitRequirementInfo,
+  TokenInfo,
 } from '../../types/common';
 import {
   DiffInfo,
@@ -3141,24 +3141,31 @@ export class GrRestApiServiceImpl implements RestApiService, Finalizable {
     }) as unknown as Promise<Hashtag[] | undefined>;
   }
 
-  deleteAccountHttpPassword(): Promise<Response> {
+  getAccountAuthTokens(): Promise<TokenInfo[] | undefined> {
+    return this._restApiHelper.fetchJSON({
+      url: '/accounts/self/tokens',
+      reportUrlAsIs: true,
+    }) as Promise<TokenInfo[] | undefined>;
+  }
+
+  deleteAccountAuthToken(tokenId: string): Promise<Response> {
     return this._restApiHelper.fetch({
       fetchOptions: {method: HttpMethod.DELETE},
-      url: '/accounts/self/password.http',
+      url: `/accounts/self/tokens/${tokenId}`,
       reportUrlAsIs: true,
       reportServerError: true,
     });
   }
 
-  generateAccountHttpPassword(): Promise<Password | undefined> {
+  generateAccountAuthToken(tokenId: string): Promise<TokenInfo | undefined> {
     return this._restApiHelper.fetchJSON({
       fetchOptions: getFetchOptions({
         method: HttpMethod.PUT,
-        body: {generate: true},
+        body: {id: tokenId},
       }),
-      url: '/accounts/self/password.http',
+      url: `/accounts/self/tokens/${tokenId}`,
       reportUrlAsIs: true,
-    }) as Promise<unknown> as Promise<Password>;
+    }) as Promise<unknown> as Promise<TokenInfo>;
   }
 
   getAccountSSHKeys() {

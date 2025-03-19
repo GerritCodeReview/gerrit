c4ef961c0a0cd19923bee25677239d3af48276cf
Thomas Dräbing <thomas.draebing@sap.com>
2025-03-19 15:42:27 +0100

    Allow to configure a lifetime for auth tokens
    HTTP passwords and authentication tokens so far always had an
    unlimited lifetime. This is not considered safe, since a
    compromised password/token is potentially valid forever.
    
    Now, authentication tokens can be assigned a lifetime. The
    minimum lifetime is 1 minute. Otherwise, it can be freely set.
    If no lifetime has been provided, it will be valid forever.
    Tokens outside their lifetime will not be considered, when
    verifying the provided credential during authentication.
    
    Release-Notes: Allow to configure a lifetime for auth tokens
    Change-Id: I6721dbe1eb6728a13904685a397a1fecde0bd5d8

--- javatests/com/google/gerrit/server/account/AuthTokenVerifierTest.java
+++ javatests/com/google/gerrit/server/account/AuthTokenVerifierTest.java
@@ -17,8 +17,14 @@ package com.google.gerrit.server.account;
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.doReturn;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableList;
 import com.google.gerrit.entities.Account;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,23 +35,43 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class AuthTokenVerifierTest {
   private static final Account.Id ACCOUNT_ID = Account.id(1);
   private AuthTokenVerifier tokenVerifier;
-  @Mock private DirectAuthTokenAccessor tokenAccessor;
+  private LoadingCache<Account.Id, List<AuthToken>> cache;
+  @Mock AuthTokenCache.Loader loader;
 
   @Before
   public void setUp() throws Exception {
-    tokenVerifier = new AuthTokenVerifier(tokenAccessor);
     ImmutableList<AuthToken> tokens =
         ImmutableList.of(
             AuthToken.createWithPlainToken("id1", "hashedToken"),
-            AuthToken.createWithPlainToken("id2", "another_Token"));
-    doReturn(tokens).when(tokenAccessor).getTokens(ACCOUNT_ID);
+            AuthToken.createWithPlainToken("id2", "another_Token"),
+            AuthToken.createWithPlainToken(
+                "id3", "tokenWithLifetime", Optional.of(Instant.now().plus(1, ChronoUnit.DAYS))),
+            AuthToken.createWithPlainToken(
+                "id4",
+                "tokenWithExpiredLifetime",
+                Optional.of(Instant.now().minus(1, ChronoUnit.DAYS))));
+    doReturn(tokens).when(loader).load(ACCOUNT_ID);
+    cache = CacheBuilder.newBuilder().build(loader);
+    AuthTokenCache authTokenCache = new AuthTokenCache(cache);
+    AuthTokenAccessor tokenAccessor = new CachingAuthTokenAccessor(authTokenCache, null);
+    tokenVerifier = new AuthTokenVerifier(tokenAccessor);
   }
 
   @Test
-  public void checkToken() {
+  public void checkTokenWithoutLimitedLifetime() {
     assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "hashedToken")).isTrue();
     assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "another_Token")).isTrue();
     assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "invalid")).isFalse();
     assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "another_token")).isFalse();
+  }
+
+  @Test
+  public void assertThatTokensSucceedAuthenticationWithinLifetime() {
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "tokenWithLifetime")).isTrue();
+  }
+
+  @Test
+  public void assertThatExpiredTokensFailAuthentication() {
+    assertThat(tokenVerifier.checkToken(ACCOUNT_ID, "tokenWithExpiredLifetime")).isFalse();
   }
 }

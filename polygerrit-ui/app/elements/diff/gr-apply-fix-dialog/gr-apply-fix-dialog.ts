dd79bac6daa5067a9e3e23510b6efbdb57659e61
Milutin Kristofic <milutin@google.com>
2025-03-20 06:38:35 -0700

    Test 2
    testing
    
    Change-Id: Ia805761ba60df648fd3e2a50f2685ead193b7e29
    Signed-off-by: Milutin Kristofic <milutin@google.com>

--- polygerrit-ui/app/elements/diff/gr-apply-fix-dialog/gr-apply-fix-dialog.ts
+++ polygerrit-ui/app/elements/diff/gr-apply-fix-dialog/gr-apply-fix-dialog.ts
@@ -1,9 +1,9 @@
 /**
  * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-3.0
  */
-import '../../../styles/shared-styles';
+import '../../../styles/shared-styles';s
 import '../../shared/gr-dialog/gr-dialog';
 import '../../shared/gr-icon/gr-icon';
 import '../../../embed/diff/gr-diff/gr-diff';

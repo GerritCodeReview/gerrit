b6cb5952537292d2abb1326211b397e29f00eff2
Thomas Dräbing <thomas.draebing@sap.com>
2025-03-21 09:11:59 +0100

    Allow to set default lifetime during migration to tokens
    A default lifetime for tokens can now be configured during
    offline migration from HTTP passwords to authentication tokens.
    
    Release-Notes: skip
    Change-Id: I1732d273fff22b5edafda32956bc5a1fdcbeedc8

--- java/com/google/gerrit/server/restapi/account/CreateToken.java
+++ java/com/google/gerrit/server/restapi/account/CreateToken.java
@@ -141,8 +141,7 @@ public class CreateToken
     try {
       token = tokensAccessor.addPlainToken(user.getAccountId(), id, newToken, expiration);
     } catch (InvalidAuthTokenException e) {
-      throw RestApiException.wrap(
-          String.format("Invalid token configuration: %s", e.getMessage()), e);
+      throw new BadRequestException(e.getMessage(), e);
     }
     try {
       emailFactories
@@ -167,22 +166,23 @@ public class CreateToken
 
   public static Optional<Instant> getExpirationInstant(AuthTokenInput input)
       throws BadRequestException {
-    if (input.lifetime == null || input.lifetime.isBlank()) {
+    return getExpirationInstant(input.lifetime);
+  }
+
+  public static Optional<Instant> getExpirationInstant(String lifetime) throws BadRequestException {
+    if (lifetime == null || lifetime.isBlank()) {
       return Optional.empty();
     }
-    long lifetime;
-    if (Strings.isNullOrEmpty(input.lifetime)) {
-      return Optional.empty();
-    }
+    long lifetimeMinutes;
     try {
-      lifetime = ConfigUtil.getTimeUnit(input.lifetime, 0, TimeUnit.MINUTES);
+      lifetimeMinutes = ConfigUtil.getTimeUnit(lifetime, 0, TimeUnit.MINUTES);
     } catch (IllegalArgumentException e) {
-      throw new BadRequestException("Invalid lifetime: " + input.lifetime, e);
+      throw new BadRequestException("Invalid lifetime: " + lifetime, e);
     }
-    if (lifetime <= 0) {
+    if (lifetimeMinutes <= 0) {
       throw new BadRequestException("Lifetime must be larger than 0");
     }
-    return Optional.of(Instant.now().plus(lifetime, ChronoUnit.MINUTES));
+    return Optional.of(Instant.now().plus(lifetimeMinutes, ChronoUnit.MINUTES));
   }
 
   @UsedAt(UsedAt.Project.PLUGIN_SERVICEUSER)

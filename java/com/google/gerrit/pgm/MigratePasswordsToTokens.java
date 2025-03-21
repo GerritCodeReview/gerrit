6d9bdda024569129b9d5f367f962f9780173a5f4
Thomas Dräbing <thomas.draebing@sap.com>
2025-03-21 13:02:41 +0100

    Add REST API to migrate HTTP passwords to tokens
    So far, HTTP passwords could only be migrated offline or by each user
    regenerating their tokens.
    
    This change adds a REST API to migrate all HTTP passwords to tokens.
    This will add the existing token with an optional default lifetime
    to the user's ref and afterwards delete all passwords from the
    external IDs.
    
    Release-Notes: Add REST API to migrate HTTP passwords to tokens
    Change-Id: Ic8407f927c3903b9c357b5d5d31f76b73e54481e

--- java/com/google/gerrit/pgm/MigratePasswordsToTokens.java
+++ java/com/google/gerrit/pgm/MigratePasswordsToTokens.java
@@ -14,69 +14,57 @@
 
 package com.google.gerrit.pgm;
 
-import static com.google.gerrit.server.account.externalids.ExternalId.SCHEME_USERNAME;
-import static com.google.gerrit.server.restapi.account.PutHttpPassword.DEFAULT_ID;
-
 import com.google.common.collect.ImmutableSet;
-import com.google.common.flogger.FluentLogger;
-import com.google.gerrit.entities.Account;
-import com.google.gerrit.exceptions.DuplicateKeyException;
 import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.lifecycle.LifecycleManager;
+import com.google.gerrit.lucene.LuceneIndexModule;
 import com.google.gerrit.pgm.init.VersionedAuthTokensOnInit;
 import com.google.gerrit.pgm.init.api.ConsoleUI;
 import com.google.gerrit.pgm.init.api.InstallAllPlugins;
 import com.google.gerrit.pgm.init.api.InstallPlugins;
 import com.google.gerrit.pgm.init.api.Section;
+import com.google.gerrit.pgm.util.BatchProgramModule;
 import com.google.gerrit.pgm.util.SiteProgram;
-import com.google.gerrit.server.account.InvalidAuthTokenException;
+import com.google.gerrit.server.DefaultRefLogIdentityProvider;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AuthTokenCache;
+import com.google.gerrit.server.account.AuthTokenModule;
+import com.google.gerrit.server.account.PasswordMigrator;
 import com.google.gerrit.server.account.VersionedAuthTokens;
-import com.google.gerrit.server.account.externalids.ExternalId;
-import com.google.gerrit.server.account.externalids.ExternalIdFactory;
-import com.google.gerrit.server.account.externalids.ExternalIds;
-import com.google.gerrit.server.account.externalids.storage.notedb.DisabledExternalIdCache;
+import com.google.gerrit.server.account.externalids.storage.notedb.ExternalIdCacheImpl;
 import com.google.gerrit.server.account.externalids.storage.notedb.ExternalIdNoteDbReadStorageModule;
 import com.google.gerrit.server.account.externalids.storage.notedb.ExternalIdNoteDbWriteStorageModule;
-import com.google.gerrit.server.account.externalids.storage.notedb.ExternalIdNotes;
-import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.account.storage.notedb.AccountNoteDbReadStorageModule;
+import com.google.gerrit.server.change.ChangeResource;
 import com.google.gerrit.server.extensions.events.GitReferenceUpdated;
-import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.MultiProgressMonitor;
+import com.google.gerrit.server.git.WorkQueue.WorkQueueModule;
 import com.google.gerrit.server.git.meta.MetaDataUpdate;
+import com.google.gerrit.server.index.options.AutoFlush;
+import com.google.gerrit.server.index.project.ProjectIndexerImpl;
+import com.google.gerrit.server.notedb.NoteDbDraftCommentsModule;
+import com.google.gerrit.server.notedb.NoteDbStarredChangesModule;
+import com.google.gerrit.server.project.DefaultLockManager;
+import com.google.gerrit.server.project.LockManager;
 import com.google.gerrit.server.restapi.account.CreateToken;
 import com.google.gerrit.server.schema.NoteDbSchemaVersionCheck;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
-import com.google.inject.Provider;
 import com.google.inject.TypeLiteral;
-import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import org.eclipse.jgit.errors.ConfigInvalidException;
-import org.eclipse.jgit.lib.ProgressMonitor;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.TextProgressMonitor;
 import org.kohsuke.args4j.Option;
 
 /** Converts HTTP passwords for all accounts to tokens */
 public class MigratePasswordsToTokens extends SiteProgram {
-  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-
   private final LifecycleManager manager = new LifecycleManager();
-  private final TextProgressMonitor monitor = new TextProgressMonitor();
 
   private Optional<Instant> lifetime = Optional.empty();
 
-  @Inject private GitRepositoryManager repoManager;
-  @Inject private AllUsersName allUsersName;
-  @Inject private Provider<MetaDataUpdate.Server> metaDataUpdateServerFactory;
-  @Inject private ExternalIdNotes.FactoryNoReindex externalIdNotesFactory;
-  @Inject private ExternalIdFactory externalIdFactory;
-  @Inject private ExternalIds externalIds;
-  @Inject private VersionedAuthTokensOnInit.Factory tokenFactory;
+  @Inject private PasswordMigrator.Factory passwordMigratorFactory;
 
   @Option(name = "--lifetime", usage = "The lifetime of migrated tokens.")
   public void setDefaultLifetime(String value) throws BadRequestException {
@@ -104,66 +92,38 @@ public class MigratePasswordsToTokens extends SiteProgram {
                 bind(new TypeLiteral<List<String>>() {})
                     .annotatedWith(InstallPlugins.class)
                     .toInstance(new ArrayList<>());
+                bind(LockManager.class).toInstance(new DefaultLockManager());
 
+                factory(PasswordMigrator.Factory.class);
                 factory(MetaDataUpdate.InternalFactory.class);
                 factory(VersionedAuthTokens.Factory.class);
                 factory(VersionedAuthTokensOnInit.Factory.class);
                 factory(Section.Factory.class);
+                factory(MultiProgressMonitor.Factory.class);
+                factory(ProjectIndexerImpl.Factory.class);
+                factory(ChangeResource.Factory.class);
+                bind(IdentifiedUser.GenericFactory.class);
 
                 install(new ExternalIdNoteDbReadStorageModule());
                 install(new ExternalIdNoteDbWriteStorageModule());
-                install(DisabledExternalIdCache.module());
+                install(new ExternalIdCacheImpl.ExternalIdCacheModule());
+                install(new ExternalIdCacheImpl.ExternalIdCacheBindingModule());
+                install(new DefaultRefLogIdentityProvider.Module());
+                install(new BatchProgramModule(dbInjector, ImmutableSet.of()));
+                install(LuceneIndexModule.latestVersion(false, AutoFlush.ENABLED));
+                install(new AccountNoteDbReadStorageModule());
+                install(new NoteDbStarredChangesModule());
+                install(new NoteDbDraftCommentsModule());
+                install(new WorkQueueModule());
+                install(AuthTokenCache.module());
+                install(new AuthTokenModule());
               }
             })
         .injectMembers(this);
 
-    monitor.beginTask("Collecting accounts", ProgressMonitor.UNKNOWN);
-    ImmutableSet<ExternalId> todo =
-        ImmutableSet.copyOf(
-            externalIds.all().stream()
-                .filter(e -> e.key().scheme().equals(SCHEME_USERNAME) && e.password() != null)
-                .collect(Collectors.toSet()));
-    monitor.endTask();
-
-    if (todo.isEmpty()) {
-      logger.atInfo().log("No accounts with HTTP passwords exist. Nothing to do.");
-      return 0;
-    }
-
-    monitor.beginTask("Migrating HTTP passwords", todo.size());
-    try (Repository repo = repoManager.openRepository(allUsersName)) {
-      ExternalIdNotes extIdNotes = externalIdNotesFactory.load(repo);
-      for (ExternalId extId : todo) {
-        migratePasswordToToken(extId, extIdNotes);
-        monitor.update(1);
-      }
-      try (MetaDataUpdate metaDataUpdate = metaDataUpdateServerFactory.get().create(allUsersName)) {
-        metaDataUpdate.setMessage("Migrate HTTP passwords to tokens");
-        extIdNotes.commit(metaDataUpdate);
-      }
-    }
-
-    monitor.endTask();
+    passwordMigratorFactory.create(lifetime).run();
 
     manager.stop();
     return 0;
-  }
-
-  private void migratePasswordToToken(ExternalId extId, ExternalIdNotes extIdNotes)
-      throws DuplicateKeyException, IOException, ConfigInvalidException, InvalidAuthTokenException {
-    String hashedPassword = extId.password();
-    Account.Id accountId = extId.accountId();
-    VersionedAuthTokensOnInit authTokens = tokenFactory.create(accountId).load();
-    if (authTokens.getToken(DEFAULT_ID) != null) {
-      logger.atWarning().log(
-          "Account %d has already a legacy token, not adding another one", accountId.get());
-      return;
-    }
-    authTokens.addToken(DEFAULT_ID, hashedPassword, lifetime);
-    authTokens.save("Migration of HTTP password to token");
-
-    ExternalId updatedExtId =
-        externalIdFactory.createWithEmail(extId.key(), extId.accountId(), extId.email());
-    extIdNotes.replace(extId, updatedExtId);
   }
 }

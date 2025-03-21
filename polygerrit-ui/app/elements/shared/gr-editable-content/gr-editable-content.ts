0673c289a926b3f1e28501692305f1ed29744487
Milutin Kristofic <milutin@google.com>
2025-03-21 09:43:08 +0100

    Reduced debounce delay for format from 3000ms to 500ms.
    500ms is sufficient to detect pauses even for users with slower typing
    speeds (below 20 WPM).
    
    We also skip debounce after clicking format.
    
    Google-Bug-Id: b/331395735
    Release-Notes: skip
    Change-Id: I86c95f45d36891589bc773dd79cabc9b2880bf61

--- polygerrit-ui/app/elements/shared/gr-editable-content/gr-editable-content.ts
+++ polygerrit-ui/app/elements/shared/gr-editable-content/gr-editable-content.ts
@@ -68,6 +68,8 @@ declare global {
   }
 }
 
+const DEBOUNCE_DELAY_MS = 500;
+
 @customElement('gr-editable-content')
 export class GrEditableContent extends LitElement {
   /**
@@ -452,14 +454,15 @@ export class GrEditableContent extends LitElement {
     ).textarea.focus();
   }
 
-  private updateFormatState() {
+  private updateFormatState(skipDebounce = false) {
     if (!this.newContent) return;
 
     // Run immediately first time
-    if (!this.formatCheckTask) {
+    if (!this.formatCheckTask || skipDebounce) {
       this.formatDisabled =
         formatCommitMessageString(this.newContent) === this.newContent;
       this.formattedErrors = detectFormattingErrorsInString(this.newContent);
+      return;
     }
 
     // Then debounce subsequent calls
@@ -470,7 +473,7 @@ export class GrEditableContent extends LitElement {
           formatCommitMessageString(this.newContent) === this.newContent;
         this.formattedErrors = detectFormattingErrorsInString(this.newContent);
       },
-      3000
+      DEBOUNCE_DELAY_MS
     );
   }
 
@@ -662,6 +665,8 @@ export class GrEditableContent extends LitElement {
     const newSelectionStart = Math.min(selectionStart, newValue.length);
     const newSelectionEnd = Math.min(selectionEnd, newValue.length);
     textarea.setSelectionRange(newSelectionStart, newSelectionEnd);
+
+    this.updateFormatState(/* skipDebounce= */ true);
   }
 
   private setCommitterEmail(e: CustomEvent<{value: string}>) {

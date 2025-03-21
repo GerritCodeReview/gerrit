1a751885616db33318d1df8d66e6c00dd20d7d45
Thomas Dräbing <thomas.draebing@sap.com>
2025-03-21 13:02:41 +0100

    Add REST API to migrate HTTP passwords to tokens
    So far, HTTP passwords could only be migrated offline or by each user
    regenerating their tokens.
    
    This change adds a REST API to migrate all HTTP passwords to tokens.
    This will add the existing token with an optional default lifetime
    to the user's ref and afterwards delete all passwords from the
    external IDs.
    
    Release-Notes: Add REST API to migrate HTTP passwords to tokens
    Change-Id: Ic8407f927c3903b9c357b5d5d31f76b73e54481e

--- java/com/google/gerrit/server/account/AuthTokenCacheImpl.java
+++ java/com/google/gerrit/server/account/AuthTokenCacheImpl.java
@@ -107,12 +107,9 @@ public class AuthTokenCacheImpl implements AuthTokenCache {
           TraceContext.newTimer(
               "Loading authentication tokens for account with username",
               Metadata.builder().accountId(accountId.get()).build())) {
-        AccountState accountState = accountCache.getEvenIfMissing(accountId);
-        Optional<ExternalId> optUser =
-            accountState.externalIds().stream()
-                .filter(e -> e.key().scheme().equals(SCHEME_USERNAME))
-                .findFirst();
-        if (!optUser.isPresent()) {
+        Optional<AccountState> accountState = accountCache.get(accountId);
+
+        if (accountState.isEmpty()) {
           return NO_SUCH_USER;
         }
 
@@ -121,7 +118,7 @@ public class AuthTokenCacheImpl implements AuthTokenCache {
 
         // Fall back to legacy HTTP password if no tokens are present.
         if (tokens.isEmpty()) {
-          Optional<AuthToken> legacyHttpPassword = getLegacyHttpPassword(accountId);
+          Optional<AuthToken> legacyHttpPassword = getLegacyHttpPassword(accountState.get());
           if (legacyHttpPassword.isPresent()) {
             return List.of(legacyHttpPassword.get());
           }
@@ -133,8 +130,7 @@ public class AuthTokenCacheImpl implements AuthTokenCache {
     }
 
     @Deprecated
-    private Optional<AuthToken> getLegacyHttpPassword(Account.Id accountId) {
-      AccountState accountState = accountCache.getEvenIfMissing(accountId);
+    private Optional<AuthToken> getLegacyHttpPassword(AccountState accountState) {
       Optional<ExternalId> optUser =
           accountState.externalIds().stream()
               .filter(e -> e.key().scheme().equals(SCHEME_USERNAME))

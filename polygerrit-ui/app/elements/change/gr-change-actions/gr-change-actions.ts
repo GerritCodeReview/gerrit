d2159955914d315f4ce64afd6128db05dbb5970c
Ben Rohlfs <brohlfs@google.com>
2025-04-07 02:03:46 -0700

    test change, please ignore review requests long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long
    Change-Id: I43505ea389d6774431df1e4b7b742ee5ff1605f2

--- polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.ts
+++ polygerrit-ui/app/elements/change/gr-change-actions/gr-change-actions.ts
@@ -2,6 +2,8 @@
  * @license
  * Copyright 2016 Google LLC
  * SPDX-License-Identifier: Apache-2.0
+ *
+ * DO NOT SUBMIT: Just a test. And another test.
  */
 import '../../admin/gr-create-change-dialog/gr-create-change-dialog';
 import '../../shared/gr-button/gr-button';

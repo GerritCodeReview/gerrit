e7d5292b72873837f13b4170e374f9b91e542459
Fabio Ponciroli <Ponch78@gmail.com>
2025-04-08 18:20:45 +0200

    AbstractFakeIndex: use virtualId to as document key
    The concept of virtualId was introduced by change I7b9acab
    but the AbstractFakeIndex wasn't entirely adapted to use it.
    
    Relase-Notes: skip
    Change-Id: I6a4b43aedecf860c74929641a61b686de7dc855a

--- java/com/google/gerrit/index/testing/AbstractFakeIndex.java
+++ java/com/google/gerrit/index/testing/AbstractFakeIndex.java
@@ -257,7 +257,7 @@ public abstract class AbstractFakeIndex<K, V, D> implements Index<K, V> {
 
     @Override
     protected Change.Id keyFor(ChangeData value) {
-      return value.getId();
+      return value.virtualId();
     }
 
     @Override

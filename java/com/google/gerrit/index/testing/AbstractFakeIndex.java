943f700b991dda30e4ad6b1fdb9ae70a033d5c6b
Fabio Ponciroli <Ponch78@gmail.com>
2025-04-08 18:04:40 +0200

    AbtractFakeIndex: use changenumber instead of legacy_is_str
    Change I64f55e8a introduced a "changenumber" field in the document
    to allow lookup of imported changes.
    
    However, the FakeIndex wasn't amended to use this new field.
    
    Release-Notes: skip
    Change-Id: Ib8c573e90ecf56082a496b95f3b5a8a22e061bf2

--- java/com/google/gerrit/index/testing/AbstractFakeIndex.java
+++ java/com/google/gerrit/index/testing/AbstractFakeIndex.java
@@ -15,6 +15,7 @@
 package com.google.gerrit.index.testing;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.gerrit.server.index.change.ChangeField.CHANGENUM_SPEC;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
@@ -290,7 +291,10 @@ public abstract class AbstractFakeIndex<K, V, D> implements Index<K, V> {
           changeDataFactory.create(
               Project.nameKey((String) doc.get(ChangeField.PROJECT_SPEC.getName())),
               Change.id(
-                  Integer.valueOf((String) doc.get(ChangeField.NUMERIC_ID_STR_SPEC.getName()))));
+                  doc.get(CHANGENUM_SPEC.getName()) != null
+                      ? (Integer) doc.get(CHANGENUM_SPEC.getName())
+                      : Integer.valueOf(
+                          (String) doc.get(ChangeField.NUMERIC_ID_STR_SPEC.getName()))));
       for (SchemaField<ChangeData, ?> field : getSchema().getSchemaFields().values()) {
         boolean isProtoField = SchemaFieldDefs.isProtoField(field);
 

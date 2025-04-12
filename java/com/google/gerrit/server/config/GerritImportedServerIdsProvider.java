b42264e921bfc05226be16a6b5c72e2fd7c8a3be
Luca Milanesio <luca.milanesio@gmail.com>
2025-04-12 10:31:22 +0200

    Prevent change numbers virtualization
    Change numbers virtualization is expensive and should never be called
    when there are no imported server ids configured in gerrit.config.
    
    Register a new ChangeNumberNoopAlgorithm that throws an exception every
    time the virtualization is invoked. This guarantees to spot any
    potential performance issues in running virtualization of change numbers
    outside the context of imported changes.
    
    Forward-Compatible: checked
    Release-Notes: Prevent change numbers virtualization for sites without imported server ids
    Change-Id: I2ca48614043906ee7113c5ec8af487d9e78e827f

--- java/com/google/gerrit/server/config/GerritImportedServerIdsProvider.java
+++ java/com/google/gerrit/server/config/GerritImportedServerIdsProvider.java
@@ -17,21 +17,29 @@ package com.google.gerrit.server.config;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.Singleton;
 import org.eclipse.jgit.lib.Config;
 
+@Singleton
 public class GerritImportedServerIdsProvider implements Provider<ImmutableList<String>> {
   public static final String SECTION = "gerrit";
   public static final String KEY = "importedServerId";
 
   private final ImmutableList<String> importedIds;
+  private final boolean hasImportedServerIds;
 
   @Inject
   public GerritImportedServerIdsProvider(@GerritServerConfig Config cfg) {
     importedIds = ImmutableList.copyOf(cfg.getStringList(SECTION, null, KEY));
+    hasImportedServerIds = !importedIds.isEmpty();
   }
 
   @Override
   public ImmutableList<String> get() {
     return importedIds;
+  }
+
+  public boolean hasImportedServerIds() {
+    return hasImportedServerIds;
   }
 }

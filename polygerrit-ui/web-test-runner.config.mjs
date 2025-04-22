d08bc2566b8d3e26912236ea4f1cf337ef7ace63
Paladox none <thomasmulhall410@yahoo.com>
2025-04-22 14:24:50 -0700

    Allow plugins to run web-test-runner
    Karma was removed from gerrit core in [0]. There was no replacement
    made for plugins. Which still had to rely on karma.
    
    We want to remove the final remnants of karma from gerrit core.
    
    [0] I46f58584aa5443c51af0ebebf86a45442982f1cc
    
    Change-Id: I4e9134e04475d7375d7a09d618892edbe0b555bc

--- polygerrit-ui/web-test-runner.config.mjs
+++ polygerrit-ui/web-test-runner.config.mjs
@@ -1,6 +1,7 @@
 import { esbuildPlugin } from "@web/dev-server-esbuild";
 import { defaultReporter, summaryReporter } from "@web/test-runner";
 import { visualRegressionPlugin } from "@web/test-runner-visual-regression/plugin";
+import path from 'path';
 
 function testRunnerHtmlFactory() {
   return (testFramework) => `
@@ -20,26 +21,69 @@ function testRunnerHtmlFactory() {
   `;
 }
 
+const runUnderBazel = !!process.env["RUNFILES_DIR"];
+
+function getModulesDir() {
+  if (runUnderBazel) {
+    // Run under bazel
+    return [
+      path.join(process.cwd(), 'external/plugins_npm/node_modules'),
+      path.join(process.cwd(), 'external/ui_npm/node_modules'),
+      path.join(process.cwd(), 'external/ui_dev_npm/node_modules')
+    ];
+  }
+
+  // Run from intellij or npm run test:kdebug
+  return [
+    path.join(process.cwd(), 'plugins/node_modules'),
+    path.join(process.cwd(), 'app/node_modules'),
+    path.join(process.cwd(), 'node_modules')
+  ];
+}
+
+function getArgValue(flag) {
+  const withEquals = process.argv.find((arg) => arg.startsWith(`${flag}=`));
+  if (withEquals) return withEquals.split("=")[1];
+
+  const index = process.argv.indexOf(flag);
+  if (index !== -1 && process.argv[index + 1] && !process.argv[index + 1].startsWith("--")) {
+    return process.argv[index + 1];
+  }
+
+  return undefined;
+}
+
+const pathBazel = runUnderBazel ? 'polygerrit-ui/' : ''
+
+const testFiles = getArgValue('--test-files') ?? `${pathBazel}app/**/*_test.{ts,js}`;
+
+const rootDir = getArgValue('--root-dir') ?? path.resolve(process.cwd()) + "/";
+
+const tsConfig = getArgValue('--ts-config');
+
 /** @type {import('@web/test-runner').TestRunnerConfig} */
 const config = {
   // TODO: https://g-issues.gerritcodereview.com/issues/365565157 - undo the
   // change once the underlying issue is fixed.
   concurrency: 1,
   files: [
-    "app/**/*_test.{ts,js}",
-    "!**/node_modules/**/*",
+    testFiles,
+    `!${pathBazel}**/node_modules/**/*`,
     ...(process.argv.includes("--run-screenshots")
       ? []
-      : ["!app/**/*_screenshot_test.{ts,js}"]),
+      : [`!${pathBazel}app/**/*_screenshot_test.{ts,js}`]),
   ],
   port: 9876,
-  nodeResolve: true,
+  nodeResolve: {
+    moduleDirectories: getModulesDir()
+  },
   testFramework: { config: { ui: "tdd", timeout: 5000 } },
   plugins: [
     esbuildPlugin({
       ts: true,
       target: "es2020",
-      tsconfig: "app/tsconfig.json",
+      //path.join(process.cwd(), 'plugins/code-owners/web/tsconfig.json')
+      tsconfig: tsConfig,
     }),
     visualRegressionPlugin({
       diffOptions: {
@@ -50,7 +94,7 @@ const config = {
   ],
   // serve from gerrit root directory so that we can serve fonts from
   // /lib/fonts/, see middleware.
-  rootDir: "..",
+  rootDir: rootDir,
   reporters: [defaultReporter(), summaryReporter()],
   middleware: [
     // Fonts are in /lib/fonts/, but css tries to load from

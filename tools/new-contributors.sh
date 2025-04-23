e24ddea285d0158fb4d26315c84e33477d009d59
Daniele Sassoli <danielesassoli@gmail.com>
2025-04-23 14:24:47 +0100

    Add tool to find new contributors
    Usage: ./tools/new-contributos.sh $START_SHA1 $END_SHA1
    Finds all users that have contributed between start_sha1
    and end_sha1, but not before start_sha1, so essentially
    identifies all new contributors to the latest version.
    e.g.: ./tools/new-contributos.sh v3.11.0 stable-3.12
    
    Release-Notes: Add tool to find new contributors
    Change-Id: I7d2fb1a91df6e46d1634e6c9a3399e569d7a0078

---
+++ tools/new-contributors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check arguments
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <start-sha> <end-sha>"
+  exit 1
+fi
+
+START_SHA=$1
+END_SHA=$2
+
+# Get authors before START_SHA
+authors_before=$(git log --format='%aN' "$START_SHA" | sort | uniq)
+
+# Get authors between START_SHA and END_SHA
+authors_between=$(git log --format='%aN' "$START_SHA..$END_SHA" | sort | uniq)
+
+# Find authors in between but not before
+new_authors=$(comm -23 <(echo "$authors_between") <(echo "$authors_before"))
+
+echo "New authors between $START_SHA and $END_SHA:"
+echo "$new_authors"

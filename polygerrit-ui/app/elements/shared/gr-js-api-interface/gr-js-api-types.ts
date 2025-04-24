13b8a30ad24a2e355514b9a0c643516fa4ace67a
Allen Li <ayatane-external@chromium.org>
2025-04-24 01:08:42 +0000

    Add addtional events for frontend plugins
    These allow adding addtional checks before code approval actions.
    
    Bug: 410935097
    Change-Id: I1c39e0d343703f2ffcfa756ef3ed244f5995eb9f

--- polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-js-api-types.ts
+++ polygerrit-ui/app/elements/shared/gr-js-api-interface/gr-js-api-types.ts
@@ -46,6 +46,8 @@ export interface JsApiService extends Finalizable {
     revertSubmissionMsg: string,
     origMsg: string
   ): string;
+  handleBeforeAction(type: ActionType, key: string): Promise<boolean>;
+  handleBeforePublishEdit(change: ChangeInfo, revision?: RevisionInfo | null): Promise<boolean>;
   handlePublishEdit(change: ChangeInfo, revision?: RevisionInfo | null): void;
   handleShowChange(detail: ShowChangeDetail): Promise<void>;
   handleShowRevisionActions(detail: ShowRevisionActionsDetail): void;
@@ -63,5 +65,6 @@ export interface JsApiService extends Finalizable {
   canSubmitChange(change: ChangeInfo, revision?: RevisionInfo | null): boolean;
   getReviewPostRevert(change?: ChangeInfo): ReviewInput;
   handleShowDiff(detail: ShowDiffDetail): void;
+  handleBeforeReplySent(reviewInput: ReviewInput): Promise<boolean>;
   handleReplySent(): Promise<void>;
 }

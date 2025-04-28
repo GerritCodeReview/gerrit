833c88725a6710aa9368d4ad6011a60a7e881d64
Paladox none <thomasmulhall410@yahoo.com>
2025-04-28 06:28:42 -0700

    Use googlefonts/emoji-metadata for emojis
    We replace our hardcoded emojis with the ones from googlefonts/emoji-metadata.
    This greatly expands what emojis are available.
    We also increase the amount of emojis shown in the dropdown from 10
    to 25.
    
    In the future we should build out a proper editor that includes
    a better emoji picker UI. And also other editor tools such as
    making the text bold/italic and so on.
    
    Release-Notes: Use googlefonts/emoji-metadata for emojis
    Change-Id: Ie437f0f705a01984a4cc897b3daa7f18c334281c

--- polygerrit-ui/app/elements/shared/gr-suggestion-textarea/gr-suggestion-textarea.ts
+++ polygerrit-ui/app/elements/shared/gr-suggestion-textarea/gr-suggestion-textarea.ts
@@ -31,32 +31,10 @@ import {getAccountDisplayName} from '../../../utils/display-name-util';
 import {configModelToken} from '../../../models/config/config-model';
 import {formStyles} from '../../../styles/form-styles';
 import {GrTextarea} from '../../../embed/gr-textarea';
+import {GrLibLoader} from '../../../elements/shared/gr-lib-loader/gr-lib-loader';
+import {EMOJIS_LIBRARY_CONFIG} from '../../../elements/shared/gr-lib-loader/emojis_config';
 
-const MAX_ITEMS_DROPDOWN = 10;
-
-const ALL_SUGGESTIONS: EmojiSuggestion[] = [
-  {value: '😊', match: 'smile :)'},
-  {value: '👍', match: 'thumbs up'},
-  {value: '😄', match: 'laugh :D'},
-  {value: '❤️', match: 'heart <3'},
-  {value: '😂', match: "tears :')"},
-  {value: '🎉', match: 'party'},
-  {value: '😎', match: 'cool |;)'},
-  {value: '😞', match: 'sad :('},
-  {value: '😐', match: 'neutral :|'},
-  {value: '😮', match: 'shock :O'},
-  {value: '🙏', match: 'pray'},
-  {value: '😕', match: 'confused'},
-  {value: '👌', match: 'ok'},
-  {value: '🔥', match: 'fire'},
-  {value: '💯', match: '100'},
-  {value: '✔', match: 'check'},
-  {value: '😋', match: 'tongue'},
-  {value: '😭', match: "crying :'("},
-  {value: '🤓', match: 'glasses'},
-  {value: '😢', match: 'tear'},
-  {value: '😜', match: 'winking tongue ;)'},
-];
+const MAX_ITEMS_DROPDOWN = 25;
 
 export interface EmojiSuggestion extends Item {
   match: string;
@@ -120,6 +98,8 @@ export class GrSuggestionTextarea extends LitElement {
 
   @state() private allowMarkdownBase64ImagesInComments = false;
 
+  @state() emojis?: EmojiSuggestion[];
+
   // Accessed in tests.
   readonly reporting = getAppContext().reportingService;
 
@@ -142,6 +122,8 @@ export class GrSuggestionTextarea extends LitElement {
   currentSearchString?: string;
 
   private readonly shortcuts = new ShortcutController(this);
+
+  private static readonly libLoader = new GrLibLoader();
 
   constructor() {
     super();
@@ -323,6 +305,17 @@ export class GrSuggestionTextarea extends LitElement {
       .verticalOffset=${20}
       role="listbox"
     ></gr-autocomplete-dropdown>`;
+  }
+
+  override firstUpdated() {
+    if (!this.emojis) {
+      GrSuggestionTextarea.libLoader
+        .getLibrary(EMOJIS_LIBRARY_CONFIG)
+        .then(emojis => {
+          if (emojis)
+            this.emojis = emojis as EmojiSuggestion[];
+        });
+    }
   }
 
   override updated(changedProperties: PropertyValues) {
@@ -638,17 +631,18 @@ export class GrSuggestionTextarea extends LitElement {
 
   // private but used in test
   computeEmojiSuggestions(suggestionsText?: string): EmojiSuggestion[] {
-    if (suggestionsText === undefined) {
+    if (suggestionsText === undefined || !this.emojis) {
       return [];
     }
+
     if (!suggestionsText.length) {
-      return this.formatSuggestions(ALL_SUGGESTIONS);
-    } else {
-      const matches = ALL_SUGGESTIONS.filter(suggestion =>
-        suggestion.match.includes(suggestionsText)
-      ).slice(0, MAX_ITEMS_DROPDOWN);
-      return this.formatSuggestions(matches);
+      return this.formatSuggestions(this.emojis.slice(0, MAX_ITEMS_DROPDOWN));
     }
+
+    const matches = this.emojis.filter(suggestion =>
+      suggestion.match.includes(suggestionsText)
+    ).slice(0, MAX_ITEMS_DROPDOWN);
+    return this.formatSuggestions(matches);
   }
 
   // TODO(dhruvsri): merge with getAccountSuggestions in account-util

6f31fc76775857972e1206a117bb0535bb41acb0
Daniele Sassoli <danielesassoli@gmail.com>
2025-05-07 11:06:07 +0200

    Print both name and email
    Change-Id: I327f3850332bcfd940c94befe7d92ad8f01d6cef

--- tools/new-contributors.sh
+++ tools/new-contributors.sh
@@ -25,10 +25,10 @@ START_SHA=$1
 END_SHA=$2
 
 # Get authors before START_SHA
-authors_before=$(git log --format='%aN' "$START_SHA" | sort | uniq)
+authors_before=$(git log --format='%an <%ae>' "$START_SHA" | sort | uniq)
 
 # Get authors between START_SHA and END_SHA
-authors_between=$(git log --format='%aN' "$START_SHA..$END_SHA" | sort | uniq)
+authors_between=$(git log --format='%an <%ae>' "$START_SHA..$END_SHA" | sort | uniq)
 
 # Find authors in between but not before
 new_authors=$(comm -23 <(echo "$authors_between") <(echo "$authors_before"))

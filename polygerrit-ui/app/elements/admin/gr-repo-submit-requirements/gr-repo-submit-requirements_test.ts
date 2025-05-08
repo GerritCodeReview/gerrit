f0620e1918b64927c693a2180d1fe16db7301451
Milutin Kristofic <milutin@google.com>
2025-05-08 09:37:38 +0200

    Allow admin to create repository submit requirements
    This commit extends the functionality for managing repository submit
    requirements within the Web UI by enabling administrators to create new
    submit requirements.
    
    It introduces a "Create Submit Requirement" button on the repository
    submit requirements screen. Clicking this button opens a dialog where
    administrators can input the details for the new requirement,
    including name, description, applicability expression, submittability
    expression, override expression, and whether override is allowed in
    child projects.
    
    The necessary REST API endpoint and corresponding service method for
    creating submit requirements have also been implemented.
    
    Screenshot: https://imgur.com/a/U92dtpn
    Google-Bug-Id: b/241540038
    Release-Notes: skip
    Change-Id: I84f77c5732e848fba65e566e60f53ba422c7e156

--- polygerrit-ui/app/elements/admin/gr-repo-submit-requirements/gr-repo-submit-requirements_test.ts
+++ polygerrit-ui/app/elements/admin/gr-repo-submit-requirements/gr-repo-submit-requirements_test.ts
@@ -102,5 +102,93 @@ suite('gr-repo-submit-requirements tests', () => {
         `
       );
     });
+
+    test('render as admin', async () => {
+      await waitEventLoop();
+      element.isAdmin = true;
+      await element.updateComplete;
+      assert.shadowDom.equal(
+        element,
+        /* HTML */ `
+          <table class="genericList" id="list">
+            <tbody>
+              <tr class="headerRow">
+                <th class="topHeader">Name</th>
+                <th class="topHeader">Description</th>
+                <th class="topHeader">Applicability Expression</th>
+                <th class="topHeader">Submittability Expression</th>
+                <th class="topHeader">Override Expression</th>
+                <th
+                  class="topHeader"
+                  title="Whether override is allowed in child projects"
+                >
+                  Allow Override
+                </th>
+              </tr>
+            </tbody>
+            <tbody id="submit-requirements">
+              <tr class="table">
+                <td class="name">Verified</td>
+                <td class="desc">
+                  CI result status for build and tests is passing
+                </td>
+                <td class="applicability"></td>
+                <td class="submittability">
+                  label:Verified=MAX AND -label:Verified=MIN
+                </td>
+                <td class="override"></td>
+                <td class="allowOverride"></td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="actions">
+            <gr-button class="createButton" primary="">
+              Create Submit Requirement
+            </gr-button>
+          </div>
+          <dialog id="createDialog" tabindex="-1">
+            <gr-dialog cancel-label="Cancel" confirm-label="Create">
+              <div class="header" slot="header">Create Submit Requirement</div>
+              <div class="main" slot="main">
+                <form class="form">
+                  <div class="form-group">
+                    <label for="name"> Name </label>
+                    <input id="name" required="" type="text" />
+                  </div>
+                  <div class="form-group">
+                    <label for="description"> Description </label>
+                    <textarea id="description"> </textarea>
+                  </div>
+                  <div class="form-group">
+                    <label for="applicability">
+                      Applicability Expression
+                    </label>
+                    <input id="applicability" type="text" />
+                  </div>
+                  <div class="form-group">
+                    <label for="submittability">
+                      Submittability Expression
+                    </label>
+                    <input id="submittability" required="" type="text" />
+                  </div>
+                  <div class="form-group">
+                    <label for="override"> Override Expression </label>
+                    <input id="override" type="text" />
+                  </div>
+                  <div class="form-group">
+                    <div class="checkbox-container">
+                      <label for="allowOverride">
+                        Allow override in child projects
+                      </label>
+                      <input id="allowOverride" type="checkbox" />
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </gr-dialog>
+          </dialog>
+        `
+      );
+    });
   });
 });

a4f2ea6b9316da4b15ffc2b783d0dfddf6a380e7
Milutin Kristofic <milutin@google.com>
2025-05-15 17:02:53 +0200

    Add 'Copy Link' Bulk Action
    This commit introduces a "Copy Link" bulk action to the change list
    view. This feature allows users to select multiple changes and copy a
    Markdown formatted list of links to these CLs, with each link on a new
    line. This is useful for pasting into comments or documents.
    The action also supports copying a single OR-based search query link
    for the selected changes.
    
    This streamlines sharing or referencing groups of CLs by automating
    the creation of formatted link lists or complex search queries, which
    previously had to be done manually.
    
    To enable this, a `gr-change-list-copy-link-flow` component has been
    added to the action bar. The existing `gr-copy-links` and
    `gr-copy-clipboard` components were enhanced to handle multiline
    content using a textarea. This accommodates the Markdown list.
    
    Screenshot: https://imgur.com/a/WIwAieB
    Google-Bug-Id: b/243783675
    Release-Notes: skip
    Change-Id: I357c02bd379cccdb4e6aaba9deea96f8fd547ad9

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -1272,7 +1272,10 @@ export class GrChangeView extends LitElement {
         value: this.change.current_revision,
       });
     }
-    return html`<gr-copy-links .copyLinks=${links}> </gr-copy-links>`;
+    return html`<gr-copy-links 
+      .copyLinks=${links}
+      .shortcutPrefix=${'l -'}
+    > </gr-copy-links>`;
   }
 
   private renderCommitActions() {

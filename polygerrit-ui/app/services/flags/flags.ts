b4447c77cfc848a7045d00ac0f909f47b9b454eb
Dhruv Srivastava <dhruvsri@google.com>
2025-05-21 15:36:24 +0200

    Add experiment for showing default reviewer suggestions
    Release-Notes: skip
    Google-bug-id: b/409478777
    Change-Id: I9655f3b1ed735a58cf9c0878a1ef2a98bea97e02

--- polygerrit-ui/app/services/flags/flags.ts
+++ polygerrit-ui/app/services/flags/flags.ts
@@ -23,4 +23,5 @@ export enum KnownExperimentId {
   SAVE_PROJECT_CONFIG_FOR_REVIEW = 'UiFeature__save_project_config_for_review',
   PARALLEL_DASHBOARD_REQUESTS = 'UiFeature__parallel_dashboard_requests',
   GET_AI_FIX = 'UiFeature__get_ai_fix',
+  SHOW_DEFAULT_REVIEWER_SUGGESTIONS= 'UiFeature__show_default_reviewer_suggestions'
 }

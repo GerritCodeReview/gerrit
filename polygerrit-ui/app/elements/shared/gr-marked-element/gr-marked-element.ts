741b2cd01b5b7c7d6f8e9fc87608722feca0e086
Paladox none <thomasmulhall410@yahoo.com>
2025-05-31 05:22:33 -0700

    UI: Replace marked-element with a custom version that uses lit
    There was no replacement so i just converted the code from Polymer to lit.
    I removed the bits that we did not need and can thus license it under
    Apache 2.0 since it's entirely rewritten.
    
    I did some basic checks and it works.
    
    We upgrade to marked version 4.3.0. Version 5 breaks all the options
    and so is a more significant upgrade. Marked version 4 converts
    to a ESM module allowing us to import without using a workaround.
    
    Release-Notes: UI: Replace marked-element with a custom version that uses lit
    Change-Id: Ib1aa32f1a4fe6e746eaaf1555cb7b770921c1127

---
+++ polygerrit-ui/app/elements/shared/gr-marked-element/gr-marked-element.ts
@@ -0,0 +1,141 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {css, html, LitElement, PropertyValues} from 'lit';
+import {
+  customElement,
+  property,
+  queryAssignedElements,
+} from 'lit/decorators.js';
+import {marked} from 'marked';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gr-marked-element': GrMarkedElement;
+  }
+}
+
+/**
+ * This is based on [marked-element](https://github.com/PolymerElements/marked-element) by Polymer
+ * but converted to use Lit. It uses the [marked](https://github.com/markedjs/marked) library.
+ */
+@customElement('gr-marked-element')
+export class GrMarkedElement extends LitElement {
+  @property({type: String}) markdown: string | null = null;
+
+  @property({type: Boolean}) breaks = false;
+
+  @property({type: Boolean}) pedantic = false;
+
+  @property({type: Function}) renderer: Function | null = null;
+
+  @property({type: Boolean}) sanitize = false;
+
+  @property({type: Function}) sanitizer: ((html: string) => string) | undefined =
+    undefined;
+
+  @property({type: Boolean}) smartypants = false;
+
+  @property({type: Function}) callback: Function | null = null;
+
+  @queryAssignedElements({
+    flatten: true,
+    slot: 'markdown-html',
+  })
+  private outputElement!: Array<HTMLElement>;
+
+  static override styles = css`
+    :host {
+      display: block;
+    }
+  `;
+
+  override render() {
+    return html`
+      <slot name="markdown-html">
+        <div id="content"></div>
+      </slot>
+    `;
+  }
+
+  private isAttached = false;
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.isAttached = true;
+    this.renderMarkdown();
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    this.isAttached = false;
+  }
+
+  protected override updated(changedProps: PropertyValues) {
+    const propsToWatch = [
+      'markdown',
+      'breaks',
+      'pedantic',
+      'renderer',
+      'sanitize',
+      'sanitizer',
+      'smartypants',
+      'callback',
+    ];
+
+    if (propsToWatch.some(prop => changedProps.has(prop))) {
+      this.renderMarkdown();
+    }
+  }
+
+  override firstUpdated() {
+    this.renderMarkdown();
+  }
+
+  private renderMarkdown() {
+    if (!this.isAttached || !this.outputElement.length) {
+      return;
+    }
+
+    if (!this.markdown) {
+      this.outputElement[0].innerHTML = '';
+      return;
+    }
+
+    const renderer = new marked.Renderer();
+    if (this.renderer) this.renderer(renderer);
+
+    const options = {
+      renderer,
+      highlight: this.highlight.bind(this),
+      breaks: this.breaks,
+      sanitize: this.sanitize,
+      sanitizer: this.sanitizer,
+      pedantic: this.pedantic,
+      smartypants: this.smartypants,
+    };
+
+    let output = marked(this.markdown, options);
+    if (this.callback) {
+      output = this.callback('', output);
+    }
+
+    this.outputElement[0].innerHTML = output;
+    this.dispatchEvent(
+      new CustomEvent('marked-render-complete', {bubbles: true, composed: true})
+    );
+  }
+
+  private highlight(code: string, lang: string): string {
+    const event = new CustomEvent('syntax-highlight', {
+      detail: {code, lang},
+      bubbles: true,
+      composed: true,
+    });
+    this.dispatchEvent(event);
+    return event.detail.code || code;
+  }
+}

9edd015401d41034a73a896746e1d7bd49b669db
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 12:59:46 +0200

    Move config experiments REST API to its own package
    Change-Id: I4be974ca507edfe35de3dcacbb29e3a75a88f2c3

--- java/com/google/gerrit/server/restapi/config/experiments/GetExperiment.java
+++ java/com/google/gerrit/server/restapi/config/experiments/GetExperiment.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.config;
+package com.google.gerrit.server.restapi.config.experiments;
 
 import com.google.gerrit.extensions.common.ExperimentInfo;
 import com.google.gerrit.extensions.restapi.Response;

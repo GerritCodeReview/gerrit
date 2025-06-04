503d21f08753e81c776a2199214712d2f8a3e5d1
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 13:05:40 +0200

    Move config topmenus REST API to its own package
    Release-Notes: Move REST API child endpoints to own packages
    Change-Id: Ie6f5008cdec0f751c9a8b476802549120e9a3336

--- java/com/google/gerrit/server/restapi/config/topmenus/TopMenuCollection.java
+++ java/com/google/gerrit/server/restapi/config/topmenus/TopMenuCollection.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.config;
+package com.google.gerrit.server.restapi.config.topmenus;
 
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.restapi.ChildCollection;

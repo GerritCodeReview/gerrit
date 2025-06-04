12d940a6b2961d05807705ce0dcf2aadeab68a82
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 12:28:06 +0200

    Move account sshkeys REST API to its own package
    Release-Notes: Move REST API child endpoints to own packages
    Change-Id: I19a406fe59cfbc3d69c130fb0ac7d519f68ea2fc

--- java/com/google/gerrit/server/restapi/account/sshkeys/GetSshKeys.java
+++ java/com/google/gerrit/server/restapi/account/sshkeys/GetSshKeys.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.account;
+package com.google.gerrit.server.restapi.account.sshkeys;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;

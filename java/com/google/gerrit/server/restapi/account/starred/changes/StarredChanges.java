4df90d2bc4167d0cf48efa092bb72c36a970b1f6
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 12:30:12 +0200

    Move account starred REST API to its own package
    Release-Notes: Move REST API child endpoints to own packages
    Change-Id: Ida8118eda6e7684e4b7d5f2f47f3f781a0534caa

--- java/com/google/gerrit/server/restapi/account/starred/changes/StarredChanges.java
+++ java/com/google/gerrit/server/restapi/account/starred/changes/StarredChanges.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.account;
+package com.google.gerrit.server.restapi.account.starred.changes;
 
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.exceptions.DuplicateKeyException;

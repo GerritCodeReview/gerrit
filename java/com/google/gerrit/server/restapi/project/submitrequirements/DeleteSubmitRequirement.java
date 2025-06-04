861720580c0ff18e1b5eb9066ca05bca9cd46380
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 13:24:37 +0200

    Move project submitrequirements REST API to its own package
    Change-Id: Ibdeaabe7f93f73273d2f56e265de2264d0524707

--- java/com/google/gerrit/server/restapi/project/submitrequirements/DeleteSubmitRequirement.java
+++ java/com/google/gerrit/server/restapi/project/submitrequirements/DeleteSubmitRequirement.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.project;
+package com.google.gerrit.server.restapi.project.submitrequirements;
 
 import com.google.gerrit.extensions.common.Input;
 import com.google.gerrit.extensions.restapi.IdString;
@@ -21,6 +21,7 @@ import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestModifyView;
 import com.google.gerrit.server.project.ProjectConfig;
 import com.google.gerrit.server.project.SubmitRequirementResource;
+import com.google.gerrit.server.restapi.project.RepoMetaDataUpdater;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 

ab57a0e6650e74ed65826da5c3e476463d380000
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 12:28:06 +0200

    Move account sshkeys REST API to its own package
    Change-Id: I19a406fe59cfbc3d69c130fb0ac7d519f68ea2fc

--- java/com/google/gerrit/server/restapi/account/sshkeys/AddSshKey.java
+++ java/com/google/gerrit/server/restapi/account/sshkeys/AddSshKey.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.account;
+package com.google.gerrit.server.restapi.account.sshkeys;
 
 import static com.google.gerrit.server.mail.EmailFactories.KEY_ADDED;
 import static java.nio.charset.StandardCharsets.UTF_8;

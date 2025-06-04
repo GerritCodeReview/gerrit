901d1297eb683cc0f4e3c84effc1ed1af21511d2
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 12:39:03 +0200

    Move change edit REST API to its own package
    Change-Id: Ide58ba45124215952e6c61ce104d9fbea8cafbce

--- java/com/google/gerrit/server/restapi/change/edit/DeleteChangeEdit.java
+++ java/com/google/gerrit/server/restapi/change/edit/DeleteChangeEdit.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.change;
+package com.google.gerrit.server.restapi.change.edit;
 
 import com.google.gerrit.extensions.common.Input;
 import com.google.gerrit.extensions.restapi.AuthException;

c0434fed2b7e094490db4db89535d657d1ea7426
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 12:44:49 +0200

    Move change reviewers REST API to its own package
    Release-Notes: Move REST API child endpoints to own packages
    Change-Id: I8bdd2ec558daf4cf984589ab01e6ca567a75b83a

---
+++ java/com/google/gerrit/server/restapi/change/reviewers/ChangeReviewersRestApiModule.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.restapi.change.reviewers;
+
+import static com.google.gerrit.server.change.ChangeResource.CHANGE_KIND;
+import static com.google.gerrit.server.change.ReviewerResource.REVIEWER_KIND;
+
+import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.RestApiModule;
+import com.google.gerrit.server.restapi.change.Votes;
+
+public class ChangeReviewersRestApiModule extends RestApiModule {
+  @Override
+  protected void configure() {
+    bind(Reviewers.class);
+
+    DynamicMap.mapOf(binder(), REVIEWER_KIND);
+
+    child(CHANGE_KIND, "reviewers").to(Reviewers.class);
+    postOnCollection(REVIEWER_KIND).to(PostReviewers.class);
+    delete(REVIEWER_KIND).to(DeleteReviewer.class);
+    get(REVIEWER_KIND).to(GetReviewer.class);
+    post(REVIEWER_KIND, "delete").to(DeleteReviewer.class);
+    child(REVIEWER_KIND, "votes").to(Votes.class);
+  }
+}
--- java/com/google/gerrit/server/restapi/change/reviewers/Reviewers.java
+++ java/com/google/gerrit/server/restapi/change/reviewers/Reviewers.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.change;
+package com.google.gerrit.server.restapi.change.reviewers;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.entities.Account;

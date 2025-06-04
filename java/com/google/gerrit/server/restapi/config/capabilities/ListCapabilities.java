3aff7fe7553fc3b40bfca701c902ba173a778eb2
Thomas Dräbing <thomas.draebing@sap.com>
2025-06-04 12:57:54 +0200

    Move config capabilities REST API to its own package
    Release-Notes: Move REST API child endpoints to own packages
    Change-Id: I8d657d4b5256bec646e7f22a87f2d81adaa15119

--- java/com/google/gerrit/server/restapi/config/capabilities/ListCapabilities.java
+++ java/com/google/gerrit/server/restapi/config/capabilities/ListCapabilities.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.server.restapi.config;
+package com.google.gerrit.server.restapi.config.capabilities;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 

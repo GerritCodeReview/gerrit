d1a4a872a8bc2acea640ccae42bf41ee2ab03214
Milutin Kristofic <milutin@google.com>
2025-06-05 07:26:41 -0700

    Revert "Remove ML_SUGGESTED_EDIT_V2 flag"
    This reverts commit 49367ff3240ad90d0cab447f023e3e4fbe2a7833.
    
    Reason for revert: Reintroduced since some hosts want to disable ml
    suggestion for now
    
    Google-Bug-Id: b/413341335
    Change-Id: Ie06d6c484453e759aa33aab8af17338a20e8a450

--- polygerrit-ui/app/services/flags/flags.ts
+++ polygerrit-ui/app/services/flags/flags.ts
@@ -18,6 +18,7 @@ export enum KnownExperimentId {
   NEW_IMAGE_DIFF_UI = 'UiFeature__new_image_diff_ui',
   CHECKS_DEVELOPER = 'UiFeature__checks_developer',
   PUSH_NOTIFICATIONS_DEVELOPER = 'UiFeature__push_notifications_developer',
+  ML_SUGGESTED_EDIT_V2 = 'UiFeature__ml_suggested_edit_v2',
   REVISION_PARENTS_DATA = 'UiFeature__revision_parents_data',
   COMMENT_AUTOCOMPLETION = 'UiFeature__comment_autocompletion_enabled',
   SAVE_PROJECT_CONFIG_FOR_REVIEW = 'UiFeature__save_project_config_for_review',

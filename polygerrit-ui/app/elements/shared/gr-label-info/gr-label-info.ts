2f04b7eef252014e03db9267ba8170d5e712d8b1
Ben Rohlfs <brohlfs@google.com>
2025-06-06 14:41:07 +0200

    Fix deleting votes
    This was broken by change 479141.
    
    We are not just fixing the issue of the event not being processed,
    but are also simplifying and refactoring a bit.
    
    Release-Notes: skip
    Google-Bug-Id: b/422943081
    Change-Id: I252c7e160251567247cebb48fc02f318c90b4d34
    (cherry picked from commit 37ea687fa61110de734080cf357316fc05af8284)

--- polygerrit-ui/app/elements/shared/gr-label-info/gr-label-info.ts
+++ polygerrit-ui/app/elements/shared/gr-label-info/gr-label-info.ts
@@ -18,9 +18,18 @@ import {
   AccountId,
   isDetailedLabelInfo,
 } from '../../../types/common';
+<<<<<<< HEAD   (6a972b5d5f90228cd7bfff7a4b3474f15a038b08 Merge branch 'stable-3.10' into stable-3.11)
 import {LitElement, css, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {GrButton} from '../gr-button/gr-button';
+||||||| BASE   (6eda60bf466fe4b5ed82acd68c1c050ce011be19 TraceContext.close: Log an error if an exception happens)
+import {css, html, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {GrButton} from '../gr-button/gr-button';
+=======
+import {css, html, LitElement} from 'lit';
+import {customElement, property, state} from 'lit/decorators.js';
+>>>>>>> CHANGE (37ea687fa61110de734080cf357316fc05af8284 Fix deleting votes)
 import {
   canReviewerVote,
   getApprovalInfo,
@@ -33,7 +42,6 @@ import {ParsedChangeInfo} from '../../../types/types';
 import {fontStyles} from '../../../styles/gr-font-styles';
 import {sharedStyles} from '../../../styles/shared-styles';
 import {votingStyles} from '../../../styles/gr-voting-styles';
-import {ifDefined} from 'lit/directives/if-defined.js';
 import {fireReload} from '../../../utils/event-util';
 import {sortReviewers} from '../../../utils/attention-set-util';
 
@@ -73,12 +81,11 @@ export class GrLabelInfo extends LitElement {
   @property({type: Boolean})
   showAllReviewers = true;
 
+  @state() private deleteButtonDisabled = false;
+
   private readonly restApiService = getAppContext().restApiService;
 
   private readonly reporting = getAppContext().reportingService;
-
-  // TODO(TS): not used, remove later
-  _xhrPromise?: Promise<void>;
 
   static override get styles() {
     return [
@@ -191,17 +198,21 @@ export class GrLabelInfo extends LitElement {
   }
 
   private renderRemoveVote(reviewer: AccountInfo) {
+    const accountId = reviewer._account_id;
+    const canDeleteVote = this.canDeleteVote(
+      reviewer,
+      this.mutable,
+      this.change
+    );
+    if (!accountId || !canDeleteVote) return;
+
     return html`<gr-tooltip-content has-tooltip title="Remove vote">
       <gr-button
         link
         aria-label="Remove vote"
-        @click=${this.onDeleteVote}
-        data-account-id=${ifDefined(reviewer._account_id as number | undefined)}
-        class="deleteBtn ${this.computeDeleteClass(
-          reviewer,
-          this.mutable,
-          this.change
-        )}"
+        @click=${() => this.onDeleteVote(accountId)}
+        ?disabled=${this.deleteButtonDisabled}
+        class="deleteBtn"
       >
         <gr-icon icon="delete" filled></gr-icon>
       </gr-button>
@@ -246,71 +257,37 @@ export class GrLabelInfo extends LitElement {
    * A user is able to delete a vote iff the mutable property is true and the
    * reviewer that left the vote exists in the list of removable_reviewers
    * received from the backend.
-   *
-   * @param reviewer An object describing the reviewer that left the
-   *     vote.
    */
-  private computeDeleteClass(
+  private canDeleteVote(
     reviewer: ApprovalInfo,
     mutable: boolean,
     change?: ParsedChangeInfo
   ) {
     if (!mutable || !change || !change.removable_reviewers) {
-      return 'hidden';
+      return false;
     }
     const removable = change.removable_reviewers;
     if (removable.find(r => r._account_id === reviewer?._account_id)) {
-      return '';
+      return true;
     }
-    return 'hidden';
+    return false;
   }
 
-  /**
-   * Closure annotation for Polymer.prototype.splice is off.
-   * For now, suppressing annotations.
-   */
-  private onDeleteVote(e: MouseEvent) {
-    if (!this.change) return;
+  private async onDeleteVote(accountId: AccountId) {
+    if (!this.change || !accountId) return;
 
-    e.preventDefault();
-    let target = e.composedPath()[0] as GrButton;
-    while (!target.classList.contains('deleteBtn')) {
-      if (!target.parentElement) {
-        return;
-      }
-      target = target.parentElement as GrButton;
+    this.deleteButtonDisabled = true;
+    try {
+      const response = await this.restApiService.deleteVote(
+        this.change._number,
+        accountId,
+        this.label
+      );
+      if (response.ok && this.change) fireReload(this);
+    } catch (err) {
+      this.reporting.error('Delete vote', err as Error);
+    } finally {
+      this.deleteButtonDisabled = false;
     }
-
-    target.disabled = true;
-    const accountID = Number(
-      `${target.getAttribute('data-account-id')}`
-    ) as AccountId;
-    this._xhrPromise = this.restApiService
-      .deleteVote(this.change._number, accountID, this.label)
-      .then(response => {
-        target.disabled = false;
-        if (!response.ok) {
-          return;
-        }
-        if (this.change) {
-          fireReload(this);
-        }
-      })
-      .catch(err => {
-        this.reporting.error('Delete vote', err);
-        target.disabled = false;
-        return;
-      });
-  }
-
-  _computeValueTooltip(labelInfo: LabelInfo | undefined, score: string) {
-    if (
-      !labelInfo ||
-      !isDetailedLabelInfo(labelInfo) ||
-      !labelInfo.values?.[score]
-    ) {
-      return '';
-    }
-    return labelInfo.values[score];
   }
 }

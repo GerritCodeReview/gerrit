025d064fa84948383145bb5f8a3aab51ca878431
Paladox none <thomasmulhall410@yahoo.com>
2025-06-12 13:34:38 -0700

    UI: Replace iron-dropdown with md-menu
    Screen recording: https://imgur.com/a/MpeBzmk
    
    Release-Notes: UI: Replace iron-dropdown with md-menu
    Change-Id: Ib860381f489684e75ec962628038b0af2dcbc6cf

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -1169,33 +1169,36 @@ export class GrChangeView extends LitElement {
           ></gr-change-status>`
         )}
       </div>
-      ${this.renderCopyLinksDropdown()}
-      <gr-button
-        flatten
-        down-arrow
-        class="showCopyLinkDialogButton"
-        @click=${(e: MouseEvent) => {
-          // We don't want to handle clicks on the star or the <a> link.
-          // Calling `stopPropagation()` from the click handler of <a> is not an
-          // option, because then the click does not reach the top-level gr-page
-          // click handler and would result is a full page reload.
-          if ((e.target as HTMLElement)?.nodeName !== 'GR-BUTTON') return;
-          this.copyLinksDropdown?.toggleDropdown();
-        }}
-        ><gr-change-star
-          id="changeStar"
-          .change=${this.change}
-          @toggle-star=${(e: CustomEvent<ChangeStarToggleStarDetail>) =>
-            this.handleToggleStar(e)}
-          ?hidden=${!this.loggedIn}
-        ></gr-change-star>
-        <a
-          class="changeNumber"
-          aria-label=${`Change ${this.change?._number}`}
-          href=${ifDefined(this.computeChangeUrl(true))}
-          >${this.change?._number}</a
-        >
-      </gr-button>
+      <div style="position:relative;">
+        ${this.renderCopyLinksDropdown()}
+        <gr-button
+          id="copyLinkDialogButton"
+          flatten
+          down-arrow
+          class="showCopyLinkDialogButton"
+          @click=${(e: MouseEvent) => {
+            // We don't want to handle clicks on the star or the <a> link.
+            // Calling `stopPropagation()` from the click handler of <a> is not an
+            // option, because then the click does not reach the top-level gr-page
+            // click handler and would result is a full page reload.
+            if ((e.target as HTMLElement)?.nodeName !== 'GR-BUTTON') return;
+            this.copyLinksDropdown?.toggleDropdown(e.target as HTMLElement);
+          }}
+          ><gr-change-star
+            id="changeStar"
+            .change=${this.change}
+            @toggle-star=${(e: CustomEvent<ChangeStarToggleStarDetail>) =>
+              this.handleToggleStar(e)}
+            ?hidden=${!this.loggedIn}
+          ></gr-change-star>
+          <a
+            class="changeNumber"
+            aria-label=${`Change ${this.change?._number}`}
+            href=${ifDefined(this.computeChangeUrl(true))}
+            >${this.change?._number}</a
+          >
+        </gr-button>
+      </div>
       <div class="headerSubject">
         ${trimWithEllipsis(this.change?.subject, 80)}
       </div>

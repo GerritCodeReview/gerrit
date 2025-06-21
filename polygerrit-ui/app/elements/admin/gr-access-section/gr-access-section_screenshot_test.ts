1b8615d0f09ae57c37dad6489d58f7c12bf8292c
Milutin Kristofic <milutin@google.com>
2025-06-21 19:37:11 +0200

    Add screenshot test for gr-access-section
    Release-Notes: skip
    Change-Id: Ib5ea3fc90b68eeee7db79596d9417fed19272a70

---
+++ polygerrit-ui/app/elements/admin/gr-access-section/gr-access-section_screenshot_test.ts
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import '../../../test/common-test-setup';
+import './gr-access-section';
+import {fixture, html} from '@open-wc/testing';
+// Until https://github.com/modernweb-dev/web/issues/2804 is fixed
+// @ts-ignore
+import {visualDiff} from '@web/test-runner-visual-regression';
+import {GrAccessSection} from './gr-access-section';
+import {visualDiffDarkTheme} from '../../../test/test-utils';
+import {PermissionAccessSection} from '../gr-repo-access/gr-repo-access-interfaces';
+import { GitRef } from '../../../types/common';
+
+suite('gr-access-section screenshot tests', () => {
+  let element: GrAccessSection;
+
+  setup(async () => {
+    const section: PermissionAccessSection = {
+      id: 'refs/heads/*' as GitRef,
+      value: {
+        permissions: {
+          read: {
+            label: 'Read',
+            rules: {},
+          },
+        },
+      },
+    };
+
+    element = await fixture<GrAccessSection>(
+      html`<gr-access-section></gr-access-section>`
+    );
+    element.section = section;
+    element.capabilities = {};
+    await element.updateComplete;
+  });
+
+  test('screenshot', async () => {
+    await visualDiff(element, 'gr-access-section');
+    await visualDiffDarkTheme(element, 'gr-access-section');
+  });
+}); 

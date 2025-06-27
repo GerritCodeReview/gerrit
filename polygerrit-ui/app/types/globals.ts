77b411838038ae055496f0c2019c2ccfddf5dfb3
Milutin Kristofic <milutin@google.com>
2025-06-27 22:07:59 +0200

    Add material web element types to tag name map
    Extends the global `HTMLElementTagNameMap` to include type definitions
    for a set of Material Web components, such as buttons, tabs, and form
    controls. This makes TypeScript and the lit-analyzer aware of these
    custom element tags.
    
    This is a prerequisite for enabling the `no-unknown-tag-name` linting
    rule. As part of the ongoing migration to Material Web, this and other
    related linting rules have been temporarily disabled to allow the work to
    proceed incrementally.
    
    Release-Notes: skip
    Change-Id: Ib826ba6372a095777e6cb85c00e4c56cbe117215

--- polygerrit-ui/app/types/globals.ts
+++ polygerrit-ui/app/types/globals.ts
@@ -3,12 +3,39 @@
  * Copyright 2020 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
+import {MdElevatedButton} from '@material/web/button/elevated-button';
+import {MdTextButton} from '@material/web/button/text-button';
+import {MdCheckbox} from '@material/web/checkbox/checkbox';
+import {MdFab} from '@material/web/fab/fab';
+import {MdIcon} from '@material/web/icon/icon';
+import {MdIconButton} from '@material/web/iconbutton/icon-button';
+import {MdFilledCard} from '@material/web/labs/card/filled-card';
+import {MdFilledSelect} from '@material/web/select/filled-select';
+import {MdSelectOption} from '@material/web/select/select-option';
+import {MdSwitch} from '@material/web/switch/switch';
+import {MdSecondaryTab} from '@material/web/tabs/secondary-tab';
+import {MdTabs} from '@material/web/tabs/tabs';
 import {ParsedJSON} from './common';
 import {HighlightJS} from './types';
 
 export {};
 
 declare global {
+  interface HTMLElementTagNameMap {
+    'md-elevated-button': MdElevatedButton;
+    'md-text-button': MdTextButton;
+    'md-checkbox': MdCheckbox;
+    'md-fab': MdFab;
+    'md-icon': MdIcon;
+    'md-icon-button': MdIconButton;
+    'md-filled-card': MdFilledCard;
+    'md-filled-select': MdFilledSelect;
+    'md-select-option': MdSelectOption;
+    'md-switch': MdSwitch;
+    'md-tabs': MdTabs;
+    'md-secondary-tab': MdSecondaryTab;
+  }
+
   interface Window {
     CANONICAL_PATH?: string;
     INITIAL_DATA?: {[key: string]: ParsedJSON};

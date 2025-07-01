b5f46ea6c7eca5d721c29d57944d820e6f8d7664
Dhruv Srivastava <dhruvsri@google.com>
2025-07-01 11:28:39 +0200

    Refetch change if update signal is received
    Google-bug-id: b/392029850
    Release-Notes: skip
    Change-Id: I34d2ee7a866addea04cafe27544b0475ded87549

--- polygerrit-ui/app/models/change/change-model.ts
+++ polygerrit-ui/app/models/change/change-model.ts
@@ -511,6 +511,19 @@ export class ChangeModel extends Model<ChangeState> {
     this.latestRevisionWithEdit$ = this.selectRevision(
       this.latestPatchNumWithEdit$
     );
+    this.change$.subscribe(change => {
+      const changeUpdatesPlugins =
+        this.pluginLoader.pluginsModel.getChangeUpdatesPlugins();
+      for (const plugin of changeUpdatesPlugins) {
+        plugin.provider.unsubcribe();
+      }
+      if (!change) return;
+      for (const plugin of changeUpdatesPlugins) {
+        plugin.provider.subscribe(change.project, change._number, () =>
+          this.loadChange()
+        );
+      }
+    });
     this.isOwner$ = select(
       combineLatest([this.change$, this.userModel.account$]),
       ([change, account]) => isOwner(change, account)

1b024be1da47f522630c223c6e167a998d261543
Dhruv Srivastava <dhruvsri@google.com>
2025-07-01 11:28:39 +0200

    Refetch change if update signal is received
    Google-bug-id: b/392029850
    Release-Notes: skip
    Change-Id: I34d2ee7a866addea04cafe27544b0475ded87549

--- polygerrit-ui/app/models/change/change-model.ts
+++ polygerrit-ui/app/models/change/change-model.ts
@@ -24,6 +24,7 @@ import {ChangeStatus, DefaultBase} from '../../constants/constants';
 import {combineLatest, forkJoin, from, Observable, of} from 'rxjs';
 import {
   catchError,
+  debounceTime,
   filter,
   map,
   switchMap,
@@ -522,6 +523,21 @@ export class ChangeModel extends Model<ChangeState> {
     this.latestRevisionWithEdit$ = this.selectRevision(
       this.latestPatchNumWithEdit$
     );
+    this.change$.subscribe(change => {
+      const changeUpdatesPlugins =
+        this.pluginLoader.pluginsModel.getChangeUpdatesPlugins();
+      for (const plugin of changeUpdatesPlugins) {
+        plugin.publisher.unsubscribe();
+      }
+      if (!change) return;
+      for (const plugin of changeUpdatesPlugins) {
+        plugin.publisher
+          .pipe(debounceTime(1000))
+          .subscribe(change.project, change._number, () =>
+            this.showRefreshChangeNotification()
+          );
+      }
+    });
     this.isOwner$ = select(
       combineLatest([this.change$, this.userModel.account$]),
       ([change, account]) => isOwner(change, account)
@@ -867,7 +883,10 @@ export class ChangeModel extends Model<ChangeState> {
     this.navigation.setUrl(url);
   }
 
-  showRefreshChangeNotification(toastMessage: string) {
+  async showRefreshChangeNotification(change: ParsedChangeInfo) {
+    const toastMessage =
+      (await this.getChangeUpdateToastMessage(change)) ??
+      'There are updates on the change';
     this.showRefreshChangeNotificationTask = debounce(
       this.showRefreshChangeNotificationTask,
       () => {

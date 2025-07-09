7887a84e9f317da55a618c189f764fcf27d94313
Luca Milanesio <luca.milanesio@gmail.com>
2025-07-09 23:58:21 +0100

    Align ChangeEditUtil.deleteRef() to the rest of Gerrit code
    Gerrit represents the removal of refs as the transition from a non-zero
    SHA to a zero-SHA, see one example in DeleteRef.deleteSingleRef()
    method.
    
    The ChangeEditUtil.deleteRef() fails to set the new object ID in the
    RefUpdate, causing a situation where other parts of the code are
    expecting a non-null SHA1 and therefore causing potential NPEs.
    
    Align the removal of refs to the rest of Gerrit and allow more
    resilience in other parts of the code and in plugins, such as the
    global-refdb.
    
    Bug: Issue 430336832
    Release-Notes: Fix removal of edits that failed when used with global-refdb
    Change-Id: I0602aa007ed5d5d3341eca6160ce631e7bae1c04

--- java/com/google/gerrit/server/edit/ChangeEditUtil.java
+++ java/com/google/gerrit/server/edit/ChangeEditUtil.java
@@ -258,6 +258,7 @@ public class ChangeEditUtil {
       String refName = edit.getRefName();
       RefUpdate ru = repo.updateRef(refName, true);
       ru.setExpectedOldObjectId(edit.getEditCommitId());
+      ru.setNewObjectId(ObjectId.zeroId());
       ru.setForceUpdate(true);
       RefUpdate.Result result = ru.delete();
       switch (result) {

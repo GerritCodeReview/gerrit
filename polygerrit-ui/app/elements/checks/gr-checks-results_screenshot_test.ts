f5f2745383a7677387cbf86a9451975a1e64bd19
Milutin Kristofic <milutin@google.com>
2025-07-16 18:21:22 +0200

    Add screenshot test for gr-checks-results
    This change introduces a new visual regression test for the
    `gr-checks-results` component. The test renders the component with a
    set of fake check runs to establish a visual baseline.
    
    By capturing screenshots for both light and dark themes, this test
    will help prevent unintended UI regressions in the checks results
    display in the future.
    
    Release-Notes: skip
    Change-Id: I0bca73270552199cf343a3848911ef092a2e8c4d

---
+++ polygerrit-ui/app/elements/checks/gr-checks-results_screenshot_test.ts
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import '../../test/common-test-setup';
+import './gr-checks-results';
+import {fixture, html} from '@open-wc/testing';
+// Until https://github.com/modernweb-dev/web/issues/2804 is fixed
+// @ts-ignore
+import {visualDiff} from '@web/test-runner-visual-regression';
+import {checksModelToken} from '../../models/checks/checks-model';
+import {setAllFakeRuns} from '../../models/checks/checks-fakes';
+import {resolve} from '../../models/dependency';
+import {GrChecksResults} from './gr-checks-results';
+import {visualDiffDarkTheme} from '../../test/test-utils';
+
+suite('gr-checks-results screenshot', () => {
+  let element: GrChecksResults;
+
+  setup(async () => {
+    element = await fixture<GrChecksResults>(
+      html`<gr-checks-results></gr-checks-results>`
+    );
+    const getChecksModel = resolve(element, checksModelToken);
+    getChecksModel().allRunsSelectedPatchset$.subscribe(
+      runs => (element.runs = runs)
+    );
+    setAllFakeRuns(getChecksModel());
+    await element.updateComplete;
+  });
+
+  test('screenshot', async () => {
+    await visualDiff(element, 'gr-checks-results');
+    await visualDiffDarkTheme(element, 'gr-checks-results');
+  });
+});

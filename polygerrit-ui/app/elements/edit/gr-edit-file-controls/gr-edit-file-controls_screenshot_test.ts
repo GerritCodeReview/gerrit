796ac1ba7d0ef04e5a5c27856d8eb88d2e1885d8
Milutin Kristofic <milutin@google.com>
2025-07-16 13:11:52 +0200

    Add screenshot test for gr-edit-file-controls
    This change adds a new screenshot test for the `gr-edit-file-controls`
    component to prevent unintentional visual regressions.
    
    The test captures the component in its default state and with the
    actions dropdown menu open. Screenshots are generated for both light and
    dark themes. A minor supporting change was made to a test utility to
    allow the `visualDiffDarkTheme` function to accept a more generic
    `Element` type.
    
    Release-Notes: skip
    Change-Id: I6705fad5d880bc11634ace715ffe5fb682ade9b3

---
+++ polygerrit-ui/app/elements/edit/gr-edit-file-controls/gr-edit-file-controls_screenshot_test.ts
@@ -0,0 +1,44 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import '../../../test/common-test-setup';
+import {assert, fixture, html} from '@open-wc/testing';
+// Until https://github.com/modernweb-dev/web/issues/2804 is fixed
+// @ts-ignore
+import {visualDiff} from '@web/test-runner-visual-regression';
+import {visualDiffDarkTheme} from '../../../test/test-utils';
+import {GrEditFileControls} from './gr-edit-file-controls';
+import './gr-edit-file-controls';
+import {GrDropdown} from '../../shared/gr-dropdown/gr-dropdown';
+import {queryAndAssert} from '../../../test/test-utils';
+
+suite('gr-edit-file-controls screenshot tests', () => {
+  let element: GrEditFileControls;
+
+  setup(async () => {
+    element = await fixture(
+      html`<gr-edit-file-controls></gr-edit-file-controls>`
+    );
+    element.filePath = 'foo/bar.baz';
+    await element.updateComplete;
+  });
+
+  test('screenshot', async () => {
+    await visualDiff(element, 'gr-edit-file-controls');
+    await visualDiffDarkTheme(element, 'gr-edit-file-controls');
+  });
+
+  test('dropdown open', async () => {
+    const actions = queryAndAssert<GrDropdown>(element, '#actions');
+    actions.dropdownTriggerTapHandler();
+    await actions.updateComplete;
+
+    const dropdownContent = actions.shadowRoot?.querySelector('.dropdown-content');
+    assert.isOk(dropdownContent);
+
+    await visualDiff(dropdownContent!, 'gr-edit-file-controls-dropdown');
+    await visualDiffDarkTheme(dropdownContent!, 'gr-edit-file-controls-dropdown');
+  });
+});

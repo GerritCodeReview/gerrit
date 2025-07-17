71853adc3ee230356b368cb36a1019055e3eee79
Edwin Kempin <ekempin@google.com>
2025-07-17 09:26:26 +0000

    Test creating a merge change without conflicts for all merge strategies
    Add tests for all merge strategies that create a merge change via the
    Create Change REST endpoint when there are no conflicts.
    
    Release-Notes: skip
    Change-Id: I670a8b0a02f8b1303f5b7b0918a3a9baeb3a3fc0
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- javatests/com/google/gerrit/acceptance/rest/change/CreateChangeIT.java
+++ javatests/com/google/gerrit/acceptance/rest/change/CreateChangeIT.java
@@ -726,16 +726,40 @@ public class CreateChangeIT extends AbstractDaemonTest {
   }
 
   @Test
-  public void createMergeChange() throws Exception {
+  public void createMergeChangeNoConflicts_Resolve() throws Exception {
+    testCreateMergeChangeNoConflicts("resolve");
+  }
+
+  @Test
+  public void createMergeChangeNoConflicts_Recursive() throws Exception {
+    testCreateMergeChangeNoConflicts("recursive");
+  }
+
+  @Test
+  public void createMergeChangeNoConflicts_SimpleTwoWayInCore() throws Exception {
+    testCreateMergeChangeNoConflicts("simple-two-way-in-core");
+  }
+
+  @Test
+  public void createMergeChangeNoConflicts_Ours() throws Exception {
+    testCreateMergeChangeNoConflicts("ours");
+  }
+
+  @Test
+  public void createMergeChangeNoConflicts_Theirs() throws Exception {
+    testCreateMergeChangeNoConflicts("theirs");
+  }
+
+  public void testCreateMergeChangeNoConflicts(String mergeStrategy) throws Exception {
     String sourceBranch = "sourceBranch";
     String targetBranch = "targetBranch";
     ImmutableMap<String, Result> results =
         changeInTwoBranches(sourceBranch, "a.txt", targetBranch, "b.txt");
-    ChangeInput in = newMergeChangeInput(targetBranch, sourceBranch, "");
+    ChangeInput in = newMergeChangeInput(targetBranch, sourceBranch, mergeStrategy);
     ChangeInfo change = assertCreateSucceeds(in);
 
     // Verify the message that has been posted on the change.
-    List<ChangeMessageInfo> messages = gApi.changes().id(change._number).messages();
+    List<ChangeMessageInfo> messages = gApi.changes().id(project.get(), change._number).messages();
     assertThat(messages).hasSize(1);
     assertThat(Iterables.getOnlyElement(messages).message).isEqualTo("Uploaded patch set 1.");
 

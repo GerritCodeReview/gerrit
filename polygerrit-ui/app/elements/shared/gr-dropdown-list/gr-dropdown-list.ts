3f7ffa70ea0478cabf8bfd2e20624cd6102aa45a
Paladox none <thomasmulhall410@yahoo.com>
2025-07-23 09:30:08 -0700

    gr-dropdown-list: Bring in improvements to gr-dropdown
    Brings back default behaviour for focus ring and it also sets
    skipRestoreFocus on md-menu.
    
    Release-Notes: skip
    Change-Id: If65953e700c06cece3baee54de57976c90396bbf

--- polygerrit-ui/app/elements/shared/gr-dropdown-list/gr-dropdown-list.ts
+++ polygerrit-ui/app/elements/shared/gr-dropdown-list/gr-dropdown-list.ts
@@ -124,6 +124,7 @@ export class GrDropdownList extends LitElement {
           --md-menu-container-color: var(--dropdown-background-color);
           --md-menu-top-space: 0px;
           --md-menu-bottom-space: 0px;
+          --md-focus-ring-duration: 0s;
         }
         md-divider {
           margin: auto;
@@ -233,7 +234,6 @@ export class GrDropdownList extends LitElement {
         this.cursor.setCursorAtIndex(this.selectedIndex);
         if (this.cursor.target !== null) {
           this.cursor.target.focus();
-          this.handleAddSelected();
         }
         this.setUpGlobalEventListeners();
       } else {
@@ -264,7 +264,7 @@ export class GrDropdownList extends LitElement {
     return html`<div class="dropdown">
       <gr-button
         id="trigger"
-        ?disabled=${!!this.disabled}
+        ?disabled=${this.disabled}
         down-arrow
         link
         class="dropdown-trigger"
@@ -294,6 +294,7 @@ export class GrDropdownList extends LitElement {
         tabindex="-1"
         .menuCorner=${'start-start'}
         ?quick=${true}
+        .skipRestoreFocus=${true}
         @click=${this.handleDropdownClick}
         @opened=${(e: Event) => {
           this.opened = true;
@@ -323,9 +324,11 @@ export class GrDropdownList extends LitElement {
       <md-menu-item
         ?selected=${this.value === String(item.value)}
         ?active=${this.value === String(item.value)}
-        ?disabled=${!!item.disabled}
-        @click=${() => {
+        ?disabled=${item.disabled}
+        @click=${(e: MouseEvent) => {
           this.value = String(item.value);
+          const target = e.currentTarget as HTMLElement;
+          target?.blur();
         }}
         @keydown=${(e: KeyboardEvent) => {
           if (e.key === Key.ENTER || e.key === Key.SPACE) {
@@ -399,18 +402,14 @@ export class GrDropdownList extends LitElement {
    * Handle the up key.
    */
   private handleUp() {
-    this.handleRemoveSelected();
     this.cursor.previous();
-    this.handleAddSelected();
   }
 
   /**
    * Handle the down key.
    */
   private handleDown() {
-    this.handleRemoveSelected();
     this.cursor.next();
-    this.handleAddSelected();
   }
 
   /**
@@ -420,7 +419,6 @@ export class GrDropdownList extends LitElement {
     if (this.cursor.target !== null) {
       const el = this.cursor.target.shadowRoot?.querySelector(':not([hidden])');
       if (el) {
-        this.handleRemoveSelected();
         (el as HTMLElement).click();
       }
     }
@@ -486,30 +484,6 @@ export class GrDropdownList extends LitElement {
         this.shadowRoot?.querySelectorAll('md-menu-item') ?? []
       );
     }
-  }
-
-  private handleRemoveSelected() {
-    // We workaround an issue to allow cursor to work.
-    // For some reason without this, it doesn't work half the time.
-    // E.g. you press enter or you close the dropdown, reopen it,
-    // you expect it to be focused with the first item selected.
-    // The below fixes it. It's an ugly hack but works for now.
-    const mdFocusRing = this.cursor.target?.shadowRoot
-      ?.querySelector('md-item')
-      ?.querySelector('md-focus-ring');
-    if (mdFocusRing) mdFocusRing.visible = false;
-  }
-
-  private handleAddSelected() {
-    // We workaround an issue to allow cursor to work.
-    // For some reason without this, it doesn't work half the time.
-    // E.g. you press enter or you close the dropdown, reopen it,
-    // you expect it to be focused with the first item selected.
-    // The below fixes it. It's an ugly hack but works for now.
-    const mdFocusRing = this.cursor.target?.shadowRoot
-      ?.querySelector('md-item')
-      ?.querySelector('md-focus-ring');
-    if (mdFocusRing) mdFocusRing.visible = true;
   }
 }
 

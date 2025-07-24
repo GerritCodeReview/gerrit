44376b2d056753b096a98d9d64b06734e36c7313
Edwin Kempin <ekempin@google.com>
2025-07-24 16:10:00 +0000

    CreateTag: Allow callers to specify the creation date for the new tag
    This makes it easiler for callers to include a valid signature into the
    message when creating signed tags.
    
    Release-Notes: skip
    Change-Id: Id2654e23f13a7fc57d308297199762a241146645
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/server/restapi/project/CreateTag.java
+++ java/com/google/gerrit/server/restapi/project/CreateTag.java
@@ -95,6 +95,9 @@ public class CreateTag implements RestCollectionCreateView<ProjectResource, TagR
     if (input.ref != null && !ref.equals(input.ref)) {
       throw new BadRequestException("ref must match URL");
     }
+    if (input.date != null && TimeUtil.now().isBefore(input.date.toInstant())) {
+      throw new BadRequestException("date cannot be in the future");
+    }
     if (input.revision != null) {
       input.revision = input.revision.trim();
     }
@@ -145,7 +148,9 @@ public class CreateTag implements RestCollectionCreateView<ProjectResource, TagR
                     resource
                         .getUser()
                         .asIdentifiedUser()
-                        .newCommitterIdent(TimeUtil.now(), ZoneId.systemDefault()));
+                        .newCommitterIdent(
+                            input.date != null ? input.date.toInstant() : TimeUtil.now(),
+                            ZoneId.systemDefault()));
           }
 
           try {

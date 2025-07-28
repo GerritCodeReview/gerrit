cdab84c6097cf9b14b4ed5fbc332c69a2ced614e
I547210 <hitesh.chaudhari@sap.com>
2025-07-28 10:07:32 +0200

    Add a new ExtensionPoint for AI code review
    and enhance getServerInfo response API enhancement
    with ai models supported.
    
    The individual plugin(s) can contribute to this ExtensionPoint
    by providing the implementation to connect to AI API.
    This will be used to get the AI code review for the current change.
    
    AI API expects 3 parameters:
    1. model: 1. model: AI model to request an AI code review from.
    2. prompt: Prompt containing information about change.
    This will be fetched from existing functionality of
    `Create AI Review Prompt` on the change.
    3. Plugin_name: Name of the plugin to request an AI review from.
    This enables that multiple plugins implementing the AICodeReview
    extension point can be installed simultaneously.
    
    It also contains enhancement of GetServerInfo API response to expose
    supported AI code review implementations whoever is using this
    ExtensionPoint in their respective plugin.
    
    Release-Notes: Added a new ExtensionPoint ai-review
    Change-Id: Ie21c93009e1c4a58570955608ebeaf85f87df39d

---
+++ java/com/google/gerrit/server/restapi/change/AiCodeReview.java
@@ -0,0 +1,69 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.restapi.change;
+
+import com.google.common.base.Strings;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.api.changes.AiCodeReviewProvider;
+import com.google.gerrit.extensions.common.AiCodeReviewInput;
+import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.change.RevisionResource;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+
+@Singleton
+public class AiCodeReview implements RestModifyView<RevisionResource, AiCodeReviewInput> {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private final DynamicMap<AiCodeReviewProvider> aiCodeReviewsProvider;
+
+  @Inject
+  public AiCodeReview(DynamicMap<AiCodeReviewProvider> aiCodeReviewsProvider) {
+    this.aiCodeReviewsProvider = aiCodeReviewsProvider;
+  }
+
+  @Override
+  public Response<String> apply(RevisionResource resource, AiCodeReviewInput input)
+      throws AuthException, BadRequestException, IOException, ResourceConflictException {
+    String response;
+    validateInput(input);
+    AiCodeReviewProvider providerInstance =
+        aiCodeReviewsProvider.get(input.pluginName, input.model);
+    if (providerInstance != null) {
+      response = providerInstance.getAiReview(input);
+    } else {
+      logger.atSevere().log(
+          "No AI Code Review provider found for the given plugin name: %s and model: %s",
+          input.pluginName,
+          input.model);
+      throw new BadRequestException(
+          "Something went wrong. Please check the plugin name and model.");
+    }
+    return Response.ok(response);
+  }
+
+  private void validateInput(AiCodeReviewInput input) throws BadRequestException {
+    if (Strings.isNullOrEmpty(input.prompt)
+        || Strings.isNullOrEmpty(input.model)
+        || Strings.isNullOrEmpty(input.pluginName)) {
+      throw new BadRequestException("Input fields model, prompt and plugin_name are mandatory");
+    }
+  }
+}

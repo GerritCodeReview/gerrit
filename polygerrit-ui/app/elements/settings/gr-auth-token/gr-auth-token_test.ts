4f5ab565904aaed45991d79941ddb922fbe65923
Thomas Dräbing <thomas.draebing@sap.com>
2025-08-14 13:48:05 +0200

    Use supporting-text to display auth token lifetime hint
    The supporting-text attribute provides a more nicely styled support text.
    
    Also hint on how to create a token with unlimited lifetime.
    
    Release-Notes: skip
    Change-Id: I5debf8f39daa7a8b5787312738ee103b8bd35465

--- polygerrit-ui/app/elements/settings/gr-auth-token/gr-auth-token_test.ts
+++ polygerrit-ui/app/elements/settings/gr-auth-token/gr-auth-token_test.ts
@@ -79,11 +79,10 @@ suite('gr-auth-token tests', () => {
                       id="lifetime"
                       inputmode=""
                       placeholder="Lifetime (e.g. 30d)"
+                      supporting-text="Max. allowed lifetime: unlimited. Leave empty for unlimited lifetime."
                       type="text"
                     >
                     </md-outlined-text-field>
-                      </br>
-                      (Max. allowed lifetime: unlimited)
                     </th>
                     <th>
                       <gr-button

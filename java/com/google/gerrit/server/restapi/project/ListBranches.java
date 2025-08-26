d77ecbf320756da8a067ab5672bd13db50ad7bc4
Ben Rohlfs <brohlfs@google.com>
2025-08-26 12:59:59 +0200

    Match target branch name of symbolic refs
    Release-Notes: skip
    Google-Bug-Id: b/441049660
    Change-Id: Ib96c7ed7d4f6a5b4f8b35a7f88ebbdff0b6738df

--- java/com/google/gerrit/server/restapi/project/ListBranches.java
+++ java/com/google/gerrit/server/restapi/project/ListBranches.java
@@ -179,8 +179,14 @@ public class ListBranches implements RestReadView<ProjectResource> {
     // Filter on refs/heads/*, substring and regex without checking ref visibility
     List<Ref> allBranches = readAllBranches(rsrc);
     Set<String> targets = getTargets(allBranches);
+    // If you filter on "main" then you also want to find "HEAD" pointing to "main", not just
+    // where "main" is pointing to. Thus match on both name and target name for symbolic refs.
+    Function<Ref, String> refNameExtractor = (Ref r) -> r.getName();
+    if (ref.isSymbolic()) {
+      refNameExtractor = (Ref r) -> r.getName() + " " + r.getTarget().getName();
+    }
     ImmutableList<Ref> filtered =
-        new RefFilter<>(Constants.R_HEADS, (Ref r) -> r.getName())
+        new RefFilter<>(Constants.R_HEADS, refNameExtractor)
             .subString(matchSubstring).regex(matchRegex).filter(allBranches).stream()
                 .sorted(new RefComparator())
                 .collect(ImmutableList.toImmutableList());

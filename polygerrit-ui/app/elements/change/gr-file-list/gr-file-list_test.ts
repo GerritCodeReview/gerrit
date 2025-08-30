ee0da3a4bcf3a31c3d8ce0271f05551be07b13ea
Paladox none <thomasmulhall410@yahoo.com>
2025-08-30 06:56:19 -0700

    Experiment: DO NOT MERGE
    Change-Id: I945cbddb763b9075b0e5d420f2a449f89c65649f

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list_test.ts
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list_test.ts
@@ -49,7 +49,7 @@ import {
 } from '../../../utils/common-util';
 import {GrFileList, NormalizedFileInfo} from './gr-file-list';
 import {FileInfo, PatchSetNumber} from '../../../api/rest-api';
-import {GrButton} from '../../shared/gr-button/gr-button';
+// import {GrButton} from '../../shared/gr-button/gr-button';
 import {ParsedChangeInfo} from '../../../types/types';
 import {normalize} from '../../../models/change/files-model';
 import {GrDiffHost} from '../../diff/gr-diff-host/gr-diff-host';
@@ -439,7 +439,7 @@ suite('gr-file-list tests', () => {
       element.files = createFiles(250);
       await element.updateComplete;
       await waitEventLoop();
-      assert.equal(200, element.numFilesShown);
+      /*assert.equal(200, element.numFilesShown);
 
       assert.equal(
         queryAll<HTMLDivElement>(element, '.file-row').length,
@@ -464,7 +464,7 @@ suite('gr-file-list tests', () => {
       await waitEventLoop();
 
       assert.equal(element.numFilesShown, 250);
-      assert.isTrue(controlRow.classList.contains('invisible'));
+      assert.isTrue(controlRow.classList.contains('invisible')); */
     });
 
     test('calculate totals for patch number', async () => {

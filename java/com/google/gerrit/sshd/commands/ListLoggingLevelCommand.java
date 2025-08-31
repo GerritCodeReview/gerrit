8a0dd15d13846f73e343095fe066d30ef1f56c7e
paladox <thomasmulhall410@yahoo.com>
2025-08-31 17:27:37 +0100

    Migrate to log4j2
    Main features:
    
    * Log4j2 is garbage-free (or at least low-garbage) since version 2.6,
      see [1]
    * Supports plugins [2].
    * Support for JSON-based configuration and automatic reloading of its
      configuration.
    * Increase in performance [3].
    * Supports java 9 since version 2.10.0.
    * Log4j 1.x is no longer supported and is replaced with log4j 2.x,
      which is recommended and is also maintained.
    * log4j2 is incompatible with log4j 1.x syntax so this is quite a
      breaking change. See [4] and [5].
    
    [1] https://logging.apache.org/log4j/2.x/manual/garbagefree.html
    [2] https://logging.apache.org/log4j/2.x/manual/plugins.html
    [3] https://logging.apache.org/log4j/2.x/manual/performance.html
    [4] https://logging.apache.org/log4j/2.x/migrate-from-log4j1.html
    [5] https://stackoverflow.com/questions/30019585/log4j2-why-would-you-use-it-over-log4j
    
    Existing installs will need to either re-run init or update the
    gerrit.config, containing `[container]` `javaOptions`
    with "-Dflogger.backend_factory=com.google.common.flogger.backend.log4j2.Log4j2BackendFactory#getInstance".
    
    Bug: Issue 40008033
    Release-Notes: Migrate to log4j2; this is a breaking change, all plugins' calls to log4j need to be revisited
    Change-Id: I470078dd0dba0936c56c9936f6644723d486197d

--- java/com/google/gerrit/sshd/commands/ListLoggingLevelCommand.java
+++ java/com/google/gerrit/sshd/commands/ListLoggingLevelCommand.java
@@ -17,15 +17,18 @@ package com.google.gerrit.sshd.commands;
 import static com.google.gerrit.sshd.CommandMetaData.Mode.MASTER_OR_SLAVE;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterators;
 import com.google.gerrit.common.data.GlobalCapability;
 import com.google.gerrit.extensions.annotations.RequiresCapability;
 import com.google.gerrit.sshd.CommandMetaData;
 import com.google.gerrit.sshd.SshCommand;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
 import org.kohsuke.args4j.Argument;
 
 @RequiresCapability(GlobalCapability.ADMINISTRATE_SERVER)
@@ -42,18 +45,40 @@ public class ListLoggingLevelCommand extends SshCommand {
   protected void run() {
     enableGracefulStop();
     Map<String, String> logs = new TreeMap<>();
-    for (Logger logger : getCurrentLoggers()) {
+    for (Logger logger : getAllLoggers()) {
       if (name == null || logger.getName().contains(name)) {
-        logs.put(logger.getName(), logger.getEffectiveLevel().toString());
+        logs.put(logger.getName(), getEffectiveLevel(logger));
       }
     }
     for (Map.Entry<String, String> e : logs.entrySet()) {
-      stdout.println(e.getKey() + ": " + e.getValue());
+      if (e.getKey() != null && !e.getKey().isEmpty()) {
+        stdout.println(e.getKey() + ": " + e.getValue());
+      }
     }
   }
 
-  @SuppressWarnings({"unchecked", "JdkObsolete"})
-  private static ImmutableList<Logger> getCurrentLoggers() {
-    return ImmutableList.copyOf(Iterators.forEnumeration(LogManager.getCurrentLoggers()));
+  private static ImmutableList<Logger> getAllLoggers() {
+    LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+    Set<String> seenNames = new HashSet<>();
+    ImmutableList.Builder<Logger> builder = ImmutableList.builder();
+
+    ctx.getLoggers()
+        .forEach(
+            logger -> {
+              if (seenNames.add(logger.getName())) {
+                builder.add(logger);
+              }
+            });
+
+    return builder.build();
+  }
+
+  private static String getEffectiveLevel(Logger logger) {
+    Level level = logger.getLevel();
+    if (level != null) {
+      return level.name();
+    }
+    Logger parent = logger.getParent();
+    return (parent != null && parent.getLevel() != null) ? parent.getLevel().name() : "UNKNOWN";
   }
 }
--- java/com/google/gerrit/util/logging/JsonLayout.java
+++ java/com/google/gerrit/util/logging/JsonLayout.java
@@ -17,22 +17,32 @@ package com.google.gerrit.util.logging;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.apache.log4j.Layout;
-import org.apache.log4j.spi.LoggingEvent;
+import java.nio.charset.Charset;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.layout.AbstractStringLayout;
 
-public abstract class JsonLayout extends Layout {
+@Plugin(
+    name = "JsonLayout",
+    category = "Core",
+    elementType = Layout.ELEMENT_TYPE,
+    printObject = true)
+public abstract class JsonLayout extends AbstractStringLayout {
+
   private final Gson gson;
   protected final LogTimestampFormatter timestampFormatter;
 
-  public JsonLayout() {
+  protected JsonLayout(Charset charset) {
+    super(charset);
     timestampFormatter = new LogTimestampFormatter();
     gson = newGson();
   }
 
-  public abstract JsonLogEntry toJsonLogEntry(LoggingEvent event);
+  public abstract JsonLogEntry toJsonLogEntry(LogEvent event);
 
   @Override
-  public String format(LoggingEvent event) {
+  public String toSerializable(LogEvent event) {
     return gson.toJson(toJsonLogEntry(event)) + "\n";
   }
 
@@ -42,13 +52,5 @@ public abstract class JsonLayout extends Layout {
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
             .disableHtmlEscaping();
     return gb.create();
-  }
-
-  @Override
-  public void activateOptions() {}
-
-  @Override
-  public boolean ignoresThrowable() {
-    return false;
   }
 }

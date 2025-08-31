8e01f77c5722393e162a9325bfc96c0b8ece22dd
paladox <thomasmulhall410@yahoo.com>
2025-08-31 17:27:37 +0100

    Migrate to log4j2
    Main features:
    
    * Log4j2 is garbage-free (or at least low-garbage) since version 2.6,
      see [1]
    * Supports plugins [2].
    * Support for JSON-based configuration and automatic reloading of its
      configuration.
    * Increase in performance [3].
    * Supports java 9 since version 2.10.0.
    * Log4j 1.x is no longer supported and is replaced with log4j 2.x,
      which is recommended and is also maintained.
    * log4j2 is incompatible with log4j 1.x syntax so this is quite a
      breaking change. See [4] and [5].
    
    [1] https://logging.apache.org/log4j/2.x/manual/garbagefree.html
    [2] https://logging.apache.org/log4j/2.x/manual/plugins.html
    [3] https://logging.apache.org/log4j/2.x/manual/performance.html
    [4] https://logging.apache.org/log4j/2.x/migrate-from-log4j1.html
    [5] https://stackoverflow.com/questions/30019585/log4j2-why-would-you-use-it-over-log4j
    
    Existing installs will need to either re-run init or update the
    gerrit.config, containing `[container]` `javaOptions`
    with "-Dflogger.backend_factory=com.google.common.flogger.backend.log4j2.Log4j2BackendFactory#getInstance".
    
    Bug: Issue 40008033
    Release-Notes: Migrate to log4j2; this is a breaking change, all plugins' calls to log4j need to be revisited
    Change-Id: I470078dd0dba0936c56c9936f6644723d486197d

--- java/com/google/gerrit/sshd/SshLog.java
+++ java/com/google/gerrit/sshd/SshLog.java
@@ -37,19 +37,27 @@ import com.google.gerrit.sshd.SshScope.Context;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
-import org.apache.log4j.AsyncAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggingEvent;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.AsyncAppender;
+import org.apache.logging.log4j.core.config.AppenderRef;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.impl.ContextDataFactory;
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.logging.log4j.message.SimpleMessage;
+import org.apache.logging.log4j.util.SortedArrayStringMap;
+import org.apache.logging.log4j.util.StringMap;
 import org.eclipse.jgit.lib.Config;
 
 @Singleton
 class SshLog implements LifecycleListener, GerritConfigListener {
-  private static final Logger log = Logger.getLogger(SshLog.class);
-
   private static final String JSON_SUFFIX = ".json";
+  private static final String LOG_NAME = "sshd_log";
 
-  protected static final String LOG_NAME = "sshd_log";
   protected static final String P_SESSION = "session";
   protected static final String P_TRACE_ID = "traceId";
   protected static final String P_USER_NAME = "userName";
@@ -68,11 +76,11 @@ class SshLog implements LifecycleListener, GerritConfigListener {
   private volatile AsyncAppender async;
   private final GroupAuditService auditService;
   private final SystemLog systemLog;
-
   private final boolean json;
   private final boolean text;
 
   private final Object lock = new Object();
+  private boolean reconfiguring = false;
 
   @Inject
   SshLog(
@@ -86,32 +94,75 @@ class SshLog implements LifecycleListener, GerritConfigListener {
     this.context = context;
     this.auditService = auditService;
     this.systemLog = systemLog;
-
     this.json = logConfig.isJsonLogging();
     this.text = logConfig.isTextLogging();
 
     if (config.getBoolean("sshd", "requestLog", true)) {
       enableLogging();
+
+      // This is triggered when reconfiguring e.g. in set-level running reset
+      LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+      ctx.addPropertyChangeListener(
+          evt -> {
+            if ("config".equals(evt.getPropertyName())) {
+              synchronized (lock) {
+                enableLogging();
+              }
+            }
+          });
     }
   }
 
   /** Returns true if a change in state has occurred */
   public boolean enableLogging() {
     synchronized (lock) {
-      if (async == null) {
-        async = new AsyncAppender();
+      if (reconfiguring) return false;
+      reconfiguring = true;
+      try {
+        if (async == null || !async.isStarted()) {
+          LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+          Configuration cfg = ctx.getConfiguration();
 
-        if (text) {
-          async.addAppender(systemLog.createAsyncAppender(LOG_NAME, new SshLogLayout()));
-        }
+          if (async != null) {
+            async.stop();
+            async = null;
+          }
 
-        if (json) {
-          async.addAppender(
-              systemLog.createAsyncAppender(LOG_NAME + JSON_SUFFIX, new SshLogJsonLayout()));
+          List<AppenderRef> refList = new ArrayList<>();
+          if (text) {
+            String name = LOG_NAME;
+            if (!cfg.getAppenders().containsKey(name)) {
+              cfg.addAppender(systemLog.createAsyncAppender(name, new SshLogLayout()));
+            }
+            refList.add(AppenderRef.createAppenderRef(name, null, null));
+          }
+
+          if (json) {
+            String name = LOG_NAME + JSON_SUFFIX;
+            if (!cfg.getAppenders().containsKey(name)) {
+              cfg.addAppender(systemLog.createAsyncAppender(name, new SshLogJsonLayout()));
+            }
+            refList.add(AppenderRef.createAppenderRef(name, null, null));
+          }
+
+          AppenderRef[] refs = refList.toArray(new AppenderRef[0]);
+          async =
+              AsyncAppender.newBuilder()
+                  .setName("SshAsync")
+                  .setAppenderRefs(refs)
+                  .setConfiguration(cfg)
+                  .build();
+
+          async.start();
+          cfg.addAppender(async);
+          ctx.updateLoggers();
+
+          return true;
         }
-        return true;
+        return false;
+      } finally {
+        reconfiguring = false;
       }
-      return false;
     }
   }
 
@@ -119,7 +170,7 @@ class SshLog implements LifecycleListener, GerritConfigListener {
   public boolean disableLogging() {
     synchronized (lock) {
       if (async != null) {
-        async.close();
+        async.stop();
         async = null;
         return true;
       }
@@ -136,38 +187,29 @@ class SshLog implements LifecycleListener, GerritConfigListener {
   }
 
   void onLogin() {
-    LoggingEvent entry = log("LOGIN FROM " + session.get().getRemoteAddressAsString());
-    if (async != null) {
-      async.append(entry);
-    }
+    LogEvent entry = log("LOGIN FROM " + session.get().getRemoteAddressAsString());
+    if (async != null) async.append(entry);
     audit(context.get(), "0", "LOGIN");
   }
 
   void onAuthFail(SshSession sd) {
-    final LoggingEvent event =
-        new LoggingEvent( //
-            Logger.class.getName(), // fqnOfCategoryClass
-            log, // logger
-            TimeUtil.nowMs(), // when
-            Level.INFO, // level
-            "AUTH FAILURE FROM " + sd.getRemoteAddressAsString(), // message text
-            "SSHD", // thread name
-            null, // exception information
-            null, // current NDC string
-            null, // caller location
-            null // MDC properties
-            );
+    LogEvent base = log("AUTH FAILURE FROM " + sd.getRemoteAddressAsString());
+    StringMap ctxData = new SortedArrayStringMap();
+    base.getContextData().forEach((k, v) -> ctxData.putValue(k, v));
+    if (sd.getAuthenticationError() != null)
+      ctxData.putValue(P_STATUS, sd.getAuthenticationError());
 
-    event.setProperty(P_SESSION, id(sd.getSessionId()));
-    event.setProperty(P_USER_NAME, sd.getUsername());
+    LogEvent event =
+        Log4jLogEvent.newBuilder()
+            .setLoggerName(base.getLoggerName())
+            .setLevel(Level.INFO)
+            .setTimeMillis(base.getTimeMillis())
+            .setThreadName("SSHD")
+            .setMessage(new SimpleMessage(base.getMessage().getFormattedMessage()))
+            .setContextData(ctxData)
+            .build();
 
-    final String error = sd.getAuthenticationError();
-    if (error != null) {
-      event.setProperty(P_STATUS, error);
-    }
-    if (async != null) {
-      async.append(event);
-    }
+    if (async != null) async.append(event);
     audit(null, "FAIL", "AUTH");
   }
 
@@ -179,146 +221,118 @@ class SshLog implements LifecycleListener, GerritConfigListener {
     final Context ctx = context.get();
     ctx.finish();
 
-    String cmd = extractWhat(dcmd);
+    LogEvent base = log(extractWhat(dcmd));
+    StringMap ctxData = new SortedArrayStringMap();
+    base.getContextData().forEach((k, v) -> ctxData.putValue(k, v));
 
-    final LoggingEvent event = log(cmd);
-    event.setProperty(P_WAIT, ctx.getWait() + "ms");
-    event.setProperty(P_EXEC, ctx.getExec() + "ms");
-    event.setProperty(P_TOTAL_CPU, ctx.getTotalCpu() + "ms");
-    event.setProperty(P_USER_CPU, ctx.getUserCpu() + "ms");
-    event.setProperty(P_MEMORY, String.valueOf(ctx.getAllocatedMemory()));
+    ctxData.putValue(P_WAIT, ctx.getWait() + "ms");
+    ctxData.putValue(P_EXEC, ctx.getExec() + "ms");
+    ctxData.putValue(P_TOTAL_CPU, ctx.getTotalCpu() + "ms");
+    ctxData.putValue(P_USER_CPU, ctx.getUserCpu() + "ms");
+    ctxData.putValue(P_MEMORY, String.valueOf(ctx.getAllocatedMemory()));
+    ctxData.putValue(P_STATUS, statusString(exitValue));
+    if (sshSession.getPeerAgent() != null) ctxData.putValue(P_AGENT, sshSession.getPeerAgent());
+    if (message != null) ctxData.putValue(P_MESSAGE, message);
 
-    final String status;
-    switch (exitValue) {
-      case BaseCommand.STATUS_CANCEL:
-        status = "killed";
-        break;
+    LogEvent event =
+        Log4jLogEvent.newBuilder()
+            .setLoggerName(base.getLoggerName())
+            .setLevel(Level.INFO)
+            .setTimeMillis(base.getTimeMillis())
+            .setThreadName(Thread.currentThread().getName())
+            .setMessage(new SimpleMessage(base.getMessage().getFormattedMessage()))
+            .setContextData(ctxData)
+            .build();
 
-      case BaseCommand.STATUS_NOT_FOUND:
-        status = "not-found";
-        break;
+    if (async != null) async.append(event);
+    audit(ctx, statusString(exitValue), dcmd);
+  }
 
-      case BaseCommand.STATUS_NOT_ADMIN:
-        status = "not-admin";
-        break;
+  void onLogout() {
+    LogEvent entry = log("LOGOUT");
+    if (async != null) async.append(entry);
+    audit(context.get(), "0", "LOGOUT");
+  }
 
-      default:
-        status = String.valueOf(exitValue);
-        break;
+  private LogEvent log(String msg) {
+    SshSession sd = session.get();
+    CurrentUser user = sd.getUser();
+    StringMap contextData = ContextDataFactory.createContextData();
+
+    contextData.putValue(P_SESSION, HexFormat.fromInt(sd.getSessionId()));
+    String traceId = context.get().getTraceId();
+    if (traceId != null) contextData.putValue(P_TRACE_ID, traceId);
+
+    String userName = "-", accountId = "-";
+    if (user != null && user.isIdentifiedUser()) {
+      IdentifiedUser u = user.asIdentifiedUser();
+      userName = u.getUserName().orElse("-");
+      accountId = "a/" + u.getAccountId().toString();
+    } else if (user instanceof PeerDaemonUser) {
+      userName = PeerDaemonUser.USER_NAME;
     }
-    event.setProperty(P_STATUS, status);
-    String peerAgent = sshSession.getPeerAgent();
-    if (peerAgent != null) {
-      event.setProperty(P_AGENT, peerAgent);
-    }
 
-    if (message != null) {
-      event.setProperty(P_MESSAGE, message);
-    }
+    contextData.putValue(P_USER_NAME, userName);
+    contextData.putValue(P_ACCOUNT_ID, accountId);
 
-    if (async != null) {
-      async.append(event);
-    }
-    audit(context.get(), status, dcmd);
+    return Log4jLogEvent.newBuilder()
+        .setLoggerName(SshLog.class.getName())
+        .setLevel(Level.INFO)
+        .setTimeMillis(TimeUtil.nowMs())
+        .setThreadName(Thread.currentThread().getName())
+        .setMessage(new SimpleMessage(msg))
+        .setContextData(contextData)
+        .build();
+  }
+
+  private static String statusString(int exitValue) {
+    return switch (exitValue) {
+      case BaseCommand.STATUS_CANCEL -> "killed";
+      case BaseCommand.STATUS_NOT_FOUND -> "not-found";
+      case BaseCommand.STATUS_NOT_ADMIN -> "not-admin";
+      default -> String.valueOf(exitValue);
+    };
   }
 
   private ListMultimap<String, ?> extractParameters(DispatchCommand dcmd) {
-    if (dcmd == null) {
-      return MultimapBuilder.hashKeys(0).arrayListValues(0).build();
-    }
-    String[] cmdArgs = dcmd.getArguments();
+    if (dcmd == null) return MultimapBuilder.hashKeys(0).arrayListValues(0).build();
+    String[] args = dcmd.getArguments();
     String paramName = null;
     int argPos = 0;
     ListMultimap<String, String> parms = MultimapBuilder.hashKeys().arrayListValues().build();
-    for (int i = 2; i < cmdArgs.length; i++) {
-      String arg = cmdArgs[i];
-      // -- stop parameters parsing
+    for (int i = 2; i < args.length; i++) {
+      String arg = args[i];
       if (arg.equals("--")) {
-        for (i++; i < cmdArgs.length; i++) {
-          parms.put("$" + argPos++, cmdArgs[i]);
-        }
+        for (i++; i < args.length; i++) parms.put("$" + argPos++, args[i]);
         break;
       }
-      // --param=value
       int eqPos = arg.indexOf('=');
       if (arg.startsWith("--") && eqPos > 0) {
         parms.put(arg.substring(0, eqPos), arg.substring(eqPos + 1));
         continue;
       }
-      // -p value or --param value
       if (arg.startsWith("-")) {
-        if (paramName != null) {
-          parms.put(paramName, null);
-        }
+        if (paramName != null) parms.put(paramName, null);
         paramName = arg;
         continue;
       }
-      // value
-      if (paramName == null) {
-        parms.put("$" + argPos++, arg);
-      } else {
+      if (paramName == null) parms.put("$" + argPos++, arg);
+      else {
         parms.put(paramName, arg);
         paramName = null;
       }
     }
-    if (paramName != null) {
-      parms.put(paramName, null);
-    }
+    if (paramName != null) parms.put(paramName, null);
     return parms;
   }
 
-  void onLogout() {
-    LoggingEvent entry = log("LOGOUT");
-    if (async != null) {
-      async.append(entry);
+  private String extractWhat(DispatchCommand dcmd) {
+    if (dcmd == null) return "Command was already destroyed";
+    StringBuilder sb = new StringBuilder(dcmd.getCommandName());
+    for (int i = 1; i < dcmd.getArguments().length; i++) {
+      sb.append(".").append(dcmd.getArguments()[i]);
     }
-    audit(context.get(), "0", "LOGOUT");
-  }
-
-  private LoggingEvent log(String msg) {
-    final SshSession sd = session.get();
-    final CurrentUser user = sd.getUser();
-
-    final LoggingEvent event =
-        new LoggingEvent( //
-            Logger.class.getName(), // fqnOfCategoryClass
-            log, // logger
-            TimeUtil.nowMs(), // when
-            Level.INFO, // level
-            msg, // message text
-            Thread.currentThread().getName(), // thread name
-            null, // exception information
-            null, // current NDC string
-            null, // caller location
-            null // MDC properties
-            );
-
-    event.setProperty(P_SESSION, id(sd.getSessionId()));
-    String traceId = context.get().getTraceId();
-    if (traceId != null) {
-      event.setProperty(P_TRACE_ID, context.get().getTraceId());
-    }
-
-    String userName = "-";
-    String accountId = "-";
-
-    if (user != null && user.isIdentifiedUser()) {
-      IdentifiedUser u = user.asIdentifiedUser();
-      userName = u.getUserName().orElse(null);
-      accountId = "a/" + u.getAccountId().toString();
-
-    } else if (user instanceof PeerDaemonUser) {
-      userName = PeerDaemonUser.USER_NAME;
-    }
-
-    event.setProperty(P_USER_NAME, userName);
-    event.setProperty(P_ACCOUNT_ID, accountId);
-
-    return event;
-  }
-
-  private static String id(int id) {
-    return HexFormat.fromInt(id);
+    return sb.toString();
   }
 
   void audit(Context ctx, Object result, String cmd) {
@@ -330,48 +344,27 @@ class SshLog implements LifecycleListener, GerritConfigListener {
   }
 
   private void audit(Context ctx, Object result, String cmd, ListMultimap<String, ?> params) {
-    String sessionId;
-    CurrentUser currentUser;
-    long created;
-    if (ctx == null) {
-      sessionId = null;
-      currentUser = null;
-      created = TimeUtil.nowMs();
-    } else {
-      SshSession session = ctx.getSession();
-      sessionId = HexFormat.fromInt(session.getSessionId());
-      currentUser = session.getUser();
+    String sessionId = null;
+    CurrentUser user = null;
+    long created = TimeUtil.nowMs();
+
+    if (ctx != null) {
+      SshSession ssh = ctx.getSession();
+      sessionId = HexFormat.fromInt(ssh.getSessionId());
+      user = ssh.getUser();
       created = ctx.getCreated();
     }
-    auditService.dispatch(new SshAuditEvent(sessionId, currentUser, cmd, created, params, result));
-  }
-
-  private String extractWhat(DispatchCommand dcmd) {
-    if (dcmd == null) {
-      return "Command was already destroyed";
-    }
-    StringBuilder commandName = new StringBuilder(dcmd.getCommandName());
-    String[] args = dcmd.getArguments();
-    for (int i = 1; i < args.length; i++) {
-      commandName.append(".").append(args[i]);
-    }
-    return commandName.toString();
+    auditService.dispatch(new SshAuditEvent(sessionId, user, cmd, created, params, result));
   }
 
   @Override
   public Multimap<UpdateResult, ConfigUpdateEntry> configUpdated(ConfigUpdatedEvent event) {
     ConfigKey sshdRequestLog = ConfigKey.create("sshd", "requestLog");
-    if (!event.isValueUpdated(sshdRequestLog)) {
-      return ConfigUpdatedEvent.NO_UPDATES;
-    }
+    if (!event.isValueUpdated(sshdRequestLog)) return ConfigUpdatedEvent.NO_UPDATES;
     boolean stateUpdated;
     try {
       boolean enabled = event.getNewConfig().getBoolean("sshd", "requestLog", true);
-      if (enabled) {
-        stateUpdated = enableLogging();
-      } else {
-        stateUpdated = disableLogging();
-      }
+      stateUpdated = enabled ? enableLogging() : disableLogging();
       return stateUpdated ? event.accept(sshdRequestLog) : ConfigUpdatedEvent.NO_UPDATES;
     } catch (IllegalArgumentException iae) {
       return event.reject(sshdRequestLog);

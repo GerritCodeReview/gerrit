0c3094674c3c37e983745f054e660810ec516c27
paladox <thomasmulhall410@yahoo.com>
2025-08-31 17:27:37 +0100

    Migrate to log4j2
    Main features:
    
    * Log4j2 is garbage-free (or at least low-garbage) since version 2.6,
      see [1]
    * Supports plugins [2].
    * Support for JSON-based configuration and automatic reloading of its
      configuration.
    * Increase in performance [3].
    * Supports java 9 since version 2.10.0.
    * Log4j 1.x is no longer supported and is replaced with log4j 2.x,
      which is recommended and is also maintained.
    * log4j2 is incompatible with log4j 1.x syntax so this is quite a
      breaking change. See [4] and [5].
    
    [1] https://logging.apache.org/log4j/2.x/manual/garbagefree.html
    [2] https://logging.apache.org/log4j/2.x/manual/plugins.html
    [3] https://logging.apache.org/log4j/2.x/manual/performance.html
    [4] https://logging.apache.org/log4j/2.x/migrate-from-log4j1.html
    [5] https://stackoverflow.com/questions/30019585/log4j2-why-would-you-use-it-over-log4j
    
    Bug: Issue 40008033
    Release-Notes: Migrate to log4j2; this is a breaking change, all plugins' calls to log4j need to be revisited
    Change-Id: I470078dd0dba0936c56c9936f6644723d486197d

--- java/com/google/gerrit/server/git/GarbageCollectionLogFile.java
+++ java/com/google/gerrit/server/git/GarbageCollectionLogFile.java
@@ -20,19 +20,40 @@ import com.google.gerrit.server.config.LogConfig;
 import com.google.gerrit.server.config.SitePaths;
 import com.google.gerrit.server.util.SystemLog;
 import com.google.inject.Inject;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import org.apache.log4j.Appender;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
+/** Manages the gc_log configuration, resilient against live Log4j reconfiguration. */
 public class GarbageCollectionLogFile implements LifecycleListener {
   private static final String LOG_NAME = "gc_log";
 
+  private final Path logdir;
+  private final boolean rotate;
+
+  private static final Object lock = new Object();
+  private static final AtomicBoolean reconfiguring = new AtomicBoolean(false);
+
   @Inject
   public GarbageCollectionLogFile(SitePaths sitePaths, LogConfig config) {
+    this.logdir = sitePaths.logs_dir;
+    this.rotate = config.shouldRotate();
+
     if (SystemLog.shouldConfigure()) {
-      initLogSystem(sitePaths.logs_dir, config.shouldRotate());
+      LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+      reinstallGcLoggers(ctx, logdir, rotate);
+
+      ctx.addPropertyChangeListener(
+          evt -> {
+            if ("config".equals(evt.getPropertyName())) {
+              reinstallGcLoggers(ctx, logdir, rotate);
+            }
+          });
     }
   }
 
@@ -41,24 +62,54 @@ public class GarbageCollectionLogFile implements LifecycleListener {
 
   @Override
   public void stop() {
-    getLogger(GarbageCollection.class).removeAllAppenders();
-    getLogger(GarbageCollectionRunner.class).removeAllAppenders();
+    removeAllAppenders(getLogger(GarbageCollection.class));
+    removeAllAppenders(getLogger(GarbageCollectionRunner.class));
   }
 
-  private static void initLogSystem(Path logdir, boolean rotate) {
-    Appender appender =
-        SystemLog.createAppender(logdir, LOG_NAME, new PatternLayout("[%d] %-5p %x: %m%n"), rotate);
-    initGcLogger(getLogger(GarbageCollection.class), appender);
-    initGcLogger(getLogger(GarbageCollectionRunner.class), appender);
+  private static void reinstallGcLoggers(LoggerContext ctx, Path logdir, boolean rotate) {
+    synchronized (lock) {
+      if (reconfiguring.get()) {
+        return;
+      }
+      reconfiguring.set(true);
+      try {
+        Appender existing = getLogger(GarbageCollection.class).getAppenders().get(LOG_NAME);
+        if (existing != null && existing.isStarted()) {
+          return; // already configured
+        }
+
+        Appender appender =
+            SystemLog.createAppender(
+                logdir,
+                LOG_NAME,
+                PatternLayout.newBuilder()
+                    .withPattern("[%d] %-5p %x: %m%n")
+                    .withCharset(StandardCharsets.UTF_8)
+                    .build(),
+                rotate);
+
+        initGcLogger(getLogger(GarbageCollection.class), appender);
+        initGcLogger(getLogger(GarbageCollectionRunner.class), appender);
+
+        ctx.updateLoggers();
+      } finally {
+        reconfiguring.set(false);
+      }
+    }
   }
 
   private static Logger getLogger(Class<?> clazz) {
-    return LogManager.getLogger(Platform.getBackend(clazz.getName()).getLoggerName());
+    String loggerName = Platform.getBackend(clazz.getName()).getLoggerName();
+    return (Logger) LogManager.getLogger(loggerName);
   }
 
   private static void initGcLogger(Logger gcLogger, Appender appender) {
-    gcLogger.removeAllAppenders();
+    removeAllAppenders(gcLogger);
     gcLogger.addAppender(appender);
-    gcLogger.setAdditivity(false);
+    gcLogger.setAdditive(false);
+  }
+
+  private static void removeAllAppenders(Logger logger) {
+    logger.getAppenders().values().forEach(a -> logger.removeAppender(a));
   }
 }

dd9f2070d78b98b2069a972ba7dc957d0f134e25
Dhruv Srivastava <dhruvsri@google.com>
2025-09-01 12:13:02 +0200

    Add bug/issue footers to reverted change when reverting a submission
    When reverting a single change we transfer exiting bug/issue
    footers to the created revert.
    
    When reverting an entire submission the behaviour we do the same.
    submission.
    
    Release-Notes: skip
    Google-bug-id: b/343552361
    Change-Id: I34358488e55ad36d5a3539598bbc55524f88b15a

--- javatests/com/google/gerrit/acceptance/api/change/RevertIT.java
+++ javatests/com/google/gerrit/acceptance/api/change/RevertIT.java
@@ -1630,13 +1630,7 @@ public class RevertIT extends AbstractDaemonTest {
         pushFactory.create(
             admin.newIdent(),
             testRepo,
-            "Change with bug and issue\\n"
-                + //
-                "\\n"
-                + //
-                "Bug: 12345\\n"
-                + //
-                "Issue: 67890",
+            "Change with bug and issue\n" + "\n" + "Bug: 12345\n" + "Issue: 67890",
             "a.txt",
             "content");
     PushOneCommit.Result r = push.to("refs/for/master");
@@ -1655,19 +1649,41 @@ public class RevertIT extends AbstractDaemonTest {
   }
 
   @Test
+  public void revertSubmissionWithBugAndIssueFooters() throws Exception {
+    // Create a change with bug and issue footers
+    PushOneCommit push =
+        pushFactory.create(
+            admin.newIdent(),
+            testRepo,
+            "Change with bug and issue\n" + "\n" + "Bug: 12345\n" + "Issue: 67890",
+            "a.txt",
+            "content");
+    PushOneCommit.Result r = push.to("refs/for/master");
+    r.assertOkStatus();
+
+    gApi.changes().id(r.getChangeId()).revision(r.getCommit().name()).review(ReviewInput.approve());
+    gApi.changes().id(r.getChangeId()).revision(r.getCommit().name()).submit();
+
+    // Revert the submission
+    List<ChangeInfo> revertChanges =
+        gApi.changes().id(r.getChangeId()).revertSubmission().revertChanges;
+    assertThat(revertChanges).hasSize(1);
+    ChangeInfo revertChange = revertChanges.get(0);
+
+    // Check that the revert commit message contains the footers
+    String commitMessage = gApi.changes().id(revertChange.id).current().commit(false).message;
+    assertThat(commitMessage).contains("Bug: 12345");
+    assertThat(commitMessage).contains("Issue: 67890");
+  }
+
+  @Test
   public void revertChangeWithExistingFooters() throws Exception {
     // Create a change with bug and issue footers
     PushOneCommit push =
         pushFactory.create(
             admin.newIdent(),
             testRepo,
-            "Change with bug and issue\\n"
-                + //
-                "\\n"
-                + //
-                "Bug: 12345\\n"
-                + //
-                "Issue: 67890",
+            "Change with bug and issue\n" + "\n" + "Bug: 12345\n" + "Issue: 67890",
             "a.txt",
             "content");
     PushOneCommit.Result r = push.to("refs/for/master");
@@ -1680,6 +1696,38 @@ public class RevertIT extends AbstractDaemonTest {
     RevertInput revertInput = new RevertInput();
     revertInput.message = "Reverting this change\n\nBug: 12345\nIssue: 67890";
     ChangeInfo revertChange = gApi.changes().id(r.getChangeId()).revert(revertInput).get();
+
+    // Check that the footers are not duplicated
+    String commitMessage = gApi.changes().id(revertChange.id).current().commit(false).message;
+    int bugOccurrences = commitMessage.split("Bug: 12345", -1).length - 1;
+    assertThat(bugOccurrences).isEqualTo(1);
+    int issueOccurrences = commitMessage.split("Issue: 67890", -1).length - 1;
+    assertThat(issueOccurrences).isEqualTo(1);
+  }
+
+  @Test
+  public void revertSubmissionWithExistingFooters() throws Exception {
+    // Create a change with bug and issue footers
+    PushOneCommit push =
+        pushFactory.create(
+            admin.newIdent(),
+            testRepo,
+            "Change with bug and issue\n" + "\n" + "Bug: 12345\n" + "Issue: 67890",
+            "a.txt",
+            "content");
+    PushOneCommit.Result r = push.to("refs/for/master");
+    r.assertOkStatus();
+
+    gApi.changes().id(r.getChangeId()).revision(r.getCommit().name()).review(ReviewInput.approve());
+    gApi.changes().id(r.getChangeId()).revision(r.getCommit().name()).submit();
+
+    // Revert the submission with a message that already contains the footers
+    RevertInput revertInput = new RevertInput();
+    revertInput.message = "Reverting this change\n\nBug: 12345\nIssue: 67890";
+    List<ChangeInfo> revertChanges =
+        gApi.changes().id(r.getChangeId()).revertSubmission().revertChanges;
+    assertThat(revertChanges).hasSize(1);
+    ChangeInfo revertChange = revertChanges.get(0);
 
     // Check that the footers are not duplicated
     String commitMessage = gApi.changes().id(revertChange.id).current().commit(false).message;

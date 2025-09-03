05fffcf477c43293ccdfc809c077edde599b7243
I547210 <hitesh.chaudhari@sap.com>
2025-09-03 09:32:54 +0200

    Move `Create AI Review Prompt` impl to backend.
    Curently, Create AI Review Prompt is implmented in the UI (TS).
    Moving the implentation to the backend using API.
    
    API has a Query parameter `command` which can expects one of the
    following values:
    - code-review: generates code proview prompt for UI radio button
      'Help me with Review'.
    - commit-message: generates commit message prompt to enhance commit
      message of change for UI radio button `Improve commit message`
    - patch-content: to generate the patch content for UI radio button
      `Just patch content`.
    
    Release-Notes: Move Create AI Review Prompt impl to backend.
    Change-Id: If8506d76adbb657844e8dd2f25dfcbffdd251e18

---
+++ java/com/google/gerrit/pgm/init/InitAIPrompt.java
@@ -0,0 +1,65 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,//
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.pgm.init;
+
+import com.google.gerrit.pgm.init.api.InitStep;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Singleton
+public class InitAIPrompt implements InitStep {
+
+  public static final String CODE_REVIEW = "code_review_prompt_template.txt";
+  public static final String COMMIT_MSG = "commit_message_prompt_template.txt";
+
+  private final SitePaths sitePaths;
+
+  @Inject
+  public InitAIPrompt(SitePaths sitePaths) {
+    this.sitePaths = sitePaths;
+  }
+
+  @Override
+  public void run() throws Exception {
+    Path etcDir = sitePaths.resolve("etc");
+    Files.createDirectories(etcDir);
+
+    createFileIfMissing(etcDir.resolve(CODE_REVIEW), readPromptFile(CODE_REVIEW));
+    createFileIfMissing(etcDir.resolve(COMMIT_MSG), readPromptFile(COMMIT_MSG));
+  }
+
+  private void createFileIfMissing(Path path, String content) throws IOException {
+    if (!Files.exists(path)) {
+      Files.writeString(path, content, StandardCharsets.UTF_8);
+    }
+  }
+
+  private String readPromptFile(String fileName) throws IOException {
+    try (InputStream in =
+        getClass().getResourceAsStream("/com/google/gerrit/pgm/init/prompt/" + fileName)) {
+      if (in == null) {
+        throw new IOException("Resource file not found: " + fileName);
+      }
+
+      return new String(in.readAllBytes(), StandardCharsets.UTF_8);
+    }
+  }
+}
---
+++ javatests/com/google/gerrit/acceptance/rest/change/GetPromptIT.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gerrit.acceptance.rest.change;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.BinaryResult;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.server.change.RevisionResource;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.gerrit.server.restapi.change.GetAiPrompt;
+import com.google.gerrit.server.restapi.change.GetPatch;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.Test;
+
+public class GetPromptIT extends AbstractDaemonTest {
+
+  @Test
+  public void returnsPatchContent_whenCommandIsPatchContent() throws Exception {
+    Change.Id changeId = createChange().getChange().getId();
+
+    RestResponse r =
+        adminRestSession.get(
+            "/changes/" + changeId + "/revisions/current/ai_prompt?command=patch_content");
+    r.assertOK();
+
+    String text = r.getEntityContent();
+    assertThat(text).isNotNull();
+    assertThat(text.substring(text.indexOf('\n') + 1)).startsWith("\"From ");
+  }
+
+  @Test
+  public void returnsCodeReviewPromptTemplate_whenCommandIsCodeReview() throws Exception {
+    Path siteDir =
+        createPromptFile(
+            "code_review_prompt_template.txt", "This is the code review template with {{Patch}}");
+    SitePaths sitePaths = new SitePaths(siteDir);
+    GetPatch getPatch = fetchPatchContent();
+    GetAiPrompt getPrompt = new GetAiPrompt(sitePaths, getPatch);
+    getPrompt.command = "code_review";
+
+    String response = getPrompt.apply(mock(RevisionResource.class)).value();
+
+    assertThat(response).contains("FAKE_PATCH_CONTENT");
+    assertThat(response).doesNotContain("{{Patch}}");
+  }
+
+  @Test
+  public void returnsCommitMessagePromptTemplate_whenCommandCommitMessage() throws Exception {
+    Path siteDir =
+        createPromptFile(
+            "commit_message_prompt_template.txt",
+            "This is the commit message template with {{Patch}}");
+    SitePaths sitePaths = new SitePaths(siteDir);
+    GetPatch getPatch = fetchPatchContent();
+    GetAiPrompt getPrompt = new GetAiPrompt(sitePaths, getPatch);
+    getPrompt.command = "commit_message";
+
+    String response = getPrompt.apply(mock(RevisionResource.class)).value();
+
+    assertThat(response).contains("FAKE_PATCH_CONTENT");
+    assertThat(response).doesNotContain("{{Patch}}");
+  }
+
+  private static GetPatch fetchPatchContent()
+      throws BadRequestException,
+          ResourceConflictException,
+          IOException,
+          ResourceNotFoundException {
+    GetPatch getPatch = mock(GetPatch.class);
+    BinaryResult fakePatch = BinaryResult.create("FAKE_PATCH_CONTENT");
+    when(getPatch.apply(any())).thenReturn(Response.ok(fakePatch));
+    return getPatch;
+  }
+
+  private static Path createPromptFile(String file, String csq) throws IOException {
+    Path siteDir = Files.createTempDirectory("gerrit_site");
+    Path etcDir = siteDir.resolve("etc");
+    Files.createDirectories(etcDir);
+
+    Path templateFile = etcDir.resolve(file);
+    Files.writeString(templateFile, csq);
+    return siteDir;
+  }
+
+  @Test
+  public void returnsBadRequest_whenCommandMissing() throws Exception {
+    Change.Id changeId = createChange().getChange().getId();
+
+    RestResponse r = adminRestSession.get("/changes/" + changeId + "/revisions/current/ai_prompt");
+    r.assertBadRequest();
+  }
+
+  @Test
+  public void returnsBadRequest_whenCommandInvalid() throws Exception {
+    Change.Id changeId = createChange().getChange().getId();
+
+    RestResponse r =
+        adminRestSession.get("/changes/" + changeId + "/revisions/current/ai_prompt?command=FOO");
+    r.assertBadRequest();
+    assertThat(r.getEntityContent()).contains("Invalid command value");
+  }
+}

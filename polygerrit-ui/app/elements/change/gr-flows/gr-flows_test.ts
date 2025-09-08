149d697746dacf62b3291a860b5c32881ea3d2d6
Dhruv Srivastava <dhruvsri@google.com>
2025-09-08 11:02:33 +0200

    Add Delete Flow functionality
    Change-Id: I04ee0d290cecd6fa0b0cbd6fea261be931da9a56

--- polygerrit-ui/app/elements/change/gr-flows/gr-flows_test.ts
+++ polygerrit-ui/app/elements/change/gr-flows/gr-flows_test.ts
@@ -12,6 +12,7 @@ import {queryAndAssert, stubRestApi} from '../../../test/test-utils';
 import {NumericChangeId} from '../../../types/common';
 import {GrCreateFlow} from './gr-create-flow';
 import sinon from 'sinon';
+import {GrButton} from '../../shared/gr-button/gr-button';
 
 suite('gr-flows tests', () => {
   let element: GrFlows;
@@ -89,11 +90,12 @@ suite('gr-flows tests', () => {
           <div>
             <h2 class="main-heading">Existing Flows</h2>
             <div class="flow">
-              <div class="flow-id hidden">Flow flow1</div>
-              <div class="owner-container">
-                Owner:
-                <gr-account-chip></gr-account-chip>
+              <div class="flow-header">
+                <gr-button link title="Delete flow">
+                  <gr-icon icon="delete" filled></gr-icon>
+                </gr-button>
               </div>
+              <div class="flow-id hidden">Flow flow1</div>
               <div>
                 Created:
                 <gr-date-formatter withtooltip></gr-date-formatter>
@@ -120,11 +122,12 @@ suite('gr-flows tests', () => {
               </div>
             </div>
             <div class="flow">
-              <div class="flow-id hidden">Flow flow2</div>
-              <div class="owner-container">
-                Owner:
-                <gr-account-chip></gr-account-chip>
+              <div class="flow-header">
+                <gr-button link title="Delete flow">
+                  <gr-icon icon="delete" filled></gr-icon>
+                </gr-button>
               </div>
+              <div class="flow-id hidden">Flow flow2</div>
               <div>
                 Created:
                 <gr-date-formatter withtooltip></gr-date-formatter>
@@ -149,8 +152,48 @@ suite('gr-flows tests', () => {
           </div>
         </div>
       `,
-      {ignoreAttributes: ['style', 'class', 'account', 'changenum', 'datestr']}
+      {
+        ignoreAttributes: [
+          'style',
+          'class',
+          'account',
+          'changenum',
+          'datestr',
+          'aria-disabled',
+          'role',
+          'tabindex',
+        ],
+      }
     );
+  });
+
+  test('deletes a flow', async () => {
+    const flows: FlowInfo[] = [
+      {
+        uuid: 'flow1',
+        owner: {name: 'owner1'},
+        created: '2025-01-01T10:00:00.000Z' as Timestamp,
+        stages: [
+          {
+            expression: {condition: 'label:Code-Review=+1'},
+            state: FlowStageState.DONE,
+          },
+        ],
+      },
+    ];
+    stubRestApi('listFlows').returns(Promise.resolve(flows));
+    const deleteFlowStub = stubRestApi('deleteFlow').returns(
+      Promise.resolve(new Response())
+    );
+    await element['loadFlows']();
+    await element.updateComplete;
+
+    const deleteButton = queryAndAssert<GrButton>(element, '.flow gr-button');
+    deleteButton.click();
+
+    await element.updateComplete;
+
+    assert.isTrue(deleteFlowStub.calledOnceWith(123, 'flow1'));
   });
 
   test('reloads flows on flow-created event', async () => {

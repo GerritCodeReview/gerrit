da96a42939022d541119ab4ae008ee7a99050e94
Ravi Mistry <rmistry@google.com>
2025-09-13 19:52:21 +0000

    Experimenting
    Change-Id: I0dc608560117fa798f397e2fedc736224c8cbec6

---
+++ polygerrit-ui/app/elements/shared/gr-avatar/robot-avatar-map.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ * @fileoverview Map of profile pictures for specific known robots that post
+ * comments in Gerrit used to override the empty user avatar image when
+ * possible.
+ */
+const ROBOT_AVATAR_OVERRIDES: ReadonlyMap<string, string> = new Map([
+  [
+    'treehugger-gerrit@google.com',
+    // TODO: Replace with Treehugger avatar when available
+    'https://gstatic.com/buganizer/img/v2/gerrit_logo.svg',
+  ],
+]);
+
+/**
+ * Returns a custom avatar URL for a known robot account, or undefined if not
+ * found.
+ */
+export function getRobotAvatarUrl(email?: string): string|undefined {
+  if (!email) {
+    return undefined;
+  }
+  return ROBOT_AVATAR_OVERRIDES.get(email);
+}

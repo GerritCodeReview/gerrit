ed078275100c07bed158ccad3b68d4481dcd59b1
Paladox none <thomasmulhall410@yahoo.com>
2025-09-22 13:25:40 -0700

    Fix gr-validation-options layout in gr-confirm-rebase-dialog
    Screenshots: https://imgur.com/a/aJljtPc
    
    Release-Notes: skip
    Change-Id: I786d0dd13c763d423b58c3fc3a29aa3803759a70

--- polygerrit-ui/app/elements/change/gr-validation-options/gr-validation-options.ts
+++ polygerrit-ui/app/elements/change/gr-validation-options/gr-validation-options.ts
@@ -29,9 +29,8 @@ export class GrValidationOptions extends LitElement {
           display: block;
           margin-left: -4px;
         }
-        md-checkbox {
-          --md-checkbox-container-size: 15px;
-          --md-checkbox-icon-size: 15px;
+        label {
+          cursor: pointer;
         }
       `,
     ];
@@ -60,10 +59,14 @@ export class GrValidationOptions extends LitElement {
     return html`
       <md-checkbox
         class="selectionLabel"
+        id=${option.name}
+        touch-target="wrapper"
         ?checked=${!!this.isOptionSelected.get(option.name)}
         @click=${() => this.toggleCheckbox(option)}
       ></md-checkbox>
-      ${capitalizeFirstLetter(option.description)}
+      <label for=${option.name}
+        >${capitalizeFirstLetter(option.description)}</label
+      >
     `;
   }
 

272bec3494df5eb2a302531d48efa7a489e070d1
Dhruv Srivastava <dhruvsri@google.com>
2025-09-23 12:46:36 +0200

    Add isFlowsEnabled API
    Internally we want to control whether flows is enabled for the
    user or not.
    
    Bug: Google b/441706571
    Release-Notes: skip
    Change-Id: I39552e991fabe23578a5457f606c409c45b8da9e

--- java/com/google/gerrit/acceptance/TestExtensions.java
+++ java/com/google/gerrit/acceptance/TestExtensions.java
@@ -30,6 +30,8 @@ import com.google.gerrit.entities.PatchSet;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.SubmitRecord;
 import com.google.gerrit.exceptions.StorageException;
+import com.google.gerrit.extensions.common.IsFlowsEnabledInfo;
+import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.server.ChangeUtil;
 import com.google.gerrit.server.PluginPushOption;
 import com.google.gerrit.server.ValidationOptionsListener;
@@ -231,6 +233,13 @@ public class TestExtensions {
               .build();
       flows.put(flowKey, flow);
       return flow;
+    }
+
+    @Override
+    public IsFlowsEnabledInfo isFlowsEnabled(Project.NameKey projectName, Change.Id changeId)
+        throws RestApiException {
+      // Always return true for testing purposes.
+      return new IsFlowsEnabledInfo(true);
     }
 
     @Override

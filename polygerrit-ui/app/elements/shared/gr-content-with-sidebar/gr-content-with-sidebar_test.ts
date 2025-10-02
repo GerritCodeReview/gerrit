0061f5ae3dbf5e61cfae7738c6f1bb2ceb8dc856
Ben Rohlfs <brohlfs@google.com>
2025-10-02 07:37:02 +0200

    Implement a sidebar for the change-view
    This will be used for AI Chat.
    
    Screenshot https://imgur.com/a/tVAQJQx
    
    Google-Bug-Id: b/437944720
    Release-Notes: skip
    Change-Id: Ie23938106577cf506ca646e8d639f509263413d4

---
+++ polygerrit-ui/app/elements/shared/gr-content-with-sidebar/gr-content-with-sidebar_test.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import '../../../test/common-test-setup';
+import './gr-content-with-sidebar';
+import {assert, fixture, html} from '@open-wc/testing';
+import {GrContentWithSidebar} from './gr-content-with-sidebar';
+
+suite('gr-content-with-sidebar tests', () => {
+  let element: GrContentWithSidebar;
+
+  setup(async () => {
+    element = await fixture<GrContentWithSidebar>(
+      html`<gr-content-with-sidebar></gr-content-with-sidebar>`
+    );
+    await element.updateComplete;
+  });
+
+  test('renders no sidebar', async () => {
+    element.hideSide = true;
+    await element.updateComplete;
+
+    assert.shadowDom.equal(
+      element,
+      /* HTML */ `
+        <div>
+          <div style="width:calc(100% - 0px);">
+            <slot name="main"></slot>
+          </div>
+        </div>
+      `
+    );
+  });
+
+  test('renders sidebar', async () => {
+    element.hideSide = false;
+    await element.updateComplete;
+
+    assert.shadowDom.equal(
+      element,
+      /* HTML */ `
+        <div>
+          <div style="width: calc(100% - 300px);">
+            <slot name="main"> </slot>
+          </div>
+          <div class="sidebar-wrapper" style="width:300px;">
+            <div class="resizer-wrapper">
+              <div class="resizer"></div>
+            </div>
+            <div class="sidebar">
+              <slot name="side"> </slot>
+            </div>
+          </div>
+        </div>
+      `
+    );
+  });
+});

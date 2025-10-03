c113a7d7a92165661f25a13e8d1f81c0dd098659
Srishti Gupta <srishtigt@google.com>
2025-10-03 03:28:56 -0700

    [DO NOT SUBMIT]Add comment "// C1" to TESTING2.java.
    Change-Id: I579988eb1cf89e21333214ddf09bd83c11636eed

--- javatests/com/google/gerrit/testing/TESTING2.java
+++ javatests/com/google/gerrit/testing/TESTING2.java
@@ -1,3 +1,10 @@
+<<<<<<< PATCH SET (2224d8d27709e7d7b111b1f3832373f8b61ac7b3 [DO NOT SUBMIT]Add comment "// C1" to TESTING2.java.)
+// Test file 2
+
+// C1
+||||||| BASE      (68ad7a62dbf411a522b2f982593dd8372eada5a6 [DO NOT SUBMIT]Add new test file TESTING2.java.)
+// Test file 2
+=======
 <<<<<<< PATCH SET (68ad7a62dbf411a522b2f982593dd8372eada5a6 [DO NOT SUBMIT]Add new test file TESTING2.java.)
 // Test file 2
 ||||||| BASE      (9dd75e5330af04400f43886067f67f1f9590d02f Merge "doc: Document necessity of using 'http.saveCookies'")
@@ -6,3 +13,4 @@
 
 // C1
 >>>>>>> BASE      (2eaec583179e18f8395c3d7852c8678aca588723 Add marker for Commit 2)
+>>>>>>> BASE      (40d0b90159cacef7be37deacc3134117f642ea86 [DO NOT SUBMIT]Add new test file TESTING2.java.)

4f588ec4c460f3d8813a77f7e9916b523f1abd12
Srishti Gupta <srishtigt@google.com>
2025-10-03 02:24:46 -0700

    Add marker for Commit 2
    This change is part of testing a chain of CLs.
    
    Change-Id: I8c221f568ec24d646d452141bcb501bd68ee89c3

--- java/com/google/gerrit/testing/testing_chain_ofCls.java
+++ java/com/google/gerrit/testing/testing_chain_ofCls.java
@@ -2,6 +2,11 @@
 <<<<<<< PATCH SET (5b5faa294c0280d0a8d433af2ce074aeb88a9d2b [Do not submit] commit 1)
 
 // Commit 1
+<<<<<<< PATCH SET (b9892c6906314c185c961750a48fe64bf93b1a15 Add marker for Commit 2)
+
+// Commit 2
+||||||| BASE      (5b5faa294c0280d0a8d433af2ce074aeb88a9d2b [Do not submit] commit 1)
+=======
 ||||||| BASE      (331245c75d9e3d9c37eb6ac13bec4371dee55d09 [Do not submit] Testing)
 =======
 <<<<<<< PATCH SET (331245c75d9e3d9c37eb6ac13bec4371dee55d09 [Do not submit] Testing)
@@ -13,3 +18,4 @@
 // Commit 2
 >>>>>>> BASE      (01c4f4fbef41cf8ae1869df0f43dd7df76f39cbe [DO NOT SUBMIT]Add comment "// C1" to TESTING2.java.)
 >>>>>>> BASE      (28e8c6c538f36efda26a463d6643b27d1d581e0b [Do not submit] Testing)
+>>>>>>> BASE      (5af758280783544569a768bc415d936da094d47d [Do not submit] commit 1)

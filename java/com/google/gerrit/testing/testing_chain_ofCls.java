5af758280783544569a768bc415d936da094d47d
Srishti Gupta <srishtigt@google.com>
2025-10-03 02:24:03 -0700

    [Do not submit] commit 1
    Change-Id: I1cc0b5fc9827a4d1801d27f5b99747bc11346245

--- java/com/google/gerrit/testing/testing_chain_ofCls.java
+++ java/com/google/gerrit/testing/testing_chain_ofCls.java
@@ -1,4 +1,9 @@
 // Testing chain of CLs
+<<<<<<< PATCH SET (5b5faa294c0280d0a8d433af2ce074aeb88a9d2b [Do not submit] commit 1)
+
+// Commit 1
+||||||| BASE      (331245c75d9e3d9c37eb6ac13bec4371dee55d09 [Do not submit] Testing)
+=======
 <<<<<<< PATCH SET (331245c75d9e3d9c37eb6ac13bec4371dee55d09 [Do not submit] Testing)
 ||||||| BASE      (2224d8d27709e7d7b111b1f3832373f8b61ac7b3 [DO NOT SUBMIT]Add comment "// C1" to TESTING2.java.)
 =======
@@ -7,3 +12,4 @@
 
 // Commit 2
 >>>>>>> BASE      (01c4f4fbef41cf8ae1869df0f43dd7df76f39cbe [DO NOT SUBMIT]Add comment "// C1" to TESTING2.java.)
+>>>>>>> BASE      (28e8c6c538f36efda26a463d6643b27d1d581e0b [Do not submit] Testing)

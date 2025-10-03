73e0cec7790aded25941cbe92be4fd8a35353884
Srishti Gupta <srishtigt@google.com>
2025-10-03 01:26:40 -0700

    [Do not submit] commit 2
    Change-Id: I67dcbd74f56594c466a32b5b6ec2c7af3de69bd4

--- java/com/google/gerrit/server/account/testing.java
+++ java/com/google/gerrit/server/account/testing.java
@@ -2,7 +2,13 @@
 // Testing
 
 // Commit 1
+<<<<<<< PATCH SET (e859626459ca7613ac568787713aa818a2b406cd [Do not submit] commit 2)
+
+// Commit 2
+||||||| BASE      (538f1c35c52d26b8b0bafb1d38810230ee68e8ec [Do not Submit] Chain of cl)
+=======
 ||||||| BASE      (baebc2bd8a245c326021faf4fef1b6a47d1eb8eb [Do not Submit])
 // Testing 
 =======
 >>>>>>> BASE      (c59b53bfcb09c0c5f310dcbc3db4584ecd124e87 [Do not Submit])
+>>>>>>> BASE      (44e0a2031acd9ebde89abaaf8d5b8d639aca238d [Do not Submit] Chain of cl)

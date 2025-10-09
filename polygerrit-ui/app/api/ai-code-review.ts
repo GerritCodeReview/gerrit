725cb6ddf1279184a5bda6a35874b1c3a701b2ac
Ben Rohlfs <brohlfs@google.com>
2025-10-09 12:04:11 +0200

    Add a plugin API for AI Chat
    Release-Notes: skip
    Google-Bug-Id: b/437944720
    Change-Id: I6bf89a58ccf638649d3eb6d8642c2b9d138f245e

---
+++ polygerrit-ui/app/api/ai-code-review.ts
@@ -0,0 +1,71 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import {ChangeInfo} from './rest-api';
+
+export declare interface AiCodeReviewPluginApi {
+  /**
+   * Must only be called once. You cannot register twice. You cannot unregister.
+   */
+  register(provider: AiCodeReviewProvider): void;
+}
+
+export declare interface ChatRequest {
+  /**
+   * The prompt to be sent to the LLM.
+   */
+  prompt: string;
+  /**
+   * UUID of the conversation. To start a new conversation, the caller should
+   * generate a new UUID and set turn_index to 0.
+   * To continue an existing conversation, the caller should provide the UUID of
+   * the conversation.
+   */
+  conversationId: string;
+  /**
+   * The 0-based turn index of the request. The caller should set it based on
+   * the history of the conversation. It should be one more than the last known
+   * turn. For new conversations, it should be 0.
+   */
+  turnIndex: number;
+  /**
+   * Plugins can choose what context they want to derive from the change and
+   * send along to their backends. `changeInfo` contains broadly all the
+   * information about the change, and the plugin can also make additional
+   * requests to the REST API (e.g. getting patch content) by using properties
+   * from the change info.
+   */
+  changeInfo: ChangeInfo;
+}
+
+/**
+ * The chat response may come in as a stream, so instead of just one response
+ * object this listener will get multiple calls until the response is completed.
+ */
+export declare interface ChatResponseListener {
+  /**
+   * Emits one piece of a streaming text response from the backend, to be
+   * interpreted as markdown by the web app and to be shown as is to the user.
+   */
+  emitText(text: string): void;
+  /**
+   * Emits an error message, indicating that the turn has failed. Will be
+   * immediately followed by a done() call.
+   */
+  emitError(error: string): void;
+  /**
+   * The turn is completed. The listener can be discarded.
+   */
+  done(): void;
+}
+
+export declare interface AiCodeReviewProvider {
+  /**
+   * If a suggestion provider is registered that implements this method, then
+   * Gerrit will offer a side panel for the user to have an AI Chat
+   * conversation. Each chat() call is one turn of such a conversation.
+   */
+  chat?(req: ChatRequest, listener: ChatResponseListener): void;
+}

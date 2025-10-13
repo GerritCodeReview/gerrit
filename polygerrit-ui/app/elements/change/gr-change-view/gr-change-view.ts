af027a59ed569d4f35779962511caf1da9eed8d0
paladox <thomasmulhall410@yahoo.com>
2025-10-13 10:38:39 +0100

    Revert "Implement a sidebar for the change-view"
    Revert submission 514801
    
    Reason for revert: Breaks mobile view.
    
    See [0] and [1].
    
    Reverted changes: /q/submissionid:514801
    
    [0] https://phabricator.wikimedia.org/F66747220
    [1] https://phabricator.wikimedia.org/F66747221
    
    This broke scrolling in the diff view.
    
    Release-Notes: skip
    Bug: Google b/451939028
    Change-Id: Ib09a6d960a5f9ce5f0adf98b71a6224a78d427da

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -16,7 +16,6 @@ import '../../shared/gr-change-status/gr-change-status';
 import '../../shared/gr-editable-content/gr-editable-content';
 import '../../shared/gr-formatted-text/gr-formatted-text';
 import '../../shared/gr-tooltip-content/gr-tooltip-content';
-import '../../shared/gr-content-with-sidebar/gr-content-with-sidebar';
 import '../gr-change-actions/gr-change-actions';
 import '../gr-change-summary/gr-change-summary';
 import '../gr-change-metadata/gr-change-metadata';
@@ -161,7 +160,6 @@ const ACCIDENTAL_STARRING_LIMIT_MS = 10 * 1000;
 const TRAILING_WHITESPACE_REGEX = /[ \t]+$/gm;
 
 const PREFIX = '#message-';
-
 @customElement('gr-change-view')
 export class GrChangeView extends LitElement {
   /**
@@ -351,9 +349,6 @@ export class GrChangeView extends LitElement {
   // visible for testing
   @state()
   activeTab: Tab | string = Tab.FILES;
-
-  @state()
-  private showSidebarChat = false;
 
   @property({type: Boolean})
   unresolvedOnly = true;
@@ -800,13 +795,6 @@ export class GrChangeView extends LitElement {
       sharedStyles,
       modalStyles,
       css`
-        :host {
-          --change-header-height: 38px;
-          --sidebar-top: calc(
-            var(--main-header-height) + var(--change-header-height)
-          );
-          --sidebar-bottom-overflow: var(--main-footer-height);
-        }
         .tabs {
           display: flex;
         }
@@ -825,7 +813,6 @@ export class GrChangeView extends LitElement {
           padding: var(--spacing-s) var(--spacing-l);
           position: sticky;
           top: var(--main-header-height);
-          height: var(--change-header-height);
           z-index: 110;
         }
         .header.active {
@@ -1135,18 +1122,12 @@ export class GrChangeView extends LitElement {
 
   private renderMainContent() {
     return html`
-      ${this.renderHeader()}
-      <gr-content-with-sidebar .hideSide=${!this.showSidebarChat}>
-        <div slot="main">
-          <div id="mainContent" class="container" ?hidden=${!!this.loading}>
-            ${this.renderChangeInfoSection()}
-            <h2 class="assistive-tech-only">Files and Comments tabs</h2>
-            ${this.renderTabHeaders()} ${this.renderTabContent()}
-            ${this.renderChangeLog()}
-          </div>
-        </div>
-        <div slot="side">${this.renderSidebar()}</div>
-      </gr-content-with-sidebar>
+      <div id="mainContent" class="container" ?hidden=${!!this.loading}>
+        ${this.renderHeader()} ${this.renderChangeInfoSection()}
+        <h2 class="assistive-tech-only">Files and Comments tabs</h2>
+        ${this.renderTabHeaders()} ${this.renderTabContent()}
+        ${this.renderChangeLog()}
+      </div>
       <gr-apply-fix-dialog id="applyFixDialog"></gr-apply-fix-dialog>
       <dialog id="downloadModal" tabindex="-1">
         <gr-download-dialog
@@ -1179,7 +1160,6 @@ export class GrChangeView extends LitElement {
   }
 
   private renderHeader() {
-    if (this.loading) return;
     return html`
       <div class=${this.computeHeaderClass()}>
         <h1 class="assistive-tech-only">
@@ -1188,11 +1168,6 @@ export class GrChangeView extends LitElement {
         ${this.renderHeaderTitle()} ${this.renderCommitActions()}
       </div>
     `;
-  }
-
-  private renderSidebar() {
-    if (!this.showSidebarChat) return;
-    return html` <div>Hello, I am a fancy sidebar for AI Chat.</div> `;
   }
 
   private renderChangeInfoSection() {

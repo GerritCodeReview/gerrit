7d9358f541112498d760340b519322c5d39a93d2
Paladox none <thomasmulhall410@yahoo.com>
2025-10-13 02:36:58 -0700

    Revert "Add a flag and a button for triggering AI Chat"
    Revert submission 517083
    
    Reason for revert: Broke mobile view
    
    See [0] and [1].
    
    Reverted changes: /q/submissionid:517083
    
    [0] https://phabricator.wikimedia.org/F66747220
    [1] https://phabricator.wikimedia.org/F66747221
    
    This broke scrolling in the diff view.
    
    Release-Notes: skip
    Bug: Google b/451939028
    Change-Id: Iff4d6aceed88321e55202efeaf07bd2f018c18c2

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -1190,10 +1190,6 @@ export class GrChangeView extends LitElement {
     `;
   }
 
-  private toggleChat() {
-    this.showSidebarChat = !this.showSidebarChat;
-  }
-
   private renderSidebar() {
     if (!this.showSidebarChat) return;
     return html` <div>Hello, I am a fancy sidebar for AI Chat.</div> `;
@@ -1319,7 +1315,6 @@ export class GrChangeView extends LitElement {
       <div class="commitActions">
         <gr-change-actions
           id="actions"
-          @ai-chat=${() => this.toggleChat()}
           @edit-tap=${() => this.handleEditTap()}
           @stop-edit-tap=${() => this.handleStopEditTap()}
           @download-tap=${() => this.handleOpenDownloadDialog()}

fb64ab3a9af2459d062baaf6d239b50fc6ec0d77
Edwin Kempin <ekempin@google.com>
2025-10-27 08:52:18 +0000

    Add a user kind field to the server REST API latency metric
    The new field will allow us to distinguish between service users and
    human users. For the latency metric this distinction is interesting
    because latency for human users is usually more critical than latency
    for bots.
    
    Also latency for bots can be generally higher due to more expensive
    requests (e.g. more complicated change queries, more change options that
    require lazy load etc.) and hence high bot traffic can make the overall
    latency numbers look worse than it is for human users.
    
    We consider to make some behaviour that has an impact on latency
    dependent on the user type, e.g. do change indexing synchronously only
    if the caller is a server user. If we do this we want to be able to
    distinguish between service users and human users when looking at the
    latency metrics.
    
    Service users are identified by membership in the 'Service Users' group
    (see ServiceUserClassifier). All users that are not classified as a
    service user are classified as a human user. This includes anonymous
    users that cannot be classified by the ServiceUserClassifier.
    
    Release-Notes: skip
    Bug: Google b/454856463
    Change-Id: I5d799d55489c3e20535e4a3b029ddb7c9f7cb502
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/httpd/restapi/RestApiServlet.java
+++ java/com/google/gerrit/httpd/restapi/RestApiServlet.java
@@ -101,6 +101,7 @@ import com.google.gerrit.server.InvalidDeadlineException;
 import com.google.gerrit.server.OptionUtil;
 import com.google.gerrit.server.RequestInfo;
 import com.google.gerrit.server.RequestListener;
+import com.google.gerrit.server.account.ServiceUserClassifier;
 import com.google.gerrit.server.audit.ExtendedHttpAuditEvent;
 import com.google.gerrit.server.cache.PerThreadCache;
 import com.google.gerrit.server.cancellation.RequestCancelledException;
@@ -248,6 +249,7 @@ public class RestApiServlet extends HttpServlet {
     final DeadlineChecker.Factory deadlineCheckerFactory;
     final CancellationMetrics cancellationMetrics;
     final AclInfoController aclInfoController;
+    final ServiceUserClassifier serviceUserClassifier;
     final Provider<TraceContext> requestTraceContext;
 
     @Inject
@@ -270,6 +272,7 @@ public class RestApiServlet extends HttpServlet {
         DeadlineChecker.Factory deadlineCheckerFactory,
         CancellationMetrics cancellationMetrics,
         AclInfoController aclInfoController,
+        ServiceUserClassifier serviceUserClassifier,
         @Named(REQUEST_TRACE_CONTEXT) Provider<TraceContext> requestTraceContext) {
       this.currentUser = currentUser;
       this.webSession = webSession;
@@ -290,6 +293,7 @@ public class RestApiServlet extends HttpServlet {
       this.deadlineCheckerFactory = deadlineCheckerFactory;
       this.cancellationMetrics = cancellationMetrics;
       this.aclInfoController = aclInfoController;
+      this.serviceUserClassifier = serviceUserClassifier;
       this.requestTraceContext = requestTraceContext;
     }
   }
@@ -765,6 +769,7 @@ public class RestApiServlet extends HttpServlet {
       globals.metrics.serverLatency.record(
           metric,
           currentUser.getAccessPath().name(),
+          globals.serviceUserClassifier.getUserKind(currentUser),
           System.nanoTime() - startNanos,
           TimeUnit.NANOSECONDS);
       globals.auditService.dispatch(

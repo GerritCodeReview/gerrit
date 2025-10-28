e8decd212331714575811af3a80cb3abad2806ec
Paladox none <thomasmulhall410@yahoo.com>
2025-10-28 15:33:56 -0700

    gr-change-view: Fix issue with subject going out of screen
    Release-Notes: gr-change-view: Fix issue with subject going out of screen
    Change-Id: Ic32a3e8c9227f1067519bb24a1209e615e56b926

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -1112,6 +1112,9 @@ export class GrChangeView extends LitElement {
             height: 90vh;
             width: initial;
           }
+          .headerSubject {
+            line-break: anywhere;
+          }
         }
         .patch-set-dropdown {
           margin: var(--spacing-m) 0 0 var(--spacing-m);

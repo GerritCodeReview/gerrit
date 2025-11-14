ac3af8ed51a1a759c7d5a2449ceeb80091c279c8
Adithya Chakilam <achakilam@nvidia.com>
2025-11-13 20:50:57 -0600

    WorkQueue: Prevent premature removal of tasks on cancellation
    Previously, interrupted tasks were removed from the WorkQueue
    immediately. This caused them to disappear from the show-queue
    output even though they were still running in the background and
    occupying an executor thread.
    
    This behavior was especially confusing when using server-side
    quotas via the `quota` plugin, as tasks appeared gone while still
    affecting resource usage.
    
    Ensure that a task is removed immediately from the executor only
    if its state never reached to `STARTED`. If it has already begun
    execution, we rely on the finally block in `run()` to remove it
    at the correct time.
    
    Release-Notes: show-queue now shows cancelled tasks until completion
    Change-Id: I9e9b129fb66a39de9bc280f8df623006daf72b32

--- java/com/google/gerrit/server/git/WorkQueue.java
+++ java/com/google/gerrit/server/git/WorkQueue.java
@@ -621,10 +621,7 @@ public class WorkQueue {
     }
 
     void remove(Task<?> task) {
-      boolean isRemoved = all.remove(task.getTaskId(), task);
-      if (isRemoved) {
-        cancelIfParked(task);
-      }
+      all.remove(task.getTaskId(), task);
     }
 
     void cancelIfParked(Task<?> task) {
@@ -859,12 +856,14 @@ public class WorkQueue {
         // as running and allow it to clean up. This ensures we do
         // not invoke cancel twice.
         //
-        if (runnable instanceof CancelableRunnable) {
-          if (runningState.compareAndSet(null, State.RUNNING)) {
+        if (runningState.compareAndSet(null, State.RUNNING)) {
+          if (runnable instanceof CancelableRunnable) {
             ((CancelableRunnable) runnable).cancel();
-          } else if (runnable instanceof CanceledWhileRunning) {
-            ((CanceledWhileRunning) runnable).setCanceledWhileRunning();
           }
+          executor.remove(this);
+          executor.purge();
+        } else if (runnable instanceof CanceledWhileRunning) {
+          ((CanceledWhileRunning) runnable).setCanceledWhileRunning();
         }
         if (runnable instanceof Future<?>) {
           // Creating new futures eventually passes through
@@ -875,8 +874,7 @@ public class WorkQueue {
           ((Future<?>) runnable).cancel(mayInterruptIfRunning);
         }
 
-        executor.remove(this);
-        executor.purge();
+        executor.cancelIfParked(this);
         return true;
       }
       return false;

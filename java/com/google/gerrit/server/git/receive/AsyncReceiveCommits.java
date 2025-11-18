1813090d923dc9a2c5bcce4a0236e7e59d14e85b
Antonio Barone <syntonyze@gmail.com>
2025-11-18 09:11:28 +0100

    Handle size-based quota exceptions explicitly
    Quota enforcerers have already been integrated into gerrit core.  When
    handling receive-packs, Gerrit asks for available bytes the repository
    can grow, before reaching a size quota, if any.
    
    The way this limit is enforced is through the JGit maxPackSizeLimit
    property, which will throw a TooLargePackException, should the packfile
    being uploaded exceeds the allowance.
    
    This error is exposed to the client as-is:
    
    ``` TooLargePackException: Pack exceeds the limit of 190,821 bytes,
    rejecting the pack
    ```
    
    For a committer reading the error, it is not at all intuitive to
    understand that this error is associated to the enforcing of a quota.
    
    To fix this we now explicitly handle the TooLargePackException and
    construct an informative QuotaException instead.
    
    ```
    Push rejected: it would exceed the available repository size quota of
    190,821 bytes.
    ```
    
    A follow up to this change will be to allow Quota Enforcerers (for
    example the quota plugin), to specify their own error message, so that
    each enforcerer can differentiate how to inform the user about the quota
    being exceeded and what they can do about it (i.e. get in touch with
    the administrator, open an issue, etc).
    
    Bug: Issue 459520484
    Change-Id: If45a1bc9b8a6a77f1c4ec1bf22cb5c74b8c7f5f9

--- java/com/google/gerrit/server/git/receive/AsyncReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/AsyncReceiveCommits.java
@@ -81,12 +81,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import org.eclipse.jgit.errors.TooLargePackException;
 import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.PreReceiveHook;
 import org.eclipse.jgit.transport.ReceiveCommand;
 import org.eclipse.jgit.transport.ReceiveCommand.Result;
 import org.eclipse.jgit.transport.ReceivePack;
+import org.eclipse.jgit.transport.UnpackErrorHandler;
 
 /**
  * Hook that delegates to {@link ReceiveCommits} in a worker thread.
@@ -359,7 +361,26 @@ public class AsyncReceiveCommits {
           REPOSITORY_SIZE_GROUP, projectName);
       throw new RuntimeException(e);
     }
-    availableTokens.availableTokens().ifPresent(receivePack::setMaxPackSizeLimit);
+    availableTokens
+        .availableTokens()
+        .ifPresent(
+            availBytes -> {
+              receivePack.setMaxPackSizeLimit(availBytes);
+              UnpackErrorHandler defaultErrorHandler = receivePack.getUnpackErrorHandler();
+              receivePack.setUnpackErrorHandler(
+                  t -> {
+                    Throwable unpackException = t;
+                    if (t instanceof TooLargePackException) {
+                      unpackException =
+                          new QuotaException(
+                              String.format(
+                                  "Push rejected: it would exceed the available repository size"
+                                      + " quota of %d bytes.",
+                                  availBytes));
+                    }
+                    defaultErrorHandler.handleUnpackException(unpackException);
+                  });
+            });
   }
 
   /** Determine if the user can upload commits. */

f173c9f8ac3b161990c552f40b361a1b2d0fdfba
Adithya Chakilam <achakilam@nvidia.com>
2025-12-09 17:18:49 -0600

    Add exception handling for WorkQueue TaskListener callbacks
    TaskListener callbacks (onStart and onStop) can throw exceptions that
    prevent tasks from executing properly. Wrap these callbacks in try-catch
    blocks to log exceptions without disrupting task execution.
    
    This ensures that a misbehaving TaskListener implementation cannot
    prevent other listeners from being notified or tasks from running.
    
    Added tests to verify:
    - Tasks execute normally even when onStart throws an exception
    - Other TaskListeners still receive callbacks when one throws exception
      on onStop
    
    Release-Notes: skip
    Change-Id: Id023ab6d0a86ef6d8f4dbe8dd163e299be2362f1

--- java/com/google/gerrit/server/git/WorkQueue.java
+++ java/com/google/gerrit/server/git/WorkQueue.java
@@ -27,6 +27,7 @@ import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.lifecycle.LifecycleModule;
 import com.google.gerrit.metrics.Description;
 import com.google.gerrit.metrics.MetricMaker;
+import com.google.gerrit.server.cancellation.RequestCancelledException;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.config.ScheduleConfig.Schedule;
 import com.google.gerrit.server.logging.LoggingContext;
@@ -661,11 +662,29 @@ public class WorkQueue {
     }
 
     public void onStart(Task<?> task) {
-      listeners.runEach(extension -> extension.get().onStart(task));
+      listeners.runEach(
+          extension -> {
+            TaskListener listener = extension.getProvider().get();
+            try {
+              listener.onStart(task);
+            } catch (RuntimeException e) {
+              logger.atSevere().withCause(e).log(
+                  "Exception invoking onStart for task [%s] on listener [%s]", task, listener);
+            }
+          });
     }
 
     public void onStop(Task<?> task) {
-      listeners.runEach(extension -> extension.get().onStop(task));
+      listeners.runEach(
+          extension -> {
+            TaskListener listener = extension.getProvider().get();
+            try {
+              listener.onStop(task);
+            } catch (RuntimeException e) {
+              logger.atSevere().withCause(e).log(
+                  "Exception invoking onStop for task [%s] on listener [%s]", task, listener);
+            }
+          });
       updateParked();
     }
 

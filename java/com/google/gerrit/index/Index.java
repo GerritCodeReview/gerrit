0bb69f1e0655be56c326af6007fe46cf71464771
Fabio Ponciroli <ponch78@gmail.com>
2025-12-16 21:15:41 +0100

    Introduce flushAndCommit() in Index interface
    Introduce a new Index.flushAndCommit() method to allow implementations
    to flush and commit pending index updates and sync referenced index
    files. Provide a default implementation throwing a NotImplementedException,
    so that the upper layers can be aware that the indexing backend
    doesn't support explicit flush.
    
    Bug: Issue 454827187
    Release-Notes: Introduce flushAndCommit() in Index interface
    Change-Id: I0b1b9117af83cf1a5ade5daad217a9567110f6f3

--- java/com/google/gerrit/index/Index.java
+++ java/com/google/gerrit/index/Index.java
@@ -16,6 +16,7 @@ package com.google.gerrit.index;
 
 import com.google.common.collect.ImmutableList;
 import com.google.gerrit.exceptions.StorageException;
+import com.google.gerrit.extensions.restapi.NotImplementedException;
 import com.google.gerrit.index.query.DataSource;
 import com.google.gerrit.index.query.FieldBundle;
 import com.google.gerrit.index.query.IndexPredicate;
@@ -183,5 +184,16 @@ public interface Index<K, V> {
    */
   default boolean snapshot(String id) throws IOException {
     return false;
+  }
+
+  /**
+   * Optionally flushes and commits pending changes to the index and syncs referenced index files.
+   * Implementations that buffer state should override this to flush their state; the default
+   * implementation throws a {@link NotImplementedException} exception.
+   *
+   * @throws IOException if flushing to disk fails.
+   */
+  default void flushAndCommit() throws IOException {
+    throw new NotImplementedException();
   }
 }

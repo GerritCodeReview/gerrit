32f5a9415cdd9ba1bec4754d4f3ac819a4c5834c
David Ostrovsky <david@ostrovsky.org>
2026-01-05 16:27:37 +0000

    Suppress no-unnecessary-type-assertion for intentional assertion
    The non-null assertion documents that the data-index attribute must be
    present, even though the receiver accepts null.
    
    Release-Notes: skip
    Change-Id: I93b9e1dad2092c2ae92dbc8415d4cd63b6a3123f

--- polygerrit-ui/app/elements/settings/gr-ssh-editor/gr-ssh-editor.ts
+++ polygerrit-ui/app/elements/settings/gr-ssh-editor/gr-ssh-editor.ts
@@ -235,6 +235,7 @@ export class GrSshEditor extends LitElement {
 
   private showKey(e: Event) {
     const el = e.target as GrButton;
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     const index = Number(el.getAttribute('data-index')!);
     this.keyToView = this.keys[index];
     this.viewKeyModal.showModal();
@@ -242,6 +243,7 @@ export class GrSshEditor extends LitElement {
 
   private handleDeleteKey(e: Event) {
     const el = e.target as GrButton;
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     const index = Number(el.getAttribute('data-index')!);
     this.keysToRemove.push(this.keys[index]);
     this.keys.splice(index, 1);

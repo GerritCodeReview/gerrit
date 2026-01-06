3946b8afaa0f9220c0aee51888612dd5b3339bcd
Daniele Sassoli <danielesassoli@gmail.com>
2026-01-06 16:33:09 +0000

    Allow caches to register non-primitive key types
    This enables caches to register their non-primitive key types(i.e.
    GeneralProjectName) so that they can be correctly serialized and
    deserialized.
    
    This will enable, for example, multi-site to wrap encoding of, for
    example, a GeneralProjectName with its string value and its type. When
    deserializing, we can then figure out the class name from the type via a
    reverse look-up of the table and correctly decode the element.
    
    If we simply encode/decode as a string, when we then go to evict
    elements from the cache, if a cache as GeneralProjectName as key, but
    this has been deserialized as a string, it won't match and the element
    won't be deleted.
    
    Release-Notes: Allow plugins to register their own key types
    Bug: Issue 465491615
    Change-Id: Ia5c4661bd69331dfff225ee0285d998a470a8d54

---
+++ java/com/google/gerrit/server/cache/CacheKeyType.java
@@ -0,0 +1,50 @@
+package com.google.gerrit.server.cache;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.AccountGroup;
+import com.google.gerrit.entities.GeneralProjectName;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** Class for registering cache key types */
+public class CacheKeyType {
+  private static final Map<String, Class<?>> keyTypesByString = new HashMap<>();
+
+  static {
+    registerKeyType("account_id", Account.Id.class);
+    registerKeyType("account_group_id", AccountGroup.Id.class);
+    registerKeyType("project_name", GeneralProjectName.class);
+  }
+  /**
+   * Register a key type and associated class.
+   *
+   * @param keyType The key type to register.
+   * @param keyClass The key class to register.
+   */
+  public static void registerKeyType(String keyType, Class<?> keyClass) {
+    keyTypesByString.put(keyType, keyClass);
+  }
+
+  /**
+   * Get the class for a key type.
+   *
+   * @param type The type.
+   * @return The key class, or null if no class is registered with the given type
+   */
+  public static Class<?> getKeyClass(String type) {
+    return keyTypesByString.get(type);
+  }
+
+  /**
+   * Get a copy of all currently registered keys.
+   *
+   * <p>The key is the one given to the keyType parameter of the {@link #registerKeyType} method.
+   *
+   * @return ImmutableMap of key types, key classes.
+   */
+  public static ImmutableMap<String, Class<?>> getRegisteredKeys() {
+    return ImmutableMap.copyOf(keyTypesByString);
+  }
+}

ba141b4322e51af1169546b9a0d37ab9d448bb79
Daniele Sassoli <danielesassoli@gmail.com>
2026-01-06 16:33:09 +0000

    Expose cache's definitions to plugins
    By dynamically binding cache's definitions we allow plugins to access
    the currently registered cache's definitions.
    
    Release-Notes: Expose cache's definitions to plugins
    Bug: Issue 476330479
    Change-Id: Ie4e8e5150f2bfcaf016e7675592d98f200ec7dbb

---
+++ javatests/com/google/gerrit/server/cache/CacheFactoryIT.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.cache;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.inject.Inject;
+import org.junit.Test;
+
+public class CacheFactoryIT extends AbstractDaemonTest {
+
+  @Inject
+  private DynamicMap<CacheDef<?, ?>> cacheDefs;
+
+  @Override
+  public com.google.inject.Module createModule() {
+    return new TestModule();
+  }
+
+  @Test
+  public void newCacheDefIsAvailableInDynamicMap() {
+    assertThat(cacheDefs).isNotNull();
+
+    CacheDef<?, ?> def = null;
+    for (com.google.gerrit.extensions.registration.Extension<CacheDef<?, ?>> e : cacheDefs) {
+      if (e.getExportName().equals("dummy-cache")) {
+        def = e.getProvider().get();
+        break;
+      }
+    }
+    assertThat(def).isNotNull();
+    assertThat(def.keyType().getRawType()).isEqualTo(String.class);
+    assertThat(def.valueType().getRawType()).isEqualTo(String.class);
+  }
+
+  public static class TestModule extends CacheModule {
+
+    @Override
+    protected void configure() {
+      cache("dummy-cache", String.class, String.class);
+    }
+  }
+}

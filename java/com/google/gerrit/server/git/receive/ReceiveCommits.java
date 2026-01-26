961dc4b05334465ea62ee6588ee80a628360054c
Sven Selberg <svense@axis.com>
2026-01-26 13:19:37 +0100

    ReceiveCommits: fire hashtags-changed event when created on push
    For some reason hashtags-changed triggering was never enabled when the
    hashtag was created from a push option, and when the SetHashtagsOp was
    introduced the event-firing was disabled:
    
    * bce0e922c4 Set hashtags using a BatchUpdate.Op
    
    It doesn't make sense for services interested in hashtags-changed should
    need to listen to two events.
    
    Change-Id: I648a761b12fe862cf45a1960504d13d25861a23f

--- java/com/google/gerrit/server/git/receive/ReceiveCommits.java
+++ java/com/google/gerrit/server/git/receive/ReceiveCommits.java
@@ -3077,7 +3077,7 @@ class ReceiveCommits {
                 changeId,
                 hashtagsFactory
                     .create(new HashtagsInput(magicBranch.hashtags))
-                    .setFireEvent(false));
+                    .setFireEvent(true));
           }
           if (!Strings.isNullOrEmpty(magicBranch.topic)) {
             bu.addOp(changeId, setTopicFactory.create(magicBranch.topic));

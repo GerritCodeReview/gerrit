f3470ad4b32904ff73323740e81204c29c677c97
Nitzan Gur-Furman <nitzan@google.com>
2026-01-29 06:03:15 -0800

    Add support for adding reviewers on behalf of another user
    This change introduces an onBehalfOf field in ReviewerInput. When this
    field is set, the reviewer is added as if the specified user performed
    the action. The real user performing the action is recorded in the
    change message. The impersonated user must have read access to the
    change.
    
    Note that updating the reviewers does not generate a change message. Instead, a
    `ReviewerUpdateInfo` object is saved. Later, in the FE, the message and the
    reviewer-update lists are combined.
    
    Google-Bug-Id: b/446827309
    Release-Notes: skip
    Change-Id: I8890d5e96a4239169646998f01938f8838e5631b

--- java/com/google/gerrit/extensions/api/changes/ReviewerInput.java
+++ java/com/google/gerrit/extensions/api/changes/ReviewerInput.java
@@ -26,6 +26,7 @@ public class ReviewerInput {
   public ReviewerState state;
   public NotifyHandling notify;
   public Map<RecipientType, NotifyInfo> notifyDetails;
+  public String onBehalfOf;
 
   public boolean confirmed() {
     return (confirmed != null) ? confirmed : false;

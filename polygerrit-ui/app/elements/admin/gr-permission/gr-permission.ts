bf345b3f71f6516c6b2387bf24b1cc339227760f
Milutin Kristofic <milutin@google.com>
2026-02-12 10:56:45 +0100

    Add documentation links to access UI permissions
    Adds a help icon next to each permission in the Access UI that links
    directly to the corresponding section in the access control
    documentation. This improves discoverability of permission details.
    
    Screenshot: https://imgur.com/a/98lkqIg
    Google-Bug-Id: b/483785597
    Release-Notes: skip
    Change-Id: I73ee3f1431f1367738eda21029f540f2d75731b0

--- polygerrit-ui/app/elements/admin/gr-permission/gr-permission.ts
+++ polygerrit-ui/app/elements/admin/gr-permission/gr-permission.ts
@@ -10,10 +10,15 @@ import '../gr-rule-editor/gr-rule-editor';
 import {css, html, LitElement, PropertyValues} from 'lit';
 import {
   AccessPermissionId,
+  getAccessDocsAnchor,
   PermissionArray,
   PermissionArrayItem,
   toSortedPermissionsArray,
 } from '../../../utils/access-util';
+import {resolve} from '../../../models/dependency';
+import {configModelToken} from '../../../models/config/config-model';
+import {subscribe} from '../../lit/subscription-controller';
+import {getDocUrl} from '../../../utils/url-util';
 import {customElement, property, query, state} from 'lit/decorators.js';
 import {
   GitRef,
@@ -112,15 +117,25 @@ export class GrPermission extends LitElement {
   @state()
   originalExclusiveValue?: boolean;
 
+  @state()
+  private docsBaseUrl = '';
+
   @query('#groupAutocomplete')
   private groupAutocomplete!: GrAutocomplete;
 
   private readonly restApiService = getAppContext().restApiService;
 
+  private readonly getConfigModel = resolve(this, configModelToken);
+
   constructor() {
     super();
     this.query = () => this.getGroupSuggestions();
     this.addEventListener('access-saved', () => this.handleAccessSaved());
+    subscribe(
+      this,
+      () => this.getConfigModel().docsBaseUrl$,
+      docsBaseUrl => (this.docsBaseUrl = docsBaseUrl)
+    );
   }
 
   override connectedCallback() {
@@ -172,6 +187,12 @@ export class GrPermission extends LitElement {
         .title {
           margin-bottom: var(--spacing-s);
         }
+        .title a {
+          display: inline-block;
+          vertical-align: middle;
+          margin-left: var(--spacing-s);
+          text-decoration: none;
+        }
         #addRule,
         #removeBtn {
           display: none;
@@ -220,7 +241,21 @@ export class GrPermission extends LitElement {
       >
         <div id="mainContainer">
           <div class="header">
-            <span class="title">${this.name}</span>
+            <span class="title">
+              ${this.name}
+              ${when(
+                this.computeHelpUrl(),
+                () => html`
+                  <a
+                    href=${this.computeHelpUrl()!}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <gr-icon icon="help" title="Help"></gr-icon>
+                  </a>
+                `
+              )}
+            </span>
             <div class="right">
               ${when(
                 !this.permissionIsOwnerOrGlobal(
@@ -301,6 +336,17 @@ export class GrPermission extends LitElement {
     }
     this.originalExclusiveValue = !!this.permission.value.exclusive;
     this.requestUpdate();
+  }
+
+  private computeHelpUrl(): string | undefined {
+    if (!this.permission || !this.permission.id || !this.docsBaseUrl) {
+      return undefined;
+    }
+    const anchor = getAccessDocsAnchor(this.permission.id as string);
+    if (!anchor) {
+      return undefined;
+    }
+    return getDocUrl(this.docsBaseUrl, `access-control.html#${anchor}`);
   }
 
   private handleAccessSaved() {

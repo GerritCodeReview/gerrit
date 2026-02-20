ba147ea83b03fbb9f7c1f955c3acdbd742efb647
Dhruv Srivastava <dhruvsri@google.com>
2026-02-20 14:45:20 +0100

    Check submit permission on individual changes in force push
    When force pushing a topic, explicitly check that the user has the
    permission to submit individual changes.
    
    Bug: 486131256
    Release-Notes: skip
    Change-Id: Ib4b8168ba706ac42994199f914db2764a04e4a72

--- java/com/google/gerrit/server/submit/MergeOp.java
+++ java/com/google/gerrit/server/submit/MergeOp.java
@@ -739,14 +739,20 @@ public class MergeOp implements AutoCloseable {
         // Reload ChangeSet so that we don't rely on (potentially) stale index data for merging
         ChangeSet noteDbChangeSet = reloadChanges(indexBackedChangeSet);
 
-        // At this point, any change that isn't new can be filtered out since they were only here
-        // in the first place due to stale index.
         List<ChangeData> filteredChanges = new ArrayList<>();
         for (ChangeData changeData : noteDbChangeSet.changes()) {
+          // At this point, any change that isn't new can be filtered out since they were only here
+          // in the first place due to stale index.
           if (!changeData.change().getStatus().equals(Status.NEW)) {
             logger.atFine().log(
                 "Change %s has status %s due to stale index, so it is skipped during submit",
                 changeData.getId(), changeData.change().getStatus().name());
+            continue;
+          }
+          if (!permissionBackend.user(caller).change(changeData).test(ChangePermission.SUBMIT)) {
+            logger.atFine().log(
+                "Change %s is skipped during submit because user %s lacks submit permission",
+                changeData.getId(), caller.getLoggableName());
             continue;
           }
           filteredChanges.add(changeData);

92a58fe71caa259af1a4f203c3b07c2589710742
Daniele Sassoli <danielesassoli@gmail.com>
2026-02-26 22:41:48 +0000

    Display JJ change-id in the UI
    When utilising JJ change-ids, the change ID is no longer visible in the
    UI, as its not part of the commit message.
    
    Add an extra section, below the commit message, showing the JJ id.
    
    Release-Notes: Display JJ change-id in the UI
    Change-Id: I08cb7c20d43ece26e5e95fc2442da33ae5903a67

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view.ts
@@ -948,6 +948,11 @@ export class GrChangeView extends LitElement {
         .commitMessage gr-formatted-text {
           overflow-wrap: anywhere;
         }
+        .jjChangeId {
+          color: var(--deemphasized-text-color);
+          font-family: var(--font-family);
+          margin-top: var(--spacing-l);
+        }
         #commitMessageEditor {
           /* Account for border and padding and rounding errors. */
           min-width: calc(72ch + 2px + 2 * var(--spacing-m) + 0.4px);
@@ -1406,6 +1411,7 @@ export class GrChangeView extends LitElement {
                   .markdown=${false}
                 ></gr-formatted-text>
               </gr-editable-content>
+              ${this.renderJujutsuChangeId()}
             </div>
             <h3 class="assistive-tech-only">Comments and Checks Summary</h3>
             <gr-change-summary></gr-change-summary>
@@ -1423,6 +1429,23 @@ export class GrChangeView extends LitElement {
         </div>
       </div>
     </div>`;
+  }
+
+  private renderJujutsuChangeId() {
+    const changeId = this.change?.change_id;
+    if (
+      !changeId ||
+      !/^[a-z]{32}$/.test(changeId) ||
+      this.editingCommitMessage
+    ) {
+      return nothing;
+    }
+    return html`
+      <div class="jjChangeId">
+        <hr />
+        JJ change-id: <span>${changeId}</span>
+      </div>
+    `;
   }
 
   private renderTabHeaders() {

20f01b2791adaea5b62c1fc2b523f572d68a1e12
John Blacksmith <john.blacksmith000@gmail.com>
2026-03-08 16:11:56 -0700

    Fix change reindex for imported projects
    When reindexing projects that have been imported from other Gerrit
    servers document reuse fails because the staleness checker is unable to
    find the previously indexed change.
    When imported projects are large and the document reuse flag is used
    this creates a noticable increase in the reindex time, especially during
    offline reindexing.
    
    The problem appears to be that the ChangeIndexer is passing Id when it
    needs to pass virtualId to resolve the change to an imported project.
    
    This patch updates the ChangeIndexer to use virtualId instead of Id when
    performing the staleness check.
    
    One new problem found with this change is in the verbose log output
    during a reindex, when a stale change from an imported project is being
    indexed the virtualId of the change is used in the log message instead
    of Id.  I have not attempted to fix this as part of this patch and
    consider it mostly a harmless side-effect as it only impacts the verbose
    log message.
    
    Bug: Issue 490877743
    Change-Id: Ic1ebc4a1d6e1266620fdd39f87641605fc56128c

--- java/com/google/gerrit/server/index/change/ChangeIndexer.java
+++ java/com/google/gerrit/server/index/change/ChangeIndexer.java
@@ -485,26 +485,27 @@ public class ChangeIndexer {
   /**
    * Synchronously check if a change is stale, and reindex if it is.
    *
-   * @param cd the change data to be checked for staleness.
-   * @return true if the change was stale, false if it was up-to-date
-   */
-  public boolean reindexIfStale(ChangeData cd) {
-    return reindexIfStale(cd.project(), cd.getId());
-  }
-
-  /**
-   * Synchronously check if a change is stale, and reindex if it is.
-   *
    * @param project the project to which the change belongs.
    * @param id ID of the change to index.
    * @return true if the change was stale, false if it was up-to-date
    */
   public boolean reindexIfStale(Project.NameKey project, Change.Id id) {
+    return reindexIfStale(changeDataFactory.create(project, id));
+  }
+
+  /**
+   * Synchronously check if a change is stale, and reindex if it is.
+   *
+   * @param cd the change data to be checked for staleness.
+   * @return true if the change was stale, false if it was up-to-date
+   */
+  public boolean reindexIfStale(ChangeData cd) {
+    final Change.Id cdUniqueId = cd.virtualId();
     try {
-      StalenessCheckResult stalenessCheckResult = stalenessChecker.check(id);
+      StalenessCheckResult stalenessCheckResult = stalenessChecker.check(cdUniqueId);
       if (stalenessCheckResult.isStale()) {
         logger.atInfo().log("Reindexing stale document %s", stalenessCheckResult);
-        indexImpl(changeDataFactory.create(project, id));
+        indexImpl(cd);
         return true;
       }
     } catch (Exception e) {
@@ -512,7 +513,8 @@ public class ChangeIndexer {
         throw e;
       }
       logger.atFine().log(
-          "Change %s belongs to deleted project %s, aborting reindexing the change.", id, project);
+          "Change %s belongs to deleted project %s, aborting reindexing the change.",
+          cd.getId(), cd.project());
     }
     return false;
   }

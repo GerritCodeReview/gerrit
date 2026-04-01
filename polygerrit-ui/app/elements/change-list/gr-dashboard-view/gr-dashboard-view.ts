ecbaf988a2fea5ce417c755fbaf3fd9811f5ee5a
Dhruv Srivastava <dhruvsri@google.com>
2026-04-01 16:04:51 +0200

    Remove automatic reloading of dashboard
    Internally we are noticing a performance regression in dashboard
    loading.
    
    We can bring this back if required but it should be configurable.
    
    Release-Notes: skip
    Bug: Google b/496505982
    Change-Id: I2c4da3da1ac9a73d50d481e1240934a0242a4cd1

--- polygerrit-ui/app/elements/change-list/gr-dashboard-view/gr-dashboard-view.ts
+++ polygerrit-ui/app/elements/change-list/gr-dashboard-view/gr-dashboard-view.ts
@@ -35,7 +35,6 @@ import {
   firePageError,
   fireTitleChange,
 } from '../../../utils/event-util';
-import {RELOAD_DASHBOARD_INTERVAL_MS} from '../../../constants/constants';
 import {ChangeListSection} from '../gr-change-list/gr-change-list';
 import {a11yStyles} from '../../../styles/gr-a11y-styles';
 import {sharedStyles} from '../../../styles/shared-styles';
@@ -153,31 +152,11 @@ export class GrDashboardView extends LitElement {
     this.shortcuts.addAbstract(Shortcut.UP_TO_DASHBOARD, () => this.reload());
   }
 
-  private readonly visibilityChangeListener = () => {
-    if (document.visibilityState === 'visible') {
-      if (
-        Date.now() - this.lastVisibleTimestampMs >
-        RELOAD_DASHBOARD_INTERVAL_MS
-      )
-        this.reload(true);
-    } else {
-      this.lastVisibleTimestampMs = Date.now();
-    }
-  };
-
   override connectedCallback() {
     super.connectedCallback();
-    document.addEventListener(
-      'visibilitychange',
-      this.visibilityChangeListener
-    );
   }
 
   override disconnectedCallback() {
-    document.removeEventListener(
-      'visibilitychange',
-      this.visibilityChangeListener
-    );
     super.disconnectedCallback();
   }
 

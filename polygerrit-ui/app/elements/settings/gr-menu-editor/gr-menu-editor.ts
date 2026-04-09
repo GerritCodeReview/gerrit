96a5c95f0cc95f73b59d52503557169a8791c028
Paladox none <thomasmulhall410@yahoo.com>
2026-04-09 05:03:26 -0700

    gr-menu-editor: Allow changing url target after saving
    There's no reason to disable this after saving. This will improve
    the user experience. So that they no longer have to delete a url and
    re-add it with the target either checked or unchecked.
    
    We also fix the UX on mobile to make it look a little nicer.
    
    Release-Notes: gr-menu-editor: Allow changing url target after saving
    Change-Id: Ie882434bb215d13292947a7b87d30fe4a492253e

--- polygerrit-ui/app/elements/settings/gr-menu-editor/gr-menu-editor.ts
+++ polygerrit-ui/app/elements/settings/gr-menu-editor/gr-menu-editor.ts
@@ -71,6 +71,10 @@ export class GrMenuEditor extends LitElement {
         tbody tr:last-of-type td .moveDownButton {
           display: none;
         }
+        td.nameCell {
+          min-width: 11em;
+          width: auto;
+        }
         td.urlCell {
           overflow-wrap: anywhere;
         }
@@ -79,6 +83,14 @@ export class GrMenuEditor extends LitElement {
         }
         th {
           white-space: nowrap;
+        }
+        tfoot th:nth-child(1),
+        tfoot th:nth-child(2),
+        tfoot th:nth-child(3) {
+          padding-right: 1em;
+        }
+        table {
+          margin-bottom: 8px;
         }
       `,
     ];
@@ -128,12 +140,12 @@ export class GrMenuEditor extends LitElement {
   private renderMenuItemRow(item: TopMenuItemInfo, index: number) {
     return html`
       <tr>
-        <td>${item.name}</td>
+        <td class="nameCell">${item.name}</td>
         <td class="urlCell">${item.url}</td>
         <td>
           <md-checkbox
-            disabled
             ?checked=${item.target === '_blank'}
+            @change=${(e: Event) => this.handleCheckboxChange(e, index)}
           ></md-checkbox>
         </td>
         <td class="buttonColumn">
@@ -201,7 +213,6 @@ export class GrMenuEditor extends LitElement {
         </th>
         <th>
           <md-checkbox
-            id="lineWrappingInput"
             ?checked=${this.newTarget}
             @change=${() => (this.newTarget = !this.newTarget)}
           ></md-checkbox>
@@ -253,6 +264,7 @@ export class GrMenuEditor extends LitElement {
     });
     this.newName = '';
     this.newUrl = '';
+    this.newTarget = false;
     this.requestUpdate('menuItems');
   }
 
@@ -261,6 +273,17 @@ export class GrMenuEditor extends LitElement {
       e.stopPropagation();
       this.handleAddButton();
     }
+  }
+
+  private handleCheckboxChange(e: Event, index: number) {
+    const checkbox = e.target as HTMLInputElement;
+
+    this.menuItems[index] = {
+      ...this.menuItems[index],
+      target: checkbox.checked ? '_blank' : undefined,
+    };
+
+    this.requestUpdate('menuItems');
   }
 }
 

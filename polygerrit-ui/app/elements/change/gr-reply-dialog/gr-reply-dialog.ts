f549759da9af249905da8853ab04be9afc15c2f0
Dhruv Srivastava <dhruvsri@google.com>
2026-04-10 08:18:12 +0200

    Allow sending reply if Autosubmit checkbox is clicked
    Release-Notes: skip
    Bug: Google b/500845669
    Change-Id: If11057345795e0c7edfdbda3dd3aecf7b0837619

--- polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.ts
+++ polygerrit-ui/app/elements/change/gr-reply-dialog/gr-reply-dialog.ts
@@ -2251,6 +2251,13 @@ export class GrReplyDialog extends LitElement {
     if (this.commentEditing || this.disabled) {
       return true;
     }
+    // If the user is creating an autosubmit flow then allow sending reply
+    if (
+      this.autosubmitChecked &&
+      !this.getFlowsModel().hasAutosubmitFlowAlready()
+    ) {
+      return false;
+    }
     if (this.canBeStarted === true) {
       return false;
     }

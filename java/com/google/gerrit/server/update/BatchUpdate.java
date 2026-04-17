57d78b11f50be866d3f776763d8756d4e278b423
Dhruv Srivastava <dhruvsri@google.com>
2026-04-17 06:25:14 -0700

    Add logging to indexAsync
    We want to confirm if indexing is happening async or not.
    
    Based on the config it's set to true so it's a sanity check.
    
    Release-Notes: skip
    Bug: Google b/503673108
    Change-Id: I35261ef7245895a3cfeeddb79e1fe1e016dba42f

--- java/com/google/gerrit/server/update/BatchUpdate.java
+++ java/com/google/gerrit/server/update/BatchUpdate.java
@@ -565,13 +565,19 @@ public class BatchUpdate implements AutoCloseable {
   @UsedAt(GOOGLE)
   private boolean indexAsync() {
     if (!gerritConfig.getBoolean("index", "indexChangesAsync", false)) {
+      logger.atFine().log(
+          "Triggering indexing synchronously because indexChangesAsync config is false");
       return false;
     }
 
     if (user.getAccessPath().equals(AccessPath.WEB_BROWSER)) {
+      logger.atFine().log("Triggering indexing asynchronously");
       return true;
     }
 
+    logger.atFine().log(
+        "Triggering indexing asynchronously: " + user.isIdentifiedUser()
+            && !serviceUserClassifier.isServiceUser(user.getAccountId()));
     return user.isIdentifiedUser() && !serviceUserClassifier.isServiceUser(user.getAccountId());
   }
 

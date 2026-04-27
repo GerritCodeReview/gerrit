c0d8a7c53eaf5b720ccf13474122e8cea2c3f65d
Daniele Sassoli <danielesassoli@gmail.com>
2026-04-27 22:46:51 +0100

    Test that change-index deletion events are fired without project name
    This is done in preparation for future work, where the project name will
    be added to the event.
    
    Release-Notes: skip
    Change-Id: Ic69ee6935633bab175190d736c9c402d392a6fbf

--- javatests/com/google/gerrit/acceptance/api/change/ChangeIT.java
+++ javatests/com/google/gerrit/acceptance/api/change/ChangeIT.java
@@ -1220,8 +1220,8 @@ public class ChangeIT extends AbstractDaemonTest {
 
   @Test
   public void deleteAllForProjectNotifiesListeners() {
-    TestDeleteAllForProjectListener deleteAllForProjectsListener =
-        new TestDeleteAllForProjectListener();
+    TestDeleteForProjectListener deleteAllForProjectsListener =
+        new TestDeleteForProjectListener();
 
     try (Registration ignore =
         extensionRegistry.newRegistration().add(deleteAllForProjectsListener)) {
@@ -1229,7 +1229,22 @@ public class ChangeIT extends AbstractDaemonTest {
       indexer.deleteAllForProject(project);
     }
 
-    assertThat(deleteAllForProjectsListener.getFiredCount()).isEqualTo(1);
+    assertThat(deleteAllForProjectsListener.getAllChangesPerProjectDeletedFiredCount()).isEqualTo(1);
+  }
+
+  @Test
+  public void deleteChangeFromIndexNotifiesListenersWithoutProjectName() {
+    TestChange change = changeOperations.newChange().createAndGet();
+    TestDeleteForProjectListener deleteAllForProjectsListener =
+        new TestDeleteForProjectListener();
+
+    try (Registration ignore =
+             extensionRegistry.newRegistration().add(deleteAllForProjectsListener)) {
+
+      indexer.delete(change.numericChangeId());
+    }
+
+    assertThat(deleteAllForProjectsListener.getSingleChangeDeletedFiredCount()).isEqualTo(1);
   }
 
   @Test
@@ -5417,23 +5432,29 @@ public class ChangeIT extends AbstractDaemonTest {
     }
   }
 
-  public static class TestDeleteAllForProjectListener implements ChangeIndexedListener {
-    private final AtomicInteger firedCount = new AtomicInteger(0);
+  public static class TestDeleteForProjectListener implements ChangeIndexedListener {
+    private final AtomicInteger allChangesPerProjectDeletedFiredCount = new AtomicInteger(0);
+    private final AtomicInteger singleChangeDeletedFiredCount = new AtomicInteger(0);
+    private String projectName = "no-project-set";
 
     @Override
     public void onChangeIndexed(String projectName, int id) {}
 
     @Override
-    public void onChangeDeleted(int id) {}
+    public void onChangeDeleted(int id) {
+      singleChangeDeletedFiredCount.incrementAndGet();
+    }
 
     @Override
     public void onAllChangesDeletedForProject(String projectName) {
-      firedCount.incrementAndGet();
+      allChangesPerProjectDeletedFiredCount.incrementAndGet();
     }
 
-    public int getFiredCount() {
-      return firedCount.get();
+    public int getAllChangesPerProjectDeletedFiredCount() {
+      return allChangesPerProjectDeletedFiredCount.get();
     }
+    public int getSingleChangeDeletedFiredCount() {return singleChangeDeletedFiredCount.get();}
+    public String getProjectName(){return projectName;}
   }
 
   private void voteLabel(String changeId, String labelName, int score) throws RestApiException {

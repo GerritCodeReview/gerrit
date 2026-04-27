049981fa7878eed2143dfdc4ec107fb32be81228
Daniele Sassoli <danielesassoli@gmail.com>
2026-04-27 16:56:37 +0100

    Include projectName in ChangeIndex delete message
    When this was first introduced with I87de17de2, change numbers where
    unique across projects, so propagating only the change number was sufficient
    for consumers to uniquely identify a change. With I42bac21a, change
    numbers are no longer unique per installation, but rather per project.
    Passing only the change number would allow a situation where a consumer
    tries to delete a change with the same number but for a different
    project than the intended one. If no projectName is present we fallback
    to the current behaviour.
    
    Include projectName in ChangeIndex message to allow consumers to
    uniquely identify a change.
    
    Change-Id: I304c575d1f14daf4162e8b1735499e2a3923a721

--- java/com/google/gerrit/server/index/change/ChangeIndexer.java
+++ java/com/google/gerrit/server/index/change/ChangeIndexer.java
@@ -376,9 +376,9 @@ public class ChangeIndexer {
     }
   }
 
-  private void fireChangeDeletedFromIndexEvent(int id) {
+  private void fireChangeDeletedFromIndexEvent(int id, String projectName) {
     if (notifyListeners) {
-      indexedListeners.runEach(l -> l.onChangeDeleted(id));
+      indexedListeners.runEach(l -> l.onChangeDeleted(id, projectName));
     }
   }
 
@@ -692,7 +692,8 @@ public class ChangeIndexer {
               e);
         }
       }
-      fireChangeDeletedFromIndexEvent(id.get());
+      project.ifPresentOrElse(p ->
+          fireChangeDeletedFromIndexEvent(id.get(), p.get()), () -> fireChangeDeletedFromIndexEvent(id.get(), ""));
       return null;
     }
   }

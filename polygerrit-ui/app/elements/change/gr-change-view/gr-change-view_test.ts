b46f6540a35675f9d13b8882812a359c23946817
Andrew Chang <andrewjc@google.com>
2026-04-28 13:42:13 -0700

    Integrate automatic commit message suggestions, no UI changes.
    Google-Bug-ID:b/505405738
    Release-Notes: skip
    Change-Id: Ib859a7597bedaa32f0f7a444044d3816e7b58466

--- polygerrit-ui/app/elements/change/gr-change-view/gr-change-view_test.ts
+++ polygerrit-ui/app/elements/change/gr-change-view/gr-change-view_test.ts
@@ -80,6 +80,7 @@ import {testResolver} from '../../../test/common-test-setup';
 import {UserModel, userModelToken} from '../../../models/user/user-model';
 import {pluginLoaderToken} from '../../shared/gr-js-api-interface/gr-plugin-loader';
 import {commentsModelToken} from '../../../models/comments/comments-model';
+import {ChatModel, chatModelToken} from '../../../models/chat/chat-model';
 
 suite('gr-change-view tests', () => {
   let element: GrChangeView;
@@ -1497,6 +1498,70 @@ suite('gr-change-view tests', () => {
       label: 'Change URL',
       shortcut: 'u',
       value: 'http://localhost:9876/review/c/test-project/+/42',
+    });
+  });
+
+  suite('commit message suggestion', () => {
+    let chatModel: ChatModel;
+    let fetchStub: sinon.SinonStub;
+
+    setup(() => {
+      chatModel = testResolver(chatModelToken);
+      fetchStub = sinon.stub(chatModel, 'fetchCommitMessageSuggestion');
+    });
+
+    teardown(() => {
+      fetchStub.restore();
+    });
+
+    test('maybeFetchCommitMessageSuggestion calls fetch if flag is enabled', async () => {
+      stubFlags('isEnabled').returns(true);
+
+      element.change = {
+        ...createChangeViewChange(),
+        _number: 42 as NumericChangeId,
+        current_revision: 'rev_1' as CommitId,
+      } as ParsedChangeInfo;
+
+      await element.updateComplete;
+
+      assert.isTrue(fetchStub.calledOnce);
+    });
+
+    test('maybeFetchCommitMessageSuggestion does not call fetch if flag is disabled', async () => {
+      stubFlags('isEnabled').returns(false);
+
+      element.change = {
+        ...createChangeViewChange(),
+        _number: 42 as NumericChangeId,
+        current_revision: 'rev_1' as CommitId,
+      } as ParsedChangeInfo;
+
+      await element.updateComplete;
+
+      assert.isFalse(fetchStub.called);
+    });
+
+    test("maybeFetchCommitMessageSuggestion does not call fetch if revision hasn't changed", async () => {
+      stubFlags('isEnabled').returns(true);
+
+      const change = {
+        ...createChangeViewChange(),
+        _number: 42 as NumericChangeId,
+        current_revision: 'rev_1' as CommitId,
+      } as ParsedChangeInfo;
+
+      element.change = change;
+      await element.updateComplete;
+
+      fetchStub.resetHistory();
+
+      element.change = {
+        ...change,
+      };
+      await element.updateComplete;
+
+      assert.isFalse(fetchStub.called);
     });
   });
 });

f7b944615d388c3e00077c82cc87a6ae175bf00b
Erik Flodin <erik@flodin.me>
2026-05-07 17:02:11 +0200

    Use validation message enum for receive commits warnings
    This will then output "WARNING" instead of "warning" and align with
    other validators.
    
    To further align the output with branch commit validators use "commit"
    prefix before abbreviated commit hash.
    
    Release-Notes: Change commit validation output format
    
    Change-Id: I1c991f0edf8d75b9950a27c1b0efea0855c59e34

--- javatests/com/google/gerrit/acceptance/git/AbstractPushForReview.java
+++ javatests/com/google/gerrit/acceptance/git/AbstractPushForReview.java
@@ -499,7 +499,7 @@ public abstract class AbstractPushForReview extends AbstractDaemonTest {
         .committer(new PersonIdent(admin.newIdent(), testRepo.getInstant()))
         .create();
     PushResult result = pushHead(testRepo, "refs/for/master");
-    assertThat(result.getMessages()).contains("warning: pushing without Change-Id is deprecated");
+    assertThat(result.getMessages()).contains("WARNING: pushing without Change-Id is deprecated");
   }
 
   @Test
@@ -3027,7 +3027,7 @@ public abstract class AbstractPushForReview extends AbstractDaemonTest {
     assertPushOk(pr, r);
     assertThat(pr.getMessages())
         .contains(
-            "warning: no changes between prior commit "
+            "WARNING: no changes between prior commit "
                 + abbreviateName(c)
                 + " and new commit "
                 + abbreviateName(amended));
@@ -3056,7 +3056,8 @@ public abstract class AbstractPushForReview extends AbstractDaemonTest {
     pr = pushHead(testRepo, r, false);
     assertPushOk(pr, r);
     assertThat(pr.getMessages())
-        .contains("warning: " + abbreviateName(amended) + ": no files changed, message updated");
+        .contains(
+            "WARNING: commit " + abbreviateName(amended) + ": no files changed, message updated");
   }
 
   @Test
@@ -3080,7 +3081,8 @@ public abstract class AbstractPushForReview extends AbstractDaemonTest {
     pr = pushHead(testRepo, r, false);
     assertPushOk(pr, r);
     assertThat(pr.getMessages())
-        .contains("warning: " + abbreviateName(amended) + ": no files changed, author changed");
+        .contains(
+            "WARNING: commit " + abbreviateName(amended) + ": no files changed, author changed");
   }
 
   @Test
@@ -3107,7 +3109,7 @@ public abstract class AbstractPushForReview extends AbstractDaemonTest {
     pr = pushHead(testRepo, r, false);
     assertPushOk(pr, r);
     assertThat(pr.getMessages())
-        .contains("warning: " + abbreviateName(amended) + ": no files changed, was rebased");
+        .contains("WARNING: commit " + abbreviateName(amended) + ": no files changed, was rebased");
   }
 
   @Test

6bc95746ef0cbc3c178a2f706fe9982fa779f688
Daniele Sassoli <danielesassoli@gmail.com>
2026-05-08 18:00:20 +0100

    Add support for change-id formats with tilde in config/server/index.changes api
    Change Ibae7643e introduced a new flag to delete changes from the index
    when they're missing from notedb, however, no tests where added at the
    time. Once added, the tests highlited that the API wouldn't work with
    ids in the format project~changeNumber or project~branch~changeId.
    
    Tests config/server/index.changes api when deleting changes that are
    missing from notedb. Also update deletion logic to support more changeId
    formats.
    
    Release-Notes: Add support for change-id formats with tilde in
    config/server/index.changes api
    Change-Id: Ied7818ca96bdbe934d17b05bb6379e47aee83300
    
    Change-Id: I8d7e788a31452d7f0e61e3e6f7f9ac3b373e6236

--- java/com/google/gerrit/server/restapi/config/IndexChanges.java
+++ java/com/google/gerrit/server/restapi/config/IndexChanges.java
@@ -39,7 +39,7 @@ public class IndexChanges implements RestModifyView<ConfigResource, Input> {
 
   public static class Input {
     public Set<String> changes;
-    boolean deleteMissing;
+    public boolean deleteMissing;
   }
 
   private final ChangeFinder changeFinder;
@@ -66,7 +66,9 @@ public class IndexChanges implements RestModifyView<ConfigResource, Input> {
       if (notes.isEmpty()) {
         logger.atWarning().log("Change %s missing in NoteDb", id);
         if (input.deleteMissing) {
-          Optional<Change.Id> changeId = Change.Id.tryParse(id);
+          int tilde = id.lastIndexOf('~');
+          String numericPart = tilde >= 0 ? id.substring(tilde + 1) : id;
+          Optional<Change.Id> changeId = Change.Id.tryParse(numericPart);
           if (changeId.isPresent()) {
             logger.atWarning().log("Deleting change %s from index", changeId.get());
             indexer.delete(changeId.get());

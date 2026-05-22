cb635be8ff8de4df911b3bd36632a5077e4f6cd9
Antonio Barone <syntonyze@gmail.com>
2026-05-22 15:18:09 +0200

    Return 200 OK when checking mergeability of already merged change
    Checking the mergeability of an already merged change should not cause a
    409 Conflict error to the client.
    
    The endpoint payload already has the necessary field-set to notify the
    client of the mergeability status.
    
    Release-Notes: Stop mergeable endpoint returning 409 for merged changes
    Change-Id: Ief8903b3a5d7eba2c1fa88e30a7bc124fe8defa3

--- java/com/google/gerrit/server/restapi/change/Mergeable.java
+++ java/com/google/gerrit/server/restapi/change/Mergeable.java
@@ -28,7 +28,6 @@ import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ResourceConflictException;
 import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestReadView;
-import com.google.gerrit.server.ChangeUtil;
 import com.google.gerrit.server.change.MergeabilityCache;
 import com.google.gerrit.server.change.MergeabilityComputationBehavior;
 import com.google.gerrit.server.change.RevisionResource;
@@ -102,9 +101,7 @@ public class Mergeable implements RestReadView<RevisionResource> {
     PatchSet ps = resource.getPatchSet();
     MergeableInfo result = new MergeableInfo();
 
-    if (!change.isNew()) {
-      throw new ResourceConflictException("change is " + ChangeUtil.status(change));
-    } else if (!ps.id().equals(change.currentPatchSetId())) {
+    if (!ps.id().equals(change.currentPatchSetId())) {
       // Only the current revision is mergeable. Others always fail.
       return Response.ok(result);
     }
@@ -119,6 +116,14 @@ public class Mergeable implements RestReadView<RevisionResource> {
           projectCache.get(change.getProject()).orElseThrow(illegalState(change.getProject()));
       String strategy = mergeUtilFactory.create(projectState).mergeStrategyName();
       result.strategy = strategy;
+
+      if (change.isMerged()) {
+        result.mergeable = false;
+        result.commitMerged = true;
+        result.contentMerged = true;
+        return Response.ok(result);
+      }
+
       result.mergeable = isMergable(git, change, commit, ref, result.submitType, strategy);
 
       if (otherBranches) {

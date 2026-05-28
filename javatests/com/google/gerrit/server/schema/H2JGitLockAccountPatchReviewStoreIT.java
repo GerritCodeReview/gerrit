36c17cf15ad80c62cb4966c7e83417176885735c
Adithya Chakilam <achakilam@nvidia.com>
2026-05-28 17:10:09 -0500

    Make accountPatchReviewDb work with shared-disk multi-primary setup
    Introduces pluggable locking for H2-based AccountPatchReviewDb,
    allowing a single H2 file on a shared filesystem (e.g. NFS) to be
    used safely across multiple processes without requiring a separate
    external database.
    
    Introduce abstract base that opens H2 with FILE_LOCK=NO and
    delegates lock acquisition to sub implementations. No connection
    pool is maintained; each operation opens a fresh connection.
    git-style locking implementation using JGit's LockFile is added.
    
    Lock acquisition uses a deadline-based retry loop with exponential
    backoff (1 ms initial, 500 ms max).
    
    Release-Notes: accountPatchReviewDb now works with h2 on shared-disk multi-primary setup
    Change-Id: I730abde0e60b2893f20e7d3f8d81217b4297f2ad

---
+++ javatests/com/google/gerrit/server/schema/H2JGitLockAccountPatchReviewStoreIT.java
@@ -0,0 +1,125 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.server.schema;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.entities.PatchSet;
+import com.google.gerrit.server.config.SitePaths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class H2JGitLockAccountPatchReviewStoreIT {
+  @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
+  private H2JGitLockAccountPatchReviewStore store;
+  private static final Account.Id ACCOUNT = Account.id(1);
+  private static final PatchSet.Id PS = PatchSet.id(Change.id(1), 1);
+  private static final String FILE = "foo/bar.txt";
+
+  @Before
+  public void setUp() throws Exception {
+    SitePaths sitePaths = new SitePaths(temporaryFolder.getRoot().toPath());
+    Files.createDirectories(sitePaths.db_dir);
+    Config cfg = new Config();
+    store = new H2JGitLockAccountPatchReviewStore(cfg, sitePaths);
+    store.start();
+  }
+
+  @Test
+  public void markAndFindReviewed() {
+    assertThat(store.findReviewed(PS, ACCOUNT)).isEmpty();
+
+    var unused = store.markReviewed(PS, ACCOUNT, FILE);
+
+    assertThat(store.findReviewed(PS, ACCOUNT)).isPresent();
+    assertThat(store.findReviewed(PS, ACCOUNT).get().files()).containsExactly(FILE);
+  }
+
+  @Test
+  public void clearReviewedFile() {
+    var unused = store.markReviewed(PS, ACCOUNT, FILE);
+    assertThat(store.findReviewed(PS, ACCOUNT)).isPresent();
+
+    store.clearReviewed(PS, ACCOUNT, FILE);
+
+    assertThat(store.findReviewed(PS, ACCOUNT)).isEmpty();
+  }
+
+  @Test
+  public void clearReviewedPatchSet() {
+    var unused = store.markReviewed(PS, ACCOUNT, FILE);
+    assertThat(store.findReviewed(PS, ACCOUNT)).isPresent();
+    assertThat(store.findReviewed(PS, ACCOUNT).get().files()).containsExactly(FILE);
+
+    store.clearReviewed(PS);
+
+    assertThat(store.findReviewed(PS, ACCOUNT)).isEmpty();
+  }
+
+  @Test
+  public void concurrentMarksAreAllDurable() throws Exception {
+    int nThreads = 8;
+    int filesPerThread = 5;
+    CyclicBarrier startGate = new CyclicBarrier(nThreads);
+    AtomicReference<Throwable> firstFailure = new AtomicReference<>();
+
+    List<Thread> threads = new ArrayList<>();
+    for (int t = 0; t < nThreads; t++) {
+      int threadIdx = t;
+      threads.add(
+          new Thread(
+              () -> {
+                try {
+                  startGate.await(); // all threads start simultaneously
+                  for (int f = 0; f < filesPerThread; f++) {
+                    var unused =
+                        store.markReviewed(PS, ACCOUNT, "file-" + threadIdx + "-" + f + ".txt");
+                  }
+                } catch (Throwable e) {
+                  firstFailure.compareAndSet(null, e);
+                }
+              }));
+    }
+
+    threads.forEach(Thread::start);
+    for (Thread t : threads) {
+      t.join();
+    }
+
+    assertThat(firstFailure.get()).isNull();
+
+    ImmutableSet<String> expected =
+        IntStream.range(0, nThreads)
+            .boxed()
+            .flatMap(
+                t ->
+                    IntStream.range(0, filesPerThread)
+                        .mapToObj(f -> "file-" + t + "-" + f + ".txt"))
+            .collect(ImmutableSet.toImmutableSet());
+    assertThat(store.findReviewed(PS, ACCOUNT).get().files()).isEqualTo(expected);
+  }
+}

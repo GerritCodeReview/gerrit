b5f84af4b5a58a358af3cae07f28a171e364572b
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-30 19:42:10 +0200

    Abstract ServerConfigCacheImpl into a generic interface
    Do not refer directly to ServerConfigCacheImpl, but rather use a generic
    interface bound at the Guice level, where the implementation details on
    how the ServerConfig is obtained from the cache are hidden.
    
    Hiding the implementation details allows for a cleaner decoupling from
    the IndexServlet and future improvement of the way caching is
    implemented in ServerConfigCacheImpl.
    
    Also, rely on Java 21 record rather than autovalues, which are gradually
    abandoned in the Gerrit project.
    
    Release-Notes: skip
    Change-Id: I64a9ea8efd351559ae2bbaab477f5b0ba358fdba

--- javatests/com/google/gerrit/httpd/raw/IndexServletTest.java
+++ javatests/com/google/gerrit/httpd/raw/IndexServletTest.java
@@ -29,6 +29,7 @@ import com.google.gerrit.extensions.api.config.Config;
 import com.google.gerrit.extensions.api.config.Server;
 import com.google.gerrit.extensions.common.ServerInfo;
 import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.server.config.ServerConfigCache;
 import com.google.gerrit.server.config.ServerConfigCacheImpl;
 import com.google.gerrit.server.experiments.ConfigExperimentFeatures;
 import com.google.gerrit.server.experiments.ExperimentFeatures;
@@ -72,9 +73,9 @@ public class IndexServletTest {
     serverConfig.setStringList(
         "experiments", null, "disabled", ImmutableList.of("DisabledFeature"));
     ExperimentFeatures experimentFeatures = new ConfigExperimentFeatures(serverConfig);
-    com.google.common.cache.Cache<
-            String, com.google.gerrit.server.config.ServerConfigCacheImpl.ServerConfigData>
-        serverConfigCache = com.google.common.cache.CacheBuilder.newBuilder().build();
+    Cache<String, ServerConfigCache.ServerConfigData> cache =
+        com.google.common.cache.CacheBuilder.newBuilder().build();
+    ServerConfigCache serverConfigCache = new ServerConfigCacheImpl(cache, gerritApi);
     IndexServlet servlet =
         new IndexServlet(
             testCanonicalUrl,
@@ -137,8 +138,9 @@ public class IndexServletTest {
     when(gerritApi.accounts()).thenReturn(accountsApi);
     when(gerritApi.config()).thenReturn(configApi);
 
-    Cache<String, ServerConfigCacheImpl.ServerConfigData> serverConfigCache =
+    Cache<String, ServerConfigCache.ServerConfigData> cache =
         com.google.common.cache.CacheBuilder.newBuilder().build();
+    ServerConfigCache serverConfigCache = new ServerConfigCacheImpl(cache, gerritApi);
 
     IndexServlet servlet =
         new IndexServlet(

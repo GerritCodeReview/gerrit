b8b31822daca0629c3593d0ce364f239ef9171fe
Ivan Frade <ifrade@google.com>
2026-06-03 11:05:02 +0300

    PushPermissionsIT: Use proper branch name in test
    JGit now has a stricter check on branch names [1] and this test fails
    because refs/main is not valid ref.
    
    Push to refs/heads/main, as the branch itself is irrelevant to the test.
    
    [1] https://gerrithub.io/c/eclipse-jgit/jgit/+/202190
    
    Release-Notes: skip
    Change-Id: I529e1c037b496e1b49200b83f254bd7830e0e54f

--- javatests/com/google/gerrit/acceptance/git/PushPermissionsIT.java
+++ javatests/com/google/gerrit/acceptance/git/PushPermissionsIT.java
@@ -425,14 +425,14 @@ public class PushPermissionsIT extends AbstractDaemonTest {
     // We use "refs/main" instead of "refs/heads/main", because the latter only allows commits.
     {
       // An extra colon (:) makes it a tree reference
-      PushResult r = push(commit.getId().getName() + "::refs/main");
+      PushResult r = push(commit.getId().getName() + "::refs/heads/main");
       RemoteRefUpdate refUpdate = r.getRemoteUpdates().stream().findFirst().get();
       assertThat(refUpdate.getStatus()).isEqualTo(Status.REJECTED_OTHER_REASON);
       assertThat(refUpdate.getMessage()).contains("is neither Commit or Tag");
     }
 
     {
-      PushResult r = push(commit.getTree().getId().getName() + ":refs/main");
+      PushResult r = push(commit.getTree().getId().getName() + ":refs/heads/main");
       RemoteRefUpdate refUpdate = r.getRemoteUpdates().stream().findFirst().get();
       assertThat(refUpdate.getStatus()).isEqualTo(Status.REJECTED_OTHER_REASON);
       assertThat(refUpdate.getMessage()).contains("is neither Commit or Tag");

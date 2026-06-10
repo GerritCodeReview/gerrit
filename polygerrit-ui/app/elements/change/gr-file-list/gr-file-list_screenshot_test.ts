8e154d5aec2ec95cae04805aa37c734ca2b194d5
beckysiegel <beckysiegel@google.com>
2026-06-10 20:41:01 +0000

    COPYBARA CHANGE
    PiperOrigin-RevId: 930051365
    Change-Id: I6db4f2c69b4919d84058b4e32b31dd161eaad9b6

--- polygerrit-ui/app/elements/change/gr-file-list/gr-file-list_screenshot_test.ts
+++ polygerrit-ui/app/elements/change/gr-file-list/gr-file-list_screenshot_test.ts
@@ -3,29 +3,34 @@
  * Copyright 2022 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
+
+import {fixture, html} from '@open-wc/testing';
 import '../../../test/common-test-setup';
 import '../../shared/gr-date-formatter/gr-date-formatter';
-import {fixture, html} from '@open-wc/testing';
 // Until https://github.com/modernweb-dev/web/issues/2804 is fixed
 // @ts-ignore
 import {visualDiff} from '@web/test-runner-visual-regression';
-import {FileInfo, PARENT, RevisionPatchSetNum} from '../../../api/rest-api';
-import {normalize} from '../../../models/change/files-model';
-import {PatchRange} from '../../../types/common';
 import {DiffPreferencesInfo} from '../../../api/diff';
-import {GrFileList, NormalizedFileInfo} from './gr-file-list';
-import './gr-file-list';
+import {FileInfo, PARENT, RevisionPatchSetNum} from '../../../api/rest-api';
+import {
+  filesModelToken,
+  normalize
+} from '../../../models/change/files-model';
+import {testResolver} from '../../../test/common-test-setup';
 import {visualDiffDarkTheme} from '../../../test/test-utils';
+import {PatchRange} from '../../../types/common';
+import './gr-file-list';
+import {GrFileList, NormalizedFileInfo} from './gr-file-list';
 
 suite('gr-file-list screenshot tests', () => {
   let element: GrFileList;
 
   function createFiles(
     count: number,
-    fileInfo: FileInfo
+    fileInfo: FileInfo,
   ): NormalizedFileInfo[] {
-    return Array.from(Array(count).keys()).map(index =>
-      normalize(fileInfo, `/file${index}`)
+    return Array.from({length: count}).map((_, index) =>
+      normalize(fileInfo, `/file${index}`),
     );
   }
 
@@ -46,7 +51,7 @@ suite('gr-file-list screenshot tests', () => {
         .patchNum=${patchRange.patchNum}
         .basePatchNum=${patchRange.basePatchNum}
         .diffPrefs=${diffPrefs}
-      ></gr-file-list>`
+      ></gr-file-list>`,
     );
   });
 
@@ -59,5 +64,19 @@ suite('gr-file-list screenshot tests', () => {
 
     await visualDiff(element, 'gr-file-list');
     await visualDiffDarkTheme(element, 'gr-file-list');
+  });
+
+  test('screenshot with safety limit warning', async () => {
+    const filesModel = testResolver(filesModelToken);
+    filesModel.updateState({
+      rawFiles: [
+        ...createFiles(3, {lines_inserted: 9}),
+        ...createFiles(1000, {lines_deleted: 14}),
+      ],
+    });
+    await element.updateComplete;
+
+    await visualDiff(element, 'gr-file-list-warning');
+    await visualDiffDarkTheme(element, 'gr-file-list-warning');
   });
 });

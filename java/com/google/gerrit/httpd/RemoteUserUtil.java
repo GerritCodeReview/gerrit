a8024cb8ca26814a50efd5cb8dcd1c45a40de823
Luca Milanesio <luca.milanesio@gmail.com>
2026-06-11 14:45:32 +0100

    Introduce auth.httpTrustedProxyNetworks for securing HTTP auth
    When using auth = HTTP, Gerrit relies on receiving the incoming username
    from an HTTP header. When misusing this configuration with a remote
    proxy and having Gerrit listening to an external interface over plain
    HTTP without firewalls, the security of the incoming username is subject
    to HTTP header spoofing.
    
    Highlight the risk in the documentation and also add a mitigation of the
    problem with a new auth.httpTrustedProxyNetworks that would restrict the
    fetching of the username from HTTP to a subset of CIDRs.
    
    The solution is similar to the mitigation adopted in other projects with
    a similar vulnerability.
    
    The parsing of the auth.httpTrustedProxyNetworks is expensive because it
    involves the creation of a SubnetUtils object and the string-based
    parsing of CIDRs.
    As a mitigation of the computational costs, parse and store the subnets
    in the RemoteUserUtil during the singleton instantiation, so that incoming
    HTTP calls can reuse the SubnetUtils objects and reduce the runtime memory
    and CPU footprint.
    
    Also, with regards to the subnet matching, differentiate between individual
    IP matching (CIDRs ending with /32) and full networking address matching
    with SubnetUtils. The individual IP matching is a simple O(1) independently
    from the number of entries, however, the full CIDRs matching will loop
    through all the networks and check the netmasks against the IP, with a O(N)
    complexity.
    
    The detection of the proxy-IP is quite complex because involves also the
    wrapping of the X-Forwarded* headers management by Jetty, which overwrite
    the socket remote IP address with the original client IP forwarded by
    the proxy in between. It is necessary to wrap the concept of extraction
    of the address into a generic interface which can be overridden by actual
    implementations, with a default fallback to the standard servlet mechanism.
    
    In case of JettyServer, we need to extend the ForwardedReqeustCustomizer
    to remember what is the original IP before the wrapping and store into
    a request attribute. The runtime filter than then access the attribute
    transparently and, if present, use the proxy IP instead of the wrapped
    client IP. This also protects from malicious attackers trying to fake
    their client IP by spoofing also the X-Forwarded* header values.
    
    Bug: Issue 520369774
    Release-Notes: Introduce auth.httpTrustedProxyNetworks for mitigating the vulnerability of auth=HTTP or auth=HTTP_LDAP on external IPs
    Change-Id: If9809ff092520aa467fa108b4179b7b75afe0a85

--- java/com/google/gerrit/httpd/RemoteUserUtil.java
+++ java/com/google/gerrit/httpd/RemoteUserUtil.java
@@ -18,13 +18,51 @@ import static com.google.common.base.Strings.emptyToNull;
 import static com.google.common.net.HttpHeaders.AUTHORIZATION;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import com.google.common.flogger.FluentLogger;
 import com.google.common.io.BaseEncoding;
 import com.google.gerrit.common.Nullable;
+import com.google.gerrit.server.config.AuthConfig;
+import com.google.inject.ProvisionException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.net.util.SubnetUtils;
 
 @Singleton
 public class RemoteUserUtil {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final Set<SubnetUtils.SubnetInfo> trustedProxySubnets;
+  private final Set<String> trustedProxyNetworks;
+  private final ProxyAddressProvider proxyAddressProvider;
+
+  @Inject
+  RemoteUserUtil(AuthConfig authConfig, ProxyAddressProvider proxyAddressProvider) {
+    // Single-IP matching (networks ending with '/32') are evaluated
+    // faster via O(1) String matching against trustedProxyNetworks.
+    trustedProxyNetworks = authConfig.getTrustedProxyNetworks();
+
+    try {
+      trustedProxySubnets =
+          trustedProxyNetworks.stream()
+              .filter(Predicate.not(RemoteUserUtil::isSingleIp)) // Handled by O(1) String matching
+              .map(SubnetUtils::new)
+              .map(SubnetUtils::getInfo)
+              .collect(Collectors.toSet());
+      this.proxyAddressProvider = proxyAddressProvider;
+    } catch (IllegalArgumentException e) {
+      throw new ProvisionException("Invalid auth trusted proxy definition: " + e.getMessage(), e);
+    }
+  }
+
+  private static boolean isSingleIp(String network) {
+    return network.endsWith("/32");
+  }
+
   /**
    * Tries to get username from a request with following strategies:
    *
@@ -35,12 +73,14 @@ public class RemoteUserUtil {
    * </ul>
    *
    * @param req request to extract username from.
-   * @param loginHeader name of header which is used for extracting username.
+   * @param loginHeader name of header which is used for extracting username. authentication
    * @return the extracted username or null.
    */
   @Nullable
   public String getRemoteUser(HttpServletRequest req, String loginHeader) {
-    if (AUTHORIZATION.equals(loginHeader)) {
+    boolean isAuthorizationHeader = AUTHORIZATION.equals(loginHeader);
+
+    if (isAuthorizationHeader) {
       String user = emptyToNull(req.getRemoteUser());
       if (user != null) {
         // The container performed the authentication, and has the user
@@ -48,16 +88,50 @@ public class RemoteUserUtil {
         // configured to honor HTTP authentication.
         return user;
       }
-
-      // If the container didn't do the authentication we might
-      // have done it in the front-end web server. Try to split
-      // the identity out of the Authorization header and honor it.
-      String auth = req.getHeader(AUTHORIZATION);
-      return extractUsername(auth);
     }
-    // Nonstandard HTTP header. We have been told to trust this
-    // header blindly as-is.
-    return emptyToNull(req.getHeader(loginHeader));
+
+    if (!isRequestFromTrustedProxyNetworks(req)) {
+      return null;
+    }
+
+    String auth = req.getHeader(loginHeader);
+    return isAuthorizationHeader
+        ?
+        // If the container didn't do the authentication we might
+        // have done it in the front-end web server. Try to split
+        // the identity out of the Authorization header and honor it.
+        extractUsername(auth)
+        :
+        // Nonstandard HTTP header. We have been told to trust this
+        // header blindly as-is.
+        emptyToNull(auth);
+  }
+
+  private boolean isRequestFromTrustedProxyNetworks(HttpServletRequest req) {
+    if (trustedProxyNetworks.isEmpty()) {
+      return true;
+    }
+
+    String remoteAddress = proxyAddressProvider.getRemoteAddr(req);
+    if (isIpv6Address(remoteAddress)) {
+      logger.atWarning().atMostEvery(1, TimeUnit.MINUTES).log(
+          "IPv6 remote address: %s - trusted proxy enforcement supports only IPv4, HTTP header"
+              + " rejected",
+          remoteAddress);
+      return false;
+    }
+
+    if (trustedProxyNetworks.contains(remoteAddress + "/32")) {
+      return true;
+    }
+
+    if (trustedProxySubnets.stream().anyMatch(subnet -> subnet.isInRange(remoteAddress))) {
+      return true;
+    }
+
+    logger.atWarning().atMostEvery(1, TimeUnit.MINUTES).log(
+        "Untrusted remote address: %s - authentication via HTTP header rejected", remoteAddress);
+    return false;
   }
 
   /**
@@ -91,5 +165,9 @@ public class RemoteUserUtil {
     } else {
       return null;
     }
+  }
+
+  private static boolean isIpv6Address(String ipAddress) {
+    return ipAddress.contains(":");
   }
 }

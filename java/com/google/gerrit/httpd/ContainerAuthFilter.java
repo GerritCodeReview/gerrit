83d0074681e69a75e2b026e9fa512e1206eed11b
Luca Milanesio <luca.milanesio@gmail.com>
2026-06-11 11:13:08 +0100

    Introduce auth.httpheaderTrustedNetworks for securing HTTP auth
    When using auth = HTTP, Gerrit relies on receiving the incoming username
    from an HTTP header. When misuing this configuration with a remote proxy
    and having Gerrit listening to an external interface over plain HTTP
    without firewalls, the security of the incoming username is subject to
    HTTP header spoofing.
    
    Highlight the risk in the documentation and also add a mitigation of the
    problem with a new auth.httpheaderTrustedNetworks that would restrict
    the fetching of the username from HTTP to a subset of CIDRs.
    
    The solution is similar to the mitigation adopted in other projects with
    a similar vulnerability.
    
    Bug: Issue 520369774
    Release-Notes: Introduce auth.httpheaderTrustedNetworks for mitigating the vulnerability of auth=HTTP on external IPs
    Change-Id: If9809ff092520aa467fa108b4179b7b75afe0a85

--- java/com/google/gerrit/httpd/ContainerAuthFilter.java
+++ java/com/google/gerrit/httpd/ContainerAuthFilter.java
@@ -35,6 +35,7 @@ import com.google.inject.Singleton;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Pattern;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -66,6 +67,7 @@ class ContainerAuthFilter implements Filter {
   private final AccountCache accountCache;
   private final Config config;
   private final String loginHttpHeader;
+  private final Set<String> trustedProxyNetworks;
 
   @Inject
   ContainerAuthFilter(
@@ -78,6 +80,7 @@ class ContainerAuthFilter implements Filter {
     this.config = config;
 
     loginHttpHeader = firstNonNull(emptyToNull(authConfig.getLoginHttpHeader()), AUTHORIZATION);
+    trustedProxyNetworks = authConfig.getTrustedProxyNetworks();
   }
 
   @Override
@@ -98,7 +101,7 @@ class ContainerAuthFilter implements Filter {
   }
 
   private boolean verify(HttpServletRequest req, HttpServletResponse rsp) throws IOException {
-    String username = RemoteUserUtil.getRemoteUser(req, loginHttpHeader);
+    String username = RemoteUserUtil.getRemoteUser(req, loginHttpHeader, trustedProxyNetworks);
     if (username == null) {
       if (isLfsOverSshRequest(req)) {
         // LFS-over-SSH auth request cannot be authorized by container

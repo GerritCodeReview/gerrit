f1466c7e340a5a89bb96c92d1f0cab6a63b5d743
David Ostrovsky <david@ostrovsky.org>
2026-06-15 14:18:20 +0200

    Move servlet RawInput adapter to RestApiServlet
    Remove RawInputUtil.create(HttpServletRequest) from the common
    package. The only caller is RestApiServlet, so keep the servlet-shaped
    RawInput adapter at the HTTP boundary and leave RawInputUtil as a
    byte/string helper.
    
    Drop the direct servlet API dependency from common and remove the
    package from the servlet API visibility allowlist. This prevents common
    from reintroducing direct javax.servlet references without a reviewed
    lib/BUILD change.
    
    Release-Notes: skip
    Change-Id: I6f959d1b0ab7cc0559baa858cb1e44344b788b31

--- java/com/google/gerrit/common/RawInputUtil.java
+++ java/com/google/gerrit/common/RawInputUtil.java
@@ -21,7 +21,6 @@ import com.google.gerrit.extensions.restapi.RawInput;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import javax.servlet.http.HttpServletRequest;
 
 public class RawInputUtil {
   public static RawInput create(String content) {
@@ -52,22 +51,4 @@ public class RawInputUtil {
     return create(bytes, "application/octet-stream");
   }
 
-  public static RawInput create(HttpServletRequest req) {
-    return new RawInput() {
-      @Override
-      public String getContentType() {
-        return req.getContentType();
-      }
-
-      @Override
-      public long getContentLength() {
-        return req.getContentLength();
-      }
-
-      @Override
-      public InputStream getInputStream() throws IOException {
-        return req.getInputStream();
-      }
-    };
-  }
 }

f20c0d8f7771e848c77504893cb99f45ecd21a1b
David Ostrovsky <david@ostrovsky.org>
2026-06-15 12:57:47 +0200

    Use HttpStatus constants outside servlet code
    Replace servlet response status constants in non-servlet core code
    with org.apache.http.HttpStatus. The affected code only needs HTTP
    status values, not servlet request or response types.
    
    Remove stale servlet API dependencies from packages that no longer
    import servlet types, and narrow the servlet API visibility allowlist
    to the remaining direct users.
    
    Release-Notes: skip
    Change-Id: I57e3f9c9ba002105709a39798b535ae6553c1509

--- java/com/google/gerrit/server/git/validators/UploadValidationException.java
+++ java/com/google/gerrit/server/git/validators/UploadValidationException.java
@@ -14,7 +14,7 @@
 
 package com.google.gerrit.server.git.validators;
 
-import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static org.apache.http.HttpStatus.SC_OK;
 
 import org.eclipse.jgit.transport.ServiceMayNotContinueException;
 

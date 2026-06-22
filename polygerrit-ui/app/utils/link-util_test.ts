GerritCodeReview/gerrit

commit 8631e4bd599488a3c92e49b2695655318e1ecb95

Fix same-origin check for link rewrites
Avoid adding rel="noopener noreferrer" and target="_blank" attributes
to same-origin links generated by comment link rewrites.

We resolve template placeholders in the link template before checking
sameOrigin, ensuring we check the resolved URL rather than the template
string (which often looks like a relative path, e.g. '$1', causing
false-positive same-origin results).

Also updated tests to align with this behavior.

Google-Bug-Id: b/519426997
Release-Notes: skip
Change-Id: Ie94657c3e658ed44b28c8acb43d36219ee2af824

polygerrit-ui/app/utils/link-util.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import {CommentLinkInfo, CommentLinks} from '../types/common';
-import {getBaseUrl} from './url-util';
+import {getBaseUrl, sameOrigin} from './url-util';
 
 /**
  * Finds links within the base string and convert them to HTML. Config-based
@@ -115,15 +115,23 @@ function getReplacementText(
   const replacementHref = rewrite.link.startsWith('/')
     ? `${getBaseUrl()}${rewrite.link}`
     : rewrite.link;
-  const regexp = new RegExp(rewrite.match, 'g');
-  return matchedText.replace(
-    regexp,
-    createLinkTemplate(
-      replacementHref,
-      rewrite.text ?? '$&',
-      rewrite.prefix,
-      rewrite.suffix
-    )
+  // Use regexp without 'g' flag for replacement to avoid lastIndex issues
+  // when running replace multiple times.
+  const regexp = new RegExp(rewrite.match);
+  const resolvedHref = matchedText.replace(regexp, replacementHref);
+  const resolvedText = matchedText.replace(regexp, rewrite.text ?? '$&');
+  const resolvedPrefix = rewrite.prefix
+    ? matchedText.replace(regexp, rewrite.prefix)
+    : undefined;
+  const resolvedSuffix = rewrite.suffix
+    ? matchedText.replace(regexp, rewrite.suffix)
+    : undefined;
+
+  return createLinkTemplate(
+    resolvedHref,
+    resolvedText,
+    resolvedPrefix,
+    resolvedSuffix
   );
 }
 
@@ -133,9 +141,10 @@ function createLinkTemplate(
   prefix?: string,
   suffix?: string
 ) {
-  return `${
-    prefix ?? ''
-  }<a href="${href}" rel="noopener noreferrer" target="_blank">${displayText}</a>${
+  const attributes = sameOrigin(href)
+    ? ''
+    : ' rel="noopener noreferrer" target="_blank"';
+  return `${prefix ?? ''}<a href="${href}"${attributes}>${displayText}</a>${
     suffix ?? ''
   }`;
 }

polygerrit-ui/app/utils/link-util_test.ts
@@ -12,16 +12,20 @@ suite('link-util tests', () => {
     return `<a href="${href}" rel="noopener noreferrer" target="_blank">${text}</a>`;
   }
 
+  function internalLink(text: string, href: string) {
+    return `<a href="${href}">${text}</a>`;
+  }
+
   suite('link rewrites', () => {
     test('without text', () => {
       assert.equal(
         linkifyUrlsAndApplyRewrite('foo', {
           fooLinkWithoutText: {
             match: 'foo',
-            link: 'foo.gov',
+            link: 'http://foo.gov',
           },
         }),
-        link('foo', 'foo.gov')
+        link('foo', 'http://foo.gov')
       );
     });
 
@@ -30,11 +34,11 @@ suite('link-util tests', () => {
         linkifyUrlsAndApplyRewrite('foo', {
           fooLinkWithText: {
             match: 'foo',
-            link: 'foo.gov',
+            link: 'http://foo.gov',
             text: 'foo site',
           },
         }),
-        link('foo site', 'foo.gov')
+        link('foo site', 'http://foo.gov')
       );
     });
 
@@ -43,13 +47,13 @@ suite('link-util tests', () => {
         linkifyUrlsAndApplyRewrite('there are 12 foos here', {
           fooLinkWithText: {
             match: '(.*)(bug|foo)s(.*)',
-            link: '$2.gov',
+            link: 'http://$2.gov',
             text: '$2 list',
             prefix: '$1on the ',
             suffix: '$3',
           },
         }),
-        `there are 12 on the ${link('foo list', 'foo.gov')} here`
+        `there are 12 on the ${link('foo list', 'http://foo.gov')} here`
       );
     });
 
@@ -58,10 +62,10 @@ suite('link-util tests', () => {
         linkifyUrlsAndApplyRewrite('foo foo', {
           foo: {
             match: 'foo',
-            link: 'foo.gov',
+            link: 'http://foo.gov',
           },
         }),
-        `${link('foo', 'foo.gov')} ${link('foo', 'foo.gov')}`
+        `${link('foo', 'http://foo.gov')} ${link('foo', 'http://foo.gov')}`
       );
     });
   });
@@ -71,18 +75,18 @@ suite('link-util tests', () => {
       linkifyUrlsAndApplyRewrite('foobarbaz', {
         foo: {
           match: 'foo',
-          link: 'foo.gov',
+          link: 'http://foo.gov',
         },
         foobarbaz: {
           match: 'foobarbaz',
-          link: 'foobarbaz.gov',
+          link: 'http://foobarbaz.gov',
         },
         foobar: {
           match: 'foobar',
-          link: 'foobar.gov',
+          link: 'http://foobar.gov',
         },
       }),
-      link('foobarbaz', 'foobarbaz.gov')
+      link('foobarbaz', 'http://foobarbaz.gov')
     );
   });
 
@@ -91,18 +95,18 @@ suite('link-util tests', () => {
       linkifyUrlsAndApplyRewrite('foobarbaz', {
         foo: {
           match: 'baz',
-          link: 'Baz.gov',
+          link: 'http://Baz.gov',
         },
         foobarbaz: {
           match: 'foobarbaz',
-          link: 'FooBarBaz.gov',
+          link: 'http://FooBarBaz.gov',
         },
         foobar: {
           match: 'barbaz',
-          link: 'BarBaz.gov',
+          link: 'http://BarBaz.gov',
         },
       }),
-      link('foobarbaz', 'FooBarBaz.gov')
+      link('foobarbaz', 'http://FooBarBaz.gov')
     );
   });
 
@@ -111,18 +115,18 @@ suite('link-util tests', () => {
       linkifyUrlsAndApplyRewrite('foobarbaz', {
         foo: {
           match: 'foo',
-          link: 'FOO',
+          link: 'http://FOO',
         },
         oobarba: {
           match: 'oobarba',
-          link: 'OOBARBA',
+          link: 'http://OOBARBA',
         },
         baz: {
           match: 'baz',
-          link: 'BAZ',
+          link: 'http://BAZ',
         },
       }),
-      `${link('foo', 'FOO')}bar${link('baz', 'BAZ')}`
+      `${link('foo', 'http://FOO')}bar${link('baz', 'http://BAZ')}`
     );
   });
 
@@ -148,10 +152,10 @@ suite('link-util tests', () => {
           text: 'bug/$4',
         },
       }),
-      `bugs: ${link('bug/123', 'bug/123')} ${link('bug/234', 'bug/234')} ${link(
-        'bug/345',
-        'bug/345'
-      )}`
+      `bugs: ${internalLink('bug/123', 'bug/123')} ${internalLink(
+        'bug/234',
+        'bug/234'
+      )} ${internalLink('bug/345', 'bug/345')}`
     );
   });
 });

polygerrit-ui/app/elements/shared/gr-formatted-text/gr-formatted-text_test.ts
@@ -3,6 +3,7 @@
  * Copyright 2022 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
+
 import * as sinon from 'sinon';
 import '../../../test/common-test-setup';
 import {assert, fixture, html} from '@open-wc/testing';
@@ -63,7 +64,7 @@ suite('gr-formatted-text tests', () => {
         suffix: '$3',
       },
     });
-    self.CANONICAL_PATH = 'http://localhost';
+    self.CANONICAL_PATH = '';
     element = (
       await fixture(
         wrapInProvider(
@@ -128,7 +129,7 @@ suite('gr-formatted-text tests', () => {
         /* HTML */ `
           <gr-endpoint-decorator name="formatted-text-endpoint">
             <pre class="plaintext">
-          FOO<a href="a.b.c" rel="noopener noreferrer" target="_blank">foo</a>
+          FOO<a href="a.b.c">foo</a>
         </pre>
           </gr-endpoint-decorator>
         `
@@ -159,10 +160,10 @@ suite('gr-formatted-text tests', () => {
           <gr-endpoint-decorator name="formatted-text-endpoint">
             <pre class="plaintext">
             Start:
-            <a href="bug/123" rel="noopener noreferrer" target="_blank">
+            <a href="bug/123">
               bug/123
             </a>
-            <a href="bug/456" rel="noopener noreferrer" target="_blank">
+            <a href="bug/456">
               bug/456
             </a>
           </pre>
@@ -200,13 +201,7 @@ suite('gr-formatted-text tests', () => {
               LinkRewriteMe
             </a>
             text with complex link: A
-            <a
-              href="http://localhost/page?id=12"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Link 12
-            </a>
+            <a href="/page?id=12">Link 12</a>
           </pre>
           </gr-endpoint-decorator>
         `
@@ -372,13 +367,7 @@ suite('gr-formatted-text tests', () => {
                 <p>text without a link: NotA Link 15 cats</p>
                 <p>
                   text with complex link: A
-                  <a
-                    href="http://localhost/page?id=12"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    Link 12
-                  </a>
+                  <a href="/page?id=12">Link 12</a>
                 </p>
               </div>
             </gr-marked-element>
@@ -420,13 +409,7 @@ suite('gr-formatted-text tests', () => {
           </a>
         text without a link: NotA Link 15 cats
         text with complex link: A
-          <a
-            href="http://localhost/page?id=12"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Link 12
-          </a>
+          <a href="/page?id=12">Link 12</a>
         </pre>
           </gr-endpoint-decorator>
         `
@@ -951,6 +934,45 @@ An <a href="example.com">inline HTML link with [markup link](http://google.com)<
       // Do not linkify URLs without a recognized TLD.
       await checkLinking('google.foogle/path', false);
       await checkLinking('google.com.blah/path', false);
+    });
+
+    test('reproduce b/519426997: linkification in loose list', async () => {
+      element.content = '1. A Link 1234\n\n2. aaa';
+      await element.updateComplete;
+      await new Promise<void>(resolve => {
+        const listener = () => {
+          element.removeEventListener('marked-render-complete', listener);
+          resolve();
+        };
+        element.addEventListener('marked-render-complete', listener);
+        setTimeout(() => {
+          element.removeEventListener('marked-render-complete', listener);
+          resolve();
+        }, 100);
+      });
+
+      assert.shadowDom.equal(
+        element,
+        /* HTML */ `
+          <gr-endpoint-decorator name="formatted-text-endpoint">
+            <gr-marked-element>
+              <div slot="markdown-html" class="markdown-html">
+                <ol>
+                  <li>
+                    <p>
+                      A
+                      <a href="/page?id=1234">Link 1234</a>
+                    </p>
+                  </li>
+                  <li>
+                    <p>aaa</p>
+                  </li>
+                </ol>
+              </div>
+            </gr-marked-element>
+          </gr-endpoint-decorator>
+        `
+      );
     });
 
     suite('user suggest fix', () => {

commit 821c388865cb4236e032ab77cd2085a8475462ce

Fix extra dot in linkified Flag fields
When resolving commentlinks, getReplacementText was re-evaluating the
regex standalone on the matched text. This caused mismatches when the
regex relied on context that was lost in the standalone string, such as
word boundaries (\b) at the end of the match.

This regression was introduced in commit 8631e4bd59
(https://gerrit.googlesource.com/gerrit/+/8631e4bd599488a3c92e49b2695655318e1ecb95),
which resolved templates before creating the link template to fix
same-origin checks.

For example, with "Flag: build.RELEASE...", the regex matched
"Flag: build." in context because of the boundary before "RELEASE". But
standalone, "Flag: build." has no boundary at the end, so the regex
only matched "Flag: build", leaving the dot "." as unmatched trailing
text. This unmatched dot was then incorrectly prepended to the link,
rendering as "Flag: .build.RELEASE...".

Fix this by reusing the original match groups (RegExpExecArray) that
were found in the base string, avoiding re-evaluation entirely. This
preserves the same-origin check fix from 8631e4bd59 but avoids the
context-loss regression.

Google-Bug-Id: b/526406326
Release-Notes: Fix extra dot in linkified Flag fields
Change-Id: I8d245b033b9ed7ccb0b3647f2a79efe496604bdb

polygerrit-ui/app/utils/link-util.ts
@@ -39,7 +39,7 @@ function getRewriteResultsFromConfig(
     let match: RegExpExecArray | null;
 
     while ((match = regexp.exec(base)) !== null) {
-      const fullReplacementText = getReplacementText(match[0], rewrite);
+      const fullReplacementText = getReplacementText(match, rewrite);
       // The replacement may not be changing the entire matched substring so we
       // "trim" the replacement position and text to the part that is actually
       // different. This makes sure that unchanged portions are still eligible
@@ -104,31 +104,41 @@ function applyRewrites(base: string, rewriteResults: RewriteResult[]): string {
   );
 }
 
+function resolveTemplate(template: string, match: RegExpExecArray): string {
+  return template.replace(/\$(\$|&|\d+)/g, (placeholder, p1) => {
+    if (p1 === '$') return '$';
+    if (p1 === '&') return match[0];
+    const index = parseInt(p1, 10);
+    if (!isNaN(index)) {
+      return match[index] ?? '';
+    }
+    return placeholder;
+  });
+}
+
 /**
  * For a given regexp match, apply the rewrite based on the rewrite's type and
  * return the resulting string.
  */
 function getReplacementText(
-  matchedText: string,
+  match: RegExpExecArray,
   rewrite: CommentLinkInfo
 ): string {
-  const replacementHref = rewrite.link.startsWith('/')
-    ? `${getBaseUrl()}${rewrite.link}`
-    : rewrite.link;
-  // Use regexp without 'g' flag for replacement to avoid lastIndex issues
-  // when running replace multiple times.
-  const regexp = new RegExp(rewrite.match);
-  const resolvedHref = matchedText.replace(regexp, replacementHref);
-  const resolvedText = matchedText.replace(regexp, rewrite.text ?? '$&');
+  const resolvedHref = resolveTemplate(rewrite.link, match);
+  const resolvedText = resolveTemplate(rewrite.text ?? '$&', match);
   const resolvedPrefix = rewrite.prefix
-    ? matchedText.replace(regexp, rewrite.prefix)
+    ? resolveTemplate(rewrite.prefix, match)
     : undefined;
   const resolvedSuffix = rewrite.suffix
-    ? matchedText.replace(regexp, rewrite.suffix)
+    ? resolveTemplate(rewrite.suffix, match)
     : undefined;
 
+  const replacementHref = resolvedHref.startsWith('/')
+    ? `${getBaseUrl()}${resolvedHref}`
+    : resolvedHref;
+
   return createLinkTemplate(
-    resolvedHref,
+    replacementHref,
     resolvedText,
     resolvedPrefix,
     resolvedSuffix

polygerrit-ui/app/utils/link-util_test.ts
@@ -68,6 +68,20 @@ suite('link-util tests', () => {
         `${link('foo', 'http://foo.gov')} ${link('foo', 'http://foo.gov')}`
       );
     });
+
+    test('boundary match with trailing non-word character in match group', () => {
+      assert.equal(
+        linkifyUrlsAndApplyRewrite('Flag: build.RELEASE', {
+          flag: {
+            match: '(^|\\s)([fF][lL][aA][gG][:=]\\s*)([a-z0-9_\\.]+)\\b',
+            link: 'http://flag/$3',
+            prefix: '$1$2',
+            text: '$3',
+          },
+        }),
+        `Flag: ${link('build.', 'http://flag/build.')}RELEASE`
+      );
+    });
   });
 
   test('for overlapping rewrites prefer the latest ending', () => {
37f7b42cbc3d0e7c6282980481b83fb18e28b94c
Sam Saccone <samccone@google.com>
2026-07-07 23:49:36 +0000

    Hide service account reviewer update messages behind "Show all entries"
    Treat reviewer update and reviewer addition/removal messages (e.g. "Added to reviewer:")
    created by service users (bots) as unimportant by default in `computeIsImportant`, so
    they are hidden behind the "Show all entries" toggle instead of cluttering the change log.
    
    Release-Notes: skip
    Change-Id: I37fa7dd2e12dcd7a78e66fd323763013a8eea5ee

--- polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list_test.ts
+++ polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list_test.ts
@@ -7,7 +7,7 @@ import * as sinon from 'sinon';
 import '../../../test/common-test-setup';
 import './gr-messages-list';
 import {CombinedMessage, GrMessagesList, TEST_ONLY} from './gr-messages-list';
-import {MessageTag} from '../../../constants/constants';
+import {AccountTag, MessageTag} from '../../../constants/constants';
 import {
   query,
   queryAll,
@@ -486,6 +486,30 @@ suite('gr-messages-list tests', () => {
       assert.isTrue(TEST_ONLY.computeIsImportant(m1, [m1, m2, m3]));
       assert.isFalse(TEST_ONLY.computeIsImportant(m2, [m1, m2, m3]));
       assert.isFalse(TEST_ONLY.computeIsImportant(m3, [m1, m2, m3]));
+    });
+
+    test('isImportant service user reviewer update vs other messages', () => {
+      const reviewerUpdateFromBot = randomMessage({
+        author: {
+          _account_id: 123 as AccountId,
+          tags: [AccountTag.SERVICE_USER],
+        },
+        tag: MessageTag.TAG_REVIEWER_UPDATE as ReviewInputTag,
+        message: 'Added to reviewer: Performance Presubmit',
+      });
+      const commentFromBot = randomMessage({
+        author: {
+          _account_id: 123 as AccountId,
+          tags: [AccountTag.SERVICE_USER],
+        },
+        message: 'Build succeeded: 10 tests passed',
+      });
+      assert.isFalse(
+        TEST_ONLY.computeIsImportant(reviewerUpdateFromBot, [reviewerUpdateFromBot])
+      );
+      assert.isTrue(
+        TEST_ONLY.computeIsImportant(commentFromBot, [commentFromBot])
+      );
     });
 
     test('isImportant is evaluated after tag update', async () => {

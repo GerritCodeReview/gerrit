bbdc096823a70e9cee3a5d5ba2b4a3746540e07c
Sam Saccone <samccone@google.com>
2026-08-22 20:29:54 +0000

    Highlight unresolved comments in submit group on confirm submit dialog
    When submitting a change, the confirmation dialog previously only
    checked and displayed unresolved comments on the current change.
    If other changes in the submit group (e.g. parent CLs or topic changes)
    had unresolved comments, no warning was presented in the dialog.
    
    Observe submittedTogether$ from RelatedChangesModel in
    gr-confirm-submit-dialog and render a warning with links to any other
    changes in the submit group that have unresolved comments along with
    their respective unresolved comment counts.
    
    Google-Bug-Id: b/251276938
    Release-Notes: Highlight unresolved comments in submit group on submit confirmation dialog
    TAG=agy
    CONV=56a4c4d5-7e67-489d-bb3e-7f8a4eb5362b
    
    Change-Id: I32211febe4e28c46e94a3dbbade5b3e4f91a8557

---
+++ polygerrit-ui/app/elements/change/gr-confirm-submit-dialog/gr-confirm-submit-dialog_screenshot_test.ts
@@ -0,0 +1,68 @@
+/**
+ * @license
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import '../../../test/common-test-setup';
+import './gr-confirm-submit-dialog';
+import {fixture, html} from '@open-wc/testing';
+// Until https://github.com/modernweb-dev/web/issues/2804 is fixed
+// @ts-expect-error
+import {visualDiff} from '@web/test-runner-visual-regression';
+import {GrConfirmSubmitDialog} from './gr-confirm-submit-dialog';
+import {
+  createChange,
+  createParsedChange,
+  createSubmittedTogetherInfo,
+} from '../../../test/test-data-generators';
+import {NumericChangeId} from '../../../types/common';
+import {visualDiffDarkTheme} from '../../../test/test-utils';
+
+suite('gr-confirm-submit-dialog screenshot tests', () => {
+  let element: GrConfirmSubmitDialog;
+
+  setup(async () => {
+    element = await fixture<GrConfirmSubmitDialog>(
+      html`<gr-confirm-submit-dialog></gr-confirm-submit-dialog>`
+    );
+    element.action = {label: 'Submit'};
+    element.change = {
+      ...createParsedChange(),
+      _number: 2241714 as NumericChangeId,
+      subject: 'Child CL: Implement feature XYZ',
+    };
+    element.submittedTogether = {
+      ...createSubmittedTogetherInfo(),
+      changes: [
+        {
+          ...createChange(),
+          _number: 2241713 as NumericChangeId,
+          subject: 'Parent CL: Refactor base service',
+          unresolved_comment_count: 2,
+        },
+        {
+          ...createChange(),
+          _number: 2241714 as NumericChangeId,
+          subject: 'Child CL: Implement feature XYZ',
+          unresolved_comment_count: 0,
+        },
+      ],
+    };
+    element.init();
+    await element.updateComplete;
+  });
+
+  test('submit group with unresolved comments on parent change (light)', async () => {
+    await visualDiff(
+      element,
+      'gr-confirm-submit-dialog-submit-group-unresolved'
+    );
+  });
+
+  test('submit group with unresolved comments on parent change (dark)', async () => {
+    await visualDiffDarkTheme(
+      element,
+      'gr-confirm-submit-dialog-submit-group-unresolved'
+    );
+  });
+});

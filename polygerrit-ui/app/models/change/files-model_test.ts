9f4ec24136e3211bffb81e54f05f93bc48754dc2
Milutin Kristofic <milutin@google.com>
2026-08-27 08:35:35 +0000

    polygerrit: Move cleanlyMergedPaths state and fetching to FilesModel
    Resolves TODO in GrFileList to move cleanlyMergedPaths state and async
    RPC fetching out of the UI element into the centralized FilesModel.
    
    FilesModel now reactively computes and updates cleanlyMergedPaths and
    cleanlyMergedOldPaths when change and patch range change, allowing
    GrFileList to subscribe to clean observable streams and eliminating
    imperative RPC fetching and RestApiService from GrFileList.
    
    Release-Notes: skip
    Change-Id: I8b26ef2693446aab85b1468bc952308b25e9dda3

---
+++ polygerrit-ui/app/models/change/files-model_test.ts
@@ -0,0 +1,167 @@
+/**
+ * @license
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import * as sinon from 'sinon';
+import '../../test/common-test-setup';
+import {assert} from '@open-wc/testing';
+import {FilesModel} from './files-model';
+import {ChangeModel} from './change-model';
+import {
+  createChangeViewState,
+  createCommit,
+  createParsedChange,
+  createRevision,
+  TEST_NUMERIC_CHANGE_ID,
+} from '../../test/test-data-generators';
+import {stubRestApi, waitUntilObserved} from '../../test/test-utils';
+import {
+  CommitId,
+  FileNameToFileInfoMap,
+  PARENT,
+  RevisionPatchSetNum,
+} from '../../types/common';
+import {getAppContext} from '../../services/app-context';
+import {testResolver} from '../../test/common-test-setup';
+import {ChangeViewModel, changeViewModelToken} from '../views/change';
+import {navigationToken} from '../../elements/core/gr-navigation/gr-navigation';
+import {userModelToken} from '../user/user-model';
+import {commentsModelToken} from '../comments/comments-model';
+import {checksModelToken} from '../checks/checks-model';
+import {pluginLoaderToken} from '../../elements/shared/gr-js-api-interface/gr-plugin-loader';
+
+suite('files-model tests', () => {
+  let changeModel: ChangeModel;
+  let changeViewModel: ChangeViewModel;
+  let filesModel: FilesModel;
+
+  setup(() => {
+    stubRestApi('getAllRevisionFiles').resolves({});
+    changeViewModel = testResolver(changeViewModelToken);
+    changeModel = new ChangeModel(
+      testResolver(navigationToken),
+      changeViewModel,
+      getAppContext().restApiService,
+      testResolver(userModelToken),
+      testResolver(pluginLoaderToken),
+      getAppContext().reportingService
+    );
+    filesModel = new FilesModel(
+      changeModel,
+      testResolver(commentsModelToken),
+      testResolver(checksModelToken),
+      getAppContext().restApiService,
+      getAppContext().reportingService
+    );
+  });
+
+  teardown(() => {
+    filesModel.finalize();
+    changeModel.finalize();
+  });
+
+  test('cleanly merged paths for merge commit', async () => {
+    stubRestApi('getChangeOrEditFiles').callsFake((_changeNum, range) => {
+      if (range?.basePatchNum === -1) {
+        return Promise.resolve({
+          'conflict.txt': {},
+          'cleanlyMerged.txt': {old_path: 'cleanlyMergedOld.txt'},
+        } as FileNameToFileInfoMap);
+      }
+      return Promise.resolve({
+        'conflict.txt': {},
+      } as FileNameToFileInfoMap);
+    });
+
+    const mergeCommit = {
+      ...createCommit(),
+      parents: [
+        {commit: 'p1' as CommitId, subject: 'parent 1'},
+        {commit: 'p2' as CommitId, subject: 'parent 2'},
+      ],
+    };
+
+    const change = {
+      ...createParsedChange(),
+      _number: TEST_NUMERIC_CHANGE_ID,
+      revisions: {
+        rev1: {
+          ...createRevision(1),
+          commit: mergeCommit,
+        },
+      },
+      current_revision: 'rev1' as CommitId,
+    };
+
+    changeViewModel.setState({
+      ...createChangeViewState(),
+      patchNum: 1 as RevisionPatchSetNum,
+      basePatchNum: PARENT,
+    });
+    changeModel.updateStateChange(change);
+
+    const cleanlyMergedPaths = await waitUntilObserved(
+      filesModel.cleanlyMergedPaths$,
+      paths => paths.length > 0
+    );
+    assert.deepEqual(cleanlyMergedPaths, ['cleanlyMerged.txt']);
+
+    const cleanlyMergedOldPaths = await waitUntilObserved(
+      filesModel.cleanlyMergedOldPaths$,
+      paths => paths.length > 0
+    );
+    assert.deepEqual(cleanlyMergedOldPaths, ['cleanlyMergedOld.txt']);
+  });
+
+  test('non-merge commit does not query -1 base', async () => {
+    const getChangeOrEditFilesStub = stubRestApi(
+      'getChangeOrEditFiles'
+    ).resolves({
+      'file1.txt': {},
+    } as FileNameToFileInfoMap);
+
+    const singleParentCommit = {
+      ...createCommit(),
+      parents: [{commit: 'p1' as CommitId, subject: 'parent 1'}],
+    };
+
+    const change = {
+      ...createParsedChange(),
+      _number: TEST_NUMERIC_CHANGE_ID,
+      revisions: {
+        rev1: {
+          ...createRevision(1),
+          commit: singleParentCommit,
+        },
+      },
+      current_revision: 'rev1' as CommitId,
+    };
+
+    changeViewModel.setState({
+      ...createChangeViewState(),
+      patchNum: 1 as RevisionPatchSetNum,
+      basePatchNum: PARENT,
+    });
+    changeModel.updateStateChange(change);
+
+    const files = await waitUntilObserved(filesModel.files$, f => f.length > 0);
+    assert.equal(files.length, 1);
+
+    // Verify getChangeOrEditFiles was not called with basePatchNum: -1
+    assert.isFalse(
+      getChangeOrEditFilesStub.calledWith(
+        TEST_NUMERIC_CHANGE_ID,
+        sinon.match({
+          basePatchNum: -1,
+        })
+      )
+    );
+
+    const cleanlyMergedPaths = await waitUntilObserved(
+      filesModel.cleanlyMergedPaths$,
+      paths => paths.length === 0
+    );
+    assert.deepEqual(cleanlyMergedPaths, []);
+  });
+});

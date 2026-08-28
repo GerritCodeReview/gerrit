e9f16255e5ffb400966002e3a73bff7ac35eb4cf
Josie Cuff <josiecuff@google.com>
2026-08-28 16:35:10 -0700

    fix: Allow a reviewer to still comment on a change post-submit
    Allow for a reviewer who voted on a submitted change to still respond to comments post-submit. Before this change, the existing label scores are resubmitted in the reply dialog. This causes the error "Voting on labels disallowed after submit" for a user only trying to post comments. This change ignores the label votes on merged changes if they are unchanged.
    
    Release-Notes: skip
    Google-Bug-Id: b/551982360
    Change-Id: Ice70ce8c8a9ced9299306d4209247d7f21abb8d5

--- polygerrit-ui/app/elements/change/gr-label-scores/gr-label-scores.ts
+++ polygerrit-ui/app/elements/change/gr-label-scores/gr-label-scores.ts
@@ -5,25 +5,16 @@
  */
 import '../gr-label-score-row/gr-label-score-row';
 import '../../../styles/shared-styles';
+
 import {css, html, LitElement, nothing} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
-import {
-  AccountInfo,
-  ChangeInfo,
-  LabelNameToValueMap,
-} from '../../../types/common';
-import {GrLabelScoreRow} from '../gr-label-score-row/gr-label-score-row';
-import {
-  computeLabels,
-  computeOrderedLabelValues,
-  getApplicableLabels,
-  getDefaultValue,
-  getTriggerVotes,
-  Label,
-} from '../../../utils/label-util';
+
+import {LabelNameToValuesMap} from '../../../api/rest-api';
 import {ChangeStatus} from '../../../constants/constants';
 import {fontStyles} from '../../../styles/gr-font-styles';
-import {LabelNameToValuesMap} from '../../../api/rest-api';
+import {AccountInfo, ChangeInfo, LabelNameToValueMap,} from '../../../types/common';
+import {computeLabels, computeOrderedLabelValues, getApplicableLabels, getDefaultValue, getTriggerVotes, getVoteForAccount, Label,} from '../../../utils/label-util';
+import {GrLabelScoreRow} from '../gr-label-score-row/gr-label-score-row';
 
 @customElement('gr-label-scores')
 export class GrLabelScores extends LitElement {
@@ -141,6 +132,7 @@ export class GrLabelScores extends LitElement {
         ?hidden=${this.change?.status !== ChangeStatus.MERGED}
       >
         Because this change has been merged, votes may not be decreased.
+        You can still reply to comments without changing your vote.
       </div>
       <div
         class="abandonedMessage"
@@ -169,7 +161,13 @@ export class GrLabelScores extends LitElement {
       if (selectedVal === undefined) continue;
 
       const defValNum = getDefaultValue(this.change?.labels, label);
-      if (includeDefaults || selectedVal !== defValNum) {
+      // The user's previous vote from the change labels.
+      const prevValStr = getVoteForAccount(label, this.account, this.change);
+      const prevValNum = prevValStr !== null ? Number(prevValStr) : defValNum;
+
+      // If includeDefaults is true, include the label.
+      // Otherwise, ONLY include it if the user actually changed their vote.
+      if (includeDefaults || selectedVal !== prevValNum) {
         labels[label] = selectedVal;
       }
     }
@@ -182,3 +180,4 @@ declare global {
     'gr-label-scores': GrLabelScores;
   }
 }
+

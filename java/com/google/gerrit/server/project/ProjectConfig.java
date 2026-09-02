0fdf65d39c5d6583c815c4daf0c32336edaf9cb2
Luca Milanesio <luca.milanesio@gmail.com>
2026-09-02 14:58:13 +0100

    Do not load or save contributor agreements for regular projects
    The contributor agreements are set on the All-Projects level; however,
    the code was still loading and saving them on a project-level basis,
    which is incorrect.
    
    Block the loading and storage of the incorrect section and ignore its
    content altogether.
    
    Change-Id: I2fa68c3efa1b69b12cc3c2e26f79dc2e3997283b

--- java/com/google/gerrit/server/project/ProjectConfig.java
+++ java/com/google/gerrit/server/project/ProjectConfig.java
@@ -695,7 +695,9 @@ public class ProjectConfig extends VersionedMetaData implements ValidationError.
     this.project = p.build();
 
     loadAccountsSection(rc);
-    loadContributorAgreements(rc);
+    if (projectName.equals(allProjectsName)) {
+      loadContributorAgreements(rc);
+    }
     loadAccessSections(rc);
     loadBranchOrderSection(rc);
     loadNotifySections(rc);
@@ -1352,7 +1354,9 @@ public class ProjectConfig extends VersionedMetaData implements ValidationError.
 
     Set<AccountGroup.UUID> keepGroups = new HashSet<>();
     saveAccountsSection(rc, keepGroups);
-    saveContributorAgreements(rc, keepGroups);
+    if (projectName.equals(allProjectsName)) {
+      saveContributorAgreements(rc, keepGroups);
+    }
     saveAccessSections(rc, keepGroups);
     saveNotifySections(rc, keepGroups);
     savePluginSections(rc, keepGroups);

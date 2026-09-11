990acabaf229ec7abe5d07d6d244ed5d9eb2717c
Luca Milanesio <luca.milanesio@gmail.com>
2026-09-11 21:03:45 +0100

    Invoke StopPluginListener also before the plugin stop
    The StopPluginListener did not have a way to access the context of the
    plugin being stopped because all its Guice context was already destroyed
    at the time of the call of onStopPlugin(), causing the inability to
    access any of the plugin's resources.
    
    Introduce a new method beforeStop() to the StopPluginListener so that
    listeners can have the right context for an orderly shutdown.
    
    Previously, the listener was pretty much helpless because the plugin
    object passed to it was an empty box, without the ability to investigate
    its resources and performing any necessary cleanup.
    
    By accessing the plugin Guice environment just before the final stop and
    unload of the plugin, the listener can now effectively investigate what
    was the status of the plugin that is being stopped.
    
    Release-Notes: Introduce StopPluginListener.beforeStopPlugin() called immediately before the stop of the plugin PluginGuiceEnvironment.
    Change-Id: Ic32d14f600a396f6a0517c739d3c7b0c37e3a2f9

--- javatests/com/google/gerrit/acceptance/api/plugin/PluginOnStartStopIT.java
+++ javatests/com/google/gerrit/acceptance/api/plugin/PluginOnStartStopIT.java
@@ -21,8 +21,8 @@ import org.junit.Test;
     name = "plugin-start-stop-listener",
     sysModule = "com.google.gerrit.acceptance.api.plugin.PluginOnStartStopIT$TestModule")
 public class PluginOnStartStopIT extends LightweightPluginDaemonTest {
-  private static final String TEST_PLUGIN = "test-plugin";
-  private static final String TEST_PLUGIN_FILENAME = TEST_PLUGIN + ".jar";
+  static final String TEST_PLUGIN = "test-plugin";
+  static final String TEST_PLUGIN_FILENAME = TEST_PLUGIN + ".jar";
 
   @Singleton
   public static class TestStartPluginListener implements StartPluginListener {
@@ -37,6 +37,12 @@ public class PluginOnStartStopIT extends LightweightPluginDaemonTest {
   @Singleton
   public static class TestStopPluginListener implements StopPluginListener {
     public volatile Plugin plugin;
+    public volatile Injector pluginInjector;
+
+    @Override
+    public void beforeStopPlugin(Plugin plugin) {
+      this.pluginInjector = plugin.getSysInjector();
+    }
 
     @Override
     public void onStopPlugin(Plugin plugin) {
@@ -80,5 +86,6 @@ public class PluginOnStartStopIT extends LightweightPluginDaemonTest {
 
     assertThat(testStopPluginListener.plugin).isNotNull();
     assertThat(testStopPluginListener.plugin.getName()).isEqualTo(TEST_PLUGIN);
+    assertThat(testStopPluginListener.pluginInjector).isNotNull();
   }
 }
